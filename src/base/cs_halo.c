b05d1845f667267627522b34d54212bc4a18bf40
Code_Saturne dev team <saturne-support@noreply.code-saturne.org>
2008-08-08 09:55:58 +0000

    Improve a halo coherency check
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/branches/Version1_3@420 e44c5d27-f830-0410-8ce8-974f7a764be6

--- src/base/cs_halo.c
+++ src/base/cs_halo.c
@@ -1990,6 +1990,13 @@ _count_in_gcell_to_dist_vtx_connect(cs_mesh_t            *mesh,
   const cs_int_t  *fac_vtx_idx = mesh->i_face_vtx_idx;
   const cs_int_t  *fac_vtx_lst = mesh->i_face_vtx_lst;
 
+  const char err_corresp[]
+    = N_("Incohérence repérée lors de la construction du halo.\n"
+         "Plusieurs points locaux ont le même correspondant distant ;\n"
+         "ceci est probablement dû à un effet de bord de la construction\n"
+         "de périodicités multiples par le Préprocesseur.\n"
+         "Coordonnées du premier point impacté: [%12.5e, %12.5e %12.5e].");
+
   _define_vtx_interface_idx(ifs,
                             halo->c_domain_rank[rank_id],
                             tr_id,
@@ -2045,12 +2052,10 @@ _count_in_gcell_to_dist_vtx_connect(cs_mesh_t            *mesh,
             if (n_added_vertices > 0) {
 
               if (n_added_vertices > 1)
-                bft_error(__FILE__, __LINE__, 0,
-                          _("Incohérence repérée lors de la construction du halo.\n"
-                            "Plusieurs points locaux ont le même correspondant\n"
-                            "distant ; ceci est probablement dû à un effet de bord\n"
-                            "de la construction de périodicités multiples par le\n"
-                            "Préprocesseur."));
+                bft_error(__FILE__, __LINE__, 0, _(err_corresp),
+                          mesh->vtx_coord[vtx_id*3],
+                          mesh->vtx_coord[vtx_id*3+1],
+                          mesh->vtx_coord[vtx_id*3+2]);
 
               /* Add this vertex if not already checked */
 
