392c8d1331679334adf778c86a514ffd49603f82
Code_Saturne dev team <saturne-support@noreply.code-saturne.org>
2009-10-19 13:47:47 +0000

    Revert old spurious commit.
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@1052 e44c5d27-f830-0410-8ce8-974f7a764be6

--- users/base/ussatc.f90
+++ users/base/ussatc.f90
@@ -31,14 +31,44 @@
 subroutine ussatc &
 !================
 
- ( )
+ ( idbia0 , idbra0 , numcpl ,                                     &
+   ndim   , ncelet , ncel   , nfac   , nfabor , nfml   , nprfml , &
+   nnod   , lndfac , lndfbr , ncelbr ,                            &
+   nituse , nrtuse ,                                              &
+   ifacel , ifabor , ifmfbr , ifmcel , iprfml , maxelt , lstelt , &
+   ipnfac , nodfac , ipnfbr , nodfbr ,                            &
+   lcesup , lfbsup , lcecpl , lfbcpl ,                            &
+   ncesup , nfbsup , ncecpl , nfbcpl ,                            &
+   xyzcen , surfac , surfbo , cdgfac , cdgfbo , xyznod)
 
 !===============================================================================
 ! FONCTION :
 ! --------
 
-!     DEFINITION DES COUPLAGES CODE_SATURNE / CODE_SATURNE
+! DEFINITION DES COUPLAGES CODE_SATURNE / CODE_SATURNE
 
+! L'identification des faces de bord concernees se fait grace
+! a la commande GETFBR.
+
+!  GETFBR(CHAINE,NLELT,LSTELT) :
+!  - CHAINE est une chaine de caractere fournie par l'utilisateur
+!    qui donne les criteres de selection
+!  - NLTELT est renvoye par la commande. C'est un entier qui
+!    correspond au nombre de faces de bord trouveees repondant au
+!    critere
+!  - LSTELT est renvoye par la commande. C'est un tableau d'entiers
+!    de taille NLTELT donnant la liste des faces de bord trouvees
+!    repondant au critere.
+
+!  CHAINE peut etre constitue de :
+!  - references de couleurs (ex. : 1, 8, 26, ...
+!  - references de groupes (ex. : entrees, groupe1, ...)
+!  - criteres geometriques (ex. X<0.1, Y>=0.25, ...)
+!  Ces criteres peuvent etre combines par des operateurs logiques
+!  (AND et OR) et des parentheses
+!  ex. : '1 AND (groupe2 OR groupe3) AND Y<1' permettra de recuperer
+!  les faces de bord de couleur 1, appartenant aux groupes 'groupe2'
+!  ou 'groupe3' et de coordonnee Y inferieure a 1.
 
 !-------------------------------------------------------------------------------
 ! Arguments
@@ -60,16 +90,48 @@ implicit none
 !===============================================================================
 
 include "paramx.h"
+include "pointe.h"
+include "numvar.h"
+include "optcal.h"
+include "cstphy.h"
+include "cstnum.h"
 include "entsor.h"
 include "parall.h"
+include "period.h"
+include "ihmpre.h"
+include "cplsat.h"
 
 !===============================================================================
 
 ! Arguments
 
-! Variables locales
+integer          idbia0 , idbra0
+integer          numcpl
+integer          ndim   , ncelet , ncel   , nfac   , nfabor
+integer          nfml   , nprfml
+integer          nnod   , lndfac , lndfbr , ncelbr
+integer          nituse , nrtuse
 
-integer          numsat, nbcsat, ii
+integer          ifacel(2,nfac) , ifabor(nfabor)
+integer          ifmfbr(nfabor) , ifmcel(ncelet)
+integer          iprfml(nfml,nprfml), maxelt, lstelt(maxelt)
+integer          ipnfac(nfac+1), nodfac(lndfac)
+integer          ipnfbr(nfabor+1), nodfbr(lndfbr)
+integer          lcesup(ncel), lfbsup(nfabor)
+integer          lcecpl(ncel), lfbcpl(nfabor)
+integer          ncecpl , nfbcpl
+integer          ncesup , nfbsup
+
+double precision xyzcen(ndim,ncelet)
+double precision surfac(ndim,nfac), surfbo(ndim,nfabor)
+double precision cdgfac(ndim,nfac), cdgfbo(ndim,nfabor)
+double precision xyznod(ndim,nnod)
+
+! VARIABLES LOCALES
+
+integer          idebia, idebra
+integer          ifac, iel
+integer          ilelt, nlelt
 
 !===============================================================================
 
@@ -82,26 +144,42 @@ if(1.eq.1) return
 !===============================================================================
 ! TEST_TO_REMOVE_FOR_USE_OF_SUBROUTINE_END
 
-numsat = -1
-nbcsat = 1
+!===============================================================================
+! 1.  DEFINITION DU SUPPORT (commun pour tous les couplages...)
+!===============================================================================
 
-do ii = 1, nbcsat
+ncesup = 0
+nfbsup = 0
 
-  ! --- On definit le couplage 1 suivant:
-
-  ! support cellule:          all[]
-  ! support face de bord:     "vide"
-  ! cellules couplees:        "vide"
-  ! faces de bord couplees:   99
-
-  if (ii.eq.1) then
-
-    call defsat(numsat, 'SATURNE_01', 'all[]', ' ', ' ', '99')
-    !==========
-
-  endif
-
+do iel = 1, ncel
+  ncesup = ncesup + 1
+  lcesup(ncesup) = iel
 enddo
+
+!===============================================================================
+! 2.  DEFINITION DES ELEMENTS COUPLES (differents pour chaque couplage..)
+!===============================================================================
+
+ncecpl = 0
+nfbcpl = 0
+
+! --- On definit une zone de couplage pour les faces de couleur 99
+!     pour le couplage numero 1
+
+if (numcpl.eq.1) then
+
+  CALL GETFBR('99',NLELT,LSTELT)
+  !==========
+  do ilelt = 1, nlelt
+
+    ifac = lstelt(ilelt)
+
+    nfbcpl = nfbcpl + 1
+    lfbcpl(nfbcpl) = ifac
+
+  enddo
+
+endif
 
 return
 end subroutine
