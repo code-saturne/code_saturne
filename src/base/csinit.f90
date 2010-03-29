b53ae2f87506993fa1f5c7eb9a828386d987c68a
Code_Saturne dev team <saturne-support@noreply.code-saturne.org>
2010-03-29 15:57:37 +0000

    Missing file in previous commit (related to Fortran/C split).
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@1216 e44c5d27-f830-0410-8ce8-974f7a764be6

--- src/base/csinit.f90
+++ src/base/csinit.f90
@@ -28,7 +28,7 @@
 subroutine csinit &
 !================
 
- ( argifo , irgpar , nrgpar , nthpar , ilisr0 , ilisrp )
+ ( argifo , irgpar , nrgpar , nthpar )
 
 !===============================================================================
 !  FONCTION  :
@@ -49,16 +49,6 @@ subroutine csinit &
 ! irgpar           ! e  ! <-- ! rang si parallele ; -1 si sequentiel           !
 ! nrgpar           ! e  ! <-- ! nombre de processus ; 1 si sequentiel          !
 ! nthpar           ! e  ! <-- ! nombre de threads                              !
-! ilisr0           ! e  ! <-- ! option de sortie du listing :                  !
-!                  !    !     !   0 : rang 0 non redirige                      !
-!                  !    !     !   1 : rang 0 dans fichier listing,             !
-! ilisrp           ! e  ! <-- ! option de sortie du listing :                  !
-!                  !    !     !   0 : rangs > 0 non rediriges                  !
-!                  !    !     !       (pour debugger par exemple)              !
-!                  !    !     !   1 : rangs > 0 rediriges dans                 !
-!                  !    !     !       fichiers listing_n*                      !
-!                  !    !     !   2 : rangs > 0 rediriges dans                 !
-!                  !    !     !       /dev/null (suppression)                  !
 !__________________!____!_____!________________________________________________!
 
 !     TYPE : E (ENTIER), R (REEL), A (ALPHANUMERIQUE), T (TABLEAU)
@@ -80,9 +70,7 @@ include "parall.h"
 
 !===============================================================================
 
-integer          argifo, irgpar, nrgpar, nthpar, ilisr0, ilisrp
-
-character        name*300
+integer          argifo, irgpar, nrgpar, nthpar
 
 !===============================================================================
 
@@ -103,59 +91,6 @@ ngrpb = 1
 !===============================================================================
 
 ifoenv = argifo
-
-!-------------------------------------------
-! --- stdout :
-!     NFECRA = 6 par défaut
-!   Les autres fichiers listing sont fermes dans csclli a la fin de cs_main.
-!-------------------------------------------
-
-nfecra = 6
-
-if (irangp.le.0) then
-  if (ilisr0.eq.1) then
-    nfecra = 9
-    name = 'listing'
-  endif
-else
-  if (ilisrp.eq.1) then
-    nfecra = 9
-    if (nrangp.ge.10000) then
-      write (name,'(A9,I7.4)') 'listing_n', irangp + 1
-    else
-      write (name,'(A9,I4.4)') 'listing_n', irangp + 1
-    endif
-  else if (ilisrp.eq.2) then
-    nfecra = 9
-    NAME = '/dev/null'
-  endif
-endif
-
-if (nfecra.eq.9) then
-   open (file=name, unit=nfecra,                                  &
-         form='FORMATTED', status='UNKNOWN', err=900)
-endif
-
-goto 950
-
- 900  write (0, 999) name
-call csexit (1)
-
- 950  continue
-
-#if defined(_CS_LANG_FR)
-
- 999  format(/,                                                   &
-'Code_Saturne : Erreur d''initialisation :',/,              &
-'Impossible d''ouvrir le fichier : ',A,/)
-
-#else
-
- 999  format(/,                                                   &
-'Code_Saturne: Initialization error:',/,                    &
-'Impossible to open the file: ',A,/)
-
-#endif
 
 return
 end subroutine
