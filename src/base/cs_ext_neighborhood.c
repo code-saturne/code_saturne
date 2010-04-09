b7f7504ffe79595ff23a107c5f4cdfc855e4af3d
Code_Saturne dev team <saturne-support@noreply.code-saturne.org>
2010-04-09 12:28:19 +0000

    Fix some integer sizes in the reduced-neighborhood algorithm (only relevant for big meshes).
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@1250 e44c5d27-f830-0410-8ce8-974f7a764be6

--- src/base/cs_ext_neighborhood.c
+++ src/base/cs_ext_neighborhood.c
@@ -58,6 +58,8 @@
 #include <bft_mem.h>
 #include <bft_printf.h>
 
+#include <fvm_parall.h>
+
 /*----------------------------------------------------------------------------
  *  Local headers
  *----------------------------------------------------------------------------*/
@@ -950,7 +952,7 @@ void
 CS_PROCF (redvse, REDVSE) (const cs_real_t  *anomax)
 {
   cs_int_t  i, face_id, cell_id, cell_i, cell_j;
-  size_t  init_cell_cells_connect_size;
+  fvm_gnum_t  init_cell_cells_connect_size;
 
   cs_real_t  v_ij[3];
   cs_real_t  face_normal[3];
@@ -958,7 +960,7 @@ CS_PROCF (redvse, REDVSE) (const cs_real_t  *anomax)
   cs_real_t  dprod;
   double     ratio;
 
-  cs_int_t  n_deleted_cells = 0;
+  fvm_gnum_t  n_deleted_cells = 0;
   cs_int_t  previous_idx = 0, new_idx = -1;
 
   cs_mesh_t  *mesh = cs_glob_mesh;
@@ -1140,13 +1142,12 @@ CS_PROCF (redvse, REDVSE) (const cs_real_t  *anomax)
 
       if (cs_glob_n_ranks > 1) {
 
-        unsigned long count_g[2];
-        unsigned long count_l[2];
+        fvm_gnum_t count_g[2], count_l[2];
 
         count_l[0] = init_cell_cells_connect_size;
         count_l[1] = n_deleted_cells;
 
-        MPI_Allreduce(count_l, count_g, 2, MPI_UNSIGNED_LONG,
+        MPI_Allreduce(count_l, count_g, 2, FVM_MPI_GNUM,
                       MPI_SUM, cs_glob_mpi_comm);
 
         init_cell_cells_connect_size = count_g[0];
