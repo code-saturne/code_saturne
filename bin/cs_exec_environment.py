c8ad8e94ac873bb974b5c18bcc339bdf1d39012c
Yvan Fournier <yvan.fournier@noreply.code-saturne.org>
2011-04-20 10:58:43 +0000

    Fix bug in hostfile handling for SGE batch system.
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@2028 e44c5d27-f830-0410-8ce8-974f7a764be6

--- bin/cs_exec_environment.py
+++ bin/cs_exec_environment.py
@@ -286,7 +286,7 @@ class resource_info(batch_info):
             else:
                 s = os.getenv('PE_HOSTFILE')
                 if s != None:
-                    self.hosts_file = 'PE_HOSTFILE'
+                    self.hosts_file = '$PE_HOSTFILE'
 
         # Check hosts file presence
 
