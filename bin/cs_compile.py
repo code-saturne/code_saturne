ab23de40dd0fbacb65b260b0db45f1907c96757a
David Monfort <david.monfort@noreply.code-saturne.org>
2011-09-30 11:30:27 +0000

    Clean Code_Saturne Copyright headers with 'Copyright 1998-2011 EDF S.A.' (Code_Saturne-only related). Years could be verified in a second step.
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@2657 e44c5d27-f830-0410-8ce8-974f7a764be6

--- bin/cs_compile.py
+++ bin/cs_compile.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 #-------------------------------------------------------------------------------
-#   This file is part of the Code_Saturne Solver.
+
+# This file is part of Code_Saturne, a general-purpose CFD tool.
 #
-#   Copyright (C) 2009-2011  EDF
+# Copyright (C) 1998-2011 EDF S.A.
 #
-#   The Code_Saturne Preprocessor is free software; you can redistribute it
-#   and/or modify it under the terms of the GNU General Public License
-#   as published by the Free Software Foundation; either version 2 of
-#   the License, or (at your option) any later version.
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
 #
-#   The Code_Saturne Preprocessor is distributed in the hope that it will be
-#   useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-#   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
 #
-#   You should have received a copy of the GNU General Public Licence
-#   along with the Code_Saturne Preprocessor; if not, write to the
-#   Free Software Foundation, Inc.,
-#   51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+# Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
 #-------------------------------------------------------------------------------
 
 import fnmatch
