b8ac6b38928c1d760f25b8fa8653b260206b5309
David Monfort <david.monfort@noreply.code-saturne.org>
2011-11-24 16:58:36 +0000

    Merge rev. 2796 from trunk (fix memory issue with FSI).
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/branches/Version2_1@2797 e44c5d27-f830-0410-8ce8-974f7a764be6

--- src/base/tridim.f90
+++ src/base/tridim.f90
@@ -1372,9 +1372,11 @@ if (iccvfg.eq.0) then
     endif
 
     ! Free memory
-    deallocate(flmalf, flmalb)
-    deallocate(cofale)
-    deallocate(xprale)
+    if (allocated(flmalf)) then
+      deallocate(flmalf, flmalb)
+      deallocate(cofale)
+      deallocate(xprale)
+    endif
 
   endif
 
