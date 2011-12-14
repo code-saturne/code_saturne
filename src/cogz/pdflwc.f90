664d297a13775af4b075cea17fdcd12f9d71f86d
David Monfort <david.monfort@noreply.code-saturne.org>
2011-12-14 16:12:46 +0000

    Revert changes to the enthalpy calculation after a discussion with P. Plion.
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/branches/Version2_1@2852 e44c5d27-f830-0410-8ce8-974f7a764be6

--- src/cogz/pdflwc.f90
+++ src/cogz/pdflwc.f90
@@ -25,7 +25,6 @@ subroutine pdflwc &
 
  ( ncelet , ncel  ,                                               &
    fm     , fp2m  , yfm    , yfp2m ,                              &
-   hm     ,                                                       &
    propce )
 
 !===============================================================================
@@ -72,7 +71,6 @@ subroutine pdflwc &
 ! fp2m             ! tr ! <-- ! variance de la fraction de melange             !
 ! yfm              ! tr ! <-- ! moyenne de la fraction massique                !
 ! yfp2m            ! tr ! <-- ! variance de la fraction massique               !
-! hm               ! tr ! <-- ! moyenne de l'enthalpie de melange              !
 ! propce(ncelet, *)! ra ! <-- ! physical properties at cell centers            !
 !__________________!____!_____!________________________________________________!
 
@@ -109,7 +107,6 @@ integer          ncelet, ncel
 
 double precision fm(ncelet)   , fp2m(ncelet)
 double precision yfm(ncelet)  , yfp2m(ncelet)
-double precision hm(ncelet)
 double precision propce(ncelet,*)
 
 ! Local variables
@@ -215,6 +212,9 @@ do iel = 1, ncel
 
 
   fmp    =  max(min(fmax,fm(iel)),fmin)
+  yfmp = fmp-((fmp-max(zero,(fmp-fs(1))/(1.d0-fs(1))))            &
+                  /(fm(iel)-max(zero,                             &
+       (fm(iel)-fs(1))/(1.d0-fs(1))))*(fm(iel)-yfm(iel)))
   yfmp   =  max(min(yfm(iel),fmp),                                &
                     zero,(fmp-fs(1))/(1.d0-fs(1)))
   fp2mp  =  max(min(fp2m(iel),                                    &
@@ -247,14 +247,8 @@ do iel = 1, ncel
 
 ! ---> Calcul de l'enthalpie
 
-      if (     ippmod(icolwc).eq.0 &
-          .or. ippmod(icolwc).eq.2 &
-          .or. ippmod(icolwc).eq.4) then
-        h(idirac) = ((hmax-hmin)*f(idirac) + hmin*fmax - hmax*fmin) &
-                  / (fmax-fmin)
-      else
-        h(idirac) = hm(iel)
-      endif
+      h(idirac) = ((hmax-hmin)*f(idirac) + hmin*fmin - hmax*fmax) &
+                / (fmin-fmax)
 
 ! ---> Calcul de la fraction massique des gaz (F, O et P)
 
@@ -649,15 +643,8 @@ do iel = 1, ncel
     sum17 = zero
 
     do idirac = 1, ndirac
-
-      if (     ippmod(icolwc).eq.0 &
-          .or. ippmod(icolwc).eq.2 &
-          .or. ippmod(icolwc).eq.4) then
-        h(idirac) = ((hmax-hmin)*f(idirac) + hmin*fmax - hmax*fmin) &
-                  / (fmax-fmin)
-      else
-        h(idirac) = hm(iel)
-      endif
+      h(idirac) = ((hmax-hmin)*f(idirac) + hmin*fmin - hmax*fmax) &
+                / (fmin-fmax)
 
 ! ---> Calcul de la fraction massique des gaz (F, O et P) en 1 et 2
 
