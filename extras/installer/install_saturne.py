769c82bd47bf327ee73ca81ce509161ec9c4c303
Yvan Fournier <yvan.fournier@noreply.code-saturne.org>
2012-04-23 14:04:34 +0000

    Release Code_Saturne 2.1.7.
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/branches/Version2_1@3352 e44c5d27-f830-0410-8ce8-974f7a764be6

--- extras/installer/install_saturne.py
+++ extras/installer/install_saturne.py
@@ -321,8 +321,8 @@ class Setup:
             Package(name="Code_Saturne",
                     description="Code_Saturne CFD tool",
                     package="code_saturne",
-                    version="2.1.6",
-                    archive="code_saturne-2.1.6.tar.gz",
+                    version="2.1.7",
+                    archive="code_saturne-2.1.7.tar.gz",
                     url=url_cs)
 
         p = self.packages['code_saturne']
