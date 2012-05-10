344c5c6d974f4a429ac898d07d51302b4a7c1f6b
Alexandre Douce <alexandre.douce@noreply.code-saturne.org>
2012-05-10 12:57:13 +0000

    Autovnv: fix for update of files of parameters (indentation error).
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@3417 e44c5d27-f830-0410-8ce8-974f7a764be6

--- bin/autovnv/Study.py
+++ bin/autovnv/Study.py
@@ -173,7 +173,7 @@ class Case(object):
             from core.XMLinitialize import XMLinit
 
         for fn in os.listdir(os.path.join(self.__repo, self.label, "DATA")):
-            fp = os.path.abspath(fn)
+            fp = os.path.join(self.__repo, self.label, "DATA", fn)
             if os.path.isfile(fp):
                 fd = os.open(fp , os.O_RDONLY)
                 f = os.fdopen(fd)
@@ -181,17 +181,17 @@ class Case(object):
                 f.close()
                 if l.startswith('''<?xml version="1.0" encoding="utf-8"?><Code_Saturne_GUI''') or \
                    l.startswith('''<?xml version="1.0" encoding="utf-8"?><NEPTUNE_CFD_GUI'''):
-        try:
+                    try:
                         case = Case(package = self.pkg, file_name = fp)
-        except:
-            print "File of parameters reading error.\n"
-            print "This file is not in accordance with XML specifications."
-            sys.exit(1)
+                    except:
+                        print "File of parameters reading error.\n"
+                        print "This file is not in accordance with XML specifications."
+                        sys.exit(1)
 
-        case['xmlfile'] = fp
-        case.xmlCleanAllBlank(case.xmlRootNode())
-        XMLinit(case).initialize()
-        case.xmlSaveDocument()
+                    case['xmlfile'] = fp
+                    case.xmlCleanAllBlank(case.xmlRootNode())
+                    XMLinit(case).initialize()
+                    case.xmlSaveDocument()
 
         # 2) Create RESU directory if needed
         r = os.path.join(self.__repo, self.label, "RESU")
