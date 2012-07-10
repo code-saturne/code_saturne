a84d85e4ee6ef831512c5a0edfdbef14ea0688cb
Yvan Fournier <yvan.fournier@noreply.code-saturne.org>
2012-07-10 16:07:01 +0000

    Update Copyright.
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/branches/Version2_0@3529 e44c5d27-f830-0410-8ce8-974f7a764be6

--- src/base/cs_opts.c
+++ src/base/cs_opts.c
@@ -3,7 +3,7 @@
  *     This file is part of the Code_Saturne Kernel, element of the
  *     Code_Saturne CFD tool.
  *
- *     Copyright (C) 1998-2009 EDF S.A., France
+ *     Copyright (C) 1998-2012 EDF S.A., France
  *
  *     contact: saturne-support@edf.fr
  *
@@ -336,7 +336,7 @@ cs_opts_logfile_head(int    argc,
              "                      Version %s\n\n",
              CS_APP_VERSION);
 
-  bft_printf("\n  Copyright (C) 1998-2011 EDF S.A., France\n\n");
+  bft_printf("\n  Copyright (C) 1998-2012 EDF S.A., France\n\n");
 
   bft_printf(_("  build %s\n"), str);
 
