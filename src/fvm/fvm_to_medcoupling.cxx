65d74e3106ed9e15a0378b769c5f66fd57eca5f2
Yvan Fournier <yvan.fournier@noreply.code-saturne.org>
2012-09-03 14:28:24 +0000

    Merge rev. 3824 from trunk (fix MEDCoupling plugin compilation when MPI is not used).
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/branches/Version2_2@3828 e44c5d27-f830-0410-8ce8-974f7a764be6

--- src/fvm/fvm_to_medcoupling.cxx
+++ src/fvm/fvm_to_medcoupling.cxx
@@ -703,8 +703,6 @@ _export_vertex_coords_l(const fvm_nodal_t           *mesh,
   Coords->decrRef();
 }
 
-#if defined(HAVE_MPI)
-
 /*----------------------------------------------------------------------------
  * Write strided connectivity block to a MEDCoupling mesh
  *
@@ -742,6 +740,8 @@ _write_connect_block(fvm_element_t      type,
     med_mesh->insertNextCell(med_type, stride, elt_buf);
   }
 }
+
+#if defined(HAVE_MPI)
 
 /*----------------------------------------------------------------------------
  * Write strided global connectivity block to a MEDCoupling object
