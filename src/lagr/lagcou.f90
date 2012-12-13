f4f889cc6d0caa544bdb8cccc59fd3c97c6ec9cb
Jacques Fontaine <jacques-j.fontaine@noreply.code-saturne.org>
2012-12-13 13:38:23 +0000

    Fix in lagrangian module (provided by Mathieu Guingo <mathieu.guingo@edf.fr>).
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@4270 e44c5d27-f830-0410-8ce8-974f7a764be6

--- src/lagr/lagcou.f90
+++ src/lagr/lagcou.f90
@@ -275,10 +275,10 @@ if (ltsdyn.eq.1) then
 
     do iel = 1,ncel
 
-      tslag(iel,itske) = tslag(iel,itske)                         &
-                       - ettp(npt,juf) * tslag(iel,itsvx)         &
-                       - ettp(npt,jvf) * tslag(iel,itsvy)         &
-                       - ettp(npt,jwf) * tslag(iel,itsvz)
+      tslag(iel,itske) = tslag(iel,itske)                       &
+                       - rtp(iel,iu) * tslag(iel,itsvx)         &
+                       - rtp(iel,iv) * tslag(iel,itsvy)         &
+                       - rtp(iel,iw) * tslag(iel,itsvz)
 
     enddo
 
