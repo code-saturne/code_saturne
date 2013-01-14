52d5d1cefabbc77f5a60cd1d1c0579e85a04a817
Dorothée Sénéchal <dorothee.senechal@noreply.code-saturne.org>
2013-01-14 09:21:55 +0000

    Fix in weakly compressible algos (idilat = 2 or 3).
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@4405 e44c5d27-f830-0410-8ce8-974f7a764be6

--- src/base/resopv.f90
+++ src/base/resopv.f90
@@ -1236,7 +1236,8 @@ if (ncesmp.gt.0) then
 endif
 
 ! --- Source term associated to the mass aggregation
-if (idilat.eq.2.or.idilat.eq.3) then
+if ((idilat.eq.2.or.idilat.eq.3).and. &
+    (ntcabs.gt.1.or.isuite.gt.0)) then
   do iel = 1, ncel
     drom = propce(iel,ipcrom) - propce(iel,ipcroa)
     divu(iel) = divu(iel) + drom*volume(iel)/dt(iel)
