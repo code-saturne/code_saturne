579de82d416c7eb0db6b891c725a6767a1f9f986
Martin Ferrand <martin.ferrand@noreply.code-saturne.org>
2013-02-13 11:24:54 +0000

    Doxygen numvar.
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@4620 e44c5d27-f830-0410-8ce8-974f7a764be6

--- src/base/numvar.f90
+++ src/base/numvar.f90
@@ -31,97 +31,225 @@ module numvar
 
   !=============================================================================
 
-  integer, save :: ipr       !< pressure
-  integer, save :: iu        !< velocity component \f$ u_x \f$
-  integer, save :: iv        !< velocity component \f$ u_y \f$
-  integer, save :: iw        !< velocity component \f$ u_z \f$
-  integer, save :: ik        !< turbulent kinetic energy \f$ k \f$
-  integer, save :: iep       !< turbulent dissipation \f$ \varepsilon \f$
-  integer, save :: ir11      !< Reynolds stress component \f$ R_{xx} \f$
-  integer, save :: ir22      !< Reynolds stress component \f$ R_{yy} \f$
-  integer, save :: ir33      !< Reynolds stress component \f$ R_{zz} \f$
-  integer, save :: ir12      !< Reynolds stress component \f$ R_{xy} \f$
-  integer, save :: ir23      !< Reynolds stress component \f$ R_{yz} \f$
-  integer, save :: ir13      !< Reynolds stress component \f$ R_{zz} \f$
-  integer, save :: iphi      !< variable \f$ \phi \f$ of the \f$ \phi-f_b \f$ model
-  integer, save :: ifb       !< variable \f$ f_b \f$ of the \f$ \phi-f_b \f$ model
-  integer, save :: ial       !< variable \f$ \alpha \f$ of the \f$ Bl-v^2-k \f$ model
-  integer, save :: iomg      !< variable \f$ \omega \f$ of the \f$ k-\omega \f$ SST
-  integer, save :: inusa     !< variable \f$ \widetilde{\nu}_T \f$ of the Spalart Allmaras
-  integer, save :: isca(nscamx) !< isca(i) is the index of the scalar i
-  integer, save :: iscapp(nscamx) !< iscapp(i) is the index of the specific physics scalar i
-  integer, save :: nscaus    !< number of user scalars
-  integer, save :: nscapp    !< number of specific physics scalars
-  integer, save :: nscasp    !< number of species scalars
-  integer, save :: iuma      !< mesh velocity component \f$ w_x \f$
-  integer, save :: ivma      !< mesh velocity component \f$ w_y \f$
-  integer, save :: iwma      !< mesh velocity component \f$ w_z \f$
+  !----------------------------------------------------------------------------
+  ! Main variables
+  !----------------------------------------------------------------------------
 
-  ! Position des proprietes (physiques ou numeriques)
-  !  (dans propce, propfa et propfb)
-  !    le numero des proprietes est unique, quelle aue soit la
-  !      localisation de ces dernieres (cellule, face, face de bord)
-  !    Voir cs_user_boundary_conditions pour quelques exemples
+  !> \defgroup main_variables Main variables
+  !> \brief Main variables stored in rtp, rtpa.
 
-  ! ipproc : pointeurs dans propce
-  ! ipprof : pointeurs dans propfa
-  ! ipprob : pointeurs dans propfb
+  !> \addtogroup main_variables
+  !> \{
 
-  ! irom   : Density at the current time step
-  ! iroma  : Density at the previous time step
-  ! iviscl : Viscosite moleculaire dynamique en kg/(ms) des phases
-  ! ivisct : Viscosite turbulente des phases
-  ! ivisla : Viscosite moleculaire dynamique en kg/(ms) des phases au pas
-  !          de temps precedent
-  ! ivista : Viscosite turbulente des phases au pas de temps precedent
-  ! icp    : Chaleur specifique des phases
-  ! icpa   : Chaleur specifique des phases au pas de temps precedent
-  ! itsnsa : Terme source Navier Stokes des phases au pas de temps precedent
-  ! itstua : Terme source des grandeurs turbulentes au pas de temps precedent
-  ! itssca : Terme source des scalaires au pas de temps precedent
-  ! iestim : Estimateur d'erreur pour Navier-Stokes
-  ! ifluma : Flux de masse associe aux variables
-  ! ifluaa : Flux de masse explicite (plus vu comme un tableau de travail)
-  !          associe aux variables
-  ! ismago : constante de Smagorinsky dynamique
-  ! icour  : Nombre de Courant des phases
-  ! ifour  : Nombre de Fourier des phases
-  ! iprtot : Pression totale au centre des cellules Ptot=P*+rho*g.(x-x0)
-  !                                                             -  - -
-  ! ivisma : Viscosite de maillage en ALE (eventuellement orthotrope)
-  ! iustdy : pointer for dilatation source terms
-  ! itsrho : pointer for global dilatation source terms
-  ! ibetau : pointer for thermal expansion coefficient
+  !> pressure
+  integer, save :: ipr
 
+  !> velocity component \f$ u_x \f$
+  integer, save :: iu
 
-  integer, save :: ipproc(npromx), ipprof(npromx), ipprob(npromx), &
-                   irom  , iroma , iviscl,                         &
-                   ivisct, ivisla, ivista,                         &
-                   icp   , icpa  , itsnsa,                         &
-                   itstua, itssca(nscamx),                         &
-                   iestim(nestmx)         , ifluma(nvarmx),        &
-                   ifluaa(nvarmx), ismago, icour ,                 &
-                   ifour , iprtot, ivisma(3),                      &
-                   iustdy(nscamx), itsrho,                         &
-                   ibeta
+  !> velocity component \f$ u_y \f$
+  integer, save :: iv
 
-  ! Position des conditions aux limites
-  !  (position dans coefa et coefb des coef (coef. coef.f) relatifs a
-  !   une variable donnee)
+  !> velocity component \f$ u_z \f$
+  integer, save :: iw
 
-  ! icoef   : coef numeros 1 (anciens coefa coefb)
-  ! icoeff  : coef numeros 2 (anciens coefaf coefbf)
-  ! icoefr  : coef number 3 (for the Rij in the momentum eq)
-  ! iclrtp  : pointeur dans COEFA et COEFB
+  !> turbulent kinetic energy \f$ k \f$
+  integer, save :: ik
 
-  integer, save :: icoef , icoeff , icoefr , iclrtp(nvarmx,3)
+  !> turbulent dissipation \f$ \varepsilon \f$
+  integer, save :: iep
 
+  !> Reynolds stress component \f$ R_{xx} \f$
+  integer, save :: ir11
+
+  !> Reynolds stress component \f$ R_{yy} \f$
+  integer, save :: ir22
+
+  !> Reynolds stress component \f$ R_{zz} \f$
+  integer, save :: ir33
+
+  !> Reynolds stress component \f$ R_{xy} \f$
+  integer, save :: ir12
+
+  !> Reynolds stress component \f$ R_{yz} \f$
+  integer, save :: ir23
+
+  !> Reynolds stress component \f$ R_{zz} \f$
+  integer, save :: ir13
+
+  !> variable \f$ \phi \f$ of the \f$ \phi-f_b \f$ model
+  integer, save :: iphi
+
+  !> variable \f$ f_b \f$ of the \f$ \phi-f_b \f$ model
+  integer, save :: ifb
+
+  !> variable \f$ \alpha \f$ of the \f$ Bl-v^2-k \f$ model
+  integer, save :: ial
+
+  !> variable \f$ \omega \f$ of the \f$ k-\omega \f$ SST
+  integer, save :: iomg
+
+  !> variable \f$ \widetilde{\nu}_T \f$ of the Spalart Allmaras
+  integer, save :: inusa
+
+  !> isca(i) is the index of the scalar i
+  integer, save :: isca(nscamx)
+
+  !> iscapp(i) is the index of the specific physics scalar i
+  integer, save :: iscapp(nscamx)
+
+  !> number of user scalars
+  integer, save :: nscaus
+
+  !> number of specific physics scalars
+  integer, save :: nscapp
+
+  !> number of species scalars
+  integer, save :: nscasp
+
+  !> mesh velocity component \f$ w_x \f$
+  integer, save :: iuma
+
+  !> mesh velocity component \f$ w_y \f$
+  integer, save :: ivma
+
+  !> mesh velocity component \f$ w_z \f$
+  integer, save :: iwma
+
+  !> \}
+
+  !----------------------------------------------------------------------------
+  ! Physical properties
+  !----------------------------------------------------------------------------
+
+  !> \defgroup physical_prop Physical properties
+  !> \brief Physical properties are stored in propce, propfa and propfb.
+  !> the same index is used for a given properties for cells, internal
+  !> faces and boundary faces. See \ref cs_user_boundary_conditions
+  !> for some examples.
+
+  !> \addtogroup physical_prop
+  !> \{
+
+  !> pointer to cell properties (propce)
+  integer, save :: ipproc(npromx)
+  !> pointer to internal face properties (propfa)
+  integer, save :: ipprof(npromx)
+  !> pointer to boundary face properties (propfb)
+  integer, save :: ipprob(npromx)
+
+  !> Density at the current time step
+  integer, save :: irom
+
+  !> Density at the previous time step
+  integer, save :: iroma
+
+  !> dynamic molecular viscosity (in kg/(m.s))
+  integer, save :: iviscl
+
+  !> dynamic turbulent viscosity
+  integer, save :: ivisct
+
+  !> dynamic molecular viscosity (in kg/(m.s)) at the previous time-step
+  integer, save :: ivisla
+
+  !> dynamic turbulent viscosity at the previous time-step
+  integer, save :: ivista
+
+  !> specific heat \f$ C_p \f$
+  integer, save :: icp
+
+  !> specific heat \f$ C_p \f$ at the previous time-step
+  integer, save :: icpa
+
+  !> Navier-Stokes source terms at the previous time-step
+  integer, save :: itsnsa
+
+  !> Turbulent source terms at the previous time-step
+  integer, save :: itstua
+
+  !> transported scalars source terms at the previous time-step
+  integer, save :: itssca(nscamx)
+
+  !> error estimator for Navier-Stokes
+  integer, save :: iestim(nestmx)
+
+  !> convective mass flux of the variables
+  integer, save :: ifluma(nvarmx)
+
+  !> convective mass flux of the variables at the previous time-step
+  integer, save :: ifluaa(nvarmx)
+
+  !> dynamic constant of Smagorinsky
+  integer, save :: ismago
+
+  !> Courant number
+  integer, save :: icour
+
+  !> Fourier number
+  integer, save :: ifour
+
+  !> Total pressure at cell centres
+  !> \f$ P_{tot} = P^\star +\rho \vect{g} \cdot (\vect{x}-\vect{x}_0) \f$
+  integer, save :: iprtot
+
+  !> Mesh velocity viscosity for the ALE module
+  !> \remark might be orthotropic
+  integer, save :: ivisma(3)
+
+  !> pointer for dilatation source terms
+  integer, save :: iustdy(nscamx)
+
+  !> pointer for global dilatation source terms
+  integer, save :: itsrho
+
+  !> pointer for thermal expansion coefficient
+  integer, save :: ibeta
+
+  !> \}
+
+  !----------------------------------------------------------------------------
+  ! Boundary conditions coefficients
+  !----------------------------------------------------------------------------
+
+  !>\defgroup bc_coeffs Boundary conditions coefficients
+  !> \brief Boundary condition coefficient are stored in coefa and coefb.
+  !> See \ref condli and the theory guide for more information.
+
+  !> \addtogroup bc_coeffs
+  !> \{
+
+  !> Coefficients used for gradient terms and convective terms
+  integer, save :: icoef
+
+  !> Coefficients used for diffusive terms
+  integer, save :: icoeff
+
+  !> Coefficients used for divergence terms
+  !> (for example \f$ \tens{R}_{ij} \f$ in the momentum equation)
+  integer, save :: icoefr
+
+  !> pointer to coefa and coefb arrays
+  integer, save :: iclrtp(nvarmx,3)
+
+  !> \}
+
+  !----------------------------------------------------------------------------
   ! Mapping to field structures
+  !----------------------------------------------------------------------------
 
-  ! ivarfl(i)                  Field id for variable i
-  ! iprpfl(i)                  Field id for property i
+  !> \defgroup field_map Mapping to field structures
 
-  integer, save :: ivarfl(nvarmx), iprpfl(npromx)
+  !> \addtogroup field_map
+  !> \{
+
+  !> Field id for variable i
+  integer, save :: ivarfl(nvarmx)
+
+  !> Field id for property i
+  integer, save :: iprpfl(npromx)
+
+  !> \}
 
   !=============================================================================
 
