74a148da77e6b3cf97f700e60aa177216346b433
Yvan Fournier <yvan.fournier@noreply.code-saturne.org>
2013-03-11 11:10:39 +0000

    Update base boundary conditions example for all turbulent models.
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@4762 e44c5d27-f830-0410-8ce8-974f7a764be6

--- src/user_examples/cs_user_boundary_conditions-base.f90
+++ src/user_examples/cs_user_boundary_conditions-base.f90
@@ -228,13 +228,20 @@ do ilelt = 1, nlelt
     rcodcl(ifac,ir13,1) = 0.d0
     rcodcl(ifac,ir23,1) = 0.d0
     rcodcl(ifac,iep,1)  = xeent
+    if (iturb.eq.32) then
+      rcodcl(ifac,ial,1)  = 1.d0
+    endif
 
-  elseif (iturb.eq.50) then
+  elseif (itytur.eq.5) then
 
     rcodcl(ifac,ik,1)   = xkent
     rcodcl(ifac,iep,1)  = xeent
     rcodcl(ifac,iphi,1) = d2s3
-    rcodcl(ifac,ifb,1)  = 0.d0
+    if (iturb.eq.50) then
+      rcodcl(ifac,ifb,1)  = 0.d0
+    elseif (iturb.eq.51) then
+      rcodcl(ifac,ial,1)  = 0.d0
+    endif
 
   elseif (iturb.eq.60) then
 
@@ -314,13 +321,20 @@ do ilelt = 1, nlelt
     rcodcl(ifac,ir13,1) = 0.d0
     rcodcl(ifac,ir23,1) = 0.d0
     rcodcl(ifac,iep,1)  = xeent
+    if (iturb.eq.32) then
+      rcodcl(ifac,ial,1)  = 1.d0
+    endif
 
-  elseif (iturb.eq.50) then
+  elseif (itytur.eq.5) then
 
     rcodcl(ifac,ik,1)   = xkent
     rcodcl(ifac,iep,1)  = xeent
     rcodcl(ifac,iphi,1) = d2s3
-    rcodcl(ifac,ifb,1)  = 0.d0
+    if (iturb.eq.50) then
+      rcodcl(ifac,ifb,1)  = 0.d0
+    elseif (iturb.eq.51) then
+      rcodcl(ifac,ial,1)  = 0.d0
+    endif
 
   elseif (iturb.eq.60) then
 
