1ed2c5ba294793a624d75198aee5a0c89e5c506a
Martine Paolillo <martine.paolillo@noreply.code-saturne.org>
2013-05-15 13:07:23 +0000

    When running ./code_saturne salome, the following error appears /bin/bash: -c: line 0: Erreur de syntaxe près du symbole inattendu « ; » /bin/bash: -c: line 0: `;' and correction into bin/cs_salome.py
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@4934 e44c5d27-f830-0410-8ce8-974f7a764be6

--- bin/cs_salome.py
+++ bin/cs_salome.py
@@ -6,18 +6,18 @@
 #
 # Copyright (C) 1998-2013 EDF S.A.
 #
-# This program is free software; you can redistribute it and/or modify it under
+# This program is free software, you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later
+# Foundation, either version 2 of the License, or (at your option) any later
 # version.
 #
 # This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# ANY WARRANTY, without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
 # details.
 #
 # You should have received a copy of the GNU General Public License along with
-# this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+# this program, if not, write to the Free Software Foundation, Inc., 51 Franklin
 # Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 #-------------------------------------------------------------------------------
@@ -67,10 +67,10 @@ def main(argv, pkg):
     """
 
     template = """\
-%(salomeenv)s;
-CFDSTUDY_ROOT_DIR=%(prefix)s;
-PYTHONPATH=%(pythondir)s/salome:%(pkgpythondir)s${PYTHONPATH:+:$PYTHONPATH};
-export CFDSTUDY_ROOT_DIR PYTHONPATH;
+%(salomeenv)s
+CFDSTUDY_ROOT_DIR=%(prefix)s
+PYTHONPATH=%(pythondir)s/salome:%(pkgpythondir)s${PYTHONPATH:+:$PYTHONPATH}
+export CFDSTUDY_ROOT_DIR PYTHONPATH
 %(runsalome)s --modules=%(modules)s
 """
 
