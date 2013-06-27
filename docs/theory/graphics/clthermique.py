cf683c5c2eec64b737f36e0e659c2dbafdcfe4c0
Yvan Fournier <yvan.fournier@noreply.code-saturne.org>
2013-06-27 12:36:15 +0000

    Replace Fortran77 files used to generate log law curve for documentation by Python scripts.
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@5042 e44c5d27-f830-0410-8ce8-974f7a764be6

---
+++ docs/theory/graphics/clthermique.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import math
+
+for i in range(300):
+    x = (i+1)*0.1
+    print(' %17.9e %17.9e' % (x, x*x*x/1000))
+
+# To use the curve,
+# two points must be placed for (a+at)/nu = 1/Pr = 1 
+# of which one at x = 0,
+# and two points must be placed for (a+at)/nu = 0.42 x / Prt = 0.42 x  
+# of which one at x = 30.
---
+++ docs/theory/graphics/loilog.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import math
+
+for i in range(2000):
+    x = (i+1)*0.01
+    print(' %17.9e %17.9e %17.9e %17.9e %17.9e %17.9e'
+          % (x, 1/0.42*math.log(x)+5.2,
+             x, 2/0.42*math.log(x)+5.2,
+             2*x, 2/0.42))
