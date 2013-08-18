ceaf0a613324d8f753eccf997db713ceb7bf5ec7
David Monfort <david.monfort@noreply.code-saturne.org>
2013-08-18 15:18:34 +0000

    Minor update to MinGW configure example.
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@5197 e44c5d27-f830-0410-8ce8-974f7a764be6

--- extras/windows/config.sh
+++ extras/windows/config.sh
@@ -8,6 +8,8 @@ if test -d "$SALOME" ; then
 
   OMNIORB=/mingw/opt/salome-7.2/PRODUCTS/omniORB-4.1.6
   export PATH=$OMNIORB/bin/x86_win32:$PATH
+  export PYTHONPATH=$OMNIORB/bin/x86_win32:$PATH
+  export PYTHONPATH=$OMNIORB/bin/x86_win32:$OMNIORB/lib/python/omniidl_be:$PYTHONPATH
 
   WITH_SALOME="--with-salome-kernel=$SALOME/KERNEL/RELEASE/KERNEL_INSTALL --with-salome-gui=$SALOME/GUI/RELEASE/GUI_INSTALL OMNIIDL=$OMNIORB/bin/x86_win32/omniidl.exe OMNIIDLPYBE=$OMNIORB/lib/python/omniidl_be SALOMEENVCMD=:"
 
