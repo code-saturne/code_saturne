bec2e009310daebb10313c99f773d5b286470dc6
David Monfort <david.monfort@noreply.code-saturne.org>
2013-09-29 17:51:43 +0000

    Simpler SALOME batch script.
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@5275 e44c5d27-f830-0410-8ce8-974f7a764be6

--- extras/windows/run_salome.bat
+++ extras/windows/run_salome.bat
@@ -1,9 +1,11 @@
-SET CFDSTUDY_ROOT_DIR=C:\Program Files\Code_Saturne\3.2
+SET CSVERS=3.2-alpha
 
-SET PYTHONPATH=C:\Program Files\Code_Saturne\3.2\lib\python2.7\site-packages\code_saturne;%PYTHONPATH%
-SET PYTHONPATH=C:\Program Files\Code_Saturne\3.2\lib\python2.7\site-packages\salome;%PYTHONPATH%
+SET CFDSTUDY_ROOT_DIR=C:\Program Files\Code_Saturne\%CSVERS%
 
-SET PATH=C:\Program Files\Code_Saturne\3.2\bin;%PATH%
-SET PATH=C:\Program Files\Code_Saturne\3.2\lib\salome;%PATH%
+SET PYTHONPATH=C:\Program Files\Code_Saturne\%CSVERS%\lib\python2.7\site-packages\code_saturne;%PYTHONPATH%
+SET PYTHONPATH=C:\Program Files\Code_Saturne\%CSVERS%\lib\python2.7\site-packages\salome;%PYTHONPATH%
+
+SET PATH=C:\Program Files\Code_Saturne\%CSVERS%\bin;%PATH%
+SET PATH=C:\Program Files\Code_Saturne\%CSVERS%\lib\salome;%PATH%
 
 CALL C:\SALOME\7.2.0\run_salome.bat --modules=GEOM,SMESH,MED,YACS,PARAVIS,CFDSTUDY
