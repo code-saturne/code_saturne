d926cf3c7c340ef66d875b9ad66700ccd33d3610
Yvan Fournier <yvan.fournier@noreply.code-saturne.org>
2014-01-10 07:44:57 +0000

    Merge rev. 5653 from trunk (fix incorrect class member names in MPI settings from rev. 5628).
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/branches/Version3_1@5655 e44c5d27-f830-0410-8ce8-974f7a764be6

--- bin/cs_exec_environment.py
+++ bin/cs_exec_environment.py
@@ -961,9 +961,9 @@ class mpi_environment:
                elif v[0] in ['"', "'"]:
                    v = v[1:-1]
                if k == 'mpmd':
-                   self.mpi_env.mpmd_mode = eval(v)
+                   self.mpmd = eval('MPI_MPMD_' + v)
                else:
-                   self.mpi_env.__dict__[k] = v
+                   self.__dict__[k] = v
 
         # Initialize based on known MPI types, or default.
 
@@ -1002,9 +1002,9 @@ class mpi_environment:
                elif v[0] in ['"', "'"]:
                    v = v[1:-1]
                if k == 'mpmd':
-                   self.mpi_env.mpmd_mode = eval(v)
+                   self.mpmd = eval('MPI_MPMD_' + v)
                else:
-                   self.mpi_env.__dict__[k] = v
+                   self.__dict__[k] = v
 
     #---------------------------------------------------------------------------
 
@@ -1396,6 +1396,9 @@ class mpi_environment:
                         break
                 if self.mpiexec != None:
                     break
+
+        if (self.mpiexec == None):
+            self.mpiexec = 'mpiexec'
 
         if absname:
             info_name = os.path.join(os.path.dirname(absname),
