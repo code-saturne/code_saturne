91c5f22bfb7f90741dd3130d4feb35d82985b79c
Yvan Fournier <yvan.fournier@noreply.code-saturne.org>
2014-03-21 09:36:21 +0000

    Merge rev. 5934 from trunk (fix wrong call causing script crash in handling of locally-defined mpiexec name).
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/branches/Version3_1@5937 e44c5d27-f830-0410-8ce8-974f7a764be6

--- bin/cs_exec_environment.py
+++ bin/cs_exec_environment.py
@@ -1154,7 +1154,7 @@ class mpi_environment:
         absname = ''
 
         if self.mpiexec != None:
-            absname = __get_mpiexec_absname__(self, p)
+            absname = self.__get_mpiexec_absname__(p)
 
         else:
             launcher_names = ['mpiexec.mpich', 'mpiexec.mpich2', 'mpiexec',
@@ -1375,7 +1375,7 @@ class mpi_environment:
         # Determine base executable paths
 
         if self.mpiexec != None:
-            absname = __get_mpiexec_absname__(self, p)
+            absname = self.__get_mpiexec_absname__(p)
 
         else:
             launcher_names = ['mpiexec.openmpi', 'mpirun.openmpi',
