05636d4fd799ef20ce8a616b2b2b40c2e97371b1
Yvan Fournier <yvan.fournier@noreply.code-saturne.org>
2014-09-15 16:08:37 +0000

    Fix memory leak in transient turbomachinery introduced by rev. 6040.
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@6570 e44c5d27-f830-0410-8ce8-974f7a764be6

--- src/base/cs_field.c
+++ src/base/cs_field.c
@@ -1457,8 +1457,6 @@ cs_field_allocate_values(cs_field_t  *f)
     int ii;
 
     /* Initialization */
-    for (ii = 0; ii < f->n_time_vals; ii++)
-      f->vals[ii] = NULL;
 
     for (ii = 0; ii < f->n_time_vals; ii++)
       f->vals[ii] = _add_val(n_elts[2], f->dim, f->vals[ii]);
