cf4d69431e42c81271e3ff2bb8a7392f957578a9
Yvan Fournier <yvan.fournier@noreply.code-saturne.org>
2014-12-01 08:38:02 +0000

    Merge revs. 6608 and 6609 from trunk (fix minor memory leak).
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/branches/Version3_3@6909 e44c5d27-f830-0410-8ce8-974f7a764be6

--- src/gui/cs_gui.c
+++ src/gui/cs_gui.c
@@ -3159,6 +3159,7 @@ void CS_PROCF (uiporo, UIPORO) (const int *ncelet,
 
       if (formula != NULL) {
         ev_formula = mei_tree_new(formula);
+        BFT_FREE(formula);
         mei_tree_insert(ev_formula,"x",0.0);
         mei_tree_insert(ev_formula,"y",0.0);
         mei_tree_insert(ev_formula,"z",0.0);
@@ -3215,6 +3216,7 @@ void CS_PROCF (uiporo, UIPORO) (const int *ncelet,
       BFT_FREE(zone_id);
       BFT_FREE(mdl);
     }
+    BFT_FREE(status);
   }
 }
 
