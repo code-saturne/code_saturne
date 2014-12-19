fcf03995dfe1ad75afb8564cb92178d0121da8b8
Erwan Le Coupanec <erwan.lecoupanec@noreply.code-saturne.org>
2014-12-19 17:01:52 +0000

    Merge rev. 6999 from trunk.
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/branches/Version3_3@7002 e44c5d27-f830-0410-8ce8-974f7a764be6

--- src/gui/cs_gui.c
+++ src/gui/cs_gui.c
@@ -2963,7 +2963,7 @@ void CS_PROCF (cssca3, CSSCA3) (const    int *const itherm,
           if (result <= 0)
             bft_error(__FILE__, __LINE__, 0,
                       _("mass molar value is zero or not found in the xml file.\n"));
-          density = *p0 * result / (8.31434 *(*t0));
+          density = *p0 * result / (8.31446 *(*t0));
         }
         else
           cs_gui_properties_value("density", &density);
