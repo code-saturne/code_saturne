0a7e764134cd68e94cf83471b6b5c6dda8013335
Yvan Fournier <yvan.fournier@noreply.code-saturne.org>
2014-12-19 15:34:34 +0000

    Minor style fixes.
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@6991 e44c5d27-f830-0410-8ce8-974f7a764be6

--- src/base/dttvar.f90
+++ src/base/dttvar.f90
@@ -51,10 +51,10 @@ subroutine dttvar &
 ! ncepdp           ! i  ! <-- ! number of cells with head loss                 !
 ! ncesmp           ! i  ! <-- ! number of cells with mass source term          !
 ! iwarnp           ! i  ! <-- ! verbosity                                      !
-! icepdc(ncelet    ! te ! <-- ! numero des ncepdp cellules avec pdc            !
-! icetsm(ncesmp    ! te ! <-- ! numero des cellules a source de masse          !
+! icepdc(ncelet)   ! te ! <-- ! numero des ncepdp cellules avec pdc            !
+! icetsm(ncesmp)   ! te ! <-- ! numero des cellules a source de masse          !
 ! itypsm           ! te ! <-- ! type de source de masse pour les               !
-! (ncesmp,nvar)    !    !     !  variables (cf. cs_user_mass_source_terms)                        !
+! (ncesmp,nvar)    !    !     !  variables (cf. cs_user_mass_source_terms)     !
 ! dt(ncelet)       ! ra ! <-- ! time step (per cell)                           !
 ! propce(ncelet, *)! ra ! <-- ! physical properties at cell centers            !
 ! ckupdc           ! tr ! <-- ! tableau de travail pour pdc                    !
