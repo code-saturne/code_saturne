8a6d8a1a4b1228d455a72e463169eb9960ca81ae
Yvan Fournier <yvan.fournier@noreply.code-saturne.org>
2015-01-22 12:56:50 +0000

    Merge rev. 7180 from trunk (fix out-of-bounds access).
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/branches/Version4_0@7181 e44c5d27-f830-0410-8ce8-974f7a764be6

--- src/base/initi1.f90
+++ src/base/initi1.f90
@@ -203,10 +203,11 @@ do f_id = 0, nfld - 1
   ! Is the field of type FIELD_VARIABLE?
   if (iand(f_type, FIELD_VARIABLE).eq.FIELD_VARIABLE) then
     call field_get_key_int(f_id, keyvar, ivar)
-
-    call field_get_key_struct_var_cal_opt(f_id, vcopt)
-    vcopt%iwarni= iwarni(ivar)
-    call field_set_key_struct_var_cal_opt(f_id, vcopt)
+    if (ivar.gt.0) then
+      call field_get_key_struct_var_cal_opt(f_id, vcopt)
+      vcopt%iwarni= iwarni(ivar)
+      call field_set_key_struct_var_cal_opt(f_id, vcopt)
+    endif
   endif
 enddo
 
