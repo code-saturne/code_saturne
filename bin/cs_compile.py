f7f0b095d878ef93c61e9400cb195ba4ce7589b0
Martin Ferrand <martin.ferrand@noreply.code-saturne.org>
2016-06-23 00:59:56 +0000

    Fix bug introduced in rev. 8423 (Translation of arc electric in C).
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@8828 e44c5d27-f830-0410-8ce8-974f7a764be6

--- bin/cs_compile.py
+++ bin/cs_compile.py
@@ -417,6 +417,8 @@ def compile_and_link(pkg, srcdir, destdir,
             cmd = cmd + ["-o", "cs_f_user_extra_operations.o"]
         if f == 'cs_user_initialization.f90':
             cmd = cmd + ["-o", "cs_f_user_initialization.o"]
+        if f == 'cs_user_physical_properties.f90':
+            cmd = cmd + ["-o", "cs_f_user_physical_properties.o"]
         if opt_fcflags != None:
             cmd = cmd + separate_args(opt_fcflags)
         cmd = cmd + ["-I", srcdir]
