80d95a9b37208f229036edecb2644132b5dbfd45
Cyril Baudry <cyril.baudry@noreply.code-saturne.org>
2016-07-05 15:32:05 +0000

    update XML for lagrangian call in NEPTUNE_CFD
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@8851 e44c5d27-f830-0410-8ce8-974f7a764be6

--- gui/Base/XMLinitialize.py
+++ gui/Base/XMLinitialize.py
@@ -1177,10 +1177,10 @@ class XMLinit(Variables):
                 if node:
                     XMLLagrangianNode['model'] = node['model']
                     node.xmlRemoveNode()
-                XMLBoundaryNode = self.case.xmlInitNode('boundary_conditions')
-                for nature in ('inlet', 'outlet', 'wall', 'symmetry'):
-                    for node in XMLBoundaryNode.xmlGetNodeList(nature):
-                        node['field_id'] = 'none'
+        XMLBoundaryNode = self.case.xmlInitNode('boundary_conditions')
+        for nature in ('inlet', 'outlet', 'wall', 'symmetry'):
+            for node in XMLBoundaryNode.xmlGetNodeList(nature):
+                node['field_id'] = 'none'
 
 
 #-------------------------------------------------------------------------------
