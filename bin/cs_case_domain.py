8ca635795ce252767ae3cce2f148fb214d7cc609
Yvan Fournier <yvan.fournier@noreply.code-saturne.org>
2016-07-29 16:00:14 +0000

    Update NEWS realtive to version 4.3.0 release.
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@8956 e44c5d27-f830-0410-8ce8-974f7a764be6

--- bin/cs_case_domain.py
+++ bin/cs_case_domain.py
@@ -1371,7 +1371,7 @@ class aster_domain(base_domain):
         if os.path.isabs(self.conf):
             s.path = self.conf
         else:
-            s_path = os.path.join(self.aster_home, 'config.txt')
+            s_path = os.path.join(self.aster_home, 'share', 'aster', 'config.txt')
         s = open(s_path, 'r')
         template = s.read()
         s.close()
@@ -1379,7 +1379,7 @@ class aster_domain(base_domain):
         import re
 
         e_re = re.compile('profile.sh')
-        e_subst = os.path.join(self.aster_home, 'profile.sh')
+        e_subst = os.path.join(self.aster_home, 'share', 'aster', 'profile.sh')
         template = re.sub(e_re, e_subst, template)
 
         e_re = re.compile('Execution/E_SUPERV.py')
