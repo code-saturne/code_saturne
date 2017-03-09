dd4d8cddf5489983535672c125e303562462e7ec
Yvan Fournier <yvan.fournier@noreply.code-saturne.org>
2017-03-09 17:32:47 +0000

    Merge rev. 9895 from trunk (fix restart handling of mass fluxes).
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/branches/Version4_3@9900 e44c5d27-f830-0410-8ce8-974f7a764be6

--- src/base/cs_restart_default.c
+++ src/base/cs_restart_default.c
@@ -1906,8 +1906,10 @@ cs_restart_read_linked_fields(cs_restart_t               *r,
 
   for (int f_id = 0; f_id < n_fields; f_id++) {
     const cs_field_t *f = cs_field_by_id(f_id);
-    if (key_flag != 0 && !(f->type & key_flag))
-      continue;
+    if (key_flag != 0) {
+      if (key_flag == -1 || !(f->type & key_flag))
+        continue;
+    }
     if (cs_field_get_key_int(f, key_id) > -1)
       n_required += 1;
   }
@@ -1964,8 +1966,10 @@ cs_restart_read_linked_fields(cs_restart_t               *r,
 
       const cs_field_t *f = cs_field_by_id(f_id);
 
-      if (key_flag != 0 && !(f->type & key_flag))
-        continue;
+      if (key_flag != 0) {
+        if (key_flag == -1 || !(f->type & key_flag))
+          continue;
+      }
 
       const int lnk_f_id = cs_field_get_key_int(f, key_id);
 
@@ -1996,6 +2000,8 @@ cs_restart_read_linked_fields(cs_restart_t               *r,
             if (old_lnk_f_id > -1)
               f_lnk_name
                 = cs_map_name_to_id_reverse(old_field_map, old_lnk_f_id);
+            else
+              f_lnk_name = f_lnk->name;
           }
         }
 
@@ -2086,8 +2092,10 @@ cs_restart_write_linked_fields(cs_restart_t  *r,
   for (int f_id = 0; f_id < n_fields; f_id++) {
     key_val[f_id] = -1;
     const cs_field_t *f = cs_field_by_id(f_id);
-    if (key_flag != 0 && !(f->type & key_flag))
-      continue;
+    if (key_flag != 0) {
+      if (key_flag == -1 || !(f->type & key_flag))
+        continue;
+    }
     key_val[f_id] = cs_field_get_key_int(f, key_id);
   }
 
