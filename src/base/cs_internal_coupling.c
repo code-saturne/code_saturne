code-saturne/code_saturne

commit 172d656288bf54ee2644be4470fcbf5f5877074c

Apply r9751 to internal coupling. Provided by C. Flageul.
git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@9770 e44c5d27-f830-0410-8ce8-974f7a764be6

src/base/cs_internal_coupling.c
@@ -100,105 +100,44 @@ static int                      _n_internal_couplings = 0;
  *============================================================================*/
 
 /*----------------------------------------------------------------------------
- * Update direction vector for lsq gradient taking into account the weight
- * coefficients.
- * At this step we recompute inv_kf to avoid to store kf at the internal faces
- *
- * parameters:
- *   wi   <-- Weight coefficient of cell i
- *   wj   <-- Weight coefficient of cell j
- *   w    <-> direction vector
- *   a    <-- J'F/I'J'
- *----------------------------------------------------------------------------*/
-
-static inline void
-_compute_ani_dc(const double  wi[],
-                const double  wj[],
-                double        w[],
-                double        a)
-{
-  int ii;
-  cs_real_6_t sum;
-  cs_real_6_t inv_wi;
-  cs_real_6_t inv_wj;
-  cs_real_33_t inv_kf;
-  double _w[3];
-
-  for (ii = 0; ii < 6; ii++)
-    sum[ii] = a*wi[ii] + (1. - a)*wj[ii];
-
-  cs_math_sym_33_inv_cramer(wi,
-                            inv_wi);
-  cs_math_sym_33_inv_cramer(wj,
-                            inv_wj);
-
-  cs_math_sym_33_double_product(inv_wi,
-                                sum,
-                                inv_wj,
-                                inv_kf);
-  for (ii = 0; ii < 3; ii++)
-    _w[ii] = w[0]*inv_kf[ii][0]
-           + w[1]*inv_kf[ii][1]
-           + w[2]*inv_kf[ii][2];
-
-  for (ii = 0; ii < 3; ii++)
-    w[ii] = _w[ii];
-}
-
-/*----------------------------------------------------------------------------
  * Compute the inverse of the face viscosity tensor and anisotropic vector
  * taking into account the weight coefficients to update cocg for lsq gradient.
  *
  * parameters:
  *   wi     <-- Weight coefficient of cell i
  *   wj     <-- Weight coefficient of cell j
- *   w      <-> R.H.S.
- *   a      <-- J'F/I'J'
- *   resi   --> Updated vector for cell i
- *   inv_kf --> Inverse of the face viscosity
+ *   d      <-- IJ direction
+ *   a      <-- geometric weight J'F/I'J'
+ *   ki_d   --> Updated vector for cell i
  *----------------------------------------------------------------------------*/
 
 static inline void
-_compute_ani_weighting_cocg(const double  wi[],
-                            const double  wj[],
-                            double        w[],
-                            double        a,
-                            double        resi[],
-                            cs_real_33_t  inv_kf)
+_compute_ani_weighting_cocg(const cs_real_t  wi[],
+                            const cs_real_t  wj[],
+                            const cs_real_t  d[],
+                            const cs_real_t  a,
+                            cs_real_t        ki_d[])
 {
-  int ii;
-  double _resi[3] = {0., 0., 0.};
+  cs_real_t _d[3];
   cs_real_6_t sum;
-  cs_real_6_t inv_wi;
   cs_real_6_t inv_wj;
-  double _w[3];
 
-  for (ii = 0; ii < 6; ii++)
+  for (int ii = 0; ii < 6; ii++)
     sum[ii] = a*wi[ii] + (1. - a)*wj[ii];
 
-  cs_math_sym_33_inv_cramer(wi,
-                            inv_wi);
   cs_math_sym_33_inv_cramer(wj,
                             inv_wj);
 
-  cs_math_sym_33_double_product(inv_wi,
-                                sum,
-                                inv_wj,
-                                inv_kf);
-  for (ii = 0; ii < 3; ii++)
-    _w[ii] = w[0]*inv_kf[ii][0]
-           + w[1]*inv_kf[ii][1]
-           + w[2]*inv_kf[ii][2];
+  /* Note: K_i.K_f^-1 = SUM.K_j^-1
+   *       K_j.K_f^-1 = SUM.K_i^-1
+   * So: K_i d = SUM.K_j^-1.IJ */
 
-  for (ii = 0; ii < 3; ii++)
-    w[ii] = _w[ii];
-
-  cs_math_sym_33_3_product(wi,
-                           w,
-                           _resi);
-
-  for (ii = 0; ii < 3; ii++)
-    resi[ii] += _resi[ii];
+  cs_math_sym_33_3_product(inv_wj,
+                           d,
+                           _d);
+  cs_math_sym_33_3_product(sum,
+                           _d,
+                           ki_d);
 }
 
 
@@ -206,25 +145,46 @@ _compute_ani_weighting_cocg(const double  wi[],
  * Update R.H.S. for lsq gradient taking into account the weight coefficients.
  *
  * parameters:
- *   wi   <-- Weight coefficient of cell i
- *   w    <-- R.H.S.
- *   resi --> Updated R.H.S. for cell i
+ *   wi     <-- Weight coefficient of cell i
+ *   wj     <-- Weight coefficient of cell j
+ *   p_diff <-- R.H.S.
+ *   d      <-- R.H.S.
+ *   a      <-- geometric weight J'F/I'J'
+ *   resi   --> Updated R.H.S. for cell i
  *----------------------------------------------------------------------------*/
 
 static inline void
-_compute_ani_weighting(const double  wi[],
-                       const double  w[],
-                       double        resi[])
+_compute_ani_weighting(const cs_real_t  wi[],
+                       const cs_real_t  wj[],
+                       const cs_real_t  p_diff,
+                       const cs_real_t  d[],
+                       const cs_real_t  a,
+                       cs_real_t        resi[])
 {
   int ii;
-  double _resi[3] = {0., 0., 0.};
+  cs_real_t _d[3];
+  cs_real_t ki_d[3];
+  cs_real_6_t inv_wj;
+  cs_real_6_t sum;
 
-  cs_math_sym_33_3_product(wi,
-                           w,
-                           _resi);
+  for (ii = 0; ii < 6; ii++)
+    sum[ii] = a*wi[ii] + (1. - a)*wj[ii];
+
+  cs_math_sym_33_inv_cramer(wj,
+                            inv_wj);
+
+  cs_math_sym_33_3_product(inv_wj,
+                           d,
+                           _d);
+  cs_math_sym_33_3_product(sum,
+                           _d,
+                           ki_d);
+
+  /* 1 / ||Ki. K_f^-1. IJ||^2 */
+  cs_real_t normi = 1. / cs_math_3_dot_product(ki_d, ki_d);
 
   for (ii = 0; ii < 3; ii++)
-    resi[ii] += _resi[ii];
+    resi[ii] += p_diff * ki_d[ii] * normi;
 }
 
 /*----------------------------------------------------------------------------
@@ -1152,7 +1112,6 @@ cs_internal_coupling_lsq_rhs(const cs_internal_coupling_t  *cpl,
                              const int                      w_stride,
                              cs_real_4_t                    rhsv[])
 {
-  int ll;
   cs_lnum_t face_id, cell_id;
   cs_real_t pfac;
   cs_real_3_t dc, fctb;
@@ -1213,32 +1172,46 @@ cs_internal_coupling_lsq_rhs(const cs_internal_coupling_t  *cpl,
   for (cs_lnum_t ii = 0; ii < n_local; ii++) {
     face_id = faces_local[ii];
     cell_id = b_face_cells[face_id];
-    for (ll = 0; ll < 3; ll++)
+    for (cs_lnum_t ll = 0; ll < 3; ll++)
       dc[ll] = ci_cj_vect[ii][ll];
 
-    /* Reproduce _compute_ani_dc */
-    if (tensor_diff)
-      _compute_ani_dc(&c_weight[6*cell_id],
-                      &weight[6*ii],
-                      dc,
-                      g_weight[ii]);
+    cs_real_t pond = g_weight[ii];
 
-    pfac = (pvar_local[ii] - rhsv[cell_id][3])
-         / (dc[0]*dc[0] + dc[1]*dc[1] + dc[2]*dc[2]);
-    if (scalar_diff) pfac *= weight[ii];
+    if (tensor_diff) {
+      /* (P_j - P_i)*/
+      cs_real_t p_diff = (pvar_local[ii] - rhsv[cell_id][3]);
 
-    for (ll = 0; ll < 3; ll++)
-      fctb[ll] = dc[ll] * pfac;
-
-    /* Reproduce _compute_ani_weighting */
-    if (tensor_diff)
       _compute_ani_weighting(&c_weight[6*cell_id],
-                             fctb,
+                             &weight[6*ii],
+                             p_diff,
+                             dc,
+                             pond,
                              &rhsv[cell_id][0]);
-    else {
-      for (ll = 0; ll < 3; ll++)
-        rhsv[cell_id][ll] += fctb[ll];
+    } else if (scalar_diff) {
+      /* (P_j - P_i) / ||d||^2 */
+      pfac = (pvar_local[ii] - rhsv[cell_id][3])
+           / (dc[0]*dc[0] + dc[1]*dc[1] + dc[2]*dc[2]);
+
+      for (cs_lnum_t ll = 0; ll < 3; ll++)
+        fctb[ll] = dc[ll] * pfac;
+
+      cs_real_t denom = 1. / ( pond * c_weight[cell_id]
+                              + (1. - pond) * weight[ii]);
+
+      for (cs_lnum_t ll = 0; ll < 3; ll++)
+        rhsv[ii][ll] +=  weight[ii] * denom * fctb[ll];
+    } else {
+      /* (P_j - P_i) / ||d||^2 */
+      pfac = (pvar_local[ii] - rhsv[cell_id][3])
+           / (dc[0]*dc[0] + dc[1]*dc[1] + dc[2]*dc[2]);
+
+      for (cs_lnum_t ll = 0; ll < 3; ll++)
+        fctb[ll] = dc[ll] * pfac;
+
+      for (cs_lnum_t ll = 0; ll < 3; ll++)
+        rhsv[ii][ll] +=  fctb[ll];
     }
+
   }
   /* Free memory */
   if (c_weight != NULL) BFT_FREE(weight);
@@ -1302,7 +1275,6 @@ cs_internal_coupling_lsq_cocg_weighted(const cs_internal_coupling_t  *cpl,
                                        const cs_real_t               *c_weight,
                                        cs_real_33_t                   cocg[])
 {
-  int ll, mm;
   cs_lnum_t face_id, cell_id;
   cs_real_t dc[3];
 
@@ -1326,27 +1298,23 @@ cs_internal_coupling_lsq_cocg_weighted(const cs_internal_coupling_t  *cpl,
   for (cs_lnum_t ii = 0; ii < n_local; ii++) {
     face_id = faces_local[ii];
     cell_id = b_face_cells[face_id];
-    for (ll = 0; ll < 3; ll++)
+    for (cs_lnum_t ll = 0; ll < 3; ll++)
       dc[ll] = ci_cj_vect[ii][ll];
 
     /* Reproduce _compute_ani_weighting_cocg */
     cs_real_t pond = g_weight[ii];
     cs_real_t dc_i[3] = {0., 0., 0.};
-    cs_real_t inv_kf[3][3];
     _compute_ani_weighting_cocg(&c_weight[cell_id*6],
                                 &cwgt_local[ii*6],
                                 dc,
                                 pond,
-                                dc_i,
-                                inv_kf);
+                                dc_i);
 
-    cs_real_t umdddij = 1./ cs_math_3_norm(dc);
-    for (ll = 0; ll < 3; ll++)
-      dc_i[ll] *= umdddij;
+    cs_real_t i_dci = 1./ (dc_i[0]*dc_i[0] + dc_i[1]*dc_i[1] + dc_i[2]*dc_i[2]);
 
-    for (ll = 0; ll < 3; ll++) {
-      for (mm = 0; mm < 3; mm++)
-        cocg[cell_id][ll][mm] += dc_i[ll]*dc_i[mm];
+    for (cs_lnum_t ll = 0; ll < 3; ll++) {
+      for (cs_lnum_t mm = 0; mm < 3; mm++)
+        cocg[cell_id][ll][mm] += dc_i[mm] * dc_i[ll] * i_dci;
     }
   }
 

src/alge/cs_convection_diffusion.c
@@ -7070,28 +7070,28 @@ cs_anisotropic_diffusion_scalar(int                       idtvar,
       BFT_MALLOC(pvar_local, n_local, cs_real_t);
       cs_internal_coupling_exchange_by_cell_id(cpl,
                                                1, /* Dimension */
-                                               pvar,
+                                               (const cs_real_t*) pvar,
                                                pvar_local);
 
       /* Exchange grad */
       BFT_MALLOC(grad_local, n_local, cs_real_3_t);
       cs_internal_coupling_exchange_by_cell_id(cpl,
                                                3, /* Dimension */
-                                               grad,
-                                               grad_local);
+                                               (const cs_real_t*) grad,
+                                               (cs_real_t*) grad_local);
 
       /* Exchange viscce */
       BFT_MALLOC(viscce_local, n_local, cs_real_6_t);
       cs_internal_coupling_exchange_by_cell_id(cpl,
                                                6, /* Dimension */
-                                               viscce,
-                                               viscce_local);
+                                               (const cs_real_t*) viscce,
+                                               (cs_real_t*) viscce_local);
 
       /* Exchange weighb */
       BFT_MALLOC(weighb_local, n_local, cs_real_t);
       cs_internal_coupling_exchange_by_face_id(cpl,
                                                1, /* Dimension */
-                                               weighb,
+                                               (const cs_real_t*) weighb,
                                                weighb_local);
 
       /* Flux contribution */

src/alge/cs_gradient.c
@@ -506,7 +506,8 @@ _l2_norm_3(cs_lnum_t              n_elts,
  *   wi     <-- Weight coefficient of cell i
  *   wj     <-- Weight coefficient of cell j
  *   p_diff <-- R.H.S.
- *   dc     <-- R.H.S.
+ *   d      <-- R.H.S.
+ *   a      <-- geometric weight J'F/I'J'
  *   resi   --> Updated R.H.S. for cell i
  *   resj   --> Updated R.H.S. for cell j
  *----------------------------------------------------------------------------*/
@@ -516,7 +517,7 @@ _compute_ani_weighting(const cs_real_t  wi[],
                        const cs_real_t  wj[],
                        const cs_real_t  p_diff,
                        const cs_real_t  d[],
-                       cs_real_t        a,
+                       const cs_real_t  a,
                        cs_real_t        resi[],
                        cs_real_t        resj[])
 {
@@ -579,7 +580,7 @@ static inline void
 _compute_ani_weighting_cocg(const cs_real_t  wi[],
                             const cs_real_t  wj[],
                             const cs_real_t  d[],
-                            cs_real_t        a,
+                            const cs_real_t  a,
                             cs_real_t        ki_d[],
                             cs_real_t        kj_d[])
 {
@@ -2684,8 +2685,10 @@ _lsq_scalar_gradient(const cs_mesh_t                *m,
                                      &rhsv[jj][0]);
             }
             else {
+              /* (P_j - P_i) / ||d||^2 */
               pfac =   (rhsv[jj][3] - rhsv[ii][3])
                 / (dc[0]*dc[0] + dc[1]*dc[1] + dc[2]*dc[2]);
+
               for (ll = 0; ll < 3; ll++)
                 fctb[ll] = dc[ll] * pfac;
 

commit 617505c119311da766a19bc61b094801d6db079f

Fix bug introduced in r9770 (internal coupling). Provided by C. Flageul.
git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@9909 e44c5d27-f830-0410-8ce8-974f7a764be6

src/base/cs_internal_coupling.c
@@ -1175,8 +1175,6 @@ cs_internal_coupling_lsq_rhs(const cs_internal_coupling_t  *cpl,
     for (cs_lnum_t ll = 0; ll < 3; ll++)
       dc[ll] = ci_cj_vect[ii][ll];
 
-    cs_real_t pond = g_weight[ii];
-
     if (tensor_diff) {
       /* (P_j - P_i)*/
       cs_real_t p_diff = (pvar_local[ii] - rhsv[cell_id][3]);
@@ -1185,7 +1183,7 @@ cs_internal_coupling_lsq_rhs(const cs_internal_coupling_t  *cpl,
                              &weight[6*ii],
                              p_diff,
                              dc,
-                             pond,
+                             g_weight[ii],
                              &rhsv[cell_id][0]);
     } else if (scalar_diff) {
       /* (P_j - P_i) / ||d||^2 */
@@ -1195,11 +1193,10 @@ cs_internal_coupling_lsq_rhs(const cs_internal_coupling_t  *cpl,
       for (cs_lnum_t ll = 0; ll < 3; ll++)
         fctb[ll] = dc[ll] * pfac;
 
-      cs_real_t denom = 1. / ( pond * c_weight[cell_id]
-                              + (1. - pond) * weight[ii]);
-
+      /* Compared with _lsq_scalar_gradient, weight from 
+       * _compute_physical_face_weight already contains denom */
       for (cs_lnum_t ll = 0; ll < 3; ll++)
-        rhsv[ii][ll] +=  weight[ii] * denom * fctb[ll];
+        rhsv[cell_id][ll] +=  weight[ii] * fctb[ll];
     } else {
       /* (P_j - P_i) / ||d||^2 */
       pfac = (pvar_local[ii] - rhsv[cell_id][3])
@@ -1209,7 +1206,7 @@ cs_internal_coupling_lsq_rhs(const cs_internal_coupling_t  *cpl,
         fctb[ll] = dc[ll] * pfac;
 
       for (cs_lnum_t ll = 0; ll < 3; ll++)
-        rhsv[ii][ll] +=  fctb[ll];
+        rhsv[cell_id][ll] +=  fctb[ll];
     }
 
   }