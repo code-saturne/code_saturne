a4b29146800d439c73f8b317f0ac8985cb61b5d0
Yvan Fournier <yvan.fournier@noreply.code-saturne.org>
2017-10-28 01:41:10 +0000

    Merge rev. 10665 from trunk (fix for installer).
    git-svn-id: https://svn.isais.edf.fr/mfee/saturne/branches/Version4_0@10671 e44c5d27-f830-0410-8ce8-974f7a764be6

--- install_saturne.py
+++ install_saturne.py
@@ -667,7 +667,7 @@ class Setup:
                     package="scotch",
                     version="6.0.4",
                     archive="scotch_6.0.4.tar.gz",
-                    url="https://gforge.inria.fr/frs/download.php/file/34618")
+                    url="https://gforge.inria.fr/frs/download.php/file/34618/%s")
 
     #---------------------------------------------------------------------------
 
