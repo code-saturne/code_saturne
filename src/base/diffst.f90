code-saturne/code_saturne

commit cc116b5ab9c8451dba1af51e3c54c31ad233714b

Major commit: Add the possibility to put buoyant scalars and density update in velocity-pressure loop. Provided by C. Flageul.
git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@10875 e44c5d27-f830-0410-8ce8-974f7a764be6

src/base/diffst.f90
@@ -23,7 +23,7 @@
 subroutine diffst &
 !================
 
- ( nscal  )
+ ( nscal, iterns )
 
 !===============================================================================
 ! Function :
@@ -37,6 +37,7 @@ subroutine diffst &
 ! name             !type!mode ! role                                           !
 !__________________!____!_____!________________________________________________!
 ! nscal            ! i  ! <-- ! total number of scalars                        !
+! iterns           ! i  ! <-- ! Navier-Stokes iteration number                 !
 !__________________!____!_____!________________________________________________!
 
 !     Type: i (integer), r (real), s (string), a (array), l (logical),
@@ -72,7 +73,7 @@ implicit none
 
 ! Arguments
 
-integer          nscal
+integer          nscal , iterns
 
 ! Local variables
 
@@ -83,6 +84,7 @@ integer          iconvp, idiffp, ircflp
 integer          ischcp, isstpp
 integer          ifcvsl, iflmas, iflmab
 integer          imucpp, idftnp, imasac
+integer          key_buoyant_id, is_buoyant_fld
 double precision epsrgp, climgp, extrap
 double precision blencp, relaxp, thetex
 double precision turb_schmidt
@@ -117,6 +119,15 @@ do iscal = 1, nscal
   ivar = isca(iscal)
 
   call field_get_val_s(ivarfl(isca(iscal)), cvar_scal)
+
+  ! Key id for buoyant field (inside the Navier Stokes loop)
+  call field_get_key_id("is_buoyant", key_buoyant_id)
+  call field_get_key_int(ivarfl(isca(iscal)), key_buoyant_id, is_buoyant_fld)
+
+  ! If the scalar is buoyant, it is inside the Navier Stokes loop, and so iterns >=1
+  ! otherwise it is outside of the loop and iterns = -1.
+  if (  (is_buoyant_fld.eq. 1 .and. iterns.eq.-1) &
+    .or.(is_buoyant_fld.eq.-1 .and. iterns.ne.-1)) return
 
   imucpp = 0
   if (iscavr(iscal).gt.0) then

src/base/alelav.f90
@@ -32,7 +32,16 @@
 !>
 !-------------------------------------------------------------------------------
 
-subroutine alelav
+!-------------------------------------------------------------------------------
+! Arguments
+!______________________________________________________________________________.
+!  mode           name          role                                           !
+!______________________________________________________________________________!
+!> \param[in]     iterns        Navier-Stokes iteration number
+!_______________________________________________________________________________
+
+subroutine alelav &
+ ( iterns )
 
 !===============================================================================
 
@@ -59,6 +68,10 @@ use cs_c_bindings
 !===============================================================================
 
 implicit none
+
+! Arguments
+
+integer          iterns
 
 ! Local variables
 
@@ -235,7 +248,7 @@ ivisep = 0
 
 call coditv &
 !==========
- ( idtvar , ivarfl(iuma)    , iconvp , idiffp , ndircp ,          &
+ ( idtvar , iterns , ivarfl(iuma)    , iconvp , idiffp , ndircp , &
    imrgra , nswrsp , nswrgp , imligp , ircflp , ivisep ,          &
    ischcp , isstpp , iescap , idftnp , iswdyp ,                   &
    iwarnp ,                                                       &

src/base/covofi.f90
@@ -46,6 +46,7 @@
 !> \param[in]     ncesmp        number of cells with mass source term
 !> \param[in]     nfbpcd        number of faces with condensation source terms
 !> \param[in]     ncmast        number of cells with condensation source terms
+!> \param[in]     iterns        Navier-Stokes iteration number
 !> \param[in]     iscal         scalar number
 !> \param[in]     itspdv        indicator to compute production/dissipation
 !>                              terms for a variance:
@@ -78,7 +79,7 @@
 
 subroutine covofi &
  ( nvar   , nscal  , ncepdp , ncesmp , nfbpcd , ncmast ,          &
-   iscal  , itspdv ,                                              &
+   iterns , iscal  , itspdv ,                                     &
    icepdc , icetsm , ifbpcd , ltmast ,                            &
    itypsm , itypcd , itypst ,                                     &
    dt     , tslagr ,                                              &
@@ -127,7 +128,7 @@ implicit none
 
 integer          nvar   , nscal
 integer          ncepdp , ncesmp , nfbpcd ,  ncmast
-integer          iscal  , itspdv
+integer          iterns , iscal  , itspdv
 
 integer          icepdc(ncepdp)
 integer          icetsm(ncesmp), itypsm(ncesmp,nvar)
@@ -159,6 +160,7 @@ integer          iflid , f_id, st_prv_id, st_id,  keydri, iscdri
 integer          icvflb, f_dim, iflwgr
 integer          icla
 integer          icrom_scal
+integer          key_buoyant_id, is_buoyant_fld
 
 integer          ivoid(1)
 
@@ -226,6 +228,15 @@ call field_get_val_prev_s(ivarfl(ivar), cvara_var)
 
 ! Index of the field
 iflid = ivarfl(ivar)
+
+! Key id for buoyant field (inside the Navier Stokes loop)
+call field_get_key_id("is_buoyant", key_buoyant_id)
+call field_get_key_int(iflid, key_buoyant_id, is_buoyant_fld)
+
+! If the scalar is buoyant, it is inside the Navier Stokes loop, and so iterns >=1
+! otherwise it is outside of the loop and iterns = -1.
+if (  (is_buoyant_fld.eq. 1 .and. iterns.eq.-1) &
+  .or.(is_buoyant_fld.eq.-1 .and. iterns.ne.-1)) return
 
 ! Key id for drift scalar
 call field_get_key_id("drift_scalar_model", keydri)
@@ -1226,7 +1237,7 @@ call field_get_coefbf_s(ivarfl(ivar), cofbfp)
 
 call codits &
 !==========
- ( idtvar , ivarfl(ivar)    , iconvp , idiffp , ndircp ,          &
+ ( idtvar , iterns , ivarfl(ivar)    , iconvp , idiffp , ndircp , &
    imrgra , nswrsp , nswrgp , imligp , ircflp ,                   &
    ischcp , isstpp , iescap , imucpp , idftnp , iswdyp ,          &
    iwarnp ,                                                       &

src/base/covofv.f90
@@ -41,6 +41,7 @@
 !> \param[in]     nscal         total number of scalars
 !> \param[in]     ncepdp        number of cells with head loss
 !> \param[in]     ncesmp        number of cells with mass source term
+!> \param[in]     iterns        Navier-Stokes iteration number
 !> \param[in]     iscal         scalar number
 !> \param[in]     icepdc        index of cells with head loss
 !> \param[in]     icetsm        index of cells with mass source term
@@ -56,7 +57,7 @@
 
 subroutine covofv &
  ( nvar   , nscal  , ncepdp , ncesmp ,                            &
-   iscal  ,                                                       &
+   iterns , iscal  ,                                              &
    icepdc , icetsm ,                                              &
    itypsm ,                                                       &
    dt     ,                                                       &
@@ -102,7 +103,7 @@ implicit none
 
 integer          nvar   , nscal
 integer          ncepdp , ncesmp
-integer          iscal
+integer          iterns , iscal
 
 integer          icepdc(ncepdp)
 integer          icetsm(ncesmp), itypsm(ncesmp,nvar)
@@ -127,6 +128,7 @@ integer          nswrsp, ircflp, ischcp, isstpp, iescap, ivissv
 integer          idftnp, iswdyp
 integer          iflid , st_prv_id, st_id,  keydri, iscdri
 integer          icvflb, f_dim, iflwgr
+integer          key_buoyant_id, is_buoyant_fld
 
 integer          ivoid(1)
 
@@ -180,6 +182,15 @@ iflid = ivarfl(ivar)
 
 call field_get_val_v(iflid, cvar_var)
 call field_get_val_prev_v(iflid, cvara_var)
+
+! Key id for buoyant field (inside the Navier Stokes loop)
+call field_get_key_id("is_buoyant", key_buoyant_id)
+call field_get_key_int(iflid, key_buoyant_id, is_buoyant_fld)
+
+! If the vector is buoyant, it is inside the Navier Stokes loop, and so iterns >=1
+! otherwise it is outside of the loop and iterns = -1.
+if (  (is_buoyant_fld.eq. 1 .and. iterns.eq.-1) &
+  .or.(is_buoyant_fld.eq.-1 .and. iterns.ne.-1)) return
 
 ! Key id for drift scalar
 call field_get_key_id("drift_scalar_model", keydri)
@@ -638,7 +649,7 @@ call field_get_coefbf_v(iflid, cofbfp)
 
 call coditv &
 !==========
- ( idtvar , iflid  , iconvp , idiffp , ndircp ,                   &
+ ( idtvar , iterns , iflid  , iconvp , idiffp , ndircp ,          &
    imrgra , nswrsp , nswrgp , imligp , ircflp , ivissv ,          &
    ischcp , isstpp , iescap , idftnp , iswdyp ,                   &
    iwarnp ,                                                       &

src/base/cs_c_bindings.f90
@@ -1583,7 +1583,8 @@ module cs_c_bindings
 
     ! Interface to C function cs_equation_iterative_solve_scalar
 
-    subroutine cs_equation_iterative_solve_scalar(idtvar, f_id, name, ndircp, &
+    subroutine cs_equation_iterative_solve_scalar(idtvar, iterns,             &
+                                                  f_id, name, ndircp,         &
                                                   iescap, imucpp,             &
                                                   vcopt, pvara, pvark,        &
                                                   coefap, coefbp, cofafp,     &
@@ -1597,7 +1598,7 @@ module cs_c_bindings
       bind(C, name='cs_equation_iterative_solve_scalar')
       use, intrinsic :: iso_c_binding
       implicit none
-      integer(c_int), value :: idtvar, f_id, ndircp, iescap, imucpp
+      integer(c_int), value :: idtvar, iterns, f_id, ndircp, iescap, imucpp
       character(kind=c_char, len=1), dimension(*), intent(in) :: name
       type(c_ptr), value :: vcopt
       real(kind=c_double), dimension(*), intent(in) :: pvara, pvark, coefap
@@ -1617,7 +1618,8 @@ module cs_c_bindings
 
     ! Interface to C function cs_equation_iterative_solve_vector
 
-    subroutine cs_equation_iterative_solve_vector(idtvar, f_id, name, ndircp, &
+    subroutine cs_equation_iterative_solve_vector(idtvar, iterns,             &
+                                                  f_id, name, ndircp,         &
                                                   ivisep, iescap,             &
                                                   vcopt, pvara, pvark,        &
                                                   coefav, coefbv, cofafv,     &
@@ -1631,7 +1633,7 @@ module cs_c_bindings
       bind(C, name='cs_equation_iterative_solve_vector')
       use, intrinsic :: iso_c_binding
       implicit none
-      integer(c_int), value :: idtvar, f_id, ndircp, iescap, ivisep
+      integer(c_int), value :: idtvar, iterns, f_id, ndircp, iescap, ivisep
       character(kind=c_char, len=1), dimension(*), intent(in) :: name
       type(c_ptr), value :: vcopt
       real(kind=c_double), dimension(*), intent(in) :: pvara, pvark, coefav
@@ -4135,7 +4137,8 @@ contains
 
   !> Be careful, it is forbidden to modify \f$ f_s^{imp} \f$ here!
 
-  !> \param[in]     idtvar        indicateur du schema temporel
+  !> \param[in]     idtvar        indicator of the temporal scheme
+  !> \param[in]     iterns        external sub-iteration number
   !> \param[in]     f_id          field id (or -1)
   !> \param[in]     iconvp        indicator
   !>                               - 1 convection,
@@ -4238,7 +4241,8 @@ contains
   !> \param[out]    eswork        prediction-stage error estimator
   !>                              (if iescap > 0)
 
-  subroutine codits (idtvar, f_id, iconvp, idiffp, ndircp, imrgra, nswrsp,     &
+  subroutine codits (idtvar, iterns,                                           &
+                     f_id  , iconvp, idiffp, ndircp, imrgra, nswrsp,           &
                      nswrgp, imligp, ircflp, ischcp, isstpp, iescap, imucpp,   &
                      idftnp, iswdyp, iwarnp, blencp, epsilp, epsrsp, epsrgp,   &
                      climgp, extrap, relaxp, thetap, pvara, pvark, coefap,     &
@@ -4255,7 +4259,7 @@ contains
 
     ! Arguments
 
-    integer, intent(in) :: idtvar, f_id, iconvp, idiffp, ndircp, imrgra
+    integer, intent(in) :: idtvar, iterns, f_id, iconvp, idiffp, ndircp, imrgra
     integer, intent(in) :: nswrsp, nswrgp, imligp, ircflp, ischcp, isstpp
     integer, intent(in) :: iescap, imucpp, idftnp, iswdyp, iwarnp
     double precision, intent(in) :: blencp, epsilp, epsrsp, epsrgp, climgp
@@ -4308,7 +4312,8 @@ contains
     vcopt%extrag = extrap
     vcopt%relaxv = relaxp
 
-    call cs_equation_iterative_solve_scalar(idtvar, f_id, c_name, ndircp,      &
+    call cs_equation_iterative_solve_scalar(idtvar, iterns,                    &
+                                            f_id, c_name, ndircp,              &
                                             iescap, imucpp, c_k_value,         &
                                             pvara, pvark,                      &
                                             coefap, coefbp, cofafp, cofbfp,    &
@@ -4367,6 +4372,7 @@ contains
   !> Be careful, it is forbidden to modify \f$ \tens{f_s}^{imp} \f$ here!
 
   !> \param[in]     idtvar        indicator of the temporal scheme
+  !> \param[in]     iterns        external sub-iteration number
   !> \param[in]     f_id          field id (or -1)
   !> \param[in]     iconvp        indicator
   !>                               - 1 convection,
@@ -4470,7 +4476,8 @@ contains
   !> \param[out]    eswork        prediction-stage error estimator
   !>                              (if iescap > 0)
 
-  subroutine coditv (idtvar, f_id  , iconvp, idiffp, ndircp, imrgra, nswrsp,   &
+  subroutine coditv (idtvar, iterns,                                           &
+                     f_id  , iconvp, idiffp, ndircp, imrgra, nswrsp,           &
                      nswrgp, imligp, ircflp, ivisep, ischcp, isstpp, iescap,   &
                      idftnp, iswdyp, iwarnp, blencp, epsilp, epsrsp, epsrgp,   &
                      climgp, relaxp, thetap, pvara , pvark , coefav, coefbv,   &
@@ -4488,7 +4495,7 @@ contains
 
     ! Arguments
 
-    integer, intent(in) :: idtvar, f_id, iconvp, idiffp, ndircp, imrgra
+    integer, intent(in) :: idtvar, iterns, f_id, iconvp, idiffp, ndircp, imrgra
     integer, intent(in) :: nswrsp, nswrgp, imligp, ircflp, ischcp, isstpp
     integer, intent(in) :: iescap, ivisep, idftnp, iswdyp, iwarnp
     double precision, intent(in) :: blencp, epsilp, epsrsp, epsrgp, climgp
@@ -4542,7 +4549,8 @@ contains
     vcopt%extrag = 0
     vcopt%relaxv = relaxp
 
-    call cs_equation_iterative_solve_vector(idtvar, f_id, c_name, ndircp,      &
+    call cs_equation_iterative_solve_vector(idtvar, iterns,                    &
+                                            f_id, c_name, ndircp,              &
                                             ivisep, iescap, c_k_value,         &
                                             pvara, pvark,                      &
                                             coefav, coefbv, cofafv, cofbfv,    &

src/base/cs_equation_iterative_solve.c
@@ -147,7 +147,8 @@ BEGIN_C_DECLS
  * <a href="../../theory.pdf#codits"><b>codits</b></a> section of the
  * theory guide for more informations.
  *
- * \param[in]     idtvar        indicateur du schema temporel
+ * \param[in]     idtvar        indicator of the temporal scheme
+ * \param[in]     iterns        external sub-iteration number
  * \param[in]     f_id          field id (or -1)
  * \param[in]     name          associated name if f_id < 0, or NULL
  * \param[in]     ndircp        indicator (0 if the diagonal is stepped aside)
@@ -205,6 +206,7 @@ BEGIN_C_DECLS
 
 void
 cs_equation_iterative_solve_scalar(int                   idtvar,
+                                   int                   iterns,
                                    int                   f_id,
                                    const char           *name,
                                    int                   ndircp,
@@ -610,7 +612,8 @@ cs_equation_iterative_solve_scalar(int                   idtvar,
   nswmod = CS_MAX(var_cal_opt->nswrsm, 1);
 
   /* Reconstruction loop (beginning) */
-  sinfo.n_it = 0;
+  if (iterns <= 1)
+    sinfo.n_it = 0;
   isweep = 1;
 
   while ((isweep <= nswmod && residu > epsrsp*rnorm) || isweep == 1) {
@@ -1016,6 +1019,7 @@ cs_equation_iterative_solve_scalar(int                   idtvar,
  * Be careful, it is forbidden to modify \f$ \tens{f_s}^{imp} \f$ here!
  *
  * \param[in]     idtvar        indicator of the temporal scheme
+ * \param[in]     iterns        external sub-iteration number
  * \param[in]     f_id          field id (or -1)
  * \param[in]     name          associated name if f_id < 0, or NULL
  * \param[in]     ndircp        indicator (0 if the diagonal is stepped aside)
@@ -1075,6 +1079,7 @@ cs_equation_iterative_solve_scalar(int                   idtvar,
 
 void
 cs_equation_iterative_solve_vector(int                   idtvar,
+                                   int                   iterns,
                                    int                   f_id,
                                    const char           *name,
                                    int                   ndircp,
@@ -1410,7 +1415,8 @@ cs_equation_iterative_solve_vector(int                   idtvar,
 
   /* Reconstruction loop (beginning)
    *-------------------------------- */
-  sinfo.n_it = 0;
+  if (iterns <= 1)
+    sinfo.n_it = 0;
 
   while ((isweep <= nswmod && residu > epsrsp*rnorm) || isweep == 1) {
     /* --- Solving on the increment dpvar */

src/base/cs_equation_iterative_solve.h
@@ -88,7 +88,8 @@ BEGIN_C_DECLS
  *
  * Be careful, it is forbidden to modify \f$ f_s^{imp} \f$ here!
  *
- * \param[in]     idtvar        indicateur du schema temporel
+ * \param[in]     idtvar        indicator of the temporal scheme
+ * \param[in]     iterns        external sub-iteration number
  * \param[in]     f_id          field id (or -1)
  * \param[in]     ndircp        indicator (0 if the diagonal is stepped aside)
  * \param[in]     iescap        compute the predictor indicator if 1
@@ -145,6 +146,7 @@ BEGIN_C_DECLS
 
 void
 cs_equation_iterative_solve_scalar(int                   idtvar,
+                                   int                   iterns,
                                    int                   f_id,
                                    const char           *name,
                                    int                   ndircp,
@@ -221,6 +223,7 @@ cs_equation_iterative_solve_scalar(int                   idtvar,
  * Be careful, it is forbidden to modify \f$ \tens{f_s}^{imp} \f$ here!
  *
  * \param[in]     idtvar        indicator of the temporal scheme
+ * \param[in]     iterns        external sub-iteration number
  * \param[in]     f_id          field id (or -1)
  * \param[in]     ndircp        indicator (0 if the diagonal is stepped aside)
  * \param[in]     ivisep        indicator to take \f$ \divv
@@ -279,6 +282,7 @@ cs_equation_iterative_solve_scalar(int                   idtvar,
 
 void
 cs_equation_iterative_solve_vector(int                   idtvar,
+                                   int                   iterns,
                                    int                   f_id,
                                    const char           *name,
                                    int                   ndircp,

src/base/cs_parameters.c
@@ -864,6 +864,9 @@ cs_parameters_define_field_keys(void)
 
   cs_field_define_key_int("scalar_density_id", -1, CS_FIELD_VARIABLE);
 
+  /* is the field buoyant? -1 if not, 1 if yes */
+  cs_field_define_key_int("is_buoyant", -1, CS_FIELD_VARIABLE);
+
   cs_field_define_key_int("turbulent_flux_model", 0, CS_FIELD_VARIABLE);
   cs_field_define_key_int("turbulent_flux_id", -1, CS_FIELD_VARIABLE);
 

commit df9128d0a5aefe218c6a48584e65154d650280ea

Fix diffst.f90 issue when several scalars are present.
Merged from commit 1f4d779 on master.

Issue was introduced in commit cc116b5a, Jan 10 2018.
Thanks to C. Flageul for this patch.

src/base/diffst.f90
@@ -127,7 +127,7 @@ do iscal = 1, nscal
   ! If the scalar is buoyant, it is inside the Navier Stokes loop, and so iterns >=1
   ! otherwise it is outside of the loop and iterns = -1.
   if (  (is_buoyant_fld.eq. 1 .and. iterns.eq.-1) &
-    .or.(is_buoyant_fld.eq.-1 .and. iterns.ne.-1)) return
+    .or.(is_buoyant_fld.eq.-1 .and. iterns.ne.-1)) cycle
 
   imucpp = 0
   if (iscavr(iscal).gt.0) then