code-saturne/code_saturne

commit fa9c961190a1ed7681dd7c2740061dcaa161d328

Change in wall boundary conditions for DFM/EB-DFM/EB-AFM/EB-GGDH
  * icodcl is set to 5(6) by default at walls (rough walls) for
    the elliptic blending parameter (iturt = 11, 21, 30, 31);
  * wall BCs for scalar turbulent fluxes are made independant of
    the scalars BCs: icodcl is set to 5(6) at walls(rough walls)
    (iturt = 30, 31).

src/base/clptur.f90
@@ -2031,8 +2031,8 @@ integer          ifac, iel, isou, jsou
 integer          ifcvsl, itplus, itstar
 
 double precision cpp, rkl, prdtl, visclc, romc, tplus, cofimp, cpscv
-double precision distfi, distbf, fikis, hint, heq, yptp, hflui, hext
-double precision yplus, dplus, phit, pimp, rcprod, temp, tet, uk
+double precision distfi, distbf, fikis, hint, hint_al, heq, yptp, hflui, hext
+double precision yplus, dplus, phit, pimp, pimp_al, rcprod, temp, tet, uk
 double precision viscis, visctc, xmutlm, ypth, xnuii
 double precision rinfiv(3), pimpv(3)
 double precision visci(3,3), hintt(6)
@@ -2400,72 +2400,6 @@ do ifac = 1, nfabor
       cofafp(ifac) = -heq*pimp
       cofbfp(ifac) =  heq
 
-      !--> Turbulent heat flux
-      if (ityturt(iscal).eq.3) then
-
-        ! Turbulent diffusive flux of the scalar T
-        ! (blending factor so that the component v'T' have only
-        !  mu_T/(mu+mu_T)* Phi_T)
-        phit = (cofafp(ifac) + cofbfp(ifac)*val_s(iel))
-
-        hintt(1) =   0.5d0*(visclc+rkl)/distbf                        &
-                   + visten(1,iel)*ctheta(iscal)/distbf/csrij
-        hintt(2) =   0.5d0*(visclc+rkl)/distbf                        &
-                   + visten(2,iel)*ctheta(iscal)/distbf/csrij
-        hintt(3) =   0.5d0*(visclc+rkl)/distbf                        &
-                   + visten(3,iel)*ctheta(iscal)/distbf/csrij
-        hintt(4) = visten(4,iel)*ctheta(iscal)/distbf/csrij
-        hintt(5) = visten(5,iel)*ctheta(iscal)/distbf/csrij
-        hintt(6) = visten(6,iel)*ctheta(iscal)/distbf/csrij
-
-        ! Dirichlet Boundary Condition
-        !-----------------------------
-
-        ! Add rho*uk*Tet to T'v' in High Reynolds
-        if (yplus.ge.ypth) then
-          do isou = 1, 3
-            pimpv(isou) = surfbo(isou,ifac)*phit/(surfbn(ifac)*cpp*romc)
-          enddo
-        else
-          do isou = 1, 3
-            pimpv(isou) = 0.d0
-          enddo
-        endif
-
-        call set_dirichlet_vector_aniso &
-             !========================
-           ( coefaut(:,ifac)  , cofafut(:,ifac)  ,           &
-             coefbut(:,:,ifac), cofbfut(:,:,ifac),           &
-             pimpv            , hintt            , rinfiv )
-
-        ! Boundary conditions used in the temperature equation
-        do isou = 1, 3
-          cofarut(isou,ifac) = 0.d0
-          do jsou = 1, 3
-            cofbrut(isou,jsou,ifac) = 0.d0
-          enddo
-        enddo
-
-      endif
-
-      ! EB-GGDH/AFM/DFM alpha boundary conditions
-      if (iturt(iscal).eq.11 .or. iturt(iscal).eq.21 .or. iturt(iscal).eq.31) then
-
-        ! Dirichlet Boundary Condition
-        !-----------------------------
-
-        pimp = 0.d0
-
-        hint = 1.d0/distbf
-
-        call set_dirichlet_scalar &
-             !====================
-           ( a_al(ifac), af_al(ifac),             &
-             b_al(ifac), bf_al(ifac),             &
-             pimp      , hint       , rinfin )
-
-      endif
-
       ! Storage of the thermal exchange coefficient
       ! (conversion in case of energy or enthaly)
       ! the exchange coefficient is in W/(m2 K)
@@ -2508,6 +2442,78 @@ do ifac = 1, nfabor
       endif
 
     endif ! End if icodcl.eq.5
+
+    !--> Turbulent heat flux
+    if (ityturt(iscal).eq.3) then
+
+      ! Turbulent diffusive flux of the scalar T
+      ! (blending factor so that the component v'T' have only
+      !  mu_T/(mu+mu_T)* Phi_T)
+      if (icodcl(ifac,ivar).eq.5) then
+        phit = cofafp(ifac)+cofbfp(ifac)*val_s(ifac)
+      elseif (icodcl(ifac,ivar).eq.3) then
+        phit = rcodcl(ifac,ivar,3)
+      elseif (icodcl(ifac,ivar).eq.1) then
+        phit = heq *(val_s(ifac) - pimp)
+      else
+        phit = 0.d0
+      endif
+
+      hintt(1) =   0.5d0*(visclc+rkl)/distbf                        &
+                 + visten(1,iel)*ctheta(iscal)/distbf/csrij
+      hintt(2) =   0.5d0*(visclc+rkl)/distbf                        &
+                 + visten(2,iel)*ctheta(iscal)/distbf/csrij
+      hintt(3) =   0.5d0*(visclc+rkl)/distbf                        &
+                 + visten(3,iel)*ctheta(iscal)/distbf/csrij
+      hintt(4) = visten(4,iel)*ctheta(iscal)/distbf/csrij
+      hintt(5) = visten(5,iel)*ctheta(iscal)/distbf/csrij
+      hintt(6) = visten(6,iel)*ctheta(iscal)/distbf/csrij
+
+      ! Dirichlet Boundary Condition
+      !-----------------------------
+
+      ! Add rho*uk*Tet to T'v' in High Reynolds
+      if (yplus.ge.ypth) then
+        do isou = 1, 3
+          pimpv(isou) = surfbo(isou,ifac)*phit/(surfbn(ifac)*cpp*romc)
+        enddo
+      else
+        do isou = 1, 3
+          pimpv(isou) = 0.d0
+        enddo
+      endif
+
+      call set_dirichlet_vector_aniso &
+           !========================
+         ( coefaut(:,ifac)  , cofafut(:,ifac)  ,           &
+           coefbut(:,:,ifac), cofbfut(:,:,ifac),           &
+           pimpv            , hintt            , rinfiv )
+
+      ! Boundary conditions used in the temperature equation
+      do isou = 1, 3
+        cofarut(isou,ifac) = 0.d0
+        do jsou = 1, 3
+          cofbrut(isou,jsou,ifac) = 0.d0
+        enddo
+      enddo
+
+    endif
+
+    ! EB-GGDH/AFM/DFM alpha boundary conditions
+    if (iturt(iscal).eq.11 .or. iturt(iscal).eq.21 .or. iturt(iscal).eq.31) then
+
+      ! Dirichlet Boundary Condition
+      !-----------------------------
+      pimp_al = 0.d0
+      hint_al = 1.d0/distbf
+
+      call set_dirichlet_scalar &
+           !====================
+         ( a_al(ifac), af_al(ifac),             &
+           b_al(ifac), bf_al(ifac),             &
+           pimp_al   , hint_al    , rinfin )
+
+    endif
 
     ! Save the values of of T^star and T^+ for post-processing
 

src/base/typecl.f90
@@ -113,7 +113,7 @@ integer          ifac, ivar, iel
 integer          iok, inc, iccocg, iprev, ideb, ifin, inb, isum
 integer          ityp, ii, jj, iflmab
 integer          ifadir
-integer          iut  , ivt   , iwt, iscal
+integer          iut  , ivt   , iwt, ialt, iscal
 integer          keyvar, keycpl
 integer          iivar, icpl
 integer          f_id, i_dim, f_type, nfld, f_dim, f_id_yplus
@@ -1057,7 +1057,7 @@ do ivar = 1, nvar
   endif
 enddo
 
-! Turbulent fluxes: 0 Dirichlet if Dirichlet on the scalar
+! Turbulent fluxes
 do iscal = 1, nscal
   if (ityturt(iscal).eq.3) then
     ! Name of the scalar ivar
@@ -1066,7 +1066,6 @@ do iscal = 1, nscal
     ! Index of the corresponding turbulent flux
     call field_get_id(trim(fname)//'_turbulent_flux', f_id)
 
-
     ! Set pointer values of turbulent fluxes in icodcl
     call field_get_key_int(f_id, keyvar, iut)
     ivt = iut + 1
@@ -1074,14 +1073,7 @@ do iscal = 1, nscal
 
     do ii = ideb, ifin
       ifac = itrifb(ii)
-      if (icodcl(ifac, isca(iscal)).eq.1.and.icodcl(ifac, iut).eq.0) then
-        icodcl(ifac,iut) = 1
-        icodcl(ifac,ivt) = 1
-        icodcl(ifac,iwt) = 1
-        rcodcl(ifac,iut,1) = 0.d0
-        rcodcl(ifac,ivt,1) = 0.d0
-        rcodcl(ifac,iwt,1) = 0.d0
-      else if (icodcl(ifac, isca(iscal)).eq.5.and.icodcl(ifac, iut).eq.0) then
+      if (icodcl(ifac, iut).eq.0) then
         icodcl(ifac,iut) = 5
         icodcl(ifac,ivt) = 5
         icodcl(ifac,iwt) = 5
@@ -1091,6 +1083,28 @@ do iscal = 1, nscal
       endif
     enddo
   endif
+
+  ! EB-GGDH/AFM/DFM alpha boundary conditions
+  if (iturt(iscal).eq.11 .or. iturt(iscal).eq.21 .or. iturt(iscal).eq.31) then
+    ! Name of the scalar ivar
+    call field_get_name(ivarfl(isca(iscal)), fname)
+
+    ! Index of the corresponding turbulent flux
+    call field_get_id(trim(fname)//'_alpha', f_id)
+
+    ! Set pointer values of turbulent fluxes in icodcl
+    call field_get_key_int(f_id, keyvar, ialt)
+
+    do ii = ideb, ifin
+      ifac = itrifb(ii)
+      if (icodcl(ifac, ialt).eq.0) then
+        icodcl(ifac,ialt) = 5
+        rcodcl(ifac,ialt,1) = 0.d0
+      endif
+    enddo
+  endif
+
+! End loop over scalars
 enddo
 
 
@@ -1153,7 +1167,7 @@ do ivar = 1, nvar
   endif
 enddo
 
-! Turbulent fluxes: 0 Dirichlet if Dirichlet on the scalar
+! Turbulent fluxes
 do iscal = 1, nscal
   if (ityturt(iscal).eq.3) then
     ! Name of the scalar ivar
@@ -1162,7 +1176,6 @@ do iscal = 1, nscal
     ! Index of the corresponding turbulent flux
     call field_get_id(trim(fname)//'_turbulent_flux', f_id)
 
-
     ! Set pointer values of turbulent fluxes in icodcl
     call field_get_key_int(f_id, keyvar, iut)
     ivt = iut + 1
@@ -1170,14 +1183,7 @@ do iscal = 1, nscal
 
     do ii = ideb, ifin
       ifac = itrifb(ii)
-      if (icodcl(ifac, isca(iscal)).eq.1.and.icodcl(ifac, iut).eq.0) then
-        icodcl(ifac,iut) = 1
-        icodcl(ifac,ivt) = 1
-        icodcl(ifac,iwt) = 1
-        rcodcl(ifac,iut,1) = 0.d0
-        rcodcl(ifac,ivt,1) = 0.d0
-        rcodcl(ifac,iwt,1) = 0.d0
-      else if (icodcl(ifac, isca(iscal)).eq.6.and.icodcl(ifac, iut).eq.0) then
+      if (icodcl(ifac, iut).eq.0) then
         icodcl(ifac,iut) = 6
         icodcl(ifac,ivt) = 6
         icodcl(ifac,iwt) = 6
@@ -1187,6 +1193,28 @@ do iscal = 1, nscal
       endif
     enddo
   endif
+
+  ! EB-GGDH/AFM/DFM alpha boundary conditions
+  if (iturt(iscal).eq.11 .or. iturt(iscal).eq.21 .or. iturt(iscal).eq.31) then
+    ! Name of the scalar ivar
+    call field_get_name(ivarfl(isca(iscal)), fname)
+
+    ! Index of the corresponding turbulent flux
+    call field_get_id(trim(fname)//'_alpha', f_id)
+
+    ! Set pointer values of turbulent fluxes in icodcl
+    call field_get_key_int(f_id, keyvar, ialt)
+
+    do ii = ideb, ifin
+      ifac = itrifb(ii)
+      if (icodcl(ifac, ialt).eq.0) then
+        icodcl(ifac,ialt) = 6
+        rcodcl(ifac,ialt,1) = 0.d0
+      endif
+    enddo
+  endif
+
+! End loop over scalars
 enddo
 
 !===============================================================================

commit b01dfbb67fa011fe59661f557dd280f503f655ce

Fix bug introduced in commit fa9c9611 : bad indexing (28-05-2018)

src/base/clptur.f90
@@ -2450,11 +2450,11 @@ do ifac = 1, nfabor
       ! (blending factor so that the component v'T' have only
       !  mu_T/(mu+mu_T)* Phi_T)
       if (icodcl(ifac,ivar).eq.5) then
-        phit = cofafp(ifac)+cofbfp(ifac)*val_s(ifac)
+        phit = cofafp(ifac)+cofbfp(ifac)*val_s(iel)
       elseif (icodcl(ifac,ivar).eq.3) then
         phit = rcodcl(ifac,ivar,3)
       elseif (icodcl(ifac,ivar).eq.1) then
-        phit = heq *(val_s(ifac) - pimp)
+        phit = heq *(val_s(iel) - pimp)
       else
         phit = 0.d0
       endif