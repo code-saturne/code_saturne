8d6b15aaa7f3d495845bd1954bfb30493026a81e
Jérôme Bonelle <jerome.bonelle@edf.fr>
2018-06-13 14:51:11 +0200

    CDO: Minor modifications in the computation of the flux across a plane

--- src/cdo/cs_cdovb_scaleq.c
+++ src/cdo/cs_cdovb_scaleq.c
@@ -1479,15 +1479,10 @@ cs_cdovb_scaleq_compute_flux_across_plane(const cs_real_t             normal[],
 
     const cs_lnum_t  n_i_faces = connect->n_faces[2];
     const cs_lnum_t  *cell_ids = f2c->ids + f2c->idx[n_i_faces];
-    cs_lnum_t  bf_id;
 
     for (cs_lnum_t i = 0; i < n_elts[0]; i++) {
 
-      if (elt_ids != NULL)
-        bf_id = elt_ids[i];
-      else
-        bf_id = i;
-
+      const cs_lnum_t  bf_id = (elt_ids != NULL) ? elt_ids[i] : i;
       const cs_lnum_t  f_id = n_i_faces + bf_id;
       const cs_lnum_t  c_id = cell_ids[bf_id];
       const cs_quant_t  f = cs_quant_set_face(f_id, quant);
@@ -1522,12 +1517,12 @@ cs_cdovb_scaleq_compute_flux_across_plane(const cs_real_t             normal[],
 
       }
 
-    } // Loop on selected border faces
+    } /* Loop on selected border faces */
 
   }
   else if (ml_t == CS_MESH_LOCATION_INTERIOR_FACES) {
 
-    if (elt_ids == NULL)
+    if (n_elts[0] > 0 && elt_ids == NULL)
       bft_error(__FILE__, __LINE__, 0,
                 _(" Computing the flux across all interior faces is"
                   " not managed yet."));
@@ -1588,7 +1583,7 @@ cs_cdovb_scaleq_compute_flux_across_plane(const cs_real_t             normal[],
 
       }
 
-    } // Loop on selected interior faces
+    }  /* Loop on selected interior faces */
 
   } // Set of interior or border faces
 
