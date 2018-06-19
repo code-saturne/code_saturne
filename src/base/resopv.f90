cf206d4c764dca656006e7b2be0e318599f477a9
Martin Ferrand <martin.ferrand@edf.fr>
2018-06-19 20:12:07 +0200

    Merge from commit d89e177 on master.
    Fix bug in severe accident (metal source mass source terms was not
    correctly taken into account).

--- src/base/resopv.f90
+++ src/base/resopv.f90
@@ -1320,7 +1320,7 @@ if (icondv.eq.0) then
   do ii = 1, ncmast
     iel= ltmast(ii)
     surfbm(iel) = s_metal*volume(iel)/voltot
-    res(iel) = res(iel) - surfbm(iel)*svcond(iel,ipr)
+    cpro_divu(iel) = cpro_divu(iel) - surfbm(iel)*svcond(iel,ipr)
   enddo
 
   deallocate(surfbm)
