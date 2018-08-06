code-saturne/code_saturne

commit 8b9efac827371fdc981a54168f57447a32df930f

Avoid GUI crash when entering empty numbers.

gui/Base/QtPage.py
@@ -136,7 +136,7 @@ if os.environ.get('QT_API', 'pyqt') == 'pyqt':
 
     def from_qvariant(qobj=None, convfunc=None):
         """Convert QVariant object to Python object
-        This is a transitional function from PyQt API #1 (QVariant exist)
+        This is a transitional function from PyQt API #1 (QVariant exists)
         to PyQt API #2 and Pyside (QVariant does not exist)"""
         if PYQT_API_1:
             assert isinstance(convfunc, collections.Callable)
@@ -151,15 +151,16 @@ if os.environ.get('QT_API', 'pyqt') == 'pyqt':
             else:
                 return convfunc(qobj)
         else:
-            if convfunc in TEXT_TYPES or convfunc is to_text_string:
-                return str(qobj)
-            elif convfunc is int:
-                return int(qobj)
-            elif convfunc is float:
-                try:
-                    return float(qobj)
-                except Exception:
-                    return locale.atof(qobj)
+            if (qobj):
+                if convfunc in TEXT_TYPES or convfunc is to_text_string:
+                    return str(qobj)
+                elif convfunc is int:
+                    return int(qobj)
+                elif convfunc is float:
+                    try:
+                        return float(qobj)
+                    except Exception:
+                        return locale.atof(qobj)
             else:
                 return qobj
 

commit 04950843072c021664d57f277e07294a90898545

Fix various crashes using the PyQt API2.
Some of the crashes were introduced by commit 8b9efac (May 27 2019).

Cherry-picked commits a3f5f19 and 4a02835 on master.

gui/Base/QtPage.py
@@ -136,7 +136,7 @@ if os.environ.get('QT_API', 'pyqt') == 'pyqt':
 
     def from_qvariant(qobj=None, convfunc=None):
         """Convert QVariant object to Python object
-        This is a transitional function from PyQt API #1 (QVariant exist)
+        This is a transitional function from PyQt API #1 (QVariant exists)
         to PyQt API #2 and Pyside (QVariant does not exist)"""
         if PYQT_API_1:
             assert isinstance(convfunc, collections.Callable)
@@ -151,15 +151,18 @@ if os.environ.get('QT_API', 'pyqt') == 'pyqt':
             else:
                 return convfunc(qobj)
         else:
-            if convfunc in TEXT_TYPES or convfunc is to_text_string:
-                return str(qobj)
-            elif convfunc is int:
-                return int(qobj)
-            elif convfunc is float:
-                try:
-                    return float(qobj)
-                except Exception:
-                    return locale.atof(qobj)
+            if (qobj != None):
+                if convfunc in TEXT_TYPES or convfunc is to_text_string:
+                    return str(qobj)
+                elif convfunc is int:
+                    return int(qobj)
+                elif convfunc is float:
+                    try:
+                        return float(qobj)
+                    except Exception:
+                        return locale.atof(qobj)
+                else:
+                    return qobj
             else:
                 return qobj
 