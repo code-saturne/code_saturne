code-saturne/code_saturne

commit 8cbab47f468417600d7adb7ebc961c6f9c095cb2

Numerous fixes based on compiler warnings.
git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@3960 e44c5d27-f830-0410-8ce8-974f7a764be6

src/gui/cs_gui_radiative_transfer.c
@@ -331,7 +331,7 @@ _radiative_boundary(const   char *const label,
   cs_xpath_add_function_text(&path);
 
   if (cs_gui_get_double(path, &res)) {
-    if (res != *value)
+    if (!cs_gui_is_equal_real(res, *value))
       *value = res;
   }
 
@@ -1001,8 +1001,8 @@ void CS_PROCF (uiray2, UIRAY2)
         isothp[ifbr] = boundary->type[izone];
         if (isothp[ifbr] == *itpimp)
         {
-            epsp[ifbr] = boundary->emissivity[izone];
-            tintp[ifbr] = boundary->internal_temp[izone];
+          epsp[ifbr] = boundary->emissivity[izone];
+          tintp[ifbr] = boundary->internal_temp[izone];
         }
         else if (isothp[ifbr] == *ipgrno)
         {
@@ -1011,8 +1011,8 @@ void CS_PROCF (uiray2, UIRAY2)
           textp[ifbr] = boundary->external_temp[izone];
           tintp[ifbr] = boundary->internal_temp[izone];
           epsp[ifbr] = boundary->emissivity[izone];
-          if (boundary->emissivity[izone] == 0.)
-               isothp[ifbr] = *iprefl;
+          if (cs_gui_is_equal_real(boundary->emissivity[izone], 0.))
+            isothp[ifbr] = *iprefl;
         }
         else if (isothp[ifbr] == *ifgrno)
         {
@@ -1020,8 +1020,8 @@ void CS_PROCF (uiray2, UIRAY2)
             = boundary->conduction_flux[izone];
           tintp[ifbr] = boundary->internal_temp[izone];
           epsp[ifbr] = boundary->emissivity[izone];
-          if (boundary->emissivity[izone] != 0.)
-               isothp[ifbr] = *ifrefl;
+          if (!cs_gui_is_equal_real(boundary->emissivity[izone], 0.))
+            isothp[ifbr] = *ifrefl;
         }
       }
 

src/alge/cs_matrix.c
@@ -3779,7 +3779,7 @@ _matrix_check(int                    n_variants,
 
           cs_matrix_variant_t *v = m_variant + v_id;
 
-          cs_matrix_vector_product_t        *vector_multiply = NULL;
+          cs_matrix_vector_product_t  *vector_multiply = NULL;
 
           if (sym_flag == 0) {
             if (v->symmetry == 1)
@@ -4359,10 +4359,11 @@ _variant_init(cs_matrix_variant_t  *v)
 
   v->matrix_create_cost = -1.;
 
-  for (i = 0; i < 6; i++) {
+  for (i = 0; i < 6; i++)
     v->vector_multiply[i] = NULL;
+
+  for (i = 0; i < 4; i++)
     v->matrix_assign_cost[i] = -1.;
-  }
 
   for (i = 0; i < 8; i++)
     v->matrix_vector_cost[i] = -1.;

src/alge/cs_matrix_priv.h
@@ -341,7 +341,7 @@ struct _cs_matrix_variant_t {
 
   int                    loop_length;
 
-  /* Function pointer arrays, with 4 variants:
+  /* Function pointer arrays, with 6 variants (0 <= block_flag <= 2):
      block_flag*2 + exclude_diagonal_flag + extra_block_flag */
 
   cs_matrix_vector_product_t        *vector_multiply[6];

src/alge/cs_multigrid.c
@@ -1854,9 +1854,6 @@ void CS_PROCF(resmgr, RESMGR)
 (
  const char       *cname,     /* <-- variable name */
  const cs_int_t   *lname,     /* <-- variable name length */
- const cs_int_t   *ncelet,    /* <-- Number of cells, halo included */
- const cs_int_t   *ncel,      /* <-- Number of local cells */
- const cs_int_t   *nfac,      /* <-- Number of faces */
  const cs_int_t   *iresds,    /* <-- Descent smoother type:
                                      0: pcg; 1: Jacobi; 2: cg-stab */
  const cs_int_t   *iresas,    /* <-- Ascent smoother type:
@@ -1894,9 +1891,6 @@ void CS_PROCF(resmgr, RESMGR)
   int _ireslp = *ireslp;
 
   cs_halo_rotation_t rotation_mode = CS_HALO_ROTATION_COPY;
-
-  assert(*ncelet >= *ncel);
-  assert(*nfac > 0);
 
   if (*iinvpe == 2)
     rotation_mode = CS_HALO_ROTATION_ZERO;

src/alge/cs_multigrid.h
@@ -98,9 +98,6 @@ void CS_PROCF(resmgr, RESMGR)
 (
  const char       *cname,     /* <-- variable name */
  const cs_int_t   *lname,     /* <-- variable name length */
- const cs_int_t   *ncelet,    /* <-- Number of cells, halo included */
- const cs_int_t   *ncel,      /* <-- Number of local cells */
- const cs_int_t   *nfac,      /* <-- Number of faces */
  const cs_int_t   *iresds,    /* <-- Descent smoother type:
                                      0: pcg; 1: Jacobi; 2: cg-stab */
  const cs_int_t   *iresas,    /* <-- Ascent smoother type:

src/alge/diftnv.f90
@@ -386,7 +386,7 @@ if (idtvar.lt.0) then
 
   do ig = 1, ngrpb
     !$omp parallel do private(ifac, ii, isou, jsou, diipbv,               &
-    !$omp                     pfacd, pir, pipr, flux,        )            &
+    !$omp                     pfacd, pir, pipr, flux)                     &
     !$omp          if(nfabor > thr_n_min)
     do it = 1, nthrdb
       do ifac = iomplb(1,ig,it), iomplb(2,ig,it)

src/alge/divrij.f90
@@ -114,6 +114,11 @@ double precision epsrgp, climgp, extrap
 ! 1.  INITIALISATION
 !===============================================================================
 
+! Initialization to avoid compiler warnings
+ivar1 = 0
+ivar2 = 0
+ivar2 = 0
+
 ! --- Memoire
 
 ! --- Masse volumique

src/alge/invers.f90
@@ -136,7 +136,7 @@ if (imgrp.eq.1) then
 
   call resmgr                                                     &
   !==========
- ( cnom   , lnom   , ncelet , ncel   , nfac   ,                   &
+ ( cnom   , lnom   ,                                              &
    iresds , iresas , ireslp , ipol   ,                            &
    ncymxp , nitmds , nitmas , nitmap , iinvpe ,                   &
    iwarnp , icycle , niterf , epsilp , rnorm  , residu ,          &

commit de6d329b46043e3dfa0b6f9a7730cf3a35391cf9

FIX: Choice of flux BC by the Rad solver. Bug introduced in commit 8cbab47 (27/09/2012)
  * The test which chooses between ifgrno(emissivity+gray wall) and ifrefl
    (reflecting  wall) was wrong.

src/gui/cs_gui_radiative_transfer.c
@@ -713,7 +713,7 @@ cs_gui_radiative_transfer_bcs(const    int   itypfb[],
           rcodcl[2 * n_b_faces * nvar + ivart * n_b_faces + ifbr]
             = _boundary->conduction_flux[izone];
           tintp[ifbr] = _boundary->internal_temp[izone];
-          if (!cs_gui_is_equal_real(_boundary->emissivity[izone], 0.))
+          if (cs_gui_is_equal_real(_boundary->emissivity[izone], 0.))
             isothp[ifbr] = cs_glob_rad_transfer_params->ifrefl;
           else
             epsp[ifbr] = _boundary->emissivity[izone];