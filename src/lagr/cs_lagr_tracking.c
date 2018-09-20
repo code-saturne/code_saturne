code-saturne/code_saturne

commit c0295b30917765476bcd4a1d7595d042f0376dc6

Make particle tracking compatible with transient turbomachinery model.

src/lagr/cs_lagr_tracking.c
@@ -67,8 +67,10 @@
 #include "cs_parall.h"
 #include "cs_prototypes.h"
 #include "cs_random.h"
+#include "cs_rotation.h"
 #include "cs_search.h"
 #include "cs_timer_stats.h"
+#include "cs_turbomachinery.h"
 
 #include "cs_field.h"
 #include "cs_field_pointer.h"
@@ -215,7 +217,7 @@ typedef struct {
   cs_lnum_t  *cell_face_idx;
   cs_lnum_t  *cell_face_lst;
 
-  cs_lagr_halo_t    *halo;   /* Lagrangian halo structure */
+  cs_lagr_halo_t      *halo;   /* Lagrangian halo structure */
 
   cs_interface_set_t  *face_ifs;
 
@@ -229,7 +231,7 @@ typedef struct {
 
 static  cs_lagr_track_builder_t  *_particle_track_builder = NULL;
 
-static  int                 _max_propagation_loops = 100;
+static  int            _max_propagation_loops = 100;
 
 /* MPI datatype associated to each particle "structure" */
 
@@ -2280,6 +2282,8 @@ _local_propagation(void                           *particle,
 
       if (move_particle != CS_LAGR_PART_MOVE_OFF) {
 
+        /* Now update cell */
+
         if (cur_cell_id == c_id1) {
           cs_lagr_particle_set_lnum(particle, p_am, CS_LAGR_CELL_NUM,
                                     c_id2 + 1);
@@ -3012,7 +3016,29 @@ _initialize_displacement(cs_lagr_particle_set_t  *particles,
 
   const cs_real_t  *b_face_surf = cs_glob_mesh_quantities->b_face_surf;
 
+  /* Initialize builder if needed */
+
+  if (_particle_track_builder == NULL)
+    _particle_track_builder
+      = _init_track_builder(particles->n_particles_max,
+                            particles->p_am->extents);
+
   assert(am->lb >= sizeof(cs_lagr_tracking_info_t));
+
+  /* Info for rotor-stator cases; the time step should actually
+     be based on the global (non-Lagrangian) time step in case
+     the two differ. Currently both are in lock-step; if one
+     becomes a mutiplier of the other, the rotor-movement update
+     would need to be done at a frequency matching that multiplier. */
+
+  const int *cell_rotor_num = NULL;
+  cs_real_34_t  *rot_m = NULL;
+
+  if (cs_turbomachinery_get_model() == CS_TURBOMACHINERY_TRANSIENT) {
+    cell_rotor_num = cs_turbomachinery_get_cell_rotor_num();
+    cs_real_t dt = cs_glob_lagr_time_step->dtp;
+    rot_m = cs_turbomachinery_get_rotation_matrices(-1.0 * dt);
+  }
 
   /* Prepare tracking info */
 
@@ -3054,11 +3080,20 @@ _initialize_displacement(cs_lagr_particle_set_t  *particles,
     _tracking_info(particles, i)->start_coords[1] = prv_part_coord[1];
     _tracking_info(particles, i)->start_coords[2] = prv_part_coord[2];
 
+    if (cell_rotor_num != NULL) {
+      cs_lnum_t  cur_cell_id = CS_ABS(cur_part_cell_num) - 1;
+      int r_num = cell_rotor_num[cur_cell_id];
+      if (r_num > 0) {
+        _apply_vector_transfo((const cs_real_t (*)[4])rot_m[r_num],
+                              _tracking_info(particles, i)->start_coords);
+      }
+    }
+
     /* Just after injection, reduce displacment so as to simulate
        continuous injection */
 
     cs_real_t res_time = cs_lagr_particles_get_real(particles, i,
-                                                   CS_LAGR_RESIDENCE_TIME);
+                                                    CS_LAGR_RESIDENCE_TIME);
 
     if (res_time < 0) {
       cs_real_t fraction =   (cs_glob_lagr_time_step->dtp + res_time)
@@ -3091,6 +3126,8 @@ _initialize_displacement(cs_lagr_particle_set_t  *particles,
                            part_b_mass_flux);
 
   }
+
+  BFT_FREE(rot_m);
 
 #if 0 && defined(DEBUG) && !defined(NDEBUG)
   bft_printf("\n Particle set after %s\n", __func__);
@@ -3207,6 +3244,9 @@ _finalize_displacement(cs_lagr_particle_set_t  *particles,
   bft_printf("\n Particle set after %s\n", __func__);
   cs_lagr_particle_set_dump(particles);
 #endif
+
+  if (cs_turbomachinery_get_model() == CS_TURBOMACHINERY_TRANSIENT)
+    _particle_track_builder = _destroy_track_builder(_particle_track_builder);
 }
 
 /*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
@@ -3247,12 +3287,6 @@ cs_lagr_tracking_initialize(void)
     _cs_mpi_particle_type = _define_particle_datatype(p_set->p_am);
   }
 #endif
-
-  /* Initialize builder */
-
-  _particle_track_builder
-    = _init_track_builder(p_set->n_particles_max,
-                          p_set->p_am->extents);
 }
 
 /*----------------------------------------------------------------------------*/

src/base/cs_parameters_check.c
@@ -2038,16 +2038,6 @@ cs_parameters_check(void)
      module */
   int model = cs_turbomachinery_get_model();
 
-  if (model == CS_TURBOMACHINERY_TRANSIENT) {
-    cs_parameters_is_equal_int(CS_ABORT_DELAYED,
-                               _("transient turbomachinery module not "
-                                 "compatible "
-                                 "with the lagrangian module"),
-                               "cs_glob_lagr_time_scheme->iilagr",
-                               cs_glob_lagr_time_scheme->iilagr,
-                               0);
-  }
-
   if (model > CS_TURBOMACHINERY_NONE) {
     cs_parameters_is_equal_int(CS_ABORT_DELAYED,
                                _("turbomachinery module not compatible "

src/base/cs_turbomachinery.c
@@ -1589,6 +1589,28 @@ cs_turbomachinery_rotation_matrix(int        rotor_num,
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief Return number of rotors.
+ *
+ * Note that the number of associated rotations is n_rotors + 1, as the
+ * first rotation id is reserved for the fixed portion of the domain.
+ *
+ * \return  number of rotors
+ */
+/*----------------------------------------------------------------------------*/
+
+int
+cs_turbomachinery_n_rotors(void)
+{
+  int retval = 1;
+
+  if (_turbomachinery != NULL)
+    retval = _turbomachinery->n_rotors;
+
+  return retval;
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief Return cell rotor number.
  *
  * Each cell may be associated with a given rotor, or rotation, with 0
@@ -1661,6 +1683,38 @@ cs_turbomachinery_set_rotation_retry(int     n_max_join_retries,
 
   tbm->n_max_join_tries = n_max_join_retries;
   tbm->dt_retry = dt_retry_multiplier;
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Build rotation matrices for a given time interval.
+ *
+ * The caller is responsible for freeing the array when not needed.
+ *
+ * \param[in]  dt  associated time delta (0 for current, unmodified time)
+ *
+ * \return  array of rotation matrices.
+ */
+/*----------------------------------------------------------------------------*/
+
+cs_real_34_t *
+cs_turbomachinery_get_rotation_matrices(double dt)
+{
+  const cs_turbomachinery_t *tbm = _turbomachinery;
+  cs_real_34_t  *m;
+
+  BFT_MALLOC(m, tbm->n_rotors+1, cs_real_34_t);
+
+  for (int j = 0; j < tbm->n_rotors+1; j++) {
+    cs_rotation_t *r = tbm->rotation + j;
+
+    cs_rotation_matrix(r->omega*dt,
+                       r->axis,
+                       r->invariant,
+                       m[j]);
+  }
+
+  return m;
 }
 
 /*----------------------------------------------------------------------------*/

src/base/cs_turbomachinery.h
@@ -214,6 +214,19 @@ cs_turbomachinery_rotation_matrix(int        rotor_num,
                                   cs_real_t  matrix[3][4]);
 
 /*----------------------------------------------------------------------------
+ * Return number of rotors.
+ *
+ * Note that the number of associated rotations is n_rotors + 1, as the
+ * first rotation id is reserved for the fixed portion of the domain.
+ *
+ * return:
+ *   number of rotors
+ *----------------------------------------------------------------------------*/
+
+int
+cs_turbomachinery_n_rotors(void);
+
+/*----------------------------------------------------------------------------
  * Return cell rotor number.
  *
  * Each cell may be associated with a given rotor, or rotation, with 0
@@ -249,6 +262,21 @@ cs_turbomachinery_get_rotation_velocity(int  rotor_num);
 void
 cs_turbomachinery_set_rotation_velocity(int     rotor_num,
                                         double  omega);
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Build rotation matrices for a given time interval.
+ *
+ * The caller is responsible for freeing the array when not needed.
+ *
+ * \param[in]  dt  associated time delta (0 for current, unmodified time)
+ *
+ * \return  array of rotation matrices.
+ */
+/*----------------------------------------------------------------------------*/
+
+cs_real_34_t *
+cs_turbomachinery_get_rotation_matrices(double dt);
 
 /*----------------------------------------------------------------------------*/
 /*!

src/base/navstv.f90
@@ -722,7 +722,7 @@ if (iturbo.eq.2 .and. iterns.eq.1) then
 
       ! Resize other arrays, depending on user options
 
-      if (iilagr.gt.0) &
+      if (iilagr.gt.0 .and. ntersl.gt.0) &
         call resize_n_sca_real_arrays(ntersl, tslagr)
 
       if (iphydr.eq.1) then

src/lagr/cs_lagr_particle.c
@@ -1231,7 +1231,6 @@ cs_lagr_particle_set_dump(const cs_lagr_particle_set_t  *particles)
     bft_printf_flush();
 
     for (cs_lnum_t i = 0; i < particles->n_particles; i++) {
-      bft_printf("  dump_particle_set i = %d \n", i);
       _dump_particle(particles, i);
     }
 

commit 150b274427a5c4551c7f6f33e7275a9ca79d00e5

Fix bug in the Lagrangian module introduced by commit c0295b3 (2018-06-15). To simplify the code, mesh adjacencies structure is used instead of the Lagrangian cell->faces connectivity.

src/lagr/cs_lagr_tracking.c
@@ -3567,47 +3567,42 @@ cs_lagr_test_wall_cell(const void                     *particle,
 
   if (cell_num < 0) return;
 
-  cs_lagr_track_builder_t  *builder = _particle_track_builder;
-  cs_lnum_t  *cell_face_idx = builder->cell_face_idx;
-  cs_lnum_t  *cell_face_lst = builder->cell_face_lst;
+  cs_lnum_t  *cell_b_face_idx = cs_glob_mesh_adjacencies->cell_b_faces_idx;
+  cs_lnum_t  *cell_b_faces = cs_glob_mesh_adjacencies->cell_b_faces;
   cs_lnum_t cell_id = cell_num - 1;
 
   *yplus = 10000;
   *face_id = -1;
 
-  cs_lnum_t  start = cell_face_idx[cell_id];
-  cs_lnum_t  end =  cell_face_idx[cell_id + 1];
+  cs_lnum_t  start = cell_b_face_idx[cell_id];
+  cs_lnum_t  end =  cell_b_face_idx[cell_id + 1];
 
   for (cs_lnum_t i = start; i < end; i++) {
-    cs_lnum_t  face_num = cell_face_lst[i];
+    cs_lnum_t f_id = cell_b_faces[i];
 
-    if (face_num < 0) {
+    assert(cs_glob_lagr_boundary_conditions != NULL);
 
-      assert(cs_glob_lagr_boundary_conditions != NULL);
+    const char b_type = cs_glob_lagr_boundary_conditions->elt_type[f_id];
 
-      cs_lnum_t f_id = CS_ABS(face_num) - 1;
-      const char b_type = cs_glob_lagr_boundary_conditions->elt_type[f_id];
+    if (   (b_type == CS_LAGR_DEPO1)
+        || (b_type == CS_LAGR_DEPO2)
+        || (b_type == CS_LAGR_DEPO_DLVO)) {
 
-      if (   (b_type == CS_LAGR_DEPO1)
-          || (b_type == CS_LAGR_DEPO2)
-          || (b_type == CS_LAGR_DEPO_DLVO)) {
+      cs_real_t x_face = cs_glob_lagr_b_u_normal[f_id][0];
+      cs_real_t y_face = cs_glob_lagr_b_u_normal[f_id][1];
+      cs_real_t z_face = cs_glob_lagr_b_u_normal[f_id][2];
 
-        cs_real_t x_face = cs_glob_lagr_b_u_normal[f_id][0];
-        cs_real_t y_face = cs_glob_lagr_b_u_normal[f_id][1];
-        cs_real_t z_face = cs_glob_lagr_b_u_normal[f_id][2];
+      cs_real_t offset_face = cs_glob_lagr_b_u_normal[f_id][3];
+      const cs_real_t  *particle_coord
+        = cs_lagr_particle_attr_const(particle, p_am, CS_LAGR_COORDS);
 
-        cs_real_t offset_face = cs_glob_lagr_b_u_normal[f_id][3];
-        const cs_real_t  *particle_coord
-          = cs_lagr_particle_attr_const(particle, p_am, CS_LAGR_COORDS);
-
-        cs_real_t dist_norm = CS_ABS(  particle_coord[0] * x_face
-                                     + particle_coord[1] * y_face
-                                     + particle_coord[2] * z_face
-                                     + offset_face) / visc_length[f_id];
-        if (dist_norm  < *yplus) {
-          *yplus = dist_norm;
-          *face_id = f_id;
-        }
+      cs_real_t dist_norm = CS_ABS(  particle_coord[0] * x_face
+                                   + particle_coord[1] * y_face
+                                   + particle_coord[2] * z_face
+                                   + offset_face) / visc_length[f_id];
+      if (dist_norm  < *yplus) {
+        *yplus = dist_norm;
+        *face_id = f_id;
       }
     }
 