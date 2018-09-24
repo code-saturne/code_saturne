code-saturne/code_saturne

commit dfdb31147c18238c1f6cab0946242b83a331cd20

Fix bug in Lagrangian module (iternal deposition and parallel computation).
git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@10007 e44c5d27-f830-0410-8ce8-974f7a764be6

src/lagr/cs_lagr_tracking.c
@@ -3474,8 +3474,15 @@ cs_lagr_tracking_particle_movement(const cs_real_t  visc_length[])
     }
   }
 
-  /* Imposed_motion: additional loop */
+  /* Internal deposition: additional loop */
   if (cs_glob_porous_model == 3) {
+    cs_real_t *covered_surface = NULL;
+    BFT_MALLOC(covered_surface, cs_glob_mesh->n_cells_with_ghosts, cs_real_t);
+
+    /* Initialization */
+    for (cs_lnum_t cell_id = 0; cell_id < cs_glob_mesh->n_cells_with_ghosts; cell_id++)
+      covered_surface[cell_id] = 0.;
+
     for (cs_lnum_t face_id = 0; face_id < cs_glob_mesh->n_i_faces ; face_id++) {
       /* Internal face flagged as internal deposition */
       if (cs_glob_lagr_internal_conditions->i_face_zone_id[face_id] >= 0) {
@@ -3483,73 +3490,63 @@ cs_lagr_tracking_particle_movement(const cs_real_t  visc_length[])
           fvq->i_f_face_normal[3*face_id+j] = fvq->i_face_normal[3*face_id+j];
       }
     }
-  }
 
-  if (lagr_model->deposition == 1) {
+    if (lagr_model->deposition == 1) {
 
-    for (cs_lnum_t ip = 0; ip < particles->n_particles; ip++) {
+      for (cs_lnum_t ip = 0; ip < particles->n_particles; ip++) {
 
-      cs_lnum_t cell_num
-        = cs_lagr_particles_get_lnum(particles, ip, CS_LAGR_CELL_NUM);
+        cs_lnum_t cell_num
+          = cs_lagr_particles_get_lnum(particles, ip, CS_LAGR_CELL_NUM);
 
-      if (   cell_num >=0
-          && (   cs_lagr_particles_get_lnum(particles, ip, CS_LAGR_DEPOSITION_FLAG)
+        if (   cell_num >=0
+            && (   cs_lagr_particles_get_lnum(particles, ip, CS_LAGR_DEPOSITION_FLAG)
               == CS_LAGR_PART_IMPOSED_MOTION)) {
 
-        cs_lnum_t cell_id = cell_num - 1;
+          cs_lnum_t cell_id = cell_num - 1;
 
-        /* Loop over internal faces of the current particle faces */
-        for (cs_lnum_t i = _particle_track_builder->cell_face_idx[cell_id];
-             i < _particle_track_builder->cell_face_idx[cell_id+1] ;
-             i++ ) {
+          covered_surface[cell_id] += cs_math_pi * 0.25
+            * pow(cs_lagr_particles_get_real(particles, ip, CS_LAGR_DIAMETER),2.)
+            * cs_lagr_particles_get_real(particles, ip, CS_LAGR_FOULING_INDEX)
+            * cs_lagr_particles_get_real(particles, ip, CS_LAGR_STAT_WEIGHT);
 
-          cs_lnum_t face_num = _particle_track_builder->cell_face_lst[i];
+          //FIXME useless cs_lagr_particles_set_lnum(particles, ip, CS_LAGR_NEIGHBOR_FACE_ID, face_id);
 
-          if (face_num > 0) {
-
-            cs_lnum_t face_id = face_num - 1;
-
-            /* Internal face flagged as internal deposition */
-            if (cs_glob_lagr_internal_conditions->i_face_zone_id[face_id] >= 0) {
-
-              cs_real_t temp = cs_math_pi * 0.25
-                * pow(cs_lagr_particles_get_real(particles, ip, CS_LAGR_DIAMETER),2.)
-                * cs_lagr_particles_get_real(particles, ip, CS_LAGR_FOULING_INDEX)
-                * cs_lagr_particles_get_real(particles, ip, CS_LAGR_STAT_WEIGHT);
-
-              cs_lagr_particles_set_lnum(particles, ip, CS_LAGR_NEIGHBOR_FACE_ID, face_id);
-
-              /* Remove from the particle area from fluid section */
-              if (cs_glob_porous_model == 3) {
-                for (cs_lnum_t id = 0; id < 3; id++)
-                  fvq->i_f_face_normal[3*face_id + id] -= temp
-                    * fvq->i_face_normal[3*face_id + id]
-                    / fvq->i_face_surf[face_id];
-              }
-
-            }
-
-          }
         }
       }
     }
-  }
 
-  /* Clip fluid section to 0 if negative */
-  if (cs_glob_porous_model == 3) {
-    for (cs_lnum_t ifac = 0; ifac < cs_glob_mesh->n_i_faces; ifac++) {
+    /* Synchronization */
+    if (mesh->halo != NULL)
+      cs_halo_sync_var(mesh->halo, CS_HALO_STANDARD, covered_surface);
+
+
+    /* Compute fluid section and clip it to 0 if negative */
+    for (cs_lnum_t face_id = 0; face_id < cs_glob_mesh->n_i_faces; face_id++) {
       cs_real_t temp = 0.;
 
-      /* If S_fluid . S is negative, that means we removed too much surface
-       * to fluid surface */
-      for (int j = 0; j < 3; j++)
-        temp += fvq->i_f_face_normal[3*ifac+j] * fvq->i_face_normal[3*ifac+j];
+      /* Internal face flagged as internal deposition */
+      if (cs_glob_lagr_internal_conditions->i_face_zone_id[face_id] >= 0) {
+        cs_lnum_t cell_id1 = mesh->i_face_cells[face_id][0];;
+        cs_lnum_t cell_id2 = mesh->i_face_cells[face_id][1];
+        /* Remove from the particle area from fluid section */
+        for (cs_lnum_t id = 0; id < 3; id++)
+          fvq->i_f_face_normal[3*face_id + id] -=
+            (covered_surface[cell_id1] + covered_surface[cell_id2])
+            * fvq->i_face_normal[3*face_id + id]
+            / fvq->i_face_surf[face_id];
 
-      if (temp <= 0.) {
-        for (int j = 0; j < 3; j++)
-          fvq->i_f_face_normal[3*ifac+j] = 0.;
+
+        /* If S_fluid . S is negative, that means we removed too much surface
+         * to fluid surface */
+        for (cs_lnum_t j = 0; j < 3; j++)
+          temp += fvq->i_f_face_normal[3*face_id+j] * fvq->i_face_normal[3*face_id+j];
+
+        if (temp <= 0.) {
+          for (cs_lnum_t j = 0; j < 3; j++)
+            fvq->i_f_face_normal[3*face_id+j] = 0.;
+        }
+        fvq->i_f_face_surf[face_id] = cs_math_3_norm(fvq->i_f_face_normal + 3*face_id);
       }
-      fvq->i_f_face_surf[ifac] = cs_math_3_norm(fvq->i_f_face_normal + 3*ifac);
     }
   }
 

commit 8e5bced9901b3858c7d2da88d869813068f77df2

Fix bug in the Lagrangian module for internal deposition and resuspension. Bug introduced in commits dfdb311 (2017-03-25) and 1d65879 (2016-09-13).

src/lagr/cs_lagr_resuspension.c
@@ -173,6 +173,50 @@ cs_lagr_resuspension(void)
     cs_lnum_t flag = cs_lagr_particle_get_lnum(part, p_am, CS_LAGR_DEPOSITION_FLAG);
     cs_real_t diam_mean = cs_glob_lagr_clogging_model->diam_mean;
 
+    /* Treatment of internal deposition and user imposed motion */
+    if (flag == CS_LAGR_PART_IMPOSED_MOTION && face_id > -1) {
+      bft_printf(" imposed motion\n");
+
+      /* Reorient the face so that it is the outwarding normal */
+      int reorient_face = 1;
+      if (iel == mesh->i_face_cells[face_id][1])
+        reorient_face = -1;
+
+      /* Adhesion forces not implemented */
+
+      /* Gravity forces */
+      cs_real_3_t gravity = {cs_glob_physical_constants->gravity[0],
+        cs_glob_physical_constants->gravity[1],
+        cs_glob_physical_constants->gravity[2]};
+
+      cs_real_t fgrav
+        =   p_mass * reorient_face
+        * cs_math_3_dot_product(gravity, i_face_normal[face_id]);
+
+      /* Forces due to pressure difference */
+      cs_lnum_t c_id1 = mesh->i_face_cells[face_id][0];
+      cs_lnum_t c_id2 = mesh->i_face_cells[face_id][1];
+
+      if (iel == c_id2) {
+        c_id2 = c_id1;
+        c_id1 = iel;
+      }
+
+      /* Warning: dynamic pressure, FIXME for iphydr = 1 */
+      cs_real_t press_out = cs_glob_lagr_extra_module->pressure->val[c_id1];
+      cs_real_t press_in = cs_glob_lagr_extra_module->pressure->val[c_id2];
+
+      cs_real_t fpres = (press_out - press_in) * cs_math_pi * pow(p_diam, 2) * 0.25
+        * cs_lagr_particle_get_real(part, p_am, CS_LAGR_FOULING_INDEX);
+
+      /* Resuspension criterion: Fgrav + Fpres < 0 */
+      if ((fgrav + fpres) < 0.) {
+        cs_lagr_particle_set_lnum(part, p_am, CS_LAGR_DEPOSITION_FLAG,
+            CS_LAGR_PART_IN_FLOW);
+        /* TODO: impose particle velocity? Do some stats? */
+      }
+    }
+
     /* Monolayer resuspension model */
     if (face_id > 0 &&
         bound_stat[face_id + n_faces * lag_bi->ihdepm] < diam_mean ) {
@@ -339,48 +383,6 @@ cs_lagr_resuspension(void)
 
         }
 
-      }
-      /* Treatment of internal deposition and user imposed motion */
-      else if (flag == CS_LAGR_PART_IMPOSED_MOTION) {
-
-        /* Reorient the face so that it is the outwarding normal */
-        int reorient_face = 1;
-        if (iel == mesh->i_face_cells[face_id][1])
-          reorient_face = -1;
-
-        /* Adhesion forces not implemented */
-
-        /* Gravity forces */
-        cs_real_3_t gravity = {cs_glob_physical_constants->gravity[0],
-                               cs_glob_physical_constants->gravity[1],
-                               cs_glob_physical_constants->gravity[2]};
-
-        cs_real_t fgrav
-          =   p_mass * reorient_face
-            * cs_math_3_dot_product(gravity, i_face_normal[face_id]);
-
-        /* Forces due to pressure difference */
-        cs_lnum_t c_id1 = mesh->i_face_cells[face_id][0];
-        cs_lnum_t c_id2 = mesh->i_face_cells[face_id][1];
-
-        if (iel == c_id2) {
-          c_id2 = c_id1;
-          c_id1 = iel;
-        }
-
-        /* Warning: dynamic pressure, FIXME for iphydr = 1 */
-        cs_real_t press_out = cs_glob_lagr_extra_module->pressure->val[c_id1];
-        cs_real_t press_in = cs_glob_lagr_extra_module->pressure->val[c_id2];
-
-        cs_real_t fpres = (press_out - press_in) * cs_math_pi * pow(p_diam, 2) * 0.25
-          * cs_lagr_particle_get_real(part, p_am, CS_LAGR_FOULING_INDEX);
-
-        /* Resuspension criterion: Fgrav + Fpres < 0 */
-        if ((fgrav + fpres) < 0.) {
-          cs_lagr_particle_set_lnum(part, p_am, CS_LAGR_DEPOSITION_FLAG,
-                                  CS_LAGR_PART_IN_FLOW);
-          /* TODO: impose particle velocity? Do some stats? */
-        }
       }
     } /* Enf of monolayer resuspension */
     else {

src/lagr/cs_lagr_tracking.c
@@ -1033,15 +1033,11 @@ _internal_treatment(cs_lagr_particle_set_t    *particles,
 
     cs_real_t energ = 0.5 * particle_mass * (uxn+vyn+wzn) * (uxn+vyn+wzn);
 
-    /* Computation of the energy barrier */
+    /* No energy barrier yet for internal deposition */
     cs_real_t  energt = 0.;
 
-    cs_lagr_barrier_pp(particle_diameter,
-                       cur_cell_id,
-                       &energt);
-
      /* Deposition criterion: E_kin > E_barr */
-    if (energ > energt * 0.5 * particle_diameter) {
+    if (energ >= energt * 0.5 * particle_diameter) {
 
       cs_real_t *cell_cen = fvq->cell_cen + (3*cur_cell_id);
       cs_real_3_t vect_cen;
@@ -3447,7 +3443,26 @@ cs_lagr_tracking_particle_movement(const cs_real_t  visc_length[])
             * cs_lagr_particles_get_real(particles, ip, CS_LAGR_FOULING_INDEX)
             * cs_lagr_particles_get_real(particles, ip, CS_LAGR_STAT_WEIGHT);
 
-          //FIXME useless cs_lagr_particles_set_lnum(particles, ip, CS_LAGR_NEIGHBOR_FACE_ID, face_id);
+          /* Loop over internal faces of the current particle faces
+           * NB: usefull for resuspension, the last face_id is stored.
+           * face_id is unique in many cases.
+           * */
+          for (cs_lnum_t i = _particle_track_builder->cell_face_idx[cell_id];
+              i < _particle_track_builder->cell_face_idx[cell_id+1] ;
+              i++ ) {
+
+            cs_lnum_t face_num = _particle_track_builder->cell_face_lst[i];
+
+            if (face_num > 0) {
+
+              cs_lnum_t face_id = face_num - 1;
+
+              /* Internal face flagged as internal deposition */
+              if (cs_glob_lagr_internal_conditions->i_face_zone_id[face_id] >= 0)
+                cs_lagr_particles_set_lnum(particles, ip, CS_LAGR_NEIGHBOR_FACE_ID, face_id);
+
+            }
+          }
 
         }
       }