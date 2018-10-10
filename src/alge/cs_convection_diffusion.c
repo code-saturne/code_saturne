code-saturne/code_saturne

commit 3de3922d9ab792408e8cff1efe6ee3e388bd4a9c

Remove some dijpf (not consistent with potential clippings of II' and JJ').

src/alge/cs_convection_diffusion.c
@@ -3417,8 +3417,6 @@ cs_convection_diffusion_vector(int                         idtvar,
     = (const cs_real_3_t *restrict)fvq->diipf;
   const cs_real_3_t *restrict djjpf
     = (const cs_real_3_t *restrict)fvq->djjpf;
-  const cs_real_3_t *restrict dijpf
-    = (const cs_real_3_t *restrict)fvq->dijpf;
   const cs_real_3_t *restrict diipb
     = (const cs_real_3_t *restrict)fvq->diipb;
 
@@ -4781,17 +4779,22 @@ cs_convection_diffusion_vector(int                         idtvar,
           double grdtrv =      pnd*(grad[ii][0][0]+grad[ii][1][1]+grad[ii][2][2])
                  + (1.-pnd)*(grad[jj][0][0]+grad[jj][1][1]+grad[jj][2][2]);
 
-          double tgrdfl;
-          /* We need to compute trans_grad(u).IJ which is equal to IJ.grad(u) */
+          cs_real_3_t n, ipjp;
+          cs_math_3_normalise(i_face_normal[face_id], n);
 
+          /* I'J' */
+          for (int i = 0; i < 3; i++)
+            ipjp[i] = n[i] * i_dist[face_id];
+
+          /* We need to compute trans_grad(u).I'J' which is equal to I'J'.grad(u) */
           for (int isou = 0; isou < 3; isou++) {
 
-            tgrdfl = dijpf[face_id][0] * (      pnd*grad[ii][0][isou]
-                                         + (1.-pnd)*grad[jj][0][isou])
-                   + dijpf[face_id][1] * (      pnd*grad[ii][1][isou]
-                                         + (1.-pnd)*grad[jj][1][isou])
-                   + dijpf[face_id][2] * (      pnd*grad[ii][2][isou]
-                                         + (1.-pnd)*grad[jj][2][isou]);
+            double tgrdfl = ipjp[0] * (      pnd*grad[ii][0][isou]
+                                      + (1.-pnd)*grad[jj][0][isou])
+                          + ipjp[1] * (      pnd*grad[ii][1][isou]
+                                      + (1.-pnd)*grad[jj][1][isou])
+                          + ipjp[2] * (      pnd*grad[ii][2][isou]
+                                      + (1.-pnd)*grad[jj][2][isou]);
 
             double flux = visco*tgrdfl + secvis*grdtrv*i_f_face_normal[face_id][isou];
 
@@ -7781,8 +7784,6 @@ cs_anisotropic_left_diffusion_vector(int                         idtvar,
   const cs_real_t *restrict weight = fvq->weight;
   const cs_real_3_t *restrict i_f_face_normal
     = (const cs_real_3_t *restrict)fvq->i_f_face_normal;
-  const cs_real_3_t *restrict dijpf
-    = (const cs_real_3_t *restrict)fvq->dijpf;
   const cs_real_3_t *restrict diipf
     = (const cs_real_3_t *restrict)fvq->diipf;
   const cs_real_3_t *restrict djjpf
@@ -8170,18 +8171,25 @@ cs_anisotropic_left_diffusion_vector(int                         idtvar,
             =        pnd*(gradv[ii][0][0]+gradv[ii][1][1]+gradv[ii][2][2])
               + (1.-pnd)*(gradv[jj][0][0]+gradv[jj][1][1]+gradv[jj][2][2]);
 
+          cs_real_3_t n, ipjp;
+          cs_math_3_normalise(i_face_normal[face_id], n);
+
+          /* I'J' */
+          for (int i = 0; i < 3; i++)
+            ipjp[i] = n[i] * i_dist[face_id];
+
           for (int i = 0; i < 3; i++) {
 
             cs_real_t flux = secvis*grdtrv*i_f_face_normal[face_id][i];
 
-            /* We need to compute (K grad(u)^T) .IJ
-               which is equal to IJ . (grad(u) . K^T)
-               But: (IJ . (grad(u) . K^T))_i = IJ_k grad(u)_kj K_ij
-               Warning, in FORTRAN K_ij = K(j, i) */
+            /* We need to compute (K grad(u)^T) .I'J'
+               which is equal to I'J' . (grad(u) . K^T)
+               But: (I'J' . (grad(u) . K^T))_i = I'J'_k grad(u)_kj K_ij
+            */
 
             for (int j = 0; j < 3; j++) {
               for (int k = 0; k < 3; k++) {
-                flux += dijpf[face_id][k]
+                flux += ipjp[k]
                             *(pnd*gradv[ii][k][j]+(1-pnd)*gradv[jj][k][j])
                             *i_visc[face_id][i][j];
               }

src/alge/cs_divergence.c
@@ -1657,8 +1657,6 @@ cs_ext_force_flux(const cs_mesh_t          *m,
     = (const cs_real_3_t *restrict)fvq->b_face_normal;
   const cs_real_3_t *restrict i_face_cog
     = (const cs_real_3_t *restrict)fvq->i_face_cog;
-  const cs_real_3_t *restrict dijpf
-    = (const cs_real_3_t *restrict)fvq->dijpf;
   const cs_real_3_t *restrict diipf
     = (const cs_real_3_t *restrict)fvq->diipf;
   const cs_real_3_t *restrict djjpf

commit cf763cd10af3f080f5b7926df550d03d9b75ec9e

Fix compilation bug introduced in 3de3922 (today)

src/alge/cs_convection_diffusion.c
@@ -7784,6 +7784,9 @@ cs_anisotropic_left_diffusion_vector(int                         idtvar,
   const cs_real_t *restrict weight = fvq->weight;
   const cs_real_3_t *restrict i_f_face_normal
     = (const cs_real_3_t *restrict)fvq->i_f_face_normal;
+  const cs_real_3_t *restrict i_face_normal
+    = (const cs_real_3_t *restrict)fvq->i_face_normal;
+  const cs_real_t *restrict i_dist = fvq->i_dist;
   const cs_real_3_t *restrict diipf
     = (const cs_real_3_t *restrict)fvq->diipf;
   const cs_real_3_t *restrict djjpf