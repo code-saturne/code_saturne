code-saturne/code_saturne

commit a2ddc3bfdd4bfa871c2580d6cc61f93366dd713d

Major commit: change the time stepping to ensure 2nd time order for variable density flow if 2nd time order is activated. It impacts all the second time order time stepping and also all variable densities algorithms.  * If you want to go back to the previous algorithm for variable    density, you can specify ipredfl = 0 in the usipsu    (cs_user_parameters.f90) subroutine.  * The momentum equation is staggered in time, that is to say, when    2nd order is activated, velocity is solved from time n-1/2 to n+1/2.    A special care should be done for time averaged quantities.  * Remove deprecated "thetro" keyword.
Worked done in collaboration with C. Flageul.

src/base/varpos.f90
@@ -405,16 +405,12 @@ endif
 ! or dilatable algorithm
 if (ivofmt.ge.0.or.idilat.gt.1) then
   call field_set_n_previous(icrom, 2)
-  if (iroext.gt.0) then
-    call field_set_n_previous(ibrom, 2)
-  endif
+  call field_set_n_previous(ibrom, 2)
   ! The density at the previous time step is required if
   ! we perform a hydrostatic pressure correction (icalhy=1)
 else if (iroext.gt.0.or.icalhy.eq.1.or.ipthrm.eq.1.or.ippmod(icompf).ge.0) then
   call field_set_n_previous(icrom, 1)
-  if (iroext.gt.0) then
-    call field_set_n_previous(ibrom, 1)
-  endif
+  call field_set_n_previous(ibrom, 1)
 endif
 ! Dans le cas d'une extrapolation de la viscosite totale
 if (iviext.gt.0) then

src/base/covofi.f90
@@ -956,10 +956,14 @@ endif
 
 ! Low Mach compressible algos (conservative in time).
 ! Same algo. for Volume of Fluid method
-if (idilat.gt.1 .or. ivofmt.ge.0) then
-  call field_get_val_prev_s(icrom_scal, pcrom)
+if ((idilat.gt.1 .or. ivofmt.ge.0 .or. ipredfl.eq.0).and.irovar.eq.1) then
+  if (iterns.eq.1) then
+    call field_get_val_prev2_s(icrom_scal, pcrom)
+  else
+    call field_get_val_prev_s(icrom_scal, pcrom)
+  endif
 
-! Standard algo
+! Deprecated algo or constant density
 else
   call field_get_val_s(icrom_scal, pcrom)
 endif

src/base/cs_physical_constants.c
@@ -266,7 +266,7 @@ BEGIN_C_DECLS
         (dimensionless)
   \var  cs_fluid_properties_t::ipthrm
         uniform variable thermodynamic pressure:
-        - 0: false
+        - 0: false (ie not variable)
         - 1: true
   \var  cs_fluid_properties_t::pther
         Thermodynamic pressure for the current time step.

src/base/fldini.f90
@@ -676,6 +676,21 @@ endif
 ! It is then used in tridim to update buoyant scalars and density in U-P loop
 call cs_parameters_set_n_buoyant_scalars
 
+! If density is variable, a particular care must be taken when dealing with
+! density in the unsteady term in the velocity pressure loop
+if (irovar.eq.1) then
+  ! EOS density, imposed after the correction step, so we need
+  ! to keep the previous one, which is in balance with the mass
+  f_name = 'density_mass'
+  itycat = FIELD_PROPERTY
+  ityloc = 1 ! cells
+  call field_create(f_name, itycat, ityloc, idim1, inoprv, f_id)
+  f_name = 'boundary_density_mass'
+  itycat = FIELD_PROPERTY
+  ityloc = 3 ! boundary faces
+  call field_create(f_name, itycat, ityloc, idim1, inoprv, f_id)
+endif
+
 ! Some C mappings
 call cs_field_pointer_map_boundary
 

src/base/impini.f90
@@ -543,7 +543,7 @@ write(nfecra,9900)
 
 ! --- Stokes
 write(nfecra,4114)istmpf,thetfl,     &
-     iroext,thetro,                  &
+     iroext,                         &
      iviext,thetvi,                  &
      icpext,thetcp,                  &
      thetsn,thetst,epsup
@@ -569,8 +569,6 @@ write(nfecra,9900)
 '                ',14x,       ' (0 : explicite',                /,&
 '                ',14x,       ' (1 : n+thetro avec thetro=1/2', /,&
 '                ',14x,       ' (2 : n+thetro avec thetro=1',   /,&
-'       THETRO = ', e14.5,    ' (theta pour masse volumique',   /,&
-'                               ((1+theta)nouveau-theta ancien',/,&
 '       IVIEXT = ',4x,i10,    ' (extrap. viscosite totale',     /,&
 '                ',14x,       ' (0 : explicite',                /,&
 '                ',14x,       ' (1 : n+thetvi avec thetro=1/2', /,&
@@ -609,8 +607,6 @@ write(nfecra,9900)
 '                ',14x,       ' (0: explicit',                  /,&
 '                ',14x,       ' (1: n+thetro with thetro=1/2',  /,&
 '                ',14x,       ' (2: n+thetro with thetro=1',    /,&
-'       THETRO = ', e14.5,    ' (theta for density',            /,&
-'                               ((1+theta).new-theta.old',      /,&
 '       IVIEXT = ',4x,i10,    ' (total viscosity extrapolation',/,&
 '                ',14x,       ' (0: explicit',                  /,&
 '                ',14x,       ' (1: n+thetvi with thetro=1/2',  /,&

src/base/iniini.f90
@@ -441,7 +441,6 @@ thetst = -999.d0
 
 !     Masse volumique
 iroext = -999
-thetro = -999.d0
 initro = 0
 !     Viscosite totale
 iviext = -999

src/base/iniva0.f90
@@ -168,19 +168,25 @@ do iel = 1, ncel
   crom(iel)  = ro0
 enddo
 
-! Note: for VOF or dilatable algorithms, density at twice previous time step is
-! also stored and written here with "current to previous" function
-if (    iroext.gt.0.or.icalhy.eq.1.or.idilat.gt.1 &
-    .or.ivofmt.ge.0.or.ipthrm.eq.1) then
-  call field_current_to_previous(icrom)
-endif
-
 !     Masse volumique aux faces de bord (et au pdt precedent si ordre2)
 do ifac = 1, nfabor
   brom(ifac) = ro0
 enddo
-if (iroext.gt.0.or.ivofmt.ge.0.or.idilat.gt.1) then
-  call field_current_to_previous(ibrom)
+
+! Note: for VOF or dilatable algorithms, density at twice previous time step is
+! also stored and written here with "current to previous" function
+if (ivofmt.ge.0.or.idilat.ge.1.or.ipredfl.eq.0) then
+  call field_current_to_previous(icrom)
+  call field_current_to_previous(icrom)
+  if (iroext.gt.0.or.idilat.gt.1) then
+    call field_current_to_previous(ibrom)
+    call field_current_to_previous(ibrom)
+  endif
+else if (iroext.gt.0.or.icalhy.eq.1.or.ipthrm.eq.1.or.ippmod(icompf).ge.0) then
+  call field_current_to_previous(icrom)
+  if (iroext.gt.0) then
+    call field_current_to_previous(ibrom)
+  endif
 endif
 
 !     Viscosite moleculaire

src/base/modini.f90
@@ -184,16 +184,6 @@ elseif (istmpf.eq.2) then
 endif
 
 !    -- Proprietes physiques
-if (abs(thetro+999.d0).gt.epzero) then
-  write(nfecra,1011) 'IROEXT',iroext,'THETRO'
-  iok = iok + 1
-elseif (iroext.eq.0) then
-  thetro = 0.0d0
-elseif (iroext.eq.1) then
-  thetro = 0.5d0
-elseif (iroext.eq.2) then
-  thetro = 1.d0
-endif
 if (abs(thetvi+999.d0).gt.epzero) then
   write(nfecra,1011) 'IVIEXT',iviext,'THETVI'
   iok = iok + 1

commit 16b85d30318bc768567f3b3ce4f356810d95b760

Fix bug introduced in commit a2ddc3b (2018-04-16): density at the two     previous time step is now needed when variable even for specific     physics.

src/base/addfld.f90
@@ -389,8 +389,6 @@ if ((iturb.eq.30.and.irijec.eq.1).or.              &
 
 endif
 
-
-
 !===============================================================================
 ! 3. Additional postprocessing fields
 !===============================================================================
@@ -457,6 +455,22 @@ if (itempb .ge. 0) then
     call field_get_label(f_id, f_label)
     call field_set_key_str(itempb, keylbl, f_label)
   endif
+endif
+
+!===============================================================================
+! 4. Set number of previous values if needed
+!===============================================================================
+
+! Density at the second previous time step for VOF algorithm
+! or dilatable algorithm
+if (ivofmt.ge.0.or.idilat.gt.1.or.irovar.eq.1) then
+  call field_set_n_previous(icrom, 2)
+  call field_set_n_previous(ibrom, 2)
+  ! The density at the previous time step is required if
+  ! we perform a hydrostatic pressure correction (icalhy=1)
+else if (icalhy.eq.1.or.ipthrm.eq.1.or.ippmod(icompf).ge.0) then
+  call field_set_n_previous(icrom, 1)
+  call field_set_n_previous(ibrom, 1)
 endif
 
 !---

src/base/varpos.f90
@@ -403,17 +403,6 @@ if (idilat.ge.4) then
   call field_set_key_int(id, keyvis, 0)
 endif
 
-! Density at the second previous time step for VOF algorithm
-! or dilatable algorithm
-if (ivofmt.ge.0.or.idilat.gt.1.or.irovar.eq.1) then
-  call field_set_n_previous(icrom, 2)
-  call field_set_n_previous(ibrom, 2)
-  ! The density at the previous time step is required if
-  ! we perform a hydrostatic pressure correction (icalhy=1)
-else if (icalhy.eq.1.or.ipthrm.eq.1.or.ippmod(icompf).ge.0) then
-  call field_set_n_previous(icrom, 1)
-  call field_set_n_previous(ibrom, 1)
-endif
 ! Dans le cas d'une extrapolation de la viscosite totale
 if (iviext.gt.0) then
   call field_set_n_previous(iviscl, 1)