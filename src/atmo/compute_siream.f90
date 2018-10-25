code-saturne/code_saturne

commit 72cd1ef414a658519898f0928f16c6028ec4778e

Remove use of rtp and rtpa in atmo.
git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@6677 e44c5d27-f830-0410-8ce8-974f7a764be6

src/atmo/compute_siream.f90
@@ -31,12 +31,11 @@
 !   mode          name          role
 !------------------------------------------------------------------------------
 !> \param[in]     dt             time step (per cell)
-!> \param[in,out] rtp            calculated variables at cell centers at current time step
 !> \param[in]     propce         physical properties at cell centers
 !______________________________________________________________________________
 
 subroutine compute_siream &
- ( dt     , rtp,   propce)
+ ( dt     , propce)
 
 !===============================================================================
 ! Module files
@@ -51,6 +50,7 @@ use cstphy
 use cstnum
 use parall
 use period
+use pointe
 use ppppar
 use ppthch
 use ppincl
@@ -68,12 +68,12 @@ implicit none
 
 ! Arguments
 
-double precision dt(ncelet), rtp(ncelet,nflown:nvar)
+double precision dt(ncelet)
 double precision propce(ncelet,*)
 
 ! Local Variables
 
-integer ii, jb, jsp, iel
+integer ii, jb, jsp, iel, index
 double precision dlconc(nespg_siream)
 double precision dlconc_aer(nbin_aer,nesp_aer)
 double precision dlnum_aer(nbin_aer)
@@ -81,6 +81,10 @@ double precision dltemp, dlpress, dlhumid, dldens
 double precision zent                               ! Z coordinate of a cell
 
 double precision, dimension(:), pointer :: crom
+type(pmapper_double_r1), dimension(:), allocatable :: cvar_espg
+type(pmapper_double_r1), dimension(:), allocatable :: cvar_aer
+type(pmapper_double_r1), dimension(:), allocatable :: cvar_naer
+double precision, dimension(:), pointer :: cvar_totwt
 
 ! Initialisation
 
@@ -89,9 +93,31 @@ dldens = ro0
 dlpress = dldens*rair*dltemp ! ideal gas law
 dlhumid = 0.0d0
 
+allocate(cvar_espg(nespg_siream))
+allocate(cvar_espg(nespg_siream+nbin_aer+(nesp_aer-1)*nbin_aer))
+allocate(cvar_espg(nespg_siream+nesp_aer*nbin_aer+nbin_aer))
+
 ! Get Physical quantities
 ! Densisty
 call field_get_val_s(icrom, crom)
+
+if (ippmod(iatmos).ge.2) call field_get_val_s(ivarfl(isca(itotwt)), cvar_totwt)
+
+! Arrays of pointers containing the fields values for each species
+! (loop on cells outside loop on species)
+do jsp = 1, nespg_siream
+  call field_get_val_s(ivarfl(isca(jsp)), cvar_espg(jsp)%p)
+enddo
+do jb = 1, nbin_aer
+  do jsp = 1, nesp_aer
+    index = nespg_siream+jb+(jsp-1)*nbin_aer
+    call field_get_val_s(ivarfl(isca(index)), cvar_aer(index)%p)
+  enddo
+enddo
+do jb = 1, nbin_aer
+  index = nespg_siream+nesp_aer*nbin_aer+jb
+  call field_get_val_s(ivarfl(isca(index)), cvar_naer(index)%p)
+enddo
 
 ! Clipping before calling siream
 
@@ -103,21 +129,20 @@ enddo
 do iel = 1, ncel
 
   zent = xyzcen(3,iel)
-  ! Verifier l'ordre dans lequel on rentre rtp, dlconc et dlconc_aer
 
   ! Filling working arrays
   do jsp = 1, nespg_siream
-    dlconc(jsp) = rtp(iel,isca(jsp))
+    dlconc(jsp) = cvar_espg(jsp)%p(iel)
   enddo
 
   do jb = 1, nbin_aer
     do jsp = 1, nesp_aer
-      dlconc_aer(jb,jsp) = rtp(iel,isca(nespg_siream+jb+(jsp-1)*nbin_aer))
+      dlconc_aer(jb,jsp) = cvar_aer(nespg_siream+jb+(jsp-1)*nbin_aer)%p(iel)
     enddo
   enddo
 
   do jb = 1, nbin_aer
-    dlnum_aer(jb) = rtp(iel,isca(nespg_siream+nesp_aer*nbin_aer+jb))
+    dlnum_aer(jb) = cvar_naer(nespg_siream+nesp_aer*nbin_aer+jb)%p(iel)
   enddo
 
   ! Temperature and density
@@ -147,7 +172,7 @@ do iel = 1, ncel
   ! Specific hymidity
   ! Humid atmosphere
   if (ippmod(iatmos).ge.2) then
-    dlhumid = (rtp(iel,isca(itotwt))-propce(iel,ipproc(iliqwt)))   &
+    dlhumid = (cvar_totwt(iel)-propce(iel,ipproc(iliqwt)))   &
               /(1.d0-propce(iel,ipproc(iliqwt)))
 
   ! Constant density or dry atmosphere with a meteo file
@@ -184,22 +209,24 @@ do iel = 1, ncel
        couples_coag, first_index_coag, second_index_coag,               &
        coefficient_coag, dlconc_aer, dlnum_aer)
 
-  ! Updating rtp
+  ! Updating arrays
   do jsp = 1, nespg_siream
-    rtp(iel,isca(jsp)) = dlconc(jsp)
+    cvar_espg(jsp)%p(iel) = dlconc(jsp)
   enddo
 
   do jb = 1, nbin_aer
     do jsp = 1, nesp_aer
-      rtp(iel,isca(nespg_siream+jb+(jsp-1)*nbin_aer)) = dlconc_aer(jb,jsp)
+      cvar_aer(nespg_siream+jb+(jsp-1)*nbin_aer)%p(iel) = dlconc_aer(jb,jsp)
     enddo
   enddo
 
   do jb = 1, nbin_aer
-    rtp(iel,isca(nespg_siream+nesp_aer*nbin_aer+jb)) = dlnum_aer(jb)
+    cvar_naer(nespg_siream+nesp_aer*nbin_aer+jb)%p(iel) = dlnum_aer(jb)
   enddo
 
 enddo ! Loop on cells
+
+deallocate(cvar_espg, cvar_aer, cvar_naer)
 
 ! Clipping after calling Siream
 

src/atmo/atiniv.f90
@@ -24,7 +24,7 @@ subroutine atiniv &
 !================
 
  ( nvar   , nscal  ,                                              &
-   dt     , rtp    )
+   dt     )
 
 !===============================================================================
 ! FONCTION :
@@ -59,8 +59,6 @@ subroutine atiniv &
 ! nvar             ! i  ! <-- ! total number of variables                      !
 ! nscal            ! i  ! <-- ! total number of scalars                        !
 ! dt(ncelet)       ! tr ! <-- ! valeur du pas de temps                         !
-! rtp              ! tr ! <-- ! variables de calcul au centre des              !
-! (ncelet,*)       !    !     !    cellules                                    !
 !__________________!____!_____!________________________________________________!
 
 !     TYPE : E (ENTIER), R (REEL), A (ALPHANUMERIQUE), T (TABLEAU)
@@ -94,7 +92,7 @@ implicit none
 
 integer          nvar   , nscal
 
-double precision dt(ncelet), rtp(ncelet,nflown:nvar)
+double precision dt(ncelet)
 
 ! Local variables
 
@@ -110,6 +108,8 @@ double precision, dimension(:), pointer :: cvar_k, cvar_ep, cvar_phi
 double precision, dimension(:), pointer :: cvar_fb, cvar_omg, cvar_nusa
 double precision, dimension(:), pointer :: cvar_r11, cvar_r22, cvar_r33
 double precision, dimension(:), pointer :: cvar_r12, cvar_r13, cvar_r23
+double precision, dimension(:), pointer :: cvar_despgi, cvar_sc
+double precision, dimension(:), pointer :: cvar_scalt, cvar_totwt, cvar_ntdrp
 
 !===============================================================================
 
@@ -187,17 +187,17 @@ if (ifilechemistry.ge.1) then
   ! Volume initilization with profiles for species present
   ! in the chemical profiles file
   if (init_at_chem.eq.1) then
-    do iel = 1, ncel
+    do k = 1, nespgi
+      call field_get_val_s(ivarfl(isca(idespgi(k))), cvar_despgi)
 
-      zent = xyzcen(3,iel)
-
-      do k = 1, nespgi
+      do iel = 1, ncel
+        zent = xyzcen(3,iel)
         call intprf                                                         &
         !==========
         (nbchmz, nbchim,                                                    &
          zproc, tchem, espnum(1+(k-1)*nbchim*nbchmz), zent  , ttcabs, xcent )
         ! The first nespg user scalars are supposed to be chemical species
-        rtp(iel,isca(idespgi(k))) = xcent
+        cvar_despgi(iel) = xcent
       enddo
 
     enddo
@@ -212,10 +212,11 @@ if (iaerosol.eq.1) then
 
   ! Initialization
   if (init_at_chem.eq.1) then
-    do iel = 1, ncel
-      do ii = 1, nesp_aer*nbin_aer + nbin_aer
-        isc = (isca_chem(1) - 1) + nespg_siream + ii
-        rtp(iel,isca(isc)) = dlconc0(ii)
+    do ii = 1, nesp_aer*nbin_aer + nbin_aer
+      isc = (isca_chem(1) - 1) + nespg_siream + ii
+      call field_get_val_s(ivarfl(isca(isc)), cvar_sc)
+      do iel = 1, ncel
+        cvar_sc(iel) = dlconc0(ii)
       enddo
     enddo
   endif
@@ -244,21 +245,30 @@ endif
 if (isuite.eq.0) then
 
   if (initmeteo.eq.1) then
+
+    if (ippmod(iatmos).eq.1) then
+      call field_get_val_s(ivarfl(isca(iscalt)), cvar_scalt)
+    else if (ippmod(iatmos).eq.2) then
+      call field_get_val_s(ivarfl(isca(iscalt)), cvar_scalt)
+      call field_get_val_s(ivarfl(isca(itotwt)), cvar_totwt)
+      call field_get_val_s(ivarfl(isca(intdrp)), cvar_ntdrp)
+    endif
+
     if (imeteo.eq.0) then
 
       if (ippmod(iatmos).eq.1) then
         ! The thermal scalar is potential temperature
         do iel = 1, ncel
-          rtp(iel,isca(iscalt)) = t0
+          cvar_scalt(iel) = t0
         enddo
       endif
 
       if (ippmod(iatmos).eq.2) then
         ! The thermal scalar is liquid potential temperature
         do iel = 1, ncel
-          rtp(iel,isca(iscalt)) = t0
-          rtp(iel,isca(itotwt)) = 0.d0
-          rtp(iel,isca(intdrp)) = 0.d0
+          cvar_scalt(iel) = t0
+          cvar_totwt(iel) = 0.d0
+          cvar_ntdrp(iel) = 0.d0
         enddo
       endif
 
@@ -335,7 +345,7 @@ if (isuite.eq.0) then
          (nbmett, nbmetm,                                               &
           ztmet, tmmet, tpmet, zent  , ttcabs, tpent )
 
-            rtp(iel,isca(iscalt)) = tpent
+            cvar_scalt(iel) = tpent
         endif
 
         if (ippmod(iatmos).eq.2) then
@@ -344,19 +354,19 @@ if (isuite.eq.0) then
             !==========
          (nbmett, nbmetm,                                               &
           ztmet, tmmet, tpmet, zent  , ttcabs, tpent )
-            rtp(iel,isca(iscalt)) = tpent
+            cvar_scalt(iel) = tpent
 
             call intprf                                                 &
             !==========
          (nbmett, nbmetm,                                               &
           ztmet, tmmet, qvmet, zent  , ttcabs, qvent )
-            rtp(iel,isca(itotwt)) = qvent
+            cvar_totwt(iel) = qvent
 
             call intprf                                                 &
             !==========
          (nbmett, nbmetm,                                               &
           ztmet, tmmet, ncmet, zent  , ttcabs, ncent )
-            rtp(iel,isca(intdrp)) = ncent
+            cvar_ntdrp(iel) = ncent
         endif
 
       enddo

src/atmo/atphyv.f90
@@ -23,8 +23,7 @@
 subroutine atphyv &
 !================
 
-   ( rtp    ,                                                       &
-     propce )
+   ( propce )
 
 !===============================================================================
 ! FONCTION :
@@ -82,8 +81,6 @@ subroutine atphyv &
 !__________________.____._____.________________________________________________.
 ! name             !type!mode ! role                                           !
 !__________________!____!_____!________________________________________________!
-! rtp              ! ra ! <-- ! calculated variables at cell centers           !
-!  (ncelet, *)     !    !     !  (at current and previous time steps)          !
 ! propce(ncelet, *)! ra ! <-- ! physical properties at cell centers            !
 !__________________!____!_____!________________________________________________!
 
@@ -121,7 +118,6 @@ implicit none
 
 ! Arguments
 
-double precision rtp(ncelet,nflown:nvar)
 double precision propce(ncelet,*)
 
 ! Local variables
@@ -133,7 +129,10 @@ double precision xrtp, rhum, rscp, pp, zent
 double precision lrhum, lrscp
 double precision qsl, deltaq
 double precision qliq, qwt, tliq, dum
+
 double precision, dimension(:), pointer :: brom, crom
+double precision, dimension(:), pointer :: cvar_vart, cvar_totwt
+
 logical activate
 
 ! External function
@@ -183,6 +182,9 @@ endif
 call field_get_val_s(icrom, crom)
 call field_get_val_s(ibrom, brom)
 
+call field_get_val_s(ivarfl(ivart), cvar_vart)
+if (ippmod(iatmos).ge.2) call field_get_val_s(ivarfl(isca(itotwt)), cvar_totwt)
+
 ipctem = ipproc(itempc)
 
 ! From potential temperature, compute:
@@ -200,12 +202,12 @@ lrscp = rair/cp0
 
 do iel = 1, ncel
 
-  xrtp = rtp(iel,ivart) !  The thermal scalar is potential temperature
+  xrtp = cvar_vart(iel) !  The thermal scalar is potential temperature
 
   if (ippmod(iatmos).ge.2) then  ! humid atmosphere
-    lrhum = rair*(1.d0 + (rvsra - 1.d0)*rtp(iel, isca(itotwt)))
+    lrhum = rair*(1.d0 + (rvsra - 1.d0)*cvar_totwt(iel))
     lrscp = (rair/cp0)*(1.d0 + (rvsra - cpvcpa)*                    &
-            rtp(iel,isca(itotwt)))
+            cvar_totwt(iel))
   endif
 
   zent = xyzcen(3,iel)
@@ -304,9 +306,9 @@ do iel = 1, ncel
          ztmet , tmmet , phmet , zent, ttcabs, pp )
   endif
 
-  xrtp = rtp(iel,ivart) ! thermal scalar: liquid potential temperature
+  xrtp = cvar_vart(iel) ! thermal scalar: liquid potential temperature
   tliq = xrtp*(pp/ps)**rscp ! liquid temperature
-  qwt  = rtp(iel, isca(itotwt)) !total water content
+  qwt  = cvar_totwt(iel) !total water content
   qsl = qsatliq(tliq, pp) ! saturated vapor content
   deltaq = qwt - qsl
 
@@ -408,9 +410,9 @@ do iel = 1, ncel
     call intprf(nbmett, nbmetm, ztmet , tmmet , phmet , zent, ttcabs, pp)
   endif
 
-  xrtp = rtp(iel,ivart) ! thermal scalar: liquid potential temperature
+  xrtp = cvar_vart(iel) ! thermal scalar: liquid potential temperature
   tliq = xrtp*(pp/ps)**rscp ! liquid temperature
-  qwt  = rtp(iel, isca(itotwt)) ! total water content
+  qwt  = cvar_totwt(iel) ! total water content
   qsl = qsatliq(tliq, pp) ! saturated vapor content
   deltaq = qwt - qsl
   alpha = (clatev*qsl/(rvap*tliq**2))*(pp/ps)**rscp

src/atmo/atr1vf.f90
@@ -22,7 +22,7 @@
 
 subroutine atr1vf &
 !================
-     ( rtpa, propce )
+     ( propce )
 
 !===============================================================================
 !  Purpose:
@@ -39,8 +39,6 @@ subroutine atr1vf &
 !__________________.____._____.________________________________________________.
 ! name             !type!mode ! role                                           !
 !__________________!____!_____!________________________________________________!
-! rtpa             ! ra ! <-- ! calculated variables at cell centers           !
-!  (ncelet, *)     !    !     !  (preceding time step)                         !
 ! propce           ! ra ! <-- ! properties cell centers                        !
 !  (ncelet, *)     !    !     !  (current time step)                           !
 !__________________!____!_____!________________________________________________!
@@ -78,7 +76,6 @@ implicit none
 ! Arguments
 
 double precision propce(ncelet,*)
-double precision rtpa(ncelet,nflown:nvar)
 
 ! Local variables
 integer k, ii, jj
@@ -98,6 +95,7 @@ double precision, allocatable :: zproj(:), ttvert(:), qvvert(:), romvert(:)
 double precision, allocatable :: aeroso(:)
 double precision, allocatable :: coords(:,:,:), infrad(:)
 double precision, dimension(:), pointer :: crom
+double precision, dimension(:), pointer :: cvara_totwt
 
 save ideb
 data ideb/0/
@@ -148,6 +146,7 @@ if (mod(ntcabs,nfatr1).eq.0.or.ideb.eq.0) then
   enddo
 
   call field_get_val_s(icrom, crom)
+  call field_get_val_prev_s(ivarfl(isca(itotwt)), cvara_totwt)
 
   !===============================================================================
   ! 2.  Computing long-wave and short-wave radiative fluxes
@@ -173,7 +172,7 @@ if (mod(ntcabs,nfatr1).eq.0.or.ideb.eq.0) then
 
   call gripol(igrid, propce(:,ipproc(itempc)), ttvert)
   !===========
-  call gripol(igrid, rtpa(:,isca(itotwt)), qvvert)
+  call gripol(igrid, cvara_totwt, qvvert)
   !===========
   call gripol(igrid, crom, romvert)
   !===========

src/atmo/attssc.f90
@@ -24,7 +24,7 @@ subroutine attssc &
 !================
 
    ( iscal  ,                                                       &
-     rtpa   , rtp    , propce ,                                     &
+     propce ,                                                       &
      crvexp )
 
 !===============================================================================
@@ -45,8 +45,6 @@ subroutine attssc &
 ! name             !type!mode ! role                                           !
 !__________________!____!_____!________________________________________________!
 ! iscal            ! i  ! <-- ! scalar number                                  !
-! rtp, rtpa        ! ra ! <-- ! calculated variables at cell centers           !
-!  (ncelet, *)     !    !     !  (at current and previous time steps)          !
 ! propce(ncelet, *)! ra ! <-- ! physical properties at cell centers            !
 ! smacel           ! tr ! <-- ! valeur des variables associee a la             !
 ! (ncesmp,*   )    !    !     !  source de masse                               !
@@ -83,7 +81,6 @@ implicit none
 
 integer          iscal
 
-double precision rtp(ncelet,nflown:nvar), rtpa(ncelet,nflown:nvar)
 double precision propce(ncelet,*)
 double precision crvexp(ncelet)
 
@@ -105,7 +102,8 @@ integer, save :: treated_scalars = 0
 
 double precision, dimension(:), allocatable :: pphy
 double precision, dimension(:), allocatable :: refrad
-double precision, dimension(:), pointer ::  crom
+double precision, dimension(:), pointer :: crom
+double precision, dimension(:), pointer :: cvar_scapp3
 double precision, dimension(:,:), pointer :: vel
 
 !===============================================================================
@@ -143,7 +141,7 @@ if (ippmod(iatmos).ge.1.and.iatra1.ge.1) then
     ! --- Computes the divergence of the ir and solar radiative fluxes :
 
     ! --- Cressman interpolation of the 1D radiative fluxes on the 3D mesh:
-    call atr1vf(rtpa, propce )
+    call atr1vf(propce)
 
     call mscrss   &
          (idrayi, 1, ray3Di)
@@ -183,6 +181,8 @@ if ( ippmod(iatmos).eq.2.and.modsedi.eq.1 ) then ! for humid atmosphere physics 
 
     if (.not.r3_is_defined)then
 
+      call field_get_val_s(ivarfl(isca(iscapp(3))), cvar_scapp3)
+
       ! First : diagnose the droplet number
 
       if(modnuc.gt.0)then
@@ -212,7 +212,7 @@ if ( ippmod(iatmos).eq.2.and.modsedi.eq.1 ) then ! for humid atmosphere physics 
         enddo
 
         call nuclea (                                                 &
-             rtp(1,isca(iscapp(3))),                                  &
+             cvar_scapp3,                                             &
              vel,                                                     &
              crom,                                                    &
              propce(1,ipproc(itempc)),                                &
@@ -316,7 +316,7 @@ r3max = 0.d0
 do iel = 1, ncel
   rho = crom(iel)
   qliq = propce(iel,ipproc(iliqwt))
-  nc = rtp(iel,isca(iscapp(3)))
+  nc = cvar_scapp3(iel)
   if(qliq.ge.1e-8)then
     nc = max(nc,1.d0)
     r3(iel) = ((rho*qliq)/(rho_water*nc*conversion))**(1.d0/3.d0)
@@ -479,8 +479,8 @@ allocate(local_field(ncelet))
 ! --------------------------------------------------------
 
 do iel = 1, ncel
-  local_field(iel) = rtp(iel,isca(iscapp(3))) & ! number of droplets 1/cm**3
-       *vit_sed( r3(iel) )                    & ! deposition velocity m/s
+  local_field(iel) = cvar_scapp3(iel)   & ! number of droplets 1/cm**3
+       *vit_sed( r3(iel) )              & ! deposition velocity m/s
        *exp(-sigc**2)                     ! coefficient coming from log-normal
                                           ! law of the droplet spectrum
 enddo

src/atmo/chem_source_terms.f90
@@ -36,14 +36,12 @@
 !  mode           name          role                                           !
 !______________________________________________________________________________!
 !> \param[in]     iscal         scalar number
-!> \param[in]     rtpa          calculated variables at cell centers
-!>                               (preceding time steps)
 !> \param[out]    crvexp        explicit part of the source term
 !> \param[out]    crvimp        implicit part of the source term
 !_______________________________________________________________________________
 
 subroutine chem_source_terms &
- ( iscal  , rtpa   ,                                      &
+ ( iscal  ,                  &
    crvexp , crvimp)
 
 
@@ -72,7 +70,6 @@ implicit none
 ! Arguments
 
 integer          iscal
-double precision rtpa(ncelet,nflown:nvar)
 double precision crvexp(ncelet), crvimp(ncelet)
 
 ! Local variables
@@ -85,10 +82,19 @@ double precision rom
 double precision conv_factor(nespg) ! conversion factors for reaction rates
 
 double precision, dimension(:), pointer :: crom
+type(pmapper_double_r1), dimension(:), allocatable :: cvara_espg
 
 !===============================================================================
 
+allocate(cvara_espg(nespg))
+
 call field_get_val_s(icrom, crom)
+
+! Arrays of pointers containing the fields values for each species
+! (loop on cells outside loop on species)
+do ii = 1, nespg
+  call field_get_val_prev_s(ivarfl(isca(ii)), cvara_espg(ii)%p)
+enddo
 
 do iel = 1, ncel
 
@@ -102,7 +108,7 @@ do iel = 1, ncel
 
   ! Filling working arrays
   do ii = 1, nespg
-    dlconc(chempoint(ii)) = rtpa(iel,isca(ii))  ! rtpa
+    dlconc(chempoint(ii)) = cvara_espg(ii)%p(iel)
     conv_factor(chempoint(ii)) = rom*navo*(1.0d-12)/dmmk(ii)
     source(ii) = 0.0d0
   enddo
@@ -128,6 +134,8 @@ do iel = 1, ncel
   crvexp(iel) = crvexp(iel)+dchema(chempoint(iscal))*rom*volume(iel)
 
 enddo
+
+deallocate(cvara_espg)
 
 !--------
 ! FORMATS

src/atmo/compute_gaseous_chemistry.f90
@@ -35,13 +35,10 @@
 !  mode           name          role                                           !
 !______________________________________________________________________________!
 !> \param[in]     dt            time step (per cell)
-!> \param[in]     rtpa          calculated variables at cell centers
-!>                               (preceding time steps)
-!> \param[in,out] rtp           calculated variables at cell centers
 !_______________________________________________________________________________
 
 subroutine compute_gaseous_chemistry &
-( dt     , rtpa   , rtp    )
+( dt )
 
 !===============================================================================
 ! Module files
@@ -56,6 +53,7 @@ use cstphy
 use cstnum
 use parall
 use period
+use pointe
 use ppppar
 use ppthch
 use ppincl
@@ -72,7 +70,7 @@ implicit none
 
 ! Arguments
 
-double precision dt(ncelet), rtp(ncelet,nflown:nvar), rtpa(ncelet,nflown:nvar)
+double precision dt(ncelet)
 
 ! Local Variables
 
@@ -90,10 +88,20 @@ integer ncycle
 double precision dtrest
 
 double precision, dimension(:), pointer :: crom
+type(pmapper_double_r1), dimension(:), allocatable :: cvar_espg, cvara_espg
 
 !===============================================================================
 
+allocate(cvar_espg(nespg), cvara_espg(nespg))
+
 call field_get_val_s(icrom, crom)
+
+! Arrays of pointers containing the fields values for each species
+! (loop on cells outside loop on species)
+do ii = 1, nespg
+  call field_get_val_s(ivarfl(isca(ii)), cvar_espg(ii)%p)
+  call field_get_val_prev_s(ivarfl(isca(ii)), cvara_espg(ii)%p)
+enddo
 
 do iel = 1, ncel
 
@@ -118,9 +126,9 @@ do iel = 1, ncel
     ! -- splitted Rosenbrock solver
     ! -----------------------------
 
-    ! Filling working array dlconc with rtp
+    ! Filling working array dlconc with values at current time step
     do ii = 1, nespg
-      dlconc(chempoint(ii)) = rtp(iel,isca(ii))
+      dlconc(chempoint(ii)) = cvar_espg(ii)%p(iel)
     enddo
 
   else
@@ -128,9 +136,9 @@ do iel = 1, ncel
     ! -- semi-coupled Rosenbrock solver
     ! -----------------------------
 
-    ! Filling working array dlconc with rtpa
+    ! Filling working array dlconc with values at previous time step
     do ii = 1, nespg
-      dlconc(chempoint(ii)) = rtpa(iel,isca(ii))
+      dlconc(chempoint(ii)) = cvara_espg(ii)%p(iel)
     enddo
 
     ! Computation of C(Xn)
@@ -152,7 +160,7 @@ do iel = 1, ncel
     ! (X*-Xn)/dt(dynamics) - C(Xn). See usatch.f90
     ! The first nespg user scalars are supposed to be chemical species
     do ii = 1, nespg
-      source(chempoint(ii)) = (rtp(iel,isca(ii))-rtpa(iel,isca(ii)))/dtc  &
+      source(chempoint(ii)) = (cvar_espg(ii)%p(iel)-cvara_espg(ii)%p(iel))/dtc  &
                             - dchema(chempoint(ii))
     enddo
 
@@ -172,12 +180,14 @@ do iel = 1, ncel
     call roschem (dlconc,source,source,conv_factor,dtrest,rk,rk)
   endif
 
-  ! Update of rtp
+  ! Update of values at current time step
   do ii = 1, nespg
-    rtp(iel,isca(ii)) = dlconc(chempoint(ii))
+    cvar_espg(ii)%p(iel) = dlconc(chempoint(ii))
   enddo
 
 enddo
+
+deallocate(cvar_espg, cvara_espg)
 
 ! TODO: clipping or not ?
 ! Clipping

src/atmo/kinrates.f90
@@ -32,12 +32,11 @@
 !______________________________________________________________________________.
 !  mode           name          role                                           !
 !______________________________________________________________________________!
-!> \param[in]     rtp           calculated variables at cell centers
 !> \param[in]     propce        physical properties at cell centers
 !_______________________________________________________________________________
 
 subroutine kinrates &
- (rtp    , propce)
+ (propce)
 
 !===============================================================================
 ! Module files
@@ -67,7 +66,6 @@ implicit none
 
 ! Arguments
 
-double precision rtp(ncelet,nflown:nvar)
 double precision propce(ncelet,*)
 
 ! Local variables
@@ -84,7 +82,8 @@ double precision heurtu              ! yime (UTC)
 double precision albe                ! albedo, useless here
 double precision fo                  ! solar constant, useless here
 
-double precision, dimension(:), pointer ::  crom
+double precision, dimension(:), pointer :: crom
+double precision, dimension(:), pointer :: cvar_totwt
 
 ! Initialisation
 
@@ -94,6 +93,7 @@ press = dens*rair*temp ! ideal gas law
 hspec = 0.0d0
 
 if (ippmod(iatmos).ge.1) call field_get_val_s(icrom, crom)
+if (ippmod(iatmos).ge.2) call field_get_val_s(ivarfl(isca(itotwt)), cvar_totwt)
 
 ! Computation of kinetic rates in every cell
 
@@ -139,7 +139,7 @@ do iel = 1, ncel
   ! Specific hymidity
   ! Humid atmosphere
   if (ippmod(iatmos).ge.2) then
-    hspec = (rtp(iel,isca(itotwt))-propce(iel,ipproc(iliqwt)))    &
+    hspec = (cvar_totwt(iel)-propce(iel,ipproc(iliqwt)))    &
           / (1.d0-propce(iel,ipproc(iliqwt)))
 
   ! Constant density or dry atmosphere with a meteo file

commit 0102362750f07ceff3a556c7b77828a75c52e273

Fix bug introduced in commit 72cd1ef (2014-10-20, allocation of pmappers in atmo. aerosols model).

src/atmo/compute_siream.f90
@@ -90,8 +90,8 @@ dlpress = dldens*rair*dltemp ! ideal gas law
 dlhumid = 0.0d0
 
 allocate(cvar_espg(nespg_siream))
-allocate(cvar_espg(nespg_siream+nbin_aer+(nesp_aer-1)*nbin_aer))
-allocate(cvar_espg(nespg_siream+nesp_aer*nbin_aer+nbin_aer))
+allocate(cvar_aer(nesp_aer*nbin_aer))
+allocate(cvar_naer(nbin_aer))
 
 ! Get Physical quantities
 ! Densisty
@@ -112,12 +112,12 @@ enddo
 do jb = 1, nbin_aer
   do jsp = 1, nesp_aer
     index = nespg_siream+jb+(jsp-1)*nbin_aer
-    call field_get_val_s(ivarfl(isca(index)), cvar_aer(index)%p)
+    call field_get_val_s(ivarfl(isca(index)), cvar_aer(jb+(jsp-1)*nbin_aer)%p)
   enddo
 enddo
 do jb = 1, nbin_aer
   index = nespg_siream+nesp_aer*nbin_aer+jb
-  call field_get_val_s(ivarfl(isca(index)), cvar_naer(index)%p)
+  call field_get_val_s(ivarfl(isca(index)), cvar_naer(jb)%p)
 enddo
 
 ! Clipping before calling siream
@@ -138,12 +138,12 @@ do iel = 1, ncel
 
   do jb = 1, nbin_aer
     do jsp = 1, nesp_aer
-      dlconc_aer(jb,jsp) = cvar_aer(nespg_siream+jb+(jsp-1)*nbin_aer)%p(iel)
+      dlconc_aer(jb,jsp) = cvar_aer(jb+(jsp-1)*nbin_aer)%p(iel)
     enddo
   enddo
 
   do jb = 1, nbin_aer
-    dlnum_aer(jb) = cvar_naer(nespg_siream+nesp_aer*nbin_aer+jb)%p(iel)
+    dlnum_aer(jb) = cvar_naer(jb)%p(iel)
   enddo
 
   ! Temperature and density
@@ -217,12 +217,12 @@ do iel = 1, ncel
 
   do jb = 1, nbin_aer
     do jsp = 1, nesp_aer
-      cvar_aer(nespg_siream+jb+(jsp-1)*nbin_aer)%p(iel) = dlconc_aer(jb,jsp)
+      cvar_aer(jb+(jsp-1)*nbin_aer)%p(iel) = dlconc_aer(jb,jsp)
     enddo
   enddo
 
   do jb = 1, nbin_aer
-    cvar_naer(nespg_siream+nesp_aer*nbin_aer+jb)%p(iel) = dlnum_aer(jb)
+    cvar_naer(jb)%p(iel) = dlnum_aer(jb)
   enddo
 
 enddo ! Loop on cells