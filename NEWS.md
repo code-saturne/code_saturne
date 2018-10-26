f87271a0c05fa4b9a894d439b9bc28b294b3100e
Erwan Le Coupanec <erwan.lecoupanec@edf.fr>
2018-10-26 19:12:59 +0200

    Fix parallel max. in sedimentation source term (atmo). Bugged since its introduction in commit 336ed15, 2012-07-20.

--- NEWS.md
+++ NEWS.md
@@ -1,6 +1,11 @@
 Master (not on release branches yet)
 ------------------------------------
 
+Bug fixes:
+
+- Fix parallel maximum in sedimentation source term with humid atmosphere
+  model.
+
 Release 5.3.0 - October 26, 2018
 --------------------------------
 
--- src/atmo/attssc.f90
+++ src/atmo/attssc.f90
@@ -157,7 +157,7 @@ endif
 ! we assume that the vertical direction (given by the gravity)
 ! is ALWAYS oriented along the z axis.
 
-if ( ippmod(iatmos).eq.2.and.modsedi.eq.1 ) then ! for humid atmosphere physics only
+if (ippmod(iatmos).eq.2.and.modsedi.eq.1) then ! for humid atmo. physics only
 
   call field_get_val_s(iliqwt, cpro_liqwt)
   call field_get_val_s(itempc, cpro_tempc)
@@ -167,6 +167,7 @@ if ( ippmod(iatmos).eq.2.and.modsedi.eq.1 ) then ! for humid atmosphere physics 
   do iel = 1, ncelet
     qliqmax = max(cpro_liqwt(iel),qliqmax)
   enddo
+  if (irangp.ge.0) call parmax(qliqmax)
 
   if(qliqmax.gt.1e-8)then
 
