code-saturne/code_saturne

commit 0b440f28789ff6ecb082b220aac5f13d88a6ed81

Remove iroext Fortran key word (use time_extrapolated field key word instead).

src/base/varpos.f90
@@ -77,6 +77,7 @@ integer          ivisph, iest
 integer          key_buoyant_id, is_buoyant_fld
 integer          key_t_ext_id, icpext
 integer          iviext
+integer          iroext
 
 double precision gravn2
 
@@ -200,13 +201,14 @@ if (istmpf.eq.-999) then
     istmpf = 2
   endif
 endif
-!     Masse volumique
-if (iroext.eq.-999) then
+
+! Density
+call field_get_key_int(icrom, key_t_ext_id, iroext)
+if (iroext.eq.-1) then
   if (ischtp.eq.1) then
     iroext = 0
   else if (ischtp.eq.2) then
-    !       Pour le moment par defaut on ne prend pas l'ordre 2
-    !              IROEXT = 1
+    ! not extrapolated by default
     iroext = 0
   endif
 endif
@@ -372,12 +374,6 @@ endif
 ! Schema en temps pour les termes sources des grandeurs turbulentes
 if (isto2t.ne.0.and.isto2t.ne. 1.and.isto2t.ne.2) then
   write(nfecra,8131) 'ISTO2T',isto2t
-  iok = iok + 1
-endif
-
-! Schema en temps pour la masse volumique
-if (iroext.ne.0.and.iroext.ne. 1.and.iroext.ne.2) then
-  write(nfecra,8131) 'IROEXT',iroext
   iok = iok + 1
 endif
 

src/atmo/atprke.f90
@@ -75,6 +75,7 @@ integer          iccocg, inc
 integer          iivar
 integer          key_t_ext_id
 integer          iviext
+integer          iroext
 
 double precision gravke, prdtur
 double precision theta_virt
@@ -106,6 +107,7 @@ call field_get_key_id("time_extrapolated", key_t_ext_id)
 call field_get_val_s(icrom, cromo)
 call field_get_val_s(ivisct, cpro_pcvto)
 if(isto2t.gt.0) then
+  call field_get_key_int(icrom, key_t_ext_id, iroext)
   if (iroext.gt.0) then
     call field_get_val_prev_s(icrom, cromo)
   endif

src/base/cs_parameters.c
@@ -937,11 +937,19 @@ cs_parameters_define_field_keys(void)
 
   cs_field_define_key_int("coupling_entity", -1, 0);
 
-  /* Is the field time-extrapolated?
+  /*! Is the field time-extrapolated?
    * -1: default automatic value
-   *  0: not extrapolated
-   *  1: extrapolated
-   *  */
+   *  0: "standard" first-order: the value calculated at
+   *     the beginning of the current time step (from the
+   *     variables known at the end of the previous time step) is used
+   *  1: second-order: the physical property \f$\phi\f$ is
+   *     extrapolated according to the formula
+   *     \f$\phi^{n+\theta}=[(1+\theta)\phi^n-\theta \phi^{n-1}]\f$, \f$\theta\f$ being
+   *     given by the value of 0.5
+   *  2: first-order: the physical property \f$\phi\f$ is
+   *     extrapolated at $n+1$ according to the same formula
+   *     as when = 1 but with \f$\theta\f$ = 1
+   */
   cs_field_define_key_int("time_extrapolated", -1, 0);
 
   cs_field_define_key_int("measures_set_id", -1, CS_FIELD_VARIABLE);

src/base/impini.f90
@@ -85,8 +85,6 @@ integer          f_id, n_fields
 integer          igg, ige
 double precision scmaxp, scminp
 double precision turb_schmidt
-integer          key_t_ext_id, icpext
-integer          iviext
 
 character(len=3), dimension(3) :: nomext3
 character(len=4), dimension(3) :: nomext63
@@ -109,15 +107,6 @@ end interface
 ! 0. Initialisation
 !===============================================================================
 
-! Time extrapolation?
-call field_get_key_id("time_extrapolated", key_t_ext_id)
-
-if (icp.ge.0) then
-  call field_get_key_int(icp, key_t_ext_id, icpext)
-else
-  icpext = 0
-endif
-call field_get_key_int(iviscl, key_t_ext_id, iviext)
 
 !===============================================================================
 ! 1. Introduction
@@ -559,9 +548,8 @@ write(nfecra,9900)
 
 ! --- Stokes
 write(nfecra,4114)istmpf,thetfl,     &
-     iroext,                         &
-     iviext,thetvi,                  &
-     icpext,thetcp,                  &
+     thetvi,                  &
+     thetcp,                  &
      thetsn,thetst,epsup
 
 write(nfecra,9900)
@@ -581,20 +569,8 @@ write(nfecra,9900)
 '                ',14x,       ' (1 : schema std (Saturne 1.0 )',/,&
 '                ',14x,       ' (2 : ordre 2   (THETFL = 0.5 )',/,&
 '       THETFL = ', e14.5,    ' (theta pour flux de masse    )',/,&
-'       IROEXT = ',4x,i10,    ' (extrap. masse volumique',      /,&
-'                ',14x,       ' (0 : explicite',                /,&
-'                ',14x,       ' (1 : n+thetro avec thetro=1/2', /,&
-'                ',14x,       ' (2 : n+thetro avec thetro=1',   /,&
-'       IVIEXT = ',4x,i10,    ' (extrap. viscosite totale',     /,&
-'                ',14x,       ' (0 : explicite',                /,&
-'                ',14x,       ' (1 : n+thetvi avec thetro=1/2', /,&
-'                ',14x,       ' (2 : n+thetvi avec thetro=1',   /,&
 '       THETVI = ', e14.5,    ' (theta pour viscosite totale',  /,&
 '                               ((1+theta)nouveau-theta ancien',/,&
-'       ICPEXT = ',4x,i10,    ' (extrap. chaleur specifique',   /,&
-'                ',14x,       ' (0 : explicite',                /,&
-'                ',14x,       ' (1 : n+thetcp avec thetro=1/2', /,&
-'                ',14x,       ' (2 : n+thetcp avec thetro=1',   /,&
 '       THETCP = ', e14.5,    ' (theta schema chaleur spec',    /,&
 '                               ((1+theta)nouveau-theta ancien',/,&
 '       THETSN = ', e14.5,    ' (theta schema T.S. Nav-Stokes)',/,&
@@ -612,21 +588,13 @@ write(nfecra,9900)
 ' ** STOKES',                                                   /,&
 '    ------',                                                   /,&
                                                                 /,&
-'  -- Phase continue :',                                        /,&
+'  -- Continuous phase:',                                       /,&
                                                                 /,&
 '       ISTMPF = ',4x,i10,    ' (time scheme for flow',         /,&
 '                ',14x,       ' (0: explicit (THETFL = 0     )',/,&
 '                ',14x,       ' (1: std scheme (Saturne 1.0  )',/,&
 '                ',14x,       ' (2: 2nd-order (THETFL = 0.5  )',/,&
 '       THETFL = ', e14.5,    ' (theta for mass flow         )',/,&
-'       IROEXT = ',4x,i10,    ' (density extrapolation',        /,&
-'                ',14x,       ' (0: explicit',                  /,&
-'                ',14x,       ' (1: n+thetro with thetro=1/2',  /,&
-'                ',14x,       ' (2: n+thetro with thetro=1',    /,&
-'       IVIEXT = ',4x,i10,    ' (total viscosity extrapolation',/,&
-'                ',14x,       ' (0: explicit',                  /,&
-'                ',14x,       ' (1: n+thetvi with thetro=1/2',  /,&
-'                ',14x,       ' (2: n+thetvi with thetro=1',    /,&
 '       THETVI = ', e14.5,    ' (theta for total viscosity',    /,&
 '                               ((1+theta).new-theta.old',      /,&
 '       ICPEXT = ',4x,i10,    ' (specific heat extrapolation',  /,&

src/base/iniini.f90
@@ -440,7 +440,6 @@ thetst = -999.d0
 !       fichier suite portant les valeurs adaptees)
 
 !     Masse volumique
-iroext = -999
 initro = 0
 !     Viscosite totale
 thetvi = -999.d0

src/base/iniva0.f90
@@ -175,19 +175,10 @@ enddo
 
 ! Note: for VOF or dilatable algorithms, density at twice previous time step is
 ! also stored and written here with "current to previous" function
-if (ivofmt.ge.0.or.idilat.ge.1.or.ipredfl.eq.0) then
-  call field_current_to_previous(icrom)
-  call field_current_to_previous(icrom)
-  if (iroext.gt.0.or.idilat.gt.1) then
-    call field_current_to_previous(ibrom)
-    call field_current_to_previous(ibrom)
-  endif
-else if (iroext.gt.0.or.icalhy.eq.1.or.ipthrm.eq.1.or.ippmod(icompf).ge.0) then
-  call field_current_to_previous(icrom)
-  if (iroext.gt.0) then
-    call field_current_to_previous(ibrom)
-  endif
-endif
+call field_current_to_previous(icrom)
+call field_current_to_previous(icrom)
+call field_current_to_previous(ibrom)
+call field_current_to_previous(ibrom)
 
 ! Moleacular viscosity
 call field_get_val_s(iviscl, viscl)

src/base/optcal.f90
@@ -149,20 +149,6 @@ module optcal
   !> otherwise to 0.
   integer, save ::          isso2t(nscamx)
 
-  !> \ref iroext specifies the time scheme activated
-  !> for the physical property \f$\phi\f$ density.
-  !> - 0: "standard" first-order: the value calculated at
-  !> the beginning of the current time step (from the
-  !> variables known at the end of the previous time step) is used
-  !> - 1: second-order: the physical property \f$\phi\f$ is
-  !> extrapolated according to the formula
-  !> \f$\phi^{n+\theta}=[(1+\theta)\phi^n-\theta \phi^{n-1}]\f$, \f$\theta\f$ being
-  !> given by the value of 0.5
-  !> - 2: first-order: the physical property \f$\phi\f$ is
-  !> extrapolated at $n+1$ according to the same formula
-  !> as when \ref iroext = 1 but with \f$\theta\f$ = 1
-  integer, save ::          iroext
-
   !> initvi : =1 if total viscosity read from checkpoint file
   integer, save ::          initvi
 

src/base/schtmp.f90
@@ -76,6 +76,7 @@ integer          iflmas , iflmab
 integer          f_id
 integer          key_t_ext_id, icpext
 integer          iviext
+integer          iroext
 
 double precision flux   , theta  , viscos, varcp
 
@@ -167,6 +168,7 @@ if (iappel.eq.1) then
       ! Densisty
       call field_get_key_int (ivarfl(isca(iscal)), kromsl, f_id)
       if (f_id.ge.0.and.iscavr(iscal).le.0) then
+        call field_get_key_int(f_id, key_t_ext_id, iroext)
         if (iroext.gt.0) then
           call field_current_to_previous(f_id)
         endif
@@ -195,7 +197,8 @@ elseif (iappel.eq.2) then
 
   if (initro.ne.1) then
     initro = 1
-    if (iroext.gt.0) then!FIXME rm this option
+    call field_get_key_int(icrom, key_t_ext_id, iroext)
+    if (iroext.gt.0) then
       call field_current_to_previous(icrom)
       call field_current_to_previous(ibrom)
     endif

commit 4041fa0232b55aac47e77f4fbe5c2f33a496cccb

Fix warning bug introduced in commit 0b440f2 (remove iroext).

src/base/varpos.f90
@@ -202,6 +202,7 @@ if (iroext.eq.-1) then
     ! not extrapolated by default
     iroext = 0
   endif
+  call field_set_key_int(icrom, key_t_ext_id, iroext)
 endif
 
 ! Molecular Viscosity