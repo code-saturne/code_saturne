code-saturne/code_saturne

commit 7a9e65a8e20d7606bf14caeafb8465121b6ed4df

Remove ivsext Fortran key word (use time_extrapolated field key word instead).

src/base/varpos.f90
@@ -285,13 +285,17 @@ do iscal = 1, nscal
       isso2t(iscal) = 1
     endif
   endif
-  ! Diffusivite scalaires
-  if (ivsext(iscal).eq.-999) then
-    if (ischtp.eq.1) then
-      ivsext(iscal) = 0
-    else if (ischtp.eq.2) then
-      ! Pour le moment par defaut on ne prend pas l'ordre 2
-      ivsext(iscal) = 0
+  ! Diffusivity of scalars
+  call field_get_key_int(ivarfl(isca(ii)), kivisl, ifcvsl)
+  if (ifcvsl.ge.0) then
+    call field_get_key_int(ifcvsl, key_t_ext_id, iviext)
+    if (iviext.eq.-1) then
+      if (ischtp.eq.1) then
+        iviext = 0
+      else if (ischtp.eq.2) then
+        ! Pour le moment par defaut on ne prend pas l'ordre 2
+        iviext = 0
+      endif
     endif
   endif
 
@@ -382,12 +386,6 @@ do iscal = 1, nscal
   if (isso2t(iscal).ne.0.and.                                    &
       isso2t(iscal).ne. 1.and.isso2t(iscal).ne.2) then
     write(nfecra,8141) iscal,'ISSO2T',isso2t(iscal)
-    iok = iok + 1
-  endif
-  ! Schema en temps pour la viscosite
-  if (ivsext(iscal).ne.0.and.                                    &
-      ivsext(iscal).ne. 1.and.ivsext(iscal).ne.2) then
-    write(nfecra,8141) iscal,'IVSEXT',ivsext(iscal)
     iok = iok + 1
   endif
 enddo
@@ -496,14 +494,16 @@ if (nscal.ge.1) then
     ! Diffusivity
     call field_get_key_int (ivarfl(isca(ii)), kivisl, ifcvsl)
     if (ifcvsl.ge.0.and.iscavr(ii).le.0) then
-      if (ivsext(ii).gt.0) then
+      call field_get_key_int(ifcvsl, key_t_ext_id, iviext)
+      if (iviext.gt.0) then
         call field_set_n_previous(ifcvsl, 1)
       endif
     endif
     ! Density
     call field_get_key_int (ivarfl(isca(ii)), kromsl, ifcvsl)
     if (ifcvsl.ge.0.and.iscavr(ii).le.0) then
-      if (ivsext(ii).gt.0) then
+      call field_get_key_int(ifcvsl, key_t_ext_id, iviext)
+      if (iviext.gt.0) then
         call field_set_n_previous(ifcvsl, 1)
       endif
     endif

src/base/impini.f90
@@ -929,7 +929,7 @@ if (nscal.ge.1) then
   write(nfecra,6030)
   write(nfecra,6040)
   do ii = 1, nscal
-    write(nfecra,6041) ii,thetss(ii),ivsext(ii),thetvs(ii)
+    write(nfecra,6041) ii,thetss(ii),thetvs(ii)
   enddo
   write(nfecra,6042)
 
@@ -990,19 +990,15 @@ endif
 '          pris en compte que si ICLVFL = 2',                   /)
  6040 format(                                                     &
 '------------------------------------------------------',       /,&
-'   Scalaire      THETSS    IVSEXT      THETVS',                /,&
+'   Scalaire      THETSS     THETVS',                /,&
 '------------------------------------------------------'         )
  6041 format(                                                     &
- 1x,     i10,      e12.4,      i10,      e12.4                   )
+ 1x,     i10,      e12.4,   e12.4                   )
  6042 format(                                                     &
 '------------------------------------------------------',       /,&
                                                                 /,&
 '       THETSS =                (theta pour termes sources   )',/,&
 '                               ((1+theta)nouveau-theta ancien',/,&
-'       IVSEXT =                (extrap. viscosite totale    )',/,&
-'                               (0 : explicite               )',/,&
-'                               (1 : n+thetvs avec thetvs=1/2', /,&
-'                               (2 : n+thetvs avec thetvs=1  )',/,&
 '       THETVS =                (theta pour diffusiv. scalaire',/,&
 '                               ((1+theta)nouveau-theta ancien',/)
 
@@ -1058,19 +1054,15 @@ endif
 '          only if ICLVFL = 2',                                 /)
  6040 format(                                                     &
 '------------------------------------------------------',       /,&
-'   Scalar        THETSS    IVSEXT      THETVS',                /,&
+'   Scalar        THETSS      THETVS',                /,&
 '------------------------------------------------------'         )
  6041 format(                                                     &
- 1x,     i10,      e12.4,      i10,      e12.4                   )
+ 1x,     i10,      e12.4,     e12.4                   )
  6042 format(                                                     &
 '------------------------------------------------------',       /,&
                                                                 /,&
 '       THETSS =                (theta for source terms      )',/,&
 '                               ((1+theta).new-theta.old     )',/,&
-'       IVSEXT =                (extrap. total viscosity     )',/,&
-'                               (0: explicit                 )',/,&
-'                               (1: n+thetvs with thetvs=1/2 )',/,&
-'                               (2: n+thetvs with thetvs=1   )',/,&
 '       THETVS =                (theta for scalar diffusivity', /,&
 '                               ((1+theta).new-theta.old     )',/)
 

src/base/iniini.f90
@@ -490,7 +490,6 @@ do iscal = 1, nscamx
 !     INIT.. =1 indique que la variable a ete proprement initialisee (dans un
 !       fichier suite portant les valeurs adaptees)
 
-  ivsext(iscal) = -999
   thetvs(iscal) = -999.d0
   initvs(iscal) = 0
 

src/base/modini.f90
@@ -250,15 +250,21 @@ do iscal = 1, nscal
   elseif (isso2t(iscal).eq.0) then
     thetss(iscal) = 0.d0
   endif
-!    -- Diffusivite des scalaires
+  ! Scalars diffusivity
+  call field_get_key_int(ivarfl(isca(ii)), kivisl, f_id)
+  if (f_id.ge.0) then
+    call field_get_key_int(f_id, key_t_ext_id, iviext)
+  else
+    iviext = 0
+  endif
   if (abs(thetvs(iscal)+999.d0).gt.epzero) then
-    write(nfecra,1021) iscal,'IVSEXT',ivsext(iscal),'THETVS'
+    write(nfecra,1021) iscal,'IVSEXT',iviext,'THETVS'
     iok = iok + 1
-  elseif (ivsext(iscal).eq.0) then
+  elseif (iviext.eq.0) then
     thetvs(iscal) = 0.d0
-  elseif (ivsext(iscal).eq.1) then
+  elseif (iviext.eq.1) then
     thetvs(iscal) = 0.5d0
-  elseif (ivsext(iscal).eq.2) then
+  elseif (iviext.eq.2) then
     thetvs(iscal) = 1.d0
   endif
 enddo

src/base/optcal.f90
@@ -163,20 +163,6 @@ module optcal
   !> as when \ref iroext = 1 but with \f$\theta\f$ = 1
   integer, save ::          iroext
 
-  !> for each scalar iscal, \ref ivsext (iscal) specifies the time scheme
-  !> activated for the physical property \f$\phi\f$ "diffusivity".
-  !> - 0: "standard" first-order: the value calculated at
-  !> the beginning of the current time step (from the variables known
-  !> at the end of the previous time step) is used
-  !> - 1: second-order: the physical property \f$\phi\f$ is
-  !> extrapolated according to the formula
-  !> \f$\phi^{n+\theta}=[(1+\theta)\phi^n-\theta \phi^{n-1}]\f$, \f$\theta\f$
-  !> being given by the value of \ref thetvs (iscal) = 0.5
-  !> - 2: first-order: the physical property \f$\phi\f$ is
-  !> extrapolated at $n+1$ according to the same formula as
-  !> when \ref ivsext = 1, but with \f$\theta\f$ = \ref thetvs (iscal) = 1
-  integer, save ::          ivsext(nscamx)
-
   !> initvi : =1 if total viscosity read from checkpoint file
   integer, save ::          initvi
 
@@ -258,10 +244,10 @@ module optcal
 
   !> \f$ \theta \f$-scheme for the extrapolation of the physical
   !> property \f$\phi\f$ "diffusivity" when the extrapolation has
-  !> been activated (see \ref ivsext), according to the formula
+  !> been activated (see \ref time_extrapolated key word), according to the formula
   !> \f$\phi^{n+\theta}=(1+\theta)\phi^n-\theta \phi^{n-1}\f$.\n
   !> The value of\f$\theta\f$ = \ref thetvs is deduced from the value
-  !> chosen for \ref ivsext. Generally, only the value 0.5 is used.
+  !> chosen for \ref time_extrapolated key word. Generally, only the value 0.5 is used.
   !>    -  0 : explicit
   !>    - 1/2: extrapolated in n+1/2
   !>    -  1 : extrapolated in n+1

src/base/schtmp.f90
@@ -159,7 +159,8 @@ if (iappel.eq.1) then
       ! Diffusivity
       call field_get_key_int (ivarfl(isca(iscal)), kivisl, f_id)
       if (f_id.ge.0.and.iscavr(iscal).le.0) then
-        if (ivsext(iscal).gt.0) then
+        call field_get_key_int(f_id, key_t_ext_id, iviext)
+        if (iviext.gt.0) then
           call field_current_to_previous(f_id)
         endif
       endif
@@ -230,7 +231,8 @@ elseif (iappel.eq.2) then
         initvs(iscal) = 1
         call field_get_key_int (ivarfl(isca(iscal)), kivisl, f_id)
         if (f_id.ge.0.and.iscavr(iscal).le.0) then
-          if (ivsext(iscal).gt.0) then
+          call field_get_key_int(f_id, key_t_ext_id, iviext)
+          if (iviext.gt.0) then
             call field_current_to_previous(f_id)
           endif
         endif
@@ -296,7 +298,8 @@ elseif (iappel.eq.2) then
       ! Diffusivity
       call field_get_key_int (ivarfl(isca(iscal)), kivisl, f_id)
       if (f_id.ge.0.and.iscavr(iscal).le.0) then
-        if (ivsext(iscal).gt.0) then
+        call field_get_key_int(f_id, key_t_ext_id, iviext)
+        if (iviext.gt.0) then
           theta  = thetvs(iscal)
           call field_get_val_s(f_id, cpro_visls)
           call field_get_val_prev_s(f_id, cproa_visls)
@@ -472,7 +475,8 @@ elseif (iappel.eq.5) then
       ! Diffusivity
       call field_get_key_int (ivarfl(isca(iscal)), kivisl, f_id)
       if (f_id.ge.0.and.iscavr(iscal).le.0) then
-        if (ivsext(iscal).gt.0) then
+        call field_get_key_int(f_id, key_t_ext_id, iviext)
+        if (iviext.gt.0) then
           call field_get_val_s(f_id, cpro_visls)
           call field_get_val_prev_s(f_id, cproa_visls)
           do iel = 1, ncel

src/base/verini.f90
@@ -82,6 +82,7 @@ integer          kscmin, kscmax, ifcvsl
 integer          keyvar, keysca
 integer          key_t_ext_id, icpext
 integer          iviext
+integer          ivisext
 double precision scmaxp, scminp
 double precision turb_schmidt
 
@@ -214,8 +215,12 @@ do iscal = 1, nscal
   if (isso2t(iscal).ne.isno2t) then
     write(nfecra,2133) iscal,isso2t(iscal),isno2t
   endif
-  if (ivsext(iscal).ne.iviext) then
-    write(nfecra,2134) iscal,ivsext(iscal),iviext
+  call field_get_key_int (ivarfl(isca(iscal)), kivisl, f_id)
+  if (f_id.ge.0.and.iscavr(iscal).le.0) then
+    call field_get_key_int(f_id, key_t_ext_id, ivisext)
+    if (ivisext.ne.iviext) then
+      write(nfecra,2134) iscal,ivisext,iviext
+    endif
   endif
 enddo
 
@@ -225,16 +230,6 @@ if (ischtp.eq.2.and.vcopt%ibdtso.gt.1) then
   write(nfecra,1135)
   iok = iok + 1
 endif
-
-do iscal = 1, nscal
-  if (ivsext(iscal).gt.0) then
-    call field_get_key_int (ivarfl(isca(iscal)), kivisl, ifcvsl)
-    if (ifcvsl.lt.0) then
-      write(nfecra,2136) iscal, ivsext(iscal), ifcvsl
-      iok = iok + 1
-    endif
-  endif
-enddo
 
 !     Pour les tests suivants : Utilise-t-on un estimateur d'erreur ?
 indest = 0
@@ -385,10 +380,9 @@ if (ippmod(iphpar).ge.1) then
       (thetcp .gt.0.d0).or.                                &
       (icpext .gt.0   )) istop = 1
   do iscal = 1, nscal
-    if ((    thetss(iscal)       .gt.0.d0 ).or.            &
-        (    isso2t(iscal)       .gt.0    ).or.            &
-        (    thetvs(iscal).gt.0.d0 ).or.                   &
-        (    ivsext(iscal).gt.0    )    ) istop = 1
+    if ((thetss(iscal)       .gt.0.d0 ).or.            &
+        (isso2t(iscal)       .gt.0    ).or.            &
+        (thetvs(iscal).gt.0.d0 )) istop = 1
   enddo
 
   if (istop.ne.0) then
@@ -1090,32 +1084,6 @@ endif
 '@',                                                            /,&
 '@  Il est conseille de verifier les parametres donnes via',    /,&
 '@  l''interface ou cs_user_parameters.f90.',                   /,&
-'@',                                                            /,&
-'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
-'@',                                                            /)
- 2136 format(                                                     &
-'@',                                                            /,&
-'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
-'@',                                                            /,&
-'@ @@ ATTENTION :      A L''ENTREE DES DONNEES',                /,&
-'@    =========',                                               /,&
-'@   CHOIX INCOMPATIBLE POUR LE SCHEMA EN TEMPS',               /,&
-'@',                                                            /,&
-'@   Scalaire ISCAL = ', i10,                                   /,&
-'@     La  diffusivite      est extrapolee en temps avec',      /,&
-'@       IVSEXT(ISCAL) = ', i10,                                /,&
-'@     Pour cela, elle doit etre variable, or',                 /,&
-'@       scalar_diffusivity_id = ', i10, ' pour ce champ.'      /,&
-'@',                                                            /,&
-'@  Le calcul ne sera pas execute',                             /,&
-'@',                                                            /,&
-'@  Verifier les parametres donnes via l''interface',           /,&
-'@    ou cs_user_parameters.f90.',                              /,&
-'@    - desactiver le choix d''extrapolation en temps',         /,&
-'@                                     de la diffusivite',      /,&
-'@      ou',                                                    /,&
-'@    - imposer la diffusivite variable',                       /,&
-'@         (et le renseigner alors via l''interface ou usphyv)',/,&
 '@',                                                            /,&
 '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
 '@',                                                            /)
@@ -2357,31 +2325,6 @@ endif
 '@ Check the input data given through the User Interface',      /,&
 '@   or in cs_user_parameters.f90.',                            /,&
 '@',                                                            /,&
-'@',                                                            /,&
-'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
-'@',                                                            /)
- 2136 format(                                                     &
-'@',                                                            /,&
-'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
-'@',                                                            /,&
-'@ @@   WARNING :      WHEN READING INPUT DATA',                /,&
-'@    =========',                                               /,&
-'@  INCOMPATIBILITY FOR TIME DISCRETISATION SCHEME',            /,&
-'@',                                                            /,&
-'@   Scalar   ISCAL = ', i10,                                   /,&
-'@    Diffusivity   is extrapolated in time wih',               /,&
-'@       IVSEXT(ISCAL) = ', i10,                                /,&
-'@     it should thus be a variable, or',                       /,&
-'@       scalar_diffusivity_id = ', i10, ' for this field.'     /,&
-'@',                                                            /,&
-'@ Computation will  NOT  proceed',                             /,&
-'@',                                                            /,&
-'@  Verify the parameters',                                     /,&
-'@    - deactivate intepolation in time',                       /,&
-'@                                     for diffusivity',        /,&
-'@      or',                                                    /,&
-'@    - impose diffusivite variable',                           /,&
-'@         (define its variation law in the GUI ou usphyv)',    /,&
 '@',                                                            /,&
 '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
 '@',                                                            /)

commit e6cdcdf13cae792e90dd30737483acf5df8282f5

Merged from commit 9a4d12b on master.
Fix again bug introduced in commit 7a9e65a (2018-10-24) for specifi physics
(remove ivsext fortran key word).

src/base/addfld.f90
@@ -79,7 +79,7 @@ implicit none
 
 ! Local variables
 
-integer          ii
+integer          ii, iscal
 integer          ifcvsl, kbfid
 integer          iflid, iopchr
 integer          itycat, ityloc, idim1, idim3
@@ -90,6 +90,10 @@ integer          kfturt_alpha
 integer          keycpl, keydri
 integer          ivar, iscdri
 logical          iprev, inoprv, is_set
+integer          key_t_ext_id
+integer          nfld
+integer          n_prev
+integer          t_ext
 
 character(len=80) :: name, f_name, f_label, s_label, s_name
 type(var_cal_opt) :: vcopt_dfm, vcopt_alpha, vcopt
@@ -132,6 +136,12 @@ call field_get_key_id('coupled', keycpl)
 
 ! Key id for drift scalar
 call field_get_key_id("drift_scalar_model", keydri)
+
+! Time extrapolation?
+call field_get_key_id("time_extrapolated", key_t_ext_id)
+
+! Number of fields
+call field_get_n_fields(nfld)
 
 !===============================================================================
 ! 0. Initialization
@@ -458,7 +468,7 @@ if (itempb .ge. 0) then
 endif
 
 !===============================================================================
-! 4. Set number of previous values if needed
+! 4. Set some field keys and number of previous values if needed
 !===============================================================================
 
 ! Density at the second previous time step for VOF algorithm
@@ -472,6 +482,89 @@ else if (icalhy.eq.1.or.ipthrm.eq.1.or.ippmod(icompf).ge.0) then
   call field_set_n_previous(icrom, 1)
   call field_set_n_previous(ibrom, 1)
 endif
+
+!Time extrapolation
+!-------------------
+
+! Density
+call field_get_key_int(icrom, key_t_ext_id, t_ext)
+if (t_ext.eq.-1) then
+  if (ischtp.eq.1) then
+    t_ext = 0
+  else if (ischtp.eq.2) then
+    ! not extrapolated by default
+    t_ext = 0
+  endif
+  call field_set_key_int(icrom, key_t_ext_id, t_ext)
+endif
+
+! Molecular Viscosity
+call field_get_key_int(iviscl, key_t_ext_id, t_ext)
+if (t_ext.eq.-1) then
+  if (ischtp.eq.1) then
+    t_ext = 0
+  else if (ischtp.eq.2) then
+    ! not extrapolated by default
+    t_ext = 0
+  endif
+  call field_set_key_int(iviscl, key_t_ext_id, t_ext)
+endif
+
+! Turbulent Viscosity
+call field_get_key_int(ivisct, key_t_ext_id, t_ext)
+if (t_ext.eq.-1) then
+  if (ischtp.eq.1) then
+    t_ext = 0
+  else if (ischtp.eq.2) then
+    ! not extrapolated by default
+    t_ext = 0
+  endif
+  call field_set_key_int(ivisct, key_t_ext_id, t_ext)
+endif
+
+! Specific heat
+if (icp.ge.0) then
+  call field_get_key_int(icp, key_t_ext_id, t_ext)
+  if (t_ext.eq.-1) then
+    if (ischtp.eq.1) then
+      t_ext = 0
+    else if (ischtp.eq.2) then
+      ! not extrapolated by default
+      t_ext = 0
+    endif
+  endif
+  call field_set_key_int(icp, key_t_ext_id, t_ext)
+endif
+
+! Scalar diffusivity time extrapolation
+do iscal = 1, nscal
+  ! Diffusivity of scalars
+  call field_get_key_int(ivarfl(isca(iscal)), kivisl, f_id)
+  if (f_id.ge.0) then
+    call field_get_key_int(f_id, key_t_ext_id, t_ext)
+    if (t_ext.eq.-1) then
+      if (ischtp.eq.1) then
+        t_ext = 0
+      else if (ischtp.eq.2) then
+        ! Pour le moment par defaut on ne prend pas l'ordre 2
+        t_ext = 0
+      endif
+    endif
+    call field_set_key_int(f_id, key_t_ext_id, t_ext)
+  endif
+enddo
+
+! If time extrapolation, set previous values
+do f_id = 0, nfld - 1
+  call field_get_key_int(f_id, key_t_ext_id, t_ext)
+  if (t_ext.gt.0) then
+    call field_get_n_previous(iviscl, n_prev)
+    if (n_prev .lt. 1) then
+      call field_set_n_previous(iviscl, 1)
+    endif
+  endif
+enddo
+
 
 !---
 ! Formats

src/base/cs_field.c
@@ -296,6 +296,10 @@ cs_f_field_set_n_previous(int  id,
                           int  n_previous);
 
 void
+cs_f_field_get_n_previous(int  id,
+                          int  n_previous[1]);
+
+void
 cs_f_field_var_ptr_by_id(int          id,
                          int          pointer_type,
                          int          pointer_rank,
@@ -871,6 +875,25 @@ cs_f_field_get_dimension(int  id,
   const cs_field_t *f = cs_field_by_id(id);
 
   dim[0] = f->dim;
+}
+
+/*----------------------------------------------------------------------------
+ * Return the number of previous values of a field.
+ *
+ * This function is intended for use by Fortran wrappers.
+ *
+ * parameters:
+ *   id  <-- field id
+ *   dim --> field dimension
+ *----------------------------------------------------------------------------*/
+
+void
+cs_f_field_get_n_previous(int  id,
+                          int  n_previous[1])
+{
+  const cs_field_t *f = cs_field_by_id(id);
+
+  n_previous[0] = f->n_time_vals - 1;
 }
 
 /*----------------------------------------------------------------------------

src/base/field.f90
@@ -325,6 +325,19 @@ module field
 
     !---------------------------------------------------------------------------
 
+    ! Interface to C function to get the number of previous values
+
+    subroutine cs_f_field_get_n_previous(f_id, n_previous)  &
+      bind(C, name='cs_f_field_get_n_previous')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      integer(c_int), value :: f_id
+      integer(c_int), dimension(1), intent(out) :: n_previous
+    end subroutine cs_f_field_get_n_previous
+
+
+    !---------------------------------------------------------------------------
+
     ! Interface to C function allocating field values
 
     subroutine cs_field_allocate_values(f)  &
@@ -1053,6 +1066,38 @@ contains
     return
 
   end subroutine field_set_n_previous
+
+  !=============================================================================
+
+  !> \brief Return a given field's number of previous values.
+
+  !> \param[in]   f_id   field id
+  !> \param[out]  f_n    number of previous values
+
+  subroutine field_get_n_previous(f_id, f_n)
+
+    use, intrinsic :: iso_c_binding
+    implicit none
+
+    ! Arguments
+
+    integer, intent(in)  :: f_id
+    integer, intent(out) :: f_n
+
+    ! Local variables
+
+    integer(c_int) :: c_f_id
+    integer(c_int), dimension(1) :: c_n
+
+    c_f_id = f_id
+
+    call cs_f_field_get_n_previous(c_f_id, c_n)
+
+    f_n = c_n(1)
+
+    return
+
+  end subroutine field_get_n_previous
 
   !=============================================================================
 

src/base/varpos.f90
@@ -69,15 +69,12 @@ implicit none
 
 ! Local variables
 
-integer          iscal , id, ityloc, itycat, ifcvsl, pflag
+integer          iscal , id, ityloc, itycat, pflag
 integer          ii
 integer          iok
 integer          f_id, idftnp
 integer          ivisph, iest
 integer          key_buoyant_id, is_buoyant_fld
-integer          key_t_ext_id, icpext
-integer          iviext
-integer          iroext
 
 double precision gravn2
 
@@ -91,9 +88,6 @@ type(var_cal_opt) :: vcopt
 
 ! Key id for buoyant field (inside the Navier Stokes loop)
 call field_get_key_id("is_buoyant", key_buoyant_id)
-
-! Time extrapolation?
-call field_get_key_id("time_extrapolated", key_t_ext_id)
 
 ! Determine itycor now that irccor is known (iturb/itytur known much earlier)
 ! type of rotation/curvature correction for turbulent viscosity models
@@ -193,55 +187,6 @@ if (istmpf.eq.-999) then
   endif
 endif
 
-! Density
-call field_get_key_int(icrom, key_t_ext_id, iroext)
-if (iroext.eq.-1) then
-  if (ischtp.eq.1) then
-    iroext = 0
-  else if (ischtp.eq.2) then
-    ! not extrapolated by default
-    iroext = 0
-  endif
-  call field_set_key_int(icrom, key_t_ext_id, iroext)
-endif
-
-! Molecular Viscosity
-call field_get_key_int(iviscl, key_t_ext_id, iviext)
-if (iviext.eq.-1) then
-  if (ischtp.eq.1) then
-    iviext = 0
-  else if (ischtp.eq.2) then
-    ! not extrapolated by default
-    iviext = 0
-  endif
-  call field_set_key_int(iviscl, key_t_ext_id, iviext)
-endif
-
-! Turbulent Viscosity
-call field_get_key_int(ivisct, key_t_ext_id, iviext)
-if (iviext.eq.-1) then
-  if (ischtp.eq.1) then
-    iviext = 0
-  else if (ischtp.eq.2) then
-    ! not extrapolated by default
-    iviext = 0
-  endif
-  call field_set_key_int(ivisct, key_t_ext_id, iviext)
-endif
-
-! Specific heat
-if (icp.ge.0) then
-  call field_get_key_int(icp, key_t_ext_id, icpext)
-  if (icpext.eq.-1) then
-    if (ischtp.eq.1) then
-      icpext = 0
-    else if (ischtp.eq.2) then
-      ! not extrapolated by default
-      icpext = 0
-    endif
-  endif
-  call field_set_key_int(icp, key_t_ext_id, icpext)
-endif
 !     Termes sources NS,
 if (isno2t.eq.-999) then
   if (ischtp.eq.1) then
@@ -278,20 +223,6 @@ do iscal = 1, nscal
       ! generalement pas de TS scalaire a interpoler.
       isso2t(iscal) = 1
     endif
-  endif
-  ! Diffusivity of scalars
-  call field_get_key_int(ivarfl(isca(iscal)), kivisl, ifcvsl)
-  if (ifcvsl.ge.0) then
-    call field_get_key_int(ifcvsl, key_t_ext_id, iviext)
-    if (iviext.eq.-1) then
-      if (ischtp.eq.1) then
-        iviext = 0
-      else if (ischtp.eq.2) then
-        ! Pour le moment par defaut on ne prend pas l'ordre 2
-        iviext = 0
-      endif
-    endif
-    call field_set_key_int(ifcvsl, key_t_ext_id, iviext)
   endif
 
   ! Model for turbulent fluxes u'T' (SGDH, GGDH, AFM, DFM)
@@ -401,24 +332,6 @@ if (idilat.ge.4) then
   call field_set_key_int(id, keyvis, 0)
 endif
 
-! Dans le cas d'une extrapolation de la viscosite totale
-call field_get_key_int(iviscl, key_t_ext_id, iviext)
-if (iviext.gt.0) then
-  call field_set_n_previous(iviscl, 1)
-endif
-call field_get_key_int(ivisct, key_t_ext_id, iviext)
-if (iviext.gt.0) then
-  call field_set_n_previous(ivisct, 1)
-endif
-
-! CP s'il est variable
-if (icp.ge.0) then
-  call field_get_key_int(icp, key_t_ext_id, icpext)
-  if (icpext.gt.0) then
-    call field_set_n_previous(icp, 1)
-  endif
-endif
-
 ! On a besoin d'un tableau pour les termes sources de Navier Stokes
 !  a extrapoler. Ce tableau est NDIM dans le cas general et NDIM+1
 !  si on extrapole aussi les termes sources de l equation sur le taux
@@ -464,7 +377,6 @@ if (isto2t.gt.0) then
 endif
 
 ! Proprietes des scalaires : termes sources pour theta schema
-!   et VISCLS si elle est variable
 if (nscal.ge.1) then
   do ii = 1, nscal
     if (isso2t(ii).gt.0) then
@@ -479,22 +391,6 @@ if (nscal.ge.1) then
     call field_get_key_struct_var_cal_opt(ivarfl(isca(ii)), vcopt)
     if (vcopt%isstpc.eq.2) then
       call add_source_term_field(ivarfl(isca(ii)))
-    endif
-    ! Diffusivity
-    call field_get_key_int (ivarfl(isca(ii)), kivisl, ifcvsl)
-    if (ifcvsl.ge.0.and.iscavr(ii).le.0) then
-      call field_get_key_int(ifcvsl, key_t_ext_id, iviext)
-      if (iviext.gt.0) then
-        call field_set_n_previous(ifcvsl, 1)
-      endif
-    endif
-    ! Density
-    call field_get_key_int (ivarfl(isca(ii)), kromsl, ifcvsl)
-    if (ifcvsl.ge.0.and.iscavr(ii).le.0) then
-      call field_get_key_int(ifcvsl, key_t_ext_id, iviext)
-      if (iviext.gt.0) then
-        call field_set_n_previous(ifcvsl, 1)
-      endif
     endif
   enddo
 endif