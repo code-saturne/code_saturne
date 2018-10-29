code-saturne/code_saturne

commit d0274e80efdf137738d1b62eb1c6bec372b71489

Use anisotropic face diffusion calculation for mesh velocity solving (ALE). Subsequently remove orthotropic face diffusion function and iortvm keyword.

src/base/cs_ale.c
@@ -41,6 +41,7 @@
 
 #include "cs_base.h"
 #include "cs_boundary_conditions.h"
+#include "cs_convection_diffusion.h"
 #include "cs_face_viscosity.h"
 #include "cs_field.h"
 #include "cs_field_pointer.h"
@@ -223,7 +224,7 @@ cs_ale_project_displacement(const int           ialtyb[],
              +dvol2*(meshv[cell_id2][i] + gradm[cell_id2][i][0]*cen2_node[0]
                                         + gradm[cell_id2][i][1]*cen2_node[1]
                                         + gradm[cell_id2][i][2]*cen2_node[2])
-             *dt[cell_id2];
+              *dt[cell_id2];
           }
 
           vtx_counter[vtx_id] += dvol1+dvol2;
@@ -414,14 +415,14 @@ cs_ale_update_mesh(const int           itrale,
  * so that it can be used to update mass fluxes (due to mesh displacement).
  *
  * \param[in]       iterns        Navier-Stokes iteration number
- * \param[in]       ialtyb        Type of boundary for ALE
  * \param[in]       ndircl        Number of Dirichlet BCs for mesh velocity
+ * \param[in]       impale        Indicator for fixed node displacement
+ * \param[in]       ialtyb        Type of boundary for ALE
  */
 /*----------------------------------------------------------------------------*/
 
 void
 cs_ale_solve_mesh_velocity(const int   iterns,
-                           const int   iortvm,
                            const int   ndircl,
                            const int  *impale,
                            const int  *ialtyb)
@@ -459,18 +460,6 @@ cs_ale_solve_mesh_velocity(const int   iterns,
   BFT_MALLOC(smbr, n_cells_ext, cs_real_3_t);
   BFT_MALLOC(fimp, n_cells_ext, cs_real_33_t);
 
-  cs_real_t *i_visc, *b_visc;
-  BFT_MALLOC(i_visc, n_i_faces, cs_real_t);
-  BFT_MALLOC(b_visc, n_b_faces, cs_real_t);
-
-  cs_real_t *mesh_viscs;
-  cs_real_3_t *mesh_viscv;
-  if (iortvm == 0) {
-    mesh_viscs = cs_field_by_name("mesh_viscosity")->val;
-  } else {
-    mesh_viscv = (cs_real_3_t *)cs_field_by_name("mesh_viscosity")->val;
-  }
-
   cs_real_3_t *mshvel = (cs_real_3_t *)CS_F_(mesh_u)->val;
   cs_real_3_t *mshvela = (cs_real_3_t *)CS_F_(mesh_u)->val_pre;
 
@@ -498,21 +487,21 @@ cs_ale_solve_mesh_velocity(const int   iterns,
   cs_real_33_t *bc_b   = (cs_real_33_t *)bc_coeffs->b;
   cs_real_33_t *bc_bf  = (cs_real_33_t *)bc_coeffs->bf;
 
-  /* The mesh move in the direction of the gravity in case of free-surface */
+  int idftnp = var_cal_opt.idften;
+
+  /* The mesh moves in the direction of the gravity in case of free-surface */
   for (cs_lnum_t face_id = 0 ; face_id < n_b_faces ; face_id++) {
     if (ialtyb[face_id] == CS_FREE_SURFACE) {
       cs_lnum_t cell_id = b_face_cells[face_id];
       cs_real_t distbf = b_dist[face_id];
-      cs_real_t srfbn2 = cs_math_pow2(b_face_surf[face_id]);
 
-      cs_real_t hint;
-      if (iortvm == 0) {
-        hint = mesh_viscs[cell_id] / distbf;
-      } else { /* FIXME */
-        hint = (mesh_viscv[cell_id][0] * cs_math_pow2(b_face_normal[face_id][0])
-             +  mesh_viscv[cell_id][1] * cs_math_pow2(b_face_normal[face_id][1])
-             +  mesh_viscv[cell_id][2] * cs_math_pow2(b_face_normal[face_id][2]))
-             / distbf / srfbn2;
+      cs_real_6_t hintt = {0., 0., 0., 0., 0., 0.};
+      if (idftnp & CS_ISOTROPIC_DIFFUSION) {
+        for (int isou  = 0; isou < 3; isou++)
+          hintt[isou] = CS_F_(vism)->val[cell_id] / distbf;
+      } else if (idftnp & CS_ANISOTROPIC_LEFT_DIFFUSION) {
+          for (int isou  = 0; isou < 5; isou++)
+            hintt[isou] = CS_F_(vism)->val[6*cell_id+isou] / distbf;
      }
 
      cs_real_t prosrf = cs_math_3_dot_product(grav, b_face_normal[face_id]);
@@ -521,18 +510,19 @@ cs_ale_solve_mesh_velocity(const int   iterns,
      for (int i = 0 ; i < 3 ; i++)
        pimpv[i] = grav[i]*b_massflux[face_id]/(brom[face_id]*prosrf);
 
-     cs_boundary_conditions_set_dirichlet_vector(&(bc_a[face_id]),
-                                                 &(bc_af[face_id]),
-                                                 &(bc_b[face_id]),
-                                                 &(bc_bf[face_id]),
-                                                 pimpv,
-                                                 hint,
-                                                 rinfiv);
+     cs_boundary_conditions_set_dirichlet_vector_aniso(&(bc_a[face_id]),
+                                                       &(bc_af[face_id]),
+                                                       &(bc_b[face_id]),
+                                                       &(bc_bf[face_id]),
+                                                       pimpv,
+                                                       hintt,
+                                                       rinfiv);
 
     }
   }
 
   /* 2. Solving of the mesh velocity equation */
+
   if (var_cal_opt.iwarni >= 1)
     bft_printf("\n\n           SOLVING VARIABLE %s\n\n",
                CS_F_(mesh_u)->name);
@@ -545,19 +535,28 @@ cs_ale_solve_mesh_velocity(const int   iterns,
     }
   }
 
-  if (iortvm == 0) {
+  cs_real_t *i_visc, *b_visc;
+
+  BFT_MALLOC(b_visc, n_b_faces, cs_real_t);
+
+  if (idftnp & CS_ISOTROPIC_DIFFUSION) {
+    BFT_MALLOC(i_visc, n_i_faces, cs_real_t);
+
     cs_face_viscosity(m,
                       fvq,
                       cs_glob_space_disc->imvisf,
-                      mesh_viscs,
+                      CS_F_(vism)->val,
                       i_visc,
                       b_visc);
-  } else {
-    cs_face_orthotropic_viscosity_vector(m,
+
+  } else if (idftnp & CS_ANISOTROPIC_LEFT_DIFFUSION) {
+    BFT_MALLOC(i_visc, 9*n_i_faces, cs_real_t);
+
+    cs_face_anisotropic_viscosity_vector(m,
                                          fvq,
                                          cs_glob_space_disc->imvisf,
-                                         mesh_viscv,
-                                         i_visc,
+                                         (cs_real_6_t *)CS_F_(vism)->val,
+                                         (cs_real_33_t *)i_visc,
                                          b_visc);
   }
 
@@ -566,7 +565,6 @@ cs_ale_solve_mesh_velocity(const int   iterns,
   var_cal_opt.istat  = -1;
   var_cal_opt.idifft = -1;
 
-  /* FIXME : do a proper anisotropic version */
   cs_equation_iterative_solve_vector(cs_glob_time_step_options->idtvar,
                                      iterns,
                                      CS_F_(mesh_u)->id,
@@ -587,18 +585,17 @@ cs_ale_solve_mesh_velocity(const int   iterns,
                                      b_visc,
                                      i_visc,
                                      b_visc,
-                                     i_visc,
-                                     b_visc,
-                                     NULL,
-                                     NULL,
-                                     NULL,
-                                     0, /* icvflv */
-                                     NULL,
+                                     NULL, /* i_secvis */
+                                     NULL, /* b_secvis */
+                                     NULL, /* viscel */
+                                     NULL, /* weighf */
+                                     NULL, /* weighb */
+                                     0,    /* icvflv */
+                                     NULL, /* icvfli */
                                      (const cs_real_33_t *)fimp,
                                      smbr,
                                      mshvel,
-                                     NULL);
-
+                                     NULL); /* eswork */
 
   /* Free memory */
   BFT_FREE(smbr);
@@ -628,7 +625,7 @@ cs_ale_solve_mesh_velocity(const int   iterns,
                               (const cs_real_33_t *)gradm,
                               (const cs_real_3_t *)bc_coeffs->a,
                               (const cs_real_33_t *)bc_coeffs->b,
-                              (const cs_real_t *)CS_F_(dt),
+                              (const cs_real_t *)CS_F_(dt)->val,
                               dproj);
 
   /* FIXME : warning if nterup > 1, use itrale ? */

src/alge/cs_face_viscosity.c
@@ -814,169 +814,6 @@ cs_face_anisotropic_viscosity_scalar(const cs_mesh_t               *m,
   BFT_FREE(w2);
 }
 
-
-/*----------------------------------------------------------------------------*/
-/*!
- * \brief Orthotropic diffusion velocity computation.
- *
- * viscf,b = viscosity*surface/distance, in kg/s
- *
- * \f[ viscf,b = (nx^2*visc11\_moy\_face
- *             +  ny^2*visc22\_moy\_face
- *             +  nz^2*visc33\_moy\_face)*surface/distance
- * \f]
- *
- * The viscosity is given by w1, w2, w3
- *
- * \remark There's no need for a reconstruction technique. (To improve if necessary)
- *
- * Please refer to the
- * <a href="../../theory.pdf#visort"><b>visort</b></a> section of the
- * theory guide for more informations.
- *
- * \param[in]     imvisf        Face viscosity computation method
- *                              - 0: arithmetic
- *                              - 1: harmonic
- * \param[in]     w             Viscosity values
- * \param[out]    i_visc        inner face viscosity
- *                               (times surface divided by distance)
- * \param[out]    b_visc        boundary face viscosity
- *                               (surface, must be consistent with flux BCs)
- */
-/*----------------------------------------------------------------------------*/
-
-void
-cs_face_orthotropic_viscosity_vector(const cs_mesh_t               *m,
-                                     cs_mesh_quantities_t          *fvq,
-                                     const int                      imvisf,
-                                     cs_real_3_t          *restrict c_visc,
-                                     cs_real_t            *restrict i_visc,
-                                     cs_real_t            *restrict b_visc)
-{
-  const cs_halo_t  *halo = m->halo;
-  const cs_lnum_t n_cells = m->n_cells;
-  const cs_lnum_t n_cells_ext = m->n_cells_with_ghosts;
-  const cs_lnum_2_t *restrict i_face_cells
-    = (const cs_lnum_2_t *restrict)m->i_face_cells;
-  const cs_lnum_t *restrict b_face_cells
-    = (const cs_lnum_t *restrict)m->b_face_cells;
-  const cs_real_t *restrict i_dist = fvq->i_dist;
-  const cs_real_t *restrict b_dist = fvq->b_dist;
-  const cs_real_t *restrict weight = fvq->weight;
-  const cs_real_t *restrict b_face_surf = fvq->b_face_surf;
-  const cs_real_3_t *restrict i_face_normal
-    = (const cs_real_3_t *restrict)fvq->i_face_normal;
-  const cs_real_t *restrict i_face_surf
-    = (const cs_real_t *restrict)fvq->i_face_surf;
-
-  cs_real_3_t visci, viscj, surf2;
-  cs_real_3_t *c_poro_visc = NULL, *w2 = NULL;
-  cs_real_t pnd;
-
-  /* Porosity fields */
-  cs_field_t *fporo = cs_field_by_name_try("porosity");
-  cs_real_t *porosi = NULL;
-
-  if (cs_glob_porous_model == 1 || cs_glob_porous_model == 2)
-    porosi = fporo->val;
-
-  /* Without porosity */
-  if (porosi == NULL) {
-
-    c_poro_visc = c_visc;
-
-  /* With porosity */
-  } else if (porosi != NULL) {
-
-    BFT_MALLOC(w2, n_cells_ext, cs_real_3_t);
-    for (cs_lnum_t cell_id = 0; cell_id < n_cells; cell_id++) {
-      for (int isou = 0; isou < 3; isou++) {
-        w2[cell_id][isou] = porosi[cell_id]*c_visc[cell_id][isou];
-      }
-    }
-    c_poro_visc = w2;
-
-  }
-
-  /* ---> Periodicity and parallelism treatment */
-  if (halo != NULL) {
-    cs_halo_type_t halo_type = CS_HALO_STANDARD;
-    cs_halo_sync_var_strided(halo, halo_type, (cs_real_t *)c_poro_visc, 3);
-    if (m->n_init_perio > 0)
-      cs_halo_perio_sync_var_sym_tens(halo,
-                                      halo_type,
-                                      (cs_real_t *)c_poro_visc);
-  }
-
-  /* Arithmetic mean */
-  if (imvisf == 0) {
-
-    for (cs_lnum_t face_id = 0; face_id < m->n_i_faces; face_id++) {
-
-      cs_lnum_t ii = i_face_cells[face_id][0];
-      cs_lnum_t jj = i_face_cells[face_id][1];
-
-      for (int kk = 0; kk < 3; kk++) {
-        visci[kk] = c_poro_visc[ii][kk];
-        viscj[kk] = c_poro_visc[jj][kk];
-      }
-
-      for (int isou = 0; isou < 3; isou++)
-        surf2[isou] = cs_math_pow2(i_face_normal[face_id][isou]);
-
-      i_visc[face_id] = 0.5*((visci[0] + viscj[0])*surf2[0]
-                           + (visci[1] + viscj[1])*surf2[1]
-                           + (visci[2] + viscj[2])*surf2[2])
-                           / (i_face_surf[face_id]*i_dist[face_id]);
-    }
-
-  /* Harmonic mean */
-  } else {
-
-    for (cs_lnum_t face_id = 0; face_id < m->n_i_faces; face_id++) {
-
-      cs_lnum_t ii = i_face_cells[face_id][0];
-      cs_lnum_t jj = i_face_cells[face_id][1];
-
-      pnd = weight[face_id];
-
-      for (int kk = 0; kk < 3; kk++) {
-        visci[kk] = c_poro_visc[ii][kk];
-        viscj[kk] = c_poro_visc[jj][kk];
-      }
-
-      for (int isou = 0; isou < 3; isou++)
-        surf2[isou] = cs_math_pow2(i_face_normal[face_id][isou]);
-
-      i_visc[face_id] = (visci[0]*viscj[0]*surf2[0]/(pnd*visci[0] + (1.-pnd)*viscj[0]))
-                      + (visci[1]*viscj[1]*surf2[1]/(pnd*visci[1] + (1.-pnd)*viscj[1]))
-                      + (visci[2]*viscj[2]*surf2[2]/(pnd*visci[2] + (1.-pnd)*viscj[2]))
-                      / (i_face_surf[face_id]*i_dist[face_id]);
-    }
-
-  }
-
-  /* Without porosity */
-  if (porosi == NULL) {
-
-    for (cs_lnum_t face_id = 0; face_id < m->n_b_faces; face_id++)
-      b_visc[face_id] = b_face_surf[face_id];
-
-  /* With porosity */
-  } else if (porosi != NULL) {
-
-    for (cs_lnum_t face_id = 0; face_id < m->n_b_faces; face_id++) {
-
-      cs_lnum_t ii = b_face_cells[face_id];
-
-      b_visc[face_id] = b_face_surf[face_id]*porosi[ii];
-
-    }
-
-    BFT_FREE(w2);
-  }
-}
-
 /*----------------------------------------------------------------------------*/
 
 END_C_DECLS

src/alge/cs_face_viscosity.h
@@ -190,44 +190,6 @@ cs_face_anisotropic_viscosity_scalar(const cs_mesh_t               *m,
                                      cs_real_t            *restrict b_visc);
 
 /*----------------------------------------------------------------------------*/
-/*!
- * \brief Orthotropic diffusion velocity computation.
- *
- * viscf,b = viscosity*surface/distance, in kg/s
- *
- * \f[ viscf,b = (nx^2*visc11\_moy\_face
- *             +  ny^2*visc22\_moy\_face
- *             +  nz^2*visc33\_moy\_face)*surface/distance
- * \f]
- *
- * The viscosity is given by w1, w2, w3
- *
- * \remark There's no need for a reconstruction technique. (To improve if necessary)
- *
- * Please refer to the
- * <a href="../../theory.pdf#visort"><b>visort</b></a> section of the
- * theory guide for more informations.
- *
- * \param[in]     imvisf        Face viscosity computation method
- *                              - 0: arithmetic
- *                              - 1: harmonic
- * \param[in]     w             Viscosity values
- * \param[out]    i_visc        inner face viscosity
- *                               (times surface divided by distance)
- * \param[out]    b_visc        boundary face viscosity
- *                               (surface, must be consistent with flux BCs)
- */
-/*----------------------------------------------------------------------------*/
-
-void
-cs_face_orthotropic_viscosity_vector(const cs_mesh_t               *m,
-                                     cs_mesh_quantities_t          *fvq,
-                                     const int                      imvisf,
-                                     cs_real_3_t          *restrict c_visc,
-                                     cs_real_t            *restrict i_visc,
-                                     cs_real_t            *restrict b_visc);
-
-/*----------------------------------------------------------------------------*/
 
 END_C_DECLS
 

src/base/albase.f90
@@ -42,8 +42,6 @@ module albase
   integer, save :: nalinf
   !> maximum number of imlicitation iterations of of the structure displacement
   integer, save :: nalimx
-  !> viscosity type of mesh : isotropic=0, orthotropic=1.
-  integer, save :: iortvm
   !> relative precision of implicitation of the structure displacement
   double precision, save :: epalim
   !> iteration (yes=1, no=0) to initialize ALE

src/base/clsyvt.f90
@@ -114,7 +114,7 @@ double precision velipb(nfabor,ndim), rijipb(nfabor,6)
 
 integer          ifac, ii, isou
 integer          iel, f_dim
-integer          iscal , ivar
+integer          iscal , ivar, idftnp
 
 double precision rnx, rny, rnz, rxnn
 double precision upx, upy, upz, usn
@@ -123,7 +123,7 @@ double precision clsyme
 double precision eloglo(3,3), alpha(6,6)
 double precision srfbnf, rcodcn, hint, visclc, visctc, distbf
 double precision cpp
-double precision vismsh(3), hintv(3)
+double precision vismsh(3), hintv(6), rnn(6), htnn(6)
 double precision visci(3,3), fikis, viscis, distfi
 double precision fcoefa(6), fcoefb(6), fcofaf(6), fcofbf(6), fcofad(6), fcofbd(6)
 
@@ -149,7 +149,7 @@ double precision, dimension(:), pointer :: viscl, visct
 double precision, dimension(:), pointer :: cpro_visma_s
 double precision, dimension(:,:), pointer :: cpro_visma_v
 
-type(var_cal_opt) :: vcopt_rij
+type(var_cal_opt) :: vcopt_uma, vcopt_rij
 
 !===============================================================================
 
@@ -669,9 +669,12 @@ if (iale.eq.1) then
   call field_get_coefaf_v(ivarfl(iuma), cfaale)
   call field_get_coefbf_v(ivarfl(iuma), cfbale)
 
-  if (iortvm.eq.0) then
+  call field_get_key_struct_var_cal_opt(ivarfl(iuma), vcopt_uma)
+  idftnp = vcopt_uma%idften
+
+  if (iand(idftnp, ISOTROPIC_DIFFUSION).ne.0) then
     call field_get_val_s(ivisma, cpro_visma_s)
-  else
+  else if (iand(idftnp, ANISOTROPIC_LEFT_DIFFUSION).ne.0) then
     call field_get_val_v(ivisma, cpro_visma_v)
   endif
 
@@ -689,19 +692,16 @@ if (iale.eq.1) then
       srfbnf = surfbn(ifac)
 
       ! --- Physical properties
-      if (iortvm.eq.0) then
-        vismsh(1) = cpro_visma_s(iel)
-        vismsh(2) = cpro_visma_s(iel)
-        vismsh(3) = cpro_visma_s(iel)
-      else
-        vismsh(1) = cpro_visma_v(1,iel)
-        vismsh(2) = cpro_visma_v(2,iel)
-        vismsh(3) = cpro_visma_v(3,iel)
+      if (iand(idftnp, ISOTROPIC_DIFFUSION).ne.0) then
+        do ii = 1, 3
+          hintv(ii) = cpro_visma_s(iel)/distbf
+          hintv(ii+3) = 0.d0
+        enddo
+      else if (iand(idftnp, ANISOTROPIC_LEFT_DIFFUSION).ne.0) then
+        do ii = 1, 6
+          hintv(ii) = cpro_visma_v(ii,iel)/distbf
+        enddo
       endif
-
-      hintv(1) = vismsh(1)/distbf
-      hintv(2) = vismsh(2)/distbf
-      hintv(3) = vismsh(3)/distbf
 
       ! Unit normal
       rnx = surfbo(1,ifac)/srfbnf
@@ -731,16 +731,23 @@ if (iale.eq.1) then
       cfaale(2,ifac) = 0.d0
       cfaale(3,ifac) = 0.d0
 
-      cfbale(1,1,ifac) = hintv(1)*rnx**2
-      cfbale(2,2,ifac) = hintv(2)*rny**2
-      cfbale(3,3,ifac) = hintv(3)*rnz**2
+      rnn(1) = rnx**2
+      rnn(2) = rny**2
+      rnn(3) = rnz**2
+      rnn(4) = rnx*rny
+      rnn(5) = rny*rnz
+      rnn(6) = rnx*rnz
 
-      cfbale(1,2,ifac) = hintv(1)*rnx*rny
-      cfbale(2,1,ifac) = hintv(2)*rny*rnx
-      cfbale(1,3,ifac) = hintv(1)*rnx*rnz
-      cfbale(3,1,ifac) = hintv(3)*rnz*rnx
-      cfbale(2,3,ifac) = hintv(2)*rny*rnz
-      cfbale(3,2,ifac) = hintv(3)*rnz*rny
+      call symmetric_matrix_product(hintv, rnn, htnn)
+      cfbale(1,1,ifac) = htnn(1)
+      cfbale(2,2,ifac) = htnn(2)
+      cfbale(3,3,ifac) = htnn(3)
+      cfbale(1,2,ifac) = htnn(4)
+      cfbale(2,1,ifac) = htnn(4)
+      cfbale(1,3,ifac) = htnn(6)
+      cfbale(3,1,ifac) = htnn(6)
+      cfbale(2,3,ifac) = htnn(5)
+      cfbale(3,2,ifac) = htnn(5)
 
     endif
   enddo

src/base/condli.f90
@@ -3080,9 +3080,9 @@ if (iale.eq.1) then
   call field_get_coefaf_v(ivarfl(iuma), cfaale)
   call field_get_coefbf_v(ivarfl(iuma), cfbale)
 
-  if (iortvm.eq.0) then
+  if (iand(vcopt%idften, ISOTROPIC_DIFFUSION).ne.0) then
     call field_get_val_s(ivisma, cpro_visma_s)
-  else
+  elseif (iand(vcopt%idften, ANISOTROPIC_DIFFUSION).ne.0) then
     call field_get_val_v(ivisma, cpro_visma_v)
   endif
 
@@ -3090,14 +3090,25 @@ if (iale.eq.1) then
 
     iel = ifabor(ifac)
     distbf = distb(ifac)
-    srfbn2 = surfbn(ifac)**2
-    if (iortvm.eq.0) then
-      hint = cpro_visma_s(iel)/distbf
-    else
-      hint = ( cpro_visma_v(1,iel)*surfbo(1,ifac)**2    &
-             + cpro_visma_v(2,iel)*surfbo(2,ifac)**2    &
-             + cpro_visma_v(3,iel)*surfbo(3,ifac)**2 )  &
-           /distbf/srfbn2
+
+    if (iand(vcopt%idften, ISOTROPIC_DIFFUSION).ne.0) then
+
+      hintt(1) = cpro_visma_s(iel)/distbf
+      hintt(2) = cpro_visma_s(iel)/distbf
+      hintt(3) = cpro_visma_s(iel)/distbf
+      hintt(4) = 0.d0
+      hintt(5) = 0.d0
+      hintt(6) = 0.d0
+
+    elseif (iand(vcopt%idften, ANISOTROPIC_DIFFUSION).ne.0) then
+
+      hintt(1) = cpro_visma_v(1,iel)/distbf
+      hintt(2) = cpro_visma_v(2,iel)/distbf
+      hintt(3) = cpro_visma_v(3,iel)/distbf
+      hintt(4) = cpro_visma_v(4,iel)/distbf
+      hintt(5) = cpro_visma_v(5,iel)/distbf
+      hintt(6) = cpro_visma_v(6,iel)/distbf
+
     endif
 
     ! Dirichlet Boundary Conditions
@@ -3112,10 +3123,10 @@ if (iale.eq.1) then
       hextv(2) = rcodcl(ifac,ivma,2)
       hextv(3) = rcodcl(ifac,iwma,2)
 
-      call set_dirichlet_vector &
+      call set_dirichlet_vector_aniso &
          ( claale(:,ifac)  , cfaale(:,ifac)  ,             &
            clbale(:,:,ifac), cfbale(:,:,ifac),             &
-           pimpv           , hint            , hextv )
+           pimpv           , hintt           , hextv )
 
     ! Neumann Boundary Conditions
     !----------------------------
@@ -3128,10 +3139,10 @@ if (iale.eq.1) then
       qimpv(2) = rcodcl(ifac,ivma,3)
       qimpv(3) = rcodcl(ifac,iwma,3)
 
-      call set_neumann_vector &
+      call set_neumann_vector_aniso &
          ( claale(:,ifac)  , cfaale(:,ifac)  ,             &
            clbale(:,:,ifac), cfbale(:,:,ifac),             &
-           qimpv           , hint )
+           qimpv           , hintt )
 
 
     ! Convective Boundary Conditions
@@ -3148,10 +3159,10 @@ if (iale.eq.1) then
       pimpv(3) = rcodcl(ifac,iwma,1)
       cflv(3) = rcodcl(ifac,iwma,2)
 
-      call set_convective_outlet_vector &
+      call set_convective_outlet_vector_aniso &
          ( claale(:,ifac)  , cfaale(:,ifac)  ,             &
            clbale(:,:,ifac), cfbale(:,:,ifac),             &
-           pimpv           , cflv            , hint )
+           pimpv           , cflv            , hintt )
 
     endif
 

src/base/cs_ale.h
@@ -105,14 +105,14 @@ cs_ale_update_mesh(const int           itrale,
  * so that it can be used to update mass fluxes (due to mesh displacement).
  *
  * \param[in]       iterns        Navier-Stokes iteration number
- * \param[in]       ialtyb        Type of boundary for ALE
  * \param[in]       ndircl        Number of Dirichlet BCs for mesh velocity
+ * \param[in]       impale        Indicator for fixed node displacement
+ * \param[in]       ialtyb        Type of boundary for ALE
  */
 /*----------------------------------------------------------------------------*/
 
 void
 cs_ale_solve_mesh_velocity(const int   iterns,
-                           const int   iortvm,
                            const int   ndircl,
                            const int  *impale,
                            const int  *ialtyb);

src/base/cs_boundary_conditions.h
@@ -361,6 +361,62 @@ cs_boundary_conditions_set_convective_outlet_scalar(cs_real_t *coefa ,
                                                     cs_real_t  hint);
 
 /*----------------------------------------------------------------------------*/
+/*!
+ * \brief Set Dirichlet BC for a vector for a given face with left anisotropic
+ *        diffusion.
+ *
+ * \param[out]  a      explicit BC coefficient for gradients
+ * \param[out]  af     explicit BC coefficient for diffusive flux
+ * \param[out]  b      implicit BC coefficient for gradients
+ * \param[out]  bf     implicit BC coefficient for diffusive flux
+ * \param[in]   pimpv  dirichlet value to impose
+ * \param[in]   hintt  internal exchange coefficient
+ * \param[in]   hextv  external exchange coefficient
+ *                     (assumed infinite/ignored if < 0)
+ */
+/*----------------------------------------------------------------------------*/
+
+inline static void
+cs_boundary_conditions_set_dirichlet_vector_aniso(cs_real_3_t   *a,
+                                                  cs_real_3_t   *af,
+                                                  cs_real_33_t  *b,
+                                                  cs_real_33_t  *bf,
+                                                  cs_real_3_t    pimpv,
+                                                  cs_real_6_t    hintt,
+                                                  cs_real_3_t    hextv)
+{
+  for (int isou = 0 ; isou < 3 ; isou++) {
+    if (fabs(hextv[isou]) > 0.5*cs_math_infinite_r) {
+
+      /* Gradient BCs */
+      *a[isou] = pimpv[isou];
+      for (int jsou = 0 ; jsou < 3 ; jsou++)
+        *b[isou][jsou] = 0.;
+
+    } else {
+
+      cs_exit(1);
+
+    }
+  }
+
+  /* Flux BCs */
+  cs_math_sym_33_3_product(hintt, pimpv, *af);
+  for (int isou = 0 ; isou < 3 ; isou++)
+    *af[isou] = -*af[isou];
+
+  *bf[0][0] = hintt[0];
+  *bf[1][1] = hintt[1];
+  *bf[2][2] = hintt[2];
+  *bf[0][1] = hintt[3];
+  *bf[1][0] = hintt[3];
+  *bf[1][2] = hintt[4];
+  *bf[2][1] = hintt[4];
+  *bf[0][2] = hintt[5];
+  *bf[2][0] = hintt[5];
+}
+
+/*----------------------------------------------------------------------------*/
 
 END_C_DECLS
 

commit 3faea90e2b66c3e2f59ebf501f6d77cd79fa6866

Fix bug in orthotropic diffusion for ALE introduced in commit d0274e8 (2018-10-25).

src/base/cs_ale.c
@@ -503,7 +503,7 @@ cs_ale_solve_mesh_velocity(const int   iterns,
         for (int isou = 0; isou < 3; isou++)
           hintt[isou] = CS_F_(vism)->val[cell_id] / distbf;
       } else if (idftnp & CS_ANISOTROPIC_LEFT_DIFFUSION) {
-          for (int isou  = 0; isou < 5; isou++)
+          for (int isou = 0; isou < 6; isou++)
             hintt[isou] = CS_F_(vism)->val[6*cell_id+isou] / distbf;
      }
 