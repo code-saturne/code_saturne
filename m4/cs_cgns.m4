code-saturne/code_saturne

commit 59e6180353ce87236cce1c0ca059dced2691d708

Add detection of CGNS from SALOME builds using CGNS_ROOT_DIR.

m4/cs_cgns.m4
@@ -37,10 +37,12 @@ AC_ARG_WITH(cgns,
             [if test "x$withval" = "x"; then
                with_cgns=yes
              elif test "x$withval" = "xsalome"; then
-               if test -z "$CGNSHOME"; then
-                 AC_MSG_FAILURE([no SALOME path information for CGNS (needed by --with-cgns=salome)!])
-               else
+               if test "x$CGNSHOME" = "x"; then
                  with_cgns=$CGNSHOME
+               elif test "x$CGNS_ROOT_DIR" = "x"; then
+                 with_cgns=$CGNS_ROOT_DIR
+               else
+                 AC_MSG_FAILURE([no SALOME path information for CGNS (needed by --with-cgns=salome)!])
                fi
              fi],
             [with_cgns=check])

commit 784b703e2bd6626a2c3044066419db7e580a7699

Fix CGNS from SALOME_CFD detection, broken by commit 59e6180 (Sep 20 2018).

m4/cs_cgns.m4
@@ -37,9 +37,9 @@ AC_ARG_WITH(cgns,
             [if test "x$withval" = "x"; then
                with_cgns=yes
              elif test "x$withval" = "xsalome"; then
-               if test "x$CGNSHOME" = "x"; then
+               if test "x$CGNSHOME" != "x"; then
                  with_cgns=$CGNSHOME
-               elif test "x$CGNS_ROOT_DIR" = "x"; then
+               elif test "x$CGNS_ROOT_DIR" != "x"; then
                  with_cgns=$CGNS_ROOT_DIR
                else
                  AC_MSG_FAILURE([no SALOME path information for CGNS (needed by --with-cgns=salome)!])