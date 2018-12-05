code-saturne/code_saturne

commit 2a6fac7d55525b41ed1e5a6ac7b115b13739951a

CFD_STUDY: porting into SALOME9, Python3 and New Salome Study management.

salome/cfd_study/src/CFDSTUDYGUI/CFDSTUDYGUI_DataModel.py
@@ -82,9 +82,9 @@ from omniORB import CORBA
 #-------------------------------------------------------------------------------
 # Salome modules
 #-------------------------------------------------------------------------------
-
-from SALOME_NamingServicePy import *
-from LifeCycleCORBA import *
+from omniORB import CORBA
+from SALOME_NamingServicePy import SALOME_NamingServicePy_i
+from LifeCycleCORBA import LifeCycleCORBA
 import SALOMEDS
 import SALOMEDS_Attributes_idl
 
@@ -95,7 +95,7 @@ import salome
 # Application modules
 #-------------------------------------------------------------------------------
 
-from CFDSTUDYGUI_Commons import CFD_Code, BinCode, Trace, sgPyQt, sg
+from CFDSTUDYGUI_Commons import CFD_Code, BinCode, Trace, sg
 from CFDSTUDYGUI_Commons import CaseInProcessStart, CaseInProcessEnd
 from CFDSTUDYGUI_Commons import CFD_Saturne, CFD_Neptune
 import CFDSTUDYGUI_SolverGUI
@@ -295,68 +295,66 @@ icon_collection[dict_object["CouplingStudy"]]         = "CFDSTUDY_STUDY_OBJ_ICON
 ObjectTR = QObject()
 
 #-------------------------------------------------------------------------------
-# CORBA
-#-------------------------------------------------------------------------------
-
-# init ORB
-orb = CORBA.ORB_init([''], CORBA.ORB_ID)
-
-# create naming service instance
-naming_service = SALOME_NamingServicePy_i(orb)
-
-# create life cycle CORBA instance
-lcc = LifeCycleCORBA(orb)
-
-#-------------------------------------------------------------------------------
-# Get Study manager
-#-------------------------------------------------------------------------------
-# Interface to manipulate SALOME Studies. You will find in this interface the
-# methods to create, open, close, and save a SALOME Study. Since a SALOME
-# session is multi-document, you will also find methods allowing to navigate
-# through a collection of SALOME Studies currently present in a session.
-
-obj = naming_service.Resolve('/myStudyManager')
-studyManager = obj._narrow(SALOMEDS.StudyManager)
-
-#-------------------------------------------------------------------------------
 # Internal methods
 #-------------------------------------------------------------------------------
 
-def _getEngine():
-    """
-    Gets component engine.
+###
+# Get ORB reference
+###
+__orb__ = None
+def getORB():
+    global __orb__
+    if __orb__ is None:
+        __orb__ = CORBA.ORB_init( [''], CORBA.ORB_ID )
+        pass
+    return __orb__
 
-    @return: engine part of the module CFDSTUDY.
-    @rtype: C{Engine}
-    """
-    import CFDSTUDY_ORB
-    engine = lcc.FindOrLoadComponent("FactoryServerPy", __MODULE_NAME__)
-    return engine
+#--------------------------------------------------------------------------
+###
+# Get naming service instance
+###
+__naming_service__ = None
+def getNS():
+    global __naming_service__
+    if __naming_service__ is None:
+        __naming_service__ = SALOME_NamingServicePy_i( getORB() )
+        pass
+    return __naming_service__
+
+#--------------------------------------------------------------------------
+##
+# Get life cycle CORBA instance
+##
+__lcc__ = None
+def getLCC():
+    global __lcc__
+    if __lcc__ is None:
+        __lcc__ = LifeCycleCORBA( getORB() )
+        pass
+    return __lcc__
 
 
+#--------------------------------------------------------------------------
+##
+# Get study
+###
+__study__ = None
 def _getStudy():
-    """
-    Gets active SALOME Study. C{Study} is a warehouse of data. It can be understood as a document,
-    the data storage of the upper level. SALOME Study contains data of multiple components, it's a
-    single document for all components. Most of operations on a SALOME Study object are handled by
-    C{StudyManager} and C{StudyBuilder} interfaces.
+    global __study__
+    if __study__ is None:
+        obj = getNS().Resolve( '/Study' )
+        __study__ = obj._narrow( SALOMEDS.Study )
+        pass
+    return __study__
 
-    @return: active Study.
-    @rtype: C{Study}
-    """
-    # getStudyId() -> get study associated to component instance
-    # return -1: not initialised (Internal Error)
-    #         0: multistudy component instance
-    #        >0: study id associated to this instance
-
-    studyId = sgPyQt.getStudyId()
-    study = studyManager.GetStudyByID(studyId)
-    return study
-
-
-def _getStudy_Id(studyName) :
-    s = studyManager.GetStudyByName(studyName)
-    return s._get_StudyId()
+#--------------------------------------------------------------------------
+__engine__ = None
+def _getEngine():
+    global __engine__
+    if __engine__ is None:
+        __engine__ = getLCC().FindOrLoadComponent( "FactoryServerPy", __MODULE_NAME__() )
+        pass
+    return __engine__
 
 
 def _getNewBuilder():
@@ -403,6 +401,7 @@ def _findOrCreateComponent():
     @return: the root C{SObject} for the Object browser representation.
     @rtype: C{SObject}
     """
+    log.debug("_findOrCreateComponent")
     study = _getStudy()
     father = study.FindComponent(__MODULE_NAME__)
     if father is None:
@@ -419,7 +418,6 @@ def _findOrCreateComponent():
             builder.DefineComponentInstance(father, _getEngine())
         except:
             pass
-
     return father
 
 def _SetCaseLocation(theCasePath):
@@ -428,7 +426,6 @@ def _SetCaseLocation(theCasePath):
     builder       = study.NewBuilder()
     father        = _findOrCreateComponent()
     theStudyPath  = os.path.dirname(theCasePath)
-
     aCaseName      = os.path.basename(theCasePath)
     studyObject   = FindStudyByPath(theStudyPath)
     if studyObject == None:
@@ -478,7 +475,6 @@ def _SetStudyLocation(theStudyPath, theCaseNames,theCreateOpt,
     builder = study.NewBuilder()
     father  = _findOrCreateComponent()
     studyObject = FindStudyByPath(theStudyPath)
-
     if studyObject == None:
         #obtain name and dir for new study
         lst = os.path.split(theStudyPath)
@@ -534,6 +530,7 @@ def replaceOrInsertCouplingPath(The_coupling_parameters_path):
     """
     coupling_parameters.py file is updated according to syrthes path needed
     """
+    log.debug("replaceOrInsertCouplingPath")
     f = open(The_coupling_parameters_path,"r")
     l = f.readlines()
     f.close()
@@ -605,6 +602,7 @@ def _CallCreateScript(theStudyPath, isCreateStudy, theCaseNames,
 
 def updateCasePath(theCasePath):
 
+    log.debug("updateCasePath")
     mess = ""
     scrpt, c ,mess = BinCode()
     if mess == "" :
@@ -625,6 +623,7 @@ def _UpdateStudy():
     """
     Updates CFD study tree of data from the root.
     """
+    log.debug("_UpdateStudy")
     study   = _getStudy()
     component = study.FindComponent(__MODULE_NAME__)
     if component == None:
@@ -645,13 +644,12 @@ def UpdateSubTree(theObject=None):
     """
     log.debug("UpdateSubTree")
     if theObject != None:
-        log.debug("_RebuildTreeRecursively -> path: %s" % _GetPath(theObject))
+        log.debug("UpdateSubTree -> path: %s" % _GetPath(theObject))
         _RebuildTreeRecursively(theObject)
     else:
         _UpdateStudy()
     # --- update object browser from a thread different of the main thread is not safe !
-    studyId = sgPyQt.getStudyId()
-    sgPyQt.updateObjBrowser(studyId,1)
+    sg.updateObjBrowser()
 
 def closeCFDStudyTree(theObject):
     """
@@ -842,6 +840,7 @@ def _CreateItem(theFather,theNewName) :
 def getNameCodeFromXmlCasePath(XMLCasePath) :
     """
     """
+    log.debug("getNameCodeFromXmlCasePath")
     code = ""
     if os.path.isfile(XMLCasePath):
         fd = os.open(XMLCasePath,os.O_RDONLY)
@@ -881,13 +880,14 @@ def _FillObject(theObject, theParent, theBuilder):
     @type theBuilder: C{SUIT_Study}
     @param theBuilder: C{SObject} constructor for create an attribut.
     """
+    log.debug("_FillObject")
     attr = theBuilder.FindOrCreateAttribute(theParent, "AttributeLocalID")
     parentId = attr.Value()
     name = theObject.GetName()
     objectId = dict_object["OtherFile"]
     path = os.path.join(_GetPath(theParent), name)
-    #log.debug("_FillObject: %s" % name)
-
+    log.debug("_FillObject Object Name : %s" % name)
+    log.debug("_FillObject Parent Name : %s" % theParent.GetName())
     # Parent is study
     if parentId == dict_object["Study"] or parentId == dict_object["CouplingStudy"]:
         if Trace(): print("_FillObject : parent is Study ", theParent.GetName())
@@ -1405,7 +1405,7 @@ def GetCase(theObject):
         attr = builder.FindOrCreateAttribute(cur, "AttributeLocalID")
         if Trace():
             print("attr:",attr)
-            print("Value", attr.Value())
+            print("Value for Case", attr.Value())
         value = attr.Value()
         if value == dict_object["Case"]:
             return cur
@@ -1474,6 +1474,7 @@ def FindStudyByPath(theStudyPath):
     @return: the CFD study.
     @rtype: C{SObject} or C{None}
     """
+    log.debug("FindStudyByPath")
     component = _getComponent()
     if component == None:
         return None
@@ -1502,6 +1503,7 @@ def FindCaseByPath(theCasePath):
     @return: the CFD study.
     @rtype: C{SObject} or C{None}
     """
+    log.debug("FindCaseByPath")
     component = _getComponent()
     if component == None:
         return None
@@ -1606,7 +1608,7 @@ def getXmlCaseNameList(theCase):
     while iter.More():
         aName = iter.Value().GetName()
         if aName != "" :
-            if "XML" in subprocess.check_output(["file",_GetPath(iter.Value())]):
+            if "XML" in subprocess.check_output(["file",_GetPath(iter.Value())]).decode():
                 XmlCaseNameList.append(iter.Value().GetName())
         iter.Next()
     return XmlCaseNameList
@@ -1678,16 +1680,15 @@ def getType(theObject):
     return attr.Value()
 
 def hasTheSameType(ListObject):
-
     if ListObject == []:
         return False
     typListBool = True
     typListBoolRESUSub = None
     typList = []
-    typ     = getType(ListObject[0])
+    typ     = getType(list(ListObject)[0])
     typList.append(typ)
     if len(ListObject)> 1:
-        for SObject in ListObject[1:]:
+        for SObject in list(ListObject)[1:]:
             typListBool = typListBool and getType(SObject) == typ
             typList.append(getType(SObject))
         if not typListBool:
@@ -1905,6 +1906,7 @@ def setCaseInProcess(theCasePath, isInProcess):
     @type isInProcess: C{True} or C{False}
     @param isInProcess: if C{True}, shows the I{CaseInProcess} icon.
     """
+    log.debug("setCaseInProcess")
     aStudyPath, aCaseName = os.path.split(theCasePath)
     aStudyObj = FindStudyByPath(aStudyPath)
     if not aStudyPath:
@@ -1968,7 +1970,6 @@ def getOrLoadObject(item):
     object = item.GetObject()
     if object is None: # the engine has not been loaded yet
         sComponent = item.GetFatherComponent()
-        #self.loadComponentEngine(sComponent)
         study   = _getStudy()
         builder = study.NewBuilder()
         engine = _getEngine()
@@ -2010,7 +2011,6 @@ def getMeshFromGroup(meshGroupItem):
     group = None
     meshItem = None
     obj = getOrLoadObject(meshGroupItem)
-    #obj = self.editor.getOrLoadObject(meshGroupItem) # version 515
 
     if obj is not None:
         group = obj._narrow(SMESH.SMESH_GroupBase)

salome/cfd_study/src/CFDSTUDYGUI/CFDSTUDYGUI.py
@@ -80,8 +80,7 @@ log.setLevel(logging.NOTSET)
 # Global definitions
 #-------------------------------------------------------------------------------
 __MODULE_NAME__ = "CFDSTUDY"
-studyId = salome.myStudyId
-d_activation = {}
+
 
 # Desktop manager: instance of CFDSTUDYGUI_DesktopMgr class to store the SALOME Workspace
 _DesktopMgr = CFDSTUDYGUI_DesktopMgr.CFDSTUDYGUI_DesktopMgr()
@@ -133,12 +132,12 @@ def windows():
     return winMap
 
 
-def closeStudy(aStudyId) :
-    """
-    This method is called when salome study is closed (Salome desktop button File -> close -> close w/o saving button) and Salome Main window desktop is already available
-    """
-    if salome_version.getVersion() >= '7.5.0' :
-        CFDSTUDYGUI_SolverGUI._c_CFDGUI.cleanAllDock(sgPyQt.getDesktop())
+#MPdef closeStudy(aStudyId) :
+#MP    """
+#MP    This method is called when salome study is closed (Salome desktop button File -> close -> close w/o saving button) and Salome Main window desktop is already available
+#MP    """
+#MP    if salome_version.getVersion() >= '7.5.0' :
+#MP        CFDSTUDYGUI_SolverGUI._c_CFDGUI.cleanAllDock(sgPyQt.getDesktop())
 
 
 def views():
@@ -205,9 +204,9 @@ def activate():
     @return: C{True} only if the activation is successful.
     """
     log.debug("activate")
-    global d_activation, studyId
+#MP    global d_activation, studyId
     dsk = sgPyQt.getDesktop()
-    studyId = sgPyQt.getStudyId()
+#MP    studyId = sgPyQt.getStudyId()
     dsk.setTabPosition(Qt.RightDockWidgetArea,QTabWidget.South)
     dsk.setTabPosition(Qt.LeftDockWidgetArea,QTabWidget.South)
 
@@ -218,42 +217,42 @@ def activate():
     # instance of the CFDSTUDYGUI_ActionsHandler class for the current desktop
     ActionHandler = _DesktopMgr.getActionHandler(dsk)
 
-    if studyId not in list(d_activation.keys()):
-        d_activation[studyId] = 1
+#MP    if studyId not in list(d_activation.keys()):
+#MP        d_activation[studyId] = 1
 
-    if d_activation[studyId] == 1:
-        d_activation[studyId] = 0
-        env_saturne, mess1 = CheckCFD_CodeEnv(CFD_Saturne)
-        env_neptune, mess2 = CheckCFD_CodeEnv(CFD_Neptune)
+#MP    if d_activation[studyId] == 1:
+#MP        d_activation[studyId] = 0
+    env_saturne, mess1 = CheckCFD_CodeEnv(CFD_Saturne)
+    env_neptune, mess2 = CheckCFD_CodeEnv(CFD_Neptune)
 
-        log.debug("activate -> env_saturne = %s" % env_saturne)
-        log.debug("activate -> env_neptune = %s" % env_neptune)
+    log.debug("activate -> env_saturne = %s" % env_saturne)
+    log.debug("activate -> env_neptune = %s" % env_neptune)
 
-        if not env_saturne and not env_neptune:
-            QMessageBox.critical(ActionHandler.dskAgent().workspace(),
-                                 "Error", mess1, QMessageBox.Ok, 0)
-            QMessageBox.critical(ActionHandler.dskAgent().workspace(),
-                                 "Error", mess2, QMessageBox.Ok, 0)
-            d_activation[studyId] = 1
+    if not env_saturne and not env_neptune:
+        QMessageBox.critical(ActionHandler.dskAgent().workspace(),
+                             "Error", mess1, QMessageBox.Ok, 0)
+        QMessageBox.critical(ActionHandler.dskAgent().workspace(),
+                             "Error", mess2, QMessageBox.Ok, 0)
+#MP        d_activation[studyId] = 1
+        return False
+
+    if env_neptune:
+        if mess2 != "":
+            mess = cfdstudyMess.trMessage(ObjectTR.tr("CFDSTUDY_INVALID_ENV"),[]) + " ; "+ mess2
+            cfdstudyMess.aboutMessage(mess)
+#MP            d_activation[studyId] = 1
             return False
+        else:
+            ActionHandler.DialogCollector.InfoDialog.setCode(env_saturne, env_neptune)
 
-        if env_neptune:
-            if mess2 != "":
-                mess = cfdstudyMess.trMessage(ObjectTR.tr("CFDSTUDY_INVALID_ENV"),[]) + " ; "+ mess2
-                cfdstudyMess.aboutMessage(mess)
-                d_activation[studyId] = 1
-                return False
-            else:
-                ActionHandler.DialogCollector.InfoDialog.setCode(env_saturne, env_neptune)
-
-        elif env_saturne:
-            if mess1 != "":
-                mess = cfdstudyMess.trMessage(ObjectTR.tr("CFDSTUDY_INVALID_ENV"),[]) + " ; "+ mess2
-                cfdstudyMess.aboutMessage(mess)
-                d_activation[studyId] = 1
-                return False
-            else:
-                ActionHandler.DialogCollector.InfoDialog.setCode(env_saturne, False)
+    elif env_saturne:
+        if mess1 != "":
+            mess = cfdstudyMess.trMessage(ObjectTR.tr("CFDSTUDY_INVALID_ENV"),[]) + " ; "+ mess2
+            cfdstudyMess.aboutMessage(mess)
+#MP            d_activation[studyId] = 1
+            return False
+        else:
+            ActionHandler.DialogCollector.InfoDialog.setCode(env_saturne, False)
 
     ActionHandler._SalomeSelection.currentSelectionChanged.connect(ActionHandler.updateActions)
 
@@ -328,8 +327,8 @@ def createPopupMenu(popup, context):
                             dictSobj[sobj] = id
 
         if dictSobj != {}:
-            if CFDSTUDYGUI_DataModel.isASmeshListObject(dictSobj.keys()) :
-                for sobj in dictSobj.keys():
+            if CFDSTUDYGUI_DataModel.isASmeshListObject(list(dictSobj.keys())) :
+                for sobj in list(dictSobj.keys()):
                     if sobj.GetFatherComponent().GetName() == "Mesh":
                         if CFDSTUDYGUI_DataModel.getMeshFromMesh(sobj) == None:
                             meshGroupObject,group = CFDSTUDYGUI_DataModel.getMeshFromGroup(sobj)
@@ -342,9 +341,9 @@ def createPopupMenu(popup, context):
                             ActionHandler.customPopup(id, popup)
                             popup.removeAction(ActionHandler.commonAction(CFDSTUDYGUI_ActionsHandler.DisplayOnlyGroupMESHAction))
 
-            elif CFDSTUDYGUI_DataModel.isACFDSTUDYListObject(dictSobj.keys()):
-                if CFDSTUDYGUI_DataModel.hasTheSameType(dictSobj.keys()):
-                    for sobj in dictSobj.keys():
+            elif CFDSTUDYGUI_DataModel.isACFDSTUDYListObject(list(dictSobj.keys())):
+                if CFDSTUDYGUI_DataModel.hasTheSameType(list(dictSobj.keys())):
+                    for sobj in list(dictSobj.keys()):
                         id = dictSobj[sobj]
                         ActionHandler.customPopup(id, popup)
                         fathername = sobj.GetFather().GetName()

commit 724545b098d7d16eff6bc9aafce54a18ebd896fc

Fix an error introduced in commit 2a6fac7d (Nov. 27).

salome/cfd_study/src/CFDSTUDYGUI/CFDSTUDYGUI_DataModel.py
@@ -352,7 +352,7 @@ __engine__ = None
 def _getEngine():
     global __engine__
     if __engine__ is None:
-        __engine__ = getLCC().FindOrLoadComponent( "FactoryServerPy", __MODULE_NAME__() )
+        __engine__ = getLCC().FindOrLoadComponent( "FactoryServerPy", __MODULE_NAME__ )
         pass
     return __engine__
 