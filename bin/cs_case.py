code-saturne/code_saturne

commit c890faf6e46ef2b18e01da96f5c0203bbf0e3a8b

Scripts: factor detection of case structure.

bin/cs_case.py
@@ -45,28 +45,6 @@ homard_prefix = None
 # Utility functions
 #===============================================================================
 
-def adaptation(adaptation, saturne_script, case_dir):
-
-    """
-    Call HOMARD adaptation.
-    """
-
-    cmd = os.path.join(homard_prefix, 'saturne_homard')
-
-    if adaptation == '-help':
-        cmd.append('-help')
-        cs_exec_environment.run_command(cmd)
-        sys.exit(0)
-    else:
-        homard_options = '-v'
-        cmd = cmd + ['-Saturne_Script', saturne_script, case_dir]
-        cmd = cmd + ['-Pilotage_Adaptation', adaptation, homard_options]
-
-    if cs_exec_environment.run_command(cmd) != 0:
-        sys.exit(0)
-
-#-------------------------------------------------------------------------------
-
 def check_exec_dir_stamp(d):
 
     """
@@ -87,6 +65,90 @@ def check_exec_dir_stamp(d):
             retval = d
 
     return retval
+
+#-------------------------------------------------------------------------------
+
+def get_case_dir(case=None, param=None, coupling=None, id=None):
+
+    """
+    Check and return case path based on partial or sub-path.
+    """
+
+    casedir = None
+    staging_dir = None
+    param = None
+    coupling= None
+    data = None
+    src = None
+
+    if coupling:
+
+        # Multiple domain case
+
+        if param and coupling:
+            err_str = 'Error: coupling and param options are incompatible.\n'
+            raise RunCaseError(err_str)
+
+        coupling = os.path.realpath(coupling)
+        if not os.path.isfile(coupling):
+            err_str = 'Error: coupling parameters: ' + coupling + '\n' \
+                      'not found or not a file.\n'
+            raise RunCaseError(err_str)
+
+        if id:
+            cwd = os.path.split(coupling)[0]
+            if os.path.basename(cwd) == str(id):
+                d = os.path.split(cwd)[0]
+                if os.path.basename(d) == 'RESU_COUPLING':
+                    staging_dir = cwd
+
+        if case:
+            casedir = os.path.realpath(case)
+        else:
+            casedir = os.path.split(coupling)[0]
+            if staging_dir:
+                casedir = os.path.split(os.path.split(staging_dir)[0])[0]
+
+    else:
+
+        cwd = os.getcwd()
+
+        # Single domain case
+
+        if param:
+            param = os.path.basename(param)
+            if param != param:
+                datadir = os.path.split(os.path.realpath(param))[0]
+                (casedir, data) = os.path.split(datadir)
+                if data != 'DATA': # inconsistent paramaters location.
+                    casedir = None
+
+        if id:
+            if os.path.basename(cwd) == str(id):
+                d = os.path.split(cwd)[0]
+                if os.path.basename(d) == 'RESU':
+                    staging_dir = cwd
+
+        if case:
+            casedir = os.path.realpath(case)
+            data = os.path.join(casedir, 'DATA')
+            src = os.path.join(casedir, 'SRC')
+        else:
+            casedir = cwd
+            while os.path.basename(casedir):
+                data = os.path.join(casedir, 'DATA')
+                src = os.path.join(casedir, 'SRC')
+                if os.path.isdir(data) and os.path.isdir(src):
+                    break
+                casedir = os.path.split(casedir)[0]
+
+        if not (os.path.isdir(data) and os.path.isdir(src)):
+            casedir = None
+
+    # Return casedir or None
+
+    return casedir, staging_dir
+
 
 #===============================================================================
 # Main class

bin/cs_run.py
@@ -147,90 +147,33 @@ def process_cmd_line(argv, pkg):
     staging_dir = None
     param = None
     coupling= None
-    data = None
-    src = None
     compute_build = None
 
-    if options.coupling:
-
+    if options.coupling and options.param:
         # Multiple domain case
+        cmd_line = sys.argv[0]
+        for arg in sys.argv[1:]:
+            cmd_line += ' ' + arg
+        err_str = 'Error:\n' + cmd_line + '\n' \
+                  '--coupling and -p/--param options are incompatible.\n'
+        sys.stderr.write(err_str)
+        sys.exit(1)
 
-        if options.param:
-            cmd_line = sys.argv[0]
-            for arg in sys.argv[1:]:
-                cmd_line += ' ' + arg
-            err_str = 'Error:\n' + cmd_line + '\n' \
-                      '--coupling and -p/--param options are incompatible.\n'
-            sys.stderr.write(err_str)
-            sys.exit(1)
+    casedir, staging_dir = cs_case.get_case_dir(case=options.case,
+                                                param=options.param,
+                                                coupling=options.coupling,
+                                                id=options.id)
 
-        coupling = os.path.realpath(options.coupling)
-        if not os.path.isfile(coupling):
-            cmd_line = sys.argv[0]
-            for arg in sys.argv[1:]:
-                cmd_line += ' ' + arg
-            err_str = 'Error:\n' + cmd_line + '\n' \
-                      'coupling parameters: ' + options.coupling + '\n' \
-                      'not found or not a file.\n'
-            sys.stderr.write(err_str)
-            sys.exit(1)
+    if casedir == None:
+        cmd_line = sys.argv[0]
+        for arg in sys.argv[1:]:
+            cmd_line += ' ' + arg
+        err_str = 'Error:\n' + cmd_line + '\n' \
+                  'run from directory \"' + str(os.getcwd()) + '\",\n' \
+                  'which does not seem to be inside a case directory.\n'
+        sys.stderr.write(err_str)
 
-        if options.id:
-            cwd = os.path.split(coupling)[0]
-            if os.path.basename(cwd) == str(options.id):
-                d = os.path.split(cwd)[0]
-                if os.path.basename(d) == 'RESU_COUPLING':
-                    staging_dir = cwd
-
-        if options.case:
-            casedir = os.path.realpath(options.case)
-        else:
-            casedir = os.path.split(coupling)[0]
-            if staging_dir:
-                casedir = os.path.split(os.path.split(staging_dir)[0])[0]
-
-    else:
-
-        cwd = os.getcwd()
-
-        # Single domain case
-
-        if options.param:
-            param = os.path.basename(options.param)
-            if param != options.param:
-                datadir = os.path.split(os.path.realpath(options.param))[0]
-                (casedir, data) = os.path.split(datadir)
-                if data != 'DATA': # inconsistent paramaters location.
-                    casedir = None
-
-        if options.id:
-            if os.path.basename(cwd) == str(options.id):
-                d = os.path.split(cwd)[0]
-                if os.path.basename(d) == 'RESU':
-                    staging_dir = cwd
-
-        if options.case:
-            casedir = os.path.realpath(options.case)
-            data = os.path.join(casedir, 'DATA')
-            src = os.path.join(casedir, 'SRC')
-        else:
-            casedir = os.getcwd()
-            while os.path.basename(casedir):
-                data = os.path.join(casedir, 'DATA')
-                src = os.path.join(casedir, 'SRC')
-                if os.path.isdir(data) and os.path.isdir(src):
-                    break
-                casedir = os.path.split(casedir)[0]
-
-        if not (os.path.isdir(data) and os.path.isdir(src)):
-            casedir = None
-            cmd_line = sys.argv[0]
-            for arg in sys.argv[1:]:
-                cmd_line += ' ' + arg
-            err_str = 'Error:\n' + cmd_line + '\n' \
-                      'run from directory \"' + str(os.getcwd()) + '\",\n' \
-                      'which does not seem to be inside a case directory.\n'
-            sys.stderr.write(err_str)
+    param = options.param
 
     # Stages to run (if no filter given, all are done).
 

commit 5bc0cb290922a504c6241824d33a51a3108706f2

Fix coupled runs, broken by commit c890faf6e (Dec 5).

bin/cs_case.py
@@ -76,8 +76,6 @@ def get_case_dir(case=None, param=None, coupling=None, id=None):
 
     casedir = None
     staging_dir = None
-    param = None
-    coupling= None
     data = None
     src = None
 

bin/cs_run.py
@@ -146,7 +146,6 @@ def process_cmd_line(argv, pkg):
     casedir = None
     staging_dir = None
     param = None
-    coupling= None
     compute_build = None
 
     if options.coupling and options.param:
@@ -228,7 +227,7 @@ def process_cmd_line(argv, pkg):
     n_procs = options.nprocs
     n_threads = options.nthreads
 
-    return  (casedir, staging_dir, options.id, param, coupling,
+    return  (casedir, staging_dir, options.id, param, options.coupling,
              options.id_prefix, options.id_suffix, options.suggest_id, force_id,
              n_procs, n_threads, stages, compute_build)
 