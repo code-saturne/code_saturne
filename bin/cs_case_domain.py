code-saturne/code_saturne

commit 44ffb72bcf9de9c268a573a9159803050fee0d74

Add automatic restart selection option.

bin/cs_case_domain.py
@@ -433,6 +433,35 @@ class domain(base_domain):
 
     #---------------------------------------------------------------------------
 
+    def __set_auto_restart__(self):
+        """
+        Select latest valid checkpoint directory for restart, based on name
+        """
+
+        self.restart_input = None
+
+        from cs_exec_environment import get_command_output
+
+        results_dir = os.path.abspath(os.path.join(self.result_dir, '..'))
+        results = os.listdir(results_dir)
+        results.sort(reverse=True)
+        for r in results:
+            m = os.path.join(results_dir, r, 'checkpoint', 'main')
+            if os.path.isfile(m):
+                try:
+                    cmd = self.package.get_io_dump()
+                    cmd += ' --location 0 ' + m
+                    res = get_command_output(cmd)
+                except Exception:
+                    print('checkpoint of result: ' + r + ' does not seem usable')
+                    continue
+                self.restart_input = os.path.join(results_dir, r, 'checkpoint')
+                break
+
+        return
+
+    #---------------------------------------------------------------------------
+
     def for_domain_str(self):
 
         if self.name == None:
@@ -622,7 +651,7 @@ class domain(base_domain):
         Copy data to the execution directory
         """
 
-        err_str = None
+        err_str = ""
 
         # If we are using a previous preprocessing, simply link to it here
         if self.mesh_input:
@@ -660,6 +689,11 @@ class domain(base_domain):
 
         # Restart files
 
+        # Handle automatic case first
+
+        if self.restart_input == '*':
+            self.__set_auto_restart__()
+
         if self.restart_input != None:
 
             restart_input =  os.path.expanduser(self.restart_input)
@@ -667,12 +701,14 @@ class domain(base_domain):
                 restart_input = os.path.join(self.case_dir, restart_input)
 
             if not os.path.exists(restart_input):
-                err_str = restart_input + ' does not exist.\n\n'
+                err_str += restart_input + ' does not exist.\n\n'
             elif not os.path.isdir(restart_input):
-                err_str = restart_input + ' is not a directory.\n\n.'
+                err_str += restart_input + ' is not a directory.\n\n.'
             else:
                 self.symlink(restart_input,
                              os.path.join(self.exec_dir, 'restart'))
+
+            print(' Restart from ' + self.restart_input + '\n')
 
         if self.restart_mesh_input != None and err_str == None:
 
@@ -682,12 +718,14 @@ class domain(base_domain):
                                                   restart_mesh_input)
 
             if not os.path.exists(restart_mesh_input):
-                err_str = restart_mesh_input + ' does not exist.\n\n'
+                err_str += restart_mesh_input + ' does not exist.\n\n'
             elif not os.path.isfile(restart_mesh_input):
-                err_str = restart_mesh_input + ' is not a file.\n\n.'
+                err_str += restart_mesh_input + ' is not a file.\n\n.'
             else:
                 self.symlink(restart_mesh_input,
                              os.path.join(self.exec_dir, 'restart_mesh_input'))
+
+            print(' Restart mesh ' + self.restart_mesh_input + '\n')
 
         # Partition input files
 
@@ -700,8 +738,7 @@ class domain(base_domain):
             if os.path.exists(partition_input):
 
                 if not os.path.isdir(partition_input):
-                    err_str = partition_input + ' is not a directory.'
-                    raise RunCaseError(err_str)
+                    err_str += partition_input + ' is not a directory.\n\n'
                 else:
                     self.symlink(partition_input,
                                  os.path.join(self.exec_dir, 'partition_input'))
@@ -718,9 +755,9 @@ class domain(base_domain):
                 src_path = os.path.join(self.exec_dir, self.param)
                 shutil.copy2(src_path, link_path)
 
-        if err_str:
-            sys.stderr.write(err_str)
+        if len(err_str) > 0:
             self.error = 'data preparation'
+            sys.stderr.write(err_str)
         else:
             try:
                 os.symlink(os.path.join("run_solver.log"), "listing")
@@ -1569,7 +1606,6 @@ class aster_domain(base_domain):
 
 class cathare_domain(domain):
 
-
     #---------------------------------------------------------------------------
 
     def __init__(self,
@@ -1736,11 +1772,11 @@ class cathare_domain(domain):
                     self.symlink(partition_input,
                                  os.path.join(self.exec_dir, 'partition_input'))
 
-    #---------------------------------------------------------------------------
-
 #-------------------------------------------------------------------------------
-class python_domain(base_domain):
 
+# Coupling with a general Python-based code
+
+class python_domain(base_domain):
 
     #---------------------------------------------------------------------------
 
@@ -1754,7 +1790,6 @@ class python_domain(base_domain):
                  n_procs_min = 1,
                  n_procs_max = None,
                  n_threads = 1):
-
 
         base_domain.__init__(self,
                              package,
@@ -1778,8 +1813,6 @@ class python_domain(base_domain):
 
     #---------------------------------------------------------------------------
 
-    #---------------------------------------------------------------------------
-
     def set_case_dir(self, case_dir, staging_dir = None):
 
         base_domain.set_case_dir(self, case_dir, staging_dir)
@@ -1787,8 +1820,6 @@ class python_domain(base_domain):
         self.data_dir = os.path.join(self.case_dir, "DATA")
         self.src_dir  = os.path.join(self.case_dir, "SRC")
         self.result_dir = os.path.join(self.case_dir, "RESU")
-
-    #---------------------------------------------------------------------------
 
     #---------------------------------------------------------------------------
 
@@ -1806,8 +1837,6 @@ class python_domain(base_domain):
 
     #---------------------------------------------------------------------------
 
-    #---------------------------------------------------------------------------
-
     def set_result_dir(self, name, given_dir = None):
 
         if given_dir == None:
@@ -1819,8 +1848,6 @@ class python_domain(base_domain):
 
         if not os.path.isdir(self.result_dir):
             os.makedirs(self.result_dir)
-
-    #---------------------------------------------------------------------------
 
     #---------------------------------------------------------------------------
 
@@ -1838,16 +1865,12 @@ class python_domain(base_domain):
 
     #---------------------------------------------------------------------------
 
-    #---------------------------------------------------------------------------
-
     def preprocess(self):
         """
         Preprocess dummy function: Does nothing for a standard python script
         """
 
         # Nothing to do
-
-    #---------------------------------------------------------------------------
 
     #---------------------------------------------------------------------------
 
@@ -1860,8 +1883,6 @@ class python_domain(base_domain):
 
     #---------------------------------------------------------------------------
 
-    #---------------------------------------------------------------------------
-
     def summary_info(self, s):
         """
         output summary data into file s
@@ -1871,11 +1892,9 @@ class python_domain(base_domain):
 
     #---------------------------------------------------------------------------
 
-    #---------------------------------------------------------------------------
-
     def solver_command(self, **kw):
         """
-        Returns a tuple indicating SYRTHES's working directory,
+        Returns a tuple indicating the script's working directory,
         executable path, and associated command-line arguments.
         """
 
@@ -1899,10 +1918,6 @@ class python_domain(base_domain):
         args += ' --log ' + enquote_arg(self.logfile)
 
         return wd, exec_path, args
-
-    #---------------------------------------------------------------------------
-
-#-------------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------------
 # End

bin/cs_user_scripts.py
@@ -28,45 +28,6 @@ import os
 # Local functions
 #===============================================================================
 
-def domain_auto_restart(domain, n_add):
-    """
-    Select latest valid checkpoint for restart, and
-    create control_file to add n_add time steps.
-    """
-
-    from cs_exec_environment import get_command_output
-
-    results_dir = os.path.abspath(os.path.join(self.result_dir, '..'))
-    results = os.listdir(results_dir)
-    results.sort(reverse=True)
-    for r in results:
-        m = os.path.join(results_dir, r, 'checkpoint', 'main')
-        if os.path.isfile(m):
-            try:
-                cmd = self.package.get_io_dump()
-                cmd += ' --section nbre_pas_de_temps --extract ' + m
-                res = get_command_output(cmd)
-                n_steps = int(get_command_output(cmd))
-            except Exception:
-                print('checkpoint of result: ' + r + ' does not seem usable')
-                continue
-            info_line = 'Restart from iterations ' + str(n_steps)
-            n_steps += n_add
-            info_line += ' to ' + str(n_steps)
-            print(info_line)
-            f = open(os.path.join(self.exec_dir, 'control_file'), 'w')
-            l = ['#Target number of time steps, determined by script\n',
-                 str(n_steps)]
-            f.writelines(l)
-            f.close()
-            domain.restart_input = os.path.join('RESU',
-                                                r,
-                                                'checkpoint')
-            print('using ' + domain.restart_input)
-            break
-
-    return
-
 #===============================================================================
 # Defining parameters for a calculation domain
 #===============================================================================
@@ -78,11 +39,6 @@ def domain_prepare_data_add(domain):
     in DATA and copy of link of restart files as defined by domain).
     """
 
-    # Example: select latest valid checkpoint file for restart
-
-    if False:
-        domain_auto_restart(domain, 200)
-
     return
 
 #-------------------------------------------------------------------------------
@@ -92,16 +48,6 @@ def domain_copy_results_add(domain):
     Additional steps to copy results or cleanup execution directory
     (called at beginning of data copy stage).
     """
-
-    # Example: clean some temporary files
-
-    if False:
-        import fnmatch
-        dir_files = os.listdir(self.exec_dir)
-        tmp_files = (fnmatch.filter(dir_files, '*.tmp')
-                     + fnmatch.filter(dir_files, '*.fort'))
-        for f in tmp_files:
-            os.remove(os.path.join(self.exec_dir, f))
 
     return
 
@@ -194,7 +140,7 @@ def define_domain_parameters(domain):
 
     # Additionnal compiler flags may be passed to the C, C++, or Fortran
     # compilers, and libraries may be added, in case linking of user
-    # subroutines against external libraries is needed.
+    # functions against external libraries is needed.
 
     # Note that compiler flags will be added before the default flags;
     # this helps ensure added search paths have priority, but also implies

gui/Pages/StartRestartForm.ui
@@ -6,7 +6,7 @@
    <rect>
     <x>0</x>
     <y>0</y>
-    <width>517</width>
+    <width>689</width>
     <height>331</height>
    </rect>
   </property>
@@ -139,6 +139,13 @@
          </widget>
         </item>
         <item>
+         <widget class="QRadioButton" name="radioButtonAuto">
+          <property name="text">
+           <string>automatic</string>
+          </property>
+         </widget>
+        </item>
+        <item>
          <spacer>
           <property name="orientation">
            <enum>Qt::Horizontal</enum>
@@ -238,6 +245,19 @@
               <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;mesh_input&lt;/span&gt; or &lt;span style=&quot; font-weight:600;&quot;&gt;mesh_output&lt;/span&gt; from previous run matching the restart files.&lt;/p&gt;&lt;p&gt;If this file is not available (not saved in previous calculation), re-running the previous calculation in &amp;quot;preprocessing only&amp;quot; mode may allow rebuilding it.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
              </property>
             </widget>
+           </item>
+           <item row="2" column="2">
+            <spacer name="horizontalSpacerMesh">
+             <property name="orientation">
+              <enum>Qt::Horizontal</enum>
+             </property>
+             <property name="sizeHint" stdset="0">
+              <size>
+               <width>40</width>
+               <height>0</height>
+              </size>
+             </property>
+            </spacer>
            </item>
           </layout>
          </item>

gui/Pages/StartRestartModel.py
@@ -79,7 +79,7 @@ class StartRestartModel(Model):
     @Variables.noUndo
     def getRestartPath(self):
         """
-        Return restart path if applicable
+        Return restart path if applicable; use '*' for automatic mode.
         """
         node = self.node_start.xmlInitNode('restart', 'path')
         restart = node['path']
@@ -92,7 +92,7 @@ class StartRestartModel(Model):
     @Variables.undoLocal
     def setRestartPath(self, v):
         """
-        Set restart path if applicable
+        Set restart path if applicable; use '*' for automatic mode.
         """
         node = self.node_start.xmlInitNode('restart', 'path')
         if v:

gui/Pages/StartRestartView.py
@@ -237,6 +237,7 @@ class StartRestartView(QWidget, Ui_StartRestartForm):
 
         self.radioButtonYes.clicked.connect(self.slotStartRestart)
         self.radioButtonNo.clicked.connect(self.slotStartRestart)
+        self.radioButtonAuto.clicked.connect(self.slotStartRestart)
         self.toolButton.pressed.connect(self.slotSearchRestartDirectory)
         self.toolButtonRestartMesh.pressed.connect(self.slotSearchRestartMesh)
         self.checkBox.clicked.connect(self.slotFrozenField)
@@ -251,20 +252,25 @@ class StartRestartView(QWidget, Ui_StartRestartForm):
         self.restart_mesh_path = self.model.getRestartMeshPath()
 
         if self.restart_path:
-            if not os.path.isdir(os.path.join(self.case['case_path'],
-                                              self.restart_path)):
-                title = self.tr("WARNING")
-                msg   = self.tr("Invalid path in %s!" % self.restart_path)
-                QMessageBox.warning(self, title, msg)
+            if self.restart_path == '*':
+                self.radioButtonNo.setChecked(False)
+                self.radioButtonYes.setChecked(False)
+                self.radioButtonAuto.setChecked(True)
+            else:
+                if not os.path.isdir(os.path.join(self.case['case_path'],
+                                                  self.restart_path)):
+                    title = self.tr("WARNING")
+                    msg   = self.tr("Invalid path in %s!" % self.restart_path)
+                    QMessageBox.warning(self, title, msg)
 
-            self.radioButtonYes.setChecked(True)
-            self.radioButtonNo.setChecked(False)
-            self.checkBoxRestartMesh.show()
+                self.radioButtonNo.setChecked(False)
+                self.radioButtonYes.setChecked(True)
+                self.radioButtonAuto.setChecked(False)
 
         else:
-            self.radioButtonYes.setChecked(False)
             self.radioButtonNo.setChecked(True)
-            self.checkBoxRestartMesh.hide()
+            self.radioButtonYes.setChecked(False)
+            self.radioButtonAuto.setChecked(False)
 
         self.slotStartRestart()
 
@@ -398,15 +404,27 @@ class StartRestartView(QWidget, Ui_StartRestartForm):
         Input IRESTART Code_Saturne keyword.
         """
         if self.radioButtonYes.isChecked():
-            if not self.restart_path:
+            if not self.restart_path or self.restart_path == '*':
                 self.slotSearchRestartDirectory()
-
+        elif self.radioButtonAuto.isChecked():
+            self.restart_path = '*'
         else:
             self.restart_path = None
 
         if self.restart_path:
             self.model.setRestartPath(self.restart_path)
-            self.radioButtonYes.setChecked(True)
+            if self.restart_path == '*':
+                self.radioButtonYes.setChecked(False)
+                self.radioButtonAuto.setChecked(True)
+                self.labelRestartDir.hide()
+                self.lineEdit.hide()
+                self.toolButton.hide()
+            else:
+                self.radioButtonYes.setChecked(True)
+                self.radioButtonAuto.setChecked(False)
+                self.labelRestartDir.show()
+                self.lineEdit.show()
+                self.toolButton.show()
             self.radioButtonNo.setChecked(False)
             self.frameRestart.show()
             self.lineEdit.setText(self.restart_path)

commit be0ee1d0f48cead346856f6ab3de4110f1732d1e

Fix bug introduced in commit 44ffb72.

bin/cs_case_domain.py
@@ -710,7 +710,7 @@ class domain(base_domain):
 
             print(' Restart from ' + self.restart_input + '\n')
 
-        if self.restart_mesh_input != None and err_str == None:
+        if self.restart_mesh_input != None and err_str == '':
 
             restart_mesh_input =  os.path.expanduser(self.restart_mesh_input)
             if not os.path.isabs(restart_mesh_input):