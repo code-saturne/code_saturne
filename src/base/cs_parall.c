code-saturne/code_saturne

commit 281d0d2946983bde231871fde37cfef7872507dc

Ensure all cs_parall_* operations have a correct default behavior in serial mode.
git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@9683 e44c5d27-f830-0410-8ce8-974f7a764be6

src/base/cs_parall.c
@@ -767,6 +767,13 @@ cs_parall_max_loc_vals(int         n,
   }
 
 #endif
+
+  if (cs_glob_n_ranks == 1) {
+
+    for (int i = 0; i < n; i++)
+      max_loc_vals[i] = max[i];
+
+  }
 }
 
 /*----------------------------------------------------------------------------*/
@@ -805,12 +812,19 @@ cs_parall_min_loc_vals(int         n,
   }
 
 #endif
+
+  if (cs_glob_n_ranks == 1) {
+
+    for (int i = 0; i < n; i++)
+      min_loc_vals[i] = min[i];
+
+  }
 }
 
 /*----------------------------------------------------------------------------*/
 /*!
- * \brief Given an (id, rank, value) tuple, return the local id, rank,
- *        and value corresponding to the global minimum value.
+ * \brief Given an (id, rank, value) tuple, return the local id and rank
+ *        corresponding to the global minimum value.
  *
  * \param[in, out]   elt_id   element id for which the value is the smallest
  *                            (local in, global out)
@@ -918,6 +932,15 @@ cs_parall_allgather_r(int        n_elts,
   }
 
 #endif
+
+  if (cs_glob_n_ranks == 1) {
+
+    assert(n_elts == n_g_elts);
+
+    for (int i = 0; i < n_elts; i++)
+      g_array[i] = array[i];
+
+  }
 }
 
 /*----------------------------------------------------------------------------*/

src/base/cs_parall.h
@@ -324,8 +324,8 @@ cs_parall_min_loc_vals(int         n,
                        cs_real_t   min_loc_vals[]);
 
 /*----------------------------------------------------------------------------
- * Given an (id, rank, value) tuple, return the local id, rank,
- * and value corresponding to the global minimum value.
+ * Given an (id, rank, value) tuple, return the local id and rank
+ * corresponding to the global minimum value.
  *
  * parameters:
  *   elt_id  <-> element id for which the value is the smallest

commit 7d429c148fe38e282b39e8cc551b075083073fe9

Fix error in serial mode using cs_parall_max/min_loc_vals.
Error was introduced in commit 281d0d29 (Feb 1 2017).

src/base/cs_parall.c
@@ -767,13 +767,6 @@ cs_parall_max_loc_vals(int         n,
   }
 
 #endif
-
-  if (cs_glob_n_ranks == 1) {
-
-    for (int i = 0; i < n; i++)
-      max_loc_vals[i] = max[i];
-
-  }
 }
 
 /*----------------------------------------------------------------------------*/
@@ -812,13 +805,6 @@ cs_parall_min_loc_vals(int         n,
   }
 
 #endif
-
-  if (cs_glob_n_ranks == 1) {
-
-    for (int i = 0; i < n; i++)
-      min_loc_vals[i] = min[i];
-
-  }
 }
 
 /*----------------------------------------------------------------------------*/