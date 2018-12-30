code-saturne/code_saturne

commit 34fa51808da3c2777d2e0d7aee4384301c85431e

Move user subroutine example cs_user_initialization-base from Fortran to C.
Warning: the prototype of the C function has been changed to get the
"cs_glob_domain" as argument.

src/base/cs_base_fortran.c
@@ -624,6 +624,13 @@ cs_user_extra_operations_wrapper(void)
   cs_user_extra_operations(cs_glob_domain);
 }
 
+void
+cs_user_initialization_wrapper(void)
+{
+  cs_user_initialization(cs_glob_domain);
+}
+
+
 /*----------------------------------------------------------------------------*/
 
 END_C_DECLS

src/base/cs_base_fortran.h
@@ -237,6 +237,9 @@ cs_user_extra_operations_initialize_wrapper(void);
 void
 cs_user_extra_operations_wrapper(void);
 
+void
+cs_user_extra_operations_wrapper(void);
+
 /*----------------------------------------------------------------------------*/
 
 END_C_DECLS

docs/doxygen/examples/cs_user_initialization.h
@@ -80,31 +80,16 @@
 
   \subsection user_initialization_base_loc_var_dec Local variables to be added
 
-  The following local variables need to be defined for the examples
-  in this section:
-
-  \snippet cs_user_initialization-base.f90 loc_var_dec
-
-  \subsection alloc Allocation
-
-  Before user initialization, work arrays must be allocated.
-
-  \snippet cs_user_initialization-base.f90 alloc
-
   \subsection user_initialization_base_s_init Initialization
 
-   isca(1) is the number related to the first user-defined scalar variable.
-   rtp(iel,isca(1)) is the value of this variable in cell number iel.
+  One can get any field using \ref cs_field_by_name function (use
+  \ref cs_field_by_name_try if one is not sure the field exists).
+  "scalar1" is the name related to the first user-defined scalar variable.
+  \c f->val[\c cell_id] is the value of this variable in cell number \c cell_id.
 
-   ONLY done if there is no restart computation.
+  ONLY done if there is no restart computation.
 
-  \snippet cs_user_initialization-base.f90 init
-
-  \subsection user_initialization_base_finalize Finalization
-
-  At the end of the subroutine, it is recommended to deallocate the work array:
-
-  \snippet cs_user_initialization-base.f90 finalize
+  \snippet cs_user_initialization-base.c init
 
 */
 // __________________________________________________________________________________
@@ -391,8 +376,8 @@
 /*!
 
   \page user_initialization_unified_combustion Unified combustion coal example
-  isca(1) is the number related to the first user-defined scalar variable.
-  rtp(iel,isca(1)) is the value of this variable in cell number iel.
+  One can get any field using \ref field_get_val_s_by_name function.
+  \c cvar_*(iel) is the value of this variable in cell number \c iel.
   ONLY done if there is no restart computation
 
   \section unified_combustion Unified combustion coal example

src/base/cs_c_bindings.f90
@@ -1589,7 +1589,7 @@ module cs_c_bindings
     ! Interface to C user function for initialization
 
     subroutine user_initialization()  &
-      bind(C, name='cs_user_initialization')
+      bind(C, name='cs_user_initialization_wrapper')
       use, intrinsic :: iso_c_binding
       implicit none
     end subroutine user_initialization

src/base/cs_prototypes.h
@@ -343,12 +343,16 @@ cs_user_extra_operations(cs_domain_t     *domain);
 void
 cs_user_extra_operations_finalize(cs_domain_t     *domain);
 
-/*----------------------------------------------------------------------------
- * This function is called one time step to initialize problem.
- *----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief This function is called one time step to initialize problem.
+ *
+ * \param[in, out]  domain   pointer to a cs_domain_t structure
+ */
+/*----------------------------------------------------------------------------*/
 
 void
-cs_user_initialization(void);
+cs_user_initialization(cs_domain_t     *domain);
 
 /*----------------------------------------------------------------------------*/
 /*!

src/user/cs_user_initialization.c
@@ -106,11 +106,13 @@ BEGIN_C_DECLS
  *
  * This is intended to initialize or modify (when restarted)
  * variable and time step values.
+ *
+ * \param[in, out]  domain   pointer to a cs_domain_t structure
  */
 /*----------------------------------------------------------------------------*/
 
 void
-cs_user_initialization(void)
+cs_user_initialization(cs_domain_t     *domain)
 {
 
 }

src/user_examples/Makefile.am
@@ -78,7 +78,6 @@ cs_user_1d_wall_thermal.c \
 cs_user_boundary_conditions-electric_arcs_ieljou_3_or_4.c \
 cs_user_boundary_conditions-medcoupling_remapper_2d.c \
 cs_user_boundary_conditions-notebook.c \
-cs_user_extra_operations-verif_cdo_diffusion.c \
 cs_user_coupling-saturne.c \
 cs_user_coupling-syrthes.c \
 cs_user_coupling.c \
@@ -86,9 +85,11 @@ cs_user_electric_scaling.c \
 cs_user_extra_operations-balance_by_zone.c \
 cs_user_extra_operations-scalar_balance.c \
 cs_user_extra_operations-turbomachinery.c \
+cs_user_extra_operations-verif_cdo_diffusion.c \
 cs_user_gwf-example.c \
 cs_user_head_losses.c \
 cs_user_hgn-relax_time_scale.c \
+cs_user_initialization-base.c \
 cs_user_lagr_boundary_conditions.c \
 cs_user_lagr_volume_conditions.c \
 cs_user_lagr_model.c \
@@ -148,7 +149,6 @@ cs_user_extra_operations-parallel_operations.f90 \
 cs_user_extra_operations-richards_flux.f90 \
 cs_user_fluid_structure_interaction.f90 \
 cs_user_initialization-atmospheric.f90 \
-cs_user_initialization-base.f90 \
 cs_user_initialization-compressible.f90 \
 cs_user_initialization-cooling_towers.f90 \
 cs_user_initialization-electric_arcs.f90 \

src/user_examples/cs_user_initialization-base.f90
@@ -1,142 +1,0 @@
-!-------------------------------------------------------------------------------
-
-!VERS
-
-! This file is part of Code_Saturne, a general-purpose CFD tool.
-!
-! Copyright (C) 1998-2018 EDF S.A.
-!
-! This program is free software; you can redistribute it and/or modify it under
-! the terms of the GNU General Public License as published by the Free Software
-! Foundation; either version 2 of the License, or (at your option) any later
-! version.
-!
-! This program is distributed in the hope that it will be useful, but WITHOUT
-! ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-! FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-! details.
-!
-! You should have received a copy of the GNU General Public License along with
-! this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
-! Street, Fifth Floor, Boston, MA 02110-1301, USA.
-
-!-------------------------------------------------------------------------------
-
-!===============================================================================
-! Purpose:
-! -------
-
-!> \file cs_user_initialization-base.f90
-!>
-!> \brief Basic examples
-!>
-!> See \subpage cs_user_initialization for examples.
-!>
-!
-!-------------------------------------------------------------------------------
-
-!-------------------------------------------------------------------------------
-! Arguments
-!______________________________________________________________________________.
-!  mode           name          role                                           !
-!______________________________________________________________________________!
-!> \param[in]     nvar          total number of variables
-!> \param[in]     nscal         total number of scalars
-!> \param[in]     dt            time step (per cell)
-!_______________________________________________________________________________
-
-
-subroutine cs_user_f_initialization &
- ( nvar   , nscal  ,                                              &
-   dt     )
-
-!===============================================================================
-
-!===============================================================================
-! Module files
-!===============================================================================
-
-use paramx
-use pointe
-use numvar
-use optcal
-use cstphy
-use cstnum
-use entsor
-use parall
-use period
-use ppppar
-use ppthch
-use coincl
-use cpincl
-use ppincl
-use atincl
-use ctincl
-use ppcpfu
-use cs_coal_incl
-use cs_fuel_incl
-use mesh
-use field
-
-!===============================================================================
-
-implicit none
-
-! Arguments
-
-integer          nvar   , nscal
-
-double precision dt(ncelet)
-
-! Local variables
-
-!< [loc_var_dec]
-integer          iel, iutile
-integer, allocatable, dimension(:) :: lstelt
-double precision, dimension(:), pointer :: cvar_scal
-!< [loc_var_dec]
-
-!===============================================================================
-
-!===============================================================================
-! Initialization
-!===============================================================================
-
-!< [alloc]
-allocate(lstelt(ncel)) ! temporary array for cells selection
-!< [alloc]
-
-!===============================================================================
-! Variables initialization:
-!
-!   isca(1) is the number related to the first user-defined scalar variable.
-!   cvar_scal(iel) is the value of this variable in cell number iel.
-!
-!   ONLY done if there is no restart computation
-!===============================================================================
-
-!< [init]
-if (isuite.eq.0) then
-  call field_get_val_s(ivarfl(isca(1)), cvar_scal)
-
-  do iel = 1, ncel
-    cvar_scal(iel) = 25.d0
-  enddo
-
-endif
-!< [init]
-
-!--------
-! Formats
-!--------
-
-!----
-! End
-!----
-
-!< [finalize]
-deallocate(lstelt)  ! temporary array for cells selection
-!< [finalize]
-
-return
-end subroutine cs_user_f_initialization

commit 038ee399267f2f586f9df939dc45854afe608376

Fix bug introduced in commit 34fa518 (2018-12-29).

src/base/cs_base_fortran.c
@@ -609,7 +609,7 @@ cs_base_fortran_bft_printf_to_f(void)
 }
 
 /*----------------------------------------------------------------------------
- * Wrappers to cs_user_extra_operations
+ * Wrappers to cs_user_*
  *----------------------------------------------------------------------------*/
 
 void

src/base/cs_base_fortran.h
@@ -228,7 +228,7 @@ void
 cs_base_fortran_bft_printf_to_f(void);
 
 /*----------------------------------------------------------------------------
- * Wrappers to cs_user_extra_operations
+ * Wrappers for Fortran C
  *----------------------------------------------------------------------------*/
 
 void
@@ -238,7 +238,7 @@ void
 cs_user_extra_operations_wrapper(void);
 
 void
-cs_user_extra_operations_wrapper(void);
+cs_user_initialization_wrapper(void);
 
 /*----------------------------------------------------------------------------*/
 