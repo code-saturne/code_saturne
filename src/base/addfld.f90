7bc4dbf739df94850aeef475722efacd07719135
Yvan Fournier <yvan.fournier@noreply.edf.fr>
2019-01-02 16:57:28 +0100

    Update Copyright.

--- src/base/addfld.f90
+++ src/base/addfld.f90
@@ -3,7 +3,7 @@
 !     This file is part of the Code_Saturne Kernel, element of the
 !     Code_Saturne CFD tool.
 
-!     Copyright (C) 1998-2018 EDF S.A., France
+!     Copyright (C) 1998-2019 EDF S.A., France
 
 !     contact: saturne-support@edf.fr
 
