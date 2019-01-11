code-saturne/code_saturne

commit 3f2b1ecd1eb204517a0bec20dd10b74f3699677e

Style fixes.

src/lagr/cs_lagr_car.c
@@ -69,8 +69,6 @@
 #include "cs_field.h"
 #include "cs_field_pointer.h"
 
-#include "cs_gui_particles.h"
-
 #include "cs_physical_constants.h"
 #include "cs_physical_model.h"
 
@@ -350,7 +348,7 @@ cs_lagr_car(int              iprev,
           cs_real_t tl  = cl * energi[cell_id] / dissip[cell_id];
           tl  = CS_MAX(tl, cs_math_epzero);
 
-          for (cs_lnum_t i = 0; i < 3; i++){
+          for (cs_lnum_t i = 0; i < 3; i++) {
 
             vpart[i] = part_vel[i];
             vflui[i] = part_vel_seen[i];
@@ -372,7 +370,7 @@ cs_lagr_car(int              iprev,
 
             if (stat_w->val[cell_id] > cs_glob_lagr_stat_options->threshold) {
 
-              for (cs_lnum_t i = 0; i < 3; i++){
+              for (cs_lnum_t i = 0; i < 3; i++) {
                 vpart[i] = stat_vel->val[cell_id * 3 + i];
                 vflui[i] = extra->vel->vals[iprev][cell_id * 3 + i];
               }
@@ -460,9 +458,10 @@ cs_lagr_car(int              iprev,
 
             for (cs_lnum_t id = 0; id < 3; id++) {
 
-              cs_real_t bxi = dissip[cell_id] * (  (c0  * bbi[id] * ktil / energi[cell_id])
-                                                 + (  (bbi[id] * ktil / energi[cell_id] - 1.0)
-                                                    * 2.0 / 3.0));
+              cs_real_t bxi
+                = dissip[cell_id] * (  (c0  * bbi[id] * ktil / energi[cell_id])
+                                     + (  (bbi[id] * ktil / energi[cell_id] - 1.0)
+                                        * 2.0 / 3.0));
               if (bxi > 0.0)
                 bx[ip][id][nor-1] = sqrt(bxi);
               else
@@ -496,7 +495,7 @@ cs_lagr_car(int              iprev,
 
         else {
 
-          for (cs_lnum_t id = 0; id < 3; id++ ){
+          for (cs_lnum_t id = 0; id < 3; id++ ) {
 
             tlag[ip][id]    = cs_math_epzero;
             bx[ip][id][nor-1] = 0.0;
@@ -519,7 +518,7 @@ cs_lagr_car(int              iprev,
 
       if (cs_lagr_particle_get_cell_id(particle, p_am) >= 0) {
 
-        for (cs_lnum_t id = 0; id < 3; id++ ){
+        for (cs_lnum_t id = 0; id < 3; id++ ) {
 
           tlag[ip][id] = cs_math_epzero;
           bx[ip][id][nor-1] = 0.0;
@@ -536,37 +535,37 @@ cs_lagr_car(int              iprev,
   /* 4. CALCUL DE PII     */
   /* ====================================================================   */
 
-  for (cs_lnum_t id = 0; id < 3; id++) {
+  if (   cs_glob_lagr_time_scheme->modcpl > 0
+      &&   cs_glob_time_step->nt_cur
+         > cs_glob_lagr_time_scheme->modcpl) {
 
-    for (cs_lnum_t ip = 0; ip < p_set->n_particles; ip++) {
+    int stat_type = cs_lagr_stat_type_from_attr_id(CS_LAGR_VELOCITY);
 
-      unsigned char *particle = p_set->p_buffer + p_am->extents * ip;
+    cs_field_t *stat_vel
+      = cs_lagr_stat_get_moment(stat_type,
+                                CS_LAGR_MOMENT_MEAN,
+                                0,
+                                -1);
 
-      cs_lnum_t cell_id = cs_lagr_particle_get_cell_id(particle, p_am);
+    cs_field_t *stat_w = cs_lagr_stat_get_stat_weight(0);
 
-      if (cell_id >= 0) {
+    for (cs_lnum_t id = 0; id < 3; id++) {
 
-        /* -->   Compute: II = ( -grad(P)/Rom(f)+grad(<Vf>)*(<Up>-<Uf>) + g )
-         *       or
-         *       Compute: II = ( -grad(P)/Rom(f) + g) */
+      for (cs_lnum_t ip = 0; ip < p_set->n_particles; ip++) {
 
-        cs_real_t romf = extra->cromf->val[cell_id];
+        unsigned char *particle = p_set->p_buffer + p_am->extents * ip;
 
-        piil[ip][id] = -gradpr[cell_id][id] / romf + grav[id];
+        cs_lnum_t cell_id = cs_lagr_particle_get_cell_id(particle, p_am);
 
-        if (   cs_glob_lagr_time_scheme->modcpl > 0
-            &&   cs_glob_time_step->nt_cur
-               > cs_glob_lagr_time_scheme->modcpl) {
+        if (cell_id >= 0) {
 
-          int stat_type = cs_lagr_stat_type_from_attr_id(CS_LAGR_VELOCITY);
+          /* -->   Compute: II = ( -grad(P)/Rom(f)+grad(<Vf>)*(<Up>-<Uf>) + g )
+           *       or
+           *       Compute: II = ( -grad(P)/Rom(f) + g) */
 
-          cs_field_t *stat_vel
-            = cs_lagr_stat_get_moment(stat_type,
-                                      CS_LAGR_MOMENT_MEAN,
-                                      0,
-                                      -1);
+          cs_real_t romf = extra->cromf->val[cell_id];
 
-          cs_field_t *stat_w = cs_lagr_stat_get_stat_weight(0);
+          piil[ip][id] = -gradpr[cell_id][id] / romf + grav[id];
 
           if (stat_w->val[cell_id] > cs_glob_lagr_stat_options->threshold) {
 
@@ -585,8 +584,6 @@ cs_lagr_car(int              iprev,
       }
     }
   }
-
-  return;
 }
 
 /*----------------------------------------------------------------------------*/

src/base/cs_c_bindings.f90
@@ -157,8 +157,8 @@ module cs_c_bindings
     bind(C, name='cs_max_limiter_building')
       use, intrinsic :: iso_c_binding
       implicit none
-      integer(c_int),value :: f_id
-      integer(c_int),value :: inc
+      integer(c_int), value :: f_id
+      integer(c_int), value :: inc
       real(c_double), dimension(*) , intent(in) :: rovsdt
     end subroutine max_limiter_building
 
@@ -2679,7 +2679,7 @@ contains
   !> \param[in]   f_id     field id
   !> \param[in]   k_value  structure associated with key
 
-  subroutine field_set_key_struct_var_cal_opt (f_id, k_value)
+  subroutine field_set_key_struct_var_cal_opt(f_id, k_value)
 
     use, intrinsic :: iso_c_binding
     implicit none
@@ -2858,7 +2858,7 @@ contains
   !> \param[in]   f_id     field id
   !> \param[out]  k_value  integer value associated with key id for this field
 
-  subroutine field_get_key_struct_var_cal_opt (f_id, k_value)
+  subroutine field_get_key_struct_var_cal_opt(f_id, k_value)
 
     use, intrinsic :: iso_c_binding
     implicit none
@@ -2903,7 +2903,7 @@ contains
   !> \param[in]   f_id     field id
   !> \param[out]  k_value  integer value associated with key id for this field
 
-  subroutine field_get_key_struct_solving_info (f_id, k_value)
+  subroutine field_get_key_struct_solving_info(f_id, k_value)
 
     use, intrinsic :: iso_c_binding
     implicit none

src/base/tridim.f90
@@ -200,8 +200,7 @@ interface
 
   end subroutine navstv
 
-  subroutine richards &
- (icvrge, dt)
+  subroutine richards(icvrge, dt)
 
     use dimens, only: ndimfb
     use mesh, only: nfabor
@@ -213,8 +212,17 @@ interface
 
   end subroutine richards
 
-end interface
+  subroutine cs_lagr_head_losses(n_hl_cells, cell_ids, bc_type, cku) &
+    bind(C, name='cs_lagr_head_losses')
+    use, intrinsic :: iso_c_binding
+    implicit none
+    integer(c_int), value :: n_hl_cells
+    integer(c_int), dimension(*), intent(in) :: cell_ids
+    integer(c_int), dimension(*) :: bc_type
+    real(kind=c_double), dimension(*) :: cku
+  end subroutine cs_lagr_head_losses
 
+end interface
 
 !===============================================================================
 
@@ -465,7 +473,7 @@ if (ncpdct.gt.0) then
   call cs_head_losses_compute(ckupdc)
 
  if (iflow .eq.1) then
-   call laghlo(ncepdc, icepdc, itypfb, ckupdc)
+   call cs_lagr_head_losses(ncepdc, icepdc, itypfb, ckupdc)
  endif
 
 endif

src/lagr/cs_lagr_head_losses.c
@@ -60,6 +60,7 @@
 #include "cs_mesh.h"
 #include "cs_mesh_quantities.h"
 #include "cs_parameters.h"
+#include "cs_parameters_check.h"
 #include "cs_parall.h"
 #include "cs_prototypes.h"
 
@@ -89,12 +90,11 @@ BEGIN_C_DECLS
  *----------------------------------------------------------------------------*/
 
 static void
-_porcel(cs_real_t   mdiam[],
-        cs_real_t   porosi[],
-        cs_lnum_t   itypfb[])
+_porcel(cs_real_t         mdiam[],
+        cs_real_t         porosi[],
+        const cs_lnum_t   bc_type[])
 {
-  /* Initialization
-   *----------------*/
+  /* Initialization */
 
   const cs_mesh_t  *m = cs_glob_mesh;
 
@@ -120,7 +120,7 @@ _porcel(cs_real_t   mdiam[],
 
   for (cs_lnum_t ifac = 0; ifac < nfabor; ifac++) {
 
-    if (   (itypfb[ifac] == CS_SMOOTHWALL || itypfb[ifac] == CS_ROUGHWALL)
+    if (   (bc_type[ifac] == CS_SMOOTHWALL || bc_type[ifac] == CS_ROUGHWALL)
         && bound_stat[ifac + lag_bdy_i->ihdepm * nfabor] > 0.0) {
       indic = 1;
       itytmp[ifac] = CS_INLET;
@@ -159,9 +159,8 @@ _porcel(cs_real_t   mdiam[],
   if (indic > 0)
     CS_PROCF(distpr, DISTPR)(itytmp, distpw);
 
-  /* ====================================================================
-   * 2. Compute  n = Grad(DISTPW)/|Grad(DISTPW)|
-   * ====================================================================   */
+  /* Compute  n = Grad(DISTPW)/|Grad(DISTPW)|
+   * ======================================== */
 
   /* Distance to wall is 0 at the wall by definition, zero flux elsewhere   */
 
@@ -211,7 +210,7 @@ _porcel(cs_real_t   mdiam[],
                      NULL, /* internal coupling */
                      q);
 
-  /* Normalisation (attention, le gradient peut etre nul, parfois) */
+  /* Normalisation (caution, gradient can be zero sometimes) */
 
   for (cs_lnum_t iel = 0; iel < ncel; iel++) {
 
@@ -231,9 +230,8 @@ _porcel(cs_real_t   mdiam[],
     cs_halo_perio_sync_var_vect(m->halo, CS_HALO_STANDARD,
                                 (cs_real_t *)q, 3);
 
-  /* ====================================================================
-   * 3. Compute  porosity
-   * ==================================================================== */
+  /* Compute  porosity
+   * ================= */
 
   cs_real_t epsi = 1e-06;
 
@@ -266,7 +264,7 @@ _porcel(cs_real_t   mdiam[],
 
   }
 
-  /* Paralellism and periodicity    */
+  /* Paralellism and periodicity */
 
   cs_parall_counter_max(&indic, 1);
 
@@ -283,10 +281,10 @@ _porcel(cs_real_t   mdiam[],
       cs_lnum_t iel1  = cs_glob_mesh->i_face_cells[ifac][0];
       cs_lnum_t iel2  = cs_glob_mesh->i_face_cells[ifac][1];
 
-      for (cs_lnum_t ii = 0; ii < 3; ii++){
+      for (cs_lnum_t ii = 0; ii < 3; ii++) {
 
-        prod1 +=   q[iel1][ii] * cs_glob_mesh_quantities->i_face_normal[ifac * 3 + ii];
-        prod2 += - q[iel2][ii] * cs_glob_mesh_quantities->i_face_normal[ifac * 3 + ii];
+        prod1 += q[iel1][ii] * cs_glob_mesh_quantities->i_face_normal[ifac * 3 + ii];
+        prod2 -= q[iel2][ii] * cs_glob_mesh_quantities->i_face_normal[ifac * 3 + ii];
 
       }
 
@@ -295,8 +293,10 @@ _porcel(cs_real_t   mdiam[],
 
       else {
 
-        masflu[iel1] += - (porosi[iel1] - cs_glob_lagr_clogging_model->mporos) / dt * volume[iel1];
-        masflu[iel2] +=   (porosi[iel1] - cs_glob_lagr_clogging_model->mporos) / dt * volume[iel1];
+        masflu[iel1] -=   (porosi[iel1] - cs_glob_lagr_clogging_model->mporos)
+                        / dt * volume[iel1];
+        masflu[iel2] +=   (porosi[iel1] - cs_glob_lagr_clogging_model->mporos)
+                        / dt * volume[iel1];
         mdiam[iel2]   = mdiam[iel1];
 
       }
@@ -306,8 +306,10 @@ _porcel(cs_real_t   mdiam[],
 
       else {
 
-        masflu[iel2] += - (porosi[iel2] - cs_glob_lagr_clogging_model->mporos) / dt * volume[iel2];
-        masflu[iel1] +=   (porosi[iel2] - cs_glob_lagr_clogging_model->mporos) / dt * volume[iel2];
+        masflu[iel2] -=   (porosi[iel2] - cs_glob_lagr_clogging_model->mporos)
+                        / dt * volume[iel2];
+        masflu[iel1] +=   (porosi[iel2] - cs_glob_lagr_clogging_model->mporos)
+                        / dt * volume[iel2];
         mdiam[iel1]   = mdiam[iel2];
 
       }
@@ -348,7 +350,7 @@ _porcel(cs_real_t   mdiam[],
 
   }
 
-  /* Free memory     */
+  /* Free memory */
 
   BFT_FREE(masflu);
   BFT_FREE(depvol);
@@ -357,11 +359,7 @@ _porcel(cs_real_t   mdiam[],
   BFT_FREE(coefbp);
   BFT_FREE(distpw);
   BFT_FREE(itytmp);
-
-  return;
-
 }
-
 
 /*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
 
@@ -369,16 +367,22 @@ _porcel(cs_real_t   mdiam[],
  * Public function definitions
  *============================================================================*/
 
-/*-------------------------------------------------------------------*/
-/*! \brief Define Head losses to take into account deposit in the flow
+/*----------------------------------------------------------------------------*/
+/*!
+ *  \brief Define Head losses to take into account deposit in the flow
+ *
+ * \param[in]   n_hl_cells  number of cells on which to apply head losses
+ * \param[in]   cell_ids    ids of cells on which to apply head losses
+ * \param[in]   bc_type     boundary face type
+ * \param[out]  cku         head loss coefficients at matchin cells
  */
-/*-------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
 
 void
-CS_PROCF(laghlo, LAGHLO)(cs_lnum_t *ncepdc,
-                         cs_lnum_t *icepdc,
-                         cs_lnum_t  itypfb[],
-                         cs_real_t  ckupdc[])
+cs_lagr_head_losses(cs_lnum_t        n_hl_cells,
+                    const cs_lnum_t  cell_ids[],
+                    const cs_lnum_t  bc_type[],
+                    cs_real_t        cku[][6])
 {
   cs_lnum_t ncel   = cs_glob_mesh->n_cells;
   cs_lnum_t ncelet = cs_glob_mesh->n_cells_with_ghosts;
@@ -388,20 +392,15 @@ CS_PROCF(laghlo, LAGHLO)(cs_lnum_t *ncepdc,
   cs_lagr_extra_module_t *extra = cs_glob_lagr_extra_module;
 
   /* Check that head loss zone definitions are consistent */
-  if (*ncepdc != ncel)
-    bft_error(__FILE__, __LINE__, 0,
-              _("@\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
-                "@\n"
-                "@ @@ ERROR:\n"
-                "@    ======\n"
-                "@   TO BE COMPATIBLE WITH THE LAGRANGIAN DEPOSITION MODEL,\n"
-                "@     HEAD LOSS ZONES MUST COVER THE WHOLE MESH\n"
-                "@ Head loss coefficients may be locally zero.\n"
-                "@ Check your case setup.\n"
-                "@\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n@"));
+  if (n_hl_cells != ncel)
+    cs_parameters_error
+      (CS_ABORT_IMMEDIATE,
+       _("in Lagrangian module"),
+       _("The number of cells in the head loss zones must cover\n"
+         "the whole mesh (though the local head loss may be zero).\n"));
 
   /* ==================================================================
-   * ckupdc: compute head loss coefficients in the calculation coordinates,
+   * cku: compute head loss coefficients in the calculation coordinates,
    *            organized in order k11, k22, k33, k12, k13, k23
    * Note:
    *     - make sure diagonal coefficients are positive. The calculation
@@ -409,37 +408,35 @@ CS_PROCF(laghlo, LAGHLO)(cs_lnum_t *ncepdc,
    *      be done
    * ==================================================================*/
 
-
-   /* ===================================================================
+  /* ===================================================================
    * Porosity calculation for the influence of the deposit on the flow
    * by head losses
    * ====================================================================*/
+
   cs_real_t *mdiam;
   BFT_MALLOC(mdiam, ncelet, cs_real_t);
 
   /* cs_lnum_t poro_id; */
   /* field_get_id_try ("clogging_porosity", &poro_id); */
 
-  /* TODO ce champ n'est jamais cree */
+  /* TODO this field is never built */
   cs_field_t *f_poro = cs_field_by_name_try("clogging_porosity");
 
   cs_real_t *lporo;
   if (f_poro == NULL)
     BFT_MALLOC(lporo, ncelet, cs_real_t);
-
   else
     lporo = f_poro->val;
 
-  _porcel(mdiam, lporo, itypfb);
+  _porcel(mdiam, lporo, bc_type);
 
-  /* ====================================================================
-   * Calculation of the head loss term with the Ergun law
+  /* Calculation of the head loss term with the Ergun law
    * mdiam :  mean diameter of deposited particles
-   * lcell :  characteristic length in the flow direction    */
+   * lcell :  characteristic length in the flow direction */
 
-  for (cs_lnum_t ielpdc = 0; ielpdc < *ncepdc; ielpdc++) {
+  for (cs_lnum_t ielpdc = 0; ielpdc < n_hl_cells; ielpdc++) {
 
-    cs_lnum_t iel = icepdc[ielpdc];
+    cs_lnum_t iel = cell_ids[ielpdc];
 
     if (mdiam[iel] > 0.0) {
 
@@ -454,12 +451,12 @@ CS_PROCF(laghlo, LAGHLO)(cs_lnum_t *ncepdc,
                     * lcell / mdiam[iel]
                     +  (lcell * 150.0 * visccf) / (romf * pow(mdiam[iel], 2))
                      * pow((1 - lporo[iel]), 2) / lporo[iel] * 3;
-      ckupdc[iel * 6 + 0] = ck;
-      ckupdc[iel * 6 + 1] = ck;
-      ckupdc[iel * 6 + 2] = ck;
-      ckupdc[iel * 6 + 3] = 0.0;
-      ckupdc[iel * 6 + 4] = 0.0;
-      ckupdc[iel * 6 + 5] = 0.0;
+      cku[iel][0] = ck;
+      cku[iel][1] = ck;
+      cku[iel][2] = ck;
+      cku[iel][3] = 0.0;
+      cku[iel][4] = 0.0;
+      cku[iel][5] = 0.0;
 
     }
 
@@ -469,9 +466,6 @@ CS_PROCF(laghlo, LAGHLO)(cs_lnum_t *ncepdc,
     BFT_FREE(lporo);
 
   BFT_FREE(mdiam);
-
-  return;
-
 }
 
 /*----------------------------------------------------------------------------*/

src/lagr/cs_lagr_head_losses.h
@@ -51,15 +51,20 @@ BEGIN_C_DECLS
 
 /*----------------------------------------------------------------------------*/
 /*!
- * \brief Define Head losses to take into account deposit in the flow
+ *  \brief Define Head losses to take into account deposit in the flow
+ *
+ * \param[in]   n_hl_cells  number of cells on which to apply head losses
+ * \param[in]   cell_ids    ids of cells on which to apply head losses
+ * \param[in]   bc_type     boundary face type
+ * \param[out]  cku         head loss coefficients at matchin cells
  */
 /*----------------------------------------------------------------------------*/
 
 void
-CS_PROCF(laghlo, LAGHLO)(cs_lnum_t *ncepdc,
-                         cs_lnum_t *icepdc,
-                         cs_lnum_t  itypfb[],
-                         cs_real_t  ckupdc[]);
+cs_lagr_head_losses(cs_lnum_t        n_hl_cells,
+                    const cs_lnum_t  cell_ids[],
+                    const cs_lnum_t  bc_type[],
+                    cs_real_t        cku[][6]);
 
 /*----------------------------------------------------------------------------*/
 

src/lagr/cs_lagr_options.c
@@ -1161,7 +1161,7 @@ cs_lagr_option_definition(cs_int_t   *isuite,
     }
 
   }
-  else{
+  else {
 
     cs_glob_lagr_specific_physics->itpvar = 0;
     cs_glob_lagr_specific_physics->impvar = 0;
@@ -1538,8 +1538,8 @@ cs_lagr_option_definition(cs_int_t   *isuite,
   cs_parameters_is_in_range_int(CS_ABORT_DELAYED,
                                 _("in Lagrangian module"),
                                 "cs_glob_lagr_time_scheme->isttio",
-                                  lagr_time_scheme->isttio,
-                                  0, 2);
+                                lagr_time_scheme->isttio,
+                                0, 2);
 
   if (lagr_time_scheme->iilagr == 2) {
 
@@ -1701,7 +1701,7 @@ cs_lagr_option_definition(cs_int_t   *isuite,
     }
 
   }
-  else{
+  else {
 
     cs_glob_lagr_source_terms->ltsdyn = 0;
     cs_glob_lagr_source_terms->ltsmas = 0;
@@ -2016,7 +2016,7 @@ cs_lagr_option_definition(cs_int_t   *isuite,
 
 
   }
-  else{
+  else {
 
     cs_glob_lagr_boundary_interactions->iencnbbd = 0;
     cs_glob_lagr_boundary_interactions->iencmabd = 0;
@@ -2272,7 +2272,7 @@ cs_lagr_option_definition(cs_int_t   *isuite,
   cs_glob_lagr_source_terms->itsco = 0;
   cs_glob_lagr_source_terms->itsfp4 = 0;
 
-  /* Dynamique : Vitesse + Turbulence    */
+  /* Dynamics: velocity + turbulence */
   if (cs_glob_lagr_source_terms->ltsdyn == 1) {
 
     _define_st_field("velocity_st_lagr", 3);
@@ -2414,6 +2414,13 @@ cs_lagr_option_definition(cs_int_t   *isuite,
                             false); /* has previous */
 
   /* Now activate basic statistics */
+
+#if 0
+  if (   cs_glob_lagr_time_scheme->modcpl > 0
+      || cs_glob_lagr_time_scheme->ilapoi == 1)
+    cs_lagr_stat_activate(CS_LAGR_STAT_CUMULATIVE_WEIGHT);
+#endif
+
   cs_lagr_stat_initialize();
 }
 

commit 97db8f2b79cc70842e0d114cfea803f4ba67ff70

Fix crash introduced in commit 3f2b1ecd (Jan 2), improve loop.

src/lagr/cs_lagr_car.c
@@ -549,21 +549,19 @@ cs_lagr_car(int              iprev,
 
     cs_field_t *stat_w = cs_lagr_stat_get_stat_weight(0);
 
-    for (cs_lnum_t id = 0; id < 3; id++) {
+    for (cs_lnum_t ip = 0; ip < p_set->n_particles; ip++) {
 
-      for (cs_lnum_t ip = 0; ip < p_set->n_particles; ip++) {
+      unsigned char *particle = p_set->p_buffer + p_am->extents * ip;
 
-        unsigned char *particle = p_set->p_buffer + p_am->extents * ip;
+      cs_lnum_t cell_id = cs_lagr_particle_get_cell_id(particle, p_am);
 
-        cs_lnum_t cell_id = cs_lagr_particle_get_cell_id(particle, p_am);
+      if (cell_id >= 0) {
 
-        if (cell_id >= 0) {
+        /* Compute: II = ( -grad(P)/Rom(f)+grad(<Vf>)*(<Up>-<Uf>) + g ) */
 
-          /* -->   Compute: II = ( -grad(P)/Rom(f)+grad(<Vf>)*(<Up>-<Uf>) + g )
-           *       or
-           *       Compute: II = ( -grad(P)/Rom(f) + g) */
+        cs_real_t romf = extra->cromf->val[cell_id];
 
-          cs_real_t romf = extra->cromf->val[cell_id];
+        for (cs_lnum_t id = 0; id < 3; id++) {
 
           piil[ip][id] = -gradpr[cell_id][id] / romf + grav[id];
 
@@ -571,8 +569,8 @@ cs_lagr_car(int              iprev,
 
             for (cs_lnum_t i = 0; i < 3; i++) {
 
-              cs_real_t vpm   = stat_vel->val[i + cell_id * 3];
-              cs_real_t vflui = extra->vel->vals[iprev][i + cell_id * 3];
+              cs_real_t vpm   = stat_vel->val[cell_id*3 + i];
+              cs_real_t vflui = extra->vel->vals[iprev][cell_id*3 + i];
               piil[ip][id] += gradvf[cell_id][id][i] * (vpm - vflui);
 
             }
@@ -584,6 +582,29 @@ cs_lagr_car(int              iprev,
       }
     }
   }
+
+  else {
+
+    for (cs_lnum_t ip = 0; ip < p_set->n_particles; ip++) {
+
+      unsigned char *particle = p_set->p_buffer + p_am->extents * ip;
+
+      cs_lnum_t cell_id = cs_lagr_particle_get_cell_id(particle, p_am);
+
+      if (cell_id >= 0) {
+
+        /* Compute: II = ( -grad(P)/Rom(f) + g) */
+
+        cs_real_t romf = extra->cromf->val[cell_id];
+
+        for (cs_lnum_t id = 0; id < 3; id++)
+          piil[ip][id] = -gradpr[cell_id][id] / romf + grav[id];
+
+      }
+    }
+
+  }
+
 }
 
 /*----------------------------------------------------------------------------*/