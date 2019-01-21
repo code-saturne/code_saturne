code-saturne/code_saturne

commit 1f2f7922eab33ef91112e1f868f9857146a9bc04

Add a ENUM for the Lagrangian module and optimize the calculations on frozen velocities (store pressure and velocity gradients).

src/lagr/cs_lagr.c
@@ -154,7 +154,7 @@ cs_lagr_dim_t *cs_glob_lagr_dim = &_lagr_dim;
 /* Time and Lagrangian-Eulerian coupling scheme */
 
 static cs_lagr_time_scheme_t _lagr_time_scheme
-  = {.iilagr = 0,
+  = {.iilagr = CS_LAGR_OFF,
      .isttio = 0,
      .isuila = 1,
      .t_order = 0,
@@ -362,7 +362,9 @@ static cs_lagr_extra_module_t _lagr_extra_module
      .cvar_r11 = NULL,
      .cvar_r22 = NULL,
      .cvar_r33 = NULL,
-     .cvar_rij = NULL};
+     .cvar_rij = NULL,
+     .grad_pr = NULL,
+     .grad_vel = NULL};
 
 cs_lagr_extra_module_t *cs_glob_lagr_extra_module = &_lagr_extra_module;
 
@@ -1167,6 +1169,13 @@ cs_lagr_finalize(void)
   cs_lagr_tracking_finalize();
 
   cs_lagr_finalize_zone_conditions();
+
+  /* Fluid gradients */
+  cs_lagr_extra_module_t *extra = cs_glob_lagr_extra_module;
+  BFT_FREE(extra->grad_pr);
+  if (extra->grad_vel != NULL)
+    BFT_FREE(extra->grad_vel);
+
 }
 
 /*----------------------------------------------------------------------------*/
@@ -1611,10 +1620,18 @@ cs_lagr_solve_initialize(const cs_real_t  *dt)
 {
   CS_UNUSED(dt);
 
+  /* Allocate pressure and velocity gradients */
+  cs_lagr_extra_module_t *extra = cs_glob_lagr_extra_module;
+  cs_lnum_t ncelet = cs_glob_mesh->n_cells_with_ghosts;
+
+  BFT_MALLOC(extra->grad_pr, ncelet, cs_real_3_t);
+  if (cs_glob_lagr_time_scheme->modcpl > 0)
+    BFT_MALLOC(extra->grad_vel, ncelet, cs_real_33_t);
+
   /* For frozen field:
      values at previous time step = values at current time step */
 
-  if (cs_glob_lagr_time_scheme->iilagr == 3) {
+  if (cs_glob_lagr_time_scheme->iilagr == CS_LAGR_FROZEN_CONTINUOUS_PHASE) {
 
     int n_fields = cs_field_n_fields();
 
@@ -1625,6 +1642,9 @@ cs_lagr_solve_initialize(const cs_real_t  *dt)
         cs_field_current_to_previous(f);
 
     }
+
+    /* Compute gradients of current value fields */
+    cs_lagr_gradients(0, extra->grad_pr, extra->grad_vel);
 
   }
 
@@ -1641,7 +1661,7 @@ cs_lagr_solve_initialize(const cs_real_t  *dt)
 
   /* Read particle restart data */
 
-  if (cs_glob_lagr_time_scheme->iilagr > 0)
+  if (cs_glob_lagr_time_scheme->iilagr != CS_LAGR_OFF)
     cs_lagr_restart_read_p();
 }
 
@@ -1677,16 +1697,11 @@ cs_lagr_solve_time_step(const int         itypfb[],
   cs_real_t *surfbn = cs_glob_mesh_quantities->b_face_normal;
 
   /* Allocate temporary arrays */
-  cs_real_3_t *gradpr;
-  BFT_MALLOC(gradpr, ncelet, cs_real_3_t);
   cs_real_t *w1, *w2;
   BFT_MALLOC(w1, ncelet, cs_real_t);
   BFT_MALLOC(w2, ncelet, cs_real_t);
 
   /* Allocate other arrays depending on user options    */
-  cs_real_33_t *gradvf = NULL;
-  if (cs_glob_lagr_time_scheme->modcpl > 0)
-    BFT_MALLOC(gradvf, ncelet, cs_real_33_t);
 
   cs_real_t *tempp = NULL;
   if (   lagr_model->clogging == 1
@@ -1964,13 +1979,15 @@ cs_lagr_solve_time_step(const int         itypfb[],
 
     /* At the first time step we initialize particles to
        values at current time step and not at previous time step, because
-       values at previous time step = initialization (null gradients) */
+       values at previous time step = initialization (zero gradients) */
 
-    mode = 1;
-    if (ts->nt_cur == 1)
-      mode = 0;
+    if (cs_glob_lagr_time_scheme->iilagr != CS_LAGR_FROZEN_CONTINUOUS_PHASE) {
+      mode = 1;
+      if (ts->nt_cur == 1)
+        mode = 0;
 
-    cs_lagr_gradients(mode, gradpr, gradvf);
+      cs_lagr_gradients(mode, extra->grad_pr, extra->grad_vel);
+    }
 
     /* Particles progression
        --------------------- */
@@ -1996,11 +2013,11 @@ cs_lagr_solve_time_step(const int         itypfb[],
       BFT_MALLOC(bx, p_set->n_particles, cs_real_33_t);
 
       cs_real_t *tsfext = NULL;
-      if (cs_glob_lagr_time_scheme->iilagr == 2)
+      if (cs_glob_lagr_time_scheme->iilagr == CS_LAGR_TWOWAY_COUPLING)
         BFT_MALLOC(tsfext, p_set->n_particles, cs_real_t);
 
       cs_real_t *cpgd1 = NULL, *cpgd2 = NULL, *cpght = NULL;
-      if (   cs_glob_lagr_time_scheme->iilagr == 2
+      if (   cs_glob_lagr_time_scheme->iilagr == CS_LAGR_TWOWAY_COUPLING
           && lagr_model->physical_model == 2
           && cs_glob_lagr_source_terms->ltsthe == 1) {
 
@@ -2034,8 +2051,8 @@ cs_lagr_solve_time_step(const int         itypfb[],
       /* Computation of the fluid's pressure and velocity gradient
          at n+1 (with values at current time step) */
       if (   cs_glob_lagr_time_step->nor == 2
-          && cs_glob_lagr_time_scheme->iilagr != 3)
-        cs_lagr_gradients(0, gradpr, gradvf);
+          && cs_glob_lagr_time_scheme->iilagr != CS_LAGR_FROZEN_CONTINUOUS_PHASE)
+        cs_lagr_gradients(0, extra->grad_pr, extra->grad_vel);
 
       /* use fields at previous or current time step */
       if (cs_glob_lagr_time_step->nor == 1)
@@ -2071,8 +2088,8 @@ cs_lagr_solve_time_step(const int         itypfb[],
                   piil,
                   bx,
                   tempct,
-                  gradpr,
-                  gradvf,
+                  extra->grad_pr,
+                  extra->grad_vel,
                   w1,
                   w2);
 
@@ -2084,8 +2101,8 @@ cs_lagr_solve_time_step(const int         itypfb[],
                   (const cs_real_3_t *)piil,
                   (const cs_real_33_t *)bx,
                   tsfext,
-                  (const cs_real_3_t *)gradpr,
-                  (const cs_real_33_t *)gradvf,
+                  extra->grad_pr,
+                  extra->grad_vel,
                   terbru,
                   vislen,
                   &nresnew);
@@ -2277,7 +2294,7 @@ cs_lagr_solve_time_step(const int         itypfb[],
       /* Reverse coupling: compute source terms
          -------------------------------------- */
 
-      if (   cs_glob_lagr_time_scheme->iilagr == 2
+      if (   cs_glob_lagr_time_scheme->iilagr == CS_LAGR_TWOWAY_COUPLING
           && cs_glob_lagr_time_step->nor == cs_glob_lagr_time_scheme->t_order)
         cs_lagr_coupling(taup, tempct, tsfext, cpgd1, cpgd2, cpght, w1, w2);
 
@@ -2289,10 +2306,10 @@ cs_lagr_solve_time_step(const int         itypfb[],
       BFT_FREE(piil);
       BFT_FREE(bx);
 
-      if (cs_glob_lagr_time_scheme->iilagr == 2)
+      if (cs_glob_lagr_time_scheme->iilagr == CS_LAGR_TWOWAY_COUPLING)
         BFT_FREE(tsfext);
 
-      if (   cs_glob_lagr_time_scheme->iilagr == 2
+      if (   cs_glob_lagr_time_scheme->iilagr == CS_LAGR_TWOWAY_COUPLING
           && lagr_model->physical_model == 2
           && cs_glob_lagr_source_terms->ltsthe == 1) {
         BFT_FREE(cpgd1);
@@ -2522,12 +2539,8 @@ cs_lagr_solve_time_step(const int         itypfb[],
     cs_lagr_print(ts->t_cur);
 
   /* Free memory */
-
-  BFT_FREE(gradpr);
   BFT_FREE(w1);
   BFT_FREE(w2);
-  if (cs_glob_lagr_time_scheme->modcpl > 0)
-    BFT_FREE(gradvf);
 
   if (   lagr_model->deposition == 1
       && ts->nt_cur == ts->nt_max)

src/base/cs_parameters_check.c
@@ -1109,7 +1109,7 @@ cs_parameters_check(void)
                                  "time-dependant by nature"),
                                "cs_glob_lagr_time_scheme->iilagr",
                                cs_glob_lagr_time_scheme->iilagr,
-                               0);
+                               CS_LAGR_OFF);
 
     int les_iturb[3] = {40, 41, 42};
     cs_parameters_is_not_in_list_int(CS_ABORT_DELAYED,
@@ -1343,7 +1343,7 @@ cs_parameters_check(void)
 
   /* In lagrangian with two-way coupling, k-omega SST is forbidden (not
      properly implemented) */
-  if (cs_glob_lagr_time_scheme->iilagr == 2) {
+  if (cs_glob_lagr_time_scheme->iilagr == CS_LAGR_TWOWAY_COUPLING) {
     cs_parameters_is_not_equal_int(CS_ABORT_DELAYED,
                                    _("while reading input data,\n"
                                      "two way coupling in lagrangian modelling "

src/gui/cs_gui_particles.c
@@ -197,14 +197,14 @@ cs_gui_particles_model(void)
 
   const char *model = cs_tree_node_get_tag(tn_lagr, "model");
 
-  cs_glob_lagr_time_scheme->iilagr = 0;
+  cs_glob_lagr_time_scheme->iilagr = CS_LAGR_OFF;
   if (model != NULL) {
     if (! strcmp(model, "one_way"))
-      cs_glob_lagr_time_scheme->iilagr = 1;
+      cs_glob_lagr_time_scheme->iilagr = CS_LAGR_ONEWAY_COUPLING;
     else if (! strcmp(model, "two_way"))
-      cs_glob_lagr_time_scheme->iilagr = 2;
+      cs_glob_lagr_time_scheme->iilagr = CS_LAGR_TWOWAY_COUPLING;
     else if (! strcmp(model, "frozen"))
-      cs_glob_lagr_time_scheme->iilagr = 3;
+      cs_glob_lagr_time_scheme->iilagr = CS_LAGR_FROZEN_CONTINUOUS_PHASE;
   }
 
 #if _XML_DEBUG_
@@ -212,7 +212,7 @@ cs_gui_particles_model(void)
   bft_printf("--iilagr = %i\n", cs_glob_lagr_time_scheme->iilagr);
 #endif
 
-  if (cs_glob_lagr_time_scheme->iilagr == 0)
+  if (cs_glob_lagr_time_scheme->iilagr == CS_LAGR_OFF)
     return;
 
   /* Global settings */
@@ -284,7 +284,7 @@ cs_gui_particles_model(void)
 
   /* Two-way coupling */
 
-  if (cs_glob_lagr_time_scheme->iilagr == 2) {
+  if (cs_glob_lagr_time_scheme->iilagr == CS_LAGR_TWOWAY_COUPLING) {
     cs_tree_node_t *tn = cs_tree_node_get_child(tn_lagr, "two_way_coupling");
 
     cs_gui_node_get_child_int(tn, "iteration_start",
@@ -465,7 +465,7 @@ cs_gui_particles_model(void)
     break;
   }
 
-  if (cs_glob_lagr_time_scheme->iilagr == 2) {
+  if (cs_glob_lagr_time_scheme->iilagr == CS_LAGR_TWOWAY_COUPLING) {
     bft_printf("--nstits = %i\n", cs_glob_lagr_source_terms->nstits);
     bft_printf("--ltsdyn = %i\n", cs_glob_lagr_source_terms->ltsdyn);
     bft_printf("--ltsmas = %i\n", cs_glob_lagr_source_terms->ltsmas);

src/lagr/cs_lagr.h
@@ -112,6 +112,28 @@ typedef enum {
 
 } cs_lagr_deposition_state_t;
 
+  /*! Lagrangian module status.
+     the different values correspond to the following coupling:
+     - CS_LAGR_OFF: Lagrangian module off
+     - CS_LAGR_ONEWAY_COUPLING: Lagrangian two-phase flow in one-way coupling
+         (no influence of the particles on the continuous phase)
+     - CS_LAGR_TWOWAY_COUPLING: Lagrangian two-phase flow with two-way coupling
+         (influence of the particles on the dynamics of the continuous phase).
+         Dynamics, temperature and mass may be coupled independently.
+     - CS_LAGR_FROZEN_CONTINUOUS_PHASE: Lagrangian two-phase flow on frozen i
+         continuous phase. This option
+         only only be used in case of a calculation restart. All the
+         Eulerian fields are frozen (including the scalar fields).
+         This option automatically implies \ref iccvfg = 1 */
+
+typedef enum {
+  CS_LAGR_OFF = 0,
+  CS_LAGR_ONEWAY_COUPLING = 1,
+  CS_LAGR_TWOWAY_COUPLING = 2,
+  CS_LAGR_FROZEN_CONTINUOUS_PHASE = 3
+};
+
+
 /*! Fixed maximum sizes */
 /*----------------------*/
 
@@ -143,14 +165,14 @@ typedef struct {
 typedef struct {
 
   /*! Lagrangian module status.
-     the different values correspond to the following coupling:
-     - 0: Lagrangian module off
-     - 1: Lagrangian two-phase flow in one-way coupling (no influence of
-          the particles on the continuous phase)
-     - 2 Lagrangian two-phase flow with two-way coupling (influence of
-         the particles on the dynamics of the continuous phase). Dynamics,
-         temperature and mass may be coupled independently.
-     - 3 Lagrangian two-phase flow on frozen continuous phase. This option
+     - CS_LAGR_OFF: Lagrangian module off
+     - CS_LAGR_ONEWAY_COUPLING: Lagrangian two-phase flow in one-way coupling
+         (no influence of the particles on the continuous phase)
+     - CS_LAGR_TWOWAY_COUPLING: Lagrangian two-phase flow with two-way coupling
+         (influence of the particles on the dynamics of the continuous phase).
+         Dynamics, temperature and mass may be coupled independently.
+     - CS_LAGR_FROZEN_CONTINUOUS_PHASE: Lagrangian two-phase flow on frozen i
+         continuous phase. This option
          only only be used in case of a calculation restart. All the
          Eulerian fields are frozen (including the scalar fields).
          This option automatically implies \ref iccvfg = 1 */
@@ -163,7 +185,9 @@ typedef struct {
       (starting respectively from the iterations \ref nstist)
       and calculate time-averaged two-way coupling source terms (from the
       time step \ref nstits).
-      Useful if \ref iilagr=1 or \ref iilagr=2 (if \ref iilagr=3,
+      Useful if \ref iilagr = CS_LAGR_ONEWAY_COUPLING
+      or \ref iilagr = CS_LAGR_TWOWAY_COUPLING
+      (if \ref iilagr = CS_LAGR_FROZEN_CONTINUOUS_PHASE,
       then \ref isttio=1 automatically) */
   int  isttio;
 
@@ -560,11 +584,11 @@ typedef struct {
 
   /*! activation (=1) or not (=0) of the two-way coupling on the dynamics
     of the continuous phase.
-    Useful if \ref iilagr = 2 and \ref iccvfg = 0 */
+    Useful if \ref iilagr = CS_LAGR_TWOWAY_COUPLING and \ref iccvfg = 0 */
   int  ltsdyn;
 
   /*! activation (=1) or not (=0) of the two-way coupling on the mass.
-    Useful if \ref iilagr = 2, \ref physical_model = 1 and \ref impvar = 1 */
+    Useful if \ref iilagr = CS_LAGR_TWOWAY_COUPLING, \ref physical_model = 1 and \ref impvar = 1 */
   int  ltsmas;
 
   /*  if \ref physical_model = 1 and \ref itpvar = 1, \ref ltsthe
@@ -572,7 +596,7 @@ typedef struct {
    if \ref physical_model = 2, \ref ltsthe activates (=1) or not (=0) the
    two-way coupling on the eulerian variables related to pulverised
    coal combustion.
-   Useful if \ref iilagr = 2 */
+   Useful if \ref iilagr = CS_LAGR_TWOWAY_COUPLING */
   int  ltsthe;
 
   /*! implicit source term for the continuous phase velocity and
@@ -618,7 +642,7 @@ typedef struct {
     steady state (transition period) and the averages appearing in the source
     terms are reinitialized at each time step, as it is the case for unsteady
     flows (\ref isttio=0).
-    Useful if \ref iilagr = 2 and \ref isttio = 1 */
+    Useful if \ref iilagr = CS_LAGR_TWOWAY_COUPLING and \ref isttio = 1 */
   int  nstits;
 
   /*! number of time steps for source terms accumulations */
@@ -996,6 +1020,12 @@ typedef struct {
 
   /* Reynolds Stress Tensor */
   cs_field_t *cvar_rij;
+
+  /* Total pressure gradient */
+  cs_real_3_t *grad_pr;
+
+  /* velocity gradient */
+  cs_real_33_t *grad_vel;
 
 } cs_lagr_extra_module_t;
 

src/lagr/cs_lagr_gradients.c
@@ -94,15 +94,15 @@ BEGIN_C_DECLS
  *  - velocity gradient
  *
  * \param[in]   time_id   0: current time, 1: previous
- * \param[out]  gradpr    pressure gradient
- * \param[out]  gradvf    velocity gradient
+ * \param[out]  grad_pr   pressure gradient
+ * \param[out]  grad_vel  velocity gradient
  */
 /*----------------------------------------------------------------------------*/
 
 void
 cs_lagr_gradients(int            time_id,
-                  cs_real_3_t   *gradpr,
-                  cs_real_33_t  *gradvf)
+                  cs_real_3_t   *grad_pr,
+                  cs_real_33_t  *grad_vel)
 {
   cs_lnum_t n_cells_with_ghosts = cs_glob_mesh->n_cells_with_ghosts;
   cs_lnum_t n_cells = cs_glob_mesh->n_cells;
@@ -217,7 +217,7 @@ cs_lagr_gradients(int            time_id,
                      wpres,
                      weight,
                      cpl,
-                     gradpr);
+                     grad_pr);
 
   if (wpres != solved_pres)
     BFT_FREE(wpres);
@@ -225,7 +225,7 @@ cs_lagr_gradients(int            time_id,
   if (cs_glob_physical_model_flag[CS_COMPRESSIBLE] < 0) {
     for (cs_lnum_t iel = 0; iel < cs_glob_mesh->n_cells; iel++) {
       for (cs_lnum_t id = 0; id < 3; id++)
-        gradpr[iel][id] += ro0 * grav[id];
+        grad_pr[iel][id] += ro0 * grav[id];
     }
   }
 
@@ -238,7 +238,7 @@ cs_lagr_gradients(int            time_id,
     cs_field_gradient_vector(extra->vel,
                              time_id,
                              inc,
-                             gradvf);
+                             grad_vel);
   }
 }
 

src/lagr/cs_lagr_lec.c
@@ -134,7 +134,7 @@ cs_restart_lagrangian_checkpoint_read(void)
 
   /* Default initializations */
 
-  if (cs_glob_lagr_time_scheme->iilagr == 2) {
+  if (cs_glob_lagr_time_scheme->iilagr == CS_LAGR_TWOWAY_COUPLING) {
 
     for (int ivar = 0; ivar < cs_glob_lagr_dim->ntersl; ivar++) {
 
@@ -601,7 +601,7 @@ cs_restart_lagrangian_checkpoint_read(void)
 
     }
 
-    if (cs_glob_lagr_time_scheme->iilagr == 2) {
+    if (cs_glob_lagr_time_scheme->iilagr == CS_LAGR_TWOWAY_COUPLING) {
 
       {
         cs_int_t tabvar[1];
@@ -1534,7 +1534,7 @@ cs_restart_lagrangian_checkpoint_write(void)
    * ======================================================== */
 
   if (   cs_glob_time_step->nt_cur >= cs_glob_lagr_stat_options->idstnt
-      || cs_glob_lagr_time_scheme->iilagr == 2
+      || cs_glob_lagr_time_scheme->iilagr == CS_LAGR_TWOWAY_COUPLING
       || cs_glob_lagr_dim->n_boundary_stats > 0) {
 
     cs_log_printf(CS_LOG_DEFAULT,
@@ -1673,7 +1673,7 @@ cs_restart_lagrangian_checkpoint_write(void)
 
     /* Source terms for return coupling */
 
-    if (cs_glob_lagr_time_scheme->iilagr == 2) {
+    if (cs_glob_lagr_time_scheme->iilagr == CS_LAGR_TWOWAY_COUPLING) {
 
       {
         char rubriq[] = "iteration_debut_termes_sources_stationnaires";

src/lagr/cs_lagr_log.c
@@ -211,7 +211,7 @@ _log_setup_injection(cs_log_t  log)
   if (cs_glob_lagr_time_scheme == NULL)
     return;
 
-  if (cs_glob_lagr_time_scheme->iilagr < 1)
+  if (cs_glob_lagr_time_scheme->iilagr == CS_LAGR_OFF)
     return;
 
   cs_lagr_extra_module_t *extra = cs_get_lagr_extra_module();
@@ -342,7 +342,7 @@ cs_lagr_log_setup(void)
   if (cs_glob_lagr_time_scheme == NULL)
     return;
 
-  if (cs_glob_lagr_time_scheme->iilagr < 1)
+  if (cs_glob_lagr_time_scheme->iilagr == CS_LAGR_OFF)
     return;
 
   /* Now add Lagrangian setup info */
@@ -555,7 +555,7 @@ cs_lagr_log_iteration(void)
   if (cs_glob_lagr_time_scheme == NULL)
     return;
 
-  if (cs_glob_lagr_time_scheme->iilagr < 1)
+  if (cs_glob_lagr_time_scheme->iilagr == CS_LAGR_OFF)
     return;
 
   const cs_real_t  *b_stats = bound_stat;
@@ -809,7 +809,7 @@ cs_lagr_log_iteration(void)
 
   /* Information about two-way coupling  */
 
-  if (cs_glob_lagr_time_scheme->iilagr == 2) {
+  if (cs_glob_lagr_time_scheme->iilagr == CS_LAGR_TWOWAY_COUPLING) {
 
     if (cs_glob_lagr_time_scheme->isttio == 0) {
 

src/lagr/cs_lagr_options.c
@@ -282,7 +282,7 @@ cs_lagr_option_definition(cs_int_t   *isuite,
 
   /* Default initializations for Lagrangian module. */
 
-  lagr_time_scheme->iilagr = 0;
+  lagr_time_scheme->iilagr = CS_LAGR_OFF;
   lagr_time_scheme->isuila = 0;
 
   cs_glob_lagr_stat_options->isuist = 0;
@@ -347,7 +347,7 @@ cs_lagr_option_definition(cs_int_t   *isuite,
 
   cs_user_lagr_model();
 
-  if (lagr_time_scheme->iilagr == 0) {
+  if (lagr_time_scheme->iilagr == CS_LAGR_OFF) {
 
     _free_lagr_encrustation_pointers();
     _free_lagr_boundary_interaction_pointers();
@@ -369,12 +369,12 @@ cs_lagr_option_definition(cs_int_t   *isuite,
                                 _("in Lagrangian module"),
                                 "cs_glob_lagr_time_scheme->iilagr",
                                 lagr_time_scheme->iilagr,
-                                0, 4);
+                                CS_LAGR_OFF, CS_LAGR_FROZEN_CONTINUOUS_PHASE + 1);
 
   /* Restart needed if computation on frozen field.
      Note that for the Lagrangian module, frozen field also includes scalars. */
 
-  if (lagr_time_scheme->iilagr == 3 && *isuite != 1)
+  if (lagr_time_scheme->iilagr == CS_LAGR_FROZEN_CONTINUOUS_PHASE && *isuite != 1)
     cs_parameters_error
       (CS_ABORT_DELAYED,
        _("in Lagrangian module"),
@@ -383,10 +383,10 @@ cs_lagr_option_definition(cs_int_t   *isuite,
          "but the background Eulerian computation is not a restart.\n"),
        lagr_time_scheme->iilagr);
 
-  if (lagr_time_scheme->iilagr == 3)
+  if (lagr_time_scheme->iilagr == CS_LAGR_FROZEN_CONTINUOUS_PHASE)
     *iccvfg = 1;
 
-  if (   lagr_time_scheme->iilagr != 2
+  if (   lagr_time_scheme->iilagr != CS_LAGR_TWOWAY_COUPLING
       && cs_glob_physical_model_flag[CS_COMBUSTION_PCLC] >= 1)
     cs_parameters_error
       (CS_ABORT_DELAYED,
@@ -396,10 +396,10 @@ cs_lagr_option_definition(cs_int_t   *isuite,
          "on the continuous phase is not activated:\n"
          "  cs_glob_lagr_time_scheme->iilagr = %d\n"
          "The return coupling must be acivated for this model:\n"
-         "  cs_glob_lagr_time_scheme->iilagr = 2\n"),
+         "  cs_glob_lagr_time_scheme->iilagr = CS_LAGR_TWOWAY_COUPLING\n"),
        lagr_time_scheme->iilagr);
 
-  if (lagr_time_scheme->iilagr > 0
+  if (lagr_time_scheme->iilagr != CS_LAGR_OFF
       && (   cs_glob_time_step->is_local
           || cs_glob_time_step->is_variable)) {
 
@@ -1532,7 +1532,7 @@ cs_lagr_option_definition(cs_int_t   *isuite,
 
   /* ISTTIO NSTITS LTSDYN LTSMAS LTSTHE  */
   /* Si champs figes alors forcement en stationnaire    */
-  if (lagr_time_scheme->iilagr == 3)
+  if (lagr_time_scheme->iilagr == CS_LAGR_FROZEN_CONTINUOUS_PHASE)
     lagr_time_scheme->isttio = 1;
 
   cs_parameters_is_in_range_int(CS_ABORT_DELAYED,
@@ -1541,7 +1541,7 @@ cs_lagr_option_definition(cs_int_t   *isuite,
                                 lagr_time_scheme->isttio,
                                 0, 2);
 
-  if (lagr_time_scheme->iilagr == 2) {
+  if (lagr_time_scheme->iilagr == CS_LAGR_TWOWAY_COUPLING) {
 
     if (lagr_time_scheme->isttio == 1 &&
         cs_glob_lagr_source_terms->nstits < 1) {

commit f6e596dea1e7048e58088799f1476e0a1763113b

Fix bug introduced in commit 1f2f792 (2019-01-21)

src/lagr/cs_lagr.c
@@ -1643,9 +1643,6 @@ cs_lagr_solve_initialize(const cs_real_t  *dt)
 
     }
 
-    /* Compute gradients of current value fields */
-    cs_lagr_gradients(0, extra->grad_pr, extra->grad_vel);
-
   }
 
   /* First call (initializations)
@@ -1663,6 +1660,11 @@ cs_lagr_solve_initialize(const cs_real_t  *dt)
 
   if (cs_glob_lagr_time_scheme->iilagr != CS_LAGR_OFF)
     cs_lagr_restart_read_p();
+
+  /* Compute gradients of current value fields */
+  if (cs_glob_lagr_time_scheme->iilagr == CS_LAGR_FROZEN_CONTINUOUS_PHASE)
+    cs_lagr_gradients(0, extra->grad_pr, extra->grad_vel);
+
 }
 
 /*----------------------------------------------------------------------------