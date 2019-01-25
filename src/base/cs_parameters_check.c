code-saturne/code_saturne

commit 2dd2b11c258642038bf9d69877786528d8884018

Add the non linear quadratic k-epsilon model from Baglietto et al.   * Activated with iturb = 23   * Low Re model, compatible with wall functions

src/base/cs_parameters_check.c
@@ -1279,14 +1279,14 @@ cs_parameters_check(void)
   /* Turbulence */
 
   /* Model */
-  const int iturb_vals[15] = {0,          /* laminar */
-                              10,         /* mixing length */
-                              20, 21, 22, /* k-epsilon */
-                              30, 31, 32, /* RSM */
-                              40, 41, 42, /* LES */
-                              50, 51,     /* v2f */
-                              60,         /* k-omega */
-                              70};        /* Spallart-Allmaras */
+  const int iturb_vals[15] = {0,              /* laminar */
+                              10,             /* mixing length */
+                              20, 21, 22, 23, /* k-epsilon */
+                              30, 31, 32,     /* RSM */
+                              40, 41, 42,     /* LES */
+                              50, 51,         /* v2f */
+                              60,             /* k-omega */
+                              70};            /* Spallart-Allmaras */
 
   cs_parameters_is_in_list_int(CS_ABORT_DELAYED,
                                _("while reading input data"),
@@ -1421,15 +1421,17 @@ cs_parameters_check(void)
                                   cs_glob_turb_rans_model->ikecou,
                                   0, 2);
 
-    /* En k-eps a prod lin et en v2f on force IKECOU a 0 */
+    /* En k-eps a prod lin/LS/Quad et en v2f on force IKECOU a 0 */
     if (   cs_glob_turb_model->iturb == 21
         || cs_glob_turb_model->iturb == 22
+        || cs_glob_turb_model->iturb == 23
         || cs_glob_turb_model->itytur == 5) {
       cs_parameters_is_equal_int(CS_ABORT_DELAYED,
                                  _("while reading input data,\n"
                                    "with k-epsilon LP (iturb=21),"
-                                   "k-epsilon LS (iturb=22) or v2f model "
-                                   "(iturb=50/51)"),
+                                   "k-epsilon LS (iturb=22), "
+                                   "k-epislon quadratic (iturb=23)"
+                                   "or v2f model (iturb=50/51)"),
                                  "cs_glob_turb_rans_model->ikecou",
                                  cs_glob_turb_rans_model->ikecou,
                                  0);

src/base/addfld.f90
@@ -346,7 +346,8 @@ endif
 
 ! Wall distance for some turbulence models
 
-if ((iturb.eq.30.and.irijec.eq.1).or.              &
+if ( iturb.eq.23.or.                                &
+     (iturb.eq.30.and.irijec.eq.1).or.              &
      (itytur.eq.4.and.idries.eq.1).or.              &
      iturb.eq.60.or.iturb.eq.70      ) then
   ineedy = 1

src/base/clptur.f90
@@ -922,7 +922,9 @@ do ifac = 1, nfabor
 
     if (itytur.eq.2) then
 
+      ! ==================================
       ! Launder Sharma boundary conditions
+      ! ==================================
       if(iturb.eq.22) then
 
         ! Dirichlet Boundary Condition on k
@@ -980,7 +982,66 @@ do ifac = 1, nfabor
              coefb_ep(ifac), coefbf_ep(ifac),             &
              pimp         , hint          , rinfin )
 
+
+      ! ===================================
+      ! Quadratic Baglietto k-epsilon model
+      ! ===================================
+      else if(iturb.eq.23) then
+        
+        ! Dirichlet Boundary Condition on k
+        !----------------------------------
+        if (iwallf.eq.0) then
+          ! No wall functions forces by user
+          pimp = 0.d0
+        else
+          ! Use of wall functions
+          if (iuntur.eq.1) then
+            pimp = uk**2/sqrcmu
+          else
+            pimp = 0.d0
+          endif
+        endif
+
+        hint = (visclc+visctc/sigmak)/distbf
+
+        call set_dirichlet_scalar &
+             !====================
+           ( coefa_k(ifac), coefaf_k(ifac),             &
+             coefb_k(ifac), coefbf_k(ifac),             &
+             pimp         , hint          , rinfin )
+
+        ! Dirichlet Boundary Condition on epsilon
+        !---------------------------------------
+        if(iwallf.ne.0) then
+
+          pimp_lam = 2.0d0*visclc/romc*cvar_k(iel)/distbf**2
+
+          if (yplus > epzero) then
+            pimp_turb = 5.d0*uk**4*romc/        &
+                        (xkappa*visclc*(yplus+dplus))
+
+            ! Blending between wall and homogeneous layer
+            fep       = exp(-(yplus/4.d0)**1.5d0)
+            dep       = 1.d0- exp(-(yplus/9.d0)**2.1d0)
+            pimp      = fep*pimp_lam + (1.d0-fep)*dep*pimp_turb
+          else
+            pimp = pimp_lam
+          end if
+
+        else
+          
+          pimp = 2.0d0*visclc/romc*cvar_k(iel)/distbf**2
+        end if
+
+        call set_dirichlet_scalar &
+             !====================
+           ( coefa_ep(ifac), coefaf_ep(ifac),             &
+             coefb_ep(ifac), coefbf_ep(ifac),             &
+             pimp          , hint           , rinfin )
+
+      ! ==============================================
       ! k-epsilon and k-epsilon LP boundary conditions
+      ! ==============================================
       else
 
         ! Dirichlet Boundary Condition on k

src/base/cstphy.f90
@@ -325,6 +325,31 @@ module cstphy
   !> Useful if and only if \ref optcal::iturb "iturb"= 20, 21 or 30
   !> (\f$k-\varepsilon\f$ or \f$R_{ij}-\varepsilon\f$ LRR)
   double precision, save :: ce2
+  
+  !> constant \f$C_{NL1}\f$ for the \f$k-\varepsilon\f$ 
+  !> model from Baglietto et al. (quadratric)
+  !> Useful if and only if \ref optcal::iturb "iturb"= 23
+  double precision, save :: cnl1
+
+  !> constant \f$C_{NL2}\f$ for the \f$k-\varepsilon\f$ 
+  !> model from Baglietto et al. (quadratric)
+  !> Useful if and only if \ref optcal::iturb "iturb"= 23
+  double precision, save :: cnl2
+
+  !> constant \f$C_{NL3}\f$ for the \f$k-\varepsilon\f$ 
+  !> model from Baglietto et al. (quadratric)
+  !> Useful if and only if \ref optcal::iturb "iturb"= 23
+  double precision, save :: cnl3
+
+  !> constant \f$C_{NL4}\f$ for the \f$k-\varepsilon\f$ 
+  !> model from Baglietto et al. (quadratric)
+  !> Useful if and only if \ref optcal::iturb "iturb"= 23
+  double precision, save :: cnl4
+
+  !> constant \f$C_{NL5}\f$ for the \f$k-\varepsilon\f$ 
+  !> model from Baglietto et al. (quadratric)
+  !> Useful if and only if \ref optcal::iturb "iturb"= 23
+  double precision, save :: cnl5
 
   !> Coefficient of interfacial coefficient in k-eps,
   !> used in Lagrange treatment

src/base/iniini.f90
@@ -776,6 +776,13 @@ ce2     = 1.92d0
 ce4     = 1.20d0
 sigmak  = 1.00d0
 
+!   pour le k-epsilon quadratic (Baglietto)
+cnl1  = 0.8d0
+cnl2  = 11.d0
+cnl3  = 4.5d0
+cnl4  = 1.d3
+cnl5  = 1.d0
+
 !   pour le Rij-epsilon standard (et SSG pour CRIJ3)
 crij1  = 1.80d0
 crij2  = 0.60d0

src/base/modini.f90
@@ -749,8 +749,8 @@ if (icdpar.eq.-1 .and. ikw.eq.1 .and. isuite.eq.1)                &
 if (ikecou.eq.-999) then
   if (idtvar.lt.0) then
     ikecou = 0
-  else if (iturb.eq.21 .or. iturb.eq.22 .or. itytur.eq.5           &
-       .or. iturb.eq.60 ) then
+  else if (iturb.eq.21 .or. iturb.eq.22 .or. iturb.eq.23 .or. &
+           itytur.eq.5 .or. iturb.eq.60 ) then
     ikecou = 0
   else
     ikecou = 1

src/base/phyvar.f90
@@ -290,6 +290,10 @@ elseif (itytur.eq.2) then
   call field_get_val_s(ivarfl(iep), cvar_ep)
 
   if (iturb.eq.22) then
+    
+    ! 4.3.1 Launder-Sharma
+    ! --------------------
+
     do iel = 1, ncel
       xk   = cvar_k(iel)
       xe   = cvar_ep(iel)
@@ -299,12 +303,25 @@ elseif (itytur.eq.2) then
       xfmu = exp(-3.4d0/(1.d0+xmut/xmu/50.d0)**2.d0)
       visct(iel) = cmu*xfmu*xmut
     enddo
+
+  else if (iturb.eq.23) then
+
+    ! 4.3.2 Non-linear quadratic Baglietto
+    ! ------------------------------------
+    
+    call visqke
+  
   else
+
+    ! 4.3.3 Standard and Linear Production
+    ! ------------------------------------
+
     do iel = 1, ncel
       xk = cvar_k(iel)
       xe = cvar_ep(iel)
       visct(iel) = crom(iel)*cmu*xk**2/xe
     enddo
+
   endif
 
 elseif (itytur.eq.3) then

src/base/predvv.f90
@@ -951,87 +951,114 @@ endif
 
 !-------------------------------------------------------------------------------
 ! ---> - Divergence of tensor Rij
+! ---> - Non linear part of Rij for non-liear Eddy Viscosity Models
 
-if(itytur.eq.3.and.iterns.eq.1) then
+if((itytur.eq.3.or.iturb.eq.23).and.iterns.eq.1) then
 
   allocate(rij(6,ncelet))
-  if(irijco.eq.1) then !TODO change index of rij
-    do iel = 1, ncelet
-      rij(1,iel) = cvara_rij(1,iel)
-      rij(2,iel) = cvara_rij(2,iel)
-      rij(3,iel) = cvara_rij(3,iel)
-      rij(4,iel) = cvara_rij(4,iel)
-      rij(5,iel) = cvara_rij(5,iel)
-      rij(6,iel) = cvara_rij(6,iel)
-    enddo
-  else
-    do iel = 1, ncelet
-      rij(1,iel) = cvara_r11(iel)
-      rij(2,iel) = cvara_r22(iel)
-      rij(3,iel) = cvara_r33(iel)
-      rij(4,iel) = cvara_r12(iel)
-      rij(5,iel) = cvara_r23(iel)
-      rij(6,iel) = cvara_r13(iel)
-    enddo
-  endif
-! --- Boundary conditions on the components of the tensor Rij
-
   allocate(coefat(6,nfabor))
-  if(irijco.eq.1) then
-    call field_get_coefad_v(ivarfl(irij),coefap)
-    coefat = coefap
-  else
-    call field_get_coefad_s(ivarfl(ir11),coef1)
-    call field_get_coefad_s(ivarfl(ir22),coef2)
-    call field_get_coefad_s(ivarfl(ir33),coef3)
-    call field_get_coefad_s(ivarfl(ir12),coef4)
-    call field_get_coefad_s(ivarfl(ir23),coef5)
-    call field_get_coefad_s(ivarfl(ir13),coef6)
-    do ifac = 1, nfabor
-      coefat(1,ifac) = coef1(ifac)
-      coefat(2,ifac) = coef2(ifac)
-      coefat(3,ifac) = coef3(ifac)
-      coefat(4,ifac) = coef4(ifac)
-      coefat(5,ifac) = coef5(ifac)
-      coefat(6,ifac) = coef6(ifac)
-    enddo
-  endif
-
   allocate(coefbt(6,6,nfabor))
-  do ifac = 1, nfabor
-    do ii = 1, 6
-      do jj = 1, 6
-        coefbt(jj,ii,ifac) = 0.d0
+
+  ! Reynolds Stress Models
+  if(itytur.eq.3) then 
+
+    if(irijco.eq.1) then !TODO change index of rij
+      do iel = 1, ncelet
+        rij(1,iel) = cvara_rij(1,iel)
+        rij(2,iel) = cvara_rij(2,iel)
+        rij(3,iel) = cvara_rij(3,iel)
+        rij(4,iel) = cvara_rij(4,iel)
+        rij(5,iel) = cvara_rij(5,iel)
+        rij(6,iel) = cvara_rij(6,iel)
+      enddo
+    else
+      do iel = 1, ncelet
+        rij(1,iel) = cvara_r11(iel)
+        rij(2,iel) = cvara_r22(iel)
+        rij(3,iel) = cvara_r33(iel)
+        rij(4,iel) = cvara_r12(iel)
+        rij(5,iel) = cvara_r23(iel)
+        rij(6,iel) = cvara_r13(iel)
+      enddo
+    endif
+    ! --- Boundary conditions on the components of the tensor Rij
+
+    if(irijco.eq.1) then
+      call field_get_coefad_v(ivarfl(irij),coefap)
+      coefat = coefap
+    else
+      call field_get_coefad_s(ivarfl(ir11),coef1)
+      call field_get_coefad_s(ivarfl(ir22),coef2)
+      call field_get_coefad_s(ivarfl(ir33),coef3)
+      call field_get_coefad_s(ivarfl(ir12),coef4)
+      call field_get_coefad_s(ivarfl(ir23),coef5)
+      call field_get_coefad_s(ivarfl(ir13),coef6)
+      do ifac = 1, nfabor
+        coefat(1,ifac) = coef1(ifac)
+        coefat(2,ifac) = coef2(ifac)
+        coefat(3,ifac) = coef3(ifac)
+        coefat(4,ifac) = coef4(ifac)
+        coefat(5,ifac) = coef5(ifac)
+        coefat(6,ifac) = coef6(ifac)
+      enddo
+    endif
+
+    do ifac = 1, nfabor
+      do ii = 1, 6
+        do jj = 1, 6
+          coefbt(jj,ii,ifac) = 0.d0
+        enddo
       enddo
     enddo
-  enddo
 
-  if(irijco.eq.1) then
-    call field_get_coefbd_v(ivarfl(irij),coefbp)
-    coefbt = coefbp
-  else
-    call field_get_coefbd_s(ivarfl(ir11),coef1)
-    call field_get_coefbd_s(ivarfl(ir22),coef2)
-    call field_get_coefbd_s(ivarfl(ir33),coef3)
-    call field_get_coefbd_s(ivarfl(ir12),coef4)
-    call field_get_coefbd_s(ivarfl(ir23),coef5)
-    call field_get_coefbd_s(ivarfl(ir13),coef6)
+    if(irijco.eq.1) then
+      call field_get_coefbd_v(ivarfl(irij),coefbp)
+      coefbt = coefbp
+    else
+      call field_get_coefbd_s(ivarfl(ir11),coef1)
+      call field_get_coefbd_s(ivarfl(ir22),coef2)
+      call field_get_coefbd_s(ivarfl(ir33),coef3)
+      call field_get_coefbd_s(ivarfl(ir12),coef4)
+      call field_get_coefbd_s(ivarfl(ir23),coef5)
+      call field_get_coefbd_s(ivarfl(ir13),coef6)
+      do ifac = 1, nfabor
+        coefbt(1,1,ifac) = coef1(ifac)
+        coefbt(2,2,ifac) = coef2(ifac)
+        coefbt(3,3,ifac) = coef3(ifac)
+        coefbt(4,4,ifac) = coef4(ifac)
+        coefbt(5,5,ifac) = coef5(ifac)
+        coefbt(6,6,ifac) = coef6(ifac)
+      enddo
+    endif
+  
+  ! Baglietto et al. quadratic k-epislon model
+  else if(iturb.eq.23) then
+
+    ! --- Compute the non linear part of Rij
+    call cnlevm (rij)
+    
+    ! --- Boundary conditions : Homogeneous Neumann
     do ifac = 1, nfabor
-      coefbt(1,1,ifac) = coef1(ifac)
-      coefbt(2,2,ifac) = coef2(ifac)
-      coefbt(3,3,ifac) = coef3(ifac)
-      coefbt(4,4,ifac) = coef4(ifac)
-      coefbt(5,5,ifac) = coef5(ifac)
-      coefbt(6,6,ifac) = coef6(ifac)
+      do ii = 1, 6
+        coefat(ii,ifac) = 0.d0
+        do jj = 1, 6
+          coefbt(jj,ii,ifac) = 1.d0
+        enddo
+      enddo
     enddo
-  endif
+
+  end if
 
   ! Flux computation options
   f_id = -1
   init = 1;
   inc  = 1;
   iflmb0 = 0;
-  call field_get_key_struct_var_cal_opt(ivarfl(ir11), vcopt)
+  if(itytur.eq.3) then
+    call field_get_key_struct_var_cal_opt(ivarfl(ir11), vcopt)
+  else
+    call field_get_key_struct_var_cal_opt(ivarfl(ik), vcopt)
+  end if
   nswrgp = vcopt%nswrgr;
   imligp = vcopt%imligr;
   iwarnp = vcopt%iwarni;
@@ -1104,7 +1131,6 @@ if(itytur.eq.3.and.iterns.eq.1) then
   endif
 
 endif
-
 
 !-------------------------------------------------------------------------------
 ! ---> Face diffusivity for the velocity

commit 63dc581659ae8243896a3ed2251c59e01762fe26

Fix bug introduced in commit 2dd2b11c (24-01-2019)

src/base/cs_parameters_check.c
@@ -1279,7 +1279,7 @@ cs_parameters_check(void)
   /* Turbulence */
 
   /* Model */
-  const int iturb_vals[15] = {0,              /* laminar */
+  const int iturb_vals[16] = {0,              /* laminar */
                               10,             /* mixing length */
                               20, 21, 22, 23, /* k-epsilon */
                               30, 31, 32,     /* RSM */