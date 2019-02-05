code-saturne/code_saturne

commit fd4431a0cd2d95cbbb8d347df0f10bd69aeffb16

Major MEG: Change behaviour. Functions are now directly created in RESU/src folder and not in the case SRC folder

bin/cs_case_domain.py
@@ -43,6 +43,8 @@ from cs_exec_environment import run_command, source_shell_script
 from cs_exec_environment import enquote_arg, separate_args
 from cs_exec_environment import source_syrthes_env
 
+from cs_mei_to_c import mei_to_c_interpreter
+
 #===============================================================================
 # Utility functions
 #===============================================================================
@@ -435,6 +437,9 @@ class domain(base_domain):
 
         self.adaptation = adaptation
 
+        # MEG expression generator
+        self.mci = None
+
     #---------------------------------------------------------------------------
 
     def __set_auto_restart__(self):
@@ -590,6 +595,24 @@ class domain(base_domain):
 
         if self.exec_solver and len(src_files) > 0:
             return True
+        elif self.param != None:
+            from Base.XMLengine import Case
+            from Base.XMLinitialize import XMLinit
+
+            fp = os.path.join(self.data_dir, self.param)
+            case = Case(package=self.package, file_name=fp)
+            case['xmlfile'] = fp
+            case.xmlCleanAllBlank(case.xmlRootNode())
+            XMLinit(case).initialize()
+            case.xmlSaveDocument()
+
+            case['case_path'] = self.exec_dir
+            self.mci = mei_to_c_interpreter(case)
+            if self.mci.has_meg_code():
+                return True
+            else:
+                self.mci = None
+                return False
         else:
             return False
 
@@ -600,27 +623,31 @@ class domain(base_domain):
         Compile and link user subroutines if necessary
         """
         # Check if there are files to compile in source path
-
+        # or if MEG functions need to be generated
         src_files = cs_compile.files_to_compile(self.src_dir)
 
-        if len(src_files) > 0:
+        if len(src_files) > 0 or self.mci != None:
 
-            # Add header files to list so as not to forget to copy them
-
-            dir_files = os.listdir(self.src_dir)
-            src_files = src_files + (  fnmatch.filter(dir_files, '*.h')
-                                     + fnmatch.filter(dir_files, '*.hxx')
-                                     + fnmatch.filter(dir_files, '*.hpp'))
-
+            # Create the src folder
             exec_src = os.path.join(self.exec_dir, 'src')
 
-            # Copy source files to execution directory
-
             make_clean_dir(exec_src)
-            for f in src_files:
-                src_file = os.path.join(self.src_dir, f)
-                dest_file = os.path.join(exec_src, f)
-                shutil.copy2(src_file, dest_file)
+
+            if len(src_files) > 0:
+                # Add header files to list so as not to forget to copy them
+                dir_files = os.listdir(self.src_dir)
+                src_files = src_files + (  fnmatch.filter(dir_files, '*.h')
+                                         + fnmatch.filter(dir_files, '*.hxx')
+                                         + fnmatch.filter(dir_files, '*.hpp'))
+
+                # Copy source files to execution directory
+                for f in src_files:
+                    src_file = os.path.join(self.src_dir, f)
+                    dest_file = os.path.join(exec_src, f)
+                    shutil.copy2(src_file, dest_file)
+
+            if self.mci != None:
+                mci_state = self.mci.save_all_functions()
 
             log_name = os.path.join(self.exec_dir, 'compile.log')
             log = open(log_name, 'w')

Makefile.am
@@ -265,6 +265,7 @@ bin/cs_runcase.py \
 bin/cs_salome.py \
 bin/cs_script.py \
 bin/cs_submit.py \
+bin/cs_mei_to_c.py \
 bin/cs_xml_reader.py
 
 studymanagerdir = $(pkgpythondir)/studymanager

bin/cs_mei_to_c.py
@@ -1,4 +1,4 @@
-import os, shutil
+import os
 import re
 
 from code_saturne.Pages.NotebookModel import NotebookModel
@@ -92,6 +92,16 @@ class mei_to_c_interpreter:
         self.notebook = {}
         for (nme, val) in nb.getNotebookList():
             self.notebook[nme] = str(val)
+        print(len(self.notebook))
+
+        # Volume code
+        self.generate_volume_code()
+
+        # Bouondary code
+        self.generate_boundary_code()
+
+        print(len(self.vol_funcs))
+        print(len(self.bnd_funcs))
     # -------------------------------
 
     # -------------------------------
@@ -492,48 +502,7 @@ class mei_to_c_interpreter:
     # -------------------------------
 
     # -------------------------------
-    def delete_file(self, c_file_name):
-
-        # Copy function file if needed
-        fpath = os.path.join(self.case['case_path'], 'SRC', c_file_name);
-        if os.path.isfile(fpath):
-            os.remove(fpath)
-    # -------------------------------
-
-    # -------------------------------
-    def save_file(self, c_file_name, code_to_write):
-
-        if code_to_write != '':
-            # Try and write the volume function in the SRC folder
-            try:
-                fpath = os.path.join(self.case['case_path'], 'SRC', c_file_name)
-                new_file = open(fpath, 'w')
-                new_file.write(code_to_write)
-                new_file.close()
-                return 0
-
-            except:
-                # If the SRC doesnt exist, check if we are in a RESU folder, and
-                # hence a src folder is present. A warning will be shown
-                # if it is the case
-                try:
-                    fpath = os.path.join(self.case['case_path'], 'src', c_file_name)
-                    new_file = open(fpath, 'w')
-                    new_file.write(code_to_write)
-                    new_file.close()
-                    return -1
-
-                except:
-                    # Cant save the function. xml file will still be saved
-                    return 1
-
-        # Return 0 if nothing is written for robustness
-        else:
-            return 0
-    # -------------------------------
-
-    # -------------------------------
-    def save_volume_function(self):
+    def generate_volume_code(self):
 
         if self.case['package'].name == 'code_saturne':
             authorized_fields = ['density', 'molecular_viscosity',
@@ -594,37 +563,10 @@ class mei_to_c_interpreter:
                         exp, req, sca, sym = tm.getFormulaComponents(fieldId,
                                                                     'temperature')
                         self.init_cell_block(exp, req, sym, sca, name)
-
-
-        # Delete previous existing file
-        file2write = 'cs_meg_volume_function.c'
-        self.delete_file(file2write)
-
-        # Generate the functions code if needed
-        code_to_write = ''
-        if len(self.vol_funcs.keys()) > 0:
-            code_to_write = _file_header + _vol_function_header
-            for key in self.vol_funcs.keys():
-                m1 = 'User defined formula for variable %s over zone %s' \
-                        % (key, self.vol_funcs[key]['zone'])
-                m2 = '/* '+'-'*(len(m1))+' */\n'
-                m1 = '/* '+m1+' */'
-
-                code_to_write += "  " + m2
-                code_to_write += "  " + m1 + '\n'
-                code_to_write += self.write_cell_block(key)
-                code_to_write += "  " + m2 + '\n'
-
-            code_to_write += _file_footer
-
-        # Write the C file if necessary
-        write_status = self.save_file(file2write, code_to_write)
-
-        return write_status
     # -------------------------------
 
     # -------------------------------
-    def save_boundary_function(self):
+    def generate_boundary_code(self):
 
         if self.case['package'].name == 'code_saturne':
             from code_saturne.Pages.LocalizationModel import LocalizationModel
@@ -735,8 +677,82 @@ class mei_to_c_interpreter:
                         else:
                             req = [sca, 'hc']
                         self.init_bnd_block(exp, req, sca, zone._label, c)
+    # -------------------------------
 
+    # -------------------------------
+    def has_meg_code(self):
 
+        retcode = False
+
+        if len(self.vol_funcs) > 0 or len(self.bnd_funcs) > 0:
+            retcode = True
+
+        return retcode
+    # -------------------------------
+
+    # -------------------------------
+    def delete_file(self, c_file_name):
+
+        # Copy function file if needed
+        fpath = os.path.join(self.case['case_path'], 'SRC', c_file_name);
+        if os.path.isfile(fpath):
+            os.remove(fpath)
+    # -------------------------------
+
+    # -------------------------------
+    def save_file(self, c_file_name, code_to_write):
+
+        if code_to_write != '':
+            # Try and write the volume function in the src if in RESU folder
+            # For debugging purposes
+            try:
+                fpath = os.path.join(self.case['case_path'], 'src', c_file_name)
+                new_file = open(fpath, 'w')
+                new_file.write(code_to_write)
+                new_file.close()
+                return 1
+
+            except:
+                # Cant save the function. xml file will still be saved
+                return -1
+
+        # Return 0 if nothing is written for robustness
+        else:
+            return 0
+    # -------------------------------
+
+    # -------------------------------
+    def save_volume_function(self):
+
+        # Delete previous existing file
+        file2write = 'cs_meg_volume_function.c'
+        self.delete_file(file2write)
+
+        # Generate the functions code if needed
+        code_to_write = ''
+        if len(self.vol_funcs.keys()) > 0:
+            code_to_write = _file_header + _vol_function_header
+            for key in self.vol_funcs.keys():
+                m1 = 'User defined formula for variable %s over zone %s' \
+                        % (key, self.vol_funcs[key]['zone'])
+                m2 = '/* '+'-'*(len(m1))+' */\n'
+                m1 = '/* '+m1+' */'
+
+                code_to_write += "  " + m2
+                code_to_write += "  " + m1 + '\n'
+                code_to_write += self.write_cell_block(key)
+                code_to_write += "  " + m2 + '\n'
+
+            code_to_write += _file_footer
+
+        # Write the C file if necessary
+        write_status = self.save_file(file2write, code_to_write)
+
+        return write_status
+    # -------------------------------
+
+    # -------------------------------
+    def save_boundary_function(self):
 
         # Delete previous existing file
         file2write = 'cs_meg_boundary_function.c'

bin/studymanager/cs_studymanager_study.py
@@ -59,8 +59,6 @@ except Exception:
 
 from studymanager.cs_studymanager_run import run_studymanager_command
 
-from code_saturne.Pages.QMeiToCCode import mei_to_c_interpreter
-
 #-------------------------------------------------------------------------------
 # log config.
 #-------------------------------------------------------------------------------
@@ -266,11 +264,6 @@ class Case(object):
                     case.xmlCleanAllBlank(case.xmlRootNode())
                     XMLinit(case).initialize()
                     case.xmlSaveDocument()
-
-                    # Save MEG functions
-                    case['case_path'] = os.path.join(self.__repo, subdir)
-                    mci = mei_to_c_interpreter(case)
-                    mci_state = mci.save_all_functions()
 
 
         # 2) Create RESU and SRC directory if needed

gui/Base/MainView.py
@@ -94,7 +94,7 @@ from code_saturne.Pages.ScriptRunningModel import ScriptRunningModel
 from code_saturne.Pages.SolutionDomainModel import getRunType
 from code_saturne.Base.QtPage import getexistingdirectory
 from code_saturne.Base.QtPage import from_qvariant, to_text_string, getopenfilename, getsavefilename
-from code_saturne.Pages.QMeiToCCode import mei_to_c_interpreter
+from code_saturne.cs_mei_to_c import mei_to_c_interpreter
 #-------------------------------------------------------------------------------
 # log config
 #-------------------------------------------------------------------------------
@@ -1185,20 +1185,6 @@ class MainView(object):
             state = mci.save_all_functions()
 
             if state == 1:
-                title = self.tr("Save error")
-                msg  = "You are not in a CASE structure.\n"
-                msg += "The xml file was saved, but the User Definitions' "
-                msg += "functions could not be saved in the SRC folder.\n"
-                msg += "Please re-open your xml file within a CASE structure "
-                msg += "and save it again."
-                err_msg = self.tr(msg)
-                QMessageBox.critical(self, title, err_msg)
-                msg = self.tr("Saving incomplete.\n \
-                              Reopen xml file within a CASE structure")
-                self.statusbar.showMessage(msg, 2000)
-                self.updateTitleBar()
-                return
-            elif state == -1:
                 title = self.tr("Warning!")
                 msg  = "You are within a RESU folder!\n"
                 msg += "The xml file was saved, as the cs_meg C functions "

gui/Pages/FluidCharacteristicsView.py
@@ -35,7 +35,6 @@ This module contains the following classes and function:
 #-------------------------------------------------------------------------------
 
 import logging
-#from code_saturne.Pages.QMeiToCCode import mei_to_c_interpreter
 
 #-------------------------------------------------------------------------------
 # EOS

gui/Pages/Makefile.am
@@ -150,7 +150,6 @@ PreprocessingView.py \
 ProfilesModel.py \
 ProfilesView.py \
 QMeiEditorView.py \
-QMeiToCCode.py \
 SalomeActors.py \
 SalomeHandler.py \
 ScriptRunningModel.py \

commit 7520a3bc14abfcfd7e69456aae29107839931906

Fix launch of prepro mode, broken by commit fd4431a (28/01/2019)

bin/cs_case_domain.py
@@ -601,12 +601,15 @@ class domain(base_domain):
         if self.param != None:
             from model.XMLengine import Case
             from model.XMLinitialize import XMLinit
+            from code_saturne.model.SolutionDomainModel import getRunType
 
             fp = os.path.join(self.data_dir, self.param)
             case = Case(package=self.package, file_name=fp)
             case['xmlfile'] = fp
             case.xmlCleanAllBlank(case.xmlRootNode())
-            XMLinit(case).initialize()
+
+            prepro = (getRunType(case) != 'standard')
+            XMLinit(case).initialize(prepro)
             case.xmlSaveDocument()
 
             case['case_path'] = self.exec_dir