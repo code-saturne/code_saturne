32a138102cf3dedeacd56a5d4360cb411f4fcdcd
Jérôme Bonelle <jerome.bonelle@edf.fr>
2019-02-06 17:18:38 +0100

    CDO: (minor) Remove an unused macro to build flags for the algebraic systems

--- src/cdo/cs_flag.h
+++ src/cdo/cs_flag.h
@@ -62,7 +62,6 @@ BEGIN_C_DECLS
 #define CS_FLAG_SYS_TIME_DIAG    (1 << 2) /*!<  4: lumping by construction */
 #define CS_FLAG_SYS_SOURCES_HLOC (1 << 3) /*!<  8: Hodge op. for source terms */
 #define CS_FLAG_SYS_VECTOR       (1 << 4) /*!< 16: vector-valued variable */
-#define CS_FLAG_SYS_DEBUG        (1 << 5) /*!< 32: activate debug mode */
 
 /*!
  * @}
