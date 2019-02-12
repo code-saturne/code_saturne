code-saturne/code_saturne

commit 01d839439f6330a1bc78ff6161eca159df8a2ee1

Improve Model / View separation in GUI: * move gui models to bin/model directory,   this gives access to GUI model functions in an installation of Code_Saturne   without GUI (also fix a dependency to GUI code of studymanager by moving path   model of studymanager moved to bin/studymanager) * light code reorganisation in order to ensure that model code does not draw   any Qt class or function (dicolabel, GuiParam moved to Common).

bin/studymanager/cs_studymanager_study.py
@@ -45,8 +45,8 @@ import cs_create
 from cs_create import set_executable
 import cs_runcase
 
-from code_saturne.Base import XMLengine
-from code_saturne.studymanager_gui import PathesModel
+from code_saturne.model import XMLengine
+from code_saturne.studymanager.cs_studymanager_pathes_model import PathesModel
 
 from studymanager.cs_studymanager_parser import Parser
 from studymanager.cs_studymanager_texmaker import Report1, Report2

commit c0343549402eafa3fc3d66be34df9e73e7288eae

Fix bug in the studymanager GUI (apparently introduced in 01d8394 2019/01/27)

bin/studymanager/cs_studymanager_study.py
@@ -88,7 +88,7 @@ def create_base_xml_file(filepath):
     # using xml engine from Code_Saturne GUI
     smgr = XMLengine.Case(None, studymanager=True)
     smgr['xmlfile'] = filename
-    pm = PathesModel.PathesModel(smgr)
+    pm = PathesModel(smgr)
 
     # empty repo and dest
     pm.setRepositoryPath('')