code-saturne/code_saturne

commit f91e16129654a3363e9b7adfe35edfc7ac780716

Fixes in wall distance computation (introduced in rev. 10142):  * it should not be solved with a relaxation  * it should not be initialized to a big value
git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@10297 e44c5d27-f830-0410-8ce8-974f7a764be6

src/turb/vissst.f90
@@ -82,6 +82,10 @@ integer          iel, inc
 integer          iprev
 integer          f_id
 
+integer          ipass
+data             ipass /0/
+save             ipass
+
 double precision d1s3, d2s3
 double precision xk, xw, rom, xmu, xdist, xarg2, xf2
 
@@ -114,6 +118,8 @@ call field_get_val_s(f_id, w_dist)
 
 d1s3 = 1.d0/3.d0
 d2s3 = 2.d0/3.d0
+
+ipass = ipass + 1
 
 !===============================================================================
 ! 2. Compute the scalar s2kw rate SijSij and the trace of the velocity
@@ -172,10 +178,17 @@ do iel = 1, ncel
 
   ! Wall distance
   xdist = max(w_dist(iel), epzero)
+
+  ! FIXME should be a check on xw...
   if (xk > 0.d0) then
-    xarg2 = max (2.d0*sqrt(xk)/cmu/xw/xdist,                  &
-                 500.d0*xmu/rom/xw/xdist**2)
-    xf2 = tanh(xarg2**2)
+    ! Wall distance has no value at the first pass, we consider it as infinite
+    if (ipass.eq.1) then
+      xf2 = 0.d0
+    else
+      xarg2 = max (2.d0*sqrt(xk)/cmu/xw/xdist,                  &
+                   500.d0*xmu/rom/xw/xdist**2)
+      xf2 = tanh(xarg2**2)
+    endif
     visct(iel) =   rom*ckwa1*xk                               &
                  / max(ckwa1*xw, sqrt(cpro_s2kw(iel))*xf2)
   else

src/base/addfld.f90
@@ -320,6 +320,9 @@ if ((iturb.eq.30.and.irijec.eq.1).or.              &
   call field_get_key_struct_var_cal_opt(iflid, vcopt)
   vcopt%iconv = 0
   vcopt%istat = 0
+  vcopt%nswrsm = 2
+  vcopt%idifft = 0
+  vcopt%relaxv = 1.d0 ! No relaxation, even for steady algorithm.
   call field_set_key_struct_var_cal_opt(iflid, vcopt)
 
 endif

src/base/distpr.f90
@@ -94,6 +94,7 @@ double precision, allocatable, dimension(:,:) :: grad
 double precision, allocatable, dimension(:) :: w1
 double precision, allocatable, dimension(:) :: imasfl, bmasfl
 double precision, pointer, dimension(:)   :: cvar_var
+double precision, pointer, dimension(:)   :: cvara_var
 double precision, pointer, dimension(:) :: coefap, coefbp
 double precision, pointer, dimension(:) :: cofafp, cofbfp
 type(var_cal_opt) :: vcopt
@@ -126,6 +127,9 @@ allocate(w1(ncelet))
 call field_get_id("wall_distance", f_id)
 
 call field_get_key_struct_var_cal_opt(f_id, vcopt)
+
+call field_get_val_s(f_id, cvar_var)
+call field_get_val_prev_s(f_id, cvara_var)
 
 !===============================================================================
 ! 2. Boundary conditions
@@ -179,6 +183,14 @@ enddo
 
 if (irangp.ge.0) call parcpt(ndircp)
 
+! If no wall initialization to a big value
+if (ndircp.eq.0) then
+  do iel = 1, ncel
+    cvar_var(iel) = grand
+  enddo
+  return
+endif
+
 !===============================================================================
 ! 3. Prepare system to solve
 !===============================================================================
@@ -206,8 +218,6 @@ call viscfa                                                       &
 !===============================================================================
 
 ! Distance to wall is initialized to 0 for reconstruction
-
-call field_get_val_s(f_id, cvar_var)
 
 iconvp = vcopt%iconv
 idiffp = vcopt%idiff
@@ -253,7 +263,7 @@ call codits &
    iwarnp ,                                                       &
    blencp , epsilp , epsrsp , epsrgp , climgp , extrap ,          &
    relaxp , thetap ,                                              &
-   cvar_var        , cvar_var        ,                            &
+   cvara_var       , cvara_var       ,                            &
    coefap , coefbp , cofafp , cofbfp ,                            &
    imasfl , bmasfl ,                                              &
    viscf  , viscb  , viscf  , viscb  , rvoid  ,                   &

src/base/tridim.f90
@@ -271,16 +271,6 @@ if ((nfpt1t.gt.0).and.(nbccou.le.0)) then
   isvhb = iscalt
 endif
 
-! If wall distance has to be updated, we initialize it to a big value,
-! so that it is OK in phyvar for the k-omega turbulence model
-if (ipass.eq.1 .and. ineedy.eq.1 .and. imajdy.eq.0) then
-  call field_get_id("wall_distance", f_id)
-  call field_get_val_s(f_id, w_dist)
-  do iel = 1, ncel
-    w_dist(iel) = grand
-  enddo
-endif
-
 call field_get_val_s(ivarfl(ipr), cvar_pr)
 call field_get_val_prev_s(ivarfl(ipr), cvara_pr)
 
@@ -1195,68 +1185,37 @@ do while (iterns.le.nterup)
   !     ON N'A PLUS BESOIN DE ISVHB OU ISVHT (POUR HBORD ET TBORD)
   !     A PARTIR D'ICI
 
+  ! Compute wall distance
+  ! TODO it has to be moved before phyvar, for that bc types have to be known
+  ! (itypfb)
 
-
-  !     CALCUL DE LA DISTANCE A LA PAROI
   !       (Nouvel algorithme. L'ancien est dans condli)
-  !     En ALE on ne fait ce calcul qu'a la premiere des
-  !       sous-iterations d'implicitation ITALIM, car le maillage
-  !       n'est pas modifie dans les sous-iterations suivantes
-
+  ! In ALE, this computation is done only for the first step.
   if (italim.eq.1) then
 
-    if(ineedy.eq.1.and.iwarny.ge.1) then
-      call dmtmps(tdist1)
-    endif
-
-
-    ! On ne fait le calcul que s'il y a des parois, 'w_dist'  est reserve
-    ! et initialise a GRAND avant. S'il n'y a pas de paroi, il restera = GRAND)
 
     ! Pour le moment, on suppose que l'on peut se contenter de faire
     !  cela au premier passage, sauf avec les maillages mobiles. Attention donc
     !  aux conditions aux limites variables (faces qui deviennent des parois ou
     !  parois qui deviennent autre chose)
 
-    ! Nombre de faces de paroi
-    if (ipass.eq.1) then
-      if (ineedy.eq.1) then
-        infpar = 0
-        do ifac = 1, nfabor
-          if (itypfb(ifac).eq.iparoi .or. itypfb(ifac).eq.iparug) then
-            infpar = infpar+1
-          endif
-        enddo
-        if (irangp.ge.0) then
-          call parcpt(infpar)
-        endif
-      endif
-    endif
-
-    !     On calcule la distance a la paroi
-    !          si elle doit etre mise a jour
-    !       et si on en a besoin,
-    !       et si on a choisi ce mode de calcul,
+    ! Wall distance is computed if:
+    !   - it has to be updated
+    !   - we need it
+    ! In cas there is no wall, distance is a big value.
     if (imajdy.eq.0 .and. ineedy.eq.1) then
 
-      !  S'il n'y a pas de paroi, on garde l'initialisation a GRAND
-      if (infpar.eq.0) then
-        imajdy = 1
-        ! If we have walls, we must compute
-      else
-        if (abs(icdpar).eq.1) then
-          call distpr(itypfb)
-        ! Deprecated algorithm
-        else if (abs(icdpar).eq.2) then
-          call distpr2(itypfb)
-        endif
-        !     La distance n'a plus a etre mise a jour sauf en ALE
-        if (iale.eq.0) imajdy = 1
+      if (abs(icdpar).eq.1) then
+        call distpr(itypfb)
+      ! Deprecated algorithm
+      else if (abs(icdpar).eq.2) then
+        call distpr2(itypfb)
       endif
+      ! Wall distance is not updated except if ALE is switched on
+      if (iale.eq.0) imajdy = 1
     endif
 
   endif
-
 
   !     CALCUL DE L'AMORTISSEMENT DE VAN DRIEST
   !     OU CALCUL DE Y+ POUR LE LAGRANGIEN
@@ -1265,6 +1224,18 @@ do while (iterns.le.nterup)
   ! Compute y+ if needed
 
   if (itytur.eq.4 .and. idries.eq.1) then
+    ! Compute the number of walls
+    if (ipass.eq.1) then
+      infpar = 0
+      do ifac = 1, nfabor
+        if (itypfb(ifac).eq.iparoi .or. itypfb(ifac).eq.iparug) then
+          infpar = infpar+1
+        endif
+      enddo
+      if (irangp.ge.0) then
+        call parcpt(infpar)
+      endif
+    endif
 
     if (infpar.gt.0) then
       call distyp(itypfb, yplpar)
@@ -1283,12 +1254,6 @@ do while (iterns.le.nterup)
       call vandri(visvdr, yplpar)
     endif
 
-  endif
-
-  if (ineedy.eq.1.and.iwarny.ge.1) then
-    call dmtmps(tdist2)
-    tditot = tdist2-tdist1
-    write(nfecra,4010)tditot
   endif
 
 !===============================================================================
@@ -1778,7 +1743,7 @@ endif
 !===============================================================================
 
 !--------
-! FORMATS
+! Formats
 !--------
 
 #if defined(_CS_LANG_FR)
@@ -1823,9 +1788,6 @@ endif
                                                               /,/,&
  ' RESOLUTION DES TRANSFERTS THERMIQUES RADIATIFS             ',/,&
 '  ==============================================             ',/)
- 4010 format(/,                                                   &
-' ** TEMPS POUR LA DISTANCE A LA PAROI : ',E14.5               ,/,&
-'    ---------------------------------                        ',/)
 
 #else
 
@@ -1870,14 +1832,10 @@ endif
  ' SOLVING THERMAL RADIATIVE TRANSFER                         ',/,&
 '  ==================================                         ',/)
 
- 4010 format(/,                                                   &
-' ** TIME FOR THE WALL DISTANCE: ',E14.5                       ,/,&
-'    ---------------------------                              ',/)
-
 #endif
 
 !----
-! FIN
+! End
 !----
 
 end subroutine

commit 0b95e9db8f42b8bda84c624f5e50011fa3c7b6a1

Fix bug introduced in commit f91e1612   * Fix restart issues with k-omega SST model

src/turb/vissst.f90
@@ -82,10 +82,6 @@ integer          iel, inc
 integer          iprev
 integer          f_id
 
-integer          ipass
-data             ipass /0/
-save             ipass
-
 double precision d1s3, d2s3
 double precision xk, xw, rom, xmu, xdist, xarg2, xf2
 
@@ -118,8 +114,6 @@ call field_get_val_s(f_id, w_dist)
 
 d1s3 = 1.d0/3.d0
 d2s3 = 2.d0/3.d0
-
-ipass = ipass + 1
 
 !===============================================================================
 ! 2. Compute the scalar s2kw rate SijSij and the trace of the velocity
@@ -181,8 +175,8 @@ do iel = 1, ncel
 
   ! FIXME should be a check on xw...
   if (xk > 0.d0) then
-    ! Wall distance has no value at the first pass, we consider it as infinite
-    if (ipass.eq.1) then
+    ! Wall distance has no value at the first time step, we consider it as infinite
+    if (ntcabs.eq.1) then
       xf2 = 0.d0
     else
       xarg2 = max (2.d0*sqrt(xk)/cmu/xw/xdist,                  &