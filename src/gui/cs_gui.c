code-saturne/code_saturne

commit 2ba9d68c3680e060e31627f8d5e074b4b845256f

Fix in CDO and ALE: GUI options for linear solvers was not taken into account.

src/gui/cs_gui.c
@@ -52,6 +52,7 @@
 
 #include "cs_base.h"
 #include "cs_boundary_zone.h"
+#include "cs_equation_param.h"
 #include "cs_field.h"
 #include "cs_field_pointer.h"
 #include "cs_file.h"
@@ -2002,34 +2003,39 @@ void CS_PROCF (uinum1, UINUM1) (double  *cdtvar)
 
   /* 1) variables from velocity_pressure and turbulence */
   /* 1-a) for pressure or hydraulic head */
-  cs_field_t *c_pres = NULL;
+  cs_field_t *f = NULL;
   if (cs_glob_physical_model_flag[CS_GROUNDWATER] > -1) {
-    c_pres = cs_field_by_name("hydraulic_head");
+    f = cs_field_by_name("hydraulic_head");
   }
   else {
-    c_pres = cs_field_by_name("pressure");
+    f = cs_field_by_name("pressure");
   }
-  cs_field_get_key_struct(c_pres, key_cal_opt_id, &var_cal_opt);
-  int j = cs_field_get_key_int(c_pres, var_key_id) -1;
+  cs_field_get_key_struct(f, key_cal_opt_id, &var_cal_opt);
 
-  cs_tree_node_t *tn_v = _find_node_variable(c_pres->name);
+  cs_tree_node_t *tn_v = _find_node_variable(f->name);
 
   cs_gui_node_get_child_real(tn_v, "solver_precision", &var_cal_opt.epsilo);
   cs_gui_node_get_child_int(tn_v, "rhs_reconstruction", &var_cal_opt.nswrsm);
   cs_gui_node_get_child_int(tn_v, "verbosity", &var_cal_opt.iwarni);
 
+  /* For CDO equation */
+  cs_equation_param_t *eqp = cs_equation_param_by_name(f->name);
+  if (eqp != NULL) {
+    eqp->sles_param.eps = var_cal_opt.epsilo;
+  }
+
   /* Set Field calculation options in the field structure */
-  cs_field_set_key_struct(c_pres, key_cal_opt_id, &var_cal_opt);
+  cs_field_set_key_struct(f, key_cal_opt_id, &var_cal_opt);
 
   /* 1-b) for the other variables */
   int n_fields = cs_field_n_fields();
   for (int f_id = 0; f_id < n_fields; f_id++) {
-    cs_field_t  *f = cs_field_by_id(f_id);
+    f = cs_field_by_id(f_id);
     if (   f->type & CS_FIELD_VARIABLE
         && !cs_gui_strcmp(f->name, "pressure")
         && !cs_gui_strcmp(f->name, "hydraulic_head")) {
 
-      j = cs_field_get_key_int(f, var_key_id) -1;
+      int j = cs_field_get_key_int(f, var_key_id) -1;
       cs_field_get_key_struct(f, key_cal_opt_id, &var_cal_opt);
 
       const char *ref_name = f->name;
@@ -2046,6 +2052,12 @@ void CS_PROCF (uinum1, UINUM1) (double  *cdtvar)
 
       cs_gui_node_get_child_real(tn_v, "blending_factor", &var_cal_opt.blencv);
       cs_gui_node_get_child_real(tn_v, "solver_precision", &var_cal_opt.epsilo);
+
+      /* For CDO equation */
+      eqp = cs_equation_param_by_name(f->name);
+      if (eqp != NULL) {
+        eqp->sles_param.eps = var_cal_opt.epsilo;
+      }
 
       // only for nscaus and model scalar
       cs_gui_node_get_child_real(tn_v, "time_step_factor", &cdtvar[j]);

src/base/iniusi.f90
@@ -168,6 +168,11 @@ endif
 iihmpu = iihmpr
 call usipph(iihmpu, iturb, itherm, iale, ivofmt, icavit)
 
+! Activate CDO for ALE
+if (iale.eq.2) then
+  call cs_ale_activate
+endif
+
 ! Other model parameters, including user-defined scalars
 !-------------------------------------------------------
 
@@ -362,11 +367,6 @@ endif
 call cs_user_internal_coupling
 
 call cs_internal_coupling_setup
-
-! Activate CDO for ALE
-if (iale.eq.2) then
-  call cs_ale_activate
-endif
 
 !----
 ! Formats

commit 2ad26ba30938584706a5e2ec5713c187c526b020

Fix bug introduced in commit 2ba9d68 (epsilo from the GUI for CDO).

src/gui/cs_gui.c
@@ -2054,11 +2054,10 @@ void CS_PROCF (uinum1, UINUM1) (double  *cdtvar)
       cs_gui_node_get_child_real(tn_v, "blending_factor", &var_cal_opt.blencv);
       cs_gui_node_get_child_real(tn_v, "solver_precision", &var_cal_opt.epsilo);
 
-      /* For CDO equation */
+      /* For CDO equation, if non-automatic value ie != -1 */
       eqp = cs_equation_param_by_name(f->name);
-      if (eqp != NULL) {
+      if (eqp != NULL && var_cal_opt.epsilo != -1)
         eqp->sles_param.eps = var_cal_opt.epsilo;
-      }
 
       // only for nscaus and model scalar
       cs_gui_node_get_child_real(tn_v, "time_step_factor", &cdtvar[j]);