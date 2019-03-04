code-saturne/code_saturne

commit a9898dfa13e345bd142f8f3bd1eb42eebdd97e1c

Add setVariableValue based on name in NotebookModel

bin/model/NotebookModel.py
@@ -167,7 +167,7 @@ class NotebookModel(Model):
                 else:
                     self.addVariable()
                     idx = len(self.getVarList())
-                    self.setVariableValue(idx-1, value)
+                    self.setVariableValue(value, idx=idx-1)
                     self.setVariableName(idx-1, var)
                     self.setVariableOt(idx-1, oturns)
 
@@ -205,17 +205,23 @@ class NotebookModel(Model):
         value = node['value']
         if not value:
             value = self.defaultNotebookValues()['val']
-            self.setVariableValue(idx, value)
+            self.setVariableValue(value, idx=idx)
 
         return value
 
 
     @Variables.undoGlobal
-    def setVariableValue(self, idx, val):
+    def setVariableValue(self, val, idx=None, var = None):
         """
         Return list of case
         """
-        node = self.node_note.xmlInitChildNode("var", id = idx)
+        if idx:
+            node = self.node_note.xmlInitChildNode("var", id = idx)
+        elif var:
+            node = self.node_note.xmlInitChildNode("var", name = name)
+        else:
+            raise Exception("No id or name were specified for setVariableValue")
+
         node['value'] = val
 
 

gui/Pages/NotebookView.py
@@ -431,7 +431,7 @@ class VariableStandardItemModel(QAbstractItemModel):
         elif index.column() == 1:
             value = str(from_qvariant(value, float))
             item.item.value = value
-            self.mdl.setVariableValue(item.item.index, item.item.value)
+            self.mdl.setVariableValue(item.item.value, idx=item.item.index)
 
         elif index.column() == 2:
             value = from_qvariant(value, to_text_string)

salome/cfd_study/src/CFDSTUDYOTURNS/CFDSTUDYOTURNS_StudyInterface.py
@@ -306,17 +306,10 @@ class cfd_openturns_study:
         # Update xml file
         from code_saturne.model.NotebookModel import NotebookModel
         nb = NotebookModel(self.case)
-        nb_ids = nb.getVarList()
-        nb_names = nb.getVarNameList()
         for key in self.vars_dico.keys():
-            try:
-                ix = nb_names.index(key)
-            except:
-                ix = -1
-
-            if ix != -1:
-                idx = nb_ids[ix]
-                nb.setVariableValue(idx=idx, val=str(self.vars_dico[key]))
+            if key in nb.getVarNameList():
+                nb.setVariableValue(val=str(self.vars_dico[key]),
+                                    var=key)
 
         self.case.xmlSaveDocument()
     # ---------------------------------------

commit e75f70a47f988e3da9ccf9093d9f60c57e003389

Fix bug in the GUI using Notebook introduced in commit a9898df (2019-02-26)

bin/model/NotebookModel.py
@@ -215,9 +215,9 @@ class NotebookModel(Model):
         """
         Return list of case
         """
-        if idx:
+        if idx != None:
             node = self.node_note.xmlInitChildNode("var", id = idx)
-        elif var:
+        elif var != None:
             node = self.node_note.xmlInitChildNode("var", name = var)
         else:
             raise Exception("No id or name were specified for setVariableValue")