c3c805d15990601af9065f7ba1275568ec4e6ccb
Yvan Fournier <yvan.fournier@noreply.edf.fr>
2019-03-19 19:24:24 +0100

    Add missing global headers.

---
+++ src/alge/cs_alge_headers.h
@@ -0,0 +1,61 @@
+#ifndef __CS_ALGE_HEADERS_H__
+#define __CS_ALGE_HEADERS_H__
+
+/*============================================================================
+ * Global Code_Saturne headers file for easier include
+ *============================================================================*/
+
+/*
+  This file is part of Code_Saturne, a general-purpose CFD tool.
+
+  Copyright (C) 1998-2019 EDF S.A.
+
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation; either version 2 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+  Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/*----------------------------------------------------------------------------
+ * Local headers
+ *----------------------------------------------------------------------------*/
+
+#include "cs_bad_cells_regularisation.h"
+#include "cs_balance_by_zone.h"
+#include "cs_balance.h"
+#include "cs_benchmark.h"
+#include "cs_blas.h"
+#include "cs_convection_diffusion.h"
+#include "cs_divergence.h"
+#include "cs_face_viscosity.h"
+#include "cs_gradient.h"
+#include "cs_gradient_perio.h"
+#include "cs_grid.h"
+#include "cs_matrix_assembler.h"
+#include "cs_matrix_building.h"
+#include "cs_matrix_default.h"
+#include "cs_matrix.h"
+#include "cs_matrix_priv.h"
+#include "cs_matrix_tuning.h"
+#include "cs_matrix_util.h"
+#include "cs_multigrid.h"
+#include "cs_sles_default.h"
+#include "cs_sles.h"
+#include "cs_sles_it.h"
+#include "cs_sles_pc.h"
+
+// Avoid extra warnings by not including this by default...
+// #include "cs_sles_petsc.h"
+
+/*----------------------------------------------------------------------------*/
+
+#endif /* __CS_ALGE_HEADERS_H__ */
