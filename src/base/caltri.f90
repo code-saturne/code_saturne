code-saturne/code_saturne

commit eec3093653d97de88c7bd0fdb19c538f581bb593

Major fix for open-boundary boundary conditions. Mainly impact the new atmospheric automatic treatment for open-boundaries.
A temporary BC code is creates for the pressure: icodcl = -1
to specify that the pressure is imposed in solved pressure directly (not
in total pressure) and icodcl = 1 is set automatically after.

This might impact other BCs: a first pass before the time stepping in
called so that BC types (itypfb) and reference point for pressure is
computed.

Cherry-picked from ca561696, 478ec368, 220cd690, 3eb58165 and cb503c5c

src/base/caltri.f90
@@ -100,6 +100,10 @@ integer          itrale , ntmsav
 integer          nent
 
 integer          stats_id, restart_stats_id, lagr_stats_id, post_stats_id
+integer          isvhb, iscal
+integer          nbccou
+integer          kcpsyr, icpsyr
+integer          italim, itrfin, itrfup, ineefl
 
 double precision titer1, titer2
 
@@ -112,11 +116,39 @@ double precision, pointer, dimension(:)   :: dt => null()
 double precision, pointer, dimension(:)   :: porosi => null()
 double precision, pointer, dimension(:,:) :: disale => null()
 
+integer, allocatable, dimension(:,:) :: icodcl
+integer, allocatable, dimension(:) :: isostd
+double precision, allocatable, dimension(:,:,:) :: rcodcl
+
 !===============================================================================
 ! Interfaces
 !===============================================================================
 
 interface
+
+  subroutine condli_ini &
+  ( nvar   , nscal  ,                                              &
+    itrale ,                                                       &
+    icodcl , isostd ,                                              &
+    dt     , rcodcl )
+
+    use mesh, only: nfac, nfabor
+
+    implicit none
+
+    integer          nvar, nscal
+    integer          itrale
+
+    integer, dimension(nfabor,nvar) :: icodcl
+    integer, dimension(nfabor+1) :: isostd
+
+    double precision, dimension(nfabor,nvar,3) :: rcodcl
+
+    double precision, pointer, dimension(:)   :: dt
+
+  end subroutine condli_ini
+
+  !=============================================================================
 
   subroutine tridim(itrale, nvar, nscal, dt)
 
@@ -518,6 +550,48 @@ if (iporos.ge.1) then
   call cs_f_mesh_quantities_fluid_vol_reductions
 
 endif
+
+! First pass for the BCs:
+! - initilalize itypfb, reference pressure point...
+!--------------------------------------------------
+! --- Indicateur de stockage d'un scalaire et de son coef
+!     d'echange associe.
+!     Pour le moment, on stocke uniquement dans le cas couplage SYRTHES.
+!     ISVHB donne le numero du scalaire (on suppose qu'il n'y en a qu'un).
+!     Dans le cas ou on a un couplage avec le module thermique 1D en paroi,
+!     on utilise le meme scalaire que celui qui sert a Syrthes (s'il y a
+!     couplage Syrthes), sinon on stocke le scalaire thermique.
+
+call field_get_key_id("syrthes_coupling", kcpsyr)
+
+call nbcsyr (nbccou)
+isvhb = 0
+if (nbccou .ge. 1) then
+  do iscal = 1, nscal
+    call field_get_key_int(ivarfl(isca(iscal)), kcpsyr, icpsyr)
+    if (icpsyr.eq.1) then
+      isvhb = iscal
+    endif
+  enddo
+endif
+
+if ((nfpt1t.gt.0).and.(nbccou.le.0)) then
+  isvhb = iscalt
+endif
+
+! Deprecated, only for compatibility reason
+nvarcl = nvar
+
+allocate(icodcl(nfabor,nvar))
+allocate(rcodcl(nfabor,nvar,3))
+allocate(isostd(nfabor+1))
+
+! First pass for initialization BC types
+call condli_ini(nvar, nscal, itrale, icodcl, isostd, dt, rcodcl)
+
+deallocate(icodcl)
+deallocate(rcodcl)
+deallocate(isostd)
 
 !==============================================================================
 ! On appelle cs_user_boundary_mass_source_terms lorqu'il y a sur un processeur

src/atmo/attycl.f90
@@ -33,14 +33,34 @@
 !> \param[in]   itypfb          boundary face types
 !> \param[in]   izfppp          boundary face zone number for atmospheric module
 !> \param[out]  icodcl          face boundary condition code
+!>                               - 1 Dirichlet
+!>                               - 2 Radiative outlet
+!>                               - 3 Neumann
+!>                               - 4 sliding and
+!>                                 \f$ \vect{u} \cdot \vect{n} = 0 \f$
+!>                               - 5 smooth wall and
+!>                                 \f$ \vect{u} \cdot \vect{n} = 0 \f$
+!>                               - 6 rough wall and
+!>                                 \f$ \vect{u} \cdot \vect{n} = 0 \f$
+!>                               - 9 free inlet/outlet
+!>                                 (input mass flux blocked to 0)
+!>                               - 13 Dirichlet for the advection operator and
+!>                                    Neumann for the diffusion operator
 !> \param[out]  rcodcl          Boundary conditions value
-!>- rcodcl(1) = valeur du dirichlet
-!>- rcodcl(2) = valeur du coef. d'echange ext. (infinie si pas d'echange)
-!>- rcodcl(3) = valeur de la densite de flux (negatif si gain) w/m2 ou
-!> hauteur de rugosite (m) si icodcl=6  \n
-!>    pour les vitesses (vistl+visct)*gradu \n
-!>    pour la pression             dt*gradp \n
-!>    pour les scalaires cp*(viscls+visct/turb_schmidt)*gradt
+!>                               - rcodcl(1) value of the dirichlet
+!>                               - rcodcl(2) value of the exterior exchange
+!>                                 coefficient (infinite if no exchange)
+!>                               - rcodcl(3) value flux density
+!>                                 (negative if gain) in w/m2 or roughness
+!>                                 in m if icodcl=6
+!>                                 -# for the velocity \f$ (\mu+\mu_T)
+!>                                    \gradv \vect{u} \cdot \vect{n}  \f$
+!>                                 -# for the pressure \f$ \Delta t
+!>                                    \grad P \cdot \vect{n}  \f$
+!>                                 -# for a scalar \f$ cp \left( K +
+!>                                     \dfrac{K_T}{\sigma_T} \right)
+!>                                     \grad T \cdot \vect{n} \f$
+!
 !-------------------------------------------------------------------------------
 subroutine attycl ( itypfb, izfppp, icodcl, rcodcl )
 
@@ -343,8 +363,9 @@ do ifac = 1, nfabor
 
       if (iautom(ifac).ge.1) then
 
-        ! Dirichlet on the pressure
-        icodcl(ifac,ipr) = 1
+        ! Dirichlet on the pressure: expressed in solved pressure directly
+        ! (not in total pressure), that is why -1 is used (transformed as 1 in typecl.f90).
+        icodcl(ifac,ipr) = -1
         rcodcl(ifac,ipr,1) = coefap(ifac)
 
         ! Dirichlet on turbulent variables

src/base/stdtcl.f90
@@ -25,8 +25,8 @@ subroutine stdtcl &
 
  ( nbzfmx , nozfmx ,                                              &
    iqimp  , icalke , qimp   , dh     , xintur ,                   &
-   itypfb , iznfbr , ilzfbr ,                                     &
-   rcodcl , qcalc  )
+   itypfb , iznfbr ,                                              &
+   rcodcl )
 
 !===============================================================================
 ! FONCTION :
@@ -47,7 +47,6 @@ subroutine stdtcl &
 ! itypfb           ! ia ! <-- ! boundary face types                            !
 ! iznfbr           ! te ! <-- ! numero de zone de la face de bord              !
 ! (nfabor)         !    !     !                                                !
-! ilzfbr(nbzfmx    ! te ! <-- ! tableau de travail                             !
 ! rcodcl           ! tr ! --> ! valeur des conditions aux limites              !
 !                  !    !     !  aux faces de bord                             !
 !                  !    !     ! rcodcl(1) = valeur du dirichlet                !
@@ -60,13 +59,8 @@ subroutine stdtcl &
 !                  !    !     ! pour la pression             dt*gradp          !
 !                  !    !     ! pour les scalaires                             !
 !                  !    !     !        cp*(viscls+visct/turb_schmidt)*gradt    !
-! qcalc(nozfmx)    ! tr ! --- ! tab de travail (debit par zone)                !
 !__________________!____!_____!________________________________________________!
 
-!     TYPE : E (ENTIER), R (REEL), A (ALPHANUMERIQUE), T (TABLEAU)
-!            L (LOGIQUE)   .. ET TYPES COMPOSES (EX : TR TABLEAU REEL)
-!     MODE : <-- donnee, --> resultat, <-> Donnee modifiee
-!            --- tableau de travail
 !===============================================================================
 
 !===============================================================================
@@ -96,11 +90,10 @@ integer          nbzfmx
 
 integer          iqimp(nozfmx), icalke(nozfmx)
 integer          itypfb(nfabor)
-integer          iznfbr(nfabor), ilzfbr(nbzfmx)
+integer          iznfbr(nfabor)
 
 double precision qimp(nozfmx), dh(nozfmx), xintur(nozfmx)
 double precision rcodcl(nfabor,nvar,3)
-double precision qcalc(nozfmx)
 
 ! Local variables
 
@@ -110,6 +103,8 @@ integer          icke, ii, iel, iok
 double precision qisqc, viscla, uref2, rhomoy, dhy, xiturb
 double precision, dimension(:), pointer :: brom
 double precision, dimension(:), pointer :: viscl
+integer, allocatable, dimension(:) :: ilzfbr
+double precision, allocatable, dimension(:) :: qcalc
 
 integer          ipass
 data             ipass /0/
@@ -120,6 +115,9 @@ save             ipass
 !===============================================================================
 ! 1.  INITIALISATIONS
 !===============================================================================
+
+allocate(ilzfbr(nbzfmx))
+allocate(qcalc(nozfmx))
 
 
 call field_get_val_s(ibrom, brom)
@@ -433,11 +431,13 @@ do ifac = 1, nfabor
 
 enddo
 
+! Free memory
+deallocate(ilzfbr)
+deallocate(qcalc)
 
 !----
 ! FORMATS
 !----
-
 
 !----
 ! FIN

commit 2bddb0202c9fea4aac904f5f0cfa4c488f995d6f

Fix boundary condition-based internal coupling.
Cherry-pick ed from commit 6defd13e on master.

This was broken by commit eec30936 (Feb 18 2019).

src/base/caltri.f90
@@ -567,6 +567,9 @@ allocate(rcodcl(nfabor,nvar,3))
 allocate(isostd(nfabor+1))
 
 ! First pass for initialization BC types
+! -- Couplage Code_Saturne/Code_Saturne
+
+call cscini(nvar)
 call condli_ini(nvar, nscal, itrale, icodcl, isostd, dt, rcodcl)
 
 deallocate(icodcl)
@@ -786,10 +789,6 @@ if (iale.ge.1) then
 endif
 
 ! -- Fin de zone Structures mobiles en ALE
-
-! -- Couplage Code_Saturne/Code_Saturne
-
-call cscini(nvar)
 
 ! Lagrangian initialization
 

src/base/condli.f90
@@ -5158,6 +5158,12 @@ endif
 
 iterns = 1
 
+! Locate internal BC-based coupling
+if (nbrcpl.gt.0) then
+  call cscloc
+  call cscfbr_init(nscal, icodcl, itypfb)
+endif
+
 call typecl &
  ( nvar   , nscal  , iterns ,                                     &
    itypfb , itrifb , icodcl , isostd ,                            &

src/base/csc2cl.f90
@@ -429,3 +429,142 @@ deallocate(grad)
 
 return
 end subroutine csc2cl
+
+!===============================================================================
+
+!> \brief Initialization of the "itypfb(*, *) = icscpl" condition.
+
+!------------------------------------------------------------------------------
+! Arguments
+!------------------------------------------------------------------------------
+!   mode          name          role
+!------------------------------------------------------------------------------
+!> \param[in]     nvcp
+!> \param[in]     nfbcpl
+!> \param[in]     nfbncp
+!> \param[out]    icodcl        face boundary condition code:
+!>                               - 1 Dirichlet
+!>                               - 2 Radiative outlet
+!>                               - 3 Neumann
+!>                               - 4 sliding and
+!>                                 \f$ \vect{u} \cdot \vect{n} = 0 \f$
+!>                               - 5 smooth wall and
+!>                                 \f$ \vect{u} \cdot \vect{n} = 0 \f$
+!>                               - 6 rough wall and
+!>                                 \f$ \vect{u} \cdot \vect{n} = 0 \f$
+!>                               - 9 free inlet/outlet
+!>                                 (input mass flux blocked to 0)
+!>                               - 13 Dirichlet for the advection operator and
+!>                                    Neumann for the diffusion operator
+!> \param[in]     lfbcpl
+!> \param[in]     lfbncp
+!> \param[out]    itypfb        boundary face types
+!______________________________________________________________________________
+
+subroutine csc2cl_init &
+ (nvcp, nfbcpl, nfbncp, icodcl, itypfb, lfbcpl, lfbncp)
+
+!===============================================================================
+! Module files
+!===============================================================================
+
+use paramx
+use numvar
+use optcal
+use cstphy
+use cstnum
+use dimens, only: nvar
+use entsor
+use parall
+use period
+use cplsat
+use mesh
+use field
+use field_operator
+
+!===============================================================================
+
+implicit none
+
+! Arguments
+
+integer          nvcp
+integer          nfbcpl , nfbncp
+
+integer          icodcl(nfabor,nvar)
+integer          lfbcpl(nfbcpl)  , lfbncp(nfbncp)
+integer          itypfb(nfabor)
+
+! Local variables
+
+integer          ifac, icscp
+integer          ivar
+integer          ipt
+
+!===============================================================================
+
+!===============================================================================
+! 1.  Translation of the coupling to boundary conditions
+!===============================================================================
+
+if (ifaccp.eq.0) then
+  icscp = icscpl
+else
+  icscp = icscpd
+endif
+
+! Reminder: variables are received in the order of VARPOS;
+! looping on variables is thus sufficient.
+
+do ivar = 1, nvcp
+
+  if (ifaccp.eq.1) then
+
+    do ipt = 1, nfbcpl
+
+      ifac = lfbcpl(ipt)
+
+      itypfb(ifac) = icscp
+      icodcl(ifac,ivar  ) = 1
+
+    enddo
+
+  else
+
+    do ipt = 1, nfbcpl
+
+      ifac = lfbcpl(ipt)
+
+      itypfb(ifac) = icscp
+
+      if (ivar.ne.ipr) then
+        icodcl(ifac,ivar  ) = 1
+      else
+        icodcl(ifac,ivar  ) = 3
+      endif
+
+    enddo
+
+  endif
+
+  ! Non-located boundary faces
+
+  do ipt = 1, nfbncp
+
+    ifac = lfbncp(ipt)
+
+    itypfb(ifac) = icscp
+
+    icodcl(ifac,ivar  ) = 3
+
+  enddo
+
+enddo
+
+!----
+! End
+!----
+
+return
+end subroutine csc2cl_init
+

src/base/cscfbr.f90
@@ -305,3 +305,143 @@ enddo
 
 return
 end subroutine
+
+!===============================================================================
+
+!> \brief Initialization for coupling two Code_Saturne intances
+!>        with boundary faces.
+!>
+!------------------------------------------------------------------------------
+
+!------------------------------------------------------------------------------
+! Arguments
+!------------------------------------------------------------------------------
+!   mode          name          role
+!------------------------------------------------------------------------------
+!> \param[in]     nscal         total number of scalars
+!> \param[in]     icodcl        face boundary condition code:
+!>                               - 1 Dirichlet
+!>                               - 2 Radiative outlet
+!>                               - 3 Neumann
+!>                               - 4 sliding and
+!>                                 \f$ \vect{u} \cdot \vect{n} = 0 \f$
+!>                               - 5 smooth wall and
+!>                                 \f$ \vect{u} \cdot \vect{n} = 0 \f$
+!>                               - 6 rough wall and
+!>                                 \f$ \vect{u} \cdot \vect{n} = 0 \f$
+!>                               - 9 free inlet/outlet
+!>                                 (input mass flux blocked to 0)
+!>                               - 13 Dirichlet for the advection operator and
+!>                                    Neumann for the diffusion operator
+!> \param[in]     itypfb        boundary face types
+!______________________________________________________________________________
+
+subroutine cscfbr_init(nscal, icodcl, itypfb)
+
+!===============================================================================
+! Module files
+!===============================================================================
+
+use paramx
+use numvar
+use entsor
+use optcal
+use cstphy
+use cstnum
+use dimens, only: nvar
+use parall
+use period
+use cplsat
+use mesh
+
+!===============================================================================
+
+implicit none
+
+! Arguments
+
+integer          nscal
+
+integer          icodcl(nfabor,nvar)
+integer          itypfb(nfabor)
+
+double precision dt(ncelet)
+double precision rcodcl(nfabor,nvar,3)
+
+! Local variables
+
+integer          numcpl , ivarcp
+integer          ncesup , nfbsup
+integer          ncecpl , nfbcpl , ncencp , nfbncp
+integer          ncedis , nfbdis
+integer          nfbcpg , nfbdig
+integer          ityloc , ityvar
+integer          stride
+
+integer, allocatable, dimension(:) :: lcecpl , lfbcpl , lcencp , lfbncp
+integer, allocatable, dimension(:) :: locpts
+
+double precision, allocatable, dimension(:,:) :: coopts , djppts , dofpts
+double precision, allocatable, dimension(:,:) :: dofcpl
+double precision, allocatable, dimension(:) :: pndpts
+double precision, allocatable, dimension(:) :: pndcpl
+double precision, allocatable, dimension(:,:) :: rvdis , rvfbr
+
+!===============================================================================
+
+
+do numcpl = 1, nbrcpl
+
+!===============================================================================
+! 1.  DEFINITION DE CHAQUE COUPLAGE
+!===============================================================================
+
+  call nbecpl                                                     &
+ ( numcpl ,                                                       &
+   ncesup , nfbsup ,                                              &
+   ncecpl , nfbcpl , ncencp , nfbncp )
+
+  ! Allocate temporary arrays for coupling information
+  allocate(lcecpl(ncecpl), lcencp(ncencp))
+  allocate(lfbcpl(nfbcpl), lfbncp(nfbncp))
+
+!       Liste des cellules et faces de bord localisées
+  call lelcpl                                                     &
+ ( numcpl ,                                                       &
+   ncecpl , nfbcpl ,                                              &
+   lcecpl , lfbcpl )
+
+!       Liste des cellules et faces de bord non localisées
+  call lencpl                                                     &
+ ( numcpl ,                                                       &
+   ncencp , nfbncp ,                                              &
+   lcencp , lfbncp )
+
+  ! Free memory
+  deallocate(lcecpl, lcencp)
+
+!===============================================================================
+! 2.  TRADUCTION DU COUPLAGE EN TERME DE CONDITIONS AUX LIMITES
+!===============================================================================
+
+  if (nfbcpg.gt.0) then
+
+    call csc2cl_init &
+  ( nvarcp(numcpl), nfbcpl , nfbncp ,                             &
+    icodcl , itypfb ,                                             &
+    lfbcpl , lfbncp )
+
+  endif
+
+  ! Free memory
+  deallocate(lfbcpl, lfbncp)
+
+enddo
+!     Fin de la boucle sur les couplages
+
+!----
+! FIN
+!----
+
+return
+end subroutine cscfbr_init