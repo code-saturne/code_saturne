code-saturne/code_saturne

commit 2dd2b11c258642038bf9d69877786528d8884018

Add the non linear quadratic k-epsilon model from Baglietto et al.   * Activated with iturb = 23   * Low Re model, compatible with wall functions

src/turb/visqke.f90
@@ -1,0 +1,174 @@
+!-------------------------------------------------------------------------------
+
+! This file is part of Code_Saturne, a general-purpose CFD tool.
+!
+! Copyright (C) 1998-2019 EDF S.A.
+!
+! This program is free software; you can redistribute it and/or modify it under
+! the terms of the GNU General Public License as published by the Free Software
+! Foundation; either version 2 of the License, or (at your option) any later
+! version.
+!
+! This program is distributed in the hope that it will be useful, but WITHOUT
+! ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+! FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+! details.
+!
+! You should have received a copy of the GNU General Public License along with
+! this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+! Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+!-------------------------------------------------------------------------------
+
+
+!===============================================================================
+! Function:
+! --------
+!> \file visqke.f90
+!> \brief Calculation of turbulent viscosity for
+!>         the non-linear quadratic K-epsilon from
+!>         Baglietto et al. (2005)  
+!>
+!> Edge faces types are vailable at the previous time step
+!>  (except at the first time step, when the itypfb anf itrifb tables
+!>  have not been filled)
+!-------------------------------------------------------------------------------
+
+!-------------------------------------------------------------------------------
+! Arguments
+!______________________________________________________________________________.
+!  mode           name          role
+!______________________________________________________________________________!
+!______________________________________________________________________________!
+
+subroutine visqke
+
+!===============================================================================
+! Module files
+!===============================================================================
+
+use paramx
+use numvar
+use optcal
+use cstphy
+use entsor
+use mesh
+use field
+use field_operator
+
+!===============================================================================
+
+implicit none
+
+! Arguments
+
+! Local variables
+
+integer          iel, inc, f_id
+integer          iprev
+
+double precision s11, s22, s33
+double precision dudy, dudz, dvdx, dvdz, dwdx, dwdy
+double precision xk, xe, xrom, xmu, xmut, xcmu, xfmu, xss, xrey, xdist
+double precision xttke
+
+double precision, allocatable, dimension(:) :: s2
+double precision, dimension(:,:,:), allocatable :: gradv
+double precision, dimension(:), pointer :: crom
+double precision, dimension(:), pointer :: viscl, visct
+double precision, dimension(:), pointer :: w_dist
+double precision, dimension(:), pointer :: cvar_k, cvar_ep, cvar_phi
+
+!===============================================================================
+
+!===============================================================================
+! 1.  Initialization
+!===============================================================================
+
+! --- Memory
+allocate(s2(ncelet))
+
+call field_get_val_s(iviscl, viscl)
+call field_get_val_s(ivisct, visct)
+call field_get_val_s(icrom, crom)
+
+call field_get_val_s(ivarfl(ik), cvar_k)
+call field_get_val_s(ivarfl(iep), cvar_ep)
+
+call field_get_id("wall_distance", f_id)
+call field_get_val_s(f_id, w_dist)
+
+!===============================================================================
+! 2.  Calculation of velocity gradient and of
+!       S2 = S11**2+S22**2+S33**2+2*(S12**2+S13**2+S23**2)
+!===============================================================================
+
+! Allocate temporary arrays for gradients calculation
+allocate(gradv(3,3,ncelet))
+
+inc = 1
+iprev = 0
+
+call field_gradient_vector(ivarfl(iu), iprev, imrgra, inc,    &
+                           gradv)
+
+do iel = 1, ncel
+
+  s11  = gradv(1,1,iel)
+  s22  = gradv(2,2,iel)
+  s33  = gradv(3,3,iel)
+  dudy = gradv(2,1,iel)
+  dudz = gradv(3,1,iel)
+  dvdx = gradv(1,2,iel)
+  dvdz = gradv(3,2,iel)
+  dwdx = gradv(1,3,iel)
+  dwdy = gradv(2,3,iel)
+
+  s2(iel) = s11**2 + s22**2 + s33**2  &
+          + 0.5d0*(dudy+dvdx)**2      &
+          + 0.5d0*(dudz+dwdx)**2      &
+          + 0.5d0*(dvdz+dwdy)**2
+
+  s2(iel) = max(s2(iel),1.d-10)
+
+enddo
+
+! Free memory
+deallocate(gradv)
+
+!===============================================================================
+! 3.  Calculation of viscosity
+!===============================================================================
+
+
+do iel = 1, ncel
+  xk   = cvar_k(iel)
+  xe   = cvar_ep(iel)
+  xrom = crom(iel)
+  xmu  = viscl(iel)
+  xdist= w_dist(iel)
+
+  xmut = xrom*xk**2/xe
+  xrey = xdist*sqrt(xk)*xrom/xmu
+  xss = xttke*sqrt(.5d0*s2(iel))
+
+  xfmu = 1.d0-exp(-2.9d-2*xrey**.5d0-1.1d-4*xrey**2.d0)
+  xcmu = 2.d0/3.d0/(3.9d0 + xss)
+
+  visct(iel) = xcmu*xfmu*xmut
+enddo
+
+! Free memory
+deallocate(s2)
+
+!----
+! Format
+!----
+
+
+!----
+! End
+!----
+
+return
+end subroutine

src/base/addfld.f90
@@ -346,7 +346,8 @@ endif
 
 ! Wall distance for some turbulence models
 
-if ((iturb.eq.30.and.irijec.eq.1).or.              &
+if ( iturb.eq.23.or.                                &
+     (iturb.eq.30.and.irijec.eq.1).or.              &
      (itytur.eq.4.and.idries.eq.1).or.              &
      iturb.eq.60.or.iturb.eq.70      ) then
   ineedy = 1

src/base/clptur.f90
@@ -922,7 +922,9 @@ do ifac = 1, nfabor
 
     if (itytur.eq.2) then
 
+      ! ==================================
       ! Launder Sharma boundary conditions
+      ! ==================================
       if(iturb.eq.22) then
 
         ! Dirichlet Boundary Condition on k
@@ -980,7 +982,66 @@ do ifac = 1, nfabor
              coefb_ep(ifac), coefbf_ep(ifac),             &
              pimp         , hint          , rinfin )
 
+
+      ! ===================================
+      ! Quadratic Baglietto k-epsilon model
+      ! ===================================
+      else if(iturb.eq.23) then
+        
+        ! Dirichlet Boundary Condition on k
+        !----------------------------------
+        if (iwallf.eq.0) then
+          ! No wall functions forces by user
+          pimp = 0.d0
+        else
+          ! Use of wall functions
+          if (iuntur.eq.1) then
+            pimp = uk**2/sqrcmu
+          else
+            pimp = 0.d0
+          endif
+        endif
+
+        hint = (visclc+visctc/sigmak)/distbf
+
+        call set_dirichlet_scalar &
+             !====================
+           ( coefa_k(ifac), coefaf_k(ifac),             &
+             coefb_k(ifac), coefbf_k(ifac),             &
+             pimp         , hint          , rinfin )
+
+        ! Dirichlet Boundary Condition on epsilon
+        !---------------------------------------
+        if(iwallf.ne.0) then
+
+          pimp_lam = 2.0d0*visclc/romc*cvar_k(iel)/distbf**2
+
+          if (yplus > epzero) then
+            pimp_turb = 5.d0*uk**4*romc/        &
+                        (xkappa*visclc*(yplus+dplus))
+
+            ! Blending between wall and homogeneous layer
+            fep       = exp(-(yplus/4.d0)**1.5d0)
+            dep       = 1.d0- exp(-(yplus/9.d0)**2.1d0)
+            pimp      = fep*pimp_lam + (1.d0-fep)*dep*pimp_turb
+          else
+            pimp = pimp_lam
+          end if
+
+        else
+          
+          pimp = 2.0d0*visclc/romc*cvar_k(iel)/distbf**2
+        end if
+
+        call set_dirichlet_scalar &
+             !====================
+           ( coefa_ep(ifac), coefaf_ep(ifac),             &
+             coefb_ep(ifac), coefbf_ep(ifac),             &
+             pimp          , hint           , rinfin )
+
+      ! ==============================================
       ! k-epsilon and k-epsilon LP boundary conditions
+      ! ==============================================
       else
 
         ! Dirichlet Boundary Condition on k

src/base/cs_parameters_check.c
@@ -1279,14 +1279,14 @@ cs_parameters_check(void)
   /* Turbulence */
 
   /* Model */
-  const int iturb_vals[15] = {0,          /* laminar */
-                              10,         /* mixing length */
-                              20, 21, 22, /* k-epsilon */
-                              30, 31, 32, /* RSM */
-                              40, 41, 42, /* LES */
-                              50, 51,     /* v2f */
-                              60,         /* k-omega */
-                              70};        /* Spallart-Allmaras */
+  const int iturb_vals[15] = {0,              /* laminar */
+                              10,             /* mixing length */
+                              20, 21, 22, 23, /* k-epsilon */
+                              30, 31, 32,     /* RSM */
+                              40, 41, 42,     /* LES */
+                              50, 51,         /* v2f */
+                              60,             /* k-omega */
+                              70};            /* Spallart-Allmaras */
 
   cs_parameters_is_in_list_int(CS_ABORT_DELAYED,
                                _("while reading input data"),
@@ -1421,15 +1421,17 @@ cs_parameters_check(void)
                                   cs_glob_turb_rans_model->ikecou,
                                   0, 2);
 
-    /* En k-eps a prod lin et en v2f on force IKECOU a 0 */
+    /* En k-eps a prod lin/LS/Quad et en v2f on force IKECOU a 0 */
     if (   cs_glob_turb_model->iturb == 21
         || cs_glob_turb_model->iturb == 22
+        || cs_glob_turb_model->iturb == 23
         || cs_glob_turb_model->itytur == 5) {
       cs_parameters_is_equal_int(CS_ABORT_DELAYED,
                                  _("while reading input data,\n"
                                    "with k-epsilon LP (iturb=21),"
-                                   "k-epsilon LS (iturb=22) or v2f model "
-                                   "(iturb=50/51)"),
+                                   "k-epsilon LS (iturb=22), "
+                                   "k-epislon quadratic (iturb=23)"
+                                   "or v2f model (iturb=50/51)"),
                                  "cs_glob_turb_rans_model->ikecou",
                                  cs_glob_turb_rans_model->ikecou,
                                  0);

src/base/cstphy.f90
@@ -325,6 +325,31 @@ module cstphy
   !> Useful if and only if \ref optcal::iturb "iturb"= 20, 21 or 30
   !> (\f$k-\varepsilon\f$ or \f$R_{ij}-\varepsilon\f$ LRR)
   double precision, save :: ce2
+  
+  !> constant \f$C_{NL1}\f$ for the \f$k-\varepsilon\f$ 
+  !> model from Baglietto et al. (quadratric)
+  !> Useful if and only if \ref optcal::iturb "iturb"= 23
+  double precision, save :: cnl1
+
+  !> constant \f$C_{NL2}\f$ for the \f$k-\varepsilon\f$ 
+  !> model from Baglietto et al. (quadratric)
+  !> Useful if and only if \ref optcal::iturb "iturb"= 23
+  double precision, save :: cnl2
+
+  !> constant \f$C_{NL3}\f$ for the \f$k-\varepsilon\f$ 
+  !> model from Baglietto et al. (quadratric)
+  !> Useful if and only if \ref optcal::iturb "iturb"= 23
+  double precision, save :: cnl3
+
+  !> constant \f$C_{NL4}\f$ for the \f$k-\varepsilon\f$ 
+  !> model from Baglietto et al. (quadratric)
+  !> Useful if and only if \ref optcal::iturb "iturb"= 23
+  double precision, save :: cnl4
+
+  !> constant \f$C_{NL5}\f$ for the \f$k-\varepsilon\f$ 
+  !> model from Baglietto et al. (quadratric)
+  !> Useful if and only if \ref optcal::iturb "iturb"= 23
+  double precision, save :: cnl5
 
   !> Coefficient of interfacial coefficient in k-eps,
   !> used in Lagrange treatment

src/base/iniini.f90
@@ -776,6 +776,13 @@ ce2     = 1.92d0
 ce4     = 1.20d0
 sigmak  = 1.00d0
 
+!   pour le k-epsilon quadratic (Baglietto)
+cnl1  = 0.8d0
+cnl2  = 11.d0
+cnl3  = 4.5d0
+cnl4  = 1.d3
+cnl5  = 1.d0
+
 !   pour le Rij-epsilon standard (et SSG pour CRIJ3)
 crij1  = 1.80d0
 crij2  = 0.60d0

src/base/modini.f90
@@ -749,8 +749,8 @@ if (icdpar.eq.-1 .and. ikw.eq.1 .and. isuite.eq.1)                &
 if (ikecou.eq.-999) then
   if (idtvar.lt.0) then
     ikecou = 0
-  else if (iturb.eq.21 .or. iturb.eq.22 .or. itytur.eq.5           &
-       .or. iturb.eq.60 ) then
+  else if (iturb.eq.21 .or. iturb.eq.22 .or. iturb.eq.23 .or. &
+           itytur.eq.5 .or. iturb.eq.60 ) then
     ikecou = 0
   else
     ikecou = 1

src/base/phyvar.f90
@@ -290,6 +290,10 @@ elseif (itytur.eq.2) then
   call field_get_val_s(ivarfl(iep), cvar_ep)
 
   if (iturb.eq.22) then
+    
+    ! 4.3.1 Launder-Sharma
+    ! --------------------
+
     do iel = 1, ncel
       xk   = cvar_k(iel)
       xe   = cvar_ep(iel)
@@ -299,12 +303,25 @@ elseif (itytur.eq.2) then
       xfmu = exp(-3.4d0/(1.d0+xmut/xmu/50.d0)**2.d0)
       visct(iel) = cmu*xfmu*xmut
     enddo
+
+  else if (iturb.eq.23) then
+
+    ! 4.3.2 Non-linear quadratic Baglietto
+    ! ------------------------------------
+    
+    call visqke
+  
   else
+
+    ! 4.3.3 Standard and Linear Production
+    ! ------------------------------------
+
     do iel = 1, ncel
       xk = cvar_k(iel)
       xe = cvar_ep(iel)
       visct(iel) = crom(iel)*cmu*xk**2/xe
     enddo
+
   endif
 
 elseif (itytur.eq.3) then

commit 2eac5f1c024b6d498c5bed1129f5ca34eee98af5

Fix bug introduced in commit 2dd2b11c2 (non initialized value)

src/turb/visqke.f90
@@ -150,6 +150,7 @@ do iel = 1, ncel
 
   xmut = xrom*xk**2/xe
   xrey = xdist*sqrt(xk)*xrom/xmu
+  xttke = xk/xe
   xss = xttke*sqrt(.5d0*s2(iel))
 
   xfmu = 1.d0-exp(-2.9d-2*xrey**.5d0-1.1d-4*xrey**2.d0)