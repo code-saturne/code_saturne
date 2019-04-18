code-saturne/code_saturne

commit cdcc2cd28dabfbba9fddbbdb6e8c3776d9f1a6b3

Allow mapping from different meshes for restarts.

src/base/caltri.f90
@@ -160,6 +160,24 @@ interface
     implicit none
   end subroutine cs_gui_profile_output
 
+  !=============================================================================
+
+  subroutine cs_restart_map_build()  &
+    bind(C, name='cs_restart_map_build')
+    use, intrinsic :: iso_c_binding
+    implicit none
+  end subroutine cs_restart_map_build
+
+  !=============================================================================
+
+  subroutine cs_restart_map_free()  &
+    bind(C, name='cs_restart_map_free')
+    use, intrinsic :: iso_c_binding
+    implicit none
+  end subroutine cs_restart_map_free
+
+  !=============================================================================
+
 end interface
 
 !===============================================================================
@@ -547,6 +565,8 @@ if (isuite.eq.1) then
 
   call timer_stats_start(restart_stats_id)
 
+  call cs_restart_map_build
+
   call lecamo
 
   ! Using ALE, geometric parameters must be recalculated
@@ -567,6 +587,18 @@ if (isuite.eq.1) then
 
   endif
 
+  ! Radiative module restart */
+  if (iirayo.gt.0) then
+    call cs_rad_transfer_read
+  endif
+
+  ! Lagrangian module restart (particles) */
+  if (iilagr.gt.0) then
+    call laglec()
+  endif
+
+  call cs_restart_map_free
+
   call timer_stats_stop(restart_stats_id)
 
 endif
@@ -577,22 +609,6 @@ endif
 !===============================================================================
 
 call inivar(nvar, nscal)
-
-!===============================================================================
-! Radiative transfer: possible restart
-!===============================================================================
-
-if (iirayo.gt.0) then
-   call cs_rad_transfer_read
-endif
-
-!===============================================================================
-! Initialize particles for Lagrangian module
-!===============================================================================
-
-if (iilagr.gt.0) then
-  call laglec()
-endif
 
 !===============================================================================
 ! Initializations for the 1D thermal wall module
@@ -622,17 +638,12 @@ if (nfpt1t.gt.0) then
 
   else
 
-!     Creation du maillage, initialisation de la temperature.
+    ! Create mesh, initialize temperature.
     call cs_1d_wall_thermal_mesh_create
 
   endif
 
 endif
-
-!     Les infos sur l'epaisseur de la paroi, le nombre de points de
-!     discretisation et la raison geometrique ont ete transmises a
-!     la structure C. Elles sont maintenant inutiles dans le Fortran.
-!     -> on libere la memoire.
 
 !===============================================================================
 ! Initialization for the Synthetic turbulence Inlets
@@ -646,7 +657,8 @@ if (isuisy.eq.1) then
   call lecsyn('les_inflow'//c_null_char)
 endif
 
-! ATMO MODULE : INITIALIZATION FOR THE SOIL MODEL (ippmo(iatmos) = 2)
+!===============================================================================
+! Initialization for the atmospheric soil model
 !===============================================================================
 
 if (ippmod(iatmos).ge.2.and.iatsoil.eq.1) then
@@ -701,10 +713,7 @@ if (ivrtex.eq.1) then
 
   iappel = 1
 
-  call usvort &
- ( nvar   , nscal  , iappel ,                                     &
-   dt     )
-
+  call usvort(nvar, nscal, iappel, dt)
   call vorver (nfabor, iappel)
 
   call init_vortex

src/base/Makefile.am
@@ -142,6 +142,7 @@ cs_renumber.h \
 cs_resource.h \
 cs_restart.h \
 cs_restart_default.h \
+cs_restart_map.h \
 cs_rotation.h \
 cs_sat_coupling.h \
 cs_search.h \
@@ -297,6 +298,7 @@ cs_range_set.c \
 cs_resource.c \
 cs_restart.c \
 cs_restart_default.c \
+cs_restart_map.c \
 cs_rotation.c \
 cs_sat_coupling.c \
 cs_search.c \

src/base/cs_base_headers.h
@@ -51,6 +51,8 @@
 #include "cs_preprocess.h"
 #include "cs_probe.h"
 #include "cs_prototypes.h"
+#include "cs_restart.h"
+#include "cs_restart_map.h"
 #include "cs_rotation.h"
 #include "cs_selector.h"
 #include "cs_stokes_model.h"

commit fe8d391f1d044b8ae420f4baebc600e53005fdfe

Fix crash in synthetic turbulence inlets on restart.
Fix bug introduced by commit cdcc2cd2 (Dec 10 2018) when allowing
restarts from different meshes (routine lecsyn has to be called
before restart map is free).

src/base/caltri.f90
@@ -589,6 +589,13 @@ if (nftcdt.gt.0) then
 endif
 
 !===============================================================================
+! Initialization for the Synthetic turbulence Inlets
+!===============================================================================
+
+nent = 0
+call defsyn(nent)
+
+!===============================================================================
 ! Possible restart
 !===============================================================================
 
@@ -631,6 +638,10 @@ if (isuite.eq.1) then
   ! Lagrangian module restart (particles) */
   if (iilagr.gt.0) then
     call laglec()
+  endif
+
+  if (isuisy.eq.1) then
+    call lecsyn('les_inflow'//c_null_char)
   endif
 
   ! TODO
@@ -710,18 +721,6 @@ call condli_ini(nvar, nscal, itrale, icodcl, isostd, dt, rcodcl)
 deallocate(icodcl)
 deallocate(rcodcl)
 deallocate(isostd)
-
-!===============================================================================
-! Initialization for the Synthetic turbulence Inlets
-!===============================================================================
-
-nent = 0
-
-call defsyn(nent)
-
-if (isuisy.eq.1) then
-  call lecsyn('les_inflow'//c_null_char)
-endif
 
 !===============================================================================
 ! Arrays for time block, to discard afterwards