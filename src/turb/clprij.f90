code-saturne/code_saturne

commit b292f80bdf6ba694a2c24a303bfa241ba885683c

Make clipping of Reynolds stress in the coupled version more robust.
When there is a negative eigen value, Rij is clipped with a return to
isotropy term (rather than increasing the TKE).

src/turb/clprij.f90
@@ -444,20 +444,26 @@ do iel = 1, ncel
     eigen_min = minval(eigen_vals(1:3))
     eigen_max = maxval(eigen_vals(1:3))
 
+    ! If negative eigen value, return to isotropie
     if ( (eigen_min .le. (eigen_tol*eigen_max)) .or. &
       (eigen_min .le. epzero) ) then
 
       is_clipped = 1
 
-      eigen_offset = (eigen_tol - eigen_min) * trrij
-
+      eigen_min = min(eigen_min, - eigen_tol)
       eigen_offset = max(eigen_offset, epzero*rijref)
+      eigen_offset = min(- eigen_min / (1.d0/3.d0 - eigen_min) + 0.1d0, 1.d0)
 
-      do isou = 1, 3
-        cvar_rij(isou,iel) = cvar_rij(isou,iel) + eigen_offset
+
+      do isou = 1, 6
+        cvar_rij(isou,iel) = (1.d0 - eigen_offset) * cvar_rij(isou,iel)
+
+        if (isou.le.3) then
+          cvar_rij(isou,iel) = cvar_rij(isou,iel) + trrij * eigen_offset / 3.d0
+        endif
 
         if (clip_r_id.ge.0) &
-          cpro_rij_clipped(isou, iel) = eigen_offset
+          cpro_rij_clipped(isou, iel) = eigen_offset * cvar_rij(isou,iel)
 
         iclrij(isou) = iclrij(isou) + 1
       end do

commit 19c437ebe5b0a079d5ef25bed0d515396307c887

Fix bug in coupled Reynolds stress clipping (introduced in commit b292f80, 2019-04-19)

src/turb/clprij.f90
@@ -451,14 +451,13 @@ do iel = 1, ncel
       is_clipped = 1
 
       eigen_min = min(eigen_min, - eigen_tol)
-      eigen_offset = max(eigen_offset, epzero*rijref)
       eigen_offset = min(- eigen_min / (1.d0/3.d0 - eigen_min) + 0.1d0, 1.d0)
 
       do isou = 1, 6
         cvar_rij(isou,iel) = (1.d0 - eigen_offset) * cvar_rij(isou,iel)
 
         if (isou.le.3) then
-          cvar_rij(isou,iel) = cvar_rij(isou,iel) + trrij * eigen_offset / 3.d0
+          cvar_rij(isou,iel) = cvar_rij(isou,iel) + trrij * (eigen_offset + eigen_tol) / 3.d0
         endif
 
         if (clip_r_id.ge.0) &