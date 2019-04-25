code-saturne/code_saturne

commit c0964847db2fb0f31f2c0f9c65c7333879daae3f

MAJOR: Deploy MEG functions for initialization and source terms

bin/model/SourceTermsModel.py
@@ -46,6 +46,9 @@ from code_saturne.model.XMLmodel import XMLmodel, ModelTest
 from code_saturne.model.XMLvariables import Model, Variables
 from code_saturne.model.DefineUserScalarsModel import DefineUserScalarsModel
 from code_saturne.model.LocalizationModel import LocalizationModel
+from code_saturne.model.ThermalScalarModel import ThermalScalarModel
+from code_saturne.model.DefineUserScalarsModel import DefineUserScalarsModel
+from code_saturne.model.NotebookModel import NotebookModel
 
 #-------------------------------------------------------------------------------
 # Variables and Scalar model initialization modelling class
@@ -68,6 +71,10 @@ class SourceTermsModel(Model):
         self.node_therm    = self.models.xmlGetNode('thermal_scalar', 'model')
         self.node_sterm   = self.models.xmlInitNode('source_terms')
 
+        self.therm   = ThermalScalarModel(self.case)
+        self.th_sca  = DefineUserScalarsModel(self.case)
+        self.notebook = NotebookModel(self.case)
+
 
     def __verifyZone(self, zone):
         """Private method.
@@ -75,6 +82,42 @@ class SourceTermsModel(Model):
         """
         self.isInt(int(zone))
         self.isInList(zone, LocalizationModel('VolumicZone', self.case).getCodeNumbersList())
+
+
+    def getMomentumFormulaComponents(self, zone):
+
+        exp = self.getMomentumFormula(zone)
+        if not exp:
+            exp = """Su = 0;\nSv = 0;\nSw = 0;\n
+dSudu = 0;\ndSudv = 0;\ndSudw = 0;\n
+dSvdu = 0;\ndSvdv = 0;\ndSvdw = 0;\n
+dSwdu = 0;\ndSwdv = 0;\ndSwdw = 0;\n"""
+
+        req = [('Su', "x component of the momentum source term"),
+               ('Sv', "y component of the momentum source term"),
+               ('Sw', "z component of the momentum source term"),
+               ('dSudu', "x component x velocity derivative"),
+               ('dSudv', "x component y velocity derivative"),
+               ('dSudw', "x component z velocity derivative"),
+               ('dSvdu', "y component x velocity derivative"),
+               ('dSvdv', "y component y velocity derivative"),
+               ('dSvdw', "y component z velocity derivative"),
+               ('dSwdu', "z component x velocity derivative"),
+               ('dSwdv', "z component y velocity derivative"),
+               ('dSwdw', "z component z velocity derivative")]
+        sym = [('x', 'cell center coordinate'),
+               ('y', 'cell center coordinate'),
+               ('z', 'cell center coordinate')]
+
+        sym.append( ("velocity[0]", 'x velocity component'))
+        sym.append( ("velocity[1]", 'y velocity component'))
+        sym.append( ("velocity[2]", 'z velocity component'))
+        sym.append( ("rho", 'local density (kg/m^3)'))
+
+        for (nme, val) in self.notebook.getNotebookList():
+            sym.append((nme, 'value (notebook) = ' + str(val)))
+
+        return exp, req, sym
 
 
     @Variables.undoLocal
@@ -104,6 +147,25 @@ class SourceTermsModel(Model):
         formula = node.xmlGetString('momentum_formula', zone_id=zone)
 
         return formula
+
+
+    def getSpeciesFormulaComponents(self, zone, scalar):
+
+        exp = self.getSpeciesFormula(zone, scalar)
+        if not exp:
+            exp = """S = 0;\ndS = 0;\n"""
+        req = [('S', 'species source term'),
+               ('dS', 'species source term derivative')]
+        sym = [('x', 'cell center coordinate'),
+               ('y', 'cell center coordinate'),
+               ('z', 'cell center coordinate')]
+        name = self.th_sca.getScalarName(self.scalar)
+        sym.append((name, 'current species'))
+
+        for (nme, val) in self.notebook.getNotebookList():
+            sym.append((nme, 'value (notebook) = ' + str(val)))
+
+        return exp, req, sym
 
 
     @Variables.undoGlobal
@@ -141,6 +203,27 @@ class SourceTermsModel(Model):
         return formula
 
 
+    def getGroundWaterSpeciesFormulaComponents(self, zone, species):
+
+        exp = self.getGroundWaterSpeciesFormula(zone, scalar)
+        if not exp:
+            exp = """Q = 0;"""
+
+        req = [('Q', 'species source term')]
+        sym = [('x', 'cell center coordinate'),
+               ('y', 'cell center coordinate'),
+               ('z', 'cell center coordinate'),
+               ('t', 'current time')]
+
+        name = self.th_sca.getScalarName(self.scalar)
+        sym.append((name, 'current species'))
+
+        for (nme, val) in self.notebook.getNotebookList():
+            sym.append((nme, 'value (notebook) = ' + str(val)))
+
+        return exp, req, sym
+
+
     @Variables.undoGlobal
     def setGroundWaterSpeciesFormula(self, zone, species, formula):
         """
@@ -176,6 +259,23 @@ class SourceTermsModel(Model):
         return formula
 
 
+    def getRichardsFormulaComponents(self, zone):
+        exp = self.getRichardsFormula(zone)
+        if not exp:
+            exp = """Qs = 0;\n"""
+
+        req = [('Qs', 'volumetric source term')]
+        sym = [('x', 'cell center coordinate'),
+               ('y', 'cell center coordinate'),
+               ('z', 'cell center coordinate'),
+               ('t', 'current time')]
+
+        for (nme, val) in self.notebook.getNotebookList():
+            sym.append((nme, 'value (notebook) = ' + str(val)))
+
+        return exp, req, sym
+
+
     @Variables.undoGlobal
     def setRichardsFormula(self, zone, formula):
         """
@@ -205,6 +305,33 @@ class SourceTermsModel(Model):
         formula = node.xmlGetString('volumetric_source_term', zone_id=zone)
 
         return formula
+
+
+    def getThermalFormulaComponents(self, zone, scalar):
+
+        exp = self.getThermalFormula(zone, scalar)
+        if not exp:
+            exp = self.getDefaultThermalFormula(scalar)
+        req = [('S', 'thermal source term'),
+               ('dS', 'thermal source term derivative')]
+        sym = [('x', 'cell center coordinate'),
+               ('y', 'cell center coordinate'),
+               ('z', 'cell center coordinate')]
+
+        if self.case['package'].name == 'code_saturne':
+            if self.therm.getThermalScalarModel() == 'enthalpy':
+                sym.append(('enthalpy', 'thermal scalar'))
+            if self.therm.getThermalScalarModel() == 'total_energy':
+                sym.append(('total_energy', 'thermal scalar'))
+            else:
+                sym.append(('temperature', 'thermal scalar'))
+        else:
+            sym.append(('enthalpy', 'Enthalpy'))
+
+        for (nme, val) in self.notebook.getNotebookList():
+            sym.append((nme, 'value (notebook) = ' + str(val)))
+
+        return exp, req, sym
 
 
     @Variables.undoGlobal

bin/model/InitializationModel.py
@@ -50,6 +50,7 @@ from code_saturne.model.LocalizationModel import LocalizationModel
 from code_saturne.model.CompressibleModel import CompressibleModel
 from code_saturne.model.ElectricalModel import ElectricalModel
 from code_saturne.model.ThermalScalarModel import ThermalScalarModel
+from code_saturne.model.NotebookModel import NotebookModel
 
 #-------------------------------------------------------------------------------
 # Variables and Scalar model initialization modelling class
@@ -80,6 +81,7 @@ class InitializationModel(Model):
         self.turbulenceModes = ('formula',
                                 'reference_value')
         self.node_scalartherm = self.models.xmlGetNode('thermal_scalar')
+        self.notebook = NotebookModel(self.case)
 
 
     def __defaultValues(self):
@@ -202,6 +204,60 @@ omega = k^0.5/almax;"""
         node_init['choice'] = init_mode
 
 
+    def getTurbFormulaComponents(self, zone, turb_model):
+        """
+        public method.
+        Return formula components (exp, req and symbols)
+        """
+
+        exp = self.getTurbFormula(zone, turb_model)
+
+        if turb_model in ('k-epsilon', 'k-epsilon-PL'):
+            req = [('k', "turbulent energy"),
+                   ('epsilon', "turbulent dissipation")]
+        elif turb_model in ('Rij-epsilon', 'Rij-SSG'):
+            req = [('r11', "Reynolds stress R11"),
+                   ('r22', "Reynolds stress R22"),
+                   ('r33', "Reynolds stress R33"),
+                   ('r12', "Reynolds stress R12"),
+                   ('r23', "Reynolds stress R23"),
+                   ('r13', "Reynolds stress R13"),
+                   ('epsilon', "turbulent dissipation")]
+        elif turb_model == 'Rij-EBRSM':
+            req = [('r11', "Reynolds stress R11"),
+                   ('r22', "Reynolds stress R22"),
+                   ('r33', "Reynolds stress R33"),
+                   ('r12', "Reynolds stress R12"),
+                   ('r23', "Reynolds stress R23"),
+                   ('r13', "Reynolds stress R13"),
+                   ('epsilon', "turbulent dissipation"),
+                   ('alpha', "alpha")]
+        elif turb_model == 'v2f-BL-v2/k':
+            req = [('k', "turbulent energy"),
+                   ('epsilon', "turbulent dissipation"),
+                   ('phi', "variable phi in v2f model"),
+                   ('alpha', "variable alpha in v2f model")]
+        elif turb_model == 'k-omega-SST':
+            req = [('k', "turbulent energy"),
+                   ('omega', "specific dissipation rate")]
+        elif turb_model == 'Spalart-Allmaras':
+            req = [('nu_tilda', "nusa")]
+
+        sym = [('rho0', 'density (reference value)'),
+               ('mu0', 'viscosity (reference value)'),
+               ('cp0', 'specific heat (reference value)'),
+               ('x','cell center coordinate'),
+               ('y','cell center coordinate'),
+               ('z','cell center coordinate'),
+               ('uref','reference velocity'),
+               ('almax','reference length')]
+
+        for (nme, val) in self.notebook.getNotebookList():
+            sym.append((nme, 'value (notebook) = ' + str(val)))
+
+        return exp, req, sym
+
+
     @Variables.noUndo
     def getTurbFormula(self, zone, turb_model):
         """
@@ -233,6 +289,25 @@ omega = k^0.5/almax;"""
         n.xmlSetTextNode(formula)
 
 
+    def getVelocityFormulaComponents(self, zone):
+        exp = self.getVelocityFormula(zone)
+        if not exp:
+            exp = self.getDefaultVelocityFormula()
+
+        req = [('velocity[0]', "velocity"),
+               ('velocity[1]', "velocity"),
+               ('velocity[2]', "velocity")]
+        sym = [('uref', 'reference velocity'),
+               ('x', 'cell center coordinate'),
+               ('y', 'cell center coordinate'),
+               ('z', 'cell center coordinate')]
+
+        for (nme, val) in self.notebook.getNotebookList():
+            sym.append((nme, 'value (notebook) = ' + str(val)))
+
+        return exp, req, sym
+
+
     @Variables.undoLocal
     def setVelocityFormula(self, zone, formula):
         """
@@ -256,6 +331,27 @@ omega = k^0.5/almax;"""
 
         formula = node.xmlGetString('formula', zone_id=zone)
         return formula
+
+
+    def getThermalFormulaComponents(self, zone):
+        exp = self.getThermalFormula(zone)
+        if not exp:
+            exp = self.getDefaultThermalFormula()
+
+        if self.therm.getThermalScalarModel() == "enthalpy":
+            req = [('enthalpy', 'enthalpy')]
+        elif self.therm.getThermalScalarModel() == "total_energy":
+            req = [('total_energy', 'total energy')]
+        else:
+            req = [('temperature', 'temperature')]
+        sym = [('x', 'cell center coordinate'),
+               ('y', 'cell center coordinate'),
+               ('z', 'cell center coordinate')]
+
+        for (nme, val) in self.notebook.getNotebookList():
+            sym.append((nme, 'value (notebook) = ' + str(val)))
+
+        return exp, req, sym
 
 
     @Variables.undoLocal
@@ -389,6 +485,24 @@ omega = k^0.5/almax;"""
         n['status'] = status
 
 
+    def getPressureFormulaComponents(self, zone):
+        exp = self.getPressureFormula(zone)
+        if not exp:
+            exp = """p0 = 0.;
+g = 9.81;
+ro = 1.17862;
+pressure = p0 + g * ro * z;\n"""
+        req = [('pressure', 'pressure')]
+        sym = [('x', 'cell center coordinate'),
+               ('y', 'cell center coordinate'),
+               ('z', 'cell center coordinate')]
+
+        for (nme, val) in self.notebook.getNotebookList():
+            sym.append((nme, 'value (notebook) = ' + str(val)))
+
+        return exp, req, sym
+
+
     @Variables.undoLocal
     def setPressureFormula(self, zone, formula):
         """
@@ -420,6 +534,23 @@ omega = k^0.5/almax;"""
 
         formula = node.xmlGetString('formula', zone_id=zone)
         return formula
+
+
+    def getHydraulicHeadFormulaComponents(self, zone):
+
+        exp = self.getHydraulicHeadFormula(zone)
+        if not exp:
+            exp = """H = z;\n"""
+        req = [('H', 'hydraulic head')]
+        sym = [('x', 'cell center coordinate'),
+               ('y', 'cell center coordinate'),
+               ('z', 'cell center coordinate')]
+
+        for (nme, val) in self.notebook.getNotebookList():
+            sym.append((nme, 'value (notebook) = ' + str(val)))
+
+        return exp, req, sym
+
 
     @Variables.undoLocal
     def setHydraulicHeadFormula(self, zone, formula):
@@ -454,6 +585,23 @@ omega = k^0.5/almax;"""
         return formula
 
 
+    def getDensityFormulaComponents(self, zone):
+        exp = self.getDensityFormula(zone)
+        if not exp:
+            exp = """density = 0;\n"""
+
+        req = [('density', 'density')]
+        sym = [('x', 'cell center coordinate'),
+               ('y', 'cell center coordinate'),
+               ('z', 'cell center coordinate')]
+
+        for (nme, val) in self.notebook.getNotebookList():
+            sym.append((nme, 'value (notebook) = ' + str(val)))
+
+        return exp, req, sym
+
+
+
     @Variables.undoLocal
     def setDensityFormula(self, zone, formula):
         """
@@ -483,6 +631,22 @@ omega = k^0.5/almax;"""
 
         formula = node.xmlGetString('formula', zone_id=zone)
         return formula
+
+
+    def getTemperatureFormulaComponents(self, zone):
+        exp = self.getTemperatureFormula(zone)
+        if not exp:
+            exp = """temperature = 0;\n"""
+
+        req = [('temperature', 'temperature')]
+        sym = [('x', 'cell center coordinate'),
+               ('y', 'cell center coordinate'),
+               ('z', 'cell center coordinate')]
+
+        for (nme, val) in self.notebook.getNotebookList():
+            sym.append((nme, 'value (notebook) = ' + str(val)))
+
+        return exp, req, sym
 
 
     @Variables.undoLocal
@@ -516,6 +680,21 @@ omega = k^0.5/almax;"""
 
         formula = node.xmlGetString('formula', zone_id=zone)
         return formula
+
+
+    def getEnergyFormulaComponents(self, zone):
+        exp = self.getEnergyFormula(zone)
+        if not exp:
+            exp = """total_energy = 0;\n"""
+        req = [('total_energy', 'Energy')]
+        sym = [('x', 'cell center coordinate'),
+               ('y', 'cell center coordinate'),
+               ('z', 'cell center coordinate')]
+
+        for (nme, val) in self.notebook.getNotebookList():
+            sym.append((nme, 'value (notebook) = ' + str(val)))
+
+        return exp, req, sym
 
 
     @Variables.undoLocal
@@ -571,6 +750,23 @@ omega = k^0.5/almax;"""
         return box_list
 
 
+    def getSpeciesFormulaComponents(self, zone, species):
+        exp = self.getSpeciesFormula(zone, species)
+        name = DefineUserScalarsModel(self.case).getScalarName(species)
+        if not exp:
+            exp = str(name)+""" = 0;\n"""
+
+        req = [(str(name), str(name))]
+        sym = [('x', 'cell center coordinate'),
+               ('y', 'cell center coordinate'),
+               ('z', 'cell center coordinate')]
+
+        for (nme, val) in self.notebook.getNotebookList():
+            sym.append((nme, 'value (notebook) = ' + str(val)))
+
+        return exp, req, sym
+
+
     @Variables.undoLocal
     def setSpeciesFormula(self, zone, species, formula):
         """
@@ -603,6 +799,21 @@ omega = k^0.5/almax;"""
         formula = node.xmlGetString('formula', zone_id=zone)
 
         return formula
+
+
+    def getMeteoFormulaComponents(self, zone, scalar):
+        exp = self.getMeteoFormula(zone, scalar)
+        if not exp:
+            exp = str(scalar)+""" = 0;\n"""
+        req = [(str(scalar), str(scalar))]
+        sym = [('x', 'cell center coordinate'),
+               ('y', 'cell center coordinate'),
+               ('z', 'cell center coordinate')]
+
+        for (nme, val) in self.notebook.getNotebookList():
+            sym.append((nme, 'value (notebook) = ' + str(val)))
+
+        return exp, req, sym
 
 
     @Variables.undoLocal

gui/Pages/InitializationView.py
@@ -272,20 +272,9 @@ class InitializationView(QWidget, Ui_InitializationForm):
     def slotVelocityFormula(self):
         """
         """
-        exp = self.init.getVelocityFormula(self.zone)
-        if not exp:
-            exp = self.init.getDefaultVelocityFormula()
         exa = """#example: \n""" + self.init.getDefaultVelocityFormula()
-        req = [('velocity[0]', "velocity"),
-               ('velocity[1]', "velocity"),
-               ('velocity[2]', "velocity")]
-        sym = [('uref', 'reference velocity'),
-               ('x', 'cell center coordinate'),
-               ('y', 'cell center coordinate'),
-               ('z', 'cell center coordinate')]
 
-        for (nme, val) in self.notebook.getNotebookList():
-            sym.append((nme, 'value (notebook) = ' + str(val)))
+        exp, req, sym = self.init.getVelocityFormulaComponents(self.zone)
 
         dialog = QMeiEditorView(self,
                                 check_syntax = self.case['package'].get_check_syntax(),
@@ -308,49 +297,8 @@ class InitializationView(QWidget, Ui_InitializationForm):
         """
         turb_model = self.turb.getTurbulenceModel()
         exa = """#example \n""" + self.init.getDefaultTurbFormula(turb_model)
-        exp = self.init.getTurbFormula(self.zone, turb_model)
-        sym = [('rho0', 'density (reference value)'),
-               ('mu0', 'viscosity (reference value)'),
-               ('cp0', 'specific heat (reference value)'),
-               ('lambda0', 'thermal conductivity (reference value)'),
-               ('x','cell center coordinate'),
-               ('y','cell center coordinate'),
-               ('z','cell center coordinate'),
-               ('uref','reference velocity'),
-               ('almax','reference length')]
-        if turb_model in ('k-epsilon', 'k-epsilon-PL'):
-            req = [('k', "turbulent energy"),
-                   ('epsilon', "turbulent dissipation")]
-        elif turb_model in ('Rij-epsilon', 'Rij-SSG'):
-            req = [('r11', "Reynolds stress R11"),
-                   ('r22', "Reynolds stress R22"),
-                   ('r33', "Reynolds stress R33"),
-                   ('r12', "Reynolds stress R12"),
-                   ('r23', "Reynolds stress R23"),
-                   ('r13', "Reynolds stress R13"),
-                   ('epsilon', "turbulent dissipation")]
-        elif turb_model == 'Rij-EBRSM':
-            req = [('r11', "Reynolds stress R11"),
-                   ('r22', "Reynolds stress R22"),
-                   ('r33', "Reynolds stress R33"),
-                   ('r12', "Reynolds stress R12"),
-                   ('r23', "Reynolds stress R23"),
-                   ('r13', "Reynolds stress R13"),
-                   ('epsilon', "turbulent dissipation"),
-                   ('alpha', "alpha")]
-        elif turb_model == 'v2f-BL-v2/k':
-            req = [('k', "turbulent energy"),
-                   ('epsilon', "turbulent dissipation"),
-                   ('phi', "variable phi in v2f model"),
-                   ('alpha', "variable alpha in v2f model")]
-        elif turb_model == 'k-omega-SST':
-            req = [('k', "turbulent energy"),
-                   ('omega', "specific dissipation rate")]
-        elif turb_model == 'Spalart-Allmaras':
-            req = [('nu_tilda', "nusa")]
 
-        for (nme, val) in self.notebook.getNotebookList():
-            sym.append((nme, 'value (notebook) = ' + str(val)))
+        exp, req, sym = self.init.getTurbFormulaComponents(self.zone, turb_model)
 
         dialog = QMeiEditorView(self,
                                 check_syntax = self.case['package'].get_check_syntax(),
@@ -371,22 +319,9 @@ class InitializationView(QWidget, Ui_InitializationForm):
         """
         Input the initial formula of thermal scalar
         """
-        exp = self.init.getThermalFormula(self.zone)
-        if not exp:
-            exp = self.init.getDefaultThermalFormula()
         exa = """#example \n""" + self.init.getDefaultThermalFormula()
-        if self.therm.getThermalScalarModel() == "enthalpy":
-            req = [('enthalpy', 'enthalpy')]
-        elif self.therm.getThermalScalarModel() == "total_energy":
-            req = [('total_energy', 'total energy')]
-        else:
-            req = [('temperature', 'temperature')]
-        sym = [('x', 'cell center coordinate'),
-               ('y', 'cell center coordinate'),
-               ('z', 'cell center coordinate')]
 
-        for (nme, val) in self.notebook.getNotebookList():
-            sym.append((nme, 'value (notebook) = ' + str(val)))
+        exp, req, sym = self.init.getThermalFormulaComponents(self.zone)
 
         dialog = QMeiEditorView(self,
                                 check_syntax = self.case['package'].get_check_syntax(),
@@ -407,18 +342,9 @@ class InitializationView(QWidget, Ui_InitializationForm):
         """
         Input the initial formula of species
         """
-        exp = self.init.getSpeciesFormula(self.zone, self.scalar)
-        name = self.th_sca.getScalarName(self.scalar)
-        if not exp:
-            exp = str(name)+""" = 0;\n"""
-        exa = """#example: \n""" + str(name)+""" = 0;\n"""
-        req = [(str(name), str(name))]
-        sym = [('x', 'cell center coordinate'),
-               ('y', 'cell center coordinate'),
-               ('z', 'cell center coordinate')]
+        exp, req, sym = self.init.getSpeciesFormulaComponents(self.zone, self.scalar)
 
-        for (nme, val) in self.notebook.getNotebookList():
-            sym.append((nme, 'value (notebook) = ' + str(val)))
+        exa = """#example: \n""" + str(name)+""" = 0;\n"""
 
         dialog = QMeiEditorView(self,
                                 check_syntax = self.case['package'].get_check_syntax(),
@@ -438,18 +364,10 @@ class InitializationView(QWidget, Ui_InitializationForm):
     def slotMeteoFormula(self):
         """
         """
-        exp = self.init.getMeteoFormula(self.zone, self.scalar_meteo)
         name = self.scalar_meteo
-        if not exp:
-            exp = str(name)+""" = 0;\n"""
         exa = """#example: \n""" + str(name)+""" = 0;\n"""
-        req = [(str(name), str(name))]
-        sym = [('x', 'cell center coordinate'),
-               ('y', 'cell center coordinate'),
-               ('z', 'cell center coordinate')]
 
-        for (nme, val) in self.notebook.getNotebookList():
-            sym.append((nme, 'value (notebook) = ' + str(val)))
+        exp, req, sym = self.init.getMeteoFormulaComponents(self.zone, self.scalar_meteo)
 
         dialog = QMeiEditorView(self,
                                 check_syntax = self.case['package'].get_check_syntax(),
@@ -618,20 +536,9 @@ class InitializationView(QWidget, Ui_InitializationForm):
         """
         Input the initial Pressure formula
         """
-        exp = self.init.getPressureFormula(self.zone)
-        if not exp:
-            exp = """p0 = 0.;
-g = 9.81;
-ro = 1.17862;
-pressure = p0 + g * ro * z;\n"""
         exa = """#example: """
-        req = [('pressure', 'pressure')]
-        sym = [('x', 'cell center coordinate'),
-               ('y', 'cell center coordinate'),
-               ('z', 'cell center coordinate')]
 
-        for (nme, val) in self.notebook.getNotebookList():
-            sym.append((nme, 'value (notebook) = ' + str(val)))
+        exp, req, sym = self.init.getPressureFormulaComponents(self.zone)
 
         dialog = QMeiEditorView(self,
                                 check_syntax = self.case['package'].get_check_syntax(),
@@ -653,17 +560,9 @@ pressure = p0 + g * ro * z;\n"""
         """
         Input the initial Hydraulic Head formula
         """
-        exp = self.init.getHydraulicHeadFormula(self.zone)
-        if not exp:
-            exp = """H = z;\n"""
         exa = """#example: """
-        req = [('H', 'hydraulic head')]
-        sym = [('x', 'cell center coordinate'),
-               ('y', 'cell center coordinate'),
-               ('z', 'cell center coordinate')]
 
-        for (nme, val) in self.notebook.getNotebookList():
-            sym.append((nme, 'value (notebook) = ' + str(val)))
+        exp, req, sym = self.init.getHydraulicHeadFormulaComponents(self.zone)
 
         dialog = QMeiEditorView(self,
                                 check_syntax = self.case['package'].get_check_syntax(),
@@ -685,17 +584,9 @@ pressure = p0 + g * ro * z;\n"""
         """
         Input the initial Density formula
         """
-        exp = self.init.getDensityFormula(self.zone)
-        if not exp:
-            exp = """density = 0;\n"""
         exa = """#example: """
-        req = [('density', 'density')]
-        sym = [('x', 'cell center coordinate'),
-               ('y', 'cell center coordinate'),
-               ('z', 'cell center coordinate')]
 
-        for (nme, val) in self.notebook.getNotebookList():
-            sym.append((nme, 'value (notebook) = ' + str(val)))
+        exp, req, sym = self.init.getDensityFormulaComponents(self.zone)
 
         dialog = QMeiEditorView(self,
                                 check_syntax = self.case['package'].get_check_syntax(),
@@ -717,17 +608,9 @@ pressure = p0 + g * ro * z;\n"""
         """
         Input the initial Temperature formula
         """
-        exp = self.init.getTemperatureFormula(self.zone)
-        if not exp:
-            exp = """temperature = 0;\n"""
         exa = """#example: """
-        req = [('temperature', 'temperature')]
-        sym = [('x', 'cell center coordinate'),
-               ('y', 'cell center coordinate'),
-               ('z', 'cell center coordinate')]
 
-        for (nme, val) in self.notebook.getNotebookList():
-            sym.append((nme, 'value (notebook) = ' + str(val)))
+        exp, req, sym = self.init.getTemperatureFormulaComponents(self.zone)
 
         dialog = QMeiEditorView(self,
                                 check_syntax = self.case['package'].get_check_syntax(),
@@ -749,17 +632,9 @@ pressure = p0 + g * ro * z;\n"""
         """
         Input the initial Energy formula
         """
-        exp = self.init.getEnergyFormula(self.zone)
-        if not exp:
-            exp = """total_energy = 0;\n"""
         exa = """#example: """
-        req = [('total_energy', 'Energy')]
-        sym = [('x', 'cell center coordinate'),
-               ('y', 'cell center coordinate'),
-               ('z', 'cell center coordinate')]
 
-        for (nme, val) in self.notebook.getNotebookList():
-            sym.append((nme, 'value (notebook) = ' + str(val)))
+        exp, req, sym = self.init.getEnergyFormulaComponenets(self.zone)
 
         dialog = QMeiEditorView(self,
                                 check_syntax = self.case['package'].get_check_syntax(),

gui/Pages/QMegEditorView.py
@@ -310,18 +310,12 @@ class QMegEditorView(QDialog, Ui_QMeiDialog):
         # Could, and should, be modified in the future to identify
         # the good key if needed...
         new_exp = str(self.textEditExpression.toPlainText()) + '\n'
-        if len(self.mei_to_c.vol_funcs.keys()) > 0:
-            k = self.mei_to_c.vol_funcs.keys()[0]
-            self.mei_to_c.update_cell_block_expression(new_exp, k)
-            check, err_msg, n_errors = self.mei_to_c.check_meg_code_syntax('volume')
-
-        elif len(self.mei_to_c.bnd_funcs.keys()) > 0:
-            k = self.mei_to_c.bnd_funcs.keys()[0]
-            self.mei_to_c.update_bnd_block_expression(new_exp, k)
-            check, err_msg, n_errors = self.mei_to_c.check_meg_code_syntax('boundary')
-
-        else:
-            check = 0
+        check = 0
+        for func_type in self.mei_to_c.funcs.keys():
+            if len(self.mei_to_c.funcs[func_type].keys()) > 0:
+                k = self.mei_to_c.funcs[func_type][0]
+                self.mei_to_c.update_block_expression(func_type, k, new_exp)
+                check, err_msg, n_erros = self.mei_to_c.check_meg_code_syntax(func_type)
 
         if check != 0:
             err_msg = err_msg.decode('utf-8').replace(u"\u2018", "'")

commit a8c6429f6007ba6c2c18231cadcbc8f2fc54753d

[GUI] Fix bug in the species source terms (introduced in c096484, 2019-02-12)

bin/model/SourceTermsModel.py
@@ -149,9 +149,9 @@ dSwdu = 0;\ndSwdv = 0;\ndSwdw = 0;\n"""
         return formula
 
 
-    def getSpeciesFormulaComponents(self, zone, scalar):
+    def getSpeciesFormulaComponents(self, zone, species):
 
-        exp = self.getSpeciesFormula(zone, scalar)
+        exp = self.getSpeciesFormula(zone, species)
         if not exp:
             exp = """S = 0;\ndS = 0;\n"""
         req = [('S', 'species source term'),
@@ -159,7 +159,7 @@ dSwdu = 0;\ndSwdv = 0;\ndSwdw = 0;\n"""
         sym = [('x', 'cell center coordinate'),
                ('y', 'cell center coordinate'),
                ('z', 'cell center coordinate')]
-        name = self.th_sca.getScalarName(self.scalar)
+        name = self.th_sca.getScalarName(species)
         sym.append((name, 'current species'))
 
         for (nme, val) in self.notebook.getNotebookList():