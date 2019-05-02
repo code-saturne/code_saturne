code-saturne/code_saturne

commit 1c5e9fc7e234ebad74c11f1412bb4d86df756781

Lagrangian module: all boundary mass flow statistics now based on events.
Some other boundary statistics also use the new system, so the legacy
boundary stats has been reduced to stats which previously set "imoybr = 0".
This has allowed some simplifications.

An example of a user-defined boundary statistic is also provided.

src/gui/cs_gui_particles.c
@@ -412,24 +412,39 @@ cs_gui_particles_model(void)
 
     if (boundary_stats) {
 
-      _get_stats_post(tn_bs, "Part_impact_number",
-                      &cs_glob_lagr_boundary_interactions->has_part_impact_nbr);
-      _get_stats_post(tn_bs, "Part_bndy_mass_flux",
-                      &cs_glob_lagr_boundary_interactions->iflmbd);
-      _get_stats_post(tn_bs, "Part_impact_angle",
-                      &cs_glob_lagr_boundary_interactions->iangbd);
-      _get_stats_post(tn_bs, "Part_impact_velocity",
-                      &cs_glob_lagr_boundary_interactions->ivitbd);
+      int flag = 0;
+      _get_stats_post(tn_bs, "Part_impact_number", &flag);
+      if (flag)
+        cs_lagr_stat_activate(CS_LAGR_STAT_E_CUMULATIVE_WEIGHT);
+
+      flag = 0;
+      _get_stats_post(tn_bs, "Part_bndy_mass_flux", &flag);
+      if (flag)
+        cs_lagr_stat_activate(CS_LAGR_STAT_MASS_FLUX);
+
+      flag = 0;
+      _get_stats_post(tn_bs, "Part_impact_velocity", &flag);
+      if (flag)
+        cs_lagr_stat_activate(CS_LAGR_STAT_IMPACT_VELOCITY);
 
       /* Coal fouling statistics*/
-      _get_stats_post(tn_bs, "Part_fouled_impact_number",
-                      &cs_glob_lagr_boundary_interactions->iencnbbd);
-      _get_stats_post(tn_bs, "Part_fouled_mass_flux",
-                      &cs_glob_lagr_boundary_interactions->iencmabd);
-      _get_stats_post(tn_bs, "Part_fouled_diam",
-                      &cs_glob_lagr_boundary_interactions->iencdibd);
-      _get_stats_post(tn_bs, "Part_fouled_Xck",
-                      &cs_glob_lagr_boundary_interactions->iencckbd);
+
+      flag = 0;
+      _get_stats_post(tn_bs, "Part_fouled_impact_number", &flag);
+      cs_lagr_stat_activate(CS_LAGR_STAT_FOULING_CUMULATIVE_WEIGHT);
+
+      flag = 0;
+      _get_stats_post(tn_bs, "Part_fouled_mass_flux", &flag);
+      if (flag)
+        cs_lagr_stat_activate(CS_LAGR_STAT_FOULING_MASS_FLUX);
+
+      _get_stats_post(tn_bs, "Part_fouled_diam", &flag);
+      if (flag)
+        cs_lagr_stat_activate(CS_LAGR_STAT_FOULING_DIAMETER);
+
+      _get_stats_post(tn_bs, "Part_fouled_Xck", &flag);
+      if (flag)
+        cs_lagr_stat_activate(CS_LAGR_STAT_FOULING_COKE_FRACTION);
 
     }
   }
@@ -491,12 +506,7 @@ cs_gui_particles_model(void)
   if (b_stats) {
     bft_printf("--has_part_impact_nbr   = %i\n",
                cs_glob_lagr_boundary_interactions->has_part_impact_nbr);
-    bft_printf("--iflmbd   = %i\n", cs_glob_lagr_boundary_interactions->iflmbd);
-    bft_printf("--iangbd   = %i\n", cs_glob_lagr_boundary_interactions->iangbd);
-    bft_printf("--ivitbd   = %i\n", cs_glob_lagr_boundary_interactions->ivitbd);
     bft_printf("--iencnbbd = %i\n", cs_glob_lagr_boundary_interactions->iencnbbd);
-    bft_printf("--iencmabd = %i\n", cs_glob_lagr_boundary_interactions->iencmabd);
-    bft_printf("--iencdibd = %i\n", cs_glob_lagr_boundary_interactions->iencdibd);
     bft_printf("--iencckbd = %i\n", cs_glob_lagr_boundary_interactions->iencckbd);
   }
 

po/fr.po
@@ -7,7 +7,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: code-saturne\n"
 "Report-Msgid-Bugs-To: saturne-support@edf.fr\n"
-"POT-Creation-Date: 2019-04-25 17:17+0200\n"
+"POT-Creation-Date: 2019-04-27 21:20+0200\n"
 "PO-Revision-Date: 2012-01-26 12:00+0100\n"
 "Last-Translator: Code_Saturne <saturne-support@edf.fr>\n"
 "Language-Team: French <traduc@traduc.org>\n"
@@ -12713,6 +12713,17 @@ msgid "VARIANCE"
 msgstr ""
 
 #, c-format
+msgid "    start time: %g"
+msgstr "    temps de début : %g"
+
+#, c-format
+msgid "    start time step: %d"
+msgstr "    pas de temps de début : %d"
+
+msgid " (reset allowed)\n"
+msgstr " (remise à zéro autorisée)\n"
+
+#, c-format
 msgid "Out-of range statistics type: %d"
 msgstr ""
 
@@ -12775,6 +12786,92 @@ msgstr ""
 #, c-format
 msgid "The '%s' argument to %s must not be NULL."
 msgstr ""
+
+msgid ""
+"\n"
+"  Mesh-based statistics\n"
+"  ---------------------\n"
+msgstr ""
+"\n"
+"  Statistiques basées sur le maillage\n"
+"  -----------------------------------\n"
+
+#, c-format
+msgid ""
+"\n"
+"  statistic %d\n"
+"    group: %s\n"
+"    class: %d\n"
+msgstr ""
+
+#, c-format
+msgid "    field: \"%s\" (%d)\n"
+msgstr "    champ : \"%s\" (%d)\n"
+
+#, c-format
+msgid "    location: %s\n"
+msgstr "    support : %s\n"
+
+msgid "    mesh-based data function\n"
+msgstr ""
+
+msgid ""
+"\n"
+"  Lagrangian moment accumulators\n"
+"  ------------------------------\n"
+msgstr ""
+"\n"
+"  Accumulateurs pour moments Lagrangiens\n"
+"  --------------------------------------\n"
+
+#, c-format
+msgid ""
+"\n"
+"  accumulator %d\n"
+"    group: %s\n"
+"    class: %d\n"
+msgstr ""
+"\n"
+"  accumulateur : %d\n"
+"    groupe : %s\n"
+"    classe : %d\n"
+
+msgid "    particle-based data function\n"
+msgstr "    fonction de données basée sur les particules\n"
+
+msgid "    event-based data function\n"
+msgstr "    fonction de données basée sur les événements\n"
+
+msgid ""
+"\n"
+"  Lagrangian moments\n"
+"  ------------------\n"
+msgstr ""
+"\n"
+"  Moments statistiques Lagrangiens\n"
+"  --------------------------------\n"
+
+#, c-format
+msgid ""
+"\n"
+"  moment %d\n"
+"    accumulator id: %d\n"
+"    class: %d\n"
+"    moment type: %s\n"
+msgstr ""
+"\n"
+"  moment %d\n"
+"    id accumulateur : %d\n"
+"    classe : %d\n"
+"    type de moment : %s\n"
+
+#, c-format
+msgid "    predefined stat type: %d\n"
+msgstr "    type de statistique prédéfine : %d\n"
+
+#, c-format
+msgid "    component id: %d\n"
+msgstr "    id de composante : %d\n"
 
 msgid ""
 "\n"

src/lagr/cs_lagr.c
@@ -302,25 +302,8 @@ static cs_lagr_boundary_interactions_t _cs_glob_lagr_boundary_interactions
   = {.npstf = 0,
      .npstft = 0,
      .has_part_impact_nbr = 0,
-     .iflmbd = 0,
-     .iangbd= 0,
-     .ivitbd = 0,
      .iclgst = 0,
-     .iencnbbd = 0,
-     .iencmabd = 0,
-     .iencdibd = 0,
-     .iencckbd = 0,
      .inbr = -1,
-     .iflm = -1,
-     .iang = -1,
-     .ivit = -1,
-     .ires = -1,
-     .iflres = -1,
-     .iencnb = -1,
-     .iencma = -1,
-     .iencdi = -1,
-     .iencck = -1,
-     .imoybr = NULL,
      .inclg = -1,
      .inclgt = -1,
      .iclogt = -1,
@@ -1147,8 +1130,6 @@ cs_lagr_finalize(void)
   BFT_FREE(cs_glob_lagr_encrustation->visref);
 
   /* boundary interaction pointers */
-
-  BFT_FREE(cs_glob_lagr_boundary_interactions->imoybr);
 
   for (int i = 0; i < cs_glob_lagr_dim->n_boundary_stats; i++)
     BFT_FREE(cs_glob_lagr_boundary_interactions->nombrd[i]);

src/lagr/cs_lagr.h
@@ -701,35 +701,31 @@ typedef struct {
 
 typedef struct {
 
-  /*  activates (=1) or not (=0) the option of coal particle fouling.
-   It then is necessary to specify the domain boundaries
-   on which fouling may take place. Useful if \ref physical_model = 2*/
+  /* Activates (=1) or not (=0) the option of coal particle fouling.
+     It then is necessary to specify the domain boundaries
+     on which fouling may take place. Useful if \ref physical_model = 2*/
   int  iencra;
 
-  /*  encrustation data*/
+  /* encrustation data*/
   int  npencr;
   // TODO cf particles->n_part_fou in cs_lagr_tracking.c
 
-  /*  encrustation data*/
-//TODO
-  cs_real_t  *enc1;//ncharm2
-  /*  encrustation data*/
-//TODO
-  cs_real_t  *enc2;//ncharm2
+  /* encrustation data*/
+  cs_real_t  *enc1;  // size: ncharm2
+  /* encrustation data*/
+  cs_real_t  *enc2;  // size: ncharm2
 
-  /*  limit temperature (in degree Celsius) below which the coal particles do
-   not cause any fouling (if the fouling model is activated).
-   Useful if \ref physical_model = 2 and \ref iencra = 1*/
-//TODO
-  cs_real_t  *tprenc;//ncharm2
+  /* Limit temperature (in degree Celsius) below which the coal particles do
+     not cause any fouling (if the fouling model is activated).
+     Useful if \ref physical_model = 2 and \ref iencra = 1*/
+  cs_real_t  *tprenc; // size: ncharm2
 
-  /*  ash critical viscosity in \f$ kg.m^{-1}.s^{-1} \f$, in the fouling model
-   cf J.D. Watt et T. Fereday (J.Inst.Fuel, Vol.42-p99).
-   Useful if \ref physical_model = 2 and \ref iencra = 1*/
-//TODO
-  cs_real_t  *visref;//ncharm2
+  /* Ash critical viscosity in \f$ kg.m^{-1}.s^{-1} \f$, in the fouling model
+     cf J.D. Watt et T. Fereday (J.Inst.Fuel, Vol.42-p99).
+     Useful if \ref physical_model = 2 and \ref iencra = 1*/
+  cs_real_t  *visref;  // size: ncharm2
 
-  /*  encrustation data */
+  /* encrustation data */
   cs_real_t  dnpenc;
 
 } cs_lagr_encrustation_t;
@@ -777,7 +773,7 @@ typedef struct {
 
 typedef struct {
 
-  /*! number of iterations during which steady boundary statistics have
+  /*! Number of iterations during which steady boundary statistics have
     been accumulated.
     Useful if \ref isttio=1 and \ref nstist inferior
     or equal to the current time step.
@@ -795,94 +791,16 @@ typedef struct {
 
   /*! activation (=1) or not (=0) of the recording of the number of
     particle/boundary interactions, and of the calculation of the associated
-    boundary statistics.
-    \ref has_part_impact_nbr is set to 1 when using the particulate average
-    \ref imoybr = 2. */
+    boundary statistics. */
   int  has_part_impact_nbr;
-
-  /*!  activation (=1) or not (=0) of the recording of the particulate mass flow
-    related to the particle/boundary interactions, and of the calculation of
-    the associated boundary statistics.
-    If set to 2 or 3 respectively, time averages and variances are also
-    computed.
-    \ref has_part_impact_nbr is set to 1 when using \ref iflmbd=1.
-   */
-  int  iflmbd;
-
-  /*!  activation (=1) or not (=0) of the recording of the angle between a
-    particle trajectory and a boundary face involved in a particle/boundary
-    interaction, and of the calculation of the associated boundary statistics. */
-  int  iangbd;
-
-  /*!  activation (=1) or not (=0) of the recording of the velocity of a particle
-    involved in a particle/boundary interaction, and of the calculation of
-    the associated boundary statistics. */
-  int  ivitbd;
 
   /*!  activation (=1) or not (=0) of the recording of clogging parameters
     involved in a particle/boundary interaction, and of the calculation of
     the associated boundary statistics. */
   int  iclgst;
 
-  /*! flag for number of recorded particle/boundary interactions with fouling */
-  int  iencnbbd;
-
-  /*! flag for mass of fouled coal particles */
-  int  iencmabd;
-
-  /*! flag for diameter of fouled coal particles */
-  int  iencdibd;
-
-  /*! flag for coke fraction of fouled coal particles */
-  int  iencckbd;
-
   /*!  id for number of particle/boundary interactions */
   int  inbr;
-
-  /*!  id for particle mass flow at the boundary faces */
-  int  iflm;
-
-  /*!  id for mean interaction angle with the boundary faces */
-  int  iang;
-
-  /*!  id for mean interaction velocity with the boundary faces */
-  int  ivit;
-
-  /*!  id for number of resuspended particles */
-  int  ires;
-
-  /*!  id for mass flow of resuspended particles at the boundary faces */
-  int  iflres;
-
-  /*! flag for number of recorded particle/boundary interactions with fouling */
-  int  iencnb;
-
-  /*! id for mass of fouled coal particles */
-  int  iencma;
-
-  /*! id for diameter of fouled coal particles */
-  int  iencdi;
-
-  /*! id for coke fraction of fouled coal particles */
-  int  iencck;
-
-  /*! the recordings in \ref bound_stat at every particle/boundary interaction are
-      cumulated values (possibly reset to zero at every iteration in the
-      unsteady case). They must therefore be divided by a quantity to
-      get boundary statistics. The user can choose between two average types:
-      - = 0: no average is applied to the recorded cumulated values.
-      - = 1: a time-average is calculated. The cumulated value
-             is divided by the physical duration in the case of steady
-             averages (\ref isttio=1). The cumulated value is divided by the
-             value of the last time step in the case of unsteady averages
-             (\ref isttio=0), and also in the case of steady averages while the
-             absolute iteration number is inferior to \ref nstist.
-      - = 2: a particulate average is calculated. The cumulated value is divided
-             by the number of particle/boundary interactions (in terms of
-             statistical weight) recorded in \ref bound_stat "bound_stat"(nfabor,inbr).
-             This average can only be calculated when \ref has_part_impact_nbr = 1.
-             Only the cumulated value is recorded in the restart file. */
-  int  *imoybr;
 
   /*! id for number of deposited particles */
   int  inclg;

src/lagr/cs_lagr_event.c
@@ -423,6 +423,10 @@ cs_lagr_event_initialize(void)
        CS_LAGR_TEMPERATURE,
        CS_LAGR_FLUID_TEMPERATURE,
        CS_LAGR_CP,
+       CS_LAGR_WATER_MASS,
+       CS_LAGR_COAL_MASS,
+       CS_LAGR_COKE_MASS,
+       CS_LAGR_SHRINKING_DIAMETER,
        CS_LAGR_STAT_CLASS,
        CS_LAGR_USER};
 

src/lagr/cs_lagr_event.h
@@ -50,16 +50,28 @@ BEGIN_C_DECLS
  */
 
 /*! inflow event */
-#define CS_EVENT_INFLOW              (1 << 0)
+#define CS_EVENT_INFLOW                (1 << 0)
 
 /*! outflow event */
-#define CS_EVENT_OUTFLOW             (1 << 1)
+#define CS_EVENT_OUTFLOW               (1 << 1)
 
 /*! rebound on wall */
-#define CS_EVENT_REBOUND             (1 << 2)
+#define CS_EVENT_REBOUND               (1 << 2)
 
-/*! resulspension event */
-#define CS_EVENT_RESUSPENSION        (1 << 3)
+/*! deposistion event */
+#define CS_EVENT_DEPOSITION            (1 << 3)
+
+/*! resuspension event */
+#define CS_EVENT_RESUSPENSION          (1 << 4)
+
+/*! roll off */
+#define CS_EVENT_ROLL_OFF              (1 << 5)
+
+/*! roll on */
+#define CS_EVENT_ROLL_ON               (1 << 6)
+
+/*! fouling  */
+#define CS_EVENT_FOULING               (1 << 7)
 
 /*============================================================================
  * Type definitions

src/lagr/cs_lagr_injection.c
@@ -1407,7 +1407,7 @@ cs_lagr_injection(int        time_id,
           /* Add particle tracking events for boundary injection */
 
           if (   particle_face_ids != NULL
-              && cs_glob_lagr_boundary_interactions->iflmbd) {
+              && cs_lagr_stat_is_active(CS_LAGR_STAT_GROUP_TRACKING_EVENT)) {
 
             cs_lagr_event_set_t  *events
               = cs_lagr_event_set_boundary_interaction();

commit bba5663a1ea55bc2a90f1e737b1260c6e835fb9a

Lagrangian module: fixes to statistics naming.
Naming was broken by commit 1c5e9fc7 (April 26).

src/gui/cs_gui_particles.c
@@ -423,6 +423,11 @@ cs_gui_particles_model(void)
         cs_lagr_stat_activate(CS_LAGR_STAT_MASS_FLUX);
 
       flag = 0;
+      _get_stats_post(tn_bs, "Part_impact_angle", &flag);
+      if (flag)
+        cs_lagr_stat_activate(CS_LAGR_STAT_IMPACT_ANGLE);
+
+      flag = 0;
       _get_stats_post(tn_bs, "Part_impact_velocity", &flag);
       if (flag)
         cs_lagr_stat_activate(CS_LAGR_STAT_IMPACT_VELOCITY);

src/lagr/cs_lagr_stat.c
@@ -341,7 +341,7 @@ _class_name(const char  *name,
   size_t l0 = strlen(_class_ext);
 
   snprintf(class_name, 63 - l0, name);
-  class_name[64-l0] = '\0';
+  class_name[63-l0] = '\0';
   strcat(class_name, _class_ext);
   class_name[63] = '\0';
 }
@@ -1087,7 +1087,7 @@ _moment_name(const char            *base_name,
 
   snprintf(name,
            63 - l0,
-           "%s_particle_%s",
+           "%s_%s",
            type_name[moment_type],
            base_name);
   name[63] = '\0';
@@ -3324,7 +3324,7 @@ _event_stat_initialize(void)
      (all part of particle movement) */
 
   int                      b_stat_type[3];
-  char                     b_stat_name[64][3];
+  char                     b_stat_name[3][64];
   cs_lagr_moment_m_data_t *b_stat_u_func[3];
   cs_lagr_moment_m_data_t *b_stat_tm_func[3];
   void                    *b_stat_u_input[3];
@@ -3479,37 +3479,29 @@ _event_stat_initialize(void)
 
         switch(stat_type) {
         case CS_LAGR_STAT_IMPACT_ANGLE:
-          _moment_name("particle_impact_angle", -1, class, m_type, name);
+          strncpy(name, "particle_impact_angle", 63);
           stat_type_def = -1;
           data_func = _boundary_impact_angle;
           break;
         case CS_LAGR_STAT_IMPACT_VELOCITY:
-          _moment_name("particle_impact_velocity", -1, class, m_type, name);
+          strncpy(name, "particle_impact_velocity", 63);
           stat_type_def = -1;
           data_func = _boundary_impact_velocity;
           break;
         case CS_LAGR_STAT_FOULING_DIAMETER:
-          _moment_name("particle_fouing_diameter", -1, class, m_type, name);
+          strncpy(name, "particle_fouling_diameter", 63);
           stat_type_def = -1;
           data_func = _boundary_fouling_diameter;
           w_data_func = _boundary_fouling_weight;
           break;
         case CS_LAGR_STAT_FOULING_COKE_FRACTION:
-          _moment_name("particle_fouing_coke_fraction", -1, class, m_type, name);
+          strncpy(name, "particle_fouling_coke_fraction", 63);
           stat_type_def = -1;
           data_func = _boundary_fouling_coke_fraction;
           w_data_func = _boundary_fouling_weight;
           break;
         default:
-          {
-            const int attr_id = cs_lagr_stat_type_to_attr_id(stat_type);
-            if (attr_id > 0)
-              _attr_moment_name(attr_id,
-                                -1,       /* component_id */
-                                class,
-                                m_type,
-                                name);
-          }
+          snprintf(name, 63, "particle_event_%d", (int)stat_type);
           break;
         }
 
@@ -3963,7 +3955,7 @@ cs_lagr_stat_activate(int  stat_type)
 
   int level = 3;
 
-  if (stat_type < CS_LAGR_STAT_ATTR) {
+  if (stat_type < CS_LAGR_STAT_IMPACT_ANGLE) { /* TODO keep this updated */
     switch(stat_type) {
     case CS_LAGR_STAT_CUMULATIVE_WEIGHT:
     case CS_LAGR_STAT_E_CUMULATIVE_WEIGHT:
@@ -4223,10 +4215,8 @@ cs_lagr_stat_initialize(void)
 
           if (stat_type == CS_LAGR_STAT_VOLUME_FRACTION) {
 
-            _moment_name("particle_volume_fraction", -1, class, m_type, name);
-
             cs_lagr_stat_time_moment_define
-              (name,
+              ("particle_volume_fraction",
                CS_MESH_LOCATION_CELLS,
                stat_type,
                m_type,