code-saturne/code_saturne

commit 7b9d4d721ff8708e24cce714ac1d8dabfb28673e

Add EK model (sorption) and radiaoctive decay in GUI (prov. by V. Loizeau).
git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@10870 e44c5d27-f830-0410-8ce8-974f7a764be6

src/gui/cs_gui.c
@@ -100,6 +100,7 @@
 #include "cs_balance_by_zone.h"
 #include "cs_fan.h"
 #include "cs_volume_zone.h"
+#include "cs_gwf_physical_properties.h"
 
 /*----------------------------------------------------------------------------
  * Header for the current file
@@ -3661,42 +3662,6 @@ void CS_PROCF(uitssc, UITSSC)(const int                  *idarcy,
             tsexp[iel] *= cell_f_vol[iel];
           }
         }
-        else {  /* groundwater flow */
-          /* only radioactive decay can be set with the GUI source terms view */
-          /* TODO: handle it by zone in the ground water law view
-             as done for diffusivity */
-          ev_formula = mei_tree_new(formula);
-          mei_tree_insert(ev_formula,"x",0.);
-          mei_tree_insert(ev_formula,"y",0.);
-          mei_tree_insert(ev_formula,"z",0.);
-          mei_tree_insert(ev_formula,"t",0.);
-
-          /* add variable from notebook */
-          _add_notebook_variables(ev_formula);
-
-          /* try to build the interpreter */
-          if (mei_tree_builder(ev_formula))
-            bft_error(__FILE__, __LINE__, 0,
-                      _("Error: can not interpret expression: %s\n %i"),
-                      ev_formula->string, mei_tree_builder(ev_formula));
-
-          const char *symbols[] = {"lambda"};
-          if (mei_tree_find_symbols(ev_formula, 1, symbols))
-            bft_error(__FILE__, __LINE__, 0,
-                      _("Error: can not find the required symbol: %s\n"), "lambda");
-
-          for (cs_lnum_t icel = 0; icel < n_cells; icel++) {
-            cs_lnum_t iel = cell_ids[icel];
-            mei_tree_insert(ev_formula, "x", cell_cen[iel][0]);
-            mei_tree_insert(ev_formula, "y", cell_cen[iel][1]);
-            mei_tree_insert(ev_formula, "z", cell_cen[iel][2]);
-            mei_tree_insert(ev_formula, "t", cs_glob_time_step->t_cur);
-            mei_evaluate(ev_formula);
-            /* first order decay coefficient - lambda [kg.m3.s-1] positive */
-            cs_real_t lambda = mei_tree_lookup(ev_formula,"lambda");
-            tsimp[iel] = -cell_f_vol[iel]*lambda;
-          }
-        }
         mei_tree_destroy(ev_formula);
       }
     }
@@ -5329,12 +5294,13 @@ void CS_PROCF (uidapp, UIDAPP) (const int       *permeability,
       const int kivisl = cs_field_key_id("scalar_diffusivity_id");
       int n_fields = cs_field_n_fields();
 
-      /* get diffusivity and Kd for each scalar
-         defined by the user on current zone */
+      /* get diffusivity and Kd for each scalar defined by the user on current zone
+         (and kplus and kminus only for scalars with kinetic model) */
       for (int f_id = 0; f_id < n_fields; f_id++) {
         cs_field_t *f = cs_field_by_id(f_id);
         if (   (f->type & CS_FIELD_VARIABLE)
             && (f->type & CS_FIELD_USER)) {
+
           /* get kd for current scalar and current zone */
           char *kdname = NULL;
           int len = strlen(f->name) + 4;
@@ -5384,6 +5350,50 @@ void CS_PROCF (uidapp, UIDAPP) (const int       *permeability,
           for (cs_lnum_t icel = 0; icel < n_cells; icel++) {
             cs_lnum_t iel = cell_ids[icel];
             fdiff->val[iel] = saturation_field[iel]*diff_val;
+          }
+
+          /* get kplus and kminus for current scalar and current zone (if EK model is chosen) */
+          cs_gwf_soilwater_partition_t sorption_scal;
+          int key_part = cs_field_key_id("gwf_soilwater_partition");
+          cs_field_t *kp, *km;
+          cs_field_get_key_struct(f, key_part, &sorption_scal);
+          if (sorption_scal.kinetic == 1) {
+            kp = cs_field_by_id(sorption_scal.ikp);
+            km = cs_field_by_id(sorption_scal.ikm);
+
+            cs_real_t kp_val = 0.;
+            path = cs_xpath_init_path();
+            cs_xpath_add_elements(&path, 3,
+                                  "thermophysical_models",
+                                  "groundwater",
+                                  "groundwater_law");
+            _add_zone_id_test_attribute(&path, z->id);
+            cs_xpath_add_element(&path, "scalar");
+            cs_xpath_add_test_attribute(&path, "name", f->name);
+            cs_xpath_add_element(&path, "kplus");
+            cs_xpath_add_function_text(&path);
+            cs_gui_get_double(path, &kp_val);
+            BFT_FREE(path);
+
+            cs_real_t km_val = 0.;
+            path = cs_xpath_init_path();
+            cs_xpath_add_elements(&path, 3,
+                                  "thermophysical_models",
+                                  "groundwater",
+                                  "groundwater_law");
+            _add_zone_id_test_attribute(&path, z->id);
+            cs_xpath_add_element(&path, "scalar");
+            cs_xpath_add_test_attribute(&path, "name", f->name);
+            cs_xpath_add_element(&path, "kminus");
+            cs_xpath_add_function_text(&path);
+            cs_gui_get_double(path, &km_val);
+            BFT_FREE(path);
+
+            for (cs_lnum_t icel = 0; icel < n_cells; icel++) {
+              cs_lnum_t iel = cell_ids[icel];
+              kp->val[iel] = kp_val;
+              km->val[iel] = km_val;
+            }
           }
         }
       }

gui/Pages/GroundwaterForm.ui
@@ -6,9 +6,21 @@
    <rect>
     <x>0</x>
     <y>0</y>
-    <width>408</width>
-    <height>345</height>
+    <width>650</width>
+    <height>800</height>
    </rect>
+  </property>
+  <property name="minimumSize">
+   <size>
+    <width>600</width>
+    <height>800</height>
+   </size>
+  </property>
+  <property name="maximumSize">
+   <size>
+    <width>8000</width>
+    <height>5000</height>
+   </size>
   </property>
   <property name="windowTitle">
    <string>Form</string>
@@ -27,43 +39,35 @@
      </property>
     </spacer>
    </item>
-   <item row="0" column="2">
-    <spacer name="horizontalSpacer">
-     <property name="orientation">
-      <enum>Qt::Horizontal</enum>
-     </property>
-     <property name="sizeHint" stdset="0">
-      <size>
-       <width>5</width>
-       <height>20</height>
-      </size>
-     </property>
-    </spacer>
-   </item>
-   <item row="1" column="1">
-    <spacer>
-     <property name="orientation">
-      <enum>Qt::Vertical</enum>
-     </property>
-     <property name="sizeHint" stdset="0">
-      <size>
-       <width>16</width>
-       <height>16</height>
-      </size>
-     </property>
-    </spacer>
-   </item>
    <item row="0" column="1">
     <widget class="QGroupBox" name="groupBoxGroundwaterOptions">
+     <property name="minimumSize">
+      <size>
+       <width>400</width>
+       <height>600</height>
+      </size>
+     </property>
+     <property name="maximumSize">
+      <size>
+       <width>1500</width>
+       <height>1500</height>
+      </size>
+     </property>
      <property name="toolTip">
       <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Define model options for the groundwater flow module.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
      </property>
      <property name="title">
-      <string>Darcy model options</string>
+      <string>Groundwaterflow module options</string>
      </property>
      <layout class="QGridLayout" name="gridLayout_3">
       <item row="0" column="0">
        <widget class="QGroupBox" name="groupBox">
+        <property name="maximumSize">
+         <size>
+          <width>400</width>
+          <height>150</height>
+         </size>
+        </property>
         <property name="title">
          <string>Richards equation</string>
         </property>
@@ -136,8 +140,21 @@
            </property>
           </widget>
          </item>
-         <item row="2" column="1">
+         <item row="3" column="0">
+          <widget class="QCheckBox" name="checkBoxGravity">
+           <property name="toolTip">
+            <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Set gravity (used to compute hydraulic head from pressure head).&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+           </property>
+           <property name="text">
+            <string>Gravity is taken into account</string>
+           </property>
+          </widget>
+         </item>
+         <item row="2" column="1" rowspan="2">
           <widget class="QComboBox" name="comboBoxPermeability">
+           <property name="enabled">
+            <bool>true</bool>
+           </property>
            <property name="minimumSize">
             <size>
              <width>100</width>
@@ -155,21 +172,11 @@
            </property>
           </widget>
          </item>
-         <item row="3" column="0">
-          <widget class="QCheckBox" name="checkBoxGravity">
-           <property name="toolTip">
-            <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Set gravity (used to compute hydraulic head from pressure head).&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
-           </property>
-           <property name="text">
-            <string>Gravity is taken into account</string>
-           </property>
-          </widget>
-         </item>
         </layout>
        </widget>
       </item>
       <item row="1" column="0">
-       <widget class="QGroupBox" name="groupBox_2">
+       <widget class="QGroupBox" name="groupBoxTransport">
         <property name="title">
          <string>Transport equation</string>
         </property>
@@ -204,11 +211,82 @@
           </widget>
          </item>
          <item row="1" column="0" colspan="2">
-          <widget class="QLabel" name="label">
-           <property name="text">
-            <string>Remark: please note that the radioactive decay
-             is treated in the source term section</string>
+          <widget class="QGroupBox" name="groupBoxSpeciesProperties">
+           <property name="toolTip">
+            <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Select species to enter the radioactive decay and the chemistry model.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
            </property>
+           <property name="title">
+            <string>Species properties</string>
+           </property>
+           <widget class="QLabel" name="labelSpeciesName">
+            <property name="geometry">
+             <rect>
+              <x>20</x>
+              <y>30</y>
+              <width>59</width>
+              <height>14</height>
+             </rect>
+            </property>
+            <property name="text">
+             <string>Name</string>
+            </property>
+           </widget>
+           <widget class="QLabel" name="labelDecayRate">
+            <property name="geometry">
+             <rect>
+              <x>20</x>
+              <y>60</y>
+              <width>151</width>
+              <height>16</height>
+             </rect>
+            </property>
+            <property name="text">
+             <string>First-order decay rate</string>
+            </property>
+           </widget>
+           <widget class="QLabel" name="labelChemistryModel">
+            <property name="geometry">
+             <rect>
+              <x>20</x>
+              <y>90</y>
+              <width>111</width>
+              <height>16</height>
+             </rect>
+            </property>
+            <property name="text">
+             <string>Chemistry model</string>
+            </property>
+           </widget>
+           <widget class="QComboBox" name="comboBoxChemistryModel">
+            <property name="geometry">
+             <rect>
+              <x>250</x>
+              <y>90</y>
+              <width>61</width>
+              <height>22</height>
+             </rect>
+            </property>
+           </widget>
+           <widget class="QComboBox" name="comboBoxSpeciesName">
+            <property name="geometry">
+             <rect>
+              <x>250</x>
+              <y>30</y>
+              <width>91</width>
+              <height>22</height>
+             </rect>
+            </property>
+           </widget>
+           <widget class="QLineEdit" name="lineEditDecayRate">
+            <property name="geometry">
+             <rect>
+              <x>250</x>
+              <y>60</y>
+              <width>91</width>
+              <height>22</height>
+             </rect>
+            </property>
+           </widget>
           </widget>
          </item>
         </layout>
@@ -216,6 +294,32 @@
       </item>
      </layout>
     </widget>
+   </item>
+   <item row="1" column="1">
+    <spacer name="verticalSpacer">
+     <property name="orientation">
+      <enum>Qt::Vertical</enum>
+     </property>
+     <property name="sizeHint" stdset="0">
+      <size>
+       <width>16</width>
+       <height>16</height>
+      </size>
+     </property>
+    </spacer>
+   </item>
+   <item row="0" column="2">
+    <spacer name="horizontalSpacer">
+     <property name="orientation">
+      <enum>Qt::Horizontal</enum>
+     </property>
+     <property name="sizeHint" stdset="0">
+      <size>
+       <width>5</width>
+       <height>20</height>
+      </size>
+     </property>
+    </spacer>
    </item>
   </layout>
  </widget>

gui/Pages/GroundwaterLawForm.ui
@@ -293,6 +293,60 @@
                    </item>
                   </layout>
                  </item>
+                 <item>
+                  <layout class="QHBoxLayout" name="horizontalLayout_8">
+                   <item>
+                    <widget class="QLabel" name="label_kplus">
+                     <property name="text">
+                      <string>k_plus</string>
+                     </property>
+                    </widget>
+                   </item>
+                   <item>
+                    <spacer name="horizontalSpacer_48">
+                     <property name="orientation">
+                      <enum>Qt::Horizontal</enum>
+                     </property>
+                     <property name="sizeHint" stdset="0">
+                      <size>
+                       <width>40</width>
+                       <height>20</height>
+                      </size>
+                     </property>
+                    </spacer>
+                   </item>
+                   <item>
+                    <widget class="QLineEdit" name="lineEditkplus"/>
+                   </item>
+                  </layout>
+                 </item>
+                 <item>
+                  <layout class="QHBoxLayout" name="horizontalLayout_8">
+                   <item>
+                    <widget class="QLabel" name="label_kminus">
+                     <property name="text">
+                      <string>k_minus</string>
+                     </property>
+                    </widget>
+                   </item>
+                   <item>
+                    <spacer name="horizontalSpacer_47">
+                     <property name="orientation">
+                      <enum>Qt::Horizontal</enum>
+                     </property>
+                     <property name="sizeHint" stdset="0">
+                      <size>
+                       <width>40</width>
+                       <height>20</height>
+                      </size>
+                     </property>
+                    </spacer>
+                   </item>
+                   <item>
+                    <widget class="QLineEdit" name="lineEditkminus"/>
+                   </item>
+                  </layout>
+                 </item>
                 </layout>
                </item>
                <item row="0" column="2">

gui/Pages/GroundwaterLawModel.py
@@ -96,9 +96,11 @@ class GroundwaterLawModel(Variables, Model):
         default['transverse']            = 0.0
         default['isotropic']             = 0.0
         default['diffusion_choice']      = 'variable'
-        default['soil_density']          = 1.5
+        default['soil_density']          = 1.0
         default['diffusivity']           = 0.0
         default['kd']                    = 0.0
+        default['kplus']                 = 0.0
+        default['kminus']                = 0.0
         return default
 
 
@@ -323,7 +325,7 @@ permeability=1.;"""
         self.isInt(int(zoneid))
         self.isNotInList(scalar_name, self.sca_mo.getScalarsVarianceList())
         self.isInList(scalar_name, self.sca_mo.getUserScalarNameList())
-        self.isInList(prop, ['diffusivity','kd'])
+        self.isInList(prop, ['diffusivity','kd', 'kplus', 'kminus'])
 
         nodeZone = self.node_darcy.xmlGetNode('groundwater_law', zone_id=zoneid)
         nodeScalar = nodeZone.xmlInitChildNode('scalar', name=scalar_name)
@@ -343,7 +345,7 @@ permeability=1.;"""
         self.isInt(int(zoneid))
         self.isNotInList(scalar_name, self.sca_mo.getScalarsVarianceList())
         self.isInList(scalar_name, self.sca_mo.getUserScalarNameList())
-        self.isInList(prop, ['diffusivity','kd'])
+        self.isInList(prop, ['diffusivity','kd', 'kplus', 'kminus'])
 
         nodeZone = self.node_darcy.xmlGetNode('groundwater_law', zone_id=zoneid)
         nodeScalar = nodeZone.xmlInitChildNode('scalar', name=scalar_name)

gui/Pages/GroundwaterLawView.py
@@ -182,6 +182,8 @@ class GroundwaterLawView(QWidget, Ui_GroundwaterLawForm):
         self.lineEditSoilDensity.setValidator(DoubleValidator(self.lineEditSoilDensity))
         self.lineEditDiffusivity.setValidator(DoubleValidator(self.lineEditDiffusivity))
         self.lineEditKd.setValidator(DoubleValidator(self.lineEditKd))
+        self.lineEditkplus.setValidator(DoubleValidator(self.lineEditkplus))
+        self.lineEditkminus.setValidator(DoubleValidator(self.lineEditkminus))
 
         self.scalar = ""
         scalar_list = self.m_sca.getUserScalarNameList()
@@ -224,6 +226,8 @@ class GroundwaterLawView(QWidget, Ui_GroundwaterLawForm):
         self.comboBoxNameDiff.activated[str].connect(self.slotNameDiff)
         self.lineEditDiffusivity.textChanged[str].connect(self.slotDiffusivity)
         self.lineEditKd.textChanged[str].connect(self.slotKd)
+        self.lineEditkplus.textChanged[str].connect(self.slotkplus)
+        self.lineEditkminus.textChanged[str].connect(self.slotkminus)
 
         # Initialize Widgets
 
@@ -324,6 +328,21 @@ class GroundwaterLawView(QWidget, Ui_GroundwaterLawForm):
             self.lineEditDiffusivity.setText(str(value))
             value = self.mdl.getGroundWaterScalarPropertyByZone(scal, name, 'kd')
             self.lineEditKd.setText(str(value))
+            # chemistry model
+            if GroundwaterModel(self.case).getChemistryModel(scal) == "EK":
+                self.lineEditkplus.show()
+                self.label_kplus.show()
+                self.lineEditkminus.show()
+                self.label_kminus.show()
+                value = self.mdl.getGroundWaterScalarPropertyByZone(scal, name, 'kplus')
+                self.lineEditkplus.setText(str(value))
+                value = self.mdl.getGroundWaterScalarPropertyByZone(scal, name, 'kminus')
+                self.lineEditkminus.setText(str(value))
+            else:
+                self.lineEditkplus.hide()
+                self.label_kplus.hide()
+                self.lineEditkminus.hide()
+                self.label_kminus.hide()
 
         if GroundwaterModel(self.case).getDispersionType() == 'anisotropic':
             self.groupBoxIsotropicDispersion.hide()
@@ -720,6 +739,20 @@ class GroundwaterLawView(QWidget, Ui_GroundwaterLawForm):
         self.lineEditDiffusivity.setText(str(value))
         value = self.mdl.getGroundWaterScalarPropertyByZone(scal, name, 'kd')
         self.lineEditKd.setText(str(value))
+        if GroundwaterModel(self.case).getChemistryModel(scal) == "EK":
+            self.lineEditkplus.show()
+            self.label_kplus.show()
+            self.lineEditkminus.show()
+            self.label_kminus.show()
+            value = self.mdl.getGroundWaterScalarPropertyByZone(scal, name, 'kplus')
+            self.lineEditkplus.setText(str(value))
+            value = self.mdl.getGroundWaterScalarPropertyByZone(scal, name, 'kminus')
+            self.lineEditkminus.setText(str(value))
+        else:
+            self.lineEditkplus.hide()
+            self.label_kplus.hide()
+            self.lineEditkminus.hide()
+            self.label_kminus.hide()
 
     @pyqtSlot(str)
     def slotDiffusivity(self, text):
@@ -740,6 +773,26 @@ class GroundwaterLawView(QWidget, Ui_GroundwaterLawForm):
             val = float(text)
             scal = self.scalar
             self.mdl.setGroundWaterScalarPropertyByZone(scal, name, 'kd', val)
+
+    @pyqtSlot(str)
+    def slotkplus(self, text):
+        """
+        """
+        label, name, local = self.modelGroundwaterLaw.getItem(self.entriesNumber)
+        if self.lineEditkplus.validator().state == QValidator.Acceptable:
+            val = float(text)
+            scal = self.scalar
+            self.mdl.setGroundWaterScalarPropertyByZone(scal, name, 'kplus', val)
+
+    @pyqtSlot(str)
+    def slotkminus(self, text):
+        """
+        """
+        label, name, local = self.modelGroundwaterLaw.getItem(self.entriesNumber)
+        if self.lineEditkminus.validator().state == QValidator.Acceptable:
+            val = float(text)
+            scal = self.scalar
+            self.mdl.setGroundWaterScalarPropertyByZone(scal, name, 'kminus', val)
 
     def tr(self, text):
         """

gui/Pages/GroundwaterModel.py
@@ -45,6 +45,7 @@ from code_saturne.Base.XMLvariables               import Variables, Model
 from code_saturne.Base.XMLmodel                   import ModelTest
 from code_saturne.Pages.TurbulenceModel           import TurbulenceModel
 from code_saturne.Pages.FluidCharacteristicsModel import FluidCharacteristicsModel
+from code_saturne.Pages.DefineUserScalarsModel import DefineUserScalarsModel
 
 #-------------------------------------------------------------------------------
 # Mobil Mesh model class
@@ -63,6 +64,8 @@ class GroundwaterModel(Variables, Model):
         self.node_models  = self.case.xmlGetNode('thermophysical_models')
         self.node_darcy = self.node_models.xmlInitChildNode('groundwater_model', 'model')
 
+        self.sca_mo       = DefineUserScalarsModel(self.case)
+
 
     def __defaultValues(self):
         """
@@ -75,7 +78,8 @@ class GroundwaterModel(Variables, Model):
         default['groundwater_model'] = 'off'
         default['gravity']           = 'off'
         default['unsaturated']       = 'true'
-
+        default['chemistry_model']   = 'Kd'
+        default['fo_decay_rate']     = 0.0
         return default
 
 
@@ -295,3 +299,68 @@ class GroundwaterModel(Variables, Model):
         self.isOnOff(v)
         node = self.node_darcy.xmlInitChildNode('gravity', 'status')
         node['status'] = v
+
+
+    @Variables.noUndo
+    def getDecayRate(self, scalar_name):
+        """
+        Get value for the first-order decay rate of one scalar
+        """
+        prop = 'fo_decay_rate'
+
+        self.isNotInList(scalar_name, self.sca_mo.getScalarsVarianceList())
+        self.isInList(scalar_name, self.sca_mo.getUserScalarNameList())
+
+        nodeScalar = self.node_darcy.xmlInitChildNode('scalar', name=scalar_name)
+        value = nodeScalar.xmlGetDouble(prop)
+
+        if value == None:
+            value = self.__defaultValues()[prop]
+            self.setDecayRate(scalar_name, value)
+
+        return value
+
+
+    @Variables.undoLocal
+    def setDecayRate(self, scalar_name, value):
+        """
+        Set value for the first-order decay rate of one scalar
+        """
+        self.isNotInList(scalar_name, self.sca_mo.getScalarsVarianceList())
+        self.isInList(scalar_name, self.sca_mo.getUserScalarNameList())
+
+        nodeScalar = self.node_darcy.xmlInitChildNode('scalar', name=scalar_name)
+        nodeScalar.xmlSetData('fo_decay_rate', value)
+
+
+    @Variables.noUndo
+    def getChemistryModel(self, scalar_name):
+        """
+        Get choice of the chemistry model (representing soil-water partition) of one scalar
+        """
+        prop = 'chemistry_model'
+
+        self.isNotInList(scalar_name, self.sca_mo.getScalarsVarianceList())
+        self.isInList(scalar_name, self.sca_mo.getUserScalarNameList())
+
+        nodeScalar = self.node_darcy.xmlInitChildNode('scalar', name=scalar_name)
+        choice = nodeScalar['chemistry_model']
+
+        if choice == None:
+            choice = self.__defaultValues()[prop]
+            self.setChemistryModel(scalar_name, choice)
+
+        return choice
+
+
+    @Variables.undoLocal
+    def setChemistryModel(self, scalar_name, choice):
+        """
+        Set choice of the chemistry model of one scalar: Kd or EK
+        """
+        self.isNotInList(scalar_name, self.sca_mo.getScalarsVarianceList())
+        self.isInList(scalar_name, self.sca_mo.getUserScalarNameList())
+        self.isInList(choice, ['Kd', 'EK'])
+
+        nodeScalar = self.node_darcy.xmlInitChildNode('scalar', name=scalar_name)
+        nodeScalar['chemistry_model'] = choice

commit bafe368d185ef22ef4346a14686914109f3af228

Fix bug in the GUI concerning scalar source terms maybe introduced by commit 7b9d4d7 (2018-01-09).
This may broke GWF scalar source terms.

src/gui/cs_gui.c
@@ -2735,7 +2735,7 @@ void CS_PROCF(uitssc, UITSSC)(const int                  *idarcy,
       formula = cs_tree_node_get_value_str(tn);
 
       if (formula != NULL) {
-        if (*idarcy == 0) {
+        if (*idarcy == -1) {
           cs_real_t *st_vals = cs_meg_source_terms(z,
                                                    f->name,
                                                    "scalar_source_term");