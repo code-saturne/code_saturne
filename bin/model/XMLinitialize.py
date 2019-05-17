code-saturne/code_saturne

commit 30ef572b3b7c0be6e5fddf1e8b1423a3795c9dd8

MEG major change for volume functions:
  * Function now takes as input an array of cs_field_t instead of a
    single cs_field_t.
  * Block is used only if all fields' names correspond!
  * Tensorial porosity name is updated from porosity to
    tensorial_porosity in the GUI and formulae. Backward compatibility
    function is implemented to update the formulae.
  * MEG is deployed for Groundwater Laws and Anisotropic porosity

bin/model/XMLinitialize.py
@@ -1687,6 +1687,22 @@ class XMLinit(BaseXmlInit):
                 else:
                     nodep['choice'] = 'predefined_law'
 
+        # For porosity, rename porosity[XX] as tensorial_porosity[XX], ..
+        porosity_rename = {'porosity[XX]':'tensorial_porosity[XX]',
+                           'porosity[YY]':'tensorial_porosity[YY]',
+                           'porosity[ZZ]':'tensorial_porosity[ZZ]',
+                           'porosity[XY]':'tensorial_porosity[XY]',
+                           'porosity[XZ]':'tensorial_porosity[XZ]',
+                           'porosity[YZ]':'tensorial_porosity[YZ]'}
+        node_poro = XMLThermoPhysicalNode.xmlGetNode('porosities')
+        for n in node_poro.xmlGetNodeList('porosity'):
+            nf = n.xmlGetNode('formula')
+            if nf != None:
+                f = n.xmlGetString('formula')
+                for k in porosity_rename.keys():
+                    f = f.replace(k, porosity_rename[k])
+                nf.xmlSetTextNode(f)
+
 
 #-------------------------------------------------------------------------------
 # End of XMLinit

bin/cs_mei_to_c.py
@@ -87,7 +87,7 @@ END_C_DECLS
 
 _function_header = { \
 'vol':"""void
-cs_meg_volume_function(cs_field_t       *f,
+cs_meg_volume_function(cs_field_t       *f[],
                        const cs_zone_t  *vz)
 {
 """,
@@ -121,7 +121,7 @@ _function_names = {'vol':'cs_meg_volume_function.c',
                    'src':'cs_meg_source_terms.c',
                    'ini':'cs_meg_initialization.c'}
 
-_block_comments = {'vol':'User defined formula for variable %s over zone %s',
+_block_comments = {'vol':'User defined formula for variable(s) %s over zone %s',
                    'bnd':'User defined formula for "%s" over BC=%s',
                    'src':'User defined source term for %s over zone %s',
                    'ini':'User defined initialization for variable %s over zone %s'}
@@ -155,6 +155,75 @@ _pkg_glob_struct = {'code_saturne':'cs_glob_fluid_properties',
 #===============================================================================
 # Utility functions
 #===============================================================================
+
+#---------------------------------------------------------------------------
+def create_req_field(name, dim=0):
+
+    r = {'name':name,
+         'dim':dim,
+         'components':[]}
+
+    return r
+
+def rfield_add_comp(rf, c):
+
+    rf['components'].append(c)
+    rf['dim'] += 1
+
+def split_req_components(req_list):
+    """
+    Look at a list of field names used in the formula.
+    Check if its a component (f[X], f[XY], ..) or a field (f).
+    return a list with it
+    """
+    req_fields = []
+    for r in req_list:
+
+        rf = r
+        if bool(re.search('\[[A-Za-z0-9]\]', r)):
+            rf = re.sub('\[[A-Za-z0-9]\]', '', r)
+        elif bool(re.search('\[[A-Za-z0-9][A-Za-z0-9]\]', r)):
+            rf = re.sub('\[[A-Za-z0-9][A-Za-z0-9]\]', '', r)
+
+        if rf == r:
+            req_fields.append(create_req_field(r,1))
+
+        else:
+            new_field = True
+            for f in req_fields:
+                if f['name'] == rf:
+                    rfield_add_comp(f,r)
+                    new_field = False
+                    break
+
+            if new_field:
+                req_fields.append(create_req_field(rf))
+                rfield_add_comp(req_fields[-1], r)
+
+    return req_fields
+
+def get_req_field_info(req_fields, r):
+
+    for i in range(len(req_fields)):
+        if req_fields[i]['dim'] == 1:
+            if req_fields[i]['name'] == r:
+                return i, -1, req_fields[i]['dim']
+
+        else:
+            if r in req_fields[i]['components']:
+                return i, req_fields[i]['components'].index(r), req_fields[i]['dim']
+
+    return None, None, None
+
+def dump_req_fields(req_fields):
+
+    print("===========================")
+    for f in req_fields:
+        print("Name: %s" % (f['name']))
+        print("Dim: %d" % (f['dim']))
+        print(f['components'])
+        print("===========================")
+
 
 #---------------------------------------------------------------------------
 
@@ -841,6 +910,10 @@ def parse_gui_expression(expression,
 
     if_open = 0
 
+    if func_type == 'vol':
+        req_fields = split_req_components(req)
+#        dump_req_fields(req_fields)
+
     # ------------------------------------
     # Parse the Mathematical expression and generate the C block code
 
@@ -910,7 +983,14 @@ def parse_gui_expression(expression,
 
                 elif lf[0].strip() in req:
                     if func_type == 'vol':
-                        new_v = 'f->val[c_id]'
+                        fid, fcomp, fdim = get_req_field_info(req_fields, lf[0].strip())
+                        if fid == None:
+                            raise Exception("Uknown field: %s" %(lf[0].strip()))
+
+                        if fcomp < 0:
+                            new_v = 'f[%d]->val[c_id]' % (fid)
+                        else:
+                            new_v = 'f[%d]->val[c_id*%d + %d]' % (fid, fdim, fcomp)
 
                     elif func_type == 'bnd':
                         ir = req.index(lf[0].strip())
@@ -1083,9 +1163,6 @@ class mei_to_c_interpreter:
         else:
             required = func_params['req']
 
-        if len(required) > 1:
-            raise Exception("Received more than one argument in write_cell_block function")
-
         zone, name = func_key.split('::')
         exp_lines_comp = func_params['lines']
 
@@ -1204,8 +1281,12 @@ class mei_to_c_interpreter:
             usr_defs += parsed_exp[1]
 
         # Write the block
-        usr_blck = tab + 'if (strcmp(f->name, "%s") == 0 && strcmp(vz->name, "%s") == 0) {\n' \
-                % (name, zone)
+        nsplit = name.split('+')
+        usr_blck = tab + 'if (strcmp(f[0]->name, "%s") == 0 &&\n' % (nsplit[0])
+        for i in range(1,len(nsplit)):
+            usr_blck += tab + '    strcmp(f[%d]->name, "%s") == 0 &&\n' % (i, nsplit[i])
+
+        usr_blck += tab + '    strcmp(vz->name, "%s") == 0) {\n' % (zone)
 
         usr_blck += usr_defs + '\n'
 
@@ -1639,6 +1720,7 @@ class mei_to_c_interpreter:
     def generate_volume_code(self):
 
         from code_saturne.model.LocalizationModel import LocalizationModel
+        from code_saturne.model.GroundwaterLawModel import GroundwaterLawModel
         if self.pkg_name == 'code_saturne':
             from code_saturne.model.FluidCharacteristicsModel \
                 import FluidCharacteristicsModel
@@ -1662,6 +1744,24 @@ class mei_to_c_interpreter:
                         fcm.getFormulaComponents('scalar_diffusivity', s)
                         self.init_block('vol', 'all_cells', dname,
                                         exp, req, sym, sca)
+
+            # GroundWater Flows Law
+            vlm = LocalizationModel('VolumicZone', self.case)
+            glm = GroundwaterLawModel(self.case)
+
+            for zone in vlm.getZones():
+                z_id = str(zone.getCodeNumber())
+                zone_name = zone.getLabel()
+                nature_list = zone.getNatureList()
+
+                if "groundwater_law" in nature_list:
+                    if zone.getNature()['groundwater_law'] == 'on':
+                        if glm.getGroundwaterLawModel(z_id) == 'user':
+                            exp, req, sym = glm.getGroundwaterLawFormulaComponents(z_id)
+                            self.init_block('vol', zone_name,
+                                            'capacity+saturation+permeability',
+                                            exp, req, sym, [])
+
 
         else:
             from code_saturne.model.ThermodynamicsModel import ThermodynamicsModel
@@ -1734,10 +1834,13 @@ class mei_to_c_interpreter:
                 nature_list = zone.getNatureList()
                 if 'porosity' in nature_list:
                     if zone.getNature()['porosity'] == 'on':
+                        fname = 'porosity'
+                        if prm.getPorosityModel(z_id) == 'anisotropic':
+                            fname += '+tensorial_porosity'
                         exp, req, known_fields, sym = \
                         prm.getPorosityFormulaComponents(z_id)
 
-                        self.init_block('vol', zone_name, 'porosity',
+                        self.init_block('vol', zone_name, fname,
                                         exp, req, sym, known_fields)
 
 
@@ -2359,6 +2462,7 @@ class mei_to_c_interpreter:
             code_to_write += _function_header[func_type]
             for key in self.funcs[func_type].keys():
                 zone_name, var_name = key.split('::')
+                var_name = var_name.replace("+", ", ")
                 m1 = _block_comments[func_type] % (var_name, zone_name)
                 m2 = '/*-' + '-'*len(m1) + '-*/\n'
                 m1 = '/* ' + m1 + ' */\n'

bin/model/GroundwaterLawModel.py
@@ -46,6 +46,7 @@ from code_saturne.model.XMLmodel import ModelTest
 from code_saturne.model.LocalizationModel import LocalizationModel, VolumicLocalizationModel, Zone
 from code_saturne.model.GroundwaterModel import GroundwaterModel
 from code_saturne.model.DefineUserScalarsModel import DefineUserScalarsModel
+from code_saturne.model.NotebookModel import NotebookModel
 
 #-------------------------------------------------------------------------------
 # GroundwaterLaw model class
@@ -314,6 +315,42 @@ saturation = 1.;
 permeability=1.;"""
 
         return formula
+
+
+    @Variables.noUndo
+    def getGroundwaterLawFormulaComponents(self, zoneid):
+        """
+        Public method.
+        Return formula, required symbols and available symbols.
+        """
+
+        exp = self.getGroundwaterLawFormula(zoneid)
+        if exp == None:
+            exp = self.getDefaultGroundwaterLawFormula()
+
+        if GroundwaterModel(self.case).getPermeabilityType() == 'anisotropic':
+            req = [('capacity',     'Capacity'),
+                   ('saturation',   'Saturation'),
+                   ('permeability[XX]', 'Permeability'),
+                   ('permeability[YY]', 'Permeability'),
+                   ('permeability[ZZ]', 'Permeability'),
+                   ('permeability[XY]', 'Permeability'),
+                   ('permeability[XZ]', 'Permeability'),
+                   ('permeability[YZ]', 'Permeability')]
+        else:
+            req = [('capacity',     'Capacity'),
+                   ('saturation',   'Saturation'),
+                   ('permeability', 'Permeability')]
+
+        sym = [('x', 'cell center coordinate'),
+               ('y', 'cell center coordinate'),
+               ('z', 'cell center coordinate')]
+
+        for (nme, val) in NotebookModel(self.case).getNotebookList():
+            sym.append((nme, 'value (notebook) = ' + str(val)))
+
+
+        return exp, req, sym
 
 
     @Variables.noUndo

bin/model/PorosityModel.py
@@ -180,12 +180,12 @@ class PorosityModel(Variables, Model):
             formula = """porosity=1.;"""
         else:
             formula = """porosity=1.;
-porosity[XX]=1.;
-porosity[YY]=1.;
-porosity[ZZ]=1.;
-porosity[XY]=0.;
-porosity[XZ]=0.;
-porosity[YZ]=0.;"""
+tensorial_porosity[XX]=1.;
+tensorial_porosity[YY]=1.;
+tensorial_porosity[ZZ]=1.;
+tensorial_porosity[XY]=0.;
+tensorial_porosity[XZ]=0.;
+tensorial_porosity[YZ]=0.;"""
 
         return formula
 
@@ -199,12 +199,12 @@ porosity[YZ]=0.;"""
             req = [('porosity', 'Porosity')]
         else:
             req = [('porosity', 'Porosity'),
-                   ('porosity[XX]', 'Porosity'),
-                   ('porosity[YY]', 'Porosity'),
-                   ('porosity[ZZ]', 'Porosity'),
-                   ('porosity[XY]', 'Porosity'),
-                   ('porosity[XZ]', 'Porosity'),
-                   ('porosity[YZ]', 'Porosity')]
+                   ('tensorial_porosity[XX]', 'Tensor Porosity'),
+                   ('tensorial_porosity[YY]', 'Tensor Porosity'),
+                   ('tensorial_porosity[ZZ]', 'Tensor Porosity'),
+                   ('tensorial_porosity[XY]', 'Tensor Porosity'),
+                   ('tensorial_porosity[XZ]', 'Tensor Porosity'),
+                   ('tensorial_porosity[YZ]', 'Tensor Porosity')]
 
         sym = [('x', 'cell center coordinate'),
                ('y', 'cell center coordinate'),

gui/Pages/GroundwaterLawView.py
@@ -52,7 +52,7 @@ from code_saturne.Base.QtPage import ComboModel, DoubleValidator
 from code_saturne.Base.QtPage import from_qvariant, to_text_string
 from code_saturne.Pages.GroundwaterLawForm import Ui_GroundwaterLawForm
 from code_saturne.model.LocalizationModel import LocalizationModel, Zone
-from code_saturne.Pages.QMeiEditorView import QMeiEditorView
+from code_saturne.Pages.QMegEditorView import QMegEditorView
 from code_saturne.model.GroundwaterLawModel import GroundwaterLawModel
 from code_saturne.model.GroundwaterModel import GroundwaterModel
 from code_saturne.model.DefineUserScalarsModel import DefineUserScalarsModel
@@ -685,40 +685,19 @@ class GroundwaterLawView(QWidget, Ui_GroundwaterLawForm):
         """
         label, name, local = self.modelGroundwaterLaw.getItem(self.entriesNumber)
 
-        exp = self.mdl.getGroundwaterLawFormula(name)
-
-        if exp == None:
-            exp = self.getDefaultGroundwaterLawFormula(choice)
-
-        if GroundwaterModel(self.case).getPermeabilityType() == 'anisotropic':
-            req = [('capacity',     'Capacity'),
-                   ('saturation',   'Saturation'),
-                   ('permeability[XX]', 'Permeability'),
-                   ('permeability[YY]', 'Permeability'),
-                   ('permeability[ZZ]', 'Permeability'),
-                   ('permeability[XY]', 'Permeability'),
-                   ('permeability[XZ]', 'Permeability'),
-                   ('permeability[YZ]', 'Permeability')]
-        else:
-            req = [('capacity',     'Capacity'),
-                   ('saturation',   'Saturation'),
-                   ('permeability', 'Permeability')]
+        exp, req, sym = self.mdl.getGroundwaterLawFormulaComponents(name)
 
         exa = """#example: \n""" + self.mdl.getDefaultGroundwaterLawFormula()
 
-        sym = [('x', 'cell center coordinate'),
-               ('y', 'cell center coordinate'),
-               ('z', 'cell center coordinate')]
+        dialog = QMegEditorView(parent        = self,
+                                function_type = 'vol',
+                                zone_name     = label,
+                                variable_name = 'capacity+saturation+permeability',
+                                expression    = exp,
+                                required      = req,
+                                symbols       = sym,
+                                examples      = exa)
 
-        for (nme, val) in self.notebook.getNotebookList():
-            sym.append((nme, 'value (notebook) = ' + str(val)))
-
-        dialog = QMeiEditorView(self,
-                                check_syntax = self.case['package'].get_check_syntax(),
-                                expression = exp,
-                                required   = req,
-                                symbols    = sym,
-                                examples   = exa)
         if dialog.exec_():
             result = dialog.get_result()
             log.debug("slotFormula -> %s" % str(result))

gui/Pages/PorosityView.py
@@ -219,6 +219,9 @@ class PorosityView(QWidget, Ui_PorosityForm):
         label, name, local = self.modelPorosity.getItem(self.entriesNumber)
 
         choice = self.mdl.getPorosityModel(name)
+        fname = 'porosity'
+        if choice == 'anisotropic':
+            fname += '+tensorial_porosity'
 
         exp, req, sca, sym = self.mdl.getPorosityFormulaComponents(name)
 
@@ -227,12 +230,16 @@ class PorosityView(QWidget, Ui_PorosityForm):
 
         exa = """#example: \n""" + self.mdl.getDefaultPorosityFormula(choice)
 
-        dialog = QMeiEditorView(self,
-                                check_syntax = self.case['package'].get_check_syntax(),
-                                expression = exp,
-                                required   = req,
-                                symbols    = sym,
-                                examples   = exa)
+        dialog = QMegEditorView(parent        = self,
+                                function_type = 'vol',
+                                zone_name     = label,
+                                variable_name = fname,
+                                expression    = exp,
+                                required      = req,
+                                symbols       = sym,
+                                known_fields  = sca,
+                                examples      = exa)
+
         if dialog.exec_():
             result = dialog.get_result()
             log.debug("slotFormulaPorosity -> %s" % str(result))

src/base/cs_prototypes.h
@@ -795,14 +795,14 @@ cs_meg_boundary_function(const char       *field_name,
  * \brief This function is used to compute user defined values for fields over a
  *        given volume zone
  *
- * \param[in, out]  f   pointer to cs_field_t
- * \param[in]       vz  pointer to cs_zone_t structure related to a volume
+ * \param[in, out]  f[]         array of pointers to cs_field_t
+ * \param[in]       vz          pointer to cs_zone_t structure related to a volume
  */
 /*----------------------------------------------------------------------------*/
 
 void
-cs_meg_volume_function(cs_field_t         *f,
-                       const cs_zone_t    *vz);
+cs_meg_volume_function(cs_field_t       *f[],
+                       const cs_zone_t  *vz);
 
 /*----------------------------------------------------------------------------*/
 /*!

src/gui/cs_gui.c
@@ -281,8 +281,10 @@ _physical_property(cs_field_t          *c_prop,
     tn = cs_tree_get_node(tn, "formula");
     law = cs_tree_node_get_value_str(tn);
 
-    if (law != NULL)
-      cs_meg_volume_function(c_prop, z);
+    if (law != NULL) {
+      cs_field_t *fmeg[1] = {c_prop};
+      cs_meg_volume_function(fmeg, z);
+    }
 
   }
   else if (cs_gui_strcmp(prop_choice, "thermal_law")) {
@@ -2507,60 +2509,15 @@ void CS_PROCF(uiporo, UIPORO)(void)
       const char *formula = cs_tree_node_get_child_value_str(tn_zp, "formula");
 
       if (formula != NULL) {
-        ev_formula = mei_tree_new(formula);
-        mei_tree_insert(ev_formula,"x",0.0);
-        mei_tree_insert(ev_formula,"y",0.0);
-        mei_tree_insert(ev_formula,"z",0.0);
-
-        /* add variable from notebook */
-        cs_gui_add_notebook_variables(ev_formula);
-
-        /* try to build the interpreter */
-        if (mei_tree_builder(ev_formula))
-          bft_error(__FILE__, __LINE__, 0,
-                    _("Error: can not interpret expression: %s\n %i"),
-                    ev_formula->string, mei_tree_builder(ev_formula));
-
         if (cs_gui_strcmp(mdl, "anisotropic")) {
-          const char *symbols[] = {"porosity",
-                                   "porosity[XX]",
-                                   "porosity[YY]",
-                                   "porosity[ZZ]",
-                                   "porosity[XY]",
-                                   "porosity[YZ]",
-                                   "porosity[XZ]"};
-          if (mei_tree_find_symbols(ev_formula, 7, symbols))
-            bft_error(__FILE__, __LINE__, 0,
-                      _("Error: can not find the required symbol: %s\n %s\n"),
-                      "porosity, porosity[XX], porosity[YY], porosity[ZZ]",
-                      "          porosity[XY], porosity[XZ] or porosity[YZ]");
-        }
-        else {
-          const char *symbols[] = {"porosity"};
-          if (mei_tree_find_symbols(ev_formula, 1, symbols))
-            bft_error(__FILE__, __LINE__, 0,
-                      _("Error: can not find the required symbol: %s\n"),
-                      "porosity");
+          cs_field_t *fmeg[2] = {fporo, ftporo};
+          cs_meg_volume_function(fmeg, z);
+
+        } else {
+          cs_field_t *fmeg[1] = {fporo};
+          cs_meg_volume_function(fmeg, z);
         }
 
-        for (cs_lnum_t i = 0; i < n_cells; i++) {
-          cs_lnum_t iel = cell_ids[i];
-          mei_tree_insert(ev_formula, "x", cell_cen[iel][0]);
-          mei_tree_insert(ev_formula, "y", cell_cen[iel][1]);
-          mei_tree_insert(ev_formula, "z", cell_cen[iel][2]);
-          mei_evaluate(ev_formula);
-          porosi[iel] = mei_tree_lookup(ev_formula,"porosity");
-          if (cs_gui_strcmp(mdl, "anisotropic")) {
-            porosf[iel][0] = mei_tree_lookup(ev_formula, "porosity[XX]");
-            porosf[iel][1] = mei_tree_lookup(ev_formula, "porosity[YY]");
-            porosf[iel][2] = mei_tree_lookup(ev_formula, "porosity[ZZ]");
-            porosf[iel][3] = mei_tree_lookup(ev_formula, "porosity[XY]");
-            porosf[iel][4] = mei_tree_lookup(ev_formula, "porosity[YZ]");
-            porosf[iel][5] = mei_tree_lookup(ev_formula, "porosity[XZ]");
-          }
-        }
-
-        mei_tree_destroy(ev_formula);
       }
     }
   }
@@ -3657,85 +3614,15 @@ void CS_PROCF (uidapp, UIDAPP) (const int       *permeability,
           }
         }
 
-      }
+      } else {
       /* user law for permeability */
-      else {
-
         const char *formula
           = cs_tree_node_get_child_value_str(tn_zl, "formula");
 
         if (formula != NULL) {
-          ev_formula = mei_tree_new(formula);
-          mei_tree_insert(ev_formula,"x",0.0);
-          mei_tree_insert(ev_formula,"y",0.0);
-          mei_tree_insert(ev_formula,"z",0.0);
-
-          /* add variable from notebook */
-          cs_gui_add_notebook_variables(ev_formula);
-
-          /* try to build the interpreter */
-          if (mei_tree_builder(ev_formula))
-            bft_error(__FILE__, __LINE__, 0,
-                      _("Error: can not interpret expression: %s\n %i"),
-                      ev_formula->string, mei_tree_builder(ev_formula));
-
-          if (*permeability == 0) {
-            const char *symbols[] = {"capacity",
-                                     "saturation"
-                                     "permeability"};
-            if (mei_tree_find_symbols(ev_formula, 3, symbols))
-                bft_error(__FILE__, __LINE__, 0,
-                          _("Error: can not find the required symbol: %s\n"),
-                          "capacity, saturation or permeability");
-          }
-          else {
-            const char *symbols[] = {"capacity",
-                                     "saturation",
-                                     "permeability[XX]",
-                                     "permeability[YY]",
-                                     "permeability[ZZ]",
-                                     "permeability[XY]",
-                                     "permeability[XZ]",
-                                     "permeability[YZ]"};
-            if (mei_tree_find_symbols(ev_formula, 8, symbols))
-              bft_error
-                (__FILE__, __LINE__, 0,
-                 _("Error: can not find the required symbol: %s\n %s\n %s\n"),
-                 "capacity, saturation,",
-                 "          permeability[XX], permeability[YY], permeability[ZZ]",
-                 "          permeability[XY], permeability[YZ] or permeability[XZ]");
-          }
-
-          for (cs_lnum_t icel = 0; icel < n_cells; icel++) {
-            cs_lnum_t iel = cell_ids[icel];
-            mei_tree_insert(ev_formula, "x", cell_cen[iel][0]);
-            mei_tree_insert(ev_formula, "y", cell_cen[iel][1]);
-            mei_tree_insert(ev_formula, "z", cell_cen[iel][2]);
-            mei_evaluate(ev_formula);
-
-            capacity_field[iel] = mei_tree_lookup(ev_formula,"capacity");
-            saturation_field[iel] = mei_tree_lookup(ev_formula,"saturation");
-            if (*permeability == 1) {
-              permeability_field_v[iel][0] = mei_tree_lookup(ev_formula,
-                                                             "permeability[XX]");
-              permeability_field_v[iel][1] = mei_tree_lookup(ev_formula,
-                                                             "permeability[YY]");
-              permeability_field_v[iel][2] = mei_tree_lookup(ev_formula,
-                                                             "permeability[ZZ]");
-              permeability_field_v[iel][3] = mei_tree_lookup(ev_formula,
-                                                             "permeability[XY]");
-              permeability_field_v[iel][4] = mei_tree_lookup(ev_formula,
-                                                             "permeability[YZ]");
-              permeability_field_v[iel][5] = mei_tree_lookup(ev_formula,
-                                                             "permeability[XZ]");
-            }
-            else
-              permeability_field[iel] = mei_tree_lookup(ev_formula,
-                                                        "permeability");
-          }
-          mei_tree_destroy(ev_formula);
+          cs_field_t *fmeg[3] = {fcapacity, fsaturation, fpermeability};
+          cs_meg_volume_function(fmeg, z);
         }
-
       }
 
       const int kivisl = cs_field_key_id("diffusivity_id");

commit c5df67e6de9768a4fc0a601e76ef5744bb8ac450

GUI and scripts: fix crash introduced by commit 30ef572b (1 day ago).

bin/model/XMLinitialize.py
@@ -1687,21 +1687,22 @@ class XMLinit(BaseXmlInit):
                 else:
                     nodep['choice'] = 'predefined_law'
 
-        # For porosity, rename porosity[XX] as tensorial_porosity[XX], ..
-        porosity_rename = {'porosity[XX]':'tensorial_porosity[XX]',
-                           'porosity[YY]':'tensorial_porosity[YY]',
-                           'porosity[ZZ]':'tensorial_porosity[ZZ]',
-                           'porosity[XY]':'tensorial_porosity[XY]',
-                           'porosity[XZ]':'tensorial_porosity[XZ]',
-                           'porosity[YZ]':'tensorial_porosity[YZ]'}
         node_poro = XMLThermoPhysicalNode.xmlGetNode('porosities')
-        for n in node_poro.xmlGetNodeList('porosity'):
-            nf = n.xmlGetNode('formula')
-            if nf != None:
-                f = n.xmlGetString('formula')
-                for k in porosity_rename.keys():
-                    f = f.replace(k, porosity_rename[k])
-                nf.xmlSetTextNode(f)
+        if node_poro:
+            # For porosity, rename porosity[XX] as tensorial_porosity[XX], ..
+            porosity_rename = {'porosity[XX]':'tensorial_porosity[XX]',
+                               'porosity[YY]':'tensorial_porosity[YY]',
+                               'porosity[ZZ]':'tensorial_porosity[ZZ]',
+                               'porosity[XY]':'tensorial_porosity[XY]',
+                               'porosity[XZ]':'tensorial_porosity[XZ]',
+                               'porosity[YZ]':'tensorial_porosity[YZ]'}
+            for n in node_poro.xmlGetNodeList('porosity'):
+                nf = n.xmlGetNode('formula')
+                if nf != None:
+                    f = n.xmlGetString('formula')
+                    for k in porosity_rename.keys():
+                        f = f.replace(k, porosity_rename[k])
+                        nf.xmlSetTextNode(f)
 
 
 #-------------------------------------------------------------------------------