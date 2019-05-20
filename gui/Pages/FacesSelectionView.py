7ec4a3eeedaf9a73346249c055ca19d2506dd8b7
Chaï Koren <chai.koren@noreply.edf.fr>
2019-05-20 10:01:37 +0200

    Add selection criteria autocompletion where it was missing
    (cherry picked from commit f7d17d0fb71a89e1fbf8841367fcd0ee20750472)

--- gui/Pages/FacesSelectionView.py
+++ gui/Pages/FacesSelectionView.py
@@ -105,10 +105,33 @@ class LineEditDelegateSelector(QItemDelegate):
         editor = QLineEdit(parent)
         validator =  RegExpValidator(editor, QRegExp("[ -~]*"))
         editor.setValidator(validator)
+
+        # Autocompletion for selection criteria!
+        comp_list = ['all[]']
+        comp_list += ['plane[a, b, c, d, epsilon]',
+                      'plane[a, b, c, d, inside]',
+                      'plane[a, b, c, d, outside]',
+                      'plane[n_x, n_y, n_z, x0, y0, z0, epsilon]',
+                      'plane[n_x, n_y, n_z, x0, y0, z0, inside]',
+                      'plane[n_x, n_y, n_z, x0, y0, z0, outside]',
+                      'box[xmin, ymin, zmin, xmax, ymax, zmax]',
+                      'box[x0, y0, z0, dx1, dy1, dz1, dx2, dy2, dz2, dx3, dy3, dz3]',
+                      'cylinder[x0, y0, z0, x1, y1, z1, radius]',
+                      'sphere[x_c, y_c, z_c, radius]']
+
+        completer = QCompleter()
+        editor.setCompleter(completer)
+        model = QStringListModel()
+        completer.setModel(model)
+        model.setStringList(comp_list)
+
         return editor
 
 
     def setEditorData(self, lineEdit, index):
+        # This line is used to avoid an overlay of old and new text
+        editor.setAutoFillBackground(True)
+
         value = from_qvariant(index.model().data(index, Qt.DisplayRole), to_text_string)
         lineEdit.setText(value)
 
