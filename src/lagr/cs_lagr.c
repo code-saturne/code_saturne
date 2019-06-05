code-saturne/code_saturne

commit 9f0092b7d9a063b3ec0632a12f983b9d88e8703b

Lagrangian module: progess in style and log consistency cleanup.

src/lagr/cs_lagr.c
@@ -190,8 +190,8 @@ static cs_lagr_particle_counter_t _lagr_particle_counter
      .n_g_cumulative_failed = 0,
      .n_g_total = 0,
      .n_g_new = 0,
-     .n_g_merged = 0,
      .n_g_exit = 0,
+     .n_g_merged = 0,
      .n_g_deposited = 0,
      .n_g_fouling = 0,
      .n_g_resuspended = 0,
@@ -199,6 +199,7 @@ static cs_lagr_particle_counter_t _lagr_particle_counter
      .w_total = 0.,
      .w_new = 0.,
      .w_exit = 0.,
+     .w_merged = 0.,
      .w_deposited = 0.,
      .w_fouling = 0.,
      .w_resuspended = 0.};
@@ -1282,6 +1283,7 @@ cs_lagr_update_particle_counter(void)
 
   cs_real_t wsum[6] = {p_set->weight,
                        p_set->weight_new,
+                       p_set->weight_merged,
                        p_set->weight_out,
                        p_set->weight_dep,
                        p_set->weight_fou,
@@ -1290,14 +1292,14 @@ cs_lagr_update_particle_counter(void)
   cs_lnum_t size_count = sizeof(gcount) / sizeof(gcount[0]);
 
   cs_parall_counter(gcount, size_count);
-  cs_parall_sum(6, CS_REAL_TYPE, wsum);
+  cs_parall_sum(7, CS_REAL_TYPE, wsum);
 
   cs_lnum_t iter = 0;
 
   pc->n_g_total = gcount[iter++];
   pc->n_g_new = gcount[iter++];
-  pc->n_g_merged = gcount[iter++];
   pc->n_g_exit = gcount[iter++];
+  pc->n_g_merged = gcount[iter++];
   pc->n_g_deposited = gcount[iter++];
   pc->n_g_fouling = gcount[iter++];
   pc->n_g_resuspended = gcount[iter++];
@@ -1306,9 +1308,10 @@ cs_lagr_update_particle_counter(void)
   pc->w_total = wsum[0];
   pc->w_new = wsum[1];
   pc->w_exit = wsum[2];
-  pc->w_deposited = wsum[3];
-  pc->w_fouling = wsum[4];
-  pc->w_resuspended = wsum[5];
+  pc->w_merged = wsum[3];
+  pc->w_deposited = wsum[4];
+  pc->w_fouling = wsum[5];
+  pc->w_resuspended = wsum[6];
 
   return pc;
 }
@@ -1807,8 +1810,8 @@ cs_lagr_solve_time_step(const int         itypfb[],
 
   part_c->n_g_total = 0;
   part_c->n_g_new = 0;
-  part_c->n_g_merged = 0;
   part_c->n_g_exit = 0;
+  part_c->n_g_merged = 0;
   part_c->n_g_deposited = 0;
   part_c->n_g_fouling = 0;
   part_c->n_g_resuspended = 0;
@@ -1816,6 +1819,7 @@ cs_lagr_solve_time_step(const int         itypfb[],
   part_c->w_total = 0;
   part_c->w_new = 0;
   part_c->w_exit = 0;
+  part_c->w_merged = 0;
   part_c->w_deposited = 0;
   part_c->w_fouling = 0;
   part_c->w_resuspended = 0;

src/lagr/cs_lagr.h
@@ -315,11 +315,11 @@ typedef struct {
   /*! total number of particles*/
   cs_gnum_t   n_g_new;
 
-  /*! number of merged particles*/
-  cs_gnum_t   n_g_merged;
-
   /*! number of exited particles*/
   cs_gnum_t   n_g_exit;
+
+  /*! number of merged particles*/
+  cs_gnum_t   n_g_merged;
 
   /*! number of deposited particles */
   cs_gnum_t   n_g_deposited;
@@ -341,6 +341,9 @@ typedef struct {
 
   /*! weight of exited particles*/
   cs_real_t   w_exit;
+
+  /*! weight of merged particles*/
+  cs_real_t   w_merged;
 
   /*! weight of deposited particles */
   cs_real_t   w_deposited;

src/lagr/cs_lagr_log.c
@@ -420,7 +420,8 @@ cs_lagr_log_setup(void)
 
   if (cs_glob_lagr_time_scheme->modcpl) {
     const char c_dir[] = "xyze";
-    int _idirla = cs_glob_lagr_time_scheme->modcpl;
+    int _idirla = cs_glob_lagr_time_scheme->idirla;
+    assert(_idirla > -1 && _idirla < 4);
     cs_log_printf
       (CS_LOG_SETUP,
        _("    complete model main flow direction: %c\n"),

src/lagr/cs_lagr_particle.h
@@ -211,8 +211,8 @@ typedef struct {
 
   cs_lnum_t  n_particles;                     /* number of particle in domain */
   cs_lnum_t  n_part_new;
-  cs_lnum_t  n_part_merged;                   /* number of merged particles */
   cs_lnum_t  n_part_out;
+  cs_lnum_t  n_part_merged;                   /* number of merged particles */
   cs_lnum_t  n_part_dep;
   cs_lnum_t  n_part_fou;
   cs_lnum_t  n_part_resusp;
@@ -221,6 +221,7 @@ typedef struct {
   cs_real_t  weight;
   cs_real_t  weight_new;
   cs_real_t  weight_out;
+  cs_real_t  weight_merged;
   cs_real_t  weight_dep;
   cs_real_t  weight_fou;
   cs_real_t  weight_resusp;

src/lagr/cs_lagr_print.c
@@ -109,10 +109,7 @@ cs_lagr_print(cs_real_t ttcabs)
       && flal == NULL && _ipass == 1)
     flal = fopen("lagrangian.log","w");
 
-  /* ====================================================================   */
-  /* 2. OUVERTURE DU FICHIER DE STOCKAGE */
-  /* ====================================================================   */
-  /*     Seul le premier processeur ecrit les informations   */
+  /* Open log file on rank 0 only */
 
   if (flal != NULL) {
 
@@ -130,75 +127,68 @@ cs_lagr_print(cs_real_t ttcabs)
                 "# column  4: inst. number of particles (weighted)\n"
                 "# column  5: inst. number of injected particles\n"
                 "# column  6: inst. number of injected particles (weighted)\n"
-                "# column  7: inst. number of merged particles\n"
-                "# column  8: inst. number of exited, or deposited and removed particles\n"
-                "# column  9: inst. number of exited, or deposited and removed particles (weighted)\n"
-                "# column 10: inst. number of deposited particles\n"
-                "# column 11: inst. number of deposited particles (weighted)\n");
+                "# column  7: inst. number of exited, or deposited and removed particles\n"
+                "# column  8: inst. number of exited, or deposited and removed particles (weighted)\n"
+                "# column  9: inst. number of deposited particles\n"
+                "# column 10: inst. number of deposited particles (weighted)\n");
+
+        int col_id = 11;
+        if (cs_glob_lagr_model->agglomeration) {
+          fprintf(flal,
+                  "# column %2d: inst. number of merged particles\n",
+                  "# column %2d: inst. number of merged particles (weighted)\n",
+                  col_id, col_id+1);
+          col_id += 2;
+        }
 
         if (   lagr_model->physical_model == 2
-            && lagr_model->fouling == 1)
+            && lagr_model->fouling == 1) {
           fprintf(flal,
-                  "# column 12: inst. number of fouled particles (coal)\n"
-                  "# column 13: inst. number of fouled particles (coal, weighted)\n"
-                  "# column 14: inst. number of lost particles\n"
-                  "#\n");
+                  "# column %2d: inst. number of fouled particles (coal)\n"
+                  "# column %2d: inst. number of fouled particles (coal, weighted)\n");
+          col_id += 2;
+        }
 
-        else if (lagr_model->resuspension > 0)
+        else if (lagr_model->resuspension > 0) {
           fprintf(flal,
-                  "# column 12: inst. number of resuspended particles\n"
-                  "# column 13: inst. number of resuspended particles (weighted)\n"
-                  "# column 14: inst. number of lost particles\n"
-                  "#\n");
+                  "# column %2d: inst. number of resuspended particles\n"
+                  "# column %2d: inst. number of resuspended particles (weighted)\n");
+          col_id += 2;
+        }
 
-        else
-          fprintf(flal,
-                  "# column 12: inst. number of lost particles\n"
-                  "#\n");
+        fprintf(flal,
+                "# column %2d: inst. number of lost particles\n"
+                "#\n",
+                col_id);
 
       }
 
-      /* ====================================================================
-       * 2 - Ecriture des INFORMATIONS
-       * ====================================================================   */
+      /* Write output */
+
+      fprintf(flal, " %8d %11.4e %8llu %11.4e %8llu %11.4e %8llu %11.4e %8llu %11.4e",
+              cs_glob_time_step->nt_cur,
+              ttcabs,
+              (unsigned long long)(pc->n_g_total), pc->w_total,
+              (unsigned long long)(pc->n_g_new), pc->w_new,
+              (unsigned long long)(pc->n_g_exit - pc->n_g_fouling),
+              pc->w_exit - pc->w_fouling,
+              (unsigned long long)(pc->n_g_deposited), pc->w_deposited);
+
+      if (cs_glob_lagr_model->agglomeration)
+        fprintf(flal, " %8llu %11.4e",
+                (unsigned long long)(pc->n_g_merged), pc->w_merged);
 
       if (   lagr_model->physical_model == 2
           && lagr_model->fouling == 1)
-        fprintf(flal, " %8d %11.4E %8llu %11.4E %8llu %11.4E %8llu %8llu %11.4E %8llu %11.4E %8llu %11.4E %8llu\n",
-                cs_glob_time_step->nt_cur,
-                ttcabs,
-                (unsigned long long)(pc->n_g_total), pc->w_total,
-                (unsigned long long)(pc->n_g_new), pc->w_new,
-                (unsigned long long)(pc->n_g_merged),
-                (unsigned long long)(pc->n_g_exit - pc->n_g_fouling),
-                pc->w_exit - pc->w_fouling,
-                (unsigned long long)(pc->n_g_deposited), pc->w_deposited,
-                (unsigned long long)(pc->n_g_fouling), pc->w_fouling,
-                (unsigned long long)(pc->n_g_failed));
+        fprintf(flal, " %8llu %11.4e",
+                (unsigned long long)(pc->n_g_fouling), pc->w_fouling);
 
       else if (lagr_model->resuspension > 0)
-        fprintf(flal, " %8d %11.4E %8llu %11.4E %8llu %11.4E %8llu %8llu %11.4E %8llu %11.4E %8llu %11.4E %8llu\n",
-                cs_glob_time_step->nt_cur,
-                ttcabs,
-                (unsigned long long)(pc->n_g_total), pc->w_total,
-                (unsigned long long)(pc->n_g_new), pc->w_new,
-                (unsigned long long)(pc->n_g_merged),
-                (unsigned long long)(pc->n_g_exit), pc->w_exit,
-                (unsigned long long)(pc->n_g_deposited), pc->w_deposited,
-                (unsigned long long)(pc->n_g_resuspended), pc->w_resuspended,
-                (unsigned long long)(pc->n_g_failed));
+        fprintf(flal, " %8llu %11.4e",
+                (unsigned long long)(pc->n_g_resuspended), pc->w_resuspended);
 
-      else
-        fprintf(flal, " %8d %11.4E %8llu %11.4E %8llu %11.4E %8llu %8llu %11.4E %8llu %11.4E %8llu\n",
-                cs_glob_time_step->nt_cur,
-                ttcabs,
-                (unsigned long long)(pc->n_g_total), pc->w_total,
-                (unsigned long long)(pc->n_g_new), pc->w_new,
-                (unsigned long long)(pc->n_g_merged),
-                (unsigned long long)(pc->n_g_exit),
-                pc->w_exit,
-                (unsigned long long)(pc->n_g_deposited), pc->w_deposited,
-                (unsigned long long)(pc->n_g_failed));
+      fprintf(flal, " %8llu\n",
+              (unsigned long long)(pc->n_g_failed));
 
     }
 

src/lagr/cs_lagr_tracking.c
@@ -2574,6 +2574,7 @@ _sync_particle_set(cs_lagr_particle_set_t  *particles)
   cs_lnum_t  n_failed_particles = 0;
 
   cs_real_t  exit_weight = 0.0;
+  cs_real_t  merged_weight = 0.0;
   cs_real_t  fail_weight = 0.0;
   cs_real_t  tot_weight = 0.0;
 
@@ -2760,6 +2761,7 @@ _sync_particle_set(cs_lagr_particle_set_t  *particles)
 
     else if (cur_part_state == CS_LAGR_PART_MERGED) {
       n_merged_particles++;
+      merged_weight += cur_part_stat_weight;
     }
 
     else if (cur_part_state < CS_LAGR_PART_OUT) {
@@ -2799,6 +2801,7 @@ _sync_particle_set(cs_lagr_particle_set_t  *particles)
   particles->weight_failed += fail_weight;
 
   particles->n_part_merged += n_merged_particles;
+  particles->weight_merged += merged_weight;
 
   /* Exchange particles, then update set */
 

commit d287dcfd323667d9d6aa00b6a06d015433225173

Lagrangian module: fix crash introduced by commit 9f0092b7 (4 days ago).

src/lagr/cs_lagr.c
@@ -1281,18 +1281,19 @@ cs_lagr_update_particle_counter(void)
                         p_set->n_part_resusp,
                         p_set->n_failed_part};
 
-  cs_real_t wsum[6] = {p_set->weight,
-                       p_set->weight_new,
-                       p_set->weight_merged,
-                       p_set->weight_out,
-                       p_set->weight_dep,
-                       p_set->weight_fou,
-                       p_set->weight_resusp};
+  cs_real_t wsum[] = {p_set->weight,
+                      p_set->weight_new,
+                      p_set->weight_merged,
+                      p_set->weight_out,
+                      p_set->weight_dep,
+                      p_set->weight_fou,
+                      p_set->weight_resusp};
 
   cs_lnum_t size_count = sizeof(gcount) / sizeof(gcount[0]);
+  cs_lnum_t size_sum = sizeof(wsum) / sizeof(wsum[0]);
 
   cs_parall_counter(gcount, size_count);
-  cs_parall_sum(7, CS_REAL_TYPE, wsum);
+  cs_parall_sum(size_sum, CS_REAL_TYPE, wsum);
 
   cs_lnum_t iter = 0;
 