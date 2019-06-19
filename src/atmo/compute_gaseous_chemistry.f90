code-saturne/code_saturne

commit 6c65caa8f89bf73bedfae8d4b00c2df0f5dd7d73

Atmo. plug in the atmospheric module the SIze REsolved Aerosol Model (SIREAM) implemented in Polyphemus (provided by Raphael Bresson <raphael.bresson@edf.fr>).
git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@5339 e44c5d27-f830-0410-8ce8-974f7a764be6

src/atmo/compute_gaseous_chemistry.f90
@@ -1,0 +1,198 @@
+!-------------------------------------------------------------------------------
+
+! This file is part of Code_Saturne, a general-purpose CFD tool.
+!
+! Copyright (C) 1998-2013 EDF S.A.
+!
+! This program is free software; you can redistribute it and/or modify it under
+! the terms of the GNU General Public License as published by the Free Software
+! Foundation; either version 2 of the License, or (at your option) any later
+! version.
+!
+! This program is distributed in the hope that it will be useful, but WITHOUT
+! ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+! FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+! details.
+!
+! You should have received a copy of the GNU General Public License along with
+! this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+! Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+!-------------------------------------------------------------------------------
+
+!===============================================================================
+!  Purpose:
+!  --------
+
+!> \file compute_gaseous_chemistry.f90
+!> \brief Calls the rosenbrock resolution for atmospheric chemistry
+!
+!-------------------------------------------------------------------------------
+
+!-------------------------------------------------------------------------------
+! Arguments
+!______________________________________________________________________________.
+!  mode           name          role                                           !
+!______________________________________________________________________________!
+!> \param[in]     dt            time step (per cell)
+!> \param[in]     rtpa          calculated variables at cell centers
+!>                               (preceding time steps)
+!> \param[in,out] rtp           calculated variables at cell centers
+!_______________________________________________________________________________
+
+subroutine compute_gaseous_chemistry &
+!================
+
+( dt     , rtpa   , rtp    )
+
+!===============================================================================
+! Module files
+!===============================================================================
+
+use paramx
+use numvar
+use entsor
+use optcal
+use cstphy
+use cstnum
+use parall
+use period
+use ppppar
+use ppthch
+use ppincl
+use elincl
+use mesh
+use field
+use dimens
+use atchem
+use siream
+
+implicit none
+
+!===============================================================================
+
+! Arguments
+
+double precision dt(ncelet), rtp(ncelet,*), rtpa(ncelet,*)
+
+! Local Variables
+
+integer iel,ii
+double precision rom
+
+!  Variables used for Rosenbrock resolution
+double precision  dlconc(nespg)
+double precision  source(nespg)
+double precision  dchema(nespg)
+double precision  conv_factor(nespg) ! conversion factors for reaction rates
+double precision rk(nrg)
+double precision dtc
+integer ncycle
+double precision dtrest
+double precision rvoid(1)
+
+double precision, dimension(:), pointer :: crom
+
+!===============================================================================
+
+call field_get_val_s(icrom, crom)
+
+do iel = 1, ncel
+
+  ! time step
+  dtc = dt(iel)
+
+  ! density
+  rom = crom(iel)
+
+  ! Filling working array rk
+  do ii = 1, nrg
+    rk(ii) = reacnum((ii-1)*ncel+iel)
+  enddo
+
+  do ii = 1, nespg
+    conv_factor(chempoint(ii)) = rom*navo*(1.0d-12)/dmmk(ii)
+    source(ii) = 0.0d0
+  enddo
+
+  if ((isepchemistry.eq.1).or.(ntcabs.eq.1)) then
+    ! -----------------------------
+    ! -- splitted Rosenbrock solver
+    ! -----------------------------
+
+    ! Filling working array dlconc with rtp
+    do ii = 1, nespg
+      dlconc(chempoint(ii)) = rtp(iel,isca(ii))
+    enddo
+
+  else
+    ! -----------------------------
+    ! -- semi-coupled Rosenbrock solver
+    ! -----------------------------
+
+    ! Filling working array dlconc with rtpa
+    do ii = 1, nespg
+      dlconc(chempoint(ii)) = rtpa(iel,isca(ii))
+    enddo
+
+    ! Computation of C(Xn)
+    if (ichemistry.eq.1) then
+      call fexchem_1 (nespg,nrg,dlconc,rk,source,conv_factor,dchema)
+    else if (ichemistry.eq.2) then
+      call fexchem_2 (nespg,nrg,dlconc,rk,source,conv_factor,dchema)
+    else if (ichemistry.eq.3) then
+      if (iaerosol.eq.1) then
+        call fexchem_siream (nespg,nrg,dlconc,rk,source,conv_factor,dchema)
+      else
+        call fexchem_3 (nespg,nrg,dlconc,rk,source,conv_factor,dchema)
+      endif
+    else if (ichemistry.eq.4) then
+      call fexchem (nespg,nrg,dlconc,rk,source,conv_factor,dchema)
+    endif
+
+    ! Explicit contribution from dynamics as a source term:
+    ! (X*-Xn)/dt(dynamics) - C(Xn). See usatch.f90
+    ! The first nespg user scalars are supposed to be chemical species
+    do ii = 1, nespg
+      source(chempoint(ii)) = (rtp(iel,isca(ii))-rtpa(iel,isca(ii)))/dtc  &
+                            - dchema(chempoint(ii))
+    enddo
+
+  endif ! End test isepchemistry
+
+  ! Rosenbrock resoluion
+
+  ! The maximum time step used for chemistry resolution is dtchemmax
+  if (dtc.le.dtchemmax) then
+    call roschem (dlconc,source,source,conv_factor,dtc,rk,rk)
+  else
+    ncycle = int(dtc/dtchemmax)
+    dtrest = mod(dtc,dtchemmax)
+    do ii = 1, ncycle
+      call roschem (dlconc,source,source,conv_factor,dtchemmax,rk,rk)
+    enddo
+    call roschem (dlconc,source,source,conv_factor,dtrest,rk,rk)
+  endif
+
+  ! Update of rtp
+  do ii = 1, nespg
+    rtp(iel,isca(ii)) = dlconc(chempoint(ii))
+  enddo
+
+enddo
+
+! TODO: clipping or not ?
+! Clipping
+do ii = 1, nespg
+  call clpsca (ncelet, ncel, ii, rvoid, rtp)
+enddo
+
+!--------
+! FORMATS
+!--------
+!----
+! END
+!----
+
+return
+end subroutine compute_gaseous_chemistry

src/Makefile.am
@@ -66,6 +66,7 @@ $(top_srcdir)/src/base/post.f90 \
 $(top_srcdir)/src/atmo/atincl.f90 \
 $(top_srcdir)/src/atmo/atsoil.f90 \
 $(top_srcdir)/src/atmo/atchem.f90 \
+$(top_srcdir)/src/atmo/siream.f90 \
 $(top_srcdir)/src/atmo/spefun.f90 \
 $(top_srcdir)/src/cfbl/cfpoin.f90 \
 $(top_srcdir)/src/cogz/coincl.f90 \
@@ -101,6 +102,7 @@ $(top_srcdir)/src/base/optcal.f90: paramx.lo
 $(top_srcdir)/src/base/pointe.f90: paramx.lo numvar.lo parall.lo period.lo optcal.lo entsor.lo lagran.lo radiat.lo ppincl.lo ihmpre.lo albase.lo cstnum.lo
 $(top_srcdir)/src/atmo/atincl.f90: ppppar.lo ppincl.lo mesh.lo atsoil.lo
 $(top_srcdir)/src/atmo/atchem.f90: ppppar.lo mesh.lo entsor.lo numvar.lo
+$(top_srcdir)/src/atmo/siream.f90: ppppar.lo
 $(top_srcdir)/src/atmo/spefun.f90: entsor.lo
 $(top_srcdir)/src/cogz/coincl.f90: paramx.lo ppppar.lo
 $(top_srcdir)/src/comb/cpincl.f90: ppppar.lo ppthch.lo

src/atmo/Makefile.am
@@ -43,7 +43,8 @@ AM_LDFLAGS =
 
 # Public header files (to be installed)
 
-pkginclude_HEADERS =
+pkginclude_HEADERS = \
+cs_at_plugin.h
 
 # Library source files
 
@@ -51,6 +52,7 @@ noinst_LTLIBRARIES = libcsatmo.la
 libcsatmo_la_SOURCES = \
 atini1.f90 \
 atiniv.f90 \
+atleca.f90 \
 atlecc.f90 \
 atlecm.f90 \
 atmcls.f90 \
@@ -59,31 +61,34 @@ atmstd.f90 \
 atphyv.f90 \
 atprke.f90 \
 atprop.f90 \
-attycl.f90 \
 atr1vf.f90 \
 attssc.f90 \
+attycl.f90 \
 atvarp.f90 \
+chem_luscheme_siream.f90 \
 chem_luscheme1.f90 \
 chem_luscheme2.f90 \
 chem_luscheme3.f90 \
 chem_solvelu.f90 \
+chem_source_terms.f90 \
+compute_gaseous_chemistry.f90 \
+compute_siream.f90 \
+cs_at_plugin.c \
 etheq.f90  \
 intprf.f90 \
 kinrates.f90 \
-mrchim.f90 \
 nuclea.f90 \
-satfun.f90 \
 rayigc.f90 \
 rayir.f90 \
 rayive.f90 \
 rayso.f90 \
 raysze.f90 \
 roschem.f90 \
+satfun.f90 \
 solcat.f90 \
 soliva.f90 \
 solmoy.f90 \
-solvar.f90 \
-stchim.f90
+solvar.f90
 
 libcsatmo_la_LDFLAGS = -no-undefined
 

src/atmo/atchem.f90
@@ -30,9 +30,10 @@ module atchem
 use ppppar, only: nozppm
 
 !=============================================================================
-!> \defgroup at_chemistry Chemistry parameters for the atmospheric module
+!> \defgroup at_gaseous_chemistry Gaseous chemistry parameters for the
+!>                                atmospheric module
 
-!> \addtogroup at_chemistry
+!> \addtogroup at_gaseous_chemistry
 !> \{
 ! Useful constants for chemistry
 !> Avogadro constant (molecules/mol)
@@ -120,7 +121,7 @@ use numvar, only: nscaus
 
 implicit none
 
-integer imode,ii
+integer imode, ii
 
 init_at_chem = 1
 
@@ -170,7 +171,7 @@ allocate(ychem(nbchim))
 '@                                                            ',/,&
 '@ @@ ATTENTION : ARRET A L''ENTREE DES DONNEES               ',/,&
 '@    =========                                               ',/,&
-'@    CHIMIE ATMOSPHERIQUE DEMANDEE                           ',/,&
+'@    CHIMIE ATMOSPHERIQUE GAZEUSE DEMANDEE                   ',/,&
 '@                                                            ',/,&
 '@  Le nombre de scalaires utilisateurs declares doit etre    ',/,&
 '@  superieur ou egal au nombre d''especes chimiques          ',/,&
@@ -191,7 +192,7 @@ allocate(ychem(nbchim))
 '@                                                            ',/,&
      '@ @@  WARNING:   STOP WHILE READING INPUT DATA (atlecc) ',/,&
 '@    =========                                               ',/,&
-'@    ATMOSPHERIC CHEMISTRY                                   ',/,&
+'@    ATMOSPHERIC GASEOUS CHEMISTRY                           ',/,&
 '@                                                            ',/,&
 '@  The number of user scalars must be greater                ',/,&
 '@  than the number of chemical species                       ',/,&
@@ -199,7 +200,7 @@ allocate(ychem(nbchim))
 '@   Number of user scalars declared: ',I10                    ,/,&
 '@   Number of chemical species declared : ',I10               ,/,&
 '@                                                            ',/,&
-     '@  The computation will not be run                      ',/,&
+'@  The computation will not be run                           ',/,&
 '@                                                            ',/,&
 '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
 '@                                                            ',/)

src/atmo/atini1.f90
@@ -62,6 +62,8 @@ use ppincl
 use atincl
 use atsoil
 use atchem
+use siream
+
 !===============================================================================
 
 implicit none
@@ -69,7 +71,7 @@ implicit none
 ! Local variables
 
 integer          ii, isc, jj, ipp
-
+character*2                :: nbin
 !===============================================================================
 
 
@@ -362,10 +364,13 @@ initmeteo = 1
 call usati1
 !==========
 
-
-! Atmospheric Chemistry
-! if a chemical scheme is solved, a concentration profiles file must be used
+! Atmospheric gaseous chemistry
+! Do not change this order
+if (iaerosol.eq.1) ichemistry = 3
+! if a chemical scheme is solved, a concentration profiles
+! file must be used
 if (ichemistry.ge.1) ifilechemistry = ichemistry
+if (inogaseouschemistry.eq.1) ichemistry = 0
 
 if (ifilechemistry.ge.1) then
 
@@ -390,8 +395,8 @@ if (ifilechemistry.ge.1) then
     dmmk(3)=48.d-3 ! Molar mass O3
     dmmk(4)=16.d-3 ! Molar mass O3P
     chempoint = (/ 4, 3, 2, 1 /)
-
-  else if (ifilechemistry.eq.2) then ! scheme with 20 species and 34 reactions
+  ! scheme with 20 species and 34 reactions
+  else if (ifilechemistry.eq.2) then
     nrg = 34
     nespg = 20
     allocate(dmmk(nespg))
@@ -441,8 +446,13 @@ if (ifilechemistry.ge.1) then
                    10, 1, 12, 11, 13, 5, 6 /)
   ! scheme CB05 with 52 species and 155 reactions
   else if (ifilechemistry.eq.3) then
-    nrg = 155
-    nespg = 52
+    if (iaerosol.eq.1) then
+      nrg = 162
+      nespg = 65
+    else
+      nrg = 155
+      nespg = 52
+    endif
     allocate(dmmk(nespg))
     allocate(chempoint(nespg))
 
@@ -550,13 +560,97 @@ if (ifilechemistry.ge.1) then
     dmmk(50)=64.06d-3  ! Molar mass SO2
     dmmk(51)=98.08d-3  ! Molar mass H2SO4
     dmmk(52)=63.03d-3  ! Molar mass HCO3
-    chempoint = (/ 48, 52, 47, 43, 1, 42, 50, 17, 44, 9, 15, 38, 13,      &
-                   37, 41, 45, 51, 10, 35,46, 14, 49, 39, 33, 2, 3, 40,   &
-                   11, 19, 20, 4, 21, 36, 22, 34, 16, 23, 24, 25, 31, 32, &
-                   26, 5, 6, 27, 12, 28, 30, 29, 7, 8, 18 /)
+    if (iaerosol.eq.1) then
+      nomvar(ipprtp(isca(53)))='HC8'
+      nomvar(ipprtp(isca(54)))='API'
+      nomvar(ipprtp(isca(55)))='LIM'
+      nomvar(ipprtp(isca(56)))='CVARO1'
+      nomvar(ipprtp(isca(57)))='CVARO2'
+      nomvar(ipprtp(isca(58)))='CVALK1'
+      nomvar(ipprtp(isca(59)))='CVOLE1'
+      nomvar(ipprtp(isca(60)))='CVAPI1'
+      nomvar(ipprtp(isca(61)))='CVAPI2'
+      nomvar(ipprtp(isca(62)))='CVLIM1'
+      nomvar(ipprtp(isca(63)))='CVLIM2'
+      nomvar(ipprtp(isca(64)))='NH3'
+      nomvar(ipprtp(isca(65)))='HCL'
+      nomvar(ipprtp(isca(66)))='CVBIBMP'
+      nomvar(ipprtp(isca(67)))='CVANCLP'
+      nomvar(ipprtp(isca(68)))='CVBIISO1'
+      nomvar(ipprtp(isca(69)))='CVBIISO2'
+      dmmk(53)=114.0d0   ! Molar mass HC8
+      dmmk(54)=136.0d0   ! Molar mass API
+      dmmk(55)=136.0d0   ! Molar mass LIM
+      dmmk(56)=150.0d0   ! Molar mass CVARO1
+      dmmk(57)=150.0d0   ! Molar mass CVARO2
+      dmmk(58)=140.0d0   ! Molar mass CVALK1
+      dmmk(59)=140.0d0   ! Molar mass CVOLE1
+      dmmk(60)=184.0d0   ! Molar mass CVAPI1
+      dmmk(61)=184.0d0   ! Molar mass CVAPI2
+      dmmk(62)=200.0d0   ! Molar mass CVLIM1
+      dmmk(63)=200.0d0   ! Molar mass CVLIM2
+      dmmk(64)=17.0d0    ! Molar mass NH3
+      dmmk(65)=36.5d0    ! Molar mass HCL
+    endif
+    if (iaerosol.eq.1) then
+      chempoint = (/ 64, 65, 59, 57, 3, 55, 61, 20, 56, 16, 23, 50,&
+                     17, 51, 54, 60, 62, 13, 48, 58, 18, 63, 52, 46,&
+                     4, 5, 53, 14, 22, 35, 6, 32, 49, 33, 47, 19, 34,&
+                     36, 37, 44, 45, 39, 7, 8, 40, 15, 41, 43, 42, 9,&
+                     10, 21, 11, 24, 27, 30, 31, 12, 38, 25, 26, 28, 29,&
+                     1, 2 /)
+    else
+      chempoint = (/ 48, 52, 47, 43, 1, 42, 50, 17, 44, 9, 15, 38, 13, 37,&
+                     41, 45, 51, 10, 35, 46, 14, 49, 39, 33, 2, 3, 40, 11,&
+                     19, 20, 4, 21, 36, 22, 34, 16, 23, 24, 25, 31, 32, 26,&
+                     5, 6, 27, 12, 28, 30, 29, 7, 8, 18 /)
+    endif
+ endif
 
+endif
+
+! Atmospheric aerosol chemistry
+if (iaerosol.eq.1) then
+
+  write (nfecra,*) "Atmospheric aerosol chemistry activated"
+
+  ! logical unit and name of the chemical profiles file
+  impmea=28
+  ficmea='aerosols'
+
+  ! 4 species not followed in gaseous scheme 3 but necessary to siream
+  nespg_siream=nespg+4
+
+
+  ! Verification
+  if (ifilechemistry.ne.3) then
+    write(nfecra,1004)
+    call csexit (1)
   endif
 
+  ! The user must declare at least as many user scalars as the number
+  ! of chemical species nespg_siream+nbin_aer*nesp_aer
+  ! The user can declare more user scalars than nespg. But only the first
+  ! nespg will be considered for chemistry resolution
+  if (nscaus.lt.nespg_siream+nesp_aer*nbin_aer+nbin_aer) then
+    write(nfecra,1005) nscaus, nespg_siream+nesp_aer*nbin_aer+nbin_aer
+    call csexit (1)
+  endif
+
+  ! Filling the names array
+  esp_siream=(/'MD    ', 'BC    ', 'NA    ', 'H2SO4 ', 'NH3   ', 'HNO3  ',&
+ 'HCL   ','ARO1  ', 'ARO2  ', 'ALK1  ', 'OLE1  ', 'API1  ', 'API2  ',&
+ 'LIM1  ', 'LIM2  ', 'ANCLP ', 'BIISO1', 'BIISO2',&
+ 'BIBMP ', 'POA   ', 'H2O   '/)
+  do ii = 1, nbin_aer
+    write(nbin,"(i2)") ii
+    do jj = 1, nesp_aer
+      nomvar(ipprtp(isca(nespg_siream+ii+(jj-1)*nbin_aer)))=&
+             trim(esp_siream(jj))//'_bin'//trim(adjustl(nbin))
+    enddo
+      nomvar(ipprtp(isca(nespg_siream+nesp_aer*nbin_aer+ii)))=&
+            'Naero_bin'//trim(adjustl(nbin))
+  enddo
 endif
 
 !--------
@@ -640,6 +734,44 @@ endif
 '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
 '@                                                            ',/)
 
+ 1004 format(                                                           &
+'@                                                            ',/,&
+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
+'@                                                            ',/,&
+'@ @@ ATTENTION : ARRET A L''ENTREE DES DONNEES               ',/,&
+'@    =========                                               ',/,&
+'@    CHIMIE ATMOSPHERIQUE PARTICULAIRE DEMANDEE              ',/,&
+'@                                                            ',/,&
+'@  Lorsque le modele de chimie particulaire siream est utilise',/,&
+'@   un schema gazeux complet(CB05) est automatiquement utilise',/,&
+'@  L''utilisateur ne doit pas en specifier d''autre (ichemistry)',/,&
+'@  Le calcul ne sera pas execute.                            ',/,&
+'@                                                            ',/,&
+'@  Verifier usati1 (cs_user_parameters.f90)                  ',/,&
+'@                                                            ',/,&
+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
+'@                                                            ',/)
+
+ 1005 format(                                                           &
+'@                                                            ',/,&
+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
+'@                                                            ',/,&
+'@ @@ ATTENTION : ARRET A L''ENTREE DES DONNEES               ',/,&
+'@    =========                                               ',/,&
+'@    CHIMIE ATMOSPHERIQUE PARTICULAIRE DEMANDEE              ',/,&
+'@                                                            ',/,&
+'@  Le nombre de scalaires utilisateurs declares doit etre    ',/,&
+'@  superieur ou egal au nombre d''especes chimiques         ',/,&
+'@                                                            ',/,&
+'@   Nombre de scalaires utilisateurs declares : ',I10         ,/,&
+'@   Nombre d''especes chimiques declarees : ',I10             ,/,&
+'@                                                            ',/,&
+'@  Le calcul ne sera pas execute.                            ',/,&
+'@                                                            ',/,&
+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
+'@                                                            ',/)
+
+
 #else
 
  1000 format(                                                     &
@@ -711,6 +843,44 @@ endif
 '@                                                            ',/,&
 '@  Check the input data given through the User Interface     ',/,&
 '@   or in cs_user_parameters.f90.                            ',/,&
+'@                                                            ',/,&
+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
+'@                                                            ',/)
+
+ 1004 format(                                                           &
+'@                                                            ',/,&
+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
+'@                                                            ',/,&
+'@ @@  WARNING:   STOP WHILE READING INPUT DATA               ',/,&
+'@    =========                                               ',/,&
+'@    ATMOSPHERIC AEROSOL CHEMISTRY MODULE                    ',/,&
+'@                                                            ',/,&
+'@  When aerosol chemistry model siream is used               ',/,&
+'@   a full gaseous scheme (CB05) is automatically used       ',/,&
+'@  The user cannot specify any other scheme (ichemistry)     ',/,&
+'@  Computation CAN NOT run.                                  ',/,&
+'@                                                            ',/,&
+'@  Check the input data given through the User Interface     ',/,&
+'@   or in cs_user_parameters.f90.                            ',/,&
+'@                                                            ',/,&
+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
+'@                                                            ',/)
+
+ 1005 format(                                                           &
+'@                                                            ',/,&
+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
+'@                                                            ',/,&
+     '@ @@  WARNING:   STOP WHILE READING INPUT DATA          ',/,&
+'@    =========                                               ',/,&
+'@    ATMOSPHERIC AEROSOL CHEMISTRY MODULE                    ',/,&
+'@                                                            ',/,&
+'@  The number of user scalars must be greater                ',/,&
+'@  than the number of chemical species                       ',/,&
+'@                                                            ',/,&
+'@   Number of user scalars declared: ',I10                    ,/,&
+'@   Number of chemical species declared : ',I10               ,/,&
+'@                                                            ',/,&
+'@  The computation will not be run                           ',/,&
 '@                                                            ',/,&
 '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
 '@                                                            ',/)

src/atmo/atiniv.f90
@@ -86,6 +86,8 @@ use ppincl
 use atincl
 use mesh
 use atchem
+use siream
+
 !===============================================================================
 
 implicit none
@@ -132,7 +134,7 @@ if (iatra1.gt.0) then
 
 endif
 
-! Atmospheric Chemistry
+! Atmospheric gaseous chemistry
 if (ifilechemistry.ge.1) then
 
   ! Second reading of chemical profiles file
@@ -141,36 +143,54 @@ if (ifilechemistry.ge.1) then
   !==========
   ( imode)
 
-  ! Computation of the conversion factor matrix used for the reaction rates jaccobian matrix
+  ! Computation of the conversion factor matrix used for
+  ! the reaction rates jaccobian matrix
   do ii = 1, nespg
     do k = 1, nespg
       conv_factor_jac((chempoint(k)-1)*nespg+chempoint(ii)) = dmmk(ii)/dmmk(k)
     enddo
   enddo
 
-  ! Volume initilization with profiles for species present in the chemical profiles file
-  if (init_at_chem.eq.1) then
+  ! Volume initilization with profiles for species present
+  ! in the chemical profiles file
+  if (isuite.eq.0) then
     do iel = 1, ncel
 
       zent = xyzcen(3,iel)
 
       do k = 1, nespgi
-        call intprf                                                   &
+        call intprf                                                         &
         !==========
-        (nbchmz, nbchim,                                               &
+        (nbchmz, nbchim,                                                    &
          zproc, tchem, espnum(1+(k-1)*nbchim*nbchmz), zent  , ttcabs, xcent )
-
-        rtp(iel,isca(idespgi(k))) = xcent ! The first nespg user scalars
-                                          ! are supposed to be chemical species
+        ! The first nespg user scalars are supposed to be chemical species
+        rtp(iel,isca(idespgi(k))) = xcent
       enddo
 
     enddo
   endif
 endif
 
-! Verifications
+! Atmospheric aerosol chemistry
+if (iaerosol.eq.1) then
+
+  ! Reading intial concentrations and numbers
+  call atleca()
+
+  ! Initialization
+if (isuite.eq.0) then
+  do iel = 1, ncel
+    do ii=1,nesp_aer*nbin_aer+nbin_aer
+     rtp(iel,isca(nespg_siream+ii))=dlconc0(ii)
+    enddo
+  enddo
+endif
+
+endif
+
+!Verifications
 if ((iatra1.eq.1.or.ichemistry.ge.1).and.(syear.eq.-999.or.squant.eq.-999.or.shour.eq.-999&
-     .or.smin.eq.-999.or.ssec.eq.-999)) then
+.or.smin.eq.-999.or.ssec.eq.-999)) then
   if (iatra1.eq.1) write(nfecra,1000)
   if (ichemistry.ge.1) write(nfecra,1001)
   call csexit (1)

src/atmo/atleca.f90
@@ -1,0 +1,108 @@
+!-------------------------------------------------------------------------------
+
+! This file is part of Code_Saturne, a general-purpose CFD tool.
+!
+! Copyright (C) 1998-2013 EDF S.A.
+!
+! This program is free software; you can redistribute it and/or modify it under
+! the terms of the GNU General Public License as published by the Free Software
+! Foundation; either version 2 of the License, or (at your option) any later
+! version.
+!
+! This program is distributed in the hope that it will be useful, but WITHOUT
+! ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+! FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+! details.
+!
+! You should have received a copy of the GNU General Public License along with
+! this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+! Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+!-------------------------------------------------------------------------------
+
+!> \file atleca.f90
+!> \brief Reads intial aerosol concentrations and number
+!>
+!------------------------------------------------------------------------------
+
+!------------------------------------------------------------------------------
+! Arguments
+!------------------------------------------------------------------------------
+!   mode          name          role
+!------------------------------------------------------------------------------
+!______________________________________________________________________________
+
+subroutine atleca
+!================
+
+!===============================================================================
+! Module files
+!===============================================================================
+
+use paramx
+use pointe
+use entsor
+use cstnum
+use cstphy
+use ppppar
+use atincl
+use numvar
+use atchem
+use siream
+
+implicit none
+
+!===============================================================================
+
+! Arguments
+
+
+! Local variables
+
+integer jsp,jb
+
+!================================================================================
+! READINGS
+!================================================================================
+
+write(nfecra,*) ''
+write(nfecra,*) 'reading of aerosols numbers and concentrations'
+
+open(impmea,file=ficmea,status='old')
+  ! Reading aerosol numbers
+  do jb=1,nbin_aer
+    read(impmea,*) dlconc0(nesp_aer*nbin_aer+jb)
+  enddo
+  ! Reading aerosol concentrations
+  do jb=1,nbin_aer
+    do jsp=1,nesp_aer
+      read(impmea,*) dlconc0(jb+(jsp-1)*nbin_aer)
+    enddo
+  enddo
+close(impmea)
+
+!================================================================================
+! PRINTINGS
+!================================================================================
+
+write(nfecra, *)
+write(nfecra, *) '==================================================='
+write(nfecra, *) 'printing aerosol numbers'
+do jb=1,nbin_aer
+  write(nfecra,1000) jb, dlconc0(nesp_aer*nbin_aer+jb)
+enddo
+1000 format("Bin ",I2," : ",ES10.2)
+write(nfecra, *)
+write(nfecra, *) '==================================================='
+write(nfecra, *) 'printing aerosol concentrations'
+do jb=1,nbin_aer
+  write(nfecra,*) "Bin ",jb
+  do jsp=1,nesp_aer
+    write(nfecra,1001) nomvar(ipprtp(isca(nespg_siream+jb+(jsp-1)*nbin_aer))),&
+                       dlconc0(jb+(jsp-1)*nbin_aer)
+  enddo
+enddo
+1001 format(A10," : ",ES10.2)
+
+end subroutine
+

commit 3355b0dc4b8d0c1846dbede667b37f1236a18e52

[Atmo] Continue fix 512970 for the chemistry (clipping of scalar may be done on wrong scalars). Bug introduced in 6c65caa with the addition of the model itself (2013-10-21).

src/atmo/compute_gaseous_chemistry.f90
@@ -186,10 +186,9 @@ enddo
 
 deallocate(cvar_espg, cvara_espg)
 
-! TODO: clipping or not ?
 ! Clipping
 do ii = 1, nespg
-  call clpsca (ii)
+  call clpsca(isca_chem(ii))
 enddo
 
 !--------