0273bcfbdadaab351c3195f31555db99e3585363
Chaï Koren <chai.koren@noreply.edf.fr>
2019-06-20 11:00:42 +0200

    Remove unnecessary indirection in meg generation script

--- bin/cs_mei_to_c.py
+++ bin/cs_mei_to_c.py
@@ -1459,7 +1459,6 @@ class mei_to_c_interpreter:
 
         if need_for_loop:
             usr_blck += 2*tab + 'for (cs_lnum_t e_id = 0; e_id < bz->n_elts; e_id++) {\n'
-            usr_blck += 3*tab + 'cs_lnum_t f_id = bz->elt_ids[e_id];\n'
 
         usr_blck += usr_code
 
@@ -1590,7 +1589,6 @@ class mei_to_c_interpreter:
         usr_blck += usr_defs + '\n'
 
         usr_blck += 2*tab + 'for (cs_lnum_t e_id = 0; e_id < vz->n_elts; e_id++) {\n'
-        usr_blck += 3*tab + 'cs_lnum_t c_id = vz->elt_ids[e_id];\n'
 
         usr_blck += usr_code
 
@@ -1727,7 +1725,6 @@ class mei_to_c_interpreter:
         usr_blck += usr_defs + '\n'
 
         usr_blck += 2*tab + 'for (cs_lnum_t e_id = 0; e_id < vz->n_elts; e_id++) {\n'
-        usr_blck += 3*tab + 'cs_lnum_t c_id = vz->elt_ids[e_id];\n'
 
         usr_blck += usr_code
 
