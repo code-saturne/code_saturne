code-saturne/code_saturne

commit 2788a5680edc57a4b91ab67a12ff34a696e46f4c

Major cleanup and some fixes in Lagrangian model.
- A global bit-mask based CS_LAGR_P_FLAG attribute is now always present
  * It includes the features CS_LAGR_DEPOSITION_FLAG
  * It also includes a CS_LAGR_PART_FIXED bit replacing the use of a negative
    particle cell number.

- Local zero-based particle cell id is used instead of signed 1-based cell-number

- Advanced user functions are added for particle/face interaction, replacing
  the (never implemented) JBORD codes.

src/base/cs_restart.c
@@ -2799,7 +2799,7 @@ cs_restart_read_particles_info(cs_restart_t  *restart,
 int
 cs_restart_read_particles(cs_restart_t  *restart,
                           int            particles_location_id,
-                          cs_lnum_t     *particle_cell_num,
+                          cs_lnum_t     *particle_cell_id,
                           cs_real_t     *particle_coords)
 {
   double timing[2];
@@ -2864,7 +2864,7 @@ cs_restart_read_particles(cs_restart_t  *restart,
                                      false,
                                      g_cells_num,
                                      g_part_cell_num,
-                                     particle_cell_num);
+                                     particle_cell_id);
 
     BFT_FREE(g_part_cell_num);
 
@@ -2875,13 +2875,16 @@ cs_restart_read_particles(cs_restart_t  *restart,
 
 #endif /* #if defined(HAVE_MPI) */
 
-  if (cs_glob_n_ranks == 1)
+  if (cs_glob_n_ranks == 1) {
     retcode = cs_restart_read_section(restart,
                                       sec_name,
                                       particles_location_id,
                                       1,
                                       CS_TYPE_cs_int_t,
-                                      particle_cell_num);
+                                      particle_cell_id);
+    for (cs_lnum_t i = 0; i < n_particles; i++)
+      particle_cell_id[i] -= 1;
+  }
 
   BFT_FREE(sec_name);
 
@@ -2902,8 +2905,8 @@ cs_restart_read_particles(cs_restart_t  *restart,
  *                                on local numbers, plus the sum of particles
  *                                on lower MPI ranks
  * \param[in]  n_particles        local number of particles
- * \param[in]  particle_cell_num  local cell number (1 to n) to which particles
- *                                belong; 0 for untracked particles
+ * \param[in]  particle_cell_id   local cell id (0 to n-1) to which particles
+ *                                belong
  * \param[in]  particle_coords    local particle coordinates (interleaved)
  *
  * \return  the location id assigned to the particles
@@ -2915,7 +2918,7 @@ cs_restart_write_particles(cs_restart_t     *restart,
                            const char       *name,
                            bool              number_by_coords,
                            cs_lnum_t         n_particles,
-                           const cs_lnum_t  *particle_cell_num,
+                           const cs_lnum_t  *particle_cell_id,
                            const cs_real_t  *particle_coords)
 {
   double timing[2];
@@ -2989,15 +2992,15 @@ cs_restart_write_particles(cs_restart_t     *restart,
     const cs_gnum_t  *g_cell_num
       = restart->location[CS_MESH_LOCATION_CELLS-1].ent_global_num;
     for (i = 0; i < n_particles; i++) {
-      if (particle_cell_num[i] > 0)
-        global_part_cell_num[i] = g_cell_num[particle_cell_num[i]-1];
+      if (particle_cell_id[i] > -1)
+        global_part_cell_num[i] = g_cell_num[particle_cell_id[i]];
       else
         global_part_cell_num[i] = 0;
     }
   }
   else {
     for (i = 0; i < n_particles; i++)
-      global_part_cell_num[i] = particle_cell_num[i];
+      global_part_cell_num[i] = particle_cell_id[i] + 1;
   }
 
   BFT_MALLOC(sec_name, strlen(name) + strlen(cell_num_postfix) + 1, char);

gui/Pages/LagrangianForm.ui
@@ -448,7 +448,7 @@
      <item>
       <widget class="QGroupBox" name="groupBox_turbdepo">
        <property name="toolTip">
-        <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Activate or not the particle deposition sub-model.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+        <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
        </property>
        <property name="title">
         <string>Turbulent deposition modeling</string>
@@ -472,20 +472,20 @@
           <item row="0" column="1">
            <widget class="QLabel" name="labelIENCRA_2">
             <property name="toolTip">
-             <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Activate or not the particle deposition sub-model.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+             <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
             </property>
             <property name="text">
-             <string>Particle deposition sub-model</string>
+             <string/>
             </property>
            </widget>
           </item>
           <item row="0" column="0">
            <widget class="QCheckBox" name="checkBoxIDEPST">
             <property name="toolTip">
-             <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Activate or not the particle deposition sub-model.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+             <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Activate the particle deposition sub-model based on turbulence.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
             </property>
             <property name="text">
-             <string/>
+             <string>Turbulence-based deposition model</string>
             </property>
            </widget>
           </item>

gui/Pages/LagrangianStatisticsForm.ui
@@ -7,7 +7,7 @@
     <x>0</x>
     <y>0</y>
     <width>419</width>
-    <height>414</height>
+    <height>418</height>
    </rect>
   </property>
   <property name="windowTitle">
@@ -61,10 +61,10 @@
         <item row="1" column="1">
          <widget class="QLabel" name="labelNBCLST">
           <property name="toolTip">
-           <string/>
+           <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Defining multiple statistical classes allows compation of separate statistics for each statistical class of particles. These classes can be used to keep track of different physical characteristics as well as injection zones, in any combination needed.&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;Statistical class 0 includes all particles, so statistical classes assigned to injected particles should be &amp;gt; 0 where separate statistics are required for those particles.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
           </property>
           <property name="text">
-           <string>Number of particle clusters </string>
+           <string>Number of particle statistical classes</string>
           </property>
          </widget>
         </item>

po/fr.po
@@ -7,7 +7,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: code-saturne\n"
 "Report-Msgid-Bugs-To: saturne-support@edf.fr\n"
-"POT-Creation-Date: 2019-04-27 21:20+0200\n"
+"POT-Creation-Date: 2019-05-02 04:23+0200\n"
 "PO-Revision-Date: 2012-01-26 12:00+0100\n"
 "Last-Translator: Code_Saturne <saturne-support@edf.fr>\n"
 "Language-Team: French <traduc@traduc.org>\n"
@@ -11926,7 +11926,7 @@ msgid ""
 " Here IDIRLA = %d\n"
 msgstr ""
 
-msgid "Couplage thermique non-fonctionnel en multi-layer"
+msgid "Thermal coupling not implemented in multi-layer case"
 msgstr ""
 
 #, c-format
@@ -12566,14 +12566,17 @@ msgstr ""
 msgid "rebound"
 msgstr "rebond"
 
+msgid "deposition and elimination"
+msgstr ""
+
 msgid "deposition"
 msgstr "déposition"
 
 msgid "fouling"
 msgstr "encrassement"
 
-msgid "dlvo conditions"
-msgstr "conditions dlvo"
+msgid "DLVO conditions"
+msgstr "conditions DLVO"
 
 msgid "   Boundary statistics :\n"
 msgstr ""
@@ -12680,12 +12683,6 @@ msgstr "%s ne devrait pas être appelée après %s."
 
 msgid "Error reading main particle restart data."
 msgstr "Erreur à la lecture des données de reprise particulaires."
-
-#, c-format
-msgid ""
-" Error in %s: inclg < 0 \n"
-"Face number: %d, Particle number %d \n"
-msgstr ""
 
 #, c-format
 msgid ""

src/base/cs_restart.h
@@ -723,8 +723,8 @@ cs_restart_read_particles(cs_restart_t  *restart,
  *                                on local numbers, plus the sum of particles
  *                                on lower MPI ranks
  * \param[in]  n_particles        local number of particles
- * \param[in]  particle_cell_num  local cell number (1 to n) to which particles
- *                                belong; 0 for untracked particles
+ * \param[in]  particle_cell_id   local cell id (0 to n-1) to which particles
+ *                                belong
  * \param[in]  particle_coords    local particle coordinates (interleaved)
  *
  * \return  the location id assigned to the particles
@@ -736,7 +736,7 @@ cs_restart_write_particles(cs_restart_t     *restart,
                            const char       *name,
                            bool              number_by_coords,
                            cs_lnum_t         n_particles,
-                           const cs_lnum_t  *particle_cell_num,
+                           const cs_lnum_t  *particle_cell_id,
                            const cs_real_t  *particle_coords);
 
 /*----------------------------------------------------------------------------*/

src/lagr/cs_lagr.c
@@ -952,15 +952,14 @@ _get_n_occupied_cells(const cs_lagr_particle_set_t  *p_set,
   /* Main loop to update the counter */
   if (end - start > 1) {
     counter_particle_cells = 1;
-    cs_lnum_t prev_cell_num
-      = cs_lagr_particles_get_lnum(p_set, start, CS_LAGR_CELL_NUM);
-    cs_lnum_t curr_cell_num = prev_cell_num;
+    cs_lnum_t prev_cell_id
+      = cs_lagr_particles_get_lnum(p_set, start, CS_LAGR_CELL_ID);
+    cs_lnum_t curr_cell_id = prev_cell_id;
     for (cs_lnum_t p = start + 1; p < end; ++p) {
-      curr_cell_num = cs_lagr_particles_get_lnum(p_set, p, CS_LAGR_CELL_NUM);
-      if (prev_cell_num != curr_cell_num) {
+      curr_cell_id = cs_lagr_particles_get_lnum(p_set, p, CS_LAGR_CELL_ID);
+      if (prev_cell_id != curr_cell_id)
         counter_particle_cells++;
-      }
-      prev_cell_num = curr_cell_num;
+      prev_cell_id = curr_cell_id;
     }
   }
   else if (p_set->n_particles == 1) {
@@ -1004,7 +1003,7 @@ _occupied_cells(cs_lagr_particle_set_t  *p_set,
   if (end - start >= 1) {
     /* Initialization */
     cs_lnum_t prev_cell_id
-      = cs_lagr_particles_get_lnum(p_set, start, CS_LAGR_CELL_NUM) - 1;
+      = cs_lagr_particles_get_lnum(p_set, start, CS_LAGR_CELL_ID);
     cs_lnum_t curr_cell_id = prev_cell_id;
     cs_lnum_t counter = 0;
     occupied_cell_ids[0] = curr_cell_id;
@@ -1015,7 +1014,7 @@ _occupied_cells(cs_lagr_particle_set_t  *p_set,
     /* Update lists */
     for (cs_lnum_t part = start + 1; part < end; ++part) {
       curr_cell_id
-        = cs_lagr_particles_get_lnum(p_set, part, CS_LAGR_CELL_NUM) - 1;
+        = cs_lagr_particles_get_lnum(p_set, part, CS_LAGR_CELL_ID);
       if (prev_cell_id != curr_cell_id) {
         occupied_cell_ids[counter] = curr_cell_id;
         particle_gaps[counter] = part;
@@ -1048,12 +1047,8 @@ _get_n_deleted(cs_lagr_particle_set_t  *p_set,
   cs_lnum_t res = 0;
 
   for (cs_lnum_t idx = start; idx < end; ++idx) {
-    cs_lnum_t cell_num = cs_lagr_particles_get_lnum(p_set, idx, CS_LAGR_CELL_NUM);
-
-    /* Check for negative cells (bin flag) */
-    if (cell_num < 0) {
+    if (cs_lagr_particles_get_flag(p_set, idx, CS_LAGR_PART_TO_DELETE))
       res++;
-    }
   }
   return res;
 }
@@ -1465,7 +1460,6 @@ cs_lagr_get_internal_conditions(void)
 
     for (cs_lnum_t i = 0; i < cs_glob_mesh->n_i_faces; i++)
       cs_glob_lagr_internal_conditions->i_face_zone_id[i] = -1;
-
   }
 
   return cs_glob_lagr_internal_conditions;
@@ -1709,6 +1703,11 @@ cs_lagr_solve_time_step(const int         itypfb[],
       vislen[ifac] = -cs_math_big_r;
   }
 
+  /* mask for for deposition-related states */
+
+  int deposition_mask =   CS_LAGR_PART_DEPOSITED | CS_LAGR_PART_ROLLING
+                        | CS_LAGR_PART_IMPOSED_MOTION;
+
   /* Initialization
      -------------- */
 
@@ -1764,7 +1763,6 @@ cs_lagr_solve_time_step(const int         itypfb[],
 
         ustarmoy = dtmp[1];
         surftot  = dtmp[2];
-
       }
 
       if (dtmp[0] > 0.0)
@@ -1967,8 +1965,8 @@ cs_lagr_solve_time_step(const int         itypfb[],
     for (cs_lnum_t ip = 0; ip < p_set->n_particles; ip++) {
 
       cs_lagr_particles_set_lnum_n
-        (p_set, ip, 1, CS_LAGR_CELL_NUM,
-         cs_lagr_particles_get_lnum(p_set, ip, CS_LAGR_CELL_NUM));
+        (p_set, ip, 1, CS_LAGR_CELL_ID,
+         cs_lagr_particles_get_lnum(p_set, ip, CS_LAGR_CELL_ID));
 
       cs_lagr_particles_set_lnum_n(p_set, ip, 1, CS_LAGR_RANK_ID,
                                    cs_glob_rank_id);
@@ -2231,7 +2229,8 @@ cs_lagr_solve_time_step(const int         itypfb[],
 
           cs_lnum_t local_size = end_part - start_part;
           cs_lnum_t deleted_parts = _get_n_deleted(p_set, start_part, end_part);
-          size_t swap_buffer_size = p_set->p_am->extents * (local_size - deleted_parts);
+          size_t swap_buffer_size =   p_set->p_am->extents
+                                    * (local_size - deleted_parts);
           size_t swap_buffer_deleted = p_set->p_am->extents * deleted_parts;
 
           /* Create buffers for deleted particles */
@@ -2241,19 +2240,17 @@ cs_lagr_solve_time_step(const int         itypfb[],
 
           /* Update buffer for existing particles */
           cs_lnum_t count_del = 0, count_swap = 0;
-          for (cs_lnum_t i = 0; i < local_size; ++i) {
-            cs_lnum_t c_id = cs_lagr_particles_get_lnum(p_set,
-                                                        i+start_part,
-                                                        CS_LAGR_CELL_NUM);
-            if (c_id < 0) {
+          for (cs_lnum_t i = start_part; i < end_part; ++i) {
+            if (cs_lagr_particles_get_flag(p_set, i,
+                                           CS_LAGR_PART_TO_DELETE)) {
               memcpy(deleted_buffer + p_set->p_am->extents * count_del,
-                     p_set->p_buffer + p_set->p_am->extents * (i+start_part),
+                     p_set->p_buffer + p_set->p_am->extents * i,
                      p_set->p_am->extents);
               count_del++;
             }
             else {
               memcpy(swap_buffer + p_set->p_am->extents * count_swap,
-                     p_set->p_buffer + p_set->p_am->extents * (i+start_part),
+                     p_set->p_buffer + p_set->p_am->extents * i,
                      p_set->p_am->extents);
               count_swap++;
             }
@@ -2372,7 +2369,7 @@ cs_lagr_solve_time_step(const int         itypfb[],
 
         for (cs_lnum_t npt = 0; npt < p_set->n_particles; npt++) {
 
-          if (cs_lagr_particles_get_lnum(p_set, npt, CS_LAGR_CELL_NUM) - 1 >= 0) {
+          if (cs_lagr_particles_get_lnum(p_set, npt, CS_LAGR_CELL_ID) >= 0) {
             cs_real_t res_time
               = cs_lagr_particles_get_real(p_set, npt,
                                            CS_LAGR_RESIDENCE_TIME)
@@ -2401,13 +2398,13 @@ cs_lagr_solve_time_step(const int         itypfb[],
 
         for (cs_lnum_t npt = 0; npt < p_set->n_particles; npt++) {
 
-          if (cs_lagr_particles_get_lnum(p_set, npt, CS_LAGR_DEPOSITION_FLAG) > 0) {
+          if (cs_lagr_particles_get_flag(p_set, npt, deposition_mask)) {
 
             cs_real_t p_depo_time
               = cs_lagr_particles_get_real(p_set, npt, CS_LAGR_DEPO_TIME);
             cs_real_t p_consol_height
               = CS_MIN(cs_lagr_particles_get_real(p_set, npt, CS_LAGR_HEIGHT),
-                       cs_glob_lagr_consolidation_model->rate_consol * p_depo_time );
+                       cs_glob_lagr_consolidation_model->rate_consol * p_depo_time);
             cs_lagr_particles_set_real(p_set, npt,
                                        CS_LAGR_CONSOL_HEIGHT, p_consol_height);
             cs_lagr_particles_set_real(p_set, npt, CS_LAGR_DEPO_TIME,
@@ -2434,16 +2431,14 @@ cs_lagr_solve_time_step(const int         itypfb[],
         /* Height and time of deposit     */
 
         for (cs_lnum_t ifac = 0; ifac < n_b_faces; ifac++) {
-
           bound_stat[lag_bdi->iclogt * n_b_faces + ifac] = 0.0;
           bound_stat[lag_bdi->iclogh * n_b_faces + ifac] = 0.0;
           bound_stat[lag_bdi->ihdiam * n_b_faces + ifac] = 0.0;
-
         }
 
         for (cs_lnum_t npt = 0; npt < p_set->n_particles; npt++) {
 
-          if (cs_lagr_particles_get_lnum(p_set, npt, CS_LAGR_DEPOSITION_FLAG) == 1) {
+          if (cs_lagr_particles_get_flag(p_set, npt, deposition_mask)) {
 
             cs_lnum_t face_id = cs_lagr_particles_get_lnum
                                   (p_set, npt, CS_LAGR_NEIGHBOR_FACE_ID);

src/lagr/cs_lagr.h
@@ -82,35 +82,18 @@ typedef void
 
 typedef enum {
 
-  CS_LAGR_BC_UNDEFINED,
-  CS_LAGR_INLET,
-  CS_LAGR_OUTLET,
-  CS_LAGR_REBOUND,
-  CS_LAGR_DEPO1,
-  CS_LAGR_DEPO2,
-  CS_LAGR_FOULING,
-  CS_LAGR_JBORD1,
-  CS_LAGR_JBORD2,
-  CS_LAGR_JBORD3,
-  CS_LAGR_JBORD4,
-  CS_LAGR_JBORD5,
-  CS_LAGR_DEPO_DLVO,
-  CS_LAGR_SYM
+  CS_LAGR_BC_UNDEFINED,  /*!< undefined conditions */
+  CS_LAGR_SYM,           /*!< symmetry */
+  CS_LAGR_INLET,         /*!< inlet */
+  CS_LAGR_OUTLET,        /*!< outlet */
+  CS_LAGR_REBOUND,       /*!< elastic rebound */
+  CS_LAGR_DEPO1,         /*!< immediate deposition and elimination */
+  CS_LAGR_DEPO2,         /*!< deposition */
+  CS_LAGR_DEPO_DLVO,     /*!< deposition based on DLVO theory */
+  CS_LAGR_FOULING,       /*!< fouling (combustion) */
+  CS_LAGR_BC_USER        /*!< user-defined */
 
 } cs_lagr_bc_type_t;
-
-/*! Lagrangian deposition state */
-
-typedef enum {
-
-  CS_LAGR_PART_IN_FLOW        = 0,
-  CS_LAGR_PART_DEPOSITED      = 1,
-  CS_LAGR_PART_ROLLING        = 2,
-  CS_LAGR_PART_TO_DELETE      = 3,
-  CS_LAGR_PART_NO_MOTION      = 10,
-  CS_LAGR_PART_IMPOSED_MOTION = 11
-
-} cs_lagr_deposition_state_t;
 
   /*! Lagrangian module status.
      the different values correspond to the following coupling:
@@ -132,7 +115,6 @@ typedef enum {
   CS_LAGR_TWOWAY_COUPLING = 2,
   CS_LAGR_FROZEN_CONTINUOUS_PHASE = 3
 } cs_lagr_module_status_t;
-
 
 /*! Fixed maximum sizes */
 /*----------------------*/

src/lagr/cs_lagr_agglo.c
@@ -641,7 +641,7 @@ cs_lagr_agglomeration(cs_lnum_t  cell_id,
         inserted_vel_f[2] = p1_vel_f[2];
 
         cs_lagr_particles_set_lnum(p_set, inserted_parts-1,
-                                   CS_LAGR_CELL_NUM, cell_id+1);
+                                   CS_LAGR_CELL_ID, cell_id);
         cs_lagr_particles_set_lnum(p_set, inserted_parts-1,
                                    CS_LAGR_PARTICLE_AGGREGATE, new_class_nb);
 
@@ -700,13 +700,12 @@ cs_lagr_agglomeration(cs_lnum_t  cell_id,
       cs_lnum_t part_idx = interf_tot[start_gap][1];
       cs_real_t weight = cs_lagr_particles_get_real
                           (p_set, part_idx, CS_LAGR_STAT_WEIGHT);
-      /* Delete particle (if weight <0) */
+      /* Delete particle (if weight < 0) */
       if (weight <= 0.) {
         cs_lagr_particles_set_lnum(p_set, part_idx,
                                    CS_LAGR_PARTICLE_AGGREGATE, 0);
         cs_lagr_particles_set_real(p_set, part_idx, CS_LAGR_STAT_WEIGHT, 0);
-        cs_lagr_particles_set_lnum(p_set, part_idx,
-                                   CS_LAGR_CELL_NUM, -2); //cemetary cell
+        cs_lagr_particles_set_flag(p_set, part_idx, CS_LAGR_PART_TO_DELETE);
       }
 
       continue;
@@ -759,12 +758,11 @@ cs_lagr_agglomeration(cs_lnum_t  cell_id,
         cs_lnum_t part_idx = interf_tot[idx][1];
         cs_real_t weight = cs_lagr_particles_get_real(p_set, part_idx,
                                                       CS_LAGR_STAT_WEIGHT);
-
         if (weight > 0. && weight < parcmin && idx != last_small) {
           cs_lagr_particles_set_lnum(p_set, part_idx,
                                      CS_LAGR_PARTICLE_AGGREGATE, 0);
           cs_lagr_particles_set_real(p_set, part_idx, CS_LAGR_STAT_WEIGHT, 0);
-          cs_lagr_particles_set_lnum(p_set, part_idx, CS_LAGR_CELL_NUM, -2);
+          cs_lagr_particles_set_flag(p_set, part_idx, CS_LAGR_PART_TO_DELETE);
         }
       }
     }
@@ -779,7 +777,7 @@ cs_lagr_agglomeration(cs_lnum_t  cell_id,
         cs_lagr_particles_set_lnum(p_set, part_idx,
                                    CS_LAGR_PARTICLE_AGGREGATE, 0);
         cs_lagr_particles_set_real(p_set, part_idx, CS_LAGR_STAT_WEIGHT, 0);
-        cs_lagr_particles_set_lnum(p_set, part_idx, CS_LAGR_CELL_NUM, -2);
+        cs_lagr_particles_set_flag(p_set, part_idx, CS_LAGR_PART_TO_DELETE);
       }
     }
   }

commit 12ff98db31dcc7946d1d2328decc14542c849958

Lagrangian module: fix restart, broken by commit 2788a568 (April 30, 2019).

src/base/cs_restart.c
@@ -2859,7 +2859,7 @@ cs_restart_read_particles(cs_restart_t  *restart,
     timing[0] = cs_timer_wtime();
 
     cs_block_to_part_global_to_local(n_particles,
-                                     1,
+                                     0,
                                      n_cells,
                                      false,
                                      g_cells_num,