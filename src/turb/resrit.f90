code-saturne/code_saturne

commit e6cc75824754d09b04b3d2fe1bbf3e321956a067

Fix in turbulent isotropic diffusion (Shir model) with EB-RSM
  * The value of mu_t computed in phyvar.f90 is used in resssg
  * mu_t is computed with the normal Reynolds stress close to the wall

This fixes gitlab issue #84.

src/turb/resrit.f90
@@ -486,12 +486,7 @@ if (iand(vcopt_ut%idften, ANISOTROPIC_RIGHT_DIFFUSION).ne.0) then
 else
 
   do iel = 1, ncel
-    if (irijco.eq.1) then
-      trrij = 0.5d0 * (cvar_rij(1,iel) + cvar_rij(2,iel) + cvar_rij(3,iel))
-    else
-      trrij = 0.5d0 * (cvar_r11(iel) + cvar_r22(iel) + cvar_r33(iel))
-    end if
-    rctse = crom(iel) * csrij * trrij**2 / cvar_ep(iel)
+    rctse = csrij * visct(iel) / cmu
     w1(iel) = viscl(iel) + vcopt%idifft*rctse
   enddo
 

src/base/phyvar.f90
@@ -75,6 +75,7 @@ use ppthch
 use ppincl
 use mesh
 use field
+use field_operator
 use cavitation
 use vof
 use darcy_module
@@ -93,18 +94,23 @@ double precision dt(ncelet)
 
 character(len=80) :: chaine
 integer          ivar  , iel   , ifac  , iscal
-integer          ii    , iok   , iok1  , iok2  , iisct, idfm, iggafm, iebdfm
+integer          ii    , jj    , iok   , iok1  , iok2  , iisct, idfm, iggafm, iebdfm
 integer          nn    , isou
 integer          mbrom , ifcvsl
 integer          iclipc, idftnp
+integer          iprev , inc, iccocg
+
 double precision xk, xe, xnu, xrom, vismax(nscamx), vismin(nscamx)
+double precision xrij(3,3), xnal(3), xnoral
 double precision xfmu, xmu, xmut
 double precision nusa, xi3, fv1, cv13
 double precision varmn(4), varmx(4), tt, ttmin, ttke, viscto
 double precision xttkmg, xttdrb
 double precision trrij,rottke
+double precision alpha3, xrnn
 double precision, dimension(:), pointer :: brom, crom
 double precision, dimension(:), pointer :: cvar_k, cvar_ep, cvar_phi, cvar_nusa
+double precision, dimension(:), pointer :: cvar_al
 double precision, dimension(:), pointer :: cvar_r11, cvar_r22, cvar_r33
 double precision, dimension(:), pointer :: cvar_r12, cvar_r13, cvar_r23
 double precision, dimension(:,:), pointer :: cvar_rij
@@ -113,6 +119,7 @@ double precision, dimension(:,:), pointer :: visten, vistes, cpro_visma_v
 double precision, dimension(:), pointer :: viscl, visct, cpro_vis
 double precision, dimension(:), pointer :: cvar_voidf
 double precision, dimension(:), pointer :: cpro_var, cpro_beta, cpro_visma_s
+double precision, allocatable, dimension(:,:) :: grad
 
 integer          ipass
 data             ipass /0/
@@ -330,26 +337,97 @@ elseif (itytur.eq.3) then
   call field_get_val_s(ivisct, visct)
   call field_get_val_s(icrom, crom)
   call field_get_val_s(ivarfl(iep), cvar_ep)
-
   if (irijco.eq.1) then
     call field_get_val_v(ivarfl(irij), cvar_rij)
-
-    do iel = 1, ncel
-      xk = 0.5d0*(cvar_rij(1,iel)+cvar_rij(2,iel)+cvar_rij(3,iel))
-      xe = cvar_ep(iel)
-      visct(iel) = crom(iel)*cmu*xk**2/xe
-    enddo
   else
     call field_get_val_s(ivarfl(ir11), cvar_r11)
     call field_get_val_s(ivarfl(ir22), cvar_r22)
     call field_get_val_s(ivarfl(ir33), cvar_r33)
-
-    do iel = 1, ncel
-      xk = 0.5d0*(cvar_r11(iel)+cvar_r22(iel)+cvar_r33(iel))
-      xe = cvar_ep(iel)
-      visct(iel) = crom(iel)*cmu*xk**2/xe
-    enddo
+    call field_get_val_s(ivarfl(ir12), cvar_r12)
+    call field_get_val_s(ivarfl(ir23), cvar_r23)
+    call field_get_val_s(ivarfl(ir13), cvar_r13)
   endif
+
+  ! In case we are in EB-RSM, we compute the normals
+  if (iturb.eq.32) then
+    call field_get_val_s(ivarfl(ial), cvar_al)
+
+    allocate(grad(3,ncelet))
+
+    ! Compute the gradient of Alpha
+    iprev  = 1
+    inc    = 1
+    iccocg = 1
+
+    call field_gradient_scalar(ivarfl(ial), iprev, imrgra, inc,     &
+                               iccocg,                              &
+                               grad)
+  endif
+
+  do iel = 1, ncel
+
+    if (irijco.eq.1) then
+      xrij(1,1) = cvar_rij(1,iel)
+      xrij(2,2) = cvar_rij(2,iel)
+      xrij(3,3) = cvar_rij(3,iel)
+      xrij(1,2) = cvar_rij(4,iel)
+      xrij(2,3) = cvar_rij(5,iel)
+      xrij(1,3) = cvar_rij(6,iel)
+      xrij(2,1) = xrij(1,2)
+      xrij(3,1) = xrij(1,3)
+      xrij(3,2) = xrij(2,3)
+    else
+      xrij(1,1) = cvar_r11(iel)
+      xrij(2,2) = cvar_r22(iel)
+      xrij(3,3) = cvar_r33(iel)
+      xrij(1,2) = cvar_r12(iel)
+      xrij(1,3) = cvar_r13(iel)
+      xrij(2,3) = cvar_r23(iel)
+      xrij(2,1) = xrij(1,2)
+      xrij(3,1) = xrij(1,3)
+      xrij(3,2) = xrij(2,3)
+    endif
+
+    alpha3 = 1.d0
+    xrnn = 0.d0
+
+    if (iturb.eq.32) then
+      ! Compute the magnitude of the Alpha gradient
+      xnoral = ( grad(1,iel)*grad(1,iel)          &
+             +   grad(2,iel)*grad(2,iel)          &
+             +   grad(3,iel)*grad(3,iel) )
+      xnoral = sqrt(xnoral)
+     ! Compute the unitary vector of Alpha
+      if (xnoral.le.epzero) then
+        xnal(1) = 0.d0
+        xnal(2) = 0.d0
+        xnal(3) = 0.d0
+      else
+        xnal(1) = grad(1,iel)/xnoral
+        xnal(2) = grad(2,iel)/xnoral
+        xnal(3) = grad(3,iel)/xnoral
+      endif
+
+      alpha3 = cvar_al(iel)**3
+
+      ! We compute the normal Reynolds Stresses
+      do ii = 1, 3
+        do jj = 1, 3
+          xrnn = xrnn + xrij(ii,jj)*xnal(ii)*xnal(jj)
+        enddo
+      enddo
+    endif
+
+    xk   = 0.5d0*(xrij(1,1)+xrij(2,2)+xrij(3,3))
+    xrnn = (1.d0-alpha3)*xrnn + alpha3*xk
+    xe = cvar_ep(iel)
+    visct(iel) = crom(iel)*cmu*xrnn*xk/xe
+  enddo
+
+  if (iturb.eq.32) then
+    deallocate(grad)
+  end if
+
 elseif (iturb.eq.40) then
 
 ! 4.5 LES Smagorinsky

src/turb/resssg.f90
@@ -175,7 +175,7 @@ double precision, dimension(:), pointer :: cvara_ep, cvar_al
 double precision, dimension(:), pointer :: cvara_r11, cvara_r22, cvara_r33
 double precision, dimension(:), pointer :: cvara_r12, cvara_r13, cvara_r23
 double precision, dimension(:), pointer :: cvar_var, cvara_var
-double precision, dimension(:), pointer :: viscl, c_st_prv
+double precision, dimension(:), pointer :: viscl, c_st_prv, visct
 
 type pmapper_double_r1
    double precision, dimension(:),  pointer :: p !< rank 1 array pointer
@@ -215,6 +215,7 @@ endif
 
 call field_get_val_s(icrom, crom)
 call field_get_val_s(iviscl, viscl)
+call field_get_val_s(ivisct, visct)
 
 call field_get_val_prev_s(ivarfl(iep), cvara_ep)
 if (iturb.ne.31) call field_get_val_s(ivarfl(ial), cvar_al)
@@ -761,8 +762,7 @@ if (iand(vcopt%idften, ANISOTROPIC_DIFFUSION).ne.0) then
 else
 
   do iel = 1, ncel
-    trrij = 0.5d0 * (cvara_r11(iel) + cvara_r22(iel) + cvara_r33(iel))
-    rctse = crom(iel) * csrij * trrij**2 / cvara_ep(iel)
+    rctse = csrij * visct(iel) / cmu
     w1(iel) = viscl(iel) + vcopt%idifft*rctse
   enddo
 

src/turb/resssg2.f90
@@ -189,7 +189,7 @@ double precision, dimension(:,:,:), pointer :: coefbp, cofbfp
 double precision, dimension(:,:), pointer :: visten
 double precision, dimension(:), pointer :: cvara_ep, cvar_al
 double precision, dimension(:,:), pointer :: cvar_var, cvara_var
-double precision, dimension(:), pointer :: viscl
+double precision, dimension(:), pointer :: viscl, visct
 double precision, dimension(:,:), pointer:: c_st_prv, lagr_st_rij
 double precision, dimension(:,:), pointer :: cpro_buoyancy
 
@@ -244,6 +244,7 @@ endif
 
 call field_get_val_s(icrom, crom)
 call field_get_val_s(iviscl, viscl)
+call field_get_val_s(ivisct, visct)
 
 call field_get_val_prev_s(ivarfl(iep), cvara_ep)
 if (iturb.ne.31) call field_get_val_s(ivarfl(ial), cvar_al)
@@ -998,8 +999,7 @@ if (iand(vcopt%idften, ANISOTROPIC_RIGHT_DIFFUSION).ne.0) then
 else
 
   do iel = 1, ncel
-    trrij = 0.5d0 * (cvara_var(1,iel) + cvara_var(2,iel) + cvara_var(3,iel))
-    rctse = crom(iel) * csrij * trrij**2 / cvara_ep(iel)
+    rctse = csrij * visct(iel) / cmu
     w1(iel) = viscl(iel) + vcopt%idifft*rctse
   enddo
 

commit ee1a0b4724344320d99750fae0c4282950692a85

Fix bug introduced in commit e6cc758 (wrong diffusion coefficient)

src/turb/resrit.f90
@@ -486,7 +486,7 @@ if (iand(vcopt_ut%idften, ANISOTROPIC_RIGHT_DIFFUSION).ne.0) then
 else
 
   do iel = 1, ncel
-    rctse = csrij * visct(iel) / cmu
+    rctse = ctheta(iscal) * visct(iel) / cmu
     w1(iel) = viscl(iel) + vcopt%idifft*rctse
   enddo
 