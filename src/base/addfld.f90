code-saturne/code_saturne

commit a2ddc3bfdd4bfa871c2580d6cc61f93366dd713d

Major commit: change the time stepping to ensure 2nd time order for variable density flow if 2nd time order is activated. It impacts all the second time order time stepping and also all variable densities algorithms.  * If you want to go back to the previous algorithm for variable    density, you can specify ipredfl = 0 in the usipsu    (cs_user_parameters.f90) subroutine.  * The momentum equation is staggered in time, that is to say, when    2nd order is activated, velocity is solved from time n-1/2 to n+1/2.    A special care should be done for time averaged quantities.  * Remove deprecated "thetro" keyword.
Worked done in collaboration with C. Flageul.

src/base/fldini.f90
@@ -676,6 +676,21 @@ endif
 ! It is then used in tridim to update buoyant scalars and density in U-P loop
 call cs_parameters_set_n_buoyant_scalars
 
+! If density is variable, a particular care must be taken when dealing with
+! density in the unsteady term in the velocity pressure loop
+if (irovar.eq.1) then
+  ! EOS density, imposed after the correction step, so we need
+  ! to keep the previous one, which is in balance with the mass
+  f_name = 'density_mass'
+  itycat = FIELD_PROPERTY
+  ityloc = 1 ! cells
+  call field_create(f_name, itycat, ityloc, idim1, inoprv, f_id)
+  f_name = 'boundary_density_mass'
+  itycat = FIELD_PROPERTY
+  ityloc = 3 ! boundary faces
+  call field_create(f_name, itycat, ityloc, idim1, inoprv, f_id)
+endif
+
 ! Some C mappings
 call cs_field_pointer_map_boundary
 

src/base/modini.f90
@@ -184,16 +184,6 @@ elseif (istmpf.eq.2) then
 endif
 
 !    -- Proprietes physiques
-if (abs(thetro+999.d0).gt.epzero) then
-  write(nfecra,1011) 'IROEXT',iroext,'THETRO'
-  iok = iok + 1
-elseif (iroext.eq.0) then
-  thetro = 0.0d0
-elseif (iroext.eq.1) then
-  thetro = 0.5d0
-elseif (iroext.eq.2) then
-  thetro = 1.d0
-endif
 if (abs(thetvi+999.d0).gt.epzero) then
   write(nfecra,1011) 'IVIEXT',iviext,'THETVI'
   iok = iok + 1

src/base/navstv.f90
@@ -114,7 +114,7 @@ double precision, pointer, dimension(:,:,:) :: ximpa
 ! Local variables
 
 integer          iccocg, inc, iel, iel1, iel2, ifac, imax, imaxt, imin, imint
-integer          ii    , inod, itypfl
+integer          ii    , inod, itypfl, f_id
 integer          isou, ivar, iitsm
 integer          init, iautof
 integer          iflmas, iflmab
@@ -142,6 +142,7 @@ double precision, allocatable, dimension(:,:,:), target :: viscf
 double precision, allocatable, dimension(:), target :: viscb
 double precision, allocatable, dimension(:,:,:), target :: wvisfi
 double precision, allocatable, dimension(:), target :: wvisbi
+double precision, allocatable, dimension(:), target :: cpro_rho_tc, bpro_rho_tc
 double precision, allocatable, dimension(:) :: phi
 double precision, allocatable, dimension(:) :: w1
 double precision, allocatable, dimension(:) :: w7, w8, w9
@@ -162,7 +163,7 @@ double precision, dimension(:,:), pointer :: coefau, cofafu, claale
 double precision, dimension(:,:,:), pointer :: coefbu, cofbfu, clbale
 double precision, dimension(:), pointer :: coefa_p
 double precision, dimension(:), pointer :: imasfl, bmasfl
-double precision, dimension(:), pointer :: brom, crom, croma, viscl, visct
+double precision, dimension(:), pointer :: brom, broma, crom, croma, viscl, visct
 double precision, dimension(:), pointer :: ivoifl, bvoifl
 double precision, dimension(:), pointer :: coavoi, cobvoi
 double precision, dimension(:,:), pointer :: trav
@@ -175,6 +176,9 @@ double precision, dimension(:), pointer :: cvar_pr
 double precision, dimension(:), pointer :: cpro_prtot, c_estim
 double precision, dimension(:), pointer :: cvar_voidf, cvara_voidf
 double precision, dimension(:), pointer :: cvara_k
+double precision, dimension(:), pointer :: cpro_rho_mass
+double precision, dimension(:), pointer :: bpro_rho_mass
+double precision, dimension(:), pointer :: brom_eos, crom_eos
 
 type(var_cal_opt) :: vcopt_p, vcopt_u, vcopt
 
@@ -371,6 +375,62 @@ t2 = 0.d0
 t3 = 0.d0
 t4 = 0.d0
 
+! Id of the mass flux
+call field_get_key_int(ivarfl(iu), kimasf, iflmas)
+call field_get_key_int(ivarfl(iu), kbmasf, iflmab)
+
+! Pointers to the mass fluxes
+call field_get_val_s(iflmas, imasfl)
+call field_get_val_s(iflmab, bmasfl)
+
+! Pointers to properties
+call field_get_val_s(icrom, crom_eos)
+call field_get_val_s(ibrom, brom_eos)
+
+if (irovar.eq.1) then
+  call field_get_val_prev_s(icrom, croma)
+  call field_get_val_prev_s(ibrom, broma)
+  ! If iterns = 1: this is density at time n
+  call field_get_id("density_mass", f_id)
+  call field_get_val_s(f_id, cpro_rho_mass)
+  call field_get_id("boundary_density_mass", f_id)
+  call field_get_val_s(f_id, bpro_rho_mass)
+
+  ! Time interpolated density
+  if (vcopt_u%thetav .lt. 1.d0) then
+    allocate(cpro_rho_tc(ncelet))
+    allocate(bpro_rho_tc(nfabor))
+
+    do iel = 1, ncelet
+      cpro_rho_tc(iel) = vcopt_u%thetav * cpro_rho_mass(iel) &
+        + (1.d0 - vcopt_u%thetav) * croma(iel)
+    enddo
+
+    crom => cpro_rho_tc
+
+    do ifac = 1, nfabor
+      bpro_rho_tc(ifac) = vcopt_u%thetav * bpro_rho_mass(ifac) &
+        + (1.d0 - vcopt_u%thetav) * broma(ifac)
+    enddo
+
+    brom => bpro_rho_tc
+
+  else
+    crom => cpro_rho_mass
+    brom => bpro_rho_mass
+  endif
+
+else
+  crom => crom_eos
+  brom => brom_eos
+endif
+
+! Pointers to BC coefficients
+call field_get_coefa_v(ivarfl(iu), coefau)
+call field_get_coefb_v(ivarfl(iu), coefbu)
+call field_get_coefaf_v(ivarfl(iu), cofafu)
+call field_get_coefbf_v(ivarfl(iu), cofbfu)
+
 !===============================================================================
 ! 1. Prediction of the mass flux in case of Low Mach compressible algorithm
 !===============================================================================
@@ -428,24 +488,6 @@ endif
 
 iappel = 1
 
-! Id of the mass flux
-call field_get_key_int(ivarfl(iu), kimasf, iflmas)
-call field_get_key_int(ivarfl(iu), kbmasf, iflmab)
-
-! Pointers to the mass fluxes
-call field_get_val_s(iflmas, imasfl)
-call field_get_val_s(iflmab, bmasfl)
-
-! Pointers to properties
-call field_get_val_s(icrom, crom)
-call field_get_val_s(ibrom, brom)
-
-! Pointers to BC coefficients
-call field_get_coefa_v(ivarfl(iu), coefau)
-call field_get_coefb_v(ivarfl(iu), coefbu)
-call field_get_coefaf_v(ivarfl(iu), cofafu)
-call field_get_coefbf_v(ivarfl(iu), cofbfu)
-
 call predvv &
 ( iappel ,                                                       &
   nvar   , nscal  , iterns ,                                     &
@@ -453,9 +495,8 @@ call predvv &
   icepdc , icetsm , ifbpcd , ltmast ,                            &
   itypsm ,                                                       &
   dt     , vel    , vela   ,                                     &
-  imasfl , bmasfl ,                                              &
   tslagr , coefau , coefbu , cofafu , cofbfu ,                   &
-  ckupdc , smacel , spcond , svcond , frcxt  , grdphd ,          &
+  ckupdc , smacel , spcond ,          frcxt  , grdphd ,          &
   trava  , ximpa  , uvwk   , dfrcxt , dttens ,  trav  ,          &
   viscf  , viscb  , viscfi , viscbi , secvif , secvib ,          &
   w1     , w7     , w8     , w9     )
@@ -484,7 +525,7 @@ if (iprco.le.0) then
    iflmb0 , init   , inc    , imrgra , nswrgp , imligp ,          &
    iwarnp ,                                                       &
    epsrgp , climgp ,                                              &
-   crom, brom,                                                    &
+   crom   , brom   ,                                              &
    vel    ,                                                       &
    coefau , coefbu ,                                              &
    imasfl , bmasfl )
@@ -622,6 +663,8 @@ if (iprco.le.0) then
   ! Free memory
   !--------------
   deallocate(coefa_dp, coefb_dp)
+  if (allocated(cpro_rho_tc)) deallocate(cpro_rho_tc)
+  if (allocated(bpro_rho_tc)) deallocate(bpro_rho_tc)
 
   return
 
@@ -1349,7 +1392,6 @@ if (iestim(iescor).ge.0.or.iestim(iestot).ge.0) then
     enddo
 
     !   APPEL A PREDVV AVEC VALEURS AU PAS DE TEMPS COURANT
-    !                  AVEC LE FLUX DE MASSE RECALCULE
     iappel = 2
     call predvv &
  ( iappel ,                                                       &
@@ -1358,9 +1400,8 @@ if (iestim(iescor).ge.0.or.iestim(iestot).ge.0) then
    icepdc , icetsm , ifbpcd , ltmast ,                            &
    itypsm ,                                                       &
    dt     , vel    , vel    ,                                     &
-   esflum , esflub ,                                              &
    tslagr , coefau , coefbu , cofafu , cofbfu ,                   &
-   ckupdc , smacel , spcond , svcond , frcxt  , grdphd ,          &
+   ckupdc , smacel , spcond ,          frcxt  , grdphd ,          &
    trava  , ximpa  , uvwk   , dfrcxt , dttens , trav   ,          &
    viscf  , viscb  , viscfi , viscbi , secvif , secvib ,          &
    w1     , w7     , w8     , w9     )
@@ -1632,6 +1673,8 @@ deallocate(w1)
 deallocate(w7, w8, w9)
 if (allocated(wvisfi)) deallocate(wvisfi, wvisbi)
 if (allocated(secvif)) deallocate(secvif, secvib)
+if (allocated(cpro_rho_tc)) deallocate(cpro_rho_tc)
+if (allocated(bpro_rho_tc)) deallocate(bpro_rho_tc)
 if (iphydr.eq.2) deallocate(grdphd)
 
 ! Free memory

src/base/resopv.f90
@@ -229,7 +229,8 @@ double precision, allocatable, dimension(:,:), target :: tpusro
 double precision, dimension(:), pointer :: viscap
 double precision, dimension(:,:), pointer :: vitenp
 double precision, dimension(:), pointer :: imasfl, bmasfl
-double precision, dimension(:), pointer :: brom, crom, croma
+double precision, dimension(:), pointer :: brom, crom, croma, broma
+double precision, dimension(:), pointer :: brom_eos, crom_eos
 double precision, dimension(:), pointer :: cvar_pr
 double precision, dimension(:), pointer :: cpro_divu
 double precision, dimension(:), pointer :: cpro_wgrec_s
@@ -242,6 +243,8 @@ double precision, allocatable, dimension(:) :: cpro_visc
 double precision, allocatable, dimension(:,:) :: cpro_vitenp
 double precision, allocatable, dimension(:,:) :: trav
 double precision, dimension(:,:), pointer :: cpro_poro_div_duq
+double precision, dimension(:), pointer :: cpro_rho_mass, bpro_rho_mass
+double precision, dimension(:), allocatable, target :: cpro_rho_tc, bpro_rho_tc
 
 !===============================================================================
 
@@ -315,11 +318,49 @@ call field_get_coefaf_s(ivarfl(ipr), coefaf_p)
 call field_get_coefbf_s(ivarfl(ipr), coefbf_p)
 
 ! --- Physical quantities
-call field_get_val_s(icrom, crom)
-if (icalhy.eq.1.or.idilat.gt.1) then
+call field_get_val_s(icrom, crom_eos)
+if (icalhy.eq.1.or.idilat.gt.1.or.irovar.eq.1) then
   call field_get_val_prev_s(icrom, croma)
 endif
-call field_get_val_s(ibrom, brom)
+call field_get_val_s(ibrom, brom_eos)
+if (irovar.eq.1) then
+  call field_get_val_prev_s(ibrom, broma)
+endif
+
+if (irovar.eq.1) then
+  call field_get_id("density_mass", f_id)
+  call field_get_val_s(f_id, cpro_rho_mass)
+  call field_get_id("boundary_density_mass", f_id)
+  call field_get_val_s(f_id, bpro_rho_mass)
+
+  ! Time interpolated density
+  if (vcopt_u%thetav .lt. 1.d0 .and. iterns.gt.1) then
+    allocate(cpro_rho_tc(ncelet))
+    allocate(bpro_rho_tc(nfabor))
+
+    do iel = 1, ncelet
+      cpro_rho_tc(iel) = vcopt_u%thetav * cpro_rho_mass(iel) &
+        + (1.d0 - vcopt_u%thetav) * croma(iel)
+    enddo
+
+    crom => cpro_rho_tc
+
+    do ifac = 1, nfabor
+      bpro_rho_tc(ifac) = vcopt_u%thetav * bpro_rho_mass(ifac) &
+        + (1.d0 - vcopt_u%thetav) * broma(ifac)
+    enddo
+
+    brom => bpro_rho_tc
+
+  else
+    crom => cpro_rho_mass
+    brom => bpro_rho_mass
+  endif
+
+else
+  crom => crom_eos
+  brom => brom_eos
+endif
 
 call field_get_val_s(ivarfl(ipr), cvar_pr)
 
@@ -1290,10 +1331,13 @@ endif
 
 ! --- Source term associated to the mass aggregation
 if (idilat.eq.2.or.idilat.eq.3) then
+
+  ! Add source term
   do iel = 1, ncel
-    drom = crom(iel) - croma(iel)
+    drom = crom_eos(iel) - croma(iel)
     cpro_divu(iel) = cpro_divu(iel) + drom*cell_f_vol(iel)/dt(iel)
   enddo
+
 endif
 
 ! ---> Termes sources Lagrangien
@@ -1842,6 +1886,19 @@ else if (iand(vcopt_p%idften, ANISOTROPIC_DIFFUSION).ne.0) then
 
 endif
 
+! Update density (which is coherent with the mass)
+!-------------------------------------------------
+
+if (irovar.eq.1) then
+  do iel = 1, ncelet
+    cpro_rho_mass(iel) = crom_eos(iel)
+  enddo
+
+  do ifac = 1, nfabor
+    bpro_rho_mass(ifac) = brom_eos(ifac)
+  enddo
+endif
+
 !===============================================================================
 ! 6. Suppression of the mesh hierarchy
 !===============================================================================
@@ -2246,6 +2303,8 @@ if (ivofmt.ge.0.or.idilat.eq.4) then
   if (allocated(xunsro)) deallocate(xunsro)
   if (allocated(tpusro)) deallocate(tpusro)
 endif
+if (allocated(cpro_rho_tc)) deallocate(cpro_rho_tc)
+if (allocated(bpro_rho_tc)) deallocate(bpro_rho_tc)
 
 !--------
 ! Formats

commit bb74d4764ad0b4648ec219dc93fa2b3ad5e25e0c

Fix time stepping of VOF algorithm (broken by commit a2ddc3b, 2018-10-19) and set irovar to 1 for this type of calculation.
Done in internship of A. Banon.

src/base/addfld.f90
@@ -553,12 +553,12 @@ endif
 
 ! Density at the second previous time step for VOF algorithm
 ! or dilatable algorithm
-if (ivofmt.ge.0.or.idilat.gt.1.or.irovar.eq.1) then
+if (ivofmt.ge.0.or.(idilat.gt.1.and.ipredfl.eq.0).or.irovar.eq.1) then
   call field_set_n_previous(icrom, 2)
   call field_set_n_previous(ibrom, 2)
-  ! The density at the previous time step is required if
-  ! we perform a hydrostatic pressure correction (icalhy=1)
-else if (icalhy.eq.1.or.ipthrm.eq.1.or.ippmod(icompf).ge.0) then
+! The density at the previous time step is required if
+! we perform a hydrostatic pressure correction (icalhy=1)
+else if (icalhy.eq.1.or.ipthrm.eq.1.or.ippmod(icompf).ge.0.or.idilat.gt.1) then
   call field_set_n_previous(icrom, 1)
   call field_set_n_previous(ibrom, 1)
 endif

src/base/fldini.f90
@@ -728,7 +728,7 @@ call cs_parameters_set_n_buoyant_scalars
 ! For Low Mach and compressible (increment) algorithms, a particular care
 ! must be taken when dealing with density in the unsteady term in the velocity
 ! pressure loop
-if (irovar.eq.1.and.(idilat.gt.1.or.ippmod(icompf).eq.3)) then
+if (irovar.eq.1.and.(idilat.gt.1.or.ivofmt.ge.0.or.ippmod(icompf).eq.3)) then
   ! EOS density, imposed after the correction step, so we need
   ! to keep the previous one, which is in balance with the mass
   f_name = 'density_mass'

src/base/fldvar.f90
@@ -196,26 +196,33 @@ endif
 ! Cavitation: activate VoF
 if (icavit.ge.0.and.ivofmt.lt.0) ivofmt = 0
 
+! Mass balance equation options (pressure)
+
 call field_get_key_struct_var_cal_opt(ivarfl(ipr), vcopt)
-if (ippmod(icompf).ge.0) then
+
+! elliptic equation
+vcopt%iconv = 0
+
+if (ippmod(icompf).ge.0) then ! compressible algorithm
   vcopt%istat = 1
 else
   vcopt%istat = 0
 endif
-! VoF algorithm: activate the weighthening for the pressure
+
+! VoF algorithm: activate the weightening for the pressure
 if (ivofmt.ge.0) vcopt%iwgrec = 1
-vcopt%iconv = 0
+
 call field_set_key_struct_var_cal_opt(ivarfl(ipr), vcopt)
 
-! Void fraction (Volume of Fluid algorithm)
+! void fraction (VoF algorithm)
 if (ivofmt.ge.0) then
   call add_variable_field('void_fraction', 'Void Fraction', 1, ivolf2, iloc1)
   call field_get_key_struct_var_cal_opt(ivarfl(ivolf2), vcopt)
-  vcopt%idiff = 0
+  vcopt%idiff = 0  ! pure convection equation
   call field_set_key_struct_var_cal_opt(ivarfl(ivolf2), vcopt)
 endif
 
-! --- Turbulence
+! Turbulence
 
 if (itytur.eq.2) then
   call add_variable_field('k', 'Turb Kinetic Energy', 1, ik, iloc1)

src/base/modini.f90
@@ -146,9 +146,19 @@ endif
 
 ! Logging and postprocessing output
 
-if (irovar.eq.0.and.ivofmt.lt.0) then
+if (irovar.eq.0) then
   call hide_property(icrom)
   call field_set_key_int(ibrom, keylog, 0)
+else ! irovar.eq.1
+  call field_set_key_int(icrom, keylog, 1)
+  call field_set_key_int(icrom, keyvis, 1)
+endif
+
+if (ivivar.eq.0) then
+  call hide_property(iviscl)
+else ! ivivar.eq.1
+  call field_set_key_int(iviscl, keylog, 1)
+  call field_set_key_int(iviscl, keyvis, 1)
 endif
 
 if (idtvar.lt.0) then

src/base/navstv.f90
@@ -1390,7 +1390,7 @@ endif
 ! Update density (which is coherent with the mass)
 !-------------------------------------------------
 
-if (irovar.eq.1.and.(idilat.gt.1.or.ippmod(icompf).eq.3)) then
+if (irovar.eq.1.and.(idilat.gt.1.or.ivofmt.ge.0.or.ippmod(icompf).eq.3)) then
   do iel = 1, ncelet
     cpro_rho_mass(iel) = crom_eos(iel)
   enddo

src/base/resopv.f90
@@ -346,7 +346,7 @@ if (irovar.eq.1) then
   call field_get_val_prev_s(ibrom, broma)
 endif
 
-if (irovar.eq.1.and.(idilat.gt.1.or.ippmod(icompf).eq.3)) then
+if (irovar.eq.1.and.(idilat.gt.1.or.ivofmt.ge.0.or.ippmod(icompf).eq.3)) then
   call field_get_id("density_mass", f_id)
   call field_get_val_s(f_id, cpro_rho_mass)
   call field_get_id("boundary_density_mass", f_id)

src/base/schtmp.f90
@@ -120,7 +120,7 @@ if (iappel.eq.1) then
     call field_current_to_previous(icrom)
     call field_current_to_previous(ibrom)
 
-    if (idilat.gt.1.or.ippmod(icompf).eq.3) then
+    if (idilat.gt.1.or.ivofmt.ge.0.or.ippmod(icompf).eq.3) then
       ! Save the latest density field seen by the mass equation
       ! it will be updated in the correction step.
       call field_get_id("density_mass", f_id)

src/base/varpos.f90
@@ -103,6 +103,12 @@ pflag = POST_ON_LOCATION + POST_MONITOR
 ! Additional physical properties
 !===============================================================================
 
+if (ivofmt.ge.0) then
+  ! variable density
+  irovar = 1
+  ivivar = 1
+endif
+
 ! CP when variable
 if (icp.ge.0) then
   call add_property_field_1d('specific_heat', 'Specific Heat', icp)