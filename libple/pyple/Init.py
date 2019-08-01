2cd77c22f4db22a3120db44d48da2436ae5c038d
Chaï Koren <chai.koren@noreply.edf.fr>
2019-08-01 16:24:54 +0200

    Update and improve the PLE_COUPLING python bindings
      * SWIG is replaced by the native CPython wrapping
      * The python layer now contains python classes providing methods which
        call upon the corresponding C functions. This allows for a more
        pythonesque use of PLE by a Python code.
      * Binding requires the presence of Python and mpi4py
    
    cherry picked from commit 5d95768 on master

--- libple/pyple/Init.py
+++ libple/pyple/Init.py
@@ -1,4 +1,4 @@
-#-------------------------------------------------------------------------------
+# ==============================================================================
 #   This file is part of the "Parallel Location and Exchange" library,
 #   intended to provide mesh or particle-based code coupling services.
 #
@@ -17,7 +17,7 @@
 #   You should have received a copy of the GNU Lesser General Public
 #   License along with this library; if not, write to the Free Software
 #   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-#-------------------------------------------------------------------------------
+# ==============================================================================
 
 # ==============================================================================
 # MPI UTILITIES FOR EASIER USE BY EXTERNAL CODES
