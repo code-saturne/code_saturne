code-saturne/code_saturne

commit c3a833d6505dddf58cfda034d7d54ae00deccb6c

Major commit: First pass of merge between Cooling Towers module and atmospheric module.
This closes issue #14 on gitlab.
Humid air properties are moved to C and merged between the two modules.
The one of Cooling Towers have a larger scope and therefore are used for
the two modules.

Some renaming are performed:
 - function qsatliq -> cs_air_yw_sat
 - fortran variable itotwt -> iymw
 - function for air EOS cs_ctwr_* -> cs_air_*

src/atmo/atincl.f90
@@ -107,8 +107,8 @@ double precision, allocatable, dimension(:) :: nn
 ! 1.2 Pointers for the positions of the variables
 !------------------------------------------------
 !   Variables specific to the atmospheric physics:
-!> itotwt---> total water content (for humid atmosphere)
-integer, save :: itotwt
+!> total water content (for humid atmosphere)
+integer, save :: iymw
 !> intdrp---> total number of droplets (for humid atmosphere)
 integer, save :: intdrp
 
@@ -177,14 +177,8 @@ integer, save :: theo_interp
 !> reference pressure (to compute potential temp: 1.0d+5)
 double precision, save:: ps
 
-!> ratio gaz constant h2o/ dry air: 1.608d0
-double precision, save:: rvsra
-
 !> ratio Cp h2o/ dry air: 1.866d0
 double precision, save:: cpvcpa
-
-!> latent heat of evaporation: 2.501d+6
-double precision, save:: clatev
 
 !> temperature gradient for the standard atmosphere (-6.5d-03 K/m)
 double precision, save:: gammat
@@ -385,6 +379,118 @@ integer, save :: init_at_chem
 integer, save :: kopint
 
 !> \}
+
+!=============================================================================
+
+  interface
+
+    !---------------------------------------------------------------------------
+
+    !> \brief Calculation of the specific enthalpy of liquid water
+
+    !> \return specific enthalpy of liquid water
+
+    !> \param[in]  t_l  liquid water temperature (in Celsius)
+
+    function cs_liq_t_to_h(t_l) result(h_l) &
+        bind(C, name='cs_liq_t_to_h')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      real(c_double), value :: t_l
+      real(c_double) :: h_l
+    end function cs_liq_t_to_h
+
+    !---------------------------------------------------------------------------
+
+    !> \brief Calculation of the absolute humidity at saturation for a given temperature.
+
+    !> \param[in]  t_c  temperature (in Celsius)
+    !> \param[in]  p    pressure
+
+    function cs_air_x_sat(t_c, p) result(x_s) &
+        bind(C, name='cs_air_x_sat')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      real(c_double), value :: t_c, p
+      real(c_double) :: x_s
+    end function cs_air_x_sat
+
+    !---------------------------------------------------------------------------
+
+    !> \brief Calculation of the air water mass fraction at saturation for a given temperature.
+
+    !> \param[in]  t_c  temperature (in Celsius)
+    !> \param[in]  p    pressure
+
+    function cs_air_yw_sat(t_c, p) result(x_s) &
+        bind(C, name='cs_air_yw_sat')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      real(c_double), value :: t_c, p
+      real(c_double) :: x_s
+    end function cs_air_yw_sat
+
+    !---------------------------------------------------------------------------
+
+    !> \brief Computes the saturation water vapour pressure function of the temperature (C).
+
+    !> \param[in]  t_c  temperature (in Celsius)
+
+    function cs_air_pwv_sat(t_c) result(x_s) &
+        bind(C, name='cs_air_pwv_sat')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      real(c_double), value :: t_c
+      real(c_double) :: x_s
+    end function cs_air_pwv_sat
+
+    !---------------------------------------------------------------------------
+
+    !> \brief Convert the absolute humidity of humid air to the air water mass fraction.
+
+    !> \param[in]  x  absolute humidity of humid air
+
+    function cs_air_x_to_yw(x) result(qw) &
+        bind(C, name='cs_air_x_to_yw')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      real(c_double), value :: x
+      real(c_double) :: qw
+    end function cs_air_x_to_yw
+
+    !---------------------------------------------------------------------------
+
+    !> \brief Convert the air water mass fraction to the absolute humidity of humid air.
+
+    !> \param[in]  qw  air water mass fraction
+
+    function cs_air_yw_to_x(qw) result(x) &
+        bind(C, name='cs_air_yw_to_x')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      real(c_double), value :: qw
+      real(c_double) :: x
+    end function cs_air_yw_to_x
+
+    !---------------------------------------------------------------------------
+
+    !> \brief Calculation of the density of humid air.
+
+    !> \param[in]  qw  air water mass fraction
+    !> \param[in]  p    pressure
+    !> \param[in]  t_h  temperature (in Celsius)
+
+    function cs_rho_humidair(qw, p, t_h) result(rho_h) &
+        bind(C, name='cs_rho_humidair')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      real(c_double), value :: qw, p, t_h
+      real(c_double) :: rho_h
+    end function cs_rho_humidair
+
+    !=============================================================================
+
+  end interface
 
 contains
 

commit 01c657a5c48413f215612aa893c6345ecf3b2df6

[Cooling tower, Atmo] Fix bug introduced in commit c3a833d65
The Gaussian scheme still needs to be cleaned-up and move to C.
Auto update of the GUI has been done.

Minor differences on 60_HILL testcase, some differences in
23_WANG/WANGRSH test case (pressur of vapor at saturation is 0 when T
<-20°C).

bin/model/AtmosphericFlowsModel.py
@@ -171,7 +171,7 @@ class AtmosphericFlowsModel(Model):
         if model != AtmosphericFlowsModel.off:
 
             if model == AtmosphericFlowsModel.dry:
-                self.__removeScalar(node, 'total_water')
+                self.__removeScalar(node, 'ym_water')
                 self.__removeScalar(node, 'number_of_droplets')
                 self.__removeProperty(node, 'liquid_water')
                 self.__setProperty(node, 'RealTemp', 'real_temperature')
@@ -179,7 +179,7 @@ class AtmosphericFlowsModel(Model):
                     self.__fluidProp.setPropertyMode('density', 'predefined_law')
 
             elif model == AtmosphericFlowsModel.humid:
-                self.__setScalar(node, 'TotWater', 'total_water', 'model')
+                self.__setScalar(node, 'TotWater', 'ym_water', 'model')
                 self.__setScalar(node, 'TotDrop', 'number_of_droplets', 'model')
                 self.__setProperty(node, 'RealTemp', 'real_temperature')
                 self.__setProperty(node, 'LiqWater', 'liquid_water')
@@ -187,13 +187,13 @@ class AtmosphericFlowsModel(Model):
                     self.__fluidProp.setPropertyMode('density', 'predefined_law')
 
             elif model == AtmosphericFlowsModel.constant:
-                self.__removeScalar(node, 'total_water')
+                self.__removeScalar(node, 'ym_water')
                 self.__removeScalar(node, 'number_of_droplets')
                 self.__removeProperty(node, 'liquid_water')
                 FluidCharacteristicsModel(self.case).setPropertyMode('density', 'constant')
 
         else:
-            self.__removeScalar(node, 'total_water')
+            self.__removeScalar(node, 'ym_water')
             self.__removeScalar(node, 'number_of_droplets')
             self.__removeProperty(node, 'liquid_water')
 
@@ -313,7 +313,7 @@ class AtmosphericFlowsTestCase(ModelTest):
 
         doc = """<atmospheric_flows model="humid">
                     <read_meteo_data status="on">
-                        <variable label="total water" name="total_water" type="model"/>
+                        <variable label="total water" name="ym_water" type="model"/>
                         <variable label="number of droplets" name="number_of_droplets" type="model"/>
                         <property label="Real temp" name="real_temperature"/>
                         <property label="Liquid water" name="liquid_water"/>

bin/model/XMLinitialize.py
@@ -335,6 +335,10 @@ class XMLinit(BaseXmlInit):
             if from_vers[:3] < "6.0.0":
                 self.__backwardCompatibilityFrom_5_3()
 
+        if from_vers[:3] < "7.0.0":
+            if from_vers[:3] < "6.1.0":
+                self.__backwardCompatibilityFrom_6_0()
+
 
     def __backwardCompatibilityBefore_3_0(self):
         """
@@ -1643,6 +1647,27 @@ class XMLinit(BaseXmlInit):
                             nc.xmlChildsCopy(node)
                             node.xmlRemoveNode()
 
+    def __backwardCompatibilityFrom_6_0(self):
+        """
+        Change XML in order to ensure backward compatibility.
+        """
+
+        # Update due to Cooling Towers and Atmospheric Flows merge
+        XMLThermoPhysicalModelNode = self.case.xmlGetNode('thermophysical_models')
+        npr = XMLThermoPhysicalModelNode.xmlGetNode('atmospheric_flows')
+        if npr:
+            node = npr.xmlGetNode('variable', name="total_water")
+            if node:
+                node['name'] = "ym_water"
+
+        XMLAnaControl = self.case.xmlGetNode('analysis_control')
+        node_profiles = XMLAnaControl.xmlGetNode('profiles')
+        if node_profiles:
+            node_profile = node_profiles.xmlGetNode('profile')
+            if node_profile:
+                node = node_profile.xmlGetNode('var_prop', name="total_water")
+                if node:
+                    node['name'] = "ym_water"
 
     def _backwardCompatibilityCurrentVersion(self):
         """

src/atmo/atincl.f90
@@ -476,17 +476,20 @@ integer, save :: kopint
 
     !> \brief Calculation of the density of humid air.
 
-    !> \param[in]  qw  air water mass fraction
-    !> \param[in]  p    pressure
-    !> \param[in]  t_h  temperature (in Celsius)
+    !> \param[in]  ywm           air water mass fraction
+    !> \param[in]  t_liq         pressure
+    !> \param[in]  p             pressure
+    !> \param[out] yw_liq        liquid water mass fraction
+    !> \param[out] t_h           temperature of humid air in Celsius
+    !> \param[out] rho_h         density of humid air
 
-    function cs_rho_humidair(qw, p, t_h) result(rho_h) &
+    subroutine cs_rho_humidair(ywm, t_liq, p, yw_liq, t_h, rho_h) &
         bind(C, name='cs_rho_humidair')
       use, intrinsic :: iso_c_binding
       implicit none
-      real(c_double), value :: qw, p, t_h
-      real(c_double) :: rho_h
-    end function cs_rho_humidair
+      real(c_double), value :: ywm, t_liq, p
+      real(c_double), intent(out) :: yw_liq, t_h, rho_h
+    end subroutine cs_rho_humidair
 
     !=============================================================================
 

src/atmo/atphyv.f90
@@ -63,9 +63,9 @@ integer          ivart, iel
 double precision xvart, rhum, rscp, pp, zent
 double precision lrhum, lrscp
 double precision qsl, deltaq
-double precision qliq, qwt, tliq, dum
+double precision yw_liq, qwt, tliq, dum
 
-double precision, dimension(:), pointer :: brom, crom
+double precision, dimension(:), pointer :: crom
 double precision, dimension(:), pointer :: cvar_vart, cvar_totwt
 double precision, dimension(:), pointer :: cpro_tempc, cpro_liqwt
 
@@ -107,14 +107,15 @@ else
   call csexit (1)
 endif
 
-! --- Masse volumique
-
+! Density
 call field_get_val_s(icrom, crom)
-call field_get_val_s(ibrom, brom)
 
 call field_get_val_s(itempc,cpro_tempc)
 call field_get_val_s(ivarfl(ivart), cvar_vart)
-if (ippmod(iatmos).ge.2) call field_get_val_s(ivarfl(isca(iymw)), cvar_totwt)
+if (ippmod(iatmos).ge.2) then
+  call field_get_val_s(ivarfl(isca(iymw)), cvar_totwt)
+  call field_get_val_s(iliqwt,cpro_liqwt)
+endif
 
 ! From potential temperature, compute:
 ! - Temperature in Celsius
@@ -126,23 +127,14 @@ if (ippmod(iatmos).ge.2) call field_get_val_s(ivarfl(isca(iymw)), cvar_totwt)
 rhum = rair
 rscp = rair/cp0
 
-lrhum = rair
 lrscp = rair/cp0
 
 do iel = 1, ncel
 
-  xvart = cvar_vart(iel) !  The thermal scalar is potential temperature
-
-  if (ippmod(iatmos).ge.2) then  ! humid atmosphere
-    lrhum = rair*(1.d0 + (rvsra - 1.d0)*cvar_totwt(iel))
-    ! R/Cp
-    lrscp = (rair/cp0)*(1.d0 + (rvsra - cpvcpa)*                    &
-            cvar_totwt(iel))
-  endif
-
   zent = xyzcen(3,iel)
 
-  if (imeteo.eq.0) then
+  ! Reference pressure
+  if (imeteo.eq.0) then !FIXME useless...
     call atmstd(zent,pp,dum,dum)
   else
     ! Pressure profile from meteo file:
@@ -152,31 +144,39 @@ do iel = 1, ncel
          ztmet , tmmet , phmet , zent, ttcabs, pp )
   endif
 
-  ! Temperature in Celsius in cell centers:
-  ! ---------------------------------------
+  ! Potential temperature
+  ! or liquid potential temperature for humid atmosphere
+  xvart = cvar_vart(iel)
+
+  ! (liquid) temperature
   ! law: T = theta * (p/ps) ** (Rair/Cp0)
+  tliq = xvart*(pp/ps)**rscp
 
-  cpro_tempc(iel) = xvart*(pp/ps)**lrscp
-  cpro_tempc(iel) = cpro_tempc(iel) - tkelvi
+  ! Dry atmosphere, total water fraction
+  qwt = 0.d0
 
-  !   Density in cell centers:
-  !   ------------------------
-  !   law:    RHO       =   P / ( Rair * T(K) )
+  ! Humid atmosphere
+  if (ippmod(iatmos).ge.2) then
+    qwt = cvar_totwt(iel)
+  endif
 
-  crom(iel) = cs_rho_humidair(cvar_totwt(iel), pp, cpro_tempc(iel))
+  ! Density in cell centers:
+  ! ------------------------
+  ! law: rho = P / ( R_mixture * T_mixture(K) )
+
+  call cs_rho_humidair(qwt, tliq, pp, yw_liq, cpro_tempc(iel), crom(iel))
+
+  ! Humid atmosphere
+  if (ippmod(iatmos).ge.2) then
+    cpro_liqwt(iel) = yw_liq
+  endif
 
 enddo
 
-if (ippmod(iatmos).ge.2) then ! humid atmosphere physics
-
-  call field_get_val_s(iliqwt,cpro_liqwt)
-
-  if (moddis.eq.1)then ! all or nothing condensation scheme
-    call all_or_nothing()
-  elseif (moddis.ge.2)then ! gaussian subgrid condensation scheme
-    call gaussian()
-  endif
-endif ! (ippmod(iatmos).ge.2)
+! Gaussian subgrid condensation scheme for humid atmosphere physics
+if (ippmod(iatmos).ge.2.and. moddis.ge.2) then
+  call gaussian()
+endif
 
 ! User re-definition:
 call usatph ()
@@ -209,73 +209,11 @@ call usatph ()
 '@                                                            ',/)
 
 !----
-! FIN
+! End
 !----
 
 return
 contains
-
-! *******************************************************************
-!> \brief Internal function -
-!>      for all cells : initialise physical variables of the atmospheric module
-!-------------------------------------------------------------------------------
-subroutine all_or_nothing()
-
-lrhum = rhum
-
-do iel = 1, ncel
-
-  zent = xyzcen(3,iel)
-
-  if (imeteo.eq.0) then
-    call atmstd(zent,pp,dum,dum)
-  else
-    ! Pressure profile from meteo file:
-    call intprf &
-         !   ===========
-       ( nbmett, nbmetm,                                            &
-         ztmet , tmmet , phmet , zent, ttcabs, pp )
-  endif
-
-  xvart = cvar_vart(iel) ! thermal scalar: liquid potential temperature
-  tliq = xvart*(pp/ps)**rscp ! liquid temperature
-  qwt  = cvar_totwt(iel) !total water content
-  qsl = cs_air_yw_sat(tliq-tkelvi, pp) ! saturated vapor content
-  deltaq = qwt - qsl
-
-  if (activate) then
-    write(nfecra,*)"atphyv::all_or_nothing::xvart = ",xvart
-    write(nfecra,*)"atphyv::all_or_nothing::tliq = ",tliq
-    write(nfecra,*)"atphyv::all_or_nothing::qwt = ",qwt
-    write(nfecra,*)"atphyv::all_or_nothing::qsl = ",qsl
-    write(nfecra,*)"atphyv::all_or_nothing::qwt,qsl,deltaq = ",qwt,qsl,deltaq
-    write(nfecra,*)"atphyv::all_or_nothing::zc = ",xyzcen(3,iel)
-    write(nfecra,*)"atphyv::all_or_nothing::pp = ",pp
-    write(nfecra,*)"atphyv::all_or_nothing::p0 = ",ps
-    write(nfecra,*)"atphyv::all_or_nothing::zent = ",zent
-  endif
-
-  if (deltaq.le.0.d0) then ! unsaturated air parcel
-    !Celcius temperature of the air parcel
-    cpro_tempc(iel) = tliq - tkelvi
-    !liquid water content
-    cpro_liqwt(iel) = 0.d0
-    nebdia(iel) = 0.d0
-    nn(iel) = 0.d0
-  else ! saturated (ie. with liquid water) air parcel
-    qliq = deltaq/ &
-         (1.d0 + qsl*clatev**2/(rair*rvsra*cp0*tliq**2))
-    ! liquid water content
-    cpro_liqwt(iel) = qliq
-    ! Celcius temperature of the air parcel
-    cpro_tempc(iel) = tliq + (clatev/cp0)*qliq - tkelvi
-    nebdia(iel) = 1.d0
-    nn(iel) = 0.d0
-  endif
-  crom(iel) = cs_rho_humidair(qwt, pp, tliq-tkelvi)
-
-enddo ! iel = 1, ncel
-end subroutine all_or_nothing
 
 ! *******************************************************************
 !> \brief Internal function -
@@ -351,7 +289,7 @@ do iel = 1, ncel
   qsl = cs_air_yw_sat(tliq-tkelvi, pp) ! saturated vapor content
   deltaq = qwt - qsl
   alpha = (clatev*qsl/(rvap*tliq**2))*(pp/ps)**rscp
-  sig_flu = sqrt(var_q + alpha**2*var_tl - 2.d0*alpha*cov_tlq)
+  sig_flu = sqrt(var_q + alpha**2*var_tl - 2.d0*alpha*cov_tlq) !FIXME a^2 +b^2 -2ab = (a-b)^2, a = a_coeff dqsd; b = alpha dtlsd
 
   if (sig_flu.lt.1.d-30) sig_flu = 1.d-30
   q1 = deltaq/sig_flu
@@ -360,42 +298,42 @@ do iel = 1, ncel
 
   nebdia(iel) = 0.5d0*(1.d0 + ferf(q1/sqrt(2.d0)))
 
-  qliq = (sig_flu                                                               &
+  !FIXME MF : put in input of the global function...
+  yw_liq = (sig_flu                                                               &
         /(1.d0 + qsl*clatev**2/(rvap*cp0*tliq**2)))                             &
         *(nebdia(iel)*q1 + exp(-q1**2/2.d0)/sqrt(2.d0*pi))
-  qliq = max(qliq,1d-15)
+  yw_liq = max(yw_liq, 1d-15)
   nn(iel) = nebdia(iel) - (nebdia(iel)*q1                                       &
           + exp(-q1**2/2.d0)/sqrt(2.d0*pi))*exp(-q1**2/2.d0)/sqrt(2.d0*pi)
 
-  if(qwt.lt.qliq)then
-    ! go back to all or nothing
+  ! go back to all or nothing
+  if(qwt.lt.yw_liq)then
+
+    nn(iel) = 0.d0
+
+    ! deltaq set to 0 if unsaturted air parcel
     if (deltaq.le.0.d0) then ! unsaturated air parcel
-      !Celcius temperature of the air parcel
-      cpro_tempc(iel) = tliq - tkelvi
-      !density of the air parcel
-      !liquid water content
-      cpro_liqwt(iel) = 0.d0
+      deltaq = 0.d0
       nebdia(iel) = 0.d0
-      nn(iel) = 0.d0
     else ! saturated (ie. with liquid water) air parcel
-      qliq = deltaq / (1.d0 + qsl*clatev**2/(rair*rvsra*cp0*tliq**2))
-      ! liquid water content
-      cpro_liqwt(iel) = qliq
-      ! Celcius temperature of the air parcel
-      cpro_tempc(iel) = tliq+(clatev/cp0)*qliq - tkelvi
       nebdia(iel) = 1.d0
-      nn(iel) = 0.d0
     endif
-    crom(iel) = cs_rho_humidair(qwt, pp, tliq-tkelvi)
-  else ! coherent subgrid diagnostic
-    lrhum = rair*(1.d0 - qliq + (rvsra - 1.d0)*(qwt - qliq))
-    ! liquid water content
-    cpro_liqwt(iel) = qliq
-    !Celcius temperature of the air parcel
-    cpro_tempc(iel) = tliq + (clatev/cp0)*qliq - tkelvi
-    !density
-    crom(iel) = pp/(lrhum*(tliq + (clatev/cp0)*qliq)) !FIXME use property function of humid air at saturation?
-  endif ! qwt.lt.qliq
+
+    ! TODO input ?
+    ! 0 if unsaturated air parcel
+    yw_liq = deltaq / (1.d0 + qsl*clatev**2/(rvap*cp0*tliq**2))
+  endif ! qwt.lt.yw_liq
+
+  cpro_liqwt(iel) = yw_liq
+  ! Celcius temperature of the air parcel
+  cpro_tempc(iel) = tliq + (clatev/cp0)*yw_liq - tkelvi
+
+  !FIXME back to the previous formulation
+  call cs_rho_humidair(qwt, tliq, pp,   &
+                       cpro_liqwt(iel), &
+                       cpro_tempc(iel), &
+                       crom(iel))
+
 enddo
 
 ! when properly finished deallocate dtlsd

src/atmo/cs_air_props.c
@@ -43,6 +43,7 @@
 #include "bft_mem.h"
 
 #include "cs_base.h"
+#include "cs_math.h"
 #include "cs_physical_constants.h"
 
 /*----------------------------------------------------------------------------
@@ -458,7 +459,7 @@ cs_air_x_sat(const cs_real_t  t_c,
  *
  * \return the air water mass fraction at saturation
  *
- * \param[in]     t_c            temperature in Celsius degree
+ * \param[in]     t_c          temperature in Celsius degree
  * \param[in]     p            reference pressure
  */
 /*----------------------------------------------------------------------------*/
@@ -603,22 +604,27 @@ cs_air_yw_to_x(const cs_real_t  qw)
 /*!
  * \brief Calculation of the density of humid air
  *
- * \return density of humid air
- *
- * \param[in]     qwt           air water mass fraction
+ * \param[in]     ywm           air water mass fraction
+ * \param[in]     t_liq         temperature computed from
+ *                              liquid potential temperature (K)
  * \param[in]     p             pressure
- * \param[in]     t_h           temperature of humid air in Celsius
+ * \param[out]    yw_liq        liquid water mass fraction
+ * \param[out]    t_h           temperature of humid air in Celsius
+ * \param[out]    rho_h         density of humid air
  */
 /*----------------------------------------------------------------------------*/
 
-cs_real_t
-cs_rho_humidair(const cs_real_t qwt,
+void
+cs_rho_humidair(const cs_real_t ywm, //TODO rename yw_h
+                const cs_real_t t_liq,
                 const cs_real_t p,
-                const cs_real_t t_h)
+                cs_real_t      *yw_liq,
+                cs_real_t      *t_h,
+                cs_real_t      *rho_h)
 {
-  cs_real_t rho_h;
-  cs_real_t tkelvi = cs_physical_constants_celsius_to_kelvin;
   const cs_fluid_properties_t *phys_pro = cs_get_glob_fluid_properties();
+
+  /* Mixture equivalent R perfect gas constant */
   cs_real_t lrhum;
 
   cs_real_t rair = phys_pro->r_pg_cnst;
@@ -626,23 +632,33 @@ cs_rho_humidair(const cs_real_t qwt,
   cs_real_t clatev = phys_pro->clatev;
   cs_real_t cp0 = phys_pro->cp0;
 
-  cs_real_t qsl = cs_air_yw_sat(t_h, p);         // saturated vapor content
-  cs_real_t deltaq = qwt - qsl;
-  cs_real_t t_h_k = t_h + tkelvi;         // temperature in kelvin
+  /* Saturated vapor content */
+  cs_real_t yw_sat = cs_air_yw_sat(*t_h, p);
+  cs_real_t delta_yw = ywm - yw_sat;
 
-  if (deltaq <= 0.) {                         // unsaturated air parcel
-    lrhum         = rair*(1. + (rvsra - 1.)*qwt);
-    // density of the air parcel
-    rho_h = p/(lrhum*t_h_k);
-    }
-  else {                                 // saturated (ie. with liquid water) air parcel
-    cs_real_t qliq = deltaq/(1. + qsl*pow(clatev,2.)/(rair*rvsra*cp0*pow(t_h_k,2.)));
-    lrhum = rair*(1. - qliq + (rvsra - 1.)*(qwt - qliq));
-    // density
-    rho_h = p/(lrhum*(t_h_k + (clatev/cp0)*qliq));
+  /* Temperature of the mixture in Kelvin */
+  cs_real_t t_h_k = t_liq;
+
+  /* Density of the air parcel
+   * ------------------------- */
+
+  /* Unsaturated air parcel */
+  if (delta_yw <= 0.) {
+    lrhum = rair*(1. + (rvsra - 1.)*ywm);
+    *yw_liq = 0.;
+  }
+  /* Saturated (ie. with liquid water) air parcel */
+  else {
+    *yw_liq = delta_yw
+      / (1. + yw_sat * cs_math_pow2(clatev) / (rair*rvsra*cp0*cs_math_pow2(t_h_k))); //FIXME rair * rvsra =rvap
+    lrhum = rair*(1. + (rvsra - 1.)*(ywm - *yw_liq) -* yw_liq);
+    t_h_k += (clatev/cp0) * *yw_liq;
   }
 
-  return rho_h;
+  /* Temperature of the mixture in Celsius */
+  *t_h = t_h_k - cs_physical_constants_celsius_to_kelvin;
+  /* Perfect gas law */
+  *rho_h = p / (lrhum * t_h_k);
 }
 
 /*----------------------------------------------------------------------------*/

src/atmo/cs_air_props.h
@@ -267,18 +267,23 @@ cs_air_yw_to_x(const cs_real_t  qw);
 /*!
  * \brief Calculation of the density of humid air
  *
- * \return density of humid air
- *
- * \param[in]     qwt           air water mass fraction
+ * \param[in]     ywm           air water mass fraction
+ * \param[in]     t_liq         temperature  computed from
+ *                              liquid potential temperature (K)
  * \param[in]     p             pressure
- * \param[in]     t_h           temperature of humid air in Celsius
+ * \param[out]    yw_liq        liquid water mass fraction
+ * \param[out]    t_h           temperature of humid air in Celsius
+ * \param[out]    rho_h         density of humid air
  */
 /*----------------------------------------------------------------------------*/
 
-cs_real_t
-cs_rho_humidair(const cs_real_t qwt,
+void
+cs_rho_humidair(const cs_real_t ywm, //TODO rename yw_h
+                const cs_real_t t_liq,
                 const cs_real_t p,
-                const cs_real_t t_h);
+                cs_real_t      *yw_liq,
+                cs_real_t      *t_h,
+                cs_real_t      *rho_h);
 
 /*----------------------------------------------------------------------------*/
 /*!