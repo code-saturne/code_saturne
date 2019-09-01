code-saturne/code_saturne

commit 66fd4db14efb85f025eff23ddbc6788a5c07d912

[ALE] use MEG for BC formulae.
In the GUI, 'mesh_velocity_U/V/W' are renammed 'mesh_velocity[0/1/2]'
and 'mesh_x/y/z' are renammed 'mesh_displacement[0/1/2]'. Xml update is
done.

For CDO solver, vertices values are "hard coded" (only face value is
copied without weightening between two faces).
MEG version for nodes would be smarter or at least a sum (which requires
a synchro...).

bin/model/XMLinitialize.py
@@ -1679,6 +1679,26 @@ class XMLinit(BaseXmlInit):
                 node = node_time_average.xmlGetNode('var_prop', name="total_water")
                 if node:
                     node['name'] = "ym_water"
+        # Update for ALE using MEG
+        XMLBoundaryNode = self.case.xmlGetNode('boundary_conditions')
+        for node in XMLBoundaryNode.xmlGetNodeList('ale'):
+            nn = node.xmlGetNode('formula')
+            if nn:
+                content = nn.xmlGetTextNode()
+                # Substitute only perfectly matching labels (between '\\b' and '\\b')
+                pattern = '\\bmesh_velocity_U\\b'
+                content = re.sub(pattern, 'mesh_velocity[0]', content)
+                pattern = '\\bmesh_velocity_V\\b'
+                content = re.sub(pattern, 'mesh_velocity[1]', content)
+                pattern = '\\bmesh_velocity_W\\b'
+                content = re.sub(pattern, 'mesh_velocity[2]', content)
+                pattern = '\\bmesh_x\\b'
+                content = re.sub(pattern, 'mesh_displacement[0]', content)
+                pattern = '\\bmesh_y\\b'
+                content = re.sub(pattern, 'mesh_displacement[1]', content)
+                pattern = '\\bmesh_z\\b'
+                content = re.sub(pattern, 'mesh_displacement[2]', content)
+                nn.xmlSetTextNode(content)
 
 
     def _backwardCompatibilityCurrentVersion(self):

bin/cs_mei_to_c.py
@@ -2045,10 +2045,36 @@ class mei_to_c_interpreter:
             tm = TurbulenceModel(self.case)
 
             for zone in blm.getZones():
-                if zone._nature == "symmetry":
-                    continue
 
                 boundary = Boundary(zone._nature, zone._label, self.case)
+
+                # ALE: imposed mesh velocity
+                c = boundary.getALEChoice()
+                if c == "fixed_velocity":
+                    sym = ['x', 'y', 'z', 't', 'dt', 'iter', 'surface']
+                    for (name, val) in NotebookModel(self.case).getNotebookList():
+                        sym.append((name, 'value (notebook) = ' + str(val)))
+                    req = ['mesh_velocity[0]', 'mesh_velocity[1]', 'mesh_velocity[2]']
+                    exp = boundary.getALEFormula()
+
+                    name = 'mesh_velocity'
+                    self.init_block('bnd', zone._label, name,
+                                    exp, req, sym, known_fields=[],
+                                    condition=c)
+                elif c == "fixed_displacement":
+                    sym = ['x', 'y', 'z', 't', 'dt', 'iter', 'surface']
+                    for (name, val) in NotebookModel(self.case).getNotebookList():
+                        sym.append((name, 'value (notebook) = ' + str(val)))
+                    req = ['mesh_displacement[0]', 'mesh_displacement[1]', 'mesh_displacement[2]']
+                    exp = boundary.getALEFormula()
+
+                    name = 'mesh_velocity'
+                    self.init_block('bnd', zone._label, name,
+                                    exp, req, sym, known_fields=[],
+                                    condition=c)
+
+                if zone._nature == "symmetry":
+                    continue
 
                 # Velocity for inlets
                 if 'inlet' in zone._nature and zone._nature != 'free_inlet_outlet':

bin/model/Boundary.py
@@ -178,8 +178,8 @@ class Boundary(object) :
 
         formula_velocity = 'ale_formula_' + "fixed_velocity"
         formula_displacement = 'ale_formula_' + "fixed_displacement"
-        self._defaultValues[formula_velocity] = 'mesh_velocity_U = 0;\nmesh_velocity_V = 0;\nmesh_velocity_W = 0;'
-        self._defaultValues[formula_displacement] = 'mesh_x = 0;\nmesh_y = 0;\nmesh_z = 0;'
+        self._defaultValues[formula_velocity] = 'mesh_velocity[0] = 0;\nmesh_velocity[1] = 0;\nmesh_velocity[2] = 0;'
+        self._defaultValues[formula_displacement] = 'mesh_displacement[0] = 0;\nmesh_displacement[1] = 0;\nmesh_displacement[2] = 0;'
 
 
     @Variables.noUndo
@@ -216,7 +216,7 @@ class Boundary(object) :
 
 
     @Variables.noUndo
-    def getFormula(self):
+    def getALEFormula(self):
         """
         Get the formula from the xml
         """

gui/Pages/BoundaryConditionsMobileMeshView.py
@@ -53,7 +53,7 @@ from code_saturne.model.MobileMeshModel import MobileMeshModel
 from code_saturne.model.LocalizationModel import LocalizationModel, Zone
 from code_saturne.model.Boundary import Boundary
 
-from code_saturne.Pages.QMeiEditorView import QMeiEditorView
+from code_saturne.Pages.QMegEditorView import QMegEditorView
 from code_saturne.model.NotebookModel import NotebookModel
 
 #-------------------------------------------------------------------------------
@@ -113,37 +113,45 @@ class BoundaryConditionsMobileMeshView(QWidget, Ui_BoundaryConditionsMobileMeshF
         """
         Run formula editor.
         """
-        exp = self.__boundary.getFormula()
-        aleChoice = self.__boundary.getALEChoice();
+        exp = self.__boundary.getALEFormula()
+        c = self.__boundary.getALEChoice();
 
-        if aleChoice == "fixed_velocity":
+        if c == "fixed_velocity":
             if not exp:
-                exp = 'mesh_velocity_U ='
-            req = [('mesh_velocity_U', 'Fixed velocity of the mesh'),
-                   ('mesh_velocity_V', 'Fixed velocity of the mesh'),
-                   ('mesh_velocity_W', 'Fixed velocity of the mesh')]
-            exa = 'mesh_velocity_U = 1000;\nmesh_velocity_V = 1000;\nmesh_velocity_W = 1000;'
-        elif aleChoice == "fixed_displacement":
+                exp = 'mesh_velocity[0] = 0.;\nmesh_velocity[1] = 0.;\nmesh_velocity[2] = 0.;'
+            req = [('mesh_velocity[0]', 'Fixed velocity of the mesh'),
+                   ('mesh_velocity[1]', 'Fixed velocity of the mesh'),
+                   ('mesh_velocity[2]', 'Fixed velocity of the mesh')]
+            exa = 'mesh_velocity[0] = 0.;\nmesh_velocity[1] = 0.;\nmesh_velocity[2] = 1.;'
+        elif c == "fixed_displacement":
             if not exp:
-                exp = 'mesh_x ='
-            req = [('mesh_x', 'Fixed displacement of the mesh'),
-                   ('mesh_y', 'Fixed displacement of the mesh'),
-                   ('mesh_z', 'Fixed displacement of the mesh')]
-            exa = 'mesh_x = 1000;\nmesh_y = 1000;\nmesh_z = 1000;'
+                exp = 'mesh_displacement[0] = 0.;\nmesh_displacement[1] = 0.;\nmesh_displacement[2] = 0.;'
+            req = [('mesh_displacement[0]', 'Fixed displacement of the mesh'),
+                   ('mesh_displacement[1]', 'Fixed displacement of the mesh'),
+                   ('mesh_displacement[2]', 'Fixed displacement of the mesh')]
+            exa = 'mesh_displacement[0] = 0.;\nmesh_displacement[1] = 0.;\nmesh_displacement[2] = 1.;'
 
-        symbs = [('dt', 'time step'),
-                 ('t', 'current time'),
-                 ('iter', 'number of iteration')]
+        sym = [('x', "X face's gravity center"),
+               ('y', "Y face's gravity center"),
+               ('z', "Z face's gravity center"),
+               ('dt', 'time step'),
+               ('t', 'current time'),
+               ('iter', 'number of iteration'),
+               ('surface', 'Boundary zone surface')]
 
         for (nme, val) in self.notebook.getNotebookList():
-            symbs.append((nme, 'value (notebook) = ' + str(val)))
+            sym.append((nme, 'value (notebook) = ' + str(val)))
 
-        dialog = QMeiEditorView(self,
-                                check_syntax = self.case['package'].get_check_syntax(),
-                                expression = exp,
-                                required   = req,
-                                symbols    = symbs,
-                                examples   = exa)
+        dialog = QMegEditorView(parent = self,
+                                function_type = 'bnd',
+                                zone_name     = self.__boundary._label,
+                                variable_name = 'mesh_velocity',
+                                expression    = exp,
+                                required      = req,
+                                symbols       = sym,
+                                condition     = c,
+                                examples      = exa)
+
         if dialog.exec_():
             result = dialog.get_result()
             log.debug("slotFormulaMobileMeshBoundary -> %s" % str(result))
@@ -163,7 +171,7 @@ class BoundaryConditionsMobileMeshView(QWidget, Ui_BoundaryConditionsMobileMeshF
             return
 
         self.__boundary.setALEChoice(modelData)
-        exp = self.__boundary.getFormula()
+        exp = self.__boundary.getALEFormula()
 
         # Hide/Show formula button.
         # Formula is always reset when changing values, so set

src/base/cs_ale.c
@@ -512,12 +512,12 @@ _update_bcs(const cs_domain_t  *domain,
 
     case CS_BOUNDARY_ALE_IMPOSED_VEL:
       {
-        /* Retrieve the velocity to enforce */
-        cs_real_3_t vel = {0., 0., 0.};
-        cs_gui_mobile_mesh_get_fixed_velocity(z->name, vel);
+        /* Retrieve the velocities to enforce  on faces */
+        cs_real_t *bc_vals = cs_gui_mobile_mesh_get_fixed_velocity(z->name);
 
         assert(select_id < _cdo_bc->n_selections);
 
+#if 0 //TODO when we will have meg on vertices
         /* Loop on selected border vertices */
         for (cs_lnum_t i = 0; i < _cdo_bc->n_vertices[select_id]; i++) {
 
@@ -526,19 +526,37 @@ _update_bcs(const cs_domain_t  *domain,
           _val[0] = vel[0];
           _val[1] = vel[1];
           _val[2] = vel[2];
-
-
         }
+#endif
 
-        /* Loop over boundary faces for the fluid velocity */
+        /* Loop over boundary faces */
         for (cs_lnum_t elt_id = 0; elt_id < z->n_elts; elt_id++) {
           const cs_lnum_t face_id = z->elt_ids[elt_id];
 
+          /* ALE BC on vertices */
+          const cs_lnum_t s = m->b_face_vtx_idx[face_id];
+          const cs_lnum_t e = m->b_face_vtx_idx[face_id+1];
+
+          /* Compute the portion of surface associated to v_id_1 */
+          for (cs_lnum_t k = s; k < e; k++) {
+
+            const cs_lnum_t v_id = m->b_face_vtx_lst[k];
+            cs_real_t *_val = _cdo_bc->vtx_values + 3 * v_id;
+
+            //FIXME prorata
+            for (int d = 0; d < 3; d++)
+              _val[d] = bc_vals[elt_id + d * z->n_elts];
+
+          }
+
+          /* fluid velocity BC */
           ale_bc_type[face_id] = CS_ALE_IMPOSED_VEL;
-          b_fluid_vel[face_id][0] = vel[0];
-          b_fluid_vel[face_id][1] = vel[1];
-          b_fluid_vel[face_id][2] = vel[1];
+          for (int d = 0; d < 3; d++)
+            b_fluid_vel[face_id][d] = bc_vals[elt_id + d * z->n_elts];
         }
+
+        /* Free memory */
+        BFT_FREE(bc_vals);
 
         select_id++;
       }

src/gui/cs_gui_mobile_mesh.c
@@ -59,6 +59,7 @@
 #include "cs_gui_variables.h"
 #include "cs_gui_boundary_conditions.h"
 #include "cs_mesh.h"
+#include "cs_prototypes.h"
 #include "cs_timer.h"
 #include "cs_time_step.h"
 
@@ -227,30 +228,19 @@ _get_ale_boundary_formula(cs_tree_node_t  *tn_w,
  *
  * parameters:
  *   tn_w           <-- pointer to tree node for a given mobile_boundary BC
- *   begin          <-- begin index for nodfbr
- *   end            <-- end index for nodfbr
- *   nnod           <-> number of nodes
- *   b_face_vtx_lst <-- nodfbr
+ *   z              <-- selected boundary zone
  *   impale         --> impale
  *   disale         --> disale
- *   dtref          <-- time step
- *   ttcabs         <-- current time
- *   ntcabs         <-- current iteration number
  *----------------------------------------------------------------------------*/
 
 static void
 _uialcl_fixed_displacement(cs_tree_node_t   *tn_w,
-                           const cs_lnum_t   begin,
-                           const cs_lnum_t   end,
-                           const cs_lnum_t   b_face_vtx_lst[],
+                           const cs_zone_t  *z,
                            int              *impale,
-                           cs_real_3_t      *disale,
-                           const double      dtref,
-                           const double      ttcabs,
-                           const int         ntcabs)
+                           cs_real_3_t      *disale)
 {
-  const char*  variables[3] = {"mesh_x", "mesh_y", "mesh_z"};
-  int variable_nbr = 3;
+
+  const cs_mesh_t *m = cs_glob_mesh;
 
   /* Get formula */
   const char *formula = _get_ale_boundary_formula(tn_w, "fixed_displacement");
@@ -260,27 +250,30 @@ _uialcl_fixed_displacement(cs_tree_node_t   *tn_w,
               _("Boundary nature formula is null for %s."),
               cs_gui_node_get_tag(tn_w, "label"));
 
-  /* Init mei */
-  mei_tree_t *ev = _init_mei_tree(formula, variables, variable_nbr,
-                                  0, 0, 0, dtref, ttcabs, ntcabs);
+  /* Evaluate formula using meg */
+  cs_real_t *bc_vals = cs_meg_boundary_function(z,
+                                                "mesh_velocity",
+                                                "fixed_displacement");
 
-  mei_evaluate(ev);
+  /* Loop over boundary faces */
+  for (cs_lnum_t elt_id = 0; elt_id < z->n_elts; elt_id++) {
+    const cs_lnum_t face_id = z->elt_ids[elt_id];
 
-  /* Get mei results */
-  cs_real_t x_mesh = mei_tree_lookup(ev, "mesh_x");
-  cs_real_t y_mesh = mei_tree_lookup(ev, "mesh_y");
-  cs_real_t z_mesh = mei_tree_lookup(ev, "mesh_z");
+    /* ALE BC on vertices */
+    const cs_lnum_t s = m->b_face_vtx_idx[face_id];
+    const cs_lnum_t e = m->b_face_vtx_idx[face_id+1];
 
-  mei_tree_destroy(ev);
+    /* Compute the portion of surface associated to v_id_1 */
+    for (cs_lnum_t k = s; k < e; k++) {
 
-  /* Set disale and impale */
-  for (cs_lnum_t ii = begin; ii < end; ++ii) {
-    cs_lnum_t inod = b_face_vtx_lst[ii];
-    if (impale[inod] == 0) {
-      disale[inod][0] = x_mesh;
-      disale[inod][1] = y_mesh;
-      disale[inod][2] = z_mesh;
-      impale[inod] = 1;
+      const cs_lnum_t v_id = m->b_face_vtx_lst[k];
+      cs_real_t *_val = disale + 3 * v_id;
+
+      impale[v_id] = 1;
+      //FIXME prorata
+      for (int d = 0; d < 3; d++)
+        _val[d] = bc_vals[elt_id + d * z->n_elts];
+
     }
   }
 }
@@ -294,12 +287,8 @@ _uialcl_fixed_displacement(cs_tree_node_t   *tn_w,
  *   ivma         <-- ivma
  *   iwma         <-- iwma
  *   nfabor       <-- Number of boundary faces
- *   n_faces      <-- number of selected faces
- *   faces_list   <-- listof selected faces
+ *   z            <-- selected boundary zone
  *   rcodcl       --> rcodcl
- *   dtref        <-- time step
- *   ttcabs       <-- current time
- *   ntcabs       <-- current iteration number
  *----------------------------------------------------------------------------*/
 
 static void
@@ -309,18 +298,10 @@ _uialcl_fixed_velocity(cs_tree_node_t  *tn_w,
                        int              iwma,
                        int              ivimpo,
                        cs_lnum_t        nfabor,
-                       cs_lnum_t        n_faces,
-                       const cs_lnum_t  faces_list[],
+                       const cs_zone_t *z,
                        int              ialtyb[],
-                       double          *rcodcl,
-                       double           dtref,
-                       double           ttcabs,
-                       int              ntcabs)
+                       double          *rcodcl)
 {
-  const char*  variables[3] = {"mesh_velocity_U",
-                               "mesh_velocity_V",
-                               "mesh_velocity_W" };
-
   /* Get formula */
   const char *formula = _get_ale_boundary_formula(tn_w, "fixed_velocity");
 
@@ -329,26 +310,28 @@ _uialcl_fixed_velocity(cs_tree_node_t  *tn_w,
               _("Boundary nature formula is null for %s."),
               cs_gui_node_get_tag(tn_w, "label"));
 
-  /* Init MEI */
-  mei_tree_t *ev = _init_mei_tree(formula, variables, 3, 0, 0, 0,
-                                  dtref, ttcabs, ntcabs);
+  /* Evaluate formula using meg */
+  cs_real_t *bc_vals = cs_meg_boundary_function(z,
+                                                "mesh_velocity",
+                                                "fixed_velocity");
 
-  for (cs_lnum_t ifac = 0; ifac < n_faces; ifac++) {
+  /* Loop over boundary faces */
+  for (cs_lnum_t elt_id = 0; elt_id < z->n_elts; elt_id++) {
+    const cs_lnum_t face_id = z->elt_ids[elt_id];
 
-    cs_lnum_t ifbr = faces_list[ifac];
-
-    mei_evaluate(ev);
 
     /* Fill  rcodcl */
-    rcodcl[(iuma-1) * nfabor + ifbr] = mei_tree_lookup(ev, "mesh_velocity_U");
-    rcodcl[(ivma-1) * nfabor + ifbr] = mei_tree_lookup(ev, "mesh_velocity_V");
-    rcodcl[(iwma-1) * nfabor + ifbr] = mei_tree_lookup(ev, "mesh_velocity_W");
+    rcodcl[(iuma-1) * nfabor + face_id] = bc_vals[elt_id + 0 * z->n_elts];
+    rcodcl[(ivma-1) * nfabor + face_id] = bc_vals[elt_id + 1 * z->n_elts];
+    rcodcl[(iwma-1) * nfabor + face_id] = bc_vals[elt_id + 2 * z->n_elts];
 
-    ialtyb[ifbr]  = ivimpo;
+    ialtyb[face_id]  = ivimpo;
 
   }
 
-  mei_tree_destroy(ev);
+  /* Free memory */
+  BFT_FREE(bc_vals);
+
 }
 
 /*-----------------------------------------------------------------------------
@@ -792,28 +775,16 @@ void CS_PROCF (uialcl, UIALCL) (const int *const    ibfixe,
       }
     }
     else if (nature == ale_boundary_nature_fixed_displacement) {
-      t0 = cs_timer_wtime();
-      for (cs_lnum_t ifac = 0; ifac < n_faces; ifac++) {
-        cs_lnum_t ifbr = faces_list[ifac];
-        _uialcl_fixed_displacement(tn_bc,
-                                   m->b_face_vtx_idx[ifbr],
-                                   m->b_face_vtx_idx[ifbr+1],
-                                   m->b_face_vtx_lst, impale, disale,
-                                   cs_glob_time_step->dt_ref,
-                                   cs_glob_time_step->t_cur,
-                                   cs_glob_time_step->nt_cur);
-      }
-      cs_gui_add_mei_time(cs_timer_wtime() - t0);
+      _uialcl_fixed_displacement(tn_bc,
+                                 z,
+                                 impale,
+                                 disale);
     }
     else if (nature == ale_boundary_nature_fixed_velocity) {
-      t0 = cs_timer_wtime();
       _uialcl_fixed_velocity(tn_bc, *iuma, *ivma, *iwma, *ivimpo,
-                             m->n_b_faces, n_faces, faces_list,
-                             ialtyb, rcodcl,
-                             cs_glob_time_step->dt_ref,
-                             cs_glob_time_step->t_cur,
-                             cs_glob_time_step->nt_cur);
-      cs_gui_add_mei_time(cs_timer_wtime() - t0);
+                             m->n_b_faces,
+                             z,
+                             ialtyb, rcodcl);
     }
   }
 }
@@ -1151,28 +1122,13 @@ cs_gui_mobile_mesh_get_boundaries(cs_domain_t     *domain)
 
     /* TODO */
     /* else if (nature == ale_boundary_nature_fixed_displacement) { */
-    /*   t0 = cs_timer_wtime(); */
-    /*   for (cs_lnum_t ifac = 0; ifac < n_faces; ifac++) { */
-    /*     cs_lnum_t ifbr = faces_list[ifac]; */
-    /*     _uialcl_fixed_displacement(tn_bndy, */
-    /*                                m->b_face_vtx_idx[ifbr], */
-    /*                                m->b_face_vtx_idx[ifbr+1], */
-    /*                                m->b_face_vtx_lst, impale, disale, */
-    /*                                cs_glob_time_step->dt_ref, */
-    /*                                cs_glob_time_step->t_cur, */
-    /*                                cs_glob_time_step->nt_cur); */
-    /*   } */
-    /*   cs_gui_add_mei_time(cs_timer_wtime() - t0); */
+    /*   _uialcl_fixed_displacement(tn_bndy, z, */
+    /*                              impale, disale); */
     /* } */
     /* else if (nature == ale_boundary_nature_fixed_velocity) { */
-    /*   t0 = cs_timer_wtime(); */
     /*   _uialcl_fixed_velocity(tn_bndy, *iuma, *ivma, *iwma, *ivimpo, */
-    /*                          m->n_b_faces, n_faces, faces_list, */
-    /*                          ialtyb, rcodcl, */
-    /*                          cs_glob_time_step->dt_ref, */
-    /*                          cs_glob_time_step->t_cur, */
-    /*                          cs_glob_time_step->nt_cur); */
-    /*   cs_gui_add_mei_time(cs_timer_wtime() - t0); */
+    /*                          m->n_b_faces, z, */
+    /*                          ialtyb, rcodcl); */
     /* } */
 
 }
@@ -1182,17 +1138,13 @@ cs_gui_mobile_mesh_get_boundaries(cs_domain_t     *domain)
  * \brief Return the fixed velocity for a boundary
  *
  * \param[in]  label boundary condition label
- * \param[out] vel   imposed mesh velocity
+ *
+ * \return a pointer to an array of cs_real_t values
  *----------------------------------------------------------------------------*/
 
-void
-cs_gui_mobile_mesh_get_fixed_velocity(const char*    label,
-                                      cs_real_3_t    vel)
+cs_real_t *
+cs_gui_mobile_mesh_get_fixed_velocity(const char    *label)
 {
-  cs_real_t dtref = cs_glob_time_step->dt_ref;
-  cs_real_t ttcabs = cs_glob_time_step->t_cur;
-  int ntcabs = cs_glob_time_step->nt_cur;
-
   cs_tree_node_t *tn_b0 = cs_tree_get_node(cs_glob_tree, "boundary_conditions");
 
   /* Loop on boundary zones */
@@ -1212,9 +1164,6 @@ cs_gui_mobile_mesh_get_fixed_velocity(const char*    label,
 
     if (strcmp(label_bndy, label) == 0) {
 
-      const char*  variables[3] = {"mesh_velocity_U",
-                                   "mesh_velocity_V",
-                                   "mesh_velocity_W" };
 
       /* Get formula */
       const char *formula = _get_ale_boundary_formula(tn, "fixed_velocity");
@@ -1224,20 +1173,17 @@ cs_gui_mobile_mesh_get_fixed_velocity(const char*    label,
                   _("Boundary nature formula is null for %s."),
                   cs_gui_node_get_tag(tn, "label"));
 
-      /* Init MEI */
-      mei_tree_t *ev = _init_mei_tree(formula, variables, 3, 0, 0, 0,
-                                      dtref, ttcabs, ntcabs);
+      const cs_zone_t *bz = cs_boundary_zone_by_name(label);
 
-      mei_evaluate(ev);
-
-      vel[0] = mei_tree_lookup(ev, "mesh_velocity_U");
-      vel[1] = mei_tree_lookup(ev, "mesh_velocity_V");
-      vel[2] = mei_tree_lookup(ev, "mesh_velocity_W");
-
-      mei_tree_destroy(ev);
+      /* Evaluate formula using meg */
+      return cs_meg_boundary_function(bz,
+                                      "mesh_velocity",
+                                      "fixed_velocity");
 
     }
   }
+
+  return NULL; /* avoid a compilation warning */
 }
 
 /*----------------------------------------------------------------------------*/

src/gui/cs_gui_mobile_mesh.h
@@ -214,12 +214,12 @@ cs_gui_mobile_mesh_get_boundaries(cs_domain_t     *domain);
  * \brief Return the fixed velocity for a boundary
  *
  * \param[in]  label boundary condition label
- * \param[out] vel   imposed mesh velocity
+ *
+ * \return a pointer to an array of cs_real_t values
  *----------------------------------------------------------------------------*/
 
-void
-cs_gui_mobile_mesh_get_fixed_velocity(const char*    label,
-                                      cs_real_t     *vel);
+cs_real_t *
+cs_gui_mobile_mesh_get_fixed_velocity(const char*    label);
 
 /*----------------------------------------------------------------------------*/
 

commit 563bb244bf1f5cfe0c724c05535dc4d230542461

GUI: fix for crash in some ALE cases introduced by commit 66fd4db1 (Aug 25 2019).

bin/model/XMLinitialize.py
@@ -1682,8 +1682,7 @@ class XMLinit(BaseXmlInit):
         # Update for ALE using MEG
         XMLBoundaryNode = self.case.xmlGetNode('boundary_conditions')
         for node in XMLBoundaryNode.xmlGetNodeList('ale'):
-            nn = node.xmlGetNode('formula')
-            if nn:
+            for nn in node.xmlGetNodeList('formula'):
                 content = nn.xmlGetTextNode()
                 if content: # node formula can be empty
                     # Substitute only perfectly matching labels (between '\\b' and '\\b')