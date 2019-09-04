0027028d7f6ce74d0e9534851a1ac4a77ed5f797
Yvan Fournier <yvan.fournier@noreply.edf.fr>
2019-09-04 12:07:54 +0200

    Fix error in some cases using SFC-based mesh renumbering.
    Cherry-picked from commit ac3f6196 on master.

--- src/base/cs_renumber.c
+++ src/base/cs_renumber.c
@@ -3380,7 +3380,7 @@ _get_coord_extents(int               dim,
 {
   size_t  i, j;
 
-  /* Get global min/max coordinates */
+  /* Get local min/max coordinates */
 
   for (j = 0; j < (size_t)dim; j++) {
     extents[j]       = DBL_MAX;
@@ -3391,7 +3391,7 @@ _get_coord_extents(int               dim,
     for (j = 0; j < (size_t)dim; j++) {
       if (coords[i*dim + j] < extents[j])
         extents[j] = coords[i*dim + j];
-      else if (coords[i*dim + j] > extents[j + dim])
+      if (coords[i*dim + j] > extents[j + dim])
         extents[j + dim] = coords[i*dim + j];
     }
   }
