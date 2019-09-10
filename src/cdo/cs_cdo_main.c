code-saturne/code_saturne

commit 3c3219d00e761963325fc7c2981cf1e6a8ba6eb5

[CDO] (MAJOR) Enable to solve CDO equations with mass flux arising from the legacy scheme
Call to the subroutines solving CDO equations is now possible from
CALTRI and it enables to better mix CDO/FV schemes
Split initialization stages in CDO to be more compatible with the legacy one

src/cdo/cs_cdo_main.c
@@ -81,6 +81,17 @@ BEGIN_C_DECLS
 static int  cs_cdo_ts_id;
 
 /*============================================================================
+ * Prototypes for functions intended for use only by Fortran wrappers.
+ * (descriptions follow, with function bodies).
+ *============================================================================*/
+
+void
+cs_f_cdo_solve_steady_state_domain(void);
+
+void
+cs_f_cdo_solve_unsteady_state_domain(void);
+
+/*============================================================================
  * Private function prototypes
  *============================================================================*/
 
@@ -234,6 +245,9 @@ _solve_steady_state_domain(cs_domain_t  *domain)
 
   cs_post_activate_writer(CS_POST_WRITER_ALL_ASSOCIATED, true);
 
+  /* User-defined extra operations */
+  cs_user_extra_operations(domain);
+
   cs_domain_post(domain);
 
   cs_post_time_step_end();
@@ -324,13 +338,41 @@ _log_setup(const cs_domain_t   *domain)
 }
 
 /*============================================================================
+ * Fortran wrapper function definitions
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Solve only steady-state equations
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_f_cdo_solve_steady_state_domain(void)
+{
+  _solve_steady_state_domain(cs_glob_domain);
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Solve all the equations of a computational domain for one time step
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_f_cdo_solve_unsteady_state_domain(void)
+{
+  _solve_domain(cs_glob_domain);
+}
+
+/*============================================================================
  * Public function prototypes
  *============================================================================*/
 
 /*----------------------------------------------------------------------------*/
 /*!
  * \brief  Initialize the computational domain when CDO/HHO schemes are
- *         activated
+ *         activated and cs_user_model() has been called
  *
  * \param[in, out]  domain    pointer to a cs_domain_t structure
  */
@@ -339,15 +381,11 @@ _log_setup(const cs_domain_t   *domain)
 void
 cs_cdo_initialize_setup(cs_domain_t   *domain)
 {
-  /* Add an automatic boundary zone gathering all "wall" boundaries */
-  cs_boundary_def_wall_zones(domain->boundaries);
-
   if (cs_domain_get_cdo_mode(domain) == CS_DOMAIN_CDO_MODE_OFF)
     return;
 
   /* Timer statistics */
   cs_cdo_ts_id = cs_timer_stats_create("stages", "cdo", "cdo");
-  cs_timer_stats_start(cs_cdo_ts_id);
 
   /* Store the fact that the CDO/HHO module is activated */
   cs_domain_cdo_log(domain);
@@ -356,6 +394,11 @@ cs_cdo_initialize_setup(cs_domain_t   *domain)
   cs_property_t  *pty = cs_property_add("unity", CS_PROPERTY_ISO);
 
   cs_property_def_iso_by_value(pty, "cells", 1.0);
+
+  cs_timer_stats_start(cs_cdo_ts_id);
+
+  /* Add an automatic boundary zone gathering all "wall" boundaries */
+  cs_boundary_def_wall_zones(domain->boundaries);
 
   cs_timer_t t0 = cs_timer_time();
 
@@ -406,23 +449,16 @@ cs_cdo_initialize_structures(cs_domain_t           *domain,
   /* Timer statistics */
   cs_timer_stats_start(cs_cdo_ts_id);
 
+  cs_domain_init_cdo_structures(domain);
+
   /* Last setup stage */
-  cs_domain_finalize_setup(domain, m, mq);
+  cs_domain_finalize_setup(domain);
 
   /* Initialization default post-processing for the computational domain */
   cs_domain_post_init(domain);
 
   /* Summary of the settings */
   _log_setup(domain);
-
-  /* Output information */
-  cs_log_printf(CS_LOG_DEFAULT, "\n%s", h1_sep);
-  cs_log_printf(CS_LOG_DEFAULT, "#      Start main loop\n");
-  cs_log_printf(CS_LOG_DEFAULT, "%s", h1_sep);
-
-  /*  Build high-level structures and create algebraic systems
-      Set the initial values of the fields and properties */
-  cs_domain_initialize_systems(domain);
 
   /* Flush log files */
   cs_log_printf_flush(CS_LOG_DEFAULT);
@@ -526,10 +562,19 @@ cs_cdo_main(cs_domain_t   *domain)
   /* Force the activation of writers for postprocessing */
   cs_post_activate_writer(CS_POST_WRITER_ALL_ASSOCIATED, true);
 
+  /*  Build high-level structures and create algebraic systems
+      Set the initial values of the fields and properties */
+  cs_domain_initialize_systems(domain);
+
   /* Initialization for user-defined extra operations. Should be done
      after the domain initialization if one wants to overwrite the field
      initialization for instance */
   cs_user_extra_operations_initialize(cs_glob_domain);
+
+  /* Output information */
+  cs_log_printf(CS_LOG_DEFAULT, "\n%s", h1_sep);
+  cs_log_printf(CS_LOG_DEFAULT, "#      Start main loop\n");
+  cs_log_printf(CS_LOG_DEFAULT, "%s", h1_sep);
 
   /* Build and solve equations related to the computational domain in case of
      steady-state equations */
@@ -551,17 +596,18 @@ cs_cdo_main(cs_domain_t   *domain)
     /* Build and solve equations related to the computational domain */
     _solve_domain(domain);
 
-    /* Increment time */
-    cs_domain_increment_time(domain);
-
     /* Extra operations and post-processing of the computed solutions */
     cs_post_time_step_begin(domain->time_step);
+
+    /* User-defined extra operations */
+    cs_user_extra_operations(domain);
 
     cs_domain_post(domain);
 
     cs_post_time_step_end();
 
-    /* Increment time */
+    /* Increment time and time steps */
+    cs_domain_increment_time(domain);
     cs_domain_increment_time_step(domain);
 
     /* Read a control file if present */

src/cdo/cs_domain_op.c
@@ -74,6 +74,14 @@ BEGIN_C_DECLS
  *----------------------------------------------------------------------------*/
 
 /*============================================================================
+ * Prototypes for functions intended for use only by Fortran wrappers.
+ * (descriptions follow, with function bodies).
+ *============================================================================*/
+
+void
+cs_f_cdo_post_domain(void);
+
+/*============================================================================
  * Static global variables
  *============================================================================*/
 
@@ -369,6 +377,22 @@ _domain_post(void                      *input,
 }
 
 /*============================================================================
+ * Fortran wrapper function definitions
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Solve only steady-state equations
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_f_cdo_post_domain(void)
+{
+  cs_domain_post(cs_glob_domain);
+}
+
+/*============================================================================
  * Public function prototypes
  *============================================================================*/
 
@@ -400,22 +424,20 @@ cs_domain_post(cs_domain_t  *domain)
 {
   cs_timer_t  t0 = cs_timer_time();
 
+  assert(domain->cdo_context != NULL);
+
   /* Extra-operations */
   /* ================ */
 
   /* Predefined extra-operations related to advection fields */
-  assert(domain->cdo_context != NULL);
-
   cs_advection_field_update(domain->time_step->t_cur, true);
-
-  /* User-defined extra operations */
-  cs_user_extra_operations(domain);
 
   /* Log output */
   if (cs_domain_needs_log(domain)) {
 
     /* Basic statistic related to variables */
-    cs_log_iteration();
+    if (domain->cdo_context->mode == CS_DOMAIN_CDO_MODE_ONLY)
+      cs_log_iteration(); /* Otherwise called from the FORTRAN part */
 
     /* Post-processing */
     /* =============== */

src/base/caltri.f90
@@ -72,6 +72,7 @@ use ptrglo
 use turbomachinery
 use cs_c_bindings
 use cs_f_interfaces
+use cdomod
 
 use, intrinsic :: iso_c_binding
 
@@ -671,6 +672,10 @@ endif
 
 call inivar(nvar, nscal)
 
+if (icdo.ge.1) then ! CDO mode
+  call cs_f_initialize_cdo_systems
+endif
+
 !===============================================================================
 ! Initializations for the 1D thermal wall module
 !===============================================================================
@@ -806,6 +811,14 @@ if (iilagr.gt.0) then
 
 endif
 
+! CDO module (user-defined equations)
+!====================================
+
+if (icdo.eq.1) then
+   ! FV and CDO activated
+   call cs_f_cdo_solve_steady_state_domain
+endif
+
 ! Logging of initial values
 
 call log_iteration
@@ -928,6 +941,14 @@ call dmtmps(titer1)
 call tridim(itrale, nvar, nscal, dt)
 
 if (ntmabs.gt.ntpabs .and. itrale.gt.0) then
+
+  ! CDO module (user-defined equations)
+  !====================================
+
+  if (icdo.eq.1) then
+     ! FV and CDO activated
+     call cs_f_cdo_solve_unsteady_state_domain
+  endif
 
   ! Lagrangian module
   !==================
@@ -1067,6 +1088,14 @@ endif
 !===============================================================================
 
 call pstvar(nvar, nscal)
+
+! CDO module (user-defined equations)
+!====================================
+
+if (icdo.eq.1) then
+  ! FV and CDO activated
+  call cs_f_cdo_post_domain
+endif
 
 !===============================================================================
 ! Structures

src/base/cdomod.f90
@@ -42,6 +42,44 @@ module cdomod
   !> If icdo=2 (CDO only)
   integer, save :: icdo
 
+  interface
+
+    ! Interface to C function to solve the unsteady state for related CDO
+    ! equations
+
+    subroutine cs_f_cdo_solve_unsteady_state_domain()  &
+      bind(C, name='cs_f_cdo_solve_unsteady_state_domain')
+      use, intrinsic :: iso_c_binding
+      implicit none
+    end subroutine cs_f_cdo_solve_unsteady_state_domain
+
+    ! Interface to C function to solve the steady state for related CDO
+    ! equations
+
+    subroutine cs_f_cdo_solve_steady_state_domain()  &
+      bind(C, name='cs_f_cdo_solve_steady_state_domain')
+      use, intrinsic :: iso_c_binding
+      implicit none
+    end subroutine cs_f_cdo_solve_steady_state_domain
+
+    ! Interface to C function related to the initialization CDO systems
+
+    subroutine cs_f_initialize_cdo_systems()  &
+      bind(C, name='cs_f_initialize_cdo_systems')
+      use, intrinsic :: iso_c_binding
+      implicit none
+    end subroutine cs_f_initialize_cdo_systems
+
+    ! Interface to C function to postprocess data related to CDO schemes
+
+    subroutine cs_f_cdo_post_domain()  &
+      bind(C, name='cs_f_cdo_post_domain')
+      use, intrinsic :: iso_c_binding
+      implicit none
+    end subroutine cs_f_cdo_post_domain
+
+  end interface
+
 end module cdomod
 
 !=============================================================================

src/base/initi1.f90
@@ -172,7 +172,11 @@ if (icdo.lt.2) then
    call usipes(nmodpp)
 
    ! Avoid a second spurious call to this function
-   call user_finalize_setup_wrapper
+   ! Call in the C part if CDO is activated, i.e. when
+   ! additional geometric quantities and connectivities are built
+   if (icdo.lt.0) then
+      call user_finalize_setup_wrapper
+   endif
 endif
 
 !===============================================================================

src/cdo/cs_cdo_main.h
@@ -55,7 +55,7 @@ BEGIN_C_DECLS
 /*----------------------------------------------------------------------------*/
 /*!
  * \brief  Initialize the computational domain when CDO/HHO schemes are
- *         activated
+ *         activated and cs_user_model() has been called
  *
  * \param[in, out]  domain    pointer to a cs_domain_t structure
  */

src/cdo/cs_domain_setup.c
@@ -99,6 +99,14 @@ static const char _err_empty_cdo_context[] =
   " Please check your settings.\n";
 
 /*============================================================================
+ * Prototypes for functions intended for use only by Fortran wrappers.
+ * (descriptions follow, with function bodies).
+ *============================================================================*/
+
+void
+cs_f_initialize_cdo_systems(void);
+
+/*============================================================================
  * Private function prototypes
  *============================================================================*/
 
@@ -236,6 +244,23 @@ _set_scheme_flags(cs_domain_t    *domain)
 
   } /* NavSto is activated */
 
+}
+
+/*============================================================================
+ * Fortran wrapper function definitions
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Initialize CDO systems
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_f_initialize_cdo_systems(void)
+{
+  assert(cs_glob_domain != NULL);
+  cs_domain_initialize_systems(cs_glob_domain);
 }
 
 /*============================================================================
@@ -464,18 +489,15 @@ cs_domain_initialize_setup(cs_domain_t    *domain)
 
 /*----------------------------------------------------------------------------*/
 /*!
- * \brief  Last setup stage of the cs_domain_t structure
+ * \brief  After having read the mesh and the first setup stage build the
+ *         connectivities and mesh quantities related to CDO/HHO schemes
  *
  * \param[in, out]  domain            pointer to a cs_domain_t struct.
- * \param[in, out]  mesh              pointer to a cs_mesh_t struct.
- * \param[in]       mesh_quantities   pointer to a cs_mesh_quantities_t struct.
  */
 /*----------------------------------------------------------------------------*/
 
 void
-cs_domain_finalize_setup(cs_domain_t                 *domain,
-                         cs_mesh_t                   *mesh,
-                         const cs_mesh_quantities_t  *mesh_quantities)
+cs_domain_init_cdo_structures(cs_domain_t                 *domain)
 {
   if (domain == NULL)
     bft_error(__FILE__, __LINE__, 0, _err_empty_domain);
@@ -492,15 +514,15 @@ cs_domain_finalize_setup(cs_domain_t                 *domain,
   /* Build additional connectivity structures
      Update mesh structure with range set structures */
   cs_domain_cdo_context_t  *cc = domain->cdo_context;
-  domain->connect = cs_cdo_connect_init(mesh,
+  domain->connect = cs_cdo_connect_init(domain->mesh,
                                         cc->vb_scheme_flag,
                                         cc->vcb_scheme_flag,
                                         cc->fb_scheme_flag,
                                         cc->hho_scheme_flag);
 
   /* Build additional mesh quantities in a seperate structure */
-  domain->cdo_quantities =  cs_cdo_quantities_build(mesh,
-                                                    mesh_quantities,
+  domain->cdo_quantities =  cs_cdo_quantities_build(domain->mesh,
+                                                    domain->mesh_quantities,
                                                     domain->connect);
 
   /* Shared main generic structure
@@ -513,21 +535,6 @@ cs_domain_finalize_setup(cs_domain_t                 *domain,
                                   domain->connect);
   cs_advection_field_set_shared_pointers(domain->cdo_quantities,
                                          domain->connect);
-
-  /* Groundwater flow module */
-  if (cs_gwf_is_activated()) {
-
-    /* Setup for the soil structures and the tracer equations */
-    cs_user_gwf_setup(domain);
-
-    /* Add if needed new terms (as diffusion or reaction) to tracer equations
-       according to the settings */
-    cs_gwf_add_tracer_terms();
-
-  }
-
-  /* Allocate all fields created during the setup stage */
-  cs_field_allocate_or_map_all();
 
   /* Allocate common structures for solving equations */
   cs_equation_common_init(domain->connect,
@@ -556,6 +563,38 @@ cs_domain_finalize_setup(cs_domain_t                 *domain,
                                     cc->fb_scheme_flag,
                                     cc->hho_scheme_flag);
 
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Last setup stage of the cs_domain_t structure
+ *
+ * \param[in, out]  domain            pointer to a cs_domain_t struct.
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_domain_finalize_setup(cs_domain_t         *domain)
+{
+  if (domain == NULL)
+    bft_error(__FILE__, __LINE__, 0, _err_empty_domain);
+  if (domain->cdo_context == NULL)
+    bft_error(__FILE__, __LINE__, 0, _err_empty_cdo_context);
+
+  /* Groundwater flow module */
+  if (cs_gwf_is_activated()) {
+
+    /* Setup for the soil structures and the tracer equations */
+    cs_user_gwf_setup(domain);
+
+    /* Add if needed new terms (as diffusion or reaction) to tracer equations
+       according to the settings */
+    cs_gwf_add_tracer_terms();
+
+  }
+
+  /* Allocate all fields created during the setup stage */
+  cs_field_allocate_or_map_all();
 
   /* Set the definition of user-defined properties and/or advection
    * fields (no more fields are created at this stage)
@@ -568,7 +607,7 @@ cs_domain_finalize_setup(cs_domain_t                 *domain,
    * --> Source term
    */
 
-  cs_user_finalize_setup(cs_glob_domain);
+  cs_user_finalize_setup(domain);
 
   /* Assign to a cs_equation_t structure a list of function to manage this
    * structure during the computation.
@@ -726,9 +765,11 @@ cs_domain_setup_log(const cs_domain_t   *domain)
       cs_log_printf(CS_LOG_SETUP, " * Time step **constant**\n\n");
     else if (domain->time_options.idtvar == 1)
       cs_log_printf(CS_LOG_SETUP, " * Time step **variable in time**\n\n");
-    else
-      bft_error(__FILE__, __LINE__, 0,
-                _(" Invalid idtvar value for the CDO module.\n"));
+    else {
+      if (cdo_mode != CS_DOMAIN_CDO_MODE_WITH_FV)
+        bft_error(__FILE__, __LINE__, 0,
+                  _(" Invalid idtvar value for the CDO module.\n"));
+    }
 
     cs_xdef_log("        Time step definition", domain->time_step_def);
     cs_log_printf(CS_LOG_SETUP, "\n");

src/cdo/cs_domain_setup.h
@@ -152,18 +152,26 @@ cs_domain_initialize_setup(cs_domain_t                 *domain);
 
 /*----------------------------------------------------------------------------*/
 /*!
- * \brief  Last setup stage of the cs_domain_t structure
+ * \brief  After having read the mesh and the first setup stage build the
+ *         connectivities and mesh quantities related to CDO/HHO schemes
  *
  * \param[in, out]  domain            pointer to a cs_domain_t struct.
- * \param[in, out]  mesh              pointer to a cs_mesh_t struct.
- * \param[in]       mesh_quantities   pointer to a cs_mesh_quantities_t struct.
  */
 /*----------------------------------------------------------------------------*/
 
 void
-cs_domain_finalize_setup(cs_domain_t                 *domain,
-                         cs_mesh_t                   *mesh,
-                         const cs_mesh_quantities_t  *mesh_quantities);
+cs_domain_init_cdo_structures(cs_domain_t              *domain);
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Last setup stage of the cs_domain_t structure
+ *
+ * \param[in, out]  domain            pointer to a cs_domain_t struct.
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_domain_finalize_setup(cs_domain_t         *domain);
 
 /*----------------------------------------------------------------------------*/
 /*!

commit 6e3dcba4ba44222fb3601ca8bf1ce66b07797453

[CDO] Fix a bug introduced in commit 3c3219d (2 months ago)

src/cdo/cs_cdo_main.c
@@ -211,9 +211,13 @@ _solve_steady_state_domain(cs_domain_t  *domain)
                     "-ite- 0; >> Initial state");
       cs_log_printf(CS_LOG_DEFAULT, "\n%s\n", h1_sep);
 
-      /* Basic statistic related to variables */
-      if (cs_domain_needs_log(domain))
-        cs_log_iteration();
+      /* Extra operations and post-processing of the computed solutions */
+      cs_post_time_step_begin(domain->time_step);
+
+      cs_domain_post(domain);
+
+      cs_post_time_step_end();
+
       return;
     }
   }
@@ -265,9 +269,6 @@ _solve_steady_state_domain(cs_domain_t  *domain)
   cs_post_time_step_begin(domain->time_step);
 
   cs_post_activate_writer(CS_POST_WRITER_ALL_ASSOCIATED, true);
-
-  /* User-defined extra operations */
-  cs_user_extra_operations(domain);
 
   cs_domain_post(domain);
 
@@ -630,18 +631,18 @@ cs_cdo_main(cs_domain_t   *domain)
     /* Build and solve equations related to the computational domain */
     _solve_domain(domain);
 
+    /* Increment time (time increment is not performed at the same time as the
+       time step (since one starts with nt_cur == 1) */
+    cs_domain_increment_time(domain);
+
     /* Extra operations and post-processing of the computed solutions */
     cs_post_time_step_begin(domain->time_step);
-
-    /* User-defined extra operations */
-    cs_user_extra_operations(domain);
 
     cs_domain_post(domain);
 
     cs_post_time_step_end();
 
-    /* Increment time and time steps */
-    cs_domain_increment_time(domain);
+    /* Increment time steps */
     cs_domain_increment_time_step(domain);
 
     /* Read a control file if present */

src/cdo/cs_domain_op.c
@@ -430,6 +430,9 @@ cs_domain_post(cs_domain_t  *domain)
   /* Extra-operations */
   /* ================ */
 
+  /* User-defined extra operations */
+  cs_user_extra_operations(domain);
+
   /* Predefined extra-operations related to advection fields */
   cs_advection_field_update(domain->time_step->t_cur, true);
 