code-saturne/code_saturne

commit 3701fd212769ddd39adcdcc91d276738c87ef447

Porous modelling: allow to set or unset fluid properties using fields.

src/mesh/cs_mesh_quantities.c
@@ -2732,7 +2732,32 @@ cs_mesh_quantities_set_porous_model(int  porous_model)
 void
 cs_mesh_quantities_set_has_disable_flag(int  flag)
 {
-  cs_glob_mesh_quantities->has_disable_flag = flag;
+  cs_mesh_quantities_t *mq =cs_glob_mesh_quantities;
+
+  mq->has_disable_flag = flag;
+  /* if off, fluid surfaces points toward cell surfaces */
+  /* Porous models */
+  if (cs_glob_porous_model > 0) {
+    if (flag == 0) {
+      /* Set pointer of compution faces to standard faces */
+      if (cs_glob_porous_model == 3) {
+        mq->i_f_face_normal = mq->i_face_normal;
+        mq->b_f_face_normal = mq->b_face_normal;
+        mq->i_f_face_surf   = mq->i_face_surf;
+        mq->b_f_face_surf   = mq->b_face_surf;
+      }
+      mq->cell_f_vol = mq->cell_vol;
+    } else {
+      /* Use fluid sufaces and volume */
+      if (cs_glob_porous_model == 3) {
+        mq->i_f_face_normal = cs_field_by_name("i_f_face_normal")->val;
+        mq->b_f_face_normal = cs_field_by_name("b_f_face_normal")->val;
+        mq->i_f_face_surf   = cs_field_by_name("i_f_face_surf")->val;
+        mq->b_f_face_surf   = cs_field_by_name("b_f_face_surf")->val;
+      }
+      mq->cell_f_vol        = cs_field_by_name("cell_f_vol")->val;
+    }
+  }
 }
 
 /*----------------------------------------------------------------------------*/
@@ -3117,48 +3142,23 @@ cs_mesh_quantities_compute(const cs_mesh_t       *mesh,
 
   cs_mesh_quantities_compute_preprocess(mesh, mesh_quantities);
 
-  /* Balance porous model */
-  if (cs_glob_porous_model == 3) {
-    if (mesh_quantities->i_f_face_normal == NULL)
-      BFT_MALLOC(mesh_quantities->i_f_face_normal, n_i_faces*dim, cs_real_t);
+  /* Fluid surfaces and volume: point to standard quantities and
+   * may be modified afterwards */
+  mesh_quantities->i_f_face_normal = mesh_quantities->i_face_normal;
+  mesh_quantities->b_f_face_normal = mesh_quantities->b_face_normal;
+  mesh_quantities->i_f_face_surf = mesh_quantities->i_face_surf;
+  mesh_quantities->b_f_face_surf = mesh_quantities->b_face_surf;
 
-    if (mesh_quantities->b_f_face_normal == NULL)
-      BFT_MALLOC(mesh_quantities->b_f_face_normal, n_b_faces*dim, cs_real_t);
-
-    if (mesh_quantities->i_f_face_surf == NULL)
-      BFT_MALLOC(mesh_quantities->i_f_face_surf, n_i_faces, cs_real_t);
-
-    if (mesh_quantities->b_f_face_surf == NULL)
-      BFT_MALLOC(mesh_quantities->b_f_face_surf, n_b_faces, cs_real_t);
-
-    if (mesh_quantities->i_f_face_factor == NULL)
-      BFT_MALLOC(mesh_quantities->i_f_face_factor, n_i_faces, cs_real_2_t);
-
-    if (mesh_quantities->b_f_face_factor == NULL)
-      BFT_MALLOC(mesh_quantities->b_f_face_factor, n_b_faces, cs_real_t);
-
-  }
-  else {
-    mesh_quantities->i_f_face_normal = mesh_quantities->i_face_normal;
-    mesh_quantities->b_f_face_normal = mesh_quantities->b_face_normal;
-    mesh_quantities->i_f_face_surf = mesh_quantities->i_face_surf;
-    mesh_quantities->b_f_face_surf = mesh_quantities->b_face_surf;
-  }
+  mesh_quantities->cell_f_vol = mesh_quantities->cell_vol;
 
   /* Porous models */
   if (cs_glob_porous_model > 0) {
     mesh_quantities->has_disable_flag = 1;
-    if (mesh_quantities->cell_f_vol == NULL)
-      BFT_MALLOC(mesh_quantities->cell_f_vol, n_cells_with_ghosts, cs_real_t);
-
   }
   else {
-    mesh_quantities->cell_f_vol = mesh_quantities->cell_vol;
-
     mesh_quantities->min_f_vol = mesh_quantities->min_vol;
     mesh_quantities->max_f_vol = mesh_quantities->max_vol;
     mesh_quantities->tot_f_vol = mesh_quantities->tot_vol;
-
   }
 
   if (mesh_quantities->has_disable_flag == 1) {

src/base/varpos.f90
@@ -406,6 +406,15 @@ if (iporos.ge.1) then
   call field_create(f_name, itycat, ityloc, 1, .false., ipori)
   call field_set_key_int(ipori, keylog, 1)
   call field_set_key_int(ipori, keyvis, pflag)
+
+  f_name = 'cell_f_vol'
+  call field_create(f_name,&
+                    itycat,&
+                    1,& ! location: cell
+                    1,& ! dimension
+                    .false.,&
+                    f_id)
+
   if (iporos.eq.2) then
     f_name = 'tensorial_porosity'
     call field_create(f_name, itycat, ityloc, 6, .false., iporf)
@@ -442,8 +451,42 @@ if (iporos.ge.1) then
                       1,& ! dimension
                       .false.,&
                       f_id)
+    f_name = 'i_f_face_normal'
+    call field_create(f_name,&
+                      itycat,&
+                      2,& ! location: inner faces
+                      3,& ! dimension
+                      .false.,&
+                      f_id)
+
+    f_name = 'i_f_face_surf'
+    call field_create(f_name,&
+                      itycat,&
+                      2,& ! location: inner faces
+                      1,& ! dimension
+                      .false.,&
+                      f_id)
+
+    f_name = 'b_f_face_normal'
+    call field_create(f_name,&
+                      itycat,&
+                      3,& ! location: boundary faces
+                      3,& ! dimension
+                      .false.,&
+                      f_id)
+
+    f_name = 'b_f_face_surf'
+    call field_create(f_name,&
+                      itycat,&
+                      3,& ! location: boundary faces
+                      1,& ! dimension
+                      .false.,&
+                      f_id)
 
   endif
+
+  ! Make fluid surfaces of mesh quantity point to the created fields
+  call cs_mesh_quantities_set_has_disable_flag(1)
 endif
 
 !===============================================================================

commit bc11231b96d5f2239bd572ef46939000377651e3

Fix memory leak introduced in commit 3701fd21 (Jul 29 2019).

src/mesh/cs_mesh_quantities.c
@@ -3173,12 +3173,14 @@ cs_mesh_quantities_compute(const cs_mesh_t       *mesh,
   }
 
   if (mesh_quantities->has_disable_flag == 1) {
-    BFT_MALLOC(mesh_quantities->c_disable_flag, n_cells_with_ghosts, int);
+    if (mesh_quantities->c_disable_flag == NULL)
+      BFT_MALLOC(mesh_quantities->c_disable_flag, n_cells_with_ghosts, int);
     for (cs_lnum_t cell_id = 0; cell_id < n_cells_with_ghosts; cell_id++)
       mesh_quantities->c_disable_flag[cell_id] = 0;
   }
   else {
-    BFT_MALLOC(mesh_quantities->c_disable_flag, 1, int);
+    if (mesh_quantities->c_disable_flag == NULL)
+      BFT_MALLOC(mesh_quantities->c_disable_flag, 1, int);
     mesh_quantities->c_disable_flag[0] = 0;
   }
 