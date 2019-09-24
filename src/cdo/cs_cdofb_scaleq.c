259433f1f513cd3551bf151f9201a722fe83fe17
Yvan Fournier <yvan.fournier@noreply.edf.fr>
2019-09-24 12:32:38 +0200

    Fix compilation with gcc 9 and OpenMP.

--- src/cdo/cs_cdofb_scaleq.c
+++ src/cdo/cs_cdofb_scaleq.c
@@ -1920,6 +1920,7 @@ cs_cdofb_scaleq_balance(const cs_equation_param_t     *eqp,
   const cs_real_t  dt_cur = cs_shared_time_step->dt[0];
   const cs_real_t  inv_dtcur = 1./dt_cur;
   const cs_real_t  time_eval = t_cur + 0.5*dt_cur;
+  const char *func_name = __func__;
 
   cs_timer_t  t0 = cs_timer_time();
 
@@ -1930,8 +1931,8 @@ cs_cdofb_scaleq_balance(const cs_equation_param_t     *eqp,
   cs_equation_balance_t  *eb = cs_equation_balance_create(cs_flag_primal_cell,
                                                           quant->n_cells);
 
-# pragma omp parallel if (quant->n_cells > CS_THR_MIN) default(none)    \
-  shared(quant, connect, eqp, eqb, eqc, pot, eb, cs_cdofb_cell_bld)     \
+# pragma omp parallel if (quant->n_cells > CS_THR_MIN) default(none)            \
+  shared(quant, connect, eqp, eqb, eqc, pot, eb, cs_cdofb_cell_bld, func_name)  \
   firstprivate(time_eval, inv_dtcur)
   {
 #if defined(HAVE_OPENMP) /* Determine default number of OpenMP threads */
@@ -2005,7 +2006,7 @@ cs_cdofb_scaleq_balance(const cs_equation_param_t     *eqp,
             (p_cur[cm->n_fc] - p_prev[cm->n_fc]);
         else
           bft_error(__FILE__, __LINE__, 0,
-                    "%s: Not implemented yet.", __func__);
+                    "%s: Not implemented yet.", func_name);
 
       } /* End of time contribution */
 
