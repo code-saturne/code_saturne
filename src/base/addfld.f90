code-saturne/code_saturne

commit 110422c5573f9b07ee5812dc9fd282e66ea33dc3

Handle scalar boundary conditions consistently when idiff = 0.
This should adress issue 159 from the Mantis bug tracker (reported
for version 4.0 using thermal radiation, but not limited to this:
even with idiff = 0 for the thermal scalar, the diffusivity was taken
into account to compute the convective heat flux that enters the
balance at the wall surface).

src/base/addfld.f90
@@ -495,8 +495,14 @@ if (iscalt.le.0) then
 endif
 
 if (ipstdv(ipstnu).gt.0) then
-  call field_find_or_create('tplus', itycat, ityloc, idim1, iflid)
-  call field_find_or_create('tstar', itycat, ityloc, idim1, iflid)
+  if (iscalt.gt.0) then
+    f_id = ivarfl(isca(iscalt))
+    call field_get_key_struct_var_cal_opt(f_id, vcopt)
+    if (vcopt_dfm%idiff .ne. 0) then
+      call field_find_or_create('tplus', itycat, ityloc, idim1, iflid)
+      call field_find_or_create('tstar', itycat, ityloc, idim1, iflid)
+    endif
+  endif
 endif
 
 ! In case of ALE or boundary efforts postprocessing, create appropriate field

src/base/clptrg.f90
@@ -1791,6 +1791,17 @@ endif
 
 call field_get_key_struct_var_cal_opt(ivarfl(ivar), vcopt)
 
+! If we have no diffusion, no boundary face should have a wall BC type
+! (this is ensured in typecl)
+
+if (vcopt%idiff .eq. 0) then
+  tetmax = 0.d0
+  tetmin = 0.d0
+  tplumx = 0.d0
+  tplumn = 0.d0
+  return
+endif
+
 if (iand(vcopt%idften, ANISOTROPIC_DIFFUSION).ne.0.or.ityturt(iscal).eq.3) then
   if (iturb.ne.32.or.ityturt(iscal).eq.3) then
     call field_get_val_v(ivsten, visten)

src/base/clptur.f90
@@ -2212,6 +2212,17 @@ endif
 
 call field_get_key_struct_var_cal_opt(ivarfl(ivar), vcopt)
 
+! If we have no diffusion, no boundary face should have a wall BC type
+! (this is ensured in typecl)
+
+if (vcopt%idiff .eq. 0) then
+  tetmax = 0.d0
+  tetmin = 0.d0
+  tplumx = 0.d0
+  tplumn = 0.d0
+  return
+endif
+
 if (    iand(vcopt%idften, ANISOTROPIC_DIFFUSION).ne.0             &
     .or.ityturt(iscal).eq.3) then
   if (iturb.ne.32.or.ityturt(iscal).eq.3) then

src/base/typecl.f90
@@ -1614,6 +1614,31 @@ do f_id = 0, nfld - 1
   endif
 enddo
 
+! Ensure that for all scalars without diffusion
+! wall values ignore diffusion
+
+do iscal = 1, nscal
+  ivar = isca(iscal)
+  f_id = ivarfl(ivar)
+  ! Name of the scalar ivar
+  call field_get_type(f_id, f_type)
+  if (iand(f_type, FIELD_CDO) .ne. 0) cycle
+  call field_get_key_struct_var_cal_opt(f_id, vcopt)
+  if (vcopt%idiff .eq. 0) then
+    call field_get_dim (f_id, f_dim)
+    do i_dim = 0, f_dim-1
+      do ifac = 1, nfabor
+        if (icodcl(ifac,ivar+i_dim).eq.5 .or. icodcl(ifac,ivar+i_dim).eq.6) then
+          icodcl(ifac,ivar+i_dim) = 3
+          rcodcl(ifac,ivar+i_dim,3) = 0
+        else if (icodcl(ifac,ivar+i_dim).eq.3) then
+          rcodcl(ifac,ivar+i_dim,3) = 0
+        endif
+      enddo
+    enddo
+  endif
+enddo
+
 !===============================================================================
 ! 7.  RENFORCEMENT DIAGONALE DE LA MATRICE SI AUCUN POINTS DIRICHLET
 !===============================================================================

commit 95596445482c4daf4f9a83ca27ad8cb604ab7c74

Fix bug introduced in commit 110422c5 : post processing boundary fields creation conditionned by non intialised value.

src/base/addfld.f90
@@ -498,7 +498,7 @@ if (ipstdv(ipstnu).gt.0) then
   if (iscalt.gt.0) then
     f_id = ivarfl(isca(iscalt))
     call field_get_key_struct_var_cal_opt(f_id, vcopt)
-    if (vcopt_dfm%idiff .ne. 0) then
+    if (vcopt%idiff .ne. 0) then
       call field_find_or_create('tplus', itycat, ityloc, idim1, iflid)
       call field_find_or_create('tstar', itycat, ityloc, idim1, iflid)
     endif