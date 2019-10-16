d77f426aab43a2835b46493a0010e5a0b9747d62
Yvan Fournier <yvan.fournier@noreply.edf.fr>
2019-10-16 12:13:25 +0200

    GUI: fix loss of --initialize-only setting.
    This fixes GitLab issue #126.

--- bin/cs_exec_environment.py
+++ bin/cs_exec_environment.py
@@ -170,8 +170,6 @@ def update_command_no_value(args, options, present):
     syntax of the options tuple will be used.
     """
 
-    i = -1
-
     # Update first occurence
 
     count = 0
@@ -180,7 +178,7 @@ def update_command_no_value(args, options, present):
         target = 1
 
     for opt in options:
-        j = i
+        j = 0
         while j < len(args):
             if args[j] == opt:
                 count += 1
