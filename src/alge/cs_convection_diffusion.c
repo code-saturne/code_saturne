code-saturne/code_saturne

commit 7dff1964ead8e1d8134ffdc66209736a3b933070

Finer-grained performance logging for gradient reconstruction.

src/alge/cs_convection_diffusion.c
@@ -1847,7 +1847,7 @@ cs_convection_diffusion_scalar(int                       idtvar,
     bft_error(__FILE__, __LINE__, 0,
               _("invalid value of isstpp for a work array"));
   } else {
-    strcpy(var_name, "Work array");
+    strcpy(var_name, "[scalar convection-diffusion]");
   }
   var_name[31] = '\0';
 
@@ -3190,7 +3190,7 @@ cs_face_convection_scalar(int                       idtvar,
     bft_error(__FILE__, __LINE__, 0,
               _("invalid value of isstpp for a work array"));
   } else {
-    strcpy(var_name, "Work array");
+    strcpy(var_name, "[scalar face flux from convection]");
   }
   var_name[31] = '\0';
 
@@ -4245,7 +4245,7 @@ cs_convection_diffusion_vector(int                         idtvar,
     snprintf(var_name, 31, "%s", f->name);
   }
   else
-    strcpy(var_name, "Work array");
+    strcpy(var_name, "[convection-diffusion, vector]");
   var_name[31] = '\0';
 
   /* Discontinuous porous treatment */
@@ -5906,7 +5906,7 @@ cs_convection_diffusion_tensor(int                         idtvar,
     snprintf(var_name, 31, "%s", f->name);
   }
   else
-    strcpy(var_name, "Work array");
+    strcpy(var_name, "[convection-diffusion, tensor]");
   var_name[31] = '\0';
 
   if (iwarnp >= 2 && iconvp == 1) {
@@ -6908,7 +6908,7 @@ cs_convection_diffusion_thermal(int                       idtvar,
     snprintf(var_name, 31, "%s", f->name);
   }
   else
-    strcpy(var_name, "Work array");
+    strcpy(var_name, "[convection-diffusion, thermal]");
   var_name[31] = '\0';
 
   if (iwarnp >= 2) {
@@ -8086,7 +8086,7 @@ cs_anisotropic_diffusion_scalar(int                       idtvar,
     snprintf(var_name, 31, "%s", f->name);
   }
   else
-    strcpy(var_name, "Work array");
+    strcpy(var_name, "[anisotropic diffusion, scalar]");
   var_name[31] = '\0';
 
   /* Porosity fields */
@@ -8836,7 +8836,7 @@ cs_anisotropic_left_diffusion_vector(int                         idtvar,
     snprintf(var_name, 31, "%s", f->name);
   }
   else
-    strcpy(var_name, "Work array");
+    strcpy(var_name, "[anisotropic left diffusion, vector]");
   var_name[31] = '\0';
 
   if (icoupl > 0) {
@@ -9387,7 +9387,7 @@ cs_anisotropic_right_diffusion_vector(int                         idtvar,
     snprintf(var_name, 31, "%s", f->name);
   }
   else
-    strcpy(var_name, "Work array");
+    strcpy(var_name, "[anisotropic right diffusion, vector]");
   var_name[31] = '\0';
 
   viscce = viscel;
@@ -9403,7 +9403,6 @@ cs_anisotropic_right_diffusion_vector(int                         idtvar,
                                        &n_distant,
                                        &faces_distant);
   }
-
 
   /* 2. Compute the diffusive part with reconstruction technics */
 
@@ -10094,7 +10093,7 @@ cs_anisotropic_diffusion_tensor(int                         idtvar,
     snprintf(var_name, 31, "%s", f->name);
   }
   else
-    strcpy(var_name, "Work array");
+    strcpy(var_name, "[anisotropic diffusion, tensor]");
   var_name[31] = '\0';
 
   /* Porosity fields */
@@ -10547,7 +10546,7 @@ cs_anisotropic_diffusion_tensor(int                         idtvar,
  *
  * Please refer to the
  * <a href="../../theory.pdf#itrmas"><b>itrmas/itrgrp</b></a> section of the
- * theory guide for more informations.
+ * theory guide for more information.
  *
  * \param[in]     f_id          field id (or -1)
  * \param[in]     m             pointer to mesh
@@ -10703,7 +10702,7 @@ cs_face_diffusion_potential(const int                 f_id,
     snprintf(var_name, 31, "%s", f->name);
   }
   else
-    strcpy(var_name, "Work array");
+    strcpy(var_name, "[face mass flux update]");
   var_name[31] = '\0';
 
   /* Handle parallelism and periodicity */
@@ -11070,7 +11069,7 @@ cs_face_anisotropic_diffusion_potential(const int                 f_id,
     snprintf(var_name, 31, "%s", f->name);
   }
   else
-    strcpy(var_name, "Work array");
+    strcpy(var_name, "[face mass flux update]");
   var_name[31] = '\0';
 
   /* Porosity fields */
@@ -11543,7 +11542,7 @@ cs_diffusion_potential(const int                 f_id,
     snprintf(var_name, 31, "%s", f->name);
   }
   else
-    strcpy(var_name, "Work array");
+    strcpy(var_name, "[cell mass flux divergence update]");
   var_name[31] = '\0';
 
   /* Handle parallelism and periodicity */
@@ -11939,7 +11938,7 @@ cs_anisotropic_diffusion_potential(const int                 f_id,
     snprintf(var_name, 31, "%s", f->name);
   }
   else
-    strcpy(var_name, "Work array");
+    strcpy(var_name, "[cell mass flux divergence update]");
   var_name[31] = '\0';
 
   /* Porosity fields */

src/alge/cs_divergence.c
@@ -472,7 +472,7 @@ cs_mass_flux(const cs_mesh_t          *m,
     snprintf(var_name, 31, "%s", f->name); var_name[31] = '\0';
   }
   else {
-    strcpy(var_name, "Work array (momentum)"); var_name[31] = '\0';
+    strcpy(var_name, "[momentum]"); var_name[31] = '\0';
   }
 
   /* ---> Momentum computation */
@@ -1007,7 +1007,7 @@ cs_tensor_face_flux(const cs_mesh_t          *m,
     snprintf(var_name, 31, "%s", f->name); var_name[31] = '\0';
   }
   else {
-    strcpy(var_name, "Work array"); var_name[31] = '\0';
+    strcpy(var_name, "[tensor face flux]"); var_name[31] = '\0';
   }
 
   /* ---> Momentum computation */

po/fr.po
@@ -7,7 +7,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: code-saturne\n"
 "Report-Msgid-Bugs-To: saturne-support@edf.fr\n"
-"POT-Creation-Date: 2019-10-03 15:49+0200\n"
+"POT-Creation-Date: 2019-10-22 19:47+0200\n"
 "PO-Revision-Date: 2012-01-26 12:00+0100\n"
 "Last-Translator: Code_Saturne <saturne-support@edf.fr>\n"
 "Language-Team: French <traduc@traduc.org>\n"
@@ -3060,25 +3060,21 @@ msgstr "Reconstruction itérative"
 msgid "Least-squares"
 msgstr "Moindres carrés"
 
-msgid "Least-squares, then iterative (old))"
-msgstr "Moindres carrés puis itérative (ancienne)"
-
-msgid "conservative, reconstruction with Least-squares"
-msgstr "conservatif, reconstruction par Moindres carrés"
-
-msgid "Iterative (old)"
-msgstr "Itérative (ancienne)"
+msgid "Reconstruction with Least-squares"
+msgstr "Reconstruction Green-Gauss avec Moindres carrés"
 
 #, c-format
 msgid ""
 "\n"
-"Summary of gradient computations for \"%s\" (%s):\n"
+"Summary of gradient computations for \"%s\":\n"
 "\n"
+"  Reconstruction type:   %s\n"
 "  Number of calls:       %d\n"
 msgstr ""
 "\n"
-"Bilan des calculs de gradient pour \"%s\" (%s) :\n"
+"Bilan des calculs de gradient pour \"%s\" :\n"
 "\n"
+"  Type de méthode :      %s\n"
 "  Nombre d'appels :      %d\n"
 
 #, c-format
@@ -6232,15 +6228,6 @@ msgstr ""
 
 #, c-format
 msgid "Called cs_interface_set_max with unhandled datatype (%d)."
-msgstr ""
-
-msgid "Extended neighborhood not implemented for internal coupling."
-msgstr "Voisinage étendu non utilisable avec le couplage interne."
-
-#, c-format
-msgid ""
-"Gradient type %s is \n"
-"not implemented with internal coupling."
 msgstr ""
 
 #, c-format
@@ -10002,6 +9989,12 @@ msgid ""
 msgstr ""
 
 #, c-format
+msgid ""
+" %s: Undefined iterative solver for solving %s equation.\n"
+" Please modify your settings."
+msgstr ""
+
+#, c-format
 msgid " %s: Invalid key for setting the equation %s."
 msgstr "%s : type de clé invalide pour l'équation %s."
 
@@ -10009,12 +10002,6 @@ msgstr "%s : type de clé invalide pour l'équation %s."
 msgid ""
 " %s: Equation %s is not modifiable anymore.\n"
 " Please check your settings."
-msgstr ""
-
-#, c-format
-msgid ""
-" %s: Undefined iterative solver for solving %s equation.\n"
-" Please modify your settings."
 msgstr ""
 
 #, c-format
@@ -16416,3 +16403,9 @@ msgstr ""
 "    cssr1:       %14.5e (Coef c_r1)\n"
 "    cssr2:       %14.5e (Coef c_r2)\n"
 "    cssr3:       %14.5e (Coef c_r3)\n"
+
+#~ msgid "Least-squares, then iterative (old))"
+#~ msgstr "Moindres carrés puis itérative (ancienne)"
+
+#~ msgid "Extended neighborhood not implemented for internal coupling."
+#~ msgstr "Voisinage étendu non utilisable avec le couplage interne."

src/alge/cs_gradient.c
@@ -157,8 +157,7 @@ static cs_gradient_info_t **cs_glob_gradient_systems = NULL;
 const char *cs_gradient_type_name[]
   = {N_("Iterative reconstruction"),
      N_("Least-squares"),
-     N_("Reconstruction with Least-squares"),
-     N_("Iterative (old)")};
+     N_("Reconstruction with Least-squares")};
 
 /* Timer statistics */
 
@@ -428,7 +427,8 @@ _gradient_info_dump(cs_gradient_info_t *this_info)
 
   cs_log_printf(CS_LOG_PERFORMANCE,
                 _("\n"
-                  "Summary of gradient computations for \"%s\" (%s):\n\n"
+                  "Summary of gradient computations for \"%s\":\n\n"
+                  "  Reconstruction type:   %s\n"
                   "  Number of calls:       %d\n"),
                 this_info->name, cs_gradient_type_name[this_info->type],
                 n_calls);

src/lagr/cs_lagr_gradients.c
@@ -222,7 +222,7 @@ cs_lagr_gradients(int            time_id,
   /* Compute pressure gradient
    * ========================= */
 
-  cs_gradient_scalar("Work array",
+  cs_gradient_scalar("pressure [Lagrangian module]",
                      gradient_type,
                      halo_type,
                      inc,

commit 9b385d5a1743c4d2de76a22d7bf60d152b6c1ec0

Avoid some stack errors introduced in commit 7dff1964 (Oct 22 2019).

src/alge/cs_convection_diffusion.c
@@ -1745,7 +1745,7 @@ cs_convection_diffusion_scalar(int                       idtvar,
 
   /* Local variables */
 
-  char var_name[32];
+  char var_name[64];
 
   int iupwin = 0;
   int tr_dim = 0;
@@ -3096,7 +3096,7 @@ cs_face_convection_scalar(int                       idtvar,
 
   /* Local variables */
 
-  char var_name[32];
+  char var_name[64];
 
   int iupwin = 0;
   int tr_dim = 0;
@@ -4232,7 +4232,7 @@ cs_convection_diffusion_vector(int                         idtvar,
   cs_real_t *df_limiter = NULL;
 
   cs_field_t *f = NULL;
-  char var_name[32];
+  char var_name[64];
 
   if (f_id != -1) {
     f = cs_field_by_id(f_id);
@@ -5847,7 +5847,7 @@ cs_convection_diffusion_tensor(int                         idtvar,
 
   /* Local variables */
 
-  char var_name[32];
+  char var_name[64];
 
   int tr_dim = 0;
 
@@ -6815,7 +6815,7 @@ cs_convection_diffusion_thermal(int                       idtvar,
 
   /* Local variables */
 
-  char var_name[32];
+  char var_name[64];
 
   cs_gnum_t n_upwind;
   int iupwin;
@@ -8021,7 +8021,7 @@ cs_anisotropic_diffusion_scalar(int                       idtvar,
 
   cs_real_t *df_limiter = NULL;
 
-  char var_name[32];
+  char var_name[64];
 
   int tr_dim = 0;
   int w_stride = 1;
@@ -8788,7 +8788,7 @@ cs_anisotropic_left_diffusion_vector(int                         idtvar,
 
   cs_real_t *df_limiter = NULL;
 
-  char var_name[32];
+  char var_name[64];
 
   cs_real_33_t *gradv;
   cs_real_t *bndcel;
@@ -9337,7 +9337,7 @@ cs_anisotropic_right_diffusion_vector(int                         idtvar,
 
   cs_real_t *df_limiter = NULL;
 
-  char var_name[32];
+  char var_name[64];
 
   cs_real_6_t *viscce;
   cs_real_33_t *grad;
@@ -10037,7 +10037,7 @@ cs_anisotropic_diffusion_tensor(int                         idtvar,
   cs_real_t *cv_limiter = NULL;
   cs_real_t *df_limiter = NULL;
 
-  char var_name[32];
+  char var_name[64];
 
   cs_real_6_t *viscce;
   cs_real_6_t *w2;
@@ -10652,7 +10652,7 @@ cs_face_diffusion_potential(const int                 f_id,
 
   /* Local variables */
 
-  char var_name[32];
+  char var_name[64];
   int tr_dim = 0;
   int w_stride = 1;
 
@@ -11016,7 +11016,7 @@ cs_face_anisotropic_diffusion_potential(const int                 f_id,
 
   cs_real_t *df_limiter = NULL;
 
-  char var_name[32];
+  char var_name[64];
   int tr_dim = 0;
   int w_stride = 6;
 
@@ -11493,7 +11493,7 @@ cs_diffusion_potential(const int                 f_id,
 
   /* Local variables */
 
-  char var_name[32];
+  char var_name[64];
   int tr_dim = 0;
   int mass_flux_rec_type = cs_glob_stokes_model->irecmf;
   int w_stride = 1;
@@ -11877,7 +11877,7 @@ cs_anisotropic_diffusion_potential(const int                 f_id,
   cs_real_t *cv_limiter = NULL;
   cs_real_t *df_limiter = NULL;
 
-  char var_name[32];
+  char var_name[64];
   int tr_dim = 0;
   int w_stride = 6;
 

src/alge/cs_divergence.c
@@ -443,7 +443,7 @@ cs_mass_flux(const cs_mesh_t          *m,
   const cs_real_3_t *restrict dofij
     = (const cs_real_3_t *restrict)fvq->dofij;
 
-  char var_name[32];
+  char var_name[64];
 
   cs_real_3_t *qdm, *f_momentum, *coefaq;
   cs_real_33_t *grdqdm;
@@ -979,7 +979,7 @@ cs_tensor_face_flux(const cs_mesh_t          *m,
 
   /* Local variables */
 
-  char var_name[32];
+  char var_name[64];
 
   cs_real_6_t *c_mass_var, *b_mass_var, *coefaq;
 