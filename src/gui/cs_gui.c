code-saturne/code_saturne

commit f5d8002104b7c9c7c2d6df1add2a31ffc51f8762

Allow finer control of extended neighborhood.

src/gui/cs_gui.c
@@ -55,6 +55,7 @@
 #include "cs_boundary_zone.h"
 #include "cs_equation.h"
 #include "cs_equation_param.h"
+#include "cs_ext_neighborhood.h"
 #include "cs_field.h"
 #include "cs_field_pointer.h"
 #include "cs_file.h"
@@ -2116,12 +2117,63 @@ void CS_PROCF (uinum1, UINUM1) (double  *cdtvar)
  * INTEGER          IMRGRA  -->   gradient reconstruction
  *----------------------------------------------------------------------------*/
 
-void CS_PROCF (csnum2, CSNUM2)(double *relaxp,
-                               double *extrag,
-                                  int *imrgra)
+void CS_PROCF (csnum2, CSNUM2)(double  *relaxp,
+                               double  *extrag,
+                               int     *imrgra)
 {
   cs_piso_t *piso = cs_get_glob_piso();
   cs_stokes_model_t *stokes = cs_get_glob_stokes_model();
+
+  const char *choice = NULL;
+
+  cs_tree_node_t *tn_n = cs_tree_get_node(cs_glob_tree, "numerical_parameters");
+
+  int _imrgra = -1;
+
+  cs_ext_neighborhood_type_t enh_type = cs_ext_neighborhood_get_type();
+
+  choice = cs_tree_node_get_tag(cs_tree_get_node(tn_n, "gradient_reconstruction"),
+                                "choice");
+  if (cs_gui_strcmp(choice, "green_iter"))
+    _imrgra = 0;
+  else if (cs_gui_strcmp(choice, "lsq"))
+    _imrgra = 1;
+  else if (cs_gui_strcmp(choice, "lsq_iter"))
+    _imrgra = 4;
+
+  if (_imrgra != 0) {
+    choice = cs_tree_node_get_tag(cs_tree_get_node(tn_n, "extended_neighborhood"),
+                                  "choice");
+    if (cs_gui_strcmp(choice, "none")) {
+      enh_type = CS_EXT_NEIGHBORHOOD_NONE;
+    }
+    else if (cs_gui_strcmp(choice, "complete")) {
+      enh_type = CS_EXT_NEIGHBORHOOD_COMPLETE;
+      _imrgra += 1;
+    }
+    else if (cs_gui_strcmp(choice, "cell_center_opposite")) {
+      enh_type = CS_EXT_NEIGHBORHOOD_CELL_CENTER_OPPOSITE;
+      _imrgra += 2;
+    }
+    else if (cs_gui_strcmp(choice, "face_center_opposite")) {
+      enh_type = CS_EXT_NEIGHBORHOOD_FACE_CENTER_OPPOSITE;
+      _imrgra += 2;
+    }
+    else if (cs_gui_strcmp(choice, "face_center_aligned")) {
+      enh_type = CS_EXT_NEIGHBORHOOD_FACE_CENTER_ALIGNED;
+      _imrgra += 2;
+    }
+    else if (cs_gui_strcmp(choice, "non_ortho_max")) {
+      enh_type = CS_EXT_NEIGHBORHOOD_NON_ORTHO_MAX;
+      _imrgra += 2;
+    }
+  }
+
+  cs_ext_neighborhood_set_type(enh_type);
+
+  if (_imrgra > -1)
+    *imrgra = _imrgra;
+
   _numerical_int_parameters("gradient_transposed", &(stokes->ivisse));
   _numerical_int_parameters("velocity_pressure_coupling", &(stokes->ipucou));
   _numerical_int_parameters("gradient_reconstruction", imrgra);

bin/model/NumericalParamGlobalModel.py
@@ -79,12 +79,8 @@ class NumericalParamGlobalModel(Model):
         self.default['hydrostatic_equilibrium'] ='off'
         self.default['wall_pressure_extrapolation'] = 'neumann'
         self.default['time_scheme_order'] = 1
-        from code_saturne.model.GroundwaterModel import GroundwaterModel
-        if GroundwaterModel(self.case).getGroundwaterModel() != 'off':
-            self.default['gradient_reconstruction'] = 1
-        else:
-            self.default['gradient_reconstruction'] = 0
-        del GroundwaterModel
+        self.default['gradient_reconstruction'] = 'default'
+        self.default['extended_neighborhood'] = 'default'
         return self.default
 
 
@@ -186,13 +182,24 @@ class NumericalParamGlobalModel(Model):
     @Variables.noUndo
     def getGradientReconstruction(self):
         """
-        Return IMRGRA value : 0, 1, 2, 3, 4, 5, or 6
+        Return gradient reconstruction method
         """
         node = self.node_np.xmlInitNode('gradient_reconstruction', 'choice')
         choice = node['choice']
         if not choice:
             choice = self._defaultValues()['gradient_reconstruction']
-            self.setGradientReconstruction(choice)
+        return choice
+
+
+    @Variables.noUndo
+    def getExtendedNeighborType(self):
+        """
+        Return extended neighborhood type
+        """
+        node = self.node_np.xmlInitNode('extended_neighborhood', 'choice')
+        choice = node['choice']
+        if not choice:
+            choice = self._defaultValues()['extended_neighborhood']
         return choice
 
 
@@ -287,10 +294,23 @@ class NumericalParamGlobalModel(Model):
         """
         Put value of gradient_reconstruction
         """
-        self.isInt(value)
-        self.isInList(value, (0, 1, 2, 3, 4, 5, 6))
         node = self.node_np.xmlInitNode('gradient_reconstruction', 'choice')
-        node['choice'] = value
+        if value == self._defaultValues()['gradient_reconstruction']:
+            node.xmlRemoveNode()
+        else:
+            node['choice'] = value
+
+
+    @Variables.undoLocal
+    def setExtendedNeighborType(self, value):
+        """
+        Put value of extended_neighborhood
+        """
+        node = self.node_np.xmlInitNode('extended_neighborhood', 'choice')
+        if value == self._defaultValues()['extended_neighborhood']:
+            node.xmlRemoveNode()
+        else:
+            node['choice'] = value
 
 
     @Variables.undoLocal

bin/model/XMLinitialize.py
@@ -1687,6 +1687,7 @@ class XMLinit(BaseXmlInit):
         """
         Change XML in order to ensure backward compatibility.
         """
+
         # Reference values
         XMLThermoPhysicalNode = self.case.xmlInitNode('thermophysical_models')
         nodeRefValues = XMLThermoPhysicalNode.xmlInitNode('reference_values')
@@ -1782,6 +1783,29 @@ class XMLinit(BaseXmlInit):
                     pattern = '\\bmesh_z\\b'
                     content = re.sub(pattern, 'mesh_displacement[2]', content)
                     nn.xmlSetTextNode(content)
+
+        # Update gradients handling
+
+        node_np = self.case.xmlInitNode('numerical_parameters')
+        node = node_np.xmlGetNode('gradient_reconstruction')
+        if node:
+            if node['choice'] in ('0', '1', '2', '3', '4', '5', '6'):
+                c = int(node['choice'])
+                if c == 0:
+                    node['choice'] = 'green_iter'
+                elif c in (1, 2, 3):
+                    node['choice'] = 'lsq'
+                elif c in (4, 5, 6):
+                    node['choice'] = 'green_lsq'
+                if c > 0:
+                    node = node_np.xmlInitNode('extended_neighborhood', 'choice')
+                    if c in (1, 4):
+                        node['choice'] = 'none'
+                    elif c in (2, 5):
+                        node['choice'] = 'complete'
+                    else:
+                        node['choice'] = 'non_ortho_max'
+
 
 #-------------------------------------------------------------------------------
 # End of XMLinit

docs/doxygen/images/ext_neighborhood_cell_center_opposite.svg
@@ -1,0 +1,267 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!-- Created with Inkscape (http://www.inkscape.org/) -->
+
+<svg
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   width="99.450745mm"
+   height="83.334229mm"
+   viewBox="0 0 99.450745 83.334229"
+   version="1.1"
+   id="svg8"
+   sodipodi:docname="neighbor_ext_min3.svg"
+   inkscape:version="0.92.4 5da689c313, 2019-01-14">
+  <defs
+     id="defs2" />
+  <sodipodi:namedview
+     id="base"
+     pagecolor="#ffffff"
+     bordercolor="#666666"
+     borderopacity="1.0"
+     inkscape:pageopacity="0.0"
+     inkscape:pageshadow="2"
+     inkscape:zoom="2.8"
+     inkscape:cx="185.05016"
+     inkscape:cy="217.43497"
+     inkscape:document-units="mm"
+     inkscape:current-layer="layer1"
+     showgrid="false"
+     inkscape:window-width="1608"
+     inkscape:window-height="1015"
+     inkscape:window-x="144"
+     inkscape:window-y="0"
+     inkscape:window-maximized="0" />
+  <metadata
+     id="metadata5">
+    <rdf:RDF>
+      <cc:Work
+         rdf:about="">
+        <dc:format>image/svg+xml</dc:format>
+        <dc:type
+           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+        <dc:title></dc:title>
+      </cc:Work>
+    </rdf:RDF>
+  </metadata>
+  <g
+     inkscape:label="Calque 1"
+     inkscape:groupmode="layer"
+     id="layer1"
+     transform="translate(-29.307274,-146.88414)">
+    <path
+       style="fill:#000000;fill-opacity:0.19047618;stroke:#000000;stroke-width:0.26458332px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+       d="M 61.043155,191.92262 102.43155,167.54315 58.379954,209.829 Z"
+       id="path3721"
+       inkscape:connector-curvature="0"
+       sodipodi:nodetypes="cccc" />
+    <path
+       style="fill:none;stroke:#000000;stroke-width:0.26458332px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:0.25098039"
+       d="m 81.116301,163.76614 -0.267269,-16.57071 21.582518,20.34772"
+       id="path3749"
+       inkscape:connector-curvature="0" />
+    <path
+       style="fill:none;stroke:#000000;stroke-width:0.26499999;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:0.25"
+       d="m 80.84903,147.19543 47.44034,11.22532"
+       id="path3751"
+       inkscape:connector-curvature="0" />
+    <path
+       style="fill:none;stroke:#000000;stroke-width:0.26458332px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:0.25098039"
+       d="m 102.43155,167.54315 25.85782,-9.1224"
+       id="path3753"
+       inkscape:connector-curvature="0" />
+    <path
+       style="fill:none;stroke:#000000;stroke-width:0.26458332px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:0.25098039"
+       d="m 58.379955,209.829 6.432904,19.95271"
+       id="path3755"
+       inkscape:connector-curvature="0" />
+    <path
+       style="fill:none;stroke:#000000;stroke-width:0.26458332px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:0.25098039"
+       d="m 46.516023,229.68722 18.296836,0.0945"
+       id="path3757"
+       inkscape:connector-curvature="0"
+       sodipodi:nodetypes="cc" />
+    <path
+       style="fill:none;fill-opacity:0.19215686;stroke:#000000;stroke-width:0.26458332px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:0.25098039"
+       d="m 61.043155,191.92262 -14.93916,-2.76588 -16.570712,31.00327 31.509872,-28.23739"
+       id="path4596"
+       inkscape:connector-curvature="0" />
+    <path
+       style="fill:none;fill-opacity:0.19047618;stroke:#000000;stroke-width:0.26458332px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:0.25098039"
+       d="m 58.379955,209.829 -11.863933,19.85822 18.296837,0.0945 -6.432904,-19.95271 6.432904,19.9527 4.499273,-14.35629 -10.932177,-5.59642"
+       id="path4598"
+       inkscape:connector-curvature="0"
+       sodipodi:nodetypes="ccccccc" />
+    <path
+       style="fill:#00fe00;fill-opacity:0.19047618;stroke:#000000;stroke-width:0.26458332px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:0.25098039"
+       d="M 102.43155,167.54315 58.379955,209.829 69.312132,215.42542 102.43155,167.54315"
+       id="path4602"
+       inkscape:connector-curvature="0"
+       sodipodi:nodetypes="cccc" />
+    <path
+       style="fill:none;fill-opacity:0.19215686;stroke:#000000;stroke-width:0.26720655px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:0.25098039"
+       d="M 102.43155,167.54315 69.312132,215.42542 115.45806,171.52044 Z"
+       id="path4606"
+       inkscape:connector-curvature="0"
+       sodipodi:nodetypes="cccc" />
+    <path
+       style="fill:none;stroke:#000000;stroke-width:0.26458332px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:0.25098039"
+       d="m 102.43155,167.54315 25.85782,-9.1224 -47.44034,-11.22532 21.58252,20.34772 -21.582518,-20.34772 0.267269,16.57071 21.315249,3.77701 -21.315249,-3.77701 -35.012306,25.3906 56.327555,-21.61359"
+       id="path4608"
+       inkscape:connector-curvature="0" />
+    <path
+       style="fill:#00fe00;fill-opacity:0.19047618;stroke:#000000;stroke-width:0.26458332px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:0.25098039"
+       d="m 102.43155,167.54315 -56.327555,21.61359 14.93916,2.76588 z"
+       id="path4610"
+       inkscape:connector-curvature="0"
+       sodipodi:nodetypes="cccc" />
+    <path
+       style="fill:#00fe00;fill-opacity:0.19047618;stroke:#000000;stroke-width:0.26458332px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:0.25098039"
+       d="m 61.043155,191.92262 -14.527133,37.7646 11.863933,-19.85821 z"
+       id="path4612"
+       inkscape:connector-curvature="0" />
+    <path
+       style="fill:none;fill-opacity:0.19215686;stroke:#000000;stroke-width:0.26458332px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:0.25098039"
+       d="m 61.043155,191.92262 -31.509872,28.23739 16.982739,9.52721 14.527133,-37.7646"
+       id="path4646"
+       inkscape:connector-curvature="0" />
+    <path
+       style="fill:none;stroke:#003b00;stroke-width:0.26499999;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:0.265, 0.795;stroke-dashoffset:0;stroke-opacity:1"
+       d="m 55.799382,210.50303 43.399617,-55.35678 v 0"
+       id="path4648"
+       inkscape:connector-curvature="0"
+       sodipodi:nodetypes="ccc" />
+    <path
+       style="fill:none;stroke:#005900;stroke-width:0.26499999;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:0.265, 1.59;stroke-dashoffset:0;stroke-opacity:1"
+       d="m 72.651888,226.33235 c -3.710862,-31.56562 -4.88561,-41.8888 -4.88561,-41.8888"
+       id="path4650"
+       inkscape:connector-curvature="0"
+       sodipodi:nodetypes="cc" />
+    <path
+       style="fill:none;stroke:#005900;stroke-width:0.26312754;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:0.26312754, 1.05251018;stroke-dashoffset:0;stroke-opacity:1"
+       d="M 74.418663,200.39137 C 47.697291,169.23742 48.141238,169.75229 48.141238,169.75229"
+       id="path4650-3"
+       inkscape:connector-curvature="0" />
+    <ellipse
+       style="fill:#000000;fill-opacity:0.25;stroke:#000000;stroke-width:0.1;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:0.50196078"
+       id="path6360-56"
+       cx="55.656986"
+       cy="210.53795"
+       rx="0.75595242"
+       ry="0.80319941" />
+    <ellipse
+       style="fill:#000000;fill-opacity:0.25;stroke:#000000;stroke-width:0.1;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:0.50196078"
+       id="path6360-56-2"
+       cx="67.941223"
+       cy="184.07961"
+       rx="0.75595242"
+       ry="0.80319941" />
+    <ellipse
+       style="fill:#000000;fill-opacity:0.25;stroke:#000000;stroke-width:0.1;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:0.50196078"
+       id="path6360-56-1"
+       cx="74.461304"
+       cy="200.42708"
+       rx="0.75595242"
+       ry="0.80319941" />
+    <ellipse
+       style="fill:#000000;fill-opacity:0.25;stroke:#000000;stroke-width:0.1;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:0.50196078"
+       id="path6360-56-9-2"
+       cx="80.130951"
+       cy="170.94493"
+       rx="0.75595242"
+       ry="0.80319941" />
+    <ellipse
+       style="fill:none;fill-opacity:0.25;stroke:#000000;stroke-width:0.1;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:0.50196078"
+       id="path6360-56-9-7"
+       cx="103.37649"
+       cy="159.03868"
+       rx="0.75595242"
+       ry="0.80319941" />
+    <ellipse
+       style="fill:none;fill-opacity:0.25098039;stroke:#000000;stroke-width:0.1;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:0.50196078"
+       id="path6360-56-9-0"
+       cx="98.681908"
+       cy="181.16209"
+       rx="0.75595242"
+       ry="0.80319941" />
+    <ellipse
+       style="fill:#000000;fill-opacity:0.25;stroke:#000000;stroke-width:0.1;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:0.50196078"
+       id="path6360-56-9-9"
+       cx="68.791664"
+       cy="193.6235"
+       rx="0.75595242"
+       ry="0.80319941" />
+    <ellipse
+       style="fill:none;fill-opacity:0.25;stroke:#000000;stroke-width:0.1;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:0.50196078"
+       id="path6360-56-9-3"
+       cx="87.218002"
+       cy="159.60565"
+       rx="0.75595242"
+       ry="0.80319941" />
+    <ellipse
+       style="fill:none;fill-opacity:0.25;stroke:#000000;stroke-width:0.1;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:0.50196078"
+       id="path6360-56-9-6"
+       cx="47.436012"
+       cy="198.25372"
+       rx="0.75595242"
+       ry="0.80319941" />
+    <ellipse
+       style="fill:none;fill-opacity:0.25;stroke:#000000;stroke-width:0.1;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:0.50196078"
+       id="path6360-56-9-06"
+       cx="44.695686"
+       cy="214.97917"
+       rx="0.75595242"
+       ry="0.80319941" />
+    <ellipse
+       style="fill:none;fill-opacity:0.25;stroke:#000000;stroke-width:0.1;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:0.50196078"
+       id="path6360-56-9-26"
+       cx="56.129463"
+       cy="223.10565"
+       rx="0.75595242"
+       ry="0.80319941" />
+    <ellipse
+       style="fill:none;fill-opacity:0.25;stroke:#000000;stroke-width:0.1;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:0.50196078"
+       id="path6360-56-9-1"
+       cx="64.255943"
+       cy="218.09747"
+       rx="0.75595242"
+       ry="0.80319941" />
+    <path
+       style="fill:none;fill-opacity:0.19215686;stroke:#000000;stroke-width:0.26458332px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:0.25098039"
+       d="m 128.28937,158.42075 -25.85782,9.1224 13.02651,3.97729 12.83131,-13.09969"
+       id="path8340"
+       inkscape:connector-curvature="0" />
+    <ellipse
+       style="fill:none;fill-opacity:0.25;stroke:#000000;stroke-width:0.1;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:0.50196078"
+       id="path6360-56-9-7-7"
+       cx="114.25773"
+       cy="167.10701"
+       rx="0.75595242"
+       ry="0.80319941" />
+    <path
+       style="fill:none;stroke:#fd0000;stroke-width:0.25600001;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:0.256, 0.256;stroke-dashoffset:0;stroke-opacity:1"
+       d="m 80.458977,171.21759 3.690674,2.95097 v 0"
+       id="path11141"
+       inkscape:connector-curvature="0"
+       sodipodi:nodetypes="ccc" />
+    <path
+       style="fill:none;stroke:#ff0000;stroke-width:0.25600001;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:0.256, 0.256;stroke-dashoffset:0;stroke-opacity:1"
+       d="m 67.766278,184.44355 -4.055361,3.28592 v 0 0"
+       id="path11147"
+       inkscape:connector-curvature="0" />
+    <path
+       style="fill:none;stroke:#ff0000;stroke-width:0.27578473;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:0.27578474, 0.27578474;stroke-dashoffset:0;stroke-opacity:1"
+       d="m 69.814734,200.96864 4.623695,-0.40628"
+       id="path11153"
+       inkscape:connector-curvature="0" />
+    <path
+       style="fill:#edf67f;fill-opacity:0.19215686;stroke:#000000;stroke-width:0.26458332px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:0.25098039"
+       d="m 81.116301,163.76614 -35.012306,25.3906 56.327555,-21.61359 z"
+       id="path13348"
+       inkscape:connector-curvature="0" />
+  </g>
+</svg>

commit 9eed5446ed19f5ac5af92456f2ccf70b792bd2f6

GUI reader: fix handling of gradient reconstruction options.
This was broken by commit f5d80021 (Octeober 23 2019).

src/gui/cs_gui.c
@@ -2138,7 +2138,7 @@ void CS_PROCF (csnum2, CSNUM2)(double  *relaxp,
     _imrgra = 0;
   else if (cs_gui_strcmp(choice, "lsq"))
     _imrgra = 1;
-  else if (cs_gui_strcmp(choice, "lsq_iter"))
+  else if (cs_gui_strcmp(choice, "green_lsq"))
     _imrgra = 4;
 
   if (_imrgra != 0) {
@@ -2176,7 +2176,6 @@ void CS_PROCF (csnum2, CSNUM2)(double  *relaxp,
 
   _numerical_int_parameters("gradient_transposed", &(stokes->ivisse));
   _numerical_int_parameters("velocity_pressure_coupling", &(stokes->ipucou));
-  _numerical_int_parameters("gradient_reconstruction", imrgra);
   _numerical_int_parameters("piso_sweep_number", &(piso->nterup));
   _numerical_double_parameters("wall_pressure_extrapolation", extrag);
   _numerical_double_parameters("pressure_relaxation", relaxp);