code-saturne/code_saturne

commit 508664bcd778a23d9f3bba96de4199dffa075fd0

[atmo] Fix chemistry source term when other scalars are present (the temperature for instance).

src/base/covofi.f90
@@ -459,9 +459,9 @@ endif
 ! Atmospheric chemistry
 ! In case of a semi-coupled resolution, computation of the explicit
 ! chemical source term to be considered during dynamical resolution
-! The first nespg user scalars are supposed to be chemical species
 if ((ichemistry.ge.1).and.(isepchemistry.eq.2)                    &
-     .and.(iscal.le.nespg).and.(ntcabs.gt.1)) then
+     .and.(isca_chem(1).le.iscal).and.(iscal.le.isca_chem(nespg)) &
+     .and.(ntcabs.gt.1)) then
   call chem_source_terms(iscal, smbrs, rovsdt)
 endif
 

src/atmo/chem_source_terms.f90
@@ -130,7 +130,8 @@ do iel = 1, ncel
   ! Adding source term to crvexp
   ! The first nespg user scalars are supposed to be chemical species
   ! TODO: try to implicit the ST
-  crvexp(iel) = crvexp(iel)+dchema(chempoint(iscal))*rom*volume(iel)
+  crvexp(iel) = crvexp(iel) + rom * cell_f_vol(iel) &
+              * dchema(chempoint(iscal-isca_chem(1)+1))
 
 enddo
 

commit d49a9c7496de45ac548de2fc4b086621af1f038d

Fix test leading to crash in debug mode.
Broken by commit 508664bc (Nov 26 2019).

src/base/covofi.f90
@@ -459,12 +459,11 @@ endif
 ! Atmospheric chemistry
 ! In case of a semi-coupled resolution, computation of the explicit
 ! chemical source term to be considered during dynamical resolution
-if ((ichemistry.ge.1).and.(isepchemistry.eq.2)                    &
-     .and.(isca_chem(1).le.iscal).and.(iscal.le.isca_chem(nespg)) &
-     .and.(ntcabs.gt.1)) then
-  call chem_source_terms(iscal, smbrs, rovsdt)
+if ((ichemistry.ge.1) .and. (isepchemistry.eq.2) .and. (ntcabs.gt.1)) then
+  if ((isca_chem(1).le.iscal).and.(iscal.le.isca_chem(nespg))) then
+    call chem_source_terms(iscal, smbrs, rovsdt)
+  endif
 endif
-
 
 ! Precipitation/dissolution for lagrangian module
 ! Calculation of source terms du to precipitation and dissolution phenomena