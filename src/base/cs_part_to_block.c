e8b0b9ed3d77b434e5961a88a14aafe5d2330189
Yvan Fournier <yvan.fournier@noreply.edf.fr>
2019-12-30 01:13:39 +0100

    Fix comment.

--- src/base/cs_part_to_block.c
+++ src/base/cs_part_to_block.c
@@ -84,7 +84,7 @@ struct _cs_part_to_block_t {
 
   size_t       n_block_ents;    /* Number of entities to receive (this block) */
   size_t       n_part_ents;     /* Number of entities to send (partition) */
-  size_t       recv_size;       /* Size of receive buffer for MPI_Alltoallv
+  size_t       recv_size;       /* Size of receive buffer for MPI_Gatherv
                                    (send_size not necessary, as send_size
                                    should always be equal to n_part_ents,
                                    though elements may be assembled in a
