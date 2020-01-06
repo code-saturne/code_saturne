code-saturne/code_saturne

commit d1d143f02b0b5f2212add406936dd95af57cd714

Complete and fix handing of reverse exchange with dest_ids for cs_all_to_all.
This requires a minor API change in the cs_crystal_router API,
but allows correct behavior of reverse exchange in cases where multiple
ranks contribute to a same value.

src/base/cs_all_to_all.c
@@ -166,6 +166,10 @@ struct _cs_all_to_all_t {
   cs_lnum_t                  n_elts_src;   /* Number of source elements */
   cs_lnum_t                  n_elts_dest;  /* Number of destination elements
                                               (-1 before metadata available) */
+  cs_lnum_t                  n_elts_dest_e; /* Number of destination elements
+                                               exchanged, independently of
+                                                dest_id (-1 before metadata
+                                                available) */
 
   int                        flags;        /* option flags */
 
@@ -275,6 +279,7 @@ _all_to_all_create_base(size_t    n_elts,
 
   d->n_elts_src = n_elts;
   d->n_elts_dest = -1; /* undetermined as yet */
+  d->n_elts_dest_e = -1;
 
   d->flags = flags;
 
@@ -675,12 +680,20 @@ _alltoall_caller_prepare_i(_mpi_all_to_all_caller_t  *dc,
 
   /* Allocate send buffer */
 
-  if (reverse) {
+  if (reverse && recv_id == NULL) {
     BFT_FREE(dc->_send_buffer);
     dc->send_buffer = data;
   }
   else {
-    size_t n_sub_elts = src_index[n_elts];
+    size_t n_sub_elts = 0;
+    if (reverse == false || recv_id == NULL)
+      n_sub_elts = src_index[n_elts];
+    else {
+      for (size_t j = 0; j < dc->send_size; j++) {
+        cs_lnum_t k = recv_id[j];
+        n_sub_elts += src_index[k+1] - src_index[k];
+      }
+    }
     BFT_REALLOC(dc->_send_buffer, n_sub_elts*dc->comp_size, unsigned char);
     dc->send_buffer = dc->_send_buffer;
   }
@@ -766,20 +779,22 @@ _alltoall_caller_prepare_i(_mpi_all_to_all_caller_t  *dc,
       dc->send_displ[i] -= dc->send_count[i];
   }
   else if (recv_id != NULL) { /* reverse here */
-    for (i = 0; i < dc->n_ranks; i++) {
-      size_t i_s = dc->recv_displ[i];
-      size_t i_e = dc->recv_displ[i+1];
-      size_t w_displ = dc->send_displ[i]*elt_size;
-      for (size_t j = i_s; j < i_e; j++) {
-        cs_lnum_t k = recv_id[j];
-        cs_lnum_t n_sub_send = src_index[k+1] - src_index[k];
-        size_t r_displ = src_index[k]*elt_size;
-        size_t sub_size = elt_size*n_sub_send;
-        for (size_t l = 0; l < sub_size; l++)
-          dc->_send_buffer[w_displ + l] = _data[r_displ + l];
-        w_displ += sub_size;
-      }
+    size_t w_displ = 0;
+    for (size_t j = 0; j < dc->send_size; j++) {
+      cs_lnum_t k = recv_id[j];
+      cs_lnum_t n_sub_send = src_index[k+1] - src_index[k];
+      size_t r_displ = src_index[k]*elt_size;
+      size_t sub_size = elt_size*n_sub_send;
+      for (size_t l = 0; l < sub_size; l++)
+        dc->_send_buffer[w_displ + l] = _data[r_displ + l];
+      w_displ += sub_size;
     }
+
+    /* Recompute associated displacements */
+    _compute_displ(dc->n_ranks, dc->send_count, dc->send_displ);
+    _compute_displ(dc->n_ranks, dc->recv_count, dc->recv_displ);
+
+
   }
   else { /* If revert and no recv_id */
     assert(dc->send_buffer == data);
@@ -895,6 +910,7 @@ _alltoall_caller_exchange_s(cs_all_to_all_t           *d,
         dest_id_max = d->recv_id[i];
       d->n_elts_dest = dest_id_max + 1;
     }
+    d->n_elts_dest_e = d->dc->recv_size;
   }
 
   /* Now handle main data buffer (reverse implies reordering data) */
@@ -974,15 +990,8 @@ _alltoall_caller_exchange_i(cs_all_to_all_t           *d,
   /* Data buffer for MPI exchange (may merge data and metadata) */
 
   if (d->recv_id != NULL || reverse) {
-    size_t _n_dest_buf = _n_dest_sub;
-    if (d->recv_id != NULL) {
-      _n_dest_buf = 0;
-      for (size_t i = 0; i < d->dc->recv_size; i++) {
-        cs_lnum_t j = d->recv_id[i];
-        _n_dest_buf += dest_index[j+1] - dest_index[j];
-      }
-    }
-    BFT_MALLOC(_recv_data, _n_dest_buf*elt_size, unsigned char);
+    size_t _n_dest_buf = dc->recv_displ[dc->n_ranks] * dc->comp_size;
+    BFT_MALLOC(_recv_data, _n_dest_buf, unsigned char);
   }
   else
     _recv_data = _dest_data;
@@ -1172,7 +1181,7 @@ _cr_recv_id_by_src_rank(cs_all_to_all_t      *d,
   assert(d->recv_id == NULL);
 
   int *src_rank;
-  BFT_MALLOC(src_rank, d->n_elts_dest, int);
+  BFT_MALLOC(src_rank, d->n_elts_dest_e, int);
 
   cs_crystal_router_get_data(cr,
                              &src_rank,
@@ -1540,8 +1549,10 @@ cs_all_to_all_n_elts_dest(cs_all_to_all_t  *d)
                                    false,
                                    NULL,
                                    NULL);
-        else
+        else {
           d->n_elts_dest = d->dc->recv_size;
+          d->n_elts_dest_e = d->dc->recv_size;
+        }
       }
       break;
 
@@ -1554,6 +1565,7 @@ cs_all_to_all_n_elts_dest(cs_all_to_all_t  *d)
                                         _cr_flags(d, false),
                                        NULL,
                                        NULL,
+                                       d->dest_id,
                                        d->dest_rank,
                                        d->comm);
 
@@ -1567,6 +1579,7 @@ cs_all_to_all_n_elts_dest(cs_all_to_all_t  *d)
         _all_to_all_calls[CS_ALL_TO_ALL_TIME_METADATA] += 1;
 
         d->n_elts_dest = cs_crystal_router_n_elts(cr);
+        d->n_elts_dest_e = cs_crystal_router_n_recv_elts(cr);
 
         if (d->flags & CS_ALL_TO_ALL_ORDER_BY_SRC_RANK)
           _cr_recv_id_by_src_rank(d, cr);
@@ -1664,6 +1677,7 @@ cs_all_to_all_copy_array(cs_all_to_all_t   *d,
                                        d->n_elts_src,
                                        d->dest_rank);
         d->n_elts_dest = d->dc->recv_size;
+        d->n_elts_dest_e = d->dc->recv_size;
       }
       size_t n_elts = (reverse) ? d->n_elts_dest : d->n_elts_src;
       if (reverse)
@@ -1701,14 +1715,15 @@ cs_all_to_all_copy_array(cs_all_to_all_t   *d,
                                         datatype,
                                         _cr_flags(d, reverse),
                                         src_data,
+                                        NULL,
                                         d->dest_id,
                                         d->dest_rank,
                                         d->comm);
         tcr0 = cs_timer_time();
         cs_crystal_router_exchange(cr);
         tcr1 = cs_timer_time();
-        if (d->n_elts_dest < 0) {
-          d->n_elts_dest = cs_crystal_router_n_elts(cr);
+        if (d->n_elts_dest_e < 0) {
+          d->n_elts_dest_e = cs_crystal_router_n_recv_elts(cr);
           if (d->flags & CS_ALL_TO_ALL_ORDER_BY_SRC_RANK)
             _cr_recv_id_by_src_rank(d, cr);
         }
@@ -1719,13 +1734,18 @@ cs_all_to_all_copy_array(cs_all_to_all_t   *d,
                                    &(d->src_id),
                                    NULL, /* dest_index */
                                    &_dest_data);
+        if (d->n_elts_dest < 0)
+          d->n_elts_dest = cs_crystal_router_n_elts(cr);
       }
       else {
-        cr = cs_crystal_router_create_s(d->n_elts_dest,
+        const cs_lnum_t *elt_id
+          = (d->n_elts_dest < d->n_elts_dest_e) ? d->recv_id : NULL;
+        cr = cs_crystal_router_create_s(d->n_elts_dest_e,
                                         stride,
                                         datatype,
                                         _cr_flags(d, reverse),
                                         src_data,
+                                        elt_id,
                                         d->src_id,
                                         d->src_rank,
                                         d->comm);
@@ -1795,11 +1815,14 @@ cs_all_to_all_copy_index(cs_all_to_all_t  *d,
   cs_lnum_t *src_count = NULL;
   cs_lnum_t *_dest_index = dest_index;
 
+  cs_all_to_all_n_elts_dest(d); /* force sync. if needed */
+
   cs_lnum_t n_src = (reverse) ? d->n_elts_dest : d->n_elts_src;
   cs_lnum_t n_dest = -1;
 
-  if (dest_index == NULL)
-    n_dest = (reverse) ? d->n_elts_src : cs_all_to_all_n_elts_dest(d);
+  if (dest_index == NULL) {
+    n_dest = (reverse) ? d->n_elts_src : d->n_elts_dest;
+  }
 
   t0 = cs_timer_time();
 
@@ -1953,14 +1976,17 @@ cs_all_to_all_copy_indexed(cs_all_to_all_t  *d,
                                         _cr_flags(d, reverse),
                                         src_index,
                                         src_data,
+                                        NULL,
                                         d->dest_id,
                                         d->dest_rank,
                                         d->comm);
         tcr0 = cs_timer_time();
         cs_crystal_router_exchange(cr);
         tcr1 = cs_timer_time();
-        if (d->n_elts_dest < 0)
+        if (d->n_elts_dest < 0) {
           d->n_elts_dest = cs_crystal_router_n_elts(cr);
+          d->n_elts_dest_e = cs_crystal_router_n_recv_elts(cr);
+        }
         int **p_src_rank = (d->src_rank == NULL) ? &(d->src_rank) : NULL;
         cs_crystal_router_get_data(cr,
                                    p_src_rank,
@@ -1970,11 +1996,14 @@ cs_all_to_all_copy_indexed(cs_all_to_all_t  *d,
                                    &_dest_data);
       }
       else {
-        cr = cs_crystal_router_create_i(d->n_elts_dest,
+        const cs_lnum_t *elt_id
+          = (d->n_elts_dest < d->n_elts_dest_e) ? d->recv_id : NULL;
+        cr = cs_crystal_router_create_i(d->n_elts_dest_e,
                                         datatype,
                                         _cr_flags(d, reverse),
                                         src_index,
                                         src_data,
+                                        elt_id,
                                         d->src_id,
                                         d->src_rank,
                                         d->comm);

src/base/cs_crystal_router.h
@@ -92,6 +92,8 @@ typedef struct _cs_crystal_router_t  cs_crystal_router_t;
  * \param[in]  datatype          type of data considered
  * \param[in]  flags             add destination id ?
  * \param[in]  elt               element values
+ * \param[in]  src_id            optional parent element id (indirection
+ *                               for elt array), or NULL
  * \param[in]  dest_id           element destination id, or NULL
  * \param[in]  dest_rank         destination rank for each element
  * \param[in]  comm              associated MPI communicator
@@ -106,6 +108,7 @@ cs_crystal_router_create_s(size_t            n_elts,
                            cs_datatype_t     datatype,
                            int               flags,
                            const void       *elt,
+                           const cs_lnum_t  *src_id,
                            const cs_lnum_t  *dest_id,
                            const int         dest_rank[],
                            MPI_Comm          comm);
@@ -129,6 +132,8 @@ cs_crystal_router_create_s(size_t            n_elts,
  * \param[in]  flags             add destination id ?
  * \param[in]  elt_idx           element values start and past-the-last index
  * \param[in]  elt               element values
+ * \param[in]  src_id            optional parent element id (indirection
+ *                               for elt and elt_idx arrays), or NULL
  * \param[in]  dest_id           element destination id, or NULL
  * \param[in]  dest_rank         destination rank for each element
  * \param[in]  comm              associated MPI communicator
@@ -143,6 +148,7 @@ cs_crystal_router_create_i(size_t            n_elts,
                            int               flags,
                            const cs_lnum_t  *eld_idx,
                            const void       *elt,
+                           const cs_lnum_t  *src_id,
                            const cs_lnum_t  *dest_id,
                            const int         dest_rank[],
                            MPI_Comm          comm);
@@ -188,6 +194,26 @@ cs_crystal_router_n_elts(const cs_crystal_router_t  *cr);
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief Get number of elements received with Crystal Router.
+ *
+ * The number of elements is the number of elements received in the exchange.
+ * In most cases this is the same as that returned using
+ * \ref cs_crystal_router_n_elts, but may be larger when a destination id is
+ * used and multiple elements point to a same destination (for example when
+ * distributing from partitioned data with elements duplicated on
+ * partition boundaries to blocks with a single occurence of elements).
+ *
+ * \param[in]  cr  pointer to associated Crystal Router
+ *
+ * \return  number of elements associated with distributor.
+ */
+/*----------------------------------------------------------------------------*/
+
+cs_lnum_t
+cs_crystal_router_n_recv_elts(const cs_crystal_router_t  *cr);
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief Get data elements associated with a Crystal Router.
  *
  * Depending on the creation options, some elements may be available or not.
@@ -200,13 +226,24 @@ cs_crystal_router_n_elts(const cs_crystal_router_t  *cr);
  * to the caller (who is responsible for freeing it when no longer needed).
  *
  * Note also that if the destination id is provided in the Crystal Router,
- * it will be applied automatically to all extracted arrays, except the
- * dest_id array itself, which is always in receive order. If the Crystal
+ * it will be applied automatically to the data and data_index arrays. The
+ * dest_id array itself is always in receive order. If the Crystal
  * Router does not contain destination id info but the \c dest_id
  * argument points to a non-NULL value, the provided id will be used to
  * order extracted data. This allows saving the destination id on the receive
  * side, and not re-sending it (saving bandwidth) for subsequent calls
  * with a similar Crystal Router.
+ *
+ * If all destination ids appear only once, or the
+ * CS_CRYSTAL_ROUTER_USE_DEST_ID is not set, dest_id is also applied to
+ * src_rank and src_id. Otherwise, those arrays are kept in buffer order.
+ * This can be checked by comparing the output of
+ * \ref cs_crystal_router_n_elts and \ref cs_crystal_router_n_recv_elts.
+ *
+ * With this behavior, for reverse exchange, src_id and src_rank can be
+ * used as dest_id and dest_rank respectively in the call to
+ * \ref cs_crystal_router_create_s, while NULL is passed to elt_id
+ * with single ids, and dest_id passed in case of duplicate ids.
  *
  * \param[in]       cr          pointer to associated Crystal Router
  * \param[out]      src_rank    pointer to (pointer to) source rank array,

src/base/cs_rank_neighbors.c
@@ -653,6 +653,7 @@ cs_rank_neighbors_symmetrize(cs_rank_neighbors_t  *n,
                                                          flags,
                                                          NULL,
                                                          NULL,
+                                                         NULL,
                                                          n->rank,
                                                          comm);
 
@@ -900,6 +901,7 @@ cs_rank_neighbors_sync_count(const cs_rank_neighbors_t   *n_send,
                                                          CS_LNUM_TYPE,
                                                          flags,
                                                          send_count,
+                                                         NULL,
                                                          NULL,
                                                          n_send->rank,
                                                          comm);

tests/cs_all_to_all_test.c
@@ -37,6 +37,8 @@
 #include <bft_printf.h>
 
 #include "cs_base.h"
+#include "cs_block_dist.h"
+
 #include "cs_all_to_all.h"
 
 /*---------------------------------------------------------------------------*/
@@ -130,66 +132,119 @@ main (int argc, char *argv[])
   sprintf(mem_trace_name, "cs_all_to_all_test_mem.%d", rank);
   bft_mem_init(mem_trace_name);
 
-  cs_all_to_all_type_t a2at[3] = {CS_ALL_TO_ALL_MPI_DEFAULT,
+  cs_all_to_all_type_t a2at[5] = {CS_ALL_TO_ALL_MPI_DEFAULT,
                                   CS_ALL_TO_ALL_CRYSTAL_ROUTER,
+                                  CS_ALL_TO_ALL_CRYSTAL_ROUTER,
+                                  CS_ALL_TO_ALL_MPI_DEFAULT,
                                   CS_ALL_TO_ALL_CRYSTAL_ROUTER};
 
-  int a2a_flags[3] = {0, 0, CS_ALL_TO_ALL_ORDER_BY_SRC_RANK};
+  int a2a_flags[5] = {0, 0, CS_ALL_TO_ALL_ORDER_BY_SRC_RANK,
+                      CS_ALL_TO_ALL_USE_DEST_ID,
+                      CS_ALL_TO_ALL_USE_DEST_ID};
 
-  for (int test_id = 0; test_id < 3; test_id++) {
+  for (int test_id = 0; test_id < 5; test_id++) {
 
     cs_all_to_all_set_type(a2at[test_id]);
 
     int flags = a2a_flags[test_id];
 
     bft_printf("\n"
-               "Using all-to-all type %d (flags %d)\n"
-               "---------------------\n\n",
-               (int)a2at[test_id], flags);
+               "%d - Using all-to-all type %d (flags %d)\n"
+               "---------------------------\n\n",
+               test_id, (int)a2at[test_id], flags);
 
     /* Build test array */
 
-    cs_lnum_t n_elts = 3 + rank%3;
-
-    BFT_MALLOC(dest_rank, n_elts, int);
-
-    for (cs_lnum_t ii = 0; ii < n_elts; ii++) {
-      int _rank = rank + ii%5 - 2;
-      if (_rank < 0) _rank = 0;
-      if (_rank >= size) _rank = size-1;
-      dest_rank[ii] = _rank;
-    }
-
+    cs_lnum_t n_elts = 0;
     cs_lnum_t *src_index = NULL;
-    BFT_MALLOC(src_index, n_elts + 1, cs_lnum_t);
-
-    src_index[0] = 0;
-    for (cs_lnum_t ii = 0; ii < n_elts; ii++)
-      src_index[ii+1] = src_index[ii] + 2 + ii%2;
-
     cs_gnum_t *src_val = NULL;
-    BFT_MALLOC(src_val, src_index[n_elts], cs_gnum_t);
-    for (cs_lnum_t ii = 0; ii < n_elts; ii++) {
-      bft_printf("%d -> rank %d :", ii, dest_rank[ii]);
-      src_val[src_index[ii]] = ii;
-      src_val[src_index[ii]+1] = rank;
-      for (cs_lnum_t jj = src_index[ii] + 2;
-           jj < src_index[ii+1];
-           jj++)
-        src_val[jj] = jj;
-      for (cs_lnum_t jj = src_index[ii];
-           jj < src_index[ii+1];
-           jj++)
-        bft_printf(" %llu", (unsigned long long)src_val[jj]);
-      bft_printf("\n");
+    cs_gnum_t *part_gnum = NULL;
+    cs_all_to_all_t *d = NULL;
+
+    if (test_id < 3) {
+
+      n_elts = 3 + rank%3;
+
+      BFT_MALLOC(dest_rank, n_elts, int);
+
+      for (cs_lnum_t ii = 0; ii < n_elts; ii++) {
+        int _rank = rank + ii%5 - 2;
+        if (_rank < 0) _rank = 0;
+        if (_rank >= size) _rank = size-1;
+        dest_rank[ii] = _rank;
+      }
+
+      BFT_MALLOC(src_index, n_elts + 1, cs_lnum_t);
+      src_index[0] = 0;
+      for (cs_lnum_t ii = 0; ii < n_elts; ii++)
+        src_index[ii+1] = src_index[ii] + 2 + ii%2;
+
+      BFT_MALLOC(src_val, src_index[n_elts], cs_gnum_t);
+      for (cs_lnum_t ii = 0; ii < n_elts; ii++) {
+        bft_printf("%d -> rank %d :", ii, dest_rank[ii]);
+        src_val[src_index[ii]] = ii;
+        src_val[src_index[ii]+1] = rank;
+        for (cs_lnum_t jj = src_index[ii] + 2;
+             jj < src_index[ii+1];
+             jj++)
+          src_val[jj] = jj;
+        for (cs_lnum_t jj = src_index[ii];
+             jj < src_index[ii+1];
+             jj++)
+          bft_printf(" %llu", (unsigned long long)src_val[jj]);
+        bft_printf("\n");
+      }
+
+      d = cs_all_to_all_create(n_elts,
+                               flags,
+                               NULL, /* dest_id */
+                               dest_rank,
+                               cs_glob_mpi_comm);
+
     }
+    else {
 
-    cs_all_to_all_t *d = cs_all_to_all_create(n_elts,
-                                              flags,
-                                              NULL, /* dest_id */
-                                              dest_rank,
-                                              cs_glob_mpi_comm);
+      n_elts = 7;
+      cs_gnum_t n_g_elts = n_elts + (size-1)*(n_elts-2);
 
+      const cs_block_dist_info_t  bi = cs_block_dist_compute_sizes(rank,
+                                                                   size,
+                                                                   1,
+                                                                   0,
+                                                                   n_g_elts);
+
+      BFT_MALLOC(part_gnum, n_elts, cs_gnum_t);
+
+      for (cs_lnum_t ii = 0; ii < n_elts; ii++) {
+        part_gnum[ii] = ii+1 + rank*(n_elts-2);
+      }
+
+      d = cs_all_to_all_create_from_block(n_elts,
+                                          flags,
+                                          part_gnum,
+                                          bi,
+                                          cs_glob_mpi_comm);
+
+      BFT_MALLOC(src_index, n_elts + 1, cs_lnum_t);
+      src_index[0] = 0;
+      for (cs_lnum_t ii = 0; ii < n_elts; ii++)
+        src_index[ii+1] = src_index[ii] + 2 + part_gnum[ii]%2;
+
+      BFT_MALLOC(src_val, src_index[n_elts], cs_gnum_t);
+      for (cs_lnum_t ii = 0; ii < n_elts; ii++) {
+        bft_printf("%d -> gnum %d :", ii, (int)part_gnum[ii]);
+        for (cs_lnum_t jj = src_index[ii];
+             jj < src_index[ii+1];
+             jj++)
+          src_val[jj] = part_gnum[ii] + jj - src_index[ii];
+        for (cs_lnum_t jj = src_index[ii];
+             jj < src_index[ii+1];
+             jj++)
+          bft_printf(" %llu", (unsigned long long)src_val[jj]);
+        bft_printf("\n");
+      }
+
+    }
 
     cs_lnum_t *dest_index = cs_all_to_all_copy_index(d,
                                                      false, /* reverse */
@@ -205,6 +260,8 @@ main (int argc, char *argv[])
                                                      NULL);
 
     cs_lnum_t n_elts_dest = cs_all_to_all_n_elts_dest(d);
+
+    bft_printf("\n");
 
     for (cs_lnum_t ii = 0; ii < n_elts_dest; ii++) {
       bft_printf("r %d -> (%d - %d) :", ii, dest_index[ii], dest_index[ii+1]);
@@ -224,9 +281,12 @@ main (int argc, char *argv[])
     cs_lnum_t s_id = 0;
     for (cs_lnum_t ii = 0; ii < n_elts_dest; ii++) {
       cs_lnum_t n_sub = dest_index[ii+1] - s_id;
-      for (cs_lnum_t jj = 0; jj < n_sub; jj++)
+      cs_gnum_t s = 0;
+      for (cs_lnum_t jj = 0; jj < n_sub; jj++) {
+        s += dest_val[s_id + jj];
         reverse_val[dest_index[ii] + jj] = dest_val[s_id + jj];
-      reverse_val[dest_index[ii] + n_sub] = ii+1;
+      }
+      reverse_val[dest_index[ii] + n_sub] = s + 100;
       s_id = dest_index[ii+1];
       dest_index[ii+1] += ii+1;
       bft_printf("%d -> (%d - %d) :", ii, dest_index[ii], dest_index[ii+1]);
@@ -254,7 +314,10 @@ main (int argc, char *argv[])
 
     cs_all_to_all_destroy(&d);
 
+    BFT_FREE(part_gnum);
     BFT_FREE(reverse_val);
+
+    bft_printf("\n");
 
     for (cs_lnum_t ii = 0; ii < n_elts; ii++) {
       bft_printf("r %d -> (%d - %d) :", ii, src_index[ii], src_index[ii+1]);

commit 1b1d3b79ca2754a11ba35d99745ae959e5e404e6

cs_all_to_all: fix issue introduced by commit d1d143f0 (today).

src/base/cs_all_to_all.c
@@ -1190,6 +1190,9 @@ _cr_recv_id_by_src_rank(cs_all_to_all_t      *d,
                              NULL, /* dest_index */
                              NULL);
 
+  if (d->n_elts_dest < 0)
+    d->n_elts_dest = cs_crystal_router_n_elts(cr);
+
   _recv_id_by_src_rank_order(d, src_rank);
 
   BFT_FREE(src_rank);