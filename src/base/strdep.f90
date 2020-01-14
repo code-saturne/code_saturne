code-saturne/code_saturne

commit 3c652a492b5e2dfea4d667f801cfb9d120508309

code_aster coupling: remove the "milieu" module.
Cherry-picked from commits c025afd0 and afba4364 on master.

src/base/strdep.f90
@@ -116,6 +116,28 @@ type(var_cal_opt) :: vcopt
 !===============================================================================
 
 !===============================================================================
+! Interfaces
+!===============================================================================
+
+interface
+
+  function cs_ast_coupling_get_ext_cvg() result (icvext) &
+    bind(C, name='cs_ast_coupling_get_ext_cvg')
+    use, intrinsic :: iso_c_binding
+    implicit none
+    integer(c_int) :: icvext
+  end function cs_ast_coupling_get_ext_cvg
+
+  subroutine cs_ast_coupling_send_cvg(icv) &
+    bind(C, name='cs_ast_coupling_send_cvg')
+    use, intrinsic :: iso_c_binding
+    implicit none
+    integer(c_int), value :: icv
+  end subroutine cs_ast_coupling_send_cvg
+
+end interface
+
+!===============================================================================
 ! 1. Initialization
 !===============================================================================
 
@@ -180,7 +202,6 @@ enddo
 !     Envoi de l'effort applique aux structures externes
 if (nbaste.gt.0) then
   call astfor(ntcast, nbfast, forast)
-  !==========
 endif
 
 ! Free memory
@@ -198,7 +219,6 @@ if (nbstru.gt.0) then
   if (iihmpr.eq.1) then
 
     call uistr2 &
-    !==========
  ( xmstru, xcstru, xkstru,     &
    forstp,                     &
    dtref , ttcabs, ntcabs   )
@@ -206,7 +226,6 @@ if (nbstru.gt.0) then
   endif
 
   call usstr2                                                     &
-  !==========
  ( nbstru ,                                                       &
    idfstr ,                                                       &
    dt     ,                                                       &
@@ -257,50 +276,49 @@ if (nbstru.gt.0) then
   if (delta.lt.epalim) icvint = 1
 endif
 
-if (nbaste.gt.0) call astcv1(ntcast, icvext)
+if (nbaste.gt.0) icvext = cs_ast_coupling_get_ext_cvg()
 
 if (nbstru.gt.0.and.nbaste.gt.0) then
-   icved = icvext*icvint
+  icved = icvext*icvint
 elseif (nbstru.gt.0.and.nbaste.eq.0) then
-   icved = icvint
+  icved = icvint
 elseif (nbaste.gt.0.and.nbstru.eq.0) then
-   icved = icvext
+  icved = icvext
 endif
 
 if (vcopt%iwarni.ge.2) write(nfecra,1000) italim, delta
 
-!     si convergence
+! if converged
 if (icved.eq.1) then
   if (itrfin.eq.1) then
-!       si ITRFIN=1 on sort
+    ! si itrfin=1 on sort
     if (vcopt%iwarni.ge.1) write(nfecra,1001) italim, delta
     itrfin = -1
   else
-!       sinon on refait une derniere iteration pour SYRTHES/T1D/rayonnement
-!        et on remet ICVED a 0 pour que Code_Aster refasse une iteration aussi
+    ! otherwise one last iteration for SYRTHES/T1D/radiation
+    ! and reset icved to 0 so code_aster also runs an iteration.
     itrfin = 1
     icved = 0
   endif
 elseif (itrfin.eq.0 .and. italim.eq.nalimx-1) then
-!       ce sera la derniere iteration
+  ! this will be the lst iteration
   itrfin = 1
 elseif (italim.eq.nalimx) then
-!       on a forcement ITRFIN=1 et on sort
+  ! we have itrfin=1 and are finished
   if (nalimx.gt.1) write(nfecra,1100) italim, delta
   itrfin = -1
-!       On met ICVED a 1 pour que Code_Aster s'arrete lui aussi
+  ! Set icved to 1 so code_aster also stops
   icved = 1
 endif
 
-!     On renvoie l'indicateur de convergence final a Code_Aster
-call astcv2(ntcast, icved)
-!==========
+! Return the final convergence indicator to code_aster
+call cs_ast_coupling_send_cvg(icved)
 
 !===============================================================================
 ! 6. Re set previous values if required
 !===============================================================================
 
-! If NTERUP .GT. 1, values at previous time step have been modified after NAVSTO
+! If nterup .gt. 1, values at previous time step have been modified after navstv
 ! We must then go back to a previous value
 if (itrfin.ne.-1) then
   do ii = 1, nvar
@@ -376,16 +394,16 @@ endif
 #else
 
  1000 format (                                                          &
- '            IMPLICIT ALE: ITER=',I5,' DERIVE=',E12.5     )
+ '            IMPLICIT ALE: ITER=',I5,' DRIFT=',E12.5     )
  1001 format (                                                          &
- 'CONVERGENCE IMPLICIT ALE: ITER=',I5,' DERIVE=',E12.5     )
+ 'CONVERGENCE IMPLICIT ALE: ITER=',I5,' DRIFT=',E12.5     )
  1100 format (                                                          &
-'@                                                            ',/,&
-'@ @@ WARNING: IMPLICIT ALE                                   ',/,&
-'@    ========                                                ',/,&
-'@  Maximum number of iterations ',I10   ,' reached           ',/,&
-'@  Normed derive :',E12.5                                     ,/,&
-'@                                                            '  )
+'@'                                                            ,/,&
+'@ @@ WARNING: IMPLICIT ALE'                                   ,/,&
+'@    ========'                                                ,/,&
+'@  Maximum number of iterations ',I10   ,' reached'           ,/,&
+'@  Normed drift:',E12.5                                     ,/,&
+'@'                                                              )
 
 #endif
 

bin/cs_case.py
@@ -166,7 +166,6 @@ class case:
                  domains = None,
                  syr_domains = None,
                  ast_domain = None,
-                 fsi_coupler = None,
                  py_domains = None):
 
         # Package specific information
@@ -212,8 +211,6 @@ class case:
         else:
             self.ast_domains = [ast_domain]
 
-        self.fsi_coupler = fsi_coupler
-
         if py_domains == None:
             self.py_domains = []
         elif type(py_domains) == tuple:
@@ -228,7 +225,7 @@ class case:
         # to better handle cases were only some domains are coupled.
         # For now, we assume that the first CFD domain is the coupled one.
 
-        if fsi_coupler:
+        if len(self.ast_domains) > 0:
             self.domains[0].fsi_aster = True
 
         # Check names in case of multiple domains (coupled by MPI)
@@ -389,8 +386,8 @@ class case:
         for d in (self.domains + self.syr_domains + self.py_domains):
             np_list.append(d.get_n_procs())
 
-        # Code_Aster is handled in a specific manner: processes are counted
-        # against the total, but the coupler is not, and processes will not by
+        # code_aster is handled in a specific manner: processes are counted
+        # against the total, and processes will not by
         # run by the same mpiexec instance (TODO: check/improve how this
         # interacts with resource managers, so that some nodes are not
         # oversubscribed while others are unused).
@@ -768,8 +765,6 @@ class case:
         ast_mesh_name = os.path.join(ast_dir_name, ast_d.mesh)
         ast_comm = os.path.join(ast_dir_name, ast_d.comm)
 
-        c = self.fsi_coupler
-
         pkgdatadir = self.package.get_dir('pkgdatadir')
         s_path = os.path.join(pkgdatadir, 'salome', 'fsi_yacs_scheme.xml')
         s = open(s_path, 'r')
@@ -781,29 +776,11 @@ class case:
         e_re = re.compile('@AST_WORKINGDIR@')
         template = re.sub(e_re, ast_d.exec_dir, template)
 
-        e_re = re.compile('@COCAS_WORKINGDIR@')
-        template = re.sub(e_re, self.exec_dir, template)
-
         e_re = re.compile('@CFD_WORKINGDIR@')
         template = re.sub(e_re, cfd_d.exec_dir, template)
 
         e_re = re.compile('@AST_MAIL@')
         template = re.sub(e_re, ast_mesh_name, template)
-
-        e_re = re.compile('@NBPDTM@')
-        template = re.sub(e_re, str(c['max_time_steps']), template)
-
-        e_re = re.compile('@NBSSIT@')
-        template = re.sub(e_re, str(c['n_sub_iterations']), template)
-
-        e_re = re.compile('@DTREF@')
-        template = re.sub(e_re, str(c['time_step']), template)
-
-        e_re = re.compile('@TTINIT@')
-        template = re.sub(e_re, str(c['start_time']), template)
-
-        e_re = re.compile('@EPSILO@')
-        template = re.sub(e_re, str(c['epsilon']), template)
 
         e_re = re.compile('@COMM_FNAME@')
         template = re.sub(e_re, ast_comm, template)

bin/cs_case_coupling.py
@@ -67,7 +67,6 @@ def coupling(package,
     nep_domains = []
     cat_domains = []
     ast_domain = []
-    fsi_coupler = None
     py_domains = []
 
     if domains == None:
@@ -75,8 +74,7 @@ def coupling(package,
 
     for d in domains:
 
-        if ((d.get('script') == None or d.get('domain') == None) \
-            and d.get('coupler') == None):
+        if (d.get('script') == None or d.get('domain') == None):
             msg = 'Check your coupling definition.\n'
             msg += 'script or domain key is missing.'
             raise RunCaseError(msg)
@@ -158,28 +156,6 @@ def coupling(package,
 
             ast_domain.append(dom)
 
-        elif (d.get('coupler') == 'FSI_coupler'):
-
-            if fsi_coupler:
-                err_str = 'Only 1 FSI coupler is currently handled\n'
-                raise RunCaseError(err_str)
-
-            try:
-                fsi_coupler = {'max_time_steps' : d.get('max_time_steps'),
-                               'n_sub_iterations' : d.get('n_sub_iterations'),
-                               'time_step' : d.get('time_step'),
-                               'start_time' : d.get('start_time'),
-                               'epsilon' : d.get('epsilon')}
-
-            except Exception:
-                err_str = 'Cannot create FSI coupler\n'
-                err_str += '  max_time_steps = ' + d.get('max_time_steps') + '\n'
-                err_str += '  n_sub_iterations = ' + d.get('n_sub_iterations' + '\n')
-                err_str += '  time_step = ' + d.get('time_step') + '\n'
-                err_str += '  start_time = ' + d.get('start_time') + '\n'
-                err_str += '  epsilon = ' + d.get('epsilon') + '\n'
-                raise RunCaseError(err_str)
-
         elif (d.get('solver') == 'CATHARE'):
             # Current version using Cathare2: the cathare case is converted to a
             # .so library which is opened and launched by a NEPTUNE_CFD executable
@@ -246,9 +222,7 @@ domains = [
         d_first = False
         k_first = True
         for k in ('solver', 'domain', 'script', 'n_procs_weight', 'n_procs_min',
-                  'coupler', 'max_time_steps', 'n_sub_iterations', 'time_step',
-                  'start_time', 'epsilon', 'cathare_case_file',
-                  'neptune_cfd_domain'):
+                  'cathare_case_file', 'neptune_cfd_domain'):
             v = d.get(k)
             if v:
                 if type(v) == str:
@@ -272,7 +246,6 @@ domains = [
              domains = sat_domains + nep_domains + cat_domains,
              syr_domains = syr_domains,
              ast_domain = ast_domain,
-             fsi_coupler = fsi_coupler,
              py_domains = py_domains)
 
     if verbose:
@@ -283,9 +256,8 @@ domains = [
             msg += '   o SYRTHES      [' + str(len(syr_domains)) + ' domain(s)];\n'
         if use_neptune == True:
             msg += '   o NEPTUNE_CFD  [' + str(len(nep_domains)) + ' domain(s)];\n'
-        if ast_domain or fsi_coupler:
+        if ast_domain:
             msg += '   o Code_Aster   [1 domain(s)];\n'
-            msg += '                  [1 coupler(s)];\n'
         if use_cathare == True:
             msg += '   o CATHARE2     [' + str(len(cat_domains)) + ' domain(s)];\n'
         if use_py_code == True:

bin/cs_create.py
@@ -522,18 +522,6 @@ domains = [
             template = re.sub(e_dom, self.ast_case_name, template)
             dict_str += template
 
-            template = \
-"""
-    ,
-    {'coupler': 'FSI_coupler',
-     'max_time_steps': 10,
-     'n_sub_iterations': 1,
-     'time_step': 0.0001,
-     'start_time': 0.0,
-     'epsilon': 0.00001}
-"""
-            dict_str += template
-
         if self.cat_case_name is not None:
 
             c = os.path.normpath(self.cat_case_name)

configure.ac
@@ -1188,11 +1188,9 @@ AC_CONFIG_FILES([Makefile po/Makefile.in
                  gui/studymanager_gui/Makefile gui/trackcvg/Makefile
                  salome/fsi_coupling/Makefile
                  salome/fsi_coupling/idl/Makefile
-                 salome/fsi_coupling/libmilieu/Makefile
                  salome/fsi_coupling/resources/Makefile
                  salome/fsi_coupling/src/Makefile
                  salome/fsi_coupling/src/FSI_ASTER/Makefile
-                 salome/fsi_coupling/src/FSI_MILIEU/Makefile
                  salome/fsi_coupling/src/FSI_SATURNE/Makefile
                  salome/cfd_study/Makefile
                  salome/cfd_study/doc/Makefile

extras/salome/fsi_yacsgen.py
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------------
 #   This file is part of the Code_Saturne CFD tool.
 #
-#   Copyright (C) 2011-2016 EDF S.A.
+#   Copyright (C) 2011-2019 EDF S.A.
 #
 #   The Code_Saturne CFD tool is free software; you can redistribute it
 #   and/or modify it under the terms of the GNU General Public License
@@ -29,9 +29,6 @@ from module_generator import ASTERComponent, CPPComponent
 
 asterdir = os.path.expanduser("/home/aster/NEW11")
 
-milieuinc = "-I$(top_srcdir)/salome/libmilieu"
-milieulib = "$(top_builddir)/salome/libmilieu/libmilieu.la"
-
 # SALOME environment be must loaded beforehand
 
 KERNEL_ROOT_DIR = os.getenv("KERNEL_ROOT_DIR")
@@ -47,42 +44,31 @@ context = {'update':1,
 # Definition of all the in/out streams
 # ------------------------------------
 
-astDisplacement = ("DEPAST","CALCIUM_double","I")
-astVelocity = ("VITAST","CALCIUM_double","I")
-astForces = ("FORAST","CALCIUM_double","I")
+nbIter = ("NBPDTM", "CALCIUM_integer","I")
+nbSubIter = ("NBSSIT", "CALCIUM_integer","I")
 
-satDisplacement = ("DEPSAT","CALCIUM_double","I")
-satForces = ("FORSAT","CALCIUM_double","I")
+indSync =("ISYNCP", "CALCIUM_integer","I")
+frequency = ("NTCHRO", "CALCIUM_integer","I")
 
-geometricData = ("DONGEO","CALCIUM_integer","I")
-satLengthScale = ("ALMAXI","CALCIUM_double","I")
+timeStep = ("PDTREF", "CALCIUM_double","I")
+prevTime = ("TTINIT", "CALCIUM_double","I")
+epsilon = ("EPSILO", "CALCIUM_double","I")
 
-nbFor = ("NB_FOR","CALCIUM_integer","I")
-nbDyn = ("NB_DYN","CALCIUM_integer","I")
+newTimeStep = ("DTCALC", "CALCIUM_double","I")
+nbDyn = ("NB_DYN", "CALCIUM_integer","I")
+nbFor = ("NB_FOR", "CALCIUM_integer","I")
 
-nodesXYZ = ("COONOD","CALCIUM_double","I")
-facesXYZ = ("COOFAC","CALCIUM_double","I")
+nodesColor = ("COLNOD", "CALCIUM_integer","I")
+facesColor = ("COLFAC", "CALCIUM_integer","I")
+nodesXYZ = ("COONOD", "CALCIUM_double","I")
+facesXYZ = ("COOFAC", "CALCIUM_double","I")
 
-nodesColor = ("COLNOD","CALCIUM_integer","I")
-facesColor = ("COLFAC","CALCIUM_integer","I")
-
-nbIter = ("NBPDTM","CALCIUM_integer","I")
-nbSubIter = ("NBSSIT","CALCIUM_integer","I")
-
-indSync =("ISYNCP","CALCIUM_integer","I")
-frequency = ("NTCHRO","CALCIUM_integer","I")
-
-epsilon = ("EPSILO","CALCIUM_double","I")
-astConvergence = ("ICVAST","CALCIUM_integer","I")
-satConvergence = ("ICV",   "CALCIUM_integer","I")
-extConvergence = ("ICVEXT","CALCIUM_integer","I")
-
-prevTime = ("TTINIT","CALCIUM_double","I")
-timeStep = ("PDTREF","CALCIUM_double","I")
+astForces = ("FORAST", "CALCIUM_double","I")
+astConvergence = ("ICVAST", "CALCIUM_integer","I")
 
 astTimeStep = ("DTAST", "CALCIUM_double","I")
-satTimeStep = ("DTSAT", "CALCIUM_double","I")
-newTimeStep = ("DTCALC","CALCIUM_double","I")
+astDisplacement = ("DEPAST", "CALCIUM_double","I")
+astVelocity = ("VITAST", "CALCIUM_double","I")
 
 # Code_Aster streams
 
@@ -99,44 +85,21 @@ aster_instream = [nbFor, nbDyn,
 
 # Code_Saturne streams
 
-saturne_instream = [satDisplacement,
-                    epsilon,
-                    newTimeStep,
-                    prevTime,
-                    timeStep,
-                    nbIter, nbSubIter,
-                    extConvergence]
+saturne_instream = [astDisplacement, astVelocity, astTimeStep]
 
-saturne_outstream = [satTimeStep,
-                     satForces,
-                     satLengthScale,
+saturne_outstream = [nbFor, nbDyn,
                      nodesXYZ, facesXYZ,
                      nodesColor, facesColor,
-                     satConvergence,
-                     geometricData]
-
-# Milieu streams
-
-milieu_outstream = [newTimeStep,
-                    epsilon,
-                    prevTime, timeStep,
-                    satDisplacement, astForces,
-                    nbIter, nbSubIter,
-                    indSync, frequency,
-                    extConvergence, astConvergence,
-                    nbDyn, nbFor]
-
-milieu_instream = [satLengthScale,
-                   satTimeStep, astTimeStep,
-                   astDisplacement, astVelocity,
-                   satForces,
-                   geometricData,
-                   satConvergence]
+                     astForces,
+                     nbIter, nbSubIter,
+                     epsilon, astConvergence,
+                     indSync, frequency,
+                     prevTime, timeStep, newTimeStep]
 
 # Creation of the different components
 # ------------------------------------
 
-# Code_Aster component
+# code_aster component
 
 aster_service = Service("op0117",
                         instream = aster_instream,
@@ -173,38 +136,6 @@ c2 = CPPComponent("FSI_SATURNE",
                   services = [saturne_service],
                   kind = "exe",
                   exe_path = "./run_solver")
-
-# Milieu component
-
-milieu_defs = """#include <runmilieu.h>
-#include <donnees.h>"""
-
-milieu_body = """inter_cs_ast_set_nbpdtm(NBPDTM);
-inter_cs_ast_set_nbssit(NBSSIT);
-inter_cs_ast_set_isyncp(ISYNCP);
-inter_cs_ast_set_ntchr(NTCHR);
-inter_cs_ast_set_dtref(DTREF);
-inter_cs_ast_set_ttinit(TTINIT);
-inter_cs_ast_set_epsilo(EPSILO);
-runmilieu(component);"""
-
-milieu_service = Service("inter_run",
-                         inport=[("NBPDTM", "long"),
-                                 ("NBSSIT", "long"),
-                                 ("ISYNCP", "long"),
-                                 ("NTCHR",  "long"),
-                                 ("DTREF",  "double"),
-                                 ("TTINIT", "double"),
-                                 ("EPSILO", "double")],
-                         outstream = milieu_outstream,
-                         instream = milieu_instream,
-                         defs = milieu_defs,
-                         body = milieu_body)
-
-c3 = CPPComponent("FSI_MILIEU",
-                  services = [milieu_service],
-                  includes = milieuinc,
-                  libs = milieulib)
 
 # Creation of the FSI module
 # --------------------------

salome/fsi_coupling/Makefile.am
@@ -22,4 +22,4 @@
 
 #-------------------------------------------------------------------------------
 
-SUBDIRS = idl libmilieu resources src
+SUBDIRS = idl resources src

commit 14b88c9ef1e0ff692c2476863958db9645a6c59d

Fix crash in ALE with internal structures introduced by commit 3c652a49 (Dec. 9 2019).
Cherry-picked from commit 44a62312 on master.

src/base/strdep.f90
@@ -312,7 +312,9 @@ elseif (italim.eq.nalimx) then
 endif
 
 ! Return the final convergence indicator to code_aster
-call cs_ast_coupling_send_cvg(icved)
+if (nbaste.gt.0) then
+  call cs_ast_coupling_send_cvg(icved)
+endif
 
 !===============================================================================
 ! 6. Re set previous values if required