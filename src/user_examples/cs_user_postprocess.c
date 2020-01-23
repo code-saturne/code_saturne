code-saturne/code_saturne

commit de18fc1cdae585524feefdb8358837a06994b5ef

Very slightly precise a user example for postprocessing.
Close issue #132 (GitLab).

src/user_examples/cs_user_postprocess.c
@@ -608,36 +608,42 @@ cs_user_postprocess_values(const char            *mesh_name,
                            const cs_lnum_t        vertex_list[],
                            const cs_time_step_t  *ts)
 {
-  /* Output of k=1/2(R11+R22+R33) for the Rij-epsilon model
+  /* Output of k = 1/2 (R11+R22+R33) for the Rij-epsilon model
      ------------------------------------------------------ */
 
   /*< [postprocess_values_ex_1] */
-  if (cat_id == CS_POST_MESH_VOLUME && cs_glob_turb_model->itytur == 3) {
+  if (cat_id == CS_POST_MESH_VOLUME) { /* filter: only for volume
+                                          postprocessing mesh */
 
-    cs_real_t *s_cell;
-    BFT_MALLOC(s_cell, n_cells, cs_real_t);
+    if (cs_glob_turb_model->itytur == 3) {
 
-    if (cs_glob_turb_rans_model->irijco) {
-      const cs_real_6_t *cvar_r = (const cs_real_6_t *)(CS_F_(rij)->val);
-      for (cs_lnum_t i = 0; i < n_cells; i++) {
-        cs_lnum_t cell_id = cell_list[i];
-        s_cell[i] = 0.5* (  cvar_r[cell_id][0]
-                          + cvar_r[cell_id][1]
-                          + cvar_r[cell_id][2]);
+      cs_real_t *s_cell;
+      BFT_MALLOC(s_cell, n_cells, cs_real_t);
+
+      if (cs_glob_turb_rans_model->irijco) {
+        const cs_real_6_t *cvar_r = (const cs_real_6_t *)(CS_F_(rij)->val);
+        for (cs_lnum_t i = 0; i < n_cells; i++) {
+          cs_lnum_t cell_id = cell_list[i];
+          s_cell[i] = 0.5* (  cvar_r[cell_id][0]
+                            + cvar_r[cell_id][1]
+                            + cvar_r[cell_id][2]);
+        }
       }
-    }
 
-    else {
-      const cs_real_t *cvar_r11 = CS_F_(r11)->val;
-      const cs_real_t *cvar_r22 = CS_F_(r22)->val;
-      const cs_real_t *cvar_r33 = CS_F_(r33)->val;
+      else {
+        const cs_real_t *cvar_r11 = CS_F_(r11)->val;
+        const cs_real_t *cvar_r22 = CS_F_(r22)->val;
+        const cs_real_t *cvar_r33 = CS_F_(r33)->val;
 
-      for (cs_lnum_t i = 0; i < n_cells; i++) {
-        cs_lnum_t cell_id = cell_list[i];
-        s_cell[i] = 0.5* (  cvar_r11[cell_id]
-                          + cvar_r22[cell_id]
-                          + cvar_r33[cell_id]);
+        for (cs_lnum_t i = 0; i < n_cells; i++) {
+          cs_lnum_t cell_id = cell_list[i];
+          s_cell[i] = 0.5* (  cvar_r11[cell_id]
+                            + cvar_r22[cell_id]
+                            + cvar_r33[cell_id]);
+        }
+
       }
+
     }
 
     cs_post_write_var(mesh_id,

commit 301fcfdcf964aa578011ab2831afbc8cd3116553

Fix compilation, broken by commit de18fc1cd.

src/user_examples/cs_user_postprocess.c
@@ -644,21 +644,22 @@ cs_user_postprocess_values(const char            *mesh_name,
 
       }
 
+      cs_post_write_var(mesh_id,
+                        CS_POST_WRITER_ALL_ASSOCIATED,  /* writer id filter */
+                        "Turb energy",                  /* var_name */
+                        1,                              /* var_dim */
+                        true,                           /* interlace, */
+                        false,                          /* use_parent */
+                        CS_POST_TYPE_cs_real_t,         /* var_type */
+                        s_cell,                         /* cel_vals */
+                        NULL,                           /* i_face_vals */
+                        NULL,                           /* b_face_vals */
+                        ts);
+
+      BFT_FREE(s_cell);
+
     }
 
-    cs_post_write_var(mesh_id,
-                      CS_POST_WRITER_ALL_ASSOCIATED,  /* writer id filter */
-                      "Turb energy",                  /* var_name */
-                      1,                              /* var_dim */
-                      true,                           /* interlace, */
-                      false,                          /* use_parent */
-                      CS_POST_TYPE_cs_real_t,         /* var_type */
-                      s_cell,                         /* cel_vals */
-                      NULL,                           /* i_face_vals */
-                      NULL,                           /* b_face_vals */
-                      ts);
-
-    BFT_FREE(s_cell);
   }
   /*< [postprocess_values_ex_1] */
 