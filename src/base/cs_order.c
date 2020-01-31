code-saturne/code_saturne

commit f50d931b2e053b91388dea574eeefd06d7216bda

Replace cs_block_to_part_create_adj with cs_all_to_all_create_from_gnum.

src/base/cs_order.c
@@ -1403,5 +1403,84 @@ cs_order_reorder_data(cs_lnum_t         n_elts,
 }
 
 /*----------------------------------------------------------------------------*/
+/*!
+ * \brief Build a sorted array containing a single occurence of each global
+ *        number in a given array.
+ *
+ * Global numbers under a given "base" value are extruded.
+ *
+ * The caller is responsible for freeing the returned array.
+ *
+ * \param[in]   n_ent     size of input array
+ * \param[in]   base      base id; numbers lower than this are dropped
+ * \param[in]   number    array containing of all referenced entity numbers
+ * \param[out]  n_single  array number of single occurences >= base
+ * \param[out]  single    sorted array of unique numbers >= base
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_order_single_gnum(size_t            n_ent,
+                     const cs_gnum_t   base,
+                     const cs_gnum_t   number[],
+                     size_t           *n_single,
+                     cs_gnum_t        *single[])
+{
+  if (n_ent == 0) {
+    *n_single = 0;
+    *single = NULL;
+    return;
+  }
+
+  /* Sort global numbers */
+
+  cs_lnum_t *order = cs_order_gnum(NULL, number, n_ent);
+
+  /* Count number of distinct global entities */
+
+  size_t _n_single = 0;
+
+  size_t s_id = 0;
+  while (s_id < n_ent && _n_single == 0) {
+    if (number[order[s_id]] >= base)
+      _n_single = 1;
+    s_id++;
+  }
+
+  for (size_t i = s_id+1; i < n_ent; i++) {
+    if (number[order[i]] > number[order[i-1]])
+      _n_single += 1;
+  }
+
+  cs_gnum_t *_single = NULL;
+
+  if (_n_single > 0) {
+
+    BFT_MALLOC(_single, _n_single, cs_gnum_t);
+
+    size_t j = 0;
+    cs_gnum_t num_c = number[order[s_id]];
+
+    _single[j++] = number[order[s_id]];
+
+    cs_gnum_t num_p = num_c;
+    for (size_t i = s_id+1; i < n_ent; i++) {
+      num_c = number[order[i]];
+      if (num_c > num_p) {
+        _single[j++] = num_c;
+        num_p = num_c;
+      }
+    }
+
+    assert(j == _n_single);
+  }
+
+  BFT_FREE(order);
+
+  *n_single = _n_single;
+  *single = _single;
+}
+
+/*----------------------------------------------------------------------------*/
 
 END_C_DECLS

src/base/cs_block_to_part.c
@@ -43,6 +43,7 @@
 #include "bft_mem.h"
 #include "bft_error.h"
 
+#include "cs_all_to_all.h"
 #include "cs_block_dist.h"
 #include "cs_order.h"
 
@@ -557,133 +558,6 @@ cs_block_to_part_create_by_adj_s(MPI_Comm              comm,
 }
 
 /*----------------------------------------------------------------------------
- * Initialize block to partition distributor for entities adjacent to
- * already distributed entities.
- *
- * arguments:
- *   comm           <-- communicator
- *   block          <-- block size and range info
- *   adjacent_block <-- block info for adjacent entities
- *   adjacency      <-- entity adjacency (1 to n numbering)
- *
- * returns:
- *   initialized block to partition distributor
- *----------------------------------------------------------------------------*/
-
-cs_block_to_part_t *
-cs_block_to_part_create_adj(MPI_Comm              comm,
-                            cs_block_dist_info_t  adjacent_block,
-                            size_t                adjacency_size,
-                            const cs_gnum_t       adjacency[])
-{
-  int i;
-  size_t j;
-
-  size_t recv_size = 0;
-  cs_lnum_t *adj_list = NULL, *_adj_list = NULL;
-  cs_gnum_t *send_num = NULL, *recv_num = NULL;
-
-  cs_block_to_part_t *d = _block_to_part_create(comm);
-
-  const int n_ranks = d->n_ranks;
-
-  /* Sort adjacency list so as to remove duplicates */
-
-  _ordered_list(adjacency_size,
-                adjacency,
-                &d->n_part_ents,
-                &_adj_list);
-
-  /* Use adjacent global number to determine to which ranks they will
-     be sent. Send and receive counts are exchanged here, as ranks
-     requesting (i.e receiving entities) are determined first */
-
-  for (i = 0; i < d->n_ranks; i++)
-    d->recv_count[i] = 0;
-
- /* Ignore possible 0 values in global numbering */
-
-  if (d->n_part_ents > 0) {
-    if (adjacency[_adj_list[0]] == 0) {
-      d->n_part_ents -= 1;
-      adj_list = _adj_list + 1;
-    }
-    else
-      adj_list = _adj_list;
-  }
-
-  for (j = 0; j < d->n_part_ents; j++) {
-    cs_gnum_t adj_g_id = adjacency[adj_list[j]] - 1;
-    int adj_ent_rank =   (adj_g_id / adjacent_block.block_size)
-                       * adjacent_block.rank_step;
-    d->recv_count[adj_ent_rank] += 1;
-  }
-
-  MPI_Alltoall(d->recv_count, 1, MPI_INT, d->send_count, 1, MPI_INT, comm);
-
-  d->send_size = _compute_displ(n_ranks, d->send_count, d->send_displ);
-  recv_size = _compute_displ(n_ranks, d->recv_count, d->recv_displ);
-
-  if (d->n_part_ents != recv_size)
-    bft_error
-      (__FILE__, __LINE__, 0,
-       _("inconsistent sizes computed for a block to partition distributor\n"
-         "(%llu expected, %llu determined)."),
-       (unsigned long long)(d->n_part_ents), (unsigned long long)recv_size);
-
-  /* Allocate distributor arrays */
-
-  BFT_MALLOC(d->send_list, d->send_size, cs_lnum_t);
-  BFT_MALLOC(d->recv_order, d->n_part_ents, cs_lnum_t);
-
-  BFT_MALLOC(d->_recv_global_num, d->n_part_ents, cs_gnum_t);
-  d->recv_global_num = d->_recv_global_num;
-
-  /* We already have all the necessary info to build the global numbering */
-
-  for (j = 0; j < d->n_part_ents; j++)
-    d->_recv_global_num[j] = adjacency[adj_list[j]];
-
-  /* Prepare destination rank request and receive_order at the same time
-     (temporarily modifying d->recv_displ) */
-
-  BFT_MALLOC(send_num, d->send_size, cs_gnum_t);
-  BFT_MALLOC(recv_num, d->n_part_ents, cs_gnum_t);
-
-  for (j = 0; j < d->n_part_ents; j++) {
-    cs_gnum_t adj_g_num = adjacency[adj_list[j]];
-    int adj_ent_rank =   ((adj_g_num-1) / adjacent_block.block_size)
-                       * adjacent_block.rank_step;
-    recv_num[d->recv_displ[adj_ent_rank]] = adj_g_num;
-    d->recv_order[j] = d->recv_displ[adj_ent_rank];
-    d->recv_displ[adj_ent_rank] += 1;
-  }
-
-  for (i = 0; i < n_ranks; i++)
-    d->recv_displ[i] -= d->recv_count[i];
-
-  BFT_FREE(_adj_list);
-  adj_list = NULL;
-
-  MPI_Alltoallv(recv_num, d->recv_count, d->recv_displ, CS_MPI_GNUM,
-                send_num, d->send_count, d->send_displ, CS_MPI_GNUM,
-                d->comm);
-
-  BFT_FREE(recv_num);
-
-  /* Now prepare send list */
-
-  for (j = 0; j < d->send_size; j++)
-    d->send_list[j] = send_num[j] - adjacent_block.gnum_range[0];
-
-  BFT_FREE(send_num);
-
-  /* Return initialized structure */
-
-  return d;
-}
-
-/*----------------------------------------------------------------------------
  * Destroy a block to partition distributor structure.
  *
  * arguments:
@@ -1059,8 +933,9 @@ cs_block_to_part_copy_indexed(cs_block_to_part_t  *d,
 
 #endif /* defined(HAVE_MPI) */
 
-/*----------------------------------------------------------------------------
- * Determine local references from references to global numbers.
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Determine local references from references to global numbers.
  *
  * This is based on finding the local id of a given global number
  * using a binary search.
@@ -1074,18 +949,16 @@ cs_block_to_part_copy_indexed(cs_block_to_part_t  *d,
  * global number (i.e not necessarily the smallest one) may be
  * assigned to the corresponding local_number[] entry.
  *
- * arguments:
- *   n_ents                <-- number of entities
- *   base                  <-- base numbering (typically 0 or 1)
- *   global_list_size      <-- size of global entity list
- *   global_list_is_sorted <-- true if global entity list is guaranteed
- *                             to be sorted
- *   global_list           <-- global entity list
- *   global_number         <-- entity global numbers
- *                             (size: n_ents)
- *   local_number          --> entity local numbers
- *                             (size: n_ents)
- *----------------------------------------------------------------------------*/
+ * \param[in]   n_ents                 number of entities
+ * \param[in]   base                   base numbering (typically 0 or 1)
+ * \param[in]   global_list_size       size of global entity list
+ * \param[in]   global_list_is_sorted  true if global entity list is
+ *                                     guaranteed to be sorted
+ * \param[in]   global_list            global entity list
+ * \param[in]   global_number          entity global numbers (size: n_ents)
+ * \param[out]  local_number           entity local numbers (size: n_ents)
+ */
+/*----------------------------------------------------------------------------*/
 
 void
 cs_block_to_part_global_to_local(cs_lnum_t        n_ents,

src/base/cs_block_to_part.h
@@ -105,26 +105,6 @@ cs_block_to_part_create_by_adj_s(MPI_Comm              comm,
                                  int                   default_rank[]);
 
 /*----------------------------------------------------------------------------
- * Initialize block to partition distributor for entities adjacent to
- * already distributed entities.
- *
- * arguments:
- *   comm      <-- communicator
- *   bi        <-- block size and range info
- *   adj_bi    <-- block info for adjacent entities
- *   adjacency <-- entity adjacency (1 to n numbering)
- *
- * returns:
- *   initialized block to partition distributor
- *----------------------------------------------------------------------------*/
-
-cs_block_to_part_t *
-cs_block_to_part_create_adj(MPI_Comm              comm,
-                            cs_block_dist_info_t  adj_bi,
-                            size_t                adjacency_size,
-                            const cs_gnum_t       adjacency[]);
-
-/*----------------------------------------------------------------------------
  * Destroy a block to partition distributor structure.
  *
  * arguments:
@@ -223,8 +203,9 @@ cs_block_to_part_copy_indexed(cs_block_to_part_t  *d,
 
 #endif /* defined(HAVE_MPI) */
 
-/*----------------------------------------------------------------------------
- * Determine local references from references to global numbers.
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Determine local references from references to global numbers.
  *
  * This is based on finding the local id of a given global number
  * using a binary search.
@@ -238,18 +219,16 @@ cs_block_to_part_copy_indexed(cs_block_to_part_t  *d,
  * global number (i.e not necessarily the smallest one) may be
  * assigned to the corresponding local_number[] entry.
  *
- * arguments:
- *   n_ents                <-- number of entities
- *   base                  <-- base numbering (typically 0 or 1)
- *   global_list_size      <-- size of global entity list
- *   global_list_is_sorted <-- true if global entity list is guaranteed
- *                             to be sorted
- *   global_list           <-- global entity list
- *   global_number         <-- entity global numbers
- *                             (size: n_ents)
- *   local_number          --> entity local numbers
- *                             (size: n_ents)
- *----------------------------------------------------------------------------*/
+ * \param[in]   n_ents                 number of entities
+ * \param[in]   base                   base numbering (typically 0 or 1)
+ * \param[in]   global_list_size       size of global entity list
+ * \param[in]   global_list_is_sorted  true if global entity list is
+ *                                     guaranteed to be sorted
+ * \param[in]   global_list            global entity list
+ * \param[in]   global_number          entity global numbers (size: n_ents)
+ * \param[out]  local_number           entity local numbers (size: n_ents)
+ */
+/*----------------------------------------------------------------------------*/
 
 void
 cs_block_to_part_global_to_local(cs_lnum_t        n_ents,

src/base/cs_order.h
@@ -315,6 +315,30 @@ cs_order_reorder_data(cs_lnum_t         n_elts,
                       void             *data);
 
 /*----------------------------------------------------------------------------*/
+/*!
+ * \brief Build a sorted array containing a single occurence of each global
+ *        number in a given array.
+ *
+ * Global numbers under a given "base" value are extruded.
+ *
+ * The caller is responsible for freeing the returned array.
+ *
+ * \param[in]   n_ent     size of input array
+ * \param[in]   base      base id; numbers lower than this are dropped
+ * \param[in]   number    array containing of all referenced entity numbers
+ * \param[out]  n_single  array number of single occurences >= base
+ * \param[out]  single    sorted array of unique numbers >= base
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_order_single_gnum(size_t            n_ent,
+                     const cs_gnum_t   base,
+                     const cs_gnum_t   number[],
+                     size_t           *n_single,
+                     cs_gnum_t        *single[]);
+
+/*----------------------------------------------------------------------------*/
 
 END_C_DECLS
 

src/mesh/cs_mesh_from_builder.c
@@ -54,6 +54,7 @@
 #include "cs_block_to_part.h"
 #include "cs_mesh.h"
 #include "cs_mesh_builder.h"
+#include "cs_order.h"
 #include "cs_partition.h"
 
 /*----------------------------------------------------------------------------
@@ -855,14 +856,8 @@ _precompute_free_face_center(const cs_mesh_builder_t  *mb,
   cs_datatype_t real_type = (sizeof(cs_real_t) == 8) ? CS_DOUBLE : CS_FLOAT;
 
   cs_lnum_t _n_faces = 0;
-  cs_lnum_t _n_vertices = 0;
 
-  cs_gnum_t *_vtx_num = NULL;
   cs_lnum_t *_face_vertices = NULL;
-
-  cs_real_t *_vtx_coord = NULL;
-
-  cs_block_to_part_t *d = NULL;
 
   /* Initialization */
 
@@ -875,24 +870,31 @@ _precompute_free_face_center(const cs_mesh_builder_t  *mb,
   /* Distribute vertices */
   /*---------------------*/
 
-  d = cs_block_to_part_create_adj(comm,
-                                  mb->vertex_bi,
-                                  mb->face_vertices_idx[_n_faces],
-                                  mb->face_vertices);
+  size_t _n_vertices = 0;
+  cs_gnum_t *_vtx_num = NULL;
 
-  _n_vertices = cs_block_to_part_get_n_part_ents(d);
+  cs_order_single_gnum(mb->face_vertices_idx[_n_faces],
+                       1, /* base */
+                       mb->face_vertices,
+                       &_n_vertices,
+                       &_vtx_num);
 
-  BFT_MALLOC(_vtx_coord, _n_vertices*3, cs_real_t);
+  cs_all_to_all_t *d
+    = cs_all_to_all_create_from_block(_n_vertices,
+                                      CS_ALL_TO_ALL_USE_DEST_ID,
+                                      _vtx_num,
+                                      mb->vertex_bi,
+                                      comm);
 
-  cs_block_to_part_copy_array(d,
+  cs_real_t *_vtx_coord
+    = cs_all_to_all_copy_array(d,
                                real_type,
                                3,
+                               true, /* reverse */
                                mb->vertex_coords,
-                               _vtx_coord);
+                               NULL);
 
-  _vtx_num = cs_block_to_part_transfer_gnum(d);
-
-  cs_block_to_part_destroy(&d);
+  cs_all_to_all_destroy(&d);
 
   /* Now convert face -> vertex connectivity to local vertex numbers */
 
@@ -1238,26 +1240,33 @@ _decompose_data_g(cs_mesh_t          *mesh,
 
   /* Vertices */
 
-  d = cs_block_to_part_create_adj(comm,
-                                  mb->vertex_bi,
-                                  _face_vertices_idx[_n_faces],
-                                  _face_gvertices);
+  size_t _n_vertices = 0;
 
-  mesh->n_vertices = cs_block_to_part_get_n_part_ents(d);
+  cs_order_single_gnum(_face_vertices_idx[_n_faces],
+                       1, /* base */
+                       _face_gvertices,
+                       &_n_vertices,
+                       &(mesh->global_vtx_num));
 
-  BFT_MALLOC(mesh->vtx_coord, mesh->n_vertices*3, cs_real_t);
+  mesh->n_vertices = _n_vertices;
 
-  cs_block_to_part_copy_array(d,
-                              real_type,
-                              3,
-                              mb->vertex_coords,
-                              mesh->vtx_coord);
+  cs_all_to_all_t *dv
+    = cs_all_to_all_create_from_block(mesh->n_vertices,
+                                      CS_ALL_TO_ALL_USE_DEST_ID,
+                                      mesh->global_vtx_num,
+                                      mb->vertex_bi,
+                                      comm);
+
+  mesh->vtx_coord = cs_all_to_all_copy_array(dv,
+                                             real_type,
+                                             3,
+                                             true, /* reverse */
+                                             mb->vertex_coords,
+                                             NULL);
 
   BFT_FREE(mb->vertex_coords);
 
-  mesh->global_vtx_num = cs_block_to_part_transfer_gnum(d);
-
-  cs_block_to_part_destroy(&d);
+  cs_all_to_all_destroy(&dv);
 
   /* Now convert face -> vertex connectivity to local vertex numbers */
 

src/mesh/cs_partition.c
@@ -97,6 +97,7 @@ extern "C" {
 #include "cs_log.h"
 #include "cs_mesh.h"
 #include "cs_mesh_builder.h"
+#include "cs_order.h"
 #include "cs_part_to_block.h"
 #include "cs_timer.h"
 
@@ -597,19 +598,15 @@ _precompute_cell_center_g(const cs_mesh_builder_t  *mb,
 
   cs_lnum_t _n_cells = 0;
   cs_lnum_t _n_faces = 0;
-  cs_lnum_t _n_vertices = 0;
 
   cs_gnum_t *_cell_num = NULL;
   cs_gnum_t *_face_num = NULL;
-  cs_gnum_t *_vtx_num = NULL;
   cs_gnum_t *_face_gcells = NULL;
   cs_gnum_t *_face_gvertices = NULL;
 
   cs_lnum_t *_face_cells = NULL;
   cs_lnum_t *_face_vertices_idx = NULL;
   cs_lnum_t *_face_vertices = NULL;
-
-  cs_real_t *_vtx_coord = NULL;
 
   cs_block_to_part_t *d = NULL;
 
@@ -687,24 +684,31 @@ _precompute_cell_center_g(const cs_mesh_builder_t  *mb,
 
   /* Vertices */
 
-  d = cs_block_to_part_create_adj(comm,
-                                  mb->vertex_bi,
-                                  _face_vertices_idx[_n_faces],
-                                  _face_gvertices);
+  size_t     _n_vertices = 0;
+  cs_gnum_t *_vtx_num = NULL;
 
-  _n_vertices = cs_block_to_part_get_n_part_ents(d);
+  cs_order_single_gnum(_face_vertices_idx[_n_faces],
+                       1, /* base */
+                       _face_gvertices,
+                       &_n_vertices,
+                       &_vtx_num);
 
-  BFT_MALLOC(_vtx_coord, _n_vertices*3, cs_real_t);
+  cs_all_to_all_t *dv
+    = cs_all_to_all_create_from_block(_n_vertices,
+                                      CS_ALL_TO_ALL_USE_DEST_ID,
+                                      _vtx_num,
+                                      mb->vertex_bi,
+                                      comm);
 
-  cs_block_to_part_copy_array(d,
-                              real_type,
-                              3,
-                              mb->vertex_coords,
-                              _vtx_coord);
+  cs_real_t *_vtx_coord
+    = cs_all_to_all_copy_array(dv,
+                               real_type,
+                               3,
+                               true, /* reverse */
+                               mb->vertex_coords,
+                               NULL);
 
-  _vtx_num = cs_block_to_part_transfer_gnum(d);
-
-  cs_block_to_part_destroy(&d);
+  cs_all_to_all_destroy(&dv);
 
   /* Now convert face -> vertex connectivity to local vertex numbers */
 

commit 0ca018044ad7284a254ce0b338406d13bfcf6ce0

Fix crash in some parallel cases, introduced by commit f50d931b (Jan 16 2020).
The crash was observed on the INTERNAL_COUPLING test case with mesh tet05,
using Morton (bounding box) partitioning and running on 8 ranks.
This case has some isolated faces.

src/base/cs_order.c
@@ -1444,7 +1444,8 @@ cs_order_single_gnum(size_t            n_ent,
   while (s_id < n_ent && _n_single == 0) {
     if (number[order[s_id]] >= base)
       _n_single = 1;
-    s_id++;
+    else
+      s_id++;
   }
 
   for (size_t i = s_id+1; i < n_ent; i++) {
@@ -1461,7 +1462,7 @@ cs_order_single_gnum(size_t            n_ent,
     size_t j = 0;
     cs_gnum_t num_c = number[order[s_id]];
 
-    _single[j++] = number[order[s_id]];
+    _single[j++] = num_c;
 
     cs_gnum_t num_p = num_c;
     for (size_t i = s_id+1; i < n_ent; i++) {