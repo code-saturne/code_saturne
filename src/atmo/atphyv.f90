code-saturne/code_saturne

commit c3a833d6505dddf58cfda034d7d54ae00deccb6c

Major commit: First pass of merge between Cooling Towers module and atmospheric module.
This closes issue #14 on gitlab.
Humid air properties are moved to C and merged between the two modules.
The one of Cooling Towers have a larger scope and therefore are used for
the two modules.

Some renaming are performed:
 - function qsatliq -> cs_air_yw_sat
 - fortran variable itotwt -> iymw
 - function for air EOS cs_ctwr_* -> cs_air_*

src/atmo/atphyv.f90
@@ -71,12 +71,6 @@ double precision, dimension(:), pointer :: cpro_tempc, cpro_liqwt
 
 logical activate
 
-! External function
-
-double precision qsatliq
-external qsatliq
-! call as: qsatliq(temperature,pressure)
-
 !===============================================================================
 ! 0. INITIALISATIONS A CONSERVER
 !===============================================================================
@@ -120,7 +114,7 @@ call field_get_val_s(ibrom, brom)
 
 call field_get_val_s(itempc,cpro_tempc)
 call field_get_val_s(ivarfl(ivart), cvar_vart)
-if (ippmod(iatmos).ge.2) call field_get_val_s(ivarfl(isca(itotwt)), cvar_totwt)
+if (ippmod(iatmos).ge.2) call field_get_val_s(ivarfl(isca(iymw)), cvar_totwt)
 
 ! From potential temperature, compute:
 ! - Temperature in Celsius
@@ -141,6 +135,7 @@ do iel = 1, ncel
 
   if (ippmod(iatmos).ge.2) then  ! humid atmosphere
     lrhum = rair*(1.d0 + (rvsra - 1.d0)*cvar_totwt(iel))
+    ! R/Cp
     lrscp = (rair/cp0)*(1.d0 + (rvsra - cpvcpa)*                    &
             cvar_totwt(iel))
   endif
@@ -168,7 +163,7 @@ do iel = 1, ncel
   !   ------------------------
   !   law:    RHO       =   P / ( Rair * T(K) )
 
-  crom(iel) = pp/(lrhum*xvart)*(ps/pp)**lrscp
+  crom(iel) = cs_rho_humidair(cvar_totwt(iel), pp, cpro_tempc(iel))
 
 enddo
 
@@ -245,7 +240,7 @@ do iel = 1, ncel
   xvart = cvar_vart(iel) ! thermal scalar: liquid potential temperature
   tliq = xvart*(pp/ps)**rscp ! liquid temperature
   qwt  = cvar_totwt(iel) !total water content
-  qsl = qsatliq(tliq, pp) ! saturated vapor content
+  qsl = cs_air_yw_sat(tliq-tkelvi, pp) ! saturated vapor content
   deltaq = qwt - qsl
 
   if (activate) then
@@ -261,11 +256,8 @@ do iel = 1, ncel
   endif
 
   if (deltaq.le.0.d0) then ! unsaturated air parcel
-    lrhum = rair*(1.d0 + (rvsra - 1.d0)*qwt)
     !Celcius temperature of the air parcel
     cpro_tempc(iel) = tliq - tkelvi
-    !density of the air parcel
-    crom(iel) = pp/(lrhum*tliq)
     !liquid water content
     cpro_liqwt(iel) = 0.d0
     nebdia(iel) = 0.d0
@@ -273,16 +265,14 @@ do iel = 1, ncel
   else ! saturated (ie. with liquid water) air parcel
     qliq = deltaq/ &
          (1.d0 + qsl*clatev**2/(rair*rvsra*cp0*tliq**2))
-    lrhum = rair*(1.d0 - qliq + (rvsra - 1.d0)*(qwt - qliq))
     ! liquid water content
     cpro_liqwt(iel) = qliq
     ! Celcius temperature of the air parcel
     cpro_tempc(iel) = tliq + (clatev/cp0)*qliq - tkelvi
-    ! density
-    crom(iel) = pp/(lrhum*(tliq + (clatev/cp0)*qliq))
     nebdia(iel) = 1.d0
     nn(iel) = 0.d0
   endif
+  crom(iel) = cs_rho_humidair(qwt, pp, tliq-tkelvi)
 
 enddo ! iel = 1, ncel
 end subroutine all_or_nothing
@@ -295,6 +285,8 @@ end subroutine all_or_nothing
 subroutine gaussian()
 double precision, dimension(:,:), allocatable :: dtlsd
 double precision, dimension(:,:), allocatable :: dqsd
+integer    iccocg
+integer    inc
 
 double precision a_const
 double precision a_coeff
@@ -310,11 +302,19 @@ double precision, dimension(:), pointer :: cvar_k, cvar_ep
 allocate(dtlsd(3,ncelet))
 allocate(dqsd(3,ncelet))
 
-! computation of grad(thetal)
-call grad_thetal(dtlsd)
+iccocg = 1
+inc = 1
+
+! computation of grad(theta_l)
+call field_gradient_scalar(ivarfl(isca(iscalt)), 1, imrgra, inc,    &
+                           iccocg,                                  &
+                           dtlsd)
 
 ! computation of grad(qw)
-call grad_qw(dqsd)
+call field_gradient_scalar(ivarfl(isca(iymw)), 1, imrgra, inc,      &
+                           iccocg,                                  &
+                           dqsd)
+
 
 call field_get_val_s(ivarfl(ik), cvar_k)
 call field_get_val_s(ivarfl(iep), cvar_ep)
@@ -348,7 +348,7 @@ do iel = 1, ncel
   xvart = cvar_vart(iel) ! thermal scalar: liquid potential temperature
   tliq = xvart*(pp/ps)**rscp ! liquid temperature
   qwt  = cvar_totwt(iel) ! total water content
-  qsl = qsatliq(tliq, pp) ! saturated vapor content
+  qsl = cs_air_yw_sat(tliq-tkelvi, pp) ! saturated vapor content
   deltaq = qwt - qsl
   alpha = (clatev*qsl/(rvap*tliq**2))*(pp/ps)**rscp
   sig_flu = sqrt(var_q + alpha**2*var_tl - 2.d0*alpha*cov_tlq)
@@ -370,27 +370,23 @@ do iel = 1, ncel
   if(qwt.lt.qliq)then
     ! go back to all or nothing
     if (deltaq.le.0.d0) then ! unsaturated air parcel
-      lrhum = rair*(1.d0 + (rvsra-1.d0)*qwt)
       !Celcius temperature of the air parcel
       cpro_tempc(iel) = tliq - tkelvi
       !density of the air parcel
-      crom(iel) = pp/(lrhum*tliq)
       !liquid water content
       cpro_liqwt(iel) = 0.d0
       nebdia(iel) = 0.d0
       nn(iel) = 0.d0
     else ! saturated (ie. with liquid water) air parcel
       qliq = deltaq / (1.d0 + qsl*clatev**2/(rair*rvsra*cp0*tliq**2))
-      lrhum = rair*(1.d0 - qliq + (rvsra - 1.d0)*(qwt - qliq))
       ! liquid water content
       cpro_liqwt(iel) = qliq
       ! Celcius temperature of the air parcel
       cpro_tempc(iel) = tliq+(clatev/cp0)*qliq - tkelvi
-      ! density
-      crom(iel) = pp/(lrhum*(tliq + (clatev/cp0)*qliq))
       nebdia(iel) = 1.d0
       nn(iel) = 0.d0
     endif
+    crom(iel) = cs_rho_humidair(qwt, pp, tliq-tkelvi)
   else ! coherent subgrid diagnostic
     lrhum = rair*(1.d0 - qliq + (rvsra - 1.d0)*(qwt - qliq))
     ! liquid water content
@@ -398,9 +394,8 @@ do iel = 1, ncel
     !Celcius temperature of the air parcel
     cpro_tempc(iel) = tliq + (clatev/cp0)*qliq - tkelvi
     !density
-    crom(iel) = pp/(lrhum*(tliq + (clatev/cp0)*qliq))
+    crom(iel) = pp/(lrhum*(tliq + (clatev/cp0)*qliq)) !FIXME use property function of humid air at saturation?
   endif ! qwt.lt.qliq
-
 enddo
 
 ! when properly finished deallocate dtlsd
@@ -409,67 +404,4 @@ deallocate(dqsd)
 
 end subroutine gaussian
 
-! *******************************************************************
-!> \brief Internal function -
-!> Computation of the gradient of the potential temperature
-!-------------------------------------------------------------------------------
-! Arguments
-!______________________________________________________________________________.
-!  mode           name          role                                           !
-!______________________________________________________________________________!
-!> \param[out]   dtlsd          gradient of potential temperature
-!-------------------------------------------------------------------------------
-subroutine grad_thetal(dtlsd)
-double precision dtlsd(3,ncelet)
-
-integer    iccocg
-integer    iivar
-integer    inc
-integer    itpp
-
-itpp = isca(iscalt)
-
-! options for gradient calculation
-
-iccocg = 1
-inc = 1
-
-iivar = itpp
-
-call field_gradient_scalar(ivarfl(iivar), 1, imrgra, inc,           &
-                           iccocg,                                  &
-                           dtlsd)
-
-end subroutine grad_thetal
-
-! *******************************************************************
-! *
-! *******************************************************************
-!> \brief Internal function -
-!> Compute the gradient of the total humiduty
-!-------------------------------------------------------------------------------
-! Arguments
-!______________________________________________________________________________.
-!  mode           name          role                                           !
-!______________________________________________________________________________!
-!> \param[out]      dqsd        gradient of total humidity
-!-------------------------------------------------------------------------------
-subroutine grad_qw(dqsd)
-double precision dqsd(3,ncelet)
-
-integer    iccocg
-integer    iivar
-integer    inc
-integer    iqw
-
-iccocg = 1
-inc = 1
-iqw = isca(itotwt)
-iivar = iqw
-
-call field_gradient_scalar(ivarfl(iivar), 1, imrgra, inc,           &
-                           iccocg,                                  &
-                           dqsd)
-
-end subroutine grad_qw
 end subroutine atphyv

src/atmo/Makefile.am
@@ -55,6 +55,7 @@ AM_LDFLAGS =
 # Public header files (to be installed)
 
 pkginclude_HEADERS = \
+cs_air_props.h \
 cs_at_plugin.h \
 cs_at_opt_interp.h \
 cs_at_data_assim.h \
@@ -89,6 +90,7 @@ chem_solvelu.f90 \
 chem_source_terms.f90 \
 compute_gaseous_chemistry.f90 \
 compute_siream.f90 \
+cs_air_props.c \
 cs_at_plugin.c \
 cs_at_opt_interp.c \
 cs_at_data_assim.c \
@@ -104,7 +106,6 @@ rayive.f90 \
 rayso.f90 \
 raysze.f90 \
 roschem.f90 \
-satfun.f90 \
 solcat.f90 \
 soliva.f90 \
 solmoy.f90 \

src/atmo/atimbr.f90
@@ -1352,8 +1352,6 @@ integer ih2o
 data ih2o/0/
 save ih2o
 double precision rap
-double precision qsatliq
-external qsatliq
 
 if (ippmod(iatmos).eq.2) ih2o=1
 if(.not.allocated(pressure))then
@@ -1375,9 +1373,9 @@ integration_direction:if (ihpm.eq.0) then
              "hydrostatic_pressure::k=",k
         tmoy = 0.5d0*(tempC(k-1,j,i) + tempC(k,j,i)) + tkelvi
         if (ippmod(iatmos).eq.2) then
-          q0 = min( qw(k-1,j,i), qsatliq( tempC(k-1,j,i)+tkelvi, &
+          q0 = min( qw(k-1,j,i), cs_air_yw_sat( tempC(k-1,j,i), &
                pressure(k-1,j,i)))
-          q1 = min( qw(k  ,j,i), qsatliq( tempC(k  ,j,i)+tkelvi, &
+          q1 = min( qw(k  ,j,i), cs_air_yw_sat( tempC(k  ,j,i), &
                pressure(k-1,j,i)))
         else
           q0 = qw(k-1,j,i)
@@ -1399,9 +1397,9 @@ else
       do k = thermal_profile_dim, 2, -1
         tmoy = 0.5d0*(tempC(k-1,j,i) + tempC(k,j,i)) + tkelvi
         if (ippmod(iatmos).eq.2) then
-          q0 = min( qw(k-1,j,i), qsatliq( tempC(k-1,j,i)+tkelvi, &
+          q0 = min( qw(k-1,j,i), cs_air_yw_sat( tempC(k-1,j,i), &
                pressure(k  ,j,i)))
-          q1 = min( qw(k  ,j,i), qsatliq( tempC(k  ,j,i)+tkelvi, &
+          q1 = min( qw(k  ,j,i), cs_air_yw_sat( tempC(k  ,j,i), &
                pressure(k  ,j,i)))
         else
           q0 = qw(k-1,j,i)

src/atmo/atincl.f90
@@ -107,8 +107,8 @@ double precision, allocatable, dimension(:) :: nn
 ! 1.2 Pointers for the positions of the variables
 !------------------------------------------------
 !   Variables specific to the atmospheric physics:
-!> itotwt---> total water content (for humid atmosphere)
-integer, save :: itotwt
+!> total water content (for humid atmosphere)
+integer, save :: iymw
 !> intdrp---> total number of droplets (for humid atmosphere)
 integer, save :: intdrp
 
@@ -177,14 +177,8 @@ integer, save :: theo_interp
 !> reference pressure (to compute potential temp: 1.0d+5)
 double precision, save:: ps
 
-!> ratio gaz constant h2o/ dry air: 1.608d0
-double precision, save:: rvsra
-
 !> ratio Cp h2o/ dry air: 1.866d0
 double precision, save:: cpvcpa
-
-!> latent heat of evaporation: 2.501d+6
-double precision, save:: clatev
 
 !> temperature gradient for the standard atmosphere (-6.5d-03 K/m)
 double precision, save:: gammat
@@ -385,6 +379,118 @@ integer, save :: init_at_chem
 integer, save :: kopint
 
 !> \}
+
+!=============================================================================
+
+  interface
+
+    !---------------------------------------------------------------------------
+
+    !> \brief Calculation of the specific enthalpy of liquid water
+
+    !> \return specific enthalpy of liquid water
+
+    !> \param[in]  t_l  liquid water temperature (in Celsius)
+
+    function cs_liq_t_to_h(t_l) result(h_l) &
+        bind(C, name='cs_liq_t_to_h')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      real(c_double), value :: t_l
+      real(c_double) :: h_l
+    end function cs_liq_t_to_h
+
+    !---------------------------------------------------------------------------
+
+    !> \brief Calculation of the absolute humidity at saturation for a given temperature.
+
+    !> \param[in]  t_c  temperature (in Celsius)
+    !> \param[in]  p    pressure
+
+    function cs_air_x_sat(t_c, p) result(x_s) &
+        bind(C, name='cs_air_x_sat')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      real(c_double), value :: t_c, p
+      real(c_double) :: x_s
+    end function cs_air_x_sat
+
+    !---------------------------------------------------------------------------
+
+    !> \brief Calculation of the air water mass fraction at saturation for a given temperature.
+
+    !> \param[in]  t_c  temperature (in Celsius)
+    !> \param[in]  p    pressure
+
+    function cs_air_yw_sat(t_c, p) result(x_s) &
+        bind(C, name='cs_air_yw_sat')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      real(c_double), value :: t_c, p
+      real(c_double) :: x_s
+    end function cs_air_yw_sat
+
+    !---------------------------------------------------------------------------
+
+    !> \brief Computes the saturation water vapour pressure function of the temperature (C).
+
+    !> \param[in]  t_c  temperature (in Celsius)
+
+    function cs_air_pwv_sat(t_c) result(x_s) &
+        bind(C, name='cs_air_pwv_sat')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      real(c_double), value :: t_c
+      real(c_double) :: x_s
+    end function cs_air_pwv_sat
+
+    !---------------------------------------------------------------------------
+
+    !> \brief Convert the absolute humidity of humid air to the air water mass fraction.
+
+    !> \param[in]  x  absolute humidity of humid air
+
+    function cs_air_x_to_yw(x) result(qw) &
+        bind(C, name='cs_air_x_to_yw')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      real(c_double), value :: x
+      real(c_double) :: qw
+    end function cs_air_x_to_yw
+
+    !---------------------------------------------------------------------------
+
+    !> \brief Convert the air water mass fraction to the absolute humidity of humid air.
+
+    !> \param[in]  qw  air water mass fraction
+
+    function cs_air_yw_to_x(qw) result(x) &
+        bind(C, name='cs_air_yw_to_x')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      real(c_double), value :: qw
+      real(c_double) :: x
+    end function cs_air_yw_to_x
+
+    !---------------------------------------------------------------------------
+
+    !> \brief Calculation of the density of humid air.
+
+    !> \param[in]  qw  air water mass fraction
+    !> \param[in]  p    pressure
+    !> \param[in]  t_h  temperature (in Celsius)
+
+    function cs_rho_humidair(qw, p, t_h) result(rho_h) &
+        bind(C, name='cs_rho_humidair')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      real(c_double), value :: qw, p, t_h
+      real(c_double) :: rho_h
+    end function cs_rho_humidair
+
+    !=============================================================================
+
+  end interface
 
 contains
 

src/atmo/atini0.f90
@@ -61,9 +61,7 @@ implicit none
 !    (see definition in atincl.f90):
 
 ps = 1.0d5
-rvsra = 1.608d0
 cpvcpa = 1.866d0
-clatev = 2.501d6
 gammat = -6.5d-03
 rvap = rvsra*rair
 

src/atmo/atini1.f90
@@ -87,9 +87,7 @@ endif
 !    (see definition in atincl.h):
 
 ps = 1.0d5
-rvsra = 1.608d0
 cpvcpa = 1.866d0
-clatev = 2.501d6
 gammat = -6.5d-03
 rvap = rvsra*rair
 

src/atmo/atiniv.f90
@@ -244,7 +244,7 @@ if (isuite.eq.0) then
       call field_get_val_s(ivarfl(isca(iscalt)), cvar_scalt)
     else if (ippmod(iatmos).eq.2) then
       call field_get_val_s(ivarfl(isca(iscalt)), cvar_scalt)
-      call field_get_val_s(ivarfl(isca(itotwt)), cvar_totwt)
+      call field_get_val_s(ivarfl(isca(iymw)), cvar_totwt)
       call field_get_val_s(ivarfl(isca(intdrp)), cvar_ntdrp)
     endif
 

src/atmo/atlecm.f90
@@ -53,7 +53,6 @@ integer           imode
 
 ! a function used in the routine
 ! for the diagnostic of liquid water content
-double precision qsatliq
 
 ! Local variables
 
@@ -337,10 +336,10 @@ if (imode.eq.1) then
       tmoy = 0.5d0*(ttmet(k-1,itp) + ttmet(k,itp)) + tkelvi
 
       if(ippmod(iatmos).eq.2) then ! take liquid water into account
-        q0 = min( qvmet(k-1,itp), qsatliq( ttmet(k-1,itp) &
-            + tkelvi, phmet(k-1,itp)))
-        q1 = min( qvmet(k  ,itp), qsatliq( ttmet(k  ,itp) &
-            + tkelvi, phmet(k-1,itp)))
+        q0 = min( qvmet(k-1,itp), cs_air_yw_sat( ttmet(k-1,itp) &
+            , phmet(k-1,itp)))
+        q1 = min( qvmet(k  ,itp), cs_air_yw_sat( ttmet(k  ,itp) &
+            , phmet(k-1,itp)))
         !in q1=.. phmet(k-1,itp) is not a mistake: we can not use phmet(k,itp)
         !since this is what we want to estimate.
       else
@@ -360,8 +359,8 @@ if (imode.eq.1) then
       tmoy = 0.5d0*(ttmet(k+1,itp) + ttmet(k,itp)) + tkelvi
 
       if(ippmod(iatmos).eq.2) then ! take liquid water into account
-        q0 = min( qvmet(k,itp), qsatliq( ttmet(k,itp)+tkelvi, phmet(k+1,itp)))
-        q1 = min( qvmet(k+1  ,itp), qsatliq( ttmet(k+1,itp)+tkelvi, phmet(k+1,itp)))
+        q0 = min( qvmet(k,itp), cs_air_yw_sat( ttmet(k,itp), phmet(k+1,itp)))
+        q1 = min( qvmet(k+1  ,itp), cs_air_yw_sat( ttmet(k+1,itp), phmet(k+1,itp)))
         !in q0=.. phmet(k+1,itp) is not a mistake: we can not use phmet(k,itp)
         !since this is what we want to estimate.
       else
@@ -479,7 +478,7 @@ if (imode.eq.1) then
       write(nfecra,7999)                                            &
            ztmet(ii), ttmet(ii,itp), tpmet(ii,itp),              &
            rmet(ii,itp), phmet(ii,itp), qvmet(ii,itp),           &
-           qsatliq(ttmet(ii,itp)+tkelvi , phmet(ii,itp)),         &
+           cs_air_yw_sat(ttmet(ii,itp) , phmet(ii,itp)),         &
            ncmet(ii,itp)
 7999  format(1x, 3f8.2,f8.4,f12.3,e10.3,e10.3,e12.5)
     enddo

commit 17a84e93e2adfa19454a96e0600fc8115710aacf

[Atmo.] fix bug in humid atmosphere introduced in commit c3a833d65 (2019-04-03).
The liqui water content given by the Gaussian law for droplets was overwritten
by the generic Equatio Of State.

Results in 69_PARISFOG are consistent with v5.0 version, even if not
exacty equal (a clipping on yw_liq at 10^-15 was removed and another one
at 10^-8).

src/atmo/atphyv.f90
@@ -309,7 +309,7 @@ do iel = 1, ncel
   yw_liq = (sig_flu                                                               &
         /(1.d0 + qsl*clatev**2/(rvap*cp0*tliq**2)))                             &
         *(nebdia(iel)*q1 + exp(-q1**2/2.d0)/sqrt(2.d0*pi))
-  yw_liq = max(yw_liq, 1d-15)
+  yw_liq = max(yw_liq, 0.d0)
   nn(iel) = nebdia(iel) - (nebdia(iel)*q1                                       &
           + exp(-q1**2/2.d0)/sqrt(2.d0*pi))*exp(-q1**2/2.d0)/sqrt(2.d0*pi)
 
@@ -331,15 +331,15 @@ do iel = 1, ncel
     yw_liq = deltaq / (1.d0 + qsl*clatev**2/(rvap*cp0*tliq**2))
   endif ! qwt.lt.yw_liq
 
-  cpro_liqwt(iel) = yw_liq
   ! Celcius temperature of the air parcel
   cpro_tempc(iel) = tliq + (clatev/cp0)*yw_liq - tkelvi
-
-  !FIXME back to the previous formulation
-  call cs_rho_humidair(qwt, tliq, pp,   &
-                       cpro_liqwt(iel), &
-                       cpro_tempc(iel), &
-                       crom(iel))
+  lrhum = rair*(1.d0 - yw_liq + (rvsra - 1.d0)*(qwt - yw_liq))
+  ! liquid water content
+  cpro_liqwt(iel) = yw_liq
+  !Celcius temperature of the air parcel
+  cpro_tempc(iel) = tliq + (clatev/cp0)*yw_liq - tkelvi
+  !density
+  crom(iel) = pp/(lrhum*(tliq + (clatev/cp0)*yw_liq))
 
 enddo
 

src/atmo/solvar.f90
@@ -201,7 +201,7 @@ do isol = 1, nfmodsol
     !     ====================================
 
     ! on impose t = tmer et hr = 100 %
-    esat = 610.78d0*exp(17.2694d0*tmer/(tmer + tkelvi-35.86d0))
+    esat  = cs_air_pwv_sat(tmer)
 
     if (imeteo.eq.0) then
       call atmstd(zreel,pres1,dum,dum)
@@ -284,12 +284,12 @@ do isol = 1, nfmodsol
     !     7) calcul de la pression de vapeur saturante esat et de d(qsat)/dt
     !     ==================================================================
 
-    esat = 610.78d0*exp(17.2694d0*(tssol - tkelvi)/(tssol - 35.86d0))
+    esat  = cs_air_pwv_sat(tssol-tkelvi)
     rapsat = rvsra*pres1+esat*(1.d0-rvsra)
     qsat = esat/rapsat
-    cstder = 17.2694d0*(tkelvi - 35.86d0)
+    cstder = 17.438d0*(239.78d0)!TODO code it in  cs_air_prop.c
     dqsat = pres1*rvsra/rapsat/rapsat*cstder*esat                     &
-         /(tssol - 35.86d0)/(tssol - 35.86d0)
+         /(tssol -tkelvi+ 239.78d0)/(tssol -tkelvi+ 239.78d0)!TODO code it in  cs_air_prop.c
 
     !     ===========================================================
     !     8) calcul du premier membre de l'equation d'evolution de tssol