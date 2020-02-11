code-saturne/code_saturne

commit f566c24bf6f9a43e7b051030b405e71dc7335fa8

GUI Update: Allow naming of probes in GUI
  * Added a "name" field to monitoring points page
  * Closes Gitlab issue #151

gui/Pages/OutputControlView.py
@@ -1115,7 +1115,7 @@ class StandardItemModelMonitoring(QStandardItemModel):
         """
         QStandardItemModel.__init__(self)
 
-        self.setColumnCount(4)
+        self.setColumnCount(5)
         self.dataMonitoring = []
 
 
@@ -1131,10 +1131,12 @@ class StandardItemModelMonitoring(QStandardItemModel):
             if index.column() == 0:
                 return dico['n']
             elif index.column() == 1:
-                return dico['X']
+                return dico['name']
             elif index.column() == 2:
-                return dico['Y']
+                return dico['X']
             elif index.column() == 3:
+                return dico['Y']
+            elif index.column() == 4:
                 return dico['Z']
             else:
                 return None
@@ -1159,10 +1161,12 @@ class StandardItemModelMonitoring(QStandardItemModel):
             if section == 0:
                 return self.tr("n")
             elif section == 1:
-                return self.tr("X")
+                return self.tr("name")
             elif section == 2:
-                return self.tr("Y")
+                return self.tr("X")
             elif section == 3:
+                return self.tr("Y")
+            elif section == 4:
                 return self.tr("Z")
         return None
 
@@ -1173,12 +1177,15 @@ class StandardItemModelMonitoring(QStandardItemModel):
             n = from_qvariant(value, int)
             self.dataMonitoring[row]['n'] = n
         elif index.column() == 1:
+            name = from_qvariant(value, str)
+            self.dataMonitoring[row]['name'] = name
+        elif index.column() == 2:
             X = from_qvariant(value, float)
             self.dataMonitoring[row]['X'] = X
-        elif index.column() == 2:
+        elif index.column() == 3:
             Y = from_qvariant(value, float)
             self.dataMonitoring[row]['Y'] = Y
-        elif index.column() == 3:
+        elif index.column() == 4:
             Z = from_qvariant(value, float)
             self.dataMonitoring[row]['Z'] = Z
 
@@ -1186,29 +1193,31 @@ class StandardItemModelMonitoring(QStandardItemModel):
         return True
 
 
-    def insertData(self, num, X, Y, Z):
+    def insertData(self, num, name, X, Y, Z):
         """
         Add a new 'item' into the table.
         """
         dico = {}
-        dico['n'] = num
-        dico['X'] = X
-        dico['Y'] = Y
-        dico['Z'] = Z
+        dico['n']    = num
+        dico['name'] = name
+        dico['X']    = X
+        dico['Y']    = Y
+        dico['Z']    = Z
         self.dataMonitoring.append(dico)
 
         row = self.rowCount()
         self.setRowCount(row + 1)
 
 
-    def replaceData(self, row, label, X, Y, Z):
+    def replaceData(self, row, label, name, X, Y, Z):
         """
         Replace value in an existing 'item' into the table.
         """
-        self.dataMonitoring[row]['n'] = label
-        self.dataMonitoring[row]['X'] = X
-        self.dataMonitoring[row]['Y'] = Y
-        self.dataMonitoring[row]['Z'] = Z
+        self.dataMonitoring[row]['n']    = label
+        self.dataMonitoring[row]['name'] = name
+        self.dataMonitoring[row]['X']    = X
+        self.dataMonitoring[row]['Y']    = Y
+        self.dataMonitoring[row]['Z']    = Z
 
         row = self.rowCount()
         self.setRowCount(row)
@@ -1216,6 +1225,10 @@ class StandardItemModelMonitoring(QStandardItemModel):
 
     def getLabel(self, index):
         return self.getData(index)['n']
+
+
+    def getName(self, index):
+        return self.getData(index)['name']
 
 
     def getData(self, index):
@@ -1244,8 +1257,8 @@ class MonitoringPointDelegate(QItemDelegate):
         """
         super(MonitoringPointDelegate, self).__init__(parent)
         self.table = parent
-        self.case = case
-        self.mdl = model
+        self.case  = case
+        self.mdl   = model
 
 
     def createEditor(self, parent, option, index):
@@ -1253,7 +1266,11 @@ class MonitoringPointDelegate(QItemDelegate):
             editor = QFrame(parent)
         else:
             editor = QLineEdit(parent)
-            editor.setValidator(DoubleValidator(editor))
+            if index.column() > 1:
+                editor.setValidator(DoubleValidator(editor))
+            else:
+                rx = "[_A-Za-z0-9\(\)]{1," + str(LABEL_LENGTH_MAX-1) + "}"
+                editor.setValidator(RegExpValidator(editor, QRegExp(rx)))
             editor.setFrame(False)
             editor.returnPressed.connect(self.commitAndCloseEditor)
             editor.setCursorPosition(0)
@@ -1288,7 +1305,9 @@ class MonitoringPointDelegate(QItemDelegate):
                 y = float(dico['Y'])
                 z = float(dico['Z'])
                 label = str(dico['n'])
+                name  = str(dico['name'])
                 self.mdl.replaceMonitoringPointCoordinates(label, x, y, z)
+                self.mdl.setMonitoringPointName(label, name)
                 if self.case['probes']:
                     self.case['probes'].updateLocation(label, [x, y, z])
 
@@ -1635,9 +1654,10 @@ class OutputControlView(QWidget, Ui_OutputControlForm):
             self.toolButtonDuplicate.setEnabled(True)
 
         for n in range(self.mdl.getNumberOfMonitoringPoints()):
-            name = str(n+1)
-            X, Y, Z = self.mdl.getMonitoringPointCoordinates(name)
-            self.__insertMonitoringPoint(name, X, Y, Z)
+            num = str(n+1)
+            name = self.mdl.getMonitoringPointName(num)
+            X, Y, Z = self.mdl.getMonitoringPointCoordinates(num)
+            self.__insertMonitoringPoint(num, name, X, Y, Z)
             if self.case['salome']:
                 self.__salomeHandlerAddMonitoringPoint(name, X, Y, Z)
 
@@ -2689,11 +2709,11 @@ class OutputControlView(QWidget, Ui_OutputControlForm):
             self.mdl.setMonitoringPointFrequency(n)
 
 
-    def __insertMonitoringPoint(self, num, X, Y, Z):
+    def __insertMonitoringPoint(self, num, name, X, Y, Z):
         """
         Add a new 'item' into the Hlist.
         """
-        self.modelMonitoring.insertData(num, X, Y, Z)
+        self.modelMonitoring.insertData(num, name, X, Y, Z)
 
 
     @pyqtSlot()

bin/model/OutputControlModel.py
@@ -78,22 +78,22 @@ class OutputControlModel(Model):
         return default
 
 
-    def __getCoordinates(self, name, coord):
+    def __getCoordinates(self, num, coord):
         """
-        Private method: return coordinate 'coord' for probe named 'name'
+        Private method: return coordinate 'coord' for probe number 'num'
         """
-        val = self.node_out.xmlGetNode('probe', name = name).xmlGetDouble(coord)
+        val = self.node_out.xmlGetNode('probe', id = num).xmlGetDouble(coord)
         if val == None:
             val = self.defaultInitialValues()['coordinate']
             self.__setCoordinates(name, coord, val)
         return val
 
 
-    def __setCoordinates(self, name, coord, val):
+    def __setCoordinates(self, num, coord, val):
         """
-        Private method: put value of coordinate 'coord' for probe named 'name'
+        Private method: put value of coordinate 'coord' for probe number 'num'
         """
-        self.node_out.xmlGetNode('probe', name=name).xmlSetData(coord, val)
+        self.node_out.xmlGetNode('probe', id=num).xmlSetData(coord, val)
 
 
     @Variables.noUndo
@@ -1093,7 +1093,7 @@ class OutputControlModel(Model):
         self.isFloat(z)
         num = str(self.getNumberOfMonitoringPoints() + 1)
         status="on"
-        node = self.node_out.xmlInitNode('probe', name=num, status=status)
+        node = self.node_out.xmlInitNode('probe', id=num, name=num, status=status)
         for coord, val in [('probe_x', x), ('probe_y', y), ('probe_z', z)]:
             self.__setCoordinates(num, coord, val)
 
@@ -1125,13 +1125,13 @@ class OutputControlModel(Model):
         self.isFloat(z)
         num = str(self.getNumberOfMonitoringPoints() + 1)
         status="on"
-        node = self.node_out.xmlInitNode('probe', name=num, status=status)
+        node = self.node_out.xmlInitNode('probe', id=num, name=num, status=status)
         for coord, val in [('probe_x', x), ('probe_y', y), ('probe_z', z)]:
             self.__setCoordinates(num, coord, val)
 
 
     @Variables.undoLocal
-    def replaceMonitoringPointCoordinates(self, name, x=0.0, y=0.0, z=0.0):
+    def replaceMonitoringPointCoordinates(self, num, x=0.0, y=0.0, z=0.0):
         """
         Public method.
         Change the coordinates of a monitoring point.
@@ -1147,12 +1147,12 @@ class OutputControlModel(Model):
         self.isFloat(x)
         self.isFloat(y)
         self.isFloat(z)
-        self.isStr(name)
-        self.isGreater(float(name), 0.0)
-        self.isLowerOrEqual(float(name), self.getNumberOfMonitoringPoints())
+        self.isStr(num)
+        self.isGreater(float(num), 0.0)
+        self.isLowerOrEqual(float(num), self.getNumberOfMonitoringPoints())
 
         for coord, val in [('probe_x', x), ('probe_y', y), ('probe_z', z)]:
-            self.__setCoordinates(name, coord, val)
+            self.__setCoordinates(num, coord, val)
 
 
     def deleteMonitoringPoints(self, lst):
@@ -1165,8 +1165,8 @@ class OutputControlModel(Model):
         lst.sort()
         r = len(lst)
         for n in range(r):
-            name = str(lst[n])
-            self.deleteMonitoringPoint(name)
+            num = str(lst[n])
+            self.deleteMonitoringPoint(num)
             for i in range(n, r):
                 lst[i] = lst[i] - 1
 
@@ -1185,21 +1185,23 @@ class OutputControlModel(Model):
 
         # delete the node of the monitoring point
 
-        node = self.node_out.xmlGetNode('probe', name=num)
+        node = self.node_out.xmlGetNode('probe', id=num)
         if node:
             node.xmlRemoveNode()
-            self.case.xmlRemoveChild('probe_recording', name=num)
+            self.case.xmlRemoveChild('probe_recording', id=num)
 
             # renumbering of all monitoring points
 
             for p in range(int(num)+1, self.getNumberOfMonitoringPoints()+2):
-                probe = self.node_out.xmlGetNode('probe', name=p)
-                probe['name'] = p - 1
-                for probe_recording in self.case.xmlGetNodeList('probe_recording', name=p):
-                    probe_recording['name'] = p - 1
+                probe = self.node_out.xmlGetNode('probe', id=p)
+                if probe['name'] == probe['id']:
+                    probe['name'] = p - 1
+                probe['id'] = p - 1
+                for probe_recording in self.case.xmlGetNodeList('probe_recording', num=p):
+                    probe_recording['num'] = p - 1
 
     @Variables.noUndo
-    def getMonitoringPointCoordinates(self, name):
+    def getMonitoringPointCoordinates(self, num):
         """
         Public method.
         @type name: C{String}
@@ -1207,13 +1209,38 @@ class OutputControlModel(Model):
         @return: coordinates X, Y, Z for the monitoring point I{name}
         @rtype: C{List} of C{Float}
         """
-        self.isStr(name)
-        self.isGreater(float(name), 0.0)
-        self.isLowerOrEqual(float(name), self.getNumberOfMonitoringPoints())
-        X = self.__getCoordinates(name, 'probe_x')
-        Y = self.__getCoordinates(name, 'probe_y')
-        Z = self.__getCoordinates(name, 'probe_z')
+        self.isStr(num)
+        self.isGreater(float(num), 0.0)
+        self.isLowerOrEqual(float(num), self.getNumberOfMonitoringPoints())
+        X = self.__getCoordinates(num, 'probe_x')
+        Y = self.__getCoordinates(num, 'probe_y')
+        Z = self.__getCoordinates(num, 'probe_z')
         return X, Y, Z
+
+
+    @Variables.undoLocal
+    def setMonitoringPointName(self, num, name):
+
+        self.isStr(num)
+        self.isGreater(float(num), 0.0)
+        self.isLowerOrEqual(float(num), self.getNumberOfMonitoringPoints())
+
+        node = self.node_out.xmlGetNode('probe', id=num)
+        node['name'] = name
+
+
+    @Variables.noUndo
+    def getMonitoringPointName(self, num):
+
+        self.isStr(num)
+        self.isGreater(float(num), 0.0)
+        self.isLowerOrEqual(float(num), self.getNumberOfMonitoringPoints())
+        val = self.node_out.xmlGetNode('probe', id=num)['name']
+        if val == None:
+            val = num
+            self.setMonitoringPointName(self, num, name)
+
+        return val
 
 
     @Variables.noUndo
@@ -1272,7 +1299,7 @@ class OutputControlModelTestCase(ModelTest):
         model = OutputControlModel(self.case)
         model.addMonitoringPoint(11.1, 22.2, 33.3)
         doc = '''<output>
-                    <probe name="1" status="on">
+                    <probe id="1" name="1" status="on">
                         <probe_x>11.1</probe_x>
                         <probe_y>22.2</probe_y>
                         <probe_z>33.3</probe_z>
@@ -1292,12 +1319,12 @@ class OutputControlModelTestCase(ModelTest):
         model.addMonitoringPoint(5, 5.1, 5.21)
         model.replaceMonitoringPointCoordinates("2",5., 6, 7.)
         doc = '''<output>
-                    <probe name="1" status="on">
+                    <probe id="1" name="1" status="on">
                         <probe_x>11.1</probe_x>
                         <probe_y>22.2</probe_y>
                         <probe_z>33.3</probe_z>
                     </probe>
-                    <probe name="2" status="on">
+                    <probe id="2" name="2" status="on">
                         <probe_x>5</probe_x>
                         <probe_y>6</probe_y>
                         <probe_z>7</probe_z>
@@ -1316,12 +1343,12 @@ class OutputControlModelTestCase(ModelTest):
         model.addMonitoringPoint(9.,8.,7.)
         model.deleteMonitoringPoint("2")
         doc = '''<output>
-                    <probe name="1" status="on">
+                    <probe id="1" name="1" status="on">
                         <probe_x>11.1</probe_x>
                         <probe_y>22.2</probe_y>
                         <probe_z>33.3</probe_z>
                     </probe>
-                    <probe name="2" status="on">
+                    <probe id="2" name="2" status="on">
                         <probe_x>9</probe_x>
                         <probe_y>8</probe_y>
                         <probe_z>7</probe_z>

src/gui/cs_gui_output.c
@@ -488,6 +488,11 @@ cs_gui_postprocess_meshes(void)
 
   if (n_probes > 0) {
 
+    char **probe_labels;
+    BFT_MALLOC(probe_labels, n_probes, char *);
+    for (int ii = 0; ii < n_probes; ii++)
+      BFT_MALLOC(probe_labels[ii], 128, char);
+
     const char *coord_node_name[] = {"probe_x", "probe_y", "probe_z"};
 
     cs_real_3_t *p_coords;
@@ -498,18 +503,27 @@ cs_gui_postprocess_meshes(void)
          tn != NULL;
          tn = cs_tree_node_get_next_of_name(tn), i++) {
 
+      /* Probe coordinates */
       for (int j = 0; j < 3; j++) {
         v_r = cs_tree_node_get_child_values_real(tn, coord_node_name[j]);
         p_coords[i][j] = (v_r != NULL) ? v_r[0] : 0;
       }
+
+      /* Probe name */
+      const char *pn = cs_tree_node_get_child_value_str(tn, "name");
+      strcpy(probe_labels[i], pn);
     }
 
     cs_probe_set_create_from_array("probes",
                                    n_probes,
                                    (const cs_real_3_t *)p_coords,
-                                   NULL);
+                                   probe_labels);
 
     BFT_FREE(p_coords);
+
+    for (int ii = 0; ii < n_probes; ii++)
+      BFT_FREE(probe_labels[ii]);
+    BFT_FREE(probe_labels);
 
   }
 

commit 111eb89ca5c2c6885654abfc30bf62e260e0c0ba

Fix bug in import from csv introduced in commit f566c24b (2020-01-13).

gui/Pages/OutputControlView.py
@@ -2723,13 +2723,14 @@ class OutputControlView(QWidget, Ui_OutputControlForm):
         The number of the monitoring point is added at the precedent one
         """
         self.mdl.addMonitoringPoint(x=0.0, y=0.0, z=0.0)
-        n = self.mdl.getNumberOfMonitoringPoints()
-        self.__insertMonitoringPoint(n, str('0'), str('0'), str('0'))
+        num = self.mdl.getNumberOfMonitoringPoints()
+        name = self.mdl.getMonitoringPointName(num)
+        self.__insertMonitoringPoint(num, name, str('0'), str('0'), str('0'))
 
         self.toolButtonDuplicate.setEnabled(True)
 
         if self.case['salome']:
-            self.__salomeHandlerAddMonitoringPoint(n, 0., 0., 0.)
+            self.__salomeHandlerAddMonitoringPoint(num, 0., 0., 0.)
 
 
     @pyqtSlot()
@@ -2753,9 +2754,10 @@ class OutputControlView(QWidget, Ui_OutputControlForm):
 
         self.modelMonitoring.deleteAllData()
         for n in range(self.mdl.getNumberOfMonitoringPoints()):
-            name = str(n + 1)
-            X, Y, Z = self.mdl.getMonitoringPointCoordinates(name)
-            self.__insertMonitoringPoint(name, X, Y, Z)
+            num = str(n + 1)
+            name = self.mdl.getMonitoringPointName(num)
+            X, Y, Z = self.mdl.getMonitoringPointCoordinates(num)
+            self.__insertMonitoringPoint(num, name, X, Y, Z)
 
         if self.case['salome']:
             self.__salomeHandlerDeleteMonitoringPoint(l2)
@@ -2780,11 +2782,12 @@ class OutputControlView(QWidget, Ui_OutputControlForm):
         idx = 1
 
         for index in selectionModel.selectedRows():
-            name = str(index.row() + 1)
-            X, Y, Z = self.mdl.getMonitoringPointCoordinates(name)
-            new_name = str(probe_number + idx)
+            num = str(index.row() + 1)
+            X, Y, Z = self.mdl.getMonitoringPointCoordinates(num)
+            new_num = str(probe_number + idx)
+            new_name = self.mdl.getMonitoringPointName(new_num)
             self.mdl.addMonitoringPoint(x=X, y=Y, z=Z)
-            self.__insertMonitoringPoint(new_name, X, Y, Z)
+            self.__insertMonitoringPoint(new_num, new_name, X, Y, Z)
 
             if self.case['salome']:
                 self.__salomeHandlerAddMonitoringPoint(probe_number + idx, X, Y, Z)
@@ -2813,9 +2816,10 @@ class OutputControlView(QWidget, Ui_OutputControlForm):
         lst = self.mdl.ImportProbesFromCSV(fle)
 
         for idx in range(lst):
-            new_name = str(probe_number + idx + 1)
-            X, Y, Z = self.mdl.getMonitoringPointCoordinates(new_name)
-            self.__insertMonitoringPoint(new_name, X, Y, Z)
+            num = str(probe_number + idx + 1)
+            X, Y, Z = self.mdl.getMonitoringPointCoordinates(num)
+            name = self.mdl.getMonitoringPointName(num)
+            self.__insertMonitoringPoint(num, name, X, Y, Z)
 
             if self.case['salome']:
                 self.__salomeHandlerAddMonitoringPoint(probe_number + idx, X, Y, Z)