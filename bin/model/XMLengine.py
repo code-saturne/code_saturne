code-saturne/code_saturne

commit d9402b659eca99644d15dd0eaf83dbedd78790c3

Allow script to combine code_saturne and neptune_cfd cases for coupling.

bin/model/XMLengine.py
@@ -108,6 +108,7 @@ class Dico:
         self.data['batch']            = None
         self.data['no_boundary_conditions'] = False
         self.data['salome']           = False
+        self.data['module']           = None
         self.data['package']          = None
         self.data['current_page']     = ""
         self.data['current_index']    = None
@@ -1223,7 +1224,11 @@ class XMLDocument(XMLElement):
 
 class Case(Dico, XMLDocument):
 
-    def __init__(self, package=None, file_name="", studymanager=False):
+    def __init__(self,
+                 package=None,
+                 file_name="",
+                 module=None,
+                 studymanager=False):
         """
         Instantiate a new dico and a new xml doc
         """
@@ -1232,10 +1237,20 @@ class Case(Dico, XMLDocument):
 
         if package:
             self['package'] = package
-            rootNode = '<' + self['package'].code_name +'_GUI study="" case="" version="2.0"/>'
         else:
-            rootNode = '<' + '_GUI study="" case="" version="2.0"/>'
+            from code_saturne import cs_package
+            self['package'] = cs_package.package()
 
+        code_name = ''
+        if module:
+            if module == 'code_saturne':
+                code_name = 'Code_Saturne'
+            elif module == 'neptune_cfd':
+                code_name = 'NEPTUNE_CFD'
+        if package and not code_name:
+            code_name = package.code_name
+
+        rootNode = '<' + code_name +'_GUI study="" case="" version="2.0"/>'
         if studymanager:
             rootNode = '<studymanager/>'
 
@@ -1253,6 +1268,14 @@ class Case(Dico, XMLDocument):
         self.xml_prev = ""
         self.xml_saved = self.toString()
 
+
+    def module_name(self):
+        # Specific module
+        if (self.doc.documentElement.tagName == "NEPTUNE_CFD_GUI"):
+            return 'neptune_cfd'
+
+        # General case
+        return 'code_saturne'
 
     def xmlRootNode(self):
         """
@@ -1449,11 +1472,9 @@ class Case(Dico, XMLDocument):
             file.write('sys.path.insert(0, "' + self['package'].dirs['pythondir'][1] + '")\n')
             file.write('sys.path.insert(0, "' + os.path.join(self['package'].dirs['pythondir'][1], self['package'].name) + '")\n\n')
 
-            if self['package'].code_name == 'Code_Saturne':
-                file.write('from code_saturne import cs_package\n')
+            if self.module_name() == 'code_saturne':
                 file.write('from model.XMLinitialize import XMLinit\n')
-            else:
-                file.write('from neptune_cfd import nc_package\n')
+            elif self.module_name() == 'neptune_cfd':
                 file.write('from model.XMLinitializeNeptune import XMLinit\n')
             file.write('from model.XMLengine import Case\n\n')
 
@@ -1463,10 +1484,7 @@ class Case(Dico, XMLDocument):
                 name = os.path.splitext(self['pythonfile'])[0] + ".xml"
                 file.write("fp = '" + name + "'\n")
 
-            if self['package'].code_name == 'Code_Saturne':
-                file.write("case = Case(package = cs_package.package())\n")
-            else:
-                file.write("case = Case(package = nc_package.package())\n")
+            file.write("case = Case(module=" + self.module_name + ")\n")
             file.write("case['xmlfile'] = fp\n")
             file.write("case.xmlCleanAllBlank(case.xmlRootNode())\n")
             file.write("XMLinit(case).initialize()\n")

bin/cs_case.py
@@ -319,15 +319,21 @@ class case:
 
         name = self.package.code_name
 
-        if len(self.domains) == 1:
-            if self.domains[0].n_procs > 1:
-                msg = ' Parallel ' + name + ' on ' \
-                    + str(self.domains[0].n_procs) + ' processes.\n'
+        for d in self.domains:
+            solver_name = os.path.basename(d.solver_path)
+            if solver_name == 'cs_solver':
+                name = 'code_saturne'
+            elif solver_name == 'nc_solver':
+                name = 'neptune_cfd'
+
+            if len(self.domains) == 1:
+                if d.n_procs > 1:
+                    msg = ' Parallel ' + name + ' on ' \
+                          + str(d.n_procs) + ' processes.\n'
+                else:
+                    msg = ' Single processor ' + name + ' simulation.\n'
+                sys.stdout.write(msg)
             else:
-                msg = ' Single processor ' + name + ' simulation.\n'
-            sys.stdout.write(msg)
-        else:
-            for d in self.domains:
                 msg = ' ' + name + ' domain ' + d.name + ' on ' \
                     + str(d.n_procs) + ' process(es).\n'
                 sys.stdout.write(msg)

bin/cs_case_coupling.py
@@ -139,10 +139,10 @@ def coupling(package,
             use_syrthes = True
             syr_domains.append(dom)
 
-        elif (d.get('solver') == 'Code_Aster' or d.get('solver') == 'Aster'):
+        elif (d.get('solver') in ('Code_Aster', 'code_aster', 'Aster')):
 
             if len(ast_domain) > 0:
-                err_str = 'Only 1 Code_Aster domain is currently handled\n'
+                err_str = 'Only 1 code_aster domain is currently handled\n'
                 raise RunCaseError(err_str)
 
             try:
@@ -151,7 +151,7 @@ def coupling(package,
                                    param = d.get('script'))
 
             except Exception:
-                err_str = 'Cannot create Code_Aster domain.\n'
+                err_str = 'Cannot create code_aster domain.\n'
                 err_str += ' domain = ' + d.get('domain') + '\n'
                 err_str += ' script = ' + d.get('script') + '\n'
                 raise RunCaseError(err_str)

bin/cs_case_domain.py
@@ -357,7 +357,7 @@ class base_domain:
         else:
             debug_args += 'python '
         cs_pkg_dir = self.package.get_dir('pkgpythondir')
-        if self.package.name == 'neptune_cfd':
+        if self.package.name != 'code_saturne':
             cs_pkg_dir = os.path.join(cs_pkg_dir, '../code_saturne')
         dbg_wrapper_path = os.path.join(cs_pkg_dir, 'cs_debug_wrapper.py')
         debug_args += dbg_wrapper_path + ' '
@@ -406,7 +406,8 @@ class domain(base_domain):
 
         # Default executable
 
-        self.solver_path = self.package_compute.get_solver()
+        self.solver_path = os.path.join(self.package_compute.get_dir("pkglibexecdir"),
+                                        "cs_solver" + self.package.config.exeext)
 
         # Preprocessor options
 
@@ -490,16 +491,19 @@ class domain(base_domain):
         """
 
         if param != None:
-            root_str = self.package.code_name + '_GUI'
             version_str = '2.0'
             P = cs_xml_reader.Parser(os.path.join(self.data_dir, param),
-                                     root_str = root_str,
                                      version_str = version_str)
             params = P.getParams()
             for k in list(params.keys()):
                 self.__dict__[k] = params[k]
 
             self.param = param
+
+            if params['xml_root_name'] == 'NEPTUNE_CFD_GUI':
+                solver_dir = self.package_compute.get_dir("pkglibexecdir")
+                solver_name = "nc_solver" + self.package.config.exeext
+                self.solver_path = os.path.join(solver_dir, solver_name)
 
     #---------------------------------------------------------------------------
 
@@ -612,15 +616,17 @@ class domain(base_domain):
             case.xmlCleanAllBlank(case.xmlRootNode())
 
             prepro = (getRunType(case) != 'standard')
-            if case['package'].name == 'code_saturne':
+            module_name = case.module_name()
+            if module_name == 'code_saturne':
                 from code_saturne.model.XMLinitialize import XMLinit
-            else:
-                from code_saturne.model.XMLinitializeNeptune import XMLinitNeptune as XMLinit
-            XMLinit(case).initialize(prepro)
+                XMLinit(case).initialize(prepro)
+            elif module_name == 'neptune_cfd':
+                from code_saturne.model.XMLinitializeNeptune import XMLinitNeptune
+                XMLinitNeptune(case).initialize(prepro)
             case.xmlSaveDocument()
 
             case['case_path'] = self.exec_dir
-            self.mci = mei_to_c_interpreter(case)
+            self.mci = mei_to_c_interpreter(case, module_name=module_name)
 
             if self.mci.has_meg_code():
                 needs_comp = True
@@ -673,7 +679,10 @@ class domain(base_domain):
             log_name = os.path.join(self.exec_dir, 'compile.log')
             log = open(log_name, 'w')
 
+            solver_name = os.path.basename(self.solver_path)
+
             retval = cs_compile.compile_and_link(self.package_compute,
+                                                 solver_name,
                                                  exec_src,
                                                  self.exec_dir,
                                                  self.compile_cflags,
@@ -687,8 +696,8 @@ class domain(base_domain):
             log.close()
 
             if retval == 0:
-                self.solver_path = os.path.join('.',
-                                                self.package_compute.solver)
+                solver_dir = '.'
+                self.solver_path = os.path.join(solver_dir, solver_name)
             else:
                 # In case of error, copy source to results directory now,
                 # as no calculation is possible, then raise exception
@@ -1004,7 +1013,7 @@ class domain(base_domain):
 
         if hasattr(self, 'fsi_aster'):
             if not sys.platform.startswith('linux'):
-                raise RunCaseError(' Coupling with Code_Aster only avalaible on Linux.')
+                raise RunCaseError(' Coupling with code_aster only avalaible on Linux.')
             salome_module = os.path.join(self.package_compute.get_dir('libdir'),
                                          'salome',
                                          'libFSI_SATURNEExelib.so')
@@ -1061,7 +1070,8 @@ class domain(base_domain):
 
         # Determine files from this stage to ignore or to possibly remove
 
-        for f in [self.package.solver, self.package.runsolver]:
+        solver_name = os.path.basename(self.solver_path)
+        for f in [solver_name, self.package.runsolver]:
             if f in dir_files:
                 purge_list.append(f)
         purge_list.extend(fnmatch.filter(dir_files, 'core*'))

bin/cs_compile.py
@@ -552,7 +552,7 @@ class cs_compile(object):
 
     #---------------------------------------------------------------------------
 
-    def compile_and_link(self, srcdir, destdir=None, src_list=None,
+    def compile_and_link(self, base_name, srcdir, destdir=None, src_list=None,
                          opt_cflags=None, opt_cxxflags=None, opt_fcflags=None,
                          opt_libs=None, force_link=False, keep_going=False,
                          stdout=sys.stdout, stderr=sys.stderr):
@@ -563,7 +563,7 @@ class cs_compile(object):
 
         # Determine executable name
 
-        exec_name = self.pkg.solver
+        exec_name = base_name
         if destdir != None:
             exec_name = os.path.join(destdir, exec_name)
 
@@ -609,7 +609,7 @@ class cs_compile(object):
 
 #---------------------------------------------------------------------------
 
-def compile_and_link(pkg, srcdir, destdir=None,
+def compile_and_link(pkg, base_name, srcdir, destdir=None,
                      opt_cflags=None, opt_cxxflags=None, opt_fcflags=None,
                      opt_libs=None, force_link=False, keep_going=False,
                      stdout=sys.stdout, stderr=sys.stderr):
@@ -618,7 +618,8 @@ def compile_and_link(pkg, srcdir, destdir=None,
     """
     c = cs_compile(pkg)
 
-    retcode = c.compile_and_link(srcdir,
+    retcode = c.compile_and_link(base_name,
+                                 srcdir,
                                  destdir=destdir,
                                  opt_cflags=opt_cflags,
                                  opt_cxxflags=opt_cxxflags,
@@ -658,6 +659,7 @@ def main(argv, pkg):
         dest_dir = None
 
     retcode = compile_and_link(pkg,
+                               pkg.solver,
                                src_dir,
                                destdir=dest_dir,
                                opt_cflags=cflags,

bin/cs_mei_to_c.py
@@ -1089,10 +1089,13 @@ class mei_to_c_interpreter:
 
     #---------------------------------------------------------------------------
 
-    def __init__(self, case, create_functions=True):
+    def __init__(self, case, create_functions=True, module_name=None):
 
         self.case = case
-        self.pkg_name = case['package'].name
+        if module_name:
+            self.module_name = module_name
+        else:
+            self.module_name = case.module_name()
 
         self.data_path = os.path.join(case['case_path'], 'DATA')
         self.tmp_path = os.path.join(self.data_path, 'tmp')
@@ -1235,7 +1238,7 @@ class mei_to_c_interpreter:
                         usr_defs += ntabs*tab + l
                         known_symbols.append(sn)
 
-                    elif sn in _pkg_fluid_prop_dict[self.pkg_name].keys():
+                    elif sn in _pkg_fluid_prop_dict[self.module_name].keys():
                         if len(name.split("_")) > 1:
                             try:
                                 phase_id = int(name.split('_')[-1])-1
@@ -1243,9 +1246,9 @@ class mei_to_c_interpreter:
                                 phase_id = -1
                         else:
                             phase_id = -1
-                        gs = _pkg_glob_struct[self.pkg_name].replace('PHASE_ID',
+                        gs = _pkg_glob_struct[self.module_name].replace('PHASE_ID',
                                                                      str(phase_id))
-                        pn = _pkg_fluid_prop_dict[self.pkg_name][sn]
+                        pn = _pkg_fluid_prop_dict[self.module_name][sn]
                         ms = 'const cs_real_t %s = %s->%s;\n' %(sn, gs, pn)
                         usr_defs += ntabs*tab + ms
                         known_symbols.append(sn)
@@ -1688,7 +1691,7 @@ class mei_to_c_interpreter:
                         usr_defs += ntabs*tab + l
                         known_symbols.append(sn)
 
-                    elif sn in _pkg_fluid_prop_dict[self.pkg_name].keys():
+                    elif sn in _pkg_fluid_prop_dict[self.module_name].keys():
                         if len(name.split("_")) > 1:
                             try:
                                 phase_id = int(name.split('_')[-1])-1
@@ -1697,9 +1700,9 @@ class mei_to_c_interpreter:
                         else:
                             phase_id = -1
 
-                        gs = _pkg_glob_struct[self.pkg_name].replace('PHASE_ID',
+                        gs = _pkg_glob_struct[self.module_name].replace('PHASE_ID',
                                                                      str(phase_id))
-                        pn = _pkg_fluid_prop_dict[self.pkg_name][sn]
+                        pn = _pkg_fluid_prop_dict[self.module_name][sn]
                         ms = 'const cs_real_t %s = %s->%s;\n' %(sn, gs, pn)
                         usr_defs += ntabs*tab + ms
                         known_symbols.append(sn)
@@ -1907,7 +1910,7 @@ class mei_to_c_interpreter:
 
         from code_saturne.model.LocalizationModel import LocalizationModel
         from code_saturne.model.GroundwaterLawModel import GroundwaterLawModel
-        if self.pkg_name == 'code_saturne':
+        if self.module_name == 'code_saturne':
             from code_saturne.model.FluidCharacteristicsModel \
                 import FluidCharacteristicsModel
 
@@ -1954,7 +1957,7 @@ class mei_to_c_interpreter:
             # Ground water model enabled ?
             gwm = not (GroundwaterModel(self.case).getGroundwaterModel() == 'off')
 
-        elif self.pkg_name == 'neptune_cfd':
+        elif self.module_name == 'neptune_cfd':
             from code_saturne.model.ThermodynamicsModel import ThermodynamicsModel
             from code_saturne.model.MainFieldsModel import MainFieldsModel
 
@@ -2044,7 +2047,7 @@ class mei_to_c_interpreter:
 
         from code_saturne.model.NotebookModel import NotebookModel
 
-        if self.pkg_name == 'code_saturne':
+        if self.module_name == 'code_saturne':
             from code_saturne.model.LocalizationModel import LocalizationModel
             from code_saturne.model.Boundary import Boundary
             from code_saturne.model.TurbulenceModel import TurbulenceModel
@@ -2313,7 +2316,7 @@ class mei_to_c_interpreter:
 
     def generate_source_terms_code(self):
 
-        if self.pkg_name == 'code_saturne':
+        if self.module_name == 'code_saturne':
             from code_saturne.model.LocalizationModel import LocalizationModel
             from code_saturne.model.SourceTermsModel import SourceTermsModel
             from code_saturne.model.GroundwaterModel import GroundwaterModel
@@ -2397,7 +2400,7 @@ class mei_to_c_interpreter:
 
     def generate_initialize_code(self):
 
-        if self.pkg_name == 'code_saturne':
+        if self.module_name == 'code_saturne':
             from code_saturne.model.LocalizationModel import LocalizationModel
             from code_saturne.model.InitializationModel import InitializationModel
             from code_saturne.model.CompressibleModel import CompressibleModel
@@ -2559,7 +2562,7 @@ class mei_to_c_interpreter:
 
     def generate_immersed_boundaries_code(self):
 
-        if self.pkg_name == 'neptune_cfd':
+        if self.module_name == 'neptune_cfd':
             from code_saturne.model.ImmersedBoundariesModel import ImmersedBoundariesModel
             ibm = ImmersedBoundariesModel(self.case)
 
@@ -2590,6 +2593,7 @@ class mei_to_c_interpreter:
         out = open('comp.out', 'w')
         err = open('comp.err', 'w')
         compilation_test = cs_compile.compile_and_link(self.case['package'],
+                                                       self.case['package'].solver,
                                                        self.tmp_path,
                                                        opt_cflags='-w',
                                                        stdout=out,
@@ -2693,7 +2697,7 @@ class mei_to_c_interpreter:
         code_to_write = ''
         if len(self.funcs[func_type].keys()) > 0:
             code_to_write = _file_header
-#            if self.pkg_name != "code_saturne":
+#            if self.module_name != "code_saturne":
 #                code_to_write += _file_header2
             code_to_write += _file_header3
             code_to_write += _function_header[func_type]

bin/cs_xml_reader.py
@@ -107,7 +107,6 @@ class Parser:
 
     def __init__(self,
                  fileName,
-                 root_str = None,
                  version_str = None):
 
         self.dict = {}
@@ -129,13 +128,17 @@ class Parser:
 
         self.root = self.doc.documentElement
 
-        if root_str != None:
-            if root_str != self.root.tagName:
-                errStr = '%s\n' \
-                    +'type: "%s", but "%s" expected.'
-                raise XMLError(errStr % (fileName,
-                                         self.root.tagName,
-                                         root_str))
+        root_names = ('Code_Saturne_GUI',
+                      'NEPTUNE_CFD_GUI')
+
+        if self.root.tagName not in root_names:
+            errStr = '%s\n' \
+                     +'type: "%s", but one of "%s" expected.'
+            raise XMLError(errStr % (fileName,
+                                     self.root.tagName,
+                                     str(root_names)))
+
+        self.dict['xml_root_name'] = self.root.tagName
 
         if version_str != None:
             version = self.root.getAttribute('version')

bin/model/BodyForcesModel.py
@@ -58,10 +58,10 @@ class BodyForcesModel(Model):
         """
         self.case = case
 
-        if self.case.xmlRootNode().tagName == "Code_Saturne_GUI":
+        if self.case.module_name() == "code_saturne":
             self.node_pp      = self.case.xmlGetNode('physical_properties')
             self.node_gravity = self.node_pp.xmlInitNode('gravity')
-        elif self.case.xmlRootNode().tagName == "NEPTUNE_CFD_GUI":
+        elif self.case.module_name() == "neptune_cfd":
             self.node_pp = self.case.xmlGetNode('closure_modeling')
             self.node_gravity = self.node_pp.xmlInitNode('gravity')
 

commit 7b5f0e9b6a76ff84d3a0a27349c7d0b0ee3fba46

GUI: fix Python dump, broken by commit d9402b65 (October 23 2018).

bin/model/XMLengine.py
@@ -1479,7 +1479,7 @@ class Case(Dico, XMLDocument):
         # now write
         try:
             file = open(self['pythonfile'], 'w')
-            file.write("#automatic python script\n")
+            file.write("# automaticaly generated Python script\n\n")
             file.write('import os, sys\n')
             file.write('sys.path.insert(0, "' + self['package'].dirs['pythondir'][1] + '")\n')
             file.write('sys.path.insert(0, "' + os.path.join(self['package'].dirs['pythondir'][1], self['package'].name) + '")\n\n')
@@ -1496,7 +1496,7 @@ class Case(Dico, XMLDocument):
                 name = os.path.splitext(self['pythonfile'])[0] + ".xml"
                 file.write("fp = '" + name + "'\n")
 
-            file.write("case = Case(module=" + self.module_name + ")\n")
+            file.write("case = Case(module=" + self.module_name() + ")\n")
             file.write("case['xmlfile'] = fp\n")
             file.write("case.xmlCleanAllBlank(case.xmlRootNode())\n")
             file.write("XMLinit(case).initialize()\n")