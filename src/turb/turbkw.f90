code-saturne/code_saturne

commit e841dfbb203bc5a31dfd0dbb1344001ec146d855

Add Boussinesq approximation (use it with idilat = 0).
Note that "thermal_expansion" and the thermal scalar fields must be defined.

src/turb/turbkw.f90
@@ -161,6 +161,8 @@ double precision, dimension(:), pointer :: ddes_fd_coeff
 double precision, dimension(:), pointer :: w_dist
 double precision, dimension(:), pointer :: cpro_k_clipped
 double precision, dimension(:), pointer :: cpro_w_clipped
+double precision, dimension(:), pointer :: cpro_beta
+double precision, allocatable, dimension(:) :: grad_dot_g
 
 type(var_cal_opt) :: vcopt_w, vcopt_k
 
@@ -407,35 +409,7 @@ if (igrake.eq.1) then
 
   ! Allocate a temporary array for the gradient calculation
   allocate(grad(3,ncelet))
-
-  ! --- Buoyant term:     G = Beta*g*GRAD(T)/PrT/rho
-  !     Here is computed: G =-g*GRAD(rho)/PrT/rho
-
-  iccocg = 1
-  inc = 1
-
-  nswrgp = vcopt_k%nswrgr
-  epsrgp = vcopt_k%epsrgr
-  imligp = vcopt_k%imligr
-  iwarnp = vcopt_k%iwarni
-  climgp = vcopt_k%climgr
-  extrap = vcopt_k%extrag
-
-  ! BCs on rho: Dirichlet ROMB
-  !  NB: viscb is used as COEFB
-
-  do ifac = 1, nfabor
-    viscb(ifac) = 0.d0
-  enddo
-
-  f_id = -1
-
-  call gradient_s                                                 &
- ( f_id   , imrgra , inc    , iccocg , nswrgp , imligp ,          &
-   iwarnp , epsrgp , climgp , extrap ,                            &
-   cromo  , bromo  , viscb  ,                                     &
-   grad   )
-
+  allocate(grad_dot_g(ncelet))
 
   ! Buoyancy production
   !   prodk=min(P,c1*eps)+G
@@ -447,12 +421,67 @@ if (igrake.eq.1) then
     prdtur = 1.d0
   endif
 
+
+  ! --- Buoyant term:     G = Beta*g*GRAD(T)/PrT/rho
+  !     Here is computed: G =-g*GRAD(rho)/PrT/rho
+
+  ! Boussinesq approximation, only for the thermal scalar for the moment
+  if (idilat.eq.0)  then
+
+    iccocg = 1
+    inc = 1
+
+    call field_gradient_scalar(ivarfl(isca(iscalt)), 1, imrgra, inc, iccocg, &
+                               grad)
+
+    !FIXME make it dependant on the scalar and use is_buoyant field
+    call field_get_val_s(ibeta, cpro_beta)
+
+    ! - Beta grad(T) . g / Pr_T
+    do iel = 1, ncel
+      grad_dot_g(iel) = - cpro_beta(iel) &
+        * (grad(1,iel)*gx + grad(2,iel)*gy + grad(3,iel)*gz) / (prdtur)
+    enddo
+
+  else
+
+    iccocg = 1
+    inc = 1
+
+    nswrgp = vcopt_k%nswrgr
+    epsrgp = vcopt_k%epsrgr
+    imligp = vcopt_k%imligr
+    iwarnp = vcopt_k%iwarni
+    climgp = vcopt_k%climgr
+    extrap = vcopt_k%extrag
+
+    ! BCs on rho: Dirichlet ROMB
+    !  NB: viscb is used as COEFB
+
+    do ifac = 1, nfabor
+      viscb(ifac) = 0.d0
+    enddo
+
+    f_id = -1
+
+    call gradient_s                                                 &
+   ( f_id   , imrgra , inc    , iccocg , nswrgp , imligp ,          &
+     iwarnp , epsrgp , climgp , extrap ,                            &
+     cromo  , bromo  , viscb  ,                                     &
+     grad   )
+
+    do iel = 1, ncel
+      grad_dot_g(iel) = (grad(1,iel)*gx + grad(2,iel)*gy + grad(3,iel)*gz) &
+        / (rho*prdtur)
+    enddo
+
+  endif
+
   do iel = 1, ncel
     rho = cromo(iel)
     visct = cpro_pcvto(iel)
 
-    w2(iel) = -(grad(1,iel)*gx + grad(2,iel)*gy + grad(3,iel)*gz) / &
-               (rho*prdtur)
+    w2(iel) = - grad_dot_g(iel)
 
     prodw(iel) = prodw(iel)+visct*max(w2(iel),zero)
     prodk(iel) = prodk(iel)+visct*w2(iel)
@@ -464,6 +493,7 @@ if (igrake.eq.1) then
 
   ! Free memory
   deallocate(grad)
+  deallocate(grad_dot_g)
 
 endif
 

src/base/cs_parameters_check.c
@@ -1652,6 +1652,21 @@ cs_parameters_check(void)
                                 cs_glob_stokes_model->ipucou,
                                 0, 2);
 
+  /* Dilatable algorithm: 0 to 5 */
+  cs_parameters_is_in_range_int(CS_ABORT_DELAYED,
+                                _("while reading input data"),
+                                "cs_glob_stokes_model->idilat",
+                                cs_glob_stokes_model->idilat,
+                                0, 6);
+
+  if (cs_glob_stokes_model->idilat == 0) {
+    cs_parameters_is_greater_int(CS_ABORT_DELAYED,
+                                  _("while reading input data"),
+                                  "Boussinesq approximation requires the thermal_expansion field",
+                                  cs_field_by_name("thermal_expansion"),
+                                  0);
+  }
+
   cs_field_get_key_struct(CS_F_(vel), key_cal_opt_id, &var_cal_opt);
   /* steady or variable time step time algorithm not compatible with theta
      scheme with theta different from 1 for the velocity */
@@ -1796,7 +1811,7 @@ cs_parameters_check(void)
                                      _(f_desc),
                                      "var_cal_opt.nswrsm",
                                      var_cal_opt.nswrsm,
-                                     19);
+                                     20);
 
         BFT_FREE(f_desc);
 

src/base/cs_stokes_model.c
@@ -129,6 +129,8 @@ BEGIN_C_DECLS
         \f$>\f$ 0 and the calculation is a restart.
   \var  cs_stokes_model_t::idilat
         algorithm to take into account the density variation in time
+        - 0: Boussinesq approximation (rho constant expect in the buoyant
+             term where \f$\Delta \rho \vect{g} = - \rho \beta \Delta T \vect{g} \f$
         - 1: dilatable steady algorithm (default)
         - 2: dilatable unsteady algorithm
         - 3: low-Mach algorithm
@@ -392,7 +394,8 @@ cs_stokes_model_log_setup(void)
      _("\n"
        "Stokes model\n"
        "------------\n\n"
-       "    idilat:      %14d (1: without unsteady term\n"
+       "    idilat:      %14d (0: Boussines approximation\n"
+       "                                 1: without unsteady term\n"
        "                                    in the continuity equation\n"
        "                                 2: with unsteady term in\n"
        "                                    the continuity equation\n"

src/base/predvv.f90
@@ -244,6 +244,7 @@ double precision, dimension(:), pointer :: cpro_wgrec_s
 double precision, dimension(:,:), pointer :: cpro_wgrec_v
 double precision, dimension(:), pointer :: imasfl, bmasfl
 double precision, dimension(:), pointer :: imasfl_prev, bmasfl_prev
+double precision, dimension(:), pointer :: cpro_beta, cvar_t
 
 type(var_cal_opt) :: vcopt_p, vcopt_u, vcopt
 
@@ -595,6 +596,21 @@ else if (ippmod(icompf).ge.0) then
     trav(2,iel) = trav(2,iel)+(rom*gy - cpro_gradp(2,iel)) * cell_f_vol(iel)
     trav(3,iel) = trav(3,iel)+(rom*gz - cpro_gradp(3,iel)) * cell_f_vol(iel)
   enddo
+  ! Boussinesq approximation
+else if (idilat.eq.0) then
+
+  !FIXME make it dependant on the scalar and use is_buoyant field
+  call field_get_val_s(ibeta, cpro_beta)
+  call field_get_val_s(ivarfl(isca(iscalt)), cvar_t)
+
+  ! Delta rho = - rho_0 beta Delta T
+  do iel = 1, ncel
+    drom = - ro0 * cpro_beta(iel) * (cvar_t(iel) - t0)
+    trav(1,iel) = trav(1,iel) + (drom * gx - cpro_gradp(1,iel)) * cell_f_vol(iel)
+    trav(2,iel) = trav(2,iel) + (drom * gy - cpro_gradp(2,iel)) * cell_f_vol(iel)
+    trav(3,iel) = trav(3,iel) + (drom * gz - cpro_gradp(3,iel)) * cell_f_vol(iel)
+  enddo
+
 else
   do iel = 1, ncel
     drom = (crom(iel)-ro0)
@@ -1205,15 +1221,33 @@ if (iappel.eq.1.and.iphydr.eq.1) then
   !     NB: frcxt was used in typecl, and will be updated
   !         at the end of navstv
 
-  do iel = 1, ncel
+  ! External force variation between time step n and n+1
+  ! (used in the correction step)
+  !-----------------------------
 
-    ! External force variation between time step n and n+1
-    ! (used in the correction step)
-    drom = (crom(iel)-ro0)
-    dfrcxt(1, iel) = drom*gx - frcxt(1, iel)
-    dfrcxt(2, iel) = drom*gy - frcxt(2, iel)
-    dfrcxt(3, iel) = drom*gz - frcxt(3, iel)
-  enddo
+  ! Boussinesq approximation
+  if (idilat.eq.0) then
+
+    !FIXME make it dependant on the scalar and use is_buoyant field
+    call field_get_val_s(ibeta, cpro_beta)
+    call field_get_val_s(ivarfl(isca(iscalt)), cvar_t)
+
+    ! Delta rho = - rho_0 beta Delta T
+    do iel = 1, ncel
+      drom = - ro0 * cpro_beta(iel) * (cvar_t(iel) - t0)
+      dfrcxt(1, iel) = drom*gx - frcxt(1, iel)
+      dfrcxt(2, iel) = drom*gy - frcxt(2, iel)
+      dfrcxt(3, iel) = drom*gz - frcxt(3, iel)
+    enddo
+
+  else
+    do iel = 1, ncel
+      drom = (crom(iel)-ro0)
+      dfrcxt(1, iel) = drom*gx - frcxt(1, iel)
+      dfrcxt(2, iel) = drom*gy - frcxt(2, iel)
+      dfrcxt(3, iel) = drom*gz - frcxt(3, iel)
+    enddo
+  endif
 
   ! Add head losses
   if (ncepdp.gt.0) then

src/turb/turbke.f90
@@ -157,6 +157,7 @@ double precision, allocatable, dimension(:) :: coefa_sqs, coefb_sqs
 double precision, allocatable, dimension(:,:) :: grad_sqk, grad_sqs
 double precision, allocatable, dimension(:,:,:) :: gradv
 double precision, allocatable, dimension(:) :: dpvar
+double precision, allocatable, dimension(:) :: grad_dot_g
 
 double precision, dimension(:), pointer :: imasfl, bmasfl
 double precision, dimension(:), pointer :: brom, crom, bromo, cromo
@@ -171,6 +172,7 @@ double precision, dimension(:), pointer :: viscl, cvisct
 double precision, dimension(:), pointer :: c_st_k_p, c_st_eps_p
 double precision, dimension(:,:), pointer :: vel
 double precision, dimension(:), pointer :: cvar_al
+double precision, dimension(:), pointer :: cpro_beta
 
 type(var_cal_opt) :: vcopt_k, vcopt_e
 
@@ -539,38 +541,67 @@ else if (igrake.eq.1) then
 
   ! Allocate a temporary for the gradient calculation
   allocate(grad(3,ncelet))
+  allocate(grad_dot_g(ncelet))
 
-  iccocg = 1
-  inc = 1
-  nswrgp = vcopt_k%nswrgr
-  epsrgp = vcopt_k%epsrgr
-  imligp = vcopt_k%imligr
-  iwarnp = vcopt_k%iwarni
-  climgp = vcopt_k%climgr
-  extrap = vcopt_k%extrag
-
-  ! Dirichlet boundary condition on the gradient of rho
-  do ifac = 1, nfabor
-    viscb(ifac) = 0.d0
-  enddo
-
-  f_id0 = -1
-
-  call gradient_s                                                 &
- ( f_id0  , imrgra , inc    , iccocg , nswrgp , imligp ,          &
-   iwarnp , epsrgp , climgp , extrap ,                            &
-   cromo  , bromo  , viscb  ,                                     &
-   grad   )
-
-  ! Production term due to buoyancy
-  !   smbrk = P+G
-  !   smbre = P+(1-ce3)*G
   if (iscalt.gt.0.and.nscal.ge.iscalt) then
     call field_get_key_double(ivarfl(isca(iscalt)), ksigmas, turb_schmidt)
     prdtur = turb_schmidt
   else
     prdtur = 1.d0
   endif
+
+  ! Boussinesq approximation, only for the thermal scalar for the moment
+  if (idilat.eq.0)  then
+
+    iccocg = 1
+    inc = 1
+
+    call field_gradient_scalar(ivarfl(isca(iscalt)), 1, imrgra, inc, iccocg, &
+                               grad)
+
+    !FIXME make it dependant on the scalar and use is_buoyant field
+    call field_get_val_s(ibeta, cpro_beta)
+
+    ! - Beta grad(T) . g / Pr_T
+    do iel = 1, ncel
+      grad_dot_g(iel) = - cpro_beta(iel) &
+        * (grad(1,iel)*gx + grad(2,iel)*gy + grad(3,iel)*gz) / (prdtur)
+    enddo
+
+  else
+    iccocg = 1
+    inc = 1
+    nswrgp = vcopt_k%nswrgr
+    epsrgp = vcopt_k%epsrgr
+    imligp = vcopt_k%imligr
+    iwarnp = vcopt_k%iwarni
+    climgp = vcopt_k%climgr
+    extrap = vcopt_k%extrag
+
+    ! Dirichlet boundary condition on the gradient of rho
+    do ifac = 1, nfabor
+      viscb(ifac) = 0.d0
+    enddo
+
+    f_id0 = -1
+
+    call gradient_s                                                 &
+   ( f_id0  , imrgra , inc    , iccocg , nswrgp , imligp ,          &
+     iwarnp , epsrgp , climgp , extrap ,                            &
+     cromo  , bromo  , viscb  ,                                     &
+     grad   )
+
+
+    do iel = 1, ncel
+      grad_dot_g(iel) = (grad(1,iel)*gx + grad(2,iel)*gy + grad(3,iel)*gz) &
+        / (rho*prdtur)
+    enddo
+
+  endif
+
+  ! Production term due to buoyancy
+  !   smbrk = P+G
+  !   smbre = P+(1-ce3)*G
 
   ! smbr* store mu_TxS**2
   do iel = 1, ncel
@@ -580,8 +611,7 @@ else if (igrake.eq.1) then
     xk   = cvara_k(iel)
     ttke = xk / xeps
 
-    gravke = -(grad(1,iel)*gx + grad(2,iel)*gy + grad(3,iel)*gz) &
-           / (rho*prdtur)
+    gravke = - grad_dot_g(iel)
 
     ! Implicit Buoyant terms when negativ
     tinstk(iel) = tinstk(iel) + max(-rho*cell_f_vol(iel)*cmu*ttke*gravke, 0.d0)
@@ -593,6 +623,7 @@ else if (igrake.eq.1) then
 
   ! Free memory
   deallocate(grad)
+  deallocate(grad_dot_g)
 
 endif
 

src/turb/turrij.f90
@@ -141,6 +141,7 @@ double precision, dimension(:), pointer :: cvar_ep, cvar_al
 double precision, dimension(:,:), pointer :: cvara_rij, cvar_rij, vel
 double precision, dimension(:,:), pointer :: lagr_st_rij
 double precision, dimension(:,:), pointer :: cpro_produc
+double precision, dimension(:), pointer :: cpro_beta
 
 type(var_cal_opt) :: vcopt
 
@@ -463,7 +464,6 @@ if (igrari.eq.1 .and. ippmod(iatmos).ge.1) then
 
   call field_get_val_prev_s(ivarfl(isca(iscalt)), cvara_scalt)
 
-  iprev = 1
   inc = 1
   iccocg = 1
 
@@ -484,43 +484,67 @@ else if (igrari.eq.1) then
   ! Allocate a temporary array for the gradient calculation
   allocate(gradro(3,ncelet))
 
-  ! Boundary conditions: Dirichlet romb
-  ! We use viscb to store the relative coefficient of rom
-  ! We impose in Dirichlet (coefa) the value romb
+  ! Boussinesq approximation, only for the thermal scalar for the moment
+  if (idilat.eq.0)  then
 
-  do ifac = 1, nfabor
-    viscb(ifac) = 0.d0
-  enddo
+    iccocg = 1
+    inc = 1
 
-  ! The choice below has the advantage to be simple
-  call field_get_key_struct_var_cal_opt(ivarfl(irij), vcopt)
+    ! Use the current value...
+    call field_gradient_scalar(ivarfl(isca(iscalt)), 0, imrgra, inc, iccocg, &
+                               gradro)
 
-  nswrgp = vcopt%nswrgr
-  imligp = vcopt%imligr
-  iwarnp = vcopt%iwarni
-  epsrgp = vcopt%epsrgr
-  climgp = vcopt%climgr
-  extrap = vcopt%extrag
+    !FIXME make it dependant on the scalar and use is_buoyant field
+    call field_get_val_s(ibeta, cpro_beta)
 
-  f_id0 = -1
-  iccocg = 1
+    ! gradro stores: - beta grad(T)
+    ! grad(rho) and grad(T) have opposite signs
+    do iel = 1, ncel
+      gradro(1, iel) = - ro0 * cpro_beta(iel) * gradro(1, iel)
+      gradro(2, iel) = - ro0 * cpro_beta(iel) * gradro(2, iel)
+      gradro(3, iel) = - ro0 * cpro_beta(iel) * gradro(3, iel)
+    enddo
 
-  call field_get_key_int(icrom, key_t_ext_id, iroext)
-  ! If we extrapolate the source terms and rho, we use cpdt rho^n
-  if(isto2t.gt.0.and.iroext.gt.0) then
-    call field_get_val_prev_s(icrom, cromo)
-    call field_get_val_prev_s(ibrom, bromo)
   else
-    call field_get_val_s(icrom, cromo)
-    call field_get_val_s(ibrom, bromo)
+
+    ! Boundary conditions: Dirichlet romb
+    ! We use viscb to store the relative coefficient of rom
+    ! We impose in Dirichlet (coefa) the value romb
+
+    do ifac = 1, nfabor
+      viscb(ifac) = 0.d0
+    enddo
+
+    ! The choice below has the advantage to be simple
+    call field_get_key_struct_var_cal_opt(ivarfl(irij), vcopt)
+
+    nswrgp = vcopt%nswrgr
+    imligp = vcopt%imligr
+    iwarnp = vcopt%iwarni
+    epsrgp = vcopt%epsrgr
+    climgp = vcopt%climgr
+    extrap = vcopt%extrag
+
+    f_id0 = -1
+    iccocg = 1
+
+    call field_get_key_int(icrom, key_t_ext_id, iroext)
+    ! If we extrapolate the source terms and rho, we use cpdt rho^n
+    if(isto2t.gt.0.and.iroext.gt.0) then
+      call field_get_val_prev_s(icrom, cromo)
+      call field_get_val_prev_s(ibrom, bromo)
+    else
+      call field_get_val_s(icrom, cromo)
+      call field_get_val_s(ibrom, bromo)
+    endif
+
+    call gradient_s                                                 &
+      ( f_id0  , imrgra , inc    , iccocg , nswrgp , imligp ,       &
+      iwarnp , epsrgp , climgp , extrap ,                           &
+      cromo  , bromo  , viscb           ,                           &
+      gradro )
+
   endif
-
-  call gradient_s                                                 &
- ( f_id0  , imrgra , inc    , iccocg , nswrgp , imligp ,          &
-   iwarnp , epsrgp , climgp , extrap ,                            &
-   cromo  , bromo  , viscb           ,                            &
-   gradro )
-
 endif
 
 !===============================================================================

src/user_examples/cs_user_parameters-base.c
@@ -283,6 +283,7 @@ cs_user_parameters(cs_domain_t *domain)
    *                   - 0: false
    *  idilat: algorithm to take into account the density
    *          variation in time
+   *                   - 0: Boussinesq approximation
    *                   - 1: dilatable steady algorithm (default)
    *                   - 2: dilatable unsteady algorithm
    *                   - 3: low-Mach algorithm

commit 87e9ae7c20ce63effcc1efda04526c660ab05637

Fix bug in k-omega turbulence model introduced in commit e841dfbb (2019-01-04).

src/turb/turbkw.f90
@@ -570,6 +570,7 @@ if (igrake.eq.1) then
      grad   )
 
     do iel = 1, ncel
+      rho = cromo(iel)
       grad_dot_g(iel) = (grad(1,iel)*gx + grad(2,iel)*gy + grad(3,iel)*gz) &
         / (rho*prdtur)
     enddo
@@ -577,7 +578,6 @@ if (igrake.eq.1) then
   endif
 
   do iel = 1, ncel
-    rho = cromo(iel)
     visct = cpro_pcvto(iel)
 
     w2(iel) = - grad_dot_g(iel)