code-saturne/code_saturne

commit 3e772f93136a82f02f7b1f6b0aea936012765b37

Major evolution: Addition of file extensions to chcekpoint and mesh files
  * .csm extension is now used for internal mesh format files.
    mesh_input or mesh_output files are still accepted
  * .csc extension is now used for checkpoint files
    In case of non existing .csc files in the checkpoint directory, the
    code will search for the same files without the extension, to allow
    for older versions checkpoints to be used.
  * A generic "mesh_input_folder" is now used in the RUN direcotry to
    store the different mesh_input/output files.

bin/cs_case_domain.py
@@ -401,7 +401,6 @@ class domain(base_domain):
 
         self.restart_input = None
         self.restart_mesh_input = None
-        self.mesh_input = None
         self.partition_input = None
 
         # Default executable
@@ -783,7 +782,7 @@ class domain(base_domain):
 
         restart_input_mesh = None
         if self.restart_input:
-            restart_input_mesh = os.path.join(self.exec_dir, 'restart', 'mesh_input')
+            restart_input_mesh = os.path.join(self.exec_dir, 'restart', 'mesh_input.csm')
             if not os.path.exists(restart_input_mesh):
                 restart_input_mesh = None
 
@@ -793,7 +792,14 @@ class domain(base_domain):
                 mesh_input = os.path.expanduser(self.mesh_input)
                 if not os.path.isabs(mesh_input):
                     mesh_input = os.path.join(self.case_dir, mesh_input)
-                link_path = os.path.join(self.exec_dir, 'mesh_input')
+
+                # Differentiate between a folder and file, since we now
+                # have a file extension
+                if os.path.isdir(mesh_input):
+                    link_path = os.path.join(self.exec_dir, 'mesh_input_folder')
+                else:
+                    link_path = os.path.join(self.exec_dir, 'mesh_input.csm')
+
                 self.purge_result(link_path) # in case of previous run here
                 self.symlink(mesh_input, link_path)
         else:
@@ -889,7 +895,7 @@ class domain(base_domain):
 
         if len(self.meshes) > 1:
             mesh_id = 0
-            destdir = 'mesh_input'
+            destdir = 'mesh_input_folder'
             make_clean_dir(destdir)
 
         # Set environment
@@ -946,11 +952,11 @@ class domain(base_domain):
                 mesh_id += 1
                 cmd = cmd + ['--log', 'preprocessor_%02d.log' % (mesh_id)]
                 cmd = cmd + ['--out', os.path.join('mesh_input',
-                                                   'mesh_%02d' % (mesh_id))]
+                                                   'mesh_%02d.csm' % (mesh_id))]
                 cmd = cmd + ['--case', 'preprocessor_%02d' % (mesh_id)]
             else:
                 cmd = cmd + ['--log']
-                cmd = cmd + ['--out', 'mesh_input']
+                cmd = cmd + ['--out', 'mesh_input.csm']
 
             cmd.append(mesh_path)
 
@@ -1048,10 +1054,10 @@ class domain(base_domain):
 
         purge_list = []
 
-        f = 'mesh_input'
         if not self.mesh_input and self.exec_solver:
-            if f in dir_files:
-                purge_list.append(f)
+            for f in ['mesh_input', 'mesh_input.csm', 'mesh_input_folder']:
+                if f in dir_files:
+                    purge_list.append(f)
 
         for f in ['restart', 'partition_input']:
             if f in dir_files:
@@ -1585,7 +1591,14 @@ class cathare_domain(domain):
             mesh_input = os.path.expanduser(self.mesh_input)
             if not os.path.isabs(mesh_input):
                 mesh_input = os.path.join(self.case_dir, mesh_input)
-            link_path = os.path.join(self.exec_dir, 'mesh_input')
+
+            # Differentiate between a folder and file, since we now
+            # have a file extension
+            if os.path.isdir(mesh_input):
+                link_path = os.path.join(self.exec_dir, 'mesh_input_folder')
+            else:
+                link_path = os.path.join(self.exec_dir, 'mesh_input.csm')
+
             self.purge_result(link_path) # in case of previous run here
             self.symlink(mesh_input, link_path)
 

gui/Pages/SolutionDomainView.py
@@ -619,7 +619,7 @@ class MeshInputDialog(QFileDialog):
         caption =  self.tr("Select input mesh file or directory")
         self.setWindowTitle(caption)
 
-        self.name_filter = str(self.tr("Imported or preprocessed meshes (mesh_input mesh_output)"))
+        self.name_filter = str(self.tr("Imported or preprocessed meshes (mesh_input mesh_output mesh_input_folder *.csm)"))
         self.setNameFilter(self.name_filter)
 
         self.select_label = str(self.tr("Select"))

preprocessor/appli/ecs_cmd.c
@@ -386,7 +386,7 @@ _print_help(void)
   _fct_prt("-o", _("<file>"),
            _(": output file name"));
   sprintf(opt_str, _("  (default file: \"%s\")"),
-          "mesh_input");
+          "mesh_input.csm");
   _fct_prt("", "", opt_str);
 
   _fct_prt("--out", _("<file>"), _(": same"));
@@ -1004,7 +1004,7 @@ ecs_cmd__lit_arg(int    argc,
       if (argc - 1 > iarg && strncmp(argv[iarg + 1], "-", 1))
         outfic = argv[++iarg];
       else
-        outfic = "mesh_input";
+        outfic = "mesh_input.csm";
 
       ECS_MALLOC(cmd->nom_out, strlen(outfic) + 1, char);
 
@@ -1135,9 +1135,9 @@ ecs_cmd__lit_arg(int    argc,
 
   if (cmd->nom_out == NULL && bool_no_write == false) {
     ECS_MALLOC(cmd->nom_out,
-               strlen("mesh_input") + 1,
+               strlen("mesh_input.csm") + 1,
                char);
-    strcpy(cmd->nom_out, "mesh_input");
+    strcpy(cmd->nom_out, "mesh_input.csm");
   }
 
   /*-----------------------------------------*/

src/base/caltri.f90
@@ -639,7 +639,7 @@ if (isuite.eq.1) then
   endif
 
   if (isuisy.eq.1) then
-    call lecsyn('les_inflow'//c_null_char)
+    call lecsyn('les_inflow.csc'//c_null_char)
   endif
 
   ! TODO
@@ -1048,7 +1048,7 @@ if (iisuit.eq.1) then
   endif
 
   if (nent.gt.0) then
-    call ecrsyn('les_inflow'//c_null_char)
+    call ecrsyn('les_inflow.csc'//c_null_char)
   endif
 
   if (iilagr.gt.0) then

src/base/cs_1d_wall_thermal.c
@@ -596,7 +596,7 @@ cs_1d_wall_thermal_read(void)
   cs_lnum_t           ii, jj, ifac, indfac, ierror;
   cs_lnum_t           n_b_faces = cs_glob_mesh->n_b_faces;
 
-  char nomsui[] = "1dwall_module";
+  char nomsui[] = "1dwall_module.csc";
 
   cs_restart_t             *suite;
   cs_mesh_location_type_t   support;
@@ -982,7 +982,7 @@ cs_1d_wall_thermal_write(void)
   cs_lnum_t            nbvent;
   cs_lnum_t            ii, jj, ifac;
 
-  char nomsui[] = "1dwall_module";
+  char nomsui[] = "1dwall_module.csc";
 
   cs_lnum_t n_b_faces = cs_glob_mesh->n_b_faces;
 

src/base/cs_file.c
@@ -3889,4 +3889,51 @@ cs_file_remove(const char  *path)
 
 /*----------------------------------------------------------------------------*/
 
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Check if file name/path ends with a specific string
+ *
+ * The function returns an int: 1 if the file name ends with the
+ * given string, 0 otherwise.
+ *
+ * \param[in]  path name of file
+ * \param[in]  end  end string to test
+ *
+ * \return an int. 1 if the path ends with the given string, 0 otherwise.
+ */
+/*----------------------------------------------------------------------------*/
+
+int
+cs_file_endswith(const char *path,
+                 const char *end)
+{
+
+  int retval = 0;
+
+  /* If either pointers is NULL, return 0 */
+  if (path == NULL || end == NULL)
+    retval = 0;
+
+  else {
+
+    const int lpath = strlen(path);
+    const int lext  = strlen(end);
+
+    /* If either strings is empty, or if the path is shorter than the end
+     * string, return 0
+     */
+    if (lpath == 0 || lext == 0)
+      retval = 0;
+
+    else if (lext > lpath)
+      retval = 0;
+
+    else
+      retval = (strcmp(path + (lpath-lext), end) == 0);
+  }
+
+  return retval;
+
+}
+/*----------------------------------------------------------------------------*/
 END_C_DECLS

src/base/cs_file.h
@@ -831,6 +831,21 @@ cs_file_size(const char  *path);
 int
 cs_file_remove(const char  *path);
 
+/*----------------------------------------------------------------------------
+ * Check if a file name ends with a specific string (extension)
+ *
+ * parameters
+ *   path <-- file path.
+ *   end  <-- string to compare
+ *
+ * returns:
+ *   1 in case of success, 0 otherwise.
+ *----------------------------------------------------------------------------*/
+
+int
+cs_file_endswith(const char *path,
+                 const char *end);
+
 /*----------------------------------------------------------------------------*/
 
 END_C_DECLS

src/base/cs_parameters.c
@@ -1137,7 +1137,7 @@ cs_parameters_read_restart_info(void)
 {
   if (cs_restart_present()) {
     cs_restart_t *r
-      = cs_restart_create("main", "restart", CS_RESTART_MODE_READ);
+      = cs_restart_create("main.csc", "restart", CS_RESTART_MODE_READ);
     cs_restart_read_time_step_info(r);
     cs_restart_destroy(&r);
   }

commit 1dd7f225587de1a29ea2de72e888bbf922796031

Fix automatic checkpoint selection, broken by commit 3e772f93 (Jan 31 2020).
With the .csc extension added to fies produced, both file name with
and without the extension need to be checked.

bin/cs_case_domain.py
@@ -459,7 +459,9 @@ class domain(base_domain):
         results = os.listdir(results_dir)
         results.sort(reverse=True)
         for r in results:
-            m = os.path.join(results_dir, r, 'checkpoint', 'main')
+            m = os.path.join(results_dir, r, 'checkpoint', 'main.csc')
+            if not os.path.isfile(m):
+                m = os.path.join(results_dir, r, 'checkpoint', 'main')
             if os.path.isfile(m):
                 try:
                     cmd = self.package.get_io_dump()

bin/model/StartRestartModel.py
@@ -77,6 +77,8 @@ def getRestartInfo(package, results_dir=None, restart_path='*'):
             m = os.path.join(results_dir, r, 'checkpoint', 'main')
         else:
             m = os.path.join(r, 'main')
+        if not os.path.isfile(m):
+            m += '.csc'
         if os.path.isfile(m):
             if True: # try:
                 nt = -1