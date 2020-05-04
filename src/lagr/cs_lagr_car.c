code-saturne/code_saturne

commit 728bf31c89275b6d7f6e67b7f1cfa7efc7a79f8e

Lagrangian module: add local projection (idirla 4) for turbulent dispersion.
Adapted from patch by J. Lavieville.

src/lagr/cs_lagr_car.c
@@ -180,7 +180,8 @@ cs_lagr_car(int              iprev,
     cs_real_t  xnul          = extra->viscl->val[cell_id] / rom;
     cs_real_t *part_vel_seen = cs_lagr_particle_attr(particle, p_am,
                                                      CS_LAGR_VELOCITY_SEEN);
-    cs_real_t *part_vel      = cs_lagr_particle_attr(particle, p_am, CS_LAGR_VELOCITY);
+    cs_real_t *part_vel      = cs_lagr_particle_attr(particle, p_am,
+                                                     CS_LAGR_VELOCITY);
 
     cs_real_t rel_vel_norm = cs_math_3_distance(part_vel_seen, part_vel);
 
@@ -209,9 +210,8 @@ cs_lagr_car(int              iprev,
 
     cs_user_lagr_rt(ip, rep, rel_vel_norm, rom, p_rom, xnul, taup, dt);
 
-    /**************************
-     *    Tc computation      *
-     **************************/
+    /* Tc computation */
+
     if (   (   cs_glob_lagr_model->physical_model == 1
             && cs_glob_lagr_specific_physics->itpvar == 1)
         || (cs_glob_lagr_model->physical_model == 2) ) {
@@ -254,9 +254,8 @@ cs_lagr_car(int              iprev,
 
   }
 
-  /* ====================================================================
-   * 3. CALCUL DE TL
-   * ====================================================================   */
+  /* Compute TL
+     ---------- */
 
   /* Compute turbulent kinetic energy and dissipation
      based on turbulence model */
@@ -313,17 +312,16 @@ cs_lagr_car(int              iprev,
 
     else {
 
-      bft_printf(_("\n WARNING: STOP AT LAGRANGIAN MODULE EXECUTION\n"));
-      bft_printf
-        (_("The lagrangian module is not compatible with the selected turbulence model.\n"
+      bft_error
+        (__FILE__, __LINE__, 0,
+         _("Lagrangian turbulent dispersion is not compatible with\n"
+           "the selected turbulence model.\n"
            "\n"
-           "Turbulent dispersion is taken into account with IDISTU = %d\n"
+           "Turbulent dispersion is taken into account with idistu = %d\n"
            " Activated turbulence model is %d, when only k-eps, Rij-eps,\n"
-           " V2f or k-omega are compatible with turbulent dispersion and Lagrangian module.\n"
-           "\n"),
+           " V2f or k-omega are handled."),
          (int)cs_glob_lagr_time_scheme->idistu,
          (int)extra->iturb);
-      cs_exit(1);
 
     }
 
@@ -391,16 +389,16 @@ cs_lagr_car(int              iprev,
         for  (cs_lnum_t i = 0; i < 3; i++)
           uvwdif += cs_math_sq(vflui[i] - vpart[i]);
 
-        uvwdif   = (3.0 * uvwdif) / (2.0 * energi[cell_id]);
+        uvwdif = (3.0 * uvwdif) / (2.0 * energi[cell_id]);
 
         if (   cs_glob_lagr_time_scheme->modcpl > 0
             && cs_glob_time_step->nt_cur > cs_glob_lagr_time_scheme->modcpl) {
 
           if (cs_glob_lagr_time_scheme->idirla == 1) {
 
-            bbi[0] = sqrt (1.0 + cbcb * uvwdif);
-            bbi[1] = sqrt (1.0 + 4.0 * cbcb * uvwdif);
-            bbi[2] = sqrt (1.0 + 4.0 * cbcb * uvwdif);
+            bbi[0] = sqrt(1.0 +       cbcb * uvwdif);
+            bbi[1] = sqrt(1.0 + 4.0 * cbcb * uvwdif);
+            bbi[2] = sqrt(1.0 + 4.0 * cbcb * uvwdif);
             for (cs_lnum_t id = 0; id < 3; id++)
               tlag[ip][id] = tl / bbi[id];
 
@@ -408,9 +406,9 @@ cs_lagr_car(int              iprev,
 
           else if (cs_glob_lagr_time_scheme->idirla == 2) {
 
-            bbi[0] = sqrt (1.0 + 4.0 * cbcb * uvwdif);
-            bbi[1] = sqrt (1.0 + cbcb * uvwdif);
-            bbi[2] = sqrt (1.0 + 4.0 * cbcb * uvwdif);
+            bbi[0] = sqrt(1.0 + 4.0 * cbcb * uvwdif);
+            bbi[1] = sqrt(1.0 +       cbcb * uvwdif);
+            bbi[2] = sqrt(1.0 + 4.0 * cbcb * uvwdif);
             for (cs_lnum_t id = 0; id < 3; id++)
               tlag[ip][id] = tl / bbi[id];
 
@@ -418,27 +416,76 @@ cs_lagr_car(int              iprev,
 
           else if (cs_glob_lagr_time_scheme->idirla == 3) {
 
-            bbi[0] = sqrt (1.0 + 4.0 * cbcb * uvwdif);
-            bbi[1] = sqrt (1.0 + 4.0 * cbcb * uvwdif);
-            bbi[2] = sqrt (1.0 + cbcb * uvwdif);
+            bbi[0] = sqrt(1.0 + 4.0 * cbcb * uvwdif);
+            bbi[1] = sqrt(1.0 + 4.0 * cbcb * uvwdif);
+            bbi[2] = sqrt(1.0 +       cbcb * uvwdif);
             for (cs_lnum_t id = 0; id < 3; id++)
               tlag[ip][id] = tl / bbi[id];
 
           }
 
-          else {
+          else { /* if (cs_glob_lagr_time_scheme->idirla == 4) */
 
-            bft_printf(_("\n WARNING: STOP AT LAGRANGIAN MODULE EXECUTION\n"));
-            bft_printf
-              (_("Complete model direction choice value is not correct (in LAGCAR).\n"
-                 " IDIRLA should be an integer ranging from 1 to 3:\n"
-                 " =1 for a flow along x-axis\n"
-                 " =2 for a flow along y-axis\n"
-                 " =3 for a flow along z-axis\n"
-                 " Here IDIRLA = %d\n"),
-               (int)cs_glob_lagr_time_scheme->idirla);
-            cs_exit(1);
+            /* relative main direction */
+            cs_real_t vrn[3];
+            for  (cs_lnum_t i = 0; i < 3; i++)
+              vrn[i] = vpart[i] - vflui[i];
+            cs_real_t vrnn = cs_math_3_norm(vrn);
+            vrnn = sqrt(vrnn);
+            if (vrnn > 1.e-10) { /* TODO replace this with adimensional test */
+              for  (cs_lnum_t i = 0; i < 3; i++)
+                vrn[i] /= vrnn;
+            }
+            else {
+              vrn[0] = 1.;
+              vrn[1] = 0.;
+              vrn[2] = 0.;
+            }
 
+            /* Choose the first normal direction */
+            cs_real_t vn1[3] = {0, 0, 1};
+            cs_real_t n1[3]  = {1, 0, 0};
+            cs_real_t n2[3]  = {0, 1, 0};
+            cs_real_t n3[3]  = {0, 0, 1};
+
+            cs_real_t psca1 = cs_math_fabs(cs_math_3_dot_product(vrn, n1));
+            cs_real_t psca2 = cs_math_fabs(cs_math_3_dot_product(vrn, n2));
+            cs_real_t psca3 = cs_math_fabs(cs_math_3_dot_product(vrn, n3));
+
+            if (psca1 < psca2 && psca1 < psca3)
+              cs_math_3_cross_product(vrn, n1, vn1);
+            else if (psca2 < psca1 && psca2 < psca3)
+              cs_math_3_cross_product(vrn, n2, vn1);
+            else if (psca3 < psca1 && psca3 < psca2)
+              cs_math_3_cross_product(vrn, n3, vn1);
+
+            /* second normal direction */
+            cs_real_t vn2[3];
+            cs_math_3_cross_product(vrn, vn1, vn2);
+
+            /* crossing trajectory in the vrn direction */
+            cs_real_t an, at;
+            an = (1.0 + cbcb * uvwdif);
+            at = (1.0 + 4.0 * cbcb * uvwdif);
+
+            /* projection in space directions */
+
+            bbi[0] =   an * cs_math_pow2(cs_math_3_dot_product(vrn, n1))
+                     + at * cs_math_pow2(cs_math_3_dot_product(vn1, n1))
+                     + at * cs_math_pow2(cs_math_3_dot_product(vn2, n1));
+            bbi[1] =   an * cs_math_pow2(cs_math_3_dot_product(vrn, n2))
+                     + at * cs_math_pow2(cs_math_3_dot_product(vn1, n2))
+                     + at * cs_math_pow2(cs_math_3_dot_product(vn2, n2));
+            bbi[2] =   an * cs_math_pow2(cs_math_3_dot_product(vrn, n3))
+                     + at * cs_math_pow2(cs_math_3_dot_product(vn1, n3))
+                     + at * cs_math_pow2(cs_math_3_dot_product(vn2, n3));
+
+            bbi[0] = sqrt(bbi[0]);
+            bbi[1] = sqrt(bbi[1]);
+            bbi[2] = sqrt(bbi[2]);
+
+            for (cs_lnum_t id = 0; id < 3; id++)
+              tlag[ip][id] = tl / bbi[id];
           }
 
           if (extra->itytur == 3) {
@@ -461,7 +508,8 @@ cs_lagr_car(int              iprev,
 
           }
 
-          else if (extra->itytur == 2 || extra->iturb == 50 || extra->iturb == 60)
+          else if (   extra->itytur == 2
+                   || extra->iturb == 50 || extra->iturb == 60)
             ktil = energi[cell_id];
 
           for (cs_lnum_t id = 0; id < 3; id++) {
@@ -524,13 +572,12 @@ cs_lagr_car(int              iprev,
 
   }
 
-  /* ====================================================================   */
-  /* 4. CALCUL DE PII     */
-  /* ====================================================================   */
+  /* Compute Pii
+     ----------- */
 
   if (   cs_glob_lagr_time_scheme->modcpl > 0
-      &&   cs_glob_time_step->nt_cur
-         > cs_glob_lagr_time_scheme->modcpl) {
+      && (  cs_glob_time_step->nt_cur
+          > cs_glob_lagr_time_scheme->modcpl)) {
 
     int stat_type = cs_lagr_stat_type_from_attr_id(CS_LAGR_VELOCITY);
 

bin/model/LagrangianModel.py
@@ -97,7 +97,7 @@ class LagrangianModel(Model):
         default['turbulent_dispersion']                = "on"
         default['fluid_particles_turbulent_diffusion'] = "off"
         default['complete_model_iteration']            = 0
-        default['complete_model_direction']            = 1
+        default['complete_model_direction']            = 4
 
         return default
 
@@ -682,7 +682,7 @@ class LagrangianModel(Model):
         Set value for complete model direction.
         """
         self.isInt(value)
-        self.isInList(value, (1,2,3))
+        self.isInList(value, (1,2,3,4))
         node_direction = self.node_lagr.xmlInitChildNode('complete_model_direction', 'choice')
         node_direction['choice'] = value
 

gui/Pages/LagrangianView.py
@@ -97,6 +97,7 @@ class LagrangianAdvancedOptionsDialogView(QDialog, Ui_LagrangianAdvancedOptionsD
         self.modelIDIRLA.addItem(self.tr("X"), "1")
         self.modelIDIRLA.addItem(self.tr("Y"), "2")
         self.modelIDIRLA.addItem(self.tr("Z"), "3")
+        self.modelIDIRLA.addItem(self.tr("Local projection"), "4")
 
         # Connections
         self.comboBoxNORDRE.activated[str].connect(self.slotNORDRE)

src/lagr/cs_lagr.h
@@ -191,7 +191,7 @@ typedef struct {
     either be 0 or be larger than \ref idstnt. */
   int     modcpl;
 
-  /*!  direction (1=x, 2=y, 3=z) of the complete model.
+  /*!  direction (1=x, 2=y, 3=z, 4=local_max) of the complete model.
     it corresponds to the main directions of the flow.
     Useful if \ref modcpl > 0 */
   int     idirla;

src/lagr/cs_lagr_gradients.c
@@ -116,12 +116,22 @@ cs_lagr_gradients(int            time_id,
 
   /* Use pressure gradient of NEPTUNE_CFD if needed */
   if (cs_field_by_name_try("velocity_1") != NULL) {
-    cs_real_t *romf = extra->cromf->val;
     cs_real_t *cpro_pgradlagr = cs_field_by_name_try("lagr_pressure_gradient")->val;
 
     for (cs_lnum_t iel = 0; iel < cs_glob_mesh->n_cells; iel++)
       for (cs_lnum_t id = 0; id < 3; id++)
         grad_pr[iel][id] = cpro_pgradlagr[3*iel + id];
+
+    cs_real_33_t *cpro_vgradlagr = cs_field_by_name_try("lagr_velocity_gradient")->val;
+
+    if (cpro_vgradlagr != NULL) {
+      for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++) {
+        for (cs_lnum_t i = 0; i < 3; i++) {
+          for (cs_lnum_t j = 0; j < 3; j++)
+            grad_vel[c_id][i][j] = cpro_vgradlagr[c_id][i][j];
+       }
+     }
+    }
 
     return;
   }
@@ -247,8 +257,8 @@ cs_lagr_gradients(int            time_id,
      ========================= */
 
   if ( (  cs_glob_lagr_time_scheme->modcpl > 0
-       && cs_glob_time_step->nt_cur >= cs_glob_lagr_time_scheme->modcpl )
-       || cs_glob_lagr_model->shape > 0 ) {
+        && cs_glob_time_step->nt_cur >= cs_glob_lagr_time_scheme->modcpl)
+      || cs_glob_lagr_model->shape > 0 ) {
 
     cs_field_gradient_vector(extra->vel,
                              time_id,

src/lagr/cs_lagr_log.c
@@ -45,7 +45,6 @@
  *----------------------------------------------------------------------------*/
 
 #include "bft_printf.h"
-#include "bft_error.h"
 #include "bft_mem.h"
 
 #include "cs_log.h"
@@ -432,9 +431,9 @@ cs_lagr_log_setup(void)
      cs_glob_lagr_time_scheme->modcpl);
 
   if (cs_glob_lagr_time_scheme->modcpl) {
-    const char c_dir[] = "xyze";
+    const char c_dir[] = "xyz*";
     int _idirla = cs_glob_lagr_time_scheme->idirla;
-    assert(_idirla > -1 && _idirla < 4);
+    assert(_idirla > -1 && _idirla < 5);
     cs_log_printf
       (CS_LOG_SETUP,
        _("    complete model main flow direction: %c\n"),

src/lagr/cs_lagr_options.c
@@ -41,7 +41,6 @@
  *  Local headers
  *----------------------------------------------------------------------------*/
 
-#include "bft_error.h"
 #include "bft_mem.h"
 #include "bft_printf.h"
 
@@ -798,7 +797,7 @@ cs_lagr_option_definition(cs_int_t   *isuite,
                                   _("in Lagrangian module"),
                                   "cs_glob_lagr_time_scheme->idirla",
                                   lagr_time_scheme->idirla,
-                                  1, 4);
+                                  1, 5);
   }
 
   cs_parameters_is_in_range_int(CS_ABORT_DELAYED,

commit e2d1ef2410fabc55f24f4ae174a2f5420cc0b98e

[Lagrangian] Fix bug introduced in commit 728bf31c8 ((2019-10-03) and simplify formulae for Lagrangian General model for turbulent dispersion.

src/lagr/cs_lagr_car.c
@@ -427,62 +427,23 @@ cs_lagr_car(int              iprev,
           else { /* if (cs_glob_lagr_time_scheme->idirla == 4) */
 
             /* relative main direction */
-            cs_real_t vrn[3];
+            cs_real_3_t vrn, n_dir;
             for (cs_lnum_t i = 0; i < 3; i++)
               vrn[i] = vpart[i] - vflui[i];
 
-            cs_real_t vrnn = cs_math_3_norm(vrn);
-            if (vrnn > 1.e-10) { /* TODO replace this with adimensional test */
-              for (cs_lnum_t i = 0; i < 3; i++)
-                vrn[i] /= vrnn;
-            }
-            else {
-              vrn[0] = 1.;
-              vrn[1] = 0.;
-              vrn[2] = 0.;
-            }
+            cs_math_3_normalise(vrn, n_dir);
 
-            /* Choose the first normal direction */
-            cs_real_t vn1[3] = {0, 0, 1};
-            cs_real_t n1[3]  = {1, 0, 0};
-            cs_real_t n2[3]  = {0, 1, 0};
-            cs_real_t n3[3]  = {0, 0, 1};
-
-            cs_real_t psca1 = cs_math_fabs(cs_math_3_dot_product(vrn, n1));
-            cs_real_t psca2 = cs_math_fabs(cs_math_3_dot_product(vrn, n2));
-            cs_real_t psca3 = cs_math_fabs(cs_math_3_dot_product(vrn, n3));
-
-            if (psca1 < psca2 && psca1 < psca3)
-              cs_math_3_cross_product(vrn, n1, vn1);
-            else if (psca2 < psca1 && psca2 < psca3)
-              cs_math_3_cross_product(vrn, n2, vn1);
-            else if (psca3 < psca1 && psca3 < psca2)
-              cs_math_3_cross_product(vrn, n3, vn1);
-
-            /* second normal direction */
-            cs_real_t vn2[3];
-            cs_math_3_cross_product(vrn, vn1, vn2);
-
-            /* crossing trajectory in the vrn direction */
+            /* crossing trajectory in the n_dir direction */
             cs_real_t an, at;
             an = (1.0 + cbcb * uvwdif);
             at = (1.0 + 4.0 * cbcb * uvwdif);
 
-            /* projection in space directions */
+            /* We take the diagonal part of
+             *  an. n(x)n + at (1 - n(x)n) */
 
-            bbi[0] =   an * cs_math_pow2(cs_math_3_dot_product(vrn, n1))
-                     + at * cs_math_pow2(cs_math_3_dot_product(vn1, n1))
-                     + at * cs_math_pow2(cs_math_3_dot_product(vn2, n1));
-            bbi[1] =   an * cs_math_pow2(cs_math_3_dot_product(vrn, n2))
-                     + at * cs_math_pow2(cs_math_3_dot_product(vn1, n2))
-                     + at * cs_math_pow2(cs_math_3_dot_product(vn2, n2));
-            bbi[2] =   an * cs_math_pow2(cs_math_3_dot_product(vrn, n3))
-                     + at * cs_math_pow2(cs_math_3_dot_product(vn1, n3))
-                     + at * cs_math_pow2(cs_math_3_dot_product(vn2, n3));
-
-            bbi[0] = sqrt(bbi[0]);
-            bbi[1] = sqrt(bbi[1]);
-            bbi[2] = sqrt(bbi[2]);
+            for (cs_lnum_t id = 0; id < 3; id++)
+              bbi[id] = sqrt(an * cs_math_pow2(n_dir[id])
+                           + at * (1. - cs_math_pow2(n_dir[id])));
 
             for (cs_lnum_t id = 0; id < 3; id++)
               tlag[ip][id] = tl / bbi[id];