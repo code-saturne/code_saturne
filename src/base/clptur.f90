code-saturne/code_saturne

commit f014a54913d856c41f9c8cc2bbdff48173df3170

Fix internal coupling: clip turbulent viscosity in solid zone.
This also fixes k-omega with ikecou=1 option (not tested and deprecated)
and with the porous approach (use cell_f_vol rather volume).

src/base/clptur.f90
@@ -170,7 +170,7 @@ double precision uetmax, uetmin, ukmax, ukmin, yplumx, yplumn
 double precision tetmax, tetmin, tplumx, tplumn
 double precision uk, uet, nusury, yplus, dplus
 double precision sqrcmu, ek
-double precision xnuii, xnuit, xmutlm
+double precision xnuii, xnuit, xmutlm, mut_lm_dmut
 double precision rcprod
 double precision hflui, hint, pimp, qimp
 double precision eloglo(3,3), alpha(6,6)
@@ -796,11 +796,16 @@ do ifac = 1, nfabor
       if (itytur.eq.2.or.iturb.eq.60) then
 
         xmutlm = xkappa*visclc*yplus
+        if (cell_is_active(iel).eq.0) then
+          mut_lm_dmut = xmutlm/visctc
+        else
+          mut_lm_dmut = 0.d0
+        endif
 
         ! If yplus=0, uiptn is set to 0 to avoid division by 0.
         ! By the way, in this case: iuntur=0
         if (yplus.gt.epzero) then !TODO use iuntur.eq.1
-          rcprod = min(xkappa , max(1.0d0,sqrt(xmutlm/visctc))/yplus)
+          rcprod = min(xkappa , max(1.0d0,sqrt(mut_lm_dmut))/yplus)
 
           uiptn  = utau + distbf*uet*uk*romc/xkappa/visclc*(       &
                1.0d0/(2.0d0*yplus-dplus) - 2.0d0*rcprod )
@@ -2053,6 +2058,7 @@ double precision viscis, visctc, xmutlm, ypth, xnuii
 double precision rinfiv(3), pimpv(3)
 double precision visci(3,3), hintt(6)
 double precision turb_schmidt, exchange_coef
+double precision mut_lm_dmut
 
 character(len=80) :: fname
 
@@ -2474,7 +2480,13 @@ do ifac = 1, nfabor
         ! In the log layer
         if (yplus.ge.ypth.and.iturb.ne.0) then
           xmutlm = xkappa*visclc*yplus
-          rcprod = min(xkappa , max(1.0d0,sqrt(xmutlm/visctc))/yplus)
+          if (cell_is_active(iel).eq.0) then
+            mut_lm_dmut = xmutlm/visctc
+          else
+            mut_lm_dmut = 0.d0
+          endif
+
+          rcprod = min(xkappa , max(1.0d0,sqrt(mut_lm_dmut))/yplus)
 
           cofimp = 1.d0 - yptp(ifac)*turb_schmidt/xkappa*                   &
                           (2.0d0*rcprod - 1.0d0/(2.0d0*yplus-dplus))

src/base/tridim.f90
@@ -492,8 +492,12 @@ if (vcopt_u%iwarni.ge.1) then
   write(nfecra,1010)
 endif
 
+! Disable solid cells in fluid_solid mode
+if (fluid_solid) call cs_mesh_quantities_set_has_disable_flag(1)
 iterns = -1
 call phyvar(nvar, nscal, iterns, dt)
+! Re enable solid cells in fluid_solid mode
+if (fluid_solid) call cs_mesh_quantities_set_has_disable_flag(0)
 
 if (itrale.gt.0) then
   iappel = 2
@@ -1110,9 +1114,11 @@ do while (iterns.le.nterup)
         call diffst(nscal, iterns)
       endif
 
-      ! Update the density
-      !-------------------
+      ! Update the density and turbulent viscosity
+      !-------------------------------------------
 
+      ! Disable solid cells in fluid_solid mode
+      if (fluid_solid) call cs_mesh_quantities_set_has_disable_flag(1)
       call phyvar(nvar, nscal, iterns, dt)
 
     endif
@@ -1124,9 +1130,6 @@ do while (iterns.le.nterup)
     ! Coupled solving of the velocity components
 
     if (ippmod(idarcy).eq.-1) then
-
-      ! Disable solid cells in fluid_solid mode
-      if (fluid_solid) call cs_mesh_quantities_set_has_disable_flag(1)
 
       call navstv &
       ( nvar   , nscal  , iterns , icvrge , itrale ,                   &

src/base/cs_c_bindings.f90
@@ -2865,6 +2865,18 @@ module cs_c_bindings
 
     !---------------------------------------------------------------------------
 
+    ! Interface to C function returning 1 for active cells
+
+    function cs_f_mesh_quantities_cell_is_active(cell_id) result(is_active) &
+      bind(C, name='cs_f_mesh_quantities_cell_is_active')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      integer(c_int), value :: cell_id
+      integer(kind=c_int) :: is_active
+    end function cs_f_mesh_quantities_cell_is_active
+
+    !---------------------------------------------------------------------------
+
     !> (DOXYGEN_SHOULD_SKIP_THIS) \endcond
 
     !---------------------------------------------------------------------------
@@ -5889,6 +5901,35 @@ contains
     val = c_val
 
   end function notebook_parameter_value_by_name
+
+  !=============================================================================
+
+  !> \brief Indicate of a cell is active fo the current variable
+
+  !> \param[in]     iel       cell number (cell_id + 1)
+  !> \result        is_active
+
+  function cell_is_active(iel) result(is_active)
+
+    use, intrinsic :: iso_c_binding
+    implicit none
+
+    ! Arguments
+
+    integer :: iel
+    integer :: is_active
+
+    ! Local variables
+
+    integer(kind=c_int) :: c_cell_id
+    integer(kind=c_int) :: c_is_active
+
+
+    c_cell_id = iel - 1
+    c_is_active = cs_f_mesh_quantities_cell_is_active(c_cell_id)
+    is_active = c_is_active
+
+  end function cell_is_active
 
   !=============================================================================
 

src/base/cs_equation_iterative_solve.c
@@ -494,8 +494,6 @@ cs_equation_iterative_solve_scalar(int                   idtvar,
   {
 #   pragma omp for nowait
     for (cs_lnum_t cell_id = 0; cell_id < n_cells; cell_id++) {
-      if (has_dc * mq->c_disable_flag[has_dc * cell_id] != 0)
-        smbrp[cell_id] = 0.;
       smbini[cell_id] = smbrp[cell_id];
       smbrp[cell_id] = 0.;
     }
@@ -1408,14 +1406,12 @@ cs_equation_iterative_solve_vector(int                   idtvar,
   /* Before looping, the RHS without reconstruction is stored in smbini */
 
   int has_dc = mq->has_disable_flag;
+  bft_printf("CODITV, has_dc %d \n", has_dc);
 # pragma omp parallel if(n_cells > CS_THR_MIN)
   {
 #   pragma omp for nowait
     for (cs_lnum_t cell_id = 0; cell_id < n_cells; cell_id++) {
       for (cs_lnum_t isou = 0; isou < 3; isou++) {
-        /* Overwrite disabled cells */
-        if (has_dc * mq->c_disable_flag[has_dc * cell_id] != 0)
-          smbrp[cell_id][isou] = 0.;
         smbini[cell_id][isou] = smbrp[cell_id][isou];
         smbrp[cell_id][isou] = 0.;
       }
@@ -2240,9 +2236,6 @@ cs_equation_iterative_solve_tensor(int                   idtvar,
 #   pragma omp for nowait
     for (cs_lnum_t cell_id = 0; cell_id < n_cells; cell_id++) {
       for (cs_lnum_t isou = 0; isou < 6; isou++) {
-        /* Overwrite disabled cells */
-        if (has_dc * mq->c_disable_flag[has_dc * cell_id] != 0)
-          smbrp[cell_id][isou] = 0.;
         smbini[cell_id][isou] = smbrp[cell_id][isou];
         smbrp[cell_id][isou] = 0.;
       }

src/base/cs_stokes_model.c
@@ -545,6 +545,13 @@ cs_stokes_model_log_setup(void)
        _("    arak:        %14.5e (Arakawa factor)\n"),
        var_cal_opt.relaxv * stokes_model->arak);
   }
+  if (stokes_model->fluid_solid)
+    cs_log_printf
+      (CS_LOG_SETUP,
+       _("\n"
+         "  Fluid-solid mode (disable dynamics in the solid part)\n\n"));
+
+
 }
 
 /*----------------------------------------------------------------------------*/

src/base/navstv.f90
@@ -990,9 +990,9 @@ if (ippmod(icompf).lt.0.or.ippmod(icompf).eq.3) then
       ! Update external forces for the computation of the gradients
       !$omp parallel do
       do iel=1,ncel
-        frcxt(1 ,iel) = frcxt(1 ,iel) * (1 - isolid(iporos, iel)) + dfrcxt(1 ,iel)
-        frcxt(2 ,iel) = frcxt(2 ,iel) * (1 - isolid(iporos, iel)) + dfrcxt(2 ,iel)
-        frcxt(3 ,iel) = frcxt(3 ,iel) * (1 - isolid(iporos, iel)) + dfrcxt(3 ,iel)
+        frcxt(1 ,iel) = frcxt(1 ,iel) * cell_is_active(iel) + dfrcxt(1 ,iel)
+        frcxt(2 ,iel) = frcxt(2 ,iel) * cell_is_active(iel) + dfrcxt(2 ,iel)
+        frcxt(3 ,iel) = frcxt(3 ,iel) * cell_is_active(iel) + dfrcxt(3 ,iel)
       enddo
       if (irangp.ge.0.or.iperio.eq.1) then
         call synvin(frcxt)
@@ -1081,13 +1081,13 @@ if (ippmod(icompf).lt.0.or.ippmod(icompf).eq.3) then
 
         mass_fl_drhovol1 = 0.d0
         ! If it is not a solid cell
-        if (isolid(iporos, iel1).eq.0) &
+        if (cell_is_active(iel1).eq.1) &
           mass_fl_drhovol1 = imasfl(ifac) / (crom(iel1) * cell_f_vol(iel1))
 
 
         mass_fl_drhovol2 = 0.d0
         ! If it is not a solid cell
-        if (isolid(iporos, iel2).eq.0) &
+        if (cell_is_active(iel2).eq.1) &
           mass_fl_drhovol2 = imasfl(ifac) / (crom(iel2) * cell_f_vol(iel2))
 
         vel(1, iel1) = vel(1, iel1) &
@@ -1111,7 +1111,7 @@ if (ippmod(icompf).lt.0.or.ippmod(icompf).eq.3) then
 
         mass_fl_drhovol1 = 0.d0
         ! If it is not a solid cell
-        if (isolid(iporos, iel1).eq.0) &
+        if (cell_is_active(iel1).eq.1) &
           mass_fl_drhovol1 = bmasfl(ifac) / (crom(iel1) * cell_f_vol(iel1))
 
         vel(1, iel1) = vel(1, iel1) &
@@ -1133,13 +1133,13 @@ if (ippmod(icompf).lt.0.or.ippmod(icompf).eq.3) then
 
         mass_fl_drhovol1 = 0.d0
         ! If it is not a solid cell
-        if (isolid(iporos, iel1).eq.0) &
+        if (cell_is_active(iel1).eq.1) &
           mass_fl_drhovol1 = imasfl(ifac) / cell_f_vol(iel1)
 
 
         mass_fl_drhovol2 = 0.d0
         ! If it is not a solid cell
-        if (isolid(iporos, iel2).eq.0) &
+        if (cell_is_active(iel2).eq.1) &
           mass_fl_drhovol2 = imasfl(ifac) / cell_f_vol(iel2)
 
         vel(1, iel1) = vel(1, iel1) &
@@ -1163,7 +1163,7 @@ if (ippmod(icompf).lt.0.or.ippmod(icompf).eq.3) then
 
         mass_fl_drhovol1 = 0.d0
         ! If it is not a solid cell
-        if (isolid(iporos, iel1).eq.0) &
+        if (cell_is_active(iel1).eq.1) &
           mass_fl_drhovol1 = bmasfl(ifac) / cell_f_vol(iel1)
 
         vel(1, iel1) = vel(1, iel1) &

src/base/phyvar.f90
@@ -545,7 +545,7 @@ if (idfm.eq.1 .or. itytur.eq.3 .and. idirsm.eq.1) then
           ! Durbin scale
           xttkmg = xct*sqrt(viscl(iel)/crom(iel)/cvar_ep(iel))
           xttdrb = max(ttke,xttkmg)
-          rottke  = csrij * crom(iel) * xttdrb
+          rottke  = csrij * crom(iel) * xttdrb * cell_is_active(iel)
 
           do isou = 1, 6
             visten(isou, iel) = rottke*cvar_rij(isou, iel)
@@ -558,7 +558,7 @@ if (idfm.eq.1 .or. itytur.eq.3 .and. idirsm.eq.1) then
 
           do iel = 1, ncel
             trrij = 0.5d0*(cvar_rij(1,iel)+cvar_rij(2,iel)+cvar_rij(3,iel))
-            rottke  = csrij * crom(iel) * trrij / cvar_ep(iel)
+            rottke  = csrij * crom(iel) * trrij / cvar_ep(iel) * cell_is_active(iel)
 
             do isou = 1, 6
               vistes(isou, iel) = rottke*cvar_rij(isou, iel)
@@ -571,7 +571,7 @@ if (idfm.eq.1 .or. itytur.eq.3 .and. idirsm.eq.1) then
 
           do iel = 1, ncel
             trrij = 0.5d0*(cvar_rij(1,iel)+cvar_rij(2,iel)+cvar_rij(3,iel))
-            rottke  = csrij * crom(iel) * trrij / cvar_ep(iel)
+            rottke  = csrij * crom(iel) * trrij / cvar_ep(iel) * cell_is_active(iel)
 
             do isou = 1, 6
               vistes(isou, iel) = rottke*cvar_rij(isou, iel)
@@ -583,7 +583,7 @@ if (idfm.eq.1 .or. itytur.eq.3 .and. idirsm.eq.1) then
       else
         do iel = 1, ncel
           trrij = 0.5d0*(cvar_rij(1,iel)+cvar_rij(2,iel)+cvar_rij(3,iel))
-          rottke  = csrij * crom(iel) * trrij / cvar_ep(iel)
+          rottke  = csrij * crom(iel) * trrij / cvar_ep(iel) * cell_is_active(iel)
 
           do isou = 1, 6
             visten(isou, iel) = rottke*cvar_rij(isou, iel)
@@ -613,7 +613,7 @@ if (idfm.eq.1 .or. itytur.eq.3 .and. idirsm.eq.1) then
           ! Durbin scale
           xttkmg = xct*sqrt(viscl(iel)/crom(iel)/cvar_ep(iel))
           xttdrb = max(ttke,xttkmg)
-          rottke  = csrij * crom(iel) * xttdrb
+          rottke  = csrij * crom(iel) * xttdrb * cell_is_active(iel)
 
           visten(1,iel) = rottke*cvar_r11(iel)
           visten(2,iel) = rottke*cvar_r22(iel)
@@ -634,7 +634,7 @@ if (idfm.eq.1 .or. itytur.eq.3 .and. idirsm.eq.1) then
             xttkmg = xct*sqrt(viscl(iel)/crom(iel)/cvar_ep(iel))
             xttdrb = max(ttke,xttkmg)
             !FIXME xttdrbt = xttdrb*sqrt((1.d0-alpha3)*PR/XRH + alpha3)
-            rottke  = csrij * crom(iel) * xttdrb
+            rottke  = csrij * crom(iel) * xttdrb * cell_is_active(iel)
 
             vistes(1,iel) = rottke*cvar_r11(iel)
             vistes(2,iel) = rottke*cvar_r22(iel)
@@ -650,7 +650,7 @@ if (idfm.eq.1 .or. itytur.eq.3 .and. idirsm.eq.1) then
 
           do iel = 1, ncel
             trrij = 0.5d0*(cvar_r11(iel)+cvar_r22(iel)+cvar_r33(iel))
-            rottke  = csrij * crom(iel) * trrij / cvar_ep(iel)
+            rottke  = csrij * crom(iel) * trrij / cvar_ep(iel) * cell_is_active(iel)
 
             vistes(1,iel) = rottke*cvar_r11(iel)
             vistes(2,iel) = rottke*cvar_r22(iel)
@@ -666,7 +666,7 @@ if (idfm.eq.1 .or. itytur.eq.3 .and. idirsm.eq.1) then
       else
         do iel = 1, ncel
           trrij = 0.5d0*(cvar_r11(iel)+cvar_r22(iel)+cvar_r33(iel))
-          rottke  = csrij * crom(iel) * trrij / cvar_ep(iel)
+          rottke  = csrij * crom(iel) * trrij / cvar_ep(iel) * cell_is_active(iel)
 
           visten(1,iel) = rottke*cvar_r11(iel)
           visten(2,iel) = rottke*cvar_r22(iel)
@@ -750,7 +750,8 @@ if (iturb.eq.41) then
 endif
 
 !===============================================================================
-! 9. Checking of the user values
+! 9. Checking of the user values and put turbulent viscosity to 0 in
+!    disabled cells
 !===============================================================================
 
 ! ---> Calcul des bornes des variables et impressions
@@ -780,7 +781,13 @@ do ii = 1, nn
   else
     ivar = 1
     if (ii.eq.2) call field_get_val_s(iviscl, cpro_var)
-    if (ii.eq.3) call field_get_val_s(ivisct, cpro_var)
+    if (ii.eq.3) then
+      call field_get_val_s(ivisct, cpro_var)
+      ! Set turbulent viscosity to 0 in disabled cells
+      do iel = 1, ncel
+        cpro_var(iel) = cpro_var(iel) * cell_is_active(iel)
+      enddo
+    endif
     if (ii.eq.4) call field_get_val_s(icp, cpro_var)
     varmx(ii) = cpro_var(1)
     varmn(ii) = cpro_var(1)

src/base/predvv.f90
@@ -1286,19 +1286,19 @@ if (iappel.eq.1.and.iphydr.eq.1) then
 
     ! Delta rho = - rho_0 beta Delta T
     do iel = 1, ncel
-      drom = - ro0 * cpro_beta(iel) * (cvar_t(iel) - t0) * (1 - isolid(iporos, iel))
-      dfrcxt(1, iel) = drom*gx - frcxt(1, iel)
-      dfrcxt(2, iel) = drom*gy - frcxt(2, iel)
-      dfrcxt(3, iel) = drom*gz - frcxt(3, iel)
+      drom = - ro0 * cpro_beta(iel) * (cvar_t(iel) - t0) * cell_is_active(iel)
+      dfrcxt(1, iel) = drom*gx - frcxt(1, iel) * cell_is_active(iel)
+      dfrcxt(2, iel) = drom*gy - frcxt(2, iel) * cell_is_active(iel)
+      dfrcxt(3, iel) = drom*gz - frcxt(3, iel) * cell_is_active(iel)
     enddo
 
   else
     do iel = 1, ncel
-      drom = (crom(iel)-ro0) * (1 - isolid(iporos, iel))
+      drom = (crom(iel)-ro0) * cell_is_active(iel)
 
-      dfrcxt(1, iel) = drom*gx - frcxt(1, iel)
-      dfrcxt(2, iel) = drom*gy - frcxt(2, iel)
-      dfrcxt(3, iel) = drom*gz - frcxt(3, iel)
+      dfrcxt(1, iel) = drom*gx - frcxt(1, iel) * cell_is_active(iel)
+      dfrcxt(2, iel) = drom*gy - frcxt(2, iel) * cell_is_active(iel)
+      dfrcxt(3, iel) = drom*gz - frcxt(3, iel) * cell_is_active(iel)
     enddo
   endif
 
@@ -1306,9 +1306,9 @@ if (iappel.eq.1.and.iphydr.eq.1) then
   if (ncepdp.gt.0) then
     do ielpdc = 1, ncepdp
       iel=icepdc(ielpdc)
-      vit1   = vela(1,iel) * (1 - isolid(iporos, iel))
-      vit2   = vela(2,iel) * (1 - isolid(iporos, iel))
-      vit3   = vela(3,iel) * (1 - isolid(iporos, iel))
+      vit1   = vela(1,iel) * cell_is_active(iel)
+      vit2   = vela(2,iel) * cell_is_active(iel)
+      vit3   = vela(3,iel) * cell_is_active(iel)
       cpdc11 = ckupdc(1,ielpdc)
       cpdc22 = ckupdc(2,ielpdc)
       cpdc33 = ckupdc(3,ielpdc)
@@ -1329,21 +1329,21 @@ if (iappel.eq.1.and.iphydr.eq.1) then
 
     ! Reference frame rotation
     do iel = 1, ncel
-      rom = -2.d0*crom(iel) * (1 - isolid(iporos, iel))
+      rom = -2.d0*crom(iel) * cell_is_active(iel)
       call add_coriolis_v(0, rom, vela(:,iel), dfrcxt(:,iel))
     enddo
     ! Turbomachinery frozen rotors rotation
     if (iturbo.eq.1) then
       do iel = 1, ncel
         if (irotce(iel).gt.0) then
-          rom = -crom(iel) * (1 - isolid(iporos, iel))
+          rom = -crom(iel) * cell_is_active(iel)
           call add_coriolis_v(irotce(iel), rom, vela(:,iel), dfrcxt(:,iel))
         endif
       enddo
     else if (icorio.eq.1) then
       do iel = 1, ncel
         if (irotce(iel).gt.0) then
-          rom = -crom(iel) * (1 - isolid(iporos, iel))
+          rom = -crom(iel) * cell_is_active(iel)
           call add_coriolis_v(0, rom, vela(:,iel), dfrcxt(:,iel))
         endif
       enddo
@@ -1355,7 +1355,7 @@ if (iappel.eq.1.and.iphydr.eq.1) then
     do iel = 1, ncel
       dvol = 0.d0
       ! If it is not a solid cell
-      if (isolid(iporos, iel).eq.0) dvol = 1.d0 / cell_f_vol(iel)
+      if (cell_is_active(iel).eq.1) dvol = 1.d0 / cell_f_vol(iel)
       do isou = 1, 3
         dfrcxt(isou, iel) = dfrcxt(isou, iel) - divt(isou, iel)*dvol
       enddo
@@ -1368,7 +1368,7 @@ if (iappel.eq.1.and.iphydr.eq.1) then
     do iel = 1, ncel
       dvol = 0.d0
       ! If it is not a solid cell
-      if (isolid(iporos, iel).eq.0) dvol = 1.d0 / cell_f_vol(iel)
+      if (cell_is_active(iel).eq.1) dvol = 1.d0 / cell_f_vol(iel)
 
       do isou = 1, 3
         dfrcxt(isou, iel) = dfrcxt(isou, iel) + tsexp(isou, iel)*dvol

commit 2d2c28b6de6ca498f81cc173a6c1f1ae5815e9ff

Fix bug introduced in commit f014a549.
Cherry-picked from commit b2b13c48 on master.

* fluid_solid option was badly set for k-epsilon and SSG model
* fix inconsistency with fluid_solid option and ikecou option

src/base/clptur.f90
@@ -575,9 +575,6 @@ allocate(byplus(nfabor))
 allocate(bdplus(nfabor))
 allocate(buk(nfabor))
 
-! Disable solid cells in fluid_solid mode
-if (fluid_solid) call cs_mesh_quantities_set_has_disable_flag(1)
-
 ! --- Loop on boundary faces
 do ifac = 1, nfabor
 
@@ -1711,9 +1708,6 @@ do ifac = 1, nfabor
 enddo
 ! --- End of loop over faces
 
-
-! Re enable solid cells in fluid_solid mode
-if (fluid_solid) call cs_mesh_quantities_set_has_disable_flag(0)
 
 !===========================================================================
 ! 8. Boundary conditions on the other scalars

src/base/modini.f90
@@ -730,6 +730,16 @@ endif
 if (icdpar.eq.-1 .and. ikw.eq.1 .and. isuite.eq.1)                &
      write(nfecra,2001)
 
+! ---> IKECOU
+!      If the fluid_solid option is enabled, we force ikecou to 0. 
+if (fluid_solid) then
+  if(ikecou .eq. 1) then
+    ikecou = 0
+    write(nfecra,5000)
+  endif
+endif
+
+
 ! ---> RELAXV
 if (idtvar.lt.0) then
   relxsp = 1.d0-relxst
@@ -1213,6 +1223,23 @@ endif
 '@',                                                            /,&
 '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
 '@',                                                            /)
-
+ 5000 format(                                                     &
+'@',                                                            /,&
+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
+'@',                                                            /,&
+'@ @@ WARNING:       IN THE DATA SPECIFICATION',                /,&
+'@    ========',                                                /,&
+'@',                                                            /,&
+'@  The pseudo coupling of turbulent dissipation and turbulent',/,&
+'@  kinetic energy (ikecou = 1) is not compatible with the use',/,&
+'@  of fluid/solid option to disable the dynamic in the solid ',/,&
+'@  cells (fluid_solid =1). ',                                  /,&
+'@',                                                            /,&
+'@  The parameter ikecou is forced to 0 (no coupling)',         /,&
+'@',                                                            /,&
+'@  The calculation will be run.',                              /,&
+'@',                                                            /,&
+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
+'@',                                                            /)
 return
 end subroutine

src/base/tridim.f90
@@ -496,8 +496,6 @@ endif
 if (fluid_solid) call cs_mesh_quantities_set_has_disable_flag(1)
 iterns = -1
 call phyvar(nvar, nscal, iterns, dt)
-! Re enable solid cells in fluid_solid mode
-if (fluid_solid) call cs_mesh_quantities_set_has_disable_flag(0)
 
 if (itrale.gt.0) then
   iappel = 2
@@ -1028,9 +1026,6 @@ do while (iterns.le.nterup)
   ! Compute y+ if needed
   ! and Van Driest "amortissement"
   if (itytur.eq.4 .and. idries.eq.1) then
-    ! Disable solid cells in fluid_solid mode
-    if (fluid_solid) call cs_mesh_quantities_set_has_disable_flag(1)
-
     call distyp(itypfb, visvdr)
   endif
 