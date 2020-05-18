code-saturne/code_saturne

commit 353ec9d0824096c79a7af3f70b4087c0aef8ae6a

Use `cs_probe_t` type profiles for GUI-defined profiles.
This required adding the possibility of defining fields to output with
a given postprocessing mesh using the new `cs_post_mesh_attach_field`
and `cs_probe_set_associate_field` functions.
For profiles, the curvilinear and cartesian coordinates may also
be output using the `cs_probe_set_auto_curvilinear_coords` and
`cs_probe_set_auto_cartesian_coords` functions.

src/gui/cs_gui_output.c
@@ -54,13 +54,14 @@
 #include "cs_gui_util.h"
 #include "cs_gui_variables.h"
 #include "cs_log.h"
+#include "cs_math.h"
 #include "cs_mesh_location.h"
 #include "cs_selector.h"
 #include "cs_physical_model.h"
 #include "cs_post.h"
 #include "cs_field.h"
 #include "cs_field_pointer.h"
-#include "cs_parameters.h"
+#include "cs_prototypes.h"
 #include "cs_thermal_model.h"
 #include "cs_time_moment.h"
 #include "cs_volume_zone.h"
@@ -97,8 +98,10 @@ BEGIN_C_DECLS
  *============================================================================*/
 
 /*============================================================================
- * Static global variables
+ * Static local variables
  *============================================================================*/
+
+static char _rij_c_names[6][4] = {"r11", "r22", "r33", "r12", "r23", "r13"};
 
 /*============================================================================
  * Private function definitions
@@ -227,6 +230,343 @@ _surfacic_variable_post(const char  *name,
   }
 
   return active;
+}
+
+/*----------------------------------------------------------------------------
+ * Return a string value associated with a child node for a profile.
+ *
+ * If the matching child node is not present, an error is produced
+ *
+ * parameters:
+ *   tn <-- tree node associated with profile
+ *
+ * return:
+ *   pointer to matching child string
+ *----------------------------------------------------------------------------*/
+
+static const char *
+_get_profile_child_str(cs_tree_node_t  *tn,
+                       const char      *child_name)
+{
+  const char *name = cs_tree_node_get_child_value_str(tn, child_name);
+
+  if (name == NULL) {
+    cs_base_warn(__FILE__, __LINE__);
+    bft_printf(_("Incorrect setup tree definition for the following node:\n"));
+    cs_tree_dump(CS_LOG_DEFAULT, 2, tn);
+    bft_error(__FILE__, __LINE__, 0,
+              _("Missing child node: %s"), child_name);
+  }
+
+  return name;
+}
+
+/*----------------------------------------------------------------------------
+ * Return an array of integers associated with a child node for a profile.
+ *
+ * If the matching child node is not present, an error is produced
+ *
+ * parameters:
+ *   tn <-- tree node associated with profile
+ *
+ * return:
+ *   pointer to matching child values
+ *----------------------------------------------------------------------------*/
+
+static const int *
+_get_profile_child_int(cs_tree_node_t  *tn,
+                       const char      *child_name)
+{
+  const int *v = cs_tree_node_get_child_values_int(tn, child_name);
+
+  if (v == NULL) {
+    cs_base_warn(__FILE__, __LINE__);
+    bft_printf(_("Incorrect setup tree definition for the following node:\n"));
+    cs_tree_dump(CS_LOG_DEFAULT, 2, tn);
+    bft_error(__FILE__, __LINE__, 0,
+              _("Missing child node: %s"), child_name);
+  }
+
+  return v;
+}
+
+/*----------------------------------------------------------------------------
+ * Get output format for 1D profile
+ *
+ * parameters:
+ *   tn <-- tree node associated with profile
+ *
+ * return:
+ *   1 for CSV, 0 for DAT
+ *----------------------------------------------------------------------------*/
+
+static int
+_get_profile_format(cs_tree_node_t  *tn)
+{
+  int format = 0;
+
+  const char *format_s
+    = cs_tree_node_get_tag(cs_tree_node_get_child(tn, "format"),
+                           "name");
+
+  if (format_s != NULL) {
+    if (cs_gui_strcmp(format_s, "CSV"))
+      format = 1;
+    else if (cs_gui_strcmp(format_s, "DAT"))
+      format = 0;
+    else
+      bft_error(__FILE__, __LINE__, 0,
+                _("Invalid profile format: %s"), format_s);
+  }
+
+  return format;
+}
+
+/*----------------------------------------------------------------------------
+ * Return the component of variables or properties or scalar for 1D profile
+ *
+ * parameters:
+ *   tn <-- tree node associated with profile variable
+ *----------------------------------------------------------------------------*/
+
+static int
+_get_profile_v_component(cs_tree_node_t  *tn)
+{
+  int comp_id = -1;
+
+  const char *c_name = cs_tree_node_get_tag(tn, "component");
+
+  if (c_name != NULL) {
+    int n = sscanf(c_name, "%d", &comp_id);
+    if (n != 1)
+      bft_error(__FILE__, __LINE__, 0,
+                _("Error converting profile component tag %s to integer."),
+                c_name);
+  }
+
+  return comp_id;
+}
+
+/*----------------------------------------------------------------------------
+ * Return associated field for a node referring to a field.
+ *
+ * A node referring to a field must have a "name" tag (i.e. child with a
+ * string value).
+ *
+ * In the case of NEPTUNE_CFD, an additional "field_id" tag requiring
+ * addition of the "_<field_id>" extension to match the field's name
+ * may be present, so this is tested also.
+ *
+ * parameters:
+ *   tn   <-- tree node associated with profile variable
+ *   name <-- value of node's "name" tag (already determined)
+ *
+ * return:
+ *   pointer to field if match, NULL otherwise
+ *----------------------------------------------------------------------------*/
+
+static const cs_field_t *
+_tree_node_get_field(cs_tree_node_t  *tn)
+{
+  const cs_field_t *f = NULL;
+
+  const char *name = cs_gui_node_get_tag(tn, "name");
+  const char *id_name = cs_tree_node_get_tag(tn, "field_id");
+
+  /* Handle phase id (field_id tag in xml) for NEPTUNE_CFD */
+
+  if (id_name != NULL) {
+    if (strcmp(id_name, "none") != 0) {
+      char buffer[128];
+      snprintf(buffer, 127, "%s_%s", name, id_name);
+      buffer[127] = '\0';
+      if (strlen(buffer) >= 127)
+        bft_error(__FILE__, __LINE__, 0,
+                  "Local buffer too small to assemble field name with:\n"
+                  "name: %s\n"
+                  "field_id: %s\n", name, id_name);
+      f = cs_field_by_name_try(buffer);
+    }
+  }
+
+  if (f == NULL) {
+
+    /* Handle segregated Reynolds tensor solver */
+    if (strcmp(name, "rij") == 0) {
+      int idim = _get_profile_v_component(tn);
+      f = cs_field_by_name_try(_rij_c_names[idim]);
+    }
+
+    /* Fix time step output */
+    else if (strcmp(name, "local_time_step") == 0)
+      f = CS_F_(dt);
+
+    /* General case */
+    else
+      f = cs_field_by_name_try(name);
+
+  }
+
+  if (f == NULL)
+    bft_error(__FILE__, __LINE__, 0,
+              _("Field with name \"%s\" not found"), name);
+
+  return f;
+}
+
+/*----------------------------------------------------------------------------
+ * Profile definitions
+ *----------------------------------------------------------------------------*/
+
+static void
+_define_profiles(void)
+{
+  /* Loop on 1D profile definitions */
+
+  int profile_id = 0;
+
+  const char path0[] = "analysis_control/profiles/profile";
+
+  int n_writers = 0;
+
+  int        *w_i_vals = NULL;
+  cs_real_t  *w_r_vals = NULL;
+
+  int writer_id_start = cs_post_get_free_writer_id();
+
+  const char *format_name[] = {"dat", "csv"};
+
+  for (cs_tree_node_t *tn = cs_tree_get_node(cs_glob_tree, path0);
+       tn != NULL;
+       tn = cs_tree_node_get_next_of_name(tn), profile_id++) {
+
+    const char *name = cs_gui_node_get_tag(tn, "label");
+
+    /* for each profile, check the output frequency */
+
+    int writer_id = 0;
+    int output_format = _get_profile_format(tn);
+    int output_at_end = 0;
+    int output_frequency = -1;
+    cs_real_t time_output = -1.;
+
+    const char *output_type = _get_profile_child_str(tn, "output_type");
+    if (cs_gui_strcmp(output_type, "time_value")) {
+      const cs_real_t *v
+        = cs_tree_node_get_child_values_real(tn, "output_frequency");
+      if (v != NULL)
+        time_output = v[0];
+    }
+    else if (cs_gui_strcmp(output_type, "frequency")) {
+      const int *v
+        = cs_tree_node_get_child_values_int(tn, "output_frequency");
+      if (v != NULL)
+        output_frequency = v[0];
+      else
+        output_frequency = 1;
+    }
+    else if (cs_gui_strcmp(output_type, "end")) {
+      output_at_end = true;
+    }
+
+    /* Check if the output frequency matches an existing writer id;
+       create a writer if necessary */
+
+    for (int i = 0; i < n_writers; i++) {
+      if (   w_i_vals[i*3]   == output_format
+          && w_i_vals[i*3+1] == output_at_end
+          && w_i_vals[i*3+2] == output_frequency
+          && fabs(w_r_vals[i] - time_output) < cs_math_epzero) {
+        writer_id = writer_id_start - i;
+        break;
+      }
+    }
+
+    if (writer_id == 0) {  /* Add new writer */
+
+      int i = n_writers;
+
+      n_writers += 1;
+      BFT_REALLOC(w_i_vals, n_writers*3, int);
+      BFT_REALLOC(w_r_vals, n_writers, cs_real_t);
+      w_i_vals[i*3] = output_format;
+      w_i_vals[i*3+1] = output_at_end;
+      w_i_vals[i*3+2] = output_frequency;
+      w_r_vals[i] = time_output;
+      writer_id = writer_id_start - i;
+
+      bool _output_at_end = (output_at_end) ? true : false;
+
+      cs_post_define_writer(writer_id,
+                            "",
+                            "profiles",
+                            "plot",
+                            format_name[output_format],
+                            FVM_WRITER_FIXED_MESH,
+                            false, /* output_at_start */
+                            _output_at_end,
+                            output_frequency,
+                            time_output);
+
+    }
+
+    int n_coords = 0;
+    const int *v_np = _get_profile_child_int(tn, "points");
+    if (v_np != NULL)
+      n_coords = v_np[0];
+
+    cs_real_3_t *coords;
+    BFT_MALLOC(coords, n_coords, cs_real_3_t);
+
+    cs_meg_post_profiles(name, n_coords, coords);
+
+    cs_probe_set_t *pset
+      = cs_probe_set_create_from_array(name,
+                                       n_coords,
+                                       coords,
+                                       NULL);
+
+    BFT_FREE(coords);
+
+    cs_probe_set_assign_curvilinear_abscissa(pset, NULL);
+
+    int writer_ids[1] = {writer_id};
+    cs_probe_set_associate_writers(pset, 1, writer_ids);
+
+    cs_probe_set_auto_var(pset, false);
+    cs_probe_set_snap_mode(pset, CS_PROBE_SNAP_ELT_CENTER);
+
+    /* TODO: add global flag to mesh to indicate whether it
+       is fixed or not, so as to set transient_location
+       to false when not required */
+
+    cs_probe_set_option(pset, "transient_location", "true");
+
+    /* Output coordinates */
+
+    cs_probe_set_auto_curvilinear_coords(pset, true);
+    cs_probe_set_auto_cartesian_coords(pset, true);
+
+    /* Associate fields */
+
+    for (cs_tree_node_t *tn_vp = cs_tree_node_get_child(tn, "var_prop");
+         tn_vp != NULL;
+         tn_vp = cs_tree_node_get_next_of_name(tn_vp)) {
+
+      const cs_field_t *f = _tree_node_get_field(tn_vp);
+      int comp_id = _get_profile_v_component(tn_vp);
+
+      cs_probe_set_associate_field(pset,
+                                   writer_id,
+                                   f->id,
+                                   comp_id);
+
+    }
+
+  }
+
+  BFT_FREE(w_i_vals);
+  BFT_FREE(w_r_vals);
 }
 
 /*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
@@ -507,6 +847,13 @@ cs_gui_postprocess_meshes(void)
                         false,                   /* output_at_end */
                         frequency_n,
                         frequency_t);
+
+  /* Profile definitions;
+     note that this may lead to additional writer definitions, as
+     the GUI does not yet present profiles in a consistent
+     writer/mesh logic (TODO) */
+
+  _define_profiles();
 }
 
 /*----------------------------------------------------------------------------

src/base/caltri.f90
@@ -178,14 +178,6 @@ interface
 
   !=============================================================================
 
-  subroutine cs_gui_profile_output()  &
-    bind(C, name='cs_gui_profile_output')
-    use, intrinsic :: iso_c_binding
-    implicit none
-  end subroutine cs_gui_profile_output
-
-  !=============================================================================
-
   subroutine cs_restart_map_build()  &
     bind(C, name='cs_restart_map_build')
     use, intrinsic :: iso_c_binding
@@ -945,7 +937,6 @@ if (itrale.gt.0) then
 
   ! 1D profiles postprocessing output
 
-  call cs_gui_profile_output()
   call uiexop()
 
   call cs_f_user_extra_operations(nvar, nscal, dt)

src/base/cs_post.h
@@ -625,6 +625,30 @@ cs_post_mesh_detach_writer(int  mesh_id,
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief Associate a writer to a postprocessing mesh.
+ *
+ * This function must be called during the postprocessing output definition
+ * stage, before any output actually occurs.
+ *
+ * If the field should already be output automatically based on the mesh
+ * category and field output keywords, it will not be added.
+ *
+ * \param[in]  mesh_id    id of associated mesh
+ * \param[in]  writer_id  id of specified associated writer,
+ *                        or \ref CS_POST_WRITER_ALL_ASSOCIATED for all
+ * \param[in]  field_id   id of field to attach
+ * \param[in]  comp_id    id of field component (-1 for all)
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_post_mesh_attach_field(int  mesh_id,
+                          int  writer_id,
+                          int  field_id,
+                          int  comp_id);
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief Get a postprocessing meshes entity presence flag.
  *
  * This flag is an array of 5 integers, indicating the presence of elements

src/base/cs_probe.c
@@ -87,8 +87,10 @@ BEGIN_C_DECLS
 #define CS_PROBE_TRANSIENT   (1 << 0) //   1: locations of probes may change
 #define CS_PROBE_BOUNDARY    (1 << 1) //   2: locations only on the border mesh
 #define CS_PROBE_ON_CURVE    (1 << 2) //   4: locations are on a curve
-#define CS_PROBE_AUTO_VAR    (1 << 3) //   8: automatic output of variables
-#define CS_PROBE_OVERWRITE   (1 << 4) //  16: allow re-creation of probe set
+#define CS_PROBE_OVERWRITE   (1 << 3) //   8: allow re-creation of probe set
+#define CS_PROBE_AUTO_VAR    (1 << 4) //  16: automatic output of variables
+#define CS_PROBE_AUTO_S      (1 << 5) //  32: output curvilinear coordinates
+#define CS_PROBE_AUTO_COORD  (1 << 6) //  64: output cartesian coordinates
 
 /*=============================================================================
  * Local Structure Definitions
@@ -141,6 +143,13 @@ struct _cs_probe_set_t {
 
   int           n_writers;      /* Number of writers (-1 if unset) */
   int          *writer_ids;     /* List of writer ids */
+
+  /* User-defined fields associated to this set of probes */
+
+  int           n_fields;       /* Number of associated fields in list */
+  int           n_max_fields;   /* Maximum number of associated fields in list */
+  int          *field_info;     /* List of associated field tuples:
+                                   (writer_id, field_id, component_id) */
 
 };
 
@@ -291,6 +300,9 @@ _probe_set_free(cs_probe_set_t   *pset)
 
   if (pset->n_writers > 0)
     BFT_FREE(pset->writer_ids);
+
+  if (pset->n_fields > 0)
+    BFT_FREE(pset->field_info);
 }
 
 /*----------------------------------------------------------------------------*/
@@ -362,6 +374,10 @@ _probe_set_create(const char    *name,
 
   pset->n_writers = -1;
   pset->writer_ids = NULL;
+
+  pset->n_fields = 0;
+  pset->n_max_fields = 0;
+  pset->field_info = NULL;
 
   return pset;
 }
@@ -435,10 +451,11 @@ _build_local_probe_set(cs_probe_set_t  *pset)
   pset->s_coords = s;
 }
 
-/*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
-
 /*============================================================================
- * Public function definitions
+ * Semi-private function definitions
+ *
+ * The following functions are intended to be used by the postprocessing layer
+ * (cs_post.c), not directly by the user.
  *============================================================================*/
 
 /*----------------------------------------------------------------------------*/
@@ -459,6 +476,42 @@ cs_probe_finalize(void)
   _n_probe_sets = 0;
   BFT_FREE(_probe_set_array);
 }
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Transfer info on associated fields to the caller.
+ *
+ * This function transfert the property of the associated arrays to the caller
+ * and removes it from the probe set info.
+ *
+ * \param[in]   pset        pointer to a cs_probe_set_t structure
+ * \param[out]  n_fields    number of associated fields
+ * \param[out]  field_info  associated field info (array of tuples with
+ *                          writer id, field id, component id)
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_probe_set_transfer_associated_field_info(cs_probe_set_t   *pset,
+                                            int              *n_fields,
+                                            int             **field_info)
+{
+  if (pset == NULL)
+    bft_error(__FILE__, __LINE__, 0, _(_err_empty_pset));
+
+  *n_fields = pset->n_fields;
+  *field_info = pset->field_info;
+
+  pset->n_fields = 0;
+  pset->n_max_fields = 0;
+  pset->field_info = NULL;
+}
+
+/*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
+
+/*============================================================================
+ * Public function definitions
+ *============================================================================*/
 
 /*----------------------------------------------------------------------------*/
 /*!
@@ -554,6 +607,8 @@ cs_probe_set_get_name(cs_probe_set_t   *pset)
  * \param[out] on_boundary     true if probes are located on boundary
  * \param[out] on_curve        true if the probe set has cuvilinear coordinates
  * \param[out] auto_variables  true if set of variables to output is predefined
+ * \param[out] auto_curve_coo  true if curvilinear coordinates should be output
+ * \param[out] auto_cart_coo   true if cartesian coordinates should be output
  * \param[out] n_writers       number of associated  user-defined writers,
  *                             or -1 if default unchanged
  * \param[out] writer_ids      pointer to a list of writer ids
@@ -566,6 +621,8 @@ cs_probe_set_get_post_info(const cs_probe_set_t   *pset,
                            bool                   *on_boundary,
                            bool                   *on_curve,
                            bool                   *auto_variables,
+                           bool                   *auto_curve_coo,
+                           bool                   *auto_cart_coo,
                            int                    *n_writers,
                            int                    *writer_ids[])
 {
@@ -576,6 +633,10 @@ cs_probe_set_get_post_info(const cs_probe_set_t   *pset,
     *time_varying = (pset->flags & CS_PROBE_TRANSIENT) ? true : false;
   if (auto_variables != NULL)
     *auto_variables = (pset->flags & CS_PROBE_AUTO_VAR) ? true: false;
+  if (auto_curve_coo != NULL)
+    *auto_curve_coo = (pset->flags & CS_PROBE_AUTO_S) ? true: false;
+  if (auto_cart_coo != NULL)
+    *auto_cart_coo = (pset->flags & CS_PROBE_AUTO_COORD) ? true: false;
   if (on_curve != NULL)
     *on_curve = (pset->flags & CS_PROBE_ON_CURVE) ? true : false;
   if (on_boundary != NULL)
@@ -835,6 +896,57 @@ cs_probe_set_allow_overwrite(const char  *name)
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief  Assign curvilinear abscissa for the given probe set
+ *
+ * This is effective only when using probe sets to which curvilinear
+ * coordinates have not already been assigned.
+ *
+ * So this may be used following \ref cs_probe_set_create (and probe additions)
+ * or \ref cs_probe_set_create_from_array, but will be ignored for probe sets
+ * defined using \ref cs_probe_set_create_from_segment and
+ * \ref cs_probe_set_create_from_local.
+ *
+ * If provided, the array of curvilinear absicssa must match the size of the
+ * probe set. If set to NULL, it is assumed a unifor spacing is provided
+ * between points.
+ *
+ * \param[in]  pset  pointer to a cs_probe_set_t structure
+ * \param[in]  s     array of curvilinear abscissa, or NULL
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_probe_set_assign_curvilinear_abscissa(cs_probe_set_t   *pset,
+                                         const cs_real_t  *s)
+{
+  if (pset == NULL)
+    return;
+
+  if (pset->flags & CS_PROBE_ON_CURVE)
+    return;
+
+  pset->flags |= CS_PROBE_ON_CURVE;
+
+  BFT_REALLOC(pset->s_coords, pset->n_probes, cs_real_t);
+
+  if (s != NULL) {
+    for (int i = 0; i < pset->n_probes; i++)
+      pset->s_coords[i] = s[i];
+  }
+  else if (pset->n_probes > 0) {
+    pset->s_coords[0] = 0.;
+    if (pset->n_probes > 1) {
+      int npm1 = pset->n_probes - 1;
+      cs_real_t a = 1./(cs_real_t)npm1;
+      for (int i = 1; i < npm1; i++)
+        pset->s_coords[i] = i*a;;
+      pset->s_coords[npm1] = 1.;
+    }
+  }
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief  Associate a list of writers to a probe set.
  *
  * \param[in, out] pset        pointer to a cs_probe_set_t structure to set
@@ -864,6 +976,50 @@ cs_probe_set_associate_writers(cs_probe_set_t   *pset,
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief Associate a field to a probe set.
+ *
+ * This function must be called during the postprocessing output definition
+ * stage, before any output actually occurs.
+ *
+ * If the field should already be output automatically based on the mesh
+ * category and field output keywords, it will be filtered at a later stage.
+ *
+ * \param[in]  pset       pointer to a cs_probe_set_t structure
+ * \param[in]  writer_id  id of specified associated writer,
+ *                        or 0 (\ref CS_POST_WRITER_ALL_ASSOCIATED) for all
+ * \param[in]  field_id   id of field to attach
+ * \param[in]  comp_id    id of field component (-1 for all)
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_probe_set_associate_field(cs_probe_set_t   *pset,
+                             int               writer_id,
+                             int               field_id,
+                             int               comp_id)
+{
+  if (pset == NULL)
+    bft_error(__FILE__, __LINE__, 0, _(_err_empty_pset));
+
+  if (pset->n_max_fields <= pset->n_fields) {
+    if (pset->n_max_fields == 0)
+      pset->n_max_fields = 8;
+    else
+      pset->n_max_fields *= 2;
+    BFT_REALLOC(pset->field_info, 3*(pset->n_max_fields), int);
+  }
+
+  int *afi = pset->field_info + 3*pset->n_fields;
+
+  afi[0] = writer_id;
+  afi[1] = field_id;
+  afi[2] = comp_id;
+
+  pset->n_fields += 1;
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief  Set to true or false the automatic post-processing of variables
  *
  * \param[in, out] pset     pointer to a cs_probe_set_t structure
@@ -884,6 +1040,54 @@ cs_probe_set_auto_var(cs_probe_set_t   *pset,
   }
   else
     pset->flags |= CS_PROBE_AUTO_VAR;
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Set automatic output of curvilinear coordinates.
+ *
+ * \param[in, out] pset     pointer to a cs_probe_set_t structure
+ * \param[in]      mode     true or false
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_probe_set_auto_curvilinear_coords(cs_probe_set_t   *pset,
+                                     bool              mode)
+{
+  if (pset == NULL)
+    bft_error(__FILE__, __LINE__, 0, _(_err_empty_pset));
+
+  if (mode == false) {
+    if (pset->flags & CS_PROBE_AUTO_S)
+      pset->flags -= CS_PROBE_AUTO_S;
+  }
+  else
+    pset->flags |= CS_PROBE_AUTO_S;
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Set automatic output of curvilinear coordinates.
+ *
+ * \param[in, out] pset     pointer to a cs_probe_set_t structure
+ * \param[in]      mode     true or false
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_probe_set_auto_cartesian_coords(cs_probe_set_t   *pset,
+                                   bool              mode)
+{
+  if (pset == NULL)
+    bft_error(__FILE__, __LINE__, 0, _(_err_empty_pset));
+
+  if (mode == false) {
+    if (pset->flags & CS_PROBE_AUTO_COORD)
+      pset->flags -= CS_PROBE_AUTO_COORD;
+  }
+  else
+    pset->flags |= CS_PROBE_AUTO_COORD;
 }
 
 /*----------------------------------------------------------------------------*/
@@ -1551,7 +1755,6 @@ cs_probe_set_dump(const cs_probe_set_t   *pset)
     bft_printf("\n");
 
   }
-
 }
 
 /*----------------------------------------------------------------------------*/

commit 8105e61e33a861c00439995b757f54cadd497616

Fix handling of profiles for Rij in some XML configurations.
The issue was introduced in commit 353ec9d0 (May 13 2020).

src/gui/cs_gui_output.c
@@ -391,19 +391,21 @@ _tree_node_get_field(cs_tree_node_t  *tn)
 
   if (f == NULL) {
 
-    /* Handle segregated Reynolds tensor solver */
-    if (strcmp(name, "rij") == 0) {
-      int idim = _get_profile_v_component(tn);
-      f = cs_field_by_name_try(_rij_c_names[idim]);
-    }
-
     /* Fix time step output */
-    else if (strcmp(name, "local_time_step") == 0)
+    if (strcmp(name, "local_time_step") == 0)
       f = CS_F_(dt);
 
     /* General case */
     else
       f = cs_field_by_name_try(name);
+
+    /* Handle segregated Reynolds tensor solver */
+    if (f == NULL) {
+      if (strcmp(name, "rij") == 0) {
+        int idim = _get_profile_v_component(tn);
+        f = cs_field_by_name_try(_rij_c_names[idim]);
+      }
+    }
 
   }
 