code-saturne/code_saturne

commit fb3e39fe6bfd256d5679f838b7489ed74c60cc46

Add high-level option to interpolate probe values.
Also improve probe set Doxygen documentation.

src/base/cs_post.c
@@ -2830,48 +2830,6 @@ _boundary_submeshes_by_group(const cs_mesh_t   *mesh,
   BFT_FREE(group_flag);
 }
 
-/*----------------------------------------------------------------------------*/
-/*!
- * \brief Interpolate values from fields defined on cells or faces.
- *
- * \param[in, out]  input           pointer to optional (untyped) value
- *                                  or structure.
- * \param[in]       datatype        associated datatype
- * \param[in]       val_dim         dimension of data values
- * \param[in]       n_points        number of interpolation points
- * \param[in]       point_location  location of points in mesh elements
- * \param[in]       point_coords    point coordinates
- * \param[in]       location_vals   values at mesh location
- * \param[out]      point_vals      interpolated values at points
- */
-/*----------------------------------------------------------------------------*/
-
-static void
-_field_interpolate(void                *input,
-                   cs_datatype_t        datatype,
-                   int                  val_dim,
-                   cs_lnum_t            n_points,
-                   const cs_lnum_t      point_location[],
-                   const cs_real_3_t    point_coords[],
-                   const void          *location_vals,
-                   void                *point_vals)
-{
-  CS_UNUSED(input);
-
-  /* int f_id = *((int *)input); */
-
-  /* TODO add options here */
-
-  cs_interpolate_from_location_p0(NULL,
-                                  datatype,
-                                  val_dim,
-                                  n_points,
-                                  point_location,
-                                  point_coords,
-                                  location_vals,
-                                  point_vals);
-}
-
 /*----------------------------------------------------------------------------
  * Check if a given field matches the profile for post_write_var.
  *
@@ -3068,6 +3026,15 @@ static void
 _cs_post_output_fields(cs_post_mesh_t        *post_mesh,
                        const cs_time_step_t  *ts)
 {
+  cs_interpolate_from_location_t
+    *interpolate_func = cs_interpolate_from_location_p0;
+
+  cs_probe_set_t  *pset = (cs_probe_set_t *)post_mesh->sel_input[4];
+  if (pset != NULL) {
+    if (cs_probe_set_get_interpolation(pset) == 1)
+      interpolate_func = cs_interpolate_from_location_p1;
+  }
+
   /* Base output for cell and boundary meshes */
   /*------------------------------------------*/
 
@@ -3100,9 +3067,25 @@ _cs_post_output_fields(cs_post_mesh_t        *post_mesh,
       if (name == NULL)
         name = f->name;
 
-      if (   field_loc_type == CS_MESH_LOCATION_CELLS
-          || field_loc_type == CS_MESH_LOCATION_BOUNDARY_FACES
-          || field_loc_type == CS_MESH_LOCATION_INTERIOR_FACES) {
+      if (pset != NULL) {
+        char interpolate_input[96];
+        strncpy(interpolate_input, f->name, 95); interpolate_input[95] = '\0';
+
+        cs_post_write_probe_values(post_mesh->id,
+                                   CS_POST_WRITER_ALL_ASSOCIATED,
+                                   name,
+                                   f->dim,
+                                   CS_POST_TYPE_cs_real_t,
+                                   f->location_id,
+                                   interpolate_func,
+                                   interpolate_input,
+                                   f->val,
+                                   ts);
+      }
+
+      else if (   field_loc_type == CS_MESH_LOCATION_CELLS
+               || field_loc_type == CS_MESH_LOCATION_BOUNDARY_FACES
+               || field_loc_type == CS_MESH_LOCATION_INTERIOR_FACES) {
 
         const cs_real_t *cell_val = NULL, *b_face_val = NULL;
 
@@ -3169,14 +3152,17 @@ _cs_post_output_fields(cs_post_mesh_t        *post_mesh,
       if (name == NULL)
         name = f->name;
 
+      char interpolate_input[96];
+      strncpy(interpolate_input, f->name, 95); interpolate_input[95] = '\0';
+
       cs_post_write_probe_values(post_mesh->id,
                                  CS_POST_WRITER_ALL_ASSOCIATED,
                                  name,
                                  f->dim,
                                  CS_POST_TYPE_cs_real_t,
                                  f->location_id,
-                                 _field_interpolate,
-                                 &f_id,
+                                 interpolate_func,
+                                 interpolate_input,
                                  f->val,
                                  ts);
 
@@ -3241,8 +3227,16 @@ static void
 _cs_post_output_attached_fields(cs_post_mesh_t        *post_mesh,
                                 const cs_time_step_t  *ts)
 {
-  cs_probe_set_t  *pset = (cs_probe_set_t *)post_mesh->sel_input[4];
   const int label_key_id = cs_field_key_id("label");
+
+  cs_probe_set_t  *pset = (cs_probe_set_t *)post_mesh->sel_input[4];
+  cs_interpolate_from_location_t
+    *interpolate_func = cs_interpolate_from_location_p0;
+
+  if (pset != NULL) {
+    if (cs_probe_set_get_interpolation(pset) == 1)
+      interpolate_func = cs_interpolate_from_location_p1;
+  }
 
   for (int i = 0; i < post_mesh->n_a_fields; i++) {
 
@@ -3263,7 +3257,9 @@ _cs_post_output_attached_fields(cs_post_mesh_t        *post_mesh,
     if (name == NULL)
       name = f->name;
 
-    int fc_id = f_id;
+    cs_interpolate_from_location_t
+      *_interpolate_func = interpolate_func;
+
     int f_dim = f->dim;
     cs_real_t *_val = NULL;
     const cs_real_t *f_val = f->val;
@@ -3273,10 +3269,10 @@ _cs_post_output_attached_fields(cs_post_mesh_t        *post_mesh,
         continue;
       else {
         _val = _extract_field_component(f, comp_id, name, name_buf);
-        fc_id = -1;
         f_dim = 1;
         f_val = _val;
         name = name_buf;
+        _interpolate_func = cs_interpolate_from_location_p0;
       }
     }
 
@@ -3328,14 +3324,17 @@ _cs_post_output_attached_fields(cs_post_mesh_t        *post_mesh,
                  || field_loc_type == CS_MESH_LOCATION_BOUNDARY_FACES
                  || field_loc_type == CS_MESH_LOCATION_VERTICES)) {
 
+      char interpolate_input[96];
+      strncpy(interpolate_input, f->name, 95); interpolate_input[95] = '\0';
+
       cs_post_write_probe_values(post_mesh->id,
                                  writer_id,
                                  name,
                                  f_dim,
                                  CS_POST_TYPE_cs_real_t,
                                  f->location_id,
-                                 _field_interpolate,
-                                 &fc_id,
+                                 _interpolate_func,
+                                 interpolate_input,
                                  f_val,
                                  ts);
 

docs/doxygen/examples/cs_user_postprocess.dox
@@ -383,6 +383,12 @@
 
   \snippet cs_user_postprocess.c post_define_probes_1
 
+  The same set could also be defined using predifined arrays:
+
+  \snippet cs_user_postprocess.c post_define_probes_2
+
+  In both cases, labels are optional (NULL may be used instead).
+
   Probe set creation functions return a pointer to a \ref cs_probe_set_t
   structure which can be used to specify additional options using the
   \ref cs_probe_set_option function.
@@ -390,6 +396,62 @@
   A writer (id = CS_POST_WRITER_PROBES) using the format "time_plot" is
   associated by default to a set of monitoring probes.
   This is not the case for profiles.
+
+  Profiles are special probe sets, for which a curvilinear coordinate is
+  usually provided (or deduced, when \ref cs_probe_set_create_from_segment
+  or \ref cs_probe_set_create_from_local construction function is used).
+  Curvilinear coordinates may also be assigned to probe sets generated
+  using \ref cs_probe_set_create or \cs_probe_set_create_from_array
+  by using the \ref cs_probe_set_assign_curvilinear_abscissa function.
+
+  Instead of being assigned by default to a time plot, profiles are generally
+  used with writers using the "plot" format (with a "dat" or "csv" sub-option).
+  Instead of using one file per variable with one column per point and
+  one line per output time, this produces one file pour output time, with
+  une column per variable and one line per point. But they may also be used
+  with time plots (as a regular probe set), or as with any post-processing
+  output mesh, with 3D formats such as EnSight or MED.
+
+  The following example shows how to simply define a profile on a given
+  segment, and associate it to writer 2:
+
+  \snippet cs_user_postprocess.c post_define_profile_1
+
+  Probe sets (including profiles) may also be located along the domain
+  boundary, if this is specified. In the following example, we set the
+  "boundary" option to "true" to specify that the probes should be located
+  on the boundary instead of the default volume mesh. We could also set
+  a "criteria" option to define a selection filter for the portion of the mesh
+  in which probes should be located (by default, the whole mesh).
+
+  Here We also choose to "snap" the points to the nearest vertices. Snapping
+  probes to cell centers or vertices is useful mainly when the values to
+  plot are located on those points.
+
+  \snippet cs_user_postprocess.c post_define_profile_2
+
+  \section cs_user_postprocess_h_profile_vars Defining output on a probe set
+
+  To select a specific set of fields to output with a given probe set
+  or profile, the \ref cs_probe_set_associate_field function may be used.
+  It allows specifying whether the output relates to all associated writers
+  or specific ones, and for multidimensional fields, allows selecting specific
+  components (\comp_id = -1 for all components, or the (0 to n-1)-th
+  component if filtering is desired.
+
+  This is used in general for probe sets whose "automatic" variables
+  output is turned off (using \cs_probe_set_auto_var), though it can be used
+  to extend that glob setting.
+
+  Finally, the user may choose to output the curvilinear coordinates
+  associated with a profile and/or the point coordinates, using the
+  \ref cs_probe_set_auto_curvilinear_coords and
+  \ref cs_probe_set_auto_cartesian_coords respectively, since these are
+  not output by default.
+
+  This last example illustrates a combination of those possibilities:
+
+  \snippet cs_user_postprocess.c post_define_profile_2
 
   \section cs_user_postprocess_h_profile_advanced Advanced profile definitions
 
@@ -444,6 +506,27 @@
 
   The examples of post-processing given below use meshes defined in the
   examples for \ref cs_user_postprocess_meshes above.
+
+  \subsection cs_user_postprocess_h_var_add_f_1 Output a given field or component with a given mesh
+
+  To output specific field values on a given postprocessing mesh, that
+  field may be attached (associated) to that mesh.
+
+  In the following example, the pressure is output on mesh *4*, for
+  all associated writers:
+
+  \snippet cs_user_postprocess.c post_attach_field_1
+
+  In this second example, the velocity's z component is ouput on mesh 4,
+  only for writer 1:
+
+  \snippet cs_user_postprocess.c post_attach_field_2
+
+  If the mesh has the \c auto_variables option enabled, and a field is already
+  selected for "standard" postprocessing (i.e. selected for output in the GUI,
+  or \c post_vis key value includes the \c CS_POST_ON_LOCATION bit), it will
+  not be output an additional time, so this option should be safely usable even
+  with limited precautions.
 
   \subsection cs_user_postprocess_h_volume_mesh_tke Output of the turbulent kinetic energy for the Rij-Epsilon model on the volume mesh
 
@@ -511,6 +594,8 @@
 
   \page cs_user_postprocess_h_interpolate_p Interpolation
 
+  \section cs_user_postprocess_h_probe_interpolate Probe set interpolation
+
   By default, probes and profile values are "P0" interpolated,
   that is their value is that of the containing cell or face,
   or closest vertex.
@@ -519,7 +604,7 @@
 
   The P1 interpolation is based on a local least-squares gradient,
   so ghost cell values must be synchronized  (this is automatically
-  the case for fields, must must be handled by the user in case of
+  the case for fields, but must be handled by the user in case of
   auxiliary arrays).
 
   If a field's boundary values (i.e. associated field) are known,
@@ -527,10 +612,26 @@
   are defined, they are used. When neither boundary values nor boundary
   conditions are known (i.e. for a non-solved variable field or
   values not defined as a field), homogeneous Neumann boundary conditions
-  are assumed.
+  are assumed. Improved gradient interpolation for stratification and
+  "jumps" is also ignored here, so this may lead to excessively smooth
+  output in areas where this should occur.
 
-  The following example shows how intepolation may be used in
-  the \ref cs_user_postprocess_values function.
+  Interpolation may be activated in a global manner for all outputs
+  using a given probe set as by setting the set's "interpolate", option to 1,
+  as shown on the following example. The "probes" set configured here
+  is the default probe set defined using the GUI:
+
+  \snippet cs_user_postprocess.c post_set_probes_interpolate
+
+  If a finer control is needed, such as applying a different interpolation
+  option to selected fields, or providing a more precice (but usually
+  more costly interpolation, interpolation may also be defined on a case per
+  case basis using the \ref cs_user_postprocess_values function.
+
+  The following example shows how this may be done, using the built-in
+  \ref cs_interpolate_from_location_p1 function (which is based on the
+  "P1" interpolation described above) by passing its pointer to the
+  to \ref cs_post_write_probe_values outout function.
 
   \snippet cs_user_postprocess-probes-interpolate.c post_probes_interpolate_var_1
 
@@ -543,6 +644,10 @@
 
   Note also that interpolation could be also used in
   some \ref cs_user_extra_operations cases.
+
+  If needed, users may also define their own interpolation functions,
+  whose arguments should match \ref cs_interpolate_from_location_t and pass the
+  appropriate pointer to \ref cs_post_write_probe_values.
 
   \page cs_user_postprocess_h_activation_p Advanced management of output times
 

src/base/cs_probe.c
@@ -139,6 +139,9 @@ struct _cs_probe_set_t {
 
   char         *located;        /* 1 for located probes, 0 for unlocated */
 
+  int           interpolation;  /* 0: no interpolation;
+                                   1: local gradient-based interpolation */
+
   /* User-defined writers associated to this set of probes */
 
   int           n_writers;      /* Number of writers (-1 if unset) */
@@ -161,6 +164,7 @@ typedef enum {
   PSETKEY_BOUNDARY,
   PSETKEY_SELECT_CRIT,
   PSETKEY_TOLERANCE,
+  PSETKEY_INTERPOLATION,
   PSETKEY_ERROR
 
 } psetkey_t;
@@ -208,6 +212,8 @@ _print_psetkey(psetkey_t  key)
     return "selection_criteria";
   case PSETKEY_TOLERANCE:
     return "tolerance";
+  case PSETKEY_INTERPOLATION:
+    return "interpolation";
   default:
     assert(0);
   }
@@ -239,6 +245,8 @@ _get_psetkey(const char  *keyname)
     key = PSETKEY_SELECT_CRIT;
   else if (strcmp(keyname, "tolerance") == 0)
     key = PSETKEY_TOLERANCE;
+  else if (strcmp(keyname, "interpolation") == 0)
+    key = PSETKEY_INTERPOLATION;
 
   return key;
 }
@@ -371,6 +379,8 @@ _probe_set_create(const char    *name,
   pset->elt_id = NULL;
   pset->vtx_id = NULL;
   pset->located = NULL;
+
+  pset->interpolation = 0;
 
   pset->n_writers = -1;
   pset->writer_ids = NULL;
@@ -666,6 +676,26 @@ cs_probe_set_get_location_criteria(cs_probe_set_t   *pset)
     return  NULL;
 
   return pset->sel_criter;
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Return the interpolation option for a given probe set
+ *
+ * Interpolation will be applied only where possible.
+ *
+ * \param[in]   pset       pointer to a cs_probe_set_t structure
+ *
+ * \return interpolation option value
+ */
+/*----------------------------------------------------------------------------*/
+
+int
+cs_probe_set_get_interpolation(cs_probe_set_t   *pset) {
+  if (pset == NULL)
+    return  0;
+
+  return pset->interpolation;
 }
 
 /*----------------------------------------------------------------------------*/
@@ -1124,6 +1154,10 @@ cs_probe_set_snap_mode(cs_probe_set_t   *pset,
  *
  * - \c \b selection_criteria where keyval is selection criteria string
  * - \c \b tolerance  where keyval is for instance "0.05" (default "0.10")
+ * - \c \b interpolation if \ c 0, P0 interpolation (default); if \c 1,
+ *         simple gradient-based interpolation for volume probes (ignored
+ *         for boundaries). Other interpolation options might be added
+ *         in the future.
  *
  * \param[in, out] pset     pointer to a cs_probe_set_t structure to set
  * \param[in]      keyname  name of the keyword related to the parameter to set
@@ -1194,6 +1228,10 @@ cs_probe_set_option(cs_probe_set_t   *pset,
 
   case PSETKEY_TOLERANCE:
     pset->tolerance = atof(keyval);
+    break;
+
+  case PSETKEY_INTERPOLATION:
+    pset->interpolation = atoi(keyval);
     break;
 
   default:

src/base/cs_probe.h
@@ -230,6 +230,21 @@ cs_probe_set_get_location_criteria(cs_probe_set_t   *pset);
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief  Return the interpolation option for a given probe set
+ *
+ * Interpolation will be applied only where possible.
+ *
+ * \param[in]   pset       pointer to a cs_probe_set_t structure
+ *
+ * \return interpolation option value
+ */
+/*----------------------------------------------------------------------------*/
+
+int
+cs_probe_set_get_interpolation(cs_probe_set_t   *pset);
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief  Create a new set of probes.
  *
  * \param[in]   name        name of the set of probes
@@ -467,6 +482,10 @@ cs_probe_set_snap_mode(cs_probe_set_t   *pset,
  *
  * - \c \b selection_criteria where keyval is selection criteria string
  * - \c \b tolerance  where keyval is for instance "0.05" (default "0.10")
+ * - \c \b interpolation if \ c 0, P0 interpolation (default); if \c 1,
+ *         simple gradient-based interpolation for volume probes (ignored
+ *         for boundaries). Other interpolation options might be added
+ *         in the future.
  *
  * \param[in, out] pset     pointer to a cs_probe_set_t structure to set
  * \param[in]      keyname  name of the keyword related to the parameter to set

src/gui/cs_gui_output.c
@@ -534,7 +534,14 @@ _define_profiles(void)
     cs_probe_set_associate_writers(pset, 1, writer_ids);
 
     cs_probe_set_auto_var(pset, false);
+
+    /* TODO: add option in GUI to select betwen "snap to cell center"
+       and interpolation (or other combinations) */
+#if 1
     cs_probe_set_snap_mode(pset, CS_PROBE_SNAP_ELT_CENTER);
+#else
+    cs_probe_set_option(pset, "interpolation", "1");
+#endif
 
     /* TODO: add global flag to mesh to indicate whether it
        is fixed or not, so as to set transient_location

commit 38c57796e9338097118262f85a11d9b90d6bca66

Postprocessing: fix crash due to incorrect probe/location match test.
Issue seems to have been introduced in commit fb3e39fe (May 13 2020).

src/base/cs_post.c
@@ -3026,13 +3026,22 @@ static void
 _cs_post_output_fields(cs_post_mesh_t        *post_mesh,
                        const cs_time_step_t  *ts)
 {
-  cs_interpolate_from_location_t
-    *interpolate_func = cs_interpolate_from_location_p0;
-
+  int  pset_interpolation = 0;
+  bool pset_on_boundary = false;
   cs_probe_set_t  *pset = (cs_probe_set_t *)post_mesh->sel_input[4];
+
   if (pset != NULL) {
-    if (cs_probe_set_get_interpolation(pset) == 1)
-      interpolate_func = cs_interpolate_from_location_p1;
+    cs_probe_set_get_post_info(pset,
+                               NULL,
+                               &pset_on_boundary,
+                               NULL,
+                               NULL,
+                               NULL,
+                               NULL,
+                               NULL,
+                               NULL);
+    if (pset_on_boundary == false && cs_probe_set_get_interpolation(pset) == 1)
+      pset_interpolation = 1;
   }
 
   /* Base output for cell and boundary meshes */
@@ -3070,6 +3079,12 @@ _cs_post_output_fields(cs_post_mesh_t        *post_mesh,
       if (pset != NULL) {
         char interpolate_input[96];
         strncpy(interpolate_input, f->name, 95); interpolate_input[95] = '\0';
+
+        cs_interpolate_from_location_t
+          *interpolate_func = cs_interpolate_from_location_p0;
+        if (   field_loc_type == CS_MESH_LOCATION_CELLS
+            && pset_interpolation == 1)
+          interpolate_func = cs_interpolate_from_location_p1;
 
         cs_post_write_probe_values(post_mesh->id,
                                    CS_POST_WRITER_ALL_ASSOCIATED,
@@ -3140,10 +3155,17 @@ _cs_post_output_fields(cs_post_mesh_t        *post_mesh,
       const cs_mesh_location_type_t field_loc_type
         = cs_mesh_location_get_type(f->location_id);
 
-      if (   field_loc_type != CS_MESH_LOCATION_CELLS
-          && field_loc_type != CS_MESH_LOCATION_BOUNDARY_FACES
-          && field_loc_type != CS_MESH_LOCATION_VERTICES)
-        continue;
+      if (pset_on_boundary) {
+        if (   field_loc_type != CS_MESH_LOCATION_CELLS
+            && field_loc_type != CS_MESH_LOCATION_BOUNDARY_FACES
+            && field_loc_type != CS_MESH_LOCATION_VERTICES)
+          continue;
+      }
+      else {
+        if (   field_loc_type != CS_MESH_LOCATION_CELLS
+            && field_loc_type != CS_MESH_LOCATION_VERTICES)
+          continue;
+      }
 
       if (! (cs_field_get_key_int(f, vis_key_id) & CS_POST_MONITOR))
         continue;
@@ -3151,6 +3173,12 @@ _cs_post_output_fields(cs_post_mesh_t        *post_mesh,
       const char *name = cs_field_get_key_str(f, label_key_id);
       if (name == NULL)
         name = f->name;
+
+      cs_interpolate_from_location_t
+        *interpolate_func = cs_interpolate_from_location_p0;
+      if (   field_loc_type == CS_MESH_LOCATION_CELLS
+          && pset_interpolation == 1)
+        interpolate_func = cs_interpolate_from_location_p1;
 
       char interpolate_input[96];
       strncpy(interpolate_input, f->name, 95); interpolate_input[95] = '\0';
@@ -3229,12 +3257,23 @@ _cs_post_output_attached_fields(cs_post_mesh_t        *post_mesh,
 {
   const int label_key_id = cs_field_key_id("label");
 
+  bool pset_on_boundary = false;
   cs_probe_set_t  *pset = (cs_probe_set_t *)post_mesh->sel_input[4];
+
   cs_interpolate_from_location_t
     *interpolate_func = cs_interpolate_from_location_p0;
 
   if (pset != NULL) {
-    if (cs_probe_set_get_interpolation(pset) == 1)
+    cs_probe_set_get_post_info(pset,
+                               NULL,
+                               &pset_on_boundary,
+                               NULL,
+                               NULL,
+                               NULL,
+                               NULL,
+                               NULL,
+                               NULL);
+    if (pset_on_boundary == false && cs_probe_set_get_interpolation(pset) == 1)
       interpolate_func = cs_interpolate_from_location_p1;
   }
 
@@ -3323,6 +3362,10 @@ _cs_post_output_attached_fields(cs_post_mesh_t        *post_mesh,
              && (   field_loc_type == CS_MESH_LOCATION_CELLS
                  || field_loc_type == CS_MESH_LOCATION_BOUNDARY_FACES
                  || field_loc_type == CS_MESH_LOCATION_VERTICES)) {
+
+      if (! pset_on_boundary
+          && field_loc_type != CS_MESH_LOCATION_BOUNDARY_FACES)
+        continue;
 
       char interpolate_input[96];
       strncpy(interpolate_input, f->name, 95); interpolate_input[95] = '\0';
@@ -6003,7 +6046,7 @@ cs_post_write_probe_values(int                              mesh_id,
   int nt_cur = (ts != NULL) ? ts->nt_cur : -1;
   double t_cur = (ts != NULL) ? ts->t_cur : 0.;
 
-  bool on_boundary, on_curve;
+  bool on_boundary;
 
   /* Initializations */
 
@@ -6018,7 +6061,7 @@ cs_post_write_probe_values(int                              mesh_id,
   cs_probe_set_get_post_info(pset,
                              NULL,
                              &on_boundary,
-                             &on_curve,
+                             NULL,
                              NULL,
                              NULL,
                              NULL,