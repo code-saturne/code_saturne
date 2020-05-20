code-saturne/code_saturne

commit 8765c7dc0eb174afaa60da18dbb2f29ee30dc9ab

Allow modifying boundary faces so as to ignore some of them in the computation.
This should be useful mostly to speed-up 1d or 2d models where most
boundary faces are symmetry faces and have no contribution to the
Finite Volume scheme.

In this case, we must still take care to remove the selected faces from
the boundary mesh postprocessing output (replacing the "all[]" default
selection criteria with "not symmetry" for example). Doing this
automatically would require being able to rebuid volume cells in another
manner, or managing faces to ignore in another manner (for example
as a special symmetry zone, handled in a more integrated manner).

Currently, only the Green-Gauss based gradient reconstruction
with iterative handling o non-orthogonalities may be used with this,
as the least squares gradients may fail due to insufficient
covariance directions. This could be improved by automatically
adding "cocg" entries in the normal directions when all contributions
are on a same plane or axis, or using the "hidden" symmetry boundary
cells in this step. The first solution woud be preferred as adaptable to
true 1d or 2d meshes.

src/base/cs_preprocess.c
@@ -374,6 +374,9 @@ cs_preprocess_mesh(cs_halo_type_t   halo_type)
   if (need_save)
     cs_mesh_save(cs_glob_mesh, NULL, NULL, "mesh_output.csm");
 
+  cs_glob_mesh->n_b_faces_all = cs_glob_mesh->n_b_faces_all;
+  cs_glob_mesh->n_g_b_faces_all = cs_glob_mesh->n_g_b_faces_all;
+
   /* Destroy the temporary structure used to build the main mesh */
 
   cs_mesh_builder_destroy(&cs_glob_mesh_builder);
@@ -440,6 +443,81 @@ cs_preprocess_mesh(cs_halo_type_t   halo_type)
   /* Re-enable writers disabled when entering this stage */
 
   cs_post_enable_writer(0);
+
+  cs_timer_stats_switch(t_top_id);
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Apply numbering changes to ignore selected boundary faces.
+ *
+ * \param[in, out]  m         pointer to mesh structure
+ * \param[in, out]  mq        pointer to mesh quantities structure
+ * \param[in]       n_faces   number of selected faces
+ * \param[in]       face_ids  ids of selected faces
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_preprocess_mesh_selected_b_faces_ignore(cs_mesh_t             *m,
+                                           cs_mesh_quantities_t  *mq,
+                                           cs_lnum_t              n_faces,
+                                           const cs_lnum_t        face_ids[])
+{
+  double  t1, t2;
+
+  int t_stat_id = cs_timer_stats_id_by_name("mesh_processing");
+
+  int t_top_id = cs_timer_stats_switch(t_stat_id);
+
+  cs_mesh_update_auxiliary(m);
+
+  /* Renumber mesh based on code options */
+
+  cs_renumber_b_faces_select_ignore(m,
+                                    n_faces,
+                                    face_ids);
+
+  /* Rebuild some structures */
+
+  cs_mesh_update_b_cells(m);
+  cs_mesh_init_group_classes(m);
+
+  /* Print info on mesh */
+
+  cs_mesh_print_info(m, _("Mesh"));
+
+  /* Second pass to define internal coupling locators */
+
+  cs_internal_coupling_map(m);
+
+  /* Compute geometric quantities related to the mesh */
+
+  bft_printf_flush();
+
+  t1 = cs_timer_wtime();
+
+  cs_mesh_quantities_compute(m, mq);
+
+  t2 = cs_timer_wtime();
+
+  bft_printf(_("\n Computing geometric quantities (%.3g s)\n"), t2-t1);
+
+  /* Initialize selectors and locations for the mesh */
+
+  cs_mesh_init_selectors();
+  cs_mesh_location_build(cs_glob_mesh, -1);
+  cs_volume_zone_build_all(true);
+  cs_volume_zone_print_info();
+  cs_boundary_zone_build_all(true);
+  cs_boundary_zone_print_info();
+
+  /* For debugging purposes */
+
+#if 0 && defined(DEBUG) && !defined(NDEBUG)
+  cs_mesh_dump(cs_glob_mesh);
+  cs_mesh_quantities_dump(cs_glob_mesh, cs_glob_mesh_quantities);
+#endif
 
   cs_timer_stats_switch(t_top_id);
 }

src/apps/cs_solver.c
@@ -322,6 +322,9 @@ _run(void)
 
   cs_post_init_meshes(check_mask);
 
+  cs_user_mesh_modify_partial(cs_glob_mesh,
+                              cs_glob_mesh_quantities);
+
   /* Compute iterations or quality criteria depending on verification options */
 
   if (opts.verif == true) {

src/base/cs_preprocess.h
@@ -34,6 +34,8 @@
 #include "cs_defs.h"
 
 #include "cs_halo.h"
+#include "cs_mesh.h"
+#include "cs_mesh_quantities.h"
 
 /*----------------------------------------------------------------------------*/
 
@@ -82,6 +84,23 @@ cs_preprocess_mesh_define(void);
 
 void
 cs_preprocess_mesh(cs_halo_type_t   halo_type);
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Apply numbering changes to ignore selected boundary faces.
+ *
+ * \param[in, out]  m         pointer to mesh structure
+ * \param[in, out]  mq        pointer to mesh quantities structure
+ * \param[in]       n_faces   number of selected faces
+ * \param[in]       face_ids  ids of selected faces
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_preprocess_mesh_selected_b_faces_ignore(cs_mesh_t             *m,
+                                           cs_mesh_quantities_t  *mq,
+                                           cs_lnum_t              n_faces,
+                                           const cs_lnum_t        face_ids[]);
 
 /*----------------------------------------------------------------------------
  * Update fortran arrays relative to the global mesh.

src/base/cs_prototypes.h
@@ -524,6 +524,20 @@ cs_user_mesh_save(cs_mesh_t  *mesh);
 void
 cs_user_mesh_warping(void);
 
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Apply partial modifications to the mesh after the preprocessing
+ *        and initial postprocessing mesh building stage.
+ *
+ * \param[in,out] mesh  pointer to a cs_mesh_t structure
+ * \param[in,out] mesh_quantities pointer to a cs_mesh_quantities_t structure
+*/
+/*----------------------------------------------------------------------------*/
+
+void
+cs_user_mesh_modify_partial(cs_mesh_t             *mesh,
+                            cs_mesh_quantities_t  *mesh_quantities);
+
 /*----------------------------------------------------------------------------
  * Select physical model options, including user fields.
  *

src/base/cs_renumber.c
@@ -6117,10 +6117,137 @@ cs_renumber_b_faces_by_gnum(cs_mesh_t  *mesh)
 
 /*----------------------------------------------------------------------------*/
 /*!
- * \brief Renumber vertices depending on code options and target machine.
+ * \brief Renumber boundary faces such that selected faces appear last
+ *        and will be ignored.
  *
- * parameters:
- *   mesh  <->  pointer to global mesh structure
+ * Those faces will appear last, and the local number of boundary faces set
+ * to the number of remaining faces; The mesh's n_b_faces_all and
+ * n_g_b_faces_all allows accessing the full boundary faces list.
+ *
+ * \param[in, out]  mesh      pointer to global mesh structure
+ * \param[in]       n_faces   number of selected faces
+ * \param[in]       face_ids  number of selected faces
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_renumber_b_faces_select_ignore(cs_mesh_t        *mesh,
+                                  cs_lnum_t         n_faces,
+                                  const cs_lnum_t   face_ids[])
+{
+  /* Try to ensure number of total boundary faces with symmetry is updated */
+
+  if (mesh->n_b_faces_all < mesh->n_b_faces) {
+    cs_glob_mesh->n_g_b_faces_all = cs_glob_mesh->n_g_b_faces;
+    mesh->n_b_faces_all = mesh->n_b_faces;
+  }
+
+  cs_glob_mesh->n_g_b_faces = cs_glob_mesh->n_g_b_faces_all;
+  mesh->n_b_faces = mesh->n_b_faces_all;
+
+  if (n_faces < 1)
+    return;
+
+  if (mesh->b_face_numbering != NULL)
+    cs_numbering_destroy(&(mesh->b_face_numbering));
+
+  /* Revert to initial numbering */
+
+  if (mesh->global_b_face_num != NULL) {
+    cs_lnum_t *new_to_old_b = cs_order_gnum(NULL,
+                                            mesh->global_b_face_num,
+                                            mesh->n_b_faces);
+    _cs_renumber_update_b_faces(mesh, new_to_old_b);
+
+    BFT_FREE(new_to_old_b);
+
+    if (mesh->n_domains < 2)
+      BFT_FREE(mesh->global_b_face_num);
+  }
+
+  cs_lnum_t *new_to_old = NULL;
+  {
+    const cs_lnum_t n = mesh->n_b_faces;
+    const cs_lnum_t n_s = n_faces;
+    const cs_lnum_t *s_id = face_ids;
+
+    char *sel_flag;
+    BFT_MALLOC(new_to_old, n, cs_lnum_t);
+    BFT_MALLOC(sel_flag, n, char);
+
+    for (cs_lnum_t i = 0; i < n; i++)
+      sel_flag[i] = 0;
+
+    for (cs_lnum_t i = 0; i < n_s; i++)
+      sel_flag[s_id[i]] = 1;
+
+    cs_lnum_t k = 0, l = n - n_s;
+
+    for (cs_lnum_t i = 0; i < n; i++) {
+      if (sel_flag[i] == 0) {
+        new_to_old[k] = i;
+        k++;
+      }
+      else {
+        new_to_old[l] = i;
+        l++;
+      }
+    }
+
+    BFT_FREE(sel_flag);
+  }
+
+  _cs_renumber_update_b_faces(mesh, new_to_old);
+  BFT_FREE(new_to_old);
+
+  /* Also modify global boundary faces so that the remaining faces can
+     be used in parallel operators requiring them in a consistent manner */
+
+  mesh->n_b_faces = mesh->n_b_faces_all - n_faces;
+
+  if (mesh->n_domains > 1 || mesh->global_b_face_num != NULL) {
+
+    cs_lnum_t n_b_faces = mesh->n_b_faces;
+    cs_lnum_t n_b_faces_ext = mesh->n_b_faces_all - mesh->n_b_faces;
+
+    fvm_io_num_t *n_io_num
+      = fvm_io_num_create_from_select(NULL,
+                                      mesh->global_b_face_num,
+                                      n_b_faces,
+                                      0);
+    fvm_io_num_t *n_io_num_end
+      = fvm_io_num_create_from_select(NULL,
+                                      mesh->global_b_face_num + mesh->n_b_faces,
+                                      n_b_faces_ext,
+                                      0);
+
+    const cs_gnum_t *b_gnum = fvm_io_num_get_global_num(n_io_num);
+    const cs_gnum_t *b_gnum_end = fvm_io_num_get_global_num(n_io_num_end);
+
+    cs_gnum_t n_g_b_faces = fvm_io_num_get_global_count(n_io_num);
+
+    for (cs_lnum_t i = 0; i < n_b_faces; i++)
+      mesh->global_b_face_num[i] = b_gnum[i];
+
+    for (cs_lnum_t i = 0; i < n_b_faces_ext; i++)
+      mesh->global_b_face_num[n_b_faces + i] = b_gnum_end[i] + n_g_b_faces;
+
+    n_io_num = fvm_io_num_destroy(n_io_num);
+    n_io_num_end = fvm_io_num_destroy(n_io_num);
+
+    mesh->n_g_b_faces = n_g_b_faces;
+
+  }
+  else
+    mesh->n_g_b_faces = mesh->n_b_faces;
+
+  mesh->b_face_numbering
+    = cs_numbering_create_default(mesh->n_b_faces);
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Renumber vertices depending on code options and target machine.
  *
  * \param[in, out]  mesh  pointer to global mesh structure
  */

src/base/cs_renumber.h
@@ -290,6 +290,26 @@ cs_renumber_b_faces_by_gnum(cs_mesh_t  *mesh);
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief Renumber boundary faces such that selected faces appear last
+ *        and will be ignored.
+ *
+ * Those faces will appear last, and the local number of boundary faces set
+ * to the number of remaining faces; The mesh's n_b_faces_all and
+ * n_g_b_faces_all allows accessing the full boundary faces list.
+ *
+ * \param[in, out]  mesh      pointer to global mesh structure
+ * \param[in]       n_faces   number of selected faces
+ * \param[in]       face_ids  number of selected faces
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_renumber_b_faces_select_ignore(cs_mesh_t        *mesh,
+                                  cs_lnum_t         n_faces,
+                                  const cs_lnum_t   face_ids[]);
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief Renumber vertices depending on code options and target machine.
  *
  * parameters:

src/mesh/cs_mesh.c
@@ -2249,6 +2249,10 @@ cs_mesh_create(void)
   /* Status flags */
 
   mesh->n_g_free_faces = 0;
+
+  mesh->n_g_b_faces_all = 0;
+  mesh->n_b_faces_all = 0;
+
   mesh->verbosity = 1;
   mesh->modified = 0;
   mesh->save_if_modified = 1;

src/mesh/cs_mesh.h
@@ -184,6 +184,12 @@ typedef struct {
 
   cs_gnum_t n_g_free_faces;        /*!< global number of boundary faces
                                      which are in fact isolated */
+
+  cs_gnum_t n_g_b_faces_all;       /*!< global number of boundary faces
+                                     inluding those ignored in FV schemes */
+  cs_lnum_t n_b_faces_all;         /*!< number of boundary faces including
+                                     faces ignored in FV schemes */
+
   int verbosity;                   /*!< current verbosity level */
   int modified;                    /*!< modification status */
   int save_if_modified;            /*!< flag for mesh saving behavior:

commit cacbc66fa61623e30ce59a3d8796cb78e7c5b6a3

Fix mesh quantities computation, broken y commit 3b310e6b (yesterday).
The crash was actually due to a combination of commit 3b310e6b (a fix) and
and older bug introduced by a typo in commit 8765c7dc (Feb 19 2020).

src/base/cs_preprocess.c
@@ -374,8 +374,8 @@ cs_preprocess_mesh(cs_halo_type_t   halo_type)
   if (need_save)
     cs_mesh_save(cs_glob_mesh, NULL, NULL, "mesh_output.csm");
 
-  cs_glob_mesh->n_b_faces_all = cs_glob_mesh->n_b_faces_all;
-  cs_glob_mesh->n_g_b_faces_all = cs_glob_mesh->n_g_b_faces_all;
+  cs_glob_mesh->n_b_faces_all = cs_glob_mesh->n_b_faces;
+  cs_glob_mesh->n_g_b_faces_all = cs_glob_mesh->n_g_b_faces;
 
   /* Destroy the temporary structure used to build the main mesh */
 