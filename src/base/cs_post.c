code-saturne/code_saturne

commit 87b56f0eaf11c2ec68b2da6f70d08784dc001121

Fix postprocessing output of boundary fields, broken by commit 353ec9d0 (May 13 2020).

src/base/cs_post.c
@@ -2857,7 +2857,7 @@ _cs_post_match_post_write_var(const cs_post_mesh_t     *post_mesh,
     if (field_loc_type == CS_MESH_LOCATION_VERTICES)
       match = true;
     else if (   field_loc_type == CS_MESH_LOCATION_BOUNDARY_FACES
-             && post_mesh->ent_flag[CS_POST_LOCATION_B_FACE] == 0)
+             && post_mesh->ent_flag[CS_POST_LOCATION_I_FACE] == 0)
       match = true;
   }
 

commit 3d4530dbc74d4b4c757e52b848540f973ae4baa7

Fix output of profile values, broken by commit 87b56f0e (May 20 2020).

src/base/cs_post.c
@@ -3364,7 +3364,7 @@ _cs_post_output_attached_fields(cs_post_mesh_t        *post_mesh,
                  || field_loc_type == CS_MESH_LOCATION_VERTICES)) {
 
       if (! pset_on_boundary
-          && field_loc_type != CS_MESH_LOCATION_BOUNDARY_FACES)
+          && field_loc_type == CS_MESH_LOCATION_BOUNDARY_FACES)
         continue;
 
       char interpolate_input[96];