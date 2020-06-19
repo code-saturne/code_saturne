code-saturne/code_saturne

commit 74daa7a5f5b6ec6897b62d1efcfda5f1ef100dd0

Generate a build configuration info file.
This should be usable in the future to handle configuration info
currently handled directly by cs_package.py and cs_config.py so as to
avoid reloading the Python package when using different compute build,
as this mechanism is fragile.

config/cs_auto_flags.sh
@@ -141,11 +141,13 @@ if test "x$GCC" = "xyes"; then
   # Intel and LLVM compilers may pass as GCC but
   # may be recognized by version string
 
-  if test -n "`$CC --version | grep ICC`" ; then
+  cs_ac_cc_version=`$CC $user_CFLAGS --version 2>&1 | head -1`
+
+  if test -n "`echo cs_ac_cc_version | grep ICC`" ; then
     cs_gcc=icc
-  elif test -n "`$CC --version | grep ICX`" ; then
+  elif test -n "`echo cs_ac_cc_version | grep ICX`" ; then
     cs_gcc=icx
-  elif test -n "`$CC --version | grep clang`" ; then
+  elif test -n "`echo cs_ac_cc_version | grep clang`" ; then
     cs_gcc=clang
   else
     cs_gcc=gcc
@@ -157,7 +159,6 @@ if test "x$cs_gcc" = "xgcc"; then
 
   # Version strings for logging purposes and known compiler flag
   $CC -v > $outfile 2>&1
-  cs_ac_cc_version=`$CC --version 2>&1 | head -1`
   cs_cc_compiler_known=yes
 
   # Practical version info for option setting
@@ -243,22 +244,16 @@ if test "x$cs_gcc" = "xgcc"; then
       ;;
   esac
 
-# Otherwise, are we using icc or icx ?
-#-------------------------------------
+# Otherwise, are we using ICC Classic ?
+#--------------------------------------
 
-elif test "x$cs_gcc" = "xicc" -o "x$cs_gcc" = "xicx" ; then
+elif test "x$cs_gcc" = "xicc" ; then
 
-  if test "x$cs_gcc" = "xicc" ; then
-    cs_cc_version=`echo $CC --version | grep ICC |sed 's/[a-zA-Z()]//g'`
-    echo "compiler '$CC' is Intel ICC"
-  else
-    cs_cc_version=`echo $CC --version | grep ICX |sed 's/[a-zA-Z()]//g'`
-    echo "compiler '$CC' is Intel ICX"
-  fi
+  cs_cc_version=`echo $cs_ac_cc_version | grep ICC |sed 's/[a-zA-Z()]//g'`
+  echo "compiler '$CC' is Intel ICC Classic"
 
   # Version strings for logging purposes and known compiler flag
-  $CC -V conftest.c > $outfile 2>&1
-  cs_ac_cc_version=`$CC --version 2>&1 | head -1`
+  $CC $user_CFLAGS -V conftest.c > $outfile 2>&1
   cs_cc_compiler_known=yes
 
   # Some version numbers
@@ -281,6 +276,34 @@ elif test "x$cs_gcc" = "xicc" -o "x$cs_gcc" = "xicx" ; then
       cflags_default_omp="-openmp"
       ;;
   esac
+
+# Otherwise, are we using ICC NextGen ?
+#--------------------------------------
+
+elif test "x$cs_gcc" = "xicc" -o "x$cs_gcc" = "xicx" ; then
+
+  cs_cc_version=`echo $cs_ac_cc_version | grep ICX |sed 's/[a-zA-Z()]//g'`
+  echo "compiler '$CC' is Intel ICC NextGen"
+
+  # Version strings for logging purposes and known compiler flag
+  $CC $user_CFLAGS -V conftest.c > $outfile 2>&1
+  cs_cc_compiler_known=yes
+
+  # Some version numbers
+  cs_cc_vers_major=`echo $cs_ac_cc_version | cut -f 3 -d" " | cut -f1 -d.`
+  cs_cc_vers_minor=`echo $cs_ac_cc_version | cut -f 3 -d" " | cut -f2 -d.`
+  cs_cc_vers_patch=`echo $cs_ac_cc_version | cut -f 3 -d" " | cut -f3 -d.`
+  test -n "$cs_cc_vers_major" || cs_cc_vers_major=0
+  test -n "$cs_cc_vers_minor" || cs_cc_vers_minor=0
+  test -n "$cs_cc_vers_patch" || cs_cc_vers_patch=0
+
+  # Default compiler flags
+  # (temporarily disable "operands evaluated in unspecified order" remark -- 981)
+  cflags_default="-funsigned-char -Wall -Wcheck -Wshadow -Wpointer-arith -Wmissing-prototypes -Wuninitialized -Wunused -wd981"
+  cflags_default_dbg="-g -O0 -ftrapuv"
+  cflags_default_opt="-O2"
+  cflags_default_hot="-O3"
+  cflags_default_omp="-qopenmp"
 
 # Otherwise, are we using clang ?
 #--------------------------------
@@ -380,7 +403,7 @@ if test "x$cs_cc_compiler_known" != "xyes" ; then
 fi
 
 # Otherwise, are we using the Cray compiler ?
-#------------------------------------------
+#--------------------------------------------
 
 if test "x$cs_cc_compiler_known" != "xyes" ; then
 
@@ -462,11 +485,13 @@ if test "x$GXX" = "xyes"; then
   # Intel and LLVM compilers may pass as GXX but
   # may be recognized by version string
 
-  if test -n "`$CXX --version | grep ICC`" ; then
+  cs_ac_cxx_version=`$CXX $user_CXXFLAGS --version 2>&1 | head -1`
+
+  if test -n "`echo $cs_ac_cxx_version | grep ICC`" ; then
     cs_gxx=icpc
-  elif test -n "`$CXX --version | grep ICX`" ; then
+  elif test -n "`echo $cs_ac_cxx_version | grep ICX`" ; then
     cs_gxx=icpx
-  elif test -n "`$CXX --version | grep clang`" ; then
+  elif test -n "`echo $cs_ac_cxx_version | grep clang`" ; then
     cs_gxx=clang
   else
     cs_gxx=g++
@@ -478,7 +503,6 @@ if test "x$cs_gxx" = "xg++"; then
 
   # Version strings for logging purposes and known compiler flag
   $CXX -v > $outfile 2>&1
-  cs_ac_cxx_version=`$CXX --version 2>&1 | head -1`
   cs_cxx_compiler_known=yes
 
   # Practical version info for option setting
@@ -562,22 +586,16 @@ if test "x$cs_gxx" = "xg++"; then
       ;;
   esac
 
-# Otherwise, are we using icpc or icpx ?
-#---------------------------------------
+# Otherwise, are we using ICC Classic ?
+#--------------------------------------
 
-elif test "x$cs_gxx" = "xicpc" -o "x$cs_gxx" = "xicpx"; then
+elif test "x$cs_gxx" = "xicpc"; then
 
-  if test "x$cs_gxx" = "xicpc"; then
-    cs_cxx_version=`echo $CXX --version | grep ICC |sed 's/[a-zA-Z()]//g'`
-    echo "compiler '$CXX' is Intel ICC"
-  elif test "x$cs_gxx" = "xicpx"; then
-    cs_cxx_version=`echo $CXX --version | grep ICX |sed 's/[a-zA-Z()]//g'`
-    echo "compiler '$CXX' is Intel ICX"
-  fi
+  cs_cxx_version=`echo $CXX --version | grep ICC |sed 's/[a-zA-Z()]//g'`
+  echo "compiler '$CXX' is Intel ICC Classic"
 
   # Version strings for logging purposes and known compiler flag
-  $CXX -V conftest.c > $outfile 2>&1
-  cs_ac_cxx_version=`$CXX --version 2>&1 | head -1`
+  $CXX $user_CXXFLAGS -V conftest.c > $outfile 2>&1
   cs_cxx_compiler_known=yes
 
   cs_cxx_vers_major=`echo $cs_ac_cxx_version | cut -f 3 -d" " | cut -f1 -d.`
@@ -600,6 +618,33 @@ elif test "x$cs_gxx" = "xicpc" -o "x$cs_gxx" = "xicpx"; then
       cxxflags_default_omp="-openmp"
       ;;
   esac
+
+# Otherwise, are we using ICC NextGen ?
+#--------------------------------------
+
+elif test "x$cs_gxx" = "xicpc" -o "x$cs_gxx" = "xicpx"; then
+
+  cs_cxx_version=`echo $CXX --version | grep ICX |sed 's/[a-zA-Z()]//g'`
+  echo "compiler '$CXX' is Intel ICC NextGen"
+
+  # Version strings for logging purposes and known compiler flag
+  $CXX $user_CXXFLAGS -V conftest.c > $outfile 2>&1
+  cs_cxx_compiler_known=yes
+
+  cs_cxx_vers_major=`echo $cs_ac_cxx_version | cut -f 3 -d" " | cut -f1 -d.`
+  cs_cxx_vers_minor=`echo $cs_ac_cxx_version | cut -f 3 -d" " | cut -f2 -d.`
+  cs_cxx_vers_patch=`echo $cs_ac_cxx_version | cut -f 3 -d" " | cut -f3 -d.`
+  test -n "$cs_cxx_vers_major" || cs_cxx_vers_major=0
+  test -n "$cs_cxx_vers_minor" || cs_cxx_vers_minor=0
+  test -n "$cs_cxx_vers_patch" || cs_cxx_vers_patch=0
+
+  # Default compiler flags
+  cxxflags_default="-Wall -Wcheck -Wshadow -Wpointer-arith -Wmissing-prototypes -Wuninitialized -Wunused"
+  cxxflags_default_dbg="-g -O0 -ftrapuv"
+  cxxflags_default_opt="-O2"
+  cxxflags_default_hot="-O3"
+  cxxflags_default_omp="-qopenmp"
+  cxxflags_default_std="-funsigned-char"
 
 # Otherwise, are we using clang ?
 #--------------------------------
@@ -764,10 +809,12 @@ fcflags_default_prf="-g"
 
 cs_gfortran=no
 
+cs_ac_fc_version=`$FC $user_FCFLAGS --version 2>&1 | head -1`
+
 # Are we using gfortran ?
 #------------------------
 
-$FC --version 2>&1 | grep 'GNU Fortran' > /dev/null
+echo $cs_ac_fc_version | grep 'GNU Fortran' > /dev/null
 
 if test "$?" = "0" ; then
 
@@ -780,7 +827,6 @@ if test "$?" = "0" ; then
 
   # Version strings for logging purposes and known compiler flag
   $FC -v > $outfile 2>&1
-  cs_ac_fc_version=`$FC --version 2>&1 | head -1`
   cs_fc_compiler_known=yes
 
   # Practical version info for option setting
@@ -830,19 +876,18 @@ fi
 
 if test "x$cs_fc_compiler_known" != "xyes" ; then
 
-  # Are we using ifort ?
-  #---------------------
+  # Are we using IFORT Classic ?
+  #-----------------------------
 
-  $FC --version 2>&1 | grep 'IFORT' > /dev/null
+  echo $cs_ac_fc_version | grep 'ifort' > /dev/null
   if test "$?" = "0" ; then
 
-    cs_fc_version=`echo $FC --version | grep ifort |sed 's/[a-zA-Z()]//g'`
+    cs_fc_version=`echo $cs_ac_fc_version | sed 's/[a-zA-Z()]//g'`
 
-    echo "compiler '$FC' is Intel Fortran"
+    echo "compiler '$FC' is Intel Fortran Classic"
 
     # Version strings for logging purposes and known compiler flag
-    $FC -V > $outfile 2>&1
-    cs_ac_fc_version=`$FC --version 2>&1 | head -1`
+    $FC $user_FCLAGS -V > $outfile 2>&1
     cs_fc_compiler_known=yes
 
     cs_fc_vers_major=`echo $cs_ac_fc_version | cut -f 3 -d" " | cut -f1 -d.`
@@ -871,6 +916,42 @@ fi
 
 if test "x$cs_fc_compiler_known" != "xyes" ; then
 
+  # Are we using IFORT NextGen ?
+  #-----------------------------
+
+  echo $cs_ac_fc_version | grep 'ifx' > /dev/null
+  if test "$?" = "0" ; then
+
+    cs_fc_version=`echo $cs_ac_fc_version | sed 's/[a-zA-Z()]//g'`
+
+    echo "compiler '$FC' is Intel Fortran"
+
+    # Version strings for logging purposes and known compiler flag
+    $FC $user_FCLAGS-V > $outfile 2>&1
+    cs_fc_compiler_known=yes
+
+    cs_fc_vers_major=`echo $cs_ac_fc_version | cut -f 3 -d" " | cut -f1 -d.`
+    cs_fc_vers_minor=`echo $cs_ac_fc_version | cut -f 3 -d" " | cut -f2 -d.`
+    cs_fc_vers_patch=`echo $cs_ac_fc_version | cut -f 3 -d" " | cut -f3 -d.`
+    test -n "$cs_fc_vers_major" || cs_fc_vers_major=0
+    test -n "$cs_fc_vers_minor" || cs_fc_vers_minor=0
+    test -n "$cs_fc_vers_patch" || cs_fc_vers_patch=0
+
+    # Default compiler flags
+    # (temporarily disable "unused variable" remark -- 7712)
+    fcflags_default="-cpp -fpic -warn -diag-disable 7712"
+    fcflags_default_dbg="-g -O0 -traceback -check all -check nopointer -fpe0 -ftrapuv"
+    fcflags_default_opt="-O2"
+    fcflags_default_hot="-O3"
+    fcflags_default_omp="-qopenmp"
+
+  fi
+fi
+
+if test "x$cs_fc_compiler_known" != "xyes" ; then
+
+  cs_ac_fc_version=""
+
   # Are we using pgf95 ?
   #---------------------
 
@@ -881,7 +962,7 @@ if test "x$cs_fc_compiler_known" != "xyes" ; then
 
     # Version strings for logging purposes and known compiler flag
     $FC -V > $outfile 2>&1
-    cs_ac_cc_version=`grep -i pgf $outfile`
+    cs_ac_fc_version=`grep -i pgf $outfile`
     cs_fc_compiler_known=yes
 
     # Default compiler flags
@@ -1003,12 +1084,12 @@ nvccflags_default_dbg="-g"
 nvccflags_default_opt="-O2"
 nvccflags_default_prf="-g"
 
+
 ############
 #          #
 #  Linker  #
 #          #
 ############
-
 
 # Default linker flags
 #---------------------

Makefile.am
@@ -90,6 +90,7 @@ sbin/bootstrap \
 sbin/clean \
 sbin/rmb \
 config/cs_auto_flags.sh \
+config/code_saturne_build.cfg.in.in \
 m4/cs_blas.m4 \
 m4/cs_catalyst.m4 \
 m4/cs_ccm.m4 \
@@ -124,6 +125,7 @@ cs_version.h
 DISTCLEANFILES = \
 bin/code_saturne \
 bin/cs_package.py \
+lib/code_saturne_build.cfg \
 cs_config.h \
 cs_version.h \
 libtool_cuda
@@ -165,6 +167,33 @@ edit_package = sed \
 	-e 's|@docdir[@]|$(docdir)|g' \
 	-e 's|@pdfdir[@]|$(pdfdir)|g' \
 	-e 's|@sysconfdir[@]|$(sysconfdir)|g' \
+	-e 's|@PACKAGE_NAME[@]|$(PACKAGE_NAME)|g' \
+	-e 's|@PACKAGE_TARNAME[@]|$(PACKAGE_TARNAME)|g' \
+	-e 's|@PACKAGE_VERSION[@]|$(PACKAGE_VERSION)|g' \
+	-e 's|@PACKAGE_STRING[@]|$(PACKAGE_STRING)|g' \
+	-e 's|@PACKAGE_BUGREPORT[@]|$(PACKAGE_BUGREPORT)|g' \
+	-e 's|@PACKAGE_URL[@]|$(PACKAGE_URL)|g' \
+	-e 's|@SALOME_KERNEL[@]|$(SALOME_KERNEL)|g' \
+	-e 's|@SALOMEENVCMD[@]|$(SALOMEENVCMD)|g'
+
+edit_cfg = sed \
+	-e 's|@cfg_prefix[@]|$(prefix)|g' \
+	-e 's|@cfg_exec_prefix[@]|$(exec_prefix)|g' \
+	-e 's|@cfg_bindir[@]|$(bindir)|g' \
+	-e 's|@cfg_includedir[@]|$(includedir)|g' \
+	-e 's|@cfg_pkgincludedir[@]|$(pkgincludedir)|g' \
+	-e 's|@cfg_libdir[@]|$(libdir)|g' \
+	-e 's|@cfg_libexecdir[@]|$(libexecdir)|g' \
+	-e 's|@cfg_pkglibexecdir[@]|$(pkglibexecdir)|g' \
+	-e 's|@cfg_pythondir[@]|$(pythondir)|g' \
+	-e 's|@cfg_pkgpythondir[@]|$(pkgpythondir)|g' \
+	-e 's|@cfg_localedir[@]|$(localedir)|g' \
+	-e 's|@cfg_datarootdir[@]|$(datarootdir)|g' \
+	-e 's|@cfg_datadir[@]|$(datadir)|g' \
+	-e 's|@cfg_pkgdatadir[@]|$(pkgdatadir)|g' \
+	-e 's|@cfg_docdir[@]|$(docdir)|g' \
+	-e 's|@cfg_pdfdir[@]|$(pdfdir)|g' \
+	-e 's|@cfg_sysconfdir[@]|$(sysconfdir)|g' \
 	-e 's|@PACKAGE_NAME[@]|$(PACKAGE_NAME)|g' \
 	-e 's|@PACKAGE_TARNAME[@]|$(PACKAGE_TARNAME)|g' \
 	-e 's|@PACKAGE_VERSION[@]|$(PACKAGE_VERSION)|g' \
@@ -221,9 +250,28 @@ bin/code_saturne: $(srcdir)/bin/code_saturne.$(LAUNCHER_EXT).in
 cs_version.h: Makefile $(top_srcdir)/src/base/cs_version.h.in
 	$(PYTHON) ${top_srcdir}/build-aux/cs_version.py --replace ${top_srcdir}/src/base/cs_version.h.in cs_version.h
 
+config/code_saturne_build.cfg.in: $(srcdir)/Makefile.in $(top_builddir)/config.status
+	@case '$?' in \
+	  *config.status*) \
+	    echo ' $(SHELL) ./config.status'; \
+	    $(SHELL) ./config.status;; \
+	  *) \
+	    echo ' cd $(top_builddir) && $(SHELL) ./config.status $@ $(am__maybe_remake_depfiles)'; \
+	    cd $(top_builddir) && $(SHELL) ./config.status $@ $(am__maybe_remake_depfiles);; \
+	esac;
+
+lib/code_saturne_build.cfg: Makefile config/code_saturne_build.cfg.in
+	rm -f $@ $@.tmp
+	$(MKDIR_P) lib
+	srcdir=''; \
+	$(edit_cfg) config/code_saturne_build.cfg.in >$@.tmp
+	mv $@.tmp $@
+	$(PYTHON) ${top_srcdir}/build-aux/cs_version.py --replace $@ $@
+
 BUILT_SOURCES = \
+cs_version.h \
 bin/cs_package.py \
-cs_version.h
+lib/code_saturne_build.cfg
 
 CLEANFILES = $(edit_scripts)
 EXTRA_DIST += bin/code_saturne.py.in bin/cs_package.py.in \
@@ -238,6 +286,9 @@ bin/code_saturne
 if HAVE_SHELL_LAUNCHER
 nodist_bin_SCRIPTS += bin/code_saturne.py
 endif
+
+nodist_bin_SCRIPTS = \
+bin/code_saturne
 
 # Install Python library
 
@@ -464,11 +515,13 @@ all-local:
 
 install-data-local:
 	@$(NORMAL_INSTALL)
+	cp lib/code_saturne_build.cfg "$(DESTDIR)$(libdir)"
 	test -z "$(pkgdatadir)/data/thch" || $(MKDIR_P) "$(DESTDIR)$(pkgdatadir)/data/thch"
 	tar xvJf "$(top_srcdir)/data/thch/dp_radiat_MFS.tar.xz" -C "$(DESTDIR)$(pkgdatadir)/data/thch"
 
 uninstall-local:
 	rm -rf "$(DESTDIR)$(pkgdatadir)/data/thch/dp_radiat_MFS"
+	rm -f"$(DESTDIR)$(libdir)"
 	@$(NORMAL_UNINSTALL)
 
 install-shortcut:

config/code_saturne_build.cfg.in.in
@@ -1,0 +1,241 @@
+# code_saturne build and install configuration information.
+
+# Generated automaticaly at build and install stages.
+
+#-------------------------------------------------------------------------------
+
+# This file is part of Code_Saturne, a general-purpose CFD tool.
+#
+# Copyright (C) 1998-2020 EDF S.A.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+# Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+#-------------------------------------------------------------------------------
+
+# Package information
+# -------------------
+
+[package]
+
+name: @PACKAGE_NAME@
+tarname: @PACKAGE_TARNAME@
+pkgversion: @PACKAGE_VERSION@
+string: @PACKAGE_STRING@
+bugreport: @PACKAGE_BUGREPORT@
+url: @PACKAGE_URL@
+version: @cs_version@
+version_full: @cs_version_full@
+version_short: @cs_version_short@
+revision: @cs_revision@
+
+# Installation directories
+# ------------------------
+
+[install]
+
+prefix: @cfg_prefix@
+exec_prefix: @cfg_exec_prefix@
+bindir: @cfg_bindir@
+includedir: @cfg_includedir@
+pkgincludedir: @cfg_pkgincludedir@
+libdir: @cfg_libdir@
+libexecdir: @cfg_libexecdir@
+pkglibexecdir: @cfg_pkglibexecdir@
+pythondir: @cfg_pythondir@
+pkgpythondir: @cfg_pkgpythondir@
+localedir: @cfg_localedir@
+datarootdir: @cfg_datarootdir@
+datadir: @cfg_datadir@
+pkgdatadir: @cfg_pkgdatadir@
+docdir: @cfg_docdir@
+pdfdir: @cfg_pdfdir@
+sysconfdir: @cfg_sysconfdir@
+
+# Compilers and associated flags
+# ------------------------------
+
+[compilers]
+
+cc: @CC@
+cxx: @CXX@
+fc: @FC@
+ld: @CS_LD@
+
+cc_vendor: @cc_vendor@
+cc_version: @cc_version@
+cxx_vendor: @cxx_vendor@
+cxx_version: @cxx_version@
+fc_vendor: @fc_vendor@
+fc_version: @fc_version@
+nvcc_version: @nvcc_version@
+
+cflags: @CFLAGS@ @CFLAGS_DBG@ @CFLAGS_OPT@
+cxxflags: @CXXFLAGS@ @CXXFLAGS_DBG@ @CXXFLAGS_OPT@
+fcflags: @FCFLAGS@ @FCFLAGS_DBG@ @FCFLAGS_OPT@
+
+cppflags: @CPPFLAGS@
+ldflags: @LDFLAGS@
+libs: @LIBS@ @FCLIBS@
+
+fcmodinclude: @FCMODINCLUDE@
+rpath: @LDRPATH@
+special_user_link: @cs_special_user_link@
+
+[python]
+
+python: @PYTHON@
+
+# Execution environment
+#----------------------
+
+[environment]
+
+env_modules: @cs_env_modules@
+env_modulecmd: @MODULECMD@
+
+# SALOME-related information
+
+salome_env: @SALOMEENVCMD@
+
+# Optional features
+#------------------
+
+[features]
+
+debug: @debug@
+relocatable: @relocatable@
+shared: @enable_shared@
+gui: @cs_have_gui@
+frontend: @cs_have_frontend@
+mpi: @cs_have_mpi@
+openmp: @cs_have_openmp@
+cuda: @cs_have_cuda@
+long-gnum: @cs_have_long_gnum@
+build_os: @build_os@
+
+# Library information
+#--------------------
+
+[ple]
+variant: @ple_type@
+cppflags: @PLE_CPPFLAGS@
+ldflags: @PLE_LDFLAGS@
+libs: @PLE_LIBS@
+
+[blas]
+have: @cs_have_blas@
+cppflags: @BLAS_CPPFLAGS@
+ldflags: @BLAS_LDFLAGS@
+libs: @BLAS_LIBS@
+
+[ccm]
+have: @cs_have_ccm@
+cppflags: @CCM_CPPFLAGS@
+ldflags: @CCM_LDFLAGS@
+libs: @CCM_LIBS@
+
+[cgns]
+have: @cs_have_cgns@
+cppflags: @CGNS_CPPFLAGS@
+ldflags: @CGNS_LDFLAGS@
+libs: @CGNS_LIBS@
+
+[hdf5]
+have: @cs_have_hdf5@
+cppflags: @HDF5_CPPFLAGS@
+ldflags: @HDF5_LDFLAGS@
+libs: @HDF5_LIBS@
+
+[med]
+have: @cs_have_med@
+cppflags: @MED_CPPFLAGS@
+ldflags: @MED_LDFLAGS@
+libs: @MED_LIBS@
+
+[catalyst]
+have: @cs_have_catalyst@
+dynamic_load: @cs_py_have_plugin_catalyst@
+cppflags: @CATALYST_CPPFLAGS@
+ldflags: @CATALYST_LDFLAGS@
+libs: @CATALYST_LIBS@
+
+[melissa]
+have: @cs_have_melissa@
+dynamic_load: @cs_py_have_plugin_melissa@
+cppflags: @MELISSA_CPPFLAGS@
+ldflags: @MELISSA_LDFLAGS@
+libs: @MELISSA_LIBS@
+
+[medcoupling]
+have: @cs_have_medcoupling@
+dynamic_load: @cs_py_have_plugin_medcoupling@
+cppflags: @MEDCOUPLING_CPPFLAGS@
+ldflags: @MEDCOUPLING_LDFLAGS@
+libs: @MEDCOUPLING_LIBS@
+
+[eos]
+have: @cs_have_eos@
+prefix: @eos_prefix@
+cppflags: @EOS_CPPFLAGS@
+ldflags: @EOS_LDFLAGS@
+libs: @EOS_LIBS@
+
+[freesteam]
+have: @cs_have_freesteam@
+prefix: @freesteam_prefix@
+cppflags: @FREESTEAM_CPPFLAGS@
+ldflags: @FREESTEAM_LDFLAGS@
+libs: @FREESTEAM_LIBS@
+
+[coolprop]
+have: @cs_have_coolprop@
+prefix: @coolprop_prefix@
+cppflags: @COOLPROP_CPPFLAGS@
+ldflags: @COOLPROP_LDFLAGS@
+libs: @COOLPROP_LIBS@
+pythonpath: @COOLPROPPYTHONPATH@
+
+[metis]
+have: @cs_have_metis@
+cppflags: @METIS_CPPFLAGS@
+ldflags: @METIS_LDFLAGS@
+libs: @METIS_LIBS@
+
+[mpi]
+have: @cs_have_mpi@
+variant: @mpi_type@
+bindir: @mpi_bindir@
+libdir: @mpi_libdir@
+cppflags: @MPI_CPPFLAGS@
+ldflags: @MPI_LDFLAGS@
+libs: @MPI_LIBS@
+
+[cuda]
+have: @cs_have_cuda@
+cppflags: @CUDA_CPPFLAGS@
+ldflags: @CUDA_LDFLAGS@
+libs: @CUDA_LIBS@
+
+[petsc]
+have: @cs_have_petsc@
+cppflags: @PETSC_CPPFLAGS@
+ldflags: @PETSC_LDFLAGS@
+libs: @PETSC_LIBS@
+
+[scotch]
+have: @cs_have_scotch@
+cppflags: @SCOTCH_CPPFLAGS@
+ldflags: @SCOTCH_LDFLAGS@
+libs: @SCOTCH_LIBS@

configure.ac
@@ -53,7 +53,7 @@ m4_define([cs_licence_c_comment],
 m4_define([cs_version_string],
           [m4_normalize(esyscmd([build-aux/cs_version.py]))])
 
-AC_INIT([code_saturne],[cs_version_string],[saturne-support@edf.fr],[],[http://code-saturne.org])
+AC_INIT([code_saturne],[cs_version_string],[saturne-support@edf.fr],[],[https://code-saturne.org])
 AC_CONFIG_SRCDIR([src/apps/cs_solver.c])
 
 # Use the config directory for libtool stuff ...
@@ -263,7 +263,13 @@ if test "x$auto_flags" = "xyes" ; then
   AC_SUBST(FCFLAGS_HOT)
 
   AC_SUBST(LDRPATH)
-  AC_SUBST(ccversion, [${cs_cc_version}])
+  AC_SUBST(cc_vendor, [${cs_cc_vendor}])
+  AC_SUBST(cc_version, [${cs_cc_version}])
+  AC_SUBST(cxx_vendor, [${cs_cxx_vendor}])
+  AC_SUBST(cxx_version, [${cs_cxx_version}])
+  AC_SUBST(fc_vendor, [${cs_fc_vendor}])
+  AC_SUBST(fc_version, [${cs_fc_version}])
+  AC_SUBST(nvcc_version, [${cs_nvcc_version}])
 
 fi
 
@@ -563,24 +569,14 @@ fi
 # Checks for additional version information.
 #------------------------------------------------------------------------------
 
-# Define the different version numbers where needed
+# Define the different version number where needed
+# (detailed version numbers are generated during the build step as a
+# precaution, so that when switching revisions not requiring
+# rebuild of the configure file / re-configure, the correct
+# version number is used).
 
 cs_version_short=`$PYTHON ${srcdir}/build-aux/cs_version.py --short`
 AC_SUBST(cs_version_short)
-
-case "$host_os" in
-  mingw* | cygwin)
-    cs_version=`$PYTHON ${srcdir}/build-aux/cs_version.py --short`
-    cs_version_full=`$PYTHON ${srcdir}/build-aux/cs_version.py --full`
-    cs_version_short=`$PYTHON ${srcdir}/build-aux/cs_version.py --short`
-    AC_SUBST(cs_version)
-    AC_SUBST(cs_version_full)
-    AM_SUBST_NOTMAKE(cs_version)
-    AM_SUBST_NOTMAKE(cs_version_full)
-    ;;
-  *)
-    ;;
-esac
 
 #------------------------------------------------------------------------------
 # Optional shared library and plugin support
@@ -1147,7 +1143,8 @@ AC_CONFIG_FILES([Makefile
                  docs/refcard/Makefile docs/theory/Makefile
                  docs/user/Makefile
                  tests/Makefile
-                 bin/cs_config.py])
+                 bin/cs_config.py
+                 config/code_saturne_build.cfg.in])
 
 #------------------------------------------------------------------------------
 

commit d56981af1648b3b289ffe002f1d06ca279b6d8a9

Fix detection of Intel compilers, broken by commit 74daa7a5 (yesterday).

config/cs_auto_flags.sh
@@ -143,11 +143,11 @@ if test "x$GCC" = "xyes"; then
 
   cs_ac_cc_version=`$CC $user_CFLAGS --version 2>&1 | head -1`
 
-  if test -n "`echo cs_ac_cc_version | grep ICC`" ; then
+  if test -n "`echo $cs_ac_cc_version | grep ICC`" ; then
     cs_gcc=icc
-  elif test -n "`echo cs_ac_cc_version | grep ICX`" ; then
+  elif test -n "`echo $cs_ac_cc_version | grep ICX`" ; then
     cs_gcc=icx
-  elif test -n "`echo cs_ac_cc_version | grep clang`" ; then
+  elif test -n "`echo $cs_ac_cc_version | grep clang`" ; then
     cs_gcc=clang
   else
     cs_gcc=gcc
@@ -924,7 +924,7 @@ if test "x$cs_fc_compiler_known" != "xyes" ; then
 
     cs_fc_version=`echo $cs_ac_fc_version | sed 's/[a-zA-Z()]//g'`
 
-    echo "compiler '$FC' is Intel Fortran"
+    echo "compiler '$FC' is Intel Fortran NextGen"
 
     # Version strings for logging purposes and known compiler flag
     $FC $user_FCLAGS-V > $outfile 2>&1