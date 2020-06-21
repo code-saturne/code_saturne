code-saturne/code_saturne

commit 128f6e8a294b1744b5ffd76830f64ceee4e218f4

Fix in the trajectography when segment goes through face center exactly.

src/mesh/cs_geom.c
@@ -252,6 +252,7 @@ cs_geom_segment_intersect_face(int               orient,
                             sx0[2] - face_cog[2]};
 
   int n_intersects = 0;
+  bool in_cog = true;
 
   /* Principle:
    *  - loop on sub-triangles of the face
@@ -286,7 +287,10 @@ cs_geom_segment_intersect_face(int               orient,
   cs_real_3_t e0, e1;
   int pip1, p0;
 
-  /* 1st vertex: vector e0, p0 = e0 ^ vgo  */
+  /* 1st vertex: test edge e0 with respect to (OD)
+   * in an orthogonal plane of (OD)
+   * vector e0 = [G, v0], p0 = e0 ^ OG
+   * return "p0 . OD > 0 ?" */
   cs_lnum_t vtx_id_0 = vertex_ids[0];
   const cs_real_t *vtx_0 = vtx_coord[vtx_id_0];
 
@@ -320,7 +324,7 @@ cs_geom_segment_intersect_face(int               orient,
      */
     int sign_od_p = (od_p > 0 ? 1 : -1);
 
-    /* 2nd edge: vector ei+1, pi+1 = ei+1 ^ vgo */
+    /* 2nd edge: vector ei+1, pi+1 = ei+1 ^ OG */
 
     int pi = pip1;
     if (i == n_vertices - 1)
@@ -328,10 +332,10 @@ cs_geom_segment_intersect_face(int               orient,
     else
       pip1 = _test_edge(sx0, sx1, face_cog, vtx_1);
 
-    const int u_sign = pip1 * sign_od_p;
+    int u_sign = pip1 * sign_od_p;
 
-    /* 1st edge: vector ei, pi = ei ^ vgo */
-    const int v_sign = - pi * sign_od_p;
+    /* 1st edge: vector ei, pi = ei ^ OG */
+    int v_sign = - pi * sign_od_p;
 
     /* 3rd edge: vector e_out */
 
@@ -352,6 +356,17 @@ cs_geom_segment_intersect_face(int               orient,
     int w_sign = _test_edge(sx0, sx1, vtx_0, vtx_1)
                  * reorient_edge * sign_od_p;
 
+    /* Special treatment if the intersection point is
+     * in G exactly (so w_sign is < 0 for all e_out)
+     */
+    if (w_sign > 0)
+      in_cog = false;
+    /* Last vertex */
+    if ((i == (n_vertices - 1)) && in_cog) {
+      u_sign = 1;
+      v_sign = 1;
+    }
+
     /* The projection of point O along displacement (OD) is outside of the
      * triangle then no intersection */
     if (w_sign > 0 || u_sign  < 0 || v_sign < 0)
@@ -360,6 +375,8 @@ cs_geom_segment_intersect_face(int               orient,
     /* Line (OD) intersects the triangle because
      * u_sign >= 0, v_sign >= 0 and w_sign <= 0
      */
+    /* No need to deal with the special case where the intersection is in G */
+    in_cog = false;
 
     double og_p = - cs_math_3_dot_product(vgo, pvec);
 

commit c7fb4f53c1be66e99c1f40f6c5f2078962bafcc4

Fix bug in the trajectography introduced in rev 128f6e8.

src/mesh/cs_geom.c
@@ -297,6 +297,13 @@ cs_geom_segment_intersect_face(int               orient,
   p0 = _test_edge(sx0, sx1, face_cog, vtx_0);
   pip1 = p0;
 
+  /* Sign of "[OD].p" for the first sub-triangle.
+   * We need to store this to check if all e_out are around
+   * (OD), and for that the multiplier "[OD].p" must be the same
+   * Otherwise we can have troubles when [OD] is orthogonal to the face normal
+   * (it can switch sign from one triangle to an other...) */
+  int sign_od_p0 = 0;
+
   /* Loop on vertices of the face */
   for (cs_lnum_t i = 0; i < n_vertices; i++) {
 
@@ -320,9 +327,12 @@ cs_geom_segment_intersect_face(int               orient,
     double od_p = cs_math_3_dot_product(disp, pvec);
 
     /* This sign is absolute (ie same result is obtained if the face is seen
-     * from the other neighbooring cell).
+     * from the other neighbouring cell).
      */
     int sign_od_p = (od_p > 0 ? 1 : -1);
+
+    if (sign_od_p0 == 0)
+      sign_od_p0 = sign_od_p;
 
     /* 2nd edge: vector ei+1, pi+1 = ei+1 ^ OG */
 
@@ -358,10 +368,17 @@ cs_geom_segment_intersect_face(int               orient,
 
     /* Special treatment if the intersection point is
      * in G exactly (so w_sign is < 0 for all e_out)
+     * Not: all e_out should be talen with the same noram p.
+     * We take the first sign_od_p
      */
-    if (w_sign > 0)
+    if (w_sign * sign_od_p *sign_od_p0 > 0)
       in_cog = false;
-    /* Last vertex */
+
+    /* Last vertex for the face,
+     * in_cog is true for all e_out edges which means that
+     * w_sign <= 0 for all
+     * AND no triangle was selected (because the line is passing exactly
+     * in the cog G) */
     if ((i == (n_vertices - 1)) && in_cog) {
       u_sign = 1;
       v_sign = 1;