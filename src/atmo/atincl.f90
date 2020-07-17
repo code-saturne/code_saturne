code-saturne/code_saturne

commit 5a3866561cfc643b857c321ec10ae478371209e0

Map some atmospheric model options and arrays.
Some cleanup by Y. Fournier.

src/atmo/atincl.f90
@@ -46,7 +46,7 @@ implicit none
 !   Arrays specific to values read in the input meteo file:
 
 !> time (in sec) of the meteo profile
-double precision, allocatable, dimension(:) :: tmmet
+double precision, dimension(:), pointer :: tmmet
 
 !> altitudes of the dynamic profiles (read in the input meteo file)
 double precision, allocatable, dimension(:) :: zdmet
@@ -59,7 +59,7 @@ double precision, allocatable, dimension(:,:) :: mom_met
 double precision, allocatable, dimension(:,:) :: mom
 
 !> altitudes of the temperature profile (read in the input meteo file)
-double precision, allocatable, dimension(:) :: ztmet
+double precision, dimension(:), pointer :: ztmet
 
 !> meteo u  profiles (read in the input meteo file)
 double precision, allocatable, dimension(:,:) :: umet
@@ -103,13 +103,13 @@ double precision, allocatable, dimension(:,:) :: rmet
 double precision, allocatable, dimension(:,:) :: tpmet
 
 !> hydrostatic pressure from Laplace integration
-double precision, allocatable, dimension(:,:) :: phmet
+double precision, dimension(:,:), pointer :: phmet
 
 !> Diagnosed nebulosity
-double precision, allocatable, dimension(:) :: nebdia
+double precision, dimension(:), pointer :: nebdia
 
 !> fractional nebulosity
-double precision, allocatable, dimension(:) :: nn
+double precision, dimension(:), pointer :: nn
 
 ! 1.2 Pointers for the positions of the variables
 !------------------------------------------------
@@ -142,16 +142,16 @@ integer, save :: imomst
 !>flag for reading the meteo input file
 !> - = 0 -> no reading
 !> - = 1 -> reading
-integer, save :: imeteo
+integer(c_int), pointer, save :: imeteo
 
 !> numbers of altitudes for the dynamics
-integer, save :: nbmetd
+integer(c_int), pointer, save :: nbmetd
 
 !> numbers of altitudes for the temperature and specific humidity
-integer, save :: nbmett
+integer(c_int), pointer, save :: nbmett
 
 !> numbers of time steps for the meteo profiles
-integer, save :: nbmetm
+integer(c_int), pointer, save :: nbmetm
 
 !> read zone boundary conditions from profile
 integer, save :: iprofm(nozppm)
@@ -221,7 +221,7 @@ real(c_double), pointer, save:: xlat
 ! 2.3 Data specific to the meteo profile above the domain
 !--------------------------------------------------------
 !> Number of vertical levels (cf. 1D radiative scheme
-integer, save:: nbmaxt
+integer(c_int), pointer, save:: nbmaxt
 
 !> flag to compute the hydrostastic pressure by Laplace integration
 !> in the meteo profiles
@@ -229,7 +229,6 @@ integer, save:: nbmaxt
 !> = 1 : top to bottom Laplace integration based on P computed for
 !>            the standard atmosphere at z(nbmaxt)
 integer, save:: ihpm
-
 
 ! 2.4 Data specific to the 1D vertical grid:
 !-------------------------------------------
@@ -394,6 +393,34 @@ integer, save :: kopint
 
     !---------------------------------------------------------------------------
 
+    !> \brief Return pointers to atmo includes
+
+    subroutine cs_f_atmo_get_pointers(syear, squant, shour, smin, ssec, &
+        longitude, latitude,                                            &
+        compute_z_ground,                                               &
+        sedimentation_model, deposition_model, nucleation_model,        &
+        subgrid_model, ichemistry, nespg, nrg, chem_with_photo,         &
+        iaerosol, frozen_gas_chem, init_gas_with_lib,                   &
+        init_aero_with_lib, n_aero, n_sizebin, imeteo,                  &
+        nbmetd, nbmett, nbmetm, nbmaxt)                                 &
+      bind(C, name='cs_f_atmo_get_pointers')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      type(c_ptr), intent(out) :: compute_z_ground, ichemistry, nespg, nrg
+      type(c_ptr), intent(out) :: sedimentation_model, deposition_model
+      type(c_ptr), intent(out) :: nucleation_model
+      type(c_ptr), intent(out) :: subgrid_model
+      type(c_ptr), intent(out) :: syear, squant, shour, smin, ssec
+      type(c_ptr), intent(out) :: longitude, latitude
+      type(c_ptr), intent(out) :: iaerosol, frozen_gas_chem
+      type(c_ptr), intent(out) :: init_gas_with_lib, init_aero_with_lib
+      type(c_ptr), intent(out) :: n_aero, n_sizebin, chem_with_photo
+      type(c_ptr), intent(out) :: imeteo
+      type(c_ptr), intent(out) :: nbmetd, nbmett, nbmetm, nbmaxt
+    end subroutine cs_f_atmo_get_pointers
+
+    !---------------------------------------------------------------------------
+
     !> \brief Calculation of the specific enthalpy of liquid water
 
     !> \return specific enthalpy of liquid water
@@ -529,6 +556,8 @@ contains
     type(c_ptr) :: c_longitude, c_latitude
     type(c_ptr) :: c_modelaero, c_frozen_gas_chem, c_nlayer, c_nsize
     type(c_ptr) :: c_init_gas_with_lib, c_init_aero_with_lib, c_chem_with_photo
+    type(c_ptr) :: c_imeteo
+    type(c_ptr) :: c_nbmetd, c_nbmett, c_nbmetm, c_nbmaxt
 
     call cs_f_atmo_get_pointers(                  &
       c_syear, c_squant, c_shour, c_smin, c_ssec, &
@@ -540,7 +569,8 @@ contains
       c_modelaero, c_frozen_gas_chem,             &
       c_init_gas_with_lib,                        &
       c_init_aero_with_lib, c_nlayer,             &
-      c_nsize)
+      c_nsize, c_imeteo,                          &
+      c_nbmetd, c_nbmett, c_nbmetm, c_nbmaxt)
 
     call c_f_pointer(c_syear, syear)
     call c_f_pointer(c_squant, squant)
@@ -568,6 +598,11 @@ contains
     call c_f_pointer(c_init_aero_with_lib, init_aero_with_lib)
     call c_f_pointer(c_nlayer, nlayer_aer)
     call c_f_pointer(c_nsize, n_aer)
+    call c_f_pointer(c_imeteo, imeteo)
+    call c_f_pointer(c_nbmetd, nbmetd)
+    call c_f_pointer(c_nbmett, nbmett)
+    call c_f_pointer(c_nbmetm, nbmetm)
+    call c_f_pointer(c_nbmaxt, nbmaxt)
 
     return
 
@@ -577,11 +612,29 @@ contains
 !> \brief Initialisation of meteo data
 subroutine init_meteo
 
+use cs_c_bindings
 use atsoil
 
 implicit none
 
 integer :: imode, ifac
+! Local variables
+type(c_ptr) :: c_z_temp_met, c_time_met
+type(c_ptr) :: c_frac_neb, c_diag_neb
+type(c_ptr) :: c_hyd_p_met
+
+integer(c_int),   dimension(2) :: dim_hyd_p_met
+
+call cs_f_atmo_arrays_get_pointers(                &
+     c_z_temp_met, c_time_met,                     &
+     c_hyd_p_met, dim_hyd_p_met,                   &
+     c_frac_neb, c_diag_neb)
+
+call c_f_pointer(c_z_temp_met, ztmet, [nbmaxt])
+call c_f_pointer(c_time_met, tmmet, [nbmetm])
+call c_f_pointer(c_hyd_p_met, phmet, [dim_hyd_p_met])
+call c_f_pointer(c_frac_neb, nn, [ncelet])
+call c_f_pointer(c_diag_neb, nebdia, [ncelet])
 
 ! Allocate additional arrays for Water Microphysics
 
@@ -655,9 +708,12 @@ end subroutine init_meteo
 !> \brief Final step for deallocation
 subroutine finalize_meteo
 
+use cs_c_bindings
 use atsoil
 
 implicit none
+
+call cs_f_atmo_finalize()
 
 if (ippmod(iatmos).ge.2) then
   deallocate(nebdia)

src/atmo/cs_atmo.c
@@ -119,6 +119,17 @@ static cs_atmo_option_t  _atmo_option = {
   .deposition_model = 0,
   .nucleation_model = 0,
   .subgrid_model = 0,
+  .imeteo = 0,
+  .nbmetd = -1,
+  .nbmett = -1,
+  .nbmetm = -1,
+  .nbmaxt = -1,
+
+  .z_temp_met = NULL,
+  .time_met   = NULL,
+  .hyd_p_met  = NULL,
+  .frac_neb  = NULL,
+  .diag_neb  = NULL
 };
 
 /* atmo chemistry options structure */
@@ -184,7 +195,20 @@ cs_f_atmo_get_pointers(int                    **syear,
                        bool                   **init_gas_with_lib,
                        bool                   **init_aero_with_lib,
                        int                    **n_layer,
-                       int                    **n_size);
+                       int                    **n_size,
+                       int                    **imeteo,
+                       int                    **nbmetd,
+                       int                    **nbmett,
+                       int                    **nbmetm,
+                       int                    **nbmaxt);
+
+void
+cs_f_atmo_arrays_get_pointers(cs_real_t **z_temp_met,
+                              cs_real_t **time_met,
+                              cs_real_t **hyd_p_met,
+                              int         dim_hyd_p_met[2],
+                              cs_real_t **frac_neb,
+                              cs_real_t **diag_neb);
 
 void
 cs_f_atmo_chem_arrays_get_pointers(int       **species_to_scalar_id,
@@ -196,6 +220,9 @@ cs_f_atmo_chem_initialize_species_to_fid(int *species_fid);
 
 void
 cs_f_atmo_chem_finalize(void);
+
+void
+cs_f_atmo_finalize(void);
 
 /*============================================================================
  * Fortran wrapper function definitions
@@ -227,7 +254,12 @@ cs_f_atmo_get_pointers(int                    **syear,
                        bool                   **init_gas_with_lib,
                        bool                   **init_aero_with_lib,
                        int                    **n_layer,
-                       int                    **n_size)
+                       int                    **n_size,
+                       int                    **imeteo,
+                       int                    **nbmetd,
+                       int                    **nbmett,
+                       int                    **nbmetm,
+                       int                    **nbmaxt)
 {
   *syear     = &(_atmo_option.syear);
   *squant    = &(_atmo_option.squant);
@@ -241,6 +273,11 @@ cs_f_atmo_get_pointers(int                    **syear,
   *deposition_model = &(_atmo_option.deposition_model);
   *nucleation_model = &(_atmo_option.nucleation_model);
   *subgrid_model = &(_atmo_option.subgrid_model);
+  *imeteo     = &(_atmo_option.imeteo);
+  *nbmetd     = &(_atmo_option.nbmetd);
+  *nbmett     = &(_atmo_option.nbmett);
+  *nbmetm     = &(_atmo_option.nbmetm);
+  *nbmaxt     = &(_atmo_option.nbmaxt);
   *model = &(_atmo_chem.model);
   *n_species = &(_atmo_chem.n_species);
   *n_reactions = &(_atmo_chem.n_reactions);
@@ -273,6 +310,39 @@ cs_f_atmo_chem_arrays_get_pointers(int       **species_to_scalar_id,
 }
 
 void
+cs_f_atmo_arrays_get_pointers(cs_real_t **z_temp_met,
+                              cs_real_t **time_met,
+                              cs_real_t **hyd_p_met,
+                              int         dim_hyd_p_met[2],
+                              cs_real_t **frac_neb,
+                              cs_real_t **diag_neb)
+{
+  const cs_mesh_t  *m = cs_glob_mesh;
+  cs_lnum_t n_cells_ext = m->n_cells_with_ghosts;
+
+  if (_atmo_option.z_temp_met == NULL)
+    BFT_MALLOC(_atmo_option.z_temp_met, _atmo_option.nbmaxt, cs_real_t);
+  if (_atmo_option.time_met == NULL)
+    BFT_MALLOC(_atmo_option.time_met, _atmo_option.nbmetm, cs_real_t);
+  if (_atmo_option.hyd_p_met == NULL)
+    BFT_MALLOC(_atmo_option.hyd_p_met,
+               _atmo_option.nbmetm*_atmo_option.nbmaxt, cs_real_t);
+  if (_atmo_option.frac_neb == NULL)
+    BFT_MALLOC(_atmo_option.frac_neb, n_cells_ext, cs_real_t);
+  if (_atmo_option.diag_neb == NULL)
+    BFT_MALLOC(_atmo_option.diag_neb, n_cells_ext, cs_real_t);
+
+  *hyd_p_met       = _atmo_option.hyd_p_met;
+  dim_hyd_p_met[0] = _atmo_option.nbmaxt;
+  dim_hyd_p_met[1] = _atmo_option.nbmetm;
+
+  *z_temp_met = _atmo_option.z_temp_met;
+  *time_met   = _atmo_option.time_met;
+  *frac_neb   = _atmo_option.frac_neb;
+  *diag_neb   = _atmo_option.diag_neb;
+}
+
+void
 cs_f_atmo_chem_initialize_species_to_fid(int *species_fid)
 {
   assert(species_fid != NULL);
@@ -294,6 +364,17 @@ cs_f_atmo_chem_finalize(void)
   BFT_FREE(_atmo_chem.chempoint);
   BFT_FREE(_atmo_chem.spack_file_name);
   BFT_FREE(_atmo_chem.aero_file_name);
+}
+
+
+void
+cs_f_atmo_finalize(void)
+{
+  BFT_FREE(_atmo_option.z_temp_met);
+  BFT_FREE(_atmo_option.time_met);
+  BFT_FREE(_atmo_option.hyd_p_met);
+  BFT_FREE(_atmo_option.frac_neb);
+  BFT_FREE(_atmo_option.diag_neb);
 }
 
 /*============================================================================
@@ -363,29 +444,28 @@ cs_atmo_z_ground_compute(void)
      (const cs_real_3_t *restrict)mq->i_face_normal;
   const cs_real_3_t *restrict b_face_normal =
      (const cs_real_3_t *restrict)mq->b_face_normal;
-  const cs_real_3_t *restrict b_face_cog =
-     (const cs_real_3_t *restrict)mq->b_face_cog;
+  const cs_real_3_t *restrict b_face_cog
+    = (const cs_real_3_t *restrict)mq->b_face_cog;
 
   const int *bc_type = cs_glob_bc_type;
 
   /* Pointer to z_ground field */
   cs_field_t *f = cs_field_by_name_try("z_ground");
 
-  cs_real_t *restrict i_massflux =
-    cs_field_by_id(
-        cs_field_get_key_int(f, cs_field_key_id("inner_mass_flux_id")))->val;
-  cs_real_t *restrict b_massflux =
-    cs_field_by_id(
-        cs_field_get_key_int(f, cs_field_key_id("boundary_mass_flux_id")))->val;
+  cs_real_t *restrict i_massflux
+    = cs_field_by_id
+        (cs_field_get_key_int(f, cs_field_key_id("inner_mass_flux_id")))->val;
+  cs_real_t *restrict b_massflux
+    = cs_field_by_id
+        (cs_field_get_key_int(f, cs_field_key_id("boundary_mass_flux_id")))->val;
 
   cs_var_cal_opt_t vcopt;
   cs_field_get_key_struct(f, cs_field_key_id("var_cal_opt"), &vcopt);
 
-  cs_real_3_t normal;
+  cs_real_t normal[3];
   /* Normal direction is given by the gravity */
-  cs_math_3_normalise(
-      (const cs_real_t *)(cs_glob_physical_constants->gravity),
-      normal);
+  cs_math_3_normalise((const cs_real_t *)(cs_glob_physical_constants->gravity),
+                      normal);
 
   for (int i = 0; i < 3; i++)
     normal[i] *= -1.;
@@ -398,7 +478,6 @@ cs_atmo_z_ground_compute(void)
 
   for (cs_lnum_t face_id = 0; face_id < m->n_b_faces; face_id++)
     b_massflux[face_id] = cs_math_3_dot_product(normal, b_face_normal[face_id]);
-
 
   /* Boundary conditions
    *====================*/
@@ -516,10 +595,10 @@ cs_atmo_z_ground_compute(void)
                                      NULL);
 
   /* Free memory */
+
   BFT_FREE(dpvar);
   BFT_FREE(rhs);
   BFT_FREE(rovsdt);
-
 }
 
 /*----------------------------------------------------------------------------*/
@@ -545,7 +624,6 @@ cs_atmo_chemistry_set_spack_file_name(const char *file_name)
              char);
 
   sprintf(_atmo_chem.spack_file_name, "%s", file_name);
-
 }
 
 /*----------------------------------------------------------------------------*/
@@ -571,7 +649,6 @@ cs_atmo_chemistry_set_aerosol_file_name(const char *file_name)
              char);
 
   sprintf(_atmo_chem.aero_file_name, "%s", file_name);
-
 }
 
 /*----------------------------------------------------------------------------*/
@@ -651,7 +728,7 @@ cs_atmo_declare_chem_from_spack(void)
                 line_num);
 
     /* The order is already ok */
-    _atmo_chem.chempoint[i] = i+1;//FIXME ?
+    _atmo_chem.chempoint[i] = i+1; //FIXME ?
 
     /* Build name of the field:
      * species_name in lower case */
@@ -701,7 +778,6 @@ cs_atmo_compute_solar_angles(cs_real_t latitude,
                              cs_real_t *omega,
                              cs_real_t *fo)
 {
-
   /* 1 - initialisations */
   *fo = 1370.;
 
@@ -714,16 +790,18 @@ cs_atmo_compute_solar_angles(cs_real_t latitude,
 
   /* 2 - compute declinaison (maximum error < 3 mn) */
 
-  cs_real_t decl = 0.006918 - 0.399912*cos(t00) + 0.070257*sin(t00)
-    - 0.006758*cos(2.*t00) + 0.000907*sin(2.*t00) - 0.002697*cos(3.*t00)
-     + 0.001480*sin(3.*t00);
+  cs_real_t decl
+    =   0.006918 - 0.399912*cos(t00) + 0.070257*sin(t00)
+      - 0.006758*cos(2.*t00) + 0.000907*sin(2.*t00) - 0.002697*cos(3.*t00)
+      + 0.001480*sin(3.*t00);
 
   /* 3 - compute local solar hour
    * equation du temps     erreur maxi    35 secondes
    */
 
-  cs_real_t eqt = (0.000075 + 0.001868*cos(t00) - 0.032077*sin(t00)
-      - 0.014615*cos(2.*t00) - 0.040849*sin(2.*t00))*12./cs_math_pi;
+  cs_real_t eqt
+    = (  0.000075 + 0.001868*cos(t00) - 0.032077*sin(t00)
+       - 0.014615*cos(2.*t00) - 0.040849*sin(2.*t00)) * 12./cs_math_pi;
 
   cs_real_t local_time = utc + flong + eqt;
 
@@ -751,7 +829,7 @@ cs_atmo_compute_solar_angles(cs_real_t latitude,
   }
   *omega -= cs_glob_atmo_option->domain_orientation * cs_math_pi / 180.;
 
-  /* 5 - calcul de l'albedo sur mer qui depend de l'angle zenithal */
+  /* 5 - compute albedo at sea which depends on the zenithal angle */
 
   if (sea_id == 1) {
     cs_real_t ho = acos(*muzero);
@@ -768,10 +846,10 @@ cs_atmo_compute_solar_angles(cs_real_t latitude,
     corfo=(r0/r)**2
     precision better than e-04 */
 
-  cs_real_t corfo = 1.000110 + 0.034221*cos(t00) + 0.001280*sin(t00)
-    + 0.000719*cos(2.*t00) + 0.000077*sin(2.*t00);
+  cs_real_t corfo
+    =   1.000110 + 0.034221*cos(t00) + 0.001280*sin(t00)
+      + 0.000719*cos(2.*t00) + 0.000077*sin(2.*t00);
   *fo *= corfo;
-
 }
 
 /*----------------------------------------------------------------------------*/
@@ -783,21 +861,21 @@ cs_atmo_compute_solar_angles(cs_real_t latitude,
 void
 cs_atmo_chemistry_log_setup(void)
 {
-  cs_log_printf
-    (CS_LOG_SETUP,
-     _("\nAtmospheric chemistry options\n"
-       "---------------------\n\n"));
+  cs_log_printf(CS_LOG_SETUP,
+                _("\n"
+                  "Atmospheric chemistry options\n"
+                  "-----------------------------\n\n"));
 
   if (cs_glob_atmo_chemistry->model == 0) {
 
     /* No atmospheric chemistry */
-    cs_log_printf
-      (CS_LOG_SETUP,
-       _("  No atmospheric chemistry\n\n"));
+    cs_log_printf(CS_LOG_SETUP,
+                  _("  No atmospheric chemistry\n\n"));
 
-  } else if (cs_glob_atmo_chemistry->model == 1
-             || cs_glob_atmo_chemistry->model == 2
-             || cs_glob_atmo_chemistry->model == 3) {
+  }
+  else if (   cs_glob_atmo_chemistry->model == 1
+           || cs_glob_atmo_chemistry->model == 2
+           || cs_glob_atmo_chemistry->model == 3) {
 
     /* Pre-defined schemes */
     cs_log_printf

src/atmo/cs_atmo.h
@@ -108,7 +108,14 @@ typedef struct {
   cs_real_t longitude;
   /*! latitude of the domain origin */
   cs_real_t latitude;
-
+  /*! numbers of altitudes for the dynamics */
+  int nbmetd;
+  /*! numbers of altitudes for the temperature and specific humidity */
+  int nbmett;
+  /* numbers of time steps for the meteo profiles */
+  int nbmetm;
+  /* Number of vertical levels */
+  int nbmaxt;
   /*! Domain orientation (angle in degree between y direction and north),
    * 0 by default */
   cs_real_t domain_orientation;
@@ -119,6 +126,19 @@ typedef struct {
   int deposition_model;
   int nucleation_model;
   int subgrid_model;
+  int imeteo;
+
+  /* Altitudes of the temperature profile */
+  cs_real_t *z_temp_met;
+  /* Time (in sec) of the meteo profile */
+  cs_real_t *time_met;
+  /* Hydrostatic pressure from Laplace integration */
+  cs_real_t *hyd_p_met;
+  /* Fractional nebulosity */
+  cs_real_t *frac_neb;
+  /* Diagnosed nebulosity */
+  cs_real_t *diag_neb;
+
 } cs_atmo_option_t;
 
 /*----------------------------------------------------------------------------

src/base/cs_c_bindings.f90
@@ -2339,32 +2339,6 @@ module cs_c_bindings
 
     !---------------------------------------------------------------------------
 
-    !> \brief Return pointers to atmo includes
-
-    subroutine cs_f_atmo_get_pointers(syear, squant, shour, smin, ssec, &
-        longitude, latitude,                                            &
-        compute_z_ground,                                               &
-        sedimentation_model, deposition_model, nucleation_model,        &
-        subgrid_model, ichemistry, nespg, nrg, chem_with_photo,         &
-        iaerosol, frozen_gas_chem, init_gas_with_lib,                   &
-        init_aero_with_lib, n_aero, n_sizebin)                          &
-      bind(C, name='cs_f_atmo_get_pointers')
-      use, intrinsic :: iso_c_binding
-      implicit none
-      type(c_ptr), intent(out) :: compute_z_ground, ichemistry, nespg, nrg
-      type(c_ptr), intent(out) :: sedimentation_model, deposition_model
-      type(c_ptr), intent(out) :: nucleation_model
-      type(c_ptr), intent(out) :: subgrid_model
-      type(c_ptr), intent(out) :: syear, squant, shour, smin, ssec
-      type(c_ptr), intent(out) :: longitude, latitude
-      type(c_ptr), intent(out) :: iaerosol, frozen_gas_chem
-      type(c_ptr), intent(out) :: init_gas_with_lib, init_aero_with_lib
-      type(c_ptr), intent(out) :: n_aero, n_sizebin, chem_with_photo
-
-    end subroutine cs_f_atmo_get_pointers
-
-    !---------------------------------------------------------------------------
-
     !> \brief Compute the relative ground elevation (mainly for the atmospheric
     !>  module).
 
@@ -2388,6 +2362,20 @@ module cs_c_bindings
 
     !---------------------------------------------------------------------------
 
+    !> \brief Return pointers to atmo arrays
+
+    subroutine cs_f_atmo_arrays_get_pointers(p_ztmet, p_tmmet, &
+         p_phmet, dim_phmet,                                   &
+         p_nn, p_nebdia)                                       &
+         bind(C, name='cs_f_atmo_arrays_get_pointers')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      integer(c_int), dimension(2) :: dim_phmet
+      type(c_ptr), intent(out) :: p_ztmet, p_tmmet, p_phmet, p_nn, p_nebdia
+    end subroutine cs_f_atmo_arrays_get_pointers
+
+    !---------------------------------------------------------------------------
+
     !> \brief Deallocate arrays for atmo chemistry
 
     subroutine cs_f_atmo_chem_finalize() &
@@ -2395,6 +2383,16 @@ module cs_c_bindings
       use, intrinsic :: iso_c_binding
       implicit none
     end subroutine cs_f_atmo_chem_finalize
+
+    !---------------------------------------------------------------------------
+
+    !> \brief Deallocate arrays for atmo module
+
+    subroutine cs_f_atmo_finalize() &
+         bind(C, name='cs_f_atmo_finalize')
+      use, intrinsic :: iso_c_binding
+      implicit none
+    end subroutine cs_f_atmo_finalize
 
     !---------------------------------------------------------------------------
 

commit 82081d54a43869d08ef5294c1dfd5de9802e81a3

Fix mapping of some atmospheric arrays, broken by commit 5a386656 (yesterday).

src/atmo/atincl.f90
@@ -608,7 +608,8 @@ contains
 
   end subroutine atmo_init
 
-  !=============================================================================
+!===============================================================================
+
 !> \brief Initialisation of meteo data
 subroutine init_meteo
 
@@ -625,10 +626,13 @@ type(c_ptr) :: c_hyd_p_met
 
 integer(c_int),   dimension(2) :: dim_hyd_p_met
 
-call cs_f_atmo_arrays_get_pointers(                &
-     c_z_temp_met, c_time_met,                     &
-     c_hyd_p_met, dim_hyd_p_met,                   &
-     c_frac_neb, c_diag_neb)
+if (imeteo.gt.0) then
+  call atlecm(0)
+endif
+
+call cs_f_atmo_arrays_get_pointers(c_z_temp_met, c_time_met,     &
+                                   c_hyd_p_met, dim_hyd_p_met,   &
+                                   c_frac_neb, c_diag_neb)
 
 call c_f_pointer(c_z_temp_met, ztmet, [nbmaxt])
 call c_f_pointer(c_time_met, tmmet, [nbmetm])
@@ -638,20 +642,11 @@ call c_f_pointer(c_diag_neb, nebdia, [ncelet])
 
 ! Allocate additional arrays for Water Microphysics
 
-if (ippmod(iatmos).ge.2) then
-  allocate(nebdia(ncelet))
-  allocate(nn(ncelet))
-endif
-
 if (imeteo.gt.0) then
-
-  imode = 0
-
-  call atlecm ( imode )
 
   ! NB : only ztmet,ttmet,qvmet,ncmet are extended to 11000m if iatr1=1
   !           rmet,tpmet,phmet
-  allocate(tmmet(nbmetm), zdmet(nbmetd), ztmet(nbmaxt))
+  allocate(zdmet(nbmetd))
   allocate(dpdt_met(nbmetd))
   allocate(mom(3, nbmetd))
   allocate(mom_met(3, nbmetd))
@@ -660,7 +655,7 @@ if (imeteo.gt.0) then
   allocate(ttmet(nbmaxt,nbmetm), qvmet(nbmaxt,nbmetm), ncmet(nbmaxt,nbmetm))
   allocate(pmer(nbmetm))
   allocate(xmet(nbmetm), ymet(nbmetm))
-  allocate(rmet(nbmaxt,nbmetm), tpmet(nbmaxt,nbmetm), phmet(nbmaxt,nbmetm))
+  allocate(rmet(nbmaxt,nbmetm), tpmet(nbmaxt,nbmetm))
 
   ! Allocate and initialize auto inlet/outlet flag
 
@@ -675,7 +670,7 @@ if (imeteo.gt.0) then
 
     imode = 0
 
-    call usatdv ( imode )
+    call usatdv(imode)
 
     allocate(xyvert(nvert,3), zvert(kmx))
     allocate(acinfe(kmx), dacinfe(kmx), aco2(kmx,kmx), aco2s(kmx,kmx))
@@ -686,9 +681,9 @@ if (imeteo.gt.0) then
 
     allocate(soilvert(nvert))
 
-    call mestcr  ("rayi",  len("rayi"), 1, 0, idrayi)
-    call mestcr  ("rayst", len("rayst"), 1, 0, idrayst)
-    call gridcr  ("int_grid", len("int_grid"), igrid)
+    call mestcr("rayi",  len("rayi"), 1, 0, idrayi)
+    call mestcr("rayst", len("rayst"), 1, 0, idrayst)
+    call gridcr("int_grid", len("int_grid"), igrid)
 
     if (irdu.eq.1) then
       allocate(iru(kmx,nvert), ird(kmx,nvert))
@@ -705,6 +700,7 @@ endif
 end subroutine init_meteo
 
 !=============================================================================
+
 !> \brief Final step for deallocation
 subroutine finalize_meteo
 
@@ -715,21 +711,16 @@ implicit none
 
 call cs_f_atmo_finalize()
 
-if (ippmod(iatmos).ge.2) then
-  deallocate(nebdia)
-  deallocate(nn)
-endif
-
 if (imeteo.gt.0) then
 
-  deallocate(tmmet, zdmet, ztmet)
+  deallocate(zdmet)
   deallocate(mom, mom_met, dpdt_met)
   deallocate(umet, vmet, wmet)
   deallocate(ekmet, epmet)
   deallocate(ttmet, qvmet, ncmet)
   deallocate(pmer)
   deallocate(xmet, ymet)
-  deallocate(rmet, tpmet, phmet)
+  deallocate(rmet, tpmet)
 
   deallocate(iautom)
 