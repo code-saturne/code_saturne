code-saturne/code_saturne

commit 66050b8f4db9c0deb65d77c7af876bc32c88abbb

Improve compiler version reporting.
Also remove remaining traces of Blue Gene/Q support.

src/base/cs_system_info.c
@@ -54,10 +54,6 @@
 #include <pwd.h>
 #endif
 
-#if defined(__bgq__) && defined(__xlc__)
-#include <spi/include/kernel/location.h>
-#endif
-
 #if defined(HAVE_CUDA)
 #include "cs_base_cuda.h"
 #endif
@@ -242,6 +238,43 @@ _sys_info_issue(char      *issue_str,
 #endif
 }
 
+#if defined(HAVE_MPI)
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Determine min an max number of ranks per node
+ *
+ * \param[in]  comm            associated MPI communicator
+ * \param[in]  ranks_per_node  number of ranks per node (min and max)
+ */
+/*----------------------------------------------------------------------------*/
+
+static void
+_mpi_ranks_per_node(MPI_Comm  comm,
+                    int       ranks_per_node[2])
+{
+#if (MPI_VERSION < 3)
+  ranks_per_node[0] = -1;
+  ranks_per_node[1] = -1;
+#else
+
+  MPI_Comm sh_comm;
+  MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 0,
+                      MPI_INFO_NULL, &sh_comm);
+  int sh_ranks;
+  MPI_Comm_rank(sh_comm, &sh_ranks);
+  sh_ranks += 1;
+  MPI_Allreduce(MPI_IN_PLACE, &sh_ranks, 1, MPI_INT, MPI_MAX, sh_comm);
+  MPI_Comm_free(&sh_comm);
+
+  MPI_Allreduce(&sh_ranks, ranks_per_node, 1, MPI_INT, MPI_MIN, comm);
+  MPI_Allreduce(&sh_ranks, ranks_per_node+1, 1, MPI_INT, MPI_MAX, comm);
+
+#endif
+}
+
+#endif /* defined(HAVE_MPI) */
+
 /*----------------------------------------------------------------------------*/
 /*!
  * \brief Print available system information.
@@ -290,11 +323,6 @@ _system_info(bool  log)
 
 # if defined(HAVE_MPI)
   int mpi_flag = 0;
-#endif
-
-#if defined(__bgq__) && defined(__xlc__)
-  Personality_t personality;
-  Kernel_GetPersonality(&personality, sizeof(personality));
 #endif
 
   /* Date */
@@ -350,9 +378,7 @@ _system_info(bool  log)
 
   /* Available memory */
 
-#if defined(__bgq__) && defined(__xlc__)
-  ram = personality.DDR_Config.DDRSizeMB;
-#elif defined(__linux__) \
+#if defined(__linux__) \
    && defined(HAVE_SYS_SYSINFO_H) && defined(HAVE_SYSINFO)
   {
     struct sysinfo info;
@@ -374,9 +400,9 @@ _system_info(bool  log)
 
 #if defined(HAVE_GETPWUID) && defined(HAVE_GETEUID)
 
-  /* Functions not available on IBM Blue Gene or Cray XT,
+  /* Functions not available on Cray XT,
      but a stub may exist, so we make sure we ignore it */
-#if   defined(__bg__) || defined(_CRAYC) \
+#if   defined(_CRAYC) \
    || defined(__CRAYXT) || defined(__CRAYXE) || defined(__CRAYXC)
   pwd_user = NULL;
 #else
@@ -425,45 +451,8 @@ _system_info(bool  log)
     MPI_Comm_size(comm, &n_ranks);
     MPI_Comm_size(MPI_COMM_WORLD, &n_world_ranks);
 
-#   if defined(__bgq__) && defined(__xlc__)
-
-    {
-      int a_torus, b_torus, c_torus, d_torus, e_torus;
-      int n_flags = personality.Network_Config.NetFlags;
-      int n_hw_threads = Kernel_ProcessorCount();
-
-      for (log_id = 0; log_id < n_logs; log_id++) {
-        cs_log_printf(logs[log_id],
-                      "  %s%d\n", _("MPI ranks:           "), n_ranks);
-        if (n_world_ranks > n_ranks)
-          cs_log_printf(logs[log_id],
-                        "  %s%d\n", _("MPI_COMM_WORLD size: "),
-                        n_world_ranks);
-        cs_log_printf(logs[log_id],
-                      "  %s%d\n", _("Hardware threads:    "), n_hw_threads);
-      }
-
-      if (n_flags & ND_ENABLE_TORUS_DIM_A) a_torus = 1; else a_torus = 0;
-      if (n_flags & ND_ENABLE_TORUS_DIM_B) b_torus = 1; else b_torus = 0;
-      if (n_flags & ND_ENABLE_TORUS_DIM_C) c_torus = 1; else c_torus = 0;
-      if (n_flags & ND_ENABLE_TORUS_DIM_D) d_torus = 1; else d_torus = 0;
-      if (n_flags & ND_ENABLE_TORUS_DIM_E) e_torus = 1; else e_torus = 0;
-
-      for (log_id = 0; log_id < n_logs; log_id++) {
-        cs_log_printf(logs[log_id],
-                      "  %s<%d,%d,%d,%d,%d>\n", _("Block shape:         "),
-                      personality.Network_Config.Anodes,
-                      personality.Network_Config.Bnodes,
-                      personality.Network_Config.Cnodes,
-                      personality.Network_Config.Dnodes,
-                      personality.Network_Config.Enodes);
-        cs_log_printf(logs[log_id],
-                      "  %s<%d,%d,%d,%d,%d>\n", _("Torus links enabled: "),
-                      a_torus, b_torus, c_torus, d_torus, e_torus);
-      }
-    }
-
-#   else
+    int ranks_per_node[2];
+    _mpi_ranks_per_node(comm, ranks_per_node);
 
     {
       int appnum = -1;
@@ -486,14 +475,22 @@ _system_info(bool  log)
           cs_log_printf(logs[log_id],
                         "  %s%d\n", _("MPI ranks:           "), n_ranks);
 
+        if (ranks_per_node[0] > 0 && ranks_per_node[0] < n_ranks) {
+          if (ranks_per_node[0] == ranks_per_node[1])
+            cs_log_printf(logs[log_id],
+                          "  %s%d\n", _("MPI ranks per node:  "),
+                          ranks_per_node[0]);
+          else
+            cs_log_printf(logs[log_id],
+                          "  %s%d - %d\n", _("MPI ranks per node:  "),
+                          ranks_per_node[0], ranks_per_node[1]);
+        }
         if (n_world_ranks > n_ranks)
           cs_log_printf(logs[log_id],
                         "  %s%d\n", _("MPI_COMM_WORLD size: "),
                         n_world_ranks);
       }
     }
-
-#   endif /* defined(HAVE_MPI) */
 
   }
 
@@ -521,7 +518,29 @@ _system_info(bool  log)
 #if defined(HAVE_CUDA)
   for (log_id = 0; log_id < n_logs; log_id++)
     cs_base_cuda_device_info(log_id);
-#else
+#endif
+
+#if    defined(CS_CC_VERSION_STRING) || defined(CS_CXX_VERSION_STRING) \
+    || defined(CS_FC_VERSION_STRING) || defined(CS_NVCC_VERSION_STRING)
+  for (log_id = 0; log_id < n_logs; log_id++) {
+    cs_log_printf(logs[log_id], "\n  Compilers used for build:\n");
+#   if defined(CS_CC_VERSION_STRING)
+    cs_log_printf(logs[log_id],
+                  "    %s%s\n", _("C compiler:        "), CS_CC_VERSION_STRING);
+#   endif
+#   if defined(CS_CXX_VERSION_STRING)
+    cs_log_printf(logs[log_id],
+                  "    %s%s\n", _("C++ compiler:      "), CS_CXX_VERSION_STRING);
+#   endif
+#   if defined(CS_FC_VERSION_STRING)
+    cs_log_printf(logs[log_id],
+                  "    %s%s\n", _("Fortran compiler:  "), CS_FC_VERSION_STRING);
+#   endif
+#   if defined(CS_NVCC_VERSION_STRING)
+    cs_log_printf(logs[log_id],
+                  "    %s%s\n", _("CUDA compiler:     "), CS_NVCC_VERSION_STRING);
+#   endif
+  }
 #endif
 }
 

Makefile.am
@@ -250,16 +250,6 @@ bin/code_saturne: $(srcdir)/bin/code_saturne.$(LAUNCHER_EXT).in
 cs_version.h: Makefile $(top_srcdir)/src/base/cs_version.h.in
 	$(PYTHON) ${top_srcdir}/build-aux/cs_version.py --replace ${top_srcdir}/src/base/cs_version.h.in cs_version.h
 
-config/code_saturne_build.cfg.in: $(srcdir)/Makefile.in $(top_builddir)/config.status
-	@case '$?' in \
-	  *config.status*) \
-	    echo ' $(SHELL) ./config.status'; \
-	    $(SHELL) ./config.status;; \
-	  *) \
-	    echo ' cd $(top_builddir) && $(SHELL) ./config.status $@ $(am__maybe_remake_depfiles)'; \
-	    cd $(top_builddir) && $(SHELL) ./config.status $@ $(am__maybe_remake_depfiles);; \
-	esac;
-
 lib/code_saturne_build.cfg: Makefile config/code_saturne_build.cfg.in
 	rm -f $@ $@.tmp
 	$(MKDIR_P) lib

bin/cs_case.py
@@ -843,45 +843,6 @@ class case:
 
     #---------------------------------------------------------------------------
 
-    def generate_solver_mpmd_configfile_bgq(self, n_procs, mpi_env):
-        """
-        Generate MPMD mpiexec config file for BG/Q.
-        """
-
-        e_path = os.path.join(self.exec_dir, 'mpmd_configfile')
-        e = open(e_path, 'w')
-
-        rank_id = 0
-
-        for d in (self.syr_domains + self.domains + self.py_domains):
-            cmd = '#mpmdbegin %d-%d\n' % (rank_id, rank_id + d.n_procs - 1)
-            e.write(cmd)
-            s_args = d.solver_command()
-            if s_args[1][0:2] == './':
-                s_path = os.path.relpath(os.path.join(s_args[0], s_args[1]),
-                                         self.exec_dir)
-            else:
-                s_path = s_args[1]
-            cmd = '#mpmdcmd ' + s_path + s_args[2] \
-                + ' -w ' + os.path.basename(s_args[0]) + '\n'
-            e.write(cmd)
-            e.write('#mpmdend\n')
-            rank_id += d.n_procs
-
-        e.write('#mapping ABCDET\n')
-
-        # NOTE: adding the mapping line above seems to help in some cases where
-        # the mapping file is not interpreted correctly, but seems not to be
-        # always required (the documentation is minimalistic);
-        # with driver V1R2M0_17, reading of the mapping file seems fragile
-        # and subject to random failures unless this line is added.
-
-        e.close()
-
-        return e_path
-
-    #---------------------------------------------------------------------------
-
     def generate_solver_mpmd_script(self, n_procs, mpi_env):
         """
         Generate MPMD dispatch file.
@@ -1017,18 +978,7 @@ class case:
 
             elif mpi_env.mpmd & cs_exec_environment.MPI_MPMD_configfile:
 
-                if mpi_env.type == 'BGQ_MPI':
-                    e_path = self.generate_solver_mpmd_configfile_bgq(n_procs,
-                                                                      mpi_env)
-                    if mpi_env.mpiexec == 'srun':
-                        mpi_cmd += '--launcher-opts=\'--mapping ' + e_path + '\' '
-                    else:
-                        mpi_cmd += '--mapping ' + e_path + ' '
-                    if mpi_env.mpiexec_separator != None:
-                        mpi_cmd += mpi_env.mpiexec_separator + ' '
-                    mpi_cmd += self.package_compute.get_solver()
-
-                elif mpi_env.mpiexec == 'srun':
+                if mpi_env.mpiexec == 'srun':
                     e_path = self.generate_solver_mpmd_configfile_srun(n_procs,
                                                                        mpi_env)
                     mpi_cmd += '--multi-prog ' + e_path

bin/cs_exec_environment.py
@@ -1421,7 +1421,6 @@ class mpi_environment:
                                'MSMPI':self.__init_msmpi__,
                                'OpenMPI':self.__init_openmpi__,
                                'BullxMPI':self.__init_openmpi__,
-                               'BGQ_MPI':self.__init_bgq__,
                                'Platform_MPI':self.__init_platform_mpi__}
             if self.type in mpi_env_by_type:
                 init_method = mpi_env_by_type[self.type]
@@ -1788,46 +1787,6 @@ class mpi_environment:
         # Info commands
 
         self.info_cmds = ['ompi_info -a']
-
-    #---------------------------------------------------------------------------
-
-    def __init_bgq__(self, p, resource_info=None, wdir = None):
-
-        """
-        Initialize for Blue Gene/Q environment.
-        """
-
-        # Set base executable path
-
-        self.mpiexec = 'runjob'
-
-        # Determine processor count and MPMD handling
-
-        self.mpiexec_n = ' --np '
-        self.mpmd = MPI_MPMD_configfile
-
-        rm = None
-        ppn = 1
-        if resource_info != None:
-            rm = resource_info.manager
-            ppn = resource_info.n_procs_per_node()
-        if rm == 'SLURM':
-            self.mpiexec = 'srun'
-            self.mpiexec_n = ' --ntasks='
-            if ppn:
-                self.mpiexec_n_per_node = ' --ntasks-per-node=' + str(ppn)
-        else:
-            if ppn != 1:
-                self.mpiexec_n_per_node = ' --ranks-per-node ' + str(ppn)
-            self.mpiexec_separator = ':'
-
-        # Other options to add
-
-        # self.mpiexec_exe = '--exe'
-        # self.mpiexec_args = '--args'
-        # self.mpiexec_envs = '--envs OMP_NUM_THREADS=' + str(omp_num_threads)
-
-        # Info commands
 
     #---------------------------------------------------------------------------
 

config/code_saturne_build.cfg.in.in
@@ -73,13 +73,10 @@ cxx: @CXX@
 fc: @FC@
 ld: @CS_LD@
 
-cc_vendor: @cc_vendor@
-cc_version: @cc_version@
-cxx_vendor: @cxx_vendor@
-cxx_version: @cxx_version@
-fc_vendor: @fc_vendor@
-fc_version: @fc_version@
-nvcc_version: @nvcc_version@
+cc_version: @cs_cc_version_string@
+cxx_version: @cs_cxx_version_string@
+fc_version: @cs_fc_version_string@
+nvcc_version: @cs_nvcc_version_string@
 
 cflags: @CFLAGS@ @CFLAGS_DBG@ @CFLAGS_OPT@
 cxxflags: @CXXFLAGS@ @CXXFLAGS_DBG@ @CXXFLAGS_OPT@

config/cs_auto_flags.sh
@@ -64,8 +64,12 @@
 # Two other environment variable strings are defined, containing possibly
 # more detailed compiler information:
 #
-# cs_ac_cc_version      # Compiler version string, 1 line max.
-# cs_ac_cc_version_full # Compiler version string, 10 lines max.
+# cs_ac_cc_version       # C compiler version string, 1 line max.
+# cs_ac_cc_version_full  # C compiler version string, 10 lines max.
+# cs_ac_cxx_version      # C++ compiler version string, 1 line max.
+# cs_ac_cxx_version_full # C++ compiler version string, 10 lines max.
+# cs_ac_fc_version       # Fortran compiler version string, 1 line max.
+# cs_ac_fc_version_full  # Fortran compiler version string, 10 lines max.
 
 # The sourcing approach and some tests are borrowed from the HDF5 configure
 # environment.
@@ -95,7 +99,7 @@ _______EOF
 save_LANG=$LANG
 unset LANG;
 
-# Libraries only added on special cases (such as IBM Blue Gene),
+# Libraries only added on special cases),
 # so initialize empty variables here
 
 libs_default=""
@@ -250,7 +254,6 @@ if test "x$cs_gcc" = "xgcc"; then
 elif test "x$cs_gcc" = "xicc" ; then
 
   cs_cc_version=`echo $cs_ac_cc_version | grep ICC |sed 's/[a-zA-Z()]//g'`
-  cs_cc_vendor='Intel ICC Classic'
   echo "compiler '$CC' is Intel ICC Classic"
 
   # Version strings for logging purposes and known compiler flag
@@ -284,7 +287,6 @@ elif test "x$cs_gcc" = "xicc" ; then
 elif test "x$cs_gcc" = "xicc" -o "x$cs_gcc" = "xicx" ; then
 
   cs_cc_version=`echo $cs_ac_cc_version | grep ICX |sed 's/[a-zA-Z()]//g'`
-  cs_cc_vendor='Intel ICC NextGen'
   echo "compiler '$CC' is Intel ICC NextGen"
 
   # Version strings for logging purposes and known compiler flag
@@ -365,7 +367,7 @@ if test "x$cs_cc_compiler_known" != "xyes" ; then
   $CC -qversion 2>&1 | grep 'XL C' > /dev/null
   if test "$?" = "0" ; then
 
-    echo "compiler '$CC' is IBM XL C compiler"
+    echo "compiler '$CC' is IBM XL C/C++ compiler"
 
     # Version strings for logging purposes and known compiler flag
     $CC -qversion > $outfile 2>&1
@@ -385,21 +387,6 @@ if test "x$cs_cc_compiler_known" != "xyes" ; then
     ldflags_default_opt="-O3"
     ldflags_default_dbg="-g -qfullpath"
     ldflags_rpath="-R"
-
-    # Adjust options for IBM Blue Gene cross-compiler
-
-    grep 'Blue Gene' $outfile > /dev/null
-    if test "$?" = "0" ; then
-      # Default compiler flags (we assume that MPI wrappers are used)
-      if test -f /bgsys/drivers/ppcfloor/cnk/bin/bgq_kernel.elf ; then
-        cs_ibm_bg_type="Q"
-        cppflags_default=""
-        cflags_default=""                    # "-qlanglvl=extc99" by default
-        cflags_default_opt="-g -O3"
-        cflags_default_hot="-g -O3 -qhot"
-        cflags_default_dbg="-g"
-      fi
-    fi
 
   fi
 fi
@@ -598,7 +585,6 @@ elif test "x$cs_gxx" = "xicpc"; then
 
   # Version strings for logging purposes and known compiler flag
   $CXX $user_CXXFLAGS -V conftest.c > $outfile 2>&1
-  cs_cxx_vendor='Intel ICC Classic'
   cs_cxx_compiler_known=yes
 
   cs_cxx_vers_major=`echo $cs_ac_cxx_version | cut -f 3 -d" " | cut -f1 -d.`
@@ -632,7 +618,6 @@ elif test "x$cs_gxx" = "xicpc" -o "x$cs_gxx" = "xicpx"; then
 
   # Version strings for logging purposes and known compiler flag
   $CXX $user_CXXFLAGS -V conftest.c > $outfile 2>&1
-  cs_cxx_vendor='Intel ICC NextGen'
   cs_cxx_compiler_known=yes
 
   cs_cxx_vers_major=`echo $cs_ac_cxx_version | cut -f 3 -d" " | cut -f1 -d.`
@@ -721,19 +706,6 @@ if test "x$cs_cxx_compiler_known" != "xyes" ; then
     cxxflags_default_dbg="-g"
     cxxflags_default_omp="-qsmp=omp -qthreaded"
     cxxflags_default_std=""
-
-    # Adjust options for IBM Blue Gene cross-compiler
-
-    grep 'Blue Gene' $outfile > /dev/null
-    if test "$?" = "0" ; then
-      # Default compiler flags (we assume that MPI wrappers are used)
-      if test -f /bgsys/drivers/ppcfloor/cnk/bin/bgq_kernel.elf ; then
-        cxxflags_default="-qlanglvl=redefmac"
-        cxxflags_default_opt="-g -O3"
-        cxxflags_default_hot="-g -O3 -qhot"
-        cxxflags_default_dbg="-g"
-      fi
-    fi
 
   fi
 fi
@@ -928,7 +900,6 @@ if test "x$cs_fc_compiler_known" != "xyes" ; then
   if test "$?" = "0" ; then
 
     cs_fc_version=`echo $cs_ac_fc_version | sed 's/[a-zA-Z()]//g'`
-    cs_fc_vendor='Intel IFORT NextGen'
 
     echo "compiler '$FC' is Intel Fortran NextGen"
 
@@ -1003,21 +974,6 @@ if test "x$cs_fc_compiler_known" != "xyes" ; then
     fcflags_default_opt="-O3"
     fcflags_default_omp="-qsmp=omp -qthreaded"
 
-    # Adjust options for IBM Blue Gene cross-compiler
-
-    grep 'Blue Gene' $outfile > /dev/null
-
-    if test "$?" = "0" ; then
-
-      # Default compiler flags (we assume that MPI wrappers are used)
-      if test -f /bgsys/drivers/ppcfloor/cnk/bin/bgq_kernel.elf ; then
-        fcflags_default="-qextname -qsuffix=cpp=f90"
-        fcflags_default_dbg="-g -qcheck"
-        fcflags_default_opt="-g -O3"
-        fcflags_default_hot="-g -O3 -qhot"
-      fi
-    fi
-
   fi
 fi
 
@@ -1083,6 +1039,8 @@ cs_nvcc_version="`$NVCC -version 2>&1 |grep 'release' |\
                   sed 's/.*release \([-a-z0-9\.]*\).*/\1/'`"
 
 echo "compiler '${NVCC}' version ${cs_nvcc_version}"
+
+cs_ac_nvcc_version="`$NVCC -version 2>&1 |grep 'release' | head -1`"
 
 # Default compiler flags
 nvccflags_default=""

configure.ac
@@ -263,13 +263,26 @@ if test "x$auto_flags" = "xyes" ; then
   AC_SUBST(FCFLAGS_HOT)
 
   AC_SUBST(LDRPATH)
-  AC_SUBST(cc_vendor, [${cs_cc_vendor}])
-  AC_SUBST(cc_version, [${cs_cc_version}])
-  AC_SUBST(cxx_vendor, [${cs_cxx_vendor}])
-  AC_SUBST(cxx_version, [${cs_cxx_version}])
-  AC_SUBST(fc_vendor, [${cs_fc_vendor}])
-  AC_SUBST(fc_version, [${cs_fc_version}])
-  AC_SUBST(nvcc_version, [${cs_nvcc_version}])
+  AC_SUBST(cs_cc_version_string, [${cs_ac_cc_version}])
+  AC_SUBST(cs_cxx_version_string, [${cs_ac_cxx_version}])
+  AC_SUBST(cs_fc_version_string, [${cs_ac_fc_version}])
+  AC_SUBST(cs_nvcc_version_string, [${cs_ac_nvcc_version}])
+
+  if test "x$cs_ac_cc_version" != "x" -a "x$cs_ac_cc_version" != "xunknown" ; then
+    AC_DEFINE_UNQUOTED([CS_CC_VERSION_STRING], ["$cs_ac_cc_version"], [C compiler version string])
+    echo "yes $cs_ac_cc_version"
+  else
+    echo "no $cs_ac_cc_version"
+  fi
+  if test "x$cs_ac_cxx_version" != "x" -a "x$cs_ac_cxx_version" != "xunknown" ; then
+    AC_DEFINE_UNQUOTED([CS_CXX_VERSION_STRING], ["$cs_ac_cxx_version"], [C++ compiler version string])
+  fi
+  if test "x$cs_ac_fc_version" != "x" -a "x$cs_ac_fc_version" != "xunknown" ; then
+    AC_DEFINE_UNQUOTED([CS_FC_VERSION_STRING], ["$cs_ac_fc_version"], [Fortran compiler version string])
+  fi
+  if test "x$cs_ac_nvcc_version" != "x" ; then
+    AC_DEFINE_UNQUOTED([CS_NVCC_VERSION_STRING], ["$cs_ac_nvcc_version"], [CUDA compiler version string])
+  fi
 
 fi
 
@@ -406,7 +419,7 @@ AC_ARG_ENABLE(long-gnum,
 )
 
 if test "x$cs_have_long_gnum" = "xyes"; then
-  AC_DEFINE([HAVE_LONG_GNUM],1,[Use 64-bit type if available for cs_gnum_t.])
+  AC_DEFINE([HAVE_LONG_GNUM], 1, [Use 64-bit type if available for cs_gnum_t.])
 fi
 AC_SUBST(cs_have_long_gnum)
 

m4/cs_blas.m4
@@ -107,13 +107,7 @@ if test "x$with_blas" != "xno" ; then
 
       if test "$1" = "yes" -o "x$with_blas_libs" = "x"; then # Threaded version ?
 
-        if test "x$cs_ibm_bg_type" = "xQ"; then
-          BLAS_LIBS="-lesslbg"
-          BLAS_LDFLAGS="-L$with_blas/lib64"
-          BLASRUNPATH="-R$with_blas/lib64"
-        else
-          BLAS_LIBS="-lesslsmp"
-        fi
+        BLAS_LIBS="-lesslsmp"
 
         LDFLAGS="${saved_LDFLAGS} ${BLAS_LDFLAGS}"
         LIBS=" ${BLAS_LIBS} ${saved_LIBS}"
@@ -130,9 +124,7 @@ if test "x$with_blas" != "xno" ; then
       if test "$cs_have_blas" = "no" ; then # Test for non-threaded version
                                             # or explicitely specified libs
 
-        if test "x$cs_ibm_bg_type" = "xQ" ; then
-          BLAS_LIBS="-lesslbg"
-        elif test "x$with_blas_libs" != "x" -a "x$with_blas_type" = "xESSL"; then
+        if test "x$with_blas_libs" != "x" -a "x$with_blas_type" = "xESSL"; then
           BLAS_LIBS="$with_blas_libs"
         else
           BLAS_LIBS="-lessl"

commit 822b1e8bc6e60287f97d23a0e8d3ca3a4a8cfa5d

Fix external couplings, broken by commit 66050b8f (Jun 21 2020).

src/base/cs_system_info.c
@@ -259,7 +259,7 @@ _mpi_ranks_per_node(MPI_Comm  comm,
 #else
 
   MPI_Comm sh_comm;
-  MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 0,
+  MPI_Comm_split_type(comm, MPI_COMM_TYPE_SHARED, 0,
                       MPI_INFO_NULL, &sh_comm);
   int sh_ranks;
   MPI_Comm_rank(sh_comm, &sh_ranks);