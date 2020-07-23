code-saturne/code_saturne

commit 5f63f244b48bde832aa3fee72d400dcf37585978

Fix python coupling launcher with srun mpmd paradigme
  * The launch command for python script was not generated correctly on
    slurm with mpmd, thus leading to a crash in the execution.
  * Fix the srun-mpmd script generator for a python domain
  * Modify the pyple_coupler class of pyple to account for the "-wdir"
    flag and change the script execution directory if necessary

bin/cs_case.py
@@ -806,7 +806,7 @@ class case:
 
         rank_id = 0
 
-        for d in (self.syr_domains + self.domains + self.py_domains):
+        for d in (self.syr_domains + self.domains):
             s_args = d.solver_command()
             if s_args[1][0:2] == './':
                 s_path = os.path.join(s_args[0], s_args[1])
@@ -816,6 +816,19 @@ class case:
             cmd = '%d-%d\t' % (rank_id, rank_id + d.n_procs - 1) \
                    + s_path + s_args[2] \
                    + ' -wdir ' + os.path.basename(s_args[0]) + '\n'
+            e.write(cmd)
+            rank_id += d.n_procs
+
+        for d in self.py_domains:
+            s_args = d.solver_command()
+            _pypath, _pyscript = a_args[1].split(" ")
+            _rundir = os.path.basename(s_args[0])
+
+            cmd  = '%d-%d\t' % (rank_id, rank_id + d.n_procs - 1)
+            cmd += '%s %s/%s\t' % (_pypath, _rundir, _pyscript)
+            cmd += s_args[2]
+            cmd += ' -wdir %s' % _rundir
+
             e.write(cmd)
             rank_id += d.n_procs
 

bin/cs_case_coupling.py
@@ -170,7 +170,7 @@ def coupling(package,
                 dom = python_domain(package,
                                     name = d.get('domain'),
                                     cmd_line = d.get('command_line'),
-                                    script_name = d.get('py_code'))
+                                    script_name = d.get('script'))
 
             except Exception:
                 err_str = 'Cannot create Python code domain.\n'

bin/cs_create.py
@@ -502,7 +502,7 @@ domains = [
     ,
     {'solver': 'PYTHON_CODE',
      'domain': 'DOMAIN',
-     'py_code': 'pycode.py',
+     'script': 'pycode.py',
      'command_line': '',
      'n_procs_weight': None,
      'n_procs_min': 1,

libple/pyple/pyple_coupler.py
@@ -61,9 +61,22 @@ class pyple_coupler():
 
         self.name = None
 
+        # Check if we need to change directory
         if run_dir:
+            # user imposed directly
             import os
             os.chdir(run_dir)
+
+        else:
+            # imposed from arguments (mpmd like launch)
+            import sys
+            if len(sys.argv) > 1:
+                for i, arg in enumerate(sys.argv):
+                    if arg == "-wdir" and i+1 <= len(sys.argv) - 1:
+                        import os
+                        run_dir = sys.argv[i+1]
+                        os.chdir(run_dir)
+                        break
 
         self.user_options = {'rundir':run_dir,
                              'verbos':verbosity,

commit 029fa859526bd4a06aa7aed1e186cf60cbc8dc51

Fix crash with python coupling introduced in commit 5f63f244b4

bin/cs_case.py
@@ -821,7 +821,7 @@ class case:
 
         for d in self.py_domains:
             s_args = d.solver_command()
-            _pypath, _pyscript = a_args[1].split(" ")
+            _pypath, _pyscript = s_args[1].split(" ")
             _rundir = os.path.basename(s_args[0])
 
             cmd  = '%d-%d\t' % (rank_id, rank_id + d.n_procs - 1)