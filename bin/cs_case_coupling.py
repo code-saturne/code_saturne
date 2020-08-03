code-saturne/code_saturne

commit f154f60e54e9969bfe5ea95dd525d9972c01165a

[GUI] Add the possibility to edit coupling parameters stored in run.cfg

bin/cs_run.py
@@ -231,7 +231,10 @@ def process_cmd_line(argv, pkg):
     # Check for multiple domain case
     # Kept the 'coupling' file def for the definition of the case_dir function
     coupling = None
-    coupled_domains = get_coupling_parameters_from_run_conf(run_conf)
+    coupling_domains =[]
+    if run_conf:
+        coupled_domains = run_conf.get_coupling_parameters()
+
     if coupled_domains != []:
         coupling = run_config_path
 
@@ -472,39 +475,6 @@ def generate_run_config_file(path, resource_name, r_c, s_c, pkg):
     run_conf = cs_run_conf.run_conf(None)
     run_conf.sections = sections
     run_conf.save(path, new=True)
-
-#-------------------------------------------------------------------------------
-# Retrieve coupling related dictionary
-#-------------------------------------------------------------------------------
-
-def get_coupling_parameters_from_run_conf(run_conf):
-    """
-    Return the list of coupled domains defined inside a run.cfg.
-    Empty list if single-case run.
-    """
-
-    domains = []
-
-    if not run_conf:
-        return domains
-
-    domain_names = run_conf.get('setup', 'coupled_domains')
-
-    if domain_names:
-        for d in domain_names.split(":"):
-            dtemp = run_conf.sections[d.lower()]
-            domains.append({})
-
-            for key in dtemp.keys():
-                if dtemp[key] and dtemp[key] != 'None':
-                    if key in ('n_procs_max', 'n_procs_min', 'n_procs_weight'):
-                        domains[-1][key] = int(dtemp[key])
-                    else:
-                        domains[-1][key] = str(dtemp[key])
-                else:
-                    domains[-1][key] = None
-
-    return domains
 
 #===============================================================================
 # Run the calculation

bin/cs_gui.py
@@ -197,8 +197,12 @@ def main(argv, pkg):
         app.processEvents()
         QTimer.singleShot(1500, splash.hide)
 
-    from code_saturne.Base.MainView import MainView
-    mv = MainView(cmd_package = pkg, cmd_case = case)
+    if os.path.split(case)[1] == "run.cfg":
+        from code_saturne.Base.QCouplingEditorView import QCouplingEditor
+        mv = QCouplingEditor(cfgfile=case, standalone_mode=True)
+    else:
+        from code_saturne.Base.MainView import MainView
+        mv = MainView(cmd_package = pkg, cmd_case = case)
 
     try:
         mv.show()

bin/cs_run_conf.py
@@ -581,6 +581,35 @@ class run_conf(object):
             f.write(line + br)
         f.close()
 
+    #---------------------------------------------------------------------------
+
+    def get_coupling_parameters(self):
+        """
+        Return the list of coupled domains defined inside a run.cfg.
+        Empty list if single-case run.
+        """
+
+        domains = []
+
+        domain_names = self.get('setup', 'coupled_domains')
+
+        if domain_names:
+            for dom in domain_names.split(":"):
+                d = self.sections[dom.lower()]
+                domains.append({})
+
+                for key in d.keys():
+                    if d[key] and d[key] != 'None':
+                        if key in ('n_procs_max', 'n_procs_min', 'n_procs_weight'):
+                            domains[-1][key] = int(d[key])
+                        else:
+                            domains[-1][key] = str(d[key])
+                    else:
+                        domains[-1][key] = None
+
+        return domains
+
+
 #-------------------------------------------------------------------------------
 # End
 #-------------------------------------------------------------------------------

bin/cs_script.py
@@ -40,6 +40,7 @@ class master_script:
                          'bdump':self.bdump,
                          'compile':self.compile,
                          'config':self.config,
+                         'cplgui':self.gui,
                          'create':self.create,
                          'gui':self.gui,
                          'studymanagergui':self.studymanager_gui,

gui/Base/MainForm.ui
@@ -207,6 +207,7 @@
     <bool>false</bool>
    </attribute>
    <addaction name="runOrSubmitAction"/>
+   <addaction name="editCouplingParametersAction"/>
   </widget>
   <action name="fileOpenAction">
    <property name="icon">
@@ -636,6 +637,21 @@
    </property>
    <property name="statusTip">
     <string>Run computation</string>
+   </property>
+  </action>
+  <action name="editCouplingParametersAction">
+   <property name="icon">
+    <iconset resource="resource_base.qrc">
+     <normaloff>:/icons/22x22/coupling_editor.png</normaloff>:/icons/22x22/coupling_editor.png</iconset>
+   </property>
+   <property name="text">
+    <string>Edit coupling parameters</string>
+   </property>
+   <property name="toolTip">
+    <string>Edit coupling parameters</string>
+   </property>
+   <property name="statusTip">
+    <string>Edit coupling parameters</string>
    </property>
   </action>
  </widget>

gui/Base/MainView.py
@@ -378,6 +378,8 @@ class MainView(object):
 
         self.runOrSubmitAction.triggered.connect(self.runOrSubmit)
 
+        self.editCouplingParametersAction.triggered.connect(self.editCouplingParams)
+
         self.openXtermAction.triggered.connect(self.openXterm)
         self.displayCaseAction.triggered.connect(self.displayCase)
 
@@ -1315,6 +1317,59 @@ class MainView(object):
         if hasattr(self, 'case'):
             dialog = BatchRunningDialogView(self, self.case)
             dialog.show()
+
+
+    def editCouplingParams(self):
+        """
+        """
+
+        open_editor = True
+        if not hasattr(self, 'case'):
+            open_editor = False
+        else:
+            if not hasattr(self, 'IdPthMdl'):
+                self.IdPthMdl = IdentityAndPathesModel(self.case)
+
+            fic = self.IdPthMdl.getXmlFileName()
+            if not fic:
+                open_editor = False
+            else:
+                file_dir = os.path.split(fic)[0]
+                if file_dir:
+                    if not os.path.basename(file_dir) == "DATA":
+                        open_editor = False
+
+        if not open_editor:
+            title = self.tr("Warning")
+            msg   = self.tr("Warning: you can only manage coupling parameters "\
+                            "inside the run.cfg if you are editing a code_saturne "\
+                            "case. Otherwise launch code_saturne cplgui run.cfg")
+
+            QMessageBox.warning(self, title, msg)
+            return
+
+        studydir = os.path.split(os.path.split(file_dir)[0])[0]
+        if studydir:
+            if not os.path.isfile(os.path.join(studydir, "run.cfg")):
+                open_editor = False
+        else:
+            open_editor = False
+
+        if not open_editor:
+            title = self.tr("Warning")
+            msg   = self.tr("Warning: could not find a run.cfg file")
+            QMessageBox.warning(self, title, msg)
+            return
+
+        path_to_cfg = os.path.join(studydir, "run.cfg")
+
+        from code_saturne.Base.QCouplingEditorView import QCouplingEditor
+
+        couplingEditor = QCouplingEditor(parent=self,
+                                         cfgfile=path_to_cfg)
+
+        couplingEditor.show()
+
 
 
     def runOTMode(self):

gui/Base/Makefile.am
@@ -41,6 +41,7 @@ BrowserView.py \
 CommandMgrDialogView.py \
 CompletionTextEditor.py \
 MainView.py \
+QCouplingEditorView.py \
 QFileEditor.py \
 QtCase.py \
 QtPage.py \

gui/Base/QCouplingEditorView.py
@@ -1,0 +1,416 @@
+# -*- coding: utf-8 -*-
+
+#-------------------------------------------------------------------------------
+
+# This file is part of Code_Saturne, a general-purpose CFD tool.
+#
+# Copyright (C) 1998-2020 EDF S.A.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+# Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+#-------------------------------------------------------------------------------
+
+"""
+This module defines the view for coupling parameters handling.
+"""
+#-------------------------------------------------------------------------------
+# Standard modules
+#-------------------------------------------------------------------------------
+
+import sys, os, shutil
+import logging
+from code_saturne.Base.QtCore    import *
+from code_saturne.Base.QtGui     import *
+from code_saturne.Base.QtWidgets import *
+
+# Check if QString exists
+has_qstring = True
+try:
+    from code_saturne.Base.QtCore import QString
+    _fromUtf8 = QString.fromUtf8
+except ImportError:
+    has_qstring = False
+    def _fromUtf8(s):
+        return s
+
+    def QString(s):
+        return s
+
+try:
+    # PyQt5
+    from code_saturne.Base.QtWidgets import QMainWindow, QMessageBox, \
+        QAction, QFileDialog, QTextEdit, QPlainTextEdit, QSizePolicy, QMenu, QMessageBox
+except Exception:
+    # PyQt4
+    from code_saturne.Base.QtGui import QMainWindow, QMessageBox, \
+        QAction, QFileDialog, QTextEdit, QPlainTextEdit, QSizePolicy, QMenu, QMessagBox
+
+#-------------------------------------------------------------------------------
+# log config
+#-------------------------------------------------------------------------------
+
+logging.basicConfig()
+log = logging.getLogger("QCouplingEditorView")
+log.setLevel(GuiParam.DEBUG)
+
+#-------------------------------------------------------------------------------
+# CouplingEditorView class
+#-------------------------------------------------------------------------------
+
+_ordered_keys = ['domain', 'solver', 'n_procs_min', 'n_procs_max', 'n_procs_weight']
+
+#-------------------------------------------------------------------------------
+# domain_entry class
+#-------------------------------------------------------------------------------
+
+class domain_entry(object):
+    """
+    Class for domain data entry management.
+    """
+
+    # ---------------------------------------------------------------
+    def __init__(self, data_dict):
+        """
+        Constructor
+        """
+
+        self.name   = None
+        self.n_ent  = 0
+        self.keys   = []
+        self.labels = []
+        self.vals   = []
+
+        keys = data_dict.keys()
+
+        for k in _ordered_keys:
+            if k in keys:
+                self.keys.append(k)
+                self.labels.append(self.__create_label__(k))
+                self.vals.append(self.__create_line__(data_dict[k]))
+
+                if k in ('domain', 'solver'):
+                    self.vals[-1].setEnabled(False)
+                    if k == 'domain':
+                        self.name = data_dict[k]
+
+                self.n_ent += 1
+
+        for k in keys:
+            if k not in _ordered_keys:
+                self.keys.append(k)
+                self.labels.append(self.__create_label__(k))
+                self.vals.append(self.__create_line__(data_dict[k]))
+                self.n_ent += 1
+    # ---------------------------------------------------------------
+
+    # ---------------------------------------------------------------
+    def __create_label__(self, text):
+
+        label = QLabel()
+
+        label.setText(text)
+        label.setAlignment(Qt.AlignLeft|Qt.AlignVCenter)
+
+        return label
+    # ---------------------------------------------------------------
+
+    # ---------------------------------------------------------------
+    def __create_line__(self, val):
+
+        line = QLineEdit()
+        line.setText(str(val))
+
+        return line
+    # ---------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# CouplingEditorView class
+#-------------------------------------------------------------------------------
+
+class CouplingEditorView(QWidget):
+
+    # ---------------------------------------------------------------
+    def __init__(self, parent, cfgfile=None):
+        """
+        Constructor
+        """
+
+        QWidget.__init__(self, parent)
+
+        self.cfgfile=cfgfile
+
+        self.data  = []
+        self.pages = []
+
+        self.data_modified = False
+        self.file_loaded   = False
+
+        self.initUI(self.cfgfile)
+    # ---------------------------------------------------------------
+
+    # ---------------------------------------------------------------
+    def load_cfg_file(self, cfgfile=None):
+        """
+        Load a configuration file
+        """
+
+        self.cfgfile = cfgfile
+        if self.cfgfile:
+            from code_saturne import cs_run_conf
+            self.run_conf = cs_run_conf.run_conf(self.cfgfile)
+            if self.run_conf:
+                for dom in self.run_conf.get_coupling_parameters():
+                    self.add_tab_data(dom)
+
+                if self.data != []:
+                    self.file_loaded   = True
+                self.data_modified = False
+    # ---------------------------------------------------------------
+
+    # ---------------------------------------------------------------
+    def save_cfg_file(self):
+        """
+        Save the configuration file
+        """
+
+        for d in self.data:
+            secname = d.name.lower()
+            for entry in range(d.n_ent):
+                self.run_conf.set(secname,
+                                  str(d.keys[entry]),
+                                  str(d.vals[entry].text()))
+
+        self.run_conf.save()
+    # ---------------------------------------------------------------
+
+    # ---------------------------------------------------------------
+    def initUI(self, cfgfile):
+        """
+        Create the visual layout
+        """
+
+        # Generate dictionary here
+        self.load_cfg_file(cfgfile)
+
+        if not self.file_loaded:
+           title = self.tr("Warning")
+           msg   = self.tr("File %s does not contain coupling parameters." % cfgfile)
+           QMessageBox.warning(self, title, msg)
+           return
+
+        self.tabwidget = QTabWidget(self)
+        vbox = QVBoxLayout()
+
+        # Title
+        l1 = QLabel()
+        l1.setText("Coupled cases")
+        l1.setAlignment(Qt.AlignCenter)
+        lfont = QFont()
+        lfont.setBold(True)
+        l1.setFont(lfont)
+
+        # File
+        fileLabel = QLabel()
+        fileLabel.setText("Parameters file: ")
+        fileLabel.setFont(lfont)
+        filePLabel = QLabel()
+        filePLabel.setText(cfgfile)
+        filePLabel.setFont(lfont)
+        fileHbox = QHBoxLayout()
+        fileHbox.addWidget(fileLabel)
+        fileHbox.addWidget(filePLabel)
+        fileHbox.addStretch(1)
+
+        # Final layout
+        vbox.addWidget(l1)
+        vbox.addLayout(fileHbox)
+        vbox.addWidget(self.tabwidget)
+
+        self.setLayout(vbox)
+
+        for i, d in enumerate(self.data):
+            dom = d.name
+            if dom:
+                tab_name = "domain: %s" % dom
+            else:
+                tab_name = "domain: unknown"
+
+            self.pages.append(self.add_tab(d))
+            self.tabwidget.addTab(self.pages[i], tab_name)
+
+
+        self.tabwidget.setCurrentIndex(0)
+    # ---------------------------------------------------------------
+
+    # ---------------------------------------------------------------
+    def add_tab_data(self, data):
+        """
+        Add tab data
+        """
+
+        self.data.append(domain_entry(data))
+    # ---------------------------------------------------------------
+
+    # ---------------------------------------------------------------
+    def add_tab(self, dom_data):
+        """
+        Add a tab to the tabWidget
+        """
+
+        rows = dom_data.n_ent
+        cols = 2
+
+        Labels = dom_data.labels
+        LineEdits = dom_data.vals
+
+        layout = QGridLayout()
+
+        for row in range(rows):
+            layout.addWidget(Labels[row], row, 0)
+            layout.addWidget(LineEdits[row]  , row, 1)
+            LineEdits[row].textChanged[str].connect(self.dataModifed)
+
+        lSpacer = QLabel()
+        lSpacer.setText(30*" ")
+        vSpacer = QSpacerItem(40, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        layout.addWidget(lSpacer, rows, 0)
+        layout.addItem(vSpacer, rows, 1)
+
+
+        tab = QWidget()
+        tab.setLayout(layout)
+
+        return tab
+    # ---------------------------------------------------------------
+
+    # ---------------------------------------------------------------
+    @pyqtSlot()
+    def dataModifed(self):
+
+        self.data_modified = True
+    # ---------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# QCouplingEditor class (Independent window)
+#-------------------------------------------------------------------------------
+
+class QCouplingEditor(QMainWindow):
+    """
+    Independant window
+    """
+
+    # ---------------------------------------------------------------
+    def __init__(self, parent=None, cfgfile=None, standalone_mode=False):
+        """
+        Constructor
+        """
+
+        super(QCouplingEditor, self).__init__(parent)
+        self.setGeometry(50, 50, 500, 300)
+
+        self.setWindowTitle("Coupling parameters editor")
+        self.parent = parent
+        self.stdalone = standalone_mode
+
+        self.cfgfile = cfgfile
+
+        self.editorView = CouplingEditorView(self, None, self.cfgfile)
+
+        # If standalone mode, exit if file not loaded
+        if self.stdalone:
+            if not self.editorView.file_loaded:
+                self.close()
+                sys.exit(0)
+                return None
+
+        # Save file action
+        save_img_path = ":/icons/22x22/document-save.png"
+        icon_save     = QIcon()
+        icon_save.addPixmap(QPixmap(_fromUtf8(save_img_path)),
+                            QIcon.Normal,
+                            QIcon.Off)
+        self.saveFileAction = QAction(icon_save, "Save", self)
+        self.saveFileAction.setShortcut("Ctrl+S")
+        self.saveFileAction.setStatusTip('Save file')
+        self.saveFileAction.triggered.connect(self.saveFile)
+
+        # Close file action
+        close_img_path = ":/icons/22x22/process-stop.png"
+        icon_close     = QIcon()
+        icon_close.addPixmap(QPixmap(_fromUtf8(close_img_path)),
+                             QIcon.Normal,
+                             QIcon.Off)
+        self.closeFileAction = QAction(icon_close, "Close file", self)
+        self.closeFileAction.setShortcut("Ctrl+Q")
+        self.closeFileAction.setStatusTip('Close opened file')
+        self.closeFileAction.triggered.connect(self.closeOpenedFile)
+
+        # Toolbar
+        self.toolbar = self.addToolBar("Options")
+        self.toolbar.addAction(self.saveFileAction)
+        self.toolbar.addAction(self.closeFileAction)
+
+        self.setCentralWidget(self.editorView)
+    # ---------------------------------------------------------------
+
+    # ---------------------------------------------------------------
+    def saveFile(self):
+        """
+        Save the configuration file
+        """
+
+        self.editorView.save_cfg_file()
+        self.editorView.data_modified = False
+    # ---------------------------------------------------------------
+
+    # ---------------------------------------------------------------
+    def closeOpenedFile(self):
+        """
+        Close and save if necessary the opened configuration file
+        """
+
+        if self.editorView.file_loaded:
+            choice = QMessageBox.question(self, 'Coupling parameters editor',
+                                          'Exit editor ?',
+                                          QMessageBox.Yes | QMessageBox.No)
+        else:
+            choice = QMessageBox.Yes
+
+        if choice == QMessageBox.Yes:
+
+            if self.editorView.data_modified:
+                choice = QMessageBox.question(self, 'Coupling parameters editor',
+                                              'Save modifications ?',
+                                              QMessageBox.Yes | QMessageBox.No)
+                if choice == QMessageBox.Yes:
+                    self.saveFile()
+                else:
+                    pass
+
+            settings = QSettings()
+            settings.setValue("MainWindow/Geometry",
+                              self.saveGeometry())
+
+            self.editorView.file_loaded = False
+
+            self.close()
+            return 0
+        else:
+            return 1
+    # ---------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# END OF FILE
+#-------------------------------------------------------------------------------

commit 796455ec884c7e02a21755424e663d61a6635ae0

Fix code coupling broken by commit f154f60e (Jul 31 2020).

bin/cs_case_coupling.py
@@ -172,7 +172,7 @@ def coupling(package,
                 dom = syrthes_domain(package,
                                      cmd_line = d.get('opt'),
                                      name = domain_s,
-                                     param = d.get('script'),
+                                     param = d.get('param'),
                                      n_procs_weight = d.get('n_procs_weight'),
                                      n_procs_min = d.get('n_procs_min'),
                                      n_procs_max = d.get('n_procs_max'))

bin/cs_run.py
@@ -231,7 +231,8 @@ def process_cmd_line(argv, pkg):
     # Check for multiple domain case
     # Kept the 'coupling' file def for the definition of the case_dir function
     coupling = None
-    coupling_domains =[]
+    coupled_domains =[]
+
     if run_conf:
         coupled_domains = run_conf.get_coupling_parameters()
 

bin/studymanager/cs_studymanager_study.py
@@ -207,9 +207,8 @@ class Case(object):
             # Apply coupling parameters information
 
             from code_saturne import cs_case_coupling
-            from code_saturne.cs_run import get_coupling_parameters_from_run_conf
 
-            coupled_domains = get_coupling_parameters_from_run_conf(run_conf)
+            coupled_domains = run_conf.get_coupling_parameters()
 
             self.subdomains = []
             for d in coupled_domains:
@@ -745,6 +744,7 @@ class Study(object):
             os.chdir(c.label)
             refdir = os.path.join(self.__repo, c.label)
             retval = 1
+            resu_coupling = None
             for node in os.listdir(refdir):
                 ref = os.path.join(self.__repo, c.label, node)
                 if node in c.subdomains:
@@ -763,9 +763,9 @@ class Study(object):
                                                              package=self.__package)
                         if temp_run_conf.get('setup', 'coupled_domains'):
                             resu_coupling = 'RESU_COUPLING'
-                            if not os.path.isdir(resu_coupling):
-                                os.mkdir(resu_coupling)
-                            run_conf_path = 'run.cfg'
+
+            if resu_coupling and not os.path.isdir(resu_coupling):
+                os.mkdir(resu_coupling)
 
             create_local_launcher(self.__package, self.__dest)
             os.chdir(self.__dest)