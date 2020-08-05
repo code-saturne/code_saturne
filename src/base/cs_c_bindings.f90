code-saturne/code_saturne

commit 1facdcfec133d8a501aaf534f10982bab085bfdf

Untangle some dependencies.

src/base/cs_c_bindings.f90
@@ -2705,33 +2705,33 @@ module cs_c_bindings
 
     ! Init fluid mesh quantities
 
-    subroutine cs_mesh_init_fluid_quantities()   &
-      bind(C, name='cs_mesh_init_fluid_quantities')
+    subroutine cs_porous_model_init_fluid_quantities()   &
+      bind(C, name='cs_porous_model_init_fluid_quantities')
       use, intrinsic :: iso_c_binding
       implicit none
-    end subroutine cs_mesh_init_fluid_quantities
+    end subroutine cs_porous_model_init_fluid_quantities
 
     !---------------------------------------------------------------------------
 
     ! Set has_disable_flag
 
-    subroutine cs_mesh_quantities_set_has_disable_flag(flag)   &
-      bind(C, name='cs_mesh_quantities_set_has_disable_flag')
+    subroutine cs_porous_model_set_has_disable_flag(flag)   &
+      bind(C, name='cs_porous_model_set_has_disable_flag')
       use, intrinsic :: iso_c_binding
       implicit none
       integer(kind=c_int), value :: flag
-    end subroutine cs_mesh_quantities_set_has_disable_flag
+    end subroutine cs_porous_model_set_has_disable_flag
 
     !---------------------------------------------------------------------------
 
     ! Set porosity model.
 
-    subroutine cs_mesh_quantities_set_porous_model(iporos)   &
-      bind(C, name='cs_mesh_quantities_set_porous_model')
+    subroutine cs_porous_model_set_model(iporos)   &
+      bind(C, name='cs_porous_model_set_model')
       use, intrinsic :: iso_c_binding
       implicit none
       integer(kind=c_int), value :: iporos
-    end subroutine cs_mesh_quantities_set_porous_model
+    end subroutine cs_porous_model_set_model
 
     !---------------------------------------------------------------------------
 
@@ -2989,13 +2989,13 @@ module cs_c_bindings
 
     ! Interface to C function returning 1 for active cells
 
-    function cs_f_mesh_quantities_cell_is_active(cell_id) result(is_active) &
-      bind(C, name='cs_f_mesh_quantities_cell_is_active')
+    function cs_f_porous_model_cell_is_active(cell_id) result(is_active) &
+      bind(C, name='cs_f_porous_model_cell_is_active')
       use, intrinsic :: iso_c_binding
       implicit none
       integer(c_int), value :: cell_id
       integer(kind=c_int) :: is_active
-    end function cs_f_mesh_quantities_cell_is_active
+    end function cs_f_porous_model_cell_is_active
 
     !---------------------------------------------------------------------------
 
@@ -6158,7 +6158,7 @@ contains
 
 
     c_cell_id = iel - 1
-    c_is_active = cs_f_mesh_quantities_cell_is_active(c_cell_id)
+    c_is_active = cs_f_porous_model_cell_is_active(c_cell_id)
     is_active = c_is_active
 
   end function cell_is_active

src/base/cs_porous_model.c
@@ -1,0 +1,245 @@
+/*============================================================================
+ * Porous model management
+ *============================================================================*/
+
+/*
+  This file is part of Code_Saturne, a general-purpose CFD tool.
+
+  Copyright (C) 1998-2020 EDF S.A.
+
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation; either version 2 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+  Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/*----------------------------------------------------------------------------*/
+
+#include "cs_defs.h"
+
+/*----------------------------------------------------------------------------
+ * Standard C library headers
+ *----------------------------------------------------------------------------*/
+
+#include <math.h>
+#include <float.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+/*----------------------------------------------------------------------------
+ *  Local headers
+ *----------------------------------------------------------------------------*/
+
+#include "bft_mem.h"
+#include "bft_error.h"
+#include "bft_printf.h"
+
+#include "cs_base.h"
+#include "cs_field.h"
+#include "cs_log.h"
+#include "cs_math.h"
+#include "cs_mesh.h"
+#include "cs_mesh_quantities.h"
+#include "cs_preprocess.h"
+
+/*----------------------------------------------------------------------------
+ *  Header for the current file
+ *----------------------------------------------------------------------------*/
+
+#include "cs_porous_model.h"
+
+/*----------------------------------------------------------------------------*/
+
+BEGIN_C_DECLS
+
+/*----------------------------------------------------------------------------*/
+/*! \file cs_porous_model.c
+ *
+ * \brief Porous model management
+ */
+/*----------------------------------------------------------------------------*/
+
+/*! \cond DOXYGEN_SHOULD_SKIP_THIS */
+
+/*=============================================================================
+ * Local Macro definitions
+ *============================================================================*/
+
+/*============================================================================
+ * Local type definitions
+ *============================================================================*/
+
+/*============================================================================
+ * Static global variables
+ *============================================================================*/
+
+/* Choice of the porous model */
+int cs_glob_porous_model = 0;
+
+/*============================================================================
+ * Prototypes for functions intended for use only by Fortran wrappers.
+ * (descriptions follow, with function bodies).
+ *============================================================================*/
+
+void
+cs_f_porous_model_get_pointers(int   **iporos);
+
+int
+cs_f_porous_model_cell_is_active(cs_lnum_t  cell_id);
+
+/*=============================================================================
+ * Private function definitions
+ *============================================================================*/
+
+/*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
+
+/*============================================================================
+ * Fortran wrapper function definitions
+ *============================================================================*/
+
+/*! \cond DOXYGEN_SHOULD_SKIP_THIS */
+
+/*----------------------------------------------------------------------------
+ * Get pointers to global variables.
+ *
+ * This function is intended for use by Fortran wrappers, and
+ * enables mapping to Fortran global pointers.
+ *
+ * parameters:
+ *   iporos  --> pointer to cs_glob_porous_model
+ *----------------------------------------------------------------------------*/
+
+void
+cs_f_porous_model_get_pointers(int   **iporos)
+{
+  *iporos = &(cs_glob_porous_model);
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Return 0 if cell is disabled, 1 otherwise
+ *
+ * \param[in]  cell_id
+ *
+ * \return  0  if cell is disabled
+ */
+/*----------------------------------------------------------------------------*/
+
+int
+cs_f_porous_model_cell_is_active(cs_lnum_t  cell_id)
+{
+  cs_mesh_quantities_t *mq = cs_glob_mesh_quantities;
+
+  return (1 - (mq->has_disable_flag
+          * mq->c_disable_flag[mq->has_disable_flag * cell_id]));
+}
+
+/*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
+
+/*=============================================================================
+ * Public function definitions
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------
+ * Set porous model option.
+ *
+ * parameters:
+ *   porous_model <-- porous model option (> 0 for porosity)
+ *----------------------------------------------------------------------------*/
+
+void
+cs_porous_model_set_model(int  porous_model)
+{
+  cs_glob_porous_model = porous_model;
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Set (unset) has_disable_flag
+ *
+ * \param[in]  flag   1: on, 0: off
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_porous_model_set_has_disable_flag(int  flag)
+{
+  cs_mesh_quantities_t *mq =cs_glob_mesh_quantities;
+
+  mq->has_disable_flag = flag;
+
+  if (mq->has_disable_flag == 1) {
+    cs_lnum_t n_cells_ext = cs_glob_mesh->n_cells_with_ghosts;
+    if (mq->c_disable_flag == NULL)
+      BFT_REALLOC(mq->c_disable_flag, n_cells_ext, int);
+    for (cs_lnum_t cell_id = 0; cell_id < n_cells_ext; cell_id++)
+      mq->c_disable_flag[cell_id] = 0;
+  }
+  else {
+    if (mq->c_disable_flag == NULL)
+      BFT_REALLOC(mq->c_disable_flag, 1, int);
+    mq->c_disable_flag[0] = 0;
+  }
+
+  /* if off, fluid surfaces point toward cell surfaces */
+  /* Porous models */
+  if (cs_glob_porous_model > 0) {
+    if (flag == 0) {
+      /* Set pointers of face quantities to the standard ones */
+      if (cs_glob_porous_model == 3) {
+        mq->i_f_face_normal = mq->i_face_normal;
+        mq->b_f_face_normal = mq->b_face_normal;
+        mq->i_f_face_surf   = mq->i_face_surf;
+        mq->b_f_face_surf   = mq->b_face_surf;
+        mq->i_f_face_factor = NULL;
+        mq->b_f_face_factor = NULL;
+      }
+      mq->cell_f_vol = mq->cell_vol;
+    }
+    else {
+      /* Use fluid surfaces and volumes */
+      if (cs_glob_porous_model == 3) {
+        mq->i_f_face_normal = cs_field_by_name("i_f_face_normal")->val;
+        mq->b_f_face_normal = cs_field_by_name("b_f_face_normal")->val;
+        mq->i_f_face_surf   = cs_field_by_name("i_f_face_surf")->val;
+        mq->b_f_face_surf   = cs_field_by_name("b_f_face_surf")->val;
+        mq->i_f_face_factor
+          = (cs_real_2_t *)cs_field_by_name("i_f_face_factor")->val;
+        mq->b_f_face_factor = cs_field_by_name("b_f_face_factor")->val;
+      }
+      mq->cell_f_vol        = cs_field_by_name("cell_f_vol")->val;
+    }
+  }
+
+  /* Update Fortran pointer quantities */
+  cs_preprocess_mesh_update_fortran();
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Init fluid quantities
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_porous_model_init_fluid_quantities(void)
+{
+  if (cs_glob_porous_model == 3) {
+    cs_mesh_init_fluid_sections(cs_glob_mesh, cs_glob_mesh_quantities);
+  }
+}
+
+/*----------------------------------------------------------------------------*/
+
+END_C_DECLS

src/base/cs_porous_model.h
@@ -1,0 +1,96 @@
+#ifndef __CS_POROUS_MODEL_H__
+#define __CS_POROUS_MODEL_H__
+
+/*============================================================================
+ * Porous model management
+ *============================================================================*/
+
+/*
+  This file is part of Code_Saturne, a general-purpose CFD tool.
+
+  Copyright (C) 1998-2020 EDF S.A.
+
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation; either version 2 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+  Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+ *  Local headers
+ *----------------------------------------------------------------------------*/
+
+#include "cs_base.h"
+#include "cs_mesh.h"
+#include "cs_mesh_quantities.h"
+
+/*----------------------------------------------------------------------------*/
+
+BEGIN_C_DECLS
+
+/*=============================================================================
+ * Macro definitions
+ *============================================================================*/
+
+/*============================================================================
+ * Type definition
+ *============================================================================*/
+
+/*============================================================================
+ * Global variables
+ *============================================================================*/
+
+/* Choice of the porous model */
+extern int cs_glob_porous_model;
+
+/*=============================================================================
+ * Public function prototypes
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------
+ * Compute fluid volumes and fluid surfaces in addition to cell volumes
+ * and surfaces.
+ *
+ * parameters:
+ *   porous_model <-- porous model option (> 0 for porosity)
+ *----------------------------------------------------------------------------*/
+
+void
+cs_porous_model_set_model(int  porous_model);
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Set (unset) has_disable_flag
+ *
+ * \param[in]  flag   1: on, 0: off
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_porous_model_set_has_disable_flag(int  flag);
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Init fluid quantities
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_porous_model_init_fluid_quantities(void);
+
+/*----------------------------------------------------------------------------*/
+
+END_C_DECLS
+
+#endif /* __CS_POROUS_MODEL_H__ */

src/mesh/cs_mesh_quantities.c
@@ -53,7 +53,6 @@
 #include "cs_mesh_connect.h"
 #include "cs_parall.h"
 #include "cs_bad_cells_regularisation.h"
-#include "cs_preprocess.h"
 
 /*----------------------------------------------------------------------------
  *  Header for the current file
@@ -107,9 +106,6 @@ static int _ajust_face_cog_compat_v11_v52 = 0;
 unsigned cs_glob_mesh_quantities_flag =
   CS_BAD_CELLS_WARPED_CORRECTION + CS_FACE_DISTANCE_CLIP;
 
-/* Choice of the porous model */
-int cs_glob_porous_model = 0;
-
 /* Number of computation updates */
 
 static int _n_computations = 0;
@@ -120,13 +116,7 @@ static int _n_computations = 0;
  *============================================================================*/
 
 void
-cs_f_mesh_quantities_get_pointers(int   **iporos);
-
-void
 cs_f_mesh_quantities_fluid_vol_reductions(void);
-
-int
-cs_f_mesh_quantities_cell_is_active(cs_lnum_t  cell_id);
 
 /*=============================================================================
  * Private function definitions
@@ -2149,22 +2139,6 @@ _b_thickness(const cs_mesh_t             *m,
 /*! \cond DOXYGEN_SHOULD_SKIP_THIS */
 
 /*----------------------------------------------------------------------------
- * Get pointers to global variables.
- *
- * This function is intended for use by Fortran wrappers, and
- * enables mapping to Fortran global pointers.
- *
- * parameters:
- *   iporos  --> pointer to cs_glob_porous_model
- *----------------------------------------------------------------------------*/
-
-void
-cs_f_mesh_quantities_get_pointers(int   **iporos)
-{
-  *iporos = &(cs_glob_porous_model);
-}
-
-/*----------------------------------------------------------------------------
  * Compute the total, min, and max fluid volumes of cells
  *----------------------------------------------------------------------------*/
 
@@ -2176,25 +2150,6 @@ cs_f_mesh_quantities_fluid_vol_reductions(void)
 
   cs_mesh_quantities_fluid_vol_reductions(m,
                                           mq);
-}
-
-/*----------------------------------------------------------------------------*/
-/*!
- * \brief  Return 0 if cell is disabled, 1 otherwise
- *
- * \param[in]  cell_id
- *
- * \return  0  if cell is disabled
- */
-/*----------------------------------------------------------------------------*/
-
-int
-cs_f_mesh_quantities_cell_is_active(cs_lnum_t  cell_id)
-{
-  cs_mesh_quantities_t *mq = cs_glob_mesh_quantities;
-
-  return (1 - (mq->has_disable_flag
-          * mq->c_disable_flag[mq->has_disable_flag * cell_id]));
 }
 
 /*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
@@ -2244,80 +2199,6 @@ cs_mesh_quantities_face_cog_choice(int  algo_choice)
     _ajust_face_cog_compat_v11_v52 = algo_choice;
 
   return _ajust_face_cog_compat_v11_v52;
-}
-
-/*----------------------------------------------------------------------------
- * Compute fluid volumes and fluid surfaces in addition to cell volumes
- * and surfaces.
- *
- * parameters:
- *   porous_model <-- porous model option (> 0 for porosity)
- *----------------------------------------------------------------------------*/
-
-void
-cs_mesh_quantities_set_porous_model(int  porous_model)
-{
-  cs_glob_porous_model = porous_model;
-}
-
-/*----------------------------------------------------------------------------*/
-/*!
- * \brief  Set (unset) has_disable_flag
- *
- * \param[in]  flag 1: on, 0: off
- */
-/*----------------------------------------------------------------------------*/
-void
-cs_mesh_quantities_set_has_disable_flag(int  flag)
-{
-  cs_mesh_quantities_t *mq =cs_glob_mesh_quantities;
-
-  mq->has_disable_flag = flag;
-  /* if off, fluid surfaces point toward cell surfaces */
-  /* Porous models */
-  if (cs_glob_porous_model > 0) {
-    if (flag == 0) {
-      /* Set pointers of face quantities to the standard ones */
-      if (cs_glob_porous_model == 3) {
-        mq->i_f_face_normal = mq->i_face_normal;
-        mq->b_f_face_normal = mq->b_face_normal;
-        mq->i_f_face_surf   = mq->i_face_surf;
-        mq->b_f_face_surf   = mq->b_face_surf;
-        mq->i_f_face_factor = NULL;
-        mq->b_f_face_factor = NULL;
-      }
-      mq->cell_f_vol = mq->cell_vol;
-    } else {
-      /* Use fluid surfaces and volumes */
-      if (cs_glob_porous_model == 3) {
-        mq->i_f_face_normal = cs_field_by_name("i_f_face_normal")->val;
-        mq->b_f_face_normal = cs_field_by_name("b_f_face_normal")->val;
-        mq->i_f_face_surf   = cs_field_by_name("i_f_face_surf")->val;
-        mq->b_f_face_surf   = cs_field_by_name("b_f_face_surf")->val;
-        mq->i_f_face_factor
-          = (cs_real_2_t *)cs_field_by_name("i_f_face_factor")->val;
-        mq->b_f_face_factor = cs_field_by_name("b_f_face_factor")->val;
-      }
-      mq->cell_f_vol        = cs_field_by_name("cell_f_vol")->val;
-    }
-  }
-
-  /* Update Fortran pointer quantities */
-  cs_preprocess_mesh_update_fortran();
-}
-
-/*----------------------------------------------------------------------------*/
-/*!
- * \brief  Init fluid quantities
- */
-/*----------------------------------------------------------------------------*/
-
-void
-cs_mesh_init_fluid_quantities(void)
-{
-  if (cs_glob_porous_model == 3) {
-    cs_mesh_init_fluid_sections(cs_glob_mesh, cs_glob_mesh_quantities);
-  }
 }
 
 /*----------------------------------------------------------------------------*/
@@ -2688,26 +2569,10 @@ cs_mesh_quantities_compute(const cs_mesh_t       *m,
   mq->cell_f_vol = mq->cell_vol;
 
   /* Porous models */
-  if (cs_glob_porous_model > 0) {
-    mq->has_disable_flag = 1;
-  }
-  else {
-    mq->min_f_vol = mq->min_vol;
-    mq->max_f_vol = mq->max_vol;
-    mq->tot_f_vol = mq->tot_vol;
-  }
-
-  if (mq->has_disable_flag == 1) {
-    if (mq->c_disable_flag == NULL)
-      BFT_MALLOC(mq->c_disable_flag, n_cells_with_ghosts, int);
-    for (cs_lnum_t cell_id = 0; cell_id < n_cells_with_ghosts; cell_id++)
-      mq->c_disable_flag[cell_id] = 0;
-  }
-  else {
-    if (mq->c_disable_flag == NULL)
-      BFT_MALLOC(mq->c_disable_flag, 1, int);
-    mq->c_disable_flag[0] = 0;
-  }
+  mq->has_disable_flag = 0;
+  mq->min_f_vol = mq->min_vol;
+  mq->max_f_vol = mq->max_vol;
+  mq->tot_f_vol = mq->tot_vol;
 
   if (mq->i_dist == NULL)
     BFT_MALLOC(mq->i_dist, n_i_faces, cs_real_t);

src/alge/cs_bad_cells_regularisation.c
@@ -54,6 +54,7 @@
 #include "cs_mesh_bad_cells.h"
 #include "cs_mesh_quantities.h"
 #include "cs_parall.h"
+#include "cs_parameters.h"
 #include "cs_sles_default.h"
 
 /*----------------------------------------------------------------------------

src/alge/cs_convection_diffusion.c
@@ -64,6 +64,7 @@
 #include "cs_mesh_quantities.h"
 #include "cs_parall.h"
 #include "cs_parameters.h"
+#include "cs_porous_model.h"
 #include "cs_prototypes.h"
 #include "cs_timer.h"
 #include "cs_stokes_model.h"

src/alge/cs_divergence.c
@@ -60,6 +60,7 @@
 #include "cs_ext_neighborhood.h"
 #include "cs_mesh_quantities.h"
 #include "cs_parameters.h"
+#include "cs_porous_model.h"
 #include "cs_prototypes.h"
 #include "cs_timer.h"
 

src/alge/cs_face_viscosity.c
@@ -63,6 +63,7 @@
 #include "cs_mesh_quantities.h"
 #include "cs_parall.h"
 #include "cs_parameters.h"
+#include "cs_porous_model.h"
 #include "cs_prototypes.h"
 #include "cs_timer.h"
 

commit 818b9651f7c38d57294073054064a4e73cceb503

Fix use of disable flag from Fortran, broken by commit 1facdcfe (Jul 22 2020).

src/base/cs_c_bindings.f90
@@ -2752,6 +2752,16 @@ module cs_c_bindings
 
     !---------------------------------------------------------------------------
 
+    ! Initialize has_disable_flag
+
+    subroutine cs_porous_model_init_disable_flag()   &
+      bind(C, name='cs_porous_model_init_disable_flag')
+      use, intrinsic :: iso_c_binding
+      implicit none
+    end subroutine cs_porous_model_init_disable_flag
+
+    !---------------------------------------------------------------------------
+
     ! Set has_disable_flag
 
     subroutine cs_porous_model_set_has_disable_flag(flag)   &

src/base/cs_porous_model.c
@@ -166,6 +166,45 @@ cs_porous_model_set_model(int  porous_model)
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief  Initialize disable_flag
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_porous_model_init_disable_flag(void)
+{
+  cs_mesh_t *m =cs_glob_mesh;
+  cs_mesh_quantities_t *mq =cs_glob_mesh_quantities;
+
+  if (cs_glob_porous_model > 0) {
+    cs_lnum_t n_cells_ext = m->n_cells_with_ghosts;
+    if (mq->c_disable_flag == NULL) {
+      BFT_MALLOC(mq->c_disable_flag, n_cells_ext, int);
+      for (cs_lnum_t cell_id = 0; cell_id < n_cells_ext; cell_id++)
+        mq->c_disable_flag[cell_id] = 0;
+    }
+    else {
+      cs_lnum_t n_cells = m->n_cells;
+      BFT_REALLOC(mq->c_disable_flag, n_cells_ext, int);
+      for (cs_lnum_t cell_id = n_cells; cell_id < n_cells_ext; cell_id++)
+        mq->c_disable_flag[cell_id] = 0;
+    }
+    if (m->halo != NULL)
+      cs_halo_sync_untyped(m->halo, CS_HALO_STANDARD, sizeof(int),
+                           mq->c_disable_flag);
+  }
+  else {
+    if (mq->c_disable_flag == NULL)
+      BFT_MALLOC(mq->c_disable_flag, 1, int);
+    mq->c_disable_flag[0] = 0;
+  }
+
+  /* Update Fortran pointer quantities */
+  cs_preprocess_mesh_update_fortran();
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief  Set (unset) has_disable_flag
  *
  * \param[in]  flag   1: on, 0: off
@@ -178,19 +217,6 @@ cs_porous_model_set_has_disable_flag(int  flag)
   cs_mesh_quantities_t *mq =cs_glob_mesh_quantities;
 
   mq->has_disable_flag = flag;
-
-  if (mq->has_disable_flag == 1) {
-    cs_lnum_t n_cells_ext = cs_glob_mesh->n_cells_with_ghosts;
-    if (mq->c_disable_flag == NULL)
-      BFT_REALLOC(mq->c_disable_flag, n_cells_ext, int);
-    for (cs_lnum_t cell_id = 0; cell_id < n_cells_ext; cell_id++)
-      mq->c_disable_flag[cell_id] = 0;
-  }
-  else {
-    if (mq->c_disable_flag == NULL)
-      BFT_REALLOC(mq->c_disable_flag, 1, int);
-    mq->c_disable_flag[0] = 0;
-  }
 
   /* if off, fluid surfaces point toward cell surfaces */
   /* Porous models */

src/base/cs_porous_model.h
@@ -71,6 +71,15 @@ cs_porous_model_set_model(int  porous_model);
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief  Initialize disable_flag
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_porous_model_init_disable_flag(void);
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief  Set (unset) has_disable_flag
  *
  * \param[in]  flag   1: on, 0: off

src/base/cs_preprocess.c
@@ -63,6 +63,7 @@
 #include "cs_mesh_warping.h"
 #include "cs_parall.h"
 #include "cs_partition.h"
+#include "cs_porous_model.h"
 #include "cs_post.h"
 #include "cs_prototypes.h"
 #include "cs_preprocessor_data.h"
@@ -412,6 +413,7 @@ cs_preprocess_mesh(cs_halo_type_t   halo_type)
   cs_mesh_quantities_compute(cs_glob_mesh, cs_glob_mesh_quantities);
 
   /* If fluid_solid mode is activate: disable solid cells for the dynamics */
+  cs_porous_model_init_disable_flag();
   if (stokes->fluid_solid)
     cs_internal_coupling_tag_disable_cells(cs_glob_mesh, cs_glob_mesh_quantities);
 

src/base/iniva0.f90
@@ -282,8 +282,6 @@ if (iporos.ge.1) then
   call field_get_val_s(ipori, porosi)
   do iel = 1, ncelet
     porosi(iel) = 1.d0
-    ! No solid cells
-    isolid_0(iel) = 0
   enddo
 
   ! Tensorial porosity
@@ -298,9 +296,6 @@ if (iporos.ge.1) then
       porosf(6, iel) = 0.d0
     enddo
   endif
-
-else
-  isolid_0(1) = 0
 endif
 
 !===============================================================================

src/mesh/cs_mesh_quantities.c
@@ -2569,17 +2569,17 @@ cs_mesh_quantities_compute(const cs_mesh_t       *m,
   mq->cell_f_vol = mq->cell_vol;
 
   /* Porous models */
-  if (mq->has_disable_flag == 1) {
-    cs_lnum_t n_cells_ext = cs_glob_mesh->n_cells_with_ghosts;
-    if (mq->c_disable_flag == NULL)
-      BFT_REALLOC(mq->c_disable_flag, n_cells_ext, int);
-    for (cs_lnum_t cell_id = 0; cell_id < n_cells_ext; cell_id++)
-      mq->c_disable_flag[cell_id] = 0;
-  }
-  else {
-    if (mq->c_disable_flag == NULL)
-      BFT_REALLOC(mq->c_disable_flag, 1, int);
-    mq->c_disable_flag[0] = 0;
+  if (mq->c_disable_flag == NULL) {
+    if (mq->has_disable_flag == 1) {
+      cs_lnum_t n_cells_ext = cs_glob_mesh->n_cells_with_ghosts;
+      BFT_MALLOC(mq->c_disable_flag, n_cells_ext, int);
+      for (cs_lnum_t cell_id = 0; cell_id < n_cells_ext; cell_id++)
+        mq->c_disable_flag[cell_id] = 0;
+    }
+    else {
+      BFT_MALLOC(mq->c_disable_flag, 1, int);
+      mq->c_disable_flag[0] = 0;
+    }
   }
 
   mq->min_f_vol = mq->min_vol;