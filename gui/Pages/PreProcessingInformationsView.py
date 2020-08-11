code-saturne/code_saturne

commit 76b7940fa6cb21b65369e969cb329f00a8abde46

GUI: enable Qt translation mechanism.
Translation files are not maintained, but may be built in the folowing manner,
for a given language <lang> (where <lang> is the standard IETF language tag,
such as 'en_US', 'fr', 'fr_FR', 'zh', 'zh_CN', ...), and an installation
under <install_prefix>:

> cd gui
> mkdir -p translations && cd translations
> pylupdate5 ../*/*.py ../*/*.ui -ts code_saturne_<lang>.ts
> linguist-qt5 code_saturne_<lang>.ts
> lrelease code_saturne_<lang>.ts
> cp code_saturne_<lang>.ts <install_prefix>/share/locale

Using linguist to update the translations.

gui/Pages/PreProcessingInformationsView.py
@@ -94,13 +94,6 @@ def preprocessorFile(parent, initdir):
     return file_name
 
 
-
-def tr(text):
-    """
-    Translation
-    """
-    return text
-
 #-------------------------------------------------------------------------------
 # Informations class
 #-------------------------------------------------------------------------------

commit e9e31341f0c75ae1e967a0f10b7e9408c485399f

[GUI] Fix bug introduced in commit 76b7940f.
It closes issue #222.

gui/Pages/PreProcessingInformationsView.py
@@ -66,7 +66,7 @@ def preprocessorFile(parent, initdir):
     Verify if the choses of file is correct
     """
     file_name = ""
-    title = tr("Select a preprocessor log")
+    title = parent.tr("Select a preprocessor log")
     filetypes = "Preprocessor log (*.log);;All Files (*)"
     filt = "All files (*)"
     initdir = os.path.join(initdir, 'check_mesh')
@@ -86,9 +86,9 @@ def preprocessorFile(parent, initdir):
                 break
 
         if j == -1:
-            title = tr("Informations")
-            msg = tr("Warning : the selected file is not a correct file.\n\n"\
-                     "Verify your selection")
+            title = parent.tr("Informations")
+            msg = parent.tr("Warning : the selected file is not a correct file.\n\n"\
+                            "Verify your selection")
             QMessageBox.information(parent, title, msg)
 
     return file_name