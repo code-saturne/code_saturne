code-saturne/code_saturne

commit 6272af55767ba77b09f904f80ae5f565b1fd7179

Merge "turbulent Prandtl" number for turbulence model with the existing field key word (so remove "constants" sigmak, sigmae cpalse and their corresponding variable in C).
Also introduce the notion of turbulent Schmidt field for transported
scalars and k-epsilon turbulent variables.

src/turb/cs_turbulence_model.c
@@ -474,19 +474,6 @@ const double cs_turb_ce2 = 1.92;
 const double cs_turb_ce4 = 1.20;
 
 /*!
- * Prandtl number for \f$k\f$ with \f$k-\varepsilon\f$ and v2f models.
- * Useful if and only if \ref iturb=20, 21 or 50 (\f$k-\varepsilon\f$ or v2f).
- */
-const double cs_turb_sigmak = 1.0;
-
-/*!
- * Prandtl number for \f$\varepsilon\f$.
- * Useful if and only if \ref iturb= 20, 21, 30, 31 or 50
- * (\f$k-\varepsilon\f$, \f$R_{ij}-\varepsilon\f$ or v2f).
- */
-double cs_turb_sigmae = -1.;
-
-/*!
  * Constant \f$C_1\f$ for the \f$R_{ij}-\varepsilon\f$ LRR model.
  * Useful if and only if \ref iturb=30 (\f$R_{ij}-\varepsilon\f$ LRR).
  */
@@ -616,9 +603,6 @@ const double cs_turb_cpale3 = 2.3;
 
 /*! Specific constant of v2f "BL-v2k" (or phi-alpha). */
 const double cs_turb_cpale4 = 0.4;
-
-/*! Specific constant of v2f "BL-v2k" (or phi-alpha). */
-const double cs_turb_cpalse = 1.5;
 
 /*! Specific constant of v2f "BL-v2k" (or phi-alpha). */
 const double cs_turb_cpalmu = 0.22;
@@ -1031,8 +1015,7 @@ cs_f_turb_reference_values(double  **almax,
                            double  **xlomlg);
 
 void
-cs_f_turb_model_constants_get_pointers(double  **sigmae,
-                                       double  **cmu,
+cs_f_turb_model_constants_get_pointers(double  **cmu,
                                        double  **cmu025,
                                        double **crij1,
                                        double **crij2);
@@ -1173,19 +1156,18 @@ cs_f_turb_reference_values(double  **almax,
  * enables mapping to Fortran global pointers.
  *
  * parameters:
- *   sigmae --> pointer to cs_turb_sigmae
  *   cmu    --> pointer to cs_turb_cmu
  *   cmu025 --> pointer to cs_turb_cmu025
+ *   crij1  --> pointer to cs_turb_crij1
+ *   crij2  --> pointer to cs_turb_crij2
  *----------------------------------------------------------------------------*/
 
 void
-cs_f_turb_model_constants_get_pointers(double  **sigmae,
-                                       double  **cmu,
+cs_f_turb_model_constants_get_pointers(double  **cmu,
                                        double  **cmu025,
                                        double **crij1,
                                        double **crij2)
 {
-  *sigmae = &cs_turb_sigmae;
   *cmu    = &cs_turb_cmu;
   *cmu025 = &cs_turb_cmu025;
   *crij1 = &cs_turb_crij1;
@@ -1440,13 +1422,21 @@ cs_turb_compute_constants(void)
   cs_turb_cstlog_alpha = exp(-cs_turb_xkappa
                              * (cs_turb_cstlog_rough - cs_turb_cstlog));
 
+  if (CS_F_(k) != NULL)
+    cs_field_set_key_double(CS_F_(k), cs_field_key_id("turbulent_schmidt"), 1.);
+
+  if (CS_F_(phi) != NULL)
+    cs_field_set_key_double(CS_F_(phi), cs_field_key_id("turbulent_schmidt"), 1.);
+
   if (   cs_glob_turb_model->iturb == CS_TURB_RIJ_EPSILON_LRR
       || cs_glob_turb_model->iturb == CS_TURB_RIJ_EPSILON_SSG)
-    cs_turb_sigmae = 1.22;
+    cs_field_set_key_double(CS_F_(eps), cs_field_key_id("turbulent_schmidt"), 1.22);
   else if (cs_glob_turb_model->iturb == CS_TURB_RIJ_EPSILON_EBRSM)
-    cs_turb_sigmae = 1.15;
+    cs_field_set_key_double(CS_F_(eps), cs_field_key_id("turbulent_schmidt"), 1.15);
+  else if (cs_glob_turb_model->iturb == CS_TURB_V2F_BL_V2K)
+    cs_field_set_key_double(CS_F_(eps), cs_field_key_id("turbulent_schmidt"), 1.5);
   else
-    cs_turb_sigmae = 1.30;
+    cs_field_set_key_double(CS_F_(eps), cs_field_key_id("turbulent_schmidt"), 1.30);
 
   if (cs_glob_turb_model->iturb == CS_TURB_RIJ_EPSILON_EBRSM)
     cs_turb_csrij = 0.21;
@@ -1849,11 +1839,8 @@ cs_turb_constants_log_setup(void)
       (CS_LOG_SETUP,
        _("    ce1:         %14.5e (Cepsilon 1: production coef.)\n"
          "    ce2:         %14.5e (Cepsilon 2: dissipat.  coef.)\n"
-         "    sigmak:      %14.5e (Prandtl relative to k)\n"
-         "    sigmae:      %14.5e (Prandtl relative to epsilon )\n"
          "    cmu:         %14.5e (Cmu constant)\n"),
-       cs_turb_ce1, cs_turb_ce2, cs_turb_sigmak,
-       cs_turb_sigmae, cs_turb_cmu);
+         cs_turb_ce1, cs_turb_ce2, cs_turb_cmu);
 
   else if (turb_model->iturb == CS_TURB_RIJ_EPSILON_LRR)
     cs_log_printf
@@ -1863,14 +1850,13 @@ cs_turb_constants_log_setup(void)
          "    crij1:       %14.5e (Slow term coefficient)\n"
          "    crij2:       %14.5e (Fast term coefficient)\n"
          "    crij3:       %14.5e (Gravity term coefficient)\n"
-         "    sigmae:      %14.5e (sigma_eps coeff.)\n"
          "    csrij:       %14.5e (Rij diffusion coeff.)\n"
          "    crijp1:      %14.5e (Slow coeff. for wall echo)\n"
          "    crijp2:      %14.5e (Fast coeff. for wall echo)\n"
          "    cmu:         %14.5e (Cmu constant)\n"),
-       cs_turb_ce1, cs_turb_ce2, cs_turb_crij1, cs_turb_crij2,
-       cs_turb_crij3, cs_turb_sigmae, cs_turb_csrij, cs_turb_crijp1,
-       cs_turb_crijp2, cs_turb_cmu);
+         cs_turb_ce1, cs_turb_ce2, cs_turb_crij1, cs_turb_crij2,
+         cs_turb_crij3, cs_turb_csrij, cs_turb_crijp1,
+         cs_turb_crijp2, cs_turb_cmu);
 
   else if (turb_model->iturb == CS_TURB_RIJ_EPSILON_SSG)
     cs_log_printf
@@ -1886,13 +1872,12 @@ cs_turb_constants_log_setup(void)
          "    crij3:       %14.5e (Gravity term coeff.)\n"
          "    ce1:         %14.5e (Ceps1 coeff.)\n"
          "    cssge2:      %14.5e (Ceps2 coeff.)\n"
-         "    sigmae:      %14.5e (sigma_eps coeff.)\n"
          "    cmu:         %14.5e (Cmu constant)\n"),
-       cs_turb_cssgs1, cs_turb_cssgs2, cs_turb_cssgr1,
-       cs_turb_cssgr2, cs_turb_cssgr3, cs_turb_cssgr4,
-       cs_turb_cssgr5, cs_turb_csrij, cs_turb_crij3,
-       cs_turb_ce1, cs_turb_cssge2, cs_turb_sigmae,
-       cs_turb_cmu);
+         cs_turb_cssgs1, cs_turb_cssgs2, cs_turb_cssgr1,
+         cs_turb_cssgr2, cs_turb_cssgr3, cs_turb_cssgr4,
+         cs_turb_cssgr5, cs_turb_csrij, cs_turb_crij3,
+         cs_turb_ce1, cs_turb_cssge2,
+         cs_turb_cmu);
 
   else if (turb_model->iturb == CS_TURB_RIJ_EPSILON_EBRSM) {
     cs_turb_crij3 = 0.6;
@@ -1908,16 +1893,15 @@ cs_turb_constants_log_setup(void)
          "    crij3:       %14.5e (Gravity term coeff.)\n"
          "    cebme2:      %14.5e (Coef Ceps2)\n"
          "    ce1:         %14.5e (Coef Ceps1)\n"
-         "    sigmae:      %14.5e (Coef sigma_eps)\n"
          "    xa1:         %14.5e (Coef A1)\n"
-         "    sigmak:      %14.5e (Coef sigma_k)\n"
          "    xceta:       %14.5e (Coef Ceta)\n"
          "    xct:         %14.5e (Coef CT)\n"),
-       cs_turb_cebms1, cs_turb_cebmr1, cs_turb_cebmr2,
-       cs_turb_cebmr3, cs_turb_cebmr4, cs_turb_cebmr5,
-       cs_turb_csrij, cs_turb_crij3,  cs_turb_cebme2,
-       cs_turb_ce1, cs_turb_sigmae, cs_turb_xa1,
-       cs_turb_sigmak, cs_turb_xceta, cs_turb_xct);
+         cs_turb_cebms1, cs_turb_cebmr1, cs_turb_cebmr2,
+         cs_turb_cebmr3, cs_turb_cebmr4, cs_turb_cebmr5,
+         cs_turb_csrij, cs_turb_crij3,  cs_turb_cebme2,
+         cs_turb_ce1, cs_turb_xa1,
+         cs_turb_xceta, cs_turb_xct);
+
   }
 
   else if (turb_model->iturb == CS_TURB_V2F_PHI)
@@ -1925,18 +1909,16 @@ cs_turb_constants_log_setup(void)
       (CS_LOG_SETUP,
        _("    cv2fa1:      %14.5e (a1 to calculate Cepsilon1)\n"
          "    cv2fe2:      %14.5e (Cepsilon 2: dissip. coeff.)\n"
-         "    sigmak:      %14.5e (Prandtl relative to k)\n"
-         "    sigmae:      %14.5e (Prandtl relative to epsilon)\n"
          "    cv2fmu:      %14.5e (Cmu constant)\n"
          "    cv2fct:      %14.5e (CT constant)\n"
          "    cv2fcl:      %14.5e (CL constant)\n"
          "    cv2fet:      %14.5e (C_eta constant)\n"
          "    cv2fc1:      %14.5e (C1 constant)\n"
          "    cv2fc2:      %14.5e (C2 constant)\n"),
-       cs_turb_cv2fa1, cs_turb_cv2fe2, cs_turb_sigmak,
-       cs_turb_sigmae, cs_turb_cv2fmu, cs_turb_cv2fct,
-       cs_turb_cv2fcl, cs_turb_cv2fet, cs_turb_cv2fc1,
-       cs_turb_cv2fc2);
+         cs_turb_cv2fa1, cs_turb_cv2fe2,
+         cs_turb_cv2fmu, cs_turb_cv2fct,
+         cs_turb_cv2fcl, cs_turb_cv2fet, cs_turb_cv2fc1,
+         cs_turb_cv2fc2);
 
   else if (turb_model->iturb == CS_TURB_V2F_BL_V2K)
     cs_log_printf
@@ -1945,18 +1927,16 @@ cs_turb_constants_log_setup(void)
          "    cpale2:      %14.5e (Cepsilon 2 : Diss. coeff.)\n"
          "    cpale3:      %14.5e (Cepsilon 3 : E term coeff.)\n"
          "    cpale4:      %14.5e (Cepsilon 4 : Mod Diss. coef.)\n"
-         "    sigmak:      %14.5e (Prandtl relative to k)\n"
-         "    cpalse:      %14.5e (Prandtl relative to epsilon)\n"
          "    cpalmu:      %14.5e (Cmu constant)\n"
          "    cpalct:      %14.5e (CT constant)\n"
          "    cpalcl:      %14.5e (CL constant)\n"
          "    cpalet:      %14.5e (C_eta constant)\n"
          "    cpalc1:      %14.5e (C1 constant)\n"
          "    cpalc2:      %14.5e (C2 constant)\n"),
-       cs_turb_cpale1, cs_turb_cpale2, cs_turb_cpale3,
-       cs_turb_cpale4, cs_turb_sigmak, cs_turb_cpalse,
-       cs_turb_cpalmu, cs_turb_cpalct, cs_turb_cpalcl,
-       cs_turb_cpalet, cs_turb_cpalc1, cs_turb_cpalc2);
+         cs_turb_cpale1, cs_turb_cpale2, cs_turb_cpale3,
+         cs_turb_cpale4,
+         cs_turb_cpalmu, cs_turb_cpalct, cs_turb_cpalcl,
+         cs_turb_cpalet, cs_turb_cpalc1, cs_turb_cpalc2);
 
   else if (turb_model->iturb == CS_TURB_K_OMEGA)
     cs_log_printf

src/base/addfld.f90
@@ -95,6 +95,8 @@ integer          nfld
 integer          n_prev
 integer          t_ext
 integer          kclipp
+integer          key_turb_schmidt, kscavr
+integer          var_f_id
 
 character(len=80) :: name, f_name, f_label, s_label, s_name
 type(var_cal_opt) :: vcopt_dfm, vcopt_alpha, vcopt
@@ -134,6 +136,7 @@ call field_get_key_id('turbulent_flux_model', kturt)
 call field_get_key_id('turbulent_flux_id', kfturt)
 call field_get_key_id('alpha_turbulent_flux_id', kfturt_alpha)
 call field_get_key_id('coupled', keycpl)
+call field_get_key_id("first_moment_id", kscavr)
 
 ! Key id for drift scalar
 call field_get_key_id("drift_scalar_model", keydri)
@@ -235,7 +238,8 @@ enddo
 do ii = 1, nscal
   f_id = ivarfl(isca(ii))
   call field_get_key_int(f_id, kivisl, ifcvsl)
-  if (ifcvsl.eq.0 .and. iscavr(ii).le.0) then
+  call field_get_key_int(f_id, kscavr, var_f_id)
+  if (ifcvsl.eq.0 .and. var_f_id.lt.0) then
     ! Build name and label, using a general rule, with a
     ! fixed name for temperature or enthalpy
     call field_get_name(f_id, s_name)
@@ -300,7 +304,8 @@ enddo
 do ii = 1, nscal
   f_id = ivarfl(isca(ii))
   call field_get_key_int(f_id, kromsl, ifcvsl)
-  if (ifcvsl.eq.0 .and. iscavr(ii).le.0) then
+  call field_get_key_int(f_id, kscavr, var_f_id)
+  if (ifcvsl.eq.0 .and.var_f_id.lt.0) then
     ! Build name and label, using a general rule, with a
     ! fixed name for temperature or enthalpy
     call field_get_name(f_id, s_name)
@@ -314,7 +319,7 @@ do ii = 1, nscal
   endif
 enddo
 
-! For variances, the diffusivity is that of the associated scalar,
+! For variances, the density is that of the associated scalar,
 ! and must not be initialized first.
 
 do ii = 1, nscal
@@ -326,6 +331,42 @@ do ii = 1, nscal
       write(nfecra,7040) f_id, ivarfl(isca(iscavr(ii))), ifcvsl
     else
       call field_set_key_int(f_id, kromsl, ifcvsl)
+    endif
+  endif
+enddo
+
+! Add a scalar turbulent Schmidt field
+call field_get_key_id("turbulent_schmidt_id", key_turb_schmidt)
+
+do ii = 1, nvar
+  f_id = ivarfl(ii)
+  call field_get_key_int(f_id, key_turb_schmidt, ifcvsl)
+  call field_get_key_int(f_id, kscavr, var_f_id)
+  if (ifcvsl.ge.0 .and. var_f_id.lt.0) then
+    ! Build name and label, using a general rule, with a
+    ! fixed name for temperature or enthalpy
+    call field_get_name(f_id, s_name)
+    call field_get_label(f_id, s_label)
+    f_name  = trim(s_name) // '_turb_schmidt'
+    f_label = trim(s_label) // ' ScT'
+
+    ! Now create matching property
+    call add_property_field(f_name, f_label, 1, .false., ifcvsl)
+    call field_set_key_int(ivarfl(ii), key_turb_schmidt, ifcvsl)
+  endif
+enddo
+
+! For variances, the Schmidt is that of the associated scalar,
+! and must not be initialized first.
+do ii = 1, nscal
+  if (iscavr(ii).gt.0) then
+    f_id = ivarfl(isca(ii))
+    call field_get_key_int(ivarfl(isca(iscavr(ii))), key_turb_schmidt, ifcvsl)
+    call field_is_key_set(f_id, key_turb_schmidt, is_set)
+    if (is_set.eqv..true.) then
+      write(nfecra,7040) f_id, ivarfl(isca(iscavr(ii))), ifcvsl
+    else
+      call field_set_key_int(f_id, key_turb_schmidt, ifcvsl)
     endif
   endif
 enddo

src/base/clptrg.f90
@@ -189,6 +189,7 @@ double precision fcofbf(6), fcofad(6), fcofbd(6)
 double precision rxx, rxy, rxz, ryy, ryz, rzz, rnnb
 double precision rttb, alpha_rnn, liqwt, totwt
 double precision cpp
+double precision sigmak, sigmae
 
 double precision, dimension(:), pointer :: crom
 double precision, dimension(:), pointer :: viscl, visct, cpro_cp, yplbr, ustar
@@ -339,6 +340,7 @@ if (ik.gt.0) then
   call field_get_coefb_s(ivarfl(ik), coefb_k)
   call field_get_coefaf_s(ivarfl(ik), coefaf_k)
   call field_get_coefbf_s(ivarfl(ik), coefbf_k)
+  call field_get_key_double(ivarfl(ik), ksigmas, sigmak)
 
   ! Diffuion limiter
   call field_get_key_int(ivarfl(ik), kdflim, f_id)
@@ -359,6 +361,7 @@ if (iep.gt.0) then
   call field_get_coefb_s(ivarfl(iep), coefb_ep)
   call field_get_coefaf_s(ivarfl(iep), coefaf_ep)
   call field_get_coefbf_s(ivarfl(iep), coefbf_ep)
+  call field_get_key_double(ivarfl(iep), ksigmas, sigmae)
   ! Diffuion limiter
   call field_get_key_int(ivarfl(iep), kdflim, f_id)
   if (f_id.ge.0) then

src/base/clptur.f90
@@ -185,6 +185,7 @@ double precision fcoefa(6), fcoefb(6), fcofaf(6), fcofbf(6), fcofad(6), fcofbd(6
 double precision rxx, rxy, rxz, ryy, ryz, rzz, rnnb
 double precision rttb, alpha_rnn
 double precision roughness
+double precision sigmak, sigmae
 
 double precision, dimension(:), pointer :: crom
 double precision, dimension(:), pointer :: viscl, visct, cpro_cp, yplbr, ustar
@@ -319,6 +320,7 @@ if (ik.gt.0) then
   call field_get_coefb_s(ivarfl(ik), coefb_k)
   call field_get_coefaf_s(ivarfl(ik), coefaf_k)
   call field_get_coefbf_s(ivarfl(ik), coefbf_k)
+  call field_get_key_double(ivarfl(ik), ksigmas, sigmak)
 else
   coefa_k => null()
   coefb_k => null()
@@ -331,6 +333,7 @@ if (iep.gt.0) then
   call field_get_coefb_s(ivarfl(iep), coefb_ep)
   call field_get_coefaf_s(ivarfl(iep), coefaf_ep)
   call field_get_coefbf_s(ivarfl(iep), coefbf_ep)
+  call field_get_key_double(ivarfl(iep), ksigmas, sigmae)
 else
   coefa_ep => null()
   coefb_ep => null()

src/base/condli.f90
@@ -228,6 +228,7 @@ double precision temp, exchange_coef
 double precision turb_schmidt
 double precision, allocatable, dimension(:) :: pimpts, hextts, qimpts, cflts
 double precision, allocatable, dimension(:) :: tb_save
+double precision sigmae
 
 character(len=80) :: fname
 
@@ -1615,13 +1616,13 @@ if (itytur.eq.2.or.iturb.eq.60) then
     !     nul)
     if (ii.eq.1.and.itytur.eq.2) then
       ivar   = ik
-      sigma  = sigmak
+      call field_get_key_double(ivarfl(ik), ksigmas, sigma)
     elseif (ii.eq.1.and.iturb.eq.60) then
       ivar   = ik
       sigma  = ckwsk2 !fixme it is not consistent with the model
     elseif (itytur.eq.2) then
       ivar   = iep
-      sigma  = sigmae
+      call field_get_key_double(ivarfl(iep), ksigmas, sigma)
     else
       ivar   = iomg
       sigma  = ckwsw2 !fixme it is not consistent with the model
@@ -2034,6 +2035,7 @@ elseif (itytur.eq.3) then
   call field_get_coefb_s(ivarfl(ivar), coefbp)
   call field_get_coefaf_s(ivarfl(ivar), cofafp)
   call field_get_coefbf_s(ivarfl(ivar), cofbfp)
+  call field_get_key_double(ivarfl(iep), ksigmas, sigmae)
 
   call field_get_key_struct_var_cal_opt(ivarfl(ivar), vcopt)
 
@@ -2247,13 +2249,13 @@ elseif (itytur.eq.5) then
 
     if (ii.eq.1) then
       ivar   = ik
-      sigma  = sigmak
+      call field_get_key_double(ivarfl(ik), ksigmas, sigma)
     elseif (ii.eq.2) then
       ivar   = iep
-      sigma  = sigmae
+      call field_get_key_double(ivarfl(iep), ksigmas, sigma)
     else
       ivar   = iphi
-      sigma  = sigmak
+      call field_get_key_double(ivarfl(iphi), ksigmas, sigma)
     endif
 
     call field_get_coefa_s(ivarfl(ivar), coefap)

src/base/covofi.f90
@@ -163,6 +163,8 @@ integer          icrom_scal
 integer          key_buoyant_id, is_buoyant_fld
 integer          key_t_ext_id
 integer          iviext
+integer          key_turb_schmidt
+integer          t_scd_id
 
 integer          ivoid(1)
 
@@ -204,6 +206,7 @@ double precision, dimension(:,:), pointer :: cvara_rij
 double precision, dimension(:), pointer :: cvar_al
 double precision, dimension(:), pointer :: visct, viscl, cpro_cp, cproa_scal_st
 double precision, dimension(:), pointer :: cpro_scal_st
+double precision, dimension(:), pointer :: cpro_turb_schmidt
 double precision, dimension(:), pointer :: cpro_viscls, cpro_visct
 double precision, dimension(:), pointer :: cpro_tsscal
 double precision, dimension(:), pointer :: cpro_x2icla
@@ -399,6 +402,12 @@ endif
 
 ! Retrieve turbulent Schmidt value for current scalar
 call field_get_key_double(ivarfl(isca(iscal)), ksigmas, turb_schmidt)
+! If turbulent Schmidt is variable, id of the corresponding field
+call field_get_key_id("turbulent_schmidt_id", key_turb_schmidt)
+call field_get_key_int(ivarfl(isca(iscal)), key_turb_schmidt, t_scd_id)
+if (t_scd_id.ge.0) then
+  call field_get_val_s(t_scd_id, cpro_turb_schmidt)
+endif
 
 !===============================================================================
 ! 2. Source terms
@@ -823,12 +832,22 @@ if (itspdv.eq.1) then
         enddo
       ! SGDH model
       else
-        do iel = 1, ncel
-          cproa_scal_st(iel) = cproa_scal_st(iel)                             &
-               + 2.d0*xcpp(iel)*max(cpro_visct(iel),zero)                     &
-               *cell_f_vol(iel)/turb_schmidt                                 &
-               *(grad(1,iel)**2 + grad(2,iel)**2 + grad(3,iel)**2)
-        enddo
+        ! Variable Schmidt number
+        if (t_scd_id.ge.0) then
+          do iel = 1, ncel
+            cproa_scal_st(iel) = cproa_scal_st(iel)                             &
+                 + 2.d0*xcpp(iel)*max(cpro_visct(iel),zero)                     &
+                 *cell_f_vol(iel)/cpro_turb_schmidt(iel)                        &
+                 *(grad(1,iel)**2 + grad(2,iel)**2 + grad(3,iel)**2)
+          enddo
+        else
+          do iel = 1, ncel
+            cproa_scal_st(iel) = cproa_scal_st(iel)                             &
+                 + 2.d0*xcpp(iel)*max(cpro_visct(iel),zero)                     &
+                 *cell_f_vol(iel)/turb_schmidt                                 &
+                 *(grad(1,iel)**2 + grad(2,iel)**2 + grad(3,iel)**2)
+          enddo
+        endif
       endif
 
     ! If not time extrapolation...
@@ -869,22 +888,42 @@ if (itspdv.eq.1) then
 
       ! SGDH model
       else
-        do iel = 1, ncel
-          smbrs(iel) = smbrs(iel)                                            &
-                     + 2.d0*xcpp(iel)*max(cpro_visct(iel),zero)           &
-                     * cell_f_vol(iel)/turb_schmidt                       &
-                     * (grad(1,iel)**2 + grad(2,iel)**2 + grad(3,iel)**2)
-        enddo
+        ! Variable Schmidt number
+        if (t_scd_id.ge.0) then
+          do iel = 1, ncel
+            smbrs(iel) = smbrs(iel)                                         &
+                       + 2.d0*xcpp(iel)*max(cpro_visct(iel),zero)           &
+                       * cell_f_vol(iel)/cpro_turb_schmidt(iel)             &
+                       * (grad(1,iel)**2 + grad(2,iel)**2 + grad(3,iel)**2)
+          enddo
+        else
+          do iel = 1, ncel
+            smbrs(iel) = smbrs(iel)                                         &
+                       + 2.d0*xcpp(iel)*max(cpro_visct(iel),zero)           &
+                       * cell_f_vol(iel)/turb_schmidt                       &
+                       * (grad(1,iel)**2 + grad(2,iel)**2 + grad(3,iel)**2)
+          enddo
+        endif
       endif
 
       ! Production term for a variance  TODO compute ustdy when isso2t >0
       if (idilat.ge.4) then
-        do iel = 1, ncel
-          cpro_tsscal(iel) = cpro_tsscal(iel) +                   &
-               2.d0*xcpp(iel)*max(cpro_visct(iel),zero)        &
-             *cell_f_vol(iel)/turb_schmidt                     &
-             *(grad(1,iel)**2 + grad(2,iel)**2 + grad(3,iel)**2)
-        enddo
+        ! Variable Schmidt number
+        if (t_scd_id.ge.0) then
+          do iel = 1, ncel
+            cpro_tsscal(iel) = cpro_tsscal(iel) +                &
+                 2.d0*xcpp(iel)*max(cpro_visct(iel),zero)        &
+               *cell_f_vol(iel)/cpro_turb_schmidt(iel)           &
+               *(grad(1,iel)**2 + grad(2,iel)**2 + grad(3,iel)**2)
+          enddo
+        else
+          do iel = 1, ncel
+            cpro_tsscal(iel) = cpro_tsscal(iel) +                &
+                 2.d0*xcpp(iel)*max(cpro_visct(iel),zero)        &
+               *cell_f_vol(iel)/turb_schmidt                     &
+               *(grad(1,iel)**2 + grad(2,iel)**2 + grad(3,iel)**2)
+          enddo
+        endif
       endif
     endif
 
@@ -1044,14 +1083,26 @@ if (vcopt%idiff.ge.1) then
 
     if (ifcvsl.lt.0) then
       do iel = 1, ncel
-        w1(iel) = visls0(iscal)                                     &
-           + idifftp*xcpp(iel)*max(visct(iel),zero)/turb_schmidt
+        w1(iel) = visls0(iscal)
       enddo
     else
       do iel = 1, ncel
-        w1(iel) = cpro_viscls(iel)                                &
-           + idifftp*xcpp(iel)*max(visct(iel),zero)/turb_schmidt
+        w1(iel) = cpro_viscls(iel)
       enddo
+    endif
+    if (idifftp.ge.1) then
+      ! Variable Schmidt number
+      if (t_scd_id.ge.0) then
+        do iel = 1, ncel
+          w1(iel) = w1(iel)                                &
+             + idifftp*xcpp(iel)*max(visct(iel),zero)/cpro_turb_schmidt(iel)
+        enddo
+      else
+        do iel = 1, ncel
+          w1(iel) = w1(iel)                                &
+             + idifftp*xcpp(iel)*max(visct(iel),zero)/turb_schmidt
+        enddo
+      endif
     endif
 
     if (vcopt%iwgrec.eq.1) then

src/base/cs_parameters.c
@@ -1029,6 +1029,7 @@ cs_parameters_define_field_keys(void)
 
   cs_field_define_key_double("turbulent_schmidt",
                              1., CS_FIELD_VARIABLE); /* sigmas(iscal) */
+  cs_field_define_key_int("turbulent_schmidt_id", -1, CS_FIELD_VARIABLE);
 
   cs_field_define_key_int("gradient_weighting_id", -1, CS_FIELD_VARIABLE);
 

src/base/cstphy.f90
@@ -371,21 +371,10 @@ module cstphy
   !> in \f$k-\varepsilon\f$ and \f$R_{ij}-\varepsilon\f$ with two-way coupling.
   double precision, save :: ce4
 
-  !> Prandtl number for \f$k\f$ with \f$k-\varepsilon\f$ and v2f models.
-  !> Useful if and only if \ref iturb=20, 21 or 50
-  !> (\f$k-\varepsilon\f$ or v2f)
-  double precision, save :: sigmak
-
-  !> Prandtl number for \f$\varepsilon\f$.
-  !> Useful if and only if \ref iturb= 20, 21, 30, 31 or 50
-  !> (\f$k-\varepsilon\f$, \f$R_{ij}-\varepsilon\f$ or v2f)
-  real(c_double), pointer, save :: sigmae
-
   !> constant \f$C_1\f$ for the \f$R_{ij}-\varepsilon\f$ LRR model.
   !> Useful if and only if \ref iturb=30
   !> (\f$R_{ij}-\varepsilon\f$ LRR)
   real(c_double), pointer, save :: crij1
-
 
   !> constant \f$C_2\f$ for the \f$R_{ij}-\varepsilon\f$ LRR model.
   !> Useful if and only if \ref iturb=30
@@ -489,9 +478,6 @@ module cstphy
 
   !> specific constant of v2f "BL-v2k" (or phi-alpha)
   double precision, save :: cpale4
-
-  !> specific constant of v2f "BL-v2k" (or phi-alpha)
-  double precision, save :: cpalse
 
   !> specific constant of v2f "BL-v2k" (or phi-alpha)
   double precision, save :: cpalmu
@@ -882,11 +868,11 @@ module cstphy
     ! Interface to C function retrieving pointers to constants of the
     ! turbulence model
 
-    subroutine cs_f_turb_model_constants_get_pointers(sigmae, cmu, cmu025, crij1, crij2) &
+    subroutine cs_f_turb_model_constants_get_pointers(cmu, cmu025, crij1, crij2) &
       bind(C, name='cs_f_turb_model_constants_get_pointers')
       use, intrinsic :: iso_c_binding
       implicit none
-      type(c_ptr), intent(out) :: sigmae , cmu  , cmu025 , crij1 , crij2
+      type(c_ptr), intent(out) :: cmu  , cmu025 , crij1 , crij2
     end subroutine cs_f_turb_model_constants_get_pointers
 
     !---------------------------------------------------------------------------
@@ -1013,11 +999,10 @@ contains
 
     ! Local variables
 
-    type(c_ptr) :: c_sigmae, c_cmu, c_cmu025, c_crij1, c_crij2
+    type(c_ptr) :: c_cmu, c_cmu025, c_crij1, c_crij2
 
-    call cs_f_turb_model_constants_get_pointers(c_sigmae, c_cmu, c_cmu025, c_crij1, c_crij2)
+    call cs_f_turb_model_constants_get_pointers(c_cmu, c_cmu025, c_crij1, c_crij2)
 
-    call c_f_pointer(c_sigmae , sigmae)
     call c_f_pointer(c_cmu    , cmu   )
     call c_f_pointer(c_cmu025 , cmu025)
     call c_f_pointer(c_crij1 , crij1)

commit ca58d1e89260ff49341a77c4c45ac4eb9b707aa2

Fix crash in many CDO test cases, broken by commit 6272af55 (Aug 11 2020).
Adding accesses to CS_F_(field) broke cases where no field had been mapped.

src/base/cs_field_pointer.c
@@ -165,6 +165,19 @@ _init_pointers(void)
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief Ensure field pointer array is initialized.
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_field_pointer_ensure_init(void)
+{
+  if (_field_pointer == NULL)
+    _init_pointers();
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief Free all field pointer data.
  */
 /*----------------------------------------------------------------------------*/
@@ -291,7 +304,8 @@ cs_field_pointer_map_base(void)
   cs_field_pointer_map(CS_ENUMF_(omg), cs_field_by_name_try("omega"));
   cs_field_pointer_map(CS_ENUMF_(nusa), cs_field_by_name_try("nu_tilda"));
 
-  cs_field_pointer_map(CS_ENUMF_(hybrid_blend), cs_field_by_name_try("hybrid_blend"));
+  cs_field_pointer_map(CS_ENUMF_(hybrid_blend),
+                       cs_field_by_name_try("hybrid_blend"));
 
   cs_field_pointer_map(CS_ENUMF_(mesh_u),
                        cs_field_by_name_try("mesh_velocity"));

src/base/cs_field_pointer.h
@@ -278,6 +278,13 @@ extern struct cs_field_pointer_array_t  *cs_glob_field_pointers;
  *============================================================================*/
 
 /*----------------------------------------------------------------------------
+ * Ensure field pointer array is initialized.
+ *----------------------------------------------------------------------------*/
+
+void
+cs_field_pointer_ensure_init(void);
+
+/*----------------------------------------------------------------------------
  * Free all field pointer data.
  *----------------------------------------------------------------------------*/
 

src/turb/cs_turbulence_model.c
@@ -1422,6 +1422,8 @@ cs_turb_compute_constants(void)
   cs_turb_cstlog_alpha = exp(-cs_turb_xkappa
                              * (cs_turb_cstlog_rough - cs_turb_cstlog));
 
+  cs_field_pointer_ensure_init();
+
   if (CS_F_(k) != NULL)
     cs_field_set_key_double(CS_F_(k), cs_field_key_id("turbulent_schmidt"), 1.);
 