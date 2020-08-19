77b7982c1f955f57d89a4a4e8f5d0c6752b043ba
Chaï Koren <chai.koren@noreply.edf.fr>
2020-08-19 11:32:15 +0200

    Fix the '--id-suffix' run option which behaved as '--id-prefix'
      * Bug introduced in commit d1a5e68 (2015-11-23)
      * merged from commit b1c35b9 on master branch

--- bin/cs_case.py
+++ bin/cs_case.py
@@ -2004,7 +2004,7 @@ $appli/salome kill `cat $port_log`
             if run_id_prefix:
                 run_id = run_id_prefix + run_id
             if run_id_suffix:
-                run_id = run_id_suffix + run_id
+                run_id = run_id + run_id_suffix
 
             result_dir = os.path.join(r, run_id)
 
