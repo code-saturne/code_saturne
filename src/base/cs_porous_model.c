code-saturne/code_saturne

commit 818b9651f7c38d57294073054064a4e73cceb503

Fix use of disable flag from Fortran, broken by commit 1facdcfe (Jul 22 2020).

src/base/cs_porous_model.c
@@ -166,6 +166,45 @@ cs_porous_model_set_model(int  porous_model)
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief  Initialize disable_flag
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_porous_model_init_disable_flag(void)
+{
+  cs_mesh_t *m =cs_glob_mesh;
+  cs_mesh_quantities_t *mq =cs_glob_mesh_quantities;
+
+  if (cs_glob_porous_model > 0) {
+    cs_lnum_t n_cells_ext = m->n_cells_with_ghosts;
+    if (mq->c_disable_flag == NULL) {
+      BFT_MALLOC(mq->c_disable_flag, n_cells_ext, int);
+      for (cs_lnum_t cell_id = 0; cell_id < n_cells_ext; cell_id++)
+        mq->c_disable_flag[cell_id] = 0;
+    }
+    else {
+      cs_lnum_t n_cells = m->n_cells;
+      BFT_REALLOC(mq->c_disable_flag, n_cells_ext, int);
+      for (cs_lnum_t cell_id = n_cells; cell_id < n_cells_ext; cell_id++)
+        mq->c_disable_flag[cell_id] = 0;
+    }
+    if (m->halo != NULL)
+      cs_halo_sync_untyped(m->halo, CS_HALO_STANDARD, sizeof(int),
+                           mq->c_disable_flag);
+  }
+  else {
+    if (mq->c_disable_flag == NULL)
+      BFT_MALLOC(mq->c_disable_flag, 1, int);
+    mq->c_disable_flag[0] = 0;
+  }
+
+  /* Update Fortran pointer quantities */
+  cs_preprocess_mesh_update_fortran();
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief  Set (unset) has_disable_flag
  *
  * \param[in]  flag   1: on, 0: off
@@ -178,19 +217,6 @@ cs_porous_model_set_has_disable_flag(int  flag)
   cs_mesh_quantities_t *mq =cs_glob_mesh_quantities;
 
   mq->has_disable_flag = flag;
-
-  if (mq->has_disable_flag == 1) {
-    cs_lnum_t n_cells_ext = cs_glob_mesh->n_cells_with_ghosts;
-    if (mq->c_disable_flag == NULL)
-      BFT_REALLOC(mq->c_disable_flag, n_cells_ext, int);
-    for (cs_lnum_t cell_id = 0; cell_id < n_cells_ext; cell_id++)
-      mq->c_disable_flag[cell_id] = 0;
-  }
-  else {
-    if (mq->c_disable_flag == NULL)
-      BFT_REALLOC(mq->c_disable_flag, 1, int);
-    mq->c_disable_flag[0] = 0;
-  }
 
   /* if off, fluid surfaces point toward cell surfaces */
   /* Porous models */

src/base/cs_c_bindings.f90
@@ -2752,6 +2752,16 @@ module cs_c_bindings
 
     !---------------------------------------------------------------------------
 
+    ! Initialize has_disable_flag
+
+    subroutine cs_porous_model_init_disable_flag()   &
+      bind(C, name='cs_porous_model_init_disable_flag')
+      use, intrinsic :: iso_c_binding
+      implicit none
+    end subroutine cs_porous_model_init_disable_flag
+
+    !---------------------------------------------------------------------------
+
     ! Set has_disable_flag
 
     subroutine cs_porous_model_set_has_disable_flag(flag)   &

src/base/cs_porous_model.h
@@ -71,6 +71,15 @@ cs_porous_model_set_model(int  porous_model);
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief  Initialize disable_flag
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_porous_model_init_disable_flag(void);
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief  Set (unset) has_disable_flag
  *
  * \param[in]  flag   1: on, 0: off

src/base/cs_preprocess.c
@@ -63,6 +63,7 @@
 #include "cs_mesh_warping.h"
 #include "cs_parall.h"
 #include "cs_partition.h"
+#include "cs_porous_model.h"
 #include "cs_post.h"
 #include "cs_prototypes.h"
 #include "cs_preprocessor_data.h"
@@ -412,6 +413,7 @@ cs_preprocess_mesh(cs_halo_type_t   halo_type)
   cs_mesh_quantities_compute(cs_glob_mesh, cs_glob_mesh_quantities);
 
   /* If fluid_solid mode is activate: disable solid cells for the dynamics */
+  cs_porous_model_init_disable_flag();
   if (stokes->fluid_solid)
     cs_internal_coupling_tag_disable_cells(cs_glob_mesh, cs_glob_mesh_quantities);
 

src/base/iniva0.f90
@@ -282,8 +282,6 @@ if (iporos.ge.1) then
   call field_get_val_s(ipori, porosi)
   do iel = 1, ncelet
     porosi(iel) = 1.d0
-    ! No solid cells
-    isolid_0(iel) = 0
   enddo
 
   ! Tensorial porosity
@@ -298,9 +296,6 @@ if (iporos.ge.1) then
       porosf(6, iel) = 0.d0
     enddo
   endif
-
-else
-  isolid_0(1) = 0
 endif
 
 !===============================================================================

src/mesh/cs_mesh_quantities.c
@@ -2569,17 +2569,17 @@ cs_mesh_quantities_compute(const cs_mesh_t       *m,
   mq->cell_f_vol = mq->cell_vol;
 
   /* Porous models */
-  if (mq->has_disable_flag == 1) {
-    cs_lnum_t n_cells_ext = cs_glob_mesh->n_cells_with_ghosts;
-    if (mq->c_disable_flag == NULL)
-      BFT_REALLOC(mq->c_disable_flag, n_cells_ext, int);
-    for (cs_lnum_t cell_id = 0; cell_id < n_cells_ext; cell_id++)
-      mq->c_disable_flag[cell_id] = 0;
-  }
-  else {
-    if (mq->c_disable_flag == NULL)
-      BFT_REALLOC(mq->c_disable_flag, 1, int);
-    mq->c_disable_flag[0] = 0;
+  if (mq->c_disable_flag == NULL) {
+    if (mq->has_disable_flag == 1) {
+      cs_lnum_t n_cells_ext = cs_glob_mesh->n_cells_with_ghosts;
+      BFT_MALLOC(mq->c_disable_flag, n_cells_ext, int);
+      for (cs_lnum_t cell_id = 0; cell_id < n_cells_ext; cell_id++)
+        mq->c_disable_flag[cell_id] = 0;
+    }
+    else {
+      BFT_MALLOC(mq->c_disable_flag, 1, int);
+      mq->c_disable_flag[0] = 0;
+    }
   }
 
   mq->min_f_vol = mq->min_vol;

commit 9a8ddedfd9217e91796f2109af0e60a35657aab9

Fix handling of disable flag, broken by commit 818b9651 (Aug 5 2020).

src/base/cs_porous_model.c
@@ -176,22 +176,28 @@ cs_porous_model_init_disable_flag(void)
   cs_mesh_t *m =cs_glob_mesh;
   cs_mesh_quantities_t *mq =cs_glob_mesh_quantities;
 
+  static cs_lnum_t n_cells_prev = 0;
+
   if (cs_glob_porous_model > 0) {
     cs_lnum_t n_cells_ext = m->n_cells_with_ghosts;
-    if (mq->c_disable_flag == NULL) {
-      BFT_MALLOC(mq->c_disable_flag, n_cells_ext, int);
+    if (mq->c_disable_flag == NULL || m->n_cells != n_cells_prev) {
+      /* Currently should handle turbomachinery (ghost cells only can change),
+         not mesh refinement */
+      assert(m->n_cells == n_cells_prev || n_cells_prev == 0);
+
+      BFT_REALLOC(mq->c_disable_flag, n_cells_ext, int);
       for (cs_lnum_t cell_id = 0; cell_id < n_cells_ext; cell_id++)
         mq->c_disable_flag[cell_id] = 0;
+
+      n_cells_prev = m->n_cells;
     }
     else {
-      cs_lnum_t n_cells = m->n_cells;
-      BFT_REALLOC(mq->c_disable_flag, n_cells_ext, int);
-      for (cs_lnum_t cell_id = n_cells; cell_id < n_cells_ext; cell_id++)
-        mq->c_disable_flag[cell_id] = 0;
+      if (mq->has_disable_flag != 0)
+        BFT_REALLOC(mq->c_disable_flag, n_cells_ext, int);
+      if (m->halo != NULL)
+        cs_halo_sync_untyped(m->halo, CS_HALO_STANDARD, sizeof(int),
+                             mq->c_disable_flag);
     }
-    if (m->halo != NULL)
-      cs_halo_sync_untyped(m->halo, CS_HALO_STANDARD, sizeof(int),
-                           mq->c_disable_flag);
   }
   else {
     if (mq->c_disable_flag == NULL)
@@ -214,7 +220,7 @@ cs_porous_model_init_disable_flag(void)
 void
 cs_porous_model_set_has_disable_flag(int  flag)
 {
-  cs_mesh_quantities_t *mq =cs_glob_mesh_quantities;
+  cs_mesh_quantities_t *mq = cs_glob_mesh_quantities;
 
   mq->has_disable_flag = flag;
 