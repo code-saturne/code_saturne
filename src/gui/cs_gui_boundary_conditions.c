code-saturne/code_saturne

commit 65ae6a7b864d3fd1f2b813d34d7062594d639873

Major commit: merge rough and smooth wall functions.
User setting of roughness is now using the dedicated fields
(boundary_roughness and boundary_thermal_roughness).
Buoyancy modification (Louis) are also available for dry and humid
atmosphere for rough-smooth wall functions.

Add some log info to the local Monin-Obukhov inverse length.

Note: yplus local variable is changed for scalable wall functions:
yplus is now not clipped, the clipped/shifted value is "yplus+dplus".

src/gui/cs_gui_boundary_conditions.c
@@ -1474,7 +1474,7 @@ void CS_PROCF (uiclim, UICLIM)(const int  *idarcy,
                                                  bz->elt_ids);
     else if (cs_gui_strcmp(boundaries->nature[izone], "wall")) {
       if (boundaries->rough[izone] >= 0.0)
-        wall_type = 6;
+        wall_type = 6;//TODO remove and use all roughness wall function
       else
         wall_type = 5;
     }
@@ -2237,20 +2237,19 @@ void CS_PROCF (uiclim, UICLIM)(const int  *idarcy,
         int ivarv = cs_field_get_key_int(fv, var_key_id) -1;
 
         iwall = CS_ROUGHWALL;
+        cs_field_t *f_roughness = cs_field_by_name_try("boundary_roughness");
+        cs_field_t *f_roughness_t = cs_field_by_name_try("boundary_thermal_roughness");
 
-        /* Roughness value is stored in Velocity_U (z0) */
-        /* Remember: rcodcl(elt_id, ivar, 1) -> rcodcl[k][j][i]
-           = rcodcl[ k*dim1*dim2 + j*dim1 + i] */
+        /* Roughness value (z0) */
         for (cs_lnum_t elt_id = 0; elt_id < bz->n_elts; elt_id++) {
           cs_lnum_t face_id = bz->elt_ids[elt_id];
-          cs_lnum_t idx = 2 * n_b_faces * (*nvar) + ivarv * n_b_faces + face_id;
-          rcodcl[idx] = boundaries->rough[izone];
+          assert(f_roughness != NULL);
+          f_roughness->val[face_id] = boundaries->rough[izone];
 
-          /* Roughness value is also stored in Velocity_V for eventual scalar
-           * (even if there is no scalar). In this case rugd = rugt. */
-          cs_lnum_t idx2 = 2 * n_b_faces * (*nvar)
-                         + (ivarv + 1) * n_b_faces + face_id;
-          rcodcl[idx2] = boundaries->rough[izone];
+          /* Thermal Roughness value.
+           * In this case thermal roughness is equal to the roughness. */
+          if (f_roughness_t != NULL)
+            f_roughness_t->val[face_id] = boundaries->rough[izone];
         }
       }
 

src/atmo/atincl.f90
@@ -774,7 +774,7 @@ subroutine mo_phim_s (z,dlmo,coef)
   b=2.5d0
   x=z * dlmo
 
-  coef=1.d0+a*(x+(x**b)*((1.d0+x**b)**((1.d0-b)/b)))/(x+(1.d0+x**b)**(1./b))
+  coef=1.d0+a*(x+(x**b)*((1.d0+x**b)**((1.d0-b)/b)))/(x+(1.d0+x**b)**(1.d0/b))
 
 end subroutine mo_phim_s
 
@@ -1074,6 +1074,7 @@ subroutine mo_compute_from_thermal_flux(z,z0,du,flux,tm,gredu,dlmo,ustar)
   double precision coef_mom
   double precision prec_lmo,prec_ustar,prec_tstar,arr_lmo
   double precision num, denom
+  double precision :: dlmoclip = 0.05d0
   integer icompt
 
   ! Precision initialisation
@@ -1117,9 +1118,9 @@ subroutine mo_compute_from_thermal_flux(z,z0,du,flux,tm,gredu,dlmo,ustar)
   endif
 
   ! Clipping dlmo (|LMO| < 20m  ie 1/|LMO| > 0.05 m^-1)
-  if (abs(dlmo).ge.0.05d0) then
-    if (dlmo.ge.0.d0) dlmo=0.05d0
-    if (dlmo.le.0.d0) dlmo=-0.05d0
+  if (abs(dlmo).ge.dlmoclip) then
+    if (dlmo.ge.0.d0) dlmo =   dlmoclip
+    if (dlmo.le.0.d0) dlmo = - dlmoclip
   endif
 
   ! Evaluate universal functions
@@ -1176,6 +1177,7 @@ subroutine mo_compute_from_thermal_diff(z,z0,du,dt,tm,gredu,dlmo,ustar)
   double precision coef_mom,coef_moh
   double precision prec_lmo,prec_ustar,prec_tstar,arr_lmo
   double precision num, denom
+  double precision :: dlmoclip = 0.05d0
   integer icompt
 
   ! Precision initialisation
@@ -1224,10 +1226,10 @@ subroutine mo_compute_from_thermal_diff(z,z0,du,dt,tm,gredu,dlmo,ustar)
     dlmo = 0.d0 !FIXME
   endif
 
-  ! Clipping LMO
-  if (abs(dlmo).ge.0.05d0) then
-    if (dlmo.ge.0.d0) dlmo= 0.05d0
-    if (dlmo.le.0.d0) dlmo=-0.05d0
+  ! Clipping dlmo (|LMO| < 20m  ie 1/|LMO| > 0.05 m^-1)
+  if (abs(dlmo).ge.dlmoclip) then
+    if (dlmo.ge.0.d0) dlmo =   dlmoclip
+    if (dlmo.le.0.d0) dlmo = - dlmoclip
   endif
 
   ! Evaluate universal functions

src/atmo/atmcls.f90
@@ -29,14 +29,19 @@
 ! mode               name       role                                           !
 !______________________________________________________________________________!
 !> \param[in]     ifac          treated boundary face
-!> \param[in]     utau          tangential mean
-!> \param[in]     yplus         adim distance to he boundary faces
+!> \param[in]     utau          tangential mean velocity
+!> \param[in]     rough_d       roughness z0
+!> \param[in]     duplus        1 over dimensionless velocity in neutral
+!>                              conditions
+!> \param[in]     dtplus        1 over dimensionless temperature in neutral
+!>                              conditions
+!> \param[in]     yplus_t       thermal dimensionless wall distance
 !> \param[out]    uet           friction velocity
 !> \param[out]    gredu         reduced gravity for non horizontal wall
-!> \param[out]    cfnnu         non neutral correction coefficients for profiles of wind
 !> \param[out]    cfnns         non neutral correction coefficients for profiles of scalar
 !> \param[out]    cfnnk         non neutral correction coefficients for profiles of k
 !> \param[out]    cfnne         non neutral correction coefficients for profiles of eps
+!> \param[out]    dlmo          inverse Monin Obukhov length (for log only)
 !> \param[in]     temp          potential temperature in boundary cell
 !> \param[in]     totwt         total water content in boundary cell
 !> \param[in]     liqwt         liquid water content in boundary cell
@@ -61,8 +66,7 @@
 !>                               - rcodcl(2) value of the exterior exchange
 !>                                 coefficient (infinite if no exchange)
 !>                               - rcodcl(3) value flux density
-!>                                 (negative if gain) in w/m2 or roughness
-!>                                 in m if icodcl=6
+!>                                 (negative if gain) in w/m2
 !>                                 -# for the velocity \f$ (\mu+\mu_T)
 !>                                    \gradv \vect{u} \cdot \vect{n}  \f$
 !>                                 -# for the pressure \f$ \Delta t
@@ -74,11 +78,13 @@
 
 subroutine atmcls &
  ( ifac   ,                                                       &
-   utau   , yplus  ,                                              &
+   utau   , rough_d , duplus , dtplus ,                           &
+   yplus_t,                                                       &
    uet    ,                                                       &
    gredu  ,                                                       &
-   cfnnu ,  cfnns  , cfnnk  , cfnne  ,                            &
-   temp  ,  totwt  , liqwt  ,                                     &
+   cfnns  , cfnnk   , cfnne  ,                                    &
+   dlmo   ,                                                       &
+   temp   ,  totwt  , liqwt  ,                                    &
    icodcl ,                                                       &
    rcodcl )
 
@@ -113,9 +119,11 @@ integer          ifac
 
 integer          icodcl(nfabor,nvar)
 
-double precision utau, yplus, uet
+double precision utau, rough_d, duplus, dtplus, uet
+double precision yplus_t
 double precision gredu
-double precision cfnnu, cfnns, cfnnk,cfnne
+double precision cfnns, cfnnk,cfnne
+double precision dlmo
 double precision temp, totwt, liqwt
 double precision rcodcl(nfabor,nvar,3)
 
@@ -124,9 +132,9 @@ double precision rcodcl(nfabor,nvar,3)
 double precision rib, q0
 double precision tpot1, tpot2, tpotv1, tpotv2
 double precision rscp1, rscp2
-double precision actu, actt, b, c, d
+double precision b, c, d
 double precision fm, fh, fmden1, fmden2, fhden
-double precision rugd, rugt, distbf
+double precision distbf
 
 !===============================================================================
 
@@ -138,12 +146,7 @@ b = 5.d0
 c = 5.d0
 d = 5.d0
 
-rugd = rcodcl(ifac,iu,3)
-distbf = yplus*rugd
-rugt = rcodcl(ifac,iv,3)
-! 1/U+
-actu = xkappa/log((distbf+rugd)/rugd)
-actt = xkappa/log((distbf+rugt)/rugt)
+distbf = distb(ifac)
 
 ! Take into account humidity in ratio r/cp
 if (ippmod(iatmos).eq.2) then
@@ -176,9 +179,9 @@ if (ntcabs.eq.1) tpotv2 = tpotv1
 
 ! NB: rib = 0 if thermal flux conditions are imposed and tpot1 not defined
 if (abs(utau).le.epzero.or.icodcl(ifac,isca(iscalt)).eq.3) then
- rib = 0.d0
+  rib = 0.d0
 else
- rib = 2.d0*gredu*distbf*(tpotv2 - tpotv1)/(tpotv1 + tpotv2)/utau/utau
+  rib = 2.d0*gredu*distbf*(tpotv2 - tpotv1)/(tpotv1 + tpotv2)/utau/utau
 endif
 
 ! Compute correction factors based on ECMWF parametrisation
@@ -190,18 +193,16 @@ if (rib.ge.epzero) then
   fh = 1.d0/(1.d0 + 3.d0*b*rib*sqrt(1.d0 + d*rib))
 else
   ! Unstable case
-  fmden1 = (distbf + rugt)*abs(rib)/rugt
-  fmden2 = 1.d0 + 3.d0*b*c*actu*actt*sqrt(fmden1)
+  fmden1 = (yplus_t + 1.d0)*abs(rib)
+  fmden2 = 1.d0 + 3.d0*b*c*duplus*dtplus*sqrt(fmden1)
   fm = 1.d0 - 2.d0*b*rib/fmden2
-  fhden = 3.d0*b*c*actu*actt*sqrt((distbf + rugt)/rugt)
+  fhden = 3.d0*b*c*duplus*dtplus*sqrt(yplus_t + 1.d0)
   fh = 1.d0 - (3.d0*b*rib)/(1.d0 + fhden*sqrt(abs(rib)))
 endif
 
 if (fm.le.epzero) fm = epzero
 if (abs(fh).le.epzero) fh = epzero
 
-cfnnu = 1.d0/sqrt(fm)
-cfnns = fh/sqrt(fm)
 if ((1.d0-rib).gt.epzero)then
   cfnnk = sqrt(1.d0 - rib)  ! +correction with turbulent Prandtl
   cfnne = (1.d0-rib)/sqrt(fm)
@@ -210,11 +211,20 @@ else
   cfnne = 1.d0
 endif
 
-! Compute friction velocity  uet
-uet = actu * utau * sqrt(fm)
+! Note: non neutral correction coefficients for profiles of wind
+! (Re) compute friction velocity uet (for non neutral)
+! uet = U/U^+ = U / U^{+,n} * sqrt(fm)
+uet = duplus * utau * sqrt(fm)
 
 ! Compute surface sensible heat flux q0
-q0 = (tpot1-tpot2) * uet * actt * fh / sqrt(fm)
+! Note: non-consistent with two velocity scales
+q0 = (tpot1-tpot2) * uet * dtplus * fh / sqrt(fm) !FIXME it is cfnns factor
+cfnns = fh / sqrt(fm)
+!FIXME tet should be output as uet is...
+
+! Compute local Monin-Obukhov inverse length for log
+! 1/L =  Ri / (z Phim)
+dlmo = rib * sqrt(fm) / (distbf + rough_d)
 
 !----
 ! End

src/atmo/atprop.f90
@@ -54,12 +54,20 @@ integer          f_id, idim1, ityloc, itycat
 !===============================================================================
 
 !===============================================================================
-! 1. POSITIONNEMENT DES PROPRIETES
-!    Atmospheric modules:  dry and humid atmosphere
+! 1. Atmospheric modules properties (mainly dry and humid atmosphere)
 !===============================================================================
 
-! Temperature (ippmod(iatmos) = 1 or 2)
+! Roughness length scales
 
+! boundary roughness
+call add_boundary_property_field_owner('boundary_roughness',  &
+                                       'Boundary Roughness', f_id)
+
+! boundary thermal roughness
+call add_boundary_property_field_owner('boundary_thermal_roughness',  &
+                                       'Boundary Thermal Roughness', f_id)
+
+! Temperature (ippmod(iatmos) = 1 or 2)
 if (ippmod(iatmos).ge.1) then
   call add_property_field_1d('real_temperature', 'RealTemp', itempc)
 
@@ -82,13 +90,6 @@ if (ippmod(iatmos).eq.2) then
     ! wall friction velocity if not already created
     call field_find_or_create('ustar', itycat, ityloc, idim1, f_id)
 
-    ! boundary roughness
-    call add_boundary_property_field_owner('boundary_roughness',  &
-                                           'Boundary Roughness', f_id)
-
-    ! boundary thermal roughness
-    call add_boundary_property_field_owner('boundary_thermal_roughness',  &
-                                           'Boundary Thermal Roughness', f_id)
   endif
 endif
 

src/base/addfld.f90
@@ -371,13 +371,14 @@ do ii = 1, nscal
   endif
 enddo
 
-! Boundary roughness
+! Boundary roughness (may be already created by the atmospheric module)
 if (iwallf.eq.5.or.iwallf.eq.6) then
-  call add_boundary_property_field_owner('boundary_roughness', &
-                                         'Boundary Roughness', &
-                                         iflid)
-endif
+  idim1  = 1
+  itycat = FIELD_INTENSIVE + FIELD_PROPERTY
+  ityloc = 3 ! boundary faces
 
+  call field_find_or_create('boundary_roughness', itycat, ityloc, idim1, iflid)
+endif
 
 ! Van Driest damping
 if (idries.eq.-1) then

src/base/cs_wall_functions.c
@@ -217,7 +217,7 @@ void CS_PROCF (wallfunctions, WALLFUNCTIONS)
  const cs_real_t  *const t_visc,
  const cs_real_t  *const vel,
  const cs_real_t  *const y,
- const cs_real_t  *const roughness,
+ const cs_real_t  *const rough_d,
  const cs_real_t  *const rnnb,
  const cs_real_t  *const kinetic_en,
        int              *iuntur,
@@ -239,7 +239,7 @@ void CS_PROCF (wallfunctions, WALLFUNCTIONS)
                              *t_visc,
                              *vel,
                              *y,
-                             *roughness,
+                             *rough_d,
                              *rnnb,
                              *kinetic_en,
                              iuntur,
@@ -260,8 +260,12 @@ void CS_PROCF (wallfunctions, WALLFUNCTIONS)
 void CS_PROCF (hturbp, HTURBP)
 (
  const int        *const iwalfs,
+ const cs_real_t  *const l_visc,
  const cs_real_t  *const prl,
  const cs_real_t  *const prt,
+ const cs_real_t  *const y,
+ const cs_real_t  *const rough_t,
+ const cs_real_t  *const uk,
  const cs_real_t  *const yplus,
  const cs_real_t  *const dplus,
        cs_real_t        *htur,
@@ -269,8 +273,12 @@ void CS_PROCF (hturbp, HTURBP)
 )
 {
   cs_wall_functions_scalar((cs_wall_f_s_type_t)*iwalfs,
+                           *l_visc,
                            *prl,
                            *prt,
+                           *y,
+                           *rough_t,
+                           *uk,
                            *yplus,
                            *dplus,
                            htur,
@@ -303,7 +311,8 @@ cs_get_glob_wall_functions(void)
  * \param[in]     t_visc        turbulent kinematic viscosity
  * \param[in]     vel           wall projected cell center velocity
  * \param[in]     y             wall distance
- * \param[in]     roughness     roughness
+ * \param[in]     rough_d       roughness length scale
+ *                              (not sand grain roughness)
  * \param[in]     rnnb          \f$\vec{n}.(\tens{R}\vec{n})\f$
  * \param[in]     kinetic_en    turbulente kinetic energy
  * \param[in]     iuntur        indicator: 0 in the viscous sublayer
@@ -327,7 +336,7 @@ cs_wall_functions_velocity(cs_wall_f_type_t  iwallf,
                            cs_real_t         t_visc,
                            cs_real_t         vel,
                            cs_real_t         y,
-                           cs_real_t         roughness,
+                           cs_real_t         rough_d,
                            cs_real_t         rnnb,
                            cs_real_t         kinetic_en,
                            int              *iuntur,
@@ -359,6 +368,9 @@ cs_wall_functions_velocity(cs_wall_f_type_t  iwallf,
     if (mq->c_disable_flag[cell_id])
       iwallf = CS_WALL_F_DISABLED;
   }
+
+  /* Sand Grain roughness */
+  cs_real_t sg_rough = rough_d * exp(cs_turb_xkappa*cs_turb_cstlog_rough);
 
   switch (iwallf) {
   case CS_WALL_F_DISABLED:
@@ -435,7 +447,7 @@ cs_wall_functions_velocity(cs_wall_f_type_t  iwallf,
                                        cofimp);
     break;
   case CS_WALL_F_2SCALES_VDRIEST:
-    cs_wall_functions_2scales_vdriest(rnnb,
+   cs_wall_functions_2scales_vdriest(rnnb,
                                       l_visc,
                                       vel,
                                       y,
@@ -449,7 +461,7 @@ cs_wall_functions_velocity(cs_wall_f_type_t  iwallf,
                                       ypup,
                                       cofimp,
                                       &lmk,
-                                      roughness,
+                                      sg_rough,
                                       wf);
     break;
   case CS_WALL_F_2SCALES_SMOOTH_ROUGH:
@@ -457,7 +469,7 @@ cs_wall_functions_velocity(cs_wall_f_type_t  iwallf,
                                            t_visc,
                                            vel,
                                            y,
-                                           roughness,
+                                           rough_d,
                                            kinetic_en,
                                            iuntur,
                                            nsubla,
@@ -504,8 +516,12 @@ cs_wall_functions_velocity(cs_wall_f_type_t  iwallf,
  *  \f]
  *
  * \param[in]     iwalfs        type of wall functions for scalar
+ * \param[in]     l_visc        kinematic viscosity
  * \param[in]     prl           laminar Prandtl number
  * \param[in]     prt           turbulent Prandtl number
+ * \param[in]     y             wall distance
+ * \param[in]     rough_t       scalar roughness lenghth scale
+ * \param[in]     uk            velocity scale based on TKE
  * \param[in]     yplus         dimensionless distance to the wall
  * \param[in]     dplus         dimensionless distance for scalable
  *                              wall functions
@@ -516,8 +532,12 @@ cs_wall_functions_velocity(cs_wall_f_type_t  iwallf,
 
 void
 cs_wall_functions_scalar(cs_wall_f_s_type_t  iwalfs,
+                         cs_real_t           l_visc,
                          cs_real_t           prl,
                          cs_real_t           prt,
+                         cs_real_t           y,
+                         cs_real_t           rough_t,
+                         cs_real_t           uk,
                          cs_real_t           yplus,
                          cs_real_t           dplus,
                          cs_real_t          *htur,
@@ -537,6 +557,17 @@ cs_wall_functions_scalar(cs_wall_f_s_type_t  iwalfs,
                                 prt,
                                 yplus,
                                 htur);
+    break;
+  case CS_WALL_F_S_SMOOTH_ROUGH:
+    cs_wall_functions_s_smooth_rough(l_visc,
+                                     prl,
+                                     prt,
+                                     y,
+                                     rough_t,
+                                     uk,
+                                     yplus,
+                                     dplus,
+                                     htur);
     break;
   default:
     break;

commit bb8e9811d206265915829f9f632312f88a49ef79

Fix bug in rough wall functions when using GUI (bug introduced in commit 65ae6a7b 2020-08-11).

src/gui/cs_gui_boundary_conditions.c
@@ -2787,8 +2787,15 @@ cs_gui_boundary_conditions_define(cs_boundary_t  *bdy)
             && wall_fnt->iwallf != CS_WALL_F_2SCALES_CONTINUOUS) {
           cs_real_t roughness = -1.;
           cs_gui_node_get_child_real(tn_vp, "roughness", &roughness);
-          if (roughness > 0)
+          if (roughness > 0) {
             bc_type |= CS_BOUNDARY_ROUGH_WALL;
+            /* Create roughness field if needed */
+            cs_field_find_or_create("boundary_roughness",
+                                    CS_FIELD_INTENSIVE + CS_FIELD_PROPERTY,
+                                    CS_MESH_LOCATION_BOUNDARY_FACES,
+                                    1, /* dim */
+                                    false); /* has previous */
+          }
         }
 
       }