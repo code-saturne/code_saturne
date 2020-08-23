code-saturne/code_saturne

commit d4b827c037391b4e8ca58e8eb8483e9831ecad9d

Replace iscacp array with field keyword "is_temperature".

src/base/condli.f90
@@ -207,7 +207,7 @@ integer          ihcp  , iscal
 integer          inc   , iprev , iccocg
 integer          isoent, isorti, ncpt,   isocpt(2)
 integer          iclsym, ipatur, ipatrg, isvhbl
-integer          ifcvsl
+integer          iscacp, ifcvsl
 integer          itplus, itstar
 integer          f_id, iut, ivt, iwt, ialt, iflmab
 integer          kbfid, b_f_id
@@ -274,16 +274,6 @@ type(var_cal_opt) :: vcopt
 !===============================================================================
 
 interface
-
-  subroutine b_h_to_t(h_b, t_b)
-
-    use mesh, only: nfabor
-    implicit none
-
-    double precision, dimension(nfabor), intent(in) :: h_b
-    double precision, dimension(nfabor), intent(out), target :: t_b
-
-  end subroutine b_h_to_t
 
   subroutine clptur(nscal, isvhb, icodcl, rcodcl, velipb, rijipb, &
                     visvdr, hbord, theipb)
@@ -2582,6 +2572,8 @@ if (nscal.ge.1) then
       call field_get_val_s(ifcvsl, viscls)
     endif
 
+    call field_get_key_int(f_id, kscacp, iscacp)
+
     ! --- Indicateur de prise en compte de Cp ou non
     !       (selon si le scalaire (scalaire associe pour une fluctuation)
     !        doit etre ou non traite comme une temperature)
@@ -2595,7 +2587,7 @@ if (nscal.ge.1) then
       iscal = iscavr(ii)
     endif
 
-    if (iscacp(iscal).eq.1) then
+    if (iscacp.eq.1) then
       if(icp.ge.0) then
         ihcp = 2
       else
@@ -2832,7 +2824,7 @@ if (nscal.ge.1) then
               endif
 
             ! Temperature
-            elseif (iscacp(ii).eq.1) then
+            elseif (iscacp.eq.1) then
               exchange_coef = hint
             endif
           endif

src/base/addfld.f90
@@ -80,7 +80,7 @@ implicit none
 ! Local variables
 
 integer          ii, iscal
-integer          ifcvsl, kbfid
+integer          iscacp, ifcvsl, kbfid
 integer          iflid, iopchr
 integer          itycat, ityloc, idim1, idim3
 integer          f_id, potr, poti, flag
@@ -244,7 +244,9 @@ do ii = 1, nscal
       s_name = 'thermal'
       s_label = 'Th'
     endif
-    if (iscacp(ii).gt.0) then
+    call field_get_key_int(f_id, kscacp, iscacp)
+    print *, iscacp
+    if (iscacp.gt.0) then
       f_name  = trim(s_name) // '_conductivity'
       f_label = trim(s_label) // ' Cond'
     else

src/base/clptrg.f90
@@ -1795,7 +1795,7 @@ double precision, dimension(:) :: bdlmo_loc
 
 integer          ivar, f_id, b_f_id, isvhbl
 integer          ifac, iel, isou, jsou
-integer          ifcvsl, itplus, itstar
+integer          iscacp, ifcvsl, itplus, itstar
 
 double precision cpp, rkl, prdtl, visclc, romc, tplus, cpscv
 double precision distfi, distbf, fikis, hint, heq, hflui, hext
@@ -1952,8 +1952,11 @@ else
   endif
 endif
 
+! Does the scalar behave as a temperature ?
+call field_get_key_int(f_id, kscacp, iscacp)
+
 ! retrieve turbulent Schmidt value for current scalar
-call field_get_key_double(ivarfl(isca(iscal)), ksigmas, turb_schmidt)
+call field_get_key_double(f_id, ksigmas, turb_schmidt)
 
 ! --- Loop on boundary faces
 do ifac = 1, nfabor
@@ -1977,7 +1980,7 @@ do ifac = 1, nfabor
     distbf = distb(ifac)
 
     cpp = 1.d0
-    if (iscacp(iscal).eq.1) then
+    if (iscacp.eq.1) then
       if (icp.ge.0) then
         cpp = cpro_cp(iel)
       else
@@ -2230,7 +2233,7 @@ do ifac = 1, nfabor
           endif
 
           ! Temperature
-        elseif (iscacp(iscal).eq.1) then
+        elseif (iscacp.eq.1) then
           exchange_coef = hflui
         endif
       endif

src/base/clptur.f90
@@ -2050,7 +2050,7 @@ double precision tetmax, tetmin, tplumx, tplumn
 integer          ivar, f_id, b_f_id, isvhbl
 integer          f_id_ut, f_id_al
 integer          ifac, iel, isou, jsou
-integer          ifcvsl, itplus, itstar
+integer          iscacp, ifcvsl, itplus, itstar
 
 double precision cpp, rkl, prdtl, visclc, romc, tplus, cofimp, cpscv
 double precision distfi, distbf, fikis, hint_al, heq, hflui, hext
@@ -2242,8 +2242,11 @@ else
   endif
 endif
 
+! Does the scalar behave as a temperature ?
+call field_get_key_int(f_id, kscacp, iscacp)
+
 ! retrieve turbulent Schmidt value for current scalar
-call field_get_key_double(ivarfl(isca(iscal)), ksigmas, turb_schmidt)
+call field_get_key_double(f_id, ksigmas, turb_schmidt)
 
 ! --- Loop on boundary faces
 do ifac = 1, nfabor
@@ -2269,7 +2272,7 @@ do ifac = 1, nfabor
     distbf = distb(ifac)
 
     cpp = 1.d0
-    if (iscacp(iscal).eq.1) then
+    if (iscacp.eq.1) then
       if (icp.ge.0) then
         cpp = cpro_cp(iel)
       else
@@ -2440,7 +2443,7 @@ do ifac = 1, nfabor
     distbf = distb(ifac)
 
     cpp = 1.d0
-    if (iscacp(iscal).eq.1) then
+    if (iscacp.eq.1) then
       if (icp.ge.0) then
         cpp = cpro_cp(iel)
       else
@@ -2535,7 +2538,7 @@ do ifac = 1, nfabor
           endif
 
         ! Temperature
-        elseif (iscacp(iscal).eq.1) then
+        elseif (iscacp.eq.1) then
           exchange_coef = hflui
         endif
       endif
@@ -2781,7 +2784,7 @@ double precision, dimension(:) :: byplus, bdplus, buk
 
 integer          ivar, f_id, isvhbl
 integer          ifac, iel
-integer          ifcvsl
+integer          iscacp, ifcvsl
 
 double precision cpp, rkl, prdtl, visclc, romc, cofimp
 double precision distbf, heq, yptp, hflui, hext
@@ -2831,8 +2834,11 @@ if (icp.ge.0) then
   call field_get_val_s(icp, cpro_cp)
 endif
 
+! Does the vector behave as a temperature ?
+call field_get_key_int(f_id, kscacp, iscacp)
+
 ! retrieve turbulent Schmidt value for current vector
-call field_get_key_double(ivarfl(isca(iscal)), ksigmas, turb_schmidt)
+call field_get_key_double(f_id, ksigmas, turb_schmidt)
 
 isvhbl = 0
 if (iscal.eq.isvhb) then
@@ -2871,7 +2877,7 @@ do ifac = 1, nfabor
     xnuii = visclc / romc
 
     cpp = 1.d0
-    if (iscacp(iscal).eq.1) then
+    if (iscacp.eq.1) then
       if (icp.ge.0) then
         cpp = cpro_cp(iel)
       else

src/base/clsyvt.f90
@@ -783,7 +783,7 @@ integer          icodcl(nfabor,nvar)
 
 integer          ivar, f_id
 integer          ifac, iel, isou, jsou
-integer          ifcvsl
+integer          iscacp, ifcvsl
 
 double precision cpp, rkl, visclc
 double precision distbf, srfbnf
@@ -828,6 +828,9 @@ if (ifcvsl .ge. 0) then
   call field_get_val_s(ifcvsl, viscls)
 endif
 
+! Does the scalar behave as a temperature ?
+call field_get_key_int(f_id, kscacp, iscacp)
+
 ! Turbulent diffusive flux of the scalar T
 ! (blending factor so that the component v'T' have only
 !  mu_T/(mu+mu_T)* Phi_T)
@@ -870,7 +873,7 @@ do ifac = 1, nfabor
     ! --- Physical Propreties
     visclc = viscl(iel)
     cpp = 1.d0
-    if (iscacp(iscal).eq.1) then
+    if (iscacp.eq.1) then
       if (icp.ge.0) then
         cpp = cpro_cp(iel)
       else

src/base/covofi.f90
@@ -149,7 +149,7 @@ integer          ivar
 integer          ii, ifac , iel, isou
 integer          iprev , inc   , iccocg, iiun, ibcl
 integer          ivarsc
-integer          iiscav
+integer          iiscav, iscacp
 integer          ifcvsl, iflmas, iflmab, f_oi_id
 integer          nswrgp, imligp, iwarnp
 integer          iconvp, idiffp, ndircp
@@ -346,11 +346,13 @@ endif
 
 imucpp = 0
 if (iscavr(iscal).gt.0) then
-  if (abs(iscacp(iscavr(iscal))).eq.1) then
+  call field_get_key_int(ivarfl(isca(iscavr(iscal))), kscacp, iscacp)
+  if (iscacp.eq.1) then
     imucpp = 1
   endif
 else
-  if (abs(iscacp(iscal)).eq.1) then
+  call field_get_key_int(iflid, kscacp, iscacp)
+  if (iscacp.eq.1) then
     imucpp = 1
   endif
 endif

src/base/cpvosy.f90
@@ -72,7 +72,7 @@ double precision dt(ncelet)
 ! Local variables
 
 integer          nbccou, inbcou, inbcoo, ncecpl, iloc, iel
-integer          mode, isvol
+integer          mode, isvol, iscacp
 
 integer, dimension(:), allocatable :: lcecpl
 double precision, dimension(:), allocatable :: tfluid, hvol
@@ -88,6 +88,9 @@ double precision, dimension(:), pointer :: cvar_scalt
 
 call nbcsyr(nbccou)
 !==========
+
+! Does the scalar behave as a temperature ?
+call field_get_key_int(ivarfl(isca(iscalt)), kscacp, iscacp)
 
 !---> Loop on couplings
 
@@ -112,7 +115,7 @@ do inbcou = 1, nbccou
     endif
 
     if (iscalt.eq.isvtf) then
-      if (iscacp(iscalt).ne.1) then
+      if (iscacp.ne.1) then
         write(nfecra, 1000)
         call csexit(1)
       endif

src/base/cs_parameters.c
@@ -1015,6 +1015,9 @@ cs_parameters_define_field_keys(void)
   /* is the field buoyant? 0 if not, 1 if yes */
   cs_field_define_key_int("is_buoyant", 0, CS_FIELD_VARIABLE);
 
+  /* does the field behave like a temperature ? (iscacp) */
+  cs_field_define_key_int("is_temperature", -1, CS_FIELD_VARIABLE);
+
   cs_field_define_key_int("turbulent_flux_model", 0, CS_FIELD_VARIABLE);
   cs_field_define_key_int("turbulent_flux_id", -1, CS_FIELD_VARIABLE);
   cs_field_define_key_int("alpha_turbulent_flux_id", -1, CS_FIELD_VARIABLE);

commit d4685ce905933e3f9850fc8026b5940c56b395f5

Fix bug in radiative transfer introduced in commit d4b827c (2020-07-20).

src/base/condli.f90
@@ -508,7 +508,6 @@ if (itrfin.eq.1 .and. itrfup.eq.1) then
 
 endif
 
-
 !Radiative transfer: add contribution to energy BCs.
 if (iirayo.gt.0 .and. itrfin.eq.1 .and. itrfup.eq.1) then
   call cs_rad_transfer_bcs(nvar, itypfb, icodcl, dt, rcodcl)
@@ -2608,7 +2607,7 @@ if (nscal.ge.1) then
       call field_get_val_s(ifcvsl, viscls)
     endif
 
-    call field_get_key_int(f_id, kscacp, iscacp)
+    call field_get_key_int(ivarfl(ivar), kscacp, iscacp)
 
     ! --- Indicateur de prise en compte de Cp ou non
     !       (selon si le scalaire (scalaire associe pour une fluctuation)
@@ -2871,6 +2870,7 @@ if (nscal.ge.1) then
           ! ---> Radiative transfer
           if (iirayo.ge.1 .and. ii.eq.iscalt) then
             bhconv(ifac) = exchange_coef
+            write(nfecra,*) "CONDLI, exchange=",ifac, bhconv(ifac)
 
             ! The outgoing flux is stored (Q = h(Ti'-Tp): negative if
             !  gain for the fluid) in W/m2

src/rayt/cs_rad_transfer_wall_flux.c
@@ -55,6 +55,7 @@
 #include "cs_mesh_quantities.h"
 #include "cs_parall.h"
 #include "cs_parameters.h"
+#include "cs_physical_constants.h"
 #include "cs_sles.h"
 #include "cs_sles_it.h"
 #include "cs_timer.h"
@@ -142,8 +143,8 @@ cs_rad_transfer_wall_flux(int         nvar,
   const int     nb2int = 5;
   const int     nbrrdp = 5;
 
-  const cs_real_t stephn = 5.6703e-8;
-  const cs_real_t tkelvi = 273.15e0;
+  const cs_real_t stephn = cs_physical_constants_stephan;
+  const cs_real_t tkelvi = cs_physical_constants_celsius_to_kelvin;
 
   /* local variables */
   cs_gnum_t  inttm1[nb1int], inttm2[nb2int];
@@ -205,6 +206,7 @@ cs_rad_transfer_wall_flux(int         nvar,
 
   for (cs_lnum_t ifac = 0; ifac < n_b_faces; ifac++) {
 
+    bft_printf("hfconp =%f\n", hfconp[ifac]);
     cs_real_t qconv = 0.0;
     cs_real_t qrayt = 0.0;
 