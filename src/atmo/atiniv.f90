code-saturne/code_saturne

commit 69e7849d34a5500de193d5821e8998dc8cbd3848

[GUI] Fix in specific physics for variable initialization.
GUI variable initialization was overwritten by ppiniv.

src/atmo/atiniv.f90
@@ -381,18 +381,9 @@ if (isuite.eq.0) then
 
 endif
 
-!===============================================================================
-! 4. USER  OPTIONS
-!===============================================================================
-
-call cs_user_f_initialization &
-!==========================
-( nvar   , nscal  ,                                            &
-  dt     )
-
-!----
-! FORMATS
-!----
+!--------
+! Formats
+!--------
 
  1000 format(                                                     &
 '@                                                            ',/,&

src/pprt/ppiniv.f90
@@ -20,51 +20,25 @@
 
 !-------------------------------------------------------------------------------
 
-subroutine ppiniv &
-!================
+!-------------------------------------------------------------------------------
+!> \file ppiniv.f90
+!> \brief Initialisation of specific physic variables
+!>
+!>  Physical properties SHOULD not be modified here
+!>  First stage BEFOR user input (GUI and user subroutines).
+!-------------------------------------------------------------------------------
+! Arguments
+!______________________________________________________________________________.
+!  mode           name          role                                           !
+!______________________________________________________________________________!
+!> \param[in]   nvar        total number of variables
+!> \param[in]   nscal       total number of scalars
+!> \param[in]   dt          time step value
+!-------------------------------------------------------------------------------
 
+subroutine ppiniv0 &
  ( nvar   , nscal  ,                                              &
    dt     )
-
-!===============================================================================
-! FONCTION :
-! --------
-
-! INITIALISATION DES VARIABLES DE CALCUL POUR
-!      LA PHYSIQUE PARTICULIERE
-
-! Cette routine est appelee en debut de calcul (suite ou non)
-!     avant le debut de la boucle en temps
-
-! Elle permet d'INITIALISER ou de MODIFIER (pour les calculs suite)
-!     les variables de calcul,
-!     les valeurs du pas de temps
-
-
-! On dispose ici de ROM et VISCL initialises par RO0 et VISCL0
-!     ou relues d'un fichier suite
-! On ne dispose des variables VISCLS, CP (quand elles sont
-!     definies) que si elles ont pu etre relues dans un fichier
-!     suite de calcul
-
-! LA MODIFICATION DES PROPRIETES PHYSIQUES (ROM, VISCL, VISCLS, CP)
-!     SE FERA EN STANDARD DANS LE SOUS PROGRAMME PPPHYV
-!     ET PAS ICI
-
-! Arguments
-!__________________.____._____.________________________________________________.
-! name             !type!mode ! role                                           !
-!__________________!____!_____!________________________________________________!
-! nvar             ! i  ! <-- ! total number of variables                      !
-! nscal            ! i  ! <-- ! total number of scalars                        !
-! dt(ncelet)       ! tr ! <-- ! valeur du pas de temps                         !
-!__________________!____!_____!________________________________________________!
-
-!     TYPE : E (ENTIER), R (REEL), A (ALPHANUMERIQUE), T (TABLEAU)
-!            L (LOGIQUE)   .. ET TYPES COMPOSES (EX : TR TABLEAU REEL)
-!     MODE : <-- donnee, --> resultat, <-> Donnee modifiee
-!            --- tableau de travail
-!===============================================================================
 
 !===============================================================================
 ! Module files
@@ -94,32 +68,20 @@ double precision dt(ncelet)
 
 ! Local variables
 
-
 !===============================================================================
-
-!===============================================================================
-! 1.  INITIALISATION VARIABLES LOCALES
-!===============================================================================
-
-
-!===============================================================================
-! 2. AIGUILLAGE VERS LE MODELE ADEQUAT
-!===============================================================================
-
 
 ! ---> Combustion gaz
 !      Flamme de diffusion : chimie 3 points
-
- if ( ippmod(icod3p).ge.0 ) then
+if ( ippmod(icod3p).ge.0 ) then
   call d3pini                                                     &
- ( nvar   , nscal  ,                                              &
-   dt     )
-  endif
+    ( nvar   , nscal  ,                                              &
+    dt     )
+endif
 
 ! ---> Combustion gaz
 !      Flamme de premelange : modele EBU
 
- if (ippmod(icoebu).ge.0) then
+if (ippmod(icoebu).ge.0) then
   call ebuini(nvar, nscal, dt)
 endif
 
@@ -156,14 +118,9 @@ if ( ippmod(ieljou).ge.1 .or.                                     &
      ippmod(ielarc).ge.1       ) then
 
   call eliniv(isuite)
-
-  ! give back the hand to the user... in fortran.
-  call cs_user_f_initialization(nvar, nscal, dt)
-
 endif
 
 ! ---> Ecoulements atmospheriques
-
 if (ippmod(iatmos).ge.0) then
 
   call atiniv                                                     &
@@ -175,19 +132,85 @@ endif
 ! ---> Cooling towers
 
 if (ippmod(iaeros).ge.0) then
+  call ctiniv0(nvar, nscal, dt)
+endif
 
-  call ctiniv(nvar, nscal, dt)
+!----
+! Formats
+!----
 
+!----
+! End
+!----
+
+return
+end subroutine
+
+!-------------------------------------------------------------------------------
+!> \file ppiniv.f90
+!> \brief Initialisation of specific physic variables
+!>
+!>  Physical properties SHOULD not be modified here
+!>  Second stage after the GUI and user modifications.
+!-------------------------------------------------------------------------------
+! Arguments
+!______________________________________________________________________________.
+!  mode           name          role                                           !
+!______________________________________________________________________________!
+!> \param[in]   nvar        total number of variables
+!> \param[in]   nscal       total number of scalars
+!> \param[in]   dt          time step value
+!-------------------------------------------------------------------------------
+
+
+subroutine ppiniv1 &
+ ( nvar   , nscal  ,                                              &
+   dt     )
+
+!===============================================================================
+
+!===============================================================================
+! Module files
+!===============================================================================
+
+use paramx
+use numvar
+use optcal
+use cstphy
+use cstnum
+use entsor
+use parall
+use ppppar
+use ppthch
+use coincl
+use cpincl
+use ppincl
+use mesh
+
+!===============================================================================
+
+implicit none
+
+integer          nvar   , nscal
+
+double precision dt(ncelet)
+
+! Local variables
+
+!===============================================================================
+
+! ---> Cooling towers
+
+if (ippmod(iaeros).ge.0) then
+  call ctiniv1(nvar, nscal, dt)
 endif
 
 ! Gas mixture modelling in presence of noncondensable gases and
 ! condensable gas as stream.
 if (ippmod(igmix).ge.0) then
-
   call cs_gas_mix_initialization &
   ( nvar   , nscal  ,                                            &
     dt     )
-
 endif
 
 ! Compressible
@@ -195,9 +218,7 @@ endif
 ! mixture composition is taken into account in the thermodynamic
 ! law, if gas mix specific physics is enabled.
 if (ippmod(icompf).ge.0) then
-
   call cfiniv(nvar, nscal, dt)
-
 endif
 
 !----

src/base/cs_gas_mix_initialization.f90
@@ -127,16 +127,9 @@ call field_get_key_struct_gas_mix_species_prop(f_id, s_d)
 call field_get_val_s(igmxml, mix_mol_mas)
 
 !===============================================================================
-! 2. User initialization
-!===============================================================================
-
-call cs_user_f_initialization &
-( nvar   , nscal  ,                                            &
-  dt     )
-
-!===============================================================================
-! 3. Deduce the mass fraction (y_d) from the mass fractions (yk) of
+! 2. Deduce the mass fraction (y_d) from the mass fractions (yk) of
 !    the noncondensable gases transported
+!    Note: mass fraction has been provided by the user in the GUI or routines
 !===============================================================================
 
 if (isuite.eq.0) then
@@ -205,7 +198,7 @@ if (isuite.eq.0) then
   endif
 
   !===============================================================================
-  ! 4. Print to the log to check the variables intialization
+  ! 3. Print to the log to check the variables intialization
   !===============================================================================
 
   write(nfecra, 200)
@@ -214,7 +207,7 @@ if (isuite.eq.0) then
 endif
 
 !===============================================================================
-! 5. Stop if problem
+! 4. Stop if problem
 !===============================================================================
 
 if (iok.gt.0) then

src/base/inivar.f90
@@ -171,25 +171,29 @@ do ivar = 1, nvar
   endif
 enddo
 
+! First pre-initialization for specific physic modules
+! before the GUI
+if (ippmod(iphpar).gt.0) then
+  call ppiniv0(nvar, nscal, dt)
+endif
+
 ! - Interface Code_Saturne
 !   ======================
 
 call uiiniv (isuite, ippmod(idarcy), ithvar)
 
-!   - Sous-programme utilisateur
-!     ==========================
+! User subroutine
+! ===============
 
-if (ippmod(iphpar).eq.0) then
-
-  call cs_user_f_initialization(nvar, nscal, dt)
-
-else
-
-  call ppiniv(nvar, nscal, dt)
-
-endif
+call cs_user_f_initialization(nvar, nscal, dt)
 
 call user_initialization()
+
+! Second stage of initialization for specific physic modules
+! after the user
+if (ippmod(iphpar).gt.0) then
+  call ppiniv1(nvar, nscal, dt)
+endif
 
 ! VoF algorithm
 if (ivofmt.gt.0) then

src/cfbl/cfiniv.f90
@@ -90,15 +90,6 @@ double precision, dimension(:), pointer :: cpro_cp, cpro_cv, mix_mol_mas
 !===============================================================================
 
 !===============================================================================
-! User initialisation of the variables only if this not a resumed computation
-! and if it has not already been done in gas mix initialization
-!===============================================================================
-
-if (isuite.eq.0.and.ippmod(igmix).lt.0) then
-  call cs_user_f_initialization(nvar, nscal, dt)
-endif
-
-!===============================================================================
 ! Computation of variable Cv in order to have a correct initialization
 ! of the total energy (computed in inivar by a call to a thermodynamic
 ! function), now that initial gas mixture composition is known.

src/cogz/d3pini.f90
@@ -60,10 +60,6 @@ subroutine d3pini &
 ! dt(ncelet)       ! tr ! <-- ! valeur du pas de temps                         !
 !__________________!____!_____!________________________________________________!
 
-!     TYPE : E (ENTIER), R (REEL), A (ALPHANUMERIQUE), T (TABLEAU)
-!            L (LOGIQUE)   .. ET TYPES COMPOSES (EX : TR TABLEAU REEL)
-!     MODE : <-- donnee, --> resultat, <-> Donnee modifiee
-!            --- tableau de travail
 !===============================================================================
 
 !===============================================================================
@@ -179,11 +175,6 @@ if ( isuite.eq.0 ) then
   ( mode   , ngazg , ngazgm  , coefg  ,                           &
     npo    , npot   , th     , ehgazg ,                           &
     hinfue , tinfue )
-
-  ! User initilization
-  call cs_user_f_initialization &
-  ( nvar   , nscal  ,                                            &
-    dt     )
 
   ! ---> Parallelism and periodic exchange
   if (irangp.ge.0.or.iperio.eq.1) then

src/cogz/ebuini.f90
@@ -21,8 +21,6 @@
 !-------------------------------------------------------------------------------
 
 subroutine ebuini &
-!================
-
  ( nvar   , nscal  ,                                              &
    dt     )
 
@@ -125,7 +123,6 @@ save             ipass
 !===============================================================================
 
 ipass = ipass + 1
-
 
 do igg = 1, ngazgm
   coefg(igg) = zero
@@ -315,12 +312,6 @@ if ( isuite.eq.0 ) then
 
     enddo
 
-! ----- On donne la main a l'utilisateur
-    call cs_user_f_initialization &
-    !==========================
-  ( nvar   , nscal  ,                                            &
-    dt     )
-
 ! ----- En periodique et en parallele,
 !       il faut echanger ces initialisations
 
@@ -336,7 +327,6 @@ if ( isuite.eq.0 ) then
         !==========
       endif
     endif
-
 
 !      Impressions de controle
 

commit f7df5c36239b514077dbd0d0a83169b0f8e605d1

[Atmo] Fix bug introduced in 69e7849 (2020-08-25).
Atmo initialization of variables should be done in two stages, before
and after the GUI.
The variable "initmeteo" should be ideally set by the GUI, to know if
variables are set from a meteo profile or from the GUI formula.
Here we chose to use the meteo profile if the user specify one, use the
GUI otherwise.

src/atmo/atiniv.f90
@@ -25,6 +25,8 @@
 !>
 !> Initialise for example the meteorological field for each cell of
 !> the domain by interpolation of the data from the meteo file
+!> First stage, before GUI.
+!
 !-------------------------------------------------------------------------------
 ! Arguments
 !______________________________________________________________________________.
@@ -35,7 +37,7 @@
 !> \param[in]   dt          time step value
 !-------------------------------------------------------------------------------
 
-subroutine atiniv  ( nvar, nscal, dt )
+subroutine atiniv0( nvar, nscal, dt )
 
 !===============================================================================
 ! Module files
@@ -346,7 +348,6 @@ if (isuite.eq.0) then
 
         endif
 
-
         if (ippmod(iatmos).eq.1) then
           ! The thermal scalar is potential temperature
             call intprf                                                 &
@@ -470,9 +471,365 @@ endif
 '@                                                            ',/)
 
 !----
-! FIN
+! End
 !----
 
 return
 
-end subroutine atiniv
+end subroutine atiniv0
+
+!-------------------------------------------------------------------------------
+!> \file atiniv.f90
+!> \brief Initialisation of calculation variables for the atmospheric module,
+!> it is the counterpart of usiniv.f90.
+!>
+!> Initialise for example the meteorological field for each cell of
+!> the domain by interpolation of the data from the meteo file
+!> Second stage.
+!
+!-------------------------------------------------------------------------------
+! Arguments
+!______________________________________________________________________________.
+!  mode           name          role                                           !
+!______________________________________________________________________________!
+!> \param[in]   nvar        total number of variables
+!> \param[in]   nscal       total number of scalars
+!> \param[in]   dt          time step value
+!-------------------------------------------------------------------------------
+
+subroutine atiniv1( nvar, nscal, dt )
+
+!===============================================================================
+! Module files
+!===============================================================================
+
+use paramx
+use numvar
+use optcal
+use cstphy
+use cstnum
+use entsor
+use ppppar
+use ppthch
+use ppincl
+use atincl
+use mesh
+use atchem
+use sshaerosol
+use field
+use cs_c_bindings
+
+!===============================================================================
+
+implicit none
+
+integer          nvar   , nscal
+
+double precision dt(ncelet)
+
+! Local variables
+
+integer          imode, iel
+double precision d2s3
+double precision zent,xuent,xvent,xkent,xeent,tpent,qvent,ncent
+
+integer k,ii, isc
+double precision xcent
+
+double precision, dimension(:,:), pointer :: vel
+double precision, dimension(:), pointer :: cvar_k, cvar_ep, cvar_phi
+double precision, dimension(:), pointer :: cvar_fb, cvar_omg, cvar_nusa
+double precision, dimension(:,:), pointer :: cvar_rij
+double precision, dimension(:), pointer :: cvar_r11, cvar_r22, cvar_r33
+double precision, dimension(:), pointer :: cvar_r12, cvar_r13, cvar_r23
+double precision, dimension(:), pointer :: cvar_despgi, cvar_sc
+double precision, dimension(:), pointer :: cvar_scalt, cvar_totwt, cvar_ntdrp
+
+!===============================================================================
+
+! Map field arrays
+call field_get_val_v(ivarfl(iu), vel)
+
+!===============================================================================
+! 1.  INITIALISATION VARIABLES LOCALES
+!===============================================================================
+
+d2s3 = 2.d0/3.d0
+
+if (itytur.eq.2) then
+  call field_get_val_s(ivarfl(ik), cvar_k)
+  call field_get_val_s(ivarfl(iep), cvar_ep)
+elseif (itytur.eq.3) then
+  if (irijco.eq.1) then
+    call field_get_val_v(ivarfl(irij), cvar_rij)
+  else
+    call field_get_val_s(ivarfl(ir11), cvar_r11)
+    call field_get_val_s(ivarfl(ir22), cvar_r22)
+    call field_get_val_s(ivarfl(ir33), cvar_r33)
+    call field_get_val_s(ivarfl(ir12), cvar_r12)
+    call field_get_val_s(ivarfl(ir23), cvar_r23)
+    call field_get_val_s(ivarfl(ir13), cvar_r13)
+  endif
+  call field_get_val_s(ivarfl(iep), cvar_ep)
+elseif (iturb.eq.50) then
+  call field_get_val_s(ivarfl(ik), cvar_k)
+  call field_get_val_s(ivarfl(iep), cvar_ep)
+  call field_get_val_s(ivarfl(iphi), cvar_phi)
+  call field_get_val_s(ivarfl(ifb), cvar_fb)
+elseif (iturb.eq.60) then
+  call field_get_val_s(ivarfl(ik), cvar_k)
+  call field_get_val_s(ivarfl(iomg), cvar_omg)
+elseif (iturb.eq.70) then
+  call field_get_val_s(ivarfl(inusa), cvar_nusa)
+endif
+
+!===============================================================================
+! 2. Checks of the reading the meteo profile file (if imeteo = 1 default option)
+!===============================================================================
+
+! Check simulation times used by atmo
+! radiative transfer or chemistry models
+if (     (iatra1.eq.1.or.ifilechemistry.ge.1)              &
+    .and.(syear.eq.-1.or.squant.eq.-1.or.shour.eq.-1 &
+          .or.smin.eq.-1.or.ssec.le.-1.d0)) then
+  if (iatra1.eq.1) write(nfecra,1000)
+  if (ifilechemistry.ge.1) write(nfecra,1001)
+  call csexit (1)
+endif
+
+! Check radiative module latitude / longitude
+if (iatra1.eq.1 .and. (xlat.ge.rinfin*0.5 .or. xlon.ge.rinfin*0.5)) then
+  write(nfecra,1002)
+  call csexit (1)
+endif
+
+! Check latitude / longitude from meteo file
+if (imeteo.gt.0) then
+  if (maxval(xmet).ge.rinfin*0.5 .or. maxval(ymet).ge.rinfin*0.5) then
+    write(nfecra,1003)
+    call csexit (1)
+  endif
+endif
+
+! Check latitude / longitude from chemistry file
+if (ifilechemistry.ge.1) then
+  if (maxval(xchem).ge.rinfin*0.5 .or. maxval(ychem).ge.rinfin*0.5) then
+    write(nfecra,1004)
+    call csexit (1)
+  endif
+endif
+
+!===============================================================================
+! 3. Dry atmosphere: default initialization of potential temperature
+!===============================================================================
+
+! Only if the simulation is not a restart from another one
+if (isuite.eq.0) then
+
+  !Note: if no meteo profile: done by user or previously
+  if (initmeteo.eq.1.and.imeteo.eq.1) then
+
+    if (ippmod(iatmos).eq.1) then
+      call field_get_val_s(ivarfl(isca(iscalt)), cvar_scalt)
+    else if (ippmod(iatmos).eq.2) then
+      call field_get_val_s(ivarfl(isca(iscalt)), cvar_scalt)
+      call field_get_val_s(ivarfl(isca(iymw)), cvar_totwt)
+      call field_get_val_s(ivarfl(isca(intdrp)), cvar_ntdrp)
+    endif
+
+    do iel = 1, ncel
+
+      zent = xyzcen(3,iel)
+
+      call intprf                                                   &
+        (nbmetd, nbmetm,                                               &
+        zdmet, tmmet, umet , zent  , ttcabs, xuent )
+
+      call intprf                                                   &
+        (nbmetd, nbmetm,                                               &
+        zdmet, tmmet, vmet , zent  , ttcabs, xvent )
+
+      call intprf                                                   &
+        (nbmetd, nbmetm,                                               &
+        zdmet, tmmet, ekmet, zent  , ttcabs, xkent )
+
+      call intprf                                                   &
+        (nbmetd, nbmetm,                                               &
+        zdmet, tmmet, epmet, zent  , ttcabs, xeent )
+
+      vel(1,iel) = xuent
+      vel(2,iel) = xvent
+      vel(3,iel) = 0.d0
+
+      !     ITYTUR est un indicateur qui vaut ITURB/10
+      if    (itytur.eq.2) then
+
+        cvar_k(iel)  = xkent
+        cvar_ep(iel) = xeent
+
+      elseif (itytur.eq.3) then
+
+        if (irijco.eq.1) then
+          cvar_rij(1,iel) = d2s3*xkent
+          cvar_rij(2,iel) = d2s3*xkent
+          cvar_rij(3,iel) = d2s3*xkent
+          cvar_rij(4,iel) = 0.d0
+          cvar_rij(5,iel) = 0.d0
+          cvar_rij(6,iel) = 0.d0
+        else
+          cvar_r11(iel) = d2s3*xkent
+          cvar_r22(iel) = d2s3*xkent
+          cvar_r33(iel) = d2s3*xkent
+          cvar_r12(iel) = 0.d0
+          cvar_r13(iel) = 0.d0
+          cvar_r23(iel) = 0.d0
+        endif
+        cvar_ep(iel)  = xeent
+
+      elseif (iturb.eq.50) then
+
+        cvar_k(iel)   = xkent
+        cvar_ep(iel)  = xeent
+        cvar_phi(iel) = d2s3
+        cvar_fb(iel)  = 0.d0
+
+      elseif (iturb.eq.60) then
+
+        cvar_k(iel)   = xkent
+        cvar_omg(iel) = xeent/cmu/xkent
+
+      elseif (iturb.eq.70) then
+
+        cvar_nusa(iel) = cmu*xkent**2/xeent
+
+      endif
+
+
+      if (ippmod(iatmos).eq.1) then
+        ! The thermal scalar is potential temperature
+        call intprf                                                 &
+          (nbmett, nbmetm,                                               &
+          ztmet, tmmet, tpmet, zent  , ttcabs, tpent )
+
+        cvar_scalt(iel) = tpent
+      endif
+
+      if (ippmod(iatmos).eq.2) then
+        ! The thermal scalar is liquid potential temperature
+        call intprf                                                 &
+          (nbmett, nbmetm,                                               &
+          ztmet, tmmet, tpmet, zent  , ttcabs, tpent )
+        cvar_scalt(iel) = tpent
+
+        call intprf                                                 &
+          (nbmett, nbmetm,                                               &
+          ztmet, tmmet, qvmet, zent  , ttcabs, qvent )
+        cvar_totwt(iel) = qvent
+
+        call intprf                                                 &
+          (nbmett, nbmetm,                                               &
+          ztmet, tmmet, ncmet, zent  , ttcabs, ncent )
+        cvar_ntdrp(iel) = ncent
+      endif
+
+    enddo
+
+  endif
+
+endif
+
+!--------
+! Formats
+!--------
+
+ 1000 format(                                                     &
+'@                                                            ',/,&
+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
+'@                                                            ',/,&
+'@ @@  WARNING:   STOP WHILE READING INPUT DATA               ',/,&
+'@    =========                                               ',/,&
+'@                ATMOSPHERIC  MODULE                         ',/,&
+'@                RADITIVE MODEL (IATRA1)                     ',/,&
+'@                                                            ',/,&
+'@    The simulation time is wrong                            ',/,&
+'@    Check variables syear, squant, shour, smin, ssec        ',/,&
+'@                                                            ',/,&
+'@    By decreasing priority, these variables can be defined  ',/,&
+'@    in cs_user_parameters or the meteo file                 ',/,&
+'@    or the chemistry file                                   ',/,&
+'@                                                            ',/,&
+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
+'@                                                            ',/)
+
+ 1001 format(                                                     &
+'@                                                            ',/,&
+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
+'@                                                            ',/,&
+'@ @@  WARNING:   STOP WHILE READING INPUT DATA               ',/,&
+'@    =========                                               ',/,&
+'@      ATMOSPHERIC CHEMISTRY                                 ',/,&
+'@                                                            ',/,&
+'@    The simulation time is wrong                            ',/,&
+'@    Check variables syear, squant, shour, smin, ssec        ',/,&
+'@                                                            ',/,&
+'@    By decreasing priority, these variables can be defined  ',/,&
+'@    in cs_user_parameters or the meteo file                 ',/,&
+'@    or the chemistry file                                   ',/,&
+'@                                                            ',/,&
+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
+'@                                                            ',/)
+
+ 1002 format(                                                     &
+'@                                                            ',/,&
+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
+'@                                                            ',/,&
+'@ @@  WARNING:   STOP WHILE READING INPUT DATA               ',/,&
+'@    =========                                               ',/,&
+'@                ATMOSPHERIC  MODULE                         ',/,&
+'@                RADITIVE MODEL (IATRA1)                     ',/,&
+'@                                                            ',/,&
+'@    Wrong xlat and xlon coordinates.                        ',/,&
+'@                                                            ',/,&
+'@    See cs_user_parameters.f90                              ',/,&
+'@                                                            ',/,&
+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
+'@                                                            ',/)
+
+ 1003 format(                                                     &
+'@                                                            ',/,&
+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
+'@                                                            ',/,&
+'@ @@  WARNING:   STOP WHILE READING INPUT DATA               ',/,&
+'@    =========                                               ',/,&
+'@      ATMOSPHERIC MODULE                                    ',/,&
+'@                                                            ',/,&
+'@    Wrong coordinates xmet, ymet for the meteo profile.     ',/,&
+'@                                                            ',/,&
+'@    See cs_user_parameters.f90                              ',/,&
+'@                                                            ',/,&
+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
+'@                                                            ',/)
+
+ 1004 format(                                                     &
+'@                                                            ',/,&
+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
+'@                                                            ',/,&
+'@ @@  WARNING:   STOP WHILE READING INPUT DATA               ',/,&
+'@    =========                                               ',/,&
+'@      ATMOSPHERIC CHEMISTRY                                 ',/,&
+'@                                                            ',/,&
+'@    Wrong xchem, ychem coordinates for the concentration    ',/,&
+'@    profiles (chemistry model).                             ',/,&
+'@                                                            ',/,&
+'@    See cs_user_parameters.f90                              ',/,&
+'@                                                            ',/,&
+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
+'@                                                            ',/)
+
+!----
+! End
+!----
+
+return
+
+end subroutine atiniv1

src/pprt/ppiniv.f90
@@ -120,10 +120,10 @@ if ( ippmod(ieljou).ge.1 .or.                                     &
   call eliniv(isuite)
 endif
 
-! ---> Ecoulements atmospheriques
+! Atmospheric flows, first stage
 if (ippmod(iatmos).ge.0) then
 
-  call atiniv                                                     &
+  call atiniv0                                                     &
  ( nvar   , nscal  ,                                              &
    dt     )
 
@@ -199,8 +199,16 @@ double precision dt(ncelet)
 
 !===============================================================================
 
-! ---> Cooling towers
+! Atmospheric flows, second stage
+if (ippmod(iatmos).ge.0) then
 
+  call atiniv1                                                     &
+ ( nvar   , nscal  ,                                              &
+   dt     )
+
+endif
+
+! ---> Cooling towers
 if (ippmod(iaeros).ge.0) then
   call ctiniv1(nvar, nscal, dt)
 endif