code-saturne/code_saturne

commit 4cf78cd147573703461a192f122d46e1253f34cf

Enable to launch a job with srun even if n_procs=1

bin/cs_case.py
@@ -932,7 +932,9 @@ class case:
         mpi_cmd = ''
         mpi_cmd_exe = ''
         mpi_cmd_args = ''
-        if n_procs > 1 and mpi_env.mpiexec != None:
+
+        if (n_procs > 1 and mpi_env.mpiexec != None) or \
+           (os.path.basename(mpi_env.mpiexec)[:4] == 'srun'):
             mpi_cmd = mpi_env.mpiexec
             if mpi_env.mpiexec_opts != None:
                 mpi_cmd += ' ' + mpi_env.mpiexec_opts

commit 0ad937fb5ea0c8a2fa0be20c7651aedd74a50963

Fix run for builds without MPI, broken by commit 4cf78cd1 (Jul 17 2020).

bin/cs_case.py
@@ -947,9 +947,11 @@ class case:
         mpi_cmd_exe = ''
         mpi_cmd_args = ''
 
-        if (n_procs > 1 and mpi_env.mpiexec != None) or \
-           (os.path.basename(mpi_env.mpiexec)[:4] == 'srun'):
-            mpi_cmd = mpi_env.mpiexec
+        if mpi_env.mpiexec != None:
+            if (n_procs > 1 or os.path.basename(mpi_env.mpiexec)[:4] == 'srun'):
+                mpi_cmd = mpi_env.mpiexec
+
+        if mpi_cmd:
             if mpi_env.mpiexec_opts != None:
                 mpi_cmd += ' ' + mpi_env.mpiexec_opts
             if mpiexec_mpmd == False: