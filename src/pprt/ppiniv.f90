code-saturne/code_saturne

commit 69e7849d34a5500de193d5821e8998dc8cbd3848

[GUI] Fix in specific physics for variable initialization.
GUI variable initialization was overwritten by ppiniv.

src/pprt/ppiniv.f90
@@ -20,51 +20,25 @@
 
 !-------------------------------------------------------------------------------
 
-subroutine ppiniv &
-!================
+!-------------------------------------------------------------------------------
+!> \file ppiniv.f90
+!> \brief Initialisation of specific physic variables
+!>
+!>  Physical properties SHOULD not be modified here
+!>  First stage BEFOR user input (GUI and user subroutines).
+!-------------------------------------------------------------------------------
+! Arguments
+!______________________________________________________________________________.
+!  mode           name          role                                           !
+!______________________________________________________________________________!
+!> \param[in]   nvar        total number of variables
+!> \param[in]   nscal       total number of scalars
+!> \param[in]   dt          time step value
+!-------------------------------------------------------------------------------
 
+subroutine ppiniv0 &
  ( nvar   , nscal  ,                                              &
    dt     )
-
-!===============================================================================
-! FONCTION :
-! --------
-
-! INITIALISATION DES VARIABLES DE CALCUL POUR
-!      LA PHYSIQUE PARTICULIERE
-
-! Cette routine est appelee en debut de calcul (suite ou non)
-!     avant le debut de la boucle en temps
-
-! Elle permet d'INITIALISER ou de MODIFIER (pour les calculs suite)
-!     les variables de calcul,
-!     les valeurs du pas de temps
-
-
-! On dispose ici de ROM et VISCL initialises par RO0 et VISCL0
-!     ou relues d'un fichier suite
-! On ne dispose des variables VISCLS, CP (quand elles sont
-!     definies) que si elles ont pu etre relues dans un fichier
-!     suite de calcul
-
-! LA MODIFICATION DES PROPRIETES PHYSIQUES (ROM, VISCL, VISCLS, CP)
-!     SE FERA EN STANDARD DANS LE SOUS PROGRAMME PPPHYV
-!     ET PAS ICI
-
-! Arguments
-!__________________.____._____.________________________________________________.
-! name             !type!mode ! role                                           !
-!__________________!____!_____!________________________________________________!
-! nvar             ! i  ! <-- ! total number of variables                      !
-! nscal            ! i  ! <-- ! total number of scalars                        !
-! dt(ncelet)       ! tr ! <-- ! valeur du pas de temps                         !
-!__________________!____!_____!________________________________________________!
-
-!     TYPE : E (ENTIER), R (REEL), A (ALPHANUMERIQUE), T (TABLEAU)
-!            L (LOGIQUE)   .. ET TYPES COMPOSES (EX : TR TABLEAU REEL)
-!     MODE : <-- donnee, --> resultat, <-> Donnee modifiee
-!            --- tableau de travail
-!===============================================================================
 
 !===============================================================================
 ! Module files
@@ -94,32 +68,20 @@ double precision dt(ncelet)
 
 ! Local variables
 
-
 !===============================================================================
-
-!===============================================================================
-! 1.  INITIALISATION VARIABLES LOCALES
-!===============================================================================
-
-
-!===============================================================================
-! 2. AIGUILLAGE VERS LE MODELE ADEQUAT
-!===============================================================================
-
 
 ! ---> Combustion gaz
 !      Flamme de diffusion : chimie 3 points
-
- if ( ippmod(icod3p).ge.0 ) then
+if ( ippmod(icod3p).ge.0 ) then
   call d3pini                                                     &
- ( nvar   , nscal  ,                                              &
-   dt     )
-  endif
+    ( nvar   , nscal  ,                                              &
+    dt     )
+endif
 
 ! ---> Combustion gaz
 !      Flamme de premelange : modele EBU
 
- if (ippmod(icoebu).ge.0) then
+if (ippmod(icoebu).ge.0) then
   call ebuini(nvar, nscal, dt)
 endif
 
@@ -156,14 +118,9 @@ if ( ippmod(ieljou).ge.1 .or.                                     &
      ippmod(ielarc).ge.1       ) then
 
   call eliniv(isuite)
-
-  ! give back the hand to the user... in fortran.
-  call cs_user_f_initialization(nvar, nscal, dt)
-
 endif
 
 ! ---> Ecoulements atmospheriques
-
 if (ippmod(iatmos).ge.0) then
 
   call atiniv                                                     &
@@ -175,19 +132,85 @@ endif
 ! ---> Cooling towers
 
 if (ippmod(iaeros).ge.0) then
+  call ctiniv0(nvar, nscal, dt)
+endif
 
-  call ctiniv(nvar, nscal, dt)
+!----
+! Formats
+!----
 
+!----
+! End
+!----
+
+return
+end subroutine
+
+!-------------------------------------------------------------------------------
+!> \file ppiniv.f90
+!> \brief Initialisation of specific physic variables
+!>
+!>  Physical properties SHOULD not be modified here
+!>  Second stage after the GUI and user modifications.
+!-------------------------------------------------------------------------------
+! Arguments
+!______________________________________________________________________________.
+!  mode           name          role                                           !
+!______________________________________________________________________________!
+!> \param[in]   nvar        total number of variables
+!> \param[in]   nscal       total number of scalars
+!> \param[in]   dt          time step value
+!-------------------------------------------------------------------------------
+
+
+subroutine ppiniv1 &
+ ( nvar   , nscal  ,                                              &
+   dt     )
+
+!===============================================================================
+
+!===============================================================================
+! Module files
+!===============================================================================
+
+use paramx
+use numvar
+use optcal
+use cstphy
+use cstnum
+use entsor
+use parall
+use ppppar
+use ppthch
+use coincl
+use cpincl
+use ppincl
+use mesh
+
+!===============================================================================
+
+implicit none
+
+integer          nvar   , nscal
+
+double precision dt(ncelet)
+
+! Local variables
+
+!===============================================================================
+
+! ---> Cooling towers
+
+if (ippmod(iaeros).ge.0) then
+  call ctiniv1(nvar, nscal, dt)
 endif
 
 ! Gas mixture modelling in presence of noncondensable gases and
 ! condensable gas as stream.
 if (ippmod(igmix).ge.0) then
-
   call cs_gas_mix_initialization &
   ( nvar   , nscal  ,                                            &
     dt     )
-
 endif
 
 ! Compressible
@@ -195,9 +218,7 @@ endif
 ! mixture composition is taken into account in the thermodynamic
 ! law, if gas mix specific physics is enabled.
 if (ippmod(icompf).ge.0) then
-
   call cfiniv(nvar, nscal, dt)
-
 endif
 
 !----

src/atmo/atiniv.f90
@@ -381,18 +381,9 @@ if (isuite.eq.0) then
 
 endif
 
-!===============================================================================
-! 4. USER  OPTIONS
-!===============================================================================
-
-call cs_user_f_initialization &
-!==========================
-( nvar   , nscal  ,                                            &
-  dt     )
-
-!----
-! FORMATS
-!----
+!--------
+! Formats
+!--------
 
  1000 format(                                                     &
 '@                                                            ',/,&

src/base/cs_gas_mix_initialization.f90
@@ -127,16 +127,9 @@ call field_get_key_struct_gas_mix_species_prop(f_id, s_d)
 call field_get_val_s(igmxml, mix_mol_mas)
 
 !===============================================================================
-! 2. User initialization
-!===============================================================================
-
-call cs_user_f_initialization &
-( nvar   , nscal  ,                                            &
-  dt     )
-
-!===============================================================================
-! 3. Deduce the mass fraction (y_d) from the mass fractions (yk) of
+! 2. Deduce the mass fraction (y_d) from the mass fractions (yk) of
 !    the noncondensable gases transported
+!    Note: mass fraction has been provided by the user in the GUI or routines
 !===============================================================================
 
 if (isuite.eq.0) then
@@ -205,7 +198,7 @@ if (isuite.eq.0) then
   endif
 
   !===============================================================================
-  ! 4. Print to the log to check the variables intialization
+  ! 3. Print to the log to check the variables intialization
   !===============================================================================
 
   write(nfecra, 200)
@@ -214,7 +207,7 @@ if (isuite.eq.0) then
 endif
 
 !===============================================================================
-! 5. Stop if problem
+! 4. Stop if problem
 !===============================================================================
 
 if (iok.gt.0) then

src/base/inivar.f90
@@ -171,25 +171,29 @@ do ivar = 1, nvar
   endif
 enddo
 
+! First pre-initialization for specific physic modules
+! before the GUI
+if (ippmod(iphpar).gt.0) then
+  call ppiniv0(nvar, nscal, dt)
+endif
+
 ! - Interface Code_Saturne
 !   ======================
 
 call uiiniv (isuite, ippmod(idarcy), ithvar)
 
-!   - Sous-programme utilisateur
-!     ==========================
+! User subroutine
+! ===============
 
-if (ippmod(iphpar).eq.0) then
-
-  call cs_user_f_initialization(nvar, nscal, dt)
-
-else
-
-  call ppiniv(nvar, nscal, dt)
-
-endif
+call cs_user_f_initialization(nvar, nscal, dt)
 
 call user_initialization()
+
+! Second stage of initialization for specific physic modules
+! after the user
+if (ippmod(iphpar).gt.0) then
+  call ppiniv1(nvar, nscal, dt)
+endif
 
 ! VoF algorithm
 if (ivofmt.gt.0) then

src/cfbl/cfiniv.f90
@@ -90,15 +90,6 @@ double precision, dimension(:), pointer :: cpro_cp, cpro_cv, mix_mol_mas
 !===============================================================================
 
 !===============================================================================
-! User initialisation of the variables only if this not a resumed computation
-! and if it has not already been done in gas mix initialization
-!===============================================================================
-
-if (isuite.eq.0.and.ippmod(igmix).lt.0) then
-  call cs_user_f_initialization(nvar, nscal, dt)
-endif
-
-!===============================================================================
 ! Computation of variable Cv in order to have a correct initialization
 ! of the total energy (computed in inivar by a call to a thermodynamic
 ! function), now that initial gas mixture composition is known.

src/cogz/d3pini.f90
@@ -60,10 +60,6 @@ subroutine d3pini &
 ! dt(ncelet)       ! tr ! <-- ! valeur du pas de temps                         !
 !__________________!____!_____!________________________________________________!
 
-!     TYPE : E (ENTIER), R (REEL), A (ALPHANUMERIQUE), T (TABLEAU)
-!            L (LOGIQUE)   .. ET TYPES COMPOSES (EX : TR TABLEAU REEL)
-!     MODE : <-- donnee, --> resultat, <-> Donnee modifiee
-!            --- tableau de travail
 !===============================================================================
 
 !===============================================================================
@@ -179,11 +175,6 @@ if ( isuite.eq.0 ) then
   ( mode   , ngazg , ngazgm  , coefg  ,                           &
     npo    , npot   , th     , ehgazg ,                           &
     hinfue , tinfue )
-
-  ! User initilization
-  call cs_user_f_initialization &
-  ( nvar   , nscal  ,                                            &
-    dt     )
 
   ! ---> Parallelism and periodic exchange
   if (irangp.ge.0.or.iperio.eq.1) then

src/cogz/ebuini.f90
@@ -21,8 +21,6 @@
 !-------------------------------------------------------------------------------
 
 subroutine ebuini &
-!================
-
  ( nvar   , nscal  ,                                              &
    dt     )
 
@@ -125,7 +123,6 @@ save             ipass
 !===============================================================================
 
 ipass = ipass + 1
-
 
 do igg = 1, ngazgm
   coefg(igg) = zero
@@ -315,12 +312,6 @@ if ( isuite.eq.0 ) then
 
     enddo
 
-! ----- On donne la main a l'utilisateur
-    call cs_user_f_initialization &
-    !==========================
-  ( nvar   , nscal  ,                                            &
-    dt     )
-
 ! ----- En periodique et en parallele,
 !       il faut echanger ces initialisations
 
@@ -336,7 +327,6 @@ if ( isuite.eq.0 ) then
         !==========
       endif
     endif
-
 
 !      Impressions de controle
 

src/cogz/lwcini.f90
@@ -126,7 +126,6 @@ save             ipass
 
 ipass = ipass + 1
 
-
 do igg = 1, ngazgm
   coefg(igg) = zero
 enddo
@@ -301,12 +300,6 @@ if ( isuite.eq.0 ) then
           hinit  , tentm )
     endif
 
-
-! ----- On donne la main a l'utilisateur
-    call cs_user_f_initialization &
-  ( nvar   , nscal  ,                                            &
-    dt     )
-
 ! ----- En periodique et en parallele,
 !       il faut echanger ces initialisations
 
@@ -326,7 +319,6 @@ if ( isuite.eq.0 ) then
       endif
 
     endif
-
 
 !      Impressions de controle
 

commit 3763f4a4df248faebcbc45c60b8600e2ebb2d8f8

Fix for electric arcs initialization, broken by commit 69e7849d (Aug 25, 2020).

src/pprt/ppiniv.f90
@@ -72,10 +72,8 @@ double precision dt(ncelet)
 
 ! ---> Combustion gaz
 !      Flamme de diffusion : chimie 3 points
-if ( ippmod(icod3p).ge.0 ) then
-  call d3pini                                                     &
-    ( nvar   , nscal  ,                                              &
-    dt     )
+if (ippmod(icod3p).ge.0) then
+  call d3pini(nvar, nscal, dt)
 endif
 
 ! ---> Combustion gaz
@@ -106,27 +104,13 @@ endif
 
 ! ---> Combustion fuel
 
-if  (ippmod(icfuel).ge.0) then
+if (ippmod(icfuel).ge.0) then
   call cs_fuel_varini(nvar, nscal, dt)
-endif
-
-! ---> Version electrique
-!      Effet Joule
-!      Conduction ionique
-
-if ( ippmod(ieljou).ge.1 .or.                                     &
-     ippmod(ielarc).ge.1       ) then
-
-  call eliniv(isuite)
 endif
 
 ! Atmospheric flows, first stage
 if (ippmod(iatmos).ge.0) then
-
-  call atiniv0                                                     &
- ( nvar   , nscal  ,                                              &
-   dt     )
-
+  call atiniv0(nvar, nscal, dt)
 endif
 
 ! ---> Cooling towers
@@ -199,13 +183,15 @@ double precision dt(ncelet)
 
 !===============================================================================
 
+! Electric arcs, Joule effect or ionic conduction
+
+if (ippmod(ieljou).ge.1 .or. ippmod(ielarc).ge.1) then
+  call eliniv(isuite)
+endif
+
 ! Atmospheric flows, second stage
 if (ippmod(iatmos).ge.0) then
-
-  call atiniv1                                                     &
- ( nvar   , nscal  ,                                              &
-   dt     )
-
+  call atiniv1(nvar, nscal, dt)
 endif
 
 ! ---> Cooling towers
@@ -216,9 +202,7 @@ endif
 ! Gas mixture modelling in presence of noncondensable gases and
 ! condensable gas as stream.
 if (ippmod(igmix).ge.0) then
-  call cs_gas_mix_initialization &
-  ( nvar   , nscal  ,                                            &
-    dt     )
+  call cs_gas_mix_initialization(nvar, nscal, dt)
 endif
 
 ! Compressible