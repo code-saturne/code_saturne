code-saturne/code_saturne

commit d061a7b5b7d35543b961791a02f0bc23cb71adac

Call turbulence user source terms earlier

src/turb/resrij.f90
@@ -259,8 +259,6 @@ endif
 ! 2. User source terms
 !===============================================================================
 
-call user_source_terms(ivarfl(ivar), smbr, rovsdt)
-
 !     If we extrapolate the source terms
 if (st_prv_id.ge.0) then
   do iel = 1, ncel

src/turb/resrij2.f90
@@ -264,16 +264,6 @@ endif
 ! 2. User source terms
 !===============================================================================
 
-call cs_user_turbulence_source_terms2 &
- ( nvar   , nscal  , ncepdp , ncesmp ,                            &
-   ivarfl(irij)    ,                                              &
-   icepdc , icetsm , itypsm ,                                     &
-   ckupdc , smacel ,                                              &
-   smbr   , rovsdt )
-
-! C version
-call user_source_terms(ivarfl(irij), smbr, rovsdt)
-
   !     If we extrapolate the source terms
 if (st_prv_id.ge.0) then
   do iel = 1, ncel

src/turb/resssg.f90
@@ -300,8 +300,6 @@ endif
 ! 2. User source terms
 !===============================================================================
 
-call user_source_terms(ivarfl(ivar), smbr, rovsdt)
-
 !     If we extrapolate the source terms
 if (st_prv_id.ge.0) then
   do iel = 1, ncel

src/turb/turrij.f90
@@ -360,6 +360,20 @@ iprev = 1
 call field_gradient_vector(ivarfl(iu), iprev, 0, inc, gradv)
 
 !===============================================================================
+! 1.2 Call source terms for Rij
+!===============================================================================
+
+call cs_user_turbulence_source_terms2 &
+ ( nvar   , nscal  , ncepdp , ncesmp ,                            &
+   ivarfl(ivar)    ,                                              &
+   icepdc , icetsm , itypsm ,                                     &
+   ckupdc , smacel ,                                              &
+   smbrts , rovsdtts, gradv)
+
+! C version
+call user_source_terms(ivarfl(ivar), smbrts, rovsdtts)
+
+!===============================================================================
 ! 2.2 Compute the production term for Rij
 !===============================================================================
 

src/user/cs_user_source_terms.f90
@@ -620,7 +620,7 @@ subroutine cs_user_turbulence_source_terms2 &
    f_id   ,                                                       &
    icepdc , icetsm , itypsm ,                                     &
    ckupdc , smacel ,                                              &
-   crvexp , crvimp )
+   crvexp , crvimp, gradv)
 
 !===============================================================================
 
@@ -656,6 +656,7 @@ integer          icetsm(ncesmp), itypsm(ncesmp,nvar)
 double precision dt(ncelet)
 double precision ckupdc(6,ncepdp), smacel(ncesmp,nvar)
 double precision crvexp(6,ncelet), crvimp(6,6,ncelet)
+double precision gradv(3, 3, ncelet)
 
 ! Local variables
 

src/turb/resssg2.f90
@@ -317,16 +317,6 @@ endif
 ! 2. User source terms
 !===============================================================================
 
-call cs_user_turbulence_source_terms2 &
- ( nvar   , nscal  , ncepdp , ncesmp ,                            &
-   ivarfl(ivar)    ,                                              &
-   icepdc , icetsm , itypsm ,                                     &
-   ckupdc , smacel ,                                              &
-   smbr   , rovsdt )
-
-! C version
-call user_source_terms(ivarfl(ivar), smbr, rovsdt)
-
 do isou = 1, dimrij
   ! If we extrapolate the source terms
   if (st_prv_id.ge.0) then

commit 1d31a1e00883f437020ee19573945dac963d8251

Fix bug introduced in d061a7b5 (2020-09-17)
User source termes for Rij (coupled version) are called before...
This was broken the non coupled version (and other problems).

src/turb/resrij.f90
@@ -259,6 +259,8 @@ endif
 ! 2. User source terms
 !===============================================================================
 
+call user_source_terms(ivarfl(ivar), smbr, rovsdt)
+
 !     If we extrapolate the source terms
 if (st_prv_id.ge.0) then
   do iel = 1, ncel

src/turb/resrij2.f90
@@ -71,7 +71,7 @@ subroutine resrij2 &
  ( nvar   , nscal  , ncepdp , ncesmp ,                            &
    icepdc , icetsm , itypsm ,                                     &
    dt     ,                                                       &
-   gradv, produc , gradro ,                                              &
+   gradv  , produc , gradro ,                                     &
    ckupdc , smacel ,                                              &
    viscf  , viscb  ,                                              &
    tslagi ,                                                       &

src/turb/resssg.f90
@@ -300,6 +300,8 @@ endif
 ! 2. User source terms
 !===============================================================================
 
+call user_source_terms(ivarfl(ivar), smbr, rovsdt)
+
 !     If we extrapolate the source terms
 if (st_prv_id.ge.0) then
   do iel = 1, ncel

src/turb/turrij.f90
@@ -216,6 +216,24 @@ endif
 call field_get_key_id("time_extrapolated", key_t_ext_id)
 
 !===============================================================================
+! 1.1 Call source terms for Rij
+!===============================================================================
+
+if (irijco.eq.1) then
+
+  call cs_user_turbulence_source_terms2 &
+   ( nvar   , nscal  , ncepdp , ncesmp ,                            &
+     ivarfl(irij)    ,                                              &
+     icepdc , icetsm , itypsm ,                                     &
+     ckupdc , smacel ,                                              &
+     smbrts , rovsdtts)
+
+  ! C version
+  call user_source_terms(ivarfl(irij), smbrts, rovsdtts)
+
+endif
+
+!===============================================================================
 ! 1.1 Advanced init for EBRSM
 !===============================================================================
 
@@ -358,20 +376,6 @@ inc = 1
 iprev = 1
 
 call field_gradient_vector(ivarfl(iu), iprev, 0, inc, gradv)
-
-!===============================================================================
-! 1.2 Call source terms for Rij
-!===============================================================================
-
-call cs_user_turbulence_source_terms2 &
- ( nvar   , nscal  , ncepdp , ncesmp ,                            &
-   ivarfl(ivar)    ,                                              &
-   icepdc , icetsm , itypsm ,                                     &
-   ckupdc , smacel ,                                              &
-   smbrts , rovsdtts, gradv)
-
-! C version
-call user_source_terms(ivarfl(ivar), smbrts, rovsdtts)
 
 !===============================================================================
 ! 2.2 Compute the production term for Rij
@@ -599,7 +603,7 @@ if (irijco.eq.1) then
    ckupdc , smacel ,                                              &
    viscf  , viscb  ,                                              &
    tslagi ,                                                       &
-   smbrts   , rovsdtts )
+   smbrts , rovsdtts )
 
   ! Rij-epsilon SSG or EBRSM
   elseif (iturb.eq.31.or.iturb.eq.32) then

src/user/cs_user_source_terms.f90
@@ -620,7 +620,7 @@ subroutine cs_user_turbulence_source_terms2 &
    f_id   ,                                                       &
    icepdc , icetsm , itypsm ,                                     &
    ckupdc , smacel ,                                              &
-   crvexp , crvimp, gradv)
+   crvexp , crvimp )
 
 !===============================================================================
 
@@ -656,7 +656,6 @@ integer          icetsm(ncesmp), itypsm(ncesmp,nvar)
 double precision dt(ncelet)
 double precision ckupdc(6,ncepdp), smacel(ncesmp,nvar)
 double precision crvexp(6,ncelet), crvimp(6,6,ncelet)
-double precision gradv(3, 3, ncelet)
 
 ! Local variables
 