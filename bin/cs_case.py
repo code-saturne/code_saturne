code-saturne/code_saturne

commit d1a5e681666864a5014924ed8339679807db7f60

Added Code_Saturne submit command. This includes refactoring of suggested run-id handling.
git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@8190 e44c5d27-f830-0410-8ce8-974f7a764be6

bin/cs_case.py
@@ -470,20 +470,19 @@ class case:
 
         r = os.path.join(self.case_dir, 'RESU')
 
+        # Coupled case
+        if len(self.domains) + len(self.syr_domains) + len(self.ast_domains) > 1:
+            r += '_COUPLING'
+
         if os.path.isdir(r):
             self.result_dir = os.path.join(r, self.run_id)
         else:
-            r += '_COUPLING'
-            if os.path.isdir(r):
-                self.result_dir = os.path.join(r, self.run_id)
-            else:
-                r = os.path.join(self.case_dir, 'RESU')
-                err_str = \
+            r = os.path.join(self.case_dir, 'RESU')
+            err_str = \
                     '\nResults directory: ' + r + '\n' \
-                    + '               or: ' + r + '_COUPLING' + '\n' \
                     + 'does not exist.\n' \
                     + 'Calculation will not be run.\n'
-                raise RunCaseError(err_str)
+            raise RunCaseError(err_str)
 
     #---------------------------------------------------------------------------
 
@@ -491,12 +490,13 @@ class case:
 
         self.define_result_dir()
 
-        if os.path.isdir(self.result_dir) and not force:
-            err_str = \
-                '\nResults directory: ' + self.result_dir \
-                + ' already exists.\n' \
-                + 'Calculation will not be run.\n'
-            raise RunCaseError(err_str)
+        if os.path.isdir(self.result_dir):
+            if not force:
+                err_str = \
+                    '\nResults directory: ' + self.result_dir \
+                    + ' already exists.\n' \
+                    + 'Calculation will not be run.\n'
+                raise RunCaseError(err_str)
 
         else:
             os.mkdir(self.result_dir)
@@ -1354,8 +1354,7 @@ $appli/runSession $appli/bin/salome/driver -e -d 0 fsi_yacs_scheme.xml
     #---------------------------------------------------------------------------
 
     def set_run_id(self,
-                   run_id = None,
-                   force_id = False):
+                   run_id = None):
 
         """
         Set run id for calculation.
@@ -1363,18 +1362,18 @@ $appli/runSession $appli/bin/salome/driver -e -d 0 fsi_yacs_scheme.xml
 
         if run_id != None:
             self.run_id = run_id
+            self.define_result_dir()
+
+        # When id not assigned, choose an id not already present
 
         if self.run_id == None:
-            now = datetime.datetime.now()
-            run_id_base = now.strftime('%Y%m%d-%H%M')
-            self.run_id = run_id_base
-            # When id not forced, choose an id not already present
-            if not force_id:
+            self.run_id = self.suggest_id()
+            self.define_result_dir()
+            j = 1
+            run_id_base = self.run_id
+            while os.path.isdir(self.result_dir):
+                self.run_id = run_id_base + '_' + str(j)
                 self.define_result_dir()
-                j = 0
-                while os.path.isdir(self.result_dir):
-                    self.run_id = run_id_base + str(j)
-                    self.define_result_dir()
 
     #---------------------------------------------------------------------------
 
@@ -1562,9 +1561,8 @@ $appli/runSession $appli/bin/salome/driver -e -d 0 fsi_yacs_scheme.xml
         # Standard or error exit
 
         if len(self.error) > 0:
-            stage = {'preprocess':'preprocessing'}
-            if self.error in stage:
-                error_stage = stage[self.error]
+            if self.error == 'preprocess':
+                error_stage = 'preprocessing'
             else:
                 error_stage = self.error
             err_str = ' Error in ' + error_stage + ' stage.\n\n'
@@ -1728,13 +1726,19 @@ $appli/runSession $appli/bin/salome/driver -e -d 0 fsi_yacs_scheme.xml
             scratchdir = None,
             run_id = None,
             force_id = False,
-            prepare_data = True,
-            run_solver = True,
-            save_results = True):
+            stages = None):
 
         """
         Main script.
         """
+
+        # Define run stages if not provided
+
+        if not stages:
+            stages = {'prepare_data':True,
+                      'initialize':True,
+                      'run_solver':True,
+                      'save_results':True}
 
         # Define scratch directory
         # priority: argument, environment variable, preference setting.
@@ -1776,12 +1780,12 @@ $appli/runSession $appli/bin/salome/driver -e -d 0 fsi_yacs_scheme.xml
         # If preparation stage is not requested, it must have been done
         # previously, and the id must be forced.
 
-        if not prepare_data:
+        if not stages['prepare_data']:
             force_id = True
 
         # Run id and associated directories
 
-        self.set_run_id(run_id, force_id)
+        self.set_run_id(run_id)
 
         # Set result copy mode
 
@@ -1796,8 +1800,8 @@ $appli/runSession $appli/bin/salome/driver -e -d 0 fsi_yacs_scheme.xml
 
         msg = \
             '\n' \
-            + '                      ' + self.package.code_name + ' is running\n' \
-            + '                      ***********************\n' \
+            + '                      ' + self.package.code_name + '\n' \
+            + '                      ************\n' \
             + '\n' \
             + ' Version:   ' + self.package.version + '\n' \
             + ' Path:      ' + self.package.get_dir('exec_prefix') + '\n'
@@ -1821,17 +1825,17 @@ $appli/runSession $appli/bin/salome/driver -e -d 0 fsi_yacs_scheme.xml
 
         try:
             retcode = 0
-            if prepare_data:
+            if stages['prepare_data']:
                 retcode = self.prepare_data(force_id)
 
-            if prepare_data and  retcode == 0:
+            if stages['initialize'] and  retcode == 0:
                 retcode = self.preprocess(n_procs,
                                           n_threads,
                                           mpiexec_options)
-            if run_solver == True and retcode == 0:
+            if stages['run_solver'] == True and retcode == 0:
                 self.run_solver()
 
-            if save_results == True:
+            if stages['save_results'] == True:
                 self.save_results()
 
         finally:
@@ -1846,10 +1850,10 @@ $appli/runSession $appli/bin/salome/driver -e -d 0 fsi_yacs_scheme.xml
         # Standard or error exit
 
         if len(self.error) > 0:
-            stage = {'preprocess':'preprocessing',
-                     'solver':'calculation'}
-            if self.error in stage:
-                error_stage = stage[self.error]
+            check_stage = {'preprocess':'preprocessing',
+                           'solver':'calculation'}
+            if self.error in check_stage:
+                error_stage = check_stage[self.error]
             else:
                 error_stage = self.error
             err_str = ' Error in ' + error_stage + ' stage.\n\n'
@@ -1857,6 +1861,48 @@ $appli/runSession $appli/bin/salome/driver -e -d 0 fsi_yacs_scheme.xml
             return 1
         else:
             return 0
+
+    #---------------------------------------------------------------------------
+
+    def suggest_id(self,
+                   run_id_prefix = None,
+                   run_id_suffix = None):
+
+        """
+        Suggest run id.
+        """
+
+        now = datetime.datetime.now()
+        run_id_base = now.strftime('%Y%m%d-%H%M')
+
+        r = os.path.join(self.case_dir, 'RESU')
+
+        if len(self.domains) + len(self.syr_domains) + len(self.ast_domains) > 1:
+            r += '_COUPLING'
+
+        # Make sure directory does not exist already
+
+        j = 0
+
+        while True:
+
+            run_id = run_id_base
+            if j > 0:
+                run_id += '_' + str(j)
+
+            if run_id_prefix:
+                run_id = run_id_prefix + run_id
+            if run_id_suffix:
+                run_id = run_id_suffix + run_id
+
+            result_dir = os.path.join(r, run_id)
+
+            if os.path.isdir(result_dir):
+                j += 1
+            else:
+                break
+
+        return run_id
 
 #-------------------------------------------------------------------------------
 # End

Makefile.am
@@ -242,6 +242,7 @@ bin/cs_run.py \
 bin/cs_runcase.py \
 bin/cs_salome.py \
 bin/cs_script.py \
+bin/cs_submit.py \
 bin/cs_xml_reader.py
 
 autovnvdir = $(pkgpythondir)/autovnv

bin/cs_batch.py
@@ -86,6 +86,8 @@ class batch:
 
         self.rm_type = None
 
+        self.submit_cmd = ''
+
         self.params = {}
 
         self.params['job_name'] = None
@@ -776,7 +778,7 @@ class batch:
 
         cmd_prefix = ''
 
-        rm_type = self.batch.rm_type
+        rm_type = self.rm_type
 
         if rm_type == 'SLURM':
             cmd_prefix = 'sbatch '

bin/cs_run.py
@@ -34,7 +34,6 @@ This module defines the following functions:
 # Import required Python modules
 #===============================================================================
 
-import datetime
 import os, sys
 import types, string, re, fnmatch
 from optparse import OptionParser
@@ -108,6 +107,10 @@ def process_cmd_line(argv, pkg):
                       help="run the data preparation stage even if " \
                            + "the matching execution directory exists")
 
+    parser.add_option("--stage", dest="stage",
+                      action="store_true",
+                      help="stage data prior to preparation and execution")
+
     parser.add_option("--initialize", dest="initialize",
                       action="store_true",
                       help="run the data preparation stage")
@@ -122,6 +125,7 @@ def process_cmd_line(argv, pkg):
 
     parser.set_defaults(compute_build=False)
     parser.set_defaults(suggest_id=False)
+    parser.set_defaults(stage=False)
     parser.set_defaults(initialize=False)
     parser.set_defaults(execute=False)
     parser.set_defaults(finalize=False)
@@ -213,57 +217,76 @@ def process_cmd_line(argv, pkg):
     # Stages to run (if no filter given, all are done).
 
     compute_build = options.compute_build
-    prepare_data = options.initialize
-    run_solver = options.execute
-    save_results = options.finalize
+
+    stages = {'prepare_data':options.stage,
+              'initialize':options.initialize,
+              'run_solver':options.execute,
+              'save_results':options.finalize}
 
     if not options.force:
         force_id = False
     else:
         force_id = True
 
-    if not (prepare_data or run_solver or save_results):
-        prepare_data = True
-        run_solver = True
-        save_results = True
+    # Stages to run (if no filter given, all are run; specific stages
+    # are given, all stages in thar range are run).
+
+    ordered_stages = ['prepare_data',
+                      'initialize',
+                      'run_solver',
+                      'save_results']
+
+    stages = {'prepare_data':options.stage,
+              'initialize':options.initialize,
+              'run_solver':options.execute,
+              'save_results':options.finalize}
+
+    stages_start = len(ordered_stages)
+    stages_end = -1
+
+    i = 0
+    for k in ordered_stages:
+        if stages[k] == True and stages_start > i:
+            stages_start = i
+        if stages[k] and stages_end < i+1:
+            stages_end = i + 1
+        i += 1
+
+    # Default if nothing provided, ensure range is filled otherwise
+
+    if stages_end < 0:
+        for k in ordered_stages:
+            stages[k] = True
+
+    else:
+        for i in range(stages_start + 1, stages_end -1):
+            stages[ordered_stages[i]] = True
+
+    # Forced number of ranks and threads
 
     n_procs = options.nprocs
     n_threads = options.nthreads
 
     return  (casedir, options.id, param, coupling,
              options.id_prefix, options.id_suffix, options.suggest_id, force_id,
-             n_procs, n_threads, prepare_data, run_solver, save_results,
-             compute_build)
+             n_procs, n_threads, stages, compute_build)
 
 #===============================================================================
 # Run the calculation
 #===============================================================================
 
-def main(argv, pkg):
+def run(argv, pkg):
     """
-    Main function.
+    Run calculation;
+    returns return code, run id, and results directory path when created.
     """
 
     (casedir, run_id, param, coupling,
      id_prefix, id_suffix, suggest_id, force, n_procs, n_threads,
-     prepare_data, run_solver, save_results,
-     compute_build) = process_cmd_line(argv, pkg)
+     stages, compute_build) = process_cmd_line(argv, pkg)
 
     if not casedir:
-        return 1
-
-    if not run_id or suggest_id:
-        now = datetime.datetime.now()
-        run_id = now.strftime('%Y%m%d-%H%M')
-
-    if id_prefix:
-        run_id = id_prefix + run_id
-    if id_suffix:
-        run_id += id_suffix
-
-    if suggest_id:
-        print(run_id)
-        return 0
+        return 1, None, None
 
     # Use alternate compute (back-end) package if defined
 
@@ -305,17 +328,34 @@ def main(argv, pkg):
                          case_dir=casedir,
                          domains=d)
 
+    # Determine run id if not forced
+
+    if not run_id or suggest_id:
+        run_id = c.suggest_id(id_prefix, id_suffix)
+
+        if suggest_id:
+            print(run_id)
+            return 0, run_id, None
+
     # Now run case
 
     retval = c.run(n_procs=n_procs,
                    n_threads=n_threads,
                    run_id=run_id,
                    force_id=force,
-                   prepare_data=prepare_data,
-                   run_solver=run_solver,
-                   save_results=save_results)
+                   stages=stages)
 
-    return retval
+    return retval, c.run_id, c.result_dir
+
+#===============================================================================
+# Main function
+#===============================================================================
+
+def main(argv, pkg):
+    """
+    Main function.
+    """
+    return run(argv, pkg)[0]
 
 #-------------------------------------------------------------------------------
 

bin/cs_runcase.py
@@ -145,6 +145,24 @@ class runcase(object):
 
     #---------------------------------------------------------------------------
 
+    def get_run_args(self):
+        """
+        Get the run command and arguments, as a list
+        """
+
+        return separate_args(self.lines[self.run_cmd_line_id])
+
+    #---------------------------------------------------------------------------
+
+    def set_run_args(self, args):
+        """
+        Set the run command and arguments from a list
+        """
+
+        self.lines[self.run_cmd_line_id] = assemble_args(args)
+
+    #---------------------------------------------------------------------------
+
     def build_template(self, package=None, study_name=None, case_name=None):
         """
         Build batch file template

bin/cs_script.py
@@ -43,7 +43,8 @@ class master_script:
                          'gui':self.gui,
                          'info':self.info,
                          'run':self.run,
-                         'salome':self.salome}
+                         'salome':self.salome,
+                         'submit':self.submit}
 
         if package != None:
             sys.path.insert(1, package.get_dir('pythondir'))
@@ -107,6 +108,7 @@ Topics:
   info
   run
   salome
+  submit
 
 Options:
   -h, --help  show this help message and exit"""
@@ -152,6 +154,10 @@ Options:
     def salome(self, options = None):
         import cs_salome
         return cs_salome.main(options, self.package)
+
+    def submit(self, options = None):
+        import cs_submit
+        return cs_submit.main(options, self.package)
 
 #-------------------------------------------------------------------------------
 # End

bin/cs_submit.py
@@ -1,0 +1,196 @@
+#!/usr/bin/env python
+
+#-------------------------------------------------------------------------------
+
+# This file is part of Code_Saturne, a general-purpose CFD tool.
+#
+# Copyright (C) 1998-2015 EDF S.A.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+# Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+#-------------------------------------------------------------------------------
+
+"""
+This module describes the script used to run a study/case for Code_Saturne.
+
+This module defines the following functions:
+- process_cmd_line
+- main
+"""
+
+#===============================================================================
+# Import required Python modules
+#===============================================================================
+
+import os, sys, subprocess
+import types, string
+
+import cs_batch
+from cs_exec_environment import enquote_arg
+import cs_run
+import cs_runcase
+
+#-------------------------------------------------------------------------------
+# Print a help page.
+#-------------------------------------------------------------------------------
+
+def print_help(submit_cmd, pkg):
+    """
+    Print a help page.
+    """
+
+    help_string = \
+"""
+Usage: %s [batch options] <runcase_script> [script args]
+
+Options:
+
+  Any option provided by the batch submission command (%s)
+
+Runcase script:
+
+  Shell script containing "%s run" command.
+"""
+    print(help_string % sys.argv[0], pkg.name, submit_cmd)
+
+#-------------------------------------------------------------------------------
+# Process the command line arguments
+#-------------------------------------------------------------------------------
+
+def process_cmd_line(argv, submit_cmd, pkg):
+    """
+    Process the passed command line arguments.
+    """
+
+    runcase_path = None
+    need_help = False
+
+    if "-h" in argv or "--help" in argv:
+        need_help = True
+    elif len(argv) < 1:
+        need_help = True
+    else:
+        for a in argv:
+            if os.path.isfile(a):
+                runcase_path = a
+                break
+
+    if not runcase_path:
+        need_help = True
+
+    if need_help:
+        print_help(submit_cmd, pkg)
+
+    return runcase_path
+
+#===============================================================================
+# Run the calculation
+#===============================================================================
+
+def main(argv, pkg):
+    """
+    Main function.
+    """
+
+    # Use alternate compute (back-end) package if defined
+
+    batch = cs_batch.batch(pkg)
+
+    submit_cmd = batch.submit_command_prefix()
+
+    if not submit_cmd:
+        err_str = ' no batch system is configured:\n' \
+                  '   check ' + os.path.join(pkg.get_dir("sysconfdir"),
+                                             pkg.configfile) + '.'
+        raise Exception(err_str)
+
+    runcase_path = process_cmd_line(argv, submit_cmd, pkg)
+
+    scripts_dir = os.path.dirname(runcase_path)
+
+    runcase = cs_runcase.runcase(runcase_path)
+
+    # Adjust run command for staging only
+
+    run_args = runcase.get_run_args()
+
+    for a in ['--stage', '--initialize', '--execute', '--finalize']:
+        while a in run_args:
+            run_args.remove(a)
+
+    run_args.append('--stage')
+
+    retcode, run_id, result_path = cs_run.run(run_args, pkg)
+
+    # Now prepare runcase for submission
+
+    if retcode != 0 or not result_path:
+        err_str = ' run not staged due to prior error.'
+        raise Exception(err_str)
+
+    run_args = runcase.get_run_args()
+
+    while '--stage' in run_args:
+        run_args.remove(a)
+
+    stages = []
+    for a in ['--initialize', '--execute', '--finalize']:
+        if a in run_args:
+            stages.append(a)
+
+    if not stages:
+        run_args.append('--initialize')
+        run_args.append('--finalize')
+
+    runcase.set_run_args(run_args)
+    runcase.set_run_id(run_id=run_id)
+
+    runcase_path = os.path.join(result_path,
+                                os.path.basename(runcase_path))
+
+    runcase.save(runcase_path)
+
+    # Now submit case
+
+    save_wd = os.getcwd()
+    os.chdir(result_path)
+
+    for a in argv:
+        if os.path.isfile(a) and runcase_path:
+            submit_cmd += ' ' + enquote_arg(runcase_path)
+            runcase_path = None
+        else:
+            submit_cmd += ' ' + enquote_arg(a)
+
+    return subprocess.call(submit_cmd, shell=True)
+
+    os.chdir(save_wd)
+
+#-------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    # Run package
+    from cs_package import package
+    pkg = package()
+
+    retval = main(sys.argv[1:], pkg)
+
+    sys.exit(retval)
+
+#-------------------------------------------------------------------------------
+# End
+#-------------------------------------------------------------------------------
+

extras/bash_completion/code_saturne
@@ -165,9 +165,12 @@ _code_saturne() {
                 --n|--nprocs)            COMPREPLY=( ); return 0;;
                 --nt|--threads-per-task) COMPREPLY=( ); return 0;;
                 *) cmdOpts="-p --param --case --id --id-prefix --id-suffix \
-                     --suggest-id --force --initialize --execute --finalize \
-                     -n --nprocs --nt --threads-per-task";;
+                     --suggest-id --force --stage --initialize --execute \
+                     --finalize -n --nprocs --nt --threads-per-task";;
             esac
+            ;;
+        submit)
+            _filedir; return 0
             ;;
         *)
             cmdOpts=""

commit 2531f73cba67e06737d38f253be878932f0aa48f

[FIX] Print correct path when result dir cannot be created.
  * Bug introduced in commit d1a5e68166 (2015-11-23)

bin/cs_case.py
@@ -561,7 +561,6 @@ class case:
         if os.path.isdir(r):
             self.result_dir = os.path.join(r, self.run_id)
         else:
-            r = os.path.join(self.case_dir, 'RESU')
             err_str = \
                     '\nResults directory: ' + r + '\n' \
                     + 'does not exist.\n' \