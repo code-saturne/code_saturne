code-saturne/code_saturne

commit da085a9671f7f0c357366821305de7dd47768446

Always copy NCFD user functions to "EXAMPLES" folder when solver is available

bin/cs_update.py
@@ -179,9 +179,12 @@ def update_case(options, pkg):
         copy_directory(user_examples_distpath, user_examples, True)
 
         add_datadirs = []
-        if pkg.name == 'neptune_cfd' :
-            add_datadirs.append(os.path.join(pkg.get_dir("datadir"),
-                                             pkg.name))
+
+        # If neptune_cfd is present, copy user functions to EXAMPLES folder
+        ncfd_user_examples = os.path.join(self.package.get_dir("datadir"),
+                                          "neptune_cfd")
+        if os.path.isdir(ncfd_user_examples):
+            add_datadirs.append(ncfd_user_examples)
 
         for d in add_datadirs:
             user_ref_distpath = os.path.join(d, 'user')

bin/cs_create.py
@@ -538,9 +538,12 @@ class study:
             shutil.copytree(user_examples_distpath, user_examples)
 
             add_datadirs = []
-            if self.package.name == 'neptune_cfd' :
-                add_datadirs.append(os.path.join(self.package.get_dir("datadir"),
-                                                 self.package.name))
+
+            # If neptune_cfd is present, copy user functions to EXAMPLES folder
+            ncfd_user_examples = os.path.join(self.package.get_dir("datadir"),
+                                              "neptune_cfd")
+            if os.path.isdir(ncfd_user_examples):
+                add_datadirs.append(ncfd_user_examples)
 
             for d in add_datadirs:
                 user_distpath = os.path.join(d, 'user')

extras/code_saturne.desktop.template
@@ -3,8 +3,8 @@ Type=Application
 Terminal=false
 Exec=CSPATH/bin/code_saturne gui %f
 Name=Code_Saturne CSVERSION
-Icon=CSPATH/share/code_saturne/images/splashscreen.png
+Icon=CSPATH/share/code_saturne/images/logo_salome_cfd.png
 X-Desktop-File-Install-Version=0.23
-GenericName=Code_Saturne vCSVERSION
+GenericName=code_saturne vCSVERSION
 Categories=Education;Science;
 MimeType=application/x-hdf;

gui/Makefile.am
@@ -31,7 +31,8 @@ EXTRA_DIST = Base/icons/code_saturne.ico
 imagesdir = ${pkgdatadir}/images
 
 images_DATA = \
-Base/icons/splashscreen.png
+Base/icons/splashscreen.png \
+Base/icons/22x22/logo_salome_cfd.png
 
 install-exec-local:
 	@case ${host_os} in \

commit 593aae5e156f52577ff9f379829950cbe03de7b7

[TUI] Fix up/update command broken commit da085a9671
  * Typo introduced in commit da085a9671 on master (02-10-2020)
  * Close #234

bin/cs_update.py
@@ -181,7 +181,7 @@ def update_case(options, pkg):
         add_datadirs = []
 
         # If neptune_cfd is present, copy user functions to EXAMPLES folder
-        ncfd_user_examples = os.path.join(self.package.get_dir("datadir"),
+        ncfd_user_examples = os.path.join(pkg.get_dir("datadir"),
                                           "neptune_cfd")
         if os.path.isdir(ncfd_user_examples):
             add_datadirs.append(ncfd_user_examples)