bdf27f5ac7e641515ac0d72be2521dd1ec4464d0
YohannEude <yohann.eude@renuda.com>
2020-10-29 14:24:55 +0100

    Move of the "Gas combustion" panel before the "Thermal model" panel in the GUI

--- gui/Base/BrowserView.py
+++ gui/Base/BrowserView.py
@@ -482,7 +482,7 @@ class BrowserView(QWidget, Ui_BrowserForm):
             return ['Preprocessing']
         elif section == 'Calculation features':
             return ['Main fields', 'Deformable mesh', 'Turbulence models',
-                    'Thermal model', 'Body forces', 'Gas combustion',
+                    'Gas combustion', 'Thermal model', 'Body forces',
                     'Pulverized fuel combustion', 'Electrical models',
                     'Conjugate heat transfer', 'Atmospheric flows',
                     'Species transport', 'Turbomachinery', 'Groundwater flows',
