code-saturne/code_saturne

commit f5356d22941a6fd6d8487f1998861d9e0d41d0b9

Allow definition of constant Dirichlet BC's using a cs_equation_params_t structure.
This is the very first step of migrating all boundary condition definitons to
a setup similar to that used by CDO. Definitions done through the GUI
will now go through this type of structure.

This required calling some GUI-defined boundary initializations earlier,
and mapping some more Fortran variables to C to simplify the calls.

src/cdo/cs_equation_param.c
@@ -3028,13 +3028,14 @@ cs_equation_add_bc_by_value(cs_equation_param_t         *eqp,
                 "%s: This situation is not handled yet.\n", __func__);
   }
 
-  cs_flag_t  bc_flag = cs_cdo_bc_get_flag(bc_type);
+  cs_flag_t  meta_flag = (eqp-> space_scheme == CS_SPACE_SCHEME_LEGACY) ?
+    (cs_flag_t)bc_type : cs_cdo_bc_get_flag(bc_type);
 
   cs_xdef_t  *d = cs_xdef_boundary_create(CS_XDEF_BY_VALUE,
                                           dim,
                                           cs_get_bdy_zone_id(z_name),
                                           CS_FLAG_STATE_UNIFORM, // state flag
-                                          bc_flag, // meta
+                                          meta_flag,
                                           (void *)values);
 
   int  new_id = eqp->n_bc_defs;
@@ -3108,11 +3109,14 @@ cs_equation_add_bc_by_array(cs_equation_param_t        *eqp,
                 "%s: This situation is not handled yet.\n", __func__);
   }
 
+  cs_flag_t  meta_flag = (eqp-> space_scheme == CS_SPACE_SCHEME_LEGACY) ?
+    (cs_flag_t)bc_type : cs_cdo_bc_get_flag(bc_type);
+
   cs_xdef_t  *d = cs_xdef_boundary_create(CS_XDEF_BY_ARRAY,
                                           dim,
                                           z_id,
                                           state_flag,
-                                          cs_cdo_bc_get_flag(bc_type), // meta
+                                          meta_flag,
                                           (void *)&input);
 
   int  new_id = eqp->n_bc_defs;
@@ -3184,11 +3188,14 @@ cs_equation_add_bc_by_analytic(cs_equation_param_t        *eqp,
                                      .input = input,
                                      .free_input = NULL };
 
+  cs_flag_t  meta_flag = (eqp-> space_scheme == CS_SPACE_SCHEME_LEGACY) ?
+    (cs_flag_t)bc_type : cs_cdo_bc_get_flag(bc_type);
+
   cs_xdef_t  *d = cs_xdef_boundary_create(CS_XDEF_BY_ANALYTIC_FUNCTION,
                                           dim,
                                           z_id,
                                           0, // state
-                                          cs_cdo_bc_get_flag(bc_type), // meta
+                                          meta_flag,
                                           &ac);
 
   int  new_id = eqp->n_bc_defs;

src/base/condli.f90
@@ -375,8 +375,7 @@ call precli(nvar, icodcl, rcodcl)
 !    -> sera modifie lors de la restructuration des zones de bord
 
 call uiclim &
-  ( ippmod(idarcy),                                                &
-    nozppm, ncharm, ncharb, nclpch,                                &
+  ( nozppm,                                                        &
     iqimp,  icalke, ientat, ientcp, inmoxy, ientox,                &
     ientfu, ientgb, ientgf, iprofm, iautom,                        &
     itypfb, izfppp, icodcl,                                        &
@@ -5170,8 +5169,7 @@ call precli(nvar, icodcl, rcodcl)
 !    -> sera modifie lors de la restructuration des zones de bord
 
 call uiclim &
-  ( ippmod(idarcy),                                                &
-    nozppm, ncharm, ncharb, nclpch,                                &
+  ( nozppm,                                                        &
     iqimp,  icalke, ientat, ientcp, inmoxy, ientox,                &
     ientfu, ientgb, ientgf, iprofm, iautom,                        &
     itypfb, izfppp, icodcl,                                        &

src/base/cs_boundary.c
@@ -399,6 +399,13 @@ cs_boundary_id_by_zone_id(const cs_boundary_t  *boundaries,
   if (boundaries == NULL)
     return -1;
 
+  /* Fast test in case boundariy numbers are aligned with zones */
+  if (z_id > -1 && z_id < boundaries->n_boundaries) {
+    if (boundaries->zone_ids[z_id] == z_id)
+      return z_id;
+  }
+
+  /* General test */
   for (int i = 0; i < boundaries->n_boundaries; i++) {
     if (boundaries->zone_ids[i] == z_id)
       return i;

src/base/cs_boundary_conditions.c
@@ -57,6 +57,7 @@
 #include "cs_gradient.h"
 #include "cs_gui_util.h"
 #include "cs_field.h"
+#include "cs_field_default.h"
 #include "cs_field_operator.h"
 #include "cs_flag_check.h"
 #include "cs_halo.h"
@@ -221,6 +222,139 @@ _inlet_sum(int                          var_id,
   }
 
   cs_parall_sum(dim, CS_REAL_TYPE, inlet_sum);
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief   Compute the values of the homogeneous Dirichlet BCs
+ *
+ * \param[in]       mesh        pointer to mesh structure
+ * \param[in]       boundaries  pointer to associated boundaries
+ * \param[in]       eqp         pointer to a cs_equation_param_t
+ * \param[in]       def         pointer to a boundary condition definition
+ * \param[in]       var_id      matching variable id
+ * \param[in, out]  icodcl      boundary conditions type array
+ * \param[in, out]  rcodcl      boundary conditions values array
+ */
+/*----------------------------------------------------------------------------*/
+
+static void
+_compute_dirichlet_hmg_bc(const cs_mesh_t            *mesh,
+                          const cs_boundary_t        *boundaries,
+                          const cs_equation_param_t  *eqp,
+                          const cs_xdef_t            *def,
+                          int                         var_id,
+                          int                        *icodcl,
+                          double                     *rcodcl)
+{
+  assert(eqp->dim == def->dim);
+
+  const cs_lnum_t n_b_faces = mesh->n_b_faces;
+  const cs_zone_t *bz = cs_boundary_zone_by_id(def->z_id);
+  const cs_lnum_t *face_ids = bz->elt_ids;
+
+  cs_boundary_type_t boundary_type
+    = boundaries->types[cs_boundary_id_by_zone_id(boundaries, def->z_id)];
+
+  const int wall_type = (boundary_type & CS_BOUNDARY_WALL) ? 5 : 1;
+
+  for (int coo_id = 0; coo_id < def->dim; coo_id++) {
+
+    int        *_icodcl = icodcl + (var_id+coo_id)*n_b_faces;
+    cs_real_t  *_rcodcl = rcodcl + (var_id+coo_id)*n_b_faces;
+
+    if (face_ids == NULL) {
+#     pragma omp parallel for if (bz->n_elts > CS_THR_MIN)
+      for (cs_lnum_t i = 0; i < bz->n_elts; i++) {
+        _icodcl[i] = wall_type;
+        _rcodcl[i] = 0;
+      }
+    }
+    else {
+#     pragma omp parallel for if (bz->n_elts > CS_THR_MIN)
+      for (cs_lnum_t i = 0; i < bz->n_elts; i++) {
+        const cs_lnum_t  elt_id = (face_ids == NULL) ? i : face_ids[i];
+        _icodcl[elt_id] = wall_type;
+        _rcodcl[elt_id] = 0;
+      }
+    }
+  }
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief   Compute the values of the Dirichlet BCs
+ *
+ * \param[in]       mesh        pointer to mesh structure
+ * \param[in]       boundaries  pointer to associated boundaries
+ * \param[in]       eqp         pointer to a cs_equation_param_t
+ * \param[in]       def         pointer to a boundary condition definition
+ * \param[in]       var_id      matching variable id
+ * \param[in]       t_eval      time at which one evaluates the boundary cond.
+ * \param[in, out]  icodcl      boundary conditions type array
+ * \param[in, out]  rcodcl      boundary conditions values array
+ */
+/*----------------------------------------------------------------------------*/
+
+static void
+_compute_dirichlet_bc(const cs_mesh_t            *mesh,
+                      const cs_boundary_t        *boundaries,
+                      const cs_equation_param_t  *eqp,
+                      const cs_xdef_t            *def,
+                      int                         var_id,
+                      cs_real_t                   t_eval,
+                      int                        *icodcl,
+                      double                     *rcodcl)
+{
+  assert(eqp->dim == def->dim);
+
+  const cs_lnum_t n_b_faces = mesh->n_b_faces;
+  const cs_zone_t *bz = cs_boundary_zone_by_id(def->z_id);
+  const cs_lnum_t *face_ids = bz->elt_ids;
+
+  cs_boundary_type_t boundary_type
+    = boundaries->types[cs_boundary_id_by_zone_id(boundaries, def->z_id)];
+
+  const int wall_type = (boundary_type & CS_BOUNDARY_WALL) ? 5 : 1;
+
+  switch(def->type) {
+
+  case CS_XDEF_BY_VALUE:
+    {
+      const cs_real_t  *constant_val = (cs_real_t *)def->context;
+
+      for (int coo_id = 0; coo_id < def->dim; coo_id++) {
+
+        int        *_icodcl = icodcl + (var_id+coo_id)*n_b_faces;
+        cs_real_t  *_rcodcl = rcodcl + (var_id+coo_id)*n_b_faces;
+
+        if (face_ids == NULL) {
+#         pragma omp parallel for if (bz->n_elts > CS_THR_MIN)
+          for (cs_lnum_t i = 0; i < bz->n_elts; i++) {
+            _icodcl[i] = wall_type;
+            _rcodcl[i] = constant_val[coo_id];
+          }
+        }
+        else {
+#         pragma omp parallel for if (bz->n_elts > CS_THR_MIN)
+          for (cs_lnum_t i = 0; i < bz->n_elts; i++) {
+            const cs_lnum_t  elt_id = (face_ids == NULL) ? i : face_ids[i];
+            _icodcl[elt_id] = wall_type;
+            _rcodcl[elt_id] = constant_val[coo_id];
+          }
+        }
+
+      }
+    }
+    break;
+
+  default:
+    bft_error(__FILE__, __LINE__, 0,
+              _(" %s: Unhandled %s definition type."),
+              __func__, cs_xdef_type_get_name(def->type));
+    break;
+
+  }
 }
 
 /*============================================================================
@@ -789,7 +923,6 @@ cs_boundary_conditions_free(void)
 /*!
  * \brief Set convective oulet boundary condition for a scalar.
  *
- * Parameters:
  * \param[out]    coefa         explicit BC coefficient for gradients
  * \param[out]    cofaf         explicit BC coefficient for diffusive flux
  * \param[out]    coefb         implicit BC coefficient for gradients
@@ -816,6 +949,92 @@ cs_boundary_conditions_set_convective_outlet_scalar(cs_real_t *coefa ,
   /* Flux BCs */
   *cofaf = - hint * *coefa;
   *cofbf =   hint * (1.0 - *coefb);
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Update per variable boundary condition codes.
+ *
+ * \param[in]       nvar             number of variables requiring BC's
+ * \param[in]       itypfb           type of boundary for each face
+ * \param[in, out]  icodcl           boundary condition codes
+ * \param[in, out]  rcodcl           boundary condition values
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_boundary_conditions_compute(int         nvar,
+                               int        *itypfb,
+                               int        *icodcl,
+                               double     *rcodcl)
+{
+  /* Initialization */
+
+  static int var_id_key = -1;
+  if (var_id_key < 0)
+    var_id_key = cs_field_key_id("variable_id");
+  assert(var_id_key >= 0);
+
+  const cs_mesh_t *mesh = cs_glob_mesh;
+  const cs_boundary_t *boundaries = cs_glob_boundaries;
+  const cs_real_t t_eval = cs_glob_time_step->t_cur;
+
+  /* Loop on fields */
+
+  const int n_fields = cs_field_n_fields();
+
+  for (int f_id = 0; f_id < n_fields; f_id++) {
+    cs_field_t  *f = cs_field_by_id(f_id);
+    const cs_equation_param_t *eqp = NULL;
+    if (f->type & CS_FIELD_VARIABLE)
+      eqp = cs_field_get_equation_param_const(f);
+
+    if (eqp == NULL)
+      continue;
+
+    /* Only handle legacy discretization here */
+
+    if (eqp-> space_scheme != CS_SPACE_SCHEME_LEGACY)
+      continue;
+
+    /* Get associated variable id  */
+
+    int var_id = cs_field_get_key_int(f, var_id_key) - 1;
+    assert(var_id >= 0);
+
+    /* Loop on boundary conditions */
+
+    for (int bc_id = 0; bc_id < eqp->n_bc_defs; bc_id++) {
+      const cs_xdef_t *def = eqp->bc_defs[bc_id];
+      cs_param_bc_type_t bc_type = (cs_param_bc_type_t)(def->meta);
+      switch (bc_type) {
+
+      case CS_PARAM_BC_HMG_DIRICHLET:
+        _compute_dirichlet_hmg_bc(mesh,
+                                  boundaries,
+                                  eqp,
+                                  def,
+                                  var_id,
+                                  icodcl,
+                                  rcodcl);
+        break;
+
+      case CS_PARAM_BC_DIRICHLET:
+        _compute_dirichlet_bc(mesh,
+                              boundaries,
+                              eqp,
+                              def,
+                              var_id,
+                              t_eval,
+                              icodcl,
+                              rcodcl);
+        break;
+
+      default:
+        break;
+      }
+    }
+  }
 }
 
 /*----------------------------------------------------------------------------*/

src/base/cs_boundary_conditions.h
@@ -417,6 +417,23 @@ cs_boundary_conditions_set_dirichlet_vector_aniso(cs_real_3_t    a,
 }
 
 /*----------------------------------------------------------------------------*/
+/*!
+ * \brief Update per variable boundary condition codes.
+ *
+ * \param[in]       nvar             number of variables requiring BC's
+ * \param[in]       itypfb           type of boundary for each face
+ * \param[in, out]  icodcl           boundary condition codes
+ * \param[in, out]  rcodcl           boundary condition values
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_boundary_conditions_compute(int         nvar,
+                               int        *itypfb,
+                               int        *icodcl,
+                               double     *rcodcl);
+
+/*----------------------------------------------------------------------------*/
 
 END_C_DECLS
 

src/cdo/cs_xdef.c
@@ -64,6 +64,18 @@ BEGIN_C_DECLS
  * Global static variables
  *============================================================================*/
 
+static const char *_xdef_type_name[]
+  = {"CS_XDEF_BY_ANALYTIC_FUNCTION",
+     "CS_XDEF_BY_ARRAY",
+     "CS_XDEF_BY_DOF_FUNCTION",
+     "CS_XDEF_BY_FIELD",
+     "CS_XDEF_BY_FUNCTION",
+     "CS_XDEF_BY_QOV",
+     "CS_XDEF_BY_SUB_DEFINITIONS",
+     "CS_XDEF_BY_TIME_FUNCTION",
+     "CS_XDEF_BY_VALUE",
+     "out of range"};
+
 /*============================================================================
  * Private function prototypes
  *============================================================================*/
@@ -927,6 +939,25 @@ cs_xdef_log(const char          *prefix,
   cs_log_printf(CS_LOG_SETUP, "%s | Quadrature: %s\n",
                 _p, cs_quadrature_get_type_name(d->qtype));
 
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Retrieve a pointer to the cs_xdef_type's name string
+ *
+ * \param[in] xdef_type  type to query
+ *
+ * \return a pointer to mathing name string
+ */
+/*----------------------------------------------------------------------------*/
+
+const char *
+cs_xdef_type_get_name(cs_xdef_type_t  xdef_type)
+{
+  if (xdef_type < 0 || xdef_type >= CS_N_XDEF_TYPES)
+    xdef_type = CS_N_XDEF_TYPES;
+
+  return _xdef_type_name[xdef_type];
 }
 
 /*----------------------------------------------------------------------------*/

src/cdo/cs_xdef.h
@@ -641,6 +641,19 @@ cs_xdef_log(const char          *prefix,
             const cs_xdef_t     *d);
 
 /*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Retrieve a pointer to the cs_xdef_type's name string
+ *
+ * \param[in] xdef_type  type to query
+ *
+ * \return a pointer to mathing name string
+ */
+/*----------------------------------------------------------------------------*/
+
+const char *
+cs_xdef_type_get_name(cs_xdef_type_t  xdef_type);
+
+/*----------------------------------------------------------------------------*/
 
 END_C_DECLS
 

commit 248afeed273f0862cf04519ac34d16136b146417

Fix compressible inlet from XML, broken by commit f5356d22 (Nov 2 2020).

src/cdo/cs_equation_param.c
@@ -3183,10 +3183,10 @@ cs_equation_add_bc_by_analytic(cs_equation_param_t        *eqp,
   int  z_id = cs_get_bdy_zone_id(z_name);
 
   /* Add a new cs_xdef_t structure */
-  cs_xdef_analytic_context_t  ac = { .z_id = z_id,
-                                     .func = analytic,
-                                     .input = input,
-                                     .free_input = NULL };
+  cs_xdef_analytic_context_t  ac = {.z_id = z_id,
+                                    .func = analytic,
+                                    .input = input,
+                                    .free_input = NULL};
 
   cs_flag_t  meta_flag = (eqp-> space_scheme == CS_SPACE_SCHEME_LEGACY) ?
     (cs_flag_t)bc_type : cs_cdo_bc_get_flag(bc_type);
@@ -3204,6 +3204,90 @@ cs_equation_add_bc_by_analytic(cs_equation_param_t        *eqp,
   eqp->bc_defs[new_id] = d;
 
   return d;
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Return pointer to existing boundary condition definition structure
+ *         for the given equation param structure and zone.
+ *
+ * \param[in, out] eqp       pointer to a cs_equation_param_t structure
+ * \param[in]      z_name    name of the associated zone (if NULL or "" if
+ *                           all cells are considered)
+ *
+ * \return a pointer to the \ref cs_xdef_t structure if present, or NULL
+*/
+/*----------------------------------------------------------------------------*/
+
+cs_xdef_t *
+cs_equation_find_bc(cs_equation_param_t   *eqp,
+                    const char            *z_name)
+{
+  if (eqp == NULL)
+    return NULL;
+
+  int z_id = -2;
+
+  const cs_zone_t  *z = cs_boundary_zone_by_name_try(z_name);
+  if (z != NULL)
+    z_id = z->id;
+
+  /* Search for given BC. */
+  int j = -1;
+  for (int i = 0; i < eqp->n_bc_defs; i++) {
+    if (eqp->bc_defs[i]->z_id == z_id) {
+      return eqp->bc_defs[i];
+    }
+  }
+
+  return NULL;
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Remove boundary condition from the given equation param structure
+ *         for a given zone.
+ *
+ * If no matching boundary condition is found, the function returns
+ * silently.
+ *
+ * \param[in, out] eqp       pointer to a cs_equation_param_t structure
+ * \param[in]      z_name    name of the associated zone (if NULL or "" if
+ *                           all cells are considered)
+*/
+/*----------------------------------------------------------------------------*/
+
+void
+cs_equation_remove_bc(cs_equation_param_t   *eqp,
+                      const char            *z_name)
+{
+  if (eqp == NULL)
+    return;
+
+  int z_id = -2;
+
+  const cs_zone_t  *z = cs_boundary_zone_by_name_try(z_name);
+  if (z != NULL)
+    z_id = z->id;
+
+  /* Search for given BC. */
+  int j = -1;
+  for (int i = 0; i < eqp->n_bc_defs; i++) {
+    if (eqp->bc_defs[i]->z_id == z_id) {
+      j = i;
+      break;
+    }
+  }
+
+  /* Remove it if found */
+  if (j > -1) {
+    eqp->bc_defs[j] = cs_xdef_free(eqp->bc_defs[j]);
+    for (int i = j+1; i < eqp->n_bc_defs; i++) {
+      eqp->bc_defs[i-1] = eqp->bc_defs[i];
+    }
+    eqp->n_bc_defs -= 1;
+    BFT_REALLOC(eqp->bc_defs, eqp->n_bc_defs, cs_xdef_t *);
+  }
 }
 
 /*----------------------------------------------------------------------------*/

src/cdo/cs_equation_param.h
@@ -1593,6 +1593,41 @@ cs_equation_add_bc_by_analytic(cs_equation_param_t        *eqp,
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief  Return pointer to existing boundary condition definition structure
+ *         for the given equation param structure and zone.
+ *
+ * \param[in, out] eqp       pointer to a cs_equation_param_t structure
+ * \param[in]      z_name    name of the associated zone (if NULL or "" if
+ *                           all cells are considered)
+ *
+ * \return a pointer to the \ref cs_xdef_t structure if present, or NULL
+*/
+/*----------------------------------------------------------------------------*/
+
+cs_xdef_t *
+cs_equation_find_bc(cs_equation_param_t   *eqp,
+                    const char            *z_name);
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Remove boundary condition from the given equation param structure
+ *         for a given zone.
+ *
+ * If no matching boundary condition is found, the function returns
+ * silently.
+ *
+ * \param[in, out] eqp       pointer to a cs_equation_param_t structure
+ * \param[in]      z_name    name of the associated zone (if NULL or "" if
+ *                           all cells are considered)
+*/
+/*----------------------------------------------------------------------------*/
+
+void
+cs_equation_remove_bc(cs_equation_param_t   *eqp,
+                      const char            *z_name);
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief  Define and initialize a new structure to set a sliding boundary
  *         condition related to the given equation structure
  *         z_name corresponds to the name of a pre-existing cs_zone_t

src/gui/cs_gui_boundary_conditions.c
@@ -151,7 +151,6 @@ typedef struct {
   double        *prein;    /* inlet pressure (compressible model) */
   double        *rhoin;    /* inlet density  (compressible model) */
   double        *tempin;   /* inlet temperature (compressible model) */
-  double        *entin;    /* inlet total energy (compressible model) */
   double        *dh;       /* inlet hydraulic diameter */
   double        *xintur;   /* inlet turbulent intensity */
   int          **type_code;  /* type of boundary for each variable */
@@ -457,6 +456,8 @@ _boundary_scalar(cs_tree_node_t  *tn_bc,
                          0, 0, 0, 0, 0, 0, 0, 0, 0};
   assert(dim <= 9);
 
+  bool possibly_incomplete = false;
+
   /* FIXME: we should not need a loop over components, but
      directly use vector values; if we do not yet have
      multidimensional user variables in the GUI, we can hendle
@@ -484,6 +485,8 @@ _boundary_scalar(cs_tree_node_t  *tn_bc,
         if (v != NULL) {
           value[i] = *v;
         }
+        else
+          possibly_incomplete = true;
       }
       else if (! strcmp(choice, "neumann")) {
         /* Vector values per component for CDO,
@@ -549,11 +552,19 @@ _boundary_scalar(cs_tree_node_t  *tn_bc,
 
   if (choice != NULL) {
 
-    if (! strcmp(choice, "dirichlet"))
-      cs_equation_add_bc_by_value(eqp,
-                                  CS_PARAM_BC_DIRICHLET,
-                                  z_name,
-                                  value);
+    if (! strcmp(choice, "dirichlet")) {
+
+      /* Some specific models may have set value already, so
+         if the value here is the default, it should probably be
+         ignored (the XML/tree structure should be improved to
+         avoid this type of problem )*/
+      if (   possibly_incomplete == false
+          || cs_equation_find_bc(eqp, z_name) == NULL)
+        cs_equation_add_bc_by_value(eqp,
+                                    CS_PARAM_BC_DIRICHLET,
+                                    z_name,
+                                    value);
+    }
 
     else if (! strcmp(choice, "neumann"))
       cs_equation_add_bc_by_value(eqp,
@@ -699,12 +710,16 @@ static void
 _inlet_compressible(cs_tree_node_t  *tn_vp,
                     int              izone)
 {
+  const cs_zone_t *z = cs_boundary_zone_by_id(izone + 1);
+
   bool status;
 
   cs_tree_node_t *tn = cs_tree_get_node(tn_vp, "compressible_type");
   const char *choice = cs_gui_node_get_tag(tn, "choice");
 
   if (cs_gui_strcmp(choice, "imposed_inlet")) {
+
+    cs_real_t te_ind = 0;
 
     boundaries->itype[izone] = CS_ESICF;
 
@@ -730,7 +745,14 @@ _inlet_compressible(cs_tree_node_t  *tn_vp,
     status = false;
     cs_gui_node_get_status_bool(tn, &status);
     if (status)
-      cs_gui_node_get_real(tn, &boundaries->entin[izone]);
+      cs_gui_node_get_real(tn, &te);
+
+    cs_equation_param_t *eqp = _get_equation_param("total_energy");
+    cs_equation_remove_bc(eqp, z->name);
+    cs_equation_add_bc_by_value(eqp,
+                                CS_PARAM_BC_DIRICHLET,
+                                z->name,
+                                &te_in);
 
   }
   else if (cs_gui_strcmp(choice, "subsonic_inlet_PH")) {
@@ -739,8 +761,16 @@ _inlet_compressible(cs_tree_node_t  *tn_vp,
 
     cs_gui_node_get_child_real
       (tn_vp, "total_pressure", &boundaries->prein[izone]);
-    cs_gui_node_get_child_real
-      (tn_vp, "enthalpy", &boundaries->entin[izone]);
+
+    cs_real_t h_in = 0;
+    cs_gui_node_get_child_real(tn_vp, "enthalpy", &h_in);
+
+    cs_equation_param_t *eqp = _get_equation_param("total_energy");
+    cs_equation_remove_bc(eqp, z->name);
+    cs_equation_add_bc_by_value(eqp,
+                                CS_PARAM_BC_DIRICHLET,
+                                z->name,
+                                &h_in);
 
   }
 }
@@ -970,7 +1000,6 @@ _init_boundaries(void)
     BFT_MALLOC(boundaries->prein,   n_zones, double);
     BFT_MALLOC(boundaries->rhoin,   n_zones, double);
     BFT_MALLOC(boundaries->tempin,  n_zones, double);
-    BFT_MALLOC(boundaries->entin,   n_zones, double);
   }
   else if (cs_gui_strcmp(vars->model, "groundwater_model")) {
     BFT_MALLOC(boundaries->groundwat_e, n_zones, bool);
@@ -1050,7 +1079,6 @@ _init_boundaries(void)
       boundaries->prein[izone]     = 0;
       boundaries->rhoin[izone]     = 0;
       boundaries->tempin[izone]    = 0;
-      boundaries->entin[izone]     = 0;
     }
 
     else if (cs_gui_strcmp(vars->model, "groundwater_model")) {
@@ -1785,17 +1813,14 @@ void CS_PROCF (uiclim, UICLIM)(const int  *nozppm,
       else if (cs_gui_strcmp(vars->model, "compressible_model")) {
         const int var_key_id = cs_field_key_id("variable_id");
 
-        if (boundaries->itype[izone] == CS_ESICF ||
-            boundaries->itype[izone] == CS_EPHCF) {
+        if (  boundaries->itype[izone] == CS_ESICF
+            ||boundaries->itype[izone] == CS_EPHCF) {
           const cs_field_t  *fp = cs_field_by_name_try("pressure");
           int ivarp = cs_field_get_key_int(fp, var_key_id) -1;
-          const cs_field_t  *fe = cs_field_by_name_try("total_energy");
-          int ivare = cs_field_get_key_int(fe, var_key_id) -1;
 
           for (cs_lnum_t elt_id = 0; elt_id < bz->n_elts; elt_id++) {
             cs_lnum_t face_id = bz->elt_ids[elt_id];
             rcodcl[ivarp * n_b_faces + face_id] = boundaries->prein[izone];
-            rcodcl[ivare * n_b_faces + face_id] = boundaries->entin[izone];
           }
         }
 
@@ -2289,12 +2314,10 @@ void CS_PROCF (uiclim, UICLIM)(const int  *nozppm,
           bft_printf("-----premin=%g \n",boundaries->prein[zone_nbr-1]);
           bft_printf("-----rhoin=%g \n",boundaries->rhoin[zone_nbr-1]);
           bft_printf("-----tempin=%g \n",boundaries->tempin[zone_nbr-1]);
-          bft_printf("-----entin=%g \n",boundaries->entin[zone_nbr-1]);
         }
         if (boundaries->itype[izone] == CS_EPHCF) {
           bft_printf("-----subsonic_inlet_PH\n");
           bft_printf("-----prein=%g \n",boundaries->prein[zone_nbr-1]);
-          bft_printf("-----entin=%g \n",boundaries->entin[zone_nbr-1]);
         }
       }
       else {
@@ -2960,7 +2983,6 @@ cs_gui_boundary_conditions_free_memory(void)
       BFT_FREE(boundaries->prein);
       BFT_FREE(boundaries->rhoin);
       BFT_FREE(boundaries->tempin);
-      BFT_FREE(boundaries->entin);
     }
     if (cs_gui_strcmp(vars->model, "groundwater_model")) {
       BFT_FREE(boundaries->groundwat_e);