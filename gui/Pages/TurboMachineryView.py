code-saturne/code_saturne

commit ba7f6a150e83d9941788961a702143d082e8a104

GUI (major): remove PyQt4 API 1 support.
When using PyQt4, SIP API2 is forced. This allows removing the
to_qvariant compatibility function, and will simplify further cleanup.

gui/Pages/TurboMachineryView.py
@@ -46,7 +46,7 @@ from code_saturne.Base.QtWidgets import *
 
 from code_saturne.model.Common import GuiParam
 from code_saturne.Base.QtPage import ComboModel, RegExpValidator, DoubleValidator
-from code_saturne.Base.QtPage import to_qvariant, from_qvariant, to_text_string
+from code_saturne.Base.QtPage import to_text_string
 from code_saturne.Pages.TurboMachineryForm import Ui_TurboMachineryForm
 from code_saturne.model.TurboMachineryModel import TurboMachineryModel
 from code_saturne.Pages.FacesSelectionView import StandardItemModelFaces
@@ -85,7 +85,7 @@ class LineEditDelegateSelector(QItemDelegate):
 
     def setModelData(self, editor, model, index):
         value = editor.text()
-        model.setData(index, to_qvariant(value), Qt.DisplayRole)
+        model.setData(index, value, Qt.DisplayRole)
 
 
 #-------------------------------------------------------------------------------
@@ -117,7 +117,7 @@ class VelocityDelegate(QItemDelegate):
 
         if editor.validator().state == QValidator.Acceptable:
             value = from_qvariant(editor.text(), float)
-            model.setData(index, to_qvariant(value), Qt.DisplayRole)
+            model.setData(index, value, Qt.DisplayRole)
 
 #-------------------------------------------------------------------------------
 # Model class
@@ -145,20 +145,20 @@ class StandardItemModelRotor(QStandardItemModel):
 
     def data(self, index, role):
         if not index.isValid():
-            return to_qvariant()
+            return None
 
         if role == Qt.ToolTipRole:
-            return to_qvariant(self.tooltip[index.column()])
+            return self.tooltip[index.column()]
 
         if role == Qt.DisplayRole:
             data = self._data[index.row()][index.column()]
             if index.column() in (0, 1):
                 if data:
-                    return to_qvariant(data)
+                    return data
                 else:
-                    return to_qvariant()
+                    return None
 
-        return to_qvariant()
+        return None
 
 
     def flags(self, index):
@@ -169,8 +169,8 @@ class StandardItemModelRotor(QStandardItemModel):
 
     def headerData(self, section, orientation, role):
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
-            return to_qvariant(self.headers[section])
-        return to_qvariant()
+            return self.headers[section]
+        return None
 
 
     def setData(self, index, value, role):

bin/cs_gui.py
@@ -41,13 +41,15 @@ if not hasattr(sys, 'version_info') or sys.version_info <= (2, 4, 0, 'final'):
 # Third-party modules
 #-------------------------------------------------------------------------------
 
-# to use PyQt API 2
-#if sys.version_info[0] == 2:
-#    import sip
-#    sip.setapi('QString', 2)
-#    sip.setapi('QVariant', 2)
-#    sip.setapi('QTime', 2)
-#    sip.setapi('QUrl', 2)
+# Force PyQt API 2 for PyQt4
+
+try:
+    import sip
+    for api in ('QDate', 'QDateTime', 'QString', 'QTextStream',
+                'QTime', 'QUrl', 'QVariant'):
+        sip.setapi(api, 2)
+except Exception:
+    print("Warning: error forcing SIP to force v2 API.")
 
 try:
     from code_saturne.Base.QtCore    import *
@@ -64,8 +66,8 @@ if list(map(int, QT_VERSION_STR.split( "."))) < [4, 3, 0]:
                      "(found %s)." % QT_VERSION_STR)
 
 
-if list(map(int, PYQT_VERSION_STR.split("."))) < [4, 3, 0]:
-    raise SystemExit("Graphical user interface requires PyQt 4.3 or later "\
+if list(map(int, PYQT_VERSION_STR.split("."))) < [4, 5, 0]:
+    raise SystemExit("Graphical user interface requires PyQt 4.5 or later "\
                      "(found %s)." % PYQT_VERSION_STR)
 
 #-------------------------------------------------------------------------------

gui/Base/BrowserView.py
@@ -48,7 +48,7 @@ from code_saturne.Base.QtWidgets import *
 from code_saturne.Base.BrowserForm import Ui_BrowserForm
 from code_saturne.model.Common import GuiParam
 from code_saturne.Base.Toolbox import displaySelectedPage
-from code_saturne.Base.QtPage import to_qvariant, from_qvariant, to_text_string
+from code_saturne.Base.QtPage import from_qvariant, to_text_string
 
 import resource_base_rc
 
@@ -125,7 +125,7 @@ class TreeModel(QAbstractItemModel):
         QAbstractItemModel.__init__(self, parent)
 
         rootData = []
-        rootData.append(to_qvariant("Pages"))
+        rootData.append("Pages")
 
         self.rootItem = TreeItem(rootData, "folder")
         self.populateModel(data.split("\n"), self.rootItem)
@@ -154,7 +154,7 @@ class TreeModel(QAbstractItemModel):
         @return: C{QVariant}
         """
         if not index.isValid():
-            return to_qvariant()
+            return None
 
         item = index.internalPointer()
         column = index.column()
@@ -162,7 +162,7 @@ class TreeModel(QAbstractItemModel):
         if role == Qt.DisplayRole:
             # return text for columns
             if column == 0:
-                return to_qvariant(item.itemData[column])
+                return item.itemData[column]
 
         elif role == Qt.DecorationRole:
             # return icon for first column
@@ -228,9 +228,9 @@ class TreeModel(QAbstractItemModel):
                     elif item.itemType == "file-new":
                         icon = style.standardIcon(QStyle.SP_ToolBarHorizontalExtensionButton)
 
-                return to_qvariant(icon)
+                return icon
 
-        return to_qvariant()
+        return None
 
 
     def flags(self, index):
@@ -255,7 +255,7 @@ class TreeModel(QAbstractItemModel):
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
             return self.rootItem.data(section)
 
-        return to_qvariant()
+        return None
 
 
     def index(self, row, column, parent):
@@ -358,7 +358,7 @@ class TreeModel(QAbstractItemModel):
         """
         """
         info = []
-        search_item = from_qvariant(to_qvariant(data), to_text_string)
+        search_item = from_qvariant(data, to_text_string)
 
         start = self.index(0, 0, QModelIndex())
         indexList = self.match(start, role, search_item, -1, Qt.MatchExactly)
@@ -442,7 +442,7 @@ class BrowserView(QWidget, Ui_BrowserForm):
         self.setupUi(self)
 
         tree = self._browser()
-        self.model = TreeModel(from_qvariant(to_qvariant(tree), to_text_string))
+        self.model = TreeModel(from_qvariant(tree, to_text_string))
 
         self.treeView.setModel(self.model)
         self.treeView.header().hide()

gui/Base/QtPage.py
@@ -111,66 +111,38 @@ def to_text_string(obj, encoding=None):
 
 #==============================================================================
 # QVariant conversion utilities
+#
+# Note: to_qvariant was removed recently; from_qvariant may be removed in
+#       many_places and the "to_text_string" variants could be replaced
+#       by "to_str"; where the value is already known to be of the correct
+#       type, or None handled in the associated code,
+#       this could even be ignored
 #==============================================================================
-PYQT_API_1 = False
+
 import collections
 
 if os.environ.get('QT_API', 'pyqt') == 'pyqt':
-    import sip
-    if QT_API == "PYQT4":
-        try:
-            PYQT_API_1 = sip.getapi('QVariant') == 1
-        except AttributeError:
-            PYQT_API_1 = True
-
-    def to_qvariant(pyobj=None):
-        """Convert Python object to QVariant
-        This is a transitional function from PyQt API #1 (QVariant exist)
-        to PyQt API #2 and Pyside (QVariant does not exist)"""
-        if PYQT_API_1:
-            from code_saturne.Base.QtCore import QVariant
-            return QVariant(pyobj)
-        else:
-            return pyobj
 
     def from_qvariant(qobj=None, convfunc=None):
         """Convert QVariant object to Python object
         This is a transitional function from PyQt API #1 (QVariant exists)
         to PyQt API #2 and Pyside (QVariant does not exist)"""
-        if PYQT_API_1:
-            assert isinstance(convfunc, collections.Callable)
+        if (qobj != None):
             if convfunc in TEXT_TYPES or convfunc is to_text_string:
-                return convfunc(qobj.toString())
-            elif convfunc is bool:
-                return qobj.toBool()
+                return str(qobj)
             elif convfunc is int:
-                return qobj.toInt()[0]
+                return int(qobj)
             elif convfunc is float:
-                return qobj.toDouble()[0]
-            else:
-                return convfunc(qobj)
-        else:
-            if (qobj != None):
-                if convfunc in TEXT_TYPES or convfunc is to_text_string:
-                    return str(qobj)
-                elif convfunc is int:
-                    return int(qobj)
-                elif convfunc is float:
-                    try:
-                        return float(qobj)
-                    except Exception:
-                        return locale.atof(qobj)
-                else:
-                    return qobj
+                try:
+                    return float(qobj)
+                except Exception:
+                    return locale.atof(qobj)
             else:
                 return qobj
+        else:
+            return qobj
 
 else:
-    def to_qvariant(obj=None):
-        """Convert Python object to QVariant
-        This is a transitional function from PyQt API#1 (QVariant exist)
-        to PyQt API#2 and Pyside (QVariant does not exist)"""
-        return obj
 
     def from_qvariant(qobj=None, pytype=None):
         """Convert QVariant object to Python object
@@ -182,6 +154,12 @@ def qbytearray_to_str(qba):
     """Convert QByteArray object to str in a way compatible with Python 2/3"""
     return str(bytes(qba.toHex().data()).decode())
 
+
+def to_str(s):
+    """Convert argument to string, using an empty string for None"""
+    if s is None:
+        return ''
+    return str(s)
 
 #==============================================================================
 # Wrappers around QFileDialog static methods
@@ -668,10 +646,7 @@ class IntValidator(QIntValidator):
 
         self.state = state
 
-        if PYQT_API_1:
-            return (state, pos)
-        else:
-            return (state, stri, pos)
+        return (state, stri, pos)
 
 
     def tr(self, text):
@@ -798,10 +773,7 @@ class DoubleValidator(QDoubleValidator):
 
         self.state = state
 
-        if PYQT_API_1:
-            return (state, pos)
-        else:
-            return (state, stri, pos)
+        return (state, stri, pos)
 
 
     def tr(self, text):
@@ -850,10 +822,7 @@ class RegExpValidator(QRegExpValidator):
 
         self.state = state
 
-        if PYQT_API_1:
-            return (state, pos)
-        else:
-            return (state, stri, pos)
+        return (state, stri, pos)
 
 
     def tr(self, text):

gui/Pages/BalanceView.py
@@ -46,7 +46,7 @@ from code_saturne.Base.QtWidgets import *
 
 from code_saturne.model.Common import GuiParam
 from code_saturne.Base.QtPage import RegExpValidator, IntValidator
-from code_saturne.Base.QtPage import to_qvariant, from_qvariant, to_text_string
+from code_saturne.Base.QtPage import from_qvariant, to_text_string
 from code_saturne.Pages.BalanceForm import Ui_BalanceForm
 from code_saturne.Pages.FacesSelectionView import StandardItemModelFaces
 
@@ -84,7 +84,7 @@ class LineEditDelegateSelector(QItemDelegate):
 
     def setModelData(self, editor, model, index):
         value = editor.text()
-        model.setData(index, to_qvariant(value), Qt.DisplayRole)
+        model.setData(index, value, Qt.DisplayRole)
 
 
 #-------------------------------------------------------------------------------
@@ -116,7 +116,7 @@ class LineEditDelegateIndex(QItemDelegate):
     def setModelData(self, editor, model, index):
         if editor.validator().state == QValidator.Acceptable:
             value = from_qvariant(editor.text(), int)
-            model.setData(index, to_qvariant(value), Qt.DisplayRole)
+            model.setData(index, value, Qt.DisplayRole)
 
 
 #-------------------------------------------------------------------------------
@@ -145,18 +145,18 @@ class StandardItemModelPressureDrop(QStandardItemModel):
 
     def data(self, index, role):
         if not index.isValid():
-            return to_qvariant()
+            return None
 
         if role == Qt.ToolTipRole:
-            return to_qvariant(self.tooltip[index.column()])
+            return self.tooltip[index.column()]
 
         if role == Qt.DisplayRole:
             row = index.row()
             col = index.column()
             if index.column() in (0, 1):
-                return to_qvariant(self._data[row][col])
+                return self._data[row][col]
 
-        return to_qvariant()
+        return None
 
 
     def flags(self, index):
@@ -170,8 +170,8 @@ class StandardItemModelPressureDrop(QStandardItemModel):
 
     def headerData(self, section, orientation, role):
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
-            return to_qvariant(self.headers[section])
-        return to_qvariant()
+            return self.headers[section]
+        return None
 
 
     def setData(self, index, value, role):
@@ -245,19 +245,19 @@ class StandardItemModelScalarBalance(QStandardItemModel):
 
     def data(self, index, role):
         if not index.isValid():
-            return to_qvariant()
+            return None
 
         if role == Qt.ToolTipRole:
-            return to_qvariant(self.tooltip[index.column()])
+            return self.tooltip[index.column()]
 
         if role == Qt.DisplayRole:
             row = index.row()
             col = index.column()
 
             if index.column() in (0, 1, 2):
-                return to_qvariant(self._data[row][col])
+                return self._data[row][col]
 
-        return to_qvariant()
+        return None
 
 
     def flags(self, index):
@@ -271,8 +271,8 @@ class StandardItemModelScalarBalance(QStandardItemModel):
 
     def headerData(self, section, orientation, role):
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
-            return to_qvariant(self.headers[section])
-        return to_qvariant()
+            return self.headers[section]
+        return None
 
 
     def setData(self, index, value, role):

gui/Pages/BalanceViewNeptune.py
@@ -46,7 +46,7 @@ from code_saturne.Base.QtWidgets import *
 
 from code_saturne.model.Common import GuiParam
 from code_saturne.Base.QtPage import RegExpValidator, IntValidator
-from code_saturne.Base.QtPage import to_qvariant, from_qvariant, to_text_string
+from code_saturne.Base.QtPage import from_qvariant, to_text_string
 from code_saturne.Pages.BalanceForm import Ui_BalanceForm
 from code_saturne.model.BalanceModelNeptune import BalanceModelNeptune
 from code_saturne.Pages.FacesSelectionView import StandardItemModelFaces
@@ -84,7 +84,7 @@ class LineEditDelegateSelector(QItemDelegate):
 
     def setModelData(self, editor, model, index):
         value = editor.text()
-        model.setData(index, to_qvariant(value), Qt.DisplayRole)
+        model.setData(index, value, Qt.DisplayRole)
 
 
 #-------------------------------------------------------------------------------
@@ -115,7 +115,7 @@ class LineEditDelegateIndex(QItemDelegate):
     def setModelData(self, editor, model, index):
         if editor.validator().state == QValidator.Acceptable:
             value = from_qvariant(editor.text(), int)
-            model.setData(index, to_qvariant(value), Qt.DisplayRole)
+            model.setData(index, value, Qt.DisplayRole)
 
 
 #-------------------------------------------------------------------------------
@@ -144,18 +144,18 @@ class StandardItemModelPressureDrop(QStandardItemModel):
 
     def data(self, index, role):
         if not index.isValid():
-            return to_qvariant()
+            return None
 
         if role == Qt.ToolTipRole:
-            return to_qvariant(self.tooltip[index.column()])
+            return self.tooltip[index.column()]
 
         if role == Qt.DisplayRole:
             row = index.row()
             col = index.column()
             if index.column() in (0, 1):
-                return to_qvariant(self._data[row][col])
+                return self._data[row][col]
 
-        return to_qvariant()
+        return None
 
 
     def flags(self, index):
@@ -169,8 +169,8 @@ class StandardItemModelPressureDrop(QStandardItemModel):
 
     def headerData(self, section, orientation, role):
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
-            return to_qvariant(self.headers[section])
-        return to_qvariant()
+            return self.headers[section]
+        return None
 
 
     def setData(self, index, value, role):
@@ -244,19 +244,19 @@ class StandardItemModelScalarBalance(QStandardItemModel):
 
     def data(self, index, role):
         if not index.isValid():
-            return to_qvariant()
+            return None
 
         if role == Qt.ToolTipRole:
-            return to_qvariant(self.tooltip[index.column()])
+            return self.tooltip[index.column()]
 
         if role == Qt.DisplayRole:
             row = index.row()
             col = index.column()
 
             if index.column() in (0, 1, 2):
-                return to_qvariant(self._data[row][col])
+                return self._data[row][col]
 
-        return to_qvariant()
+        return None
 
 
     def flags(self, index):
@@ -270,8 +270,8 @@ class StandardItemModelScalarBalance(QStandardItemModel):
 
     def headerData(self, section, orientation, role):
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
-            return to_qvariant(self.headers[section])
-        return to_qvariant()
+            return self.headers[section]
+        return None
 
 
     def setData(self, index, value, role):

gui/Pages/BatchRunningView.py
@@ -69,7 +69,7 @@ from code_saturne.Pages.BatchRunningStopByIterationDialogForm import Ui_BatchRun
 
 from code_saturne.model.Common import GuiParam
 from code_saturne.Base.QtPage import ComboModel, IntValidator, RegExpValidator
-from code_saturne.Base.QtPage import to_qvariant, from_qvariant
+from code_saturne.Base.QtPage import from_qvariant
 from code_saturne.Base.CommandMgrDialogView import CommandMgrDialogView
 from code_saturne.model.BatchRunningModel import BatchRunningModel
 from code_saturne.model.ScriptRunningModel import ScriptRunningModel
@@ -414,7 +414,7 @@ class BatchRunningView(QWidget, Ui_BatchRunningForm):
                 if not build_type_label:
                     build_type_label = "[default]"
                 self.comboBoxBuildType.addItem(self.tr(build_type_label),
-                                               to_qvariant(build_type_label))
+                                               build_type_label)
             self.comboBoxBuildType.setCurrentIndex(compute_build_id)
 
         self.class_list = None
@@ -1008,10 +1008,10 @@ class BatchRunningView(QWidget, Ui_BatchRunningForm):
                 self.class_list = self.jmdl.batch.get_class_list()
                 if len(self.class_list) > 0:
                     for c in self.class_list:
-                        self.comboBoxClass.addItem(self.tr(c), to_qvariant(c))
+                        self.comboBoxClass.addItem(self.tr(c), c)
                 else:
                     c = self.job_class
-                    self.comboBoxClass.addItem(self.tr(c), to_qvariant(c))
+                    self.comboBoxClass.addItem(self.tr(c), c)
 
             # All passes
             try:

gui/Pages/BoundaryConditionsCoalInletView.py
@@ -50,7 +50,7 @@ from code_saturne.Base.QtWidgets import *
 
 from code_saturne.model.Common import GuiParam
 from code_saturne.Base.QtPage import DoubleValidator, ComboModel
-from code_saturne.Base.QtPage import to_qvariant, from_qvariant, to_text_string
+from code_saturne.Base.QtPage import from_qvariant, to_text_string
 
 from code_saturne.Pages.BoundaryConditionsCoalInletForm import Ui_BoundaryConditionsCoalInletForm
 import code_saturne.model.CoalCombustionModel as CoalCombustion
@@ -91,7 +91,7 @@ class ValueDelegate(QItemDelegate):
     def setModelData(self, editor, model, index):
         if editor.validator().state == QValidator.Acceptable:
             value = from_qvariant(editor.text(), float)
-            model.setData(index, to_qvariant(value), Qt.DisplayRole)
+            model.setData(index, value, Qt.DisplayRole)
 
 #-------------------------------------------------------------------------------
 # StandarItemModel class to display Coals in a QTableView
@@ -114,10 +114,10 @@ class StandardItemModelCoal(QStandardItemModel):
 
     def data(self, index, role):
         if not index.isValid():
-            return to_qvariant()
+            return None
         if role == Qt.DisplayRole:
-            return to_qvariant(self.dataCoal[index.row()][index.column()])
-        return to_qvariant()
+            return self.dataCoal[index.row()][index.column()]
+        return None
 
 
     def flags(self, index):
@@ -131,8 +131,8 @@ class StandardItemModelCoal(QStandardItemModel):
 
     def headerData(self, section, orientation, role):
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
-            return to_qvariant(self.headers[section])
-        return to_qvariant()
+            return self.headers[section]
+        return None
 
 
     def setData(self, index, value, role):
@@ -193,16 +193,16 @@ class StandardItemModelCoalMass(QStandardItemModel):
 
     def data(self, index, role):
         if not index.isValid():
-            return to_qvariant()
+            return None
         if role == Qt.DisplayRole:
             classe = index.row()
             coal   = index.column()
             if classe < self.coalClassesNumber[coal]:
                 try:
-                    return to_qvariant(self.ratio[coal][classe])
+                    return self.ratio[coal][classe]
                 except:
                     log.debug("ERROR no data for self.ratio[%i][%i] "%(coal, classe))
-        return to_qvariant()
+        return None
 
 
     def flags(self, index):
@@ -216,10 +216,10 @@ class StandardItemModelCoalMass(QStandardItemModel):
 
     def headerData(self, section, orientation, role):
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
-            return to_qvariant("Coal" + " " + str(section+1))
+            return "Coal" + " " + str(section+1)
         if orientation == Qt.Vertical and role == Qt.DisplayRole:
-            return to_qvariant("Class" + " " + str(section+1))
-        return to_qvariant()
+            return "Class" + " " + str(section+1)
+        return None
 
 
     def setData(self, index, value, role):

commit 132104c4ffc36d3c2ab6c937b9ed81ca1b64d942

GUI: fix crash in turbomachinery rotor selection, broken by commit ba7f6a15 (March 13 2019).

gui/Pages/TurboMachineryView.py
@@ -46,7 +46,7 @@ from code_saturne.Base.QtWidgets import *
 
 from code_saturne.model.Common import GuiParam
 from code_saturne.Base.QtPage import ComboModel, RegExpValidator, DoubleValidator
-from code_saturne.Base.QtPage import to_text_string
+from code_saturne.Base.QtPage import from_qvariant, to_text_string
 from code_saturne.Pages.TurboMachineryForm import Ui_TurboMachineryForm
 from code_saturne.model.TurboMachineryModel import TurboMachineryModel
 from code_saturne.Pages.FacesSelectionView import StandardItemModelFaces