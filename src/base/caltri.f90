code-saturne/code_saturne

commit dabf0215420852a6c159d125d4fc419736b1408e

[Atmo] major commit to imrove compatibility of atmospheric module and standard user setting.
Fortran routine usatin1 has been removed and options can be set in
usppmo/cs_user_model and usipsu/cs_user_parameters.
The logic "Default values -> GUI -> Fortran routines -> C routines" is
now ok.
Fortran "ficmet" global variable has been moved to C (in cs_atmo_option
struct, meteo_file_name).

src/base/caltri.f90
@@ -432,19 +432,7 @@ call init_aux_arrays(ncelet, nfabor)
 call turbomachinery_init
 
 if (ippmod(iatmos).ge.0) then
-
-  call init_meteo
-
-  if (imbrication_flag) then
-    call activate_imbrication
-  endif
-
-  call cs_at_data_assim_build_ops
-
-  if (ifilechemistry.ge.1) then
-    call init_chemistry
-  endif
-
+  call init_atmo_autom(nfabor)
 endif
 
 if (ippmod(icompf).ge.0) then

src/atmo/Makefile.am
@@ -72,7 +72,6 @@ cs_intprf.h
 
 noinst_LTLIBRARIES = libcsatmo.la
 libcsatmo_la_SOURCES = \
-atini0.f90 \
 atini1.f90 \
 atiniv.f90 \
 atleca.f90 \

src/atmo/atincl.f90
@@ -177,6 +177,7 @@ integer(c_int), pointer, save :: iatmst
 !> - 1: the user can directly impose the exact meteo velocity
 !> by declaring the 'meteo_velocity' field
 !> Useful for iatmst = 1
+!> Note: deprecated, imeteo=2 can be used instead.
 integer, save :: theo_interp
 
 ! 2.1 Constant specific to the physics (defined in atini1.f90)
@@ -384,6 +385,19 @@ integer, save :: kopint
 
     !---------------------------------------------------------------------------
 
+    ! Interface to C function returning a meteo file name
+
+    subroutine cs_f_atmo_get_meteo_file_name(f_name_max, f_name, f_name_len)  &
+      bind(C, name='cs_f_atmo_get_meteo_file_name')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      integer(c_int), value       :: f_name_max
+      type(c_ptr), intent(out)    :: f_name
+      integer(c_int), intent(out) :: f_name_len
+    end subroutine cs_f_atmo_get_meteo_file_name
+
+    !---------------------------------------------------------------------------
+
     !> \brief Return pointers to atmo includes
 
     subroutine cs_f_atmo_get_pointers(ps,                               &
@@ -549,6 +563,44 @@ integer, save :: kopint
 
 contains
 
+    !=============================================================================
+
+    !> \brief Return meteo file name
+
+    !> \param[out]  name   meteo file name
+
+    subroutine atmo_get_meteo_file_name(name)
+
+      use, intrinsic :: iso_c_binding
+      implicit none
+
+      ! Arguments
+
+      character(len=*), intent(out) :: name
+
+      ! Local variables
+
+      integer :: i
+      integer(c_int) :: name_max, c_name_len
+      type(c_ptr) :: c_name_p
+      character(kind=c_char, len=1), dimension(:), pointer :: c_name
+
+      name_max = len(name)
+
+      call cs_f_atmo_get_meteo_file_name(name_max, c_name_p, c_name_len)
+      call c_f_pointer(c_name_p, c_name, [c_name_len])
+
+      do i = 1, c_name_len
+        name(i:i) = c_name(i)
+      enddo
+      do i = c_name_len + 1, name_max
+        name(i:i) = ' '
+      enddo
+
+      return
+
+    end subroutine atmo_get_meteo_file_name
+
   !=============================================================================
 
   !> \brief Map fortran to C variables
@@ -636,7 +688,7 @@ use atsoil
 implicit none
 
 ! Local variables
-integer :: imode, ifac, n_level
+integer :: imode, n_level, n_times, n_level_t
 
 type(c_ptr) :: c_z_temp_met, c_time_met
 type(c_ptr) :: c_frac_neb, c_diag_neb
@@ -667,28 +719,23 @@ if (imeteo.gt.0) then
 
   ! NB : only ztmet,ttmet,qvmet,ncmet are extended to 11000m if iatr1=1
   !           rmet,tpmet,phmet
-  allocate(zdmet(nbmetd))
+  n_level = max(1, nbmetd)
+  n_times = max(1, nbmetm)
+  n_level_t = max(1, nbmaxt)
+  allocate(zdmet(n_level))
 
   if (iatmst.ge.1) then
-    n_level = max(1, nbmetd)
     allocate(dpdt_met(n_level))
     allocate(mom(3, n_level))
     allocate(mom_met(3, n_level))
   endif
 
-  allocate(umet(nbmetd,nbmetm), vmet(nbmetd,nbmetm), wmet(nbmetd,nbmetm))
-  allocate(ekmet(nbmetd,nbmetm), epmet(nbmetd,nbmetm))
-  allocate(ttmet(nbmaxt,nbmetm), qvmet(nbmaxt,nbmetm), ncmet(nbmaxt,nbmetm))
-  allocate(pmer(nbmetm))
-  allocate(xmet(nbmetm), ymet(nbmetm))
-  allocate(rmet(nbmaxt,nbmetm), tpmet(nbmaxt,nbmetm))
-
-  ! Allocate and initialize auto inlet/outlet flag
-
-  allocate(iautom(nfabor))
-  do ifac = 1, nfabor
-    iautom(ifac) = 0
-  enddo
+  allocate(umet(n_level,n_times), vmet(n_level,n_times), wmet(n_level,n_times))
+  allocate(ekmet(n_level,n_times), epmet(n_level,n_times))
+  allocate(ttmet(n_level_t,n_times), qvmet(n_level_t,n_times), ncmet(n_level_t,n_times))
+  allocate(pmer(n_times))
+  allocate(xmet(n_times), ymet(n_times))
+  allocate(rmet(n_level_t,n_times), tpmet(n_level_t,n_times))
 
   ! Allocate additional arrays for 1D radiative model
 
@@ -724,6 +771,33 @@ if (imeteo.gt.0) then
 endif
 
 end subroutine init_meteo
+
+!===============================================================================
+
+!> \brief Initialisation of meteo data
+subroutine init_atmo_autom(nfabor)
+
+use cs_c_bindings
+
+implicit none
+
+integer nfabor
+
+! Local variables
+integer :: ifac
+
+! Allocate additional arrays for Water Microphysics
+if (imeteo.gt.0) then
+
+  ! Allocate and initialize auto inlet/outlet flag
+  allocate(iautom(nfabor))
+  do ifac = 1, nfabor
+    iautom(ifac) = 0
+  enddo
+
+endif
+
+end subroutine init_atmo_autom
 
 !=============================================================================
 

src/atmo/atini0.f90
@@ -1,193 +1,0 @@
-!-------------------------------------------------------------------------------
-
-! This file is part of Code_Saturne, a general-purpose CFD tool.
-!
-! Copyright (C) 1998-2020 EDF S.A.
-!
-! This program is free software; you can redistribute it and/or modify it under
-! the terms of the GNU General Public License as published by the Free Software
-! Foundation; either version 2 of the License, or (at your option) any later
-! version.
-!
-! This program is distributed in the hope that it will be useful, but WITHOUT
-! ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-! FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-! details.
-!
-! You should have received a copy of the GNU General Public License along with
-! this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
-! Street, Fifth Floor, Boston, MA 02110-1301, USA.
-
-!-------------------------------------------------------------------------------
-!> \file atini0.f90
-!> \brief Initialisation of variable options for Code_Saturne atmospheric
-!>     module in addition to what is done previously in iniini function
-subroutine atini0
-
-!===============================================================================
-! Module files
-!===============================================================================
-use paramx
-use dimens
-use numvar
-use optcal
-use cstphy
-use entsor
-use cstnum
-use ppppar
-use ppthch
-use ppincl
-use atincl
-use atsoil
-use atchem
-use atimbr
-use field
-use sshaerosol
-
-!===============================================================================
-
-implicit none
-
-! Local variables
-
-!===============================================================================
-
-!===============================================================================
-! 0. General info
-!===============================================================================
-
-!--> constants used in the atmospheric physics module
-!    (see definition in atincl.f90):
-
-p0 = 1.01325d5
-t0 = 288.15d0
-
-!===============================================================================
-! 1. Global variables for atmospheric flows
-!===============================================================================
-
-! Space and time reference of the run:
-! ------------------------------------
-
-! Option for the meteo profile computation
-!ihpm   --> flag to compute the hydrostastic pressure by Laplace integration
-!           in the meteo profiles
-!       = 0 : bottom to top Laplace integration, based on P(sea level) (default)
-!       = 1 : top to bottom Laplace integration based on P computed for
-!            the standard atmosphere at z(nbmaxt)
-ihpm = 0
-
-! 1d radiative transfer model:
-! ----------------------------
-
-! iatra1 -->  flag for the use of the 1d atmo radiative model
-! nfatr1 --> 1d radiative model pass frequency
-! ivert  --> flag for the definition of the vertical grid
-!            -1: no vertical grid
-!            0 : automatic definition !!!!!!!!!MM 2do
-!            1 : definition by the user in usatdv
-! iqv0   --> flag for humidity above the domain (0 : no humidity; 1 : decreasing)
-
-iatra1 = 0
-nfatr1 = 1
-ivert = 1  ! if iatra1=1 alors ivert=1
-iqv0 = 0
-
-! flag to use the soil model (if humid atmosphere)
-iatsoil = 0
-! Initial values for soil variables
-tsini  = 20.d0   !TSINI  : Surface ground temperature
-tprini = 20.d0   !TPRINI : Deep ground temperature
-qvsini = 0.d0    !QVSINI : Ground humidity
-tmer   = 20.d0   !Sea temperature
-
-!  -------------------------------------------------------------------------------
-!  Microphysics parameterization options
-!  -------------------------------------------------------------------------------
-!  --> Option for subgrid models
-!  modsub = 0 : the simplest parameterization (for numerical verifications)
-!  modsub = 1 : Bechtold et al. 1995 (Luc Musson-Genon)
-!  modsub = 2 : Bouzereau et al. 2004
-!  modsub = 3 : Cuijpers and Duynkerke 1993, Deardorff 1976, Sommeria
-!               and Deardorff 1977
-modsub = 0
-
-!  --> Option for liquid water content distribution models
-!  moddis = 1 : all or nothing
-!  moddis = 2 : Gaussian distribution
-moddis = 1
-
-! logaritmic standard deviation of the log-normal law of the droplet spectrum
-! adimensional
-sigc = 0.53 ! other referenced values are 0.28, 0.15
-
-!  -----------------------------------------------------------------------------
-!  Atmospheric imbrication on large scale meteo (atimbr module)
-!  -----------------------------------------------------------------------------
-
-! activation flag
-imbrication_flag = .false.
-imbrication_verbose = .false.
-
-! ------------------------------------------------------------------------------
-! flags for activating the cressman interpolation for the boundary conditions
-! ------------------------------------------------------------------------------
-cressman_u = .false.
-cressman_v = .false.
-cressman_tke = .false.
-cressman_eps = .false.
-cressman_theta = .false.
-cressman_qw = .false.
-cressman_nc = .false.
-
-! --------------------------------------------------------------
-! numerical parameters for the cressman interpolation formulas
-! --------------------------------------------------------------
-horizontal_influence_radius = 8500.d0
-vertical_influence_radius = 100.d0
-
-! key id for optimal interpolation
-
-call field_get_key_id("opt_interp_id", kopint)
-
-theo_interp = 0
-
-! -------------------------------------
-! flags for 1d radiative transfer model
-! -------------------------------------
-
-! no computation / storage of downward and upward infrared radiative fluxes
-irdu = 1
-! no computation / storage of downward and upward solar radiative fluxes
-soldu = 1
-
-!===============================================================================
-! 2. ON DONNE LA MAIN A L'UTLISATEUR
-!===============================================================================
-
-! initmeteo --> use meteo profile for variables initialization
-!               (0: not used; 1: used )
-! NB : should eventually be set by interface
-
-initmeteo = 1
-
-call usati1
-
-! Atmospheric gaseous chemistry
-! Do not change this order
-if (iaerosol.eq.CS_ATMO_AEROSOL_SSH) ichemistry = 4
-! if a chemical scheme is solved, a concentration profiles
-! file must be used
-if (ichemistry.ge.1) ifilechemistry = ichemistry
-if (nogaseouschemistry .and. iaerosol.eq.CS_ATMO_AEROSOL_OFF) ichemistry = 0
-
-!--------
-! Formats
-!--------
-
-!----
-! End
-!----
-
-return
-end subroutine atini0

src/atmo/atini1.f90
@@ -166,6 +166,27 @@ endif
 
 if (itytur.eq.3) irijnu = 1
 
+!===============================================================================
+! 7. Some initialization for meteo...
+!===============================================================================
+
+if (ippmod(iatmos).ge.0) then
+
+  call init_meteo
+
+  if (imbrication_flag) then
+    call activate_imbrication
+  endif
+
+  call cs_at_data_assim_build_ops
+
+  if (ifilechemistry.ge.1) then
+    call init_chemistry
+  endif
+
+endif
+
+
 !--------
 ! FORMATS
 !--------

src/atmo/atlecc.f90
@@ -150,7 +150,7 @@ else
     call csexit (1)
   endif
 
-  ! --> if the date and time are not completed in usati1.f90 nor and if no meteo
+  ! --> if the date and time are not completed in usppmo/cs_user_model if no meteo
   ! --> file is given,
   ! --> the date and time of the first concentration profile are taken as the
   !     starting time of the simulation

src/atmo/atlecm.f90
@@ -59,6 +59,8 @@ integer           imode
 
 ! Local variables
 
+character(len=80) :: ficmet
+
 integer itp, ii, ios, k
 integer year, quant,hour,minute, month, day
 integer ih2o
@@ -88,6 +90,8 @@ endif
 CSAUTE = '/'
 
 ! --> Opens the meteo file
+call atmo_get_meteo_file_name(ficmet)
+
 open (unit=impmet, file=ficmet,                                  &
      status='old', form='formatted', access='sequential',       &
      iostat=ios, err=99)
@@ -153,7 +157,7 @@ else
     call csexit (1)
   endif
 
-  ! --> if the date and time are not completed in usati1.f90,
+  ! --> if the date and time are not completed in usppmo / cs_user_model
   !     the date and time of the first meteo profile are taken as the
   !     starting time of the simulation
 

src/atmo/atsoil.f90
@@ -79,7 +79,8 @@ type variables_sol
   double precision :: w2
 end type variables_sol
 
-! Initialisation values for soil variables (filled in usati1)
+! Initialisation values for soil variables
+! (filled in usispu/cs_user_parameters)
 
 !> initial soil surface temperature
 double precision :: tsini

commit 13f12c602c93c98228546f30f5f14f5253da0eb2

Fix memory minor leak introduced in commit dabf0215.

src/base/caltri.f90
@@ -1198,9 +1198,9 @@ call turbulence_model_free_bc_ids
 
 call finalize_aux_arrays
 
-if (ippmod(iatmos).ge.0) then
+call finalize_meteo
 
-  call finalize_meteo
+if (ippmod(iatmos).ge.0) then
 
   if(imbrication_flag)then
     call finalize_imbrication