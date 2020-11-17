code-saturne/code_saturne

commit dabf0215420852a6c159d125d4fc419736b1408e

[Atmo] major commit to imrove compatibility of atmospheric module and standard user setting.
Fortran routine usatin1 has been removed and options can be set in
usppmo/cs_user_model and usipsu/cs_user_parameters.
The logic "Default values -> GUI -> Fortran routines -> C routines" is
now ok.
Fortran "ficmet" global variable has been moved to C (in cs_atmo_option
struct, meteo_file_name).

src/atmo/atincl.f90
@@ -177,6 +177,7 @@ integer(c_int), pointer, save :: iatmst
 !> - 1: the user can directly impose the exact meteo velocity
 !> by declaring the 'meteo_velocity' field
 !> Useful for iatmst = 1
+!> Note: deprecated, imeteo=2 can be used instead.
 integer, save :: theo_interp
 
 ! 2.1 Constant specific to the physics (defined in atini1.f90)
@@ -384,6 +385,19 @@ integer, save :: kopint
 
     !---------------------------------------------------------------------------
 
+    ! Interface to C function returning a meteo file name
+
+    subroutine cs_f_atmo_get_meteo_file_name(f_name_max, f_name, f_name_len)  &
+      bind(C, name='cs_f_atmo_get_meteo_file_name')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      integer(c_int), value       :: f_name_max
+      type(c_ptr), intent(out)    :: f_name
+      integer(c_int), intent(out) :: f_name_len
+    end subroutine cs_f_atmo_get_meteo_file_name
+
+    !---------------------------------------------------------------------------
+
     !> \brief Return pointers to atmo includes
 
     subroutine cs_f_atmo_get_pointers(ps,                               &
@@ -549,6 +563,44 @@ integer, save :: kopint
 
 contains
 
+    !=============================================================================
+
+    !> \brief Return meteo file name
+
+    !> \param[out]  name   meteo file name
+
+    subroutine atmo_get_meteo_file_name(name)
+
+      use, intrinsic :: iso_c_binding
+      implicit none
+
+      ! Arguments
+
+      character(len=*), intent(out) :: name
+
+      ! Local variables
+
+      integer :: i
+      integer(c_int) :: name_max, c_name_len
+      type(c_ptr) :: c_name_p
+      character(kind=c_char, len=1), dimension(:), pointer :: c_name
+
+      name_max = len(name)
+
+      call cs_f_atmo_get_meteo_file_name(name_max, c_name_p, c_name_len)
+      call c_f_pointer(c_name_p, c_name, [c_name_len])
+
+      do i = 1, c_name_len
+        name(i:i) = c_name(i)
+      enddo
+      do i = c_name_len + 1, name_max
+        name(i:i) = ' '
+      enddo
+
+      return
+
+    end subroutine atmo_get_meteo_file_name
+
   !=============================================================================
 
   !> \brief Map fortran to C variables
@@ -636,7 +688,7 @@ use atsoil
 implicit none
 
 ! Local variables
-integer :: imode, ifac, n_level
+integer :: imode, n_level, n_times, n_level_t
 
 type(c_ptr) :: c_z_temp_met, c_time_met
 type(c_ptr) :: c_frac_neb, c_diag_neb
@@ -667,28 +719,23 @@ if (imeteo.gt.0) then
 
   ! NB : only ztmet,ttmet,qvmet,ncmet are extended to 11000m if iatr1=1
   !           rmet,tpmet,phmet
-  allocate(zdmet(nbmetd))
+  n_level = max(1, nbmetd)
+  n_times = max(1, nbmetm)
+  n_level_t = max(1, nbmaxt)
+  allocate(zdmet(n_level))
 
   if (iatmst.ge.1) then
-    n_level = max(1, nbmetd)
     allocate(dpdt_met(n_level))
     allocate(mom(3, n_level))
     allocate(mom_met(3, n_level))
   endif
 
-  allocate(umet(nbmetd,nbmetm), vmet(nbmetd,nbmetm), wmet(nbmetd,nbmetm))
-  allocate(ekmet(nbmetd,nbmetm), epmet(nbmetd,nbmetm))
-  allocate(ttmet(nbmaxt,nbmetm), qvmet(nbmaxt,nbmetm), ncmet(nbmaxt,nbmetm))
-  allocate(pmer(nbmetm))
-  allocate(xmet(nbmetm), ymet(nbmetm))
-  allocate(rmet(nbmaxt,nbmetm), tpmet(nbmaxt,nbmetm))
-
-  ! Allocate and initialize auto inlet/outlet flag
-
-  allocate(iautom(nfabor))
-  do ifac = 1, nfabor
-    iautom(ifac) = 0
-  enddo
+  allocate(umet(n_level,n_times), vmet(n_level,n_times), wmet(n_level,n_times))
+  allocate(ekmet(n_level,n_times), epmet(n_level,n_times))
+  allocate(ttmet(n_level_t,n_times), qvmet(n_level_t,n_times), ncmet(n_level_t,n_times))
+  allocate(pmer(n_times))
+  allocate(xmet(n_times), ymet(n_times))
+  allocate(rmet(n_level_t,n_times), tpmet(n_level_t,n_times))
 
   ! Allocate additional arrays for 1D radiative model
 
@@ -724,6 +771,33 @@ if (imeteo.gt.0) then
 endif
 
 end subroutine init_meteo
+
+!===============================================================================
+
+!> \brief Initialisation of meteo data
+subroutine init_atmo_autom(nfabor)
+
+use cs_c_bindings
+
+implicit none
+
+integer nfabor
+
+! Local variables
+integer :: ifac
+
+! Allocate additional arrays for Water Microphysics
+if (imeteo.gt.0) then
+
+  ! Allocate and initialize auto inlet/outlet flag
+  allocate(iautom(nfabor))
+  do ifac = 1, nfabor
+    iautom(ifac) = 0
+  enddo
+
+endif
+
+end subroutine init_atmo_autom
 
 !=============================================================================
 

src/atmo/cs_atmo.c
@@ -124,6 +124,7 @@ static cs_atmo_option_t  _atmo_option = {
   .nucleation_model = 0,
   .subgrid_model = 0,
   .meteo_profile = 0, /* no meteo profile */
+  .meteo_file_name = NULL,
   .meteo_dlmo = 0.,
   .meteo_z0 = -1.,
   .meteo_zref = -1.,
@@ -223,6 +224,11 @@ void
 cs_atmo_compute_meteo_profiles(void);
 
 void
+cs_f_atmo_get_meteo_file_name(int           name_max,
+                              const char  **name,
+                              int          *name_len);
+
+void
 cs_f_atmo_get_pointers(cs_real_t              **ps,
                        int                    **syear,
                        int                    **squant,
@@ -278,6 +284,35 @@ cs_f_atmo_finalize(void);
 /*============================================================================
  * Fortran wrapper function definitions
  *============================================================================*/
+
+/*----------------------------------------------------------------------------
+ * Return the name meteo file
+ *
+ * This function is intended for use by Fortran wrappers.
+ *
+ * parameters:
+ *   name_max <-- maximum name length
+ *   name     --> pointer to associated length
+ *   name_len --> length of associated length
+ *----------------------------------------------------------------------------*/
+
+void
+cs_f_atmo_get_meteo_file_name(int           name_max,
+                              const char  **name,
+                              int          *name_len)
+{
+  *name = _atmo_option.meteo_file_name;
+  *name_len = strlen(*name);
+
+  if (*name_len > name_max) {
+    bft_error
+      (__FILE__, __LINE__, 0,
+       _("Error retrieving meteo file  (\"%s\"):\n"
+         "Fortran caller name length (%d) is too small for name \"%s\"\n"
+         "(of length %d)."),
+       _atmo_option.meteo_file_name, name_max, *name, *name_len);
+  }
+}
 
 /*----------------------------------------------------------------------------
  * Get pointer
@@ -425,6 +460,7 @@ cs_f_atmo_chem_finalize(void)
 void
 cs_f_atmo_finalize(void)
 {
+  BFT_FREE(_atmo_option.meteo_file_name);
   BFT_FREE(_atmo_option.z_temp_met);
   BFT_FREE(_atmo_option.time_met);
   BFT_FREE(_atmo_option.hyd_p_met);
@@ -527,17 +563,6 @@ cs_atmo_init_meteo_profiles(void)
   cs_real_t corio_f = 4. * cs_math_pi / 86164.
     * sin(aopt->latitude * cs_math_pi / 180.);
   aopt->meteo_zi = zi_coef * ustar0 / corio_f;
-
-  bft_printf("\n"
-             " Meteo profile information\n"
-             "  roughness = %f\n"
-             "  ground ustar = %f\n"
-             "  uref = %f\n"
-             "  zref = %f\n"
-             "  tstar = %f\n"
-             "  1/LMO = %f\n"
-             "  zi = %f\n",
-             z0, ustar0, aopt->meteo_uref, zref, tstar, dlmo, aopt->meteo_zi);
 
 }
 
@@ -829,6 +854,55 @@ cs_atmo_z_ground_compute(void)
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief This function computes hydrostatic profiles of density and pressure
+ *
+ *  This function solves the following transport equation on \f$ \varia \f$:
+ *  \f[
+ *  \divs \left( \grad \varia \right)
+ *      = \divs \left( \dfrac{\vect{g}}{c_p \theta} \right) \varia 0
+ *  \f]
+ *  where \f$ \vect{g} \f$ is the gravity field and \f$ \theta \f$
+ *  is the potential temperature.
+ *
+ *  The boundary conditions on \f$ \varia \f$ read:
+ *  \f[
+ *   \varia = \left(\dfrac{P_{sea}}{p_s}\right)^{R/C_p} \textrm{on the ground}
+ *  \f]
+ *  and Neumann elsewhere.
+ *
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_atmo_hydrostatic_profiles_compute(void)
+{
+ //TODO
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief This function set the file name of the meteo file.
+ *
+ * \param[in] file_name  name of the file.
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_atmo_set_meteo_file_name(const char *file_name)
+{
+  if (file_name == NULL) {
+    return;
+  }
+
+  BFT_MALLOC(_atmo_option.meteo_file_name,
+             strlen(file_name) + 1,
+             char);
+
+  sprintf(_atmo_option.meteo_file_name, "%s", file_name);
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief This function set the file name of the SPACK file.
  *
  * \param[in] file_name  name of the file.
@@ -1087,7 +1161,7 @@ cs_atmo_compute_solar_angles(cs_real_t latitude,
 void
 cs_atmo_log_setup(void)
 {
-  if (cs_glob_physical_model_flag[CS_ATMOSPHERIC] < 0)
+  if (cs_glob_physical_model_flag[CS_ATMOSPHERIC] == CS_ATMO_OFF)
     return;
 
   cs_log_printf(CS_LOG_SETUP,
@@ -1096,17 +1170,17 @@ cs_atmo_log_setup(void)
                   "--------------------------\n\n"));
 
   switch(cs_glob_physical_model_flag[CS_ATMOSPHERIC]) {
-    case 0:
+    case CS_ATMO_CONSTANT_DENSITY:
       /* Constant density */
       cs_log_printf(CS_LOG_SETUP,
                   _("  Constant density\n\n"));
       break;
-    case 1:
+    case CS_ATMO_DRY:
       /* Dry */
       cs_log_printf(CS_LOG_SETUP,
                   _("  Dry atmosphere\n\n"));
       break;
-    case 2:
+    case CS_ATMO_HUMID:
       /* Humid */
       cs_log_printf(CS_LOG_SETUP,
                   _("  Humid atmosphere\n\n"));
@@ -1151,6 +1225,14 @@ cs_atmo_log_setup(void)
        "    longitude: %6f\n\n"),
      cs_glob_atmo_option->latitude,
      cs_glob_atmo_option->longitude);
+
+  if (cs_glob_atmo_option->meteo_profile == 1) {
+    cs_log_printf
+      (CS_LOG_SETUP,
+       _("  Large scale Meteo file: %s\n\n"),
+       cs_glob_atmo_option->meteo_file_name);
+  }
+
 
   if (cs_glob_atmo_option->meteo_profile == 2) {
     cs_log_printf

src/atmo/cs_atmo.h
@@ -57,6 +57,19 @@ BEGIN_C_DECLS
  *============================================================================*/
 
 /*----------------------------------------------------------------------------
+ * Atmospheric models
+ *----------------------------------------------------------------------------*/
+
+typedef enum {
+
+  CS_ATMO_OFF = -1,
+  CS_ATMO_CONSTANT_DENSITY = 0,
+  CS_ATMO_DRY = 1,
+  CS_ATMO_HUMID = 2,
+
+} cs_atmo_model_t;
+
+/*----------------------------------------------------------------------------
  * Atmospheric nucleation models
  *----------------------------------------------------------------------------*/
 
@@ -135,6 +148,9 @@ typedef struct {
    *  - 1: use a meteo file
    *  - 2: directly enter values */
   int meteo_profile;
+
+  /*! Meteo file */
+  char *meteo_file_name;
 
   /*! Meteo Monin obukhov inverse length */
   cs_real_t meteo_dlmo;
@@ -262,6 +278,17 @@ extern cs_atmo_chemistry_t *cs_glob_atmo_chemistry;
 /*============================================================================
  * Public function definitions
  *============================================================================*/
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief This function set the file name of the meteo file.
+ *
+ * \param[in] file_name  name of the file.
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_atmo_set_meteo_file_name(const char *file_name);
 
 /*----------------------------------------------------------------------------*/
 /*!

src/atmo/cs_atprke.c
@@ -563,14 +563,14 @@ cs_atprke(cs_real_t  tinstk[],
   /* Compute potential temperature derivatives
      ======================================== */
 
-  if (cs_glob_physical_model_flag[CS_ATMOSPHERIC] == 1)
+  if (cs_glob_physical_model_flag[CS_ATMOSPHERIC] == CS_ATMO_DRY)
     _dry_atmosphere(cromo,
                     cpro_pcvto,
                     tinstk,
                     smbrk,
                     smbre);
 
-  else if (cs_glob_physical_model_flag[CS_ATMOSPHERIC] == 2)
+  else if (cs_glob_physical_model_flag[CS_ATMOSPHERIC] == CS_ATMO_HUMID)
     _humid_atmosphere(cromo,
                       cpro_pcvto,
                       tinstk,

src/base/cs_c_bindings.f90
@@ -2495,6 +2495,17 @@ module cs_c_bindings
 
     !---------------------------------------------------------------------------
 
+    !> \brief Sets the meteo file name
+
+    subroutine cs_atmo_set_meteo_file_name(name) &
+      bind(C, name='cs_atmo_set_meteo_file_name')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      character(kind=c_char, len=1), dimension(*), intent(in) :: name
+    end subroutine cs_atmo_set_meteo_file_name
+
+    !---------------------------------------------------------------------------
+
     !> \brief Sets the file name used to initialize SPACK
 
     subroutine cs_atmo_chemistry_set_spack_file_name(name) &
@@ -6200,6 +6211,30 @@ contains
     is_active = c_is_active
 
   end function cell_is_active
+
+  !=============================================================================
+
+  !> \brief Sets the meteo file name
+
+  !> \param[in]     name      name of the file
+
+  subroutine atmo_set_meteo_file_name(name)
+
+    use, intrinsic :: iso_c_binding
+    implicit none
+
+    ! Arguments
+
+    character(len=*), intent(in) :: name
+
+    ! Local variables
+
+    character(len=len_trim(name)+1, kind=c_char) :: c_name
+
+    c_name = trim(name)//c_null_char
+    call cs_atmo_set_meteo_file_name(c_name)
+
+  end subroutine atmo_set_meteo_file_name
 
   !=============================================================================
 

src/atmo/Makefile.am
@@ -72,7 +72,6 @@ cs_intprf.h
 
 noinst_LTLIBRARIES = libcsatmo.la
 libcsatmo_la_SOURCES = \
-atini0.f90 \
 atini1.f90 \
 atiniv.f90 \
 atleca.f90 \

src/atmo/atini0.f90
@@ -1,193 +1,0 @@
-!-------------------------------------------------------------------------------
-
-! This file is part of Code_Saturne, a general-purpose CFD tool.
-!
-! Copyright (C) 1998-2020 EDF S.A.
-!
-! This program is free software; you can redistribute it and/or modify it under
-! the terms of the GNU General Public License as published by the Free Software
-! Foundation; either version 2 of the License, or (at your option) any later
-! version.
-!
-! This program is distributed in the hope that it will be useful, but WITHOUT
-! ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-! FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-! details.
-!
-! You should have received a copy of the GNU General Public License along with
-! this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
-! Street, Fifth Floor, Boston, MA 02110-1301, USA.
-
-!-------------------------------------------------------------------------------
-!> \file atini0.f90
-!> \brief Initialisation of variable options for Code_Saturne atmospheric
-!>     module in addition to what is done previously in iniini function
-subroutine atini0
-
-!===============================================================================
-! Module files
-!===============================================================================
-use paramx
-use dimens
-use numvar
-use optcal
-use cstphy
-use entsor
-use cstnum
-use ppppar
-use ppthch
-use ppincl
-use atincl
-use atsoil
-use atchem
-use atimbr
-use field
-use sshaerosol
-
-!===============================================================================
-
-implicit none
-
-! Local variables
-
-!===============================================================================
-
-!===============================================================================
-! 0. General info
-!===============================================================================
-
-!--> constants used in the atmospheric physics module
-!    (see definition in atincl.f90):
-
-p0 = 1.01325d5
-t0 = 288.15d0
-
-!===============================================================================
-! 1. Global variables for atmospheric flows
-!===============================================================================
-
-! Space and time reference of the run:
-! ------------------------------------
-
-! Option for the meteo profile computation
-!ihpm   --> flag to compute the hydrostastic pressure by Laplace integration
-!           in the meteo profiles
-!       = 0 : bottom to top Laplace integration, based on P(sea level) (default)
-!       = 1 : top to bottom Laplace integration based on P computed for
-!            the standard atmosphere at z(nbmaxt)
-ihpm = 0
-
-! 1d radiative transfer model:
-! ----------------------------
-
-! iatra1 -->  flag for the use of the 1d atmo radiative model
-! nfatr1 --> 1d radiative model pass frequency
-! ivert  --> flag for the definition of the vertical grid
-!            -1: no vertical grid
-!            0 : automatic definition !!!!!!!!!MM 2do
-!            1 : definition by the user in usatdv
-! iqv0   --> flag for humidity above the domain (0 : no humidity; 1 : decreasing)
-
-iatra1 = 0
-nfatr1 = 1
-ivert = 1  ! if iatra1=1 alors ivert=1
-iqv0 = 0
-
-! flag to use the soil model (if humid atmosphere)
-iatsoil = 0
-! Initial values for soil variables
-tsini  = 20.d0   !TSINI  : Surface ground temperature
-tprini = 20.d0   !TPRINI : Deep ground temperature
-qvsini = 0.d0    !QVSINI : Ground humidity
-tmer   = 20.d0   !Sea temperature
-
-!  -------------------------------------------------------------------------------
-!  Microphysics parameterization options
-!  -------------------------------------------------------------------------------
-!  --> Option for subgrid models
-!  modsub = 0 : the simplest parameterization (for numerical verifications)
-!  modsub = 1 : Bechtold et al. 1995 (Luc Musson-Genon)
-!  modsub = 2 : Bouzereau et al. 2004
-!  modsub = 3 : Cuijpers and Duynkerke 1993, Deardorff 1976, Sommeria
-!               and Deardorff 1977
-modsub = 0
-
-!  --> Option for liquid water content distribution models
-!  moddis = 1 : all or nothing
-!  moddis = 2 : Gaussian distribution
-moddis = 1
-
-! logaritmic standard deviation of the log-normal law of the droplet spectrum
-! adimensional
-sigc = 0.53 ! other referenced values are 0.28, 0.15
-
-!  -----------------------------------------------------------------------------
-!  Atmospheric imbrication on large scale meteo (atimbr module)
-!  -----------------------------------------------------------------------------
-
-! activation flag
-imbrication_flag = .false.
-imbrication_verbose = .false.
-
-! ------------------------------------------------------------------------------
-! flags for activating the cressman interpolation for the boundary conditions
-! ------------------------------------------------------------------------------
-cressman_u = .false.
-cressman_v = .false.
-cressman_tke = .false.
-cressman_eps = .false.
-cressman_theta = .false.
-cressman_qw = .false.
-cressman_nc = .false.
-
-! --------------------------------------------------------------
-! numerical parameters for the cressman interpolation formulas
-! --------------------------------------------------------------
-horizontal_influence_radius = 8500.d0
-vertical_influence_radius = 100.d0
-
-! key id for optimal interpolation
-
-call field_get_key_id("opt_interp_id", kopint)
-
-theo_interp = 0
-
-! -------------------------------------
-! flags for 1d radiative transfer model
-! -------------------------------------
-
-! no computation / storage of downward and upward infrared radiative fluxes
-irdu = 1
-! no computation / storage of downward and upward solar radiative fluxes
-soldu = 1
-
-!===============================================================================
-! 2. ON DONNE LA MAIN A L'UTLISATEUR
-!===============================================================================
-
-! initmeteo --> use meteo profile for variables initialization
-!               (0: not used; 1: used )
-! NB : should eventually be set by interface
-
-initmeteo = 1
-
-call usati1
-
-! Atmospheric gaseous chemistry
-! Do not change this order
-if (iaerosol.eq.CS_ATMO_AEROSOL_SSH) ichemistry = 4
-! if a chemical scheme is solved, a concentration profiles
-! file must be used
-if (ichemistry.ge.1) ifilechemistry = ichemistry
-if (nogaseouschemistry .and. iaerosol.eq.CS_ATMO_AEROSOL_OFF) ichemistry = 0
-
-!--------
-! Formats
-!--------
-
-!----
-! End
-!----
-
-return
-end subroutine atini0

src/atmo/atini1.f90
@@ -166,6 +166,27 @@ endif
 
 if (itytur.eq.3) irijnu = 1
 
+!===============================================================================
+! 7. Some initialization for meteo...
+!===============================================================================
+
+if (ippmod(iatmos).ge.0) then
+
+  call init_meteo
+
+  if (imbrication_flag) then
+    call activate_imbrication
+  endif
+
+  call cs_at_data_assim_build_ops
+
+  if (ifilechemistry.ge.1) then
+    call init_chemistry
+  endif
+
+endif
+
+
 !--------
 ! FORMATS
 !--------

commit 2219d69ef5fba9538b03cea0061df823c4522bb8

[Atmo] move nebulosity arrays to fields and fix bug introduced in commit dabf021 (2020-10-26)

src/atmo/atincl.f90
@@ -105,12 +105,6 @@ double precision, allocatable, dimension(:,:) :: tpmet
 !> hydrostatic pressure from Laplace integration
 double precision, dimension(:,:), pointer :: phmet
 
-!> Diagnosed nebulosity
-double precision, dimension(:), pointer :: nebdia
-
-!> fractional nebulosity
-double precision, dimension(:), pointer :: nn
-
 ! 1.2 Pointers for the positions of the variables
 !------------------------------------------------
 !   Variables specific to the atmospheric physics:
@@ -691,7 +685,6 @@ implicit none
 integer :: imode, n_level, n_times, n_level_t
 
 type(c_ptr) :: c_z_temp_met, c_time_met
-type(c_ptr) :: c_frac_neb, c_diag_neb
 type(c_ptr) :: c_hyd_p_met
 
 integer(c_int),   dimension(2) :: dim_hyd_p_met
@@ -704,14 +697,11 @@ if (imeteo.eq.2) then
 endif
 
 call cs_f_atmo_arrays_get_pointers(c_z_temp_met, c_time_met,     &
-                                   c_hyd_p_met, dim_hyd_p_met,   &
-                                   c_frac_neb, c_diag_neb)
+                                   c_hyd_p_met, dim_hyd_p_met)
 
 call c_f_pointer(c_z_temp_met, ztmet, [nbmaxt])
 call c_f_pointer(c_time_met, tmmet, [nbmetm])
 call c_f_pointer(c_hyd_p_met, phmet, [dim_hyd_p_met])
-call c_f_pointer(c_frac_neb, nn, [ncelet])
-call c_f_pointer(c_diag_neb, nebdia, [ncelet])
 
 ! Allocate additional arrays for Water Microphysics
 

src/atmo/atphyv.f90
@@ -245,6 +245,7 @@ double precision var_tl,var_q,cov_tlq
 double precision q1,qsup, rvap
 
 double precision, dimension(:), pointer :: cvar_k, cvar_ep
+double precision, dimension(:), pointer :: nn, nebdia
 
 rvap = rair*rvsra
 
@@ -262,6 +263,8 @@ call field_gradient_scalar(ivarfl(isca(iymw)), 1, 0, inc, iccocg, dqsd)
 
 call field_get_val_s(ivarfl(ik), cvar_k)
 call field_get_val_s(ivarfl(iep), cvar_ep)
+call field_get_val_s_by_name("nebulosity_frac", nn)
+call field_get_val_s_by_name("nebulosity_diag", nebdia)
 
 ! -------------------------------------------------------------
 ! Gradients are used for estimating standard deviations of the

src/atmo/atprop.f90
@@ -91,6 +91,12 @@ if (ippmod(iatmos).eq.2) then
     call field_find_or_create('ustar', itycat, ityloc, idim1, f_id)
 
   endif
+  if (moddis.ge.2) then
+    !> fractional nebulosity
+    call add_property_field_1d('nebulosity_frac', 'Nebulo frac', f_id)
+  endif
+  !> Diagnosed nebulosity
+  call add_property_field_1d('nebulosity_diag', 'Nebulo diag', f_id)
 endif
 
 return

src/atmo/atr1vf.f90
@@ -73,6 +73,7 @@ double precision, allocatable, dimension(:) :: aeroso, infrad
 double precision, allocatable, dimension(:,:,:) :: coords(:,:,:)
 double precision, dimension(:), pointer :: crom, cpro_pcliq
 double precision, dimension(:), pointer :: cvara_totwt, cpro_tempc, cvara_ntdrp
+double precision, dimension(:), pointer :: nebdia
 
 save ideb
 data ideb/0/
@@ -134,6 +135,7 @@ if (mod(ntcabs,nfatr1).eq.0.or.ideb.eq.0) then
 
     call field_get_val_prev_s(ivarfl(isca(iymw)), cvara_totwt)
     call field_get_val_prev_s(ivarfl(isca(intdrp)), cvara_ntdrp)
+    call field_get_val_s_by_name("nebulosity_diag", nebdia)
   endif
 
   !=============================================================================

src/atmo/cs_atmo.c
@@ -150,9 +150,7 @@ static cs_atmo_option_t  _atmo_option = {
   .nbmaxt = 0,
   .z_temp_met = NULL,
   .time_met   = NULL,
-  .hyd_p_met  = NULL,
-  .frac_neb  = NULL,
-  .diag_neb  = NULL
+  .hyd_p_met  = NULL
 };
 
 /* global atmo constants structure */
@@ -267,9 +265,7 @@ void
 cs_f_atmo_arrays_get_pointers(cs_real_t **z_temp_met,
                               cs_real_t **time_met,
                               cs_real_t **hyd_p_met,
-                              int         dim_hyd_p_met[2],
-                              cs_real_t **frac_neb,
-                              cs_real_t **diag_neb);
+                              int         dim_hyd_p_met[2]);
 
 void
 cs_f_atmo_chem_arrays_get_pointers(int       **species_to_scalar_id,
@@ -407,9 +403,7 @@ void
 cs_f_atmo_arrays_get_pointers(cs_real_t **z_temp_met,
                               cs_real_t **time_met,
                               cs_real_t **hyd_p_met,
-                              int         dim_hyd_p_met[2],
-                              cs_real_t **frac_neb,
-                              cs_real_t **diag_neb)
+                              int         dim_hyd_p_met[2])
 {
   const cs_mesh_t  *m = cs_glob_mesh;
   cs_lnum_t n_cells_ext = m->n_cells_with_ghosts;
@@ -421,10 +415,6 @@ cs_f_atmo_arrays_get_pointers(cs_real_t **z_temp_met,
   if (_atmo_option.hyd_p_met == NULL)
     BFT_MALLOC(_atmo_option.hyd_p_met,
                _atmo_option.nbmetm*_atmo_option.nbmaxt, cs_real_t);
-  if (_atmo_option.frac_neb == NULL)
-    BFT_MALLOC(_atmo_option.frac_neb, n_cells_ext, cs_real_t);
-  if (_atmo_option.diag_neb == NULL)
-    BFT_MALLOC(_atmo_option.diag_neb, n_cells_ext, cs_real_t);
 
   *hyd_p_met       = _atmo_option.hyd_p_met;
   dim_hyd_p_met[0] = _atmo_option.nbmaxt;
@@ -432,8 +422,6 @@ cs_f_atmo_arrays_get_pointers(cs_real_t **z_temp_met,
 
   *z_temp_met = _atmo_option.z_temp_met;
   *time_met   = _atmo_option.time_met;
-  *frac_neb   = _atmo_option.frac_neb;
-  *diag_neb   = _atmo_option.diag_neb;
 }
 
 void
@@ -468,8 +456,6 @@ cs_f_atmo_finalize(void)
   BFT_FREE(_atmo_option.z_temp_met);
   BFT_FREE(_atmo_option.time_met);
   BFT_FREE(_atmo_option.hyd_p_met);
-  BFT_FREE(_atmo_option.frac_neb);
-  BFT_FREE(_atmo_option.diag_neb);
 }
 
 /*============================================================================

src/atmo/cs_atmo.h
@@ -197,10 +197,6 @@ typedef struct {
   cs_real_t *time_met;
   /* Hydrostatic pressure from Laplace integration */
   cs_real_t *hyd_p_met;
-  /* Fractional nebulosity */
-  cs_real_t *frac_neb;
-  /* Diagnosed nebulosity */
-  cs_real_t *diag_neb;
 
 } cs_atmo_option_t;
 

src/atmo/cs_atprke.c
@@ -390,6 +390,9 @@ _humid_atmosphere(const cs_real_t  cromo[],
 
   cs_real_t pphy, dum;
 
+  cs_real_t *diag_neb = cs_field_by_name("nebulosity_diag")->val;
+  cs_real_t *frac_neb = cs_field_by_name("nebulosity_frac")->val;
+
   for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++) {
 
     /* calculate the physical pressure 'pphy' */
@@ -413,8 +416,8 @@ _humid_atmosphere(const cs_real_t  cromo[],
            cvara_tpp[c_id],
            cvara_qw[c_id],
            cpro_pcliq[c_id],
-           cs_glob_atmo_option->diag_neb[c_id],
-           cs_glob_atmo_option->frac_neb[c_id],
+           diag_neb[c_id],
+           frac_neb[c_id],
            &(etheta[c_id]),
            &(eq[c_id]));
   }

src/base/cs_c_bindings.f90
@@ -2464,13 +2464,12 @@ module cs_c_bindings
     !> \brief Return pointers to atmo arrays
 
     subroutine cs_f_atmo_arrays_get_pointers(p_ztmet, p_tmmet, &
-         p_phmet, dim_phmet,                                   &
-         p_nn, p_nebdia)                                       &
+         p_phmet, dim_phmet)                                   &
          bind(C, name='cs_f_atmo_arrays_get_pointers')
       use, intrinsic :: iso_c_binding
       implicit none
       integer(c_int), dimension(2) :: dim_phmet
-      type(c_ptr), intent(out) :: p_ztmet, p_tmmet, p_phmet, p_nn, p_nebdia
+      type(c_ptr), intent(out) :: p_ztmet, p_tmmet, p_phmet
     end subroutine cs_f_atmo_arrays_get_pointers
 
     !---------------------------------------------------------------------------