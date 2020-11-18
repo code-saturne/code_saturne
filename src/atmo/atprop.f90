code-saturne/code_saturne

commit 2219d69ef5fba9538b03cea0061df823c4522bb8

[Atmo] move nebulosity arrays to fields and fix bug introduced in commit dabf021 (2020-10-26)

src/atmo/atprop.f90
@@ -91,6 +91,12 @@ if (ippmod(iatmos).eq.2) then
     call field_find_or_create('ustar', itycat, ityloc, idim1, f_id)
 
   endif
+  if (moddis.ge.2) then
+    !> fractional nebulosity
+    call add_property_field_1d('nebulosity_frac', 'Nebulo frac', f_id)
+  endif
+  !> Diagnosed nebulosity
+  call add_property_field_1d('nebulosity_diag', 'Nebulo diag', f_id)
 endif
 
 return

src/atmo/atincl.f90
@@ -105,12 +105,6 @@ double precision, allocatable, dimension(:,:) :: tpmet
 !> hydrostatic pressure from Laplace integration
 double precision, dimension(:,:), pointer :: phmet
 
-!> Diagnosed nebulosity
-double precision, dimension(:), pointer :: nebdia
-
-!> fractional nebulosity
-double precision, dimension(:), pointer :: nn
-
 ! 1.2 Pointers for the positions of the variables
 !------------------------------------------------
 !   Variables specific to the atmospheric physics:
@@ -691,7 +685,6 @@ implicit none
 integer :: imode, n_level, n_times, n_level_t
 
 type(c_ptr) :: c_z_temp_met, c_time_met
-type(c_ptr) :: c_frac_neb, c_diag_neb
 type(c_ptr) :: c_hyd_p_met
 
 integer(c_int),   dimension(2) :: dim_hyd_p_met
@@ -704,14 +697,11 @@ if (imeteo.eq.2) then
 endif
 
 call cs_f_atmo_arrays_get_pointers(c_z_temp_met, c_time_met,     &
-                                   c_hyd_p_met, dim_hyd_p_met,   &
-                                   c_frac_neb, c_diag_neb)
+                                   c_hyd_p_met, dim_hyd_p_met)
 
 call c_f_pointer(c_z_temp_met, ztmet, [nbmaxt])
 call c_f_pointer(c_time_met, tmmet, [nbmetm])
 call c_f_pointer(c_hyd_p_met, phmet, [dim_hyd_p_met])
-call c_f_pointer(c_frac_neb, nn, [ncelet])
-call c_f_pointer(c_diag_neb, nebdia, [ncelet])
 
 ! Allocate additional arrays for Water Microphysics
 

src/atmo/atphyv.f90
@@ -245,6 +245,7 @@ double precision var_tl,var_q,cov_tlq
 double precision q1,qsup, rvap
 
 double precision, dimension(:), pointer :: cvar_k, cvar_ep
+double precision, dimension(:), pointer :: nn, nebdia
 
 rvap = rair*rvsra
 
@@ -262,6 +263,8 @@ call field_gradient_scalar(ivarfl(isca(iymw)), 1, 0, inc, iccocg, dqsd)
 
 call field_get_val_s(ivarfl(ik), cvar_k)
 call field_get_val_s(ivarfl(iep), cvar_ep)
+call field_get_val_s_by_name("nebulosity_frac", nn)
+call field_get_val_s_by_name("nebulosity_diag", nebdia)
 
 ! -------------------------------------------------------------
 ! Gradients are used for estimating standard deviations of the

src/atmo/atr1vf.f90
@@ -73,6 +73,7 @@ double precision, allocatable, dimension(:) :: aeroso, infrad
 double precision, allocatable, dimension(:,:,:) :: coords(:,:,:)
 double precision, dimension(:), pointer :: crom, cpro_pcliq
 double precision, dimension(:), pointer :: cvara_totwt, cpro_tempc, cvara_ntdrp
+double precision, dimension(:), pointer :: nebdia
 
 save ideb
 data ideb/0/
@@ -134,6 +135,7 @@ if (mod(ntcabs,nfatr1).eq.0.or.ideb.eq.0) then
 
     call field_get_val_prev_s(ivarfl(isca(iymw)), cvara_totwt)
     call field_get_val_prev_s(ivarfl(isca(intdrp)), cvara_ntdrp)
+    call field_get_val_s_by_name("nebulosity_diag", nebdia)
   endif
 
   !=============================================================================

src/atmo/cs_atmo.c
@@ -150,9 +150,7 @@ static cs_atmo_option_t  _atmo_option = {
   .nbmaxt = 0,
   .z_temp_met = NULL,
   .time_met   = NULL,
-  .hyd_p_met  = NULL,
-  .frac_neb  = NULL,
-  .diag_neb  = NULL
+  .hyd_p_met  = NULL
 };
 
 /* global atmo constants structure */
@@ -267,9 +265,7 @@ void
 cs_f_atmo_arrays_get_pointers(cs_real_t **z_temp_met,
                               cs_real_t **time_met,
                               cs_real_t **hyd_p_met,
-                              int         dim_hyd_p_met[2],
-                              cs_real_t **frac_neb,
-                              cs_real_t **diag_neb);
+                              int         dim_hyd_p_met[2]);
 
 void
 cs_f_atmo_chem_arrays_get_pointers(int       **species_to_scalar_id,
@@ -407,9 +403,7 @@ void
 cs_f_atmo_arrays_get_pointers(cs_real_t **z_temp_met,
                               cs_real_t **time_met,
                               cs_real_t **hyd_p_met,
-                              int         dim_hyd_p_met[2],
-                              cs_real_t **frac_neb,
-                              cs_real_t **diag_neb)
+                              int         dim_hyd_p_met[2])
 {
   const cs_mesh_t  *m = cs_glob_mesh;
   cs_lnum_t n_cells_ext = m->n_cells_with_ghosts;
@@ -421,10 +415,6 @@ cs_f_atmo_arrays_get_pointers(cs_real_t **z_temp_met,
   if (_atmo_option.hyd_p_met == NULL)
     BFT_MALLOC(_atmo_option.hyd_p_met,
                _atmo_option.nbmetm*_atmo_option.nbmaxt, cs_real_t);
-  if (_atmo_option.frac_neb == NULL)
-    BFT_MALLOC(_atmo_option.frac_neb, n_cells_ext, cs_real_t);
-  if (_atmo_option.diag_neb == NULL)
-    BFT_MALLOC(_atmo_option.diag_neb, n_cells_ext, cs_real_t);
 
   *hyd_p_met       = _atmo_option.hyd_p_met;
   dim_hyd_p_met[0] = _atmo_option.nbmaxt;
@@ -432,8 +422,6 @@ cs_f_atmo_arrays_get_pointers(cs_real_t **z_temp_met,
 
   *z_temp_met = _atmo_option.z_temp_met;
   *time_met   = _atmo_option.time_met;
-  *frac_neb   = _atmo_option.frac_neb;
-  *diag_neb   = _atmo_option.diag_neb;
 }
 
 void
@@ -468,8 +456,6 @@ cs_f_atmo_finalize(void)
   BFT_FREE(_atmo_option.z_temp_met);
   BFT_FREE(_atmo_option.time_met);
   BFT_FREE(_atmo_option.hyd_p_met);
-  BFT_FREE(_atmo_option.frac_neb);
-  BFT_FREE(_atmo_option.diag_neb);
 }
 
 /*============================================================================

src/atmo/cs_atmo.h
@@ -197,10 +197,6 @@ typedef struct {
   cs_real_t *time_met;
   /* Hydrostatic pressure from Laplace integration */
   cs_real_t *hyd_p_met;
-  /* Fractional nebulosity */
-  cs_real_t *frac_neb;
-  /* Diagnosed nebulosity */
-  cs_real_t *diag_neb;
 
 } cs_atmo_option_t;
 

src/atmo/cs_atprke.c
@@ -390,6 +390,9 @@ _humid_atmosphere(const cs_real_t  cromo[],
 
   cs_real_t pphy, dum;
 
+  cs_real_t *diag_neb = cs_field_by_name("nebulosity_diag")->val;
+  cs_real_t *frac_neb = cs_field_by_name("nebulosity_frac")->val;
+
   for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++) {
 
     /* calculate the physical pressure 'pphy' */
@@ -413,8 +416,8 @@ _humid_atmosphere(const cs_real_t  cromo[],
            cvara_tpp[c_id],
            cvara_qw[c_id],
            cpro_pcliq[c_id],
-           cs_glob_atmo_option->diag_neb[c_id],
-           cs_glob_atmo_option->frac_neb[c_id],
+           diag_neb[c_id],
+           frac_neb[c_id],
            &(etheta[c_id]),
            &(eq[c_id]));
   }

src/base/cs_c_bindings.f90
@@ -2464,13 +2464,12 @@ module cs_c_bindings
     !> \brief Return pointers to atmo arrays
 
     subroutine cs_f_atmo_arrays_get_pointers(p_ztmet, p_tmmet, &
-         p_phmet, dim_phmet,                                   &
-         p_nn, p_nebdia)                                       &
+         p_phmet, dim_phmet)                                   &
          bind(C, name='cs_f_atmo_arrays_get_pointers')
       use, intrinsic :: iso_c_binding
       implicit none
       integer(c_int), dimension(2) :: dim_phmet
-      type(c_ptr), intent(out) :: p_ztmet, p_tmmet, p_phmet, p_nn, p_nebdia
+      type(c_ptr), intent(out) :: p_ztmet, p_tmmet, p_phmet
     end subroutine cs_f_atmo_arrays_get_pointers
 
     !---------------------------------------------------------------------------

commit 688d938a0f5bcda098524fa87a1c178508ab2186

Fix bug introduced in commit 2219d69 (yesterday).

src/atmo/atprop.f90
@@ -91,10 +91,8 @@ if (ippmod(iatmos).eq.2) then
     call field_find_or_create('ustar', itycat, ityloc, idim1, f_id)
 
   endif
-  if (moddis.ge.2) then
-    !> fractional nebulosity
-    call add_property_field_1d('nebulosity_frac', 'Nebulo frac', f_id)
-  endif
+  !> fractional nebulosity
+  call add_property_field_1d('nebulosity_frac', 'Nebulo frac', f_id)
   !> Diagnosed nebulosity
   call add_property_field_1d('nebulosity_diag', 'Nebulo diag', f_id)
 endif