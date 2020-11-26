code-saturne/code_saturne

commit 23bef257c91235902727b9a86cda2d4ba2e15c7e

Improve handling for cathare path for coupled case

bin/cs_create.py
@@ -308,13 +308,7 @@ class study:
 
         # Creating Cathare case
         if self.cat_case_name is not None:
-            config = configparser.ConfigParser()
-            config.read(self.package.get_configfiles())
-            if config.has_option('install', 'cathare'):
-                self.create_cathare_case(repbase, config.get('install', 'cathare'))
-            else:
-                sys.stderr.write("Cannot locate Cathare installation.")
-                sys.exit(1)
+            self.create_cathare_case(repbase)
 
         # Creating the Python script case
         if self.py_case_name is not None:
@@ -352,7 +346,7 @@ class study:
 
     #---------------------------------------------------------------------------
 
-    def create_cathare_case(self, repbase, cathare_path):
+    def create_cathare_case(self, repbase):
         """
         Create and initialize Cathare case directory for coupling.
         """
@@ -664,18 +658,6 @@ class study:
         run_conf = cs_run_conf.run_conf(run_conf_path,
                                         package=self.package,
                                         create_if_missing=True)
-
-        # If a cathare LIBPATH is given, it is added to LD_LIBRARY_PATH.
-        # This modification is needed for the dlopen of the cathare .so file
-        if cathare_path:
-            i_c = cs_run_conf.get_install_config_info(self.package)
-            resource_name = cs_run_conf.get_resource_name(i_c)
-            v25_3_line="export v25_3=%s\n" % cathare_path
-            new_line="export LD_PATH_LIBRARY=$v25_3/%s/"+":$LD_LIBRARY_PATH\n"
-            add_lines = v25_3_line
-            add_lines += new_line % ("lib")
-            add_lines += new_line % ("ICoCo/lib")
-            run_conf.set(resource_name, 'compute_prologue', add_lines)
 
         run_conf.save()
 

bin/cs_case.py
@@ -1068,24 +1068,34 @@ class case:
             cs_exec_environment.write_prepend_path(s,
                                                    'LD_LIBRARY_PATH',
                                                    mpi_libdir)
+        s.write('\n')
 
         # HANDLE CATHARE COUPLING
+        wrote_cathare_path = False
         if self.domains:
             for d in self.domains:
                 if hasattr(d, "cathare_case_file"):
-                    config = configparser.ConfigParser()
-                    config.read(self.package.get_configfiles())
-                    cathare_path = config.get('install', 'cathare')
-                    for p in ['lib', 'ICoCo/lib']:
-                        lp = os.path.join(cathare_path, p)
-                        cs_exec_environment.write_prepend_path(s,
-                                                               "LD_LIBRARY_PATH",
-                                                               lp)
+                    if not wrote_cathare_path:
+                        cs_exec_environment.write_script_comment(s, \
+                            'Export paths necessary for CATHARE coupling.\n')
+                        config = configparser.ConfigParser()
+                        config.read(self.package.get_configfiles())
+                        cathare_path = config.get('install', 'cathare')
+                        for p in ['lib', 'ICoCo/lib']:
+                            lp = os.path.join(cathare_path, p)
+                            cs_exec_environment.write_prepend_path( \
+                                    s, "LD_LIBRARY_PATH", lp)
+
+                        wrote_cathare_path = True
+                        s.write('\n')
 
         # Handle python coupling
         if self.py_domains:
+            cs_exec_environment.write_script_comment(s, \
+                'Export paths necessary for python coupling.\n')
             pydir = self.package_compute.get_dir("pythondir")
             cs_exec_environment.write_prepend_path(s, "PYTHONPATH", pydir)
+            s.write('\n')
 
         # Handle environment modules if used
 

commit 4e1dce50398c45b4ce9a7aba79d2c1a636dd210a

Fix error in coupled case creation introduced in commit 23bef257c

bin/cs_create.py
@@ -449,16 +449,8 @@ class study:
         if not os.path.isdir(resu):
             os.mkdir(resu)
 
-        if self.cat_case_name is not None:
-            config = configparser.ConfigParser()
-            config.read(self.package.get_configfiles())
-            cathare_libpath=config.get('install', 'cathare')
-        else:
-            cathare_libpath=None
-
         self.__build_run_cfg__(distrep = repbase,
-                               casename = 'coupling',
-                               cathare_path = cathare_libpath)
+                               casename = 'coupling')
 
         self.__coupled_run_cfg__(distrep = repbase,
                                  coupled_domains = coupled_domains)
@@ -637,8 +629,7 @@ class study:
 
     def __build_run_cfg__(self,
                           distrep,
-                          casename,
-                          cathare_path=None):
+                          casename):
         """
         Retrieve batch file for the current system
         Update batch file for the study