code-saturne/code_saturne

commit 0b5775cb9f30dd9e63545dfeb0744f30b6e6b095

GUI: add a node in the main (left-hand tree) for each volume and boundary zone.
Based on dynamic tree view implementation from C. Koren,
With adjustments to fit recent code evolutions.

This is a first step in a change of zone setup presentation.

gui/Pages/HeadLossesForm.ui
@@ -6,8 +6,8 @@
    <rect>
     <x>0</x>
     <y>0</y>
-    <width>410</width>
-    <height>440</height>
+    <width>441</width>
+    <height>451</height>
    </rect>
   </property>
   <property name="windowTitle">
@@ -15,40 +15,27 @@
   </property>
   <layout class="QVBoxLayout" name="verticalLayout">
    <item>
-    <widget class="QGroupBox" name="groupBoxZones">
-     <property name="maximumSize">
-      <size>
-       <width>16777215</width>
-       <height>150</height>
-      </size>
+    <widget class="QLabel" name="zoneLabel">
+     <property name="text">
+      <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;ZoneLabel&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
      </property>
-     <property name="toolTip">
-      <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Define head losses on volume zones defined in the Volume regions definition heading.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+    </widget>
+   </item>
+   <item>
+    <widget class="Line" name="line">
+     <property name="orientation">
+      <enum>Qt::Horizontal</enum>
      </property>
-     <property name="title">
-      <string>Select volume zone for head losses </string>
-     </property>
-     <layout class="QGridLayout">
-      <item row="0" column="0">
-       <widget class="QTreeView" name="treeView">
-        <property name="toolTip">
-         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Define head losses on volume zones defined in the Volume regions definition heading.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
-        </property>
-        <property name="indentation">
-         <number>0</number>
-        </property>
-        <property name="rootIsDecorated">
-         <bool>false</bool>
-        </property>
-       </widget>
-      </item>
-     </layout>
     </widget>
    </item>
    <item>
     <widget class="QGroupBox" name="groupBoxDef">
      <property name="toolTip">
-      <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Define the coefficients of the head losses tensor. This tensor is symmetric and positive.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;(code_saturne key word: &lt;/span&gt;&lt;span style=&quot; font-size:9pt; font-weight:600;&quot;&gt;CKUPDC(NCEPDP,6)&lt;/span&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;)&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+      <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Define the
+       coefficients of the head losses tensor. This tensor is symmetric and positive.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span
+       style=&quot; font-size:9pt;&quot;&gt;(code_saturne key word: &lt;/span&gt;&lt;span style=&quot; font-size:9pt;
+       font-weight:600;&quot;&gt;CKUPDC(NCEPDP,6)&lt;/span&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;)&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;
+      </string>
      </property>
      <property name="title">
       <string>Tensor coefficients</string>

commit 054b23b2bd05969d168bf20b6dde5078fc1d66a0

[GUI] fix head losses handling, broken by commit 0b5775cb (August 21 2020).
Also includes some cleanup and consistency improvments.

bin/model/HeadLossesModel.py
@@ -210,7 +210,7 @@ class HeadLossesModel(Variables, Model):
 
 
     @Variables.noUndo
-    def getMatrixComposant(self, zoneid, a):
+    def getMatrixComponent(self, zoneid, a):
         """
         Get values of one composant of the matrix of the change reference frame,
         for the head loss with zone's id.
@@ -222,7 +222,7 @@ class HeadLossesModel(Variables, Model):
         value = node.xmlGetDouble(a)
         if value == None:
             value = self.__defaultValues()[a]
-            self.setMatrixComposant(zoneid, a, value)
+            self.setMatrixComponent(zoneid, a, value)
 
         return value
 
@@ -235,21 +235,21 @@ class HeadLossesModel(Variables, Model):
         """
         self.isInt(int(zoneid))
 
-        a11 = self.getMatrixComposant(zoneid, 'a11')
-        a12 = self.getMatrixComposant(zoneid, 'a12')
-        a13 = self.getMatrixComposant(zoneid, 'a13')
-        a21 = self.getMatrixComposant(zoneid, 'a21')
-        a22 = self.getMatrixComposant(zoneid, 'a22')
-        a23 = self.getMatrixComposant(zoneid, 'a23')
-        a31 = self.getMatrixComposant(zoneid, 'a31')
-        a32 = self.getMatrixComposant(zoneid, 'a32')
-        a33 = self.getMatrixComposant(zoneid, 'a33')
+        a11 = self.getMatrixComponent(zoneid, 'a11')
+        a12 = self.getMatrixComponent(zoneid, 'a12')
+        a13 = self.getMatrixComponent(zoneid, 'a13')
+        a21 = self.getMatrixComponent(zoneid, 'a21')
+        a22 = self.getMatrixComponent(zoneid, 'a22')
+        a23 = self.getMatrixComponent(zoneid, 'a23')
+        a31 = self.getMatrixComponent(zoneid, 'a31')
+        a32 = self.getMatrixComponent(zoneid, 'a32')
+        a33 = self.getMatrixComponent(zoneid, 'a33')
 
         return a11, a12, a13, a21, a22, a23, a31, a32, a33
 
 
     @Variables.undoLocal
-    def setMatrixComposant(self, zoneid, a, value):
+    def setMatrixComponent(self, zoneid, a, value):
         """
         Set value of composant of matrix of the change reference frame,
         for the head loss with zone's id.
@@ -272,15 +272,15 @@ class HeadLossesModel(Variables, Model):
         for a in (a11, a12, a13, a21, a22, a23, a31, a32, a33):
             self.isFloat(a)
 
-        self.setMatrixComposant(zoneid, 'a11', a11)
-        self.setMatrixComposant(zoneid, 'a12', a12)
-        self.setMatrixComposant(zoneid, 'a13', a13)
-        self.setMatrixComposant(zoneid, 'a21', a21)
-        self.setMatrixComposant(zoneid, 'a22', a22)
-        self.setMatrixComposant(zoneid, 'a23', a23)
-        self.setMatrixComposant(zoneid, 'a31', a31)
-        self.setMatrixComposant(zoneid, 'a32', a32)
-        self.setMatrixComposant(zoneid, 'a33', a33)
+        self.setMatrixComponent(zoneid, 'a11', a11)
+        self.setMatrixComponent(zoneid, 'a12', a12)
+        self.setMatrixComponent(zoneid, 'a13', a13)
+        self.setMatrixComponent(zoneid, 'a21', a21)
+        self.setMatrixComponent(zoneid, 'a22', a22)
+        self.setMatrixComponent(zoneid, 'a23', a23)
+        self.setMatrixComponent(zoneid, 'a31', a31)
+        self.setMatrixComponent(zoneid, 'a32', a32)
+        self.setMatrixComponent(zoneid, 'a33', a33)
 
 
 #-------------------------------------------------------------------------------
@@ -414,7 +414,7 @@ class HeadLossesModelTestCase(ModelTest):
         assert mdl.getMatrix('2') == (1., 1.2, 1.5, 2., 2.2, 2.5, 3., 3.2, 3.5),\
             'Could not get matrix for head losses'
 
-        mdl.setMatrixComposant('2', 'a23', 2300.55)
+        mdl.setMatrixComponent('2', 'a23', 2300.55)
         doc2='''<head_losses>
                     <head_loss zone_id="2">
                         <kxx>0</kxx>
@@ -433,7 +433,7 @@ class HeadLossesModelTestCase(ModelTest):
                 </head_losses>'''
         assert mdl.node_hloss == self.xmlNodeFromString(doc2),\
             'Could not set one composant of the matrix for head losses'
-        assert mdl.getMatrixComposant('2', 'a23') == 2300.55,\
+        assert mdl.getMatrixComponent('2', 'a23') == 2300.55,\
             'Could not get one composant of the matrix for head losses'
 
 

bin/model/LocalizationModel.py
@@ -737,7 +737,7 @@ class VolumicLocalizationModel(LocalizationModel):
         # update data in the entire case
         lst = self.__natureOptions
         lst.append('initial_value')
-        lst.append('head_loss')
+        lst.append('head_losses')
         for tag in lst:
             for n in self.case.xmlGetNodeList(tag, zone=old_zone.getCodeNumber()):
                 n['zone'] = newCodeNumber

bin/model/XMLinitialize.py
@@ -1904,12 +1904,14 @@ class XMLinit(BaseXmlInit):
         """
         Change XML in order to ensure backward compatibility.
         """
+
         return
 
     def _backwardCompatibilityCurrentVersion(self):
         """
         Change XML in order to ensure backward compatibility.
         """
+
         XMLLagrangianNode = self.case.xmlGetNode('lagrangian')
         if XMLLagrangianNode:
             for attr in ('complete_model',

gui/Pages/HeadLossesForm.ui
@@ -74,7 +74,7 @@
            </widget>
           </item>
           <item>
-           <widget class="QLineEdit" name="lineEdit">
+           <widget class="QLineEdit" name="lineEdit_kxx">
             <property name="toolTip">
              <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Define the first diagonal component of the head losses tensor.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
             </property>
@@ -104,7 +104,7 @@
            </widget>
           </item>
           <item>
-           <widget class="QLineEdit" name="lineEdit_2">
+           <widget class="QLineEdit" name="lineEdit_kyy">
             <property name="toolTip">
              <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Define the second diagonal component of the head losses tensor.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
             </property>
@@ -134,7 +134,7 @@
            </widget>
           </item>
           <item>
-           <widget class="QLineEdit" name="lineEdit_3">
+           <widget class="QLineEdit" name="lineEdit_kzz">
             <property name="toolTip">
              <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Define the third diagonal component of the head losses tensor.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
             </property>
@@ -264,7 +264,7 @@
               </widget>
              </item>
              <item row="0" column="1">
-              <widget class="QLineEdit" name="lineEdit_4">
+              <widget class="QLineEdit" name="lineEdit_a11">
                <property name="minimumSize">
                 <size>
                  <width>50</width>
@@ -302,7 +302,7 @@
               </widget>
              </item>
              <item row="0" column="3">
-              <widget class="QLineEdit" name="lineEdit_5">
+              <widget class="QLineEdit" name="lineEdit_a12">
                <property name="minimumSize">
                 <size>
                  <width>50</width>
@@ -338,7 +338,7 @@
               </widget>
              </item>
              <item row="0" column="5">
-              <widget class="QLineEdit" name="lineEdit_6">
+              <widget class="QLineEdit" name="lineEdit_a13">
                <property name="minimumSize">
                 <size>
                  <width>50</width>
@@ -384,7 +384,7 @@
               </widget>
              </item>
              <item row="1" column="1">
-              <widget class="QLineEdit" name="lineEdit_8">
+              <widget class="QLineEdit" name="lineEdit_a21">
                <property name="minimumSize">
                 <size>
                  <width>50</width>
@@ -420,7 +420,7 @@
               </widget>
              </item>
              <item row="1" column="3">
-              <widget class="QLineEdit" name="lineEdit_9">
+              <widget class="QLineEdit" name="lineEdit_a22">
                <property name="minimumSize">
                 <size>
                  <width>50</width>
@@ -456,7 +456,7 @@
               </widget>
              </item>
              <item row="1" column="5">
-              <widget class="QLineEdit" name="lineEdit_7">
+              <widget class="QLineEdit" name="lineEdit_a23">
                <property name="minimumSize">
                 <size>
                  <width>50</width>
@@ -502,7 +502,7 @@
               </widget>
              </item>
              <item row="2" column="1">
-              <widget class="QLineEdit" name="lineEdit_11">
+              <widget class="QLineEdit" name="lineEdit_a31">
                <property name="minimumSize">
                 <size>
                  <width>50</width>
@@ -538,7 +538,7 @@
               </widget>
              </item>
              <item row="2" column="3">
-              <widget class="QLineEdit" name="lineEdit_12">
+              <widget class="QLineEdit" name="lineEdit_a32">
                <property name="minimumSize">
                 <size>
                  <width>50</width>
@@ -574,7 +574,7 @@
               </widget>
              </item>
              <item row="2" column="5">
-              <widget class="QLineEdit" name="lineEdit_10">
+              <widget class="QLineEdit" name="lineEdit_a33">
                <property name="minimumSize">
                 <size>
                  <width>50</width>

gui/Pages/HeadLossesView.py
@@ -101,46 +101,46 @@ class HeadLossesView(QWidget, Ui_HeadLossesForm):
         self.case.undoStartGlobal()
 
     def setValidators(self):
-        validator = DoubleValidator(self.lineEdit, min=0.0)
-        validator_2 = DoubleValidator(self.lineEdit_2, min=0.0)
-        validator_3 = DoubleValidator(self.lineEdit_3, min=0.0)
-        validator_4 = DoubleValidator(self.lineEdit_4)
-        validator_5 = DoubleValidator(self.lineEdit_5)
-        validator_6 = DoubleValidator(self.lineEdit_6)
-        validator_8 = DoubleValidator(self.lineEdit_8)
-        validator_9 = DoubleValidator(self.lineEdit_9)
-        validator_7 = DoubleValidator(self.lineEdit_7)
-        validator_11 = DoubleValidator(self.lineEdit_11)
-        validator_12 = DoubleValidator(self.lineEdit_12)
-        validator_10 = DoubleValidator(self.lineEdit_10)
+        validator_kxx = DoubleValidator(self.lineEdit_kxx, min=0.0)
+        validator_kyy = DoubleValidator(self.lineEdit_kyy, min=0.0)
+        validator_kzz = DoubleValidator(self.lineEdit_kzz, min=0.0)
+        validator_a11 = DoubleValidator(self.lineEdit_a11)
+        validator_a12 = DoubleValidator(self.lineEdit_a12)
+        validator_a13 = DoubleValidator(self.lineEdit_a13)
+        validator_a21 = DoubleValidator(self.lineEdit_a21)
+        validator_a22 = DoubleValidator(self.lineEdit_a22)
+        validator_a23 = DoubleValidator(self.lineEdit_a23)
+        validator_a31 = DoubleValidator(self.lineEdit_a31)
+        validator_a32 = DoubleValidator(self.lineEdit_a32)
+        validator_a33 = DoubleValidator(self.lineEdit_a33)
         # Apply validators
-        self.lineEdit.setValidator(validator)
-        self.lineEdit_2.setValidator(validator_2)
-        self.lineEdit_3.setValidator(validator_3)
-        self.lineEdit_4.setValidator(validator_4)
-        self.lineEdit_5.setValidator(validator_5)
-        self.lineEdit_6.setValidator(validator_6)
-        self.lineEdit_8.setValidator(validator_8)
-        self.lineEdit_9.setValidator(validator_9)
-        self.lineEdit_7.setValidator(validator_7)
-        self.lineEdit_11.setValidator(validator_11)
-        self.lineEdit_12.setValidator(validator_12)
-        self.lineEdit_10.setValidator(validator_10)
+        self.lineEdit_kxx.setValidator(validator_kxx)
+        self.lineEdit_kyy.setValidator(validator_kyy)
+        self.lineEdit_kzz.setValidator(validator_kzz)
+        self.lineEdit_a11.setValidator(validator_a11)
+        self.lineEdit_a12.setValidator(validator_a12)
+        self.lineEdit_a13.setValidator(validator_a13)
+        self.lineEdit_a21.setValidator(validator_a21)
+        self.lineEdit_a22.setValidator(validator_a22)
+        self.lineEdit_a23.setValidator(validator_a23)
+        self.lineEdit_a31.setValidator(validator_a31)
+        self.lineEdit_a32.setValidator(validator_a32)
+        self.lineEdit_a33.setValidator(validator_a33)
 
     def setConnections(self):
         self.groupBox_3.clicked[bool].connect(self.slotTransfoMatrix)
-        self.lineEdit.textChanged[str].connect(self.slotKxx)
-        self.lineEdit_2.textChanged[str].connect(self.slotKyy)
-        self.lineEdit_3.textChanged[str].connect(self.slotKzz)
-        self.lineEdit_4.textChanged[str].connect(self.slotA11)
-        self.lineEdit_5.textChanged[str].connect(self.slotA12)
-        self.lineEdit_6.textChanged[str].connect(self.slotA13)
-        self.lineEdit_8.textChanged[str].connect(self.slotA21)
-        self.lineEdit_9.textChanged[str].connect(self.slotA22)
-        self.lineEdit_7.textChanged[str].connect(self.slotA23)
-        self.lineEdit_11.textChanged[str].connect(self.slotA31)
-        self.lineEdit_12.textChanged[str].connect(self.slotA32)
-        self.lineEdit_10.textChanged[str].connect(self.slotA33)
+        self.lineEdit_kxx.textChanged[str].connect(self.slotKxx)
+        self.lineEdit_kyy.textChanged[str].connect(self.slotKyy)
+        self.lineEdit_kzz.textChanged[str].connect(self.slotKzz)
+        self.lineEdit_a11.textChanged[str].connect(self.slotA11)
+        self.lineEdit_a12.textChanged[str].connect(self.slotA12)
+        self.lineEdit_a13.textChanged[str].connect(self.slotA13)
+        self.lineEdit_a21.textChanged[str].connect(self.slotA21)
+        self.lineEdit_a22.textChanged[str].connect(self.slotA22)
+        self.lineEdit_a23.textChanged[str].connect(self.slotA23)
+        self.lineEdit_a31.textChanged[str].connect(self.slotA31)
+        self.lineEdit_a32.textChanged[str].connect(self.slotA32)
+        self.lineEdit_a33.textChanged[str].connect(self.slotA33)
 
     def selectHeadLossesZones(self):
         label = self.zone.getLabel()
@@ -151,9 +151,9 @@ class HeadLossesView(QWidget, Ui_HeadLossesForm):
         self.groupBoxDef.show()
         self.groupBox_3.show()
         kxx, kyy, kzz = self.model.getKCoefficients(name)
-        self.lineEdit.setText(str(kxx))
-        self.lineEdit_2.setText(str(kyy))
-        self.lineEdit_3.setText(str(kzz))
+        self.lineEdit_kxx.setText(str(kxx))
+        self.lineEdit_kyy.setText(str(kyy))
+        self.lineEdit_kzz.setText(str(kzz))
 
         if self.model.getMatrixChoice(name, 'choice') == 'on':
             self.groupBox_3.setChecked(True)
@@ -173,7 +173,7 @@ class HeadLossesView(QWidget, Ui_HeadLossesForm):
     def slotKxx(self, text):
         zone_id = self.zone.getCodeNumber()
         model = HeadLossesModel(self.case)
-        if self.lineEdit.validator().state == QValidator.Acceptable:
+        if self.lineEdit_kxx.validator().state == QValidator.Acceptable:
             value = from_qvariant(text, float)
             model.setCoefficient(zone_id, 'kxx', value)
 
@@ -181,7 +181,7 @@ class HeadLossesView(QWidget, Ui_HeadLossesForm):
     def slotKyy(self, text):
         zone_id = self.zone.getCodeNumber()
         model = HeadLossesModel(self.case)
-        if self.lineEdit.validator().state == QValidator.Acceptable:
+        if self.lineEdit_kyy.validator().state == QValidator.Acceptable:
             value = from_qvariant(text, float)
             model.setCoefficient(zone_id, 'kyy', value)
 
@@ -189,7 +189,7 @@ class HeadLossesView(QWidget, Ui_HeadLossesForm):
     def slotKzz(self, text):
         zone_id = self.zone.getCodeNumber()
         model = HeadLossesModel(self.case)
-        if self.lineEdit.validator().state == QValidator.Acceptable:
+        if self.lineEdit_kzz.validator().state == QValidator.Acceptable:
             value = from_qvariant(text, float)
             model.setCoefficient(zone_id, 'kzz', value)
 
@@ -209,87 +209,87 @@ class HeadLossesView(QWidget, Ui_HeadLossesForm):
             a11, a12, a13, a21, a22, a23, a31, a32, a33 = 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0
             self.groupBox_3.setChecked(False)
 
-        self.lineEdit_4.setText(str(a11))
-        self.lineEdit_5.setText(str(a12))
-        self.lineEdit_6.setText(str(a13))
-        self.lineEdit_8.setText(str(a21))
-        self.lineEdit_9.setText(str(a22))
-        self.lineEdit_7.setText(str(a23))
-        self.lineEdit_11.setText(str(a31))
-        self.lineEdit_12.setText(str(a32))
-        self.lineEdit_10.setText(str(a33))
+        self.lineEdit_a11.setText(str(a11))
+        self.lineEdit_a12.setText(str(a12))
+        self.lineEdit_a13.setText(str(a13))
+        self.lineEdit_a21.setText(str(a21))
+        self.lineEdit_a22.setText(str(a22))
+        self.lineEdit_a23.setText(str(a23))
+        self.lineEdit_a31.setText(str(a31))
+        self.lineEdit_a32.setText(str(a32))
+        self.lineEdit_a33.setText(str(a33))
 
     @pyqtSlot(str)
     def slotA11(self, text):
         zone_id = self.zone.getCodeNumber()
         model = HeadLossesModel(self.case)
-        if self.lineEdit.validator().state == QValidator.Acceptable:
+        if self.lineEdit_a11.validator().state == QValidator.Acceptable:
             value = from_qvariant(text, float)
-            model.setCoefficient(zone_id, 'a11', value)
+            model.setMatrixComponent(zone_id, 'a11', value)
 
     @pyqtSlot(str)
     def slotA12(self, text):
         zone_id = self.zone.getCodeNumber()
         model = HeadLossesModel(self.case)
-        if self.lineEdit.validator().state == QValidator.Acceptable:
+        if self.lineEdit_a12.validator().state == QValidator.Acceptable:
             value = from_qvariant(text, float)
-            model.setCoefficient(zone_id, 'a12', value)
+            model.setMatrixComponent(zone_id, 'a12', value)
 
     @pyqtSlot(str)
     def slotA13(self, text):
         zone_id = self.zone.getCodeNumber()
         model = HeadLossesModel(self.case)
-        if self.lineEdit.validator().state == QValidator.Acceptable:
+        if self.lineEdit_a13.validator().state == QValidator.Acceptable:
             value = from_qvariant(text, float)
-            model.setCoefficient(zone_id, 'a13', value)
+            model.setMatrixComponent(zone_id, 'a13', value)
 
     @pyqtSlot(str)
     def slotA21(self, text):
         zone_id = self.zone.getCodeNumber()
         model = HeadLossesModel(self.case)
-        if self.lineEdit.validator().state == QValidator.Acceptable:
+        if self.lineEdit_a21.validator().state == QValidator.Acceptable:
             value = from_qvariant(text, float)
-            model.setCoefficient(zone_id, 'a21', value)
+            model.setMatrixComponent(zone_id, 'a21', value)
 
     @pyqtSlot(str)
     def slotA22(self, text):
         zone_id = self.zone.getCodeNumber()
         model = HeadLossesModel(self.case)
-        if self.lineEdit.validator().state == QValidator.Acceptable:
+        if self.lineEdit_a22.validator().state == QValidator.Acceptable:
             value = from_qvariant(text, float)
-            model.setCoefficient(zone_id, 'a22', value)
+            model.setMatrixComponent(zone_id, 'a22', value)
 
     @pyqtSlot(str)
     def slotA23(self, text):
         zone_id = self.zone.getCodeNumber()
         model = HeadLossesModel(self.case)
-        if self.lineEdit.validator().state == QValidator.Acceptable:
+        if self.lineEdit_a23.validator().state == QValidator.Acceptable:
             value = from_qvariant(text, float)
-            model.setCoefficient(zone_id, 'a23', value)
+            model.setMatrixComponent(zone_id, 'a23', value)
 
     @pyqtSlot(str)
     def slotA31(self, text):
         zone_id = self.zone.getCodeNumber()
         model = HeadLossesModel(self.case)
-        if self.lineEdit.validator().state == QValidator.Acceptable:
+        if self.lineEdit_a31.validator().state == QValidator.Acceptable:
             value = from_qvariant(text, float)
-            model.setCoefficient(zone_id, 'a31', value)
+            model.setMatrixComponent(zone_id, 'a31', value)
 
     @pyqtSlot(str)
     def slotA32(self, text):
         zone_id = self.zone.getCodeNumber()
         model = HeadLossesModel(self.case)
-        if self.lineEdit.validator().state == QValidator.Acceptable:
+        if self.lineEdit_a32.validator().state == QValidator.Acceptable:
             value = from_qvariant(text, float)
-            model.setCoefficient(zone_id, 'a32', value)
+            model.setMatrixComponent(zone_id, 'a32', value)
 
     @pyqtSlot(str)
     def slotA33(self, text):
         zone_id = self.zone.getCodeNumber()
         model = HeadLossesModel(self.case)
-        if self.lineEdit.validator().state == QValidator.Acceptable:
+        if self.lineEdit_a33.validator().state == QValidator.Acceptable:
             value = from_qvariant(text, float)
-            model.setCoefficient(zone_id, 'a33', value)
+            model.setMatrixComponent(zone_id, 'a33', value)
 
 
 # -------------------------------------------------------------------------------