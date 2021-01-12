code-saturne/code_saturne

commit 48c036d900a83b7e3cae44554385b2d6daae5e63

[GUI] Remove old placeholder for Lagragian BCs, add missing code in neptune_cfd.

gui/Pages/BoundaryConditionsNeptune.ui
@@ -20,6 +20,18 @@
       <string>Boundary conditions</string>
      </property>
      <layout class="QGridLayout" name="gridLayout">
+      <item row="3" column="0">
+       <widget class="BoundaryConditionsVelocityInletView" name="VelocityWidget" native="true"/>
+      </item>
+      <item row="1" column="0">
+       <widget class="BoundaryConditionsPressureView" name="PressureWidget" native="true"/>
+      </item>
+      <item row="2" column="0">
+       <widget class="BoundaryConditionsFractionView" name="FractionWidget" native="true"/>
+      </item>
+      <item row="4" column="0">
+       <widget class="BoundaryConditionsTurbulenceInletView" name="TurbulenceWidget" native="true"/>
+      </item>
       <item row="0" column="0">
        <widget class="QTableView" name="tableViewFields">
         <property name="minimumSize">
@@ -36,18 +48,6 @@
         </property>
        </widget>
       </item>
-      <item row="1" column="0">
-       <widget class="BoundaryConditionsPressureView" name="PressureWidget" native="true"/>
-      </item>
-      <item row="2" column="0">
-       <widget class="BoundaryConditionsFractionView" name="FractionWidget" native="true"/>
-      </item>
-      <item row="3" column="0">
-       <widget class="BoundaryConditionsVelocityInletView" name="VelocityWidget" native="true"/>
-      </item>
-      <item row="4" column="0">
-       <widget class="BoundaryConditionsTurbulenceInletView" name="TurbulenceWidget" native="true"/>
-      </item>
       <item row="5" column="0">
        <widget class="BoundaryConditionsEnergyView" name="EnergyWidget" native="true"/>
       </item>
@@ -63,23 +63,10 @@
       <item row="9" column="0">
        <widget class="BoundaryConditionsWallView" name="WallWidget" native="true"/>
       </item>
-      <item row="10" column="0">
-       <spacer>
-        <property name="orientation">
-         <enum>Qt::Vertical</enum>
-        </property>
-        <property name="sizeHint" stdset="0">
-         <size>
-          <width>20</width>
-          <height>20</height>
-         </size>
-        </property>
-       </spacer>
-      </item>
      </layout>
     </widget>
    </item>
-   <item row="1" column="0">
+   <item row="2" column="0">
     <spacer>
      <property name="orientation">
       <enum>Qt::Vertical</enum>
@@ -92,19 +79,34 @@
      </property>
     </spacer>
    </item>
+   <item row="1" column="0">
+    <widget class="LagrangianBoundaryView" name="particlesWidget" native="true"/>
+   </item>
   </layout>
  </widget>
  <customwidgets>
   <customwidget>
    <class>BoundaryConditionsVelocityInletView</class>
    <extends>QWidget</extends>
-   <header>BoundaryConditionsVelocityInletViewNeptune.h</header>
+   <header location="global">BoundaryConditionsVelocityInletView.h</header>
    <container>1</container>
   </customwidget>
   <customwidget>
    <class>BoundaryConditionsTurbulenceInletView</class>
    <extends>QWidget</extends>
-   <header>BoundaryConditionsTurbulenceInletViewNeptune.h</header>
+   <header location="global">BoundaryConditionsTurbulenceInletView.h</header>
+   <container>1</container>
+  </customwidget>
+  <customwidget>
+   <class>BoundaryConditionsPressureView</class>
+   <extends>QWidget</extends>
+   <header location="global">BoundaryConditionsPressureView.h</header>
+   <container>1</container>
+  </customwidget>
+  <customwidget>
+   <class>LagrangianBoundaryView</class>
+   <extends>QWidget</extends>
+   <header location="global">LagrangianBoundaryView.h</header>
    <container>1</container>
   </customwidget>
   <customwidget>
@@ -129,12 +131,6 @@
    <class>BoundaryConditionsScalarView</class>
    <extends>QWidget</extends>
    <header>BoundaryConditionsScalarViewNeptune.h</header>
-   <container>1</container>
-  </customwidget>
-  <customwidget>
-   <class>BoundaryConditionsPressureView</class>
-   <extends>QWidget</extends>
-   <header>BoundaryConditionsPressureViewNeptune.h</header>
    <container>1</container>
   </customwidget>
   <customwidget>

gui/Pages/BoundaryConditionsViewNeptune.py
@@ -58,6 +58,7 @@ from code_saturne.Pages.BoundaryConditionsNeptune import Ui_BoundaryConditions
 from code_saturne.model.BoundaryNeptune import *
 from code_saturne.model.BoundaryConditionsModelNeptune import *
 from code_saturne.model.MainFieldsModel import MainFieldsModel
+from code_saturne.model.LagrangianModel import LagrangianModel
 
 #-------------------------------------------------------------------------------
 # log config
@@ -184,6 +185,9 @@ class BoundaryConditionsView(QWidget, Ui_BoundaryConditions):
         self.__nature = self.zone.getNature()
         self.__label = self.zone.getLabel()
 
+        # Set the case for custom widgets
+        self.particlesWidget.setup(self.case)
+
         self.__hideAllWidgets()
         self.__slotSelectBoundary()
 
@@ -257,6 +261,9 @@ class BoundaryConditionsView(QWidget, Ui_BoundaryConditions):
             if len(MainFieldsModel(self.case).getSolidFieldIdList()) > 0:
                 self.tableViewFields.show()
             self.__selectWallBoundary(boundary)
+
+        if LagrangianModel(self.case).getLagrangianModel() != 'off':
+            self.particlesWidget.showWidget(self.zone)
 
     def __selectInletBoundary(self, boundary):
         """

gui/Base/BrowserView.py
@@ -509,8 +509,7 @@ class BrowserView(QWidget, Ui_BrowserForm):
         elif section == "Boundary conditions":
             return []
         elif section == 'Additional BC models':
-            return ['Particle boundary conditions',
-                    'Fluid structure interaction', 'Cathare Coupling',
+            return ['Fluid structure interaction', 'Cathare Coupling',
                     'Immersed Boundaries']
         elif section == 'Time settings':
             return ['Start/Restart']
@@ -805,7 +804,6 @@ class BrowserView(QWidget, Ui_BrowserForm):
         """
         """
         self.setRowClose(self.tr('Boundary_conditions'))
-        self.setRowClose(self.tr('Particle boundary conditions'))
         self.setRowClose(self.tr('Fluid structure interaction'))
         self.setRowClose(self.tr('Cathare Coupling'))
         self.setRowClose(self.tr('Immersed Boundaries'))
@@ -1027,13 +1025,12 @@ class BrowserView(QWidget, Ui_BrowserForm):
         # Boundary zones
 
         self.setRowShow(self.tr('Boundary conditions'))
-        self.setRowShow(self.tr('Particle boundary conditions'), m_lagr)
         self.setRowShow(self.tr('Fluid structure interaction'), m_ale)
         self.setRowShow(self.tr('Cathare Coupling'), is_ncfd)
         # Immersed boundaries is deactivated for the moment. Will be
         # reactivated following v6.1 once Page is updated in NCFD
         self.setRowShow(self.tr('Immersed Boundaries'), False)
-        self.setRowShow(self.tr("Additional BC models"), m_lagr or m_ale or is_ncfd or False)
+        self.setRowShow(self.tr("Additional BC models"), m_ale or is_ncfd or False)
 
         # Time settings
 

commit 0eeefeeb27b4b12a6bb9b2a3644972e1662300c2

[GUI] Fix boundary conditions page for ncfd broken by commit 48c036d9 (06/01/2021)

gui/Pages/BoundaryConditionsNeptune.ui
@@ -88,19 +88,19 @@
   <customwidget>
    <class>BoundaryConditionsVelocityInletView</class>
    <extends>QWidget</extends>
-   <header location="global">BoundaryConditionsVelocityInletView.h</header>
+   <header location="global">BoundaryConditionsVelocityInletViewNeptune.h</header>
    <container>1</container>
   </customwidget>
   <customwidget>
    <class>BoundaryConditionsTurbulenceInletView</class>
    <extends>QWidget</extends>
-   <header location="global">BoundaryConditionsTurbulenceInletView.h</header>
+   <header location="global">BoundaryConditionsTurbulenceInletViewNeptune.h</header>
    <container>1</container>
   </customwidget>
   <customwidget>
    <class>BoundaryConditionsPressureView</class>
    <extends>QWidget</extends>
-   <header location="global">BoundaryConditionsPressureView.h</header>
+   <header location="global">BoundaryConditionsPressureViewNeptune.h</header>
    <container>1</container>
   </customwidget>
   <customwidget>

gui/Pages/BoundaryConditionsViewNeptune.py
@@ -338,6 +338,7 @@ class BoundaryConditionsView(QWidget, Ui_BoundaryConditions):
         self.InterfacialAreaWidget.hideWidget()
         self.ScalarWidget.hideWidget()
         self.WallWidget.hideWidget()
+        self.particlesWidget.hideWidget()
 
 
 #-------------------------------------------------------------------------------