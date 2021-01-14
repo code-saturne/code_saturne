code-saturne/code_saturne

commit e53f13721d724b65544490f73d168464f9eaf827

Lagrangian module enhancements based on Renuda developments.
GitHub pull request #66, plus edition by Y. Fournier and M. Guingo.

- Implementation of the full turbulence dispersion scheme with rotation of
  the frame of reference.

- Modification of CS flags to reflect these changes.

- modcpl = 1 now enables the full turbulence dispersion scheme and this is
  its default value. It takes a value of 0 when the user specifies that
  the particles are supposed to be fluid-like tracker particles in which
  case the scheme is disabled. The value of modcpl also
  affects the values of two other flags:

 * modcpl = 1 => idistu = 1 (turbulent dispersion is enabled), idiffl = 1
   (turbulent diffusion is disabled)

 * modcpl = 0 => idistu = 1 (turbulent dispersion is disabled), idiffl = 1
   (turbulent diffusion is enabled)

- irdla has been removed.

- The Code_Saturne GUI has been modified to reflect the changes above.

- Modification of the wall boundary conditions for particles.

src/lagr/cs_lagr_car.c
@@ -389,66 +389,23 @@ cs_lagr_car(int              iprev,
 
         if (turb_disp_model) {
 
-          /* The complete model is made isotropic
-           *
-           * TL_i^* = TL / B_i
-           *
-           * B^2_n  = 1 +  beta^2 |<u_r>|^2/(2 k_f / 3)
-           * B^2_t1 = 1 +4 beta^2 |<u_r>|^2/(2 k_f / 3)
-           * B^2_t2 = 1 +4 beta^2 |<u_r>|^2/(2 k_f / 3)
-           *
-           * is replaced by "tr(B^2)/3 Id"
-           * */
-          if (cs_glob_lagr_model->idirla == 0) {
-            bbi[0] = sqrt(1.0 + 3. * cbcb * uvwdif);
-            bbi[1] = sqrt(1.0 + 3. * cbcb * uvwdif);
-            bbi[2] = sqrt(1.0 + 3. * cbcb * uvwdif);
-          }
+          /* relative main direction */
+          cs_real_3_t vrn, n_dir;
+          for (cs_lnum_t i = 0; i < 3; i++)
+            vrn[i] = vpart[i] - vflui[i];
 
-          /* Mean direction is X */
-          else if (cs_glob_lagr_model->idirla == 1) {
-            bbi[0] = sqrt(1.0 +       cbcb * uvwdif);
-            bbi[1] = sqrt(1.0 + 4.0 * cbcb * uvwdif);
-            bbi[2] = sqrt(1.0 + 4.0 * cbcb * uvwdif);
-          }
+          cs_math_3_normalise(vrn, n_dir);
 
-          /* Mean direction is Y */
-          else if (cs_glob_lagr_model->idirla == 2) {
-            bbi[0] = sqrt(1.0 + 4.0 * cbcb * uvwdif);
-            bbi[1] = sqrt(1.0 +       cbcb * uvwdif);
-            bbi[2] = sqrt(1.0 + 4.0 * cbcb * uvwdif);
-          }
+          /* crossing trajectory in the n_dir direction */
+          cs_real_t an, at;
+          an = (1.0 + cbcb * uvwdif);
+          at = (1.0 + 4.0 * cbcb * uvwdif);
 
-          /* Mean direction is Z */
-          else if (cs_glob_lagr_model->idirla == 3) {
-            bbi[0] = sqrt(1.0 + 4.0 * cbcb * uvwdif);
-            bbi[1] = sqrt(1.0 + 4.0 * cbcb * uvwdif);
-            bbi[2] = sqrt(1.0 +       cbcb * uvwdif);
-          }
-
-          /* Mean direction is computed locally */
-          else { /* if (cs_glob_lagr_model->idirla == 4) */
-
-            /* relative main direction */
-            cs_real_3_t vrn, n_dir;
-            for (cs_lnum_t i = 0; i < 3; i++)
-              vrn[i] = vpart[i] - vflui[i];//FIXME should be MEAN particle velocity
-
-            cs_math_3_normalise(vrn, n_dir);
-
-            /* crossing trajectory in the n_dir direction */
-            cs_real_t an, at;
-            an = (1.0 + cbcb * uvwdif);
-            at = (1.0 + 4.0 * cbcb * uvwdif);
-
-            /* We take (only) the diagonal part of
-             *  an. n(x)n + at (1 - n(x)n) */
-
-            for (cs_lnum_t id = 0; id < 3; id++)
-              bbi[id] = sqrt(an * cs_math_pow2(n_dir[id])
-                           + at * (1. - cs_math_pow2(n_dir[id])));
-
-          }
+          /* We take (only) the diagonal part of
+           *  an. n(x)n + at (1 - n(x)n) */
+          for (cs_lnum_t id = 0; id < 3; id++)
+            bbi[id] = sqrt(an * cs_math_pow2(n_dir[id]) +
+                           at * (1. - cs_math_pow2(n_dir[id])));
 
           /* Compute the timescale of the fluid velocities seen by discrete
            * particles in parallel and transverse directions */

bin/model/LagrangianModel.py
@@ -93,10 +93,7 @@ class LagrangianModel(Model):
         default['dynamic']                             = "off"
         default['mass']                                = "off"
         default['scheme_order']                        = 1
-        default['turbulent_dispersion']                = "on"
-        default['fluid_particles_turbulent_diffusion'] = "off"
-        default['complete_model_iteration']            = 0
-        default['complete_model_direction']            = 4
+        default['regular_particles']                   = 1
 
         return default
 
@@ -586,96 +583,26 @@ class LagrangianModel(Model):
 
 
     @Variables.undoLocal
-    def setTurbulentDispersion(self, status):
+    def setRegularParticles(self, value):
         """
-        Update the status markup for turbulent dispersion status from the XML document.
-        """
-        self.isOnOff(status)
-        node_turb = self.node_lagr.xmlInitChildNode('turbulent_dispersion', 'status')
-        node_turb['status'] = status
-
-
-    @Variables.noUndo
-    def getTurbulentDispersion(self):
-        """
-        Return status of turbulent dispersion status.
-        """
-        node_turb = self.node_lagr.xmlInitChildNode('turbulent_dispersion', 'status')
-        status = node_turb['status']
-        if not status:
-            status = self.defaultParticlesValues()['turbulent_dispersion']
-            self.setTurbulentDispersion(status)
-        return status
-
-
-    @Variables.undoLocal
-    def setTurbulentDiffusion(self, status):
-        """
-        Update the status markup for turbulent diffusion status from the XML document.
-        """
-        self.isOnOff(status)
-        node_turb = self.node_lagr.xmlInitChildNode('fluid_particles_turbulent_diffusion', 'status')
-        node_turb['status'] = status
-
-
-    @Variables.noUndo
-    def getTurbulentDiffusion(self):
-        """
-        Return status of turbulent diffusion status.
-        """
-        node_turb = self.node_lagr.xmlInitChildNode('fluid_particles_turbulent_diffusion', 'status')
-        status = node_turb['status']
-        if not status:
-            status = self.defaultParticlesValues()['fluid_particles_turbulent_diffusion']
-            self.setTurbulentDiffusion(status)
-        return status
-
-
-    @Variables.undoLocal
-    def setCompleteModelStartIteration(self, iteration):
-        """
-        Set value for complete model start iteration.
-        """
-        self.isInt(iteration)
-        self.isGreaterOrEqual(iteration, 0)
-        self.node_lagr.xmlSetData('complete_model', iteration)
-
-
-    @Variables.noUndo
-    def getCompleteModelStartIteration(self):
-        """
-        Return value for complete model iteration.
-        """
-        iteration = self.node_lagr.xmlGetInt('complete_model')
-        if iteration == None:
-            iteration = self.defaultParticlesValues()['complete_model_iteration']
-            self.setCompleteModelStartIteration(iteration)
-        return iteration
-
-
-    @Variables.undoLocal
-    def setCompleteModelDirection(self, value):
-        """
-        Set value for complete model direction.
+        Set value for regular particles
         """
         self.isInt(value)
-        self.isInList(value, (1,2,3,4))
-        node_direction = self.node_lagr.xmlInitChildNode('complete_model_direction', 'choice')
-        node_direction['choice'] = value
+        self.isInList(value, (0, 1))
+        self.node_lagr.xmlSetData('regular_particles', value)
 
 
     @Variables.noUndo
-    def getCompleteModelDirection(self):
+    def getRegularParticles(self):
         """
-        Return value for complete model direction.
+        Return value for regular particles
         """
-        node_direction = self.node_lagr.xmlInitChildNode('complete_model_direction', 'choice')
-        if node_direction:
-            val = node_direction['choice']
-            if val == "":
-                val = self.defaultParticlesValues()['complete_model_direction']
-                self.setCompleteModelDirection(val)
-        return val
+        value = self.node_lagr.xmlGetInt('regular_particles')
+        if value == None:
+            value = self.defaultParticlesValues()['regular_particles']
+            self.setRegularParticles(value)
+        return value
+
 
 #-------------------------------------------------------------------------------
 # Lagrangian test case
@@ -994,74 +921,6 @@ class LagrangianTestCase(ModelTest):
 
         assert mdl.node_lagr == self.xmlNodeFromString(doc), \
              'Could not get default values for scheme order.'
-
-
-    def checkTurbulentDispersion(self):
-        """Check whether the turbulent dispersion could be set and get."""
-        mdl = LagrangianModel(self.case)
-        status = mdl.getTurbulentDispersion()
-
-        assert status == mdl.defaultParticlesValues()['turbulent_dispersion'], \
-            'Could not get default values for turbulent dispersion status.'
-
-        mdl.setTurbulentDispersion('on')
-        doc = """<lagrangian model="off">
-                     <turbulent_dispersion status="on"/>
-                 </lagrangian>"""
-
-        assert mdl.node_lagr == self.xmlNodeFromString(doc), \
-            'Could not set default values for turbulent dispersion status.'
-
-
-    def checkTurbulentDiffusion(self):
-        """Check whether the turbulent diffusion could be set and get."""
-        mdl = LagrangianModel(self.case)
-        status = mdl.getTurbulentDiffusion()
-
-        assert status == mdl.defaultParticlesValues()['fluid_particles_turbulent_diffusion'], \
-            'Could not get default values for turbulent diffusion status.'
-
-        mdl.setTurbulentDiffusion('on')
-        doc = """<lagrangian model="off">
-                     <fluid_particles_turbulent_diffusion status="on"/>
-                 </lagrangian>"""
-
-        assert mdl.node_lagr == self.xmlNodeFromString(doc), \
-            'Could not set default values for turbulent diffusion status.'
-
-
-    def checkCompleteModelStartIteration(self):
-        """Check whether the complete model start iteration could be set and get."""
-        mdl = LagrangianModel(self.case)
-        status = mdl.getCompleteModelStartIteration()
-
-        assert status == mdl.defaultParticlesValues()['complete_model_iteration'], \
-            'Could not get default values for complete model start iteration.'
-
-        mdl.setCompleteModelStartIteration(1234)
-        doc = """<lagrangian model="off">
-                     <complete_model>1234</complete_model>
-                 </lagrangian>"""
-
-        assert mdl.node_lagr == self.xmlNodeFromString(doc), \
-            'Could not set default values for complete model start iteration.'
-
-
-    def checkCompleteModelDirection(self):
-        """Check whether the complete model direction could be set and get."""
-        mdl = LagrangianModel(self.case)
-        status = mdl.getCompleteModelDirection()
-
-        assert status == mdl.defaultParticlesValues()['complete_model_direction'], \
-            'Could not get default values for complete model direction.'
-
-        mdl.setCompleteModelDirection(2)
-        doc = """<lagrangian model="off">
-                    <complete_model_direction choice="2"/>
-                 </lagrangian>"""
-
-        assert mdl.node_lagr == self.xmlNodeFromString(doc), \
-            'Could not set default values for complete model direction.'
 
 
 def suite():

bin/model/XMLinitialize.py
@@ -1905,6 +1905,15 @@ class XMLinit(BaseXmlInit):
         """
         Change XML in order to ensure backward compatibility.
         """
+        XMLLagrangianNode = self.case.xmlGetNode('lagrangian')
+        if XMLLagrangianNode:
+            for attr in ('complete_model',
+                         'complete_model_direction choice',
+                         'turbulent_dispersion',
+                         'fluid_particles_turbulent_diffusion'):
+                node = XMLLagrangianNode.xmlGetNode(attr)
+                if node:
+                    node.xmlRemoveNode()
 
 #-------------------------------------------------------------------------------
 # End of XMLinit

gui/Pages/LagrangianAdvancedOptionsDialogForm.ui
@@ -1,314 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>LagrangianAdvancedOptionsDialogForm</class>
- <widget class="QDialog" name="LagrangianAdvancedOptionsDialogForm">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>420</width>
-    <height>295</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>Dialog</string>
-  </property>
-  <layout class="QGridLayout" name="gridLayout">
-   <item row="0" column="0">
-    <spacer name="horizontalSpacer">
-     <property name="orientation">
-      <enum>Qt::Horizontal</enum>
-     </property>
-     <property name="sizeHint" stdset="0">
-      <size>
-       <width>10</width>
-       <height>20</height>
-      </size>
-     </property>
-    </spacer>
-   </item>
-   <item row="0" column="1">
-    <layout class="QGridLayout">
-     <item row="0" column="0" colspan="4">
-      <widget class="QGroupBox" name="groupBoxOrder">
-       <property name="toolTip">
-        <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Specify the time scheme order for the trajectory equations.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
-       </property>
-       <property name="title">
-        <string>Integration for the stochastic diﬀerential equations</string>
-       </property>
-       <layout class="QGridLayout">
-        <item row="0" column="0">
-         <spacer>
-          <property name="orientation">
-           <enum>Qt::Horizontal</enum>
-          </property>
-          <property name="sizeHint" stdset="0">
-           <size>
-            <width>26</width>
-            <height>21</height>
-           </size>
-          </property>
-         </spacer>
-        </item>
-        <item row="0" column="1">
-         <widget class="QComboBox" name="comboBoxNORDRE">
-          <property name="minimumSize">
-           <size>
-            <width>350</width>
-            <height>0</height>
-           </size>
-          </property>
-          <property name="toolTip">
-           <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Specify the time scheme order for the trajectory equations.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
-          </property>
-         </widget>
-        </item>
-        <item row="0" column="2">
-         <spacer>
-          <property name="orientation">
-           <enum>Qt::Horizontal</enum>
-          </property>
-          <property name="sizeHint" stdset="0">
-           <size>
-            <width>27</width>
-            <height>21</height>
-           </size>
-          </property>
-         </spacer>
-        </item>
-       </layout>
-      </widget>
-     </item>
-     <item row="1" column="0">
-      <widget class="QLabel" name="labelIDISTU">
-       <property name="toolTip">
-        <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Activate or not the particle turbulent dispersion. The turbulent dispersion is compatible only with the RANS turbulent models.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
-       </property>
-       <property name="text">
-        <string>Particle turbulent dispersion</string>
-       </property>
-      </widget>
-     </item>
-     <item row="1" column="1">
-      <spacer>
-       <property name="orientation">
-        <enum>Qt::Horizontal</enum>
-       </property>
-       <property name="sizeHint" stdset="0">
-        <size>
-         <width>73</width>
-         <height>20</height>
-        </size>
-       </property>
-      </spacer>
-     </item>
-     <item row="1" column="2">
-      <widget class="QCheckBox" name="checkBoxIDISTU">
-       <property name="toolTip">
-        <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Activate or not the particle turbulent dispersion. The turbulent dispersion is compatible only with the RANS turbulent models.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
-       </property>
-       <property name="text">
-        <string/>
-       </property>
-      </widget>
-     </item>
-     <item row="1" column="3">
-      <spacer>
-       <property name="orientation">
-        <enum>Qt::Horizontal</enum>
-       </property>
-       <property name="sizeHint" stdset="0">
-        <size>
-         <width>72</width>
-         <height>20</height>
-        </size>
-       </property>
-      </spacer>
-     </item>
-     <item row="2" column="0">
-      <widget class="QLabel" name="labelIDIFFL">
-       <property name="toolTip">
-        <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Activate or not the deletion of the crossing trajectory effect, making turbulent dispersion for the particles identical to the turbulent diffusion of fluid particles.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
-       </property>
-       <property name="text">
-        <string>Suppresses the crossing trajectory eﬀect</string>
-       </property>
-      </widget>
-     </item>
-     <item row="2" column="1">
-      <spacer>
-       <property name="orientation">
-        <enum>Qt::Horizontal</enum>
-       </property>
-       <property name="sizeHint" stdset="0">
-        <size>
-         <width>73</width>
-         <height>20</height>
-        </size>
-       </property>
-      </spacer>
-     </item>
-     <item row="2" column="2">
-      <widget class="QCheckBox" name="checkBoxIDIFFL">
-       <property name="toolTip">
-        <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Activate or not the deletion of the crossing trajectory effect, making turbulent dispersion for the particles identical to the turbulent diffusion of fluid particles.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
-       </property>
-       <property name="text">
-        <string/>
-       </property>
-      </widget>
-     </item>
-     <item row="2" column="3">
-      <spacer>
-       <property name="orientation">
-        <enum>Qt::Horizontal</enum>
-       </property>
-       <property name="sizeHint" stdset="0">
-        <size>
-         <width>72</width>
-         <height>20</height>
-        </size>
-       </property>
-      </spacer>
-     </item>
-     <item row="3" column="0" colspan="4">
-      <widget class="QGroupBox" name="groupBoxModel">
-       <property name="toolTip">
-        <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Activate or not the complete turbulent dispersion model.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
-       </property>
-       <property name="title">
-        <string>Complete model for turbulent dispersion</string>
-       </property>
-       <property name="checkable">
-        <bool>true</bool>
-       </property>
-       <layout class="QGridLayout">
-        <item row="0" column="0">
-         <widget class="QLabel" name="labelMODCPL">
-          <property name="toolTip">
-           <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Specify the starting lagrangian iteration.  When strictly positive, its value is interpreted as the absolute Lagrangian time step number (including restarts) after which the complete model is applied.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
-          </property>
-          <property name="text">
-           <string>Starting lagrangian iteration</string>
-          </property>
-         </widget>
-        </item>
-        <item row="0" column="1">
-         <widget class="QLineEdit" name="lineEditMODCPL">
-          <property name="maximumSize">
-           <size>
-            <width>100</width>
-            <height>16777215</height>
-           </size>
-          </property>
-          <property name="toolTip">
-           <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Specify the starting lagrangian iteration.  When strictly positive, its value is interpreted as the absolute Lagrangian time step number (including restarts) after which the complete model is applied.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
-          </property>
-         </widget>
-        </item>
-        <item row="1" column="0">
-         <widget class="QLabel" name="labelIDIRLA">
-          <property name="toolTip">
-           <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Specify the main direction of the flow.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
-          </property>
-          <property name="text">
-           <string>Main direction of the ﬂow</string>
-          </property>
-         </widget>
-        </item>
-        <item row="1" column="1">
-         <widget class="QComboBox" name="comboBoxIDIRLA">
-          <property name="minimumSize">
-           <size>
-            <width>60</width>
-            <height>0</height>
-           </size>
-          </property>
-          <property name="toolTip">
-           <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Specify the main direction of the flow.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
-          </property>
-         </widget>
-        </item>
-       </layout>
-      </widget>
-     </item>
-     <item row="4" column="0" colspan="4">
-      <widget class="QDialogButtonBox" name="buttonBox">
-       <property name="orientation">
-        <enum>Qt::Horizontal</enum>
-       </property>
-       <property name="standardButtons">
-        <set>QDialogButtonBox::Cancel|QDialogButtonBox::Ok</set>
-       </property>
-       <property name="centerButtons">
-        <bool>true</bool>
-       </property>
-      </widget>
-     </item>
-    </layout>
-   </item>
-   <item row="0" column="2">
-    <spacer name="horizontalSpacer_2">
-     <property name="orientation">
-      <enum>Qt::Horizontal</enum>
-     </property>
-     <property name="sizeHint" stdset="0">
-      <size>
-       <width>6</width>
-       <height>20</height>
-      </size>
-     </property>
-    </spacer>
-   </item>
-   <item row="1" column="1">
-    <spacer name="verticalSpacer">
-     <property name="orientation">
-      <enum>Qt::Vertical</enum>
-     </property>
-     <property name="sizeHint" stdset="0">
-      <size>
-       <width>20</width>
-       <height>36</height>
-      </size>
-     </property>
-    </spacer>
-   </item>
-  </layout>
- </widget>
- <resources/>
- <connections>
-  <connection>
-   <sender>buttonBox</sender>
-   <signal>accepted()</signal>
-   <receiver>LagrangianAdvancedOptionsDialogForm</receiver>
-   <slot>accept()</slot>
-   <hints>
-    <hint type="sourcelabel">
-     <x>248</x>
-     <y>254</y>
-    </hint>
-    <hint type="destinationlabel">
-     <x>157</x>
-     <y>274</y>
-    </hint>
-   </hints>
-  </connection>
-  <connection>
-   <sender>buttonBox</sender>
-   <signal>rejected()</signal>
-   <receiver>LagrangianAdvancedOptionsDialogForm</receiver>
-   <slot>reject()</slot>
-   <hints>
-    <hint type="sourcelabel">
-     <x>316</x>
-     <y>260</y>
-    </hint>
-    <hint type="destinationlabel">
-     <x>286</x>
-     <y>274</y>
-    </hint>
-   </hints>
-  </connection>
- </connections>
-</ui>

commit 37a5d8c586b434080712614cc7a4ab5bc0e112ea

[Lagr] fix bug introduced in commit e53f137 (2020-12-22).
The Lagrangian time TL* must be expressed in the local referenceframe
and not in the global one (the rotation before integration is done for
that).

src/lagr/cs_lagr_car.c
@@ -389,23 +389,16 @@ cs_lagr_car(int              iprev,
 
         if (turb_disp_model) {
 
-          /* relative main direction */
-          cs_real_3_t vrn, n_dir;
-          for (cs_lnum_t i = 0; i < 3; i++)
-            vrn[i] = vpart[i] - vflui[i];
-
-          cs_math_3_normalise(vrn, n_dir);
-
-          /* crossing trajectory in the n_dir direction */
+          /* Crossing trajectory in the direction of "<u_f>-<u_p>"
+           * and in the span-wise direction */
           cs_real_t an, at;
           an = (1.0 + cbcb * uvwdif);
           at = (1.0 + 4.0 * cbcb * uvwdif);
 
-          /* We take (only) the diagonal part of
-           *  an. n(x)n + at (1 - n(x)n) */
-          for (cs_lnum_t id = 0; id < 3; id++)
-            bbi[id] = sqrt(an * cs_math_pow2(n_dir[id]) +
-                           at * (1. - cs_math_pow2(n_dir[id])));
+          bbi[0] = sqrt(an); /* First direction, n, in the local reference
+                                frame */
+          bbi[1] = sqrt(at); /* Second and third direction, orthogonal to n */
+          bbi[2] = sqrt(at);
 
           /* Compute the timescale of the fluid velocities seen by discrete
            * particles in parallel and transverse directions */