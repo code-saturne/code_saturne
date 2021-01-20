code-saturne/code_saturne

commit bbb958f533251d6a21618e542ab93c6f38c638e7

Fix bug introduced in commit 7ee5fd (default BCs for hydro pressure).

src/base/typecl.f90
@@ -883,10 +883,8 @@ do ivar = 1, nvar
   endif
 enddo
 
-
 ! Free memory
 deallocate(pripb)
-
 
 ! Symmetry
 ! =========
@@ -1512,6 +1510,28 @@ do f_id = 0, nfld - 1
     endif
   endif
 enddo
+
+!===============================================================================
+! Automatic treatment for some variables
+!===============================================================================
+
+! Put homogeneous Neumann on hydrostatic pressure for iphydr=1
+! if not modified by the user
+
+call field_get_id_try("hydrostatic_pressure", f_id)
+if (f_id.ge.0) then
+  call field_get_type(f_id, f_type)
+  ! Is the field of type FIELD_VARIABLE?
+  if (iand(f_type, FIELD_VARIABLE).eq.FIELD_VARIABLE) then
+    if (iand(f_type, FIELD_CDO)/=FIELD_CDO) then
+      call field_get_key_int(f_id, keyvar, ivar)
+
+      if(icodcl(ifac,ivar).eq.0) then
+        icodcl(ifac,ivar) = 3
+      endif
+    endif
+  endif
+endif
 
 ! ensure that for all variables of dimension higher than 1 and which components
 ! are coupled, icodcl is the same for all the components

commit 5331093202ab848a8d3de0461e0832c66a29a7df

Fix crash introduced in commit bbb958f5 (Jan 15 2021) du to missing loop.

src/base/typecl.f90
@@ -1525,10 +1525,11 @@ if (f_id.ge.0) then
   if (iand(f_type, FIELD_VARIABLE).eq.FIELD_VARIABLE) then
     if (iand(f_type, FIELD_CDO)/=FIELD_CDO) then
       call field_get_key_int(f_id, keyvar, ivar)
-
-      if(icodcl(ifac,ivar).eq.0) then
-        icodcl(ifac,ivar) = 3
-      endif
+      do ifac = 1, nfabor
+        if(icodcl(ifac,ivar).eq.0) then
+          icodcl(ifac,ivar) = 3
+        endif
+      enddo
     endif
   endif
 endif
@@ -1544,7 +1545,6 @@ do f_id = 0, nfld - 1
     if (iand(f_type, FIELD_CDO)/=FIELD_CDO) then
       call field_get_dim (f_id, f_dim)
       call field_get_key_int(f_id, keycpl, icpl)
-
       if (f_dim.gt.1.and.icpl.eq.1) then
         call field_get_key_int(f_id, keyvar, ivar)
         do ifac = 1, nfabor