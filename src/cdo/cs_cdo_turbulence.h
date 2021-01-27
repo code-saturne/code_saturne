code-saturne/code_saturne

commit bca4fa26c3574d60dd721e7e9e2263f65e47fa6d

[CDO] continue k-epsilon integration (WIP).

src/cdo/cs_cdo_turbulence.h
@@ -232,7 +232,7 @@ typedef struct {
    * @}
    * @name Main structure and function pointers
    *
-   * \brief The context structure is a structure cast on-the-fly and functino
+   * \brief The context structure is a structure cast on-the-fly and function
    * pointers are a limited set of functions associated to the main operations
    * for this structure: initialization, destruction, computation and update.
    *
@@ -263,6 +263,13 @@ typedef struct {
    */
 
   cs_turb_compute_t            *compute;
+
+  /*! \var compute_steady
+   * Function pointer to compute in steady mode all variables related to a
+   * turbulence model
+   */
+
+  cs_turb_compute_t            *compute_steady;
 
   /*! \var update
    * Function pointer to perform the update step (properties or arrays

src/atmo/cs_atmo.c
@@ -1096,12 +1096,14 @@ cs_atmo_declare_chem_from_spack(void)
     strcat(name, label);
 
     /* Field of dimension 1 */
+    /* Give the original name as label */
     _atmo_chem.species_to_field_id[i]
       = cs_variable_field_create(name, line, CS_MESH_LOCATION_CELLS, 1);
 
     /* Scalar field, store in isca_chem/species_to_scalar_id (FORTRAN/C) array */
     _atmo_chem.species_to_scalar_id[i]
       = cs_add_model_field_indexes(_atmo_chem.species_to_field_id[i]);
+
   }
 }
 

src/base/fldvar.f90
@@ -113,9 +113,6 @@ call field_get_key_id('coupled', keycpl)
 ! ---> Remplissage de ITYTUR
 itytur = iturb/10
 
-! Set type and order of the turbulence model
-call cs_set_type_order_turbulence_model()
-
 ! ---> Coherence modele
 !     Rq : ATTENTION il faudrait renforcer le blindage
 

src/base/iniusi.f90
@@ -159,6 +159,9 @@ call usipph(1, iturb, itherm, iale)
 ! Flow and other models selection through user C function
 call cs_user_model
 
+! Set type and order of the turbulence model
+call cs_set_type_order_turbulence_model()
+
 ! Activate CDO for ALE
 if (iale.eq.2) then
   call cs_ale_activate

src/cdo/cs_cdo_turbulence.c
@@ -131,7 +131,7 @@ _tke_source_term(cs_lnum_t            n_elts,
                  void                *input,
                  cs_real_t           *retval)
 {
-  /* TBD */
+  /* TODO */
   CS_UNUSED(n_elts);
   CS_UNUSED(elt_ids);
   CS_UNUSED(dense_output);
@@ -162,7 +162,7 @@ _tke_lin_source_term(cs_lnum_t            n_elts,
                      void                *input,
                      cs_real_t           *retval)
 {
-  /* TBD */
+  /* TODO */
   CS_UNUSED(n_elts);
   CS_UNUSED(elt_ids);
   CS_UNUSED(dense_output);
@@ -242,6 +242,7 @@ cs_turbulence_create(cs_turbulence_param_t    *tbp)
   turb->init_context = NULL;
   turb->free_context = NULL;
   turb->compute = NULL;
+  turb->compute_steady = NULL;
   turb->update = NULL;
 
   return turb;
@@ -291,7 +292,7 @@ cs_turbulence_init_setup(cs_turbulence_t   *turb)
   const cs_turbulence_param_t  *tbp = turb->param;
   const cs_turb_model_t  *model = tbp->model;
 
-  if (model->iturb == CS_TURB_NONE)
+  if (model->type == CS_TURB_NONE)
     return; /* Nothing to do if there is a laminar flow */
 
   /* Set field metadata */
@@ -332,7 +333,7 @@ cs_turbulence_init_setup(cs_turbulence_t   *turb)
     turb->init_context = cs_turb_init_k_eps_context;
     turb->free_context = cs_turb_free_k_eps_context;
     turb->compute = cs_turb_compute_k_eps;
-    turb->update = NULL; /* TBD */
+    turb->update = cs_turb_update_k_eps;
 
     turb->context = turb->init_context(model);
     break;
@@ -380,7 +381,7 @@ cs_turbulence_finalize_setup(const cs_mesh_t            *mesh,
   const cs_turbulence_param_t  *tbp = tbs->param;
   const cs_turb_model_t  *model = tbp->model;
 
-  if (model->iturb == CS_TURB_NONE)
+  if (model->type == CS_TURB_NONE)
     return; /* Nothing to do */
 
   /* Define the property related to the total viscosity */
@@ -520,12 +521,12 @@ cs_turb_init_k_eps_context(const cs_turb_model_t      *tbm)
 
   BFT_MALLOC(kec, 1, cs_turb_context_k_eps_t);
 
-  /* Add new equations for the tubulent kinetic energy (tke) and the dissipation
+  /* Add new equations for the turbulent kinetic energy (tke) and the dissipation
      (epsilon) */
 
   kec->tke = cs_equation_add("k", /* equation name */
                              "k", /* variable name */
-                             CS_EQUATION_TYPE_NAVSTO,
+                             CS_EQUATION_TYPE_NAVSTO, /* related to NS */
                              1,
                              CS_PARAM_BC_HMG_NEUMANN);
 
@@ -557,10 +558,10 @@ cs_turb_init_k_eps_context(const cs_turb_model_t      *tbm)
   /* Reaction (implicit source terms) coefficients */
 
   kec->tke_reaction = cs_property_add("k_reaction",
-                                                CS_PROPERTY_ISO);
+                                      CS_PROPERTY_ISO);
 
   kec->eps_reaction = cs_property_add("epsilon_reaction",
-                                                CS_PROPERTY_ISO);
+                                      CS_PROPERTY_ISO);
 
   /* Retrieve the mass density */
 
@@ -591,8 +592,8 @@ cs_turb_init_k_eps_context(const cs_turb_model_t      *tbm)
 
   cs_equation_add_time(eps_eqp, mass_density);
   cs_equation_add_diffusion(eps_eqp, kec->eps_diffusivity);
-  cs_equation_add_reaction(tke_eqp, kec->tke_reaction);
-  cs_equation_add_advection(tke_eqp, adv);
+  cs_equation_add_reaction(eps_eqp, kec->eps_reaction);
+  cs_equation_add_advection(eps_eqp, adv);
 
   return kec;
 }
@@ -622,6 +623,34 @@ cs_turb_free_k_eps_context(void     *tbc)
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief  Update for the current time step the new state for the turbulence
+ *         model. This is used to update the turbulent viscosity.
+ *
+ * \param[in]      mesh      pointer to a \ref cs_mesh_t structure
+ * \param[in]      connect   pointer to a cs_cdo_connect_t structure
+ * \param[in]      cdoq      pointer to a cs_cdo_quantities_t structure
+ * \param[in]      tbp       pointer to a \ref cs_turbulence_param_t structure
+ * \param[in]      tbp       pointer to a \ref cs_navsto_param_t structure
+ * \param[in, out] tbc       pointer to a structure cast on-the-fly
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_turb_update_k_eps(const cs_mesh_t              *mesh,
+                     const cs_time_step_t         *time_step,
+                     const cs_cdo_connect_t       *connect,
+                     const cs_cdo_quantities_t    *cdoq,
+                     const cs_turbulence_param_t  *tpb,
+                     void                         *tbc)
+{
+  if (tbc == NULL)
+    return;
+
+  //TODO
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief  Compute for the current time step the new state for the turbulence
  *         model. This means that all related equations are built and then
  *         solved.
@@ -639,8 +668,9 @@ cs_turb_compute_k_eps(const cs_mesh_t              *mesh,
 {
   if (tbc == NULL)
     return;
-}
 
+  //TODO
+}
 /*----------------------------------------------------------------------------*/
 
 END_C_DECLS

src/cdo/cs_navsto_param.h
@@ -568,7 +568,7 @@ typedef struct {
    * @{
    */
   /*! \var phys_constants
-   * Main physical constants (gravity vector and coriolis source term). This
+   * Main physical constants (gravity vector and Coriolis source term). This
    * structure is shared with the legacy part.
    */
   cs_physical_constants_t       *phys_constants;

src/cdo/cs_navsto_system.c
@@ -1238,15 +1238,22 @@ cs_navsto_system_update(const cs_mesh_t             *mesh,
                         const cs_cdo_quantities_t   *cdoq)
 {
   CS_UNUSED(mesh);
-  CS_UNUSED(time_step);
-  CS_UNUSED(connect);
-  CS_UNUSED(cdoq);
 
   cs_navsto_system_t  *ns = cs_navsto_system;
 
   if (ns == NULL) bft_error(__FILE__, __LINE__, 0, _(_err_empty_ns));
 
-  /* Nothing to do up to now */
+  /* Update quantities relative to turbulence (e.g. turbulence
+   * viscosity...) */
+  cs_turbulence_t  *turb = ns->turbulence;
+  if (turb->update != NULL)
+    turb->update(mesh,
+                 time_step,
+                 connect,
+                 cdoq,
+                 turb->param,
+                 turb->context);
+
 }
 
 /*----------------------------------------------------------------------------*/
@@ -1275,6 +1282,11 @@ cs_navsto_system_compute_steady_state(const cs_mesh_t             *mesh,
   if (!cs_navsto_param_is_steady(nsp))
     return;
 
+  cs_turbulence_t  *turb = ns->turbulence;
+
+  /* Update variable, properties according to the computed variables */
+  cs_navsto_system_update(mesh, time_step, connect, cdoq);
+
   /* First resolve the thermal system if needed */
   cs_equation_t  *th_eq = cs_thermal_system_get_equation();
 
@@ -1284,6 +1296,12 @@ cs_navsto_system_compute_steady_state(const cs_mesh_t             *mesh,
 
     /* Build and solve the Navier-Stokes system */
     ns->compute_steady(mesh, nsp, ns->scheme_context);
+
+    /* Build and solve the turbulence variable system */
+    if (turb->compute_steady != NULL)
+      turb->compute_steady(mesh,
+                           turb->param,
+                           turb->context);
 
     /* Solve the thermal equation */
     if (cs_equation_param_has_time(cs_equation_get_param(th_eq)) == false)
@@ -1310,7 +1328,7 @@ cs_navsto_system_compute_steady_state(const cs_mesh_t             *mesh,
 
     cs_real_t  inv_tref = cs_thermal_system_get_reference_temperature();
     if (fabs(inv_tref) > cs_math_zero_threshold)
-      inv_tref = 1/inv_tref;
+      inv_tref = 1./inv_tref;
     else
       inv_tref = 1.;
 
@@ -1324,6 +1342,12 @@ cs_navsto_system_compute_steady_state(const cs_mesh_t             *mesh,
 
       /* Build and solve the Navier-Stokes system */
       ns->compute_steady(mesh, nsp, ns->scheme_context);
+
+      /* Build and solve the turbulence variable system */
+      if (turb->compute_steady != NULL)
+        turb->compute_steady(mesh,
+                             turb->param,
+                             turb->context);
 
       /* Check convergence */
       cs_real_t  delta = -1;
@@ -1359,10 +1383,16 @@ cs_navsto_system_compute_steady_state(const cs_mesh_t             *mesh,
     /* Build and solve the Navier-Stokes system */
     ns->compute_steady(mesh, nsp, ns->scheme_context);
 
+    /* Build and solve the turbulence variable system
+     * TODO, we should loop over NS+turb and add a
+     * stopping criteria on the NS matrix for instance
+     * */
+    if (turb->compute_steady != NULL)
+      turb->compute_steady(mesh,
+                           turb->param,
+                           turb->context);
   }
 
-  /* Update variable, properties according to the new computed variables */
-  cs_navsto_system_update(mesh, time_step, connect, cdoq);
 }
 
 /*----------------------------------------------------------------------------*/
@@ -1388,6 +1418,8 @@ cs_navsto_system_compute(const cs_mesh_t             *mesh,
 
   const cs_navsto_param_t  *nsp = ns->param;
 
+  cs_turbulence_t  *turb = ns->turbulence;
+
   if (nsp->model_flag & CS_NAVSTO_MODEL_PASSIVE_THERMAL_TRACER) {
 
     /* First resolve the thermal system if needed */
@@ -1396,8 +1428,16 @@ cs_navsto_system_compute(const cs_mesh_t             *mesh,
     assert(th_eq != NULL);
 
     /* Build and solve the Navier-Stokes system */
-    if (cs_navsto_param_is_steady(nsp) == false)
+    if (cs_navsto_param_is_steady(nsp) == false) {
       ns->compute(mesh, nsp, ns->scheme_context);
+
+      /* Build and solve the turbulence variable system */
+      if (turb->compute != NULL)
+        turb->compute(mesh,
+                      turb->param,
+                      turb->context);
+
+    }
 
     /* Solve the thermal equation */
     if (cs_equation_param_has_time(cs_equation_get_param(th_eq)))
@@ -1424,6 +1464,13 @@ cs_navsto_system_compute(const cs_mesh_t             *mesh,
       /* Build and solve the Navier-Stokes system */
       ns->compute(mesh, nsp, ns->scheme_context);
 
+      /* Build and solve the turbulence variable system */
+      if (turb->compute != NULL)
+        turb->compute(mesh,
+                      turb->param,
+                      turb->context);
+
+
     }
     else { /* Thermal system is declared as steady. So, this is equivalent to a
               standard Navier-Stokes iteration */
@@ -1441,6 +1488,13 @@ cs_navsto_system_compute(const cs_mesh_t             *mesh,
 
     /* Build and solve the Navier-Stokes system */
     ns->compute(mesh, nsp, ns->scheme_context);
+
+    /* Build and solve the turbulence variable system */
+    if (turb->compute != NULL)
+      turb->compute(mesh,
+                    turb->param,
+                    turb->context);
+
 
   }
 

commit e5ac80e9650db2a628224e1b923c5a1461426438

Fix compilation broken by commit bca4fa26 (today)

src/cdo/cs_cdo_turbulence.h
@@ -399,6 +399,28 @@ cs_turb_free_k_eps_context(void     *tbc);
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief  Update for the current time step the new state for the turbulence
+ *         model. This is used to update the turbulent viscosity.
+ *
+ * \param[in]      mesh      pointer to a \ref cs_mesh_t structure
+ * \param[in]      connect   pointer to a cs_cdo_connect_t structure
+ * \param[in]      cdoq      pointer to a cs_cdo_quantities_t structure
+ * \param[in]      tbp       pointer to a \ref cs_turbulence_param_t structure
+ * \param[in]      tbp       pointer to a \ref cs_navsto_param_t structure
+ * \param[in, out] tbc       pointer to a structure cast on-the-fly
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_turb_update_k_eps(const cs_mesh_t              *mesh,
+                     const cs_time_step_t         *time_step,
+                     const cs_cdo_connect_t       *connect,
+                     const cs_cdo_quantities_t    *cdoq,
+                     const cs_turbulence_param_t  *tpb,
+                     void                         *tbc);
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief  Compute for the current time step the new state for the turbulence
  *         model. This means that all related equations are built and then
  *         solved.