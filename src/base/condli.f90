code-saturne/code_saturne

commit a2d7d7833df07a835b58ffcf6ac5a3efe28e7cc9

Move simpled mapped inlet handling from GUI to cs_boundary_conditions API.
This is necessary as mapped inlets should be handled after the general
GUI and xdef-based boundary conditions are set.

It is also now possible to used this outside the GUI.

src/base/cs_boundary_conditions.c
@@ -59,6 +59,7 @@
 #include "cs_field.h"
 #include "cs_field_default.h"
 #include "cs_field_operator.h"
+#include "cs_field_pointer.h"
 #include "cs_flag_check.h"
 #include "cs_halo.h"
 #include "cs_math.h"
@@ -101,6 +102,18 @@ BEGIN_C_DECLS
  * Local Type Definitions
  *============================================================================*/
 
+typedef struct {
+
+  int             bc_location_id;      /* location id of boundary zone */
+  int             source_location_id;  /* location if of source elements */
+  cs_real_t       coord_shift[3];      /* coordinates shift relative to
+                                          selected boundary faces */
+  double          tolerance;           /* search tolerance */
+
+  ple_locator_t  *locator;             /* associated locator */
+
+} cs_bc_map_t;
+
 /*----------------------------------------------------------------------------
  * Local Structure Definitions
  *----------------------------------------------------------------------------*/
@@ -116,6 +129,9 @@ const int *cs_glob_bc_type = NULL;
 static int *_bc_face_zone;
 
 const int *cs_glob_bc_face_zone = NULL;
+
+static int _n_bc_maps = 0;
+static cs_bc_map_t *_bc_maps = NULL;
 
 /*============================================================================
  * Prototypes for functions intended for use only by Fortran wrappers.
@@ -141,6 +157,53 @@ cs_f_boundary_conditions_get_pointers(int  **itypfb,
 /*============================================================================
  * Private function definitions
  *============================================================================*/
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Build or update map of shifted boundary face coordinates on
+ *        cells or boundary faces for automatic interpolation.
+ *
+ * \warning
+ *
+ * This function does not currently update the location in case of moving
+ * meshes. It could be modifed to do so.
+ *
+ * \param[in]  map_id      id of defined map
+ */
+/*----------------------------------------------------------------------------*/
+
+static void
+_update_bc_map(int  map_id)
+{
+  assert(map_id > -1 && map_id < _n_bc_maps);
+
+  cs_bc_map_t *bc_map = _bc_maps + map_id;
+
+  if (bc_map->locator != NULL)
+    return;
+
+  cs_mesh_location_type_t location_type
+    = cs_mesh_location_get_type(bc_map->source_location_id);
+
+  cs_lnum_t n_location_elts
+    = cs_mesh_location_get_n_elts(bc_map->source_location_id)[0];
+  cs_lnum_t n_faces
+    = cs_mesh_location_get_n_elts(bc_map->bc_location_id)[0];
+
+  const cs_lnum_t *location_elts
+    = cs_mesh_location_get_elt_ids_try(bc_map->source_location_id);
+  const cs_lnum_t *faces
+    = cs_mesh_location_get_elt_ids_try(bc_map->bc_location_id);
+
+  bc_map->locator = cs_boundary_conditions_map(location_type,
+                                               n_location_elts,
+                                               n_faces,
+                                               location_elts,
+                                               faces,
+                                               &(bc_map->coord_shift),
+                                               0,
+                                               bc_map->tolerance);
+}
 
 /*----------------------------------------------------------------------------
  * Compute balance at inlet
@@ -882,7 +945,7 @@ cs_boundary_conditions_mapped_set(const cs_field_t          *f,
   assert(sizeof(ple_coord_t) == sizeof(cs_real_t));
 
   if (location_type == CS_MESH_LOCATION_CELLS || interpolate) {
-    /* FIXME: we cheat here with the constedness of the field relative to
+    /* FIXME: we cheat here with the constedness of the field
        for a possible ghost values update, but having a finer control
        of when syncing is required would be preferable */
     cs_field_t *_f = cs_field_by_id(f->id);
@@ -899,7 +962,7 @@ cs_boundary_conditions_mapped_set(const cs_field_t          *f,
     const cs_lnum_t *b_face_cells = cs_glob_mesh->b_face_cells;
     const cs_field_bc_coeffs_t   *bc_coeffs = f->bc_coeffs;
 
-    /* If no boundary condition coefficients are available */
+    /* If boundary condition coefficients are available */
 
     if (bc_coeffs != NULL) {
 
@@ -1000,7 +1063,60 @@ cs_boundary_conditions_mapped_set(const cs_field_t          *f,
 
 /*----------------------------------------------------------------------------*/
 /*!
- * \brief Create the legacy boundary conditions face type and face zone arrays
+ * \brief Add location of locate shifted boundary face coordinates on
+ *        cells or boundary faces for automatic interpolation.
+ *
+ * \note
+ * This function is currently restricted to mapping of boundary face
+ * locations (usually from boundary zones) to cell of boundary face
+ * locations, but could be extended to other location types in the future.
+ *
+ * \param[in]  bc_location_id      id of selected boundary mesh location;
+ *                                 currently restricted to subsets of
+ *                                 boundary faces (i.e. boundary zone
+ *                                 location ids).
+ * \param[in]  source_location_id  id of selected location  mesh location
+ *                                 (usually CS_MESH_LOCATION_CELLS but can be
+ *                                 a more restricted cell or boundary face zone
+ *                                 location location id).
+ * \param[in]  coord_shift      coordinates shift relative to selected
+ *                              boundary faces
+ * \param[in]  tolerance        relative tolerance for point location.
+ *
+ * \return  id of added map
+ */
+/*----------------------------------------------------------------------------*/
+
+int
+cs_boundary_conditions_add_map(int         bc_location_id,
+                               int         source_location_id,
+                               cs_real_t   coord_shift[3],
+                               double      tolerance)
+{
+  int map_id = _n_bc_maps;
+
+  BFT_REALLOC(_bc_maps, _n_bc_maps+1, cs_bc_map_t);
+
+  cs_bc_map_t *bc_map = _bc_maps + _n_bc_maps;
+
+  bc_map->bc_location_id = bc_location_id;
+  bc_map->source_location_id = source_location_id;
+
+  for (int i = 0; i < 3; i++)
+    bc_map->coord_shift[i] = coord_shift[i];
+
+  bc_map->tolerance = tolerance;
+
+  bc_map->locator = NULL;
+
+  _n_bc_maps += 1;
+
+  return map_id;
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Create the legacy boundary conditions face type and face zone arrays.
  */
 /*----------------------------------------------------------------------------*/
 
@@ -1036,15 +1152,25 @@ cs_boundary_conditions_create(void)
   cs_glob_bc_face_zone = _bc_face_zone;
 }
 
-/*----------------------------------------------------------------------------
- * Free the boundary conditions face type and face zone arrays
- *----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Free the boundary conditions face type and face zone arrays.
+ *
+ * This also frees boundary condition mappings which may have been defined.
+ */
+/*----------------------------------------------------------------------------*/
 
 void
 cs_boundary_conditions_free(void)
 {
   BFT_FREE(_bc_type);
   BFT_FREE(_bc_face_zone);
+
+  for (int i = 0; i < _n_bc_maps; i++)
+    ple_locator_destroy((_bc_maps + i)->locator);
+
+  BFT_FREE(_bc_maps);
+  _n_bc_maps = 0;
 }
 
 /*----------------------------------------------------------------------------*/
@@ -1098,6 +1224,11 @@ cs_boundary_conditions_compute(int         nvar,
 {
   /* Initialization */
 
+  const cs_time_step_t *ts = cs_glob_time_step;
+
+  for (int map_id = 0; map_id < _n_bc_maps; map_id++)
+    _update_bc_map(map_id);
+
   static int var_id_key = -1;
   if (var_id_key < 0)
     var_id_key = cs_field_key_id("variable_id");
@@ -1105,7 +1236,7 @@ cs_boundary_conditions_compute(int         nvar,
 
   const cs_mesh_t *mesh = cs_glob_mesh;
   const cs_boundary_t *boundaries = cs_glob_boundaries;
-  const cs_real_t t_eval = cs_glob_time_step->t_cur;
+  const cs_real_t t_eval = ts->t_cur;
 
   /* Loop on fields */
 
@@ -1183,6 +1314,48 @@ cs_boundary_conditions_compute(int         nvar,
         break;
       }
     }
+
+    /* Treatment of mapped inlets */
+
+    for (int map_id = 0; map_id < _n_bc_maps; map_id++) {
+
+      cs_bc_map_t *bc_map = _bc_maps + map_id;
+
+      if (bc_map->locator == NULL || ts->nt_cur <= 1)
+        continue;
+
+      int interpolate = 0;
+      int normalize = 0;
+      if (f == CS_F_(vel))
+        normalize = 1;
+      else {
+        const int keysca = cs_field_key_id("scalar_id");
+        if (cs_field_get_key_int(f, keysca) > 0)
+          normalize = 1;
+      }
+
+      if (f != CS_F_(p)) {
+        cs_mesh_location_type_t location_type
+          = cs_mesh_location_get_type(bc_map->source_location_id);
+        cs_lnum_t n_faces
+          = cs_mesh_location_get_n_elts(bc_map->bc_location_id)[0];
+        const cs_lnum_t *faces
+          = cs_mesh_location_get_elt_ids_try(bc_map->bc_location_id);
+
+        cs_boundary_conditions_mapped_set(f,
+                                          bc_map->locator,
+                                          location_type,
+                                          normalize,
+                                          interpolate,
+                                          n_faces,
+                                          faces,
+                                          NULL,
+                                          nvar,
+                                          rcodcl);
+      }
+
+    }
+
   }
 }
 

src/base/cs_boundary_conditions.h
@@ -178,6 +178,38 @@ cs_boundary_conditions_mapped_set(const cs_field_t          *f,
                                   int                        nvar,
                                   cs_real_t                  rcodcl[]);
 
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Add location of locate shifted boundary face coordinates on
+ *        cells or boundary faces for automatic interpolation.
+ *
+ * \note
+ * This function is currently restricted to mapping of boundary face
+ * locations (usually from boundary zones) to cell of boundary face
+ * locations, but could be extended to other location types in the future.
+ *
+ * \param[in]  bc_location_id      id of selected boundary mesh location;
+ *                                 currently restricted to subsets of
+ *                                 boundary faces (i.e. boundary zone
+ *                                 location ids).
+ * \param[in]  source_location_id  id of selected location  mesh location
+ *                                 (usually CS_MESH_LOCATION_CELLS but can be
+ *                                 a more restricted cell or boundary face zone
+ *                                 location location id).
+ * \param[in]  coord_shift      coordinates shift relative to selected
+ *                              boundary faces
+ * \param[in]  tolerance        relative tolerance for point location.
+ *
+ * \return  id of added map
+ */
+/*----------------------------------------------------------------------------*/
+
+int
+cs_boundary_conditions_add_map(int         bc_location_id,
+                               int         source_location_id,
+                               cs_real_t   coord_shift[3],
+                               double      tolerance);
+
 /*----------------------------------------------------------------------------
  * Create the boundary conditions face type and face zone arrays
  *----------------------------------------------------------------------------*/
@@ -186,7 +218,9 @@ void
 cs_boundary_conditions_create(void);
 
 /*----------------------------------------------------------------------------
- * Free the boundary conditions face type and face zone arrays
+ * Free the boundary conditions face type and face zone arrays.
+ *
+ * This also frees boundary condition mappings which may have been defined.
  *----------------------------------------------------------------------------*/
 
 void

src/gui/cs_gui_boundary_conditions.c
@@ -167,8 +167,6 @@ typedef struct {
 
   cs_meteo_t    *meteo;     /* inlet or outlet info for atmospheric flow */
 
-  ple_locator_t **locator;   /* locator for mapped inlet */
-
 } cs_gui_boundary_t;
 
 /*============================================================================
@@ -288,26 +286,18 @@ _boundary_status_vp(const char  *nature,
 }
 
 /*-----------------------------------------------------------------------------
- * Check if a zone uses a mapped inlet, and return the locator from the
- * associated mapping if this is the case.
+ * Check if a zone uses a mapped inlet, and define the associated mapping
+ * if this is the case.
  *
  * parameters:
  *   label    <-- label of wall boundary condition
- *   n_faces  <-- number of selected boundary faces
- *   faces    <-- list of selected boundary faces (0 to n-1),
- *                or NULL if no indirection is needed
- *
- * returns:
- *   pointer to mapping locator, or NULL
+ *    z       <-- pointer to boundary zone
  *----------------------------------------------------------------------------*/
 
-static  ple_locator_t *
-_mapped_inlet(const char                *label,
-              cs_lnum_t                  n_faces,
-              const cs_lnum_t           *faces)
+static void
+_check_and_add_mapped_inlet(const char       *label,
+                            const cs_zone_t  *z)
 {
-  ple_locator_t *bl = NULL;
-
   int mapped_inlet = 0;
 
   cs_tree_node_t *tn
@@ -325,9 +315,7 @@ _mapped_inlet(const char                *label,
                            "translation_z"};
 
     for (int i = 0; i < 3; i++) {
-
-      cs_tree_node_t *node = NULL;
-      node = cs_tree_get_node(tn, tname[i]);
+      cs_tree_node_t *node = cs_tree_get_node(tn, tname[i]);
 
       const  cs_real_t *v = NULL;
       v = cs_tree_node_get_values_real(node);
@@ -335,17 +323,11 @@ _mapped_inlet(const char                *label,
         coord_shift[i] = v[0];
     }
 
-    bl = cs_boundary_conditions_map(CS_MESH_LOCATION_CELLS,
-                                    cs_glob_mesh->n_cells,
-                                    n_faces,
-                                    NULL,
-                                    faces,
-                                    &coord_shift,
-                                    0,
-                                    0.1);
+    cs_boundary_conditions_add_map(z->location_id,
+                                   CS_MESH_LOCATION_CELLS,
+                                   coord_shift,
+                                   0.1);
   }
-
-  return bl;
 }
 
 /*-----------------------------------------------------------------------------
@@ -993,7 +975,6 @@ _init_boundaries(void)
   BFT_MALLOC(boundaries->rough,     n_zones,    double);
   BFT_MALLOC(boundaries->norm,      n_zones,    double);
   BFT_MALLOC(boundaries->dir,       n_zones,    cs_real_3_t);
-  BFT_MALLOC(boundaries->locator,   n_zones,    ple_locator_t *);
 
   BFT_MALLOC(boundaries->velocity_e,  n_zones,  bool);
   BFT_MALLOC(boundaries->direction_e, n_zones,  bool);
@@ -1071,7 +1052,6 @@ _init_boundaries(void)
     boundaries->velocity_e[izone]  = false;
     boundaries->direction_e[izone] = false;
     boundaries->head_loss_e[izone] = false;
-    boundaries->locator[izone]   = NULL;
 
     if (solid_fuels) {
       const cs_combustion_model_t *cm = cs_glob_combustion_model;
@@ -1207,6 +1187,8 @@ _init_boundaries(void)
         continue;
 
     if (cs_gui_strcmp(nature, "inlet")) {
+
+      _check_and_add_mapped_inlet(label, z);
 
       cs_tree_node_t *tn_vp
         = cs_tree_node_get_child(tn, "velocity_pressure");
@@ -1580,7 +1562,6 @@ void CS_PROCF (uiclim, UICLIM)(const int  *nozppm,
   const cs_mesh_quantities_t *mq = cs_glob_mesh_quantities;
   const cs_real_t   *b_face_surf = mq->b_face_surf;
   const cs_real_3_t *b_face_normal = (const cs_real_3_t *)mq->b_face_normal;
-  const cs_time_step_t *ts = cs_glob_time_step;
   const int n_fields = cs_field_n_fields();
 
   const int ncharm = CS_COMBUSTION_MAX_COALS;
@@ -1632,12 +1613,7 @@ void CS_PROCF (uiclim, UICLIM)(const int  *nozppm,
 
     /* Mapped inlet? */
 
-    if (   cs_gui_strcmp(boundaries->nature[izone], "inlet")
-        && boundaries->locator[izone] == NULL)
-      boundaries->locator[izone] = _mapped_inlet(boundaries->label[izone],
-                                                 bz->n_elts,
-                                                 bz->elt_ids);
-    else if (cs_gui_strcmp(boundaries->nature[izone], "wall")) {
+    if (cs_gui_strcmp(boundaries->nature[izone], "wall")) {
       if (boundaries->rough[izone] >= 0.0)
         wall_type = 6;//TODO remove and use all roughness wall function
       else
@@ -2534,35 +2510,6 @@ void CS_PROCF (uiclim, UICLIM)(const int  *nozppm,
                 boundaries->nature[izone]);
     }
 
-    /* treatment of mapped inlet for each field */
-
-    if (boundaries->locator[izone] != NULL && ts->nt_cur > 1) {
-      icalke[zone_nbr-1] = 0;
-
-      for (int f_id = 0; f_id < n_fields; f_id++) {
-        const cs_field_t  *f = cs_field_by_id(f_id);
-
-        if (f->type & CS_FIELD_VARIABLE) {
-          int interpolate = 0;
-          int normalize = 0;
-          if (f == CS_F_(vel))
-            normalize = 1;
-          else {
-            const int keysca = cs_field_key_id("scalar_id");
-            if (cs_field_get_key_int(f, keysca) > 0)
-              normalize = 1;
-          }
-          if (f != CS_F_(p))
-            cs_boundary_conditions_mapped_set(f, boundaries->locator[izone],
-                                              CS_MESH_LOCATION_CELLS,
-                                              normalize, interpolate,
-                                              bz->n_elts, bz->elt_ids,
-                                              NULL, *nvar, rcodcl);
-        }
-
-      }
-    }
-
 #if _XML_DEBUG_
     if (bz->n_elts > 0) {
       cs_lnum_t face_id = bz->elt_ids[0];
@@ -2592,7 +2539,6 @@ void CS_PROCF (uiclim, UICLIM)(const int  *nozppm,
                                  itypfb,
                                  icodcl,
                                  rcodcl);
-
 }
 
 /*----------------------------------------------------------------------------
@@ -3025,12 +2971,6 @@ cs_gui_boundary_conditions_free_memory(void)
     if (cs_glob_physical_model_flag[CS_ATMOSPHERIC] > -1)
       BFT_FREE(boundaries->meteo);
 
-    for (izone = 0; izone < n_zones; izone++) {
-      if (boundaries->locator[izone] != NULL)
-        boundaries->locator[izone]
-          = ple_locator_destroy(boundaries->locator[izone]);
-    }
-
     BFT_FREE(boundaries->label);
     BFT_FREE(boundaries->nature);
     BFT_FREE(boundaries->bc_num);
@@ -3049,7 +2989,6 @@ cs_gui_boundary_conditions_free_memory(void)
     BFT_FREE(boundaries->direction_e);
     BFT_FREE(boundaries->scalar_e);
     BFT_FREE(boundaries->head_loss_e);
-    BFT_FREE(boundaries->locator);
 
     BFT_FREE(boundaries);
   }

commit ceea1c494c7e1cd3cbca64acd0633ea2851a533b

Fix mapped inlet for turbulence (broken by commit a2d7d783 (Nov 13 2020).
Cherry picked from commit 400fecea on master.

src/base/condli.f90
@@ -342,6 +342,17 @@ interface
 
   end subroutine clsyvt
 
+  subroutine cs_boundary_conditions_complete(nvar, itypfb, icodcl, rcodcl) &
+    bind(C, name='cs_boundary_conditions_complete')
+
+    use, intrinsic :: iso_c_binding
+    implicit none
+    integer(c_int), value :: nvar
+    integer(c_int), dimension(*), intent(inout) :: itypfb, icodcl
+    real(kind=c_double), dimension(*), intent(inout) :: rcodcl
+
+  end subroutine cs_boundary_conditions_complete
+
   subroutine cs_syr_coupling_recv_boundary(nvar, bc_type, icodcl, rcodcl) &
     bind(C, name = 'cs_syr_coupling_recv_boundary')
 
@@ -409,42 +420,31 @@ if (ippmod(iphpar).eq.0.or.ippmod(igmix).ge.0.or.ippmod(icompf).ge.0) then
 
 endif
 
-!     - Sous-programme utilisateur
-!       ==========================
+call cs_boundary_conditions_complete(nvar, itypfb, icodcl, rcodcl)
+
+! User-defined functions
+! ==========================
 
 call cs_f_user_boundary_conditions &
-  ( nvar   , nscal  ,                                              &
-  icodcl , itrifb , itypfb , izfppp ,                            &
-  dt     ,                                                       &
-  rcodcl )
+  (nvar, nscal, icodcl, itrifb, itypfb, izfppp, dt, rcodcl )
 
 call user_boundary_conditions(nvar, itypfb, icodcl, rcodcl)
 
-!     - Interface Code_Saturne
-!       ======================
+! Check consistency with GUI definitions
 
 call uiclve(nozppm, itypfb, izfppp)
 
-! --- Couplage code/code entre deux instances (ou plus) de Code_Saturne
-!       On s'occupe ici du couplage via les faces de bord, et de la
-!       transformation de l'information recue en condition limite.
+! BC'based coupling with other code_saturne instances.
 
 if (nbrcpl.gt.0) then
-
-  call cscfbr &
-    ( nscal  ,                                                       &
-    icodcl , itypfb ,                                              &
-    dt     ,                                                       &
-    rcodcl )
-
+  call cscfbr(nscal, icodcl, itypfb, dt, rcodcl)
 endif
 
-! -- Synthetic Eddy Method en L.E.S. :
-!    (Transfert des structures dans les tableaux rcodcl)
+! Synthetic Eddy Method for L.E.S.
 
 call synthe(ttcabs, dt, rcodcl)
 
-! -- Methode ALE (CL de vitesse de maillage et deplacement aux noeuds)
+! ALE method (mesh velocity BC and vertices displacement)
 
 if (iale.ge.1) then
 
@@ -458,23 +458,17 @@ if (iale.ge.1) then
   ! - Interface Code_Saturne
   !   ======================
 
-  call uialcl &
-    ( ibfixe, igliss, ivimpo, ifresf,    &
-      ialtyb,                            &
-      impale,                            &
-      disale,                            &
-      iuma, ivma, iwma,                  &
-      rcodcl)
+  call uialcl(ibfixe, igliss, ivimpo, ifresf,   &
+             ialtyb, impale, disale,            &
+             iuma, ivma, iwma,                  &
+             rcodcl)
 
-  ! TODO in the future version: remove xyzno0, and disale because they are fields
+  ! TODO in the future version: remove dt, xyzno0, and disale
+  ! because they are avaliable as fields.
 
-  call usalcl &
-    ( itrale ,                                                       &
-    nvar   , nscal  ,                                              &
-    icodcl , itypfb , ialtyb ,                                     &
-    impale ,                                                       &
-    dt     ,                                                       &
-    rcodcl , xyzno0 , disale )
+  call usalcl(itrale, nvar, nscal,              &
+              icodcl, itypfb, ialtyb, impale,   &
+              dt, rcodcl, xyzno0, disale)
 
   !     Au cas ou l'utilisateur aurait touche disale sans mettre impale=1, on
   !     remet le deplacement initial

src/base/cs_boundary_conditions.c
@@ -105,7 +105,7 @@ BEGIN_C_DECLS
 typedef struct {
 
   int             bc_location_id;      /* location id of boundary zone */
-  int             source_location_id;  /* location if of source elements */
+  int             source_location_id;  /* location id of source elements */
   cs_real_t       coord_shift[3];      /* coordinates shift relative to
                                           selected boundary faces */
   double          tolerance;           /* search tolerance */
@@ -1227,10 +1227,10 @@ cs_boundary_conditions_set_convective_outlet_scalar(cs_real_t *coefa ,
 /*----------------------------------------------------------------------------*/
 
 void
-cs_boundary_conditions_compute(int         nvar,
-                               int        *itypfb,
-                               int        *icodcl,
-                               double     *rcodcl)
+cs_boundary_conditions_compute(int     nvar,
+                               int     itypfb[],
+                               int     icodcl[],
+                               double  rcodcl[])
 {
   /* Initialization */
 
@@ -1338,6 +1338,76 @@ cs_boundary_conditions_compute(int         nvar,
       }
     }
 
+  }
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Automatic adjustments for boundary condition codes.
+ *
+ * Currently handles mapped inlets, after the call to \ref stdtcl.
+ * As portions of stdtcl are migrated to C, they should be called here,
+ * before mapped inlets.
+ *
+ * \param[in]       nvar             number of variables requiring BC's
+ * \param[in]       itypfb           type of boundary for each face
+ * \param[in, out]  icodcl           boundary condition codes
+ * \param[in, out]  rcodcl           boundary condition values
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_boundary_conditions_complete(int     nvar,
+                                int     itypfb[],
+                                int     icodcl[],
+                                double  rcodcl[])
+{
+  CS_NO_WARN_IF_UNUSED(itypfb);
+
+  /* Initialization */
+
+  const cs_time_step_t *ts = cs_glob_time_step;
+
+  for (int map_id = 0; map_id < _n_bc_maps; map_id++)
+    _update_bc_map(map_id);
+
+  static int var_id_key = -1;
+  if (var_id_key < 0)
+    var_id_key = cs_field_key_id("variable_id");
+  assert(var_id_key >= 0);
+
+  const cs_real_t t_eval = ts->t_cur;
+
+  /* Loop on fields */
+
+  const int n_fields = cs_field_n_fields();
+
+  for (int f_id = 0; f_id < n_fields; f_id++) {
+    cs_field_t  *f = cs_field_by_id(f_id);
+
+    if (! (f->type & CS_FIELD_VARIABLE))
+      continue;
+
+    const cs_equation_param_t *eqp
+      = cs_field_get_equation_param_const(f);
+
+    /* Only handle legacy discretization here */
+    if (eqp != NULL) {
+      if (eqp->space_scheme != CS_SPACE_SCHEME_LEGACY)
+        continue;
+    }
+
+    /* Settings in eqp may not be well-defined at this stage. The following
+       test should be more appropriate to decide if one skips this field or
+       not */
+    if (f->type & CS_FIELD_CDO)
+      continue;
+
+    /* Get associated variable id  */
+
+    int var_id = cs_field_get_key_int(f, var_id_key) - 1;
+    assert(var_id >= 0);
+
     /* Treatment of mapped inlets */
 
     for (int map_id = 0; map_id < _n_bc_maps; map_id++) {
@@ -1375,6 +1445,18 @@ cs_boundary_conditions_compute(int         nvar,
                                           NULL,
                                           nvar,
                                           rcodcl);
+
+        if (f == CS_F_(h)) {
+          const cs_lnum_t n_b_faces = cs_glob_mesh->n_b_faces;
+          int  *_icodcl = rcodcl + var_id*n_b_faces;
+
+          for (cs_lnum_t i = 0; i < n_faces; i++) {
+            const cs_lnum_t j = (faces != NULL) ? faces[i] : i;
+            if (_icodcl[j] < 0)
+              _icodcl[j] *= -1;
+          }
+        }
+
       }
 
     }

src/base/cs_boundary_conditions.h
@@ -462,10 +462,31 @@ cs_boundary_conditions_set_dirichlet_vector_aniso(cs_real_3_t    a,
 /*----------------------------------------------------------------------------*/
 
 void
-cs_boundary_conditions_compute(int         nvar,
-                               int        *itypfb,
-                               int        *icodcl,
-                               double     *rcodcl);
+cs_boundary_conditions_compute(int     nvar,
+                               int     itypfb[],
+                               int     icodcl[],
+                               double  rcodcl[]);
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Automatic adjustments for boundary condition codes.
+ *
+ * Currently handles mapped inlets, after the call to \ref stdtcl.
+ * As portions of stdtcl are migrated to C, they should be called here,
+ * before mapped inlets.
+ *
+ * \param[in]       nvar             number of variables requiring BC's
+ * \param[in]       itypfb           type of boundary for each face
+ * \param[in, out]  icodcl           boundary condition codes
+ * \param[in, out]  rcodcl           boundary condition values
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_boundary_conditions_complete(int     nvar,
+                                int     itypfb[],
+                                int     icodcl[],
+                                double  rcodcl[]);
 
 /*----------------------------------------------------------------------------*/
 