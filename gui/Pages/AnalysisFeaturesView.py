code-saturne/code_saturne

commit 32dc75a5310a88de322dfda1aa8e4e2f98fa8caa

[GUI] Display heat and mass transfer pages for user-defined flows (neptune)

gui/Pages/AnalysisFeaturesView.py
@@ -197,7 +197,7 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
         self.comboBoxSinglePhase.activated[str].connect(self.slotSinglePhase)
         self.comboBoxGroundwater.activated[str].connect(self.slotGroundwater)
         self.comboBoxHgn.activated[str].connect(self.slotHgn)
-        self.comboBoxNeptuneCFD.activated[str].connect(self.slotNeptuneCFD)
+        self.comboBoxNeptuneCFD.currentTextChanged[str].connect(self.slotNeptuneCFD)
         self.checkBoxNeptuneHeatMass.stateChanged.connect(self.slotNeptuneHeatMass)
         self.checkBoxALE.stateChanged.connect(self.slotALE)
         self.checkBoxFans.stateChanged.connect(self.slotFans)
@@ -739,22 +739,26 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
             check_state = {"on": Qt.Checked, "off": Qt.Unchecked}[heat_mass_transfer]
             self.checkBoxNeptuneHeatMass.setCheckState(check_state)
         else:
+            self.checkBoxNeptuneHeatMass.setCheckState(Qt.Checked)
             self.checkBoxNeptuneHeatMass.hide()
 
         self.browser.configureTree(self.case)
 
     @pyqtSlot(int)
     def slotNeptuneHeatMass(self, val):
+        predefined_flow = self.nept.getPredefinedFlow()
         if val == 0:
             self.nept.setHeatMassTransferStatus("off")
-            for field_id in self.nept.getFieldIdList():
-                self.nept.setEnergyModel(field_id, "off")
-            NeptuneWallTransferModel(self.case).clear()
-            InterfacialEnthalpyModel(self.case).deleteLiquidVaporEnthalpyTransfer()
+            if predefined_flow != "None":
+                for field_id in self.nept.getFieldIdList():
+                    self.nept.setEnergyModel(field_id, "off")
+                NeptuneWallTransferModel(self.case).clear()
+                InterfacialEnthalpyModel(self.case).deleteLiquidVaporEnthalpyTransfer()
         else:
             self.nept.setHeatMassTransferStatus("on")
-            for field_id in self.nept.getFieldIdList():
-                self.nept.setEnergyModel(field_id, "total_enthalpy")
+            if predefined_flow != "None":
+                for field_id in self.nept.getFieldIdList():
+                    self.nept.setEnergyModel(field_id, "total_enthalpy")
         self.browser.configureTree(self.case)
 
     @pyqtSlot(str)

commit c53abb0069c4e2ce8aa3732616941312fa31e892

[GUI] Fix minor issues introduced in commit 32dc75a53

gui/Pages/AnalysisFeaturesView.py
@@ -474,7 +474,7 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
         self.modelNeptuneCFD.setItem(str_model=predefined_flow)
         # Force refresh
         text = self.modelNeptuneCFD.dicoM2V[predefined_flow]
-        self.comboBoxNeptuneCFD.activated[str].emit(text)
+        self.comboBoxNeptuneCFD.currentTextChanged[str].emit(text)
 
         self.radioButtonNeptuneCFD.setChecked(True)
 
@@ -728,8 +728,11 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
         """
         Called when the comboBoxNeptuneCFD changed
         """
+        model_p = self.nept.getPredefinedFlow()
         model = self.__stringModelFromCombo('NeptuneCFD')
-        self.nept.setPredefinedFlow(model)
+        print("Model: ", model)
+        if model != model_p:
+            self.nept.setPredefinedFlow(model)
 
         self.checkBoxALE.hide()
         self.checkBoxFans.hide()