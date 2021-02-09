code-saturne/code_saturne

commit 6c5249c11cb72a8d36702327bb82d974164e76d7

Move cs_param_sles.* files from cdo/ directory to alge/

src/alge/cs_alge_headers.h
@@ -48,6 +48,7 @@
 #include "cs_matrix_util.h"
 #include "cs_multigrid.h"
 #include "cs_multigrid_smoother.h"
+#include "cs_param_sles.h"-
 #include "cs_sles_default.h"
 #include "cs_sles.h"
 #include "cs_sles_it.h"

src/alge/Makefile.am
@@ -70,6 +70,13 @@ libcsalge_amgx_la_CPPFLAGS =\
 -I$(top_srcdir)/src/mesh \
 $(AMGX_CPPFLAGS) $(MPI_CPPFLAGS)
 
+libcsalge_extension_la_CPPFLAGS =\
+-I$(top_srcdir)/src/base \
+-I$(top_srcdir)/src/bft \
+-I$(top_srcdir)/src/fvm \
+-I$(top_srcdir)/src/mesh \
+$(PETSC_CPPFLAGS)  $(MUMPS_CPPFLAGS) $(MPI_CPPFLAGS)
+
 # Public header files (to be installed)
 
 pkginclude_HEADERS = \
@@ -97,6 +104,7 @@ cs_matrix_tuning.h \
 cs_matrix_util.h \
 cs_multigrid.h \
 cs_multigrid_smoother.h \
+cs_param_sles.h \
 cs_sles.h \
 cs_sles_default.h \
 cs_sles_it.h \
@@ -155,16 +163,23 @@ libcsalge_la_LDFLAGS = -no-undefined
 
 libcsalge_la_LIBADD =
 
+noinst_LTLIBRARIES += libcsalge_extension.la
+libcsalge_la_LIBADD += libcsalge_extension.la
+libcsalge_extension_la_SOURCES = \
+cs_param_sles.c
+
 if HAVE_PETSC
 noinst_LTLIBRARIES += libcsalge_petsc.la
 libcsalge_la_LIBADD += libcsalge_petsc.la
-libcsalge_petsc_la_SOURCES = cs_sles_petsc.c
+libcsalge_petsc_la_SOURCES = \
+cs_sles_petsc.c
 endif
 
 if HAVE_MUMPS
 noinst_LTLIBRARIES += libcsalge_mumps.la
 libcsalge_la_LIBADD += libcsalge_mumps.la
-libcsalge_mumps_la_SOURCES = cs_sles_mumps.c
+libcsalge_mumps_la_SOURCES = \
+cs_sles_mumps.c
 libcsalge_mumps_la_LIBADD = \
 $(MUMPS_LDFLAGS) $(MUMPSRUNPATH) $(MUMPS_LIBS)
 libcsalge_mumps_la_LDFLAGS = -no-undefined

src/cdo/Makefile.am
@@ -115,7 +115,6 @@ cs_navsto_param.h \
 cs_navsto_sles.h \
 cs_navsto_system.h \
 cs_param_cdo.h \
-cs_param_sles.h \
 cs_property.h \
 cs_quadrature.h \
 cs_reco.h \
@@ -205,7 +204,6 @@ libcscdo_extension_la_SOURCES = \
 cs_cdofb_ac.c \
 cs_cdofb_monolithic_sles.c \
 cs_cdofb_predco.c \
-cs_param_sles.c \
 cs_navsto_param.c \
 cs_navsto_sles.c
 

src/cdo/cs_cdo_headers.h
@@ -77,7 +77,6 @@
 #include "cs_navsto_param.h"
 #include "cs_navsto_system.h"
 #include "cs_param_cdo.h"
-#include "cs_param_sles.h"
 #include "cs_property.h"
 #include "cs_quadrature.h"
 #include "cs_reco.h"

commit f693b2990eb983c9b8bf5f20f38ad3f50ec04e6d

Fix typo introduced in commit 6c5249c1 (Feb 2 2021).

src/alge/cs_alge_headers.h
@@ -48,7 +48,7 @@
 #include "cs_matrix_util.h"
 #include "cs_multigrid.h"
 #include "cs_multigrid_smoother.h"
-#include "cs_param_sles.h"-
+#include "cs_param_sles.h"
 #include "cs_sles_default.h"
 #include "cs_sles.h"
 #include "cs_sles_it.h"