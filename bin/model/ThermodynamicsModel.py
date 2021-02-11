code-saturne/code_saturne

commit 04e87e0e0b8ded4c403b9626c1039c7093aa9f8d

[GUI] Minor fix following commit a2df46397
  * Reset physical properties handling using zone_id instead of
    zone_name when dealing with xml file.

bin/model/ThermodynamicsModel.py
@@ -424,7 +424,7 @@ class ThermodynamicsModel(MainFieldsModel, Variables, Model):
 
 
     @Variables.noUndo
-    def getFormula(self, fieldId, tag, zone="all_cells"):
+    def getFormula(self, fieldId, tag, zone="1"):
         """
         Return a formula for properties
         """
@@ -434,12 +434,11 @@ class ThermodynamicsModel(MainFieldsModel, Variables, Model):
         self.isInList(tag, self.propertiesFormulaList())
         node = self.XMLNodeproperty.xmlGetNode('property', field_id = fieldId, name=tag)
 
-        if zone != "all_cells":
-            if node.xmlGetChildNode("zone", name=zone):
-                node = node.xmlGetChildNode("zone", name=zone)
+        if zone != "1":
+            if node.xmlGetChildNode("zone", zone_id=zone):
+                node = node.xmlGetChildNode("zone", zone_id=zone)
             else:
-                node = node.xmlInitChildNode("zone")
-                node["name"] = zone
+                node = node.xmlInitChildNode("zone", zone_id=zone)
 
         if node:
             return node.xmlGetChildString('formula')
@@ -448,7 +447,7 @@ class ThermodynamicsModel(MainFieldsModel, Variables, Model):
 
 
     @Variables.undoLocal
-    def setFormula(self, fieldId, tag, strg, zone="all_cells"):
+    def setFormula(self, fieldId, tag, strg, zone="1"):
         """
         Gives a formula for properties
         """
@@ -458,12 +457,11 @@ class ThermodynamicsModel(MainFieldsModel, Variables, Model):
         self.isInList(tag, self.propertiesFormulaList())
         node = self.XMLNodeproperty.xmlGetNode('property', field_id = fieldId, name=tag)
 
-        if zone != "all_cells":
-            if node.xmlGetChildNode("zone", name=zone):
-                node = node.xmlGetChildNode("zone", name=zone)
+        if zone != "1":
+            if node.xmlGetChildNode("zone", zone_id=zone):
+                node = node.xmlGetChildNode("zone", zone_id=zone)
             else:
-                node = node.xmlInitChildNode("zone")
-                node["name"] = zone
+                node = node.xmlInitChildNode("zone", zone_id=zone)
 
         node.xmlSetData('formula', strg)
 
@@ -547,7 +545,7 @@ class ThermodynamicsModel(MainFieldsModel, Variables, Model):
 
 
     # MEG Generation related functions
-    def getFormulaComponents(self, fieldId, tag, zone="all_cells"):
+    def getFormulaComponents(self, fieldId, tag, zone="1"):
         """
         Get the formula components for a given tag
         """
@@ -596,7 +594,7 @@ class ThermodynamicsModel(MainFieldsModel, Variables, Model):
             raise Exception(msg)
 
 
-    def getFormulaRhoComponents(self, fieldId, zone="all_cells"):
+    def getFormulaRhoComponents(self, fieldId, zone="1"):
         """
         User formula for density
         """
@@ -635,7 +633,7 @@ class ThermodynamicsModel(MainFieldsModel, Variables, Model):
         return exp, req, known_fields, symbols
 
 
-    def getFormulaMuComponents(self, fieldId, zone="all_cells"):
+    def getFormulaMuComponents(self, fieldId, zone="1"):
         """
         User formula for molecular viscosity
         """
@@ -674,7 +672,7 @@ class ThermodynamicsModel(MainFieldsModel, Variables, Model):
         return exp, req, known_fields, symbols
 
 
-    def getFormulaCpComponents(self, fieldId, zone="all_cells"):
+    def getFormulaCpComponents(self, fieldId, zone="1"):
         """
         User formula for specific heat
         """
@@ -714,7 +712,7 @@ class ThermodynamicsModel(MainFieldsModel, Variables, Model):
         return exp, req, known_fields, symbols
 
 
-    def getFormulaAlComponents(self, fieldId, zone="all_cells"):
+    def getFormulaAlComponents(self, fieldId, zone="1"):
         """
         User formula for thermal conductivity
         """
@@ -793,7 +791,7 @@ class ThermodynamicsModel(MainFieldsModel, Variables, Model):
         return exp, req, known_fields, symbols
 
 
-    def getFormulaTemperatureComponents(self, fieldId, zone="all_cells"):
+    def getFormulaTemperatureComponents(self, fieldId, zone="1"):
         """
         User formula for temperature as a function of enthalpy
         """
@@ -828,7 +826,7 @@ class ThermodynamicsModel(MainFieldsModel, Variables, Model):
         return exp, req, known_fields, symbols
 
 
-    def getFormuladrodpComponents(self, fieldId, zone="all_cells"):
+    def getFormuladrodpComponents(self, fieldId, zone="1"):
         """
         User formula for d(ro) / dp (compressible flow)
         """
@@ -864,7 +862,7 @@ class ThermodynamicsModel(MainFieldsModel, Variables, Model):
         return  exp, req, known_fields, symbols
 
 
-    def getFormuladrodhComponents(self, fieldId, zone="all_cells"):
+    def getFormuladrodhComponents(self, fieldId, zone="1"):
         """
         User formula for d(ro) / dh (compressible flow)
         """
@@ -900,7 +898,7 @@ class ThermodynamicsModel(MainFieldsModel, Variables, Model):
         return exp, req, known_fields, symbols
 
 
-    def getFormuladTsatdpComponents(self, zone="all_cells"):
+    def getFormuladTsatdpComponents(self, zone="1"):
 
         exp = self.getFormula('none', 'd_Tsat_d_P', zone)
         label = self.m_out.getVariableLabel('none', 'd_Tsat_d_P')
@@ -925,7 +923,7 @@ class ThermodynamicsModel(MainFieldsModel, Variables, Model):
         return exp, req, known_fields, symbols
 
 
-    def getFormulaHlatComponents(self, zone="all_cells"):
+    def getFormulaHlatComponents(self, zone="1"):
 
         exp = self.getFormula('none', 'LatentHeat', zone)
         label = self.m_out.getVariableLabel('none', 'LatentHeat')
@@ -950,7 +948,7 @@ class ThermodynamicsModel(MainFieldsModel, Variables, Model):
         return exp, req, known_fields, symbols
 
 
-    def getFormulaTsatComponents(self, zone="all_cells"):
+    def getFormulaTsatComponents(self, zone="1"):
 
         exp = self.getFormula('none', 'SaturationTemperature', zone)
         label = self.m_out.getVariableLabel('none', 'SaturationTemperature')
@@ -974,7 +972,7 @@ class ThermodynamicsModel(MainFieldsModel, Variables, Model):
 
         return exp, req, known_fields, symbols
 
-    def getFormuladHsatdpComponents(self, tag, zone="all_cells"):
+    def getFormuladHsatdpComponents(self, tag, zone="1"):
 
         exp = self.getFormula('none', tag, zone)
 
@@ -1000,7 +998,7 @@ class ThermodynamicsModel(MainFieldsModel, Variables, Model):
         return exp, req, known_fields, symbols
 
 
-    def getFormulaHsatComponents(self, tag, zone="all_cells"):
+    def getFormulaHsatComponents(self, tag, zone="1"):
 
         label = self.m_out.getVariableLabel('none', tag)
         exp = self.getFormula('none', tag, zone)

bin/cs_meg_to_c.py
@@ -1255,11 +1255,12 @@ class meg_to_c_interpreter:
                     self.init_block('vol', 'all_cells', fk,
                                     exp, req, sym, sca)
                     for zone in vlm.getZones():
-                        if zone.getLabel() != "all_cells" and \
+                        zname = zone.getLabel()
+                        z_id  = str(zone.getCodeNumber())
+                        if zname != "all_cells" and \
                                 zone.isNatureActivated('physical_properties'):
-                            zname = zone.getLabel()
                             exp, req, sca, sym = \
-                            fcm.getFormulaComponents(fk, zone=zname)
+                            fcm.getFormulaComponents(fk, zone=z_id)
                             self.init_block('vol', zname, fk,
                                             exp, req, sym, sca)
 
@@ -1278,10 +1279,11 @@ class meg_to_c_interpreter:
                                         exp, req, sym, sca)
                         for zone in vlm.getZones():
                             zname = zone.getLabel()
+                            z_id = str(zone.getCodeNumber())
                             if zname != "all_cells" and \
                             zone.isNatureActivated('physical_properties'):
                                 exp, req, sca, sym = \
-                                fcm.getFormulaComponents('scalar_diffusivity', scalar=s, zone=zname)
+                                fcm.getFormulaComponents('scalar_diffusivity', scalar=s, zone=z_id)
                                 self.init_block('vol', zname, dname,
                                                 exp, req, sym, sca)
 
@@ -1349,9 +1351,10 @@ class meg_to_c_interpreter:
                             if tm.getPropertyMode(fieldId, fk) == 'user_law':
                                 name = fk + '_' + str(fieldId)
                                 for zone in vlm.getZones():
+                                    zname = zone.getLabel()
+                                    z_id = str(zone.getCodeNumber())
                                     if zone.isNatureActivated('physical_properties'):
-                                        zname = zone.getLabel()
-                                        exp, req, sca, sym = tm.getFormulaComponents(fieldId,fk,zone=zname)
+                                        exp, req, sca, sym = tm.getFormulaComponents(fieldId,fk,zone=z_id)
                                         self.init_block('vol', zname, name,
                                                         exp, req, sym, sca)
 
@@ -1359,9 +1362,10 @@ class meg_to_c_interpreter:
                             for fk in compressible_fields:
                                 name = fk + '_' + str(fieldId)
                                 for zone in vlm.getZones():
+                                    zname = zone.getLabel()
+                                    z_id = str(zone.getCodeNumber())
                                     if zone.isNatureActivated('physical_properties'):
-                                        zname = zone.getLabel()
-                                        exp, req, sca, sym = tm.getFormulaComponents(fieldId,fk,zone=zname)
+                                        exp, req, sca, sym = tm.getFormulaComponents(fieldId,fk,zone=z_id)
                                         self.init_block('vol', zname, name,
                                                         exp, req, sym, sca)
 
@@ -1369,12 +1373,13 @@ class meg_to_c_interpreter:
                         if mfm.getEnergyResolution(fieldId) == 'on':
                             name = 'temperature_' + str(fieldId)
                             for zone in vlm.getZones():
+                                zname = zone.getLabel()
+                                z_id = str(zone.getCodeNumber())
                                 if zone.isNatureActivated('physical_properties'):
-                                    zname = zone.getLabel()
                                     exp, req, sca, sym = \
                                         tm.getFormulaComponents(fieldId,
                                                                 'temperature',
-                                                                zone=zname)
+                                                                zone=z_id)
                                     self.init_block('vol', zname, name,
                                                     exp, req, sym, sca)
 
@@ -1384,9 +1389,10 @@ class meg_to_c_interpreter:
                 if user_gas_liq_fields:
                     for fk in gas_liq_fields:
                         for zone in vlm.getZones():
+                            zname = zone.getLabel()
+                            z_id = str(zone.getCodeNumber())
                             if zone.isNatureActivated('physical_properties'):
-                                zname = zone.getLabel()
-                                exp, req, sca, sym = tm.getFormulaComponents('none', fk, zone=zname)
+                                exp, req, sca, sym = tm.getFormulaComponents('none', fk, zone=z_id)
                                 self.init_block('vol', zname, fk,
                                                 exp, req, sym, sca)
 

bin/model/FluidCharacteristicsModel.py
@@ -823,7 +823,7 @@ class FluidCharacteristicsModel(Variables, Model):
 
 
     @Variables.noUndo
-    def getFormula(self, tag, zone="all_cells"):
+    def getFormula(self, tag, zone="1"):
         """
         Return a formula for I{tag} 'density', 'molecular_viscosity',
         'specific_heat' or 'thermal_conductivity'
@@ -832,12 +832,11 @@ class FluidCharacteristicsModel(Variables, Model):
                             'specific_heat', 'thermal_conductivity',
                             'volume_viscosity'))
         node = self.node_fluid.xmlGetNode('property', name=tag)
-        if zone != "all_cells":
-            if node.xmlGetChildNode("zone", name=zone):
-                node = node.xmlGetChildNode("zone", name=zone)
+        if zone != "1":
+            if node.xmlGetChildNode("zone", zone_id=zone):
+                node = node.xmlGetChildNode("zone", zone_id=zone)
             else:
-                node = node.xmlInitChildNode("zone")
-                node["name"] = zone
+                node = node.xmlInitChildNode("zone", zone_id=zone)
         formula = node.xmlGetChildString('formula')
         if not formula:
             formula = self.getDefaultFormula(tag)
@@ -866,7 +865,7 @@ class FluidCharacteristicsModel(Variables, Model):
 
 
     @Variables.undoLocal
-    def setFormula(self, tag, formula, zone="all_cells"):
+    def setFormula(self, tag, formula, zone="1"):
         """
         Gives a formula for 'density', 'molecular_viscosity',
         'specific_heat'or 'thermal_conductivity'
@@ -875,12 +874,11 @@ class FluidCharacteristicsModel(Variables, Model):
                             'specific_heat', 'thermal_conductivity',
                             'volume_viscosity'))
         node = self.node_fluid.xmlGetNode('property', name=tag)
-        if zone != "all_cells":
-            if node.xmlGetChildNode("zone", name=zone):
-                node = node.xmlGetChildNode("zone", name=zone)
+        if zone != "1":
+            if node.xmlGetChildNode("zone", zone_id=zone):
+                node = node.xmlGetChildNode("zone", zone_id=zone)
             else:
-                node = node.xmlInitChildNode("zone")
-                node["name"] = zone
+                node = node.xmlInitChildNode("zone", zone_id=zone)
         node.xmlSetData('formula', formula)
 
 
@@ -921,7 +919,7 @@ class FluidCharacteristicsModel(Variables, Model):
                 node.xmlRemoveChild('postprocessing_recording')
 
     # MEG Generation related functions
-    def getFormulaComponents(self, tag, scalar=None, zone="all_cells"):
+    def getFormulaComponents(self, tag, scalar=None, zone="1"):
         """
         Get the formula components for a given tag
         """
@@ -949,7 +947,7 @@ class FluidCharacteristicsModel(Variables, Model):
             raise Exception(msg)
 
 
-    def getFormulaRhoComponents(self, zone="all_cells"):
+    def getFormulaRhoComponents(self, zone="1"):
         """
         User formula for density
         """
@@ -975,7 +973,7 @@ class FluidCharacteristicsModel(Variables, Model):
         return exp, req, self.list_scalars, symbols;
 
 
-    def getFormulaMuComponents(self, zone="all_cells"):
+    def getFormulaMuComponents(self, zone="1"):
         """
         User formula for molecular viscosity
         """
@@ -1012,7 +1010,7 @@ class FluidCharacteristicsModel(Variables, Model):
         return exp, req, self.list_scalars, symbols;
 
 
-    def getFormulaCpComponents(self, zone="all_cells"):
+    def getFormulaCpComponents(self, zone="1"):
         """
         User formula for specific heat
         """
@@ -1037,7 +1035,7 @@ class FluidCharacteristicsModel(Variables, Model):
         return exp, req, self.list_scalars, symbols;
 
 
-    def getFormulaViscv0Components(self, zone="all_cells"):
+    def getFormulaViscv0Components(self, zone="1"):
         """
         User formula for volumic viscosity
         """
@@ -1064,7 +1062,7 @@ class FluidCharacteristicsModel(Variables, Model):
         return exp, req, self.list_scalars, symbols;
 
 
-    def getFormulaAlComponents(self, zone="all_cells"):
+    def getFormulaAlComponents(self, zone="1"):
         """
         User formula for thermal conductivity
         """
@@ -1088,7 +1086,7 @@ class FluidCharacteristicsModel(Variables, Model):
         return exp, req, self.list_scalars, symbols;
 
 
-    def getFormulaDiffComponents(self, scalar, zone="all_cells"):
+    def getFormulaDiffComponents(self, scalar, zone="1"):
         """
         User formula for the diffusion coefficient
         """

gui/Pages/FluidCharacteristicsView.py
@@ -58,6 +58,7 @@ from code_saturne.model.GroundwaterModel import GroundwaterModel
 from code_saturne.Pages.QMegEditorView import QMegEditorView
 from code_saturne.model.NotebookModel import NotebookModel
 from code_saturne.model.InternalCouplingModel import InternalCouplingModel
+from code_saturne.model.LocalizationModel import LocalizationModel
 
 #-------------------------------------------------------------------------------
 # log config
@@ -189,13 +190,19 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
         self.mdl      = None
         self.notebook = None
         self.zone     = None
+        self.zone_name = None
+        self.zone_id   = None
 
 
     def setup(self, case, zone_name):
 
         self.case = case
 
-        self.zone = zone_name
+        for zone in LocalizationModel('VolumicZone', self.case).getZones():
+            if zone.getLabel() == zone_name:
+                self.zone = zone
+                self.zone_name = zone.getLabel()
+                self.zone_id   = str(zone.getCodeNumber())
 
         self.case.undoStopGlobal()
 
@@ -391,9 +398,9 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
         mdls = self.mdl.getThermoPhysicalModel()
         mdl_atmo, mdl_joule, mdl_thermal, mdl_gas, mdl_coal, mdl_comp, mdl_hgn = mdls
 
-        is_main_zone = True
-        if self.zone != "all_cells":
-            is_main_zone = False
+
+
+        is_main_zone = (self.zone_name == "all_cells")
 
         self.groupBoxPressure.setVisible(is_main_zone)
         self.groupBoxTemperature.setVisible(is_main_zone)
@@ -402,7 +409,7 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
         self.groupBoxMassMolar.hide()
 
         if not is_main_zone:
-            if self.internal_cpl.isZoneCoupled(self.zone):
+            if self.internal_cpl.isZoneCoupled(self.zone_id):
                 self.checkBoxSolid.setChecked(True)
             else:
                 self.checkBoxSolid.setChecked(False)
@@ -940,15 +947,15 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
             __button.show()
             exp = None
             if sym == "Rho":
-                exp = self.mdl.getFormula('density', self.zone)
+                exp = self.mdl.getFormula('density', self.zone_id)
             elif sym == "Mu":
-                exp = self.mdl.getFormula('molecular_viscosity', self.zone)
+                exp = self.mdl.getFormula('molecular_viscosity', self.zone_id)
             elif sym == "Cp":
-                exp = self.mdl.getFormula('specific_heat', self.zone)
+                exp = self.mdl.getFormula('specific_heat', self.zone_id)
             elif sym == "Viscv0":
-                exp = self.mdl.getFormula('volume_viscosity', self.zone)
+                exp = self.mdl.getFormula('volume_viscosity', self.zone_id)
             elif sym == "Al":
-                exp = self.mdl.getFormula('thermal_conductivity', self.zone)
+                exp = self.mdl.getFormula('thermal_conductivity', self.zone_id)
             elif sym == "Diff":
                 name = self.mdl.m_sca.getScalarDiffusivityName(self.scalar)
                 exp = self.mdl.m_sca.getDiffFormula(self.scalar)
@@ -1084,7 +1091,8 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
         """
         User formula for density
         """
-        exp, req, sca, symbols_rho = self.mdl.getFormulaRhoComponents(self.zone);
+        exp, req, sca, symbols_rho = \
+                self.mdl.getFormulaRhoComponents(self.zone_id)
 
         self.m_th = ThermalScalarModel(self.case)
         s = self.m_th.getThermalScalarName()
@@ -1101,7 +1109,7 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
 
         dialog = QMegEditorView(parent        = self,
                                 function_type = 'vol',
-                                zone_name     = self.zone,
+                                zone_name     = self.zone_name,
                                 variable_name = 'density',
                                 expression    = exp,
                                 required      = req,
@@ -1112,7 +1120,7 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
         if dialog.exec_():
             result = dialog.get_result()
             log.debug("slotFormulaRho -> %s" % str(result))
-            self.mdl.setFormula('density', str(result), self.zone)
+            self.mdl.setFormula('density', str(result), self.zone_id)
             self.pushButtonRho.setToolTip(result)
             self.pushButtonRho.setStyleSheet("background-color: green")
 
@@ -1123,7 +1131,8 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
         User formula for molecular viscosity
         """
 
-        exp, req, sca, symbols_mu = self.mdl.getFormulaMuComponents(self.zone)
+        exp, req, sca, symbols_mu = \
+                self.mdl.getFormulaMuComponents(self.zone_id)
 
         self.m_th = ThermalScalarModel(self.case)
         s = self.m_th.getThermalScalarName()
@@ -1140,7 +1149,7 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
 
         dialog = QMegEditorView(parent        = self,
                                 function_type = 'vol',
-                                zone_name     = self.zone,
+                                zone_name     = self.zone_name,
                                 variable_name = 'molecular_viscosity',
                                 expression    = exp,
                                 required      = req,
@@ -1151,7 +1160,7 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
         if dialog.exec_():
             result = dialog.get_result()
             log.debug("slotFormulaMu -> %s" % str(result))
-            self.mdl.setFormula('molecular_viscosity', str(result), self.zone)
+            self.mdl.setFormula('molecular_viscosity', str(result), self.zone_id)
             self.pushButtonMu.setToolTip(result)
             self.pushButtonMu.setStyleSheet("background-color: green")
 
@@ -1161,13 +1170,14 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
         """
         User formula for specific heat
         """
-        exp, req, sca, symbols_cp = self.mdl.getFormulaCpComponents(self.zone)
+        exp, req, sca, symbols_cp = \
+                self.mdl.getFormulaCpComponents(self.zone_id)
 
         exa = FluidCharacteristicsView.specific_heat
 
         dialog = QMegEditorView(parent        = self,
                                 function_type = 'vol',
-                                zone_name     = self.zone,
+                                zone_name     = self.zone_name,
                                 variable_name = 'specific_heat',
                                 expression    = exp,
                                 required      = req,
@@ -1178,7 +1188,7 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
         if dialog.exec_():
             result = dialog.get_result()
             log.debug("slotFormulaRho -> %s" % str(result))
-            self.mdl.setFormula('specific_heat', str(result), self.zone)
+            self.mdl.setFormula('specific_heat', str(result), self.zone_id)
             self.pushButtonCp.setToolTip(result)
             self.pushButtonCp.setStyleSheet("background-color: green")
 
@@ -1188,13 +1198,14 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
         """
         User formula for volumic viscosity
         """
-        exp, req, sca, symbols_viscv0 = self.mdl.getFormulaViscv0Components(self.zone)
+        exp, req, sca, symbols_viscv0 = \
+                self.mdl.getFormulaViscv0Components(self.zone_id)
 
         exa = FluidCharacteristicsView.volume_viscosity
 
         dialog = QMegEditorView(parent        = self,
                                 function_type = 'vol',
-                                zone_name     = self.zone,
+                                zone_name     = self.zone_name,
                                 variable_name = 'volume_viscosity',
                                 expression    = exp,
                                 required      = req,
@@ -1205,7 +1216,7 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
         if dialog.exec_():
             result = dialog.get_result()
             log.debug("slotFormulaViscv0 -> %s" % str(result))
-            self.mdl.setFormula('volume_viscosity', str(result), self.zone)
+            self.mdl.setFormula('volume_viscosity', str(result), self.zone_id)
             self.pushButtonViscv0.setToolTip(result)
             self.pushButtonViscv0.setStyleSheet("background-color: green")
 
@@ -1215,7 +1226,8 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
         """
         User formula for thermal conductivity
         """
-        exp, req, sca, symbols_al = self.mdl.getFormulaAlComponents(self.zone)
+        exp, req, sca, symbols_al = \
+                self.mdl.getFormulaAlComponents(self.zone_id)
 
         self.m_th = ThermalScalarModel(self.case)
         s = self.m_th.getThermalScalarName()
@@ -1230,7 +1242,7 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
 
         dialog = QMegEditorView(parent        = self,
                                 function_type = 'vol',
-                                zone_name     = self.zone,
+                                zone_name     = self.zone_name,
                                 variable_name = 'thermal_conductivity',
                                 expression    = exp,
                                 required      = req,
@@ -1241,7 +1253,7 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
         if dialog.exec_():
             result = dialog.get_result()
             log.debug("slotFormulaAl -> %s" % str(result))
-            self.mdl.setFormula('thermal_conductivity', str(result), self.zone)
+            self.mdl.setFormula('thermal_conductivity', str(result), self.zone_id)
             self.pushButtonAl.setToolTip(result)
             self.pushButtonAl.setStyleSheet("background-color: green")
 
@@ -1251,7 +1263,8 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
         """
         User formula for the diffusion coefficient
         """
-        exp, req, sca, sym = self.mdl.getFormulaDiffComponents(self.scalar, self.zone)
+        exp, req, sca, sym = \
+                self.mdl.getFormulaDiffComponents(self.scalar, self.zone_id)
 
         exa = ''
 
@@ -1259,7 +1272,7 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
 
         dialog = QMegEditorView(parent        = self,
                                 function_type = 'vol',
-                                zone_name     = self.zone,
+                                zone_name     = self.zone_name,
                                 variable_name = dname,
                                 expression    = exp,
                                 required      = req,
@@ -1270,7 +1283,7 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
         if dialog.exec_():
             result = dialog.get_result()
             log.debug("slotFormulaDiff -> %s" % str(result))
-            self.mdl.m_sca.setDiffFormula(self.scalar, str(result), self.zone)
+            self.mdl.m_sca.setDiffFormula(self.scalar, str(result), self.zone_id)
             self.pushButtonDiff.setToolTip(result)
             self.pushButtonDiff.setStyleSheet("background-color: green")
 
@@ -1279,9 +1292,9 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
     def slotSolidZone(self, val):
 
         if val == 0:
-            self.internal_cpl.removeZone(self.zone)
+            self.internal_cpl.removeZone(self.zone_id)
         else:
-            self.internal_cpl.addZone(self.zone)
+            self.internal_cpl.addZone(self.zone_id)
 
 
 #-------------------------------------------------------------------------------

commit c9509b9fca3aa06a8de5aed3dc5de76fe8780759

[GUI] Fix physical properties formulae reader for NCFD broken by commit 04e87e0e

bin/model/ThermodynamicsModel.py
@@ -422,7 +422,7 @@ class ThermodynamicsModel(MainFieldsModel, Variables, Model):
         self.isInList(tag, self.propertiesFormulaList())
         node = self.get_property_node(fieldId, tag)
 
-        if zone != "1":
+        if str(zone) != "1":
             if node.xmlGetChildNode("zone", zone_id=zone):
                 node = node.xmlGetChildNode("zone", zone_id=zone)
             else: