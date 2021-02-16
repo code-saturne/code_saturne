code-saturne/code_saturne

commit c29ad329c1ebffd50033deb482435f6ea8f2d16c

Major evolution concerning coupling with external codes
  * Create a generic page "Coupling parameters" after boundary
    conditions in the tree view. This page gathers the parameters for
    syrthes, aster and cathare couplings.
  * The definition of a coupling with syrthes is now done directly as a
    thermal boundary condition (like flux or temperature).
  * Display external coupling tab as deprecated

gui/Pages/BoundaryConditionsEnergyView.py
@@ -51,6 +51,7 @@ from code_saturne.model.Common import GuiParam
 from code_saturne.Base.QtPage import DoubleValidator, ComboModel, from_qvariant
 from code_saturne.model.MainFieldsModel import MainFieldsModel
 from code_saturne.model.ThermodynamicsModel import ThermodynamicsModel
+from code_saturne.model.ConjugateHeatTransferModel import ConjugateHeatTransferModel
 
 #-------------------------------------------------------------------------------
 # log config
@@ -88,6 +89,8 @@ class BoundaryConditionsEnergyView(QWidget, Ui_BoundaryConditionsEnergy) :
 
         self.lineEditEnergy.setValidator(validatorEner)
 
+        self.lineEditSyrthes.editingFinished.connect(self.__slotEnergySyrthes)
+
 
     def setup(self, case, fieldId):
         """
@@ -96,6 +99,7 @@ class BoundaryConditionsEnergyView(QWidget, Ui_BoundaryConditionsEnergy) :
         self.case = case
         self.__boundary = None
         self.__currentField = fieldId
+        self.cht_model = ConjugateHeatTransferModel(self.case)
 
 
     def showWidget(self, boundary):
@@ -115,6 +119,7 @@ class BoundaryConditionsEnergyView(QWidget, Ui_BoundaryConditionsEnergy) :
         elif self.__boundary.getNature() == "wall" :
             self.__modelEnergy.addItem(self.tr("Flux"), 'flux')
             self.__modelEnergy.addItem(self.tr("Temperature"), 'temperature')
+            self.__modelEnergy.addItem(self.tr("SYRTHES coupling"), "syrthes_coupling")
 
         if self.__boundary.getNature() == "inlet" or  self.__boundary.getNature() == "outlet":
             if MainFieldsModel(self.case).getEnergyResolution(self.__currentField) == 'on' :
@@ -148,12 +153,28 @@ class BoundaryConditionsEnergyView(QWidget, Ui_BoundaryConditionsEnergy) :
                 energychoice = self.__boundary.getEnthalpyChoice("none")
                 self.__modelEnergy.setItem(str_model=energychoice)
                 val = self.__boundary.getEnthalpy("none")
-                self.lineEditEnergy.setText(str(val))
+
+                if energychoice == 'syrthes_coupling':
+                    self.lineEditEnergy.hide()
+                    self.lineEditSyrthes.show()
+                    syrCompleter = \
+                    QCompleter(self.cht_model.getSyrthesInstancesList())
+                    self.lineEditSyrthes.setCompleter(syrCompleter)
+
+                    self.lineEditSyrthes.setText(str(val))
+
+                else:
+                    self.lineEditSyrthes.hide()
+                    self.lineEditEnergy.show()
+                    self.lineEditEnergy.setText(str(val))
+
                 if energychoice == 'flux' :
                     self.labelEnergy.setText('W/m2')
-                else :
+                elif energychoice == 'temperature':
                     self.labelEnergy.setText('K')
-                self.lineEditEnergy.show()
+                else:
+                    self.labelEnergy.setText('')
+
                 self.labelEnergy.show()
                 self.show()
             else :
@@ -173,22 +194,45 @@ class BoundaryConditionsEnergyView(QWidget, Ui_BoundaryConditionsEnergy) :
         INPUT choice of method of calculation of the energy
         """
         energy_choice = self.__modelEnergy.dicoV2M[str(text)]
+        old_choice = self.__boundary.getEnthalpyChoice(self.__currentField)
+        # If we switch from coupling to BC condition, delete all previous
+        # data
+        if old_choice != energy_choice and old_choice == "syrthes_coupling":
+            syrthes_name = self.__boundary.getEnthalpy(self.__currentField)
+            bnd_label = self.__boundary.getLabel()
+            self.cht_model.deleteSyrthesCoupling(syrthes_name, bnd_label)
+            self.__boundary.setEnthalpy(self.__currentField, 0.)
+
         self.__boundary.setEnthalpyChoice(self.__currentField, energy_choice)
+
 
         if energy_choice == "hsat_P" :
             self.lineEditEnergy.hide()
+            self.lineEditSyrthes.hide()
             self.labelEnergy.hide()
         else :
-            self.lineEditEnergy.show()
             self.labelEnergy.show()
             val = self.__boundary.getEnthalpy(self.__currentField)
-            self.lineEditEnergy.setText(str(val))
+            if energy_choice == "syrthes_coupling":
+                self.lineEditSyrthes.show()
+                self.lineEditEnergy.hide()
+                if val not in self.cht_model.getSyrthesInstancesList():
+                    val = ""
+                    self.__boundary.setEnthalpy(self.__currentField, val)
+                self.lineEditSyrthes.setText(str(val))
+            else:
+                self.lineEditSyrthes.hide()
+                self.lineEditEnergy.show()
+                self.lineEditEnergy.setText(str(val))
+
             if energy_choice == 'dirichlet':
                 self.labelEnergy.setText('J/kg')
             elif energy_choice == 'flux' :
                 self.labelEnergy.setText('W/m2')
-            else :
+            elif energy_choice == 'temperature' :
                 self.labelEnergy.setText('K')
+            else:
+                self.labelEnergy.setText('')
 
 
     @pyqtSlot(str)
@@ -200,6 +244,27 @@ class BoundaryConditionsEnergyView(QWidget, Ui_BoundaryConditionsEnergy) :
             value = from_qvariant(text, float)
             self.__boundary.setEnthalpy(self.__currentField, value)
 
+
+    @pyqtSlot()
+    def __slotEnergySyrthes(self):
+        """
+        Input syrthes instance name
+        """
+
+        value = str(self.lineEditSyrthes.text())
+        if value:
+            value_p = val = self.__boundary.getEnthalpy("none")
+            bnd_label = self.__boundary.getLabel()
+            if value != value_p:
+                self.cht_model.deleteSyrthesCoupling(value_p, bnd_label)
+
+
+            if value not in self.cht_model.getSyrthesInstancesList():
+                self.cht_model.addSyrthesCoupling(value)
+
+            self.cht_model.addBoundaryLabel(value, bnd_label)
+
+            self.__boundary.setEnthalpy(self.__currentField, value)
 
 #-------------------------------------------------------------------------------
 # End

bin/model/Boundary.py
@@ -32,14 +32,15 @@ import sys, unittest
 # Application modules import
 #-------------------------------------------------------------------------------
 
-from code_saturne.model.Common      import GuiParam
+from code_saturne.model.Common import GuiParam
 from code_saturne.model.XMLvariables import Model, Variables
 from code_saturne.model.XMLmodel     import ModelTest
 from code_saturne.model.XMLengine    import *
 
 from code_saturne.model.DefineUserScalarsModel import DefineUserScalarsModel
 from code_saturne.model.ThermalScalarModel     import ThermalScalarModel
-from code_saturne.model.TurbulenceModel        import TurbulenceModel
+from code_saturne.model.TurbulenceModel import TurbulenceModel
+from code_saturne.model.ConjugateHeatTransferModel import ConjugateHeatTransferModel
 
 #-------------------------------------------------------------------------------
 # log config
@@ -100,9 +101,9 @@ class Boundary(object) :
     def __init__(self, nature, label, case) :
         """
         """
+        self.case = case
         self._label = label
         self._nature = nature
-        self.case = case
         self._XMLBoundaryConditionsNode = self.case.xmlGetNode('boundary_conditions')
         self._thermalLabelsList = ('temperature',
                                    'enthalpy',
@@ -272,7 +273,6 @@ class Boundary(object) :
         """
         return self._label
 
-
     @Variables.noUndo
     def getNature(self):
         """
@@ -280,6 +280,9 @@ class Boundary(object) :
         """
         return self._nature
 
+    @Variables.noUndo
+    def getLocalization(self):
+        return None
 
     @Variables.noUndo
     def getDefinedScalarFormulaList(self):
@@ -2854,7 +2857,7 @@ class WallBoundary(Boundary) :
         self._fluxChoices=['temperature', 'flux']
         self._scalarChoices = ['dirichlet', 'neumann', 'exchange_coefficient',
                                'dirichlet_formula', 'neumann_formula',
-                               'exchange_coefficient_formula']
+                               'exchange_coefficient_formula', 'syrthes_coupling']
         self._scalarFormula = ['dirichlet_formula', 'neumann_formula',
                                'exchange_coefficient_formula']
 
@@ -2865,6 +2868,9 @@ class WallBoundary(Boundary) :
         # Scalars
         for name in self.sca_model.getScalarNameList():
             self.getScalarChoice(name)
+
+        # Syrthes conjugate heat transfer model
+        self._syrthesModel = ConjugateHeatTransferModel(self.case)
 
 
     def __deleteVelocities(self, node):
@@ -2907,6 +2913,10 @@ class WallBoundary(Boundary) :
                 if tt != tag:
                     scalarNode.xmlRemoveChild(tt)
 
+    def __deleteSyrthesNodes(self):
+        syr_inst = self.boundNode.xmlGetChildNode("syrthes")['instance_name']
+        self._syrthesModel.deleteSyrthesCoupling(syr_inst, self._label)
+        self.boundNode.xmlRemoveChild("syrthes")
 
     def __defaultValues(self):
         """
@@ -2914,9 +2924,9 @@ class WallBoundary(Boundary) :
         """
         dico = {}
         dico['velocityChoice'] = "off"
-        dico['velocityValue']  = 0.
-        dico['scalarChoice']   = "neumann"
-        dico['scalarValue']   = 0.
+        dico['velocityValue'] = 0.
+        dico['scalarChoice'] = "neumann"
+        dico['scalarValue'] = 0.
         dico['roughness'] = 0.01
         dico['flux'] = 0
         return dico
@@ -3076,6 +3086,20 @@ class WallBoundary(Boundary) :
         else:
             node.xmlSetData('roughness', value)
 
+    @Variables.undoLocal
+    def setConjugateHeatTransferCoupling(self, value):
+        # TODO : use a dedicated model as an input instead of individual property
+        node = self.boundNode.xmlInitNode("syrthes")
+        node.xmlSetAttribute(instance_name=value)
+        return
+
+    @Variables.noUndo
+    def getConjugateHeatTransferCoupling(self):
+        # TODO output a dedicated cht model instead of individual properties
+        node = self.boundNode.xmlGetNode("syrthes")
+        if node == None:
+            return None
+        return node.xmlGetAttribute("instance_name")
 
     @Variables.noUndo
     def getScalarChoice(self, name):
@@ -3125,9 +3149,14 @@ class WallBoundary(Boundary) :
             self.getScalarValue(name, 'dirichlet')
             self.getScalarValue(name, 'exchange_coefficient')
             self.__deleteScalarNodes(name, choice)
+        elif choice == "syrthes_coupling":
+            self.__deleteScalarNodes(name, choice)
         else:
             value = self.getScalarValue(name, choice)
             self.__deleteScalarNodes(name, choice)
+
+        if choice != "syrthes_coupling":
+            self.__deleteSyrthesNodes()
 
 
     @Variables.noUndo

bin/model/BoundaryNeptune.py
@@ -1125,7 +1125,7 @@ class WallBoundary(Boundary) :
         """
         Set the enthalpy choice for field
         """
-        Model().isInList(value, ['temperature','flux'])
+        Model().isInList(value, ['temperature','flux', 'syrthes_coupling'])
 
         XMLEnergyNode = self.boundNode.xmlInitNode('variable', 'choice', name='enthalpy')
 
@@ -1139,14 +1139,16 @@ class WallBoundary(Boundary) :
         """
         XMLEnergyNode = self.boundNode.xmlGetChildNode('variable', 'choice', name='enthalpy')
 
-        Model().isInList(XMLEnergyNode['choice'], ['temperature','flux'])
+        Model().isInList(XMLEnergyNode['choice'], ['temperature','flux','syrthes_coupling'])
 
         Childnode = XMLEnergyNode.xmlGetChildNode('value')
         if Childnode == None :
             value = self.__defaultValues()['flux']
             self.setEnthalpy(fieldId, value)
 
-        value = XMLEnergyNode.xmlGetChildDouble('value')
+        value = XMLEnergyNode.xmlGetChildString('value')
+        if self.getEnthalpyChoice("none") != "syrthes_coupling":
+            value = float(value)
         return value
 
 
@@ -1155,7 +1157,8 @@ class WallBoundary(Boundary) :
         """
         Set energy value for field
         """
-        Model().isFloat(value)
+        if self.getEnthalpyChoice(fieldId) in ['temperature','flux']:
+            Model().isFloat(value)
 
         XMLEnergyNode = self.boundNode.xmlGetChildNode('variable', 'choice', name='enthalpy')
         XMLEnergyNode.xmlSetData('value', str(value))
@@ -1193,6 +1196,11 @@ class WallBoundary(Boundary) :
         Model().isInList(mdl, self._wallModel)
         XMLNode = self.boundNode.xmlInitChildNode('wall_model', field_id = fieldId)
         XMLNode['model'] = mdl
+
+
+    @Variables.undoLocal
+    def setConjugateHeatTransferCoupling(self, syrthes_name):
+        print("toto")
 
 
 #-------------------------------------------------------------------------------

bin/model/ConjugateHeatTransferModel.py
@@ -60,24 +60,8 @@ class ConjugateHeatTransferModel(Variables, Model):
 
         self.__node_models = self.case.xmlGetNode('thermophysical_models')
         self.__node_cht    = self.__node_models.xmlInitNode('conjugate_heat_transfer')
-        self.__node_syr    = self.__node_cht.xmlInitNode('external_coupling')
-
-    def defaultValues(self):
-        """
-        Return in a dictionnary which contains default values.
-        """
-        default = {}
-        default['syrthes_name']       = "SYRTHES"
-        default['verbosity']  = 0
-        default['visualization']  = 1
-        default['projection_axis']    = "off"
-        default['selection_criteria'] = "all[]"
-        return default
-
-
-    def __getNumberOfSyrthesCoupling(self):
-        return len(self.__node_syr.xmlGetNodeList('syrthes'))
-
+        self.__node_syr = self.__node_cht.xmlInitNode('external_coupling')
+        self.__node_inst = self.__node_syr.xmlInitNode('syrthes_instances')
 
     @Variables.undoLocal
     def deleteConjugateHeatTransfer(self):
@@ -86,8 +70,26 @@ class ConjugateHeatTransferModel(Variables, Model):
         @type status: C{String}
         @param status: set to "on" or "off" the conjugate heat transfer
         """
-        self.__node_syr.xmlRemoveChild('syrthes')
+        self.__node_inst.xmlRemoveNode()
 
+    def getNumberOfSyrthesCoupling(self):
+        return len(self.__node_inst.xmlGetNodeList('instance'))
+
+    @Variables.noUndo
+    def getSyrthesInstancesList(self):
+        """
+        @return: list of syrthes instances
+        """
+
+        lst = []
+        node_list = self.__node_inst.xmlGetNodeList('instance')
+
+        for node in node_list:
+            syrthes_name = node.xmlGetAttribute("name")
+            if syrthes_name and syrthes_name not in lst:
+                lst.append(syrthes_name)
+
+        return lst
 
     @Variables.noUndo
     def getSyrthesCouplingList(self):
@@ -95,263 +97,91 @@ class ConjugateHeatTransferModel(Variables, Model):
         @return: list of Syrthes coupling description.
         @rtype: C{List}
         """
-        node_list = self.__node_syr.xmlGetNodeList('syrthes')
+        node_list = self.__node_inst.xmlGetNodeList('instance')
         lst = []
-        for index in range(len(node_list)):
-            num = index + 1
-            syrthes_name  = self.getSyrthesInstanceName(num)
-            verbosity     = self.getSyrthesVerbosity(num)
-            visualization = self.getSyrthesVisualization(num)
-            proj_axis     = self.getSyrthesProjectionAxis(num)
-            location      = self.getSelectionCriteria(num)
-            lst.append([syrthes_name, verbosity, visualization,
-                         proj_axis, location])
-
+        for node in node_list:
+            syrthes_name = node.xmlGetAttribute("name")
+            boundary_labels = self.getBoundaryLabelList(syrthes_name)
+            lst.append([syrthes_name, boundary_labels])
         return lst
 
-
     @Variables.undoGlobal
-    def addSyrthesCoupling(self, syrthes_name,
-                           verbosity, visualization, proj_axis, location):
+    def addSyrthesCoupling(self, syrthes_name):
         """
         Add a new definition of a Syrthes coupling.
 
         @type syrthes_name: C{String}
         @param syrthes_name: Syrthes instance name
-        @type verbosity: C{Int}
-        @param verbosity: Syrthes verbosity
-        @param visualization: Syrthes visualization output
-        @type proj_axis: C{String}
-        @param proj_axis: Syrthes projection axis
-        @type location: C{String}
-        @param location: selection criteria
-        @return: new number of Syrthes coupling
-        @rtype: C{Int}
         """
-        num = len(self.__node_syr.xmlGetNodeList('syrthes'))
-        node_new = self.__node_syr.xmlAddChild('syrthes')
+        node_new = self.__node_inst.xmlInitChildNode('instance', name=syrthes_name)
+        return
 
-        num = num + 1
-        self.setSyrthesInstanceName(num, syrthes_name)
-        self.setSyrthesVerbosity(num, verbosity)
-        self.setSyrthesVisualization(num, visualization)
-        self.setSyrthesProjectionAxis(num, proj_axis)
-        self.setSelectionCriteria(num, location)
 
-        return num
+    @Variables.undoGlobal
+    def deleteSyrthesCoupling(self, syrthes_name, boundary_label):
+        """
+        Delete a syrthes coupling. If the syrthes instance has no
+        other coupled boundaries, remove it from the instances list.
+        """
 
+        node = self.__node_inst.xmlGetChildNode('instance', name=syrthes_name)
+        if node:
+            # First delete coupling
+            for node_b in node.xmlGetChildNodeList("coupled_boundary"):
+                if node_b.xmlGetAttribute("label") == boundary_label:
+                    node_b.xmlRemoveNode()
+                    break
+
+            # If instance has no other coupling, delete it
+            if not node.xmlGetChildNodeList("coupled_boundary"):
+                node.xmlRemoveNode()
+
+
+
+    def addBoundaryLabel(self, syrthes_name, boundary_label):
+
+        # If boundary allready added, exit
+        if boundary_label in self.getBoundaryLabelList(syrthes_name):
+            return
+
+        node_instance = self.__node_inst.xmlGetChildNode('instance', name=syrthes_name)
+        if node_instance == None:
+            raise ValueError("SYRTHES instance not found : ", syrthes_name)
+        node_zone = node_instance.xmlInitChildNode('coupled_boundary', label=boundary_label)
+
+    def getBoundaryLabelList(self, syrthes_name):
+        node_instance = self.__node_inst.xmlGetChildNode('instance', name=syrthes_name)
+        if node_instance == None:
+            raise ValueError("SYRTHES instance not found : ", syrthes_name)
+        return [n.xmlGetAttribute("label") for n in node_instance.xmlGetChildNodeList("coupled_boundary")]
+
+    def flushBoundaryLabels(self):
+        for node_instance in self.__node_inst.xmlGetChildNodeList('instance'):
+            node_instance.xmlRemoveChildren()
 
     @Variables.undoLocal
-    def deleteSyrthesCoupling(self, num):
-        """
-        Delete a definition of a Syrthes coupling.
-
-        @type num: C{Int}
-        @param num: Syrthes coupling number
-        """
-        self.isLowerOrEqual(num, self.__getNumberOfSyrthesCoupling())
-        node_list = self.__node_syr.xmlGetNodeList('syrthes')
-        node = node_list[num-1]
-        node.xmlRemoveNode()
-
-    #------------------------------------------------------------------
-    # Helper function
-    #------------------------------------------------------------------
-    def __getStringData(self, index, name, setFunction):
-        """
-        Get string value from xml file.
-        """
-        self.isLowerOrEqual(index+1, self.__getNumberOfSyrthesCoupling())
-        node = self.__node_syr.xmlGetNodeList('syrthes')[index]
-        value = node.xmlGetString(name)
-        return self.__getDefaultDataIfNone(index, value, name, setFunction)
-
-
-    def __getIntData(self, index, name, setFunction):
-        """
-        Get int value from xml file.
-        """
-        self.isLowerOrEqual(index+1, self.__getNumberOfSyrthesCoupling())
-        node = self.__node_syr.xmlGetNodeList('syrthes')[index]
-        value = node.xmlGetInt(name)
-        return self.__getDefaultDataIfNone(index, value, name, setFunction)
-
-
-    def __getDefaultDataIfNone(self, index, value, name, setFunction):
-        """
-        Get default value if value is none.
-        """
-        if value == None or value == "":
-            value = self.defaultValues()[name]
-            setFunction(index+1, value)
-        return value
-
-    #------------------------------------------------------------------
-    # Syrthes instance name
-    #------------------------------------------------------------------
-    @Variables.undoLocal
-    def setSyrthesInstanceName(self, num, value):
-        """
-        Set value of Syrthes instance name.
-
-        @type num: C{Int}
-        @param num: Syrthes coupling number
-        @type value: C{String}
-        @param value: Syrthes instance name
-        """
-        self.isLowerOrEqual(num, self.__getNumberOfSyrthesCoupling())
-        self.isStr(value)
-        node = self.__node_syr.xmlGetNodeList('syrthes')[num-1]
-        node.xmlSetData('syrthes_name', value)
-
+    def setSyrthesVerbosity(self, value):
+        self.__node_syr.xmlSetData('verbosity', value)
 
     @Variables.noUndo
-    def getSyrthesInstanceName(self, num):
-        """
-        Get value of Syrthes instance name.
+    def getSyrthesVerbosity(self):
+        return self.__node_syr.xmlGetString('verbosity')
 
-        @type num: C{Int}
-        @param num: Syrthes coupling number
-        @return: Syrthes verbosity
-        @rtype: C{String}
-        """
-        return self.__getStringData(num-1,
-                                    'syrthes_name',
-                                    self.setSyrthesInstanceName)
-
-    #------------------------------------------------------------------
-    # Syrthes verbosity
-    #------------------------------------------------------------------
     @Variables.undoLocal
-    def setSyrthesVerbosity(self, num, value):
-        """
-        Set value of Syrthes verbosity.
-
-        @type num: C{Int}
-        @param num: Syrthes coupling number
-        @type value: C{Int}
-        @param value: Syrthes verbosity
-        """
-        self.isLowerOrEqual(num, self.__getNumberOfSyrthesCoupling())
-        self.isInt(value)
-        self.isGreaterOrEqual(value, 0)
-        node = self.__node_syr.xmlGetNodeList('syrthes')[num-1]
-        node.xmlSetData('verbosity', value)
-
+    def setSyrthesVisualization(self, value):
+        self.__node_syr.xmlSetData('visualization', value)
 
     @Variables.noUndo
-    def getSyrthesVerbosity(self, num):
-        """
-        Get value of Syrthes verbosity.
+    def getSyrthesVisualization(self):
+        return self.__node_syr.xmlGetString('visualization')
 
-        @type num: C{Int}
-        @param num: Syrthes coupling number
-        @return: Syrthes verbosity
-        @rtype: C{Int}
-        """
-        return self.__getIntData(num-1,
-                                 'verbosity',
-                                 self.setSyrthesVerbosity)
-
-    #------------------------------------------------------------------
-    # Syrthes visualization output
-    #------------------------------------------------------------------
     @Variables.undoLocal
-    def setSyrthesVisualization(self, num, value):
-        """
-        Set value of Syrthes visualization.
-
-        @type num: C{Int}
-        @param num: Syrthes coupling number
-        @type value: C{Int}
-        @param value: Syrthes visualization
-        """
-        self.isLowerOrEqual(num, self.__getNumberOfSyrthesCoupling())
-        self.isInt(value)
-        self.isGreaterOrEqual(value, 0)
-        node = self.__node_syr.xmlGetNodeList('syrthes')[num-1]
-        node.xmlSetData('visualization', value)
-
+    def setSyrthesProjectionAxis(self, value):
+        self.__node_syr.xmlSetData('projection_axis', value)
 
     @Variables.noUndo
-    def getSyrthesVisualization(self, num):
-        """
-        Get value of Syrthes visualization.
-
-        @type num: C{Int}
-        @param num: Syrthes coupling number
-        @return: Syrthes visualization
-        @rtype: C{Int}
-        """
-        return self.__getIntData(num-1,
-                                 'visualization',
-                                 self.setSyrthesVisualization)
-
-    #------------------------------------------------------------------
-    # Projection axis
-    #------------------------------------------------------------------
-    @Variables.undoLocal
-    def setSyrthesProjectionAxis(self, num, value):
-        """
-        Set value of Syrthes projection axis.
-
-        @type num: C{Int}
-        @param num: Syrthes coupling number
-        @type value: C{String}
-        @param value: Syrthes projection axis
-        """
-        self.isLowerOrEqual(num, self.__getNumberOfSyrthesCoupling())
-        self.isStr(value)
-        node = self.__node_syr.xmlGetNodeList('syrthes')[num-1]
-        node.xmlSetData('projection_axis', value)
-
-
-    @Variables.noUndo
-    def getSyrthesProjectionAxis(self, num):
-        """
-        Get value of Syrthes projection axis.
-
-        @type num: C{Int}
-        @param num: Syrthes coupling number
-        @return: Syrthes projection axis
-        @rtype: C{String}
-        """
-        return self.__getStringData(num-1,
-                                    'projection_axis',
-                                    self.setSyrthesVerbosity)
-
-    #------------------------------------------------------------------
-    # Selection criteria
-    #------------------------------------------------------------------
-    @Variables.undoLocal
-    def setSelectionCriteria(self, num, value):
-        """
-        Set value of selection criteria.
-
-        @type num: C{Int}
-        @param num: Syrthes coupling number
-        @type value: C{String}
-        @param value: selection criteria
-        """
-        self.isLowerOrEqual(num, self.__getNumberOfSyrthesCoupling())
-        self.isStr(value)
-        node = self.__node_syr.xmlGetNodeList('syrthes')[num-1]
-        node.xmlSetData('selection_criteria', value)
-
-
-    @Variables.noUndo
-    def getSelectionCriteria(self, num):
-        """
-        Get value of selection criteria.
-
-        @type num: C{Int}
-        @param num: Syrthes coupling number
-        @return: selection criteria
-        @rtype: C{String}
-        """
-        return self.__getStringData(num-1,
-                                    'selection_criteria',
-                                    self.setSelectionCriteria)
+    def getSyrthesProjectionAxis(self):
+        return self.__node_syr.xmlGetString('projection_axis')
 
 #-------------------------------------------------------------------------------
 # ConjugateHeatTransferModel test case

commit bd86f7b6bae0a837c8b3caf6e96e1a5c9b54fedc

[GUI] Fix incorrect hide/show of the syrthes instance lineEdit for NCFD boundary conditions
  * Bug introduced in commit c29ad329

gui/Pages/BoundaryConditionsEnergyView.py
@@ -108,6 +108,9 @@ class BoundaryConditionsEnergyView(QWidget, Ui_BoundaryConditionsEnergy) :
         """
         self.__boundary = boundary
 
+        # By default hide the syrthes instance line
+        self.lineEditSyrthes.hide()
+
         for nb in range(len(self.__modelEnergy.getItems())):
             self.__modelEnergy.delItem(0)
 