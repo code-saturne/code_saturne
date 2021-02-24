code-saturne/code_saturne

commit d60bd382e4cce16737817e531e7cc2800df542eb

[GUI] Add more options for probes/profiles:
  * User can now choose whether or not to snap a probe/profile point
    which is currently the default behavior. When snap is activated for
    a set of points and several are contained within the same cell, we
    only keep the one closest to the cell center.
  * User can now activate the interpolation for probes/profiles, hence
    providing an interpolated value at the chosen coordinate instead of
    the value of the closest cell center.
  * For better robustness for the interpolation, users can now only
    define fields as output for probes/profiles, and no longer only a
    component. For example, for the velocity, all 3 components will be
    written. For older xml files which contained components, the latter
    are kept for the sake of compatibility.

bin/model/OutputControlModel.py
@@ -74,6 +74,8 @@ class OutputControlModel(Model):
         default['probe_recording_frequency_time'] = 0.1
         default['probe_format'] = "CSV"
         default['coordinate'] = 0.0
+        default['probes_snap'] = 'snap_to_center'
+        default['probes_interpolation'] = 'no'
 
         return default
 
@@ -1079,6 +1081,56 @@ class OutputControlModel(Model):
         """
         self.isInList(choice, ('DAT', 'CSV'))
         node = self.node_out.xmlInitNode('probe_format', 'choice')
+        node['choice'] = choice
+
+
+    @Variables.noUndo
+    def getMonitoringPointsSnap(self):
+        """
+        Return status of SnapToCenter activation or not.
+        """
+
+        node = self.node_out.xmlInitNode('probes_snap', 'choice')
+        choice = node['choice']
+        if not choice:
+            choice = self.defaultInitialValues()['probes_snap']
+            self.setMonitoringPointsSnap(choice)
+
+        return choice
+
+
+    @Variables.undoLocal
+    def setMonitoringPointsSnap(self, choice):
+        """
+        Set choice of snap mode
+        """
+
+        node = self.node_out.xmlInitNode('probes_snap', 'choice')
+        node['choice'] = choice
+
+
+    @Variables.noUndo
+    def getMonitoringPointsInterpolation(self):
+        """
+        Return status of probes interpolation activation.
+        """
+
+        node = self.node_out.xmlInitNode('probes_interpolation', 'choice')
+        choice = node['choice']
+        if not choice:
+            choice = self.defaultInitialValues()['probes_interpolation']
+            self.setMonitoringPointsInterpolation(choice)
+
+        return choice
+
+
+    @Variables.undoLocal
+    def setMonitoringPointsInterpolation(self, choice):
+        """
+        Activate/deactivate interpolation for monitoring points.
+        """
+
+        node = self.node_out.xmlInitNode('probes_interpolation', 'choice')
         node['choice'] = choice
 
 

bin/model/ProfilesModel.py
@@ -75,13 +75,15 @@ class ProfilesModel(Model):
         Returns a dictionnary with default values.
         """
         value = {}
-        value['nfreq']     = -1
-        value['formula']   =  "x = 0;\ny = s;\nz = 0;\n"
-        value['points']    =  200
-        value['label']     =  "profile"
-        value['choice']    =  "frequency"
-        value['frequency'] =  1
-        value['format']    =  "CSV"
+        value['nfreq']         = -1
+        value['formula']       =  "x = 0;\ny = s;\nz = 0;\n"
+        value['points']        =  200
+        value['label']         =  "profile"
+        value['choice']        =  "frequency"
+        value['frequency']     =  1
+        value['format']        =  "CSV"
+        value['snap_mode']     =  "snap_to_center"
+        value['interpolation'] =  "no"
 
         return value
 
@@ -161,6 +163,47 @@ class ProfilesModel(Model):
         self.isInList(label, self.getProfilesLabelsList())
         node = self.node_prof.xmlGetNode('profile', label = label)
         return node.xmlGetInt('points')
+
+
+    @Variables.undoLocal
+    def setSnapMode(self, label, mode):
+        """
+        """
+        node = self.node_prof.xmlGetNode('profile', label=label)
+        node.xmlSetData('snap_mode', mode)
+
+
+    @Variables.noUndo
+    def getSnapMode(self, label):
+        """
+        """
+        node = self.node_prof.xmlGetNode('profile', label = label)
+        mode = node.xmlGetString('snap_mode')
+        if not mode:
+            mode = self.__defaultValues()['snap_mode']
+            self.setSnapMode(label, mode)
+        return mode
+
+
+    @Variables.undoLocal
+    def setProfileInterpolation(self, label, state):
+        """
+        """
+        node = self.node_prof.xmlGetNode('profile', label = label)
+        node.xmlSetData('interpolation', state)
+
+
+    @Variables.noUndo
+    def getProfileInterpolation(self, label):
+        """
+        """
+        node = self.node_prof.xmlGetNode('profile', label = label)
+        state = node.xmlGetString('interpolation')
+        if not state:
+            state = self.__defaultValues()["interpolation"]
+            self.setProfileInterpolation(label, state)
+
+        return state
 
 
     @Variables.noUndo

src/gui/cs_gui_output.c
@@ -567,13 +567,20 @@ _define_profiles(void)
 
     cs_probe_set_auto_var(pset, false);
 
-    /* TODO: add option in GUI to select betwen "snap to cell center"
-       and interpolation (or other combinations) */
-#if 1
-    cs_probe_set_snap_mode(pset, CS_PROBE_SNAP_ELT_CENTER);
-#else
-    cs_probe_set_option(pset, "interpolation", "1");
-#endif
+    /* Set snap mode. Default is "SNAP_TO_CENTER" */
+    const char *snap_mode = _get_profile_child_str(tn, "snap_mode");
+    if (cs_gui_strcmp(snap_mode, "snap_to_vertex"))
+      cs_probe_set_snap_mode(pset, CS_PROBE_SNAP_VERTEX);
+    else if (cs_gui_strcmp(snap_mode, "none"))
+      cs_probe_set_snap_mode(pset, CS_PROBE_SNAP_NONE);
+    else
+      cs_probe_set_snap_mode(pset, CS_PROBE_SNAP_ELT_CENTER);
+
+    /* Activate interpolation if needed. Default is no */
+    const char *activate_interpolation =
+      _get_profile_child_str(tn, "interpolation");
+    if (cs_gui_strcmp(activate_interpolation, "yes"))
+      cs_probe_set_option(pset, "interpolation", "1");
 
     if (cs_glob_mesh->time_dep > CS_MESH_FIXED)
       cs_probe_set_option(pset, "transient_location", "true");
@@ -856,10 +863,30 @@ cs_gui_postprocess_meshes(void)
       strcpy(probe_labels[i], pn);
     }
 
-    cs_probe_set_create_from_array("probes",
-                                   n_probes,
-                                   (const cs_real_3_t *)p_coords,
-                                   (const char **)probe_labels);
+    cs_probe_set_t *pset =
+      cs_probe_set_create_from_array("probes",
+                                     n_probes,
+                                     (const cs_real_3_t *)p_coords,
+                                     (const char **)probe_labels);
+
+    /* Set snap mode. Default is "SNAP_TO_CENTER" */
+    const char *snap_mode =
+      cs_tree_node_get_tag(cs_tree_node_get_child(tn_o, "probes_snap"),
+                           "choice");
+    if (cs_gui_strcmp(snap_mode, "snap_to_vertex"))
+      cs_probe_set_snap_mode(pset, CS_PROBE_SNAP_VERTEX);
+    else if (cs_gui_strcmp(snap_mode, "none"))
+      cs_probe_set_snap_mode(pset, CS_PROBE_SNAP_NONE);
+    else
+      cs_probe_set_snap_mode(pset, CS_PROBE_SNAP_ELT_CENTER);
+
+    /* Activate interpolation if needed. Default is no */
+    const char *activate_interpolation =
+      cs_tree_node_get_tag(cs_tree_node_get_child(tn_o, "probes_interpolation"),
+                           "choice");
+    if (cs_gui_strcmp(activate_interpolation, "yes"))
+      cs_probe_set_option(pset, "interpolation", "1");
+
 
     BFT_FREE(p_coords);
 

bin/model/OutputVolumicVariablesModel.py
@@ -143,7 +143,10 @@ class OutputVolumicVariablesModel(Variables, Model):
         return l
 
 
-    def getVolumeFieldsLabel2Name(self, constant=False, time_averages=True):
+    def getVolumeFieldsLabel2Name(self,
+                                  constant=False,
+                                  time_averages=True,
+                                  get_components=False):
         """
         Returns a dictionnary to relate name and label to volume fields
         (variables, properties) for time averages and profiles.
@@ -158,42 +161,40 @@ class OutputVolumicVariablesModel(Variables, Model):
             if not label:
                 label = name
 
-            dim = node['dimension']
-            if dim and int(dim) > 1:
-                # If we consider the Rij tensor, the user will see
-                # R11, R22, ... in the GUI instead of Rij[0], Rij[1], ...
-                # This choice was considered as the clearest.
-                if name == 'rij':
-                    rij_lbls = ['R11', 'R22', 'R33', 'R12', 'R23', 'R13']
-                    for ii in range(int(dim)):
-                        label1 = rij_lbls[ii]
-                        if not (node['support'] and node['support'] == "boundary"):
-                            dicoLabel2Name[label1] = (name, str(ii))
-                    if not (node['support'] and node['support'] == "boundary"):
-                        dicoLabel2Name[label] = (name, str(-1))
+            # Check that we wish to show the field
+            if not (node['support'] and node['support'] == "boundary"):
+                # Add main field. "-1" is used as default since for
+                # vectors/tensors it yields all components
+                dicoLabel2Name[label] = (name, str(-1))
 
-                elif 'reynolds_stress' in name:
-                    comp = label.split('reynolds_stress')[-1]
-                    rij_lbls = ['reynolds_stress11', 'reynolds_stress22',
-                                'reynolds_stress33', 'reynolds_stress12',
-                                'reynolds_stress23', 'reynolds_stress13']
-                    for ii in range(int(dim)):
-                        label1 = rij_lbls[ii] + comp
-                        if not (node['support'] and node['support'] == 'boundary'):
+                # Check if we want to also get components
+                dim = node['dimension']
+                if dim and int(dim) > 1 and get_components:
+                    # If we consider the Rij tensor, the user will see
+                    # R11, R22, ... in the GUI instead of Rij[0], Rij[1], ...
+                    # This choice was considered as the clearest.
+                    if name == 'rij':
+                        rij_lbls = ['R11', 'R22', 'R33', 'R12', 'R23', 'R13']
+                        for ii in range(int(dim)):
+                            label1 = rij_lbls[ii]
                             dicoLabel2Name[label1] = (name, str(ii))
-                    if not (node['support'] and node['support'] == 'boundary'):
-                        dicoLabel2Name[label] = (name, str(-1))
 
-                else:
-                    for ii in range(int(dim)):
-                        label1 = label + "[" + str(ii) + "]"
-                        if not (node['support'] and node['support'] == "boundary"):
+                    elif 'reynolds_stress' in name:
+                        comp = label.split('reynolds_stress')[-1]
+                        rij_lbls = ['reynolds_stress11',
+                                    'reynolds_stress22',
+                                    'reynolds_stress33',
+                                    'reynolds_stress12',
+                                    'reynolds_stress23',
+                                    'reynolds_stress13']
+                        for ii in range(int(dim)):
+                            label1 = rij_lbls[ii] + comp
                             dicoLabel2Name[label1] = (name, str(ii))
-                    if not (node['support'] and node['support'] == "boundary"):
-                        dicoLabel2Name[label] = (name, str(-1))
-            else:
-                if not (node['support'] and node['support'] == "boundary"):
-                    dicoLabel2Name[label] = (name, str(0))
+
+                    else:
+                        for ii in range(int(dim)):
+                            label1 = label + "[" + str(ii) + "]"
+                            dicoLabel2Name[label1] = (name, str(ii))
 
         return dicoLabel2Name
 

bin/model/TimeAveragesModel.py
@@ -88,7 +88,8 @@ class TimeAveragesModel(Model):
         """
         mdl = OutputVolumicVariablesModel(self.case)
 
-        self.dicoLabel2Name = mdl.getVolumeFieldsLabel2Name(time_averages=False)
+        # For time averages, we also consider field components
+        self.dicoLabel2Name = mdl.getVolumeFieldsLabel2Name(time_averages=False, get_components=True)
 
         return self.dicoLabel2Name
 

gui/Pages/OutputControlForm.ui
@@ -27,7 +27,7 @@
       <string/>
      </property>
      <property name="currentIndex">
-      <number>0</number>
+      <number>4</number>
      </property>
      <widget class="QWidget" name="tabOUTPUT">
       <attribute name="title">
@@ -1302,6 +1302,37 @@
                <size>
                 <width>18</width>
                 <height>22</height>
+               </size>
+              </property>
+             </spacer>
+            </item>
+           </layout>
+          </item>
+          <item row="3" column="0">
+           <layout class="QHBoxLayout" name="horizontalLayout_4">
+            <item>
+             <widget class="QCheckBox" name="checkBoxSnapToCenter">
+              <property name="text">
+               <string>One probe per cell</string>
+              </property>
+             </widget>
+            </item>
+            <item>
+             <widget class="QCheckBox" name="checkBoxActivateInterpolation">
+              <property name="text">
+               <string>Activate interpolation</string>
+              </property>
+             </widget>
+            </item>
+            <item>
+             <spacer name="horizontalSpacer_3">
+              <property name="orientation">
+               <enum>Qt::Horizontal</enum>
+              </property>
+              <property name="sizeHint" stdset="0">
+               <size>
+                <width>40</width>
+                <height>20</height>
                </size>
               </property>
              </spacer>

gui/Pages/OutputControlView.py
@@ -1533,6 +1533,10 @@ class OutputControlView(QWidget, Ui_OutputControlForm):
         self.comboBoxTimePlot.activated[str].connect(self.slotMonitoringPoint)
         self.comboBoxProbeFmt.activated[str].connect(self.slotOutputProbeFmt)
 
+        # Check box
+        self.checkBoxSnapToCenter.clicked.connect(self.slotProbeSnapMode)
+        self.checkBoxActivateInterpolation.clicked.connect(self.slotProbesInterpolation)
+
 
         # Validators
 
@@ -1650,6 +1654,16 @@ class OutputControlView(QWidget, Ui_OutputControlForm):
             self.__insertMonitoringPoint(num, name, X, Y, Z)
             if self.case['salome']:
                 self.__salomeHandlerAddMonitoringPoint(name, X, Y, Z)
+
+        if self.mdl.getMonitoringPointsSnap() == 'snap_to_center':
+            self.checkBoxSnapToCenter.setChecked(True)
+        else:
+            self.checkBoxSnapToCenter.setChecked(False)
+
+        if self.mdl.getMonitoringPointsInterpolation() == 'yes':
+            self.checkBoxActivateInterpolation.setChecked(True)
+        else:
+            self.checkBoxActivateInterpolation.setChecked(False)
 
         # Writer initialisation
 
@@ -2877,6 +2891,30 @@ class OutputControlView(QWidget, Ui_OutputControlForm):
         if self.lineEditProbesRadius.validator().state == QValidator.Acceptable:
             r = from_qvariant(text, float)
             self.case['probes'].setRadius(r)
+
+
+    @pyqtSlot()
+    def slotProbeSnapMode(self):
+       """
+       Set snap mode for probes.
+       """
+
+       if self.checkBoxSnapToCenter.isChecked():
+           self.mdl.setMonitoringPointsSnap("snap_to_center")
+       else:
+           self.mdl.setMonitoringPointsSnap("none")
+
+
+    @pyqtSlot()
+    def slotProbesInterpolation(self):
+        """
+        Activate interpolation for probes.
+        """
+
+        if self.checkBoxActivateInterpolation.isChecked():
+            self.mdl.setMonitoringPointsInterpolation("yes")
+        else:
+            self.mdl.setMonitoringPointsInterpolation("no")
 
 
     def isSteady(self):

commit 74a61afbf55a417d289a48ee3a7f818b28003207

Fix crash introduced by commit d60bd382 (today).
Additional XML settings should be optional, and removed from XML
when matching defaults. The reader should not assume they are present.

bin/model/OutputControlModel.py
@@ -1094,7 +1094,6 @@ class OutputControlModel(Model):
         choice = node['choice']
         if not choice:
             choice = self.defaultInitialValues()['probes_snap']
-            self.setMonitoringPointsSnap(choice)
 
         return choice
 
@@ -1105,8 +1104,13 @@ class OutputControlModel(Model):
         Set choice of snap mode
         """
 
-        node = self.node_out.xmlInitNode('probes_snap', 'choice')
-        node['choice'] = choice
+        if choice == self.defaultInitialValues()['probes_snap']:
+            node = self.node_out.xmlGetNode('probes_snap', 'choice')
+            if node:
+                node.xmlRemoveNode()
+        else:
+            node = self.node_out.xmlInitNode('probes_snap', 'choice')
+            node['choice'] = choice
 
 
     @Variables.noUndo
@@ -1119,7 +1123,6 @@ class OutputControlModel(Model):
         choice = node['choice']
         if not choice:
             choice = self.defaultInitialValues()['probes_interpolation']
-            self.setMonitoringPointsInterpolation(choice)
 
         return choice
 
@@ -1130,8 +1133,13 @@ class OutputControlModel(Model):
         Activate/deactivate interpolation for monitoring points.
         """
 
-        node = self.node_out.xmlInitNode('probes_interpolation', 'choice')
-        node['choice'] = choice
+        if choice == self.defaultInitialValues()['probes_interpolation']:
+            node = self.node_out.xmlGetNode('probes_interpolation', 'choice')
+            if node:
+                node.xmlRemoveNode()
+        else:
+            node = self.node_out.xmlInitNode('probes_interpolation', 'choice')
+            node['choice'] = choice
 
 
     @Variables.undoLocal

bin/model/ProfilesModel.py
@@ -170,7 +170,10 @@ class ProfilesModel(Model):
         """
         """
         node = self.node_prof.xmlGetNode('profile', label=label)
-        node.xmlSetData('snap_mode', mode)
+        if mode == self.__defaultValues()['snap_mode']:
+            node.xmlRemoveChild('snap_mode')
+        else:
+            node.xmlSetData('snap_mode', mode)
 
 
     @Variables.noUndo
@@ -181,7 +184,6 @@ class ProfilesModel(Model):
         mode = node.xmlGetString('snap_mode')
         if not mode:
             mode = self.__defaultValues()['snap_mode']
-            self.setSnapMode(label, mode)
         return mode
 
 
@@ -190,7 +192,10 @@ class ProfilesModel(Model):
         """
         """
         node = self.node_prof.xmlGetNode('profile', label = label)
-        node.xmlSetData('interpolation', state)
+        if state == self.__defaultValues()["interpolation"]:
+            node.xmlRemoveChild('interpolation')
+        else:
+            node.xmlSetData('interpolation', state)
 
 
     @Variables.noUndo
@@ -201,7 +206,6 @@ class ProfilesModel(Model):
         state = node.xmlGetString('interpolation')
         if not state:
             state = self.__defaultValues()["interpolation"]
-            self.setProfileInterpolation(label, state)
 
         return state
 

src/gui/cs_gui_output.c
@@ -568,7 +568,7 @@ _define_profiles(void)
     cs_probe_set_auto_var(pset, false);
 
     /* Set snap mode. Default is "SNAP_TO_CENTER" */
-    const char *snap_mode = _get_profile_child_str(tn, "snap_mode");
+    const char *snap_mode = cs_tree_node_get_child_value_str(tn, "snap_mode");
     if (cs_gui_strcmp(snap_mode, "snap_to_vertex"))
       cs_probe_set_snap_mode(pset, CS_PROBE_SNAP_VERTEX);
     else if (cs_gui_strcmp(snap_mode, "none"))
@@ -577,8 +577,8 @@ _define_profiles(void)
       cs_probe_set_snap_mode(pset, CS_PROBE_SNAP_ELT_CENTER);
 
     /* Activate interpolation if needed. Default is no */
-    const char *activate_interpolation =
-      _get_profile_child_str(tn, "interpolation");
+    const char *activate_interpolation
+      = cs_tree_node_get_child_value_str(tn, "interpolation");
     if (cs_gui_strcmp(activate_interpolation, "yes"))
       cs_probe_set_option(pset, "interpolation", "1");
 
@@ -870,9 +870,9 @@ cs_gui_postprocess_meshes(void)
                                      (const char **)probe_labels);
 
     /* Set snap mode. Default is "SNAP_TO_CENTER" */
-    const char *snap_mode =
-      cs_tree_node_get_tag(cs_tree_node_get_child(tn_o, "probes_snap"),
-                           "choice");
+    const char *snap_mode
+      = cs_tree_node_get_tag(cs_tree_node_get_child(tn_o, "probes_snap"),
+                             "choice");
     if (cs_gui_strcmp(snap_mode, "snap_to_vertex"))
       cs_probe_set_snap_mode(pset, CS_PROBE_SNAP_VERTEX);
     else if (cs_gui_strcmp(snap_mode, "none"))
@@ -881,9 +881,10 @@ cs_gui_postprocess_meshes(void)
       cs_probe_set_snap_mode(pset, CS_PROBE_SNAP_ELT_CENTER);
 
     /* Activate interpolation if needed. Default is no */
-    const char *activate_interpolation =
-      cs_tree_node_get_tag(cs_tree_node_get_child(tn_o, "probes_interpolation"),
-                           "choice");
+    const char *activate_interpolation
+      = cs_tree_node_get_tag(cs_tree_node_get_child(tn_o,
+                                                    "probes_interpolation"),
+                             "choice");
     if (cs_gui_strcmp(activate_interpolation, "yes"))
       cs_probe_set_option(pset, "interpolation", "1");
 