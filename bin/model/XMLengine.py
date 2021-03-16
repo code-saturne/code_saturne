code-saturne/code_saturne

commit 4ec3ce8c6464040859934cc46471b2a79b276412

More XML cleanup upon visit/edit.

bin/model/XMLengine.py
@@ -642,7 +642,7 @@ class XMLElement:
         log.debug("xmlSetTextNode-> %s" % self.__xmlLog())
 
 
-    def xmlSetData(self, tag, textNode, *attrList, **kwargs):
+    def xmlSetData(self, tag, textNode, default=None, *attrList, **kwargs):
         """
         Set the textNode in an elementNode, which is a single node.
         If the searched 'tag' doesn't exist, it is created
@@ -656,21 +656,28 @@ class XMLElement:
         #
         if type(textNode) == float: textNode = str("%.12g" % (textNode))
 
+        if type(default) == float: default = str("%.12g" % (default))
+        if textNode == default:
+            textNode = None
+
         nodeList = self._childNodeList(tag, *attrList, **kwargs)
         elementList = []
 
         # Add a new ELEMENT_NODE and a new TEXT_NODE
         # or replace an existing TEXT_NODE
         #
-        if not nodeList:
+        if textNode and not nodeList:
             child = self.xmlAddChild(tag, *attrList, **kwargs)
             child.xmlSetTextNode(textNode)
             elementList.append(child)
-        else:
+        elif nodeList:
             for node in nodeList:
                 child = self._inst(node)
-                child.xmlSetTextNode(textNode)
-                elementList.append(child)
+                if textNode:
+                    child.xmlSetTextNode(textNode)
+                    elementList.append(child)
+                else:
+                    child.xmlRemoveNode()
 
         return elementList
 

bin/model/NumericalParamGlobalModel.py
@@ -86,11 +86,12 @@ class NumericalParamGlobalModel(Model):
         """
         Return status of transposed gradient
         """
-        node = self.node_np.xmlInitNode('gradient_transposed', 'status')
-        status = node['status']
+        status = None
+        node = self.node_np.xmlGetNode('gradient_transposed')
+        if node:
+            status = node['status']
         if not status:
             status = self._defaultValues()['gradient_transposed']
-            self.setTransposedGradient(status)
         return status
 
 
@@ -99,11 +100,12 @@ class NumericalParamGlobalModel(Model):
         """
         Return status of IPUCOU value is activated or not
         """
-        node = self.node_np.xmlInitNode('velocity_pressure_coupling', 'status')
-        status = node['status']
+        status = None
+        node = self.node_np.xmlGetNode('velocity_pressure_coupling')
+        if node:
+            status = node['status']
         if not status:
             status = self._defaultValues()['velocity_pressure_coupling']
-            self.setVelocityPressureCoupling(status)
         return status
 
 
@@ -112,11 +114,10 @@ class NumericalParamGlobalModel(Model):
         """
         Return status of ICFGRP value (for hydrostatic equilibrium) is activated or not
         """
-        node = self.node_np.xmlInitNode('hydrostatic_equilibrium', 'status')
+        node = self.node_np.xmlGetNode('hydrostatic_equilibrium')
         status = node['status']
         if not status:
             status = self._defaultValues()['hydrostatic_equilibrium']
-            self.setHydrostaticEquilibrium(status)
         return status
 
 
@@ -143,7 +144,6 @@ class NumericalParamGlobalModel(Model):
         value = self.node_np.xmlGetDouble('pressure_relaxation')
         if value == None:
             value = self._defaultValues()['pressure_relaxation']
-            self.setPressureRelaxation(value)
         return value
 
 
@@ -165,7 +165,7 @@ class NumericalParamGlobalModel(Model):
         Return gradient reconstruction method
         """
         choice = None
-        node = self.node_np.xmlGetNode('gradient_reconstruction', 'choice')
+        node = self.node_np.xmlGetNode('gradient_reconstruction')
         if node:
             choice = node['choice']
         if not choice:
@@ -179,7 +179,7 @@ class NumericalParamGlobalModel(Model):
         Return extended neighborhood type
         """
         choice = None
-        node = self.node_np.xmlGetNode('extended_neighborhood', 'choice')
+        node = self.node_np.xmlGetNode('extended_neighborhood')
         if node:
             choice = node['choice']
         if not choice:
@@ -222,6 +222,7 @@ class NumericalParamGlobalModel(Model):
         else:
             for node in node_ipucou.xmlGetNodeList('property'):
                 node.xmlRemoveNode()
+            node_ipucou.xmlRemoveNode()
 
 
     @Variables.undoLocal
@@ -256,7 +257,8 @@ class NumericalParamGlobalModel(Model):
         Put value of pressure_relaxation
         """
         self.isPositiveFloat(value)
-        self.node_np.xmlSetData('pressure_relaxation', value)
+        self.node_np.xmlSetData('pressure_relaxation', value,
+                                default=self._defaultValues()['pressure_relaxation'])
 
 
     @Variables.undoLocal
@@ -266,7 +268,8 @@ class NumericalParamGlobalModel(Model):
         """
         self.isGreaterOrEqual(value, 0.0)
         self.isLower(value, 1.0)
-        self.node_np.xmlSetData('density_relaxation', value)
+        self.node_np.xmlSetData('density_relaxation', value,
+                                default=self._defaultValues()['density_relaxation'])
 
 
     @Variables.undoLocal

bin/model/TimeStepModel.py
@@ -216,9 +216,11 @@ class TimeStepModel(Model):
         node['choice'] = value
         if value == 'simple' or value =='simplec':
             self.setVelocityPressureParamSweepNumber(1)
-        elif self.getVelocityPressureParamSweepNumber() < self.defaultValues()['piso_sweep_number']:
-            value = self.defaultValues()['piso_sweep_number']
-            self.setVelocityPressureParamSweepNumber(value)
+        else:
+            default = self.defaultValues()['piso_sweep_number']
+            if self.getVelocityPressureParamSweepNumber() < default:
+                value = default
+                self.setVelocityPressureParamSweepNumber(value)
 
 
     @Variables.noUndo
@@ -230,7 +232,6 @@ class TimeStepModel(Model):
         value = self.node_algo.xmlGetInt('piso_sweep_number')
         if not value:
             value = self.defaultValues()['piso_sweep_number']
-            self.setVelocityPressureParamSweepNumber(value)
         return value
 
 
@@ -241,7 +242,7 @@ class TimeStepModel(Model):
         """
         self.isInt(value)
         self.node_algo = self.node_np.xmlGetNode('velocity_pressure_algo')
-        self.node_algo.xmlSetData('piso_sweep_number',value)
+        self.node_algo.xmlSetData('piso_sweep_number', value, default=1)
 
 
     @Variables.noUndo

commit 08bb6dce61b9d0e61bde29455d98b496fafbd140

GUI: fix loss of some settings, broken by commit 4ec3ce8c (Jan 30 2021).

bin/model/XMLengine.py
@@ -673,7 +673,7 @@ class XMLElement:
         elif nodeList:
             for node in nodeList:
                 child = self._inst(node)
-                if textNode:
+                if textNode != None:
                     child.xmlSetTextNode(textNode)
                     elementList.append(child)
                 else: