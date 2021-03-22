code-saturne/code_saturne

commit bed71322820969834c4084eb6cc426065699eeb0

Fix Nusselt and thermal flux postprocessing at start of calculation (exchange coefficient at internally coupled faces is not computed yet, we skip coupling contribution calculation at first iteration).

src/base/post_util.f90
@@ -205,7 +205,8 @@ if (iscalt.gt.0) then
                 - flumab*cpp/srfbn*(coefap(ifac) + coefbp(ifac)*theipb(ifac))
     ! here bflux = 0 if current face is coupled
 
-    if (vcopt%icoupl.gt.0) then
+    if (vcopt%icoupl.gt.0.and.ntpabs.gt.1) then
+      ! FIXME exchange coefs not computed at start of calculation
       if (cpl_faces(ifac)) then
         heq = hextp(ifac) * hintp(ifac) / ((hextp(ifac) + hintp(ifac))*srfbn)
         bflux(iloc) = heq*(theipb(ifac)-dist_theipb(ifac))
@@ -417,7 +418,8 @@ if (itstar.ge.0 .and. itplus.ge.0) then
     numer = (cofafp(ifac) + cofbfp(ifac)*theipb(ifac)) * distb(ifac)
     ! here numer = 0 if current face is coupled
 
-    if (vcopt%icoupl.gt.0) then
+    if (vcopt%icoupl.gt.0.and.ntpabs.gt.1) then
+      ! FIXME exchange coefs not computed at start of calculation
       if (cpl_faces(ifac)) then
         heq = hextp(ifac) * hintp(ifac) / ((hextp(ifac) + hintp(ifac))*srfbn)
         numer = heq*(theipb(ifac)-dist_theipb(ifac)) * distb(ifac)

commit 73f03c9fad3db761ab555a2f85a5441b07442d40

Fix boundary thermal flux output when not a restart.
Broken by commit bed71322 (Jan 10 2020) dur to incorrect test.

src/base/post_util.f90
@@ -202,7 +202,7 @@ if (iscalt.gt.0) then
                 - flumab*cpp/srfbn*(coefap(ifac) + coefbp(ifac)*theipb(ifac))
     ! here bflux = 0 if current face is coupled
 
-    if (vcopt%icoupl.gt.0.and.ntpabs.gt.1) then
+    if (vcopt%icoupl.gt.0.and.ntcabs.gt.1) then
       ! FIXME exchange coefs not computed at start of calculation
       if (cpl_faces(ifac)) then
         heq = hextp(ifac) * hintp(ifac) / ((hextp(ifac) + hintp(ifac))*srfbn)
@@ -415,7 +415,7 @@ if (itstar.ge.0 .and. itplus.ge.0) then
     numer = (cofafp(ifac) + cofbfp(ifac)*theipb(ifac)) * distb(ifac)
     ! here numer = 0 if current face is coupled
 
-    if (vcopt%icoupl.gt.0.and.ntpabs.gt.1) then
+    if (vcopt%icoupl.gt.0.and.ntcabs.gt.1) then
       ! FIXME exchange coefs not computed at start of calculation
       if (cpl_faces(ifac)) then
         heq = hextp(ifac) * hintp(ifac) / ((hextp(ifac) + hintp(ifac))*srfbn)