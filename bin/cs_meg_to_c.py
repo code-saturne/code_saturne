code-saturne/code_saturne

commit 4d52701e950378530a8c2519310b6efeafeef06a

[GUI] Provide access to fluid volume/surface of zone for formulae

bin/cs_meg_to_c.py
@@ -195,7 +195,9 @@ _base_tokens = {'dt':'const cs_real_t dt = cs_glob_time_step->dt[0];',
                 't':'const cs_real_t t = cs_glob_time_step->t_cur;',
                 'iter':'const int iter = cs_glob_time_step->nt_cur;',
                 'volume':'const cs_real_t volume = zone->measure;',
-                'surface':'const cs_real_t volume = zone->measure;',
+                'fluid_volume':'const cs_real_t fluid_volume = zone->f_measure;',
+                'surface':'const cs_real_t surface = zone->measure;',
+                'fluid_surface':'const cs_real_t fluid_surface = zone->f_measure;'
                 'pi':'const cs_real_t pi = cs_math_pi;',
                 'uref':'const cs_real_t uref = cs_glob_turb_ref_values->uref;',
                 'almax':'const cs_real_t almax = cs_glob_turb_ref_values->almax;'}

bin/model/FluidCharacteristicsModel.py
@@ -966,6 +966,7 @@ class FluidCharacteristicsModel(Variables, Model):
         symbols.append(('p0', 'Reference pressure = ' + str(ref_pressure)))
 
         symbols.append(('volume', 'Zone volume'))
+        symbols.append(('fluid_volume', 'Zone fluid volume (different if porous)'))
 
         for (nme, val) in self.notebook.getNotebookList():
             symbols.append((nme, 'value (notebook) = ' + str(val)))
@@ -997,6 +998,7 @@ class FluidCharacteristicsModel(Variables, Model):
         symbols.append(('rho', 'Density'))
 
         symbols.append(('volume', 'Zone volume'))
+        symbols.append(('fluid_volume', 'Zone fluid volume (different if porous)'))
 
         from code_saturne.model.CompressibleModel import CompressibleModel
         if CompressibleModel(self.case).getCompressibleModel() == 'on':
@@ -1028,6 +1030,7 @@ class FluidCharacteristicsModel(Variables, Model):
         symbols.append(('p0', 'Reference pressure = ' + str(ref_pressure)))
 
         symbols.append(('volume', 'Zone volume'))
+        symbols.append(('fluid_volume', 'Zone fluid volume (different if porous)'))
 
         for (nme, val) in self.notebook.getNotebookList():
             symbols.append((nme, 'value (notebook) = ' + str(val)))
@@ -1055,6 +1058,7 @@ class FluidCharacteristicsModel(Variables, Model):
         symbols.append(('T', 'Temperature'))
 
         symbols.append(('volume', 'Zone volume'))
+        symbols.append(('fluid_volume', 'Zone fluid volume (different if porous)'))
 
         for (nme, val) in self.notebook.getNotebookList():
             symbols.append((nme, 'value (notebook) = ' + str(val)))
@@ -1079,6 +1083,7 @@ class FluidCharacteristicsModel(Variables, Model):
         symbols.append(('p0', 'Reference pressure = ' + str(ref_pressure)))
 
         symbols.append(('volume', 'Zone volume'))
+        symbols.append(('fluid_volume', 'Zone fluid volume (different if porous)'))
 
         for (nme, val) in self.notebook.getNotebookList():
             symbols.append((nme, 'value (notebook) = ' + str(val)))
@@ -1096,12 +1101,14 @@ class FluidCharacteristicsModel(Variables, Model):
         sym = [('x', 'cell center coordinate'),
                ('y', 'cell center coordinate'),
                ('z', 'cell center coordinate'),
-               ('volume', 'Zone volume')]
+               ('volume', 'Zone volume'),
+               ('fluid_volume', 'Zone fluid volume (different if porous)')]
         sym.append((str(scalar),str(scalar)))
         diff0_value = self.m_sca.getScalarDiffusivityInitialValue(scalar)
         sym.append((str(name)+'_ref', str(scalar)+' diffusion coefficient (reference value, m^2/s) = '+str(diff0_value)))
 
         sym.append(('volume', 'Zone volume'))
+        sym.append(('fluid_volume', 'Zone fluid volume (different if porous)'))
 
         for (nme, val) in self.notebook.getNotebookList():
             sym.append((nme, 'value (notebook) = ' + str(val)))

bin/model/GroundwaterLawModel.py
@@ -347,6 +347,7 @@ permeability=1.;"""
                ('z', 'cell center coordinate')]
 
         sym.append(('volume', 'Zone volume'))
+        sym.append(('fluid_volume', 'Zone fluid volume (different if porous)'))
 
         for (nme, val) in NotebookModel(self.case).getNotebookList():
             sym.append((nme, 'value (notebook) = ' + str(val)))

bin/model/InitializationModel.py
@@ -252,6 +252,7 @@ omega = k^0.5/almax;"""
                ('y','cell center coordinate'),
                ('z','cell center coordinate'),
                ('volume', 'Zone volume'),
+               ('fluid_volume', 'Zone fluid volume'),
                ('uref','reference velocity'),
                ('almax','reference length')]
 
@@ -304,7 +305,8 @@ omega = k^0.5/almax;"""
                ('x', 'cell center coordinate'),
                ('y', 'cell center coordinate'),
                ('z', 'cell center coordinate'),
-               ('volume', 'Zone volume')]
+               ('volume', 'Zone volume'),
+               ('fluid_volume', 'Zone fluid volume')]
 
         for (nme, val) in self.notebook.getNotebookList():
             sym.append((nme, 'value (notebook) = ' + str(val)))
@@ -351,7 +353,8 @@ omega = k^0.5/almax;"""
         sym = [('x', 'cell center coordinate'),
                ('y', 'cell center coordinate'),
                ('z', 'cell center coordinate'),
-               ('volume', 'Zone volume')]
+               ('volume', 'Zone volume'),
+               ('fluid_volume', 'Zone fluid volume')]
 
         for (nme, val) in self.notebook.getNotebookList():
             sym.append((nme, 'value (notebook) = ' + str(val)))
@@ -504,7 +507,8 @@ pressure = p0 + g * ro * z;\n"""
         sym = [('x', 'cell center coordinate'),
                ('y', 'cell center coordinate'),
                ('z', 'cell center coordinate'),
-               ('volume', 'Zone volume')]
+               ('volume', 'Zone volume'),
+               ('fluid_volume', 'Zone fluid volume')]
 
         for (nme, val) in self.notebook.getNotebookList():
             sym.append((nme, 'value (notebook) = ' + str(val)))
@@ -554,7 +558,8 @@ pressure = p0 + g * ro * z;\n"""
         sym = [('x', 'cell center coordinate'),
                ('y', 'cell center coordinate'),
                ('z', 'cell center coordinate'),
-               ('volume', 'Zone volume')]
+               ('volume', 'Zone volume'),
+               ('fluid_volume', 'Zone fluid volume')]
 
         for (nme, val) in self.notebook.getNotebookList():
             sym.append((nme, 'value (notebook) = ' + str(val)))
@@ -604,7 +609,8 @@ pressure = p0 + g * ro * z;\n"""
         sym = [('x', 'cell center coordinate'),
                ('y', 'cell center coordinate'),
                ('z', 'cell center coordinate'),
-               ('volume', 'Zone volume')]
+               ('volume', 'Zone volume'),
+               ('fluid_volume', 'Zone fluid volume')]
 
         for (nme, val) in self.notebook.getNotebookList():
             sym.append((nme, 'value (notebook) = ' + str(val)))
@@ -653,7 +659,8 @@ pressure = p0 + g * ro * z;\n"""
         sym = [('x', 'cell center coordinate'),
                ('y', 'cell center coordinate'),
                ('z', 'cell center coordinate'),
-               ('volume', 'Zone volume')]
+               ('volume', 'Zone volume'),
+               ('fluid_volume', 'Zone fluid volume')]
 
         for (nme, val) in self.notebook.getNotebookList():
             sym.append((nme, 'value (notebook) = ' + str(val)))
@@ -702,7 +709,8 @@ pressure = p0 + g * ro * z;\n"""
         sym = [('x', 'cell center coordinate'),
                ('y', 'cell center coordinate'),
                ('z', 'cell center coordinate'),
-               ('volume', 'Zone volume')]
+               ('volume', 'Zone volume'),
+               ('fluid_volume', 'Zone fluid volume')]
 
         for (nme, val) in self.notebook.getNotebookList():
             sym.append((nme, 'value (notebook) = ' + str(val)))
@@ -773,7 +781,8 @@ pressure = p0 + g * ro * z;\n"""
         sym = [('x', 'cell center coordinate'),
                ('y', 'cell center coordinate'),
                ('z', 'cell center coordinate'),
-               ('volume', 'Zone volume')]
+               ('volume', 'Zone volume'),
+               ('fluid_volume', 'Zone fluid volume')]
 
         for (nme, val) in self.notebook.getNotebookList():
             sym.append((nme, 'value (notebook) = ' + str(val)))
@@ -823,7 +832,8 @@ pressure = p0 + g * ro * z;\n"""
         sym = [('x', 'cell center coordinate'),
                ('y', 'cell center coordinate'),
                ('z', 'cell center coordinate'),
-               ('volume', 'Zone volume')]
+               ('volume', 'Zone volume'),
+               ('fluid_volume', 'Zone fluid volume')]
 
         for (nme, val) in self.notebook.getNotebookList():
             sym.append((nme, 'value (notebook) = ' + str(val)))
@@ -878,7 +888,8 @@ pressure = p0 + g * ro * z;\n"""
         sym = [('x', 'cell center coordinate'),
                ('y', 'cell center coordinate'),
                ('z', 'cell center coordinate'),
-               ('volume', 'Zone volume')]
+               ('volume', 'Zone volume'),
+               ('fluid_volume', 'Zone fluid volume')]
 
         for (nme, val) in self.notebook.getNotebookList():
             sym.append((nme, 'value (notebook) = ' + str(val)))

bin/model/SourceTermsModel.py
@@ -109,7 +109,8 @@ dSwdu = 0;\ndSwdv = 0;\ndSwdw = 0;\n"""
                ('y', 'cell center coordinate'),
                ('z', 'cell center coordinate'),
                ('t', 'current time'),
-               ('volume', 'Source terms zone volume')]
+               ('volume', 'Source terms zone volume'),
+               ('fluid_volume', 'Source terms zone fluid volume')]
 
         sym.append( ("u", 'x velocity component'))
         sym.append( ("v", 'y velocity component'))
@@ -162,7 +163,8 @@ dSwdu = 0;\ndSwdv = 0;\ndSwdw = 0;\n"""
                ('y', 'cell center coordinate'),
                ('z', 'cell center coordinate'),
                ('t', 'current time'),
-               ('volume', 'Source terms zone volume')]
+               ('volume', 'Source terms zone volume'),
+               ('fluid_volume', 'Source terms zone fluid volume')]
         name = self.th_sca.getScalarName(species)
         sym.append((name, 'current species'))
 
@@ -220,7 +222,8 @@ dSwdu = 0;\ndSwdv = 0;\ndSwdw = 0;\n"""
                ('y', 'cell center coordinate'),
                ('z', 'cell center coordinate'),
                ('t', 'current time'),
-               ('volume', 'Source terms zone volume')]
+               ('volume', 'Source terms zone volume'),
+               ('fluid_volume', 'Source terms zone fluid volume')]
 
         name = self.th_sca.getScalarName(species)
         sym.append((name, 'current species'))
@@ -276,7 +279,8 @@ dSwdu = 0;\ndSwdv = 0;\ndSwdw = 0;\n"""
                ('y', 'cell center coordinate'),
                ('z', 'cell center coordinate'),
                ('t', 'current time'),
-               ('volume', 'Source terms zone volume')]
+               ('volume', 'Source terms zone volume'),
+               ('fluid_volume', 'Source terms zone fluid volume')]
 
         for (nme, val) in self.notebook.getNotebookList():
             sym.append((nme, 'value (notebook) = ' + str(val)))
@@ -327,6 +331,7 @@ dSwdu = 0;\ndSwdv = 0;\ndSwdw = 0;\n"""
                ('z', 'cell center coordinate'),
                ('t', 'current time'),
                ('volume', 'Source terms zone volume'),
+               ('fluid_volume', 'Source terms zone fluid volume'),
                ('rho', 'density (kg/m^3)')]
 
         if self.case.module_name() == 'code_saturne':

commit cefdae6ed27a424eae02b411274d4a4ac6825de3

Fix bug introduced in commit 4d52701e (previous)

bin/cs_meg_to_c.py
@@ -197,7 +197,7 @@ _base_tokens = {'dt':'const cs_real_t dt = cs_glob_time_step->dt[0];',
                 'volume':'const cs_real_t volume = zone->measure;',
                 'fluid_volume':'const cs_real_t fluid_volume = zone->f_measure;',
                 'surface':'const cs_real_t surface = zone->measure;',
-                'fluid_surface':'const cs_real_t fluid_surface = zone->f_measure;'
+                'fluid_surface':'const cs_real_t fluid_surface = zone->f_measure;',
                 'pi':'const cs_real_t pi = cs_math_pi;',
                 'uref':'const cs_real_t uref = cs_glob_turb_ref_values->uref;',
                 'almax':'const cs_real_t almax = cs_glob_turb_ref_values->almax;'}