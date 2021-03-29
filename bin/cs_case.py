code-saturne/code_saturne

commit b870b06cd736814a1d4fa60ed5b7df0577f09c2a

Run script: define_domain_parameters now called after copying base setup files.

bin/cs_case.py
@@ -1119,7 +1119,7 @@ class case:
                                                    mpi_libdir)
         s.write('\n')
 
-        # HANDLE CATHARE COUPLING
+        # Handle cathare coupling
         wrote_cathare_path = False
         if self.domains:
             for d in self.domains:
@@ -1415,6 +1415,14 @@ class case:
 
         os.chdir(self.exec_dir)
 
+        # Set (read and possibly modify) script parameters
+
+        sys.stdout.write('Copying base setup data\n'
+                         '-----------------------\n\n')
+
+        for d in self.domains:
+            d.copy_data()
+
         # Compile user subroutines if necessary
         # (for some domain types, such as for Syrthes, this may be done later,
         # during the general prepare_data stage).
@@ -1428,9 +1436,8 @@ class case:
                 if need_compile == False: # Print banner on first pass
                     need_compile = True
                     msg = \
-                        " *********************************************\n" \
-                        "  Compiling and linking user-defined functions\n" \
-                        " *********************************************\n\n"
+                        "Compiling and linking user-defined functions\n" \
+                        "--------------------------------------------\n\n"
                     sys.stdout.write(msg)
                     sys.stdout.flush()
                 d.compile_and_link()
@@ -1446,10 +1453,8 @@ class case:
         # Setup data
         #===========
 
-        sys.stdout.write('\n'
-                         ' ****************************\n'
-                         '  Preparing calculation data\n'
-                         ' ****************************\n\n')
+        sys.stdout.write('Preparing calculation data\n'
+                         '--------------------------\n\n')
         sys.stdout.flush()
 
         for d in (self.domains + self.syr_domains + self.py_domains):
@@ -1578,10 +1583,8 @@ class case:
         # Preprocessing
         #==============
 
-        sys.stdout.write('\n'
-                         ' ***************************\n'
-                         '  Preprocessing calculation\n'
-                         ' ***************************\n\n')
+        sys.stdout.write('Preprocessing calculation\n'
+                         '-------------------------\n\n')
         sys.stdout.flush()
 
         self.summary_init(exec_env)
@@ -1633,10 +1636,8 @@ class case:
 
         self.update_scripts_tmp('ready', 'running')
 
-        sys.stdout.write('\n'
-                         ' **********************\n'
-                         '  Starting calculation\n'
-                         ' **********************\n\n')
+        sys.stdout.write('Starting calculation\n'
+                         '--------------------\n\n')
         sys.stdout.flush()
 
         # Maximum remaining time if defined through run configuration
@@ -1729,10 +1730,8 @@ class case:
 
         # Now save results
 
-        sys.stdout.write('\n'
-                         ' *****************************\n'
-                         '  Post-calculation operations\n'
-                         ' *****************************\n\n')
+        sys.stdout.write('Post-calculation operations\n'
+                         '---------------------------\n\n')
         sys.stdout.flush()
 
         self.summary_finalize()
@@ -1863,23 +1862,21 @@ class case:
         msg = \
             '\n' \
             + '                      ' + self.module_name + '\n' \
-            + '                      ************\n' \
+            + '                      ============\n' \
             + '\n' \
-            + ' Version:   ' + self.package.version + '\n' \
-            + ' Path:      ' + self.package.get_dir('exec_prefix') + '\n'
+            + 'Version:   ' + self.package.version + '\n' \
+            + 'Path:      ' + self.package.get_dir('exec_prefix') + '\n'
         if self.package_compute != self.package:
-            msg += '   compute: ' + self.package_compute.get_dir('exec_prefix') + '\n\n'
+            msg += '  compute: ' + self.package_compute.get_dir('exec_prefix') + '\n\n'
         else:
             msg += '\n'
-        msg += ' Result directory:\n' \
-               + '   ' +  str(self.result_dir) + '\n\n'
+        msg += 'Result directory:\n' \
+               + '  ' +  str(self.result_dir) + '\n\n'
 
         if self.exec_dir != self.result_dir:
-            msg += ' Working directory (to be periodically cleaned):\n' \
-                + '   ' +  str(self.exec_dir) + '\n'
+            msg += 'Working directory (to be periodically cleaned):\n' \
+                + '  ' +  str(self.exec_dir) + '\n\n'
             self.add_exec_dir_stamp()
-
-        msg += '\n'
 
         sys.stdout.write(msg)
         sys.stdout.flush()

bin/cs_case_domain.py
@@ -220,6 +220,15 @@ class base_domain:
 
     #---------------------------------------------------------------------------
 
+    def copy_data(self):
+        """
+        Copy base data to the execution directory
+        """
+
+        return
+
+    #---------------------------------------------------------------------------
+
     def copy_result(self, name, purge=False):
         """
         Copy a file or directory to the results directory,
@@ -446,6 +455,61 @@ class domain(base_domain):
 
     #---------------------------------------------------------------------------
 
+    def __set_case_parameters__(self):
+
+        # We may now import user python script functions if present.
+
+        self.user_locals = None
+
+        user_scripts = os.path.join(self.exec_dir, 'cs_user_scripts.py')
+        if os.path.isfile(user_scripts):
+            try:
+                exec(compile(open(user_scripts).read(), user_scripts, 'exec'),
+                     locals(),
+                     locals())
+                self.user_locals = locals()
+            except Exception:
+                execfile(user_scripts, locals(), locals())
+                self.user_locals = locals()
+
+        # We may now parse the optional XML parameter file
+        # now that its path may be built and checked.
+
+        param = self.param
+        if param == None:
+            if os.path.isfile(os.path.join(self.exec_dir, 'setup.xml')):
+                param = 'setup.xml'
+
+        if param != None:
+            version_str = '2.0'
+            P = cs_xml_reader.Parser(os.path.join(self.exec_dir, param),
+                                     version_str = version_str)
+            params = P.getParams()
+            for k in list(params.keys()):
+                self.__dict__[k] = params[k]
+
+            self.param = param
+
+            if params['xml_root_name'] == 'NEPTUNE_CFD_GUI':
+                solver_dir = self.package_compute.get_dir("pkglibexecdir")
+                solver_name = "nc_solver" + self.package_compute.config.exeext
+                self.solver_path = os.path.join(solver_dir, solver_name)
+
+        # Now override or complete data from the XML file.
+
+        if self.user_locals:
+            m = 'define_domain_parameters'
+            if m in self.user_locals.keys():
+                eval(m + '(self)', globals(), self.user_locals)
+                del self.user_locals[m]
+
+        # Finally, ensure some fields are of the required types
+
+        if type(self.meshes) != list:
+            self.meshes = [self.meshes,]
+
+    #---------------------------------------------------------------------------
+
     def __set_auto_restart__(self):
         """
         Select latest valid checkpoint directory for restart, based on name
@@ -528,51 +592,50 @@ class domain(base_domain):
 
             self.param = param
 
-            if params['xml_root_name'] == 'NEPTUNE_CFD_GUI':
-                solver_dir = self.package_compute.get_dir("pkglibexecdir")
-                solver_name = "nc_solver" + self.package_compute.config.exeext
-                self.solver_path = os.path.join(solver_dir, solver_name)
-
     #---------------------------------------------------------------------------
 
-    def set_case_dir(self, case_dir, staging_dir = None):
+    def copy_data(self):
+        """
+        Copy base data to the execution directory
+        """
 
-        # Names, directories, and files in case structure
+        err_str = ""
 
-        base_domain.set_case_dir(self, case_dir, staging_dir)
+        # Create the src folder if there are files to compile in source path
 
-        # We may now import user python script functions if present.
+        src_files = cs_compile.files_to_compile(self.src_dir)
+        if len(src_files) > 0:
+            exec_src = os.path.join(self.exec_dir, 'src')
 
-        self.user_locals = None
+            make_clean_dir(exec_src)
 
-        user_scripts = os.path.join(self.data_dir, 'cs_user_scripts.py')
-        if os.path.isfile(user_scripts):
-            try:
-                exec(compile(open(user_scripts).read(), user_scripts, 'exec'),
-                     locals(),
-                     locals())
-                self.user_locals = locals()
-            except Exception:
-                execfile(user_scripts, locals(), locals())
-                self.user_locals = locals()
+            # Add header files to list so as not to forget to copy them
+            dir_files = os.listdir(self.src_dir)
+            src_files = src_files + (  fnmatch.filter(dir_files, '*.h')
+                                     + fnmatch.filter(dir_files, '*.hxx')
+                                     + fnmatch.filter(dir_files, '*.hpp'))
 
-        # We may now parse the optional XML parameter file
-        # now that its path may be built and checked.
+            # Copy source files to execution directory
+            for f in src_files:
+                src_file = os.path.join(self.src_dir, f)
+                dest_file = os.path.join(exec_src, f)
+                shutil.copy2(src_file, dest_file)
 
-        self.read_parameter_file(self.param)
+        # Copy data files
 
-        # Now override or complete data from the XML file.
+        dir_files = os.listdir(self.data_dir)
 
-        if self.user_locals:
-            m = 'define_domain_parameters'
-            if m in self.user_locals.keys():
-                eval(m + '(self)', globals(), self.user_locals)
-                del self.user_locals[m]
+        if self.package.name in dir_files:
+            dir_files.remove(self.package.name)
 
-        # Finally, ensure some fields are of the required types
+        for f in dir_files:
+            src = os.path.join(self.data_dir, f)
+            if os.path.isfile(src):
+                shutil.copy2(src, os.path.join(self.exec_dir, f))
 
-        if type(self.meshes) != list:
-            self.meshes = [self.meshes,]
+        # Now set parameters
+
+        self.__set_case_parameters__()
 
     #---------------------------------------------------------------------------
 
@@ -629,13 +692,14 @@ class domain(base_domain):
 
         needs_comp = False
 
-        src_files = cs_compile.files_to_compile(self.src_dir)
-
-        if self.exec_solver and len(src_files) > 0:
-            needs_comp = True
+        exec_src = os.path.join(self.exec_dir, 'src')
+        if os.path.isdir(exec_src):
+            src_files = cs_compile.files_to_compile(exec_src)
+            if self.exec_solver and len(src_files) > 0:
+                needs_comp = True
 
         if self.param != None:
-            fp = os.path.join(self.data_dir, self.param)
+            fp = os.path.join(self.exec_dir, self.param)
             case = self.__xml_case_initialize__(fp)
 
             # Do not call case.xmlSaveDocument() to avoid side effects in case
@@ -660,9 +724,14 @@ class domain(base_domain):
         """
         Compile and link user subroutines if necessary
         """
+
+        src_files = []
+
         # Check if there are files to compile in source path
         # or if MEG functions need to be generated
-        src_files = cs_compile.files_to_compile(self.src_dir)
+        exec_src = os.path.join(self.exec_dir, 'src')
+        if os.path.isdir(exec_src):
+            src_files = cs_compile.files_to_compile(exec_src)
 
         if len(src_files) > 0 or self.mci != None:
 
@@ -734,20 +803,6 @@ class domain(base_domain):
         """
 
         err_str = ""
-
-        # Copy data files
-
-        dir_files = os.listdir(self.data_dir)
-
-        if self.package.name in dir_files:
-            dir_files.remove(self.package.name)
-
-        for f in dir_files:
-            src = os.path.join(self.data_dir, f)
-            if os.path.isfile(src):
-                shutil.copy2(src, os.path.join(self.exec_dir, f))
-                if f == 'cs_user_scripts.py':  # Copy user script to results now
-                    shutil.copy2(src,  os.path.join(self.result_dir, f))
 
         if not self.exec_solver:
             return
@@ -1586,6 +1641,9 @@ class syrthes_domain(base_domain):
 # Cathare coupling
 
 class cathare_domain(domain):
+    """
+    Class specific to coupling with CATHARE.
+    """
 
     #---------------------------------------------------------------------------
 
@@ -1678,9 +1736,8 @@ class cathare_domain(domain):
         CATHARE2.
         """
 
-        msg = " ****************************************\n" \
-              "  Generating CATHARE2 .so file\n" \
-              " ****************************************\n\n"
+        msg = "Generating CATHARE2 .so file\n" \
+              "----------------------------\n\n"
         sys.stdout.write(msg)
         sys.stdout.flush()
 
@@ -1752,101 +1809,6 @@ class cathare_domain(domain):
 
         # Then compile NCFD source files if needed
         super(cathare_domain, self).compile_and_link()
-
-    #---------------------------------------------------------------------------
-
-    def prepare_data(self):
-        """
-        Copy data to the execution directory
-        """
-
-        # If we are using a previous preprocessing, simply link to it here
-        if self.mesh_input:
-            mesh_input = os.path.expanduser(self.mesh_input)
-            if not os.path.isabs(mesh_input):
-                mesh_input = os.path.join(self.case_dir, mesh_input)
-
-            # Differentiate between a folder and file, since we now
-            # have a file extension
-            if os.path.isdir(mesh_input):
-                link_path = os.path.join(self.exec_dir, 'mesh_input')
-            else:
-                link_path = os.path.join(self.exec_dir, 'mesh_input.csm')
-
-            self.purge_result(link_path) # in case of previous run here
-            self.symlink(mesh_input, link_path)
-
-        # Copy data files
-
-        dir_files = os.listdir(self.data_dir)
-
-        if self.package.name in dir_files:
-            dir_files.remove(self.package.name)
-
-        for f in dir_files:
-            src = os.path.join(self.data_dir, f)
-            if os.path.isfile(src):
-                shutil.copy2(src, os.path.join(self.exec_dir, f))
-                if f == 'cs_user_scripts.py':  # Copy user script to results now
-                    shutil.copy2(src,  os.path.join(self.result_dir, f))
-
-        if not self.exec_solver:
-            return
-
-        # Fixed parameter name
-
-        setup_ref = "setup.xml"
-        if self.param != None and self.param != "setup.xml":
-            link_path = os.path.join(self.exec_dir, setup_ref)
-            self.purge_result(link_path) # in case of previous run here
-            try:
-                os.symlink(self.param, link_path)
-            except Exception:
-                src_path = os.path.join(self.exec_dir, self.param)
-                shutil.copy2(src_path, link_path)
-
-        # Call user script if necessary
-
-        if self.user_locals:
-            m = 'domain_prepare_data_add'
-            if m in self.user_locals.keys():
-                eval(m + '(self)', globals(), self.user_locals)
-                del self.user_locals[m]
-
-        # Restart files
-
-        if self.restart_input != None:
-
-            restart_input =  os.path.expanduser(self.restart_input)
-            if not os.path.isabs(restart_input):
-                restart_input = os.path.join(self.case_dir, restart_input)
-
-            if not os.path.exists(restart_input):
-                err_str = restart_input + ' does not exist.'
-                raise RunCaseError(err_str)
-            elif not os.path.isdir(restart_input):
-                err_str = restart_input + ' is not a directory.'
-                raise RunCaseError(err_str)
-            else:
-                self.symlink(restart_input,
-                             os.path.join(self.exec_dir, 'restart'))
-
-        # Partition input files
-
-        if self.partition_input != None:
-
-            partition_input = os.path.expanduser(self.partition_input)
-            if not os.path.isabs(partition_input):
-                partition_input = os.path.join(self.case_dir, partition_input)
-
-            if os.path.exists(partition_input):
-
-                if not os.path.isdir(partition_input):
-                    err_str = partition_input + ' is not a directory.'
-                    raise RunCaseError(err_str)
-                else:
-                    self.symlink(partition_input,
-                                 os.path.join(self.exec_dir, 'partition_input'))
 
     #---------------------------------------------------------------------------
 
@@ -1940,7 +1902,7 @@ class python_domain(base_domain):
 
     #---------------------------------------------------------------------------
 
-    def prepare_data(self):
+    def copy_data(self):
         """
         Copy data to run directory
         """
@@ -1968,7 +1930,7 @@ class python_domain(base_domain):
         Copy results dummy function: Does nothing for a standard python script
         """
         # Nothing to do
-        pass
+        return
 
     #---------------------------------------------------------------------------
 

data/user/cs_user_scripts.py
@@ -46,7 +46,7 @@ def domain_prepare_data_add(domain):
 def domain_copy_results_add(domain):
     """
     Additional steps to copy results or cleanup execution directory
-    (called at beginning of data copy stage).
+    (called at beginning of results copy stage).
     """
 
     return
@@ -56,6 +56,8 @@ def domain_copy_results_add(domain):
 def define_domain_parameters(domain):
     """
     Define domain execution parameters.
+    (called in data preparation stage, just after copy of base setup files
+    in execution directory, and before compiling user-defined functions).
     """
 
     # Reusing output from previous runs

commit 00c104c9a5322e0fed3f4ef9641ab08bf503ef47

Scripts: fix "code_saturne submit", broken by commit b870b06c (March 25 2021).

bin/cs_case.py
@@ -1506,12 +1506,15 @@ class case:
             solver = os.path.basename(d.solver_path)
             if os.path.isfile(os.path.join(d.exec_dir, solver)):
                 d.solver_path = os.path.join('.', solver)
+            d.init_staged_data()
 
         for d in self.syr_domains:
             d.solver_path = os.path.join('.', 'syrthes')
+            d.init_staged_data()
 
         for d in self.py_domains:
             d.solver_path = self.package.config.python
+            d.init_staged_data()
 
     #---------------------------------------------------------------------------
 

bin/cs_case_domain.py
@@ -222,7 +222,16 @@ class base_domain:
 
     def copy_data(self):
         """
-        Copy base data to the execution directory
+        Copy base data to the execution directory.
+        """
+
+        return
+
+    #---------------------------------------------------------------------------
+
+    def init_staged_data(self):
+        """
+        Initialize staged data in the execution directory.
         """
 
         return
@@ -632,6 +641,17 @@ class domain(base_domain):
             src = os.path.join(self.data_dir, f)
             if os.path.isfile(src):
                 shutil.copy2(src, os.path.join(self.exec_dir, f))
+
+        # Now set parameters
+
+        self.__set_case_parameters__()
+
+    #---------------------------------------------------------------------------
+
+    def init_staged_data(self):
+        """
+        Initialize staged data in the execution directory.
+        """
 
         # Now set parameters
 