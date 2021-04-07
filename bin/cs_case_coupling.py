code-saturne/code_saturne

commit a3856761cf7a41f863d9805b233fe137d29e6361

Try to improve sourcing of shell scripts, using shebang in simple cases.
Commit b4fea050 is also reverted, as it was not by itself useful.

bin/cs_exec_environment.py
@@ -682,7 +682,23 @@ def source_shell_script(path):
     if sys.platform.startswith('win'):
         return
 
-    user_shell = os.getenv('SHELL')
+    user_shell = None
+
+    # If shebang is present, try to use it.
+    # (TODO: handle more complex cases with env)
+    try:
+        f = open(path, 'r')
+        l = f.readline()
+        f.close()
+        if l[:2] == '#!':
+            e = l[2:].split()[0]
+            if e[-3:] != 'env':
+                user_shell = e
+    except Exception:
+        pass
+
+    if not user_shell:
+        user_shell = os.getenv('SHELL')
     if not user_shell:
         user_shell = '/bin/sh'
 
@@ -818,15 +834,15 @@ def source_syrthes_env(pkg):
     # for consistency with case creation parameters; it this is not enough,
     # use existing environment or load one based on current configuration.
 
-    try:
-        for p in sys.path:
-            if p[-14:] == '/share/syrthes' or p[-14:] == '\share\syrthes':
-                syr_profile = os.path.join(p[:,-14], 'bin', 'syrthes.profile')
-                if os.path.isfile(syr_profile):
-                    print("Sourcing SYRTHES environment: " + syr_profile)
+    for p in sys.path:
+        if p[-14:] == '/share/syrthes' or p[-14:] == '\share\syrthes':
+            syr_profile = os.path.join(p[:,-14], 'bin', 'syrthes.profile')
+            if os.path.isfile(syr_profile):
+                print("Sourcing SYRTHES environment: " + syr_profile)
+                try:
                     source_shell_script(syr_profile)
-    except Exception:
-        pass
+                except Exception:
+                    print("  Failed sourcing SYRTHES environment: ")
 
     env_syrthes_home = os.getenv('SYRTHES4_HOME')
 
@@ -846,6 +862,7 @@ def source_syrthes_env(pkg):
 
     # Now source environment if not done already or different
 
+    print(syrthes_home, env_syrthes_home)
     if syrthes_home != env_syrthes_home:
         syr_profile = os.path.join(config.get('install', 'syrthes'),
                                    'bin', 'syrthes.profile')
@@ -862,6 +879,7 @@ def source_syrthes_env(pkg):
         if sys.path.count(p) > 0:
             sys.path.remove(p)
         sys.path.insert(0, p)
+
 
 #-------------------------------------------------------------------------------
 

bin/cs_case_domain.py
@@ -1416,19 +1416,7 @@ class syrthes_domain(base_domain):
 
         compile_logname = os.path.join(self.exec_dir, 'compile.log')
 
-        # Try to work around syrthes detection issue in some environments.
-        path_save = os.getenv('PATH')
-        if path_save:
-            for p in sys.path:
-                f = os.path.join(p, 'syrthes4_create_case')
-                if os.path.isfile(f):
-                    os.environ['PATH'] = p + ':' + path_save
-                    break
-
         retval = self.syrthes_case.prepare_run(exec_srcdir, compile_logname)
-
-        if path_save:
-            os.environ['PATH'] = path_save
 
         self.copy_result(compile_logname)
 

commit 0fc89a84a370bc1924ccf023d29b382c849d324e

Fix launch of syrthes coupling including multiple instances
  * Syrthes launch broken by commit a3856761 (2020-10-24).
  * Multiple syrthes instances lead to a deadlock in python
    (cs_case_domain). Adding a "break" statement to exit the corresponding
    loop

bin/cs_case_coupling.py
@@ -181,8 +181,7 @@ def coupling(package,
 
             except Exception:
                 err_str = 'Cannot create SYRTHES domain. Opt = ' + d.get('opt') + '\n'
-                err_str += ' domain = ' + domain_s
-                err_str += ' script = ' + d.get('script') + '\n'
+                err_str += ' domain = ' + domain_s + '\n'
                 err_str += ' n_procs_weight = ' + str(d.get('n_procs_weight')) + '\n'
                 raise RunCaseError(err_str)
 

bin/cs_exec_environment.py
@@ -887,7 +887,7 @@ def source_syrthes_env(pkg, verbose=True):
 
     for p in sys.path:
         if p[-14:] == '/share/syrthes' or p[-14:] == '\share\syrthes':
-            syr_profile = os.path.join(p[:,-14], 'bin', 'syrthes.profile')
+            syr_profile = os.path.join(p[:-14], 'bin', 'syrthes.profile')
             if os.path.isfile(syr_profile):
                 if verbose:
                     sys.stdout.write("Sourcing SYRTHES environment: " \
@@ -896,6 +896,8 @@ def source_syrthes_env(pkg, verbose=True):
                     source_shell_script(syr_profile)
                 except Exception:
                     sys.stderr.write("  Failed sourcing SYRTHES environment.\n")
+                # Ensure exit from the loop once syrthes is found
+                break
 
     env_syrthes_home = os.getenv('SYRTHES4_HOME')
     if not env_syrthes_home: