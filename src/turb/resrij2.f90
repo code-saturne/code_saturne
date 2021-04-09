code-saturne/code_saturne

commit d061a7b5b7d35543b961791a02f0bc23cb71adac

Call turbulence user source terms earlier

src/turb/resrij2.f90
@@ -264,16 +264,6 @@ endif
 ! 2. User source terms
 !===============================================================================
 
-call cs_user_turbulence_source_terms2 &
- ( nvar   , nscal  , ncepdp , ncesmp ,                            &
-   ivarfl(irij)    ,                                              &
-   icepdc , icetsm , itypsm ,                                     &
-   ckupdc , smacel ,                                              &
-   smbr   , rovsdt )
-
-! C version
-call user_source_terms(ivarfl(irij), smbr, rovsdt)
-
   !     If we extrapolate the source terms
 if (st_prv_id.ge.0) then
   do iel = 1, ncel

src/turb/resssg2.f90
@@ -317,16 +317,6 @@ endif
 ! 2. User source terms
 !===============================================================================
 
-call cs_user_turbulence_source_terms2 &
- ( nvar   , nscal  , ncepdp , ncesmp ,                            &
-   ivarfl(ivar)    ,                                              &
-   icepdc , icetsm , itypsm ,                                     &
-   ckupdc , smacel ,                                              &
-   smbr   , rovsdt )
-
-! C version
-call user_source_terms(ivarfl(ivar), smbr, rovsdt)
-
 do isou = 1, dimrij
   ! If we extrapolate the source terms
   if (st_prv_id.ge.0) then

src/turb/turrij.f90
@@ -360,6 +360,20 @@ iprev = 1
 call field_gradient_vector(ivarfl(iu), iprev, 0, inc, gradv)
 
 !===============================================================================
+! 1.2 Call source terms for Rij
+!===============================================================================
+
+call cs_user_turbulence_source_terms2 &
+ ( nvar   , nscal  , ncepdp , ncesmp ,                            &
+   ivarfl(ivar)    ,                                              &
+   icepdc , icetsm , itypsm ,                                     &
+   ckupdc , smacel ,                                              &
+   smbrts , rovsdtts, gradv)
+
+! C version
+call user_source_terms(ivarfl(ivar), smbrts, rovsdtts)
+
+!===============================================================================
 ! 2.2 Compute the production term for Rij
 !===============================================================================
 

src/turb/resrij.f90
@@ -259,8 +259,6 @@ endif
 ! 2. User source terms
 !===============================================================================
 
-call user_source_terms(ivarfl(ivar), smbr, rovsdt)
-
 !     If we extrapolate the source terms
 if (st_prv_id.ge.0) then
   do iel = 1, ncel

src/turb/resssg.f90
@@ -300,8 +300,6 @@ endif
 ! 2. User source terms
 !===============================================================================
 
-call user_source_terms(ivarfl(ivar), smbr, rovsdt)
-
 !     If we extrapolate the source terms
 if (st_prv_id.ge.0) then
   do iel = 1, ncel

src/user/cs_user_source_terms.f90
@@ -620,7 +620,7 @@ subroutine cs_user_turbulence_source_terms2 &
    f_id   ,                                                       &
    icepdc , icetsm , itypsm ,                                     &
    ckupdc , smacel ,                                              &
-   crvexp , crvimp )
+   crvexp , crvimp, gradv)
 
 !===============================================================================
 
@@ -656,6 +656,7 @@ integer          icetsm(ncesmp), itypsm(ncesmp,nvar)
 double precision dt(ncelet)
 double precision ckupdc(6,ncepdp), smacel(ncesmp,nvar)
 double precision crvexp(6,ncelet), crvimp(6,6,ncelet)
+double precision gradv(3, 3, ncelet)
 
 ! Local variables
 

commit 5a801927f58e39b2fcc2aa36b5d130cdbbfc4f42

[Turbulence] Fix bug introduced in commit d061a7b5b (2020-09-17): Rij user source terms was not taken into account.

src/turb/resrij2.f90
@@ -255,19 +255,6 @@ else
   call field_get_val_s(icrom, cromo)
 endif
 
-do iel = 1, ncel
-  do isou = 1 ,6
-    smbr(isou,iel) = 0.d0
-  enddo
-enddo
-do iel = 1, ncel
-  do isou = 1, 6
-    do jsou = 1, 6
-      rovsdt(isou,jsou,iel) = 0.d0
-    enddo
-  enddo
-enddo
-
 ! Coefficient of the "Coriolis-type" term
 if (icorio.eq.1) then
   ! Relative velocity formulation
@@ -289,27 +276,31 @@ t2v(3,1) = 6; t2v(3,2) = 5; t2v(3,3) = 3;
 ! 2. User source terms
 !===============================================================================
 
-  !     If we extrapolate the source terms
+! If we extrapolate the source terms
 if (st_prv_id.ge.0) then
   do iel = 1, ncel
     do isou = 1, dimrij
-      !       Save for exchange
+      ! Save for exchange
       tuexpr = c_st_prv(isou,iel)
-      !       For continuation and the next time step
+      ! For continuation and the next time step
       c_st_prv(isou,iel) = smbr(isou,iel)
-      !       Second member of the previous time step
-      !       We suppose -rovsdt > 0: we implicite
-      !          the user source term (the rest)
-      smbr(isou,iel) = rovsdt(isou,isou,iel)*cvara_var(isou,iel)  - thets*tuexpr
-      !       Diagonal
-      rovsdt(isou,isou,iel) = - thetv*rovsdt(isou,isou,iel)
+      ! Second member of the previous time step
+      ! We suppose -rovsdt > 0: we implicite
+      !    the user source term (the rest)
+      do jsou = 1, dimrij
+        smbr(isou,iel) = rovsdt(jsou,isou,iel)*cvara_var(jsou,iel)  - thets*tuexpr
+        ! Diagonal
+        rovsdt(jsou,isou,iel) = - thetv*rovsdt(jsou,isou,iel)
+      enddo
     enddo
   enddo
 else
   do iel = 1, ncel
     do isou = 1, dimrij
-      smbr(isou,iel)   = rovsdt(isou,isou,iel)*cvara_var(isou,iel) + smbr(isou,iel)
-      rovsdt(isou,isou,iel) = max(-rovsdt(isou,isou,iel),zero)
+      do jsou = 1, dimrij
+        smbr(isou,iel)   = rovsdt(jsou,isou,iel)*cvara_var(jsou,iel) + smbr(isou,iel)
+      enddo
+      rovsdt(isou,isou,iel) = max(-rovsdt(isou,isou,iel), 0.d0)
     enddo
   enddo
 endif
@@ -323,7 +314,7 @@ if (iilagr.eq.2 .and. ltsdyn.eq.1) then
   call field_get_val_v_by_name('rij_st_lagr', lagr_st_rij)
   do iel = 1,ncel
     do isou = 1, dimrij
-      smbr(isou, iel)   = smbr(isou, iel) + lagr_st_rij(isou,iel)
+      smbr(isou, iel) = smbr(isou, iel) + lagr_st_rij(isou,iel)
       rovsdt(isou,isou,iel) = rovsdt(isou,isou, iel) + max(-tslagi(iel),zero)
     enddo
   enddo

src/turb/resssg2.f90
@@ -283,19 +283,6 @@ else
   call field_get_val_s(icrom, cromo)
 endif
 
-do isou = 1, 6
-  do iel = 1, ncel
-    smbr(isou,iel) = 0.d0
-  enddo
-enddo
-do iel = 1, ncel
-  do isou = 1, 6
-    do jsou = 1, 6
-      rovsdt(isou,jsou,iel) = 0.d0
-    enddo
-  enddo
-enddo
-
 if (icorio.eq.1 .or. iturbo.eq.1) then
 allocate(cvara_r(3,3))
 
@@ -314,10 +301,10 @@ endif
 ! 2. User source terms
 !===============================================================================
 
-do isou = 1, dimrij
-  ! If we extrapolate the source terms
-  if (st_prv_id.ge.0) then
-    do iel = 1, ncel
+! If we extrapolate the source terms
+if (st_prv_id.ge.0) then
+  do iel = 1, ncel
+    do isou = 1, dimrij
       ! Save for exchange
       tuexpr = c_st_prv(isou,iel)
       ! For continuation and the next time step
@@ -325,17 +312,23 @@ do isou = 1, dimrij
       ! Second member of the previous time step
       ! We suppose -rovsdt > 0: we implicite
       !    the user source term (the rest)
-      smbr(isou,iel) = rovsdt(isou,isou,iel)*cvara_var(isou,iel)  - thets*tuexpr
-      ! Diagonal
-      rovsdt(isou,isou,iel) = - thetv*rovsdt(isou,isou,iel)
+      do jsou = 1, dimrij
+        smbr(isou,iel) = rovsdt(jsou,isou,iel)*cvara_var(jsou,iel)  - thets*tuexpr
+        ! Diagonal
+        rovsdt(jsou,isou,iel) = - thetv*rovsdt(jsou,isou,iel)
+      enddo
     enddo
-  else
-    do iel = 1, ncel
-      smbr(isou,iel)   = rovsdt(isou,isou,iel)*cvara_var(isou,iel) + smbr(isou,iel)
-      rovsdt(isou,isou,iel) = max(-rovsdt(isou,isou,iel),zero)
+  enddo
+else
+  do iel = 1, ncel
+    do isou = 1, dimrij
+      do jsou = 1, dimrij
+        smbr(isou,iel)   = rovsdt(jsou,isou,iel)*cvara_var(jsou,iel) + smbr(isou,iel)
+      enddo
+      rovsdt(isou,isou,iel) = max(-rovsdt(isou,isou,iel), 0.d0)
     enddo
-  endif
-enddo
+  enddo
+endif
 
 !===============================================================================
 ! 3. Lagrangian source terms

src/turb/turrij.f90
@@ -218,6 +218,18 @@ call field_get_key_id("time_extrapolated", key_t_ext_id)
 !===============================================================================
 
 if (irijco.eq.1) then
+  do iel = 1, ncel
+    do isou = 1 ,6
+      smbrts(isou,iel) = 0.d0
+    enddo
+  enddo
+  do iel = 1, ncel
+    do isou = 1, 6
+      do jsou = 1, 6
+        rovsdtts(isou,jsou,iel) = 0.d0
+      enddo
+    enddo
+  enddo
 
   call cs_user_turbulence_source_terms2 &
    ( nvar   , nscal  , ncepdp , ncesmp ,                            &