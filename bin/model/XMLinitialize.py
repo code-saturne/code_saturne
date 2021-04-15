code-saturne/code_saturne

commit 728c02d6e45da969bbe594d2e7ebe95b29b8db86

Coal and fuel combustion: align temperature field with standard models.
Cherry picked from commit 5bd07655 on master.

bin/model/XMLinitialize.py
@@ -1848,7 +1848,7 @@ class XMLinit(BaseXmlInit):
         # Fix some Combustion names
 
         p_node = self.case.xmlGetNode('thermophysical_models')
-        if node:
+        if p_node:
             p_node = node.xmlGetNode('solid_fuels')
 
         if p_node:
@@ -1922,7 +1922,6 @@ class XMLinit(BaseXmlInit):
                 if node:
                     node.xmlRemoveNode()
 
-
         # Check that all zones have the 'physical_properties' flag
         XMLVolumicNode = self.case.xmlGetNode('volumic_conditions')
         for node in XMLVolumicNode.xmlGetChildNodeList('zone', 'label', 'id'):
@@ -1933,6 +1932,23 @@ class XMLinit(BaseXmlInit):
                     node['physical_properties'] = 'on'
                 else:
                     node['physical_properties'] = 'off'
+
+        # Rename some coal combustion fields
+        p_node = self.case.xmlGetNode('thermophysical_models')
+        if p_node:
+            p_node = p_node.xmlGetNode('solid_fuels')
+            if p_node:
+                rename = {"t_gas": "temperature"}
+                for attr in rename.keys():
+                    node = p_node.xmlGetNode('property', name=attr)
+                    if node:
+                        for n in self.case.xmlGetNodeList('var_prop'):
+                            name = n["name"]
+                            if name:
+                                for key in rename.keys():
+                                    if name == key:
+                                        n["name"] = rename[key]
+                        node['name'] = rename[attr]
 
 #-------------------------------------------------------------------------------
 # End of XMLinit

bin/model/CoalCombustionModel.py
@@ -300,7 +300,7 @@ class CoalCombustionModel(Variables, Model):
         """
         classesNumber = self.getClassesNumber()
 
-        lst = ["t_gas", "rho_gas", "ym_chx1m", "ym_chx2m",
+        lst = ["temperature", "rho_gas", "ym_chx1m", "ym_chx2m",
                "ym_co", "ym_o2", "ym_co2", "ym_h2o", "ym_n2",
                "ym_h2s", "ym_h2", "ym_hcn", "ym_nh3", "ym_so2",
                "xm"]
@@ -2028,7 +2028,7 @@ class CoalCombustionModelTestCase(ModelTest):
                     <variable label="Fr_HET" name="het_fraction" type="model">
                             <flux_reconstruction status="off"/>
                     </variable>
-                    <property label="Temp_GAZ" name="t_gas"/>
+                    <property label="Temp_GAZ" name="temperature"/>
                     <property label="ROM_GAZ" name="rho_gas"/>
                     <property label="YM_CHx1m" name="ym_chx1m"/>
                     <property label="YM_CHx2m" name="ym_chx2m"/>
@@ -2073,7 +2073,7 @@ class CoalCombustionModelTestCase(ModelTest):
                     <variable label="Fr_MV101" name="fr_mv1_01" type="model"><flux_reconstruction status="off"/></variable>
                     <variable label="Fr_MV201" name="fr_mv2_01" type="model"><flux_reconstruction status="off"/></variable>
                     <variable label="Fr_HET" name="het_fraction" type="model"><flux_reconstruction status="off"/></variable>
-                    <property label="Temp_GAZ" name="t_gas"/>
+                    <property label="Temp_GAZ" name="temperature"/>
                     <property label="ROM_GAZ" name="rho_gas"/>
                     <property label="YM_CHx1m" name="ym_chx1m"/>
                     <property label="YM_CHx2m" name="ym_chx2m"/>
@@ -2142,7 +2142,7 @@ class CoalCombustionModelTestCase(ModelTest):
                     <variable label="Fr_MV101" name="fr_mv1_01" type="model"><flux_reconstruction status="off"/></variable>
                     <variable label="Fr_MV201" name="fr_mv2_01" type="model"><flux_reconstruction status="off"/></variable>
                     <variable label="Fr_HET" name="het_fraction" type="model"><flux_reconstruction status="off"/></variable>
-                    <property label="Temp_GAZ" name="t_gas"/>
+                    <property label="Temp_GAZ" name="temperature"/>
                     <property label="ROM_GAZ" name="rho_gas"/>
                     <property label="YM_CHx1m" name="ym_chx1m"/>
                     <property label="YM_CHx2m" name="ym_chx2m"/>
@@ -2250,7 +2250,7 @@ class CoalCombustionModelTestCase(ModelTest):
                     <variable label="Fr_MV101" name="fr_mv1_01" type="model"><flux_reconstruction status="off"/></variable>
                     <variable label="Fr_MV201" name="fr_mv2_01" type="model"><flux_reconstruction status="off"/></variable>
                     <variable label="Fr_HET" name="het_fraction" type="model"><flux_reconstruction status="off"/></variable>
-                    <property label="Temp_GAZ" name="t_gas"/>
+                    <property label="Temp_GAZ" name="temperature"/>
                     <property label="ROM_GAZ" name="rho_gas"/>
                     <property label="YM_CHx1m" name="ym_chx1m"/>
                     <property label="YM_CHx2m" name="ym_chx2m"/>
@@ -2340,7 +2340,7 @@ class CoalCombustionModelTestCase(ModelTest):
                     <variable label="Fr_MV101" name="fr_mv1_01" type="model"><flux_reconstruction status="off"/></variable>
                     <variable label="Fr_MV201" name="fr_mv2_01" type="model"><flux_reconstruction status="off"/></variable>
                     <variable label="Fr_HET" name="het_fraction" type="model"><flux_reconstruction status="off"/></variable>
-                    <property label="Temp_GAZ" name="t_gas"/>
+                    <property label="Temp_GAZ" name="temperature"/>
                     <property label="ROM_GAZ" name="rho_gas"/>
                     <property label="YM_CHx1m" name="ym_chx1m"/>
                     <property label="YM_CHx2m" name="ym_chx2m"/>

bin/model/OutputFieldsModel.py
@@ -378,18 +378,6 @@ class OutputFieldsTestCase(ModelTest):
             'Could not get VariableLabelsList'
 
 
-#    def checkGetVariableProbeList(self):
-#        """Check whether the  NonCondensableModel class could get the VariableLabelsList"""
-#        MainFieldsModel(self.case).addField()
-#        mdl = OutputFieldsModel(self.case)
-#        mdl.setProbesList('1','U1','1')
-#        print(mdl.getProbesList('1','VelocityX'))
-#        print(mdl.getVariableProbeList())
-#        print(mdl.case)
-#        assert mdl.getVariableLabelsList() == ,\
-#            'Could not get VariableLabelsList'
-
-
     def checkGetandSetVariableLabel(self):
         """Check whether the OutputFieldsModel class could set and get VariableLabel"""
         MainFieldsModel(self.case).addField()

bin/model/ThermodynamicsModel.py
@@ -1336,7 +1336,7 @@ class ThermodynamicsTestCase(ModelTest):
         mdl.setInitialValue('2','emissivity',15446.2)
         mdl.setInitialValue('2','elasticity',22.2)
         doc = '''<properties>
-                         <property choice="" field_id="1" label="temp1" name="temperature">
+                         <property choice="" field_id="1" label="Temperature" name="temperature">
                                  <listing_printing status="on"/>
                                  <postprocessing_recording status="on"/>
                          </property>
@@ -1446,7 +1446,7 @@ class ThermodynamicsTestCase(ModelTest):
         mdl = ThermodynamicsModel(self.case)
         mdl.setInitialValueDensity('1',8.8)
         doc = '''<properties>
-                         <property choice="" field_id="1" label="temp1" name="temperature">
+                         <property choice="" field_id="1" label="Temperature" name="temperature">
                                  <listing_printing status="on"/>
                                  <postprocessing_recording status="on"/>
                          </property>
@@ -1486,7 +1486,7 @@ class ThermodynamicsTestCase(ModelTest):
         mdl = ThermodynamicsModel(self.case)
         mdl.setInitialValueViscosity('1',7.7)
         doc = '''<properties>
-                         <property choice="" field_id="1" label="temp1" name="temperature">
+                         <property choice="" field_id="1" label="Temperature" name="temperature">
                                  <listing_printing status="on"/>
                                  <postprocessing_recording status="on"/>
                          </property>
@@ -1526,7 +1526,7 @@ class ThermodynamicsTestCase(ModelTest):
         mdl = ThermodynamicsModel(self.case)
         mdl.setInitialValueHeat('1',118.712)
         doc = '''<properties>
-                         <property choice="" field_id="1" label="temp1" name="temperature">
+                         <property choice="" field_id="1" label="Temperature" name="temperature">
                                  <listing_printing status="on"/>
                                  <postprocessing_recording status="on"/>
                          </property>
@@ -1566,7 +1566,7 @@ class ThermodynamicsTestCase(ModelTest):
         mdl = ThermodynamicsModel(self.case)
         mdl.setInitialValueCond('1',456.1)
         doc = '''<properties>
-                         <property choice="" field_id="1" label="temp1" name="temperature">
+                         <property choice="" field_id="1" label="Temperature" name="temperature">
                                  <listing_printing status="on"/>
                                  <postprocessing_recording status="on"/>
                          </property>
@@ -1606,7 +1606,7 @@ class ThermodynamicsTestCase(ModelTest):
         mdl = ThermodynamicsModel(self.case)
         mdl.setInitialValueTens('none',0.075)
         doc = '''<properties>
-                         <property choice="" field_id="1" label="temp1" name="temperature">
+                         <property choice="" field_id="1" label="Temperature" name="temperature">
                                  <listing_printing status="on"/>
                                  <postprocessing_recording status="on"/>
                          </property>
@@ -1651,7 +1651,7 @@ class ThermodynamicsTestCase(ModelTest):
         mdl = ThermodynamicsModel(self.case)
         mdl.setInitialValueEmissivity('2',0.008)
         doc = '''<properties>
-                         <property choice="" field_id="1" label="temp1" name="temperature">
+                         <property choice="" field_id="1" label="Temperature" name="temperature">
                                  <listing_printing status="on"/>
                                  <postprocessing_recording status="on"/>
                          </property>
@@ -1728,7 +1728,7 @@ class ThermodynamicsTestCase(ModelTest):
         mdl = ThermodynamicsModel(self.case)
         mdl.setInitialValueElastCoef('2',0.42)
         doc = '''<properties>
-                         <property choice="" field_id="1" label="temp1" name="temperature">
+                         <property choice="" field_id="1" label="Temperature" name="temperature">
                                  <listing_printing status="on"/>
                                  <postprocessing_recording status="on"/>
                          </property>
@@ -1804,7 +1804,7 @@ class ThermodynamicsTestCase(ModelTest):
         mdl = ThermodynamicsModel(self.case)
         mdl.setFormula('1','density','2.123')
         doc = '''<properties>
-                         <property choice="" field_id="1" label="temp1" name="temperature">
+                         <property choice="" field_id="1" label="Temperature" name="temperature">
                                  <listing_printing status="on"/>
                                  <postprocessing_recording status="on"/>
                          </property>
@@ -1865,7 +1865,7 @@ class ThermodynamicsTestCase(ModelTest):
         mdl = ThermodynamicsModel(self.case)
         mdl.setPropertyMode('1','density','user_law')
         doc = '''<properties>
-                         <property choice="" field_id="1" label="temp1" name="temperature">
+                         <property choice="" field_id="1" label="Temperature" name="temperature">
                                  <listing_printing status="on"/>
                                  <postprocessing_recording status="on"/>
                          </property>

docs/user/saturne1.tex
@@ -1522,7 +1522,7 @@ They allow to:
                      \end{list}
                \item Properties \texttt{cpro\_prop(iel)}
                      \begin{list}{\texttt{iprop} = }{}
-                      \item \texttt{itemp1\index{itemp1}}: temperature of the
+                      \item \texttt{itemp\index{itemp1}}: temperature of the
                             gas mixture
                       \item \texttt{iym1(1)\index{iym1(1)}}: mass fraction of
                             $CH_{X1m}$ (light volatiles) in the gas

src/base/addfld.f90
@@ -683,7 +683,6 @@ call field_get_id_try('boundary_temperature', itempb)
 
 if (itempb .ge. 0) then
   call field_get_id_try('temperature', f_id)
-  if (f_id .lt. 0) call field_get_id_try('t_gas', f_id)
   if (f_id .ge. 0) then
     call field_get_label(f_id, f_label)
     call field_set_key_str(itempb, keylbl, f_label)

src/base/cs_ht_convert.c
@@ -149,7 +149,7 @@ cs_ht_convert_h_to_t_cells(const cs_real_t  h[],
   /* Pulverized coal or fuel combustion */
   else if (   pm_flag[CS_COMBUSTION_COAL] >= 0
            || pm_flag[CS_COMBUSTION_FUEL] >= 0)
-    cpro_t = cs_field_by_name("t_gas")->val;
+    cpro_t = CS_F_(t)->val;
 
   /* Electric arcs */
   else if (   pm_flag[CS_JOULE_EFFECT] >= 1

src/base/phyvar.f90
@@ -1064,7 +1064,6 @@ call field_get_id_try('boundary_temperature', f_id)
 if (f_id .ge. 0) then
   call field_get_val_s(f_id, field_s_b)
   call field_get_id_try('temperature', f_id)
-  if (f_id .lt. 0) call field_get_id_try('t_gas', f_id)
   if (f_id .ge. 0) then
     call field_get_val_s(f_id, field_s_v)
     do ifac = 1, nfabor

commit 84eed0d2f99ad1a227c5d84d7965a27bbd26fdc4

Fix backCompatibilty function broken by commit 728c02d6

bin/model/XMLinitialize.py
@@ -1849,7 +1849,7 @@ class XMLinit(BaseXmlInit):
 
         p_node = self.case.xmlGetNode('thermophysical_models')
         if p_node:
-            p_node = node.xmlGetNode('solid_fuels')
+            p_node = p_node.xmlGetNode('solid_fuels')
 
         if p_node:
             rename = {"x_carbone": "x_carbon"}