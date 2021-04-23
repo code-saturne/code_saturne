code-saturne/code_saturne

commit 06abd0e8d5c077bdfac55f6e8921e63471bab997

Create time moments after other field creation.
It may be useful for time moments on mass fluxes.
Provided by R. Guine.

src/base/initi1.f90
@@ -135,11 +135,6 @@ if (icdo.lt.2) then
   call addfld
 endif
 
-! Time moments
-
-call cs_gui_time_moments
-call cs_user_time_moments
-
 ! Postprocessing and logging
 
 call cs_gui_output
@@ -162,6 +157,14 @@ if (icdo.lt.2) then
   call modini
   call fldini
 endif
+
+!===============================================================================
+! Time moments called after additionnal creation
+!===============================================================================
+! Time moments
+
+call cs_gui_time_moments
+call cs_user_time_moments
 
 !===============================================================================
 ! GUI-based boundary condition definitions

commit 2973da915618a958e792d6b64382c812dde77ddb

Fix GUI settings of time moments, broken by commit 06abd0e8 (yesterday).

src/base/initi1.f90
@@ -135,10 +135,6 @@ if (icdo.lt.2) then
   call addfld
 endif
 
-! Postprocessing and logging
-
-call cs_gui_output
-
 ! Restart
 
 ttsuit = -1.d0
@@ -175,6 +171,10 @@ call cs_gui_boundary_conditions_define(c_null_ptr)
 !===============================================================================
 ! Some final settings
 !===============================================================================
+
+! Postprocessing and logging
+
+call cs_gui_output
 
 ! Do not call this routine if CDO mode only (default variables and properties
 ! are not defined anymore)

src/gui/cs_gui_output.c
@@ -728,7 +728,6 @@ cs_gui_output(void)
 
 #if _XML_DEBUG_
   bft_printf("==>CSENSO\n");
-  bft_printf("--iecaux = %i\n", *iecaux);
   bft_printf("--ntlist = %i\n", cs_glob_log_frequency);
 #endif
 }