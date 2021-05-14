code-saturne/code_saturne

commit 5e9af0ccfe5a3af00d0b6424b9d67bdfd73fa67a

Fix check on the nature of volumic zones

bin/model/PorosityModel.py
@@ -90,7 +90,7 @@ class PorosityModel(Variables, Model):
         zoneDico = {}
         zonesList = LocalizationModel('VolumicZone', self.case).getZones()
         for zone in zonesList:
-            if zone.getNature()['porosity'] == 'on':
+            if zone.isNatureActivated("porosity"):
                 label = zone.getLabel()
                 zoneid = zone.getCodeNumber()
                 localization = zone.getLocalization()
@@ -106,7 +106,8 @@ class PorosityModel(Variables, Model):
         Set name and label zone for porosity markups.
         """
         self.node_porosit.xmlInitChildNode('porosity', zone_id=zoneid)
-        self.getPorosityModel(zoneid)
+        mdl = self.__defaultValues()['choice']
+        self.setPorosityModel(zoneid, mdl)
 
 
     @Variables.noUndo
@@ -117,12 +118,11 @@ class PorosityModel(Variables, Model):
         self.isInt(int(zoneid))
         node = self.node_porosit.xmlGetNode('porosity', zone_id=zoneid)
 
-        mdl = node['model']
-        if mdl == None:
-            mdl = self.__defaultValues()['choice']
-            self.setPorosityModel(zoneid, mdl)
-        return mdl
-
+        try:
+            return node["model"]
+        except TypeError as e:
+            message = "Porosity not activated for zone : {0}".format(zoneid)
+            raise Exception(message).with_traceback(e.__traceback__)
 
     @Variables.undoLocal
     def setPorosityModel(self, zoneid, choice):

bin/model/GroundwaterLawModel.py
@@ -112,7 +112,7 @@ class GroundwaterLawModel(Variables, Model):
         zoneDico = {}
         zonesList = LocalizationModel('VolumicZone', self.case).getZones()
         for zone in zonesList:
-            if zone.getNature()['groundwater_law'] == 'on':
+            if zone.isNatureActivated("groundwater_law"):
                 label = zone.getLabel()
                 zoneid = zone.getCodeNumber()
                 localization = zone.getLocalization()

bin/model/LocalizationModel.py
@@ -74,7 +74,6 @@ class Zone(object):
         else:
             raise ValueError("Unknown type zone")
 
-
     def __init__(self, typeZone, case = None, label = None, codeNumber = None,
                  localization = None, nature = None):
         """
@@ -103,56 +102,50 @@ class Zone(object):
         else:
             self._nature = self.defaultValues()['nature']
 
-
     def _initNatureList(self, case):
         self._natureList = []
         self._natureDict = {}
         self.case = case
 
-
     def setLabel(self, text):
         if Model().isStr(text):
             self._label = text
 
-
     def getLabel(self):
         return self._label
-
 
     def setLocalization(self, text):
         if Model().isStr(text):
             self._localization = text
 
-
     def getLocalization(self):
         return self._localization
-
 
     def setCodeNumber(self, number):
         if Model().isPositiveInt(number):
             self._codeNumber = number
 
-
     def getCodeNumber(self):
         return self._codeNumber
-
 
     def setNature(self, text):
         if Model().isInList(text, self._natureList):
             self._nature = text
 
-
     def getNature(self):
         return self._nature
-
 
     def getNatureList(self):
         return self._natureList
 
+    def isNatureActivated(self, text):
+        if text in self._nature.keys():
+            return {"on": True, "off": False}[self._nature[text]]
+        else:
+            return False
 
     def getModel2ViewDictionary(self):
         return self._natureDict
-
 
     def defaultValues(self):
         dico = {}
@@ -299,16 +292,24 @@ class VolumicZone(Zone):
         dico = Zone.defaultValues(self)
         dico['label'] = 'Zone_'
         dico['nature'] = {}
-        dico['nature']['initialization']       = "off"
-        dico['nature']['head_losses']          = "off"
-        dico['nature']['porosity']             = "off"
+        dico['nature']['initialization'] = "off"
+        dico['nature']['head_losses'] = "off"
+        dico['nature']['porosity'] = "off"
         dico['nature']['momentum_source_term'] = "off"
-        dico['nature']['mass_source_term']     = "off"
-        dico['nature']['thermal_source_term']  = "off"
-        dico['nature']['scalar_source_term']   = "off"
-        dico['nature']['groundwater_law']      = "off"
+        dico['nature']['mass_source_term'] = "off"
+        dico['nature']['thermal_source_term'] = "off"
+        dico['nature']['scalar_source_term'] = "off"
+        dico['nature']['groundwater_law'] = "off"
         return dico
 
+    def isNatureActivated(self, text):
+        if text == "source_term":
+            status = False
+            for source_term_type in ["momentum", "mass", "thermal", "scalar"]:
+                status = status or super().isNatureActivated(source_term_type + "_source_term")
+            return status
+        else:
+            return super().isNatureActivated(text)
 
     def tr(self, text):
         """

commit 046e63e2f826fd48d6d386417b58afafe97fb2ce

[GUI] Fix porosity page model
  * porosity model was always set to "isotropic"
  * bug introduced in commit 5e9af0ccfe (2020-09-28)

bin/model/PorosityModel.py
@@ -106,7 +106,13 @@ class PorosityModel(Variables, Model):
         Set name and label zone for porosity markups.
         """
         self.node_porosit.xmlInitChildNode('porosity', zone_id=zoneid)
-        mdl = self.__defaultValues()['choice']
+
+        # Use existing model or default if none is defined
+        try:
+            n = self.node_porosit.xmlGetNode('porosity', zone_id=zoneid)
+            mdl = n['model']
+        except:
+            mdl = self.__defaultValues()['choice']
         self.setPorosityModel(zoneid, mdl)
 
 