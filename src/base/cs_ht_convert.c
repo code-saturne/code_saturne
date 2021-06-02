code-saturne/code_saturne

commit 8b7b1185dc4b93fa0d97f2789adc25fea138765c

Major commit: migrate enthalpy/temperature conversion to C, with user control.
Some physical models are still based on Fortran code, but the base API
is now in C, and user-defined functions are zone-based.

Since some boundary temperature to enthalpy computations are still called
with an arbitrary (not zone-based) face ids list, extra computation, and
an extra copy for filtering are required, but the API is prepared for migration
to zone-based boundary conditions.

src/base/cs_ht_convert.c
@@ -1,0 +1,445 @@
+/*============================================================================
+ * Enthaly to and from temperature conversion.
+ *============================================================================*/
+
+/*
+  This file is part of Code_Saturne, a general-purpose CFD tool.
+
+  Copyright (C) 1998-2021 EDF S.A.
+
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation; either version 2 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+  Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/*----------------------------------------------------------------------------*/
+
+#include "cs_defs.h"
+
+/*----------------------------------------------------------------------------
+ * Standard C library headers
+ *----------------------------------------------------------------------------*/
+
+#include <assert.h>
+#include <math.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/*----------------------------------------------------------------------------
+ * Local headers
+ *----------------------------------------------------------------------------*/
+
+#include "bft_mem.h"
+#include "bft_error.h"
+
+#include "cs_elec_model.h"
+#include "cs_field.h"
+#include "cs_field_pointer.h"
+#include "cs_log.h"
+#include "cs_mesh_location.h"
+#include "cs_physical_constants.h"
+#include "cs_physical_model.h"
+#include "cs_prototypes.h"
+#include "cs_volume_zone.h"
+
+/*----------------------------------------------------------------------------
+ * Header for the current file
+ *----------------------------------------------------------------------------*/
+
+#include "cs_ht_convert.h"
+
+/*----------------------------------------------------------------------------*/
+
+BEGIN_C_DECLS
+
+/*=============================================================================
+ * Additional doxygen documentation
+ *============================================================================*/
+
+/*!
+  \file cs_ht_convert.c
+        Enthalpy to and from temperature conversion.
+
+        Other fields may be involved in the conversion.
+
+        TODO: when possible (based on calling functions's conversion to C)
+              a function pointer-based logic would allow migrating this
+              functionnality to cs_physical_properties, without adding
+              high level physical model dependencies (i.e. cs_physical_model.h)
+              to that lower-level API.
+*/
+
+/*! \cond DOXYGEN_SHOULD_SKIP_THIS */
+
+/*============================================================================
+ * Prototypes for Fortran subroutines
+ *============================================================================*/
+
+extern void CS_PROCF(coh2tb, COH2TB)
+(
+  const cs_real_t  *h,
+  cs_real_t        *t
+);
+
+extern void CS_PROCF(cot2hb, COT2HB)
+(
+  const cs_lnum_t  *n_faces,
+  const cs_lnum_t  *face_ids,
+  const cs_real_t  *t,
+  cs_real_t        *h
+);
+
+/*=============================================================================
+ * Local macro definitions
+ *============================================================================*/
+
+/*============================================================================
+ * Private function definitions
+ *============================================================================*/
+
+/*============================================================================
+ * Type definitions
+ *============================================================================*/
+
+/*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
+
+/*=============================================================================
+ * Public function definitions
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Convert enthalpy to temperature at all cells.
+ *
+ * This handles both user and model enthalpy conversions, so can be used
+ * safely whenever conversion is needed.
+ *
+ * \param[in]   h   enthalpy values
+ * \param[out]  t   temperature values
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_ht_convert_h_to_t_cells(const cs_real_t  h[],
+                           cs_real_t        t[])
+{
+  const cs_lnum_t n_cells = cs_glob_mesh->n_cells;
+
+  cs_real_t *cpro_t = NULL;
+
+  const int *pm_flag = cs_glob_physical_model_flag;
+
+  /* Gas combustion: premixed or diffusion flame */
+  if (   pm_flag[CS_COMBUSTION_EBU] >= 0
+      || pm_flag[CS_COMBUSTION_3PT] >= 0)
+    cpro_t = CS_F_(t)->val;
+
+  /* Pulverized coal or fuel combustion */
+  else if (   pm_flag[CS_COMBUSTION_COAL] >= 0
+           || pm_flag[CS_COMBUSTION_FUEL] >= 0)
+    cpro_t = cs_field_by_name("t_gas")->val;
+
+  /* Electric arcs */
+  else if (   pm_flag[CS_JOULE_EFFECT] >= 1
+           || pm_flag[CS_ELECTRIC_ARCS] >= 1)
+    cpro_t = CS_F_(t)->val;
+
+  /* When temperature maintained by model is available
+     ------------------------------------------------- */
+
+  if (cpro_t != NULL) {
+
+    for (cs_lnum_t i = 0; i < n_cells; i++)
+      t[i] = cpro_t[i];
+
+  }
+
+  /* Default for other cases
+     ----------------------- */
+
+  else {
+
+    const cs_field_t *f_cp = cs_field_by_name("specific_heat");
+    if (f_cp != NULL) {
+      const cs_real_t *cpro_cp = f_cp->val;
+      for (cs_lnum_t i = 0; i < n_cells; i++)
+        t[i] = h[i] / cpro_cp[i];
+    }
+    else {
+      const double cp0 = cs_glob_fluid_properties->cp0;
+      for (cs_lnum_t i = 0; i < n_cells; i++)
+        t[i] = h[i] / cp0;
+    }
+
+  }
+
+  /* Allow user functions */
+
+  int n_zones = cs_volume_zone_n_zones();
+  for (int z_id = 0; z_id < n_zones; z_id++) {
+    const cs_zone_t *z = cs_volume_zone_by_id(z_id);
+
+    /* Note: we could restrict this to
+       z->type & CS_VOLUME_ZONE_PHYSICAL_PROPERTIES
+       but the user can also easily handle this */
+
+    cs_user_physical_properties_h_to_t(cs_glob_domain,
+                                       z,
+                                       false,  /* z_local */
+                                       h,
+                                       t);
+
+  }
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Convert enthalpy to temperature at all boundary faces.
+ *
+ * This handles both user and model enthalpy conversions, so can be used
+ * safely whenever conversion is needed.
+ *
+ * \param[in]   h   enthalpy values
+ * \param[out]  t   temperature values
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_ht_convert_h_to_t_faces(const cs_real_t  h[],
+                           cs_real_t        t[])
+{
+  const cs_mesh_t *m = cs_glob_mesh;
+  const cs_mesh_quantities_t *mq = cs_glob_mesh_quantities;
+  const cs_lnum_t *b_face_cells = m->b_face_cells;
+  const cs_lnum_t n_b_faces = m->n_b_faces;
+  const cs_lnum_t has_dc = mq->has_disable_flag;
+
+  bool need_solid_default = (has_dc) ? true : false;
+
+  const int *pm_flag = cs_glob_physical_model_flag;
+
+  /* Gas combustion: premixed or diffusion flame */
+  if (   pm_flag[CS_COMBUSTION_EBU] >= 0
+      || pm_flag[CS_COMBUSTION_3PT] >= 0)
+    CS_PROCF(coh2tb, COH2TB)(h, t);
+
+  /* Pulverized coal combustion */
+  else if (pm_flag[CS_COMBUSTION_COAL] >= 0)
+    cs_coal_thfieldconv1(CS_MESH_LOCATION_BOUNDARY_FACES, h, t);
+
+  /* Pulverized fuel combustion */
+  else if (pm_flag[CS_COMBUSTION_FUEL] >= 0)
+    cs_fuel_thfieldconv1(CS_MESH_LOCATION_BOUNDARY_FACES, h, t);
+
+  /* Electric arcs */
+  else if (pm_flag[CS_JOULE_EFFECT] < 1 && pm_flag[CS_ELECTRIC_ARCS] >= 1)
+    cs_elec_convert_h_to_t_faces(h, t);
+
+  /* Default for other cases
+     ----------------------- */
+
+  else {
+
+    const cs_field_t *f_cp = cs_field_by_name("specific_heat");
+    if (f_cp != NULL) {
+      const cs_real_t *cpro_cp = f_cp->val;
+      for (cs_lnum_t i = 0; i < n_b_faces; i++) {
+        cs_lnum_t c_id = b_face_cells[i];
+        t[i] = h[i] / cpro_cp[c_id];
+      }
+    }
+    else {
+      const double cp0 = cs_glob_fluid_properties->cp0;
+      for (cs_lnum_t i = 0; i < n_b_faces; i++)
+        t[i] = h[i] / cp0;
+    }
+
+    need_solid_default = false;
+
+  }
+
+  /* Default for solid zones
+     ----------------------- */
+
+  if (need_solid_default) {
+
+    assert(has_dc == 1);
+    const int *disable_flag = mq->c_disable_flag;
+
+    const cs_field_t *f_cp = cs_field_by_name("specific_heat");
+    if (f_cp != NULL) {
+      const cs_real_t *cpro_cp = f_cp->val;
+      for (cs_lnum_t i = 0; i < n_b_faces; i++) {
+        cs_lnum_t c_id = b_face_cells[i];
+        if (disable_flag[c_id])
+          t[i] = h[i] / cpro_cp[c_id];
+      }
+    }
+    else {
+      const double cp0 = cs_glob_fluid_properties->cp0;
+      for (cs_lnum_t i = 0; i < n_b_faces; i++) {
+        cs_lnum_t c_id = b_face_cells[i];
+        if (disable_flag[c_id])
+          t[i] = h[i] / cp0;
+      }
+    }
+
+  }
+
+  /* Allow user functions */
+
+  int n_zones = cs_boundary_zone_n_zones();
+  for (int z_id = 0; z_id < n_zones; z_id++) {
+    const cs_zone_t *z = cs_boundary_zone_by_id(z_id);
+
+    cs_user_physical_properties_h_to_t(cs_glob_domain,
+                                       z,
+                                       false,  /* z_local */
+                                       h,
+                                       t);
+
+  }
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Convert temperature to enthalpy at selected boundary faces.
+ *
+ * This handles both user and model enthalpy conversions, so can be used
+ * safely whenever conversion is needed.
+ *
+ * \param[in]   n_faces   number of selected boundary faces
+ * \param[in]   face_ids  list of associated face ids
+ * \param[in]   t         temperature values
+ * \param[out]  h         enthalpy values
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_ht_convert_t_to_h_faces_l(cs_lnum_t        n_faces,
+                             const cs_lnum_t  face_ids[],
+                             const cs_real_t  t[],
+                             cs_real_t        h[])
+{
+  const cs_mesh_t *m = cs_glob_mesh;
+  const cs_mesh_quantities_t *mq = cs_glob_mesh_quantities;
+  const cs_lnum_t *b_face_cells = m->b_face_cells;
+  const cs_lnum_t n_b_faces = m->n_b_faces;
+  const cs_lnum_t has_dc = mq->has_disable_flag;
+
+  const int *pm_flag = cs_glob_physical_model_flag;
+
+  bool need_solid_default = (has_dc) ? true : false;
+
+  /* Gas combustion: premixed or diffusion flame */
+  if (   pm_flag[CS_COMBUSTION_EBU] >= 0
+      || pm_flag[CS_COMBUSTION_3PT] >= 0)
+    CS_PROCF(cot2hb, COT2HB)(&n_faces, face_ids, t, h);
+
+  /* Pulverized coal combustion */
+  else if (pm_flag[CS_COMBUSTION_COAL] >= 0)
+    cs_coal_bt2h(n_faces, face_ids, t, h);
+
+  /* Pulverized fuel combustion */
+  else if (pm_flag[CS_COMBUSTION_FUEL] >= 0)
+    cs_fuel_bt2h(n_faces, face_ids, t, h);
+
+  /* Electric arcs */
+  else if (pm_flag[CS_JOULE_EFFECT] < 1 && pm_flag[CS_ELECTRIC_ARCS] >= 1)
+    cs_elec_convert_t_to_h_faces(n_faces,  face_ids, t, h);
+
+  /* Default for other cases
+     ----------------------- */
+
+  else {
+
+    const cs_field_t *f_cp = cs_field_by_name("specific_heat");
+    if (f_cp != NULL) {
+      const cs_real_t *cpro_cp = f_cp->val;
+      for (cs_lnum_t i = 0; i < n_faces; i++) {
+        cs_lnum_t f_id = face_ids[i];
+        cs_lnum_t c_id = b_face_cells[f_id];
+        h[f_id] = t[f_id] * cpro_cp[c_id];
+      }
+    }
+    else {
+      const double cp0 = cs_glob_fluid_properties->cp0;
+      for (cs_lnum_t i = 0; i < n_faces; i++) {
+        cs_lnum_t f_id = face_ids[i];
+        h[f_id] = t[f_id] * cp0;
+      }
+    }
+
+    need_solid_default = false;
+
+  }
+
+  /* Default for solid zones
+     ----------------------- */
+
+  if (need_solid_default) {
+
+    assert(has_dc == 1);
+    const int *disable_flag = mq->c_disable_flag;
+
+    const cs_field_t *f_cp = cs_field_by_name("specific_heat");
+    if (f_cp != NULL) {
+      const cs_real_t *cpro_cp = f_cp->val;
+      for (cs_lnum_t i = 0; i < n_faces; i++) {
+        cs_lnum_t f_id = face_ids[i];
+        cs_lnum_t c_id = b_face_cells[f_id];
+        if (disable_flag[c_id])
+          h[f_id] = t[f_id] * cpro_cp[c_id];
+      }
+    }
+    else {
+      const double cp0 = cs_glob_fluid_properties->cp0;
+      for (cs_lnum_t i = 0; i < n_faces; i++) {
+        cs_lnum_t f_id = face_ids[i];
+        cs_lnum_t c_id = b_face_cells[f_id];
+        if (disable_flag[c_id])
+          h[f_id] = t[f_id] * cp0;
+      }
+    }
+
+  }
+
+  /* Allow user functions */
+
+  cs_real_t *h_f;
+  BFT_MALLOC(h_f, n_b_faces, cs_real_t);
+  for (cs_lnum_t i = 0; i < n_b_faces; i++)
+    h_f[i] = h[i];
+
+  int n_zones = cs_boundary_zone_n_zones();
+  for (int z_id = 0; z_id < n_zones; z_id++) {
+    const cs_zone_t *z = cs_boundary_zone_by_id(z_id);
+
+    cs_user_physical_properties_t_to_h(cs_glob_domain,
+                                       z,
+                                       false,  /* z_local */
+                                       t,
+                                       h_f);
+  }
+
+  for (cs_lnum_t i = 0; i < n_faces; i++) {
+    cs_lnum_t f_id = face_ids[i];
+    h[f_id] = h_f[f_id];
+  }
+}
+
+/*----------------------------------------------------------------------------*/
+
+END_C_DECLS

docs/doxygen/examples/cs_user_physical_properties-ht_convert.dox
@@ -1,0 +1,88 @@
+/*============================================================================
+ * Code_Saturne documentation page
+ *============================================================================*/
+
+/*
+  This file is part of Code_Saturne, a general-purpose CFD tool.
+
+  Copyright (C) 1998-2021 EDF S.A.
+
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation; either version 2 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+  Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/*-----------------------------------------------------------------------------*/
+
+/*!
+  \page user_ht_convert Examples of enthalpy-temperature conversion law
+
+  \section ht_convert_intro Introduction
+
+  By default, when not using a specific physical model (which
+  will usually contain a more advanced conversion), a simple
+  \f[ H = C_p T \f] law is used.
+
+  This simple law will also be used by default in solid zones even
+  when using a specific fluid model.
+
+  When using variable Cp values, an appropriate temperaure to enthalpy
+  conversion should be defined by the user if solving the Enthalpy.
+
+  The \ref cs_user_physical_properties_h_to_t and
+  \ref cs_user_physical_properties_t_to_h functions (in
+  \ref cs_user_physical_properties.c) can be used to define an
+  enthalpy-temperature conversion law and its inverse.
+
+  If can be defined on a "per-zone" basis if needed in case different
+  zones (both solid and boundary) correspond to different fluids
+  or solid ones.
+
+  \section user_ht_convert_ex Example
+
+  The following code blocks show an example of enthalpy-temperature
+  conversion law.
+
+  \subsection user_ht_convert_tab Tabulation
+
+  The following block is added either outside
+  \ref cs_user_physical_properties_h_to_t and
+  \ref cs_user_physical_properties_t_to_h functions (before all function
+  definitions in \ref cs_user_physical_properties.c), or in both
+  of these functions:
+
+  \snippet cs_user_physical_properties.c tabulation
+
+  \subsection user_ht_convert_h_to_t Enthalpy to temperature
+
+  The following block is then used in \ref cs_user_physical_properties_h_to_t
+  to convert enthalpy to temperature:
+
+  \snippet cs_user_physical_properties.c z_h_to_t
+
+  Note that \c z->name or \c z->location_id can be used as a filter
+  if "per-zone" properties are needed (such as with solid zones).
+
+  \subsection user_ht_convert_h_to_t Temperature to Enthalpy
+
+  The following block must then be added in
+  \ref cs_user_physical_properties_t_to_h for the matching
+  temperature to Enthalpy conversion:
+
+  \snippet cs_user_physical_properties.c z_t_to_h
+
+  Note that temperature to enthalpy conversion is only needed
+  when solving the Enthalpy and using radiative tranfers or
+  temperature-based boundary conditions.
+
+*/

docs/doxygen/examples/cs_user_physical_properties.h
@@ -100,12 +100,6 @@
   The values of the isobaric specific heat values are provided as a function
   of the temperature. All variables are evaluated at the cell centers.
 
-  \warning:
-  do not discard the call to the subroutine 'usthht' at the end of this
-  example: its purpose is to calculate the isochoric specific heat.
-  Indeed, this variable needs to be computed from the isobaric specific heat
-  using the thermodynamics laws.
-
   Here is the corresponding code:
 
   \snippet cs_user_physical_properties-compressible_flow.f90 example_3

docs/doxygen/examples/cs_user_tabs.h
@@ -78,7 +78,7 @@ Here is a list of user examples. Thoroughly documented snippets are provided.
 \section example_other Other Calculation control
    - \subpage cs_user_modules
    - \subpage user_solver
-   - \subpage us_thht
+   - \subpage user_ht_convert
    - \subpage us_vosy
    - \subpage us_pt1d
    - \subpage cs_user_lagr_sde

docs/doxygen/examples/usthht.h
@@ -1,62 +1,0 @@
-/*============================================================================
- * Code_Saturne documentation page
- *============================================================================*/
-
-/*
-  This file is part of Code_Saturne, a general-purpose CFD tool.
-
-  Copyright (C) 1998-2021 EDF S.A.
-
-  This program is free software; you can redistribute it and/or modify it under
-  the terms of the GNU General Public License as published by the Free Software
-  Foundation; either version 2 of the License, or (at your option) any later
-  version.
-
-  This program is distributed in the hope that it will be useful, but WITHOUT
-  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-  details.
-
-  You should have received a copy of the GNU General Public License along with
-  this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
-  Street, Fifth Floor, Boston, MA 02110-1301, USA.
-*/
-
-/*-----------------------------------------------------------------------------*/
-
-/*!
-  \page us_thht Examples of enthalpy-temperature conversion law (usthht.f90)
-
-   The \ref usthht subroutine is used to encapsulate a simple
-   enthalpy-temperature conversion law and its inverse.
-
-  \section usthht_h_intro Introduction
-
-  The \ref usthht function allows one to define a simple
-  enthalpy-temperature conversion law and its inverse. The
-  parameters \c mode allows one to know in which direction
-  the conversion will be made.
-
-  \section usthht_h_usthht Examples
-
-  The following code blocks show two examples of entahlpy-temperature
-  conversion law.
-
-  \subsection usthht_h_usthht_1 Example 1
-
-  The first example corresponds to a simple law : \f[H = C_p  dt \f]
-
-  \snippet usthht.f90 example_1
-
-  \subsection usthht_h_usthht_2 Example 2
-
-  The second example corresponds to a simple interpolation based on
-  a tabulation defined hereafter and declared as a variable :
-
-  \snippet usthht.f90 loc_var_dec
-
-  The enthalpy-temperature conversion law is then defined :
-
-  \snippet usthht.f90 example_2
-
-*/

docs/theory/elbase.tex
@@ -1065,7 +1065,7 @@ positionnement des variables}
                 \'electrique. Pour les \'etudes Joule, en particulier, les propri\'et\'es
                 doivent \^etre fournies ici sous forme de loi (des exemples sont
                 disponibles)\\
-                 &                 &                 & \fort{usthht}
+                 &                 & \fort{cs\_user\_physical\_properties} &
         & Transformation temp\'erature-enthalpie et enthalpie-temp\'erature
                 fournie par l'utilisateur (plus sp\'ecifiquement pour les
                 \'etudes Joule, pour lesquelles on ne dispose pas d'un fichier

docs/user/saturne1.tex
@@ -1824,9 +1824,10 @@ defined as (\texttt{iscalt}$>0$).
 
 {\em WARNING: The 1D thermal module is developed assuming the thermal scalar
  as a temperature. If the thermal scalar is an enthalpy, the code calls the
- subroutine \texttt{usthht} for each transfer of data between the fluid
- and the wall in order to convert the enthalpy to temperature and vice-versa.
- This function has not been tested and is firmly discouraged. If the thermal
+ enthalpy to temperature conversion as defined by the model defaults,
+ or by the user in \texttt{cs\_user\_physical\_properties} for each
+ transfer of data between the fluid and the wall in order to convert the
+ enthalpy to temperature and vice-versa. If the thermal
  variable is the total (compressible) energy, the thermal module will not work.}
 
 %==================================
@@ -1908,7 +1909,8 @@ When the continuous phase flow is unsteady, it is no longer possible to use stea
 Finally, when the so-called complete model is used for turbulent dispersion modelling, the user must make sure that the volumetric statistics are directly used for the calculation of the locally undisturbed fluid flow field.\\
 
 When the thermal evolution of the particles is activated, the associated particulate scalars are always the inclusion temperature and the locally undisturbed fluid flow temperature expressed in degrees Celsius, whatever the thermal scalar associated with the continuous phase is ({\em i.e.} temperature or enthalpy). If the
-thermal scalar associated with the continuous phase is the temperature in Kelvin, the unit is converted automatically into Celsius. If the thermal scalar associated with the continuous phase is the enthalpy, the enthalpy-temperature conversion subroutine \texttt{usthht} must be completed for \texttt{mode}=1, and must express temperatures in degrees Celsius. In all cases, the thermal backward coupling of the dispersed phase on the continuous phase is adapted to the thermal scalar transported by the fluid.
+thermal scalar associated with the continuous phase is the temperature in Kelvin, the unit is converted automatically into Celsius. If the thermal scalar associated with the continuous phase is the enthalpy, a \texttt{temperature} property or postprocessing
+field must be defined. In all cases, the thermal backward coupling of the dispersed phase on the continuous phase is adapted to the thermal scalar transported by the fluid.
 
 
 %==================================
@@ -2204,14 +2206,13 @@ It should be noted that the enthalpy is relevant.
 
 \begin{list}{-}{}
 \item For the electric arcs module, the enthalpy value is taken from the temperature
- of reference \texttt{t0} (given in \texttt{cs\_user\_parameters.f90}) from the temperature-enthalpy
- tables
- supplied in the data file \texttt{dp\_ELE.} The user must not intervene here.
-\item For the Joule effect module, the value of enthalpy must be specified by the user
-. An example is given of how to obtain the enthalpy from the temperature of reference
- \texttt{t0}(given in \texttt{cs\_user\_parameters.f90}), the temperature-enthalpy law must be
-supplied. A code is suggested in the \texttt{usthht} subroutine (provided for
- the determination of physical properties).
+ of reference \texttt{t0} (given in \texttt{cs\_user\_parameters.c})
+ from the temperature-enthalpy tables supplied in the data file \texttt{dp\_ELE.}
+ The user must not intervene here.
+\item For the Joule effect module, the value of enthalpy must be specified by the user.
+ Examples of temperature to enthalpy conversion are given in
+ \texttt{cs\_user\_physical\_properties.c}). If not defined, a simple default
+ law is used ($H = C_p T$).
 \end{list}
 
 %==================================
@@ -2235,9 +2236,10 @@ variables. Particular care should be taken with non-linear laws (for example, a
 For the Joule effect, the user is required to supply the physical properties in the
 subroutine. Examples are given which are to be adapted by the user. If the temperature is
 to be determined to calculate the physical properties, the solved variable, enthalpy must
- be deduced. The preferred temperature-enthalpy law can be selected in the subroutine
- \texttt{usthht} (an example of the interpolation is given from the law table. This
-subroutine can be re-used for the initialisation of the variables(\texttt{cs\_user\_initialization}))
+ be deduced. The preferred temperature-enthalpy law should be defined
+ (a general example is provided in (\texttt{cs\_user\_physical\_properties}),
+ and can be used for the initialisation of the variables in
+ (\texttt{cs\_user\_initialization})).
  For the electric arcs module, the physical properties are interpolated from the data file
  \texttt{dp\_ELE} supplied by the user. Modifications are generally not necessary.
 

src/base/Makefile.am
@@ -41,6 +41,7 @@ AM_CPPFLAGS = \
 -I$(top_srcdir)/src/cdo \
 -I$(top_srcdir)/src/cfbl \
 -I$(top_srcdir)/src/ctwr \
+-I$(top_srcdir)/src/elec \
 -I$(top_srcdir)/src/lagr \
 -I$(top_srcdir)/src/mesh \
 -I$(top_srcdir)/src/bft \
@@ -115,6 +116,7 @@ cs_gas_mix.h \
 cs_halo.h \
 cs_halo_perio.h \
 cs_head_losses.h \
+cs_ht_convert.h \
 cs_interface.h \
 cs_interpolate.h \
 cs_internal_coupling.h \
@@ -212,6 +214,7 @@ cs_crystal_router.c \
 cs_defs.c \
 cs_file.c \
 cs_fp_exception.c \
+cs_ht_convert.c \
 cs_interface.c \
 cs_io.c \
 cs_log.c \
@@ -231,9 +234,6 @@ libcscore_la_LIBADD = libcscorep.la $(top_builddir)/src/fvm/libfvm.la
 libcsbase_la_SOURCES = \
 addfld.f90 \
 altycl.f90 \
-b_t_to_h.f90 \
-b_h_to_t.f90 \
-c_h_to_t.f90 \
 calhyd.f90 \
 caltri.f90 \
 cs_mass_source_terms.c \

commit 26fa58430eb04f26cdfd3a5f325244d4d8dd3788

Free memory leak introduced by commit 8b7b1185 (April 7 2021).

src/base/cs_ht_convert.c
@@ -519,6 +519,8 @@ cs_ht_convert_t_to_h_faces_l(cs_lnum_t        n_faces,
     cs_lnum_t f_id = face_ids[i];
     h[f_id] = h_f[f_id];
   }
+
+  BFT_FREE(h_f);
 }
 
 /*----------------------------------------------------------------------------*/