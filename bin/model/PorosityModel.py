code-saturne/code_saturne

commit 046e63e2f826fd48d6d386417b58afafe97fb2ce

[GUI] Fix porosity page model
  * porosity model was always set to "isotropic"
  * bug introduced in commit 5e9af0ccfe (2020-09-28)

bin/model/PorosityModel.py
@@ -106,7 +106,13 @@ class PorosityModel(Variables, Model):
         Set name and label zone for porosity markups.
         """
         self.node_porosit.xmlInitChildNode('porosity', zone_id=zoneid)
-        mdl = self.__defaultValues()['choice']
+
+        # Use existing model or default if none is defined
+        try:
+            n = self.node_porosit.xmlGetNode('porosity', zone_id=zoneid)
+            mdl = n['model']
+        except:
+            mdl = self.__defaultValues()['choice']
         self.setPorosityModel(zoneid, mdl)
 
 

commit bbd72b022e52c22db777db935c3d4219b2e4fd05

[GUI] Fix possible crash when calling PorosityModel through python API
  * Bug/Weakness introduced in commit 046e63e2 (2021-05-14)

bin/model/PorosityModel.py
@@ -107,12 +107,21 @@ class PorosityModel(Variables, Model):
         """
         self.node_porosit.xmlInitChildNode('porosity', zone_id=zoneid)
 
+        # Initialize mdl at None for sanity sake
+        mdl = None
+
         # Use existing model or default if none is defined
         try:
             n = self.node_porosit.xmlGetNode('porosity', zone_id=zoneid)
             mdl = n['model']
         except:
+            pass
+
+        # If model is None set to default.
+        # Could be the result of 'try' failure or un-initialized zone
+        if not mdl:
             mdl = self.__defaultValues()['choice']
+
         self.setPorosityModel(zoneid, mdl)
 
 