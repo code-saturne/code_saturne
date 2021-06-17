code-saturne/code_saturne

commit ce8b290011f25983656d4bb300ceb399239cc775

Fix: without no GUI, missing initialization of thermal diffusivity (bug 0000062).
git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@6178 e44c5d27-f830-0410-8ce8-974f7a764be6

src/base/modini.f90
@@ -1022,6 +1022,16 @@ enddo
 ! Elles sont prises egales aux valeurs correspondantes pour le
 ! scalaire associe.
 
+if (iscalt.gt.0) then
+  if (visls0(iscalt).lt.-grand) then
+    if (itherm .eq. 1) then
+      visls0(iscalt) = viscl0
+    else if (itherm .eq. 2) then
+      visls0(iscalt) = viscl0 / cp0
+    endif
+  endif
+endif
+
 if (nscaus.gt.0) then
   do jj = 1, nscaus
     if (iscavr(jj).le.0 .and. visls0(jj).lt.-grand) then

src/base/fldvar.f90
@@ -271,6 +271,7 @@ if (nmodpp.eq.0) then
     call add_model_scalar_field('enthalpy', 'Enthalpy', ihm)
     iscalt = ihm
   endif
+  ivisls(iscalt) = 0
 
   if (itherm.ne.0 .and. iihmpr.eq.1) then
     call uithsc(iscalt)

src/user/cs_user_parameters.f90
@@ -1657,13 +1657,12 @@ endif
 !       Indeed the diffusivity of the variance of a scalar is assumed
 !       identical to that scalar's diffusivity.
 
-!     When no specific physics has been activated
-!       (coal, combustion, electric arcs) and if a user scalar represents:
-!       - the temperature:
-!       visls0(iscalt) = Lambda
-!        because the Cp is outside of the diffusion term in the temperature equation
-!       - the enthalpy:
-!       visls0(iscalt) = Lambda/Cp
+!     For user scalars jj behaving as a temperature (iscacp(jj) = 1),
+!       Cp is outside of the diffusion term in the temperature equation, so:
+!       visls0(jj) = Lambda
+
+!     For user scalars jj NOT behaving as a temperature (iscacp(jj) = 0),
+!       visls0(jj) = Lambda/Cp
 
 !     Here, as an example, we assign to viscl0 the viscosity of the fluid
 !       phase, which is fitting for passive tracers which follow the fluid

commit 14624d90124ae3f94f042ba5dbe9951048288c5a

Fix default setting of thermal conductivity for enthalpy or temperature is the GUI is not used (bug introduced in commit ce8b290011 (2014-05-27)).
This should be merge after review.

src/base/modini.f90
@@ -885,10 +885,14 @@ enddo
 if (iscalt.gt.0) then
   call field_get_key_double(ivarfl(isca(iscalt)), kvisl0, visls_0)
   if (visls_0.lt.-grand) then
+    ! For the temperature, the diffusivity factor is directly the
+    ! thermal conductivity
+    ! lambda = Cp * mu / Pr
+    ! where Pr is the (molecular) Prandtl number
     if (itherm .eq. 1) then
-      visls_0 = viscl0
+      visls_0 = viscl0 * cp0
     else if (itherm .eq. 2) then
-      visls_0 = viscl0 / cp0
+      visls_0 = viscl0
     endif
     call field_set_key_double(ivarfl(isca(iscalt)), kvisl0, visls_0)
   endif