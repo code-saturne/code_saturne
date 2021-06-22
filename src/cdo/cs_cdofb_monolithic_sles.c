code-saturne/code_saturne

commit 9b63e55093adf11c8a25c83c7f2e8a9b07953993

Fix compilation with openMP induced by the commit 91889b2d

src/cdo/cs_cdofb_monolithic_sles.c
@@ -3688,7 +3688,7 @@ cs_cdofb_monolithic_solve(const cs_navsto_param_t       *nsp,
     const cs_real_t  *solx = sol, *soly = sol+n_faces, *solz = sol+2*n_faces;
 
 # pragma omp parallel for if (CS_THR_MIN > n_faces)                     \
-  shared(gpx, gpy, gpz, dx, dy, dz, solx, doly, solz) firstprivate(n_faces)
+  shared(dx, dy, dz, solx, doly, solz) firstprivate(n_faces)
     for (cs_lnum_t f = 0; f < n_faces; f++) {
       u_f[3*f  ] = solx[f] - alpha * grad_p[3*f]/dx[f];
       u_f[3*f+1] = soly[f] - alpha * grad_p[3*f+1]/dy[f];

commit 2c05fb31904b6a6fed0d5a7897e4fca2196c1731

Re-fix compilation, broken by commit 9b63e550.

src/cdo/cs_cdofb_monolithic_sles.c
@@ -3688,7 +3688,7 @@ cs_cdofb_monolithic_solve(const cs_navsto_param_t       *nsp,
     const cs_real_t  *solx = sol, *soly = sol+n_faces, *solz = sol+2*n_faces;
 
 # pragma omp parallel for if (CS_THR_MIN > n_faces)                     \
-  shared(dx, dy, dz, solx, doly, solz) firstprivate(n_faces)
+  shared(dx, dy, dz, solx, soly, solz) firstprivate(n_faces)
     for (cs_lnum_t f = 0; f < n_faces; f++) {
       u_f[3*f  ] = solx[f] - alpha * grad_p[3*f]/dx[f];
       u_f[3*f+1] = soly[f] - alpha * grad_p[3*f+1]/dy[f];