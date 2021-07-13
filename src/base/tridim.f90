code-saturne/code_saturne

commit 9a9fe13795887554ed1d231c80bfd82fe1aa760b

Integrate a 2nd order time scheme based on collocated time grids.
- Details can be found in L. MA, Numerical Heat Transfer
  Fundamentals 76(3):1-23, 2019

src/base/tridim.f90
@@ -140,7 +140,7 @@ double precision, pointer, dimension(:) :: hbord, theipb
 double precision, pointer, dimension(:) :: visvdr
 double precision, allocatable, dimension(:) :: prdv2f
 double precision, allocatable, dimension(:) :: mass_source
-double precision, dimension(:), pointer :: brom, crom
+double precision, dimension(:), pointer :: brom, crom, cpro_rho_mass
 
 double precision, pointer, dimension(:,:) :: trava
 double precision, dimension(:,:), pointer :: vel
@@ -1136,6 +1136,20 @@ do while (iterns.le.nterup)
       if (fluid_solid) call cs_porous_model_set_has_disable_flag(1)
       call phyvar(nvar, nscal, iterns, dt)
 
+      ! Correct the scalar to ensure scalar conservation
+      call field_get_key_id("is_buoyant", key_buoyant_id)
+      call field_get_val_s(icrom,crom)
+      call field_get_val_s_by_name("density_mass",cpro_rho_mass)
+      do iscal = 1, nscal
+        ivar = isca(iscal)
+        call field_get_key_int(ivarfl(ivar), key_buoyant_id, is_buoyant_fld)
+        if (is_buoyant_fld.eq.1) then
+          call field_get_val_s(ivarfl(ivar),cvar_sca)
+          do iel = 1, ncel
+            cvar_sca(iel) = cvar_sca(iel)*cpro_rho_mass(iel)/crom(iel)
+          enddo
+        endif
+      enddo
     endif
 
     if (vcopt_u%iwarni.ge.1) then
@@ -1195,6 +1209,11 @@ do while (iterns.le.nterup)
 
       endif
 
+    endif
+
+    if (istmpf.eq.2.and.itpcol.eq.1) then
+      iappel = 3
+      call schtmp(nscal, iappel)
     endif
 
     !     Si c'est la derniere iteration : INSLST = 1

src/base/covofi.f90
@@ -529,13 +529,13 @@ if (st_prv_id .ge. 0) then
     cpro_st => cpro_scal_st
   endif
 endif
+
 if (st_prv_id .ge. 0) then
   do iel = 1, ncel
     smbexp = cproa_scal_st(iel)
     ! If the scalar is not buoyant no need of saving the current source term,
     ! save directly the previous one
     cpro_st(iel) = smbrs(iel)
-
     ! Terme source du pas de temps precedent et
     ! On suppose -ROVSDT > 0 : on implicite
     !    le terme source utilisateur (le reste)

src/base/iniini.f90
@@ -354,6 +354,8 @@ itempb = -1
 !     = 2 Ordre deux
 istmpf = -999
 
+itpcol = -999
+
 !   -- Termes sources Navier Stokes
 !     Pour les termes sources explicites en std, I..EXT definit
 !       l'extrapolation -theta ancien + (1+theta) nouveau

src/base/navstv.f90
@@ -382,7 +382,7 @@ if (irovar.eq.1.and.(idilat.gt.1.or.ivofmt.gt.0.or.ippmod(icompf).eq.3)) then
   call field_get_val_s(f_id, bpro_rho_mass)
 
   ! Time interpolated density
-  if (vcopt_u%thetav .lt. 1.d0) then
+  if (vcopt_u%thetav .lt. 1.d0 .and. itpcol .eq. 0) then
     call field_get_val_prev_s(icrom, croma)
     call field_get_val_prev_s(ibrom, broma)
     allocate(cpro_rho_tc(ncelet))

src/base/optcal.f90
@@ -75,6 +75,15 @@ module optcal
   !> and \ref istmpf = 1 otherwise.
   integer, save ::          istmpf
 
+  !> Time scheme option:
+  !>    - 0: scattered time scheme. On the time grids, the velocity is
+  !>         half a time step behind the density and the buoyant scalar.
+  !>         (See the thesis of C.D. PIERCE, 2004)
+  !>    - 1: collocated time scheme. On the time grids, the velocity is
+  !>         at the same location as the density and the buoyant scalar.
+  !>         (See L. MA, 2019)
+  integer, save ::          itpcol
+
   !> number of iterations on the velocity-pressure coupling on Navier-Stokes
   !> (for the U/P inner iterations scheme)
   integer(c_int), pointer, save ::          nterup

src/base/predvv.f90
@@ -288,7 +288,7 @@ if ((    (ippmod(icompf).ge.0.and.ippmod(icompf).ne.3)   &
 ! VOF algorithm and Low Mach compressible algos: density at time n-1
 else if ((idilat.gt.1.or.ivofmt.gt.0.or.ippmod(icompf).eq.3) &
          .and.irovar.eq.1) then
-  if (iterns.eq.1) then
+  if (itpcol.eq.0.and.iterns.eq.1) then
     pcrom => cromaa
   else
     pcrom => croma
@@ -327,7 +327,8 @@ endif
 ! NB: the mass flux (n+1) is overwritten because not used after.
 ! The mass flux for (n->n+1) will be recomputed in resopv
 ! FIXME irovar=1 and if dt varies, use theta(rho) = theta(u)*...
-if (vcopt_u%thetav .lt. 1.d0 .and. iappel.eq.1 .and. iterns.gt.1) then
+if (vcopt_u%thetav .lt. 1.d0 .and. iappel.eq.1 .and. iterns.gt.1   &
+    .and. itpcol .eq. 0) then
   allocate(cproa_rho_tc(ncelet))
 
   ! Pointer to the previous mass fluxes

src/base/resopv.f90
@@ -231,12 +231,12 @@ double precision, dimension(:), pointer :: coefaf_p, coefbf_p
 double precision, allocatable, dimension(:) :: iflux, bflux
 double precision, allocatable, dimension(:), target :: xdtsro
 double precision, allocatable, dimension(:), target  :: divu
+double precision, allocatable, dimension(:) :: divu_prev
 double precision, allocatable, dimension(:,:), target :: tpusro
 double precision, dimension(:), pointer :: viscap
 double precision, dimension(:,:), pointer :: vitenp
 double precision, dimension(:), pointer :: imasfl, bmasfl, imasfla, bmasfla
 double precision, dimension(:), pointer :: porosi
-
 double precision, dimension(:), pointer :: brom, crom, croma, broma
 double precision, dimension(:), pointer :: brom_eos, crom_eos
 double precision, dimension(:), pointer :: cvar_pr, cvara_pr
@@ -358,7 +358,8 @@ if (irovar.eq.1.and.(idilat.gt.1.or.ivofmt.gt.0.or.ippmod(icompf).eq.3)) then
   call field_get_val_s(f_id, bpro_rho_mass)
 
   ! Time interpolated density
-  if (vcopt_u%thetav .lt. 1.d0 .and. iterns.gt.1) then
+  if (vcopt_u%thetav .lt. 1.d0 .and. iterns.gt.1  &
+      .and. itpcol.eq.0) then
     allocate(cpro_rho_tc(ncelet))
     allocate(bpro_rho_tc(nfabor))
 
@@ -375,7 +376,6 @@ if (irovar.eq.1.and.(idilat.gt.1.or.ivofmt.gt.0.or.ippmod(icompf).eq.3)) then
     enddo
 
     brom => bpro_rho_tc
-
   else
     crom => cpro_rho_mass
     brom => bpro_rho_mass
@@ -1438,11 +1438,26 @@ endif
 if ((idilat.eq.2.or.idilat.eq.3).and.ippmod(icompf).ne.3) then
 
   ! Add source term
-  do iel = 1, ncel
-    drom = crom_eos(iel) - croma(iel)
-    cpro_divu(iel) = cpro_divu(iel) + drom*cell_f_vol(iel)/dt(iel)
-  enddo
+  if (itpcol.eq.1.and.vcopt_u%thetav.lt.1.d0) then
+    call field_get_val_prev_s(iflmas, imasfla)
+    call field_get_val_prev_s(iflmab, bmasfla)
 
+    allocate(divu_prev(ncelet))
+    init = 1
+    call divmas(init, imasfla, bmasfla, divu_prev)
+
+    do iel = 1, ncel
+      drom = crom_eos(iel) - croma(iel)
+      cpro_divu(iel) = cpro_divu(iel) +                                       &
+                       (1.d0 + vcopt_u%thetav)*drom*cell_f_vol(iel)/dt(iel)   &
+                      + vcopt_u%thetav*divu_prev(iel)
+    enddo
+  else
+    do iel = 1, ncel
+      drom = crom_eos(iel) - croma(iel)
+      cpro_divu(iel) = cpro_divu(iel) + drom*cell_f_vol(iel)/dt(iel)
+    enddo
+  endif
 endif
 
 ! ---> Termes sources Lagrangien
@@ -2411,6 +2426,7 @@ deallocate(dam, xam)
 deallocate(trav)
 deallocate(res, phia, dphi)
 if (allocated(divu)) deallocate(divu)
+if (allocated(divu_prev)) deallocate(divu_prev)
 deallocate(gradp)
 deallocate(rhs, rovsdt)
 if (allocated(weighf)) deallocate(weighf, weighb)

src/base/schtmp.f90
@@ -79,11 +79,12 @@ integer          key_t_ext_id, icpext
 integer          iviext
 integer          iroext
 
-double precision flux   , theta  , viscos, varcp
+double precision flux   , theta  , aa, bb, viscos, varcp
 
 double precision, dimension(:), pointer :: i_mass_flux, b_mass_flux
 double precision, dimension(:), pointer :: i_mass_flux_prev, b_mass_flux_prev
-double precision, dimension(:), pointer :: brom, crom
+double precision, dimension(:), pointer :: brom, broma, bromaa
+double precision, dimension(:), pointer :: crom, croma, cromaa
 double precision, dimension(:), pointer :: cpro_viscl, cpro_visct
 double precision, dimension(:), pointer :: cpro_cp, cpro_visls
 double precision, dimension(:), pointer :: cproa_cp, cproa_visls, cproa_visct
@@ -110,8 +111,26 @@ if (iappel.eq.1) then
   if (istmpf.ne.0 .or. staggered.eq.1) then
     call field_get_key_int(ivarfl(iu), kimasf, iflmas)
     call field_get_key_int(ivarfl(iu), kbmasf, iflmab)
-    call field_current_to_previous(iflmas)
-    call field_current_to_previous(iflmab)
+    if (itpcol.eq.0) then
+      call field_current_to_previous(iflmas)
+      call field_current_to_previous(iflmab)
+    else
+      call field_get_val_s(iflmas, i_mass_flux)
+      call field_get_val_s(iflmab, b_mass_flux)
+      call field_get_val_prev_s(iflmas, i_mass_flux_prev)
+      call field_get_val_prev_s(iflmab, b_mass_flux_prev)
+      do ifac = 1 , nfac
+        flux                   =                          i_mass_flux(ifac)
+        i_mass_flux(ifac)      = 2.d0*i_mass_flux(ifac) - i_mass_flux_prev(ifac)
+        i_mass_flux_prev(ifac) = flux
+      enddo
+
+      do ifac = 1 , nfabor
+        flux                   =                          b_mass_flux(ifac)
+        b_mass_flux(ifac)      = 2.d0*b_mass_flux(ifac) - b_mass_flux_prev(ifac)
+        b_mass_flux_prev(ifac) = flux
+      enddo
+    endif
   endif
 
   ! If required, the density at time step n-1 is updated
@@ -120,17 +139,36 @@ if (iappel.eq.1) then
   ! Note that at the begining of the calculation, previous values have been
   ! initialized by inivar
   if (irovar.gt.0) then
+    call field_get_id("density_mass", f_id)
+    call field_get_val_s(f_id, cpro_rho_mass)
+    call field_get_id("boundary_density_mass", f_id)
+    call field_get_val_s(f_id, bpro_rho_mass)
+    if (ischtp.eq.2.and.itpcol.eq.1) then
+      call field_get_val_s(icrom, crom)
+      call field_get_val_prev_s(icrom, croma)
+      call field_get_val_prev2_s(icrom, cromaa)
+
+      do iel = 1, ncelet
+        cpro_rho_mass(iel) = 3.d0*crom(iel) - 3.d0*croma(iel)     &
+                            + cromaa(iel)
+      enddo
+
+      call field_get_val_s(ibrom, brom)
+      call field_get_val_prev_s(ibrom, broma)
+      call field_get_val_prev2_s(ibrom, bromaa)
+      do ifac = 1, nfabor
+        bpro_rho_mass(ifac) = 3.d0*brom(ifac) - 3.d0*broma(ifac)  &
+                             + bromaa(ifac)
+      enddo
+    endif
+
     call field_current_to_previous(icrom)
     call field_current_to_previous(ibrom)
 
-    if (idilat.gt.1.or.ivofmt.gt.0.or.ippmod(icompf).eq.3) then
+    if (((idilat.gt.1.or.ivofmt.gt.0).and.itpcol.eq.0)            &
+        .or.ippmod(icompf).eq.3) then
       ! Save the latest density field seen by the mass equation
       ! it will be updated in the correction step.
-      call field_get_id("density_mass", f_id)
-      call field_get_val_s(f_id, cpro_rho_mass)
-      call field_get_id("boundary_density_mass", f_id)
-      call field_get_val_s(f_id, bpro_rho_mass)
-
       call field_get_val_s(icrom, crom)
       do iel = 1, ncelet
         cpro_rho_mass(iel) = crom(iel)
@@ -140,6 +178,7 @@ if (iappel.eq.1) then
       do ifac = 1, nfabor
         bpro_rho_mass(ifac) = brom(ifac)
       enddo
+
     endif
   endif
 
@@ -331,13 +370,24 @@ elseif (iappel.eq.3) then
 !     - a toutes les iterations si ISTMPF.NE.0
 !     - a toutes les iterations sauf la derniere si ISTMPF.EQ.0
 
-  if (istmpf.eq.0) then
-    call field_get_key_int(ivarfl(iu), kimasf, iflmas)
-    call field_get_key_int(ivarfl(iu), kbmasf, iflmab)
-    call field_get_val_s(iflmas, i_mass_flux)
-    call field_get_val_s(iflmab, b_mass_flux)
-    call field_get_val_prev_s(iflmas, i_mass_flux_prev)
-    call field_get_val_prev_s(iflmab, b_mass_flux_prev)
+  call field_get_key_int(ivarfl(iu), kimasf, iflmas)
+  call field_get_key_int(ivarfl(iu), kbmasf, iflmab)
+  call field_get_val_s(iflmas, i_mass_flux)
+  call field_get_val_s(iflmab, b_mass_flux)
+  call field_get_val_prev_s(iflmas, i_mass_flux_prev)
+  call field_get_val_prev_s(iflmab, b_mass_flux_prev)
+
+  if (istmpf.eq.2.and.itpcol.eq.1) then
+    theta  = 0.5d0
+    aa = 1.d0/(2.d0-theta)
+    bb = (1.d0-theta)/(2.d0-theta)
+    do ifac = 1 , nfac
+      i_mass_flux(ifac) = aa * i_mass_flux(ifac) + bb * i_mass_flux_prev(ifac)
+    enddo
+    do ifac = 1 , nfabor
+      b_mass_flux(ifac) = aa * b_mass_flux(ifac) + bb * b_mass_flux_prev(ifac)
+    enddo
+  else if (istmpf.eq.0) then
     do ifac = 1 , nfac
       i_mass_flux(ifac) = i_mass_flux_prev(ifac)
     enddo

commit 858a1f0799fe3ec9d53e9f3a87aa38f5f0393ae0

Fix bug introduced in commit 9a9fe137 (is_buoyant > 0, irovar = 0)

src/base/tridim.f90
@@ -1139,17 +1139,20 @@ do while (iterns.le.nterup)
       ! Correct the scalar to ensure scalar conservation
       call field_get_key_id("is_buoyant", key_buoyant_id)
       call field_get_val_s(icrom,crom)
-      call field_get_val_s_by_name("density_mass",cpro_rho_mass)
-      do iscal = 1, nscal
-        ivar = isca(iscal)
-        call field_get_key_int(ivarfl(ivar), key_buoyant_id, is_buoyant_fld)
-        if (is_buoyant_fld.eq.1) then
-          call field_get_val_s(ivarfl(ivar),cvar_sca)
-          do iel = 1, ncel
-            cvar_sca(iel) = cvar_sca(iel)*cpro_rho_mass(iel)/crom(iel)
-          enddo
-        endif
-      enddo
+      call field_get_id_try("density_mass",f_id)
+      if (f_id.ge.0) then
+        call field_get_val_s(f_id, cpro_rho_mass)
+        do iscal = 1, nscal
+          ivar = isca(iscal)
+          call field_get_key_int(ivarfl(ivar), key_buoyant_id, is_buoyant_fld)
+          if (is_buoyant_fld.eq.1) then
+            call field_get_val_s(ivarfl(ivar),cvar_sca)
+            do iel = 1, ncel
+              cvar_sca(iel) = cvar_sca(iel)*cpro_rho_mass(iel)/crom(iel)
+            enddo
+          endif
+        enddo
+      endif
     endif
 
     if (vcopt_u%iwarni.ge.1) then