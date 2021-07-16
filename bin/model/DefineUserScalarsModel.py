code-saturne/code_saturne

commit c66721837fda66840c50aecf667b5a77418cf98d

Major commit : Change handling of physical properties and internal coupling
  * Physical properties are now defined as a volumic condition, with
    the zone "all_cells" as the default one.
  * User-laws for physical properties can now be defined over multiple
    zones using MEG mechanism. But require definition of a
    variable property over the main zone ("all_cells")
  * A zone can now be defined as solid, which then allows activating the
    internal coupling functions for a given set of scalars (see the
    "Coupling parameters" page).
  * The internal coupling function is now available in the GUI
  * Cherry-picked from master branch based on following commits:
    - a2df46397c866714d62326a42d85c3acf0b4e676
    - b6f7429311edb0bcf70827fb9d8c75ef51b66ab1
    - 78870cb09e563b1c9e1b9cfcdaf33ade30b4c14d
    - a1b53e3187e9ed0df026bc741ea64bfd8bfd909a
    - 04e87e0e0b8ded4c403b9626c1039c7093aa9f8d

bin/model/DefineUserScalarsModel.py
@@ -638,7 +638,7 @@ class DefineUserScalarsModel(Variables, Model):
 
 
     @Variables.noUndo
-    def getDiffFormula(self, scalar):
+    def getDiffFormula(self, scalar, zone="all_cells"):
         """
         Return a formula for I{tag} 'density', 'molecular_viscosity',
         'specific_heat' or 'thermal_conductivity'
@@ -647,6 +647,14 @@ class DefineUserScalarsModel(Variables, Model):
         self.isInList(scalar, self.getUserScalarNameList())
         n = self.scalar_node.xmlGetNode('variable', name = scalar)
         node = n.xmlGetNode('property')
+
+        if zone != "all_cells":
+            if node.xmlGetChildNode("zone", name=zone):
+                node = node.xmlGetChildNode("zone", name=zone)
+            else:
+                node = node.xmlInitChildNode("zone")
+                node["name"] = zone
+
         formula = node.xmlGetString('formula')
         if not formula:
             formula = self.getDefaultFormula(scalar)
@@ -670,7 +678,7 @@ class DefineUserScalarsModel(Variables, Model):
 
 
     @Variables.undoLocal
-    def setDiffFormula(self, scalar, str):
+    def setDiffFormula(self, scalar, formula, zone="all_cells"):
         """
         Gives a formula for 'density', 'molecular_viscosity',
         'specific_heat'or 'thermal_conductivity'
@@ -679,7 +687,13 @@ class DefineUserScalarsModel(Variables, Model):
         self.isInList(scalar, self.getUserScalarNameList())
         n = self.scalar_node.xmlGetNode('variable', name = scalar)
         node = n.xmlGetNode('property')
-        node.xmlSetData('formula', str)
+        if zone != "all_cells":
+            if node.xmlGetChildNode("zone", name=zone):
+                node = node.xmlGetChildNode("zone", name=zone)
+            else:
+                node = node.xmlInitChildNode("zone")
+                node["name"] = zone
+        node.xmlSetData('formula', formula)
 
 
     @Variables.undoGlobal

Makefile.am
@@ -331,6 +331,7 @@ bin/model/HeadLossesModel.py \
 bin/model/IdentityAndPathesModel.py \
 bin/model/ImmersedBoundariesModel.py \
 bin/model/InitializationModel.py \
+bin/model/InternalCouplingModel.py \
 bin/model/__init__.py \
 bin/model/InterfacialAreaModel.py \
 bin/model/InterfacialEnthalpyModel.py \

bin/cs_meg_to_c.py
@@ -1241,6 +1241,9 @@ class meg_to_c_interpreter:
 
         from code_saturne.model.LocalizationModel import LocalizationModel
         from code_saturne.model.GroundwaterLawModel import GroundwaterLawModel
+
+        vlm = LocalizationModel('VolumicZone', self.case)
+
         if self.module_name == 'code_saturne':
             from code_saturne.model.FluidCharacteristicsModel \
                 import FluidCharacteristicsModel
@@ -1251,6 +1254,15 @@ class meg_to_c_interpreter:
                     exp, req, sca, sym = fcm.getFormulaComponents(fk)
                     self.init_block('vol', 'all_cells', fk,
                                     exp, req, sym, sca)
+                    for zone in vlm.getZones():
+                        zname = zone.getLabel()
+                        z_id  = str(zone.getCodeNumber())
+                        if zname != "all_cells" and \
+                                zone.isNatureActivated('physical_properties'):
+                            exp, req, sca, sym = \
+                            fcm.getFormulaComponents(fk, zone=z_id)
+                            self.init_block('vol', zname, fk,
+                                            exp, req, sym, sca)
 
             slist = fcm.m_sca.getUserScalarNameList()
             for s in fcm.m_sca.getScalarsVarianceList():
@@ -1261,9 +1273,19 @@ class meg_to_c_interpreter:
                     if diff_choice == 'user_law':
                         dname = fcm.m_sca.getScalarDiffusivityName(s)
                         exp, req, sca, sym, = \
-                        fcm.getFormulaComponents('scalar_diffusivity', s)
+                        fcm.getFormulaComponents('scalar_diffusivity',
+                                                 scalar=s)
                         self.init_block('vol', 'all_cells', dname,
                                         exp, req, sym, sca)
+                        for zone in vlm.getZones():
+                            zname = zone.getLabel()
+                            z_id = str(zone.getCodeNumber())
+                            if zname != "all_cells" and \
+                            zone.isNatureActivated('physical_properties'):
+                                exp, req, sca, sym = \
+                                fcm.getFormulaComponents('scalar_diffusivity', scalar=s, zone=z_id)
+                                self.init_block('vol', zname, dname,
+                                                exp, req, sym, sca)
 
             # ALE mesh viscosity
             from code_saturne.model.MobileMeshModel import MobileMeshModel
@@ -1274,7 +1296,6 @@ class meg_to_c_interpreter:
                                 exp, req, sym, sca)
 
             # GroundWater Flows Law
-            vlm = LocalizationModel('VolumicZone', self.case)
             glm = None
 
             for zone in vlm.getZones():
@@ -1329,33 +1350,51 @@ class meg_to_c_interpreter:
                         for fk in authorized_fields:
                             if tm.getPropertyMode(fieldId, fk) == 'user_law':
                                 name = fk + '_' + str(fieldId)
-                                exp, req, sca, sym = tm.getFormulaComponents(fieldId,fk)
-                                self.init_block('vol', 'all_cells', name,
-                                                exp, req, sym, sca)
+                                for zone in vlm.getZones():
+                                    zname = zone.getLabel()
+                                    z_id = str(zone.getCodeNumber())
+                                    if zone.isNatureActivated('physical_properties'):
+                                        exp, req, sca, sym = tm.getFormulaComponents(fieldId,fk,zone=z_id)
+                                        self.init_block('vol', zname, name,
+                                                        exp, req, sym, sca)
 
                         if mfm.getCompressibleStatus(fieldId) == 'on':
                             for fk in compressible_fields:
                                 name = fk + '_' + str(fieldId)
-                                exp, req, sca, sym = tm.getFormulaComponents(fieldId,fk)
-                                self.init_block('vol', 'all_cells', name,
-                                                exp, req, sym, sca)
+                                for zone in vlm.getZones():
+                                    zname = zone.getLabel()
+                                    z_id = str(zone.getCodeNumber())
+                                    if zone.isNatureActivated('physical_properties'):
+                                        exp, req, sca, sym = tm.getFormulaComponents(fieldId,fk,zone=z_id)
+                                        self.init_block('vol', zname, name,
+                                                        exp, req, sym, sca)
 
                         # Temperature as a function of enthalpy
                         if mfm.getEnergyResolution(fieldId) == 'on':
                             name = 'temperature_' + str(fieldId)
-                            exp, req, sca, sym = tm.getFormulaComponents(fieldId,
-                                                                        'temperature')
-                            self.init_block('vol', 'all_cells', name,
-                                            exp, req, sym, sca)
+                            for zone in vlm.getZones():
+                                zname = zone.getLabel()
+                                z_id = str(zone.getCodeNumber())
+                                if zone.isNatureActivated('physical_properties'):
+                                    exp, req, sca, sym = \
+                                        tm.getFormulaComponents(fieldId,
+                                                                'temperature',
+                                                                zone=z_id)
+                                    self.init_block('vol', zname, name,
+                                                    exp, req, sym, sca)
 
             # User properties for Water/Steam kind flows
             if mfm.getPredefinedFlow() != 'None' and \
                mfm.getPredefinedFlow() != "particles_flow":
                 if user_gas_liq_fields:
                     for fk in gas_liq_fields:
-                        exp, req, sca, sym = tm.getFormulaComponents('none', fk)
-                        self.init_block('vol', 'all_cells', fk,
-                                        exp, req, sym, sca)
+                        for zone in vlm.getZones():
+                            zname = zone.getLabel()
+                            z_id = str(zone.getCodeNumber())
+                            if zone.isNatureActivated('physical_properties'):
+                                exp, req, sca, sym = tm.getFormulaComponents('none', fk, zone=z_id)
+                                self.init_block('vol', zname, fk,
+                                                exp, req, sym, sca)
 
 
         # Porosity for both solvers

bin/model/FluidCharacteristicsModel.py
@@ -823,7 +823,7 @@ class FluidCharacteristicsModel(Variables, Model):
 
 
     @Variables.noUndo
-    def getFormula(self, tag):
+    def getFormula(self, tag, zone="1"):
         """
         Return a formula for I{tag} 'density', 'molecular_viscosity',
         'specific_heat' or 'thermal_conductivity'
@@ -832,10 +832,15 @@ class FluidCharacteristicsModel(Variables, Model):
                             'specific_heat', 'thermal_conductivity',
                             'volume_viscosity'))
         node = self.node_fluid.xmlGetNode('property', name=tag)
-        formula = node.xmlGetString('formula')
+        if zone != "1":
+            if node.xmlGetChildNode("zone", zone_id=zone):
+                node = node.xmlGetChildNode("zone", zone_id=zone)
+            else:
+                node = node.xmlInitChildNode("zone", zone_id=zone)
+        formula = node.xmlGetChildString('formula')
         if not formula:
             formula = self.getDefaultFormula(tag)
-            self.setFormula(tag, formula)
+            self.setFormula(tag, formula, zone)
         return formula
 
 
@@ -860,7 +865,7 @@ class FluidCharacteristicsModel(Variables, Model):
 
 
     @Variables.undoLocal
-    def setFormula(self, tag, str):
+    def setFormula(self, tag, formula, zone="1"):
         """
         Gives a formula for 'density', 'molecular_viscosity',
         'specific_heat'or 'thermal_conductivity'
@@ -869,7 +874,12 @@ class FluidCharacteristicsModel(Variables, Model):
                             'specific_heat', 'thermal_conductivity',
                             'volume_viscosity'))
         node = self.node_fluid.xmlGetNode('property', name=tag)
-        node.xmlSetData('formula', str)
+        if zone != "1":
+            if node.xmlGetChildNode("zone", zone_id=zone):
+                node = node.xmlGetChildNode("zone", zone_id=zone)
+            else:
+                node = node.xmlInitChildNode("zone", zone_id=zone)
+        node.xmlSetData('formula', formula)
 
 
     @Variables.noUndo
@@ -909,25 +919,25 @@ class FluidCharacteristicsModel(Variables, Model):
                 node.xmlRemoveChild('postprocessing_recording')
 
     # MEG Generation related functions
-    def getFormulaComponents(self, tag, scalar=None):
+    def getFormulaComponents(self, tag, scalar=None, zone="1"):
         """
         Get the formula components for a given tag
         """
 
         if tag == 'density':
-            return self.getFormulaRhoComponents()
+            return self.getFormulaRhoComponents(zone)
 
         elif tag == 'molecular_viscosity':
-            return self.getFormulaMuComponents()
+            return self.getFormulaMuComponents(zone)
 
         elif tag == 'specific_heat':
-            return self.getFormulaCpComponents()
+            return self.getFormulaCpComponents(zone)
 
         elif tag == 'thermal_conductivity':
-            return self.getFormulaAlComponents()
+            return self.getFormulaAlComponents(zone)
 
         elif tag == 'volume_viscosity':
-            return self.getFormulaViscv0Components()
+            return self.getFormulaViscv0Components(zone)
 
         elif tag == 'scalar_diffusivity' and scalar != None:
             return self.getFormulaDiffComponents(scalar)
@@ -937,12 +947,12 @@ class FluidCharacteristicsModel(Variables, Model):
             raise Exception(msg)
 
 
-    def getFormulaRhoComponents(self):
+    def getFormulaRhoComponents(self, zone="1"):
         """
         User formula for density
         """
 
-        exp = self.getFormula('density')
+        exp = self.getFormula('density', zone)
         req = [('density', 'Density')]
 
         symbols = []
@@ -963,12 +973,12 @@ class FluidCharacteristicsModel(Variables, Model):
         return exp, req, self.list_scalars, symbols;
 
 
-    def getFormulaMuComponents(self):
+    def getFormulaMuComponents(self, zone="1"):
         """
         User formula for molecular viscosity
         """
 
-        exp = self.getFormula('molecular_viscosity')
+        exp = self.getFormula('molecular_viscosity', zone)
         req = [('molecular_viscosity', 'Molecular Viscosity')]
 
         symbols = []
@@ -1000,11 +1010,11 @@ class FluidCharacteristicsModel(Variables, Model):
         return exp, req, self.list_scalars, symbols;
 
 
-    def getFormulaCpComponents(self):
+    def getFormulaCpComponents(self, zone="1"):
         """
         User formula for specific heat
         """
-        exp = self.getFormula('specific_heat')
+        exp = self.getFormula('specific_heat', zone)
         req = [('specific_heat', 'Specific heat')]
 
         symbols = []
@@ -1025,11 +1035,11 @@ class FluidCharacteristicsModel(Variables, Model):
         return exp, req, self.list_scalars, symbols;
 
 
-    def getFormulaViscv0Components(self):
+    def getFormulaViscv0Components(self, zone="1"):
         """
         User formula for volumic viscosity
         """
-        exp = self.getFormula('volume_viscosity')
+        exp = self.getFormula('volume_viscosity', zone)
         req = [('volume_viscosity', 'Volume viscosity')]
 
         symbols = []
@@ -1052,11 +1062,11 @@ class FluidCharacteristicsModel(Variables, Model):
         return exp, req, self.list_scalars, symbols;
 
 
-    def getFormulaAlComponents(self):
+    def getFormulaAlComponents(self, zone="1"):
         """
         User formula for thermal conductivity
         """
-        exp = self.getFormula('thermal_conductivity')
+        exp = self.getFormula('thermal_conductivity', zone)
         req = [('thermal_conductivity', 'Thermal conductivity')]
 
         symbols = []
@@ -1076,7 +1086,7 @@ class FluidCharacteristicsModel(Variables, Model):
         return exp, req, self.list_scalars, symbols;
 
 
-    def getFormulaDiffComponents(self, scalar):
+    def getFormulaDiffComponents(self, scalar, zone="1"):
         """
         User formula for the diffusion coefficient
         """

bin/model/InternalCouplingModel.py
@@ -1,0 +1,146 @@
+# -*- coding: utf-8 -*-
+
+#-------------------------------------------------------------------------------
+
+# This file is part of Code_Saturne, a general-purpose CFD tool.
+#
+# Copyright (C) 1998-2021 EDF S.A.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+# Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+#-------------------------------------------------------------------------------
+
+"""
+This modules defines properties for internal coupling.
+"""
+
+#-------------------------------------------------------------------------------
+# Library modules import
+#-------------------------------------------------------------------------------
+
+import os, sys, unittest
+
+#-------------------------------------------------------------------------------
+# Application modules import
+#-------------------------------------------------------------------------------
+
+
+from code_saturne.model.Common import *
+from code_saturne.model.XMLvariables import Variables, Model
+from code_saturne.model.XMLmodel import XMLmodel, ModelTest
+
+from code_saturne.model.ThermalScalarModel import ThermalScalarModel
+from code_saturne.model.DefineUserScalarsModel import DefineUserScalarsModel
+from code_saturne.model.NotebookModel import NotebookModel
+
+class InternalCouplingModel(Variables, Model):
+    """
+    Class to manipulate internal coupling parameters in xml file.
+    """
+
+    def __init__(self, case):
+        """Constructor"""
+        self.case = case
+
+        n = self.case.xmlGetNode("thermophysical_models")
+        self.node = n.xmlGetChildNode("internal_coupling")
+        if not self.node:
+            self.node = n.xmlInitChildNode("internal_coupling")
+
+        self.node_zones = self.node.xmlInitChildNode("solid_zones")
+        self.node_scals = self.node.xmlInitChildNode("coupled_scalars")
+
+
+    def addZone(self, zone_name):
+        """Add a coupled zone"""
+
+        self.node_zones.xmlInitChildNode("zone", name=zone_name)
+
+
+    def removeZone(self, zone_name):
+        """Remove a zone from the coupled list"""
+
+        n = self.node_zones.xmlGetChildNode("zone", name=zone_name)
+        if n:
+            n.xmlRemoveNode()
+
+
+    def getZonesList(self):
+        """Get list of coupled zones."""
+
+        lst = []
+        for n in self.node_zones.xmlGetChildNodeList("zone"):
+            lst.append(n['name'])
+
+        return lst
+
+
+    def isZoneCoupled(self, zone_name):
+        """Check if a zone is coupled or not."""
+
+        retval = False
+        if zone_name in self.getZonesList():
+            retval = True
+
+        return retval
+
+
+    def addScalar(self, scalar_name):
+        """Add a coupled scalar"""
+
+        self.node_scals.xmlInitChildNode("scalar", name=scalar_name)
+
+
+    def removeScalar(self, scalar_name):
+        """Remove a scalar from the coupled list"""
+        n = self.node_scals.xmlGetChildNode("scalar", name=scalar_name)
+        if n:
+            n.xmlRemoveNode()
+
+
+    def getScalarsList(self):
+        """Get list of coupled scalars."""
+
+        lst = []
+        for n in self.node_scals.xmlGetChildNodeList("scalar"):
+            lst.append(n['name'])
+
+        return lst
+
+
+    def isScalarCoupled(self, scalar_name):
+        """Check if a scalar is coupled."""
+
+        retval = False
+        if scalar_name in self.getScalarsList():
+            retval = True
+
+        return retval
+
+    def getListOfAvailableScalars(self):
+        """Get list of scalars which may be coupled"""
+
+        lst = []
+
+        thm = ThermalScalarModel(self.case)
+        thermal = thm.getThermalScalarName()
+        if thermal and thermal != "":
+            lst.append(thermal)
+
+        scm = DefineUserScalarsModel(self.case)
+        for scalar in scm.getUserScalarNameList():
+            lst.append(scalar)
+
+        return lst

bin/model/LocalizationModel.py
@@ -94,7 +94,7 @@ class Zone(object):
         else:
             self._localization = self.defaultValues()['localization']
         if nature:
-            if typeZone == 'VolumicZone' and type(nature) == bytes:
+            if typeZone == 'VolumicZone' and type(nature) == str:
                 self._nature = self.defaultValues()['nature'].copy()
                 self._nature[nature] = "on"
             else:
@@ -239,7 +239,10 @@ class VolumicZone(Zone):
         self._natureDict = {}
         self._natureDict['initialization']       = self.tr("Initialization")
 
+        self._natureDict['physical_properties'] = self.tr("Physical properties")
+        self._natureList.append('physical_properties')
         if self.case.module_name() == 'code_saturne':
+
             from code_saturne.model.GroundwaterModel import GroundwaterModel
             if GroundwaterModel(self.case).getGroundwaterModel() != "groundwater":
                 self._natureList.append('head_losses')
@@ -299,6 +302,7 @@ class VolumicZone(Zone):
         dico['nature']['thermal_source_term'] = "off"
         dico['nature']['scalar_source_term'] = "off"
         dico['nature']['groundwater_law'] = "off"
+        dico['nature']['physical_properties'] = "off"
         return dico
 
     def isNatureActivated(self, text):

commit 6bdbf31a4fa40f9c4d9e4eb0d1e86b04b314e247

[GUI] Fix crash due to wrong handling of zone names for scalar diffusivity
  * Bug introduced in commit c66721837f (2021-01-28)
  * closes #317

bin/model/DefineUserScalarsModel.py
@@ -78,7 +78,7 @@ class DefineUserScalarsModel(Variables, Model):
         default['coefficient_label']     = "Dscal"
         default['diffusion_coefficient'] = 1.83e-05
         default['diffusion_choice']      = 'constant'
-        default['zone_id']               = 1
+        default['zone_id']               = "1"
         from code_saturne.model.GroundwaterModel import GroundwaterModel
         if GroundwaterModel(self.case).getGroundwaterModel() == "groundwater":
             default['GGDH']                  = "OFF"
@@ -638,7 +638,7 @@ class DefineUserScalarsModel(Variables, Model):
 
 
     @Variables.noUndo
-    def getDiffFormula(self, scalar, zone="all_cells"):
+    def getDiffFormula(self, scalar, zone="1"):
         """
         Return a formula for I{tag} 'density', 'molecular_viscosity',
         'specific_heat' or 'thermal_conductivity'
@@ -648,14 +648,14 @@ class DefineUserScalarsModel(Variables, Model):
         n = self.scalar_node.xmlGetNode('variable', name = scalar)
         node = n.xmlGetNode('property')
 
-        if zone != "all_cells":
+        if zone != "1":
             if node.xmlGetChildNode("zone", name=zone):
                 node = node.xmlGetChildNode("zone", name=zone)
             else:
                 node = node.xmlInitChildNode("zone")
                 node["name"] = zone
 
-        formula = node.xmlGetString('formula')
+        formula = node.xmlGetChildString('formula')
         if not formula:
             formula = self.getDefaultFormula(scalar)
             self.setDiffFormula(scalar, formula)
@@ -678,7 +678,7 @@ class DefineUserScalarsModel(Variables, Model):
 
 
     @Variables.undoLocal
-    def setDiffFormula(self, scalar, formula, zone="all_cells"):
+    def setDiffFormula(self, scalar, formula, zone="1"):
         """
         Gives a formula for 'density', 'molecular_viscosity',
         'specific_heat'or 'thermal_conductivity'
@@ -687,7 +687,7 @@ class DefineUserScalarsModel(Variables, Model):
         self.isInList(scalar, self.getUserScalarNameList())
         n = self.scalar_node.xmlGetNode('variable', name = scalar)
         node = n.xmlGetNode('property')
-        if zone != "all_cells":
+        if zone != "1":
             if node.xmlGetChildNode("zone", name=zone):
                 node = node.xmlGetChildNode("zone", name=zone)
             else: