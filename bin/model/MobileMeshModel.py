code-saturne/code_saturne

commit aaf04fa1aafdfe5a4857c3a34ca59624eb6b0a51

[GUI] ALE Mesh viscosity defined in GUI is now computed with MEG
  * The variable name is changed in the GUI equation:
    - mesh_viscosity for scalar case
    - mesh_viscosity[X]/[Y]/[Z] for the three components in vector case
  * Backward compatibiliy is handled in the GUI
  * Some refactoring in MobileMeshModel.py

bin/model/MobileMeshModel.py
@@ -46,6 +46,7 @@ from code_saturne.model.XMLmodel import ModelTest
 from code_saturne.model.OutputControlModel import OutputControlModel
 from code_saturne.model.Boundary import Boundary
 from code_saturne.model.LocalizationModel import LocalizationModel
+from code_saturne.model.NotebookModel import NotebookModel
 
 #-------------------------------------------------------------------------------
 # Mobil Mesh model class
@@ -61,6 +62,9 @@ class MobileMeshModel(Model):
         """
         self.case = case
 
+        # Notebook
+        self.notebook = NotebookModel(self.case)
+
 
     def __defaultInitialValues(self):
         """
@@ -69,11 +73,31 @@ class MobileMeshModel(Model):
         default = {}
         default['nalinf' ]  = 0
         default['iortvm' ]  = 'isotrop'
-        default['formula_isotrop']  = 'mesh_viscosity_1 = 1;'
-        default['formula_orthotrop'] = 'mesh_viscosity_1 = 1;\nmesh_viscosity_2 = 1;\nmesh_viscosity_3 = 1;'
+        default['formula_isotrop']  = 'mesh_viscosity = 1;'
+        default['formula_orthotrop'] = 'mesh_viscosity[X] = 1;\nmesh_viscosity[Y] = 1;\nmesh_viscosity[Z] = 1;'
         default['ale_method']  = 'off'
 
         return default
+
+    def __getViscosityComponents(self, visc_type=None):
+        """
+        Return a dictionnary which contains the viscosity components
+        with name and labels.
+        """
+
+        viscosity_type = visc_type
+        if viscosity_type == None:
+            viscosity_type = self.getViscosity()
+
+        d = []
+        if viscosity_type == 'isotrop':
+            d.append({'name':'mesh_viscosity', 'label':'mesh_vi1'})
+        elif viscosity_type == 'orthotrop':
+            d.append({'name':'mesh_viscosity[X]', 'label':'mesh_vi1'})
+            d.append({'name':'mesh_viscosity[Y]', 'label':'mesh_vi2'})
+            d.append({'name':'mesh_viscosity[Z]', 'label':'mesh_vi3'})
+
+        return d
 
 
     def __getMobileMeshNode(self):
@@ -120,38 +144,40 @@ class MobileMeshModel(Model):
         """
         node_ale = self.__getMobileMeshNode()
         node_ale.xmlInitChildNode('variable', name='mesh_velocity', label='Mesh Velocity', dimension=3)
-        node_ale.xmlInitChildNode('property', name='mesh_viscosity_1', label='mesh_vi1')
+        mvc = self.__getViscosityComponents()
+        for d in mvc:
+            node_ale.xmlInitChildNode('property', name=d['name'], label=d['label'])
 
-        # find node for property / choice and set to default value if require
-        node = node_ale.xmlGetChildNode('property', name='mesh_viscosity_1')
+#        # find node for property / choice and set to default value if require
+#        node = node_ale.xmlGetChildNode('property', name='mesh_viscosity_1')
 
         self.__updateNodeViscosity()
 
 
-    def __updateNodeViscosity(self):
+    def __updateNodeViscosity(self, previous_visc = None):
         """
         Update properties beyond mesh visosity is isotrope or not.
+        Only used if previous viscosity type is modified.
         """
         node_ale = self.__getMobileMeshNode()
-        if self.getViscosity() == 'orthotrop':
-            node_ale.xmlInitChildNode('property', name='mesh_viscosity_2', label='mesh_vi2')
-            node_ale.xmlInitChildNode('property', name='mesh_viscosity_3', label='mesh_vi3')
+        if previous_visc:
+            dp = self.__getViscosityComponents(previous_visc)
+            d  = self.__getViscosityComponents()
 
-            # find choice for mesh_viscosity_1, create default value if require
-            node = node_ale.xmlGetChildNode('property', name='mesh_viscosity_1')
+            for e in dp:
+                n = node_ale.xmlGetChildNode('property', name=e['name'])
+                if n:
+                    n.xmlRemoveNode()
 
-            # Syncrhonize other properties
-            for n in ('mesh_viscosity_2', 'mesh_viscosity_3'):
-                node = node_ale.xmlGetChildNode('property', name=n)
+            for e in d:
+                node_ale.xmlInitChildNode('property',
+                                          name  = e['name'],
+                                          label = e['label'])
+                node = node_ale.xmlGetChildNode('property', name=e['name'])
 
-        else:
-            node1 = node_ale.xmlGetChildNode('property', name='mesh_viscosity_2')
-            node2 = node_ale.xmlGetChildNode('property', name='mesh_viscosity_3')
-
-            if node1:
-                node1.xmlRemoveNode()
-            if node2:
-                node2.xmlRemoveNode()
+            # Update formula
+            fd = self.getDefaultFormula()
+            self.setFormula(fd)
 
 
     def __removeVariablesandProperties(self):
@@ -241,10 +267,13 @@ class MobileMeshModel(Model):
         Set value of mesh viscosity into xml file.
         """
         self.isInList(value, ['isotrop', 'orthotrop'])
+        prev_val = None
+        if value != self.getViscosity():
+            prev_val = self.getViscosity()
         node_ale = self.__getMobileMeshNode()
         node = node_ale.xmlInitChildNode('mesh_viscosity')
         node['type'] = value
-        self.__updateNodeViscosity()
+        self.__updateNodeViscosity(prev_val)
 
 
     @Variables.noUndo
@@ -284,6 +313,32 @@ class MobileMeshModel(Model):
             formula = self.getDefaultFormula()
         return formula
 
+
+    @Variables.noUndo
+    def getFormulaViscComponents(self):
+        """
+        Get components of the mesh viscosity formula.
+        """
+
+        exp = self.getFormula()
+
+        req = []
+        mvc = self.__getViscosityComponents()
+        for d in mvc:
+            req.append((d['name'], d['name']))
+
+        symbols = [('x', "X cell's gravity center"),
+                   ('y', "Y cell's gravity center"),
+                   ('z', "Z cell's gravity center"),
+                   ('dt', 'time step'),
+                   ('t', 'current time'),
+                   ('iter', 'number of iteration')]
+
+        for (nme, val) in self.notebook.getNotebookList():
+            symbols.append((nme, 'value (notebook) = ' + str(val)))
+
+
+        return exp, req, [], symbols
 
     @Variables.noUndo
     def getDefaultFormula(self):

bin/model/XMLinitialize.py
@@ -1853,6 +1853,24 @@ class XMLinit(BaseXmlInit):
                 if node['id'] == None:
                     node['id'] = node['name']
 
+        # Rename mesh viscosity for meg calls
+        ale_replace_dict = {'isotrop':{'mesh_viscosity_1':'mesh_viscosity'},
+                            'orthotrop':{'mesh_viscosity_1':'mesh_viscosity[X]',
+                                         'mesh_viscosity_2':'mesh_viscosity[Y]',
+                                         'mesh_viscosity_3':'mesh_viscosity[Z]'}}
+
+        XMLAleMethod = XMLThermoPhysicalNode.xmlGetNode("ale_method")
+        if XMLAleMethod != None and XMLAleMethod['status'] == 'on':
+            nat = XMLAleMethod.xmlGetNode('mesh_viscosity')
+            ale_type = nat['type']
+            d = ale_replace_dict[ale_type]
+            nf = XMLAleMethod.xmlGetNode('formula')
+            if nf != None:
+                f = XMLAleMethod.xmlGetString("formula")
+                for k in d.keys():
+                    f = f.replace(k, d[k])
+
+                nf.xmlSetTextNode(f)
 
 #-------------------------------------------------------------------------------
 # End of XMLinit

bin/cs_math_parser.py
@@ -849,7 +849,7 @@ class cs_math_parser:
                     # global coordinates pointer
                     if tk in ['x', 'y', 'z'] and 'xyz' not in known_symbols:
                         known_symbols.append('xyz')
-                        usr_defs.append(glob_tokens['xyz'])
+                        usr_defs.append(glob_tokens['xyz']+'\n')
 
         if len(usr_defs) > 0:
             usr_defs.append('\n')

bin/cs_mei_to_c.py
@@ -894,6 +894,14 @@ class mei_to_c_interpreter:
                         self.init_block('vol', 'all_cells', dname,
                                         exp, req, sym, sca)
 
+            # ALE mesh viscosity
+            from code_saturne.model.MobileMeshModel import MobileMeshModel
+            ale_model = MobileMeshModel(self.case)
+            exp, req, sca, sym = ale_model.getFormulaViscComponents()
+            self.init_block('vol', 'all_cells', 'mesh_viscosity',
+                            exp, req, sym, sca)
+
+
             # GroundWater Flows Law
             vlm = LocalizationModel('VolumicZone', self.case)
             glm = None

gui/Pages/MobileMeshView.py
@@ -52,7 +52,7 @@ from code_saturne.Pages.MobileMeshForm  import Ui_MobileMeshForm
 from code_saturne.Base.QtPage    import IntValidator,  ComboModel, from_qvariant
 from code_saturne.model.MobileMeshModel import MobileMeshModel
 
-from code_saturne.Pages.QMeiEditorView import QMeiEditorView
+from code_saturne.Pages.QMegEditorView import QMegEditorView
 from code_saturne.model.NotebookModel import NotebookModel
 
 #-------------------------------------------------------------------------------
@@ -86,8 +86,8 @@ xcen = 5.0;
 ycen = 0.;
 zcen = 6.0;
 xray2 = (x-xcen)^2 + (y-ycen)^2 + (z-zcen)^2;
-mesh_viscosity_1 = 1;
-if (xray2 < xr2) mesh_viscosity_1 = 1e10;
+mesh_viscosity = 1;
+if (xray2 < xr2) mesh_viscosity = 1e10;
 """
 
     viscosity_ortho = """# Viscosity of the mesh allows to control the deformation
@@ -105,13 +105,13 @@ xcen = 5.0;
 ycen = 0.;
 zcen = 6.0;
 xray2 = (x-xcen)^2 + (y-ycen)^2 + (z-zcen)^2;
-mesh_viscosity_1 = 1;
-mesh_viscosity_2 = 1;
-mesh_viscosity_3 = 1;
+mesh_viscosity[X] = 1;
+mesh_viscosity[Y] = 1;
+mesh_viscosity[Z] = 1;
 if (xray2 < xr2) {
-    mesh_viscosity_1 = 1e10;
-    mesh_viscosity_2 = 1e10;
-    mesh_viscosity_3 = 1e10;
+    mesh_viscosity[X] = 1e10;
+    mesh_viscosity[Y] = 1e10;
+    mesh_viscosity[Z] = 1e10;
 }
 """
 
@@ -194,35 +194,22 @@ if (xray2 < xr2) {
         """
         exp = self.mdl.getFormula()
 
+        exp, req, sca, symb = self.mdl.getFormulaViscComponents()
+
         if self.mdl.getViscosity() == 'isotrop':
-            if not exp:
-                exp = "mesh_viscosity_1 = 1;"
-            req = [('mesh_viscosity_1', 'mesh viscosity')]
             exa = MobileMeshView.viscosity_iso
         else:
-            if not exp:
-                exp = "mesh_viscosity_1 = 1;\nmesh_viscosity_2 = 1;\nmesh_viscosity_3 = 1;"
-            req = [('mesh_viscosity_1', 'mesh viscosity X'),
-                   ('mesh_viscosity_2', 'mesh viscosity Y'),
-                   ('mesh_viscosity_3', 'mesh viscosity Z')]
             exa = MobileMeshView.viscosity_ortho
 
-        symb = [('x', "X cell's gravity center"),
-                ('y', "Y cell's gravity center"),
-                ('z', "Z cell's gravity center"),
-                ('dt', 'time step'),
-                ('t', 'current time'),
-                ('iter', 'number of iteration')]
-
-        for (nme, val) in self.notebook.getNotebookList():
-            symb.append((nme, 'value (notebook) = ' + str(val)))
-
-        dialog = QMeiEditorView(self,
-                                check_syntax = self.case['package'].get_check_syntax(),
-                                expression = exp,
-                                required   = req,
-                                symbols    = symb,
-                                examples   = exa)
+        dialog = QMegEditorView(parent = self,
+                                function_type = 'vol',
+                                zone_name     = "all_cells",
+                                variable_name = "mesh_viscosity",
+                                expression    = exp,
+                                required      = req,
+                                symbols       = symb,
+                                known_fields  = [],
+                                examples      = exa)
         if dialog.exec_():
             result = dialog.get_result()
             log.debug("slotFormulaMobileMeshView -> %s" % str(result))

src/gui/cs_gui_mobile_mesh.c
@@ -62,6 +62,7 @@
 #include "cs_prototypes.h"
 #include "cs_timer.h"
 #include "cs_time_step.h"
+#include "cs_volume_zone.h"
 
 /*----------------------------------------------------------------------------
  * Header for the current file
@@ -1036,44 +1037,14 @@ cs_gui_mesh_viscosity(void)
   if (mvisc_expr == NULL)
     return;
 
-  const cs_real_3_t *restrict cell_cen
-    = (const cs_real_3_t *restrict)cs_glob_mesh_quantities->cell_cen;
-  const cs_lnum_t n_cells   = cs_glob_mesh->n_cells;
-  const char * symbols[3]   = {"x", "y", "z" };
-  const char * variables[3] = {"mesh_viscosity_1",
-                               "mesh_viscosity_2",
-                               "mesh_viscosity_3" };
+  /* Compute mesh viscosity using the MEG function.
+   * The function accepts a field of arbitrary dimension, hence no need to
+   * check here. The formula is generated by the code.
+   */
+  const cs_zone_t *z_all = cs_volume_zone_by_name("all_cells");
+  cs_field_t *fmeg[1] = {CS_F_(vism)};
+  cs_meg_volume_function(z_all, fmeg);
 
-  int orthotropic = _ale_visc_type(tn0);
-
-  cs_lnum_t nd = (orthotropic) ? 3 : 1;
-
-  /* Init mei */
-  mei_tree_t *ev = _init_mei_tree(mvisc_expr, variables,
-                                  nd, symbols, 0, 3,
-                                  cs_glob_time_step->dt_ref,
-                                  cs_glob_time_step->t_cur,
-                                  cs_glob_time_step->nt_cur);
-
-  /* for each cell, update the value of the table of symbols for each scalar
-     (including the thermal scalar), and evaluate the interpreter */
-
-  for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++) {
-    /* insert symbols */
-    mei_tree_insert(ev, "x", cell_cen[c_id][0]);
-    mei_tree_insert(ev, "y", cell_cen[c_id][1]);
-    mei_tree_insert(ev, "z", cell_cen[c_id][2]);
-
-    mei_evaluate(ev);
-
-    /* Set mesh_u components */
-    CS_F_(vism)->val[nd*c_id] = mei_tree_lookup(ev, "mesh_viscosity_1");
-    if (orthotropic) {
-      CS_F_(vism)->val[nd*c_id + 1] = mei_tree_lookup(ev, "mesh_viscosity_2");
-      CS_F_(vism)->val[nd*c_id + 2] = mei_tree_lookup(ev, "mesh_viscosity_3");
-    }
-  }
-  mei_tree_destroy(ev);
 }
 
 /*----------------------------------------------------------------------------*/

commit 8cb3ba07b9b01da69868c9f0f52433740824b6b8

[ALE] auto update: fix bug introduced in commit aaf04fa (2020-03-06).

bin/model/MobileMeshModel.py
@@ -368,7 +368,7 @@ class MobileMeshTestCase(ModelTest):
         mdl.setMethod('on')
         doc = """<ale_method status="on">
                     <variable label="Mesh Velocity" name="mesh_velocity"/>
-                    <property label="mesh_vi1" name="mesh_viscosity_1"/>
+                    <property label="mesh_vi1" name="mesh_viscosity"/>
                     <mesh_viscosity type="isotrop"/>
                  </ale_method>"""
         assert mdl.node_ale == self.xmlNodeFromString(doc),\
@@ -384,7 +384,7 @@ class MobileMeshTestCase(ModelTest):
 ##
         doc = """<ale_method status="on">
                     <variable label="Mesh Velocity" name="mesh_velocity"/>
-                    <property label="mesh_vi1" name="mesh_viscosity_1"/>
+                    <property label="mesh_vi1" name="mesh_viscosity"/>
                     <mesh_viscosity type="isotrop"/>
                     <fluid_initialization_sub_iterations>12</fluid_initialization_sub_iterations>
                 </ale_method>"""
@@ -403,10 +403,10 @@ class MobileMeshTestCase(ModelTest):
 
         doc = """<ale_method status="on">
                     <variable label="Mesh Velocity" name="mesh_velocity"/>
-                    <property label="mesh_vi1" name="mesh_viscosity_1"/>
+                    <property label="mesh_vi1" name="mesh_viscosity[X]"/>
                     <mesh_viscosity type="orthotrop"/>
-                    <property label="mesh_vi2" name="mesh_viscosity_2"/>
-                    <property label="mesh_vi3" name="mesh_viscosity_3"/>
+                    <property label="mesh_vi2" name="mesh_viscosity[Y]"/>
+                    <property label="mesh_vi3" name="mesh_viscosity[Z]"/>
                 </ale_method>"""
         assert mdl.node_ale == self.xmlNodeFromString(doc),\
             'Could not set mobil mesh model visocity type'
@@ -418,17 +418,17 @@ class MobileMeshTestCase(ModelTest):
         """Check whether the MobileMeshModel class could be set and get formula"""
         mdl = MobileMeshModel(self.case)
         mdl.setMethod('on')
-        mdl.setFormula('mesh_viscosity_1 = 1000;')
+        mdl.setFormula('mesh_viscosity = 1000;')
 
         doc = """<ale_method status="on">
                     <variable label="Mesh Velocity" name="mesh_velocity"/>
-                    <property label="mesh_vi1" name="mesh_viscosity_1"/>
+                    <property label="mesh_vi1" name="mesh_viscosity"/>
                     <mesh_viscosity type="isotrop"/>
-                    <formula>mesh_viscosity_1 = 1000;</formula>
+                    <formula>mesh_viscosity = 1000;</formula>
                     </ale_method> """
         assert mdl.node_ale == self.xmlNodeFromString(doc),\
             'Could not set formula'
-        assert mdl.getFormula() == 'mesh_viscosity_1 = 1000;',\
+        assert mdl.getFormula() == 'mesh_viscosity = 1000;',\
             'Could not get formula'
 
 

bin/model/XMLinitialize.py
@@ -1888,17 +1888,28 @@ class XMLinit(BaseXmlInit):
 
         XMLAleMethod = XMLThermoPhysicalNode.xmlGetNode("ale_method")
         if XMLAleMethod != None and XMLAleMethod['status'] == 'on':
+
+            # If not mentionned it is isotrop
+            ale_type = 'isotrop'
             nat = XMLAleMethod.xmlGetNode('mesh_viscosity')
+
             if nat:
                 ale_type = nat['type']
-                d = ale_replace_dict[ale_type]
-                nf = XMLAleMethod.xmlGetNode('formula')
-                if nf != None:
-                    f = XMLAleMethod.xmlGetString("formula")
-                    for k in d.keys():
-                        f = f.replace(k, d[k])
 
-                    nf.xmlSetTextNode(f)
+            d = ale_replace_dict[ale_type]
+            # Rename properties
+            for k in d.keys():
+                node = XMLAleMethod.xmlGetNode('property', name=k)
+                if node:
+                    node['name'] = d[k]
+
+            nf = XMLAleMethod.xmlGetNode('formula')
+            if nf != None:
+                f = XMLAleMethod.xmlGetString("formula")
+                for k in d.keys():
+                    f = f.replace(k, d[k])
+
+                nf.xmlSetTextNode(f)
 
     def __backwardCompatibilityFrom_6_2(self):
         """