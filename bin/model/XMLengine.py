code-saturne/code_saturne

commit 32feea75b755941139b52f0002eade54693b4c18

Update Copyright.

bin/model/XMLengine.py
@@ -4,7 +4,7 @@
 
 # This file is part of Code_Saturne, a general-purpose CFD tool.
 #
-# Copyright (C) 1998-2020 EDF S.A.
+# Copyright (C) 1998-2021 EDF S.A.
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software

commit b42b50f3dc60a41a7f6dae13caf8d9967cf86ed0

XML model: fix creation of new node, broken by commit 32feea75

bin/model/XMLengine.py
@@ -666,7 +666,7 @@ class XMLElement:
         # Add a new ELEMENT_NODE and a new TEXT_NODE
         # or replace an existing TEXT_NODE
         #
-        if textNode and not nodeList:
+        if textNode != None and not nodeList:
             child = self.xmlAddChild(tag, *attrList, **kwargs)
             child.xmlSetTextNode(textNode)
             elementList.append(child)