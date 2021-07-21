code-saturne/code_saturne

commit a40a1d1de8aea74e3907989abf8b6e815d1eb2bb

Simplify handling of timers, to focus on wall-time.

src/base/cs_timer.c
@@ -199,7 +199,8 @@ static clock_t _cs_timer_clock_start;
 
 /* Reference times */
 
-static cs_timer_t  _cs_timer_start;
+static cs_timer_t  _cs_timer_start = {.sec = 0, .nsec = 0};
+static cs_timer_t  _cs_timer_cpu_start = {.sec = 0, .nsec = 0};
 
 /*============================================================================
  * Private function definitions
@@ -232,8 +233,8 @@ _cs_timer_wall_clock_gettime(cs_timer_t  *timer)
 {
   struct timespec w_time;
   (void)clock_gettime(CLOCK_REALTIME, &w_time);
-  timer->wall_sec = w_time.tv_sec;
-  timer->wall_nsec = w_time.tv_nsec;
+  timer->sec = w_time.tv_sec;
+  timer->nsec = w_time.tv_nsec;
 }
 
 #endif /* defined (HAVE_CLOCK_GETTIME) */
@@ -252,8 +253,8 @@ _cs_timer_wall_gettimeofday(cs_timer_t  *timer)
 {
   struct timeval  tv_time;
   (void)gettimeofday(&tv_time, NULL);
-  timer->wall_sec = tv_time.tv_sec;
-  timer->wall_nsec = tv_time.tv_usec*1000;
+  timer->sec = tv_time.tv_sec;
+  timer->nsec = tv_time.tv_usec*1000;
 }
 
 #endif /* defined (HAVE_GETTIMEOFDAY) */
@@ -272,8 +273,8 @@ _cs_timer_wall_stdc_time(cs_timer_t  *timer)
   double dt;
   time(&wtime_current);
   dt = difftime(wtime_current, _cs_timer_stdc_time_start);
-  timer->wall_sec = floor(dt);
-  timer->wall_nsec = (dt - timer->wall_sec) * 1.0e-9;
+  timer->sec = floor(dt);
+  timer->nsec = (dt - timer->sec) * 1.0e-9;
 }
 
 /*----------------------------------------------------------------------------
@@ -303,8 +304,8 @@ _cs_timer_cpu_clock_gettime(cs_timer_t  *timer)
 {
   struct timespec cpu_time;
   (void)clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &cpu_time);
-  timer->cpu_sec = cpu_time.tv_sec;
-  timer->cpu_nsec = cpu_time.tv_nsec;
+  timer->sec = cpu_time.tv_sec;
+  timer->nsec = cpu_time.tv_nsec;
 }
 
 #endif /* defined (HAVE_CLOCK_GETTIME) */
@@ -323,8 +324,8 @@ _cs_timer_cpu_getrusage(cs_timer_t  *timer)
 {
   struct rusage  usage;
   getrusage(RUSAGE_SELF, &usage);
-  timer->cpu_sec = usage.ru_utime.tv_sec + usage.ru_stime.tv_sec;
-  timer->cpu_nsec = (usage.ru_utime.tv_usec + usage.ru_stime.tv_usec)*1000;
+  timer->sec = usage.ru_utime.tv_sec + usage.ru_stime.tv_sec;
+  timer->nsec = (usage.ru_utime.tv_usec + usage.ru_stime.tv_usec)*1000;
 }
 
 #endif /* defined (HAVE_GETRUSAGE) */
@@ -347,7 +348,7 @@ _cs_timer_cpu_times(cs_timer_t  *timer)
   ticks = ptimer.tms_utime + ptimer.tms_stime;
   timer->cpu_sec = ticks / _cs_timer_unit;
   timer->cpu_nsec = (double)(ticks % _cs_timer_unit)*1.e+9 / _cs_timer_unit;
- }
+}
 
 #endif /* defined(_POSIX_SOURCE) */
 
@@ -362,8 +363,8 @@ void
 _cs_timer_cpu_stdc_clock(cs_timer_t  *timer)
 {
   clock_t clock_current = clock() - _cs_timer_clock_start;
-  timer->cpu_sec = clock_current / CLOCKS_PER_SEC;
-  timer->cpu_nsec = (clock_current % CLOCKS_PER_SEC)*1.e+9/CLOCKS_PER_SEC;
+  timer->sec = clock_current / CLOCKS_PER_SEC;
+  timer->nsec = (clock_current % CLOCKS_PER_SEC)*1.e+9/CLOCKS_PER_SEC;
 }
 
 /*----------------------------------------------------------------------------
@@ -373,10 +374,11 @@ _cs_timer_cpu_stdc_clock(cs_timer_t  *timer)
 static void
 _cs_timer_initialize(void)
 {
-  _cs_timer_start.wall_sec = 0;
-  _cs_timer_start.wall_nsec = 0;
-  _cs_timer_start.cpu_sec = 0;
-  _cs_timer_start.cpu_nsec = 0;
+  _cs_timer_start.sec = 0;
+  _cs_timer_start.nsec = 0;
+
+  _cs_timer_cpu_start.sec = 0;
+  _cs_timer_cpu_start.nsec = 0;
 
   /* Select timing methods, trying highest resolution first */
 
@@ -386,8 +388,8 @@ _cs_timer_initialize(void)
 
   if (_cs_timer_wall_method == CS_TIMER_DISABLE) {
     if (clock_gettime(CLOCK_REALTIME, &ts_time) == 0) {
-      _cs_timer_start.wall_sec = ts_time.tv_sec;
-      _cs_timer_start.wall_nsec = ts_time.tv_nsec;
+      _cs_timer_start.sec = ts_time.tv_sec;
+      _cs_timer_start.nsec = ts_time.tv_nsec;
       _cs_timer_wall_method = CS_TIMER_CLOCK_GETTIME;
       _cs_timer_wall = _cs_timer_wall_clock_gettime;
     }
@@ -414,8 +416,8 @@ _cs_timer_initialize(void)
   if (_cs_timer_wall_method == CS_TIMER_DISABLE) {
     static struct timeval  tv_time;
     if (gettimeofday(&tv_time, NULL) == 0) {
-      _cs_timer_start.wall_sec = tv_time.tv_sec;
-      _cs_timer_start.wall_nsec = tv_time.tv_usec*1000;
+      _cs_timer_start.sec = tv_time.tv_sec;
+      _cs_timer_start.nsec = tv_time.tv_usec*1000;
       _cs_timer_wall_method = CS_TIMER_GETTIMEOFDAY;
       _cs_timer_wall = _cs_timer_wall_gettimeofday;
     }
@@ -500,8 +502,8 @@ cs_timer_wtime(void)
   _cs_timer_wall(&t1);
 
   long long wall_nsec
-    =  (t1.wall_sec - _cs_timer_start.wall_sec) * (long long)1000000000
-      + t1.wall_nsec - _cs_timer_start.wall_nsec;
+    =  (t1.sec - _cs_timer_start.sec) * (long long)1000000000
+      + t1.nsec - _cs_timer_start.nsec;
 
   return wall_nsec*1.e-9;
 }
@@ -536,8 +538,8 @@ cs_timer_cpu_time(void)
   _cs_timer_cpu(&t1);
 
   long long cpu_nsec
-    =  (t1.cpu_sec - _cs_timer_start.cpu_sec) * (long long)1000000000
-       + t1.cpu_nsec - _cs_timer_start.cpu_nsec;
+    =  (t1.sec - _cs_timer_start.sec) * (long long)1000000000
+       + t1.nsec - _cs_timer_start.nsec;
 
   return cpu_nsec*1.e-9;
 }
@@ -616,7 +618,6 @@ cs_timer_time(void)
   /* Compute elapsed time */
 
   _cs_timer_wall(&time_current);
-  _cs_timer_cpu(&time_current);
 
   return time_current;
 }
@@ -638,10 +639,8 @@ cs_timer_diff(const cs_timer_t  *t0,
 {
   cs_timer_counter_t retval;
 
-  retval.wall_nsec =  (t1->wall_sec - t0->wall_sec) * (long long)1000000000
-                     + t1->wall_nsec - t0->wall_nsec;
-  retval.cpu_nsec =  (t1->cpu_sec - t0->cpu_sec) * (long long)1000000000
-                    + t1->cpu_nsec - t0->cpu_nsec;
+  retval.nsec =  (t1->sec - t0->sec) * (long long)1000000000
+                + t1->nsec - t0->nsec;
 
   return retval;
 }

src/alge/cs_benchmark_matrix.c
@@ -1536,7 +1536,7 @@ cs_benchmark_matrix(double                 t_measure,
   cs_log_printf(CS_LOG_PERFORMANCE,
                 "\n"
                 "Elapsed timing time: %12.3f s.\n",
-                t_tune.wall_nsec*1e-9);
+                t_tune.nsec*1e-9);
 }
 
 /*----------------------------------------------------------------------------*/

src/alge/cs_gradient.c
@@ -467,7 +467,7 @@ _gradient_info_dump(cs_gradient_info_t *this_info)
                   this_info->n_iter_max);
   cs_log_printf(CS_LOG_PERFORMANCE,
                 _("  Total elapsed time:    %.3f\n"),
-                this_info->t_tot.wall_nsec*1e-9);
+                this_info->t_tot.nsec*1e-9);
 }
 
 /*----------------------------------------------------------------------------
@@ -8802,7 +8802,7 @@ cs_gradient_finalize(void)
   cs_log_printf(CS_LOG_PERFORMANCE,
                 _("\n"
                   "Total elapsed time for all gradient computations:  %.3f s\n"),
-                _gradient_t_tot.wall_nsec*1e-9);
+                _gradient_t_tot.nsec*1e-9);
 
   /* Free system info */
 

src/alge/cs_sles.c
@@ -958,7 +958,7 @@ cs_sles_log(cs_log_t  log_type)
       (log_type,
        _("\n"
          "Total elapsed time for linear equation system solvers:  %.3f s\n"),
-       _sles_t_tot.wall_nsec*1e-9);
+       _sles_t_tot.nsec*1e-9);
 
   const char *option_category[]
     = {N_("Linear solver options modified during run (previous values)"),

src/alge/cs_sles_amgx.c
@@ -1602,8 +1602,8 @@ cs_sles_amgx_log(const void  *context,
                     "  Total solution time:           %12.3f\n"),
                   m_type,
                   c->n_setups, n_calls, n_it_min, n_it_max, n_it_mean,
-                  c->t_setup.wall_nsec*1e-9,
-                  c->t_solve.wall_nsec*1e-9);
+                  c->t_setup.nsec*1e-9,
+                  c->t_solve.nsec*1e-9);
 
   }
 }

src/alge/cs_sles_it.c
@@ -2697,7 +2697,7 @@ _gcr(cs_sles_it_t              *c,
 
       cs_matrix_vector_multiply(rotation_mode, a, zk_n, ck_n);
 
-      for(cs_lnum_t jj = 0; jj < n_iter; jj++) {
+      for(cs_lnum_t jj = 0; jj < (int)n_iter; jj++) {
         cs_real_t *ck_j = ck + jj * wa_size;
 
         gkj[(n_iter + 1) * n_iter / 2 + jj] = _dot_product(c, ck_j, ck_n);
@@ -2741,7 +2741,7 @@ _gcr(cs_sles_it_t              *c,
          jj++)
       gkj_inv[jj] = 0.0;
 
-    for (cs_lnum_t kk = 0; kk < n_iter; kk++) {
+    for (cs_lnum_t kk = 0; kk < (int)n_iter; kk++) {
       for(cs_lnum_t ii = 0; ii < kk; ii++) {
         for (cs_lnum_t jj = 0; jj < kk; jj++)
           gkj_inv[(kk + 1) * kk / 2 + ii]
@@ -2762,7 +2762,7 @@ _gcr(cs_sles_it_t              *c,
       cs_lnum_t s_id, e_id;
       cs_parall_thread_range(n_rows, sizeof(cs_real_t), &s_id, &e_id);
 
-      for(cs_lnum_t kk = 0; kk < n_iter; kk++) {
+      for(cs_lnum_t kk = 0; kk < (int)n_iter; kk++) {
         for(cs_lnum_t jj = 0; jj <= kk; jj++) {
           const cs_real_t *zk_j = zk + jj*wa_size;
           for (cs_lnum_t ii = s_id; ii < e_id; ii++)
@@ -4127,8 +4127,8 @@ cs_sles_it_log(const void  *context,
                     "  Total setup time:              %12.3f\n"
                     "  Total solution time:           %12.3f\n"),
                   c->n_setups, n_calls, n_it_min, n_it_max, n_it_mean,
-                  c->t_setup.wall_nsec*1e-9,
-                  c->t_solve.wall_nsec*1e-9);
+                  c->t_setup.nsec*1e-9,
+                  c->t_solve.nsec*1e-9);
 
     if (c->fallback != NULL) {
 
@@ -4156,7 +4156,7 @@ cs_sles_it_log(const void  *context,
                     "  Mean number of iterations:     %12d\n"
                     "  Total solution time:           %12.3f\n"),
                   n_calls, n_it_min, n_it_max, n_it_mean,
-                  c->fallback->t_solve.wall_nsec*1e-9);
+                  c->fallback->t_solve.nsec*1e-9);
 
     }
 
@@ -4357,7 +4357,7 @@ cs_sles_it_solve(void                *context,
 
   cs_sles_convergence_state_t cvg = CS_SLES_ITERATING;
 
-  cs_timer_t t0 = {0, 0, 0, 0}, t1;
+  cs_timer_t t0 = {0, 0}, t1;
 
   unsigned _n_iter = 0;
   cs_sles_it_convergence_t  convergence;

src/alge/cs_sles_mumps.c
@@ -2455,7 +2455,7 @@ cs_sles_mumps_log(const void  *context,
                     "  Total setup time:              %12.3f\n"
                     "  Total solution time:           %12.3f\n"),
                   MUMPS_VERSION, c->n_setups, c->n_solves,
-                  c->t_setup.wall_nsec*1e-9, c->t_solve.wall_nsec*1e-9);
+                  c->t_setup.nsec*1e-9, c->t_solve.nsec*1e-9);
 
   }
 }

src/alge/cs_sles_petsc.c
@@ -1625,8 +1625,8 @@ cs_sles_petsc_log(const void  *context,
                     "  Total solution time:           %12.3f\n"),
                   s_type, p_type, norm_type_name, m_type,
                   c->n_setups, n_calls, n_it_min, n_it_max, n_it_mean,
-                  c->t_setup.wall_nsec*1e-9,
-                  c->t_solve.wall_nsec*1e-9);
+                  c->t_setup.nsec*1e-9,
+                  c->t_solve.nsec*1e-9);
 
   }
 }

commit a4958e60d9b5dedddd50766f2c710c7cbddb8f86

Fix CPU time report, broken by commit a40a1d1d (May 5, 2021).
Fixes issue #327.

src/base/cs_timer.c
@@ -538,8 +538,8 @@ cs_timer_cpu_time(void)
   _cs_timer_cpu(&t1);
 
   long long cpu_nsec
-    =  (t1.sec - _cs_timer_start.sec) * (long long)1000000000
-       + t1.nsec - _cs_timer_start.nsec;
+    =  (t1.sec - _cs_timer_cpu_start.sec) * (long long)1000000000
+       + t1.nsec - _cs_timer_cpu_start.nsec;
 
   return cpu_nsec*1.e-9;
 }