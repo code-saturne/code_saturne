code-saturne/code_saturne

commit ad2c9d341af9896d6530d293c301bff72bbbc17b

GUI: add convective inlet option and fix issues with mapped inlet. Also mask mapped inlet for compressible model.
git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@9574 e44c5d27-f830-0410-8ce8-974f7a764be6

src/gui/cs_gui_boundary_conditions.c
@@ -173,6 +173,36 @@ _boundary_status(const char  *nature,
   path = cs_xpath_init_path();
   cs_xpath_add_elements(&path, 2, "boundary_conditions", nature);
   cs_xpath_add_test_attribute(&path, "label", label);
+  cs_xpath_add_element(&path, tag);
+  cs_xpath_add_attribute(&path, "status");
+
+  if (cs_gui_get_status(path, &result))
+    *data = result;
+  BFT_FREE(path);
+}
+
+/*-----------------------------------------------------------------------------
+ * Get status of data for velocity_pressure node of inlet or outlet information.
+ *
+ * parameters:
+ *   nature      <--  nature of the boundary
+ *   label       <--  label of the inlet or the outlet
+ *   tag         <--  name of researched data
+ *   data       -->   value associated to the data
+ *----------------------------------------------------------------------------*/
+
+static void
+_boundary_status_vp(const char  *nature,
+                    const char  *label,
+                    const char  *tag,
+                    int         *data)
+{
+  char  *path = NULL;
+  int  result = 0;
+
+  path = cs_xpath_init_path();
+  cs_xpath_add_elements(&path, 2, "boundary_conditions", nature);
+  cs_xpath_add_test_attribute(&path, "label", label);
   cs_xpath_add_elements(&path, 2, "velocity_pressure", tag);
   cs_xpath_add_attribute(&path, "status");
 
@@ -218,6 +248,7 @@ _mapped_inlet(const char                *label,
       cs_xpath_add_element(&path, "inlet");
       cs_xpath_add_element(&path, "mapped_inlet");
       cs_xpath_add_element(&path, tname[i]);
+      cs_xpath_add_function_text(&path);
       cs_gui_get_double(path, &value);
       BFT_FREE(path);
       coord_shift[i] = value;
@@ -1442,10 +1473,10 @@ _init_boundaries(const cs_lnum_t   n_b_faces,
       /* Inlet: data for ATMOSPHERIC FLOWS */
       if (cs_gui_strcmp(vars->model, "atmospheric_flows"))
       {
-        _boundary_status("inlet", label, "meteo_data",
-                         &boundaries->meteo[izone].read_data);
-        _boundary_status("inlet", label, "meteo_automatic",
-                         &boundaries->meteo[izone].automatic);
+        _boundary_status_vp("inlet", label, "meteo_data",
+                            &boundaries->meteo[izone].read_data);
+        _boundary_status_vp("inlet", label, "meteo_automatic",
+                            &boundaries->meteo[izone].automatic);
       }
 
       /* Inlet: data for darcy */
@@ -1473,10 +1504,10 @@ _init_boundaries(const cs_lnum_t   n_b_faces,
     else if (cs_gui_strcmp(nature, "outlet")) {
       /* Outlet: data for ATMOSPHERIC FLOWS */
       if (cs_gui_strcmp(vars->model, "atmospheric_flows")) {
-        _boundary_status("outlet", label, "meteo_data",
-                         &boundaries->meteo[izone].read_data);
-        _boundary_status("outlet", label, "meteo_automatic",
-                         &boundaries->meteo[izone].automatic);
+        _boundary_status_vp("outlet", label, "meteo_data",
+                            &boundaries->meteo[izone].read_data);
+        _boundary_status_vp("outlet", label, "meteo_automatic",
+                            &boundaries->meteo[izone].automatic);
       }
 
       /* Outlet: data for COMPRESSIBLE MODEL */
@@ -2120,17 +2151,25 @@ void CS_PROCF (uiclim, UICLIM)(const int  *idarcy,
 
       /* data by boundary faces */
 
+      int inlet_type = CS_INLET;
+
+      if (cs_gui_strcmp(vars->model, "compressible_model"))
+        inlet_type = boundaries->itype[izone];
+      else {
+        int convective_inlet = 0;
+        _boundary_status("inlet", boundaries->label[izone],
+                         "convective_inlet", &convective_inlet);
+        if (convective_inlet)
+          inlet_type = CS_CONVECTIVE_INLET;
+      }
+
       for (cs_lnum_t ifac = 0; ifac < faces; ifac++) {
         ifbr = faces_list[ifac];
 
         /* zone number and nature of boundary */
         izfppp[ifbr] = zone_nbr;
-        if (cs_gui_strcmp(vars->model, "compressible_model"))
-          itypfb[ifbr] = boundaries->itype[izone];
-        else
-          itypfb[ifbr] = CS_INLET;
+        itypfb[ifbr] = inlet_type;
       }
-
 
       if (cs_gui_strcmp(vars->model, "atmospheric_flows")) {
         iprofm[zone_nbr-1] = boundaries->meteo[izone].read_data;
@@ -2948,32 +2987,24 @@ void CS_PROCF (uiclim, UICLIM)(const int  *idarcy,
         ivar = cs_field_get_key_int(f, var_key_id) -1;
 
         if (f->type & CS_FIELD_VARIABLE) {
-          switch (boundaries->type_code[f->id][izone]) {
-          case DIRICHLET:
-          case DIRICHLET_FORMULA:
-            {
-              int interpolate = 0;
-              int normalize = 0;
-              if (f == CS_F_(u))
-                normalize = 1;
-              else {
-                const int keysca = cs_field_key_id("scalar_id");
-                if (cs_field_get_key_int(f, keysca) > 0)
-                  normalize = 1;
-              }
-              cs_boundary_conditions_mapped_set(f, boundaries->locator[izone],
-                                                CS_MESH_LOCATION_CELLS,
-                                                normalize, interpolate,
-                                                faces, faces_list,
-                                                NULL, *nvar, rcodcl);
-            }
-            break;
-          default:
-            break;
+          int interpolate = 0;
+          int normalize = 0;
+          if (f == CS_F_(u))
+            normalize = 1;
+          else {
+            const int keysca = cs_field_key_id("scalar_id");
+            if (cs_field_get_key_int(f, keysca) > 0)
+              normalize = 1;
           }
+          if (f != CS_F_(p))
+            cs_boundary_conditions_mapped_set(f, boundaries->locator[izone],
+                                              CS_MESH_LOCATION_CELLS,
+                                              normalize, interpolate,
+                                              faces, faces_list,
+                                              NULL, *nvar, rcodcl);
         }
-      }
 
+      }
     }
 
   } /*  for (izone=0 ; izone < zones ; izone++) */
@@ -3163,6 +3194,32 @@ void CS_PROCF (uiclve, UICLVE)(const int  *nozppm,
                 _("zone's label number %i is greater than %i,"
                   " the maximum allowed \n"), zone_nbr, *nozppm);
 
+    /* Boundary types compatibilty:
+       the nature of the boundary can be changed from smooth wall to
+       rough wall or vice-versa between the GUI and the user code.
+       It can also be changed from inlet to convective inlet and
+       vice-versa */
+
+    int enature = inature;
+    if (inature == CS_ROUGHWALL)
+      enature = CS_SMOOTHWALL;
+    else if (inature == CS_CONVECTIVE_INLET)
+      enature = CS_INLET;
+
+    int atmo_auto = 0;
+    int compr_auto = 0;
+
+    if (cs_gui_strcmp(cs_glob_var->model, "atmospheric_flows")) {
+      if (boundaries->meteo[izone].automatic) {
+        if (inature == CS_INLET || inature == CS_OUTLET)
+          atmo_auto = inature;
+      }
+    }
+    else if (cs_gui_strcmp(cs_glob_var->model, "compressible_model")) {
+      if (inature == CS_INLET || inature == CS_OUTLET)
+        compr_auto = inature;
+    }
+
     const cs_lnum_t *faces_list
       = cs_gui_get_boundary_faces(boundaries->label[izone], &faces);
 
@@ -3195,29 +3252,24 @@ void CS_PROCF (uiclve, UICLVE)(const int  *nozppm,
 
       inature2 = itypfb[ifbr];
 
-      /* The nature of the boundary can be changed from smooth wall to
-         rough wall or vice-versa between the GUI and the FORTRAN */
-      if (inature2 == CS_ROUGHWALL) inature2 = CS_SMOOTHWALL;
-      if (inature == CS_ROUGHWALL) inature = CS_SMOOTHWALL;
+      int enature2 = inature;
+      if (inature2 == CS_ROUGHWALL)
+        enature2 = CS_SMOOTHWALL;
+      else if (inature2 == CS_CONVECTIVE_INLET)
+        inature2 = CS_INLET;
 
-      if (cs_gui_strcmp(cs_glob_var->model, "atmospheric_flows")) {
-        if (boundaries->meteo[izone].automatic) {
-          if ((inature == CS_INLET || inature == CS_OUTLET) && inature2 == 0)
-            inature2 = inature;
-        }
-      }
-      else if (cs_gui_strcmp(cs_glob_var->model, "compressible_model")) {
-        if (inature == CS_INLET  && inature2 == CS_ESICF)
-          inature2 = inature;
-        if (inature == CS_INLET  && inature2 == CS_EPHCF)
-          inature2 = inature;
-        if (inature == CS_OUTLET  && inature2 == CS_SSPCF)
-          inature2 = inature;
-        if (inature == CS_OUTLET  && inature2 == CS_SOPCF)
+      if (atmo_auto  && inature2 == 0)
+        inature2 = inature;
+
+      else if (compr_auto) {
+        if (   (compr_auto == CS_INLET  && (   inature2 == CS_ESICF
+                                            || inature2 == CS_EPHCF))
+            || (compr_auto = CS_OUTLET  &&  (   inature2 == CS_SSPCF
+                                             || inature2 == CS_SOPCF)))
           inature2 = inature;
       }
 
-      if (inature2 != inature)
+      if (enature2 != enature)
         bft_error
           (__FILE__, __LINE__, 0,
            _("@                                                            \n"

gui/Pages/Boundary.py
@@ -308,7 +308,6 @@ class InletBoundary(Boundary):
         dico['pressure']            = 0.0
         dico['pressure_flux']       = 0.0
         dico['hydraulicHeadChoice'] = 'dirichlet'
-        dico['mapped']              = 'off'
 
         from code_saturne.Pages.GasCombustionModel import GasCombustionModel
         model = GasCombustionModel(self.case).getGasCombustionModel()
@@ -1125,6 +1124,34 @@ omega = 0.;"""
         scalarNode = self.boundNode.xmlInitNode('dirichlet_formula', name='hydraulicHead')
 
         n = scalarNode.xmlSetData('formula', formula)
+
+
+    @Variables.noUndo
+    def getConvectiveInletStatus(self):
+        """
+        Get the convective inlet status
+        """
+        status = None
+        n = self.boundNode.xmlGetNode('convective_inlet')
+        if n:
+            status = n['status']
+        if not status:
+            status = 'off'
+        return status
+
+
+    @Variables.undoLocal
+    def setConvectiveInletStatus(self, status):
+        """
+        Set the convective inlet status
+        """
+        Model().isInList(status, ('on', 'off'))
+
+        n = self.boundNode.xmlInitNode('convective_inlet', status=status)
+        if status == 'off':
+            self.boundNode.xmlRemoveChild('convective_inlet')
+        else:
+            n['status'] = status
 
 
     @Variables.noUndo

gui/Pages/BoundaryConditionsConvectiveInletForm.ui
@@ -1,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>BoundaryConditionsConvectiveInletForm</class>
+ <widget class="QWidget" name="BoundaryConditionsConvectiveInletForm">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>313</width>
+    <height>116</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>Form</string>
+  </property>
+  <layout class="QGridLayout" name="gridLayout">
+   <item row="0" column="0">
+    <widget class="QGroupBox" name="groupBoxConvectiveInlet">
+     <property name="toolTip">
+      <string>For a convective inlet, diffusion is disabled at adjacent cells.</string>
+     </property>
+     <property name="title">
+      <string>Convective Inlet</string>
+     </property>
+     <property name="checkable">
+      <bool>true</bool>
+     </property>
+    </widget>
+   </item>
+  </layout>
+ </widget>
+ <resources>
+  <include location="resources_pages.qrc"/>
+ </resources>
+ <connections/>
+</ui>

gui/Pages/BoundaryConditionsConvectiveInletView.py
@@ -24,7 +24,7 @@
 
 """
 This module contains the following classes:
-- BoundaryConditionsMappedInletView
+- BoundaryConditionsConvectiveInletView
 """
 
 #-------------------------------------------------------------------------------
@@ -45,7 +45,7 @@ from code_saturne.Base.QtWidgets import *
 # Application modules import
 #-------------------------------------------------------------------------------
 
-from code_saturne.Pages.BoundaryConditionsMappedInletForm import Ui_BoundaryConditionsMappedInletForm
+from code_saturne.Pages.BoundaryConditionsConvectiveInletForm import Ui_BoundaryConditionsConvectiveInletForm
 
 from code_saturne.Base.Toolbox import GuiParam
 from code_saturne.Base.QtPage import DoubleValidator, ComboModel, from_qvariant
@@ -60,14 +60,14 @@ from code_saturne.Pages.QMeiEditorView import QMeiEditorView
 #-------------------------------------------------------------------------------
 
 logging.basicConfig()
-log = logging.getLogger("BoundaryConditionsMappedInletView")
+log = logging.getLogger("BoundaryConditionsConvectiveInletView")
 log.setLevel(GuiParam.DEBUG)
 
 #-------------------------------------------------------------------------------
 # Main class
 #-------------------------------------------------------------------------------
 
-class BoundaryConditionsMappedInletView(QWidget, Ui_BoundaryConditionsMappedInletForm):
+class BoundaryConditionsConvectiveInletView(QWidget, Ui_BoundaryConditionsConvectiveInletForm):
     """
     Boundary condition for velocity in inlet, without particular physics.
     """
@@ -77,7 +77,7 @@ class BoundaryConditionsMappedInletView(QWidget, Ui_BoundaryConditionsMappedInle
         """
         QWidget.__init__(self, parent)
 
-        Ui_BoundaryConditionsMappedInletForm.__init__(self)
+        Ui_BoundaryConditionsConvectiveInletForm.__init__(self)
         self.setupUi(self)
 
 
@@ -91,21 +91,11 @@ class BoundaryConditionsMappedInletView(QWidget, Ui_BoundaryConditionsMappedInle
         self.__case.undoStopGlobal()
 
         # Connections
-        self.groupBoxMappedInlet.clicked[bool].connect(self.__slotMappedInlet)
-
-        self.lineEditTranslationX.textChanged[str].connect(self.__slotTrX)
-        self.lineEditTranslationY.textChanged[str].connect(self.__slotTrY)
-        self.lineEditTranslationZ.textChanged[str].connect(self.__slotTrZ)
+        self.groupBoxConvectiveInlet.clicked[bool].connect(self.__slotConvectiveInlet)
 
         # Validators
-        validatorX = DoubleValidator(self.lineEditTranslationX)
-        validatorY = DoubleValidator(self.lineEditTranslationY)
-        validatorZ = DoubleValidator(self.lineEditTranslationZ)
 
         # Apply validators
-        self.lineEditTranslationX.setValidator(validatorX)
-        self.lineEditTranslationY.setValidator(validatorY)
-        self.lineEditTranslationZ.setValidator(validatorZ)
 
         self.__case.undoStartGlobal()
 
@@ -123,14 +113,14 @@ class BoundaryConditionsMappedInletView(QWidget, Ui_BoundaryConditionsMappedInle
         if mdl.getCompressibleModel() != "off":
             hide = True
 
-        if not hide and self.__boundary.getMappedInletStatus() == "on":
+        if not hide and self.__boundary.getConvectiveInletStatus() == "on":
             checked = True
 
         if checked:
-            self.groupBoxMappedInlet.setChecked(True)
+            self.groupBoxConvectiveInlet.setChecked(True)
         else:
-            self.groupBoxMappedInlet.setChecked(False)
-        self.__slotMappedInlet(checked)
+            self.groupBoxConvectiveInlet.setChecked(False)
+        self.__slotConvectiveInlet(checked)
 
         if hide:
             self.hide()
@@ -146,61 +136,21 @@ class BoundaryConditionsMappedInletView(QWidget, Ui_BoundaryConditionsMappedInle
 
 
     @pyqtSlot(bool)
-    def __slotMappedInlet(self, checked):
+    def __slotConvectiveInlet(self, checked):
         """
         Private slot.
 
-        Activates mapped inlet boundary condition.
+        Activates convective inlet boundary condition.
 
         @type checked: C{True} or C{False}
-        @param checked: if C{True}, shows the QGroupBox mapped inlet parameters.
+        @param checked: if C{True}, shows the QGroupBox convective inlet parameters.
         """
-        self.groupBoxMappedInlet.setFlat(not checked)
+        self.groupBoxConvectiveInlet.setFlat(not checked)
 
         if checked:
-            self.__boundary.setMappedInletStatus("on")
-            self.labelDirection.show()
-            self.frameDirectionCoordinates.show()
-            x = self.__boundary.getMappedInletTranslation('translation_x')
-            y = self.__boundary.getMappedInletTranslation('translation_y')
-            z = self.__boundary.getMappedInletTranslation('translation_z')
-            self.lineEditTranslationX.setText(str(x))
-            self.lineEditTranslationY.setText(str(y))
-            self.lineEditTranslationZ.setText(str(z))
+            self.__boundary.setConvectiveInletStatus("on")
         else:
-            self.__boundary.setMappedInletStatus("off")
-            self.labelDirection.hide()
-            self.frameDirectionCoordinates.hide()
-
-
-    @pyqtSlot(str)
-    def __slotTrX(self, text):
-        """
-        INPUT value into direction of mapping translation
-        """
-        if self.lineEditTranslationX.validator().state == QValidator.Acceptable:
-            value = from_qvariant(text, float)
-            self.__boundary.setMappedInletTranslation('translation_x', value)
-
-
-    @pyqtSlot(str)
-    def __slotTrY(self, text):
-        """
-        INPUT value into direction of mapping translation
-        """
-        if self.lineEditTranslationY.validator().state == QValidator.Acceptable:
-            value = from_qvariant(text, float)
-            self.__boundary.setMappedInletTranslation('translation_y', value)
-
-
-    @pyqtSlot(str)
-    def __slotTrZ(self, text):
-        """
-        INPUT value into direction of mapping translation
-        """
-        if self.lineEditTranslationZ.validator().state == QValidator.Acceptable:
-            value = from_qvariant(text, float)
-            self.__boundary.setMappedInletTranslation('translation_z', value)
+            self.__boundary.setConvectiveInletStatus("off")
 
 
     def tr(self, text):

gui/Pages/BoundaryConditionsForm.ui
@@ -7,7 +7,7 @@
     <x>0</x>
     <y>0</y>
     <width>281</width>
-    <height>398</height>
+    <height>468</height>
    </rect>
   </property>
   <property name="windowTitle">
@@ -66,7 +66,7 @@
         </property>
        </widget>
       </item>
-      <item row="4" column="0">
+      <item row="6" column="0">
        <widget class="BoundaryConditionsVelocityInletView" name="velocityWidget" native="true">
         <property name="sizePolicy">
          <sizepolicy hsizetype="Preferred" vsizetype="Preferred">
@@ -75,6 +75,64 @@
          </sizepolicy>
         </property>
        </widget>
+      </item>
+      <item row="8" column="0">
+       <widget class="BoundaryConditionsCoalInletView" name="coalWidget" native="true">
+        <property name="sizePolicy">
+         <sizepolicy hsizetype="Preferred" vsizetype="Preferred">
+          <horstretch>0</horstretch>
+          <verstretch>0</verstretch>
+         </sizepolicy>
+        </property>
+       </widget>
+      </item>
+      <item row="9" column="0">
+       <widget class="BoundaryConditionsCompressibleOutletView" name="compressibleOutletWidget" native="true">
+        <property name="sizePolicy">
+         <sizepolicy hsizetype="Preferred" vsizetype="Preferred">
+          <horstretch>0</horstretch>
+          <verstretch>0</verstretch>
+         </sizepolicy>
+        </property>
+       </widget>
+      </item>
+      <item row="10" column="0">
+       <widget class="BoundaryConditionsTurbulenceInletView" name="turbulenceWidget" native="true">
+        <property name="sizePolicy">
+         <sizepolicy hsizetype="Preferred" vsizetype="Preferred">
+          <horstretch>0</horstretch>
+          <verstretch>0</verstretch>
+         </sizepolicy>
+        </property>
+       </widget>
+      </item>
+      <item row="11" column="0">
+       <widget class="BoundaryConditionsScalarsView" name="scalarsWidget" native="true"/>
+      </item>
+      <item row="12" column="0">
+       <widget class="BoundaryConditionsMobileMeshView" name="mobileMeshWidget" native="true"/>
+      </item>
+      <item row="13" column="0">
+       <widget class="BoundaryConditionsWallRadiativeTransferView" name="radiativeWidget" native="true"/>
+      </item>
+      <item row="14" column="0">
+       <widget class="BoundaryConditionsElectricalView" name="electricalWidget" native="true"/>
+      </item>
+      <item row="15" column="0">
+       <widget class="BoundaryConditionsExternalHeadLossesView" name="externalHeadLossesWidget" native="true">
+        <property name="sizePolicy">
+         <sizepolicy hsizetype="Preferred" vsizetype="Preferred">
+          <horstretch>0</horstretch>
+          <verstretch>0</verstretch>
+         </sizepolicy>
+        </property>
+       </widget>
+      </item>
+      <item row="16" column="0">
+       <widget class="BoundaryConditionsHydraulicHeadView" name="hydraulicheadWidget" native="true"/>
+      </item>
+      <item row="17" column="0">
+       <widget class="BoundaryConditionsPressureView" name="pressureWidget" native="true"/>
       </item>
       <item row="5" column="0">
        <widget class="BoundaryConditionsMappedInletView" name="mappedInletWidget" native="true">
@@ -86,8 +144,8 @@
         </property>
        </widget>
       </item>
-      <item row="6" column="0">
-       <widget class="BoundaryConditionsCoalInletView" name="coalWidget" native="true">
+      <item row="4" column="0">
+       <widget class="BoundaryConditionsConvectiveInletView" name="convectiveInletWidget" native="true">
         <property name="sizePolicy">
          <sizepolicy hsizetype="Preferred" vsizetype="Preferred">
           <horstretch>0</horstretch>
@@ -95,54 +153,6 @@
          </sizepolicy>
         </property>
        </widget>
-      </item>
-      <item row="7" column="0">
-       <widget class="BoundaryConditionsCompressibleOutletView" name="compressibleOutletWidget" native="true">
-        <property name="sizePolicy">
-         <sizepolicy hsizetype="Preferred" vsizetype="Preferred">
-          <horstretch>0</horstretch>
-          <verstretch>0</verstretch>
-         </sizepolicy>
-        </property>
-       </widget>
-      </item>
-      <item row="8" column="0">
-       <widget class="BoundaryConditionsTurbulenceInletView" name="turbulenceWidget" native="true">
-        <property name="sizePolicy">
-         <sizepolicy hsizetype="Preferred" vsizetype="Preferred">
-          <horstretch>0</horstretch>
-          <verstretch>0</verstretch>
-         </sizepolicy>
-        </property>
-       </widget>
-      </item>
-      <item row="9" column="0">
-       <widget class="BoundaryConditionsScalarsView" name="scalarsWidget" native="true"/>
-      </item>
-      <item row="10" column="0">
-       <widget class="BoundaryConditionsMobileMeshView" name="mobileMeshWidget" native="true"/>
-      </item>
-      <item row="11" column="0">
-       <widget class="BoundaryConditionsWallRadiativeTransferView" name="radiativeWidget" native="true"/>
-      </item>
-      <item row="12" column="0">
-       <widget class="BoundaryConditionsElectricalView" name="electricalWidget" native="true"/>
-      </item>
-      <item row="13" column="0">
-       <widget class="BoundaryConditionsExternalHeadLossesView" name="externalHeadLossesWidget" native="true">
-        <property name="sizePolicy">
-         <sizepolicy hsizetype="Preferred" vsizetype="Preferred">
-          <horstretch>0</horstretch>
-          <verstretch>0</verstretch>
-         </sizepolicy>
-        </property>
-       </widget>
-      </item>
-      <item row="14" column="0">
-       <widget class="BoundaryConditionsHydraulicHeadView" name="hydraulicheadWidget" native="true"/>
-      </item>
-      <item row="15" column="0">
-       <widget class="BoundaryConditionsPressureView" name="pressureWidget" native="true"/>
       </item>
      </layout>
     </widget>
@@ -251,6 +261,12 @@
    <class>BoundaryConditionsPressureView</class>
    <extends>QWidget</extends>
    <header location="global">BoundaryConditionsPressureView.h</header>
+   <container>1</container>
+  </customwidget>
+  <customwidget>
+   <class>BoundaryConditionsConvectiveInletView</class>
+   <extends>QWidget</extends>
+   <header>BoundaryConditionsConvectiveInletView.h</header>
    <container>1</container>
   </customwidget>
  </customwidgets>

gui/Pages/BoundaryConditionsView.py
@@ -164,13 +164,16 @@ class BoundaryConditionsView(QWidget, Ui_BoundaryConditionsForm):
         # Set the case for custom widgets
         self.roughWidget.setup(self.__case)
         self.slidingWidget.setup(self.__case)
+        self.convectiveInletWidget.setup(self.__case)
         self.mappedInletWidget.setup(self.__case)
         self.velocityWidget.setup(self.__case)
         self.turbulenceWidget.setup(self.__case)
         self.compressibleOutletWidget.setup(self.__case)
         self.coalWidget.setup(self.__case)
         self.scalarsWidget.setup(self.__case)
-        self.meteoWidget.setup(self.__case, self.velocityWidget, self.turbulenceWidget, self.scalarsWidget)
+        self.meteoWidget.setup(self.__case, self.velocityWidget,
+                               self.turbulenceWidget,
+                               self.scalarsWidget)
         self.mobileMeshWidget.setup(self.__case)
         self.radiativeWidget.setup(self.__case)
         self.electricalWidget.setup(self.__case)
@@ -233,6 +236,7 @@ class BoundaryConditionsView(QWidget, Ui_BoundaryConditionsForm):
         self.electricalWidget.showWidget(boundary)
         self.externalHeadLossesWidget.hideWidget()
         self.pressureWidget.hideWidget()
+        self.convectiveInletWidget.showWidget(boundary)
         self.mappedInletWidget.showWidget(boundary)
 
 
@@ -249,6 +253,7 @@ class BoundaryConditionsView(QWidget, Ui_BoundaryConditionsForm):
         self.externalHeadLossesWidget.hideWidget()
         self.hydraulicheadWidget.hideWidget()
         self.pressureWidget.hideWidget()
+        self.convectiveInletWidget.hideWidget()
         self.mappedInletWidget.hideWidget()
 
 
@@ -270,6 +275,7 @@ class BoundaryConditionsView(QWidget, Ui_BoundaryConditionsForm):
         else:
             self.hydraulicheadWidget.showWidget(boundary)
         self.pressureWidget.hideWidget()
+        self.convectiveInletWidget.hideWidget()
         self.mappedInletWidget.hideWidget()
 
 
@@ -287,6 +293,7 @@ class BoundaryConditionsView(QWidget, Ui_BoundaryConditionsForm):
         self.externalHeadLossesWidget.showWidget(boundary)
         self.hydraulicheadWidget.hideWidget()
         self.pressureWidget.hideWidget()
+        self.convectiveInletWidget.hideWidget()
         self.mappedInletWidget.hideWidget()
 
 
@@ -304,6 +311,7 @@ class BoundaryConditionsView(QWidget, Ui_BoundaryConditionsForm):
         self.externalHeadLossesWidget.hideWidget()
         self.hydraulicheadWidget.hideWidget()
         self.pressureWidget.showWidget(boundary)
+        self.convectiveInletWidget.hideWidget()
         self.mappedInletWidget.hideWidget()
 
 
@@ -321,6 +329,7 @@ class BoundaryConditionsView(QWidget, Ui_BoundaryConditionsForm):
         self.externalHeadLossesWidget.hideWidget()
         self.hydraulicheadWidget.showWidget(boundary)
         self.pressureWidget.hideWidget()
+        self.convectiveInletWidget.hideWidget()
         self.mappedInletWidget.hideWidget()
 
 
@@ -349,6 +358,7 @@ class BoundaryConditionsView(QWidget, Ui_BoundaryConditionsForm):
         self.externalHeadLossesWidget.hideWidget()
         self.hydraulicheadWidget.hideWidget()
         self.pressureWidget.hideWidget()
+        self.convectiveInletWidget.hideWidget()
         self.mappedInletWidget.hideWidget()
 
 

gui/Pages/Makefile.am
@@ -36,6 +36,7 @@ BodyForcesModel.py \
 BodyForcesView.py \
 BoundaryConditionsCoalInletView.py \
 BoundaryConditionsCompressibleOutletView.py \
+BoundaryConditionsConvectiveInletView.py \
 BoundaryConditionsElectricalView.py \
 BoundaryConditionsExternalHeadLossesView.py \
 BoundaryConditionsMappedInletView.py \
@@ -155,6 +156,7 @@ BatchRunningStopByIterationDialogForm.ui \
 BodyForcesForm.ui \
 BoundaryConditionsCoalInletForm.ui \
 BoundaryConditionsCompressibleOutletForm.ui \
+BoundaryConditionsConvectiveInletForm.ui \
 BoundaryConditionsForm.ui \
 BoundaryConditionsElectricalForm.ui \
 BoundaryConditionsExternalHeadLossesForm.ui \

commit 366763f25400dd80250f635574f9642e2d07ab6e

Fix bug in GUI introduced in commit ad2c9d341a (2017-01-23).
Code was wrongly stopped by inconsistency between user and GUI BCs.

src/gui/cs_gui_boundary_conditions.c
@@ -2708,21 +2708,21 @@ void CS_PROCF (uiclve, UICLVE)(const int  *nozppm,
 
       inature2 = itypfb[ifbr];
 
-      int enature2 = inature;
+      int enature2 = inature2;
       if (inature2 == CS_ROUGHWALL)
         enature2 = CS_SMOOTHWALL;
       else if (inature2 == CS_CONVECTIVE_INLET)
-        inature2 = CS_INLET;
+        enature2 = CS_INLET;
 
       if (atmo_auto  && inature2 == 0)
-        inature2 = inature;
+        enature2 = inature;
 
       else if (compr_auto) {
         if (   (compr_auto == CS_INLET  && (   inature2 == CS_ESICF
                                             || inature2 == CS_EPHCF))
             || (compr_auto == CS_OUTLET &&  (   inature2 == CS_SSPCF
                                              || inature2 == CS_SOPCF)))
-          inature2 = inature;
+          enature2 = inature;
       }
 
       if (enature2 != enature)