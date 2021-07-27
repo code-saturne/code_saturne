code-saturne/code_saturne

commit 0f3037d5b8fb754a8acc7e50854a2a411781788c

Split cs_parametric_study into study (driver) and setup (case filter) components.
The case filter can now be used directly from "code_saturne run".

bin/cs_case_domain.py
@@ -127,9 +127,10 @@ class base_domain:
         self.result_dir = None
         self.src_dir = None
 
-        # Notebook definitions and additional user arguments
+        # Notebook and parametric  definitions and additional user arguments
 
         self.notebook = None
+        self.parametric_args = None
         self.kw_args = None
 
         # Working directory and executable
@@ -465,7 +466,7 @@ class domain(base_domain):
 
     #---------------------------------------------------------------------------
 
-    def __set_case_parameters__(self):
+    def __set_case_parameters__(self, update_xml=False):
 
         # We may now import user python script functions if present.
 
@@ -485,25 +486,32 @@ class domain(base_domain):
         # We may now parse the optional XML parameter file
         # now that its path may be built and checked.
 
-        param = self.param
-        if param == None:
-            if os.path.isfile(os.path.join(self.exec_dir, 'setup.xml')):
-                param = 'setup.xml'
+        setup_path = os.path.join(self.exec_dir, "setup.xml")
+        if os.path.isfile(setup_path):
 
-        if param != None:
-            version_str = '2.0'
-            P = cs_xml_reader.Parser(os.path.join(self.exec_dir, param),
-                                     version_str = version_str)
+            # Ensure XML file is up to date as a precaution,
+            # and filter it in case of parametric or notebook arguments.
+
+            case = self.__xml_case_initialize__(setup_path, apply_filters=True)
+            case['xmlfile'] = setup_path
+
+            P = cs_xml_reader.Parser(doc=case.doc)
             params = P.getParams()
             for k in list(params.keys()):
                 self.__dict__[k] = params[k]
 
-            self.param = param
+            case.xmlSaveDocument()
 
             if params['xml_root_name'] == 'NEPTUNE_CFD_GUI':
                 solver_dir = self.package_compute.get_dir("pkglibexecdir")
                 solver_name = "nc_solver" + self.package_compute.config.exeext
                 self.solver_path = os.path.join(solver_dir, solver_name)
+
+        else:
+            msg  = ('Remark:\n'
+                    '  No setup.xml file was provided in the DATA folder.\n'
+                    '  Default settings will be used.\n')
+            print(msg, file = sys.stderr)
 
         # Now override or complete data from the XML file.
 
@@ -551,7 +559,7 @@ class domain(base_domain):
 
     #---------------------------------------------------------------------------
 
-    def __xml_case_initialize__(self, path):
+    def __xml_case_initialize__(self, path, apply_filters=False):
         """
         Build XML case object
         """
@@ -570,7 +578,15 @@ class domain(base_domain):
             from code_saturne.model.XMLinitializeNeptune import XMLinitNeptune
             XMLinitNeptune(case).initialize(preprocess_only)
 
+        if not apply_filters:
+            return case
+
         # Apply changes defined through notebook or parametric options.
+
+        if self.parametric_args:
+            from code_saturne import cs_parametric_setup
+            cs_parametric_setup.update_case_setup(case, self.parametric_args,
+                                                  pkg=self.package)
 
         if self.notebook:
             from code_saturne.model.NotebookModel import NotebookModel
@@ -673,9 +689,34 @@ class domain(base_domain):
             if os.path.isfile(src):
                 shutil.copy2(src, os.path.join(self.exec_dir, f))
 
+        # Fixed parameter name
+
+        if self.param != None:
+            param_base = os.path.basename(self.param)
+            if param_base != "setup.xml":
+                src_path = os.path.join(self.exec_dir, param_base)
+                if os.path.isfile(setup_path):
+                    self.purge_result(setup_path) # in case of previous run here
+                    fmt = ('Warning:\n'
+                           '  Both {0} and {1} exist in\n'
+                           '    {2}.\n'
+                           '  {0} will be used for the computation.\n'
+                           '  Be aware that to follow best practices '
+                           'only one of the two should be present.\n\n')
+                    msg = fmt.format(os.path.basename(self.param),
+                                     os.path.basename(setup_path),
+                                     self.data_dir)
+                    print(msg, file = sys.stderr)
+                try:
+                    if os.path.islink(setup_path):
+                        os.remove(setup_path)
+                    os.symlink(self.param, setup_path)
+                except Exception:
+                    shutil.copy2(src_path, setup_path)
+
         # Now set parameters
 
-        self.__set_case_parameters__()
+        self.__set_case_parameters__(update_xml=True)
 
     #---------------------------------------------------------------------------
 
@@ -686,7 +727,7 @@ class domain(base_domain):
 
         # Now set parameters
 
-        self.__set_case_parameters__()
+        self.__set_case_parameters__(update_xml=False)
 
         # Ensure correct executable is used.
 
@@ -959,46 +1000,6 @@ class domain(base_domain):
                 else:
                     self.symlink(partition_input,
                                  os.path.join(self.exec_dir, 'partition_input'))
-
-        # Fixed parameter name
-
-        setup_path = os.path.join(self.exec_dir, "setup.xml")
-
-        if self.param != None:
-            param_base = os.path.basename(self.param)
-            src_path = os.path.join(self.exec_dir, param_base)
-            default_path = os.path.join(self.data_dir, 'setup.xml')
-            if param_base != "setup.xml":
-                if os.path.isfile(default_path):
-                    self.purge_result(setup_path) # in case of previous run here
-                    fmt = ('Warning:\n'
-                           '  Both {0} and {1} exist in\n'
-                           '    {2}.\n'
-                           '  {0} will be used for the computation.\n'
-                           '  Be aware that to follow best practices '
-                           'only one of the two should be present.\n\n')
-                    msg = fmt.format(os.path.basename(self.param),
-                                     os.path.basename(setup_path),
-                                     self.data_dir)
-                    print(msg, file = sys.stderr)
-                try:
-                    if os.path.islink(setup_path):
-                        os.remove(setup_path)
-                    os.symlink(self.param, setup_path)
-                except Exception:
-                    shutil.copy2(src_path, setup_path)
-
-        # Ensure XML file is up to date as a precaution
-
-        if os.path.isfile(setup_path):
-            case = self.__xml_case_initialize__(setup_path)
-            case['xmlfile'] = setup_path
-            case.xmlSaveDocument()
-        else:
-            msg  = ('Remark:\n'
-                    '  No setup.xml file was provided in the DATA folder.\n'
-                    '  Default settings will be used.\n')
-            print(msg, file = sys.stderr)
 
         if len(err_str) > 0:
             self.error = 'data preparation'

Makefile.am
@@ -266,6 +266,7 @@ bin/cs_create.py \
 bin/cs_debug_wrapper.py \
 bin/cs_debug_symbol.py \
 bin/cs_exec_environment.py \
+bin/cs_parametric_setup.py \
 bin/cs_parametric_study.py \
 bin/cs_studymanager_gui.py \
 bin/cs_trackcvg.py \

bin/cs_case.py
@@ -1794,6 +1794,7 @@ class case:
             force_id = False,
             stages = None,
             notebook_args=None,
+            parametric_args=None,
             kw_args=None):
 
         """
@@ -1907,18 +1908,32 @@ class case:
             msg += '\n'
             sys.stdout.write(msg)
 
+        if parametric_args:
+            msg = 'cs_parametric filter arguments:\n'
+            msg += '  ' + str(parametric_args) + '\n\n'
+            sys.stdout.write(msg)
+
         if kw_args:
             msg = 'Additional user keyword arguments:\n'
             msg += '  ' + str(kw_args) + '\n\n'
             sys.stdout.write(msg)
 
         if notebook_args:
-            for d in (self.domains + self.syr_domains + self.py_domains):
+            for d in (self.domains):
                 if d.notebook == None:
                     d.notebook = notebook_args
                 else:
                     err_str = 'domain: + ' + str(d.name) \
                               + ' notebook already defined\n'
+                    sys.stderr.write(err_str)
+
+        if parametric_args:
+            for d in (self.domains):
+                if d.parametric_args == None:
+                    d.parametric_args = parametric_args
+                else:
+                    err_str = 'domain: + ' + str(d.name) \
+                              + ' parametric_args already defined\n'
                     sys.stderr.write(err_str)
 
         if kw_args:

bin/cs_run.py
@@ -105,7 +105,8 @@ class multi_append(argparse.Action):
         if getattr(namespace, self.dest) == None:
             setattr(namespace, self.dest, list())
         for value in values:
-            getattr(namespace, self.dest).append(value)
+            for sv in value.split():
+                getattr(namespace, self.dest).append(sv)
 
 #-------------------------------------------------------------------------------
 
@@ -146,6 +147,9 @@ def arg_parser(argv):
 
     parser.add_argument("--notebook-args", nargs='*', action = multi_append_kv,
                         help="key=value pairs to pass to user scripts and notebook")
+
+    parser.add_argument("--parametric-args", nargs='*', action = multi_append,
+                        help="key=value pairs to pass to cs_parametric filter")
 
     parser.add_argument("--kw-args", nargs='*', action = multi_append,
                         help="additional keywords to pass to user scripts")
@@ -661,6 +665,7 @@ def run(argv=[], pkg=None, run_args=None, submit_args=None):
                    force_id=r_c['force_id'],
                    stages=stages,
                    notebook_args=options.notebook_args,
+                   parametric_args=options.parametric_args,
                    kw_args=options.kw_args)
 
     if submit_args != None:

bin/cs_xml_reader.py
@@ -106,8 +106,9 @@ class Parser:
     #---------------------------------------------------------------------------
 
     def __init__(self,
-                 fileName,
-                 version_str = None):
+                 filename = 'setup.xml',
+                 doc = None,
+                 version_str = '2.0'):
 
         self.dict = {}
         self.dict['mesh_dir'] = None
@@ -115,16 +116,20 @@ class Parser:
 
         self.root = None
 
-        if fileName == None:
-            return
+        if doc != None:
+            self.doc = doc
 
-        if not os.path.isfile(fileName):
-            raise XMLError('XML file: ' + fileName + ' not found')
+        else:
+            if fileName == None:
+                return
 
-        try:
-            self.doc = minidom.parse(fileName)
-        except Exception:
-            raise XMLError('Error parsing XML file: ' + fileName)
+            if not os.path.isfile(fileName):
+                raise XMLError('XML file: ' + fileName + ' not found')
+
+            try:
+                self.doc = minidom.parse(fileName)
+            except Exception:
+                raise XMLError('Error parsing XML file: ' + fileName)
 
         self.root = self.doc.documentElement
 

commit f72f9c43603a41bf41ae8e7bf98d8290b07ee64c

Fix compilation of MEG functions, broken by commit 0f3037d5 (Yesterday).

bin/cs_case_domain.py
@@ -507,6 +507,8 @@ class domain(base_domain):
                 solver_name = "nc_solver" + self.package_compute.config.exeext
                 self.solver_path = os.path.join(solver_dir, solver_name)
 
+            self.param = "setup.xml"
+
         else:
             msg  = ('Remark:\n'
                     '  No setup.xml file was provided in the DATA folder.\n'
@@ -798,7 +800,8 @@ class domain(base_domain):
             if self.exec_solver and len(src_files) > 0:
                 needs_comp = True
 
-        if self.param != None:
+        setup_path = os.path.join(self.exec_dir, "setup.xml")
+        if os.path.isfile(setup_path):
             fp = os.path.join(self.exec_dir, self.param)
             case = self.__xml_case_initialize__(fp)
 