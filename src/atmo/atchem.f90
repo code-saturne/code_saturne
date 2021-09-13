code-saturne/code_saturne

commit f7d1fd911e8ed96875abf92bf5bf516210e06f9b

Switch to F2008. Atmo: move chemistry and aerosol profiles to C.
Fix loop order in some chemistry subroutines.

src/atmo/atchem.f90
@@ -81,10 +81,6 @@ double precision dtchemmax
 !> latitude and longitude in degres
 double precision, save ::  lat, lon
 
-!> logical unit of the concentration profiles file
-integer, save         ::  impmec
-!> name of the concentration profiles file
-character(len=10), save    ::  ficmec
 !> number of time steps for the concentration profiles file
 integer, save         ::  nbchim
 !> number of altitudes for the concentration profiles file
@@ -109,7 +105,114 @@ integer, save :: iprofc(nozppm)
 
 !> \}
 
+  interface
+
+    !---------------------------------------------------------------------------
+
+    ! Interface to C function returning a chemistry concentration file name
+
+    subroutine cs_f_atmo_get_chem_conc_file_name(f_name_max, f_name, f_name_len)  &
+      bind(C, name='cs_f_atmo_get_chem_conc_file_name')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      integer(c_int), value       :: f_name_max
+      type(c_ptr), intent(out)    :: f_name
+      integer(c_int), intent(out) :: f_name_len
+    end subroutine cs_f_atmo_get_chem_conc_file_name
+
+    !---------------------------------------------------------------------------
+
+    ! Interface to C function returning a aerosol concentration file name
+
+    subroutine cs_f_atmo_get_aero_conc_file_name(f_name_max, f_name, f_name_len)  &
+      bind(C, name='cs_f_atmo_get_aero_conc_file_name')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      integer(c_int), value       :: f_name_max
+      type(c_ptr), intent(out)    :: f_name
+      integer(c_int), intent(out) :: f_name_len
+    end subroutine cs_f_atmo_get_aero_conc_file_name
+
+
+  end interface
+
 contains
+
+!=============================================================================
+
+!> \brief Return chemistry concentration file name
+
+!> \param[out]  name   chemistry concentration file name
+
+subroutine atmo_get_chem_conc_file_name(name)
+
+  use, intrinsic :: iso_c_binding
+  implicit none
+
+  ! Arguments
+
+  character(len=*), intent(out) :: name
+
+  ! Local variables
+
+  integer :: i
+  integer(c_int) :: name_max, c_name_len
+  type(c_ptr) :: c_name_p
+  character(kind=c_char, len=1), dimension(:), pointer :: c_name
+
+  name_max = len(name)
+
+  call cs_f_atmo_get_chem_conc_file_name(name_max, c_name_p, c_name_len)
+  call c_f_pointer(c_name_p, c_name, [c_name_len])
+
+  do i = 1, c_name_len
+    name(i:i) = c_name(i)
+  enddo
+  do i = 1, c_name_len + 1, name_max
+    name(i:i) = ' '
+  enddo
+
+  return
+
+end subroutine atmo_get_chem_conc_file_name
+
+!=============================================================================
+
+!> \brief Return aerosol concentration file name
+
+!> \param[out]  name   aerosol concentration file name
+
+subroutine atmo_get_aero_conc_file_name(name)
+
+  use, intrinsic :: iso_c_binding
+  implicit none
+
+  ! Arguments
+
+  character(len=*), intent(out) :: name
+
+  ! Local variables
+
+  integer :: i
+  integer(c_int) :: name_max, c_name_len
+  type(c_ptr) :: c_name_p
+  character(kind=c_char, len=1), dimension(:), pointer :: c_name
+
+  name_max = len(name)
+
+  call cs_f_atmo_get_aero_conc_file_name(name_max, c_name_p, c_name_len)
+  call c_f_pointer(c_name_p, c_name, [c_name_len])
+
+  do i = 1, c_name_len
+    name(i:i) = c_name(i)
+  enddo
+  do i = 1, c_name_len + 1, name_max
+    name(i:i) = ' '
+  enddo
+
+  return
+
+end subroutine atmo_get_aero_conc_file_name
 
 !=============================================================================
 !> \brief Allocate memory

config/cs_auto_flags.sh
@@ -1002,7 +1002,7 @@ if test "$?" = "0" ; then
   test -n "$cs_fc_vers_patch" || cs_fc_vers_patch=0
 
   # Default compiler flags
-  fcflags_default="-x f95-cpp-input -Wall -pedantic-errors -std=f2003"
+  fcflags_default="-x f95-cpp-input -Wall -pedantic-errors -std=f2008"
   fcflags_default_dbg="-g -fcheck=bounds"
   fcflags_default_opt="-O"
   fcflags_default_hot="-O2"

src/atmo/atimbr.f90
@@ -764,7 +764,6 @@ implicit none
 character(line_len) :: a_file
 character(line_len), dimension(:), allocatable :: the_list
 integer unilog
-parameter (unilog = 10)
 character(line_len) :: current_line
 integer l_iostat, l_stat
 integer counter
@@ -772,7 +771,7 @@ integer counter
 ! ----------------------------------------------------------------
 ! executables
 ! ----------------------------------------------------------------
-open(unilog,file=imbrication_files_list,status='old', &
+open(newunit=unilog,file=imbrication_files_list,status='old', &
      form='formatted',iostat=l_iostat)
 
 counter = 0
@@ -785,7 +784,7 @@ number_of_files = counter
 
 allocate(the_list(number_of_files),stat=l_stat)
 ! find_next closes the file when reaching the end
-open(unilog,file=imbrication_files_list,status='old',&
+open(newunit=unilog,file=imbrication_files_list,status='old',&
      form='formatted',iostat=l_iostat)
 counter = 0
 do while (.true.)
@@ -871,7 +870,6 @@ subroutine read_meteo_file(meteo_file)
 !
 character(line_len) :: meteo_file
 integer unilog
-parameter (unilog = 10)
 character(line_len) :: current_line
 integer l_iostat
 integer first, last
@@ -891,7 +889,7 @@ integer i
 ! ----------------------------------------------------------------
 file_count = file_count + 1
 
-open(unilog,file=meteo_file,status='old',form='formatted',iostat=l_iostat)
+open(newunit=unilog,file=meteo_file,status='old',form='formatted',iostat=l_iostat)
 count_lines = 0
 
 ! --------------------------------------------------------------
@@ -997,7 +995,7 @@ endif
 call bounds(meteo_file,len(meteo_file),first,last)
 if (imbrication_verbose) write(nfecra,*) "reopening the file'", &
      meteo_file(first:last),"'"
-open(unilog,file=meteo_file,status='old',form='formatted',iostat=l_iostat)
+open(newunit=unilog,file=meteo_file,status='old',form='formatted',iostat=l_iostat)
 if (l_iostat.ne.0) then
   call bounds(meteo_file,len(meteo_file),first,last)
   write(nfecra,*) "read_meteo_file could not open file '", &

src/atmo/atleca.f90
@@ -62,7 +62,9 @@ implicit none
 
 integer    isc, f_id
 integer    jsp, jb
+integer    impmea
 character  label*80
+character  ficmea*80
 
 !================================================================================
 ! ALLOCATE
@@ -96,7 +98,8 @@ if (init_aero_with_lib) then
 else
 
   ! Read from file
-  open(impmea,file=ficmea,status='old')
+  call atmo_get_aero_conc_file_name(ficmea)
+  open(newunit=impmea,file=ficmea,status='old')
   ! Reading aerosol numbers
   do jb = 1, n_aer
     read(impmea,*) dlconc0(nlayer_aer*n_aer+jb)

src/atmo/atlecc.f90
@@ -70,6 +70,7 @@ integer           imode
 ! Local variables
 
 logical, save :: switch_to_labels
+integer impmec
 integer f_id
 integer itp, ii, ios, k
 integer sjday,minute
@@ -82,6 +83,8 @@ character(len=1) :: csaute
 double precision  zconctemp(nespgi+1)
 ! names of species in the initialization file
 character(len=80), dimension(:), allocatable :: labels
+! name of the chemistry concentration file
+character(len=80) :: ficmec
 
 !===============================================================================
 
@@ -99,7 +102,8 @@ else
 
 csaute = '/'
 ! --> Opens the concentration profiles file
-open ( unit=impmec, file=ficmec, status='old', iostat=ios, err=99 )
+call atmo_get_chem_conc_file_name(ficmec)
+open ( newunit=impmec, file=ficmec, status='old', iostat=ios, err=99 )
 rewind ( unit=impmec,err=99 )
 
 itp = 0

src/atmo/atlecm.f90
@@ -61,6 +61,7 @@ integer           imode
 
 character(len=80) :: ficmet
 
+integer impmet
 integer itp, ii, ios, k
 integer year, quant,hour,minute, month, day
 integer ih2o
@@ -92,7 +93,7 @@ csaute = '/'
 ! --> Opens the meteo file
 call atmo_get_meteo_file_name(ficmet)
 
-open (unit=impmet, file=ficmet,                                  &
+open (newunit=impmet, file=ficmet,                                  &
      status='old', form='formatted', access='sequential',       &
      iostat=ios, err=99)
 rewind(unit=impmet, err=99)

src/atmo/atvarp.f90
@@ -157,9 +157,8 @@ if (nogaseouschemistry .and. iaerosol.eq.CS_ATMO_AEROSOL_OFF) ichemistry = 0
 
 if (ifilechemistry.ge.1) then
 
-  ! logical unit and name of the chemical profiles file
-  impmec = 27
-  ficmec = 'chemistry'
+  ! Set the name of the chemical profiles file
+  call cs_atmo_set_chem_conc_file_name('chemistry')
 
   ! Initialization of the chemical scheme
   ! quasi steady equilibrium NOx scheme with 4 species and 5 reactions
@@ -379,9 +378,8 @@ endif
 ! Atmospheric aerosol chemistry
 if (iaerosol.ne.CS_ATMO_AEROSOL_OFF) then
 
-  ! logical unit and name of the chemical profiles file
-  impmea=28
-  ficmea='aerosols'
+  ! Set the name of the chemical profiles file
+  call cs_atmo_set_aero_conc_file_name('aerosols')
 
   ! Verification
   if (ifilechemistry.ne.4) then

src/atmo/chem_roschem.f90
@@ -107,12 +107,12 @@ endif
 !------------------------------------------------------------------------
 !*    4. Computes K1    system: DLmat * K1 = DLb1
 
-do ji = 1, nespg
-  dlb1(ji) = dlr(ji)
-  do jj = 1, nespg
+do jj = 1, nespg
+  dlb1(jj) = dlr(jj)
+  do ji = 1, nespg
     dlmat(ji,jj) = -igamma*dlstep*dldrdc(ji,jj)
   enddo
-  dlmat(ji,ji) = 1.d0 + dlmat(ji,ji)
+  dlmat(jj,jj) = 1.d0 + dlmat(jj,jj)
 enddo
 
 call cs_solvlin (0,dlmat,dlmatlu,dlk1,dlb1)

commit 21d740c348a7c2941c0242c243b58917894517a5

Atmo chemistry: fix bug introduced in commit f7d1fd91 (2021-05-26).

src/atmo/atchem.f90
@@ -165,7 +165,7 @@ subroutine atmo_get_chem_conc_file_name(name)
   do i = 1, c_name_len
     name(i:i) = c_name(i)
   enddo
-  do i = 1, c_name_len + 1, name_max
+  do i = c_name_len + 1, name_max
     name(i:i) = ' '
   enddo
 
@@ -203,7 +203,7 @@ subroutine atmo_get_aero_conc_file_name(name)
   do i = 1, c_name_len
     name(i:i) = c_name(i)
   enddo
-  do i = 1, c_name_len + 1, name_max
+  do i = c_name_len + 1, name_max
     name(i:i) = ' '
   enddo
 