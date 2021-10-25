code-saturne/code_saturne

commit 90eba1bd9e36c4e1403027cdce2cfc72c6892006

Fix run for cases with a run.cfg but no SRC folder.

bin/cs_case.py
@@ -160,17 +160,18 @@ def get_case_dir(case=None, param=None, coupling=None, id=None):
             data = os.path.join(casedir, 'DATA')
             src = os.path.join(casedir, 'SRC')
         else:
-            casedir = cwd
-            while os.path.basename(casedir):
-                data = os.path.join(casedir, 'DATA')
-                src = os.path.join(casedir, 'SRC')
+            testdir = cwd
+            while os.path.basename(testdir):
+                data = os.path.join(testdir, 'DATA')
+                src = os.path.join(testdir, 'SRC')
                 cfg = os.path.join(data, 'run.cfg')
                 if (os.path.isdir(data) and os.path.isdir(src)) \
                    or os.path.isfile(cfg):
+                    casedir = testdir
                     break
-                casedir = os.path.split(casedir)[0]
+                testdir = os.path.split(casedir)[0]
 
-        if not (os.path.isdir(data) and os.path.isdir(src)):
+        if not (os.path.isdir(data)):
             casedir = None
 
     # Return casedir or None

commit 38af0b7111fa4e086ac470e9771ff2abdd5d3b06

Fix run script failure introduced by commit 90eba1bd (Oct. 22 2021).

bin/cs_case.py
@@ -169,7 +169,7 @@ def get_case_dir(case=None, param=None, coupling=None, id=None):
                    or os.path.isfile(cfg):
                     casedir = testdir
                     break
-                testdir = os.path.split(casedir)[0]
+                testdir = os.path.split(testdir)[0]
 
         if not (os.path.isdir(data)):
             casedir = None