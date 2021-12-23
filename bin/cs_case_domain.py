code-saturne/code_saturne

commit ff079926a6ab5c1fa1938f733489d3ab8087f82e

CUDA fixes and minor additions.

bin/cs_compile.py
@@ -337,6 +337,7 @@ class cs_compile(object):
 
     def compile_src(self, base_name=None, src_list=None,
                     opt_cflags=None, opt_cxxflags=None, opt_fcflags=None,
+                    opt_nvccflags=None,
                     keep_going=False,
                     stdout=sys.stdout, stderr=sys.stderr):
         """
@@ -427,7 +428,7 @@ class cs_compile(object):
             for d in c_include_dirs:
                 cmd += ["-I", d]
             cmd.append('-DHAVE_CONFIG_H')
-            cmd += self.get_flags('nvccflags', base_name=base_name)
+            cmd += self.get_flags('cppflags', base_name=base_name)
             cmd += separate_args(pkg.config.flags['nvccflags'])
             cmd += ["-c", f]
             if run_command(cmd, pkg=pkg, echo=True,

config/cs_auto_flags.sh
@@ -1285,7 +1285,7 @@ if test "x$NVCC" != "x" ; then
 
   echo "compiler '${NVCC}' version ${cs_nvcc_version}"
 
-  cs_ac_nvcc_version="`$NVCC -version 2>&1 |grep 'release' | head -1`"
+  cs_ac_nvcc_version="`$NVCC --version 2>&1 |grep 'release' | head -1`"
 
   # Default compiler flags
   nvccflags_default=""

m4/cs_cuda.m4
@@ -59,21 +59,27 @@ if test "x$cs_have_cuda" != "xno" ; then
         [CUDA_LIBS+="64"])
   CUDA_LIBS+=" -lcudart"
 
-  # Try to detect available architectures
+  # Try to detect available architectures.
+  # As of late 2021, we do not care to support CUDA versions older than 9
+  # (and even then,target machines should be at least Volta,
+  # though developping/debugging on local machines using CUDA 9 remains useful).
 
   if test "$CUDA_ARCH_NUM" = ""; then
-    CUDA_ARCH_NUM="35 37 60 70"
+    # CUDA_ARCH_NUM="60 61 62 70 72 75 80 86"
+    CUDA_ARCH_NUM="60 70 80"
   fi
 
   NVCCFLAGS="-ccbin $CXX -DHAVE_CONFIG_H"  # wrap C++ compiler arount nvcc
-  touch conftest.cu
-  for cu_arch in "$CUDA_ARCH_NUM"; do
-    $NVCC --dryrun -c conftest.cu -o conftest.o -gencode arch=compute_${cu_arch},code=sm_${cu_arch} >/dev/null 2>&1
-    if test $? -eq 0; then
-      NVCCFLAGS="${NVCCFLAGS} -gencode arch=compute_${cu_arch},code=sm_${cu_arch}"
-    fi
-  done
-  rm -f conftest.cu conftest.o
+  if test "$CUDA_ARCH_NUM" != ""; then
+    touch conftest.cu
+    for cu_arch in $CUDA_ARCH_NUM; do
+      $NVCC --dryrun -c conftest.cu -o conftest.o -gencode arch=compute_${cu_arch},code=sm_${cu_arch} >/dev/null 2>&1
+      if test $? -eq 0; then
+        NVCCFLAGS="${NVCCFLAGS} -gencode arch=compute_${cu_arch},code=sm_${cu_arch}"
+      fi
+    done
+    rm -f conftest.cu conftest.o
+  fi
 
   NVCCFLAGS="${NVCCFLAGS} --maxrregcount=64 -Xptxas -v"
 

src/base/cs_base_accel.cxx
@@ -255,7 +255,10 @@ cs_malloc_hd(cs_alloc_mode_t   mode,
 
   /* Return pointer to allocated memory */
 
-  return me.host_ptr;
+  if (me.host_ptr != NULL)
+    return me.host_ptr;
+  else
+    return me.device_ptr;
 }
 
 /*----------------------------------------------------------------------------*/
@@ -331,6 +334,8 @@ cs_realloc_hd(void            *ptr,
     me.size = new_size;
     _hd_alloc_map.erase(ptr);
     _hd_alloc_map[me.host_ptr] = me;
+
+    ret_ptr = me.host_ptr;
   }
   else {
     ret_ptr = cs_malloc_hd(mode, 1, me.size,
@@ -808,6 +813,9 @@ cs_sync_h2d(const void  *ptr)
     return;
 
   _cs_base_accel_mem_map  me = _hd_alloc_map[ptr];
+
+  if (me.device_ptr == NULL)
+    me.device_ptr = const_cast<void *>(cs_get_device_ptr_const(ptr));
 
 #if defined(HAVE_CUDA)
 

src/base/cs_base_cuda.cu
@@ -71,6 +71,12 @@ BEGIN_C_DECLS
 
 int  cs_glob_cuda_device_id = -1;
 
+/* Other device parameters */
+
+int  cs_glob_cuda_max_threads_per_block = -1;
+int  cs_glob_cuda_max_block_size = -1;
+int  cs_glob_cuda_max_blocks = -1;
+
 /*============================================================================
  * Private function definitions
  *============================================================================*/
@@ -470,11 +476,13 @@ cs_base_cuda_device_info(cs_log_t  log_id)
         (log_id,
          _("                       Compute capability: %d.%d\n"
            "                       Memory: %llu %s\n"
+           "                       Multiprocessors: %d\n"
            "                       Integrated: %d\n"
            "                       Can map host memory: %d\n"
            "                       Compute mode: %s\n"),
          prop.major, prop.minor,
          mem, _("MB"),
+         prop.multiProcessorCount,
          prop.integrated,
          prop.canMapHostMemory, mode_name);
 
@@ -502,6 +510,24 @@ cs_base_cuda_version_info(cs_log_t  log_id)
   if (cudaRuntimeGetVersion(&runtime_version) == cudaSuccess)
     cs_log_printf(log_id,
                   "  %s%d\n", _("CUDA runtime:        "), runtime_version);
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Log information on CUDA compiler.
+ *
+ * \param[in]  log_id  id of log file in which to print information
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_base_cuda_compiler_info(cs_log_t  log_id)
+{
+  cs_log_printf(log_id,
+                "    %s%d\n", _("CUDA compiler:     "),
+                __CUDACC_VER_MAJOR__,
+                __CUDACC_VER_MINOR__,
+                __CUDACC_VER_BUILD__);
 }
 
 /*----------------------------------------------------------------------------*/
@@ -552,6 +578,16 @@ cs_base_cuda_select_default_device(void)
   }
 
   cs_glob_cuda_device_id = device_id;
+
+  /* Also query some device properties */
+
+  struct cudaDeviceProp prop;
+  CS_CUDA_CHECK(cudaGetDeviceProperties(&prop, device_id));
+  cs_glob_cuda_max_threads_per_block = prop.maxThreadsPerBlock;
+  cs_glob_cuda_max_block_size = prop.maxThreadsPerMultiProcessor;
+  cs_glob_cuda_max_blocks
+    =   prop.multiProcessorCount
+      * (prop.maxThreadsPerMultiProcessor / prop.maxThreadsPerBlock);
 
   return device_id;
 }

src/base/cs_base_cuda.h
@@ -61,6 +61,12 @@
     } \
   }
 
+/* For all current compute capabilities, the warp size is 32; If it ever
+   changes, it can be obtained through cudaDeviceProp, so we could then
+   replace this macro with a global variable */
+
+#define CS_CUDA_WARP_SIZE 32
+
 /*----------------------------------------------------------------------------*/
 
 BEGIN_C_DECLS
@@ -72,6 +78,14 @@ BEGIN_C_DECLS
 /*=============================================================================
  * Global variable definitions
  *============================================================================*/
+
+extern int  cs_glob_cuda_device_id;
+
+/* Other device parameters */
+
+extern int  cs_glob_cuda_max_threads_per_block;
+extern int  cs_glob_cuda_max_block_size;
+extern int  cs_glob_cuda_max_blocks;
 
 /*============================================================================
  * Semi-private function prototypes
@@ -384,6 +398,17 @@ cs_base_cuda_device_info(cs_log_t  log_id);
 
 void
 cs_base_cuda_version_info(cs_log_t  log_id);
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Log information on CUDA compiler.
+ *
+ * \param[in]  log_id  id of log file in which to print information
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_base_cuda_compiler_info(cs_log_t  log_id);
 
 /*----------------------------------------------------------------------------*/
 /*!

tests/Makefile.am
@@ -197,7 +197,7 @@ cs_gpu_test$(EXEEXT): $(top_srcdir)/tests/cs_gpu_test.c $(top_srcdir)/tests/cs_g
 	PYTHONPATH=$(top_srcdir)/bin \
 	$(PYTHON) -B $(top_srcdir)/build-aux/cs_compile_build.py \
 	-o cs_gpu_test \
-	$(top_srcdir)/tests/cs_gpu_test.c
+	$(top_srcdir)/tests/cs_gpu_test.c \
 	$(top_srcdir)/tests/cs_gpu_cuda_test.cu
 
 endif

commit 34af724fb9da1550fd2901139bb397ef4090eb73

Fix logging of compilation messages, broken by commit ff079926 (Dec 16 2021).

bin/cs_case_domain.py
@@ -455,6 +455,7 @@ class domain(base_domain):
         self.compile_cflags = None
         self.compile_cxxflags = None
         self.compile_fcflags = None
+        self.compile_nvccflags = None
         self.compile_libs = None
 
         # Adaptation using HOMARD
@@ -883,6 +884,7 @@ class domain(base_domain):
                                                  self.compile_cflags,
                                                  self.compile_cxxflags,
                                                  self.compile_fcflags,
+                                                 self.compile_nvccflags,
                                                  self.compile_libs,
                                                  keep_going=True,
                                                  stdout=log,

bin/cs_compile.py
@@ -586,7 +586,8 @@ class cs_compile(object):
 
     def compile_and_link(self, base_name, srcdir, destdir=None, src_list=None,
                          opt_cflags=None, opt_cxxflags=None, opt_fcflags=None,
-                         opt_libs=None, force_link=False, keep_going=False,
+                         opt_nvccflags=None, opt_libs=None, force_link=False,
+                         keep_going=False,
                          stdout=sys.stdout, stderr=sys.stderr):
         """
         Compilation and link function.
@@ -621,7 +622,8 @@ class cs_compile(object):
             src_list.append(os.path.join(srcdir, f))
 
         retval, obj_list = self.compile_src(base_name, src_list,
-                                            opt_cflags, opt_cxxflags, opt_fcflags,
+                                            opt_cflags, opt_cxxflags,
+                                            opt_fcflags, opt_nvccflags,
                                             keep_going, stdout, stderr)
 
         if retval == 0 and (force_link or len(obj_list)) > 0:
@@ -645,6 +647,7 @@ class cs_compile(object):
 
 def compile_and_link(pkg, base_name, srcdir, destdir=None,
                      opt_cflags=None, opt_cxxflags=None, opt_fcflags=None,
+                     opt_nvccflags=None,
                      opt_libs=None, force_link=False, keep_going=False,
                      stdout=sys.stdout, stderr=sys.stderr):
     """
@@ -658,6 +661,7 @@ def compile_and_link(pkg, base_name, srcdir, destdir=None,
                                  opt_cflags=opt_cflags,
                                  opt_cxxflags=opt_cxxflags,
                                  opt_fcflags=opt_fcflags,
+                                 opt_nvccflags=opt_nvccflags,
                                  opt_libs=opt_libs,
                                  force_link=force_link,
                                  keep_going=keep_going,
@@ -699,6 +703,7 @@ def main(argv, pkg):
                                opt_cflags=cflags,
                                opt_cxxflags=cxxflags,
                                opt_fcflags=fcflags,
+                               opt_nvccflags=opt_nvccflags,
                                opt_libs=libs,
                                force_link=force_link,
                                keep_going=keep_going)