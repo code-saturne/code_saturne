code-saturne/code_saturne

commit 434522ee9541c48016cde4eda4a1f092bb8ff2bc

Improve consistency of property field output settings with minimal XML file.

src/base/cs_parameters.c
@@ -1502,12 +1502,13 @@ cs_parameters_add_boundary_temperature(void)
 void
 cs_parameters_output_complete(void)
 {
+  const int k_log = cs_field_key_id("log");
+  const int k_vis = cs_field_key_id("post_vis");
+
   /* Complete default settings for moment fields */
 
   const int n_moments = cs_time_moment_n_moments();
   if (n_moments > 0) {
-    int k_log = cs_field_key_id("log");
-    int k_vis = cs_field_key_id("post_vis");
     for (int m_id = 0; m_id < n_moments; m_id++) {
       cs_field_t *f = cs_time_moment_get_field(m_id);
       if (f != NULL) {
@@ -1518,6 +1519,23 @@ cs_parameters_output_complete(void)
         if (cs_field_is_key_set(f, k_log) == false)
           cs_field_set_key_int(f, k_vis, 1);
       }
+    }
+  }
+
+  /* Complete settings for property fields; properties
+     which are hidden should already have had their settings
+     forced at this stage, so should not be changed here. */
+
+  const int n_fields = cs_field_n_fields();
+  for (int f_id = 0; f_id < n_fields; f_id++) {
+    cs_field_t *f = cs_field_by_id(f_id);
+    if (f->type & CS_FIELD_PROPERTY) {
+      if (cs_field_is_key_set(f, k_vis) == false) {
+        int flag = CS_POST_ON_LOCATION;
+        cs_field_set_key_int(f, k_vis, flag);
+      }
+      if (cs_field_is_key_set(f, k_log) == false)
+        cs_field_set_key_int(f, k_vis, 1);
     }
   }
 }

src/base/fldprp.f90
@@ -443,10 +443,7 @@ subroutine hide_property &
 ! Module files
 !===============================================================================
 
-use paramx
-use dimens
 use entsor
-use numvar
 use field
 
 !===============================================================================
@@ -518,7 +515,7 @@ integer, intent(out)         :: f_id
 
 ! Local variables
 
-integer  type_flag, post_flag, location_id
+integer  type_flag, location_id
 
 character(len=len_trim(name)+1, kind=c_char) :: c_name
 integer(c_int) :: c_type_flag
@@ -528,7 +525,6 @@ logical(c_bool) :: c_has_previous
 !===============================================================================
 
 type_flag = FIELD_INTENSIVE + FIELD_PROPERTY
-post_flag = POST_ON_LOCATION
 location_id = 1 ! variables defined on cells
 
 ! Test if the field has already been defined
@@ -554,9 +550,6 @@ call cs_physical_property_define_from_field(c_name, c_type_flag, &
   c_location_id, dim, c_has_previous)
 
 f_id = cs_physical_property_field_id_by_name(c_name)
-
-call field_set_key_int(f_id, keyvis, post_flag)
-call field_set_key_int(f_id, keylog, 1)
 
 if (len(trim(label)).gt.0) then
   call field_set_key_str(f_id, keylbl, trim(label))

src/base/modini.f90
@@ -121,17 +121,11 @@ endif
 
 if (irovar.eq.0) then
   call hide_property(icrom)
-  call field_set_key_int(ibrom, keylog, 0)
-else ! irovar.eq.1
-  call field_set_key_int(icrom, keylog, 1)
-  call field_set_key_int(icrom, keyvis, 1)
+  call hide_property(ibrom)
 endif
 
 if (ivivar.eq.0) then
   call hide_property(iviscl)
-else ! ivivar.eq.1
-  call field_set_key_int(iviscl, keylog, 1)
-  call field_set_key_int(iviscl, keyvis, 1)
 endif
 
 if (idtvar.lt.0) then

commit a6c87d3ccc1b5a8c501e108c0da3a4c885e3d688

Fix for postprocessing and logging of time moments, broken by commit 434522ee (July 7, 2021).

src/base/cs_parameters.c
@@ -1532,7 +1532,7 @@ cs_parameters_output_complete(void)
           cs_field_set_key_int(f, k_vis, flag);
         }
         if (cs_field_is_key_set(f, k_log) == false)
-          cs_field_set_key_int(f, k_vis, 1);
+          cs_field_set_key_int(f, k_log, 1);
       }
     }
   }