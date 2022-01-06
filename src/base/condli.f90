code-saturne/code_saturne

commit 04f4a78549d71c880e8afbf0ead85c702afa3a22

GUI reader: some cleanup, and replace stdtcl with C function.
Also, standard turbulence inlet types are now handled for electric arcs,
as there seemed to be no good reason for them not to be.
They could probably be used in more cases, except for combustion
cases where conversion from mass flux definitions to velocity is done later.

src/base/condli.f90
@@ -370,30 +370,16 @@ rijipb => null()
 
 call precli(nvar, icodcl, rcodcl)
 
-!     - Interface Code_Saturne
-!       ======================
-
-! N.B. Zones de face de bord : on utilise provisoirement les zones des
-!    physiques particulieres, meme sans physique particuliere
-!    -> sera modifie lors de la restructuration des zones de bord
+! Base definitions from the GUI
+! =============================
 
 call uiclim &
   ( nozppm,                                                        &
-    iqimp,  icalke, ientat, ientcp, inmoxy, ientox,                &
+    iqimp,  ientat, ientcp, inmoxy, ientox,                        &
     ientfu, ientgb, ientgf, iprofm, iautom,                        &
     itypfb, izfppp, icodcl,                                        &
-    qimp,   qimpat, qimpcp, dh,     xintur,                        &
+    qimp,   qimpat, qimpcp,                                        &
     timpat, timpcp, tkent ,  fment, distch, nvar, rcodcl)
-
-if (ippmod(iphpar).eq.0.or.ippmod(igmix).ge.0.or.ippmod(icompf).ge.0) then
-
-  ! No specific physical model here
-
-  call stdtcl &
-    ( nbzppm , nozppm ,                                              &
-      icalke , dh , xintur, itypfb , izfppp , rcodcl )
-
-endif
 
 call cs_boundary_conditions_complete(nvar, itypfb, icodcl, rcodcl)
 
@@ -4971,10 +4957,10 @@ call precli(nvar, icodcl, rcodcl)
 
 call uiclim &
   ( nozppm,                                                        &
-    iqimp,  icalke, ientat, ientcp, inmoxy, ientox,                &
+    iqimp,  ientat, ientcp, inmoxy, ientox,                        &
     ientfu, ientgb, ientgf, iprofm, iautom,                        &
     itypfb, izfppp, icodcl,                                        &
-    qimp,   qimpat, qimpcp, dh,     xintur,                        &
+    qimp,   qimpat, qimpcp,                                        &
     timpat, timpcp, tkent ,  fment, distch, nvar, rcodcl)
 
 !     - Sous-programme utilisateur

src/gui/cs_gui_boundary_conditions.c
@@ -70,6 +70,7 @@
 #include "cs_thermal_model.h"
 #include "cs_timer.h"
 #include "cs_tree.h"
+#include "cs_turbulence_bc.h"
 #include "cs_turbulence_model.h"
 #include "cs_parall.h"
 #include "cs_elec_model.h"
@@ -767,30 +768,29 @@ _dof_meg_vel_profile(cs_lnum_t         n_elts,
 
   else if (normalization == 1) {
 
-    /* For general models, apply scaling to convert from mass flow
-       to velocity; For specific models, scaling is done later. */
+    /* In all cases, update total mass flow if given by a MEG function
+       (not be needed by all models, but provided in a consistent manner) */
 
-    if (   cs_glob_physical_model_flag[CS_PHYSICAL_MODEL_FLAG] == 0
-        || cs_glob_physical_model_flag[CS_GAS_MIX] >= 0
-        || cs_glob_physical_model_flag[CS_COMPRESSIBLE] >= 0) {
+    if (c->norm_type == 11)
+      boundaries->qimp[c->zone->id - 1] = mass_flow;
 
-      assert(   n_elts == c->zone->n_elts
-             && elt_ids == c->zone->elt_ids);
+    /* For most models, apply scaling to convert from mass flow
+       to velocity; For combustion models, scaling is done later. */
 
+    bool scale_velocity = true;
+
+    for (int i = CS_COMBUSTION_3PT; i <= CS_COMBUSTION_FUEL; i++) {
+      if (cs_glob_physical_model_flag[i] >= 0)
+        scale_velocity = false;
+    }
+
+    if (scale_velocity)
       _b_mass_flow_to_vel(c->zone,
                           dense_output,
                           mass_flow,
                           c->c_pr,
                           c->c_tk,
                           retval);
-
-    }
-
-    /* In all cases, update total mass flow if given by a MEG function
-       (not be needed by all models, but provided in a consistent manner) */
-
-    if (c->norm_type == 11)
-      boundaries->qimp[c->zone->id - 1] = mass_flow;
 
   }
 }
@@ -2243,6 +2243,110 @@ _init_zones(const cs_lnum_t   n_b_faces,
 
 }
 
+/*----------------------------------------------------------------------------
+ * Define automatic turbulence values.
+ *
+ * parameters:
+ *   rdcodcl <-- boundary condition values
+ *----------------------------------------------------------------------------*/
+
+static void
+_standard_turbulence_bcs(cs_real_t  *rcodcl)
+{
+  const cs_mesh_t *m = cs_glob_mesh;
+  const cs_lnum_t n_b_faces = m->n_b_faces;
+  const cs_lnum_t *b_face_cells = m->b_face_cells;
+
+  const int var_key_id = cs_field_key_id("variable_id");
+
+  const cs_real_t *b_rho = CS_F_(rho_b)->val;
+  const cs_real_t *c_mu = CS_F_(mu)->val;
+
+  cs_lnum_t ivar_u = cs_field_get_key_int(CS_F_(vel), var_key_id) -1;
+
+  const cs_real_t  *_rcodcl_v[3];
+  for (int coo_id = 0; coo_id < 3; coo_id++)
+    _rcodcl_v[coo_id] = rcodcl + (ivar_u + coo_id)*n_b_faces;
+
+  /* Inlet BC's */
+
+  for (int izone = 0; izone < boundaries->n_zones; izone++) {
+
+    if (boundaries->icalke[izone] > 0) {
+
+      int zone_nbr = boundaries->bc_num[izone];
+      const cs_zone_t *z = cs_boundary_zone_by_id(zone_nbr);
+
+      const cs_real_t dh = boundaries->dh[izone];
+
+      if (boundaries->icalke[izone] == 1) {
+
+        for (cs_lnum_t i = 0; i < z->n_elts; i++) {
+          cs_lnum_t face_id = z->elt_ids[i];
+          cs_lnum_t cell_id = b_face_cells[face_id];
+
+          cs_real_t vel[3] = {_rcodcl_v[0][face_id],
+                              _rcodcl_v[1][face_id],
+                              _rcodcl_v[2][face_id]};
+          cs_real_t uref2 = fmax(cs_math_3_square_norm(vel),
+                                 cs_math_epzero);
+
+          cs_turbulence_bc_inlet_hyd_diam(face_id, uref2, dh,
+                                          b_rho[face_id], c_mu[cell_id],
+                                          rcodcl);
+        }
+
+      }
+      else if (boundaries->icalke[izone] == 2) {
+
+        const cs_real_t t_intensity = boundaries->xintur[izone];
+
+        for (cs_lnum_t i = 0; i < z->n_elts; i++) {
+          cs_lnum_t face_id = z->elt_ids[i];
+
+          cs_real_t vel[3] = {_rcodcl_v[0][face_id],
+                              _rcodcl_v[1][face_id],
+                              _rcodcl_v[2][face_id]};
+          cs_real_t uref2 = fmax(cs_math_3_square_norm(vel),
+                                 cs_math_epzero);
+
+          cs_turbulence_bc_inlet_turb_intensity(face_id, uref2,
+                                                t_intensity, dh,
+                                                rcodcl);
+        }
+
+      }
+
+    } /* End for inlet */
+
+  }
+
+  /* Automatic wall condition for alpha */
+
+  if (cs_glob_turb_model->iturb == CS_TURB_RIJ_EPSILON_EBRSM) {
+
+    cs_lnum_t ivar_alp = cs_field_get_key_int(CS_F_(alp_bl), var_key_id) -1;
+
+    cs_real_t  *_rcodcl_alp = rcodcl + ivar_alp*n_b_faces;
+
+    for (int izone = 0; izone < boundaries->n_zones; izone++) {
+      if (cs_gui_strcmp(boundaries->nature[izone], "wall")) {
+
+        int zone_nbr = boundaries->bc_num[izone];
+        const cs_zone_t *z = cs_boundary_zone_by_id(zone_nbr);
+
+        for (cs_lnum_t i = 0; i < z->n_elts; i++) {
+          cs_lnum_t face_id = z->elt_ids[i];
+          _rcodcl_alp[face_id] = 0.;
+        }
+
+      }
+    }
+
+  }
+
+}
+
 /*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
 
 /*============================================================================
@@ -2262,7 +2366,6 @@ _init_zones(const cs_lnum_t   n_b_faces,
  * integer          nozppm   <-- max number of boundary conditions zone
  * integer          nclpch   <-- number of simulated class per coals
  * integer          iqimp    <-- 1 if mass flow rate is applied
- * integer          icalke   <-- 1 for automatic turbulent boundary conditions
  * integer          ientat   <-- 1 for air temperature boundary conditions (coal)
  * integer          ientcp   <-- 1 for coal temperature boundary conditions (coal)
  * INTEGER          INMOXY   <-- coal: number of oxydant for the current inlet
@@ -2279,8 +2382,6 @@ _init_zones(const cs_lnum_t   n_b_faces,
  * double precision qimp     <-- inlet flow rate
  * double precision qimpat   <-- inlet air flow rate (coal)
  * double precision qimpcp   <-- inlet coal flow rate (coal)
- * double precision dh       <-- hydraulic diameter
- * double precision xintur   <-- turbulent intensity
  * double precision timpat   <-- air temperature boundary conditions (coal)
  * double precision timpcp   <-- inlet coal temperature (coal)
  * double precision tkent    <-- inlet temperature (gas combustion)
@@ -2292,7 +2393,6 @@ _init_zones(const cs_lnum_t   n_b_faces,
 
 void CS_PROCF (uiclim, UICLIM)(const int  *nozppm,
                                int        *iqimp,
-                               int        *icalke,
                                int        *ientat,
                                int        *ientcp,
                                int        *inmoxy,
@@ -2308,8 +2408,6 @@ void CS_PROCF (uiclim, UICLIM)(const int  *nozppm,
                                double     *qimp,
                                double     *qimpat,
                                double     *qimpcp,
-                               double     *dh,
-                               double     *xintur,
                                double     *timpat,
                                double     *timpcp,
                                double     *tkent,
@@ -2367,17 +2465,6 @@ void CS_PROCF (uiclim, UICLIM)(const int  *nozppm,
     bft_printf("---zone %i nature: %s\n", zone_nbr, boundaries->nature[izone]);
     bft_printf("---zone %i number of faces: %i\n", zone_nbr, bz->n_elts);
 #endif
-
-    int wall_type = 1;
-
-    /* Mapped inlet? */
-
-    if (cs_gui_strcmp(boundaries->nature[izone], "wall")) {
-      if (boundaries->rough[izone] >= 0.0)
-        wall_type = 6; //TODO remove and use all roughness wall function
-      else
-        wall_type = 5;
-    }
 
     /* for each field */
     for (int f_id = 0; f_id < n_fields; f_id++) {
@@ -2507,15 +2594,12 @@ void CS_PROCF (uiclim, UICLIM)(const int  *nozppm,
 
       tn_bc = _get_zone_bc_node(tn_bc, izone);
 
-      /* Update the zone's arrays (iqimp, dh, xintur, icalke, qimp,...)
+      /* Update the zone's arrays (iqimp, icalke, qimp,...)
          because they are re-initialized at each time step
          in PRECLI and PPPRCL routines */
 
       /* data by zone */
       iqimp[zone_nbr-1]  = boundaries->iqimp[izone];
-      dh[zone_nbr-1]     = boundaries->dh[izone];
-      xintur[zone_nbr-1] = boundaries->xintur[izone];
-      icalke[zone_nbr-1] = boundaries->icalke[izone];
 
       if (solid_fuels) {
         const cs_combustion_model_t *cm = cs_glob_combustion_model;
@@ -2608,7 +2692,7 @@ void CS_PROCF (uiclim, UICLIM)(const int  *nozppm,
       const int var_key_id = cs_field_key_id("variable_id");
 
       /* turbulent inlet, with formula */
-      if (icalke[zone_nbr-1] == 0) {
+      if (boundaries->icalke[zone_nbr-1] == 0) {
 
         tn_bc = _get_zone_bc_node(tn_bc, izone);
 
@@ -2802,8 +2886,6 @@ void CS_PROCF (uiclim, UICLIM)(const int  *nozppm,
         bft_printf("-----iqimp=%i, qimp=%12.5e \n",
                    iqimp[zone_nbr-1], qimp[zone_nbr-1]);
       }
-      bft_printf("-----icalke=%i, dh=%12.5e, xintur=%12.5e \n",
-                 icalke[zone_nbr-1], dh[zone_nbr-1], xintur[zone_nbr-1]);
 
       if (cs_glob_physical_model_flag[CS_ATMOSPHERIC] > -1) {
         bft_printf("-----iprofm=%i, automatic=%i \n",
@@ -3009,13 +3091,29 @@ void CS_PROCF (uiclim, UICLIM)(const int  *nozppm,
 
   /* Final adjustments */
 
+  /* Automatic turbulence values
+     TODO: spcecify which models do not need this instead of those who do.
+     In most cases, even if a model changes this, doing it in all cases
+     should be safe. */
+
+  bool std_turbulence_bcs = false;
+  if (   cs_glob_physical_model_flag[CS_PHYSICAL_MODEL_FLAG] == 0
+      || cs_glob_physical_model_flag[CS_ELECTRIC_ARCS] >= 0
+      || cs_glob_physical_model_flag[CS_COMPRESSIBLE] >= 0
+      || cs_glob_physical_model_flag[CS_GAS_MIX] >= 0)
+    std_turbulence_bcs = true;
+
+  if (std_turbulence_bcs)
+    _standard_turbulence_bcs(rcodcl);
+
+  /* Mass flow (TODO: merge qimp and qimpat) */
+
   for (int izone = 0; izone < boundaries->n_zones; izone++) {
     qimp[izone] = boundaries->qimp[izone];
-    if (solid_fuels) {
-      /* TODO: merge qimp and qimpat */
+    if (solid_fuels)
       qimpat[izone] = boundaries->qimp[izone];
-    }
   }
+
 }
 
 /*----------------------------------------------------------------------------

src/gui/cs_gui_boundary_conditions.h
@@ -64,7 +64,6 @@ BEGIN_C_DECLS
 
 void CS_PROCF (uiclim, UICLIM)(const int  *nozppm,
                                int        *iqimp,
-                               int        *icalke,
                                int        *ientat,
                                int        *ientcp,
                                int        *inmoxy,
@@ -80,8 +79,6 @@ void CS_PROCF (uiclim, UICLIM)(const int  *nozppm,
                                double     *qimp,
                                double     *qimpat,
                                double     *qimpcp,
-                               double     *dh,
-                               double     *xintur,
                                double     *timpat,
                                double     *timpcp,
                                double     *tkent,

src/base/Makefile.am
@@ -400,7 +400,6 @@ pthrbm.f90 \
 resvoi.f90 \
 scalai.f90 \
 schtmp.f90 \
-stdtcl.f90 \
 strdep.f90 \
 strhis.f90 \
 strini.f90 \

src/base/cs_boundary_conditions.c
@@ -266,20 +266,20 @@ _inlet_sum(int                          var_id,
     if (enforce_balance == 1) {
       for (cs_lnum_t i = 0; i < n_faces; i++) {
         const cs_lnum_t f_id = (faces != NULL) ? faces[i] : i;
-        inlet_sum[j] +=_rcodcl[f_id]*f_surf[f_id];
+        inlet_sum[j] += _rcodcl[f_id]*f_surf[f_id];
       }
     }
     else if (enforce_balance == 2) {
       for (cs_lnum_t i = 0; i < n_faces; i++) {
         const cs_lnum_t f_id = (faces != NULL) ? faces[i] : i;
-        inlet_sum[j] +=_rcodcl[f_id]*f_surf[f_id]*balance_w[f_id];
+        inlet_sum[j] += _rcodcl[f_id]*f_surf[f_id]*balance_w[f_id];
       }
     }
     else if (enforce_balance == 3) {
       for (cs_lnum_t i = 0; i < n_faces; i++) {
         const cs_lnum_t f_id = (faces != NULL) ? faces[i] : i;
         const cs_lnum_t c_id = m->b_face_cells[f_id];
-        inlet_sum[j] +=_rcodcl[f_id]*f_surf[f_id]*balance_w[c_id];
+        inlet_sum[j] += _rcodcl[f_id]*f_surf[f_id]*balance_w[c_id];
       }
     }
 
@@ -529,7 +529,6 @@ _compute_neumann_bc(const cs_mesh_t            *mesh,
   const cs_zone_t *bz = cs_boundary_zone_by_id(def->z_id);
   const cs_lnum_t *elt_ids = bz->elt_ids;
   const cs_lnum_t  n_elts = bz->n_elts;
-  const cs_lnum_t  def_dim = def->dim;
 
   assert(eqp->dim == def->dim);
 

src/base/phyvar.f90
@@ -209,7 +209,7 @@ if (ippmod(iphpar).ge.1 .or. ippmod(idarcy).ge.1) then
   call cs_physical_properties2
 endif
 
-!  ROMB SUR LES BORDS : VALEUR PAR DEFAUT (CELLE DE LA CELLULE VOISINE)
+! Boundary density based on adjacent cell value if not explicitely set.
 
 if (mbrom.eq.0) then
   call field_get_val_s(icrom, crom)
@@ -219,7 +219,6 @@ if (mbrom.eq.0) then
   enddo
 endif
 
-!
 ! Parallelism and periodicity
 !
 ! In navstv and visecv, we need rho in the halo
@@ -228,7 +227,6 @@ if (irangp.ge.0.or.iperio.eq.1) then
   call synsca(crom)
 endif
 
-!
 ! Only density may be updated in Navier Stokes loop
 if (iterns.ge.1) return
 
@@ -288,7 +286,7 @@ endif
 ! Compute the eddy viscosity
 !===============================================================================
 
-if     (iturb.eq. 0) then
+if (iturb.eq. 0) then
 
 ! Laminar
 ! =======

src/base/stdtcl.f90
@@ -1,248 +1,0 @@
-!-------------------------------------------------------------------------------
-
-! This file is part of Code_Saturne, a general-purpose CFD tool.
-!
-! Copyright (C) 1998-2022 EDF S.A.
-!
-! This program is free software; you can redistribute it and/or modify it under
-! the terms of the GNU General Public License as published by the Free Software
-! Foundation; either version 2 of the License, or (at your option) any later
-! version.
-!
-! This program is distributed in the hope that it will be useful, but WITHOUT
-! ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-! FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-! details.
-!
-! You should have received a copy of the GNU General Public License along with
-! this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
-! Street, Fifth Floor, Boston, MA 02110-1301, USA.
-
-!-------------------------------------------------------------------------------
-
-subroutine stdtcl &
- ( nbzfmx , nozfmx ,                                              &
-   icalke , dh     , xintur , itypfb , iznfbr , rcodcl )
-
-!===============================================================================
-! FONCTION :
-! --------
-
-!    CONDITIONS AUX LIMITES AUTOMATIQUES  EN STANDARD
-
-!-------------------------------------------------------------------------------
-! Arguments
-!__________________.____._____.________________________________________________.
-! name             !type!mode ! role                                           !
-!__________________!____!_____!________________________________________________!
-! nbzfmx           ! e  ! <-- ! nb max de zones de faces de bord               !
-! nozfmx           ! e  ! <-- ! numero max de zones de faces de bord           !
-! itypfb           ! ia ! <-- ! boundary face types                            !
-! iznfbr           ! te ! <-- ! numero de zone de la face de bord              !
-! (nfabor)         !    !     !                                                !
-! rcodcl           ! tr ! --> ! valeur des conditions aux limites              !
-!                  !    !     !  aux faces de bord                             !
-!                  !    !     ! rcodcl(1) = valeur du dirichlet                !
-!                  !    !     ! rcodcl(2) = valeur du coef. d'echange          !
-!                  !    !     !  ext. (infinie si pas d'echange)               !
-!                  !    !     ! rcodcl(3) = valeur de la densite de            !
-!                  !    !     !  flux (negatif si gain) w/m2 ou                !
-!                  !    !     !  hauteur de rugosite (m) si icodcl=6           !
-!                  !    !     ! pour les vitesses (vistl+visct)*gradu          !
-!                  !    !     ! pour la pression             dt*gradp          !
-!                  !    !     ! pour les scalaires                             !
-!                  !    !     !        cp*(viscls+visct/turb_schmidt)*gradt    !
-!__________________!____!_____!________________________________________________!
-
-!===============================================================================
-
-!===============================================================================
-! Module files
-!===============================================================================
-
-use paramx
-use numvar
-use optcal
-use cstphy
-use cstnum
-use dimens, only: nvar
-use ppincl, only: itempk
-use entsor
-use parall
-use mesh
-use field
-use cs_c_bindings
-
-!===============================================================================
-
-implicit none
-
-! Arguments
-
-integer          nozfmx
-integer          nbzfmx
-
-integer          icalke(nozfmx)
-integer          itypfb(nfabor)
-integer          iznfbr(nfabor)
-
-double precision dh(nozfmx), xintur(nozfmx)
-double precision rcodcl(nfabor,nvar,3)
-
-! Local variables
-
-integer          ifac, izone, ifvu, izonem
-integer          nozapm, nzfppp
-integer          icke, ii, iel, iok, itk
-double precision qisqc, viscla, uref2, rhomoy, dhy, xiturb, brom_loc
-double precision, dimension(:), pointer :: brom
-double precision, dimension(:), pointer :: viscl
-integer, allocatable, dimension(:) :: ilzfbr
-double precision, allocatable, dimension(:) :: qcalc
-
-integer          ipass
-data             ipass /0/
-save             ipass
-
-!===============================================================================
-! 1.  INITIALISATIONS
-!===============================================================================
-
-allocate(ilzfbr(nbzfmx))
-allocate(qcalc(nozfmx))
-
-
-call field_get_val_s(ibrom, brom)
-call field_get_val_s(iviscl, viscl)
-
-! --> On construit une liste des numeros des zones frontieres.
-!           (liste locale au processeur, en parallele)
-nzfppp = 0
-do ifac = 1, nfabor
-  ifvu = 0
-  do ii = 1, nzfppp
-    if (ilzfbr(ii).eq.iznfbr(ifac)) then
-      ifvu = 1
-    endif
-  enddo
-  if(ifvu.eq.0) then
-    nzfppp = nzfppp + 1
-    if(nzfppp.le.nbzfmx) then
-      ilzfbr(nzfppp) = iznfbr(ifac)
-    else
-      write(nfecra,1001) nbzfmx
-      write(nfecra,1002)(ilzfbr(ii),ii=1,nbzfmx)
-      call csexit(1)
-    endif
-  endif
-enddo
-
-! ---> Plus grand numero de zone
-
-izonem = 0
-do ii = 1, nzfppp
-  izone = ilzfbr(ii)
-  izonem = max(izonem,izone)
-enddo
-if(irangp.ge.0) then
-  call parcmx(izonem)
-  !==========
-endif
-nozapm = izonem
-
- 1001 format(                                                     &
-'@                                                            ',/,&
-'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
-'@                                                            ',/,&
-'@ @@ ATTENTION : PROBLEME DANS LES CONDITIONS AUX LIMITES    ',/,&
-'@    =========                                               ',/,&
-'@                ARRET DANS LE SOUS-PROGRAMME STDTCL         ',/,&
-'@                                                            ',/,&
-'@  The maximum number of boundary zones which can be defined ',/,&
-'@    by the user is NBZPPM = ',I10                            ,/,&
-'@    It has been exceeded.                                   ',/,&
-'@                                                            ',/,&
-'@  The calculation will not run.                             ',/,&
-'@                                                            ',/,&
-'@  Verify the boundary conditions.                           ',/,&
-'@                                                            ',/,&
-'@  The first NBZPPM boundary zones have the following number:',/,&
-'@                                                            ',/,&
-'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',/,&
-'@                                                            ',/)
- 1002 format(i10)
-
-!===============================================================================
-! 4.  REMPLISSAGE DU TABLEAU DES CONDITIONS LIMITES
-!       ON BOUCLE SUR TOUTES LES FACES D'ENTREE
-!                     =========================
-!         ON DETERMINE LA FAMILLE ET SES PROPRIETES
-!           ON IMPOSE LES CONDITIONS AUX LIMITES
-!           POUR LA TURBULENCE
-!===============================================================================
-
-do ifac = 1, nfabor
-
-  izone = iznfbr(ifac)
-
-  if (izone .gt. 0) then
-
-    if (     itypfb(ifac).eq.ientre  &
-        .or. itypfb(ifac).eq.i_convective_inlet) then
-
-! ----  Traitement automatique de la turbulence
-
-      if ( icalke(izone).ne.0 ) then
-
-!       La turbulence est calculee par defaut si ICALKE different de 0
-!          - soit a partir du diametre hydraulique, d'une vitesse
-!            de reference adaptes a l'entree courante si ICALKE = 1
-!          - soit a partir du diametre hydraulique, d'une vitesse
-!            de reference et de l'intensite turvulente
-!            adaptes a l'entree courante si ICALKE = 2
-
-        uref2 = rcodcl(ifac,iu,1)**2                       &
-              + rcodcl(ifac,iv,1)**2                       &
-              + rcodcl(ifac,iw,1)**2
-        uref2 = max(uref2,epzero)
-        rhomoy = brom(ifac)
-        iel    = ifabor(ifac)
-        viscla = viscl(iel)
-        icke   = icalke(izone)
-        dhy    = dh(izone)
-        xiturb = xintur(izone)
-        if (icke.eq.1) then
-          call turbulence_bc_inlet_hyd_diam(ifac,                            &
-                                            uref2, dhy, rhomoy, viscla,      &
-                                            rcodcl)
-        else if (icke.eq.2) then
-          call turbulence_bc_inlet_turb_intensity(ifac,                      &
-                                                  uref2, xiturb, dhy,        &
-                                                  rcodcl)
-        endif
-
-      endif
-
-    endif
-
-    if (itypfb(ifac).eq.iparoi) then
-        ! condition automatique en paroi pour alpha
-        if (iturb.eq.32) then
-          rcodcl(ifac,ial,1)  = 0.d0
-        endif
-    endif
-
-  endif
-
-enddo
-
-! Free memory
-deallocate(ilzfbr)
-deallocate(qcalc)
-
-!----
-! End
-!----
-
-return
-end subroutine

src/base/tridim.f90
@@ -537,7 +537,6 @@ if (itrale.gt.0) then
   call schtmp(nscal, iappel)
 endif
 
-
 ! REMPLISSAGE DES COEFS DE PDC
 !    ON Y PASSE MEME S'IL N'Y A PAS DE PDC SUR LE PROC COURANT AU CAS OU
 !    UN UTILISATEUR DECIDERAIT D'AVOIR UN COEFF DE PDC DEPENDANT DE

commit 540a6237d375669a128a3e68c0ea35dd1e096ff7

Fix turbulent inlet settings for combustion, broken by commit 04f4a785 (yesterday).

src/base/condli.f90
@@ -375,11 +375,11 @@ call precli(nvar, icodcl, rcodcl)
 
 call uiclim &
   ( nozppm,                                                        &
-    iqimp,  ientat, ientcp, inmoxy, ientox,                        &
+    iqimp,  icalke, ientat, ientcp, inmoxy, ientox,                &
     ientfu, ientgb, ientgf, iprofm, iautom,                        &
     itypfb, izfppp, icodcl,                                        &
-    qimp,   qimpat, qimpcp,                                        &
-    timpat, timpcp, tkent ,  fment, distch, nvar, rcodcl)
+    qimp,   qimpat, qimpcp, dh,     xintur,                        &
+    timpat, timpcp, tkent , fment,  distch, nvar, rcodcl)
 
 call cs_boundary_conditions_complete(nvar, itypfb, icodcl, rcodcl)
 
@@ -4957,11 +4957,11 @@ call precli(nvar, icodcl, rcodcl)
 
 call uiclim &
   ( nozppm,                                                        &
-    iqimp,  ientat, ientcp, inmoxy, ientox,                        &
+    iqimp,  icalke, ientat, ientcp, inmoxy, ientox,                &
     ientfu, ientgb, ientgf, iprofm, iautom,                        &
     itypfb, izfppp, icodcl,                                        &
-    qimp,   qimpat, qimpcp,                                        &
-    timpat, timpcp, tkent ,  fment, distch, nvar, rcodcl)
+    qimp,   qimpat, qimpcp, dh,     xintur,                        &
+    timpat, timpcp, tkent , fment,  distch, nvar, rcodcl)
 
 !     - Sous-programme utilisateur
 !       ==========================

src/gui/cs_gui_boundary_conditions.c
@@ -2464,6 +2464,7 @@ _rescale_elec(const int  itypfb[],
 
 void CS_PROCF (uiclim, UICLIM)(const int  *nozppm,
                                int        *iqimp,
+                               int        *icalke,
                                int        *ientat,
                                int        *ientcp,
                                int        *inmoxy,
@@ -2479,6 +2480,8 @@ void CS_PROCF (uiclim, UICLIM)(const int  *nozppm,
                                double     *qimp,
                                double     *qimpat,
                                double     *qimpcp,
+                               double     *dh,
+                               double     *xintur,
                                double     *timpat,
                                double     *timpcp,
                                double     *tkent,
@@ -2617,12 +2620,15 @@ void CS_PROCF (uiclim, UICLIM)(const int  *nozppm,
 
       tn_bc = _get_zone_bc_node(tn_bc, izone);
 
-      /* Update the zone's arrays (iqimp, icalke, qimp,...)
+      /* Update the zone's arrays (iqimp, dh, xintur, icalke, qimp,...)
          because they are re-initialized at each time step
          in PRECLI and PPPRCL routines */
 
       /* data by zone */
       iqimp[zone_nbr-1]  = boundaries->iqimp[izone];
+      dh[zone_nbr-1]     = boundaries->dh[izone];
+      xintur[zone_nbr-1] = boundaries->xintur[izone];
+      icalke[zone_nbr-1] = boundaries->icalke[izone];
 
       if (solid_fuels) {
         const cs_combustion_model_t *cm = cs_glob_combustion_model;

src/gui/cs_gui_boundary_conditions.h
@@ -64,6 +64,7 @@ BEGIN_C_DECLS
 
 void CS_PROCF (uiclim, UICLIM)(const int  *nozppm,
                                int        *iqimp,
+                               int        *icalke,
                                int        *ientat,
                                int        *ientcp,
                                int        *inmoxy,
@@ -79,6 +80,8 @@ void CS_PROCF (uiclim, UICLIM)(const int  *nozppm,
                                double     *qimp,
                                double     *qimpat,
                                double     *qimpcp,
+                               double     *dh,
+                               double     *xintur,
                                double     *timpat,
                                double     *timpcp,
                                double     *tkent,