code-saturne/code_saturne

commit 7fd16784ba6d8a88d384730eb22e87a19a08e92e

[GUI] Add handling of physical properties as formulas for solid zones.
This should also be usable for non-solid, "property" zones (which
may be useful for cases where 2 separate fluids are handled in the same
computation, whether separated by a solid zone or other boundary).

src/gui/cs_gui.c
@@ -336,6 +336,138 @@ _thermal_table_needed(const char *name)
 }
 
 /*-----------------------------------------------------------------------------
+ * Compute a physical property based on a thermal law.
+ *----------------------------------------------------------------------------*/
+
+static void
+_physical_property_thermal_law(cs_field_t           *c_prop,
+                               const cs_zone_t      *z,
+                               cs_phys_prop_type_t   property)
+{
+  /* For incompressible flows, the thermodynamic pressure is constant over
+   * time and is the reference pressure. */
+
+  cs_lnum_t thermodynamic_pressure_stride = 0;
+  cs_lnum_t thermal_f_val_stride = 1;
+  cs_real_t _p0 = cs_glob_fluid_properties->p0;
+  cs_real_t _t0 = cs_glob_fluid_properties->t0;
+
+  const cs_real_t *thermodynamic_pressure = &_p0;
+  const cs_real_t *_thermal_f_val = NULL;
+
+  if (CS_F_(t) != NULL) {
+    if (CS_F_(t)->type & CS_FIELD_VARIABLE)
+      _thermal_f_val = CS_F_(t)->val;
+  }
+  else if (CS_F_(h) != NULL) {
+    if (CS_F_(h)->type & CS_FIELD_VARIABLE)
+      _thermal_f_val = CS_F_(h)->val;
+  }
+  else if (CS_F_(e_tot) != NULL) {
+    if (CS_F_(h)->type & CS_FIELD_VARIABLE) {
+      _thermal_f_val = CS_F_(e_tot)->val;
+      thermodynamic_pressure = CS_F_(p)->val;
+      thermodynamic_pressure_stride = 1;
+    }
+  }
+  else {
+    thermal_f_val_stride = 0;
+    _thermal_f_val = &_t0;
+  }
+
+  cs_phys_prop_compute(property,
+                       z->n_elts,
+                       thermodynamic_pressure_stride,
+                       thermal_f_val_stride,
+                       thermodynamic_pressure,
+                       _thermal_f_val,
+                       c_prop->val);
+}
+
+/*-----------------------------------------------------------------------------
+ * use MEI for thermal diffusivity
+ *
+ * This is a special case of _physical_property_, where the thermal
+ * diffusivity computation is based on that of the thermal conductivity.
+ *----------------------------------------------------------------------------*/
+
+static void
+_physical_property_th_diffusivity(cs_field_t          *c_prop,
+                                  const cs_zone_t     *z)
+{
+  const char prop_name[] = "thermal_conductivity";
+
+  const cs_lnum_t n_cells = z->n_elts;
+  const cs_lnum_t *cell_ids = z->elt_ids;
+
+  int user_law = 0;
+  const char *prop_choice = _properties_choice(prop_name, NULL);
+
+  if (cs_gui_strcmp(prop_choice, "user_law"))
+    user_law = 1;
+  else if (z->id > 1 && z->type & CS_VOLUME_ZONE_PHYSICAL_PROPERTIES) {
+    const char *law = _property_formula(prop_name, z->name);
+    if (law != NULL)
+      user_law = 1;
+  }
+
+  if (user_law) {
+
+    const char *law = _property_formula(prop_name, z->name);
+
+    if (law != NULL) {
+      /* Pass "field overlay": shallow copy of field with modified name
+       so as to be handled by the MEG volume function. */
+      cs_field_t _c_prop = *c_prop;
+      _c_prop.name = prop_name;
+      cs_field_t *fmeg[1] = {&_c_prop};
+      cs_meg_volume_function(z, fmeg);
+    }
+
+  }
+  else if (cs_gui_strcmp(prop_choice, "thermal_law") &&
+           cs_gui_strcmp(z->name, "all_cells")) {
+
+    _physical_property_thermal_law(c_prop,
+                                   z,
+                                   CS_PHYS_PROP_THERMAL_CONDUCTIVITY);
+
+  }
+
+  /* If property is globaly variable but defined as a constant
+     for this zone, set from constant */
+
+  else if (z->id <= 1 && cs_gui_strcmp(prop_choice, "constant")) {
+
+    const cs_fluid_properties_t *phys_pp = cs_glob_fluid_properties;
+    const cs_real_t p_val = phys_pp->lambda0;
+
+    for (cs_lnum_t e_id = 0; e_id < n_cells; e_id++) {
+      cs_lnum_t c_id = cell_ids[e_id];
+      c_prop->val[c_id] = p_val;
+    }
+
+  }
+
+  /* Finalize special case for conduction to diffusion conversion */
+
+  if (CS_F_(cp) == NULL) {
+    const cs_real_t cp0 = cs_glob_fluid_properties->cp0;
+    for (cs_lnum_t e_id = 0; e_id < n_cells; e_id++) {
+      cs_lnum_t c_id = cell_ids[e_id];
+        c_prop->val[c_id] /= cp0;
+    }
+  }
+  else {
+    const cs_real_t *cp = CS_F_(cp)->val;
+    for (cs_lnum_t e_id = 0; e_id < n_cells; e_id++) {
+      cs_lnum_t c_id = cell_ids[e_id];
+      c_prop->val[c_id] /= cp[c_id];
+    }
+  }
+}
+
+/*-----------------------------------------------------------------------------
  * use MEI for physical property
  *----------------------------------------------------------------------------*/
 
@@ -346,8 +478,25 @@ _physical_property(cs_field_t          *c_prop,
   int user_law = 0;
   const char *prop_choice = _properties_choice(c_prop->name, NULL);
 
+  /* Special case: "thermal_conductivity" is defined by GUI, but
+     in case of Enthalpy thermal model, the matching field is
+     "thermal_diffusivity". */
+
+  if (prop_choice == NULL) {
+    if (   cs_glob_thermal_model->itherm == CS_THERMAL_MODEL_ENTHALPY
+        && cs_gui_strcmp(c_prop->name, "thermal_diffusivity")) {
+      _physical_property_th_diffusivity(c_prop, z);
+      return;
+    }
+  }
+
   if (cs_gui_strcmp(prop_choice, "user_law"))
     user_law = 1;
+  else if (z->id > 1 && z->type & CS_VOLUME_ZONE_PHYSICAL_PROPERTIES) {
+    const char *law = _property_formula(c_prop->name, z->name);
+    if (law != NULL)
+      user_law = 1;
+  }
 
   if (user_law) {
 
@@ -380,46 +529,32 @@ _physical_property(cs_field_t          *c_prop,
                 _("Error: can not evaluate property: %s using a thermal law\n"),
                 c_prop->name);
 
-    /* For incompressible flows, the thermodynamic pressure is constant over
-     * time and is the reference pressure. */
+    _physical_property_thermal_law(c_prop, z, property);
+  }
 
-    cs_lnum_t thermodynamic_pressure_stride = 0;
-    cs_lnum_t thermal_f_val_stride = 1;
-    cs_real_t _p0 = cs_glob_fluid_properties->p0;
-    cs_real_t _t0 = cs_glob_fluid_properties->t0;
+  /* If property is globaly variable but defined as a constant
+     for this zone, set from constant */
 
-    const cs_real_t *thermodynamic_pressure = &_p0;
-    const cs_real_t *_thermal_f_val = NULL;
+  else if (z->id <= 1 && cs_gui_strcmp(prop_choice, "constant")) {
+    cs_real_t p_val = -1;
+    const cs_fluid_properties_t *phys_pp = cs_glob_fluid_properties;
 
-    if (CS_F_(t) != NULL) {
-      if (CS_F_(t)->type & CS_FIELD_VARIABLE)
-        _thermal_f_val = CS_F_(t)->val;
-    }
-    else if (CS_F_(h) != NULL) {
-      if (CS_F_(h)->type & CS_FIELD_VARIABLE)
-        _thermal_f_val = CS_F_(h)->val;
-    }
-    else if (CS_F_(e_tot) != NULL) {
-      if (CS_F_(h)->type & CS_FIELD_VARIABLE) {
-        _thermal_f_val = CS_F_(e_tot)->val;
-        thermodynamic_pressure = CS_F_(p)->val;
-        thermodynamic_pressure_stride = 1;
-      }
-    }
-    else {
-      thermal_f_val_stride = 0;
-      _thermal_f_val = &_t0;
+    if (cs_gui_strcmp(c_prop->name, "density"))
+      p_val = phys_pp->ro0;
+    else if (cs_gui_strcmp(c_prop->name, "molecular_viscosity"))
+      p_val = phys_pp->viscl0;
+    else if (cs_gui_strcmp(c_prop->name, "specific_heat"))
+      p_val = phys_pp->cp0;
+    else if (cs_gui_strcmp(c_prop->name, "thermal_conductivity")) {
+      p_val = phys_pp->lambda0;
     }
 
-    const cs_lnum_t ncel = z->n_elts;
-
-    cs_phys_prop_compute(property,
-                         ncel,
-                         thermodynamic_pressure_stride,
-                         thermal_f_val_stride,
-                         thermodynamic_pressure,
-                         _thermal_f_val,
-                         c_prop->val);
+    const cs_lnum_t n_cells = z->n_elts;
+    const cs_lnum_t *cell_ids = z->elt_ids;
+    for (cs_lnum_t e_id = 0; e_id < n_cells; e_id++) {
+      cs_lnum_t c_id = cell_ids[e_id];
+      c_prop->val[c_id] = p_val;
+    }
   }
 }
 
@@ -2097,6 +2232,13 @@ cs_gui_physical_properties(void)
                             visls_0);
   }
 
+  int n_zones_pp
+    = cs_volume_zone_n_type_zones(CS_VOLUME_ZONE_PHYSICAL_PROPERTIES);
+  if (n_zones_pp > 1) {
+    phys_pp->irovar = 1;
+    phys_pp->icp = 1;
+  }
+
 #if _XML_DEBUG_
   bft_printf("==> %s\n", __func__);
   bft_printf("--gx = %f \n", phys_cst->gravity[0]);
@@ -3093,8 +3235,8 @@ void CS_PROCF(uiphyv, UIPHYV)(const int       *iviscv)
 {
   double time0 = cs_timer_wtime();
 
-  int n_zones_pp =
-    cs_volume_zone_n_type_zones(CS_VOLUME_ZONE_PHYSICAL_PROPERTIES);
+  int n_zones_pp
+    = cs_volume_zone_n_type_zones(CS_VOLUME_ZONE_PHYSICAL_PROPERTIES);
   int n_zones = cs_volume_zone_n_zones();
   /* law for density (built-in for all current integrated physical models) */
   if (cs_glob_fluid_properties->irovar == 1) {
@@ -4950,14 +5092,14 @@ cs_gui_internal_coupling(void)
 {
   int n_zones = cs_volume_zone_n_zones();
 
-  int n_volume_zones = 0;
+  int n_solid_zones = 0;
   for (int i = 0; i < n_zones; i++) {
     const cs_zone_t  *z = cs_volume_zone_by_id(i);
     if (z->type & CS_VOLUME_ZONE_SOLID)
-      n_volume_zones += 1;
+      n_solid_zones += 1;
   }
 
-  if (n_volume_zones < 1)
+  if (n_solid_zones < 1)
     return;
 
   cs_tree_node_t *node_int_cpl
@@ -4967,7 +5109,7 @@ cs_gui_internal_coupling(void)
 
     int j = 0;
     int *z_ids;
-    BFT_MALLOC(z_ids, n_volume_zones, int);
+    BFT_MALLOC(z_ids, n_solid_zones, int);
 
     for (int i = 0; i < n_zones; i++) {
       const cs_zone_t  *z = cs_volume_zone_by_id(i);
@@ -4977,7 +5119,7 @@ cs_gui_internal_coupling(void)
 
     int coupling_id = cs_internal_coupling_n_couplings();
 
-    cs_internal_coupling_add_volume_zones(n_volume_zones, z_ids);
+    cs_internal_coupling_add_volume_zones(n_solid_zones, z_ids);
     BFT_FREE(z_ids);
 
     {
@@ -4992,7 +5134,7 @@ cs_gui_internal_coupling(void)
       cs_internal_coupling_add_boundary_groups(cpl, i_name, e_name);
     }
 
-    if (n_volume_zones > 0) {
+    if (n_solid_zones > 0) {
       cs_tree_node_t *ns
         = cs_tree_node_get_child(node_int_cpl, "coupled_scalars");
       /* Add the coupled scalars defined in the GUI */

bin/cs_meg_to_c.py
@@ -1263,18 +1263,19 @@ class meg_to_c_interpreter:
                 import FluidCharacteristicsModel
 
             fcm = FluidCharacteristicsModel(self.case)
-            for (fk,sym) in fcm.lst:
+            for (fk, sym) in fcm.lst:
                 if fcm.getPropertyMode(fk) == 'user_law':
                     exp, req, sca, sym = fcm.getFormulaComponents(fk)
                     self.init_block('vol', 'all_cells', fk,
                                     exp, req, sym, sca)
-                    for zone in vlm.getZones():
-                        zname = zone.getLabel()
-                        z_id  = str(zone.getCodeNumber())
-                        if zname != "all_cells" and \
-                                zone.isNatureActivated('physical_properties'):
+                for zone in vlm.getZones():
+                    zname = zone.getLabel()
+                    z_id  = str(zone.getCodeNumber())
+                    if zname != "all_cells" and \
+                            zone.isNatureActivated('physical_properties'):
+                        if fcm.getFormulaTry(fk, zone=z_id):
                             exp, req, sca, sym = \
-                            fcm.getFormulaComponents(fk, zone=z_id)
+                                fcm.getFormulaComponents(fk, zone=z_id)
                             self.init_block('vol', zname, fk,
                                             exp, req, sym, sca)
 

bin/model/FluidCharacteristicsModel.py
@@ -823,10 +823,11 @@ class FluidCharacteristicsModel(Variables, Model):
 
 
     @Variables.noUndo
-    def getFormula(self, tag, zone="1"):
+    def getFormulaTry(self, tag, zone="1"):
         """
         Return a formula for I{tag} 'density', 'molecular_viscosity',
         'specific_heat' or 'thermal_conductivity'
+        only if actually defined, None otherwise.
         """
         self.isInList(tag, ('density', 'molecular_viscosity',
                             'specific_heat', 'thermal_conductivity',
@@ -838,6 +839,16 @@ class FluidCharacteristicsModel(Variables, Model):
             else:
                 node = node.xmlInitChildNode("zone", zone_id=zone)
         formula = node.xmlGetChildString('formula')
+        return formula
+
+
+    @Variables.noUndo
+    def getFormula(self, tag, zone="1"):
+        """
+        Return a formula for I{tag} 'density', 'molecular_viscosity',
+        'specific_heat' or 'thermal_conductivity'
+        """
+        formula = self.getFormulaTry(tag, zone)
         if not formula:
             formula = self.getDefaultFormula(tag)
             self.setFormula(tag, formula, zone)

bin/model/OutputVolumicVariablesModel.py
@@ -56,6 +56,7 @@ class OutputVolumicVariablesModel(Variables, Model):
         self.node_models    = self.case.xmlGetNode('thermophysical_models')
         self.analysis_ctrl  = self.case.xmlGetNode('analysis_control')
         self.fluid_prop     = self.case.xmlGetNode('physical_properties')
+        self.have_multizone_phys_prop = self.__has_multiple_phys_prop_nodes__()
 
         if self.node_models:
             self.node_model_vp  = self.node_models.xmlGetNode('velocity_pressure')
@@ -88,6 +89,27 @@ class OutputVolumicVariablesModel(Variables, Model):
 
     # Following private methods: (to see for gathering eventually)
 
+
+    def __has_multiple_phys_prop_nodes__(self):
+        """
+        Determine if we have mutiple physical properties nodes
+        """
+        # We do not use the LocalizationModel here, as it would lead to
+        # circular dependencies, so directly use XML definitions.
+        m_pp_zones = 0
+        n = self.case.xmlGetNode('solution_domain')
+        if n:
+            n = n.xmlGetNode('volumic_conditions')
+            if n:
+                nl = n.xmlGetChildNodeList('zone', 'label', 'id')
+                for n in nl:
+                    if n['physical_properties'] == 'on':
+                        m_pp_zones += 1
+                        if m_pp_zones > 1:
+                            return True
+        return False
+
+
     def _defaultValues(self):
         """
         Return in a dictionnary which contains default values
@@ -116,7 +138,7 @@ class OutputVolumicVariablesModel(Variables, Model):
                     if node['support'] == 'boundary':
                         continue
                 if tag == 'property':
-                    if not constant:
+                    if not (self.have_multizone_phys_prop or constant):
                         choice = node['choice']
                         if choice and choice == 'constant':
                             continue
@@ -334,7 +356,10 @@ class OutputVolumicVariablesModel(Variables, Model):
             # (could also be based on dictionary)
 
             category = node.xmlGetParentName()
-            category = category.replace('_', ' ').capitalize()
+            if category == 'fluid_properties':
+                category = 'Physical properties'
+            else:
+                category = category.replace('_', ' ').capitalize()
 
             # For NCFD multiphase, use the field_id as a parent category
             if node['field_id']:
@@ -601,12 +626,23 @@ class OutputVolumicVariablesModel(Variables, Model):
 
         if self.fluid_prop:
             node = self.fluid_prop.xmlGetNode('fluid_properties')
+
             if node:
                 for prop in ('density',
-                             'molecular_viscosity',
                              'specific_heat',
                              'thermal_conductivity'):
                     L = node.xmlGetNode('property', name=prop, choice='variable')
+                    if not L:
+                        L = node.xmlGetNode('property', name=prop, choice='user_law')
+                    if self.have_multizone_phys_prop and not L:
+                        L = node.xmlGetNode('property', name=prop, choice='constant')
+                    if L:
+                        nodeList.append(L)
+                for prop in ('molecular_viscosity',
+                             'dynamic_diffusion'):
+                    L = node.xmlGetNode('property', name=prop, choice='variable')
+                    if not L:
+                        L = node.xmlGetNode('property', name=prop, choice='user_law')
                     if L:
                         nodeList.append(L)
 

gui/Pages/FluidCharacteristicsView.py
@@ -192,6 +192,8 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
         self.zone     = None
         self.zone_name = None
         self.zone_id   = None
+        self.is_main_zone = False
+        self.is_solid  = False
 
 
     def setup(self, case, zone_name):
@@ -203,6 +205,12 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
                 self.zone = zone
                 self.zone_name = zone.getLabel()
                 self.zone_id   = str(zone.getCodeNumber())
+                if self.zone_name == "all_cells":
+                    self.is_main_zone = True
+                elif not zone.isNatureActivated("physical_properties"):
+                    return
+                if zone.isNatureActivated("solid"):
+                    self.is_solid = True
 
         self.case.undoStopGlobal()
 
@@ -396,9 +404,15 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
         mdls = self.mdl.getThermoPhysicalModel()
         mdl_atmo, mdl_joule, mdl_thermal, mdl_gas, mdl_coal, mdl_comp, mdl_hgn = mdls
 
+        if self.is_solid:
+            mdl_atmo = "off"
+            mdl_joule = "off"
+            mdl_gas = "off"
+            mdl_coal = "off"
+            mdl_comp = "off"
+            mdl_hgn = "off"
 
-
-        is_main_zone = (self.zone_name == "all_cells")
+        is_main_zone = self.is_main_zone
 
         self.groupBoxPressure.setVisible(is_main_zone)
         self.groupBoxTemperature.setVisible(is_main_zone)
@@ -506,6 +520,13 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
             self.labelUnitDiff.setVisible(is_main_zone)
             self.comboBoxDiff.setEnabled(is_main_zone)
 
+        # Solid zone
+
+        if self.is_solid:
+            self.groupBoxTempd3p.hide()
+            self.groupBoxMu.hide()
+            self.groupBoxDiftl0.hide()
+            self.groupBoxDiff.hide()
 
         # Standard Widget initialization
         for tag, symbol in self.mdl.lst:
@@ -527,6 +548,8 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
                 if c not in __model.getItems():
                     c = 'constant'
                     self.mdl.setPropertyMode(tag, c)
+                if self.is_solid:
+                    c = "user_law"
 
                 __model.setItem(str_model=c)
                 if c == 'user_law':
@@ -967,7 +990,8 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
             __labelu.show()
             __labelv.show()
 
-        self.mdl.setPropertyMode(tag, choice)
+        if self.is_main_zone:
+            self.mdl.setPropertyMode(tag, choice)
 
 
     @pyqtSlot(str)
@@ -1083,7 +1107,7 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
         User formula for density
         """
         exp, req, sca, symbols_rho = \
-                self.mdl.getFormulaRhoComponents(self.zone_id)
+            self.mdl.getFormulaRhoComponents(self.zone_id)
 
         self.m_th = ThermalScalarModel(self.case)
         s = self.m_th.getThermalScalarName()

gui/Pages/VolumicNatureView.py
@@ -166,14 +166,7 @@ class VolumicZoneNatureModel(QAbstractTableModel):
         if col == 0:
             return base_flags  # lock first column
         else:
-            h = self._headers[col]
-            zname = self._data[row][0]
-            # FIXME: For v7.0 'all_cells' is always used for physical properties.
-            # Will be changed for v7.1
-            if zname == "all_cells" and h in ("Physical properties", "Solid"):
-                return Qt.NoItemFlags
-            else:
-                return base_flags | Qt.ItemIsUserCheckable
+            return base_flags | Qt.ItemIsUserCheckable
 
 
 # Helper functions

commit afe57eab2bffa8e713d194a819f07540a8c4b3fc

Fix division of predefined model thermal conductivity by Cp when using Enthalpy.
This issue was introduced by commit 7fd16784 (May 13 2021), adding handling
of physical properties as formulas for solid zones.

It impacted at least the eletric arcs module.

src/gui/cs_gui.c
@@ -454,6 +454,11 @@ _physical_property_th_diffusivity(cs_field_t          *c_prop,
 
   }
 
+  /* If property is predefined by the model, do nothing and return. */
+
+  else if (cs_gui_strcmp(prop_choice, "predefined_law"))
+    return;
+
   /* Finalize special case for conduction to diffusion conversion */
 
   if (CS_F_(cp) == NULL) {