code-saturne/code_saturne

commit 99d01df4a49974f0703f8f6ba566216cc7754279

PETSc support: now handle matrices as external type.

src/alge/cs_benchmark_matrix.c
@@ -67,6 +67,10 @@
 #include "cs_matrix_hypre.h"
 #endif
 
+#if defined(HAVE_PETSC)
+#include "cs_matrix_petsc.h"
+#endif
+
 /*----------------------------------------------------------------------------
  *  Header for the current file
  *----------------------------------------------------------------------------*/
@@ -477,6 +481,23 @@ _variant_build_list(int                             n_fill_types,
                  &n_variants_max,
                  m_variant);
 
+#endif /* defined(HAVE_HYPRE) */
+
+#if defined(HAVE_PETSC)
+
+    _variant_add("PETSc (MATAIJ)",
+                 "PETSC",
+                 CS_MATRIX_MSR,
+                 n_fill_types,
+                 fill_types,
+                 op_flag_a,
+                 "external",
+                 "external",
+                 "external",
+                 n_variants,
+                 &n_variants_max,
+                 m_variant);
+
 #endif /* defined(HAVE_MKL) */
 
   }
@@ -731,6 +752,12 @@ _matrix_check(int                          n_variants,
           int device_id = cs_get_device_id();
           int use_device = (device_id < 0) ? 0 : 1;
           cs_matrix_set_type_hypre(m, use_device);
+        }
+#endif
+
+#if defined(HAVE_HYPRE)
+        if (strcmp(v->external_type, "PETSc") == 0) {
+          cs_matrix_set_type_petsc(m, NULL);
         }
 #endif
 

src/alge/Makefile.am
@@ -124,7 +124,10 @@ cs_sles_it_priv.h \
 cs_sles_pc.h
 
 if HAVE_PETSC
-pkginclude_HEADERS += cs_sles_petsc.h
+pkginclude_HEADERS += \
+cs_matrix_petsc.h \
+cs_matrix_petsc_priv.h \
+cs_sles_petsc.h
 endif
 
 if HAVE_MUMPS
@@ -195,6 +198,7 @@ if HAVE_PETSC
 noinst_LTLIBRARIES += libcsalge_petsc.la
 libcsalge_la_LIBADD += libcsalge_petsc.la
 libcsalge_petsc_la_SOURCES = \
+cs_matrix_petsc.c \
 cs_sles_petsc.c
 endif
 

src/alge/cs_alge_headers.h
@@ -57,6 +57,10 @@
 #include "cs_sles_hypre.h"
 #endif
 
+#if defined(HAVE_PETSC)
+#include "cs_sles_petsc.h"
+#endif
+
 #if defined(HAVE_AMGX)
 #include "cs_sles_amgx.h"
 #endif

src/alge/cs_benchmark.c
@@ -88,6 +88,10 @@
 #include "cs_sles_hypre.h"
 #endif
 
+#if defined(HAVE_PETSC)
+#include "cs_matrix_petsc.h"
+#endif
+
 /*----------------------------------------------------------------------------
  *  Header for the current file
  *----------------------------------------------------------------------------*/
@@ -797,7 +801,7 @@ _matrix_check_asmb(cs_lnum_t              n_rows,
       cs_matrix_structure_t  *ms
         = cs_matrix_structure_create_from_assembler(CS_MATRIX_MSR, ma);
 
-      for (int m_type_idx = 0; m_type_idx < 2; m_type_idx++) {
+      for (int m_type_idx = 0; m_type_idx < 3; m_type_idx++) {
 
         cs_matrix_t  *m = cs_matrix_create(ms);
 
@@ -810,6 +814,15 @@ _matrix_check_asmb(cs_lnum_t              n_rows,
             int device_id = cs_get_device_id();
             int use_device = (device_id < 0) ? 0 : 1;
             cs_matrix_set_type_hypre(m, use_device);
+          }
+#else
+          continue;
+#endif
+          break;
+        case 2:
+#if defined(HAVE_PETSC)
+          {
+            cs_matrix_set_type_petsc(m, 0);
           }
 #else
           continue;

src/alge/cs_matrix_assembler.c
@@ -2875,6 +2875,22 @@ cs_matrix_assembler_get_n_rows(const cs_matrix_assembler_t  *ma)
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief Return the global number of rows associated with a matrix assembler.
+ *
+ * \param[in]  ma  pointer to matrix assembler structure
+ *
+ * \return  number of rows associated with matrix assembler
+ */
+/*----------------------------------------------------------------------------*/
+
+cs_gnum_t
+cs_matrix_assembler_get_n_g_rows(const cs_matrix_assembler_t  *ma)
+{
+  return ma->n_g_rows;
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief Return the number of columns associated with a matrix assembler.
  *
  * \param[in]  ma  pointer to matrix assembler structure

src/alge/cs_matrix_assembler.h
@@ -454,6 +454,19 @@ cs_matrix_assembler_get_n_rows(const cs_matrix_assembler_t  *ma);
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief Return the global number of rows associated with a matrix assembler.
+ *
+ * \param[in]  ma  pointer to matrix assembler structure
+ *
+ * \return  number of rows associated with matrix assembler
+ */
+/*----------------------------------------------------------------------------*/
+
+cs_gnum_t
+cs_matrix_assembler_get_n_g_rows(const cs_matrix_assembler_t  *ma);
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief Return the number of columns associated with a matrix assembler.
  *
  * \param[in]  ma  pointer to matrix assembler structure

src/alge/cs_matrix_default.c
@@ -73,6 +73,10 @@
 #include "cs_matrix_hypre.h"
 #endif
 
+#if defined(HAVE_PETSC)
+#include "cs_matrix_petsc.h"
+#endif
+
 #include "cs_matrix_priv.h"
 #include "cs_matrix_tuning.h"
 
@@ -666,6 +670,34 @@ cs_matrix_external(const char  *type_name,
       use_device = 1;
 
     cs_matrix_set_type_hypre(m, use_device);
+
+    return m;
+  }
+#endif
+
+#if defined(HAVE_PETSC)
+  if (strncmp(type_name, "PETSc", 5) == 0) {
+    cs_matrix_t *m_r = NULL;
+
+    if (_matrix_struct[CS_MATRIX_MSR] != NULL) {
+      m_r = cs_matrix_msr(symmetric, diag_block_size, extra_diag_block_size);
+    }
+    else {
+      m_r = cs_matrix_native(symmetric, diag_block_size, extra_diag_block_size);
+    }
+
+    cs_matrix_t *m = cs_matrix_copy_to_external(m_r,
+                                                symmetric,
+                                                diag_block_size,
+                                                extra_diag_block_size);
+
+
+    const char *mat_type = NULL;
+    size_t l = strlen(type_name);
+    if (l > 7 && strncmp(type_name, "PETSc, ", 7) == 0)
+      mat_type = type_name + 7;
+
+    cs_matrix_set_type_petsc(m, mat_type);
 
     return m;
   }

commit bf914e21f845fa45baec64d94417050c489fcae3

Fix build without PETSc, broken by commit 99d01df4 (yesterday).

src/alge/cs_benchmark_matrix.c
@@ -755,7 +755,7 @@ _matrix_check(int                          n_variants,
         }
 #endif
 
-#if defined(HAVE_HYPRE)
+#if defined(HAVE_PETSC)
         if (strcmp(v->external_type, "PETSc") == 0) {
           cs_matrix_set_type_petsc(m, NULL);
         }