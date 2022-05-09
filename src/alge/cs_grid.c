code-saturne/code_saturne

commit 998c5a72ad677b0b800a9d7dff23ed3393dd31af

Fix for hang in grid merging, broken since commit 23e3dc2f (Nov 21 2019).

src/alge/cs_grid.c
@@ -116,6 +116,7 @@ struct _cs_grid_t {
                                        false otherwhise */
   bool                symmetric;    /* Symmetric matrix coefficients
                                        indicator */
+  bool                use_faces;    /* True if face information is present */
 
   cs_lnum_t           db_size;      /* Block sizes for diagonal */
   cs_lnum_t           eb_size;      /* Block sizes for extra diagonal */
@@ -447,6 +448,7 @@ _coarse_init(const cs_grid_t *f)
   c->level = f->level + 1;
   c->symmetric = f->symmetric;
   c->conv_diff = f->conv_diff;
+  c->use_faces = f->use_faces;
   c->db_size = f->db_size;
   c->eb_size = f->eb_size;
 
@@ -2199,7 +2201,7 @@ _merge_grids(cs_grid_t  *g,
       g->n_ranks = (g->n_ranks/merge_stride) + 1;
     else
       g->n_ranks = (g->n_ranks/merge_stride);
-    g->comm = cs_base_get_rank_step_comm(g->next_merge_stride);
+    g->comm = cs_base_get_rank_step_comm_recursive(g->comm, merge_stride);
   }
 
   if (verbosity > 2) {
@@ -4971,6 +4973,7 @@ cs_grid_create_from_shared(cs_lnum_t              n_faces,
   g->level = 0;
   g->conv_diff = conv_diff;
   g->symmetric = cs_matrix_is_symmetric(a);
+  g->use_faces = (face_cell != NULL) ? true : false;
 
   g->db_size = db_size;
   g->eb_size = eb_size;
@@ -5126,6 +5129,7 @@ cs_grid_create_from_parent(const cs_matrix_t  *a,
 
   g->level = 0;
   g->symmetric = cs_matrix_is_symmetric(g->matrix);
+  g->use_faces = false;
 
   const cs_lnum_t db_size = cs_matrix_get_diag_block_size(g->matrix);
   const cs_lnum_t eb_size = cs_matrix_get_extra_diag_block_size(g->matrix);
@@ -5508,7 +5512,7 @@ cs_grid_coarsen(const cs_grid_t  *f,
   /* Ensure default is available */
 
   if (coarsening_type == CS_GRID_COARSENING_DEFAULT) {
-    if (f->face_cell != NULL) {
+    if (f->use_faces) {
       if (f->conv_diff == false)
         coarsening_type = CS_GRID_COARSENING_SPD_DX;
       else
@@ -5539,7 +5543,7 @@ cs_grid_coarsen(const cs_grid_t  *f,
 
   if (   coarsening_type == CS_GRID_COARSENING_SPD_DX
       || coarsening_type == CS_GRID_COARSENING_CONV_DIFF_DX) {
-    if (f->face_cell != NULL)
+    if (f->use_faces)
       _automatic_aggregation_fc(f,
                                 coarsening_type,
                                 aggregation_limit,
@@ -5592,7 +5596,7 @@ cs_grid_coarsen(const cs_grid_t  *f,
   BFT_MALLOC(c->_xa, c->n_faces*isym, cs_real_t);
   c->xa = c->_xa;
 
-  if (  (fine_matrix_type == CS_MATRIX_NATIVE || f->face_cell != NULL)
+  if (  (fine_matrix_type == CS_MATRIX_NATIVE || f->use_faces)
       && c->relaxation > 0) {
 
     /* Allocate permanent arrays in coarse grid */
@@ -5661,7 +5665,7 @@ cs_grid_coarsen(const cs_grid_t  *f,
 
   }
 
-  else if (f->face_cell != NULL) {
+  else if (f->use_faces) {
 
     if (conv_diff)
       _compute_coarse_quantities_conv_diff(f, c, verbosity);
@@ -5783,7 +5787,7 @@ cs_grid_coarsen(const cs_grid_t  *f,
     cc->coarse_row = c->coarse_row;
     c->coarse_row = NULL;
 
-    if (c->face_cell != NULL) {
+    if (c->use_faces) {
       BFT_FREE(cc->coarse_face);
       BFT_FREE(cc->_face_cell);
       _coarsen_faces(f,
@@ -5884,7 +5888,7 @@ cs_grid_coarsen_to_single(const cs_grid_t  *f,
 #endif
   }
 
-  else if (f->face_cell != NULL) {
+  else if (f->use_faces) {
 
     BFT_MALLOC(c->_da, c->n_cols_ext * db_stride, cs_real_t);
     c->da = c->_da;

src/base/cs_base.c
@@ -1393,6 +1393,94 @@ cs_base_get_rank_step_comm(int  rank_step)
   return _step_comm[comm_id];
 }
 
+/*----------------------------------------------------------------------------
+ * Return a reduced communicator matching a multiple of the total
+ * number of ranks, and given a parent communicator.
+ *
+ * Compared to \ref cs_base_get_rank_step_comm, this function is
+ * collective only on the provided communicator.
+ *
+ * This updates the number of reduced communicators if necessary.
+ *
+ * parameters:
+ *   parent_comm <-- associated parent communicator (must be either
+ *                   cs_glob_mpi_comm or a communicator returned by a
+ *                   previous
+ *   rank_step   <-- associated multiple of ranks of parent communicator
+ *----------------------------------------------------------------------------*/
+
+MPI_Comm
+cs_base_get_rank_step_comm_recursive(MPI_Comm  parent_comm,
+				     int       rank_step)
+{
+  if (rank_step <= 1 || parent_comm == MPI_COMM_NULL)
+    return parent_comm;
+
+  int parent_n_ranks = -1;
+  MPI_Comm_size(parent_comm, &parent_n_ranks);
+
+  int n_ranks = parent_n_ranks / rank_step;
+  if (parent_n_ranks % rank_step > 0)
+    n_ranks += 1;
+
+  if (n_ranks <= 1)
+    return MPI_COMM_NULL;
+
+  int comm_id = 0;
+  if (_n_step_comms > 0) {
+    while (   _step_ranks[comm_id] != n_ranks
+           && comm_id < _n_step_comms)
+      comm_id++;
+  }
+
+  printf("get_rank_step_comm_recursive: %d glob id, %d parent, step %d -> id %d\n",
+	 cs_glob_rank_id, parent_n_ranks, rank_step, comm_id);
+
+  /* Add communicator if required */
+
+  if (comm_id >= _n_step_comms) {
+
+    _n_step_comms += 1;
+    BFT_REALLOC(_step_comm, _n_step_comms, MPI_Comm);
+    BFT_REALLOC(_step_ranks, _n_step_comms, int);
+
+    _step_ranks[comm_id] = n_ranks;
+
+    if (n_ranks == cs_glob_n_ranks)
+      _step_comm[comm_id] = cs_glob_mpi_comm;
+
+    else if (n_ranks == 1)
+      _step_comm[comm_id] = MPI_COMM_NULL;
+
+    else {
+
+      int ranges[1][3];
+      MPI_Group old_group, new_group;
+
+      MPI_Barrier(parent_comm); /* For debugging */
+
+      MPI_Comm_size(parent_comm, &n_ranks);
+      MPI_Comm_group(parent_comm, &old_group);
+
+      ranges[0][0] = 0;
+      ranges[0][1] = n_ranks - 1;
+      ranges[0][2] = rank_step;
+
+      MPI_Group_range_incl(old_group, 1, ranges, &new_group);
+      MPI_Comm_create(parent_comm, new_group, &(_step_comm[comm_id]));
+      MPI_Group_free(&new_group);
+
+      MPI_Group_free(&old_group);
+
+      MPI_Barrier(parent_comm); /* For debugging */
+
+    }
+
+  }
+
+  return _step_comm[comm_id];
+}
+
 #endif /* HAVE_MPI */
 
 /*----------------------------------------------------------------------------

src/base/cs_base.h
@@ -167,6 +167,26 @@ cs_base_mpi_init(int    *argc,
 MPI_Comm
 cs_base_get_rank_step_comm(int  rank_step);
 
+/*----------------------------------------------------------------------------
+ * Return a reduced communicator matching a multiple of the total
+ * number of ranks, and given a parent communicator.
+ *
+ * Compared to \ref cs_base_get_rank_step_comm, this function is
+ * collective only on the provided communicator.
+ *
+ * This updates the number of reduced communicators if necessary.
+ *
+ * parameters:
+ *   parent_comm <-- associated parent communicator (must be either
+ *                   cs_glob_mpi_comm or a communicator returned by a
+ *                   previous
+ *   rank_step   <-- associated multiple of ranks of parent communicator
+ *----------------------------------------------------------------------------*/
+
+MPI_Comm
+cs_base_get_rank_step_comm_recursive(MPI_Comm  parent_comm,
+				     int       rank_step);
+
 #endif /* defined(HAVE_MPI) */
 
 /*----------------------------------------------------------------------------

commit 0e5ac7c9e2864828d0f9e1d428734776ca724e34

Multigrid: fix assembler-based cases, broken by commit 998c5a72 (May 3 2022).

src/alge/cs_grid.c
@@ -4973,7 +4973,7 @@ cs_grid_create_from_shared(cs_lnum_t              n_faces,
   g->level = 0;
   g->conv_diff = conv_diff;
   g->symmetric = cs_matrix_is_symmetric(a);
-  g->use_faces = (face_cell != NULL) ? true : false;
+  g->use_faces = false;
 
   g->db_size = db_size;
   g->eb_size = eb_size;
@@ -4991,8 +4991,10 @@ cs_grid_create_from_shared(cs_lnum_t              n_faces,
   }
 #endif
 
-  if (cs_matrix_is_mapped_from_native(a))
+  if (cs_matrix_is_mapped_from_native(a)) {
     g->face_cell = face_cell;
+    g->use_faces = true;
+  }
 
   g->relaxation = 0;
 