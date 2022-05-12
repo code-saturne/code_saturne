code-saturne/code_saturne

commit f595f7e7533653704eb7db45437e6ed54aaa2b71

Accelerator support: many small fixes an improvements.
- Ensure compatibility of matrix operation between accelerated and
  non-accelerated solvers.
  - Tuning separates host and device timing.

- Fix minor memory leaks.

- Add optional variable name and allocation site info in pointer map
  used by CS_ALLOC_HD series function to ease debugging (not compiled
  by default to avoid memory overhead, activated by changing a #define
  in cs_base_accel.cxx).

src/alge/cs_matrix_default.c
@@ -815,13 +815,14 @@ cs_matrix_default_set_tuned(cs_matrix_t  *m)
     _matrix_variant_tuned[m->type][m->fill_type]
       = cs_matrix_variant_tuned(_get_matrix(m->type),
                                 1,
-                                m->fill_type,
+                                _n_min_products,
                                 _t_measure);
 
   }
 
   if (_matrix_variant_tuned[m->type][m->fill_type] != NULL)
-    cs_matrix_variant_apply(m, _matrix_variant_tuned[m->type][m->fill_type]);
+    cs_matrix_variant_apply_tuned(m,
+                                  _matrix_variant_tuned[m->type][m->fill_type]);
 }
 
 /*----------------------------------------------------------------------------

src/alge/cs_benchmark.c
@@ -1038,9 +1038,6 @@ cs_benchmark(int  mpi_trace_mode)
   cs_matrix_fill_type_t  fill_types_sym[] = {CS_MATRIX_SCALAR_SYM,
                                              CS_MATRIX_BLOCK_D_SYM};
 
-  cs_mesh_adjacencies_initialize();
-  cs_mesh_adjacencies_update_mesh();
-
   cs_matrix_initialize();
 
   cs_log_printf(CS_LOG_PERFORMANCE,

src/alge/cs_benchmark_matrix.c
@@ -421,35 +421,37 @@ _variant_build_list(int                             n_fill_types,
 
 #if defined(HAVE_CUDA)
 
-    _variant_add("CSR, CUDA",
-                 NULL,
-                 CS_MATRIX_CSR,
-                 n_fill_types,
-                 fill_types,
-                 op_flag_ae,
-                 "cuda",
-                 NULL,
-                 NULL,
-                 n_variants,
-                 &n_variants_max,
-                 m_variant);
+    if (cs_get_device_id() > -1)
+      _variant_add("CSR, CUDA",
+                   NULL,
+                   CS_MATRIX_CSR,
+                   n_fill_types,
+                   fill_types,
+                   op_flag_ae,
+                   "cuda",
+                   NULL,
+                   NULL,
+                   n_variants,
+                   &n_variants_max,
+                   m_variant);
 
 #endif /* defined(HAVE_CUDA) */
 
 #if defined(HAVE_CUSPARSE)
 
-    _variant_add("CSR, with cuSPARSE",
-                 NULL,
-                 CS_MATRIX_CSR,
-                 n_fill_types,
-                 fill_types,
-                 op_flag_ae,
-                 "cusparse",
-                 NULL,
-                 NULL,
-                 n_variants,
-                 &n_variants_max,
-                 m_variant);
+    if (cs_get_device_id() > -1)
+      _variant_add("CSR, with cuSPARSE",
+                   NULL,
+                   CS_MATRIX_CSR,
+                   n_fill_types,
+                   fill_types,
+                   op_flag_ae,
+                   "cusparse",
+                   NULL,
+                   NULL,
+                   n_variants,
+                   &n_variants_max,
+                   m_variant);
 
 #endif /* defined(HAVE_CUSPARSE) */
 
@@ -489,39 +491,41 @@ _variant_build_list(int                             n_fill_types,
 
 #if defined(HAVE_CUDA)
 
-    _variant_add("MSR, CUDA",
-                 NULL,
-                 CS_MATRIX_MSR,
-                 n_fill_types,
-                 fill_types,
-                 op_flag_ae,
-                 "cuda",
-                 "cuda",
-                 NULL,
-                 n_variants,
-                 &n_variants_max,
-                 m_variant);
+    if (cs_get_device_id() > -1)
+      _variant_add("MSR, CUDA",
+                   NULL,
+                   CS_MATRIX_MSR,
+                   n_fill_types,
+                   fill_types,
+                   op_flag_ae,
+                   "cuda",
+                   "cuda",
+                   NULL,
+                   n_variants,
+                   &n_variants_max,
+                   m_variant);
 
 #endif /* defined(HAVE_CUDA) */
 
 #if defined(HAVE_CUSPARSE)
 
-    _variant_add("MSR, with cuSPARSE",
-                 NULL,
-                 CS_MATRIX_MSR,
-                 n_fill_types,
-                 fill_types,
-                 op_flag_ae,
-                 "cusparse",
+    if (cs_get_device_id() > -1)
+      _variant_add("MSR, with cuSPARSE",
+                   NULL,
+                   CS_MATRIX_MSR,
+                   n_fill_types,
+                   fill_types,
+                   op_flag_ae,
+                   "cusparse",
 #if defined(HAVE_CUSPARSE_GENERIC_API)
-                 "cusparse",
+                   "cusparse",
 #else
-                 NULL,
+                   NULL,
 #endif
-                 "cusparse",
-                 n_variants,
-                 &n_variants_max,
-                 m_variant);
+                   "cusparse",
+                   n_variants,
+                   &n_variants_max,
+                   m_variant);
 
 #endif /* defined(HAVE_CUSPARSE) */
 

src/alge/cs_grid.c
@@ -5758,7 +5758,7 @@ cs_grid_coarsen(const cs_grid_t  *f,
     }
 
     if (coarse_mv != NULL)
-      cs_matrix_variant_apply(c->_matrix, coarse_mv);
+      cs_matrix_variant_apply_tuned(c->_matrix, coarse_mv);
   }
 
   if (c->matrix == NULL) {

src/alge/cs_matrix.c
@@ -4073,9 +4073,9 @@ _matrix_create(cs_matrix_type_t  type)
     for (cs_matrix_spmv_type_t i = 0; i < CS_MATRIX_SPMV_N_TYPES; i++) {
       m->vector_multiply[mft][i] = NULL;
 #if defined(HAVE_ACCEL)
+      m->vector_multiply_h[mft][i] = NULL;
       m->vector_multiply_d[mft][i] = NULL;
 #endif
-      m->vector_multiply_xy_hd[mft][i] = 'h';
     }
   }
 
@@ -4111,6 +4111,7 @@ _matrix_create(cs_matrix_type_t  type)
   /* Set function pointers here */
 
   m->set_coefficients = NULL;
+  m->destroy_adaptor = NULL;
 
   cs_matrix_spmv_set_defaults(m);
 
@@ -4167,12 +4168,12 @@ _matrix_create(cs_matrix_type_t  type)
     if (m->vector_multiply[i][1] == NULL)
       m->vector_multiply[i][1] = m->vector_multiply[i][0];
 #if defined(HAVE_ACCEL)
+    if (m->vector_multiply_h[i][1] == NULL)
+      m->vector_multiply_h[i][1] = m->vector_multiply_h[i][0];
     if (m->vector_multiply_d[i][1] == NULL)
       m->vector_multiply_d[i][1] = m->vector_multiply_d[i][0];
 #endif
   }
-
-  m->destroy_adaptor = NULL;
 
   return m;
 }
@@ -5923,12 +5924,20 @@ cs_matrix_vector_multiply(const cs_matrix_t   *matrix,
   if (matrix->vector_multiply[matrix->fill_type][0] != NULL) {
 
 #if defined(HAVE_ACCEL)
-    if (matrix->vector_multiply_xy_hd[matrix->fill_type][0] == 'd') {
-      cs_real_t *d_x = (cs_real_t *)cs_get_device_ptr(x);
-      cs_real_t *d_y = (cs_real_t *)cs_get_device_ptr(y);
+    if (   matrix->vector_multiply[matrix->fill_type][0]
+        == matrix->vector_multiply_d[matrix->fill_type][0]) {
+      cs_alloc_mode_t md_x = cs_check_device_ptr(x);
+      cs_alloc_mode_t md_y = cs_check_device_ptr(y);
+      if (md_x == CS_ALLOC_HOST || md_y == CS_ALLOC_HOST)
+        matrix->vector_multiply_h[matrix->fill_type][0](matrix, false, true,
+                                                        x, y);
+      else {
+        cs_real_t *d_x = (cs_real_t *)cs_get_device_ptr(x);
+        cs_real_t *d_y = (cs_real_t *)cs_get_device_ptr(y);
 
-      matrix->vector_multiply[matrix->fill_type][0](matrix, false, true,
-                                                    d_x, d_y);
+        matrix->vector_multiply[matrix->fill_type][0](matrix, false, true,
+                                                      d_x, d_y);
+      }
     }
     else
       matrix->vector_multiply[matrix->fill_type][0](matrix, false, true, x, y);
@@ -6005,12 +6014,20 @@ cs_matrix_vector_multiply_nosync(const cs_matrix_t  *matrix,
   if (matrix->vector_multiply[matrix->fill_type][0] != NULL) {
 
 #if defined(HAVE_ACCEL)
-    if (matrix->vector_multiply_xy_hd[matrix->fill_type][0] == 'd') {
-      cs_real_t *d_x = (cs_real_t *)cs_get_device_ptr(x);
-      cs_real_t *d_y = (cs_real_t *)cs_get_device_ptr(y);
+    if (   matrix->vector_multiply[matrix->fill_type][0]
+        == matrix->vector_multiply_d[matrix->fill_type][0]) {
+      cs_alloc_mode_t md_x = cs_check_device_ptr(x);
+      cs_alloc_mode_t md_y = cs_check_device_ptr(y);
+      if (md_x == CS_ALLOC_HOST || md_y == CS_ALLOC_HOST)
+        matrix->vector_multiply_h[matrix->fill_type][0](matrix, false, false,
+                                                        x, y);
+      else {
+        cs_real_t *d_x = (cs_real_t *)cs_get_device_ptr(x);
+        cs_real_t *d_y = (cs_real_t *)cs_get_device_ptr(y);
 
-      matrix->vector_multiply[matrix->fill_type][0](matrix, false, false,
-                                                    d_x, d_y);
+        matrix->vector_multiply[matrix->fill_type][0](matrix, false, false,
+                                                      d_x, d_y);
+      }
     }
     else
       matrix->vector_multiply[matrix->fill_type][0](matrix, false, false, x, y);
@@ -6054,12 +6071,20 @@ cs_matrix_vector_multiply_partial(const cs_matrix_t      *matrix,
   if (matrix->vector_multiply[matrix->fill_type][op_type] != NULL) {
 
 #if defined(HAVE_ACCEL)
-    if (matrix->vector_multiply_xy_hd[matrix->fill_type][op_type] == 'd') {
-      cs_real_t *d_x = (cs_real_t *)cs_get_device_ptr(x);
-      cs_real_t *d_y = (cs_real_t *)cs_get_device_ptr(y);
+    if (   matrix->vector_multiply[matrix->fill_type][op_type]
+        == matrix->vector_multiply_d[matrix->fill_type][op_type]) {
+      cs_alloc_mode_t md_x = cs_check_device_ptr(x);
+      cs_alloc_mode_t md_y = cs_check_device_ptr(y);
+      if (md_x == CS_ALLOC_HOST || md_y == CS_ALLOC_HOST)
+        matrix->vector_multiply_h[matrix->fill_type][op_type]
+                  (matrix, true, true, x, y);
+      else {
+        cs_real_t *d_x = (cs_real_t *)cs_get_device_ptr(x);
+        cs_real_t *d_y = (cs_real_t *)cs_get_device_ptr(y);
 
-      matrix->vector_multiply[matrix->fill_type][op_type]
-        (matrix, true, true, d_x, d_y);
+        matrix->vector_multiply[matrix->fill_type][op_type]
+                  (matrix, true, true, d_x, d_y);
+      }
     }
     else
       matrix->vector_multiply[matrix->fill_type][op_type]
@@ -6263,23 +6288,25 @@ cs_matrix_variant_build_list(const cs_matrix_t       *m,
 
 #if defined(HAVE_CUDA)
 
-    _variant_add(_("CSR, CUDA"),
-                 m->type,
-                 m->fill_type,
-                 m->numbering,
-                 "cuda",
-                 n_variants,
-                 &n_variants_max,
-                 m_variant);
+    if (cs_get_device_id() > -1)
+      _variant_add(_("CSR, CUDA"),
+                   m->type,
+                   m->fill_type,
+                   m->numbering,
+                   "cuda",
+                   n_variants,
+                   &n_variants_max,
+                   m_variant);
 
-    _variant_add(_("CSR, with cuSPARSE"),
-                 m->type,
-                 m->fill_type,
-                 m->numbering,
-                 "cusparse",
-                 n_variants,
-                 &n_variants_max,
-                 m_variant);
+    if (cs_get_device_id() > -1)
+      _variant_add(_("CSR, with cuSPARSE"),
+                   m->type,
+                   m->fill_type,
+                   m->numbering,
+                   "cusparse",
+                   n_variants,
+                   &n_variants_max,
+                   m_variant);
 
 #endif /* defined(HAVE_CUDA) */
 
@@ -6311,27 +6338,29 @@ cs_matrix_variant_build_list(const cs_matrix_t       *m,
 
 #if defined(HAVE_CUDA)
 
-    _variant_add(_("MSR, CUDA"),
-                 m->type,
-                 m->fill_type,
-                 m->numbering,
-                 "cuda",
-                 n_variants,
-                 &n_variants_max,
-                 m_variant);
+    if (cs_get_device_id() > -1)
+      _variant_add(_("MSR, CUDA"),
+                   m->type,
+                   m->fill_type,
+                   m->numbering,
+                   "cuda",
+                   n_variants,
+                   &n_variants_max,
+                   m_variant);
 
 #endif /* defined(HAVE_CUDA) */
 
 #if defined(HAVE_CUSPARSE)
 
-    _variant_add(_("MSR, with cuSPARSE"),
-                 m->type,
-                 m->fill_type,
-                 m->numbering,
-                 "cusparse",
-                 n_variants,
-                 &n_variants_max,
-                 m_variant);
+    if (cs_get_device_id() > -1)
+      _variant_add(_("MSR, with cuSPARSE"),
+                   m->type,
+                   m->fill_type,
+                   m->numbering,
+                   "cusparse",
+                   n_variants,
+                   &n_variants_max,
+                   m_variant);
 
 #endif /* defined(HAVE_CUSPARSE) */
 
@@ -6387,21 +6416,65 @@ cs_matrix_variant_apply(cs_matrix_t          *m,
   if (m == NULL || mv == NULL)
     return;
 
+  if (m->destroy_adaptor != NULL)
+    m->destroy_adaptor(m);
+
   if (   m->type < 0 || m->type > CS_MATRIX_N_BUILTIN_TYPES
       || m->fill_type < 0 || m->fill_type > CS_MATRIX_N_FILL_TYPES)
     return;
 
   for (int i = 0; i < 2; i++) {
     m->vector_multiply[m->fill_type][i] = mv->vector_multiply[i];
-    m->vector_multiply_xy_hd[m->fill_type][i] = mv->vector_multiply_xy_hd[i];
 
 #if defined(HAVE_ACCEL)
-    /* When chosen SpMV function is on device, also force device
-       function to the same (instead of default) one */
-    if (m->vector_multiply_xy_hd[m->fill_type][i] == 'd')
+    if (mv->vector_multiply_xy_hd[i] == 'h')
+      m->vector_multiply_h[m->fill_type][i] = mv->vector_multiply[i];
+    else if (mv->vector_multiply_xy_hd[i] == 'd')
       m->vector_multiply_d[m->fill_type][i] = mv->vector_multiply[i];
 #endif
   }
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Apply variants defined by tuning to a given matrix
+ *
+ * \param[in, out]  m   pointer to matrix
+ * \param[in]       mv  pointer to matrix variant pointer
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_matrix_variant_apply_tuned(cs_matrix_t          *m,
+                              cs_matrix_variant_t  *mv)
+{
+  if (m == NULL || mv == NULL)
+    return;
+
+  if (   m->type < 0 || m->type > CS_MATRIX_N_BUILTIN_TYPES
+      || m->fill_type < 0 || m->fill_type > CS_MATRIX_N_FILL_TYPES)
+    return;
+
+  if (m->destroy_adaptor != NULL)
+    m->destroy_adaptor(m);
+
+  for (int i = 0; i < 2; i++)
+    m->vector_multiply[m->fill_type][i] = mv->vector_multiply[i];
+
+#if defined(HAVE_ACCEL)
+  if (cs_get_device_id() > -1) {
+    for (int i = 0; i < 2; i++)
+      m->vector_multiply_h[m->fill_type][i] = (mv+1)->vector_multiply[i];
+    for (int i = 0; i < 2; i++)
+      m->vector_multiply_d[m->fill_type][i] = (mv+2)->vector_multiply[i];
+  }
+  else {
+    for (int i = 0; i < 2; i++)
+      m->vector_multiply_h[m->fill_type][i] = mv->vector_multiply[i];
+    for (int i = 0; i < 2; i++)
+      m->vector_multiply_d[m->fill_type][i] = NULL;
+  }
+#endif
 }
 
 /*----------------------------------------------------------------------------*/

src/alge/cs_matrix.h
@@ -1030,6 +1030,19 @@ void
 cs_matrix_variant_apply(cs_matrix_t          *m,
                         cs_matrix_variant_t  *mv);
 
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Apply variants defined by tuning to a given matrix
+ *
+ * \param[in, out]  m   pointer to matrix
+ * \param[in]       mv  pointer to matrix variant pointer
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_matrix_variant_apply_tuned(cs_matrix_t          *m,
+                              cs_matrix_variant_t  *mv);
+
 /*----------------------------------------------------------------------------
  * Select the sparse matrix-vector product function to be used by a
  * matrix variant for a given fill type.

src/alge/cs_matrix_priv.h
@@ -361,19 +361,16 @@ struct _cs_matrix_t {
 
 #if defined(HAVE_ACCEL)
 
-  /* Function pointer arrays, with CS_MATRIX_N_FILL_TYPES variants,
-     for device: fill_type*4 + full, extra-diagonal, lower, upper parts */
+  /* Function pointer arrays, with CS_MATRIX_N_FILL_TYPES variants, for host
+     or device only: fill_type*4 + full, extra-diagonal, lower, upper parts */
 
+  cs_matrix_vector_product_t  *vector_multiply_h[CS_MATRIX_N_FILL_TYPES]
+                                                [CS_MATRIX_SPMV_N_TYPES];
   cs_matrix_vector_product_t  *vector_multiply_d[CS_MATRIX_N_FILL_TYPES]
                                                 [CS_MATRIX_SPMV_N_TYPES];
 
 #endif /* defined(HAVE_ACCEL) */
 
-  /* Indicate whether main vector_multiply function is handled with
-     host ('h') or device ('d') x and y vector addresses */
-
-  char                   vector_multiply_xy_hd[CS_MATRIX_N_FILL_TYPES]
-                                              [CS_MATRIX_SPMV_N_TYPES];
 };
 
 /* Structure used for tuning variants */

src/alge/cs_matrix_spmv.c
@@ -2961,7 +2961,9 @@ _matrix_spmv_set_func_d(cs_matrix_type_t             m_type,
 
 #if defined(HAVE_CUDA)
 
+#if !defined(HAVE_CUSPARSE_GENERIC_API)
 const char s_cuda[] = "cuda";
+#endif
 
 #if defined(HAVE_CUSPARSE)
 
@@ -3021,6 +3023,11 @@ const char default_name[] = "not_implemented";
 void
 cs_matrix_spmv_set_defaults(cs_matrix_t  *m)
 {
+  char spmv_xy_hd[CS_MATRIX_SPMV_N_TYPES];
+
+  if (m->destroy_adaptor != NULL)
+    m->destroy_adaptor(m);
+
   for (cs_matrix_fill_type_t mft = 0; mft < CS_MATRIX_N_FILL_TYPES; mft++) {
     for (cs_matrix_spmv_type_t spmv_type = 0;
          spmv_type < CS_MATRIX_SPMV_N_TYPES;
@@ -3031,7 +3038,7 @@ cs_matrix_spmv_set_defaults(cs_matrix_t  *m)
                               m->numbering,
                               NULL, /* func_name */
                               m->vector_multiply[mft],
-                              m->vector_multiply_xy_hd[mft]);
+                              spmv_xy_hd);
 #if defined(HAVE_ACCEL)
       _matrix_spmv_set_func_d(m->type,
                               mft,

commit 181b8bc6926af92ee41d74b0afac4aca22ad76d6

Fix crash in some matrix selection cases, broken by commit f595f7e7 (May 7 2022).

src/alge/cs_matrix_default.c
@@ -812,11 +812,15 @@ cs_matrix_default_set_tuned(cs_matrix_t  *m)
   if (_matrix_variant_tuned[m->type][m->fill_type] == NULL
       && (_t_measure > 0 || _n_min_products > 0)) {
 
+    cs_matrix_t *m_t = _get_matrix(m->type);
+    cs_matrix_fill_type_t mft_save = m->fill_type;
+    m_t->fill_type = m->fill_type;
     _matrix_variant_tuned[m->type][m->fill_type]
-      = cs_matrix_variant_tuned(_get_matrix(m->type),
+      = cs_matrix_variant_tuned(m_t,
                                 1,
                                 _n_min_products,
                                 _t_measure);
+    m->fill_type = mft_save;
 
   }
 