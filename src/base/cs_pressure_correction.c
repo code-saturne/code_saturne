bb262e93ccad5342c70fcb899d5017052af0d9b1
LI MA <li.ma@edf.fr>
2022-05-24 10:29:13 +0200

    Remove unused parameters

--- src/base/cs_pressure_correction.c
+++ src/base/cs_pressure_correction.c
@@ -2629,63 +2629,19 @@ _pressure_correction_fv(int        iterns,
  *  \dot{m}^{n+1} = \dot{m}^{n}
  *                 - Hodge \cdot GRAD \left(\delta p \right)
  * \f]
- * \param[in]       iterns    Navier-Stokes iteration number
- * \param[in]       nfbpcd    number of faces with condensation source term
- * \param[in]       ncmast    number of cells with condensation source terms
- * \param[in]       ifbpcd    index of faces with condensation source term
- * \param[in]       ltmast    index of cells with condensation source terms
- * \param[in]       isostd    indicator of standard outlet and index
- *                            of the reference outlet face
  * \param[in]       vel       velocity
- * \param[in, out]  da_uu     velocity matrix
  * \param[in]       coefav    boundary condition array for the variable
  *                            (explicit part)
  * \param[in]       coefbv    boundary condition array for the variable
  *                            (implicit part)
- * \param[in]       coefa_dp  boundary conditions for the pressure increment
- * \param[in]       coefb_dp  boundary conditions for the pressure increment
- * \param[in]       spcond    variable value associated to the condensation
- *                            source term (for ivar=ipr, spcond is the
- *                            flow rate
- *                            \f$ \Gamma_{s,cond}^n \f$)
- * \param[in]       svcond    variable value associated to the condensation
- *                            source term (for ivar=ipr, svcond is the flow rate
- *                            \f$ \Gamma_{v, cond}^n \f$)
- * \param[in]       frcxt     external forces making hydrostatic pressure
- * \param[in]       dfrcxt    variation of the external forces
- *                            composing the hydrostatic pressure
- * \param[in]       i_visc    visc*surface/dist aux faces internes
- * \param[in]       b_visc    visc*surface/dist aux faces de bord
  */
 /*----------------------------------------------------------------------------*/
 
 static void
-_pressure_correction_cdo(int        iterns,
-                         cs_lnum_t  nfbpcd,
-                         cs_lnum_t  ncmast,
-                         cs_lnum_t  ifbpcd[nfbpcd],
-                         cs_lnum_t  ltmast[],
-                         int        isostd[],
-                         cs_real_t  vel[restrict][3],
-                         cs_real_t  da_uu[restrict][6],
+_pressure_correction_cdo(cs_real_t  vel[restrict][3],
                          cs_real_t  coefav[restrict][3],
-                         cs_real_t  coefbv[restrict][3][3],
-                         cs_real_t  coefa_dp[restrict],
-                         cs_real_t  coefb_dp[restrict],
-                         cs_real_t  spcond[restrict],
-                         cs_real_t  svcond[restrict],
-                         cs_real_t  frcxt[restrict][3],
-                         cs_real_t  dfrcxt[restrict][3],
-                         cs_real_t  i_visc[restrict],
-                         cs_real_t  b_visc[restrict])
+                         cs_real_t  coefbv[restrict][3][3])
 {
-  CS_UNUSED(iterns);
-  CS_UNUSED(ncmast);
-  CS_UNUSED(ifbpcd);
-  CS_UNUSED(ltmast);
-  CS_UNUSED(isostd);  /* etc... */
-  /* Remove unused parameters ? */
-
   const cs_mesh_t *m = cs_glob_mesh;
   const cs_mesh_quantities_t *fvq = cs_glob_mesh_quantities;
 
@@ -3369,24 +3325,10 @@ cs_pressure_correction(int        iterns,
                            i_visc,
                            b_visc);
  else
-   _pressure_correction_cdo(iterns,
-                            nfbpcd,
-                            ncmast,
-                            ifbpcd,
-                            ltmast,
-                            isostd,
-                            vel,
-                            da_uu,
+   _pressure_correction_cdo(vel,
                             coefav,
-                            coefbv,
-                            coefa_dp,
-                            coefb_dp,
-                            spcond,
-                            svcond,
-                            frcxt,
-                            dfrcxt,
-                            i_visc,
-                            b_visc);
+                            coefbv);
+
 }
 
 /*----------------------------------------------------------------------------*/
