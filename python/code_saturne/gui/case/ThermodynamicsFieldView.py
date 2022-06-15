code-saturne/code_saturne

commit c48db5b3b6306236d83dac2f1294aeb3d6943743

[GUI] Cleaning and update related to EOS
  * Improve handling of EOS data
  * Allow choosing of different reference tables for a given fluid like
    Water using a given table

python/code_saturne/gui/case/ThermodynamicsFieldView.py
@@ -70,6 +70,7 @@ from code_saturne.model.LocalizationModel import LocalizationModel
 from code_saturne.gui.case.QMegEditorView import QMegEditorView
 from code_saturne.model.NotebookModel import NotebookModel
 
+from code_saturne.model.EosWrapper import eosWrapper
 #-------------------------------------------------------------------------------
 # log config
 #-------------------------------------------------------------------------------
@@ -157,9 +158,14 @@ class MethodDelegate(QItemDelegate):
         else :
             material = self.mdl.getMaterials(fieldId)
             fls = self.eos.getFluidMethods(material)
+
+            # If non condensable-gases, filter the list, only Cathare and
+            # Cathare2 tables are allowed with EOS
+#            if len(self.parent.mdl.ncond.getNonCondensableByFieldId(fieldId)) > 0:
+#                fls = [fli for fli in fls if fli in ("Cathare", "Cathare2")]
+
             for fli in fls:
-                if fli not in ("Ovap","Flica4","StiffenedGas"):
-                    self.modelCombo.addItem(self.tr(fli),fli)
+                self.modelCombo.addItem(self.tr(fli),fli)
 
         editor.setMinimumSize(editor.sizeHint())
         editor.installEventFilter(self)
@@ -177,6 +183,68 @@ class MethodDelegate(QItemDelegate):
         txt = str(comboBox.currentText())
         value = self.modelCombo.dicoV2M[txt]
         log.debug("MethodDelegate value = %s"%value)
+
+        selectionModel = self.parent.selectionModel()
+        for idx in selectionModel.selectedIndexes():
+            if idx.column() == index.column():
+                model.setData(idx, self.modelCombo.dicoM2V[value], Qt.DisplayRole)
+
+
+#-------------------------------------------------------------------------------
+# Combo box delegate for the method
+#-------------------------------------------------------------------------------
+
+
+class ReferenceDelegate(QItemDelegate):
+    """
+    Use of a combo box in the table.
+    """
+    def __init__(self, parent, mdl, dicoM2V, dicoV2M):
+        super(ReferenceDelegate, self).__init__(parent)
+        self.parent   = parent
+        self.mdl      = mdl
+        self.dicoM2V  = dicoM2V
+        self.dicoV2M  = dicoV2M
+        self.eos = eosWrapper()
+
+    def createEditor(self, parent, option, index):
+        editor = QComboBox(parent)
+        self.modelCombo = ComboModel(editor, 1, 1)
+
+        fieldId= index.row() + 1
+
+        material = self.mdl.getMaterials(fieldId)
+        method   = self.mdl.getMethod(fieldId)
+        if material == "user_material" :
+            self.modelCombo.addItem(self.tr(self.dicoM2V["user_material"]), 'user_material')
+        else :
+            if self.eos.isActive():
+                phase = self.mdl.getFieldNature(fieldId)
+
+                if phase == "liquid":
+                    ref = self.eos.getLiquidReferences(material, method)
+                elif phase == "gas":
+                    ref = self.eos.getVaporReferences(material, method)
+
+                for r in ref:
+                    self.modelCombo.addItem(self.tr(r), r)
+
+        editor.setMinimumSize(editor.sizeHint())
+        editor.installEventFilter(self)
+        return editor
+
+
+    def setEditorData(self, comboBox, index):
+        row = index.row()
+        col = index.column()
+        string = index.model().getData(index)[col]
+        self.modelCombo.setItem(str_view=string)
+
+
+    def setModelData(self, comboBox, model, index):
+        txt = str(comboBox.currentText())
+        value = self.modelCombo.dicoV2M[txt]
+        log.debug("ReferenceDelegate value = %s"%value)
 
         selectionModel = self.parent.selectionModel()
         for idx in selectionModel.selectedIndexes():
@@ -236,12 +304,7 @@ class StandardItemModelProperty(QStandardItemModel):
             return Qt.NoItemFlags
         # Lock fields with non condensable gas
         field_id = index.row() + 1
-        if self.ncond.getNonCondensableByFieldId(field_id) != []:
-            return Qt.ItemIsSelectable
-        if index.column() == 0 or index.column() == 3:
-            return Qt.ItemIsEnabled | Qt.ItemIsSelectable
-        else:
-            return Qt.ItemIsEnabled | Qt.ItemIsSelectable | Qt.ItemIsEditable
+        return Qt.ItemIsEnabled | Qt.ItemIsSelectable | Qt.ItemIsEditable
 
 
     def headerData(self, section, orientation, role):
@@ -275,6 +338,12 @@ class StandardItemModelProperty(QStandardItemModel):
             self._data[row][col] = new_met
             self.mdl.setMethod(FieldId, self.dicoV2M[new_met])
 
+        # Reference
+        elif col == 3:
+            new_ref = value
+            self._data[row][col] = new_ref
+            self.mdl.setFluidReference(FieldId, new_ref)
+
         self.updateReference(row)
         if row < 2:
             self.updateTable(0)
@@ -291,7 +360,7 @@ class StandardItemModelProperty(QStandardItemModel):
         fieldId = row + 1
         self._data[row][1] = self.dicoM2V[str(self.mdl.getMaterials(fieldId))]
         self._data[row][2] = self.dicoM2V[str(self.mdl.getMethod(fieldId))]
-        self._data[row][3] = self.mdl.updateReference(fieldId)
+        self._data[row][3] = self.mdl.getFluidReference(fieldId)
 
 
     def updateReference(self, row):
@@ -465,9 +534,8 @@ temperature = enthalpy / 1000;
                 self.dicoV2M[fli] = fli
 
                 for flli in self.eos.getFluidMethods(fli):
-                    if flli not in ("Ovap","Flica4","StiffenedGas"):
-                        self.dicoM2V[flli] = flli
-                        self.dicoV2M[flli] = flli
+                    self.dicoM2V[flli] = flli
+                    self.dicoV2M[flli] = flli
 
         self.list_scalars = []
 
@@ -485,9 +553,14 @@ temperature = enthalpy / 1000;
 
         delegateMaterials = MaterialsDelegate(self.tableViewProperties, self.mdl, self.dicoM2V, self.dicoV2M)
         delegateMethod    = MethodDelegate(self.tableViewProperties, self.mdl, self.dicoM2V, self.dicoV2M)
+        delegateReference = ReferenceDelegate(self.tableViewProperties,
+                                              self.mdl,
+                                              self.dicoM2V,
+                                              self.dicoV2M)
 
         self.tableViewProperties.setItemDelegateForColumn(1, delegateMaterials)
         self.tableViewProperties.setItemDelegateForColumn(2, delegateMethod)
+        self.tableViewProperties.setItemDelegateForColumn(3, delegateReference)
 
         # Combo models
 

python/code_saturne/model/EosWrapper.py
@@ -92,6 +92,15 @@ class eosWrapper(object):
 
         return retval
 
+    def getNumberOfFluidReferences(self, fluid, method):
+        """
+        Get number of available references for a fluid using a given method.
+        """
+
+        retval = len(self.getFluidReferences(fluid, method))
+
+        return retval
+
     def getLiquidReferences(self, fluid, method):
         """
         Get list of available reference methods in a given table/method

python/code_saturne/model/MainFieldsModel.py
@@ -929,13 +929,12 @@ class MainFieldsModel(Variables, Model):
         self.case.undoStop()
         ThermodynamicsModel(self.case).setMaterials(fieldId, material)
         fls = self.eos.getFluidMethods(material)
-        if "Cathare" in fls:
+        if "Cathare2" in fls:
+            ThermodynamicsModel(self.case).setMethod(fieldId, "Cathare")
+        elif "Cathare" in fls:
             ThermodynamicsModel(self.case).setMethod(fieldId, "Cathare")
         else:
-            for fli in fls:
-                if fli != "Ovap" and fli != "Flica4" and fli != "StiffenedGas":
-                    ThermodynamicsModel(self.case).setMethod(fieldId, fli)
-                break
+            ThermodynamicsModel(self.case).setMethod(fieldId, fls[0])
 
     def _deleteFieldsProperties(self):
         Variables(self.case).removeVariableProperty("property", self.XMLNodeproperty, "none", "SaturationTemperature")

python/code_saturne/model/NonCondensableModel.py
@@ -183,12 +183,16 @@ class NonCondensableModel(MainFieldsModel, Variables, Model):
 
         Variables(self.case).setNewVariableProperty("variable", "", self.XMLNodeNonCondensable, fieldId, name, label)
 
-        # for non condensable we need use cathare
+        # for non condensable we need use cathare2 or cathare tables
         from code_saturne.model.ThermodynamicsModel import ThermodynamicsModel
-        ThermodynamicsModel(self.case).setMaterials(1, "Water")
-        ThermodynamicsModel(self.case).setMethod(1, "Cathare")
-        ThermodynamicsModel(self.case).setMaterials(2, "Water")
-        ThermodynamicsModel(self.case).setMethod(2, "Cathare")
+        ref_material = "Water"
+        for m in ("Cathare2", "Cathare"):
+            if m in self.eos.getFluidMethods(ref_material):
+                for i in (1,2):
+                    ThermodynamicsModel(self.case).setMaterials(i, ref_material)
+                    ThermodynamicsModel(self.case).setMethod(i, m)
+
+                break
         del ThermodynamicsModel
 
         return name

python/code_saturne/model/ThermodynamicsModel.py
@@ -269,13 +269,12 @@ class ThermodynamicsModel(MainFieldsModel, Variables, Model):
                 fls = self.eos.getFluidMethods(material)
                 if _default_method in fls:
                     self.setMethod(fieldId, _default_method)
+                elif "Cathare2" in fls:
+                    self.setMethod(fieldId, "Cathare2")
                 elif "Cathare" in fls:
                     self.setMethod(fieldId, "Cathare")
                 else:
-                    for fli in fls:
-                        if fli != "Ovap" and fli != "Flica4" and fli != "StiffenedGas":
-                            self.setMethod(fieldId, fli)
-                            break
+                    self.setMethod(fieldId, fls[0])
             else:
                 self.setMethod(fieldId, "user_properties")
 
@@ -307,16 +306,28 @@ class ThermodynamicsModel(MainFieldsModel, Variables, Model):
         else :
             phase = self.getFieldNature(fieldId)
             if self.eos.isActive():
-                ref = self.eos.getFluidReferences(material, method)
 
-                if len(ref) == 1 :
+                ref_idx = 0
+                if self.eos.getNumberOfFluidReferences(material, method) == 1:
                    # cas des gaz par exemple
-                   reference = ref[0]
+                   ref = self.eos.getFluidReferences(material, method)
                 else :
                    if phase == "liquid" :
-                      reference = self.eos.getLiquidReferences(material, method)[0]
+                      ref = self.eos.getLiquidReferences(material, method)
+                      _s = "Liquid"
                    elif phase == "gas" :
-                      reference = self.eos.getVaporReferences(material, method)[0]
+                      ref = self.eos.getVaporReferences(material, method)
+                      _s = "Vapor"
+
+                    # For Cathare tables with water force IAPWS as default if
+                    # available
+                   if method in ("Cathare","Cathare2") and material == "Water":
+                       for _t in ("IAPWS", "Water"):
+                           if _t+_s in ref:
+                               ref_idx = ref.index(_t+_s)
+                               break
+
+                reference = ref[ref_idx]
             else :
                 reference = material + phase
         # update XML
@@ -324,6 +335,37 @@ class ThermodynamicsModel(MainFieldsModel, Variables, Model):
         childNode.xmlSetAttribute(choice = reference)
 
         return reference
+
+
+    @Variables.undoGlobal
+    def setFluidReference(self, fieldId, choice):
+        """
+        Set new reference choice for the phase
+        """
+        self.check_field_id(fieldId)
+
+        node = self.get_field_node(fieldId)
+        childNode = node.xmlInitChildNode('reference')
+        childNode.xmlSetAttribute(choice = choice)
+
+
+    @Variables.noUndo
+    def getFluidReference(self, fieldId):
+        """
+        Get phase fluid reference
+        """
+        self.check_field_id(fieldId)
+
+        node = self.get_field_node(fieldId)
+        noder = node.xmlGetNode('reference')
+        if noder is None:
+            ref = self.updateReference(fieldId)
+            self.setFluidReference(fieldId, ref)
+
+        ref = node.xmlGetNode('reference')['choice']
+        return ref
+
+
 
     @Variables.noUndo
     def getInitialValue(self, fieldId, tag):

commit 0dcc6e4c554ee3835ecbf3a6cd5c6ed5285f1935

[GUI] Fix lock of fluid EOS reference method in NCFD GUI
  * Bug introduced in commit c48db5b3b6

python/code_saturne/gui/case/ThermodynamicsFieldView.py
@@ -344,7 +344,6 @@ class StandardItemModelProperty(QStandardItemModel):
             self._data[row][col] = new_ref
             self.mdl.setFluidReference(FieldId, new_ref)
 
-        self.updateReference(row)
         if row < 2:
             self.updateTable(0)
             self.updateTable(1)