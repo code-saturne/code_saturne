code-saturne/code_saturne

commit 3a2b2e216a861bdb3c4899ab3f2da9850f770fd1

[CDO] Fix the modification of SLES parameters during the iterative process

src/alge/cs_param_sles.c
@@ -2432,6 +2432,101 @@ cs_param_sles_set(bool                 use_field_id,
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief Update the settings associated to a cs_sles_t structure and apply
+ *        those defined in the given cs_param_sles_t structure.
+ *        This function is used only when a first setup has been performed.
+ *
+ *        One modifies only some specific options like the max. number of
+ *        iterations or the relative tolerance
+ *
+ * \param[in] use_field_id  if false use a name to retrieve the cs_sles_t struc.
+ * \param[in] slesp         pointer to a cs_param_sles_t structure
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_param_sles_update_cvg_settings(bool                     use_field_id,
+                                  const cs_param_sles_t   *slesp)
+{
+  if (slesp == NULL)
+    return;
+
+  /* Retrieve the sles structure associated to this sles parameters */
+
+  const  char  *sles_name = use_field_id ? NULL : slesp->name;
+  assert(slesp->field_id > -1 || sles_name != NULL);
+
+  cs_sles_t  *sles = cs_sles_find(slesp->field_id, sles_name);
+
+  if (sles == NULL)
+    return;
+
+  switch (slesp->solver_class) {
+
+  case CS_PARAM_SLES_CLASS_CS: /* code_saturne's own solvers */
+    {
+      switch (slesp->solver) {
+
+      case CS_PARAM_ITSOL_AMG:
+        {
+          cs_multigrid_t  *mg = cs_sles_get_context(sles);
+          assert(mg != NULL);
+
+          cs_multigrid_set_max_cycles(mg, slesp->n_max_iter);
+        }
+        break;
+
+      case CS_PARAM_ITSOL_GCR:
+      case CS_PARAM_ITSOL_GMRES:
+        {
+          cs_sles_it_t  *itsol = cs_sles_get_context(sles);
+          assert(itsol != NULL);
+
+          cs_sles_it_set_n_max_iter(itsol, slesp->n_max_iter);
+          cs_sles_it_set_restart_interval(itsol, slesp->restart);
+        }
+        break;
+
+      default:
+        {
+          cs_sles_it_t  *itsol = cs_sles_get_context(sles);
+          assert(itsol != NULL);
+
+          cs_sles_it_set_n_max_iter(itsol, slesp->n_max_iter);
+        }
+        break;
+
+      } /* which solver */
+
+    }   /* code_saturne class */
+    break;
+
+  case CS_PARAM_SLES_CLASS_PETSC:
+    {
+      cs_sles_petsc_t  *petsc_ctx = cs_sles_get_context(sles);
+      assert(petsc_ctx);
+
+      cs_sles_petsc_set_cvg_criteria(petsc_ctx, slesp->eps, slesp->n_max_iter);
+    }
+    break;
+
+  case CS_PARAM_SLES_CLASS_HYPRE:
+    {
+      cs_sles_hypre_t  *hypre_ctx = cs_sles_get_context(sles);
+      assert(hypre_ctx);
+
+      cs_sles_hypre_set_n_max_iter(hypre_ctx, slesp->n_max_iter);
+    }
+    break;
+
+  default:
+    /* CS_PARAM_SLES_CLASS_MUMPS => Nothing to do */
+    break;
+  }
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief Retrieve the related solver class from the amg type
  *
  * \param[in]  amg_type    type of AMG to consider

src/alge/cs_multigrid.c
@@ -4095,6 +4095,27 @@ cs_multigrid_set_solver_options(cs_multigrid_t     *mg,
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief Set the max. number of cycles for a multigrid
+ *
+ * \param[in, out]  mg              pointer to multigrid info and context
+ * \param[in]       n_max_cycles    maximum number of cycles
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_multigrid_set_max_cycles(cs_multigrid_t     *mg,
+                            int                 n_max_cycles)
+{
+  if (mg == NULL)
+    return;
+
+  cs_multigrid_info_t  *info = &(mg->info);
+
+  info->n_max_cycles = n_max_cycles;
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief Return solver type used on fine mesh.
  *
  * \param[in]  mg  pointer to multigrid info and context

src/alge/cs_multigrid.h
@@ -230,6 +230,19 @@ cs_multigrid_set_solver_options(cs_multigrid_t     *mg,
                                 double              precision_mult_ascent,
                                 double              precision_mult_coarse);
 
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Set the max. number of cycles for a multigrid
+ *
+ * \param[in, out]  mg              pointer to multigrid info and context
+ * \param[in]       n_max_cycles    maximum number of cycles
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_multigrid_set_max_cycles(cs_multigrid_t     *mg,
+                            int                 n_max_cycles);
+
 /*----------------------------------------------------------------------------
  * Return solver type used on fine mesh.
  *

src/alge/cs_param_sles.h
@@ -173,6 +173,24 @@ cs_param_sles_set(bool                 use_field_id,
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief Update the settings associated to a cs_sles_t structure and apply
+ *        those defined in the given cs_param_sles_t structure.
+ *        This function is used only when a first setup has been performed.
+ *
+ *        One modifies only some specific options like the max. number of
+ *        iterations or the relative tolerance
+ *
+ * \param[in] use_field_id  if false use a name to retrieve the cs_sles_t struc.
+ * \param[in] slesp         pointer to a cs_param_sles_t structure
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_param_sles_update_cvg_settings(bool                     use_field_id,
+                                  const cs_param_sles_t   *slesp);
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief Retrieve the related solver class from the amg type
  *
  * \param[in]  amg_type    type of AMG to consider

src/alge/cs_sles.c
@@ -1384,6 +1384,7 @@ cs_sles_define(int                 f_id,
     sles->type_id = cs_map_name_to_id(_type_name_map, type_name);
 
   /* Now define options */
+
   sles->context = context;
   sles->setup_func = setup_func;
   sles->solve_func = solve_func;

src/alge/cs_sles_hypre.c
@@ -1258,6 +1258,61 @@ cs_sles_hypre_free(void  *context)
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief Set the max. number of iterations associated to the given HYPRE
+ *        contrext
+ *
+ * \param[in,out]  context       pointer to HYPRE linear solver info
+ * \param[in]      n_max_iter    max. number of iterations
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_sles_hypre_set_n_max_iter(cs_sles_hypre_t   *context,
+                             int                n_max_iter)
+{
+  if (context == NULL)
+    return;
+
+  cs_sles_hypre_setup_t *sd = context->setup_data;
+
+  if (sd == NULL)
+    return; /* No need to continue. This will be done during the first call to
+               the solve function */
+
+  switch(context->solver_type) {
+
+  case CS_SLES_HYPRE_BOOMERAMG:
+    HYPRE_BoomerAMGSetMaxIter(sd->solver, n_max_iter);
+    break;
+
+  case CS_SLES_HYPRE_BICGSTAB:
+    HYPRE_ParCSRBiCGSTABSetMaxIter(sd->solver, n_max_iter);
+    break;
+
+  case CS_SLES_HYPRE_GMRES:
+    HYPRE_GMRESSetMaxIter(sd->solver, n_max_iter);
+    break;
+
+  case CS_SLES_HYPRE_FLEXGMRES:
+    HYPRE_ParCSRFlexGMRESSetMaxIter(sd->solver, n_max_iter);
+    break;
+
+  case CS_SLES_HYPRE_LGMRES:
+    HYPRE_ParCSRLGMRESSetMaxIter(sd->solver, n_max_iter);
+    break;
+
+  case CS_SLES_HYPRE_PCG:
+    HYPRE_PCGSetMaxIter(sd->solver, n_max_iter);
+    break;
+
+  default:
+    bft_error(__FILE__, __LINE__, 0, "%s: Solver not handled yet", __func__);
+
+  }
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief Define whether the solver should run on the host or
  *        accelerated device.
  *

src/alge/cs_sles_hypre.h
@@ -314,6 +314,20 @@ cs_sles_hypre_log(const void  *context,
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief Set the max. number of iterations associated to the given HYPRE
+ *        contrext
+ *
+ * \param[in,out]  context       pointer to HYPRE linear solver info
+ * \param[in]      n_max_iter    max. number of iterations
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_sles_hypre_set_n_max_iter(cs_sles_hypre_t   *context,
+                             int                n_max_iter);
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief Define whether the solver should run on the host or
  *        accelerated device.
  *

src/alge/cs_sles_it.c
@@ -4857,6 +4857,25 @@ cs_sles_it_set_restart_interval(cs_sles_it_t                 *context,
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief Define the max. number of iterations before stoping the algorithm
+ *
+ * \param[in, out]  context     pointer to iterative solver info and context
+ * \param[in]       n_max_iter  max. number of iterations
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_sles_it_set_n_max_iter(cs_sles_it_t        *context,
+                          int                  n_max_iter)
+{
+  if (context == NULL)
+    return;
+
+  context->n_max_iter = n_max_iter;
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief Query mean number of rows under which Conjugate Gradient algorithm
  *        uses the single-reduction variant.
  *

commit 9b1a58e375dd925f58ced1b897ac92ebf7e79f09

Fix compilation broken by commit 3a2b2e21

src/alge/cs_param_sles.c
@@ -2501,6 +2501,7 @@ cs_param_sles_update_cvg_settings(bool                     use_field_id,
     }   /* code_saturne class */
     break;
 
+#if defined(HAVE_PETSC)
   case CS_PARAM_SLES_CLASS_PETSC:
     {
       cs_sles_petsc_t  *petsc_ctx = cs_sles_get_context(sles);
@@ -2509,7 +2510,9 @@ cs_param_sles_update_cvg_settings(bool                     use_field_id,
       cs_sles_petsc_set_cvg_criteria(petsc_ctx, slesp->eps, slesp->n_max_iter);
     }
     break;
+#endif
 
+#if defined(HAVE_HYPRE)
   case CS_PARAM_SLES_CLASS_HYPRE:
     {
       cs_sles_hypre_t  *hypre_ctx = cs_sles_get_context(sles);
@@ -2518,6 +2521,7 @@ cs_param_sles_update_cvg_settings(bool                     use_field_id,
       cs_sles_hypre_set_n_max_iter(hypre_ctx, slesp->n_max_iter);
     }
     break;
+#endif
 
   default:
     /* CS_PARAM_SLES_CLASS_MUMPS => Nothing to do */