code-saturne/code_saturne

commit f49e29206da399b75d4f0cd9ed525d416965e700

[GUI] Add backflow section title to outlet boundary condition in NCFD

python/code_saturne/gui/case/BoundaryConditionsViewNeptune.py
@@ -171,6 +171,7 @@ class BoundaryConditionsView(QWidget, Ui_BoundaryConditions):
 
         Ui_BoundaryConditions.__init__(self)
         self.setupUi(self)
+        self.create_widgets()
 
         self.case = case
         self.case.undoStopGlobal()
@@ -198,6 +199,18 @@ class BoundaryConditionsView(QWidget, Ui_BoundaryConditions):
         self.__slotSelectBoundary()
 
         self.case.undoStartGlobal()
+
+    def create_widgets(self):
+        """ Create widgets in a programmatic way (elements should progressively be
+        translated in python, and eventually moved in a separate python file
+        (cf. initiative local 2021)"""
+        layout = QGridLayout()
+        self.BackflowSeparator.setLayout(layout)
+        # Group backflow parameters into the backflow groupbox
+        layout.addWidget(self.EnergyWidget, 0, 0, 1, 1)
+        layout.addWidget(self.FractionWidget, 1, 0, 1, 1)
+        layout.addWidget(self.NonCondensableWidget, 2, 0, 1, 1)
+        layout.addWidget(self.ScalarWidget, 3, 0, 1, 1)
 
     def __initializeFieldTable(self):
         self.tableModelFields = StandardItemModelMainFields(self.mdl)
@@ -344,9 +357,9 @@ class BoundaryConditionsView(QWidget, Ui_BoundaryConditions):
             self.NonCondensableWidget.setup(self.case, self.__currentField)
             self.NonCondensableWidget.showWidget(boundary)
             self.InterfacialAreaWidget.hideWidget()
-
-        self.ScalarWidget.setup(self.case, self.__currentField)
-        self.ScalarWidget.showWidget(boundary)
+            self.ScalarWidget.setup(self.case, self.__currentField)
+            self.ScalarWidget.showWidget(boundary)
+            self.BackflowSeparator.show()
 
         self.WallWidget.hideWidget()
 
@@ -356,6 +369,7 @@ class BoundaryConditionsView(QWidget, Ui_BoundaryConditions):
         Hides all promoted QWidgets, for all nature.
         """
         self.PressureWidget.hideWidget()
+        self.BackflowSeparator.hide()
         self.VelocityWidget.hideWidget()
         self.TurbulenceWidget.hideWidget()
         self.EnergyWidget.hideWidget()

python/code_saturne/gui/case/BoundaryConditionsNeptune.ui
@@ -6,8 +6,8 @@
    <rect>
     <x>0</x>
     <y>0</y>
-    <width>251</width>
-    <height>516</height>
+    <width>319</width>
+    <height>780</height>
    </rect>
   </property>
   <property name="windowTitle">
@@ -20,16 +20,16 @@
       <string>Boundary conditions</string>
      </property>
      <layout class="QGridLayout" name="gridLayout">
-      <item row="3" column="0">
+      <item row="4" column="0">
        <widget class="BoundaryConditionsVelocityInletView" name="VelocityWidget" native="true"/>
       </item>
       <item row="1" column="0">
        <widget class="BoundaryConditionsPressureView" name="PressureWidget" native="true"/>
       </item>
-      <item row="2" column="0">
+      <item row="3" column="0">
        <widget class="BoundaryConditionsFractionView" name="FractionWidget" native="true"/>
       </item>
-      <item row="4" column="0">
+      <item row="5" column="0">
        <widget class="BoundaryConditionsTurbulenceInletView" name="TurbulenceWidget" native="true"/>
       </item>
       <item row="0" column="0">
@@ -48,39 +48,46 @@
         </property>
        </widget>
       </item>
-      <item row="5" column="0">
+      <item row="6" column="0">
        <widget class="BoundaryConditionsEnergyView" name="EnergyWidget" native="true"/>
       </item>
-      <item row="6" column="0">
+      <item row="7" column="0">
        <widget class="BoundaryConditionsNonCondensableView" name="NonCondensableWidget" native="true"/>
       </item>
-      <item row="7" column="0">
+      <item row="8" column="0">
        <widget class="BoundaryConditionsInterfacialAreaView" name="InterfacialAreaWidget" native="true"/>
       </item>
-      <item row="8" column="0">
+      <item row="9" column="0">
        <widget class="BoundaryConditionsScalarView" name="ScalarWidget" native="true"/>
       </item>
-      <item row="9" column="0">
+      <item row="10" column="0">
        <widget class="BoundaryConditionsWallView" name="WallWidget" native="true"/>
+      </item>
+      <item row="2" column="0">
+       <widget class="QGroupBox" name="BackflowSeparator" native="true">
+       <property name="title">
+        <string>Backflow conditions</string>
+       </property>
+       </widget>
       </item>
      </layout>
     </widget>
    </item>
+   <item row="1" column="0">
+    <widget class="LagrangianBoundaryView" name="particlesWidget" native="true"/>
+   </item>
    <item row="2" column="0">
-    <spacer>
+    <spacer name="verticalSpacer">
      <property name="orientation">
       <enum>Qt::Vertical</enum>
      </property>
      <property name="sizeHint" stdset="0">
       <size>
-       <width>16</width>
-       <height>16</height>
+       <width>20</width>
+       <height>40</height>
       </size>
      </property>
     </spacer>
-   </item>
-   <item row="1" column="0">
-    <widget class="LagrangianBoundaryView" name="particlesWidget" native="true"/>
    </item>
   </layout>
  </widget>

commit a0c271b87028f526d2052bf3b431f63189b147c7

[GUI] Fix thermal and scalar BC in neptune_cfd
Bug introduced in commit f49e29206

python/code_saturne/gui/case/BoundaryConditionsViewNeptune.py
@@ -171,7 +171,6 @@ class BoundaryConditionsView(QWidget, Ui_BoundaryConditions):
 
         Ui_BoundaryConditions.__init__(self)
         self.setupUi(self)
-        self.create_widgets()
 
         self.case = case
         self.case.undoStopGlobal()
@@ -196,21 +195,10 @@ class BoundaryConditionsView(QWidget, Ui_BoundaryConditions):
         self.particlesWidget.setup(self.case)
 
         self.__hideAllWidgets()
+
         self.__slotSelectBoundary()
 
         self.case.undoStartGlobal()
-
-    def create_widgets(self):
-        """ Create widgets in a programmatic way (elements should progressively be
-        translated in python, and eventually moved in a separate python file
-        (cf. initiative local 2021)"""
-        layout = QGridLayout()
-        self.BackflowSeparator.setLayout(layout)
-        # Group backflow parameters into the backflow groupbox
-        layout.addWidget(self.EnergyWidget, 0, 0, 1, 1)
-        layout.addWidget(self.FractionWidget, 1, 0, 1, 1)
-        layout.addWidget(self.NonCondensableWidget, 2, 0, 1, 1)
-        layout.addWidget(self.ScalarWidget, 3, 0, 1, 1)
 
     def __initializeFieldTable(self):
         self.tableModelFields = StandardItemModelMainFields(self.mdl)
@@ -359,7 +347,14 @@ class BoundaryConditionsView(QWidget, Ui_BoundaryConditions):
             self.InterfacialAreaWidget.hideWidget()
             self.ScalarWidget.setup(self.case, self.__currentField)
             self.ScalarWidget.showWidget(boundary)
+            # Group backflow parameters into the backflow groupbox
             self.BackflowSeparator.show()
+            layout = QGridLayout()
+            layout.addWidget(self.EnergyWidget, 0, 0, 1, 1)
+            layout.addWidget(self.FractionWidget, 1, 0, 1, 1)
+            layout.addWidget(self.NonCondensableWidget, 2, 0, 1, 1)
+            layout.addWidget(self.ScalarWidget, 3, 0, 1, 1)
+            self.BackflowSeparator.setLayout(layout)
 
         self.WallWidget.hideWidget()
 