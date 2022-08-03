code-saturne/code_saturne

commit bedeb92b0d314377d082ab9d05c63d6d620ceb30

[GUI] Use constant interfacial area model for particle flows
And enforce the use of default parameters for interfacial area model
when activating a predefined flow

python/code_saturne/model/InterfacialAreaModel.py
@@ -43,6 +43,7 @@ class InterfacialAreaModel(MainFieldsModel, Variables, Model):
         """
         #
         # XML file parameters
+        print("DEBUG - in InterfacialAreaModel.__init__")
         MainFieldsModel.__init__(self, case)
         self.case              = case
         self.XMLclosure        = self.case.xmlGetNode('closure_modeling')
@@ -57,18 +58,37 @@ class InterfacialAreaModel(MainFieldsModel, Variables, Model):
     def defaultValues(self):
         default = {}
 
+        default['areamodel'] = "constant"
         default['diameter'] = 0.001
+        default['sourceterm'] = "no_coalescence_no_fragmentation"
         default['mindiam'] = 1.e-6
         default['maxdiam'] = 0.1
-        default['coupling'] = "uncoupled"
-        default['areamodel'] = "constant"
-        default['sourcetermgas'] = "no_coalescence_no_fragmentation"
-        default['sourcetermsolid'] = "no_coalescence_no_fragmentation"
-        default['areamodel'] = "interfacial_area_transport"
-        default['sourcetermgas'] = "ruyer_seiler"
+
+        flow_type = self.getPredefinedFlow()
+        if flow_type == "None":
+            flow_type = self.detectFlowType()
+
+        if flow_type in ["boiling_flow", "droplet_flow", "multiregime_flow"]:
+            default['areamodel'] = "interfacial_area_transport"
+            default['sourceterm'] = "ruyer_seiler"
 
         return default
 
+    def setDefaultParameters(self, field_id):
+        """
+        Reset all parameters to default when activating a predefined flow (non user)
+        """
+        predefined_flow = self.getPredefinedFlow()
+        if predefined_flow == "None":
+            return
+
+        default = self.defaultValues()
+        self.setAreaModel(field_id, default['areamodel'])
+        self.setInitialDiameter(field_id, default['diameter'])
+        if default['areamodel'] == "constant":
+            self.setSourceTerm(field_id, default['sourceterm'])
+            self.setMinDiameter(field_id, default['mindiam'])
+            self.setMaxDiameter(field_id, default['maxdiam'])
 
     def getAreaModelList(self) :
         """
@@ -183,10 +203,7 @@ class InterfacialAreaModel(MainFieldsModel, Variables, Model):
         noden = node.xmlGetNode('source_term')
         if noden is None :
             model = ""
-            if self.getFieldNature(fieldId) == "gas" :
-               model = self.defaultValues()['sourcetermgas']
-            else :
-               model = self.defaultValues()['sourcetermsolid']
+            model = self.defaultValues()['sourceterm']
             self.setSourceTerm(fieldId, model)
         model = node.xmlGetNode('source_term')['model']
 

python/code_saturne/gui/case/AnalysisFeaturesView.py
@@ -62,6 +62,7 @@ from code_saturne.model.AtmosphericFlowsModel import AtmosphericFlowsModel
 from code_saturne.model.GroundwaterModel import GroundwaterModel
 from code_saturne.model.MainFieldsModel import MainFieldsModel
 from code_saturne.model.InterfacialForcesModel import InterfacialForcesModel
+from code_saturne.model.InterfacialAreaModel import InterfacialAreaModel
 from code_saturne.model.NeptuneWallTransferModel import NeptuneWallTransferModel
 from code_saturne.model.InterfacialEnthalpyModel import InterfacialEnthalpyModel
 from code_saturne.model.HgnModel import HgnModel
@@ -754,7 +755,12 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
                                           QMessageBox.Yes | QMessageBox.No)
             if choice == QMessageBox.Yes:
                 self.nept.setPredefinedFlow(model)
+                #TODO : the setDefaultParameters method should be moved to setPredefinedFlow
+                # For now, this is not possible as it would introduce circular dependencies
+                # (InterfacialForcesModel and InterfacialAreaModel are children of MainFieldsModel)
                 InterfacialForcesModel(self.case).setDefaultParameters("1", "2")
+                InterfacialAreaModel(self.case).setDefaultParameters("1")
+                InterfacialAreaModel(self.case).setDefaultParameters("2")
             else:
                 self.modelNeptuneCFD.setItem(str_model=model_p)
                 return

commit 65b336f6a87d2e02775ebc526843530854bb409d

[GUI] Fix wrong default values in neptune_cfd
Introduced in bedeb92b

python/code_saturne/model/InterfacialAreaModel.py
@@ -67,9 +67,12 @@ class InterfacialAreaModel(MainFieldsModel, Variables, Model):
         if flow_type == "None":
             flow_type = self.detectFlowType()
 
-        if flow_type in ["boiling_flow", "droplet_flow", "multiregime_flow"]:
+        if flow_type in ["boiling_flow", "multiregime_flow"]:
             default['areamodel'] = "interfacial_area_transport"
             default['sourceterm'] = "ruyer_seiler"
+        elif flow_type == "droplet_flow":
+            default['areamodel'] = "interfacial_area_transport"
+            default['sourceterm'] = "no_coalescence_no_fragmentation"
 
         return default
 