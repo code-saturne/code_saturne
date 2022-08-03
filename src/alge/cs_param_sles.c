code-saturne/code_saturne

commit bebe1b2fde6a821cf3fb735c82394406990d3d2c

[CDO] (SLES) Plug MUMPS as preconditioner into the different strategies

src/alge/cs_param_sles.c
@@ -1725,6 +1725,16 @@ _set_saturne_sles(bool                 use_field_id,
 
     } /* Multigrid as preconditioner */
 
+    else if (slesp->precond == CS_PARAM_PRECOND_MUMPS            ||
+             slesp->precond == CS_PARAM_PRECOND_MUMPS_FLOAT      ||
+             slesp->precond == CS_PARAM_PRECOND_MUMPS_FLOAT_LDLT ||
+             slesp->precond == CS_PARAM_PRECOND_MUMPS_LDLT) {
+
+      pc = cs_sles_mumps_pc_create(slesp);
+      cs_sles_it_transfer_pc(itsol, &pc);
+
+    }
+
   } /* preconditioner is not defined */
 
   /* In case of high verbosity, additional output are generated */

src/base/cs_param_types.c
@@ -647,6 +647,18 @@ cs_param_get_precond_name(cs_param_precond_type_t  precond)
   case CS_PARAM_PRECOND_ICC0:
     return  "ICC0";
     break;
+  case CS_PARAM_PRECOND_MUMPS:
+    return  "dMUMPS (LU)";
+    break;
+  case CS_PARAM_PRECOND_MUMPS_FLOAT:
+    return  "sMUMPS (LU)";
+    break;
+  case CS_PARAM_PRECOND_MUMPS_FLOAT_LDLT:
+    return  "sMUMPS (LDLt)";
+    break;
+  case CS_PARAM_PRECOND_MUMPS_LDLT:
+    return  "dMUMPS (LDLt)";
+    break;
   case CS_PARAM_PRECOND_POLY1:
     return  "Neumann.Poly.O1";
     break;

src/base/cs_param_types.h
@@ -775,7 +775,7 @@ typedef enum {
  * Variant with GMRES as inner solver.
  *
  * \var CS_PARAM_PRECOND_LU
- * LU factorization (direct solver)
+ * LU factorization (direct solver) with PETSc
  *
  * \var CS_PARAM_PRECOND_ILU0
  * Incomplete LU factorization (fill-in coefficient set to 0)
@@ -784,6 +784,24 @@ typedef enum {
  * Incomplete Cholesky factorization (fill-in coefficient set to 0). This is
  * variant of the ILU0 preconditioner dedicated to symmetric positive definite
  * system
+ *
+ * \var CS_PARAM_PRECOND_MUMPS
+ * Direct solver available with the MUMPS library and used as a preconditioner
+ * Case of a L.U factorization and double-precision
+ *
+ * \var CS_PARAM_PRECOND_MUMPS_FLOAT
+ * Direct solver available with the MUMPS library and used as a preconditioner
+ * Case of a L.U factorization and single-precision
+ *
+ * \var CS_PARAM_PRECOND_MUMPS_FLOAT_LDLT
+ * Direct solver available with the MUMPS library and used as a preconditioner
+ * Case of a L.D.Lt factorization and single-precision
+ * The input matrix should be symmetric.
+ *
+ * \var CS_PARAM_PRECOND_MUMPS_LDLT
+ * Direct solver available with the MUMPS library and used as a preconditioner
+ * Case of a L.D.Lt factorization and double-precision
+ * The input matrix should be symmetric.
  *
  * \var CS_PARAM_PRECOND_POLY1
  * Neumann polynomial preconditioning. Polynoms of order 1.
@@ -800,15 +818,19 @@ typedef enum {
 
   CS_PARAM_PRECOND_NONE,
 
-  CS_PARAM_PRECOND_BJACOB_ILU0, /*!< Only with PETSc */
-  CS_PARAM_PRECOND_BJACOB_SGS,  /*!< Only with PETSc */
+  CS_PARAM_PRECOND_BJACOB_ILU0,        /*!< Only with PETSc */
+  CS_PARAM_PRECOND_BJACOB_SGS,         /*!< Only with PETSc */
   CS_PARAM_PRECOND_AMG,
   CS_PARAM_PRECOND_DIAG,
-  CS_PARAM_PRECOND_GKB_CG,      /*!< Only with PETSc */
-  CS_PARAM_PRECOND_GKB_GMRES,   /*!< Only with PETSc */
-  CS_PARAM_PRECOND_LU,
-  CS_PARAM_PRECOND_ILU0,        /*!< Only with PETSc */
-  CS_PARAM_PRECOND_ICC0,        /*!< Only with PETSc*/
+  CS_PARAM_PRECOND_GKB_CG,             /*!< Only with PETSc */
+  CS_PARAM_PRECOND_GKB_GMRES,          /*!< Only with PETSc */
+  CS_PARAM_PRECOND_LU,                 /*!< Only with PETSc */
+  CS_PARAM_PRECOND_ILU0,               /*!< Only with PETSc */
+  CS_PARAM_PRECOND_ICC0,               /*!< Only with PETSc */
+  CS_PARAM_PRECOND_MUMPS,              /*!< Only with MUMPS */
+  CS_PARAM_PRECOND_MUMPS_FLOAT,        /*!< Only with MUMPS */
+  CS_PARAM_PRECOND_MUMPS_FLOAT_LDLT,   /*!< Only with MUMPS */
+  CS_PARAM_PRECOND_MUMPS_LDLT,         /*!< Only with MUMPS */
   CS_PARAM_PRECOND_POLY1,
   CS_PARAM_PRECOND_POLY2,
   CS_PARAM_PRECOND_SSOR,

src/cdo/cs_equation_param.c
@@ -855,6 +855,29 @@ _set_key(cs_equation_param_t   *eqp,
       } /* End of switch */
 
     }
+    else if (strcmp(keyval, "mumps") == 0            ||
+             strcmp(keyval, "mumps_float") == 0      ||
+             strcmp(keyval, "mumps_float_ldlt") == 0 ||
+             strcmp(keyval, "mumps_ldlt") == 0 ) {
+
+      if (cs_param_sles_check_class(CS_PARAM_SLES_CLASS_MUMPS) !=
+          CS_PARAM_SLES_CLASS_MUMPS)
+        bft_error(__FILE__, __LINE__, 0,
+                  " %s(): Eq. %s Error detected while setting \"%s\" key.\n"
+                  " MUMPS is not available with your installation.\n"
+                  " Please check your installation settings.\n",
+                  __func__, eqname, "CS_EQKEY_PRECOND");
+
+      if (strcmp(keyval, "mumps") == 0)
+        eqp->sles_param->precond = CS_PARAM_PRECOND_MUMPS;
+      else if (strcmp(keyval, "mumps_float") == 0)
+        eqp->sles_param->precond = CS_PARAM_PRECOND_MUMPS_FLOAT;
+      else if (strcmp(keyval, "mumps_float_ldlt") == 0)
+        eqp->sles_param->precond = CS_PARAM_PRECOND_MUMPS_FLOAT_LDLT;
+      else if (strcmp(keyval, "mumps_ldlt") == 0)
+        eqp->sles_param->precond = CS_PARAM_PRECOND_MUMPS_LDLT;
+
+    }
     else if (strcmp(keyval, "poly1") == 0) {
       eqp->sles_param->precond = CS_PARAM_PRECOND_POLY1;
       eqp->sles_param->solver_class = CS_PARAM_SLES_CLASS_CS;

commit 847d14bc19f21ac2e93c44fd2aa04d8247dd8e6d

Fix compilation without MUMPS, broken by bebe1b2f (today).

src/alge/cs_param_sles.c
@@ -1730,7 +1730,12 @@ _set_saturne_sles(bool                 use_field_id,
              slesp->precond == CS_PARAM_PRECOND_MUMPS_FLOAT_LDLT ||
              slesp->precond == CS_PARAM_PRECOND_MUMPS_LDLT) {
 
+#if defined(HAVE_MUMPS)
       pc = cs_sles_mumps_pc_create(slesp);
+#else
+      bft_error(__FILE__, __LINE__, 0, "%s: MUMPS not available in this build.",
+                __func__);
+#endif
       cs_sles_it_transfer_pc(itsol, &pc);
 
     }