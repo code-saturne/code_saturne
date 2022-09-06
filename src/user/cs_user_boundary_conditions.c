code-saturne/code_saturne

commit 69511e8a37626f7c6edf9b12a033b7d68c4710d3

Change cs_user_boundary_conditions arguments for cleaner icodcl/rcodcl access.
This is mode possible following commit 73567423 (today).

src/user/cs_user_boundary_conditions.c
@@ -37,10 +37,6 @@
 #include <stdio.h>
 #include <string.h>
 
-#if defined(HAVE_MPI)
-#include <mpi.h>
-#endif
-
 /*----------------------------------------------------------------------------
  * Local headers
  *----------------------------------------------------------------------------*/
@@ -89,52 +85,48 @@ cs_user_boundary_conditions_setup(cs_domain_t  *domain)
 /*!
  * \brief User definition of boundary conditions
  *
- * \param[in]     nvar          total number of variable BC's
- * \param[in]     bc_type       boundary face types
- * \param[in]     icodcl        boundary face code
- *                                - 1  -> Dirichlet
- *                                - 2  -> convective outlet
- *                                - 3  -> flux density
- *                                - 4  -> sliding wall and u.n=0 (velocity)
- *                                - 5  -> friction and u.n=0 (velocity)
- *                                - 6  -> roughness and u.n=0 (velocity)
- *                                - 9  -> free inlet/outlet (velocity)
- *                                inflowing possibly blocked
- * \param[in]     rcodcl        boundary condition values
- *                                rcodcl(3) = flux density value
- *                                (negative for gain) in W/m2
+ * \param[in, out]  domain   pointer to a cs_domain_t structure
+ * \param[in, out]  bc_type  boundary face types
  *
  * The icodcl and rcodcl arrays are pre-initialized based on default
  * and GUI-defined definitions, and may be modified here.
  *
- * For a given variable id "ivar" and a given face "face_id", these arrays
+ * For a given variable field f, and a given face "face_id", these arrays
  * may be used as follows:
  *
  * - Boundary condition type code given at:
- *  icodcl[ivar*n_b_faces + face_id]
+ *   f->bc_coefficients->icodcl[face_id]
  *
  * - Dirichlet value defined at:
- *   rcodcl[ivar*n_b_faces + face_id]
+ *   f->bc_coefficients->rcodcl1[face_id]
  *
  * - Interior exchange coefficient (infinite if no exchange) at:
- *   rcodcl[(ivar +   nvar)*n_b_faces + face_id] = interior exchange
+ *   f->bc_coefficients->rcodcl2[face_id]
  *
  * - Flux density defined at:
- *   rcodcl[(ivar + 2*nvar)*n_b_faces + face_id];
+ *   f->bc_coefficients->rcodcl3[face_id]
  *
- * For a given field f, the "ivar" variable id may be obtained as follows:
- *   int ivar = cs_field_get_key_int(f, cs_field_key_id("variable_id")) - 1;
+ * For vector or tensor fields, these arrays are not interleaved,
+ * so for a given face "face_id" and field component "comp_id", acess
+ * is as follows (where n_b_faces is domain->mesh->n_b_faces):
+ *
+ *   f->bc_coefficients->rcodcl1[n_b_faces*comp_id + face_id]
+ *   f->bc_coefficients->rcodcl2[n_b_faces*comp_id + face_id]
+ *   f->bc_coefficients->rcodcl3[n_b_faces*comp_id + face_id]
+ *
+ * Only the icodcl code values from the first component are used in the case
+ * of vector or tensor fields, so the icodcl values can be defined as for
+ * a scalar.
  */
 /*----------------------------------------------------------------------------*/
 
 #pragma weak cs_user_boundary_conditions
 void
-cs_user_boundary_conditions(int         nvar,
-                            int         bc_type[],
-                            int         icodcl[],
-                            cs_real_t   rcodcl[])
+cs_user_boundary_conditions(cs_domain_t  *domain,
+                            int           bc_type[])
 {
-
+  CS_UNUSED(domain);
+  CS_UNUSED(bc_type);
 }
 
 /*----------------------------------------------------------------------------*/

src/base/caltri.f90
@@ -107,9 +107,9 @@ double precision, pointer, dimension(:)   :: porosi => null()
 double precision, pointer, dimension(:,:) :: disale => null()
 double precision, dimension(:,:), pointer :: xyzno0 => null()
 
-integer, allocatable, dimension(:,:) :: icodcl
 integer, allocatable, dimension(:) :: isostd
-double precision, allocatable, dimension(:,:,:) :: rcodcl
+integer, pointer, dimension(:,:) :: icodcl
+double precision, pointer, dimension(:,:,:) :: rcodcl
 
 !===============================================================================
 ! Interfaces
@@ -679,8 +679,7 @@ endif
 ! Deprecated, only for compatibility reason
 nvarcl = nvar
 
-allocate(icodcl(nfabor,nvar))
-allocate(rcodcl(nfabor,nvar,3))
+call field_build_bc_codes_all(icodcl, rcodcl)
 allocate(isostd(nfabor+1))
 
 ! First pass for initialization BC types
@@ -689,9 +688,8 @@ allocate(isostd(nfabor+1))
 call cscini(nvar)
 call condli_ini(nvar, nscal, itrale, icodcl, isostd, dt, rcodcl)
 
-deallocate(icodcl)
-deallocate(rcodcl)
 deallocate(isostd)
+call field_free_bc_codes_all(icodcl, rcodcl)
 
 !===============================================================================
 ! Arrays for time block, to discard afterwards

src/base/condli.f90
@@ -389,7 +389,7 @@ call cs_boundary_conditions_complete(nvar, itypfb, icodcl, rcodcl)
 call cs_f_user_boundary_conditions &
   (nvar, nscal, icodcl, itrifb, itypfb, izfppp, dt, rcodcl )
 
-call user_boundary_conditions(nvar, itypfb, icodcl, rcodcl)
+call user_boundary_conditions(itypfb)
 
 ! Check consistency with GUI definitions
 
@@ -4960,7 +4960,7 @@ call cs_f_user_boundary_conditions &
     dt     ,                                                       &
     rcodcl )
 
-call user_boundary_conditions(nvar, itypfb, icodcl, rcodcl)
+call user_boundary_conditions(itypfb)
 
 ! -- Methode ALE (CL de vitesse de maillage et deplacement aux noeuds)
 

src/base/cs_base_fortran.c
@@ -52,6 +52,7 @@
 #include <bft_printf.h>
 
 #include "cs_base.h"
+#include "cs_field.h"
 #include "cs_file.h"
 #include "cs_prototypes.h"
 #include "cs_timer.h"
@@ -603,6 +604,36 @@ void
 cs_user_boundary_conditions_setup_wrapper(void)
 {
   cs_user_boundary_conditions_setup(cs_glob_domain);
+}
+
+void
+cs_user_boundary_conditions_wrapper(int  *itypcl)
+{
+  cs_user_boundary_conditions(cs_glob_domain, itypcl);
+
+  /* Ensure icodcl values are uniform for multidimensional fields */
+
+  const cs_lnum_t n_b_faces = cs_glob_domain->mesh->n_b_faces;
+  const int n_fields = cs_field_n_fields();
+
+  for (int f_id = 0; f_id < n_fields; f_id++) {
+
+    const cs_field_t  *f = cs_field_by_id(f_id);
+    if (f->dim > 1 && f->type & CS_FIELD_VARIABLE) {
+
+      assert(f->bc_coeffs != NULL);
+      assert(f->bc_coeffs->icodcl != NULL || n_b_faces == 0);
+
+      int *icodcl = f->bc_coeffs->icodcl;
+
+      for (cs_lnum_t i = 1; i < f->dim; i++) {
+        for (cs_lnum_t j = 0; j < n_b_faces; j++)
+          icodcl[n_b_faces*i + j] = icodcl[j];
+      }
+
+    }
+
+  }
 }
 
 void

src/base/cs_base_fortran.h
@@ -228,6 +228,9 @@ void
 cs_user_boundary_conditions_setup_wrapper(void);
 
 void
+cs_user_boundary_conditions_wrapper(int  *itypcl);
+
+void
 cs_user_extra_operations_wrapper(void);
 
 void

src/base/cs_c_bindings.f90
@@ -2000,14 +2000,11 @@ module cs_c_bindings
 
     ! Interface to C user function for boundary conditions
 
-    subroutine user_boundary_conditions(nvar, bc_type, icodcl, rcodcl)  &
-      bind(C, name='cs_user_boundary_conditions')
+    subroutine user_boundary_conditions(bc_type)  &
+      bind(C, name='cs_user_boundary_conditions_wrapper')
       use, intrinsic :: iso_c_binding
       implicit none
-      integer(c_int), value :: nvar
       integer(kind=c_int), dimension(*), intent(inout) :: bc_type
-      integer(kind=c_int), dimension(*), intent(inout) :: icodcl
-      real(kind=c_double), dimension(*), intent(inout) :: rcodcl
     end subroutine user_boundary_conditions
 
     !---------------------------------------------------------------------------

src/base/cs_prototypes.h
@@ -345,10 +345,8 @@ cs_user_boundary_conditions_setup(cs_domain_t  *domain);
  *----------------------------------------------------------------------------*/
 
 void
-cs_user_boundary_conditions(int         nvar,
-                            int         icodcl[],
-                            int         bc_type[],
-                            cs_real_t   rcodcl[]);
+cs_user_boundary_conditions(cs_domain_t  *domain,
+                            int           bc_type[]);
 
 /*----------------------------------------------------------------------------*/
 /*!

src/user/cs_user_parameters.c
@@ -106,7 +106,7 @@ cs_user_model(void)
 void
 cs_user_parameters(cs_domain_t   *domain)
 {
-  CS_UNUSED(domain);
+  CS_NO_WARN_IF_UNUSED(domain);
 }
 
 /*----------------------------------------------------------------------------*/

commit 7ba572fe0f29d3bf148aee15f05e52eeb555aa30

Fix bug in comments introduced in commit 69511e8a376 (2022-09-02).

src/user/cs_user_boundary_conditions.c
@@ -95,24 +95,24 @@ cs_user_boundary_conditions_setup(cs_domain_t  *domain)
  * may be used as follows:
  *
  * - Boundary condition type code given at:
- *   f->bc_coefficients->icodcl[face_id]
+ *   f->bc_coeffs->icodcl[face_id]
  *
  * - Dirichlet value defined at:
- *   f->bc_coefficients->rcodcl1[face_id]
+ *   f->bc_coeffs->rcodcl1[face_id]
  *
  * - Interior exchange coefficient (infinite if no exchange) at:
- *   f->bc_coefficients->rcodcl2[face_id]
+ *   f->bc_coeffs->rcodcl2[face_id]
  *
  * - Flux density defined at:
- *   f->bc_coefficients->rcodcl3[face_id]
+ *   f->bc_coeffs->rcodcl3[face_id]
  *
  * For vector or tensor fields, these arrays are not interleaved,
  * so for a given face "face_id" and field component "comp_id", acess
  * is as follows (where n_b_faces is domain->mesh->n_b_faces):
  *
- *   f->bc_coefficients->rcodcl1[n_b_faces*comp_id + face_id]
- *   f->bc_coefficients->rcodcl2[n_b_faces*comp_id + face_id]
- *   f->bc_coefficients->rcodcl3[n_b_faces*comp_id + face_id]
+ *   f->bc_coeffs->rcodcl1[n_b_faces*comp_id + face_id]
+ *   f->bc_coeffs->rcodcl2[n_b_faces*comp_id + face_id]
+ *   f->bc_coeffs->rcodcl3[n_b_faces*comp_id + face_id]
  *
  * Only the icodcl code values from the first component are used in the case
  * of vector or tensor fields, so the icodcl values can be defined as for