code-saturne/code_saturne

commit 70bc7d6fa99e1c75b12088e30d62a2bc97ef6b9e

Many small Doxygen fixes.

src/base/cs_runaway_check.c
@@ -55,7 +55,7 @@
  * Header for the current file
  *----------------------------------------------------------------------------*/
 
-#include "cs_runaway_check.h"
+#include "cs_runaway_check.c"
 
 /*----------------------------------------------------------------------------*/
 
@@ -66,7 +66,7 @@ BEGIN_C_DECLS
  *============================================================================*/
 
 /*!
-  \file cs_runaway_check
+  \file cs_runaway_check.c
 
   \brief  Runaway (diverging) computation detection.
 */

docs/doxygen/examples/cs_user_extra_operations_examples.dox
@@ -435,9 +435,9 @@
 // __________________________________________________________________________________
 /*!
 
-  \page cs_user_extra_operations_examples_verif_cdo_diff Postprocessing of results obtained with CDO schemes for a scalar transport equation.
+  \page cs_user_extra_operations_examples_verif_cdo_diff_p Postprocessing of results obtained with CDO schemes for a scalar transport equation.
 
-  \section cs_user_extra_operations_examples_verif_cdo_diff Postprocessing of results obtained with CDO schemes for a scalar transport equation.
+  \section cs_user_extra_operations_examples_verif_cdo_diff_s Postprocessing of results obtained with CDO schemes for a scalar transport equation.
 
   This is an example of \ref cs_user_extra_operations allowing to add operations on results produced by CDO schemes. It allows to define advanced
   postprocessing.

docs/doxygen/examples/cs_user_modules.dox
@@ -43,7 +43,7 @@
 
   \section cs_user_module_h_allocate Arrays allocation
 
-  The \ref init_user_module subroutine allocates \c rwork and \c iwork if they
+  The following code allocates \c rwork and \c iwork if they
   are not already allocated or associated (for \c rwork).
 
   \snippet cs_user_modules-user-arrays.f90 allocate
@@ -51,13 +51,14 @@
   \section cs_user_module_h_c_pointer Access to arrays in C
 
   It is possible to access the \c rwork array in the C part of the code.
-  This can be done by using the \ref get_user_module_rwork subroutine.
+  This can be done by using the \c get_user_module_rwork subroutine
+  in the following example:
 
   \snippet cs_user_modules-user-arrays.f90 c_pointer
 
-  \section cs_user_module_h_free Arrays freeing
+  \section cs_user_module_h_free Freeing arrays
 
-  Eventually, the \ref finalize_user_module subroutine is used to free
+  Finally, the \c finalize_user_module subroutine is used to free
   \c iwork and \c rwork.
 
   \snippet cs_user_modules-user-arrays.f90 free

docs/doxygen/examples/cs_user_parameters.dox
@@ -93,7 +93,7 @@
 
   \snippet cs_user_parameters-base.c user_model_coriolis
 
-  \section cs_user_parameters_h_cs_user_parameters General options
+  \section cs_user_parameters_h_cs_user_parameters_general General options
 
   Choose a time step option
 
@@ -145,7 +145,7 @@
 
   \snippet cs_user_parameters-base.c param_fluid_properties
 
-  \subsection cs_user_parameters_h_cs_user_parameters1 Special fields and activate some automatic post-processings
+  \subsection cs_user_parameters_h_cs_user_parameters_fields Special fields and activate some automatic post-processings
 
   For example, to force the presence of a boundary temperature field, which
   may be useful for postprocessing:

docs/doxygen/examples/cs_user_physical_properties-ht_convert.dox
@@ -73,7 +73,7 @@
   Note that \c z->name or \c z->location_id can be used as a filter
   if "per-zone" properties are needed (such as with solid zones).
 
-  \subsection user_ht_convert_h_to_t Temperature to Enthalpy
+  \subsection user_ht_convert_t_to_h Temperature to Enthalpy
 
   The following block must then be added in
   \ref cs_user_physical_properties_t_to_h for the matching

docs/doxygen/examples/cs_var_dico.dox
@@ -170,14 +170,12 @@
       \f$ \overline{\varphi^\prime\varphi^\prime} \f$ for a fluctuating scalar
       <em>ϕ</em>). This can be made either {\em via} the
       interface or by declaring that scalar using
-      \texttt{cs\_parameters\_add\_variable\_variance} in
-      \texttt{cs\_user\_parameters.f90} (if the scalar in question is not a ``user''
-      scalar, the selection is made automatically). For instance, if \texttt{j}
-      and \texttt{k} are ``user'' scalars, the variable $\varphi$ corresponding
-      to \texttt{k} is the variable number
-      \texttt{isca(k)=isca(iscavr(j))}.
-\end{list}
-
+      `cs_parameters_add_variable_variance` in
+      \ref cs_user_parameters.f90 (if the scalar in question is not a ``user''
+      scalar, the selection is made automatically). For instance, if `j`
+      and `k` are ``user'' scalars, the variable \f$\varphi\f$ corresponding
+      to `k` is the variable number
+      `isca(k) = isca(iscavr(j))`.
 
   \par Accessing variables and properties in Fortran:
 
@@ -255,7 +253,7 @@
       <tt>tf->val[cell_id]</tt>. \n
       if there is no thermal scalar, cs_thermal_model_field returns NULL. \n\n
 
-    - In any case, all variables can be accessed using the function \ref cs_field_by_name:\n
+    - In any case, all variables can be accessed using the function \ref cs_field_by_name: \n
       <tt>\ref cs_real_t *cvar_pr = \ref cs_field_by_name("pressure")->val</tt> \n\n
 
     - A field's \ref scalar_id key-word should be ≥ 0 if it is a scalar. \n\n

src/alge/cs_bad_cells_regularisation.c
@@ -61,16 +61,15 @@
 
 /*----------------------------------------------------------------------------*/
 
-#ifndef DOXYGEN_SHOULD_SKIP_THIS
-
 BEGIN_C_DECLS
 
-#endif /* DOXYGEN_SHOULD_SKIP_THIS */
+/*! \cond DOXYGEN_SHOULD_SKIP_THIS */
+
+/*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
 
 /*----------------------------------------------------------------------------*/
 /*!
  * \brief Regularisation on bad cells for scalars
- *
  */
 /*----------------------------------------------------------------------------*/
 

src/alge/cs_balance.h
@@ -49,8 +49,8 @@ BEGIN_C_DECLS
 /*----------------------------------------------------------------------------*/
 /*!
  * \brief Wrapper to the function which adds the explicit part of the
- * convection/diffusion
- * terms of a transport equation of a scalar field \f$ \varia \f$.
+ * convection/diffusion terms of a transport equation of
+ * a scalar field \f$ \varia \f$.
  *
  * More precisely, the right hand side \f$ Rhs \f$ is updated as
  * follows:
@@ -68,8 +68,11 @@ BEGIN_C_DECLS
  * Options for the convective scheme:
  * - blencp = 0: upwind scheme for the advection
  * - blencp = 1: no upwind scheme except in the slope test
- * - ischcp = 0: second order
+ * - ischcp = 0: SOLU
  * - ischcp = 1: centered
+ * - ischcp = 2: SOLU with upwind gradient reconstruction
+ * - ischcp = 3: blending SOLU centered
+ * - ischcp = 4: NVD-TVD
  * - imucpp = 0: do not multiply the convective part by \f$ C_p \f$
  * - imucpp = 1: multiply the convective part by \f$ C_p \f$
  *
@@ -85,7 +88,9 @@ BEGIN_C_DECLS
  * \param[in]     var_cal_opt   pointer to a cs_var_cal_opt_t structure which
  *                              contains variable calculation options
  * \param[in]     pvar          solved variable (current time step)
+ *                              may be NULL if pvara != NULL
  * \param[in]     pvara         solved variable (previous time step)
+ *                              may be NULL if pvar != NULL
  * \param[in]     coefap        boundary condition array for the variable
  *                               (explicit part)
  * \param[in]     coefbp        boundary condition array for the variable
@@ -168,8 +173,11 @@ cs_balance_scalar(int                idtvar,
  * Options for the convective scheme:
  * - blencp = 0: upwind scheme for the advection
  * - blencp = 1: no upwind scheme except in the slope test
- * - ischcp = 0: second order
+ * - ischcp = 0: SOLU
  * - ischcp = 1: centered
+ * - ischcp = 2: SOLU with upwind gradient reconstruction
+ * - ischcp = 3: blending SOLU centered
+ * - ischcp = 4: NVD-TVD
  *
  * \param[in]     idtvar        indicator of the temporal scheme
  * \param[in]     f_id          field id (or -1)
@@ -264,8 +272,11 @@ cs_balance_vector(int                  idtvar,
  * Options for the convective scheme:
  * - blencp = 0: upwind scheme for the advection
  * - blencp = 1: no upwind scheme except in the slope test
- * - ischcp = 0: second order
+ * - ischcp = 0: SOLU
  * - ischcp = 1: centered
+ * - ischcp = 2: SOLU with upwind gradient reconstruction
+ * - ischcp = 3: blending SOLU centered
+ * - ischcp = 4: NVD-TVD
  *
  * \param[in]     idtvar        indicator of the temporal scheme
  * \param[in]     f_id          field id (or -1)

commit b97fd373b383862ea9bde09b2373221e0d8e109d

Fix compilation broken in commit 70bc7d6fa9.

src/base/cs_runaway_check.c
@@ -55,7 +55,7 @@
  * Header for the current file
  *----------------------------------------------------------------------------*/
 
-#include "cs_runaway_check.c"
+#include "cs_runaway_check.h"
 
 /*----------------------------------------------------------------------------*/
 