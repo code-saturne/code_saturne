code-saturne/code_saturne

commit a2786099e629b06261a9b5ac8169fd8e86c0efd2

Major build change: remove libtool dependency from build system.

m4/cs_eos.m4
@@ -58,12 +58,12 @@ AC_ARG_WITH(eos-lib,
              fi
              EOS_LDFLAGS="-L$with_eos_lib"
              # Add the libdir to the runpath as EOS is not libtoolized
-             EOSRUNPATH="-R$with_eos_lib"],
+             EOSRUNPATH="${LDRPATH}${with_eos_lib}"],
             [if test "x$with_eos" != "xno" -a "x$with_eos" != "xyes" \
 	          -a "x$with_eos" != "xcheck"; then
                EOS_LDFLAGS="-L$with_eos/lib"
                # Add the libdir to the runpath as EOS is not libtoolized
-               EOSRUNPATH="-R$with_eos/lib"
+               EOSRUNPATH="${LDRPATH}${with_eos}/lib"
              fi])
 
 if test "x$with_eos" != "xno" ; then

src/base/cs_physical_properties.c
@@ -350,6 +350,7 @@ cs_thermal_table_set(const char                        *material,
     strcat(cs_glob_thermal_table->method, method);
     cs_glob_thermal_table->type = 2;
 #if defined(HAVE_EOS)
+#if defined(HAVE_DLOPEN)
     {
       const char _reference_default[] = "";
       const char *_reference = (reference != NULL) ? reference : _reference_default;
@@ -373,6 +374,10 @@ cs_thermal_table_set(const char                        *material,
                      _reference);
     }
 #else
+    bft_error(__FILE__, __LINE__, 0,
+              "EOS only available if when dlloader is enabled in code_saturne.");
+#endif
+#else
     CS_UNUSED(reference);
 #endif
   }
@@ -390,7 +395,7 @@ void
 cs_thermal_table_finalize(void)
 {
   if (cs_glob_thermal_table != NULL) {
-#if defined(HAVE_EOS)
+#if defined(HAVE_EOS) && defined(HAVE_PLUGINS)
     if (cs_glob_thermal_table->type == 2) {
       _cs_eos_destroy();
       cs_base_dlclose("cs_eos", _cs_eos_dl_lib);
@@ -490,7 +495,7 @@ cs_phys_prop_compute(cs_phys_prop_type_t          property,
                            var2_c,
                            val);
   }
-#if defined(HAVE_EOS)
+#if defined(HAVE_EOS) && defined(HAVE_PLUGINS)
   else if (cs_glob_thermal_table->type == 2) {
     _cs_phys_prop_eos(cs_glob_thermal_table->thermo_plane,
                       property,

build-aux/cs_build_tools.py
@@ -5,7 +5,7 @@
 
 # This file is part of code_saturne, a general-purpose CFD tool.
 #
-# Copyright (C) 1998-2021 EDF S.A.
+# Copyright (C) 1998-2022 EDF S.A.
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software

build-aux/cs_compile_build.py
@@ -5,7 +5,7 @@
 
 # This file is part of code_saturne, a general-purpose CFD tool.
 #
-# Copyright (C) 1998-2021 EDF S.A.
+# Copyright (C) 1998-2022 EDF S.A.
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -172,12 +172,12 @@ class compile_build(cs_compile):
         elif flag == 'ldflags':
             tsd = os.path.join(top_builddir, 'src')
             for f in os.listdir(tsd):
-                p = os.path.join(tsd, f, '.libs')
+                p = os.path.join(tsd, f)
                 if os.path.isdir(p):
                     flags.append('-L' + p)
             if self.pkg.config.libs['ple'].variant == 'internal':
                 flags.append('-L' + os.path.join(top_builddir, 'libple', 'src'))
-            l_cwd = '-L' + os.path.join(os.getcwd(), '.libs')
+            l_cwd = '-L' + os.path.join(os.getcwd())
             if not l_cwd in flags:
                 flags.append(l_cwd)
             # Add library paths which may be indirectly required
@@ -194,7 +194,7 @@ class compile_build(cs_compile):
         Determine directory containing library in archive mode.
         """
 
-        return os.path.join(self.top_builddir, "src", "apps", ".libs")
+        return os.path.join(self.top_builddir, "src", "apps")
 
 #===============================================================================
 # Class used to manage install

build-aux/cs_config_test.py
@@ -5,7 +5,7 @@
 
 # This file is part of code_saturne, a general-purpose CFD tool.
 #
-# Copyright (C) 1998-2021 EDF S.A.
+# Copyright (C) 1998-2022 EDF S.A.
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software

build-aux/cs_link_library.py
@@ -1,0 +1,308 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#-------------------------------------------------------------------------------
+
+# This file is part of code_saturne, a general-purpose CFD tool.
+#
+# Copyright (C) 1998-2022 EDF S.A.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+# Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+#-------------------------------------------------------------------------------
+
+import sys, os.path
+import argparse
+import subprocess, fnmatch
+
+#-------------------------------------------------------------------------------
+
+# Utility functions for build system to assemble shared or archive libraries.
+# Note that some settings/options adapted to various systems are also
+# defined in `config/cs_auto_flags.sh`.
+
+# These functions have beed tested on Linux, and may need to be adapted to
+# various systems using other linker configurations and options.
+# In this case, adding functions dedicated to various cases is recommended.
+
+# These functions play a role similar to the Libtool scripts previously built.
+# All the built-in knowledge of Libtool is dropped here, and may need to be
+# re-learned here, but most of that knowledge regarded obsolete systems (or
+# systems not encountered in a computational environment), and some aspects
+# of Libtool's automation (especially the fact that incorrect paths in .la
+# files could not be ignored) caused too frequent issues.
+
+#===============================================================================
+# Utility functions
+#===============================================================================
+
+#-------------------------------------------------------------------------------
+# Process the command line arguments
+#-------------------------------------------------------------------------------
+
+def parse_cmd_line(argv):
+    """
+    Process the passed command line arguments.
+    """
+
+    # Note: we could use args to pass a calculation status file as an argument,
+    # which would allow pursuing the later calculation stages.
+
+    description="Link object or library files into a shared library."
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument("--linker", dest="linker", type=str, default='',
+                        metavar="<linker>",
+                        help="define linker")
+
+    parser.add_argument("--version", dest="version", type=str, default='',
+                        metavar="<version>",
+                        help="define library version number")
+
+    parser.add_argument("--archive", dest="archive", default=False,
+                        action="store_true",
+                        help="build archive library")
+
+    parser.add_argument("--echo", dest="echo", default=False,
+                        action="store_true",
+                        help="echo commands run")
+
+    parser.add_argument("-o", dest="output_name", type=str,
+                        metavar="<output_name>",
+                        help="output library name")
+
+    options, ext = parser.parse_known_args(argv)
+
+    return options, ext
+
+#-------------------------------------------------------------------------------
+# Call external command, with checking and simple logging for errors
+#-------------------------------------------------------------------------------
+
+def run_command(cmd, echo=False):
+    """
+    Call external command, with checking and simple logging for errors
+    """
+
+    if echo:
+        print(' '.join(cmd))
+    p = subprocess.Popen(cmd)
+    p.communicate()
+    if p.returncode != 0 and not echo:
+        print(sys.argv[0], ":", cmd, "returned", p.returncode)
+
+    return p.returncode
+
+#-------------------------------------------------------------------------------
+# Build (usually static) library archive
+#-------------------------------------------------------------------------------
+
+def build_archive(output,
+                  archives,
+                  objects,
+                  ar_options=[], ranlib_options=[],
+                  echo=False):
+    """
+    Build an archive given the archives and objects list.
+    """
+
+    # Add objects from libraries
+
+    if archives:
+        objdir = ('.tmp_objs')
+        if not os.path.isdir(objdir):
+            os.mkdir(objdir)
+        wd = os.getcwd()
+        os.chdir(objdir)
+        if os.path.isabs(output):
+            p_output = output
+        else:
+            p_output = os.path.join('..', output)
+        for a in archives:
+            if os.path.isabs(a):
+                f = a
+            else:
+                f = os.path.join('..', a)
+            cmd = ['ar', 'x', f]
+
+            retcode = run_command(cmd, echo)
+
+            dir_files = os.listdir('.')
+            o_files = fnmatch.filter(dir_files, '*.o')
+
+            cmd = ['ar', 'cr'] + ar_options + [p_output] + o_files
+
+            retcode = run_command(cmd, echo)
+
+            for f in dir_files:
+                os.remove(f)
+
+        os.chdir(wd)
+        os.rmdir(objdir)
+
+    # Add objects provided directly
+
+    if objects:
+        cmd = ['ar', 'cr'] + ar_options + [output] + objects
+        retcode = run_command(cmd, echo)
+
+    # Now generate index
+
+    cmd = ['ranlib'] + ranlib_options + [output]
+    retcode = run_command(cmd, echo)
+
+    return retcode
+
+#-------------------------------------------------------------------------------
+# Build shared library archive.
+#-------------------------------------------------------------------------------
+
+def build_shared_library(linker,
+                         output,
+                         version,
+                         archives,
+                         objects,
+                         other,
+                         echo=False):
+    """
+    Build an archive given the archives and objects list.
+    """
+
+    if not linker:
+        print("No linker specified for output library ", output)
+        return 1
+
+    o_name = os.path.basename(output)
+    if version:
+        f, e = os.path.splitext(o_name)
+        o_name_v = f + '-' + version + e
+        f, e = os.path.splitext(output)
+        output_v = f + '-' + version + e
+    else:
+        o_name_v = o_name
+        output_v = output
+
+    # TODO:
+    # Some flags should already be provided by the caller through
+    # the command-line arguments. More things could be moved to
+    # this file (or the Python scripts in general), and later
+    # removed from `config/cs_auto_flags.sh`.
+
+    cmd = [linker, "-o", output_v, "-nostdlib"]
+    cmd += ["-Wl,-soname", "-Wl," + o_name_v]
+
+    # Add objects from libraries
+
+    if archives:
+        cmd.append("-Wl,-whole-archive")
+        for a in archives:
+            cmd.append(a)
+        cmd.append("-Wl,-no-whole-archive")
+
+    # Add objects provided directly
+
+    if objects:
+        for o in objects:
+            cmd.append(o)
+
+    # Pass all other options to linker;
+    # Convert libtool-like "-R' syntax to rpath.
+
+    for o in other:
+        if o[:2] == '-R':
+            cmd += ["-Wl,-rpath", "-Wl,"+o[2:]]
+        else:
+            cmd.append(o)
+
+    # Now call linker
+
+    retcode = run_command(cmd, echo)
+
+    # Try again allowing standard libraries if failed.
+    # This occurs for example for the ParaView Catalyst adaptor plugin
+    # using GCC, where adding GCC's own `crti.o` and `crtbeginS.o`
+    # files is an alternative solution (used by Libtool).
+
+    if retcode != 0:
+        cmd.remove("-nostdlib")
+        print()
+        print("Retry, allowing standard librairies search path for link:")
+        retcode = run_command(cmd, echo)
+
+    # Add symbolic link in case of version number.
+
+    if output_v != output and retcode == 0:
+        try:
+            if os.path.islink(output):
+                os.remove(output)
+            os.symlink(o_name_v, output)
+        except Exception:
+            fmt = "Failed to create sybolic link from {0} to {1}."
+            print(fmt.format(output, o_name_v))
+            return 1
+
+    return retcode
+
+#-------------------------------------------------------------------------------
+# Main
+#-------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    import sys, os.path
+
+    retcode = 0
+
+    options, ext = parse_cmd_line(sys.argv[1:])
+
+    objects = []
+    archives = []
+    other = []
+
+    if options.echo:  # Add blank line for readability in echo mode.
+        print()
+
+    for s in ext:
+        f, e = os.path.splitext(s)
+        if e in ('.o', '.obj'):
+            objects.append(s)
+        elif e == '.a':
+            archives.append(s)
+        else:
+            other.append(s)
+
+    # Archive library case:
+
+    if options.archive:
+        retcode = build_archive(options.output_name,
+                                archives, objects,
+                                ar_options=[], ranlib_options=[],
+                                echo=options.echo)
+
+    else:
+        retcode = build_shared_library(options.linker,
+                                       options.output_name,
+                                       options.version,
+                                       archives, objects, other,
+                                       echo=options.echo)
+
+    if options.echo:  # Add blank line for readability in echo mode.
+        print()
+
+    sys.exit(retcode)
+
+#-------------------------------------------------------------------------------
+# End
+#-------------------------------------------------------------------------------

build-aux/cs_user_src_filter.py
@@ -5,7 +5,7 @@
 
 # This file is part of code_saturne, a general-purpose CFD tool.
 #
-# Copyright (C) 1998-2021 EDF S.A.
+# Copyright (C) 1998-2022 EDF S.A.
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software

build-aux/cs_version.py
@@ -5,7 +5,7 @@
 
 # This file is part of code_saturne, a general-purpose CFD tool.
 #
-# Copyright (C) 1998-2021 EDF S.A.
+# Copyright (C) 1998-2022 EDF S.A.
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -330,6 +330,10 @@ if __name__ == '__main__':
             extra = ''
         sys.stdout.write(build_version_string(major, minor, '', extra))
 
+    elif sys.argv[1] == '--lib':
+        major, minor, release, extra = version_from_news(srcdir)
+        sys.stdout.write(build_version_string(major, minor, '', ''))
+
     # Call tools
 
     elif sys.argv[1] in ['--full', '--revision-only', '--verbose',
@@ -408,6 +412,7 @@ Options:
                               version control info
   --short                     print short version string with major, minor,
                               and extras
+  --lib                       print short version string with major and minor
   --revision-only             print version string with version control info only
   --update <template> [path]  build or rebuild output file from template
   --verbose                   print fields on different lines

commit ed5194c8c9493711fca71d4302b0413411a3bed8

Fix EOS call when plugins are not activated through some other library.
This was apparently broken by consistency improvmements in commit a2786099
(August 1 2022).

m4/cs_eos.m4
@@ -68,6 +68,10 @@ AC_ARG_WITH(eos-lib,
 
 if test "x$with_eos" != "xno" ; then
 
+  if test "x$cs_have_dlloader" = "xno" ; then
+    AC_MSG_ERROR([EOS support requested but dynamic library loader disabled])
+  fi
+
   saved_CPPFLAGS="$CPPFLAGS"
   saved_CXXFLAGS="$CXXFLAGS"
   saved_LDFLAGS="$LDFLAGS"
@@ -82,7 +86,6 @@ if test "x$with_eos" != "xno" ; then
   EOS_LIBS="-leos"
 
   eosversion=`${with_eos}/bin/eos --version`
-
 
   # check for pre-v1.8.0
   # This test is necessary because starting from 1.8.0 definition of mixing

src/base/cs_physical_properties.c
@@ -395,7 +395,7 @@ void
 cs_thermal_table_finalize(void)
 {
   if (cs_glob_thermal_table != NULL) {
-#if defined(HAVE_EOS) && defined(HAVE_PLUGINS)
+#if defined(HAVE_EOS) /* always a plugin */
     if (cs_glob_thermal_table->type == 2) {
       _cs_eos_destroy();
       cs_base_dlclose("cs_eos", _cs_eos_dl_lib);
@@ -495,7 +495,7 @@ cs_phys_prop_compute(cs_phys_prop_type_t          property,
                            var2_c,
                            val);
   }
-#if defined(HAVE_EOS) && defined(HAVE_PLUGINS)
+#if defined(HAVE_EOS) /* always a plugin */
   else if (cs_glob_thermal_table->type == 2) {
     _cs_phys_prop_eos(cs_glob_thermal_table->thermo_plane,
                       property,