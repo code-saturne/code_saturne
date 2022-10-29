code-saturne/code_saturne

commit 46717ada5ea4e6e38f3fc1e9a753f5a854fe3af2

Major change to linear solver behavior: by default, avoid immediate exit.
The old behavior can be restored on a solver-by solver basis using the new
cs_sles_set_allow_no_op function, and behavior queried using
cs_sles_get_allow_no_op.

Some solvers may still exit with zero iterations, but should do so
only after having done a full convergence test involving an SpMV product,
not a test based only on the right-hand side.

This solves GitLab issues #104 and #199.

src/alge/cs_sles.c
@@ -308,6 +308,8 @@ struct _cs_sles_t {
 
   int                       n_no_op;       /* Number of solves with immediate
                                               exit */
+  bool                      allow_no_op;   /* Allow immediate exit if RHS
+                                              small relative to residual norm */
 
   int                       f_id;          /* matching field id, or < 0 */
 
@@ -422,6 +424,7 @@ _sles_create(int          f_id,
 
   sles->n_calls = 0;
   sles->n_no_op = 0;
+  sles->allow_no_op = false;
 
   sles->post_info = NULL;
 
@@ -681,6 +684,7 @@ _needs_solving(const  char        *name,
                const cs_real_t    *rhs)
 {
   int retval = 1;
+  return retval;
 
   /* Initialize residue, check for immediate return */
 
@@ -1571,6 +1575,44 @@ cs_sles_get_name(const cs_sles_t  *sles)
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief Query if immediate_return ("no-op") is allowed when initial
+ * guess is zero (solve by increments) and the RHS is already zero within the
+ * normalized tolerance criteria.
+ *
+ * \param[in]  sles  pointer to solver object
+ *
+ * \return  true if immediate return is allowed, false if at least one
+ *          iteration is required
+ */
+/*----------------------------------------------------------------------------*/
+
+bool
+cs_sles_get_allow_no_op(const cs_sles_t  *sles)
+{
+  return sles->allow_no_op;
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Indicate if immediate_return ("no-op") is allowed when initial
+ * guess is zero (solve by increments) and the RHS is already zero within the
+ * normalized tolerance criteria.
+ *
+ * \param[in, out]  sles         pointer to solver object
+ * \param[in]       allow_no_op  true if immediate return is allowed,
+ *                               false if at least one iteration is required
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_sles_set_allow_no_op(cs_sles_t  *sles,
+                        bool        allow_no_op)
+{
+  sles->allow_no_op = allow_no_op;
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief Setup sparse linear equation solver.
  *
  * Use of this function is optional: if a \ref cs_sles_solve is called
@@ -1684,20 +1726,23 @@ cs_sles_solve(cs_sles_t           *sles,
 #endif
 
   cs_sles_convergence_state_t state;
+  bool do_solve = true;
 
-  bool do_solve = _needs_solving(sles_name,
-                                 a,
-                                 sles->verbosity,
-                                 precision,
-                                 r_norm,
-                                 residue,
-                                 vx,
-                                 rhs);
+  if (sles->allow_no_op) {
+    do_solve = _needs_solving(sles_name,
+                              a,
+                              sles->verbosity,
+                              precision,
+                              r_norm,
+                              residue,
+                              vx,
+                              rhs);
 
-  if (! do_solve) {
-    sles->n_no_op += 1;
-    *n_iter = 0;
-    state = CS_SLES_CONVERGED;
+    if (! do_solve) {
+      sles->n_no_op += 1;
+      *n_iter = 0;
+      state = CS_SLES_CONVERGED;
+    }
   }
 
   while (do_solve) {

src/alge/cs_sles.h
@@ -603,6 +603,38 @@ cs_sles_get_name(const cs_sles_t  *sles);
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief Query if immediate_return ("no-op") is allowed when initial
+ * guess is zero (solve by increments) and the RHS is already zero within the
+ * normalized tolerance criteria.
+ *
+ * \param[in]  sles  pointer to solver object
+ *
+ * \return  true if immediate return is allowed, false if at least one
+ *          iteration is required
+ */
+/*----------------------------------------------------------------------------*/
+
+bool
+cs_sles_get_allow_no_op(const cs_sles_t  *sles);
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Indicate if immediate_return ("no-op") is allowed when initial
+ * guess is zero (solve by increments) and the RHS is already zero within the
+ * normalized tolerance criteria.
+ *
+ * \param[in, out]  sles         pointer to solver object
+ * \param[in]       allow_no_op  true if immediate return is allowed,
+ *                               false if at least one iteration is required
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_sles_set_allow_no_op(cs_sles_t  *sles,
+                        bool        allow_no_op);
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief Setup sparse linear equation solver.
  *
  * Use of this function is optional: if a \ref cs_sles_solve is called

commit c8f0fe9ebcd2e6b222900156078068e9763ea67a

Fix handle case where linear system to solve has zero RHS, broken by commit 46717ada.
This can be the case with ALE initialization.

src/alge/cs_sles.c
@@ -684,7 +684,6 @@ _needs_solving(const  char        *name,
                const cs_real_t    *rhs)
 {
   int retval = 1;
-  return retval;
 
   /* Initialize residue, check for immediate return */
 
@@ -1728,7 +1727,12 @@ cs_sles_solve(cs_sles_t           *sles,
   cs_sles_convergence_state_t state;
   bool do_solve = true;
 
-  if (sles->allow_no_op) {
+  /* Even if we normally require at least entering the linear equation solver,
+     if the residue normalization is really zero, we probably have zero initial
+     solution and RHS already, so check for that case, rather than enter
+     solvers whose convergence test may fail in these conditions. */
+
+  if (sles->allow_no_op || r_norm <= 0.) {
     do_solve = _needs_solving(sles_name,
                               a,
                               sles->verbosity,