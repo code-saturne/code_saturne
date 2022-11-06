code-saturne/code_saturne

commit 678f098c25f1bbdcf32dd744c35da95216f213d0

Coolprop: allow higher-level choice of back-end.

src/base/cs_coolprop.cxx
@@ -77,13 +77,17 @@
 /*!
  * \brief Computes physical properties in (P,h,Yi) for compressible flow.
  *
- * \param[in]  CoolPropMaterial type of material
- * \param[in]  thermo_plane     type of thermal plane
- * \param[in]  property         type of property to compute
- * \param[in]  n_vals           size of properties arrays
- * \param[in]  var1             array of pressure
- * \param[in]  var2             array of thermal properties
- * \param[out] val              array of property
+ * \param[in]   coolprop_material  CoolProp material
+ * \param[in]   coolprop_backend   CoolProp backend ("HEOS" by default,
+ *                                 "SRK" for cubic, "TTSE&HEOS" or
+ *                                 "BICUBIC&HEOS" for tabulated)
+ * \param[in]   thermo_plane       type of thermal plane
+ * \param[in]   property           type of property to compute
+ * \param(in]   n_vals             size of variable and property arrays
+ * \param[in]   var1               first variable of thermodynamic plane
+ *                                 (pressure)
+ * \param[in]   var2               second variable of thermodynamic plane
+ * \param[out]  val                computed property values
  */
 /*----------------------------------------------------------------------------*/
 
@@ -91,7 +95,8 @@
 extern "C"
 #endif
 void
-cs_phys_prop_coolprop(char                              *CoolPropMaterial,
+cs_phys_prop_coolprop(char                              *coolprop_material,
+                      const char                        *coolprop_backend,
                       cs_phys_prop_thermo_plane_type_t   thermo_plane,
                       cs_phys_prop_type_t                property,
                       const cs_lnum_t                    n_vals,
@@ -102,10 +107,12 @@ cs_phys_prop_coolprop(char                              *CoolPropMaterial,
   std::vector<std::string> fluids;
   std::vector<std::string> outputs;
 
-  fluids.push_back(CoolPropMaterial);
+  fluids.push_back(coolprop_material);
   std::string Name1 = "";
   std::string Name2 = "P";
-  std::string Backend = "HEOS";  // "SRK" for cubic, "BICUBIC&HEOS" for tabulated
+
+  std::string Backend = "HEOS";
+  if (coolprop_backend != NULL) Backend = coolprop_backend;
 
   if (thermo_plane == CS_PHYS_PROP_PLANE_PH)
     Name1 = "H";
@@ -128,11 +135,10 @@ cs_phys_prop_coolprop(char                              *CoolPropMaterial,
       outputs.push_back("S");
       break;
     case CS_PHYS_PROP_ISOBARIC_HEAT_CAPACITY:
-      outputs.push_back("C");
+      outputs.push_back("CPMASS");
       break;
     case CS_PHYS_PROP_ISOCHORIC_HEAT_CAPACITY:
-      bft_error(__FILE__, __LINE__, 0,
-                _("bad choice: isochoric heat capacity not available yet"));
+      outputs.push_back("CVMASS");
       break;
     case CS_PHYS_PROP_SPECIFIC_VOLUME:
       bft_error(__FILE__, __LINE__, 0,
@@ -155,8 +161,7 @@ cs_phys_prop_coolprop(char                              *CoolPropMaterial,
       outputs.push_back("V");
       break;
     case CS_PHYS_PROP_SPEED_OF_SOUND:
-      bft_error(__FILE__, __LINE__, 0,
-                _("bad choice: speed of sound not available yet"));
+      outputs.push_back("SPEED_OF_SOUND");
       break;
   }
 
@@ -169,13 +174,13 @@ cs_phys_prop_coolprop(char                              *CoolPropMaterial,
   std::vector<double> val2;
   std::vector<double> fractions;
 
-  val1.clear();
-  val2.clear();
+  val1.reserve(n_vals);
+  val2.reserve(n_vals);
   fractions.clear();
 
   for (cs_lnum_t i = 0; i < n_vals; i++) {
-    val1.push_back(var2[i]);
-    val2.push_back(var1[i]);
+    val1[i] = var2[i];
+    val2[i] = var1[i];
   }
   fractions.push_back(1.0);
 

src/base/cs_coolprop.hxx
@@ -61,14 +61,17 @@
 /*!
  * Computes physical properties in (P,h,Yi) for compressible flow.
  *
- * parameters:
- * CoolPropMaterial <--  type of material
- * thermo_plane     <--  type of thermal plane
- * property         <--  type of property to compute
- * n_vals           <--  size of properties arrays
- * var1             <--  array of pressure
- * var2             <--  array of thermal properties
- * val              -->  array of property
+ * \param[in]   coolprop_material  CoolProp material
+ * \param[in]   coolprop_backend   CoolProp backend ("HEOS" by default,
+ *                                 "SRK" for cubic, "TTSE&HEOS" or
+ *                                 "BICUBIC&HEOS" for tabulated)
+ * \param[in]   thermo_plane       type of thermal plane
+ * \param[in]   property           type of property to compute
+ * \param(in]   n_vals             size of variable and property arrays
+ * \param[in]   var1               first variable of thermodynamic plane
+ *                                 (pressure)
+ * \param[in]   var2               second variable of thermodynamic plane
+ * \param[out]  val                computed property values
  */
 /*----------------------------------------------------------------------------*/
 
@@ -76,13 +79,16 @@
 extern "C"
 #endif
 void
-cs_phys_prop_coolprop(char                              *CoolPropMaterial,
+cs_phys_prop_coolprop(char                              *coolprop_material,
+                      const char                        *coolprop_backend,
                       cs_phys_prop_thermo_plane_type_t   thermo_plane,
                       cs_phys_prop_type_t                property,
                       const cs_lnum_t                    n_vals,
                       const cs_real_t                    var1[],
                       const cs_real_t                    var2[],
                       cs_real_t                          val[]);
+
+/*----------------------------------------------------------------------------*/
 
 #endif /* __CS_COOLPROP_HXX__ */
 

src/base/cs_physical_properties.c
@@ -136,7 +136,8 @@ typedef void
                      cs_real_t                          val[]);
 
 typedef void
-(cs_phys_prop_coolprop_t)(char                              *CoolPropMaterial,
+(cs_phys_prop_coolprop_t)(const char                        *coolprop_material,
+                          const char                        *coolprop_backend,
                           cs_phys_prop_thermo_plane_type_t   thermo_plane,
                           cs_phys_prop_type_t                property,
                           const cs_lnum_t                    n_vals,
@@ -328,7 +329,7 @@ cs_thermal_table_set(const char                        *material,
 
       /* Load symbols from shared library */
 
-      /* Function pointers need to be double-casted so as to first convert
+      /* Function pointers need to be double-cast so as to first convert
          a (void *) type to a memory address and then convert it back to the
          original type. Otherwise, the compiler may issue a warning.
          This is a valid ISO C construction. */
@@ -501,6 +502,7 @@ cs_phys_prop_compute(cs_phys_prop_type_t          property,
 #if defined(HAVE_COOLPROP)
   else if (cs_glob_thermal_table->type == 3) {
     _cs_phys_prop_coolprop(cs_glob_thermal_table->material,
+                           "HEOS",
                            cs_glob_thermal_table->thermo_plane,
                            property,
                            _n_vals,
@@ -567,17 +569,11 @@ cs_phys_prop_freesteam(cs_phys_prop_thermo_plane_type_t   thermo_plane,
       case CS_PHYS_PROP_ISOCHORIC_HEAT_CAPACITY:
         val[i] = freesteam_cv(S0);
         break;
-      case CS_PHYS_PROP_SPECIFIC_VOLUME:
-        val[i] = freesteam_v(S0);
-        break;
       case CS_PHYS_PROP_DENSITY:
         val[i] = freesteam_rho(S0);
         break;
       case CS_PHYS_PROP_INTERNAL_ENERGY:
         val[i] = freesteam_u(S0);
-        break;
-      case CS_PHYS_PROP_QUALITY:
-        val[i] = freesteam_x(S0);
         break;
       case CS_PHYS_PROP_THERMAL_CONDUCTIVITY:
         val[i] = freesteam_k(S0);
@@ -615,17 +611,11 @@ cs_phys_prop_freesteam(cs_phys_prop_thermo_plane_type_t   thermo_plane,
       case CS_PHYS_PROP_ISOCHORIC_HEAT_CAPACITY:
         val[i] = freesteam_cv(S0);
         break;
-      case CS_PHYS_PROP_SPECIFIC_VOLUME:
-        val[i] = freesteam_v(S0);
-        break;
       case CS_PHYS_PROP_DENSITY:
         val[i] = freesteam_rho(S0);
         break;
       case CS_PHYS_PROP_INTERNAL_ENERGY:
         val[i] = freesteam_u(S0);
-        break;
-      case CS_PHYS_PROP_QUALITY:
-        val[i] = freesteam_x(S0);
         break;
       case CS_PHYS_PROP_THERMAL_CONDUCTIVITY:
         val[i] = freesteam_k(S0);
@@ -663,17 +653,11 @@ cs_phys_prop_freesteam(cs_phys_prop_thermo_plane_type_t   thermo_plane,
       case CS_PHYS_PROP_ISOCHORIC_HEAT_CAPACITY:
         val[i] = freesteam_cv(S0);
         break;
-      case CS_PHYS_PROP_SPECIFIC_VOLUME:
-        val[i] = freesteam_v(S0);
-        break;
       case CS_PHYS_PROP_DENSITY:
         val[i] = freesteam_rho(S0);
         break;
       case CS_PHYS_PROP_INTERNAL_ENERGY:
         val[i] = freesteam_u(S0);
-        break;
-      case CS_PHYS_PROP_QUALITY:
-        val[i] = freesteam_x(S0);
         break;
       case CS_PHYS_PROP_THERMAL_CONDUCTIVITY:
         val[i] = freesteam_k(S0);
@@ -710,18 +694,12 @@ cs_phys_prop_freesteam(cs_phys_prop_thermo_plane_type_t   thermo_plane,
       case CS_PHYS_PROP_ISOCHORIC_HEAT_CAPACITY:
         val[i] = freesteam_cv(S0);
         break;
-      case CS_PHYS_PROP_SPECIFIC_VOLUME:
-        val[i] = freesteam_v(S0);
-        break;
       case CS_PHYS_PROP_DENSITY:
         val[i] = freesteam_rho(S0);
         break;
       case CS_PHYS_PROP_INTERNAL_ENERGY:
         bft_error(__FILE__, __LINE__, 0,
                   _("bad choice: you choose to work in the %s plane."), "pu");
-        break;
-      case CS_PHYS_PROP_QUALITY:
-        val[i] = freesteam_x(S0);
         break;
       case CS_PHYS_PROP_THERMAL_CONDUCTIVITY:
         val[i] = freesteam_k(S0);
@@ -758,18 +736,11 @@ cs_phys_prop_freesteam(cs_phys_prop_thermo_plane_type_t   thermo_plane,
       case CS_PHYS_PROP_ISOCHORIC_HEAT_CAPACITY:
         val[i] = freesteam_cv(S0);
         break;
-      case CS_PHYS_PROP_SPECIFIC_VOLUME:
-        bft_error(__FILE__, __LINE__, 0,
-                  _("bad choice: you choose to work in the %s plane."), "pv");
-        break;
       case CS_PHYS_PROP_DENSITY:
         val[i] = freesteam_rho(S0);
         break;
       case CS_PHYS_PROP_INTERNAL_ENERGY:
         val[i] = freesteam_u(S0);
-        break;
-      case CS_PHYS_PROP_QUALITY:
-        val[i] = freesteam_x(S0);
         break;
       case CS_PHYS_PROP_THERMAL_CONDUCTIVITY:
         val[i] = freesteam_k(S0);
@@ -807,17 +778,11 @@ cs_phys_prop_freesteam(cs_phys_prop_thermo_plane_type_t   thermo_plane,
       case CS_PHYS_PROP_ISOCHORIC_HEAT_CAPACITY:
         val[i] = freesteam_cv(S0);
         break;
-      case CS_PHYS_PROP_SPECIFIC_VOLUME:
-        val[i] = freesteam_v(S0);
-        break;
       case CS_PHYS_PROP_DENSITY:
         val[i] = freesteam_rho(S0);
         break;
       case CS_PHYS_PROP_INTERNAL_ENERGY:
         val[i] = freesteam_u(S0);
-        break;
-      case CS_PHYS_PROP_QUALITY:
-        val[i] = freesteam_x(S0);
         break;
       case CS_PHYS_PROP_THERMAL_CONDUCTIVITY:
         val[i] = freesteam_k(S0);
@@ -854,18 +819,11 @@ cs_phys_prop_freesteam(cs_phys_prop_thermo_plane_type_t   thermo_plane,
       case CS_PHYS_PROP_ISOCHORIC_HEAT_CAPACITY:
         val[i] = freesteam_cv(S0);
         break;
-      case CS_PHYS_PROP_SPECIFIC_VOLUME:
-        val[i] = freesteam_v(S0);
-        break;
       case CS_PHYS_PROP_DENSITY:
         val[i] = freesteam_rho(S0);
         break;
       case CS_PHYS_PROP_INTERNAL_ENERGY:
         val[i] = freesteam_u(S0);
-        break;
-      case CS_PHYS_PROP_QUALITY:
-        bft_error(__FILE__, __LINE__, 0,
-                  _("bad choice: you choose to work in the %s plane."), "Tx");
         break;
       case CS_PHYS_PROP_THERMAL_CONDUCTIVITY:
         val[i] = freesteam_k(S0);

commit 732812a8e4cf87644a733870ae30665126b9c747

Fix CoolProp Usage, broken by commit 678f098c (Sept 13 2021).

src/base/cs_coolprop.cxx
@@ -179,8 +179,8 @@ cs_phys_prop_coolprop(char                              *coolprop_material,
   fractions.clear();
 
   for (cs_lnum_t i = 0; i < n_vals; i++) {
-    val1[i] = var2[i];
-    val2[i] = var1[i];
+    val1.push_back(var2[i]);
+    val2.push_back(var1[i]);
   }
   fractions.push_back(1.0);
 