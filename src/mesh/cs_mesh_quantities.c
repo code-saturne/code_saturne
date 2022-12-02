code-saturne/code_saturne

commit 7233eb86d7a5d71422a261a28ac65b718cf12de6

Use fluid quantities in gradient computation.

src/mesh/cs_mesh_quantities.c
@@ -3612,6 +3612,17 @@ cs_mesh_quantities_solid_compute(const cs_mesh_t       *m,
                         mq->diipb,
                         mq->dofij);
 
+  _compute_face_sup_vectors(m->n_cells,
+                            m->n_i_faces,
+                            (const cs_lnum_2_t *)(m->i_face_cells),
+                            (const cs_real_3_t *)(mq->i_face_normal),
+                            (const cs_real_3_t *)(mq->i_face_cog),
+                            (const cs_real_3_t *)(mq->cell_f_cen),
+                            mq->cell_f_vol,
+                            mq->i_dist,
+                            (cs_real_3_t *)(mq->diipf),
+                            (cs_real_3_t *)(mq->djjpf));
+
   /*wall distance*/
 
   cs_real_t *c_w_dist_inv = mq->c_w_dist_inv;

src/alge/cs_convection_diffusion.c
@@ -11712,7 +11712,7 @@ cs_diffusion_potential(const int                 f_id,
     cs_halo_sync_var(halo, halo_type, pvar);
 
   /*==========================================================================
-    2. Update mass flux without reconstruction technics
+    2. Update mass flux without reconstruction techniques
     ==========================================================================*/
 
   if (nswrgp <= 1) {
@@ -12150,7 +12150,7 @@ cs_anisotropic_diffusion_potential(const int                 f_id,
   }
 
   /*==========================================================================
-    3. Update mass flux with reconstruction technics
+    3. Update mass flux with reconstruction techniques
     ==========================================================================*/
 
   if (nswrgp > 1) {

commit 70ebebeb8321f8f0883aceff4568ddf3e73099ad

Fix bug introduced in commit 7233eb86d7a (few hours ago).

src/mesh/cs_mesh_quantities.c
@@ -3679,6 +3679,7 @@ cs_mesh_quantities_compute(const cs_mesh_t       *m,
   mq->b_f_face_surf = mq->b_face_surf;
 
   mq->cell_f_vol = mq->cell_vol;
+  mq->cell_f_cen = mq->cell_cen;
 
   /* Porous models */
   if (mq->c_disable_flag == NULL) {