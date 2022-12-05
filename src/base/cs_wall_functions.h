c97dab989686dd8b46ef06d223c9ecd47bf97c12
Yvan Fournier <yvan.fournier@noreply.edf.fr>
2022-12-05 19:58:37 +0100

    Revert "Fix in thermal wall function in really small y+."
    This reverts commit 07911b04.
    
    That fix was intended to solve issues postprocessing the wall temperature
    with very small y+, but broke the scalable wall function.

--- src/base/cs_wall_functions.h
+++ src/base/cs_wall_functions.h
@@ -1046,7 +1046,7 @@ cs_wall_functions_s_arpaci_larsen(cs_real_t  l_visc,
     1. Initializations
     ==========================================================================*/
 
-  (*htur) = prl * CS_MAX(yplus,epzero)/CS_MAX(yplus+dplus,epzero);
+  (*htur) = CS_MAX(yplus,epzero)/CS_MAX(yplus+dplus,epzero);
 
   prlm1 = 0.1;
 
