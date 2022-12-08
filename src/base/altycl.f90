code-saturne/code_saturne

commit 0f580358874ab5cbf9a0a99a36c1e48e56e0c47b

ALE user-defined BC's: some cleanup and documentation Fortran to C update.

src/base/paramx.f90
@@ -185,7 +185,7 @@ module paramx
   !>  - Homogeneous Neumann boundary condition for velocity and total energy (seen by the reconstruction
   !> gradients and the diffusion operator).
   !>  - Dirichlet (\ref icodcl=1) for the pressure.
-  !> Alse a boundary condition type for mesh velocity in ALE for modelling
+  !> Also a boundary condition type for mesh velocity in ALE for modelling
   !> free surface (\f$ \vect{u} \cdot \vect{S} = \vect{w} \cdot \vect{S} \f$).
   integer   ifresf
 
@@ -201,11 +201,13 @@ module paramx
   !> convection only).
   integer   i_convective_inlet
 
-
   parameter(iindef=1, ientre=2, isolib=3, isymet=4, iparoi=5,       &
             iparug=6, iesicf=7, isspcf=8, isopcf=9, iephcf=10,      &
-            ieqhcf=11, icscpl=12, icscpd=13, ifrent=14, ifresf=15,  &
+            ieqhcf=11, icscpl=12, icscpd=13, ifrent=14, ifresf=64,  &
             i_convective_inlet=16)
+
+  ! Note the ifresf maps to CS_BOUNDARY_ALE_FREE_SURFACE (1 << 6) in C,
+  ! so its value should not be changed here.
 
   !> maximal number of valuators for Navier-Stokes
   integer    nestmx

docs/doxygen/examples/cs_user_boundary_conditions_ale.dox
@@ -24,29 +24,21 @@
 
 /*-----------------------------------------------------------------------------*/
 
-
-
 /*!
 
-  \page cs_user_boundary_conditions_examples_ale Examples of data settings for ALE boundary conditions (cs_user_boundary_conditions_ale.f90)
+  \page cs_user_boundary_conditions_examples_ale Examples of data settings for ALE boundary conditions (cs_user_boundary_conditions.c)
 
  \brief User subroutine dedicated the use of ALE (Arbitrary Lagrangian
  Eulerian) Method:
-  - Fills boundary conditions (\c ialtyb, \c icodcl, \c rcodcl) for mesh velocity.
-  - This subroutine also enables one to fix displacement on nodes.
+  - Fills boundary condition types (\c ale_bc_type, \c impale)
+    and codes(\c icodcl, \c rcodcl) for mesh velocity.
+  - This function also enables one to fix displacement on nodes.
 
  \section intro_ale Introduction
 
- Here one defines boundary conditions on a per-face basis.
+ Here one defines boundary conditions for the deforming mesh on a per-face basis.
 
- Boundary faces may be identified using the \ref getfbr subroutine.
- \c cs_user_boundary_conditions.f90 subroutine,
- but a more thorough description can be found in the user guide.
-
- Boundary conditions setup for standard variables (pressure, velocity,
- turbulence, scalars) is described precisely in the user guide.
-
- Detailed explanation will be found in the theory guide.
+ Detailed explanations will be found in the theory guide.
 
  \section bc_types_ale Boundary condition types
 
@@ -54,235 +46,65 @@
 
  \subsection std_bcs_ale For "standard" boundary conditions
 
- One defines a code in the \c ialtyb array (of dimensions number of
+ One defines a code in the \c ale_bc_type array (of dimensions number of
  boundary faces). The available codes are:
 
-  - \c ialtyb(ifac) = \c ibfixe: the face \c ifac is considered to be motionless.
-           A zero Dirichlet boundary condition is automatically imposed on mesh
-           velocity. Moreover the displacement of corresponding nodes will
-           automatically be set to 0 (for further information please
-           read the paragraph dedicated to the description of \c impale array in the
-           cs_user_boundary_conditions_ale.f90 subroutine), unless the USER has modified the condition of
-           at least one mesh velocity component (modification of \c icodcl array,
-           please read the following paragraph \ref non_std_bc_ale)
+ - \c ale_bc_type[face_id] = \ref CS_BOUNDARY_ALE_FIXED: the face \c face_id
+   is considered to be motionless. A zero Dirichlet boundary condition is
+   automatically imposed on the mesh velocity. Moreover the displacement of
+   corresponding vertices will automatically be set to 0, unless the user has
+   modified the \c icodcl condition of the mesh velocity component.
 
-  - \c ialtyb(ifac) = \c igliss: The mesh slides on corresponding face \c ifac.
-           The normal component of mesh velocity is automatically set to 0.
-           A homogeneous Neumann condition is automatically prescribed for the
-           other components, as it's the case for 'Symmetry' fluid condition.
+ - \c ale_bc_type[face_id] = \ref CS_BOUNDARY_ALE_SLIDING: The mesh slides on
+   the corresponding face \c face_id. The normal component of the mesh velocity
+   is automatically set to 0. A homogeneous Neumann condition is automatically
+   prescribed for the other components, as is the case for "Symmetry" fluid condition.
 
-  - \c ialtyb(ifac) = \c ivimpo: the mesh velocity is imposed on face \c ifac. Thus,
-           the users needs to specify the mesh velocity values filling \c rcodcl
-           arrays as follows:
-            - \c rcodcl(ifac,iuma,1) = mesh velocity in 'x' direction
-            - \c rcodcl(ifac,ivma,1) = mesh velocity in 'y' direction
-            - \c rcodcl(ifac,iwma,1) = mesh velocity in 'z' direction
-            .
-           Components of \c rcodcl(.,i.ma,1) arrays that are not specified by user
-           will automatically be set to 0, meaning that user only needs to specify
-           non zero mesh velocity components.
-
+ - \c ale_bc_type[face_id] = \ref CS_BOUNDARY_ALE_IMPOSED_VEL: the mesh velocity
+   is imposed on face \c face_id. Thus, the mesh velocity's
+   \c bc_coeffs->rcodcl1 values arrays need to be set.
 
  \subsection non_std_bc_ale For "non-standard" conditions
 
- Other than (fixed boundary, sliding mesh boundary, fixed velocity), one
- defines for each face and each component \c IVAR = IUMA, IVMA, IWMA:
-  - a code
-    - \c icodcl(ifac, ivar)
-  - three real values:
-    - \c rcodcl(ifac, ivar, 1)
-    - \c rcodcl(ifac, ivar, 2)
-    - \c rcodcl(ifac, ivar, 3)
-
- The value of \c icodcl is taken from the following:
+ The value of \c icodcl for the "mesh_velocity" field's `bc_coeffs` structure
+ is taken from the following:
   - 1: Dirichlet
   - 3: Neumann
   - 4: Symmetry
 
  The values of the 3 \c rcodcl components are:
-  - \c rcodcl(ifac, ivar, 1):
-     Dirichlet for the variable if \c icodcl(ifac, ivar) = 1
-     The dimension of \c rcodcl(ifac, ivar, 1) is in \f$m \cdot s^{-1}\f$
-  - \c rcodcl(ifac, ivar, 2):
-    "exterior" exchange coefficient (between the prescribed value
-                      and the value at the domain boundary),\n
-                      rinfin = infinite by default
-    \f$  rcodcl(ifac,ivar,2) =  \dfrac{VISCMA}{d} \f$
-          (d has the dimension of a distance in \f$m\f$, \f$VISCMA\f$ stands for
-          the mesh viscosity)
+  - \c rcodcl1[face_id]:
+     Dirichlet for the variable if \c icodcl[face_id] = 1
+     The dimension of \c rcodcl1[face_id] is in \f$m \cdot s^{-1}\f$
+  - \c rcodcl2[face_id]:
+    "exterior" exchange coefficient; mmake no sense for the mesh velocity,
+    so can be ignored.
 
-
- \remark
-  - The definition of \c rcodcl(.,.,2) is based on the manner
-            other standard variables are managed in the same case.
-            This type of boundary condition appears nonsense
-            concerning mesh in that context.
-
-      - \c rcodcl(ifac,ivar,3) :
-    Flux density (in \f$kg \cdot m \cdot s^2\f$) = J if icodcl(ifac, ivar) = 3
+  - \c rcodcl3[face_id]:
+    Flux density (in \f$kg \cdot m \cdot s^2\f$) = J if icodcl[face_id] = 3
                  (<0 if gain, n outwards-facing normal)
-    \f$ rcodcl(ifac,ivar,3) = -(VISCMA) \grad {Um}.\vect{n} \f$
+    \f$ rcodcl3[face_id] = -(mesh_viscosity) \grad {Um}.\vect{n} \f$
               \f$(Um\f$ represents mesh velocity)
 
+    - \c rcodcl3[face_id] = 0. enables one to specify a homogeneous
+      Neuman condition on mesh velocity. Any other value make no sense
+      in this context.
 
-  - The definition of condition \c rcodcl(ifac,ivar,3)
-            is based on the manner other standard variables are
-            managed in the same case.
-            \c rcodcl(.,.,3) = 0.d0 enables one to specify a homogeneous
-            Neuman condition on mesh velocity. Any other value will be
-            physically nonsense in that context.
+ If the user assigns a value to \c ale_bc_type equal to
+ \ref CS_BOUNDARY_ALE_FIXED, \ref CS_BOUNDARY_ALE_SLIDING, or
+ \ref CS_BOUNDARY_ALE_IMPOSED_VEL and does not modify \c icodcl (zero value by
+ default), \c ale_bc_type will define the boundary condition type.
 
-  - If the user assigns a value to \c ialtyb equal to \c ibfixe, \c igliss,
- or \c ivimpo and does not modify \c icodcl (zero value by
- default), \c ialtyb will define the boundary condition type.
+ To the contrary, if the user prescribes \c icodcl[face_id] (nonzero) for
+ the mesh velocity, the values assigned to rcodcl* will be used for the
+ considered face.
 
- To the contrary, if the user prescribes \c icodcl(ifac, ivar) (nonzero),
- the values assigned to rcodcl will be used for the considered face
- and variable (if rcodcl values are not set, the default values will
- be used for the face and variable, so:
-                         - \c rcodcl(ifac, ivar, 1) = 0.d0
-                         - \c rcodcl(ifac, ivar, 2) = rinfin
-                         - \c rcodcl(ifac, ivar, 3) = 0.d0)
+ \subsection ug_ex_add_bc_ale Further ALE boundary condition elements
 
- If the user decides to prescribe his own non-standard boundary conditions,
- it will be necessary to assign values to \c icodcl AND to \c rcodcl for ALL
- mesh velocity components. Thus, the user does not need to assign values
- to \c ialtyb for each associated face, as it will not be taken into account
- in the code.
+ The [Boundary conditions for ALE](@ref sec_ug_bc_ale_legacy)
+ section of the user guide provides additional details.
 
-
- \subsection cons_rul_ale Consistency rules
-
- A consistency rules between \c icodcl codes for variables with
- non-standard boundary conditions:
-  - If a symetry code (\c icodcl = 4) is imposed for one mesh velocity
-    component, one must have the same condition for all other mesh
-    velocity components.
-
-
- \subsection fix_nod_ale Fixed displacement on nodes
-
- For a better precision concerning mesh displacement, one can also assign values
- of displacement to certain internal and/or boundary nodes. Thus, one
- need to fill \c disale and \c impale arrays :
-  - \c disale(1,inod) = displacement of node inod in 'x' direction
-  - \c disale(2,inod) = displacement of node inod in 'y' direction
-  - \c disale(3,inod) = displacement of node inod in 'z' direction
- This array is defined as the total displacement of the node compared
- its initial position in initial mesh.
- \c impale(inod) = 1 indicates that the displacement of node inod is imposed.
-
-
- \note \c impale array is initialized to the value of 0; if its value
-       is not modified, corresponding value in \c DEPALE array will not be
-       taken into account.
-
- During mesh's geometry re-calculation at each time step, the position of the
- nodes, which displacement is fixed (\c i.e. \c impale=1), is not calculated
- using the value of mesh velocity at the center of corresponding cell, but
- directly filled using the values of \c disale.
-
- If the displacement is fixed for all nodes of a boundary face it's not
- necessary to prescribe boundary conditions at this face on mesh velocity.
- \c icodcl and \c rcodcl values will be overwritten:
-  - \c icodcl is automatically set to 1 (Dirichlet)
-  - \c rcodcl value will be automatically set to face's mean mesh velocity
-    value, that is calculated using \c DEPALE array.
-
- If a fixed boundary condition (\c ialtyb(ifac)=ibfixe) is imposed to the face
- \c ifac, the displacement of each node \c inod belonging to \c ifac is considered
- to be fixed, meaning that \c impale(inod) = 1 and \c disale(.,inod) = 0.d0.
-
-
- \subsubsection nod_des_ale Description of nodes
-
- \c nnod gives the total (internal and boundary) number of nodes.
- Vertices coordinates are given by \c xyznod(3, nnod) array. This table is
- updated at each time step of the calculation.
- \c xyzno0(3,nnod) gives the coordinates of initial mesh at the beginning
- of the calculation.
-
- The faces - nodes connectivity is stored by means of four integer arrays :
- \c ipnfac, \c nodfac, \c ipnfbr, \c nodfbr.
-
- \c nodfac(nodfbr) stores sequentially the index-numbers of the nodes of each
- internal (boundary) face.
- \c ipnfac(ipnfbr) gives the position of the first node of each internal
- (boundary) face in the array \c nodfac(nodfbr).
-
- For example, in order to get all nodes of internal face \c ifac, one can
- use the following loop:
-
- \code
- do ii = ipnfac(ifac), ipnfac(ifac+1)-1 !! index number of nodfac array
-                                        !! corresponding to ifac
-
-   inod = nodfac(ii)                    !! index-number iith node of face ifac.
-   !! ...
- enddo
- \endcode
-
-
- \subsection flui_bc_ale Influence on boundary conditions related to fluid velocity
-
- The effect of fluid velocity and ALE modeling on boundary faces that
- are declared as walls (\c itypfb = \c iparoi or \c iparug) really depends on
- the physical nature of this interface.
-
- Indeed when studying an immersed structure the motion of corresponding
- boundary faces is the one of the structure, meaning that it leads to
- fluid motion. On the other hand when studying a piston the motion of vertices
- belonging to lateral boundaries has no physical meaning therefore it has
- no influence on fluid motion.
-
- Whatever the case, mesh velocity component that is normal to the boundary
- face is always taken into account
- (\f$ \vect{u}_{fluid} \cdot \vect{n} = \vect{w}_{mesh} \cdot \vect{n} \f$).
-
- The modeling of tangential mesh velocity component differs from one case
- to another.
-
- The influence of mesh velocity on boundary conditions for fluid modeling is
- managed and modeled in \c code_saturne as follows:
-  - If \c ialtyb(ifac) = ibfixe: mesh velocity equals 0. (In case of 'fluid sliding
-  wall' modeling corresponding condition will be specified in code_saturne
-  Interface or in cs_user_boundary_conditions.f90 subroutine.)
-  - If \c ialtyb(ifac) = ivimpo: tangential mesh velocity is modeled as a sliding
-  wall velocity in fluid boundary conditions unless a value for fluid sliding
-  wall velocity has been specified in the GUI or in \ref cs_user_boundary_conditions.
-  - If \c ialtyb(ifac) = igliss: tangential mesh velocity is not taken into account
-  in fluid boundary conditions (In case of 'fluid sliding wall' modeling
-  corresponding condition will be specified in code_saturne Interface
-  or in cs_user_boundary_conditions.f90 subroutine.)
-  - If \c impale(inod) = 1 for all vertices of a boundary face: tangential mesh
-  velocity value that has been derived from nodes displacement is modeled as a
-  sliding wall velocity in fluid boundary conditions unless a value for fluid
-  sliding wall velocity has been specified by USER in code_saturne Interface or
-  in cs_user_boundary_conditions.f90 subroutine.
-
- Note that mesh velocity has no influence on modeling of
- boundary faces with 'inlet' or 'free outlet' fluid boundary condition.
-
- For "non standard" conditions USER has to manage the influence of boundary
- conditions for ALE method (i.e. mesh velocity) on the ones for Navier Stokes
- equations(i.e. fluid velocity). (Note that fluid boundary conditions can be
- specified in this subroutine.)
-
-\subsubsection cell_id_ale Cells identification
-
- Cells may be identified using the getcel subroutine.
- The syntax of this subroutine is described in the
- cs_user_boundary_conditions.f90 subroutine,
- but a more thorough description can be found in the user guide.
-
- \subsubsection fac_id_ale Faces identification
-
- Faces may be identified using the \ref getfbr subroutine.
- The syntax of this subroutine is described in the
- cs_user_boundary_conditions.f90 subroutine,
- but a more thorough description can be found in the user guide.
-
-  \section example_ale Example of ALE boundary conditions
+ \section example_ale Example of ALE boundary conditions
 
 Here is the list of examples:
 
@@ -303,7 +125,7 @@ Here is the list of examples:
   Loop on faces from a subset.\n
   Set the boundary condition for each face.
 
-  \subsection calcualtion_ale Calculation of displacement at current time step
+  \subsection calculation_ale Calculation of displacement at current time step
 
   \snippet  cs_user_boundary_conditions_ale-base.c calcul
 

docs/doxygen/examples/cs_var_dico.dox
@@ -86,7 +86,7 @@
   <tt> nnod     | cs_glob_mesh->n_vertices </tt>        | Number of vertices
   -             | <tt> cs_glob_mesh->n_b_cells </tt>    | Number of boundary cells
   <tt> lndfac   | cs_glob_mesh->i_face_vtx_connect_size </tt> | Size of the connectivity \n interior faces -> vertices
-  <tt> lndfbr   | cs_glob_mesh->b_face_vtx_connect_size  </tt>| Size of the connectivity \n boundary faces -> vertices
+  <tt> lndfbr   | cs_glob_mesh->b_face_vtx_connect_size </tt>| Size of the connectivity \n boundary faces -> vertices
   <tt> ifacel   | cs_glob_mesh->i_face_cells </tt>      | Interior faces -> cells connectivity
   <tt> ifabor   | cs_glob_mesh->b_face_cells </tt>      | Boundary faces -> cells connectivity
   <tt> ipnfac   | cs_glob_mesh->i_face_vtx_idx </tt>    | Interior faces -> vertices index
@@ -95,6 +95,9 @@
   <tt> nodfbr   | cs_glob_mesh->b_face_vtx_lst </tt>    | Boundary faces -> vertices connectivity
   -             | <tt> cs_glob_mesh->b_cells </tt>      | Boundary cell list
   <tt> xyznod   | cs_glob_mesh->vtx_coord </tt>         | Vertex coordinates
+
+ Note that the faces - vertices connectivity is stored using
+ [indexed arrays](@ref sec_prg_indexed_arrays).
 
   \section cs_var_dico_mesh_q_vars Mesh quantity variables
 

src/base/condli.f90
@@ -426,10 +426,7 @@ if (iale.ge.1) then
   ! - Interface code_saturne
   !   ======================
 
-  call uialcl(ibfixe, igliss, ivimpo, ifresf,   &
-             ialtyb, impale, disale,            &
-             iuma, ivma, iwma,                  &
-             rcodcl)
+  call uialcl(ialtyb, impale, disale)
 
   ! TODO in the future version: remove dt, xyzno0, and disale
   ! because they are avaliable as fields.
@@ -4981,21 +4978,11 @@ if (iale.ge.1) then
   ! - Interface code_saturne
   !   ======================
 
-  call uialcl &
-    ( ibfixe, igliss, ivimpo, ifresf,    &
-      ialtyb,                            &
-      impale,                            &
-      disale,                            &
-      iuma, ivma, iwma,                  &
-      rcodcl)
+  call uialcl(ialtyb, impale, disale)
 
-  call usalcl &
-    ( itrale ,                                                       &
-    nvar   , nscal  ,                                              &
-    icodcl , itypfb , ialtyb ,                                     &
-    impale ,                                                       &
-    dt     ,                                                       &
-    rcodcl , xyzno0 , disale )
+  call usalcl(itrale, nvar, nscal,              &
+              icodcl, itypfb, ialtyb, impale,   &
+              dt, rcodcl, xyzno0, disale)
 
   !     Au cas ou l'utilisateur aurait touche disale sans mettre impale=1, on
   !     remet le deplacement initial

src/gui/cs_gui_mobile_mesh.c
@@ -215,24 +215,15 @@ _uialcl_fixed_displacement(cs_tree_node_t   *tn_w,
  *
  * parameters:
  *   tn_w         <-- pointer to tree node for a given mobile_boundary BC
- *   iuma         <-- iuma
- *   ivma         <-- ivma
- *   iwma         <-- iwma
  *   nfabor       <-- Number of boundary faces
  *   z            <-- selected boundary zone
- *   rcodcl       --> rcodcl
  *----------------------------------------------------------------------------*/
 
 static void
 _uialcl_fixed_velocity(cs_tree_node_t  *tn_w,
-                       int              iuma,
-                       int              ivma,
-                       int              iwma,
-                       int              ivimpo,
                        cs_lnum_t        nfabor,
                        const cs_zone_t *z,
-                       int              ialtyb[],
-                       double          *rcodcl)
+                       int              ialtyb[])
 {
   /* Get formula */
   const char *formula = _get_ale_boundary_formula(tn_w, "fixed_velocity");
@@ -247,17 +238,19 @@ _uialcl_fixed_velocity(cs_tree_node_t  *tn_w,
                                                 "mesh_velocity",
                                                 "fixed_velocity");
 
+  cs_field_t *f_mesh_u = cs_field_by_name("mesh_velocity");
+  cs_real_t *rcodcl_mv = f_mesh_u->bc_coeffs->rcodcl1;
+
   /* Loop over boundary faces */
   for (cs_lnum_t elt_id = 0; elt_id < z->n_elts; elt_id++) {
     const cs_lnum_t face_id = z->elt_ids[elt_id];
 
-
     /* Fill  rcodcl */
-    rcodcl[(iuma-1) * nfabor + face_id] = bc_vals[elt_id + 0 * z->n_elts];
-    rcodcl[(ivma-1) * nfabor + face_id] = bc_vals[elt_id + 1 * z->n_elts];
-    rcodcl[(iwma-1) * nfabor + face_id] = bc_vals[elt_id + 2 * z->n_elts];
+    rcodcl_mv[nfabor*0 + face_id] = bc_vals[elt_id + 0 * z->n_elts];
+    rcodcl_mv[nfabor*1 + face_id] = bc_vals[elt_id + 1 * z->n_elts];
+    rcodcl_mv[nfabor*2 + face_id] = bc_vals[elt_id + 2 * z->n_elts];
 
-    ialtyb[face_id]  = ivimpo;
+    ialtyb[face_id] = CS_BOUNDARY_ALE_IMPOSED_VEL;
 
   }
 
@@ -497,23 +490,11 @@ void CS_PROCF (uialin, UIALIN) (int     *nalinf,
  *   ialtyb       <-- ialtyb
  *   impale       <-- uialcl_fixed_displacement
  *   disale       <-- See uialcl_fixed_displacement
- *   iuma         <-- See uialcl_fixed_velocity
- *   ivma         <-- See uialcl_fixed_velocity
- *   iwma         <-- See uialcl_fixed_velocity
- *   rcodcl       <-- See uialcl_fixed_velocity
  *----------------------------------------------------------------------------*/
 
-void CS_PROCF (uialcl, UIALCL) (const int *const    ibfixe,
-                                const int *const    igliss,
-                                const int *const    ivimpo,
-                                const int *const    ifresf,
-                                int       *const    ialtyb,
-                                int       *const    impale,
-                                cs_real_3_t        *disale,
-                                const int *const    iuma,
-                                const int *const    ivma,
-                                const int *const    iwma,
-                                double    *const    rcodcl)
+void CS_PROCF (uialcl, UIALCL) (int         *const  ialtyb,
+                                int         *const  impale,
+                                cs_real_3_t        *disale)
 {
   const cs_mesh_t *m = cs_glob_mesh;
 
@@ -548,19 +529,19 @@ void CS_PROCF (uialcl, UIALCL) (const int *const    ibfixe,
     if (nature ==  ale_boundary_nature_fixed_wall) {
       for (cs_lnum_t ifac = 0; ifac < n_faces; ifac++) {
         cs_lnum_t ifbr = faces_list[ifac];
-        ialtyb[ifbr] = *ibfixe;
+        ialtyb[ifbr] = CS_BOUNDARY_ALE_FIXED;
       }
     }
     else if (nature ==  ale_boundary_nature_sliding_wall) {
       for (cs_lnum_t ifac = 0; ifac < n_faces; ifac++) {
         cs_lnum_t ifbr = faces_list[ifac];
-        ialtyb[ifbr] = *igliss;
+        ialtyb[ifbr] = CS_BOUNDARY_ALE_SLIDING;
       }
     }
     else if (nature ==  ale_boundary_nature_free_surface) {
       for (cs_lnum_t ifac = 0; ifac < n_faces; ifac++) {
         cs_lnum_t ifbr = faces_list[ifac];
-        ialtyb[ifbr] = *ifresf;
+        ialtyb[ifbr] = CS_BOUNDARY_ALE_FREE_SURFACE;
       }
     }
     else if (nature == ale_boundary_nature_fixed_displacement) {
@@ -570,10 +551,7 @@ void CS_PROCF (uialcl, UIALCL) (const int *const    ibfixe,
                                  disale);
     }
     else if (nature == ale_boundary_nature_fixed_velocity) {
-      _uialcl_fixed_velocity(tn_bc, *iuma, *ivma, *iwma, *ivimpo,
-                             m->n_b_faces,
-                             z,
-                             ialtyb, rcodcl);
+      _uialcl_fixed_velocity(tn_bc, m->n_b_faces, z, ialtyb);
     }
   }
 }
@@ -840,9 +818,9 @@ cs_gui_mobile_mesh_get_boundaries(cs_domain_t  *domain)
     /*                              impale, disale); */
     /* } */
     /* else if (nature == ale_boundary_nature_fixed_velocity) { */
-    /*   _uialcl_fixed_velocity(tn_bndy, *iuma, *ivma, *iwma, *ivimpo, */
+    /*   _uialcl_fixed_velocity(tn_bndy, *ivimpo, */
     /*                          m->n_b_faces, z, */
-    /*                          ialtyb, rcodcl); */
+    /*                          ialtyb); */
     /* } */
 
   } /* Loop on mobile_boundary zones */
@@ -859,7 +837,7 @@ cs_gui_mobile_mesh_get_boundaries(cs_domain_t  *domain)
 /*----------------------------------------------------------------------------*/
 
 cs_real_t *
-cs_gui_mobile_mesh_get_fixed_velocity(const char    *label)
+cs_gui_mobile_mesh_get_fixed_velocity(const char  *label)
 {
   cs_tree_node_t *tn_b0 = cs_tree_get_node(cs_glob_tree, "boundary_conditions");
 

commit 84c825208d016e48e74407a977e214f058a6b286

Fix crash in ALE with free surface introduced in commit 0f580358(Dec 1 2022).
The Fortran ifresf parameter was aligned with CS_BOUNDARY_ALE_FREE_SURFACE
but the CS_FREE_SURFACE parameter was not.

src/base/altycl.f90
@@ -178,7 +178,7 @@ do ifac = 1, nfabor
   if (ialtyb(ifac).ne.0      .and.                                &
       ialtyb(ifac).ne.ibfixe .and.                                &
       ialtyb(ifac).ne.igliss .and.                                &
-      ialtyb(ifac).ne.ifresf .and.                                &
+      ialtyb(ifac).ne.ibalfs .and.                                &
       ialtyb(ifac).ne.ivimpo ) then
     if (ialtyb(ifac).gt.0) then
       ialtyb(ifac) = -ialtyb(ifac)
@@ -281,7 +281,7 @@ do ifac = 1, nfabor
     if (icodcl(ifac,iwma).eq.0) icodcl(ifac,iwma) = 1
 
   ! --> Free surface face: the mesh velocity is imposed by the mass flux
-  elseif (ialtyb(ifac).eq.ifresf) then
+  elseif (ialtyb(ifac).eq.ibalfs) then
 
     if (icodcl(ifac,iuma).eq.0) then
       icodcl(ifac,iuma) = 1

src/base/cs_boundary.h
@@ -116,10 +116,10 @@ typedef enum {
   CS_BOUNDARY_INLET_SUBSONIC_PH   = 1<<11,
 
   /*! compressible subsonic */
-  CS_BOUNDARY_SUBSONIC             = 1<<12,
+  CS_BOUNDARY_SUBSONIC            = 1<<12,
 
   /*! compressible supersonic */
-  CS_BOUNDARY_SUPERSONIC           = 1<<13,
+  CS_BOUNDARY_SUPERSONIC          = 1<<13,
 
   /*! free surface */
   CS_BOUNDARY_FREE_SURFACE        = 1<<14,

src/base/paramx.f90
@@ -182,11 +182,9 @@ module paramx
 
   !> \anchor ifresf
   !> if \ref itypfb=ifresf: free surface for mobile mesh boundary condition
-  !>  - Homogeneous Neumann boundary condition for velocity and total energy (seen by the reconstruction
-  !> gradients and the diffusion operator).
+  !>  - Homogeneous Neumann boundary condition for velocity and total energy
+  !> (seen by the reconstruction gradients and the diffusion operator).
   !>  - Dirichlet (\ref icodcl=1) for the pressure.
-  !> Also a boundary condition type for mesh velocity in ALE for modelling
-  !> free surface (\f$ \vect{u} \cdot \vect{S} = \vect{w} \cdot \vect{S} \f$).
   integer   ifresf
 
   !> \anchor i_convective_inlet
@@ -203,7 +201,7 @@ module paramx
 
   parameter(iindef=1, ientre=2, isolib=3, isymet=4, iparoi=5,       &
             iparug=6, iesicf=7, isspcf=8, isopcf=9, iephcf=10,      &
-            ieqhcf=11, icscpl=12, icscpd=13, ifrent=14, ifresf=64,  &
+            ieqhcf=11, icscpl=12, icscpd=13, ifrent=14, ifresf=15,  &
             i_convective_inlet=16)
 
   ! Note the ifresf maps to CS_BOUNDARY_ALE_FREE_SURFACE (1 << 6) in C,
@@ -325,16 +323,23 @@ module paramx
   !> - In the case where all the nodes of a face have a imposed displacement,
   !> it is not necessary to fill the tables with boundary conditions
   !> mesh velocity for this face, they will be erased. In the other case,
-  !> the value of the Dirichlet must be given in \ref rcodcl "rcodcl"(ifac,ivar,1)
+  !> the value of the Dirichlet must be given in
+  !> \ref rcodcl "rcodcl"(ifac,ivar,1)
   !> for every value of \c ivar (\ref iuma, \ref ivma and \ref iwma).
   !> The other boxes of \ref rcodcl and \ref icodcl are completed automatically.
   !> The tangential mesh velocity is taken like a tape speed under the
   !> boundary conditions of wall for the fluid, except if wall fluid velocity
-  !> was specified by the user in the interface or \ref cs_user_boundary_conditions
-  !> (in which case it is this speed which is considered).
+  !> was specified by the user in the interface or
+  !> \ref cs_user_boundary_conditions (in which case it is this speed
+  !> which is considered).
   integer   ivimpo
 
-  parameter(ibfixe=1, igliss=2, ivimpo=4)
+  !> \anchor ifresf
+  !> Boundary condition type for mesh velocity in ALE for modelling
+  !> free surface (\f$ \vect{u} \cdot \vect{S} = \vect{w} \cdot \vect{S} \f$).
+  integer   ibalfs
+
+  parameter(ibfixe=1, igliss=2, ivimpo=4, ibalfs=64)
 
   !> maximum number of structures in ALE
   integer    nstrmx

src/base/vericl.f90
@@ -700,7 +700,7 @@ iok = 0
 if (iale.ge.1) then
   grav2 = gx**2 + gy**2 + gz**2
   do ifac = 1, nfabor
-    if (ialtyb(ifac).eq.ifresf.and.grav2.le.epzero**2) then
+    if (ialtyb(ifac).eq.ibalfs.and.grav2.le.epzero**2) then
       iok = 1
     endif
   enddo