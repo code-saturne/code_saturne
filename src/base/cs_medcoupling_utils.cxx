code-saturne/code_saturne

commit b4e2e82e77b3db0908326b8d04b8e9887e11eef5

[MEDCPL] Fix desrtoy function related do medcoupling mesh export

src/base/cs_medcoupling_utils.cxx
@@ -715,7 +715,7 @@ cs_medcoupling_mesh_destroy(cs_medcoupling_mesh_t  *mesh)
   BFT_FREE(mesh->bbox);
 
 #if defined(HAVE_MEDCOUPLING)
-  BFT_FREE(mesh->med_mesh);
+  mesh->med_mesh-decrRef();
 #endif
 
   BFT_FREE(mesh);

commit 4975161a4a32f8500b5b904895df2262f30b4573

Fix compilation with MEDCoupling, broken by typo from commit b4e2e82e.

src/base/cs_medcoupling_utils.cxx
@@ -715,7 +715,7 @@ cs_medcoupling_mesh_destroy(cs_medcoupling_mesh_t  *mesh)
   BFT_FREE(mesh->bbox);
 
 #if defined(HAVE_MEDCOUPLING)
-  mesh->med_mesh-decrRef();
+  mesh->med_mesh->decrRef();
 #endif
 
   BFT_FREE(mesh);