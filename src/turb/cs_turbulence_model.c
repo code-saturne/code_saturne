code-saturne/code_saturne

commit ba8ed0aed727533bd9097fb650af617750bcd5af

Improve/fix some C/Fortran mappings.
Many turbulence constants were not useful anymore in Fortran, but a few were
modified in the legacy Fortran version of modini based on turbulence model
options, where they should have been modified in C.

This is now fixed, and many variables not needed anymore in Fortran are
removed from the cstphy module.

Also, where possible, initialization of Fortran values with the 'save'
attibute is done directly in the declaration, rather than in iniini.f90.

A few other minor Fortran->C mappings are improved.

src/turb/cs_turbulence_model.c
@@ -54,7 +54,6 @@
 #include "cs_log.h"
 #include "cs_mesh.h"
 #include "cs_parall.h"
-#include "cs_parameters.h"
 #include "cs_mesh_location.h"
 #include "cs_time_step.h"
 #include "cs_wall_functions.h"
@@ -489,8 +488,9 @@ double cs_turb_bpow = 1.0/7.0;
 double cs_turb_dpow = -1.;
 
 /*!
- * Constant \f$C_\mu\f$ for all the RANS turbulence models except for the
- * Warning: different value for v2f models. Useful if and only if \ref iturb = 20, 21, 30, 31, 50, 51 or 60
+ * Constant \f$C_\mu\f$ for all the RANS turbulence models.
+ * Warning: different value for v2f models. Useful only for
+ * RANS models
  * (\f$k-\varepsilon\f$, \f$R_{ij}-\varepsilon\f$ or \f$k-\omega\f$).
  */
 double cs_turb_cmu = 0.09;
@@ -1023,6 +1023,9 @@ double cs_turb_cthafm = 0.236;
  * Constant of GGDH and AFM on the thermal scalar.
  */
 double cs_turb_cthdfm = 0.31;
+double cs_turb_cthebdfm = 0.22; /* For the EB-DFM model
+                                   (0.122*2.5, See F. Dehoux thesis) */
+
 
 /*! \cond DOXYGEN_SHOULD_SKIP_THIS */
 
@@ -1067,31 +1070,21 @@ cs_f_turb_reference_values(double  **almax,
                            double  **xlomlg);
 
 void
-cs_f_turb_model_constants_get_pointers(double  **cmu,
+cs_f_turb_model_constants_get_pointers(double  **apow,
+                                       double  **bpow,
+                                       double  **cmu,
                                        double  **cmu025,
                                        double  **crij1,
                                        double  **crij2,
                                        double  **crij3,
                                        double  **csmago,
-                                       double  **smagmx,
-                                       double  **smagmn,
                                        double  **cwale,
                                        double  **xlesfd,
                                        double  **xlesfl,
                                        double  **ales,
                                        double  **bles,
                                        double  **cdries,
-                                       double  **ce1,
-                                       double  **ce2,
-                                       double  **csrij,
-                                       double  **c1trit,
-                                       double  **c2trit,
-                                       double  **c3trit,
-                                       double  **c4trit,
-                                       double  **cddes,
-                                       double  **csas,
-                                       double  **csas_eta2,
-                                       double  **chtles_bt0);
+                                       double  **csrij);
 
 /*============================================================================
  * Private function definitions
@@ -1247,52 +1240,27 @@ cs_f_turb_reference_values(double  **almax,
  *
  * This function is intended for use by Fortran wrappers, and
  * enables mapping to Fortran global pointers.
- *
- * parameters:
- *   cmu    --> pointer to cs_turb_cmu
- *   cmu025 --> pointer to cs_turb_cmu025
- *   crij1  --> pointer to cs_turb_crij1
- *   crij2  --> pointer to cs_turb_crij2
- *   crij3  --> pointer to cs_turb_crij3
- *   csmago --> pointer to cs_turb_csmago
- *   smagmx --> pointer to cs_turb_smago_max
- *   smagmn --> pointer to cs_turb_smago_min
- *   cwale  --> pointer to cs_turb_cwale
- *   xlesfd --> pointer to cs_turb_xlesfd
- *   xlesfl --> pointer to cs_turb_xlesfl
- *   ales   --> pointer to cs_turb_ales
- *   bles   --> pointer to cs_turb_bles
- *   cdries --> pointer to cs_turb_cdries
- *   and so on
  *----------------------------------------------------------------------------*/
 
 void
-cs_f_turb_model_constants_get_pointers(double  **cmu,
+cs_f_turb_model_constants_get_pointers(double  **apow,
+                                       double  **bpow,
+                                       double  **cmu,
                                        double  **cmu025,
                                        double  **crij1,
                                        double  **crij2,
                                        double  **crij3,
                                        double  **csmago,
-                                       double  **smagmx,
-                                       double  **smagmn,
                                        double  **cwale,
                                        double  **xlesfd,
                                        double  **xlesfl,
                                        double  **ales,
                                        double  **bles,
                                        double  **cdries,
-                                       double  **ce1,
-                                       double  **ce2,
-                                       double  **csrij,
-                                       double  **c1trit,
-                                       double  **c2trit,
-                                       double  **c3trit,
-                                       double  **c4trit,
-                                       double  **cddes,
-                                       double  **csas,
-                                       double  **csas_eta2,
-                                       double  **chtles_bt0)
+                                       double  **csrij)
 {
+  *apow   = &cs_turb_apow;
+  *bpow   = &cs_turb_bpow;
   *cmu    = &cs_turb_cmu;
   *cmu025 = &cs_turb_cmu025;
   *crij1 = &cs_turb_crij1;
@@ -1300,25 +1268,13 @@ cs_f_turb_model_constants_get_pointers(double  **cmu,
   *crij3 = &cs_turb_crij3;
   *csmago= &cs_turb_csmago;
   *csmago= &cs_turb_csmago;
-  *smagmx= &cs_turb_csmago_max;
-  *smagmn= &cs_turb_csmago_min;
   *cwale = &cs_turb_cwale;
   *xlesfd= &cs_turb_xlesfd;
   *xlesfl= &cs_turb_xlesfl;
   *ales  = &cs_turb_ales;
   *bles  = &cs_turb_bles;
   *cdries= &cs_turb_cdries;
-  *ce1   = &cs_turb_ce1;
-  *ce2   = &cs_turb_ce2;
   *csrij = &cs_turb_csrij;
-  *c1trit= &cs_turb_c1trit;
-  *c2trit= &cs_turb_c2trit;
-  *c3trit= &cs_turb_c3trit;
-  *c4trit= &cs_turb_c4trit;
-  *cddes = &cs_turb_cddes;
-  *csas  = &cs_turb_csas;
-  *csas_eta2 = &cs_turb_csas_eta2;
-  *chtles_bt0 = &cs_turb_chtles_bt0;
 }
 
 /*============================================================================

docs/doxygen/examples/cs_user_parameters.dox
@@ -258,6 +258,16 @@
 
   \snippet cs_user_parameters-base.c param_density_id
 
+  By default, the auxiliary restart file is read if present, but it may be
+  useful to deactivate its use when restarting after a preprocessing stage
+  possibly leading to a different number of faces (such as simply joining meshes
+  on a different architecture or optimization level or with different options).
+
+  Writing of auxiliary restart files may also be deactivated setting
+  \ref cs_glob_restart_auxiliary::write_auxiliary to 0,
+
+  \snippet cs_user_parameters-base.c deactivate_aux_checkpoint_write
+
   \subsection cs_user_parameters_h_cs_user_parameters_fields Special fields and activate some automatic post-processings
 
   For example, to force the presence of a boundary temperature field, which
@@ -335,7 +345,6 @@
   Probes for Radiative Transfer (Luminance and radiative density flux vector).
 
   \snippet cs_user_parameters-base.c setup_post_lum
-
 
   \section cs_user_parameters_h_cs_user_model_cdo Base model for CDO/HHO schemes
 

src/Makefile.am
@@ -104,7 +104,7 @@ base/turbomachinery.f90
 base/albase.$(OBJEXT): base/cplsat.$(OBJEXT) base/optcal.$(OBJEXT) \
  base/cs_c_bindings.$(OBJEXT)
 
-base/alstru.$(OBJEXT): base/paramx.$(OBJEXT)
+base/alstru.$(OBJEXT): base/paramx.$(OBJEXT) base/cstnum.$(OBJEXT)
 
 base/cavitation.$(OBJEXT): base/cstphy.$(OBJEXT) base/field.$(OBJEXT) \
  base/numvar.$(OBJEXT) base/optcal.$(OBJEXT) base/pointe.$(OBJEXT) \

src/base/alaste.f90
@@ -40,8 +40,8 @@ module alaste
   ! nbaste : nombre de structures mobiles
   ! nbfast : nombre de faces couplees
 
-  integer, save ::  ntcast
-  integer, save ::  nbaste, nbfast
+  integer, save ::  ntcast = 0
+  integer, save ::  nbaste = -999, nbfast = 0
 
   !=============================================================================
 

src/base/albase.f90
@@ -44,13 +44,13 @@ module albase
   !>  - 2: CDO solver
   integer(c_int), pointer, save :: iale
   !> the number of sub-iterations of initialization of the fluid
-  integer, save :: nalinf
+  integer, save :: nalinf = 0
   !> maximum number of implicitation iterations of the structure displacement
-  integer, save :: nalimx
+  integer, save :: nalimx = 1
   !> relative precision of implicitation of the structure displacement
-  double precision, save :: epalim
+  double precision, save :: epalim = 1.d-5
   !> iteration (yes=1, no=0) to initialize ALE
-  integer, save :: italin
+  integer, save :: italin = -999
 
   !> indicator of imposed displacement
   integer, allocatable, dimension(:) :: impale

src/base/alstru.f90
@@ -29,6 +29,7 @@ module alstru
   !=============================================================================
 
   use paramx
+  use cstnum
 
   implicit none
 
@@ -40,7 +41,7 @@ module alstru
   !> \{
 
   !> number of structures, automatically computed
-  integer, save :: nbstru
+  integer, save :: nbstru = -999
 
   !> \anchor xmstru mass matrix of the structure (kg)
   !> (for \ref xmstru "xmstru"(i,j,k), i and j are the array of mass structure
@@ -92,22 +93,22 @@ module alstru
   double precision, save :: dtstr(nstrmx)
 
   !> coefficient for the predicted displacement
-  double precision, save :: aexxst
+  double precision, save :: aexxst = -grand
 
   !> coefficient for the predicted displacement
-  double precision, save :: bexxst
+  double precision, save :: bexxst = -grand
 
   !> coefficient for the predicted force
-  double precision, save :: cfopre
+  double precision, save :: cfopre = -grand
 
   !> alpha coefficient for the Newmark hht methode
-  double precision, save :: alpnmk
+  double precision, save :: alpnmk = 0.d0
 
   !> beta coefficient for the Newmark hht methode
-  double precision, save :: betnmk
+  double precision, save :: betnmk = -grand
 
   !> gamma coefficient for the Newmark hht methode
-  double precision, save :: gamnmk
+  double precision, save :: gamnmk = -grand
 
   !> \}
 

src/base/cplsat.f90
@@ -40,9 +40,9 @@ module cplsat
   !> \{
 
   !> number of couplings code_saturne / code_saturne
-  integer, save :: nbrcpl
+  integer, save :: nbrcpl = 0
   !> indicator coupling face / face only
-  integer, save :: ifaccp
+  integer, save :: ifaccp = 0
   !> maximum permissible number of coupling
   integer   nbcpmx
   parameter(nbcpmx=10)
@@ -58,10 +58,10 @@ module cplsat
   integer, save :: nvarto(nbcpmx)
   !> Absolute time value after the mesh starts to rotate (if it does),
   !> for previous calculation
-  double precision, save :: ttpmob
+  double precision, save :: ttpmob = 0.d0
   !> Current absolute time after the mesh starts to rotate (if it does).
   !> In case of restart, this is equal to ttpmob + additional computed time.
-  double precision, save :: ttcmob
+  double precision, save :: ttcmob = 0.d0
   !> \}
 
   !=============================================================================

src/base/cs_control.c
@@ -1245,7 +1245,7 @@ _control_checkpoint(const char   *cur_line,
   else if (strncmp(*s, "time_step_interval ", 19) == 0) {
     int nt;
     if (_read_next_int(cur_line, s, &nt) > 0) {
-      cs_restart_checkpoint_set_defaults(nt, -1., -1.);
+      cs_restart_checkpoint_set_interval(nt, -1., -1.);
       bft_printf("  %-32s %12d\n",
                  "checkpoint_time_step_interval", nt);
     }
@@ -1254,7 +1254,7 @@ _control_checkpoint(const char   *cur_line,
     double t;
     if (_read_next_double(true, cur_line, s, &t) > 0) {
       if (t > 0) {
-        cs_restart_checkpoint_set_defaults(-1, t, -1.);
+        cs_restart_checkpoint_set_interval(-1, t, -1.);
         bft_printf("  %-32s %12.5g\n",
                    "checkpoint_time_value_interval", t);
       }
@@ -1267,7 +1267,7 @@ _control_checkpoint(const char   *cur_line,
     double wt;
     if (_read_next_double(true, cur_line, s, &wt) > 0) {
       if (wt > 0) {
-        cs_restart_checkpoint_set_defaults(-1, -1., wt);
+        cs_restart_checkpoint_set_interval(-1, -1., wt);
         bft_printf("  %-32s %12.5g\n",
                    "checkpoint_wall_time_interval", wt);
       }

commit 520508863f6a028c5085fd20abcb0cc25f0c207e

Fix mapping of some turbumence model constants, broken by commit ba8ed0ae.

src/turb/cs_turbulence_model.c
@@ -1081,7 +1081,6 @@ cs_f_turb_model_constants_get_pointers(double  **apow,
                                        double  **crij2,
                                        double  **crij3,
                                        double  **csmago,
-                                       double  **cwale,
                                        double  **xlesfd,
                                        double  **xlesfl,
                                        double  **ales,
@@ -1255,7 +1254,6 @@ cs_f_turb_model_constants_get_pointers(double  **apow,
                                        double  **crij2,
                                        double  **crij3,
                                        double  **csmago,
-                                       double  **cwale,
                                        double  **xlesfd,
                                        double  **xlesfl,
                                        double  **ales,
@@ -1273,7 +1271,6 @@ cs_f_turb_model_constants_get_pointers(double  **apow,
   *crij3 = &cs_turb_crij3;
   *csmago= &cs_turb_csmago;
   *csmago= &cs_turb_csmago;
-  *cwale = &cs_turb_cwale;
   *xlesfd= &cs_turb_xlesfd;
   *xlesfl= &cs_turb_xlesfl;
   *ales  = &cs_turb_ales;