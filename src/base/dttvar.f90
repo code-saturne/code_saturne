code-saturne/code_saturne

commit 46a3fd7aa1f6ced03a1f323667d47f796b986044

Improve handling of time update in case of coupling.
This is partly based on merge request #1 by C. Koren, with some changes:
- The cdoe was not working in coupled cases, because the cplsync
  call was in a section for idtvar 1 or 2 only.
- For now, we keep separate synchronisation and time update call sites
  for the idtvar = 1 and other cases, so that we do not risk changing
  the parallization aspects of the coupled applications (i.e. who waits
  for who and when) without more detailed analysis/timings.
- This also reduces the risks of side effects of the change, so as to allow
  merging it into v8.0.
- In the idtvar = 1 case, the time step update is done immediately, in
  dttvar, so that dttvar handles all the time stepping aspects in that case.
- The turbomachinery rotation update was improved, so to be more self-
  contained in the turbomachinery module, and not to require the
  "ttcmob"/"ttpmob" variables, which were mostly redundant with values
  stored directly in the turbomachinery structure. The mobile times are
  updated based on the current time step only when first needed, that is
  when calling cs_turbomachinery_update_mesh.

src/base/dttvar.f90
@@ -27,7 +27,11 @@
 
 !> \file dttvar.f90
 !> \brief Compute the local time step and add the Courant and Fourier number to
-!the log.
+!>        the log.
+!>
+!> When using an adaptive time step (idtvar = 1), coupling syncronization and
+!> update (incrementation) of the time step is done in the function (rather
+!> than earlier) as soon as the required information is available.
 !>
 !> This function has access to the boundary face type, except for the first time
 !> step.
@@ -207,7 +211,6 @@ call field_get_val_s(ibrom, brom)
 if (ippmod(icompf).ge.0) then
 
   call cfdttv                                                   &
-  !==========
  ( nvar   , nscal  , ncepdp , ncesmp ,                          &
    icepdc , icetsm , itypsm ,                                   &
    dt     ,                                                     &
@@ -221,21 +224,23 @@ endif
 !===============================================================================
 
 if (vcopt_u%idiff.ge.1) then
+
   do iel = 1, ncel
     w1(iel) = viscl(iel) + vcopt_u%idifft*visct(iel)
   enddo
-
   imvisp = vcopt_u%imvisf
 
   call viscfa(imvisp, w1, viscf, viscb)
 
 else
+
   do ifac = 1, nfac
     viscf(ifac) = 0.d0
   enddo
   do ifac = 1, nfabor
     viscb(ifac) = 0.d0
   enddo
+
 endif
 
 !===============================================================================
@@ -248,8 +253,8 @@ if (idtvar.ge.0) then
 
     if (bmasfl(ifac).lt.0.d0) then
       iel = ifabor(ifac)
-      hint = vcopt_u%idiff*(  viscl(iel)                         &
-                        + vcopt_u%idifft*visct(iel))/distb(ifac)
+      hint = vcopt_u%idiff*(  viscl(iel)                              &
+                            + vcopt_u%idifft*visct(iel))/distb(ifac)
       coefbt(ifac) = 0.d0
       cofbft(ifac) = hint
     else
@@ -349,9 +354,8 @@ if (idtvar.ge.0) then
       ! Matrice a priori non symetrique
       isym = 2
 
-      call matrdt &
-        ( vcopt_u%iconv, idiff0, isym, coefbt, cofbft, imasfl, bmasfl, viscf, &
-        viscb, dam)
+      call matrdt(vcopt_u%iconv, idiff0, isym, coefbt, cofbft,   &
+                  imasfl, bmasfl, viscf, viscb, dam)
 
       if (ivofmt.eq.0) then
         do iel = 1, ncel
@@ -401,15 +405,12 @@ if (idtvar.ge.0) then
       ! Matrice a priori symetrique
       isym = 1
 
-      call matrdt &
-      !==========
- (iconv0, vcopt_u%idiff, isym, coefbt, cofbft, imasfl, bmasfl, &
-  viscf, viscb, dam)
+      call matrdt(iconv0, vcopt_u%idiff, isym, coefbt, cofbft,   &
+                  imasfl, bmasfl, viscf, viscb, dam)
 
       do iel = 1, ncel
         w2(iel) = dam(iel)/(crom(iel)*volume(iel))
       enddo
-
 
       ! ---> CALCUL DE W2     = PAS DE TEMPS VARIABLE VERIFIANT
       !       LE NOMBRE DE         FOURIER MAXIMUM PRESCRIT PAR L'UTILISATEUR
@@ -427,7 +428,6 @@ if (idtvar.ge.0) then
         enddo
         if (irangp.ge.0) then
           call parmin (w2min)
-          !==========
         endif
         do iel = 1, ncel
           w2(iel) = w2min
@@ -509,7 +509,6 @@ if (idtvar.ge.0) then
       endif
     enddo
 
-
     ! 4.1.6 ON LIMITE PAR LE PAS DE TEMPS "THERMIQUE" MAX
     ! =================================================
     !     DTTMAX = W3 = 1/SQRT(MAX(0+,gradRO.g/RO)
@@ -545,7 +544,6 @@ if (idtvar.ge.0) then
         enddo
         if (irangp.ge.0) then
           call parmin (w3min)
-          !==========
         endif
 
         do iel = 1, ncel
@@ -556,7 +554,7 @@ if (idtvar.ge.0) then
     endif
 
     ! 4.1.7 ON CLIPPE LE PAS DE TEMPS PAR RAPPORT A DTMIN ET DTMAX
-    ! ==========================================================
+    ! ============================================================
 
     icfmin(1) = 0
     icfmax(1) = 0
@@ -576,16 +574,12 @@ if (idtvar.ge.0) then
       endif
 
       ntcam1 = ntcabs - 1
-      call cplsyn (ntmabs, ntcam1, dtloc)
+      call cplsyn(ntmabs, ntcam1, dtloc)
 
       call log_iteration_clipping_field(flid, icfmin(1), icfmax(1),    &
                                         dt, dt, icfmin(1), icfmax(1))
 
-      ttcabs = ttcabs + (dtloc - dt(1))
-      if (iturbo.eq.2) then
-        ttcmob = ttcmob + (dtloc - dt(1))
-      endif
-
+      call cs_time_step_increment(dtloc)
       do iel = 1, ncel
         dt(iel) = dtloc
       enddo
@@ -620,9 +614,7 @@ if (idtvar.ge.0) then
     if (iwarnp.ge.2) then
       if (irangp.ge.0) then
         call parcpt (icfmin(1))
-        !==========
         call parcpt (icfmax(1))
-        !==========
       endif
       write (nfecra,1003) icfmin(1),dtmin,icfmax(1),dtmax
     endif
@@ -715,17 +707,15 @@ if (idtvar.ge.0) then
 !===============================================================================
 ! 5.   ALGORITHME STATIONNAIRE
 !===============================================================================
-else
+
+else ! if idtvar < 0
 
   isym = 1
   if (vcopt_u%iconv.gt.0) isym = 2
 
-  call matrdt &
-  !==========
- ( vcopt_u%iconv, vcopt_u%idiff, isym,                          &
-   coefbt, cofbft,                                              &
-   imasfl, bmasfl,                                              &
-   viscf, viscb, dt )
+  call matrdt(vcopt_u%iconv, vcopt_u%idiff, isym,               &
+              coefbt, cofbft, imasfl, bmasfl,                   &
+              viscf, viscb, dt)
 
   do iel = 1, ncel
     dt(iel) =  vcopt_u%relaxv*crom(iel)*volume(iel)/max(dt(iel),epzero)
@@ -960,9 +950,8 @@ if (vcopt_u%iconv.ge.1 .and. icour.ge.1) then
   ! Non symmetric matrix
   isym = 2
 
-  call matrdt &
-    (vcopt_u%iconv, idiff0, isym, coefbt, cofbft, imasfl, bmasfl, &
-    viscf, viscb, dam)
+  call matrdt(vcopt_u%iconv, idiff0, isym, coefbt, cofbft, imasfl, bmasfl, &
+              viscf, viscb, dam)
 
   ! Compute min and max Courant numbers
   cfmax = -grand
@@ -1024,15 +1013,13 @@ if (vcopt_u%idiff.ge.1 .and. ifour.ge.1) then
   iconv0 = 0
   cnom = ' FOURIER'
 
-  !                      2
   ! Build matrix +2.NU/DX (Fourier)=w1
 
   ! Symmetric matrix
   isym = 1
 
-  call matrdt &
-    (iconv0, vcopt_u%idiff, isym, coefbt, cofbft, imasfl, bmasfl, &
-    viscf, viscb, dam)
+  call matrdt(iconv0, vcopt_u%idiff, isym, coefbt, cofbft, imasfl, bmasfl, &
+              viscf, viscb, dam)
 
   ! Compute min/max of Fourier number
   cfmax  = -grand
@@ -1096,9 +1083,8 @@ if ((vcopt_u%idiff.ge.1.or.vcopt_u%iconv.ge.1) &
   isym = 1
   if (vcopt_u%iconv.gt.0) isym = 2
 
-  call matrdt &
-    (vcopt_u%iconv, vcopt_u%idiff, isym, coefbt, cofbft, imasfl, bmasfl, &
-    viscf, viscb, dam)
+  call matrdt(vcopt_u%iconv, vcopt_u%idiff, isym, coefbt, cofbft,   &
+              imasfl, bmasfl, viscf, viscb, dam)
 
   ! Compute Courant/Fourier min and max
   cfmax  = -grand

src/base/caltri.f90
@@ -96,7 +96,7 @@ integer          itrale , ntmsav
 integer          iterns
 integer          stats_id, restart_stats_id, lagr_stats_id, post_stats_id
 
-double precision titer1, titer2
+double precision titer1, titer2, dtcpl
 
 integer          ivoid(1)
 
@@ -177,15 +177,6 @@ interface
     use, intrinsic :: iso_c_binding
     implicit none
   end subroutine cs_post_default_write_variables
-
-  !=============================================================================
-
-  subroutine cs_time_step_increment(dt) &
-    bind(C, name='cs_time_step_increment')
-    use, intrinsic :: iso_c_binding
-    implicit none
-    real(kind=c_double), value :: dt
-  end subroutine cs_time_step_increment
 
   !=============================================================================
 
@@ -795,8 +786,6 @@ write(nfecra,2000)
 ntcabs = ntpabs
 ttcabs = ttpabs
 
-if (iturbo.eq.2)  ttcmob = ttpmob
-
 write(nfecra,3000)
 
 !     Nb d'iter ALE (nb relatif a l'execution en cours)
@@ -815,10 +804,18 @@ endif
 
 if (itrale.gt.0) then
 
-  if (idtvar.eq.0) then
-    call cplsyn (ntmabs, ntcabs, dt(1))
-  else if (idtvar.ne.1) then ! synchronization in dttvar if idtvar = 1
-    call cplsyn (ntmabs, ntcabs, dtref)
+  ! Synchronization in dttvar if idtvar = 1
+  ! (i.e. keep coupled codes waiting until time step is computed
+  ! only when needed).
+  ! In case the coupling modifies the reference time step, make sure
+  ! the matching field is updated. Do not do this after initialization.
+  ! except for the adaptive time step (idtvar = 1), handled in dttvar.
+
+  if (idtvar.ne.1) then
+    call cplsyn(ntmabs, ntcabs, dtref)
+    do iel = 1, ncelet
+      dt(iel) = dtref
+    end do
   endif
 
   if (ntmabs .eq. ntcabs .and. ntmabs.gt.ntpabs) then
@@ -848,17 +845,9 @@ endif
 
 if (itrale.gt.0 .and. ntmabs.gt.ntpabs) then
   call timer_stats_increment_time_step
-  if (idtvar.eq.0.or.idtvar.eq.1) then
-    call cs_time_step_increment(dt(1))
-  else
+  ! Time step computed in dttvar if idtvar = 1.
+  if (idtvar.ne.1) then
     call cs_time_step_increment(dtref)
-  endif
-  if (iturbo.eq.2) then
-    if(idtvar.eq.0.or.idtvar.eq.1) then
-      ttcmob = ttcmob + dt(1)
-    else
-      ttcmob = ttcmob + dtref
-    endif
   endif
 endif
 
@@ -990,10 +979,9 @@ call armtps(ntcabs,ntmabs)
 
 ! Stop test for couplings
 
-if (idtvar.eq.0) then
-  call cplsyn (ntmabs, ntcabs, dt(1))
-else if (idtvar.ne.1) then ! synchronization in dttvar if idtvar = 1
-  call cplsyn (ntmabs, ntcabs, dtref)
+if (idtvar.ne.1) then ! synchronization in dttvar if idtvar = 1
+  dtcpl = dtref
+  call cplsyn (ntmabs, ntcabs, dtcpl)
 endif
 
 !===============================================================================
@@ -1131,11 +1119,11 @@ itrale = itrale + 1
 
 if (ntcabs.lt.ntmabs) goto 100
 
-! Final synchronization for variable time step
+! Final synchronization for time step.
+! This is done after exiting the main time loop, hence telling other codes
+! that code_saturne is finished.
 
-if (idtvar.eq.1) then
-  call cplsyn (ntmabs, ntcabs, dtref)
-endif
+call cplsyn (ntmabs, ntcabs, dtcpl)
 
 ! LIBERATION DES TABLEAUX INTERMEDIAIRES (PDC+TSM)
 

src/base/cplsat.f90
@@ -56,12 +56,6 @@ module cplsat
   integer, save :: nvarcp(nbcpmx)
   !> size of exchange tables
   integer, save :: nvarto(nbcpmx)
-  !> Absolute time value after the mesh starts to rotate (if it does),
-  !> for previous calculation
-  double precision, save :: ttpmob = 0.d0
-  !> Current absolute time after the mesh starts to rotate (if it does).
-  !> In case of restart, this is equal to ttpmob + additional computed time.
-  double precision, save :: ttcmob = 0.d0
   !> \}
 
   !=============================================================================

src/base/cs_c_bindings.f90
@@ -1862,6 +1862,17 @@ module cs_c_bindings
 
     !---------------------------------------------------------------------------
 
+    ! Interface to C function incrementing time step
+
+    subroutine cs_time_step_increment(dt) &
+      bind(C, name='cs_time_step_increment')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      real(kind=c_double), value :: dt
+    end subroutine cs_time_step_increment
+
+    !---------------------------------------------------------------------------
+
     ! Interface to C function defining statistic based on its name.
 
     function cs_timer_stats_create(parent_name, name, label) result(id) &

src/base/cs_restart.c
@@ -1812,7 +1812,7 @@ void CS_PROCF (trbsui, TRBSUI)
  void
 )
 {
-  cs_mesh_save(cs_glob_mesh, NULL, "checkpoint", "mesh");
+  cs_mesh_save(cs_glob_mesh, NULL, "checkpoint", "mesh.csm");
 }
 
 /*----------------------------------------------------------------------------

src/base/cs_turbomachinery.c
@@ -111,11 +111,14 @@ typedef struct {
   char                     **rotor_cells_c;     /* rotor cells selection
                                                    criteria (for each rotor) */
 
+  int                        nt_cur;            /* current time step */
   int                        n_max_join_tries;  /* maximum number of tries
                                                    for joining differences */
   double                     dt_retry;          /* time shift multiplier for
                                                    retry position */
   double                     t_cur;             /* current time for update */
+  double                     t_cur_kc;          /* Kahan compensation for
+                                                   current time for update */
 
   cs_mesh_t                 *reference_mesh;    /* reference mesh (before
                                                    rotation and joining) */
@@ -148,6 +151,29 @@ void cs_f_map_turbomachinery_rotor(int  **irotce);
 /*============================================================================
  * Private function definitions
  *============================================================================*/
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Update Kahan compensation.
+ *
+ * Called as a function to help avoid compiler optimizating Kahan
+ * summation out (though aggressive or LTO optimizations might).
+ *
+ * new compensation term is (a - b) - c
+ *
+ * \param[in]  a  a
+ * \param[in]  b  b
+ * \param[in]  c  c
+ */
+/*----------------------------------------------------------------------------*/
+
+static void
+_update_kahan_compensation(double  *a,
+                           double  *b,
+                           double  *c)
+{
+  _turbomachinery->t_cur_kc = (*a - *b) - *c;
+}
 
 /*----------------------------------------------------------------------------
  * Function for selection of faces with joining changes.
@@ -275,8 +301,10 @@ _turbomachinery_create(void)
     r->invariant[i] = 0;
   }
 
+  tbm->nt_cur = 0;
   tbm->n_max_join_tries = 5;
   tbm->t_cur = 0;
+  tbm->t_cur_kc = 0;
   tbm->dt_retry = 1e-2;
 
   tbm->reference_mesh = cs_mesh_create();
@@ -613,20 +641,36 @@ _copy_mesh(const cs_mesh_t  *mesh,
  *----------------------------------------------------------------------------*/
 
 static void
-_update_angle(cs_real_t  t)
+_update_angle(const cs_time_step_t  *ts)
 {
   cs_turbomachinery_t *tbm = _turbomachinery;
 
   /* Now update coordinates */
 
-  double dt = t - tbm->t_cur;
+  if (tbm->nt_cur < ts->nt_cur) {
 
-  if (dt > 0) {
-    for (int j = 0; j < tbm->n_rotors+1; j++) {
-      cs_rotation_t *r = tbm->rotation + j;
-      r->angle += r->omega * dt;
+    tbm->nt_cur = ts->nt_cur;
+
+    if (ts->nt_cur > ts->nt_prev) {
+
+      double dt = ts->dt[0];
+
+      for (int j = 0; j < tbm->n_rotors+1; j++) {
+        cs_rotation_t *r = tbm->rotation + j;
+        r->angle += r->omega * dt;
+        /* To avoid degrading numerical precision after many rotations,
+           keep angle value in [0, 2pi] range since it is periodic. */
+        r->angle = fmod(r->angle, atan(1.)*8.);
+      }
+
+      double z = dt - tbm->t_cur_kc;
+      double t = tbm->t_cur + z;
+
+      _update_kahan_compensation(&t, &(tbm->t_cur), &z);
+
+      tbm->t_cur = t;
     }
-    tbm->t_cur = t;
+
   }
 }
 
@@ -807,14 +851,12 @@ _select_rotor_cells(cs_turbomachinery_t  *tbm)
  *
  * parameters:
  *   restart_mode  true for restart, false otherwise
- *   t_cur_mob     current rotor time
  *   t_elapsed     elapsed computation time
  */
 /*----------------------------------------------------------------------------*/
 
 static void
-_update_mesh_coupling(double   t_cur_mob,
-                      double  *t_elapsed)
+_update_mesh_coupling(double  *t_elapsed)
 {
   double  t_start, t_end;
 
@@ -836,7 +878,7 @@ _update_mesh_coupling(double   t_cur_mob,
 
   /* Update geometry, if necessary */
 
-  _update_angle(t_cur_mob);
+  _update_angle(cs_glob_time_step);
 
   if (tbm->n_rotors > 0)
     _update_geometry(cs_glob_mesh, 0);
@@ -859,14 +901,12 @@ _update_mesh_coupling(double   t_cur_mob,
  *
  * parameters:
  *   restart_mode  true for restart, false otherwise
- *   t_cur_mob     current rotor time
  *   t_elapsed     elapsed computation time
  */
 /*----------------------------------------------------------------------------*/
 
 static void
 _update_mesh(bool     restart_mode,
-             double   t_cur_mob,
              double  *t_elapsed)
 {
   double  t_start, t_end;
@@ -886,8 +926,7 @@ _update_mesh(bool     restart_mode,
   /* In case of simple coupling, use simpler update */
 
   if (cs_glob_n_joinings < 1) {
-    _update_mesh_coupling(t_cur_mob,
-                          t_elapsed);
+    _update_mesh_coupling(t_elapsed);
     return;
   }
 
@@ -912,7 +951,7 @@ _update_mesh(bool     restart_mode,
   cs_glob_mesh->verbosity = 0;
   cs_glob_mesh_builder = cs_mesh_builder_create();
 
-  _update_angle(t_cur_mob);
+  _update_angle(cs_glob_time_step);
 
   if (restart_mode == false) {
 
@@ -1030,7 +1069,10 @@ _update_mesh(bool     restart_mode,
     cs_mesh_to_builder_partition(tbm->reference_mesh,
                                  cs_glob_mesh_builder);
 
-    cs_preprocessor_data_add_file("restart/mesh", 0, NULL, NULL);
+    if (cs_file_isreg("restart/mesh.csm"))
+      cs_preprocessor_data_add_file("restart/mesh.csm", 0, NULL, NULL);
+    else
+      cs_preprocessor_data_add_file("restart/mesh", 0, NULL, NULL);
 
     cs_preprocessor_data_read_headers(cs_glob_mesh,
                                       cs_glob_mesh_builder,
@@ -1500,16 +1542,14 @@ cs_turbomachinery_coupling_add(const char  *sel_criteria,
 /*!
  * \brief Update mesh for unsteady rotor/stator computation.
  *
- * \param[in]   t_cur_mob     current rotor time
  * \param[out]  t_elapsed     elapsed computation time
  */
 /*----------------------------------------------------------------------------*/
 
 void
-cs_turbomachinery_update_mesh(double   t_cur_mob,
-                              double  *t_elapsed)
+cs_turbomachinery_update_mesh(double  *t_elapsed)
 {
-  _update_mesh(false, t_cur_mob, t_elapsed);
+  _update_mesh(false, t_elapsed);
 }
 
 /*----------------------------------------------------------------------------*/
@@ -1528,10 +1568,10 @@ cs_turbomachinery_restart_mesh(void)
 
   if (cs_glob_time_step->nt_prev > 0) {
     double t_elapsed;
-    if (cs_file_isreg("restart/mesh"))
-      _update_mesh(true, cs_glob_time_step->t_cur, &t_elapsed);
+    if (cs_file_isreg("restart/mesh") || cs_file_isreg("restart/mesh.csm"))
+      _update_mesh(true, &t_elapsed);
     else
-      _update_mesh(false, cs_glob_time_step->t_cur, &t_elapsed);
+      _update_mesh(false, &t_elapsed);
   }
 }
 
@@ -1608,7 +1648,7 @@ cs_turbomachinery_initialize(void)
 
   if (cs_glob_n_joinings > 0) {
     cs_real_t t_elapsed;
-    cs_turbomachinery_update_mesh(0.0, &t_elapsed);
+    cs_turbomachinery_update_mesh(&t_elapsed);
   }
 
   /* Adapt postprocessing options if required;
@@ -2017,6 +2057,7 @@ cs_turbomachinery_restart_read(cs_restart_t  *r)
                                         t_angle);
 
   if (retcode == CS_RESTART_SUCCESS) {
+    tbm->nt_cur = cs_glob_time_step->nt_prev;
     tbm->t_cur = t_angle[0];
     for (int i = 0; i < tbm->n_rotors+1; i++) {
       cs_rotation_t *rot = tbm->rotation + i;

src/base/cs_turbomachinery.h
@@ -166,13 +166,11 @@ cs_turbomachinery_finalize(void);
  * Update mesh for unsteady rotor/stator computation
  *
  * parameters:
- *   t_cur_mob    <-- current rotor time
- *   t_elapsed    --> elapsed computation time
+ *   t_elapsed --> elapsed computation time
  *----------------------------------------------------------------------------*/
 
 void
-cs_turbomachinery_update_mesh(double   t_cur_mob,
-                              double  *t_elapsed);
+cs_turbomachinery_update_mesh(double  *t_elapsed);
 
 /*----------------------------------------------------------------------------
  * Update mesh for unsteady rotor/stator computation in case of restart.

src/base/ecrava.f90
@@ -239,12 +239,6 @@ nbval  = 1
 ival(1) = ivofmt
 call restart_write_section_int_t(rp,rubriq,itysup,nbval,ival)
 
-rubriq = 'instant_mobile_precedent'
-itysup = 0
-nbval  = 1
-rval(1) = ttcmob
-call restart_write_section_real_t(rp,rubriq,itysup,nbval,rval)
-
 call turbomachinery_restart_write(rp)
 
 if (ichemistry.gt.0.or.iaerosol.ne.CS_ATMO_AEROSOL_OFF) then

commit adaca0dcafd14c2ac311efe475d46bc56a9b8155

Test on time step incrementation should be consistent between idtvar options."
This was broken by commit 46a3fd7a (Feb 4 2023).

src/base/dttvar.f90
@@ -43,6 +43,7 @@
 !______________________________________________________________________________.
 !  mode           name          role                                           !
 !______________________________________________________________________________!
+!> \param[in]     itrale        ALE iteration number
 !> \param[in]     nvar          total number of variables
 !> \param[in]     nscal         total number of scalars
 !> \param[in]     ncepdp        number of cells with head loss terms
@@ -60,7 +61,8 @@
 !_______________________________________________________________________________
 
 subroutine dttvar &
- ( nvar   , nscal  , ncepdp , ncesmp ,                            &
+ ( itrale ,                                                       &
+   nvar   , nscal  , ncepdp , ncesmp ,                            &
    iwarnp ,                                                       &
    icepdc , icetsm , itypsm ,                                     &
    dt     ,                                                       &
@@ -94,6 +96,7 @@ implicit none
 
 ! Arguments
 
+integer          itrale
 integer          nvar   , nscal
 integer          ncepdp , ncesmp
 integer          iwarnp
@@ -579,7 +582,10 @@ if (idtvar.ge.0) then
       call log_iteration_clipping_field(flid, icfmin(1), icfmax(1),    &
                                         dt, dt, icfmin(1), icfmax(1))
 
-      call cs_time_step_increment(dtloc)
+      if (itrale.gt.0 .and. ntmabs.gt.ntpabs) then
+        call cs_time_step_increment(dtloc)
+      endif
+
       do iel = 1, ncel
         dt(iel) = dtloc
       enddo

src/base/tridim.f90
@@ -744,12 +744,8 @@ if (vcopt_u%iwarni.ge.1) then
   write(nfecra,1020)
 endif
 
-call dttvar &
- ( nvar   , nscal  , ncepdc , ncetsm ,                            &
-   vcopt_u%iwarni  ,                                              &
-   icepdc , icetsm , itypsm ,                                     &
-   dt     ,                                                       &
-   ckupdc , smacel )
+call dttvar(itrale, nvar, nscal, ncepdc, ncetsm, vcopt_u%iwarni,  &
+            icepdc, icetsm, itypsm, dt, ckupdc, smacel)
 
 if (nbaste.gt.0.and.itrale.gt.nalinf) then
   ntrela = ntcabs - ntpabs