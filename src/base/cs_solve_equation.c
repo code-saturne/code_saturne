code-saturne/code_saturne

commit 90f947a6f9ec6810a6aab954f25dd5b482a9b41e

Fix bug introduced in commit 872d7d38
Move pointer setting of weighting field outside of the loop to prevent SIGSEV
in PERMEABILITY_GRADIENT verification test case

src/base/cs_solve_equation.c
@@ -502,6 +502,21 @@ _diffusion_terms(cs_real_t                   w1[],
   /* Scalar diffusivity */
   cs_real_t *cpro_wgrec_s = NULL;
   cs_real_6_t *cpro_wgrec_v = NULL;
+
+  /* weighting field for gradient */
+  if (eqp->iwgrec == 1) {
+    const int kwgrec = cs_field_key_id_try("gradient_weighting_id");
+    const int iflwgr = cs_field_get_key_int(f, kwgrec);
+    cs_field_t *f_g = cs_field_by_id(iflwgr);
+    if (f_g->dim > 1)
+      cpro_wgrec_v = (cs_real_6_t *)f_g->val;
+    else
+      cpro_wgrec_s = f_g->val;
+    if (cpro_wgrec_s != NULL)
+      cs_array_real_copy(n_cells, w1, cpro_wgrec_s);
+    cs_halo_sync_var(m->halo, CS_HALO_STANDARD, cpro_wgrec_s);
+  }
+
   if (eqp->idften & CS_ISOTROPIC_DIFFUSION) {
     int idifftp = eqp->idifft;
     if (scalar_turb_flux_model_type == 3)
@@ -516,19 +531,6 @@ _diffusion_terms(cs_real_t                   w1[],
       for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++) {
         w1[c_id] += idifftp*xcpp[c_id]*sgdh_diff[c_id];
       }
-    }
-    /* weighting field for gradient */
-    if (eqp->iwgrec == 1) {
-      const int kwgrec = cs_field_key_id_try("gradient_weighting_id");
-      const int iflwgr = cs_field_get_key_int(f, kwgrec);
-      cs_field_t *f_g = cs_field_by_id(iflwgr);
-      if (f_g->dim > 1)
-        cpro_wgrec_v = (cs_real_6_t *)f_g->val;
-      else
-        cpro_wgrec_s = f_g->val;
-      if (cpro_wgrec_s != NULL)
-        cs_array_real_copy(n_cells, w1, cpro_wgrec_s);
-      cs_halo_sync_var(m->halo, CS_HALO_STANDARD, cpro_wgrec_s);
     }
 
     cs_face_viscosity(m,

commit fcf4ea41456095ed2990c35fda0c6f7b91bd67bb

Fix in cases of gradient weighting for scalars.
This was broken by commit 90f947a6 (Feb 9 2023), in an fix for another
issue in commit 872d7d38 (Feb 6 2023).

A bit of code cleanup is added also.

src/base/cs_solve_equation.c
@@ -195,13 +195,12 @@ _init_sgdh_diff(const cs_field_t *f,
   /* Variable Schmidt number */
   else if (cpro_turb_schmidt != NULL)
     for (cs_lnum_t c_id = 0; c_id < cs_glob_mesh->n_cells; c_id++)
-      sgdh_diff[c_id]
-        = cs_math_fmax(visct[c_id],
-                       cs_math_zero_threshold)/cpro_turb_schmidt[c_id];
+      sgdh_diff[c_id] =   cs_math_fmax(visct[c_id], 0.)
+                        / cpro_turb_schmidt[c_id];
   else
     for (cs_lnum_t c_id = 0; c_id < cs_glob_mesh->n_cells; c_id++)
-      sgdh_diff[c_id] = cs_math_fmax(visct[c_id],
-                                     cs_math_zero_threshold)/turb_schmidt;
+      sgdh_diff[c_id] =   cs_math_fmax(visct[c_id], 0.)
+                        / turb_schmidt;
 }
 
 /*----------------------------------------------------------------------------
@@ -512,10 +511,6 @@ _diffusion_terms(cs_real_t                   w1[],
       cpro_wgrec_v = (cs_real_6_t *)f_g->val;
     else
       cpro_wgrec_s = f_g->val;
-    if (cpro_wgrec_s != NULL) {
-      cs_array_real_copy(n_cells, w1, cpro_wgrec_s);
-      cs_halo_sync_var(m->halo, CS_HALO_STANDARD, cpro_wgrec_s);
-    }
   }
 
   if (eqp->idften & CS_ISOTROPIC_DIFFUSION) {
@@ -527,11 +522,17 @@ _diffusion_terms(cs_real_t                   w1[],
       cs_array_set_value_real(n_cells, 1, visls_0, w1);
     else
       cs_array_real_copy(n_cells, cpro_viscls, w1);
+
     if (idifftp > 0) {
 #     pragma omp parallel for if(n_cells > CS_THR_MIN)
       for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++) {
-        w1[c_id] += idifftp*xcpp[c_id]*sgdh_diff[c_id];
+        w1[c_id] += xcpp[c_id]*sgdh_diff[c_id];
       }
+    }
+
+    if (cpro_wgrec_s != NULL) {
+      cs_array_real_copy(n_cells, w1, cpro_wgrec_s);
+      cs_halo_sync_var(m->halo, CS_HALO_STANDARD, cpro_wgrec_s);
     }
 
     cs_face_viscosity(m,
@@ -541,6 +542,7 @@ _diffusion_terms(cs_real_t                   w1[],
                       viscf,
                       viscb);
   }
+
   /* Symmetric tensor diffusivity (GGDH) */
   else if (eqp->idften & CS_ANISOTROPIC_DIFFUSION) {
     BFT_MALLOC(_weighb, n_b_faces, cs_real_t);
@@ -608,8 +610,8 @@ _diffusion_terms(cs_real_t                   w1[],
       }
     }
 
-    if (eqp->iwgrec == 1) {
-      /* Weighting for gradient */
+    /* Weighting for gradient */
+    if (cpro_wgrec_v != NULL) {
 #     pragma omp parallel for if(n_cells > CS_THR_MIN)
       for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++) {
         for (cs_lnum_t ii = 0; ii < 6; ii++)
@@ -1482,8 +1484,10 @@ cs_solve_equation_scalar(cs_field_t        *f,
   const cs_real_t *cpro_delay = NULL, *cproa_delay = NULL;
 
   if (cs_glob_physical_model_flag[CS_GROUNDWATER] == -1) {
-    for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++)
-      rovsdt[c_id] += eqp->istat*xcpp[c_id]*pcrom[c_id]*cell_f_vol[c_id]/dt[c_id];
+    if (eqp->istat == 1) {
+      for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++)
+        rovsdt[c_id] += xcpp[c_id]*pcrom[c_id]*cell_f_vol[c_id]/dt[c_id];
+    }
   }
 
   /* Darcy : we take into account the porosity and delay for underground transport */
@@ -1833,7 +1837,7 @@ cs_solve_equation_scalar(cs_field_t        *f,
         *cell_f_vol[c_id]*(cvar_var[c_id]-cvara_var[c_id])*ibcl;
     const cs_real_t sclnor = sqrt(cs_gdot(n_cells,smbrs,smbrs));
 
-    bft_printf("%s: EXPLICIT BALANCE = %14.5e\n",f->name, sclnor);
+    bft_printf("%s: EXPLICIT BALANCE = %14.5e\n\n",f->name, sclnor);
   }
 
   /* Log in case of velocity/pressure inner iterations */