code-saturne/code_saturne

commit fa440fcbd3af59fd84b803f896b3f510105241a3

Clean up porosity module using mesh quantities. Prepare for new balance formulation.
git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@7004 e44c5d27-f830-0410-8ce8-974f7a764be6

src/base/mesh.f90
@@ -123,6 +123,17 @@ module mesh
   !> and it is oriented outwards
   double precision, dimension(:,:), pointer :: surfbo
 
+  !> \anchor suffac
+  !> fluid surface vector of the internal faces. Its norm is the surface of the face
+  !> and it is oriented from \c ifacel(1,.) to \c ifacel(2,.)
+  double precision, dimension(:,:), pointer :: suffac
+
+  !> \anchor suffbo
+  !> surface vector of the boundary faces. Its norm is the surface of the face
+  !> and it is oriented outwards
+  double precision, dimension(:,:), pointer :: suffbo
+
+
   !> \anchor cdgfac
   !> coordinates of the centers of the internal faces
   double precision, dimension(:,:), pointer :: cdgfac
@@ -138,6 +149,10 @@ module mesh
   !> \anchor volume
   !> volume of each cell
   double precision, dimension(:), pointer :: volume
+
+  !> \anchor volf
+  !> fluid volume of each cell
+  double precision, dimension(:), pointer :: volf
 
   !> \anchor surfan
   !> norm of the surface vector of the internal faces

src/base/caltri.f90
@@ -99,6 +99,7 @@ integer          iiii
 
 integer          modhis, iappel, modntl, iisuit, iwarn0
 integer          ivar
+integer          iel
 
 integer          inod   , idim
 integer          itrale , ntmsav
@@ -118,6 +119,7 @@ character        ficsui*32
 integer, allocatable, dimension(:) :: isostd
 
 double precision, pointer, dimension(:)   :: dt => null()
+double precision, pointer, dimension(:)   :: porosi => null()
 double precision, pointer, dimension(:,:) :: propce => null()
 
 double precision, pointer, dimension(:,:) :: frcxt => null()
@@ -514,6 +516,17 @@ if (iporos.ge.1) then
     call uiporo(ncelet, iporos)
   endif
   call usporo
+
+  call field_get_val_s(ipori, porosi)
+
+  do iel = 1, ncel
+    volf(iel) = volume(iel) * porosi(iel)
+  enddo
+
+  if (irangp.ge.0.or.iperio.eq.1) then
+    call synsca(volf)
+  endif
+
 endif
 
 !===============================================================================

src/base/catsmv.f90
@@ -42,7 +42,7 @@
 !> \param[in]     icetsm        source mass cells pointer
 !> \param[in]     itpsmp        mass source type for the working variable
 !>                              (cf. \ref cs_user_mass_source_terms)
-!> \param[in]     volume        cells volume
+!> \param[in]     volf          cells fluid volume
 !> \param[in]     vela          variable value at time step beginning
 !> \param[in]     smcelv        value of the variable associated to the mass
 !>                              source; NOT INTERLEAVED
@@ -58,7 +58,7 @@ subroutine catsmv &
  ( ncelet , ncel   , ncesmp , iterns , isnexp ,                   &
    thetv  ,                                                       &
    icetsm , itpsmp ,                                              &
-   volume , vela   , smcelv , gamma  ,                            &
+   volf   , vela   , smcelv , gamma  ,                            &
    tsexpv , tsimpv , gavinj )
 
 !===============================================================================
@@ -72,7 +72,7 @@ implicit none
 integer          ncelet, ncel  , ncesmp, iterns, isnexp
 integer          icetsm(ncesmp), itpsmp(ncesmp)
 double precision thetv
-double precision volume(ncelet)
+double precision volf(ncelet)
 double precision vela  (3,ncelet)
 double precision smcelv(ncesmp,3), gamma (ncesmp)
 double precision tsexpv(3,ncelet), tsimpv(3,3,ncelet), gavinj(3,ncelet)
@@ -107,8 +107,8 @@ if(iterns.eq.1) then
     iel = icetsm(ii)
     if (gamma(ii).gt.0.d0 .and. itpsmp(ii).eq.1) then
       do isou = 1, 3
-        tsexpv(isou,iel) = tsexpv(isou,iel)-volume(iel)*gamma(ii)*vela(isou,iel)
-        gavinj(isou,iel) = volume(iel)*gamma(ii) * smcelv(ii,isou)
+        tsexpv(isou,iel) = tsexpv(isou,iel)-volf(iel)*gamma(ii)*vela(isou,iel)
+        gavinj(isou,iel) = volf(iel)*gamma(ii) * smcelv(ii,isou)
       enddo
     endif
   enddo
@@ -120,7 +120,7 @@ if(isnexp.gt.0) then
     iel = icetsm(ii)
     if (gamma(ii).gt.0.d0 .and. itpsmp(ii).eq.1) then
       do isou = 1, 3
-        tsimpv(isou,isou,iel) = tsimpv(isou,isou,iel)+volume(iel)*gamma(ii)*thetv
+        tsimpv(isou,isou,iel) = tsimpv(isou,isou,iel)+volf(iel)*gamma(ii)*thetv
       enddo
     endif
   enddo
@@ -129,7 +129,7 @@ else
     iel = icetsm(ii)
     if (gamma(ii).gt.0.d0 .and. itpsmp(ii).eq.1) then
       do isou = 1, 3
-        tsimpv(isou,isou,iel) = tsimpv(isou,isou,iel)+volume(iel)*gamma(ii)
+        tsimpv(isou,isou,iel) = tsimpv(isou,isou,iel)+volf(iel)*gamma(ii)
       enddo
     endif
   enddo

src/base/cs_preprocess.c
@@ -136,10 +136,13 @@ extern void cs_f_majgeo(const cs_int_t     *ncel,
                         const cs_real_t     xyzcen[],
                         const cs_real_t     surfac[],
                         const cs_real_t     surfbo[],
+                        const cs_real_t     suffac[],
+                        const cs_real_t     suffbo[],
                         const cs_real_t     cdgfac[],
                         const cs_real_t     cdgfbo[],
                         const cs_real_t     xyznod[],
                         const cs_real_t     volume[],
+                        const cs_real_t     volf[],
                         const cs_real_t     surfan[],
                         const cs_real_t     surfbn[],
                         const cs_real_t     dist[],
@@ -404,10 +407,13 @@ cs_preprocess_mesh_update_fortran(void)
               mq->cell_cen,
               mq->i_face_normal,
               mq->b_face_normal,
+              mq->i_f_face_normal,
+              mq->b_f_face_normal,
               mq->i_face_cog,
               mq->b_face_cog,
               m->vtx_coord,
               mq->cell_vol,
+              mq->cell_f_vol,
               mq->i_face_surf,
               mq->b_face_surf,
               mq->i_dist,

src/base/csc2ts.f90
@@ -89,7 +89,7 @@ do ipt = 1, ncecpl
 
   ielloc = lcecpl(ipt)
 
-  rovtau = volume(ielloc)*crom(ielloc)/xtau
+  rovtau = volf(ielloc)*crom(ielloc)/xtau
 
   do isou = 1, 3
     xdis = rvcpce(isou,ipt)

src/base/iniusi.f90
@@ -389,6 +389,9 @@ endif
 ! Choose which 3x3 cocg matrixes are computed for gradient algorithms.
 call comcoc(imrgra)
 
+! Choose the porous model
+call compor(iporos)
+
 ! --- Varpos
 call varpos
 !==========

src/base/majgeo.f90
@@ -29,8 +29,10 @@ subroutine majgeo &
    iface2 , ifabo2 , ifmfb2 , ifmce2 ,                            &
    ipnfa2 , nodfa2 , ipnfb2 , nodfb2 , isymp2 ,                   &
    volmn2 , volmx2 , voltt2 ,                                     &
-   xyzce2 , surfa2 , surfb2 , cdgfa2 , cdgfb2 , xyzno2 ,          &
-   volum2 , srfan2 , srfbn2 , dist2  , distb2 , pond2  ,          &
+   xyzce2 , surfa2 , surfb2 , suffa2 , suffb2 ,                   &
+   cdgfa2 , cdgfb2 , xyzno2 ,                                     &
+   volum2 , volf2  , srfan2 , srfbn2 ,                            &
+   dist2  , distb2 , pond2  ,                                     &
    dijpf2 , diipb2 , dofij2 )                                     &
 
  bind(C, name="cs_f_majgeo")
@@ -76,6 +78,8 @@ subroutine majgeo &
 ! xyzce2           ! ra ! <-- ! cell centers                                   !
 ! surfa2           ! ra ! <-- ! interior face normals                          !
 ! surfb2           ! ra ! <-- ! boundary face normals                          !
+! suffa2           ! ra ! <-- ! interior fluid face normals                    !
+! suffb2           ! ra ! <-- ! boundary fluid face normals                    !
 ! cdgfa2           ! ra ! <-- ! interior face centers                          !
 ! cdgfb2           ! ra ! <-- ! boundary face centers                          !
 ! xyzno2           ! ra ! <-- ! vertex coordinates                             !
@@ -134,9 +138,12 @@ double precision :: volmn2, volmx2, voltt2
 
 double precision, dimension(3,ncele2), target :: xyzce2
 double precision, dimension(3,nfac2), target :: surfa2, cdgfa2, dijpf2, dofij2
+double precision, dimension(3,nfac2), target :: suffa2
 double precision, dimension(3,nfabo2), target :: surfb2, cdgfb2, diipb2
+double precision, dimension(3,nfabo2), target :: suffb2
 double precision, dimension(3,nsom2), target :: xyzno2
 double precision, dimension(ncele2), target :: volum2
+double precision, dimension(ncele2), target :: volf2
 double precision, dimension(nfac2), target :: srfan2, dist2, pond2
 double precision, dimension(nfabo2), target :: srfbn2, distb2
 
@@ -209,12 +216,15 @@ isympa => isymp2(1:nfabor)
 
 surfac => surfa2(1:3,1:nfac)
 surfbo => surfb2(1:3,1:nfabor)
+suffac => suffa2(1:3,1:nfac)
+suffbo => suffb2(1:3,1:nfabor)
 cdgfac => cdgfa2(1:3,1:nfac)
 cdgfbo => cdgfb2(1:3,1:nfabor)
 
 xyznod => xyzno2(1:3,1:nnod)
 
 volume => volum2(1:ncelet)
+volf => volf2(1:ncelet)
 
 surfan => srfan2(1:nfac)
 surfbn => srfbn2(1:nfabor)

src/base/predvv.f90
@@ -224,9 +224,6 @@ double precision, dimension(:), allocatable :: xinvro
 double precision, dimension(:), pointer :: brom, crom, croma, pcrom
 double precision, dimension(:), pointer :: coefa_k, coefb_k
 double precision, dimension(:), pointer :: coefa_p, coefb_p
-double precision, dimension(:), pointer :: porosi
-double precision, dimension(:), pointer :: volf
-double precision, allocatable, dimension(:), target :: xvolf
 double precision, dimension(:,:), allocatable :: rij
 double precision, dimension(:), pointer :: coef1, coef2, coef3, coef4, coef5, coef6
 double precision, dimension(:,:), allocatable :: coefat
@@ -301,29 +298,6 @@ else
     call field_get_val_prev_s(ivarfl(ir23), cvara_r23)
     call field_get_val_prev_s(ivarfl(ir13), cvara_r13)
   endif
-endif
-
-! Compute the Volume of fluid (in case of porosity)
-if (iporos.eq.0)  then
-  volf => volume(:)
-
-! With porosity
-else
-
-  call field_get_val_s(ipori, porosi)
-
-  allocate(xvolf(ncelet))
-
-  do iel = 1, ncel
-    xvolf(iel) = volume(iel) * porosi(iel)
-  enddo
-
-  if (irangp.ge.0.or.iperio.eq.1) then
-    call synsca(xvolf)
-  endif
-
-  volf => xvolf(:)
-
 endif
 
 if (ineedf.eq.1 .and. iterns.eq.1) call field_get_val_v(iforbr, forbr)
@@ -873,15 +847,6 @@ if ((ncepdp.gt.0).and.(iphydr.eq.0)) then
 
     call tspdcv(ncepdp, icepdc, vela, ckupdc, trav)
 
-    ! With porosity
-    if (iporos.ge.1) then
-      do iel = 1, ncel
-        trav(1, iel) = trav(1, iel)*porosi(iel)
-        trav(2, iel) = trav(2, iel)*porosi(iel)
-        trav(3, iel) = trav(3, iel)*porosi(iel)
-      enddo
-    endif
-
     ! Si on itere sur navsto, on utilise TRAVA ; sinon TRAV
     if(nterup.gt.1) then
       do iel = 1, ncel
@@ -1374,7 +1339,7 @@ if (iterns.eq.1) then
 
     do iel = 1, ncel
       !FIXME when using porosity
-      dvol = 1.d0/volume(iel)
+      dvol = 1.d0/volf(iel)
       do isou = 1, 3
         dfrcxt(isou, iel) = dfrcxt(isou, iel) + tsexp(isou, iel)*dvol
       enddo
@@ -1383,15 +1348,6 @@ if (iterns.eq.1) then
     if (irangp.ge.0.or.iperio.eq.1) then
       call synvin(dfrcxt)
     endif
-  endif
-
-  ! Porosity
-  if (iporos.ge.1) then
-    do iel = 1, ncel
-      do isou = 1, 3
-        tsexp(isou, iel) = porosi(iel)*tsexp(isou, iel)
-      enddo
-    enddo
   endif
 
 endif
@@ -1409,54 +1365,26 @@ endif
 
 ! ---> Explicit contribution due to implicit terms
 
-! Without porosity
-if (iporos.eq.0) then
-  if (iterns.eq.1) then
-    if (nterup.gt.1) then
-      do iel = 1, ncel
-        do isou = 1, 3
-          do jsou = 1, 3
-            trava(isou,iel) = trava(isou,iel)                                  &
-                            + tsimp(isou,jsou,iel)*vela(jsou,iel)
-          enddo
+if (iterns.eq.1) then
+  if (nterup.gt.1) then
+    do iel = 1, ncel
+      do isou = 1, 3
+        do jsou = 1, 3
+          trava(isou,iel) = trava(isou,iel)                                  &
+                          + tsimp(isou,jsou,iel)*vela(jsou,iel)
         enddo
       enddo
-    else
-      do iel = 1, ncel
-        do isou = 1, 3
-          do jsou = 1, 3
-            trav(isou,iel) = trav(isou,iel)                                    &
-                           + tsimp(isou,jsou,iel)*vela(jsou,iel)
-          enddo
+    enddo
+  else
+    do iel = 1, ncel
+      do isou = 1, 3
+        do jsou = 1, 3
+          trav(isou,iel) = trav(isou,iel)                                    &
+                         + tsimp(isou,jsou,iel)*vela(jsou,iel)
         enddo
       enddo
-    endif
+    enddo
   endif
-
-! With porosity
-else
-  if (iterns.eq.1) then
-    if (nterup.gt.1) then
-      do iel = 1, ncel
-        do isou = 1, 3
-          do jsou = 1, 3
-            trava(isou,iel) = trava(isou,iel)                                  &
-                            + tsimp(isou,jsou,iel)*vela(jsou,iel)*porosi(iel)
-          enddo
-        enddo
-      enddo
-    else
-      do iel = 1, ncel
-        do isou = 1, 3
-          do jsou = 1, 3
-            trav(isou,iel) = trav(isou,iel)                                    &
-                           + tsimp(isou,jsou,iel)*vela(jsou,iel)*porosi(iel)
-          enddo
-        enddo
-      enddo
-    endif
-  endif
-
 endif
 
 ! At the first PISO iteration, explicit source terms are added
@@ -1551,14 +1479,14 @@ if (ncesmp.gt.0) then
     !==========
   ( ncelet , ncel , ncesmp , iterns , isno2t, thetav(iu),       &
     icetsm , itypsm(1,iu),                                      &
-    volume , vela , smacel(1,iu) ,smacel(1,ipr) ,               &
+    volf   , vela , smacel(1,iu) ,smacel(1,ipr) ,               &
     trav   , fimp , gavinj )
   else
     call catsmv &
     !==========
   ( ncelet , ncel , ncesmp , iterns , isno2t, thetav(iu),       &
     icetsm , itypsm(1,iu),                                      &
-    volume , vela , smacel(1,iu) ,smacel(1,ipr) ,               &
+    volf   , vela , smacel(1,iu) ,smacel(1,ipr) ,               &
     trava  , fimp  , gavinj )
   endif
 
@@ -1953,7 +1881,6 @@ deallocate(tsexp)
 deallocate(tsimp)
 if (allocated(viscce)) deallocate(viscce)
 if (allocated(divt)) deallocate(divt)
-if (allocated(xvolf)) deallocate(xvolf)
 
 !--------
 ! Formats

commit 273d99f4cfb1880337edb00665debd38dcce93d6

Fix error in documentation string for suffbo (introduced in commit fa440fcbd3)

src/base/mesh.f90
@@ -125,7 +125,7 @@ module mesh
   double precision, dimension(:,:), pointer :: suffac
 
   !> \anchor suffbo
-  !> surface vector of the boundary faces. Its norm is the surface of the face
+  !> fluid surface vector of the boundary faces. Its norm is the surface of the face
   !> and it is oriented outwards
   double precision, dimension(:,:), pointer :: suffbo
 