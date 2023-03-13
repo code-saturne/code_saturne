code-saturne/code_saturne

commit 0ba3bb8201e520f6740945a27291fd80abc0590f

Add MEG formulae for enthalpy boundary conditions for NCFD

python/code_saturne/model/BoundaryNeptune.py
@@ -123,6 +123,25 @@ class Boundary(Model) :
             self.boundNode.xmlRemoveNode()
 
 
+    @Variables.noUndo
+    def getDefaultEnthalpyFormula(self, energy_choice='dirichlet'):
+        """
+        Get default enthalpy formula
+        """
+
+        if energy_choice == 'dirichlet_formula':
+            formula = "enthalpy = 50000.0;"
+        elif energy_choice == 'flux_formula':
+            formula = "flux = 1000.;"
+        elif energy_choice in ['temperature_formula', 'timp_K_formula']:
+            formula = "temperature = 293.15;"
+        else:
+            formula = None
+
+        return formula
+
+
+
 #-------------------------------------------------------------------------------
 # InletBoundary class
 #-------------------------------------------------------------------------------
@@ -145,7 +164,7 @@ class InletBoundary(Boundary):
         self.__directionChoices = ['normal', 'formula']
         self.__directionTags = ['direction_formula']
         self.__turbulenceChoices = ['hydraulic_diameter', 'turbulent_intensity', 'formula']
-        self.__enthalpyChoices = ['flux', 'dirichlet', 'timp_K', 'hsat_P']
+        self.__enthalpyChoices = ['flux', 'enthalpy', 'timp_K', 'hsat_P']
 
         # Initialize nodes if necessary
         for field in self.mainFieldsModel.getFieldIdList():
@@ -242,7 +261,7 @@ class InletBoundary(Boundary):
             value = XMLVelocityNode.xmlGetChildDouble(choice)
         elif choice in ('norm_formula', 'flow1_formula'):
             value = XMLVelocityNode.xmlGetChildString(choice)
-        if value is None:
+        if value in [None, ""]:
             value = self.__defaultValues(fieldId)[choice]
             self.setVelocity(fieldId, value)
 
@@ -611,8 +630,6 @@ R12-23 = 5e-05;"""
         choice = XMLEnergyNode['choice']
         if not choice:
             choice = self.__defaultValues(fieldId)['EnthalpyModel']
-            if ThermodynamicsModel(self.case).getMaterials(fieldId) == 'user_material' :
-                choice = 'dirichlet'
             self.setEnthalpyChoice(fieldId, choice)
         return choice
 
@@ -623,7 +640,6 @@ R12-23 = 5e-05;"""
         Set the enthalpy choice for field
         """
         self.mainFieldsModel.isFieldIdValid(fieldId)
-        Model().isInList(value, ['dirichlet','flux','timp_K','hsat_P'])
 
         node = self._XMLBoundaryConditionsNode.xmlGetNode("inlet", field_id = fieldId, label = self._label)
         XMLEnergyNode = node.xmlInitNode('variable', 'choice', name='enthalpy')
@@ -642,17 +658,25 @@ R12-23 = 5e-05;"""
         Get energy value for field
         """
         self.mainFieldsModel.isFieldIdValid(fieldId)
-        node = self._XMLBoundaryConditionsNode.xmlGetNode("inlet", field_id = fieldId, label = self._label)
+        node = self._XMLBoundaryConditionsNode.xmlGetNode("inlet",
+                                                          field_id = fieldId,
+                                                          label = self._label)
         XMLEnergyNode = node.xmlGetChildNode('variable', 'choice', name='enthalpy')
 
-        Model().isInList(XMLEnergyNode['choice'], ['dirichlet','flux','timp_K'])
+        choice = self.getEnthalpyChoice(fieldId)
 
-        Childnode = XMLEnergyNode.xmlGetChildNode('value')
-        if Childnode is None :
-            value = self.__defaultValues(fieldId)['enthalpy']
+        if "formula" == choice[-7:]:
+            value = XMLEnergyNode.xmlGetChildString(choice)
+        else:
+            value = XMLEnergyNode.xmlGetChildDouble('value')
+
+        if value in [None, ""]:
+            if choice[-7:] == 'formula':
+                value = self.getDefaultEnthalpyFormula(choice)
+            else:
+                value = self.__defaultValues(fieldId)['enthalpy']
             self.setEnthalpy(fieldId, value)
 
-        value = XMLEnergyNode.xmlGetChildDouble('value')
         return value
 
 
@@ -662,11 +686,19 @@ R12-23 = 5e-05;"""
         Set energy value for field
         """
         self.mainFieldsModel.isFieldIdValid(fieldId)
-        Model().isFloat(value)
+        _c = self.getEnthalpyChoice(fieldId) \
+                if "_formula" in self.getEnthalpyChoice(fieldId) else 'value'
 
-        node = self._XMLBoundaryConditionsNode.xmlGetNode("inlet", field_id = fieldId, label = self._label)
+        if _c == 'value':
+            Model().isFloat(value)
+
+        node = self._XMLBoundaryConditionsNode.xmlGetNode("inlet",
+                                                          field_id = fieldId,
+                                                          label = self._label)
+
         XMLEnergyNode = node.xmlGetChildNode('variable', 'choice', name='enthalpy')
-        XMLEnergyNode.xmlSetData('value', str(value))
+
+        XMLEnergyNode.xmlSetData(_c, str(value))
 
 
     @Variables.noUndo
@@ -936,7 +968,9 @@ class OutletBoundary(Boundary) :
         Get the enthalpy choice for field
         """
         self.mainFieldsModel.isFieldIdValid(fieldId)
-        node = self._XMLBoundaryConditionsNode.xmlGetNode("outlet", field_id = fieldId, label = self._label)
+        node = self._XMLBoundaryConditionsNode.xmlGetNode("outlet",
+                                                          field_id = fieldId,
+                                                          label = self._label)
         XMLEnergyNode = node.xmlInitNode('variable', 'choice', name='enthalpy')
 
         choice = XMLEnergyNode['choice']
@@ -952,7 +986,6 @@ class OutletBoundary(Boundary) :
         Set the enthalpy choice for field
         """
         self.mainFieldsModel.isFieldIdValid(fieldId)
-        Model().isInList(value, ['dirichlet','flux','timp_K','hsat_P'])
 
         node = self._XMLBoundaryConditionsNode.xmlGetNode("outlet", field_id = fieldId, label = self._label)
         XMLEnergyNode = node.xmlInitNode('variable', 'choice', name='enthalpy')
@@ -971,17 +1004,25 @@ class OutletBoundary(Boundary) :
         Get energy value for field
         """
         self.mainFieldsModel.isFieldIdValid(fieldId)
-        node = self._XMLBoundaryConditionsNode.xmlGetNode("outlet", field_id = fieldId, label = self._label)
+        node = self._XMLBoundaryConditionsNode.xmlGetNode("outlet",
+                                                          field_id = fieldId,
+                                                          label = self._label)
         XMLEnergyNode = node.xmlGetChildNode('variable', 'choice', name='enthalpy')
 
-        Model().isInList(XMLEnergyNode['choice'], ['dirichlet','flux','timp_K'])
+        choice = self.getEnthalpyChoice(fieldId)
 
-        Childnode = XMLEnergyNode.xmlGetChildNode('value')
-        if Childnode is None :
-            value = self.__defaultValues()['enthalpy']
+        if "formula" == choice[-7:]:
+            value = XMLEnergyNode.xmlGetChildString(choice)
+        else:
+            value = XMLEnergyNode.xmlGetChildDouble('value')
+
+        if value in [None, ""]:
+            if choice[-7:] == 'formula':
+                value = self.getDefaultEnthalpyFormula(choice)
+            else:
+                value = self.__defaultValues()['enthalpy']
             self.setEnthalpy(fieldId, value)
 
-        value = XMLEnergyNode.xmlGetChildDouble('value')
         return value
 
 
@@ -991,7 +1032,8 @@ class OutletBoundary(Boundary) :
         Set energy value for field
         """
         self.mainFieldsModel.isFieldIdValid(fieldId)
-        Model().isFloat(value)
+        if '_formula' not in self.getEnthalpyChoice(fieldId):
+            Model().isFloat(value)
 
         node = self._XMLBoundaryConditionsNode.xmlGetNode("outlet", field_id = fieldId, label = self._label)
         XMLEnergyNode = node.xmlGetChildNode('variable', 'choice', name='enthalpy')
@@ -1236,7 +1278,6 @@ class WallBoundary(Boundary) :
         """
         Set the enthalpy choice for field
         """
-        Model().isInList(value, ['temperature','flux', 'syrthes_coupling'])
 
         XMLEnergyNode = self.boundNode.xmlInitNode('variable', 'choice', name='enthalpy')
 
@@ -1250,16 +1291,24 @@ class WallBoundary(Boundary) :
         """
         XMLEnergyNode = self.boundNode.xmlGetChildNode('variable', 'choice', name='enthalpy')
 
-        Model().isInList(XMLEnergyNode['choice'], ['temperature','flux','syrthes_coupling'])
 
         Childnode = XMLEnergyNode.xmlGetChildNode('value')
         if Childnode is None :
             value = self.__defaultValues()['flux']
             self.setEnthalpy(fieldId, value)
 
-        value = XMLEnergyNode.xmlGetChildString('value')
-        if self.getEnthalpyChoice("none") != "syrthes_coupling":
+        choice = self.getEnthalpyChoice("none")
+        value = XMLEnergyNode.xmlGetChildString(choice)
+        if value in [None, ""]:
+            if choice[-7:] == 'formula':
+                value = self.getDefaultEnthalpyFormula(choice)
+            else:
+                value = self.__defaultValues()['flux']
+            self.setEnthalpy('none', value)
+
+        if choice != "syrthes_coupling" and choice[-7:] != 'formula':
             value = float(value)
+
         return value
 
 

python/code_saturne/base/cs_meg_to_c.py
@@ -1738,6 +1738,65 @@ class meg_to_c_interpreter:
                                             sym,
                                             [],
                                             condition=tc)
+                # Thermal conditions
+                if zone.getNature() in ['inlet', 'outlet']:
+                    for fId in mfm.getFieldIdList():
+                        boundary = Boundary(zone.getNature(),
+                                            zone.getLabel(),
+                                            self.case,
+                                            fId)
+
+                        c = boundary.getEnthalpyChoice(fId)
+                        if '_formula' in c:
+                            sym  = ['x', 'y', 'z', 't', 'dt', 'iter', 'surface']
+                            if c == "dirichlet_formula":
+                                req = ['enthalpy']
+                            elif c == "flux_formula":
+                                req = ['flux']
+                            elif c in ['temperature_formula', 'timp_K_formula']:
+                                req = ['temperature']
+
+                            for (name, val) in NotebookModel(self.case).getNotebookList():
+                                sym.append((name, 'value (notebook) = ' + str(val)))
+
+                            exp = boundary.getEnthalpy(fId)
+                            self.init_block('bnd',
+                                            zone.getLabel(),
+                                            'enthalpy_'+str(fId),
+                                            exp,
+                                            req,
+                                            sym,
+                                            [],
+                                            condition=c)
+
+                if zone.getNature() == "wall":
+                    boundary = Boundary(zone.getNature(),
+                                        zone.getLabel(),
+                                        self.case,
+                                        'none')
+                    c = boundary.getEnthalpyChoice('none')
+                    if '_formula' in c:
+                        sym  = ['x', 'y', 'z', 't', 'dt', 'iter', 'surface']
+                        if c == "dirichlet_formula":
+                            req = ['enthalpy']
+                        elif c == "flux_formula":
+                            req = ['flux']
+                        elif c in ['temperature_formula', 'timp_K_formula']:
+                            req = ['temperature']
+
+                        for (name, val) in NotebookModel(self.case).getNotebookList():
+                            sym.append((name, 'value (notebook) = ' + str(val)))
+
+                        exp = boundary.getEnthalpy('none')
+                        self.init_block('bnd',
+                                        zone.getLabel(),
+                                        'enthalpy',
+                                        exp,
+                                        req,
+                                        sym,
+                                        [],
+                                        condition=c)
+
 
     #---------------------------------------------------------------------------
 

python/code_saturne/gui/case/BoundaryConditionsEnergy.ui
@@ -6,8 +6,8 @@
    <rect>
     <x>0</x>
     <y>0</y>
-    <width>515</width>
-    <height>274</height>
+    <width>825</width>
+    <height>584</height>
    </rect>
   </property>
   <property name="windowTitle">
@@ -33,19 +33,6 @@
        <number>9</number>
       </property>
       <item>
-       <spacer>
-        <property name="orientation">
-         <enum>Qt::Horizontal</enum>
-        </property>
-        <property name="sizeHint" stdset="0">
-         <size>
-          <width>40</width>
-          <height>20</height>
-         </size>
-        </property>
-       </spacer>
-      </item>
-      <item>
        <layout class="QHBoxLayout">
         <property name="spacing">
          <number>6</number>
@@ -54,7 +41,37 @@
          <number>0</number>
         </property>
         <item>
-         <widget class="QComboBox" name="comboBoxEnergy"/>
+         <layout class="QVBoxLayout" name="verticalLayout_2">
+          <item>
+           <spacer name="verticalSpacer_4">
+            <property name="orientation">
+             <enum>Qt::Vertical</enum>
+            </property>
+            <property name="sizeHint" stdset="0">
+             <size>
+              <width>20</width>
+              <height>40</height>
+             </size>
+            </property>
+           </spacer>
+          </item>
+          <item>
+           <widget class="QComboBox" name="comboBoxEnergy"/>
+          </item>
+          <item>
+           <spacer name="verticalSpacer_2">
+            <property name="orientation">
+             <enum>Qt::Vertical</enum>
+            </property>
+            <property name="sizeHint" stdset="0">
+             <size>
+              <width>20</width>
+              <height>40</height>
+             </size>
+            </property>
+           </spacer>
+          </item>
+         </layout>
         </item>
         <item>
          <spacer>
@@ -72,19 +89,108 @@
         <item>
          <layout class="QVBoxLayout" name="verticalLayout">
           <item>
-           <widget class="QLineEdit" name="lineEditEnergy"/>
+           <spacer name="verticalSpacer_3">
+            <property name="orientation">
+             <enum>Qt::Vertical</enum>
+            </property>
+            <property name="sizeHint" stdset="0">
+             <size>
+              <width>20</width>
+              <height>40</height>
+             </size>
+            </property>
+           </spacer>
           </item>
           <item>
-           <widget class="QLineEdit" name="lineEditSyrthes"/>
+           <widget class="QGroupBox" name="groupBoxEnergyValue">
+            <layout class="QHBoxLayout" name="boxLayoutEnergyValue">
+             <item>
+              <widget class="QLabel" name="labelValue">
+               <property name="text">
+                <string>Value</string>
+               </property>
+              </widget>
+             </item>
+             <item>
+              <spacer name="horizontalSpacer_2">
+               <property name="orientation">
+                <enum>Qt::Horizontal</enum>
+               </property>
+               <property name="sizeHint" stdset="0">
+                <size>
+                 <width>40</width>
+                 <height>20</height>
+                </size>
+               </property>
+              </spacer>
+             </item>
+             <item>
+              <widget class="QLineEdit" name="lineEditEnergy"/>
+             </item>
+             <item>
+              <widget class="QPushButton" name="pushButtonEnergy">
+               <property name="text">
+                <string/>
+               </property>
+               <property name="icon">
+                <iconset>
+                 <normaloff>:/new/prefix1/icons/22x22/edit-find-replace.png</normaloff>:/new/prefix1/icons/22x22/edit-find-replace.png</iconset>
+               </property>
+              </widget>
+             </item>
+             <item>
+              <widget class="QLabel" name="labelEnergy">
+               <property name="text">
+                <string>TextLabel</string>
+               </property>
+              </widget>
+             </item>
+            </layout>
+           </widget>
+          </item>
+          <item>
+           <widget class="QGroupBox" name="groupBoxSyrthes">
+            <layout class="QHBoxLayout" name="boxLayoutSyrthes">
+             <item>
+              <widget class="QLabel" name="label">
+               <property name="text">
+                <string>SYRTHES instance</string>
+               </property>
+              </widget>
+             </item>
+             <item>
+              <spacer name="horizontalSpacer">
+               <property name="orientation">
+                <enum>Qt::Horizontal</enum>
+               </property>
+               <property name="sizeHint" stdset="0">
+                <size>
+                 <width>40</width>
+                 <height>20</height>
+                </size>
+               </property>
+              </spacer>
+             </item>
+             <item>
+              <widget class="QLineEdit" name="lineEditSyrthes"/>
+             </item>
+            </layout>
+           </widget>
+          </item>
+          <item>
+           <spacer name="verticalSpacer">
+            <property name="orientation">
+             <enum>Qt::Vertical</enum>
+            </property>
+            <property name="sizeHint" stdset="0">
+             <size>
+              <width>20</width>
+              <height>40</height>
+             </size>
+            </property>
+           </spacer>
           </item>
          </layout>
-        </item>
-        <item>
-         <widget class="QLabel" name="labelEnergy">
-          <property name="text">
-           <string>TextLabel</string>
-          </property>
-         </widget>
         </item>
        </layout>
       </item>

commit 594d90e13c465a9a27baf67cb1c54e3ecd33a24e

[GUI] Fix thermal condition shown in GUI for NCFD (broken in commit 0ba3bb8201)

python/code_saturne/model/BoundaryNeptune.py
@@ -1370,7 +1370,7 @@ class WallBoundary(Boundary) :
             self.setEnthalpy(fieldId, value)
 
         choice = self.getEnthalpyChoice("none")
-        value = XMLEnergyNode.xmlGetChildString(choice)
+        value = XMLEnergyNode.xmlGetChildString("value")
         if value in [None, ""]:
             if choice[-7:] == 'formula':
                 value = self.getDefaultEnthalpyFormula(choice)