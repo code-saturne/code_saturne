code-saturne/code_saturne

commit a4904e59d81793c4879eb97f99fca1252c7c7719

[GUI] Fix boundary conditions generation for scalars and symmetries
  * Closes #473

python/code_saturne/base/cs_meg_to_c.py
@@ -1806,18 +1806,20 @@ class meg_to_c_interpreter:
                                         zone.getLabel(),
                                         self.case,
                                         sp_id)
-                    for _s in spm.getScalarByFieldId(sp_id):
-                        c = boundary.getScalarChoice(sp_id, _s)
-                        if 'formula' == c[-7:]:
-                            exp, req, sym = boundary.getScalarFormulaComponents(sp_id, _s)
-                            self.init_block('bnd',
-                                            zone.getLabel(),
-                                            spm.getScalarLabelByName(_s),
-                                            exp,
-                                            req,
-                                            sym,
-                                            [],
-                                            condition=c)
+                    if boundary.getNature() != "symmetry":
+                        for _s in spm.getScalarByFieldId(sp_id):
+                            c = boundary.getScalarChoice(sp_id, _s)
+                            if 'formula' == c[-7:]:
+                                exp, req, sym =
+                                boundary.getScalarFormulaComponents(sp_id, _s)
+                                self.init_block('bnd',
+                                                zone.getLabel(),
+                                                spm.getScalarLabelByName(_s),
+                                                exp,
+                                                req,
+                                                sym,
+                                                [],
+                                                condition=c)
 
 
     #---------------------------------------------------------------------------

commit 8d9a4b376bdbf23f3adcfcad572e21b72f2e5e84

Fix crash introduced by commit a4904e59 (today).

python/code_saturne/base/cs_meg_to_c.py
@@ -1810,8 +1810,8 @@ class meg_to_c_interpreter:
                         for _s in spm.getScalarByFieldId(sp_id):
                             c = boundary.getScalarChoice(sp_id, _s)
                             if 'formula' == c[-7:]:
-                                exp, req, sym =
-                                boundary.getScalarFormulaComponents(sp_id, _s)
+                                exp, req, sym = \
+                                    boundary.getScalarFormulaComponents(sp_id, _s)
                                 self.init_block('bnd',
                                                 zone.getLabel(),
                                                 spm.getScalarLabelByName(_s),
@@ -1820,7 +1820,6 @@ class meg_to_c_interpreter:
                                                 sym,
                                                 [],
                                                 condition=c)
-
 
     #---------------------------------------------------------------------------
 