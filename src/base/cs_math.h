e0960b42062e1570a9fe839c7aa918769ca4bb41
Chaï Koren <chai.koren@noreply.edf.fr>
2023-03-23 11:46:48 +0100

    Update order of vectors' computation in cs_math_3_orthonormal_basis

--- src/base/cs_math.h
+++ src/base/cs_math.h
@@ -851,26 +851,26 @@ cs_math_3_orthonormal_basis(const cs_real_t vect[3],
 {
   assert(cs_math_3_norm(vect) > cs_math_zero_threshold);
 
-  // Compute first axis
-  cs_math_3_normalize(vect, axes[0]);
+  // Compute normal - third axis
+  cs_math_3_normalize(vect, axes[2]);
 
-  // Compute second axis
+  // Compute base's first axis
   // First test projection of Ox
   cs_real_t Ox[3] = {1., 0., 0.};
   cs_real_t w[3] = {0.};
 
-  cs_math_3_orthogonal_projection(axes[0], Ox, w);
+  cs_math_3_orthogonal_projection(axes[2], Ox, w);
 
   // If Ox projection is null, project Oy
   if (cs_math_3_norm(w) < cs_math_zero_threshold) {
     cs_real_t Oy[3] = {0., 1., 0.};
-    cs_math_3_orthogonal_projection(axes[0], Oy, w);
+    cs_math_3_orthogonal_projection(axes[2], Oy, w);
   }
 
-  cs_math_3_normalize(w, axes[1]);
+  cs_math_3_normalize(w, axes[0]);
 
-  // Compute third axis using cross product
-  cs_math_3_cross_product(axes[0], axes[1], axes[2]);
+  // Compute base's second axis using cross product
+  cs_math_3_cross_product(axes[2], axes[0], axes[1]);
 }
 
 /*----------------------------------------------------------------------------*/
