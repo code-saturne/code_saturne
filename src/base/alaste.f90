code-saturne/code_saturne

commit 8c398fb5252c41c77bfc7badfc5c32ff102ba201

Migrate FSI subroutines to C.
Cherry picked from commit 4a2b5957 on master.

src/base/alaste.f90
@@ -27,6 +27,8 @@ module alaste
 
   !=============================================================================
 
+  use, intrinsic :: iso_c_binding
+
   implicit none
 
   ! Nombre de structures max en ALE et couplage code_aster
@@ -36,12 +38,13 @@ module alaste
 
   !  Methode ALE - mouvement de structures en couplage avec code_aster
 
-  ! ntcast : numero d'iteration de couplage avec code_aster
   ! nbaste : nombre de structures mobiles
   ! nbfast : nombre de faces couplees
 
-  integer, save ::  ntcast = 0
-  integer, save ::  nbaste = -999, nbfast = 0
+  integer(c_int), save ::  nbaste = -999, nbfast = 0
+
+  bind(C, name='cs_glob_ast_coupling_n_couplings') :: nbaste
+  bind(C, name='cs_glob_ast_coupling_n_faces') :: nbfast
 
   !=============================================================================
 

src/base/cs_log_setup.c
@@ -53,6 +53,8 @@
 #include "cs_function.h"
 #include "cs_log.h"
 #include "cs_mesh_quantities.h"
+#include "cs_mobile_structures.h"
+#include "cs_mobile_structures.h"
 #include "cs_parameters.h"
 #include "cs_physical_constants.h"
 #include "cs_restart.h"
@@ -165,6 +167,10 @@ _log_global_model_options(void)
   /* Face viscosity */
 
   cs_space_disc_log_setup();
+
+  /* ALE */
+
+  cs_mobile_structures_log_setup();
 
   /* Rotation info */
 

docs/doxygen/examples/cs_user_fluid_structure_interaction.dox
@@ -30,42 +30,64 @@
   \section cs_user_fluid_structure_interaction_h_intro Introduction
 
   This page provides code blocks of several examples that may be used
-  to perform fluid structure interaction computations.
+  to perform [Fluid-Structure Interaction (FSI)](@ref cs_ug_as_fsi) computations.
 
-  \section cs_user_fluid_structure_interaction_h_usstr1  Internal structures and corresponding initial conditions
+  \section cs_user_model_h_coupling_add Declaring mobile structures.
 
-  Internal structures and their behavior can be specified through the user subroutines
-  from the \ref cs_user_fluid_structure_interaction.f90 file.
+  In addition to implicit declarations with boundary conditions in the GUI,
+  internal structures may be declared in the \ref cs_user_model
+  or \ref cs_user_parameters function. For example, to declare 3 coupled
+  structures:
 
-  \subsection cs_user_fluid_structure_interaction_h_usstr1_init Initialization
+  \code{.c}
+  cs_mobile_structures_add_n_structures(3);
+  \endcode
 
-  The \c lstelt array is allocated.
+  Coupling with code_aster can be declared in a similar manner, using:
 
-  \snippet cs_user_fluid_structure_interaction.f90 usstr1_init
+  \code{.c}
+  cs_ast_coupling_add();
+  \endcode
 
-  \subsection cs_user_fluid_structure_interaction_h_usstr1_example Example
+  \section cs_user_model_h_coupling_zone Selecting coupled faces.
 
-  The following code block provides an example of definition of two internal
-  structures.
+  Whether coupled structures are defined using the GUI or as above
+  function, their association with boundary zones may be completed or
+  modified in the \ref cs_user_fsi_structure_num function from
+  the \ref cs_user_fluid_structure_interaction.c file.
 
-  Here one fills array \c idfstr(\ref nfabor). For each boundary face \c ifac,
-  \c idfstr(ifac) is the number of the structure the face belongs to
-  (if \c idfstr(ifac) = 0, the face ifac doesn't belong to any structure.
-  When using internal coupling, structure number necessarily needs to be positive
-  (as shown in following examples).
+  In the following example, 2 different structures are each associated to
+  a boundary group:
 
-  The number of "internal" structures is automatically defined with the
-  maximum value of \c idfstr, meaning that internal structure numbers must
-  be defined sequentially with positive values, beginning with integer value 1.
+  \snippet cs_user_fluid_structure_interaction.c fsi_i_str_num
 
-  In the following example, boundary faces with color 4 belong to internal structure 1.
-  Boundary faces with color 2 belong to internal structure 2.
-  The total number of internal structures equals 2.
-  The boundary faces are identified using the \ref getfbr subroutine.
+  This type of association is not necessary using the GUI in simple cases,
+  but could be used to associate multiple boundary zones to a common structure
+  for example, or conversely, associated different structures to different
+  portions of a given zone (for example, if a single zone contains multiple
+  tubes, so as to simplify fluid BC definitions, separate structures could
+  be associated to each tube using sub-selections).
 
-  \snippet cs_user_fluid_structure_interaction.f90 usstr1_example_a
+  In the next example, the same face groups are coupled with an external
+  (code_aster) structure.
 
-  For each internal structure one can here define :
+  \snippet cs_user_fluid_structure_interaction-code_aster.c fsi_ext_struct_example
+
+  Note that in this case, only the (negative) sign of the structure number is
+  used, not the actual number, so both groups of faces are coupled to the
+  same code_aster instance.
+
+  \section cs_user_fluid_structure_interaction_h_internal  Internal structure parameters
+
+  Various global and individual parameters associated to internal structures can
+  be controlled with functions from \ref cs_user_fluid_structure_interaction.c.
+
+  \subsection cs_user_fluid_structure_interaction_h_usstr1_example Initial positions and parameters
+
+  Initial positions and other parameters associated with internal structure coupling
+  may be defined using the \ref cs_user_fsi_structure_define function.
+
+  For each internal structure one can here define:
   - an initial velocity \c vstr0
   - an initial displacement \c xstr0 (i.e. \c xstr0 is the value of the
     displacement \ref xstr compared to the initial mesh at time t=0)
@@ -77,31 +99,31 @@
        -k (xstr(t)+xstreq).
     \f]
 
-  Note that \c xstr0, \c xstreq and \c vstr0 arrays are initialized at the beginning
-  of the calculations to the value of 0.
-
   When starting a calculation using ALE, or re-starting a calculation with
   ALE basing on a first calculation without ALE, an initial iteration 0 is
   automatically calculated in order to take initial arrays \c xstr0, \c vstr0 and
   \c xstreq into account. In another case, set the following option
-  \code{.f90}
-  italin=1
+  \code{.c}
+  cs_glob_ale_need_init = 1;
   \endcode
-  in subroutine \ref usipsu, so that the code can deal with arrays
+  in the \ref cs_user_parameters function, so that the code can deal with arrays
   \c xstr0, \c vstr0 or \c xstreq.
 
-  In the following example :
-  - internal structure 1 has got an initial displacement \c xstr0 of 2 meters in
+  Note that \c xstr0, \c xstreq and \c vstr0 arrays are initialized at the beginning
+  of the calculations to the value of 0.
+
+  In the following example:
+  - internal structure 0 has got an initial displacement \c xstr0 of 2 meters in
     the y direction and a displacement compared to equilibrium \c xstreq of
     1 meter in the y direction.
-  - Initial velocity \c vstr0 in the z direction of structure 2 equals -0.5 m/s.
+  - Initial velocity \c vstr0 in the z direction of structure 1 equals -0.5 m/s.
 
-  \snippet cs_user_fluid_structure_interaction.f90 usstr1_example_b
+  \snippet cs_user_fluid_structure_interaction.c fsi_i_ex_a
 
   Here one can modify the values of the prediction coefficients for
-  displacements anf fluid forces in internal FSI coupled algorithm.
+  displacements and fluid forces in internal FSI coupled algorithm.
 
-  The use of these coefficients is reminded here :
+  The use of these coefficients is reminded here:
   - predicted displacement
     \f[
     \vect{X_{n+1}} = \vect{X_n} + aexxst \Delta t \vect{X^{\prime}_n} +
@@ -117,109 +139,89 @@
     \f$ \vect{F_n} \f$ and \f$ \vect{F_{n-1}} \f$ standing for the fluid force acting on the
     structure respectively at iteration \f$ n \f$ and \f$ n-1 \f$.
 
-  \snippet cs_user_fluid_structure_interaction.f90 usstr1_example_c
+  The following code snippet redefines theses advanced coefficients for predicted
+  displacements and predicted force:
 
-  Activation of structural history output (i.e. displacement, structural
-  velocity, structural acceleration and fluid force)
-  (\c ihistr=0, disabled ; \c ihistr=1, enabled)
-  The value of structural history output step is the same as the one for
-  standard variables (\ref entsor::nthist "nthist").
+  \snippet cs_user_fluid_structure_interaction.c fsi_i_ex_b
 
-  \snippet cs_user_fluid_structure_interaction.f90 usstr1_example_d
+  The time plotting behavior of internal structures may also be modified
+  in this same function.
+  The following example shows how the default output format and interval
+  may be modified:
 
-  \section cs_user_fluid_structure_interaction_h_usstr2  Structural parameters in case of Fluid Structure internal coupling
+  \snippet cs_user_fluid_structure_interaction.c fsi_i_ex_c
 
-  Note that the subroutine \ref usstr2 is called at each time step of the calculation.
+  The output interval is based on a standard \ref cs_time_control_t logic,
+  so time-based or more advanced user-defined behavior may de used here.
+  It is recommended not to use a too large interval, so that sampling does
+  not miss important signal characteristics.
 
-  For each internal structure one defines here :
+  Note also that the movement of the internal fluid structure is computed using
+  a Newmark method, whose parameters can also be modified by calling the
+  \ref cs_mobile_structures_set_newmark_coefficients function.
+
+  \subsection cs_user_fluid_structure_interaction_h_usstr1_example Structural parameters
+
+  Some internal structure parameters, although usually fixed, may be time-varying
+  in complex models, so they may be defined and changed using the
+  \ref cs_user_fsi_structure_values function, which is called at each time
+  step when internal mobile structures are present.
+
+  For each internal structure one defines here:
   - its mass \f$ \tens{M} \f$ (\c xmstru)
   - its friction coefficient \f$ \tens{C} \f$ (\c xcstru)
   - its stiffness \f$ \tens{K} \f$ (\c xkstru)
 
-  \c forstr array gives fluid stresses acting on each internal structure.
-  Moreover it's possible to take external forces (gravity for example) into
+  The \c forstr array stores fluid stresses acting on each internal structure.
+  Moreover it is possible to take external forces (gravity for example) into
   account, too.
 
-  \c xstr array indicates the displacement of the structure compared to its
+  The \c xstr array indicates the displacement of each structure compared to its
   position in the initial mesh.
 
-  \c xstr0 array gives the displacement of the structures in initial mesh
-  compared to structural equilibrium.
+  The \c xstr0 array gives the displacement of the structures in initial mesh
+  compared to structure equilibrium.
 
-  \c vstr array stands for structural velocity.
+  The \c vstr array contains the structure velocities.
 
-  \c xstr, \c xstr0, and \c vstr arrays are data tables that can be used to define
+  The \c xstr, \c xstr0, and \c vstr arrays are data tables that can be used to define
   arrays mass, friction and stiffness. THOSE ARE NOT TO BE MODIFIED.
 
-  The 3D structural equation that is solved is the following one :
+  The 3D structure equation that is solved is:
   \f[
     \tens{M} \vect{X^{\prime\prime}} + \tens{C} \vect{X^{\prime}} + \tens{K} (\vect{X}+\vect{X_0}) = \vect{F}
   \f]
-  where \f$\vect{X}\f$ stands for the structural displacement compared to initial mesh
-      position (\c xstr) and \f$\vect{X_0}\f$ represents the displacement of the structure in initial mesh compared to equilibrium.
+  where \f$\vect{X}\f$ stands for the structural displacement compared to initial mesh position (\c xstr) and \f$\vect{X_0}\f$ represents the displacement of the structure in the initial mesh compared to equilibrium.
 
-    Note that \f$\tens{M}\f$, \f$\tens{C}\f$ and \f$\tens{K}\f$ are 3x3 matrices.
+  Note that \f$\tens{M}\f$, \f$\tens{C}\f$ and \f$\tens{K}\f$ are 3x3 matrices.
 
-    This equation is solved using a Newmark HHT algorithm.
-    Note that the time step used to solve this equation (\c dtstr) can be
-    different from the one of fluid calculations. user is free to define
-    \c dtstr array. At the beginning of the calculation \c dtstr is
-    initialized to the value of \c dt (fluid time step).
+  This equation is solved using a Newmark HHT algorithm.
+  Note that the time step used to solve this equation (\c dtstr) can be
+  different from the one of fluid calculations. user is free to define
+  \c dtstr array. At the beginning of the calculation \c dtstr is
+  initialized to the value of \c dt (fluid time step).
 
-  \subsection cs_user_fluid_structure_interaction_h_usstr2_init Initialization
-
-  The matrices \c xmstru, \c xcstru and \c xkstru are set to zero.
-
-  \snippet cs_user_fluid_structure_interaction.f90 usstr2_init
-
-  \subsection cs_user_fluid_structure_interaction_h_usstr2_example_1 Example 1
+  \subsubsection cs_user_fluid_structure_interaction_h_usstr2_example_1 Example 1
 
   Two examples of definition of the mass, the friction coefficient and the stiffness
   of an internal structure are provided hereafter.
 
-  \snippet cs_user_fluid_structure_interaction.f90 usstr2_example_1
+  \snippet cs_user_fluid_structure_interaction.c fsi_i_ex_d
 
-  \subsection cs_user_fluid_structure_interaction_h_usstr2_example_2 Example 2
+  \subsubsection cs_user_fluid_structure_interaction_h_usstr2_example_2 Example 2
 
-  \snippet cs_user_fluid_structure_interaction.f90 usstr2_example_2
+  \snippet cs_user_fluid_structure_interaction.c fsi_i_ex_e
 
   \section cs_user_fluid_structure_interaction_h_usaste Fluid Structure external coupling with code_aster
 
-  External structures (simulated with code_aster) can be specified through the
-  cs_user_fsi_external_structure_id user-defined function in
-  \ref cs_user_fluid_structure_interaction.c.
-
-  \subsection cs_user_fluid_structure_interaction_h_usaste_example Example
-
-  In the following example, two sets of boundary faces that will be coupled with
-  code_aster are defined.
-
-  Here one fills array \c idfstr[]
-  For each boundary face \c face_id, \c idfstr[face_id]) is the number of the structure
-  the face belongs to (if \c idfstr[face_id] = 0, the face \c face_id doesn't
-  belong to any structure.)
-  When using external coupling with code_aster, the structure number
-  necessarily needs to be negative (as shown in following examples).
-
-  The number of "external" structures with code_aster is automatically
-  defined with the maximum absolute value of \c idfstr table, meaning that
-  external structure numbers must be defined sequentially with negative
-  values beginning with integer value -1.
-
-  In following example, boundary faces in zone "wall_1" are associated
-  to external structure -1, while those in zone "wall_2" are associated
-  to external structure -2.
-
-  \snippet cs_user_fluid_structure_interaction-code_aster.c fsi_ext_struct_example
-
-  Note that we could also use the lower-level face selections such as
-  \ref cs_selector_get_b_face_list to extract subsets and assign them
-  to different structures.
+  For code_aster coupling, it is sufficient to declare a coupling as described in
+  the earlier \ref cs_user_model_h_coupling_add "section"
+  and possibly \ref cs_user_model_h_coupling_zone "select associated faces".
 
   Currently, multiple structures associated to faces associated with
   code_aster are handled in a grouped manner,so the structure number
   has no importance, as long as it is negative. In the future, in case
   of coupling with multiple instances of code_aster, we could assign
-  different structures to different solvers for example.
+  different structures to different coupling instances.
 
 */

src/Makefile.am
@@ -53,7 +53,6 @@ base/field.f90 \
 base/field_operator.f90 \
 base/alaste.f90 \
 base/albase.f90 \
-base/alstru.f90 \
 base/cavitation.f90 \
 base/cdomod.f90 \
 base/cplsat.f90 \
@@ -103,8 +102,6 @@ base/turbomachinery.f90
 
 base/albase.$(OBJEXT): base/cplsat.$(OBJEXT) base/optcal.$(OBJEXT) \
  base/cs_c_bindings.$(OBJEXT)
-
-base/alstru.$(OBJEXT): base/paramx.$(OBJEXT) base/cstnum.$(OBJEXT)
 
 base/cavitation.$(OBJEXT): base/cstphy.$(OBJEXT) base/field.$(OBJEXT) \
  base/numvar.$(OBJEXT) base/optcal.$(OBJEXT) base/pointe.$(OBJEXT) \

src/base/Makefile.am
@@ -139,6 +139,7 @@ cs_map.h \
 cs_mass_source_terms.h \
 cs_math.h \
 cs_measures_util.h \
+cs_mobile_structures.h \
 cs_rank_neighbors.h \
 cs_notebook.h \
 cs_numbering.h \
@@ -313,6 +314,8 @@ cs_flag_check.c \
 cs_function.c \
 cs_function_default.c \
 cs_gas_mix.c \
+cs_gas_mix_initialization.f90 \
+cs_gas_mix_physical_properties.f90 \
 cs_halo.c \
 cs_halo_perio.c \
 cs_head_losses.c \
@@ -320,13 +323,12 @@ cs_internal_coupling.c \
 cs_interpolate.c \
 cs_log_iteration.c \
 cs_log_setup.c \
-cs_notebook.c \
-cs_numbering.c \
+cs_mobile_structures.c \
 cs_measures_util.c \
 cs_mesh_tagmr.f90 \
 cs_metal_structures_tag.f90 \
-cs_gas_mix_initialization.f90 \
-cs_gas_mix_physical_properties.f90 \
+cs_notebook.c \
+cs_numbering.c \
 cs_opts.c \
 cs_parall.c \
 cs_param_types.c \
@@ -405,7 +407,6 @@ metal_structures_copain_model.f90 \
 modini.f90 \
 mmtycl.f90 \
 navstv.f90 \
-newmrk.f90 \
 phyvar.f90 \
 precli.f90 \
 predfl.f90 \
@@ -415,10 +416,6 @@ pthrbm.f90 \
 resvoi.f90 \
 scalai.f90 \
 schtmp.f90 \
-strdep.f90 \
-strhis.f90 \
-strini.f90 \
-strpre.f90 \
 tdesi1.f90 \
 tridim.f90 \
 tspdcv.f90 \

src/base/albase.f90
@@ -44,13 +44,18 @@ module albase
   !>  - 2: CDO solver
   integer(c_int), pointer, save :: iale
   !> the number of sub-iterations of initialization of the fluid
-  integer, save :: nalinf = 0
+  integer(c_int), save :: nalinf = 0
   !> maximum number of implicitation iterations of the structure displacement
-  integer, save :: nalimx = 1
+  integer(c_int), save :: nalimx = 1
   !> relative precision of implicitation of the structure displacement
-  double precision, save :: epalim = 1.d-5
+  real(c_double), save :: epalim = 1.d-5
   !> iteration (yes=1, no=0) to initialize ALE
-  integer, save :: italin = -999
+  integer(c_int), save :: italin = -999
+
+  bind(C, name='cs_glob_mobile_structures_i_max') :: nalimx
+  bind(C, name='cs_glob_mobile_structures_i_eps') :: epalim
+  bind(C, name='cs_glob_ale_n_ini_f') :: nalinf
+  bind(C, name='cs_glob_ale_need_init') :: italin
 
   !> indicator of imposed displacement
   integer, allocatable, dimension(:) :: impale
@@ -101,7 +106,19 @@ contains
   subroutine map_ale
 
     use, intrinsic :: iso_c_binding
-    use cs_c_bindings
+
+    !---------------------------------------------------------------------------
+
+    interface
+
+      subroutine cs_f_ale_get_pointers(iale) &
+        bind(C, name='cs_f_ale_get_pointers')
+        use, intrinsic :: iso_c_binding
+        implicit none
+        type(c_ptr), intent(out) :: iale
+      end subroutine cs_f_ale_get_pointers
+
+    end interface
 
     ! Local variables
 

src/base/alstru.f90
@@ -1,118 +1,0 @@
-!-------------------------------------------------------------------------------
-
-! This file is part of code_saturne, a general-purpose CFD tool.
-!
-! Copyright (C) 1998-2023 EDF S.A.
-!
-! This program is free software; you can redistribute it and/or modify it under
-! the terms of the GNU General Public License as published by the Free Software
-! Foundation; either version 2 of the License, or (at your option) any later
-! version.
-!
-! This program is distributed in the hope that it will be useful, but WITHOUT
-! ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-! FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-! details.
-!
-! You should have received a copy of the GNU General Public License along with
-! this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
-! Street, Fifth Floor, Boston, MA 02110-1301, USA.
-
-!-------------------------------------------------------------------------------
-
-
-!> \file alstru.f90
-!> Module for ALE structure movement with internal coupling
-
-module alstru
-
-  !=============================================================================
-
-  use paramx
-  use cstnum
-
-  implicit none
-
-  !=============================================================================
-
-  !> \defgroup alstru Module for ALE structure movement with internal coupling
-
-  !> \addtogroup alstru
-  !> \{
-
-  !> number of structures, automatically computed
-  integer, save :: nbstru = -999
-
-  !> \anchor xmstru mass matrix of the structure (kg)
-  !> (for \ref xmstru "xmstru"(i,j,k), i and j are the array of mass structure
-  !> and k is the index of the structure)
-  double precision, save :: xmstru(3,3,nstrmx)
-
-  !> \anchor xcstru damping matric coefficient of the structure (kg/s)
-  double precision, save :: xcstru(3,3,nstrmx)
-
-  !> \anchor xkstru spring matrix constant of the structure (kg/s2 = N/m)
-  double precision, save :: xkstru(3,3,nstrmx)
-
-  !> \anchor xstr displacement vector of the structure compared to its position
-  !> in the initial mesh (m)
-  double precision, save :: xstr(3,nstrmx)
-
-  !> \anchor xsta value of \ref xstr at the previous time step (m)
-  double precision, save :: xsta(3,nstrmx)
-
-  !> predicted value of \ref xstr (m)
-  double precision, save :: xstp(3,nstrmx)
-
-  !> \anchor xstreq equilibrum position of a structure (m)
-  double precision, save :: xstreq(3,nstrmx)
-
-  !> \anchor xpstr velocity vector of the structure (m/s)
-  double precision, save :: xpstr(3,nstrmx)
-
-  !> \ref xpstr at previous time step (m/s)
-  double precision, save :: xpsta(3,nstrmx)
-
-  !> \anchor xppstr acceleration vector of the structure (m/s2)
-  double precision, save :: xppstr(3,nstrmx)
-
-  !> \ref xppstr at previous time step (m/s2)
-  double precision, save :: xppsta(3,nstrmx)
-
-  !> \anchor forstr force vector acting on the structure (N)
-  double precision, save :: forstr(3,nstrmx)
-
-  !> \ref forstr at previous time step (N)
-  double precision, save :: forsta(3,nstrmx)
-
-  !> predicted force vector acting on the structure (N)
-  double precision, save :: forstp(3,nstrmx)
-
-  !> time step used to solved structure movement
-  !> (can be different from th fluid time step)
-  double precision, save :: dtstr(nstrmx)
-
-  !> coefficient for the predicted displacement
-  double precision, save :: aexxst = -grand
-
-  !> coefficient for the predicted displacement
-  double precision, save :: bexxst = -grand
-
-  !> coefficient for the predicted force
-  double precision, save :: cfopre = -grand
-
-  !> alpha coefficient for the Newmark hht methode
-  double precision, save :: alpnmk = 0.d0
-
-  !> beta coefficient for the Newmark hht methode
-  double precision, save :: betnmk = -grand
-
-  !> gamma coefficient for the Newmark hht methode
-  double precision, save :: gamnmk = -grand
-
-  !> \}
-
-  !=============================================================================
-
-end module alstru
-

commit ffb7027dd1ce0e64016ea89d18240e13d6adbf5b

Fix crash introduced by commit 8c398fb5 (today).
Cherry picked from commit 53e8207e on master.

src/base/alaste.f90
@@ -31,20 +31,13 @@ module alaste
 
   implicit none
 
-  ! Nombre de structures max en ALE et couplage code_aster
-
-  integer nastmx
-  parameter (nastmx=200)
-
   !  Methode ALE - mouvement de structures en couplage avec code_aster
 
   ! nbaste : nombre de structures mobiles
-  ! nbfast : nombre de faces couplees
 
-  integer(c_int), save ::  nbaste = -999, nbfast = 0
+  integer(c_int), save :: nbaste = 0
 
   bind(C, name='cs_glob_ast_coupling_n_couplings') :: nbaste
-  bind(C, name='cs_glob_ast_coupling_n_faces') :: nbfast
 
   !=============================================================================
 

src/base/cs_log_setup.c
@@ -41,6 +41,7 @@
  * Local headers
  *----------------------------------------------------------------------------*/
 
+#include "cs_ale.h"
 #include "cs_atmo.h"
 #include "cs_base.h"
 #include "cs_boundary.h"
@@ -53,7 +54,6 @@
 #include "cs_function.h"
 #include "cs_log.h"
 #include "cs_mesh_quantities.h"
-#include "cs_mobile_structures.h"
 #include "cs_mobile_structures.h"
 #include "cs_parameters.h"
 #include "cs_physical_constants.h"
@@ -170,7 +170,8 @@ _log_global_model_options(void)
 
   /* ALE */
 
-  cs_mobile_structures_log_setup();
+  if (cs_glob_ale > 0)
+    cs_mobile_structures_log_setup();
 
   /* Rotation info */
 

src/base/cs_mobile_structures.c
@@ -175,8 +175,7 @@ typedef struct {
 
 static cs_mobile_structures_t  *_mobile_structures = NULL;
 
-extern int        cs_glob_ast_coupling_n_couplings;  /* n_ast_structs */
-extern cs_lnum_t  cs_glob_ast_coupling_n_faces;      /* nbfast */
+extern int cs_glob_ast_coupling_n_couplings;         /* n_ast_structs */
 
 /* Arrays allowing return to initial state at end of ALE iteration.
  * - Mass flux: save at the first call of schtmp.