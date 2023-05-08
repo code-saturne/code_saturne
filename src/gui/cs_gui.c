code-saturne/code_saturne

commit 248d5276130ea007285c9f4d398f074c568eb25c

Many improvements to CoolProp support.
Cherry picked from commit badb78b9 on master.

- Revert to C API to align with default CoolProp install and documentation
  (even with C++, the CoolProp documentation examples use CoolPropLib.h and
  not directly CoolProp.h).
- Use error message from CoolProp in case of error, allowing easier
  troubleshooting.
- Can now use low-level API to allow use of tabulated backends.
- Auomatically switch to tabulated backends by default in case of
  variable properties.

src/gui/cs_gui.c
@@ -660,6 +660,52 @@ _scalar_diffusion_value(const cs_field_t  *f,
   cs_gui_node_get_real(tn, value);
 }
 
+/*----------------------------------------------------------------------------
+ * Check if at least one user property is based on a thermal law.
+ *
+ * Fortran Interface:
+ *
+ * subroutine uiphyv
+ * *****************
+ *
+ * integer          iviscv   <--  pointer for volumic viscosity viscv
+ *----------------------------------------------------------------------------*/
+
+static int
+_count_thermal_laws(void)
+{
+  int n_thl = 0;
+
+  int n_zones = cs_volume_zone_n_zones();
+  for (int z_id = 0; z_id < n_zones; z_id++) {
+    const cs_zone_t *z = cs_volume_zone_by_id(z_id);
+    if (! (z->type & CS_VOLUME_ZONE_PHYSICAL_PROPERTIES))
+      continue;
+    if (strcmp(z->name, "all_cells") != 0)
+      continue;
+
+    const *names[] = {
+      "density", "molecular_viscosity", "specific_heat",
+      "thermal_conductivity"
+    };
+
+    for (int i = 0; i < 4; i++) {
+      const char *prop_choice = _properties_choice(names[i], NULL);
+      if (cs_gui_strcmp(prop_choice, "thermal_law"))
+        n_thl += 1;
+    }
+
+    /* volumic viscosity (compressible model) */
+    if (cs_glob_physical_model_flag[CS_COMPRESSIBLE] > -1) {
+      const char *prop_choice = _properties_choice("volume_viscosity", NULL);
+      if (cs_gui_strcmp(prop_choice, "thermal_law"))
+        n_thl += 1;
+    }
+  }
+
+  return n_thl;
+}
+
 /*-----------------------------------------------------------------------------
  * Find the node associated with a given variable.
  *
@@ -4036,9 +4082,19 @@ cs_gui_physical_properties(void)
       //  thermal_plane = CS_PHYS_PROP_PLANE_PS;
 
       const int itpscl = cs_glob_thermal_model->itpscl;
+      const char *method = _thermal_table_choice("method");
+
+      if (   strcmp(method, "CoolProp") == 0
+          && itherm > CS_THERMAL_MODEL_NONE) {
+        if (cs_physical_properties_get_coolprop_backend() == NULL) {
+          int n_th_laws = _count_thermal_laws();
+          if (n_th_laws > 0)
+            cs_physical_properties_set_coolprop_backend("TTSE&HEOS");
+        }
+      }
 
       cs_thermal_table_set(material,
-                           _thermal_table_choice("method"),
+                           method,
                            _thermal_table_option("reference"),
                            thermal_plane,
                            itpscl);

m4/cs_coolprop.m4
@@ -67,14 +67,14 @@ AC_ARG_WITH(coolprop-include,
              COOLPROP_CPPFLAGS="-I$with_coolprop_include"],
             [if test "x$with_coolprop" != "xno" -a "x$with_coolprop" != "xyes" \
 	          -a "x$with_coolprop" != "xcheck"; then
-               if test -f "$with_coolprop/include/CoolProp.h"; then
+               if test -f "$with_coolprop/include/CoolPropLib.h"; then
                  COOLPROP_CPPFLAGS="-I$with_coolprop/include"
-               elif test -f "$with_coolprop/CoolProp.h"; then
+               elif test -f "$with_coolprop/CoolPropLib.h"; then
                  COOLPROP_CPPFLAGS="-I$with_coolprop"
                else
-                 find $with_coolprop | grep CoolProp.h > /dev/null 2>&1;
+                 find $with_coolprop | grep CoolPropLib.h > /dev/null 2>&1;
                  if test $? == "0"; then
-                   cs_coolprop_tmp=`find $with_coolprop -name CoolProp.h | head -1`
+                   cs_coolprop_tmp=`find $with_coolprop -name CoolPropLib.h | head -1`
                    COOLPROP_CPPFLAGS="-I`dirname $cs_coolprop_tmp`"
                    unset cs_coolprop_tmp
                  fi
@@ -146,22 +146,15 @@ if test "x$with_coolprop" != "xno" ; then
 
   # Check that CoolProp files exist
 
-  # CoolProp is in C++, though it also provides a C wrapper.
-  AC_LANG_PUSH([C++])
-
   AC_MSG_CHECKING([for CoolProp library)])
 
   for coolprop_ldadd in "" "-ldl"
   do
     if test "x$cs_have_coolprop" != "xyes"; then
       LDFLAGS="${saved_LDFLAGS} ${COOLPROP_LDFLAGS} ${coolprop_ldadd}"
-      AC_LINK_IFELSE([AC_LANG_PROGRAM([[#include "CoolProp.h"]],
-                                      [[std::vector<double> t;
-                                        std::vector<double> p;
-                                        std::vector<double> z;
-                                        std::vector<std::string> f;
-                                        std::vector<std::string> o;
-                                        CoolProp::PropsSImulti(o, "T", t, "P", p, "", f, z);]])],
+      AC_LINK_IFELSE([AC_LANG_PROGRAM([[#include <stdbool.h>
+                                        #include "CoolPropLib.h"]],
+                                      [[double v = PropsSI("o", "n1", 1., "n2", 1., "r");]])],
                                       [ AC_DEFINE([HAVE_COOLPROP], 1, [CoolProp support])
                                         cs_have_coolprop=yes],
                                        [cs_have_coolprop=no])
@@ -169,8 +162,6 @@ if test "x$with_coolprop" != "xno" ; then
   done
 
   AC_MSG_RESULT($cs_have_coolprop)
-
-  AC_LANG_POP
 
   if test "x$cs_have_coolprop" != "xyes"; then
     if test "x$with_coolprop" != "xcheck"; then

src/base/cs_coolprop.cxx
@@ -41,7 +41,9 @@
  * CoolProp library headers
  *----------------------------------------------------------------------------*/
 
-#include "CoolProp.h"
+extern "C" {
+#include "CoolPropLib.h"
+}
 
 /*----------------------------------------------------------------------------
  *  Local headers
@@ -60,18 +62,33 @@
 
 /*----------------------------------------------------------------------------*/
 
+/* Thermal table structure */
+
+typedef struct {
+
+  long         abstract_state_handle;  /* handle to abstract state */
+
+  char         backend[32];            /* backend name */
+  char         fluid_names[32];        /* fluid names */
+
+} cs_coolprop_state_t;
+
 /*=============================================================================
  *  Global variables
  *============================================================================*/
+
+static int  _n_states = 0;
+static cs_coolprop_state_t *_states = NULL;
 
 /*============================================================================
  * Private function definitions
  *============================================================================*/
 
-
 /*============================================================================
  * Public function definitions
  *============================================================================*/
+
+BEGIN_C_DECLS
 
 /*----------------------------------------------------------------------------*/
 /*!
@@ -91,9 +108,6 @@
  */
 /*----------------------------------------------------------------------------*/
 
-#ifdef __cplusplus
-extern "C"
-#endif
 void
 cs_phys_prop_coolprop(char                              *coolprop_material,
                       const char                        *coolprop_backend,
@@ -104,21 +118,88 @@ cs_phys_prop_coolprop(char                              *coolprop_material,
                       const cs_real_t                    var2[],
                       cs_real_t                          val[])
 {
-  std::vector<std::string> fluids;
-  std::vector<std::string> outputs;
+  const int mb_size = 511;
+  char message_buffer[mb_size + 1];
 
-  fluids.push_back(coolprop_material);
-  std::string Name1 = "";
-  std::string Name2 = "P";
+  const char *fluid_names = coolprop_material;
 
-  std::string Backend = "HEOS";
-  if (coolprop_backend != NULL) Backend = coolprop_backend;
+  char name1[] = "P";
 
-  if (thermo_plane == CS_PHYS_PROP_PLANE_PH)
-    Name1 = "H";
-  else if (thermo_plane == CS_PHYS_PROP_PLANE_PT)
-    Name1 = "T";
+  char backend[32];
+  if (coolprop_backend != NULL)
+    strncpy(backend, coolprop_backend, 32);
+  else
+    strncpy(backend, "HEOS", 32);
+  backend[31] = '\0';
 
+  long errcode = 0;
+  long state_handle = -1;
+
+  /* API choice for easier testing:
+     - 0: high level interface.
+     - 1: low level interfaces with array data.
+     - 2: low level interface with individual calls per element.
+  */
+  int api_choice = 0;
+
+  /* The high level interface is not compatible with tabulated backends */
+  if (api_choice == 0) {
+    if (   strncmp(backend, "TTSE", 4) == 0
+        || strncmp(backend, "BICUBIC", 7) == 0)
+      api_choice = 1;
+  }
+
+  if (api_choice > 0) {
+
+    for (int i = 0; i < _n_states; i++) {
+      if (   strncmp(backend, _states[i].backend, 31) == 0
+             && strncmp(fluid_names, _states[i].fluid_names, 31) == 0) {
+        state_handle = _states[i].abstract_state_handle;
+        break;
+      }
+    }
+
+    if (state_handle < 0) {
+      state_handle = AbstractState_factory(backend, fluid_names,
+                                           &errcode, message_buffer, mb_size);
+      if (errcode != 0)
+        bft_error(__FILE__, __LINE__, 0, "%s", message_buffer);
+
+      double fractions[] = {1.0};
+      AbstractState_set_fractions(state_handle, fractions, 1,
+                                  &errcode, message_buffer, mb_size);
+      if (errcode != 0)
+        bft_error(__FILE__, __LINE__, 0, "%s", message_buffer);
+
+      AbstractState_specify_phase(state_handle, "phase_unknown",
+                                  &errcode, message_buffer, mb_size);
+      if (errcode != 0)
+        bft_error(__FILE__, __LINE__, 0, "%s", message_buffer);
+
+      BFT_REALLOC(_states, _n_states+1, cs_coolprop_state_t);
+
+      _states[_n_states].abstract_state_handle = state_handle;
+      memset(_states[_n_states].backend, 0, 32);
+      strncpy(_states[_n_states].backend, backend, 31);
+      memset(_states[_n_states].fluid_names, 0, 32);
+      strncpy(_states[_n_states].fluid_names, fluid_names, 31);
+      _n_states += 1;
+    }
+  }
+
+  char name2[2];
+  long input_pair = -1;
+  if (thermo_plane == CS_PHYS_PROP_PLANE_PH) {
+    input_pair = get_input_pair_index("PH_INPUTS");
+    strncpy(name2, "H", 2);
+  }
+  else if (thermo_plane == CS_PHYS_PROP_PLANE_PT) {
+    input_pair = get_input_pair_index("PT_INPUTS");
+    strncpy(name2, "T", 2);
+  }
+  name2[1] = '\0';
+
+  char outputs[32];
   switch (property) {
     case CS_PHYS_PROP_PRESSURE:
       bft_error(__FILE__, __LINE__, 0,
@@ -126,88 +207,171 @@ cs_phys_prop_coolprop(char                              *coolprop_material,
       break;
     case CS_PHYS_PROP_TEMPERATURE:
       /* temperature is in K */
-      outputs.push_back("T");
+      strncpy(outputs, "T", 31);
       break;
     case CS_PHYS_PROP_ENTHALPY:
-      outputs.push_back("H");
+      strncpy(outputs, "H", 31);
       break;
     case CS_PHYS_PROP_ENTROPY:
-      outputs.push_back("S");
+      strncpy(outputs, "S", 31);
       break;
     case CS_PHYS_PROP_ISOBARIC_HEAT_CAPACITY:
-      outputs.push_back("CPMASS");
+      strncpy(outputs, "CPMASS", 31);
       break;
     case CS_PHYS_PROP_ISOCHORIC_HEAT_CAPACITY:
-      outputs.push_back("CVMASS");
+      strncpy(outputs, "CVMASS", 31);
       break;
     case CS_PHYS_PROP_SPECIFIC_VOLUME:
       bft_error(__FILE__, __LINE__, 0,
                 _("bad choice: specific volume not available yet"));
       break;
     case CS_PHYS_PROP_DENSITY:
-      outputs.push_back("D");
+      strncpy(outputs, "D", 31);
       break;
     case CS_PHYS_PROP_INTERNAL_ENERGY:
-      outputs.push_back("U");
+      strncpy(outputs, "U", 31);
       break;
     case CS_PHYS_PROP_QUALITY:
       bft_error(__FILE__, __LINE__, 0,
                 _("bad choice: quality not available yet"));
       break;
     case CS_PHYS_PROP_THERMAL_CONDUCTIVITY:
-      outputs.push_back("L");
+      strncpy(outputs, "L", 31);
       break;
     case CS_PHYS_PROP_DYNAMIC_VISCOSITY:
-      outputs.push_back("V");
+      strncpy(outputs, "V", 31);
       break;
     case CS_PHYS_PROP_SPEED_OF_SOUND:
-      outputs.push_back("SPEED_OF_SOUND");
+      strncpy(outputs, "SPEED_OF_SOUND", 31);
       break;
   }
+  outputs[31] = '\0';
+  long output_key = get_param_index(outputs);
 
-  //std::vector<double> T(1,298), p(1e5);
-  //std::cout << PropsSImulti(outputs,"T", T, "P", p, "", fluids, z)[0][0] << std::endl;
-  //std::cout << PropsSImulti(outputs,"T", T, "P", p, "HEOS", fluids, z)[0][0] << std::endl;
-  //std::cout << PropsSImulti(outputs,"T", T, "P", p, "REFPROP", fluids, z)[0][0] << std::endl;
+  double *prop1, *prop2, *result;
+  double *_prop1 = NULL, *_prop2 = NULL, *_result = NULL;
 
-  std::vector<double> val1;
-  std::vector<double> val2;
-  std::vector<double> fractions;
-
-  val1.reserve(n_vals);
-  val2.reserve(n_vals);
-  fractions.clear();
-
-  for (cs_lnum_t i = 0; i < n_vals; i++) {
-    val1.push_back(var2[i]);
-    val2.push_back(var1[i]);
+  if (sizeof(cs_real_t) != sizeof(double)) {
+    BFT_MALLOC(_prop1, n_vals, double);
+    BFT_MALLOC(_prop2, n_vals, double);
+    BFT_MALLOC(_result, n_vals, double);
+    for (cs_lnum_t i = 0; i < n_vals; i++) {
+      _prop1[i] = var1[i];
+      _prop2[i] = var1[i];
+    }
+    prop1 = _prop1;
+    prop2 = _prop2;
+    result = _result;
   }
-  fractions.push_back(1.0);
+  else {
+    prop1 = const_cast<double *>(var1);
+    prop2 = const_cast<double *>(var2);
+    result = val;
+  }
 
   cs_fp_exception_disable_trap();
 
-  std::vector<std::vector<double> > out = CoolProp::PropsSImulti(outputs,
-                                                                 Name1,
-                                                                 val1,
-                                                                 Name2,
-                                                                 val2,
-                                                                 Backend,
-                                                                 fluids,
-                                                                 fractions);
+  switch(api_choice) {
+  case 0:
+    {
+      long resdim1 = n_vals, resdim2 = 1;
+      double fractions[1] = {1.0};
+
+      PropsSImulti(outputs,
+                   name1, prop1, n_vals,
+                   name2, prop2, n_vals,
+                   backend,
+                   fluid_names,
+                   fractions, 1,
+                   result, &resdim1, &resdim2);
+
+      if (resdim1 != n_vals) {
+        get_global_param_string("errstring", message_buffer, mb_size);
+        bft_error(__FILE__, __LINE__, 0,
+                  _("CoolProp was unable to compute some fluid properties with\n"
+                    "input variable names: \"%s\", \"%s\" and backend \"%s\".\n\n"
+                    "Fluid(s) considered: \"%s\".\n\n"
+                    "%s."),
+                  name1, name2, backend, fluid_names, message_buffer);
+      }
+      assert(resdim2 == 1);
+    }
+    break;
+
+  case 1:
+    {
+      AbstractState_update_and_1_out(state_handle, input_pair,
+                                     prop1, prop2, n_vals, output_key,
+                                     result,
+                                     &errcode, message_buffer, mb_size);
+      if (errcode != 0)
+        bft_error(__FILE__, __LINE__, 0,
+                  _("CoolProp was unable to compute property \"%s\" with\n"
+                    "input variable names: \"%s\", \"%s\" and backend \"%s\".\n\n"
+                    "Fluid(s) considered: \"%s\".\n\n"
+                    "%s."),
+                  outputs, name1, name2, backend, fluid_names, message_buffer);
+    }
+    break;
+
+  case 2:
+    {
+      for (int i = 0; i < n_vals; i++) {
+        AbstractState_update(state_handle, input_pair,
+                             prop1[i], prop2[i],
+                             &errcode, message_buffer, mb_size);
+        if (errcode == 0)
+          result[i] = AbstractState_keyed_output(state_handle, output_key,
+                                                 &errcode, message_buffer, mb_size);
+        if (errcode != 0)
+          bft_error(__FILE__, __LINE__, 0,
+                    _("CoolProp was unable to compute property \"%s\" with\n"
+                      "input variable names: \"%s\", \"%s\" and backend \"%s\".\n\n"
+                      "Fluid(s) considered: \"%s\".\n\n"
+                      "%s."),
+                    outputs, name1, name2, backend, fluid_names, message_buffer);
+      }
+    }
+    break;
+
+  default:
+    assert(0);
+  }
 
   cs_fp_exception_restore_trap();
 
-  if ((cs_lnum_t)(out.size()) != n_vals)
-    bft_error(__FILE__, __LINE__, 0,
-              _("CoolProp was unable to compute some fluid properties with\n"
-                "input variable names: \"%s\", \"%s\" and backend \"%s\".\n\n"
-                "First of %d fluid(s) considered: \"%s\"."),
-              Name1.c_str(), Name2.c_str(), Backend.c_str(),
-              (int)n_vals, fluids[0].c_str());
+  if (_result != NULL) {
+    for (int i = 0; i < n_vals; i++)
+      val[i] = result[i];
+    BFT_FREE(_result);
+  }
 
-  for (int i = 0; i < n_vals; i++)
-    val[i] = out[i][0];
+  BFT_FREE(_prop1);
+  BFT_FREE(_prop2);
 }
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Free state for Coolprop plugin.
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_coolprop_finalize(void)
+{
+  for (int i = 0; i < _n_states; i++) {
+    long errcode = 0;
+    char message_buffer[512];
+
+    AbstractState_free(_states[i].abstract_state_handle,
+                       &errcode, message_buffer, 511);
+  }
+
+  BFT_FREE(_states);
+  _n_states = 0;
+}
+
+END_C_DECLS
 
 /*----------------------------------------------------------------------------*/
 

src/base/cs_coolprop.hxx
@@ -41,6 +41,8 @@
 
 /*----------------------------------------------------------------------------*/
 
+BEGIN_C_DECLS
+
 /*=============================================================================
  * Local Macro Definitions
  *============================================================================*/
@@ -75,9 +77,6 @@
  */
 /*----------------------------------------------------------------------------*/
 
-#ifdef __cplusplus
-extern "C"
-#endif
 void
 cs_phys_prop_coolprop(char                              *coolprop_material,
                       const char                        *coolprop_backend,
@@ -89,6 +88,17 @@ cs_phys_prop_coolprop(char                              *coolprop_material,
                       cs_real_t                          val[]);
 
 /*----------------------------------------------------------------------------*/
+/*!
+ * \brief Free state for Coolprop plugin.
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_coolprop_finalize(void);
+
+/*----------------------------------------------------------------------------*/
+
+END_C_DECLS
 
 #endif /* __CS_COOLPROP_HXX__ */
 

src/base/cs_physical_properties.c
@@ -147,6 +147,9 @@ typedef void
                           const cs_real_t                    var2[],
                           cs_real_t                          val[]);
 
+typedef void
+(cs_finalize_t)(void);
+
 /*============================================================================
  * Static global variables
  *============================================================================*/
@@ -424,6 +427,12 @@ cs_thermal_table_finalize(void)
 #endif
 #if defined(HAVE_COOLPROP) && defined(HAVE_PLUGINS)
     if (cs_glob_thermal_table->type == 3) {
+      cs_finalize_t *cs_coolprop_finalize
+        = (cs_finalize_t *)  (intptr_t)
+             cs_base_get_dl_function_pointer(_cs_coolprop_dl_lib,
+                                             "cs_coolprop_finalize",
+                                             true);
+      cs_coolprop_finalize();
       cs_base_dlclose("cs_coolprop", _cs_coolprop_dl_lib);
       _cs_phys_prop_coolprop = NULL;
     }
@@ -433,6 +442,27 @@ cs_thermal_table_finalize(void)
     BFT_FREE(cs_glob_thermal_table->method);
     BFT_FREE(cs_glob_thermal_table);
   }
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Get backend set for CoolProp.
+ *
+ * Returns NULL if CoolProp is not used or backend not set yet.
+ *
+ * \return  pointer to CoolProp backend.
+ */
+/*----------------------------------------------------------------------------*/
+
+const char *
+cs_physical_properties_get_coolprop_backend(void)
+{
+#if defined(HAVE_COOLPROP)
+  return _cs_coolprop_backend;
+#else
+  CS_UNUSED(backend);
+  returne NULL;
+#endif
 }
 
 /*----------------------------------------------------------------------------*/
@@ -538,7 +568,10 @@ cs_phys_prop_compute(cs_phys_prop_type_t          property,
     var1_c = _var1_c;
   }
 
-  if (cs_glob_thermal_table->temp_scale == 2) {
+  if (   cs_glob_thermal_table->temp_scale == 2
+      && (cs_glob_thermal_table->thermo_plane == CS_PHYS_PROP_PLANE_PT
+          || cs_glob_thermal_table->thermo_plane == CS_PHYS_PROP_PLANE_TS
+          || cs_glob_thermal_table->thermo_plane == CS_PHYS_PROP_PLANE_TX)) {
     if (_n_vals == 1) {
       _var2_c_single[0] = var2[0] + 273.15;
       var2_c = _var2_c_single;

src/base/cs_physical_properties.h
@@ -91,6 +91,19 @@ cs_thermal_table_finalize(void);
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief Get backend set for CoolProp.
+ *
+ * Returns NULL if CoolProp is not used or backend not set yet.
+ *
+ * \return  pointer to CoolProp backend.
+ */
+/*----------------------------------------------------------------------------*/
+
+const char *
+cs_physical_properties_get_coolprop_backend(void);
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief Set backend for CoolProp.
  *
  * Ignored if CoolProp is not used.

commit 5eadd2a1f8716e00dce7d47f0934ea58c38c5d9c

Fix compilation with Intel compilers, broken by commit 248d5276 (2023-04-23).
Cherry picked from commit ea1396c3 on master.

src/gui/cs_gui.c
@@ -693,7 +693,7 @@ _count_thermal_laws(void)
     if (strcmp(z->name, "all_cells") != 0)
       continue;
 
-    const *names[] = {
+    const char *names[] = {
       "density", "molecular_viscosity", "specific_heat",
       "thermal_conductivity"
     };