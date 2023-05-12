23bb231dae8c949b8f72f88104b5567994533376
Jules Lecoustre <jules.lecoustre@polytechnique.edu>
2023-05-12 13:41:23 -0400

    Fix scotch options

--- install_saturne.py
+++ install_saturne.py
@@ -387,7 +387,7 @@ class Package:
             fdr = open('Make.inc/Makefile.inc.x86-64_pc_linux2')
         fd = open('Makefile.inc','w')
 
-        re_thread = re.compile('-DSCOTCH_PTHREAD')
+        re_thread = re.compile('-DSCOTCH_PTHREAD ')
         re_intsize32 = re.compile('-DINTSIZE32')
         re_intsize64 = re.compile('-DINTSIZE64')
         re_idxsize64 = re.compile('-DIDXSIZE64')
@@ -397,7 +397,7 @@ class Package:
             if line[0:3] in ['CCS', 'CCP', 'CCD']:
                 i1 = line.find('=')
                 line = line[0:i1] + '= ' + self.cc + '\n'
-            line = re.sub(re_thread, '', line)
+            line = re.sub(re_thread, ' ', line)
             line = re.sub(re_intsize32, '', line)
             line = re.sub(re_intsize64, '', line)
             line = re.sub(re_idxsize64, '-DIDXSIZE64 -DINTSIZE64', line)
