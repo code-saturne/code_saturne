code-saturne/code_saturne

commit df4b9f21c8914b523a80374be167d35e1ab75b3a

Translate navstv_total_pressure from Fortran to C.

src/base/cs_solve_navier_stokes.c
@@ -1,0 +1,221 @@
+/*============================================================================
+ * Solve the Navier-Stokes equations.
+ *============================================================================*/
+
+/*
+  This file is part of code_saturne, a general-purpose CFD tool.
+
+  Copyright (C) 1998-2023 EDF S.A.
+
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation; either version 2 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+  Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/*----------------------------------------------------------------------------*/
+
+#include "cs_defs.h"
+
+/*----------------------------------------------------------------------------
+ * Standard C library headers
+ *----------------------------------------------------------------------------*/
+
+#include <assert.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*----------------------------------------------------------------------------
+ * Local headers
+ *----------------------------------------------------------------------------*/
+
+#include "bft_mem.h"
+#include "bft_error.h"
+#include "bft_printf.h"
+
+#include "cs_array.h"
+#include "cs_assert.h"
+#include "cs_atmo.h"
+#include "cs_field.h"
+#include "cs_field_pointer.h"
+#include "cs_math.h"
+#include "cs_mesh.h"
+#include "cs_mesh_quantities.h"
+#include "cs_parall.h"
+#include "cs_physical_constants.h"
+#include "cs_physical_model.h"
+#include "cs_prototypes.h"
+#include "cs_turbulence_model.h"
+#include "cs_velocity_pressure.h"
+
+/*----------------------------------------------------------------------------
+ * Header for the current file
+ *----------------------------------------------------------------------------*/
+
+#include "cs_solve_navier_stokes.h"
+
+/*----------------------------------------------------------------------------*/
+
+BEGIN_C_DECLS
+
+/*=============================================================================
+ * Additional doxygen documentation
+ *============================================================================*/
+
+/*! \cond DOXYGEN_SHOULD_SKIP_THIS */
+
+/*============================================================================
+ * Type and macro definitions
+ *============================================================================*/
+
+/*============================================================================
+ * Static global variables
+ *============================================================================*/
+
+/*============================================================================
+ * Prototypes for Fortran functions and variables.
+ *============================================================================*/
+
+/*============================================================================
+ * Prototypes for functions intended for use only by Fortran wrappers.
+ * (descriptions follow, with function bodies).
+ *============================================================================*/
+
+/*============================================================================
+ * Fortran function prototypes for subroutines from field.f90.
+ *============================================================================*/
+
+void
+cs_f_navier_stokes_total_pressure(void);
+
+/*============================================================================
+ * Prototypes for functions intended for use only by Fortran wrappers.
+ * (descriptions follow, with function bodies).
+ *============================================================================*/
+
+/*============================================================================
+ * Private function definitions
+ *============================================================================*/
+
+/*============================================================================
+ * Fortran wrapper function definitions
+ *============================================================================*/
+
+void
+cs_f_navier_stokes_total_pressure(void)
+{
+  cs_solve_navier_stokes_update_total_pressure(cs_glob_mesh,
+                                               cs_glob_mesh_quantities,
+                                               cs_glob_fluid_properties);
+}
+
+/*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
+
+/*============================================================================
+ * Public function definitions
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Update total pressure (defined as a post-processed property).
+ *
+ * For the compressible module, the solved pressure is already
+ * the total pressure.
+ *
+ * Note: for Eddy Viscosity Models, the TKE may be included in the
+ * solved pressure.
+ *
+ * \param[in]     m   pointer to mesh structure
+ * \param[in]     mq  pointer to mesh quantities structure
+ * \param[in]     fp  pointer to fluid properties structure
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_solve_navier_stokes_update_total_pressure(const cs_mesh_t              *m,
+                                             const cs_mesh_quantities_t   *mq,
+                                             const cs_fluid_properties_t  *fp)
+{
+  /* TODO: use a function pointer here to adapt to different cases */
+
+  cs_field_t *f = cs_field_by_name_try("total_pressure");
+
+  if ((CS_F_(p) == NULL) || (f == NULL))
+    return;
+
+  const cs_lnum_t n_cells = m->n_cells;
+
+  const cs_real_3_t *cell_cen = (const cs_real_3_t *)mq->cell_cen;
+  const cs_real_t *gxyz = cs_get_glob_physical_constants()->gravity;
+  const cs_real_t *xyzp0 = fp->xyzp0;
+  const cs_real_t p0 = fp->p0, pred0 = fp->pred0, ro0 = fp->ro0;
+
+  cs_real_t *cpro_prtot = f->val;
+  const cs_real_t *cvar_pr = CS_F_(p)->val;
+
+  const cs_real_3_t *cpro_momst = NULL;
+
+  if (cs_glob_atmo_option->sedimentation_model != 0)
+    cpro_momst
+      = (const cs_real_3_t *)cs_field_by_name("momentum_source_terms")->val;
+
+  /* Update cell values */
+
+# pragma omp parallel if (n_cells > CS_THR_MIN)
+  {
+    cs_lnum_t s_id, e_id;
+    cs_parall_thread_range(n_cells, sizeof(cs_real_t), &s_id, &e_id);
+
+    if (cpro_momst == NULL) {
+      for (cs_lnum_t c_id = s_id; c_id < e_id; c_id++) {
+        cpro_prtot[c_id] =   cvar_pr[c_id]
+                           + ro0 * cs_math_3_distance_dot_product(xyzp0,
+                                                                  cell_cen[c_id],
+                                                                  gxyz)
+                           + p0 - pred0;
+      }
+    }
+    else {
+      for (cs_lnum_t c_id = s_id; c_id < e_id; c_id++)
+        cpro_prtot[c_id] =   cvar_pr[c_id]
+                           + ro0 * cs_math_3_distance_dot_product(xyzp0,
+                                                                  cell_cen[c_id],
+                                                                  gxyz)
+                           + p0 - pred0
+                           - cs_math_3_distance_dot_product(xyzp0,
+                                                            cell_cen[c_id],
+                                                            cpro_momst[c_id]);
+    }
+
+    /* For Eddy Viscosity Models, "2/3 rho k"
+       is included in the solved pressure */
+
+    if (  (   cs_glob_turb_model->itytur == 2
+           || cs_glob_turb_model->itytur == 5
+           || cs_glob_turb_model->iturb == CS_TURB_K_OMEGA)
+        && cs_glob_turb_rans_model->igrhok != 1) {
+
+      const cs_real_t *cvar_k = CS_F_(k)->val;
+      const cs_real_t *cpro_rho = CS_F_(rho)->val;
+
+      for (cs_lnum_t c_id = s_id; c_id < e_id; c_id++)
+        cpro_prtot[c_id] -= 2.0/3 * cpro_rho[c_id]*cvar_k[c_id];
+    }
+
+  } /* cell values update */
+}
+
+/*----------------------------------------------------------------------------*/
+
+END_C_DECLS

src/base/Makefile.am
@@ -175,6 +175,7 @@ cs_scalar_clipping.h \
 cs_search.h \
 cs_selector.h \
 cs_solve_equation.h \
+cs_solve_navier_stokes.h \
 cs_sort.h \
 cs_sort_partition.h \
 cs_syr_coupling.h \
@@ -359,6 +360,7 @@ cs_search.c \
 cs_selector.c \
 cs_selector_f2c.f90 \
 cs_solve_equation.c \
+cs_solve_navier_stokes.c \
 cs_sort.c \
 cs_sort_partition.c \
 cs_syr_coupling.c \

src/base/cs_solve_navier_stokes.h
@@ -1,0 +1,85 @@
+#ifndef __CS_SOLVE_NAVIER_STOKES_H__
+#define __CS_SOLVE_NAVIER_STOKES_H__
+
+/*============================================================================
+ * Solve the Navier-Stokes equations.
+ *============================================================================*/
+
+/*
+  This file is part of code_saturne, a general-purpose CFD tool.
+
+  Copyright (C) 1998-2023 EDF S.A.
+
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation; either version 2 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+  Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+ * Standard C library headers
+ *----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+ * Local headers
+ *----------------------------------------------------------------------------*/
+
+#include "cs_defs.h"
+
+/*----------------------------------------------------------------------------*/
+
+BEGIN_C_DECLS
+
+/*============================================================================
+ * Macro definitions
+ *============================================================================*/
+
+/*============================================================================
+ * Local type definitions
+ *============================================================================*/
+
+/*=============================================================================
+ * Global variables
+ *============================================================================*/
+
+/*============================================================================
+ * Public function prototypes
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Update total pressure (defined as a post-processed property).
+ *
+ * For the compressible module, the solved pressure is already
+ * the total pressure.
+ *
+ * Note: for Eddy Viscosity Models, the TKE may be included in the
+ * solved pressure.
+ *
+ * \param[in]     m   pointer to mesh structure
+ * \param[in]     mq  pointer to mesh quantities structure
+ * \param[in]     fp  pointer to fluid properties structure
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_solve_navier_stokes_update_total_pressure(const cs_mesh_t              *m,
+                                             const cs_mesh_quantities_t   *mq,
+                                             const cs_fluid_properties_t  *fp);
+
+/*----------------------------------------------------------------------------*/
+
+END_C_DECLS
+
+#endif /* __CS_SOLVE_NAVIER_STOKES_H__ */

src/base/inivar.f90
@@ -119,6 +119,12 @@ interface
     integer(c_int) :: n_errors
   end function cs_turbulence_init_clip_and_verify
 
+  subroutine cs_navstv_total_pressure() &
+   bind(C, name='cs_f_navier_stokes_total_pressure')
+   use,intrinsic :: iso_c_binding
+   implicit none
+ end subroutine cs_navstv_total_pressure
+
 end interface
 
 !===============================================================================
@@ -274,7 +280,7 @@ if (ippmod(icompf).lt.0) then
                     + pred0 - p0
     enddo
   elseif (isuite.eq.0.or.ileaux.eq.0) then
-    call navstv_total_pressure
+    call cs_navstv_total_pressure
   endif
 
 endif

src/base/navstv.f90
@@ -309,6 +309,14 @@ interface
 
   !=============================================================================
 
+  subroutine cs_navstv_total_pressure() &
+   bind(C, name='cs_f_navier_stokes_total_pressure')
+   use,intrinsic :: iso_c_binding
+   implicit none
+  end subroutine cs_navstv_total_pressure
+
+  !=============================================================================
+
 end interface
 
 !===============================================================================
@@ -1762,7 +1770,7 @@ endif
 ! NB: for Eddy Viscosity Models, TKE might be included in the solved pressure.
 
 if (ippmod(icompf).lt.0) then
-  call navstv_total_pressure
+  call cs_navstv_total_pressure
 endif
 
 !===============================================================================
@@ -2011,121 +2019,3 @@ if (iphydr.eq.2) deallocate(grdphd)
 return
 
 end subroutine
-
-!===============================================================================
-! Local functions
-!===============================================================================
-
-!===============================================================================
-! Function:
-! ---------
-
-!> \brief Update total pressure (defined as a post-processed property).
-!
-!> For the compressible module, the solved pressure is already
-!> the total pressure.
-!
-!> Note: for Eddy Viscosity Models, the TKE may be included in the
-!> solved pressure.
-
-!-------------------------------------------------------------------------------
-
-!-------------------------------------------------------------------------------
-! Arguments
-!______________________________________________________________________________.
-!  mode           name          role                                           !
-!______________________________________________________________________________!
-!_______________________________________________________________________________
-
-
-subroutine navstv_total_pressure
-
-!===============================================================================
-
-!===============================================================================
-! Module files
-!===============================================================================
-
-use paramx
-use atincl, only: iatmst, imomst
-use numvar
-use cstphy
-use cstnum
-use optcal
-use pointe
-use parall
-use paramx, only: isymet
-use ppincl
-use mesh
-use ptrglo
-use field
-
-!===============================================================================
-
-implicit none
-
-! Local variables
-
-integer          iel
-double precision xxp0 , xyp0 , xzp0
-
-double precision, dimension(:), pointer :: cpro_rho
-double precision, dimension(:,:), pointer :: cpro_momst
-double precision, dimension(:), pointer :: cvar_pr
-double precision, dimension(:), pointer :: cpro_prtot
-double precision, dimension(:), pointer :: cvara_k
-
-!===============================================================================
-! 0. Initialization
-!===============================================================================
-
-if (ipr.lt.1 .or. iprtot.lt.0) return
-
-call field_get_val_s(iprtot, cpro_prtot)
-call field_get_val_s(ivarfl(ipr), cvar_pr)
-
-xxp0   = xyzp0(1)
-xyp0   = xyzp0(2)
-xzp0   = xyzp0(3)
-
-if (iatmst.eq.0) then
-  do iel=1,ncel
-    cpro_prtot(iel) =  cvar_pr(iel)                        &
-                     + ro0*(  gx*(xyzcen(1,iel)-xxp0)      &
-                            + gy*(xyzcen(2,iel)-xyp0)      &
-                            + gz*(xyzcen(3,iel)-xzp0) )    &
-                     + p0 - pred0
-  enddo
-else
-  call field_get_val_v(imomst, cpro_momst)
-
-  do iel=1,ncel
-    cpro_prtot(iel) =   cvar_pr(iel)                             &
-                      + ro0*(  gx*(xyzcen(1,iel)-xxp0)           &
-                             + gy*(xyzcen(2,iel)-xyp0)           &
-                             + gz*(xyzcen(3,iel)-xzp0))          &
-                      + p0 - pred0                               &
-                      - cpro_momst(1,iel)*(xyzcen(1,iel)-xxp0)   &
-                      - cpro_momst(2,iel)*(xyzcen(2,iel)-xyp0)   &
-                      - cpro_momst(3,iel)*(xyzcen(3,iel)-xzp0)
-  enddo
-endif
-
-! For Eddy Viscosity Models, "2/3 rho k" is included in the solved pressure
-if ((itytur.eq.2 .or. itytur.eq.5 .or. iturb.eq.60).and. igrhok.ne.1) then
-  call field_get_val_s(ivarfl(ik), cvara_k)
-  call field_get_val_s(icrom, cpro_rho)
-  do iel = 1, ncel
-    cpro_prtot(iel) =   cpro_prtot(iel)                              &
-                      - 2.d0 / 3.d0 * cpro_rho(iel) * cvara_k(iel)
-  enddo
-endif
-
-!----
-! End
-!----
-
-return
-
-end subroutine navstv_total_pressure
-

commit 4847652c010e3b73b49656e3056cf2f6dca72fb3

[Atmo] fix bug in Fortran C translation (introduced in df4b9f21, 2023-05-25)

src/base/cs_solve_navier_stokes.c
@@ -166,7 +166,7 @@ cs_solve_navier_stokes_update_total_pressure(const cs_mesh_t              *m,
 
   const cs_real_3_t *cpro_momst = NULL;
 
-  if (cs_glob_atmo_option->sedimentation_model != 0)
+  if (cs_glob_atmo_option->open_bcs_treatment != 0)
     cpro_momst
       = (const cs_real_3_t *)cs_field_by_name("momentum_source_terms")->val;
 