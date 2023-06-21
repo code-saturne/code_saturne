code-saturne/code_saturne

commit 6557954ff68b88dc5f219a3d74ba59283337e7c9

Avoid deadlock when number of cells per rank is too low.
In this case, a more robust SFC partitioner is used. The code
should abort on a "clean" error after that as long as this
case is not handled through reducing the communicator size.

src/mesh/cs_partition.c
@@ -3119,6 +3119,14 @@ cs_partition(cs_mesh_t             *mesh,
   int n_part_ranks = cs_glob_n_ranks / _part_rank_step[stage];
   cs_partition_algorithm_t _algorithm = _select_algorithm(stage);
 
+  /* In quasi degenerate case, avoid using graph-based library, which
+     can deadlock */
+
+  if (mesh->n_cells < cs_glob_n_ranks
+      && (   _algorithm >= CS_PARTITION_SCOTCH
+          && _algorithm < CS_PARTITION_BLOCK))
+    _algorithm = CS_PARTITION_SFC_MORTON_BOX;
+
   bool write_output = false;
   int  n_extra_partitions = 0;
 

commit 7204c460defb626a2248f757867058a8048236b3

Fix partitioning option selection, broken by commit 6557954f (2023-04-02).
This fixes GitHub issue #115.

src/mesh/cs_partition.c
@@ -3122,7 +3122,7 @@ cs_partition(cs_mesh_t             *mesh,
   /* In quasi degenerate case, avoid using graph-based library, which
      can deadlock */
 
-  if (mesh->n_cells < cs_glob_n_ranks
+  if (mesh->n_g_cells < (cs_gnum_t)(cs_glob_n_ranks*2)
       && (   _algorithm >= CS_PARTITION_SCOTCH
           && _algorithm < CS_PARTITION_BLOCK))
     _algorithm = CS_PARTITION_SFC_MORTON_BOX;