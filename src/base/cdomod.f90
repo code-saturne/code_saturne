code-saturne/code_saturne

commit ab88902ff8b579aed692e707258cb7bdb2dd207f

Major: Change the default way to activate CDO schemes
The deprecated way is kept in order to not break user settings.
The advantage of the new way is to be independant of the cs_domain_t
structure which is a high-level structure.

This commit is cherry-picked from the master branch (commit ae56d061)

src/cdo/cs_param_cdo.c
@@ -1,0 +1,157 @@
+/*============================================================================
+ * High-level metadata related to CDO/HHO schemes
+ *============================================================================*/
+
+/*
+  This file is part of code_saturne, a general-purpose CFD tool.
+
+  Copyright (C) 1998-2023 EDF S.A.
+
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation; either version 2 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+  Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/*----------------------------------------------------------------------------*/
+
+#include "cs_defs.h"
+
+/*----------------------------------------------------------------------------
+ * Standard C library headers
+ *----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+ *  Local headers
+ *----------------------------------------------------------------------------*/
+
+#include "cs_log.h"
+
+/*----------------------------------------------------------------------------
+ *  Header for the current file
+ *----------------------------------------------------------------------------*/
+
+#include "cs_param_cdo.h"
+
+/*----------------------------------------------------------------------------*/
+
+BEGIN_C_DECLS
+
+/*! \cond DOXYGEN_SHOULD_SKIP_THIS */
+
+/*=============================================================================
+ * Local Macro Definitions
+ *============================================================================*/
+
+/*============================================================================
+ * Local structure definitions
+ *============================================================================*/
+
+/*=============================================================================
+ * Local Macro definitions
+ *============================================================================*/
+
+/*============================================================================
+ * Static global variables
+ *============================================================================*/
+
+cs_param_cdo_mode_t  cs_glob_param_cdo_mode = CS_PARAM_CDO_MODE_OFF;
+
+/*============================================================================
+ * Prototypes for functions intended for use only by Fortran wrappers.
+ * (descriptions follow, with function bodies).
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------
+ * Set the CDO mode in the Fortran part
+ *
+ * parameters:
+ *   mode <-- -1: no CDO, 1: with CDO, 2: CDO only
+ *----------------------------------------------------------------------------*/
+
+extern void
+cs_f_set_cdo_mode(int  mode);
+
+/*============================================================================
+ * Private function definitions
+ *============================================================================*/
+
+
+/*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
+
+/*=============================================================================
+ * Public function definitions
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Set the global variable storing the mode of activation to apply to
+ *        CDO/HHO schemes. Deprecated way to set the CDO mode.
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_param_cdo_mode_set(cs_param_cdo_mode_t   mode)
+{
+  cs_glob_param_cdo_mode = mode;
+  cs_f_set_cdo_mode(mode);  /* Set the CDO mode in the FORTRAN part */
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Get the mode of activation for the CDO/HHO schemes.
+ *
+ * \return the mode of activation for the CDO/HHO module
+ */
+/*----------------------------------------------------------------------------*/
+
+cs_param_cdo_mode_t
+cs_param_cdo_mode_get(void)
+{
+  return cs_glob_param_cdo_mode;
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Print a welcome message indicating what is the current CDO status
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_param_cdo_log(void)
+{
+  switch (cs_glob_param_cdo_mode) {
+
+  case CS_PARAM_CDO_MODE_ONLY:
+    cs_log_printf(CS_LOG_DEFAULT,
+                  "\n -msg- CDO/HHO module is activated *** Experimental ***"
+                  "\n -msg- CDO/HHO module is in a stand-alone mode\n");
+    break;
+
+  case CS_PARAM_CDO_MODE_WITH_FV:
+    cs_log_printf(CS_LOG_DEFAULT,
+                  "\n -msg- CDO/HHO module is activated *** Experimental ***"
+                  "\n -msg- CDO/HHO module with FV schemes mode\n");
+    break;
+
+  default:
+  case CS_PARAM_CDO_MODE_OFF:
+    cs_log_printf(CS_LOG_DEFAULT,
+                  "\n -msg- CDO/HHO module is not activated\n");
+    break;
+
+  } /* Switch on CDO mode */
+}
+
+/*----------------------------------------------------------------------------*/
+
+END_C_DECLS

src/apps/cs_solver.c
@@ -467,7 +467,7 @@ _run(void)
 
       if (cs_user_solver_set() == 0) {
 
-        if (cs_domain_get_cdo_mode(cs_glob_domain) == CS_DOMAIN_CDO_MODE_ONLY) {
+        if (cs_glob_param_cdo_mode == CS_PARAM_CDO_MODE_ONLY) {
 
           /* Only C language is called within CDO */
 
@@ -787,4 +787,3 @@ main(int    argc,
 }
 
 /*----------------------------------------------------------------------------*/
-

src/base/cs_ale.c
@@ -1522,7 +1522,7 @@ cs_ale_activate(void)
 
   cs_ale_active = true;
 
-  cs_domain_set_cdo_mode(cs_glob_domain, CS_DOMAIN_CDO_MODE_WITH_FV);
+  cs_param_cdo_mode_set(CS_PARAM_CDO_MODE_WITH_FV);
 
   cs_equation_t  *eq =
     cs_equation_add("mesh_velocity", /* equation name */

src/base/cs_parameters_check.c
@@ -46,7 +46,6 @@
 #include "cs_1d_wall_thermal.h"
 #include "cs_ale.h"
 #include "cs_base.h"
-#include "cs_domain.h"
 #include "cs_field.h"
 #include "cs_field_default.h"
 #include "cs_field_pointer.h"
@@ -821,8 +820,7 @@ cs_parameters_check(void)
   const int restart_file_key_id = cs_field_key_id("restart_file");
   const int key_limiter = cs_field_key_id("limiter_choice");
 
-  const cs_domain_t  *domain = cs_glob_domain;
-  if (cs_domain_get_cdo_mode(domain) == CS_DOMAIN_CDO_MODE_ONLY)
+  if (cs_glob_param_cdo_mode == CS_PARAM_CDO_MODE_ONLY)
     return; /* Avoid the detection of false setting errors when using
                CDO schemes */
 

src/base/cs_pressure_correction.c
@@ -2963,7 +2963,7 @@ cs_pressure_correction_cdo_activate(void)
   else
     prcdo = cs_pressure_correction_cdo;
 
-  cs_domain_set_cdo_mode(cs_glob_domain, CS_DOMAIN_CDO_MODE_WITH_FV);
+  cs_param_cdo_mode_set(CS_PARAM_CDO_MODE_WITH_FV);
 
   cs_equation_t  *eq =
     cs_equation_add("pressure_increment", /* equation name */

src/cdo/Makefile.am
@@ -207,6 +207,7 @@ cs_maxwell.c \
 cs_mesh_deform.c \
 cs_navsto_coupling.c \
 cs_navsto_system.c \
+cs_param_cdo.c \
 cs_property.c \
 cs_quadrature.c \
 cs_reco.c \

src/cdo/cs_cdo_main.c
@@ -302,7 +302,7 @@ _compute_unsteady_user_equations(cs_domain_t   *domain,
 static void
 _solve_steady_state_domain(cs_domain_t  *domain)
 {
-  if (domain->cdo_context->mode == CS_DOMAIN_CDO_MODE_ONLY) {
+  if (cs_glob_param_cdo_mode == CS_PARAM_CDO_MODE_ONLY) {
 
     /* Otherwise log is called from the FORTRAN part */
 
@@ -683,20 +683,20 @@ cs_f_cdo_solve_unsteady_state_domain(void)
 
 /*----------------------------------------------------------------------------*/
 /*!
- * \brief  Initialize the computational domain when CDO/HHO schemes are
- *         activated and cs_user_model() has been called.
- *         At this stage of the settings, mesh quantities and adjacencies are
- *         not defined. Only the major moddeling options are set. The related
- *         equations and main properties have been added.
+ * \brief Initialize the computational domain when CDO/HHO schemes are
+ *        activated and cs_user_model() has been called.
+ *        At this stage of the settings, mesh quantities and adjacencies are
+ *        not defined. Only the major moddeling options are set. The related
+ *        equations and main properties have been added.
  *
- * \param[in, out]  domain    pointer to a cs_domain_t structure
+ * \param[in, out] domain    pointer to a cs_domain_t structure
  */
 /*----------------------------------------------------------------------------*/
 
 void
 cs_cdo_initialize_setup(cs_domain_t   *domain)
 {
-  if (cs_domain_get_cdo_mode(domain) == CS_DOMAIN_CDO_MODE_OFF)
+  if (cs_glob_param_cdo_mode == CS_PARAM_CDO_MODE_OFF)
     return;
 
   /* Timer statistics */
@@ -708,7 +708,7 @@ cs_cdo_initialize_setup(cs_domain_t   *domain)
 
   /* Store the fact that the CDO/HHO module is activated */
 
-  cs_domain_cdo_log(domain);
+  cs_param_cdo_log();
 
   /* A property can be called easily from everywhere:
    * cs_property_by_name("time_step")
@@ -749,14 +749,14 @@ cs_cdo_initialize_setup(cs_domain_t   *domain)
 
 /*----------------------------------------------------------------------------*/
 /*!
- * \brief  Build additional connectivities and quantities when CDO/HHO schemes
- *         are activated.
- *         Finalize the setup and from the settings, define the structures
- *         related to equations and modules
+ * \brief Build additional connectivities and quantities when CDO/HHO schemes
+ *        are activated.
+ *        Finalize the setup and from the settings, define the structures
+ *        related to equations and modules
  *
- * \param[in, out]  domain   pointer to a cs_domain_t structure
- * \param[in, out]  m        pointer to a cs_mesh_t struct.
- * \param[in]       mq       pointer to a cs_quantities_t struct.
+ * \param[in, out] domain   pointer to a cs_domain_t structure
+ * \param[in, out] m        pointer to a cs_mesh_t struct.
+ * \param[in]      mq       pointer to a cs_quantities_t struct.
  */
 /*----------------------------------------------------------------------------*/
 
@@ -772,7 +772,7 @@ cs_cdo_initialize_structures(cs_domain_t           *domain,
   domain->mesh = m;
   domain->mesh_quantities = mq;
 
-  if (cs_domain_get_cdo_mode(domain) == CS_DOMAIN_CDO_MODE_OFF)
+  if (cs_glob_param_cdo_mode == CS_PARAM_CDO_MODE_OFF)
     return;
 
   cs_timer_t  t0 = cs_timer_time();
@@ -888,17 +888,17 @@ cs_cdo_initialize_structures(cs_domain_t           *domain,
 
 /*----------------------------------------------------------------------------*/
 /*!
- * \brief   Free all structures allocated during the resolution of CDO/HHO
- *          schemes
+ * \brief Free all structures allocated during the resolution of CDO/HHO
+ *        schemes
  *
- * \param[in, out]  domain   pointer to a cs_domain_t structure
+ * \param[in, out] domain   pointer to a cs_domain_t structure
  */
 /*----------------------------------------------------------------------------*/
 
 void
 cs_cdo_finalize(cs_domain_t    *domain)
 {
-  if (cs_domain_get_cdo_mode(domain) == CS_DOMAIN_CDO_MODE_OFF)
+  if (cs_glob_param_cdo_mode == CS_PARAM_CDO_MODE_OFF)
     return;
 
   /* Timer statistics */
@@ -984,7 +984,7 @@ cs_cdo_finalize(cs_domain_t    *domain)
 
   /* Set flag to OFF */
 
-  cs_domain_set_cdo_mode(domain, CS_DOMAIN_CDO_MODE_OFF);
+  cs_param_cdo_mode_set(CS_PARAM_CDO_MODE_OFF);
 
   cs_log_printf(CS_LOG_DEFAULT,
                 "\n  Finalize and free CDO-related structures.\n");
@@ -1002,12 +1002,12 @@ cs_cdo_finalize(cs_domain_t    *domain)
 
 /*----------------------------------------------------------------------------*/
 /*!
- * \brief  Check if CDO has been initialized.
+ * \brief Check if CDO has been initialized.
  *
- * \param[in, out]  setup       indicator if setup has been initialized,
- *                              or NULL if not queried
- * \param[in, out]  structures  indicator if structures have been initialized,
- *                              or NULL if not queried
+ * \param[in, out] setup       indicator if setup has been initialized,
+ *                             or NULL if not queried
+ * \param[in, out] structures  indicator if structures have been initialized,
+ *                             or NULL if not queried
  */
 /*----------------------------------------------------------------------------*/
 
@@ -1024,20 +1024,19 @@ cs_cdo_is_initialized(bool  *setup,
 
 /*----------------------------------------------------------------------------*/
 /*!
- * \brief  Main program for running a simulation with CDO kernel
+ * \brief Main program for running a simulation with CDO kernel
  *
- * \param[in, out]  domain   pointer to a cs_domain_t structure
+ * \param[in, out] domain   pointer to a cs_domain_t structure
  */
 /*----------------------------------------------------------------------------*/
 
 void
 cs_cdo_main(cs_domain_t   *domain)
 {
-  if (cs_domain_get_cdo_mode(domain) == CS_DOMAIN_CDO_MODE_OFF)
+  if (cs_glob_param_cdo_mode == CS_PARAM_CDO_MODE_OFF)
     return;
   if (cs_equation_get_n_equations() < 1) {
-    cs_log_printf(CS_LOG_DEFAULT,
-                  "\n  No equation to solve. Immediate exit\n");
+    cs_log_printf(CS_LOG_DEFAULT, "\n  No equation to solve. Immediate exit\n");
     return;
   }
 

src/cdo/cs_cdo_main.h
@@ -54,13 +54,13 @@ BEGIN_C_DECLS
 
 /*----------------------------------------------------------------------------*/
 /*!
- * \brief  Initialize the computational domain when CDO/HHO schemes are
- *         activated and cs_user_model() has been called
- *         At this stage of the settings, mesh quantities and adjacencies are
- *         not defined. Only the major moddeling options are set. The related
- *         equations and main properties have been added.
+ * \brief Initialize the computational domain when CDO/HHO schemes are
+ *        activated and cs_user_model() has been called
+ *        At this stage of the settings, mesh quantities and adjacencies are
+ *        not defined. Only the major moddeling options are set. The related
+ *        equations and main properties have been added.
  *
- * \param[in, out]  domain    pointer to a cs_domain_t structure
+ * \param[in, out] domain    pointer to a cs_domain_t structure
  */
 /*----------------------------------------------------------------------------*/
 
@@ -69,14 +69,14 @@ cs_cdo_initialize_setup(cs_domain_t   *domain);
 
 /*----------------------------------------------------------------------------*/
 /*!
- * \brief  Build additional connectivities and quantities when CDO/HHO schemes
- *         are activated.
- *         Finalize the setup and from the settings, define the structures
- *         related to equations and modules
+ * \brief Build additional connectivities and quantities when CDO/HHO schemes
+ *        are activated.
+ *        Finalize the setup and from the settings, define the structures
+ *        related to equations and modules
  *
- * \param[in, out]  domain   pointer to a cs_domain_t structure
- * \param[in, out]  m        pointer to a cs_mesh_t struct.
- * \param[in]       mq       pointer to a cs_quantities_t struct.
+ * \param[in, out] domain   pointer to a cs_domain_t structure
+ * \param[in, out] m        pointer to a cs_mesh_t struct.
+ * \param[in]      mq       pointer to a cs_quantities_t struct.
  */
 /*----------------------------------------------------------------------------*/
 
@@ -87,10 +87,10 @@ cs_cdo_initialize_structures(cs_domain_t           *domain,
 
 /*----------------------------------------------------------------------------*/
 /*!
- * \brief   Free all structures allocated during the resolution of CDO/HHO
- *          schemes
+ * \brief Free all structures allocated during the resolution of CDO/HHO
+ *        schemes
  *
- * \param[in, out]  domain   pointer to a cs_domain_t structure
+ * \param[in, out] domain   pointer to a cs_domain_t structure
  */
 /*----------------------------------------------------------------------------*/
 
@@ -99,12 +99,12 @@ cs_cdo_finalize(cs_domain_t    *domain);
 
 /*----------------------------------------------------------------------------*/
 /*!
- * \brief  Check if CDO has been initialized.
+ * \brief Check if CDO has been initialized.
  *
- * \param[in, out]  setup       indicator if setup has been initialized,
- *                              or NULL if not queried
- * \param[in, out]  structures  indicator if structures have been initialized,
- *                              or NULL if not queried
+ * \param[in, out] setup       indicator if setup has been initialized,
+ *                             or NULL if not queried
+ * \param[in, out] structures  indicator if structures have been initialized,
+ *                             or NULL if not queried
  */
 /*----------------------------------------------------------------------------*/
 
@@ -114,9 +114,9 @@ cs_cdo_is_initialized(bool  *setup,
 
 /*----------------------------------------------------------------------------*/
 /*!
- * \brief  Main program for running a simulation with the CDO kernel
+ * \brief Main program for running a simulation with the CDO kernel
  *
- * \param[in, out]  domain   pointer to a cs_domain_t structure
+ * \param[in, out] domain   pointer to a cs_domain_t structure
  */
 /*----------------------------------------------------------------------------*/
 

commit 0f4e49357d438ea36a9abfdde7de30666f473412

Fix call of cs_user_finalize_setup without CDO, broken by ab88902f (2023-06-01).
Cherry picked from commit f5f9aabc on master.

src/base/cdomod.f90
@@ -40,7 +40,7 @@ module cdomod
   !> Activated (=1 or =2) or not activated (=0)
   !> If icdo=1 (CDO and FV at the same time)
   !> If icdo=2 (CDO only)
-  integer, save :: icdo
+  integer(c_int), pointer, save :: icdo
 
   interface
 
@@ -97,6 +97,15 @@ module cdomod
       character(kind=c_char, len=1), dimension(*), intent(in) :: eqname
     end subroutine solve_cdo_equation
 
+    ! Interface to C function retrieving pointers to global CDO parameters
+
+    subroutine cs_f_cdo_get_pointers(icdo) &
+      bind(C, name='cs_f_cdo_get_pointers')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      type(c_ptr), intent(out) :: icdo
+    end subroutine cs_f_cdo_get_pointers
+
  end interface
 
   !=============================================================================
@@ -125,30 +134,25 @@ contains
 
   end subroutine solve_steady_state_cdo_equation
 
+  !> \brief Initialize Fortran CDO flag.
+  !> This maps Fortran pointers to global C structure members.
+
+  subroutine cdo_init
+
+    use, intrinsic :: iso_c_binding
+    implicit none
+
+    ! Local variables
+
+    type(c_ptr) :: c_icdo
+
+    call cs_f_cdo_get_pointers(c_icdo)
+
+    call c_f_pointer(c_icdo, icdo)
+
+  end subroutine cdo_init
+
+  !=============================================================================
+
 end module cdomod
 
-!=============================================================================
-
-subroutine cs_f_set_cdo_mode(icdoval)  &
-  bind(C, name='cs_f_set_cdo_mode')
-
-  !===========================================================================
-  ! Module files
-  !===========================================================================
-
-  use cdomod
-
-  !===========================================================================
-
-  implicit none
-
-  ! Arguments
-
-  integer(c_int), value :: icdoval
-
-  !===========================================================================
-
-  icdo = icdoval
-
-  return
-end subroutine cs_f_set_cdo_mode

src/base/iniini.f90
@@ -33,6 +33,7 @@ subroutine iniini
 
 use atincl
 use paramx
+use cdomod
 use cstnum
 use dimens
 use numvar
@@ -127,6 +128,7 @@ iporf = -1
 !===============================================================================
 
 call atmo_init
+call cdo_init
 call time_step_init
 call time_step_options_init
 call thermal_model_init

src/cdo/cs_param_cdo.c
@@ -72,19 +72,29 @@ cs_param_cdo_mode_t  cs_glob_param_cdo_mode = CS_PARAM_CDO_MODE_OFF;
  *============================================================================*/
 
 /*----------------------------------------------------------------------------
- * Set the CDO mode in the Fortran part
- *
- * parameters:
- *   mode <-- -1: no CDO, 1: with CDO, 2: CDO only
+ * Map Fortran pointers to global C structure members.
  *----------------------------------------------------------------------------*/
 
-extern void
-cs_f_set_cdo_mode(int  mode);
+void
+cs_f_cdo_get_pointers(int  **icdo);
 
 /*============================================================================
  * Private function definitions
  *============================================================================*/
 
+/*============================================================================
+ * Fortran wrapper function definitions
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------
+ * Map Fortran pointers to global C structure members.
+ *----------------------------------------------------------------------------*/
+
+void
+cs_f_cdo_get_pointers(int  **icdo)
+{
+  *icdo = &cs_glob_param_cdo_mode;
+}
 
 /*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
 
@@ -103,7 +113,6 @@ void
 cs_param_cdo_mode_set(cs_param_cdo_mode_t   mode)
 {
   cs_glob_param_cdo_mode = mode;
-  cs_f_set_cdo_mode(mode);  /* Set the CDO mode in the FORTRAN part */
 }
 
 /*----------------------------------------------------------------------------*/