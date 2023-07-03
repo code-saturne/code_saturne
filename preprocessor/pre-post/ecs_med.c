code-saturne/code_saturne

commit 288c8f52d4042cf902f59749ee6bd364e47edd7c

Preprocessor: add support for additional MED element types (esp. MED_OCTA12).

preprocessor/pre-post/ecs_med.c
@@ -94,72 +94,104 @@ ecs_fic_med_init_elt_liste_c[ECS_MED_NBR_TYP_ELT] = {
     { 1, 2, 3 }
   },
   {                                /* 3 */
+    MED_TRIA7,
+    ECS_ELT_TYP_FAC_TRIA,
+    ECS_MED_ORDER_PARABOLIC,
+    { 1, 2, 3 }
+  },
+  {                                /* 4 */
     MED_QUAD4,
     ECS_ELT_TYP_FAC_QUAD,
     ECS_MED_ORDER_LINEAR,
     { 1, 2, 3, 4 }
   },
-  {                                /* 4 */
+  {                                /* 5 */
     MED_QUAD8,
     ECS_ELT_TYP_FAC_QUAD,
     ECS_MED_ORDER_PARABOLIC,
     { 1, 2, 3, 4 }
   },
-  {                                /* 5 */
+  {                                /* 6 */
+    MED_QUAD9,
+    ECS_ELT_TYP_FAC_QUAD,
+    ECS_MED_ORDER_PARABOLIC,
+    { 1, 2, 3, 4 }
+  },
+  {                                /* 7 */
     MED_TETRA4,
     ECS_ELT_TYP_CEL_TETRA,
     ECS_MED_ORDER_LINEAR,
     { 1, 3, 2, 4 }
   },
-  {                                /* 6 */
+  {                                /* 8 */
     MED_TETRA10,
     ECS_ELT_TYP_CEL_TETRA,
     ECS_MED_ORDER_PARABOLIC,
     { 1, 3, 2, 4 }
   },
-  {                               /*  7 */
+  {                               /*  9 */
     MED_PYRA5,
     ECS_ELT_TYP_CEL_PYRAM,
     ECS_MED_ORDER_LINEAR,
     { 1, 4, 3, 2, 5 }
   },
-  {                               /*  8 */
+  {                               /* 10 */
     MED_PYRA13,
     ECS_ELT_TYP_CEL_PYRAM,
     ECS_MED_ORDER_PARABOLIC,
     { 1, 4, 3, 2, 5 }
   },
-  {                               /*  9 */
+  {                               /* 11 */
     MED_PENTA6,
     ECS_ELT_TYP_CEL_PRISM,
     ECS_MED_ORDER_LINEAR,
     { 1, 3, 2, 4, 6, 5 }
   },
-  {                               /* 10 */
+  {                               /* 12 */
     MED_PENTA15,
     ECS_ELT_TYP_CEL_PRISM,
     ECS_MED_ORDER_PARABOLIC,
     { 1, 3, 2, 4, 6, 5 }
   },
-  {                               /* 11 */
+  {                               /* 13 */
     MED_HEXA8,
     ECS_ELT_TYP_CEL_HEXA,
     ECS_MED_ORDER_LINEAR,
     { 1, 4, 3, 2, 5, 8, 7, 6 },
   },
-  {                               /* 12 */
+  {                               /* 14 */
     MED_HEXA20,
     ECS_ELT_TYP_CEL_HEXA,
     ECS_MED_ORDER_PARABOLIC,
     { 1, 4, 3, 2, 5, 8, 7, 6 },
   },
-  {                               /* 13 */
-    MED_POLYGON,
-    ECS_ELT_TYP_FAC_POLY,
-    ECS_MED_ORDER_LINEAR,
-    { 0 },
+  {                               /* 15 */
+    MED_HEXA27,
+    ECS_ELT_TYP_CEL_HEXA,
+    ECS_MED_ORDER_PARABOLIC,
+    { 1, 4, 3, 2, 5, 8, 7, 6 },
   },
-  {                               /* 14 */
+  {                               /* 16 */
+    MED_HEXA27,
+    ECS_ELT_TYP_CEL_HEXA,
+    ECS_MED_ORDER_PARABOLIC,
+    { 1, 4, 3, 2, 5, 8, 7, 6 },
+  },
+  {                               /* 17 */
+    MED_OCTA12,
+    ECS_ELT_TYP_CEL_POLY,
+    ECS_MED_ORDER_LINEAR,
+    { 1, 2, 3, 4, 5, 6, 1,
+      7, 12, 11, 10, 9, 8, 7,
+      1, 7, 8, 2, 1,
+      2, 8, 9, 3, 2,
+      3, 9, 10, 4, 3,
+      4, 10, 11, 5, 4,
+      5, 11, 12, 6, 5,
+      6, 12, 7, 1, 6
+    },
+  },
+  {                               /* 18 */
     MED_POLYHEDRON,
     ECS_ELT_TYP_CEL_POLY,
     ECS_MED_ORDER_LINEAR,

preprocessor/pre-post/ecs_med_priv.h
@@ -124,8 +124,8 @@ extern "C" {
 #define ECS_MED_ORDER_LINEAR                               1
 #define ECS_MED_ORDER_PARABOLIC                            2
 
-#define ECS_MED_NBR_TYP_ELT                               14
-#define ECS_MED_NBR_MAX_SOM                                8
+#define ECS_MED_NBR_TYP_ELT                               18
+#define ECS_MED_NBR_MAX_SOM                               44
 
 /*============================================================================
  *                         Définitions d'énumerations

preprocessor/pre-post/ecs_pre_med.c
@@ -724,7 +724,14 @@ ecs_loc_pre_med__lit_maille(ecs_maillage_t   *maillage,
 
       if (typ_geo_med != MED_POLYGON && typ_geo_med != MED_POLYHEDRON) {
 
-        nbr_som_elt = ecs_fic_elt_typ_liste_c[typ_geo_ecs].nbr_som;
+        switch (typ_geo_med) {
+        case MED_OCTA12:
+          nbr_som_elt = 44; /* convert to polyhedron */
+          break;
+        default:
+          nbr_som_elt = ecs_fic_elt_typ_liste_c[typ_geo_ecs].nbr_som;
+          break;
+        }
 
         taille = elt_pos_som_ent[entmail_e][cpt_elt_ent[entmail_e]] - 1;
 

commit 76119d0b7a17826617623ab0a84f4c34dc9b570a

Preprocessor: fix read of MED polygons, broken by commit 288c8f52 (2022-10-26).

preprocessor/pre-post/ecs_med.c
@@ -129,7 +129,7 @@ ecs_fic_med_init_elt_liste_c[ECS_MED_NBR_TYP_ELT] = {
     ECS_MED_ORDER_PARABOLIC,
     { 1, 3, 2, 4 }
   },
-  {                               /*  9 */
+  {                                /* 9 */
     MED_PYRA5,
     ECS_ELT_TYP_CEL_PYRAM,
     ECS_MED_ORDER_LINEAR,
@@ -192,6 +192,18 @@ ecs_fic_med_init_elt_liste_c[ECS_MED_NBR_TYP_ELT] = {
     },
   },
   {                               /* 18 */
+    MED_POLYGON,
+    ECS_ELT_TYP_FAC_POLY,
+    ECS_MED_ORDER_LINEAR,
+    { 0 },
+  },
+  {                               /* 19 */
+    MED_POLYGON2,
+    ECS_ELT_TYP_FAC_POLY,
+    ECS_MED_ORDER_PARABOLIC,
+    { 0 },
+  },
+  {                               /* 20 */
     MED_POLYHEDRON,
     ECS_ELT_TYP_CEL_POLY,
     ECS_MED_ORDER_LINEAR,

preprocessor/pre-post/ecs_med_priv.h
@@ -124,7 +124,7 @@ extern "C" {
 #define ECS_MED_ORDER_LINEAR                               1
 #define ECS_MED_ORDER_PARABOLIC                            2
 
-#define ECS_MED_NBR_TYP_ELT                               18
+#define ECS_MED_NBR_TYP_ELT                               20
 #define ECS_MED_NBR_MAX_SOM                               44
 
 /*============================================================================

preprocessor/pre-post/ecs_pre_med.c
@@ -641,7 +641,7 @@ ecs_loc_pre_med__lit_maille(ecs_maillage_t   *maillage,
 
     if (nbr_ele_med > 0) { /* Special case for polygons and polyhedra */
 
-      if (typ_geo_med == MED_POLYGON)
+      if (typ_geo_med == MED_POLYGON || typ_geo_med == MED_POLYGON2)
         data_type = MED_INDEX_NODE;
       else if (typ_geo_med == MED_POLYHEDRON)
         data_type = MED_INDEX_FACE;
@@ -722,7 +722,9 @@ ecs_loc_pre_med__lit_maille(ecs_maillage_t   *maillage,
       /* Traitement des éléments "classiques" */
       /*--------------------------------------*/
 
-      if (typ_geo_med != MED_POLYGON && typ_geo_med != MED_POLYHEDRON) {
+      if (   typ_geo_med != MED_POLYGON
+          && typ_geo_med != MED_POLYGON2
+          && typ_geo_med != MED_POLYHEDRON) {
 
         switch (typ_geo_med) {
         case MED_OCTA12:
@@ -802,11 +804,16 @@ ecs_loc_pre_med__lit_maille(ecs_maillage_t   *maillage,
       /* Traitement des polygones */
       /*--------------------------*/
 
-      else if (typ_geo_med == MED_POLYGON) {
+      else if (   typ_geo_med == MED_POLYGON
+               || typ_geo_med == MED_POLYGON2) {
 
         ecs_int_t    ival;
         ecs_int_t    nbr_val_elt;
-        med_int    * index_med = NULL;
+        med_int     *index_med = NULL;
+
+        /* On ne garde que la première moité de la connectivité
+           dans le cas quadratique. */
+        med_int      d_taille = (typ_geo_med == MED_POLYGON2) ? 2 : 1;
 
         /* Taille du tableau des connectivites */
 
@@ -815,7 +822,7 @@ ecs_loc_pre_med__lit_maille(ecs_maillage_t   *maillage,
                                     MED_NO_DT,
                                     MED_NO_IT,
                                     typ_ent_med,
-                                    MED_POLYGON,
+                                    typ_geo_med,
                                     MED_CONNECTIVITY,
                                     MED_NODAL,
                                     &changement,
@@ -833,18 +840,19 @@ ecs_loc_pre_med__lit_maille(ecs_maillage_t   *maillage,
         taille = elt_pos_som_ent[entmail_e][cpt_elt_ent[entmail_e]] - 1;
 
         ECS_REALLOC(elt_val_som_ent[entmail_e],
-                    taille + (ecs_int_t)taille_med, ecs_int_t);
+                    taille + (ecs_int_t)taille_med/d_taille, ecs_int_t);
 
         ECS_MALLOC(index_med, (ecs_int_t)(nbr_ele_med + 1), med_int);
         ECS_MALLOC(connect_med, (ecs_int_t)taille_med, med_int);
 
         /* Lecture de la connectivité des polygones */
 
-        ret_med = MEDmeshPolygonRd(fic_maillage->fid,
+        ret_med = MEDmeshPolygon2Rd(fic_maillage->fid,
                                    nom_maillage_med,
                                    MED_NO_DT,
                                    MED_NO_IT,
                                    typ_ent_med,
+                                   typ_geo_med,
                                    MED_NODAL,
                                    index_med,
                                    connect_med);
@@ -861,9 +869,9 @@ ecs_loc_pre_med__lit_maille(ecs_maillage_t   *maillage,
 
         for (ielt = 0; ielt < nbr_elt; ielt++)
           elt_pos_som_ent[entmail_e][cpt_elt_ent[entmail_e] + ielt + 1]
-            = pos_elt + index_med[ielt + 1] - index_med[0];
+            = pos_elt + (index_med[ielt + 1] - index_med[0]) / d_taille;
 
-        nbr_val_elt = index_med[nbr_elt] - index_med[0];
+        nbr_val_elt = (index_med[nbr_elt] - index_med[0])/d_taille;
 
         for (ival = 0; ival < nbr_val_elt; ival++)
           elt_val_som_ent[entmail_e][pos_elt - 1 + ival]