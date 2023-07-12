code-saturne/code_saturne

commit 0184f41f8247441a447944a04d80205f3dc58889

[Lagr] Major commit: Add the extended scheme (dt>>T_L and grad(T_L)!=0, see Minier 2022) and add an option for P1 interpolation of the mean velocity and Lagragian time at the position of the particles
TBM

src/lagr/cs_lagr.c
@@ -87,7 +87,7 @@
 #include "cs_lagr_roughness.h"
 #include "cs_lagr_clogging.h"
 #include "cs_lagr_injection.h"
-#include "cs_lagr_gradients.h"
+#include "cs_lagr_aux_mean_fluid_quantities.h"
 #include "cs_lagr_car.h"
 #include "cs_lagr_coupling.h"
 #include "cs_lagr_new.h"
@@ -160,6 +160,8 @@ static cs_lagr_time_scheme_t _lagr_time_scheme
      .isttio = 0,
      .isuila = 1,
      .t_order = 0,
+     .extended_t_scheme = 0,
+     .interpol_field = 1,
      .ilapoi = 0,
      .iadded_mass = 0,
      .added_mass_const = 0};
@@ -366,7 +368,9 @@ static cs_lagr_extra_module_t _lagr_extra_module
      .cvar_omg = NULL,
      .cvar_rij = NULL,
      .grad_pr = NULL,
-     .grad_vel = NULL};
+     .grad_vel = NULL,
+     .lagr_time = NULL,
+     .grad_lagr_time = NULL};
 
 cs_lagr_extra_module_t *cs_glob_lagr_extra_module = &_lagr_extra_module;
 
@@ -730,6 +734,9 @@ _lagr_map_fields_default(void)
   _lagr_extra_module.pressure    = cs_field_by_name_try("pressure");
 
   _lagr_extra_module.luminance   = cs_field_by_name_try("luminance");
+
+  _lagr_extra_module.lagr_time = cs_field_by_name_try("lagr_time");
+
   if (cs_field_by_name_try("velocity_1") != NULL) {
     /* we are probably using NEPTUNE_CFD */
     _lagr_extra_module.vel         = cs_field_by_name_try("lagr_velocity");
@@ -1204,6 +1211,10 @@ cs_lagr_finalize(void)
   BFT_FREE(extra->grad_pr);
   if (extra->grad_vel != NULL)
     BFT_FREE(extra->grad_vel);
+
+  if (extra->grad_lagr_time != NULL)
+    BFT_FREE(extra->grad_lagr_time);
+
 }
 
 /*----------------------------------------------------------------------------*/
@@ -1712,10 +1723,15 @@ cs_lagr_solve_initialize(const cs_real_t  *dt)
   cs_lagr_extra_module_t *extra = cs_glob_lagr_extra_module;
   cs_lnum_t ncelet = cs_glob_mesh->n_cells_with_ghosts;
 
-  BFT_MALLOC(extra->grad_pr, ncelet, cs_real_3_t);
+  if ((    cs_glob_lagr_time_scheme->interpol_field != 0
+        || cs_glob_lagr_time_scheme->extended_t_scheme !=0)
+      && cs_glob_lagr_model->idistu == 1)
+      BFT_MALLOC(extra->grad_lagr_time, ncelet, cs_real_3_t);
+
   if (   cs_glob_lagr_model->modcpl > 0
-      || cs_glob_lagr_model->shape > 0)
-    BFT_MALLOC(extra->grad_vel, ncelet, cs_real_33_t);
+      || cs_glob_lagr_model->shape > 0
+      || cs_glob_lagr_time_scheme->interpol_field != 0)
+      BFT_MALLOC(extra->grad_vel, ncelet, cs_real_33_t);
 
   /* For frozen field:
      values at previous time step = values at current time step */
@@ -1749,10 +1765,6 @@ cs_lagr_solve_initialize(const cs_real_t  *dt)
 
   if (cs_glob_lagr_time_scheme->iilagr != CS_LAGR_OFF)
     cs_lagr_restart_read_p();
-
-  /* Compute gradients of current value fields */
-  if (cs_glob_lagr_time_scheme->iilagr == CS_LAGR_FROZEN_CONTINUOUS_PHASE)
-    cs_lagr_gradients(0, extra->grad_pr, extra->grad_vel);
 
   /* Read statistics restart data */
 
@@ -1852,7 +1864,8 @@ cs_lagr_solve_time_step(const int         itypfb[],
           cs_real_t surfb = surfbo[ifac];
           ustarmoy     = ustarmoy + surfb * _ustar;
           surftot      = surftot + surfb;
-          vislen[ifac] = visccf / _ustar; // FIXME to be coherent with wall fn: y/y+
+          vislen[ifac] = visccf / _ustar; // nu /u*
+          //FIXME to be coherent with wall fn: y/y+
           dtmp[0]      = dtmp[0] + 1.0;
 
         }
@@ -2095,14 +2108,34 @@ cs_lagr_solve_time_step(const int         itypfb[],
 
     }
 
-    /* Pressure and fluid velocity gradients
-       ------------------------------------- */
+    /* Compute the Lagrangian time */
+    /* Pressure fluid velocity and Lagrangian time  gradients
+       ----------------------------------------------------- */
 
     /* At the first time step we initialize particles to
        values at current time step and not at previous time step, because
        values at previous time step = initialization (zero gradients) */
 
-    if (cs_glob_lagr_time_scheme->iilagr != CS_LAGR_FROZEN_CONTINUOUS_PHASE) {
+    if (extra->itytur == 3 ) {
+      /* save previous value dor the kinetic energy */
+      for (cs_lnum_t cell_id = 0; cell_id < cs_glob_mesh->n_cells; cell_id++)
+        extra->cvar_k->vals[1][cell_id] = 0.5 *
+                                    (  extra->cvar_rij->vals[1][6*cell_id]
+                                     + extra->cvar_rij->vals[1][6*cell_id + 1]
+                                     + extra->cvar_rij->vals[1][6*cell_id + 2]);
+    }
+
+    /* First pass allocate and compute it */
+    if (extra->grad_pr == NULL) {
+      BFT_MALLOC(extra->grad_pr, cs_glob_mesh->n_cells_with_ghosts, cs_real_3_t);
+
+      cs_lagr_aux_mean_fluid_quantities(0,
+                                        extra->lagr_time,
+                                        extra->grad_pr,
+                                        extra->grad_vel,
+                                        extra->grad_lagr_time);
+    }
+    else if (cs_glob_lagr_time_scheme->iilagr != CS_LAGR_FROZEN_CONTINUOUS_PHASE) {
       mode = 1;
       if (ts->nt_cur == 1)
         mode = 0;
@@ -2114,7 +2147,11 @@ cs_lagr_solve_time_step(const int         itypfb[],
           BFT_REALLOC(extra->grad_vel, n_cells_ext, cs_real_33_t);
       }
 
-      cs_lagr_gradients(mode, extra->grad_pr, extra->grad_vel);
+      cs_lagr_aux_mean_fluid_quantities(mode,
+                                        extra->lagr_time,
+                                        extra->grad_pr,
+                                        extra->grad_vel,
+                                        extra->grad_lagr_time);
     }
 
     /* Particles progression
@@ -2132,17 +2169,21 @@ cs_lagr_solve_time_step(const int         itypfb[],
 
       cs_lnum_t nresnew = 0;
 
-      cs_real_t   *taup;
+      cs_real_t *taup;
       cs_real_33_t *bx;
       cs_real_3_t *tlag, *piil;
       BFT_MALLOC(taup, p_set->n_particles, cs_real_t);
       BFT_MALLOC(tlag, p_set->n_particles, cs_real_3_t);
-      BFT_MALLOC(piil, p_set->n_particles, cs_real_3_t);
+      BFT_MALLOC(piil, cs_glob_mesh->n_cells, cs_real_3_t);
       BFT_MALLOC(bx, p_set->n_particles, cs_real_33_t);
 
       cs_real_t *tsfext = NULL;
       if (cs_glob_lagr_time_scheme->iilagr == CS_LAGR_TWOWAY_COUPLING)
         BFT_MALLOC(tsfext, p_set->n_particles, cs_real_t);
+
+      cs_real_3_t *beta = NULL;
+      if (cs_glob_lagr_time_scheme->extended_t_scheme != 0)
+        BFT_MALLOC(beta, p_set->n_particles, cs_real_3_t);
 
       cs_real_t *cpgd1 = NULL, *cpgd2 = NULL, *cpght = NULL;
       if (   cs_glob_lagr_time_scheme->iilagr == CS_LAGR_TWOWAY_COUPLING
@@ -2180,7 +2221,11 @@ cs_lagr_solve_time_step(const int         itypfb[],
          at n+1 (with values at current time step) */
       if (   cs_glob_lagr_time_step->nor == 2
           && cs_glob_lagr_time_scheme->iilagr != CS_LAGR_FROZEN_CONTINUOUS_PHASE)
-        cs_lagr_gradients(0, extra->grad_pr, extra->grad_vel);
+        cs_lagr_aux_mean_fluid_quantities(0,
+                                          extra->lagr_time,
+                                          extra->grad_pr,
+                                          extra->grad_vel,
+                                          extra->grad_lagr_time);
 
       /* use fields at previous or current time step */
       if (cs_glob_lagr_time_step->nor == 1)
@@ -2197,7 +2242,8 @@ cs_lagr_solve_time_step(const int         itypfb[],
 
         for (cs_lnum_t ip = 0; ip < p_set->n_particles; ip++) {
 
-          cs_real_t *jbx1 = cs_lagr_particles_attr(p_set, ip, CS_LAGR_TURB_STATE_1);
+          cs_real_t *jbx1 = cs_lagr_particles_attr(p_set, ip,
+                                                   CS_LAGR_TURB_STATE_1);
 
           for (cs_lnum_t ii = 0; ii < 3; ii++) {
 
@@ -2216,8 +2262,10 @@ cs_lagr_solve_time_step(const int         itypfb[],
                   piil,
                   bx,
                   tempct,
+                  beta,
                   extra->grad_pr,
-                  extra->grad_vel);
+                  extra->grad_vel,
+                  extra->grad_lagr_time);
 
       /* Integration of SDEs: position, fluid and particle velocity */
 
@@ -2231,6 +2279,7 @@ cs_lagr_solve_time_step(const int         itypfb[],
                   (const cs_real_33_t *)extra->grad_vel,
                   terbru,
                   (const cs_real_t *)vislen,
+                  beta,
                   &nresnew);
 
       /* Integration of SDEs for orientation of spheroids without inertia */
@@ -2443,6 +2492,9 @@ cs_lagr_solve_time_step(const int         itypfb[],
 
       if (cs_glob_lagr_time_scheme->iilagr == CS_LAGR_TWOWAY_COUPLING)
         BFT_FREE(tsfext);
+
+      if (beta != NULL)
+        BFT_FREE(beta);
 
       if (   cs_glob_lagr_time_scheme->iilagr == CS_LAGR_TWOWAY_COUPLING
           && lagr_model->physical_model == CS_LAGR_PHYS_COAL

src/lagr/cs_lagr_aux_mean_fluid_quantities.c
@@ -1,5 +1,5 @@
 /*============================================================================
- * Methods for lagrangian gradients
+ * Methods for auxiliary mean fluid quantities (Lagrangian time and gradients)
  *============================================================================*/
 
 /*
@@ -25,7 +25,7 @@
 /*----------------------------------------------------------------------------*/
 
 /*============================================================================
- * Functions dealing with lagrangian gradients
+ * Functions dealing with auxiliary mean fluid quantities
  *============================================================================*/
 
 #include "cs_defs.h"
@@ -65,7 +65,7 @@
  *  Header for the current file
  *----------------------------------------------------------------------------*/
 
-#include "cs_lagr_gradients.h"
+#include "cs_lagr_aux_mean_fluid_quantities.h"
 
 /*----------------------------------------------------------------------------*/
 
@@ -85,21 +85,27 @@ BEGIN_C_DECLS
 
 /*----------------------------------------------------------------------------*/
 /*!
- * \brief Compute gradients.
+ * \brief Compute auxilary mean fluid quantities.
  *
+ *  - Lagragian time
  *  - gradient of total pressure
  *  - velocity gradient
+ *  - Lagragian time gradient
  *
- * \param[in]   time_id   0: current time, 1: previous
- * \param[out]  grad_pr   pressure gradient
- * \param[out]  grad_vel  velocity gradient
+ * \param[in]   time_id                0: current time, 1: previous
+ * \param[out]  lagr_time              Lagragian time scale
+ * \param[out]  grad_pr                pressure gradient
+ * \param[out]  grad_vel               velocity gradient
+ * \param[out]  grad_lagr_time         Lagrangian time gradient
  */
 /*----------------------------------------------------------------------------*/
 
 void
-cs_lagr_gradients(int            time_id,
-                  cs_real_3_t   *grad_pr,
-                  cs_real_33_t  *grad_vel)
+cs_lagr_aux_mean_fluid_quantities(int            time_id,
+                                  cs_field_t    *lagr_time,
+                                  cs_real_3_t   *grad_pr,
+                                  cs_real_33_t  *grad_vel,
+                                  cs_real_3_t   *grad_lagr_time)
 {
   cs_lnum_t n_cells_with_ghosts = cs_glob_mesh->n_cells_with_ghosts;
   cs_lnum_t n_cells = cs_glob_mesh->n_cells;
@@ -261,11 +267,114 @@ cs_lagr_gradients(int            time_id,
   /* Compute velocity gradient
      ========================= */
 
-  if (turb_disp_model || cs_glob_lagr_model->shape > 0) {
+  if (turb_disp_model || cs_glob_lagr_model->shape > 0
+      || cs_glob_lagr_time_scheme->interpol_field) {
     cs_field_gradient_vector(extra->vel,
                              time_id,
                              inc,
                              grad_vel);
+  }
+
+  /* Compute Lagrangian time gradient
+     ================================ */
+  if(cs_glob_lagr_model->idistu > 0) {
+
+    cs_real_t c0     = 3.5;
+
+    /* In case of Rotta model (ie LRR + Cr2 = 0) compute
+     * automatically the C0 constant */
+    if ((turb_model->iturb == CS_TURB_RIJ_EPSILON_LRR) &&
+        (CS_ABS(cs_turb_crij2) < 1.e-12))
+      c0 = (cs_turb_crij1-1.0)*2.0/3.0;
+
+    cs_real_t cl     = 1.0 / (0.5 + 0.75 * c0);
+
+    cs_real_t  *energi = NULL, *dissip = NULL;
+    BFT_MALLOC(energi, n_cells, cs_real_t);
+    BFT_MALLOC(dissip, n_cells, cs_real_t);
+
+    if (extra->itytur == 2 || extra->itytur == 4 || extra->iturb == 50) {
+
+      for (cs_lnum_t cell_id = 0; cell_id < n_cells; cell_id++) {
+        energi[cell_id] = extra->cvar_k->vals[time_id][cell_id];
+        dissip[cell_id] = extra->cvar_ep->vals[time_id][cell_id];
+      }
+
+    }
+    else if (extra->itytur == 3) {
+
+      for (cs_lnum_t cell_id = 0; cell_id < n_cells; cell_id++) {
+
+        energi[cell_id] = 0.5 * (  extra->cvar_rij->vals[time_id][6*cell_id]
+                                 + extra->cvar_rij->vals[time_id][6*cell_id+1]
+                                 + extra->cvar_rij->vals[time_id][6*cell_id+2]);
+        dissip[cell_id] = extra->cvar_ep->vals[time_id][cell_id];
+        extra->cvar_k->val[cell_id] = energi[cell_id] ;
+      }
+
+    }
+    else if (extra->iturb == 60) {
+
+      for (cs_lnum_t cell_id = 0; cell_id < n_cells; cell_id++) {
+        energi[cell_id] = extra->cvar_k->vals[time_id][cell_id];
+        dissip[cell_id] = extra->cmu * energi[cell_id]
+                                     * extra->cvar_omg->vals[time_id][cell_id];
+      }
+
+    }
+    else {
+
+      bft_error
+        (__FILE__, __LINE__, 0,
+         _("Lagrangian turbulent dispersion is not compatible with\n"
+           "the selected turbulence model.\n"
+           "\n"
+           "Turbulent dispersion is taken into account with idistu = %d\n"
+           " Activated turbulence model is %d, when only k-eps, LES, Rij-eps,\n"
+           " V2f or k-omega are handled."),
+         (int)cs_glob_lagr_model->idistu,
+         (int)extra->iturb);
+
+    }
+
+    /* Initialize cell Lagrangian time
+     *
+     * Note: the extended time scheme should
+     * compute grad(Tl*_i) = grad(Tl)/b_i - Tl grad(b_i)/b_i^2
+     *
+     * In the following, only the first term is kept
+     * to avoid computing grad(b_i) where no particles are present.
+     *
+     * The other possibility would be to compute b_i everywhere
+     * (taking <u_pi> from the statistic "correctly" initialized)
+     *
+     */
+
+    for (cs_lnum_t cell_id = 0; cell_id < n_cells; cell_id++) {
+      if (dissip[cell_id] > 0.0 && energi[cell_id] > 0.0) {
+
+        cs_real_t tl  = cl * energi[cell_id] / dissip[cell_id];
+        tl  = CS_MAX(tl, cs_math_epzero);
+
+        lagr_time->val[cell_id] = tl;
+      }
+      else {
+        for (cs_lnum_t cell_id = 0; cell_id < n_cells; cell_id++)
+          lagr_time->val[cell_id] = cs_math_epzero;
+      }
+
+    }
+    if (grad_lagr_time != NULL)
+      cs_field_gradient_scalar(lagr_time,
+                               time_id, /* use_previous_t */
+                               1, /* inc: not an increment */
+                               true, /* _recompute_cocg */
+                               grad_lagr_time);
+
+  }
+  else { // idistu == 0
+    for (cs_lnum_t cell_id = 0; cell_id < n_cells; cell_id++)
+      lagr_time->val[cell_id] = cs_math_epzero;
   }
 }
 

src/base/addfld.f90
@@ -594,6 +594,24 @@ if (itytur.eq.4 .and. iilagr.gt.0) then
   call field_set_key_int(iflid, keylog, 1)
 end if
 
+! Add TKE for DRSM model and Particle modelling
+if (itytur.eq.3 .and. iilagr.gt.0) then
+  ityloc = 1 ! variables defined on cells
+  itycat = FIELD_INTENSIVE + FIELD_PROPERTY
+  ! Warning, not ik or ivarfl(ik)
+  call field_find_or_create('k', itycat, ityloc, idim1, iflid)
+  call field_set_key_int(iflid, keylog, 1)
+end if
+
+! Add Lagrangian integral time for Lagrangian computation
+if (iilagr.gt.0) then
+  ityloc = 1 ! variables defined on cells
+  itycat = FIELD_INTENSIVE + FIELD_PROPERTY
+  call field_find_or_create('lagr_time', itycat, ityloc, idim1, iflid)
+  call field_set_key_int(iflid, keyvis, 1)
+  call field_set_key_int(iflid, keylog, 1)
+endif
+
 if (ippmod(iatmos).ge.0.and.compute_z_ground) then
   f_name  = 'z_ground'
   f_label = 'Z ground'

src/base/clptrg.f90
@@ -80,7 +80,7 @@
 !>                               - 9 free inlet/outlet
 !>                                 (input mass flux blocked to 0)
 !> \param[in,out] rcodcl        boundary condition values:
-!>                               - rcodcl(1) value of the dirichlet
+!>                               - rcodcl(1) value of the Dirichlet
 !>                               - rcodcl(2) value of the exterior exchange
 !>                                 coefficient (infinite if no exchange)
 !>                               - rcodcl(3) value flux density
@@ -162,6 +162,7 @@ integer          iuntur, f_id, iustar
 integer          nlogla, nsubla, iuiptn
 integer          kdflim
 integer          f_id_rough
+integer          f_id_tlag
 
 double precision rnx, rny, rnz
 double precision tx, ty, tz, txn, txn0, t2x, t2y, t2z
@@ -192,6 +193,7 @@ double precision fcofbf(6), fcofad(6), fcofbd(6)
 
 double precision rxx, rxy, rxz, ryy, ryz, rzz, rnnb
 double precision rttb, alpha_rnn, liqwt, totwt
+double precision c0, cl
 double precision cpp
 double precision sigmak, sigmae
 double precision coef_mom,coef_momm
@@ -207,6 +209,7 @@ double precision, dimension(:), allocatable :: bdlmo
 double precision, dimension(:), pointer :: cvar_k, bcfnns
 double precision, dimension(:,:), pointer :: cvar_rij
 double precision, dimension(:), pointer :: cvara_nusa
+double precision, dimension(:), pointer :: tlag
 
 double precision, dimension(:), pointer :: cvar_totwt, cvar_t, cpro_liqwt
 double precision, dimension(:), pointer :: bpro_rough_d
@@ -232,6 +235,8 @@ double precision, dimension(:), pointer :: coefa_fb, coefaf_fb
 double precision, dimension(:), pointer :: coefb_fb, coefbf_fb
 double precision, dimension(:), pointer :: coefa_nu, coefaf_nu
 double precision, dimension(:), pointer :: coefb_nu, coefbf_nu
+double precision, dimension(:), pointer :: coefa_tlag, coefaf_tlag
+double precision, dimension(:), pointer :: coefb_tlag, coefbf_tlag
 
 integer          ntlast , iaff
 data             ntlast , iaff /-1 , 0/
@@ -292,6 +297,13 @@ dlmo = 0.d0
 
 ! Alpha constant for a realisable BC for R12 with the SSG model
 alpha_rnn = 0.47d0
+
+if (iturb.eq.30 .and. abs(crij2).le.epzero .and. crij1.gt.1.d0) then
+  c0 = (crij1-1) * 2.0 / 3.0 ! depend on the lag model
+else
+  c0 = 3.5d0
+endif
+cl = 1.d0 / (0.5d0 + 0.75d0 * c0) ! see the different model
 
 ! pointers to y+ if saved
 
@@ -444,6 +456,23 @@ else
   coefb_nu => null()
   coefaf_nu => null()
   coefbf_nu => null()
+endif
+
+tlag => null()
+! --- Save Lagragian time scale if defined
+call field_get_id_try('lagr_time', f_id_tlag)
+
+if (f_id_tlag.ge.0) then
+  call field_get_val_s(f_id_tlag, tlag)
+  call field_get_coefa_s(f_id_tlag, coefa_tlag)
+  call field_get_coefaf_s(f_id_tlag, coefaf_tlag)
+  call field_get_coefb_s(f_id_tlag, coefb_tlag)
+  call field_get_coefbf_s(f_id_tlag, coefbf_tlag)
+else
+  coefa_tlag => null()
+  coefb_tlag => null()
+  coefaf_tlag => null()
+  coefbf_tlag => null()
 endif
 
 ! --- Physical quantities
@@ -1034,6 +1063,26 @@ do ifac = 1, nfabor
            coefb_ep(ifac), coefbf_ep(ifac),             &
            qimp          , hint )
 
+      !if defined set Dirichlet condition for the Lagrangian time scale
+      if (f_id_tlag.ge.0) then
+        if (iwallf.eq.0) then
+          ! No wall functions forced by user
+          pimp = 0.d0
+        else
+          ! Use of wall functions
+          if (iuntur.eq.1) then
+              pimp = cfnnk / (cfnne * uk) * cl / sqrcmu * xkappa * rough_d
+          else
+            pimp = 0.d0
+          endif
+        endif
+
+        call set_dirichlet_scalar &
+             ( coefa_tlag(ifac), coefaf_tlag(ifac),             &
+               coefb_tlag(ifac), coefbf_tlag(ifac),             &
+               pimp         , hint          , rinfin )
+      endif
+
       ! No diffusion reconstruction when using wall functions
       if (associated(cpro_diff_lim_eps)) cpro_diff_lim_eps(ifac) = 0.d0
 
@@ -1257,6 +1306,28 @@ do ifac = 1, nfabor
            coefb_ep(ifac), coefbf_ep(ifac),             &
            qimp          , hint )
 
+      if (f_id_tlag.ge.0) then
+        if (iwallf.eq.0) then
+          ! No wall functions forced by user
+          pimp = 0.d0
+        else
+          ! Use of wall functions
+          if (iuntur.eq.1) then
+              pimp = 0.5 * cfnnk / (cfnne * uk**3 ) * cl * xkappa * rough_d      &
+                   * ( coefa_rij(1,ifac) + coefb_rij(1,1,ifac) * rijipb(ifac,1)  &
+                     + coefa_rij(2,ifac) + coefb_rij(2,2,ifac) * rijipb(ifac,2)  &
+                     + coefa_rij(3,ifac) + coefb_rij(3,3,ifac) * rijipb(ifac,3))
+          else
+            pimp = 0.d0
+          endif
+        endif
+
+        call set_dirichlet_scalar &
+           ( coefa_tlag(ifac), coefaf_tlag(ifac),             &
+             coefb_tlag(ifac), coefbf_tlag(ifac),             &
+             pimp         , hint          , rinfin )
+      endif
+
       ! No diffusion reconstruction when using wall functions
       if (associated(cpro_diff_lim_eps)) cpro_diff_lim_eps(ifac) = 0.d0
 
@@ -1289,6 +1360,18 @@ do ifac = 1, nfabor
          ( coefa_ep(ifac), coefaf_ep(ifac),             &
            coefb_ep(ifac), coefbf_ep(ifac),             &
            pimp          , hint          , rinfin )
+
+      ! Dirichlet Boundary Condition on Lagrangian time scale
+      !-----------------------------------------------------
+      if (f_id_tlag.ge.0) then
+        pimp = 0.d0
+        hint = (visclc+visctc/sigmak)/distbf
+
+        call set_dirichlet_scalar &
+           ( coefa_tlag(ifac), coefaf_tlag(ifac),             &
+             coefb_tlag(ifac), coefbf_tlag(ifac),             &
+             pimp         , hint          , rinfin )
+      endif
 
       ! Dirichlet Boundary Condition on Phi
       !------------------------------------
@@ -1343,6 +1426,18 @@ do ifac = 1, nfabor
          ( coefa_ep(ifac), coefaf_ep(ifac),             &
            coefb_ep(ifac), coefbf_ep(ifac),             &
            pimp          , hint           , rinfin )
+
+      ! Dirichlet Boundary Condition on Lagrangian time scale
+      !-----------------------------------------------------
+      if (f_id_tlag.ge.0) then
+        pimp = 0.d0
+        hint = (visclc+visctc/sigmak)/distbf
+
+        call set_dirichlet_scalar &
+           ( coefa_tlag(ifac), coefaf_tlag(ifac),             &
+             coefb_tlag(ifac), coefbf_tlag(ifac),             &
+             pimp         , hint          , rinfin )
+      endif
 
       ! Dirichlet Boundary Condition on Phi
       !------------------------------------
@@ -1408,6 +1503,26 @@ do ifac = 1, nfabor
          ( coefa_omg(ifac), coefaf_omg(ifac),             &
            coefb_omg(ifac), coefbf_omg(ifac),             &
            qimp           , hint )
+
+      !if defined set Dirichlet condition for the Lagrangian time scale
+      if (f_id_tlag.ge.0) then
+        if (iwallf.eq.0) then
+          ! No wall functions forced by user
+          pimp = 0.d0
+        else
+          ! Use of wall functions
+          if (iuntur.eq.1) then
+            pimp = cfnnk / (cfnne * uk) * cl / sqrcmu * xkappa * rough_d
+          else
+            pimp = 0.d0
+          endif
+        endif
+
+        call set_dirichlet_scalar &
+           ( coefa_tlag(ifac), coefaf_tlag(ifac),             &
+             coefb_tlag(ifac), coefbf_tlag(ifac),             &
+             pimp         , hint          , rinfin )
+      endif
 
     !===========================================================================
     ! 7.1 Boundary conditions on the Spalart Allmaras turbulence model
@@ -1616,7 +1731,7 @@ end subroutine
 !>                               - 9 free inlet/outlet
 !>                                 (input mass flux blocked to 0)
 !> \param[in,out] rcodcl        boundary condition values:
-!>                               - rcodcl(1) value of the dirichlet
+!>                               - rcodcl(1) value of the Dirichlet
 !>                               - rcodcl(2) value of the exterior exchange
 !>                                 coefficient (infinite if no exchange)
 !>                               - rcodcl(3) value flux density

commit f7469d2e50541748aaabf748b9b67185701fa834

[Lagr] fix memory leak (introduced in commit 0184f41f82474, 2022/05/05).

src/lagr/cs_lagr.c
@@ -1270,20 +1270,18 @@ cs_lagr_add_fields(void)
     }
   }
 
-  /* Add Dissipation for LES */
+  /* Add Dissipation for LES or k-omega */
 
-  if (extra->itytur == 4) {
-    f = cs_field_by_name_try("epsilon");
-    if (f == NULL)
-      f = cs_field_by_name_try("lagr_epsilon");
-    if (f == NULL) {
-      f = cs_field_find_or_create("lagr_epsilon",
-                                  CS_FIELD_INTENSIVE | CS_FIELD_PROPERTY,
-                                  CS_MESH_LOCATION_CELLS,
-                                  1,
-                                  true);
-      cs_field_set_key_int(f, k_log, 1);
-    }
+  f = cs_field_by_name_try("epsilon");
+  if (f == NULL)
+    f = cs_field_by_name_try("lagr_epsilon");
+  if (f == NULL) {
+    f = cs_field_find_or_create("lagr_epsilon",
+                                CS_FIELD_INTENSIVE | CS_FIELD_PROPERTY,
+                                CS_MESH_LOCATION_CELLS,
+                                1,
+                                true);
+    cs_field_set_key_int(f, k_log, 1);
   }
 }
 

src/lagr/cs_lagr_aux_mean_fluid_quantities.c
@@ -284,40 +284,24 @@ cs_lagr_aux_mean_fluid_quantities(cs_field_t    *lagr_time,
 
     cs_real_t cl     = 1.0 / (0.5 + 0.75 * c0);
 
-    cs_real_t  *energi = NULL, *dissip = NULL;
-    BFT_MALLOC(energi, n_cells, cs_real_t);
-    BFT_MALLOC(dissip, n_cells, cs_real_t);
+    cs_real_t *energi = extra->cvar_k->val;
+    cs_real_t *dissip = extra->cvar_ep->val;
 
-    if (extra->itytur == 2 || extra->itytur == 4 || extra->itytur == 5) {
-
-      for (cs_lnum_t cell_id = 0; cell_id < n_cells; cell_id++) {
-        energi[cell_id] = extra->cvar_k->val[cell_id];
-        dissip[cell_id] = extra->cvar_ep->val[cell_id];
-      }
-
-    }
-    else if (extra->itytur == 3) {
-
-      for (cs_lnum_t cell_id = 0; cell_id < n_cells; cell_id++) {
-
+    if (extra->itytur == 3) {
+      for (cs_lnum_t cell_id = 0; cell_id < n_cells; cell_id++)
         energi[cell_id] = 0.5 * (  extra->cvar_rij->val[6*cell_id]
                                  + extra->cvar_rij->val[6*cell_id+1]
                                  + extra->cvar_rij->val[6*cell_id+2]);
-        dissip[cell_id] = extra->cvar_ep->val[cell_id];
-        extra->cvar_k->val[cell_id] = energi[cell_id] ;
-      }
 
     }
     else if (extra->iturb == CS_TURB_K_OMEGA) {
 
-      for (cs_lnum_t cell_id = 0; cell_id < n_cells; cell_id++) {
-        energi[cell_id] = extra->cvar_k->val[cell_id];
+      for (cs_lnum_t cell_id = 0; cell_id < n_cells; cell_id++)
         dissip[cell_id] = extra->cmu * energi[cell_id]
                                      * extra->cvar_omg->val[cell_id];
-      }
 
     }
-    else {
+    else if (extra->itytur != 2 && extra->itytur != 4 && extra->itytur != 5) {
 
       bft_error
         (__FILE__, __LINE__, 0,