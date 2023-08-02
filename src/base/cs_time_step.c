353c276754d2208bb8a2d6f31dbfb239fb28c532
Yvan Fournier <yvan.fournier@noreply.edf.fr>
2023-08-02 23:48:40 +0200

    Fix for commit f436fe3e (2023-08-02).
    Remove extra incrementation of adattive time step.
    Also remove extra status update (having a predicted time instead of the
    actual time with an error of less than one time step in the status
    file, whose role is temporary is not a big loss, so avoid sacrificing
    performance here).

--- src/base/cs_time_step.c
+++ src/base/cs_time_step.c
@@ -596,14 +596,6 @@ cs_time_step_update_dt(double  dt)
   _update_kahan_compensation(&t, &_time_step.t_cur, &z);
 
   _time_step.t_cur = t;
-  _time_step.nt_cur += 1;
-
-  if (_time_step.is_local)
-    cs_base_update_status("time step: %d\n",
-                          _time_step.nt_cur);
-  else
-    cs_base_update_status("time step: %d; t = %g\n",
-                          _time_step.nt_cur, _time_step.t_cur);
 }
 
 /*----------------------------------------------------------------------------*/
