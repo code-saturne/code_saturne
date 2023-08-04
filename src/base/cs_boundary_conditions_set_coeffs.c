code-saturne/code_saturne

commit 27908ffd1ff0fd2f0d53f366b656ef900c54b1b1

Translate confli.f90 to C (cs_boundary_condition_set_coeffs)
Edited by Y. Fournier.

Includes many C binding improvements, and various style fixes.

src/base/tridim.f90
@@ -115,8 +115,6 @@ double precision xxp0, xyp0, xzp0
 double precision relaxk, relaxe, relaxw, relaxn
 double precision hdls(6)
 
-double precision, save :: tmet
-
 integer          ipass
 data             ipass /0/
 save             ipass
@@ -160,28 +158,18 @@ type(var_cal_opt) :: vcopt, vcopt_u, vcopt_p
 
 interface
 
-  subroutine condli &
-  ( nvar   , nscal  , iterns ,                                     &
-    isvhb  ,                                                       &
-    itrale , italim , itrfin , ineefl , itrfup ,                   &
-    icodcl , isostd ,                                              &
-    dt     , rcodcl ,                                              &
-    visvdr , hbord  , theipb )
-
-    use mesh, only: nfac, nfabor
-
+  subroutine cs_boundary_conditions_set_coeffs(nvar, iterns, isvhb, itrale,    &
+                                               italim, itrfin, ineefl, itrfup, &
+                                               isostd, visvdr, hbord, theipb,  &
+                                               nftcdt)                         &
+    bind(C, name='cs_boundary_conditions_set_coeffs')
+    use, intrinsic :: iso_c_binding
     implicit none
-
-    integer          nvar, nscal, iterns, isvhb
-    integer          itrale , italim , itrfin , ineefl , itrfup
-
-    integer, pointer, dimension(:,:) :: icodcl
-    integer, dimension(nfabor+1) :: isostd
-    double precision, pointer, dimension(:) :: dt
-    double precision, pointer, dimension(:,:,:) :: rcodcl
-    double precision, pointer, dimension(:) :: visvdr, hbord, theipb
-
-  end subroutine condli
+    integer(kind=c_int), value :: nvar, iterns, itrale, italim, isvhb
+    integer(kind=c_int), value :: itrfin, itrfup, ineefl, nftcdt
+    integer(kind=c_int), dimension(*) :: isostd
+    real(kind=c_double), dimension(*) :: visvdr, hbord, theipb
+  end subroutine cs_boundary_conditions_set_coeffs
 
   function cs_mobile_structures_get_n_structures() result(n_structs)  &
     bind(C, name='cs_mobile_structures_get_n_structures')
@@ -305,6 +293,16 @@ interface
     integer(c_int), dimension(*) :: ale_bc_type
     integer(c_int), dimension(*) :: impale, isostd
   end subroutine cs_solve_navier_stokes
+
+  subroutine cou1di()  &
+    bind(C, name='cs_f_cou1di')
+    use, intrinsic :: iso_c_binding
+  end subroutine cou1di
+
+  subroutine cscloc()  &
+    bind(C, name='cs_f_cscloc')
+    use, intrinsic :: iso_c_binding
+  end subroutine cscloc
 
 end interface
 
@@ -757,13 +755,9 @@ inslst = 0
 iterns = 1
 do while (iterns.le.nterup)
   ! Calls user BCs and computes BC coefficients
-  call condli &
-    (nvar   , nscal  , iterns ,                                    &
-     isvhb  ,                                                      &
-     itrale , italim , itrfin , ineefl , itrfup ,                  &
-     icodcl , isostd ,                                             &
-     dt     , rcodcl ,                                             &
-     visvdr , hbord  , theipb )
+  call cs_boundary_conditions_set_coeffs(nvar, iterns, isvhb, itrale, italim,  &
+                                         itrfin, ineefl, itrfup, isostd,       &
+                                         visvdr, hbord, theipb, nftcdt)
 
   if (nftcdt.gt.0) then
     ! Coefficient exchange of the enthalpy scalar
@@ -819,7 +813,7 @@ do while (iterns.le.nterup)
       call cou1do(hbord, theipb)
 
       if ((iirayo.ge.1).or.(icondb.eq.0)) then
-        call cou1di(nfabor, iscalt, icodcl, rcodcl)
+        call cou1di
       endif
 
     endif
@@ -847,8 +841,9 @@ do while (iterns.le.nterup)
   ! TODO it has to be moved before phyvar, for that bc types have to be known
   ! (itypfb)
 
-  !       (Nouvel algorithme. L'ancien est dans condli)
+  ! (New algorithm. the old one is in cs_boundary_condition_set_coeffs)
   ! In ALE, this computation is done only for the first step.
+
   if (italim.eq.1) then
 
     ! Pour le moment, on suppose que l'on peut se contenter de faire

commit 05e5357e7b389042661a89b83570ec2d48af2262

BCs: fix for dimension of dttens broken by 27908ffd.
Also fix incorrect pointer argument passing from commit 2006c418.

src/base/cs_boundary_conditions_set_coeffs.c
@@ -570,7 +570,7 @@ cs_boundary_conditions_set_coeffs(int        nvar,
 
   cs_real_t   *bfconv  = NULL, *bhconv = NULL;
   cs_real_3_t *forbr   = NULL;
-  const cs_real_3_t *dttens = NULL;
+  const cs_real_6_t *dttens = NULL;
 
   /* Pointers to specific fields */
 
@@ -581,7 +581,7 @@ cs_boundary_conditions_set_coeffs(int        nvar,
 
   cs_field_t *f_dttens  = cs_field_by_name_try("dttens");
   if (f_dttens != NULL)
-    dttens = (const cs_real_3_t *)f_dttens->val;
+    dttens = (const cs_real_6_t *)f_dttens->val;
 
   cs_field_t *f_forbr = cs_field_by_name_try("boundary_forces");
 

src/base/tridim.f90
@@ -170,7 +170,7 @@ interface
     integer(kind=c_int), value :: nvar, iterns, itrale, italim, isvhb
     integer(kind=c_int), value :: itrfin, itrfup, ineefl, nftcdt
     integer(kind=c_int), dimension(*) :: isostd
-    type(c_ptr) :: visvdr, hbord, theipb
+    type(c_ptr), value :: visvdr, hbord, theipb
   end subroutine cs_boundary_conditions_set_coeffs
 
   function cs_mobile_structures_get_n_structures() result(n_structs)  &