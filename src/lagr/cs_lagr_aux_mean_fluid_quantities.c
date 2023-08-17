code-saturne/code_saturne

commit 745455f4037427e72b32c38aa232e12705e08e47

Lagrangian module: some cleanup and robustness improvements.

src/lagr/cs_lagr_aux_mean_fluid_quantities.c
@@ -166,10 +166,12 @@ cs_lagr_aux_mean_fluid_quantities(cs_field_t    *lagr_time,
         || turb_model->itytur == 6) {
       BFT_MALLOC(wpres, n_cells_with_ghosts, cs_real_t);
 
+      int time_id = (extra->cvar_k->n_time_vals > 1) ? 1 : 2;
+      const cs_real_t *cvar_k = extra->cvar_k->vals[time_id];
       for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++) {
         wpres[c_id] =  solved_pres[c_id]
                      -  2./3. * extra->cromf->val[c_id]
-                      * extra->cvar_k->val_pre[c_id];
+                      * cvar_k[c_id];
       }
     }
     else {

src/base/addfld.f90
@@ -579,39 +579,6 @@ if (ineedy.eq.1) then
 
 endif
 
-! Add Subgrid TKE and dissipation for LES and Particle modelling
-if (itytur.eq.4 .and. iilagr.gt.0) then
-  ityloc = 1 ! variables defined on cells
-  itycat = FIELD_INTENSIVE + FIELD_PROPERTY
-  ! Warning, not ik or ivarfl(ik)
-  call field_find_or_create('k', itycat, ityloc, idim1, iflid)
-  call field_set_key_int(iflid, keyvis, 1)
-  call field_set_key_int(iflid, keylog, 1)
-
-  ! Warning, not iep or ivarfl(iep)
-  call field_find_or_create('epsilon', itycat, ityloc, idim1, iflid)
-  call field_set_key_int(iflid, keyvis, 1)
-  call field_set_key_int(iflid, keylog, 1)
-end if
-
-! Add TKE for DRSM model and Particle modelling
-if (itytur.eq.3 .and. iilagr.gt.0) then
-  ityloc = 1 ! variables defined on cells
-  itycat = FIELD_INTENSIVE + FIELD_PROPERTY
-  ! Warning, not ik or ivarfl(ik)
-  call field_find_or_create('k', itycat, ityloc, idim1, iflid)
-  call field_set_key_int(iflid, keylog, 1)
-end if
-
-! Add Lagrangian integral time for Lagrangian computation
-if (iilagr.gt.0) then
-  ityloc = 1 ! variables defined on cells
-  itycat = FIELD_INTENSIVE + FIELD_PROPERTY
-  call field_find_or_create('lagr_time', itycat, ityloc, idim1, iflid)
-  call field_set_key_int(iflid, keyvis, 1)
-  call field_set_key_int(iflid, keylog, 1)
-endif
-
 if (ippmod(iatmos).ge.0.and.compute_z_ground) then
   f_name  = 'z_ground'
   f_label = 'Z ground'

src/base/fldprp.f90
@@ -47,7 +47,6 @@ use cstphy
 use cstnum
 use entsor
 use albase
-use lagran
 use parall
 use ppppar
 use ppthch

src/base/initi1.f90
@@ -74,6 +74,12 @@ interface
     implicit none
   end subroutine cs_gui_output
 
+  subroutine cs_lagr_add_fields()  &
+      bind(C, name='cs_lagr_add_fields')
+    use, intrinsic :: iso_c_binding
+    implicit none
+  end subroutine cs_lagr_add_fields
+
   subroutine cs_parameters_eqp_complete()  &
       bind(C, name='cs_parameters_eqp_complete')
     use, intrinsic :: iso_c_binding
@@ -146,6 +152,7 @@ if (iscalt.ge.1) then
   have_thermal_model = 1
 endif
 call cs_lagr_options_definition(isuite, have_thermal_model, dtref, iccvfg)
+call cs_lagr_add_fields
 
 ! Additional fields if not in CDO mode only
 

src/base/varpos.f90
@@ -47,7 +47,6 @@ use cstphy
 use cstnum
 use entsor
 use albase
-use lagran
 use parall
 use ppppar
 use ppthch

src/lagr/cs_lagr.c
@@ -725,24 +725,30 @@ _lagr_map_fields_default(void)
 {
   if (   cs_glob_physical_model_flag[CS_COMBUSTION_COAL] >= 0
       || cs_glob_physical_model_flag[CS_COMBUSTION_FUEL] >= 0) {
-    _lagr_extra_module.cromf       = cs_field_by_name_try("rho_gas");
+    _lagr_extra_module.cromf   = cs_field_by_name_try("rho_gas");
   }
   else {
-    _lagr_extra_module.cromf       = cs_field_by_name_try("density");
+    _lagr_extra_module.cromf   = cs_field_by_name_try("density");
   }
 
-  _lagr_extra_module.pressure    = cs_field_by_name_try("pressure");
+  _lagr_extra_module.pressure  = cs_field_by_name_try("pressure");
 
-  _lagr_extra_module.luminance   = cs_field_by_name_try("luminance");
+  _lagr_extra_module.luminance = cs_field_by_name_try("luminance");
 
   _lagr_extra_module.lagr_time = cs_field_by_name_try("lagr_time");
+
+  _lagr_extra_module.cvar_k = cs_field_by_name_try("k");
+  if (_lagr_extra_module.cvar_k == NULL)
+    _lagr_extra_module.cvar_k = cs_field_by_name_try("lagr_k");
+
+  _lagr_extra_module.cvar_ep = cs_field_by_name_try("epsilon");
+  if (_lagr_extra_module.cvar_ep == NULL)
+    _lagr_extra_module.cvar_ep = cs_field_by_name_try("lagr_epsilon");
 
   if (cs_field_by_name_try("velocity_1") != NULL) {
     /* we are probably using NEPTUNE_CFD */
     _lagr_extra_module.vel         = cs_field_by_name_try("lagr_velocity");
 
-    _lagr_extra_module.cvar_k      = cs_field_by_name_try("lagr_k");
-    _lagr_extra_module.cvar_ep     = cs_field_by_name_try("lagr_epsilon");
     _lagr_extra_module.cvar_omg    = NULL;
     _lagr_extra_module.cvar_rij    = cs_field_by_name_try("lagr_rij");
     _lagr_extra_module.viscl       = cs_field_by_name_try
@@ -765,8 +771,6 @@ _lagr_map_fields_default(void)
   else {
     /* we use code_saturne */
     _lagr_extra_module.vel         = cs_field_by_name_try("velocity");
-    _lagr_extra_module.cvar_k      = cs_field_by_name_try("k");
-    _lagr_extra_module.cvar_ep     = cs_field_by_name_try("epsilon");
     _lagr_extra_module.cvar_omg    = cs_field_by_name_try("omega");
     _lagr_extra_module.cvar_rij    = cs_field_by_name_try("rij");
     _lagr_extra_module.viscl       = cs_field_by_name_try("molecular_viscosity");
@@ -1215,6 +1219,75 @@ cs_lagr_finalize(void)
   if (extra->grad_lagr_time != NULL)
     BFT_FREE(extra->grad_lagr_time);
 
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Create additional fields needed by the Lagrangien model
+ *
+ * Most additional fields can be defined directly in
+ * \ref cs_lagr_options_definition, but some fields may be mapped to
+ * different fields based on the calling module (i.e. code_saturne or
+ * neptune_cfd), and possibly defined after that call.
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_lagr_add_fields(void)
+{
+  if (cs_glob_lagr_time_scheme->iilagr <= CS_LAGR_OFF)
+    return;
+
+  /* Add Lagrangian integral time for Lagrangian computation */
+
+  const int k_vis = cs_field_key_id("post_vis");
+  const int k_log = cs_field_key_id("log");
+
+  cs_lagr_extra_module_t *extra = cs_glob_lagr_extra_module;
+  cs_field_t *f = NULL;
+
+  /* Add Lagrangian integral time */
+
+  f = cs_field_create("lagr_time",
+                      CS_FIELD_INTENSIVE | CS_FIELD_PROPERTY,
+                      CS_MESH_LOCATION_CELLS,
+                      1,
+                      false);
+
+  cs_field_set_key_int(f, k_log, 1);
+  cs_field_set_key_int(f, k_vis, 1);
+
+  /* Add TKE for DRSM model */
+
+  if (extra->itytur == 3 || extra->itytur == 4) {
+    f = cs_field_by_name_try("k");
+    if (f == NULL)
+      f = cs_field_by_name_try("lagr_k");
+    if (f == NULL) {
+      f = cs_field_find_or_create("lagr_k",
+                                  CS_FIELD_INTENSIVE | CS_FIELD_PROPERTY,
+                                  CS_MESH_LOCATION_CELLS,
+                                  1,
+                                  true);
+      cs_field_set_key_int(f, k_log, 1);
+    }
+  }
+
+  /* Add Dissipation for LES */
+
+  if (extra->itytur == 4) {
+    f = cs_field_by_name_try("epsilon");
+    if (f == NULL)
+      f = cs_field_by_name_try("lagr_epsilon");
+    if (f == NULL) {
+      f = cs_field_find_or_create("lagr_epsilon",
+                                  CS_FIELD_INTENSIVE | CS_FIELD_PROPERTY,
+                                  CS_MESH_LOCATION_CELLS,
+                                  1,
+                                  true);
+      cs_field_set_key_int(f, k_log, 1);
+    }
+  }
 }
 
 /*----------------------------------------------------------------------------*/
@@ -2114,13 +2187,13 @@ cs_lagr_solve_time_step(const int         itypfb[],
        values at current time step and not at previous time step, because
        values at previous time step = initialization (zero gradients) */
 
-    if (extra->itytur == 3 ) {
+    if (extra->itytur == 3 && extra->cvar_k->n_time_vals > 1) {
       /* save previous value dor the kinetic energy */
       for (cs_lnum_t cell_id = 0; cell_id < cs_glob_mesh->n_cells; cell_id++)
-        extra->cvar_k->vals[1][cell_id] = 0.5 *
-                                    (  extra->cvar_rij->vals[1][6*cell_id]
-                                     + extra->cvar_rij->vals[1][6*cell_id + 1]
-                                     + extra->cvar_rij->vals[1][6*cell_id + 2]);
+        extra->cvar_k->vals[1][cell_id]
+          = 0.5 * (  extra->cvar_rij->vals[1][6*cell_id]
+                   + extra->cvar_rij->vals[1][6*cell_id + 1]
+                   + extra->cvar_rij->vals[1][6*cell_id + 2]);
     }
 
     /* First pass allocate and compute it */
@@ -2132,7 +2205,8 @@ cs_lagr_solve_time_step(const int         itypfb[],
                                         extra->grad_vel,
                                         extra->grad_lagr_time);
     }
-    else if (cs_glob_lagr_time_scheme->iilagr != CS_LAGR_FROZEN_CONTINUOUS_PHASE) {
+    else if (   cs_glob_lagr_time_scheme->iilagr
+             != CS_LAGR_FROZEN_CONTINUOUS_PHASE) {
 
       if (mesh->time_dep >= CS_MESH_TRANSIENT_CONNECT) {
         cs_lnum_t n_cells_ext = cs_glob_mesh->n_cells_with_ghosts;

src/lagr/cs_lagr.h
@@ -1339,20 +1339,6 @@ cs_get_lagr_extra_module(void);
 void
 cs_lagr_solve_initialize(const cs_real_t  *dt);
 
-/*--------------------------------------------------------------------
- * Execute one time step of the Lagrangian model.
- *
- * This is the main function for that model.
- *
- *  parameters:
- *    itypfb <-- boundary face types
- *    dt     <-- time step (per cell)
- *-------------------------------------------------------------------- */
-
-void
-cs_lagr_solve_time_step(const int         itypfb[],
-                        const cs_real_t  *dt);
-
 /*----------------------------------------------------------------------------
  * Return pointers to lagrangian arrays
  *
@@ -1375,6 +1361,34 @@ cs_lagr_init_c_arrays(int          dim_cs_glob_lagr_source_terms[2],
 
 void
 cs_lagr_finalize(void);
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Create additional fields needed by the Lagrangien model
+ *
+ * Most additional fields can be defined directly in
+ * \ref cs_lagr_options_definition, but some fields may be mapped to
+ * different fields based on the calling module (i.e. code_saturne or
+ * neptune_cfd), and possibly defined after that call.
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_lagr_add_fields(void);
+
+/*--------------------------------------------------------------------
+ * Execute one time step of the Lagrangian model.
+ *
+ * This is the main function for that model.
+ *
+ *  parameters:
+ *    itypfb <-- boundary face types
+ *    dt     <-- time step (per cell)
+ *-------------------------------------------------------------------- */
+
+void
+cs_lagr_solve_time_step(const int         itypfb[],
+                        const cs_real_t  *dt);
 
 /*----------------------------------------------------------------------------*/
 

src/lagr/cs_lagr_new.c
@@ -669,8 +669,11 @@ cs_lagr_new_particle_init(const cs_lnum_t  particle_range[2],
     if (extra->cvar_rij != NULL)
       cvar_rij = (const cs_real_6_t *) extra->cvar_rij->vals[time_id];
 
-    else if (extra->cvar_k != NULL)
+    else if (extra->cvar_k != NULL) {
       cvar_k = (const cs_real_t *)extra->cvar_k->vals[time_id];
+      if (extra->cvar_k != NULL)
+        cvar_k = (const cs_real_t *)extra->cvar_k->val;
+    }
 
     else {
       bft_error

commit 507ad67a403b30ca8d13953e9ccc12fe2e9217c0

[Lagr] Fix bug introduced in commit 745455f.
Should have no influence because k is always created with a previous
value (even when not necessary in LES).

src/lagr/cs_lagr_aux_mean_fluid_quantities.c
@@ -166,7 +166,7 @@ cs_lagr_aux_mean_fluid_quantities(cs_field_t    *lagr_time,
         || turb_model->itytur == 6) {
       BFT_MALLOC(wpres, n_cells_with_ghosts, cs_real_t);
 
-      int time_id = (extra->cvar_k->n_time_vals > 1) ? 1 : 2;
+      int time_id = (extra->cvar_k->n_time_vals > 1) ? 1 : 0;
       const cs_real_t *cvar_k = extra->cvar_k->vals[time_id];
       for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++) {
         wpres[c_id] =  solved_pres[c_id]