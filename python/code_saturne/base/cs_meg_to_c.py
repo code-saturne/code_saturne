code-saturne/code_saturne

commit ba1a26379762446ba0741e10ee5bad8b4da833f0

[GUI] Add usage of time tables in boundary conditions and source terms formulae
  * User defined time tables can now be called inside mathematical
    formulae for boundary conditions and source terms.
  * Variables are visible as "TABLE_NAME[LABEL_NAME]".

python/code_saturne/base/cs_meg_to_c.py
@@ -29,6 +29,7 @@ import re
 from code_saturne.base.cs_math_parser import cs_math_parser
 
 from code_saturne.model.NotebookModel import NotebookModel
+from code_saturne.model.TimeTablesModel import TimeTablesModel
 from code_saturne.model.SolutionDomainModel import getRunType
 
 #===============================================================================
@@ -330,6 +331,15 @@ class meg_to_c_interpreter:
         self.notebook = {}
         for (nme, val) in nb.getNotebookList():
             self.notebook[nme] = str(val)
+
+        TTM = TimeTablesModel(self.case)
+        self.time_tables = {}
+        for i_t, tab_name in enumerate(TTM.getTableNamesList()):
+            for _h in TTM.getTableHeadersList(i_t):
+                _k = "{}[{}]".format(tab_name, _h)
+                self.time_tables[_k] = ['{}_{}'.format(tab_name,_h),
+                                        'CS_TIME_TABLE("{}","{}")'.format(tab_name, _h)]
+
 
         if create_functions and getRunType(self.case) == 'standard':
 
@@ -636,6 +646,13 @@ class meg_to_c_interpreter:
             glob_tokens[kn] = \
             'const cs_real_t %s = cs_notebook_parameter_value_by_name("%s");' % (kn, kn)
 
+        # Time table variables
+        for ktt in self.time_tables.keys():
+            _kvals = self.time_tables[ktt]
+            glob_tokens[ktt] = \
+                'const cs_real_t {var} = {func};'.format(var=_kvals[0], \
+                                                          func=_kvals[1])
+
         # Fields
         for f in known_fields:
             glob_tokens[f[0]] = \
@@ -678,6 +695,10 @@ class meg_to_c_interpreter:
             usr_blck += 2*tab + '}\n'
 
         usr_blck += tab + '}\n'
+
+        # Replace time table calls
+        for _tt in self.time_tables.keys():
+            usr_blck = usr_blck.replace(_tt, self.time_tables[_tt][0])
 
         return usr_blck
 
@@ -746,6 +767,13 @@ class meg_to_c_interpreter:
             glob_tokens[kn] = \
             'const cs_real_t %s = cs_notebook_parameter_value_by_name("%s");' % (kn, kn)
 
+        # Time table variables
+        for ktt in self.time_tables.keys():
+            _kvals = self.time_tables[ktt]
+            glob_tokens[ktt] = \
+                'const cs_real_t {var} = {func});'.format(var=_kvals[0], \
+                                                          func=_kvals[1])
+
         for f in known_fields.keys():
             knf_name = known_fields[f]
             glob_tokens[f] = \
@@ -787,6 +815,10 @@ class meg_to_c_interpreter:
 
         usr_blck += 2*tab + '}\n'
         usr_blck += tab + '}\n'
+
+        # Replace time table calls
+        for _tt in self.time_tables.keys():
+            usr_blck = usr_blck.replace(_tt, self.time_tables[_tt][0])
 
         return usr_blck
 

python/code_saturne/gui/case/BoundaryConditionsCoalInletView.py
@@ -59,6 +59,7 @@ from code_saturne.model.LocalizationModel import LocalizationModel, Zone
 from code_saturne.model.Boundary import Boundary
 from code_saturne.gui.case.QMegEditorView import QMegEditorView
 from code_saturne.model.NotebookModel import NotebookModel
+from code_saturne.model.TimeTablesModel import TimeTablesModel
 
 #-------------------------------------------------------------------------------
 # log config
@@ -256,6 +257,7 @@ class BoundaryConditionsCoalInletView(QWidget, Ui_BoundaryConditionsCoalInletFor
 
         self.case.undoStopGlobal()
         self.notebook = NotebookModel(self.case)
+        self.time_tables = TimeTablesModel(self.case).getTablesDataDict()
 
         # Connections
         self.comboBoxTypeInlet.activated[str].connect(self.__slotInletType)
@@ -535,6 +537,11 @@ class BoundaryConditionsCoalInletView(QWidget, Ui_BoundaryConditionsCoalInletFor
 
         for (nme, val) in self.notebook.getNotebookList():
             sym.append((nme, 'value (notebook) = ' + str(val)))
+
+        for _k in self.time_tables.keys():
+            for _h in self.time_tables[_k]:
+                sym.append(('{}[{}]'.format(_k,_h),
+                           'Variable "{}" of time table "{}"'.format(_h, _k)))
 
         dialog = QMegEditorView(parent = self,
                                 function_type = 'bnd',

python/code_saturne/gui/case/BoundaryConditionsElectricalView.py
@@ -55,6 +55,7 @@ from code_saturne.model.LocalizationModel import LocalizationModel, Zone
 from code_saturne.gui.case.QMegEditorView import QMegEditorView
 from code_saturne.model.Boundary import Boundary
 from code_saturne.model.NotebookModel import NotebookModel
+from code_saturne.model.TimeTablesModel import TimeTablesModel
 
 #-------------------------------------------------------------------------------
 # log config
@@ -91,6 +92,7 @@ class BoundaryConditionsElectricalView(QWidget, Ui_BoundaryConditionsElectricalF
         self.__model = ElectricalModel(self.case)
         self.species_list = []
         self.notebook = NotebookModel(self.case)
+        self.time_tables = TimeTablesModel(self.case).getTablesDataDict()
 
         self.lineEditValuePotElec.textChanged[str].connect(self.slotPotElec)
         self.lineEditValuePotElecIm.textChanged[str].connect(self.slotPotElecIm)
@@ -363,6 +365,11 @@ class BoundaryConditionsElectricalView(QWidget, Ui_BoundaryConditionsElectricalF
         for (nme, val) in self.notebook.getNotebookList():
             sym.append((nme, 'value (notebook) = ' + str(val)))
 
+        for _k in self.time_tables.keys():
+            for _h in self.time_tables[_k]:
+                sym.append(('{}[{}]'.format(_k,_h),
+                           'Variable "{}" of time table "{}"'.format(_h, _k)))
+
         c = self.__boundary.getScalarChoice(variable_name)
         dialog = QMegEditorView(parent        = self,
                                 function_type = 'bnd',
@@ -426,6 +433,11 @@ class BoundaryConditionsElectricalView(QWidget, Ui_BoundaryConditionsElectricalF
 
         for (nme, val) in self.notebook.getNotebookList():
             sym.append((nme, 'value (notebook) = ' + str(val)))
+
+        for _k in self.time_tables.keys():
+            for _h in self.time_tables[_k]:
+                sym.append(('{}[{}]'.format(_k,_h),
+                           'Variable "{}" of time table "{}"'.format(_h, _k)))
 
         c = self.__b.getElecScalarChoice(self.potVect)
 

python/code_saturne/gui/case/BoundaryConditionsEnergyView.py
@@ -53,6 +53,7 @@ from code_saturne.model.MainFieldsModel import MainFieldsModel
 from code_saturne.model.ThermodynamicsModel import ThermodynamicsModel
 from code_saturne.model.ConjugateHeatTransferModel import ConjugateHeatTransferModel
 from code_saturne.model.NotebookModel import NotebookModel
+from code_saturne.model.TimeTablesModel import TimeTablesModel
 from code_saturne.gui.case.QMegEditorView import QMegEditorView
 
 #-------------------------------------------------------------------------------
@@ -118,6 +119,7 @@ class BoundaryConditionsEnergyView(QWidget, Ui_BoundaryConditionsEnergy) :
         self.__currentField = fieldId
         self.cht_model = ConjugateHeatTransferModel(self.case)
         self.notebook  = NotebookModel(self.case)
+        self.time_tables = TimeTablesModel(self.case).getTablesDataDict()
 
 
     def showWidget(self, boundary):
@@ -333,6 +335,11 @@ class BoundaryConditionsEnergyView(QWidget, Ui_BoundaryConditionsEnergy) :
 
         for (name, val) in self.notebook.getNotebookList():
             sym.append((name, 'value (notebook) = ' + str(val)))
+
+        for _k in self.time_tables.keys():
+            for _h in self.time_tables[_k]:
+                sym.append(('{}[{}]'.format(_k,_h),
+                           'Variable "{}" of time table "{}"'.format(_h, _k)))
 
         dialog = QMegEditorView(parent        = self,
                                 function_type = "bnd",

python/code_saturne/gui/case/BoundaryConditionsExternalHeadLossesView.py
@@ -54,6 +54,7 @@ from code_saturne.model.LocalizationModel import LocalizationModel, Zone
 from code_saturne.model.Boundary import Boundary
 from code_saturne.gui.case.QMegEditorView import QMegEditorView
 from code_saturne.model.NotebookModel import NotebookModel
+from code_saturne.model.TimeTablesModel import TimeTablesModel
 
 #-------------------------------------------------------------------------------
 # log config
@@ -86,6 +87,7 @@ class BoundaryConditionsExternalHeadLossesView(QWidget, Ui_BoundaryConditionsExt
         self.case = case
         self.__boundary = None
         self.notebook = NotebookModel(self.case)
+        self.time_tables = TimeTablesModel(self.case).getTablesDataDict()
 
         self.case.undoStopGlobal()
 
@@ -139,6 +141,11 @@ class BoundaryConditionsExternalHeadLossesView(QWidget, Ui_BoundaryConditionsExt
 
         for (nme, val) in self.notebook.getNotebookList():
             sym.append((nme, 'value (notebook) = ' + str(val)))
+
+        for _k in self.time_tables.keys():
+            for _h in self.time_tables[_k]:
+                sym.append(('{}[{}]'.format(_k,_h),
+                           'Variable "{}" of time table "{}"'.format(_h, _k)))
 
         dialog = QMegEditorView(parent        = self,
                                 function_type = 'bnd',

python/code_saturne/gui/case/BoundaryConditionsHydraulicHeadView.py
@@ -54,6 +54,7 @@ from code_saturne.model.LocalizationModel import LocalizationModel, Zone
 from code_saturne.model.Boundary import Boundary
 from code_saturne.gui.case.QMegEditorView import QMegEditorView
 from code_saturne.model.NotebookModel import NotebookModel
+from code_saturne.model.TimeTablesModel import TimeTablesModel
 
 #-------------------------------------------------------------------------------
 # log config
@@ -85,6 +86,7 @@ class BoundaryConditionsHydraulicHeadView(QWidget, Ui_BoundaryConditionsHydrauli
         self.case = case
         self.__boundary = None
         self.notebook = NotebookModel(self.case)
+        self.time_tables = TimeTablesModel(self.case).getTablesDataDict()
 
         self.case.undoStopGlobal()
 
@@ -198,6 +200,11 @@ class BoundaryConditionsHydraulicHeadView(QWidget, Ui_BoundaryConditionsHydrauli
 
         for (nme, val) in self.notebook.getNotebookList():
             sym.append((nme, 'value (notebook) = ' + str(val)))
+
+        for _k in self.time_tables.keys():
+            for _h in self.time_tables[_k]:
+                sym.append(('{}[{}]'.format(_k,_h),
+                           'Variable "{}" of time table "{}"'.format(_h, _k)))
 
         dialog = QMegEditorView(parent        = self,
                                 function_type = 'bnd',

python/code_saturne/gui/case/BoundaryConditionsMobileMeshView.py
@@ -59,6 +59,7 @@ from code_saturne.model.Boundary import Boundary
 
 from code_saturne.gui.case.QMegEditorView import QMegEditorView
 from code_saturne.model.NotebookModel import NotebookModel
+from code_saturne.model.TimeTablesModel import TimeTablesModel
 
 #-------------------------------------------------------------------------------
 # log config
@@ -341,6 +342,12 @@ class CouplingManager:
         for (nme, val) in self.notebook.getNotebookList():
             symbols.append((nme, 'value (notebook) = ' + str(val)))
 
+        self.time_tables = TimeTablesModel(self.case).getTablesDataDict()
+        for _k in self.time_tables.keys():
+            for _h in self.time_tables[_k]:
+                symbols.append(('{}[{}]'.format(_k,_h),
+                               'Variable "{}" of time table "{}"'.format(_h, _k)))
+
         m_default = default % {'t':'m'}
         c_default = default % {'t':'c'}
         k_default = default % {'t':'k'}
@@ -447,6 +454,7 @@ class BoundaryConditionsMobileMeshView(QWidget,
 
         self.__model = MobileMeshModel(self.case)
         self.notebook = NotebookModel(self.case)
+        self.time_tables = TimeTablesModel(self.case).getTablesDataDict()
 
         self.__comboModel = ComboModel(self.comboMobilBoundary, 6, 1)
         self.__comboModel.addItem(self.tr("Fixed boundary"), "fixed_boundary")
@@ -497,6 +505,11 @@ class BoundaryConditionsMobileMeshView(QWidget,
 
         for (nme, val) in self.notebook.getNotebookList():
             sym.append((nme, 'value (notebook) = ' + str(val)))
+
+        for _k in self.time_tables.keys():
+            for _h in self.time_tables[_k]:
+                sym.append('{}[{}]'.format(_k,_h),
+                           'Variable "{}" of time table "{}"'.format(_h, _k))
 
         dialog = QMegEditorView(parent = self,
                                 function_type = 'bnd',

python/code_saturne/gui/case/BoundaryConditionsScalarsView.py
@@ -59,6 +59,7 @@ from code_saturne.model.CoalCombustionModel           import CoalCombustionModel
 from code_saturne.model.GasCombustionModel            import GasCombustionModel
 from code_saturne.model.AtmosphericFlowsModel         import AtmosphericFlowsModel
 from code_saturne.model.NotebookModel import NotebookModel
+from code_saturne.model.TimeTablesModel import TimeTablesModel
 from code_saturne.model.ConjugateHeatTransferModel import ConjugateHeatTransferModel
 
 # -------------------------------------------------------------------------------
@@ -107,6 +108,8 @@ class BoundaryConditionsScalarsView(QWidget, Ui_BoundaryConditionsScalarsForm):
 
         self.case.undoStopGlobal()
         self.notebook = NotebookModel(self.case)
+        self.time_tables = TimeTablesModel(self.case).getTablesDataDict()
+
         self.cht_model = ConjugateHeatTransferModel(self.case)
 
         self.lineEditValueThermal.textChanged[str].connect(self.slotValueThermal)
@@ -572,6 +575,11 @@ class BoundaryConditionsScalarsView(QWidget, Ui_BoundaryConditionsScalarsForm):
         for (nme, val) in self.notebook.getNotebookList():
             sym.append((nme, 'value (notebook) = ' + str(val)))
 
+        for _k in self.time_tables.keys():
+            for _h in self.time_tables[_k]:
+                sym.append(('{}[{}]'.format(_k,_h),
+                           'Variable "{}" of time table "{}"'.format(_h, _k)))
+
         c = self.__boundary.getScalarChoice(variable_name)
 
         dialog = QMegEditorView(parent      = self,
@@ -616,6 +624,11 @@ class BoundaryConditionsScalarsView(QWidget, Ui_BoundaryConditionsScalarsForm):
         for (nme, val) in self.notebook.getNotebookList():
             sym.append((nme, 'value (notebook) = ' + str(val)))
 
+        for _k in self.time_tables.keys():
+            for _h in self.time_tables[_k]:
+                sym.append(('{}[{}]'.format(_k,_h),
+                           'Variable "{}" of time table "{}"'.format(_h, _k)))
+
         c = self.__boundary.getScalarChoice(self.species)
         dialog = QMegEditorView(parent        = self,
                                 function_type = 'bnd',
@@ -659,6 +672,11 @@ class BoundaryConditionsScalarsView(QWidget, Ui_BoundaryConditionsScalarsForm):
 
         for (nme, val) in self.notebook.getNotebookList():
             sym.append((nme, 'value (notebook) = ' + str(val)))
+
+        for _k in self.time_tables.keys():
+            for _h in self.time_tables[_k]:
+                sym.append(('{}[{}]'.format(_k,_h),
+                           'Variable "{}" of time table "{}"'.format(_h, _k)))
 
         dialog = QMegEditorView(parent        = self,
                                 function_type = 'bnd',

commit 6d2339a30bb3b6b212b356ead7c1ee4405bbc24c

[MEG] Fix source terms generation with time tables
  * bug introduced in commit ba1a263797 (2023-05-15)

python/code_saturne/base/cs_meg_to_c.py
@@ -771,8 +771,8 @@ class meg_to_c_interpreter:
         for ktt in self.time_tables.keys():
             _kvals = self.time_tables[ktt]
             glob_tokens[ktt] = \
-                'const cs_real_t {var} = {func});'.format(var=_kvals[0], \
-                                                          func=_kvals[1])
+                'const cs_real_t {var} = {func};'.format(var=_kvals[0], \
+                                                         func=_kvals[1])
 
         for f in known_fields.keys():
             knf_name = known_fields[f]