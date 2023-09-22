code-saturne/code_saturne

commit d63e81e80bd51a84790def715521a3a977df2c1e

Code-code coupling: add a variant to perform the coupling in reverse mode.
We also make sources terms semi-implicit and update comments.

src/atmo/attycl.f90
@@ -332,7 +332,10 @@ do ifac = 1, nfabor
 !       face de sortie (si le flux est rentrant).
     zent = cdgfbo(3,ifac)
 
-    if (imbrication_flag .and.cressman_u) then
+    ! If specified by the user or by code-code coupling
+    if (rcodcl(ifac,iu,1).lt.rinfin*0.5d0) then
+      xuent = rcodcl(ifac,iu,1)
+    else if (imbrication_flag .and.cressman_u) then
       xuent = u_bord(ifac)
     else if (imeteo.eq.1) then
       call intprf &
@@ -343,7 +346,12 @@ do ifac = 1, nfabor
     endif
 
     xwent = 0.d0
-    if (imbrication_flag .and.cressman_v) then
+    if (rcodcl(ifac,iv,1).lt.rinfin*0.5d0) then
+      xvent = rcodcl(ifac,iv,1)
+      if (rcodcl(ifac,iw,1).lt.rinfin*0.5d0) then
+        xwent = rcodcl(ifac,iw,1)
+      endif
+    else if (imbrication_flag .and.cressman_v) then
       xvent = v_bord(ifac)
     else if (imeteo.eq.1) then
       call intprf &
@@ -354,7 +362,9 @@ do ifac = 1, nfabor
       xwent = cpro_met_vel(3, iel)
     endif
 
-    if (imbrication_flag .and.cressman_tke) then
+    if (rcodcl(ifac,ik,1).lt.rinfin*0.5d0) then
+      xkent = rcodcl(ifac,ik,1)
+    else if (imbrication_flag .and.cressman_tke) then
       xkent = tke_bord(ifac)
     else if (imeteo.eq.1) then
       call intprf &
@@ -364,7 +374,9 @@ do ifac = 1, nfabor
       xkent = cpro_met_k(iel)
     endif
 
-    if (imbrication_flag .and.cressman_eps) then
+    if (rcodcl(ifac,iep,1).lt.rinfin*0.5d0) then
+      xeent = rcodcl(ifac,iep,1)
+    else if (imbrication_flag .and.cressman_eps) then
       xeent = eps_bord(ifac)
     else if (imeteo.eq.1) then
       call intprf &
@@ -374,15 +386,18 @@ do ifac = 1, nfabor
       xeent = cpro_met_eps(iel)
     endif
 
-    if(imbrication_flag .and.cressman_theta                          &
-       .and. ippmod(iatmos).ge.1 ) then
-       tpent = theta_bord(ifac)
-    else if (imeteo.eq.1) then
-      call intprf &
-      (nbmett, nbmetm,                                               &
-       ztmet, tmmet, tpmet, zent  , ttcabs, tpent )
-    else
-      tpent = cpro_met_potemp(iel)
+    if (ippmod(iatmos).ge.1) then
+      if (rcodcl(ifac,iep,1).lt.rinfin*0.5d0) then
+        tpent = rcodcl(ifac,isca(iscalt),1)
+      else if(imbrication_flag .and.cressman_theta) then
+        tpent = theta_bord(ifac)
+      else if (imeteo.eq.1) then
+        call intprf &
+          (nbmett, nbmetm,                                               &
+          ztmet, tmmet, tpmet, zent  , ttcabs, tpent )
+      else
+        tpent = cpro_met_potemp(iel)
+      endif
     endif
 
     vs = xuent*surfbo(1,ifac) + xvent*surfbo(2,ifac)

src/base/cs_c_bindings.f90
@@ -637,12 +637,13 @@ module cs_c_bindings
     ! Exchange of coupling variables between
     ! tow instances of code_saturne thanks to cells.
 
-    subroutine csccel(f_id, st_exp) &
+    subroutine csccel(f_id, st_exp, st_imp) &
      bind(C, name='cs_sat_coupling_exchange_at_cells')
      use, intrinsic :: iso_c_binding
      implicit none
      integer(c_int), value :: f_id
      real(kind=c_double), dimension(*), intent(inout) :: st_exp
+     real(kind=c_double), dimension(*), intent(inout) :: st_imp
     end subroutine
 
     !---------------------------------------------------------------------------

src/base/cs_prototypes.h
@@ -422,7 +422,8 @@ cs_fortran_resize_aux_arrays(void);
 
 void
 cs_sat_coupling_exchange_at_cells(int         f_id,
-                                  cs_real_t   st_exp[]);
+                                  cs_real_t   st_exp[],
+                                  cs_real_t   st_imp[]);
 
 /*----------------------------------------------------------------------------*/
 /*!

src/base/cs_sat_coupling.c
@@ -89,6 +89,7 @@ typedef struct {
   char    *face_loc_sel_c;  /* Face selection criteria */
   char    *cell_loc_sel_c;  /* Cell selection criteria */
   int      verbosity;       /* Verbosity level */
+  int      reverse;         /* Reverse mode if 1 */
 
 } _cs_sat_coupling_builder_t;
 
@@ -97,11 +98,12 @@ struct _cs_sat_coupling_t {
   char                   *sat_name;        /* Application name */
   cs_sat_coupling_tag_t  *tag_func;        /* Tagging function pointer */
   void                   *tag_context;     /* Tagging context */
+  int                    reverse;          /* Reverse mode */
 
-  char            *face_cpl_sel; /* Face selection criteria */
-  char            *cell_cpl_sel; /* Face selection criteria */
-  char            *face_loc_sel; /* Face selection criteria */
-  char            *cell_loc_sel; /* Face selection criteria */
+  char            *face_cpl_sel; /* Local face overlapped selection criteria */
+  char            *cell_cpl_sel; /* Local cell overlapped selection criteria */
+  char            *face_loc_sel; /* Distant face overlapped selection criteria */
+  char            *cell_loc_sel; /* Distant cell overlapped selection criteria */
 
   ple_locator_t   *localis_cel;  /* Locator associated with cells */
   ple_locator_t   *localis_fbr;  /* Locator associated with boundary faces */
@@ -115,8 +117,8 @@ struct _cs_sat_coupling_t {
                                     interpolated*/
 
   cs_real_t       *distant_dist_fbr; /* Distant vectors (distance JJ') */
-  cs_real_t       *distant_of;
-  cs_real_t       *local_of;
+  cs_real_t       *distant_of;       /* Distant vector OF */
+  cs_real_t       *local_of;         /* Local vector OF */
   cs_real_t       *distant_pond_fbr; /* Distant weighting coefficient */
   cs_real_t       *local_pond_fbr;   /* Local weighting coefficient */
 
@@ -333,6 +335,7 @@ _sat_add_mpi(int builder_id,
                       scb->face_loc_sel_c,
                       scb->cell_loc_sel_c,
                       scb->app_name,
+                      scb->reverse,
                       scb->verbosity);
 
   sat_coupling = cs_sat_coupling_by_id(cs_sat_coupling_n_couplings() - 1);
@@ -813,7 +816,9 @@ _sat_coupling_interpolate(cs_sat_coupling_t  *couplage)
 
     surface = sqrt(surface);
 
+    /* n/norm(n) . IF = FJ' */
     distance_fbr_cel /= surface;
+    /* n/norm(n) . IJ = I'J'*/
     distance_cel_cel /= surface;
 
     const cs_real_3_t *restrict b_face_cog
@@ -943,6 +948,7 @@ void CS_PROCF (defloc, DEFLOC)
 
     BFT_MALLOC(c_elt_list, cs_glob_mesh->n_cells, cs_lnum_t);
 
+    /* get the number  and a list of coupled local cells */
     cs_selector_get_cell_list(coupl->cell_loc_sel,
                               &(coupl->nbr_cel_sup),
                               c_elt_list);
@@ -953,14 +959,15 @@ void CS_PROCF (defloc, DEFLOC)
 
     BFT_MALLOC(f_elt_list, cs_glob_mesh->n_b_faces, cs_lnum_t);
 
+    /* get the number and a list of coupled local faces */
     cs_selector_get_b_face_list(coupl->face_loc_sel,
                                 &(coupl->nbr_fbr_sup),
                                 f_elt_list);
 
   }
 
-  if (coupl->nbr_cel_sup > 0) indic_loc[0] = 1;
-  if (coupl->nbr_fbr_sup > 0) indic_loc[1] = 1;
+  if (coupl->nbr_cel_sup > 0) indic_loc[0] = 1; /* have coupled cells */
+  if (coupl->nbr_fbr_sup > 0) indic_loc[1] = 1; /* have coupled faces */
 
   for (ind = 0 ; ind < 2 ; ind++)
     indic_glob[ind] = indic_loc[ind];
@@ -971,6 +978,7 @@ void CS_PROCF (defloc, DEFLOC)
                    cs_glob_mpi_comm);
 #endif
 
+  /* One rank has coupled cells */
   if (indic_glob[0] > 0) {
 
     sprintf(coupled_mesh_name, _("coupled_cells_%d"), *numcpl);
@@ -983,6 +991,7 @@ void CS_PROCF (defloc, DEFLOC)
 
   }
 
+  /* One rank has coupled faces */
   if (indic_glob[1] > 0) {
 
     sprintf(coupled_mesh_name, _("coupled_faces_%d"), *numcpl);
@@ -1114,7 +1123,7 @@ void CS_PROCF (defloc, DEFLOC)
 
 #if 0
   /* TODO: associate the FVM meshes to the post-processing,
-     with a fonction giving a pointer to the associated FVM structures,
+     with a function giving a pointer to the associated FVM structures,
      and another enabling its compacting or removing */
   {
     fvm_writer_t *w = fvm_writer_init("coupled_mesh",
@@ -1152,6 +1161,7 @@ void CS_PROCF (defloc, DEFLOC)
  * *****************
  *
  * INTEGER          NUMCPL         : --> : coupling number
+ * INTEGER          reverse        : <-- : reverse mode if 1
  * INTEGER          NCESUP         : <-- : number of "support" cells
  * INTEGER          NFBSUP         : <-- : number of "support" boundary faces
  * INTEGER          NCECPL         : <-- : number of coupled cells
@@ -1165,6 +1175,7 @@ void CS_PROCF (defloc, DEFLOC)
 void CS_PROCF (nbecpl, NBECPL)
 (
  const int        *numcpl,
+       int        *reverse,
        cs_lnum_t  *ncesup,
        cs_lnum_t  *nfbsup,
        cs_lnum_t  *ncecpl,
@@ -1183,6 +1194,8 @@ void CS_PROCF (nbecpl, NBECPL)
               *numcpl, cs_glob_sat_n_couplings);
   else
     coupl = cs_glob_sat_couplings[*numcpl - 1];
+
+  *reverse = coupl->reverse;
 
   *ncesup = coupl->nbr_cel_sup;
   *nfbsup = coupl->nbr_fbr_sup;
@@ -1425,6 +1438,7 @@ void CS_PROCF (npdcpl, NPDCPL)
  *                                 :     :   each point
  * DOUBLE PRECISION coopts(3,*)    : <-- : distant point coordinates
  * DOUBLE PRECISION djppts(3,*)    : <-- : distant vectors to the coupled face
+ *                                 :     :  (JJ')
  * DOUBLE PRECISION pndpts(*)      : <-- : distant weighting coefficients
  *----------------------------------------------------------------------------*/
 
@@ -1602,8 +1616,8 @@ void CS_PROCF (pondcp, PONDCP)
  *                                 :     : 2 : variables defined at faces
  * INTEGER          STRIDE         : --> : 1 : for scalars
  *                                 :     : 3 : for vectors
- * DOUBLE PRECISION VARDIS(*)      : --> : distant variable(to send)
- * DOUBLE PRECISION VARLOC(*)      : <-- : local variable (to receive)
+ * DOUBLE PRECISION vardis         : --> : distant variable(to send)
+ * DOUBLE PRECISION varloc         : <-- : local variable (to receive)
  *----------------------------------------------------------------------------*/
 
 void CS_PROCF (varcpl, VARCPL)
@@ -1613,8 +1627,10 @@ void CS_PROCF (varcpl, VARCPL)
  const cs_lnum_t  *nbrloc,
  const int        *ityvar,
  const cs_lnum_t  *stride,
+ const int        *reverse,
        cs_real_t  *vardis,
        cs_real_t  *varloc
+
 )
 {
   cs_lnum_t  n_val_dist_ref = 0;
@@ -1672,7 +1688,7 @@ void CS_PROCF (varcpl, VARCPL)
                                    NULL,
                                    sizeof(cs_real_t),
                                    *stride,
-                                   0);
+                                   *reverse);
   }
 
 }
@@ -1824,6 +1840,7 @@ void CS_PROCF (mxicpl, MXICPL)
  * \param[in] boundary_loc_criteria boundary face selection criteria for location
  *                                  (not functional)
  * \param[in] volume_loc_criteria   cell selection criteria for location
+ * \param[in] reverse               reverse mode if 1
  * \param[in] verbosity             verbosity level
  *
  * In the case of only 2 code_saturne instances, the 'saturne_name' argument
@@ -1840,6 +1857,7 @@ cs_sat_coupling_define(const char  *saturne_name,
                        const char  *volume_cpl_criteria,
                        const char  *boundary_loc_criteria,
                        const char  *volume_loc_criteria,
+                       int          reverse,
                        int          verbosity)
 {
   _cs_sat_coupling_builder_t *scb = NULL;
@@ -1884,6 +1902,7 @@ cs_sat_coupling_define(const char  *saturne_name,
     strcpy(scb->cell_loc_sel_c, volume_loc_criteria);
   }
 
+  scb->reverse = reverse;
   scb->verbosity = verbosity;
 
   _sat_coupling_builder_size += 1;
@@ -1966,6 +1985,7 @@ cs_sat_coupling_all_init(void)
  *   face_sel_criterion <-- criterion for selection of boundary faces
  *   cell_sel_criterion <-- criterion for selection of cells
  *   sat_name           <-- code_saturne application name
+ *   reverse            <-- reverse mode if 1
  *   verbosity          <-- verbosity level
  *----------------------------------------------------------------------------*/
 
@@ -1975,6 +1995,7 @@ cs_sat_coupling_add(const char  *face_cpl_sel_c,
                     const char  *face_loc_sel_c,
                     const char  *cell_loc_sel_c,
                     const char  *sat_name,
+                    int          reverse,
                     int          verbosity)
 {
   cs_sat_coupling_t *sat_coupling = NULL;
@@ -1988,6 +2009,7 @@ cs_sat_coupling_add(const char  *face_cpl_sel_c,
   sat_coupling->sat_name = NULL;
   sat_coupling->tag_func = NULL;
   sat_coupling->tag_context = NULL;
+  sat_coupling->reverse = reverse;
 
   if (sat_name != NULL) {
     BFT_MALLOC(sat_coupling->sat_name, strlen(sat_name) + 1, char);
@@ -2064,6 +2086,7 @@ cs_sat_coupling_add(const char  *face_cpl_sel_c,
  *   boundary_criteria <-- boundary face selection criteria, or NULL
  *   volume_criteria   <-- volume cell selection criteria, or NULL
  *   loc_tolerance     <-- location tolerance factor (0.1 recommended)
+ *   reverse           <-- reverse mode if 1
  *   verbosity         <-- verbosity level
  *----------------------------------------------------------------------------*/
 
@@ -2075,6 +2098,7 @@ cs_sat_coupling_add_internal(cs_sat_coupling_tag_t  *tag_func,
                              const char             *boundary_loc_criteria,
                              const char             *volume_loc_criteria,
                              float                   loc_tolerance,
+                             int                     reverse,
                              int                     verbosity)
 {
   CS_UNUSED(loc_tolerance);
@@ -2084,6 +2108,7 @@ cs_sat_coupling_add_internal(cs_sat_coupling_tag_t  *tag_func,
                       boundary_loc_criteria,
                       volume_loc_criteria,
                       NULL,
+                      reverse,
                       verbosity);
 
   cs_sat_coupling_t *sat_coupling

src/base/cs_sat_coupling.h
@@ -143,6 +143,7 @@ void CS_PROCF (defloc, DEFLOC)
  * *****************
  *
  * INTEGER          NUMCPL         : --> : coupling number
+ * INTEGER          reverse        : <-- : reverse mode if 1
  * INTEGER          NCESUP         : <-- : number of "support" cells
  * INTEGER          NFBSUP         : <-- : number of "support" boundary faces
  * INTEGER          NCECPL         : <-- : number of coupled cells
@@ -156,6 +157,7 @@ void CS_PROCF (defloc, DEFLOC)
 void CS_PROCF (nbecpl, NBECPL)
 (
  const int        *numcpl,
+       int        *reverse,
        cs_lnum_t  *ncesup,
        cs_lnum_t  *nfbsup,
        cs_lnum_t  *ncecpl,
@@ -334,6 +336,8 @@ void CS_PROCF (varcpl, VARCPL)
  const cs_lnum_t  *nbrloc,
  const int        *ityvar,
  const cs_lnum_t  *stride,
+ const int        *reverse,
+
        cs_real_t  *vardis,
        cs_real_t  *varloc
 );
@@ -405,6 +409,7 @@ void CS_PROCF (mxicpl, MXICPL)
  * \param[in] boundary_loc_criteria boundary face selection criteria for location
  *                                  (not functional)
  * \param[in] volume_loc_criteria   cell selection criteria for location
+ * \param[in] reverse               reverse mode if 1
  * \param[in] verbosity             verbosity level
  *
  * In the case of only 2 code_saturne instances, the 'saturne_name' argument
@@ -421,6 +426,7 @@ cs_sat_coupling_define(const char  *saturne_name,
                        const char  *volume_cpl_criteria,
                        const char  *boundary_loc_criteria,
                        const char  *volume_loc_criteria,
+                       int          reverse,
                        int          verbosity);
 
 /*----------------------------------------------------------------------------
@@ -454,6 +460,7 @@ cs_sat_coupling_by_id(int coupling_id);
  *   face_sel_criterion <-- criterion for selection of boundary faces
  *   cell_sel_criterion <-- criterion for selection of cells
  *   sat_name           <-- code_saturne application name
+ *   reverse            <-- reverse mode if 1
  *   verbosity          <-- verbosity level
  *----------------------------------------------------------------------------*/
 
@@ -463,6 +470,7 @@ cs_sat_coupling_add(const char  *face_cpl_sel_c,
                     const char  *face_loc_sel_c,
                     const char  *cell_loc_sel_c,
                     const char  *sat_name,
+                    int          reverse,
                     int          verbosity);
 
 /*----------------------------------------------------------------------------
@@ -474,6 +482,7 @@ cs_sat_coupling_add(const char  *face_cpl_sel_c,
  *   boundary_criteria <-- boundary face selection criteria, or NULL
  *   volume_criteria   <-- volume cell selection criteria, or NULL
  *   loc_tolerance     <-- location tolerance factor (0.1 recommended)
+ *   reverse           <-- reverse mode if 1
  *   verbosity         <-- verbosity level
  *----------------------------------------------------------------------------*/
 
@@ -485,6 +494,7 @@ cs_sat_coupling_add_internal(cs_sat_coupling_tag_t  *tag_func,
                              const char             *boundary_loc_criteria,
                              const char             *volume_loc_criteria,
                              float                   loc_tolerance,
+                             int                     reverse,
                              int                     verbosity);
 
 /*----------------------------------------------------------------------------

src/base/cs_solve_equation.c
@@ -1038,7 +1038,7 @@ cs_solve_equation_scalar(cs_field_t        *f,
   /* Coupling between multiple code_saturne instances */
   const int nbrcpl = cs_sat_coupling_n_couplings();
   if (nbrcpl > 0)
-    cs_sat_coupling_exchange_at_cells(f->id, rhs);
+    cs_sat_coupling_exchange_at_cells(f->id, rhs, fimp);
 
   /* Store the source terms for convective limiter
      or time extrapolation for buoyant scalar */
@@ -2028,6 +2028,13 @@ cs_solve_equation_vector(cs_field_t       *f,
                        f->id,
                        (cs_real_t *)rhs,
                        (cs_real_t *)fimp);
+
+  /* Coupling between multiple code_saturne instances */
+  const int nbrcpl = cs_sat_coupling_n_couplings();
+  if (nbrcpl > 0)
+    cs_sat_coupling_exchange_at_cells(f->id,
+                                      (cs_real_t *)rhs,
+                                      (cs_real_t *)fimp);
 
   /* Store the source terms for convective limiter */
   cs_real_3_t *cpro_vect_st = NULL;

src/base/cs_solve_navier_stokes.c
@@ -858,8 +858,9 @@ _ext_forces(const cs_mesh_t                *m,
       cs_real_t dvol = 0;
       if (cs_mesh_quantities_cell_is_active(mq, c_id) == 1)
         dvol = 1.0/cell_f_vol[c_id];
+      /* FIXME we should add tsimp*vela to tsexp as for head losses */
       for (cs_lnum_t ii = 0; ii < 3; ii++)
-        dfrcxt[c_id][ii] += tsexp[c_id][ii]*dvol;
+        dfrcxt[c_id][ii] += tsexp[c_id][ii] *dvol;
     }
   }
 
@@ -1804,7 +1805,9 @@ _velocity_prediction(const cs_mesh_t             *m,
 
   /* Coupling between two code_saturne instances */
   if (cs_sat_coupling_n_couplings() > 0)
-    cs_sat_coupling_exchange_at_cells(CS_F_(vel)->id, (cs_real_t*)tsexp);
+    cs_sat_coupling_exchange_at_cells(CS_F_(vel)->id,
+                                      (cs_real_t*)tsexp,
+                                      (cs_real_t*)tsimp);
 
   if (eqp_u->ibdtso > 1 && ts->nt_cur > ts->nt_ini
       && (   tso->idtvar == CS_TIME_STEP_CONSTANT

src/base/cs_turbomachinery.c
@@ -1578,6 +1578,7 @@ cs_turbomachinery_coupling_add(const char  *sel_criteria,
                                NULL,
                                "all[]",
                                tolerance,
+                               0, /* reverse */
                                verbosity);
 
   _turbomachinery->n_couplings += 1;

commit 7419fe0da483abc4c571e22e96f14ea36433b422

[Atmo] fix bug introduced in commit d63e81e80 (2023-09-21).
Boundary conditions was not protected for other models than k-eps.

src/atmo/attycl.f90
@@ -362,9 +362,7 @@ do ifac = 1, nfabor
       xwent = cpro_met_vel(3, iel)
     endif
 
-    if (rcodcl(ifac,ik,1).lt.rinfin*0.5d0) then
-      xkent = rcodcl(ifac,ik,1)
-    else if (imbrication_flag .and.cressman_tke) then
+    if (imbrication_flag .and.cressman_tke) then
       xkent = tke_bord(ifac)
     else if (imeteo.eq.1) then
       call intprf &
@@ -373,10 +371,13 @@ do ifac = 1, nfabor
     else
       xkent = cpro_met_k(iel)
     endif
+    if (ik.ge.1) then
+      if (rcodcl(ifac,ik,1).lt.rinfin*0.5d0) then
+        xkent = rcodcl(ifac,ik,1)
+      endif
+    endif
 
-    if (rcodcl(ifac,iep,1).lt.rinfin*0.5d0) then
-      xeent = rcodcl(ifac,iep,1)
-    else if (imbrication_flag .and.cressman_eps) then
+    if (imbrication_flag .and.cressman_eps) then
       xeent = eps_bord(ifac)
     else if (imeteo.eq.1) then
       call intprf &
@@ -384,6 +385,11 @@ do ifac = 1, nfabor
        zdmet, tmmet, epmet, zent  , ttcabs, xeent )
     else
       xeent = cpro_met_eps(iel)
+    endif
+    if (iep.ge.1) then
+      if (rcodcl(ifac,iep,1).lt.rinfin*0.5d0) then
+        xeent = rcodcl(ifac,iep,1)
+      endif
     endif
 
     if (ippmod(iatmos).ge.1) then