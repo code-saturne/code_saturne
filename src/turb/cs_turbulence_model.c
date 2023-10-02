code-saturne/code_saturne

commit 93862a350df64583d52b5edf8afcfdcdd7c5224e

Migrate initi2 to C.

src/turb/cs_turbulence_model.c
@@ -2037,8 +2037,8 @@ cs_turb_model_log_setup(void)
 /*----------------------------------------------------------------------------*/
 /*!
  * \brief Print the turbulent constants to setup.log.
- *
- *----------------------------------------------------------------------------*/
+ */
+/*----------------------------------------------------------------------------*/
 
 void
 cs_turb_constants_log_setup(void)
@@ -2256,6 +2256,36 @@ cs_turb_constants_log_setup(void)
                   cs_turb_c1trit, cs_turb_c2trit, cs_turb_c3trit,
                   cs_turb_c4trit);
 
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Compute characteristic length for turbulence if not already done.
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_turb_init_ref_quantities(void)
+{
+  const cs_mesh_quantities_t *mq = cs_glob_mesh_quantities;
+
+  if (_turb_ref_values.almax < 999)
+    _turb_ref_values.almax = pow(mq->tot_vol, 1./3.);
+
+  if (   _turb_model.type == CS_TURB_RANS
+      || _turb_model.type == CS_TURB_HYBRID) {
+
+    cs_log_printf
+      (CS_LOG_DEFAULT,
+       _("\n"
+         " Reference quantities for turbulence:\n"
+         " ------------------------------------\n\n"
+         "   almax: %11.3g (caracteristic length for initialization)\n"
+         "   uref:  %11.3g (reference velocity))\n"),
+       _turb_ref_values.almax,
+       _turb_ref_values.uref);
+
+  }
 }
 
 /*----------------------------------------------------------------------------*/

src/base/Makefile.am
@@ -388,7 +388,6 @@ idrbla.f90 \
 impini.f90 \
 iniini.f90 \
 initi1.f90 \
-initi2.f90 \
 iniusi.f90 \
 iniva0.f90 \
 inivar.f90 \

src/base/caltri.f90
@@ -189,6 +189,14 @@ interface
 
   !=============================================================================
 
+  subroutine cs_turb_init_ref_quantities()  &
+    bind(C, name='cs_turb_init_ref_quantities')
+    use, intrinsic :: iso_c_binding
+    implicit none
+  end subroutine cs_turb_init_ref_quantities
+
+  !=============================================================================
+
   subroutine cs_les_inflow_initialize()  &
     bind(C, name='cs_les_inflow_initialize')
     use, intrinsic :: iso_c_binding
@@ -307,7 +315,7 @@ endif
 ! End of modules initialization
 !===============================================================================
 
-call initi2
+call cs_turb_init_ref_quantities();
 
 ! First pass for every subroutine requiring pass count
 iappel = 1

src/base/initi2.f90
@@ -1,79 +1,0 @@
-!-------------------------------------------------------------------------------
-
-! This file is part of code_saturne, a general-purpose CFD tool.
-!
-! Copyright (C) 1998-2023 EDF S.A.
-!
-! This program is free software; you can redistribute it and/or modify it under
-! the terms of the GNU General Public License as published by the Free Software
-! Foundation; either version 2 of the License, or (at your option) any later
-! version.
-!
-! This program is distributed in the hope that it will be useful, but WITHOUT
-! ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-! FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-! details.
-!
-! You should have received a copy of the GNU General Public License along with
-! this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
-! Street, Fifth Floor, Boston, MA 02110-1301, USA.
-
-!-------------------------------------------------------------------------------
-
-!> \file initi2.f90
-!> \brief End of commons initialization.
-!>
-!------------------------------------------------------------------------------
-
-subroutine initi2
-
-!===============================================================================
-! Module files
-!===============================================================================
-
-use paramx
-use dimens
-use optcal
-use entsor
-use cstphy
-use mesh
-
-!===============================================================================
-
-implicit none
-
-! Arguments
-
-! Local variables
-
-!===============================================================================
-
-! If almax < 0, recompute it
-
-write(nfecra, 1000)
-
-if (almax.le.0.d0) then
-  almax = voltot**(1.d0/3.d0)
-  write(nfecra, 1100) almax
-  write(nfecra, 1102)
-  if (itytur.eq.2.or.itytur.eq.3                  &
-       .or. itytur.eq.5 .or. iturb.eq.60          &
-       .or. iturb.eq.70) then
-    write(nfecra, 1101)
-  endif
-endif
-
- 1000 format('')
- 1100 format(                                                           &
-'       ALMAX  = ', e14.5, ' (Characteristic length)')
- 1101 format(                                                           &
-'       ALMAX is the length used to initialize the turbulence.')
- 1102 format(                                                           &
-'       ALMAX is the cubic root of the domain volume.',/)
-
-!===============================================================================
-! End
-!===============================================================================
-
-return
-end subroutine

src/turb/cs_turbulence_model.h
@@ -496,6 +496,15 @@ cs_turb_model_log_setup(void);
 void
 cs_turb_constants_log_setup(void);
 
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Compute characteristic length for turbulence if not already done.
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_turb_init_ref_quantities(void);
+
 /*----------------------------------------------------------------------------*
  * Clip turbulent fluxes
  *----------------------------------------------------------------------------*/

commit 62d58ca396c24687bc297a61a78123e11687f9f8

Fix for user initialization of almax, broken by commit 93862a35 (2023-01-27).

src/turb/cs_turbulence_model.c
@@ -369,8 +369,8 @@ const cs_turb_model_t  *cs_glob_turb_model = NULL;
 static cs_turb_ref_values_t
 _turb_ref_values =
 {
-  .almax      = -999,
-  .uref       =-1e13
+  .almax =  -999,
+  .uref  = -1e13
 };
 
 const cs_turb_ref_values_t  *cs_glob_turb_ref_values = &_turb_ref_values;
@@ -2238,7 +2238,7 @@ cs_turb_init_ref_quantities(void)
 {
   const cs_mesh_quantities_t *mq = cs_glob_mesh_quantities;
 
-  if (_turb_ref_values.almax < 999)
+  if (_turb_ref_values.almax < 0)
     _turb_ref_values.almax = pow(mq->tot_vol, 1./3.);
 
   if (   _turb_model.type == CS_TURB_RANS