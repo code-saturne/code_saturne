code-saturne/code_saturne

commit 56bf05e6389e7a4ac163aae737cefabb7c9e9d21

Move usvist to C (in cs_user_physical_properties_turb_viscosity).
Also remove dedicated routine to change Smagorinsky contant because
it can be done in this routine now.
Also refurbished the computation of mu_t for LES.

src/user_examples/cs_user_physical_properties.c
@@ -256,34 +256,46 @@ cs_user_physical_properties_t_to_h(cs_domain_t      *domain,
 
 /*----------------------------------------------------------------------------*/
 /*!
- * \brief User modification of the Smagorinsky constant for the
- *        dynamic Smagorinsky model.
+ * \brief User modification of the turbulence viscosity.
  *
- * CS = Mij.Lij / Mij.Mij
- *
- * The local averages of the numerator and denominator are done before calling
- * this function, so
- *
- * CS = < Mij.Lij > / < Mij.Mij >
- *
- * In this subroutine, Mij.Lij and Mij.Mij are passed as arguments before
- * the local average.
+ * Turbulent viscosity \f$ \mu_T \f$ (kg/(m s)) can be modified.
+ * You can access the field by its name.
  *
  * \param[in, out]   domain      pointer to a cs_domain_t structure
- * \param[in]        mijlij      mij.lij before the local averaging
- * \param[in]        mijmij      mij.mij before the local averaging
  */
 /*----------------------------------------------------------------------------*/
 
 void
-cs_user_physical_properties_smagorinsky_c(cs_domain_t      *domain,
-                                          const cs_real_t   mijlij[],
-                                          const cs_real_t   mijmij[])
+cs_user_physical_properties_turb_viscosity(cs_domain_t      *domain)
 {
+
+  cs_lnum_t n_cells_ext = domain->mesh->n_cells_with_ghosts;
+
+  /* Recompute Mij:Mij and Lij:Mij */
+  cs_real_t  *mijlij;
+  cs_real_t  *mijmij;
+  cs_real_t *s_n, *sf_n;
+  cs_real_3_t *f_vel;
+  BFT_MALLOC(mijlij, n_cells_ext, cs_real_t);
+  BFT_MALLOC(mijmij, n_cells_ext, cs_real_t);
+  BFT_MALLOC(s_n, n_cells_ext, cs_real_t);
+  BFT_MALLOC(sf_n, n_cells_ext, cs_real_t);
+  BFT_MALLOC(f_vel, n_cells_ext, cs_real_3_t);
+
+  /* Compute:
+   *   s_n (aka sqrt(2SijSij))
+   *   sf_n (aka ||<rho*S>/<rho>||),
+   *   filtered vel
+   *   Mij:Mij
+   *   Lij:Mij
+   */
+  cs_les_mu_t_smago_dyn_prepare(s_n, sf_n, f_vel, mijmij, mijlij);
+
   const cs_lnum_t n_cells = domain->mesh->n_cells;
   const cs_real_t tot_vol = domain->mesh_quantities->tot_vol;
   const cs_real_t *cell_vol = domain->mesh_quantities->cell_vol;
 
+  /* Overwrite Smagorinsky constant */
   cs_real_t *cpro_smago
     = cs_field_by_name("smagorinsky_constant^2")->val;
 
@@ -294,6 +306,13 @@ cs_user_physical_properties_smagorinsky_c(cs_domain_t      *domain,
     mijmijmoy += mijmij[c_id]*cell_vol[c_id];
   }
 
+  /* Free memory */
+  BFT_FREE(s_n);
+  BFT_FREE(sf_n);
+  BFT_FREE(f_vel);
+  BFT_FREE(mijmij);
+  BFT_FREE(mijlij);
+
   cs_parall_sum(1, CS_REAL_TYPE, &mijlijmoy);
   cs_parall_sum(1, CS_REAL_TYPE, &mijmijmoy);
 
@@ -302,6 +321,19 @@ cs_user_physical_properties_smagorinsky_c(cs_domain_t      *domain,
 
   for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++)
     cpro_smago[c_id] = mijlijmoy/mijmijmoy;
+
+  /* Calculation of (dynamic) viscosity
+   * ================================== */
+
+  cs_real_t *visct = CS_F_(mu)->val;
+  const cs_real_t *crom  = CS_F_(rho)->val;
+  for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++) {
+    const cs_real_t coef = cpro_smago[c_id];
+    const cs_real_t delta = cs_turb_xlesfl * pow(cs_turb_ales*cell_vol[c_id],
+                                                 cs_turb_bles);
+    visct[c_id] = crom[c_id]*coef*cs_math_pow2(delta)*s_n[c_id];
+  }
+
 }
 
 /*----------------------------------------------------------------------------*/

src/user/cs_user_physical_properties.c
@@ -143,34 +143,21 @@ cs_user_physical_properties_t_to_h(cs_domain_t      *domain,
 
 /*----------------------------------------------------------------------------*/
 /*!
- * \brief User modification of the Smagorinsky constant for the
- *        dynamic Smagorinsky model.
+ * \brief User modification of the turbulence viscosity.
  *
- * CS = Mij.Lij / Mij.Mij
- *
- * The local averages of the numerator and denominator are done before calling
- * this function, so
- *
- * CS = < Mij.Lij > / < Mij.Mij >
- *
- * In this subroutine, Mij.Lij and Mij.Mij are passed as arguments before
- * the local average.
+ * Turbulent viscosity \f$ \mu_T \f$ (kg/(m s)) can be modified.
+ * You can access the field by its name.
  *
  * \param[in, out]   domain      pointer to a cs_domain_t structure
- * \param[in]        mijlij      mij.lij before the local averaging
- * \param[in]        mijmij      mij.mij before the local averaging
  */
 /*----------------------------------------------------------------------------*/
 
-#pragma weak cs_user_physical_properties_smagorinsky_c
+#pragma weak cs_user_physical_properties_turb_viscosity
 void
-cs_user_physical_properties_smagorinsky_c(cs_domain_t      *domain,
-                                          const cs_real_t   mijlij[],
-                                          const cs_real_t   mijmij[])
+cs_user_physical_properties_turb_viscosity(cs_domain_t      *domain)
 {
   CS_NO_WARN_IF_UNUSED(domain);
 }
-
 
 /*----------------------------------------------------------------------------*/
 /*!

src/base/cs_base_fortran.c
@@ -541,6 +541,12 @@ cs_user_physical_properties_wrapper(void)
 }
 
 void
+cs_user_physical_properties_turb_viscosity_wrapper(void)
+{
+  cs_user_physical_properties_turb_viscosity(cs_glob_domain);
+}
+
+void
 cs_user_source_terms_wrapper(int         f_id,
                              cs_real_t  *st_exp,
                              cs_real_t  *st_imp)

src/base/cs_c_bindings.f90
@@ -2133,6 +2133,16 @@ module cs_c_bindings
 
     !---------------------------------------------------------------------------
 
+    ! Interface to C user function for physical properties
+
+    subroutine cs_user_physical_properties_turb_viscosity_wrapper()  &
+      bind(C, name='cs_user_physical_properties_turb_viscosity_wrapper')
+      use, intrinsic :: iso_c_binding
+      implicit none
+    end subroutine cs_user_physical_properties_turb_viscosity_wrapper
+
+    !---------------------------------------------------------------------------
+
     ! Interface to C user function
 
     subroutine user_source_terms(f_id, st_exp, st_imp)  &

src/base/cs_math.h
@@ -685,6 +685,43 @@ cs_math_sym_33_3_product_add(const cs_real_t  m[6],
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief Compute the product of two symmetric matrices of 3x3 real values
+ * and take the trace.
+ * NB: Symmetric matrix are stored as follows (s11, s22, s33, s12, s23, s13)
+ * \return trace
+ *
+ * \param[in]     m1            matrix of 3x3 real values
+ * \param[in]     m2            matrix of 3x3 real values
+ */
+/*----------------------------------------------------------------------------*/
+
+static inline cs_real_t
+cs_math_sym_33_sym_33_product_trace(const cs_real_t  m1[6],
+                                    const cs_real_t  m2[6])
+{
+  return m1[0]*m2[0] + 2.*m1[3]*m2[3] + 2.*m1[5]*m2[5]
+                     +    m1[1]*m2[1] + 2.*m1[4]*m2[4]
+                                      +    m1[2]*m2[2];
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Compute the trace of a 3x3 tensor.
+ *
+ * \param[in]   t   tensor of 3x3 real values
+ *
+ * \return trace (t[0][0] + t[1][1] + t[2][2])
+ */
+/*----------------------------------------------------------------------------*/
+
+static inline cs_real_t
+cs_math_33_trace(const cs_real_t  t[3][3])
+{
+  return (t[0][0] + t[1][1] + t[2][2]);
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief Compute the trace of a symmetric tensor.
  *
  * \param[in]   t   vector of 6 real values (symmetric tensor)

src/base/cs_parameters_check.c
@@ -1864,7 +1864,7 @@ cs_parameters_check(void)
              "Use fully extended neighborhood or directly "
              "define the averaging of the\n"
              "dynamic Smagorinsky constant via the\n"
-             "cs_user_physical_properties_smagorinsky_c function."));
+             "cs_user_physical_properties_turb_viscosity function."));
         break;
       default:
         break;

src/base/cs_prototypes.h
@@ -832,29 +832,14 @@ cs_user_physical_properties_td_pressure(cs_real_t  *td_p);
 
 /*----------------------------------------------------------------------------*/
 /*!
- * \brief User modification of the Smagorinsky constant for the
- *        dynamic Smagorinsky model.
- *
- * CS = Mij.Lij / Mij.Mij
- *
- * The local averages of the numerator and denominator are done before calling
- * this function, so
- *
- * CS = < Mij.Lij > / < Mij.Mij >
- *
- * In this subroutine, Mij.Lij and Mij.Mij are passed as arguments before
- * the local average.
+ * \brief User modification of the turbulence viscosity
  *
  * \param[in, out]   domain      pointer to a cs_domain_t structure
- * \param[in]        mijlij      mij.lij before the local averaging
- * \param[in]        mijmij      mij.mij before the local averaging
  */
 /*----------------------------------------------------------------------------*/
 
 void
-cs_user_physical_properties_smagorinsky_c(cs_domain_t      *domain,
-                                          const cs_real_t   mijlij[],
-                                          const cs_real_t   mijmij[]);
+cs_user_physical_properties_turb_viscosity(cs_domain_t      *domain);
 
 /*----------------------------------------------------------------------------*/
 /*!

src/base/paramx.f90
@@ -263,10 +263,11 @@ module paramx
              cs_user_parameters, cs_user_physical_properties,         &
              cs_user_turbulence_source_terms,                         &
              cs_user_turbulence_source_terms2,                        &
-             cs_user_atmo_soil
+             cs_user_atmo_soil, cs_user_physical_properties_smagorinsky_c
+
 
   integer :: usalcl, usatdv, usati1, usatph, usatsoil, ushist,        &
              usporo, ussmag, usstr1, usstr2, ustsnv, ustsvv, usthht,  &
-             usvosy, usvpst
+             usvosy, usvpst, usvist
 
 end module paramx

commit e740623ee1f134985aa7189a78292edb86c5fcc0

Fix bug introduced in 56bf05e638 (2023-09-20).
Fix provided by T. Fonty.

src/user_examples/cs_user_physical_properties.c
@@ -325,7 +325,7 @@ cs_user_physical_properties_turb_viscosity(cs_domain_t      *domain)
   /* Calculation of (dynamic) viscosity
    * ================================== */
 
-  cs_real_t *visct = CS_F_(mu)->val;
+  cs_real_t *visct = CS_F_(mu_t)->val;
   const cs_real_t *crom  = CS_F_(rho)->val;
   for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++) {
     const cs_real_t coef = cpro_smago[c_id];