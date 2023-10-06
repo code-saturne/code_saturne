code-saturne/code_saturne

commit ad3269170f99256cc27457c4e0eb1abb6b99ba7d

Convert typecl.f90 to C.
Edited by Y. Fournier, with some refactoring and optimizations.

src/base/cs_pressure_correction.c
@@ -580,7 +580,7 @@ _pressure_correction_fv(int        iterns,
     if (indhyd == 1 || vp_param->iifren == 1) {
 
       const int *auto_flag = cs_atmo_get_auto_flag();
-      const cs_real_t *b_head_loss = cs_get_b_head_loss();
+      const cs_real_t *b_head_loss = cs_boundary_conditions_get_b_head_loss();
 
       for (cs_lnum_t f_id = 0; f_id < n_b_faces; f_id++) {
 

src/base/Makefile.am
@@ -101,6 +101,7 @@ cs_block_to_part.h \
 cs_boundary.h \
 cs_boundary_conditions.h \
 cs_boundary_conditions_set_coeffs.h \
+cs_boundary_conditions_type.h \
 cs_boundary_zone.h \
 cs_calcium.h \
 cs_control.h \
@@ -292,6 +293,7 @@ cs_base_fortran.c \
 cs_boundary.c \
 cs_boundary_conditions.c \
 cs_boundary_conditions_set_coeffs.c \
+cs_boundary_conditions_type.c \
 cs_boundary_zone.c \
 cs_calcium.c \
 csc2cl.f90 \
@@ -425,7 +427,6 @@ schtmp.f90 \
 tdesi1.f90 \
 tridim.f90 \
 tspdcv.f90 \
-typecl.f90 \
 varpos.f90 \
 vericl.f90 \
 verini.f90 \

src/base/cplsat.f90
@@ -27,6 +27,8 @@ module cplsat
 
   !=============================================================================
 
+  use, intrinsic :: iso_c_binding
+
   use paramx
 
   implicit none
@@ -42,7 +44,8 @@ module cplsat
   !> number of couplings code_saturne / code_saturne
   integer, save :: nbrcpl = 0
   !> indicator coupling face / face only
-  integer, save :: ifaccp = 0
+  !integer, save :: ifaccp = 0
+  integer(c_int), pointer, save :: ifaccp
   !> maximum permissible number of coupling
   integer   nbcpmx
   parameter(nbcpmx=10)
@@ -57,6 +60,49 @@ module cplsat
   !> size of exchange tables
   integer, save :: nvarto(nbcpmx)
   !> \}
+
+  !=============================================================================
+
+  interface
+
+    !---------------------------------------------------------------------------
+
+    !> \cond DOXYGEN_SHOULD_SKIP_THIS
+
+    !---------------------------------------------------------------------------
+
+    subroutine cs_f_sat_coupling_get_pointers(ifaccp)     &
+      bind(C, name='cs_f_sat_coupling_get_pointers')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      type(c_ptr), intent(out) :: ifaccp
+    end subroutine cs_f_sat_coupling_get_pointers
+
+  end interface
+
+  !=============================================================================
+
+contains
+
+  !=============================================================================
+
+  !> \brief Initialize Fortran code_saturne coupling API.
+  !> This maps Fortran pointers to global C structure members.
+
+  subroutine cplsat_init
+
+    use, intrinsic :: iso_c_binding
+    implicit none
+
+    ! Local variables
+
+    type(c_ptr) :: c_ifaccp
+
+    call cs_f_sat_coupling_get_pointers(c_ifaccp)
+
+    call c_f_pointer(c_ifaccp, ifaccp)
+
+  end subroutine cplsat_init
 
   !=============================================================================
 

src/base/cs_boundary_conditions.c
@@ -84,7 +84,7 @@
 #include "fvm_nodal.h"
 
 /*----------------------------------------------------------------------------
- *  Header for the current file
+ * Header for the current file
  *----------------------------------------------------------------------------*/
 
 #include "cs_boundary_conditions.h"
@@ -129,7 +129,7 @@ typedef struct {
 } cs_bc_map_t;
 
 /*============================================================================
- *  Global variables
+ * Static global variables
  *============================================================================*/
 
 static int *_bc_type;
@@ -141,6 +141,12 @@ static cs_bc_map_t *_bc_maps = NULL;
 
 static int _n_bc_open = 0;
 static cs_boundary_conditions_open_t **_bc_open = NULL;
+
+static cs_real_t *_b_head_loss = NULL;
+
+/*============================================================================
+ * Global variables
+ *============================================================================*/
 
 const int *cs_glob_bc_type = NULL;
 
@@ -2463,6 +2469,8 @@ cs_boundary_conditions_free(void)
   BFT_FREE(cs_glob_bc_pm_info->izfppp);
   BFT_FREE(cs_glob_bc_pm_info->itrifb);
   BFT_FREE(cs_glob_bc_pm_info);
+
+  BFT_FREE(_b_head_loss);
 }
 
 /*----------------------------------------------------------------------------*/
@@ -2983,6 +2991,25 @@ cs_boundary_conditions_get_legacy_zone_num(const  cs_zone_t  *z)
    zone_num = c->bc_pm_zone_num;
 
   return zone_num;
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Return pointer to boundary head losses array.
+ *
+ * The array is allocated if not previously available.
+ *
+ * \return  b_head_loss  pointer to boundary head losses array
+ */
+/*----------------------------------------------------------------------------*/
+
+cs_real_t *
+cs_boundary_conditions_get_b_head_loss(void)
+{
+  if (_b_head_loss == NULL)
+    BFT_REALLOC(_b_head_loss, cs_glob_mesh->n_b_faces, cs_real_t);
+
+  return _b_head_loss;
 }
 
 /*----------------------------------------------------------------------------*/

src/base/cs_boundary_conditions.h
@@ -62,6 +62,9 @@ BEGIN_C_DECLS
 */
 #define  CS_MAX_BC_PM_ZONE_NUM  2000
 
+/*! Maximum number of boundary condition code types */
+#define  CS_MAX_BC_TYPE  200
+
 /*============================================================================
  * Local type definitions
  *============================================================================*/
@@ -405,6 +408,19 @@ cs_boundary_conditions_get_model_inlet(const cs_zone_t  *zone);
 
 int
 cs_boundary_conditions_get_legacy_zone_num(const  cs_zone_t  *z);
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Return pointer to boundary head losses array.
+ *
+ * The array is allocated if not previously available.
+ *
+ * \return  b_head_loss  pointer to boundary head losses array
+ */
+/*----------------------------------------------------------------------------*/
+
+cs_real_t *
+cs_boundary_conditions_get_b_head_loss(void);
 
 /*----------------------------------------------------------------------------*/
 /*

src/base/cs_boundary_conditions_set_coeffs.c
@@ -54,6 +54,7 @@
 #include "cs_base.h"
 #include "cs_boundary.h"
 #include "cs_boundary_conditions.h"
+#include "cs_boundary_conditions_type.h"
 #include "cs_coupling.h"
 #include "cs_field.h"
 #include "cs_field_default.h"
@@ -122,12 +123,6 @@ cs_f_cou1di(void);
 
 void
 cs_f_mmtycl(const int  *itypfb);
-
-void
-cs_f_typecl(bool       init,
-            int        itypfb[],
-            const int  itrifb[],
-            int        isostd[]);
 
 void
 cs_f_pptycl(bool        init,
@@ -287,9 +282,8 @@ cs_boundary_conditions_set_coeffs(int        nvar,
     = (const cs_lnum_t *restrict)mesh->b_face_cells;
   const cs_real_3_t *b_face_normal  = (const cs_real_3_t *)fvq->b_face_normal;
   const cs_real_3_t *b_face_u_normal = (const cs_real_3_t *)fvq->b_face_u_normal;
-  const cs_real_3_t *cell_cen       = (const cs_real_3_t *)fvq->cell_cen;
-  const cs_real_3_t *b_face_cog
-    = (const cs_real_3_t *)fvq->b_face_cog;
+  const cs_real_3_t *cell_cen = (const cs_real_3_t *)fvq->cell_cen;
+  const cs_real_3_t *b_face_cog = (const cs_real_3_t *)fvq->b_face_cog;
   const cs_real_3_t *restrict diipb = (const cs_real_3_t *restrict)fvq->diipb;
   const cs_real_t   *b_face_surf    = fvq->b_face_surf;
   const cs_real_t   *b_dist         = fvq->b_dist;
@@ -316,7 +310,7 @@ cs_boundary_conditions_set_coeffs(int        nvar,
   cs_field_t *f_th = cs_thermal_model_field();
 
   const int *izfppp = cs_glob_bc_pm_info->izfppp;
-  const int *itrifb = cs_glob_bc_pm_info->itrifb;
+  int *itrifb = cs_glob_bc_pm_info->itrifb;
   int *bc_type = cs_f_boundary_conditions_get_bc_type();
 
   int *impale = cs_glob_ale_data->impale;
@@ -561,10 +555,10 @@ cs_boundary_conditions_set_coeffs(int        nvar,
     if (cs_turbomachinery_get_model() != CS_TURBOMACHINERY_NONE)
       cs_f_mmtycl(bc_type);
 
-    cs_f_typecl(false,
-                bc_type,
-                itrifb,
-                isostd);
+    cs_boundary_conditions_type(false,
+                                bc_type,
+                                itrifb,
+                                isostd);
   }
 
   /*--------------------------------------------------------------------------
@@ -2824,9 +2818,6 @@ cs_boundary_conditions_set_coeffs(int        nvar,
             hintt[4] = visten[c_id][4]*ctheta/distbf/cs_turb_csrij;
             hintt[5] = visten[c_id][5]*ctheta/distbf/cs_turb_csrij;
 
-            for (int iii = 0; iii < 5; iii++)
-              printf("hintt = %f", hintt[iii]);
-
             /* Dirichlet Boundary Condition
                ---------------------------- */
 
@@ -3448,7 +3439,7 @@ cs_boundary_conditions_set_coeffs_init(void)
   const cs_real_3_t *vtx_coord = (const cs_real_3_t *)mesh->vtx_coord;
 
   int *bc_type = cs_f_boundary_conditions_get_bc_type();
-  const int *itrifb = cs_glob_bc_pm_info->itrifb;
+  int *itrifb = cs_glob_bc_pm_info->itrifb;
   const int *izfppp = cs_glob_bc_pm_info->izfppp;
   cs_real_t *dt = CS_F_(dt)->val;
 
@@ -3553,10 +3544,10 @@ cs_boundary_conditions_set_coeffs_init(void)
   int *isostd;
   BFT_MALLOC(isostd, n_b_faces+1, int);
 
-  cs_f_typecl(true,
-              bc_type,
-              itrifb,
-              isostd);
+  cs_boundary_conditions_type(true,
+                              bc_type,
+                              itrifb,
+                              isostd);
 
   BFT_FREE(isostd);
 

src/base/cs_boundary_conditions_type.h
@@ -1,0 +1,76 @@
+#ifndef __CS_BOUNDARY_CONDITIONS_TYPE_H__
+#define __CS_BOUNDARY_CONDITIONS_TYPE_H__
+
+/*============================================================================
+ * Handle boundary condition type codes.
+ *============================================================================*/
+
+/*
+  This file is part of code_saturne, a general-purpose CFD tool.
+
+  Copyright (C) 1998-2023 EDF S.A.
+
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation; either version 2 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+  Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+ * Standard C library headers
+ *----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+ * Local headers
+ *----------------------------------------------------------------------------*/
+
+#include "cs_defs.h"
+
+/*----------------------------------------------------------------------------*/
+
+BEGIN_C_DECLS
+
+/*=============================================================================
+ * Public function prototypes
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------*/
+/*! \file cs_boundary_conditions_type.c
+ *
+ * \brief Handle boundary condition type codes (\ref itypfb).
+ */
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/*
+ * \brief  Handle boundary condition type code (\ref bc_type).
+ *
+ * \param[in]      init      partial treatment (before time loop) if true
+ * \param[in,out]  bc_type   type per boundary face
+ * \param[out]     itrifb    indirection for faces ordering
+ * \param[out]     isostd    standard output indicator + reference face number
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_boundary_conditions_type(bool  init,
+                            int   bc_type[],
+                            int   itrifb[],
+                            int   isostd[]);
+
+/*----------------------------------------------------------------------------*/
+
+END_C_DECLS
+
+#endif /* __CS_BOUNDARY_CONDITIONS_TYPE_H__ */

commit f6857356fc747918e3aabce735797680389d3039

Fix bug in outlet BCs introduced in commit ad3269170f (2023-09-13).

src/base/cs_boundary_conditions_type.c
@@ -497,7 +497,7 @@ cs_boundary_conditions_type(bool  init,
 
   cs_real_t xyzref[3] = {0., 0., 0.};
 
-  if (_itbslb > 0) {
+  if (_itbslb > -1) {
 
     /* If irangd is the local rank, we assign PI' to xyzref(4)
        (this is always the case in serial mode) */
@@ -586,7 +586,7 @@ cs_boundary_conditions_type(bool  init,
     xyzp0[1] = xyzref[1];
     xyzp0[2] = xyzref[2];
 
-    if (_itbslb > 0)
+    if (_itbslb > -1)
       cs_log_printf
         (CS_LOG_DEFAULT,
          _("\n"
@@ -616,7 +616,7 @@ cs_boundary_conditions_type(bool  init,
 
   /* No need to compute pressure gradient for frozen field computations */
 
-  if (   _itbslb > 0
+  if (   _itbslb > -1
       && cs_glob_lagr_time_scheme->iilagr != CS_LAGR_FROZEN_CONTINUOUS_PHASE) {
 
     cs_real_3_t *frcxt = NULL;

src/base/cs_pressure_correction.c
@@ -534,7 +534,7 @@ _pressure_correction_fv(int        iterns,
 
     /* This computation is needed only if there are outlet faces */
 
-    if (ifcsor > 0 || open_bcs_flag != 0)
+    if (ifcsor > -1 || open_bcs_flag != 0)
       cs_hydrostatic_pressure_compute(&indhyd, iterns,
                                       frcxt, dfrcxt, cvar_hydro_pres,
                                       iflux, bflux, i_visc, b_visc,