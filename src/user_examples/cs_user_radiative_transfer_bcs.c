code-saturne/code_saturne

commit 51dba353dc27c537735e041c2f9a5167de706174

cs_user_radiative_transfer_bc's: align prototype with base BC's.

src/user_examples/cs_user_radiative_transfer_bcs.c
@@ -107,38 +107,32 @@ BEGIN_C_DECLS
  *  - epap   = thickness (m)
  *  - textp  = outside temperature (K)
  *
- * \param[in]     nvar          total number of variable BC's
- * \param[in]     bc_type       boundary face types
- * \param[in]     icodcl        boundary face code
- * \param[in]     isothp        boundary face type for radiative transfer
- * \param[out]    tmin          min allowed value of the wall temperature
- * \param[out]    tmax          max allowed value of the wall temperature
- * \param[in]     tx            relaxation coefficient (0 < tx < 1)
- * \param[in]     dt            time step (per cell)
- * \param[in]     rcodcl        boundary condition values
- *                                rcodcl(3) = flux density value
- *                                (negative for gain) in W/m2
- * \param[in]     thwall        inside current wall temperature (K)
- * \param[in]     qincid        radiative incident flux  (W/m2)
- * \param[in]     hfcnvp        convective exchange coefficient (W/m2/K)
- * \param[in]     flcnvp        convective flux (W/m2)
- * \param[out]    xlamp         conductivity (W/m/K)
- * \param[out]    epap          thickness (m)
- * \param[out]    epsp          emissivity (>0)
- * \param[out]    textp         outside temperature (K)
+ * \param[in, out]  domain        pointer to a cs_domain_t structure
+ * \param[in]       bc_type       boundary face types
+ * \param[in]       isothp        boundary face type for radiative transfer
+ * \param[out]      tmin          min allowed value of the wall temperature
+ * \param[out]      tmax          max allowed value of the wall temperature
+ * \param[in]       tx            relaxation coefficient (0 < tx < 1)
+ * \param[in]       dt            time step (per cell)
+ * \param[in]       thwall        inside current wall temperature (K)
+ * \param[in]       qincid        radiative incident flux  (W/m2)
+ * \param[in]       hfcnvp        convective exchange coefficient (W/m2/K)
+ * \param[in]       flcnvp        convective flux (W/m2)
+ * \param[out]      xlamp         conductivity (W/m/K)
+ * \param[out]      epap          thickness (m)
+ * \param[out]      epsp          emissivity (>0)
+ * \param[out]      textp         outside temperature (K)
  */
 /*----------------------------------------------------------------------------*/
 
 void
-cs_user_radiative_transfer_bcs(int               nvar,
+cs_user_radiative_transfer_bcs(cs_domain_t      *domain,
                                const int         bc_type[],
-                               int               icodcl[],
                                int               isothp[],
                                cs_real_t        *tmin,
                                cs_real_t        *tmax,
                                cs_real_t        *tx,
                                const cs_real_t   dt[],
-                               cs_real_t         rcodcl[],
                                const cs_real_t   thwall[],
                                const cs_real_t   qincid[],
                                cs_real_t         hfcnvp[],
@@ -150,15 +144,12 @@ cs_user_radiative_transfer_bcs(int               nvar,
 {
   /*< [loc_var]*/
   cs_real_t tkelvi = cs_physical_constants_celsius_to_kelvin;
-  cs_lnum_t n_b_faces = cs_glob_mesh->n_b_faces;
   const cs_zone_t *zone = NULL;
   int *izfrdp = cs_boundary_zone_face_class_id();
   /*< [loc_var]*/
 
   /*< [ivar]*/
-  cs_field_t *fth = cs_thermal_model_field();
-  const cs_lnum_t ivart
-    = cs_field_get_key_int(fth, cs_field_key_id("variable_id")) - 1;
+  cs_field_t *f_th = cs_thermal_model_field();
   /*< [ivar]*/
 
   /* Min and max values for the wall temperatures (clipping otherwise)
@@ -304,7 +295,7 @@ cs_user_radiative_transfer_bcs(int               nvar,
       epsp[face_id] = 0.9;
 
       /* Conduction flux (W/m2) */
-      rcodcl[face_id + ivart * n_b_faces + 2 * nvar * n_b_faces] = 0.0;
+      f_th->bc_coeffs->rcodcl2[face_id] = 0.0;
 
     }
 
@@ -346,7 +337,7 @@ cs_user_radiative_transfer_bcs(int               nvar,
       isothp[face_id] = CS_BOUNDARY_RAD_WALL_REFL_COND_FLUX;
 
       /* Conduction flux (W/m2)*/
-      rcodcl[face_id + ivart * n_b_faces + 2 * nvar * n_b_faces ] = 0.0;
+      f_th->bc_coeffs->rcodcl2[face_id] = 0.0;
 
     }
 

src/user/cs_user_radiative_transfer_bcs.c
@@ -101,39 +101,33 @@ BEGIN_C_DECLS
  *  - epap   = thickness (m)
  *  - textp  = outside temperature (K)
  *
- * \param[in]     nvar          total number of variable BC's
- * \param[in]     bc_type       boundary face types
- * \param[in]     icodcl        boundary face code
- * \param[in]     isothp        boundary face type for radiative transfer
- * \param[out]    tmin          min allowed value of the wall temperature
- * \param[out]    tmax          max allowed value of the wall temperature
- * \param[in]     tx            relaxation coefficient (0 < tx < 1)
- * \param[in]     dt            time step (per cell)
- * \param[in]     rcodcl        boundary condition values
- *                                rcodcl(3) = flux density value
- *                                (negative for gain) in W/m2
- * \param[in]     thwall        inside current wall temperature (K)
- * \param[in]     qincid        radiative incident flux  (W/m2)
- * \param[in]     hfcnvp        convective exchange coefficient (W/m2/K)
- * \param[in]     flcnvp        convective flux (W/m2)
- * \param[out]    xlamp         conductivity (W/m/K)
- * \param[out]    epap          thickness (m)
- * \param[out]    epsp          emissivity (>0)
- * \param[out]    textp         outside temperature (K)
+ * \param[in, out]  domain        pointer to a cs_domain_t structure
+ * \param[in]       bc_type       boundary face types
+ * \param[in]       isothp        boundary face type for radiative transfer
+ * \param[out]      tmin          min allowed value of the wall temperature
+ * \param[out]      tmax          max allowed value of the wall temperature
+ * \param[in]       tx            relaxation coefficient (0 < tx < 1)
+ * \param[in]       dt            time step (per cell)
+ * \param[in]       thwall        inside current wall temperature (K)
+ * \param[in]       qincid        radiative incident flux  (W/m2)
+ * \param[in]       hfcnvp        convective exchange coefficient (W/m2/K)
+ * \param[in]       flcnvp        convective flux (W/m2)
+ * \param[out]      xlamp         conductivity (W/m/K)
+ * \param[out]      epap          thickness (m)
+ * \param[out]      epsp          emissivity (>0)
+ * \param[out]      textp         outside temperature (K)
  */
 /*----------------------------------------------------------------------------*/
 
 #pragma weak cs_user_radiative_transfer_bcs
 void
-cs_user_radiative_transfer_bcs(int               nvar,
+cs_user_radiative_transfer_bcs(cs_domain_t      *domain,
                                const int         bc_type[],
-                               int               icodcl[],
                                int               isothp[],
                                cs_real_t        *tmin,
                                cs_real_t        *tmax,
                                cs_real_t        *tx,
                                const cs_real_t   dt[],
-                               cs_real_t         rcodcl[],
                                const cs_real_t   thwall[],
                                const cs_real_t   qincid[],
                                cs_real_t         hfcnvp[],
@@ -143,15 +137,13 @@ cs_user_radiative_transfer_bcs(int               nvar,
                                cs_real_t         epsp[],
                                cs_real_t         textp[])
 {
-  CS_UNUSED(nvar);
+  CS_UNUSED(domain);
   CS_UNUSED(bc_type);
-  CS_UNUSED(icodcl);
   CS_UNUSED(isothp);
   CS_UNUSED(tmin);
   CS_UNUSED(tmax);
   CS_UNUSED(tx);
   CS_UNUSED(dt);
-  CS_UNUSED(rcodcl);
   CS_UNUSED(thwall);
   CS_UNUSED(qincid);
   CS_UNUSED(hfcnvp);

src/base/cs_prototypes.h
@@ -786,15 +786,13 @@ cs_user_radiative_transfer_parameters(void);
  *----------------------------------------------------------------------------*/
 
 void
-cs_user_radiative_transfer_bcs(int               nvar,
+cs_user_radiative_transfer_bcs(cs_domain_t      *domain,
                                const int         bc_type[],
-                               int               icodcl[],
                                int               isothp[],
                                cs_real_t        *tmin,
                                cs_real_t        *tmax,
                                cs_real_t        *tx,
                                const cs_real_t   dt[],
-                               cs_real_t         rcodcl[],
                                const cs_real_t   thwall[],
                                const cs_real_t   qincid[],
                                cs_real_t         hfcnvp[],

src/rayt/cs_rad_transfer_bcs.c
@@ -392,15 +392,13 @@ cs_rad_transfer_bcs(int         nvar,
                                   f_bxlam->val,
                                   rcodcl);
 
-    cs_user_radiative_transfer_bcs(nvar,
+    cs_user_radiative_transfer_bcs(cs_glob_domain,
                                    bc_type,
-                                   icodcl,
                                    isothm,
                                    &tmin,
                                    &tmax,
                                    &tx,
                                    dt,
-                                   rcodcl,
                                    twall,
                                    f_bqinci->val,
                                    f_bhconv->val,
@@ -443,15 +441,13 @@ cs_rad_transfer_bcs(int         nvar,
                                 f_bxlam->val,
                                 rcodcl);
 
-  cs_user_radiative_transfer_bcs(nvar,
+  cs_user_radiative_transfer_bcs(cs_glob_domain,
                                  bc_type,
-                                 icodcl,
                                  isothm,
                                  &tmin,
                                  &tmax,
                                  &tx,
                                  dt,
-                                 rcodcl,
                                  twall,
                                  f_bqinci->val,
                                  f_bhconv->val,

src/user_examples/cs_user_boundary_conditions-advanced.c
@@ -116,7 +116,7 @@ cs_user_boundary_conditions(cs_domain_t  *domain,
    *   - a Dirichlet condition on velocity (sliding wall with no-slip condition)
    *   - a Dirichlet condition on the first scalar. */
 
-  cs_zone_t  *zn = NULL;
+  const cs_zone_t  *zn = NULL;
 
   cs_field_t *scal = cs_field_by_name("scalar1");
   /*! [loc_var_dec] */

commit 8b3561c004d5f74b960c91824cc2db8b11f38660

[rad transfer] Fix bug introduced in commit 51dba353dc (2022-12-01).
Wrong user example.

src/user_examples/cs_user_radiative_transfer_bcs.c
@@ -295,7 +295,7 @@ cs_user_radiative_transfer_bcs(cs_domain_t      *domain,
       epsp[face_id] = 0.9;
 
       /* Conduction flux (W/m2) */
-      f_th->bc_coeffs->rcodcl2[face_id] = 0.0;
+      f_th->bc_coeffs->rcodcl3[face_id] = 0.0;
 
     }
 
@@ -337,7 +337,7 @@ cs_user_radiative_transfer_bcs(cs_domain_t      *domain,
       isothp[face_id] = CS_BOUNDARY_RAD_WALL_REFL_COND_FLUX;
 
       /* Conduction flux (W/m2)*/
-      f_th->bc_coeffs->rcodcl2[face_id] = 0.0;
+      f_th->bc_coeffs->rcodcl3[face_id] = 0.0;
 
     }
 