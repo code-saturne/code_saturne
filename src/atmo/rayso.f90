code-saturne/code_saturne

commit 86050e535947ef4cf813f3d97f6aac246b49b64c

[Atmo] 1D-3D radiative transfer, correct transfer form solar direct to diffuse.

src/atmo/rayso.f90
@@ -235,6 +235,7 @@ double precision, allocatable:: refbs(:,:),fabso3c(:,:),tra(:,:)
 double precision, allocatable:: dow(:,:),atln(:,:),absn(:,:)
 double precision, allocatable :: ckup_sir_f(:), ckdown_sir_r(:), ckdown_sir_f(:)
 double precision, allocatable :: ckup_suv_f(:), ckdown_suv_r(:), ckdown_suv_f(:)
+double precision, allocatable :: w0_suv(:), w0_sir(:), g_apc_suv(:), g_apc_sir(:)
 double precision, allocatable:: fnebmax(:),fneba(:)
 
 double precision, allocatable, dimension(:,:) :: dowd, trad, trard, ddfso3c
@@ -249,6 +250,8 @@ double precision, allocatable, dimension(:) ::  dfs, ufs
 double precision, dimension(:,:), pointer :: bpro_rad_inc
 double precision, dimension(:,:), pointer :: cpro_ck_up
 double precision, dimension(:,:), pointer :: cpro_ck_down
+double precision, dimension(:,:), pointer :: cpro_w0
+double precision, dimension(:,:), pointer :: cpro_gapc
 ! For computing albedo PIC, PIOC
 double precision epsc
 double precision pioco3,pioch2o,gasymo3,gasymh2o
@@ -265,7 +268,7 @@ double precision nu0, dm0, dm, coeff_E_o3(12), coeff_E_h2o(12)
 double precision pioco3C, pioch2oC
 double precision tauao3(kmx+1) , tauah2o(kmx+1)
 double precision corp, rov
-double precision mui,tauapc,ckapcd,ckapcf,picapc
+double precision mui,tauapc,ckapcd,ckapcf,picapc,gapc
 double precision ck_aero_h2of,ck_aero_h2od,ck_aero_o3f,ck_aero_o3d
 ! data for pkn and kn distribution
 data kn/4.d-5,0.002,0.035,0.377,1.95,9.40,44.6,190./
@@ -307,6 +310,7 @@ allocate(dfso3(kmx+1), ufso3(kmx+1))
 allocate(dfs(kmx+1), ufs(kmx+1))
 allocate(ckup_sir_f(kmx), ckdown_sir_r(kmx), ckdown_sir_f(kmx))
 allocate(ckup_suv_f(kmx), ckdown_suv_r(kmx), ckdown_suv_f(kmx))
+allocate(w0_sir(kmx), w0_suv(kmx), g_apc_sir(kmx), g_apc_suv(kmx))
 
 allocate(dffsh2o(kmx+1), dffso3(kmx+1))
 allocate(ddfsh2o(kmx+1), ddfso3(kmx+1))
@@ -859,7 +863,7 @@ if (muzero.gt.epzero) then
     ufso3(i) =ufso3(itop)*(1.d0 -raysoz(xstar))
   enddo
 
-  ! 6.4 Absorption by water vapor and liquid water
+  ! 6.4 Absorption by water vapor and liquid water (H20 band, SIR)
 
   ! In that case we have to solve multiple diffusion. This is achieved by means
   ! of the adding method following Lacis et Hansen, 1974
@@ -1045,7 +1049,9 @@ if (muzero.gt.epzero) then
     corp = preray(i) / 101315.d0
     rov = romray(i)*(qvray(i)*corp*sqrt(tkelvi/(temray(i) + tkelvi)))
     dy = rov
-    ! calculation of absorption coefficient ckup and ckdown useful for 3D calculations
+    ! Calculation of absorption coefficient ckup and ckdown useful
+    ! for 3D calculations
+    ! Note: gas contribution
     ckdown_sir_r(i) = dzyama(y,dy)/(0.353d0-raysve(y))
     ckup_sir_f(i) = dzyama(ystar,dy)/(0.353d0-raysve(ystar))
     ckdown_sir_f(i) = dzyama(y,dy)/(0.353d0-raysve(y))
@@ -1096,10 +1102,17 @@ if (muzero.gt.epzero) then
       ckapcf=0.d0
       ck_aero_h2of=0.d0
       ck_aero_h2od=0.d0
+      w0_sir(k) = 0.d0
+      g_apc_sir(k) = 0.d0
     else
       picapc=(pic_h2o(k)*tauc(k)+piaero_h2o*tauah2o(k))/tauapc
+      w0_sir(k) = picapc
+      ! if we take into account asymmetry factor for forward diffuse radiation
+      ! Note apc means aerosols+clouds
+      gapc=(pic_h2o(k)*tauc(k)*gasym+piaero_h2o*tauah2o(k)*gaero_h2o)/tauapc*picapc
+      g_apc_sir(k) = gapc
       ! absorption and forward diffusion
-      ckapcf=(1.d0-picapc)*tauapc/(deltaz*mui)
+      ckapcf=(1.d0-picapc*(1.d0+gapc)/2.d0)*tauapc/(deltaz*mui)
       ckapcd=tauapc/(deltaz*muzero_cor)
       ck_aero_h2of=(1.d0-piaero_h2o)*tauah2o(k)/(deltaz*mui)
       ck_aero_h2od=tauah2o(k)/(deltaz*muzero_cor)
@@ -1123,10 +1136,16 @@ if (muzero.gt.epzero) then
       ckapcf=0.d0
       ck_aero_o3f=0.d0
       ck_aero_o3d=0.d0
+      w0_suv(k) = 0.d0
+      g_apc_suv(k) = 0.d0
     else
       picapc=(pic_o3(k)*tauc(k)+piaero_o3*tauao3(k))/tauapc
+      w0_suv(k) = picapc
+      ! if we take into account asymmetry factor for forward diffuse radiation
+      gapc=(pic_o3(k)*tauc(k)*gasym+piaero_o3*tauao3(k)*gaero_o3)/tauapc*picapc
+      g_apc_suv(k) = gapc
       ! absorption and forward diffusion
-      ckapcf=(1.d0-picapc)*tauapc/(deltaz*mui)
+      ckapcf=(1.d0-picapc*(1.d0+gapc)/2.d0)*tauapc/(deltaz*mui)
       ckapcd=tauapc/(deltaz*muzero_cor)
       ck_aero_o3f=(1.d0-piaero_o3)*tauao3(k)/(deltaz*mui)
       ck_aero_o3d=tauao3(k)/(deltaz*muzero_cor)
@@ -1140,6 +1159,20 @@ if (muzero.gt.epzero) then
     ckdown_suv_f(k) = (ckdown_suv_f(k) +  ck_aero_o3f)*(1.d0-fneray(k))+&
       (ckdown_suv_f(k)+ckapcf)*(fneray(k))
   enddo
+
+  !In addition a source term has to be added in 3D for diffuse radiation
+
+  ! if mui=1/sqrt(3) quadrature method as LH74 the source term is added for
+  ! both downward and upward radiation
+  ! where  g1= 31/2(1-wo) , g3=(1-31/2µo)/2, g4=(1+31/2µo)/2 and t the total optical depth (gas + aerosol +
+  ! cloud) t = tg + ta + tc.
+  ! if mui=muzero_cor delta method the source term is added only in the downward
+  !diffuse radiation
+  ! In that condition the two-stream equations can be written:
+  ! where  g1=(1-wo)/µo, g4=1 and t the total optical depth (gas + aerosol + cloud) t = tg + ta + tc.
+
+
+
 ! if muzero < 0, it is night
 else
 
@@ -1159,6 +1192,10 @@ else
     ckup_suv_f(k)=0.d0
     ckdown_suv_r(k)=0.d0
     ckdown_suv_f(k)=0.d0
+    w0_sir(k) = 0.d0
+    w0_suv(k) = 0.d0
+    g_apc_sir(k) = 0.d0
+    g_apc_suv(k) = 0.d0
 
   enddo
   fos = 0.d0
@@ -1180,6 +1217,8 @@ if (f_id.ge.0) then
 
   call field_get_val_v_by_name("rad_absorption_coeff_up", cpro_ck_up)
   call field_get_val_v_by_name("rad_absorption_coeff_down", cpro_ck_down)
+  call field_get_val_v_by_name("simple_diffusion_albedo", cpro_w0)
+  call field_get_val_v_by_name("asymmetry_factor", cpro_gapc)
 
   c_id = 0
   ! Direct Solar (denoted by _r) (for Solar IR band absorbed by H20)
@@ -1306,6 +1345,22 @@ if (f_id.ge.0) then
 
       cpro_ck_up(c_id, iel) = var
 
+      ! Simple diffusion albedo w0
+
+      call intprz &
+        (kmray, zqq,                                               &
+        w0_sir, zent, iz1, iz2, var )
+
+      cpro_w0(c_id, iel) = var
+
+      ! Asymmetry factor
+
+      call intprz &
+        (kmray, zqq,                                               &
+        g_apc_sir, zent, iz1, iz2, var )
+
+      cpro_gapc(c_id, iel) = var
+
     enddo
 
   endif
@@ -1349,6 +1404,21 @@ if (f_id.ge.0) then
 
       cpro_ck_up(c_id, iel) = var
 
+      ! Simple diffusion albedo w0
+
+      call intprz &
+        (kmray, zqq,                                               &
+        w0_suv, zent, iz1, iz2, var )
+
+      cpro_w0(c_id, iel) = var
+
+      ! Asymmetry factor
+
+      call intprz &
+        (kmray, zqq,                                               &
+        g_apc_suv, zent, iz1, iz2, var )
+
+      cpro_gapc(c_id, iel) = var
     enddo
 
   endif
@@ -1373,6 +1443,7 @@ deallocate(ddfsh2o, ddfso3)
 deallocate(drfs, dffs, ddfs, dddsh2o, dddso3)
 deallocate(ckup_sir_f, ckdown_sir_r, ckdown_sir_f)
 deallocate(ckup_suv_f, ckdown_suv_r, ckdown_suv_f)
+deallocate(g_apc_suv, g_apc_sir, w0_suv, w0_sir)
 
 return
 

src/rayt/cs_rad_transfer_property_fields.c
@@ -400,6 +400,26 @@ cs_rad_transfer_prp(void)
     cs_field_set_key_int(f, keyvis, vis_gg);
     cs_field_set_key_int(f, keylog, 1);
 
+    /* Asymmetry factor (for solar only...)  per band */
+    f = cs_field_create("asymmetry_factor",
+                        field_type,
+                        location_id,
+                        rt_params->nwsgg,
+                        false);
+
+    cs_field_set_key_int(f, keyvis, vis_gg);
+    cs_field_set_key_int(f, keylog, 1);
+
+    /* Simple diffusion albedo (for solar only...)  per band */
+    f = cs_field_create("simple_diffusion_albedo",
+                        field_type,
+                        location_id,
+                        rt_params->nwsgg,
+                        false);
+
+    cs_field_set_key_int(f, keyvis, vis_gg);
+    cs_field_set_key_int(f, keylog, 1);
+
     /* Upward absorption coefficient by band */
     f = cs_field_create("rad_absorption_coeff_down",
                         field_type,

src/rayt/cs_rad_transfer_solve.c
@@ -435,6 +435,7 @@ _cs_rad_transfer_sol(int                        gg_id,
   /* Postprocessing atmospheric upwards and downwards flux */
   cs_field_t *f_up = NULL, *f_down = NULL;
   cs_real_t *ck_u = NULL, *ck_d = NULL;
+  cs_real_t *w0 = NULL, *g_apc = NULL;
 
   if (rt_params->atmo_model != CS_RAD_ATMO_3D_NONE) {
     f_up = cs_field_by_name_try("rad_flux_up");
@@ -443,17 +444,25 @@ _cs_rad_transfer_sol(int                        gg_id,
     BFT_MALLOC(ck_u_d,  n_cells_ext, cs_real_t);
     ck_u = cs_field_by_name("rad_absorption_coeff_up")->val;
     ck_d = cs_field_by_name("rad_absorption_coeff_down")->val;
+
+    w0 = cs_field_by_name("simple_diffusion_albedo")->val;
+    /* asymmetry factor for aerosol plus cloud */
+    g_apc = cs_field_by_name("asymmetry_factor")->val;
   }
 
   cs_real_t vect_s[3];
   cs_real_t domegat = 0;
   bool one_dir = false;
-  /* Compute the Angular direction for direct solar radiation */
+  cs_real_t muzero_cor = 0.;
+  /* For direct solar radiation */
   if ((gg_id == rt_params->atmo_dr_id)
     || (gg_id == rt_params->atmo_dr_o3_id)) {
     one_dir = true;
     domegat = cs_math_pi;
+  }
 
+  /* Compute the Angular direction */
+  if (rt_params->atmo_model != CS_RAD_ATMO_3D_NONE) {
     /* Computes universal time coordinate */
     cs_real_t utc = (cs_real_t)cs_glob_atmo_option->shour
       + (cs_real_t)cs_glob_atmo_option->smin / 60.
@@ -463,7 +472,7 @@ _cs_rad_transfer_sol(int                        gg_id,
         || cs_glob_time_step_options->idtvar == CS_TIME_STEP_ADAPTIVE)
       utc += cs_glob_time_step->t_cur / 3600.;
 
-    cs_real_t albedo, muzero_cor, omega, fo, za;
+    cs_real_t albedo, omega, fo, za;
     cs_atmo_compute_solar_angles(cs_glob_atmo_option->latitude,
                                  cs_glob_atmo_option->longitude,
                                  (cs_real_t)cs_glob_atmo_option->squant,
@@ -475,18 +484,21 @@ _cs_rad_transfer_sol(int                        gg_id,
                                  &omega,
                                  &fo);
 
-    /* Zenithal angle:
-     * muzera is almost cos(za),
-     * but take earth curvature into account */
-    cs_real_t sinza = sqrt(1. - muzero_cor * muzero_cor);
-    vect_s[0] = - sinza * sin(omega);
-    vect_s[1] = - sinza * cos(omega);
-    vect_s[2] = - muzero_cor; /*  cos(za) */
+    /* For direct solar radiation */
+    if ((gg_id == rt_params->atmo_dr_id)
+      || (gg_id == rt_params->atmo_dr_o3_id)) {
+      /* Zenithal angle:
+       * muzera is almost cos(za),
+       * but take earth curvature into account */
+      cs_real_t sinza = sqrt(1. - muzero_cor * muzero_cor);
+      vect_s[0] = - sinza * sin(omega);
+      vect_s[1] = - sinza * cos(omega);
+      vect_s[2] = - muzero_cor; /*  cos(za) */
 
-    if (verbosity > 0)
-      bft_printf("     Solar direction [%f, %f, %f] \n",
-          vect_s[0], vect_s[1], vect_s[2]);
-
+      if (verbosity > 0)
+        bft_printf("     Solar direction [%f, %f, %f] \n",
+            vect_s[0], vect_s[1], vect_s[2]);
+    }
   }
 
   /* Initialization */
@@ -663,6 +675,32 @@ _cs_rad_transfer_sol(int                        gg_id,
               if (gg_id == rt_params->atmo_ir_id)
                 rhs[cell_id] =   ck_u_d[cell_id] * cell_vol[cell_id]
                              * c_stefan * cs_math_pow4(tempk[cell_id]) * onedpi;
+
+              /* For atmospheric flow, solar diffuse band has emission
+               * from direct solar absorption
+               * */
+
+              /* Diffuse Solar IR (H2O band)
+               * or Diffuse Solar UV (O3 band) */
+              if (   gg_id == rt_params->atmo_df_id
+                  || gg_id == rt_params->atmo_df_o3_id) {
+
+                /* w0 (1 +- g mu0/mui) S0 */
+                cs_real_t vol_w0 = w0[gg_id + cell_id * stride];
+
+                cs_real_t mui = 1./sqrt(3.);
+                /* g mu0/mui */
+                cs_real_t gmudmui = g_apc[gg_id + cell_id * stride]
+                  * muzero_cor / mui;
+
+                /* rhs already contains S0* cell_vol */
+                /* Up */
+                if (cs_math_3_dot_product(grav, vect_s) < 0.0)
+                  rhs[cell_id] *= vol_w0 * 0.5 * (1. - gmudmui);
+                /* Down */
+                else
+                  rhs[cell_id] *= vol_w0 * 0.5 * (1. + gmudmui);
+              }
             }
           }
 
@@ -1741,16 +1779,20 @@ cs_rad_transfer_solve(int               bc_type[],
           snprintf(f_name, 63, "spectral_absorption_%02d",
               rt_params->atmo_dr_id + 1);
           cs_field_t *f_abs_dr  = cs_field_by_name_try(f_name);
+          /* Emission initialized by direct absorption S0
+           * Note: radiance is "1/pi * direct flux"
+           * */
           for (cs_lnum_t cell_id = 0; cell_id < n_cells; cell_id++)
-            f_emi->val[cell_id] = 0.25 * onedpi * f_abs_dr->val[cell_id];
+            f_emi->val[cell_id] = f_abs_dr->val[cell_id] * onedpi;
         }
         if (gg_id == rt_params->atmo_df_o3_id) {
 
           snprintf(f_name, 63, "spectral_absorption_%02d",
               rt_params->atmo_dr_o3_id + 1);
           cs_field_t *f_abs_dr  = cs_field_by_name_try(f_name);
+          /* Emission initialized by direct absorption S0*/
           for (cs_lnum_t cell_id = 0; cell_id < n_cells; cell_id++)
-            f_emi->val[cell_id] = 0.25 * onedpi * f_abs_dr->val[cell_id];
+            f_emi->val[cell_id] = f_abs_dr->val[cell_id] * onedpi;
         }
 
         for (cs_lnum_t cell_id = 0; cell_id < n_cells; cell_id++)

commit 21676beff1598a458cbea03de1b78c28054a3193

fix bug in radiative transfer 1D introduced in 86050e535 (2023-08-28)
It should fix the problem in parallel mode.

src/atmo/rayso.f90
@@ -328,12 +328,15 @@ ibase = 0
 epsc=1.d-8
 
 do k = 1,kmray
-
+  w0_sir(k) = 0.d0
+  w0_suv(k) = 0.d0
+  g_apc_sir(k) = 0.d0
+  g_apc_suv(k) = 0.d0
   ! TODO usefull ?
   if (qlray(k).lt.epsc) then
     qlray(k) = 0.d0
     fneray(k)=0.d0
- endif
+  endif
 enddo
 
 do k = 1, kmx+1