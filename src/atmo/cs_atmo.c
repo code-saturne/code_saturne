code-saturne/code_saturne

commit 42bdb9750b01844b3bfad20005ea04bf0b4382ba

fix meteo profiles for humide air.
Computation of reference values was not correct in case of liquide
phase.

src/atmo/cs_atmo.c
@@ -195,6 +195,7 @@ static cs_atmo_option_t  _atmo_option = {
   .meteo_qwstar = DBL_MAX,
   .meteo_qw1 = DBL_MAX,
   .meteo_qw2 = DBL_MAX,
+  .meteo_ql0 = 0.,
   .meteo_evapor = DBL_MAX,
   .meteo_sensi = DBL_MAX,
   .meteo_psea = 101325.,
@@ -2487,12 +2488,22 @@ cs_atmo_init_meteo_profiles(void)
     bft_error(__FILE__,__LINE__, 0,
               _("Atmo meteo profiles: gravity must not be 0.\n"));
 
-  cs_real_t theta0 = aopt->meteo_t0 * pow(pref/ aopt->meteo_psea, rscp);
-
   /* Reference fluid properties set from meteo values */
   phys_pro->p0 = aopt->meteo_psea;
   phys_pro->t0 = aopt->meteo_t0; /* ref temp T0 */
-  phys_pro->ro0 = phys_pro->p0/(rair * aopt->meteo_t0); /* ref density T0 */
+
+  /* Compute reference q_l, theta_liq and rho */
+  cs_real_t t_c = aopt->meteo_t0 - cs_physical_constants_celsius_to_kelvin;
+  cs_real_t q_sat = cs_air_yw_sat(t_c, aopt->meteo_psea);
+  aopt->meteo_ql0 = CS_MAX(aopt->meteo_qw0 - q_sat, 0.);
+  cs_real_t rvsra = phys_pro->rvsra;
+  cs_real_t rhum = rair*(1. + (rvsra - 1.)*(aopt->meteo_qw0 - aopt->meteo_ql0)
+                        - aopt->meteo_ql0);
+  phys_pro->ro0 = phys_pro->p0/(rhum * aopt->meteo_t0); /* ref density T0 */
+  cs_real_t clatev = phys_pro->clatev;
+  cs_real_t theta0 = (aopt->meteo_t0 - clatev/cp0 * aopt->meteo_ql0)
+                   * pow(pref/ aopt->meteo_psea, rscp);
+
 
   cs_real_t z0 = aopt->meteo_z0;
   cs_real_t zref = aopt->meteo_zref;
@@ -2682,7 +2693,7 @@ cs_atmo_init_meteo_profiles(void)
     aopt->meteo_qw0    = qw1
       - aopt->meteo_qwstar * cs_mo_psih(z1 + z0, z0, dlmo) / kappa;
     aopt->meteo_t0     = t1
-      - aopt->meteo_tstar * cs_mo_psih(z1 + z0, z0, dlmo) / kappa;
+      - aopt->meteo_tstar * cs_mo_psih(z1 + z0, z0, dlmo) / kappa;//FIXME conversion theta->T
 
   }
 
@@ -2730,13 +2741,14 @@ cs_atmo_init_meteo_profiles(void)
 
   bft_printf("\n Meteo preprocessing values for computation:\n"
              "  dlmo=%17.9e\n ustar=%17.9e\n tstar=%17.9e\n qwstar=%17.9e\n"
-             " t0=%17.9e\n qw0=%17.9e\n",
+             " t0=%17.9e\n qw0=%17.9e\n ql0=%17.9e\n",
              aopt->meteo_dlmo,
              aopt->meteo_ustar0,
              aopt->meteo_tstar,
              aopt->meteo_qwstar,
              aopt->meteo_t0,
-             aopt->meteo_qw0);
+             aopt->meteo_qw0,
+             aopt->meteo_ql0);
 }
 
 /*----------------------------------------------------------------------------*/

src/atmo/cs_atmo.h
@@ -317,14 +317,16 @@ typedef struct {
 
   /*! Meteo reference mass fraction at 2m */
   cs_real_t meteo_qw0;
+  /*! Meteo reference ground friction mass fraction */
+  cs_real_t meteo_qwstar;
   /*! Meteo reference mass fraction 1 */
   cs_real_t meteo_qw1;
   /*! Meteo reference mass fraction 2 */
   cs_real_t meteo_qw2;
+  /*! Meteo reference mass fraction at 2m */
+  cs_real_t meteo_ql0;
   /*! Meteo reference evaporation  */
   cs_real_t meteo_evapor;
-  /*! Meteo reference ground friction mass fraction */
-  cs_real_t meteo_qwstar;
   /*! Meteo reference sensible heat */
   cs_real_t meteo_sensi;
   /*! Universal function Phi_m for stable condition */

commit 043350f1714983e8c46ddef5e67fa90ee59cc1a5

[Atmo] Fix bug introduced in commit (42bdb975).

src/atmo/cs_atmo.c
@@ -191,7 +191,7 @@ static cs_atmo_option_t  _atmo_option = {
   .meteo_t1 = 0.,
   .meteo_t2 = 0.,
   .meteo_tstar = 0.,
-  .meteo_qw0 = DBL_MAX,
+  .meteo_qw0 = 0.,
   .meteo_qwstar = DBL_MAX,
   .meteo_qw1 = DBL_MAX,
   .meteo_qw2 = DBL_MAX,
@@ -2730,7 +2730,7 @@ cs_atmo_init_meteo_profiles(void)
   /* Force the computation of z_ground */
   aopt->compute_z_ground = true;
 
-  if (is_humid && aopt->meteo_qw0 <= 0. && aopt->meteo_qw1 < 0
+  if (is_humid && aopt->meteo_qw0 < 0. && aopt->meteo_qw1 < 0
       && aopt->meteo_qw2 < 0)
     bft_error(__FILE__,
               __LINE__,