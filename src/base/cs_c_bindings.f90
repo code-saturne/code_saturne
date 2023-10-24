code-saturne/code_saturne

commit b6bdcb5548165264b3fdbb8acce1a50b8c15cc36

A few simplifications and updates in Fortran gradient API.

src/base/cs_c_bindings.f90
@@ -3392,22 +3392,20 @@ module cs_c_bindings
 
     !---------------------------------------------------------------------------
 
-    ! Interface to C function for scalar potential gradient
+    ! Interface to C function for scalar gradient with homogeneous Neumann BCs
 
-    subroutine cs_f_gradient_potential(f_id, imrgra, inc, n_r_sweeps,          &
-                                       iphydp, iwarnp, imligp,                 &
-                                       epsrgp, climgp,                         &
-                                       f_ext, coefap, coefbp, pvar, grad)      &
-      bind(C, name='cs_f_gradient_potential')
+    subroutine cs_f_gradient_hn_s(f_id, imrgra, inc, n_r_sweeps,               &
+                                  iwarnp, imligp, epsrgp, climgp,              &
+                                  pvar, grad)                                  &
+      bind(C, name='cs_f_gradient_s')
       use, intrinsic :: iso_c_binding
       implicit none
       integer(c_int), value :: f_id, imrgra, inc, n_r_sweeps
-      integer(c_int), value :: iphydp, iwarnp, imligp
+      integer(c_int), value :: iwarnp, imligp
       real(kind=c_double), value :: epsrgp, climgp
-      real(kind=c_double), dimension(*), intent(in) :: coefap, coefbp
-      real(kind=c_double), dimension(*), intent(inout) :: f_ext, pvar
+      real(kind=c_double), dimension(*), intent(inout) :: pvar
       real(kind=c_double), dimension(*), intent(inout) :: grad
-    end subroutine cs_f_gradient_potential
+    end subroutine cs_f_gradient_hn_s
 
     !---------------------------------------------------------------------------
 
@@ -4205,6 +4203,52 @@ contains
                          epsrgp, climgp, coefap, coefbp, pvar, grad)
 
   end subroutine gradient_s
+
+  !=============================================================================
+
+  !> \brief  Compute cell gradient for scalar with homegeneous gradient.
+
+  !> \param[in]       f_id             field id, or -1
+  !> \param[in]       imrgra           gradient computation mode
+  !> \param[in]       inc              0: increment; 1: do not increment
+  !> \param[in]       nswrgp           number of sweeps for reconstruction
+  !> \param[in]       imligp           gradient limitation method:
+  !>                                     < 0 no limitation
+  !>                                     = 0 based on neighboring gradients
+  !>                                     = 1 based on mean gradient
+  !> \param[in]       iwarnp           verbosity
+  !> \param[in]       epsrgp           relative precision for reconstruction
+  !> \param[in]       climgp           limiter coefficient for imligp
+  !> \param[in, out]  pvar             cell values whose gradient is computed
+  !> \param[out]      grad             resulting gradient
+
+  subroutine gradient_hn_s(f_id, imrgra, inc, nswrgp,                          &
+                           imligp, iwarnp, epsrgp, climgp,                     &
+                           pvar, grad)
+
+    use, intrinsic :: iso_c_binding
+    use paramx
+    use mesh
+    use field
+    use period
+
+    implicit none
+
+    ! Arguments
+
+    integer, intent(in) :: f_id, imrgra, inc, nswrgp
+    integer, intent(in) :: imligp, iwarnp
+    double precision, intent(in) :: epsrgp, climgp
+    real(kind=c_double), dimension(ncelet), intent(inout) :: pvar
+    real(kind=c_double), dimension(3, ncelet), intent(out) :: grad
+
+    ! The gradient of a potential (pressure, ...) is a vector
+
+    call cs_f_gradient_hn_s(f_id, imrgra, inc, nswrgp,                         &
+                            iwarnp, imligp,                                    &
+                            epsrgp, climgp, pvar, grad)
+
+  end subroutine gradient_hn_s
 
   !=============================================================================
 

src/alge/cs_gradient.cxx
@@ -9175,20 +9175,16 @@ cs_f_gradient_s(int               f_id,
                 cs_real_3_t       grad[]);
 
 void
-cs_f_gradient_potential(int               f_id,
-                        int               imrgra,
-                        int               inc,
-                        int               n_r_sweeps,
-                        int               iphydp,
-                        int               iwarnp,
-                        int               imligp,
-                        cs_real_t         epsrgp,
-                        cs_real_t         climgp,
-                        cs_real_3_t       f_ext[],
-                        const cs_real_t   coefap[],
-                        const cs_real_t   coefbp[],
-                        cs_real_t         pvar[],
-                        cs_real_3_t       grad[]);
+cs_f_gradient_hn_s(int               f_id,
+                   int               imrgra,
+                   int               inc,
+                   int               n_r_sweeps,
+                   int               iwarnp,
+                   int               imligp,
+                   cs_real_t         epsrgp,
+                   cs_real_t         climgp,
+                   cs_real_t         pvar[],
+                   cs_real_3_t       grad[]);
 
 void
 cs_f_gradient_weighted_s(int               f_id,
@@ -9212,8 +9208,7 @@ cs_f_gradient_weighted_s(int               f_id,
  *============================================================================*/
 
 /*----------------------------------------------------------------------------
- * Compute cell gradient of scalar field or component of vector or
- * tensor field.
+ * Compute cell gradient of scalar field.
  *----------------------------------------------------------------------------*/
 
 void
@@ -9283,75 +9278,33 @@ cs_f_gradient_s(int               f_id,
 }
 
 /*----------------------------------------------------------------------------
- * Compute cell gradient of potential-type values.
+ * Compute cell gradient of scalar field with homogeneous Neumann BC's.
  *----------------------------------------------------------------------------*/
 
 void
-cs_f_gradient_potential(int               f_id,
-                        int               imrgra,
-                        int               inc,
-                        int               n_r_sweeps,
-                        int               iphydp,
-                        int               iwarnp,
-                        int               imligp,
-                        cs_real_t         epsrgp,
-                        cs_real_t         climgp,
-                        cs_real_3_t       f_ext[],
-                        const cs_real_t   coefap[],
-                        const cs_real_t   coefbp[],
-                        cs_real_t         pvar[],
-                        cs_real_3_t       grad[])
+cs_f_gradient_hn_s(int               f_id,
+                   int               imrgra,
+                   int               inc,
+                   int               n_r_sweeps,
+                   int               iwarnp,
+                   int               imligp,
+                   cs_real_t         epsrgp,
+                   cs_real_t         climgp,
+                   cs_real_t         pvar[],
+                   cs_real_3_t       grad[])
 {
-  cs_halo_type_t halo_type = CS_HALO_STANDARD;
-  cs_gradient_type_t gradient_type = CS_GRADIENT_GREEN_ITER;
-
-  char var_name[32];
-  if (f_id > -1) {
-    cs_field_t *f = cs_field_by_id(f_id);
-    snprintf(var_name, 31, "%s", f->name);
-  }
-  else
-    strcpy(var_name, "Work array");
-  var_name[31] = '\0';
-
-  /* Choose gradient type */
-
-  cs_gradient_type_by_imrgra(imrgra,
-                             &gradient_type,
-                             &halo_type);
-
-  /* Check if given field has internal coupling  */
-  cs_internal_coupling_t  *cpl = NULL;
-  if (f_id > -1) {
-    const int key_id = cs_field_key_id_try("coupling_entity");
-    if (key_id > -1) {
-      const cs_field_t *f = cs_field_by_id(f_id);
-      int coupl_id = cs_field_get_key_int(f, key_id);
-      if (coupl_id > -1)
-        cpl = cs_internal_coupling_by_id(coupl_id);
-    }
-  }
-
-  /* Compute gradient */
-
-  cs_gradient_scalar(var_name,
-                     gradient_type,
-                     halo_type,
-                     inc,
-                     n_r_sweeps,
-                     iphydp,
-                     1,             /* w_stride */
-                     iwarnp,
-                     (cs_gradient_limit_t)imligp,
-                     epsrgp,
-                     climgp,
-                     f_ext,
-                     coefap,
-                     coefbp,
-                     pvar,
-                     NULL,          /* c_weight */
-                     cpl,
-                     grad);
+  cs_f_gradient_s(f_id,
+                  imrgra,
+                  inc,
+                  n_r_sweeps,
+                  iwarnp,
+                  imligp,
+                  epsrgp,
+                  climgp,
+                  NULL,  /* coefap */
+                  NULL,  /* coefbp */
+                  pvar,
+                  grad);
 }
 
 /*----------------------------------------------------------------------------
@@ -9432,19 +9385,6 @@ cs_f_gradient_weighted_s(int               f_id,
 /*============================================================================
  * Public function definitions for Fortran API
  *============================================================================*/
-
-/*----------------------------------------------------------------------------
- * Compute the steady balance due to porous modelling for the pressure
- * gradient
- *----------------------------------------------------------------------------*/
-
-void CS_PROCF (grdpor, GRDPOR)
-(
- const int   *const inc          /* <-- 0 or 1: increment or not         */
-)
-{
-  cs_gradient_porosity_balance(*inc);
-}
 
 /*============================================================================
  * Public function definitions

src/alge/cs_gradient.h
@@ -90,20 +90,6 @@ typedef enum {
 
 extern const char *cs_gradient_type_name[];
 
-/*============================================================================
- * Public function prototypes for Fortran API
- *============================================================================*/
-
-/*----------------------------------------------------------------------------
- * Compute the steady balance due to porous modelling for the pressure
- * gradient
- *----------------------------------------------------------------------------*/
-
-void CS_PROCF (grdpor, GRDPOR)
-(
- const int   *const inc          /* <-- 0 or 1: increment or not         */
-);
-
 /*=============================================================================
  * Public function prototypes
  *============================================================================*/

src/atmo/attssc.f90
@@ -334,7 +334,6 @@ contains
     double precision climgp, epsrgp, depo
 
     integer    ii, iifld, imligp, inc, iwarnp, imrgrp, nswrgp, ifac, iel
-    double precision, dimension(:), allocatable :: local_coefa, local_coefb
     double precision, dimension(:), allocatable :: local_field, sed_vel
     double precision, dimension(:), allocatable :: pres
 
@@ -418,16 +417,6 @@ contains
     iwarnp = vcopt%iwarni
     climgp = vcopt%climgr
 
-    ! homogeneous Neumann BCs for gradient computation
-    allocate(local_coefa(nfabor))
-    do ifac = 1, nfabor
-      local_coefa(ifac) = 0.d0
-    enddo
-    allocate(local_coefb(nfabor))
-    do ifac = 1, nfabor
-      local_coefb(ifac) = 1.d0
-    enddo
-
     allocate(local_field(ncelet))
 
     ! Computation of the gradient of rho*qliq*V(r3)*exp(5*sc^2)
@@ -440,11 +429,10 @@ contains
                                             ! law of the droplet spectrum
     enddo
 
-    call gradient_s                                                 &
+    call gradient_hn_s                                              &
    ( iifld  , imrgrp , inc    , nswrgp , imligp,                    &
      iwarnp , epsrgp , climgp ,                                     &
-     local_field     , local_coefa , local_coefb ,                  &
-     grad1   )
+     local_field     , grad1   )
 
     ! Computation of the gradient of Nc*V(r3)*exp(-sc^2)
 
@@ -456,16 +444,13 @@ contains
                                             ! law of the droplet spectrum
     enddo
 
-    call gradient_s                                                 &
+    call gradient_hn_s                                              &
    ( iifld  , imrgrp , inc    , nswrgp , imligp,                    &
      iwarnp , epsrgp , climgp ,                                     &
-     local_field     , local_coefa , local_coefb ,                  &
-     grad2   )
+     local_field     , grad2   )
 
     deallocate(sed_vel)
 
-    deallocate(local_coefa)
-    deallocate(local_coefb)
     deallocate(local_field)
 
   end subroutine grad_sed

src/base/cs_field_operator.c
@@ -117,14 +117,6 @@ cs_f_field_gradient_scalar(int                    f_id,
                            cs_real_3_t  *restrict grad);
 
 void
-cs_f_field_gradient_potential(int                    f_id,
-                              int                    use_previous_t,
-                              int                    inc,
-                              int                    hyd_p_flag,
-                              cs_real_3_t            f_ext[],
-                              cs_real_3_t  *restrict grad);
-
-void
 cs_f_field_gradient_vector(int                     f_id,
                            int                     use_previous_t,
                            int                     inc,
@@ -381,40 +373,6 @@ cs_f_field_gradient_scalar(int                    f_id,
                            _use_previous_t,
                            inc,
                            grad);
-}
-
-/*----------------------------------------------------------------------------
- * Compute cell gradient of scalar field or component of vector or
- * tensor field.
- *
- * parameters:
- *   f_id           <-- field id
- *   use_previous_t <-- should we use values from the previous time step ?
- *   halo_type      <-- halo type
- *   inc            <-- if 0, solve on increment; 1 otherwise
- *   hyd_p_flag     <-- flag for hydrostatic pressure
- *   f_ext          <-- exterior force generating the hydrostatic pressure
- *   grad           --> gradient
- *----------------------------------------------------------------------------*/
-
-void
-cs_f_field_gradient_potential(int                    f_id,
-                              int                    use_previous_t,
-                              int                    inc,
-                              int                    hyd_p_flag,
-                              cs_real_3_t            f_ext[],
-                              cs_real_3_t  *restrict grad)
-{
-  bool _use_previous_t = use_previous_t ? true : false;
-
-  const cs_field_t *f = cs_field_by_id(f_id);
-
-  cs_field_gradient_potential(f,
-                              _use_previous_t,
-                              inc,
-                              hyd_p_flag,
-                              f_ext,
-                              grad);
 }
 
 /*----------------------------------------------------------------------------

src/base/field_operator.f90
@@ -55,30 +55,6 @@ module field_operator
 
     !---------------------------------------------------------------------------
 
-    !> \brief  Compute cell gradient of potential field
-
-    !> \param[in]   f_id             field id
-    !> \param[in]   use_previous_t   1 if values at previous time step should
-    !>                               be used, 0 otherwise
-    !> \param[in]   inc              0: increment; 1: do not increment
-    !> \param[in]   hyd_p_flag       flag for hydrostatic pressure
-    !> \param[in]   f_ext            exterior force generating
-    !>                               the hydrostatic pressure
-    !> \param[out]  grad             gradient
-
-    subroutine field_gradient_potential(f_id, use_previous_t, inc, hyd_p_flag, &
-                                        f_ext, grad)                           &
-      bind(C, name='cs_f_field_gradient_potential')
-      use, intrinsic :: iso_c_binding
-      implicit none
-      integer(c_int), value                :: f_id, use_previous_t, inc
-      integer(c_int), value                :: hyd_p_flag
-      real(kind=c_double), dimension(3, *) :: f_ext
-      real(kind=c_double), dimension(*)    :: grad
-    end subroutine field_gradient_potential
-
-    !---------------------------------------------------------------------------
-
     !> \brief  Compute cell gradient of vector field.
 
     !> \param[in]   f_id             field id

src/cfbl/cfener.f90
@@ -450,26 +450,11 @@ if (vcopt_e%idiff.ge. 1) then
   epsrgp = vcopt_u%epsrgr
   climgp = vcopt_u%climgr
 
-! Allocate temporary arrays
-  allocate(coefap(nfabor))
-  allocate(coefbp(nfabor))
-
-  do ifac = 1, nfabor
-    coefap(ifac) = zero
-    coefbp(ifac) = 1.d0
-  enddo
-
-!  f_id0 = -1 (indicates, for the rotation periodicity,
-!              that the variable is not Rij)
   f_id0 = -1
-  call gradient_s                                                   &
+  call gradient_hn_s                                                &
    ( f_id0  , imrgrp , inc    , nswrgp , imligp ,                   &
-     iwarnp , epsrgp , climgp ,                                     &
-     w7     , coefap , coefbp ,                                     &
+     iwarnp , epsrgp , climgp , w7     ,                            &
      grad   )
-
-! Free memory
-  deallocate(coefap, coefbp)
 
 ! Internal faces
 

src/comb/cs_coal_fp2st.f90
@@ -122,9 +122,9 @@ call field_get_key_double(ivarfl(isca(iscal)), krvarfl, rvarfl)
 !===============================================================================
 ! Allocation dynamic arrays
 !----
-allocate(x1(1:ncelet) , f1f2(1:ncelet),                STAT=iok1)
+allocate(x1(1:ncelet), f1f2(1:ncelet), stat=iok1)
 allocate(grad(3,ncelet), stat=iok1)
-if ( iok1 > 0 ) then
+if (iok1 > 0) then
   write(nfecra,*) ' Memory allocation error inside: '
   write(nfecra,*) '     cs_coal_fp2st               '
   call csexit(1)
@@ -205,28 +205,10 @@ if ( itytur.eq.2 .or. iturb.eq.50 .or.             &
 
 ! --> Calcul du gradient de f1f2
 
-  ! Allocate temporary arrays
-  allocate(coefap(nfabor), coefbp(nfabor), STAT=iok1)
-
-  if (iok1 > 0) then
-    write(nfecra,*) ' Memory allocation error inside : '
-    write(nfecra,*) '     cs_coal_fp2st                '
-    call csexit(1)
-  endif
-
-  do ifac = 1, nfabor
-    ! Homogenous Neumann on the gradient
-    coefap(ifac) = zero
-    coefbp(ifac) = 1.d0
-  enddo
-
-!  f_id0 = -1 (indique pour la periodicite de rotation que la variable
-!              n'est pas Rij)
   f_id0  = -1
-  call gradient_s                                                 &
+  call gradient_hn_s                                              &
  ( f_id0  , imrgrp , inc    , nswrgp , imligp ,                   &
-   iwarnp , epsrgp , climgp ,                                     &
-   f1f2   , coefap , coefbp ,                                     &
+   iwarnp , epsrgp , climgp , f1f2   ,                            &
    grad   )
 
   call field_get_key_double(ivarfl(isca(iscal)), ksigmas, turb_schmidt)
@@ -337,7 +319,6 @@ endif
 
 ! Free memory
 deallocate(x1,f1f2,grad,stat=iok1)
-deallocate(coefap, coefbp, stat=iok2)
 
 if ( iok1 > 0 .or. iok2 > 0) then
   write(nfecra,*) ' Memory deallocation error inside: '

commit 8e2443523c1a6496e6927a246e8232e3944ec080

Fix Fortran binding, broken by commit b6bdcb55 (yesterday).

src/base/cs_c_bindings.f90
@@ -3397,7 +3397,7 @@ module cs_c_bindings
     subroutine cs_f_gradient_hn_s(f_id, imrgra, inc, n_r_sweeps,               &
                                   iwarnp, imligp, epsrgp, climgp,              &
                                   pvar, grad)                                  &
-      bind(C, name='cs_f_gradient_s')
+      bind(C, name='cs_f_gradient_hn_s')
       use, intrinsic :: iso_c_binding
       implicit none
       integer(c_int), value :: f_id, imrgra, inc, n_r_sweeps