7494fc25a6d7d1c9cdef511dd4ce815ec68934db
ddiakiteaneo <ddiakite@aneo.fr>
2023-11-02 15:36:50 +0100

    Gather version in progress

---
+++ src/alge/cs_gradient_lsq_vector_gather.cuh
@@ -0,0 +1,78 @@
+/*============================================================================
+ * Gradient reconstruction, CUDA implementations.
+ *============================================================================*/
+
+/*
+  This file is part of code_saturne, a general-purpose CFD tool.
+
+  Copyright (C) 1998-2023 EDF S.A.
+
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation; either version 2 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+  Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/*----------------------------------------------------------------------------*/
+
+__global__ static void
+_compute_rhs_lsq_v_i_face_gather(cs_lnum_t            size,
+                          const cs_lnum_t      *restrict cell_cells_idx,
+                          const cs_lnum_t      *restrict cell_cells_lst,
+                          const cs_real_3_t    *restrict cell_f_cen,
+                          cs_real_33_t         *restrict rhs,
+                          const cs_real_3_t    *restrict pvar,
+                          const cs_real_t         *restrict weight,
+                          const cs_real_t      *restrict c_weight)
+{
+  cs_lnum_t c_id = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if(c_id >= size){
+    return;
+  }
+  cs_real_t dc[3], fctb[3], ddc, _weight1, _weight2, _denom, _pond, pfac;
+  cs_lnum_t c_id1, c_id2;
+
+  cs_lnum_t s_id = cell_cells_idx[c_id];
+  cs_lnum_t e_id = cell_cells_idx[c_id + 1];
+
+  for(cs_lnum_t index = s_id; index < e_id; index++){
+    c_id2 = cell_cells_lst[index];
+
+    dc[0] = cell_f_cen[c_id2][0] - cell_f_cen[c_id][0];
+    dc[1] = cell_f_cen[c_id2][1] - cell_f_cen[c_id][1];
+    dc[2] = cell_f_cen[c_id2][2] - cell_f_cen[c_id][2];
+
+    ddc = 1./(dc[0]*dc[0] + dc[1]*dc[1] + dc[2]*dc[2]);
+    
+    if (c_weight == NULL){
+      _weight1 = 1.;
+      _weight2 = 1.;
+    }
+    else{
+      _pond = weight[index];
+      _denom = 1. / (  _pond       *c_weight[c_id]
+                                  + (1. - _pond)*c_weight[c_id2]);
+      _weight1 = c_weight[c_id] * _denom;
+      _weight2 = c_weight[c_id2] * _denom;
+    }
+    
+    for(cs_lnum_t i = 0; i < 3; i++){
+      pfac = (pvar[c_id2][i] - pvar[c_id][i]) * ddc;
+      for(cs_lnum_t j = 0; j < 3; j++){
+        fctb[j] = dc[j] * pfac;
+        rhs[c_id][i][j] += _weight2 * fctb[j];
+        rhs[c_id2][i][j] += _weight1 * fctb[j];
+      }
+    }
+  }
+}
