code-saturne/code_saturne

commit cd14099b783a13dc3464d3eac399f01350096221

[MEG] Add an xdef wrapper for MEG functions
  * The wrapper function is based on cs_analytic_func_t in order to
    minimize modifications of other parts of the code.
  * This wrapper uses a specific input structure which allows providing
    the correct input data to the meg function.
  * First step of linking CDO solvers to GUI

src/meg/cs_meg_xdef_wrapper.c
@@ -1,0 +1,158 @@
+
+/*----------------------------------------------------------------------------
+ *  Local headers
+ *----------------------------------------------------------------------------*/
+
+#include "cs_array.h"
+#include "cs_base.h"
+#include "cs_meg_prototypes.h"
+#include "cs_meg_xdef_wrapper.h"
+
+BEGIN_C_DECLS
+
+
+/*============================================================================
+ * Public functions
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Wrapper function allowing to call MEG functions by xdef structres.
+ * This is done by using the cs_xdef_analytic_function type.
+ *
+ * \param[in] time          when ?
+ * \param[in] n_elts        number of elements to consider
+ * \param[in] elt_ids       list of elements ids (in coords and retval)
+ * \param[in] coords        where ?
+ * \param[in] dense_output  perform an indirection in retval or not
+ * \param[in] input         pointer to cs_meg_xdef_input_t
+ * \param[in] retval        resultint value(s). Must be allocated
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_meg_xdef_wrapper(cs_real_t         time,
+                    cs_lnum_t         n_elts,
+                    const cs_lnum_t  *elt_ids,
+                    const cs_real_t  *coords,
+                    bool              dense_output,
+                    void             *input,
+                    cs_real_t        *retval)
+{
+  CS_UNUSED(time);
+
+  const cs_meg_xdef_input_t *_input = input;
+
+  const cs_real_3_t *_coords = (const cs_real_3_t *)coords;
+
+
+  cs_real_t *meg_vals = NULL;
+  /* Volume function takes as an input arrays over the entire domain */
+  if (_input->type == CS_MEG_VOLUME_FUNC) {
+    if (dense_output)
+      bft_error(__FILE__, __LINE__, 0,
+                _("Error: MEG Volume function cannot be used with a dense output.\n"));
+    else
+      meg_vals = retval;
+  }
+  else {
+    if (dense_output)
+      meg_vals = retval;
+    else
+      BFT_MALLOC(meg_vals, cs_glob_mesh->n_cells * _input->stride, cs_real_t);
+  }
+
+  switch(_input->type) {
+  case CS_MEG_BOUNDARY_FUNC:
+    {
+      const cs_zone_t *z = cs_boundary_zone_by_id(_input->z_id);
+      cs_meg_boundary_function(z->name,
+                               n_elts,
+                               elt_ids,
+                               _coords,
+                               _input->input_name,
+                               _input->additional_data,
+                               meg_vals);
+
+      /* Copy values to retval, knowing that "meg_vals" is dense! */
+      if (!dense_output) {
+        cs_array_real_copy_subset(n_elts,
+                                  _input->stride * n_elts,
+                                  elt_ids,
+                                  CS_ARRAY_SUBSET_OUT,
+                                  meg_vals,
+                                  retval);
+        BFT_FREE(meg_vals);
+      }
+    }
+    break;
+  case CS_MEG_VOLUME_FUNC:
+    {
+      const cs_zone_t *z = cs_volume_zone_by_id(_input->z_id);
+      cs_meg_volume_function(z->name,
+                             n_elts,
+                             elt_ids,
+                             _coords,
+                             _input->input_name,
+                             &(meg_vals));
+    }
+    break;
+  case CS_MEG_INITIALIZATION_FUNC:
+    {
+      const cs_zone_t *z = cs_volume_zone_by_id(_input->z_id);
+      cs_meg_initialization(z->name,
+                            n_elts,
+                            elt_ids,
+                            _coords,
+                            _input->input_name,
+                            meg_vals);
+
+      /* Copy values to retval, knowing that "meg_vals" is dense! */
+      if (!dense_output) {
+        cs_array_real_copy_subset(n_elts,
+                                  _input->stride * n_elts,
+                                  elt_ids,
+                                  CS_ARRAY_SUBSET_OUT,
+                                  meg_vals,
+                                  retval);
+        BFT_FREE(meg_vals);
+      }
+    }
+    break;
+  case CS_MEG_SOURCE_TERM_FUNC:
+    {
+      const cs_zone_t *z = cs_volume_zone_by_id(_input->z_id);
+      cs_meg_source_terms(z->name,
+                          n_elts,
+                          elt_ids,
+                          _coords,
+                          _input->input_name,
+                          _input->additional_data,
+                          meg_vals);
+
+      /* Copy values to retval, knowing that "meg_vals" is dense! */
+      if (!dense_output) {
+        cs_array_real_copy_subset(n_elts,
+                                  _input->stride * n_elts,
+                                  elt_ids,
+                                  CS_ARRAY_SUBSET_OUT,
+                                  meg_vals,
+                                  retval);
+        BFT_FREE(meg_vals);
+      }
+    }
+    break;
+  default:
+    {
+      bft_error(__FILE__, __LINE__, 0,
+                _("Error: \"%s\" was called with an incompatible type of MEG function.\n"),
+                __func__);
+    }
+    break;
+  }
+
+}
+
+/*----------------------------------------------------------------------------*/
+
+END_C_DECLS

src/meg/Makefile.am
@@ -66,6 +66,7 @@ EXTRA_DIST =
 
 pkginclude_HEADERS = \
 cs_meg_prototypes.h \
+cs_meg_xdef_wrapper.h \
 cs_meg_headers.h
 
 # Library source files
@@ -78,7 +79,8 @@ cs_meg_initialization.c \
 cs_meg_post_output.c \
 cs_meg_post_profile.c \
 cs_meg_source_terms.c \
-cs_meg_volume_function.c
+cs_meg_volume_function.c \
+cs_meg_xdef_wrapper.c
 
 noinst_LIBRARIES = libcsmeg.a
 libcsmeg_a_SOURCES = $(megfiles)

src/meg/cs_meg_headers.h
@@ -30,6 +30,7 @@
  *----------------------------------------------------------------------------*/
 
 #include "cs_meg_prototypes.h"
+#include "cs_meg_xdef_wrapper.h"
 
 /*----------------------------------------------------------------------------*/
 

src/meg/cs_meg_xdef_wrapper.h
@@ -1,0 +1,107 @@
+#ifndef __CS_MEG_XDEF_WRAPPER_H__
+#define __CS_MEG_XDEF_WRAPPER_H__
+
+/*============================================================================
+ * MEG (Mathematical Expression Generator) functions xdef wrapper
+ *============================================================================*/
+
+/*
+  This file is part of code_saturne, a general-purpose CFD tool.
+
+  Copyright (C) 1998-2023 EDF S.A.
+
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation; either version 2 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+  Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+ *  Local headers
+ *----------------------------------------------------------------------------*/
+
+#include "cs_base.h"
+
+/*----------------------------------------------------------------------------*/
+
+BEGIN_C_DECLS
+
+/*============================================================================
+ * Type definitions
+ *============================================================================*/
+
+/* Enum for MEG function type */
+typedef enum {
+  CS_MEG_BOUNDARY_FUNC,
+  CS_MEG_VOLUME_FUNC,
+  CS_MEG_INITIALIZATION_FUNC,
+  CS_MEG_SOURCE_TERM_FUNC,
+  CS_MEG_IBM_FUNC,
+  CS_MEG_FSI_STRUCT_FUNC,
+  CS_MEG_POST_ACTIVATE_FUNC,
+  CS_MEG_POST_PROFILES_FUNC,
+
+  CS_MEG_N_FUNC_TYPES
+} cs_meg_function_type_t;
+
+typedef struct {
+  /* type of meg function */
+  cs_meg_function_type_t type;
+
+  /* cs_zone_t id */
+  int z_id;
+
+  /* values stride size */
+  int stride;
+
+  /* Input name used for function */
+  char input_name[512];
+
+  /* boundary or source term data*/
+  char additional_data[512];
+
+} cs_meg_xdef_input_t;
+
+/*============================================================================
+ * Public functions
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Wrapper function allowing to call MEG functions by xdef structres.
+ * This is done by using the cs_xdef_analytic_function type.
+ *
+ * \param[in] time          when ?
+ * \param[in] n_elts        number of elements to consider
+ * \param[in] elt_ids       list of elements ids (in coords and retval)
+ * \param[in] coords        where ?
+ * \param[in] dense_output  perform an indirection in retval or not
+ * \param[in] input         pointer to cs_meg_xdef_input_t
+ * \param[in] retval        resultint value(s). Must be allocated
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_meg_xdef_wrapper(cs_real_t         time,
+                    cs_lnum_t         n_elts,
+                    const cs_lnum_t  *elt_ids,
+                    const cs_real_t  *coords,
+                    bool              dense_output,
+                    void             *input,
+                    cs_real_t        *retval);
+/*----------------------------------------------------------------------------*/
+
+END_C_DECLS
+
+#endif

commit 9e481b283b72e61091187195b143b59067331bca

[MEG] Fix stride size in MEG xdef wrapper function.
  * bug introduced in commit cd14099b

src/meg/cs_meg_xdef_wrapper.c
@@ -191,7 +191,7 @@ cs_meg_xdef_wrapper(cs_real_t         time,
       /* Copy values to retval, knowing that "meg_vals" is dense! */
       if (!dense_output) {
         cs_array_real_copy_subset(n_elts,
-                                  _input->stride * n_elts,
+                                  _input->stride,
                                   elt_ids,
                                   CS_ARRAY_SUBSET_OUT,
                                   meg_vals,
@@ -224,7 +224,7 @@ cs_meg_xdef_wrapper(cs_real_t         time,
       /* Copy values to retval, knowing that "meg_vals" is dense! */
       if (!dense_output) {
         cs_array_real_copy_subset(n_elts,
-                                  _input->stride * n_elts,
+                                  _input->stride,
                                   elt_ids,
                                   CS_ARRAY_SUBSET_OUT,
                                   meg_vals,
@@ -247,7 +247,7 @@ cs_meg_xdef_wrapper(cs_real_t         time,
       /* Copy values to retval, knowing that "meg_vals" is dense! */
       if (!dense_output) {
         cs_array_real_copy_subset(n_elts,
-                                  _input->stride * n_elts,
+                                  _input->stride,
                                   elt_ids,
                                   CS_ARRAY_SUBSET_OUT,
                                   meg_vals,