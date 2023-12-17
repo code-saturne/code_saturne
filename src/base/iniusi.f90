code-saturne/code_saturne

commit c1b752e549e37e0029ae4e193e00b03151904261

Major commit: move radiative property definitions to an earlier point.
This aligns those definitions with those of other models and allows setting
field vamues at the usual call sites (especially user-defined functions).

Also:
- Rename the file cs_rad_transfer_property_fields.
- Use field structure for the radiance.
- It will allow to set up boundary conditions as for the rest of the code,
  and to use equation parameters per band.
- Remove cs_user_radiative_transfer_parameters which can be set in
  cs_user_model.
- Prepare use of classical BC arrays (rcodcl, icodcl).
- Computation of net flux should be corrected with multibands and
  inlet/outlets. For now it is coded as before.

Edit by Y. Fournier: restore optional cs_user_radiative_transfer_parameters
function to try to avoid breaking some test cases, but mark it as deprecated.

src/base/iniusi.f90
@@ -227,6 +227,12 @@ interface
     real(c_double), value :: value
   end subroutine cs_runaway_check_define_field_max
 
+  subroutine cs_user_radiative_transfer_parameters()  &
+       bind(C, name='cs_user_radiative_transfer_parameters')
+    use, intrinsic :: iso_c_binding
+    implicit none
+  end subroutine cs_user_radiative_transfer_parameters
+
   ! Interface to C function to initialize CDO model structures
 
   subroutine cs_f_domain_setup_init_model_context()  &
@@ -295,6 +301,14 @@ call csiphy()
 ! before property fields are created).
 call cs_gui_physical_constants
 
+! Activate radiative transfer model
+
+! This module must be activated early so as to reserve the associated
+! variables in some physical models.
+
+call cs_gui_radiative_transfer_parameters
+call cs_user_radiative_transfer_parameters ! deprecated, kept for compatibility
+
 ! Flow and other models selection through user C function
 call cs_user_model
 
@@ -336,16 +350,7 @@ call cs_velocity_pressure_set_solid
 
 call cfnmtd(ficfpp, len(ficfpp))
 
-! --- Activation du module transferts radiatifs
-
-!     Il est necessaire de connaitre l'activation du module transferts
-!     radiatifs tres tot de maniere a pouvoir reserver les variables
-!     necessaires dans certaines physiques particuliere
-
-!   - Interface code_saturne
-!     ======================
-
-call cs_gui_radiative_transfer_parameters
+call cs_rad_transfer_options
 
 ! Define fields for variables, check and build iscapp
 ! and computes the number of user scalars (nscaus)

src/base/cs_boundary_conditions_set_coeffs.c
@@ -156,6 +156,61 @@ cs_f_user_boundary_conditions_wrapper(const cs_lnum_t  itrifb[],
  *============================================================================*/
 
 /*----------------------------------------------------------------------------
+ * Compute boundary condition code for radiative transfer
+ *----------------------------------------------------------------------------*/
+
+static void
+_boundary_condition_rt_type(const cs_mesh_t             *m,
+                            const cs_mesh_quantities_t  *mq,
+                            const bool                  init,
+                            const cs_real_t             dt[],
+                            const int                   bc_type[])
+{
+  const cs_lnum_t n_b_faces = m->n_b_faces;
+  const cs_lnum_t *b_face_cells = m->b_face_cells;
+
+  const cs_real_t *surfbn = mq->b_face_surf;
+  const cs_real_3_t *surfbo = (const cs_real_3_t *)mq->b_face_normal;
+
+  cs_rad_transfer_params_t *rt_params = cs_glob_rad_transfer_params;
+
+  int nwsgg = rt_params->nwsgg;
+
+  /* Initialization
+   * -------------- */
+
+  /* TODO: make a rt_bc_type as ale_bc_type */
+
+  for (int gg_id = 0; gg_id < nwsgg; gg_id++) {
+
+    cs_real_t *radiance = CS_FI_(radiance, gg_id)->val;
+    cs_field_t *f_rad = CS_FI_(radiance, gg_id);
+
+    int *icodcl_rad = NULL;
+    cs_real_t *rcodcl1_rad = NULL;
+
+    if (f_rad->bc_coeffs != NULL) {
+      icodcl_rad = f_rad->bc_coeffs->icodcl;
+      rcodcl1_rad = f_rad->bc_coeffs->rcodcl1;
+    }
+
+
+# pragma omp parallel for  if (n_b_faces > CS_THR_MIN)
+    for (cs_lnum_t face_id = 0; face_id < n_b_faces; face_id++) {
+      if (rcodcl1_rad[face_id] > cs_math_infinite_r*0.5)
+        rcodcl1_rad[face_id] = 0;
+    }
+
+    /* Check the consistency of BC types
+     * --------------------------------- */
+
+    int ierror[1] = {0};
+
+  }
+
+}
+
+/*----------------------------------------------------------------------------
  * Compute boundary condition code for ALE
  *----------------------------------------------------------------------------*/
 
@@ -829,6 +884,13 @@ cs_boundary_conditions_set_coeffs(int        nvar,
                                    false,
                                    dt,
                                    bc_type);
+
+    if (cs_glob_rad_transfer_params->type != CS_RAD_TRANSFER_NONE)
+      _boundary_condition_rt_type(mesh,
+                                  fvq,
+                                  false,
+                                  dt,
+                                  bc_type);
 
     if (cs_turbomachinery_get_model() != CS_TURBOMACHINERY_NONE)
       cs_f_mmtycl(bc_type);
@@ -3737,6 +3799,13 @@ cs_boundary_conditions_set_coeffs_init(void)
                                  true,
                                  dt,
                                  bc_type);
+
+  if (cs_glob_rad_transfer_params->type != CS_RAD_TRANSFER_NONE)
+    _boundary_condition_rt_type(mesh,
+                                cs_glob_mesh_quantities,
+                                true,
+                                dt,
+                                bc_type);
 
   if (cs_turbomachinery_get_model() != CS_TURBOMACHINERY_NONE)
     cs_f_mmtycl(bc_type);

src/base/cs_field_pointer.h
@@ -170,6 +170,7 @@ typedef enum {
 
   CS_ENUMF_(rad_energy),   /*!< Radiative energy */
   CS_ENUMF_(rad_q),        /*!< Radiative flux */
+  CS_ENUMF_(radiance),     /*!< (spectral) radiance field */
 
   CS_ENUMF_(rad_est),      /*!< Radiative flux explicit source term */
   CS_ENUMF_(rad_ist),      /*!< Radiative flux implicit source term */

src/base/cs_parameters.c
@@ -59,6 +59,7 @@
 #include "cs_physical_model.h"
 #include "cs_restart.h"
 #include "cs_restart_default.h"
+#include "cs_rad_transfer_fields.h"
 #include "cs_turbulence_model.h"
 #include "cs_time_moment.h"
 #include "cs_thermal_model.h"
@@ -1518,6 +1519,9 @@ cs_parameters_define_auxiliary_fields(void)
     cs_field_set_key_int(fld, cs_field_key_id("log"), 1);
     cs_field_set_key_int(fld, cs_field_key_id("post_vis"), post_flag);
   }
+
+  /* Property fields relative to radiative transfer */
+  cs_rad_transfer_add_property_fields();
 }
 
 /*----------------------------------------------------------------------------*/

src/base/cs_prototypes.h
@@ -119,6 +119,20 @@ extern void CS_PROCF (initi1, INITI1)
  *   f_id <--   field id
  *
  * returns:
+ *   variable number for defined field
+ *----------------------------------------------------------------------------*/
+
+int
+cs_add_variable_field_indexes(int  f_id);
+
+/*----------------------------------------------------------------------------
+ * Add field indexes associated with a new non-user solved variable,
+ * with default options
+ *
+ * parameters:
+ *   f_id <--   field id
+ *
+ * returns:
  *   scalar number for defined field
  *----------------------------------------------------------------------------*/
 
@@ -947,13 +961,6 @@ cs_user_matrix_tuning(void);
 void
 cs_user_parameters(cs_domain_t *domain);
 
-/*----------------------------------------------------------------------------
- * User function for input of radiative transfer module options.
- *----------------------------------------------------------------------------*/
-
-void
-cs_user_radiative_transfer_parameters(void);
-
 /*-----------------------------------------------------------------------------
  * User subroutine for input of radiative transfer boundary conditions
  *----------------------------------------------------------------------------*/
@@ -1061,10 +1068,6 @@ cs_user_rad_transfer_absorption(const int  bc_type[],
 
 void
 cs_user_rad_transfer_net_flux(const int        itypfb[],
-                              const cs_real_t  coefap[],
-                              const cs_real_t  coefbp[],
-                              const cs_real_t  cofafp[],
-                              const cs_real_t  cofbfp[],
                               const cs_real_t  twall[],
                               const cs_real_t  qincid[],
                               const cs_real_t  xlam[],

src/base/fldvar.f90
@@ -922,7 +922,77 @@ end subroutine add_model_field
 !  mode           name          role                                           !
 !______________________________________________________________________________!
 !> \param[in]  f_id           field id
-!> \param[out] iscal          variable number for defined field
+!> \param[out] ivar           variable number for defined field
+!_______________________________________________________________________________
+
+subroutine add_variable_field_indexes &
+ ( f_id, ivar )
+
+!===============================================================================
+! Module files
+!===============================================================================
+
+use paramx
+use dimens
+use entsor
+use numvar
+use field
+
+!===============================================================================
+
+implicit none
+
+procedure() :: fldvar_check_nvar, init_var_cal_opt
+procedure() :: csexit
+
+! Arguments
+
+integer, intent(in)  :: f_id
+integer, intent(out) :: ivar
+
+! Local variables
+
+integer  dim, ii
+
+integer, save :: keyvar = -1
+
+! Get field dimension
+
+call field_get_dim(f_id, dim)
+
+if (keyvar.lt.0) then
+  call field_get_key_id("variable_id", keyvar)
+endif
+
+ivar = nvar + 1
+nvar = nvar + dim
+
+! Check we have enough slots
+call fldvar_check_nvar
+
+do ii = 1, dim
+  ivarfl(ivar + ii - 1) = f_id
+enddo
+
+call field_set_key_int(f_id, keyvar, ivar)
+call init_var_cal_opt(f_id)
+
+return
+
+end subroutine add_variable_field_indexes
+
+!===============================================================================
+!
+!> \brief add field indexes associated with a new non-user solved
+!>        scalar variable, with default options
+!
+!-------------------------------------------------------------------------------
+! Arguments
+!______________________________________________________________________________.
+!  mode           name          role                                           !
+!______________________________________________________________________________!
+!> \param[in]  f_id           field id
+!> \param[out] iscal          scalar id for defined field
 !_______________________________________________________________________________
 
 subroutine add_model_field_indexes &
@@ -1188,6 +1258,42 @@ end subroutine fldvar_check_nscapp
 !===============================================================================
 ! C bindings (reverse)
 !===============================================================================
+
+!-------------------------------------------------------------------------------
+!> \brief add field indexes associated with a new non-user solved
+!>        variable, with default options
+!
+!> \param[in]  f_id    field id
+
+!> \result             variable number for defined field
+!-------------------------------------------------------------------------------
+
+function cs_add_variable_field_indexes(f_id) result(ivar) &
+  bind(C, name='cs_add_variable_field_indexes')
+
+  use, intrinsic :: iso_c_binding
+  use cs_c_bindings
+
+  implicit none
+
+  procedure() :: add_variable_field_indexes
+
+  ! Arguments
+
+  integer(c_int), value :: f_id
+  integer(c_int) :: ivar
+
+  ! Local variables
+
+  integer f_id0, ivar0
+
+  f_id0 = f_id
+
+  call add_variable_field_indexes(f_id0, ivar0)
+
+  ivar = ivar0
+
+end function cs_add_variable_field_indexes
 
 !-------------------------------------------------------------------------------
 !> \brief add field indexes associated with a new non-user solved

src/base/initi1.f90
@@ -146,8 +146,6 @@ call ppini1
 
 call elec_option_init
 
-call cs_rad_transfer_options
-
 if (ippmod(iatmos).ge.0) call cs_at_data_assim_initialize
 
 ! Lagrangian model options

src/pprt/ppvarp.f90
@@ -118,6 +118,16 @@ interface
 
 end interface
 
+interface
+
+  subroutine cs_rad_transfer_add_variable_fields()  &
+    bind(C, name='cs_rad_transfer_add_variable_fields')
+    use, intrinsic :: iso_c_binding
+    implicit none
+  end subroutine cs_rad_transfer_add_variable_fields
+
+end interface
+
 !===============================================================================
 
 ! Key ids for clipping
@@ -256,6 +266,9 @@ if (ippmod(igmix).ge.0) then
   ! MAP to C API
   call cs_field_pointer_map_gas_mix
 endif
+
+! Radiative transfer
+call cs_rad_transfer_add_variable_fields()
 
 return
 end subroutine

commit 24e1aa51f8bac7537a6e2fcb09f5ec78ab914ba4

Fix coal combustion with radiation, broken by commit c1b752e5 (2023-12-15).

src/base/iniusi.f90
@@ -350,8 +350,6 @@ call cs_velocity_pressure_set_solid
 
 call cfnmtd(ficfpp, len(ficfpp))
 
-call cs_rad_transfer_options
-
 ! Define fields for variables, check and build iscapp
 ! and computes the number of user scalars (nscaus)
 if (icdo.lt.2) then
@@ -360,6 +358,8 @@ if (icdo.lt.2) then
   ! Activate the pressure correction model only if CDO mode is not stand-alone
   call cs_pressure_correction_model_activate
 endif
+
+call cs_rad_transfer_options
 
 if (iale.ge.1) then
   call cs_gui_ale_diffusion_type