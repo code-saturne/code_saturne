code-saturne/code_saturne

commit c1b752e549e37e0029ae4e193e00b03151904261

Major commit: move radiative property definitions to an earlier point.
This aligns those definitions with those of other models and allows setting
field vamues at the usual call sites (especially user-defined functions).

Also:
- Rename the file cs_rad_transfer_property_fields.
- Use field structure for the radiance.
- It will allow to set up boundary conditions as for the rest of the code,
  and to use equation parameters per band.
- Remove cs_user_radiative_transfer_parameters which can be set in
  cs_user_model.
- Prepare use of classical BC arrays (rcodcl, icodcl).
- Computation of net flux should be corrected with multibands and
  inlet/outlets. For now it is coded as before.

Edit by Y. Fournier: restore optional cs_user_radiative_transfer_parameters
function to try to avoid breaking some test cases, but mark it as deprecated.

src/base/fldvar.f90
@@ -922,7 +922,77 @@ end subroutine add_model_field
 !  mode           name          role                                           !
 !______________________________________________________________________________!
 !> \param[in]  f_id           field id
-!> \param[out] iscal          variable number for defined field
+!> \param[out] ivar           variable number for defined field
+!_______________________________________________________________________________
+
+subroutine add_variable_field_indexes &
+ ( f_id, ivar )
+
+!===============================================================================
+! Module files
+!===============================================================================
+
+use paramx
+use dimens
+use entsor
+use numvar
+use field
+
+!===============================================================================
+
+implicit none
+
+procedure() :: fldvar_check_nvar, init_var_cal_opt
+procedure() :: csexit
+
+! Arguments
+
+integer, intent(in)  :: f_id
+integer, intent(out) :: ivar
+
+! Local variables
+
+integer  dim, ii
+
+integer, save :: keyvar = -1
+
+! Get field dimension
+
+call field_get_dim(f_id, dim)
+
+if (keyvar.lt.0) then
+  call field_get_key_id("variable_id", keyvar)
+endif
+
+ivar = nvar + 1
+nvar = nvar + dim
+
+! Check we have enough slots
+call fldvar_check_nvar
+
+do ii = 1, dim
+  ivarfl(ivar + ii - 1) = f_id
+enddo
+
+call field_set_key_int(f_id, keyvar, ivar)
+call init_var_cal_opt(f_id)
+
+return
+
+end subroutine add_variable_field_indexes
+
+!===============================================================================
+!
+!> \brief add field indexes associated with a new non-user solved
+!>        scalar variable, with default options
+!
+!-------------------------------------------------------------------------------
+! Arguments
+!______________________________________________________________________________.
+!  mode           name          role                                           !
+!______________________________________________________________________________!
+!> \param[in]  f_id           field id
+!> \param[out] iscal          scalar id for defined field
 !_______________________________________________________________________________
 
 subroutine add_model_field_indexes &
@@ -1188,6 +1258,42 @@ end subroutine fldvar_check_nscapp
 !===============================================================================
 ! C bindings (reverse)
 !===============================================================================
+
+!-------------------------------------------------------------------------------
+!> \brief add field indexes associated with a new non-user solved
+!>        variable, with default options
+!
+!> \param[in]  f_id    field id
+
+!> \result             variable number for defined field
+!-------------------------------------------------------------------------------
+
+function cs_add_variable_field_indexes(f_id) result(ivar) &
+  bind(C, name='cs_add_variable_field_indexes')
+
+  use, intrinsic :: iso_c_binding
+  use cs_c_bindings
+
+  implicit none
+
+  procedure() :: add_variable_field_indexes
+
+  ! Arguments
+
+  integer(c_int), value :: f_id
+  integer(c_int) :: ivar
+
+  ! Local variables
+
+  integer f_id0, ivar0
+
+  f_id0 = f_id
+
+  call add_variable_field_indexes(f_id0, ivar0)
+
+  ivar = ivar0
+
+end function cs_add_variable_field_indexes
 
 !-------------------------------------------------------------------------------
 !> \brief add field indexes associated with a new non-user solved

src/base/iniusi.f90
@@ -227,6 +227,12 @@ interface
     real(c_double), value :: value
   end subroutine cs_runaway_check_define_field_max
 
+  subroutine cs_user_radiative_transfer_parameters()  &
+       bind(C, name='cs_user_radiative_transfer_parameters')
+    use, intrinsic :: iso_c_binding
+    implicit none
+  end subroutine cs_user_radiative_transfer_parameters
+
   ! Interface to C function to initialize CDO model structures
 
   subroutine cs_f_domain_setup_init_model_context()  &
@@ -295,6 +301,14 @@ call csiphy()
 ! before property fields are created).
 call cs_gui_physical_constants
 
+! Activate radiative transfer model
+
+! This module must be activated early so as to reserve the associated
+! variables in some physical models.
+
+call cs_gui_radiative_transfer_parameters
+call cs_user_radiative_transfer_parameters ! deprecated, kept for compatibility
+
 ! Flow and other models selection through user C function
 call cs_user_model
 
@@ -336,16 +350,7 @@ call cs_velocity_pressure_set_solid
 
 call cfnmtd(ficfpp, len(ficfpp))
 
-! --- Activation du module transferts radiatifs
-
-!     Il est necessaire de connaitre l'activation du module transferts
-!     radiatifs tres tot de maniere a pouvoir reserver les variables
-!     necessaires dans certaines physiques particuliere
-
-!   - Interface code_saturne
-!     ======================
-
-call cs_gui_radiative_transfer_parameters
+call cs_rad_transfer_options
 
 ! Define fields for variables, check and build iscapp
 ! and computes the number of user scalars (nscaus)

src/rayt/cs_rad_transfer_fields.c
@@ -50,10 +50,14 @@
 #include "cs_mesh_location.h"
 
 #include "cs_field.h"
+#include "cs_field_default.h"
 #include "cs_field_pointer.h"
+#include "cs_gui_output.h"
+#include "cs_gui_radiative_transfer.h"
 
 #include "cs_parameters.h"
 #include "cs_post.h"
+#include "cs_prototypes.h"
 
 #include "cs_rad_transfer.h"
 
@@ -61,7 +65,7 @@
  *  Header for the current file
  *----------------------------------------------------------------------------*/
 
-#include "cs_rad_transfer_property_fields.h"
+#include "cs_rad_transfer_fields.h"
 #include "cs_physical_model.h"
 /*----------------------------------------------------------------------------*/
 
@@ -73,9 +77,10 @@ BEGIN_C_DECLS
 
 /*! \cond DOXYGEN_SHOULD_SKIP_THIS */
 
-/*  \file cs_rad_transfer_property_field.c    */
-/*  \brief Properties definitions for radiative model.     */
-
+/*  \file cs_rad_transfer_fields.c
+ *
+ * \brief Properties definitions for radiative model.
+*/
 
 /*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
 
@@ -85,21 +90,78 @@ BEGIN_C_DECLS
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief Create variable fields for radiative solver
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_rad_transfer_add_variable_fields(void)
+{
+  cs_rad_transfer_params_t *rt_params = cs_glob_rad_transfer_params;
+  if (rt_params->type <= CS_RAD_TRANSFER_NONE)
+    return;
+
+  /* radiance (per band) */
+  for (int gg_id = 0; gg_id < rt_params->nwsgg; gg_id ++) {
+    char f_name[80];
+    char f_label[80];
+    char suffix[16];
+
+    snprintf(suffix,  15, "_%02d", gg_id);
+    suffix[15] = '\0';
+
+    snprintf(f_name, 63, "radiance%s", suffix); f_name[63] = '\0';
+    snprintf(f_label, 63, "Radiance%s", suffix); f_label[63]  = '\0';
+    int f_id = cs_variable_field_create(f_name,
+                                        f_label,
+                                        CS_MESH_LOCATION_CELLS,
+                                        1);
+
+    cs_field_t *f = cs_field_by_id(f_id);
+    cs_add_variable_field_indexes(f->id);
+
+    cs_field_pointer_map_indexed(CS_ENUMF_(radiance), gg_id, f);
+
+    /* Equation parameters */
+    cs_equation_param_t *eqp = cs_field_get_equation_param(f);
+    eqp->verbosity =  rt_params->verbosity - 1;
+    eqp->iconv = 1; /* pure convection */
+    eqp->idiff  = 0; /* no diffusion */
+    eqp->istat = 0; /* no unsteady term */
+    eqp->idircl= 0; /* No reinforcement of diagonal */
+    eqp->idifft = 0;
+    eqp->isstpc =  0;
+    eqp->nswrsm =  1; /* One sweep is sufficient because of the upwind scheme */
+    eqp->imrgra =  cs_glob_space_disc->imrgra;
+    eqp->blencv =  0; /* Pure upwind...*/
+    eqp->epsrsm =  1e-08;  /* TODO: try with default (1e-07) */
+
+    if (rt_params->dispersion) {
+      eqp->idiff  =  1; /* Added face diffusion */
+      eqp->nswrgr = 20;
+      eqp->nswrsm =  2;
+    }
+  }
+
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief Create property fields for radiative solver
  */
 /*----------------------------------------------------------------------------*/
 
 void
-cs_rad_transfer_prp(void)
+cs_rad_transfer_add_property_fields(void)
 {
-  if (cs_glob_rad_transfer_params->type <= CS_RAD_TRANSFER_NONE)
+  cs_rad_transfer_params_t *rt_params = cs_glob_rad_transfer_params;
+  if (rt_params->type <= CS_RAD_TRANSFER_NONE)
     return;
 
   const int keylbl = cs_field_key_id("label");
   const int keyvis = cs_field_key_id("post_vis");
   const int keylog = cs_field_key_id("log");
 
-  cs_rad_transfer_params_t *rt_params = cs_glob_rad_transfer_params;
   cs_field_t *f = NULL;
 
   int field_type = CS_FIELD_INTENSIVE | CS_FIELD_PROPERTY;
@@ -243,52 +305,6 @@ cs_rad_transfer_prp(void)
       cs_field_set_key_str(f, keylbl, f_label);
 
       cs_field_pointer_map_indexed(CS_ENUMF_(rad_cak), irphas, f);
-    }
-  }
-
-  /* Add bands for Direct Solar, diFUse solar and InfraRed
-   * and for solar: make the distinction between UV-visible (absorbed by O3)
-   * and Solar IR (SIR) absobed by H2O
-   * if activated */
-  {
-    if (rt_params->atmo_model
-        != CS_RAD_ATMO_3D_NONE)
-      rt_params->nwsgg = 0;
-
-    /* Fields for atmospheric Direct Solar (DR) model
-     * (SIR only if SUV is not activated) */
-    if (rt_params->atmo_model
-        & CS_RAD_ATMO_3D_DIRECT_SOLAR) {
-      rt_params->atmo_dr_id = rt_params->nwsgg;
-      rt_params->nwsgg++;
-    }
-
-    /* Fields for atmospheric Direct Solar (DR) model
-     * (SUV band) */
-    if (rt_params->atmo_model
-        & CS_RAD_ATMO_3D_DIRECT_SOLAR_O3BAND) {
-      rt_params->atmo_dr_o3_id = rt_params->nwsgg;
-      rt_params->nwsgg++;
-    }
-
-    /* Fields for atmospheric diFfuse Solar (DF) model
-     * (SIR only if SUV is activated) */
-    if (rt_params->atmo_model & CS_RAD_ATMO_3D_DIFFUSE_SOLAR) {
-      rt_params->atmo_df_id = rt_params->nwsgg;
-      rt_params->nwsgg++;
-    }
-
-    /* Fields for atmospheric diFfuse Solar (DF) model
-     * (SUV band) */
-    if (rt_params->atmo_model & CS_RAD_ATMO_3D_DIFFUSE_SOLAR_O3BAND) {
-      rt_params->atmo_df_o3_id = rt_params->nwsgg;
-      rt_params->nwsgg++;
-    }
-
-    /* Fields for atmospheric infrared absorption model */
-    if (rt_params->atmo_model & CS_RAD_ATMO_3D_INFRARED) {
-      rt_params->atmo_ir_id = rt_params->nwsgg;
-      rt_params->nwsgg++;
     }
   }
 
@@ -547,6 +563,10 @@ cs_rad_transfer_prp(void)
     cs_field_set_key_str(f, keylbl, "Convective_exch_coef");
     cs_field_pointer_map(CS_ENUMF_(hconv), f);
   }
+
+  /* Postprocessing */
+
+  cs_gui_radiative_transfer_postprocess();
 }
 
 /*----------------------------------------------------------------------------*/

src/rayt/cs_rad_transfer_options.c
@@ -61,15 +61,13 @@
 #include "cs_time_control.h"
 #include "cs_timer.h"
 #include "cs_thermal_model.h"
-#include "cs_gui_output.h"
 
 #include "cs_combustion_model.h"
 #include "cs_physical_model.h"
 #include "cs_prototypes.h"
 
-#include "cs_gui_radiative_transfer.h"
 #include "cs_rad_transfer.h"
-#include "cs_rad_transfer_property_fields.h"
+#include "cs_rad_transfer_fields.h"
 #include "cs_rad_transfer_dir.h"
 
 /*----------------------------------------------------------------------------
@@ -119,9 +117,7 @@ cs_rad_transfer_options(void)
   /* -> For pulverized coal and fuel combustion:   */
 
   if (cs_glob_physical_model_flag[CS_COMBUSTION_COAL] >= 0)
-    rt_params->nrphas = 1 + cs_glob_combustion_model->coal->nclacp;
-  else
-    rt_params->nrphas = 1;
+    rt_params->nrphas += cs_glob_combustion_model->coal->nclacp;
 
   /* Default initializations
    * ----------------------- */
@@ -140,28 +136,6 @@ cs_rad_transfer_options(void)
                                     true,    /* at start */
                                     false);  /* at end */
 
-  /* ->  Quadrature number and Tn parameter */
-
-  rt_params->i_quadrature = 1;
-  rt_params->ndirec       = 3;
-
-  /* ->  Cell percentage for which it is admitted that optical length
-   *     is greater than one for P-1 model*/
-
-  rt_params->xnp1mx       = 10.0;
-
-  /* -> Wall temperature verbosity */
-
-  rt_params->iimpar       = 1;
-
-  /* -> Number of iterations used to solve the ETR.
-   *    Must at least be one to make the standard models work. */
-
-  /* User parameters  */
-
-  cs_gui_radiative_transfer_parameters();
-  cs_user_radiative_transfer_parameters();
-
   /* ->  Explicit radiative source term computation mode
    *            = 0 => Semi-analytic (mandatory if transparent)
    *            = 1 => Conservative
@@ -179,6 +153,52 @@ cs_rad_transfer_options(void)
 
   if (rt_params->imfsck == 1)
     rt_params->nwsgg = 7;
+
+  /* Add bands for Direct Solar, diFUse solar and InfraRed
+   * and for solar: make the distinction between UV-visible (absorbed by O3)
+   * and Solar IR (SIR) absobed by H2O
+   * if activated */
+  {
+    if (rt_params->atmo_model
+        != CS_RAD_ATMO_3D_NONE)
+      rt_params->nwsgg = 0;
+
+    /* Fields for atmospheric Direct Solar (DR) model
+     * (SIR only if SUV is not activated) */
+    if (rt_params->atmo_model
+        & CS_RAD_ATMO_3D_DIRECT_SOLAR) {
+      rt_params->atmo_dr_id = rt_params->nwsgg;
+      rt_params->nwsgg++;
+    }
+
+    /* Fields for atmospheric Direct Solar (DR) model
+     * (SUV band) */
+    if (rt_params->atmo_model
+        & CS_RAD_ATMO_3D_DIRECT_SOLAR_O3BAND) {
+      rt_params->atmo_dr_o3_id = rt_params->nwsgg;
+      rt_params->nwsgg++;
+    }
+
+    /* Fields for atmospheric diFfuse Solar (DF) model
+     * (SIR only if SUV is activated) */
+    if (rt_params->atmo_model & CS_RAD_ATMO_3D_DIFFUSE_SOLAR) {
+      rt_params->atmo_df_id = rt_params->nwsgg;
+      rt_params->nwsgg++;
+    }
+
+    /* Fields for atmospheric diFfuse Solar (DF) model
+     * (SUV band) */
+    if (rt_params->atmo_model & CS_RAD_ATMO_3D_DIFFUSE_SOLAR_O3BAND) {
+      rt_params->atmo_df_o3_id = rt_params->nwsgg;
+      rt_params->nwsgg++;
+    }
+
+    /* Fields for atmospheric infrared absorption model */
+    if (rt_params->atmo_model & CS_RAD_ATMO_3D_INFRARED) {
+      rt_params->atmo_ir_id = rt_params->nwsgg;
+      rt_params->nwsgg++;
+    }
+  }
 
   /* In case of imfsck == 2, spectral radiative properties depend on
    * user provided radiation library coupled with flamelet library.
@@ -216,9 +236,6 @@ cs_rad_transfer_options(void)
   /* Verifications */
 
   if (rt_params->type > CS_RAD_TRANSFER_NONE) {
-
-    /* Property fields */
-    cs_rad_transfer_prp();
 
     /* --> NFREQR */
 
@@ -275,9 +292,6 @@ cs_rad_transfer_options(void)
 
   cs_rad_transfer_dir();
 
-  /* Postprocessing */
-
-  cs_gui_radiative_transfer_postprocess();
 }
 
 /*----------------------------------------------------------------------------*/

src/base/cs_boundary_conditions_set_coeffs.c
@@ -156,6 +156,61 @@ cs_f_user_boundary_conditions_wrapper(const cs_lnum_t  itrifb[],
  *============================================================================*/
 
 /*----------------------------------------------------------------------------
+ * Compute boundary condition code for radiative transfer
+ *----------------------------------------------------------------------------*/
+
+static void
+_boundary_condition_rt_type(const cs_mesh_t             *m,
+                            const cs_mesh_quantities_t  *mq,
+                            const bool                  init,
+                            const cs_real_t             dt[],
+                            const int                   bc_type[])
+{
+  const cs_lnum_t n_b_faces = m->n_b_faces;
+  const cs_lnum_t *b_face_cells = m->b_face_cells;
+
+  const cs_real_t *surfbn = mq->b_face_surf;
+  const cs_real_3_t *surfbo = (const cs_real_3_t *)mq->b_face_normal;
+
+  cs_rad_transfer_params_t *rt_params = cs_glob_rad_transfer_params;
+
+  int nwsgg = rt_params->nwsgg;
+
+  /* Initialization
+   * -------------- */
+
+  /* TODO: make a rt_bc_type as ale_bc_type */
+
+  for (int gg_id = 0; gg_id < nwsgg; gg_id++) {
+
+    cs_real_t *radiance = CS_FI_(radiance, gg_id)->val;
+    cs_field_t *f_rad = CS_FI_(radiance, gg_id);
+
+    int *icodcl_rad = NULL;
+    cs_real_t *rcodcl1_rad = NULL;
+
+    if (f_rad->bc_coeffs != NULL) {
+      icodcl_rad = f_rad->bc_coeffs->icodcl;
+      rcodcl1_rad = f_rad->bc_coeffs->rcodcl1;
+    }
+
+
+# pragma omp parallel for  if (n_b_faces > CS_THR_MIN)
+    for (cs_lnum_t face_id = 0; face_id < n_b_faces; face_id++) {
+      if (rcodcl1_rad[face_id] > cs_math_infinite_r*0.5)
+        rcodcl1_rad[face_id] = 0;
+    }
+
+    /* Check the consistency of BC types
+     * --------------------------------- */
+
+    int ierror[1] = {0};
+
+  }
+
+}
+
+/*----------------------------------------------------------------------------
  * Compute boundary condition code for ALE
  *----------------------------------------------------------------------------*/
 
@@ -829,6 +884,13 @@ cs_boundary_conditions_set_coeffs(int        nvar,
                                    false,
                                    dt,
                                    bc_type);
+
+    if (cs_glob_rad_transfer_params->type != CS_RAD_TRANSFER_NONE)
+      _boundary_condition_rt_type(mesh,
+                                  fvq,
+                                  false,
+                                  dt,
+                                  bc_type);
 
     if (cs_turbomachinery_get_model() != CS_TURBOMACHINERY_NONE)
       cs_f_mmtycl(bc_type);
@@ -3737,6 +3799,13 @@ cs_boundary_conditions_set_coeffs_init(void)
                                  true,
                                  dt,
                                  bc_type);
+
+  if (cs_glob_rad_transfer_params->type != CS_RAD_TRANSFER_NONE)
+    _boundary_condition_rt_type(mesh,
+                                cs_glob_mesh_quantities,
+                                true,
+                                dt,
+                                bc_type);
 
   if (cs_turbomachinery_get_model() != CS_TURBOMACHINERY_NONE)
     cs_f_mmtycl(bc_type);

src/base/cs_field_pointer.h
@@ -170,6 +170,7 @@ typedef enum {
 
   CS_ENUMF_(rad_energy),   /*!< Radiative energy */
   CS_ENUMF_(rad_q),        /*!< Radiative flux */
+  CS_ENUMF_(radiance),     /*!< (spectral) radiance field */
 
   CS_ENUMF_(rad_est),      /*!< Radiative flux explicit source term */
   CS_ENUMF_(rad_ist),      /*!< Radiative flux implicit source term */

src/base/cs_parameters.c
@@ -59,6 +59,7 @@
 #include "cs_physical_model.h"
 #include "cs_restart.h"
 #include "cs_restart_default.h"
+#include "cs_rad_transfer_fields.h"
 #include "cs_turbulence_model.h"
 #include "cs_time_moment.h"
 #include "cs_thermal_model.h"
@@ -1518,6 +1519,9 @@ cs_parameters_define_auxiliary_fields(void)
     cs_field_set_key_int(fld, cs_field_key_id("log"), 1);
     cs_field_set_key_int(fld, cs_field_key_id("post_vis"), post_flag);
   }
+
+  /* Property fields relative to radiative transfer */
+  cs_rad_transfer_add_property_fields();
 }
 
 /*----------------------------------------------------------------------------*/

src/base/cs_prototypes.h
@@ -119,6 +119,20 @@ extern void CS_PROCF (initi1, INITI1)
  *   f_id <--   field id
  *
  * returns:
+ *   variable number for defined field
+ *----------------------------------------------------------------------------*/
+
+int
+cs_add_variable_field_indexes(int  f_id);
+
+/*----------------------------------------------------------------------------
+ * Add field indexes associated with a new non-user solved variable,
+ * with default options
+ *
+ * parameters:
+ *   f_id <--   field id
+ *
+ * returns:
  *   scalar number for defined field
  *----------------------------------------------------------------------------*/
 
@@ -947,13 +961,6 @@ cs_user_matrix_tuning(void);
 void
 cs_user_parameters(cs_domain_t *domain);
 
-/*----------------------------------------------------------------------------
- * User function for input of radiative transfer module options.
- *----------------------------------------------------------------------------*/
-
-void
-cs_user_radiative_transfer_parameters(void);
-
 /*-----------------------------------------------------------------------------
  * User subroutine for input of radiative transfer boundary conditions
  *----------------------------------------------------------------------------*/
@@ -1061,10 +1068,6 @@ cs_user_rad_transfer_absorption(const int  bc_type[],
 
 void
 cs_user_rad_transfer_net_flux(const int        itypfb[],
-                              const cs_real_t  coefap[],
-                              const cs_real_t  coefbp[],
-                              const cs_real_t  cofafp[],
-                              const cs_real_t  cofbfp[],
                               const cs_real_t  twall[],
                               const cs_real_t  qincid[],
                               const cs_real_t  xlam[],

commit f7f4ddb37ed39a457509b4c8729323bd70a1e575

Fix bug introduced in commit c1b752e549e3 and 24e1aa51f8b.
For atmospheric flows with multi bands, the number of bands must be
computed before creating fields (radiance).

For coal combustion, number of coal must be known before.

src/base/fldvar.f90
@@ -320,12 +320,7 @@ endif
 
 nscaus = cs_parameters_n_added_variables()
 
-! ---> Lecture donnees thermochimie
-
-call pplecd
-
-! ---> Definition des variables
-
+! Specific physics variables
 call ppvarp
 
 ! Thermal model with no specific physics

src/base/iniusi.f90
@@ -277,6 +277,9 @@ call cs_wall_condensation_set_onoff_state(icondb, icondv)
 ! ALE parameters
 call uialin (nalinf, nalimx, epalim)
 
+! Read thermochemical data for specific physics
+call pplecd
+
 ! thermal model
 call cs_gui_thermal_model
 
@@ -350,6 +353,8 @@ call cs_velocity_pressure_set_solid
 
 call cfnmtd(ficfpp, len(ficfpp))
 
+call cs_rad_transfer_options
+
 ! Define fields for variables, check and build iscapp
 ! and computes the number of user scalars (nscaus)
 if (icdo.lt.2) then
@@ -358,8 +363,6 @@ if (icdo.lt.2) then
   ! Activate the pressure correction model only if CDO mode is not stand-alone
   call cs_pressure_correction_model_activate
 endif
-
-call cs_rad_transfer_options
 
 if (iale.ge.1) then
   call cs_gui_ale_diffusion_type

src/rayt/cs_rad_transfer_fields.c
@@ -56,8 +56,10 @@
 #include "cs_gui_radiative_transfer.h"
 
 #include "cs_parameters.h"
+#include "cs_parameters_check.h"
 #include "cs_post.h"
 #include "cs_prototypes.h"
+#include "cs_thermal_model.h"
 
 #include "cs_rad_transfer.h"
 
@@ -100,6 +102,18 @@ cs_rad_transfer_add_variable_fields(void)
   cs_rad_transfer_params_t *rt_params = cs_glob_rad_transfer_params;
   if (rt_params->type <= CS_RAD_TRANSFER_NONE)
     return;
+
+  /* Coherency check with thermal model */
+  if (   rt_params->type == CS_RAD_TRANSFER_DOM
+      || rt_params->type == CS_RAD_TRANSFER_P1)
+    cs_parameters_is_in_range_int
+      (CS_ABORT_DELAYED,
+       _("in Radiative module"),
+       _("Thermal model option (cs_glob_thermal model->itherm)"),
+       cs_glob_thermal_model->itherm,
+       CS_THERMAL_MODEL_TEMPERATURE, CS_THERMAL_MODEL_TOTAL_ENERGY);
+
+  cs_parameters_error_barrier();
 
   /* radiance (per band) */
   for (int gg_id = 0; gg_id < rt_params->nwsgg; gg_id ++) {

src/rayt/cs_rad_transfer_options.c
@@ -222,17 +222,6 @@ cs_rad_transfer_options(void)
                                 cs_glob_rad_transfer_params->imodak,
                                 0, 3);
 
-  if (   rt_params->type == CS_RAD_TRANSFER_DOM
-      || rt_params->type == CS_RAD_TRANSFER_P1)
-    cs_parameters_is_in_range_int
-      (CS_ABORT_DELAYED,
-       _("in Radiative module"),
-       _("Thermal model option (cs_glob_thermal model->itherm)"),
-       cs_glob_thermal_model->itherm,
-       CS_THERMAL_MODEL_TEMPERATURE, CS_THERMAL_MODEL_TOTAL_ENERGY);
-
-  cs_parameters_error_barrier();
-
   /* Verifications */
 
   if (rt_params->type > CS_RAD_TRANSFER_NONE) {