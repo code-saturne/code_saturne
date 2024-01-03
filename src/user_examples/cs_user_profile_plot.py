cfc3d8f9752b4bf8f75282ec67b957a46c435194
Yvan Fournier <yvan.fournier@noreply.edf.fr>
2024-01-03 20:10:35 +0100

    Add missing copyright header.

--- src/user_examples/cs_user_profile_plot.py
+++ src/user_examples/cs_user_profile_plot.py
@@ -1,11 +1,33 @@
 #!/bin/python3
 
+#-------------------------------------------------------------------------------
+
+# This file is part of code_saturne, a general-purpose CFD tool.
+#
+# Copyright (C) 1998-2024 EDF S.A.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+# Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+#-------------------------------------------------------------------------------
+
 import matplotlib.pyplot as plt
 import os
 import csv
 import re
 
-## Define a python class equivalent for its properties to the structure defined in cs_user_profile.h but storing all time step dumped
+## class matching that in cs_user_profile.h but aggregating all dumped time steps
 
 class Profile():
 
