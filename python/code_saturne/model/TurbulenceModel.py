code-saturne/code_saturne

commit 1c2ac278eb482d28833dda20389776347aeb3776

GUI: better alignement of convective scheme options with numerical options.
- A compatibility mode for the previous 'upwind' option, which was redundant
  with blencv = 0, is added directly in the reader, so that XML compatibility
  does not need to be involved (though it would probably not make new
  files incompatible with older ones, since 'centered' + 'blencv = 0'
  should be equivalent.

- To check whether a given field can use a beta limiter, hidden info
  on whether some fields are convected or not is stored using a hidden
  attribute (starting with "_"). This allows more systematic tests,
  as in the C code.

python/code_saturne/model/TurbulenceModel.py
@@ -293,6 +293,9 @@ class TurbulenceModel(Variables, Model):
                     self.setNewVariable(self.node_turb, 'rij', label='Rij', dim='6')
                 else:
                     self.setNewVariable(self.node_turb, v, label=v)
+                if v == 'alpha':
+                    v_n = node.xmlGetNode('variable', name=v)
+                    v_n['_convect'] = 'no'
             self.setNewProperty(self.node_turb, 'turbulent_viscosity')
             self.__updateInletsForTurbulence()
             self.__removeVariablesAndProperties(lst, 'smagorinsky_constant^2')

python/code_saturne/gui/case/NumericalParamEquationForm.ui
@@ -111,7 +111,7 @@
           </size>
          </property>
          <property name="toolTip">
-          <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Define the scheme options for each variable:&lt;br/&gt;- Name of the variable,&lt;br/&gt;- Scheme: choose between two second-order convective schemes (&lt;/span&gt;&lt;span style=&quot; font-size:9pt; font-weight:600;&quot;&gt;Centered&lt;/span&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt; or &lt;/span&gt;&lt;span style=&quot; font-size:9pt; font-weight:600;&quot;&gt;SOLU&lt;/span&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;) and one first-order convective scheme (&lt;/span&gt;&lt;span style=&quot; font-size:9pt; font-weight:600;&quot;&gt;Upwind&lt;/span&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;).&lt;br/&gt;- Blending factor: indicate the proportion of second-order convective scheme (0 corresponds to an upwind convective scheme). &lt;br/&gt;- Slope test: indicate whether a slope test should be used to switch from a second-order to an upwind convective scheme under certain conditions, to ensure stability. The use of the slope test stabilizes the calculation but may bring the space order to decrease quickly.&lt;br/&gt;- Flux reconstruction: indicate whether the convective and diffusive fluxes at the faces should be reconstructed. It is only useful for non-orthogonal meshes. Deactivating the reconstruction of the fluxes can have a stabilizing effect on the calculation. It is sometimes useful with the k−ε model, if the mesh is strongly non-orthogonal in the near-wall region, where the gradients of k and ε are strong.&lt;br/&gt;- RHS Sweep Reconstruction: indicate the number of iterations for the reconstruction of the right-hand sides of the equation.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+          <string/>
          </property>
         </widget>
        </item>

python/code_saturne/model/NumericalParamEquationModel.py
@@ -107,7 +107,12 @@ class NumericalParamEquationModel(Model):
         self.default['solver_precision_pressure'] = 1e-8
         if NumericalParamGlobalModel(self.case).getTimeSchemeOrder() == 2:
             self.default['solver_precision_pressure'] = 1e-5
-        self.default['slope_test'] = 'on'
+        if name == 'void_fraction':
+            self.default['slope_test'] = 'beta_limiter'
+            self.default['nvd_limiter'] = 'cicsam'
+        else:
+            self.default['slope_test'] = 'on'
+            self.default['nvd_limiter'] = 'gamma'
         self.default['flux_reconstruction'] = 'on'
 
         self.default['solver_choice'] = 'automatic'
@@ -122,7 +127,6 @@ class NumericalParamEquationModel(Model):
 
         from code_saturne.model.CompressibleModel import CompressibleModel
         if CompressibleModel(self.case).getCompressibleModel() != 'off':
-            self.default['order_scheme'] = 'upwind'
             self.default['blending_factor'] = 0.
         del CompressibleModel
 
@@ -363,17 +367,25 @@ class NumericalParamEquationModel(Model):
 
     @Variables.noUndo
     def getSchemeList(self):
-        """ Return the variables name list for scheme parameters """
+        """
+        Return the variables name and category list for scheme parameters.
+        category values are 0 (not convected), 1 (scalar), 2 (non-scalar),
+        and 3 (VoF)
+        """
         lst = []
-        if self.darcy == "off":
-            for node in self._getSchemeNodesList():
-                for n in node:
-                    lst.append(n['name'])
-        else:
-            for node in self._getSchemeNodesList():
-                for n in node:
-                    if n['name'] != "velocity":
-                        lst.append(n['name'])
+        for node in self._getSchemeNodesList():
+            for n in node:
+                category = 1
+                name = n['name']
+                dim = n['dimension']
+                if dim:
+                    if int(dim) > 1:
+                        category = 2
+                if n['_convect'] == 'off':
+                    category = 0
+                elif name == 'void_fraction':
+                    category = 3
+                lst.append((n['name'], category))
         return lst
 
 
@@ -417,12 +429,21 @@ class NumericalParamEquationModel(Model):
     def getScheme(self, name):
         """ Return value of order scheme for variable labelled name """
         node = self._getSchemeNameNode(name)
-        if self._isPressure(node):
+        if node['_convect'] == 'off':
             return None
         value = self._defaultValues(name)['order_scheme']
         n = node.xmlGetNode('order_scheme')
         if n:
             value = n['choice']
+
+            # Compatibility with older definitions
+            # (which were not consistent with the internal code_saturne
+            # model, as upwind could be defined in 2 different ways).
+            if value == 'upwind':
+                value = 'centered'
+                node.xmlSetData('blending_factor', 0.0)
+                n['choice'] = 'centered'
+
         return value
 
 
@@ -447,7 +468,32 @@ class NumericalParamEquationModel(Model):
         value = self._defaultValues(name)['slope_test']
         n = node.xmlGetNode('slope_test')
         if n:
-            value = n['status']
+            attributes = n.xmlGetAttributeDictionary()
+            if 'status' in attributes:
+                value = n['status']
+            elif 'choice' in attributes:
+                value = n['choice']
+        return value
+
+
+    @Variables.noUndo
+    def getNVDLimiter(self, name):
+        """ Return value of NVD limiter for variable labelled name """
+        node = self._getSchemeNameNode(name)
+        if node['_convect'] == 'off':
+            return None
+
+        sv = self._defaultValues(name)['order_scheme']
+        n = node.xmlGetNode('order_scheme')
+        if n:
+            sv = n['choice']
+        if sv != 'nvd_tvd':
+            return None
+
+        value = self._defaultValues(name)['nvd_limiter']
+        n = node.xmlGetNode('nvd_limiter')
+        if n:
+                value = n['choice']
         return value
 
 
@@ -482,18 +528,11 @@ class NumericalParamEquationModel(Model):
             return
         self.isGreaterOrEqual(value, 0.)
         self.isLowerOrEqual(value, 1.)
-        scheme = self.getScheme(name)
-        if scheme == self._defaultValues(name)['order_scheme']:
-            if scheme == 'upwind':
-                node.xmlRemoveChild('blending_factor')
-            else:
-                node.xmlSetData('blending_factor', value)
+        if self.getScheme(name) == 'automatic' \
+           and value == self._defaultValues(name)['blending_factor']:
+            node.xmlRemoveChild('blending_factor')
         else:
             node.xmlSetData('blending_factor', value)
-#            if value != self._defaultValues(name)['blending_factor']:
-#                node.xmlSetData('blending_factor', value)
-#            else:
-#                node.xmlRemoveChild('blending_factor')
 
 
     @Variables.undoGlobal
@@ -502,29 +541,56 @@ class NumericalParamEquationModel(Model):
         Put value of order scheme for variable or scalar labelled name
         only if it 's different of default value
         """
-        self.isInList(value, ('automatic', 'upwind', 'centered', 'solu'))
+        self.isInList(value, ('automatic', 'centered', 'solu',
+                              'solu_upwind_gradient', 'blending',
+                              'nvd_tvd'))
         node = self._getSchemeNameNode(name)
         if value == self._defaultValues(name)['order_scheme']:
             node.xmlRemoveChild('order_scheme')
-            if self.getBlendingFactor(name) == self._defaultValues(name)['blending_factor']\
-                or value == 'centered' and self.getBlendingFactor(name) == 0. \
-                or value == 'upwind' and self.getBlendingFactor(name) != 0.:
+            blencv = node.xmlGetDouble('blending_factor')
+            if blencv is not None:
+                if blencv == self._defaultValues(name)['blending_factor']:
                     node.xmlRemoveChild('blending_factor')
         else:
             n = node.xmlInitNode('order_scheme')
             n['choice'] = value
 
+        if value != 'nvd_tvd':   # precaution
+            node.xmlRemoveChild('nvd_limiter')
+
 
     @Variables.undoLocal
-    def setSlopeTest(self, name, status):
+    def setSlopeTest(self, name, value):
         """ Put status of slope test for variable labelled name """
-        self.isOnOff(status)
+        self.isInList(value, ('on', 'off', 'beta_limiter'))
         node = self._getSchemeNameNode(name)
-        if status == self._defaultValues(name)['slope_test']:
+        if value == self._defaultValues(name)['slope_test']:
             node.xmlRemoveChild('slope_test')
         else:
             n = node.xmlInitNode('slope_test')
-            n['status'] = status
+            if value in ('on', 'off'):
+                n['status'] = value
+                if 'choice' in n.xmlGetAttributeDictionary():
+                    del n['choice']
+            elif value == 'beta_limiter':
+                n['choice'] = value
+                if 'status' in n.xmlGetAttributeDictionary():
+                    del n['status']
+
+
+    @Variables.undoLocal
+    def setNVDLimiter(self, name, value):
+        """ Put status of NVD limiter for variable labelled name """
+        if value:
+            self.isInList(value, ('gamma', 'smart', 'cubista', 'superbee',
+                                  'muscl', 'minmod', 'clam', 'stoic',
+                                  'osher', 'waseb', 'hric', 'cicsam', 'stacs'))
+        node = self._getSchemeNameNode(name)
+        if not value or value == self._defaultValues(name)['nvd_limiter']:
+            node.xmlRemoveChild('nvd_limiter')
+        else:
+            n = node.xmlInitNode('nvd_limiter')
+            n['choice'] = value
 
 
     @Variables.undoLocal

python/code_saturne/model/XMLinitialize.py
@@ -210,6 +210,8 @@ class XMLinit(BaseXmlInit):
                 self.setNewVariable(node, 'hydraulic_head')
             else:
                 self.setNewVariable(node, 'pressure')
+                n = node.xmlGetNode('variable', name='pressure')
+                n['_convect'] = 'no'
             self.setNewVariable(node, 'velocity', dim = '3')
             self.setNewProperty(node, 'total_pressure')
 

src/alge/cs_convection_diffusion.c
@@ -52,6 +52,7 @@
 #include "cs_blas.h"
 #include "cs_bad_cells_regularisation.h"
 #include "cs_boundary_conditions.h"
+#include "cs_equation_param.h"
 #include "cs_halo.h"
 #include "cs_halo_perio.h"
 #include "cs_log.h"
@@ -59,6 +60,7 @@
 #include "cs_math.h"
 #include "cs_mesh.h"
 #include "cs_field.h"
+#include "cs_field_default.h"
 #include "cs_field_operator.h"
 #include "cs_field_pointer.h"
 #include "cs_gradient.h"
@@ -137,7 +139,8 @@ _calc_heq(cs_real_t h1,
  * beta limiter (ensuring preservation of a given min/max pair of values).
  *
  * parameters:
- *   f_id        <-- field id
+ *   f           <-- pointer to field
+ *   eqp         <-- associated equation parameters
  *   inc         <-- 0 if an increment, 1 otherwise
  *   denom_inf   --> computed denominator for the lower bound
  *   denom_sup   --> computed denominator for the upper bound
@@ -147,10 +150,11 @@ _calc_heq(cs_real_t h1,
  *----------------------------------------------------------------------------*/
 
 static void
-_beta_limiter_denom(const int              f_id,
-                    const int              inc,
-                    cs_real_t    *restrict denom_inf,
-                    cs_real_t    *restrict denom_sup)
+_beta_limiter_denom(cs_field_t                 *f,
+                    const cs_equation_param_t  *eqp,
+                    const int                   inc,
+                    cs_real_t         *restrict denom_inf,
+                    cs_real_t         *restrict denom_sup)
 {
   const cs_mesh_t  *m = cs_glob_mesh;
   cs_mesh_quantities_t  *fvq = cs_glob_mesh_quantities;
@@ -174,25 +178,17 @@ _beta_limiter_denom(const int              f_id,
   const cs_real_3_t *restrict djjpf
     = (const cs_real_3_t *restrict)fvq->djjpf;
 
-  /* Get option from the field */
-  cs_field_t *f = cs_field_by_id(f_id);
-
   const cs_real_t *restrict pvar  = f->val;
   const cs_real_t *restrict pvara = f->val_pre;
 
   const cs_real_t *restrict coefap = f->bc_coeffs->a;
   const cs_real_t *restrict coefbp = f->bc_coeffs->b;
 
-  int key_cal_opt_id = cs_field_key_id("var_cal_opt");
-  cs_var_cal_opt_t var_cal_opt;
-
-  cs_field_get_key_struct(f, key_cal_opt_id, &var_cal_opt);
-
-  const int ischcp = var_cal_opt.ischcv;
-  const int ircflp = var_cal_opt.ircflu;
-  const int imrgra = var_cal_opt.imrgra;
-  const cs_real_t thetap = var_cal_opt.thetav;
-  const cs_real_t blencp = var_cal_opt.blencv;
+  const int ischcp = eqp->ischcv;
+  const int ircflp = eqp->ircflu;
+  const int imrgra = eqp->imrgra;
+  const cs_real_t thetap = eqp->thetav;
+  const cs_real_t blencp = eqp->blencv;
 
   const int kimasf = cs_field_key_id("inner_mass_flux_id");
   const int kbmasf = cs_field_key_id("boundary_mass_flux_id");
@@ -226,7 +222,7 @@ _beta_limiter_denom(const int              f_id,
     /* local extrema computation */
     BFT_MALLOC(local_max, n_cells_ext, cs_real_t);
     BFT_MALLOC(local_min, n_cells_ext, cs_real_t);
-    cs_field_local_extrema_scalar(f_id,
+    cs_field_local_extrema_scalar(f->id,
                                   halo_type,
                                   local_max,
                                   local_min);
@@ -234,7 +230,7 @@ _beta_limiter_denom(const int              f_id,
     /* cell Courant number computation */
     if (limiter_choice >= CS_NVD_VOF_HRIC) {
       BFT_MALLOC(courant, n_cells_ext, cs_real_t);
-      cs_cell_courant_number(f_id, courant);
+      cs_cell_courant_number(f->id, courant);
     }
   }
 
@@ -251,6 +247,8 @@ _beta_limiter_denom(const int              f_id,
   BFT_MALLOC(grdpa, n_cells_ext, cs_real_3_t);
   BFT_MALLOC(grdpaa, n_cells_ext, cs_real_3_t);
 
+  /* TODO check if we can remove this initialization, as it seems
+     redundant with assignment in following calls */
 # pragma omp parallel for
   for (cs_lnum_t cell_id = 0; cell_id < n_cells_ext; cell_id++) {
     grdpa[cell_id][0] = 0.;
@@ -263,7 +261,7 @@ _beta_limiter_denom(const int              f_id,
   }
 
   /* legacy SOLU Scheme or centered scheme */
-  if (ischcp == 0 || ischcp == 1 || ischcp == 3 || ischcp == 4) {
+  if (ischcp != 2) {
 
     cs_field_gradient_scalar(f,
                              false, /* use_previous_t */
@@ -279,7 +277,7 @@ _beta_limiter_denom(const int              f_id,
   /* pure SOLU scheme (upwind gradient reconstruction) */
   else if (ischcp == 2) {
 
-    cs_upwind_gradient(f_id,
+    cs_upwind_gradient(f->id,
                        inc,
                        halo_type,
                        coefap,
@@ -289,7 +287,7 @@ _beta_limiter_denom(const int              f_id,
                        pvar,
                        grdpa);
 
-    cs_upwind_gradient(f_id,
+    cs_upwind_gradient(f->id,
                        inc,
                        halo_type,
                        coefap,
@@ -443,15 +441,15 @@ _beta_limiter_denom(const int              f_id,
         cs_real_t flui = 0.5*(i_massflux[face_id] +fabs(i_massflux[face_id]));
         cs_real_t fluj = 0.5*(i_massflux[face_id] -fabs(i_massflux[face_id]));
 
-        cs_real_t flux =     thetap  * ( (pif  - pi )*flui
-                                       + (pjf  - pj )*fluj)
-                       + (1.-thetap) * ( (pifa - pia)*flui
-                                       + (pjfa - pja)*fluj);
+        cs_real_t flux =       thetap  * (  (pif  - pi )*flui
+                                          + (pjf  - pj )*fluj)
+                         + (1.-thetap) * (  (pifa - pia)*flui
+                                          + (pjfa - pja)*fluj);
 
         /* blending to prevent lower bound violation
           We need to take the positive part*/
-        cs_real_t partii = 0.5*(flux + CS_ABS(flux));
-        cs_real_t partjj = 0.5*(flux - CS_ABS(flux));
+        cs_real_t partii = 0.5*(flux + cs_math_fabs(flux));
+        cs_real_t partjj = 0.5*(flux - cs_math_fabs(flux));
 
         denom_inf[ii] += partii;
         denom_inf[jj] -= partjj;
@@ -473,14 +471,14 @@ _beta_limiter_denom(const int              f_id,
   BFT_FREE(courant);
   BFT_FREE(grdpa);
   BFT_FREE(grdpaa);
-
 }
 
 /*----------------------------------------------------------------------------
  * Return the diagonal part of the numerator to build the Min/max limiter.
  *
  * parameters:
- *   f_id        <-- field id (or -1)
+ *   f           <-- pointer to field
+ *   eqp         <-- associated equation parameters
  *   rovsdt      <-- rho * volume / dt
  *   num_inf     --> computed numerator for the lower bound
  *   num_sup     --> computed numerator for the upper bound
@@ -488,11 +486,12 @@ _beta_limiter_denom(const int              f_id,
  *----------------------------------------------------------------------------*/
 
 static void
-_beta_limiter_num(const int           f_id,
-                  const int           inc,
-                  const cs_real_t     rovsdt[],
-                  cs_real_t          *num_inf,
-                  cs_real_t          *num_sup)
+_beta_limiter_num(cs_field_t                 *f,
+                  const cs_equation_param_t  *eqp,
+                  const int                   inc,
+                  const cs_real_t             rovsdt[],
+                  cs_real_t                  *num_inf,
+                  cs_real_t                  *num_sup)
 {
   const cs_mesh_t  *m = cs_glob_mesh;
 
@@ -510,7 +509,6 @@ _beta_limiter_num(const int           f_id,
     = (const cs_lnum_t *restrict)m->b_face_cells;
 
   /* Get option from the field */
-  cs_field_t *f = cs_field_by_id(f_id);
 
   const cs_real_t *restrict pvara = f->val_pre;
 
@@ -523,12 +521,7 @@ _beta_limiter_num(const int           f_id,
   cs_real_t scalar_max = cs_field_get_key_double(f, key_scamax_id);
   cs_real_t scalar_min = cs_field_get_key_double(f, key_scamin_id);
 
-  int key_cal_opt_id = cs_field_key_id("var_cal_opt");
-  cs_var_cal_opt_t var_cal_opt;
-
-  cs_field_get_key_struct(f, key_cal_opt_id, &var_cal_opt);
-
-  const cs_real_t thetex =  1.-var_cal_opt.thetav;
+  const cs_real_t thetex =  1. - eqp->thetav;
 
   const int kimasf = cs_field_key_id("inner_mass_flux_id");
   const int kbmasf = cs_field_key_id("boundary_mass_flux_id");
@@ -801,10 +794,10 @@ cs_cell_courant_number(const int   f_id,
         cs_lnum_t ii = i_face_cells[face_id][0];
         cs_lnum_t jj = i_face_cells[face_id][1];
 
-        cs_real_t cnt = CS_ABS(i_massflux[face_id])*dt[ii]/vol[ii];
+        cs_real_t cnt = cs_math_fabs(i_massflux[face_id])*dt[ii]/vol[ii];
         courant[ii] = cs_math_fmax(courant[ii], cnt); //FIXME may contain rho
 
-        cnt = CS_ABS(i_massflux[face_id])*dt[jj]/vol[jj];
+        cnt = cs_math_fabs(i_massflux[face_id])*dt[jj]/vol[jj];
         courant[jj] = cs_math_fmax(courant[jj], cnt);
       }
     }
@@ -819,7 +812,7 @@ cs_cell_courant_number(const int   f_id,
          face_id++) {
       cs_lnum_t ii = b_face_cells[face_id];
 
-      cs_real_t cnt = CS_ABS(b_massflux[face_id])*dt[ii]/vol[ii];
+      cs_real_t cnt = cs_math_fabs(b_massflux[face_id])*dt[ii]/vol[ii];
       courant[ii] = cs_math_fmax(courant[ii], cnt);
     }
   }
@@ -1455,21 +1448,18 @@ cs_beta_limiter_building(int              f_id,
                          int              inc,
                          const cs_real_t  rovsdt[])
 {
+  /* Get options from the field */
+
+  cs_field_t *f = cs_field_by_id(f_id);
+  const cs_equation_param_t *eqp = cs_field_get_equation_param(f);
+
+  if (eqp->isstpc != 2)
+    return;
+
   const cs_mesh_t *m = cs_glob_mesh;
   const cs_halo_t *halo = m->halo;
   const cs_lnum_t n_cells = m->n_cells;
   const cs_lnum_t n_cells_ext = m->n_cells_with_ghosts;
-
-  /* Get options from the field */
-
-  cs_field_t *f = cs_field_by_id(f_id);
-  int key_cal_opt_id = cs_field_key_id("var_cal_opt");
-  cs_var_cal_opt_t var_cal_opt;
-
-  cs_field_get_key_struct(f, key_cal_opt_id, &var_cal_opt);
-
-  if (var_cal_opt.isstpc != 2)
-    return;
 
   cs_real_t* cpro_beta = cs_field_by_id(
       cs_field_get_key_int(f, cs_field_key_id("convection_limiter_id")))->val;
@@ -1486,14 +1476,16 @@ cs_beta_limiter_building(int              f_id,
 
   /* computation of the denominator for the inferior and upper bound */
 
-  _beta_limiter_denom(f_id,
+  _beta_limiter_denom(f,
+                      eqp,
                       inc,
                       denom_inf,
                       denom_sup);
 
   /* computation of the numerator for the inferior and upper bound */
 
-  _beta_limiter_num(f_id,
+  _beta_limiter_num(f,
+                    eqp,
                     inc,
                     rovsdt,
                     num_inf,
@@ -1507,7 +1499,7 @@ cs_beta_limiter_building(int              f_id,
     if (denom_inf[ii] <= num_inf[ii]) {
       beta_inf = 1.;
     }
-    else if (denom_inf[ii] <= CS_ABS(num_inf[ii])) {
+    else if (denom_inf[ii] <= cs_math_fabs(num_inf[ii])) {
       beta_inf = -1.;
     }
     else {
@@ -1520,7 +1512,7 @@ cs_beta_limiter_building(int              f_id,
     if (denom_sup[ii] <= num_sup[ii]) {
       beta_sup = 1.;
     }
-    else if (denom_sup[ii] <= CS_ABS(num_sup[ii])) {
+    else if (denom_sup[ii] <= cs_math_fabs(num_sup[ii])) {
       beta_sup = -1.;
     }
     else {

src/base/cs_c_bindings.f90
@@ -146,15 +146,6 @@ module cs_c_bindings
 
   interface
 
-    subroutine beta_limiter_building(f_id, inc, rovsdt) &
-    bind(C, name='cs_beta_limiter_building')
-      use, intrinsic :: iso_c_binding
-      implicit none
-      integer(c_int), value :: f_id
-      integer(c_int), value :: inc
-      real(c_double), dimension(*) , intent(in) :: rovsdt
-    end subroutine beta_limiter_building
-
     !---------------------------------------------------------------------------
 
     !> \brief Set mapped boundary conditions for a given field and mapping

src/base/fldini.f90
@@ -82,7 +82,7 @@ integer          turb_flux_model, turb_flux_model_type
 integer          itycat, ityloc, idim1, idim3, idim6
 logical          iprev, inoprv
 integer          f_id, kscavr, f_type
-integer          iopchr, ilog
+integer          iopchr, ilog, ischcp
 integer          iscdri, icla, iclap
 integer          keyccl, keydri
 integer          idfm, iggafm, nfld
@@ -189,6 +189,16 @@ else if (iphydr.eq.2) then
   call field_find_or_create('hydrostatic_pressure_prd', &
                             itycat, ityloc, idim1, f_id)
 endif
+
+! Hybrid blending field
+
+call field_get_key_struct_var_cal_opt(ivarfl(iu), vcopt)
+ischcp = vcopt%ischcv
+if (ischcp.eq.3) then
+  itycat = FIELD_INTENSIVE + FIELD_PROPERTY
+  ityloc = 1 ! cells
+  call field_find_or_create('hybrid_blend', itycat, ityloc, idim1, f_id)
+end if
 
 ! friction velocity at the wall, in the case of a LES calculation
 ! with van Driest-wall damping (delayed here rather than placed in

commit dd6544686735d757676f9442a2b5bb2c2e23f30a

GUI: fix crash when selecting EBRSM model introduced in 1c2ac278 (2023-12-07).

python/code_saturne/model/TurbulenceModel.py
@@ -294,7 +294,7 @@ class TurbulenceModel(Variables, Model):
                 else:
                     self.setNewVariable(self.node_turb, v, label=v)
                 if v == 'alpha':
-                    v_n = node.xmlGetNode('variable', name=v)
+                    v_n = self.node_turb.xmlGetNode('variable', name=v)
                     v_n['_convect'] = 'no'
             self.setNewProperty(self.node_turb, 'turbulent_viscosity')
             self.__updateInletsForTurbulence()