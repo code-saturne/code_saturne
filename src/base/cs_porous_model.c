code-saturne/code_saturne

commit ae32ef6b2c8700aeecf05d383fdd0fda3cf4be6f

[IBM] Penalize ibm cells with small porosity, fix bug for turbulence when c_w_dist_inv=0

src/base/cs_porous_model.c
@@ -300,6 +300,22 @@ cs_f_mesh_quantities_solid_compute(void)
   cs_mesh_quantities_t *mq = cs_glob_mesh_quantities;
   cs_porosity_from_scan_opt_t *poro_from_scan = cs_glob_porosity_from_scan_opt;
   cs_real_t poro_threshold = poro_from_scan->porosity_threshold;
+  cs_real_t threshold = poro_from_scan->threshold;
+  
+  cs_real_3_t *restrict c_w_face_normal
+    = (cs_real_3_t *restrict)mq->c_w_face_normal;
+  cs_real_3_t *restrict i_f_face_normal
+    = (cs_real_3_t *restrict)mq->i_f_face_normal;
+  cs_real_3_t *restrict b_f_face_normal
+    = (cs_real_3_t *restrict)mq->b_f_face_normal;
+
+  const cs_mesh_adjacencies_t *ma = cs_glob_mesh_adjacencies;
+  cs_mesh_adjacencies_update_cell_i_faces();
+
+  const cs_lnum_t *c2c_idx = ma->cell_cells_idx;
+  const cs_lnum_t *cell_i_faces = ma->cell_i_faces;
+  const cs_lnum_t *cell_b_faces_idx = ma->cell_b_faces_idx;
+  const cs_lnum_t *cell_b_faces = ma->cell_b_faces;
 
   cs_real_3_t *cen_points = NULL;
   cs_field_t *f = cs_field_by_name_try("cell_scan_points_cog");
@@ -310,6 +326,7 @@ cs_f_mesh_quantities_solid_compute(void)
 
   /* Update the cell porosity field value */
   cs_field_t *f_poro = cs_field_by_name("porosity");
+  cs_real_t *nb_scan = cs_field_by_name("nb_scan_points")->val;
 
   /* Reactivate cells at the interface between fluid and solid */
   for (cs_lnum_t c_id = 0; c_id < m->n_cells; c_id++) {
@@ -320,8 +337,47 @@ cs_f_mesh_quantities_solid_compute(void)
     f_poro->val[c_id] = porosity;
     if (porosity > poro_threshold)
       mq->c_disable_flag[c_id] = 0;
+    
+    /* Penalize ibm cells with small porosity */
+    else if (porosity < poro_threshold && nb_scan[c_id] > threshold) {
 
+      mq->c_disable_flag[c_id] = 1;
+      
+      f_poro->val[c_id] = 0.;
+      mq->cell_f_vol[c_id] = 0.0;
+      mq->c_w_face_surf[c_id] = 0.0;
+      
+      for (cs_lnum_t i = 0; i < 3; i++)
+	c_w_face_normal[c_id][i] = 0.0;
+
+      /* Interior faces */
+      const cs_lnum_t s_id_i = c2c_idx[c_id];
+      const cs_lnum_t e_id_i = c2c_idx[c_id+1];
+
+      /* Loop on interior faces of cell c_id */
+      for (cs_lnum_t cidx = s_id_i; cidx < e_id_i; cidx++) {
+	const cs_lnum_t face_id = cell_i_faces[cidx];
+
+	i_f_face_normal[face_id][0] = 0.;
+	i_f_face_normal[face_id][1] = 0.;
+	i_f_face_normal[face_id][2] = 0.;
+	mq->i_f_face_surf[face_id] = 0.;
+      }
+
+      /* Boundary faces */
+      const cs_lnum_t s_id_b = cell_b_faces_idx[c_id];
+      const cs_lnum_t e_id_b = cell_b_faces_idx[c_id+1];
+    
+      for (cs_lnum_t cidx = s_id_b; cidx < e_id_b; cidx++) {
+	const cs_lnum_t face_id = cell_b_faces[cidx];
+
+	b_f_face_normal[face_id][0] = 0.;
+	b_f_face_normal[face_id][1] = 0.;
+	b_f_face_normal[face_id][2] = 0.;
+      }
+    }
   }
+  
   /* synchronize for use in fluid face factor calculation */
   cs_halo_sync_var(m->halo, CS_HALO_STANDARD, f_poro->val);
 }

src/base/cs_wall_functions.c
@@ -666,16 +666,18 @@ cs_immersed_boundary_wall_functions(int         f_id,
 
     for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++) {
 
-      //Geometric quantities
-      cs_real_t solid_surf = c_w_face_surf[c_id];
+      /* Geometric quantities */
+      const cs_real_t solid_surf = c_w_face_surf[c_id];
 
-      if (solid_surf > cs_math_epzero*pow(cell_f_vol[c_id], 2./3.)) {
+      const cs_real_t wall_dist  = (c_w_dist_inv[c_id] < DBL_MIN) ?
+                                    0.:
+                                    1. / c_w_dist_inv[c_id];
 
-        cs_real_t wall_dist  = (c_w_dist_inv[c_id] < DBL_MIN) ?
-                                0.:
-                                1. / c_w_dist_inv[c_id];
+      const cs_real_t pyr_vol = wall_dist * solid_surf;      
+      
+      if (pyr_vol > cs_math_epzero*cell_f_vol[c_id]) {
 
-        /* 1. Velocity components on the solid surface */
+        /* Velocity components on the solid surface */
 
         // Unit normal
         cs_real_3_t nw;
@@ -691,7 +693,7 @@ cs_immersed_boundary_wall_functions(int         f_id,
         // Unit tangent
         cs_math_3_normalize(nt, nt);
 
-        /* 2. Friction velocities */
+        /* Friction velocities */
 
         if (utau <= cs_math_epzero) utau = cs_math_epzero;
 
@@ -713,13 +715,13 @@ cs_immersed_boundary_wall_functions(int         f_id,
         if (cpro_roughness != NULL)
           w_roughness = cpro_roughness[c_id];
 
-        int iuntur;
-        cs_real_t ustar;
-        cs_real_t uk;
-        cs_real_t yplus;
-        cs_real_t ypup;
-        cs_real_t cofimp;
-        cs_real_t dplus;
+        int iuntur = 0;
+        cs_real_t ustar = 0.0;
+        cs_real_t uk = 0.;
+        cs_real_t yplus = 0.;
+        cs_real_t ypup = 1.0;
+        cs_real_t cofimp = 0.;
+        cs_real_t dplus = 0.0;
 
         /* If the cell is a solid cell, disable wall functions */
 
@@ -729,23 +731,24 @@ cs_immersed_boundary_wall_functions(int         f_id,
             iwallf_loc = CS_WALL_F_DISABLED;
         }
 
-        cs_wall_functions_velocity(iwallf_loc,
-                                   l_visc,
-                                   t_visc,
-                                   utau,
-                                   wall_dist,
-                                   w_roughness,
-                                   rnnb,
-                                   ek,
-                                   &iuntur,
-                                   &nsubla,
-                                   &nlogla,
-                                   &ustar,
-                                   &uk,
-                                   &yplus,
-                                   &ypup,
-                                   &cofimp,
-                                   &dplus);
+	if (wall_dist > DBL_MIN)
+	  cs_wall_functions_velocity(iwallf_loc,
+				     l_visc,
+				     t_visc,
+				     utau,
+				     wall_dist,
+				     w_roughness,
+				     rnnb,
+				     ek,
+				     &iuntur,
+				     &nsubla,
+				     &nlogla,
+				     &ustar,
+				     &uk,
+				     &yplus,
+				     &ypup,
+				     &cofimp,
+				     &dplus);
 
         if (ib_uk != NULL)
           ib_uk->val[c_id] = uk;

src/base/cs_wall_functions.h
@@ -976,17 +976,8 @@ cs_wall_functions_disabled(cs_real_t   l_visc,
   *cofimp = 0.;
   *iuntur = 0;
 
-  if (*yplus <= ypluli) {
-
-    /* Disable the wall funcion count the cell in the viscous sub-layer */
-    *nsubla += 1;
-
-  } else {
-
-    /* Count the cell as if it was in the viscous sub-layer */
-    *nsubla += 1;
-
-  }
+  /* Count the cell as if it was in the viscous sub-layer */
+  *nsubla += 1;
 }
 
 /*----------------------------------------------------------------------------*/

src/mesh/cs_mesh_quantities.c
@@ -4360,6 +4360,8 @@ cs_mesh_quantities_solid_compute(const cs_mesh_t       *m,
         d_dist = 1./d_dist;
 
       c_w_dist_inv[c_id] = d_dist;
+
+      /* else c_w_dist_inv = 0 by default as it is a field */
     }
   }
 

commit 2aed45f6ea192e863b0bb99a1695a263c7e91c72

[IBM] Fix bug introduced in ae32ef6b

src/base/cs_porous_model.c
@@ -300,7 +300,6 @@ cs_f_mesh_quantities_solid_compute(void)
   cs_mesh_quantities_t *mq = cs_glob_mesh_quantities;
   cs_porosity_from_scan_opt_t *poro_from_scan = cs_glob_porosity_from_scan_opt;
   cs_real_t poro_threshold = poro_from_scan->porosity_threshold;
-  cs_real_t threshold = poro_from_scan->threshold;
 
   cs_real_3_t *restrict c_w_face_normal
     = (cs_real_3_t *restrict)mq->c_w_face_normal;
@@ -326,7 +325,6 @@ cs_f_mesh_quantities_solid_compute(void)
 
   /* Update the cell porosity field value */
   cs_field_t *f_poro = cs_field_by_name("porosity");
-  cs_real_t *nb_scan = cs_field_by_name("nb_scan_points")->val;
 
   /* Reactivate cells at the interface between fluid and solid */
   for (cs_lnum_t c_id = 0; c_id < m->n_cells; c_id++) {
@@ -339,7 +337,7 @@ cs_f_mesh_quantities_solid_compute(void)
       mq->c_disable_flag[c_id] = 0;
 
     /* Penalize ibm cells with small porosity */
-    else if (porosity < poro_threshold && nb_scan[c_id] > threshold) {
+    else if (porosity < poro_threshold && porosity > cs_math_epzero) {
 
       mq->c_disable_flag[c_id] = 1;
 

src/base/cs_wall_functions.c
@@ -810,7 +810,7 @@ cs_immersed_boundary_wall_functions(int         f_id,
       const cs_real_t pyr_vol = wall_dist * solid_surf;
 
       if (pyr_vol > cs_math_epzero*cell_f_vol[c_id]) {
-        
+
         cs_real_t hint = ( cpro_mu[c_id]
                          + cpro_mut[c_id]/cpro_sigmae[c_id*sigmae_step])
                          * c_w_dist_inv[c_id];