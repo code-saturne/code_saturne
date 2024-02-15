code-saturne/code_saturne

commit 7c9922a7ddb816d7969cf966f2563a58bd594037

Translate diffst to C (cs_dilatable_scalar_diff_st.c)

src/base/cs_dilatable_scalar_diff_st.c
@@ -1,0 +1,314 @@
+/*============================================================================
+ * Weakly compressible algorithm (semi-analytic):
+   Computation of scalar diffusion terms.
+ *============================================================================*/
+
+/*
+  This file is part of code_saturne, a general-purpose CFD tool.
+
+  Copyright (C) 1998-2024 EDF S.A.
+
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation; either version 2 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+  Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/*----------------------------------------------------------------------------*/
+
+#include "cs_defs.h"
+
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+ * Standard C library headers
+ *----------------------------------------------------------------------------*/
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <math.h>
+
+#if defined(HAVE_MPI)
+#include <mpi.h>
+#endif
+
+/*----------------------------------------------------------------------------
+ * Local headers
+ *----------------------------------------------------------------------------*/
+
+#include "bft_printf.h"
+
+#include "cs_array.h"
+#include "cs_balance.h"
+#include "cs_face_viscosity.h"
+#include "cs_field_default.h"
+#include "cs_field_pointer.h"
+#include "cs_mesh.h"
+#include "cs_physical_constants.h"
+#include "cs_time_step.h"
+
+/*----------------------------------------------------------------------------
+ *  Header for the current file
+ *----------------------------------------------------------------------------*/
+
+#include "cs_dilatable_scalar_diff_st.h"
+
+/*----------------------------------------------------------------------------*/
+
+BEGIN_C_DECLS
+
+/*=============================================================================
+ * Additional doxygen documentation
+ *============================================================================*/
+
+/*!
+  \file cs_dilatable_scalar_diff_st.c
+        Weakly compressible algorithm (semi-analytic):
+        Computation of scalar diffusion terms.
+*/
+
+/*! \cond DOXYGEN_SHOULD_SKIP_THIS */
+
+/*============================================================================
+ * Private function definitions
+ *============================================================================*/
+
+/*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
+
+/*============================================================================
+ * Public function definitions
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Weakly compressible algorithm (semi-analytic):
+          Computation of scalar diffusion terms.
+ *
+ * \param[in]  iterns      Navier-Stokes iteration number
+ *
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_dilatable_scalar_diff_st(int iterns)
+
+{
+  const cs_mesh_t *mesh = cs_glob_mesh;
+  const cs_mesh_quantities_t *fvq = cs_glob_mesh_quantities;
+
+  const cs_lnum_t n_cells_ext = mesh->n_cells_with_ghosts;
+  const cs_lnum_t n_cells = mesh->n_cells;
+  const cs_lnum_t n_i_faces = mesh->n_i_faces;
+  const cs_lnum_t n_b_faces = mesh->n_b_faces;
+
+  const cs_fluid_properties_t *fluid_props = cs_glob_fluid_properties;
+  const int icp = fluid_props->icp;
+  const cs_real_t rair = fluid_props->r_pg_cnst;
+  const cs_real_t cp0 = fluid_props->cp0;
+
+  const int n_fields = cs_field_n_fields();
+  const int keysca  = cs_field_key_id("scalar_id");
+  const int kscavr = cs_field_key_id("first_moment_id");
+  const int kivisl  = cs_field_key_id("diffusivity_id");
+  const int kvisl0 = cs_field_key_id("diffusivity_ref");
+  const int kscacp  = cs_field_key_id("is_temperature");
+  const int ksigmas = cs_field_key_id("turbulent_schmidt");
+
+  /* Memory allocation */
+  cs_real_t *vistot, *i_visc, *b_visc, *xcpp;
+  BFT_MALLOC(vistot, n_cells_ext, cs_real_t);
+  BFT_MALLOC(i_visc, n_i_faces, cs_real_t);
+  BFT_MALLOC(b_visc, n_b_faces, cs_real_t);
+  BFT_MALLOC(xcpp, n_cells_ext, cs_real_t);
+
+  const cs_real_t *cpro_cp = NULL;
+  if (icp >= 0)
+    cpro_cp = CS_F_(cp)->val;
+
+  /* Index for turbulent diffusivity */
+  const cs_real_t *visct = CS_F_(mu_t)->val;
+
+  /* Loop on scalars */
+  for (int ii = 0; ii < n_fields; ii++) {
+
+    cs_field_t *f_scal = cs_field_by_id(ii);
+
+    if (!(f_scal->type & CS_FIELD_VARIABLE))
+      continue;
+    if (cs_field_get_key_int(f_scal, keysca) <= 0)
+      continue;
+
+    cs_real_t *cvar_scal = f_scal->val;
+
+    /* Key id for buoyant field (inside the Navier Stokes loop) */
+    const int key_is_buoyant = cs_field_key_id_try("is_buoyant");
+    const int is_buoyant_fld = cs_field_get_key_int(f_scal, key_is_buoyant);
+
+    if (   (is_buoyant_fld == 1 && iterns == -1)
+        || (is_buoyant_fld == 0 && iterns != -1))
+      continue;
+
+    const int iscavr = cs_field_get_key_int(f_scal, kscavr);
+
+    const int iscacp = (iscavr > 0) ?
+      cs_field_get_key_int(cs_field_by_id(iscavr), kscacp):
+      cs_field_get_key_int(f_scal, kscacp);
+
+    const int imucpp = (iscacp == 1) ? 1 : 0;
+
+    if (imucpp == 0)
+      cs_array_real_set_scalar(n_cells, 1.0, xcpp);
+    else if (imucpp == 1) { // TODO: humid air
+
+      if (icp >= 0) {
+        
+        if (iscacp == 1)
+          cs_array_real_copy(n_cells, cpro_cp, xcpp);
+        else if (iscacp == 2) {
+#   pragma omp parallel for if (n_cells > CS_THR_MIN)
+          for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++)
+            xcpp[c_id] = cpro_cp[c_id] - rair;
+        }
+      }
+      else {
+        if (iscacp == 1)
+          cs_array_real_set_scalar(n_cells, cp0, xcpp);
+        else if (iscacp == 2)
+          cs_array_real_set_scalar(n_cells, cp0-rair, xcpp);
+      }
+    }
+
+    /* Handle parallelism and periodicity */
+    if (cs_glob_rank_id > -1 || mesh->periodicity != NULL) {
+      const cs_halo_t *halo = mesh->halo;
+      cs_halo_sync_var(halo, CS_HALO_STANDARD, xcpp);
+    }
+
+    cs_equation_param_t *eqp_sc = cs_field_get_equation_param(f_scal);
+
+    /* Pointers to the mass fluxes */
+    const int iflmas
+      = cs_field_get_key_int(CS_F_(vel), cs_field_key_id("inner_mass_flux_id"));
+    const cs_real_t *i_mass_flux = cs_field_by_id(iflmas)->val;
+
+    const int iflmab
+      = cs_field_get_key_int(CS_F_(vel), cs_field_key_id("boundary_mass_flux_id"));
+    const cs_real_t *b_mass_flux = cs_field_by_id(iflmab)->val;
+
+    /* Diffusion velocity */
+
+    /* Index for molecular diffusivity */
+    const int ifcvsl = cs_field_get_key_int(f_scal, kivisl);
+    cs_real_t *viscls = NULL;
+    if (ifcvsl > -1)
+      viscls = cs_field_by_id(ifcvsl)->val;
+
+    if (eqp_sc->idiff >= 1) {
+
+      /* Only the positive part of mu_t is considered (MAX(mu_t,0)),
+         Dynamic LES can cause negative mu_t (clipping on (mu+mu_t))
+         The positive part of (K+K_t) would have been considered but
+         should allow negative K_t that is considered non physical here */
+
+      const cs_real_t turb_schmidt = cs_field_get_key_double(f_scal, ksigmas);
+
+      if (ifcvsl < 0) {
+        const cs_real_t visls_0 = cs_field_get_key_double(f_scal, kvisl0);
+#   pragma omp parallel for if (n_cells > CS_THR_MIN)
+        for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++)
+          vistot[c_id] = visls_0
+            + eqp_sc->idifft*xcpp[c_id]*cs_math_fmax(visct[c_id], 0.)
+            / turb_schmidt;
+      }
+      else {
+#   pragma omp parallel for if (n_cells > CS_THR_MIN)
+        for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++)
+          vistot[c_id] = viscls[c_id]
+            + eqp_sc->idifft*xcpp[c_id]*cs_math_fmax(visct[c_id], 0.)
+            / turb_schmidt;
+      }
+
+      cs_face_viscosity(mesh,
+                        fvq,
+                        eqp_sc->imvisf,
+                        vistot,
+                        i_visc,
+                        b_visc);
+
+    }
+    else {
+      cs_array_real_fill_zero(n_i_faces, i_visc);
+      cs_array_real_fill_zero(n_b_faces, b_visc);
+      cs_array_real_fill_zero(n_cells, vistot);
+    }
+
+    /* Source term */
+    char scalar_st_name[50];
+    sprintf(scalar_st_name, "%s_%s", f_scal->name, "dila_st");
+    cs_real_t *cpro_tsscal = cs_field_by_name(scalar_st_name)->val;
+
+    /* Diffusion term calculation */
+    const cs_field_bc_coeffs_t *bc_coeffs_sc = f_scal->bc_coeffs;
+
+    cs_equation_param_t eqp_loc = *eqp_sc;
+
+    /*  All boundary convective flux with upwind */
+
+    eqp_loc.iconv  = 0; /* diffusion term only */
+    eqp_loc.istat  = -1;
+    eqp_loc.idiff  = 1;
+    eqp_loc.idifft = -1;
+    eqp_loc.idften = CS_ISOTROPIC_DIFFUSION; /* FIXME when activating GGDH */
+    eqp_loc.iswdyn = -1;
+    eqp_loc.ischcv = 1;
+    eqp_loc.isstpc = 1;
+    eqp_loc.nswrsm = -1;
+    eqp_loc.iwgrec = 0;
+    eqp_loc.thetav = 1.0;
+    eqp_loc.blencv = 0.0;
+    eqp_loc.blend_st = 0; /* Warning, may be overwritten if a field */
+    eqp_loc.epsilo = -1;
+    eqp_loc.epsrsm = -1;
+
+    cs_balance_scalar(cs_glob_time_step_options->idtvar,
+                      -1,    /* f_id */
+                      imucpp,
+                      0,     /* imasac */
+                      1,     /* inc */
+                      &eqp_loc,
+                      cvar_scal, cvar_scal,
+                      bc_coeffs_sc,
+                      i_mass_flux, b_mass_flux,
+                      i_visc, b_visc,
+                      NULL,  /* viscel */
+                      xcpp,
+                      NULL,  /* weighf */
+                      NULL,  /* weighb */
+                      0,     /* icvflb; upwind scheme */
+                      NULL,
+                      cpro_tsscal);
+
+    /* Free memory */
+    BFT_FREE(i_visc);
+    BFT_FREE(b_visc);
+    BFT_FREE(vistot);
+    BFT_FREE(xcpp);
+  }
+
+}
+
+/*---------------------------------------------------------------------------- */
+
+END_C_DECLS

src/alge/cs_face_viscosity.c
@@ -106,29 +106,6 @@ BEGIN_C_DECLS
  *============================================================================*/
 
 /*----------------------------------------------------------------------------
- * Wrapper to cs_face_viscosity
- *----------------------------------------------------------------------------*/
-
-void CS_PROCF (viscfa, VISCFA)
-(
- const int  *const   visc_mean_type,
- cs_real_t           c_visc[],
- cs_real_t           i_visc[],
- cs_real_t           b_visc[]
-)
-{
-  const cs_mesh_t  *m = cs_glob_mesh;
-  cs_mesh_quantities_t  *fvq = cs_glob_mesh_quantities;
-
-  cs_face_viscosity(m,
-                    fvq,
-                    *visc_mean_type,
-                    c_visc,
-                    i_visc,
-                    b_visc);
-}
-
-/*----------------------------------------------------------------------------
  * Wrapper to cs_face_anisotropic_viscosity_vector
  *----------------------------------------------------------------------------*/
 

src/alge/cs_face_viscosity.h
@@ -57,18 +57,6 @@ BEGIN_C_DECLS
  *============================================================================*/
 
 /*----------------------------------------------------------------------------
- * Wrapper to cs_face_viscosity
- *----------------------------------------------------------------------------*/
-
-void CS_PROCF (viscfa, VISCFA)
-(
- const int   *const visc_mean_type,
- cs_real_t          c_visc[],
- cs_real_t          i_visc[],
- cs_real_t          b_visc[]
-);
-
-/*----------------------------------------------------------------------------
  * Wrapper to cs_face_anisotropic_viscosity_vector
  *----------------------------------------------------------------------------*/
 

src/base/Makefile.am
@@ -113,6 +113,7 @@ cs_control.h \
 cs_coupling.h \
 cs_crystal_router.h \
 cs_defs.h \
+cs_dilatable_scalar_diff_st.h \
 cs_drift_convective_flux.h \
 cs_equation_iterative_solve.h \
 cs_ext_library_info.h \
@@ -320,6 +321,7 @@ csprnt.f90 \
 cs_time_step_compute.c \
 cs_control.c \
 cs_coupling.c \
+cs_dilatable_scalar_diff_st.c \
 cs_drift_convective_flux.c \
 cs_ext_library_info.c \
 cs_ext_neighborhood.c \
@@ -404,7 +406,6 @@ cs_volume_zone.c \
 cs_wall_distance.c \
 cs_wall_functions.c \
 cs_xdef_eval_at_zone.c \
-diffst.f90 \
 distpr2.f90 \
 ecrlis.f90 \
 findpt.f90 \

src/base/cs_boundary_conditions_set_coeffs_turb.c
@@ -573,8 +573,8 @@ _cs_boundary_conditions_set_coeffs_turb_scalar(cs_field_t  *f_sc,
       cpp = (icp >= 0) ? cpro_cp[c_id] : cp0;
     else if (iscacp == 2) {
       cpp = (icp >= 0) ? cpro_cp[c_id] - rair : cp0 - rair;
-      /* FIXME: this formula does not seem consistent with that in diffst,
-         but was present in clptrg.f90 */
+      /* FIXME: this formula does not seem consistent with that
+         in cs_dilatable_scalar_diff_st, but was present in clptrg.f90 */
       if (icodcl_vel[f_id] == 6) {
         cpp = (icp >= 0) ? cpro_cv[c_id] : cp0;
       }

src/base/cs_c_bindings.f90
@@ -79,8 +79,6 @@ module cs_c_bindings
   procedure() :: csexit, dmtmps
   procedure() :: cslogname
 
-  procedure() :: viscfa
-
   !-----------------------------------------------------------------------------
 
   type, bind(c)  :: var_cal_opt

src/base/cs_dilatable_scalar_diff_st.h
@@ -1,0 +1,70 @@
+#ifndef __CS_DILATABLE_SCALAR_DIFF_ST_H__
+#define __CS_DILATABLE_SCALAR_DIFF_ST_H__
+
+/*============================================================================
+ * Weakly compressible algorithm (semi-analytic):
+   Computation of scalar diffusion terms.
+ *============================================================================*/
+
+/*
+  This file is part of code_saturne, a general-purpose CFD tool.
+
+  Copyright (C) 1998-2024 EDF S.A.
+
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation; either version 2 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+  Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+ * Standard C library headers
+ *----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+ * Local headers
+ *----------------------------------------------------------------------------*/
+
+#include "cs_defs.h"
+
+/*----------------------------------------------------------------------------*/
+
+BEGIN_C_DECLS
+
+/*=============================================================================
+ * Public function prototypes
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------*/
+/*! \file cs_dilatable_scalar_diff_st.c
+ *
+ * \brief Weakly compressible algorithm (semi-analytic):
+                 Computation of scalar diffusion terms.
+ */
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/*
+ * \param[in]  iterns      Navier-Stokes iteration number
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_dilatable_scalar_diff_st(int iterns);
+
+/*----------------------------------------------------------------------------*/
+
+END_C_DECLS
+
+#endif /* __CS_DILATABLE_SCALAR_DIFF_ST_H__ */

src/base/diffst.f90
@@ -1,303 +1,0 @@
-!-------------------------------------------------------------------------------
-
-! This file is part of code_saturne, a general-purpose CFD tool.
-!
-! Copyright (C) 1998-2024 EDF S.A.
-!
-! This program is free software; you can redistribute it and/or modify it under
-! the terms of the GNU General Public License as published by the Free Software
-! Foundation; either version 2 of the License, or (at your option) any later
-! version.
-!
-! This program is distributed in the hope that it will be useful, but WITHOUT
-! ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-! FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-! details.
-!
-! You should have received a copy of the GNU General Public License along with
-! this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
-! Street, Fifth Floor, Boston, MA 02110-1301, USA.
-
-!-------------------------------------------------------------------------------
-
-subroutine diffst &
-!================
-
- ( nscal, iterns )
-
-!===============================================================================
-! Function :
-! --------
-
-! Weakly compressible algorithm (semi-analytic):
-!  Computation of scalar diffusion terms
-
-! Arguments
-!__________________.____._____.________________________________________________.
-! name             !type!mode ! role                                           !
-!__________________!____!_____!________________________________________________!
-! nscal            ! i  ! <-- ! total number of scalars                        !
-! iterns           ! i  ! <-- ! Navier-Stokes iteration number                 !
-!__________________!____!_____!________________________________________________!
-
-!     Type: i (integer), r (real), s (string), a (array), l (logical),
-!           and composite types (ex: ra real array)
-!     mode: <-- input, --> output, <-> modifies data, --- work array
-!===============================================================================
-
-!===============================================================================
-! Module files
-!===============================================================================
-
-use paramx
-use numvar
-use optcal
-use cstphy
-use cstnum
-use entsor
-use pointe
-use albase
-use parall
-use period
-use ppppar
-use ppthch
-use ppincl
-use mesh
-use field
-use cs_c_bindings
-
-!===============================================================================
-
-implicit none
-
-! Arguments
-
-integer          nscal , iterns
-
-! Local variables
-
-integer          ivar  , iel   , ifac  , iscal, f_id0
-integer          inc
-integer          iconvp, idiffp, imvisp
-integer          ischcp, isstpp
-integer          iscacp, ifcvsl, iflmas, iflmab
-integer          imucpp, idftnp, imasac
-integer          key_buoyant_id, is_buoyant_fld
-double precision blencp, thetex
-double precision turb_schmidt, visls_0
-
-integer          icvflb
-integer          ivoid(1)
-
-double precision rvoid(1)
-
-double precision, allocatable, dimension(:) :: vistot, viscf, viscb
-double precision, allocatable, dimension(:) :: xcpp
-double precision, dimension(:), pointer :: imasfl, bmasfl
-double precision, dimension(:), pointer :: coefap, coefbp, cofafp, cofbfp
-double precision, dimension(:), pointer :: visct, cpro_cp, cpro_viscls
-double precision, dimension(:), pointer :: cvar_scal
-double precision, dimension(:), pointer :: cpro_tsscal
-
-type(var_cal_opt) :: vcopt
-type(var_cal_opt), target   :: vcopt_loc
-type(var_cal_opt), pointer  :: p_k_value
-type(c_ptr)                 :: c_k_value
-
-!===============================================================================
-
-! Memory allocation
-allocate(vistot(ncelet))
-allocate(viscf(nfac), viscb(nfabor))
-allocate(xcpp(ncelet))
-
-if (icp.ge.0) call field_get_val_s(icp, cpro_cp)
-
-do iscal = 1, nscal
-
-  ! Index for variable
-  ivar = isca(iscal)
-
-  call field_get_val_s(ivarfl(ivar), cvar_scal)
-
-  ! Key id for buoyant field (inside the Navier Stokes loop)
-  call field_get_key_id("is_buoyant", key_buoyant_id)
-  call field_get_key_int(ivarfl(ivar), key_buoyant_id, is_buoyant_fld)
-
-  ! If the scalar is buoyant, it is inside the Navier Stokes loop, and so iterns >=1
-  ! otherwise it is outside of the loop and iterns = -1.
-  if (  (is_buoyant_fld.eq. 1 .and. iterns.eq.-1) &
-    .or.(is_buoyant_fld.eq. 0 .and. iterns.ne.-1)) cycle
-
-  imucpp = 0
-  if (iscavr(iscal).gt.0) then
-    call field_get_key_int(ivarfl(isca(iscavr(iscal))), kscacp, iscacp)
-    if (iscacp.eq.1) then
-      imucpp = 1
-    endif
-  else
-    call field_get_key_int(ivarfl(ivar), kscacp, iscacp)
-    if (iscacp.eq.1) then
-      imucpp = 1
-    endif
-  endif
-
-  if (imucpp.eq.0) then
-    do iel = 1, ncel
-      xcpp(iel) = 1.d0
-    enddo
-  elseif (imucpp.eq.1) then !TODO: humid air
-    if (icp.ge.0) then
-      if (iscacp.eq.1) then
-        do iel = 1, ncel
-          xcpp(iel) = cpro_cp(iel)
-        enddo
-      elseif (iscacp.eq.2) then
-        do iel = 1, ncel
-          xcpp(iel) = cpro_cp(iel) - rair
-        enddo
-      endif
-    else
-      if (iscacp.eq.1) then
-        do iel = 1, ncel
-          xcpp(iel) = cp0
-        enddo
-      elseif (iscacp.eq.2) then
-        do iel = 1, ncel
-          xcpp(iel) = cp0 - rair
-        enddo
-      endif
-    endif
-  endif
-
-  ! Handle parallelism and periodicity
-  if (irangp.ge.0.or.iperio.eq.1) then
-    call synsca(xcpp)
-  endif
-
-  call field_get_key_struct_var_cal_opt(ivarfl(ivar), vcopt)
-
-  f_id0  = -1
-  iconvp = 0 ! diffusion term only
-  ischcp = 1
-  isstpp = 1
-  blencp = 0.d0
-  imasac = 0
-  idiffp = 1
-  inc    = 1
-  idftnp = ISOTROPIC_DIFFUSION !FIXME when activating GGDH
-  thetex = 1.d0
-  ! all boundary convective flux with upwind
-  icvflb = 0
-
-  ! Pointers to the mass fluxes
-  call field_get_key_int(ivarfl(iu), kimasf, iflmas)
-  call field_get_key_int(ivarfl(iu), kbmasf, iflmab)
-  call field_get_val_s(iflmas, imasfl)
-  call field_get_val_s(iflmab, bmasfl)
-
-  ! Diffusion velocity
-
-  ! Index for molecular diffusivity
-  call field_get_key_int (ivarfl(isca(iscal)), kivisl, ifcvsl)
-  if (ifcvsl.ge.0) then
-    call field_get_val_s(ifcvsl, cpro_viscls)
-  endif
-
-  ! Index for turbulent diffusivity
-  call field_get_val_s(ivisct, visct)
-
-  if (vcopt%idiff.ge.1) then
-
-    ! Only the positive part of mu_t is considered (MAX(mu_t,0)),
-    ! Dynamic LES can cause negative mu_t (clipping on (mu+mu_t))
-    ! The positive part of (K+K_t) would have been considered
-    ! but should allow negative K_t that is considered non physical here
-
-    call field_get_key_double(ivarfl(isca(iscal)), ksigmas, turb_schmidt)
-
-    if (ifcvsl.lt.0)then
-      call field_get_key_double(ivarfl(isca(iscal)), kvisl0, visls_0)
-      do iel = 1, ncel
-        vistot(iel) = visls_0                                           &
-           + vcopt%idifft*xcpp(iel)*max(visct(iel),zero)/turb_schmidt
-      enddo
-    else
-      do iel = 1, ncel
-        vistot(iel) = cpro_viscls(iel)                                  &
-           + vcopt%idifft*xcpp(iel)*max(visct(iel),zero)/turb_schmidt
-      enddo
-    endif
-
-    imvisp = vcopt%imvisf
-
-    call viscfa ( imvisp , vistot , viscf , viscb )
-
-  else
-
-    do ifac = 1, nfac
-      viscf(ifac) = 0.d0
-    enddo
-    do ifac = 1, nfabor
-      viscb(ifac) = 0.d0
-    enddo
-    do iel = 1, ncel
-      vistot(iel) = 0.d0
-    enddo
-
-  endif
-
-  ! Source term
-  call field_get_val_s(iustdy(iscal), cpro_tsscal)
-
-  ! Diffusion term calculation
-  call field_get_coefa_s(ivarfl(ivar), coefap)
-  call field_get_coefb_s(ivarfl(ivar), coefbp)
-  call field_get_coefaf_s(ivarfl(ivar), cofafp)
-  call field_get_coefbf_s(ivarfl(ivar), cofbfp)
-
-  vcopt_loc = vcopt
-
-  vcopt_loc%iconv  = iconvp
-  vcopt_loc%istat  = -1
-  vcopt_loc%idiff  = idiffp
-  vcopt_loc%idifft = -1
-  vcopt_loc%idften = idftnp
-  vcopt_loc%iswdyn = -1
-  vcopt_loc%ischcv = ischcp
-  vcopt_loc%isstpc = isstpp
-  vcopt_loc%nswrsm = -1
-  vcopt_loc%iwgrec = 0
-  vcopt_loc%thetav = thetex
-  vcopt_loc%blencv = blencp
-  vcopt_loc%blend_st = 0 ! Warning, may be overwritten if a field
-  vcopt_loc%epsilo = -1
-  vcopt_loc%epsrsm = -1
-
-  p_k_value => vcopt_loc
-  c_k_value = equation_param_from_vcopt(c_loc(p_k_value))
-
-  call cs_balance_scalar &
-  ( idtvar , f_id0  , imucpp , imasac , inc    ,                            &
-    c_k_value       , cvar_scal       , cvar_scal       , coefap , coefbp , &
-    cofafp , cofbfp , imasfl , bmasfl , viscf  , viscb  ,                   &
-    rvoid  , xcpp   , rvoid  , rvoid  , icvflb , ivoid  ,                   &
-    cpro_tsscal     )
-
-enddo
-
-! Free memory
-deallocate(viscf, viscb)
-deallocate(vistot)
-deallocate(xcpp)
-
-!----
-! Formats
-!----
-
-!----
-! End
-!----
-
-return
-end subroutine

commit c651cfb7ebe6ad8fbdba59cf4d8fc9e94b28aa4c

Fix crash due do premature array free, broken by commit 7c9922a7 (yesterday).

src/base/cs_dilatable_scalar_diff_st.c
@@ -173,11 +173,10 @@ cs_dilatable_scalar_diff_st(int iterns)
     else if (imucpp == 1) { // TODO: humid air
 
       if (icp >= 0) {
-        
         if (iscacp == 1)
           cs_array_real_copy(n_cells, cpro_cp, xcpp);
         else if (iscacp == 2) {
-#   pragma omp parallel for if (n_cells > CS_THR_MIN)
+#         pragma omp parallel for if (n_cells > CS_THR_MIN)
           for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++)
             xcpp[c_id] = cpro_cp[c_id] - rair;
         }
@@ -191,9 +190,8 @@ cs_dilatable_scalar_diff_st(int iterns)
     }
 
     /* Handle parallelism and periodicity */
-    if (cs_glob_rank_id > -1 || mesh->periodicity != NULL) {
-      const cs_halo_t *halo = mesh->halo;
-      cs_halo_sync_var(halo, CS_HALO_STANDARD, xcpp);
+    if (mesh->halo != NULL) {
+      cs_halo_sync_var(mesh->halo, CS_HALO_STANDARD, xcpp);
     }
 
     cs_equation_param_t *eqp_sc = cs_field_get_equation_param(f_scal);
@@ -204,7 +202,8 @@ cs_dilatable_scalar_diff_st(int iterns)
     const cs_real_t *i_mass_flux = cs_field_by_id(iflmas)->val;
 
     const int iflmab
-      = cs_field_get_key_int(CS_F_(vel), cs_field_key_id("boundary_mass_flux_id"));
+      = cs_field_get_key_int(CS_F_(vel),
+                             cs_field_key_id("boundary_mass_flux_id"));
     const cs_real_t *b_mass_flux = cs_field_by_id(iflmab)->val;
 
     /* Diffusion velocity */
@@ -226,14 +225,14 @@ cs_dilatable_scalar_diff_st(int iterns)
 
       if (ifcvsl < 0) {
         const cs_real_t visls_0 = cs_field_get_key_double(f_scal, kvisl0);
-#   pragma omp parallel for if (n_cells > CS_THR_MIN)
+#       pragma omp parallel for if (n_cells > CS_THR_MIN)
         for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++)
           vistot[c_id] = visls_0
             + eqp_sc->idifft*xcpp[c_id]*cs_math_fmax(visct[c_id], 0.)
             / turb_schmidt;
       }
       else {
-#   pragma omp parallel for if (n_cells > CS_THR_MIN)
+#       pragma omp parallel for if (n_cells > CS_THR_MIN)
         for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++)
           vistot[c_id] = viscls[c_id]
             + eqp_sc->idifft*xcpp[c_id]*cs_math_fmax(visct[c_id], 0.)
@@ -299,14 +298,13 @@ cs_dilatable_scalar_diff_st(int iterns)
                       0,     /* icvflb; upwind scheme */
                       NULL,
                       cpro_tsscal);
+  } /* end loop on fields */
 
-    /* Free memory */
-    BFT_FREE(i_visc);
-    BFT_FREE(b_visc);
-    BFT_FREE(vistot);
-    BFT_FREE(xcpp);
-  }
-
+  /* Free memory */
+  BFT_FREE(i_visc);
+  BFT_FREE(b_visc);
+  BFT_FREE(vistot);
+  BFT_FREE(xcpp);
 }
 
 /*---------------------------------------------------------------------------- */