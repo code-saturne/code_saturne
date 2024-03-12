code-saturne/code_saturne

commit d1260f95a254709f4993a54afd3bedc2b71ad7b3

Migrate clpsca from fortran to C.
Edited by Y. Fournier.

src/base/cs_scalar_clipping.c
@@ -1,0 +1,231 @@
+/*============================================================================
+ * Clip scalar (i.e. convected/diffused) fields.
+ *============================================================================*/
+
+/*
+  This file is part of code_saturne, a general-purpose CFD tool.
+
+  Copyright (C) 1998-2022 EDF S.A.
+
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation; either version 2 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+  Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/*----------------------------------------------------------------------------*/
+
+#include "cs_defs.h"
+
+/*----------------------------------------------------------------------------
+ * Standard C library headers
+ *----------------------------------------------------------------------------*/
+
+#include <assert.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*----------------------------------------------------------------------------
+ * Local headers
+ *----------------------------------------------------------------------------*/
+
+#include "bft_mem.h"
+#include "bft_printf.h"
+
+#include "cs_field.h"
+#include "cs_log_iteration.h"
+#include "cs_math.h"
+
+/*----------------------------------------------------------------------------
+ * Header for the current file
+ *----------------------------------------------------------------------------*/
+
+#include "cs_scalar_clipping.h"
+
+/*----------------------------------------------------------------------------*/
+
+BEGIN_C_DECLS
+
+/*=============================================================================
+ * Additional doxygen documentation
+ *============================================================================*/
+
+/*!
+  \file cs_scalar_clipping.c
+
+  \brief Clipping scalar field.
+*/
+
+/*! \cond DOXYGEN_SHOULD_SKIP_THIS */
+
+/*============================================================================
+ * Type and macro definitions
+ *============================================================================*/
+
+/*============================================================================
+ * Static global variables
+ *============================================================================*/
+
+/*============================================================================
+ * Prototypes for functions intended for use only by Fortran wrappers.
+ * (descriptions follow, with function bodies).
+ *============================================================================*/
+
+/*============================================================================
+ * Fortran function prototypes for subroutines from field.f90.
+ *============================================================================*/
+
+/*============================================================================
+ * Prototypes for functions intended for use only by Fortran wrappers.
+ * (descriptions follow, with function bodies).
+ *============================================================================*/
+
+void
+cs_f_scalar_clipping(int   id);
+
+/*============================================================================
+ * Private function definitions
+ *============================================================================*/
+
+/*============================================================================
+ * Fortran wrapper function definitions
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------
+ * Clip a scalar variable field.
+ *
+ * This function is intended for use by Fortran wrappers.
+ *
+ * parameters:
+ *   id       <-- field id
+ *----------------------------------------------------------------------------*/
+
+void
+cs_f_scalar_clipping(int   id)
+{
+  cs_field_t *f = cs_field_by_id(id);
+
+  cs_scalar_clipping(f);
+}
+
+/*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
+
+/*============================================================================
+ * Public function definitions
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Clip a scalar variable field.
+ *
+ * \param[in]  f  pointer to field structure
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_scalar_clipping(cs_field_t  *f)
+{
+  const cs_lnum_t n_cells = cs_glob_mesh->n_cells;
+
+  const int kscavr = cs_field_key_id("first_moment_id");
+  const int kclvfl = cs_field_key_id_try("variance_clipping");
+  const int kscmin = cs_field_key_id_try("min_scalar_clipping");
+  const int kscmax = cs_field_key_id_try("max_scalar_clipping");
+
+  cs_real_t *cvar_scal = f->val;
+
+  int variance_id = cs_field_get_key_int(f, kscavr);
+
+  /* Logging and clippings
+   * --------------------- */
+
+  /* Variances are always clipped at positive values */
+
+  /* Compute min and max values */
+  cs_real_t vmin[1] = {HUGE_VAL};
+  cs_real_t vmax[1] = {-HUGE_VAL};
+
+  for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++) {
+    vmin[0] = cs_math_fmin(cvar_scal[c_id], vmin[0]);
+    vmax[0] = cs_math_fmax(cvar_scal[c_id], vmax[0]);
+  }
+
+ /* Clipping of non-variance scalars
+  * And first clippings for the variances
+  * (usually 0 for min and +grand for max) */
+
+  cs_lnum_t iclmax[1] = {0};
+  cs_lnum_t iclmin[1] = {0};
+
+  const cs_real_t scminp = cs_field_get_key_double(f, kscmin);
+  const cs_real_t scmaxp = cs_field_get_key_double(f, kscmax);
+
+  if (scmaxp > scminp) {
+    for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++) {
+      if (cvar_scal[c_id] > scmaxp) {
+        iclmax[0] += 1;
+        cvar_scal[c_id] = scmaxp;
+      }
+      if (cvar_scal[c_id] < scminp) {
+         iclmin[0] += 1;
+         cvar_scal[c_id] = scminp;
+      }
+    }
+  }
+
+  /* Clipping of max of variances
+   * Based on associated scalar values (or 0 at min.) */
+  /* Clipping of variances */
+
+  if (variance_id > -1) {
+
+    iclmax[0] = 0;
+    iclmin[0] = 0;
+    const int iclvfl = cs_field_get_key_int(f, kclvfl);
+
+    if (iclvfl == 1) {
+
+      /* Get the corresponding scalar */
+      cs_field_t *fl = cs_field_by_id(variance_id);
+      cs_real_t *cvar_scav = fl->val;
+
+      /* Get the min clipping of the corresponding scalar */
+      const cs_real_t scmin = cs_field_get_key_double(fl, kscmin);
+      const cs_real_t scmax = cs_field_get_key_double(fl, kscmax);
+
+      for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++) {
+        const cs_real_t vfmax = (cvar_scav[c_id]-scmin)*(scmax-cvar_scav[c_id]);
+        if (cvar_scal[c_id] > vfmax) {
+          iclmax[0] += 1;
+          cvar_scal[c_id] = vfmax;
+        }
+      }
+    }
+
+  }
+
+  /* Save counts and extrema for delayed (batched) global reduction */
+
+  cs_log_iteration_clipping_field(f->id,
+                                  iclmin[0],
+                                  iclmax[0],
+                                  vmin,
+                                  vmax,
+                                  iclmin,
+                                  iclmax);
+}
+
+/*----------------------------------------------------------------------------*/
+
+END_C_DECLS

src/atmo/compute_gaseous_chemistry.f90
@@ -59,6 +59,7 @@ use mesh
 use field
 use dimens
 use atchem
+use cs_c_bindings
 
 implicit none
 

src/base/Makefile.am
@@ -162,6 +162,7 @@ cs_restart_map.h \
 cs_rotation.h \
 cs_runaway_check.h \
 cs_sat_coupling.h \
+cs_scalar_clipping.h \
 cs_search.h \
 cs_selector.h \
 cs_sort.h \
@@ -254,7 +255,6 @@ altycl.f90 \
 calhyd.f90 \
 caltri.f90 \
 cs_mass_source_terms.c \
-clpsca.f90 \
 clptrg.f90 \
 clptur.f90 \
 clsyvt.f90 \
@@ -342,6 +342,7 @@ cs_restart_map.c \
 cs_rotation.c \
 cs_runaway_check.c \
 cs_sat_coupling.c \
+cs_scalar_clipping.c\
 cs_search.c \
 cs_selector.c \
 cs_selector_f2c.f90 \

src/base/clpsca.f90
@@ -1,178 +1,0 @@
-!-------------------------------------------------------------------------------
-
-! This file is part of code_saturne, a general-purpose CFD tool.
-!
-! Copyright (C) 1998-2022 EDF S.A.
-!
-! This program is free software; you can redistribute it and/or modify it under
-! the terms of the GNU General Public License as published by the Free Software
-! Foundation; either version 2 of the License, or (at your option) any later
-! version.
-!
-! This program is distributed in the hope that it will be useful, but WITHOUT
-! ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-! FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-! details.
-!
-! You should have received a copy of the GNU General Public License along with
-! this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
-! Street, Fifth Floor, Boston, MA 02110-1301, USA.
-
-!-------------------------------------------------------------------------------
-
-!> \file clpsca.f90
-!> \brief This subroutine clips the values of a given scalar or variance.
-!>
-!-------------------------------------------------------------------------------
-
-!-------------------------------------------------------------------------------
-! Arguments
-!______________________________________________________________________________.
-!  mode           name          role                                           !
-!______________________________________________________________________________!
-!> \param[in]     iscal         scalar number
-!_______________________________________________________________________________
-
-subroutine clpsca &
- ( iscal )
-
-!===============================================================================
-! Module files
-!===============================================================================
-
-use paramx
-use numvar
-use entsor
-use optcal
-use dimens
-use cstphy
-use cstnum
-use parall
-use field
-use cs_c_bindings
-use mesh
-
-!===============================================================================
-
-implicit none
-
-! Arguments
-
-integer          iscal
-
-! Local variables
-
-integer          ivar, iel, iflid, iclvfl
-integer          iclmax(1), iclmin(1), iiscav
-integer          kscmin, kscmax, f_id, kclvfl
-double precision vmin(1), vmax(1), vfmax
-double precision scmax, scmin
-double precision scmaxp, scminp
-
-double precision, dimension(:), pointer :: cvar_scal, cvar_scav
-
-!===============================================================================
-
-!===============================================================================
-! 1. Initialisation
-!===============================================================================
-
-! --- Numero de variable de calcul et de post associe au scalaire traite
-ivar   = isca(iscal)
-iflid  = ivarfl(ivar)
-
-! --- Numero du scalaire eventuel associe dans le cas fluctuation
-iiscav = iscavr(iscal)
-
-! Map field arrays
-call field_get_val_s(ivarfl(ivar), cvar_scal)
-
-! Key ids for clipping
-call field_get_key_id("min_scalar_clipping", kscmin)
-call field_get_key_id("max_scalar_clipping", kscmax)
-call field_get_key_id("variance_clipping", kclvfl)
-
-!===============================================================================
-! 2. Printings and clippings
-!===============================================================================
-
-! Variances are always clipped at positive values
-
-! Compute min. and max. values
-vmin(1) = cvar_scal(1)
-vmax(1) = cvar_scal(1)
-do iel = 1, ncel
-  vmin(1) = min(vmin(1),cvar_scal(iel))
-  vmax(1) = max(vmax(1),cvar_scal(iel))
-enddo
-
-! Clipping of non-variance scalars
-! And first clippings for the variances (usually 0 for min and +grand for max)
-
-iclmax(1) = 0
-iclmin(1) = 0
-
-! Get the min clipping
-call field_get_key_double(iflid, kscmin, scminp)
-call field_get_key_double(iflid, kscmax, scmaxp)
-
-if (scmaxp.gt.scminp)then
-  do iel = 1, ncel
-    if(cvar_scal(iel).gt.scmaxp)then
-      iclmax(1) = iclmax(1) + 1
-      cvar_scal(iel) = scmaxp
-    endif
-    if(cvar_scal(iel).lt.scminp)then
-      iclmin(1) = iclmin(1) + 1
-      cvar_scal(iel) = scminp
-    endif
-  enddo
-endif
-
-! Clipping of max of variances
-! Based on associated scalar values (or 0 at min.)
-
-if (iiscav.ne.0) then
-
-  call field_get_key_int(iflid, kclvfl, iclvfl)
-
-  if (iclvfl.eq.1) then
-
-    ! Get the corresponding scalar
-    f_id = ivarfl(isca(iiscav))
-    call field_get_val_s(f_id, cvar_scav)
-
-    ! Get the min clipping of the corresponding scalar
-    call field_get_key_double(f_id, kscmin, scmin)
-    call field_get_key_double(f_id, kscmax, scmax)
-
-    ! Clipping of variances
-    iclmax(1) = 0
-    iclmin(1) = 0
-
-    do iel = 1, ncel
-      vfmax = (cvar_scav(iel)-scmin)*(scmax-cvar_scav(iel))
-      if(cvar_scal(iel).gt.vfmax) then
-        iclmax(1) = iclmax(1) + 1
-        cvar_scal(iel) = vfmax
-      endif
-    enddo
-
-  endif
-
-endif
-
-call log_iteration_clipping_field(iflid, iclmin(1), iclmax(1), &
-                                  vmin, vmax, iclmin(1), iclmax(1))
-
-!--------
-! Formats
-!--------
-
-!----
-! End
-!----
-
-return
-
-end subroutine

src/base/cs_base_headers.h
@@ -95,6 +95,7 @@
 #include "cs_rotation.h"
 #include "cs_runaway_check.h"
 #include "cs_sat_coupling.h"
+#include "cs_scalar_clipping.h"
 #include "cs_selector.h"
 #include "cs_syr_coupling.h"
 #include "cs_thermal_model.h"

src/base/cs_c_bindings.f90
@@ -343,6 +343,17 @@ module cs_c_bindings
 
     !---------------------------------------------------------------------------
 
+    ! Scalar clipping
+
+    subroutine cs_f_scalar_clipping(f_id)  &
+      bind(C, name='cs_f_scalar_clipping')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      integer(c_int), intent(in), value :: f_id
+    end subroutine cs_f_scalar_clipping
+
+    !---------------------------------------------------------------------------
+
     ! Temporal and z-axis interpolation for meteorological profiles
 
     function cs_intprf(nprofz, nproft, profz, proft,              &
@@ -5528,5 +5539,28 @@ contains
   end subroutine atmo_chemistry_set_aerosol_file_name
 
   !=============================================================================
+  !> brief Clipping scalar field.
+  ! \param[in]   iscal
 
-  end module cs_c_bindings
+  subroutine clpsca(iscal)
+
+    use, intrinsic :: iso_c_binding
+    use numvar
+    implicit none
+
+    ! Arguments
+    integer, intent(in) :: iscal
+
+    ! Local variables
+
+    integer(c_int) :: f_id
+
+    f_id = ivarfl(isca(iscal))
+
+    call cs_f_scalar_clipping(f_id)
+
+  end subroutine clpsca
+
+ !=============================================================================
+
+end module cs_c_bindings

src/base/cs_scalar_clipping.h
@@ -1,0 +1,75 @@
+#ifndef __CS_SCALAR_CLIPPING_H__
+#define __CS_SCALAR_CLIPPING_H__
+
+/*============================================================================
+ * Clip scalar (i.e. convected/diffused) fields.
+ *============================================================================*/
+
+/*
+  This file is part of code_saturne, a general-purpose CFD tool.
+
+  Copyright (C) 1998-2022 EDF S.A.
+
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation; either version 2 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+  Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+ * Standard C library headers
+ *----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+ * Local headers
+ *----------------------------------------------------------------------------*/
+
+#include "cs_base.h"
+
+/*----------------------------------------------------------------------------*/
+
+BEGIN_C_DECLS
+
+/*============================================================================
+ * Macro definitions
+ *============================================================================*/
+
+/*============================================================================
+ * Local type definitions
+ *============================================================================*/
+
+/*=============================================================================
+ * Global variables
+ *============================================================================*/
+
+/*============================================================================
+ * Public function prototypes
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Clip a scalar variable field.
+ *
+ * \param[in]  f  pointer to field structure
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_scalar_clipping(cs_field_t  *f);
+
+/*----------------------------------------------------------------------------*/
+
+END_C_DECLS
+
+#endif /* __CS_SCALAR_CLIPPING_H__ */

commit a7777a828905120d29157bd942ab4e1a05f44877

Fix bug introduced in commit d1260f95. Prov. by Sanz Souhait C.
The number of clipped cells for the min of variances
was systematically set to 0 in the logs, giving wrong
informations.

Cherry-picked from commit 4252409c from master.

src/base/cs_scalar_clipping.c
@@ -206,11 +206,11 @@ cs_scalar_clipping(cs_field_t  *f)
 
   if (variance_id > -1) {
 
-    iclmax[0] = 0;
-    iclmin[0] = 0;
     const int iclvfl = cs_field_get_key_int(f, kclvfl);
 
     if (iclvfl == 1) {
+
+      iclmax[0] = 0;
 
       /* Get the corresponding scalar */
       cs_field_t *fl = cs_field_by_id(variance_id);