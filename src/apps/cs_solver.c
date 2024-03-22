e66d558ffee2fc7a46eac463918a7bb1736ff6d9
Yvan Fournier <yvan.fournier@noreply.edf.fr>
2024-03-22 02:03:20 +0100

    Missing initialization for OpenMP offload.

--- src/apps/cs_solver.c
+++ src/apps/cs_solver.c
@@ -212,6 +212,9 @@ _run(void)
 #if defined(HAVE_CUDA)
   cs_base_cuda_select_default_device();
 #endif
+#if defined(HAVE_OPENMP_TARGET)
+  cs_omp_target_select_default_device();
+#endif
 
   if (cs_get_device_id() < 0)
     cs_halo_set_buffer_alloc_mode(CS_ALLOC_HOST);
