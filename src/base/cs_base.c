code-saturne/code_saturne

commit fc7ee0dbe6157f65d5b319b7afb3fb543615961b

Use common map for bft_mem and cs_base_accel allocated memory.
- The logging enabled by the CS_MEM_LOG variable now also handles
  host/device allocations.
- A few fixes were needed in CUDA code for handling of the non USM
  (i.e. host/device) mode.

src/base/cs_base.c
@@ -74,6 +74,7 @@
 #include "bft_mem.h"
 #include "bft_printf.h"
 
+#include "cs_base_accel.h"
 #include "cs_file.h"
 #include "cs_fp_exception.h"
 #include "cs_log.h"
@@ -1663,6 +1664,8 @@ cs_base_mem_init(void)
 #if defined(HAVE_ACCEL)
     else
       bft_mem_init(NULL);
+
+    bft_mem_alternative_set(cs_realloc_hd, cs_free_hd);
 #endif
 
     cs_glob_base_bft_mem_init = true;

src/alge/cs_multigrid.c
@@ -2927,8 +2927,12 @@ _multigrid_v_cycle(cs_multigrid_t       *mg,
     _aux_vectors = wr + wr_size;
     _aux_r_size -= wr_size;
   }
-  else
-    CS_MALLOC_HD(wr, wr_size, cs_real_t, amode);
+  else {
+    if (amode <= CS_ALLOC_HOST)
+      BFT_MALLOC(wr, wr_size, cs_real_t);
+    else
+      CS_MALLOC_HD(wr, wr_size, cs_real_t, CS_ALLOC_HOST_DEVICE_SHARED);
+  }
 
   /* map arrays for rhs and vx;
      for the finest level, simply point to input and output arrays */

src/alge/cs_sles_it.c
@@ -4550,7 +4550,7 @@ cs_sles_it_solve(void                *context,
         CS_FREE_HD(_vx);
       }
       else if (amode_vx < CS_ALLOC_HOST_DEVICE_SHARED)
-        cs_sync_d2h(_vx);
+        cs_sync_d2h(vx);
 
       CS_FREE_HD(_rhs_w);
     }

src/alge/cs_sles_it_cuda.cu
@@ -979,13 +979,10 @@ cs_sles_it_cuda_jacobi(cs_sles_it_t              *c,
   cs_alloc_mode_t amode_rhs = cs_check_device_ptr(rhs);
 
   if (amode_vx == CS_ALLOC_HOST_DEVICE_SHARED)
-    cudaMemPrefetchAsync(vx, vec_size, device_id, stream);
+    cudaMemPrefetchAsync(vx, vec_size, device_id, stream_pf);
 
   if (amode_rhs == CS_ALLOC_HOST_DEVICE_SHARED)
     cudaMemPrefetchAsync(rhs, vec_size, device_id, stream_pf);
-  else if (amode_rhs == CS_ALLOC_HOST) {
-    cudaMemPrefetchAsync(rhs, vec_size, device_id, stream_pf);
-  }
 
   const cs_real_t  *restrict ad
     =  cs_get_device_ptr_const(cs_matrix_get_diagonal(a));

src/apps/cs_solver.c
@@ -675,13 +675,6 @@ _run(void)
 
   cs_base_time_summary();
 
-#if defined(HAVE_ACCEL)
-  int n_alloc_hd_remain = cs_get_n_allocations_hd();
-  if (n_alloc_hd_remain > 0)
-    bft_printf(_("Warning: %d remaining host-device allocations\n"
-                 "         (possible memory leak)\n"), n_alloc_hd_remain);
-#endif
-
   cs_base_mem_finalize();
 
   cs_log_printf_flush(CS_LOG_N_TYPES);

src/base/cs_base_accel.h
@@ -125,7 +125,7 @@ cs_free_hd(_ptr, #_ptr, __FILE__, __LINE__), _ptr = NULL
  */
 
 #define CS_FREE(_ptr) \
-cs_free(_ptr, #_ptr, __FILE__, __LINE__), _ptr = NULL
+cs_free_hd(_ptr, #_ptr, __FILE__, __LINE__), _ptr = NULL
 
 /*----------------------------------------------------------------------------*/
 
@@ -134,27 +134,6 @@ BEGIN_C_DECLS
 /*============================================================================
  * Type definitions
  *============================================================================*/
-
-/*----------------------------------------------------------------------------
- * Variable value type.
- *----------------------------------------------------------------------------*/
-
-/*!
- * Allocation modes for accelerated code.
- */
-
-typedef enum {
-
-  CS_ALLOC_HOST,                /*!< allocation on host only */
-  CS_ALLOC_HOST_DEVICE,         /*!< allocation on host and device */
-  CS_ALLOC_HOST_DEVICE_PINNED,  /*!< allocation on host and device,
-                                  using page-locked memory on host
-                                  if possible */
-  CS_ALLOC_HOST_DEVICE_SHARED,  /*!< allocation on host and device,
-                                  using mapped/shared memory */
-  CS_ALLOC_DEVICE               /*!< allocation on device only */
-
-} cs_alloc_mode_t;
 
 /*=============================================================================
  * Global variable definitions
@@ -341,41 +320,6 @@ cs_free_hd(void         *ptr,
            const char   *var_name,
            const char   *file_name,
            int           line_num)
-{
-  bft_mem_free(ptr, var_name, file_name, line_num);
-}
-
-#endif
-
-/*----------------------------------------------------------------------------*/
-/*!
- * \brief Free memory on host and device for a given pointer.
- *
- * Compared to \cs_free_hd, this function also allows freeing memory
- * allocated through BFT_MEM_MALLOC / bft_mem_malloc.
- *
- * \param [in]  ptr        pointer to free
- * \param [in]  var_name   allocated variable name string
- * \param [in]  file_name  name of calling source file
- * \param [in]  line_num   line number in calling source file
- */
-/*----------------------------------------------------------------------------*/
-
-#if defined(HAVE_ACCEL)
-
-void
-cs_free(void        *ptr,
-        const char  *var_name,
-        const char  *file_name,
-        int          line_num);
-
-#else
-
-inline static void
-cs_free(void         *ptr,
-        const char   *var_name,
-        const char   *file_name,
-        int           line_num)
 {
   bft_mem_free(ptr, var_name, file_name, line_num);
 }
@@ -858,55 +802,6 @@ cs_copy_d2d(void        *dest,
             size_t       size);
 
 #endif /* defined(HAVE_ACCEL) */
-
-/*----------------------------------------------------------------------------*/
-/*!
- * \brief Return number of host-device allocations
- *
- * \returns current number of host-device allocations.
- */
-/*----------------------------------------------------------------------------*/
-
-#if defined(HAVE_ACCEL)
-
-int
-cs_get_n_allocations_hd(void);
-
-#else
-
-static inline int
-cs_get_n_allocations_hd(void)
-{
-  return 0;
-}
-
-#endif
-
-/*----------------------------------------------------------------------------*/
-/*!
- * \brief Check if a given host pointer is allocated with associated with
- *        cs_alloc_hd or cs_realloc_hd.
- *
- * \returns allocated memory size, or zero if not allocated with this
- *          mechanism.
- */
-/*----------------------------------------------------------------------------*/
-
-#if defined(HAVE_ACCEL)
-
-size_t
-cs_get_allocation_hd_size(void  *host_ptr);
-
-#else
-
-static inline size_t
-cs_get_allocation_hd_size(void  *host_ptr)
-{
-  CS_UNUSED(host_ptr);
-  return 0;
-}
-
-#endif
 
 #if defined(HAVE_OPENMP_TARGET)
 

src/bft/Makefile.am
@@ -28,6 +28,7 @@ AM_CPPFLAGS = \
 -I$(top_srcdir)/src/base \
 $(MPI_CPPFLAGS)
 AM_CFLAGS = $(CFLAGS_DBG) $(CFLAGS_OPT) $(CFLAGS_SHARED)
+AM_CXXFLAGS = $(CXXFLAGS_STD) $(CXXFLAGS_SYCL) $(CXXFLAGS_DBG) $(CXXFLAGS_OPT) $(CXXFLAGS_SHARED)
 AM_LDFLAGS =
 
 # Public header files (to be installed)
@@ -46,7 +47,7 @@ noinst_LIBRARIES = libbft.a
 libbft_a_SOURCES  = \
 bft_backtrace.c \
 bft_error.c \
-bft_mem.c \
+bft_mem.cpp \
 bft_mem_usage.c \
 bft_printf.c
 
@@ -54,3 +55,8 @@ libbft_a_LIBADD =
 
 clean-local:
 	-rm -rf .deps
+
+if HAVE_CUDA_CPP
+.cpp.$(OBJEXT):
+	$(NVCC) $(DEFS) $(AM_CPPFLAGS) -I$(top_srcdir)/src/alge -I../.. $(NVCCFLAGS) $(NVCCFLAGS_DBG) $(NVCCFLAGS_OPT) $(NVCCFLAGS_CPP) -c -o $@ $<
+endif

commit 76aeb82e0a3c40649823a17a300069d43df59240

Fix build with Intel compiler, and possible bug, broken by fc7ee0db.

src/base/cs_base.c
@@ -74,7 +74,6 @@
 #include "bft_mem.h"
 #include "bft_printf.h"
 
-#include "cs_base_accel.h"
 #include "cs_file.h"
 #include "cs_fp_exception.h"
 #include "cs_log.h"
@@ -1664,8 +1663,6 @@ cs_base_mem_init(void)
 #if defined(HAVE_ACCEL)
     else
       bft_mem_init(NULL);
-
-    bft_mem_alternative_set(cs_realloc_hd, cs_free_hd);
 #endif
 
     cs_glob_base_bft_mem_init = true;

src/base/cs_base_accel.cxx
@@ -81,6 +81,8 @@
 #pragma omp requires unified_shared_memory
 #endif
 
+static bool _initialized = false;
+
 /*! Default "host+device" allocation mode */
 /*----------------------------------------*/
 
@@ -121,6 +123,55 @@ sycl::queue   cs_glob_sycl_queue;
 /*============================================================================
  * Private function definitions
  *============================================================================*/
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Reallocate memory on host and device for ni elements of size bytes.
+ *
+ * This function calls the appropriate reallocation function based on
+ * the requested mode, and allows introspection of the allocated memory.
+ *
+ * \param [in]  host_ptr   host pointer
+ * \param [in]  ni         number of elements
+ * \param [in]  size       element size
+ * \param [in]  var_name   allocated variable name string
+ * \param [in]  file_name  name of calling source file
+ * \param [in]  line_num   line number in calling source file
+ *
+ * \returns pointer to allocated memory.
+ */
+/*----------------------------------------------------------------------------*/
+
+static void *
+_realloc_host(void            *host_ptr,
+              size_t           ni,
+              size_t           size,
+              const char      *var_name,
+              const char      *file_name,
+              int              line_num)
+{
+  return cs_realloc_hd(host_ptr,
+                       CS_ALLOC_HOST,
+                       ni, size,
+                       var_name, file_name, line_num);
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Initialize memory mapping on device.
+ */
+/*----------------------------------------------------------------------------*/
+
+static void
+_initialize(void)
+{
+  bft_mem_alternative_set(_realloc_host, cs_free_hd);
+
+  _initialized = true;
+
+  if (cs_get_device_id() < 0)
+    cs_alloc_mode = CS_ALLOC_HOST;
+}
 
 #if defined(SYCL_LANGUAGE_VERSION)
 
@@ -435,6 +486,9 @@ cs_malloc_hd(cs_alloc_mode_t   mode,
              const char       *file_name,
              int               line_num)
 {
+  if (_initialized == false)
+   _initialize();
+
   if (ni == 0)
     return nullptr;
 
@@ -571,6 +625,9 @@ cs_realloc_hd(void            *ptr,
               const char      *file_name,
               int              line_num)
 {
+  if (_initialized == false)
+   _initialize();
+
   void *ret_ptr = ptr;
   size_t new_size = ni*size;
 