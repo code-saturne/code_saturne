code-saturne/code_saturne

commit 7167da730f1c0ad74d75cc97c7076ed4b1e74915

Print values of editable notebook variables to listing file
- Editable notebook variables are printed by default
- New flag in GUI to select printed notebook variables
- Notebook default values are not stored in xml file

python/code_saturne/model/NotebookModel.py
@@ -70,6 +70,7 @@ class NotebookModel(Model):
         default['oturns']      = "No"
         default['editable']    = "No"
         default['restart']     = "Yes"
+        default['log']         = "No"
         default['description'] = ""
 
         return default
@@ -150,6 +151,7 @@ class NotebookModel(Model):
                 value  = content[1]
                 oturns = None
                 read   = None
+                log    = None
                 descr  = None
 
                 if len(content) >= 3:
@@ -163,7 +165,12 @@ class NotebookModel(Model):
                     read = "Yes"
 
                 if len(content) >= 5:
-                    descr = content[4]
+                    log = content[4]
+                else:
+                    log = "No"
+
+                if len(content) >= 6:
+                    descr = content[5]
                 else:
                     descr = ""
 
@@ -172,6 +179,7 @@ class NotebookModel(Model):
                     node['value']       = value
                     node['oturns']      = oturns
                     node['read']        = read
+                    node['log']         = log
                     node['description'] = descr
                 else:
                     self.addVariable()
@@ -180,6 +188,7 @@ class NotebookModel(Model):
                     self.setVariableName(idx-1, var)
                     self.setVariableOt(idx-1, oturns)
                     self.setVariableRestart(idx-1, read)
+                    self.setVariableLog(idx-1, log)
 
 
     @Variables.noUndo
@@ -280,6 +289,12 @@ class NotebookModel(Model):
         """
         node = self.node_note.xmlInitChildNode("var", id = idx)
         node['editable'] = editable
+        node['_editable'] = self.defaultNotebookValues()['editable']
+
+        # editable parameter are printed in log file by default
+        if editable:
+            node['log'] = editable
+            node['_log'] = self.defaultNotebookValues()['log']
 
 
     @Variables.noUndo
@@ -305,6 +320,38 @@ class NotebookModel(Model):
         """
         node = self.node_note.xmlInitChildNode("var", id = idx)
         node['restart'] = restart
+        node['_restart'] = self.defaultNotebookValues()['restart']
+
+
+    @Variables.noUndo
+    def getVariableLog(self, idx):
+        """
+        Return Yes or No to indicate if the parameter value will be printed
+        in the default log file.
+        """
+        node = self.node_note.xmlInitChildNode("var", id = idx)
+        log = node['log']
+        if not log:
+            # editable parameter are printed in log file by default
+            editable = node['editable']
+            if editable:
+                log = editable
+            else:
+                log = self.defaultNotebookValues()['log']
+            self.setVariableLog(idx, log)
+
+        return log
+
+
+    @Variables.undoGlobal
+    def setVariableLog(self, idx, log):
+        """
+        Set Yes or No to indicate if the parameter value will be printed
+        in the default log file.
+        """
+        node = self.node_note.xmlInitChildNode("var", id = idx)
+        node['log'] = log
+        node['_log'] = self.defaultNotebookValues()['log']
 
 
     @Variables.noUndo

python/code_saturne/gui/case/NotebookView.py
@@ -73,13 +73,15 @@ class item_class(object):
     '''
     custom data object
     '''
-    def __init__(self, idx, name, value, oturns_var, editable, restart, description):
+    def __init__(self, idx, name, value, oturns_var, editable, restart, log,
+                 description):
         self.index  = idx
         self.name   = name
         self.value  = value
         self.oturns = oturns_var
         self.edit   = editable
         self.read   = restart
+        self.log    = log
         self.descr  = description
 
     def __repr__(self):
@@ -114,7 +116,7 @@ class TreeItem(object):
 
 
     def columnCount(self):
-        return 6
+        return 7
 
 
     def data(self, column, role):
@@ -135,6 +137,8 @@ class TreeItem(object):
             elif column == 4 and role == Qt.DisplayRole:
                 return self.item.read
             elif column == 5 and role == Qt.DisplayRole:
+                return self.item.log
+            elif column == 6 and role == Qt.DisplayRole:
                 return self.item.descr
         return None
 
@@ -175,7 +179,7 @@ class VariableStandardItemModel(QAbstractItemModel):
         if parent and parent.isValid():
             return parent.internalPointer().columnCount()
         else:
-            return 6
+            return 7
 
 
     def data(self, index, role):
@@ -201,6 +205,8 @@ class VariableStandardItemModel(QAbstractItemModel):
             elif index.column() == 4:
                 return self.tr("Read at restart")
             elif index.column() == 5:
+                return self.tr("Print to default log file")
+            elif index.column() == 6:
                 return self.tr("Description")
 
         # Display
@@ -231,16 +237,18 @@ class VariableStandardItemModel(QAbstractItemModel):
     def headerData(self, section, orientation, role):
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
             if section == 0:
-                return self.tr("variable name")
+                return self.tr("Variable name")
             elif section == 1:
-                return self.tr("value")
+                return self.tr("Value")
             elif section == 2:
-                return self.tr("OpenTurns Variable")
+                return self.tr("OpenTurns\nVariable")
             elif section == 3:
                 return self.tr("Editable")
             elif section == 4:
-                return self.tr("Read at restart")
+                return self.tr("Read at\nrestart")
             elif section == 5:
+                return self.tr("Print to default\nlog file")
+            elif section == 6:
                 return self.tr("Description")
         return None
 
@@ -299,8 +307,9 @@ class VariableStandardItemModel(QAbstractItemModel):
             oturns = self.mdl.getVariableOt(idx)
             edit   = self.mdl.getVariableEditable(idx)
             read   = self.mdl.getVariableRestart(idx)
+            log    = self.mdl.getVariableLog(idx)
             descr  = self.mdl.getVariableDescription(idx)
-            item = item_class(idx, cname, value, oturns, edit, read, descr)
+            item = item_class(idx, cname, value, oturns, edit, read, log, descr)
             new_item = TreeItem(item, cname, parentItem)
             parentItem.appendChild(new_item)
 
@@ -327,6 +336,9 @@ class VariableStandardItemModel(QAbstractItemModel):
             editable = from_qvariant(value, to_text_string)
             item.item.edit = editable
             self.mdl.setVariableEditable(item.item.index, item.item.edit)
+            # Editable variable are automatically printed in default log file
+            item.item.log = editable
+            self.mdl.setVariableLog(item.item.index, item.item.edit)
 
         elif index.column() == 4:
             restart = from_qvariant(value, to_text_string)
@@ -334,6 +346,11 @@ class VariableStandardItemModel(QAbstractItemModel):
             self.mdl.setVariableRestart(item.item.index, item.item.read)
 
         elif index.column() == 5:
+            log = from_qvariant(value, to_text_string)
+            item.item.log = log
+            self.mdl.setVariableLog(item.item.index, item.item.log)
+
+        elif index.column() == 6:
             description = from_qvariant(value, to_text_string)
             item.item.descr = description
             self.mdl.setVariableDescription(item.item.index, item.item.descr)
@@ -395,15 +412,22 @@ class NotebookView(QWidget, Ui_NotebookForm):
 
         RestartOptions = ["Yes","No"]
         restartDelegate = ComboDelegate(self.treeViewNotebook,
-                                         opts_list=RestartOptions)
+                                        opts_list=RestartOptions)
         self.treeViewNotebook.setItemDelegateForColumn(4, restartDelegate)
 
+        LogOptions = ["No","Yes"]
+        logDelegate = ComboDelegate(self.treeViewNotebook,
+                                    opts_list=LogOptions)
+        self.treeViewNotebook.setItemDelegateForColumn(5, logDelegate)
+
         descriptionDelegate = LabelDelegate(self.treeViewNotebook)
-        self.treeViewNotebook.setItemDelegateForColumn(5, descriptionDelegate)
+        self.treeViewNotebook.setItemDelegateForColumn(6, descriptionDelegate)
 
         self.treeViewNotebook.resizeColumnToContents(0)
         self.treeViewNotebook.resizeColumnToContents(2)
+        self.treeViewNotebook.resizeColumnToContents(4)
         self.treeViewNotebook.resizeColumnToContents(5)
+        self.treeViewNotebook.resizeColumnToContents(6)
 
         # Connections
         self.toolButtonAdd.clicked.connect(self.slotAddVariable)

src/base/cs_log_iteration.c
@@ -54,6 +54,7 @@
 #include "cs_map.h"
 #include "cs_mesh.h"
 #include "cs_mesh_location.h"
+#include "cs_notebook.h"
 #include "cs_parall.h"
 #include "cs_parameters.h"
 #include "cs_prototypes.h"
@@ -1695,6 +1696,8 @@ cs_log_iteration(void)
 
   cs_fan_log_iteration();
   cs_ctwr_log_balance();
+
+  cs_notebook_log();
 }
 
 /*----------------------------------------------------------------------------*/

src/base/cs_notebook.c
@@ -90,6 +90,8 @@ typedef struct {
 
   bool        restart;    /* Can the value be read at restart */
 
+  bool        log;        /* Print */
+
 } _cs_notebook_entry_t;
 
 /*============================================================================
@@ -147,6 +149,8 @@ _entry_by_name(const char *name)
  * \param[in] uncertain  flag (int) indicating if it is an uncertain input/output
  * \param[in] editable   flag (bool) indicating if the value can be modified
  * \param[in] restart    flag (bool) indicating if the value is read at restart
+ * \param[in] log        flag (bool) indicating if the value is printed in
+                         default log file
  *
  * \return a _cs_notebook_entry_t pointer
  */
@@ -156,7 +160,8 @@ static _cs_notebook_entry_t *
 _entry_create(const char  *name,
               int          uncertain,
               bool         editable,
-              bool         restart)
+              bool         restart,
+              bool         log)
 {
   size_t l = strlen(name);
 
@@ -220,6 +225,7 @@ _entry_create(const char  *name,
 
   e->editable  = editable;
   e->restart   = restart;
+  e->log       = log;
 
   return e;
 }
@@ -295,6 +301,7 @@ cs_notebook_load_from_file(void)
     const char *c_val  = cs_tree_node_get_tag(n, "value");
     const char *c_edit = cs_tree_node_get_tag(n, "editable");
     const char *c_read = cs_tree_node_get_tag(n, "restart");
+    const char *c_log  = cs_tree_node_get_tag(n, "log");
 
     if (d == NULL)
       d = na;
@@ -322,6 +329,12 @@ cs_notebook_load_from_file(void)
       if (strcmp(c_read, "No") == 0)
         restart = false;
 
+    /* Editable values are printed by default */
+    bool log = editable;
+    if (c_log != NULL)
+      if (strcmp(c_log, "No") == 0)
+        log = false;
+
     /* If the variable is uncertain then :
      * - an input cannot be modifed, hence editable = false
      * - an output has to be modified by the code, hence editable=true
@@ -337,7 +350,11 @@ cs_notebook_load_from_file(void)
       editable = uncertain;
     }
 
-    _cs_notebook_entry_t *e = _entry_create(name, uncertain, editable, restart);
+    _cs_notebook_entry_t *e = _entry_create(name,
+                                            uncertain,
+                                            editable,
+                                            restart,
+                                            log);
 
     _entry_set_description(e, d);
     cs_real_t val = atof(c_val);
@@ -590,6 +607,7 @@ cs_notebook_log_setup(void)
                        "    uncertain:    %d\n"
                        "    editable:     %d\n"
                        "    restart:      %d\n"
+                       "    log:          %d\n"
                        "    value:        %f\n"),
                   i,
                   _entries[i]->name,
@@ -597,10 +615,77 @@ cs_notebook_log_setup(void)
                   _entries[i]->uncertain,
                   _entries[i]->editable,
                   _entries[i]->restart,
+                  _entries[i]->log,
                   _entries[i]->val);
 
   cs_log_printf(l, "\n");
   cs_log_separator(l);
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Print values of notebook variables to default log file
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_notebook_log(void)
+{
+  if (_n_entries == 0)
+    return;
+
+  static size_t _max_size_name = 0;
+  static bool log_needed = false;
+  static char prefix[4] = "n  ";
+
+  /* Verify at first iteration that at least one variable need to be printed
+     in default log file. Also compute the maximum length of names */
+  if (_max_size_name < 1)
+    for (int i = 0; i < _n_entries; i++) {
+      log_needed = _entries[i]->log;
+      _max_size_name = CS_MAX(_max_size_name, strlen(_entries[i]->name));
+    }
+
+  if (log_needed) {
+    char tmp_s[2][64] =  {"", ""};
+
+    /* Print title */ 
+    cs_log_printf(CS_LOG_DEFAULT,
+                  _("\n"
+                    "  ** Notebook editable values\n"
+                    "     ------------------------\n"));
+ 
+    cs_log_strpad(tmp_s[0], _("Name"), _max_size_name, 64);
+    cs_log_strpadl(tmp_s[1], _("value"), 14, 64);
+    cs_log_printf(CS_LOG_DEFAULT,
+                  "\n   %s  %s\n",
+                  tmp_s[0], tmp_s[1]);
+
+    /* Underline title */
+    size_t n_cols = 2;
+    for (size_t col = 0; col < n_cols; col++) {
+      size_t i;
+      size_t w0 = (col == 0) ? _max_size_name : 14;
+      for (i = 0; i < w0; i++)
+        tmp_s[col][i] = '-';
+      tmp_s[col][w0] = '\0';
+    }
+
+    /* Loop on all variables and print required values */
+    cs_log_printf(CS_LOG_DEFAULT,
+                  "-  %s  %s\n",
+                  tmp_s[0], tmp_s[1]);
+
+    for (int i = 0; i < _n_entries; i++)
+      if (_entries[i]->log) {
+        cs_log_strpad(tmp_s[0], _entries[i]->name, _max_size_name, 64);
+        cs_log_printf(CS_LOG_DEFAULT,
+                      "%s%s  %14.5g\n",
+                      prefix, tmp_s[0], _entries[i]->val);
+      }
+
+  }
+
 }
 
 /*----------------------------------------------------------------------------*/

src/base/cs_notebook.h
@@ -187,6 +187,15 @@ void
 cs_notebook_log_setup(void);
 
 /*----------------------------------------------------------------------------*/
+/*!
+ * \brief Print values of editable notebook variables to default log  file
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_notebook_log(void);
+
+/*----------------------------------------------------------------------------*/
 /*
  * \brief Number of notebook variables
  *

commit bcf3c3d0618c4d97c9d639d6bfb5c02cd9047cbd

[GUI] Fix notebook variables attributes save to xml file (broken in commit 7167da730)
  * The use of the hidden attributes function was wrongly done. The
    hidden attribute only needs to be used *if* the value is equal to
    the default value, otherwise it should be removed.
  * The fix contains some code refactoring to handle this behavior for
    all public functions in one private function.

python/code_saturne/model/NotebookModel.py
@@ -244,6 +244,36 @@ class NotebookModel(Model):
         node['value'] = val
 
 
+    @Variables.undoGlobal
+    def _setVariableAttr(self, idx, a, val):
+        """
+        Set value of attribute, set default value if not available.
+        """
+        node = self.node_note.xmlInitChildNode("var", id=idx)
+        node[a] = val
+
+        # If default value set the hidden value for cleanup
+        if val == self.defaultNotebookValues()[a]:
+            node["_{}".format(a)] = val
+        else:
+            node.xmlDelAttribute("_{}".format(a))
+
+
+    @Variables.noUndo
+    def _getVariableAttr(self, idx, a):
+        """
+        Get value of attribute, set default value if not available.
+        """
+        node = self.node_note.xmlInitChildNode("var", id=idx)
+
+        retval = node[a]
+        if not retval:
+            retval = self.defaultNotebookValues()[a]
+            self._setVariableAttr(idx, a, retval)
+
+        return retval
+
+
     @Variables.noUndo
     def getVariableOt(self, idx):
         """
@@ -273,11 +303,7 @@ class NotebookModel(Model):
         """
         Return Yes or No to indicate if the parameter value can be modified.
         """
-        node = self.node_note.xmlInitChildNode("var", id = idx)
-        editable = node['editable']
-        if not editable:
-            editable = self.defaultNotebookValues()['editable']
-            self.setVariableEditable(idx, editable)
+        editable = self._getVariableAttr(idx, 'editable')
 
         return editable
 
@@ -287,14 +313,11 @@ class NotebookModel(Model):
         """
         Set Yes or No to indicate if the parameter value can be modified.
         """
-        node = self.node_note.xmlInitChildNode("var", id = idx)
-        node['editable'] = editable
-        node['_editable'] = self.defaultNotebookValues()['editable']
+        self._setVariableAttr(idx, 'editable', editable)
 
         # editable parameter are printed in log file by default
         if editable:
-            node['log'] = editable
-            node['_log'] = self.defaultNotebookValues()['log']
+            self._setVariableAttr(idx, 'log', editable)
 
 
     @Variables.noUndo
@@ -303,11 +326,7 @@ class NotebookModel(Model):
         Return Yes or No to indicate if the parameter value will be read
         when reading a restart file.
         """
-        node = self.node_note.xmlInitChildNode("var", id = idx)
-        restart = node['restart']
-        if not restart:
-            restart = self.defaultNotebookValues()['restart']
-            self.setVariableRestart(idx, restart)
+        restart = self._getVariableAttr(idx, 'restart')
 
         return restart
 
@@ -318,9 +337,7 @@ class NotebookModel(Model):
         Set Yes or No to indicate if the parameter value will be read
         when reading a restart file.
         """
-        node = self.node_note.xmlInitChildNode("var", id = idx)
-        node['restart'] = restart
-        node['_restart'] = self.defaultNotebookValues()['restart']
+        self._setVariableAttr(idx, 'restart', restart)
 
 
     @Variables.noUndo
@@ -349,9 +366,7 @@ class NotebookModel(Model):
         Set Yes or No to indicate if the parameter value will be printed
         in the default log file.
         """
-        node = self.node_note.xmlInitChildNode("var", id = idx)
-        node['log'] = log
-        node['_log'] = self.defaultNotebookValues()['log']
+        self._setVariableAttr(idx, 'log', log)
 
 
     @Variables.noUndo