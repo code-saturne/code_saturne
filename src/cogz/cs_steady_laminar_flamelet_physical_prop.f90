code-saturne/code_saturne

commit 79f9c300718e1382b3635ebce92bc1b17d30ca1a

Translate scalai.f90 to C (cs_solve_transported_variables)

src/cogz/cs_steady_laminar_flamelet_physical_prop.f90
@@ -102,6 +102,18 @@ logical          update_rad
 integer, save :: ntcabs_prev = -1
 logical, save :: is_vploop = .false.
 
+interface
+
+  subroutine max_mid_min_progvar(zm0, cmax, cmid, cmin)  &
+    bind(C, name='cs_f_max_mid_min_progvar')
+    use, intrinsic :: iso_c_binding
+    implicit none
+    real(c_double), intent(in)    :: zm0
+    real(c_double), intent(inout) :: cmax, cmid, cmin
+  end subroutine max_mid_min_progvar
+
+end interface
+
 if (ntcabs_prev .eq. ntcabs) then
   is_vploop = .true.
 else
@@ -1017,70 +1029,6 @@ end subroutine
 !>
 !> \brief Specific physic subroutine: diffusion flame.
 !>
-!> Calculation of Min/Max of progress variable as a function of fm
-!-------------------------------------------------------------------------------
-
-subroutine max_mid_min_progvar(zm0, cmax, cmid, cmin)
-
-!===============================================================================
-! Module files
-!===============================================================================
-
-use coincl
-
-implicit none
-
-double precision, dimension(:), allocatable :: xData
-double precision :: zm0, cmax, cmid, cmin
-
-double precision :: weight_zm
-integer :: dataIndex
-
-! Start with z_m
-allocate(xData(nzm))
-xData(:) = flamelet_library(1,1,1,1,:)
-
-if(xData(1).ge.zm0) then
-  cmax = flamelet_library(FLAMELET_C,nki,1,1,1)
-  cmid = flamelet_library(FLAMELET_C,ikimid,1,1,1)
-  cmin = flamelet_library(FLAMELET_C,1,1,1,1)
-elseif(xData(size(xData)).le.zm0) then
-  cmax = flamelet_library(FLAMELET_C,nki,1,1,nzm)
-  cmid = flamelet_library(FLAMELET_C,ikimid,1,1,nzm)
-  cmin = flamelet_library(FLAMELET_C,1,1,1,nzm)
-else
-
-  dataIndex = 1
-  do while(xData(dataIndex).lt.zm0 &
-      .and. dataIndex .lt. size(xData))
-    dataIndex = dataIndex + 1
-  enddo
-  if(dataIndex .gt.1 ) then
-    weight_zm = (zm0 - xData(dataIndex-1))/(xData(dataIndex)-xData(dataIndex-1))
-    cmax = (1.0d0-weight_zm)*flamelet_library(FLAMELET_C,nki,1,1,dataIndex-1) &
-      + weight_zm*flamelet_library(FLAMELET_C,nki,1,1,dataIndex)
-
-    cmid = (1.0d0-weight_zm)*flamelet_library(FLAMELET_C,ikimid,1,1,dataIndex-1) &
-      + weight_zm*flamelet_library(FLAMELET_C,ikimid,1,1,dataIndex)
-
-    cmin = (1.0d0-weight_zm)*flamelet_library(FLAMELET_C,1,1,1,dataIndex-1) &
-      + weight_zm*flamelet_library(FLAMELET_C,1,1,1,dataIndex)
-
-  endif
-endif
-
-deallocate(xData)
-
-end subroutine
-
-!===============================================================================
-! Function:
-! ---------
-
-!> \file cs_steady_laminar_flamelet_physical_prop.f90
-!>
-!> \brief Specific physic subroutine: diffusion flame.
-!>
 !> Calculation of total scalar dissipation rate
 !-------------------------------------------------------------------------------
 
@@ -1217,3 +1165,59 @@ enddo
 end subroutine
 
 
+subroutine max_mid_min_progvar(zm0, cmax, cmid, cmin) &
+    bind(C, name='cs_f_max_mid_min_progvar')
+
+!===============================================================================
+! Module files
+!===============================================================================
+
+use coincl
+
+implicit none
+
+double precision, dimension(:), allocatable :: xData
+real(c_double) :: zm0, cmax, cmid, cmin
+double precision :: weight_zm
+integer :: dataIndex
+
+! Start with z_m
+allocate(xData(nzm))
+xData(:) = flamelet_library(1,1,1,1,:)
+
+if(xData(1).ge.zm0) then
+  cmax = flamelet_library(FLAMELET_C,nki,1,1,1)
+  cmid = flamelet_library(FLAMELET_C,ikimid,1,1,1)
+  cmin = flamelet_library(FLAMELET_C,1,1,1,1)
+elseif(xData(size(xData)).le.zm0) then
+  cmax = flamelet_library(FLAMELET_C,nki,1,1,nzm)
+  cmid = flamelet_library(FLAMELET_C,ikimid,1,1,nzm)
+  cmin = flamelet_library(FLAMELET_C,1,1,1,nzm)
+else
+
+  dataIndex = 1
+  do while(xData(dataIndex).lt.zm0 &
+      .and. dataIndex .lt. size(xData))
+    dataIndex = dataIndex + 1
+  enddo
+  if(dataIndex .gt.1 ) then
+    weight_zm = (zm0 - xData(dataIndex-1))/(xData(dataIndex)-xData(dataIndex-1))
+    cmax = (1.0d0-weight_zm)*flamelet_library(FLAMELET_C,nki,1,1,dataIndex-1) &
+      + weight_zm*flamelet_library(FLAMELET_C,nki,1,1,dataIndex)
+
+    cmid = (1.0d0-weight_zm)*flamelet_library(FLAMELET_C,ikimid,1,1,dataIndex-1) &
+      + weight_zm*flamelet_library(FLAMELET_C,ikimid,1,1,dataIndex)
+
+    cmin = (1.0d0-weight_zm)*flamelet_library(FLAMELET_C,1,1,1,dataIndex-1) &
+      + weight_zm*flamelet_library(FLAMELET_C,1,1,1,dataIndex)
+
+  endif
+endif
+
+deallocate(xData)
+
+end subroutine max_mid_min_progvar
+
+
+
+!end module steady_laminar

src/atmo/compute_gaseous_chemistry.f90
@@ -37,7 +37,8 @@
 !> \param[in]     dt            time step (per cell)
 !_______________________________________________________________________________
 
-subroutine compute_gaseous_chemistry ( dt )
+subroutine compute_gaseous_chemistry ( dt ) &
+  bind(C, name='cs_f_compute_gaseous_chemistry')
 
 !===============================================================================
 ! Module files
@@ -69,7 +70,7 @@ procedure() :: fexchem_1, fexchem_2, fexchem_3, fexchem_4, chem_roschem
 
 ! Arguments
 
-double precision dt(ncelet)
+real(c_double), dimension(*), intent(in) :: dt
 
 ! Local Variables
 

src/atmo/kinrates.f90
@@ -26,7 +26,8 @@
 !> \brief Calls the computation of reaction rates for atmospheric chemistry
 
 !-------------------------------------------------------------------------------
-subroutine kinrates ()
+subroutine kinrates ()  &
+  bind(C, name='cs_f_kinetics_rates_compute')
 
 !===============================================================================
 ! Module files

src/base/Makefile.am
@@ -188,6 +188,7 @@ cs_selector.h \
 cs_solid_zone.h \
 cs_solve_equation.h \
 cs_solve_navier_stokes.h \
+cs_solve_transported_variables.h \
 cs_sort.h \
 cs_sort_partition.h \
 cs_syr_coupling.h \
@@ -385,6 +386,7 @@ cs_selector_f2c.f90 \
 cs_solid_zone.c \
 cs_solve_equation.c \
 cs_solve_navier_stokes.c \
+cs_solve_transported_variables.c \
 cs_sort.c \
 cs_sort_partition.c \
 cs_syr_coupling.c \
@@ -426,7 +428,6 @@ modini.f90 \
 mmtycl.f90 \
 phyvar.f90 \
 pthrbm.f90 \
-scalai.f90 \
 schtmp.f90 \
 tdesi1.f90 \
 tridim.f90 \

src/base/cs_post_util.c
@@ -158,7 +158,7 @@ cs_cell_segment_intersect_select(void        *input,
  * \param[out]  n_cells   number of selected cells
  * \param[out]  cell_ids  array of selected cell ids (0 to n-1 numbering)
  * \param[out]  seg_c_len array of length of the segment in the selected cells
- * \param[out]  seg_c_cen array of center coordinates of the segment in the 
+ * \param[out]  seg_c_cen array of center coordinates of the segment in the
                           selected cells
  */
 /*----------------------------------------------------------------------------*/
@@ -234,10 +234,10 @@ cs_cell_segment_intersect_probes_define(void          *input,
   /* This version is better than cs_cell_segment_intersect_select
      because it gives the cell
      if the segment is included in this cell */
-  cs_cell_polyline_intersect_select(input, 
-                                    2, 
-                                    &n_cells, 
-                                    &cell_ids, 
+  cs_cell_polyline_intersect_select(input,
+                                    2,
+                                    &n_cells,
+                                    &cell_ids,
                                     &seg_c_len,
                                     &seg_c_cen);
 

src/base/cs_post_util.h
@@ -113,7 +113,7 @@ cs_cell_segment_intersect_select(void        *input,
  * \param[out]  n_cells   number of selected cells
  * \param[out]  cell_ids  array of selected cell ids (0 to n-1 numbering)
  * \param[out]  seg_c_len array of length of the segment in the selected cells
- * \param[out]  seg_c_cen array of center coordinates of the segment in the 
+ * \param[out]  seg_c_cen array of center coordinates of the segment in the
                           selected cells
  */
 /*----------------------------------------------------------------------------*/

src/base/cs_probe.c
@@ -669,8 +669,8 @@ _cell_profile_probes_define(void          *input,
 
   cs_mesh_intersect_polyline_cell_select(sx,
                                          2,
-                                         &n_cells, 
-                                         &cell_ids, 
+                                         &n_cells,
+                                         &cell_ids,
                                          &seg_c_len,
                                          &seg_c_cen);
 

src/base/cs_solve_equation.c
@@ -835,52 +835,6 @@ _diffusion_terms_vector(const cs_field_t            *f,
  * Fortran wrapper function definitions
  *============================================================================*/
 
-void
-cs_f_solve_equation_scalar(int              f_id,
-                           cs_lnum_t        ncesmp,
-                           int              iterns,
-                           int              itspdv,
-                           const cs_lnum_t  icetsm[],
-                           int              itypsm[],
-                           cs_real_t        smacel[],
-                           cs_real_t        viscf[],
-                           cs_real_t        viscb[])
-{
-  cs_field_t *f = cs_field_by_id(f_id);
-
-  cs_solve_equation_scalar(f,
-                           ncesmp,
-                           iterns,
-                           itspdv,
-                           icetsm,
-                           itypsm,
-                           smacel,
-                           viscf,
-                           viscb);
-}
-
-void
-cs_f_solve_equation_vector(int              f_id,
-                           cs_lnum_t        ncesmp,
-                           int              iterns,
-                           const cs_lnum_t  icetsm[],
-                           int              itypsm[],
-                           cs_real_t        smacel[],
-                           cs_real_t        viscf[],
-                           cs_real_t        viscb[])
-{
-  cs_field_t *f = cs_field_by_id(f_id);
-
-  cs_solve_equation_vector(f,
-                           ncesmp,
-                           iterns,
-                           icetsm,
-                           itypsm,
-                           smacel,
-                           viscf,
-                           viscb);
-}
-
 /*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
 
 /*============================================================================

commit 2f0f807265dad8e084cd5f61e3beb3a60f4c17a0

Fix compilation with Intel ifx, broken by commit 79f9c300 (yesterday).

src/cogz/cs_steady_laminar_flamelet_physical_prop.f90
@@ -1176,8 +1176,10 @@ use coincl
 
 implicit none
 
+real(c_double), intent(in)    :: zm0
+real(c_double), intent(inout) :: cmax, cmid, cmin
+
 double precision, dimension(:), allocatable :: xData
-real(c_double) :: zm0, cmax, cmid, cmin
 double precision :: weight_zm
 integer :: dataIndex
 