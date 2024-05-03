code-saturne/code_saturne

commit 79f9c300718e1382b3635ebce92bc1b17d30ca1a

Translate scalai.f90 to C (cs_solve_transported_variables)

src/base/cs_solve_equation.c
@@ -835,52 +835,6 @@ _diffusion_terms_vector(const cs_field_t            *f,
  * Fortran wrapper function definitions
  *============================================================================*/
 
-void
-cs_f_solve_equation_scalar(int              f_id,
-                           cs_lnum_t        ncesmp,
-                           int              iterns,
-                           int              itspdv,
-                           const cs_lnum_t  icetsm[],
-                           int              itypsm[],
-                           cs_real_t        smacel[],
-                           cs_real_t        viscf[],
-                           cs_real_t        viscb[])
-{
-  cs_field_t *f = cs_field_by_id(f_id);
-
-  cs_solve_equation_scalar(f,
-                           ncesmp,
-                           iterns,
-                           itspdv,
-                           icetsm,
-                           itypsm,
-                           smacel,
-                           viscf,
-                           viscb);
-}
-
-void
-cs_f_solve_equation_vector(int              f_id,
-                           cs_lnum_t        ncesmp,
-                           int              iterns,
-                           const cs_lnum_t  icetsm[],
-                           int              itypsm[],
-                           cs_real_t        smacel[],
-                           cs_real_t        viscf[],
-                           cs_real_t        viscb[])
-{
-  cs_field_t *f = cs_field_by_id(f_id);
-
-  cs_solve_equation_vector(f,
-                           ncesmp,
-                           iterns,
-                           icetsm,
-                           itypsm,
-                           smacel,
-                           viscf,
-                           viscb);
-}
-
 /*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
 
 /*============================================================================

src/base/cs_solve_transported_variables.h
@@ -1,0 +1,72 @@
+#ifndef __CS_SOLVE_TRANSPORTED_VARIABLES__
+#define __CS_SOLVE_TRANSPORTED_VARIABLES__
+
+/*============================================================================
+ * Resolution of source term convection diffusion equations
+ * for scalars in a time step.
+ *============================================================================*/
+
+/*
+  This file is part of code_saturne, a general-purpose CFD tool.
+
+  Copyright (C) 1998-2024 EDF S.A.
+
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation; either version 2 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+  Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+ * Standard C library headers
+ *----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+ * Local headers
+ *----------------------------------------------------------------------------*/
+
+#include "cs_defs.h"
+
+/*----------------------------------------------------------------------------*/
+
+BEGIN_C_DECLS
+
+/*=============================================================================
+ * Public function prototypes
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------*/
+/*! \file cs_solve_transported_variables.c
+ *
+ * \brief Resolution of source term convection diffusion equations
+ *        for scalars in a time step.
+ */
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/*
+ * \param[in]     nscal         total number of scalars
+ * \param[in]     iterns        Navier-Stokes iteration number
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_solve_transported_variables(int nscal,
+                               int iterns);
+
+/*----------------------------------------------------------------------------*/
+
+END_C_DECLS
+
+#endif /* __CS_SOLVE_TRANSPORTED_VARIABLES__ */

src/base/tridim.f90
@@ -162,6 +162,13 @@ procedure() :: phyvar, pthrbm, schtmp, scalai
 
 interface
 
+  subroutine cs_solve_transported_variables(iterns, nscal)  &
+    bind(C, name='cs_solve_transported_variables')
+    use, intrinsic :: iso_c_binding
+    implicit none
+    integer(kind=c_int), value :: iterns, nscal
+  end subroutine cs_solve_transported_variables
+
   subroutine cs_dilatable_scalar_diff_st(iterns)  &
     bind(C, name='cs_dilatable_scalar_diff_st')
     use, intrinsic :: iso_c_binding
@@ -996,7 +1003,7 @@ do while (iterns.le.nterup)
       if (fluid_solid) call cs_porous_model_set_has_disable_flag(0)
 
       ! Update buoyant scalar(s)
-      call scalai(nscal , iterns , dt)
+      call cs_solve_transported_variables(nscal, iterns);
 
       ! Diffusion terms for weakly compressible algorithm
       if (idilat.ge.4) then
@@ -1306,7 +1313,7 @@ if (nscal.ge.1) then
 
   ! Update non-buoyant scalar(s)
   iterns = -1
-  call scalai(nscal, iterns, dt)
+  call cs_solve_transported_variables(nscal, iterns);
 
   ! Diffusion terms for weakly compressible algorithm
   if (idilat.ge.4) then

src/atmo/compute_gaseous_chemistry.f90
@@ -37,7 +37,8 @@
 !> \param[in]     dt            time step (per cell)
 !_______________________________________________________________________________
 
-subroutine compute_gaseous_chemistry ( dt )
+subroutine compute_gaseous_chemistry ( dt ) &
+  bind(C, name='cs_f_compute_gaseous_chemistry')
 
 !===============================================================================
 ! Module files
@@ -69,7 +70,7 @@ procedure() :: fexchem_1, fexchem_2, fexchem_3, fexchem_4, chem_roschem
 
 ! Arguments
 
-double precision dt(ncelet)
+real(c_double), dimension(*), intent(in) :: dt
 
 ! Local Variables
 

src/atmo/kinrates.f90
@@ -26,7 +26,8 @@
 !> \brief Calls the computation of reaction rates for atmospheric chemistry
 
 !-------------------------------------------------------------------------------
-subroutine kinrates ()
+subroutine kinrates ()  &
+  bind(C, name='cs_f_kinetics_rates_compute')
 
 !===============================================================================
 ! Module files

src/base/Makefile.am
@@ -188,6 +188,7 @@ cs_selector.h \
 cs_solid_zone.h \
 cs_solve_equation.h \
 cs_solve_navier_stokes.h \
+cs_solve_transported_variables.h \
 cs_sort.h \
 cs_sort_partition.h \
 cs_syr_coupling.h \
@@ -385,6 +386,7 @@ cs_selector_f2c.f90 \
 cs_solid_zone.c \
 cs_solve_equation.c \
 cs_solve_navier_stokes.c \
+cs_solve_transported_variables.c \
 cs_sort.c \
 cs_sort_partition.c \
 cs_syr_coupling.c \
@@ -426,7 +428,6 @@ modini.f90 \
 mmtycl.f90 \
 phyvar.f90 \
 pthrbm.f90 \
-scalai.f90 \
 schtmp.f90 \
 tdesi1.f90 \
 tridim.f90 \

src/base/cs_post_util.c
@@ -158,7 +158,7 @@ cs_cell_segment_intersect_select(void        *input,
  * \param[out]  n_cells   number of selected cells
  * \param[out]  cell_ids  array of selected cell ids (0 to n-1 numbering)
  * \param[out]  seg_c_len array of length of the segment in the selected cells
- * \param[out]  seg_c_cen array of center coordinates of the segment in the 
+ * \param[out]  seg_c_cen array of center coordinates of the segment in the
                           selected cells
  */
 /*----------------------------------------------------------------------------*/
@@ -234,10 +234,10 @@ cs_cell_segment_intersect_probes_define(void          *input,
   /* This version is better than cs_cell_segment_intersect_select
      because it gives the cell
      if the segment is included in this cell */
-  cs_cell_polyline_intersect_select(input, 
-                                    2, 
-                                    &n_cells, 
-                                    &cell_ids, 
+  cs_cell_polyline_intersect_select(input,
+                                    2,
+                                    &n_cells,
+                                    &cell_ids,
                                     &seg_c_len,
                                     &seg_c_cen);
 

commit b891b00a1f2dfef03c7786782383ab3debe11765

Fix bugs introduced in commit 79f9c300

src/base/cs_solve_equation.c
@@ -93,6 +93,7 @@
 #include "cs_turbulence_rit.h"
 #include "cs_velocity_pressure.h"
 #include "cs_vof.h"
+#include "cs_volume_mass_injection.h"
 #include "cs_wall_condensation.h"
 #include "cs_wall_functions.h"
 
@@ -134,26 +135,7 @@ BEGIN_C_DECLS
 
 extern int cs_glob_darcy_unsteady;
 
-void
-cs_f_get_dimens(int  *nvar,
-                int  *nscal);
-
 /* Use legacy macro type to maintain compatibility with legacy user files */
-
-void
-CS_PROCF(ustssc, USTSSC)(const int        *nvar,
-                         const int        *nscal,
-                         const cs_lnum_t  *ncepdp,
-                         const cs_lnum_t  *ncesmp,
-                         const int        *iscal,
-                         const cs_lnum_t  *icepdc,
-                         const cs_lnum_t  *icetsm,
-                         cs_lnum_t        *itypsm,
-                         const cs_real_t   dt[],
-                         const cs_real_t  *ckupdc,
-                         const cs_real_t  *smacel,
-                         cs_real_t         crvexp[],
-                         cs_real_t         crvimp[]);
 
 /*============================================================================
  * Prototypes for functions intended for use only by Fortran wrappers.
@@ -168,27 +150,6 @@ CS_PROCF(ustssc, USTSSC)(const int        *nvar,
  * Prototypes for functions intended for use only by Fortran wrappers.
  * (descriptions follow, with function bodies).
  *============================================================================*/
-
-void
-cs_f_solve_equation_scalar(int              f_id,
-                           cs_lnum_t        ncesmp,
-                           int              iterns,
-                           int              itspdv,
-                           const cs_lnum_t  icetsm[],
-                           int              itypsm[],
-                           cs_real_t        smacel[],
-                           cs_real_t        viscf[],
-                           cs_real_t        viscb[]);
-
-void
-cs_f_solve_equation_vector(int              f_id,
-                           cs_lnum_t        ncesmp,
-                           int              iterns,
-                           const cs_lnum_t  icetsm[],
-                           int              itypsm[],
-                           cs_real_t        smacel[],
-                           cs_real_t        viscf[],
-                           cs_real_t        viscb[]);
 
 /*============================================================================
  * Private function definitions
@@ -869,8 +830,8 @@ cs_solve_equation_scalar(cs_field_t        *f,
                          int                iterns,
                          int                itspdv,
                          const cs_lnum_t    icetsm[],
-                         int                itypsm[],
-                         cs_real_t          smacel[],
+                         int                itypsm_sc[],
+                         cs_real_t          smacel_sc[],
                          cs_real_t          viscf[],
                          cs_real_t          viscb[])
 {
@@ -975,19 +936,6 @@ cs_solve_equation_scalar(cs_field_t        *f,
                                fimp);
   else
     cs_gui_thermal_source_terms(f, cvar_var, rhs, fimp);
-
-  {
-    int nvar = 0, nscal = 0;
-    cs_lnum_t ncepdp = 0;
-    cs_lnum_t *icepdc = NULL;
-    cs_real_t *ckupdc = NULL;
-
-    cs_f_get_dimens(&nvar, & nscal);
-
-    CS_PROCF(ustssc, USTSSC)(&nvar, &nscal, &ncepdp, &ncesmp, &iscal,
-                             icepdc, icetsm, itypsm, dt,
-                             ckupdc, smacel, rhs, fimp);
-  }
 
   cs_user_source_terms(cs_glob_domain,
                        f->id,
@@ -1408,20 +1356,29 @@ cs_solve_equation_scalar(cs_field_t        *f,
   cs_real_t *w1;
   BFT_MALLOC(w1, n_cells_ext, cs_real_t);
 
-  const int ipr = cs_field_get_key_int(CS_F_(p), keyvar);
   if (ncesmp > 0) {
     cs_real_t *srcmas;
-    int *_itypsm = itypsm + (ivar-1)*ncesmp;
-    cs_real_t *_smacel_ipr = smacel + (ipr-1)*ncesmp;
-    cs_real_t *_smacel_ivar = smacel + (ivar-1)*ncesmp;
+
+    int *_itypsm_ipr = NULL;
+    cs_lnum_t _ncetsm = 0;
+    cs_lnum_t *_icetsm = NULL;
+    cs_real_t *_smacel_ipr = NULL;
+    cs_real_t *_gamma_ipr = NULL;
+
+    cs_volume_mass_injection_get_arrays(CS_F_(p),
+                                        &_ncetsm,
+                                        &_icetsm,
+                                        &_itypsm_ipr,
+                                        &_smacel_ipr,
+                                        &_gamma_ipr);
 
     BFT_MALLOC(srcmas, ncesmp, cs_real_t);
 
     /* When treating the Temperature, the equation is multiplied by Cp */
     for (cs_lnum_t c_id = 0; c_id < ncesmp; c_id++) {
-      if ((_smacel_ipr[c_id] > 0.0) && (_itypsm[c_id] == 1)) {
+      if ((_smacel_ipr[c_id] > 0.0) && (itypsm_sc[c_id] == 1)) {
         const cs_lnum_t id = icetsm[c_id] - 1;
-        srcmas[c_id] = _smacel_ipr[c_id]* xcpp[id];
+        srcmas[c_id] = _smacel_ipr[c_id] * xcpp[id];
       }
       else {
        srcmas[c_id] = 0.;
@@ -1433,10 +1390,10 @@ cs_solve_equation_scalar(cs_field_t        *f,
                          1,
                          ncesmp,
                          icetsm,
-                         _itypsm,
+                         itypsm_sc,
                          cell_f_vol,
                          cvara_var,
-                         _smacel_ivar,
+                         smacel_sc,
                          srcmas,
                          rhs,
                          fimp,
@@ -1881,8 +1838,8 @@ cs_solve_equation_vector(cs_field_t       *f,
                          const cs_lnum_t   ncesmp,
                          int               iterns,
                          const cs_lnum_t   icetsm[],
-                         int               itypsm[],
-                         cs_real_t         smacel[],
+                         int               itypsm_v[],
+                         cs_real_t         smacel_v[],
                          cs_real_t         viscf[],
                          cs_real_t         viscb[])
 {
@@ -1903,9 +1860,6 @@ cs_solve_equation_vector(cs_field_t       *f,
 
   cs_real_3_t *cvar_var = (cs_real_3_t *)f->val;
   cs_real_3_t *cvara_var = (cs_real_3_t *)f->val_pre;
-
-  const int keyvar = cs_field_key_id("variable_id");
-  const int ivar = cs_field_get_key_int(f, keyvar);
 
   /* If the vector is buoyant, it is inside the Navier Stokes loop, and
      so iterns >=1; otherwise it is outside of the loop and iterns = -1. */
@@ -2056,21 +2010,28 @@ cs_solve_equation_vector(cs_field_t       *f,
     cs_real_3_t *gavinj;
     BFT_MALLOC(gavinj, n_cells_ext, cs_real_3_t);
 
-    const int ipr = cs_field_get_key_int(CS_F_(p), keyvar);
+    int *_itypsm_ipr = NULL;
+    cs_lnum_t _ncetsm = 0;
+    cs_lnum_t *_icetsm = NULL;
+    cs_real_t *_smacel_ipr = NULL;
+    cs_real_t *_gamma_ipr = NULL;
 
-    int *_itypsm = itypsm + (ivar-1)*ncesmp;
-    cs_real_t *_smacel_ipr = smacel + (ipr-1)*ncesmp;
-    cs_real_t *_smacel_ivar = smacel + (ivar-1)*ncesmp;
+    cs_volume_mass_injection_get_arrays(CS_F_(p),
+                                        &_ncetsm,
+                                        &_icetsm,
+                                        &_itypsm_ipr,
+                                        &_smacel_ipr,
+                                        &_gamma_ipr);
 
     /* We increment SMBRV by -Gamma RTPA and FIMP by Gamma */
     cs_mass_source_terms(1,
                          3,
                          ncesmp,
                          icetsm,
-                         _itypsm,
+                         itypsm_v,
                          cell_f_vol,
                          (const cs_real_t *)cvara_var,
-                         _smacel_ivar,
+                         smacel_v,
                          _smacel_ipr,
                          (cs_real_t *)rhs,
                          (cs_real_t *)fimp,

src/base/cs_solve_transported_variables.c
@@ -141,14 +141,12 @@ static bool _initialized = false;
  * \brief Resolution of source term convection diffusion equations
  *        for scalars in a time step.
  *
- * \param[in]     nscal         total number of scalars
  * \param[in]     iterns        Navier-Stokes iteration number
  */
 /*----------------------------------------------------------------------------*/
 
 void
-cs_solve_transported_variables(int nscal,
-                               int iterns)
+cs_solve_transported_variables(int iterns)
 {
   const cs_mesh_t *m = cs_glob_mesh;
   const cs_mesh_quantities_t *fvq = cs_glob_mesh_quantities;
@@ -313,7 +311,7 @@ cs_solve_transported_variables(int nscal,
         int itspdv = -1;
         if (iscavr == -1)
           itspdv = 0;
-        else if (iscavr > 0 && iscavr < nscal)
+        else if (iscavr > 0)
           itspdv = 1;
         else {
 
@@ -322,7 +320,6 @@ cs_solve_transported_variables(int nscal,
             "    ========\n"
             "    Scalar name = %s, id = %d\n"
             "    iscavr must be stricly positive or -1 integer\n"
-            "      and lower or equal than nscal = %d\n"
             "    its value is %d\n"
             "\n\n"
             "  If iscavr(I) = -1, the scalar I is not a variance\n"
@@ -331,7 +328,7 @@ cs_solve_transported_variables(int nscal,
             "    whose number is iscavr(I)\n"
             "\n"
             "  Check parameters"), __func__,
-                    f_scal->name, f_scal->id, nscal, iscavr);
+                    f_scal->name, f_scal->id, iscavr);
         }
 
         /* Specific process BC for gas combustion: steady laminar flamelet */
@@ -469,7 +466,7 @@ cs_solve_transported_variables(int nscal,
 
             /* Readjust electric variables j, j.E (and Pot, E) */
             if (   cs_glob_elec_option->ielcor == 1
-                 && cs_get_glob_time_step()->nt_cur > 1)
+                && cs_get_glob_time_step()->nt_cur > 1)
               cs_elec_scaling_function(m, fvq, dt);
 
           }
@@ -544,7 +541,7 @@ cs_solve_transported_variables(int nscal,
 
     if (iscavr == -1)
       itspdv = 0;
-    else if (iscavr > 0 && iscavr < nscal)
+    else if (iscavr > 0)
       itspdv = 1;
     else {
       bft_error(__FILE__, __LINE__, 0,
@@ -552,7 +549,6 @@ cs_solve_transported_variables(int nscal,
             "    ========\n"
             "    Scalar name = %s, id = %d\n"
             "    iscavr must be stricly positive or -1 integer\n"
-            "      and lower or equal than nscal = %d\n"
             "    its value is %d\n"
             "\n\n"
             "  If iscavr(I) = -1, the scalar I is not a variance\n"
@@ -561,7 +557,7 @@ cs_solve_transported_variables(int nscal,
             "    whose number is iscavr(I)\n"
             "\n"
             "  Check parameters"), __func__,
-            f_scal->name, f_scal->id, nscal, iscavr);
+            f_scal->name, f_scal->id, iscavr);
     }
 
     int *itypsm = NULL;

src/base/cs_solve_transported_variables.h
@@ -56,14 +56,12 @@ BEGIN_C_DECLS
 
 /*----------------------------------------------------------------------------*/
 /*
- * \param[in]     nscal         total number of scalars
  * \param[in]     iterns        Navier-Stokes iteration number
  */
 /*----------------------------------------------------------------------------*/
 
 void
-cs_solve_transported_variables(int nscal,
-                               int iterns);
+cs_solve_transported_variables(int iterns);
 
 /*----------------------------------------------------------------------------*/
 

src/base/tridim.f90
@@ -162,11 +162,11 @@ procedure() :: phyvar, pthrbm, schtmp
 
 interface
 
-  subroutine cs_solve_transported_variables(iterns, nscal)  &
+  subroutine cs_solve_transported_variables(iterns)  &
     bind(C, name='cs_solve_transported_variables')
     use, intrinsic :: iso_c_binding
     implicit none
-    integer(kind=c_int), value :: iterns, nscal
+    integer(kind=c_int), value :: iterns
   end subroutine cs_solve_transported_variables
 
   subroutine cs_dilatable_scalar_diff_st(iterns)  &
@@ -1003,7 +1003,7 @@ do while (iterns.le.nterup)
       if (fluid_solid) call cs_porous_model_set_has_disable_flag(0)
 
       ! Update buoyant scalar(s)
-      call cs_solve_transported_variables(nscal, iterns);
+      call cs_solve_transported_variables(iterns);
 
       ! Diffusion terms for weakly compressible algorithm
       if (idilat.ge.4) then
@@ -1313,7 +1313,7 @@ if (nscal.ge.1) then
 
   ! Update non-buoyant scalar(s)
   iterns = -1
-  call cs_solve_transported_variables(nscal, iterns);
+  call cs_solve_transported_variables(iterns);
 
   ! Diffusion terms for weakly compressible algorithm
   if (idilat.ge.4) then