edb454c0900f33a632b7852abaef5cf8f693d0dd
Samuel Gaucher <samuel.gaucher@edf.fr>
2024-05-21 14:11:47 +0200

    [IBM] Add porosity threshold for the convection scheme

--- src/base/cs_porosity_from_scan.h
+++ src/base/cs_porosity_from_scan.h
@@ -83,6 +83,7 @@ typedef struct {
   cs_lnum_t *source_c_ids;
   cs_lnum_t threshold;
   cs_real_t porosity_threshold;
+  cs_real_t convection_porosity_threshold;
 } cs_porosity_from_scan_opt_t;
 
 /*============================================================================
