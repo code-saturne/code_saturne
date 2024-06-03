5c5cfcf130a5063dbdc7466b4df9ca7f59eb3b38
Martin Ferrand <martin.ferrand@edf.fr>
2024-06-03 11:58:15 +0200

    [Atmo] make compatible constant density and meteo profiles.

--- src/atmo/cs_atmo.c
+++ src/atmo/cs_atmo.c
@@ -3510,10 +3510,13 @@ cs_atmo_hydrostatic_profiles_compute(void)
     if (idilat > 0)
       temp->val[cell_id] =   potemp->val[cell_id]
                            * pow((f->val[cell_id]/pref), rscp);
-    density->val[cell_id] = f->val[cell_id] / (rair * temp->val[cell_id]);
+    if (cs_glob_physical_model_flag[CS_ATMOSPHERIC]
+        != CS_ATMO_CONSTANT_DENSITY)
+      density->val[cell_id] = f->val[cell_id] / (rair * temp->val[cell_id]);
   }
 
-  if (has_restart == 1)
+  if (has_restart == 1 || cs_glob_physical_model_flag[CS_ATMOSPHERIC]
+        == CS_ATMO_CONSTANT_DENSITY)
     return;
 
   /* Boussinesq hypothesis */
