16963a8d1d27dc44a2c0017d9c24345eb606c823
Jérôme Bonelle <jerome.bonelle@edf.fr>
2024-08-02 14:29:10 +0200

    Fix compilation (commit 927629f3)

--- src/atmo/cs_atmo.c
+++ src/atmo/cs_atmo.c
@@ -3488,7 +3488,7 @@ cs_atmo_hydrostatic_profiles_compute(void)
         != CS_ATMO_CONSTANT_DENSITY)
       density->val[cell_id] = f->val[cell_id] / (rair * temp->val[cell_id]);
     else
-      density->val[cell_id] = phys_pro->ro0
+      density->val[cell_id] = phys_pro->ro0;
   }
 
   if (has_restart == 1 || cs_glob_physical_model_flag[CS_ATMOSPHERIC]
