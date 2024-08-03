code-saturne/code_saturne

commit 2b8a9e97cad19f3d72e86974fde6d56127f23221

Enable possibility for user to save the rhs terms in turbulent scalar fluxes equation

src/turb/cs_turbulence_rit.c
@@ -173,6 +173,36 @@ _thermal_flux_st(const char          *name,
   if (f_ut_prod != NULL)
       prod_ut = (cs_real_3_t *)f_ut_prod->val;
 
+  cs_real_3_t *phi_ut = NULL;
+  cs_field_t *f_phi_ut = cs_field_by_composite_name_try(
+      "algo:turbulent_flux_scrambling", f->name);
+  if (f_phi_ut != NULL)
+    phi_ut = (cs_real_3_t *)f_phi_ut->val;
+
+  cs_real_3_t *prod_by_vel_grad_ut = NULL;
+  cs_field_t *f_ut_prod_by_vel = cs_field_by_composite_name_try(
+      "algo:turbulent_flux_production_by_velocity_gradient", f->name);
+  if (f_ut_prod_by_vel != NULL)
+      prod_by_vel_grad_ut = (cs_real_3_t *)f_ut_prod_by_vel->val;
+
+  cs_real_3_t *prod_by_scal_grad_ut = NULL;
+  cs_field_t *f_ut_prod_by_scal = cs_field_by_composite_name_try(
+      "algo:turbulent_flux_production_by_scalar_gradient", f->name);
+  if (f_ut_prod_by_scal != NULL)
+      prod_by_scal_grad_ut = (cs_real_3_t *)f_ut_prod_by_scal->val;
+
+  cs_real_3_t *buo_ut = NULL;
+  cs_field_t *f_buo_ut = cs_field_by_composite_name_try(
+      "algo:turbulent_flux_buoyancy", f->name);
+  if (f_buo_ut != NULL)
+    buo_ut = (cs_real_3_t *)f_buo_ut->val;
+
+  cs_real_3_t *dissip_ut = NULL;
+  cs_field_t *f_dissip_ut_ut = cs_field_by_composite_name_try(
+      "algo:turbulent_flux_dissipation", f->name);
+  if (f_dissip_ut_ut != NULL)
+    dissip_ut = (cs_real_3_t *)f_buo_ut->val;
+
   if (turb_flux_model == 31) {
     char fname[128];
     snprintf(fname, 128, "%s_alpha", f->name); fname[127] = '\0';
@@ -266,10 +296,10 @@ _thermal_flux_st(const char          *name,
 
        /* Pressure/thermal fluctuation correlation term
         * --------------------------------------------- */
-
-       rhs_ut[c_id][i] +=   cell_f_vol[c_id] * crom[c_id]
-                          * (      alpha  * phiith[i]
-                             + (1.-alpha) * phiitw[i]);
+       const cs_real_t press_correl_i =       alpha  * phiith[i]
+                                        + (1.-alpha) * phiitw[i];
+       if (f_phi_ut != NULL) /* Save it if needed */
+         phi_ut[c_id][i] = press_correl_i;
 
        cs_real_t imp_term
          =   cell_f_vol[c_id] * crom[c_id]
@@ -281,34 +311,45 @@ _thermal_flux_st(const char          *name,
        /* Production terms
         *----------------- */
 
-       cs_real_t ept = 0;
-
-       /* Production term due to the mean velocity */
-       ept -= cs_math_3_dot_product(gradv[c_id][i],
-                                    xuta[c_id]);
+       /* Production term due to the mean velcoity */
+       const cs_real_t prod_by_vel_grad_i =
+         - cs_math_3_dot_product(gradv[c_id][i], xuta[c_id]);
+       if (prod_by_vel_grad_ut != NULL) /* Save it if needed */
+         prod_by_vel_grad_ut[c_id][i] = prod_by_vel_grad_i;
 
        /* Production term due to the mean temperature */
-       ept -= (  xrij[i][0]*gradt[c_id][0]
-               + xrij[i][1]*gradt[c_id][1]
-               + xrij[i][2]*gradt[c_id][2]);
+      const cs_real_t prod_by_scal_grad_i =  - (   xrij[i][0]*gradt[c_id][0]
+                                           + xrij[i][1]*gradt[c_id][1]
+                                           + xrij[i][2]*gradt[c_id][2]);
+       if (prod_by_scal_grad_ut != NULL) /* Save it if needed */
+         prod_by_scal_grad_ut[c_id][i] = prod_by_scal_grad_i;
 
        /* Production term due to the gravity */
+       cs_real_t buoyancy_i = 0.;
        if ((cvar_tt != NULL) && (cpro_beta != NULL)
            && rans_mdl->has_buoyant_term == 1)
-         ept -= grav[i] * cpro_beta[c_id] * cvara_tt[c_id];
+         buoyancy_i = -grav[i] * cpro_beta[c_id] * cvara_tt[c_id];
+
+       if (buo_ut != NULL) /* Save it if needed */
+         buo_ut[c_id][i] = buoyancy_i;
 
        /* Dissipation (Wall term only because "h" term is zero */
-       ept -=   (1.-alpha)/xttdrbw
-              * (  xxc2 * xuta[c_id][i]
-                 + xxc3 * (  xuta[c_id][0]*xnal[0]*xnal[i]
-                           + xuta[c_id][1]*xnal[1]*xnal[i]
-                           + xuta[c_id][2]*xnal[2]*xnal[i]));
-
-       rhs_ut[c_id][i] += ept * cell_f_vol[c_id]*crom[c_id];
+       const cs_real_t dissip_i =  (1.-alpha)/xttdrbw
+                                 * (  xxc2 * xuta[c_id][i]
+                                    + xxc3 * (  xuta[c_id][0]*xnal[0]*xnal[i]
+                                              + xuta[c_id][1]*xnal[1]*xnal[i]
+                                              + xuta[c_id][2]*xnal[2]*xnal[i]));
+       if (dissip_ut != NULL)/* Save it if needed */
+         dissip_ut[c_id][i] = dissip_i;
 
        /* Save production terms for post-processing */
        if (prod_ut != NULL)
-         prod_ut[c_id][i] = ept;
+         prod_ut[c_id][i] = prod_by_vel_grad_i + prod_by_scal_grad_i
+                          + buoyancy_i - dissip_i;
+
+       rhs_ut[c_id][i] += (  prod_by_vel_grad_i + prod_by_scal_grad_i
+                           + buoyancy_i - dissip_i)
+                         * cell_f_vol[c_id]*crom[c_id];
 
        /* TODO we can implicit more terms */
        imp_term =   cell_f_vol[c_id] * crom[c_id]

src/base/cs_solve_equation.c
@@ -306,6 +306,9 @@ _production_and_dissipation_terms(const cs_field_t  *f,
   /* Production Term
      --------------- */
 
+  cs_field_t *f_produc = cs_field_by_composite_name_try("algo:production",
+                                                        f->name);
+
   /* NB: diffusivity is clipped to 0 because in LES, it might be negative.
    * Problematic
    * for the variance even if it is strange to use variance and LES...
@@ -319,15 +322,24 @@ _production_and_dissipation_terms(const cs_field_t  *f,
         = (const cs_real_3_t *)cs_field_by_composite_name(f_fm->name,
                                                           "turbulent_flux")->val;
       for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++) {
-        cpro_st[c_id] -=   2 * xcpp[c_id] * cell_f_vol[c_id] * crom[c_id]
-                         * cs_math_3_dot_product(grad[c_id], xut[c_id]);
+        const cs_real_t prod = - 2 * cs_math_3_dot_product(grad[c_id],
+                                                           xut[c_id]);
+        if (f_produc != NULL )
+          f_produc->val[c_id] = prod;
+        cpro_st[c_id] += xcpp[c_id] * cell_f_vol[c_id] * crom[c_id]
+                         *  prod;
+
       }
     }
     /* SGDH model */
     else {
       for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++) {
-        cpro_st[c_id] +=   2 * xcpp[c_id] * sgdh_diff[c_id] * cell_f_vol[c_id]
+        const cs_real_t prod = 2 * sgdh_diff[c_id] / crom[c_id]
                          * cs_math_3_dot_product(grad[c_id], grad[c_id]);
+        if (f_produc != NULL )
+          f_produc->val[c_id] = prod;
+        cpro_st[c_id] += xcpp[c_id] * cell_f_vol[c_id] * crom[c_id]
+                         *  prod;
       }
     }
   }
@@ -343,7 +355,9 @@ _production_and_dissipation_terms(const cs_field_t  *f,
         const cs_real_t cprovol = xcpp[c_id] * cell_f_vol[c_id] * crom[c_id];
         /* Special time stepping to ensure positivity of the variance */
         const cs_real_t prod = -2*cs_math_3_dot_product(grad[c_id], xut[c_id]);
-        rhs[c_id] += cs_math_fmax(prod*cprovol, 0);
+        if (f_produc != NULL )
+          f_produc->val[c_id] = prod;
+        rhs[c_id] += cs_math_fmax(prod * cprovol, 0.);
 
         /* Implicit "production" term when negative, but check if the
          * variance is non-zero */
@@ -357,8 +371,12 @@ _production_and_dissipation_terms(const cs_field_t  *f,
     /* SGDH model */
     else {
       for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++) {
-        rhs[c_id] +=   2 * xcpp[c_id] * sgdh_diff[c_id] * cell_f_vol[c_id]
-                       * cs_math_3_dot_product(grad[c_id], grad[c_id]);
+        const cs_real_t prod =   2 * sgdh_diff[c_id] / crom[c_id]
+                            * cs_math_3_dot_product(grad[c_id], grad[c_id]);
+        if (f_produc != NULL )
+          f_produc->val[c_id] = prod;
+        rhs[c_id] +=   xcpp[c_id] * cell_f_vol[c_id] * crom[c_id]
+                       * prod;
       }
     }
 
@@ -374,6 +392,9 @@ _production_and_dissipation_terms(const cs_field_t  *f,
 
   /* Dissipation term
      ---------------- */
+
+  cs_field_t *f_dissip = cs_field_by_composite_name_try("algo:dissip",
+                                                        f->name);
 
   cs_real_6_t *cvara_rij = NULL;
   cs_real_t *cvara_k= NULL, *cvara_ep = NULL, *cvara_omg= NULL, *cvar_al = NULL;
@@ -431,11 +452,15 @@ _production_and_dissipation_terms(const cs_field_t  *f,
       prdtl /= visls_0;
 
     const cs_real_t xr = (1.0 - alpha_theta)*prdtl + alpha_theta*rvarfl;
-    const cs_real_t rhovst = xcpp[c_id]*crom[c_id]*xe/(xk*xr)*cell_f_vol[c_id];
+    const cs_real_t cprovol = xcpp[c_id] * crom[c_id] * cell_f_vol[c_id];
+    const cs_real_t dissip_freq = xe / (xk * xr);
+    const cs_real_t dissip = dissip_freq * cvara_var[c_id];
+    if (f_dissip != NULL)
+      f_dissip->val[c_id] = dissip;
     /* The diagonal receives eps/Rk, (*theta possibly) */
-    fimp[c_id] += rhovst*thetap;
+    fimp[c_id] += dissip_freq * cprovol * thetap;
     /* The right hand side receives the dissipation */
-    rhs[c_id] -= rhovst*cvara_var[c_id];
+    rhs[c_id] -= dissip * cprovol;
   }
 
 }

commit 7eb9de5aac74e69ee80b891d2173cb6ed0c96843

[turb] fix bug in turbulent fluxes introduced in 2b8a9e9 (2024-08-02)

src/turb/cs_turbulence_rit.c
@@ -348,7 +348,7 @@ _thermal_flux_st(const char          *name,
                           + buoyancy_i - dissip_i;
 
        rhs_ut[c_id][i] += (  prod_by_vel_grad_i + prod_by_scal_grad_i
-                           + buoyancy_i - dissip_i)
+                           + buoyancy_i + press_correl_i - dissip_i)
                          * cell_f_vol[c_id]*crom[c_id];
 
        /* TODO we can implicit more terms */