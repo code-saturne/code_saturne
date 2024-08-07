code-saturne/code_saturne

commit e8c04db554eb0e819ed7586e7951c588ab506b7f

Multigrid: use optimized smoothers at all levels, but only if consistant.
If a optimized smoother is not available for a requested solver type,
or a convergence test is requires, default to regular solvers.

Since commit 4cc7d077 (2019-04-05), smoothers requiring a convergence test
in addition to a maximum iteration count (not recommended) were not handled
correctly, as they would reach the maximum itetation count.

src/alge/cs_multigrid_smoother.cpp
@@ -2091,7 +2091,8 @@ _p_gauss_seidel(cs_sles_it_t              *c,
  *                             see \ref sles_it for details)
  * \param[in]  n_iter          number of iterations to perform
  *
- * \return a pointer to newly created smoother info object.
+ * \return a pointer to newly created smoother info object, or NULL
+ *         if not available for this solver type.
  */
 /*----------------------------------------------------------------------------*/
 
@@ -2150,7 +2151,7 @@ cs_multigrid_smoother_create(cs_sles_it_type_t    smoother_type,
     break;
 
   default: /* Other iterative solvers are not tuned for smoothing */
-    bft_error(__FILE__, __LINE__, 0, "%s: Invalid smoother.", __func__);
+    return nullptr;
     break;
 
   } /* Smoother type */

src/alge/cs_multigrid.cpp
@@ -266,7 +266,6 @@ typedef struct _cs_multigrid_setup_data_t {
 
   cs_real_t     *pc_aux;                 /* preconditioner auxiliary array */
 
-
 } cs_multigrid_setup_data_t;
 
 /* Grid hierarchy */
@@ -2027,13 +2026,27 @@ _multigrid_setup_sles(cs_multigrid_t  *mg,
       if (   mg->info.type[j+k] != CS_SLES_N_IT_TYPES
           && mg->info.type[j+k] < CS_SLES_N_SMOOTHER_TYPES) {
         cs_mg_sles_t  *mg_sles = &(mgd->sles_hierarchy[i*2 + j]);
-        mg_sles->context
-          = cs_multigrid_smoother_create(mg->info.type[j+k],
-                                         mg->info.poly_degree[j+k],
-                                         mg->info.n_max_iter[j+k]);
-        mg_sles->setup_func = cs_multigrid_smoother_setup;
-        mg_sles->solve_func = cs_multigrid_smoother_solve;
-        mg_sles->destroy_func = cs_sles_it_destroy;
+        mg_sles->context = nullptr;
+        if (mg->info.precision_mult[j] < 0) {
+          mg_sles->context
+            = cs_multigrid_smoother_create(mg->info.type[j+k],
+                                           mg->info.poly_degree[j+k],
+                                           mg->info.n_max_iter[j+k]);
+          if (mg_sles->context != nullptr) {
+            mg_sles->setup_func = cs_multigrid_smoother_setup;
+            mg_sles->solve_func = cs_multigrid_smoother_solve;
+            mg_sles->destroy_func = cs_sles_it_destroy;
+          }
+        }
+        if (mg_sles->context == nullptr) {
+          mg_sles->context
+            = cs_sles_it_create(mg->info.type[j+k],
+                                mg->info.poly_degree[j+k],
+                                mg->info.n_max_iter[j+k],
+                                false); /* stats not updated here */
+          mg_sles->setup_func = cs_sles_it_setup;
+          mg_sles->solve_func = cs_sles_it_solve;
+        }
 
         /* Share context between descent and ascent smoothers if both
            are of the cs_sles_it type */
@@ -2105,14 +2118,29 @@ _multigrid_setup_sles(cs_multigrid_t  *mg,
     mg_lv_info = mg->lv_info + i;
 
     cs_mg_sles_t  *mg_sles = &(mgd->sles_hierarchy[i*2]);
-    mg_sles->context
-      = cs_sles_it_create(mg->info.type[2+k],
-                          mg->info.poly_degree[2+k],
-                          mg->info.n_max_iter[2+k],
-                          false); /* stats not updated here */
-    mg_sles->setup_func = cs_sles_it_setup;
-    mg_sles->solve_func = cs_sles_it_solve;
-    mg_sles->destroy_func = cs_sles_it_destroy;
+
+    mg_sles->context = nullptr;
+    if (mg->info.precision_mult[2] < 0) {
+      mg_sles->context
+        = cs_multigrid_smoother_create(mg->info.type[2+k],
+                                       mg->info.poly_degree[2+k],
+                                       mg->info.n_max_iter[2+k]);
+      if (mg_sles->context != nullptr) {
+        mg_sles->setup_func = cs_multigrid_smoother_setup;
+        mg_sles->solve_func = cs_multigrid_smoother_solve;
+        mg_sles->destroy_func = cs_sles_it_destroy;
+      }
+    }
+    if (mg_sles->context == nullptr) {
+      mg_sles->context
+        = cs_sles_it_create(mg->info.type[2+k],
+                            mg->info.poly_degree[2+k],
+                            mg->info.n_max_iter[2+k],
+                            false); /* stats not updated here */
+      mg_sles->setup_func = cs_sles_it_setup;
+      mg_sles->solve_func = cs_sles_it_solve;
+      mg_sles->destroy_func = cs_sles_it_destroy;
+    }
 
     if (mg->lv_mg[2] != nullptr) {
       cs_sles_pc_t *pc = _pc_create_from_mg_sub(mg->lv_mg[2]);

src/alge/cs_multigrid_smoother.h
@@ -73,7 +73,8 @@ typedef struct _cs_multigrid_smoother_t  cs_multigrid_smoother_t;
  *                             see \ref sles_it for details)
  * \param[in]  n_iter          number of iterations to perform
  *
- * \return a pointer to newly created smoother info object.
+ * \return a pointer to newly created smoother info object, or NULL
+ *         if not available for this solver type.
  */
 /*----------------------------------------------------------------------------*/
 

commit c9a0167da0371366951db8a5a8c3d9799865585a

Multigrid: fix crash introduced in some cases by e8c04db5 (yesterday).

src/alge/cs_multigrid_smoother.cpp
@@ -2256,7 +2256,7 @@ cs_multigrid_smoother_setup(void               *context,
     {
       bool single_reduce = false;
 #if defined(HAVE_MPI)
-      cs_gnum_t n_m_rows = c->setup_data->n_rows;
+      cs_gnum_t n_m_rows = cs_matrix_get_n_rows(a) * diag_block_size;
       if (c->comm != MPI_COMM_NULL) {
         int size;
         cs_gnum_t _n_m_rows;