code-saturne/code_saturne

commit ca157ea72b068afb8ef737e70828772a0615ccc3

Migrate iniva0 from Fortran to C.

src/base/cs_initialize_fields.c
@@ -47,16 +47,24 @@
 #include "cs_ale.h"
 #include "cs_array.h"
 #include "cs_assert.h"
+#include "cs_cf_thermo.h"
 #include "cs_field.h"
 #include "cs_field_default.h"
 #include "cs_field_pointer.h"
+#include "cs_gui_mobile_mesh.h"
+#include "cs_ibm.h"
 #include "cs_math.h"
 #include "cs_mesh.h"
 #include "cs_mesh_quantities.h"
 #include "cs_parall.h"
 #include "cs_physical_constants.h"
+#include "cs_physical_model.h"
+#include "cs_porous_model.h"
+#include "cs_porosity_from_scan.h"
 #include "cs_scalar_clipping.h"
+#include "cs_time_step.h"
 #include "cs_turbulence_init.h"
+#include "cs_velocity_pressure.h"
 #include "cs_vof.h"
 
 /*----------------------------------------------------------------------------
@@ -111,12 +119,182 @@ void
 cs_initialize_fields_stage_0(void)
 {
   const cs_mesh_t *m = cs_glob_mesh;
+  const cs_fluid_properties_t *fluid_props = cs_glob_fluid_properties;
 
   const int n_fields = cs_field_n_fields();
 
   const int keysca = cs_field_key_id("scalar_id");
   const int kscavr = cs_field_key_id("first_moment_id");
   const int kdflim = cs_field_key_id("diffusion_limiter_id");
+  const int kivisl = cs_field_key_id("diffusivity_id");
+  const int kvisl0 = cs_field_key_id("diffusivity_ref");
+
+  /* Initialize temperature to reference temperature if present */
+  if (CS_F_(t) != NULL) {
+    cs_array_real_set_scalar(m->n_cells_with_ghosts,
+                             fluid_props->t0,
+                             CS_F_(t)->val);
+  }
+
+  /* Initialize boundary temperature to "marker" if present */
+  if (CS_F_(t_b) != NULL) {
+    cs_array_real_set_scalar(m->n_b_faces,
+                             cs_math_big_r,
+                             CS_F_(t_b)->val);
+  }
+
+  /* Time step
+     --------- */
+
+  /* dt might be used on the halo cells during the ALE initialization
+     otherwise dt is synchronized in the pressure correction step. */
+
+  cs_array_real_set_scalar(m->n_cells_with_ghosts,
+                           cs_glob_time_step->dt_ref,
+                           CS_F_(dt)->val);
+
+  /* Initialize physical properties
+     ------------------------------ */
+
+  /* Density */
+
+  cs_array_real_set_scalar(m->n_cells_with_ghosts,
+                           fluid_props->ro0,
+                           CS_F_(rho)->val);
+  cs_array_real_set_scalar(m->n_b_faces,
+                           fluid_props->ro0,
+                           CS_F_(rho_b)->val);
+
+  /* Boussinesq (set to impossible value at this stage) */
+  if (cs_glob_velocity_pressure_model->idilat == 0) {
+    cs_array_real_set_scalar(m->n_cells_with_ghosts,
+                             -1.,
+                             cs_field_by_name("thermal_expansion")->val);
+  }
+
+  /* Molecular viscosity */
+  cs_array_real_set_scalar(m->n_cells_with_ghosts,
+                           fluid_props->viscl0,
+                           CS_F_(mu)->val);
+
+  /* Specific heat */
+  if (fluid_props->icp >= 0) {
+    cs_array_real_set_scalar(m->n_cells_with_ghosts,
+                             fluid_props->cp0,
+                             CS_F_(cp)->val);
+  }
+
+  /* The total pressure will be initialized to P0 + rho.g.r in inivar
+     if the user has not done a prior initialization;
+     so mark to "uninit" value here */
+
+  if (cs_glob_physical_model_flag[CS_COMPRESSIBLE] < 0) {
+    cs_array_real_set_scalar(m->n_cells_with_ghosts,
+                             - cs_math_infinite_r,
+                             cs_field_by_name("total_pressure")->val);
+  }
+
+  /* Initialization of mix_mol_mas with default values (air)
+     (used in cs_cf_thermo_default_init) */
+  if (cs_glob_physical_model_flag[CS_GAS_MIX] >= 0) {
+    cs_array_real_set_scalar(m->n_cells_with_ghosts,
+                             fluid_props->xmasmr,
+                             cs_field_by_name("mix_mol_mas")->val);
+  }
+
+  /* Default initialisations for the compressible model */
+
+  if (cs_glob_physical_model_flag[CS_COMPRESSIBLE] >= 0) {
+    /* In compressible, for now, the temperature is not solved but is a field of
+       type variable anyway. The reference value has to be taken into account. */
+    cs_array_real_set_scalar(m->n_cells_with_ghosts,
+                             fluid_props->t0,
+                             CS_F_(t)->val);
+
+    /* Default isochoric specific heat (cv0), total energy and density */
+    cs_cf_thermo_default_init();
+
+    /* Default diffusivity for total energy */
+    double visls_0 = cs_field_get_key_double(CS_F_(t), kvisl0);
+    visls_0 /= fluid_props->cv0;
+    cs_field_set_key_double(CS_F_(e_tot), kvisl0, visls_0);
+  }
+
+  /* Scalars diffusivity */
+
+  for (int f_id = 0; f_id < n_fields; f_id++) {
+    cs_field_t *f = cs_field_by_id(f_id);
+    if (!(f->type & CS_FIELD_VARIABLE) || f->type & CS_FIELD_CDO)
+      continue;
+    int scalar_id = cs_field_get_key_int(f, keysca) - 1;
+    if (scalar_id < 0)
+      continue;
+
+    int ifcvsl = cs_field_get_key_int(f, kivisl);
+    double visls_0 = cs_field_get_key_double(f, kvisl0);
+    /* Diffusivity at current time step (and previous one if second order) */
+    if (ifcvsl >= 0) {
+      cs_field_t *f_vsl = cs_field_by_id(ifcvsl);
+      for (int i = 0; i < f_vsl->n_time_vals; i++) {
+        cs_array_real_set_scalar(m->n_cells_with_ghosts,
+                                 visls_0,
+                                 f_vsl->vals[i]);
+      }
+    }
+  }
+
+  /* Mesh viscosity for ALE */
+
+  if (cs_glob_ale != CS_ALE_NONE) {
+    const cs_equation_param_t *eqp
+      = cs_field_get_equation_param_const(CS_F_(mesh_u));
+    const int idftnp = eqp->idften;
+
+    if (idftnp & CS_ANISOTROPIC_LEFT_DIFFUSION) {
+      cs_real_t ref_val[6] = {1., 1., 1., 0., 0., 0.};
+      cs_array_real_set_value(m->n_cells_with_ghosts,
+                              6,
+                              ref_val,
+                              cs_field_by_name("mesh_viscosity")->val);
+    }
+    else if (idftnp & CS_ISOTROPIC_DIFFUSION) {
+      cs_array_real_set_scalar(m->n_cells_with_ghosts,
+                               1,
+                               cs_field_by_name("mesh_viscosity")->val);
+    }
+
+    cs_gui_mesh_viscosity();
+  }
+
+  /* Porosity */
+
+  if (cs_glob_porous_model > 0) {
+    cs_real_t *porosity = cs_field_by_name("porosity")->val;
+
+    if (cs_glob_porosity_from_scan_opt->compute_porosity_from_scan)
+      cs_array_real_set_scalar(m->n_cells_with_ghosts, 0., porosity);
+
+    else if (cs_glob_porosity_ibm_opt->porosity_mode > 0) {
+      cs_field_t *f_ifp = cs_field_by_name_try("i_face_porosity");
+      cs_field_t *f_bfp = cs_field_by_name_try("b_face_porosity");
+      if (f_ifp != NULL)
+        cs_array_real_set_scalar(m->n_i_faces, 1., f_ifp->val);
+      if (f_bfp != NULL)
+        cs_array_real_set_scalar(m->n_b_faces, 1., f_bfp->val);
+    }
+
+    else
+      cs_array_real_set_scalar(m->n_cells_with_ghosts, 1., porosity);
+
+    /* Tensorial porosity */
+    if (cs_glob_porous_model == 2) {
+      cs_real_t ref_val[6] = {1., 1., 1., 0., 0., 0.};
+      cs_array_real_set_value(m->n_cells_with_ghosts,
+                              6,
+                              ref_val,
+                              cs_field_by_name("tensorial_porosity")->val);
+    }
+  }
 
   /* Reference value for P*
      ---------------------- */

src/base/Makefile.am
@@ -415,7 +415,6 @@ fldvar.f90 \
 fldprp.f90 \
 impini.f90 \
 iniini.f90 \
-iniva0.f90 \
 inivar.f90 \
 majgeo.f90 \
 varpos.f90

src/base/cs_boundary_conditions_set_coeffs.h
@@ -1132,7 +1132,7 @@ cs_boundary_conditions_set_dirichlet_conv_neumann_diff_tensor
 {
   for (int isou = 0; isou < 6; isou++) {
 
-    /* BS test sur hextv ? if (abs(hextv[isou])>rinfin*0.5d0) then */
+    /* BS test on hextv ? if (abs(hextv[isou]) > cs_math_infinite_r * 0.5) */
 
     /* Gradient BCs */
     a[isou] = pimpts[isou];

src/base/cs_setup.c
@@ -588,7 +588,7 @@ _additional_fields_stage_1(void)
     cs_field_t *f = cs_field_by_id(f_id);
     if (!(f->type & CS_FIELD_VARIABLE) || f->type & CS_FIELD_CDO)
       continue;
-    int scalar_id = (ks > -1) ? cs_field_get_key_int(f, ks) : -1;
+    int scalar_id = (ks > -1) ? cs_field_get_key_int(f, ks) -1 : -1;
     if (scalar_id < 0)
       continue;
 
@@ -715,7 +715,7 @@ _additional_fields_stage_1(void)
     cs_field_t *f = cs_field_by_id(f_id);
     if (!(f->type & CS_FIELD_VARIABLE) || f->type & CS_FIELD_CDO)
       continue;
-    int scalar_id = (ks > -1) ? cs_field_get_key_int(f, ks) : -1;
+    int scalar_id = (ks > -1) ? cs_field_get_key_int(f, ks) -1 : -1;
     if (scalar_id < 0)
       continue;
     int ifcvsl = cs_field_get_key_int(f, kivisl);
@@ -919,7 +919,7 @@ _additional_fields_stage_1(void)
     cs_field_t *f = cs_field_by_id(f_id);
     if (!(f->type & CS_FIELD_VARIABLE) || f->type & CS_FIELD_CDO)
       continue;
-    int scalar_id = (ks > -1) ? cs_field_get_key_int(f, ks) : -1;
+    int scalar_id = (ks > -1) ? cs_field_get_key_int(f, ks) -1 : -1;
     if (scalar_id < 0)
       continue;
     int ifcdep = cs_field_get_key_int(f, kromsl);
@@ -1367,7 +1367,7 @@ _additional_fields_stage_1(void)
     cs_field_t *f = cs_field_by_id(f_id);
     if (!(f->type & CS_FIELD_VARIABLE) || f->type & CS_FIELD_CDO)
       continue;
-    int scalar_id = (ks > -1) ? cs_field_get_key_int(f, ks) : -1;
+    int scalar_id = (ks > -1) ? cs_field_get_key_int(f, ks) -1 : -1;
     if (scalar_id < 0)
       continue;
     int f_s_id = cs_field_get_key_int(f, kivisl);

src/base/cs_time_stepping.c
@@ -129,9 +129,6 @@ void
 cs_f_init_chemistry_reacnum(void);
 
 void
-cs_f_iniva0(void);
-
-void
 cs_f_atmo_models_boundary_conditions_map(void);
 
 void
@@ -370,7 +367,6 @@ cs_time_stepping(void)
 
   cs_field_allocate_or_map_all();
 
-  cs_f_iniva0();
   cs_initialize_fields_stage_0();
 
   if (cs_glob_porous_model >= 1) {

src/base/iniva0.f90
@@ -1,328 +1,0 @@
-!-------------------------------------------------------------------------------
-
-! This file is part of code_saturne, a general-purpose CFD tool.
-!
-! Copyright (C) 1998-2024 EDF S.A.
-!
-! This program is free software; you can redistribute it and/or modify it under
-! the terms of the GNU General Public License as published by the Free Software
-! Foundation; either version 2 of the License, or (at your option) any later
-! version.
-!
-! This program is distributed in the hope that it will be useful, but WITHOUT
-! ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-! FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-! details.
-!
-! You should have received a copy of the GNU General Public License along with
-! this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
-! Street, Fifth Floor, Boston, MA 02110-1301, USA.
-
-!-------------------------------------------------------------------------------
-
-!> \file iniva0.f90
-!> \brief Computed variable initialization.
-!> The time step, the indicator of wall distance computation are also
-!> initialized just before reading a restart file or use the user
-!> initializations.
-!>
-!------------------------------------------------------------------------------
-
-!------------------------------------------------------------------------------
-! Arguments
-!------------------------------------------------------------------------------
-!   mode          name          role
-!------------------------------------------------------------------------------
-!> \param[in]     nscal         total number of scalars
-!______________________________________________________________________________
-
-subroutine iniva0() &
-bind(C, name='cs_f_iniva0')
-
-!===============================================================================
-! Module files
-!===============================================================================
-
-use paramx
-use numvar
-use optcal
-use cstphy
-use cstnum
-use pointe
-use dimens, only: nscal
-use entsor
-use albase
-use parall
-use period
-use ppppar
-use ppthch
-use ppincl
-use field
-use mesh
-use vof
-use cs_cf_bindings
-use cs_c_bindings
-
-!===============================================================================
-
-implicit none
-
-! Arguments
-
-! Local variables
-
-integer          iscal
-integer          iel   , ifac
-integer          ii
-integer          ifcvsl
-integer          idftnp
-integer          keyvar
-integer          f_id
-integer          ibeta
-
-logical          have_previous
-
-double precision visls_0, gravn2
-
-double precision, dimension(:), pointer :: dt
-double precision, dimension(:), pointer :: brom, crom, cpro_beta
-double precision, dimension(:), pointer :: porosi
-double precision, dimension(:,:), pointer :: porosf
-double precision, dimension(:), pointer :: field_s_v
-double precision, dimension(:), pointer :: viscl, visct, cpro_cp, cpro_prtot
-double precision, dimension(:), pointer :: cpro_viscls, cproa_viscls, cvar_tempk
-double precision, dimension(:), pointer :: cpro_visma_s
-double precision, dimension(:), pointer :: mix_mol_mas
-double precision, dimension(:,:), pointer :: cpro_visma_v
-
-type(var_cal_opt) :: vcopt_uma
-
-!===============================================================================
-! Interfaces
-!===============================================================================
-
-interface
-
-  subroutine cs_gui_mesh_viscosity()  &
-    bind(C, name='cs_gui_mesh_viscosity')
-    use, intrinsic :: iso_c_binding
-    implicit none
-  end subroutine cs_gui_mesh_viscosity
-
-end interface
-
-!===============================================================================
-! 1.  INITIALISATION
-!===============================================================================
-
-call field_get_key_id("variable_id", keyvar)
-
-call field_get_val_s_by_name('dt', dt)
-
-! Initialize temperature to reference temperature if present
-call field_get_id_try('temperature', f_id)
-if (f_id .ge. 0) then
-  call field_get_val_s(f_id, field_s_v)
-  do iel = 1, ncelet
-    field_s_v(iel) = t0
-  enddo
-endif
-
-! Initialize boundary temperature to "marker" if present
-call field_get_id_try('boundary_temperature', f_id)
-if (f_id .ge. 0) then
-  call field_get_val_s(f_id, field_s_v)
-  do iel = 1, nfabor
-    field_s_v(iel) = -grand
-  enddo
-endif
-
-!===============================================================================
-! 2. PAS DE TEMPS
-!===============================================================================
-
-! dt might be used on the halo cells during the ALE initialization
-! otherwise dt is synchronized in the pressure correction step.
-do iel = 1, ncelet
-  dt(iel) = dtref
-enddo
-
-!===============================================================================
-! 3.  INITIALISATION DES PROPRIETES PHYSIQUES
-!===============================================================================
-
-!     Masse volumique
-call field_get_val_s(icrom, crom)
-call field_get_val_s(ibrom, brom)
-
-!     Masse volumique aux cellules (et au pdt precedent si ordre2 ou icalhy
-!     ou algo. VOF)
-do iel = 1, ncelet
-  crom(iel)  = ro0
-enddo
-
-!     Masse volumique aux faces de bord (et au pdt precedent si ordre2)
-do ifac = 1, nfabor
-  brom(ifac) = ro0
-enddo
-
-! Boussinesq
-if (idilat.eq.0) then
-  call field_get_id_try("thermal_expansion", ibeta)
-  call field_get_val_s(ibeta, cpro_beta)
-  do iel = 1, ncelet
-    cpro_beta(iel) = -1.d0
-  enddo
-endif
-
-! Molecular viscosity
-call field_get_val_s(iviscl, viscl)
-call field_get_val_s(ivisct, visct)
-
-! Molecular viscosity at cells
-do iel = 1, ncelet
-  viscl(iel) = viscl0
-enddo
-
-! Specific heat at cells
-if(icp.ge.0) then
-  call field_get_val_s(icp, cpro_cp)
-  do iel = 1, ncelet
-    cpro_cp(iel) = cp0
-  enddo
-endif
-
-! La pression totale sera initialisee a P0 + rho.g.r dans INIVAR
-!  si l'utilisateur n'a pas fait d'initialisation personnelle
-! Non valable en compressible
-
-gravn2 = gx**2+gy**2+gz**2
-
-if (ippmod(icompf).lt.0) then
-  call field_get_val_s_by_name('total_pressure', cpro_prtot)
-  do iel = 1, ncelet
-    cpro_prtot(iel) = - rinfin
-  enddo
-endif
-
-! Initialization of mix_mol_mas with default values (air)
-! (used in cs_cf_thermo_default_init)
-if(ippmod(igmix).ge.0) then
-  call field_get_val_s_by_name('mix_mol_mas', mix_mol_mas)
-  do iel =1, ncelet
-    mix_mol_mas(iel) = xmasmr
-  enddo
-endif
-
-! Default initialisations for the compressible model
-if (ippmod(icompf).ge.0) then
-  ! In compressible, for now, the temperature is not solved but is a field of
-  ! type variable anyway. The reference value has to be taken into account.
-  call field_get_val_s(ivarfl(isca(itempk)), cvar_tempk)
-  do iel = 1, ncelet
-    cvar_tempk(iel) = t0
-  enddo
-
-  ! Default isochoric specific heat (cv0),
-  ! total energy and density
-  call cs_cf_thermo_default_init
-
-  ! Default diffusivity for total energy
-
-  call field_get_key_double(ivarfl(isca(itempk)), kvisl0, visls_0)
-  visls_0 = visls_0 / cv0
-  call field_set_key_double(ivarfl(isca(ienerg)), kvisl0, visls_0)
-endif
-
-! Diffusivite des scalaires
-do iscal = 1, nscal
-  call field_get_key_int (ivarfl(isca(iscal)), kivisl, ifcvsl)
-  call field_get_key_double(ivarfl(isca(iscal)), kvisl0, visls_0)
-  ! Diffusivite aux cellules (et au pdt precedent si ordre2)
-  if (ifcvsl.ge.0) then
-    call field_get_val_s(ifcvsl, cpro_viscls)
-    do iel = 1, ncelet
-      cpro_viscls(iel) = visls_0
-    enddo
-    call field_have_previous(ifcvsl, have_previous)
-    if (have_previous) then
-      call field_get_val_prev_s(ifcvsl, cproa_viscls)
-      do iel = 1, ncelet
-        cproa_viscls(iel) = visls_0
-      enddo
-    endif
-  endif
-enddo
-
-! Mesh viscosity for ALE
-if (iale.ge.1) then
-
-  call field_get_key_struct_var_cal_opt(ivarfl(iuma), vcopt_uma)
-  idftnp = vcopt_uma%idften
-
-  if (iand(idftnp, ANISOTROPIC_LEFT_DIFFUSION).ne.0) then
-    call field_get_val_v_by_name('mesh_viscosity', cpro_visma_v)
-    do iel = 1, ncelet
-      do ii = 1, 3
-        cpro_visma_v(ii  ,iel) = 1.d0
-        cpro_visma_v(ii+3,iel) = 0.d0
-      enddo
-    enddo
-  else if (iand(idftnp, ISOTROPIC_DIFFUSION).ne.0) then
-    call field_get_val_s_by_name('mesh_viscosity', cpro_visma_s)
-    do iel = 1, ncelet
-      cpro_visma_s(iel) = 1.d0
-    enddo
-  endif
-
-  call cs_gui_mesh_viscosity
-
-endif
-
-! Porosity
-if (iporos.ge.1) then
-  call field_get_val_s_by_name('porosity', porosi)
-  if (compute_porosity_from_scan) then
-    do iel = 1, ncelet
-      porosi(iel) = 0.d0
-    enddo
-  else if (ibm_porosity_mode.gt.0) then
-    call field_get_id_try('i_face_porosity', f_id)
-    if (f_id .ge. 0) then
-      call field_get_val_s(f_id, field_s_v)
-      do iel = 1, nfac
-        field_s_v(iel) = 1.d0
-      enddo
-    endif
-    call field_get_id_try('b_face_porosity', f_id)
-    if (f_id .ge. 0) then
-      call field_get_val_s(f_id, field_s_v)
-      do iel = 1, nfabor
-        field_s_v(iel) = 1.d0
-      enddo
-    endif
-  else
-    do iel = 1, ncelet
-      porosi(iel) = 1.d0
-    enddo
-  endif
-  ! Tensorial porosity
-  if (iporos.eq.2) then
-    call field_get_val_v_by_name('tensorial_porosity', porosf)
-    do iel = 1, ncelet
-      porosf(1, iel) = 1.d0
-      porosf(2, iel) = 1.d0
-      porosf(3, iel) = 1.d0
-      porosf(4, iel) = 0.d0
-      porosf(5, iel) = 0.d0
-      porosf(6, iel) = 0.d0
-    enddo
-  endif
-endif
-
-!----
-! End
-!----
-
-return
-end subroutine

commit b94c0ca81f5350e11f9677ce4463eb5778ab27cc

Fix Boundary temperature initialization, broken by ca157ea7 (2024-08-13).

src/base/cs_initialize_fields.c
@@ -139,7 +139,7 @@ cs_initialize_fields_stage_0(void)
   /* Initialize boundary temperature to "marker" if present */
   if (CS_F_(t_b) != NULL) {
     cs_array_real_set_scalar(m->n_b_faces,
-                             cs_math_big_r,
+                             -cs_math_big_r,
                              CS_F_(t_b)->val);
   }
 