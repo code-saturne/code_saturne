code-saturne/code_saturne

commit 4778687ce302467db7c6a6244e82fcc09d3a042b

Add a warnings log file possibility

src/base/cs_log.c
@@ -71,10 +71,11 @@ BEGIN_C_DECLS
 
 static bool  _cs_log_atexit_set = false;
 
-static FILE* _cs_log[] = {NULL, NULL, NULL};
+static FILE* _cs_log[] = {NULL, NULL, NULL, NULL};
 static const char* _cs_log_name[] = {"",
                                      "setup.log",
-                                     "performance.log"};
+                                     "performance.log",
+                                     "warnings.log"};
 
 static bool  _cs_log_default_active = true;
 
@@ -466,6 +467,11 @@ cs_log_vprintf(cs_log_t     log,
 
     retval = vfprintf(_cs_log[log], format, arg_ptr);
 
+    if (log == CS_LOG_WARNINGS) {
+      bft_printf_proxy_t *_printf_proxy = bft_printf_proxy_get();
+      _printf_proxy(format, arg_ptr);
+    }
+
   }
 
   else {
@@ -516,6 +522,11 @@ cs_log_printf(cs_log_t     log,
     va_start(arg_ptr, format);
 
     retval = vfprintf(_cs_log[log], format, arg_ptr);
+
+    if (log == CS_LOG_WARNINGS) {
+      bft_printf_proxy_t *_printf_proxy = bft_printf_proxy_get();
+      _printf_proxy(format, arg_ptr);
+    }
 
     va_end(arg_ptr);
 

src/base/cs_log.h
@@ -50,6 +50,7 @@ typedef enum {
   CS_LOG_DEFAULT,      /* Default (main) log */
   CS_LOG_SETUP,        /* Calculation setup and options log */
   CS_LOG_PERFORMANCE,  /* Performance log */
+  CS_LOG_WARNINGS,     /* Warnings log */
   CS_LOG_N_TYPES       /* Number of log file types */
 
 } cs_log_t;

commit 072ff42a0be5e0671087a92056f693f546b5573e

Fix logging of warnings, broken by commit 4778687c by incorrect va_args usage.

src/base/cs_log.c
@@ -705,20 +705,20 @@ cs_log_warning(const char *format,
   if (cs_glob_rank_id > 0)
     return 0;
 
-  va_start(arg_ptr, format);
-
   /* Print to warning log */
+  va_start(arg_ptr, format);
   cs_log_separator(CS_LOG_WARNINGS);
   retval = cs_log_vprintf(CS_LOG_WARNINGS, format, arg_ptr);
   cs_log_separator(CS_LOG_WARNINGS);
   cs_log_printf(CS_LOG_WARNINGS, "\n");
+  va_end(arg_ptr);
 
   /* Print copy to main log */
   cs_log_printf(CS_LOG_DEFAULT, _("\n"
                                   "Warning:\n"
                                   "--------\n\n"));
+  va_start(arg_ptr, format);
   cs_log_vprintf(CS_LOG_DEFAULT, format, arg_ptr);
-
   va_end(arg_ptr);
 
   return retval;