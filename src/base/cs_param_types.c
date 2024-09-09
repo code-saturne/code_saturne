code-saturne/code_saturne

commit 1b2a556cee8d1fe26a84e6fdb496afb50aa17553

Add an enum for boundary condition types

src/base/cs_param_types.c
@@ -139,11 +139,11 @@ static const char
 cs_param_bc_type_name[CS_PARAM_N_BC_TYPES][CS_BASE_STRING_LEN] =
   { N_("Homogeneous Dirichlet"),
     N_("Dirichlet"),
-    N_("Homogeneous Neumann"),
+    N_("Symmetry"),
     N_("Neumann"),
     N_("Neumann (full)"),
     N_("Robin"),
-    N_("Sliding"),
+    N_("Circulation"),
     N_("Wall prescribed")
   };
 
@@ -387,15 +387,14 @@ const char *
 cs_param_get_bc_name(cs_param_bc_type_t    type)
 {
   switch (type) {
-  case CS_PARAM_BC_HMG_DIRICHLET:
-  case CS_PARAM_BC_DIRICHLET:
-  case CS_PARAM_BC_HMG_NEUMANN:
-  case CS_PARAM_BC_NEUMANN:
-  case CS_PARAM_BC_NEUMANN_FULL:
-  case CS_PARAM_BC_ROBIN:
-  case CS_PARAM_BC_SLIDING:
-  case CS_PARAM_BC_CIRCULATION:
-  case CS_PARAM_BC_WALL_PRESCRIBED:
+  case CS_BC_HMG_DIRICHLET:
+  case CS_BC_DIRICHLET:
+  case CS_BC_SYMMETRY:
+  case CS_BC_NEUMANN:
+  case CS_BC_NEUMANN_FULL:
+  case CS_BC_ROBIN:
+  case CS_BC_CIRCULATION:
+  case CS_BC_WALL_MODELLED:
     return cs_param_bc_type_name[type];
 
   default:

src/base/cs_ale.c
@@ -1537,7 +1537,7 @@ cs_ale_activate(void)
                     "mesh_velocity", /* associated variable field name */
                     CS_EQUATION_TYPE_PREDEFINED,
                     3,                        /* dimension of the unknown */
-                    CS_PARAM_BC_HMG_NEUMANN); /* default boundary */
+                    CS_BC_SYMMETRY); /* default boundary */
 
   cs_equation_param_t  *eqp = cs_equation_get_param(eq);
 
@@ -1766,7 +1766,7 @@ cs_ale_setup_boundaries(const cs_domain_t   *domain)
         cs_real_t  bc_value[3] = {0., 0., 0.};
 
         cs_equation_add_bc_by_value(eqp,
-                                    CS_PARAM_BC_HMG_DIRICHLET,
+                                    CS_BC_HMG_DIRICHLET,
                                     z->name,
                                     bc_value);
       }
@@ -1774,7 +1774,7 @@ cs_ale_setup_boundaries(const cs_domain_t   *domain)
 
     case CS_BOUNDARY_ALE_IMPOSED_VEL:
       cs_equation_add_bc_by_array(eqp,
-                                  CS_PARAM_BC_DIRICHLET,
+                                  CS_BC_DIRICHLET,
                                   z->name,
                                   cs_flag_primal_vtx,
                                   _cdo_bc->vtx_values,
@@ -1787,7 +1787,7 @@ cs_ale_setup_boundaries(const cs_domain_t   *domain)
 
     case CS_BOUNDARY_ALE_IMPOSED_DISP:
       cs_equation_add_bc_by_array(eqp,
-                                  CS_PARAM_BC_DIRICHLET,
+                                  CS_BC_DIRICHLET,
                                   z->name,
                                   cs_flag_primal_vtx,
                                   _cdo_bc->vtx_values,
@@ -1800,7 +1800,7 @@ cs_ale_setup_boundaries(const cs_domain_t   *domain)
 
     case CS_BOUNDARY_ALE_FREE_SURFACE:
       cs_equation_add_bc_by_array(eqp,
-                                  CS_PARAM_BC_DIRICHLET,
+                                  CS_BC_DIRICHLET,
                                   z->name,
                                   cs_flag_primal_vtx,
                                   _cdo_bc->vtx_values,

src/base/cs_boundary_conditions.c
@@ -2625,7 +2625,7 @@ cs_boundary_conditions_compute(int  bc_type[])
       cs_param_bc_type_t bc_type_l = (cs_param_bc_type_t)(def->meta);
       switch (bc_type_l) {
 
-      case CS_PARAM_BC_HMG_DIRICHLET:
+      case CS_BC_HMG_DIRICHLET:
         _compute_hmg_dirichlet_bc(mesh,
                                   boundaries,
                                   eqp,
@@ -2635,7 +2635,7 @@ cs_boundary_conditions_compute(int  bc_type[])
                                   f->bc_coeffs->rcodcl1);
         break;
 
-      case CS_PARAM_BC_DIRICHLET:
+      case CS_BC_DIRICHLET:
         _compute_dirichlet_bc(mesh,
                               boundaries,
                               f,
@@ -2649,14 +2649,14 @@ cs_boundary_conditions_compute(int  bc_type[])
                               eval_buf);
         break;
 
-      case CS_PARAM_BC_HMG_NEUMANN:
+      case CS_BC_SYMMETRY:
         _compute_hmg_neumann_bc(mesh,
                                 def,
                                 f->bc_coeffs->icodcl,
                                 f->bc_coeffs->rcodcl3);
         break;
 
-      case CS_PARAM_BC_NEUMANN:
+      case CS_BC_NEUMANN:
         _compute_neumann_bc(mesh,
                             eqp,
                             def,
@@ -2667,7 +2667,7 @@ cs_boundary_conditions_compute(int  bc_type[])
                             eval_buf);
         break;
 
-      case CS_PARAM_BC_ROBIN:
+      case CS_BC_ROBIN:
         {
           cs_lnum_t stride = 1 + f->dim + f->dim*f->dim;
           n_max_vals = stride * n_b_faces;
@@ -2691,7 +2691,7 @@ cs_boundary_conditions_compute(int  bc_type[])
         }
         break;
 
-      case CS_PARAM_BC_WALL_PRESCRIBED:
+      case CS_BC_WALL_MODELLED:
         _compute_dirichlet_bc(mesh,
                               boundaries,
                               f,
@@ -3125,7 +3125,7 @@ cs_boundary_conditions_open_set_velocity_by_value(const cs_zone_t  *z,
   cs_equation_remove_bc(eqp, c->zone->name);  /* Replace if already set */
 
   cs_equation_add_bc_by_value(eqp,
-                              CS_PARAM_BC_DIRICHLET,
+                              CS_BC_DIRICHLET,
                               c->zone->name,
                               c->vel_values);
 }
@@ -3162,7 +3162,7 @@ cs_boundary_conditions_open_set_velocity_by_normal_value(const  cs_zone_t  *z,
   cs_equation_remove_bc(eqp, c->zone->name);  /* Replace if already set */
 
   cs_equation_add_bc_by_dof_func(eqp,
-                                 CS_PARAM_BC_DIRICHLET,
+                                 CS_BC_DIRICHLET,
                                  z->name,
                                  cs_flag_boundary_face,  // location flag
                                  c->dof_func,
@@ -3217,7 +3217,7 @@ cs_boundary_conditions_open_set_velocity_by_func(const  cs_zone_t       *z,
   cs_equation_remove_bc(eqp, z->name);  /* Replace if already set */
 
   cs_equation_add_bc_by_dof_func(eqp,
-                                 CS_PARAM_BC_DIRICHLET,
+                                 CS_BC_DIRICHLET,
                                  z->name,
                                  cs_flag_boundary_face,  // location flag
                                  c->dof_func,
@@ -3342,7 +3342,7 @@ cs_boundary_conditions_open_set_mass_flow_rate_by_value(const  cs_zone_t  *z,
   cs_equation_remove_bc(eqp, z->name);  /* Replace if already set */
 
   cs_equation_add_bc_by_dof_func(eqp,
-                                 CS_PARAM_BC_DIRICHLET,
+                                 CS_BC_DIRICHLET,
                                  z->name,
                                  cs_flag_boundary_face,  // location flag
                                  c->dof_func,
@@ -3440,7 +3440,7 @@ cs_boundary_conditions_open_set_mass_flow_rate_by_func
   cs_equation_remove_bc(eqp, z->name);  /* Replace if already set */
 
   cs_equation_add_bc_by_dof_func(eqp,
-                                 CS_PARAM_BC_DIRICHLET,
+                                 CS_BC_DIRICHLET,
                                  z->name,
                                  cs_flag_boundary_face,  // location flag
                                  c->dof_func,
@@ -3513,7 +3513,7 @@ cs_boundary_conditions_open_set_volume_flow_rate_by_value(const  cs_zone_t  *z,
   cs_equation_remove_bc(eqp, z->name);  /* Replace if already set */
 
   cs_equation_add_bc_by_dof_func(eqp,
-                                 CS_PARAM_BC_DIRICHLET,
+                                 CS_BC_DIRICHLET,
                                  z->name,
                                  cs_flag_boundary_face,  // location flag
                                  c->dof_func,
@@ -3604,7 +3604,7 @@ cs_boundary_conditions_open_set_volume_flow_rate_by_func
   cs_equation_remove_bc(eqp, z->name);  /* Replace if already set */
 
   cs_equation_add_bc_by_dof_func(eqp,
-                                 CS_PARAM_BC_DIRICHLET,
+                                 CS_BC_DIRICHLET,
                                  z->name,
                                  cs_flag_boundary_face,  // location flag
                                  c->dof_func,

src/base/cs_param_types.h
@@ -439,60 +439,65 @@ typedef enum {
  * \enum cs_param_bc_type_t
  * Type of boundary condition to enforce.
  *
- * \var CS_PARAM_BC_HMG_DIRICHLET
+ * \var CS_BC_HMG_DIRICHLET
  * Homogeneous Dirichlet boundary conditions. The value of a variable is set
  * to zero.
  *
- * \var CS_PARAM_BC_DIRICHLET
+ * \var CS_BC_DIRICHLET
  * Dirichlet boundary conditions. The value of the variable is set to the user
  * requirements.
  *
- * \var CS_PARAM_BC_HMG_NEUMANN
+ * \var CS_BC_SYMMETRY
  * Homogeneous Neumann conditions. The value of the flux of variable is set
  * to zero.
  *
- * \var CS_PARAM_BC_NEUMANN
+ * \var CS_BC_NEUMANN
  * Neumann conditions (along the face normal). The value of the flux of
  * variable is set to the user requirements.
  *
- * \var CS_PARAM_BC_NEUMANN_FULL
+ * \var CS_BC_NEUMANN_FULL
  * Neumann conditions with full definition relative to all directions, i.e.
  * not only the normal direction. The value of the flux of variable is set to
  * the user requirements.
  *
- * \var CS_PARAM_BC_ROBIN
+ * \var CS_BC_ROBIN
  * Robin conditions.
  *
- * \var CS_PARAM_BC_SLIDING
+ * \var CS_BC_SYMMETRY
  * Sliding conditions. Homogeneous Dirichlet for the normal component and
  * homogeneous Neumann for the tangential components. Only available for
  * vector-valued equations.
  *
- * \var CS_PARAM_BC_CIRCULATION
+ * \var CS_BC_CIRCULATION
  * Set the tangential part of a vector-valued field. This is the part lying on
  * the boundary of a part of the computatinal domain. Nothing is prescribed for
  * the normal part of the vector-valued field.
  *
- * \var CS_PARAM_BC_WALL_PRESCRIBED
+ * \var CS_BC_WALL_MODELLED
  * Hybrid way to enforce the value at a wall. Use a wall function to prescribe
  * the wall value knowing the value at the associated cell center for instance
  */
 
 typedef enum {
 
-  CS_PARAM_BC_HMG_DIRICHLET = 0,
-  CS_PARAM_BC_DIRICHLET = 1,
-  CS_PARAM_BC_HMG_NEUMANN = 2,
-  CS_PARAM_BC_NEUMANN = 3,
-  CS_PARAM_BC_NEUMANN_FULL = 4,
-  CS_PARAM_BC_ROBIN = 5,
-  CS_PARAM_BC_SLIDING = 6,
-  CS_PARAM_BC_CIRCULATION = 7,
-  CS_PARAM_BC_WALL_PRESCRIBED = 8,
+  CS_BC_HMG_DIRICHLET = 0,
+  CS_BC_DIRICHLET = 1,
+  CS_BC_RADIATIVE_OUTLET = 2,
+  CS_BC_NEUMANN = 3,
+  CS_BC_SYMMETRY = 4,
+  CS_BC_WALL_MODELLED = 5,
+  CS_BC_ROUGH_WALL_MODELLED = 6,
+  CS_BC_NEUMANN_FULL = 7,
+  CS_BC_ROBIN = 9,
+  CS_BC_CIRCULATION = 11,
+  CS_BC_IMPOSED_TOT_FLUX = 13,
+  CS_BC_GENERALIZED_SYM = 14,
+  CS_BC_IMPOSED_EXCHANGE_COEF = 15,
 
   CS_PARAM_N_BC_TYPES
 
 } cs_param_bc_type_t;
+
 
 /*! \enum cs_param_bc_enforce_t
  * Type of method for enforcing the boundary conditions. According to the type

src/base/cs_parameters.c
@@ -437,7 +437,7 @@ static cs_equation_param_t _equation_param_default
    .relaxv = 1.,
    .b_gradient_r = 2,
 
-   .default_bc = CS_PARAM_BC_HMG_NEUMANN,
+   .default_bc = CS_BC_SYMMETRY,
    .n_bc_defs = 0,
    .bc_defs = NULL,
 

src/base/cs_pressure_correction.cpp
@@ -3557,7 +3557,7 @@ cs_pressure_correction_cdo_activate(void)
                     "pressure_increment", /* associated variable field name */
                     CS_EQUATION_TYPE_PREDEFINED,
                     1,                        /* dimension of the unknown */
-                    CS_PARAM_BC_HMG_NEUMANN); /* default boundary */
+                    CS_BC_SYMMETRY); /* default boundary */
 
   cs_equation_param_t  *eqp = cs_equation_get_param(eq);
 
@@ -3745,7 +3745,7 @@ cs_pressure_correction_cdo_finalize_setup(const cs_domain_t   *domain)
       }
 
       cs_equation_add_bc_by_array(eqp,
-                                  CS_PARAM_BC_DIRICHLET,
+                                  CS_BC_DIRICHLET,
                                   z->name,
                                   cs_flag_primal_face,
                                   prcdo->bdy_pressure_incr,

src/cdo/cs_cdo_bc.cpp
@@ -161,8 +161,7 @@ cs_cdo_bc_face_define(cs_param_bc_type_t   default_bc,
   cs_flag_t  default_flag = cs_cdo_bc_get_flag(default_bc);
 
   if (!(default_flag & CS_CDO_BC_HMG_DIRICHLET) &&
-      !(default_flag & CS_CDO_BC_HMG_NEUMANN) &&
-      !(default_flag & CS_CDO_BC_SLIDING))
+      !(default_flag & CS_CDO_BC_SYMMETRY))
     bft_error(__FILE__, __LINE__, 0,
               _(" %s: Incompatible type of boundary condition by default.\n"
                 " Please modify your settings.\n"), __func__);
@@ -191,18 +190,15 @@ cs_cdo_bc_face_define(cs_param_bc_type_t   default_bc,
     case CS_CDO_BC_FULL_NEUMANN:
       bc->n_nhmg_neu_faces += z->n_elts;
       break;
-    case CS_CDO_BC_HMG_NEUMANN:
-      bc->n_hmg_neu_faces += z->n_elts;
+    case CS_CDO_BC_SYMMETRY:
+      if (dim > 1)
+        /* For vector-valued equations only */
+        bc->n_sliding_faces += z->n_elts;
+      else
+        bc->n_hmg_neu_faces += z->n_elts;
       break;
     case CS_CDO_BC_ROBIN:
       bc->n_robin_faces += z->n_elts;
-      break;
-
-    /* For vector-valued equations only */
-
-    case CS_CDO_BC_SLIDING:
-      assert(dim > 1);
-      bc->n_sliding_faces += z->n_elts;
       break;
 
     /* For vector-valued equations only */
@@ -247,10 +243,11 @@ cs_cdo_bc_face_define(cs_param_bc_type_t   default_bc,
       bc->flag[i] = default_flag;
       if (default_flag & CS_CDO_BC_HMG_DIRICHLET)
         bc->n_hmg_dir_faces++;
-      else if (default_flag & CS_CDO_BC_HMG_NEUMANN)
-        bc->n_hmg_neu_faces++;
-      else if (default_flag & CS_CDO_BC_SLIDING)
-        bc->n_sliding_faces++;
+      else if (default_flag & CS_CDO_BC_SYMMETRY)
+        if (dim > 1)
+          bc->n_sliding_faces++;
+        else
+          bc->n_hmg_neu_faces++;
       else
         bft_error(__FILE__, __LINE__, 0,
                   "%s: Invalid type of default boundary condition", __func__);
@@ -295,39 +292,38 @@ cs_cdo_bc_face_define(cs_param_bc_type_t   default_bc,
     switch (bc->flag[i]) {
 
     case CS_CDO_BC_DIRICHLET:
-      bc->nhmg_dir_ids[shift[CS_PARAM_BC_DIRICHLET]] = i;
-      shift[CS_PARAM_BC_DIRICHLET] += 1;
+      bc->nhmg_dir_ids[shift[CS_BC_DIRICHLET]] = i;
+      shift[CS_BC_DIRICHLET] += 1;
       break;
     case CS_CDO_BC_HMG_DIRICHLET:
-      bc->hmg_dir_ids[shift[CS_PARAM_BC_HMG_DIRICHLET]] = i;
-      shift[CS_PARAM_BC_HMG_DIRICHLET] += 1;
+      bc->hmg_dir_ids[shift[CS_BC_HMG_DIRICHLET]] = i;
+      shift[CS_BC_HMG_DIRICHLET] += 1;
       break;
     case CS_CDO_BC_NEUMANN:
     case CS_CDO_BC_FULL_NEUMANN:
-      bc->nhmg_neu_ids[shift[CS_PARAM_BC_NEUMANN]] = i;
-      shift[CS_PARAM_BC_NEUMANN] += 1;
+      bc->nhmg_neu_ids[shift[CS_BC_NEUMANN]] = i;
+      shift[CS_BC_NEUMANN] += 1;
       break;
-    case CS_CDO_BC_HMG_NEUMANN:
-      bc->hmg_neu_ids[shift[CS_PARAM_BC_HMG_NEUMANN]] = i;
-      shift[CS_PARAM_BC_HMG_NEUMANN] += 1;
+    case CS_CDO_BC_SYMMETRY:
+      if (dim > 1) {
+        bc->sliding_ids[shift[CS_BC_SYMMETRY]] = i;
+        shift[CS_BC_SYMMETRY] += 1;
+      }
+      else {
+        bc->hmg_neu_ids[shift[CS_BC_SYMMETRY]] = i;
+        shift[CS_BC_SYMMETRY] += 1;
+      }
       break;
     case CS_CDO_BC_ROBIN:
-      bc->robin_ids[shift[CS_PARAM_BC_ROBIN]] = i;
-      shift[CS_PARAM_BC_ROBIN] += 1;
-      break;
-
-    /* For vector-valued equations only */
-
-    case CS_CDO_BC_SLIDING:
-      bc->sliding_ids[shift[CS_PARAM_BC_SLIDING]] = i;
-      shift[CS_PARAM_BC_SLIDING] += 1;
+      bc->robin_ids[shift[CS_BC_ROBIN]] = i;
+      shift[CS_BC_ROBIN] += 1;
       break;
 
     /* For vector-valued equations only */
 
     case CS_CDO_BC_TANGENTIAL_DIRICHLET:
-      bc->circulation_ids[shift[CS_PARAM_BC_CIRCULATION]] = i;
-      shift[CS_PARAM_BC_CIRCULATION] += 1;
+      bc->circulation_ids[shift[CS_BC_CIRCULATION]] = i;
+      shift[CS_BC_CIRCULATION] += 1;
       break;
 
     default:

src/cdo/cs_cdo_bc.h
@@ -64,9 +64,10 @@ BEGIN_C_DECLS
 
 #define CS_CDO_BC_FULL_NEUMANN          (1 << 1)
 
-/*!  4: Homogeneous Neumann boundary conditions */
+/*!  4: Homogeneous Neumann boundary conditions
+        Apply a sliding condition (for vector-valued equations) */
 
-#define CS_CDO_BC_HMG_NEUMANN           (1 << 2)
+#define CS_CDO_BC_SYMMETRY              (1 << 2)
 
 /*!  8: Dirichlet boundary conditions */
 
@@ -80,17 +81,13 @@ BEGIN_C_DECLS
 
 #define CS_CDO_BC_ROBIN                 (1 << 5)
 
-/*!  64: Apply a sliding condition (for vector-valued equations) */
+/*! 64: Apply a Dirichlet on the tangential part of a vector-valued quantity */
 
-#define CS_CDO_BC_SLIDING               (1 << 6)
+#define CS_CDO_BC_TANGENTIAL_DIRICHLET  (1 << 6)
 
-/*! 128: Apply a Dirichlet on the tangential part of a vector-valued quantity */
+/*! 128: Apply a wall function/law to prescribe the value at a wall */
 
-#define CS_CDO_BC_TANGENTIAL_DIRICHLET  (1 << 7)
-
-/*! 256: Apply a wall function/law to prescribe the value at a wall */
-
-#define CS_CDO_BC_WALL_PRESCRIBED       (1 << 8)
+#define CS_CDO_BC_WALL_PRESCRIBED       (1 << 7)
 
 /*! @} */
 
@@ -190,8 +187,8 @@ cs_cdo_bc_get_desc(cs_flag_t   bc_flag,
   case CS_CDO_BC_DIRICHLET:
     sprintf(desc, "%s", "Dirichlet");
     break;
-  case CS_CDO_BC_HMG_NEUMANN:
-    sprintf(desc, "%s", "Homogeneous Neumann");
+  case CS_CDO_BC_SYMMETRY:
+    sprintf(desc, "%s", "Homogeneous Neumann or Sliding for vectors");
     break;
   case CS_CDO_BC_NEUMANN:
     sprintf(desc, "%s", "Neumann");
@@ -201,9 +198,6 @@ cs_cdo_bc_get_desc(cs_flag_t   bc_flag,
     break;
   case CS_CDO_BC_ROBIN:
     sprintf(desc, "%s", "Robin");
-    break;
-  case CS_CDO_BC_SLIDING:
-    sprintf(desc, "%s", "Sliding");
     break;
   case CS_CDO_BC_TANGENTIAL_DIRICHLET:
     sprintf(desc, "%s", "Dirichlet on the tangential component");
@@ -234,31 +228,32 @@ cs_cdo_bc_get_flag(cs_param_bc_type_t   bc_type)
 
   switch (bc_type) {
 
-  case CS_PARAM_BC_HMG_DIRICHLET:
+  case CS_BC_HMG_DIRICHLET:
     ret_flag = CS_CDO_BC_HMG_DIRICHLET;
     break;
-  case CS_PARAM_BC_DIRICHLET:
+  case CS_BC_DIRICHLET:
     ret_flag = CS_CDO_BC_DIRICHLET;
     break;
-  case CS_PARAM_BC_HMG_NEUMANN:
-    ret_flag = CS_CDO_BC_HMG_NEUMANN;
+  case CS_BC_SYMMETRY:
+    ret_flag = CS_CDO_BC_SYMMETRY;
     break;
-  case CS_PARAM_BC_NEUMANN:
+  case CS_BC_NEUMANN:
     ret_flag = CS_CDO_BC_NEUMANN;
     break;
-  case CS_PARAM_BC_NEUMANN_FULL:
+  case CS_BC_NEUMANN_FULL:
     ret_flag = CS_CDO_BC_FULL_NEUMANN;
     break;
-  case CS_PARAM_BC_ROBIN:
+  case CS_BC_GENERALIZED_SYM:
+    bft_error(__FILE__, __LINE__, 0,
+              _("invalid boundary condition CS_BC_GENERALIZED_SYM for CDO"));
+    break;
+  case CS_BC_ROBIN:
     ret_flag = CS_CDO_BC_ROBIN;
     break;
-  case CS_PARAM_BC_SLIDING:
-    ret_flag = CS_CDO_BC_SLIDING;
-    break;
-  case CS_PARAM_BC_CIRCULATION:
+  case CS_BC_CIRCULATION:
     ret_flag = CS_CDO_BC_TANGENTIAL_DIRICHLET;
     break;
-  case CS_PARAM_BC_WALL_PRESCRIBED:
+  case CS_BC_WALL_MODELLED:
     ret_flag = CS_CDO_BC_WALL_PRESCRIBED; /* TO BE CHECKED */
     break;
 
@@ -307,7 +302,7 @@ cs_cdo_bc_is_neumann(cs_flag_t    flag)
 {
   if (flag & CS_CDO_BC_NEUMANN)
     return true;
-  else if (flag & CS_CDO_BC_HMG_NEUMANN)
+  else if (flag & CS_CDO_BC_SYMMETRY)
     return true;
   else
     return false;
@@ -326,7 +321,7 @@ cs_cdo_bc_is_neumann(cs_flag_t    flag)
 static inline bool
 cs_cdo_bc_is_sliding(cs_flag_t    flag)
 {
-  if (flag & CS_CDO_BC_SLIDING)
+  if (flag & CS_CDO_BC_SYMMETRY)
     return true;
   else
     return false;

commit f9ae1d967e84f83e2842a0a8722e4ce367f24bbc

Fix log of BCs broken by commit 1b2a556c (2024-09-04)
Since the numbering of the enum is not contiguous anymore, one needs
to change the way to log the boundary condition.

src/base/cs_param_types.c
@@ -136,18 +136,6 @@ cs_param_adv_extrapol_name
   };
 
 static const char
-cs_param_bc_type_name[CS_PARAM_N_BC_TYPES][CS_BASE_STRING_LEN] =
-  { N_("Homogeneous Dirichlet"),
-    N_("Dirichlet"),
-    N_("Symmetry"),
-    N_("Neumann"),
-    N_("Neumann (full)"),
-    N_("Robin"),
-    N_("Circulation"),
-    N_("Wall prescribed")
-  };
-
-static const char
 cs_param_bc_enforcement_name[CS_PARAM_N_BC_ENFORCEMENTS][CS_BASE_STRING_LEN] =
   { N_("weak using an algebraic manipulation"),
     N_("weak using a big penalization coefficient"),
@@ -387,18 +375,37 @@ const char *
 cs_param_get_bc_name(cs_param_bc_type_t    type)
 {
   switch (type) {
+
   case CS_BC_HMG_DIRICHLET:
+    return "Homogeneous Dirichlet";
   case CS_BC_DIRICHLET:
-  case CS_BC_SYMMETRY:
+    return "Dirichlet";
+  case CS_BC_RADIATIVE_OUTLET:
+    return "Radiative outlet";
   case CS_BC_NEUMANN:
-  case CS_BC_NEUMANN_FULL:
-  case CS_BC_ROBIN:
-  case CS_BC_CIRCULATION:
+    return "Neumann";
+  case CS_BC_SYMMETRY:
+    return "Symmetry";
   case CS_BC_WALL_MODELLED:
-    return cs_param_bc_type_name[type];
+    return "Wall modelled";
+  case CS_BC_ROUGH_WALL_MODELLED:
+    return "Rough wall modelled";
+  case CS_BC_NEUMANN_FULL:
+    return "Neumann (full)";
+  case CS_BC_ROBIN:
+    return "Robin";
+  case CS_BC_CIRCULATION:
+    return "Circulation";
+  case CS_BC_IMPOSED_TOT_FLUX:
+    return "Total flux enforcement";
+  case CS_BC_GENERALIZED_SYM:
+    return "Symmetry (Generalized)";
+  case CS_BC_IMPOSED_EXCHANGE_COEF:
+    return "Exchange coefficient enforcement";
 
   default:
-    return NULL;
+    return "Undefined. Check your settings.";
+
   }
 }
 