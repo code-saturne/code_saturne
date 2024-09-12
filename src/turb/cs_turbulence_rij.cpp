code-saturne/code_saturne

commit 04fa8b3aa54a58b28f8af7be1c246db3d89437e4

[Turb] Modify dissipation buoyancy production term for Ellitic blending models
Prov. by Corina Sanz Souhait.

src/turb/cs_turbulence_rij.cpp
@@ -394,17 +394,16 @@ _compute_up_rhop(int                 phase_id,
 
   cs_real_t cons = -1.5 * cs_turb_cmu;
 
-  const cs_field_t *tf = cs_thermal_model_field();
-  if (tf != nullptr) {
+  const cs_field_t *thf = cs_thermal_model_field();
+  if (thf != nullptr) {
     const int ksigmas = cs_field_key_id("turbulent_schmidt");
-    const cs_real_t turb_schmidt = cs_field_get_key_double(tf, ksigmas);
+    const cs_real_t turb_schmidt = cs_field_get_key_double(thf, ksigmas);
     cons = -1.5 * cs_turb_cmu / turb_schmidt;
   }
   const cs_velocity_pressure_model_t  *vp_model
     = cs_glob_velocity_pressure_model;
   const int idilat = vp_model->idilat;
 
-  const cs_field_t *thf = cs_thermal_model_field();
   if (phase_id >= 0)
     thf = CS_FI_(h_tot, phase_id);
 
@@ -414,16 +413,22 @@ _compute_up_rhop(int                 phase_id,
     turb_flux_model = cs_field_get_key_int(thf, kturt);
   const int turb_flux_model_type = turb_flux_model / 10;
 
-  if (turb_flux_model_type == 3) {
+  cs_field_t *f_hf = cs_field_by_composite_name_try(thf->name,
+                                                    "turbulent_flux");
+
+  /* We want to use the computed turbulent heat fluxes (when they exist)
+     when we are not dealing with atmo cases */
+  if (   ( f_hf != NULL && cs_glob_physical_model_flag[CS_ATMOSPHERIC] < 1 )
+       || turb_flux_model_type == 3 
+       || turb_flux_model_type == 2 ) {
+
     /* Using thermal fluxes
      * (only for thermal scalar for the moment) */
     cs_field_t *f_beta = cs_field_by_name_try("thermal_expansion");
 
     if (f_beta != nullptr) {
       /* Value of the corresponding turbulent flux */
-      cs_real_3_t *xut =
-        (cs_real_3_t *)cs_field_by_composite_name(thf->name,
-                                                  "turbulent_flux")->val;
+      cs_real_3_t *xut = (cs_real_3_t *)f_hf->val;
 
       const cs_real_t *beta = f_beta->val;
 
@@ -921,20 +926,52 @@ _gravity_st_epsilon(int              phase_id,
     f_mu = CS_FI_(mu, phase_id);
   }
 
+  cs_fluid_properties_t *fp = cs_get_glob_fluid_properties();
+
+  cs_field_t *f_t = cs_thermal_model_field();
+  cs_field_t *f_t_var = cs_field_get_variance(f_t);
+
   const cs_real_6_t *cvara_rij = (const cs_real_6_t *)f_rij->val_pre;
-
   const cs_real_t *cvara_ep = f_eps->val_pre;
-
   const cs_real_t *crom = f_rho->val;
   const cs_real_t *viscl = f_mu->val;
 
+  const int kturt = cs_field_key_id("turbulent_flux_model");
+  int turb_flux_model =  cs_field_get_key_int(f_t, kturt);
   const cs_turb_model_type_t iturb
     = (cs_turb_model_type_t)cs_glob_turb_model->iturb;
 
   const cs_real_t xct = cs_turb_xct;
   const cs_real_t ce1 = cs_turb_ce1;
 
+  /* Laminar thermal conductivity k for Prandtl calculation */
+  cs_lnum_t l_viscls = 0; /* stride for uniform/local viscosity access */
+  cs_real_t _visls_0 = -1;
+  const cs_real_t *viscls = NULL;
+  {
+    const int kivisl = cs_field_key_id("diffusivity_id");
+    int ifcvsl = cs_field_get_key_int(f_t, kivisl);
+    if (ifcvsl > -1) {
+      viscls = cs_field_by_id(ifcvsl)->val;
+      l_viscls = 1;
+    }
+    else {
+      const int kvisls0 = cs_field_key_id("diffusivity_ref");
+      _visls_0 = cs_field_get_key_double(f_t, kvisls0);
+      viscls = &_visls_0;
+      l_viscls = 0;
+    }
+  }
+
   ctx.parallel_for(n_cells, [=] CS_F_HOST_DEVICE (cs_lnum_t c_id) {
+
+    /* Specific heat for Prandtl calculation */
+    const cs_field_t *f_cp = cs_field_by_name_try("specific_heat");
+    cs_real_t xcp;
+    if (f_cp != NULL)      /* Variable */
+      xcp = f_cp->val[c_id];
+    else                /* Constant */
+      xcp = fp->cp0;
 
     const cs_real_t g_up_rhop = cs_math_3_dot_product(grav, up_rhop[c_id]);
 
@@ -942,16 +979,44 @@ _gravity_st_epsilon(int              phase_id,
 
     cs_real_t time_scale = tke / cvara_ep[c_id];
 
+    cs_real_t buoyancy_constant = cs_turb_ce1;
+
     if (iturb == CS_TURB_RIJ_EPSILON_EBRSM) {
 
       /* Calculation of the Durbin time scale */
       const cs_real_t xttkmg
         = xct*sqrt(viscl[c_id] / crom[c_id] / cvara_ep[c_id]);
       time_scale = cs_math_fmax(time_scale, xttkmg);
+
+      /* Calculation of the mixed time scale for EB thermal 
+       *  flux models (Dehoux, 2017) */
+      if (  (turb_flux_model == 11)  /* EB-GGDH */
+         || (turb_flux_model == 21)  /* EB-AFM */
+         || (turb_flux_model == 31)) /* EB-DFM */ {
+
+        cs_real_t alpha_theta = 
+           cs_field_by_composite_name(f_t->name, "alpha")->val[c_id];
+
+        const int krvarfl = cs_field_key_id("variance_dissipation");
+        const cs_real_t rvarfl = cs_field_get_key_double(f_t_var, krvarfl);
+
+        cs_real_t prdtl = viscl[c_id]*xcp/viscls[l_viscls*c_id];
+        cs_real_t xr = (1.0 - alpha_theta)*prdtl + alpha_theta*rvarfl;
+
+        buoyancy_constant = cs_turb_ce3;
+
+        time_scale = time_scale * sqrt(xr)/sqrt(prdtl); 
+      }
     }
 
-    rhs[c_id] += ce1 * cs_math_fmax(0., g_up_rhop/time_scale)
-               * cell_f_vol[c_id];
+    if ( cs_glob_turb_rans_model->dissip_buo_mdl == 1 ) {
+      rhs[c_id] += buoyancy_constant *  
+                   g_up_rhop/time_scale * cell_f_vol[c_id];
+    } else {
+      rhs[c_id] += buoyancy_constant *  
+                   cs_math_fmax(0., g_up_rhop/time_scale) * cell_f_vol[c_id];
+    }
+
   });
 
   ctx.wait();

src/base/cs_setup.c
@@ -2118,8 +2118,9 @@ _init_user
   /* Flow and other model selection through user C routines */
   cs_user_model();
 
-  /* Set type and order of the turbulence model */
-  cs_set_type_order_turbulence_model();
+  /* Set additional members of turbulence model and RANS
+     turbulence model strucutre */
+  cs_turbulence_init_models();
 
   /* If CDO is active, initialize the context structures for models which
    * have been activated */

src/turb/cs_turbulence_model.c
@@ -277,6 +277,11 @@ BEGIN_C_DECLS
         mixing length for the mixing length model
 
         Useful if and only if \ref model= CS_TURB_MIXING_LENGTH (mixing length).
+  \var  cs_turb_rans_model_t::dissip_buo_mdl
+        Turbulent dissipation buoyant production model
+
+        Useful if and only if \ref order = 
+        CS_TURB_SECOND_ORDER (\f$R_{ij}-\epsilon\f$ model). 
 */
 
 /*----------------------------------------------------------------------------*/
@@ -392,7 +397,8 @@ _turb_rans_model =
   .iclsyr     =    1,
   .iclptr     =    0,
   .ikwcln     =    1,
-  .xlomlg     = -1e13
+  .xlomlg     = -1e13,
+  .dissip_buo_mdl = 0
 };
 
 const cs_turb_rans_model_t  *cs_glob_turb_rans_model = &_turb_rans_model;
@@ -514,6 +520,13 @@ double cs_turb_ce1 = 1.44;
  * - Rij-epsilon SSG or EBRSM: 1.83
  */
 double cs_turb_ce2 = 1.92;
+
+/*!
+ * Constant \f$C_{\varepsilon 3}\f$ for EB-RSM model.
+ * Useful only for buoyant term calculation of \f$R_{ij}\f$ 
+ * in \f$R_{ij}-\varepsilon EB-RSM\f$.
+ */
+double cs_turb_ce3 = 2.02;
 
 /*!
  * Coefficient of interfacial coefficient in k-eps, used in Lagrange treatment.
@@ -1406,13 +1419,16 @@ _turbulence_model_name(cs_turb_model_type_t  id)
 
 /*----------------------------------------------------------------------------*/
 /*!
- * \brief Initialize type and order members of turbulence model structure
+ * \brief Initialize additional turbulence model members of turbulence model
+ *        and RANS model structure
  */
 /*----------------------------------------------------------------------------*/
 
 void
-cs_set_type_order_turbulence_model(void)
+cs_turbulence_init_models(void)
 {
+  /* Set type and order of turbulence model */
+
   _turb_model.type = CS_TURB_NONE;
   if (_turb_model.model == CS_TURB_MIXING_LENGTH) {
      _turb_model.type = CS_TURB_RANS;
@@ -1456,6 +1472,11 @@ cs_set_type_order_turbulence_model(void)
     _turb_model.high_low_re = CS_TURB_HIGH_LOW_RE;
   else
     _turb_model.high_low_re = CS_TURB_HIGH_RE;
+
+  /* Set the model used for the turbulent dissipation buoyant 
+     production term */
+  if ( _turb_model.model == CS_TURB_RIJ_EPSILON_EBRSM )
+    _turb_rans_model.dissip_buo_mdl = 1;
 
 }
 
@@ -1782,7 +1803,9 @@ cs_turb_model_log_setup(void)
                     "    iclsyr:           %14d (Symmetry implicitation)\n"
                     "    iclptr:           %14d (Wall implicitation)\n"
                     "    ikwcln:           %14d (Wall boundary condition"
-                                                "on omega in k-omega SST)\n"),
+                                                "on omega in k-omega SST)\n"
+                    "    dissip_buo_mdl    %14d (Turbulent dissipation"
+                                                "buoyant production model)\n"),
                   cs_glob_turb_ref_values->uref,
                   cs_glob_turb_rans_model->reinit_turb,
                   cs_glob_turb_rans_model->irijco,
@@ -1791,7 +1814,8 @@ cs_turb_model_log_setup(void)
                   cs_glob_turb_rans_model->has_buoyant_term,
                   cs_glob_turb_rans_model->iclsyr,
                   cs_glob_turb_rans_model->iclptr,
-                  cs_glob_turb_rans_model->ikwcln);
+                  cs_glob_turb_rans_model->ikwcln,
+                  cs_glob_turb_rans_model->dissip_buo_mdl);
 
     int idirsm = cs_glob_turb_rans_model->idirsm;
     if (idirsm < 0 || idirsm > 1)

src/turb/cs_turbulence_model.h
@@ -264,6 +264,10 @@ typedef struct {
 
   double        xlomlg;       /* mixing length */
 
+  int           dissip_buo_mdl;
+                              /* Turbulent dissipation buoyant production model
+                                 0: Default: Production term clipped to 0
+                                 1: For EM-RSM */
 } cs_turb_rans_model_t;
 
 /* LES turbulence model descriptor */
@@ -336,6 +340,7 @@ extern double cs_turb_cmu;
 extern double cs_turb_cmu025;
 extern double cs_turb_ce1;
 extern double cs_turb_ce2;
+extern double cs_turb_ce3;
 extern double cs_turb_ce4;
 extern double cs_turb_crij1;
 extern double cs_turb_crij2;
@@ -439,11 +444,12 @@ extern double cs_turb_cthebdfm;
  *============================================================================*/
 
 /*----------------------------------------------------------------------------
- * Set type and order of the turbulence model
+ * Initialize additional turbulence model members of turbulence model
+ * and RANS model structure
  *----------------------------------------------------------------------------*/
 
 void
-cs_set_type_order_turbulence_model(void);
+cs_turbulence_init_models(void);
 
 /*----------------------------------------------------------------------------
  * Set global pointer to turbulence model structure

commit 45cec7624626a03c7989fc3fe54ac1e303f610c9

Fix bug introduced in commit 04fa8b3a and 2eec5fd2

src/turb/cs_turbulence_rij.cpp
@@ -413,8 +413,9 @@ _compute_up_rhop(int                 phase_id,
     turb_flux_model = cs_field_get_key_int(thf, kturt);
   const int turb_flux_model_type = turb_flux_model / 10;
 
-  cs_field_t *f_hf = cs_field_by_composite_name_try(thf->name,
-                                                    "turbulent_flux");
+  cs_field_t *f_hf = nullptr;
+  if (thf != nullptr)
+    f_hf = cs_field_by_composite_name_try(thf->name, "turbulent_flux");
 
   /* We want to use the computed turbulent heat fluxes (when they exist)
      when we are not dealing with atmo cases */
@@ -937,23 +938,11 @@ _gravity_st_epsilon(int              phase_id,
   if (f_cp != nullptr)
     cpro_cp = f_cp->val;
 
-  cs_field_t *f_t = cs_thermal_model_field();
-  cs_field_t *f_t_var = cs_field_get_variance(f_t);
-  const cs_real_t rvarfl = cs_field_get_key_double(f_t_var, krvarfl);
-
-  cs_field_t *f_alpha_theta
-    = cs_field_by_composite_name_try(f_t->name, "alpha");
-
-  cs_real_t *alpha_theta = nullptr;
-  if (f_alpha_theta != nullptr)
-    alpha_theta = f_alpha_theta->val;
-
   const cs_real_6_t *cvara_rij = (const cs_real_6_t *)f_rij->val_pre;
   const cs_real_t *cvara_ep = f_eps->val_pre;
   const cs_real_t *crom = f_rho->val;
   const cs_real_t *viscl = f_mu->val;
 
-  int turb_flux_model =  cs_field_get_key_int(f_t, kturt);
   const cs_turb_model_type_t iturb
     = (cs_turb_model_type_t)cs_glob_turb_model->iturb;
   int dissip_buo_mdl = cs_glob_turb_rans_model->dissip_buo_mdl;
@@ -962,11 +951,23 @@ _gravity_st_epsilon(int              phase_id,
   const cs_real_t ce1 = cs_turb_ce1;
   const cs_real_t ce3 = cs_turb_ce3;
 
-  /* Laminar thermal conductivity k for Prandtl calculation */
+  cs_field_t *f_alpha_theta = nullptr;
+  
+  cs_real_t rvarfl = 0.0;
+  int turb_flux_model =  -1;
+
   cs_lnum_t l_viscls = 0; /* stride for uniform/local viscosity access */
   cs_real_t _visls_0 = -1;
-  const cs_real_t *viscls = NULL;
-  {
+  cs_real_t *viscls = nullptr;
+
+  cs_field_t *f_t = cs_thermal_model_field(); 
+  if (f_t != nullptr){
+    turb_flux_model =  cs_field_get_key_int(f_t, kturt);
+    
+    cs_field_t *f_t_var = cs_field_get_variance(f_t);
+    if (f_t_var != nullptr)
+      rvarfl = cs_field_get_key_double(f_t_var, krvarfl);
+
     const int kivisl = cs_field_key_id("diffusivity_id");
     int ifcvsl = cs_field_get_key_int(f_t, kivisl);
     if (ifcvsl > -1) {
@@ -979,7 +980,13 @@ _gravity_st_epsilon(int              phase_id,
       viscls = &_visls_0;
       l_viscls = 0;
     }
+
+    f_alpha_theta = cs_field_by_composite_name_try(f_t->name, "alpha");
   }
+
+  cs_real_t *alpha_theta = nullptr;
+  if (f_alpha_theta != nullptr)
+    alpha_theta = f_alpha_theta->val;
 
   ctx.parallel_for(n_cells, [=] CS_F_HOST_DEVICE (cs_lnum_t c_id) {
 