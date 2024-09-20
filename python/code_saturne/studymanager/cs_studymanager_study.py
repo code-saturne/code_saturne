code-saturne/code_saturne

commit 5913622d1a938d8d4507ac303742797ce82fc30d

Rendre studymanager et launch_vnv.py avec l'option "--mem-log"

python/code_saturne/studymanager/cs_studymanager_study.py
@@ -630,7 +630,7 @@ class Case(object):
 
     #---------------------------------------------------------------------------
 
-    def build_run_cmd(self, resource_name=None):
+    def build_run_cmd(self, resource_name=None, mem_log=False):
         """
         Define run command with specified options.
         """
@@ -645,6 +645,9 @@ class Case(object):
                 + " --case " + refdir \
                 + " --dest " + self.__dest \
                 + " --id " + self.run_id
+
+        if mem_log:
+            run_cmd += " --mem-log"
 
         if self.kw_args:
             if self.kw_args.find(" ") < 0:
@@ -687,13 +690,13 @@ class Case(object):
 
     #---------------------------------------------------------------------------
 
-    def build_run_batch(self, resource_name=None):
+    def build_run_batch(self, resource_name=None, mem_log=False):
         """
         Launch run in RESU/run_id subdirectory in an batch mode.
         """
         run_cmd_batch = "cd " + self.run_dir + os.linesep
 
-        run_cmd_batch += self.build_run_cmd(resource_name)
+        run_cmd_batch += self.build_run_cmd(resource_name, mem_log)
 
         # append run_case.log in run_dir
         run_cmd_batch += " >> run_case.log 2>&1" + os.linesep + os.linesep
@@ -702,20 +705,27 @@ class Case(object):
 
     #---------------------------------------------------------------------------
 
-    def run(self, resource_name=None):
+    def run(self, resource_name=None, mem_log=False):
         """
         Launch run in RESU/run_id subdirectory.
         """
         home = os.getcwd()
         os.chdir(self.run_dir)
 
-        run_cmd = self.build_run_cmd(resource_name)
+        run_cmd = self.build_run_cmd(resource_name, mem_log)
 
         # append run_case.log in run_dir
         file_name = os.path.join(self.run_dir, "run_case.log")
         log_run = open(file_name, mode='a')
 
         error, self.is_time = run_studymanager_command(run_cmd, log_run)
+
+        mem_log_leak = False
+        if mem_log:
+            mem_log_leak_code = 2
+            if error & mem_log_leak_code:
+                mem_log_leak = True
+                error -= mem_log_leak_code
 
         if not error:
             self.is_run = "OK"
@@ -724,7 +734,7 @@ class Case(object):
 
         os.chdir(home)
 
-        return error
+        return error, mem_log_leak
 
     #---------------------------------------------------------------------------
 
@@ -1169,6 +1179,8 @@ class Studies(object):
         self.__dis_tex           = options.disable_tex
         # tex reports compilationpdflatex
         self.__pdflatex          = not options.disable_pdflatex
+        # use the mem-log option for unfreed pointers
+        self.__mem_log           = options.mem_log
 
         # Query install configuration and current environment
         # (add number of procs based on resources to install
@@ -1772,7 +1784,8 @@ class Studies(object):
                     self.reporting('    - running %s ...' % case.title,
                                    stdout=True, report=False, status=True)
 
-                    error = case.run(resource_name = self.__resource_name)
+                    error, mem_log_leak = case.run(resource_name = self.__resource_name,
+                                          mem_log = self.__mem_log)
                     if case.is_time:
                         is_time = "%s s" % case.is_time
                     else:
@@ -1801,6 +1814,11 @@ class Studies(object):
                         self.reporting('      * see run_case.log in ' + \
                                        case.run_dir)
 
+                    if self.__mem_log and mem_log_leak:
+                        self.reporting('    - run %s --> Leaks in memory logs' \
+                                       % (case.title))
+                        self.reporting('      * see cs_mem.log(.N) in ' \
+                                       % (case.run_dir))
                     self.__log_file.flush()
 
         self.reporting('')
@@ -1861,7 +1879,7 @@ class Studies(object):
                             if (cur_batch_size == 0) or (batch_total_time + \
                                float(case.expected_time) < self.__slurm_batch_wtime):
                                 # append content of batch command with run of the case
-                                batch_cmd += case.build_run_batch()
+                                batch_cmd += case.build_run_batch(mem_log=self.__mem_log)
                                 cur_batch_size += 1
                                 batch_total_time += float(case.expected_time)
                             else:
@@ -1933,7 +1951,7 @@ class Studies(object):
                             # complete batch info as previous one was submitted
                             if submit_prev:
                                 # append content of batch command with run of the case
-                                batch_cmd += case.build_run_batch()
+                                batch_cmd += case.build_run_batch(mem_log=self.__mem_log)
                                 cur_batch_size += 1
                                 batch_total_time += float(case.expected_time)
 

python/code_saturne/base/cs_case.py
@@ -2072,13 +2072,16 @@ class case:
                         pass
 
         e_caption = None
+
+        memory_leak = False
         for d in self.domains:
             d.copy_results()
             if d.error:
                 e_caption = d.error
             # check for memory leaks if needed
             if self.mem_log:
-                d.check_memory_log()
+                if d.check_memory_log() == 1:
+                    memory_leak = True
 
         # Remove directories if empty
 
@@ -2094,6 +2097,8 @@ class case:
             self.update_scripts_tmp('failed', 'failed', e_caption)
 
         self.update_scripts_tmp('saving', None)
+
+        return memory_leak
 
     #---------------------------------------------------------------------------
 
@@ -2277,6 +2282,7 @@ class case:
             retcode = cs_exec_environment.run_command(self.run_prologue)
             os.chdir(cwd)
 
+        mem_log_leaks = False
         try:
             retcode = 0
             if stages['prepare_data']:
@@ -2292,7 +2298,7 @@ class case:
                 self.run_solver()
 
             if stages['save_results'] == True:
-                self.save_results()
+                mem_log_leaks = self.save_results()
                 self.clear_exec_dir_stamp()
 
         finally:
@@ -2318,6 +2324,9 @@ class case:
                 err_str += self.error_long + '\n\n'
             sys.stderr.write(err_str)
             retcode = 1
+
+        if self.mem_log and mem_log_leaks:
+            retcode += 2
 
         if self.run_epilogue:
             cwd = os.getcwd()

python/code_saturne/base/cs_studymanager.py
@@ -183,6 +183,12 @@ def process_cmd_line(argv, pkg):
                       default=None, type=str, action='append',
                       help="additional sbatch arguments. use --slurm-batch-arg=<arg> to handle -- in argument.")
 
+    parser.add_option("--mem-log", dest="mem_log",
+                      action="store_true", default=False,
+                      help="set CS_MEM_LOG environment variable to 'cs_mem.log' " \
+                          + "and check that memory is correctly freed at the " \
+                          + "end of the computation.")
+
     if len(argv)==0:
         parser.print_help(sys.stderr)
         sys.exit(1)

commit d9fd47813b607111c1718341cecf62045e82c9e2

[SMGR] Fix error when --mem-log option is used (introduced in commit 5913622d)

python/code_saturne/studymanager/cs_studymanager_study.py
@@ -1859,7 +1859,7 @@ class Studies(object):
                     if self.__mem_log and mem_log_leak:
                         self.reporting('    - run %s --> Leaks in memory logs' \
                                        % (case.title))
-                        self.reporting('      * see cs_mem.log(.N) in ' \
+                        self.reporting('      * see cs_mem.log(.N) in %s' \
                                        % (case.run_dir))
                     self.__log_file.flush()
 