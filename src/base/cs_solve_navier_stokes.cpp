code-saturne/code_saturne

commit 2faf8d6c60dc14d234559e921d8bbdf6ff9033ef

Fix an other memory leak

src/base/cs_solve_navier_stokes.cpp
@@ -4769,8 +4769,7 @@ cs_solve_navier_stokes(const int        iterns,
   CS_FREE_HD(da_uu);
   CS_FREE_HD(dfrcxt);
 
-  if (iterns == vp_param->nterup)
-    CS_FREE_HD(trava);
+  CS_FREE_HD(trava);
 
   CS_FREE_HD(secvib);
   CS_FREE_HD(secvif);

src/mesh/cs_mesh_cartesian.cpp
@@ -261,7 +261,6 @@ _cs_mesh_cartesian_init(const char *name,
   _new_mesh->n_vtx_on_rank   = 0;
 
   _new_mesh->ndir = ndir;
-  _new_mesh->ndir = ndir;
   BFT_MALLOC(_new_mesh->params, ndir, _cs_mesh_cartesian_direction_t *);
   for (int i = 0; i < ndir; i++)
     _new_mesh->params[i] = NULL;
@@ -729,6 +728,7 @@ cs_mesh_cartesian_define_simple(const char *name,
 {
   cs_mesh_cartesian_params_t *mp = cs_mesh_cartesian_create(name);
 
+  assert(mp->ndir == 3);
   for (int idim = 0; idim < 3; idim++)
     mp->params[idim] =
       _cs_mesh_cartesian_create_direction(CS_MESH_CARTESIAN_CONSTANT_LAW,
@@ -1048,6 +1048,7 @@ cs_mesh_cartesian_define_dir_params(cs_mesh_cartesian_params_t  *mp,
     BFT_FREE(mp->params[idim]);
   }
 
+  assert(idim < mp->ndir);
   mp->params[idim] = _cs_mesh_cartesian_create_direction(_law,
                                                          ncells,
                                                          smin,

commit fbb0c194d610229d0e2ebe6a10b2f696ef2d1f8b

Fix bug introduced in commit 2faf8d6c
  * Array trava is declared as static so no memory leak occur as it is
    only allocated when is it null. Besides, when nterup > 1, we need to
    keep track of trava

src/base/cs_solve_navier_stokes.cpp
@@ -4769,7 +4769,8 @@ cs_solve_navier_stokes(const int        iterns,
   CS_FREE_HD(da_uu);
   CS_FREE_HD(dfrcxt);
 
-  CS_FREE_HD(trava);
+  if (iterns == vp_param->nterup)
+    CS_FREE_HD(trava);
 
   CS_FREE_HD(secvib);
   CS_FREE_HD(secvif);