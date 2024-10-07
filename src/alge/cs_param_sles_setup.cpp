code-saturne/code_saturne

commit 95ed2bc8aee4d321123bbf71a16fdf1203f4ecc2

Fix commit 8f7be1fc (yesterday)
The default settings chosen in that commit are available starting the
v3.21 of PETSc
This relies on a too recent version of PETSc. One switches to another
default settings.

src/alge/cs_param_sles_setup.cpp
@@ -407,7 +407,14 @@ _set_petsc_mg_levels(const char                        *prefix,
     case CS_PARAM_AMG_GAMG_L1_JACOBI:
       _petsc_cmd(true, prefix, "mg_levels_ksp_type", "richardson");
       _petsc_cmd(true, prefix, "mg_levels_pc_type", "jacobi");
+#if PETSC_VERSION_GE(3,21,0)
       _petsc_cmd(true, prefix, "mg_levels_pc_jacobi_type", "rowl1");
+#else
+      cs_base_warn(__FILE__, __LINE__);
+      cs_log_printf(CS_LOG_WARNINGS,
+                    "%s: CS_PARAM_AMG_GAMG_L1_JACOBI not set.\n"
+                    "  PETSc v3.21 or newer is requested.", __func__);
+#endif
       break;
 
     default:
@@ -456,7 +463,14 @@ _set_petsc_mg_levels(const char                        *prefix,
     case CS_PARAM_AMG_GAMG_L1_JACOBI:
       _petsc_cmd(true, prefix, "mg_levels_ksp_type", "richardson");
       _petsc_cmd(true, prefix, "mg_levels_pc_type", "jacobi");
+#if PETSC_VERSION_GE(3,21,0)
       _petsc_cmd(true, prefix, "mg_levels_pc_jacobi_type", "rowl1");
+#else
+      cs_base_warn(__FILE__, __LINE__);
+      cs_log_printf(CS_LOG_WARNINGS,
+                    "%s: CS_PARAM_AMG_GAMG_L1_JACOBI not set.\n"
+                    "  PETSc v3.21 or newer is requested.", __func__);
+#endif
       break;
 
     default:
@@ -500,7 +514,14 @@ _set_petsc_mg_levels(const char                        *prefix,
     case CS_PARAM_AMG_GAMG_L1_JACOBI:
       _petsc_cmd(true, prefix, "mg_levels_up_ksp_type", "richardson");
       _petsc_cmd(true, prefix, "mg_levels_up_pc_type", "jacobi");
-      _petsc_cmd(true, prefix, "mg_levels_up_pc_jacobi_type", "rowl1");
+#if PETSC_VERSION_GE(3,21,0)
+      _petsc_cmd(true, prefix, "mg_levels_pc_jacobi_type", "rowl1");
+#else
+      cs_base_warn(__FILE__, __LINE__);
+      cs_log_printf(CS_LOG_WARNINGS,
+                    "%s: CS_PARAM_AMG_GAMG_L1_JACOBI not set.\n"
+                    "  PETSc v3.21 or newer is requested.", __func__);
+#endif
       break;
 
     default:

src/alge/cs_param_amg.cpp
@@ -429,10 +429,10 @@ cs_param_amg_gamg_create(void)
 
   /* Main options */
 
-  gamgp->n_down_iter   = 3;
-  gamgp->down_smoother = CS_PARAM_AMG_GAMG_L1_JACOBI;
-  gamgp->n_up_iter     = 3;
-  gamgp->up_smoother   = CS_PARAM_AMG_GAMG_L1_JACOBI;
+  gamgp->n_down_iter   = 1;
+  gamgp->down_smoother = CS_PARAM_AMG_GAMG_FORWARD_GS;
+  gamgp->n_up_iter     = 1;
+  gamgp->up_smoother   = CS_PARAM_AMG_GAMG_BACKWARD_GS;
   gamgp->coarse_solver = CS_PARAM_AMG_GAMG_BJACOBI_LU;
 
   /* Advanced options */

commit 84438b744ba2997538d3510f3a55f1c8dc7bc24b

Fix bug introduced in commit 95ed2bc8
L1-scaled Jacobi was not taken into account in settings using a
distinct down/up smoother

src/alge/cs_param_sles_setup.cpp
@@ -515,7 +515,7 @@ _set_petsc_mg_levels(const char                        *prefix,
       _petsc_cmd(true, prefix, "mg_levels_up_ksp_type", "richardson");
       _petsc_cmd(true, prefix, "mg_levels_up_pc_type", "jacobi");
 #if PETSC_VERSION_GE(3,21,0)
-      _petsc_cmd(true, prefix, "mg_levels_pc_jacobi_type", "rowl1");
+      _petsc_cmd(true, prefix, "mg_levels_up_pc_jacobi_type", "rowl1");
 #else
       cs_base_warn(__FILE__, __LINE__);
       cs_log_printf(CS_LOG_WARNINGS,