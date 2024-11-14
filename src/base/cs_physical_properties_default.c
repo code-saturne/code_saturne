code-saturne/code_saturne

commit 57c01e33ab7909dabe540b035f85fb59cf0dce22

[Atmo] continue default setting of reference value (t0, ro0, p0) with meteo files.
The value of the GUI for ro0 is also not read if predefined law on.
This point has to be checked. It would be better to remove default
writting of `initial_value` for density when not necessary (e.g. when
metoe file is used).

Warning: this will change default values of some validation cases
because p0 and t0 was not considered when using meteo files.

src/base/cs_physical_properties_default.c
@@ -200,16 +200,16 @@ _rho_mu_is_constant(const char       *name,
                     const cs_real_t  val[],
                     const cs_real_t  val_ref)
 {
-  bool is_constant = false;
+  bool is_constant = true;
 
   for (cs_lnum_t ii = 0; ii < n_elts; ii++) {
     if (fabs(val[ii] - val_ref) <= cs_math_epzero)
       continue;
-    is_constant = true;
+    is_constant = false;
     break;
   }
 
-  if (is_constant)
+  if (!is_constant) {
     bft_error(__FILE__, __LINE__, 0,
               _("Error: abort in the physical quantities computation\n"
                 "=====\n"

src/atmo/atlecm.f90
@@ -74,6 +74,7 @@ double precision rap,rscp,tmoy, rhmoy
 double precision ztop, zzmax, tlkelv, pptop, dum
 double precision rhum,q0,q1
 double precision cpvcpa
+double precision pres, temp, qv
 
 character(len=80) :: ccomnt,oneline
 character(len=1)  :: csaute
@@ -102,16 +103,10 @@ rewind(unit=impmet, err=99)
 
 itp=0
 ih2o = 0
+!--> flag to take into account the humidity
+if (ippmod(iatmos).eq.2) ih2o=1
 
 cpvcpa = cp_v / cp_a
-
-if (imode.eq.1) then
-  rscp=rair/cp0
-
-  !--> flag to take into account the humidity
-  if (ippmod(iatmos).eq.2) ih2o=1
-
-endif
 
 !===============================================================================
 ! 1. Loop on time
@@ -225,10 +220,15 @@ endif
 if (ccomnt(1:1).eq.csaute) go to 103
 backspace(impmet)
 
-if (imode.eq.0) then
-  read(impmet, *, err=999, end=999)
-else
-  read(impmet, *, err=999, end=999) xyp_met(3, itp) ! pmer = p0
+read(impmet, *, err=999, end=999) pres
+
+! p_sea = p0
+if (itp.eq.1) then
+  p0 = pres
+endif
+
+if (imode.eq.1) then
+  xyp_met(3, itp) = pres
 endif
 
 !===============================================================================
@@ -246,11 +246,15 @@ if (imode.eq.0) then
   if (nbmett.le.1) then
     write(nfecra, 8001)
     call csexit (1)
-    !==========
   endif
 
   do ii = 1, nbmett
-    read (impmet,*,err=999,end=999) zzmax
+    read (impmet,*,err=999,end=999) zzmax, temp, qv
+    if (ii.eq.1 .and. itp.eq.1) then
+      t0 = temp + tkelvi
+      rhum = rair*(1.d0+(rvsra-1.d0)*qv*ih2o)
+      ro0 = p0 / t0 /rhum
+    endif
   enddo
 
   !-->  Computes nbmaxt:
@@ -330,12 +334,7 @@ endif
 if (imode.eq.1.and.itp.eq.1) then
   p0 = xyp_met(3, itp)
   t0 = ttmet(1,itp)+tkelvi
-  ! Humid atmosphere
-  if (ippmod(iatmos).eq.2) then
-    rhum = rair*(1.d0+(rvsra-1.d0)*qvmet(1,itp)*ih2o)
-  else
-    rhum = rair
-  endif
+  rhum = rair*(1.d0+(rvsra-1.d0)*qvmet(1,itp)*ih2o)
   ro0 = p0 / t0 /rhum
 endif
 

src/gui/cs_gui.c
@@ -344,6 +344,25 @@ _thermal_table_needed(const char *name)
   return choice;
 }
 
+/*----------------------------------------------------------------------------
+ * Return 1 if predefined_law
+ *
+ * parameters:
+ *   name        <--  name of the property
+ *----------------------------------------------------------------------------*/
+
+static int
+_thermal_predefined_law(const char *name)
+{
+  int choice = 0;
+
+  const char *prop_choice = _properties_choice(name, NULL);
+  if (cs_gui_strcmp(prop_choice, "predefined_law"))
+    choice = 1;
+
+  return choice;
+}
+
 /*-----------------------------------------------------------------------------
  * Compute a physical property based on a thermal law.
  *----------------------------------------------------------------------------*/
@@ -3997,14 +4016,14 @@ cs_gui_physical_properties(void)
 
   cs_vof_parameters_t *vof_param = cs_get_glob_vof_parameters();
 
-  if (_thermal_table_needed("density") == 0) {
+  if (_thermal_predefined_law("density") == 0)
     cs_gui_properties_value("density", &phys_pp->ro0);
-    if (vof_param->vof_model & CS_VOF_ENABLED) {
-      cs_gui_properties_value_by_fluid_id(0, "density", &vof_param->rho1);
-      cs_gui_properties_value_by_fluid_id(1, "density", &vof_param->rho2);
-    }
+  else if (_thermal_predefined_law("density") == 1
+      && (vof_param->vof_model & CS_VOF_ENABLED)) {
+    cs_gui_properties_value_by_fluid_id(0, "density", &vof_param->rho1);
+    cs_gui_properties_value_by_fluid_id(1, "density", &vof_param->rho2);
   }
-  else {
+  else if (_thermal_table_needed("density") == 1) {
     cs_phys_prop_compute(CS_PHYS_PROP_DENSITY,
                          1,
                          0,

commit 6a97e6145251385a0405e9b47676dc25b831c293

Fix compilation, broken by 57c01e33.

src/base/cs_physical_properties_default.c
@@ -209,7 +209,7 @@ _rho_mu_is_constant(const char       *name,
     break;
   }
 
-  if (!is_constant) {
+  if (!is_constant)
     bft_error(__FILE__, __LINE__, 0,
               _("Error: abort in the physical quantities computation\n"
                 "=====\n"