code-saturne/code_saturne

commit 1c6c1d3849258e9e18652a826e2d0f3c3c90bd0f

Dispatch: improve handling of thread counts and appli to MG coefficients assign.
On CPU, instead of using n or 1 threads depending on element count threshold,
progressively reduce thread count based on number of elements.
Replace dispatch context's `set_n_min_for_threads` by `set_n_min_per_thread`.

For MSR-based multigrid coefficients computation, dispatch to GPU when possible.

src/alge/cs_grid.cpp
@@ -63,6 +63,7 @@
 #include "bft_printf.h"
 
 #include "cs_base.h"
+#include "cs_dispatch.h"
 #include "cs_halo.h"
 #include "cs_halo_perio.h"
 #include "cs_log.h"
@@ -289,7 +290,7 @@ static cs_matrix_variant_t **_grid_tune_variant = nullptr;
  */
 /*----------------------------------------------------------------------------*/
 
-static inline cs_lnum_t
+CS_F_HOST_DEVICE static inline cs_lnum_t
 _l_id_binary_search(cs_lnum_t        l_id_array_size,
                     cs_lnum_t        l_id,
                     const cs_lnum_t  l_id_array[])
@@ -4734,6 +4735,15 @@ _automatic_aggregation_dx_msr(const cs_grid_t       *f,
 
   const cs_lnum_t f_nnz = row_index[f_n_rows];
 
+#if defined(HAVE_ACCEL)
+  if (cs_matrix_get_alloc_mode(f->matrix) > CS_ALLOC_HOST) {
+    cs_real_t  *d_val_p = const_cast<cs_real_t *>(d_val);
+    cs_real_t  *x_val_p = const_cast<cs_real_t *>(x_val);
+    cs_prefetch_d2h(d_val_p, f_n_rows*db_size*db_size*sizeof(cs_real_t));
+    cs_prefetch_d2h(x_val_p, f_nnz*sizeof(cs_real_t));
+  }
+#endif
+
   if (db_size > 1) {
     BFT_MALLOC(_d_val, f_n_rows, cs_real_t);
     _reduce_block(f_n_rows, db_size, d_val, _d_val);
@@ -6587,6 +6597,12 @@ _coarse_quantities_msr_with_faces_stage_1(const cs_grid_t  *f,
 
   int n_loc_threads = cs_parall_n_threads(f_n_rows, CS_THR_MIN);
 
+  cs_dispatch_context ctx;
+  if (alloc_mode == CS_ALLOC_HOST) {
+    ctx.set_use_gpu(false);
+    ctx.set_n_cpu_threads(n_loc_threads);
+  }
+
   cs_real_3_t  *c_face_normal = nullptr;
   cs_real_3_t  *c_cell_cen;
   cs_real_t    *c_cell_vol;
@@ -6601,8 +6617,7 @@ _coarse_quantities_msr_with_faces_stage_1(const cs_grid_t  *f,
   if (f_face_normal != nullptr) {
     CS_MALLOC_HD(c_face_normal, c_n_faces, cs_real_3_t, alloc_mode);
 
-    #pragma omp parallel num_threads(n_loc_threads)
-    for (cs_lnum_t i = 0; i < c_n_faces; i++) {
+    ctx.parallel_for(c_n_faces, [=] CS_F_HOST_DEVICE (cs_lnum_t i) {
       c_face_normal[i][0] = 0.;
       c_face_normal[i][1] = 0.;
       c_face_normal[i][2] = 0.;
@@ -6610,16 +6625,15 @@ _coarse_quantities_msr_with_faces_stage_1(const cs_grid_t  *f,
       c_xa0ij[i][0] = 0.;
       c_xa0ij[i][1] = 0.;
       c_xa0ij[i][2] = 0.;
-    }
+    });
   }
   else {
-    #pragma omp parallel num_threads(n_loc_threads)
-    for (cs_lnum_t i = 0; i < c_n_faces; i++) {
+    ctx.parallel_for(c_n_faces, [=] CS_F_HOST_DEVICE (cs_lnum_t i) {
       c_xa0[i] = 0.;
       c_xa0ij[i][0] = 0.;
       c_xa0ij[i][1] = 0.;
       c_xa0ij[i][2] = 0.;
-    }
+    });
   }
 
   const cs_lnum_t db_size = c->db_size;
@@ -6651,8 +6665,7 @@ _coarse_quantities_msr_with_faces_stage_1(const cs_grid_t  *f,
 
   /* Loop on coarse rows */
 
-  #pragma omp parallel for  num_threads(n_loc_threads)
-  for (cs_lnum_t ii_c = 0; ii_c < c_n_rows; ii_c++) {
+  ctx.parallel_for(c_n_rows, [=] CS_F_HOST_DEVICE (cs_lnum_t ii_c) {
 
     const cs_lnum_t c_s_idx = c_row_index[ii_c];
     const cs_lnum_t n_c_cols = c_row_index[ii_c+1] - c_s_idx;
@@ -6750,7 +6763,8 @@ _coarse_quantities_msr_with_faces_stage_1(const cs_grid_t  *f,
     for (cs_lnum_t coo_id = 0; coo_id < 3; coo_id++)
       _c_cell_cen[coo_id] /= c_cell_vol[ii_c];
 
-  } /* OpenMP Loop on coarse rows */
+  }); /* Loop on coarse rows */
+  ctx.wait();
 
   /* Add to coarse grid */
 
@@ -6925,11 +6939,19 @@ _compute_coarse_quantities_msr_with_faces(const cs_grid_t  *f,
   const cs_real_3_t *c_cell_cen
     = reinterpret_cast<const cs_real_3_t *>(c->cell_cen);
 
-  cs_lnum_t n_clips_min = 0, n_clips_max = 0;
-
   const cs_real_t relax_param = c->relaxation;
 
+  int *n_clips = nullptr;
+  if (verbosity > 3)
+    CS_MALLOC_HD(n_clips, c_n_rows*2, int, f->alloc_mode);
+
   int n_c_threads = cs_parall_n_threads(c_n_rows, CS_THR_MIN);
+
+  cs_dispatch_context ctx;
+  if (f->alloc_mode == CS_ALLOC_HOST) {
+    ctx.set_use_gpu(false);
+    ctx.set_n_cpu_threads(n_c_threads);
+  }
   {
     /* Matrix initialized to c_xa0 */
 
@@ -6937,13 +6959,15 @@ _compute_coarse_quantities_msr_with_faces(const cs_grid_t  *f,
     const short int *c_cell_face_sgn = c->cell_face_sgn;
 
     if (c_face_normal != nullptr) {
-      #pragma omp parallel for  num_threads(n_c_threads) \
-        reduction(+:n_clips_min, n_clips_max)
-      for (cs_lnum_t ic = 0; ic < c_n_rows; ic++) {
+      ctx.parallel_for(c_n_rows, [=] CS_F_HOST_DEVICE (cs_lnum_t ic) {
         const cs_lnum_t c_s_idx = c_row_index[ic];
         const cs_lnum_t c_e_idx = c_row_index[ic+1];
 
         const cs_real_t *cen_ic = c_cell_cen[ic];
+
+        if (n_clips != nullptr) {
+          n_clips[ic*2] = 0, n_clips[ic*2+1] = 0;
+        }
 
         for (cs_lnum_t r_idx = c_s_idx; r_idx < c_e_idx; r_idx++) {
           const cs_lnum_t jc = c_col_id[r_idx];
@@ -6964,12 +6988,12 @@ _compute_coarse_quantities_msr_with_faces(const cs_grid_t  *f,
                                       c_face_normal[c_face_id]);
             const cs_real_t agij = dsxaij/dsigjg;
 
-            if (agij < c_x_val_c || agij > 0.) {  // Clipped matrix
-              if (agij < c_x_val_c) n_clips_min++;
-              if (agij > 0.) n_clips_max++;
-            }
-            else {
+            if (agij >= c_x_val_c && agij <= 0.) {
               c_x_val_c = agij;
+            }
+            else if (n_clips != nullptr) { // clipped matrix
+              if (agij < c_x_val_c) n_clips[ic*2] += 1;  // clip to min
+              if (agij > 0.) n_clips[ic*2+1] += 1;       // clip to max
             }
           }
 
@@ -6982,17 +7006,20 @@ _compute_coarse_quantities_msr_with_faces(const cs_grid_t  *f,
             c_d_val[ic*db_stride + db_size*kk + kk] -= c_x_val_c;
           }
         }
-      }
+      });
+      ctx.wait();
     }
 
     else { // c_face_normal == nullptr
-      #pragma omp parallel for  num_threads(n_c_threads) \
-        reduction(+:n_clips_min, n_clips_max)
-      for (cs_lnum_t ic = 0; ic < c_n_rows; ic++) {
+      ctx.parallel_for(c_n_rows, [=] CS_F_HOST_DEVICE (cs_lnum_t ic) {
         const cs_lnum_t c_s_idx = c_row_index[ic];
         const cs_lnum_t c_e_idx = c_row_index[ic+1];
 
         const cs_real_t *cen_ic = c_cell_cen[ic];
+
+        if (n_clips != nullptr) {
+          n_clips[ic*2] = 0, n_clips[ic*2+1] = 0;
+        }
 
         for (cs_lnum_t r_idx = c_s_idx; r_idx < c_e_idx; r_idx++) {
           const cs_lnum_t jc = c_col_id[r_idx];
@@ -7014,12 +7041,12 @@ _compute_coarse_quantities_msr_with_faces(const cs_grid_t  *f,
                                       c_face_normal[c_face_id]);
             const cs_real_t agij = dsxaij/dsigjg;
 
-            if (agij < c_x_val_c || agij > 0.) {  // Clipped matrix
-              if (agij < c_x_val_c) n_clips_min++;
-              if (agij > 0.) n_clips_max++;
-            }
-            else {
+            if (agij >= c_x_val_c && agij <= 0.) {
               c_x_val_c = agij;
+            }
+            else if (n_clips != nullptr) { // clipped matrix
+              if (agij < c_x_val_c) n_clips[ic*2] += 1;  // clip to min
+              if (agij > 0.) n_clips[ic*2+1] += 1;       // clip to max
             }
           }
 
@@ -7032,7 +7059,8 @@ _compute_coarse_quantities_msr_with_faces(const cs_grid_t  *f,
             c_d_val[ic*db_stride + db_size*kk + kk] -= c_x_val_c;
           }
         }
-      }
+      });
+      ctx.wait();
     }
   }
 
@@ -7071,11 +7099,22 @@ _compute_coarse_quantities_msr_with_faces(const cs_grid_t  *f,
 
   /* Optional verification */
 
-  if (verbosity > 3)
+  if (verbosity > 3) {
+    cs_lnum_t n_clips_min = 0, n_clips_max = 0;
+    #pragma omp parallel for num_threads(n_c_threads) \
+      reduction(+:n_clips_min, n_clips_max)
+    for (cs_lnum_t ic = 0; ic < c_n_rows; ic++) {
+      n_clips_min += n_clips[ic*2];
+      n_clips_max += n_clips[ic*2 + 1];
+    }
+
     _verify_grid_quantities_msr(c,
                                 n_clips_min,
                                 n_clips_max,
                                 1);
+
+    CS_FREE(n_clips);
+  }
 }
 
 /*----------------------------------------------------------------------------
@@ -8880,7 +8919,7 @@ cs_grid_restrict_row_var(cs_dispatch_context  &ctx,
   if (ctx.use_gpu())
     sum_type = CS_DISPATCH_SUM_ATOMIC;
 #if defined(HAVE_OPENMP)
-  else if (ctx.n_min_for_cpu_threads() <= f_n_rows)
+  else if (ctx.n_min_per_cpu_thread() <= f_n_rows)
     sum_type = CS_DISPATCH_SUM_ATOMIC;
 #endif
 

src/base/cs_dispatch.h
@@ -187,34 +187,56 @@ class cs_host_context : public cs_dispatch_context_mixin<cs_host_context> {
 
 private:
 
-  cs_lnum_t  n_min_for_threads;  /*!< Run on single thread
-                                   under this threshold */
+  cs_lnum_t  n_min_per_thread;  /*!< Minimum number of elements per thread */
+  int        n_threads_;        /*!< If defined, force number of threads */
 
 public:
 
   cs_host_context()
-    : n_min_for_threads(CS_THR_MIN)
+    : n_min_per_thread(CS_THR_MIN), n_threads_(-1)
   {}
 
 public:
 
   //! Set minimum number of elements threshold for CPU multithread execution.
   void
-  set_n_min_for_cpu_threads(cs_lnum_t  n) {
-    this->n_min_for_threads = n;
+  set_n_min_per_cpu_thread(cs_lnum_t  n) {
+    this->n_min_per_thread = n;
   }
 
   //! Get minimum number of elements threshold for CPU multithread execution.
   cs_lnum_t
-  n_min_for_cpu_threads(void) {
-    return this->n_min_for_threads;
+  n_min_per_cpu_thread(void) {
+    return this->n_min_per_thread;
+  }
+
+  //! Set number of threads for CPU multithread execution.
+  void
+  set_n_cpu_threads(int  n) {
+    this->n_threads_ = n;
+  }
+
+  //! Get number of threads for CPU multithread execution (-1 if automatic)
+  int
+  n_cpu_threads(void) {
+    return this->n_threads_;
   }
 
   //! Iterate using a plain omp parallel for
   template <class F, class... Args>
   bool
   parallel_for(cs_lnum_t n, F&& f, Args&&... args) {
-#   pragma omp parallel for  if (n >= n_min_for_threads)
+    int n_t = n_threads_;
+    if (n_t < 0) {
+      n_t = cs_glob_n_threads;
+      int n_t_l = n / n_min_per_thread;
+      if (n_t_l < n_t)
+        n_t = n_t_l;
+      if (n_t < 1)
+        n_t = 1;
+    }
+
+#   pragma omp parallel for  num_threads(n_t)
     for (cs_lnum_t i = 0; i < n; ++i) {
       f(i, args...);
     }
@@ -268,8 +290,19 @@ public:
                           double&   sum,
                           F&&       f,
                           Args&&... args) {
+    int n_t = n_threads_;
+    if (n_t < 0) {
+      n_t = cs_glob_n_threads;
+      int n_t_l = n / n_min_per_thread;
+      if (n_t_l < n_t)
+        n_t = n_t_l;
+      if (n_t < 1)
+        n_t = 1;
+    }
+
     sum = 0;
-#   pragma omp parallel for reduction(+:sum) if (n >= n_min_for_threads)
+
+#   pragma omp parallel for reduction(+:sum) num_threads(n_t)
     for (cs_lnum_t i = 0; i < n; ++i) {
       f(i, sum, args...);
     }

tests/cs_dispatch_test.cpp
@@ -92,7 +92,7 @@ cs_dispatch_test(void)
 
     if (i == 1) {
       ctx.set_use_gpu(false);
-      ctx.set_n_min_for_cpu_threads(20);
+      ctx.set_n_min_per_cpu_thread(20);
     }
     else if (i == 2) {
       ctx.set_use_gpu(true);

commit 29c9b5d9956ac560fdf7923628b134e376ea374c

GPU + multigrid: fixes for mixed GPU/CPU levels following broken by 1c6c1d38.

src/alge/cs_grid.cpp
@@ -4243,13 +4243,15 @@ _coarse_msr_struct(int                f_level,
  * of the code where summing on faces may be done).
  *
  * parameters:
+ *   f             <-- pointer to parent structure.
  *   g             <-- pointer to grid structure.
  *   row_index     <-- matrix row index
  *   col_id        <-- matrix column ids
  *----------------------------------------------------------------------------*/
 
 static void
-_msr_face_adjacency(cs_grid_t        *g,
+_msr_face_adjacency(const cs_grid_t  *f,
+                    cs_grid_t        *g,
                     const cs_lnum_t  *restrict row_index,
                     const cs_lnum_t  *restrict col_id)
 {
@@ -4262,8 +4264,8 @@ _msr_face_adjacency(cs_grid_t        *g,
 
   cs_lnum_t *cell_to_face;
   short int *cell_to_face_sgn;
-  CS_MALLOC_HD(cell_to_face, c_nnz, cs_lnum_t, g->alloc_mode);
-  CS_MALLOC_HD(cell_to_face_sgn, c_nnz, short int, g->alloc_mode);
+  CS_MALLOC_HD(cell_to_face, c_nnz, cs_lnum_t, f->alloc_mode);
+  CS_MALLOC_HD(cell_to_face_sgn, c_nnz, short int, f->alloc_mode);
 
   cs_lnum_t *t_f_scan = nullptr;
   int n_loc_threads = cs_parall_n_threads(c_nnz, CS_THR_MIN);
@@ -5714,7 +5716,7 @@ _compute_coarse_quantities_native(const cs_grid_t  *fine_grid,
       cs_real_t *_f_xa0 = nullptr;
       f_xa0 = fine_grid->xa;
       if (fine_grid->symmetric == false) {
-        BFT_MALLOC(_f_xa0, f_n_faces, cs_real_t);
+        CS_MALLOC(_f_xa0, f_n_faces, cs_real_t);
         for (face_id = 0; face_id < f_n_faces; face_id++)
           _f_xa0[face_id] = 0.5 * (f_xa[face_id*2] + f_xa[face_id*2+1]);
         f_xa0 = _f_xa0;
@@ -6340,7 +6342,7 @@ _compute_coarse_quantities_msr(const cs_grid_t  *fine_grid,
   _coarse_to_fine_adjacency_msr(fine_grid->level,
                                 f_n_rows,
                                 c_n_rows,
-                                coarse_grid->alloc_mode,
+                                fine_grid->alloc_mode,
                                 n_f_threads,
                                 f_c_row,
                                 f_row_index,
@@ -6532,6 +6534,7 @@ _compute_coarse_quantities_msr(const cs_grid_t  *fine_grid,
  *
  * \param[in]       f              fine grid structure
  * \param[in, out]  c              coarse grid structure
+ * \param[in, out]  ctx             reference to dispatch context
  * \param[in]       f_row_index    fine matrix row index
  * \param[in]       f_col_id       fine matrix column ids
  * \param[in]       f_d_val        fine matrix diagonal values
@@ -6546,17 +6549,18 @@ _compute_coarse_quantities_msr(const cs_grid_t  *fine_grid,
 /*----------------------------------------------------------------------------*/
 
 static void
-_coarse_quantities_msr_with_faces_stage_1(const cs_grid_t  *f,
-                                          cs_grid_t        *c,
-                                          const cs_lnum_t  *f_row_index,
-                                          const cs_lnum_t  *f_col_id,
-                                          const cs_real_t  *f_d_val,
-                                          const cs_real_t  *f_x_val,
-                                          const cs_lnum_t  *c_f_row_index,
-                                          const cs_lnum_t  *c_f_row_ids,
-                                          const cs_lnum_t  *c_row_index,
-                                          const cs_lnum_t  *c_col_id,
-                                          cs_real_t        *c_d_val)
+_coarse_quantities_msr_with_faces_stage_1(const cs_grid_t      *f,
+                                          cs_grid_t            *c,
+                                          cs_dispatch_context  &ctx,
+                                          const cs_lnum_t      *f_row_index,
+                                          const cs_lnum_t      *f_col_id,
+                                          const cs_real_t      *f_d_val,
+                                          const cs_real_t      *f_x_val,
+                                          const cs_lnum_t      *c_f_row_index,
+                                          const cs_lnum_t      *c_f_row_ids,
+                                          const cs_lnum_t      *c_row_index,
+                                          const cs_lnum_t      *c_col_id,
+                                          cs_real_t            *c_d_val)
 {
   std::chrono::high_resolution_clock::time_point t_0;
   std::chrono::high_resolution_clock::time_point t_1;
@@ -6597,11 +6601,8 @@ _coarse_quantities_msr_with_faces_stage_1(const cs_grid_t  *f,
 
   int n_loc_threads = cs_parall_n_threads(f_n_rows, CS_THR_MIN);
 
-  cs_dispatch_context ctx;
-  if (alloc_mode == CS_ALLOC_HOST) {
-    ctx.set_use_gpu(false);
+  if (ctx.use_gpu() == false)
     ctx.set_n_cpu_threads(n_loc_threads);
-  }
 
   cs_real_3_t  *c_face_normal = nullptr;
   cs_real_3_t  *c_cell_cen;
@@ -6860,7 +6861,7 @@ _compute_coarse_quantities_msr_with_faces(const cs_grid_t  *f,
   _coarse_to_fine_adjacency_msr(f->level,
                                 f_n_rows,
                                 c_n_rows,
-                                c->alloc_mode,
+                                f->alloc_mode,
                                 n_f_threads,
                                 f_c_row,
                                 f_row_index,
@@ -6895,30 +6896,59 @@ _compute_coarse_quantities_msr_with_faces(const cs_grid_t  *f,
 
   cs_lnum_t c_nnz = c_row_index[c_n_rows];
 
-  cs_real_t *restrict c_d_val, *restrict c_x_val;
+  cs_real_t *c_d_val, *c_x_val;
   CS_MALLOC_HD(c_d_val, c_n_rows*db_stride, cs_real_t, c->alloc_mode);
   CS_MALLOC_HD(c_x_val, c_nnz, cs_real_t, c->alloc_mode);
 
   /* Compute face adjacency
      ---------------------- */
 
-  _msr_face_adjacency(c,
+  _msr_face_adjacency(f,
+                      c,
                       c_row_index,
                       c_col_id);
+
+  const cs_lnum_t *_c_row_index = c_row_index;
+  const cs_lnum_t *_c_col_id = c_col_id;
+  cs_real_t *restrict _c_d_val = c_d_val;
+  cs_real_t *restrict _c_x_val = c_x_val;
+
+  if (   f->alloc_mode > CS_ALLOC_HOST
+      && c->alloc_mode == CS_ALLOC_HOST) {
+    CS_REALLOC_HD(c_row_index, c_n_rows+1, cs_lnum_t, CS_ALLOC_HOST_DEVICE);
+    cs_sync_h2d(c_row_index);
+    _c_row_index = (const cs_lnum_t *)cs_get_device_ptr(c_row_index);
+
+    CS_REALLOC_HD(c_col_id, c_nnz, cs_lnum_t, CS_ALLOC_HOST_DEVICE);
+    cs_sync_h2d(c_col_id);
+    _c_col_id = (const cs_lnum_t *)cs_get_device_ptr(c_col_id);
+
+    CS_REALLOC_HD(c_d_val, c_n_rows*db_stride, cs_real_t, CS_ALLOC_HOST_DEVICE);
+    _c_d_val = (cs_real_t *)cs_get_device_ptr(c_d_val);
+
+    CS_REALLOC_HD(c_x_val, c_nnz, cs_real_t, CS_ALLOC_HOST_DEVICE);
+    _c_x_val = (cs_real_t *)cs_get_device_ptr(c_x_val);
+  }
+
+  cs_dispatch_context ctx;
+  if (f->alloc_mode == CS_ALLOC_HOST) {
+    ctx.set_use_gpu(false);
+  }
 
   /* Compute coarse quantities, first pass */
 
   _coarse_quantities_msr_with_faces_stage_1(f,
                                             c,
+                                            ctx,
                                             f_row_index,
                                             f_col_id,
                                             f_d_val,
                                             f_x_val,
                                             c_f_row_index,
                                             c_f_row_ids,
-                                            c_row_index,
-                                            c_col_id,
-                                            c_d_val);
+                                            _c_row_index,
+                                            _c_col_id,
+                                            _c_d_val);
 
   /* Assign values
      ------------- */
@@ -6947,11 +6977,9 @@ _compute_coarse_quantities_msr_with_faces(const cs_grid_t  *f,
 
   int n_c_threads = cs_parall_n_threads(c_n_rows, CS_THR_MIN);
 
-  cs_dispatch_context ctx;
-  if (f->alloc_mode == CS_ALLOC_HOST) {
-    ctx.set_use_gpu(false);
+  if (ctx.use_gpu() == false)
     ctx.set_n_cpu_threads(n_c_threads);
-  }
+
   {
     /* Matrix initialized to c_xa0 */
 
@@ -6960,8 +6988,8 @@ _compute_coarse_quantities_msr_with_faces(const cs_grid_t  *f,
 
     if (c_face_normal != nullptr) {
       ctx.parallel_for(c_n_rows, [=] CS_F_HOST_DEVICE (cs_lnum_t ic) {
-        const cs_lnum_t c_s_idx = c_row_index[ic];
-        const cs_lnum_t c_e_idx = c_row_index[ic+1];
+        const cs_lnum_t c_s_idx = _c_row_index[ic];
+        const cs_lnum_t c_e_idx = _c_row_index[ic+1];
 
         const cs_real_t *cen_ic = c_cell_cen[ic];
 
@@ -6970,7 +6998,7 @@ _compute_coarse_quantities_msr_with_faces(const cs_grid_t  *f,
         }
 
         for (cs_lnum_t r_idx = c_s_idx; r_idx < c_e_idx; r_idx++) {
-          const cs_lnum_t jc = c_col_id[r_idx];
+          const cs_lnum_t jc = _c_col_id[r_idx];
           const cs_lnum_t c_face_id = c_cell_face[r_idx];
           const cs_real_t sgn = (cs_real_t)c_cell_face_sgn[r_idx];
           const cs_real_t *cen_jc = c_cell_cen[jc];
@@ -7001,9 +7029,9 @@ _compute_coarse_quantities_msr_with_faces(const cs_grid_t  *f,
           c_x_val_c =         relax_param *c_x_val_c
                       + (1. - relax_param)*c_xa0[c_face_id];
 
-          c_x_val[r_idx] = c_x_val_c;
+          _c_x_val[r_idx] = c_x_val_c;
           for (cs_lnum_t kk = 0; kk < db_size; kk++) {
-            c_d_val[ic*db_stride + db_size*kk + kk] -= c_x_val_c;
+            _c_d_val[ic*db_stride + db_size*kk + kk] -= c_x_val_c;
           }
         }
       });
@@ -7012,8 +7040,8 @@ _compute_coarse_quantities_msr_with_faces(const cs_grid_t  *f,
 
     else { // c_face_normal == nullptr
       ctx.parallel_for(c_n_rows, [=] CS_F_HOST_DEVICE (cs_lnum_t ic) {
-        const cs_lnum_t c_s_idx = c_row_index[ic];
-        const cs_lnum_t c_e_idx = c_row_index[ic+1];
+        const cs_lnum_t c_s_idx = _c_row_index[ic];
+        const cs_lnum_t c_e_idx = _c_row_index[ic+1];
 
         const cs_real_t *cen_ic = c_cell_cen[ic];
 
@@ -7022,7 +7050,7 @@ _compute_coarse_quantities_msr_with_faces(const cs_grid_t  *f,
         }
 
         for (cs_lnum_t r_idx = c_s_idx; r_idx < c_e_idx; r_idx++) {
-          const cs_lnum_t jc = c_col_id[r_idx];
+          const cs_lnum_t jc = _c_col_id[r_idx];
           const cs_lnum_t c_face_id = c_cell_face[r_idx];
           const cs_real_t sgn = (cs_real_t)c_cell_face_sgn[r_idx];
           const cs_real_t *cen_jc = c_cell_cen[jc];
@@ -7054,9 +7082,9 @@ _compute_coarse_quantities_msr_with_faces(const cs_grid_t  *f,
           c_x_val_c =         relax_param *c_x_val_c
                       + (1. - relax_param)*c_xa0[c_face_id];
 
-          c_x_val[r_idx] = c_x_val_c;
+          _c_x_val[r_idx] = c_x_val_c;
           for (cs_lnum_t kk = 0; kk < db_size; kk++) {
-            c_d_val[ic*db_stride + db_size*kk + kk] -= c_x_val_c;
+            _c_d_val[ic*db_stride + db_size*kk + kk] -= c_x_val_c;
           }
         }
       });
@@ -7076,6 +7104,21 @@ _compute_coarse_quantities_msr_with_faces(const cs_grid_t  *f,
   }
 
   /* Free working arrays */
+
+  if (   f->alloc_mode > CS_ALLOC_HOST
+      && c->alloc_mode == CS_ALLOC_HOST) {
+    CS_REALLOC_HD(c_row_index, c_n_rows+1, cs_lnum_t, CS_ALLOC_HOST);
+    CS_REALLOC_HD(c_col_id, c_nnz, cs_lnum_t, CS_ALLOC_HOST);
+    _c_row_index = nullptr;
+    _c_col_id = nullptr;
+
+    cs_sync_d2h(c_d_val);
+    cs_sync_d2h(c_x_val);
+    CS_REALLOC_HD(c_d_val, c_n_rows*db_stride, cs_real_t, CS_ALLOC_HOST);
+    CS_REALLOC_HD(c_x_val, c_nnz, cs_real_t, CS_ALLOC_HOST);
+    _c_d_val = nullptr;
+    _c_x_val = nullptr;
+  }
 
   CS_FREE(c_f_row_ids);
   CS_FREE(c_f_row_index);
@@ -8648,6 +8691,7 @@ cs_grid_project_var(const cs_grid_t  *g,
     cs_dispatch_context ctx;
     if (amode == CS_ALLOC_HOST)
       ctx.set_use_gpu(false);
+    ctx.set_use_gpu(false);
 
     /* Allocate temporary arrays */
 