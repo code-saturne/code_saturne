code-saturne/code_saturne

commit 352ee6a17fd6273f5d085e4bd432d80fedecdc59

Internal coupling: fix halo type and memory leaks.
The halo type became an issue with commits 97768e76 and 491bcb3e, though
an underlying issue may have been present.

src/alge/cs_matrix_default.cpp
@@ -27,7 +27,7 @@
 #include "cs_defs.h"
 
 /*----------------------------------------------------------------------------
- * Standard C library headers
+ * Standard library headers
  *----------------------------------------------------------------------------*/
 
 #include <stdarg.h>
@@ -93,11 +93,11 @@ BEGIN_C_DECLS
 /*! \cond DOXYGEN_SHOULD_SKIP_THIS */
 
 /*=============================================================================
- * Local Macro Definitions
+ * Local macro definitions
  *============================================================================*/
 
 /*=============================================================================
- * Local Type Definitions
+ * Local type definitions
  *============================================================================*/
 
 /*============================================================================
@@ -858,8 +858,11 @@ cs_matrix_default_set_type(cs_matrix_fill_type_t  fill_type,
  * \brief Return a (0-based) global block row numbering for a given matrix.
  *
  * The numbering is built or updated if not previously used, or if the
- * previous call considered a different matrix, and is simply returned
- * otherwise. In other words, this works as a matrix global numbering cache.
+ * previous call considered a different matrix or halo, and is simply
+ * returned otherwise.
+ * In other words, this works as a matrix global numbering cache.
+ *
+ * The matrix's halo is used for the update.
  *
  * \param[in]  m  associated matrix
  *
@@ -870,8 +873,33 @@ cs_matrix_default_set_type(cs_matrix_fill_type_t  fill_type,
 const cs_gnum_t *
 cs_matrix_get_block_row_g_id(const cs_matrix_t  *m)
 {
+  return cs_matrix_get_block_row_g_id(m, m->halo);
+}
+
+END_C_DECLS
+#ifdef __cplusplus
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Return a (0-based) global block row numbering for a given matrix.
+ *
+ * The numbering is built or updated if not previously used, or if the
+ * previous call considered a different matrix or halo, and is simply
+ * returned otherwise.
+ * In other words, this works as a matrix global numbering cache.
+ *
+ * \param[in]  m     associated matrix
+ * \param[in]  halo  associated halo
+ *
+ * \return  pointer to requested global numbering
+ */
+/*----------------------------------------------------------------------------*/
+
+const cs_gnum_t *
+cs_matrix_get_block_row_g_id(const cs_matrix_t  *m,
+                             const cs_halo_t    *halo)
+{
   const cs_lnum_t  n_rows = m->n_rows;
-  const cs_halo_t  *halo = m->halo;
   const cs_gnum_t *l_range = nullptr;
 
   if (m->assembler != nullptr)
@@ -887,6 +915,9 @@ cs_matrix_get_block_row_g_id(const cs_matrix_t  *m)
 
   return g_row_num;
 }
+
+#endif /* cplusplus */
+BEGIN_C_DECLS
 
 /*----------------------------------------------------------------------------*/
 /*!
@@ -1071,6 +1102,32 @@ cs_matrix_set_coefficients_by_assembler(const cs_field_t  *f,
   cs_matrix_assembler_values_finalize(&mav);
 
   return m;
+}
+
+/*----------------------------------------------------------------------------
+ * Release of destroy matrix depending on whether is is cached or not.
+ *
+ * Matrices built by assembler are destroyed.
+ *
+ * parameters:
+ *   matrix <-> pointer to matrix structure pointer
+ *----------------------------------------------------------------------------*/
+
+void
+cs_matrix_release(cs_matrix_t  **m)
+{
+  if (m == nullptr)
+    return;
+
+  cs_matrix_t *_m = *m;
+
+  if (_m == nullptr)
+    return;
+
+  cs_matrix_release_coefficients(_m);
+
+  if (_m != _matrix[cs_matrix_get_type(_m)])
+    cs_matrix_destroy(m);
 }
 
 /*----------------------------------------------------------------------------*/

src/alge/cs_matrix_default.h
@@ -228,7 +228,7 @@ int
 cs_matrix_get_tuning_runs(void);
 
 /*----------------------------------------------------------------------------*/
-/*!
+/*
  * \brief Set default matrix type for a given fill type.
  *
  * \param[in] fill type  Fill type for which tuning behavior is set
@@ -241,12 +241,15 @@ cs_matrix_default_set_type(cs_matrix_fill_type_t  fill_type,
                            cs_matrix_type_t       type);
 
 /*----------------------------------------------------------------------------*/
-/*!
+/*
  * \brief Return a (0-based) global block row numbering for a given matrix.
  *
  * The numbering is built or updated if not previously used, or if the
- * previous call considered a differeent matrix, and is simply returned
- * otherwise. In other words, this works as a matrix global numbering cache.
+ * previous call considered a different matrix or halo, and is simply
+ * returned otherwise.
+ * In other words, this works as a matrix global numbering cache.
+ *
+ * The matrix's halo is used for the update.
  *
  * \param[in]  m  associated matrix
  *
@@ -256,6 +259,32 @@ cs_matrix_default_set_type(cs_matrix_fill_type_t  fill_type,
 
 const cs_gnum_t *
 cs_matrix_get_block_row_g_id(const cs_matrix_t  *m);
+
+END_C_DECLS
+#ifdef __cplusplus
+
+/*----------------------------------------------------------------------------*/
+/*
+ * \brief Return a (0-based) global block row numbering for a given matrix.
+ *
+ * The numbering is built or updated if not previously used, or if the
+ * previous call considered a different matrix or halo, and is simply
+ * returned otherwise.
+ * In other words, this works as a matrix global numbering cache.
+ *
+ * \param[in]  m     associated matrix
+ * \param[in]  halo  associated halo
+ *
+ * \return  pointer to requested global numbering
+ */
+/*----------------------------------------------------------------------------*/
+
+const cs_gnum_t *
+cs_matrix_get_block_row_g_id(const cs_matrix_t  *m,
+                             const cs_halo_t    *halo);
+
+#endif /* cplusplus */
+BEGIN_C_DECLS
 
 /*----------------------------------------------------------------------------*/
 /*
@@ -301,6 +330,18 @@ cs_matrix_set_coefficients_by_assembler(const cs_field_t  *f,
                                         cs_lnum_t          extra_diag_block_size,
                                         const cs_real_t   *da,
                                         const cs_real_t   *xa);
+
+/*----------------------------------------------------------------------------
+ * Release of destroy matrix depending on whether is is cached or not.
+ *
+ * Matrices built by assembler are destroyed.
+ *
+ * parameters:
+ *   matrix <-> pointer to matrix structure pointer
+ *----------------------------------------------------------------------------*/
+
+void
+cs_matrix_release(cs_matrix_t  **m);
 
 /*----------------------------------------------------------------------------*/
 

src/alge/cs_sles_default.cpp
@@ -832,6 +832,21 @@ cs_sles_default_get_matrix(int          f_id,
   return a;
 }
 
+/*----------------------------------------------------------------------------
+ * Release of destroy matrix depending on whether is is cached or not.
+ *
+ * Matrices built by assembler are destroyed.
+ *
+ * parameters:
+ *   matrix <-> pointer to matrix structure pointer
+ *----------------------------------------------------------------------------*/
+
+void
+cs_sles_default_release_matrix(cs_matrix_t  **m)
+{
+  cs_matrix_release(m);
+}
+
 /*----------------------------------------------------------------------------*/
 /*!
  * \brief Call sparse linear equation solver setup for convection-diffusion
@@ -981,7 +996,6 @@ cs_sles_solve_ccc_fv(cs_sles_t           *sc,
   cs_assert(n_rows == m->n_cells);
 
   if (n_cols_ext > m->n_cells_with_ghosts) {
-
     cs_alloc_mode_t amode = cs_matrix_get_alloc_mode(a);
 
     cs_dispatch_context ctx;
@@ -990,16 +1004,25 @@ cs_sles_solve_ccc_fv(cs_sles_t           *sc,
     cs_lnum_t db_size = cs_matrix_get_diag_block_size(a);
     assert(n_rows == m->n_cells);
     cs_lnum_t _n_rows = n_rows*db_size;
-    CS_MALLOC_HD(_rhs, n_cols_ext*db_size, cs_real_t, amode);
     CS_MALLOC_HD(_vx, n_cols_ext*db_size, cs_real_t, amode);
     ctx.parallel_for(_n_rows, [=] CS_F_HOST_DEVICE (cs_lnum_t i) {
-      _rhs[i] = rhs[i];
       _vx[i] = vx[i];
     });
-    ctx.wait();
-    cs_matrix_pre_vector_multiply_sync(a, _rhs);
-    rhs_p = _rhs;
 
+    if (cs_matrix_get_type(a) == CS_MATRIX_NATIVE) {
+      CS_MALLOC_HD(_rhs, n_cols_ext*db_size, cs_real_t, amode);
+      rhs_p = _rhs;
+
+      cs_lnum_t _n_ext = (n_cols_ext - n_rows)*db_size;
+      ctx.parallel_for(_n_rows, [=] CS_F_HOST_DEVICE (cs_lnum_t i) {
+        _rhs[i] = rhs[i];
+      });
+      ctx.parallel_for(_n_ext, [=] CS_F_HOST_DEVICE (cs_lnum_t i) {
+        _rhs[_n_rows + i] = 0;
+      });
+    };
+
+    ctx.wait();
   }
 
   /* Solve system */

src/alge/cs_sles_default.h
@@ -127,6 +127,18 @@ cs_sles_default_get_matrix(int          f_id,
                            bool         symmetric);
 
 /*----------------------------------------------------------------------------
+ * Release of destroy matrix depending on whether is is cached or not.
+ *
+ * Matrices built by assembler are destroyed.
+ *
+ * parameters:
+ *   matrix <-> pointer to matrix structure
+ *----------------------------------------------------------------------------*/
+
+void
+cs_sles_default_release_matrix(cs_matrix_t  **m);
+
+/*----------------------------------------------------------------------------
  * Default finalization for sparse linear equation solver API.
  *
  * This includes performance data logging output.

src/base/cs_equation_iterative_solve.cpp
@@ -1105,7 +1105,7 @@ _equation_iterative_solve_strided(int                   idtvar,
    *==========================================================================*/
 
   cs_sles_free(sc);
-  cs_matrix_release_coefficients(a);
+  cs_matrix_release(&a);
 
   if (stride == 3) {
     /* Save diagonal in case we want to use it */
@@ -2030,7 +2030,7 @@ cs_equation_iterative_solve_scalar(int                   idtvar,
    *==========================================================================*/
 
   cs_sles_free(sc);
-  cs_matrix_release_coefficients(a);
+  cs_sles_default_release_matrix(&a);
 
   ctx.wait();
 

src/base/cs_pressure_correction.cpp
@@ -431,7 +431,7 @@ _hydrostatic_pressure_compute(const cs_mesh_t       *m,
   /* Free Memory and solver setup */
 
   cs_sles_free(sc);
-  cs_matrix_release_coefficients(a);
+  cs_sles_default_release_matrix(&a);
 
   CS_FREE_HD(viscce);
   CS_FREE_HD(rovsdt);
@@ -2632,7 +2632,7 @@ _pressure_correction_fv(int                   iterns,
      --------------------------------- */
 
   cs_sles_free(sc);
-  cs_matrix_release_coefficients(a);
+  cs_sles_default_release_matrix(&a);
 
   CS_FREE_HD(c2);
 

src/base/cs_solve_navier_stokes.cpp
@@ -515,7 +515,7 @@ _cs_mass_flux_prediction(const cs_mesh_t       *m,
      ----------------- */
 
   cs_sles_free(sc);
-  cs_matrix_release_coefficients(a);
+  cs_sles_default_release_matrix(&a);
 
   CS_FREE_HD(divu);
   CS_FREE_HD(rhs);

src/alge/cs_matrix_building.cpp
@@ -2038,7 +2038,7 @@ cs_matrix_compute_coeffs(cs_matrix_t                 *a,
         = cs_matrix_assembler_values_init(a, 1, 1);
       assert(n_cells == cs_matrix_get_n_rows(a));
 
-      const cs_gnum_t *r_g_id = cs_matrix_get_block_row_g_id(a);
+      const cs_gnum_t *r_g_id = cs_matrix_get_block_row_g_id(a, m->halo);
 
       const cs_lnum_t block_size = 800;
       cs_gnum_t g_row_id[800];
@@ -2360,7 +2360,7 @@ cs_matrix_compute_coeffs
         = cs_matrix_assembler_values_init(a, stride, eb_size);
       assert(n_cells == cs_matrix_get_n_rows(a));
 
-      const cs_gnum_t *r_g_id = cs_matrix_get_block_row_g_id(a);
+      const cs_gnum_t *r_g_id = cs_matrix_get_block_row_g_id(a, m->halo);
 
       const cs_lnum_t block_size = 800;
       cs_gnum_t g_row_id[800];

commit 61541e30772012c4541b6f842096c12dbae600ae

Fix double-free when using external solvers, broken by 352ee6a1 (2024-11-25).

src/alge/cs_matrix_default.cpp
@@ -1104,32 +1104,6 @@ cs_matrix_set_coefficients_by_assembler(const cs_field_t  *f,
   return m;
 }
 
-/*----------------------------------------------------------------------------
- * Release of destroy matrix depending on whether is is cached or not.
- *
- * Matrices built by assembler are destroyed.
- *
- * parameters:
- *   matrix <-> pointer to matrix structure pointer
- *----------------------------------------------------------------------------*/
-
-void
-cs_matrix_release(cs_matrix_t  **m)
-{
-  if (m == nullptr)
-    return;
-
-  cs_matrix_t *_m = *m;
-
-  if (_m == nullptr)
-    return;
-
-  cs_matrix_release_coefficients(_m);
-
-  if (_m != _matrix[cs_matrix_get_type(_m)])
-    cs_matrix_destroy(m);
-}
-
 /*----------------------------------------------------------------------------*/
 
 END_C_DECLS

src/alge/cs_matrix_default.h
@@ -331,18 +331,6 @@ cs_matrix_set_coefficients_by_assembler(const cs_field_t  *f,
                                         const cs_real_t   *da,
                                         const cs_real_t   *xa);
 
-/*----------------------------------------------------------------------------
- * Release of destroy matrix depending on whether is is cached or not.
- *
- * Matrices built by assembler are destroyed.
- *
- * parameters:
- *   matrix <-> pointer to matrix structure pointer
- *----------------------------------------------------------------------------*/
-
-void
-cs_matrix_release(cs_matrix_t  **m);
-
 /*----------------------------------------------------------------------------*/
 
 END_C_DECLS

src/alge/cs_sles_default.cpp
@@ -832,21 +832,6 @@ cs_sles_default_get_matrix(int          f_id,
   return a;
 }
 
-/*----------------------------------------------------------------------------
- * Release of destroy matrix depending on whether is is cached or not.
- *
- * Matrices built by assembler are destroyed.
- *
- * parameters:
- *   matrix <-> pointer to matrix structure pointer
- *----------------------------------------------------------------------------*/
-
-void
-cs_sles_default_release_matrix(cs_matrix_t  **m)
-{
-  cs_matrix_release(m);
-}
-
 /*----------------------------------------------------------------------------*/
 /*!
  * \brief Call sparse linear equation solver setup for convection-diffusion

src/alge/cs_sles_default.h
@@ -127,18 +127,6 @@ cs_sles_default_get_matrix(int          f_id,
                            bool         symmetric);
 
 /*----------------------------------------------------------------------------
- * Release of destroy matrix depending on whether is is cached or not.
- *
- * Matrices built by assembler are destroyed.
- *
- * parameters:
- *   matrix <-> pointer to matrix structure
- *----------------------------------------------------------------------------*/
-
-void
-cs_sles_default_release_matrix(cs_matrix_t  **m);
-
-/*----------------------------------------------------------------------------
  * Default finalization for sparse linear equation solver API.
  *
  * This includes performance data logging output.

src/base/cs_equation_iterative_solve.cpp
@@ -1079,7 +1079,7 @@ _equation_iterative_solve_strided(int                   idtvar,
    *==========================================================================*/
 
   cs_sles_free(sc);
-  cs_matrix_release(&a);
+  cs_matrix_release_coefficients(a);
 
   if (stride == 3) {
     /* Save diagonal in case we want to use it */
@@ -1990,7 +1990,7 @@ cs_equation_iterative_solve_scalar(int                   idtvar,
    *==========================================================================*/
 
   cs_sles_free(sc);
-  cs_sles_default_release_matrix(&a);
+  cs_matrix_release_coefficients(a);
 
   ctx.wait();
 

src/base/cs_pressure_correction.cpp
@@ -432,7 +432,7 @@ _hydrostatic_pressure_compute(const cs_mesh_t       *m,
   /* Free Memory and solver setup */
 
   cs_sles_free(sc);
-  cs_sles_default_release_matrix(&a);
+  cs_matrix_release_coefficients(a);
 
   CS_FREE_HD(viscce);
   CS_FREE_HD(rovsdt);
@@ -2640,7 +2640,7 @@ _pressure_correction_fv(int                   iterns,
      --------------------------------- */
 
   cs_sles_free(sc);
-  cs_sles_default_release_matrix(&a);
+  cs_matrix_release_coefficients(a);
 
   CS_FREE_HD(c2);
 

src/base/cs_solve_navier_stokes.cpp
@@ -516,7 +516,7 @@ _cs_mass_flux_prediction(const cs_mesh_t       *m,
      ----------------- */
 
   cs_sles_free(sc);
-  cs_sles_default_release_matrix(&a);
+  cs_matrix_release_coefficients(a);
 
   CS_FREE_HD(divu);
   CS_FREE_HD(rhs);