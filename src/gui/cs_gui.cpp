code-saturne/code_saturne

commit e5380cbafb0d365b257b74e89cea89d90ecb0891

Update HTSolver with additional outputs from GUI
  * Allow postprocessing of physical properties for HTSolver
  * remove spurious prints added by mistake in commit 23c994389

src/gui/cs_gui.cpp
@@ -1996,8 +1996,6 @@ cs_gui_dt_param(void)
     if (_t_max >= 0)
       time_stp->t_max = time_stp->t_prev + _t_max;
   }
-  bft_printf("%s[L%d] : t_max = %f\n", __func__, __LINE__, time_stp->t_max);
-  bft_printf_flush();
 
   if (_t_max < 0) {
     int _nt_max = -1;
@@ -2010,9 +2008,6 @@ cs_gui_dt_param(void)
         time_stp->nt_max = time_stp->nt_prev + _nt_max;
     }
   }
-
-  bft_printf("%s[L%d] : nt_max = %d\n", __func__, __LINE__, time_stp->nt_max);
-  bft_printf_flush();
 
   cs_gui_node_get_child_status_int(tn,
                                    "thermal_time_step",

src/base/cs_function_default.cpp
@@ -65,6 +65,7 @@
 #include "cs_parameters.h"
 #include "cs_physical_constants.h"
 #include "cs_physical_model.h"
+#include "cs_property.h"
 #include "cs_post.h"
 #include "cs_rotation.h"
 #include "cs_thermal_model.h"
@@ -571,6 +572,44 @@ _ensure_boundary_forces_are_present(void)
     bf = cs_field_create(name, type, location_id, 3, false);
     cs_field_set_key_int(bf, cs_field_key_id("log"), 0);
     cs_field_set_key_int(bf, cs_field_key_id("post_vis"), 0);
+  }
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Private function to output property values at cells.
+ */
+/*----------------------------------------------------------------------------*/
+
+static void
+_output_cells_property
+(
+  int               location_id,
+  cs_lnum_t         n_elts,
+  const cs_lnum_t  *elt_ids,
+  void             *input,
+  void             *vals
+)
+{
+  assert(location_id == CS_MESH_LOCATION_CELLS);
+
+  const char *_ppty_name = static_cast<const char *>(input);
+
+  cs_real_t *_vals = static_cast<cs_real_t *>(vals);
+
+  cs_property_t *ppty = cs_property_by_name(_ppty_name);
+
+  cs_real_t _t_cur = cs_glob_time_step->t_cur;
+  if (elt_ids != nullptr) {
+    for (cs_lnum_t e_id = 0; e_id < n_elts; e_id++) {
+      cs_lnum_t c_id = elt_ids[e_id];
+      _vals[e_id] = cs_property_get_cell_value(c_id, _t_cur, ppty);
+    }
+  }
+  else {
+    cs_property_eval_at_cells(_t_cur,
+                              ppty,
+                              _vals);
   }
 }
 
@@ -1701,6 +1740,42 @@ cs_function_q_criterion(int               location_id,
   BFT_FREE(gradv);
 }
 
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Define output function of a property (which is not a field).
+ */
+/*----------------------------------------------------------------------------*/
+
+cs_function_t *
+cs_function_define_property_cells
+(
+  char *property_name /*!<[in] name of the property */
+)
+{
+  /* sanity check */
+  cs_property_t *ppty = cs_property_by_name(property_name);
+  if (ppty == nullptr)
+    bft_error(__FILE__, __LINE__, 0,
+              "%s: property \"%s\" does not exist.\n",
+              __func__, property_name);
+
+  int _dim = cs_property_get_dim(ppty);
+  cs_function_t *f
+    = cs_function_define_by_func(property_name,
+                                 CS_MESH_LOCATION_CELLS,
+                                 _dim,
+                                 true,
+                                 CS_REAL_TYPE,
+                                 _output_cells_property,
+                                 property_name);
+
+  cs_function_set_label(f, property_name);
+  f->type = CS_FUNCTION_INTENSIVE;
+
+  f->post_vis = CS_POST_ON_LOCATION;
+
+  return f;
+}
 /*----------------------------------------------------------------------------*/
 
 END_C_DECLS

src/base/cs_function_default.h
@@ -372,6 +372,18 @@ cs_function_q_criterion(int               location_id,
                         void             *vals);
 
 /*----------------------------------------------------------------------------*/
+/*
+ * \brief Define output function of a property (which is not a field).
+ */
+/*----------------------------------------------------------------------------*/
+
+cs_function_t *
+cs_function_define_property_cells
+(
+  char *property_name /*!<[in] name of the property */
+);
+
+/*----------------------------------------------------------------------------*/
 
 END_C_DECLS
 

src/gui/cs_gui_output.cpp
@@ -61,10 +61,12 @@
 #include "cs_parameters.h"
 #include "cs_physical_model.h"
 #include "cs_post.h"
+#include "cs_property.h"
 #include "cs_field.h"
 #include "cs_field_pointer.h"
 #include "cs_function_default.h"
 #include "cs_thermal_model.h"
+#include "cs_thermal_system.h"
 #include "cs_time_moment.h"
 #include "cs_volume_zone.h"
 
@@ -216,6 +218,53 @@ _field_post(const char  *field_type,
   const char *label = cs_tree_node_get_tag(tn, "label");
   if (label != NULL)
     cs_field_set_key_str(f, k_lbl, label);
+}
+
+/*-----------------------------------------------------------------------------
+ * Post-processing options for properties
+ *
+ * parameters:
+ *   f_id <-- field id
+ *----------------------------------------------------------------------------*/
+
+static void
+_property_post(const char          *field_type,
+               const char          *label,
+               const cs_property_t *ppty)
+{
+  cs_tree_node_t *tn = _get_node(field_type, label);
+
+  if (tn == NULL)
+    return;
+
+  cs_function_t *f = cs_function_by_name_try(ppty->name);
+  if (f == nullptr)
+    f = cs_function_define_property_cells(ppty->name);
+
+  f->log = 0;
+  f->post_vis = 0;
+
+  int f_post = -999, f_monitor = -999, f_log = -999;
+
+  /* Listing output */
+
+  cs_gui_node_get_status_int(cs_tree_node_get_child(tn, "listing_printing"),
+                             &f_log);
+  if (f_log != 0)
+    f->log = 1;
+
+  cs_gui_node_get_status_int(cs_tree_node_get_child
+                               (tn, "postprocessing_recording"),
+                             &f_post);
+  if (f_post != 0)
+    f->log |= CS_POST_ON_LOCATION;
+
+  cs_gui_node_get_status_int(cs_tree_node_get_child(tn, "probes_recording"),
+                             &f_monitor);
+
+  if (f_monitor != 0)
+    f->log |= CS_POST_MONITOR;
+
 }
 
 /*-----------------------------------------------------------------------------
@@ -773,6 +822,21 @@ cs_gui_output(void)
   /* User functions */
   for (int f_id = 0; f_id < cs_function_n_functions(); f_id++) {
     _function_post(f_id);
+  }
+
+  /* Heat Transfer solver physical properties */
+  if (cs_glob_physical_model_flag[CS_HEAT_TRANSFER] > -1) {
+    cs_property_t *ppty = cs_property_by_name(CS_PROPERTY_MASS_DENSITY);
+    if (ppty != nullptr)
+      _property_post("property", "density", ppty);
+
+    ppty = cs_property_by_name(CS_THERMAL_CP_NAME);
+    if (ppty != nullptr)
+      _property_post("property", "specific_heat", ppty);
+
+    ppty = cs_property_by_name(CS_THERMAL_LAMBDA_NAME);
+    if (ppty != nullptr)
+      _property_post("property", "thermal_conductivity", ppty);
   }
 
 #if _XML_DEBUG_

src/gui/cs_gui_specific_physics.cpp
@@ -764,10 +764,6 @@ cs_gui_physical_model_select(void)
   isactiv = _get_active_thermophysical_model(&model_name, &model_value);
 
   if (isactiv)  {
-    bft_printf("%s[L%d] model : '%s' and value : '%s'\n",
-               __func__, __LINE__, model_name, model_value);
-    bft_printf_flush();
-
     if (cs_gui_strcmp(model_name, "solid_fuels")) {
       if (cs_gui_strcmp(model_value, "homogeneous_fuel"))
         cs_coal_model_set_model(CS_COMBUSTION_COAL_STANDARD);

commit 9c8d9630c68150437b0114420ce318e63d7c83f4

Fix compilation, broken by e5380cba (yesterday).

src/gui/cs_gui.cpp
@@ -4229,8 +4229,7 @@ cs_gui_physical_properties(void)
     // Lambda
     cs_property_t  *lambda = cs_property_by_name(CS_THERMAL_LAMBDA_NAME);
     int _lambda_choice = -1;
-    _properties_choice_id("thermal_conductivity", &_lambda_choice);
-    if (_properties_choice_id > 0) {
+    if (_properties_choice_id("thermal_conductivity", &_lambda_choice) > 0) {
       for (int z_id = 0; z_id < cs_volume_zone_n_zones(); z_id++) {
         const cs_zone_t *z = cs_volume_zone_by_id(z_id);
         if (z->type & CS_VOLUME_ZONE_PHYSICAL_PROPERTIES) {