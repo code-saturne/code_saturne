code-saturne/code_saturne

commit e84c5f56d81912b437a4d8bf0f22b1d95659efb1

Use single include path with explicit subdirectories.
This makes the file organization more explicit (i.e. header includes
in alphabetical order will lead to more consistent grouping),
and allows make the build include headers more similar to those
used for an installed version.

build-aux/cs_compile_build.py
@@ -93,22 +93,6 @@ def dest_subdir(destdir, d):
 
     return os.path.join(destdir, t)
 
-#-------------------------------------------------------------------------------
-
-def src_include_dirs(srcdir):
-    """
-    Return include directories in a given source directory.
-    """
-    include_dirs = []
-
-    if srcdir:
-        for f in os.listdir(os.path.join(srcdir, 'src')):
-            p = os.path.join(srcdir, 'src', f)
-            if os.path.isdir(p):
-                include_dirs.append(p)
-
-    return include_dirs
-
 #===============================================================================
 # Class used to manage compilation in build directory
 #===============================================================================
@@ -125,7 +109,8 @@ class compile_build(cs_compile):
         self.srcdir = srcdir
 
         top_builddir = os.getcwd()
-        while not os.path.isfile(os.path.join(top_builddir, "cs_config.h")):
+        while not os.path.isfile(os.path.join(top_builddir,
+                                              "src", "base", "cs_config.h")):
             ds = os.path.split(top_builddir)
             if ds[1]:
                 top_builddir = ds[0]
@@ -170,9 +155,7 @@ class compile_build(cs_compile):
                 flags.append('-I' + os.path.join(top_builddir, 'libple'))
                 flags.append('-I' + os.path.join(self.srcdir, 'libple', 'src'))
             flags.append('-I' + top_builddir)
-            include_dirs = src_include_dirs(self.srcdir)
-            for d in include_dirs:
-                flags.append('-I' + d)
+            flags.append('-I' + os.path.join(self.srcdir, 'src'))
 
         elif flag == 'ldflags':
             tsd = os.path.join(top_builddir, 'src')

tests/Makefile.am
@@ -36,15 +36,7 @@ LIBS_PLE = $(PLE_LIBS)
 # Main part
 
 AM_CPPFLAGS = \
--I$(top_srcdir)/src/alge \
--I$(top_srcdir)/src/base \
--I$(top_srcdir)/src/cdo \
--I$(top_srcdir)/src/ctwr \
--I$(top_srcdir)/src/lagr \
--I$(top_srcdir)/src/mesh \
--I$(top_srcdir)/src/bft \
--I$(top_srcdir)/src/fvm \
--I$(top_srcdir)/src/gui \
+-I$(top_srcdir)/src \
 $(CPPFLAGS_PLE) \
 $(MPI_CPPFLAGS)
 
@@ -158,9 +150,7 @@ cs_blas_test_SOURCES  = \
 cs_blas_test.cpp \
 cs_blas.cpp
 cs_blas_test_CPPFLAGS  = \
--I$(top_srcdir)/src/alge \
--I$(top_srcdir)/src/base \
--I$(top_srcdir)/src/bft \
+-I$(top_srcdir)/src \
  $(BLAS_CPPFLAGS) $(MPI_CPPFLAGS)
 cs_blas_test_CFLAGS  = $(AM_CFLAGS) $(CFLAGS_HOT)
 cs_blas_test_LDFLAGS  = $(BLAS_LDFLAGS) $(LDFLAGS_CS_TESTS)

Makefile.am
@@ -83,15 +83,16 @@ config/code_saturne_build.cfg.in.in
 # the number of extra files or patches can grow and change a lot.
 EXTRA_DIST += extras m4 patches
 
-nodist_pkginclude_HEADERS = \
-cs_config.h \
-cs_version.h
+csbase_includedir=$(pkgincludedir)/base
+nodist_csbase_include_HEADERS = \
+src/base/cs_config.h \
+src/base/cs_version.h
 
 DISTCLEANFILES = \
 bin/code_saturne \
 lib/code_saturne_build.cfg \
-cs_config.h \
-cs_version.h \
+src/base/cs_config.h \
+src/base/cs_version.h \
 libtool_cuda
 
 ACLOCAL_AMFLAGS = -I m4
@@ -173,8 +174,8 @@ endif
 
 bin/code_saturne: $(srcdir)/bin/code_saturne.$(LAUNCHER_EXT).in
 
-cs_version.h: Makefile $(top_srcdir)/src/base/cs_version.h.in
-	$(PYTHON) ${top_srcdir}/build-aux/cs_version.py --replace ${top_srcdir}/src/base/cs_version.h.in cs_version.h
+src/base/cs_version.h: Makefile $(top_srcdir)/src/base/cs_version.h.in
+	$(PYTHON) ${top_srcdir}/build-aux/cs_version.py --replace ${top_srcdir}/src/base/cs_version.h.in src/base/cs_version.h
 
 lib/code_saturne_build.cfg: Makefile config/code_saturne_build.cfg.in
 	rm -f $@ $@.tmp
@@ -185,7 +186,7 @@ lib/code_saturne_build.cfg: Makefile config/code_saturne_build.cfg.in
 	$(PYTHON) ${top_srcdir}/build-aux/cs_version.py --replace $@ $@
 
 BUILT_SOURCES = \
-cs_version.h \
+src/base/cs_version.h \
 lib/code_saturne_build.cfg
 
 CLEANFILES = $(edit_scripts)
@@ -308,7 +309,7 @@ dist-hook:
 # Additional local targets
 
 all-local:
-	@$(PYTHON) ${top_srcdir}/build-aux/cs_version.py --replace ${top_srcdir}/src/base/cs_version.h.in cs_version.h
+	@$(PYTHON) ${top_srcdir}/build-aux/cs_version.py --replace ${top_srcdir}/src/base/cs_version.h.in src/base/cs_version.h
 	@if test -f bin/cs_package.py; then \
 	  $(PYTHON) ${top_srcdir}/build-aux/cs_version.py --replace bin/cs_package.py bin/cs_package.py; \
 	fi

src/Makefile.am
@@ -46,6 +46,11 @@ noinst_LIBRARIES = libcsmodules.a
 
 if HAVE_BACKEND
 
+# Public header files (to be installed)
+
+pkginclude_HEADERS = \
+cs_headers.h
+
 libcsmodules_a_SOURCES = \
 base/paramx.f90 \
 base/mesh.f90 \
@@ -131,6 +136,7 @@ clean-local:
 install-exec-local: $(libcsmodules_a_SOURCES)
 	@$(NORMAL_INSTALL)
 	test -z "$(pkgincludedir)" || $(MKDIR_P) "$(DESTDIR)$(pkgincludedir)"
+	test -h $(DESTDIR)$(pkgincludedir)/cs_defs.h || ln -s base/cs_defs.h $(DESTDIR)$(pkgincludedir)/cs_defs.h; \
 	@list='$(libcsmodules_a_SOURCES)'; test -n "$(pkgincludedir)" || list=; \
 	modlist=`for p in $$list; do echo $$p; done | $(SED) -e 's|.f90|.$(FCMODEXT)|g'`; \
 	files=`for p in $$modlist; do echo $$p; done | $(SED) -e 's|^.*/||'`; \
@@ -145,6 +151,7 @@ install-exec-local: $(libcsmodules_a_SOURCES)
 
 uninstall-local:
 	@$(NORMAL_UNINSTALL)
+	rm -f $(DESTDIR)$(pkgincludedir)/cs_defs.h; \
 	@list='$(libcsmodules_a_SOURCES)'; test -n "$(pkgincludedir)" || list=; \
 	modlist=`for p in $$list; do echo $$p; done | $(SED) -e 's|.f90|.$(FCMODEXT)|g'`; \
 	lcfiles=`for p in $$modlist; do echo $$p; done | $(SED) -e 's|^.*/||'`; \

src/alge/Makefile.am
@@ -33,13 +33,7 @@ endif
 # Main part
 
 AM_CPPFLAGS = \
--I$(top_srcdir)/src/bft \
--I$(top_srcdir)/src/fvm \
--I$(top_srcdir)/src/base \
--I$(top_srcdir)/src/mesh \
--I$(top_srcdir)/src/cdo \
--I$(top_srcdir)/src/pprt \
--I$(top_srcdir)/src/turb \
+-I$(top_srcdir)/src -I$(top_builddir)/src \
 $(CPPFLAGS_PLE) \
 $(MPI_CPPFLAGS) $(BLAS_CPPFLAGS)
 
@@ -50,43 +44,27 @@ AM_LDFLAGS =
 # Conditionally compiled extensions
 
 libcsalge_extension_a_CPPFLAGS =\
--I$(top_srcdir)/src/bft  \
--I$(top_srcdir)/src/fvm  \
--I$(top_srcdir)/src/base \
--I$(top_srcdir)/src/mesh \
--I$(top_srcdir)/src/cdo \
+-I$(top_srcdir)/src -I$(top_builddir)/src \
 $(CPPFLAGS_PLE) \
 $(PETSC_CPPFLAGS) $(MUMPS_CPPFLAGS) $(HYPRE_CPPFLAGS) \
 $(MPI_CPPFLAGS)
 
 libcsalge_petsc_a_CPPFLAGS =\
--I$(top_srcdir)/src/base \
--I$(top_srcdir)/src/bft \
--I$(top_srcdir)/src/fvm \
--I$(top_srcdir)/src/mesh \
+-I$(top_srcdir)/src -I$(top_builddir)/src \
 $(PETSC_CPPFLAGS) $(MPI_CPPFLAGS)
 
 if HAVE_MUMPS
   libcsalge_mumps_a_CPPFLAGS =\
-  -I$(top_srcdir)/src/base \
-  -I$(top_srcdir)/src/bft \
-  -I$(top_srcdir)/src/fvm \
-  -I$(top_srcdir)/src/mesh \
+  -I$(top_srcdir)/src -I$(top_builddir)/src \
   $(MUMPS_CPPFLAGS) $(MPI_CPPFLAGS)
 endif
 
 libcsalge_amgx_a_CPPFLAGS =\
--I$(top_srcdir)/src/base \
--I$(top_srcdir)/src/bft \
--I$(top_srcdir)/src/fvm \
--I$(top_srcdir)/src/mesh \
+-I$(top_srcdir)/src -I$(top_builddir)/src \
 $(AMGX_CPPFLAGS) $(MPI_CPPFLAGS)
 
 libcsalge_hypre_a_CPPFLAGS =\
--I$(top_srcdir)/src/base \
--I$(top_srcdir)/src/bft \
--I$(top_srcdir)/src/fvm \
--I$(top_srcdir)/src/mesh \
+-I$(top_srcdir)/src -I$(top_builddir)/src \
 $(HYPRE_CPPFLAGS) $(MPI_CPPFLAGS)
 
 noinst_HEADERS = \
@@ -94,7 +72,8 @@ cs_convection_diffusion_priv.h
 
 # Public header files (to be installed)
 
-pkginclude_HEADERS = \
+csalge_includedir=$(pkgincludedir)/alge
+csalge_include_HEADERS = \
 cs_alge_headers.h \
 cs_bad_cells_regularisation.h \
 cs_balance.h \
@@ -138,7 +117,7 @@ cs_sles_pc_priv.h \
 cs_vertex_to_cell.h
 
 if HAVE_CUDA
-pkginclude_HEADERS += \
+csalge_include_HEADERS += \
 cs_benchmark_cuda.h \
 cs_blas_cuda.h \
 cs_matrix_spmv_cuda.h \
@@ -147,25 +126,25 @@ cs_sles_pc_cuda.h
 endif
 
 if HAVE_PETSC
-pkginclude_HEADERS += \
+csalge_include_HEADERS += \
 cs_matrix_petsc.h \
 cs_matrix_petsc_priv.h \
 cs_sles_petsc.h
 endif
 
 if HAVE_MUMPS
-pkginclude_HEADERS += cs_sles_mumps.h
+csalge_include_HEADERS += cs_sles_mumps.h
 endif
 
 if HAVE_HYPRE
-pkginclude_HEADERS += \
+csalge_include_HEADERS += \
 cs_matrix_hypre.h \
 cs_matrix_hypre_priv.h \
 cs_sles_hypre.h
 endif
 
 if HAVE_AMGX
-pkginclude_HEADERS += cs_sles_amgx.h
+csalge_include_HEADERS += cs_sles_amgx.h
 endif
 
 # Library source files
@@ -266,8 +245,8 @@ clean-local:
 
 if HAVE_CUDA_CPP
 .cpp.$(OBJEXT):
-	$(NVCC) $(DEFS) $(AM_CPPFLAGS) -I$(top_srcdir)/src/alge -I../.. $(NVCCFLAGS) $(NVCCFLAGS_DBG) $(NVCCFLAGS_OPT) $(NVCCFLAGS_CPP) -c -o $@ $<
+	$(NVCC) $(DEFS) $(AM_CPPFLAGS) $(NVCCFLAGS) $(NVCCFLAGS_DBG) $(NVCCFLAGS_OPT) $(NVCCFLAGS_CPP) -c -o $@ $<
 endif
 
 .cu.$(OBJEXT):
-	$(NVCC) $(DEFS) $(AM_CPPFLAGS) -I$(top_srcdir)/src/alge -I../.. $(NVCCFLAGS) $(NVCCFLAGS_DBG) $(NVCCFLAGS_OPT) -c -o $@ $<
+	$(NVCC) $(DEFS) $(AM_CPPFLAGS) $(NVCCFLAGS) $(NVCCFLAGS_DBG) $(NVCCFLAGS_OPT) -c -o $@ $<

src/apps/Makefile.am
@@ -55,23 +55,7 @@ endif
 if HAVE_BACKEND
 
 AM_CPPFLAGS = \
--I$(top_srcdir)/src/atmo \
--I$(top_srcdir)/src/alge \
--I$(top_srcdir)/src/base \
--I$(top_srcdir)/src/bft \
--I$(top_srcdir)/src/cdo \
--I$(top_srcdir)/src/cogz \
--I$(top_srcdir)/src/comb \
--I$(top_srcdir)/src/ctwr \
--I$(top_srcdir)/src/elec \
--I$(top_srcdir)/src/fvm \
--I$(top_srcdir)/src/gui \
--I$(top_srcdir)/src/gwf \
--I$(top_srcdir)/src/lagr \
--I$(top_srcdir)/src/meg \
--I$(top_srcdir)/src/mesh \
--I$(top_srcdir)/src/pprt \
--I$(top_srcdir)/src/turb \
+-I$(top_srcdir)/src -I$(top_builddir)/src \
 $(CPPFLAGS_PLE) \
 $(MPI_CPPFLAGS) $(CPPFLAGS_BLAS) \
 $(CPPFLAGS_MUMPS)
@@ -180,7 +164,7 @@ endif
 # code_saturne IO utility (minimal dependencies)
 
 if HAVE_FRONTEND
-cs_io_dump_CPPFLAGS = -DLOCALEDIR=\"'$(localedir)'\" -I$(top_srcdir)/src/base
+cs_io_dump_CPPFLAGS = -DLOCALEDIR=\"'$(localedir)'\" -I$(top_srcdir)/src -I$(top_builddir)/src
 cs_io_dump_SOURCES = cs_io_dump.cpp
 cs_io_dump_LDADD =
 endif

src/atmo/Makefile.am
@@ -33,15 +33,7 @@ endif
 # Main part
 
 AM_CPPFLAGS = \
--I$(top_srcdir)/src/alge \
--I$(top_srcdir)/src/base \
--I$(top_srcdir)/src/bft \
--I$(top_srcdir)/src/cdo \
--I$(top_srcdir)/src/fvm \
--I$(top_srcdir)/src/mesh \
--I$(top_srcdir)/src/pprt \
--I$(top_srcdir)/src/rayt \
--I$(top_srcdir)/src/turb \
+-I$(top_srcdir)/src -I$(top_builddir)/src \
 $(CPPFLAGS_PLE) \
 $(MPI_CPPFLAGS)
 
@@ -50,15 +42,14 @@ AM_CXXFLAGS = $(CXXFLAGS_STD) $(CXXFLAGS_SYCL) $(CXXFLAGS_DBG) $(CXXFLAGS_OPT) $
 
 AM_FCFLAGS = \
 $(FCMODINCLUDE)$(top_builddir)/src \
-$(FCMODINCLUDE)$(top_builddir)/src/base \
-$(FCMODINCLUDE)$(top_builddir)/src/user \
 $(FCFLAGS_DBG) $(FCFLAGS_OPT) $(FCFLAGS_SHARED)
 
 AM_LDFLAGS =
 
 # Public header files (to be installed)
 
-pkginclude_HEADERS = \
+csatmo_includedir=$(pkgincludedir)/atmo
+csatmo_include_HEADERS = \
 cs_air_props.h \
 cs_at_opt_interp.h \
 cs_at_data_assim.h \
@@ -114,5 +105,5 @@ clean-local:
 
 if HAVE_CUDA_CPP
 .cpp.$(OBJEXT):
-	$(NVCC) $(DEFS) $(AM_CPPFLAGS) -I$(top_srcdir)/src/atmo -I../.. $(NVCCFLAGS) $(NVCCFLAGS_DBG) $(NVCCFLAGS_OPT) $(NVCCFLAGS_CPP) -c -o $@ $<
+	$(NVCC) $(DEFS) $(AM_CPPFLAGS) $(NVCCFLAGS) $(NVCCFLAGS_DBG) $(NVCCFLAGS_OPT) $(NVCCFLAGS_CPP) -c -o $@ $<
 endif

src/base/Makefile.am
@@ -33,24 +33,7 @@ endif
 # Main part
 
 AM_CPPFLAGS = \
--I$(top_srcdir)/src/alge \
--I$(top_srcdir)/src/cdo \
--I$(top_srcdir)/src/cfbl \
--I$(top_srcdir)/src/ctwr \
--I$(top_srcdir)/src/cogz \
--I$(top_srcdir)/src/comb \
--I$(top_srcdir)/src/elec \
--I$(top_srcdir)/src/gwf \
--I$(top_srcdir)/src/lagr \
--I$(top_srcdir)/src/meg \
--I$(top_srcdir)/src/mesh \
--I$(top_srcdir)/src/bft \
--I$(top_srcdir)/src/fvm \
--I$(top_srcdir)/src/gui \
--I$(top_srcdir)/src/pprt \
--I$(top_srcdir)/src/rayt \
--I$(top_srcdir)/src/turb \
--I$(top_srcdir)/src/atmo \
+-I$(top_srcdir)/src -I$(top_builddir)/src \
 $(CPPFLAGS_PLE) \
 $(MPI_CPPFLAGS)
 
@@ -59,18 +42,6 @@ AM_CXXFLAGS = $(CXXFLAGS_STD) $(CXXFLAGS_SYCL) $(CXXFLAGS_DBG) $(CXXFLAGS_OPT) $
 
 AM_FCFLAGS = \
 $(FCMODINCLUDE)$(top_builddir)/src \
-$(FCMODINCLUDE)$(top_builddir)/src/alge \
-$(FCMODINCLUDE)$(top_builddir)/src/atmo \
-$(FCMODINCLUDE)$(top_builddir)/src/cfbl \
-$(FCMODINCLUDE)$(top_builddir)/src/cogz \
-$(FCMODINCLUDE)$(top_builddir)/src/comb \
-$(FCMODINCLUDE)$(top_builddir)/src/ctwr \
-$(FCMODINCLUDE)$(top_builddir)/src/elec \
-$(FCMODINCLUDE)$(top_builddir)/src/lagr \
-$(FCMODINCLUDE)$(top_builddir)/src/pprt \
-$(FCMODINCLUDE)$(top_builddir)/src/rayt \
-$(FCMODINCLUDE)$(top_builddir)/src/turb \
-$(FCMODINCLUDE)$(top_builddir)/src/user \
 $(FCFLAGS_DBG) $(FCFLAGS_OPT) $(FCFLAGS_SHARED)
 
 AM_LDFLAGS =
@@ -85,7 +56,8 @@ cs_version.h.in
 
 # Public header files (to be installed)
 
-pkginclude_HEADERS = \
+csbase_includedir=$(pkgincludedir)/base
+csbase_include_HEADERS = \
 cs_1d_wall_thermal.h \
 cs_1d_wall_thermal_check.h \
 cs_ale.h \
@@ -224,15 +196,14 @@ cs_wall_distance.h \
 cs_wall_functions.h \
 cs_xdef_eval_at_zone.h \
 cs_zone.h \
-cs_base_headers.h \
-cs_headers.h
+cs_base_headers.h
 
 noinst_HEADERS = \
 cs_boundary_conditions_priv.h \
 cs_mem_cuda_priv.h
 
 if HAVE_CUDA
-pkginclude_HEADERS += \
+csbase_include_HEADERS += \
 cs_base_cuda.h \
 cs_math_cuda.cuh \
 cs_array_cuda.h \
@@ -438,7 +409,7 @@ pkglib_LIBRARIES =
 
 # MEDCoupling wrappers, plugin or linked-in.
 
-pkginclude_HEADERS += \
+csbase_include_HEADERS += \
 cs_medcoupling_mesh.hxx \
 cs_medcoupling_utils.h \
 cs_medcoupling_remapper.h \
@@ -448,11 +419,7 @@ cs_paramedmem_coupling.h \
 cs_paramedmem_remapper.h
 
 libcs_medcoupling_a_CPPFLAGS = \
--I$(top_srcdir)/src/alge \
--I$(top_srcdir)/src/cdo \
--I$(top_srcdir)/src/fvm \
--I$(top_srcdir)/src/mesh \
--I$(top_srcdir)/src/bft \
+-I$(top_srcdir)/src -I$(top_builddir)/src \
 $(CPPFLAGS_PLE) \
 $(MEDCOUPLING_CPPFLAGS) \
 $(MED_CPPFLAGS) \
@@ -502,13 +469,10 @@ endif
 if HAVE_EOS
 
 libcs_eos_a_CPPFLAGS = \
--I$(top_srcdir)/src/cdo \
--I$(top_srcdir)/src/fvm \
--I$(top_srcdir)/src/mesh \
--I$(top_srcdir)/src/bft \
+-I$(top_srcdir)/src -I$(top_builddir)/src \
 $(EOS_CPPFLAGS) $(MPI_CPPFLAGS)
 
-pkginclude_HEADERS += cs_eos.hxx
+csbase_include_HEADERS += cs_eos.hxx
 noinst_LIBRARIES += libcs_eos.a
 libcs_eos_a_SOURCES = cs_eos.cxx
 
@@ -538,12 +502,9 @@ endif
 if HAVE_COOLPROP
 
 cs_coolprop_CPPFLAGS = \
--I$(top_srcdir)/src/cdo \
--I$(top_srcdir)/src/fvm \
--I$(top_srcdir)/src/mesh \
--I$(top_srcdir)/src/bft \
+-I$(top_srcdir)/src -I$(top_builddir)/src \
 $(COOLPROP_CPPFLAGS) $(MPI_CPPFLAGS)
-pkginclude_HEADERS += cs_coolprop.hxx
+csbase_include_HEADERS += cs_coolprop.hxx
 
 cs_coolprop = $(top_srcdir)/src/base/cs_coolprop.cxx
 cs_coolprop.$(OBJEXT): $(cs_coolprop)
@@ -575,11 +536,11 @@ endif
 
 if HAVE_CUDA_CPP
 .cpp.$(OBJEXT):
-	$(NVCC) $(DEFS) $(AM_CPPFLAGS) -I$(top_srcdir)/src/base -I../.. $(NVCCFLAGS) $(NVCCFLAGS_DBG) $(NVCCFLAGS_OPT) $(NVCCFLAGS_CPP) -c -o $@ $<
+	$(NVCC) $(DEFS) $(AM_CPPFLAGS) $(NVCCFLAGS) $(NVCCFLAGS_DBG) $(NVCCFLAGS_OPT) $(NVCCFLAGS_CPP) -c -o $@ $<
 endif
 
 .cu.$(OBJEXT):
-	$(NVCC) $(DEFS) $(AM_CPPFLAGS) -I$(top_srcdir)/src/base -I../.. $(NVCCFLAGS) $(NVCCFLAGS_DBG) $(NVCCFLAGS_OPT) -c -o $@ $<
+	$(NVCC) $(DEFS) $(AM_CPPFLAGS) $(NVCCFLAGS) $(NVCCFLAGS_DBG) $(NVCCFLAGS_OPT) -c -o $@ $<
 
 # Additional rules
 

commit beaf6778a77d2b9d8577af3234e1b97299c77069

Fix "make check", broken by e84c5f56 (2025-01-02).

build-aux/cs_compile_build.py
@@ -156,6 +156,7 @@ class compile_build(cs_compile):
                 flags.append('-I' + os.path.join(self.srcdir, 'libple', 'src'))
             flags.append('-I' + top_builddir)
             flags.append('-I' + os.path.join(self.srcdir, 'src'))
+            flags.append('-I' + os.path.join(top_builddir, 'src'))
 
         elif flag == 'ldflags':
             tsd = os.path.join(top_builddir, 'src')

tests/Makefile.am
@@ -36,7 +36,7 @@ LIBS_PLE = $(PLE_LIBS)
 # Main part
 
 AM_CPPFLAGS = \
--I$(top_srcdir)/src \
+-I$(top_srcdir)/src -I$(top_builddir)/src \
 $(CPPFLAGS_PLE) \
 $(MPI_CPPFLAGS)
 
@@ -150,7 +150,7 @@ cs_blas_test_SOURCES  = \
 cs_blas_test.cpp \
 cs_blas.cpp
 cs_blas_test_CPPFLAGS  = \
--I$(top_srcdir)/src \
+-I$(top_srcdir)/src -I$(top_builddir)/src \
  $(BLAS_CPPFLAGS) $(MPI_CPPFLAGS)
 cs_blas_test_CFLAGS  = $(AM_CFLAGS) $(CFLAGS_HOT)
 cs_blas_test_LDFLAGS  = $(BLAS_LDFLAGS) $(LDFLAGS_CS_TESTS)
@@ -293,4 +293,4 @@ clean-local:
 	-rm -rf .deps
 
 .cu.$(OBJEXT):
-	$(NVCC) $(DEFS) $(AM_CPPFLAGS) -I$(top_srcdir)/src/base -I../.. $(NVCCFLAGS) $(NVCCFLAGS_DBG) $(NVCCFLAGS_OPT) -c -o $@ $<
+	$(NVCC) $(DEFS) $(AM_CPPFLAGS) -I$(top_srcdir)/src/base -I$(top_builddir)/src/base $(NVCCFLAGS) $(NVCCFLAGS_DBG) $(NVCCFLAGS_OPT) -c -o $@ $<