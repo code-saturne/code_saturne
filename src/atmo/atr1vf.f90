code-saturne/code_saturne

commit f56629e6f9fd2d17540f5119bb8af62ba4287d8f

[Atmo] allow std profile to start from a reference altitude z_ref > 0.
It is used to complete the profile above the domain for 1D radiative
model (without a discontinuous profile).

src/atmo/atr1vf.f90
@@ -378,14 +378,20 @@ if (mod(ntcabs,nfatr1).eq.0.or.ideb.eq.0) then
       endif
 
       if (imeteo.eq.0) then
-        call atmstd(zray(k), preray(k), dum, dum)
+        call atmstd(0.d0, &
+                    p0,   &
+                    t0,   &
+                    zray(k), preray(k), dum, dum)
       else if (imeteo.eq.1) then
         call intprf(nbmetd, nbmetm, ztmet, tmmet, phmet, zray(k), ttcabs, &
                     preray(k))
       else
         !TODO would be more coherent with an averaging of "meteo_pressure"
         ! Field
-        call atmstd(zray(k), preray(k), dum, dum)
+        call atmstd(0.d0, &
+                    p0,   &
+                    t0,   &
+                    zray(k), preray(k), dum, dum)
       endif
     enddo
 
@@ -398,8 +404,10 @@ if (mod(ntcabs,nfatr1).eq.0.or.ideb.eq.0) then
       ncray(k) = 0.d0
       aeroso(k) = aevert(k, ii)
 
-      ! initialize with standard atmosphere
-      call atmstd(zray(k), preray(k), temray(k), romray(k))
+      ! Initialize with standard atmosphere
+      ! above the domain
+      call atmstd(zray(kvert), preray(kvert), temray(kvert), &
+                  zray(k), preray(k), temray(k), romray(k))
       ! Conversion Kelvin to Celsius
       temray(k) = temray(k) - tkelvi
     enddo

src/atmo/atlecm.f90
@@ -250,6 +250,8 @@ if (imode.eq.0) then
 
   do ii = 1, nbmett
     read (impmet,*,err=999,end=999) zzmax, temp, qv
+
+    ! Initialize p0, rho0 and theta0 at the first level
     if (ii.eq.1 .and. itp.eq.1) then
       t0 = temp + tkelvi
       rhum = rair*(1.d0+(rvsra-1.d0)*qv*ih2o)
@@ -288,6 +290,14 @@ else
            ttmet(ii,itp),qvmet(ii,itp)
     endif
 
+    ! Initialize p0, rho0 and theta0 at the first level
+    if (ii.eq.1 .and. itp.eq.1) then
+      qv = qvmet(ii,itp)
+      t0 = temp + tkelvi
+      rhum = rair*(1.d0+(rvsra-1.d0)*qv*ih2o)
+      ro0 = p0 / t0 /rhum
+    endif
+
     !--> Check the unity of the specific humidity (kg/kg) when used
 
     if (qvmet(ii,itp).gt.0.1 .or. qvmet(ii,itp).lt.0.) then
@@ -314,7 +324,9 @@ else
       zzmax = zzmax + 1000.d0
       ii = ii + 1
       ztmet(ii) = zzmax
-      call atmstd(ztmet(ii), dum, tlkelv, dum) ! standard temperature profile
+      ! standard temperature profile above the domaine
+      call atmstd(ztmet(nbmett), p0, ttmet(nbmett,itp)+tkelvi, &
+                  ztmet(ii), dum, tlkelv, dum)
       ttmet(ii,itp) = tlkelv - tkelvi
       if (iqv0.eq.0) then
         qvmet(ii,itp) = 0.d0   ! standard atmosphere: q=0
@@ -330,20 +342,13 @@ else
 
 endif
 
-! Initialize p0, rho0 and theta0 at the first level
-if (imode.eq.1.and.itp.eq.1) then
-  p0 = xyp_met(3, itp)
-  t0 = ttmet(1,itp)+tkelvi
-  rhum = rair*(1.d0+(rvsra-1.d0)*qvmet(1,itp)*ih2o)
-  ro0 = p0 / t0 /rhum
-endif
-
 !===============================================================================
 ! 6. Compute hydro pressure profile  (Laplace integration)
 !===============================================================================
 ! If ihpm = 0 (default): bottom to top Laplace integration based on pressure at
 ! sea level (pmer(itp))
-! If ihpm = 1 (usipsu): top to bottom Laplace integration based on pressure at
+! If ihpm = 1 (hydrostatic_pressure_model in cs_user_parameters):
+! top to bottom Laplace integration based on pressure at
 ! the top of the domain (ztmet(nbmaxt)) for the standard atmosphere
 
 if (imode.eq.1) then
@@ -374,7 +379,9 @@ if (imode.eq.1) then
       phmet(k,itp) = phmet(k-1,itp)*exp(rap)
     enddo
   else
-    call atmstd (ztmet(nbmaxt), pptop, dum, dum) ! standard profile temperature
+    ! Standard pressure profile above the domain
+    call atmstd (0.d0, p0, t0, &
+                 ztmet(nbmaxt), pptop, dum, dum)
     phmet(nbmaxt,itp) = pptop
     do k = nbmaxt-1, 1, -1
       tmoy = 0.5d0*(ttmet(k+1,itp) + ttmet(k,itp)) + tkelvi

src/atmo/attssc.f90
@@ -209,7 +209,8 @@ if (ippmod(iatmos).eq.2.and.modsedi.eq.1) then ! for humid atmo. physics only
     if (qliqmax.gt.1d-8) then
       do iel = 1, ncel
         if (imeteo.eq.0) then
-          call atmstd(xyzcen(3,iel),pp,dum,dum)
+          call atmstd(0.d0, p0, t0, &
+                      xyzcen(3,iel),pp,dum,dum)
         else if (imeteo.eq.1) then
           call intprf &
             ( nbmett, nbmetm,                                        &
@@ -384,7 +385,8 @@ contains
 
       do iel = 1, ncel
         if (imeteo.eq.0) then
-          call atmstd(xyzcen(3,iel),pres(iel),dum,dum)
+          call atmstd(0.d0, p0, t0, &
+                      xyzcen(3,iel),pres(iel),dum,dum)
         else if (imeteo.eq.1) then
           call intprf                                                      &
               ( nbmett, nbmetm,                                            &

src/atmo/cs_atmo.cpp
@@ -3531,7 +3531,11 @@ cs_atmo_bcond(void)
         cs_real_t dum1;
         cs_real_t dum2;
         if (cs_glob_atmo_option->meteo_profile == 0)
-          cs_atmo_profile_std(z_in, &pp, &dum1, &dum2);
+          cs_atmo_profile_std(0., /* z_ref */
+                              phys_pro->p0,
+                              phys_pro->t0,
+                              z_in,
+                              &pp, &dum1, &dum2);
 
         /* Pressure profile from meteo file: */
         else if (cs_glob_atmo_option->meteo_profile == 1)
@@ -3743,7 +3747,10 @@ cs_soil_model(void)
       f_foir[soil_id] = foir;
 
       if (cs_glob_atmo_option->meteo_profile == 0) {
-        cs_atmo_profile_std(cell_cen[cell_id][2], &pphy, &dum, &dum);
+        cs_atmo_profile_std(0., /* z_ref */
+                            phys_pro->p0,
+                            phys_pro->t0,
+                            cell_cen[cell_id][2], &pphy, &dum, &dum);
       }
       else if (cs_glob_atmo_option->meteo_profile == 1) {
         int nbmett = cs_glob_atmo_option->met_1d_nlevels_t;

src/atmo/cs_atmo_profile_std.cpp
@@ -89,6 +89,9 @@ BEGIN_C_DECLS
 /*!
  * \brief compute standard atmospheric profile (Holton p 374)
  *
+ * \param[in]       z_ref      reference altitude in m
+ * \param[in]       p_ref      reference pressure
+ * \param[in]       t_ref      reference temperature
  * \param[in]       z          absolute altitude in m
  * \param[out]      p          pressure in Pa
  * \param[out]      t          temperature in K
@@ -97,26 +100,27 @@ BEGIN_C_DECLS
 /*----------------------------------------------------------------------------*/
 
 void
-cs_atmo_profile_std(cs_real_t   z,
+cs_atmo_profile_std(cs_real_t   z_ref,
+                    cs_real_t   p_ref,
+                    cs_real_t   t_ref,
+                    cs_real_t   z,
                     cs_real_t  *p,
                     cs_real_t  *t,
                     cs_real_t  *r)
 {
   cs_real_t rair = cs_glob_fluid_properties->r_pg_cnst;
-  cs_real_t p0 = cs_glob_fluid_properties->p0;
-  cs_real_t t0 = cs_glob_fluid_properties->t0;
   cs_real_t zt = 11000.;
   cs_real_t g = cs_math_3_norm(cs_glob_physical_constants->gravity);
   cs_real_t a = -6.5e-3;
 
-  if (z <= zt){
-    *t = t0 + a*z;
-    *p = p0*pow((*t)/t0, -g/rair/a);
+  if (z <= zt) {
+    *t = t_ref + a*(z-z_ref);
+    *p = p_ref*pow((*t)/t_ref, -g/rair/a);
     *r = (*p)/rair/(*t);
   }
   else {
-    cs_real_t t11 = t0 + a*zt;
-    cs_real_t p11 = p0*pow(t11/t0,-g/rair/a);
+    cs_real_t t11 = t_ref + a*(zt - z_ref);
+    cs_real_t p11 = p_ref*pow(t11/t_ref,-g/rair/a);
     *t = t11;
     *p = p11*exp(-g/rair/t11*(z - zt));
     *r = (*p)/rair/(*t);

src/atmo/cs_atmo_profile_std.h
@@ -45,15 +45,21 @@ BEGIN_C_DECLS
 /*!
  * \brief compute standard atmospheric profile (Holton p 374)
  *
+ * \param[in]       z_ref      reference altitude in m
+ * \param[in]       p_ref      reference pressure
+ * \param[in]       t_ref      reference temperature
  * \param[in]       z          absolute altitude in m
- * \param[out]      p          pressure in pa
- * \param[out]      t          temperature in k
+ * \param[out]      p          pressure in Pa
+ * \param[out]      t          temperature in K
  * \param[out]      r          density in kg/m3
  */
 /*----------------------------------------------------------------------------*/
 
 void
-cs_atmo_profile_std(cs_real_t   z,
+cs_atmo_profile_std(cs_real_t   z_ref,
+                    cs_real_t   p_ref,
+                    cs_real_t   t_ref,
+                    cs_real_t   z,
                     cs_real_t  *p,
                     cs_real_t  *t,
                     cs_real_t  *r);

src/atmo/cs_atmo_variables.cpp
@@ -193,7 +193,10 @@ _gaussian(const cs_mesh_t             *m,
     cs_real_t pp = 0.0, dum = 0.0;
     const cs_real_t zent = cell_cen[c_id][2];
     if (at_opt->meteo_profile == 0)
-      cs_atmo_profile_std(zent, &pp, &dum, &dum);
+      cs_atmo_profile_std(0., /* z_ref */
+                          fluid_props->p0,
+                          fluid_props->t0,
+                          zent, &pp, &dum, &dum);
     else if (at_opt->meteo_profile == 1)
       pp = cs_intprf(at_opt->met_1d_nlevels_t,
                      at_opt->met_1d_ntimes,
@@ -993,7 +996,10 @@ cs_atmo_physical_properties_update(void)
     const cs_real_t zent = cell_cen[c_id][2];
 
     if (at_opt->meteo_profile == 0)
-      cs_atmo_profile_std(zent, &pp, &dum, &dum);
+      cs_atmo_profile_std(0., /* z_ref */
+                          fluid_props->p0,
+                          fluid_props->t0,
+                          zent, &pp, &dum, &dum);
     else if (at_opt->meteo_profile == 1)
       pp = cs_intprf(at_opt->met_1d_nlevels_t,
                      at_opt->met_1d_ntimes,

src/atmo/cs_atprke.cpp
@@ -342,6 +342,7 @@ _humid_atmosphere(const cs_real_t  cromo[],
   const cs_real_3_t *cell_cen = (const cs_real_3_t *)fvq->cell_cen;
 
   const cs_real_t *grav = cs_glob_physical_constants->gravity;
+  const cs_fluid_properties_t *fluid_props = cs_glob_fluid_properties;
 
   cs_field_t *f_thm = cs_thermal_model_field();
 
@@ -397,7 +398,10 @@ _humid_atmosphere(const cs_real_t  cromo[],
     /* calculate the physical pressure 'pphy' */
 
     if (cs_glob_atmo_option->meteo_profile == 0) {
-      cs_atmo_profile_std(cell_cen[c_id][2], &pphy, &dum, &dum);
+      cs_atmo_profile_std(0., /* z_ref */
+                          fluid_props->p0,
+                          fluid_props->t0,
+                          cell_cen[c_id][2], &pphy, &dum, &dum);
     }
     else if (cs_glob_atmo_option->meteo_profile == 1) {
       int nbmett = cs_glob_atmo_option->met_1d_nlevels_t;

commit b831da6efdbd516f8fd1a3a7bca0b090436dc207

[Atmo] fix bug introduced in commit f56629e6 (2025-01-06).
Missing conversion from Celsius to Kelvin.

src/atmo/atr1vf.f90
@@ -406,7 +406,7 @@ if (mod(ntcabs,nfatr1).eq.0.or.ideb.eq.0) then
 
       ! Initialize with standard atmosphere
       ! above the domain
-      call atmstd(zray(kvert), preray(kvert), temray(kvert), &
+      call atmstd(zray(kvert), preray(kvert), temray(kvert)+tkelvi, &
                   zray(k), preray(k), temray(k), romray(k))
       ! Conversion Kelvin to Celsius
       temray(k) = temray(k) - tkelvi