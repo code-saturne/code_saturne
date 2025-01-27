code-saturne/code_saturne

commit 4ce866112d8b30d95fd9b39adefa137f106e081b

fix setting of reference temperature using Celsius scale.
Note that T0 should always be in Kelvin as specified in the code.
The boundary conditions of Cooling tower module has also been repaired.

src/gui/cs_gui.cpp
@@ -3991,9 +3991,17 @@ cs_gui_physical_properties(void)
   /* Read T0 in each case for user */
   /* do not read reference temperature for atmo with meteo data.
    * Would be cleaner to NOT write it in the xml file... */
+  cs_real_t _t0 = -999.;
   if (!(   cs_glob_physical_model_flag[CS_ATMOSPHERIC] != -1
         && cs_glob_atmo_option->meteo_profile >= 1))
-    cs_gui_fluid_properties_value("reference_temperature", &(phys_pp->t0));
+    cs_gui_fluid_properties_value("reference_temperature", &_t0);
+
+  if (_t0 > -998) {
+    if (cs_glob_thermal_model->temperature_scale == CS_TEMPERATURE_SCALE_CELSIUS)
+      _t0 += cs_physical_constants_celsius_to_kelvin;
+
+    phys_pp->t0 = _t0;
+  }
 
   if (cs_glob_physical_model_flag[CS_COMPRESSIBLE] > -1)
     cs_gui_fluid_properties_value("reference_molar_mass", &(phys_pp->xmasmr));

src/atmo/cs_air_props.cpp
@@ -543,7 +543,7 @@ cs_rho_humidair(cs_real_t   yw_h,
  * \param[in]     x             absolute humidity of humid air
  * \param[in]     rho0          reference density of humid air
  * \param[in]     p0            reference pressure
- * \param[in]     t0            reference temperature of humid air
+ * \param[in]     t0            reference temperature of humid air (Kelvin)
  * \param[in]     molmassrat    dry air to water vapor molecular mass ratio
  * \param[in]     t_h           temperature of humid air in Celsius
  */

src/ctwr/cs_ctwr_boundary_conditions.cpp
@@ -113,7 +113,7 @@ cs_ctwr_bcond(void)
   cs_real_t tkelvin = cs_physical_constants_celsius_to_kelvin;
 
   const cs_real_t xhum = air_prop->humidity0;
-  cs_real_t ref_temp = phys_pro->t0;
+  cs_real_t ref_temp = phys_pro->t0; /* In Kelvin */
 
   if (cs_glob_physical_model_flag[CS_ATMOSPHERIC] == CS_ATMO_HUMID) {
     cs_atmo_option_t *aopt = cs_glob_atmo_option;
@@ -130,6 +130,9 @@ cs_ctwr_bcond(void)
     ref_temp = (aopt->meteo_t0 - clatev/cp0 * aopt->meteo_ql0)
                    * pow(pref/ aopt->meteo_psea, rscp);
   }
+
+  if (cs_glob_thermal_model->temperature_scale == CS_TEMPERATURE_SCALE_CELSIUS)
+    ref_temp -= cs_physical_constants_celsius_to_kelvin;
 
   for (cs_lnum_t face_id = 0; face_id < n_b_faces; face_id++) {
 

src/ctwr/cs_ctwr_initialize.cpp
@@ -233,7 +233,7 @@ cs_ctwr_fields_init1(void)
  * \brief Initialize the field variables
  *
  * \param[in]     rho0        Reference density of humid air
- * \param[in]     t0          Reference temperature of humid air
+ * \param[in]     t0          Reference temperature of humid air (Kelvin)
  * \param[in]     p0          Reference pressure
  * \param[in]     molmassrat  Dry air to water vapor molecular mass ratio
  */
@@ -404,7 +404,7 @@ cs_ctwr_init_field_vars(cs_real_t  rho0,
     }
     else {
 
-      /* Bulk humid air temperature */
+      /* Bulk humid air temperature in Celsius, t0 in Kelvin */
       t_h[cell_id] = t0 - cs_physical_constants_celsius_to_kelvin;
       t_h_a[cell_id] = t_h[cell_id];
 

src/ctwr/cs_ctwr_physical_properties.cpp
@@ -139,7 +139,7 @@ cs_ctwr_compute_reference_pressure(cs_lnum_t  cell_id,
  * \brief Reset the field variables based on the restart values
  *
  * \param[in]     rho0        Reference density of humid air
- * \param[in]     t0          Reference temperature of humid air
+ * \param[in]     t0          Reference temperature of humid air (Kelvin)
  * \param[in]     p0          Reference pressure
  * \param[in]     humidity0   Reference humidity
  * \param[in]     molmassrat  Dry air to water vapor molecular mass ratio
@@ -405,7 +405,7 @@ cs_ctwr_restart_field_vars(cs_real_t  rho0,
  *        the liquid.
  *
  * \param[in]     rho0        Reference density of humid air
- * \param[in]     t0          Reference temperature of humid air
+ * \param[in]     t0          Reference temperature of humid air (Kelvin)
  * \param[in]     p0          Reference pressure
  */
 /*----------------------------------------------------------------------------*/

src/ctwr/cs_ctwr_physical_properties.h
@@ -46,14 +46,14 @@ BEGIN_C_DECLS
  * \param[in]     cell_id       Cell index
  * \param[in]     p0            Fluid properties reference pressure
  * \param[in]     ref_ressure   Atmospheric reference pressure
- * \return        pphy            Reference pressure
+ * \return        pphy          Reference pressure
  */
 /*----------------------------------------------------------------------------*/
 
 cs_real_t
 cs_ctwr_compute_reference_pressure(cs_lnum_t  cell_id,
-                                   cs_real_t   p0,
-                                   cs_field_t  *ref_pressure);
+                                   cs_real_t  p0,
+                                   cs_field_t *ref_pressure);
 
 /*----------------------------------------------------------------------------*/
 /*!
@@ -61,7 +61,7 @@ cs_ctwr_compute_reference_pressure(cs_lnum_t  cell_id,
  *        the liquid
  *
  * \param[in]     rho0        Reference density of humid air
- * \param[in]     t0          Reference temperature of humid air
+ * \param[in]     t0          Reference temperature of humid air (Kelvin)
  * \param[in]     p0          Reference pressure
  */
 /*----------------------------------------------------------------------------*/
@@ -76,7 +76,7 @@ cs_ctwr_phyvar_update(cs_real_t  rho0,
  * \brief Reset the field variables based on the restart values
  *
  * \param[in]     rho0        Reference density of humid air
- * \param[in]     t0          Reference temperature of humid air
+ * \param[in]     t0          Reference temperature of humid air (Kelvin)
  * \param[in]     p0          Reference pressure
  * \param[in]     humidity0   Reference humidity
  * \param[in]     molmassrat  Dry air to water vapor molecular mass ratio

commit 715ecc93211f63b9e22dd6407d5984e6f928329c

Fix bug introduced in commit 4ce86611 (2024-11-20).

src/gui/cs_gui.cpp
@@ -2811,8 +2811,7 @@ void cs_gui_initial_conditions(void)
   if (cs_glob_physical_model_flag[CS_HEAT_TRANSFER] > -1) {
     cs_fluid_properties_t *phys_pp = cs_get_glob_fluid_properties();
     cs_equation_param_t  *eqp = cs_equation_param_by_name(CS_THERMAL_EQNAME);
-    /* Convert to °C since T0 is stored in Kelvin */
-    cs_real_t _t0 = phys_pp->t0 - cs_physical_constants_celsius_to_kelvin;
+    cs_real_t _t0 = phys_pp->t0;
     cs_equation_add_ic_by_value(eqp, nullptr, &(_t0));
   }
 
@@ -4067,9 +4066,18 @@ cs_gui_physical_properties(void)
     cs_gui_fluid_properties_value("reference_temperature", &_t0);
 
   if (_t0 > -998) {
-    if (cs_glob_thermal_model->temperature_scale == CS_TEMPERATURE_SCALE_CELSIUS)
+    /* FIXME: the current cooling towers code seems to assume t0 is in
+       Kelvin, but this assumption is false in general, and not consistent
+       with t0 using the current temperature scale. For now, we follow
+       this logic to avoid issues with the atmospheric models assuming
+       a Kelvin scale and the cooling towers model assuming a Celsius
+       scale, but the Cooling towers module should align itself on the
+       rest of the code, not the other way around. */
+    if (   cs_glob_physical_model_flag[CS_COOLING_TOWERS] > -1
+        && (   cs_glob_thermal_model->temperature_scale
+            == CS_TEMPERATURE_SCALE_CELSIUS)) {
       _t0 += cs_physical_constants_celsius_to_kelvin;
-
+    }
     phys_pp->t0 = _t0;
   }
 