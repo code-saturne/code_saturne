code-saturne/code_saturne

commit 23c99438996e147379b32fdd67a4044cd8f53eb0

Major commit : link CDO based heat transfer solver to GUI
  * The heat transfer solver is now usable through the GUI
  * This new feature is **experimental** and some functionnalities may
    not be available, notably for post-processing.

src/gui/cs_gui.cpp
@@ -55,6 +55,7 @@
 #include "cs_boundary.h"
 #include "cs_boundary_zone.h"
 #include "cs_cf_model.h"
+#include "cs_domain_setup.h"
 #include "cs_equation.h"
 #include "cs_equation_param.h"
 #include "cs_ext_neighborhood.h"
@@ -84,12 +85,14 @@
 #include "cs_param_sles.h"
 #include "cs_partition.h"
 #include "cs_physical_model.h"
+#include "cs_property.h"
 #include "cs_rotation.h"
 #include "cs_selector.h"
 #include "cs_timer.h"
 #include "cs_time_moment.h"
 #include "cs_time_table.h"
 #include "cs_thermal_model.h"
+#include "cs_thermal_system.h"
 #include "cs_physical_properties.h"
 #include "cs_time_step.h"
 #include "cs_turbomachinery.h"
@@ -1945,6 +1948,11 @@ cs_gui_hydrostatic_equ_param(void)
 void
 cs_gui_dt_param(void)
 {
+  /* If HTSolver & steady state exit */
+  if (   cs_glob_physical_model_flag[CS_HEAT_TRANSFER] > -1
+      && cs_glob_domain->time_options->idtvar == CS_TIME_STEP_LOCAL)
+    return;
+
   /* Default, forbidden values for time step factor */
   double cdtmin = -1., cdtmax = -1.;
 
@@ -1961,6 +1969,11 @@ cs_gui_dt_param(void)
   cs_gui_node_get_child_real(tn, "max_fourier_num", &(time_opt->foumax));
   cs_gui_node_get_child_real(tn, "time_step_var", &(time_opt->varrdt));
   cs_gui_node_get_child_real(tn, "relaxation_coefficient", &(time_opt->relxst));
+
+  /* Call CDO function for HT Solver */
+  if (   cs_glob_physical_model_flag[CS_HEAT_TRANSFER] > -1
+      && time_opt->idtvar == CS_TIME_STEP_CONSTANT)
+    cs_domain_def_time_step_by_value(cs_glob_domain, time_stp->dt_ref);
 
   if (cdtmin > 0)
     time_opt->dtmin = cdtmin * time_stp->dt_ref;
@@ -1983,6 +1996,8 @@ cs_gui_dt_param(void)
     if (_t_max >= 0)
       time_stp->t_max = time_stp->t_prev + _t_max;
   }
+  bft_printf("%s[L%d] : t_max = %f\n", __func__, __LINE__, time_stp->t_max);
+  bft_printf_flush();
 
   if (_t_max < 0) {
     int _nt_max = -1;
@@ -1995,6 +2010,9 @@ cs_gui_dt_param(void)
         time_stp->nt_max = time_stp->nt_prev + _nt_max;
     }
   }
+
+  bft_printf("%s[L%d] : nt_max = %d\n", __func__, __LINE__, time_stp->nt_max);
+  bft_printf_flush();
 
   cs_gui_node_get_child_status_int(tn,
                                    "thermal_time_step",
@@ -2790,6 +2808,15 @@ void cs_gui_initial_conditions(void)
   const cs_real_3_t *restrict cell_cen =
     (const cs_real_3_t *restrict)cs_glob_mesh_quantities->cell_cen;
 
+  /* For HT solver by default we set initialization at T0_ref on zone "all_cells" */
+  if (cs_glob_physical_model_flag[CS_HEAT_TRANSFER] > -1) {
+    cs_fluid_properties_t *phys_pp = cs_get_glob_fluid_properties();
+    cs_equation_param_t  *eqp = cs_equation_param_by_name(CS_THERMAL_EQNAME);
+    /* Convert to °C since T0 is stored in Kelvin */
+    cs_real_t _t0 = phys_pp->t0 - cs_physical_constants_celsius_to_kelvin;
+    cs_equation_add_ic_by_value(eqp, nullptr, &(_t0));
+  }
+
   for (int z_id = 0; z_id < n_zones; z_id++) {
     const cs_zone_t *z = cs_volume_zone_by_id(z_id);
 
@@ -2801,6 +2828,34 @@ void cs_gui_initial_conditions(void)
       snprintf(z_id_str, 31, "%d", z_id);
 
       if (restart == 0) {
+        /* First heat transfer solver, otherwise legacy thermal scalar */
+        if (cs_glob_physical_model_flag[CS_HEAT_TRANSFER] > -1) {
+          const char *formula_sca    = NULL;
+          cs_tree_node_t *tn_sca
+            = cs_tree_get_node(cs_glob_tree,
+                               "thermophysical_models/"
+                               "thermal_scalar/variable/formula");
+          tn_sca = _add_zone_id_test_attribute(tn_sca, z->id);
+          formula_sca = cs_tree_node_get_value_str(tn_sca);
+
+          if (formula_sca != nullptr) {
+            cs_equation_param_t  *eqp =
+              cs_equation_param_by_name(CS_THERMAL_EQNAME);
+
+            cs_meg_xdef_input_t *_input
+              = cs_meg_xdef_wrapper_add_input(CS_MEG_BOUNDARY_FUNC,
+                                              z_id,
+                                              1,
+                                              "temperature",
+                                              NULL);
+            cs_equation_add_ic_by_analytic(eqp,
+                                           z->name,
+                                           cs_meg_xdef_wrapper,
+                                           _input);
+
+          }
+          continue;
+        }
 
         cs_tree_node_t *tn_velocity
           = cs_tree_get_node(cs_glob_tree,
@@ -2811,7 +2866,7 @@ void cs_gui_initial_conditions(void)
 
         cs_field_t *c_vel = cs_field_by_name("velocity");
 
-        if (formula_uvw != NULL) {
+        if (c_vel != NULL && formula_uvw != NULL) {
           cs_real_t *ini_vals = NULL;
           BFT_MALLOC(ini_vals, c_vel->dim * n_cells, cs_real_t);
 
@@ -4123,6 +4178,83 @@ cs_gui_physical_properties(void)
       int cond_diff_id = cs_field_get_key_int(tf, k);
       if (cond_diff_id < 0)
         cs_field_set_key_int(tf, k, 0);
+    }
+  }
+
+  /* CDO HT Solver define physical properties */
+  if (cs_glob_physical_model_flag[CS_HEAT_TRANSFER] > -1) {
+    // density
+    cs_property_t  *rho = cs_property_by_name(CS_PROPERTY_MASS_DENSITY);
+    if (phys_pp->irovar == 0) {
+      cs_property_def_iso_by_value(rho, NULL, phys_pp->ro0);
+    }
+    else {
+      for (int z_id = 0; z_id < cs_volume_zone_n_zones(); z_id++) {
+        const cs_zone_t *z = cs_volume_zone_by_id(z_id);
+        if (z->type & CS_VOLUME_ZONE_PHYSICAL_PROPERTIES) {
+          cs_meg_xdef_input_t *_input
+            = cs_meg_xdef_wrapper_add_input(CS_MEG_VOLUME_FUNC,
+                                            z_id,
+                                            1,
+                                            "density",
+                                            NULL);
+          cs_property_def_by_analytic(rho,
+                                      z->name,
+                                      cs_meg_xdef_wrapper,
+                                      _input);
+
+        }
+      }
+    }
+
+    // Cp
+    cs_property_t  *cp = cs_property_by_name(CS_THERMAL_CP_NAME);
+    if (phys_pp->icp > 0) {
+      for (int z_id = 0; z_id < cs_volume_zone_n_zones(); z_id++) {
+        const cs_zone_t *z = cs_volume_zone_by_id(z_id);
+        if (z->type & CS_VOLUME_ZONE_PHYSICAL_PROPERTIES) {
+          cs_meg_xdef_input_t *_input
+            = cs_meg_xdef_wrapper_add_input(CS_MEG_VOLUME_FUNC,
+                                            z_id,
+                                            1,
+                                            "specific_heat",
+                                            NULL);
+          cs_property_def_by_analytic(cp,
+                                      z->name,
+                                      cs_meg_xdef_wrapper,
+                                      _input);
+
+        }
+      }
+    }
+    else {
+      cs_property_def_iso_by_value(cp, NULL, phys_pp->cp0);
+    }
+
+    // Lambda
+    cs_property_t  *lambda = cs_property_by_name(CS_THERMAL_LAMBDA_NAME);
+    int _lambda_choice = -1;
+    _properties_choice_id("thermal_conductivity", &_lambda_choice);
+    if (_properties_choice_id > 0) {
+      for (int z_id = 0; z_id < cs_volume_zone_n_zones(); z_id++) {
+        const cs_zone_t *z = cs_volume_zone_by_id(z_id);
+        if (z->type & CS_VOLUME_ZONE_PHYSICAL_PROPERTIES) {
+          cs_meg_xdef_input_t *_input
+            = cs_meg_xdef_wrapper_add_input(CS_MEG_VOLUME_FUNC,
+                                            z_id,
+                                            1,
+                                            "thermal_conductivity",
+                                            NULL);
+          cs_property_def_by_analytic(lambda,
+                                      z->name,
+                                      cs_meg_xdef_wrapper,
+                                      _input);
+
+        }
+      }
+    }
+    else {
+      cs_property_def_iso_by_value(lambda, NULL, phys_pp->lambda0);
     }
   }
 

python/code_saturne/base/cs_meg_to_c.py
@@ -550,7 +550,7 @@ class meg_to_c_interpreter:
         usr_blck += usr_defs
 
         usr_blck += 2*tab + 'for (cs_lnum_t e_id = 0; e_id < n_elts; e_id++) {\n'
-        usr_blck += 3*tab + '[[maybe_unused]] cs_lnum_t c_id = elt_ids[e_id];\n'
+        usr_blck += 3*tab + 'cs_lnum_t c_id = (elt_ids != nullptr) ? elt_ids[e_id] : e_id;\n'
 
         usr_blck += usr_code
 
@@ -662,7 +662,7 @@ class meg_to_c_interpreter:
 
         if need_for_loop:
             usr_blck += 2*tab + 'for (cs_lnum_t e_id = 0; e_id < n_elts; e_id++) {\n'
-            usr_blck += 3*tab + '[[maybe_unused]] cs_lnum_t b_e_id = elt_ids[e_id];\n'
+            usr_blck += 3*tab + 'cs_lnum_t b_e_id = (elt_ids != nullptr) ? elt_ids[e_id] : e_id;\n'
 
         usr_blck += usr_code
 

python/code_saturne/gui/base/BrowserView.py
@@ -838,6 +838,7 @@ class BrowserView(QWidget, Ui_BrowserForm):
         m_atmo = False
         m_gwf = False
         m_icm = False
+        m_hts = False
 
         node_pm = case.xmlGetNode('thermophysical_models')
 
@@ -882,6 +883,12 @@ class BrowserView(QWidget, Ui_BrowserForm):
                 node = node_pm.xmlGetNode('compressible_model', 'model')
                 if node and node['model'] != 'off':
                     m_cpr = True
+                    m_thermal = 1
+
+            if not m_thermal:
+                node = node_pm.xmlGetNode('hts_model', 'model')
+                if node and node['model'] != 'off':
+                    m_hts = True
                     m_thermal = 1
 
             if not m_thermal:
@@ -966,16 +973,18 @@ class BrowserView(QWidget, Ui_BrowserForm):
 
         # Thermophysical Models
 
+        print(f"{m_hts=}")
         self.setRowShow(self.tr('Calculation features'), True)
         self.setRowShow(self.tr('Main fields'), (p_module == 'neptune_cfd'))
+        self.setRowShow(self.tr('Body forces'), not m_hts)
+        self.setRowShow(self.tr('Species transport'), not m_hts)
         self.setRowShow(self.tr('Deformable mesh'), m_ale)
         self.setRowShow(self.tr('Gas combustion'), m_gas_comb)
         self.setRowShow(self.tr('Pulverized fuel combustion'), m_sf_comb)
         self.setRowShow(self.tr('Electrical models'), m_elec)
         self.setRowShow(self.tr('Atmospheric flows'), m_atmo)
         self.setRowShow(self.tr('Thermal model'), (m_thermal > -1))
-        self.setRowShow(self.tr('Body forces'), True)
-        self.setRowShow(self.tr('Turbulence models'), not m_gwf)
+        self.setRowShow(self.tr('Turbulence models'), (not (m_gwf or m_hts)))
         self.setRowShow(self.tr('Transported Variables'))
         self.setRowShow(self.tr('Groundwater flows'), m_gwf)
         self.setRowShow(self.tr('Turbomachinery'), m_tbm)

python/code_saturne/gui/case/AnalysisFeaturesForm.ui
@@ -7,7 +7,7 @@
     <x>0</x>
     <y>0</y>
     <width>1188</width>
-    <height>717</height>
+    <height>905</height>
    </rect>
   </property>
   <property name="windowTitle">
@@ -105,16 +105,21 @@
       <string>Flow Models</string>
      </property>
      <layout class="QGridLayout" name="gridLayout_2">
-      <item row="10" column="1">
-       <widget class="QComboBox" name="comboBoxHgn"/>
+      <item row="11" column="1">
+       <widget class="QComboBox" name="comboBoxNeptuneCFD"/>
       </item>
-      <item row="4" column="0">
-       <widget class="QRadioButton" name="radioButtonJouleEffect">
+      <item row="4" column="1">
+       <widget class="QComboBox" name="comboBoxJouleEffect"/>
+      </item>
+      <item row="2" column="0">
+       <widget class="QRadioButton" name="radioButtonSinglePhase">
         <property name="toolTip">
-         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Electric arcs and Joule heating.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot;
+                                     font-size:9pt;&quot;&gt;Base single-phase models.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;
+                                 </string>
         </property>
         <property name="text">
-         <string>Electric arcs</string>
+         <string>Standard Eulerian single phase</string>
         </property>
        </widget>
       </item>
@@ -135,26 +140,13 @@
         </property>
        </widget>
       </item>
-      <item row="7" column="1">
-       <widget class="QComboBox" name="comboBoxGasCombustion"/>
-      </item>
-      <item row="10" column="0">
-       <widget class="QRadioButton" name="radioButtonHgn">
-        <property name="text">
-         <string>Homogeneous Eulerian - VoF model</string>
-        </property>
-       </widget>
-      </item>
-      <item row="11" column="1">
-       <widget class="QComboBox" name="comboBoxNeptuneCFD"/>
-      </item>
-      <item row="3" column="0">
-       <widget class="QRadioButton" name="radioButtonAtmospheric">
+      <item row="11" column="0">
+       <widget class="QRadioButton" name="radioButtonNeptuneCFD">
         <property name="toolTip">
-         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Models for atmospheric flows.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Activate NEPTUNE_CFD module.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
         </property>
         <property name="text">
-         <string>Atmospheric</string>
+         <string>Eulerian multiphase (neptune_cfd)</string>
         </property>
        </widget>
       </item>
@@ -170,12 +162,6 @@
         </property>
        </widget>
       </item>
-      <item row="5" column="1">
-       <widget class="QComboBox" name="comboBoxGroundwater"/>
-      </item>
-      <item row="9" column="1">
-       <widget class="QComboBox" name="comboBoxCoalCombustion"/>
-      </item>
       <item row="6" column="0">
        <widget class="QRadioButton" name="radioButtonReactiveFlows">
         <property name="toolTip">
@@ -188,39 +174,28 @@
         </property>
        </widget>
       </item>
-      <item row="3" column="1">
-       <widget class="QComboBox" name="comboBoxAtmospheric"/>
-      </item>
-      <item row="4" column="1">
-       <widget class="QComboBox" name="comboBoxJouleEffect"/>
-      </item>
-      <item row="11" column="0">
-       <widget class="QRadioButton" name="radioButtonNeptuneCFD">
+      <item row="3" column="0">
+       <widget class="QRadioButton" name="radioButtonAtmospheric">
         <property name="toolTip">
-         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Activate NEPTUNE_CFD module.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Models for atmospheric flows.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
         </property>
         <property name="text">
-         <string>Eulerian multiphase (neptune_cfd)</string>
+         <string>Atmospheric</string>
         </property>
        </widget>
       </item>
-      <item row="1" column="1" rowspan="2">
-       <widget class="QComboBox" name="comboBoxSinglePhase"/>
+      <item row="7" column="1">
+       <widget class="QComboBox" name="comboBoxGasCombustion"/>
       </item>
-      <item row="6" column="1">
-       <widget class="QComboBox" name="comboBoxReactiveFlows"/>
-      </item>
-      <item row="2" column="0">
-       <widget class="QRadioButton" name="radioButtonSinglePhase">
-        <property name="toolTip">
-         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot;
-                                     font-size:9pt;&quot;&gt;Base single-phase models.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;
-                                 </string>
-        </property>
+      <item row="10" column="0">
+       <widget class="QRadioButton" name="radioButtonHgn">
         <property name="text">
-         <string>Standard Eulerian single phase</string>
+         <string>Homogeneous Eulerian - VoF model</string>
         </property>
        </widget>
+      </item>
+      <item row="9" column="1">
+       <widget class="QComboBox" name="comboBoxCoalCombustion"/>
       </item>
       <item row="12" column="1">
        <widget class="QCheckBox" name="checkBoxNeptuneHeatMass">
@@ -231,6 +206,41 @@
          <string>Activate gas/liquid heat and mass transfer terms</string>
         </property>
        </widget>
+      </item>
+      <item row="5" column="1">
+       <widget class="QComboBox" name="comboBoxGroundwater"/>
+      </item>
+      <item row="10" column="1">
+       <widget class="QComboBox" name="comboBoxHgn"/>
+      </item>
+      <item row="1" column="1" rowspan="2">
+       <widget class="QComboBox" name="comboBoxSinglePhase"/>
+      </item>
+      <item row="4" column="0">
+       <widget class="QRadioButton" name="radioButtonJouleEffect">
+        <property name="toolTip">
+         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Electric arcs and Joule heating.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+        </property>
+        <property name="text">
+         <string>Electric arcs</string>
+        </property>
+       </widget>
+      </item>
+      <item row="3" column="1">
+       <widget class="QComboBox" name="comboBoxAtmospheric"/>
+      </item>
+      <item row="6" column="1">
+       <widget class="QComboBox" name="comboBoxReactiveFlows"/>
+      </item>
+      <item row="13" column="0">
+       <widget class="QRadioButton" name="radioButtonHTS">
+        <property name="text">
+         <string>Solid Heat Transfer (Experimental)</string>
+        </property>
+       </widget>
+      </item>
+      <item row="13" column="1">
+       <widget class="QComboBox" name="comboBoxHTS"/>
       </item>
      </layout>
     </widget>

python/code_saturne/gui/case/AnalysisFeaturesView.py
@@ -66,11 +66,14 @@ from code_saturne.model.InterfacialAreaModel import InterfacialAreaModel
 from code_saturne.model.NeptuneWallTransferModel import NeptuneWallTransferModel
 from code_saturne.model.InterfacialEnthalpyModel import InterfacialEnthalpyModel
 from code_saturne.model.HgnModel import HgnModel
+from code_saturne.model.HTSModel import HTSModel
 
 from code_saturne.model.LagrangianModel import LagrangianModel
 from code_saturne.model.TurboMachineryModel import TurboMachineryModel
 from code_saturne.model.MobileMeshModel import MobileMeshModel
 from code_saturne.model.FansModel import FansStatus
+
+from code_saturne.model.TurbulenceModel import TurbulenceModel
 
 # -------------------------------------------------------------------------------
 # log config
@@ -124,6 +127,8 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
 
         self.modelNeptuneCFD     = QtPage.ComboModel(self.comboBoxNeptuneCFD,2,1)
 
+        self.modelHTS            = QtPage.ComboModel(self.comboBoxHTS,1,1)
+
         self.modelSinglePhase.addItem(self.tr("Incompressible"), 'off')
         self.modelSinglePhase.addItem(self.tr("Compressible, perfect gas with constant gamma"),
                                       'constant_gamma')
@@ -173,6 +178,9 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
         self.modelNeptuneCFD.addItem(self.tr("Particle-laden flow"), "particles_flow")
         self.modelNeptuneCFD.addItem(self.tr("Multiregime liquid/gas flow"), "multiregime")
 
+        # HTS combobox
+        self.modelHTS.addItem(self.tr("Heat conduction"), "hts_conduction")
+
         # Lagrangian combobox
 
         self.modelLagrangian = QtPage.ComboModel(self.comboBoxLagrangian, 4, 1)
@@ -203,6 +211,7 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
         self.comboBoxSinglePhase.activated[str].connect(self.slotSinglePhase)
         self.comboBoxGroundwater.activated[str].connect(self.slotGroundwater)
         self.comboBoxHgn.activated[str].connect(self.slotHgn)
+        self.comboBoxHTS.activated[str].connect(self.slotHTS)
         self.comboBoxNeptuneCFD.currentTextChanged[str].connect(self.slotNeptuneCFD)
         self.checkBoxNeptuneHeatMass.stateChanged.connect(self.slotNeptuneHeatMass)
         self.checkBoxALE.stateChanged.connect(self.slotALE)
@@ -213,7 +222,7 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
 
         for ind in ['SinglePhase', 'Atmospheric',
                     'JouleEffect', 'Groundwater', 'ReactiveFlows',
-                    'Hgn', 'NeptuneCFD']:
+                    'Hgn', 'NeptuneCFD', 'HTS']:
             eval('self.radioButton'+ind+'.toggled.connect(self.slotRadioButton)')
 
         # Initializations based on code
@@ -279,6 +288,7 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
         self.comboBoxJouleEffect.hide()
         self.comboBoxGroundwater.hide()
         self.comboBoxHgn.hide()
+        self.comboBoxHTS.hide()
         self.comboBoxNeptuneCFD.hide()
 
         self.checkBoxNeptuneHeatMass.hide()
@@ -300,7 +310,8 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
                         'Hgn',
                         'NeptuneCFD',
                         'Lagrangian',
-                        'TurboMachinery']:
+                        'TurboMachinery',
+                        'HTS']:
             log.debug("__stringModelFromCombo() Incorrect name for QComboBox name")
             string = ""
         else:
@@ -344,6 +355,7 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
         self.comp  = CompressibleModel(self.case)
         self.darc  = GroundwaterModel(self.case)
         self.hgn   = HgnModel(self.case)
+        self.hts   = HTSModel(self.case)
 
         from code_saturne.model.TimeStepModel import TimeStepModel
 
@@ -354,6 +366,7 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
         gas = self.gas.getGasCombustionModel()
         compressible = self.comp.getCompressibleModel()
         homogeneous = self.hgn.getHgnModel()
+        hts = self.hts.getHTSModel()
 
         # Set combobox values
 
@@ -387,6 +400,9 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
         elif homogeneous != 'off':
             self.modelHgn.setItem(str_model=homogeneous)
 
+        elif hts != 'off':
+            self.modelHTS.setItem(str_model=hts)
+
         else:
             self.modelSinglePhase.setItem(str_model=compressible)
 
@@ -399,6 +415,7 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
         ReactiveFlows = self.gas.getGasCombustionModel()  != 'off' \
                         or self.pcoal.getCoalCombustionModel() != 'off'
         Hgn = homogeneous != 'off'
+        HTS = hts != 'off'
 
         self.checkPrev = 'SinglePhase'
         combo = None
@@ -407,7 +424,8 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
                     'JouleEffect',
                     'Groundwater',
                     'ReactiveFlows',
-                    'Hgn']:
+                    'Hgn',
+                    'HTS']:
 
             radioButton = eval('self.radioButton'+ind)
             model_on = eval(ind)
@@ -580,7 +598,8 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
                     'JouleEffect',
                     'Groundwater',
                     'ReactiveFlows',
-                    'NeptuneCFD']:
+                    'NeptuneCFD',
+                    'HTS']:
 
             radioButton = eval('self.radioButton'+ind)
             combo = eval('self.comboBox'+ind)
@@ -663,6 +682,14 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
                 self.checkBoxPther.hide()
 
                 self.modelLagrangian.enableItem(str_model='two_way')
+            if self.checkPrev == 'HTS':
+                self.hts.setHTSModel('off')
+                self.labelLagrangian.show()
+                self.comboBoxLagrangian.show()
+                self.labelTurboMachinery.show()
+                self.comboBoxTurboMachinery.show()
+                self.checkBoxALE.show()
+                self.checkBoxFans.show()
 
             if checkCur == 'ReactiveFlows':
                 if self.pcoal.getCoalCombustionModel() != 'off':
@@ -673,6 +700,14 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
                     model = self.modelGasCombustion
                 combo.show()
                 model_expr = 'self.slotReactiveFlows(model)'
+
+            if checkCur == 'HTS':
+                self.labelLagrangian.hide()
+                self.comboBoxLagrangian.hide()
+                self.labelTurboMachinery.hide()
+                self.comboBoxTurboMachinery.hide()
+                self.checkBoxALE.hide()
+                self.checkBoxFans.hide()
 
             eval(model_expr)
 
@@ -875,6 +910,20 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
 
         model = self.__stringModelFromCombo('Hgn')
         self.hgn.setHgnModel(model)
+
+        self.browser.configureTree(self.case)
+
+    @pyqtSlot(str)
+    def slotHTS(self, text):
+        """
+        Called when the comboBoxHTS changed
+        """
+        self.comboBoxHTS.show()
+
+        model = self.__stringModelFromCombo('HTS')
+        self.hts.setHTSModel(model)
+
+        TurbulenceModel(self.case).setTurbulenceModel('off')
 
         self.browser.configureTree(self.case)
 

python/code_saturne/gui/case/FluidCharacteristicsView.py
@@ -56,6 +56,7 @@ from code_saturne.model.FluidCharacteristicsModel import FluidCharacteristicsMod
 from code_saturne.model.DefineUserScalarsModel import DefineUserScalarsModel
 from code_saturne.model.ThermalScalarModel import ThermalScalarModel
 from code_saturne.model.GroundwaterModel import GroundwaterModel
+from code_saturne.model.HTSModel import HTSModel
 from code_saturne.gui.case.QMegEditorView import QMegEditorView
 from code_saturne.model.NotebookModel import NotebookModel
 from code_saturne.model.InternalCouplingModel import InternalCouplingModel
@@ -223,6 +224,8 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
         self.m_th = ThermalScalarModel(self.case)
         s = self.m_th.getThermalScalarName()
         tsm = self.mdl.tsm
+
+        self.m_hts = HTSModel(self.case)
 
         # Particular Widget init. taking into account chosen fluid model
         mdl_atmo, mdl_joule, mdl_thermal, mdl_gas, mdl_coal, mdl_comp, mdl_hgn=\
@@ -691,6 +694,10 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
                     __combo.setEnabled(False)
                     __button.setEnabled(False)
                     __button.hide()
+
+            # HTS
+            if self.m_hts.getHTSModel() != 'off':
+                self.groupBoxMu.setVisible(False)
 
             # Compressible Flows
             if mdl_comp != 'off':

python/code_saturne/gui/case/ThermalView.py
@@ -60,6 +60,7 @@ from code_saturne.gui.case.ThermalRadiationAdvancedDialogForm import Ui_ThermalR
 from code_saturne.model.ThermalRadiationModel import ThermalRadiationModel
 from code_saturne.model.ThermalParticlesRadiationModel import ThermalParticlesRadiationModel
 from code_saturne.model.MainFieldsModel import MainFieldsModel
+from code_saturne.model.HTSModel import HTSModel
 
 #-------------------------------------------------------------------------------
 # log config
@@ -252,6 +253,10 @@ class ThermalView(QWidget, Ui_ThermalForm):
                 self.modelThermal.delItem(5)
                 self.modelThermal.delItem(4)
 
+            if HTSModel(self.case).getHTSModel() != 'off':
+                self.comboBoxThermal.setEnabled(False)
+                self.thermal.setThermalModel('temperature_celsius')
+
         # Select the thermal scalar model
 
         model = self.thermal.getThermalScalarModel()
@@ -319,7 +324,8 @@ class ThermalView(QWidget, Ui_ThermalForm):
         """
         Update for radiation model
         """
-        if self.case.module_name() == "code_saturne" and model == 'off':
+        if self.case.module_name() == "code_saturne" and model == 'off' \
+                or HTSModel(self.case).getHTSModel() != 'off':
             self.ThermalRadiationGroupBox.hide()
             return
 

commit 0e620a68dcd33cbb5df75b6d26ed1be05fb2dcd3

[GUI] Fix reading of physical properties for HTS solver
  * Constant lambda value was not read correctly when defined in GUI.
  * Error introduced in commit 23c9943899

src/gui/cs_gui.cpp
@@ -4222,7 +4222,7 @@ cs_gui_physical_properties(void)
 
     // Cp
     cs_property_t  *cp = cs_property_by_name(CS_THERMAL_CP_NAME);
-    if (phys_pp->icp > 0) {
+    if (phys_pp->icp == 0) {
       for (int z_id = 0; z_id < cs_volume_zone_n_zones(); z_id++) {
         const cs_zone_t *z = cs_volume_zone_by_id(z_id);
         if (z->type & CS_VOLUME_ZONE_PHYSICAL_PROPERTIES) {
@@ -4247,7 +4247,9 @@ cs_gui_physical_properties(void)
     // Lambda
     cs_property_t  *lambda = cs_property_by_name(CS_THERMAL_LAMBDA_NAME);
     int _lambda_choice = -1;
-    if (_properties_choice_id("thermal_conductivity", &_lambda_choice) > 0) {
+    _properties_choice_id("thermal_conductivity", &_lambda_choice);
+
+    if (_lambda_choice > 0) {
       for (int z_id = 0; z_id < cs_volume_zone_n_zones(); z_id++) {
         const cs_zone_t *z = cs_volume_zone_by_id(z_id);
         if (z->type & CS_VOLUME_ZONE_PHYSICAL_PROPERTIES) {
@@ -4266,6 +4268,8 @@ cs_gui_physical_properties(void)
       }
     }
     else {
+      /* Set lambda0 which has not been read before... */
+      cs_gui_properties_value("thermal_conductivity", &(phys_pp->lambda0));
       cs_property_def_iso_by_value(lambda, NULL, phys_pp->lambda0);
     }
 