code-saturne/code_saturne

commit 23c99438996e147379b32fdd67a4044cd8f53eb0

Major commit : link CDO based heat transfer solver to GUI
  * The heat transfer solver is now usable through the GUI
  * This new feature is **experimental** and some functionnalities may
    not be available, notably for post-processing.

src/base/cs_setup.cpp
@@ -2565,13 +2565,13 @@ _init_user
   cs_gui_user_arrays();
   cs_gui_calculator_functions();
 
-  /* Solid zones */
-  cs_velocity_pressure_set_solid();
-
-  /* Initialize parameters for specific physics */
-  cs_rad_transfer_options();
-
   if (cs_glob_param_cdo_mode != CS_PARAM_CDO_MODE_ONLY) {
+    /* Solid zones */
+    cs_velocity_pressure_set_solid();
+
+    /* Initialize parameters for specific physics */
+    cs_rad_transfer_options();
+
     _init_variable_fields();
     _create_variable_fields();
     cs_f_fldvar(nmodpp);
@@ -2587,16 +2587,20 @@ _init_user
   if (cs_glob_ale != CS_ALE_NONE)
     cs_gui_ale_diffusion_type();
 
-  cs_gui_laminar_viscosity();
+  if (cs_glob_physical_model_flag[CS_HEAT_TRANSFER] == -1) {
+    /* Dont run these functions if HTS mode is activated */
+    cs_gui_laminar_viscosity();
 
-  /* Compressible flows */
-  cs_gui_hydrostatic_equ_param();
-  const cs_field_t *f_id = cs_field_by_name_try("velocity");
-  if (f_id != NULL) {
-    if (cs_glob_physical_model_flag[CS_COMPRESSIBLE] != -1)
-      cs_runaway_check_define_field_max(f_id->id, 1.e5);
-    else
-      cs_runaway_check_define_field_max(f_id->id, 1.e4);
+    /* Compressible flows */
+    cs_gui_hydrostatic_equ_param();
+    const cs_field_t *f_id = cs_field_by_name_try("velocity");
+
+    if (f_id != NULL) {
+      if (cs_glob_physical_model_flag[CS_COMPRESSIBLE] != -1)
+        cs_runaway_check_define_field_max(f_id->id, 1.e5);
+      else
+        cs_runaway_check_define_field_max(f_id->id, 1.e4);
+    }
   }
 
   /* Initialization of global parameters */

src/cdo/cs_cdo_main.cpp
@@ -54,6 +54,7 @@
 #include "cs_domain_setup.h"
 #include "cs_equation.h"
 #include "cs_equation_system.h"
+#include "cs_gui.h"
 #include "cs_gwf.h"
 #include "cs_log.h"
 #include "cs_log_iteration.h"
@@ -813,6 +814,9 @@ cs_cdo_initialize_structures(cs_domain_t           *domain,
 
   cs_domain_init_cdo_structures(domain);
 
+  /* GUI initial conditions */
+  cs_gui_initial_conditions();
+
   /* Last user setup stage */
 
   cs_domain_finalize_user_setup(domain);
@@ -1114,7 +1118,7 @@ cs_cdo_main(cs_domain_t   *domain)
   /*  Build high-level structures and create algebraic systems
       Set the initial values of the fields and properties. */
 
-  _initialize_time_step(domain->time_step, &(domain->time_options));
+  _initialize_time_step(domain->time_step, domain->time_options);
 
   cs_domain_initialize_systems(domain);
 
@@ -1154,7 +1158,7 @@ cs_cdo_main(cs_domain_t   *domain)
 
     /* Define the current time step */
 
-    _define_current_time_step(domain->time_step, &(domain->time_options));
+    _define_current_time_step(domain->time_step, domain->time_options);
 
     /* Read a control file if present */
 

src/gui/cs_gui.cpp
@@ -55,6 +55,7 @@
 #include "cs_boundary.h"
 #include "cs_boundary_zone.h"
 #include "cs_cf_model.h"
+#include "cs_domain_setup.h"
 #include "cs_equation.h"
 #include "cs_equation_param.h"
 #include "cs_ext_neighborhood.h"
@@ -84,12 +85,14 @@
 #include "cs_param_sles.h"
 #include "cs_partition.h"
 #include "cs_physical_model.h"
+#include "cs_property.h"
 #include "cs_rotation.h"
 #include "cs_selector.h"
 #include "cs_timer.h"
 #include "cs_time_moment.h"
 #include "cs_time_table.h"
 #include "cs_thermal_model.h"
+#include "cs_thermal_system.h"
 #include "cs_physical_properties.h"
 #include "cs_time_step.h"
 #include "cs_turbomachinery.h"
@@ -1945,6 +1948,11 @@ cs_gui_hydrostatic_equ_param(void)
 void
 cs_gui_dt_param(void)
 {
+  /* If HTSolver & steady state exit */
+  if (   cs_glob_physical_model_flag[CS_HEAT_TRANSFER] > -1
+      && cs_glob_domain->time_options->idtvar == CS_TIME_STEP_LOCAL)
+    return;
+
   /* Default, forbidden values for time step factor */
   double cdtmin = -1., cdtmax = -1.;
 
@@ -1961,6 +1969,11 @@ cs_gui_dt_param(void)
   cs_gui_node_get_child_real(tn, "max_fourier_num", &(time_opt->foumax));
   cs_gui_node_get_child_real(tn, "time_step_var", &(time_opt->varrdt));
   cs_gui_node_get_child_real(tn, "relaxation_coefficient", &(time_opt->relxst));
+
+  /* Call CDO function for HT Solver */
+  if (   cs_glob_physical_model_flag[CS_HEAT_TRANSFER] > -1
+      && time_opt->idtvar == CS_TIME_STEP_CONSTANT)
+    cs_domain_def_time_step_by_value(cs_glob_domain, time_stp->dt_ref);
 
   if (cdtmin > 0)
     time_opt->dtmin = cdtmin * time_stp->dt_ref;
@@ -1983,6 +1996,8 @@ cs_gui_dt_param(void)
     if (_t_max >= 0)
       time_stp->t_max = time_stp->t_prev + _t_max;
   }
+  bft_printf("%s[L%d] : t_max = %f\n", __func__, __LINE__, time_stp->t_max);
+  bft_printf_flush();
 
   if (_t_max < 0) {
     int _nt_max = -1;
@@ -1995,6 +2010,9 @@ cs_gui_dt_param(void)
         time_stp->nt_max = time_stp->nt_prev + _nt_max;
     }
   }
+
+  bft_printf("%s[L%d] : nt_max = %d\n", __func__, __LINE__, time_stp->nt_max);
+  bft_printf_flush();
 
   cs_gui_node_get_child_status_int(tn,
                                    "thermal_time_step",
@@ -2790,6 +2808,15 @@ void cs_gui_initial_conditions(void)
   const cs_real_3_t *restrict cell_cen =
     (const cs_real_3_t *restrict)cs_glob_mesh_quantities->cell_cen;
 
+  /* For HT solver by default we set initialization at T0_ref on zone "all_cells" */
+  if (cs_glob_physical_model_flag[CS_HEAT_TRANSFER] > -1) {
+    cs_fluid_properties_t *phys_pp = cs_get_glob_fluid_properties();
+    cs_equation_param_t  *eqp = cs_equation_param_by_name(CS_THERMAL_EQNAME);
+    /* Convert to °C since T0 is stored in Kelvin */
+    cs_real_t _t0 = phys_pp->t0 - cs_physical_constants_celsius_to_kelvin;
+    cs_equation_add_ic_by_value(eqp, nullptr, &(_t0));
+  }
+
   for (int z_id = 0; z_id < n_zones; z_id++) {
     const cs_zone_t *z = cs_volume_zone_by_id(z_id);
 
@@ -2801,6 +2828,34 @@ void cs_gui_initial_conditions(void)
       snprintf(z_id_str, 31, "%d", z_id);
 
       if (restart == 0) {
+        /* First heat transfer solver, otherwise legacy thermal scalar */
+        if (cs_glob_physical_model_flag[CS_HEAT_TRANSFER] > -1) {
+          const char *formula_sca    = NULL;
+          cs_tree_node_t *tn_sca
+            = cs_tree_get_node(cs_glob_tree,
+                               "thermophysical_models/"
+                               "thermal_scalar/variable/formula");
+          tn_sca = _add_zone_id_test_attribute(tn_sca, z->id);
+          formula_sca = cs_tree_node_get_value_str(tn_sca);
+
+          if (formula_sca != nullptr) {
+            cs_equation_param_t  *eqp =
+              cs_equation_param_by_name(CS_THERMAL_EQNAME);
+
+            cs_meg_xdef_input_t *_input
+              = cs_meg_xdef_wrapper_add_input(CS_MEG_BOUNDARY_FUNC,
+                                              z_id,
+                                              1,
+                                              "temperature",
+                                              NULL);
+            cs_equation_add_ic_by_analytic(eqp,
+                                           z->name,
+                                           cs_meg_xdef_wrapper,
+                                           _input);
+
+          }
+          continue;
+        }
 
         cs_tree_node_t *tn_velocity
           = cs_tree_get_node(cs_glob_tree,
@@ -2811,7 +2866,7 @@ void cs_gui_initial_conditions(void)
 
         cs_field_t *c_vel = cs_field_by_name("velocity");
 
-        if (formula_uvw != NULL) {
+        if (c_vel != NULL && formula_uvw != NULL) {
           cs_real_t *ini_vals = NULL;
           BFT_MALLOC(ini_vals, c_vel->dim * n_cells, cs_real_t);
 
@@ -4123,6 +4178,83 @@ cs_gui_physical_properties(void)
       int cond_diff_id = cs_field_get_key_int(tf, k);
       if (cond_diff_id < 0)
         cs_field_set_key_int(tf, k, 0);
+    }
+  }
+
+  /* CDO HT Solver define physical properties */
+  if (cs_glob_physical_model_flag[CS_HEAT_TRANSFER] > -1) {
+    // density
+    cs_property_t  *rho = cs_property_by_name(CS_PROPERTY_MASS_DENSITY);
+    if (phys_pp->irovar == 0) {
+      cs_property_def_iso_by_value(rho, NULL, phys_pp->ro0);
+    }
+    else {
+      for (int z_id = 0; z_id < cs_volume_zone_n_zones(); z_id++) {
+        const cs_zone_t *z = cs_volume_zone_by_id(z_id);
+        if (z->type & CS_VOLUME_ZONE_PHYSICAL_PROPERTIES) {
+          cs_meg_xdef_input_t *_input
+            = cs_meg_xdef_wrapper_add_input(CS_MEG_VOLUME_FUNC,
+                                            z_id,
+                                            1,
+                                            "density",
+                                            NULL);
+          cs_property_def_by_analytic(rho,
+                                      z->name,
+                                      cs_meg_xdef_wrapper,
+                                      _input);
+
+        }
+      }
+    }
+
+    // Cp
+    cs_property_t  *cp = cs_property_by_name(CS_THERMAL_CP_NAME);
+    if (phys_pp->icp > 0) {
+      for (int z_id = 0; z_id < cs_volume_zone_n_zones(); z_id++) {
+        const cs_zone_t *z = cs_volume_zone_by_id(z_id);
+        if (z->type & CS_VOLUME_ZONE_PHYSICAL_PROPERTIES) {
+          cs_meg_xdef_input_t *_input
+            = cs_meg_xdef_wrapper_add_input(CS_MEG_VOLUME_FUNC,
+                                            z_id,
+                                            1,
+                                            "specific_heat",
+                                            NULL);
+          cs_property_def_by_analytic(cp,
+                                      z->name,
+                                      cs_meg_xdef_wrapper,
+                                      _input);
+
+        }
+      }
+    }
+    else {
+      cs_property_def_iso_by_value(cp, NULL, phys_pp->cp0);
+    }
+
+    // Lambda
+    cs_property_t  *lambda = cs_property_by_name(CS_THERMAL_LAMBDA_NAME);
+    int _lambda_choice = -1;
+    _properties_choice_id("thermal_conductivity", &_lambda_choice);
+    if (_properties_choice_id > 0) {
+      for (int z_id = 0; z_id < cs_volume_zone_n_zones(); z_id++) {
+        const cs_zone_t *z = cs_volume_zone_by_id(z_id);
+        if (z->type & CS_VOLUME_ZONE_PHYSICAL_PROPERTIES) {
+          cs_meg_xdef_input_t *_input
+            = cs_meg_xdef_wrapper_add_input(CS_MEG_VOLUME_FUNC,
+                                            z_id,
+                                            1,
+                                            "thermal_conductivity",
+                                            NULL);
+          cs_property_def_by_analytic(lambda,
+                                      z->name,
+                                      cs_meg_xdef_wrapper,
+                                      _input);
+
+        }
+      }
+    }
+    else {
+      cs_property_def_iso_by_value(lambda, NULL, phys_pp->lambda0);
     }
   }
 

python/code_saturne/base/cs_meg_to_c.py
@@ -550,7 +550,7 @@ class meg_to_c_interpreter:
         usr_blck += usr_defs
 
         usr_blck += 2*tab + 'for (cs_lnum_t e_id = 0; e_id < n_elts; e_id++) {\n'
-        usr_blck += 3*tab + '[[maybe_unused]] cs_lnum_t c_id = elt_ids[e_id];\n'
+        usr_blck += 3*tab + 'cs_lnum_t c_id = (elt_ids != nullptr) ? elt_ids[e_id] : e_id;\n'
 
         usr_blck += usr_code
 
@@ -662,7 +662,7 @@ class meg_to_c_interpreter:
 
         if need_for_loop:
             usr_blck += 2*tab + 'for (cs_lnum_t e_id = 0; e_id < n_elts; e_id++) {\n'
-            usr_blck += 3*tab + '[[maybe_unused]] cs_lnum_t b_e_id = elt_ids[e_id];\n'
+            usr_blck += 3*tab + 'cs_lnum_t b_e_id = (elt_ids != nullptr) ? elt_ids[e_id] : e_id;\n'
 
         usr_blck += usr_code
 

python/code_saturne/gui/base/BrowserView.py
@@ -838,6 +838,7 @@ class BrowserView(QWidget, Ui_BrowserForm):
         m_atmo = False
         m_gwf = False
         m_icm = False
+        m_hts = False
 
         node_pm = case.xmlGetNode('thermophysical_models')
 
@@ -882,6 +883,12 @@ class BrowserView(QWidget, Ui_BrowserForm):
                 node = node_pm.xmlGetNode('compressible_model', 'model')
                 if node and node['model'] != 'off':
                     m_cpr = True
+                    m_thermal = 1
+
+            if not m_thermal:
+                node = node_pm.xmlGetNode('hts_model', 'model')
+                if node and node['model'] != 'off':
+                    m_hts = True
                     m_thermal = 1
 
             if not m_thermal:
@@ -966,16 +973,18 @@ class BrowserView(QWidget, Ui_BrowserForm):
 
         # Thermophysical Models
 
+        print(f"{m_hts=}")
         self.setRowShow(self.tr('Calculation features'), True)
         self.setRowShow(self.tr('Main fields'), (p_module == 'neptune_cfd'))
+        self.setRowShow(self.tr('Body forces'), not m_hts)
+        self.setRowShow(self.tr('Species transport'), not m_hts)
         self.setRowShow(self.tr('Deformable mesh'), m_ale)
         self.setRowShow(self.tr('Gas combustion'), m_gas_comb)
         self.setRowShow(self.tr('Pulverized fuel combustion'), m_sf_comb)
         self.setRowShow(self.tr('Electrical models'), m_elec)
         self.setRowShow(self.tr('Atmospheric flows'), m_atmo)
         self.setRowShow(self.tr('Thermal model'), (m_thermal > -1))
-        self.setRowShow(self.tr('Body forces'), True)
-        self.setRowShow(self.tr('Turbulence models'), not m_gwf)
+        self.setRowShow(self.tr('Turbulence models'), (not (m_gwf or m_hts)))
         self.setRowShow(self.tr('Transported Variables'))
         self.setRowShow(self.tr('Groundwater flows'), m_gwf)
         self.setRowShow(self.tr('Turbomachinery'), m_tbm)

python/code_saturne/gui/case/AnalysisFeaturesForm.ui
@@ -7,7 +7,7 @@
     <x>0</x>
     <y>0</y>
     <width>1188</width>
-    <height>717</height>
+    <height>905</height>
    </rect>
   </property>
   <property name="windowTitle">
@@ -105,16 +105,21 @@
       <string>Flow Models</string>
      </property>
      <layout class="QGridLayout" name="gridLayout_2">
-      <item row="10" column="1">
-       <widget class="QComboBox" name="comboBoxHgn"/>
+      <item row="11" column="1">
+       <widget class="QComboBox" name="comboBoxNeptuneCFD"/>
       </item>
-      <item row="4" column="0">
-       <widget class="QRadioButton" name="radioButtonJouleEffect">
+      <item row="4" column="1">
+       <widget class="QComboBox" name="comboBoxJouleEffect"/>
+      </item>
+      <item row="2" column="0">
+       <widget class="QRadioButton" name="radioButtonSinglePhase">
         <property name="toolTip">
-         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Electric arcs and Joule heating.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot;
+                                     font-size:9pt;&quot;&gt;Base single-phase models.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;
+                                 </string>
         </property>
         <property name="text">
-         <string>Electric arcs</string>
+         <string>Standard Eulerian single phase</string>
         </property>
        </widget>
       </item>
@@ -135,26 +140,13 @@
         </property>
        </widget>
       </item>
-      <item row="7" column="1">
-       <widget class="QComboBox" name="comboBoxGasCombustion"/>
-      </item>
-      <item row="10" column="0">
-       <widget class="QRadioButton" name="radioButtonHgn">
-        <property name="text">
-         <string>Homogeneous Eulerian - VoF model</string>
-        </property>
-       </widget>
-      </item>
-      <item row="11" column="1">
-       <widget class="QComboBox" name="comboBoxNeptuneCFD"/>
-      </item>
-      <item row="3" column="0">
-       <widget class="QRadioButton" name="radioButtonAtmospheric">
+      <item row="11" column="0">
+       <widget class="QRadioButton" name="radioButtonNeptuneCFD">
         <property name="toolTip">
-         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Models for atmospheric flows.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Activate NEPTUNE_CFD module.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
         </property>
         <property name="text">
-         <string>Atmospheric</string>
+         <string>Eulerian multiphase (neptune_cfd)</string>
         </property>
        </widget>
       </item>
@@ -170,12 +162,6 @@
         </property>
        </widget>
       </item>
-      <item row="5" column="1">
-       <widget class="QComboBox" name="comboBoxGroundwater"/>
-      </item>
-      <item row="9" column="1">
-       <widget class="QComboBox" name="comboBoxCoalCombustion"/>
-      </item>
       <item row="6" column="0">
        <widget class="QRadioButton" name="radioButtonReactiveFlows">
         <property name="toolTip">
@@ -188,39 +174,28 @@
         </property>
        </widget>
       </item>
-      <item row="3" column="1">
-       <widget class="QComboBox" name="comboBoxAtmospheric"/>
-      </item>
-      <item row="4" column="1">
-       <widget class="QComboBox" name="comboBoxJouleEffect"/>
-      </item>
-      <item row="11" column="0">
-       <widget class="QRadioButton" name="radioButtonNeptuneCFD">
+      <item row="3" column="0">
+       <widget class="QRadioButton" name="radioButtonAtmospheric">
         <property name="toolTip">
-         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Activate NEPTUNE_CFD module.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Models for atmospheric flows.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
         </property>
         <property name="text">
-         <string>Eulerian multiphase (neptune_cfd)</string>
+         <string>Atmospheric</string>
         </property>
        </widget>
       </item>
-      <item row="1" column="1" rowspan="2">
-       <widget class="QComboBox" name="comboBoxSinglePhase"/>
+      <item row="7" column="1">
+       <widget class="QComboBox" name="comboBoxGasCombustion"/>
       </item>
-      <item row="6" column="1">
-       <widget class="QComboBox" name="comboBoxReactiveFlows"/>
-      </item>
-      <item row="2" column="0">
-       <widget class="QRadioButton" name="radioButtonSinglePhase">
-        <property name="toolTip">
-         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot;
-                                     font-size:9pt;&quot;&gt;Base single-phase models.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;
-                                 </string>
-        </property>
+      <item row="10" column="0">
+       <widget class="QRadioButton" name="radioButtonHgn">
         <property name="text">
-         <string>Standard Eulerian single phase</string>
+         <string>Homogeneous Eulerian - VoF model</string>
         </property>
        </widget>
+      </item>
+      <item row="9" column="1">
+       <widget class="QComboBox" name="comboBoxCoalCombustion"/>
       </item>
       <item row="12" column="1">
        <widget class="QCheckBox" name="checkBoxNeptuneHeatMass">
@@ -231,6 +206,41 @@
          <string>Activate gas/liquid heat and mass transfer terms</string>
         </property>
        </widget>
+      </item>
+      <item row="5" column="1">
+       <widget class="QComboBox" name="comboBoxGroundwater"/>
+      </item>
+      <item row="10" column="1">
+       <widget class="QComboBox" name="comboBoxHgn"/>
+      </item>
+      <item row="1" column="1" rowspan="2">
+       <widget class="QComboBox" name="comboBoxSinglePhase"/>
+      </item>
+      <item row="4" column="0">
+       <widget class="QRadioButton" name="radioButtonJouleEffect">
+        <property name="toolTip">
+         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Electric arcs and Joule heating.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+        </property>
+        <property name="text">
+         <string>Electric arcs</string>
+        </property>
+       </widget>
+      </item>
+      <item row="3" column="1">
+       <widget class="QComboBox" name="comboBoxAtmospheric"/>
+      </item>
+      <item row="6" column="1">
+       <widget class="QComboBox" name="comboBoxReactiveFlows"/>
+      </item>
+      <item row="13" column="0">
+       <widget class="QRadioButton" name="radioButtonHTS">
+        <property name="text">
+         <string>Solid Heat Transfer (Experimental)</string>
+        </property>
+       </widget>
+      </item>
+      <item row="13" column="1">
+       <widget class="QComboBox" name="comboBoxHTS"/>
       </item>
      </layout>
     </widget>

commit 9c62f7c4330701f65fc519f7c728bfd45f1b188f

[GUI/CHT] Fix encountered bugs :
  * Fix reading of physical properties for HTS solver
    * Constant lambda value was not read correctly when defined in GUI.
    * Error introduced in commit 23c9943899

  * Update thermal solver so that it uses the correct time step in coupling
    * Update so that the time step property reference value is updated
      accordingly to what cs_coupling_sync returns.
    * Set a default behavior where the HTSolver is a "follower" in terms
      of time step size => It uses the minimum value provided by the other
      fluid solvers. This can be undone in cs_user_parameters if needed.

  * cherry picked from commits 0e620a68d and 97f8f9f50 on master branch

src/base/cs_setup.cpp
@@ -47,6 +47,7 @@
 #include "cs_atmo_variables.h"
 #include "cs_cf_thermo.h"
 #include "cs_coal_read_data.h"
+#include "cs_coupling.h"
 #include "cs_ctwr.h"
 #include "cs_ctwr_variables.h"
 #include "cs_domain_setup.h"
@@ -85,6 +86,7 @@
 #include "cs_rad_transfer_options.h"
 #include "cs_restart.h"
 #include "cs_runaway_check.h"
+#include "cs_syr_coupling.h"
 #include "cs_thermal_model.h"
 #include "cs_turbomachinery.h"
 #include "cs_turbulence_model.h"
@@ -2663,6 +2665,26 @@ _init_user
     if (cs_glob_atmo_option->soil_cat >= 0)
       cs_f_solcat(1);
   }
+
+  /* If thermal solver, set appropriate flags for coupling if needed,
+   * We do this before cs_user_parameters to allow the user to potentially
+   * modify this behavior if needed.
+   */
+  if (cs_glob_physical_model_flag[CS_HEAT_TRANSFER] > -1) {
+    if (cs_syr_coupling_n_couplings() > 0) {
+      /* We use minimal time step size while following the one
+       * "imposed" by the fluid solvers.
+       */
+      cs_log_printf(CS_LOG_DEFAULT,
+                    _("\n"
+                      " Setting Heat Transfer Solver to follow the time step\n"
+                      " value imposed by the fluid solver. This can be undone\n"
+                      " in cs_user_parameters if needed.\n"));
+      int _flag = PLE_COUPLING_TS_MIN | PLE_COUPLING_TS_FOLLOWER;
+      cs_coupling_set_sync_flag(_flag);
+    }
+  }
+
 
   /* User functions */
   cs_f_usipsu(nmodpp);

src/cdo/cs_cdo_main.cpp
@@ -69,6 +69,7 @@
 #include "cs_solid_selection.h"
 #include "cs_solidification.h"
 #include "cs_thermal_system.h"
+#include "cs_syr_coupling.h"
 #include "cs_timer.h"
 #include "cs_timer_stats.h"
 #include "cs_volume_zone.h"
@@ -458,6 +459,12 @@ _define_current_time_step(cs_time_step_t           *ts,
   assert(cs_property_is_uniform(cs_dt_pty));
 
   const double  t_cur = ts->t_cur;
+
+  /* CHT coupling, set ref value to dt_ref which was updated previously
+   * in cs_coupling_sync, called by "cs_domain_needs_iteration"
+   */
+  if (cs_thermal_system_is_activated() && cs_syr_coupling_n_couplings() > 0)
+    cs_dt_pty->ref_value = ts->dt_ref;
 
   ts->dt[2] = ts->dt[1];
   ts->dt[1] = ts->dt[0];

src/gui/cs_gui.cpp
@@ -4216,7 +4216,7 @@ cs_gui_physical_properties(void)
 
     // Cp
     cs_property_t  *cp = cs_property_by_name(CS_THERMAL_CP_NAME);
-    if (phys_pp->icp > 0) {
+    if (phys_pp->icp == 0) {
       for (int z_id = 0; z_id < cs_volume_zone_n_zones(); z_id++) {
         const cs_zone_t *z = cs_volume_zone_by_id(z_id);
         if (z->type & CS_VOLUME_ZONE_PHYSICAL_PROPERTIES) {
@@ -4241,7 +4241,9 @@ cs_gui_physical_properties(void)
     // Lambda
     cs_property_t  *lambda = cs_property_by_name(CS_THERMAL_LAMBDA_NAME);
     int _lambda_choice = -1;
-    if (_properties_choice_id("thermal_conductivity", &_lambda_choice) > 0) {
+    _properties_choice_id("thermal_conductivity", &_lambda_choice);
+
+    if (_lambda_choice > 0) {
       for (int z_id = 0; z_id < cs_volume_zone_n_zones(); z_id++) {
         const cs_zone_t *z = cs_volume_zone_by_id(z_id);
         if (z->type & CS_VOLUME_ZONE_PHYSICAL_PROPERTIES) {
@@ -4260,6 +4262,8 @@ cs_gui_physical_properties(void)
       }
     }
     else {
+      /* Set lambda0 which has not been read before... */
+      cs_gui_properties_value("thermal_conductivity", &(phys_pp->lambda0));
       cs_property_def_iso_by_value(lambda, NULL, phys_pp->lambda0);
     }
   }