code-saturne/code_saturne

commit 23c99438996e147379b32fdd67a4044cd8f53eb0

Major commit : link CDO based heat transfer solver to GUI
  * The heat transfer solver is now usable through the GUI
  * This new feature is **experimental** and some functionnalities may
    not be available, notably for post-processing.

python/code_saturne/gui/case/TimeStepView.py
@@ -49,6 +49,7 @@ from code_saturne.model.Common import GuiParam
 from code_saturne.gui.base.QtPage import ComboModel, IntValidator, DoubleValidator, from_qvariant
 from code_saturne.gui.case.TimeStepForm import Ui_TimeStepForm
 from code_saturne.model.TimeStepModel import TimeStepModel
+from code_saturne.model.HTSModel import HTSModel
 
 #-------------------------------------------------------------------------------
 # log config
@@ -79,6 +80,8 @@ class TimeStepView(QWidget, Ui_TimeStepForm):
         self.mdl = TimeStepModel(self.case)
         self.browser = tree
 
+        self.hts_activated = HTSModel(self.case).getHTSModel() != 'off'
+
         # Check if xml contains deprecated steady algorithme
         has_deprecated_steady = False
         if str(self.mdl.getTimePassing()) == '-1':
@@ -94,6 +97,10 @@ class TimeStepView(QWidget, Ui_TimeStepForm):
         self.modelTimeOptions.addItem(self.tr("Steady (local time step)"), '2')
         if has_deprecated_steady:
             self.modelTimeOptions.addItem(self.tr("Deprecated: steady (constant relaxation coefficient)"), '-1')
+
+        self.comboBoxNTERUP.setVisible(not self.hts_activated)
+        self.labelNTERUP.setVisible(not self.hts_activated)
+        self.spinBoxNTERUP.setVisible(not self.hts_activated)
 
         self.modelNTERUP = ComboModel(self.comboBoxNTERUP,3,1)
         self.modelNTERUP.addItem(self.tr("SIMPLE"), 'simple')
@@ -284,13 +291,14 @@ class TimeStepView(QWidget, Ui_TimeStepForm):
             self.spinBoxNTERUP.setValue(value)
 
         if idtvar == -1:
-            self.labelRELXST.show()
-            self.lineEditRELXST.show()
+            self.labelRELXST.setVisible(not self.hts_activated)
+            self.lineEditRELXST.setVisible(not self.hts_activated)
             self.labelDTREF.hide()
             self.labelDTREFunit.hide()
             self.lineEditDTREF.hide()
-            relax_coef = self.mdl.getRelaxCoefficient()
-            self.lineEditRELXST.setText(str(relax_coef))
+            if not self.hts_activated:
+                relax_coef = self.mdl.getRelaxCoefficient()
+                self.lineEditRELXST.setText(str(relax_coef))
 
         else:
             self.labelRELXST.hide()
@@ -302,28 +310,43 @@ class TimeStepView(QWidget, Ui_TimeStepForm):
             self.lineEditDTREF.setText(str(dtref))
 
         if idtvar in (1, 2):
-            courant_max   = self.mdl.getOptions('max_courant_num')
-            fourier_max   = self.mdl.getOptions('max_fourier_num')
-            time_step_min_factor = self.mdl.getOptions('time_step_min_factor')
-            time_step_max_factor = self.mdl.getOptions('time_step_max_factor')
-            time_step_var = self.mdl.getOptions('time_step_var')
+            if not self.hts_activated:
+                courant_max   = self.mdl.getOptions('max_courant_num')
+                time_step_min_factor = self.mdl.getOptions('time_step_min_factor')
+                time_step_max_factor = self.mdl.getOptions('time_step_max_factor')
+                time_step_var = self.mdl.getOptions('time_step_var')
 
-            self.lineEditCOUMAX.setText(str(courant_max))
-            self.lineEditFOUMAX.setText(str(fourier_max))
-            self.lineEditCDTMIN.setText(str(time_step_min_factor))
-            self.lineEditCDTMAX.setText(str(time_step_max_factor))
-            self.lineEditVARRDT.setText(str(time_step_var))
+                self.lineEditCOUMAX.setText(str(courant_max))
+                self.lineEditCDTMIN.setText(str(time_step_min_factor))
+                self.lineEditCDTMAX.setText(str(time_step_max_factor))
+                self.lineEditVARRDT.setText(str(time_step_var))
+            elif idtvar == 1:
+                # Fourier number only used in unsteady
+                fourier_max   = self.mdl.getOptions('max_fourier_num')
+                self.lineEditFOUMAX.setText(str(fourier_max))
 
-            self.labelCOUMAX.show()
-            self.lineEditCOUMAX.show()
-            self.labelFOUMAX.show()
-            self.lineEditFOUMAX.show()
-            self.labelCDTMIN.show()
-            self.lineEditCDTMIN.show()
-            self.labelCDTMAX.show()
-            self.lineEditCDTMAX.show()
-            self.labelVARRDT.show()
-            self.lineEditVARRDT.show()
+            _hide_dt_params = self.hts_activated and idtvar == 2
+            self.groupBoxTimeStep.setVisible(not _hide_dt_params)
+            self.groupBoxStopCriterion.setVisible(not _hide_dt_params)
+
+            # If HT solver and steady state, only steady state
+            self.lineEditDTREF.setVisible(not(self.hts_activated and idtvar == 2))
+            self.labelDTREF.setVisible(not(self.hts_activated and idtvar == 2))
+            self.labelDTREFunit.setVisible(not(self.hts_activated and idtvar == 2))
+
+            self.labelFOUMAX.setVisible((not self.hts_activated) or idtvar == 1)
+            self.lineEditFOUMAX.setVisible((not self.hts_activated) or idtvar == 1)
+
+            self.labelCOUMAX.setVisible(not self.hts_activated)
+            self.lineEditCOUMAX.setVisible(not self.hts_activated)
+
+
+            self.labelCDTMIN.setVisible(not self.hts_activated)
+            self.lineEditCDTMIN.setVisible(not self.hts_activated)
+            self.labelCDTMAX.setVisible(not self.hts_activated)
+            self.lineEditCDTMAX.setVisible(not self.hts_activated)
+            self.labelVARRDT.setVisible(not self.hts_activated)
+            self.lineEditVARRDT.setVisible(not self.hts_activated)
 
         else:
             self.labelCOUMAX.hide()

python/code_saturne/base/cs_meg_to_c.py
@@ -550,7 +550,7 @@ class meg_to_c_interpreter:
         usr_blck += usr_defs
 
         usr_blck += 2*tab + 'for (cs_lnum_t e_id = 0; e_id < n_elts; e_id++) {\n'
-        usr_blck += 3*tab + '[[maybe_unused]] cs_lnum_t c_id = elt_ids[e_id];\n'
+        usr_blck += 3*tab + 'cs_lnum_t c_id = (elt_ids != nullptr) ? elt_ids[e_id] : e_id;\n'
 
         usr_blck += usr_code
 
@@ -662,7 +662,7 @@ class meg_to_c_interpreter:
 
         if need_for_loop:
             usr_blck += 2*tab + 'for (cs_lnum_t e_id = 0; e_id < n_elts; e_id++) {\n'
-            usr_blck += 3*tab + '[[maybe_unused]] cs_lnum_t b_e_id = elt_ids[e_id];\n'
+            usr_blck += 3*tab + 'cs_lnum_t b_e_id = (elt_ids != nullptr) ? elt_ids[e_id] : e_id;\n'
 
         usr_blck += usr_code
 

python/code_saturne/gui/base/BrowserView.py
@@ -838,6 +838,7 @@ class BrowserView(QWidget, Ui_BrowserForm):
         m_atmo = False
         m_gwf = False
         m_icm = False
+        m_hts = False
 
         node_pm = case.xmlGetNode('thermophysical_models')
 
@@ -882,6 +883,12 @@ class BrowserView(QWidget, Ui_BrowserForm):
                 node = node_pm.xmlGetNode('compressible_model', 'model')
                 if node and node['model'] != 'off':
                     m_cpr = True
+                    m_thermal = 1
+
+            if not m_thermal:
+                node = node_pm.xmlGetNode('hts_model', 'model')
+                if node and node['model'] != 'off':
+                    m_hts = True
                     m_thermal = 1
 
             if not m_thermal:
@@ -966,16 +973,18 @@ class BrowserView(QWidget, Ui_BrowserForm):
 
         # Thermophysical Models
 
+        print(f"{m_hts=}")
         self.setRowShow(self.tr('Calculation features'), True)
         self.setRowShow(self.tr('Main fields'), (p_module == 'neptune_cfd'))
+        self.setRowShow(self.tr('Body forces'), not m_hts)
+        self.setRowShow(self.tr('Species transport'), not m_hts)
         self.setRowShow(self.tr('Deformable mesh'), m_ale)
         self.setRowShow(self.tr('Gas combustion'), m_gas_comb)
         self.setRowShow(self.tr('Pulverized fuel combustion'), m_sf_comb)
         self.setRowShow(self.tr('Electrical models'), m_elec)
         self.setRowShow(self.tr('Atmospheric flows'), m_atmo)
         self.setRowShow(self.tr('Thermal model'), (m_thermal > -1))
-        self.setRowShow(self.tr('Body forces'), True)
-        self.setRowShow(self.tr('Turbulence models'), not m_gwf)
+        self.setRowShow(self.tr('Turbulence models'), (not (m_gwf or m_hts)))
         self.setRowShow(self.tr('Transported Variables'))
         self.setRowShow(self.tr('Groundwater flows'), m_gwf)
         self.setRowShow(self.tr('Turbomachinery'), m_tbm)

python/code_saturne/gui/case/AnalysisFeaturesForm.ui
@@ -7,7 +7,7 @@
     <x>0</x>
     <y>0</y>
     <width>1188</width>
-    <height>717</height>
+    <height>905</height>
    </rect>
   </property>
   <property name="windowTitle">
@@ -105,16 +105,21 @@
       <string>Flow Models</string>
      </property>
      <layout class="QGridLayout" name="gridLayout_2">
-      <item row="10" column="1">
-       <widget class="QComboBox" name="comboBoxHgn"/>
+      <item row="11" column="1">
+       <widget class="QComboBox" name="comboBoxNeptuneCFD"/>
       </item>
-      <item row="4" column="0">
-       <widget class="QRadioButton" name="radioButtonJouleEffect">
+      <item row="4" column="1">
+       <widget class="QComboBox" name="comboBoxJouleEffect"/>
+      </item>
+      <item row="2" column="0">
+       <widget class="QRadioButton" name="radioButtonSinglePhase">
         <property name="toolTip">
-         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Electric arcs and Joule heating.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot;
+                                     font-size:9pt;&quot;&gt;Base single-phase models.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;
+                                 </string>
         </property>
         <property name="text">
-         <string>Electric arcs</string>
+         <string>Standard Eulerian single phase</string>
         </property>
        </widget>
       </item>
@@ -135,26 +140,13 @@
         </property>
        </widget>
       </item>
-      <item row="7" column="1">
-       <widget class="QComboBox" name="comboBoxGasCombustion"/>
-      </item>
-      <item row="10" column="0">
-       <widget class="QRadioButton" name="radioButtonHgn">
-        <property name="text">
-         <string>Homogeneous Eulerian - VoF model</string>
-        </property>
-       </widget>
-      </item>
-      <item row="11" column="1">
-       <widget class="QComboBox" name="comboBoxNeptuneCFD"/>
-      </item>
-      <item row="3" column="0">
-       <widget class="QRadioButton" name="radioButtonAtmospheric">
+      <item row="11" column="0">
+       <widget class="QRadioButton" name="radioButtonNeptuneCFD">
         <property name="toolTip">
-         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Models for atmospheric flows.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Activate NEPTUNE_CFD module.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
         </property>
         <property name="text">
-         <string>Atmospheric</string>
+         <string>Eulerian multiphase (neptune_cfd)</string>
         </property>
        </widget>
       </item>
@@ -170,12 +162,6 @@
         </property>
        </widget>
       </item>
-      <item row="5" column="1">
-       <widget class="QComboBox" name="comboBoxGroundwater"/>
-      </item>
-      <item row="9" column="1">
-       <widget class="QComboBox" name="comboBoxCoalCombustion"/>
-      </item>
       <item row="6" column="0">
        <widget class="QRadioButton" name="radioButtonReactiveFlows">
         <property name="toolTip">
@@ -188,39 +174,28 @@
         </property>
        </widget>
       </item>
-      <item row="3" column="1">
-       <widget class="QComboBox" name="comboBoxAtmospheric"/>
-      </item>
-      <item row="4" column="1">
-       <widget class="QComboBox" name="comboBoxJouleEffect"/>
-      </item>
-      <item row="11" column="0">
-       <widget class="QRadioButton" name="radioButtonNeptuneCFD">
+      <item row="3" column="0">
+       <widget class="QRadioButton" name="radioButtonAtmospheric">
         <property name="toolTip">
-         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Activate NEPTUNE_CFD module.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Models for atmospheric flows.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
         </property>
         <property name="text">
-         <string>Eulerian multiphase (neptune_cfd)</string>
+         <string>Atmospheric</string>
         </property>
        </widget>
       </item>
-      <item row="1" column="1" rowspan="2">
-       <widget class="QComboBox" name="comboBoxSinglePhase"/>
+      <item row="7" column="1">
+       <widget class="QComboBox" name="comboBoxGasCombustion"/>
       </item>
-      <item row="6" column="1">
-       <widget class="QComboBox" name="comboBoxReactiveFlows"/>
-      </item>
-      <item row="2" column="0">
-       <widget class="QRadioButton" name="radioButtonSinglePhase">
-        <property name="toolTip">
-         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot;
-                                     font-size:9pt;&quot;&gt;Base single-phase models.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;
-                                 </string>
-        </property>
+      <item row="10" column="0">
+       <widget class="QRadioButton" name="radioButtonHgn">
         <property name="text">
-         <string>Standard Eulerian single phase</string>
+         <string>Homogeneous Eulerian - VoF model</string>
         </property>
        </widget>
+      </item>
+      <item row="9" column="1">
+       <widget class="QComboBox" name="comboBoxCoalCombustion"/>
       </item>
       <item row="12" column="1">
        <widget class="QCheckBox" name="checkBoxNeptuneHeatMass">
@@ -231,6 +206,41 @@
          <string>Activate gas/liquid heat and mass transfer terms</string>
         </property>
        </widget>
+      </item>
+      <item row="5" column="1">
+       <widget class="QComboBox" name="comboBoxGroundwater"/>
+      </item>
+      <item row="10" column="1">
+       <widget class="QComboBox" name="comboBoxHgn"/>
+      </item>
+      <item row="1" column="1" rowspan="2">
+       <widget class="QComboBox" name="comboBoxSinglePhase"/>
+      </item>
+      <item row="4" column="0">
+       <widget class="QRadioButton" name="radioButtonJouleEffect">
+        <property name="toolTip">
+         <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Electric arcs and Joule heating.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+        </property>
+        <property name="text">
+         <string>Electric arcs</string>
+        </property>
+       </widget>
+      </item>
+      <item row="3" column="1">
+       <widget class="QComboBox" name="comboBoxAtmospheric"/>
+      </item>
+      <item row="6" column="1">
+       <widget class="QComboBox" name="comboBoxReactiveFlows"/>
+      </item>
+      <item row="13" column="0">
+       <widget class="QRadioButton" name="radioButtonHTS">
+        <property name="text">
+         <string>Solid Heat Transfer (Experimental)</string>
+        </property>
+       </widget>
+      </item>
+      <item row="13" column="1">
+       <widget class="QComboBox" name="comboBoxHTS"/>
       </item>
      </layout>
     </widget>

python/code_saturne/gui/case/AnalysisFeaturesView.py
@@ -66,11 +66,14 @@ from code_saturne.model.InterfacialAreaModel import InterfacialAreaModel
 from code_saturne.model.NeptuneWallTransferModel import NeptuneWallTransferModel
 from code_saturne.model.InterfacialEnthalpyModel import InterfacialEnthalpyModel
 from code_saturne.model.HgnModel import HgnModel
+from code_saturne.model.HTSModel import HTSModel
 
 from code_saturne.model.LagrangianModel import LagrangianModel
 from code_saturne.model.TurboMachineryModel import TurboMachineryModel
 from code_saturne.model.MobileMeshModel import MobileMeshModel
 from code_saturne.model.FansModel import FansStatus
+
+from code_saturne.model.TurbulenceModel import TurbulenceModel
 
 # -------------------------------------------------------------------------------
 # log config
@@ -124,6 +127,8 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
 
         self.modelNeptuneCFD     = QtPage.ComboModel(self.comboBoxNeptuneCFD,2,1)
 
+        self.modelHTS            = QtPage.ComboModel(self.comboBoxHTS,1,1)
+
         self.modelSinglePhase.addItem(self.tr("Incompressible"), 'off')
         self.modelSinglePhase.addItem(self.tr("Compressible, perfect gas with constant gamma"),
                                       'constant_gamma')
@@ -173,6 +178,9 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
         self.modelNeptuneCFD.addItem(self.tr("Particle-laden flow"), "particles_flow")
         self.modelNeptuneCFD.addItem(self.tr("Multiregime liquid/gas flow"), "multiregime")
 
+        # HTS combobox
+        self.modelHTS.addItem(self.tr("Heat conduction"), "hts_conduction")
+
         # Lagrangian combobox
 
         self.modelLagrangian = QtPage.ComboModel(self.comboBoxLagrangian, 4, 1)
@@ -203,6 +211,7 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
         self.comboBoxSinglePhase.activated[str].connect(self.slotSinglePhase)
         self.comboBoxGroundwater.activated[str].connect(self.slotGroundwater)
         self.comboBoxHgn.activated[str].connect(self.slotHgn)
+        self.comboBoxHTS.activated[str].connect(self.slotHTS)
         self.comboBoxNeptuneCFD.currentTextChanged[str].connect(self.slotNeptuneCFD)
         self.checkBoxNeptuneHeatMass.stateChanged.connect(self.slotNeptuneHeatMass)
         self.checkBoxALE.stateChanged.connect(self.slotALE)
@@ -213,7 +222,7 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
 
         for ind in ['SinglePhase', 'Atmospheric',
                     'JouleEffect', 'Groundwater', 'ReactiveFlows',
-                    'Hgn', 'NeptuneCFD']:
+                    'Hgn', 'NeptuneCFD', 'HTS']:
             eval('self.radioButton'+ind+'.toggled.connect(self.slotRadioButton)')
 
         # Initializations based on code
@@ -279,6 +288,7 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
         self.comboBoxJouleEffect.hide()
         self.comboBoxGroundwater.hide()
         self.comboBoxHgn.hide()
+        self.comboBoxHTS.hide()
         self.comboBoxNeptuneCFD.hide()
 
         self.checkBoxNeptuneHeatMass.hide()
@@ -300,7 +310,8 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
                         'Hgn',
                         'NeptuneCFD',
                         'Lagrangian',
-                        'TurboMachinery']:
+                        'TurboMachinery',
+                        'HTS']:
             log.debug("__stringModelFromCombo() Incorrect name for QComboBox name")
             string = ""
         else:
@@ -344,6 +355,7 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
         self.comp  = CompressibleModel(self.case)
         self.darc  = GroundwaterModel(self.case)
         self.hgn   = HgnModel(self.case)
+        self.hts   = HTSModel(self.case)
 
         from code_saturne.model.TimeStepModel import TimeStepModel
 
@@ -354,6 +366,7 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
         gas = self.gas.getGasCombustionModel()
         compressible = self.comp.getCompressibleModel()
         homogeneous = self.hgn.getHgnModel()
+        hts = self.hts.getHTSModel()
 
         # Set combobox values
 
@@ -387,6 +400,9 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
         elif homogeneous != 'off':
             self.modelHgn.setItem(str_model=homogeneous)
 
+        elif hts != 'off':
+            self.modelHTS.setItem(str_model=hts)
+
         else:
             self.modelSinglePhase.setItem(str_model=compressible)
 
@@ -399,6 +415,7 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
         ReactiveFlows = self.gas.getGasCombustionModel()  != 'off' \
                         or self.pcoal.getCoalCombustionModel() != 'off'
         Hgn = homogeneous != 'off'
+        HTS = hts != 'off'
 
         self.checkPrev = 'SinglePhase'
         combo = None
@@ -407,7 +424,8 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
                     'JouleEffect',
                     'Groundwater',
                     'ReactiveFlows',
-                    'Hgn']:
+                    'Hgn',
+                    'HTS']:
 
             radioButton = eval('self.radioButton'+ind)
             model_on = eval(ind)
@@ -580,7 +598,8 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
                     'JouleEffect',
                     'Groundwater',
                     'ReactiveFlows',
-                    'NeptuneCFD']:
+                    'NeptuneCFD',
+                    'HTS']:
 
             radioButton = eval('self.radioButton'+ind)
             combo = eval('self.comboBox'+ind)
@@ -663,6 +682,14 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
                 self.checkBoxPther.hide()
 
                 self.modelLagrangian.enableItem(str_model='two_way')
+            if self.checkPrev == 'HTS':
+                self.hts.setHTSModel('off')
+                self.labelLagrangian.show()
+                self.comboBoxLagrangian.show()
+                self.labelTurboMachinery.show()
+                self.comboBoxTurboMachinery.show()
+                self.checkBoxALE.show()
+                self.checkBoxFans.show()
 
             if checkCur == 'ReactiveFlows':
                 if self.pcoal.getCoalCombustionModel() != 'off':
@@ -673,6 +700,14 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
                     model = self.modelGasCombustion
                 combo.show()
                 model_expr = 'self.slotReactiveFlows(model)'
+
+            if checkCur == 'HTS':
+                self.labelLagrangian.hide()
+                self.comboBoxLagrangian.hide()
+                self.labelTurboMachinery.hide()
+                self.comboBoxTurboMachinery.hide()
+                self.checkBoxALE.hide()
+                self.checkBoxFans.hide()
 
             eval(model_expr)
 
@@ -875,6 +910,20 @@ class AnalysisFeaturesView(QWidget, Ui_AnalysisFeaturesForm):
 
         model = self.__stringModelFromCombo('Hgn')
         self.hgn.setHgnModel(model)
+
+        self.browser.configureTree(self.case)
+
+    @pyqtSlot(str)
+    def slotHTS(self, text):
+        """
+        Called when the comboBoxHTS changed
+        """
+        self.comboBoxHTS.show()
+
+        model = self.__stringModelFromCombo('HTS')
+        self.hts.setHTSModel(model)
+
+        TurbulenceModel(self.case).setTurbulenceModel('off')
 
         self.browser.configureTree(self.case)
 

python/code_saturne/gui/case/FluidCharacteristicsView.py
@@ -56,6 +56,7 @@ from code_saturne.model.FluidCharacteristicsModel import FluidCharacteristicsMod
 from code_saturne.model.DefineUserScalarsModel import DefineUserScalarsModel
 from code_saturne.model.ThermalScalarModel import ThermalScalarModel
 from code_saturne.model.GroundwaterModel import GroundwaterModel
+from code_saturne.model.HTSModel import HTSModel
 from code_saturne.gui.case.QMegEditorView import QMegEditorView
 from code_saturne.model.NotebookModel import NotebookModel
 from code_saturne.model.InternalCouplingModel import InternalCouplingModel
@@ -223,6 +224,8 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
         self.m_th = ThermalScalarModel(self.case)
         s = self.m_th.getThermalScalarName()
         tsm = self.mdl.tsm
+
+        self.m_hts = HTSModel(self.case)
 
         # Particular Widget init. taking into account chosen fluid model
         mdl_atmo, mdl_joule, mdl_thermal, mdl_gas, mdl_coal, mdl_comp, mdl_hgn=\
@@ -691,6 +694,10 @@ thermal_conductivity = 6.2e-5 * temperature + 8.1e-3;
                     __combo.setEnabled(False)
                     __button.setEnabled(False)
                     __button.hide()
+
+            # HTS
+            if self.m_hts.getHTSModel() != 'off':
+                self.groupBoxMu.setVisible(False)
 
             # Compressible Flows
             if mdl_comp != 'off':

python/code_saturne/gui/case/ThermalView.py
@@ -60,6 +60,7 @@ from code_saturne.gui.case.ThermalRadiationAdvancedDialogForm import Ui_ThermalR
 from code_saturne.model.ThermalRadiationModel import ThermalRadiationModel
 from code_saturne.model.ThermalParticlesRadiationModel import ThermalParticlesRadiationModel
 from code_saturne.model.MainFieldsModel import MainFieldsModel
+from code_saturne.model.HTSModel import HTSModel
 
 #-------------------------------------------------------------------------------
 # log config
@@ -252,6 +253,10 @@ class ThermalView(QWidget, Ui_ThermalForm):
                 self.modelThermal.delItem(5)
                 self.modelThermal.delItem(4)
 
+            if HTSModel(self.case).getHTSModel() != 'off':
+                self.comboBoxThermal.setEnabled(False)
+                self.thermal.setThermalModel('temperature_celsius')
+
         # Select the thermal scalar model
 
         model = self.thermal.getThermalScalarModel()
@@ -319,7 +324,8 @@ class ThermalView(QWidget, Ui_ThermalForm):
         """
         Update for radiation model
         """
-        if self.case.module_name() == "code_saturne" and model == 'off':
+        if self.case.module_name() == "code_saturne" and model == 'off' \
+                or HTSModel(self.case).getHTSModel() != 'off':
             self.ThermalRadiationGroupBox.hide()
             return
 

python/code_saturne/gui/case/TimeStepForm.ui
@@ -15,7 +15,7 @@
   </property>
   <layout class="QGridLayout">
    <item row="3" column="0">
-    <widget class="QGroupBox" name="groupBox">
+    <widget class="QGroupBox" name="groupBoxStopCriterion">
      <layout class="QHBoxLayout" name="horizontalLayout_3">
       <item>
        <widget class="QLabel" name="label_4">
@@ -353,7 +353,7 @@
     </widget>
    </item>
    <item row="0" column="0">
-    <widget class="QGroupBox" name="groupBox">
+    <widget class="QGroupBox" name="groupBoxTimeStepMode">
      <property name="toolTip">
       <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:9pt;&quot;&gt;Define the time step options.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
      </property>

commit 237a5ca59cac125944ab455ac9a00e8551a61f85

GUI: fix display of Fourier number, broken by 23c99438 (2024-12-11).
This fixes GitLab issue #623.

python/code_saturne/gui/case/TimeStepView.py
@@ -320,10 +320,9 @@ class TimeStepView(QWidget, Ui_TimeStepForm):
                 self.lineEditCDTMIN.setText(str(time_step_min_factor))
                 self.lineEditCDTMAX.setText(str(time_step_max_factor))
                 self.lineEditVARRDT.setText(str(time_step_var))
-            elif idtvar == 1:
-                # Fourier number only used in unsteady
-                fourier_max   = self.mdl.getOptions('max_fourier_num')
-                self.lineEditFOUMAX.setText(str(fourier_max))
+
+            fourier_max   = self.mdl.getOptions('max_fourier_num')
+            self.lineEditFOUMAX.setText(str(fourier_max))
 
             _hide_dt_params = self.hts_activated and idtvar == 2
             self.groupBoxTimeStep.setVisible(not _hide_dt_params)