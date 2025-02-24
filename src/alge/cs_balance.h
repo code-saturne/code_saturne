code-saturne/code_saturne

commit 3dea508790902fabf46f580fc6efdee820a9b6f9

Major fix in mass conservation with beta limiter.
This mainly impacts the VoF model (beta limiter default for void fraction).
Now the fluxes are computed on the fly when applying the scheme.

Edits by Y. Fournier to add templating and avoid extra tests when
not using the beta limiter.

src/alge/cs_balance.h
@@ -56,7 +56,7 @@ void
 cs_balance_initialize(void);
 
 /*----------------------------------------------------------------------------*/
-/*!
+/*
  * \brief Wrapper to the function which adds the explicit part of the
  * convection/diffusion terms of a transport equation of
  * a scalar field \f$ \varia \f$.
@@ -144,6 +144,104 @@ cs_balance_scalar(int                         idtvar,
                   int                         icvflb,
                   const int                   icvfli[],
                   cs_real_t                   smbrp[]);
+
+END_C_DECLS
+
+/*----------------------------------------------------------------------------*/
+/*
+ * \brief Wrapper to the function which adds the explicit part of the
+ * convection/diffusion terms of a transport equation of
+ * a scalar field \f$ \varia \f$.
+ *
+ * More precisely, the right hand side \f$ Rhs \f$ is updated as
+ * follows:
+ * \f[
+ * Rhs = Rhs - \sum_{\fij \in \Facei{\celli}}      \left(
+ *        \dot{m}_\ij \left( \varia_\fij - \varia_\celli \right)
+ *      - \mu_\fij \gradv_\fij \varia \cdot \vect{S}_\ij  \right)
+ * \f]
+ *
+ * Warning:
+ * - \f$ Rhs \f$ has already been initialized
+ *   before calling cs_balance_scalar!
+ * - mind the minus sign
+ *
+ * Options for the convective scheme:
+ * - blencp = 0: upwind scheme for the advection
+ * - blencp = 1: no upwind scheme except in the slope test
+ * - ischcp = 0: SOLU
+ * - ischcp = 1: centered
+ * - ischcp = 2: SOLU with upwind gradient reconstruction
+ * - ischcp = 3: blending SOLU centered
+ * - ischcp = 4: NVD-TVD
+ * - imucpp = 0: do not multiply the convective part by \f$ C_p \f$
+ * - imucpp = 1: multiply the convective part by \f$ C_p \f$
+ *
+ * \param[in]     idtvar        indicator of the temporal scheme
+ * \param[in]     f_id          field id (or -1)
+ * \param[in]     imucpp        indicator
+ *                               - 0 do not multiply the convectiv term by Cp
+ *                               - 1 do multiply the convectiv term by Cp
+ * \param[in]     imasac        take mass accumulation into account?
+ * \param[in]     inc           indicator
+ *                               - 0 when solving an increment
+ *                               - 1 otherwise
+ * \param[in]     eqp           pointer to a cs_equation_param_t structure which
+ *                              contains variable calculation options
+ * \param[in]     pvar          solved variable (current time step)
+ *                              may be NULL if pvara != NULL
+ * \param[in]     pvara         solved variable (previous time step)
+ *                              may be NULL if pvar != NULL
+ * \param[in]     bc_coeffs     boundary condition structure for the variable
+ * \param[in]     i_massflux    mass flux at interior faces
+ * \param[in]     b_massflux    mass flux at boundary faces
+ * \param[in]     i_visc        \f$ \mu_\fij \dfrac{S_\fij}{\ipf \jpf} \f$
+ *                               at interior faces for the r.h.s.
+ * \param[in]     b_visc        \f$ \mu_\fib \dfrac{S_\fib}{\ipf \centf} \f$
+ *                               at boundary faces for the r.h.s.
+ * \param[in]     viscel        symmetric cell tensor \f$ \tens{\mu}_\celli \f$
+ * \param[in]     xcpp          array of specific heat (Cp)
+ * \param[in]     weighf        internal face weight between cells i j in case
+ *                               of tensor diffusion
+ * \param[in]     weighb        boundary face weight for cells i in case
+ *                               of tensor diffusion
+ * \param[in]     icvflb        global indicator of boundary convection flux
+ *                               - 0 upwind scheme at all boundary faces
+ *                               - 1 imposed flux at some boundary faces
+ * \param[in]     icvfli        boundary face indicator array of convection flux
+ *                               - 0 upwind scheme
+ *                               - 1 imposed flux
+ * \param[in,out] smbrp         right hand side \f$ \vect{Rhs} \f$
+ * \param[in,out] i_flux        interior flux (or nullptr)
+ * \param[in,out] b_flux        boundary flux (or nullptr)
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_balance_scalar(int                         idtvar,
+                  int                         f_id,
+                  int                         imucpp,
+                  int                         imasac,
+                  int                         inc,
+                  cs_equation_param_t        *eqp,
+                  cs_real_t                   pvar[],
+                  const cs_real_t             pvara[],
+                  const cs_field_bc_coeffs_t *bc_coeffs,
+                  const cs_real_t             i_massflux[],
+                  const cs_real_t             b_massflux[],
+                  const cs_real_t             i_visc[],
+                  const cs_real_t             b_visc[],
+                  cs_real_6_t                 viscel[],
+                  const cs_real_t             xcpp[],
+                  const cs_real_2_t           weighf[],
+                  const cs_real_t             weighb[],
+                  int                         icvflb,
+                  const int                   icvfli[],
+                  cs_real_t                   smbrp[],
+                  cs_real_2_t                 i_flux[],
+                  cs_real_t                   b_flux[]);
+
+BEGIN_C_DECLS
 
 /*----------------------------------------------------------------------------*/
 /*!

src/alge/cs_balance.cpp
@@ -214,6 +214,119 @@ cs_balance_scalar(int                         idtvar,
                   const int                   icvfli[],
                   cs_real_t                   smbrp[])
 {
+  cs_balance_scalar(idtvar,
+                    f_id,
+                    imucpp, imasac,
+                    inc,
+                    eqp,
+                    pvar, pvara,
+                    bc_coeffs,
+                    i_massflux, b_massflux,
+                    i_visc, b_visc,
+                    viscel,
+                    xcpp,
+                    weighf, weighb,
+                    icvflb, icvfli,
+                    smbrp,
+                    nullptr, nullptr);
+}
+
+END_C_DECLS
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Wrapper to the function which adds the explicit part of the
+ * convection/diffusion terms of a transport equation of
+ * a scalar field \f$ \varia \f$.
+ *
+ * More precisely, the right hand side \f$ Rhs \f$ is updated as
+ * follows:
+ * \f[
+ * Rhs = Rhs - \sum_{\fij \in \Facei{\celli}}      \left(
+ *        \dot{m}_\ij \left( \varia_\fij - \varia_\celli \right)
+ *      - \mu_\fij \gradv_\fij \varia \cdot \vect{S}_\ij  \right)
+ * \f]
+ *
+ * Warning:
+ * - \f$ Rhs \f$ has already been initialized
+ *   before calling cs_balance_scalar!
+ * - mind the minus sign
+ *
+ * Options for the convective scheme:
+ * - blencp = 0: upwind scheme for the advection
+ * - blencp = 1: no upwind scheme except in the slope test
+ * - ischcp = 0: SOLU
+ * - ischcp = 1: centered
+ * - ischcp = 2: SOLU with upwind gradient reconstruction
+ * - ischcp = 3: blending SOLU centered
+ * - ischcp = 4: NVD-TVD
+ * - imucpp = 0: do not multiply the convective part by \f$ C_p \f$
+ * - imucpp = 1: multiply the convective part by \f$ C_p \f$
+ *
+ * \param[in]     idtvar        indicator of the temporal scheme
+ * \param[in]     f_id          field id (or -1)
+ * \param[in]     imucpp        indicator
+ *                               - 0 do not multiply the convectiv term by Cp
+ *                               - 1 do multiply the convectiv term by Cp
+ * \param[in]     imasac        take mass accumulation into account?
+ * \param[in]     inc           indicator
+ *                               - 0 when solving an increment
+ *                               - 1 otherwise
+ * \param[in]     eqp           pointer to a cs_equation_param_t structure which
+ *                              contains variable calculation options
+ * \param[in]     pvar          solved variable (current time step)
+ *                              may be nullptr if pvara != nullptr
+ * \param[in]     pvara         solved variable (previous time step)
+ *                              may be nullptr if pvar != nullptr
+ * \param[in]     bc_coeffs     boundary condition structure for the variable
+ * \param[in]     i_massflux    mass flux at interior faces
+ * \param[in]     b_massflux    mass flux at boundary faces
+ * \param[in]     i_visc        \f$ \mu_\fij \dfrac{S_\fij}{\ipf \jpf} \f$
+ *                               at interior faces for the r.h.s.
+ * \param[in]     b_visc        \f$ \mu_\fib \dfrac{S_\fib}{\ipf \centf} \f$
+ *                               at boundary faces for the r.h.s.
+ * \param[in]     viscel        symmetric cell tensor \f$ \tens{\mu}_\celli \f$
+ * \param[in]     xcpp          array of specific heat (Cp)
+ * \param[in]     weighf        internal face weight between cells i j in case
+ *                               of tensor diffusion
+ * \param[in]     weighb        boundary face weight for cells i in case
+ *                               of tensor diffusion
+ * \param[in]     icvflb        global indicator of boundary convection flux
+ *                               - 0 upwind scheme at all boundary faces
+ *                               - 1 imposed flux at some boundary faces
+ * \param[in]     icvfli        boundary face indicator array of convection flux
+ *                               - 0 upwind scheme
+ *                               - 1 imposed flux
+ * \param[in,out] smbrp         right hand side \f$ \vect{Rhs} \f$
+ * \param[in,out] i_flux        interior flux (or nullptr)
+ * \param[in,out] b_flux        boundary flux (or nullptr)
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_balance_scalar(int                         idtvar,
+                  int                         f_id,
+                  int                         imucpp,
+                  int                         imasac,
+                  int                         inc,
+                  cs_equation_param_t        *eqp,
+                  cs_real_t                   pvar[],
+                  const cs_real_t             pvara[],
+                  const cs_field_bc_coeffs_t *bc_coeffs,
+                  const cs_real_t             i_massflux[],
+                  const cs_real_t             b_massflux[],
+                  const cs_real_t             i_visc[],
+                  const cs_real_t             b_visc[],
+                  cs_real_6_t                 viscel[],
+                  const cs_real_t             xcpp[],
+                  const cs_real_2_t           weighf[],
+                  const cs_real_t             weighb[],
+                  int                         icvflb,
+                  const int                   icvfli[],
+                  cs_real_t                   smbrp[],
+                  cs_real_2_t                 i_flux[],
+                  cs_real_t                   b_flux[])
+{
   cs_timer_t t0 = cs_timer_time();
 
   /* Local variables */
@@ -272,8 +385,11 @@ cs_balance_scalar(int                         idtvar,
                                      b_massflux,
                                      i_visc,
                                      b_visc,
-                                     smbrp);
-    } else {
+                                     smbrp,
+                                     i_flux,
+                                     b_flux);
+    }
+    else {
       /* The convective part is multiplied by Cp for the temperature */
       cs_convection_diffusion_thermal(idtvar,
                                       f_id,
@@ -310,7 +426,9 @@ cs_balance_scalar(int                         idtvar,
                                      b_massflux,
                                      i_visc,
                                      b_visc,
-                                     smbrp);
+                                     smbrp,
+                                     i_flux,
+                                     b_flux);
     }
     /* The convective part is multiplied by Cp for the temperature */
     else if (imucpp == 1 && iconvp == 1) {
@@ -353,6 +471,8 @@ cs_balance_scalar(int                         idtvar,
   if (_balance_stat_id > -1)
     cs_timer_stats_add_diff(_balance_stat_id, &t0, &t1);
 }
+
+BEGIN_C_DECLS
 
 /*----------------------------------------------------------------------------*/
 /*!

src/alge/cs_convection_diffusion.cpp
@@ -2917,10 +2917,12 @@ _face_convection_scalar_steady(const cs_field_t           *f,
  *                               at border faces for the r.h.s.
  * \param[in]     xcpp          array of specific heat (\f$ C_p \f$), or nullptr
  * \param[in,out] rhs           right hand side \f$ \vect{Rhs} \f$
+ * \param[in,out] i_flux        interior flux (or nullptr)
+ * \param[in,out] b_flux        boundary flux (or nullptr)
  */
 /*----------------------------------------------------------------------------*/
 
-template <bool is_thermal>
+template <bool is_thermal, bool have_interior_flux>
 static void
 _convection_diffusion_scalar_unsteady
   (const cs_field_t           *f,
@@ -2937,7 +2939,9 @@ _convection_diffusion_scalar_unsteady
    const cs_real_t             i_visc[],
    const cs_real_t             b_visc[],
    const cs_real_t             xcpp[],
-   cs_real_t         *restrict rhs)
+   cs_real_t         *restrict rhs,
+   cs_real_2_t       *restrict i_flux,
+   cs_real_t         *restrict b_flux)
 {
   const cs_real_t *coefap = bc_coeffs->a;
   const cs_real_t *coefbp = bc_coeffs->b;
@@ -3313,6 +3317,12 @@ _convection_diffusion_scalar_unsteady
 
         fluxj += cpj*(  thetap * (flui*_pvar[ii] + fluj*_pvar[jj])
                       - imasac *  _i_massflux*_pvar[jj]);
+
+        /* Fluxes without mass accumulation */
+        if (have_interior_flux) {
+          i_flux[face_id][0] +=  cpi*thetap*(flui*_pvar[ii] + fluj*_pvar[jj]);
+          i_flux[face_id][1] +=  cpj*thetap*(flui*_pvar[ii] + fluj*_pvar[jj]);
+        }
       }
 
       /* Diffusive flux */
@@ -3320,6 +3330,11 @@ _convection_diffusion_scalar_unsteady
       cs_real_t diff_contrib = idiffp*thetap*i_visc[face_id]*(pip - pjp);
       fluxi += diff_contrib;
       fluxj += diff_contrib;
+      /* Fluxes if needed */
+      if (have_interior_flux) {
+        i_flux[face_id][0] += diff_contrib;
+        i_flux[face_id][1] += diff_contrib;
+      }
 
       if (ii < n_cells)
         cs_dispatch_sum(&rhs[ii], -fluxi, i_sum_type);
@@ -3530,6 +3545,11 @@ _convection_diffusion_scalar_unsteady
         fluxj += cpj*(  thetap*(flui*pif + fluj*pjf)
                       - imasac*_i_massflux*_pvar[jj]);
 
+        /* Fluxes without mass accumulation if needed */
+        if (have_interior_flux) {
+          i_flux[face_id][0] +=  cpi*thetap*(flui*pif + fluj*pjf);
+          i_flux[face_id][1] +=  cpj*thetap*(flui*pif + fluj*pjf);
+        }
       }
 
       // Diffusive flux (no relaxation)
@@ -3537,6 +3557,12 @@ _convection_diffusion_scalar_unsteady
       cs_real_t diff_contrib = idiffp*thetap*i_visc[face_id]*(pip - pjp);
       fluxi += diff_contrib;
       fluxj += diff_contrib;
+
+      /* Fluxes if needed */
+      if (have_interior_flux) {
+        i_flux[face_id][0] += diff_contrib;
+        i_flux[face_id][1] += diff_contrib;
+      }
 
       if (ii < n_cells)
         cs_dispatch_sum(&rhs[ii], -fluxi, i_sum_type);
@@ -3694,6 +3720,13 @@ _convection_diffusion_scalar_unsteady
 
         fluxj += cpj*(  thetap*(flui*pif + fluj*pjf)
                       - imasac*_i_massflux*_pvar[jj]);
+
+        /* Fluxes without mass accumulation if needed */
+        if (have_interior_flux) {
+          i_flux[face_id][0] +=  cpi*thetap*(flui*pif + fluj*pjf);
+          i_flux[face_id][1] +=  cpj*thetap*(flui*pif + fluj*pjf);
+        }
+
       }
 
       // Diffusive flux (no relaxation)
@@ -3701,6 +3734,12 @@ _convection_diffusion_scalar_unsteady
       cs_real_t diff_contrib = idiffp*thetap*i_visc[face_id]*(pip - pjp);
       fluxi += diff_contrib;
       fluxj += diff_contrib;
+
+      /* Fluxes if needed */
+      if (have_interior_flux) {
+        i_flux[face_id][0] += diff_contrib;
+        i_flux[face_id][1] += diff_contrib;
+      }
 
       if (upwind_switch) {
         /* in parallel, face will be counted by one and only one rank */
@@ -3808,6 +3847,36 @@ _convection_diffusion_scalar_unsteady
 
       cs_dispatch_sum(&rhs[ii], -fluxi, b_sum_type);
 
+      /* Fluxes without mass accumulation if needed */
+      if (b_flux != nullptr) {
+        fluxi = 0.;
+
+        cs_b_upwind_flux(iconvp,
+                         thetap,
+                         0, //imasac,
+                         inc,
+                         bc_type[face_id],
+                         _pvar[ii],
+                         _pvar[ii], /* no relaxation */
+                         pip,
+                         coefap[face_id],
+                         coefbp[face_id],
+                         b_massflux[face_id],
+                         cpi,
+                         &fluxi);
+
+        cs_b_diff_flux(idiffp,
+                       thetap,
+                       inc,
+                       pip,
+                       cofafp[face_id],
+                       cofbfp[face_id],
+                       b_visc[face_id],
+                       &fluxi);
+
+        b_flux[face_id] += fluxi;
+      }
+
     });
 
     /* The scalar is internally coupled and an implicit contribution
@@ -3888,6 +3957,9 @@ _convection_diffusion_scalar_unsteady
                                 heq,
                                 &fluxi);
 
+        if (b_flux != nullptr)
+          b_flux[face_id] += thetap * fluxi;
+
         rhs[jj] -= thetap * fluxi;
       }
 
@@ -3959,6 +4031,39 @@ _convection_diffusion_scalar_unsteady
                      &fluxi);
 
       cs_dispatch_sum(&rhs[ii], -fluxi, b_sum_type);
+
+      /* Fluxes without mass accumulation if needed */
+      if (b_flux != nullptr) {
+        fluxi = 0.;
+
+        cs_b_imposed_conv_flux(iconvp,
+                               thetap,
+                               0, //imasac,
+                               inc,
+                               bc_type[face_id],
+                               icvfli[face_id],
+                               _pvar[ii],
+                               _pvar[ii], /* no relaxation */
+                               pip,
+                               coefap[face_id],
+                               coefbp[face_id],
+                               coface[face_id],
+                               cofbce[face_id],
+                               b_massflux[face_id],
+                               1., /* xcpp */
+                               &fluxi);
+
+        cs_b_diff_flux(idiffp,
+                       thetap,
+                       inc,
+                       pip,
+                       cofafp[face_id],
+                       cofbfp[face_id],
+                       b_visc[face_id],
+                       &fluxi);
+
+        b_flux[face_id] += fluxi;
+      }
 
     });
 
@@ -7186,7 +7291,7 @@ cs_beta_limiter_building(int                   f_id,
   const cs_lnum_t n_cells = m->n_cells;
   const cs_lnum_t n_cells_ext = m->n_cells_with_ghosts;
 
-  cs_real_t* cpro_beta = cs_field_by_id(
+  cs_real_t* cv_limiter = cs_field_by_id(
       cs_field_get_key_int(f, cs_field_key_id("convection_limiter_id")))->val;
 
   cs_real_t *denom_inf, *num_inf, *denom_sup, *num_sup;
@@ -7242,14 +7347,14 @@ cs_beta_limiter_building(int                   f_id,
       beta_sup = cs_math_fmin(beta_sup, 1.);
     }
 
-    cpro_beta[c_id] = cs_math_fmin(beta_inf, beta_sup);
+    cv_limiter[c_id] = cs_math_fmin(beta_inf, beta_sup);
   });
 
   ctx.wait();
 
   /* Synchronize variable */
   if (halo != nullptr)
-    cs_halo_sync(halo, CS_HALO_STANDARD, ctx.use_gpu(), cpro_beta);
+    cs_halo_sync(halo, CS_HALO_STANDARD, ctx.use_gpu(), cv_limiter);
 
   CS_FREE_HD(denom_inf);
   CS_FREE_HD(denom_sup);
@@ -7301,6 +7406,8 @@ cs_beta_limiter_building(int                   f_id,
  * \param[in]     b_visc        \f$ \mu_\fib \dfrac{S_\fib}{\ipf \centf} \f$
  *                               at border faces for the r.h.s.
  * \param[in,out] rhs           right hand side \f$ \vect{Rhs} \f$
+ * \param[in,out] i_flux        interior flux (or nullptr)
+ * \param[in,out] b_flux        boundary flux (or nullptr)
  */
 /*----------------------------------------------------------------------------*/
 
@@ -7319,7 +7426,9 @@ cs_convection_diffusion_scalar(int                         idtvar,
                                const cs_real_t             b_massflux[],
                                const cs_real_t             i_visc[],
                                const cs_real_t             b_visc[],
-                               cs_real_t         *restrict rhs)
+                               cs_real_t                  *rhs,
+                               cs_real_2_t                 i_flux[],
+                               cs_real_t                   b_flux[])
 {
   std::chrono::high_resolution_clock::time_point t_start;
   if (cs_glob_timer_kernels_flag > 0)
@@ -7330,14 +7439,24 @@ cs_convection_diffusion_scalar(int                         idtvar,
     f = cs_field_by_id(f_id);
 
   if (idtvar >= 0) {
-    _convection_diffusion_scalar_unsteady<false>
-      (f, eqp, icvflb, inc, imasac,
-       pvar, pvara,
-       icvfli,
-       bc_coeffs,
-       i_massflux, b_massflux,
-       i_visc, b_visc,
-       nullptr, rhs);
+    if (i_flux == nullptr)
+      _convection_diffusion_scalar_unsteady<false, false>
+        (f, eqp, icvflb, inc, imasac,
+         pvar, pvara,
+         icvfli,
+         bc_coeffs,
+         i_massflux, b_massflux,
+         i_visc, b_visc,
+         nullptr, rhs, i_flux, b_flux);
+    else
+      _convection_diffusion_scalar_unsteady<false, true>
+        (f, eqp, icvflb, inc, imasac,
+         pvar, pvara,
+         icvfli,
+         bc_coeffs,
+         i_massflux, b_massflux,
+         i_visc, b_visc,
+         nullptr, rhs, i_flux, b_flux);
   }
   else {
 
@@ -8131,7 +8250,7 @@ cs_convection_diffusion_thermal(int                         idtvar,
     f = cs_field_by_id(f_id);
 
   if (idtvar >= 0) {
-    _convection_diffusion_scalar_unsteady<true>
+    _convection_diffusion_scalar_unsteady<true, false>
       (f, eqp,
        false, /* icvflb */
        inc, imasac,
@@ -8140,7 +8259,7 @@ cs_convection_diffusion_thermal(int                         idtvar,
        bc_coeffs,
        i_massflux, b_massflux,
        i_visc, b_visc,
-       xcpp, rhs);
+       xcpp, rhs, nullptr, nullptr);
     return;
   }
 

src/alge/cs_convection_diffusion.h
@@ -164,6 +164,8 @@ cs_beta_limiter_building(int              f_id,
  * \param[in]     b_visc        \f$ \mu_\fib \dfrac{S_\fib}{\ipf \centf} \f$
  *                               at border faces for the r.h.s.
  * \param[in,out] rhs           right hand side \f$ \vect{Rhs} \f$
+ * \param[in,out] i_flux        interior flux (or nullptr)
+ * \param[in,out] b_flux        boundary flux (or nullptr)
  */
 /*----------------------------------------------------------------------------*/
 
@@ -182,7 +184,9 @@ cs_convection_diffusion_scalar(int                         idtvar,
                                const cs_real_t             b_massflux[],
                                const cs_real_t             i_visc[],
                                const cs_real_t             b_visc[],
-                               cs_real_t                  *rhs);
+                               cs_real_t                  *rhs,
+                               cs_real_2_t                 i_flux[],
+                               cs_real_t                   b_flux[]);
 
 /*----------------------------------------------------------------------------*/
 /*

src/alge/cs_convection_diffusion_priv.h
@@ -2608,7 +2608,7 @@ cs_central_downwind_cells(const cs_lnum_t    ii,
  * \param[in]     p_d             value at downwind cell
  * \param[in]     local_max_c     local maximum of variable
  * \param[in]     local_min_c     local minimum of variable
- * \param[in]     courant_c       central cell courant number
+ * \param[in]     courant_c       central cell Courant number
  * \param[out]    pif             contribution of i to flux from i to j
  * \param[out]    pjf             contribution of j to flux from i to j
  */

commit 740dff11ef5d1ed513944ad6ee33edbc9fbc0065

Fix compilation of C user-defined functions, broken by 3dea5087 (yesterday).

src/alge/cs_balance.h
@@ -147,6 +147,8 @@ cs_balance_scalar(int                         idtvar,
 
 END_C_DECLS
 
+#if defined(__cplusplus)
+
 /*----------------------------------------------------------------------------*/
 /*
  * \brief Wrapper to the function which adds the explicit part of the
@@ -240,6 +242,8 @@ cs_balance_scalar(int                         idtvar,
                   cs_real_t                   smbrp[],
                   cs_real_2_t                 i_flux[],
                   cs_real_t                   b_flux[]);
+
+#endif // defined(__cplusplus)
 
 BEGIN_C_DECLS
 