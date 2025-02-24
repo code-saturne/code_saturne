code-saturne/code_saturne

commit 790ddaff65ba2e9e4df35e39bdfdd67da7a911fc

[turb] Refurbished K-eps type models and deploy cs_glob_turb_rans_model->dissip_buo_mdl.
G_epsilon can now be modelled with a specific constant Ce3 (different frome Ce1) and
two models are available:
 dissip_buo_mdl=0: same as before (G_esp taken into account only if G>0)
 dissip_buo_mdl=1: always taken into account as C3.G/T

The time stepping of this term is specific to ensure that epsilon remains positive
(so implicit in time if G < 0).
This time stepping is also deployed for EBRSM (where this model is used by default).

Also:
- fix bug using Cazalbou rotation correction (introduced in 2dd2b11c25864 2019-01-24)
- fix bug V2f-phi (introduced in 1accbd8827d93 2012-10-30)
- fuses constants that are the same for different models (Ceps2, Ceps1...)

src/atmo/cs_atprke.cpp
@@ -220,18 +220,14 @@ _etheq(cs_real_t   pphy,
  *
  * \param[in]       cromo       density
  * \param[in]       cpro_pcvto  turbulent viscosity
- * \param[in, out]  tinstk      implicit part of the buoyancy term (for k)
- * \param[in, out]  smbrk       explicit part of the buoyancy term (for k)
- * \param[in, out]  smbre       explicit part of the buoyancy term (for eps)
+ * \param[in, out]  gk          explicit part of the buoyancy term (for k)
  */
 /*----------------------------------------------------------------------------*/
 
 static void
 _dry_atmosphere(const cs_real_t  cromo[],
                 const cs_real_t  cpro_pcvto[],
-                cs_real_t        tinstk[],
-                cs_real_t        smbrk[],
-                cs_real_t        smbre[])
+                cs_real_t        gk[])
 {
   cs_mesh_quantities_t *fvq   = cs_glob_mesh_quantities;
   const cs_real_t *cell_f_vol = fvq->cell_vol;
@@ -276,8 +272,7 @@ _dry_atmosphere(const cs_real_t  cromo[],
                            1,    // inc
                            grad);
 
-  /* Production and gravity terms
-     TINSTK = P + G et TINSTE = P + (1 - CE3)*G */
+  /* TKE Production by gravity term G */
 
   cs_real_t rho, visct, xeps, xk, ttke, gravke;
   cs_field_t *f_tke_buoy = cs_field_by_name_try("tke_buoyancy");
@@ -294,14 +289,8 @@ _dry_atmosphere(const cs_real_t  cromo[],
     gravke = beta *
       cs_math_3_dot_product(grad[c_id], grav) / prdtur;
 
-    /* Implicit part (no implicit part for epsilon because the source
-     * term is positive) */
-
-    tinstk[c_id] += fmax(-rho*cell_f_vol[c_id]*cs_turb_cmu*ttke*gravke, 0.);
-
-    /* Explicit part */
-    smbre[c_id] += visct*fmax(gravke, 0.);
-    smbrk[c_id] += visct*gravke;
+    /* Explicit Buoyant terms */
+    gk[c_id] += visct*gravke;
 
     /* Save for post processing */
     if (f_tke_buoy != nullptr)
@@ -322,18 +311,14 @@ _dry_atmosphere(const cs_real_t  cromo[],
  *
  * \param[in]       cromo       density
  * \param[in]       cpro_pcvto  turbulent viscosity
- * \param[in, out]  tinstk      implicit part of the buoyancy term (for k)
- * \param[in, out]  smbrk       explicit part of the buoyancy term (for k)
- * \param[in, out]  smbre       explicit part of the buoyancy term (for eps)
+ * \param[in, out]  gk          explicit part of the buoyancy term (for k)
  */
 /*----------------------------------------------------------------------------*/
 
 static void
 _humid_atmosphere(const cs_real_t  cromo[],
                   const cs_real_t  cpro_pcvto[],
-                  cs_real_t        tinstk[],
-                  cs_real_t        smbrk[],
-                  cs_real_t        smbre[])
+                  cs_real_t        gk[])
 {
   cs_mesh_quantities_t *fvq   = cs_glob_mesh_quantities;
   const cs_real_t *cell_f_vol = fvq->cell_vol;
@@ -438,7 +423,7 @@ _humid_atmosphere(const cs_real_t  cromo[],
                            grad);
 
   /* Production and gravity terms
-   * TINSTK = P + G et TINSTE = P + (1 - CE3)*G */
+   * RHS k  = P + G et RHS ep = ce1 P + ce3 * G */
 
   /* Now store the production term due to theta_liq in gravke_theta */
 
@@ -460,7 +445,7 @@ _humid_atmosphere(const cs_real_t  cromo[],
                            grad);
 
   /* Production and gravity terms
-   * TINSTK = P + G et TINSTE = P + (1 - CE3)*G */
+   * RHS k  = P + G et RHS ep = ce1 P + ce3 * G */
 
   /* Store the production term due to qw in gravke_qw */
 
@@ -484,14 +469,9 @@ _humid_atmosphere(const cs_real_t  cromo[],
 
     gravke  = gravke_theta[c_id] + gravke_qw[c_id];
 
-    /* Implicit part (no implicit part for epsilon because the source
-     * term is positive) */
+    /* Explicit Buoyant terms */
+    gk[c_id] += visct*gravke;
 
-    tinstk[c_id] += fmax(-rho*cell_f_vol[c_id]*cs_turb_cmu*ttke*gravke, 0.);
-
-    /* Explicit part */
-    smbre[c_id] += visct*fmax(gravke, 0.);
-    smbrk[c_id] += visct*gravke;
   }
 
   BFT_FREE(etheta);
@@ -517,16 +497,12 @@ _humid_atmosphere(const cs_real_t  cromo[],
  * model in the context of the atmospheric module
  * g = g*grad(theta)/prdtur/theta
  *
- * \param[in, out]  tinstk    Implicit part of the buoyancy term (for k)
- * \param[in, out]  smbrk     Explicit part of the buoyancy term (for k)
- * \param[in, out]  smbre     Explicit part of the buoyancy term (for eps)
+ * \param[in, out]  gk        Explicit part of the buoyancy term (for k)
  */
 /*----------------------------------------------------------------------------*/
 
 void
-cs_atmo_buoyancy_ke_prod(cs_real_t  tinstk[],
-                         cs_real_t  smbrk[],
-                         cs_real_t  smbre[])
+cs_atmo_buoyancy_ke_prod(cs_real_t  gk[])
 {
   /* Time extrapolation? */
   int key_t_ext_id = cs_field_key_id("time_extrapolated");
@@ -552,16 +528,12 @@ cs_atmo_buoyancy_ke_prod(cs_real_t  tinstk[],
   if (cs_glob_physical_model_flag[CS_ATMOSPHERIC] == CS_ATMO_DRY)
     _dry_atmosphere(cromo,
                     cpro_pcvto,
-                    tinstk,
-                    smbrk,
-                    smbre);
+                    gk);
 
   else if (cs_glob_physical_model_flag[CS_ATMOSPHERIC] == CS_ATMO_HUMID)
     _humid_atmosphere(cromo,
                       cpro_pcvto,
-                      tinstk,
-                      smbrk,
-                      smbre);
+                      gk);
 }
 
 /*----------------------------------------------------------------------------*/

src/atmo/cs_atprke.h
@@ -64,16 +64,12 @@ BEGIN_C_DECLS
  * model in the context of the atmospheric module
  * g = g*grad(theta)/prdtur/theta
  *
- * \param[in, out]  tinstk    Implicit part of the buoyancy term (for k)
- * \param[in, out]  smbrk     Explicit part of the buoyancy term (for k)
- * \param[in, out]  smbre     Explicit part of the buoyancy term (for eps)
+ * \param[in, out]  gk     Explicit part of the buoyancy term (for k)
  */
 /*----------------------------------------------------------------------------*/
 
 void
-cs_atmo_buoyancy_ke_prod(cs_real_t  *tinstk,
-                         cs_real_t  *smbrk,
-                         cs_real_t  *smbre);
+cs_atmo_buoyancy_ke_prod(cs_real_t  gk[]);
 
 /*----------------------------------------------------------------------------*/
 

src/turb/cs_turbulence_htles.cpp
@@ -285,8 +285,8 @@ cs_turbulence_htles(void)
       xpsi   = xbeta/(xcmu*xgamma + xr*(xbeta - xcmu*xgamma));
     }
     else if (turb_model->model == CS_TURB_V2F_BL_V2K) {
-      cs_real_t xce1 = cs_turb_cpale1;
-      cs_real_t xce2 = cs_turb_cpale2;
+      cs_real_t xce1 = cs_turb_ce1;
+      cs_real_t xce2 = cs_turb_ce2;
       xpsi   = xce2/(xce1 + xr*(xce2 - xce1));
     }
 

src/turb/cs_turbulence_model.cpp
@@ -508,19 +508,20 @@ double cs_turb_ce1 = 1.44;
 
 /*!
  * Constant \f$C_{\varepsilon 2}\f$ for the \f$k-\varepsilon\f$ and
- * \f$R_{ij}-\varepsilon\f$ LRR models.
+ * \f$R_{ij}-\varepsilon\f$ models.
  * Default value for (\f$k-\varepsilon\f$ or \f$R_{ij}-\varepsilon\f$ LRR)
  * is 1.92, modified otherwise.
- * - Rij-epsilon SSG or EBRSM: 1.83
+ * for \f$R_{ij}-\varepsilon\f$ SSG or EBRSM: 1.83
+ * for the v2f \f$\varphi\f$-model: 1.85
  */
 double cs_turb_ce2 = 1.92;
 
 /*!
- * Constant \f$C_{\varepsilon 3}\f$ for EB-RSM model.
- * Useful only for buoyant term calculation of \f$R_{ij}\f$
- * in \f$R_{ij}-\varepsilon EB-RSM\f$.
+ * Constant \f$C_{\varepsilon 3}\f$ for the \f$k-\varepsilon\f$ and
+ * for \f$R_{ij}-\varepsilon\f$ models. Equal to \c cs_turb_ce1 by default,
+ * to 2.02 for EBRSM
  */
-double cs_turb_ce3 = 2.02;
+double cs_turb_ce3 = 1.44;
 
 /*!
  * Coefficient of interfacial coefficient in k-eps, used in Lagrange treatment.
@@ -661,12 +662,6 @@ double cs_turb_xct = 6.0;
 
 /*! Constant of the Rij-epsilon EBRSM. */
 double cs_turb_xceta = 80.0;
-
-/*! Specific constant of v2f "BL-v2k" (or phi-alpha). */
-double cs_turb_cpale1 = 1.44;
-
-/*! Specific constant of v2f "BL-v2k" (or phi-alpha). */
-double cs_turb_cpale2 = 1.83;
 
 /*! Specific constant of v2f "BL-v2k" (or phi-alpha). */
 double cs_turb_cpale3 = 2.3;
@@ -952,12 +947,6 @@ double cs_turb_cdries = 26.0;
  * Useful if and only if \ref model=50 (v2f \f$\varphi\f$-model).
  */
 double cs_turb_cv2fa1 = 0.05;
-
-/*!
- * Constant \f$C_{\varepsilon 2}\f$ for the v2f \f$\varphi\f$-model.
- * Useful if and only if \ref model=50 (v2f \f$\varphi\f$-model).
- */
-double cs_turb_cv2fe2 = 1.85;
 
 /*!
  * Constant \f$C_1\f$ for the v2f \f$\varphi\f$-model.
@@ -1424,9 +1413,17 @@ cs_turb_compute_constants(int phase_id)
     cs_turb_crij_c0 = (cs_turb_crij1-1.0)*2.0/3.0;
 
   if (cs_glob_turb_model->model == CS_TURB_RIJ_EPSILON_SSG
-      || cs_glob_turb_model->model == CS_TURB_RIJ_EPSILON_EBRSM)
+      || cs_glob_turb_model->model == CS_TURB_RIJ_EPSILON_EBRSM
+      || cs_glob_turb_model->model == CS_TURB_V2F_BL_V2K)
     cs_turb_ce2 = 1.83;
 
+  if (cs_glob_turb_model->model == CS_TURB_V2F_PHI) {
+    cs_turb_ce1 = 1.4;
+    cs_turb_ce2 = 1.85;
+  }
+
+  if (cs_glob_turb_model->model == CS_TURB_RIJ_EPSILON_EBRSM)
+    cs_turb_ce3 = 2.02;
 }
 
 /*----------------------------------------------------------------------------*/
@@ -1916,8 +1913,9 @@ cs_turb_constants_log_setup(void)
       (CS_LOG_SETUP,
        _("    ce1:         %14.5e (Cepsilon 1: production coef.)\n"
          "    ce2:         %14.5e (Cepsilon 2: dissipat.  coef.)\n"
+         "    ce3:         %14.5e (Cepsilon 3: dissipat.  coef.)\n"
          "    cmu:         %14.5e (Cmu constant)\n"),
-         cs_turb_ce1, cs_turb_ce2, cs_turb_cmu);
+         cs_turb_ce1, cs_turb_ce2, cs_turb_ce3, cs_turb_cmu);
 
   else if (turb_model->model == CS_TURB_RIJ_EPSILON_LRR)
     cs_log_printf
@@ -1930,10 +1928,11 @@ cs_turb_constants_log_setup(void)
          "    crijp2:      %14.5e (Fast coeff. for wall echo)\n"
          "    ce1:         %14.5e (Cepsilon 1: production coef.)\n"
          "    ce2:         %14.5e (Cepsilon 2: dissipat.  coef.)\n"
+         "    ce3:         %14.5e (Cepsilon 3: dissipat.  coef.)\n"
          "    cmu:         %14.5e (Cmu constant)\n"),
          cs_turb_crij1, cs_turb_crij2,
          cs_turb_crij3, cs_turb_csrij, cs_turb_crijp1,
-         cs_turb_crijp2, cs_turb_ce1, cs_turb_ce2, cs_turb_cmu);
+         cs_turb_crijp2, cs_turb_ce1, cs_turb_ce2,cs_turb_ce3, cs_turb_cmu);
 
   else if (turb_model->model == CS_TURB_RIJ_EPSILON_SSG)
     cs_log_printf
@@ -1949,11 +1948,12 @@ cs_turb_constants_log_setup(void)
          "    crij3:       %14.5e (Gravity term coeff.)\n"
          "    ce1:         %14.5e (Cepsilon 1: production coef.)\n"
          "    ce2:         %14.5e (Cepsilon 2: dissipat.  coef.)\n"
+         "    ce3:         %14.5e (Cepsilon 3: dissipat.  coef.)\n"
          "    cmu:         %14.5e (Cmu constant)\n"),
          cs_turb_cssgs1, cs_turb_cssgs2, cs_turb_cssgr1,
          cs_turb_cssgr2, cs_turb_cssgr3, cs_turb_cssgr4,
          cs_turb_cssgr5, cs_turb_csrij, cs_turb_crij3,
-         cs_turb_ce1, cs_turb_ce2,
+         cs_turb_ce1, cs_turb_ce2, cs_turb_ce3,
          cs_turb_cmu);
 
   else if (turb_model->model == CS_TURB_RIJ_EPSILON_EBRSM) {
@@ -1970,12 +1970,13 @@ cs_turb_constants_log_setup(void)
          "    crij3:       %14.5e (Gravity term coeff.)\n"
          "    ce1:         %14.5e (Cepsilon 1: production coef.)\n"
          "    ce2:         %14.5e (Cepsilon 2: dissipat.  coef.)\n"
+         "    ce3:         %14.5e (Cepsilon 3: dissipat.  coef.)\n"
          "    xa1:         %14.5e (Coef A1)\n"
          "    xceta:       %14.5e (Coef Ceta)\n"
          "    xct:         %14.5e (Coef CT)\n"),
          cs_turb_cebms1, cs_turb_cebmr1, cs_turb_cebmr2,
          cs_turb_cebmr3, cs_turb_cebmr4, cs_turb_cebmr5,
-         cs_turb_csrij, cs_turb_crij3, cs_turb_ce1, cs_turb_ce2,
+         cs_turb_csrij, cs_turb_crij3, cs_turb_ce1, cs_turb_ce2, cs_turb_ce3,
          cs_turb_xa1,
          cs_turb_xceta, cs_turb_xct);
 
@@ -1985,14 +1986,17 @@ cs_turb_constants_log_setup(void)
     cs_log_printf
       (CS_LOG_SETUP,
        _("    cv2fa1:      %14.5e (a1 to calculate Cepsilon1)\n"
-         "    cv2fe2:      %14.5e (Cepsilon 2: dissip. coeff.)\n"
+         "    ce1:         %14.5e (Cepsilon 1: production coef.)\n"
+         "    ce2:         %14.5e (Cepsilon 2: dissipat.  coef.)\n"
+         "    ce3:         %14.5e (Cepsilon 3: dissipat.  coef.)\n"
          "    cmu   :      %14.5e (Cmu constant)\n"
          "    cv2fct:      %14.5e (CT constant)\n"
          "    cv2fcl:      %14.5e (CL constant)\n"
          "    cv2fet:      %14.5e (C_eta constant)\n"
          "    cv2fc1:      %14.5e (C1 constant)\n"
          "    cv2fc2:      %14.5e (C2 constant)\n"),
-         cs_turb_cv2fa1, cs_turb_cv2fe2,
+         cs_turb_cv2fa1,
+         cs_turb_ce1, cs_turb_ce2, cs_turb_ce3,
          cs_turb_cmu, cs_turb_cv2fct,
          cs_turb_cv2fcl, cs_turb_cv2fet, cs_turb_cv2fc1,
          cs_turb_cv2fc2);
@@ -2000,17 +2004,17 @@ cs_turb_constants_log_setup(void)
   else if (turb_model->model == CS_TURB_V2F_BL_V2K)
     cs_log_printf
       (CS_LOG_SETUP,
-       _("    cpale1:      %14.5e (Cepsilon 1 : Prod. coeff.)\n"
-         "    cpale2:      %14.5e (Cepsilon 2 : Diss. coeff.)\n"
-         "    cpale3:      %14.5e (Cepsilon 3 : E term coeff.)\n"
-         "    cpale4:      %14.5e (Cepsilon 4 : Mod Diss. coef.)\n"
+       _("    ce1:         %14.5e (Cepsilon 1: production coef.)\n"
+         "    ce2:         %14.5e (Cepsilon 2: Diss. coef.)\n"
+         "    cpale3:      %14.5e (Cepsilon 3: E term coef.)\n"
+         "    cpale4:      %14.5e (Cepsilon 4: Mod Diss. coe.)\n"
          "    cmu   :      %14.5e (Cmu constant)\n"
          "    cpalct:      %14.5e (CT constant)\n"
          "    cpalcl:      %14.5e (CL constant)\n"
          "    cpalet:      %14.5e (C_eta constant)\n"
          "    cpalc1:      %14.5e (C1 constant)\n"
          "    cpalc2:      %14.5e (C2 constant)\n"),
-         cs_turb_cpale1, cs_turb_cpale2, cs_turb_cpale3,
+         cs_turb_ce1, cs_turb_ce2, cs_turb_cpale3,
          cs_turb_cpale4,
          cs_turb_cmu, cs_turb_cpalct, cs_turb_cpalcl,
          cs_turb_cpalet, cs_turb_cpalc1, cs_turb_cpalc2);

src/turb/cs_turbulence_model.h
@@ -372,8 +372,6 @@ extern double cs_turb_xa1;
 extern double cs_turb_xct;
 extern double cs_turb_xclt;
 extern double cs_turb_xceta;
-extern double cs_turb_cpale1;
-extern double cs_turb_cpale2;
 extern double cs_turb_cpale3;
 extern double cs_turb_cpale4;
 extern double cs_turb_cpalc1;
@@ -425,7 +423,6 @@ extern double cs_turb_csmago_max;
 extern double cs_turb_csmago_min;
 extern double cs_turb_cdries;
 extern double cs_turb_cv2fa1;
-extern double cs_turb_cv2fe2;
 extern double cs_turb_cv2fc1;
 extern double cs_turb_cv2fc2;
 extern double cs_turb_cv2fct;

src/turb/cs_turbulence_rij.cpp
@@ -901,6 +901,7 @@ _gravity_st_rij(const cs_field_t  *f_rij,
  * \param[in]       up_rhop     work array for \f$ \vect{u}'\rho' \f$
  * \param[in]       cell_f_vol  cell fluid volume
  * \param[in]       grav        gravity
+ * \param[in, out]  fimp        work array for implicite contribution
  * \param[in, out]  rhs         work array for right-hand side
  */
 /*----------------------------------------------------------------------------*/
@@ -910,6 +911,7 @@ _gravity_st_epsilon(int              phase_id,
                     const cs_real_t  up_rhop[][3],
                     const cs_real_t  cell_f_vol[],
                     const cs_real_t  grav[],
+                    cs_real_t        fimp[],
                     cs_real_t        rhs[])
 {
   const cs_lnum_t n_cells = cs_glob_mesh->n_cells;
@@ -945,10 +947,9 @@ _gravity_st_epsilon(int              phase_id,
 
   const cs_turb_model_type_t model
     = (cs_turb_model_type_t)cs_glob_turb_model->model;
-  int dissip_buo_mdl = cs_glob_turb_rans_model->dissip_buo_mdl;
+  const int dissip_buo_mdl = cs_glob_turb_rans_model->dissip_buo_mdl;
 
   const cs_real_t xct = cs_turb_xct;
-  const cs_real_t ce1 = cs_turb_ce1;
   const cs_real_t ce3 = cs_turb_ce3;
 
   cs_field_t *f_alpha_theta = nullptr;
@@ -1000,8 +1001,6 @@ _gravity_st_epsilon(int              phase_id,
 
     cs_real_t time_scale = tke / cvara_ep[c_id];
 
-    cs_real_t buoyancy_constant = ce1;
-
     if (model == CS_TURB_RIJ_EPSILON_EBRSM) {
 
       /* Calculation of the Durbin time scale */
@@ -1020,18 +1019,18 @@ _gravity_st_epsilon(int              phase_id,
         cs_real_t xr
           = (1.0 - alpha_theta[c_id])*prdtl + alpha_theta[c_id]*rvarfl;
 
-        buoyancy_constant = ce3;
-
         time_scale = time_scale * sqrt(xr)/sqrt(prdtl);
       }
     }
 
     if (dissip_buo_mdl == 1) {
-      rhs[c_id] +=   buoyancy_constant
+      fimp[c_id] +=  ce3 * g_up_rhop/time_scale
+                     / cvara_ep[c_id] * cell_f_vol[c_id];
+      rhs[c_id] +=   ce3
                    * g_up_rhop/time_scale * cell_f_vol[c_id];
     }
     else {
-      rhs[c_id] +=   buoyancy_constant
+      rhs[c_id] +=   ce3
                    * cs_math_fmax(0., g_up_rhop/time_scale) * cell_f_vol[c_id];
     }
   });
@@ -2758,10 +2757,10 @@ _solve_epsilon(int              phase_id,
 
     /* Extrapolation of source terms (2nd order in time) */
     if (st_prv_id > -1)
-      _gravity_st_epsilon(phase_id, up_rhop, cell_f_vol, grav, c_st_prv);
+      _gravity_st_epsilon(phase_id, up_rhop, cell_f_vol, grav, fimp, c_st_prv);
 
     else
-      _gravity_st_epsilon(phase_id, up_rhop, cell_f_vol, grav, rhs);
+      _gravity_st_epsilon(phase_id, up_rhop, cell_f_vol, grav, fimp, rhs);
 
   }
 

commit 93a35ea53189bcd9153e5fbea1b056aada1d83e9

Fix bug introduced in 790ddaff (buoyant term for atmospheric module).

src/atmo/cs_atprke.cpp
@@ -290,7 +290,7 @@ _dry_atmosphere(const cs_real_t  cromo[],
       cs_math_3_dot_product(grad[c_id], grav) / prdtur;
 
     /* Explicit Buoyant terms */
-    gk[c_id] += visct*gravke;
+    gk[c_id] = visct*gravke;
 
     /* Save for post processing */
     if (f_tke_buoy != nullptr)
@@ -470,7 +470,7 @@ _humid_atmosphere(const cs_real_t  cromo[],
     gravke  = gravke_theta[c_id] + gravke_qw[c_id];
 
     /* Explicit Buoyant terms */
-    gk[c_id] += visct*gravke;
+    gk[c_id] = visct*gravke;
 
   }
 