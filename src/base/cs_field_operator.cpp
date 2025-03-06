code-saturne/code_saturne

commit d7a4af1d3739065031e5a1fc2ab2c994636f9738

Use consistent boundary values for field gradients at previous time step.
To be consistent with saved BC values, this requires also saving the previous
bc_coeffs->val_f values.

This could be improved further by updating bc_coeffs->val_f_pre at BC
initialization, or saving those terms in checkpoint/restart files, to avoid
a probable minor difference when restarting from a checkpoint.

Also, this is handled for the high-level operators, but we should also
check if lower-level calls are done with inconsistent, previous values.

src/base/cs_field_operator.cpp
@@ -668,6 +668,7 @@ cs_field_gradient_vector(const cs_field_t          *f,
   cs_real_3_t *var = (use_previous_t) ? (cs_real_3_t *)(f->val_pre)
                                       : (cs_real_3_t *)(f->val);
 
+  cs_field_bc_coeffs_t bc_coeffs_loc;
   const cs_field_bc_coeffs_t *bc_coeffs = nullptr;
 
   if (f->bc_coeffs != nullptr) {
@@ -676,6 +677,11 @@ cs_field_gradient_vector(const cs_field_t          *f,
     if (coupled_key_id > 1) {
       if (cs_field_get_key_int(f, coupled_key_id) > 0) {
         bc_coeffs = f->bc_coeffs;
+        if (use_previous_t) {
+          cs_field_bc_coeffs_init(&bc_coeffs_loc);
+          bc_coeffs_loc.val_f = f->bc_coeffs->val_f_pre;
+          bc_coeffs = &bc_coeffs_loc;
+        }
       }
     }
   }
@@ -741,12 +747,18 @@ cs_field_gradient_tensor(const cs_field_t          *f,
   cs_real_6_t *var = (use_previous_t) ? (cs_real_6_t *)(f->val_pre)
                                       : (cs_real_6_t *)(f->val);
 
-  const cs_field_bc_coeffs_t *bc_coeffs_ts;
+  cs_field_bc_coeffs_t bc_coeffs_loc;
+  const cs_field_bc_coeffs_t *bc_coeffs;
   if (f->bc_coeffs != nullptr) {
     int coupled_key_id = cs_field_key_id_try("coupled");
     if (coupled_key_id > 1) {
       if (cs_field_get_key_int(f, coupled_key_id) > 0) {
-        bc_coeffs_ts = f->bc_coeffs;
+        bc_coeffs = f->bc_coeffs;
+        if (use_previous_t) {
+          cs_field_bc_coeffs_init(&bc_coeffs_loc);
+          bc_coeffs_loc.val_f = f->bc_coeffs->val_f_pre;
+          bc_coeffs = &bc_coeffs_loc;
+        }
       }
     }
   }
@@ -760,7 +772,7 @@ cs_field_gradient_tensor(const cs_field_t          *f,
                      static_cast<cs_gradient_limit_t>(eqp->imligr),
                      eqp->epsrgr,
                      eqp->climgr,
-                     bc_coeffs_ts,
+                     bc_coeffs,
                      var,
                      grad);
 }

src/base/cs_field.cpp
@@ -45,6 +45,7 @@
 #include "bft/bft_error.h"
 #include "bft/bft_printf.h"
 
+#include "base/cs_array.h"
 #include "base/cs_log.h"
 #include "base/cs_map.h"
 #include "base/cs_mem.h"
@@ -1500,7 +1501,7 @@ cs_f_field_get_label(int           f_id,
 void
 cs_field_bc_coeffs_init(cs_field_bc_coeffs_t  *bc_coeffs)
 {
-  bc_coeffs->location_id = 0.0;
+  bc_coeffs->location_id = 0;
 
   bc_coeffs->icodcl = nullptr;
   bc_coeffs->rcodcl1 = nullptr;
@@ -1520,6 +1521,8 @@ cs_field_bc_coeffs_init(cs_field_bc_coeffs_t  *bc_coeffs)
   bc_coeffs->val_f_lim = nullptr;
   bc_coeffs->val_f_d = nullptr;
   bc_coeffs->val_f_d_lim = nullptr;
+
+  bc_coeffs->val_f_pre = nullptr;
 
   bc_coeffs->hint = nullptr;
   bc_coeffs->_hext = nullptr;
@@ -1551,6 +1554,7 @@ cs_field_bc_coeffs_shallow_copy(const cs_field_bc_coeffs_t  *ref,
   copy->val_f_d = nullptr;
   copy->val_f_lim = nullptr;
   copy->val_f_d_lim = nullptr;
+  copy->val_f_pre = nullptr;
 }
 
 /*----------------------------------------------------------------------------*/
@@ -1592,6 +1596,9 @@ cs_field_bc_coeffs_free_copy(const cs_field_bc_coeffs_t  *ref,
     CS_FREE(copy->val_f_d);
   if (copy->val_f_d_lim != ref->val_f_d_lim)
     CS_FREE(copy->val_f_d_lim);
+
+  if (copy->val_f_pre != ref->val_f_pre)
+    CS_FREE(copy->val_f_pre);
 
   if (copy->hint != ref->hint)
     CS_FREE(copy->hint);
@@ -1973,6 +1980,8 @@ cs_field_allocate_bc_coeffs(cs_field_t  *f,
       f->bc_coeffs->val_f_d = nullptr;
       f->bc_coeffs->val_f_d_lim = nullptr;
 
+      f->bc_coeffs->val_f_pre = nullptr;
+
       CS_MALLOC_HD(f->bc_coeffs->a, n_elts[0]*a_mult, cs_real_t, cs_alloc_mode);
       CS_MALLOC_HD(f->bc_coeffs->b, n_elts[0]*b_mult, cs_real_t, cs_alloc_mode);
 
@@ -2340,50 +2349,33 @@ cs_field_current_to_previous(cs_field_t  *f)
 {
   assert(f != nullptr);
 
-  if (f->n_time_vals > 1) {
+  if (f->n_time_vals < 2)
+    return;
 
-    const cs_lnum_t *n_elts = cs_mesh_location_get_n_elts(f->location_id);
-    const cs_lnum_t _n_elts = n_elts[2];
+  const cs_lnum_t dim = f->dim;
+  const cs_lnum_t *n_elts = cs_mesh_location_get_n_elts(f->location_id);
+  const cs_lnum_t n_vals = n_elts[2] * dim;
 
-#   pragma omp parallel if (_n_elts > CS_THR_MIN)
-    {
-      const int dim = f->dim;
+  if (f->is_owner) {
+    for (int kk = f->n_time_vals - 1; kk > 0; kk--)
+      cs_array_copy(n_vals, f->vals[kk-1], f->vals[kk]);
+  }
+  else {
+    cs_array_copy(n_vals, f->val, f->val_pre);
+  }
 
-      if (f->is_owner) {
-        if (dim == 1) {
-          for (int kk = f->n_time_vals - 1; kk > 0; kk--) {
-#           pragma omp for
-            for (cs_lnum_t ii = 0; ii < _n_elts; ii++)
-              f->vals[kk][ii] = f->vals[kk-1][ii];
-          }
-        }
-        else {
-          for (int kk = f->n_time_vals - 1; kk > 0; kk--) {
-#           pragma omp for
-            for (cs_lnum_t ii = 0; ii < _n_elts; ii++) {
-              for (cs_lnum_t jj = 0; jj < dim; jj++)
-                f->vals[kk][ii*dim + jj] = f->vals[kk-1][ii*dim + jj];
-            }
-          }
-        }
-      }
-      else {
-        if (dim == 1) {
-#         pragma omp for
-          for (cs_lnum_t ii = 0; ii < _n_elts; ii++)
-            f->val_pre[ii] = f->val[ii];
-        }
-        else {
-#         pragma omp for
-          for (cs_lnum_t ii = 0; ii < _n_elts; ii++) {
-            for (cs_lnum_t jj = 0; jj < dim; jj++)
-              f->val_pre[ii*dim + jj] = f->val[ii*dim + jj];
-          }
-        }
-      }
+  if (f->bc_coeffs != nullptr) {
+    if (f->bc_coeffs->val_f != nullptr) {
+      const int location_id = CS_MESH_LOCATION_BOUNDARY_FACES;
+      const cs_lnum_t n_bc_vals
+        = cs_mesh_location_get_n_elts(location_id)[0] * dim;
 
+      if (f->bc_coeffs->val_f_pre == nullptr)
+        CS_MALLOC_HD(f->bc_coeffs->val_f_pre, n_bc_vals, cs_real_t,
+                     cs_alloc_mode);
+
+      cs_array_copy(n_bc_vals, f->bc_coeffs->val_f, f->bc_coeffs->val_f_pre);
     }
-
   }
 }
 
@@ -2427,6 +2419,7 @@ cs_field_destroy_all(void)
       }
       CS_FREE_HD(f->bc_coeffs->val_f);
       CS_FREE_HD(f->bc_coeffs->val_f_d);
+      CS_FREE_HD(f->bc_coeffs->val_f_pre);
       CS_FREE(f->bc_coeffs);
     }
   }

src/base/cs_field.h
@@ -148,6 +148,8 @@ typedef struct {
   cs_real_t          *val_f_d_lim; /* face value for diffusion
                                       (limited) */
 
+  cs_real_t          *val_f_pre;   /* previous face value for gradient */
+
 } cs_field_bc_coeffs_t;
 
 /* Field descriptor */

commit 3846e141bc3a0d2c42beb0b695490851ea243c2d

Fix for strided iterative gradient at previous time step (broken by d7a4af1d).

src/base/cs_field_operator.cpp
@@ -678,7 +678,7 @@ cs_field_gradient_vector(const cs_field_t          *f,
       if (cs_field_get_key_int(f, coupled_key_id) > 0) {
         bc_coeffs = f->bc_coeffs;
         if (use_previous_t) {
-          cs_field_bc_coeffs_init(&bc_coeffs_loc);
+          cs_field_bc_coeffs_shallow_copy(bc_coeffs, &bc_coeffs_loc);
           bc_coeffs_loc.val_f = f->bc_coeffs->val_f_pre;
           bc_coeffs = &bc_coeffs_loc;
         }
@@ -755,7 +755,7 @@ cs_field_gradient_tensor(const cs_field_t          *f,
       if (cs_field_get_key_int(f, coupled_key_id) > 0) {
         bc_coeffs = f->bc_coeffs;
         if (use_previous_t) {
-          cs_field_bc_coeffs_init(&bc_coeffs_loc);
+          cs_field_bc_coeffs_shallow_copy(bc_coeffs, &bc_coeffs_loc);
           bc_coeffs_loc.val_f = f->bc_coeffs->val_f_pre;
           bc_coeffs = &bc_coeffs_loc;
         }