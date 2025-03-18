code-saturne/code_saturne

commit 57c01e33ab7909dabe540b035f85fb59cf0dce22

[Atmo] continue default setting of reference value (t0, ro0, p0) with meteo files.
The value of the GUI for ro0 is also not read if predefined law on.
This point has to be checked. It would be better to remove default
writting of `initial_value` for density when not necessary (e.g. when
metoe file is used).

Warning: this will change default values of some validation cases
because p0 and t0 was not considered when using meteo files.

src/atmo/atlecm.f90
@@ -74,6 +74,7 @@ double precision rap,rscp,tmoy, rhmoy
 double precision ztop, zzmax, tlkelv, pptop, dum
 double precision rhum,q0,q1
 double precision cpvcpa
+double precision pres, temp, qv
 
 character(len=80) :: ccomnt,oneline
 character(len=1)  :: csaute
@@ -102,16 +103,10 @@ rewind(unit=impmet, err=99)
 
 itp=0
 ih2o = 0
+!--> flag to take into account the humidity
+if (ippmod(iatmos).eq.2) ih2o=1
 
 cpvcpa = cp_v / cp_a
-
-if (imode.eq.1) then
-  rscp=rair/cp0
-
-  !--> flag to take into account the humidity
-  if (ippmod(iatmos).eq.2) ih2o=1
-
-endif
 
 !===============================================================================
 ! 1. Loop on time
@@ -225,10 +220,15 @@ endif
 if (ccomnt(1:1).eq.csaute) go to 103
 backspace(impmet)
 
-if (imode.eq.0) then
-  read(impmet, *, err=999, end=999)
-else
-  read(impmet, *, err=999, end=999) xyp_met(3, itp) ! pmer = p0
+read(impmet, *, err=999, end=999) pres
+
+! p_sea = p0
+if (itp.eq.1) then
+  p0 = pres
+endif
+
+if (imode.eq.1) then
+  xyp_met(3, itp) = pres
 endif
 
 !===============================================================================
@@ -246,11 +246,15 @@ if (imode.eq.0) then
   if (nbmett.le.1) then
     write(nfecra, 8001)
     call csexit (1)
-    !==========
   endif
 
   do ii = 1, nbmett
-    read (impmet,*,err=999,end=999) zzmax
+    read (impmet,*,err=999,end=999) zzmax, temp, qv
+    if (ii.eq.1 .and. itp.eq.1) then
+      t0 = temp + tkelvi
+      rhum = rair*(1.d0+(rvsra-1.d0)*qv*ih2o)
+      ro0 = p0 / t0 /rhum
+    endif
   enddo
 
   !-->  Computes nbmaxt:
@@ -330,12 +334,7 @@ endif
 if (imode.eq.1.and.itp.eq.1) then
   p0 = xyp_met(3, itp)
   t0 = ttmet(1,itp)+tkelvi
-  ! Humid atmosphere
-  if (ippmod(iatmos).eq.2) then
-    rhum = rair*(1.d0+(rvsra-1.d0)*qvmet(1,itp)*ih2o)
-  else
-    rhum = rair
-  endif
+  rhum = rair*(1.d0+(rvsra-1.d0)*qvmet(1,itp)*ih2o)
   ro0 = p0 / t0 /rhum
 endif
 

src/base/cs_physical_properties_default.c
@@ -200,16 +200,16 @@ _rho_mu_is_constant(const char       *name,
                     const cs_real_t  val[],
                     const cs_real_t  val_ref)
 {
-  bool is_constant = false;
+  bool is_constant = true;
 
   for (cs_lnum_t ii = 0; ii < n_elts; ii++) {
     if (fabs(val[ii] - val_ref) <= cs_math_epzero)
       continue;
-    is_constant = true;
+    is_constant = false;
     break;
   }
 
-  if (is_constant)
+  if (!is_constant) {
     bft_error(__FILE__, __LINE__, 0,
               _("Error: abort in the physical quantities computation\n"
                 "=====\n"

src/gui/cs_gui.c
@@ -344,6 +344,25 @@ _thermal_table_needed(const char *name)
   return choice;
 }
 
+/*----------------------------------------------------------------------------
+ * Return 1 if predefined_law
+ *
+ * parameters:
+ *   name        <--  name of the property
+ *----------------------------------------------------------------------------*/
+
+static int
+_thermal_predefined_law(const char *name)
+{
+  int choice = 0;
+
+  const char *prop_choice = _properties_choice(name, NULL);
+  if (cs_gui_strcmp(prop_choice, "predefined_law"))
+    choice = 1;
+
+  return choice;
+}
+
 /*-----------------------------------------------------------------------------
  * Compute a physical property based on a thermal law.
  *----------------------------------------------------------------------------*/
@@ -3997,14 +4016,14 @@ cs_gui_physical_properties(void)
 
   cs_vof_parameters_t *vof_param = cs_get_glob_vof_parameters();
 
-  if (_thermal_table_needed("density") == 0) {
+  if (_thermal_predefined_law("density") == 0)
     cs_gui_properties_value("density", &phys_pp->ro0);
-    if (vof_param->vof_model & CS_VOF_ENABLED) {
-      cs_gui_properties_value_by_fluid_id(0, "density", &vof_param->rho1);
-      cs_gui_properties_value_by_fluid_id(1, "density", &vof_param->rho2);
-    }
+  else if (_thermal_predefined_law("density") == 1
+      && (vof_param->vof_model & CS_VOF_ENABLED)) {
+    cs_gui_properties_value_by_fluid_id(0, "density", &vof_param->rho1);
+    cs_gui_properties_value_by_fluid_id(1, "density", &vof_param->rho2);
   }
-  else {
+  else if (_thermal_table_needed("density") == 1) {
     cs_phys_prop_compute(CS_PHYS_PROP_DENSITY,
                          1,
                          0,

commit 1b18d809cca5fdcf4e34dc35e1bd5d11fd122e9c

[Atmo] fix bug introduced in 57c01e33 (reference value and standard profiles).

src/atmo/atlecm.f90
@@ -75,6 +75,7 @@ double precision ztop, zzmax, tlkelv, pptop, dum
 double precision rhum,q0,q1
 double precision cpvcpa
 double precision pres, temp, qv
+double precision p_ref, t_ref
 
 character(len=80) :: ccomnt,oneline
 character(len=1)  :: csaute
@@ -320,13 +321,20 @@ else
     ztop = 11000.d0
     ii = nbmett
     zzmax = (int(ztmet(ii))/1000)*1000.d0
+    p_ref = 101325.d0
+    t_ref = 288.15d0
     do while(zzmax.le.(ztop-1000.d0))
       zzmax = zzmax + 1000.d0
       ii = ii + 1
       ztmet(ii) = zzmax
       ! standard temperature profile above the domaine
-      call atmstd(ztmet(nbmett), p0, ttmet(nbmett,itp)+tkelvi, &
-                  ztmet(ii), dum, tlkelv, dum)
+      if (ihpm.eq.0) then
+        call atmstd(ztmet(nbmett), p0, ttmet(nbmett,itp)+tkelvi, &
+                    ztmet(ii), dum, tlkelv, dum)
+      else ! if (ihpm.eq.1) then
+       call atmstd(0.d0, p_ref, t_ref, &
+                   ztmet(ii), dum, tlkelv, dum)
+      endif
       ttmet(ii,itp) = tlkelv - tkelvi
       if (iqv0.eq.0) then
         qvmet(ii,itp) = 0.d0   ! standard atmosphere: q=0
@@ -355,6 +363,8 @@ if (imode.eq.1) then
 
   phmet(1, itp) = xyp_met(3, itp)
   rscp = rair/cp0
+  p_ref = 101325.d0
+  t_ref = 288.15d0
 
   if (ihpm.eq.0) then
     phmet(1,itp) = xyp_met(3, itp)
@@ -380,7 +390,7 @@ if (imode.eq.1) then
     enddo
   else
     ! Standard pressure profile above the domain
-    call atmstd (0.d0, p0, t0, &
+    call atmstd (0.d0, p_ref, t_ref, &
                  ztmet(nbmaxt), pptop, dum, dum)
     phmet(nbmaxt,itp) = pptop
     do k = nbmaxt-1, 1, -1

src/atmo/cs_atmo_profile_std.cpp
@@ -89,6 +89,11 @@ BEGIN_C_DECLS
 /*!
  * \brief compute standard atmospheric profile (Holton p 374)
  *
+ * Note:
+ * - standard pressure is 101325Pa
+ * - standard temperature is 288.15K
+ * - standard density is 1.225 kg/m^3
+ *
  * \param[in]       z_ref      reference altitude in m
  * \param[in]       p_ref      reference pressure
  * \param[in]       t_ref      reference temperature

src/base/cs_boundary_conditions_set_coeffs_turb.cpp
@@ -674,7 +674,7 @@ _cs_boundary_conditions_set_coeffs_turb_scalar(cs_field_t  *f_sc,
         if (iwalfs != CS_WALL_F_S_MONIN_OBUKHOV) {
           cofimp = 1.0 - heq / hint[f_id];
         }
-        /* Monin obukhov */
+        /* Monin Obukhov */
         else {
           const cs_real_t rough_t = bpro_rough_t[f_id];
 
@@ -865,15 +865,13 @@ _cs_boundary_conditions_set_coeffs_turb_scalar(cs_field_t  *f_sc,
       if (   icodcl_vel[f_id] == 5
           && (   icodcl_sc[f_id] == 5
               || icodcl_sc[f_id] == 6
-              || icodcl_sc[f_id] == 15)) {
+              || icodcl_sc[f_id] == 15)
+         || (icodcl_vel[f_id] == 6 && icodcl_sc[f_id] == 6)) {
 
         phit = cofaf_sc[f_id] + cofbf_sc[f_id] * var_ip[f_id];
       }
       else if (icodcl_sc[f_id] == 1 && icodcl_vel[f_id] == 5) {
         phit = heq * (var_ip[f_id] - pimp);
-      }
-      else if (icodcl_vel[f_id] == 6 && icodcl_sc[f_id] == 6) {
-        phit = cofaf_sc[f_id] + cofbf_sc[f_id] * theipb[f_id];
       }
       /* Imposed flux with wall function for post-processing */
       else if (icodcl_sc[f_id] == 3)
@@ -1506,7 +1504,7 @@ _update_physical_quantities_rough_wall(const cs_real_t  visclc,
           The boundary term for velocity gradient is implicit
           modified for non neutral boundary layer (in uplus) */
 
-        *cofimp  = cs_math_fmin(cs_math_fmax(1 - 1/(xkappa*uplus) * rcprod, 0),
+        *cofimp  = cs_math_fmin(cs_math_fmax(1. - 1./(xkappa*uplus) * rcprod, 0),
                                 1);
 
         /* The term (rho*uet*uk) is implicit */
@@ -1713,7 +1711,7 @@ _atmo_cls(const cs_lnum_t  f_id,
   //const cs_real_t q0 = (tpot1 - tpot2) * (*uet) * dtplus * (*cfnns);
   /* FIXME tet should be output as uet is... */
 
-  /* Compute local Monin-Obukhov inverse length for log
+  /* Compute local Obukhov inverse length for log
      1/L =  Ri / (z Phim) */
   *dlmo = rib * sqrt(fm) / (distbf + rough_d);
 }
@@ -1803,6 +1801,10 @@ cs_boundary_conditions_set_coeffs_turb(int        isvhb,
   const int kscavr = cs_field_key_id("first_moment_id");
   const int ksigmas = cs_field_key_id("turbulent_schmidt");
   const int kdflim = cs_field_key_id("diffusion_limiter_id");
+
+  cs_real_t turb_prandtl = 1.;
+  if (f_th != nullptr)
+    turb_prandtl = cs_field_get_key_double(f_th, ksigmas);
 
   /* Type of wall functions for scalar */
   const cs_wall_f_s_type_t iwalfs = cs_glob_wall_functions->iwalfs;
@@ -2050,12 +2052,10 @@ cs_boundary_conditions_set_coeffs_turb(int        isvhb,
   }
 
   /* Pointers to specific fields */
-  cs_real_t *byplus = nullptr, *bdplus = nullptr, *bdlmo = nullptr; //*buk = nullptr
+  cs_real_t *byplus = nullptr, *bdplus = nullptr, *bdlmo = nullptr;
   CS_MALLOC(byplus, n_b_faces, cs_real_t);
   CS_MALLOC(bdplus, n_b_faces, cs_real_t);
   CS_MALLOC(bdlmo, n_b_faces, cs_real_t);
-
-  // CS_MALLOC(buk, n_b_faces, cs_real_t);
 
   /* Correction for atmospheric wall functions */
 
@@ -2356,28 +2356,31 @@ cs_boundary_conditions_set_coeffs_turb(int        isvhb,
          * times a shift for smooth regime
          *
          * Rough regime reads:
-         *   T+ = 1/kappa ln(y/rough_t) = 1/kappa ln(y/zeta) + 8.5
-         *                              = 1/kappa ln[y/zeta * exp(8.5 kappa)]
+         *   T+ = Prt/kappa ln(y/rough_t) = Prt * (ln(y/zeta)/kappa + 8.5)
+         *      = Prt/kappa ln[y/zeta * exp(8.5 kappa)]
          *
          * Note zeta_t = rough_t * exp(8.5 kappa)
          *
+         * Question: is 8.5 really in factor of Prt?
+         *
          * Smooth regime reads:
-         * T+ = 1/kappa ln(y uk/nu) + 5.2 = 1/kappa ln[y uk*exp(5.2 kappa) / nu]
+         * T+ = Prt *(ln(y uk/nu)/kappa + 5.2)
+         *    = Prt/kappa ln[y uk*exp(5.2 kappa) / nu]
          *
          * Mixed regime reads:
-         *   T+ = 1/kappa ln[y uk*exp(5.2 kappa)/(nu + alpha uk zeta)]
-         *      = 1/kappa ln[  y uk*exp(5.2 kappa)
+         *   T+ = Prt/kappa ln[y uk*exp(5.2 kappa)/(nu + alpha uk zeta)]
+         *      = Prt/kappa ln[  y uk*exp(5.2 kappa)
          *                   / (nu + alpha uk rough_t * exp(8.5 kappa))]
-         *      = 1/kappa ln[  y uk*exp(5.2 kappa)
+         *      = Prt/kappa ln[  y uk*exp(5.2 kappa)
          *                   / (nu + alpha uk rough_t * exp(8.5 kappa))]
          * with
          *   alpha * exp(8.5 kappa) / exp(5.2 kappa) = 1
          * ie
          *   alpha = exp(-(8.5-5.2) kappa) = 0.25
          * so
-         *   T+ = 1/kappa ln[  y uk*exp(5.2 kappa)
+         *   T+ = Prt/kappa ln[  y uk*exp(5.2 kappa)
          *                   / (nu + uk rough_t * exp(5.2 kappa))]
-         *      = 1/kappa ln[y+k / (exp(-5.2 kappa) + uk rough_t/nu)]
+         *      = Prt/kappa ln[y+k / (exp(-5.2 kappa) + uk rough_t/nu)]
          */
 
         /* shifted y+ */
@@ -2526,20 +2529,9 @@ cs_boundary_conditions_set_coeffs_turb(int        isvhb,
     else if (icodcl_vel[f_id] == 6)
       uplus = utau / uet;
 
-    /* Store u_star and uk if needed */
-    if (boundary_ustar != nullptr && icodcl_vel[f_id] == 5)
-      bpro_ustar[f_id] = uet;
-
     /* Rough wall: one velocity scale: set uk to uet */
     if (cs_glob_wall_functions->iwallf <= 2 && icodcl_vel[f_id] == 6)
       uk = uet;
-
-    if(boundary_uk != nullptr && icodcl_vel[f_id] == 5)
-      bpro_uk[f_id] = uk;
-
-    /* Save yplus if post-processed or condensation modelling */
-    if (f_yplus != nullptr)
-      yplbr[f_id] = yplus;
 
     uetmax  = cs_math_fmax(uet, uetmax);
     uetmin  = cs_math_fmin(uet, uetmin);
@@ -3731,9 +3723,13 @@ cs_boundary_conditions_set_coeffs_turb(int        isvhb,
 
     }
 
+    /* Save for future use or post-processing */
     byplus[f_id] = yplus;
     bdplus[f_id] = dplus;
     bpro_ustar[f_id] = uet;
+    /* Save yplus if post-processed or condensation modelling */
+    if (f_yplus != nullptr)
+      yplbr[f_id] = yplus;
 
     /* FIXME note taken into account yet in cs_wall_functions_scalar, cfnns */
     bcfnns[f_id] = (icodcl_vel[f_id] == 5) ? 1.0 : cfnns;