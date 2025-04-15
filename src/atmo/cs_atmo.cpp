code-saturne/code_saturne

commit 014d1aad6c111b428649f231114970a4df808e2b

[IBM] Introduction of the forced convection and boundary conditions on z_ground.
- We have holes due to the fact that in LiDAR, we don't have the height
  of the walls since the scan is done from the top, which causes the error.
  To avoid this a forced convection is introduced in the direction of scan.
- A new structure is introduced in order to globally control the type od fill,
  direction of the fill (direction vector) and also an option n_agglomeration
  is added to be used in the future to create a smooth plane from scanned points
  in order to avoid holes by averaging over the neighbouring cells.
  (src/base/cs_porosity_from_scan.cpp & src/base/cs_porosity_from_scan.h
  & src/base/cs_boundary_conditions_type.cpp)

- Computation of z_ground:
  - Introduction of boundary condition in the case of immersed boundaries to
    correctly compute the z_ground value.
    (src/atmo/cs_atmo.cpp)

- Accounting for the proper sign for interior faces.
  (src/mesh/cs_mesh_quantities.cpp)

- Introduction of necessary structure to account for immersed boundaries
  contribution.
  (src/base/cs_field.cpp & src/base/cs_field.h)

src/atmo/cs_atmo.cpp
@@ -4082,12 +4082,15 @@ cs_atmo_compute_meteo_profiles(void)
   /* Profiles */
   for (cs_lnum_t cell_id = 0; cell_id < m->n_cells; cell_id++) {
 
+    if (mq->c_disable_flag[cell_id] == 1)
+      continue;
+
     cs_real_t z_grd = 0.;
     if (z_ground != nullptr)
       z_grd = z_ground[cell_id];
 
     /* Local elevation */
-    cs_real_t z = cell_cen[cell_id][2] - z_grd;
+    cs_real_t z = cs::max(cell_cen[cell_id][2] - z_grd, 0.);
 
     /* Velocity profile */
     cs_real_t u_norm = ustar0 / kappa * cs_mo_psim(z+z0, z0, dlmo);
@@ -4268,6 +4271,14 @@ cs_atmo_z_ground_compute(void)
 
   const int *bc_type = cs_glob_bc_type;
 
+  /* Quantities required to account for the immersed walls*/
+  const cs_lnum_t  n_cells = m->n_cells;
+  const cs_real_t *cell_vol = mq->cell_vol;
+  const cs_real_t *c_w_face_surf = mq->c_w_face_surf;
+  const cs_real_3_t *c_w_face_normal = (const cs_real_3_t *)mq->c_w_face_normal;
+  const cs_real_t *c_w_dist_inv = mq->c_w_dist_inv;
+  const cs_real_3_t *c_w_face_cog = (const cs_real_3_t *)mq->c_w_face_cog;
+
   /* Pointer to z_ground field */
   cs_field_t *f = cs_field_by_name_try("z_ground");
 
@@ -4336,8 +4347,6 @@ cs_atmo_z_ground_compute(void)
     }
   }
 
-  cs_parall_max(1, CS_INT_TYPE, &(eqp_p->ndircl));
-
   /* Matrix
    * ====== */
 
@@ -4358,6 +4367,36 @@ cs_atmo_z_ground_compute(void)
 
   for (cs_lnum_t cell_id = 0; cell_id < m->n_cells_with_ghosts; cell_id++)
     rhs[cell_id] = 0.;
+
+  /*Dirichlet condition on immersed boundaries*/
+  for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++) {
+    /*Geomtric quantities */
+    const cs_real_t solid_surf = c_w_face_surf[c_id];
+    const cs_real_t wall_dist  = (c_w_dist_inv[c_id] < DBL_MIN) ?
+                                  0.:
+                                  1. / c_w_dist_inv[c_id];
+    const cs_real_t pyr_vol = wall_dist * solid_surf;
+
+    eqp_p->ndircl = 1;
+    cs_real_t hint = c_w_dist_inv[c_id];
+    cs_real_t pimp = cs_math_3_dot_product(c_w_face_cog[c_id], normal);
+
+    f->bc_coeffs->ib_g_wall_cor[c_id] = 0.;
+    f->bc_coeffs->ib_val_ext[c_id] = pimp;
+    f->bc_coeffs->ib_hint[c_id] = hint;
+    f->bc_coeffs->ib_qimp[c_id] = 0.;
+
+    cs_real_t tsimp = fmax(- cs_math_3_dot_product(c_w_face_normal[c_id], normal), 0.);
+
+    rhs[c_id] += tsimp * (pimp - f->val[c_id]); //explicit term
+    rovsdt[c_id] += cs::max(tsimp, 0.); //implicit term
+
+    norm += cs_math_pow2(pimp) * c_w_face_surf[c_id];
+    ground_surf += c_w_face_surf[c_id];
+  }
+
+  cs_parall_max(1, CS_INT_TYPE, &(eqp_p->ndircl));
+
 
   /* Norm
    * ==== */

src/base/cs_boundary_conditions_type.cpp
@@ -1286,11 +1286,13 @@ cs_boundary_conditions_type(bool  init,
 
     cs_field_t *f_yplus = cs_field_by_name_try("yplus");
     cs_field_t *f_zground = cs_field_by_name_try("z_ground");
+    cs_field_t *f_poro = cs_field_by_name_try("porosity");
 
     const int kscavr = cs_field_key_id("first_moment_id");
 
     int inlet_types[2] = {CS_INLET, CS_CONVECTIVE_INLET};
     int inlet_codes[2] = {1, 13};
+
 
     for (int field_id = 0; field_id < n_fields; field_id++) {
 
@@ -1360,11 +1362,19 @@ cs_boundary_conditions_type(bool  init,
 
             else if (rcodcl1[f_id] > 0.5*cs_math_infinite_r) {
               const cs_real_t flumbf = b_massflux[f_id];
-              /* Outgoing flux or yplus or z_ground */
+              /* Outgoing flux or yplus or z_ground or porosity */
               if (   flumbf >= - cs_math_epzero
                   || f == f_yplus
                   || f == f_zground) {
                 icodcl[f_id] = 3;
+                for (cs_lnum_t k = 0; k < f->dim; k++) {
+                  rcodcl1[k*n_b_faces+f_id] = 0.;
+                  rcodcl2[k*n_b_faces+f_id] = cs_math_infinite_r;
+                  rcodcl3[k*n_b_faces+f_id] = 0.;
+                }
+              }
+              else if (   flumbf <= 0. && f == f_poro) {
+                icodcl[f_id] = 1;
                 for (cs_lnum_t k = 0; k < f->dim; k++) {
                   rcodcl1[k*n_b_faces+f_id] = 0.;
                   rcodcl2[k*n_b_faces+f_id] = cs_math_infinite_r;

src/base/cs_field.cpp
@@ -47,10 +47,12 @@
 
 #include "base/cs_array.h"
 #include "base/cs_log.h"
+#include "base/cs_array.h"
 #include "base/cs_map.h"
 #include "base/cs_mem.h"
 #include "mesh/cs_mesh_location.h"
 #include "base/cs_parall.h"
+#include "base/cs_porous_model.h"
 
 /*----------------------------------------------------------------------------
  * Header for the current file
@@ -1377,6 +1379,14 @@ cs_field_bc_coeffs_init(cs_field_bc_coeffs_t  *bc_coeffs)
   bc_coeffs->val_f_d_lim = nullptr;
 
   bc_coeffs->val_f_pre = nullptr;
+  /*quantities to be utilised for IBM */
+  bc_coeffs->ib_bc_code = nullptr;
+  bc_coeffs->ib_g_wall_cor = nullptr;
+  bc_coeffs->ib_val_ext = nullptr;
+  bc_coeffs->ib_qimp = nullptr;
+  bc_coeffs->ib_hint = nullptr;
+  bc_coeffs->ib_hext = nullptr;
+  /**/
 
   bc_coeffs->hint = nullptr;
   bc_coeffs->_hext = nullptr;
@@ -1835,6 +1845,9 @@ cs_field_allocate_bc_coeffs(cs_field_t  *f,
       f->bc_coeffs->val_f_d_lim = nullptr;
 
       f->bc_coeffs->val_f_pre = nullptr;
+      //FIXME
+      f->bc_coeffs->wall_law = nullptr; /*to be used in cs_wall_functions.cpp
+                                          for rij model*/
 
       CS_MALLOC_HD(f->bc_coeffs->a, n_elts[0]*a_mult, cs_real_t, cs_alloc_mode);
       CS_MALLOC_HD(f->bc_coeffs->b, n_elts[0]*b_mult, cs_real_t, cs_alloc_mode);
@@ -1877,6 +1890,24 @@ cs_field_allocate_bc_coeffs(cs_field_t  *f,
         f->bc_coeffs->_hext = nullptr;
       }
 
+      if (cs_glob_porous_model == 3) {
+        const cs_lnum_t *n_elts_cell =
+          cs_mesh_location_get_n_elts(CS_MESH_LOCATION_CELLS);
+        CS_MALLOC(f->bc_coeffs->ib_bc_code, n_elts_cell[0], int);
+        CS_MALLOC(f->bc_coeffs->ib_g_wall_cor, n_elts_cell[0], cs_real_t);
+        CS_MALLOC(f->bc_coeffs->ib_val_ext, a_mult*n_elts_cell[0], cs_real_t);
+        CS_MALLOC(f->bc_coeffs->ib_qimp, a_mult*n_elts_cell[0], cs_real_t);
+        CS_MALLOC(f->bc_coeffs->ib_hint, n_elts_cell[0], cs_real_t);
+        CS_MALLOC(f->bc_coeffs->ib_hext, a_mult*n_elts_cell[0], cs_real_t);
+      }
+      else {
+        f->bc_coeffs->ib_bc_code = nullptr;
+        f->bc_coeffs->ib_g_wall_cor = nullptr;
+        f->bc_coeffs->ib_val_ext = nullptr;
+        f->bc_coeffs->ib_qimp = nullptr;
+        f->bc_coeffs->ib_hint = nullptr;
+        f->bc_coeffs->ib_hext = nullptr;
+      }
     }
 
     else {
@@ -1920,6 +1951,25 @@ cs_field_allocate_bc_coeffs(cs_field_t  *f,
       else {
         CS_FREE(f->bc_coeffs->hint);
         CS_FREE(f->bc_coeffs->_hext);
+      }
+
+      if (cs_glob_porous_model == 3) {
+        const cs_lnum_t *n_elts_cell =
+          cs_mesh_location_get_n_elts(CS_MESH_LOCATION_CELLS);
+        CS_REALLOC(f->bc_coeffs->ib_bc_code, n_elts_cell[0], int);
+        CS_REALLOC(f->bc_coeffs->ib_g_wall_cor, n_elts_cell[0], cs_real_t);
+        CS_REALLOC(f->bc_coeffs->ib_val_ext, n_elts_cell[0]*a_mult, cs_real_t);
+        CS_REALLOC(f->bc_coeffs->ib_qimp, n_elts_cell[0]*a_mult, cs_real_t);
+        CS_REALLOC(f->bc_coeffs->ib_hint, n_elts_cell[0], cs_real_t);
+        CS_REALLOC(f->bc_coeffs->ib_hext, n_elts_cell[0]*a_mult, cs_real_t);
+      }
+      else {
+        CS_FREE(f->bc_coeffs->ib_bc_code);
+        CS_FREE(f->bc_coeffs->ib_g_wall_cor);
+        CS_FREE(f->bc_coeffs->ib_val_ext);
+        CS_FREE(f->bc_coeffs->ib_qimp);
+        CS_FREE(f->bc_coeffs->ib_hint);
+        CS_FREE(f->bc_coeffs->ib_hext);
       }
 
     }
@@ -2130,6 +2180,19 @@ cs_field_init_bc_coeffs(cs_field_t  *f)
       }
     }
 
+    /* Wall correction term for gradients for immersed boundaries
+     * Initializing all the terms to zero */
+    if (f->bc_coeffs->ib_g_wall_cor != nullptr) {
+      const cs_lnum_t *n_elts_cell =
+        cs_mesh_location_get_n_elts(CS_MESH_LOCATION_CELLS);
+      cs_array_int_fill_zero(n_elts_cell[0], f->bc_coeffs->ib_bc_code);
+      cs_array_real_fill_zero(n_elts_cell[0], f->bc_coeffs->ib_g_wall_cor);
+      cs_array_real_fill_zero(n_elts_cell[0]*dim, f->bc_coeffs->ib_val_ext);
+      cs_array_real_fill_zero(n_elts_cell[0]*dim, f->bc_coeffs->ib_qimp);
+      cs_array_real_fill_zero(n_elts_cell[0], f->bc_coeffs->ib_hint);
+      cs_array_real_fill_zero(n_elts_cell[0]*dim, f->bc_coeffs->ib_hext);
+    }
+
   }
 
   else
@@ -2267,6 +2330,7 @@ cs_field_destroy_all(void)
       CS_FREE_HD(f->bc_coeffs->bc);
       CS_FREE(f->bc_coeffs->hint);
       CS_FREE(f->bc_coeffs->_hext);
+
       if (f->bc_coeffs->val_f_lim != f->bc_coeffs->val_f) {
         CS_FREE_HD(f->bc_coeffs->val_f_lim);
         CS_FREE_HD(f->bc_coeffs->val_f_d_lim);
@@ -2274,6 +2338,14 @@ cs_field_destroy_all(void)
       CS_FREE_HD(f->bc_coeffs->val_f);
       CS_FREE_HD(f->bc_coeffs->val_f_d);
       CS_FREE_HD(f->bc_coeffs->val_f_pre);
+
+      CS_FREE(f->bc_coeffs->ib_g_wall_cor);
+      CS_FREE(f->bc_coeffs->ib_val_ext);
+      CS_FREE(f->bc_coeffs->ib_qimp);
+      CS_FREE(f->bc_coeffs->ib_hint);
+      CS_FREE(f->bc_coeffs->ib_hext);
+      CS_FREE(f->bc_coeffs->wall_law);
+      CS_FREE(f->bc_coeffs->ib_bc_code);
       CS_FREE(f->bc_coeffs);
     }
   }

src/base/cs_field.h
@@ -147,6 +147,20 @@ typedef struct {
   cs_real_t          *val_f_d;     /* face value for diffusion */
   cs_real_t          *val_f_d_lim; /* face value for diffusion
                                       (limited) */
+  cs_real_t         *ib_g_wall_cor;/* wall gradient correction factor for
+                                      immersed boundaries */
+  cs_real_t         *ib_val_ext;   /* external value for
+                                      immersed boundaries */
+  int               *ib_bc_code;   /* Indicator of the type of bc
+                                      for Immersed boundaries */
+  cs_real_t         *ib_qimp;      /* flux value to impose for immersed
+                                      boundaries */
+  cs_real_t         *ib_hint;      /* Immersed boundary exchange coefficient */
+  cs_real_t         *ib_hext;      /* Immersed boundary coefficient provided
+                                      externally from an analytical or
+                                      experimental correlation */
+  int               *wall_law;     /* To store fortran indicator iuntur
+                                      0 in the viscous sublayer */
 
   cs_real_t          *val_f_pre;   /* previous face value for gradient */
 

src/base/cs_halo.cpp
@@ -2396,6 +2396,12 @@ cs_halo_sync(const cs_halo_t  *halo,
              bool              on_device,
              cs_real_t         val[]);
 
+template void
+cs_halo_sync(const cs_halo_t  *halo,
+             cs_halo_type_t    sync_mode,
+             bool              on_device,
+             cs_lnum_t         val[]);
+
 /*----------------------------------------------------------------------------*/
 
 template <int Stride, typename T>

src/base/cs_porosity_from_scan.cpp
@@ -115,10 +115,13 @@ static cs_porosity_from_scan_opt_t _porosity_from_scan_opt = {
   .transformation_matrix = {{1., 0., 0., 0.},
                             {0., 1., 0., 0.},
                             {0., 0., 1., 0.}},
+  .direction_vector = {0., 0., -1.},
+  .type_fill = CS_FILL_RADIAL,
   .nb_sources = 0,
   .sources = nullptr,
   .source_c_ids = nullptr,
   .threshold = 4,
+  .n_agglomeration = 1,
   .porosity_threshold = 1e-12,
   .convection_porosity_threshold = 0.5,
   .use_staircase = false,
@@ -243,6 +246,7 @@ _solid_plane_from_points(const cs_mesh_t   *m,
                          cs_real_t          c_w_face_normal[][3])
 {
   const cs_real_t threshold = _porosity_from_scan_opt.threshold;
+  const cs_real_t n_agglomeration = _porosity_from_scan_opt.n_agglomeration;
   cs_real_t tol_err = 1.0e-12;
 
   for (cs_lnum_t c_id = 0; c_id < m->n_cells; c_id++) {
@@ -1142,44 +1146,28 @@ cs_compute_porosity_from_scan(void)
   CS_MALLOC_HD(pvar, m->n_cells_with_ghosts, cs_real_t, cs_alloc_mode);
   CS_MALLOC_HD(dpvar, m->n_cells_with_ghosts, cs_real_t, cs_alloc_mode);
 
-  /* Loop over sources */
-  for (int s_id = 0; s_id < nb_sources; s_id++) {
+  cs_real_t direction_vector[3] = {_porosity_from_scan_opt.direction_vector[0],
+                                   _porosity_from_scan_opt.direction_vector[1],
+                                   _porosity_from_scan_opt.direction_vector[2]};
 
-    int rank_source;
-    cs_geom_closest_point(m->n_cells,
-                          (const cs_real_3_t *)mq_g->cell_cen,
-                          _porosity_from_scan_opt.sources[s_id],
-                          &(source_c_ids[s_id]),
-                          &rank_source);
+  const cs_fill_type_t type_fill = _porosity_from_scan_opt.type_fill;
 
-    cs_real_t source_cen[3];
-
-    if (source_c_ids[s_id] > -1) {
-      for (int i = 0; i < 3; i++)
-        source_cen[i] = cell_cen[source_c_ids[s_id]][i];
-    }
-
-    cs_parall_bcast(rank_source, 3, CS_REAL_TYPE, source_cen);
-
-    /* Compute the mass flux due to V = e_r
+  if (type_fill == CS_FILL_DIRECTION) {
+    /* Compute the mass flux due to V = -e_z
      *=====================================*/
 
     for (cs_lnum_t face_id = 0; face_id < m->n_i_faces; face_id++) {
-      cs_real_t x0xf[3] = {
-        i_face_cog[face_id][0] - source_cen[0],
-        i_face_cog[face_id][1] - source_cen[1],
-        i_face_cog[face_id][2] - source_cen[2]
-      };
+      cs_real_t x0xf[3] = {direction_vector[0],
+                           direction_vector[1],
+                           direction_vector[2]};
       i_massflux[face_id] = cs_math_3_dot_product(x0xf,
                                                   i_f_face_normal[face_id]);
     }
 
     for (cs_lnum_t face_id = 0; face_id < m->n_b_faces; face_id++) {
-      cs_real_t x0xf[3] = {
-        b_face_cog[face_id][0] - source_cen[0],
-        b_face_cog[face_id][1] - source_cen[1],
-        b_face_cog[face_id][2] - source_cen[2]
-      };
+      cs_real_t x0xf[3] = {direction_vector[0],
+                           direction_vector[1],
+                           direction_vector[2]};
 
       b_massflux[face_id] = cs_math_3_dot_product(x0xf,
                                                   b_f_face_normal[face_id]);
@@ -1194,8 +1182,8 @@ cs_compute_porosity_from_scan(void)
       if (b_massflux[face_id] <= 0.) {
 
        eqp->ndircl = 1;
-       cs_real_t hint = 1. / mq_g->b_dist[face_id];
-       cs_real_t pimp = 0.;
+       cs_real_t hint = 1. / mq->b_dist[face_id];
+       cs_real_t pimp = 1.;
 
        cs_boundary_conditions_set_dirichlet_scalar(face_id,
                                                    f->bc_coeffs,
@@ -1205,7 +1193,7 @@ cs_compute_porosity_from_scan(void)
       }
       else {
 
-        cs_real_t hint = 1. / mq_g->b_dist[face_id];
+        cs_real_t hint = 1. / mq->b_dist[face_id];
         cs_real_t qimp = 0.;
 
         cs_boundary_conditions_set_neumann_scalar(face_id,
@@ -1215,18 +1203,11 @@ cs_compute_porosity_from_scan(void)
 
       }
     }
-
     /* Matrix
      *=======*/
 
     for (cs_lnum_t c_id = 0; c_id < m->n_cells_with_ghosts; c_id++)
       rovsdt[c_id] = 0.;
-
-    /* Penalisation term for the source
-     * in parallel, only one rank takes that
-     * */
-    if (source_c_ids[s_id] > -1)
-      rovsdt[source_c_ids[s_id]] = mq_g->cell_vol[source_c_ids[s_id]];
 
     /* Even if there is no Dirichlet, the system is well-posed
      * so no need of diagonal reinforcement */
@@ -1240,14 +1221,10 @@ cs_compute_porosity_from_scan(void)
       pvar[c_id] = 0.;
     }
 
-    /* in parallel, only one rank takes that */
-    if (source_c_ids[s_id] > -1)
-      rhs[source_c_ids[s_id]] = mq_g->cell_vol[source_c_ids[s_id]];
-
     /* Norm
      *======*/
 
-    cs_real_t norm = mq_g->tot_vol;
+    cs_real_t norm = mq->tot_vol;
 
     /* Solving
      *=========*/
@@ -1285,12 +1262,162 @@ cs_compute_porosity_from_scan(void)
                                        nullptr);
 
     for (cs_lnum_t c_id = 0; c_id< m->n_cells; c_id++)
-      f->val[c_id] = cs::max(f->val[c_id], pvar[c_id]);
+      f->val[c_id] = CS_MAX(f->val[c_id], pvar[c_id]);
 
     /* Parallel synchronisation */
     cs_halo_sync(m->halo, false, f->val);
+  }
+  else if (type_fill == CS_FILL_RADIAL) {
+    /* Loop over sources */
+    for (int s_id = 0; s_id < nb_sources; s_id++) {
 
-  } /* End loop over sources */
+      int rank_source;
+      cs_geom_closest_point(m->n_cells,
+                            (const cs_real_3_t *)mq_g->cell_cen,
+                            _porosity_from_scan_opt.sources[s_id],
+                            &(source_c_ids[s_id]),
+                            &rank_source);
+
+      cs_real_t source_cen[3];
+
+      if (source_c_ids[s_id] > -1) {
+        for (int i = 0; i < 3; i++)
+          source_cen[i] = cell_cen[source_c_ids[s_id]][i];
+      }
+
+      cs_parall_bcast(rank_source, 3, CS_REAL_TYPE, source_cen);
+
+      /* Compute the mass flux due to V = e_r
+       *=====================================*/
+
+      for (cs_lnum_t face_id = 0; face_id < m->n_i_faces; face_id++) {
+        cs_real_t x0xf[3] = {
+          i_face_cog[face_id][0] - source_cen[0],
+          i_face_cog[face_id][1] - source_cen[1],
+          i_face_cog[face_id][2] - source_cen[2]
+        };
+        i_massflux[face_id] = cs_math_3_dot_product(x0xf,
+                                                    i_f_face_normal[face_id]);
+      }
+
+      for (cs_lnum_t face_id = 0; face_id < m->n_b_faces; face_id++) {
+        cs_real_t x0xf[3] = {
+          b_face_cog[face_id][0] - source_cen[0],
+          b_face_cog[face_id][1] - source_cen[1],
+          b_face_cog[face_id][2] - source_cen[2]
+        };
+
+        b_massflux[face_id] = cs_math_3_dot_product(x0xf,
+                                                    b_f_face_normal[face_id]);
+      }
+
+      /* Boundary conditions
+       *====================*/
+
+      /* homogeneous Neumann except if ingoing flux */
+      for (cs_lnum_t face_id = 0; face_id < m->n_b_faces; face_id++) {
+        /* Dirichlet BCs */
+        if (b_massflux[face_id] <= 0.) {
+
+          eqp->ndircl = 1;
+          cs_real_t hint = 1. / mq_g->b_dist[face_id];
+          cs_real_t pimp = 0.;
+
+          cs_boundary_conditions_set_dirichlet_scalar(face_id,
+                                                      f->bc_coeffs,
+                                                      pimp,
+                                                      hint,
+                                                      cs_math_infinite_r);
+        }
+        else {
+
+          cs_real_t hint = 1. / mq_g->b_dist[face_id];
+          cs_real_t qimp = 0.;
+
+          cs_boundary_conditions_set_neumann_scalar(face_id,
+                                                    f->bc_coeffs,
+                                                    qimp,
+                                                    hint);
+
+        }
+      }
+
+      /* Matrix
+       *=======*/
+
+      for (cs_lnum_t c_id = 0; c_id < m->n_cells_with_ghosts; c_id++)
+        rovsdt[c_id] = 0.;
+
+      /* Penalisation term for the source
+       * in parallel, only one rank takes that
+       * */
+      if (source_c_ids[s_id] > -1)
+        rovsdt[source_c_ids[s_id]] = mq_g->cell_vol[source_c_ids[s_id]];
+
+      /* Even if there is no Dirichlet, the system is well-posed
+       * so no need of diagonal reinforcement */
+      eqp->ndircl = 1;
+
+      /* Right hand side and initial guess
+       *==================================*/
+
+      for (cs_lnum_t c_id = 0; c_id < m->n_cells_with_ghosts; c_id++) {
+        rhs[c_id] = 0.;
+        pvar[c_id] = 0.;
+      }
+
+      /* in parallel, only one rank takes that */
+      if (source_c_ids[s_id] > -1)
+        rhs[source_c_ids[s_id]] = mq_g->cell_vol[source_c_ids[s_id]];
+
+      /* Norm
+       *======*/
+
+      cs_real_t norm = mq_g->tot_vol;
+
+      /* Solving
+       *=========*/
+
+      /* In case of a theta-scheme, set theta = 1;
+         no relaxation in steady case either */
+
+      cs_equation_iterative_solve_scalar(0,   /* idtvar: no steady state algo */
+                                         -1,  /* no over loops */
+                                         f->id,
+                                         nullptr,
+                                         0,   /* iescap */
+                                         0,   /* imucpp */
+                                         norm,
+                                         eqp,
+                                         f->val_pre,
+                                         f->val,
+                                         f->bc_coeffs,
+                                         i_massflux,
+                                         b_massflux,
+                                         i_massflux, /* viscosity, not used */
+                                         b_massflux, /* viscosity, not used */
+                                         i_massflux, /* viscosity, not used */
+                                         b_massflux, /* viscosity, not used */
+                                         nullptr,
+                                         nullptr,
+                                         nullptr,
+                                         0, /* icvflb (upwind) */
+                                         nullptr,
+                                         rovsdt,
+                                         rhs,
+                                         pvar,
+                                         dpvar,
+                                         nullptr,
+                                         nullptr);
+
+      for (cs_lnum_t c_id = 0; c_id< m->n_cells; c_id++)
+        f->val[c_id] = cs::max(f->val[c_id], pvar[c_id]);
+
+      /* Parallel synchronisation */
+      cs_halo_sync(m->halo, false, f->val);
+
+    } /* End loop over sources */
+  }
 
   /* Finalization */
 
@@ -1322,10 +1449,19 @@ cs_compute_porosity_from_scan(void)
 
   for (cs_lnum_t c_id = 0; c_id < m->n_cells_with_ghosts; c_id++) {
 
-    if (cs_math_3_dot_product(grdporo[c_id], c_w_face_normal[c_id]) > 0.0) {
-      for (cs_lnum_t i = 0; i < 3; i++)
-        c_w_face_normal[c_id][i] = - c_w_face_normal[c_id][i];
+    if (type_fill == CS_FILL_RADIAL) {
+      if (cs_math_3_dot_product(grdporo[c_id], c_w_face_normal[c_id]) > 0.0) {
+        for (cs_lnum_t i = 0; i < 3; i++)
+          c_w_face_normal[c_id][i] = - c_w_face_normal[c_id][i];
+      }
     }
+    else if (type_fill == CS_FILL_DIRECTION) {
+      if (cs_math_3_dot_product(direction_vector, c_w_face_normal[c_id]) < 0.0) {
+        for (cs_lnum_t i = 0; i < 3; i++)
+          c_w_face_normal[c_id][i] = - c_w_face_normal[c_id][i];
+      }
+    }
+
   }
 
   // Porosity

src/base/cs_porosity_from_scan.h
@@ -76,6 +76,13 @@ typedef enum {
 
 } cs_ibm_cog_location_t;
 
+typedef enum {
+
+  CS_FILL_RADIAL = 0,
+  CS_FILL_DIRECTION = 1,
+
+} cs_fill_type_t;
+
 typedef struct {
   bool  compute_porosity_from_scan;
   char *file_names;
@@ -89,10 +96,15 @@ typedef struct {
      coordinates transformation matrix,
      with last row = [0 0 0 1]) */
   cs_real_34_t transformation_matrix;
+  /*! To define the direction of fill,
+      by default it is filled from top (z-direction).*/
+  cs_real_3_t direction_vector;
+  cs_fill_type_t type_fill;
   int   nb_sources;
   cs_real_3_t *sources;
   cs_lnum_t *source_c_ids;
   cs_lnum_t threshold;
+  cs_lnum_t n_agglomeration;
   cs_real_t porosity_threshold;
   cs_real_t convection_porosity_threshold;
   bool      use_staircase;

commit 2bf74a6207403339af41d8dc4bb784e0f8529e6a

[Atmo] fix bug introduced in 014d1aad (2025-04-11).
The computation of z_ground should not be modified when no immersed
boundaries.

src/atmo/cs_atmo.cpp
@@ -3982,6 +3982,9 @@ cs_atmo_compute_meteo_profiles(void)
   cs_mesh_t *m = domain->mesh;
   cs_mesh_quantities_t *mq = domain->mesh_quantities;
 
+  const int *restrict c_disable_flag = (mq->has_disable_flag) ?
+    mq->c_disable_flag : nullptr;
+
   const cs_real_3_t *restrict cell_cen
     = (const cs_real_3_t *)mq->cell_cen;
 
@@ -4081,8 +4084,9 @@ cs_atmo_compute_meteo_profiles(void)
   /* Profiles */
   for (cs_lnum_t cell_id = 0; cell_id < m->n_cells; cell_id++) {
 
-    if (mq->c_disable_flag[cell_id] == 1)
-      continue;
+    if (c_disable_flag != nullptr)
+      if (c_disable_flag[cell_id] == 1)
+        continue;
 
     cs_real_t z_grd = 0.;
     if (z_ground != nullptr)
@@ -4367,31 +4371,33 @@ cs_atmo_z_ground_compute(void)
   for (cs_lnum_t cell_id = 0; cell_id < m->n_cells_with_ghosts; cell_id++)
     rhs[cell_id] = 0.;
 
-  /*Dirichlet condition on immersed boundaries*/
-  for (cs_lnum_t c_id = 0; c_id < n_cells; c_id++) {
-    /*Geomtric quantities */
-    const cs_real_t solid_surf = c_w_face_surf[c_id];
-    const cs_real_t wall_dist  = (c_w_dist_inv[c_id] < DBL_MIN) ?
-                                  0.:
-                                  1. / c_w_dist_inv[c_id];
-    const cs_real_t pyr_vol = wall_dist * solid_surf;
+  /*Dirichlet condition on immersed boundaries */
+  if (c_w_face_surf != nullptr) {
+    for (cs_lnum_t c_id = 0; c_id < n_cells ; c_id++) {
+      /* Geomtric quantities */
+      const cs_real_t solid_surf = c_w_face_surf[c_id];
+      const cs_real_t wall_dist  = (c_w_dist_inv[c_id] < DBL_MIN) ?
+                                    0.:
+                                    1. / c_w_dist_inv[c_id];
+      const cs_real_t pyr_vol = wall_dist * solid_surf;
 
-    eqp_p->ndircl = 1;
-    cs_real_t hint = c_w_dist_inv[c_id];
-    cs_real_t pimp = cs_math_3_dot_product(c_w_face_cog[c_id], normal);
+      eqp_p->ndircl = 1;
+      cs_real_t hint = c_w_dist_inv[c_id];
+      cs_real_t pimp = cs_math_3_dot_product(c_w_face_cog[c_id], normal);
 
-    f->bc_coeffs->ib_g_wall_cor[c_id] = 0.;
-    f->bc_coeffs->ib_val_ext[c_id] = pimp;
-    f->bc_coeffs->ib_hint[c_id] = hint;
-    f->bc_coeffs->ib_qimp[c_id] = 0.;
+      f->bc_coeffs->ib_g_wall_cor[c_id] = 0.;
+      f->bc_coeffs->ib_val_ext[c_id] = pimp;
+      f->bc_coeffs->ib_hint[c_id] = hint;
+      f->bc_coeffs->ib_qimp[c_id] = 0.;
 
-    cs_real_t tsimp = fmax(- cs_math_3_dot_product(c_w_face_normal[c_id], normal), 0.);
+      cs_real_t tsimp = fmax(- cs_math_3_dot_product(c_w_face_normal[c_id], normal), 0.);
 
-    rhs[c_id] += tsimp * (pimp - f->val[c_id]); //explicit term
-    rovsdt[c_id] += cs::max(tsimp, 0.); //implicit term
+      rhs[c_id] += tsimp * (pimp - f->val[c_id]); //explicit term
+      rovsdt[c_id] += cs::max(tsimp, 0.); //implicit term
 
-    norm += cs_math_pow2(pimp) * c_w_face_surf[c_id];
-    ground_surf += c_w_face_surf[c_id];
+      norm += cs_math_pow2(pimp) * c_w_face_surf[c_id];
+      ground_surf += c_w_face_surf[c_id];
+    }
   }
 
   cs_parall_max(1, CS_INT_TYPE, &(eqp_p->ndircl));

src/base/cs_boundary_conditions_type.cpp
@@ -745,6 +745,7 @@ cs_boundary_conditions_type(bool  init,
           const int met_1d_nlevels_t = cs_glob_atmo_option->met_1d_nlevels_t;
           const int met_1d_ntimes = cs_glob_atmo_option->met_1d_ntimes;
 
+          //TODO altitude relative ??
           const cs_real_t xuent = cs_intprf(met_1d_nlevels_t,
                                             met_1d_ntimes,
                                             cs_glob_atmo_option->z_temp_met,