code-saturne/code_saturne

commit 90a554af72993d26c61a7b53c890c989c9250279

Prepare automatic coupling with code_aster

python/code_saturne/base/cs_create.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 # This file is part of code_saturne, a general-purpose CFD tool.
 #
@@ -21,7 +21,7 @@
 # this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
 # Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 """
 This module describes the script used to create a study/case for code_saturne.
@@ -36,9 +36,9 @@ and the following classes:
 - class
 """
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # Library modules import
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 import os, sys, shutil, stat
 import types, string, re, fnmatch
@@ -49,9 +49,9 @@ from code_saturne.base import cs_exec_environment
 from code_saturne.base import cs_run_conf
 from code_saturne.base import cs_runcase
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # Process the passed command line arguments
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 def process_cmd_line(argv, pkg):
     """
@@ -105,6 +105,15 @@ def process_cmd_line(argv, pkg):
                       metavar="<py_case>",
                       help="create a new Python script case.")
 
+    parser.add_option(
+        "--aster",
+        dest="ast_case_names",
+        type="string",
+        metavar="<ast_cases>",
+        action="append",
+        help="create new code_aster case(s).",
+    )
+
     parser.set_defaults(use_ref=False)
     parser.set_defaults(study_name=None)
     parser.set_defaults(case_names=[])
@@ -112,6 +121,7 @@ def process_cmd_line(argv, pkg):
     parser.set_defaults(verbose=1)
     parser.set_defaults(n_sat=1)
     parser.set_defaults(syr_case_names=[])
+    parser.set_defaults(ast_case_names=None)
     parser.set_defaults(cat_case_name=None)
     parser.set_defaults(py_case_name=None)
 
@@ -123,19 +133,22 @@ def process_cmd_line(argv, pkg):
         else:
             options.case_names = ["CASE1"]
 
-    return study(pkg,
-                 options.study_name,
-                 cases=options.case_names,
-                 syr_case_names=options.syr_case_names,
-                 cat_case_name=options.cat_case_name,
-                 py_case_name=options.py_case_name,
-                 copy=options.copy,
-                 use_ref=options.use_ref,
-                 verbose=options.verbose)
+    return study(
+        pkg,
+        options.study_name,
+        cases=options.case_names,
+        syr_case_names=options.syr_case_names,
+        ast_case_names=options.ast_case_names,
+        cat_case_name=options.cat_case_name,
+        py_case_name=options.py_case_name,
+        copy=options.copy,
+        use_ref=options.use_ref,
+        verbose=options.verbose,
+    )
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # Assign executable mode (chmod +x) to a file
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 def set_executable(filename):
     """
@@ -153,9 +166,9 @@ def set_executable(filename):
 
     return
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # Remove executable mode (chmod -x) from a file or files inside a directory
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 def unset_executable(path):
     """
@@ -179,9 +192,9 @@ def unset_executable(path):
 
     return
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # Create local launch script
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 def create_local_launcher(pkg, path=None):
     """
@@ -245,22 +258,25 @@ fi
     fd.close()
     set_executable(local_script)
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # Definition of a class for a study
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 class study:
 
-    def __init__(self,
-                 package,
-                 study_name,
-                 cases=[],
-                 syr_case_names=[],
-                 cat_case_name=None,
-                 py_case_name=None,
-                 copy=None,
-                 use_ref=False,
-                 verbose=0):
+    def __init__(
+        self,
+        package,
+        study_name,
+        cases=[],
+        syr_case_names=[],
+        ast_case_name=None,
+        cat_case_name=None,
+        py_case_name=None,
+        copy=None,
+        use_ref=False,
+        verbose=0,
+    ):
         """
         Initialize the structure for a study.
         """
@@ -285,11 +301,13 @@ class study:
         for c in syr_case_names:
             self.syr_case_names.append(c)
 
+        self.ast_case_name = ast_case_name
+
         self.cat_case_name = cat_case_name
 
         self.py_case_name = py_case_name
 
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
 
     def create(self):
         """
@@ -316,6 +334,10 @@ class study:
         if len(self.syr_case_names) > 0:
             self.create_syrthes_cases(repbase)
 
+        # Creating code_aster case
+        if self.ast_case_name is not None:
+            self.create_aster_case(repbase)
+
         # Creating Cathare case
         if self.cat_case_name is not None:
             self.create_cathare_case(repbase)
@@ -325,15 +347,19 @@ class study:
             self.create_python_case(repbase)
 
         # Creating coupling structures
-        if len(self.cases) + len(self.syr_case_names) > 1 \
-           or self.cat_case_name or self.py_case_name:
+        if (
+            len(self.cases) + len(self.syr_case_names) > 1
+            or self.ast_case_name
+            or self.cat_case_name
+            or self.py_case_name
+        ):
             self.create_coupling(repbase)
             create_local_launcher(self.package, repbase)
 
         if repbase != cur_dir:
             os.chdir(cur_dir)
 
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
 
     def create_syrthes_cases(self, repbase):
         """
@@ -354,7 +380,20 @@ class study:
 
         os_environ = os_env_save
 
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
+
+    def create_aster_case(self, repbase):
+        """
+        Create and initialize code_aster case directories.
+        """
+
+        cs_exec_environment.source_aster_env(self.package)
+        import code_aster
+
+        os.chdir(repbase)
+        self.create_case(self.ast_case_name)
+
+    # ---------------------------------------------------------------------------
 
     def create_cathare_case(self, repbase):
         """
@@ -364,7 +403,7 @@ class study:
         os.chdir(repbase)
         self.create_case(self.cat_case_name)
 
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
 
     def create_python_case(self, repbase):
         """
@@ -388,7 +427,7 @@ class study:
             if not os.path.isdir(d):
                 os.mkdir(d)
 
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
 
     def create_coupling(self, repbase):
         """
@@ -427,6 +466,15 @@ class study:
             # Last entry is for additional SYRTHES options
             # (ex.: postprocessing with '-v ens' or '-v med')
 
+        if self.ast_case_name is not None:
+
+            c = os.path.normpath(self.ast_case_name)
+            base_c = os.path.basename(c)
+
+            coupled_domains.append(
+                {"solver": "code_aster", "domain": base_c, "script": "run_aster"}
+            )
+
         if self.cat_case_name is not None:
 
             c = os.path.normpath(self.cat_case_name)
@@ -436,7 +484,6 @@ class study:
                                     'domain': base_c,
                                     'cathare_case_file':' jdd_case.dat',
                                     'neptune_cfd_domain': 'NEPTUNE'})
-
 
         if self.py_case_name is not None:
 
@@ -463,7 +510,7 @@ class study:
         self.__coupled_run_cfg__(distrep = repbase,
                                  coupled_domains = coupled_domains)
 
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
 
     def create_case(self, casename):
         """
@@ -610,7 +657,7 @@ class study:
         if not os.path.isdir(resu):
             os.mkdir(resu)
 
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
 
     def __coupled_run_cfg__(self,
                           distrep,
@@ -637,13 +684,12 @@ class study:
             for key in dom.keys():
                 run_conf.set(dom_name, key, dom[key])
 
-
         if domains != "":
             run_conf.set('setup', 'coupled_domains', domains)
 
         run_conf.save()
 
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
 
     def __build_run_cfg__(self,
                           distrep,
@@ -670,7 +716,7 @@ class study:
 
         run_conf.save()
 
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
 
     def dump(self):
         """
@@ -700,9 +746,9 @@ class study:
                 print("  " + c)
         print()
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # Creation of the case of study directory tree
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 def main(argv, pkg):
     """
@@ -727,6 +773,6 @@ def main(argv, pkg):
 if __name__ == "__main__":
     main(sys.argv[1:], None)
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # End
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------

extras/bash_completion/code_saturne
@@ -123,7 +123,7 @@ _code_saturne() {
                 --syrthes|--aster|--cathare) COMPREPLY=( ); return 0;;
                 --copy-from)   _filedir -d; return 0;;
                 *) cmdOpts="-s --study -c --case --noref --copy-ref --copy-from \
-                     --syrthes --cathare --python -q --quiet -v --verbose";;
+                     --syrthes --aster --cathare --python -q --quiet -v --verbose";;
             esac
             ;;
         gui)

extras/code_saturne.cfg.template
@@ -44,6 +44,10 @@
 ### (e.g. <cathare_home>/mod5.1).
 # cathare =
 
+### Set the location to the code_aster installation directory
+### (e.g. <aster_home>/aster-17.x.yy/arch/<arch>).
+# aster =
+
 ### Optional path to sourcable shell environment initialization file
 ### (similar to .profile or .bashrc, specific to code_saturne).
 # rcfile =

python/code_saturne/base/cs_case.py
@@ -508,6 +508,11 @@ class case:
                 if d.name is None:
                     raise RunCaseError(err_str)
 
+        self.n_domains_aster = 0
+        for d in self.domains:
+            if isinstance(d, aster_domain):
+                self.n_domains_aster += 1
+
         # Names, directories, and files in case structure:
         # associate case domains and set case directory
 
@@ -1457,7 +1462,7 @@ class case:
                         s.write('\n')
 
         # Handle python coupling
-        if self.n_domains_python > 0:
+        if self.n_domains_python > 0 or self.n_domains_aster > 0:
             cs_exec_environment.write_script_comment(s, \
                 'Export paths necessary for python coupling.\n')
             pydir = self.package_compute.get_dir("pythondir")
@@ -1609,7 +1614,7 @@ class case:
 
         return s_path
 
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
 
     def update_scripts_tmp(self, src, dest, caption=None):
 
@@ -1656,7 +1661,7 @@ class case:
         except Exception:
             pass
 
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
 
     def add_exec_dir_stamp(self):
 
@@ -1669,7 +1674,7 @@ class case:
         f.write(self.exec_dir + '\n')
         f.close()
 
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
 
     def clear_exec_dir_stamp(self):
 
@@ -1683,7 +1688,7 @@ class case:
         except Exception:
             pass
 
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
 
     def exceeded_time_limit(self):
 
@@ -1697,7 +1702,7 @@ class case:
         else:
             return False
 
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
 
     def set_run_id(self,
                    run_id = None):
@@ -1721,7 +1726,7 @@ class case:
                 self.run_id = run_id_base + '_' + str(j)
                 self.define_result_dir()
 
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
 
     def prepare_data(self,
                      force_id = False):
@@ -1778,7 +1783,7 @@ class case:
             return 1
 
         # Setup data
-        #===========
+        # ===========
 
         sys.stdout.write('Preparing calculation data\n'
                          '--------------------------\n\n')
@@ -1817,7 +1822,7 @@ class case:
         else:
             return 0
 
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
 
     def init_prepared_data(self):
 
@@ -1832,7 +1837,7 @@ class case:
         for d in self.domains:
             d.init_staged_data()
 
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
 
     def preprocess(self,
                    n_procs = None,
@@ -1857,7 +1862,7 @@ class case:
         self.update_scripts_tmp('prepared', 'preprocessing')
 
         # Determine execution environment
-        #================================
+        # ================================
         # (priority for n_procs, in increasing order:
         # resource manager, method argument).
 
@@ -1888,7 +1893,7 @@ class case:
         self.print_procs_distribution()
 
         # Preprocessing
-        #==============
+        # ==============
 
         sys.stdout.write('Preprocessing calculation\n'
                          '-------------------------\n\n')
@@ -1944,7 +1949,7 @@ class case:
         else:
             return 0
 
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
 
     def run_solver(self):
 
@@ -2047,7 +2052,7 @@ class case:
 
         return retcode
 
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
 
     def save_results(self):
 
@@ -2109,7 +2114,7 @@ class case:
 
         return memory_leak
 
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
 
     def run(self,
             n_procs = None,
@@ -2345,7 +2350,7 @@ class case:
 
         return retcode
 
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
 
     def suggest_id(self,
                    run_id_prefix = None,
@@ -2403,6 +2408,6 @@ class case:
 
         return run_id
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # End
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------

python/code_saturne/base/cs_case_coupling.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 # This file is part of code_saturne, a general-purpose CFD tool.
 #
@@ -21,22 +21,20 @@
 # this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
 # Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 import configparser
 import os
-import os.path
 import sys
 
 from code_saturne.base.cs_case_domain import *
 from code_saturne.base.cs_case import *
-from code_saturne.base import cs_exec_environment
 from code_saturne.base import cs_run_conf
 from code_saturne.base import cs_runcase
 
-#===============================================================================
+# ===============================================================================
 # Main function for code coupling execution
-#===============================================================================
+# ===============================================================================
 
 def coupling(package,
              domains,
@@ -50,6 +48,7 @@ def coupling(package,
     use_syrthes = False
     use_neptune = False
     use_cathare = False
+    use_aster = False
     use_py_code = False
 
     # Use alternate compute (back-end) package if defined
@@ -65,6 +64,7 @@ def coupling(package,
     syr_domains = []
     nep_domains = []
     cat_domains = []
+    ast_domains = []
     py_domains = []
 
     if domains is None:
@@ -185,6 +185,31 @@ def coupling(package,
             use_syrthes = True
             syr_domains.append(dom)
 
+        elif solver_s == "code_aster":
+
+            if not config.has_option("install", "aster"):
+                raise RunCaseError(
+                    "Error: a code_aster installation is not defined in the configuration file."
+                )
+
+            try:
+                solver_path = f"{config.get('install', 'aster')}/bin/run_aster"
+                dom = aster_domain(
+                    package,
+                    name=domain_s,
+                    solver_path=solver_path,
+                    script_name=d.get("script"),
+                    n_procs_weight=d.get("n_procs_weight"),
+                    n_procs_min=d.get("n_procs_min"),
+                    n_procs_max=d.get("n_procs_max"),
+                )
+            except Exception:
+                err_str = f"Cannot create code_aster case with domain {domain_s} and n_procs_weight = {d.get('n_procs_weight')}."
+                raise RunCaseError(err_str)
+
+            use_aster = True
+            ast_domains.append(dom)
+
         elif solver_s == 'cathare':
 
             # Current version using Cathare2: the cathare case is converted to a
@@ -236,14 +261,16 @@ def coupling(package,
 
     # Now handle case for the corresponding calculation domain(s).
 
-    c = case(package,
-             package_compute = package_compute,
-             case_dir = casedir,
-             dest_dir = dest_dir,
-             staging_dir = staging_dir,
-             domains = sat_domains + nep_domains + cat_domains,
-             syr_domains = syr_domains,
-             py_domains = py_domains)
+    c = case(
+        package,
+        package_compute=package_compute,
+        case_dir=casedir,
+        dest_dir=dest_dir,
+        staging_dir=staging_dir,
+        domains=sat_domains + nep_domains + cat_domains + ast_domains,
+        syr_domains=syr_domains,
+        py_domains=py_domains,
+    )
 
     if verbose:
         msg = ' Coupling execution between: \n'
@@ -255,12 +282,14 @@ def coupling(package,
             msg += '   o neptune_cfd  [' + str(len(nep_domains)) + ' domain(s)];\n'
         if use_cathare == True:
             msg += '   o CATHARE2     [' + str(len(cat_domains)) + ' domain(s)];\n'
+        if use_aster == True:
+            msg += "   o code_aster   [" + str(len(ast_domains)) + " domain(s)];\n"
         if use_py_code == True:
             msg += '   o Python Script  [' + str(len(py_domains)) + ' domain(s)];\n'
         sys.stdout.write(msg+'\n')
 
     return c
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # End
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------

commit e150f5193d52f7230d1f9ed3ef020f48afbdd36e

[CLI] Fix create command broken by commit 90a554af72

python/code_saturne/base/cs_create.py
@@ -105,14 +105,9 @@ def process_cmd_line(argv, pkg):
                       metavar="<py_case>",
                       help="create a new Python script case.")
 
-    parser.add_option(
-        "--aster",
-        dest="ast_case_names",
-        type="string",
-        metavar="<ast_cases>",
-        action="append",
-        help="create new code_aster case(s).",
-    )
+    parser.add_option("--aster", dest="ast_case_name", type="string",
+                      metavar="<ast_case>",
+                      help="create new code_aster case.")
 
     parser.set_defaults(use_ref=False)
     parser.set_defaults(study_name=None)
@@ -121,7 +116,7 @@ def process_cmd_line(argv, pkg):
     parser.set_defaults(verbose=1)
     parser.set_defaults(n_sat=1)
     parser.set_defaults(syr_case_names=[])
-    parser.set_defaults(ast_case_names=None)
+    parser.set_defaults(ast_case_name=None)
     parser.set_defaults(cat_case_name=None)
     parser.set_defaults(py_case_name=None)
 
@@ -138,7 +133,7 @@ def process_cmd_line(argv, pkg):
         options.study_name,
         cases=options.case_names,
         syr_case_names=options.syr_case_names,
-        ast_case_names=options.ast_case_names,
+        ast_case_name=options.ast_case_name,
         cat_case_name=options.cat_case_name,
         py_case_name=options.py_case_name,
         copy=options.copy,