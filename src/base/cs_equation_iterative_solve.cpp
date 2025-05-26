code-saturne/code_saturne

commit 491bcb3e9173c6779fc6b7420c137a580671e85e

Assemble matrix coefficients directly for additional cases.

src/base/cs_equation_iterative_solve.cpp
@@ -66,6 +66,7 @@
 #include "cs_field.h"
 #include "cs_gradient.h"
 #include "cs_mesh_quantities.h"
+#include "cs_multigrid.h"
 #include "cs_parameters.h"
 #include "cs_porous_model.h"
 #include "cs_prototypes.h"
@@ -354,63 +355,47 @@ _equation_iterative_solve_strided(int                   idtvar,
    * Building of the "simplified" matrix
    *==========================================================================*/
 
+  cs_matrix_t *a = cs_sles_default_get_matrix
+                     (f_id, var_name, stride, eb_size, symmetric);
+
+  using diag_t = cs_real_t[stride][stride];
+  const diag_t *fimp_r = fimp;
+  diag_t *_fimp_r = nullptr;
+
+  /*  For steady computations, the diagonal is relaxed */
+  if (idtvar < 0) {
+    CS_MALLOC_HD(_fimp_r, n_cells, diag_t, cs_alloc_mode);
+    cs_real_t rf = 1. / relaxp;
+
+    ctx.parallel_for(n_cells, [=] CS_F_HOST_DEVICE (cs_lnum_t c_id) {
+      for (cs_lnum_t i = 0; i < stride; i++) {
+        for (cs_lnum_t j = 0; j < stride; j++)
+          _fimp_r[c_id][i][j] = fimp_r[c_id][i][j] * rf;
+      }
+    });
+  }
+
   int tensorial_diffusion = 1;
 
   if (idftnp & CS_ANISOTROPIC_LEFT_DIFFUSION)
     tensorial_diffusion = 2;
 
-  if (stride == 3)
-    cs_matrix_wrapper_vector(iconvp,
-                             idiffp,
-                             tensorial_diffusion,
-                             ndircp,
-                             isym,
-                             eb_size,
-                             thetap,
-                             bc_coeffs,
-                             (const cs_real_33_t *)fimp,
-                             i_massflux,
-                             b_massflux,
-                             i_viscm,
-                             b_viscm,
-                             (cs_real_33_t *)dam,
-                             xam);
-  else if (stride == 6)
-    cs_matrix_wrapper_tensor(iconvp,
-                             idiffp,
-                             tensorial_diffusion,
-                             ndircp,
-                             isym,
-                             thetap,
-                             bc_coeffs,
-                             (const cs_real_66_t *)fimp,
-                             i_massflux,
-                             b_massflux,
-                             i_viscm,
-                             b_viscm,
-                             (cs_real_66_t *)dam,
-                             xam);
+  cs_matrix_compute_coeffs(a,
+                           f,
+                           iconvp,
+                           idiffp,
+                           tensorial_diffusion,
+                           ndircp,
+                           eb_size,
+                           thetap,
+                           bc_coeffs,
+                           fimp_r,
+                           i_massflux,
+                           b_massflux,
+                           i_viscm,
+                           b_viscm);
 
-  /* Precaution if diagonal is 0, which may happen is all surrounding cells
-   * are disabled
-   * If a whole line of the matrix is 0, the diagonal is set to 1 */
-  if (mq->has_disable_flag == 1) {
-    ctx.parallel_for(n_cells, [=] CS_F_HOST_DEVICE (cs_lnum_t c_id) {
-      for (cs_lnum_t i = 0; i < stride; i++)
-        if (cs_math_fabs(dam[c_id][i][i]) < DBL_MIN)
-          dam[c_id][i][i] += 1.;
-    });
-  }
-
-  /*  For steady computations, the diagonal is relaxed */
-  if (idtvar < 0) {
-    ctx.parallel_for(n_cells, [=] CS_F_HOST_DEVICE (cs_lnum_t c_id) {
-      for (cs_lnum_t i = 0; i < stride; i++) {
-        for (cs_lnum_t j = 0; j < stride; j++)
-          dam[c_id][i][j] /= relaxp;
-      }
-    });
-  }
+  CS_FREE_HD(_fimp_r);
 
   /*===========================================================================
    * Iterative process to handle non orthogonlaities (starting from the
@@ -658,14 +643,9 @@ _equation_iterative_solve_strided(int                   idtvar,
   }
   CS_FREE_HD(pvar_i);
 
-  cs_matrix_vector_native_multiply(symmetric,
-                                   db_size,
-                                   eb_size,
-                                   f_id,
-                                   (cs_real_t *)dam,
-                                   xam,
-                                   (cs_real_t *)w2,
-                                   (cs_real_t *)w1);
+  cs_matrix_vector_multiply(a,
+                            (cs_real_t *)w2,
+                            (cs_real_t *)w1);
 
   ctx.wait(); // matrix vector multiply uses the same stream as the ctx
 
@@ -681,20 +661,20 @@ _equation_iterative_solve_strided(int                   idtvar,
     int *c_disable_flag = mq->c_disable_flag;
 
     ctx.parallel_for(n_cells, [=] CS_F_HOST_DEVICE (cs_lnum_t c_id) {
-                                /* Remove contributions from penalized cells */
-                                for (cs_lnum_t i = 0; i < stride; i++)
-                                  w1[c_id][i] += smbrp[c_id][i];
+      /* Remove contributions from penalized cells */
+      for (cs_lnum_t i = 0; i < stride; i++)
+        w1[c_id][i] += smbrp[c_id][i];
 
-                                if (c_disable_flag[c_id] != 0)
-                                  for (cs_lnum_t i = 0; i < stride; i++)
-                                    w1[c_id][i] = 0.;
-                              });
+      if (c_disable_flag[c_id] != 0)
+        for (cs_lnum_t i = 0; i < stride; i++)
+          w1[c_id][i] = 0.;
+    });
   }
   else {
     ctx.parallel_for(n_cells, [=] CS_F_HOST_DEVICE (cs_lnum_t c_id) {
-                                for (cs_lnum_t i = 0; i < stride; i++)
-                                  w1[c_id][i] += smbrp[c_id][i];
-                              });
+      for (cs_lnum_t i = 0; i < stride; i++)
+        w1[c_id][i] += smbrp[c_id][i];
+    });
   }
 
   ctx.wait();
@@ -709,6 +689,8 @@ _equation_iterative_solve_strided(int                   idtvar,
 
   /* Warning: for Weight Matrix, one and only one sweep is done. */
   int nswmod = CS_MAX(eqp->nswrsm, 1);
+
+  cs_sles_t *sc = cs_sles_find_or_add(f_id, var_name);
 
   int isweep = 1;
 
@@ -750,13 +732,8 @@ _equation_iterative_solve_strided(int                   idtvar,
                                      xam,
                                      true);
 
-    cs_sles_solve_native(f_id,
-                         var_name,
-                         symmetric,
-                         db_size,
-                         eb_size,
-                         (cs_real_t *)dam,
-                         xam,
+    cs_sles_solve_ccc_fv(sc,
+                         a,
                          epsilp,
                          rnorm,
                          &niterf,
@@ -1117,7 +1094,8 @@ _equation_iterative_solve_strided(int                   idtvar,
    * Free solver setup
    *==========================================================================*/
 
-  cs_sles_free_native(f_id, var_name);
+  cs_sles_free(sc);
+  cs_matrix_release_coefficients(a);
 
   if (stride == 3) {
     /* Save diagonal in case we want to use it */
@@ -1322,7 +1300,7 @@ cs_equation_iterative_solve_scalar(int                   idtvar,
 
   int coupling_id = -1;
   cs_field_t *f = nullptr;
-  cs_real_t *dam = nullptr, *xam = nullptr, *smbini = nullptr;
+  cs_real_t *smbini = nullptr;
   cs_real_t *w1 = nullptr;
 
   bool conv_diff_mg = false;
@@ -1381,41 +1359,38 @@ cs_equation_iterative_solve_scalar(int                   idtvar,
    * 1.  Building of the "simplified" matrix
    *==========================================================================*/
 
-  CS_MALLOC_HD(dam, n_cells_ext, cs_real_t, cs_alloc_mode);
-  CS_MALLOC_HD(xam, isym*n_i_faces, cs_real_t, cs_alloc_mode);
+  cs_matrix_t *a = cs_sles_default_get_matrix(f_id, var_name, 1, 1, symmetric);
 
-  cs_matrix_wrapper_scalar(iconvp,
+  const cs_real_t *rovsdt_c = rovsdt;
+  cs_real_t *_rovsdt_c = nullptr;
+
+  /* For steady computations, the diagonal is relaxed */
+  if (idtvar < 0) {
+    CS_MALLOC_HD(_rovsdt_c, n_cells, cs_real_t, cs_alloc_mode);
+    cs_real_t rf = 1. / relaxp;
+    ctx.parallel_for(n_cells, [=] CS_F_HOST_DEVICE (cs_lnum_t cell_id) {
+      _rovsdt_c[cell_id] = rovsdt[cell_id] * rf;
+    });
+    ctx.wait();
+    rovsdt_c = _rovsdt_c;
+  }
+
+  cs_matrix_compute_coeffs(a,
+                           f,
+                           iconvp,
                            idiffp,
                            ndircp,
-                           isym,
                            thetap,
                            imucpp,
                            bc_coeffs,
-                           rovsdt,
+                           rovsdt_c,
                            i_massflux,
                            b_massflux,
                            i_viscm,
                            b_viscm,
-                           xcpp,
-                           dam,
-                           xam);
+                           xcpp);
 
-  /* Precaution if diagonal is 0, which may happen is all surrounding cells
-   * are disabled
-   * If a whole line of the matrix is 0, the diagonal is set to 1 */
-  if (mq->has_disable_flag == 1) {
-    ctx.parallel_for(n_cells, [=] CS_F_HOST_DEVICE (cs_lnum_t cell_id) {
-      if (CS_ABS(dam[cell_id]) < DBL_MIN)
-        dam[cell_id] += 1.;
-    });
-  }
-
-  /* For steady computations, the diagonal is relaxed */
-  if (idtvar < 0) {
-    ctx.parallel_for(n_cells, [=] CS_F_HOST_DEVICE (cs_lnum_t cell_id) {
-      dam[cell_id] /= relaxp;
-    });
-  }
+  CS_FREE_HD(_rovsdt_c);
 
   /*==========================================================================
    * 2. Iterative process to handle non orthogonalities (starting from the
@@ -1580,14 +1555,7 @@ cs_equation_iterative_solve_scalar(int                   idtvar,
 
     ctx.wait();
 
-    cs_matrix_vector_native_multiply(symmetric,
-                                     1,  /* db_size */
-                                     1,  /* eb_size */
-                                     f_id,
-                                     dam,
-                                     xam,
-                                     w2,
-                                     w1);
+    cs_matrix_vector_multiply(a, w2, w1);
 
     CS_FREE_HD(w2);
 
@@ -1620,6 +1588,8 @@ cs_equation_iterative_solve_scalar(int                   idtvar,
   if (iterns <= 1)
     sinfo.n_it = 0;
 
+  cs_sles_t *sc = cs_sles_find_or_add(f_id, var_name);
+
   int isweep = 1;
 
   while ((isweep <= nswmod && residu > epsrsp*rnorm) || isweep == 1) {
@@ -1641,24 +1611,17 @@ cs_equation_iterative_solve_scalar(int                   idtvar,
     /* Solver residual */
     ressol = residu;
 
-    if (conv_diff_mg)
-      cs_sles_setup_native_conv_diff(f_id,
-                                     var_name,
-                                     1,  /* db_size */
-                                     1,  /* eb_size */
-                                     dam,
-                                     xam,
-                                     true);
+    if (conv_diff_mg) {
+      cs_multigrid_t *mg
+        = static_cast<cs_multigrid_t *>(cs_sles_get_context(sc));
+      cs_multigrid_setup_conv_diff(mg, var_name, a, true,
+                                   cs_sles_get_verbosity(sc));
+    }
 
     ctx.wait(); /* We now need dpvar, computed by ctx */
 
-    cs_sles_solve_native(f_id,
-                         var_name,
-                         symmetric,
-                         1,  /* db_size */
-                         1,  /* eb_size */
-                         dam,
-                         xam,
+    cs_sles_solve_ccc_fv(sc,
+                         a,
                          epsilp,
                          rnorm,
                          &niterf,
@@ -2051,13 +2014,12 @@ cs_equation_iterative_solve_scalar(int                   idtvar,
    * 4. Free solver setup
    *==========================================================================*/
 
-  cs_sles_free_native(f_id, var_name);
+  cs_sles_free(sc);
+  cs_matrix_release_coefficients(a);
 
   ctx.wait();
 
   /*  Free memory */
-  CS_FREE_HD(dam);
-  CS_FREE_HD(xam);
   CS_FREE_HD(smbini);
 
   if (iswdyp >= 1) {

src/alge/cs_gradient_cuda.cu
@@ -1157,7 +1157,6 @@ _gg_gradient_rescale(cs_lnum_t                       n_cells,
     grad[c_id][i][j] *= dvol;
   }
 
-
   if (warped_correction) {
     cs_real_t gradpa[3];
     for (cs_lnum_t j = 0; j < 3; j++) {

src/alge/cs_matrix.cpp
@@ -4003,6 +4003,7 @@ _matrix_create(cs_matrix_type_t  type,
               m->type);
 
   m->xa = nullptr;
+  m->_xa = nullptr;
 
   m->c2f_idx = nullptr;
   m->c2f = nullptr;
@@ -5126,7 +5127,7 @@ cs_matrix_transfer_coefficients(cs_matrix_t         *matrix,
                              *x_val);
 
   if (matrix->type < CS_MATRIX_N_BUILTIN_TYPES) {
-    matrix->xa = nullptr;
+    matrix->xa = (matrix->need_xa) ? *x_val : nullptr;
     auto mc = static_cast<cs_matrix_coeff_t *>(matrix->coeffs);
     if (   mc->d_val == *d_val
         && mc->_d_val == nullptr) {
@@ -5137,6 +5138,12 @@ cs_matrix_transfer_coefficients(cs_matrix_t         *matrix,
         && mc->_e_val == nullptr) {
       mc->_e_val = *x_val;
       *x_val = nullptr;
+    }
+    else {
+      if (matrix->need_xa)
+        matrix->_xa = *x_val;
+      else
+        CS_FREE(*x_val);
     }
   }
 }
@@ -5318,6 +5325,7 @@ cs_matrix_release_coefficients(cs_matrix_t  *matrix)
 
   if (matrix->release_coefficients != nullptr) {
     matrix->xa = nullptr;
+    BFT_FREE(matrix->_xa);
     matrix->release_coefficients(matrix);
   }
   else {

src/alge/cs_matrix_building.h
@@ -40,8 +40,6 @@
 
 /*----------------------------------------------------------------------------*/
 
-BEGIN_C_DECLS
-
 /*=============================================================================
  * Local Macro definitions
  *============================================================================*/
@@ -58,12 +56,15 @@ BEGIN_C_DECLS
  * Public function prototypes
  *============================================================================*/
 
+#if defined(__cplusplus)
+
 /*----------------------------------------------------------------------------*/
 /*
  * \brief Build the diagonal of the advection/diffusion matrix
  * for determining the variable time step, flow, Fourier.
  *
  * \param[in, out]  a             pointer to matrix structure
+ * \param[in]       f             pointer to field, or null
  * \param[in]       iconvp        indicator
  *                                 - 1 advection
  *                                 - 0 otherwise
@@ -74,96 +75,132 @@ BEGIN_C_DECLS
  * \param[in]       thetap        time scheme parameter
  * \param[in]       imucp         1 for temperature (with Cp), 0 otherwise
  * \param[in]       bc_coeffs     boundary condition structure
+ * \param[in]       rovsdt        implicit terms (rho / dt)
  * \param[in]       i_massflux    mass flux at interior faces
  * \param[in]       b_massflux    mass flux at border faces
  * \param[in]       i_visc        \f$ \mu_\fij \dfrac{S_\fij}{\ipf \jpf} \f$
  *                                 at interior faces for the matrix
  * \param[in]       b_visc        \f$ S_\fib \f$
  *                                 at border faces for the matrix
- * \param[in]      xcpp           Cp per cell, or null
+ * \param[in]       xcpp          Cp per cell, or null
  */
 /*----------------------------------------------------------------------------*/
 
 void
-cs_matrix_compute_coeffs_scalar(cs_matrix_t                *a,
-                                const cs_field_t           *f,
-                                int                         iconvp,
-                                int                         idiffp,
-                                int                         ndircp,
-                                double                      thetap,
-                                int                         imucpp,
-                                const cs_field_bc_coeffs_t *bc_coeffs,
-                                const cs_real_t             rovsdt[],
-                                const cs_real_t             i_massflux[],
-                                const cs_real_t             b_massflux[],
-                                const cs_real_t             i_visc[],
-                                const cs_real_t             b_visc[],
-                                const cs_real_t             xcpp[]);
+cs_matrix_compute_coeffs(cs_matrix_t                 *a,
+                         const cs_field_t            *f,
+                         int                          iconvp,
+                         int                          idiffp,
+                         int                          ndircp,
+                         double                       thetap,
+                         int                          imucpp,
+                         const cs_field_bc_coeffs_t  *bc_coeffs,
+                         const cs_real_t              rovsdt[],
+                         const cs_real_t              i_massflux[],
+                         const cs_real_t              b_massflux[],
+                         const cs_real_t              i_visc[],
+                         const cs_real_t              b_visc[],
+                         const cs_real_t              xcpp[]);
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Build the diagonal of the advection/diffusion matrix
+ * for determining the variable time step, flow, Fourier.
+ *
+ * \tparam  stride 3 for vectors, 6 for tensors
+ *
+ * \param[in, out]  a                   pointer to matrix structure
+ * \param[in]       f                    pointer to field, or null
+ * \param[in]       iconvp               indicator
+ *                                         - 1 advection
+ *                                         - 0 otherwise
+ * \param[in]       idiffp               indicator
+ *                                         - 1 diffusion
+ *                                         - 0 otherwise
+ * \param[in]       tensorial_diffusion  indicator
+ * \param[in]       ndircp               number of Dirichlet BCs
+ * \param[in]       thetap               time scheme parameter
+ * \param[in]       eb_size              extra-diagonal block size
+ *                                       (1 or 3 for stride 3, 1 for stride 6)
+ * \param[in]       bc_coeffs            boundary conditions structure
+ * \param[in]       fimp                 implicit terms, or null
+ * \param[in]       i_massflux           mass flux at interior faces
+ * \param[in]       b_massflux           mass flux at border faces
+ * \param[in]       i_visc        \f$ \mu_\fij \dfrac{S_\fij}{\ipf \jpf} \f$
+ *                                 at interior faces for the matrix
+ * \param[in]       b_visc        \f$ S_\fib \f$
+ *                                 at boundary faces for the matrix
+ */
+/*----------------------------------------------------------------------------*/
+
+template <cs_lnum_t stride>
+void
+cs_matrix_compute_coeffs
+(
+  cs_matrix_t                 *a,
+  const cs_field_t            *f,
+  int                          iconvp,
+  int                          idiffp,
+  int                          tensorial_diffusion,
+  int                          ndircp,
+  cs_lnum_t                    eb_size,
+  double                       thetap,
+  const cs_field_bc_coeffs_t  *bc_coeffs,
+  const cs_real_t              fimp[][stride][stride],
+  const cs_real_t              i_massflux[],
+  const cs_real_t              b_massflux[],
+  const cs_real_t              i_visc[],
+  const cs_real_t              b_visc[]
+);
 
 /*----------------------------------------------------------------------------
- * Wrapper to cs_matrix_scalar (or its counterpart for
- * symmetric matrices)
+ * Compute legacy matrix coefficients
  *----------------------------------------------------------------------------*/
 
 void
-cs_matrix_wrapper_scalar(int                         iconvp,
-                         int                         idiffp,
-                         int                         ndircp,
-                         int                         isym,
-                         double                      thetap,
-                         int                         imucpp,
-                         const cs_field_bc_coeffs_t *bc_coeffs,
-                         const cs_real_t             rovsdt[],
-                         const cs_real_t             i_massflux[],
-                         const cs_real_t             b_massflux[],
-                         const cs_real_t             i_visc[],
-                         const cs_real_t             b_visc[],
-                         const cs_real_t             xcpp[],
-                         cs_real_t                   da[],
-                         cs_real_t                   xa[]);
+cs_matrix_wrapper(int                         iconvp,
+                  int                         idiffp,
+                  int                         ndircp,
+                  int                         isym,
+                  double                      thetap,
+                  int                         imucpp,
+                  const cs_field_bc_coeffs_t *bc_coeffs,
+                  const cs_real_t             rovsdt[],
+                  const cs_real_t             i_massflux[],
+                  const cs_real_t             b_massflux[],
+                  const cs_real_t             i_visc[],
+                  const cs_real_t             b_visc[],
+                  const cs_real_t             xcpp[],
+                  cs_real_t                   da[],
+                  cs_real_t                   xa[]);
 
 /*----------------------------------------------------------------------------
- * Wrapper to cs_matrix_vector (or its counterpart for
- * symmetric matrices)
+ * Compute legacy matrix coefficients
  *----------------------------------------------------------------------------*/
 
+template <cs_lnum_t stride>
 void
-cs_matrix_wrapper_vector(int                         iconvp,
-                         int                         idiffp,
-                         int                         tensorial_diffusion,
-                         int                         ndircp,
-                         int                         isym,
-                         cs_lnum_t                   eb_size,
-                         double                      thetap,
-                         const cs_field_bc_coeffs_t *bc_coeffs_v,
-                         const cs_real_t             fimp[][3][3],
-                         const cs_real_t             i_massflux[],
-                         const cs_real_t             b_massflux[],
-                         const cs_real_t             i_visc[],
-                         const cs_real_t             b_visc[],
-                         cs_real_t                   da[][3][3],
-                         cs_real_t                   xa[]);
+cs_matrix_wrapper(int                         iconvp,
+                  int                         idiffp,
+                  int                         tensorial_diffusion,
+                  int                         ndircp,
+                  int                         isym,
+                  cs_lnum_t                   eb_size,
+                  double                      thetap,
+                  const cs_field_bc_coeffs_t *bc_coeffs_v,
+                  const cs_real_t             fimp[][stride][stride],
+                  const cs_real_t             i_massflux[],
+                  const cs_real_t             b_massflux[],
+                  const cs_real_t             i_visc[],
+                  const cs_real_t             b_visc[],
+                  cs_real_t                   da[][stride][stride],
+                  cs_real_t                   xa[]);
 
-/*----------------------------------------------------------------------------
- * Wrapper to cs_matrix_tensor (or its counterpart for
- * symmetric matrices)
- *----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
 
-void
-cs_matrix_wrapper_tensor(int                         iconvp,
-                         int                         idiffp,
-                         int                         tensorial_diffusion,
-                         int                         ndircp,
-                         int                         isym,
-                         double                      thetap,
-                         const cs_field_bc_coeffs_t *bc_coeffs_ts,
-                         const cs_real_66_t          fimp[],
-                         const cs_real_t             i_massflux[],
-                         const cs_real_t             b_massflux[],
-                         const cs_real_t             i_visc[],
-                         const cs_real_t             b_visc[],
-                         cs_real_66_t                da[],
-                         cs_real_t                   xa[]);
+#endif //defined(__cplusplus)
+
+BEGIN_C_DECLS
 
 /*----------------------------------------------------------------------------*/
 /*

src/alge/cs_matrix_priv.h
@@ -317,6 +317,7 @@ struct _cs_matrix_t {
      code first. */
 
   const cs_real_t       *xa;           /* Extra-diagonal terms */
+  cs_real_t             *_xa;          /* Extra-diagonal terms, private copy */
 
   /* Pointer to associated connectivity and geometric data needed by
      multigrid smoothing. At least cell centers and volumes are needed for

src/alge/cs_sles_it_priv.cpp
@@ -30,6 +30,8 @@
  * Standard C library headers
  *----------------------------------------------------------------------------*/
 
+#include <chrono>
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -288,6 +290,10 @@ cs_sles_it_setup_priv(cs_sles_it_t       *c,
 
       sd->ad_inv = sd->_ad_inv;
 
+      std::chrono::high_resolution_clock::time_point t_start;
+      if (cs_glob_timer_kernels_flag > 0)
+        t_start = std::chrono::high_resolution_clock::now();
+
       const cs_real_t  *restrict ad = cs_matrix_get_diagonal(a);
 
       cs_dispatch_context  ctx;
@@ -323,6 +329,17 @@ cs_sles_it_setup_priv(cs_sles_it_t       *c,
       ctx.wait();
       if (c->on_device && ctx.use_gpu() == false)
         cs_sync_h2d(sd->_ad_inv);
+
+      if (cs_glob_timer_kernels_flag > 0) {
+        std::chrono::high_resolution_clock::time_point
+          t_stop = std::chrono::high_resolution_clock::now();
+        std::chrono::microseconds elapsed
+          = std::chrono::duration_cast
+          <std::chrono::microseconds>(t_stop - t_start);
+        printf("%d: %s (fact lu, %d)", cs_glob_rank_id, __func__,
+               diag_block_size);
+        printf(", total = %ld\n", elapsed.count());
+      }
 
     }
 

src/alge/cs_sles_petsc.cpp
@@ -1197,7 +1197,7 @@ cs_sles_petsc_setup(void               *context,
      * case, but we try to keep the compiler happy. */
 
     cs_lnum_t *row_index = const_cast<cs_lnum_t *>(a_row_index);
-    cs_lnum_t *col_id    = const_cast<cs_lnum_t *>(col_id);
+    cs_lnum_t *col_id    = const_cast<cs_lnum_t *>(a_col_id);
 
     /* Matrix */
 

commit 37218ac41b3b82dbca1d691a42a823b5ae6f5aac

Fix crash when using vp_param->rcfact = 1, broken by 491bcb3e (2024-11-23).

src/base/cs_equation_iterative_solve.cpp
@@ -358,9 +358,7 @@ _equation_iterative_solve_strided(int                   idtvar,
   cs_alloc_mode_t amode = ctx.alloc_mode(true);
 
   /* Allocate temporary arrays */
-  m_t *dam;
   var_t *smbini, *dpvar;
-  CS_MALLOC_HD(dam, n_cells_ext, m_t, amode);
   CS_MALLOC_HD(smbini, n_cells_ext, var_t, amode);
   CS_MALLOC_HD(dpvar, n_cells_ext, var_t, amode);
 
@@ -1282,15 +1280,11 @@ _equation_iterative_solve_strided(int                   idtvar,
      val_f_updated, val_f_lim_updated,
      val_f_d_updated, val_f_d_lim_updated);
 
-  /*==========================================================================
-   * Free solver setup
-   *==========================================================================*/
-
-  cs_sles_free(sc);
-  cs_matrix_release(&a);
+  /* Save diagonal in case we want to use it */
 
   if (stride == 3) {
-    /* Save diagonal in case we want to use it */
+    const m_t *dam = reinterpret_cast<const m_t *>(cs_matrix_get_diagonal(a));
+
     ctx.parallel_for(n_cells, [=] CS_F_HOST_DEVICE (cs_lnum_t c_id) {
       for (cs_lnum_t i = 0; i < stride; i++)
         for (cs_lnum_t j = 0; j < stride; j++)
@@ -1300,10 +1294,16 @@ _equation_iterative_solve_strided(int                   idtvar,
     ctx.wait();
   }
 
+  /*==========================================================================
+   * Free solver setup
+   *==========================================================================*/
+
+  cs_sles_free(sc);
+  cs_matrix_release(&a);
+
   ctx.wait();
 
   /* Free memory */
-  CS_FREE_HD(dam);
   CS_FREE_HD(smbini);
   CS_FREE_HD(dpvar);
   if (iswdyp >= 1) {