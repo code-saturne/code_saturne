9bd2910f16dd0da1170f88e48f8af3e95e7c6f14
Yvan Fournier <yvan.fournier@noreply.edf.fr>
2025-05-28 11:38:30 +0200

    Fix error message format.

--- src/alge/cs_saddle_solver_setup.cpp
+++ src/alge/cs_saddle_solver_setup.cpp
@@ -732,7 +732,7 @@ _notay_transformation_setup(cs_saddle_solver_t  *solver,
                          (void *)solver); /* context structure for PETSc */
 #else
     bft_error(__FILE__, __LINE__, 0,
-              "%s: PETSc is required for solving \"%s\"\n",
+              "%s: PETSc is required for solving \"%s\"\n"
               " Please modify your settings/build code_saturne with PETSc.",
               __func__, saddlep->name);
 #endif  /* HAVE PETSC */
