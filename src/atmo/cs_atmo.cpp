code-saturne/code_saturne

commit 8353abe92ee3176dd5c8f868268a394ba275eab5

Switch to unit face normals in various specific models.

src/atmo/cs_atmo.cpp
@@ -4391,18 +4391,16 @@ cs_atmo_z_ground_compute(void)
   const cs_mesh_quantities_t *mq_g = cs_glob_mesh_quantities_g;
   const cs_lnum_t n_cells = m->n_cells;
 
-  const cs_real_3_t *restrict i_face_normal
-    = (const cs_real_3_t *)mq->i_face_normal;
-  const cs_real_3_t *restrict b_face_normal
-    = (const cs_real_3_t *)mq->b_face_normal;
+  const cs_nreal_3_t *restrict i_face_u_normal = mq_g->i_face_u_normal;
+  const cs_nreal_3_t *restrict b_face_u_normal = mq_g->b_face_u_normal;
   const cs_real_3_t *restrict b_face_cog = mq->b_face_cog;
 
   const int *bc_type = cs_glob_bc_type;
 
   const cs_real_t *restrict i_face_surf = mq_g->i_face_surf;
   const cs_real_3_t *restrict i_face_cog = mq_g->i_face_cog;
-  const cs_real_3_t *restrict i_face_normal_g
-    = (const cs_real_3_t *)mq_g->i_face_normal;
+  const cs_real_t *restrict i_face_surf_g = mq_g->i_face_surf;
+  const cs_real_t *restrict b_face_surf = mq_g->b_face_surf;
   const cs_mesh_adjacencies_t *ma = cs_glob_mesh_adjacencies;
   const cs_lnum_t *c2c = ma->cell_cells;
   const cs_lnum_t *c2c_idx = ma->cell_cells_idx;
@@ -4432,11 +4430,17 @@ cs_atmo_z_ground_compute(void)
   /* Compute the mass flux due to V = - g / ||g||
    * ============================================ */
 
-  for (cs_lnum_t face_id = 0; face_id < m->n_i_faces; face_id++)
-    i_massflux[face_id] = cs_math_3_dot_product(normal, i_face_normal[face_id]);
+  for (cs_lnum_t face_id = 0; face_id < m->n_i_faces; face_id++) {
+    i_massflux[face_id] =   cs_math_3_dot_product(normal,
+                                                  i_face_u_normal[face_id])
+                          * i_face_surf[face_id];
+  }
 
-  for (cs_lnum_t face_id = 0; face_id < m->n_b_faces; face_id++)
-    b_massflux[face_id] = cs_math_3_dot_product(normal, b_face_normal[face_id]);
+  for (cs_lnum_t face_id = 0; face_id < m->n_b_faces; face_id++) {
+    b_massflux[face_id] =   cs_math_3_dot_product(normal,
+                                                  b_face_u_normal[face_id])
+                          * b_face_surf[face_id];
+  }
 
   /* Boundary conditions
    * =================== */
@@ -4525,8 +4529,10 @@ cs_atmo_z_ground_compute(void)
         eqp_p->ndircl = 1;
         cs_real_t pimp = cs_math_3_dot_product(i_face_cog[face_id], normal);
 
-        cs_real_t tsimp
-          = fmax(- sign * cs_math_3_dot_product(i_face_normal_g[face_id], normal), 0.);
+        cs_real_t dot_ng
+          = cs_math_3_dot_product(i_face_u_normal[face_id], normal);
+        cs_real_t tsimp =   cs::max(- sign * dot_ng, 0.)
+                          * i_face_surf_g[face_id];
 
         rhs[c_id] += tsimp * (pimp - f->val[c_id]); //explicit term
         rovsdt[c_id] += cs::max(tsimp, 0.); //implicit term

src/base/cs_vof.cpp
@@ -555,7 +555,7 @@ _contact_angle_correction
 
   /* Get values or local pointers */
   const cs_lnum_t *b_face_cells = m->b_face_cells;
-  cs_real_3_t *b_face_u_normal = mq->b_face_u_normal;
+  cs_nreal_3_t *b_face_u_normal = mq->b_face_u_normal;
   cs_real_t *restrict volume = mq->cell_vol;
 
   const cs_real_t cpro_surftens = _vof_parameters.sigma_s;
@@ -1028,10 +1028,10 @@ cs_vof_log_mass_budget(const cs_mesh_t             *m,
   const cs_real_3_t *restrict i_face_cog = mq->i_face_cog;
   const cs_real_3_t *restrict b_face_cog = mq->b_face_cog;
 
-  const cs_real_3_t *restrict i_f_face_normal
-    = (const cs_real_3_t *)mq->i_face_normal;
-  const cs_real_3_t *restrict b_f_face_normal
-    = (const cs_real_3_t *)mq->b_face_normal;
+  const cs_real_t *restrict i_face_surf = mq->i_face_surf;
+  const cs_nreal_3_t *restrict i_face_u_normal = mq->i_face_u_normal;
+  const cs_real_t *restrict b_face_surf = mq->b_face_surf;
+  const cs_nreal_3_t *restrict b_face_u_normal = mq->b_face_u_normal;
 
   const int kimasf = cs_field_key_id("inner_mass_flux_id");
   const int kbmasf = cs_field_key_id("boundary_mass_flux_id");
@@ -1096,12 +1096,13 @@ cs_vof_log_mass_budget(const cs_mesh_t             *m,
                              i_face_cog[f_id],
                              vr2);
 
-        cs_real_t vr[3] = {0.5 * (vr1[0] + vr2[0]),
-                           0.5 * (vr1[1] + vr2[1]),
-                           0.5 * (vr1[2] + vr2[2])};
+        cs_real_t vr[3] = {(vr1[0] + vr2[0]),
+                           (vr1[1] + vr2[1]),
+                           (vr1[2] + vr2[2])};
 
         i_massflux_abs[f_id]
-          += rhofac * cs_math_3_dot_product(i_f_face_normal[f_id],vr);
+          += rhofac * 0.5 * cs_math_3_dot_product(i_face_u_normal[f_id], vr)
+                          * i_face_surf[f_id];
       }
     });
 
@@ -1117,7 +1118,8 @@ cs_vof_log_mass_budget(const cs_mesh_t             *m,
         cs_rotation_velocity(r + rot_ce_i, b_face_cog[f_id], vr);
 
         b_massflux_abs[f_id]
-          += bpro_rom[f_id] * cs_math_3_dot_product(b_f_face_normal[f_id], vr);
+          += bpro_rom[f_id] * cs_math_3_dot_product(b_face_u_normal[f_id], vr)
+                            * b_face_surf[f_id];
 
       }
     });

src/base/cs_wall_condensation.cpp
@@ -298,14 +298,13 @@ _compute_characteristic_length(const cs_real_t  point[3],
 /*----------------------------------------------------------------------------*/
 
 static cs_real_t
-_compute_tangential_velocity(const cs_real_t  velocity[3],
-                             const cs_real_t  normal_vector[3],
-                             const cs_real_t  coeff)
+_compute_tangential_velocity(const cs_real_t   velocity[3],
+                             const cs_nreal_t  u_normal_vector[3])
 {
   cs_real_t u_square = 0.0;
   cs_real_t u_normal = 0.0;
   for (int idir = 0; idir < 3; idir++) {
-    u_normal += velocity[idir] * normal_vector[idir] * coeff;
+    u_normal += velocity[idir] * u_normal_vector[idir];
     u_square += velocity[idir] * velocity[idir];
   }
   return sqrt(u_square - u_normal * u_normal);
@@ -368,9 +367,8 @@ _compute_exchange_forced_convection(cs_lnum_t   ieltcd,
   case CS_WALL_COND_MODEL_SCHLICHTING:
     {
       cs_real_3_t *cdgfbo = (cs_real_3_t *)cs_glob_mesh_quantities->b_face_cog;
-      const cs_real_3_t *b_face_normal
-        = (const cs_real_3_t *)cs_glob_mesh_quantities->b_face_normal;
-      const cs_real_t *  surfbn = cs_glob_mesh_quantities->b_face_surf;
+      const cs_nreal_3_t *b_face_u_normal
+        = cs_glob_mesh_quantities->b_face_u_normal;
       const cs_real_3_t *velocity
         = (cs_real_3_t *)cs_field_by_name("velocity")->val;
       const cs_real_3_t *n_ref = (cs_real_3_t *)_wall_cond.zprojcond;
@@ -380,8 +378,7 @@ _compute_exchange_forced_convection(cs_lnum_t   ieltcd,
       const cs_real_t lcar
         = _compute_characteristic_length(cdgfbo[ifac], x_ref[iz], n_ref_norm);
       const cs_real_t u_ref
-        = _compute_tangential_velocity(velocity[iel], b_face_normal[ifac],
-                                       1. / surfbn[ifac]);
+        = _compute_tangential_velocity(velocity[iel], b_face_u_normal[ifac]);
       // Reynolds number
       const cs_real_t re    = rho * u_ref * lcar / dyn_visc;
       cs_real_t       theta = 1.0;

src/cfbl/cs_cf_energy.cpp
@@ -115,7 +115,6 @@ BEGIN_C_DECLS
 static void
 _cf_div(cs_real_t div[])
 {
-
   const cs_mesh_t *mesh = cs_glob_mesh;
   const cs_mesh_quantities_t *fvq = cs_glob_mesh_quantities;
   const cs_halo_t *halo = mesh->halo;
@@ -126,10 +125,10 @@ _cf_div(cs_real_t div[])
 
   const cs_lnum_t *b_face_cells = mesh->b_face_cells;
   const cs_lnum_2_t *i_face_cells = mesh->i_face_cells;
-  const cs_real_3_t *i_f_face_normal
-    = (const cs_real_3_t *)fvq->i_face_normal;
-  const cs_real_3_t *b_f_face_normal
-    = (const cs_real_3_t *)fvq->b_face_normal;
+  const cs_real_t *i_face_surf = fvq->i_face_surf;
+  const cs_real_t *b_face_surf = fvq->b_face_surf;
+  const cs_nreal_3_t *i_face_u_normal = fvq->i_face_u_normal;
+  const cs_nreal_3_t *b_face_u_normal = fvq->b_face_u_normal;
 
   const int itytur = cs_glob_turb_model->itytur;
 
@@ -257,9 +256,10 @@ _cf_div(cs_real_t div[])
     const cs_lnum_t c_id1 = i_face_cells[f_id][1];
 
     const cs_real_t vecfac
-      =   0.5*i_f_face_normal[f_id][0]*(tempv[c_id0][0]+tempv[c_id1][0])
-        + 0.5*i_f_face_normal[f_id][1]*(tempv[c_id0][1]+tempv[c_id1][1])
-        + 0.5*i_f_face_normal[f_id][2]*(tempv[c_id0][2]+tempv[c_id1][2]);
+      =   0.5 * i_face_surf[f_id]
+          * (  i_face_u_normal[f_id][0]*(tempv[c_id0][0]+tempv[c_id1][0])
+             + i_face_u_normal[f_id][1]*(tempv[c_id0][1]+tempv[c_id1][1])
+             + i_face_u_normal[f_id][2]*(tempv[c_id0][2]+tempv[c_id1][2]));
 
     div[c_id0] = div[c_id0] + vecfac;
     div[c_id1] = div[c_id1] - vecfac;
@@ -270,9 +270,10 @@ _cf_div(cs_real_t div[])
   for (cs_lnum_t f_id = 0; f_id < n_b_faces; f_id++) {
     const cs_lnum_t c_id = b_face_cells[f_id];
 
-    const cs_real_t vecfac =   b_f_face_normal[f_id][0] * tempv[c_id][0]
-                             + b_f_face_normal[f_id][1] * tempv[c_id][1]
-                             + b_f_face_normal[f_id][2] * tempv[c_id][1];
+    const cs_real_t vecfac
+      = b_face_surf[f_id] * (  b_face_u_normal[f_id][0] * tempv[c_id][0]
+                             + b_face_u_normal[f_id][1] * tempv[c_id][1]
+                             + b_face_u_normal[f_id][2] * tempv[c_id][1]);
 
     div[c_id] = div[c_id] + vecfac;
   }

src/elec/cs_elec_model.cpp
@@ -2028,7 +2028,8 @@ cs_elec_scaling_function(const cs_mesh_t             *mesh,
                          cs_real_t                   *dt)
 {
   cs_real_t *volume = mesh_quantities->cell_vol;
-  cs_real_t *surfac = mesh_quantities->i_face_normal;
+  const cs_real_t *surf = mesh_quantities->i_face_surf;
+  const cs_nreal_3_t *u_normal = mesh_quantities->i_face_u_normal;
   cs_lnum_t  n_cells   = mesh->n_cells;
   cs_lnum_t  nfac   = mesh->n_i_faces;
 
@@ -2067,18 +2068,19 @@ cs_elec_scaling_function(const cs_mesh_t             *mesh,
       if (mesh->halo != nullptr)
         cs_halo_sync_var_strided(mesh->halo, CS_HALO_STANDARD,
                                  (cs_real_t *)cpro_curre, 3);
+      cs_lnum_t idreca_idx = cs_glob_elec_option->idreca - 1;
       for (cs_lnum_t ifac = 0; ifac < nfac; ifac++) {
         if (cs_glob_elec_option->izreca[ifac] > 0) {
           bool ok = true;
           for (int idir = 0; idir < 3; idir++)
-            if (   fabs(surfac[3 * ifac + idir]) > 0.
-                && idir != (cs_glob_elec_option->idreca - 1))
+            if (   fabs(u_normal[ifac][idir]) > 0.
+                && idir != idreca_idx)
               ok = false;
           if (ok) {
             cs_lnum_t iel = mesh->i_face_cells[ifac][0];
             if (iel < mesh->n_cells)
-              elcou += cpro_curre[iel][cs_glob_elec_option->idreca - 1]
-                     * surfac[3 * ifac + cs_glob_elec_option->idreca - 1];
+              elcou +=   cpro_curre[iel][idreca_idx]
+                       * surf[ifac] * u_normal[ifac][idreca_idx];
           }
         }
       }

src/lagr/cs_lagr_geom.cpp
@@ -106,39 +106,37 @@ cs_lagr_geom(void)
 {
   cs_mesh_t            *mesh = cs_glob_mesh;
 
-  const cs_real_3_t *restrict b_face_normal
-    = (const cs_real_3_t *)cs_glob_mesh_quantities->b_face_normal;
+  const cs_nreal_3_t *restrict b_face_u_normal
+    = cs_glob_mesh_quantities->b_face_u_normal;
   const cs_real_3_t *vtx_coord
     = (const cs_real_3_t *)(cs_glob_mesh->vtx_coord);
 
   CS_REALLOC(cs_glob_lagr_b_face_proj, mesh->n_b_faces, cs_real_33_t);
 
-/* ==============================================================================*/
-/* 1. The reference frame chang matrix is:
- *
- *  ( nx  ny  nz )
- *  ( t1x t1y t1z)
- *  ( t2x t2y t2z)
- *
- *  With n, t1, t2 three unit orthogonal vectors and n the outwrding normal to the
- *  boundary face.
- * */
-/* ==============================================================================*/
+  /*==========================================================================*/
+  /* 1. The reference frame change matrix is:
+   *
+   *  ( nx  ny  nz )
+   *  ( t1x t1y t1z)
+   *  ( t2x t2y t2z)
+   *
+   *  With n, t1, t2 three unit orthogonal vectors
+   *  and n the outward normal to the boundary face.
+   */
+  /*==========================================================================*/
 
   for (cs_lnum_t face_id = 0; face_id < mesh->n_b_faces; face_id++) {
 
     /* normal vector coordinates */
-    cs_real_3_t normal;
-    cs_math_3_normalize(b_face_normal[face_id], normal);
+    const cs_nreal_t *normal = b_face_u_normal[face_id];
 
     /* Recover the first face nodes */
     cs_lnum_t v_id0  = mesh->b_face_vtx_lst[mesh->b_face_vtx_idx[face_id]];
     cs_lnum_t v_id1  = mesh->b_face_vtx_lst[mesh->b_face_vtx_idx[face_id] + 1];
 
-    cs_real_3_t v0v1 = {
-      vtx_coord[v_id1][0] - vtx_coord[v_id0][0],
-      vtx_coord[v_id1][1] - vtx_coord[v_id0][1],
-      vtx_coord[v_id1][2] - vtx_coord[v_id0][2]};
+    cs_real_3_t v0v1 = {vtx_coord[v_id1][0] - vtx_coord[v_id0][0],
+                        vtx_coord[v_id1][1] - vtx_coord[v_id0][1],
+                        vtx_coord[v_id1][2] - vtx_coord[v_id0][2]};
 
     /* tangential projection to the wall:
      * (Id -n (x) n) vect */

src/lagr/cs_lagr_new.cpp
@@ -461,24 +461,26 @@ cs_lagr_new_v(cs_lagr_particle_set_t  *particles,
 
       cs_lnum_t face_id, n_vertices;
       const cs_lnum_t *vertex_ids;
-      const cs_real_t *face_cog, *face_normal;
+      const cs_real_t *face_cog;
+      const cs_nreal_t *face_u_normal;
 
       /* Outward normal: always well oriented for external faces,
          depend on the connectivity for internal faces */
 
-      cs_real_t v_mult = 1;
+      cs_real_t sgn_face_surf = 1;
 
       if (i < n_cell_i_faces) { /* Interior face */
 
         face_id = ma->cell_i_faces[ma->cell_cells_idx[cell_id] + i];
 
+        sgn_face_surf = fvq->i_face_surf[face_id];
         if (cell_id == mesh->i_face_cells[face_id][1])
-          v_mult = -1;
+          sgn_face_surf *= -1;
         cs_lnum_t vtx_s = mesh->i_face_vtx_idx[face_id];
         n_vertices = mesh->i_face_vtx_idx[face_id+1] - vtx_s;
         vertex_ids = mesh->i_face_vtx_lst + vtx_s;
         face_cog = fvq->i_face_cog[face_id];
-        face_normal = fvq->i_face_normal + (3*face_id);
+        face_u_normal = fvq->i_face_u_normal[face_id];
 
       }
       else { /* Boundary faces */
@@ -493,11 +495,12 @@ cs_lagr_new_v(cs_lagr_particle_set_t  *particles,
           face_id = ma->cell_hb_faces[ma->cell_hb_faces_idx[cell_id] + j];
         }
 
+        sgn_face_surf = fvq->b_face_surf[face_id];
         cs_lnum_t vtx_s = mesh->b_face_vtx_idx[face_id];
         n_vertices = mesh->b_face_vtx_idx[face_id+1] - vtx_s;
         vertex_ids = mesh->b_face_vtx_lst + vtx_s;
         face_cog = fvq->b_face_cog[face_id];
-        face_normal = fvq->b_face_normal + (3*face_id);
+        face_u_normal = fvq->b_face_u_normal[face_id];
 
       }
 
@@ -517,11 +520,10 @@ cs_lagr_new_v(cs_lagr_particle_set_t  *particles,
 
       cs_real_t fh = 0;
       if (f_surf > 0) {
-        /* face normal should have length f_surf, so no need to divide here */
         for (cs_lnum_t j = 0; j < 3; j++)
-          fh += (face_cog[j] - cell_cen[j]) * face_normal[j];
+          fh += (face_cog[j] - cell_cen[j]) * face_u_normal[j];
       }
-      fh *= v_mult;
+      fh *= sgn_face_surf;
 
       t_vol += cs::abs(fh);
       acc_vol_r[i] = t_vol;

src/lagr/cs_lagr_prototypes.h
@@ -373,7 +373,7 @@ cs_user_lagr_volume_conditions(void);
  * \param[in, out]  particles       pointer to particle set
  * \param[in]       p_id            particle id
  * \param[in]       face_id         interior face id
- * \param[in]       face_norm       unit face (or face subdivision) normal
+ * \param[in]       face_u_norm     unit face (or face subdivision) normal
  * \param[in]       c_intersect     coordinates of intersection with the face
  * \param[in]       t_intersect     relative distance (in [0, 1]) of the
  *                                  intersection point with the face relative
@@ -386,7 +386,7 @@ void
 cs_lagr_user_internal_interaction(cs_lagr_particle_set_t    *particles,
                                   cs_lnum_t                  p_id,
                                   cs_lnum_t                  face_id,
-                                  const cs_real_t            face_norm[3],
+                                  const cs_nreal_t           face_u_norm[3],
                                   const cs_real_t            c_intersect[3],
                                   cs_real_t                  t_intersect,
                                   cs_lagr_tracking_state_t  *tracking_state);

commit 7391b0e6a65284a99bb2cc02e6cd94a82fd10c90

Minor fix for immersed boundaries z_ground computation(broken by 8353abe9).

src/atmo/cs_atmo.cpp
@@ -4400,8 +4400,8 @@ cs_atmo_z_ground_compute(void)
   const cs_mesh_quantities_t *mq_g = cs_glob_mesh_quantities_g;
   const cs_lnum_t n_cells = m->n_cells;
 
-  const cs_nreal_3_t *restrict i_face_u_normal = mq_g->i_face_u_normal;
-  const cs_nreal_3_t *restrict b_face_u_normal = mq_g->b_face_u_normal;
+  const cs_nreal_3_t *restrict i_face_u_normal = mq->i_face_u_normal;
+  const cs_nreal_3_t *restrict b_face_u_normal = mq->b_face_u_normal;
   const cs_real_3_t *restrict b_face_cog = mq->b_face_cog;
 
   const int *bc_type = cs_glob_bc_type;