code-saturne/code_saturne

commit 7a9974e66c72083dbb2cde33774935bd676dc8ba

[SLES]  Update parameters for HPDDM

src/alge/cs_param_hpddm.cpp
@@ -96,8 +96,11 @@ cs_param_hpddm_create(void)
   /* Basic options */
 
   hpddmp->harmonic_overlap   = 5;
-  hpddmp->nb_eigenvector     = 80;
+  hpddmp->nb_eigenvector     = 100;
   hpddmp->relative_threshold = 5e-6;
+  // Empirical choice advised by P. Jolivet
+  hpddmp->p =
+    std::max(1, std::min(cs_glob_n_ranks / 2 - 1, 1 + cs_glob_n_ranks / 8));
 
   /* Advanced options */
 
@@ -133,6 +136,7 @@ cs_param_hpddm_copy(const cs_param_hpddm_t *hpddmp)
   cpy->harmonic_overlap   = hpddmp->harmonic_overlap;
   cpy->nb_eigenvector     = hpddmp->nb_eigenvector;
   cpy->relative_threshold = hpddmp->relative_threshold;
+  cpy->p                  = hpddmp->p;
 
   /* Advanced options */
 
@@ -188,6 +192,11 @@ cs_param_hpddm_log(const char *name, const cs_param_hpddm_t *hpddmp)
                   "  * %s | SVD relative threshold:   %e\n",
                   name,
                   hpddmp->relative_threshold);
+
+  cs_log_printf(CS_LOG_SETUP,
+                "  * %s | Number of mpi to solve coarse problem:      %d\n",
+                name,
+                hpddmp->p);
 }
 
 /*----------------------------------------------------------------------------*/

src/alge/cs_param_sles.cpp
@@ -474,6 +474,11 @@ cs_param_sles_log(cs_param_sles_t   *slesp)
       cs_param_mumps_log(slesp->name,
                          static_cast<const cs_param_mumps_t *>
                          (slesp->context_param));
+    else if (slesp->precond == CS_PARAM_PRECOND_HPDDM) {
+      cs_param_hpddm_log(slesp->name,
+                         static_cast<const cs_param_hpddm_t *>(
+                           slesp->context_param));
+    }
 
     cs_log_printf(CS_LOG_SETUP, "  * %s | SLES Block.Precond:       %s\n",
                   slesp->name,
@@ -576,6 +581,10 @@ cs_param_sles_copy_from(const cs_param_sles_t  *src,
                                          dst->amg_type))
     dst->context_param = cs_param_amg_boomer_copy(
       static_cast<const cs_param_amg_boomer_t *>(src->context_param));
+  else if (dst->precond == CS_PARAM_PRECOND_HPDDM) {
+    dst->context_param = cs_param_hpddm_copy(
+      static_cast<const cs_param_hpddm_t *>(src->context_param));
+  }
 }
 
 /*----------------------------------------------------------------------------*/
@@ -995,6 +1004,7 @@ cs_param_sles_set_precond(const char       *keyval,
                 " Please check your installation settings.\n",
                 __func__, sles_name, "CS_EQKEY_PRECOND");
 
+    cs_param_sles_hpddm_reset(slesp);
   }
   else
     ierr = EXIT_FAILURE;
@@ -2013,7 +2023,8 @@ cs_param_sles_hpddm_reset(cs_param_sles_t *slesp)
  *                                  use_neumann
  * \param[in]      relative_threshold thresold on eigenvalue to keep if do not
  *                                  use_neumann
- */
+ * \param[in]      p                number of mpi process used to solve coarse
+ * problem. If value < 1, we use automatic value.*/
 /*----------------------------------------------------------------------------*/
 
 void
@@ -2021,7 +2032,8 @@ cs_param_sles_hpddm(cs_param_sles_t *slesp,
                     const bool       use_neumann,
                     const int        nb_eigenvector,
                     const int        harmonic_overlap,
-                    const double     relative_threshold)
+                    const double     relative_threshold,
+                    const int        p)
 {
   if (slesp == nullptr)
     return;
@@ -2038,6 +2050,9 @@ cs_param_sles_hpddm(cs_param_sles_t *slesp,
   hpddmp->nb_eigenvector     = nb_eigenvector;
   hpddmp->harmonic_overlap   = harmonic_overlap;
   hpddmp->relative_threshold = relative_threshold;
+  if (p > 0) {
+    hpddmp->p = std::max(1, std::min(p, cs_glob_n_ranks / 2 - 1));
+  }
 
   /* Advanced options */
   hpddmp->adaptative = false;

src/alge/cs_param_hpddm.h
@@ -63,6 +63,7 @@ typedef struct {
   /* - 1 <= harmonic_overlap <= 10 */
   /* - 40 <= nb_eigenvector =< 500 */
   /* - 0.5 >= relative_threshold >= 1.e-6 */
+  /* - 1 <= p < nb_mpi/2 */
 
   bool use_neumann; /*!< Use neumann matrix on each subdomains */
 
@@ -70,6 +71,8 @@ typedef struct {
 
   int nb_eigenvector; /*!< Number of eigenvector to compute
                        *    (= svd_nsv or eps_nev) */
+
+  int p; /*!< Number of mpi used to solve the coarse problem */
 
   double relative_threshold; /*! < Thresold on eigenvalue to keep */
 

src/alge/cs_param_sles.h
@@ -612,6 +612,8 @@ cs_param_sles_hpddm_reset(cs_param_sles_t *slesp);
  *                                  use_neumann
  * \param[in]      relative_threshold thresold on eigenvalue to keep if do not
  *                                  use_neumann
+ * \param[in]      p                number of mpi process used to solve coarse
+ * problem. If value < 1, we use automatic value.
  */
 /*----------------------------------------------------------------------------*/
 
@@ -620,7 +622,8 @@ cs_param_sles_hpddm(cs_param_sles_t *slesp,
                     const bool       use_neumann,
                     const int        nb_eigenvector,
                     const int        harmonic_overlap,
-                    const double     relative_threshold);
+                    const double     relative_threshold,
+                    const int        p = -1);
 
 /*----------------------------------------------------------------------------*/
 /*!

src/alge/cs_param_sles_setup.cpp
@@ -1071,7 +1071,7 @@ _petsc_pchpddm_hook(const char *prefix, cs_param_sles_t *slesp, PC pc)
     _petsc_cmd(true, prefix_pc, "mat_mumps_cntl_3", "1.e-50");
     _petsc_cmd(true, prefix_pc, "mat_mumps_cntl_5", "0.");
     _petsc_cmd(true, prefix_pc, "mat_type", "baij");
-    _petsc_cmd(true, prefix_pc, "p", "1");
+    _petsc_cmd(true, prefix_pc, "p", std::to_string(hpddmp->p).c_str());
   }
 }
 #endif // PETSC_HAVE_HPDDM

commit afb4d12d98264738f9cafe0341299bbf801e8691

Fix compilation with C++14, broken by 7a9974e6

src/alge/cs_param_hpddm.cpp
@@ -100,7 +100,7 @@ cs_param_hpddm_create(void)
   hpddmp->relative_threshold = 5e-6;
   // Empirical choice advised by P. Jolivet
   hpddmp->p =
-    std::max(1, std::min(cs_glob_n_ranks / 2 - 1, 1 + cs_glob_n_ranks / 8));
+    cs::max(1, cs::min(cs_glob_n_ranks / 2 - 1, 1 + cs_glob_n_ranks / 8));
 
   /* Advanced options */
 

src/alge/cs_param_sles.cpp
@@ -2051,7 +2051,7 @@ cs_param_sles_hpddm(cs_param_sles_t *slesp,
   hpddmp->harmonic_overlap   = harmonic_overlap;
   hpddmp->relative_threshold = relative_threshold;
   if (p > 0) {
-    hpddmp->p = std::max(1, std::min(p, cs_glob_n_ranks / 2 - 1));
+    hpddmp->p = cs::max(1, cs::min(p, cs_glob_n_ranks / 2 - 1));
   }
 
   /* Advanced options */