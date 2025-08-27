code-saturne/code_saturne

commit e58c5f9dbcd0b728dc22687355ed0c0bd77db10a

[Lagr] add a check that verify that temperature is solved if thermal 2way coupling is activated.

src/base/cs_parameters_check.cpp
@@ -1190,6 +1190,18 @@ cs_parameters_check(void)
     }
   }
 
+  if (cs_glob_lagr_time_scheme->iilagr == CS_LAGR_TWOWAY_COUPLING) {
+    if (   cs_glob_lagr_source_terms->ltsthe == 1
+        && (cs_glob_lagr_model->physical_model
+          == cs_glob_lagr_specific_physics->solve_temperature == 0))
+        cs_parameters_error
+        (CS_ABORT_DELAYED,
+         _("Lagrangian module"),
+         _("Thermal two-way coupling are activated but temperature of\n"
+           "particles are not solved.\n"
+           "Verify the parameters and the cs_user_lagr_model function.\n"));
+  }
+
   /*--------------------------------------------------------------------------
    * Check options for radiation module
    *--------------------------------------------------------------------------*/

commit 1bdca7bcbfe528c3fb41890bdff97ee9f554802e

Fix bug introduced in e58c5f9 (2025-07-04).

src/base/cs_parameters_check.cpp
@@ -1192,8 +1192,8 @@ cs_parameters_check(void)
 
   if (cs_glob_lagr_time_scheme->iilagr == CS_LAGR_TWOWAY_COUPLING) {
     if (   cs_glob_lagr_source_terms->ltsthe == 1
-        && (cs_glob_lagr_model->physical_model
-          == cs_glob_lagr_specific_physics->solve_temperature == 0))
+        && (cs_glob_lagr_model->physical_model == CS_LAGR_PHYS_HEAT
+          && cs_glob_lagr_specific_physics->solve_temperature == 0))
         cs_parameters_error
         (CS_ABORT_DELAYED,
          _("Lagrangian module"),