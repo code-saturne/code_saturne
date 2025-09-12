code-saturne/code_saturne

commit b355e9fe810a31baf183d289d5f31ee5e4f514d6

[ARCH][MAJOR] Add span access to fields values (val and val_pre)
  * For cs_field_t, the val and val_pre pointers are no longer allocated
    as is. array structures are now part of cs_field_t, and val &
    val_pre and vals[] raw pointers are pointing to the "data" pointers
    of the cs_array objects.
  * The objects stored are cs_array_2d, using (n_elts, dim). 1D access
    of the object is always possible using the [] operator, and the 2D
    access is possible using the () operator.
  * This modification now also adds 3 methods to the cs_field_t object:
    - "get_vals_s" which returns a "cs_span" (1D data object)
    - "get_vals_v" which returns a "cs_span_2d" (2D data object), of
      shape (n_elts, 3)
    - "get_vals_t" which returns a "cs_span_2d" (2D data object), of
      shape (n_elts, 6)
    - If the dimensionality of the the field is not correct, a bft_error
      is raised.
  * This is a first step toward the generalization of a converged API
    (user functions for example) based on the cs::mdspan class.

src/base/cs_field.cpp
@@ -476,6 +476,8 @@ _field_create(const char   *name,
   f->val = nullptr;
   f->val_pre = nullptr;
 
+  f->_vals = nullptr;
+
   f->grad = nullptr;
 
   f->bc_coeffs = nullptr;
@@ -1518,6 +1520,10 @@ cs_field_create(const char   *name,
   for (int i = 0; i < f->n_time_vals; i++)
     f->vals[i] = nullptr;
 
+  CS_MALLOC(f->_vals, f->n_time_vals, cs_array_2d<cs_real_t> *);
+  for (int i = 0; i < f->n_time_vals; i++)
+    f->_vals[i] = new cs_array_2d<cs_real_t>(); // empty container;
+
   return f;
 }
 
@@ -1636,6 +1642,10 @@ cs_field_find_or_create(const char   *name,
     for (int i = 0; i < f->n_time_vals; i++)
       f->vals[i] = nullptr;
 
+    CS_MALLOC(f->_vals, f->n_time_vals, cs_array_2d<cs_real_t> *);
+    for (int i = 0; i < f->n_time_vals; i++)
+      f->_vals[i] = new cs_array_2d<cs_real_t>(); // empty container;
+
   }
 
   return f;
@@ -1686,20 +1696,33 @@ cs_field_set_n_time_vals(cs_field_t  *f,
   for (int i = n_time_vals_ini; i < f->n_time_vals; i++)
     f->vals[i] = nullptr;
 
+  CS_REALLOC(f->_vals, f->n_time_vals, cs_array_2d<cs_real_t> *);
+  for (int i = n_time_vals_ini; i < f->n_time_vals; i++)
+    f->_vals[i] = new cs_array_2d<cs_real_t>(); // empty container;
+
   /* If allocation or mapping has already been done */
 
   if (f->val != nullptr) {
     if (n_time_vals_ini > _n_time_vals) {
       assert(n_time_vals_ini == 2 && _n_time_vals == 1);
       if (f->is_owner)
-        CS_FREE(f->val_pre);
-      else
-        f->val_pre = nullptr;
+        f->_vals[1]->clear(); // Free internal data
+
+      f->val_pre = nullptr;
+      f->vals[1] = nullptr;
     }
     else { /* if (n_time_vals_ini < _n_time_vals) */
       if (f->is_owner) {
         const cs_lnum_t *n_elts = cs_mesh_location_get_n_elts(f->location_id);
-        f->val_pre = _add_val(n_elts[2], f->dim, f->val_pre);
+        f->_vals[1]->reshape(n_elts[2], f->dim);
+
+        /* Use host_context in this part to avoid access by GPU (too soon...) */
+        cs_host_context h_ctx;
+        f->_vals[1]->zero(static_cast<cs_dispatch_context&>(h_ctx));
+        h_ctx.wait();
+
+        f->vals[1] = f->_vals[1]->data();
+        f->val_pre = f->_vals[1]->data();
       }
     }
   }
@@ -1725,12 +1748,20 @@ cs_field_allocate_values(cs_field_t  *f)
 
     /* Initialization */
 
-    for (ii = 0; ii < f->n_time_vals; ii++)
-      f->vals[ii] = _add_val(n_elts[2], f->dim, f->vals[ii]);
+    for (ii = 0; ii < f->n_time_vals; ii++) {
+      f->_vals[ii]->reshape(n_elts[2], f->dim);
 
-    f->val = f->vals[0];
+      /* Use host_context in this part to avoid access by GPU (too soon...) */
+      cs_host_context h_ctx;
+      f->_vals[ii]->zero(static_cast<cs_dispatch_context&>(h_ctx));
+      h_ctx.wait();
+
+      f->vals[ii] = f->_vals[ii]->data();
+    }
+
+    f->val = f->_vals[0]->data();
     if (f->n_time_vals > 1)
-      f->val_pre = f->vals[1];
+      f->val_pre = f->_vals[1]->data();
   }
 }
 
@@ -1754,8 +1785,12 @@ cs_field_map_values(cs_field_t   *f,
     return;
 
   if (f->is_owner) {
-    CS_FREE(f->val);
-    CS_FREE(f->val_pre);
+    for (int i = 0; i < f->n_time_vals; i++) {
+      f->_vals[i]->clear();
+      f->vals[i] = nullptr;
+    }
+    f->val = nullptr;
+    f->val_pre = nullptr;
     f->is_owner = false;
   }
 
@@ -2264,13 +2299,14 @@ cs_field_destroy_all(void)
   for (int i = 0; i < _n_fields; i++) {
     cs_field_t  *f = _fields[i];
     if (f->is_owner) {
-      if (f->vals != nullptr) {
+      if (f->_vals != nullptr) {
         int ii;
         for (ii = 0; ii < f->n_time_vals; ii++)
-          CS_FREE(f->vals[ii]);
+          delete f->_vals[ii];
       }
     }
     CS_FREE(f->vals);
+    CS_FREE(f->_vals);
 
     if (f->grad != nullptr)
       CS_FREE(f->grad);
@@ -4898,5 +4934,51 @@ cs_field_t::get_key_str
 }
 
 ///@}
+
+/*----------------------------------------------------------------------------*/
+
+cs_span<cs_real_t>
+cs_field_t::get_vals_s
+(
+  const int time_id
+)
+{
+  if (this->dim != 1)
+    bft_error(__FILE__, __LINE__, 0,
+              _("%s: Field \"%s\" is not a scalar and has dimension %d\n"),
+              __func__, this->name, this->dim);
+
+  return this->_vals[time_id]->get_mdspan({this->_vals[time_id]->size()});
+}
+
+cs_span_2d<cs_real_t>
+cs_field_t::get_vals_v
+(
+  const int time_id
+)
+{
+  if (this->dim != 3)
+    bft_error(__FILE__, __LINE__, 0,
+              _("%s: Field \"%s\" is not a vector and has dimension %d\n"),
+              __func__, this->name, this->dim);
+
+  const cs_lnum_t n_elts = this->_vals[time_id]->size() / this->dim;
+  return this->_vals[time_id]->get_mdspan<2>({n_elts, this->dim});
+}
+
+cs_span_2d<cs_real_t>
+cs_field_t::get_vals_t
+(
+  const int time_id
+)
+{
+  if (this->dim != 6)
+    bft_error(__FILE__, __LINE__, 0,
+              _("%s: Field \"%s\" is not a tensor and has dimension %d\n"),
+              __func__, this->name, this->dim);
+
+  const cs_lnum_t n_elts = this->_vals[time_id]->size() / this->dim;
+  return this->_vals[time_id]->get_mdspan<2>({n_elts, this->dim});
+}
 
 /*----------------------------------------------------------------------------*/

src/base/cs_array.h
@@ -921,9 +921,15 @@ public:
     _offset{0},
     _size(0),
     _owner(true),
-    _data(nullptr),
-    _mode(cs_alloc_mode)
+    _data(nullptr)
   {
+#if !defined(__CUDA_ARCH__) && \
+    !defined(SYCL_LANGUAGE_VERSION) && \
+    !defined(__HIP_DEVICE_COMPILE__)
+    _mode = cs_alloc_mode;
+#else
+    _mode = CS_ALLOC_HOST_DEVICE_SHARED; // use default and avoid compiler warnings
+#endif
   }
 
   /*--------------------------------------------------------------------------*/
@@ -1355,13 +1361,46 @@ public:
     for (int i = 0; i < _N_; i++)
       s *= dims[i];
 
-    if (s != _size)
+    if (s != _size) {
+#if !defined(__CUDA_ARCH__) && \
+    !defined(SYCL_LANGUAGE_VERSION) && \
+    !defined(__HIP_DEVICE_COMPILE__)
       bft_error(__FILE__, __LINE__, 0,
-                _("%s: requested span has total size of %ld instead of %ld "
+                _("%s: requested span has total size of %d instead of %d "
                   "for this array.\n"),
                 __func__, s, _size);
+#else
+      return mdspan<T,_N_,_L_>();
+#endif
+    }
 
     return mdspan<T,_N_,_L_>(_data, dims);
+  }
+
+  /*--------------------------------------------------------------------------*/
+  /*!
+   * \brief Get span view of array with a given array of dimensions (extent).
+   *        If total size of span is different than that of the array an error
+   *        is raised.
+   *
+   * \return mdspan view with given dimensions.
+   */
+  /*--------------------------------------------------------------------------*/
+
+  template<layout _L_ = L, typename... Args>
+  CS_F_HOST_DEVICE
+  mdspan<T, sizeof...(Args), _L_>
+  get_mdspan
+  (
+    Args... indices /*!<[in] Input arguments (parameter pack) */
+  )
+  {
+    check_non_empty_args_(indices...);
+
+    constexpr int n_vals = sizeof...(Args);
+    cs_lnum_t tmp_args[n_vals] = {indices...};
+
+    return get_mdspan<n_vals, _L_>(tmp_args);
   }
 
   /*--------------------------------------------------------------------------*/
@@ -1488,7 +1527,7 @@ public:
    */
   /*--------------------------------------------------------------------------*/
 
-  CS_F_HOST_DEVICE
+  CS_F_HOST
   void
   zero()
   {
@@ -1509,7 +1548,7 @@ public:
    */
   /*--------------------------------------------------------------------------*/
 
-  CS_F_HOST_DEVICE
+  CS_F_HOST
   void
   zero
   (
@@ -1767,7 +1806,7 @@ public:
    */
   /*--------------------------------------------------------------------------*/
 
-  CS_F_HOST_DEVICE
+  CS_F_HOST
   void
   reshape
   (
@@ -1798,7 +1837,7 @@ public:
    */
   /*--------------------------------------------------------------------------*/
 
-  CS_F_HOST_DEVICE
+  CS_F_HOST
   void
   reshape_and_copy
   (
@@ -1830,7 +1869,7 @@ public:
    */
   /*--------------------------------------------------------------------------*/
 
-  CS_F_HOST_DEVICE
+  CS_F_HOST
   void
   reshape
   (
@@ -2468,6 +2507,25 @@ private:
   )
   {
     static_assert(sizeof...(Args) < N, "Too many input arguments.");
+    static_assert(cs::are_integral<Args...>::value, "Non integral input arguments.");
+  }
+
+  /*--------------------------------------------------------------------------*/
+  /*!
+   * \brief Helper function to static check sub-function input arguments.
+   */
+  /*--------------------------------------------------------------------------*/
+
+  template<typename... Args>
+  CS_F_HOST_DEVICE
+  static inline
+  void
+  check_non_empty_args_
+  (
+    Args... /*!<[in] Input arguments (parameter pack) */
+  )
+  {
+    static_assert(sizeof...(Args) > 0, "At least one input argument is needed.");
     static_assert(cs::are_integral<Args...>::value, "Non integral input arguments.");
   }
 

src/base/cs_field.h
@@ -32,6 +32,7 @@
  *----------------------------------------------------------------------------*/
 
 #include "base/cs_defs.h"
+#include "base/cs_array.h"
 
 /*----------------------------------------------------------------------------*/
 
@@ -252,6 +253,26 @@ typedef struct {
 
   const char *
   get_key_str(const char  *key);
+
+  cs_span<cs_real_t>
+  get_vals_s
+  (
+    const int time_id = 0
+  );
+
+  cs_span_2d<cs_real_t>
+  get_vals_v
+  (
+    const int time_id = 0
+  );
+
+  cs_span_2d<cs_real_t>
+  get_vals_t
+  (
+    const int time_id = 0
+  );
+
+  cs_array_2d<cs_real_t> **_vals;
 
 #endif
 

commit f8b9ba61334e1a781819850211e4f68dd1f8dd9a

Workaround breakage introduced by b355e9fe.
The root cause of the breakage still needs to be analyzed, as even under
a debugger it seems OK, and memory allocations seem consistent, but
we had an invalid read in the underlying array's "zero" method.

src/base/cs_field.cpp
@@ -1717,9 +1717,9 @@ cs_field_set_n_time_vals(cs_field_t  *f,
         f->_vals[1]->reshape(n_elts[2], f->dim);
 
         /* Use host_context in this part to avoid access by GPU (too soon...) */
-        cs_host_context h_ctx;
-        f->_vals[1]->zero(static_cast<cs_dispatch_context&>(h_ctx));
-        h_ctx.wait();
+        cs_dispatch_context ctx;
+        f->_vals[1]->zero(ctx);
+        ctx.wait();
 
         f->vals[1] = f->_vals[1]->data();
         f->val_pre = f->_vals[1]->data();
@@ -1744,20 +1744,21 @@ cs_field_allocate_values(cs_field_t  *f)
   if (f->is_owner) {
 
     const cs_lnum_t *n_elts = cs_mesh_location_get_n_elts(f->location_id);
-    int ii;
+
+    /* Use host_context in this part to avoid access by GPU (too soon...) */
+    cs_dispatch_context ctx;
+    ctx.set_use_gpu(false);
 
     /* Initialization */
 
-    for (ii = 0; ii < f->n_time_vals; ii++) {
+    for (int ii = 0; ii < f->n_time_vals; ii++) {
       f->_vals[ii]->reshape(n_elts[2], f->dim);
-
-      /* Use host_context in this part to avoid access by GPU (too soon...) */
-      cs_host_context h_ctx;
-      f->_vals[ii]->zero(static_cast<cs_dispatch_context&>(h_ctx));
-      h_ctx.wait();
+      f->_vals[ii]->zero(ctx);
 
       f->vals[ii] = f->_vals[ii]->data();
     }
+
+    ctx.wait();
 
     f->val = f->_vals[0]->data();
     if (f->n_time_vals > 1)