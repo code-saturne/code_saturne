e669473e0d850840dfd5f109c994efab7548ba64
COME FONTAINE <cf48566l@dsp1145275.postes.calibre.edf.fr>
2025-09-18 11:36:04 +0200

    Added possibility to set meteo_zi before cs_init_meteo_profile.
    Without this patch, it would be overriden.

--- src/atmo/cs_atmo.cpp
+++ src/atmo/cs_atmo.cpp
@@ -4170,10 +4170,13 @@ cs_atmo_init_meteo_profiles(void)
 
   /* BL height according to Marht 1982 formula */
   /* value of C=0.2, 0.185, 0.06, 0.14, 0.07, 0.04 */
-  cs_real_t zi_coef = 0.2;
-  cs_real_t corio_f = 4. * cs_math_pi / 86164.
+
+  if (aopt->meteo_zi < 0){
+    cs_real_t zi_coef = 0.2;
+    cs_real_t corio_f = 4. * cs_math_pi / 86164.
                          * sin(aopt->latitude * cs_math_pi / 180.);
-  aopt->meteo_zi = zi_coef * ustar0 / fabs(corio_f);
+    aopt->meteo_zi = zi_coef * ustar0 / fabs(corio_f);
+  }
 
   /* Force the computation of z_ground */
   aopt->compute_z_ground = true;
