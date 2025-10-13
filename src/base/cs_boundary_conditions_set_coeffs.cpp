code-saturne/code_saturne

commit 8c2a7309955c4ac6ec9560a90fcfa78a9d08b670

Fix memory leak with gradient limiters, from 9a63cb95 (2025-03-15).

src/base/cs_boundary_conditions_set_coeffs.cpp
@@ -4503,8 +4503,10 @@ cs_boundary_conditions_ensure_bc_coeff_face_values_allocated
     bc_coeffs->val_f_d_lim = bc_coeffs->val_f_d;
   }
   else {
-    CS_MALLOC_HD(bc_coeffs->val_f_lim, dim*n_b_faces, cs_real_t, amode);
-    CS_MALLOC_HD(bc_coeffs->val_f_d_lim, dim*n_b_faces, cs_real_t, amode);
+    if (bc_coeffs->val_f_lim == nullptr)
+      CS_MALLOC_HD(bc_coeffs->val_f_lim, dim*n_b_faces, cs_real_t, amode);
+    if (bc_coeffs->val_f_d == nullptr)
+      CS_MALLOC_HD(bc_coeffs->val_f_d_lim, dim*n_b_faces, cs_real_t, amode);
   }
 }
 

commit 2f18362f72f4806a84ec1f7901c9c55228c08c2b

[FIX] Fix potential segmentation fault introduced by commit 8c2a7309955 (20-09-2025)

src/base/cs_boundary_conditions_set_coeffs.cpp
@@ -4505,7 +4505,7 @@ cs_boundary_conditions_ensure_bc_coeff_face_values_allocated
   else {
     if (bc_coeffs->val_f_lim == nullptr)
       CS_MALLOC_HD(bc_coeffs->val_f_lim, dim*n_b_faces, cs_real_t, amode);
-    if (bc_coeffs->val_f_d == nullptr)
+    if (bc_coeffs->val_f_d_lim == nullptr)
       CS_MALLOC_HD(bc_coeffs->val_f_d_lim, dim*n_b_faces, cs_real_t, amode);
   }
 }