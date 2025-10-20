code-saturne/code_saturne

commit b4ea4478120b50bd6f3bfd894e4491321aed8533

GPU: prefetch should work on const array, as it does not really change contents.

src/base/cs_mem.cpp
@@ -3138,14 +3138,14 @@ cs_sync_d2h_if_needed_start(void  *ptr)
  * memory, shuch as those allocated using CS_ALLOC_HOST_DEVICE_SHARED.
  * It should be usable on a subset of such an array.
  *
- * \param [in, out]  ptr   pointer to data to prefetch
- * \param [in]       size  number of bytes to prefetch
+ * \param [in]  ptr   pointer to data to prefetch
+ * \param [in]  size  number of bytes to prefetch
  */
 /*----------------------------------------------------------------------------*/
 
 void
-cs_prefetch_h2d(void    *ptr,
-                size_t   size)
+cs_prefetch_h2d(const void  *ptr,
+                size_t       size)
 {
   if (ptr == nullptr)
     return;

src/base/cs_mem.h
@@ -1189,17 +1189,15 @@ cs_sync_d2h_if_needed_start(void  *ptr)
 #if defined(HAVE_ACCEL)
 
 void
-cs_prefetch_h2d(void    *ptr,
-                size_t   size);
+cs_prefetch_h2d(const void  *ptr,
+                size_t       size);
 
 #else
 
 static inline void
-cs_prefetch_h2d(void    *ptr,
-                size_t   size)
+cs_prefetch_h2d([[maybe_unused]] const void  *ptr,
+                [[maybe_unused]] size_t       size)
 {
-  CS_UNUSED(ptr);
-  CS_UNUSED(size);
 }
 
 #endif

commit 610f44435a51537b0a5e2d08060bc33ac4d850b4

Fix build/install with clang-based compilers (broken by b4ea4478, 2025-10-16).

src/base/cs_mem.cpp
@@ -414,7 +414,9 @@ _cs_mem_error_handler_default(const char  *file_name,
  *-----------------------------------------------------------------------------*/
 
 static int  _cs_mem_global_init_mode = 0;
+#if defined(HAVE_ACCEL)
 static int  _n_async_copies_in_progress = 0;
+#endif
 
 static FILE *_cs_mem_global_file = nullptr;
 
@@ -2378,6 +2380,14 @@ cs_mem_realloc_hd(void            *ptr,
   return ret_ptr;
 }
 
+#endif /* defined(HAVE_ACCEL) */
+
+/*----------------------------------------------------------------------------*/
+
+END_C_DECLS
+
+#if defined(HAVE_ACCEL)
+
 /*----------------------------------------------------------------------------*/
 /*!
  * \brief Return matching device pointer for a given pointer.
@@ -2530,8 +2540,6 @@ cs_check_device_ptr(const void  *ptr)
  */
 /*----------------------------------------------------------------------------*/
 
-#if defined(HAVE_ACCEL)
-
 bool
 cs_mem_is_device_ptr(const void  *ptr)
 {
@@ -2556,8 +2564,6 @@ cs_mem_is_device_ptr(const void  *ptr)
 #endif
 
 }
-
-#endif
 
 /*----------------------------------------------------------------------------*/
 /*!
@@ -3349,5 +3355,3 @@ cs_mem_device_pool_clear(void)
 #endif // defined(HAVE_ACCEL)
 
 /*----------------------------------------------------------------------------*/
-
-END_C_DECLS

src/base/cs_mem.h
@@ -561,6 +561,12 @@ cs_mem_stats(uint64_t  *alloc_cur,
              uint64_t  *n_frees,
              uint64_t  *n_current);
 
+/*----------------------------------------------------------------------------*/
+
+END_C_DECLS
+
+#if defined(__cplusplus)
+
 #if defined(HAVE_ACCEL)
 
 /*----------------------------------------------------------------------------*/
@@ -650,9 +656,7 @@ cs_get_device_ptr(void  *ptr)
 
 #endif
 
-END_C_DECLS
-
-#if defined(__cplusplus) && defined(HAVE_ACCEL)
+#if defined(HAVE_ACCEL)
 
 template <class T>
 inline T *
@@ -664,9 +668,7 @@ cs_get_device_ptr(T *ptr)
   return (T *)ptr_v;
 }
 
-#endif // __cplusplus && HAVE_ACCEL
-
-BEGIN_C_DECLS
+#endif // HAVE_ACCEL
 
 /*----------------------------------------------------------------------------*/
 /*!
@@ -699,9 +701,7 @@ cs_get_device_ptr_const(const void  *ptr)
 
 #endif
 
-END_C_DECLS
-
-#if defined(__cplusplus) && defined(HAVE_ACCEL)
+#if defined(HAVE_ACCEL)
 
 template <class T>
 inline const T *
@@ -713,9 +713,7 @@ cs_get_device_ptr_const(const T *ptr)
   return (const T *)ptr_v;
 }
 
-#endif // __cplusplus && HAVE_ACCEL
-
-BEGIN_C_DECLS
+#endif // HAVE_ACCEL
 
 /*----------------------------------------------------------------------------*/
 /*!
@@ -860,9 +858,7 @@ cs_set_alloc_mode(void             **host_ptr,
  */
 /*----------------------------------------------------------------------------*/
 
-END_C_DECLS
-
-#if defined(__cplusplus) && defined(HAVE_ACCEL)
+#if defined(HAVE_ACCEL)
 
 template<typename T>
 static inline void
@@ -878,9 +874,7 @@ cs_set_alloc_mode_r(T*                &host_ptr,
 
 #define cs_set_alloc_mode_r(_host_ptr, mode);
 
-#endif // __cplusplus && HAVE_ACCEL
-
-BEGIN_C_DECLS
+#endif // HAVE_ACCEL
 
 /*----------------------------------------------------------------------------*/
 /*!
@@ -970,7 +964,6 @@ cs_sync_h2d(const void  *ptr)
  * \param [in, out]  ptr  host pointer to values to copy or prefetch
  */
 /*----------------------------------------------------------------------------*/
-
 
 #if defined(HAVE_ACCEL)
 
@@ -1257,6 +1250,6 @@ cs_mem_device_pool_clear(void);
 
 /*----------------------------------------------------------------------------*/
 
-END_C_DECLS
+#endif /* defined(__cplusplus) */
 
 #endif /* CS_MEM_H */