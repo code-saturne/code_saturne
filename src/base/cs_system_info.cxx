code-saturne/code_saturne

commit 2dfbda89c9fafabdc00e7cb174584b39ccba5ab1

Move cs_mpi_device_support variable from cs_base_accel eto cs_base.
Also add logginf of that varibla at initialization, and an option to
force it using the CS_MPI_DEVICE_SUPPORT environment variable.

src/base/cs_system_info.cxx
@@ -591,6 +591,11 @@ _system_info(bool  log)
           cs_log_printf(logs[log_id],
                         "  %s%d\n", _("MPI_COMM_WORLD size: "),
                         n_world_ranks);
+#if defined(HAVE_ACCEL)
+        cs_log_printf(logs[log_id],
+                      "  %s%d\n",
+                      _("MPI device support:  "), cs_mpi_device_support);
+#endif
       }
     }
 

src/base/cs_base.cpp
@@ -58,6 +58,10 @@
 #include <setjmp.h>
 #endif
 
+#if defined(OMPI_MAJOR_VERSION)
+  #include <mpi-ext.h>
+#endif
+
 /*----------------------------------------------------------------------------
  * PLE library headers
  *----------------------------------------------------------------------------*/
@@ -203,6 +207,26 @@ static int        _n_step_comms = 0;
 static int       *_step_ranks = nullptr;
 static MPI_Comm  *_step_comm = nullptr;
 #endif
+
+/*! Is MPI device-aware ? */
+/*------------------------*/
+
+#if defined(HAVE_ACCEL)
+
+#if defined(MPIX_CUDA_AWARE_SUPPORT) && MPIX_CUDA_AWARE_SUPPORT
+int cs_mpi_device_support = 1;
+
+#elif defined(OMPI_HAVE_MPI_EXT_CUDA) && OMPI_HAVE_MPI_EXT_CUDA
+/* We need better detection here, as OMPI_HAVE_MPI_EXT_CUDA = 1
+   does not seem to guarantee device support is present or active
+   (based on test on workstation). So do not activate yet.*/
+int cs_mpi_device_support = 0;
+
+#else
+int cs_mpi_device_support = 0;
+#endif
+
+#endif /* defined(HAVE_ACCEL) */
 
 /*============================================================================
  * Private function definitions
@@ -1376,6 +1400,32 @@ cs_base_mpi_init(int    *argc,
   }
 
 #endif
+
+  /* Also detect whether MPI is device-aware,
+     when this can be set dynamically. */
+
+#if defined(HAVE_ACCEL)
+
+#if defined(I_MPI_VERSION)
+  {
+    const char *p = getenv("I_MPI_OFFLOAD");
+    if (p != nullptr) {
+      if (atoi(p) > 0)
+        cs_mpi_device_support = 1;
+    }
+  }
+#endif
+
+  /* Device support may also be forced if not detected correctly,
+     or deactivated */
+  {
+    const char *p = getenv("CS_MPI_DEVICE_SUPPORT");
+    if (p != nullptr) {
+      cs_mpi_device_support = atoi(p);
+    }
+  }
+
+#endif /* defined(HAVE_ACCEL) */
 
   /* Initialize execution context */
   cs_execution_context_glob_init();

src/base/cs_base.h
@@ -59,9 +59,9 @@
  * be better optimized on memory-aligned data (if 0 here, no alignment
  * is leveraged). This alignment is not exploited yet in code_saturne. */
 
-#define CS_MEM_ALIGN 0
+#define CS_MEM_ALIGN  0
 
-#define CS_BASE_STRING_LEN                             80
+#define CS_BASE_STRING_LEN  80
 
 /*----------------------------------------------------------------------------*/
 
@@ -83,6 +83,16 @@ typedef void (cs_base_sigint_handler_t) (int signum);
 /*=============================================================================
  * Global variable definitions
  *============================================================================*/
+
+#if defined(HAVE_ACCEL)
+
+extern int cs_mpi_device_support;
+
+#else
+
+#define cs_mpi_device_support 0
+
+#endif
 
 /*=============================================================================
  * Public function prototypes

src/base/cs_base_accel.cpp
@@ -77,26 +77,6 @@
  *  Global variables
  *============================================================================*/
 
-/*! Is MPI device-aware ? */
-/*------------------------*/
-
-#if defined(OMPI_MAJOR_VERSION)
-  #include <mpi-ext.h>
-#endif
-
-#if defined(MPIX_CUDA_AWARE_SUPPORT) && MPIX_CUDA_AWARE_SUPPORT
-int cs_mpi_device_support = 1;
-
-#elif defined(OMPI_HAVE_MPI_EXT_CUDA) && OMPI_HAVE_MPI_EXT_CUDA
-/* We need better detection here, as OMPI_HAVE_MPI_EXT_CUDA = 1
-   does not seem to guarantee device support is present or active
-   (based on test on workstation). So do not activate yet.*/
-int cs_mpi_device_support = 0;
-
-#else
-int cs_mpi_device_support = 0;
-#endif
-
 /*! Default queue for SYCL */
 
 #if defined(SYCL_LANGUAGE_VERSION)
@@ -245,21 +225,6 @@ cs_omp_target_select_default_device(void)
     cs_alloc_mode_read_mostly = CS_ALLOC_HOST_DEVICE_SHARED;
     cs_alloc_mode_device = CS_ALLOC_HOST_DEVICE;
   }
-
-  /* Also detect whether MPI is device-aware,
-     when this can be set dynamically. */
-
-#if defined(I_MPI_VERSION)
-
-  {
-    const char *p = getenv("I_MPI_OFFLOAD");
-    if (p != nullptr) {
-      if (atoi(p) > 0)
-        cs_mpi_device_support = 1;
-    }
-  }
-
-#endif
 
   /* Return default device id */
 

src/base/cs_base_accel.h
@@ -64,16 +64,6 @@ BEGIN_C_DECLS
  * Global variable definitions
  *============================================================================*/
 
-#if defined(HAVE_ACCEL)
-
-extern int cs_mpi_device_support;
-
-#else
-
-#define cs_mpi_device_support 0;
-
-#endif
-
 /*! Default queue for SYCL */
 
 #if defined(SYCL_LANGUAGE_VERSION) && !defined(CS_GLOB_SYCL_QUEUE_IS_DEFINED)

commit 02468a340d2b77574b947cf9680d992801d276e9

Fix compilation on GPU broken by commit 2dfbda89c

src/base/cs_system_info.cxx
@@ -67,6 +67,8 @@
 #include "bft/bft_printf.h"
 #include "base/cs_log.h"
 
+#include "base/cs_base.h"
+
 #if defined(HAVE_ACCEL)
 #include "base/cs_base_accel.h"
 #endif