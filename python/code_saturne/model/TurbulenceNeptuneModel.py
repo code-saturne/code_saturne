code-saturne/code_saturne

commit 3eb00447ddc4e7d9ff6b46159eb60c1d6a1e9de8

Quality of life fix in the IHM for turb model changes in neptune_cfd.
Now, we don't need to go the the initialization page after having
changed the turbulence model.
The default turbulence init formula for the new model are set on all the
volumic areas.

python/code_saturne/model/TurbulenceNeptuneModel.py
@@ -27,6 +27,7 @@ from code_saturne.model.XMLvariables import Variables, Model
 from code_saturne.model.XMLengine import *
 from code_saturne.model.MainFieldsModel import *       # TODO change
 from code_saturne.model.NotebookModel import NotebookModel
+from code_saturne.model.LocalizationModel import LocalizationModel
 import copy
 
 #-------------------------------------------------------------------------------
@@ -113,7 +114,7 @@ class TurbulenceModel(Variables, Model):
         #
         # XML file parameters
         self.mainFieldsModel  = MainFieldsModel(case)
-        self.case = case
+        self.case             = case
         self.XMLClosure       = self.case.xmlGetNode('closure_modeling')
         self.XMLturbulence    = self.XMLClosure.xmlInitNode('turbulence')
         self.XMLNodeVariable  = self.XMLturbulence.xmlInitNode('variables')
@@ -225,6 +226,15 @@ class TurbulenceModel(Variables, Model):
                                self.setTwoWayCouplingModel(fld.f_id, TurbulenceModelsDescription.continuousCouplingModels[0])
                            else:
                                self.setTwoWayCouplingModel(fld.f_id, TurbulenceModelsDescription.continuousCouplingModels[0])
+
+           # Update initialisation values to default init formula if the turbulence model is changed
+           # If more zones are present, all initialization are set to Default
+           if model != "none":
+               for zone in LocalizationModel('VolumicZone', self.case).getZones():
+                   zoneId = zone.getCodeNumber()
+                   turbFormula = self.getDefaultTurbFormula(zoneId, fieldId, model)
+                   self.setFormula(zoneId, fieldId, model, turbFormula)
+
 
     @Variables.noUndo
     def getInitialTurbulenceChoice(self, zone, fieldId):

commit 9a6bd6a04e644dfaa3a4739ab1bd618194eecd84

Fix bug introduced in commit 3eb00447d due to circular imports.
Fixed by importing the adequate function at runtime and not at module
load. Possible because we only use the function once.

python/code_saturne/model/TurbulenceNeptuneModel.py
@@ -27,7 +27,6 @@ from code_saturne.model.XMLvariables import Variables, Model
 from code_saturne.model.XMLengine import *
 from code_saturne.model.MainFieldsModel import *       # TODO change
 from code_saturne.model.NotebookModel import NotebookModel
-from code_saturne.model.LocalizationModel import LocalizationModel
 import copy
 
 #-------------------------------------------------------------------------------
@@ -230,6 +229,7 @@ class TurbulenceModel(Variables, Model):
            # Update initialisation values to default init formula if the turbulence model is changed
            # If more zones are present, all initialization are set to Default
            if model != "none":
+               from code_saturne.model.LocalizationModel import LocalizationModel
                for zone in LocalizationModel('VolumicZone', self.case).getZones():
                    zoneId = zone.getCodeNumber()
                    turbFormula = self.getDefaultTurbFormula(zoneId, fieldId, model)