code-saturne/code_saturne

commit 09b509317657889c97878297bc99e6c481f668ac

replace CS_MALLOC by CS_MALLOC_HD in arrays

src/lagr/cs_lagr.cpp
@@ -817,7 +817,7 @@ _update_boundary_face_type(void)
 
   {
     int *bc_flag;
-    CS_MALLOC(bc_flag, mesh->n_b_faces, int);
+    CS_MALLOC_HD(bc_flag, mesh->n_b_faces, int);
 
     for (cs_lnum_t i = 0; i < mesh->n_b_faces; i++)
       bc_flag[i] = bcs->elt_type[i];
@@ -984,7 +984,7 @@ cs_lagr_init_arrays(void)
   assert(bound_stat == nullptr);
 
   if (n_boundary_stats > 0)
-    CS_MALLOC(bound_stat, n_b_faces * n_boundary_stats, cs_real_t);
+    CS_MALLOC_HD(bound_stat, n_b_faces * n_boundary_stats, cs_real_t);
 }
 
 /*----------------------------------------------------------------------------
@@ -1531,7 +1531,7 @@ cs_lagr_get_internal_conditions(void)
     cs_glob_lagr_internal_conditions = _create_internal_cond_struct();
 
   if (cs_glob_lagr_internal_conditions->i_face_zone_id == nullptr) {
-    CS_MALLOC(cs_glob_lagr_internal_conditions->i_face_zone_id,
+    CS_MALLOC_HD(cs_glob_lagr_internal_conditions->i_face_zone_id,
                cs_glob_mesh->n_i_faces,
                int);
 
@@ -1754,13 +1754,13 @@ cs_lagr_solve_time_step(const int         itypfb[],
   if (   lagr_model->clogging == 1
       || lagr_model->roughness == 1
       || lagr_model->dlvo == 1)
-    CS_MALLOC(tempp, mesh->n_cells, cs_real_t);
+    CS_MALLOC_HD(tempp, mesh->n_cells, cs_real_t);
 
   ipass ++;
 
   static cs_real_t *vislen = nullptr;
   if ((lagr_model->deposition == 1) && (ipass == 1)) {
-    CS_MALLOC(vislen, n_b_faces, cs_real_t);
+    CS_MALLOC_HD(vislen, n_b_faces, cs_real_t);
     for (cs_lnum_t ifac = 0; ifac < n_b_faces; ifac++)
       vislen[ifac] = -cs_math_big_r;
   }
@@ -2127,41 +2127,41 @@ cs_lagr_solve_time_step(const int         itypfb[],
       /* First pass allocate and compute it */
       if (extra_i[phase_id].grad_pr == nullptr) {
         const cs_lnum_t ncelet = cs_glob_mesh->n_cells_with_ghosts;
-        CS_MALLOC(extra_i[phase_id].grad_pr, ncelet, cs_real_3_t);
+        CS_MALLOC_HD(extra_i[phase_id].grad_pr, ncelet, cs_real_3_t);
 
         // TODO : check if the pressure and velocity allocs can be removed
         if (  (    cs_glob_lagr_time_scheme->interpol_field != 0
                || cs_glob_lagr_time_scheme->extended_t_scheme != 0)
             && cs_glob_lagr_model->idistu == 1)
-          CS_MALLOC(extra_i[phase_id].grad_lagr_time, ncelet, cs_real_3_t);
+          CS_MALLOC_HD(extra_i[phase_id].grad_lagr_time, ncelet, cs_real_3_t);
 
         if (   cs_glob_lagr_model->modcpl > 0
             || cs_glob_lagr_model->shape > 0
             || cs_glob_lagr_time_scheme->interpol_field != 0)
-          CS_MALLOC(extra_i[phase_id].grad_vel, ncelet, cs_real_33_t);
+          CS_MALLOC_HD(extra_i[phase_id].grad_vel, ncelet, cs_real_33_t);
 
         if (   cs_glob_lagr_model->physical_model != CS_LAGR_PHYS_OFF
             && extra->temperature != nullptr
             && cs_glob_lagr_time_scheme->interpol_field > 0
             && phase_id == 0)
-          CS_MALLOC(extra_i[phase_id].grad_tempf, ncelet, cs_real_3_t);
+          CS_MALLOC_HD(extra_i[phase_id].grad_tempf, ncelet, cs_real_3_t);
 
         /* Allocate the gradients of second order statistics */
         for (int i = 0; i < 9; i++)
-          CS_MALLOC(extra_i[phase_id].grad_cov_skp[i], ncelet, cs_real_3_t);
+          CS_MALLOC_HD(extra_i[phase_id].grad_cov_skp[i], ncelet, cs_real_3_t);
         for (int i = 0; i < 6; i++)
-          CS_MALLOC(extra_i[phase_id].grad_cov_sk[i], ncelet, cs_real_3_t);
+          CS_MALLOC_HD(extra_i[phase_id].grad_cov_sk[i], ncelet, cs_real_3_t);
 
         if (   cs_glob_lagr_time_scheme->extended_t_scheme !=0
             && cs_glob_lagr_model->idistu == 1) {
-            CS_MALLOC(extra_i[phase_id].grad_lagr_time, ncelet, cs_real_3_t);
+            CS_MALLOC_HD(extra_i[phase_id].grad_lagr_time, ncelet, cs_real_3_t);
             if (cs_glob_lagr_model->modcpl == 1)
-              CS_MALLOC(extra_i[phase_id].grad_lagr_time_r_et, ncelet, cs_real_3_t);
+              CS_MALLOC_HD(extra_i[phase_id].grad_lagr_time_r_et, ncelet, cs_real_3_t);
         }
 
         if (cs_glob_lagr_model->modcpl > 0) {
-          CS_MALLOC(extra_i[phase_id].anisotropic_lagr_time, ncelet, cs_real_3_t);
-          CS_MALLOC(extra_i[phase_id].anisotropic_bx, ncelet, cs_real_3_t);
+          CS_MALLOC_HD(extra_i[phase_id].anisotropic_lagr_time, ncelet, cs_real_3_t);
+          CS_MALLOC_HD(extra_i[phase_id].anisotropic_bx, ncelet, cs_real_3_t);
         }
         cs_lagr_aux_mean_fluid_quantities(0,
                                           phase_id,
@@ -2257,8 +2257,8 @@ cs_lagr_solve_time_step(const int         itypfb[],
       n_occupied_cells
         = _get_n_occupied_cells(p_set, 0, p_set->n_particles);
 
-      CS_MALLOC(occupied_cell_ids, n_occupied_cells, cs_lnum_t);
-      CS_MALLOC(particle_list, n_occupied_cells+1, cs_lnum_t);
+      CS_MALLOC_HD(occupied_cell_ids, n_occupied_cells, cs_lnum_t);
+      CS_MALLOC_HD(particle_list, n_occupied_cells+1, cs_lnum_t);
 
       _occupied_cells(p_set, 0, p_set->n_particles,
                       n_occupied_cells,
@@ -2275,7 +2275,7 @@ cs_lagr_solve_time_step(const int         itypfb[],
       /* Initialize lists (ids of cells and particles) */
       cs_lnum_t *cell_particle_idx;
 
-      CS_MALLOC(cell_particle_idx, n_occupied_cells+1, cs_lnum_t);
+      CS_MALLOC_HD(cell_particle_idx, n_occupied_cells+1, cs_lnum_t);
       cell_particle_idx[0] = 0;
 
       cs_lnum_t enter_parts = p_set->n_particles;

src/lagr/cs_lagr_coupling.cpp
@@ -132,8 +132,8 @@ cs_lagr_coupling_initialize(void)
                            && lag_st->npts > 0);
 
   /* Init particle volume and mass in cell */
-  CS_MALLOC(volp, n_cells_ext, cs_real_t);
-  CS_MALLOC(volm, n_cells_ext, cs_real_t);
+  CS_MALLOC_HD(volp, n_cells_ext, cs_real_t);
+  CS_MALLOC_HD(volm, n_cells_ext, cs_real_t);
   cs_array_real_fill_zero(n_cells_ext, volp);
   cs_array_real_fill_zero(n_cells_ext, volm);
 
@@ -141,8 +141,8 @@ cs_lagr_coupling_initialize(void)
     /* Momentum source terms
        ===================== */
     if (is_time_averaged) {
-      CS_MALLOC(t_st_vel, n_cells_ext, cs_real_3_t);
-      CS_MALLOC(t_st_imp_vel, n_cells_ext, cs_real_t);
+      CS_MALLOC_HD(t_st_vel, n_cells_ext, cs_real_3_t);
+      CS_MALLOC_HD(t_st_imp_vel, n_cells_ext, cs_real_t);
     }
     else {
       if (f_st_vel != nullptr)
@@ -164,14 +164,14 @@ cs_lagr_coupling_initialize(void)
          (difficult to write something for v2, which loses its meaning as
          "Rij component") */
       if (is_time_averaged)
-        CS_MALLOC(t_st_k, n_cells_ext, cs_real_t);
+        CS_MALLOC_HD(t_st_k, n_cells_ext, cs_real_t);
       else if (f_st_k != nullptr)
         t_st_k = f_st_k->val;
       cs_array_real_fill_zero(n_cells_ext, t_st_k);
     }
     else if (extra->itytur == 3) {
       if (is_time_averaged)
-        CS_MALLOC(t_st_rij, n_cells_ext, cs_real_6_t);
+        CS_MALLOC_HD(t_st_rij, n_cells_ext, cs_real_6_t);
       else if (f_st_rij != nullptr)
         t_st_rij = (cs_real_6_t*)(f_st_rij->val);
       cs_array_real_fill_zero(n_cells_ext * 6, (cs_real_t *)t_st_rij);
@@ -186,7 +186,7 @@ cs_lagr_coupling_initialize(void)
           || cs_glob_lagr_model->physical_model == CS_LAGR_PHYS_CTWR )) {
     if (t_st_p == nullptr) {
       if (is_time_averaged)
-        CS_MALLOC(t_st_p, n_cells_ext, cs_real_t);
+        CS_MALLOC_HD(t_st_p, n_cells_ext, cs_real_t);
       else if (f_st_p != nullptr)
         t_st_p = f_st_p->val;
     }
@@ -208,8 +208,8 @@ cs_lagr_coupling_initialize(void)
         || cs_glob_lagr_model->physical_model == CS_LAGR_PHYS_CTWR) {
 
       if (is_time_averaged) {
-        CS_MALLOC(t_st_t_e, n_cells_ext, cs_real_t);
-        CS_MALLOC(t_st_t_i, n_cells_ext, cs_real_t);
+        CS_MALLOC_HD(t_st_t_e, n_cells_ext, cs_real_t);
+        CS_MALLOC_HD(t_st_t_i, n_cells_ext, cs_real_t);
       }
       else {
         t_st_t_e = f_st_t_e->val;

src/lagr/cs_lagr_fragmentation.cpp
@@ -450,11 +450,11 @@ cs_lagr_fragmentation(cs_real_t  dt,
 
   /* Create local array (containing the particle index) */
   cs_lnum_t *corr;
-  CS_MALLOC(corr, lnum_particles, cs_lnum_t);
+  CS_MALLOC_HD(corr, lnum_particles, cs_lnum_t);
 
   /* Create local list (containing the class and the global index) */
   cs_lnum_2_t * interf;
-  CS_MALLOC(interf, lnum_particles, cs_lnum_2_t);
+  CS_MALLOC_HD(interf, lnum_particles, cs_lnum_2_t);
 
   /* Browse the list of particles already existing */
   for (cs_lnum_t idx = main_start; idx < main_end; ++idx) {
@@ -541,7 +541,7 @@ cs_lagr_fragmentation(cs_real_t  dt,
 
   /* Local array to save new fragments (class, index) */
   cs_lnum_2_t *interf_frag;
-  CS_MALLOC(interf_frag, newpart, cs_lnum_2_t);
+  CS_MALLOC_HD(interf_frag, newpart, cs_lnum_2_t);
 
   for (cs_lnum_t i=0; i<newpart; ++i) {
     cs_lnum_t curr_class
@@ -562,7 +562,7 @@ cs_lagr_fragmentation(cs_real_t  dt,
 
   /* Sort all particles in a cell by class */
   cs_lnum_2_t *interf_tot;
-  CS_MALLOC(interf_tot, tot_size, cs_lnum_2_t);
+  CS_MALLOC_HD(interf_tot, tot_size, cs_lnum_2_t);
 
   cs_lagr_agglo_merge_arrays(interf, interf_frag,
                              lnum_particles, newpart, interf_tot);
@@ -575,7 +575,7 @@ cs_lagr_fragmentation(cs_real_t  dt,
   /* Split the local particle set by class to facilitate merging */
   cs_lnum_t* cls_gaps;
 
-  CS_MALLOC(cls_gaps, tot_size+1, cs_lnum_t);
+  CS_MALLOC_HD(cls_gaps, tot_size+1, cs_lnum_t);
 
   _gaps_classes(interf_tot, tot_size,
                 nb_cls, cls_gaps);

src/lagr/cs_lagr_porosity.cpp
@@ -123,7 +123,7 @@ cs_lagr_porosity(void)
   const cs_lnum_t n_i_faces = mesh->n_i_faces;
 
   cs_real_t *covered_surface = nullptr;
-  CS_MALLOC(covered_surface, mesh->n_cells_with_ghosts, cs_real_t);
+  CS_MALLOC_HD(covered_surface, mesh->n_cells_with_ghosts, cs_real_t);
 
   /* Initialization */
   for (cs_lnum_t cell_id = 0; cell_id < n_cells_ext; cell_id++)

commit 5f3f2d43b0f5e83ec7b7bd76a643af39d0a1406f

Fix compilation broken by commit 09b509317

src/lagr/cs_lagr.cpp
@@ -817,7 +817,7 @@ _update_boundary_face_type(void)
 
   {
     int *bc_flag;
-    CS_MALLOC_HD(bc_flag, mesh->n_b_faces, int);
+    CS_MALLOC_HD(bc_flag, mesh->n_b_faces, int, cs_alloc_mode);
 
     for (cs_lnum_t i = 0; i < mesh->n_b_faces; i++)
       bc_flag[i] = bcs->elt_type[i];
@@ -984,7 +984,7 @@ cs_lagr_init_arrays(void)
   assert(bound_stat == nullptr);
 
   if (n_boundary_stats > 0)
-    CS_MALLOC_HD(bound_stat, n_b_faces * n_boundary_stats, cs_real_t);
+    CS_MALLOC_HD(bound_stat, n_b_faces * n_boundary_stats, cs_real_t, cs_alloc_mode);
 }
 
 /*----------------------------------------------------------------------------
@@ -1533,7 +1533,7 @@ cs_lagr_get_internal_conditions(void)
   if (cs_glob_lagr_internal_conditions->i_face_zone_id == nullptr) {
     CS_MALLOC_HD(cs_glob_lagr_internal_conditions->i_face_zone_id,
                cs_glob_mesh->n_i_faces,
-               int);
+               int, cs_alloc_mode);
 
     for (cs_lnum_t i = 0; i < cs_glob_mesh->n_i_faces; i++)
       cs_glob_lagr_internal_conditions->i_face_zone_id[i] = -1;
@@ -1754,13 +1754,13 @@ cs_lagr_solve_time_step(const int         itypfb[],
   if (   lagr_model->clogging == 1
       || lagr_model->roughness == 1
       || lagr_model->dlvo == 1)
-    CS_MALLOC_HD(tempp, mesh->n_cells, cs_real_t);
+    CS_MALLOC_HD(tempp, mesh->n_cells, cs_real_t, cs_alloc_mode);
 
   ipass ++;
 
   static cs_real_t *vislen = nullptr;
   if ((lagr_model->deposition == 1) && (ipass == 1)) {
-    CS_MALLOC_HD(vislen, n_b_faces, cs_real_t);
+    CS_MALLOC_HD(vislen, n_b_faces, cs_real_t, cs_alloc_mode);
     for (cs_lnum_t ifac = 0; ifac < n_b_faces; ifac++)
       vislen[ifac] = -cs_math_big_r;
   }
@@ -2127,41 +2127,41 @@ cs_lagr_solve_time_step(const int         itypfb[],
       /* First pass allocate and compute it */
       if (extra_i[phase_id].grad_pr == nullptr) {
         const cs_lnum_t ncelet = cs_glob_mesh->n_cells_with_ghosts;
-        CS_MALLOC_HD(extra_i[phase_id].grad_pr, ncelet, cs_real_3_t);
+        CS_MALLOC_HD(extra_i[phase_id].grad_pr, ncelet, cs_real_3_t, cs_alloc_mode);
 
         // TODO : check if the pressure and velocity allocs can be removed
         if (  (    cs_glob_lagr_time_scheme->interpol_field != 0
                || cs_glob_lagr_time_scheme->extended_t_scheme != 0)
             && cs_glob_lagr_model->idistu == 1)
-          CS_MALLOC_HD(extra_i[phase_id].grad_lagr_time, ncelet, cs_real_3_t);
+          CS_MALLOC_HD(extra_i[phase_id].grad_lagr_time, ncelet, cs_real_3_t, cs_alloc_mode);
 
         if (   cs_glob_lagr_model->modcpl > 0
             || cs_glob_lagr_model->shape > 0
             || cs_glob_lagr_time_scheme->interpol_field != 0)
-          CS_MALLOC_HD(extra_i[phase_id].grad_vel, ncelet, cs_real_33_t);
+          CS_MALLOC_HD(extra_i[phase_id].grad_vel, ncelet, cs_real_33_t, cs_alloc_mode);
 
         if (   cs_glob_lagr_model->physical_model != CS_LAGR_PHYS_OFF
             && extra->temperature != nullptr
             && cs_glob_lagr_time_scheme->interpol_field > 0
             && phase_id == 0)
-          CS_MALLOC_HD(extra_i[phase_id].grad_tempf, ncelet, cs_real_3_t);
+          CS_MALLOC_HD(extra_i[phase_id].grad_tempf, ncelet, cs_real_3_t, cs_alloc_mode);
 
         /* Allocate the gradients of second order statistics */
         for (int i = 0; i < 9; i++)
-          CS_MALLOC_HD(extra_i[phase_id].grad_cov_skp[i], ncelet, cs_real_3_t);
+          CS_MALLOC_HD(extra_i[phase_id].grad_cov_skp[i], ncelet, cs_real_3_t, cs_alloc_mode);
         for (int i = 0; i < 6; i++)
-          CS_MALLOC_HD(extra_i[phase_id].grad_cov_sk[i], ncelet, cs_real_3_t);
+          CS_MALLOC_HD(extra_i[phase_id].grad_cov_sk[i], ncelet, cs_real_3_t, cs_alloc_mode);
 
         if (   cs_glob_lagr_time_scheme->extended_t_scheme !=0
             && cs_glob_lagr_model->idistu == 1) {
-            CS_MALLOC_HD(extra_i[phase_id].grad_lagr_time, ncelet, cs_real_3_t);
+            CS_MALLOC_HD(extra_i[phase_id].grad_lagr_time, ncelet, cs_real_3_t, cs_alloc_mode);
             if (cs_glob_lagr_model->modcpl == 1)
-              CS_MALLOC_HD(extra_i[phase_id].grad_lagr_time_r_et, ncelet, cs_real_3_t);
+              CS_MALLOC_HD(extra_i[phase_id].grad_lagr_time_r_et, ncelet, cs_real_3_t, cs_alloc_mode);
         }
 
         if (cs_glob_lagr_model->modcpl > 0) {
-          CS_MALLOC_HD(extra_i[phase_id].anisotropic_lagr_time, ncelet, cs_real_3_t);
-          CS_MALLOC_HD(extra_i[phase_id].anisotropic_bx, ncelet, cs_real_3_t);
+          CS_MALLOC_HD(extra_i[phase_id].anisotropic_lagr_time, ncelet, cs_real_3_t, cs_alloc_mode);
+          CS_MALLOC_HD(extra_i[phase_id].anisotropic_bx, ncelet, cs_real_3_t, cs_alloc_mode);
         }
         cs_lagr_aux_mean_fluid_quantities(0,
                                           phase_id,
@@ -2257,8 +2257,8 @@ cs_lagr_solve_time_step(const int         itypfb[],
       n_occupied_cells
         = _get_n_occupied_cells(p_set, 0, p_set->n_particles);
 
-      CS_MALLOC_HD(occupied_cell_ids, n_occupied_cells, cs_lnum_t);
-      CS_MALLOC_HD(particle_list, n_occupied_cells+1, cs_lnum_t);
+      CS_MALLOC_HD(occupied_cell_ids, n_occupied_cells, cs_lnum_t, cs_alloc_mode);
+      CS_MALLOC_HD(particle_list, n_occupied_cells+1, cs_lnum_t, cs_alloc_mode);
 
       _occupied_cells(p_set, 0, p_set->n_particles,
                       n_occupied_cells,
@@ -2275,7 +2275,7 @@ cs_lagr_solve_time_step(const int         itypfb[],
       /* Initialize lists (ids of cells and particles) */
       cs_lnum_t *cell_particle_idx;
 
-      CS_MALLOC_HD(cell_particle_idx, n_occupied_cells+1, cs_lnum_t);
+      CS_MALLOC_HD(cell_particle_idx, n_occupied_cells+1, cs_lnum_t, cs_alloc_mode);
       cell_particle_idx[0] = 0;
 
       cs_lnum_t enter_parts = p_set->n_particles;

src/lagr/cs_lagr_coupling.cpp
@@ -132,8 +132,8 @@ cs_lagr_coupling_initialize(void)
                            && lag_st->npts > 0);
 
   /* Init particle volume and mass in cell */
-  CS_MALLOC_HD(volp, n_cells_ext, cs_real_t);
-  CS_MALLOC_HD(volm, n_cells_ext, cs_real_t);
+  CS_MALLOC_HD(volp, n_cells_ext, cs_real_t, cs_alloc_mode);
+  CS_MALLOC_HD(volm, n_cells_ext, cs_real_t, cs_alloc_mode);
   cs_array_real_fill_zero(n_cells_ext, volp);
   cs_array_real_fill_zero(n_cells_ext, volm);
 
@@ -141,8 +141,8 @@ cs_lagr_coupling_initialize(void)
     /* Momentum source terms
        ===================== */
     if (is_time_averaged) {
-      CS_MALLOC_HD(t_st_vel, n_cells_ext, cs_real_3_t);
-      CS_MALLOC_HD(t_st_imp_vel, n_cells_ext, cs_real_t);
+      CS_MALLOC_HD(t_st_vel, n_cells_ext, cs_real_3_t, cs_alloc_mode);
+      CS_MALLOC_HD(t_st_imp_vel, n_cells_ext, cs_real_t, cs_alloc_mode);
     }
     else {
       if (f_st_vel != nullptr)
@@ -164,14 +164,14 @@ cs_lagr_coupling_initialize(void)
          (difficult to write something for v2, which loses its meaning as
          "Rij component") */
       if (is_time_averaged)
-        CS_MALLOC_HD(t_st_k, n_cells_ext, cs_real_t);
+        CS_MALLOC_HD(t_st_k, n_cells_ext, cs_real_t, cs_alloc_mode);
       else if (f_st_k != nullptr)
         t_st_k = f_st_k->val;
       cs_array_real_fill_zero(n_cells_ext, t_st_k);
     }
     else if (extra->itytur == 3) {
       if (is_time_averaged)
-        CS_MALLOC_HD(t_st_rij, n_cells_ext, cs_real_6_t);
+        CS_MALLOC_HD(t_st_rij, n_cells_ext, cs_real_6_t, cs_alloc_mode);
       else if (f_st_rij != nullptr)
         t_st_rij = (cs_real_6_t*)(f_st_rij->val);
       cs_array_real_fill_zero(n_cells_ext * 6, (cs_real_t *)t_st_rij);
@@ -186,7 +186,7 @@ cs_lagr_coupling_initialize(void)
           || cs_glob_lagr_model->physical_model == CS_LAGR_PHYS_CTWR )) {
     if (t_st_p == nullptr) {
       if (is_time_averaged)
-        CS_MALLOC_HD(t_st_p, n_cells_ext, cs_real_t);
+        CS_MALLOC_HD(t_st_p, n_cells_ext, cs_real_t, cs_alloc_mode);
       else if (f_st_p != nullptr)
         t_st_p = f_st_p->val;
     }
@@ -208,8 +208,8 @@ cs_lagr_coupling_initialize(void)
         || cs_glob_lagr_model->physical_model == CS_LAGR_PHYS_CTWR) {
 
       if (is_time_averaged) {
-        CS_MALLOC_HD(t_st_t_e, n_cells_ext, cs_real_t);
-        CS_MALLOC_HD(t_st_t_i, n_cells_ext, cs_real_t);
+        CS_MALLOC_HD(t_st_t_e, n_cells_ext, cs_real_t, cs_alloc_mode);
+        CS_MALLOC_HD(t_st_t_i, n_cells_ext, cs_real_t, cs_alloc_mode);
       }
       else {
         t_st_t_e = f_st_t_e->val;

src/lagr/cs_lagr_fragmentation.cpp
@@ -450,11 +450,11 @@ cs_lagr_fragmentation(cs_real_t  dt,
 
   /* Create local array (containing the particle index) */
   cs_lnum_t *corr;
-  CS_MALLOC_HD(corr, lnum_particles, cs_lnum_t);
+  CS_MALLOC_HD(corr, lnum_particles, cs_lnum_t, cs_alloc_mode);
 
   /* Create local list (containing the class and the global index) */
   cs_lnum_2_t * interf;
-  CS_MALLOC_HD(interf, lnum_particles, cs_lnum_2_t);
+  CS_MALLOC_HD(interf, lnum_particles, cs_lnum_2_t, cs_alloc_mode);
 
   /* Browse the list of particles already existing */
   for (cs_lnum_t idx = main_start; idx < main_end; ++idx) {
@@ -541,7 +541,7 @@ cs_lagr_fragmentation(cs_real_t  dt,
 
   /* Local array to save new fragments (class, index) */
   cs_lnum_2_t *interf_frag;
-  CS_MALLOC_HD(interf_frag, newpart, cs_lnum_2_t);
+  CS_MALLOC_HD(interf_frag, newpart, cs_lnum_2_t, cs_alloc_mode);
 
   for (cs_lnum_t i=0; i<newpart; ++i) {
     cs_lnum_t curr_class
@@ -562,7 +562,7 @@ cs_lagr_fragmentation(cs_real_t  dt,
 
   /* Sort all particles in a cell by class */
   cs_lnum_2_t *interf_tot;
-  CS_MALLOC_HD(interf_tot, tot_size, cs_lnum_2_t);
+  CS_MALLOC_HD(interf_tot, tot_size, cs_lnum_2_t, cs_alloc_mode);
 
   cs_lagr_agglo_merge_arrays(interf, interf_frag,
                              lnum_particles, newpart, interf_tot);
@@ -575,7 +575,7 @@ cs_lagr_fragmentation(cs_real_t  dt,
   /* Split the local particle set by class to facilitate merging */
   cs_lnum_t* cls_gaps;
 
-  CS_MALLOC_HD(cls_gaps, tot_size+1, cs_lnum_t);
+  CS_MALLOC_HD(cls_gaps, tot_size+1, cs_lnum_t, cs_alloc_mode);
 
   _gaps_classes(interf_tot, tot_size,
                 nb_cls, cls_gaps);

src/lagr/cs_lagr_porosity.cpp
@@ -123,7 +123,7 @@ cs_lagr_porosity(void)
   const cs_lnum_t n_i_faces = mesh->n_i_faces;
 
   cs_real_t *covered_surface = nullptr;
-  CS_MALLOC_HD(covered_surface, mesh->n_cells_with_ghosts, cs_real_t);
+  CS_MALLOC_HD(covered_surface, mesh->n_cells_with_ghosts, cs_real_t, cs_alloc_mode);
 
   /* Initialization */
   for (cs_lnum_t cell_id = 0; cell_id < n_cells_ext; cell_id++)