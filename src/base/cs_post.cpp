code-saturne/code_saturne

commit fa9358abbacfbbd7d694ff23892e9872a6c8a7a5

[IBM] Add IBM postprocessing

src/base/cs_post.cpp
@@ -282,6 +282,7 @@ typedef struct {
                                             probes respectively */
   void                   *sel_input[5];  /* Advanced selection input for
                                             matching selection functions */
+  bool                    ext_def;       /* External definition */
   int                     ent_flag[5];   /* Presence of cells (ent_flag[0],
                                             interior faces (ent_flag[1]),
                                             boundary faces (ent_flag[2]),
@@ -324,7 +325,6 @@ typedef struct {
   const fvm_nodal_t      *exp_mesh;      /* Associated exportable mesh */
   fvm_nodal_t            *_exp_mesh;     /* Associated exportable mesh,
                                             if owner */
-
   fvm_writer_time_dep_t   mod_flag_min;  /* Minimum mesh time dependency */
   fvm_writer_time_dep_t   mod_flag_max;  /* Maximum mesh time dependency */
 
@@ -1189,6 +1189,7 @@ _predefine_mesh(int        mesh_id,
     post_mesh->ent_flag[j] = 0;
   }
 
+  post_mesh->ext_def = false;
   post_mesh->n_i_faces = 0;
   post_mesh->n_b_faces = 0;
 
@@ -1364,13 +1365,15 @@ _define_export_mesh(cs_post_mesh_t  *post_mesh,
     }
     else {
 
-      if (   n_b_faces >= cs_glob_mesh->n_b_faces_all
-          && n_i_faces == 0)
+      n_b_faces = cs::min(cs_glob_mesh->n_b_faces,
+                          cs_glob_mesh->n_b_faces_all);
+
+      if (n_i_faces == 0)
         exp_mesh = cs_mesh_connect_faces_to_nodal(cs_glob_mesh,
                                                   post_mesh->name,
                                                   post_mesh->add_groups,
                                                   0,
-                                                  cs_glob_mesh->n_b_faces,
+                                                  n_b_faces,
                                                   nullptr,
                                                   nullptr);
       else
@@ -1949,7 +1952,7 @@ _redefine_mesh(cs_post_mesh_t        *post_mesh,
   /* Remove previous base structure (return if we do not own the mesh) */
 
   if (post_mesh->exp_mesh != nullptr) {
-    if (post_mesh->_exp_mesh == nullptr)
+    if (post_mesh->_exp_mesh == nullptr || post_mesh->ext_def == true)
       return;
     else
       post_mesh->_exp_mesh = fvm_nodal_destroy(post_mesh->_exp_mesh);
@@ -4828,6 +4831,8 @@ cs_post_define_existing_mesh(int           mesh_id,
   if (transfer == true)
     post_mesh->_exp_mesh = exp_mesh;
 
+  post_mesh->ext_def = true;
+
   /* Compute number of cells and/or faces */
 
   dim_ext_ent = fvm_nodal_get_max_entity_dim(exp_mesh);
@@ -4927,8 +4932,9 @@ cs_post_define_future_mesh(int           mesh_id,
 
   /* Assign mesh to structure */
 
-  post_mesh->_exp_mesh = fvm_nodal_create("empty_mesh", 3);
-  post_mesh->exp_mesh = post_mesh->_exp_mesh;
+  post_mesh->_exp_mesh = nullptr;
+  post_mesh->exp_mesh = nullptr;
+  post_mesh->ext_def = true;
 
   /* Compute number of cells and/or faces */
 
@@ -6306,7 +6312,7 @@ cs_post_write_var(int                    mesh_id,
 
       n_parent_lists = 2;
       parent_num_shift[0] = 0;
-      parent_num_shift[1] = cs_glob_mesh->n_b_faces_all;
+      parent_num_shift[1] = cs_glob_mesh->n_b_faces;
 
       if (post_mesh->ent_flag[CS_POST_LOCATION_B_FACE] == 1) {
         if (interlace == false) {
@@ -8203,7 +8209,7 @@ cs_post_time_step_end(void)
 
   for (int i = 0; i < _cs_post_n_meshes; i++) {
     cs_post_mesh_t  *post_mesh = _cs_post_meshes + i;
-    if (post_mesh->_exp_mesh != nullptr) {
+    if (post_mesh->_exp_mesh != nullptr && post_mesh->ext_def == false) {
       if (   post_mesh->ent_flag[3]
           || post_mesh->mod_flag_min == FVM_WRITER_TRANSIENT_CONNECT) {
         post_mesh->exp_mesh = nullptr;

src/apps/cs_solver.cxx
@@ -99,6 +99,7 @@
 #include "base/cs_paramedmem_coupling.h"
 #include "base/cs_parameters.h"
 #include "base/cs_physical_properties.h"
+#include "base/cs_porous_model.h"
 #include "base/cs_post.h"
 #include "base/cs_post_default.h"
 #include "base/cs_preprocess.h"
@@ -373,6 +374,7 @@ _run(void)
 
   cs_gui_postprocess_meshes();
   cs_user_postprocess_meshes();
+  cs_porous_model_enable_post();
   cs_user_postprocess_probes();
 
   /* Print info on fields and associated keys and other setup options */

src/base/cs_porous_model.cpp
@@ -64,6 +64,9 @@
 #include "base/cs_preprocess.h"
 #include "base/cs_renumber.h"
 
+#include "fvm/fvm_nodal_from_desc.h"
+#include "fvm/fvm_nodal_order.h"
+
 /*----------------------------------------------------------------------------
  *  Header for the current file
  *----------------------------------------------------------------------------*/
@@ -91,9 +94,18 @@ BEGIN_C_DECLS
  * Local type definitions
  *============================================================================*/
 
+static cs_porous_model_extra_faces _porous_model_extra_faces = {
+  .ib_mesh = nullptr,
+  .mesh_id = 0,
+  .activate_post = false
+};
+
 /*============================================================================
  * Global variables
  *============================================================================*/
+
+cs_porous_model_extra_faces *cs_glob_porous_model_extra_faces
+= &_porous_model_extra_faces;
 
 /*! Choice of the porous model */
 int cs_glob_porous_model = 0;
@@ -891,6 +903,231 @@ cs_porous_model_convert_cell_to_boundary(const cs_lnum_t   n_ib_cells,
   if (cs_glob_n_ranks > 1) {
     fvm_io_num_destroy(face_io_num);
   }
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Initialize porous model arrays
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_porous_model_enable_post(void) {
+  if (cs_glob_porous_model_extra_faces->activate_post == false)
+    return;
+
+  cs_glob_porous_model_extra_faces->ib_mesh = fvm_nodal_create("ib_mesh", 3);
+
+  const int writer_ids[] = {CS_POST_WRITER_DEFAULT};
+
+  int mesh_id = cs_post_get_free_mesh_id();
+
+  cs_post_define_future_mesh(mesh_id,
+                             CS_POST_MESH_BOUNDARY,
+                             true,  /* auto variables */
+                             1,     /* number of associated writers */
+                             writer_ids);
+
+  cs_glob_porous_model_extra_faces->mesh_id = mesh_id;
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Post-processes the immersed boundary (ib) planes for display
+ *         on paraview.
+ *
+ * \param[in]       n_ib_cells      ib cell number
+ * \param[in]       n_glob_vtx      total vertex number
+ * \param[in]       ibcell_cells    connectivity ib_cell->cells
+ * \param[in]       vtx_ids         vertex ids on both sides of a IB vertex
+ *                                  (v0<v1)
+ * \param[in]       w_vtx_idx       ib vertex indexes
+ * \param[in]       face_vertex_idx vertex indexes of the ib faces
+ * \param[in]       w_vtx           ib vertex coordinates
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_porous_model_post_immmersed_plane(const cs_lnum_t      n_ib_cells,
+                                     const cs_lnum_t      n_glob_vtx,
+                                     const cs_lnum_t      ibcell_cells[],
+                                     const cs_lnum_t      vtx_ids[][2],
+                                     const cs_lnum_t      w_vtx_idx[],
+                                     const cs_lnum_t      face_vertex_idx[],
+                                     const cs_real_t      w_vtx[][3])
+{
+  if (cs_glob_porous_model_extra_faces->activate_post == false)
+    return;
+
+  cs_lnum_t *face_vertex_ids;
+  CS_MALLOC(face_vertex_ids, n_glob_vtx, cs_lnum_t);
+  for (cs_lnum_t i = 0; i < n_glob_vtx; i++)
+    face_vertex_ids[i] = i;
+
+  /* Reordering the vertices to obtain a closed contour
+     -------------------------------------------------- */
+
+  cs_lnum_t n_w_vtx = 0;
+  cs_coord_3_t *edge;
+  CS_MALLOC(edge, n_glob_vtx, cs_coord_3_t);
+
+  for (cs_lnum_t c_id_ib = 0; c_id_ib < n_ib_cells; c_id_ib++) {
+
+    const cs_lnum_t c_id = ibcell_cells[c_id_ib];
+    const int n_vtx = w_vtx_idx[c_id+1]-w_vtx_idx[c_id];
+
+    /* Edge reference is the first 2 ib vtx by default, then connect other
+       ib edges by finding the corresponding vtx ids associated to the
+       second ib vertex */
+
+    cs_lnum_t k = 0;
+    const cs_lnum_t s_id = w_vtx_idx[c_id];
+    const cs_lnum_t e_id = w_vtx_idx[c_id+1];
+
+    const int n_edge = 0.5*n_vtx;
+    cs_real_23_t edge_l[n_edge];
+    cs_lnum_t vtx_ids_l[n_edge][2][2];
+
+    for (cs_lnum_t j = s_id; j < e_id; j=j+2) {
+
+      /* Store edges and associated vtx_ids of the ib cell */
+      for (cs_lnum_t i = 0; i < 3; i++) {
+        edge_l[k][0][i] = w_vtx[j][i];
+        edge_l[k][1][i] = w_vtx[j+1][i];
+      }
+      vtx_ids_l[k][0][0] = vtx_ids[j][0];
+      vtx_ids_l[k][0][1] = vtx_ids[j][1];
+
+      vtx_ids_l[k][1][0] = vtx_ids[j+1][0];
+      vtx_ids_l[k][1][1] = vtx_ids[j+1][1];
+      k++;
+    }
+
+    /* The reference edge is the first k=0 */
+    for (cs_lnum_t i = 0; i < 3; i++) {
+      edge[n_w_vtx][i]   = edge_l[0][0][i];
+      edge[n_w_vtx+1][i] = edge_l[0][1][i];
+    }
+
+    n_w_vtx += 2;
+
+    /* Reference edge id = 0 */
+    int edge_id = 0;
+    int n_iter = 0;
+    int cpt = n_edge - 1;
+
+    /* Fist edge is x0->x1. The vtx to connect is x1. */
+    cs_lnum_t vtx_to_connect[2] = {vtx_ids_l[0][1][0], vtx_ids_l[0][1][1]};
+
+    do {
+
+      for (int j = 0; j < n_edge; j++) {
+
+        if (j != edge_id) {
+
+          int new_vtx_id = -1;
+
+          /* x0 of edge j is connected to the vertex */
+          if (   vtx_ids_l[j][0][0] == vtx_to_connect[0]
+              && vtx_ids_l[j][0][1] == vtx_to_connect[1]) {
+
+            new_vtx_id = 1;
+          }
+          /* x1 of edge j is connected to the vertex */
+          else if (   vtx_ids_l[j][1][0] == vtx_to_connect[0]
+                   && vtx_ids_l[j][1][1] == vtx_to_connect[1]) {
+
+            new_vtx_id = 0;
+          }
+          else {
+            continue;
+          }
+
+          vtx_to_connect[0] = vtx_ids_l[j][new_vtx_id][0];
+          vtx_to_connect[1] = vtx_ids_l[j][new_vtx_id][1];
+
+          for (cs_lnum_t i = 0; i < 3; i++)
+            edge[n_w_vtx][i] = edge_l[j][new_vtx_id][i];
+
+          n_w_vtx++;
+          edge_id = j;
+          cpt -= 1;
+          break;
+        }
+      }
+
+      n_iter++;
+      if (n_iter > 100)
+        bft_error(__FILE__, __LINE__, 0,
+                  _("Problem connecting vertices in cell_id = %d"), c_id);
+
+    } while (cpt != 1);
+
+  } /* End loop on ib cells */
+
+  /* Create IBM mesh and give IBM faces and vertices */
+
+  fvm_nodal_t *ib_mesh = cs_glob_porous_model_extra_faces->ib_mesh;
+  const cs_lnum_t face_list_shift[2] = {0, n_ib_cells};
+
+  const cs_lnum_t *face_vtx_idx[1] = {face_vertex_idx};
+  const cs_lnum_t *face_vtx_ids[1] = {face_vertex_ids};
+
+  fvm_nodal_from_desc_add_faces(ib_mesh,
+                                1,
+                                n_ib_cells,
+                                nullptr,
+                                1,
+                                face_list_shift,
+                                face_vtx_idx,
+                                face_vtx_ids,
+                                nullptr,
+                                nullptr);
+
+  fvm_nodal_transfer_vertices(ib_mesh,
+                              reinterpret_cast<cs_coord_t *>(edge));
+  edge = nullptr;
+
+  /* Parallel numbering */
+
+  fvm_io_num_t *face_io_num = nullptr;
+  fvm_io_num_t *vtx_io_num = nullptr;
+
+  if (cs_glob_n_ranks > 1) {
+
+    /* Order faces by increasing global number */
+
+    face_io_num = fvm_io_num_create_from_scan(n_ib_cells);
+    const cs_gnum_t *face_gnum = fvm_io_num_get_global_num(face_io_num);
+    fvm_nodal_order_faces(ib_mesh, face_gnum);
+    fvm_nodal_init_io_num(ib_mesh, face_gnum, 2);
+    fvm_io_num_destroy(face_io_num);
+
+    /* Order vertices by increasing global number */
+
+    vtx_io_num = fvm_io_num_create_from_scan(n_glob_vtx);
+    const cs_gnum_t *vertex_gnum = fvm_io_num_get_global_num(vtx_io_num);
+    fvm_nodal_order_vertices(ib_mesh, vertex_gnum);
+    fvm_nodal_init_io_num(ib_mesh, vertex_gnum, 0);
+    fvm_io_num_destroy(vtx_io_num);
+
+  }
+
+  cs_lnum_t *new_parent_id;
+  CS_MALLOC(new_parent_id, n_ib_cells, cs_lnum_t);
+  cs_lnum_t shift = cs_glob_mesh->n_b_faces;  // Same as n_faces_all here.
+  for (cs_lnum_t i = 0; i < n_ib_cells; i++)
+    new_parent_id[i] = i + shift;
+
+  fvm_nodal_change_parent_id(ib_mesh, new_parent_id, 2);
+
+  cs_post_assign_existing_mesh(cs_glob_porous_model_extra_faces->mesh_id,
+                               ib_mesh,
+                               0,
+                               true);
+
+  CS_FREE(new_parent_id);
+  CS_FREE(face_vertex_ids);
 }
 
 /*----------------------------------------------------------------------------*/

src/base/cs_porous_model.h
@@ -35,6 +35,8 @@
 #include "mesh/cs_mesh.h"
 #include "mesh/cs_mesh_quantities.h"
 
+#include "fvm/fvm_nodal.h"
+
 /*----------------------------------------------------------------------------*/
 
 BEGIN_C_DECLS
@@ -47,9 +49,17 @@ BEGIN_C_DECLS
  * Type definition
  *============================================================================*/
 
+typedef struct {
+  fvm_nodal_t *ib_mesh;
+  int mesh_id;
+  bool activate_post;
+} cs_porous_model_extra_faces;
+
 /*============================================================================
  * Global variables
  *============================================================================*/
+
+extern cs_porous_model_extra_faces *cs_glob_porous_model_extra_faces;
 
 /* Choice of the porous model */
 extern int cs_glob_porous_model;
@@ -159,6 +169,40 @@ cs_porous_model_convert_cell_to_boundary(const cs_lnum_t   n_ib_cells,
                                          const cs_lnum_t   ibcell_cells[]);
 
 /*----------------------------------------------------------------------------*/
+/*!
+ * \brief Initialize porous model arrays
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_porous_model_enable_post(void);
+
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief  Post-processes the immersed boundary (ib) planes for display
+ *         on paraview.
+ *
+ * \param[in]       n_ib_cells      ib cell number
+ * \param[in]       n_glob_vtx      total vertex number
+ * \param[in]       ibcell_cells    connectivity ib_cell->cells
+ * \param[in]       vtx_ids         vertex ids on both sides of a IB vertex
+ *                                  (v0<v1)
+ * \param[in]       w_vtx_idx       ib vertex indexes
+ * \param[in]       face_vertex_idx vertex indexes of the ib faces
+ * \param[in]       w_vtx           ib vertex coordinates
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_porous_model_post_immmersed_plane(const cs_lnum_t      n_ib_cells,
+                                     const cs_lnum_t      n_glob_vtx,
+                                     const cs_lnum_t      ibcell_cells[],
+                                     const cs_lnum_t      vtx_ids[][2],
+                                     const cs_lnum_t      w_vtx_idx[],
+                                     const cs_lnum_t      face_vertex_idx[],
+                                     const cs_real_t      w_vtx[][3]);
 
 END_C_DECLS
 

src/mesh/cs_mesh_connect.cpp
@@ -122,7 +122,8 @@ _add_faces_to_nodal(const cs_mesh_t  *mesh,
 
   /* Count the number of faces to convert */
 
-  cs_lnum_t n_max_faces = mesh->n_i_faces + mesh->n_b_faces;
+  cs_lnum_t n_max_faces =   mesh->n_i_faces
+                          + cs::max(mesh->n_b_faces, mesh->n_b_faces_all);
   CS_MALLOC(extr_face_list, n_max_faces, cs_lnum_t);
 
   /* Initialize list as marker */
@@ -207,13 +208,12 @@ _order_nodal_faces(const cs_mesh_t  *mesh,
 {
   cs_lnum_t   face_id, i;
 
-  cs_lnum_t   n_max_faces = 0;
-
   cs_gnum_t  *num_glob_fac = nullptr;
 
   /* Count the number of faces to convert */
 
-  n_max_faces = mesh->n_i_faces + mesh->n_b_faces;
+  cs_lnum_t n_max_faces =   mesh->n_i_faces
+                          + cs::max(mesh->n_b_faces, mesh->n_b_faces_all);
 
   /* In case of parallelism or face renumbering, sort faces by
      increasing global number */

src/mesh/cs_mesh_quantities.cpp
@@ -56,9 +56,6 @@
 #include "base/cs_porous_model.h"
 #include "base/cs_post.h"
 
-#include "fvm/fvm_nodal_from_desc.h"
-#include "fvm/fvm_nodal_order.h"
-
 /*----------------------------------------------------------------------------
  *  Header for the current file
  *----------------------------------------------------------------------------*/
@@ -2634,213 +2631,6 @@ _compute_unit_normals(const cs_mesh_t       *m,
   }
 }
 
-/*----------------------------------------------------------------------------*/
-/*!
- * \brief  Post-processes the immersed boundary (ib) planes for display
- *         on paraview.
- *
- * \param[in]       n_ib_cells      ib cell number
- * \param[in]       n_glob_vtx      total vertex number
- * \param[in]       ibcell_cells    connectivity ib_cell->cells
- * \param[in]       vtx_ids         vertex ids on both sides of a IB vertex
- *                                  (v0<v1)
- * \param[in]       w_vtx_idx       ib vertex indexes
- * \param[in]       face_vertex_idx vertex indexes of the ib faces
- * \param[in]       w_vtx           ib vertex coordinates
- */
-/*----------------------------------------------------------------------------*/
-
-static void
-_post_plane_ib(const cs_lnum_t      n_ib_cells,
-               const cs_lnum_t      n_glob_vtx,
-               const cs_lnum_t      ibcell_cells[],
-               const cs_lnum_t      vtx_ids[][2],
-               const cs_lnum_t      w_vtx_idx[],
-               const cs_lnum_t      face_vertex_idx[],
-               const cs_real_t      w_vtx[][3])
-{
-  cs_lnum_t *face_vertex_ids;
-  CS_MALLOC(face_vertex_ids, n_glob_vtx, cs_lnum_t);
-  for (cs_lnum_t i = 0; i < n_glob_vtx; i++)
-    face_vertex_ids[i] = i;
-
-  /* Reordering the vertices to obtain a closed contour
-     -------------------------------------------------- */
-
-  cs_lnum_t n_w_vtx = 0;
-  cs_coord_3_t *edge;
-  CS_MALLOC(edge, n_glob_vtx, cs_coord_3_t);
-
-  for (cs_lnum_t c_id_ib = 0; c_id_ib < n_ib_cells; c_id_ib++) {
-
-    const cs_lnum_t c_id = ibcell_cells[c_id_ib];
-    const int n_vtx = w_vtx_idx[c_id+1]-w_vtx_idx[c_id];
-
-    /* Edge reference is the first 2 ib vtx by default, then connect other
-       ib edges by finding the corresponding vtx ids associated to the
-       second ib vertex */
-
-    cs_lnum_t k = 0;
-    const cs_lnum_t s_id = w_vtx_idx[c_id];
-    const cs_lnum_t e_id = w_vtx_idx[c_id+1];
-
-    const int n_edge = 0.5*n_vtx;
-    cs_real_23_t edge_l[n_edge];
-    cs_lnum_t vtx_ids_l[n_edge][2][2];
-
-    for (cs_lnum_t j = s_id; j < e_id; j=j+2) {
-
-      /* Store edges and associated vtx_ids of the ib cell */
-      for (cs_lnum_t i = 0; i < 3; i++) {
-        edge_l[k][0][i] = w_vtx[j][i];
-        edge_l[k][1][i] = w_vtx[j+1][i];
-      }
-      vtx_ids_l[k][0][0] = vtx_ids[j][0];
-      vtx_ids_l[k][0][1] = vtx_ids[j][1];
-
-      vtx_ids_l[k][1][0] = vtx_ids[j+1][0];
-      vtx_ids_l[k][1][1] = vtx_ids[j+1][1];
-      k++;
-    }
-
-    /* The reference edge is the first k=0 */
-    for (cs_lnum_t i = 0; i < 3; i++) {
-      edge[n_w_vtx][i]   = edge_l[0][0][i];
-      edge[n_w_vtx+1][i] = edge_l[0][1][i];
-    }
-
-    n_w_vtx += 2;
-
-    /* Reference edge id = 0 */
-    int edge_id = 0;
-    int n_iter = 0;
-    int cpt = n_edge - 1;
-
-    /* Fist edge is x0->x1. The vtx to connect is x1. */
-    cs_lnum_t vtx_to_connect[2] = {vtx_ids_l[0][1][0], vtx_ids_l[0][1][1]};
-
-    do {
-
-      for (int j = 0; j < n_edge; j++) {
-
-        if (j != edge_id) {
-
-          int new_vtx_id = -1;
-
-          /* x0 of edge j is connected to the vertex */
-          if (   vtx_ids_l[j][0][0] == vtx_to_connect[0]
-              && vtx_ids_l[j][0][1] == vtx_to_connect[1]) {
-
-            new_vtx_id = 1;
-          }
-          /* x1 of edge j is connected to the vertex */
-          else if (   vtx_ids_l[j][1][0] == vtx_to_connect[0]
-                   && vtx_ids_l[j][1][1] == vtx_to_connect[1]) {
-
-            new_vtx_id = 0;
-          }
-          else {
-            continue;
-          }
-
-          vtx_to_connect[0] = vtx_ids_l[j][new_vtx_id][0];
-          vtx_to_connect[1] = vtx_ids_l[j][new_vtx_id][1];
-
-          for (cs_lnum_t i = 0; i < 3; i++)
-            edge[n_w_vtx][i] = edge_l[j][new_vtx_id][i];
-
-          n_w_vtx++;
-          edge_id = j;
-          cpt -= 1;
-          break;
-        }
-      }
-
-      n_iter++;
-      if (n_iter > 100)
-        bft_error(__FILE__, __LINE__, 0,
-                  _("Problem connecting vertices in cell_id = %d"), c_id);
-
-    } while (cpt != 1);
-
-  } /* End loop on ib cells */
-
-  /* Create IBM mesh and give IBM faces and vertices */
-
-  fvm_nodal_t *ib_mesh = fvm_nodal_create("ib_mesh", 3);
-  const cs_lnum_t face_list_shift[2] = {0, n_ib_cells};
-
-  const cs_lnum_t *face_vtx_idx[1] = {face_vertex_idx};
-  const cs_lnum_t *face_vtx_ids[1] = {face_vertex_ids};
-
-  fvm_nodal_from_desc_add_faces(ib_mesh,
-                                -1,
-                                n_ib_cells,
-                                nullptr,
-                                1,
-                                face_list_shift,
-                                face_vtx_idx,
-                                face_vtx_ids,
-                                nullptr,
-                                nullptr);
-
-  fvm_nodal_set_shared_vertices(ib_mesh,
-                                (const cs_coord_t *)edge);
-
-  /* Parallel numbering */
-
-  fvm_io_num_t *face_io_num = nullptr;
-  fvm_io_num_t *vtx_io_num = nullptr;
-
-  if (cs_glob_n_ranks > 1) {
-
-    /* Order faces by increasing global number */
-
-    face_io_num = fvm_io_num_create_from_scan(n_ib_cells);
-    const cs_gnum_t *face_gnum = fvm_io_num_get_global_num(face_io_num);
-    fvm_nodal_order_faces(ib_mesh, face_gnum);
-    fvm_nodal_init_io_num(ib_mesh, face_gnum, 2);
-
-    /* Order vertices by increasing global number */
-
-    vtx_io_num = fvm_io_num_create_from_scan(n_glob_vtx);
-    const cs_gnum_t *vertex_gnum = fvm_io_num_get_global_num(vtx_io_num);
-    fvm_nodal_order_vertices(ib_mesh, vertex_gnum);
-    fvm_nodal_init_io_num(ib_mesh, vertex_gnum, 0);
-
-  }
-
-  /* Print ib_mesh info in listing */
-
-  //fvm_nodal_dump(ib_mesh);
-
-  /* Create default writer */
-
-  fvm_writer_t *writer = nullptr;
-  writer = fvm_writer_init("ib_mesh",
-                           "postprocessing",
-                           cs_post_get_default_format(),
-                           cs_post_get_default_format_options(),
-                           FVM_WRITER_FIXED_MESH);
-
-  /* Write current mesh */
-
-  fvm_writer_export_nodal(writer, ib_mesh);
-
-  /* Free memory */
-
-  fvm_writer_finalize(writer);
-  ib_mesh = fvm_nodal_destroy(ib_mesh);
-
-  if (cs_glob_n_ranks > 1) {
-    fvm_io_num_destroy(vtx_io_num);
-    fvm_io_num_destroy(face_io_num);
-  }
-
-  CS_FREE(face_vertex_ids);
-  CS_FREE(edge);
-}
-
 /*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
 
 BEGIN_C_DECLS
@@ -4680,13 +4470,13 @@ cs_mesh_quantities_solid_compute(const cs_mesh_t       *m,
     }
   }
 
-  _post_plane_ib(n_ib_cells,
-                 n_glob_vtx,
-                 ibcell_cells,
-                 vtx_ids,
-                 w_vtx_idx,
-                 face_vertex_idx,
-                 w_vtx);
+  cs_porous_model_post_immmersed_plane(n_ib_cells,
+                                       n_glob_vtx,
+                                       ibcell_cells,
+                                       vtx_ids,
+                                       w_vtx_idx,
+                                       face_vertex_idx,
+                                       w_vtx);
 
   if (!cs_glob_porosity_from_scan_opt->use_staircase) {
     cs_porous_model_convert_cell_to_boundary(n_ib_cells_true, ibcell_cells_true);

commit 3d0ddf97544b2704ac48d0d8f8ba3ee3671824b6

Fix postprocessing mesh output of boundary zone, broken by fa9358ab (2025-10-03).
This fixes GitHub issue #156.

src/base/cs_post.cpp
@@ -1365,10 +1365,10 @@ _define_export_mesh(cs_post_mesh_t  *post_mesh,
     }
     else {
 
-      n_b_faces = cs::min(cs_glob_mesh->n_b_faces,
-                          cs_glob_mesh->n_b_faces_all);
+      cs_lnum_t n_b_faces_all = cs::min(cs_glob_mesh->n_b_faces,
+                                        cs_glob_mesh->n_b_faces_all);
 
-      if (n_i_faces == 0)
+      if (n_b_faces >= n_b_faces_all && n_i_faces == 0)
         exp_mesh = cs_mesh_connect_faces_to_nodal(cs_glob_mesh,
                                                   post_mesh->name,
                                                   post_mesh->add_groups,