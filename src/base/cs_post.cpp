code-saturne/code_saturne

commit 3d0ddf97544b2704ac48d0d8f8ba3ee3671824b6

Fix postprocessing mesh output of boundary zone, broken by fa9358ab (2025-10-03).
This fixes GitHub issue #156.

src/base/cs_post.cpp
@@ -1365,10 +1365,10 @@ _define_export_mesh(cs_post_mesh_t  *post_mesh,
     }
     else {
 
-      n_b_faces = cs::min(cs_glob_mesh->n_b_faces,
-                          cs_glob_mesh->n_b_faces_all);
+      cs_lnum_t n_b_faces_all = cs::min(cs_glob_mesh->n_b_faces,
+                                        cs_glob_mesh->n_b_faces_all);
 
-      if (n_i_faces == 0)
+      if (n_b_faces >= n_b_faces_all && n_i_faces == 0)
         exp_mesh = cs_mesh_connect_faces_to_nodal(cs_glob_mesh,
                                                   post_mesh->name,
                                                   post_mesh->add_groups,

commit 4cd5e610c4a626673cdaebf5ae6a9806667896f2

Re-fix postprocessing mesh for boundary zone, broken by 3d0ddf97 (yesterday).
This was broken for cases with hidden faces by a fix for GitHib issue #156.

src/base/cs_post.cpp
@@ -1365,15 +1365,15 @@ _define_export_mesh(cs_post_mesh_t  *post_mesh,
     }
     else {
 
-      cs_lnum_t n_b_faces_all = cs::min(cs_glob_mesh->n_b_faces,
+      cs_lnum_t n_b_faces_adj = cs::min(cs_glob_mesh->n_b_faces,
                                         cs_glob_mesh->n_b_faces_all);
 
-      if (n_b_faces >= n_b_faces_all && n_i_faces == 0)
+      if (n_b_faces >= n_b_faces_adj&& n_i_faces == 0)
         exp_mesh = cs_mesh_connect_faces_to_nodal(cs_glob_mesh,
                                                   post_mesh->name,
                                                   post_mesh->add_groups,
                                                   0,
-                                                  n_b_faces,
+                                                  n_b_faces_adj,
                                                   nullptr,
                                                   nullptr);
       else