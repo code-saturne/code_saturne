code-saturne/code_saturne

commit fbab7a352c18e325eca40b2dbecaf1c3c70f5397

GUI: fix incorrect gradient setting with rare gradient options combination.
The gradient type can be adapted for an external neighborhood only
for least squares reconstruction.

src/gui/cs_gui.cpp
@@ -3708,7 +3708,7 @@ cs_gui_numerical_options(void)
   else if (cs_gui_strcmp(choice, "green_vtx"))
     _imrgra = 7;
 
-  if (_imrgra != 0) {
+  if (_imrgra > 0 && _imrgra < 7) {
     int _imrgra_add = 0;
     choice = cs_tree_node_get_tag(cs_tree_get_node(tn_n,
                                                    "extended_neighborhood"),

commit 5816f25e96e4a897b12ddf3f414b7f48e7a691b6

Fix missing setting of extended halo, broken by commit fbab7a35 (2025-09-15).
When the gradient type was set to default, the activation of the extended
neighborhood was not done correctly anymore.

src/base/cs_preprocess.cpp
@@ -284,6 +284,9 @@ cs_preprocess_mesh(cs_halo_type_t   halo_type)
   /* Initialize extended connectivity, ghost cells and other remaining
      parallelism-related structures */
 
+  if (cs_ext_neighborhood_get_type() > CS_EXT_NEIGHBORHOOD_NONE)
+    halo_type = CS_HALO_EXTENDED;
+
   cs_mesh_init_halo(m, cs_glob_mesh_builder, halo_type, m->verbosity, true);
   cs_mesh_update_auxiliary(m);
 

src/gui/cs_gui.cpp
@@ -3708,7 +3708,7 @@ cs_gui_numerical_options(void)
   else if (cs_gui_strcmp(choice, "green_vtx"))
     _imrgra = 7;
 
-  if (_imrgra > 0 && _imrgra < 7) {
+  if (_imrgra == -1 || (_imrgra > 0 && _imrgra < 7)) {
     int _imrgra_add = 0;
     choice = cs_tree_node_get_tag(cs_tree_get_node(tn_n,
                                                    "extended_neighborhood"),