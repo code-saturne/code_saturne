code-saturne/code_saturne

commit 67cec03f05d45362dcf099ff8c44baa5dc1af260

[GUI] added turbulence initialization in neptune_cfd gui.
Similar to what is done in code_saturne gui, choice between
initialization from reference values and a formula for each field.

The model RpRfp is not done yet. It would require creating the tensor
Rfp instead of having to define the qfpxx, qfpyy, etc...

The possibility to choose the k-omega model is also added in this
feature.

python/code_saturne/model/TurbulenceNeptuneModel.py
@@ -26,6 +26,7 @@ import sys, unittest
 from code_saturne.model.XMLvariables import Variables, Model
 from code_saturne.model.XMLengine import *
 from code_saturne.model.MainFieldsModel import *       # TODO change
+from code_saturne.model.NotebookModel import NotebookModel
 import copy
 
 #-------------------------------------------------------------------------------
@@ -108,12 +109,13 @@ class TurbulenceModel(Variables, Model):
         """
         #
         # XML file parameters
-        self.mainFieldsModel = MainFieldsModel(case)
+        self.mainFieldsModel  = MainFieldsModel(case)
         self.case = case
-        self.XMLClosure      = self.case.xmlGetNode('closure_modeling')
-        self.XMLturbulence   = self.XMLClosure.xmlInitNode('turbulence')
-        self.XMLNodeVariable = self.XMLturbulence.xmlInitNode('variables')
-        self.XMLNodeproperty = self.XMLturbulence.xmlInitNode('properties')
+        self.XMLClosure       = self.case.xmlGetNode('closure_modeling')
+        self.XMLturbulence    = self.XMLClosure.xmlInitNode('turbulence')
+        self.XMLNodeVariable  = self.XMLturbulence.xmlInitNode('variables')
+        self.XMLNodeproperty  = self.XMLturbulence.xmlInitNode('properties')
+        self.XMLturbVariables = self.XMLturbulence.xmlGetNode('variables')
 
 
     def defaultValues(self):
@@ -175,11 +177,11 @@ class TurbulenceModel(Variables, Model):
            for var in TurbulenceModelsDescription.turbulenceVariables[model]:
              if var == "reynolds_stress":
                  self.setNewVariableProperty("variable", "",
-                                                             self.XMLNodeVariable,
-                                                             fieldId,
-                                                             var,
-                                                             var+"_"+field_name,
-                                                             dim=6)
+                                             self.XMLNodeVariable,
+                                             fieldId,
+                                             var,
+                                             var+"_"+field_name,
+                                             dim=6)
              elif var == 'covariance_qfp':
                  if field.carrier_id == 'all':
                      _carr_lst = self.mainFieldsModel.getContinuousFieldList()
@@ -187,21 +189,20 @@ class TurbulenceModel(Variables, Model):
                      _carr_lst = [self.mainFieldsModel.getFieldFromId(field.carrier_id)]
 
                  for _f in _carr_lst:
-                     _vname = "_".join([var, _f.f_id])
                      _vlabel = "_".join([var, _f.label, field_name])
                      self.setNewVariableProperty("variable", "",
                                                  self.XMLNodeVariable,
                                                  fieldId,
-                                                 _vname,
+                                                 var,
                                                  _vlabel)
              else:
                  if var == 'covariance_qfp':
                      var += "_" + self.mainFieldsModel.getFieldLabelsList()[0]
                  self.setNewVariableProperty("variable", "",
-                                                             self.XMLNodeVariable,
-                                                             fieldId,
-                                                             var,
-                                                             var+"_"+field_name)
+                                             self.XMLNodeVariable,
+                                             fieldId,
+                                             var,
+                                             var+"_"+field_name)
 
            for var in TurbulenceModelsDescription.turbulenceProperties[model]:
                self.setNewVariableProperty("property", "", self.XMLNodeproperty, fieldId, var, var+"_"+field_name)
@@ -221,6 +222,30 @@ class TurbulenceModel(Variables, Model):
                                self.setTwoWayCouplingModel(fld.f_id, TurbulenceModelsDescription.continuousCouplingModels[0])
                            else:
                                self.setTwoWayCouplingModel(fld.f_id, TurbulenceModelsDescription.continuousCouplingModels[0])
+
+    @Variables.noUndo
+    def getInitialTurbulenceChoice(self, zone, fieldId):
+        """
+        get the initialization mode for the turbulence
+        """
+        node_init = self.XMLturbulence.xmlGetNode('init_choice', zone_id=zone, field_id=fieldId)
+        choice = ''
+        if not node_init:
+            init_mode = 'reference_value'
+            self.setInitialTurbulenceChoice(zone, fieldId, init_mode)
+            node_init = self.XMLturbulence.xmlGetNode('init_choice', zone_id=zone, field_id=fieldId)
+        choice = node_init['choice']
+
+        return choice
+
+
+    @Variables.undoLocal
+    def setInitialTurbulenceChoice(self, zone, fieldId, init_mode):
+        """
+        set the initialization mode for the turbulence
+        """
+        node_init = self.XMLturbulence.xmlInitNode('init_choice', zone_id=zone, field_id=fieldId)
+        node_init['choice'] = init_mode
 
 
     @Variables.noUndo
@@ -245,6 +270,147 @@ class TurbulenceModel(Variables, Model):
         model = node['model']
 
         return model
+
+    @Variables.undoLocal
+    def setFormula(self, zone, fieldId, turbModel, formula):
+        """
+        Gives a formula for initial values
+        """
+        self.mainFieldsModel.isFieldIdValid(fieldId, strict_check=True)
+
+        node = self.XMLturbVariables
+        n = node.xmlInitChildNode('initialization',
+                                  field_id=fieldId,
+                                  zone_id=zone).xmlInitChildNode('formula')
+        if formula != None:
+            n.xmlSetTextNode(formula)
+        else:
+            n.xmlRemoveNode()
+
+    @Variables.noUndo
+    def getFormula(self, zone, fieldId, turbModel):
+        """
+        Return a formula for initial values
+        """
+        self.mainFieldsModel.isFieldIdValid(fieldId)
+
+        node = self.XMLturbVariables
+        if not node:
+            msg = "There is an error: this node " + str(node) + " should be present"
+            raise ValueError(msg)
+
+        turbInit = node.xmlInitNode('initialization',
+                                    field_id=fieldId,
+                                    zone_id=zone)
+
+        formula = turbInit.xmlGetString('formula')
+        if not formula:
+                formula = self.getDefaultTurbFormula(zone, fieldId, turbModel)
+        return formula
+
+    @Variables.noUndo
+    def getFormulaComponents(self, zone, fieldId, turbModel):
+        exp = self.getFormula(zone, fieldId, turbModel)
+
+        sym = [('x', "X cell's gravity center"),
+               ('y', "Y cell's gravity center"),
+               ('z', "Z cell's gravity center")]
+
+        if 'k-epsilon' in turbModel:
+            req = [('k', 'turbulent kinetic energy'),
+                   ('eps', 'turbulent kinetic energy dissipation')]
+        elif 'k-omega' in turbModel:
+            req = [('k', 'turbulent kinetic energy'),
+                   ('omg', 'turbulent kinetic energy dissipation rate')]
+        elif 'rij-epsilon' in turbModel:
+            if 'ebrsm' not in turbModel:
+                req = [('RXX', 'XX Reynolds stress component'),
+                       ('RYY', 'YY Reynolds stress component'),
+                       ('RZZ', 'ZZ Reynolds stress component'),
+                       ('RXY', 'XY Reynolds stress component'),
+                       ('RXZ', 'XZ Reynolds stress component'),
+                       ('RYZ', 'YZ Reynolds stress component'),
+                       ('eps', 'turbulent kinetic energy dissipation')]
+            else:
+                req = [('RXX', 'XX Reynolds stress component'),
+                       ('RYY', 'YY Reynolds stress component'),
+                       ('RZZ', 'ZZ Reynolds stress component'),
+                       ('RXY', 'XY Reynolds stress component'),
+                       ('RXZ', 'XZ Reynolds stress component'),
+                       ('RYZ', 'YZ Reynolds stress component'),
+                       ('eps', 'turbulent kinetic energy dissipation'),
+                       ('alpha', 'Blending factor')]
+        elif 'q2' in turbModel:
+            req = [('qp', 'particle agitation energy'),
+                   ('qfp', 'fluid-particle velocity covariance')]
+        elif 'r2-q12' in turbModel:
+            req = [('RXX', 'XX Particle reynolds stress component'),
+                   ('RYY', 'YY Particle reynolds stress component'),
+                   ('RZZ', 'ZZ Particle reynolds stress component'),
+                   ('RXY', 'XY Particle reynolds stress component'),
+                   ('RXZ', 'XZ Particle reynolds stress component'),
+                   ('RYZ', 'YZ Particle reynolds stress component'),
+                   ('qfp', 'fluid-particle velocity covariance')]
+        elif 'r2-r12' in turbModel:
+            req = [('RXX', 'XX Particle reynolds stress component'),
+                   ('RYY', 'YY Particle reynolds stress component'),
+                   ('RZZ', 'ZZ Particle reynolds stress component'),
+                   ('RXY', 'XY Particle reynolds stress component'),
+                   ('RXZ', 'XZ Particle reynolds stress component'),
+                   ('RYZ', 'YZ Particle reynolds stress component'),
+                   ('R12XX', 'XX Fluid-Particle covariance'),
+                   ('R12YY', 'YY Fluid-Particle covariance'),
+                   ('R12ZZ', 'ZZ Fluid-Particle covariance'),
+                   ('R12XY', 'XY Fluid-Particle covariance'),
+                   ('R12XZ', 'XZ Fluid-Particle covariance'),
+                   ('R12YZ', 'YZ Fluid-Particle covariance')]
+
+        for (name, val) in NotebookModel(self.case).getNotebookList():
+            sym.append((name, 'value (notebook) = ' + str(val)))
+
+        return exp, req, sym
+
+    @Variables.noUndo
+    def getDefaultTurbFormula(self, zone, fieldId, turb_model):
+        """
+        set the default turbulence formula when needed (ref value init choice
+        for example). The expressions were previously defined in the function
+        nc_gui_initialize_setup in the routine nc_gui.cxx.
+        """
+        if 'k-epsilon' in turb_model:
+            formula = """k = 1e-5;\neps = 1e-4;"""
+        elif 'k-omega' in turb_model:
+            formula = """k = 1e-5;\nomg = 10;"""
+        elif 'rij' in turb_model:
+            if 'ebrsm' not in turb_model:
+                formula = (
+                "RXX = 1e-5; RYY = 1e-5; RZZ = 1e-5; RXY = 0; RXZ = 0; RYZ = 0;\n"
+                "eps = 1e-3;"
+                )
+            else:
+                formula = (
+                "RXX = 1e-5; RYY = 1e-5; RZZ = 1e-5; RXY = 0; RXZ = 0; RYZ = 0;\n"
+                "eps = 1e-3;\nalpha = 1;"
+                )
+        elif 'q2' in turb_model:
+            formula = """qp = 1e-5;\nqfp = 2e-5;"""
+        elif 'r2-q12' in turb_model:
+            formula = (
+            "RXX = 1e-5; RYY = 1e-5; RZZ = 1e-5; RXY = 0; RXZ = 0; RYZ = 0;\n"
+            "qfp = 2e-5;"
+            )
+        elif 'r2-r12' in turb_model:
+            formula = (
+            "RXX = 1e-5; RYY = 1e-5; RZZ = 1e-5; RXY = 0.; RXZ = 0.; RYZ = 0.;\n"
+            "R12XX = 1e-5; R12YY = 1e-5; R12ZZ = 1e-5; R12XY = 0; R12XZ = 0; R12YZ = 0"
+            )
+        else:
+            msg = "Reference value initialization for turbulence model "\
+                    + turb_model + " is not defined"
+            raise ValueError(msg)
+
+        return formula
+
 
     @Variables.undoLocal
     def setThermalTurbulentFlux(self, fieldId, model):

python/code_saturne/base/cs_meg_to_c.py
@@ -942,7 +942,6 @@ class meg_to_c_interpreter:
             'const cs_real_t %s = cs_notebook_parameter_value_by_name("%s");' % (kn, kn)
 
         # fluid properties
-
         for kp in _pkg_fluid_prop_dict[self.module_name].keys():
             if len(name.split("_")) > 1:
                 try:
@@ -957,7 +956,6 @@ class meg_to_c_interpreter:
             glob_tokens[kp] = 'const cs_real_t %s = %s->%s;' %(kp, gs, pn)
 
         # known fields
-
         for f in known_fields:
             glob_tokens[f[0]] = \
             'const cs_real_t *%s_vals = cs_field_by_name("%s")->val;' % (f[0], f[1])
@@ -2447,6 +2445,8 @@ class meg_to_c_interpreter:
             from code_saturne.model.MainFieldsInitializationModel import MainFieldsInitializationModel
             from code_saturne.model.NonCondensableModel import NonCondensableModel
             from code_saturne.model.SpeciesModel import SpeciesModel
+            from code_saturne.model.TurbulenceNeptuneModel import TurbulenceModelsDescription
+            from code_saturne.model.TurbulenceNeptuneModel import TurbulenceModel
 
             mfm = MainFieldsModel(self.case)
             mfi = MainFieldsInitializationModel(self.case)
@@ -2480,6 +2480,24 @@ class meg_to_c_interpreter:
                         self.init_block('ini', zone_name,
                                         'volume_fraction_'+str(fId),
                                         exp, req, sym, [])
+
+                        # Turbulence get turbModel here before
+                        turbModel = \
+                                TurbulenceModel(self.case).getTurbulenceModel(fId)
+                        if turbModel != 'none':
+                            exp, req, sym = \
+                                    TurbulenceModel(self.case).getFormulaComponents(z_id,
+                                                                                    fId,
+                                                                                    turbModel)
+
+                            # Play with variablesDim to find the start and end index for the
+                            # required variables
+                            self.init_block('ini', zone_name,
+                                            'turbulence_'+str(fId),
+                                            exp,
+                                            req,
+                                            sym,
+                                            [])
 
                         # Enthalpy (only if energy resolution is activated)
                         if mfm.getFieldFromId(fId).enthalpy_model != 'off':

python/code_saturne/gui/case/MainFieldsInitializationView.py
@@ -56,6 +56,7 @@ from code_saturne.model.LocalizationModel import VolumicLocalizationModel, Local
 from code_saturne.model.NonCondensableModel import NonCondensableModel
 from code_saturne.model.SpeciesModel import SpeciesModel
 from code_saturne.model.ThermodynamicsModel import ThermodynamicsModel
+from code_saturne.model.TurbulenceNeptuneModel import TurbulenceModel
 
 from code_saturne.gui.case.QMegEditorView import QMegEditorView
 
@@ -100,11 +101,13 @@ class MainFieldsInitializationView(QWidget, Ui_MainFieldsInitialization):
     def defineConnections(self):
         self.comboBoxField.activated[str].connect(self.slotField)
         self.comboBoxEnergy.activated[str].connect(self.slotEnergyModel)
+        self.comboBoxTurbulence.activated[str].connect(self.slotTurbulenceModel)
         self.comboBoxNonCondensable.activated[str].connect(self.slotNonCondensableType)
         self.comboBoxScalar.activated[str].connect(self.slotScalarName)
         self.pushButtonPressure.clicked.connect(self.slotPressure)
         self.pushButtonVelocity.clicked.connect(self.slotVelocity)
         self.pushButtonFraction.clicked.connect(self.slotFraction)
+        self.pushButtonTurbulence.clicked.connect(self.slotTurbulence)
         self.pushButtonEnergy.clicked.connect(self.slotEnergy)
         self.pushButtonNonCondensable.clicked.connect(self.slotNonCondensable)
         self.pushButtonScalar.clicked.connect(self.slotScalar)
@@ -112,6 +115,7 @@ class MainFieldsInitializationView(QWidget, Ui_MainFieldsInitialization):
     def hideAllWidgets(self):
         self.labelEnergy.hide()
         self.comboBoxEnergy.hide()
+        self.comboBoxTurbulence.hide()
         self.pushButtonEnergy.hide()
         self.labelNonCondensable.hide()
         self.comboBoxNonCondensable.hide()
@@ -140,21 +144,26 @@ class MainFieldsInitializationView(QWidget, Ui_MainFieldsInitialization):
 
         need_none = (SpeciesModel(self.case).getScalarByFieldId("none")!=[])
         if need_none:
-            self.modelField.addItem(self.tr('Non-convected scalars'), 'none')
+            self.modelField.addItem(self.tr("Non-convected scalars"), 'none')
 
         self.currentid = -1
         if len(self.mdl.mainFieldsModel.getFieldIdList()) > 0:
             self.currentid = self.mdl.mainFieldsModel.getFieldIdList()[0]
             self.modelField.setItem(str_model = self.currentid)
         self.modelEnergy = ComboModel(self.comboBoxEnergy, 3, 1)
-        self.modelEnergy.addItem(self.tr("Enthalpy"), "enthalpy")
-        self.modelEnergy.addItem(self.tr("Temperature"), "temperature")
-        self.modelEnergy.addItem(self.tr("Saturation enthalpy"), "hsat_P")
+        self.modelEnergy.addItem(self.tr("Enthalpy"), 'enthalpy')
+        self.modelEnergy.addItem(self.tr("Temperature"), 'temperature')
+        self.modelEnergy.addItem(self.tr("Saturation enthalpy"), 'hsat_P')
 
         if int(self.currentid) > 0:
             if ThermodynamicsModel(self.case).getMaterials(self.currentid) == 'user_material' :
                 self.modelEnergy.disableItem(1)
                 self.modelEnergy.disableItem(2)
+
+        self.modelTurbulence = ComboModel(self.comboBoxTurbulence, 2, 1)
+        self.modelTurbulence.addItem(self.tr("Initialization by formula"), 'formula')
+        self.modelTurbulence.addItem(self.tr("Initialization by reference value(s)"),
+                                     'reference_value')
 
         self.modelNonCondensable = ComboModel(self.comboBoxNonCondensable, 1, 1)
         self.currentNonCond = ""
@@ -188,12 +197,21 @@ class MainFieldsInitializationView(QWidget, Ui_MainFieldsInitialization):
                 self.pushButtonVelocity.setToolTip(exp)
             else:
                 self.pushButtonVelocity.setStyleSheet("background-color: red")
+
             exp = self.mdl.getFormula(self.zone_id, self.currentid, 'volume_fraction')
             if exp:
                 self.pushButtonFraction.setStyleSheet("background-color: green")
                 self.pushButtonFraction.setToolTip(exp)
             else:
                 self.pushButtonFraction.setStyleSheet("background-color: red")
+
+            turbModel = TurbulenceModel(self.case).getTurbulenceModel(self.currentid)
+            exp = TurbulenceModel(self.case).getFormula(self.zone_id, self.currentid, turbModel)
+            if exp:
+                self.pushButtonTurbulence.setStyleSheet("background-color: green")
+                self.pushButtonTurbulence.setToolTip(exp)
+            else:
+                self.pushButtonTurbulence.setStyleSheet("background-color: red")
 
             if field.enthalpy_model != "off":
                 exp = self.mdl.getFormula(self.zone_id, self.currentid, 'enthalpy')
@@ -205,7 +223,9 @@ class MainFieldsInitializationView(QWidget, Ui_MainFieldsInitialization):
 
             lst = self.NonCondensable.getNonCondensableByFieldId(self.currentid)
             if len(lst) > 0:
-                exp = self.mdl.getFormulaNonCondensable(self.zone_id, self.currentid, self.currentNonCond)
+                exp = self.mdl.getFormulaNonCondensable(self.zone_id,
+                                                        self.currentid,
+                                                        self.currentNonCond)
                 if exp:
                     self.pushButtonNonCondensable.setStyleSheet("background-color: green")
                     self.pushButtonNonCondensable.setToolTip(exp)
@@ -233,6 +253,7 @@ class MainFieldsInitializationView(QWidget, Ui_MainFieldsInitialization):
 
         if self.currentid != 'none':
             field = self.mdl.mainFieldsModel.getFieldFromId(self.currentid)
+            turbModel = TurbulenceModel(self.case).getTurbulenceModel(self.currentid)
             # Velocity
             exp = self.mdl.getFormula(self.zone_id,
                                       self.currentid,
@@ -253,6 +274,16 @@ class MainFieldsInitializationView(QWidget, Ui_MainFieldsInitialization):
                 self.pushButtonFraction.setToolTip(exp)
             else:
                 self.pushButtonFraction.setStyleSheet("background-color: red")
+
+            # Turbulence
+            exp = TurbulenceModel(self.case).getFormula(self.zone_id,
+                                                        self.currentid,
+                                                        turbModel)
+            if exp:
+                self.pushButtonTurbulence.setStyleSheet("background-color: green")
+                self.pushButtonTurbulence.setToolTip(exp)
+            else:
+                self.pushButtonTurbulence.setStyleSheet("background-color: red")
 
             # Energy
             if field.enthalpy_model != "off":
@@ -288,6 +319,29 @@ class MainFieldsInitializationView(QWidget, Ui_MainFieldsInitialization):
                 self.pushButtonScalar.setToolTip(exp)
             else:
                 self.pushButtonScalar.setStyleSheet("background-color: red")
+
+
+    @pyqtSlot(str)
+    def slotTurbulenceModel(self, text):
+        """
+        INPUT label for choice of turbulence model
+        """
+        choice = self.modelTurbulence.dicoV2M[str(text)]
+        zoneId = str(self.zone.getCodeNumber())
+        fieldId = self.currentid
+        TurbulenceModel(self.case).setInitialTurbulenceChoice(zoneId, fieldId, choice)
+        turbModel = TurbulenceModel(self.case).getTurbulenceModel(fieldId)
+
+        if 'formula' in choice:
+            self.pushButtonTurbulence.setVisible(True)
+            turbFormula = TurbulenceModel(self.case).getFormula(zoneId, fieldId, turbModel)
+            if not turbFormula:
+                self.pushButtonTurbulence.setStyleSheet("background-color: red")
+            else:
+                self.pushButtonTurbulence.setStyleSheet("background-color: green")
+                TurbulenceModel(self.case).setFormula(zoneId, fieldId, turbModel, turbFormula)
+        else:
+            self.pushButtonTurbulence.hide()
 
 
     @pyqtSlot(str)
@@ -413,6 +467,97 @@ class MainFieldsInitializationView(QWidget, Ui_MainFieldsInitialization):
             self.mdl.setFormula(self.zone_id, self.currentid, 'volume_fraction', result)
             self.pushButtonFraction.setStyleSheet("background-color: green")
             self.pushButtonFraction.setToolTip(result)
+
+    @pyqtSlot()
+    def slotTurbulence(self):
+        """
+        Formula for turbulence
+        """
+        turbModel = TurbulenceModel(self.case).getTurbulenceModel(self.currentid)
+        exp, req, sym = TurbulenceModel(self.case).getFormulaComponents(self.zone_id,
+                                                                        self.currentid,
+                                                                        turbModel)
+
+        if '' in exp:
+            if 'k-epsilon' in turbModel:
+                exp = """k = 1e-5;\neps = 1e-4;"""
+            elif 'k-omega' in turbModel:
+                exp = """k = 1e-5;\nomg = 10.;"""
+            elif 'rij' in turbModel:
+                if 'ebrsm' not in turbModel:
+                    exp = (
+                    "RXX = 1e-5; RYY = 1e-5; RZZ = 1e-5; RXY = 0; RXZ = 0; RYZ = 0;\n"
+                    "eps = 1e-3;"
+                    )
+                else:
+                    exp = (
+                    "RXX = 1e-5; RYY = 1e-5; RZZ = 1e-5; RXY = 0; RXZ = 0; RYZ = 0;\n"
+                    "eps = 1e-3;\nalpha = 1;"
+                    )
+            elif 'q2' in turbModel:
+                exp = """qp = 1e-5;\nqfp = 2e-5;"""
+            elif 'r2-q12' in turbModel:
+                exp = (
+                "RXX = 1e-5; RYY = 1e-5; RZZ = 1e-5; RXY = 0; RXZ = 0; RYZ = 0;\n"
+                "qfp = 2e-5;"
+                )
+            elif 'r2-r12' in turbModel:
+                exp = (
+                "RXX = 1e-5; RYY = 1e-5; RZZ = 1e-5; RXY = 0.; RXZ = 0.; RYZ = 0.;\n"
+                "R12XX = 1e-5; R12YY = 1e-5; R12ZZ = 1e-5; R12XY = 0; R12XZ = 0; R12YZ = 0"
+                )
+            else:
+                msg = "Reference value initialization for turbulence model "\
+                        + turbModel + " is not defined"
+                raise ValueError(msg)
+
+        if 'k-epsilon' in turbModel:
+            exa = """k = 1.0e-5;\neps = 1.0e-3;"""
+        elif 'k-omega' in turbModel:
+            exa = """k = 1.0e-5;\nomg = 10.;"""
+        elif 'rij' in turbModel:
+            if 'ebrsm' not in turbModel:
+                exa = (
+                "RXX = 1e-5; RYY = 1e-5; RZZ = 1e-5; RXY = 0.; RXZ = 0.; RYZ = 0.;\n"
+                "eps = 1e-3;"
+                )
+            else:
+                exa = (
+                "RXX = 1e-5; RYY = 1e-5; RZZ = 1e-5; RXY = 0.; RXZ = 0.; RYZ = 0.;\n"
+                "eps = 1e-3;\n"
+                "alpha = 1;"
+                )
+        elif 'q2' in turbModel:
+            exa = """qp = 1e-4; qfp = 2e-4;"""
+        elif 'r2-q12' in turbModel:
+            exa = (
+            "RXX = 1e-5; RYY = 1e-5; RZZ = 1e-5; RXY = 0.; RXZ = 0.; RYZ = 0.;\n"
+            "qfp = 2e-5;"
+            )
+        elif 'r2-r12' in turbModel:
+            exa = (
+            "RXX = 1e-5; RYY = 1e-5; RZZ = 1e-5; RXY = 0.; RXZ = 0.; RYZ = 0.;\n"
+            "R12XX = 1e-5; R12YY = 1e-5; R12ZZ = 1e-5; R12XY = 0; R12XZ = 0; R12YZ = 0;"
+            )
+
+        name = 'turbulence_%s' % (str(self.currentid))
+        zone_name = self.zone.getLabel()
+
+        dialog = QMegEditorView(parent        = self,
+                                function_type = 'ini',
+                                zone_name     = zone_name,
+                                variable_name = name,
+                                expression    = exp,
+                                required      = req,
+                                symbols       = sym,
+                                examples      = exa)
+
+        if dialog.exec_():
+            result = dialog.get_result()
+            log.debug("slotFormulaRho -> %s" % str(result))
+            TurbulenceModel(self.case).setFormula(self.zone_id, self.currentid, turbModel, result)
+            self.pushButtonTurbulence.setStyleSheet("background-color: green")
+            self.pushButtonTurbulence.setToolTip(result)
 
 
     @pyqtSlot()
@@ -572,10 +717,30 @@ pressure = P0 + rho0 * g * (zmax - z);"""
         self.comboBoxEnergy.hide()
         self.pushButtonEnergy.hide()
 
+        # Velocity and volume fraction initialization
         self.labelVelocity.setVisible(fieldId!='none')
         self.pushButtonVelocity.setVisible(fieldId!='none')
         self.labelFraction.setVisible(fieldId!='none')
         self.pushButtonFraction.setVisible(fieldId!='none')
+
+        # Turbulence initialization
+        self.labelTurbulence.hide()
+        self.pushButtonTurbulence.hide()
+        self.comboBoxTurbulence.hide()
+        turbModel = TurbulenceModel(self.case).getTurbulenceModel(fieldId)
+        if turbModel != 'none':
+            self.labelTurbulence.setVisible(fieldId!='none')
+            self.pushButtonTurbulence.setVisible(fieldId!='none')
+            self.comboBoxTurbulence.setVisible(fieldId!='none')
+            turbInitChoice = \
+                    TurbulenceModel(self.case).getInitialTurbulenceChoice(zone, fieldId)
+            self.modelTurbulence.setItem(str_model=turbInitChoice)
+
+            if turbInitChoice != 'formula':
+                turbFormula = \
+                        TurbulenceModel(self.case).getDefaultTurbFormula(zone, fieldId, turbModel)
+                TurbulenceModel(self.case).setFormula(zone, fieldId, turbModel, turbFormula)
+                self.pushButtonTurbulence.hide()
 
         field = self.mdl.mainFieldsModel.getFieldFromId(fieldId)
         if fieldId != 'none' and field.enthalpy_model != "off":

commit 0919b8e33e2bef45a2ec963ff37930b0650ee3be

[GUI] Fix crash in NCFD when selecting LES model (introduced in commit 67cec03f05)

python/code_saturne/model/TurbulenceNeptuneModel.py
@@ -407,6 +407,8 @@ class TurbulenceModel(Variables, Model):
             )
         elif 'mixing' in turb_model or 'none' in turb_model:
             return ()
+        elif 'les_smagorinsky' in turb_model or 'les_wale' in turb_model:
+            return ()
         else:
             msg = "Reference value initialization for turbulence model "\
                     + turb_model + " is not defined"