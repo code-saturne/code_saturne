code-saturne/code_saturne

commit 7c679d513e70a271ce390226f6fa575b538d5bf5

Fix SLFM radiative properties reading

src/base/cs_setup.cpp
@@ -2677,9 +2677,6 @@ _init_user(void)
   if (cs_glob_ale != CS_ALE_NONE)
     cs_gui_mobile_mesh_structures_add();
 
-  /* Read thermomechanical data for specific physics */
-  _read_specific_physics_data();
-
   /* Other model parameters, including user-defined scalars */
   cs_gui_user_variables();
   cs_gui_user_arrays();
@@ -2704,6 +2701,9 @@ _init_user(void)
     cs_pressure_correction_model_activate();
 
   }
+
+  /* Read thermomechanical data for specific physics */
+  _read_specific_physics_data();
 
   if (cs_glob_ale != CS_ALE_NONE)
     cs_gui_ale_diffusion_type();

src/cogz/cs_combustion_read_data.cpp
@@ -508,7 +508,7 @@ _combustion_slfm_read_radiation_library(void)
                                      + ixr*nwsgg*2;
               sscanf(line, "%lf %lf", &radiation_library[shift],
                                       &radiation_library[shift + 1]);
-              _next_line(file_name, line_num, line, last, false);
+              _next_line(file_name, line_num, line, last, true);
             }
           }
   }
@@ -527,7 +527,7 @@ _combustion_slfm_read_radiation_library(void)
                                      + iki*nwsgg*2;
               sscanf(line, "%lf %lf", &radiation_library[shift],
                                       &radiation_library[shift + 1]);
-              _next_line(file_name, line_num, line, last, false);
+              _next_line(file_name, line_num, line, last, true);
             }
           }
   }

commit 8e1863b2d9fa745684272de96ff4e51bf8e75f8a

Fix crash in coal combustion, broken by 7c679d51 (2026-02-03).

src/base/cs_setup.cpp
@@ -2592,7 +2592,6 @@ _read_specific_physics_data(void)
 
   /* Pulverized coal combustion */
   if (pm_flag[CS_COMBUSTION_COAL] != -1) {
-    cs_gui_coal_model();
     cs_coal_read_data();
   }
 
@@ -2675,6 +2674,9 @@ _init_user(void)
 
   if (cs_glob_ale != CS_ALE_NONE)
     cs_gui_mobile_mesh_structures_add();
+
+  if (pm_flag[CS_COMBUSTION_COAL] != -1)
+    cs_gui_coal_model();
 
   /* Other model parameters, including user-defined scalars */
   cs_gui_user_variables();