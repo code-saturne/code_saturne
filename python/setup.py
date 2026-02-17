code-saturne/code_saturne

commit dec172cc3fbce87ebaaabda2fd02540015c9477d

GUI: first steps for PyQt6 and Pyside6 port; remove PyQt4 port.
- QRegExp objects need to be replaced by QRegularExpression objects.
  This has beed done in XMLEditorView.py, but needs to be done
  everywhere else.
- PyQt5 behavior should be unchanged.
- Qvariant constructs (unneeded since PyQt4) should now be removed
  for code simplification.

python/setup.py
@@ -55,11 +55,11 @@ def _parse_saturne_version_string(version_string):
 # ------------------------------------------------------------------------------
 
 _cs_opts = {'enable_gui':False,
-            'use_qt5':False,
+            'use_qt':None,
             'exclude_dirs':[],
-            'qtpkg':None,
             'pyuic':None,
             'pyrcc':None,
+            'rcc':None,
             'version':None
             }
 
@@ -80,13 +80,17 @@ elif '--without-gui' in sys.argv:
     _cs_opts['enable_gui'] = False
     sys.argv.remove('--without-gui')
 
-if '--use-qt5' in sys.argv:
-    _cs_opts['use_qt5'] = True
-    sys.argv.remove('--use-qt5')
-    _cs_opts['qtpkg'] = 'qt5'
+if '--use-qt=pyqt5' in sys.argv:
+    _cs_opts['use_qt'] = 'pyqt5'
+    sys.argv.remove('--use-qt=pyqt5')
+elif '--use-qt=pyqt6' in sys.argv:
+    _cs_opts['use_qt'] = 'pyqt6'
+    sys.argv.remove('--use-qt=pyqt6')
+elif '--use-qt=pyside6' in sys.argv:
+    _cs_opts['use_qt'] = 'pyside6'
+    sys.argv.remove('--use-qt=pyside6')
 else:
-    _cs_opts['use_qt5'] = False
-    _cs_opts['qtpkg'] = 'qt4'
+    _cs_opts['use_qt'] = False
 
 if '--pyuic' in sys.argv:
     index = sys.argv.index('--pyuic')
@@ -98,20 +102,30 @@ if '--pyrcc' in sys.argv:
     sys.argv.pop(index)
     _cs_opts['pyrcc'] = sys.argv.pop(index)
 
+if '--uic' in sys.argv:
+    index = sys.argv.index('--uic')
+    sys.argv.pop(index)
+    _cs_opts['uic'] = sys.argv.pop(index)
+
+if '--rcc' in sys.argv:
+    index = sys.argv.index('--rcc')
+    sys.argv.pop(index)
+    _cs_opts['rcc'] = sys.argv.pop(index)
+
 # Sanity checks
 if _cs_opts['enable_gui']:
-    if not _cs_opts['pyuic']:
-        raise Exception("--pyuic must be set to PyQt pyuic")
+    if not (_cs_opts['pyuic'] or _cs_opts['uic']):
+        raise Exception("--pyuic must be set to PyQt pyuic or uic set to Qt uic")
 
-    if not _cs_opts['pyrcc']:
-        raise Exception("--pyrcc must be set to PyQt pyrcc")
+    if not (_cs_opts['pyrcc'] or _cs_opts['rcc']):
+        raise Exception("--pyrcc must be set to PyQt pyrcc or rcc set to Qt rcc")
 else:
     _cs_opts['exclude_dirs'] = ['*gui*']
     # If gui  disabled, remove all options
     _cs_opts['pyuic'] = None
     _cs_opts['pyrcc'] = None
-    _cs_opts['qtpkg'] = None
-    _cs_opts['use_qt5'] = False
+    _cs_opts['rcc'] = None
+    _cs_opts['use_qt'] = False
 
 # ==============================================
 # Overloading the egg-info command
@@ -170,10 +184,13 @@ class _build_qt_files(_cs_build_py_dummy):
 
     def run(self):
         build_command = self.distribution.command_obj.get('build', None)
-        if build_command and _cs_opts['qtpkg']:
+        qtpkg = None
+        if _cs_opts['use_qt'] in (5, 6):
+            qtpkg = 'qt'+str(_cs_opts['use_qt'])
+        if build_command and qtpkg:
             _p1 = os.path.join('code_saturne', 'gui', 'base')
             for _f in glob.glob(os.path.join(SRC_PATH, _p1,
-                                             _cs_opts['qtpkg'],
+                                             qtpkg,
                                              '*.py')):
                 _abs_dst = os.path.join(SRC_PATH, _p1, os.path.basename(_f))
                 _nf = os.path.join(build_command.build_base,
@@ -201,12 +218,19 @@ class _build_ui_files(_cs_build_py_dummy):
 
 class _build_rc_files(_cs_build_py_dummy):
     """
-    A custom command to generate the qrc based python files using pyrcc
+    A custom command to generate the qrc based python files using pyrcc/rcc
     """
 
     def run(self):
         build_command = self.distribution.command_obj.get('build', None)
-        if build_command and _cs_opts['pyrcc']:
+        cmd = None
+        cmd_post = None
+        if _cs_opts['pyrcc']:
+            cmd = [_cs_opts['pyrcc']]
+        elif _cs_opts['rcc']:
+            cmd = [_cs_opts['rcc'], '-g', 'python']
+            cmd_post = "sed -i -e 's|PySide2|PyQt6|' -e 's|PySide6|PyQt6|'"
+        if build_command and cmd:
             for _f in glob.glob(SRC_PATH+'/**/*.qrc', recursive=True):
                 _rc_f = os.path.join(build_command.build_base,
                                      'lib',
@@ -214,8 +238,11 @@ class _build_rc_files(_cs_build_py_dummy):
                                                      SRC_PATH)
                                      )
                 _rc_dir = os.path.dirname(_f)
-                subprocess.run(args=[_cs_opts['pyrcc'], '-o', _rc_f, _f],
-                               check=True)
+                cmd += ['-o', _rc_f, _f]
+                subprocess.run(args=cmd, check=True)
+                if cmd_post:
+                    cmd_post +=  " " + _rc_f
+                    subprocess.run(cmd_post, shell=True, check=True)
 
 # =========================
 # Overload build_py command

commit b9ef845f45cd4dc555821331613dc8032f6dc258

GUI: fix install, broken by dec172cc (today).

python/setup.py
@@ -185,8 +185,12 @@ class _build_qt_files(_cs_build_py_dummy):
     def run(self):
         build_command = self.distribution.command_obj.get('build', None)
         qtpkg = None
-        if _cs_opts['use_qt'] in (5, 6):
-            qtpkg = 'qt'+str(_cs_opts['use_qt'])
+        if _cs_opts['use_qt'] == 'pyqt5':
+            qtpkg = 'qt5'
+        elif _cs_opts['use_qt'] == 'pyqt6':
+            qtpkg = 'qt6'
+        elif _cs_opts['use_qt'] == 'pyside6':
+            qtpkg = 'pyside6'
         if build_command and qtpkg:
             _p1 = os.path.join('code_saturne', 'gui', 'base')
             for _f in glob.glob(os.path.join(SRC_PATH, _p1,