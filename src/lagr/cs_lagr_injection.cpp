code-saturne/code_saturne

commit 44f63986353c38db2b14fb13706640527429d720

[Lagr] simplify the use of options.

src/lagr/cs_lagr_injection.cpp
@@ -374,8 +374,7 @@ _injection_check(const cs_lagr_injection_set_t  *zis)
   }
 
   /* temperature */
-  if (   cs_glob_lagr_model->physical_model == CS_LAGR_PHYS_HEAT
-      && (   cs_glob_lagr_specific_physics->solve_temperature == 1
+  if ((   cs_glob_lagr_specific_physics->solve_temperature == 1
           || cs_glob_lagr_specific_physics->solve_temperature_seen == 1
           || cs_glob_lagr_specific_physics->solve_diameter == 1
           || cs_glob_lagr_specific_physics->solve_mass == 1)) {
@@ -460,9 +459,8 @@ _injection_check(const cs_lagr_injection_set_t  *zis)
   }
 
   /* temperature and Cp */
-  if (   cs_glob_lagr_model->physical_model == CS_LAGR_PHYS_HEAT
-      && (    cs_glob_lagr_specific_physics->solve_temperature == 1
-           || cs_glob_lagr_specific_physics->solve_temperature_seen)) {
+  if (cs_glob_lagr_specific_physics->solve_temperature == 1
+      || cs_glob_lagr_specific_physics->solve_temperature_seen == 1) {
     cs_real_t tkelvn = -cs_physical_constants_celsius_to_kelvin;
     if (zis->cp < 0.0 && cs_glob_lagr_specific_physics->solve_temperature == 1)
       bft_error(__FILE__, __LINE__, 0,

src/lagr/cs_lagr.cpp
@@ -216,7 +216,14 @@ static cs_lagr_particle_counter_t _lagr_particle_counter
 
 /* lagr specific physics structure and associated pointer */
 static cs_lagr_specific_physics_t _cs_glob_lagr_specific_physics
-  = {0, 0, 0, 0, 0., 0.};
+  = {
+    .solve_temperature_seen = 0,
+    .solve_temperature = 0,
+    .solve_diameter = 0,
+    .solve_mass = 0,
+    .tpart = -999.,
+    .cppart = -999.};
+
 cs_lagr_specific_physics_t *cs_glob_lagr_specific_physics
   = &_cs_glob_lagr_specific_physics;
 

src/lagr/cs_lagr_car.cpp
@@ -546,10 +546,7 @@ cs_lagr_car(int                iprev,
     }
 
     /* Add buoyancy effects based on Boussinesq approximation */
-    if (    (    cs_glob_lagr_model->physical_model == CS_LAGR_PHYS_COAL
-              || cs_glob_lagr_model->physical_model == CS_LAGR_PHYS_CTWR
-              || (   cs_glob_lagr_model->physical_model == CS_LAGR_PHYS_HEAT
-                  && cs_glob_lagr_specific_physics->solve_temperature_seen == 1))
+    if (cs_glob_lagr_specific_physics->solve_temperature_seen == 1
          && cs_field_by_name_try("thermal_expansion") != nullptr
          && cs_glob_velocity_pressure_model->idilat == 0) {
 

src/lagr/cs_lagr_log.cpp
@@ -281,9 +281,8 @@ _log_setup_injection(cs_log_t  log)
                         _("    fouling index: %g\n"),
                         zis->fouling_index);
 
-        if (   cs_glob_lagr_model->physical_model == CS_LAGR_PHYS_HEAT
-            && (    cs_glob_lagr_specific_physics->solve_temperature_seen == 1
-                 || cs_glob_lagr_specific_physics->solve_temperature == 1)) {
+        if (cs_glob_lagr_specific_physics->solve_temperature_seen == 1
+            || cs_glob_lagr_specific_physics->solve_temperature == 1) {
           if (zis->temperature_profile == 0)
             cs_log_printf(log,
                           _("    temperature from fluid\n"));

src/lagr/cs_lagr_new.cpp
@@ -709,11 +709,8 @@ cs_lagr_new_particle_init(const cs_lnum_t                 particle_range[2],
 
   /* Initialize pointers (used to simplify future tests) */
 
-  if (   (   cs_glob_lagr_model->physical_model == CS_LAGR_PHYS_HEAT
-          && (    cs_glob_lagr_specific_physics->solve_temperature_seen == 1
-               || cs_glob_lagr_specific_physics->solve_temperature == 1))
-      || cs_glob_lagr_model->physical_model == CS_LAGR_PHYS_COAL
-      || cs_glob_lagr_model->physical_model == CS_LAGR_PHYS_CTWR) {
+  if (cs_glob_lagr_specific_physics->solve_temperature_seen == 1
+      || cs_glob_lagr_specific_physics->solve_temperature == 1) {
 
     if (cs_glob_physical_model_flag[CS_ATMOSPHERIC] == CS_ATMO_HUMID)
       cval_t = cs_field_by_name("real_temperature")->val; /* Humid air temp */
@@ -741,9 +738,8 @@ cs_lagr_new_particle_init(const cs_lnum_t                 particle_range[2],
 
   /* Prepare enthalpy to temperature conversion if needed */
 
-  if (   cs_glob_lagr_model->physical_model == CS_LAGR_PHYS_HEAT
-      && (    cs_glob_lagr_specific_physics->solve_temperature_seen == 1
-           || cs_glob_lagr_specific_physics->solve_temperature == 1)
+  if ((cs_glob_lagr_specific_physics->solve_temperature_seen == 1
+        || cs_glob_lagr_specific_physics->solve_temperature == 1)
       && cval_t == nullptr
       && cval_h != nullptr) {
 
@@ -1291,8 +1287,7 @@ cs_lagr_new_particle_init(const cs_lnum_t                 particle_range[2],
                                    zis->density * pis6 * d3
                                    * pow(1.0-mporos, 3));
 
-      if (   cs_glob_lagr_model->physical_model == CS_LAGR_PHYS_HEAT
-          && cs_glob_lagr_specific_physics->solve_temperature_seen == 1
+      if (cs_glob_lagr_specific_physics->solve_temperature_seen == 1
           && cval_t != nullptr) {
 
         /* Set Seen temperature */

src/lagr/cs_lagr_options.cpp
@@ -229,18 +229,6 @@ cs_lagr_options_definition(int         is_restart,
 
   lagr_model->physical_model = CS_LAGR_PHYS_OFF;
 
-  cs_glob_lagr_specific_physics->solve_diameter = 0;
-
-  cs_glob_lagr_specific_physics->solve_temperature_seen = 0;
-
-  cs_glob_lagr_specific_physics->solve_temperature = 0;
-
-  cs_glob_lagr_specific_physics->solve_mass = 0;
-
-  cs_glob_lagr_specific_physics->tpart = -999.0;
-
-  cs_glob_lagr_specific_physics->cppart = -999.0;
-
   lagr_model->fouling = 0;
 
   /* Initializations for physical models */

src/lagr/cs_lagr_sde_model.cpp
@@ -1881,25 +1881,22 @@ cs_lagr_sde_model(const cs_lnum_t    p_id,
 {
   /* Integration of temperature seen by particles */
 
-  if (   cs_glob_lagr_model->physical_model == CS_LAGR_PHYS_COAL
-      || cs_glob_lagr_model->physical_model == CS_LAGR_PHYS_CTWR
-      || (   cs_glob_lagr_model->physical_model == CS_LAGR_PHYS_HEAT
-          && cs_glob_lagr_specific_physics->solve_temperature_seen == 1))
+  if (cs_glob_lagr_specific_physics->solve_temperature_seen == 1)
     _sde_i_temp_seen(p_id, dt_part, nor);
 
-  /* Integration of particles temperature */
+  /* Integration of particles temperature (CTWR and COAL done after) */
 
   if (   cs_glob_lagr_model->physical_model == CS_LAGR_PHYS_HEAT
       && cs_glob_lagr_specific_physics->solve_temperature == 1)
     _lagitp(p_id, dt_part, nor, tempct);
 
-  /* Integration of particles diameter */
+  /* Integration of particles diameter (CTWR and COAL done after) */
 
   if (   cs_glob_lagr_model->physical_model == CS_LAGR_PHYS_HEAT
       && cs_glob_lagr_specific_physics->solve_diameter == 1)
     _lagidp(p_id, dt_part, nor);
 
-  /* Integration of particles mass */
+  /* Integration of particles mass (CTWR and COAL done after) */
 
   if (   cs_glob_lagr_model->physical_model == CS_LAGR_PHYS_HEAT
       && cs_glob_lagr_specific_physics->solve_mass == 1)

commit af6c4a6b8379781052262d53cf9cf0c0b4168025

Lagrangian model: fix coal combustion, broken by 44f63986 (2025-09-17).

src/lagr/cs_lagr_injection.cpp
@@ -463,7 +463,9 @@ _injection_check(const cs_lagr_injection_set_t  *zis)
   if (cs_glob_lagr_specific_physics->solve_temperature == 1
       || cs_glob_lagr_specific_physics->solve_temperature_seen == 1) {
     cs_real_t tkelvn = -cs_physical_constants_celsius_to_kelvin;
-    if (zis->cp < 0.0 && cs_glob_lagr_specific_physics->solve_temperature == 1)
+    if (   cs_glob_lagr_specific_physics->solve_temperature == 1
+        && cs_glob_lagr_model->physical_model != CS_LAGR_PHYS_COAL
+        && zis->cp < 0.0)
       bft_error(__FILE__, __LINE__, 0,
                 _("Lagrangian %s zone %d, set %d:\n"
                   "  specific heat capacity (%g) is negative."),