code-saturne/code_saturne

commit 0933ca8a83d1edf77efd4e48ebffa08f4eae8df5

[HOUSEKEEPING] continue deploying new field API

src/turb/cs_turbulence_model.cpp
@@ -1263,23 +1263,23 @@ cs_turb_compute_constants(int phase_id)
   }
 
   if (f_k != nullptr)
-    cs_field_set_key_double(f_k, k_turb_schmidt, 1.);
+    f_k->set_key_double(k_turb_schmidt, 1.);
 
   if (f_phi != nullptr)
-    cs_field_set_key_double(f_phi, k_turb_schmidt, 1.);
+    f_phi->set_key_double(k_turb_schmidt, 1.);
 
   if (   cs_glob_turb_model->model == CS_TURB_RIJ_EPSILON_LRR
       || cs_glob_turb_model->model == CS_TURB_RIJ_EPSILON_SSG
       || cs_glob_turb_model->model == CS_TURB_RIJ_EPSILON_BFH)
-    cs_field_set_key_double(f_eps, k_turb_schmidt, 1.22);
+    f_eps->set_key_double(k_turb_schmidt, 1.22);
   else if (cs_glob_turb_model->model == CS_TURB_RIJ_EPSILON_EBRSM) {
-    cs_field_set_key_double(f_eps, k_turb_schmidt, 1.15);
+    f_eps->set_key_double(k_turb_schmidt, 1.15);
     cs_turb_crij3 = 0.6;
   }
   else if (cs_glob_turb_model->model == CS_TURB_V2F_BL_V2K)
-    cs_field_set_key_double(f_eps, k_turb_schmidt, 1.5);
+    f_eps->set_key_double(k_turb_schmidt, 1.5);
   else
-    cs_field_set_key_double(f_eps, k_turb_schmidt, 1.30);
+    f_eps->set_key_double(k_turb_schmidt, 1.30);
 
   if (cs_glob_turb_rans_model->idirsm == 0)
     cs_turb_csrij = 0.11;
@@ -2090,7 +2090,7 @@ cs_turb_constants_log_setup(void)
     if (!(f->type & CS_FIELD_VARIABLE))
       continue;
 
-    const int turb_flux_model = cs_field_get_key_int(f, kturt);
+    const int turb_flux_model = f->get_key_int(kturt);
     const int turb_flux_model_type = turb_flux_model / 10;
     has_dfm = has_dfm || (turb_flux_model_type == 3);
   }
@@ -2169,7 +2169,7 @@ cs_clip_turbulent_fluxes(int  flux_id,
   /* Get clippings field for DFM */
   cs_lnum_t kclipp, clip_rit_id;
   kclipp = cs_field_key_id("clipping_id");
-  clip_rit_id = cs_field_get_key_int(field_rit, kclipp);
+  clip_rit_id = field_rit->get_key_int(kclipp);
   if (clip_rit_id >= 0) {
     cvar_clip_rit = (cs_real_3_t *) cs_field_by_id(clip_rit_id)->val;
     for (cs_lnum_t cell_id = 0; cell_id < n_cells; cell_id++) {

src/turb/cs_les_balance.cpp
@@ -706,7 +706,7 @@ _les_balance_compute_gradients(void)
 
     for (int f_id = 0; f_id < cs_field_n_fields(); f_id ++) {
       cs_field_t *f = cs_field_by_id(f_id);
-      int isca = cs_field_get_key_int(f, keysca);
+      int isca = f->get_key_int(keysca);
       if (isca > 0) {
         const cs_equation_param_t *eqps
           = cs_field_get_equation_param_const(f);
@@ -1080,8 +1080,7 @@ _les_balance_compute_djtdjui(const void   *input,
   const cs_field_t *sca = (const cs_field_t *)input;
   const cs_lnum_t n_cells = cs_glob_mesh->n_cells;
 
-  const int keysca = cs_field_key_id("scalar_id");
-  int isca = cs_field_get_key_int(sca, keysca) - 1;
+  int isca = sca->get_key_int("scalar_id") - 1;
   assert(isca > -1);
 
   cs_dispatch_context ctx;
@@ -1157,8 +1156,7 @@ _les_balance_compute_uidjt(const void   *input,
 
   cs_dispatch_context ctx;
 
-  const int keysca = cs_field_key_id("scalar_id");
-  int isca = cs_field_get_key_int(sca, keysca) - 1;
+  int isca = sca->get_key_int("scalar_id") - 1;
   assert(isca > -1);
 
   cs_real_3_t *grdt = (cs_real_3_t *)_gradt[isca]->val;
@@ -1187,8 +1185,7 @@ _les_balance_compute_ditdit(const void   *input,
   const cs_field_t *sca = (const cs_field_t *)input;
   const cs_lnum_t n_cells = cs_glob_mesh->n_cells;
 
-  const int keysca = cs_field_key_id("scalar_id");
-  int isca = cs_field_get_key_int(sca, keysca) - 1;
+  int isca = sca->get_key_int("scalar_id") - 1;
   assert(isca > -1);
 
   cs_dispatch_context ctx;
@@ -1228,7 +1225,7 @@ _les_balance_compute_tdjtauij(const void   *input,
 
   for (int f_id = 0; f_id < cs_field_n_fields(); f_id++) {
     cs_field_t *f = cs_field_by_id(f_id);
-    if (cs_field_get_key_int(f, keysca) > 0) {
+    if (f->get_key_int(keysca) > 0) {
       if (f_id == sca->id)
         break;
     }
@@ -1277,13 +1274,12 @@ _les_balance_compute_uidivturflux(const void   *input,
   const cs_lnum_t n_cells = cs_glob_mesh->n_cells;
   const cs_lnum_t n_cells_ext = cs_glob_mesh->n_cells_with_ghosts;
   const int keysca = cs_field_key_id("scalar_id");
-  const int ksigmas = cs_field_key_id("turbulent_schmidt");
 
   cs_dispatch_context ctx;
 
   for (int f_id = 0; f_id < cs_field_n_fields(); f_id++) {
     cs_field_t *f = cs_field_by_id(f_id);
-    if (cs_field_get_key_int(f, keysca) > 0) {
+    if (f->get_key_int(keysca) > 0) {
       if (f_id == sca->id)
         break;
     }
@@ -1293,7 +1289,7 @@ _les_balance_compute_uidivturflux(const void   *input,
 
   auto vel = CS_F_(vel)->get_vals_v();
 
-  sigmas = cs_field_get_key_double(sca, ksigmas);
+  sigmas = sca->get_key_double("turbulent_schmidt");
 
   cs_array<cs_real_t> diverg(n_cells_ext, cs_alloc_mode);
   cs_array_2d<cs_real_t> w1(n_cells, 3, cs_alloc_mode);
@@ -1339,16 +1335,14 @@ _les_balance_compute_tdivturflux(const void   *input,
   const cs_field_t *sca = (const cs_field_t *)input;
   const cs_lnum_t n_cells = cs_glob_mesh->n_cells;
   const cs_lnum_t n_cells_ext = cs_glob_mesh->n_cells_with_ghosts;
-  const int keysca = cs_field_key_id("scalar_id");
-  const int ksigmas = cs_field_key_id("turbulent_schmidt");
-  int isca = cs_field_get_key_int(sca, keysca) - 1;
+  int isca = sca->get_key_int("scalar_id") - 1;
   assert(isca > -1);
 
   cs_dispatch_context ctx;
 
   cs_real_t sigmas;
 
-  sigmas = cs_field_get_key_double(sca, ksigmas);
+  sigmas = sca->get_key_double("turbulent_schmidt");
 
   cs_array<cs_real_t> diverg(n_cells_ext, cs_alloc_mode);
   cs_array_2d<cs_real_t> w1(n_cells, 3, cs_alloc_mode);
@@ -1392,8 +1386,7 @@ _les_balance_compute_nutditdit(const void   *input,
 
   cs_dispatch_context ctx;
 
-  const int keysca = cs_field_key_id("scalar_id");
-  int isca = cs_field_get_key_int(sca, keysca) - 1;
+  int isca = sca->get_key_int("scalar_id") - 1;
   assert(isca > -1);
 
   cs_real_3_t *grdt = (cs_real_3_t *)_gradt[isca]->val;
@@ -1429,8 +1422,7 @@ _les_balance_compute_nutuidjt(const void   *input,
 
   cs_dispatch_context ctx;
 
-  const int keysca = cs_field_key_id("scalar_id");
-  int isca = cs_field_get_key_int(sca, keysca) - 1;
+  int isca = sca->get_key_int("scalar_id") - 1;
   assert(isca > -1);
 
   cs_real_3_t *grdt = (cs_real_3_t *)_gradt[isca]->val;
@@ -1462,8 +1454,7 @@ _les_balance_compute_nutdjuidjt(const void   *input,
 
   cs_dispatch_context ctx;
 
-  const int keysca = cs_field_key_id("scalar_id");
-  int isca = cs_field_get_key_int(sca, keysca) - 1;
+  int isca = sca->get_key_int("scalar_id") - 1;
   assert(isca > -1);
 
   cs_real_33_t *grdv = (cs_real_33_t *)_gradv->val;
@@ -2017,7 +2008,7 @@ _les_balance_time_moment_tui(void)
   /* Define time moments for T.ui balance */
   for (int f_id = 0; f_id < cs_field_n_fields(); f_id ++) {
     cs_field_t *f = cs_field_by_id(f_id);
-    int iscal = cs_field_get_key_int(f, keysca)-1;
+    int iscal = f->get_key_int(keysca)-1;
     if (iscal > -1) {
       {
         /* t mean */
@@ -2672,7 +2663,7 @@ _les_balance_initialize_tui(void)
 
   for (int f_id = 0; f_id < cs_field_n_fields(); f_id ++) {
     cs_field_t *f = cs_field_by_id(f_id);
-    if (cs_field_get_key_int(f, keysca) > 0) {
+    if (f->get_key_int(keysca) > 0) {
       _les_balance.btui[iscal]->f_id = f_id;
       iscal++;
     }
@@ -2918,7 +2909,7 @@ cs_les_balance_create_fields(void)
 
     for (int f_id = 0; f_id < cs_field_n_fields(); f_id ++) {
       cs_field_t *f_sca = cs_field_by_id(f_id);
-      int i_sca = cs_field_get_key_int(f_sca, k_sca);
+      int i_sca = f_sca->get_key_int(k_sca);
       if (i_sca > 0) n_scal ++;
     }
 
@@ -2927,7 +2918,7 @@ cs_les_balance_create_fields(void)
     for (int f_id = 0; f_id < cs_field_n_fields(); f_id ++) {
       cs_field_t *f_sca = cs_field_by_id(f_id);
 
-      int i_sca = cs_field_get_key_int(f_sca, k_sca)-1;
+      int i_sca = f_sca->get_key_int(k_sca)-1;
       if (i_sca > -1) {
         char *name;
         int len = strlen(f_sca->name)+6;
@@ -2984,7 +2975,7 @@ cs_les_balance_create(void)
 
   for (int f_id = 0; f_id < cs_field_n_fields(); f_id ++) {
     cs_field_t *f = cs_field_by_id(f_id);
-    int isca = cs_field_get_key_int(f, keysca);
+    int isca = f->get_key_int(keysca);
     if (isca > 0) nscal++;
   }
 
@@ -3023,7 +3014,7 @@ cs_les_balance_create(void)
   for (int m_id = 0; m_id < n_moments; m_id++) {
     cs_field_t *f = cs_time_moment_get_field(m_id);
     if (f != nullptr && !cs_field_is_key_set(f, log_key_id))
-      cs_field_set_key_int(f, log_key_id, 1);
+      f->set_key_int(log_key_id, 1);
   }
 #endif
 }
@@ -3580,8 +3571,8 @@ cs_les_balance_compute_tui(void)
     cs_les_balance_tui_t *b_sca = _les_balance.btui[isca];
 
     cs_field_t *sca = cs_field_by_id(b_sca->f_id);
-    cs_real_t sigmas = cs_field_get_key_double(sca, ksigmas);
-    cs_real_t visls0 = cs_field_get_key_double(sca, kvisls0);
+    cs_real_t sigmas = sca->get_key_double(ksigmas);
+    cs_real_t visls0 = sca->get_key_double(kvisls0);
     cs_real_t xvistot = visls0 + viscl0;
 
     /* Time moments retrieved by name */

src/turb/cs_turbulence_bc.cpp
@@ -590,9 +590,9 @@ cs_turbulence_bc_init_pointers(void)
   for (int f_id = 0; f_id < n_fields; f_id++) {
     const cs_field_t *f = cs_field_by_id(f_id);
     if (f->type & CS_FIELD_VARIABLE) {
-      int s_num = cs_field_get_key_int(f, k_sca);
+      int s_num = f->get_key_int(k_sca);
       if (s_num > 0) {
-        int f_turbt = cs_field_get_key_int(f, k_turbt) ;
+        int f_turbt = f->get_key_int(k_turbt) ;
         if (f_turbt / 10 == 3)
           n_sca_ut ++;
         if (f_turbt == 11 || f_turbt == 21 || f_turbt == 31)
@@ -615,16 +615,16 @@ cs_turbulence_bc_init_pointers(void)
   for (int f_id = 0; f_id < n_fields; f_id++) {
     const cs_field_t *f = cs_field_by_id(f_id);
     if (f->type & CS_FIELD_VARIABLE) {
-      int s_num = cs_field_get_key_int(f, k_sca);
+      int s_num = f->get_key_int(k_sca);
       if (s_num > 0) {
-        int f_turbt = cs_field_get_key_int(f, k_turbt) ;
+        int f_turbt = f->get_key_int(k_turbt) ;
         if (f_turbt / 10 == 3) {
-          int fid_turbt = cs_field_get_key_int(f, k_f_turbt);
+          int fid_turbt = f->get_key_int(k_f_turbt);
           _turb_bc_id.f_ut[n_sca_ut] = cs_field_by_id(fid_turbt);
           n_sca_ut ++;
         }
         if (f_turbt == 11 || f_turbt == 21 || f_turbt == 31) {
-          int fid_turbt = cs_field_get_key_int(f, k_f_turbt_alp_bl);
+          int fid_turbt = f->get_key_int(k_f_turbt_alp_bl);
           _turb_bc_id.f_alp_bl_t[n_sca_alp_bl] = cs_field_by_id(fid_turbt);
           n_sca_alp_bl ++;
         }

src/turb/cs_turbulence_rij.cpp
@@ -340,8 +340,7 @@ _compute_up_rhop(int                 phase_id,
 
   const cs_field_t *thf = cs_thermal_model_field();
   if (thf != nullptr) {
-    const int ksigmas = cs_field_key_id("turbulent_schmidt");
-    const cs_real_t turb_schmidt = cs_field_get_key_double(thf, ksigmas);
+    const cs_real_t turb_schmidt = thf->get_key_double("turbulent_schmidt");
     cons = -1.5 * cs_turb_cmu / turb_schmidt;
   }
   const cs_velocity_pressure_model_t  *vp_model
@@ -934,17 +933,15 @@ _gravity_st_epsilon(int              phase_id,
 
     cs_field_t *f_t_var = cs_field_get_variance(f_t);
     if (f_t_var != nullptr)
-      rvarfl = cs_field_get_key_double(f_t_var, krvarfl);
+      rvarfl = f_t_var->get_key_double(krvarfl);
 
-    const int kivisl = cs_field_key_id("diffusivity_id");
-    int ifcvsl = f_t->get_key_int(kivisl);
+    int ifcvsl = f_t->get_key_int("diffusivity_id");
     if (ifcvsl > -1) {
       viscls = cs_field(ifcvsl)->val;
       l_viscls = 1;
     }
     else {
-      const int kvisls0 = cs_field_key_id("diffusivity_ref");
-      _visls_0 = cs_field_get_key_double(f_t, kvisls0);
+      _visls_0 = f_t->get_key_double("diffusivity_ref");
       viscls = &_visls_0;
       l_viscls = 0;
     }
@@ -2517,8 +2514,7 @@ _solve_epsilon(int              phase_id,
   const cs_real_t *imasfl = cs_field(iflmas)->val;
   const cs_real_t *bmasfl = cs_field(iflmab)->val;
 
-  const int ksigmas = cs_field_key_id("turbulent_schmidt");
-  const cs_real_t sigmae = cs_field_get_key_double(f_eps, ksigmas);
+  const cs_real_t sigmae = f_eps->get_key_double("turbulent_schmidt");
 
   const cs_equation_param_t *eqp
     = cs_field_get_equation_param(f_eps);

commit b07864abed5b1d0509d34d58f6f1155f19de0ba9

[TURB] Fix potential segfault during initialization with k-omega runs
  * Added safety check to epsilon pointer
  * bug introduced in commit 0933ca8a83

src/turb/cs_turbulence_model.cpp
@@ -1278,8 +1278,10 @@ cs_turb_compute_constants(int phase_id)
   }
   else if (cs_glob_turb_model->model == CS_TURB_V2F_BL_V2K)
     f_eps->set_key_double(k_turb_schmidt, 1.5);
-  else
-    f_eps->set_key_double(k_turb_schmidt, 1.30);
+  else {
+    if (f_eps != nullptr)
+      f_eps->set_key_double(k_turb_schmidt, 1.30);
+  }
 
   if (cs_glob_turb_rans_model->idirsm == 0)
     cs_turb_csrij = 0.11;