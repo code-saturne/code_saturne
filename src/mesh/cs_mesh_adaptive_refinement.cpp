code-saturne/code_saturne

commit f0a567a73b37230e7262e010941c88e41de26184

AMR: try to be more consistent in mesh auxiliary data updates.

src/mesh/cs_mesh_adaptive_refinement.cpp
@@ -57,6 +57,7 @@
 #include "alge/cs_matrix_default.h"
 
 #include "base/cs_array.h"
+#include "base/cs_ext_neighborhood.h"
 #include "base/cs_field.h"
 #include "base/cs_field_operator.h"
 #include "base/cs_field_pointer.h"
@@ -65,6 +66,7 @@
 #include "mesh/cs_mesh.h"
 #include "mesh/cs_mesh_adjacencies.h"
 #include "mesh/cs_mesh_coarsen.h"
+#include "mesh/cs_mesh_coherency.h"
 #include "mesh/cs_mesh_location.h"
 #include "mesh/cs_mesh_quantities.h"
 #include "mesh/cs_mesh_refine.h"
@@ -1179,26 +1181,66 @@ _load_balance(bool write_stats)
 #endif // defined(HAVE_MPI)
 
 /*----------------------------------------------------------------------------
- * Update mesh data after refinement / coarsening steps.
+ * Update mesh data after first refinement / coarsening steps.
+ *
+ * Most mesh data should not be needed between stages, but interpolation
+ * may require that at least some elements are available.
+ *
+ * parameters:
+ *   mesh         <-- pointer to the mesh
  *----------------------------------------------------------------------------*/
 
 static void
-_update_post_adaptation_mesh_data(void)
+_update_intermediate_mesh_data(cs_mesh_t  *mesh)
 {
-  cs_mesh_t *mesh = cs_glob_mesh;
-
-  /* Renumber mesh based on code options */
-
-  cs_renumber_mesh(mesh, nullptr, nullptr, nullptr, nullptr);
-
-  /* Free and re-compute mesh related quantities */
+  /* Free quantities that will become invalid and can be recomputed */
 
   cs_mesh_quantities_free_all(cs_glob_mesh_quantities);
+  cs_gradient_free_quantities();
+  cs_adaptive_refinement_free_gradients();
+
+  /* Re-compute mesh related quantities */
+
   cs_mesh_quantities_compute(mesh, cs_glob_mesh_quantities);
+  cs_ext_neighborhood_reduce(mesh, cs_glob_mesh_quantities);
 
   /* Update mesh adjacencies */
 
   cs_mesh_adjacencies_update_mesh();
+
+  /* Reset boundary conditions (may be needed for gradients used
+     in interpolation of refined fields) */
+
+  cs_boundary_conditions_realloc();
+  cs_field_map_and_init_bcs();
+}
+
+/*----------------------------------------------------------------------------
+ * Update mesh data after refinement / coarsening steps.
+ *
+ * parameters:
+ *   mesh   <-- pointer to the mesh
+ *----------------------------------------------------------------------------*/
+
+static void
+_update_post_adaptation_mesh_data(cs_mesh_t  *mesh)
+{
+  /* Free quantities that will become invalid and can be recomputed */
+
+  cs_mesh_quantities_free_all(cs_glob_mesh_quantities);
+  cs_gradient_free_quantities();
+  cs_adaptive_refinement_free_gradients();
+
+  /* Renumber mesh based on code options. Only do it if no load balancing
+     is to be done, as it would otherwise be redundant. */
+
+  cs_renumber_mesh(mesh, nullptr, nullptr, nullptr, nullptr);
+
+  /* Re-compute mesh related quantities */
+
+  cs_mesh_update_auxiliary(mesh);
+  cs_mesh_quantities_compute(mesh, cs_glob_mesh_quantities);
+  cs_ext_neighborhood_reduce(mesh, cs_glob_mesh_quantities);
 
   /* Initialize selectors and locations for the mesh */
 
@@ -1207,16 +1249,17 @@ _update_post_adaptation_mesh_data(void)
   cs_volume_zone_build_all(true);
   cs_boundary_zone_build_all(true);
 
+#if defined(DEBUG)
+  cs_mesh_coherency_check();
+#endif
+
+  cs_mesh_adjacencies_update_mesh();
+  cs_matrix_update_mesh();
+
   /* Reset boundary conditions */
 
   cs_boundary_conditions_realloc();
   cs_field_map_and_init_bcs();
-
-  /* Free gradients and related quantities since they can be recomputed
-   * on-the-fly */
-
-  cs_gradient_free_quantities();
-  cs_adaptive_refinement_free_gradients();
 }
 
 /*----------------------------------------------------------------------------
@@ -1251,8 +1294,6 @@ _refine_step(const int indic[])
                                  selected_cells);
 
   CS_FREE(selected_cells);
-
-  _update_post_adaptation_mesh_data();
 }
 
 /*----------------------------------------------------------------------------
@@ -1279,8 +1320,6 @@ _coarsen_step(const int indic[])
                                   selected_cells);
 
   CS_FREE(selected_cells);
-
-  _update_post_adaptation_mesh_data();
 }
 
 /*============================================================================
@@ -1438,25 +1477,25 @@ cs_adaptive_refinement_step(void)
 
   _coarsen_step(indic);
 
+  _update_intermediate_mesh_data(mesh);
+
   _refine_step(indic);
 
   CS_FREE(indic);
 
-  /* Post-adaptation updates */
+  /* We could use a more advanced test to perform load balancing only
+     if imbalance reaches a certain threshold. */
+  bool load_balance = (cs_glob_n_ranks > 1) ? true : false;
 
-  cs_gradient_free_quantities();
+  if (load_balance == false)
+    _update_post_adaptation_mesh_data(mesh);
 
   /* Perform load balancing */
 
 #if defined(HAVE_MPI)
-  if (cs_glob_n_ranks > 1) {
-    bool write_stats = true;
-    _load_balance(write_stats);
-  }
+  if (load_balance)
+    _load_balance(true);
 #endif
-
-  cs_mesh_adjacencies_update_mesh();
-  cs_matrix_update_mesh();
 
   if (cs_log_default_is_active())
     cs_log_printf(CS_LOG_DEFAULT,

src/mesh/cs_redistribute.cpp
@@ -762,8 +762,17 @@ cs_redistribute(const int                cell_dest_rank[],
 {
 #if defined(HAVE_MPI)
 
-  if (cs_glob_rank_id < 0)
+  if (cs_glob_n_ranks == 1)
     return;
+
+  /* Free some elements which will need to be rebuilt when/if used. */
+
+  cs_mesh_free_rebuildable(cs_glob_mesh, true);
+  cs_mesh_quantities_free_all(cs_glob_mesh_quantities);
+
+  _free_field_gradients();
+  cs_gradient_free_quantities();
+  cs_cell_to_vertex_free();
 
   static int iter = 0;
   iter++;
@@ -795,7 +804,7 @@ cs_redistribute(const int                cell_dest_rank[],
   cs_all_to_all_t *cd = nullptr;
   int cd_flags = 0;
 
-  // build the cell part-to-part distributor
+  // Build the cell part-to-part distributor.
 
   cs_lnum_t n_cells = 0;
 
@@ -860,7 +869,7 @@ cs_redistribute(const int                cell_dest_rank[],
                                                   false,
                                                   mesh->global_cell_num);
 
-  // Sort global cell numbers in increasing order
+  // Sort global cell numbers in increasing order;
   // this order will be used for the exchange of any cell-centered data.
 
   cs_lnum_t *cell_order = cs_order_gnum(nullptr, cell_gnum, n_cells);
@@ -1030,17 +1039,17 @@ cs_redistribute(const int                cell_dest_rank[],
   /* Interior faces
      -------------- */
 
-  // We need the destination of the halo cells.
+  // We need the destination and current ranks of the halo cells.
+
   cs_halo_sync_untyped(mesh->halo,
-                       mesh->halo_type,
+                       CS_HALO_STANDARD,
                        sizeof(int),
                        _dest_rank);
 
-  // We need the ranks of the halo cells.
   int *cell_rank;
   CS_MALLOC(cell_rank, mesh->n_cells_with_ghosts, int);
   for (int i = 0; i < mesh->n_cells; i++) cell_rank[i] = cs_glob_rank_id;
-  cs_halo_sync_untyped(mesh->halo, mesh->halo_type, sizeof(int), cell_rank);
+  cs_halo_sync_untyped(mesh->halo, CS_HALO_STANDARD, sizeof(int), cell_rank);
 
   // List of internal faces to distribute, and their destination.
   // one internal face can appear twice in the list.
@@ -1289,7 +1298,7 @@ cs_redistribute(const int                cell_dest_rank[],
   // for example by splitting the vertices into two parts: inner vertices
   // (shared by pure internal/boundary faces only), and outer vertices
   // (those that are connected to faces shared by neighbouring procs).
-  // TODO(Imad): distribute vertex-centered fields.
+  // TODO: distribute vertex-based fields.
 
   cs_all_to_all_t *vd
     = cs_all_to_all_create_from_block(n_vertices,
@@ -1398,31 +1407,36 @@ cs_redistribute(const int                cell_dest_rank[],
   // - periodicity features
   // - face status flags
 
-  // update the halo
-  cs_mesh_builder_destroy(&builder);
-  cs_mesh_free_rebuildable(mesh, true);
-  mesh->n_cells_with_ghosts = mesh->n_cells;
-  cs_mesh_init_halo(mesh, nullptr, mesh->halo_type, -1, true);
-  cs_mesh_update_auxiliary(mesh);
+  // Update the halo
 
-  cs_lnum_t *cell_n2o, *i_face_n2o, *b_face_n2o, *vtx_n2o;
-  cell_n2o = i_face_n2o = b_face_n2o = vtx_n2o = nullptr;
+  cs_mesh_builder_destroy(&builder);
+
+  mesh->n_cells_with_ghosts = mesh->n_cells;
+
+  cs_mesh_init_halo(mesh, nullptr, mesh->halo_type, -1, true);
+
+  cs_lnum_t *cell_n2o = nullptr, *vtx_n2o= nullptr;
+  cs_lnum_t *i_face_n2o = nullptr, *b_face_n2o = nullptr;
+
   cs_renumber_mesh(mesh, &cell_n2o, &i_face_n2o, &b_face_n2o, &vtx_n2o);
+
+  cs_mesh_update_auxiliary(mesh);
+  cs_mesh_quantities_compute(cs_glob_mesh,cs_glob_mesh_quantities);
+  cs_ext_neighborhood_reduce(mesh, cs_glob_mesh_quantities);
 
   cs_mesh_init_group_classes(mesh);
 
-  cs_mesh_quantities_free_all(cs_glob_mesh_quantities);
-  cs_mesh_quantities_compute(cs_glob_mesh,cs_glob_mesh_quantities);
-
   cs_mesh_update_selectors(mesh);
   cs_mesh_location_build(mesh, -1);
-  bool mesh_modified = true;
-  cs_volume_zone_build_all(mesh_modified);
-  cs_boundary_zone_build_all(mesh_modified);
+  cs_volume_zone_build_all(true);
+  cs_boundary_zone_build_all(true);
 
-  cs_ext_neighborhood_reduce(mesh, cs_glob_mesh_quantities);
-
+#if defined(DEBUG)
   cs_mesh_coherency_check();
+#endif
+
+  cs_mesh_adjacencies_update_mesh();
+  cs_matrix_update_mesh();
 
   /* Field distribution
      ------------------ */
@@ -1442,12 +1456,6 @@ cs_redistribute(const int                cell_dest_rank[],
                      i_face_n2o);
 
   _distribute_data(cd, cell_order, cell_n2o, data);
-
-  _free_field_gradients();
-  cs_gradient_free_quantities();
-  cs_cell_to_vertex_free();
-  cs_mesh_adjacencies_update_mesh();
-  cs_matrix_update_mesh();
 
   CS_FREE(vtx_n2o);
   CS_FREE(cell_n2o);

src/mesh/cs_mesh_coarsen.cpp
@@ -1999,16 +1999,16 @@ cs_mesh_coarsen_simple(cs_mesh_t  *m,
 
   /* Now merge interior faces */
 
-  cs_lnum_t  *i_f_o2n = nullptr;
-  cs_lnum_t  n_i_f_new = _i_faces_equiv(m, &i_f_o2n);
+  cs_lnum_t *i_f_o2n = nullptr;
+  cs_lnum_t n_i_f_new = _i_faces_equiv(m, &i_f_o2n);
   cs_lnum_t n_i_old = m->n_i_faces;
 
   _merge_i_faces(m, n_i_f_new, i_f_o2n);
 
   /* Then merge boundary faces */
 
-  cs_lnum_t  *b_f_o2n = nullptr;
-  cs_lnum_t  n_b_f_new = _b_faces_equiv(m, c_flag_n, &b_f_o2n);
+  cs_lnum_t *b_f_o2n = nullptr;
+  cs_lnum_t n_b_f_new = _b_faces_equiv(m, c_flag_n, &b_f_o2n);
   cs_lnum_t n_b_old = m->n_b_faces;
 
   _merge_b_faces(m, n_b_f_new, b_f_o2n);
@@ -2028,7 +2028,7 @@ cs_mesh_coarsen_simple(cs_mesh_t  *m,
   m->n_b_faces_all = m->n_b_faces;
   m->modified |= (CS_MESH_MODIFIED | CS_MESH_MODIFIED_BALANCE);
 
-  /* Rebuild auxiliary information  */
+  /* Rebuild auxiliary information */
 
   mv_save = m->verbosity;
   m->verbosity = -1;
@@ -2037,11 +2037,11 @@ cs_mesh_coarsen_simple(cs_mesh_t  *m,
 
   m->verbosity = mv_save;
 
-  /* Update fields : memory reallocation and interpolation */
+  /* Update fields: memory reallocation and interpolation */
 
   if (cs_glob_amr_info->is_set) {
 
-    cs_mesh_init_selectors();
+    cs_mesh_update_selectors(m);
     cs_mesh_location_build(m, -1);
 
     cs_amr_coarsening_interpolation_t *_realloc_and_interp_fields =

src/mesh/cs_mesh_refine.cpp
@@ -6454,7 +6454,7 @@ cs_mesh_refine_simple(cs_mesh_t  *m,
 
   m->verbosity = mv_save;
 
-  /* Update fields : memory reallocation and interpolation */
+  /* Update fields: memory reallocation and interpolation */
 
   if (cs_glob_amr_info->is_set) {
 
@@ -6463,9 +6463,9 @@ cs_mesh_refine_simple(cs_mesh_t  *m,
     cs_mesh_quantities_compute(cs_glob_mesh,cs_glob_mesh_quantities);
     cs_mesh_quantities_t *mq = cs_glob_mesh_quantities;
 
-    /* We need to rebuilt mesh locations, in order to
+    /* We need to rebuild mesh locations, in order to
        properly reallocate fields (espacially bc_coeffs) */
-    cs_mesh_init_selectors();
+    cs_mesh_update_selectors(m);
     cs_mesh_location_build(m, -1);
 
     cs_amr_refinement_interpolation_t *_realloc_and_interp_fields =

commit 44fc3528c15362e93f5c2e3c9a9b6900a5afd1aa

AMR: cleanup and fixes for mesh redistribution, broken by f0a567a7 (2026-02-24).

src/mesh/cs_mesh_adaptive_refinement.cpp
@@ -1108,7 +1108,7 @@ _load_balance(bool write_stats)
   int n_ranks = cs_glob_n_ranks;
 
   int *cell_rank = nullptr;
-  CS_MALLOC(cell_rank, mesh->n_cells, int);
+  CS_MALLOC(cell_rank, mesh->n_cells_with_ghosts, int);
 
   // Non-uniform block size.
 
@@ -1128,6 +1128,9 @@ _load_balance(bool write_stats)
         cell_rank[i] = (cell_num[i] + n_ranks_rmdr - 1) / (cells_per_rank + 1);
     }
   }
+
+  if (mesh->halo != nullptr)
+    cs_halo_sync_untyped(mesh->halo, CS_HALO_STANDARD, sizeof(int), cell_rank);
 
   fvm_io_num_destroy(cell_io_num);
 

src/mesh/cs_redistribute.cpp
@@ -300,23 +300,6 @@ _distribute_bc_type(cs_all_to_all_t  *bfd,
 }
 
 /*----------------------------------------------------------------------------
- * Compute random destination ranks for cells.
- *
- * parameters:
- *   mesh           <-- pointer to mesh
- *   cell_dest      --> cells destination rank
- *----------------------------------------------------------------------------*/
-
-static void
-_compute_cell_dest_rank(cs_mesh_t  *mesh,
-                        int         cell_dest[])
-{
-  for (int c_id = 0; c_id < mesh->n_cells; c_id++) {
-    cell_dest[c_id] = rand() % cs_glob_n_ranks;
-  }
-}
-
-/*----------------------------------------------------------------------------
  * Redistribute fields and bc_coeffs based on the mesh distributors.
  *
  * parameters:
@@ -716,6 +699,73 @@ _distribute_data(cs_all_to_all_t *cd,
   }
 }
 
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Generate random cell destination rank (useful for stress testing).
+ *
+ * \param[in]  mesh  pointer to mesh
+ *
+ * \return  random destination rank id
+ */
+/*----------------------------------------------------------------------------*/
+
+static int *
+_random_dest_rank(const cs_mesh_t  *mesh)
+{
+  MPI_Comm comm = cs_glob_mpi_comm;
+
+  /* Generate and sync random seed. */
+
+  static bool rng_on = false;
+  if (!rng_on) {
+    rng_on = true;
+
+    unsigned int seed;
+
+    if (cs_glob_rank_id <= 0) {
+      seed = time(nullptr);
+      //seed = 123456; // Fix the seed for debugging.
+    }
+
+    MPI_Bcast(&seed, 1, MPI_UINT32_T, 0, comm);
+
+    srand(seed);
+  }
+
+  /* Allocate destination rank. */
+
+  int *dest_rank = nullptr;
+  CS_MALLOC(dest_rank, mesh->n_cells_with_ghosts, int);
+
+  // Build the cell part-to-part distributor.
+
+  const int n_max_tries = 50;
+  cs_lnum_t g_min_n_cells = 0;
+
+  for (int i = 0; i < n_max_tries && g_min_n_cells == 0; i++) {
+
+    for (cs_lnum_t c_id = 0; c_id < mesh->n_cells; c_id++) {
+      dest_rank[c_id] = rand() % cs_glob_n_ranks;
+    }
+
+    cs_all_to_all_t *cd = cs_all_to_all_create(mesh->n_cells,
+                                               0, // cd_flags,
+                                               nullptr,
+                                               dest_rank,
+                                               comm);
+    g_min_n_cells = cs_all_to_all_n_elts_dest(cd);
+    cs_all_to_all_destroy(&cd);
+
+    MPI_Allreduce(MPI_IN_PLACE, &g_min_n_cells, 1, CS_MPI_LNUM, MPI_MIN, comm);
+
+  }
+  cs_assert(g_min_n_cells > 0);
+
+  cs_halo_sync_untyped(mesh->halo, CS_HALO_STANDARD, sizeof(int), dest_rank);
+
+  return dest_rank;
+}
+
 #endif // defined(HAVE_MPI)
 
 /*! \endcond DOXYGEN_SHOULD_SKIP_THIS */
@@ -745,12 +795,31 @@ cs_redistribute(const int                cell_dest_rank[],
 {
 #if defined(HAVE_MPI)
 
+  cs_mesh_t *mesh = cs_glob_mesh;
+
   if (cs_glob_n_ranks == 1)
     return;
 
-  /* Free some elements which will need to be rebuilt when/if used. */
+  /* Generate random destination rank if none is provided. */
 
-  cs_mesh_free_rebuildable(cs_glob_mesh, true);
+  int *_dest_rank = nullptr;
+  if (cell_dest_rank == nullptr) {
+    _dest_rank = _random_dest_rank(mesh);
+    cell_dest_rank = _dest_rank;
+  }
+
+  // We will need the destination and current ranks of the halo cells.
+
+  int *cell_rank;
+  CS_MALLOC(cell_rank, mesh->n_cells_with_ghosts, int);
+  for (cs_lnum_t i = 0; i < mesh->n_cells; i++)
+    cell_rank[i] = cs_glob_rank_id;
+  cs_halo_sync_untyped(mesh->halo, CS_HALO_STANDARD, sizeof(int), cell_rank);
+
+  /* Free some elements which will need to be rebuilt when/if used.
+     Halos are needed at some interior steps so destroyed later. */
+
+  cs_mesh_free_rebuildable(cs_glob_mesh, false);
   cs_mesh_quantities_free_all(cs_glob_mesh_quantities);
 
   _free_field_gradients();
@@ -759,8 +828,6 @@ cs_redistribute(const int                cell_dest_rank[],
 
   static int iter = 0;
   iter++;
-
-  cs_mesh_t *mesh = cs_glob_mesh;
 
   MPI_Comm comm = cs_glob_mpi_comm;
 
@@ -784,86 +851,39 @@ cs_redistribute(const int                cell_dest_rank[],
   /* Cells
      ----- */
 
-  cs_all_to_all_t *cd = nullptr;
-  int cd_flags = 0;
-
   // Build the cell part-to-part distributor.
 
-  cs_lnum_t n_cells = 0;
+  cs_all_to_all_t *cd = cs_all_to_all_create(mesh->n_cells,
+                                             0, // cd_flags,
+                                             nullptr,
+                                             cell_dest_rank,
+                                             comm);
 
-  // Random cell destinations if cell_dest_rank is not prescribed.
-
-  int *_dest_rank = nullptr;
-  CS_MALLOC(_dest_rank, mesh->n_cells_with_ghosts, int);
-
-  if (cell_dest_rank == nullptr) {
-
-    // RNG
-    static bool rng_on = false;
-    if (!rng_on) {
-      rng_on = true;
-
-      unsigned int seed;
-
-      if (cs_glob_rank_id <= 0) {
-        seed = time(NULL);
-        //seed = 123456; // Fix the seed for debugging.
-      }
-
-      MPI_Bcast(&seed, 1, MPI_UINT32_T, 0, comm);
-
-      srand(seed);
-    }
-
-    cs_lnum_t g_min_n_cells;
-
-    do {
-      if (cd) cs_all_to_all_destroy(&cd);
-
-      _compute_cell_dest_rank(mesh, _dest_rank);
-
-      cd = cs_all_to_all_create(mesh->n_cells,
-                                cd_flags,
-                                nullptr,
-                                _dest_rank,
-                                comm);
-      n_cells = cs_all_to_all_n_elts_dest(cd);
-
-      MPI_Allreduce(&n_cells, &g_min_n_cells, 1, MPI_INT, MPI_MIN, comm);
-
-    } while (g_min_n_cells == 0);
-  }
-  else {
-    memcpy(_dest_rank, cell_dest_rank, mesh->n_cells*sizeof(int));
-
-    cd = cs_all_to_all_create(mesh->n_cells,
-                              cd_flags,
-                              nullptr,
-                              _dest_rank,
-                              comm);
-
-    n_cells = cs_all_to_all_n_elts_dest(cd);
-  }
+  cs_lnum_t n_cells = cs_all_to_all_n_elts_dest(cd);
 
   // Distribute global cell numbers
 
-  cs_gnum_t *cell_gnum = cs_all_to_all_copy_array(cd,
-                                                  1,
-                                                  false,
-                                                  mesh->global_cell_num);
-
-  // Sort global cell numbers in increasing order;
-  // this order will be used for the exchange of any cell-centered data.
-
-  cs_lnum_t *cell_order = cs_order_gnum(nullptr, cell_gnum, n_cells);
-
+  cs_lnum_t *cell_order = nullptr;
   cs_gnum_t *global_cell_num;
   CS_MALLOC(global_cell_num, n_cells, cs_gnum_t);
-  for (cs_lnum_t i = 0; i < n_cells; i++) {
-    global_cell_num[i] = cell_gnum[cell_order[i]];
-  }
 
-  CS_FREE(cell_gnum);
+  {
+    cs_gnum_t *cd_cell_gnum = cs_all_to_all_copy_array(cd,
+                                                       1,
+                                                       false,
+                                                       mesh->global_cell_num);
+
+    // Sort global cell numbers in increasing order;
+    // this order will be used for the exchange of any cell-centered data.
+
+    cell_order = cs_order_gnum(nullptr, cd_cell_gnum, n_cells);
+
+    for (cs_lnum_t i = 0; i < n_cells; i++) {
+      global_cell_num[i] = cd_cell_gnum[cell_order[i]];
+    }
+
+    CS_FREE(cd_cell_gnum);
+  }
 
   // Distribute cell family.
 
@@ -880,7 +900,7 @@ cs_redistribute(const int                cell_dest_rank[],
   int *b_face_dest;
   CS_MALLOC(b_face_dest, mesh->n_b_faces, int);
   for (cs_lnum_t f_id = 0; f_id < mesh->n_b_faces; f_id++)
-    b_face_dest[f_id] = _dest_rank[mesh->b_face_cells[f_id]];
+    b_face_dest[f_id] = cell_dest_rank[mesh->b_face_cells[f_id]];
 
   // Boundary face distributor.
   int bfd_flags = 0;
@@ -1022,18 +1042,6 @@ cs_redistribute(const int                cell_dest_rank[],
   /* Interior faces
      -------------- */
 
-  // We need the destination and current ranks of the halo cells.
-
-  cs_halo_sync_untyped(mesh->halo,
-                       CS_HALO_STANDARD,
-                       sizeof(int),
-                       _dest_rank);
-
-  int *cell_rank;
-  CS_MALLOC(cell_rank, mesh->n_cells_with_ghosts, int);
-  for (int i = 0; i < mesh->n_cells; i++) cell_rank[i] = cs_glob_rank_id;
-  cs_halo_sync_untyped(mesh->halo, CS_HALO_STANDARD, sizeof(int), cell_rank);
-
   // List of internal faces to distribute, and their destination.
   // one internal face can appear twice in the list.
   cs_lnum_t *i_face_lst;
@@ -1055,7 +1063,7 @@ cs_redistribute(const int                cell_dest_rank[],
     int prev_rank = -1;
 
     for (int j = 0; j < 2; j++) {
-      dest_rank = _dest_rank[mesh->i_face_cells[f_id][j]];
+      dest_rank = cell_dest_rank[mesh->i_face_cells[f_id][j]];
       if (dest_rank != prev_rank) {
         i_face_lst[n_i_faces_ini] = f_id;
         i_face_dest[n_i_faces_ini] = dest_rank;
@@ -1143,8 +1151,8 @@ cs_redistribute(const int                cell_dest_rank[],
   }
 
   // Distribute the (global) internal face-cell connectivity.
-  int blank_perio = 1;
-  cell_gnum = cs_mesh_get_cell_gnum(mesh, blank_perio);
+
+  cs_gnum_t *cell_gnum = cs_mesh_get_cell_gnum(mesh, 1);
 
   cs_gnum_t *g_i_face_cells_s;
   CS_MALLOC(g_i_face_cells_s, 2*n_i_faces_ini, cs_gnum_t);
@@ -1394,6 +1402,7 @@ cs_redistribute(const int                cell_dest_rank[],
 
   cs_mesh_builder_destroy(&builder);
 
+  cs_mesh_free_rebuildable(mesh, true);
   mesh->n_cells_with_ghosts = mesh->n_cells;
 
   cs_mesh_init_halo(mesh, nullptr, mesh->halo_type, -1, true);