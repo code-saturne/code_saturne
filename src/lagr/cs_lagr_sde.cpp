code-saturne/code_saturne

commit c7f19a17c3bbc535aa7b2b925eeee128e222e106

[LAGR] add GLM model.
This is done using Euler quaternions.

WIP with C. Henry and G. Peyrot.

src/lagr/cs_lagr_sde.cpp
@@ -451,47 +451,9 @@ _sde_vels_pos_1_st_order_time_integ_mp(cs_lagr_particle_set_t         &p_set,
 
     case CS_LAGR_SHAPE_SPHERE_MODEL:
       {
-        /* Compute the main direction in the global reference
-         * frame */
-        cs_real_t dir[3];
-
-        /* Obtain the mean particle velocity for each cell */
-        const cs_real_t *mean_part_vel_p = stat_vel->val + (cell_id * 3);
-        const cs_real_t *mean_part_vel_s = stat_vel_s->val + (cell_id * 3);
-
-        /* Get Relative mean velocity */
-        if (cs_glob_lagr_model->cs_used == 1) {
-          /* Relative mean velocity
-           * <u_p> - <u_s> = <u_r>
-           * See Minier et al 2024.
-           * */
-          for (cs_lnum_t i = 0; i < 3; i++)
-            dir[i] = mean_part_vel_p[i] - mean_part_vel_s[i];
-        }
-        else { /* neptune is used */
-          /* Change the direction since mean part vel contains
-           * mean velocity fluctuation */
-          cs_real_t fluid_vel_mel[3] = {0., 0., 0.};
-          for (int phase_id = 0; phase_id < n_phases; phase_id ++) {
-            const cs_real_t *fluid_vel = cvar_vel[phase_id][cell_id];
-            for (cs_lnum_t i = 0; i < 3; i++) {
-              fluid_vel_mel[i] += lambda[phase_id] * fluid_vel[i];
-            }
-          }
-          for (cs_lnum_t i = 0; i < 3; i++)
-            dir[i] = mean_part_vel_p[i] - fluid_vel_mel[i];
-        }
-
-        cs_math_3_normalize(dir, dir);
-
-        /* the direction in the local reference frame "_r" is (1, 0, 0)
-         * by convention */
-        const cs_real_t dir_r[3] = {1.0, 0.0, 0.0};
-
-        /* Compute Euler quaternions */
-        cs_real_t euler[4];
-        cs_math_quaternions_from_2_vectors(dir, dir_r, euler);
-
+        /* Use Euler angles for spheres
+           /  Note that they are computed in aux_mean_fluid_quantities.cpp */
+        const cs_real_t *euler = p_set.attr_real_ptr(p_id, CS_LAGR_EULER);
         // Compute the matrix to change the Frame of Reference
         // using the function to transform the 4 euler parameters into a rotation
         cs_math_quaternions_into_33_rotation(euler, trans_m);

src/lagr/cs_lagr.cpp
@@ -191,6 +191,7 @@ static cs_lagr_model_t  _lagr_model
      .fragmentation = 0,
      .n_stat_classes = 0,
      .n_user_variables = 0,
+     .transport_GLM_rotated = false,
      .viscous_terms = false,
      .verbosity = 0};
 
@@ -401,6 +402,7 @@ static void _lagr_map_field_initialize(cs_lnum_t n_phases)
     _lagr_extra_module[phase_id].anisotropic_lagr_time = nullptr;
     _lagr_extra_module[phase_id].anisotropic_bx = nullptr;
     _lagr_extra_module[phase_id].grad_lagr_time_r_et = nullptr;
+    _lagr_extra_module[phase_id].anisotropic_euler = nullptr;
     for (int i = 0; i < 9; i++)
       _lagr_extra_module[phase_id].grad_cov_skp[i] = nullptr;
     for (int i = 0; i < 6; i++)
@@ -1055,6 +1057,7 @@ cs_lagr_finalize(void)
     CS_FREE(extra[phase_id].grad_lagr_time_r_et);
     CS_FREE(extra[phase_id].anisotropic_lagr_time);
     CS_FREE(extra[phase_id].anisotropic_bx);
+    CS_FREE(extra[phase_id].anisotropic_euler);
   }
   CS_FREE(cs_glob_lagr_extra_module);
 }
@@ -2160,6 +2163,9 @@ cs_lagr_solve_time_step(const int         itypfb[],
           CS_MALLOC_HD(extra_i[phase_id].anisotropic_lagr_time, ncelet, cs_real_3_t, cs_alloc_mode);
           CS_MALLOC_HD(extra_i[phase_id].anisotropic_bx, ncelet, cs_real_3_t, cs_alloc_mode);
         }
+        if (cs_glob_lagr_model->transport_GLM_rotated ) {
+          CS_MALLOC_HD(extra_i[phase_id].anisotropic_euler, ncelet, cs_real_4_t, cs_alloc_mode);
+        }
         cs_lagr_aux_mean_fluid_quantities(0,
                                           phase_id,
                                           extra_i[phase_id].lagr_time,
@@ -2169,7 +2175,8 @@ cs_lagr_solve_time_step(const int         itypfb[],
                                           extra_i[phase_id].anisotropic_lagr_time,
                                           extra_i[phase_id].anisotropic_bx,
                                           extra_i[phase_id].grad_lagr_time_r_et,
-                                          extra_i[phase_id].grad_lagr_time);
+                                          extra_i[phase_id].grad_lagr_time,
+                                          extra_i[phase_id].anisotropic_euler);
       }
       else if (   (   cs_glob_lagr_time_scheme->iilagr
                    != CS_LAGR_FROZEN_CONTINUOUS_PHASE)
@@ -2192,6 +2199,9 @@ cs_lagr_solve_time_step(const int         itypfb[],
                        cs_real_3_t);
           if (extra_i[phase_id].grad_lagr_time != nullptr)
             CS_REALLOC(extra_i[phase_id].grad_lagr_time, n_cells_ext, cs_real_3_t);
+          if (extra_i[phase_id].anisotropic_euler != nullptr)
+            CS_REALLOC(extra_i[phase_id].anisotropic_euler, n_cells_ext, cs_real_4_t);
+
         }
 
         /* TODO compute carrier field at current and previous time step
@@ -2205,7 +2215,8 @@ cs_lagr_solve_time_step(const int         itypfb[],
                                           extra_i[phase_id].anisotropic_lagr_time,
                                           extra_i[phase_id].anisotropic_bx,
                                           extra_i[phase_id].grad_lagr_time_r_et,
-                                          extra_i[phase_id].grad_lagr_time);
+                                          extra_i[phase_id].grad_lagr_time,
+                                          extra_i[phase_id].anisotropic_euler);
       }
       else {
         if (   cs_glob_lagr_model->modcpl > 0
@@ -2216,7 +2227,8 @@ cs_lagr_solve_time_step(const int         itypfb[],
                                  extra_i[phase_id].anisotropic_lagr_time,
                                  extra_i[phase_id].anisotropic_bx,
                                  extra_i[phase_id].grad_lagr_time_r_et,
-                                 extra_i[phase_id].grad_lagr_time);
+                                 extra_i[phase_id].grad_lagr_time,
+                                 extra_i[phase_id].anisotropic_euler);
 
         if (cs_glob_lagr_model->cs_used == 0)
           compute_particle_covariance_gradient(phase_id,

src/lagr/cs_lagr.h
@@ -355,6 +355,9 @@ typedef struct {
 
   int  n_user_variables;
 
+  /* Handle transport with a generic GLM */
+  bool transport_GLM_rotated;
+
   /*! Take viscous terms into account */
   bool viscous_terms;
 
@@ -1067,6 +1070,10 @@ typedef struct {
   /* (if extended_t_scheme && modcpl) gradient of anistropic Lagrangien time
    * int the relative basis used to compute beta */
   cs_real_3_t *grad_lagr_time_r_et;
+
+  /* (if transport_GLM_generic) save the Euler parameters associated to the
+   * eigenvectors for the rotation matrix (where G is diagonal) */
+  cs_real_4_t *anisotropic_euler;
 
 } cs_lagr_extra_module_t;
 

src/lagr/cs_lagr_aux_mean_fluid_quantities.cpp
@@ -66,6 +66,8 @@
 #include "pprt/cs_physical_model.h"
 #include "turb/cs_turbulence_model.h"
 #include "base/cs_velocity_pressure.h"
+#include "turb/cs_turbulence_model.h"
+#include "turb/cs_les_balance.h"
 
 /*----------------------------------------------------------------------------
  *  Header for the current file
@@ -250,6 +252,8 @@ compute_particle_covariance_gradient(int          phase_id,
  * \param[out]  grad_lagr_time_r_et    anisotropic Lagrangian time gradient in
  *                                     relative basis
  * \param[in]   grad_lagr_time         Lagrangian time gradient
+ * \param[in]   anisotropic_euler     Eigenvector to rotate the frame of reference
+ *                                      in the local one (where TL is diagonal)
  *
  */
 /*----------------------------------------------------------------------------*/
@@ -260,7 +264,8 @@ compute_anisotropic_prop(int            iprev,
                          cs_real_3_t   *anisotropic_lagr_time,
                          cs_real_3_t   *anisotropic_bx,
                          cs_real_3_t   *grad_lagr_time_r_et,
-                         cs_real_3_t   *grad_lagr_time)
+                         cs_real_3_t   *grad_lagr_time,
+                         cs_real_4_t   *anisotropic_euler )
 
 {
   int cell_wise_integ = cs_glob_lagr_time_scheme->cell_wise_integ;
@@ -272,6 +277,21 @@ compute_anisotropic_prop(int            iprev,
 
   cs_real_t *energi = extra_i[phase_id].cvar_k->val;
   cs_real_t *dissip = extra_i[phase_id].cvar_ep->val;
+
+  // Add cases where the turbulence model is based on Rij or K-Omega
+  if (extra_i[phase_id].itytur == 3) {
+    for (cs_lnum_t cell_id = 0; cell_id < cs_glob_mesh->n_cells; cell_id++)
+      energi[cell_id] = 0.5 * (  extra_i[phase_id].cvar_rij->val[6*cell_id]
+          + extra_i[phase_id].cvar_rij->val[6*cell_id+1]
+          + extra_i[phase_id].cvar_rij->val[6*cell_id+2]);
+
+  }
+  else if (extra_i[phase_id].iturb == CS_TURB_K_OMEGA) {
+
+    for (cs_lnum_t cell_id = 0; cell_id < cs_glob_mesh->n_cells; cell_id++)
+      dissip[cell_id] = extra_i[phase_id].cmu * energi[cell_id]
+        * extra_i[phase_id].cvar_omg->val[cell_id];
+  }
 
   /* Compute anisotropic value of the lagr_time and diffusion matrix */
   /* complete model */
@@ -302,6 +322,7 @@ compute_anisotropic_prop(int            iprev,
       if (stat_w->vals[cell_wise_integ][cell_id] >
             cs_glob_lagr_stat_options->threshold) {
         /* compute mean relative velocity <Up> - <Us>*/
+        // TODO: add case for neptune_cfd
         for (int i = 0; i < 3; i++)
           dir[i] = stat_vel->vals[cell_wise_integ][cell_id * 3 + i]
                  - stat_vel_s->vals[cell_wise_integ][cell_id * 3 + i];
@@ -335,6 +356,13 @@ compute_anisotropic_prop(int            iprev,
          * frame */
         cs_math_3_normalize(dir, dir);
 
+        /* Compute the 4 Euler parameters using the projection of
+         * two vectors (note that the direction in the local reference
+         * frame "_r" is (1, 0, 0) by convention */
+        const cs_real_t dir_r[3] = {1.0, 0.0, 0.0};
+        cs_math_quaternions_from_2_vectors(dir, dir_r,
+                                           anisotropic_euler[cell_id]);
+
         /* Compute k_tilde in each cell */
         cs_real_t ktil = 0;
         if (extra_i[phase_id].itytur == 3) {
@@ -346,12 +374,66 @@ compute_anisotropic_prop(int            iprev,
           ktil = 3.0 * (rnn * bbi[0] + (tr_r -rnn) * bbi[1])
                / (2.0 * (bbi[0] + bbi[1] + bbi[2]));
           /* bbi[1] == bbi[2] is used */
-        }
-        else if (   extra_i[phase_id].itytur == 2
-                 || extra_i[phase_id].itytur == 4
-                 || extra_i[phase_id].itytur == 5
-                 || extra_i[phase_id].iturb == CS_TURB_K_OMEGA) {
-          ktil  = energi[cell_id];
+
+          /* Specific treatment for a generic GLM */
+          if (cs_glob_lagr_model->transport_GLM_rotated) {
+            /* Compute new matrix A such that AR = - nu_f * Lapl R
+             * Note that -nu_f Lapl R is directly given by difflamij
+             * which is computed in turb/cs_les_balance.cpp */
+            cs_real_6_t A;
+            cs_real_6_t rij_inv;
+            cs_math_sym_33_inv_cramer(rij,rij_inv); // Inverse matrix Rij
+            cs_les_balance_t *les_balance = cs_get_glob_les_balance();
+            cs_real_6_t *difflamij = les_balance->brij->difflamij;
+            cs_math_sym_33_product(difflamij[cell_id], rij_inv, A);
+
+            /* Now we rotate the matrix A in the frame of reference
+             * where the matrix G is diagonal = (1/2 + 3/4C0) eps/k delta_ij */
+            cs_real_33_t trans_m;
+            /* Rotate the frame of reference with respect to the
+             * mean relative velocity direction by calling the
+             * function to transform the 4 euler parameters into a rotation */
+            cs_math_quaternions_into_33_rotation(anisotropic_euler[cell_id],
+                trans_m);
+
+            /* Now transform A in this local frame of reference */
+            cs_real_6_t a_rotated;
+            cs_math_sym_33_transform_a_to_r(A, trans_m, a_rotated);
+            /* Add the components of G on the diagonal of the rotated A*/
+            for (int i = 0; i < 3; i++)
+              a_rotated[i] -= 1./cl * dissip[cell_id] / energi[cell_id] ;
+
+            /* Compute the eigenvalues and eigenvectors of a_rotated */
+            cs_real_t eig_val[3];
+            cs_real_t eig_vec[3][3];
+            cs_real_t tol_err = 1.0e-12;
+            // Define a_rot = equivalent [3][3] matrix
+            cs_real_33_t a_rot;
+            a_rot[0][0] = a_rotated[0];
+            a_rot[0][1] = a_rotated[3];
+            a_rot[0][2] = a_rotated[5];
+            a_rot[1][0] = a_rotated[3];
+            a_rot[1][1] = a_rotated[1];
+            a_rot[1][2] = a_rotated[4];
+            a_rot[2][0] = a_rotated[5];
+            a_rot[2][1] = a_rotated[4];
+            a_rot[2][2] = a_rotated[2];
+            // Call specific function for eigevalues & eigenvectors
+            cs_math_33_eig_val_vec(a_rot, tol_err, eig_val, eig_vec);
+
+            /* The eigenvalues are the new Lagrangian time */
+            for (int id = 0; id < 3; id++)
+              anisotropic_lagr_time[cell_id][id] = eig_val[id];
+            /* The eigenvectors are the corresponding rotations
+             * needed to reach this frame of reference */
+            for (int id = 0; id < 4; id++) {
+              /* Call function to transform the rotation matrix
+               * (which is based on the three eigenvectors)
+               * into a quaternion (i.e. 4 Euler parameters for the rotation) */
+              cs_math_33_rotation_into_quaternions(eig_vec,
+                  anisotropic_euler[cell_id]);
+            }
+          }
         }
 
         for (int i = 0; i <3; i++) {
@@ -366,17 +448,10 @@ compute_anisotropic_prop(int            iprev,
           // FIXME: check if 2nd order scheme still works
           // especially if reading the anisotropic_euler makes sense
           // (provided it has been computed once before)
-          /* the direction in the local reference frame "_r" is (1, 0, 0)
-           * by convention */
-          const cs_real_t dir_r[3] = {1.0, 0.0, 0.0};
-
-          /* Compute Euler quaternions */
-          cs_real_t euler[4];
-          cs_math_quaternions_from_2_vectors(dir, dir_r, euler);
-
           // Compute the matrix to change the Frame of Reference
           // using the function to transform the 4 euler parameters into a rotation
-          cs_math_quaternions_into_33_rotation(euler, trans_m);
+          cs_math_quaternions_into_33_rotation(anisotropic_euler[cell_id],
+                                               trans_m);
 
           /* transform grad(Tl) in the local
            * reference frame */
@@ -433,6 +508,8 @@ compute_anisotropic_prop(int            iprev,
  * \param[out]  grad_lagr_time_r_et    anisotropic Lagrangian time gradient in
  *                                     relative basis
  * \param[out]  grad_lagr_time         Lagrangian time gradient
+ * \param[in]   anisotropic_euler      Eigenvector to rotate the frame of reference
+ *                                      in the local one (where TL is diagonal)
  *
  */
 /*----------------------------------------------------------------------------*/
@@ -447,7 +524,8 @@ cs_lagr_aux_mean_fluid_quantities(int            iprev, // FIXME compute at curr
                                   cs_real_3_t   *anisotropic_lagr_time,
                                   cs_real_3_t   *anisotropic_bx,
                                   cs_real_3_t   *grad_lagr_time_r_et,
-                                  cs_real_3_t   *grad_lagr_time)
+                                  cs_real_3_t   *grad_lagr_time,
+                                  cs_real_4_t   *anisotropic_euler)
 {
   /* TODO compute cell properties in coherence with iprev */
   cs_lnum_t n_cells_with_ghosts = cs_glob_mesh->n_cells_with_ghosts;
@@ -828,7 +906,8 @@ cs_lagr_aux_mean_fluid_quantities(int            iprev, // FIXME compute at curr
                                anisotropic_lagr_time,
                                anisotropic_bx,
                                grad_lagr_time_r_et,
-                               grad_lagr_time);
+                               grad_lagr_time,
+                               anisotropic_euler);
 
 
     else if (grad_lagr_time_r_et != nullptr) {

src/lagr/cs_lagr_aux_mean_fluid_quantities.h
@@ -76,7 +76,8 @@ compute_anisotropic_prop(int            iprev,
                          cs_real_3_t   *anisotropic_lagr_time,
                          cs_real_3_t   *anisotropic_bx,
                          cs_real_3_t   *grad_lagr_time_r_et,
-                         cs_real_3_t   *grad_lagr_time);
+                         cs_real_3_t   *grad_lagr_time,
+                         cs_real_4_t   *anisotropic_euler);
 
 /*----------------------------------------------------------------------------*/
 /*!
@@ -137,7 +138,8 @@ cs_lagr_aux_mean_fluid_quantities(int            iprev,
                                   cs_real_3_t   *anisotropic_lagr_time,
                                   cs_real_3_t   *anisotropic_bx,
                                   cs_real_3_t   *grad_lagr_time_r_et,
-                                  cs_real_3_t   *grad_lagr_time);
+                                  cs_real_3_t   *grad_lagr_time,
+                                  cs_real_4_t   *anisotropic_euler);
 
 /*----------------------------------------------------------------------------*/
 

src/lagr/cs_lagr_particle.cpp
@@ -435,6 +435,15 @@ cs_lagr_particle_attr_initialize(void)
   attr_keys[CS_LAGR_DIAMETER][0] = CS_LAGR_P_RVAR_TS;
   attr_keys[CS_LAGR_DIAMETER][1] = ++loc_count;
 
+  /* Activate Euler angles for spheroids / generic GLM
+   * in order to compute a local frame of reference */
+  // FIXME: change to activate all the time ?
+  if (lagr_model->shape != CS_LAGR_SHAPE_SPHERE_MODEL
+      || lagr_model->transport_GLM_rotated ) {
+    attr_keys[CS_LAGR_EULER][1] = ++loc_count;
+    attr_keys[CS_LAGR_EULER][2] = 4;
+  }
+
   /* Non-sphere model
    * TODO activate only required arrays */
   if (lagr_model->shape != CS_LAGR_SHAPE_SPHERE_MODEL) {
@@ -453,9 +462,6 @@ cs_lagr_particle_attr_initialize(void)
 
     attr_keys[CS_LAGR_ANGULAR_VEL][1] = ++loc_count;
     attr_keys[CS_LAGR_ANGULAR_VEL][2] = 3;
-
-    attr_keys[CS_LAGR_EULER][1] = ++loc_count;
-    attr_keys[CS_LAGR_EULER][2] = 4;
 
     attr_keys[CS_LAGR_SHAPE_PARAM][0] = CS_LAGR_P_RPRP;
     attr_keys[CS_LAGR_SHAPE_PARAM][1] = ++loc_count;

commit de7a4ab3fbf72616c4f95ccf78b47cc9cb1ae650

Fix problem for the Lagrangian module used in neptune_cfd.
The CS_LAGR_EULER attribute was not accessible through neptune_cfd when using the sphere model.
We use the original formulation that is still used when the module is accessed through
code_saturne. The problem was introduced in commit c7f19a17c (2026-01-12).

src/lagr/cs_lagr_sde.cpp
@@ -451,9 +451,60 @@ _sde_vels_pos_1_st_order_time_integ_mp(cs_lagr_particle_set_t         &p_set,
 
     case CS_LAGR_SHAPE_SPHERE_MODEL:
       {
-        /* Use Euler angles for spheres
-           /  Note that they are computed in aux_mean_fluid_quantities.cpp */
-        const cs_real_t *euler = p_set.attr_real_ptr(p_id, CS_LAGR_EULER);
+        /* Compute the main direction in the global reference
+         * frame */
+        cs_real_t dir[3];
+
+        /* Obtain the mean particle velocity for each cell */
+        const cs_real_t *mean_part_vel_p = stat_vel->val + (cell_id * 3);
+        const cs_real_t *mean_part_vel_s = stat_vel_s->val + (cell_id * 3);
+
+        /* Get Relative mean velocity */
+        /* Relative mean velocity
+         * <u_p> - <u_s> = <u_r>
+         * See Minier et al 2024.
+         * */
+        for (cs_lnum_t i = 0; i < 3; i++)
+          dir[i] = mean_part_vel_p[i] - mean_part_vel_s[i];
+
+        cs_math_3_normalize(dir, dir);
+
+        // Rotate the frame of reference with respect to the
+        // mean relative velocity direction.
+
+        // The rotation axis is the result of the cross product between
+        // the new direction vector and the main axis.
+        cs_real_t n_rot[3];
+        /* the direction in the local reference frame "_r" is (1, 0, 0)
+         * by convention */
+        const cs_real_t dir_r[3] = {1.0, 0.0, 0.0};
+
+        // Use quaternion (cos(theta/2), sin(theta/2) n_rot)
+        // where n_rot = dir ^ dir_r normalised
+        // so also       dir ^ (dir + dir_r)
+        //
+        // cos(theta/2) = || dir + dir_r|| / 2
+        cs_real_t dir_p_dir_r[3] = {dir[0] + dir_r[0],
+                                    dir[1] + dir_r[1],
+                                    dir[2] + dir_r[2]};
+        cs_real_t dir_p_dir_r_normed[3];
+        cs_math_3_normalize(dir_p_dir_r, dir_p_dir_r_normed);
+
+        /* dir ^(dir + dir_r) / || dir + dir_r|| = sin(theta/2) n_rot
+         * for the quaternion */
+        cs_math_3_cross_product(dir, dir_p_dir_r_normed, n_rot);
+
+        /* quaternion, could be normalized afterwards
+         *
+         * Note that the division seems stupid but is not
+         * in case of degenerated case where dir is null
+         * */
+        const cs_real_t euler[4]
+          = {cs_math_3_norm(dir_p_dir_r) / (cs_math_3_norm(dir)
+              + cs_math_3_norm(dir_r)),
+            n_rot[0],
+            n_rot[1],
+            n_rot[2]};
         // Compute the matrix to change the Frame of Reference
         // using the function to transform the 4 euler parameters into a rotation
         cs_math_quaternions_into_33_rotation(euler, trans_m);