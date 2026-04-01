code-saturne/code_saturne

commit aa3d64c2894d5954eb77a216b3fe04c7bb959aa7

[GUI] Update Paticle-laden flows parameters for NCFD

python/code_saturne/gui/case/SolidView.py
@@ -125,6 +125,10 @@ class SolidView(QWidget, Ui_Solid):
         validatorComp.setExclusiveMin(True)
         self.lineEditCompaction.setValidator(validatorComp)
 
+        validatorFricThres = DoubleValidator(self.lineEditFrictonalThres, min = 0.0)
+        validatorFricThres.setExclusiveMin(True)
+        self.lineEditFrictonalThres.setValidator(validatorFricThres)
+
         validatorElast = DoubleValidator(self.lineEditElastCoef, min = 0.0)
         validatorElast.setExclusiveMin(False)
         self.lineEditElastCoef.setValidator(validatorElast)
@@ -135,8 +139,22 @@ class SolidView(QWidget, Ui_Solid):
         self.comboBoxGranular.activated[str].connect(self.slotGranular)
         self.comboBoxKinetic.activated[str].connect(self.slotKinetic)
         self.lineEditCompaction.textChanged[str].connect(self.slotCompaction)
+        self.lineEditFrictonalThres.textChanged[str].connect(self.slotFrictionalThreshold)
         self.lineEditElastCoef.textChanged[str].connect(self.slotSetElasticity)
         self.checkBoxCoupling.clicked[bool].connect(self.slotCoupling)
+
+        # Show / hide polydispersed parameter
+        has_qp_qfp = False
+        for field in self.mdl.mainFieldsModel.getSolidPhaseList():
+            if self.mdl.getTurbulenceModel(field.f_id) == "q2-q12":
+                has_qp_qfp = True
+
+        self.labelCoupling.setVisible(has_qp_qfp)
+        self.checkBoxCoupling.setVisible(has_qp_qfp)
+        if has_qp_qfp:
+            isCoupling = self.mdl.getCouplingStatus() == "on"
+            self.checkBoxCoupling.setChecked(isCoupling)
+
 
         # Initialize widget
         self.initializeVariables(self.currentid)
@@ -190,13 +208,23 @@ class SolidView(QWidget, Ui_Solid):
 
 
     @pyqtSlot(str)
+    def slotFrictionalThreshold(self, var):
+        """
+        Setter slot.
+        """
+        if self.lineEditFrictonalThres.validator().state == QValidator.Acceptable:
+            value = from_qvariant(var, float)
+            self.mdl.setMinFrictionalThreshold(value)
+
+
+    @pyqtSlot(str)
     def slotSetElasticity(self, var):
         """
         Set elasiticity coefficient
         """
         if self.lineEditElastCoef.validator().state == QValidator.Acceptable:
             value = from_qvariant(var, float)
-            self.mdl.setElastCoeff(value)
+            self.mdl.setElastCoeff(value, self.currentid)
 
 
     def initializeVariables(self, fieldId):
@@ -208,7 +236,10 @@ class SolidView(QWidget, Ui_Solid):
         value = self.mdl.getCompaction()
         self.lineEditCompaction.setText(str(value))
 
-        self.lineEditElastCoef.setText(str(self.mdl.getElastCoeff()))
+        value_min_fric = self.mdl.getMinFrictionalThreshold()
+        self.lineEditFrictonalThres.setText(str(value_min_fric))
+
+        self.lineEditElastCoef.setText(str(self.mdl.getElastCoeff(fieldId)))
 
         model = self.mdl.getFrictionModel(fieldId)
         self.modelFriction.setItem(str_model=model)
@@ -219,17 +250,6 @@ class SolidView(QWidget, Ui_Solid):
         model = self.mdl.getKineticModel(fieldId)
         self.modelKinetic.setItem(str_model = model)
 
-        if self.mdl.getTurbulenceModel(fieldId) == "q2-q12" :
-            self.labelCoupling.show()
-            self.checkBoxCoupling.show()
-
-            isCoupling = self.mdl.getCouplingStatus(fieldId) == "on"
-            self.checkBoxCoupling.setChecked(isCoupling)
-
-        else :
-            self.labelCoupling.hide()
-            self.checkBoxCoupling.hide()
-
 
     @pyqtSlot(bool)
     def slotCoupling(self, checked):
@@ -239,5 +259,5 @@ class SolidView(QWidget, Ui_Solid):
         status = 'off'
         if checked:
             status = 'on'
-        self.mdl.setCouplingStatus(self.currentid, status)
+        self.mdl.setCouplingStatus(status)
 

python/code_saturne/gui/case/MainFieldsView.py
@@ -333,7 +333,7 @@ class StandardItemModelMainFields(QStandardItemModel):
                          self.tr("Phase of\nfield"),
                          self.tr("Interfacial criterion"),
                          self.tr("Carrier phase"),
-                         self.tr("Compressible"),
+                         self.tr("Variable density"),
                          self.tr("Energy\nresolution")]
 
         self.setColumnCount(len(self.headers))

python/code_saturne/gui/case/Solid.ui
@@ -45,22 +45,36 @@
         <item row="1" column="0">
          <widget class="QLabel" name="label_4">
           <property name="text">
-           <string>maximum particle compaction</string>
+           <string>Maximum particle compaction</string>
+          </property>
+         </widget>
+        </item>
+        <item row="3" column="0">
+         <widget class="QLabel" name="labelCoupling">
+          <property name="text">
+           <string>Polydispersed : coupling between dispersed phases</string>
           </property>
          </widget>
         </item>
         <item row="1" column="1">
          <widget class="QLineEdit" name="lineEditCompaction"/>
         </item>
-        <item row="0" column="0">
-         <widget class="QLabel" name="label_5">
+        <item row="3" column="1">
+         <widget class="QCheckBox" name="checkBoxCoupling">
           <property name="text">
-           <string>Elastic coefficient</string>
+           <string/>
           </property>
          </widget>
         </item>
-        <item row="0" column="1">
-         <widget class="QLineEdit" name="lineEditElastCoef"/>
+        <item row="2" column="0">
+         <widget class="QLabel" name="label_7">
+          <property name="text">
+           <string>Minimum frictional threshold</string>
+          </property>
+         </widget>
+        </item>
+        <item row="2" column="1">
+         <widget class="QLineEdit" name="lineEditFrictonalThres"/>
         </item>
        </layout>
       </item>
@@ -72,51 +86,6 @@
         <property name="sizeHint" stdset="0">
          <size>
           <width>137</width>
-          <height>20</height>
-         </size>
-        </property>
-       </spacer>
-      </item>
-     </layout>
-    </widget>
-   </item>
-   <item>
-    <widget class="QGroupBox" name="groupBoxField">
-     <property name="title">
-      <string>Field label</string>
-     </property>
-     <layout class="QGridLayout" name="gridLayout_2">
-      <item row="0" column="1">
-       <widget class="QComboBox" name="comboBoxField">
-        <property name="minimumSize">
-         <size>
-          <width>120</width>
-          <height>0</height>
-         </size>
-        </property>
-       </widget>
-      </item>
-      <item row="0" column="2">
-       <spacer name="horizontalSpacer_8">
-        <property name="orientation">
-         <enum>Qt::Horizontal</enum>
-        </property>
-        <property name="sizeHint" stdset="0">
-         <size>
-          <width>263</width>
-          <height>20</height>
-         </size>
-        </property>
-       </spacer>
-      </item>
-      <item row="0" column="0">
-       <spacer name="horizontalSpacer_7">
-        <property name="orientation">
-         <enum>Qt::Horizontal</enum>
-        </property>
-        <property name="sizeHint" stdset="0">
-         <size>
-          <width>264</width>
           <height>20</height>
          </size>
         </property>
@@ -146,6 +115,41 @@
       </item>
       <item>
        <layout class="QVBoxLayout" name="verticalLayout">
+        <item>
+         <layout class="QHBoxLayout" name="horizontalLayout_9">
+          <item>
+           <widget class="QLabel" name="label_6">
+            <property name="text">
+             <string>Field label</string>
+            </property>
+           </widget>
+          </item>
+          <item>
+           <widget class="QComboBox" name="comboBoxField">
+            <property name="minimumSize">
+             <size>
+              <width>120</width>
+              <height>0</height>
+             </size>
+            </property>
+           </widget>
+          </item>
+         </layout>
+        </item>
+        <item>
+         <layout class="QHBoxLayout" name="horizontalLayout_8">
+          <item>
+           <widget class="QLabel" name="label_5">
+            <property name="text">
+             <string>Elastic coefficient                     </string>
+            </property>
+           </widget>
+          </item>
+          <item>
+           <widget class="QLineEdit" name="lineEditElastCoef"/>
+          </item>
+         </layout>
+        </item>
         <item>
          <layout class="QHBoxLayout" name="horizontalLayout">
           <item>
@@ -189,22 +193,7 @@
          </layout>
         </item>
         <item>
-         <layout class="QHBoxLayout" name="horizontalLayout_5">
-          <item>
-           <widget class="QLabel" name="labelCoupling">
-            <property name="text">
-             <string>polydispersed : coupling between dispersed phases</string>
-            </property>
-           </widget>
-          </item>
-          <item>
-           <widget class="QCheckBox" name="checkBoxCoupling">
-            <property name="text">
-             <string/>
-            </property>
-           </widget>
-          </item>
-         </layout>
+         <layout class="QHBoxLayout" name="horizontalLayout_5"/>
         </item>
        </layout>
       </item>

python/code_saturne/gui/case/ThermalForm.ui
@@ -7,7 +7,7 @@
     <x>0</x>
     <y>0</y>
     <width>889</width>
-    <height>694</height>
+    <height>790</height>
    </rect>
   </property>
   <property name="windowTitle">
@@ -555,17 +555,7 @@
       <bool>false</bool>
      </property>
      <layout class="QGridLayout" name="gridLayout">
-      <item row="0" column="0">
-       <widget class="QLabel" name="labelEmissivity">
-        <property name="text">
-         <string>Emission coefficient</string>
-        </property>
-       </widget>
-      </item>
-      <item row="0" column="1">
-       <widget class="QLineEdit" name="lineEditEmissivity"/>
-      </item>
-      <item row="0" column="2">
+      <item row="0" column="3">
        <spacer name="horizontalSpacer_5">
         <property name="orientation">
          <enum>Qt::Horizontal</enum>
@@ -577,6 +567,19 @@
          </size>
         </property>
        </spacer>
+      </item>
+      <item row="0" column="2">
+       <widget class="QLineEdit" name="lineEditEmissivity"/>
+      </item>
+      <item row="0" column="0">
+       <widget class="QLabel" name="labelEmissivity">
+        <property name="text">
+         <string>Emission coefficient</string>
+        </property>
+       </widget>
+      </item>
+      <item row="0" column="1">
+       <widget class="QComboBox" name="comboBoxEmissivity"/>
       </item>
      </layout>
     </widget>

python/code_saturne/gui/case/ThermalView.py
@@ -265,6 +265,17 @@ class ThermalView(QWidget, Ui_ThermalForm):
         self.__setFluidRadiation__(model)
 
         # inter-particles radiation model
+        self._current_particle_f_id = None
+        self.modelParticleFields = ComboModel(self.comboBoxEmissivity, 1, 1)
+        for field in MainFieldsModel(self.case).getSolidPhaseList():
+            label = field.label
+            name = field.f_id
+            self.modelParticleFields.addItem(self.tr(label), name)
+            if self._current_particle_f_id == None:
+                self._current_particle_f_id = name
+
+        self.comboBoxEmissivity.activated[str].connect(self.slotEmissivityField)
+
         self.partRadiationModel = ThermalParticlesRadiationModel(self.case)
         self.__setParticlesRadiation__()
 
@@ -280,6 +291,13 @@ class ThermalView(QWidget, Ui_ThermalForm):
 
         self.case.undoStartGlobal()
 
+    def _update_emissivity_line(self):
+        """
+        """
+        val = self.partRadiationModel.getEmissivity(self._current_particle_f_id)
+        if val == None:
+            val = 1.0 # default
+        self.lineEditEmissivity.setText(val)
 
     def __setParticlesRadiation__(self):
         """
@@ -300,7 +318,8 @@ class ThermalView(QWidget, Ui_ThermalForm):
         else:
             self.groupBoxParticlesRadiation.setChecked(False)
             self.slotActivateParticlesRadiation(False)
-        self.lineEditEmissivity.setText(self.partRadiationModel.emissivity)
+
+        self._update_emissivity_line()
 
 
     def slotActivateParticlesRadiation(self, status):
@@ -316,7 +335,8 @@ class ThermalView(QWidget, Ui_ThermalForm):
 
     def slotSetEmissivity(self, value):
         if self.lineEditEmissivity.validator().state == QValidator.Acceptable:
-            self.partRadiationModel.emissivity = value
+            field_id = self._current_particle_f_id
+            self.partRadiationModel.setEmissivity(field_id, value)
 
     def __setFluidRadiation__(self, model):
         """
@@ -475,6 +495,16 @@ class ThermalView(QWidget, Ui_ThermalForm):
         self.__setFluidRadiation__(th)
         self.browser.configureTree(self.case)
 
+
+    @pyqtSlot(str)
+    def slotEmissivityField(self, text):
+        """
+        Update current field id for particles radiaitive model.
+        """
+        f_id = self.modelParticleFields.dicoV2M[str(text)]
+        self._current_particle_f_id = f_id
+
+        self._update_emissivity_line()
 
     @pyqtSlot(str)
     def slotFluidRadiativeTransfer(self):

python/code_saturne/model/SolidModel.py
@@ -45,13 +45,23 @@ class SolidModel(TurbulenceModel):  # TODO : should SolidModel inherit from Turb
         TurbulenceModel.__init__(self, case)
         self.case = case
         self.XMLThermo = self.case.xmlGetNode('thermophysical_models')
+        self.ipp_node = self.XMLThermo.xmlGetNode('solid_particles_properties')
         self.__XMLNodefields = self.XMLThermo.xmlInitNode('fields')
+
+    def __init_node(self):
+        """
+        Private method to initialize if needed the needed node.
+        """
+
+        if self.ipp_node == None:
+            self.ipp_node = self.XMLThermo.xmlInitNode('solid_particles_properties')
 
 
     def defaultValues(self):
         default = TurbulenceModel.defaultValues(self)
 
         default['compaction']    = 0.64
+        default['frictional_threshold'] = 0.6
         default['elasticity']    = 0.9
         default['friction']      = "none"
         default['granular']      = "none"
@@ -164,7 +174,8 @@ class SolidModel(TurbulenceModel):  # TODO : should SolidModel inherit from Turb
         """
         self.isPositiveFloat(value)
 
-        self.XMLThermo.xmlSetData('solid_compaction', value)
+        self.__init_node()
+        self.ipp_node.xmlSetData('solid_compaction', value)
 
 
     @Variables.noUndo
@@ -173,61 +184,89 @@ class SolidModel(TurbulenceModel):  # TODO : should SolidModel inherit from Turb
         Public method.
         put values for compaction.
         """
-        value = self.XMLThermo.xmlGetDouble('solid_compaction')
-        if value is None :
-           value = self.defaultValues()['compaction']
-           self.setCompaction(value)
+        value = self.defaultValues()['compaction']
+        if self.ipp_node:
+            value = self.ipp_node.xmlGetDouble('solid_compaction')
+            if value == None:
+                value = self.defaultValues()['compaction']
+
         return value
 
 
     @Variables.undoLocal
-    def setElastCoeff(self, value):
+    def setMinFrictionalThreshold(self, value):
+        """
+        Public method.
+        Set value for minimal frictional threshold.
+        """
+        self.isPositiveFloat(value)
+
+        self.__init_node()
+        self.ipp_node.xmlSetData('min_frictional_threshold', value)
+
+
+    @Variables.noUndo
+    def getMinFrictionalThreshold(self):
+        """
+        Public method.
+        Get value for minimal frictional threshold.
+        """
+
+        value = self.defaultValues()['frictional_threshold']
+        if self.ipp_node:
+            value = self.ipp_node.xmlGetDouble('min_frictional_threshold')
+            if value == None:
+                value = self.defaultValues()['frictional_threshold']
+
+        return value
+
+
+    @Variables.undoLocal
+    def setElastCoeff(self, f_id, value):
         """
         Set elasticity coefficient.
         """
-        self.isPositiveFloat(value)
-        self.XMLThermo.xmlSetData('solid_elasticity_coefficient', value)
+        self.isPositiveFloat(float(value))
+        self.__init_node()
+        self.ipp_node.xmlSetData('elasticity_coefficient', value, field_id=f_id)
 
 
     @Variables.noUndo
-    def getElastCoeff(self):
+    def getElastCoeff(self, f_id):
         """
         Get elasticity coefficient
         """
-        value = self.XMLThermo.xmlGetDouble('solid_elasticity_coefficient')
-        if value is None:
+        if self.ipp_node:
+            value = self.ipp_node.xmlGetDouble('elasticity_coefficient', field_id=f_id)
+            if value == None:
+                value = self.defaultValues()['elasticity']
+        else:
             value = self.defaultValues()['elasticity']
-            self.setElastCoeff(value)
 
         return value
 
 
     @Variables.undoLocal
-    def setCouplingStatus(self, fieldId, status):
+    def setCouplingStatus(self, status):
         """
         put polydispersed coupling status
         """
-        self.mainFieldsModel.isFieldIdValid(fieldId)
         self.isOnOff(status)
 
-        node = self.__XMLNodefields.xmlGetNode('field', field_id = fieldId)
-        node.xmlSetData('polydispersed', status)
+        node = self.XMLThermo.xmlInitNode('solid_particles_properties')
+        self.__init_node()
+        self.ipp_node.xmlSetData('polydispersion', status)
 
 
     @Variables.noUndo
-    def getCouplingStatus(self, fieldId):
+    def getCouplingStatus(self):
         """
         get polydispersed coupling status
         """
-        self.mainFieldsModel.isFieldIdValid(fieldId)
+        status = self.defaultValues()['polydispersed']
+        if self.ipp_node:
+            status = self.ipp_node.xmlGetString('polydiserpsion')
 
-        node = self.__XMLNodefields.xmlGetNode('field', field_id = fieldId)
-
-        ChildNode = node.xmlGetChildNode('polydispersed')
-        if ChildNode is None:
-           status = self.defaultValues()['polydispersed']
-           self.setCouplingStatus(fieldId, status)
-        status = node.xmlGetString('polydispersed')
         return status
 
 

python/code_saturne/model/ThermalParticlesRadiationModel.py
@@ -59,7 +59,6 @@ class ThermalParticlesRadiationModel(Variables, Model):
 
         # Default values
         self._isActivated = "off"
-        self._emissivity = "1.0"
 
         tm_node = self.case.xmlGetNode('thermophysical_models')
         self.xml_node = tm_node.xmlInitNode("interparticles_radiative_transfer")
@@ -68,8 +67,7 @@ class ThermalParticlesRadiationModel(Variables, Model):
         self.read_xml()
 
     def initialize_xml(self):
-        for childNode, value in zip(["status", "emissivity"],
-                [self._isActivated, self._emissivity]):
+        for childNode, value in zip(["status"], [self._isActivated]):
             if self.xml_node.xmlGetChildNode(childNode) == None:
                 self.xml_node.xmlInitChildNode(childNode)
                 self.xml_node.xmlSetData(childNode, value)
@@ -78,22 +76,24 @@ class ThermalParticlesRadiationModel(Variables, Model):
     def read_xml(self):
         values = []
         self._isActivated = self.xml_node.xmlGetChildString("status")
-        self._emissivity= self.xml_node.xmlGetChildString("emissivity")
 
     @property
     def isActivated(self):
         return self._isActivated
-
-    @property
-    def emissivity(self):
-        return self._emissivity
 
     @isActivated.setter
     def isActivated(self, status):
         self.xml_node.xmlSetData("status", status)
         self._isActivated = status
 
-    @emissivity.setter
-    def emissivity(self, value):
-        self.xml_node.xmlSetData("emissivity", value)
-        self._emissivity = value
+    def setEmissivity(self, field_id, value):
+        if field_id:
+            self.xml_node.xmlSetData('emissivity', value, field_id=field_id)
+
+    def getEmissivity(self, field_id):
+        retval = None
+
+        if field_id:
+            retval = self.xml_node.xmlGetChildString("emissivity", field_id=field_id)
+
+        return retval

python/code_saturne/model/XMLinitializeNeptune.py
@@ -49,6 +49,7 @@ from code_saturne.model.MainFieldsModel import MainFieldsModel
 from code_saturne.model.InterfacialForcesModel import InterfacialForcesModel
 from code_saturne.model.NeptuneWallTransferModel import NeptuneWallTransferModel
 from code_saturne.model.NucleateBoilingModel import NucleateBoilingModel
+from code_saturne.model.ThermalParticlesRadiationModel import ThermalParticlesRadiationModel
 
 from code_saturne.model.EosWrapper import eosWrapper
 
@@ -926,12 +927,79 @@ class XMLinitNeptune(BaseXmlInit):
                     node["conv_field"] = old_f_id
                     node["field_id"] = "none"
 
+        # Update particles emissivity coefficients
+        tprm = ThermalParticlesRadiationModel(self.case)
+        ipp_node = None
+        if tprm.isActivated == 'on':
+            tm_node = self.case.xmlGetNode('thermophysical_models')
+            irt_node = tm_node.xmlGetNode('interparticles_radiative_transfer')
+
+            if irt_node:
+                ipp_node = tm_node.xmlInitNode('solid_particles_properties')
+                e_node_list = irt_node.xmlGetChildNodeList('emissivity')
+                if len(e_node_list) == 1:
+                    ipp_irt_node = ipp_node.xmlInitNode('particles_radiative_transfer')
+
+                    val_e = irt_node.xmlGetChildString('emissivity')
+                    irt_node.xmlRemoveChild('emissivity')
+
+                    mfm = MainFieldsModel(self.case)
+                    for field in mfm.getSolidPhaseList():
+                        ipp_irt_node.xmlSetData('emissivity', val_e, field_id = field.f_id)
+
+                irt_node.xmlRemoveNode()
+
+        # Update polydispersion
+        _has_polydispersion = False
+        mfm = MainFieldsModel(self.case)
+        tm_node = self.case.xmlGetNode('thermophysical_models')
+        flds_node = tm_node.xmlGetNode('fields')
+        for f in mfm.getSolidPhaseList():
+            nn = flds_node.xmlGetChildNode('field', field_id=f.f_id)
+            node_poly = nn.xmlGetChildNode('polydispersed')
+            if node_poly:
+                if nn.xmlGetString('polydispersed') == 'on':
+                    _has_polydispersion = True
+                node_poly.xmlRemoveNode()
+
+        if _has_polydispersion:
+            # Only create node if needed, since default is 'off'
+            ipp_node = tm_node.xmlInitNode('solid_particles_properties')
+            ipp_node.xmlSetData('polydispersion', 'on')
+
+        # Particles compaction
+        sc_node = tm_node.xmlGetChildNode('solid_compaction')
+        if sc_node != None:
+            sc_val = tm_node.xmlGetDouble('solid_compaction')
+            sc_node.xmlRemoveNode()
+            if not ipp_node:
+                ipp_node = tm_node.xmlInitNode('solid_particles_properties')
+            ipp_node.xmlSetData('solid_compaction', sc_val)
+
+        # Particles elasticity
+        if tm_node.xmlGetChildNode('solid_elasticity_coefficient'):
+            sec_val = tm_node.xmlGetDouble('solid_elasticity_coefficient')
+            tm_node.xmlRemoveChild('solid_elasticity_coefficient')
+
+            if not ipp_node:
+                ipp_node = tm_node.xmlInitNode('solid_particles_properties')
+
+            for f in mfm.getSolidPhaseList():
+                ipp_node.xmlSetData('elasticity_coefficient', sec_val, field_id=f.f_id)
+
+
+        # Threshold
+        if ipp_node:
+            if not ipp_node.xmlGetNode('min_frictional_threshold'):
+                ipp_node.xmlSetData('min_frictional_threshold', 0.6)
+
         return
 
     def _backwardCompatibilityCurrentVersion(self):
         """
         Change XML in order to ensure backward compatibility.
         """
+        self.__backwardCompatibilityTo_9_0()
         return;
 
 #-------------------------------------------------------------------------------

commit 8d47cb2615471feae304a85c0c45dbe589b10a4b

[GUI] Fix setting of solid particles elasticity coefficent (NCFD)
  * Bug introduced in commit aa3d64c289 (2025-03-19)

python/code_saturne/gui/case/SolidView.py
@@ -223,7 +223,7 @@ class SolidView(QWidget, Ui_Solid):
         """
         if self.lineEditElastCoef.validator().state == QValidator.State.Acceptable:
             value = from_qvariant(var, float)
-            self.mdl.setElastCoeff(value, self.currentid)
+            self.mdl.setElastCoeff(self.currentid, value)
 
 
     def initializeVariables(self, fieldId):