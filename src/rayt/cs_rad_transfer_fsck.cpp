code-saturne/code_saturne

commit da137973b55e462f14f8ec9bdae6b2afdf8ce5e2

[ARCH][RAD] Switch raw pointers to cs_array in radiative solver
  * Replace temporary work arrays based on raw pointers and malloc/free
    calls with cs_array objects. This also allows for multidimensional
    arrays for grey gases based models.
  * Some code optimization, replacing const with constexpr, allowing
    compile time evaluation.

src/rayt/cs_rad_transfer_fsck.cpp
@@ -81,33 +81,33 @@ BEGIN_C_DECLS
 /*! \cond DOXYGEN_SHOULD_SKIP_THIS */
 
 /*=============================================================================
+ * Local constexpr variables
+ *============================================================================*/
+
+constexpr int ng = 100;
+constexpr int nt     = 38;
+constexpr int nconc  = 5;
+constexpr int nband  = 450;
+constexpr int maxit  = 1000;
+constexpr int imlinear    = 0;
+constexpr int im2dspline  = 6;
+constexpr cs_real_t eps    = 3e-14;
+constexpr cs_real_t x_kg[5] = {0.0001, 0.25, 0.5, 0.75, 1.0};
+
+/*=============================================================================
  * Local static variables
  *============================================================================*/
 
 static int ipass = 0;
 
-static cs_real_t  *gi;
-static cs_real_t  *tt;
-static cs_real_t  *kpco2;
-static cs_real_t  *kph2o;
-static cs_real_t  *wv;
-static cs_real_t  *dwv;
-static cs_real_t  *kmfs;
-static cs_real_t  *gq;
-
-/*=============================================================================
- * Local const variables
- *============================================================================*/
-
-const int ng = 100;
-const int nt     = 38;
-const int nconc  = 5;
-const int nband  = 450;
-const int maxit  = 1000;
-const int imlinear    = 0;
-const int im2dspline  = 6;
-const cs_real_t eps    = 3e-14;
-const cs_real_t x_kg[5] = {0.0001, 0.25, 0.5, 0.75, 1.0};
+static cs_real_t gi[ng];
+static cs_real_t tt[nt];
+static cs_real_t kpco2[nt];
+static cs_real_t kph2o[nt];
+static cs_real_t wv[nband];
+static cs_real_t dwv[nband];
+static cs_real_t kmfs[nconc*nconc*nt*nt*ng];
+static cs_real_t *gq;
 
 /*=============================================================================
  * Private function definitions
@@ -646,18 +646,16 @@ _interpolation4d(cs_real_t trad,
   int itx[4][4];
   int nix, nit;
 
-  cs_real_t *karray, *kint1, *kint2, *kint3;
-  CS_MALLOC(karray, ng*4*4*4*4, cs_real_t);
-  CS_MALLOC(kint1,  ng*4*4*4, cs_real_t);
-  CS_MALLOC(kint2,  ng*4*4, cs_real_t);
-  CS_MALLOC(kint3,  ng*4, cs_real_t);
+  cs_mdarray<cs_real_t, 5> karray({ng, 4, 4, 4, 4});
+  cs_array_4d<cs_real_t> kint1(ng, 4, 4, 4);
+  cs_array_3d<cs_real_t> kint2(ng, 4, 4);
+  cs_array_2d<cs_real_t> kint3(ng, 4);
 
-  cs_real_t *b, *c, *d, *kg_t2, *kg_x2;
-  CS_MALLOC(b, 4, cs_real_t);
-  CS_MALLOC(c, 4, cs_real_t);
-  CS_MALLOC(d, 4, cs_real_t);
-  CS_MALLOC(kg_t2, 4, cs_real_t);
-  CS_MALLOC(kg_x2, 4, cs_real_t);
+  cs_real_t b[4];
+  cs_real_t c[4];
+  cs_real_t d[4];
+  cs_real_t kg_t2[4];
+  cs_real_t kg_x2[4];
 
   /* Determine positions in x and T
    * in the NB database for interpolation. */
@@ -701,11 +699,7 @@ _interpolation4d(cs_real_t trad,
       for (int it = 0; it < nit; it++) {
         for (int itrad = 0; itrad < nit; itrad++) {
           for (int ig = 0; ig < ng; ig++) {
-            karray[  ih2o
-                   + ico2*4
-                   + it*4*4
-                   + itrad*4*4*4
-                   + ig*4*4*4*4]
+            karray(ig, itrad, it, ico2, ih2o)
               = kmfs[  ih2oa[ih2o]
                      + ico2a[ico2]*nconc
                      + ita[it]*nconc*nconc
@@ -732,16 +726,16 @@ _interpolation4d(cs_real_t trad,
             int nargs = 4;
             _splmi(nargs,
                    kg_x2,
-                   &karray[ih2o + ico2*4 + it*4*4 + itrad*4*4*4 + ig*4*4*4*4],
+                   karray.sub_array(ig, itrad, it, ico2),
                    b,
                    c,
                    d);
 
-            kint1[ico2 + it*4 + itrad*4*4 + ig*4*4*4]
+            kint1(ig, itrad, ig, ico2)
               = _seval(nargs,
                        xh2o,
                        kg_x2,
-                       &karray[ih2o + ico2*4 + it*4*4 + itrad*4*4*4 + ig*4*4*4*4],
+                       karray.sub_array(ig, itrad, it, ico2),
                        b,
                        c,
                        d);
@@ -756,9 +750,9 @@ _interpolation4d(cs_real_t trad,
       for (int it = 0; it < nit; it++) {
         for (int itrad = 0; itrad < nit; itrad++) {
           for (int ig = 0; ig < ng; ig++) {
-            kint1[ico2 + it*4 + itrad*4*4 + ig*4*4*4]
-              =          wx  * karray[1 + ico2*4 + it*4*4 + itrad*4*4*4 + ig*4*4*4*4]
-                + (1.0 - wx) * karray[0 + ico2*4 + it*4*4 + itrad*4*4*4 + ig*4*4*4*4];
+            kint1(ig, itrad, it, ico2)
+              =          wx  * karray(ig, itrad, it, ico2, 1)
+                + (1.0 - wx) * karray(ig, itrad, it, ico2, 0);
           }
         }
       }
@@ -780,15 +774,15 @@ _interpolation4d(cs_real_t trad,
           int nargs = 4;
           _splmi(nargs,
                  kg_x2,
-                 &kint1[ico2 + it*4 + itrad*4*4 + ig*4*4*4],
+                 kint1.sub_array(ig, itrad, it),
                  b,
                  c,
                  d);
-          kint2[it + itrad*4 + ig*4*4]
+          kint2(ig, itrad, it)
             = _seval(nargs,
                      xco2,
                      kg_x2,
-                     &kint1[ico2 + it*4 + itrad*4*4 + ig*4*4*4],
+                     kint1.sub_array(ig, itrad, it),
                      b,
                      c,
                      d);
@@ -801,9 +795,9 @@ _interpolation4d(cs_real_t trad,
     for (int it = 0; it < nit; it++) {
       for (int itrad = 0; itrad < nit; itrad++) {
         for (int ig = 0; ig < ng; ig++) {
-          kint2[it + itrad*4 + ig*4*4]
-            =          wx  * kint1[1 + it*4 + itrad*4*4 + ig*4*4*4]
-              + (1.0 - wx) * kint1[0 + it*4 + itrad*4*4 + ig*4*4*4];
+          kint2(ig, itrad, it)
+            =          wx  * kint1(ig, itrad, it, 1)
+              + (1.0 - wx) * kint1(ig, itrad, it, 0);
         }
       }
     }
@@ -821,17 +815,17 @@ _interpolation4d(cs_real_t trad,
         int nargs = 4;
         _splmi(nargs,
                kg_t2,
-               &kint2[0 + itrad*4 + ig*4*4],
+               kint2.sub_array(ig, itrad),
                b,
                c,
                d);
-        kint3[itrad + ig*4] = _seval(nargs,
-                                     t,
-                                     kg_t2,
-                                     &kint2[0 + itrad*4 + ig*4*4],
-                                     b,
-                                     c,
-                                     d);
+        kint3(ig, itrad) = _seval(nargs,
+                                  t,
+                                  kg_t2,
+                                  kint2.sub_array(ig, itrad),
+                                  b,
+                                  c,
+                                  d);
       }
     }
   }
@@ -839,8 +833,8 @@ _interpolation4d(cs_real_t trad,
     cs_real_t wt = (t - tt[ita[0]]) / (tt[ita[1]] - tt[ita[0]]);
     for (int itrad = 0; itrad < nit; itrad++) {
       for (int ig = 0; ig < ng; ig++) {
-        kint3[itrad + ig*4] =         wt  * kint2[1 + itrad*4 + ig*4*4]
-                             + (1.0 - wt) * kint2[0 + itrad*4 + ig*4*4];
+        kint3(ig, itrad) =        wt  * kint2(ig, itrad, 1)
+                         + (1.0 - wt) * kint2(ig, itrad, 0);
       }
     }
   }
@@ -856,30 +850,20 @@ _interpolation4d(cs_real_t trad,
       int nargs = 4;
       _splmi(nargs,
              kg_t2,
-             &kint3[0 + ig*4],
+             kint3.sub_array(ig),
              b,
              c,
              d);
-      kdb[ig] = _seval(nargs, trad, kg_t2, &kint3[0 + ig*4], b, c, d);
+      kdb[ig] = _seval(nargs, trad, kg_t2, kint3.sub_array(ig), b, c, d);
     }
   }
   else {
     cs_real_t wt = (trad - tt[itrada[0]]) / (tt[itrada[1]] - tt[itrada[0]]);
     for (int ig = 0; ig < ng; ig++) {
-      kdb[ig] = wt * kint3[1 + ig*4] + (1.0 - wt) * kint3[0 + ig*4];
+      kdb[ig] = wt * kint3(ig, 1) + (1.0 - wt) * kint3(ig, 0);
     }
   }
 
-  /* Free memory */
-  CS_FREE(karray);
-  CS_FREE(kint1);
-  CS_FREE(kint2);
-  CS_FREE(kint3);
-  CS_FREE(b);
-  CS_FREE(c);
-  CS_FREE(d);
-  CS_FREE(kg_t2);
-  CS_FREE(kg_x2);
 }
 
 /*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
@@ -912,20 +896,17 @@ cs_rad_transfer_fsck(const cs_real_t  *restrict pco2,
 {
   /* Initialization */
 
-  cs_real_t   *gfskref, *kfskref, *kfsk, *gfsk, *gg1;
-  cs_real_t   *kg1, *as, *ag, *aw, *kloctmp;
+  cs_real_t gfskref[ng];
+  cs_real_t kfskref[ng];
+  cs_real_t gfsk[ng];
+  cs_real_t kfsk[ng];
+  cs_real_t gg1[ng];
+  cs_real_t kg1[ng];
+  cs_real_t as[ng];
 
-  CS_MALLOC(gfskref, ng, cs_real_t);
-  CS_MALLOC(kfskref, ng, cs_real_t);
-  CS_MALLOC(gfsk, ng, cs_real_t);
-  CS_MALLOC(kfsk, ng, cs_real_t);
-  CS_MALLOC(gg1, ng, cs_real_t);
-  CS_MALLOC(kg1, ng, cs_real_t);
-  CS_MALLOC(as, ng, cs_real_t);
-
-  CS_MALLOC(ag, cs_glob_rad_transfer_params->nwsgg, cs_real_t);
-  CS_MALLOC(aw, cs_glob_rad_transfer_params->nwsgg, cs_real_t);
-  CS_MALLOC(kloctmp, cs_glob_rad_transfer_params->nwsgg, cs_real_t);
+  cs_array<cs_real_t> ag(cs_glob_rad_transfer_params->nwsgg);
+  cs_array<cs_real_t> aw(cs_glob_rad_transfer_params->nwsgg);
+  cs_array<cs_real_t> kloctmp(cs_glob_rad_transfer_params->nwsgg);
 
   cs_field_t *f_bound_t = cs_field_by_name_try("boundary_temperature");
   cs_real_t *tpfsck = f_bound_t->val;
@@ -938,15 +919,6 @@ cs_rad_transfer_fsck(const cs_real_t  *restrict pco2,
     FILE *radfile = nullptr;
     const char *pathdatadir = cs_base_get_pkgdatadir();
     char filepath[256];
-
-    CS_MALLOC(gi,    ng, cs_real_t);
-    CS_MALLOC(tt,    nt, cs_real_t);
-    CS_MALLOC(kpco2, nt, cs_real_t);
-    CS_MALLOC(kph2o, nt, cs_real_t);
-    CS_MALLOC(wv,    nband, cs_real_t);
-    CS_MALLOC(dwv,   nband, cs_real_t);
-
-    CS_MALLOC(kmfs, nconc * nconc * nt * nt * ng, cs_real_t);
 
     /* Read k-distributions */
     {
@@ -1027,17 +999,15 @@ cs_rad_transfer_fsck(const cs_real_t  *restrict pco2,
     /* Allocation */
     CS_MALLOC(gq, cs_glob_rad_transfer_params->nwsgg, cs_real_t);
     int m = (cs_glob_rad_transfer_params->nwsgg + 1) / 2;
-    cs_real_t *p1, *p2, *p3, *pp;
-    cs_real_t *z, *z1, *arth;
-    bool *unfinished;
-    CS_MALLOC(p1, m, cs_real_t);
-    CS_MALLOC(p2, m, cs_real_t);
-    CS_MALLOC(p3, m, cs_real_t);
-    CS_MALLOC(pp, m, cs_real_t);
-    CS_MALLOC(z,  m, cs_real_t);
-    CS_MALLOC(z1, m, cs_real_t);
-    CS_MALLOC(arth, m, cs_real_t);
-    CS_MALLOC(unfinished, m, bool);
+
+    cs_array<cs_real_t> p1(m);
+    cs_array<cs_real_t> p2(m);
+    cs_array<cs_real_t> p3(m);
+    cs_array<cs_real_t> pp(m);
+    cs_array<cs_real_t> z(m);
+    cs_array<cs_real_t> z1(m);
+    cs_array<cs_real_t> arth(m);
+    cs_array<bool> unfinished(m);
 
     /* Boundaries */
     cs_real_t x1 = 0.0;
@@ -1126,19 +1096,10 @@ cs_rad_transfer_fsck(const cs_real_t  *restrict pco2,
 
     for (int j = 0; j < m; j++) {
       cs_glob_rad_transfer_params->wq[j]
-        = 2.0 * xl / ((1.0 - pow(z[j], 2.0)) * pow(pp[j], 2.0));
+        = 2.0 * xl / ((1.0 - pow(z[j], 2.0)) * cs::pow2(pp[j]));
       cs_glob_rad_transfer_params->wq[n - j + 1]
         = cs_glob_rad_transfer_params->wq[j];
     }
-
-    CS_FREE(p1);
-    CS_FREE(p2);
-    CS_FREE(p3);
-    CS_FREE(pp);
-    CS_FREE(z);
-    CS_FREE(z1);
-    CS_FREE(arth);
-    CS_FREE(unfinished);
 
   }
 
@@ -1230,7 +1191,7 @@ cs_rad_transfer_fsck(const cs_real_t  *restrict pco2,
                     kfsk,
                     cs_glob_rad_transfer_params->nwsgg,
                     gq,
-                    kloctmp);
+                    kloctmp.data());
     for (int i = 0; i < cs_glob_rad_transfer_params->nwsgg; i++)
       kloc[i * cs_glob_mesh->n_cells + iel] = kloctmp[i];
 
@@ -1267,7 +1228,7 @@ cs_rad_transfer_fsck(const cs_real_t  *restrict pco2,
                     as,
                     cs_glob_rad_transfer_params->nwsgg,
                     gq,
-                    ag);
+                    ag.data());
     for (int i = 0; i < cs_glob_rad_transfer_params->nwsgg; i++)
       aloc[i * cs_glob_mesh->n_cells + iel] = ag[i];
 
@@ -1300,24 +1261,16 @@ cs_rad_transfer_fsck(const cs_real_t  *restrict pco2,
                     as,
                     cs_glob_rad_transfer_params->nwsgg,
                     gq,
-                    aw);
+                    aw.data());
     for (int i = 0; i < cs_glob_rad_transfer_params->nwsgg; i++)
       alocb[i * cs_glob_mesh->n_b_faces + ifac] = aw[i];
   }
 
   /* free memory */
   if (cs_glob_time_step->nt_cur == cs_glob_time_step->nt_max) {
-    CS_FREE(gi);
-    CS_FREE(tt);
-    CS_FREE(kpco2);
-    CS_FREE(kph2o);
-    CS_FREE(wv);
-    CS_FREE(dwv);
-    CS_FREE(kmfs);
     CS_FREE(gq);
   }
 
-  CS_FREE(kloctmp);
 }
 
 /*----------------------------------------------------------------------------*/

src/base/cs_mdspan.h
@@ -435,7 +435,7 @@ public:
   (
     T*      data_ptr, /*!<[in] New data pointer */
     Args... dims      /*!<[in] New dimensions (integer types) */
-  ) const
+  )
   {
     check_operator_args_(dims...);
     set_size_(dims...);

src/rayt/cs_rad_transfer_absorption.cpp
@@ -126,7 +126,9 @@ cs_rad_transfer_absorption(const cs_real_t  tempk[],
                            cs_real_t        agas[],
                            cs_real_t        agasb[])
 {
-  cs_real_t *w1 = nullptr, *w2 = nullptr, *w3 = nullptr;
+  cs_array<cs_real_t> w1;
+  cs_array<cs_real_t> w2;
+  cs_array<cs_real_t> w3;
 
   const cs_mesh_t *m = cs_glob_mesh;
   const int n_cells = m->n_cells;
@@ -140,9 +142,9 @@ cs_rad_transfer_absorption(const cs_real_t  tempk[],
   if (   rt_params->imgrey >= 1
       || rt_params->imoadf >= 1
       || rt_params->imfsck >= 1) {
-    CS_MALLOC(w1, n_cells_ext, cs_real_t);
-    CS_MALLOC(w2, n_cells_ext, cs_real_t);
-    CS_MALLOC(w3, n_cells_ext, cs_real_t);
+    w1.reshape(n_cells_ext);
+    w2.reshape(n_cells_ext);
+    w3.reshape(n_cells_ext);
   }
 
   cs_real_t *crom = CS_F_(rho)->val;
@@ -166,12 +168,11 @@ cs_rad_transfer_absorption(const cs_real_t  tempk[],
       const int n_gas_g = cm->n_gas_species;
       cs_real_t xpro;
 
-      cs_real_t *_w;
-      CS_MALLOC(_w, n_gas_e + n_gas_g + n_gas_e, cs_real_t);
+      cs_array<cs_real_t> _w(n_gas_e + n_gas_g + n_gas_e);
 
-      cs_real_t *xk = _w;
-      cs_real_t *yi = _w + n_gas_e;
-      cs_real_t *yk = _w + (n_gas_e + n_gas_g);
+      cs_real_t *xk = _w.data();
+      cs_real_t *yi = _w.data() + n_gas_e;
+      cs_real_t *yk = _w.data() + (n_gas_e + n_gas_g);
 
       const cs_real_t xsoot = cm->xsoot;
       const cs_real_t rosoot = cm->rosoot;
@@ -229,10 +230,9 @@ cs_rad_transfer_absorption(const cs_real_t  tempk[],
       }
 
       if (rt_params->imgrey == 1) {
-        cs_rad_transfer_modak(cpro_cak0, w1, w2, w3, cpro_temp);
+        cs_rad_transfer_modak(cpro_cak0, w1.data(), w2.data(), w3.data(), cpro_temp);
       }
 
-      CS_FREE(_w);
     }
     else if (rt_params->imfsck == 2) {
 
@@ -280,16 +280,16 @@ cs_rad_transfer_absorption(const cs_real_t  tempk[],
       }
 
       if (rt_params->imgrey == 1)
-        cs_rad_transfer_modak(cpro_cak0, w1, w2, w3, cpro_temp);
+        cs_rad_transfer_modak(cpro_cak0, w1.data(), w2.data(), w3.data(), cpro_temp);
 
       else if (rt_params->imoadf == 1)
-        cs_rad_transfer_adf08(w1, w2, tempk, kgas, agas, agasb);
+        cs_rad_transfer_adf08(w1.data(), w2.data(), tempk, kgas, agas, agasb);
 
       else if (rt_params->imoadf == 2)
-        cs_rad_transfer_adf50(w1, w2, tempk, kgas, agas, agasb);
+        cs_rad_transfer_adf50(w1.data(), w2.data(), tempk, kgas, agas, agasb);
 
       else if (rt_params->imfsck >= 1)
-        cs_rad_transfer_fsck(w1, w2, tempk, kgas, agas, agasb);
+        cs_rad_transfer_fsck(w1.data(), w2.data(), tempk, kgas, agas, agasb);
     }
 
     else {
@@ -298,12 +298,6 @@ cs_rad_transfer_absorption(const cs_real_t  tempk[],
     }
 
   }
-
-  /* Free temporary memory */
-
-  CS_FREE(w1);
-  CS_FREE(w2);
-  CS_FREE(w3);
 
   /* Absorption coefficient of particles per class k2/x2 (m-1)
      --------------------------------------------------------- */
@@ -377,8 +371,10 @@ cs_rad_transfer_absorption(const cs_real_t  tempk[],
      Do not use with ADF model !!! */
 
   if (rt_params->type == CS_RAD_TRANSFER_P1 && rt_params->imoadf == 0) {
-
-    CS_MALLOC(w3, n_cells_ext, cs_real_t);
+    if (w3.size() == 0)
+      w3.reshape(n_cells_ext);
+    else
+      w3.zero();
 
     for (cs_lnum_t cell_id = 0; cell_id < n_cells; cell_id++)
       w3[cell_id] = cpro_cak0[cell_id];
@@ -404,9 +400,7 @@ cs_rad_transfer_absorption(const cs_real_t  tempk[],
 
     }
 
-    cs_rad_transfer_absorption_check_p1(w3);
-
-    CS_FREE(w3);
+    cs_rad_transfer_absorption_check_p1(w3.data());
 
   }
 }
@@ -429,8 +423,6 @@ cs_rad_transfer_rcfsk_absorption(const cs_real_t  tempk[],
                                  cs_real_t        agas[],
                                  cs_real_t        agasb[])
 {
-  cs_real_t *w1 = nullptr, *w2 = nullptr, *w3 = nullptr;
-
   const cs_mesh_t *m           = cs_glob_mesh;
   const int        n_cells     = m->n_cells;
   const int        n_cells_ext = m->n_cells_with_ghosts;
@@ -440,9 +432,9 @@ cs_rad_transfer_rcfsk_absorption(const cs_real_t  tempk[],
 
   /* Initialization */
 
-  CS_MALLOC(w1, n_cells_ext, cs_real_t);
-  CS_MALLOC(w2, n_cells_ext, cs_real_t);
-  CS_MALLOC(w3, n_cells_ext, cs_real_t);
+  cs_array<cs_real_t> w1(n_cells_ext);
+  cs_array<cs_real_t> w2(n_cells_ext);
+  cs_array<cs_real_t> w3(n_cells_ext);
 
   cs_real_t *crom = CS_F_(rho)->val;
 
@@ -491,15 +483,10 @@ cs_rad_transfer_rcfsk_absorption(const cs_real_t  tempk[],
     }
 
     /* Calculation of the absorption coefficient using the RCFSK scheme */
-    cs_rad_transfer_rcfsk(w1, w2, w3, tempk, kgas, agas, agasb);
+    cs_rad_transfer_rcfsk(w1.data(), w2.data(), w3.data(), tempk, kgas, agas, agasb);
 
   }
 
-  /* Free temporary memory */
-
-  CS_FREE(w1);
-  CS_FREE(w2);
-  CS_FREE(w3);
 }
 
 /*----------------------------------------------------------------------------*/

src/rayt/cs_rad_transfer_adf_models.cpp
@@ -173,19 +173,20 @@ cs_rad_transfer_adf08(const cs_real_t  pco2[],
 
   /* Memory allocation and initialization */
 
-  cs_real_t *y, *tpaadf;
   cs_field_t *f_b_temp = cs_field_by_name_try("boundary_temperature");
-  CS_MALLOC(y, cs_glob_mesh->n_cells_with_ghosts, cs_real_t);
+
+  cs_array<cs_real_t> tpaadf;
+  cs_array<cs_real_t> y(cs_glob_mesh->n_cells_with_ghosts);
 
   if (cs_glob_thermal_model->temperature_scale == CS_TEMPERATURE_SCALE_CELSIUS) {
 
-    CS_MALLOC(tpaadf, nfabor, cs_real_t );
+    tpaadf.reshape(nfabor);
     for (cs_lnum_t ifac = 0; ifac < nfabor; ifac++)
       tpaadf[ifac] = f_b_temp->val[ifac] + tkelvi;
 
   }
   else
-    tpaadf = f_b_temp->val;
+    tpaadf = cs_array<cs_real_t>(f_b_temp->val, nfabor);
 
   /* Absorption coefficient of gas mix (m-1)
      --------------------------------------- */
@@ -261,22 +262,20 @@ cs_rad_transfer_adf08(const cs_real_t  pco2[],
       fgets(line, 256, radfile);
 
       for (int j = 0; j < ntsto; j++) {
-        cs_real_t *temp;
-        CS_MALLOC(temp, 2*nysto, cs_real_t);
+        cs_array_2d<cs_real_t>temp(2, nysto);
         int nvalues;
-        _line_to_array(radfile, temp, &nvalues);
+        _line_to_array(radfile, temp.data(), &nvalues);
         assert(nvalues == nysto * 2);
         for (int k = 0; k < nysto; k++) {
-          ksto2[i + k * nwsgg + j * nysto * nwsgg] = temp[k];
+          ksto2[i + k * nwsgg + j * nysto * nwsgg] = temp(0, k);
           /* ksto2(i,k,j): Radiation coefficient of the i-th grey gas, */
           /*               the k-th ratio of ph2o/pco2, and */
           /*               the j-th tabulated temperature   */
-          asto[i + k * nwsgg + j * nysto * nwsgg] = temp[k + nysto];
+          asto[i + k * nwsgg + j * nysto * nwsgg] = temp(1, k);
           /* asto2(i,k,j): Weight of the i-th grey gas,     */
           /*               the k-th ratio of ph2o/pco2, and */
           /*               the j-th tabulated temperature   */
         }
-        CS_FREE(temp);
       }
     }
   }
@@ -402,7 +401,6 @@ cs_rad_transfer_adf08(const cs_real_t  pco2[],
 
   }
 
-  CS_FREE(tpaadf);
 }
 
 /*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
@@ -443,18 +441,18 @@ cs_rad_transfer_adf50(const cs_real_t  pco2[],
 
   /* Memory allocation and initialization */
 
-  cs_real_t *tpaadf;
+  cs_array<cs_real_t> tpaadf;
   cs_field_t *f_b_temp = cs_field_by_name_try("boundary_temperature");
 
   if (cs_glob_thermal_model->temperature_scale == CS_TEMPERATURE_SCALE_CELSIUS) {
 
-    CS_MALLOC(tpaadf, nfabor, cs_real_t );
+    tpaadf.reshape(nfabor);
     for (cs_lnum_t ifac = 0; ifac < nfabor; ifac++)
       tpaadf[ifac] = f_b_temp->val[ifac] + tkelvi;
 
   }
   else
-    tpaadf = f_b_temp->val;
+    tpaadf = cs_array<cs_real_t>(f_b_temp->val, nfabor);
 
   /* Absorption coefficient of gas mix (m-1)
      --------------------------------------- */
@@ -676,7 +674,6 @@ cs_rad_transfer_adf50(const cs_real_t  pco2[],
     }
   }
 
-  CS_FREE(tpaadf);
 }
 
 /*----------------------------------------------------------------------------*/

src/rayt/cs_rad_transfer_bcs.cpp
@@ -151,26 +151,22 @@ _set_internal_coupling_bcs(cs_internal_coupling_t  *cpl,
   }
 
   if (have_unset) {
-    int *is_solid = nullptr;
 
     if (n_distant > 0) {
       cs_lnum_t n_cells_ext = m->n_cells_with_ghosts;
-      CS_MALLOC(is_solid, n_cells_ext, int);
-      for (cs_lnum_t i = 0; i < n_cells_ext; i++)
-        is_solid[i] = 0;
-      cs_volume_zone_tag_cell_type(CS_VOLUME_ZONE_SOLID, 1, is_solid);
-    }
+      cs_array<int> is_solid(n_cells_ext);
+      is_solid.zero();
+      cs_volume_zone_tag_cell_type(CS_VOLUME_ZONE_SOLID, 1, is_solid.data());
 
-    for (cs_lnum_t i = 0; i < n_distant; i++) {
-      cs_lnum_t face_id = faces_local[i];
-      if (beps[face_id] < 0) {
-        cs_lnum_t cell_id = m->b_face_cells[face_id];
-        if (is_solid[cell_id])
-          beps[face_id] = 0;
+      for (cs_lnum_t i = 0; i < n_distant; i++) {
+        cs_lnum_t face_id = faces_local[i];
+        if (beps[face_id] < 0) {
+          cs_lnum_t cell_id = m->b_face_cells[face_id];
+          if (is_solid[cell_id])
+            beps[face_id] = 0;
+        }
       }
     }
-
-    CS_FREE(is_solid);
   }
 }
 
@@ -257,12 +253,10 @@ cs_rad_transfer_bcs(int bc_type[])
 
   /* Allocate temporary arrays */
 
-  int  *isothm;
-  cs_real_t *tempk, *text, *twall;
-  CS_MALLOC(isothm, n_b_faces, int);
-  CS_MALLOC(tempk, cs_glob_mesh->n_cells_with_ghosts, cs_real_t);
-  CS_MALLOC(text, n_b_faces, cs_real_t);
-  CS_MALLOC(twall, n_b_faces, cs_real_t);
+  cs_array<int> isothm(n_b_faces);
+  cs_array<cs_real_t> tempk(cs_glob_mesh->n_cells_with_ghosts);
+  cs_array<cs_real_t> text(n_b_faces);
+  cs_array<cs_real_t> twall(n_b_faces);
 
   /* Map field arrays */
   cs_field_t *f_tempb = CS_F_(t_b);
@@ -369,27 +363,27 @@ cs_rad_transfer_bcs(int bc_type[])
     /* User definitions */
 
     cs_gui_radiative_transfer_bcs(bc_type,
-                                  isothm,
+                                  isothm.data(),
                                   f_beps->val,
                                   f_bepa->val,
-                                  text,
+                                  text.data(),
                                   f_bxlam->val);
 
     cs_user_radiative_transfer_bcs(cs_glob_domain,
                                    bc_type,
-                                   isothm,
+                                   isothm.data(),
                                    &tmin,
                                    &tmax,
                                    &tx,
                                    dt,
-                                   twall,
+                                   twall.data(),
                                    f_qinci->val,
                                    f_bhconv->val,
                                    f_bfconv->val,
                                    f_bxlam->val,
                                    f_bepa->val,
                                    f_beps->val,
-                                   text);
+                                   text.data());
 
     cs_log_printf(CS_LOG_DEFAULT,
                   _("\n"
@@ -416,27 +410,27 @@ cs_rad_transfer_bcs(int bc_type[])
   /* Values for boundary faces */
 
   cs_gui_radiative_transfer_bcs(bc_type,
-                                isothm,
+                                isothm.data(),
                                 f_beps->val,
                                 f_bepa->val,
-                                text,
+                                text.data(),
                                 f_bxlam->val);
 
   cs_user_radiative_transfer_bcs(cs_glob_domain,
                                  bc_type,
-                                 isothm,
+                                 isothm.data(),
                                  &tmin,
                                  &tmax,
                                  &tx,
                                  dt,
-                                 twall,
+                                 twall.data(),
                                  f_qinci->val,
                                  f_bhconv->val,
                                  f_bfconv->val,
                                  f_bxlam->val,
                                  f_bepa->val,
                                  f_beps->val,
-                                 text);
+                                 text.data());
 
   /* Internal coupling settings */
   if (cs_internal_coupling_n_couplings() > 0) {
@@ -452,10 +446,10 @@ cs_rad_transfer_bcs(int bc_type[])
 
     if (cpl != nullptr)
       _set_internal_coupling_bcs(cpl,
-                                 isothm,
+                                 isothm.data(),
                                  f_bxlam->val,
                                  f_beps->val,
-                                 text);
+                                 text.data());
   }
 
   /* Check user BC definitions */
@@ -941,7 +935,7 @@ cs_rad_transfer_bcs(int bc_type[])
     cs_field_t *f_enthalpy = CS_F_(h);
 
     /* Results: T to K */
-    cs_ht_convert_h_to_t_cells(f_enthalpy->val, tempk);
+    cs_ht_convert_h_to_t_cells(f_enthalpy->val, tempk.data());
 
   }
 
@@ -980,19 +974,19 @@ cs_rad_transfer_bcs(int bc_type[])
   }
 
   if (!is_start)
-    cs_rad_transfer_compute_wall_t(isothm,
+    cs_rad_transfer_compute_wall_t(isothm.data(),
                                    tmin,
                                    tmax,
                                    tx,
                                    f_qinci->val,
-                                   text,
+                                   text.data(),
                                    f_bxlam->val,
                                    f_bepa->val,
                                    f_beps->val,
                                    f_bhconv->val,
                                    f_bfconv->val,
-                                   tempk,
-                                   twall);
+                                   tempk.data(),
+                                   twall.data());
 
   /* Change user boundary conditions */
 
@@ -1025,11 +1019,10 @@ cs_rad_transfer_bcs(int bc_type[])
     /* Read user data;
      * convert twall to enthalpy at boundary */
 
-    cs_lnum_t *lstfac;
-    CS_MALLOC(lstfac, n_b_faces, cs_lnum_t);
+    cs_array<cs_lnum_t> lstfac(n_b_faces);
 
-    cs_real_t *wall_enth = nullptr, *ext_enth = nullptr;
-    CS_MALLOC(wall_enth, n_b_faces, cs_real_t);
+    cs_array<cs_real_t> ext_enth;
+    cs_array<cs_real_t> wall_enth(n_b_faces);
     for (cs_lnum_t face_id = 0; face_id < n_b_faces; face_id++)
       wall_enth[face_id] = 0.;
 
@@ -1046,7 +1039,7 @@ cs_rad_transfer_bcs(int bc_type[])
       }
     }
     if (nlst > 0)
-      cs_ht_convert_t_to_h_faces_l(nlst, lstfac, twall, wall_enth);
+      cs_ht_convert_t_to_h_faces_l(nlst, lstfac.data(), twall.data(), wall_enth.data());
 
     nlst = 0;
     for (cs_lnum_t face_id = 0; face_id < n_b_faces; face_id++) {
@@ -1060,11 +1053,9 @@ cs_rad_transfer_bcs(int bc_type[])
     }
 
     if (nlst > 0) {
-      CS_MALLOC(ext_enth, n_b_faces, cs_real_t);
-      for (cs_lnum_t face_id = 0; face_id < n_b_faces; face_id++)
-        ext_enth[face_id] = 0.;
-
-      cs_ht_convert_t_to_h_faces_l(nlst, lstfac, text, ext_enth);
+      ext_enth.reshape(n_b_faces);
+      ext_enth.zero();
+      cs_ht_convert_t_to_h_faces_l(nlst, lstfac.data(), text.data(), ext_enth.data());
     }
 
     for (cs_lnum_t face_id = 0; face_id < n_b_faces; face_id++) {
@@ -1113,9 +1104,6 @@ cs_rad_transfer_bcs(int bc_type[])
       }
     }
 
-    CS_FREE(lstfac);
-    CS_FREE(ext_enth);
-    CS_FREE(wall_enth);
   }
 
   /* Update boundary temperature field   */
@@ -1137,12 +1125,6 @@ cs_rad_transfer_bcs(int bc_type[])
     }
   }
 
-  /* Free memory */
-
-  CS_FREE(isothm);
-  CS_FREE(tempk);
-  CS_FREE(text);
-  CS_FREE(twall);
 }
 
 /*----------------------------------------------------------------------------*/

commit fcfae74a3dde831f3f219db6d45a27d6b9018c8d

[RAD] Fix index error introduced in commit da137973b5

src/rayt/cs_rad_transfer_fsck.cpp
@@ -728,7 +728,7 @@ _interpolation4d(cs_real_t trad,
                    c,
                    d);
 
-            kint1(ig, itrad, ig, ico2)
+            kint1(ig, itrad, it, ico2)
               = _seval(nargs,
                        xh2o,
                        kg_x2,