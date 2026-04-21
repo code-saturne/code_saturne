code-saturne/code_saturne

commit 979c857ceae4248cfd573c027bb6fc3d748383cc

Fixes related to notebook variables properties
- Fix restart property when modified in the graphical user interface
  Related to issue 158 in github
- Fix logging of notebook variables

python/code_saturne/model/NotebookModel.py
@@ -338,14 +338,19 @@ class NotebookModel(Model):
         when reading a restart file.
         """
         if idx != None:
-            node = self.node_note.xmlInitChildNode("var", id = idx)
+            self._setVariableAttr(idx, 'restart', restart)
         elif var != None:
             node = self.node_note.xmlInitChildNode("var", name = var)
+            node['restart'] = restart
+
+            # If default value set the hidden value for cleanup
+            if val == self.defaultNotebookValues()['restart']:
+                node["_{}".format('restart')] = val
+            else:
+                node.xmlDelAttribute("_{}".format('restart'))
+
         else:
             raise Exception("No id or name were specified for setVariableRestart")
-
-        node['restart'] = restart
-
 
     @Variables.noUndo
     def getVariableLog(self, idx):

src/base/cs_notebook.cpp
@@ -328,10 +328,10 @@ cs_notebook_load_from_file(void)
         restart = false;
 
     /* Editable values are printed by default */
-    bool log = editable;
+    bool log = false;
     if (c_log != nullptr)
-      if (strcmp(c_log, "No") == 0)
-        log = false;
+      if (strcmp(c_log, "Yes") == 0)
+        log = true;
 
     /* If the variable is uncertain then :
      * - an input cannot be modifed, hence editable = false
@@ -640,7 +640,7 @@ cs_notebook_log(void)
      in default log file. Also compute the maximum length of names */
   if (_max_size_name < 1)
     for (int i = 0; i < _n_entries; i++) {
-      log_needed = _entries[i]->log;
+      log_needed = log_needed || _entries[i]->log;
       _max_size_name = cs::max(_max_size_name, strlen(_entries[i]->name));
     }
 

commit 421009c168493735ff7960ef94690b3eccc9bea8

[CLI] Fix launching run using the --notebook-args option broken by commit 979c857c

python/code_saturne/model/NotebookModel.py
@@ -344,8 +344,8 @@ class NotebookModel(Model):
             node['restart'] = restart
 
             # If default value set the hidden value for cleanup
-            if val == self.defaultNotebookValues()['restart']:
-                node["_{}".format('restart')] = val
+            if restart == self.defaultNotebookValues()['restart']:
+                node["_{}".format('restart')] = restart
             else:
                 node.xmlDelAttribute("_{}".format('restart'))
 