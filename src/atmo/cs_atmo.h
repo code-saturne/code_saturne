code-saturne/code_saturne

commit 09d14fdfcfee799e8ca09ac3d48ee21122cd71be

Fix compilation with some older compilers.

src/atmo/cs_atmo.h
@@ -178,26 +178,20 @@ typedef struct {
   cs_atmo_projection_t projection_type;
   int utm_zone; // If using utm, a zone needs to be specified
 
-  union {
-    int met_1d_nlevels_d;
-    [[deprecated]] int nbmetd;
-  };
+  int met_1d_nlevels_d;
+  [[deprecated]] int& nbmetd() {return met_1d_nlevels_d;};
+
   /*! numbers of altitudes for the temperature and specific humidity */
-  union {
-    int met_1d_nlevels_t;
-    [[deprecated]] int nbmett;
-  };
+  int met_1d_nlevels_t;
+  [[deprecated]] int& nbmett() {return met_1d_nlevels_t;};
+
   /*! numbers of time steps for the meteo profiles */
-  union {
-    int met_1d_ntimes;
-    [[deprecated]] int nbmetm;
-  };
+  int met_1d_ntimes;
+  [[deprecated]] int& nbmetm() {return met_1d_ntimes;};
 
   /*! Number of vertical levels */
-  union {
-    int met_1d_nlevels_max_t;
-    [[deprecated]] int nbmaxt;
-  };
+  int met_1d_nlevels_max_t;
+  [[deprecated]] int& nbmaxt() {return met_1d_ntimes;};
 
   /*! Domain orientation (angle in degree between y direction and north),
    * 0 by default */

src/lagr/cs_lagr.cpp
@@ -1165,7 +1165,7 @@ cs_lagr_map_specific_physics(void)
     bft_error(__FILE__, __LINE__, 0,
         "%s: Turbulence modelling is not set.", __func__);
 
-  _lagr_extra_module[0].iturb  = turb_model->iturb;
+  _lagr_extra_module[0].iturb  = turb_model->model;
   _lagr_extra_module[0].itytur = turb_model->itytur;
   _lagr_extra_module[0].icp    = cs_glob_fluid_properties->icp;
 

src/lagr/cs_lagr.h
@@ -442,26 +442,23 @@ typedef struct {
       Useful if \ref physical_model=1 and if there is a thermal scalar
       associated with the continuous phase.
   */
-  union {
-    int   solve_temperature;
-    [[deprecated]] int   itpvar;
-  };
+  int   solve_temperature;
+  [[deprecated("renamed to solve_temperature")]] \
+  int&  itpvar() {return solve_temperature;};
 
   /*  activation (=1) or not (=0) of an evolution equation on the particle
       diameter. Useful if \ref physical_model = 1.
   */
-  union {
-    int   solve_diameter;
-    [[deprecated]] int   idpvar;
-  };
+  int   solve_diameter;
+  [[deprecated("renamed to solve_diameter")]] \
+  int&   idpvar() {return solve_diameter;};
 
   /*  activation (=1) or not (=0) of an evolution equation on the particle mass
       Useful if \ref physical_model = 1
   */
-  union {
-    int   solve_mass;
-    [[deprecated]] int   impvar;
-  };
+  int   solve_mass;
+  [[deprecated("renamed to solve_mass")]] \
+  int&   impvar() {return solve_mass;};
 
   /*  initialization temperature (in degree Celsius) for the particles already
       present in the calculation domain when an evolution equation on

src/turb/cs_turbulence_model.h
@@ -125,8 +125,7 @@ enum {
 
 typedef struct {
 
-  union {
-    int           model;/*! turbulence model
+  int           model;  /*! turbulence model
                           - CS_TURB_NONE: no turbulence model (laminar flow)
                           - CS_TURB_MIXING_LENGTH: mixing length model
                           - CS_TURB_K_EPSILON: standard k-epsilon model
@@ -148,10 +147,11 @@ typedef struct {
                           - CS_TURB_V2F_BL_V2K: v2f BL-v2-k
                           - CS_TURB_K_OMEGA: k-omega SST
                           - CS_TURB_SPALART_ALLMARAS: Spalart-Allmaras model */
-    int           iturb;  /*! Deprecated */
-  };
+
+  [[deprecated]] int& iturb() {return model;};
+
   int           itytur;       /* class of turbulence model (integer value
-                                 iturb/10) */
+                                 model/10) */
   int           hybrid_turb;  /*! Type of Hybrid Turbulence Model
                                    - CS_HYBRID_NONE:  No model
                                    - CS_HYBRID_DES:   Detached Eddy Simulation

commit d3105c1a11995868de92bcd2fd7628a8620bd1ad

Fix error introduced in commit 09d14fdfcf

src/atmo/cs_atmo.h
@@ -191,7 +191,7 @@ typedef struct {
 
   /*! Number of vertical levels */
   int met_1d_nlevels_max_t;
-  [[deprecated]] int& nbmaxt() {return met_1d_ntimes;};
+  [[deprecated]] int& nbmaxt() {return met_1d_nlevels_max_t;};
 
   /*! Domain orientation (angle in degree between y direction and north),
    * 0 by default */