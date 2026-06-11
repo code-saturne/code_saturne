code-saturne/code_saturne

commit ae6514db66429ac57eebf35f7ea9ab1afaeca1c3

[Internal coupling] Take radiative transfer contribution.
Add some arrays which take additional exchange coef and fluxes due to
radiative transfer.
For the moment it is to be done by the user (as one can want to impose a
radiative flux with no radiative transfer computation).
The adaptation to DOM and 1D atmo radiative module.

Done with B . Berlioux.

src/base/cs_field_default.cpp
@@ -49,6 +49,7 @@
 #include "base/cs_log.h"
 #include "base/cs_mem.h"
 #include "base/cs_post.h"
+#include "base/cs_thermal_model.h"
 #include "mesh/cs_mesh_location.h"
 #include "pprt/cs_physical_model.h"
 #include "turb/cs_turbulence_bc.h"
@@ -354,20 +355,22 @@ cs_field_map_and_init_bcs(void)
   const int k_turb_flux_model = cs_field_key_id("turbulent_flux_model");
   const int n_fields = cs_field_n_fields();
 
-  /* Some fields may require BC coefficients; use 4 flags per field:
+  /* Some fields may require BC coefficients; use 5 flags per field:
      - 0: have_flux_bc
      - 1: have_mom_bc
      - 2: have_conv_bc
-     - 3: have_exch_bc */
+     - 3: have_exch_bc
+     - 4: have_rad_bc */
 
   bool *bc_flags;
-  CS_MALLOC(bc_flags, n_fields*4, bool);
-  for (int i = 0; i < n_fields*4; i++)
+  int n_bc_flags = 5;
+  CS_MALLOC(bc_flags, n_fields * n_bc_flags, bool);
+  for (int i = 0; i < n_fields * n_bc_flags; i++)
     bc_flags[i] = false;
 
   if (cs_glob_physical_model_flag[CS_COMPRESSIBLE] >= 0) {
-    bc_flags[cs_field_by_name("velocity")->id * 4 + 2] = true;
-    bc_flags[cs_field_by_name("total_energy")->id * 4 + 2] = true;
+    bc_flags[cs_field_by_name("velocity")->id * n_bc_flags + 2] = true;
+    bc_flags[cs_field_by_name("total_energy")->id * n_bc_flags + 2] = true;
   }
 
   /* BC coeffs also used for some fields which are not directly
@@ -379,12 +382,15 @@ cs_field_map_and_init_bcs(void)
     const cs_field_t *f = cs_field_by_name_try(sp_names[i]);
     if (f != nullptr) {
       if (! (f->type & CS_FIELD_USER))
-        bc_flags[f->id * 4] = true;
+        bc_flags[f->id * n_bc_flags] = true;
     }
   }
 
   /* Loop over variables
      ------------------- */
+
+  cs_field_t *f_th = cs_thermal_model_field();
+  cs_equation_param_t *eqp_th = cs_field_get_equation_param(f_th);
 
   for (int f_id = 0; f_id < n_fields; f_id++) {
     cs_field_t  *f = cs_field_by_id(f_id);
@@ -405,14 +411,18 @@ cs_field_map_and_init_bcs(void)
         continue;
     }
 
-    bc_flags[f->id * 4] = true;
+    bc_flags[f->id * n_bc_flags] = true;
 
     /* Internal exchange coefficient are always needed */
-    bc_flags[f_id*4 + 3] = true;
+    bc_flags[f_id * n_bc_flags + 3] = true;
+
+    /* Radiative coefficient for thermal scalar when internal coupling */
+    if (f_id == f_th->id && eqp_th->icoupl > 0)
+      bc_flags[f_id * n_bc_flags + 3] = true;
 
     if (f->dim == 6) {
       if (strcmp(f->name, "rij") == 0)
-        bc_flags[f_id*4 + 1] = true;
+        bc_flags[f_id * n_bc_flags + 1] = true;
     }
 
     int turb_flux_model_type = f->get_key_int(k_turb_flux_model) / 10;
@@ -421,7 +431,7 @@ cs_field_map_and_init_bcs(void)
     if (turb_flux_model_type == 3) {
       cs_field_t  *f_ut = cs_field_by_composite_name_try(f->name,
                                                          "turbulent_flux");
-      bc_flags[f_ut->id*4 + 1] = true;
+      bc_flags[f_ut->id * n_bc_flags + 1] = true;
     }
   }
 
@@ -431,16 +441,21 @@ cs_field_map_and_init_bcs(void)
 
     /* have flux_bc always true if other coefficients are used */
 
-    if (bc_flags[f_id*4] == false)
+    if (bc_flags[f_id * n_bc_flags] == false)
       continue;
 
     cs_field_t  *f = cs_field_by_id(f_id);
 
-    bool has_mom_bc  = bc_flags[f_id*4 + 1];
-    bool has_conv_bc = bc_flags[f_id*4 + 2];
-    bool has_exch_bc = bc_flags[f_id*4 + 3];
+    bool has_mom_bc  = bc_flags[f_id * n_bc_flags + 1];
+    bool has_conv_bc = bc_flags[f_id * n_bc_flags + 2];
+    bool has_exch_bc = bc_flags[f_id * n_bc_flags + 3];
+    bool has_rad_bc  = bc_flags[f_id * n_bc_flags + 4];
 
-    cs_field_allocate_bc_coeffs(f, true, has_mom_bc, has_conv_bc, has_exch_bc);
+    cs_field_allocate_bc_coeffs(f, true,
+                                has_mom_bc,
+                                has_conv_bc,
+                                has_exch_bc,
+                                has_rad_bc);
     cs_field_init_bc_coeffs(f);
 
   }

src/base/cs_boundary_conditions_set_coeffs_turb.cpp
@@ -358,6 +358,8 @@ _cs_boundary_conditions_set_coeffs_turb_scalar(cs_field_t  *f_sc,
 
   cs_real_t *hint, *yptp;
   cs_real_t *h_int_tot = f_sc->bc_coeffs->h_int_tot;
+  cs_real_t *h_rad   = f_sc->bc_coeffs->h_rad;
+  cs_real_t *phi_rad = f_sc->bc_coeffs->flux_rad;
   CS_MALLOC(hint, n_b_faces, cs_real_t);
   CS_MALLOC(yptp, n_b_faces, cs_real_t);
 

src/base/cs_field.cpp
@@ -1358,6 +1358,8 @@ cs_field_bc_coeffs_init(cs_field_bc_coeffs_t  *bc_coeffs)
   bc_coeffs->rcodcl3 = nullptr;
 
   bc_coeffs->h_int_tot = nullptr;
+  bc_coeffs->h_rad = nullptr;
+  bc_coeffs->flux_rad = nullptr;
 
   bc_coeffs->val_f = nullptr;
   bc_coeffs->val_f_pre = nullptr;
@@ -1445,6 +1447,13 @@ cs_field_bc_coeffs_free_copy(const cs_field_bc_coeffs_t  *ref,
 
   if (copy->h_int_tot != ref->h_int_tot)
     CS_FREE(copy->h_int_tot);
+
+  if (copy->h_rad != ref->h_rad)
+    CS_FREE(copy->h_rad);
+
+  if (copy->flux_rad != ref->flux_rad)
+    CS_FREE(copy->flux_rad);
+
 }
 
 /*----------------------------------------------------------------------------*/
@@ -1464,6 +1473,9 @@ cs_field_bc_coeffs_clear(cs_field_bc_coeffs_t  *bc_coeffs)
   CS_FREE(bc_coeffs->rcodcl3);
 
   CS_FREE(bc_coeffs->h_int_tot);
+
+ CS_FREE(bc_coeffs->h_rad);
+ CS_FREE(bc_coeffs->flux_rad);
 
   CS_FREE(bc_coeffs->val_f);
   CS_FREE(bc_coeffs->val_f_pre);
@@ -1863,6 +1875,8 @@ cs_field_map_values(cs_field_t   *f,
  *                                are added
  * \param[in]       have_exch_bc  if true, exchange boundary coefficients
  *                               (h_int_tot and hext) are added
+ * \param[in]       have_rad_bc   if true, radiative boundary coefficients
+ *                               (h_rad and flux_rad) are added
  */
 /*----------------------------------------------------------------------------*/
 
@@ -1871,7 +1885,8 @@ cs_field_allocate_bc_coeffs(cs_field_t  *f,
                             bool         have_flux_bc,
                             bool         have_mom_bc,
                             bool         have_conv_bc,
-                            bool         have_exch_bc)
+                            bool         have_exch_bc,
+                            bool         have_rad_bc)
 {
   /* Add boundary condition coefficients if required */
 
@@ -1942,9 +1957,13 @@ cs_field_allocate_bc_coeffs(cs_field_t  *f,
 
       if (have_exch_bc) {
         CS_MALLOC_HD(f->bc_coeffs->h_int_tot, n_elts[0], cs_real_t, amode);
+        CS_MALLOC_HD(f->bc_coeffs->h_rad, n_elts[0], cs_real_t, amode);
+        CS_MALLOC_HD(f->bc_coeffs->flux_rad, n_elts[0], cs_real_t, amode);
       }
       else {
         f->bc_coeffs->h_int_tot = nullptr;
+        f->bc_coeffs->h_rad = nullptr;
+        f->bc_coeffs->flux_rad  = nullptr;
       }
 
     }
@@ -1991,6 +2010,15 @@ cs_field_allocate_bc_coeffs(cs_field_t  *f,
       }
       else {
         CS_FREE(f->bc_coeffs->h_int_tot);
+      }
+
+      if (have_rad_bc) {
+        CS_REALLOC_HD(f->bc_coeffs->h_rad, n_elts[0], cs_real_t, amode);
+        CS_REALLOC_HD(f->bc_coeffs->flux_rad, n_elts[0], cs_real_t, amode);
+      }
+      else {
+        CS_FREE(f->bc_coeffs->h_rad);
+        CS_FREE(f->bc_coeffs->flux_rad);
       }
 
     }
@@ -2141,6 +2169,18 @@ cs_field_init_bc_coeffs(cs_field_t  *f)
         h_int_tot[i] = 0.;
       });
     }
+    if (f->bc_coeffs->h_rad != nullptr) {
+      cs_real_t *h_rad = f->bc_coeffs->h_rad;
+      ctx.parallel_for(n_elts[0], [=] CS_F_HOST_DEVICE (cs_lnum_t i) {
+        h_rad[i] = 0.;
+      });
+    }
+    if (f->bc_coeffs->flux_rad != nullptr) {
+      cs_real_t *flux_rad = f->bc_coeffs->flux_rad;
+      ctx.parallel_for(n_elts[0], [=] CS_F_HOST_DEVICE (cs_lnum_t i) {
+        flux_rad[i] = 0.;
+      });
+    }
 
   }
 
@@ -2286,6 +2326,8 @@ cs_field_destroy_all(void)
       CS_FREE(f->bc_coeffs->ac);
       CS_FREE(f->bc_coeffs->bc);
       CS_FREE(f->bc_coeffs->h_int_tot);
+      CS_FREE(f->bc_coeffs->h_rad);
+      CS_FREE(f->bc_coeffs->flux_rad);
 
       CS_FREE(f->bc_coeffs->val_f);
       CS_FREE(f->bc_coeffs->flux_diff);

src/base/cs_field.h
@@ -118,6 +118,14 @@ public:
                                       (used for internal coupling and
                                       coupling) */
 
+  cs_real_t         *h_rad;       /* Exchange coef for radiative transfer
+                                     (used for internal coupling and
+                                      coupling) */
+
+  cs_real_t         *flux_rad;    /* Exchange coef for radiative transfer
+                                     (used for internal coupling and
+                                      coupling) */
+
   cs_real_t         *val_f;       /* face value for gradient */
   cs_real_t         *val_f_pre;   /* previous face value for gradient */
 
@@ -151,6 +159,9 @@ public:
     rcodcl1 = nullptr;
     rcodcl2 = nullptr;
     rcodcl3 = nullptr;
+
+    h_rad = nullptr;
+    flux_rad = nullptr;
 
     h_int_tot = nullptr;
 
@@ -842,6 +853,8 @@ cs_field_map_values(cs_field_t   *f,
  *   have_conv_bc <-- if true, convection BC coefficients (ac and bc) are added
  *   have_exch_bc <-- if true, exchange boundary coefficients (hint and hext)
  *                    are added
+ *   have_rad_bc  <-- if true, radiative boundary coefficients (h_rad and
+ *                    flux_rad) are added
  *----------------------------------------------------------------------------*/
 
 void
@@ -849,7 +862,8 @@ cs_field_allocate_bc_coeffs(cs_field_t  *f,
                             bool         have_flux_bc,
                             bool         have_mom_bc,
                             bool         have_conv_bc,
-                            bool         have_exch_bc);
+                            bool         have_exch_bc,
+                            bool         have_rad_bc);
 
 /*----------------------------------------------------------------------------*/
 /* Initialize boundary condition coefficients arrays.

commit f6c65e989bb0ddd710072b7424540796bdbd5711

Fix crash without thermal model introduced by commit ae6514db.

src/base/cs_field_default.cpp
@@ -390,7 +390,6 @@ cs_field_map_and_init_bcs(void)
      ------------------- */
 
   cs_field_t *f_th = cs_thermal_model_field();
-  cs_equation_param_t *eqp_th = cs_field_get_equation_param(f_th);
 
   for (int f_id = 0; f_id < n_fields; f_id++) {
     cs_field_t  *f = cs_field_by_id(f_id);
@@ -417,8 +416,11 @@ cs_field_map_and_init_bcs(void)
     bc_flags[f_id * n_bc_flags + 3] = true;
 
     /* Radiative coefficient for thermal scalar when internal coupling */
-    if (f_id == f_th->id && eqp_th->icoupl > 0)
-      bc_flags[f_id * n_bc_flags + 3] = true;
+    if (f == f_th) {
+      cs_equation_param_t *eqp_th = cs_field_get_equation_param(f_th);
+      if (eqp_th->icoupl > 0)
+        bc_flags[f_id * n_bc_flags + 3] = true;
+    }
 
     if (f->dim == 6) {
       if (strcmp(f->name, "rij") == 0)