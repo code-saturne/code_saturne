code-saturne/code_saturne

commit 09d14fdfcfee799e8ca09ac3d48ee21122cd71be

Fix compilation with some older compilers.

src/turb/cs_turbulence_model.h
@@ -125,8 +125,7 @@ enum {
 
 typedef struct {
 
-  union {
-    int           model;/*! turbulence model
+  int           model;  /*! turbulence model
                           - CS_TURB_NONE: no turbulence model (laminar flow)
                           - CS_TURB_MIXING_LENGTH: mixing length model
                           - CS_TURB_K_EPSILON: standard k-epsilon model
@@ -148,10 +147,11 @@ typedef struct {
                           - CS_TURB_V2F_BL_V2K: v2f BL-v2-k
                           - CS_TURB_K_OMEGA: k-omega SST
                           - CS_TURB_SPALART_ALLMARAS: Spalart-Allmaras model */
-    int           iturb;  /*! Deprecated */
-  };
+
+  [[deprecated]] int& iturb() {return model;};
+
   int           itytur;       /* class of turbulence model (integer value
-                                 iturb/10) */
+                                 model/10) */
   int           hybrid_turb;  /*! Type of Hybrid Turbulence Model
                                    - CS_HYBRID_NONE:  No model
                                    - CS_HYBRID_DES:   Detached Eddy Simulation

src/atmo/cs_atmo.h
@@ -178,26 +178,20 @@ typedef struct {
   cs_atmo_projection_t projection_type;
   int utm_zone; // If using utm, a zone needs to be specified
 
-  union {
-    int met_1d_nlevels_d;
-    [[deprecated]] int nbmetd;
-  };
+  int met_1d_nlevels_d;
+  [[deprecated]] int& nbmetd() {return met_1d_nlevels_d;};
+
   /*! numbers of altitudes for the temperature and specific humidity */
-  union {
-    int met_1d_nlevels_t;
-    [[deprecated]] int nbmett;
-  };
+  int met_1d_nlevels_t;
+  [[deprecated]] int& nbmett() {return met_1d_nlevels_t;};
+
   /*! numbers of time steps for the meteo profiles */
-  union {
-    int met_1d_ntimes;
-    [[deprecated]] int nbmetm;
-  };
+  int met_1d_ntimes;
+  [[deprecated]] int& nbmetm() {return met_1d_ntimes;};
 
   /*! Number of vertical levels */
-  union {
-    int met_1d_nlevels_max_t;
-    [[deprecated]] int nbmaxt;
-  };
+  int met_1d_nlevels_max_t;
+  [[deprecated]] int& nbmaxt() {return met_1d_ntimes;};
 
   /*! Domain orientation (angle in degree between y direction and north),
    * 0 by default */

src/lagr/cs_lagr.cpp
@@ -1165,7 +1165,7 @@ cs_lagr_map_specific_physics(void)
     bft_error(__FILE__, __LINE__, 0,
         "%s: Turbulence modelling is not set.", __func__);
 
-  _lagr_extra_module[0].iturb  = turb_model->iturb;
+  _lagr_extra_module[0].iturb  = turb_model->model;
   _lagr_extra_module[0].itytur = turb_model->itytur;
   _lagr_extra_module[0].icp    = cs_glob_fluid_properties->icp;
 

src/lagr/cs_lagr.h
@@ -442,26 +442,23 @@ typedef struct {
       Useful if \ref physical_model=1 and if there is a thermal scalar
       associated with the continuous phase.
   */
-  union {
-    int   solve_temperature;
-    [[deprecated]] int   itpvar;
-  };
+  int   solve_temperature;
+  [[deprecated("renamed to solve_temperature")]] \
+  int&  itpvar() {return solve_temperature;};
 
   /*  activation (=1) or not (=0) of an evolution equation on the particle
       diameter. Useful if \ref physical_model = 1.
   */
-  union {
-    int   solve_diameter;
-    [[deprecated]] int   idpvar;
-  };
+  int   solve_diameter;
+  [[deprecated("renamed to solve_diameter")]] \
+  int&   idpvar() {return solve_diameter;};
 
   /*  activation (=1) or not (=0) of an evolution equation on the particle mass
       Useful if \ref physical_model = 1
   */
-  union {
-    int   solve_mass;
-    [[deprecated]] int   impvar;
-  };
+  int   solve_mass;
+  [[deprecated("renamed to solve_mass")]] \
+  int&   impvar() {return solve_mass;};
 
   /*  initialization temperature (in degree Celsius) for the particles already
       present in the calculation domain when an evolution equation on

commit f0d630f89b3a919f2e941afee9ba4b2c28637680

Commit 09d14fdf broke some deprecated user code, so try something cleaner.
Replacing a union value with a method seems OK for rvalues, not lvalues.
Also, using a constructure for the model could fix this, but since a
single model instance is built, exhibiting this in the header is a bit
overkill. So we simply try to check the C++ version, given that compilers
which required the nested initializer were all C++14, whike gcc 16 does
not accept this syntax anymore when in default C++ 20 mode (C++17 is OK).

src/turb/cs_turbulence_model.cpp
@@ -349,7 +349,11 @@
 
 static cs_turb_model_t  _turb_model =
 {
+#if __cplusplus > 201402L
   .model  = -999,
+#else
+  {.model = -999},
+#endif
   .itytur = -999,
   .hybrid_turb = 0,
   .type = -1,

src/turb/cs_turbulence_model.h
@@ -125,7 +125,8 @@ enum {
 
 typedef struct {
 
-  int           model;  /*! turbulence model
+  union {
+    int           model;/*! turbulence model
                           - CS_TURB_NONE: no turbulence model (laminar flow)
                           - CS_TURB_MIXING_LENGTH: mixing length model
                           - CS_TURB_K_EPSILON: standard k-epsilon model
@@ -147,9 +148,8 @@ typedef struct {
                           - CS_TURB_V2F_BL_V2K: v2f BL-v2-k
                           - CS_TURB_K_OMEGA: k-omega SST
                           - CS_TURB_SPALART_ALLMARAS: Spalart-Allmaras model */
-
-  [[deprecated]] int& iturb() {return model;};
-
+    [[deprecated]] int iturb;
+  };
   int           itytur;       /* class of turbulence model (integer value
                                  model/10) */
   int           hybrid_turb;  /*! Type of Hybrid Turbulence Model