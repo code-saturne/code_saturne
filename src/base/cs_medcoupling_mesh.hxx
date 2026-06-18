a50942f67f2125d75e3452506d42e9e6ca4c40ec
Chaï Koren <chai.koren@noreply.edf.fr>
2026-06-18 09:17:32 +0200

    Continue fine tuning medcoupling warnings suppression

--- src/base/cs_medcoupling_mesh.hxx
+++ src/base/cs_medcoupling_mesh.hxx
@@ -49,6 +49,7 @@
 // Disable warnings introduced by including this header file, which
 // result from usage of deprecated objects in MEDCouplingUMesh
 DISABLE_WARNING_PUSH
+DISABLE_WARNING(-Wdeprecated-declarations)
 // Disable this warning on LLVM based compilers
 #if defined(__llvm__)
 DISABLE_WARNING(-Winconsistent-missing-override)
