code-saturne/code_saturne

commit edd43985274df8602f8fa40658dcd06a57531541

Fix in coupled version of DRSM turbulence models (irijco=1) (not all implicit terms was taken into account in the momentum equation).
git-svn-id: https://svn.isais.edf.fr/mfee/saturne/trunk@10431 e44c5d27-f830-0410-8ce8-974f7a764be6

src/base/cs_math.h
@@ -290,6 +290,72 @@ cs_math_sym_33_3_product(const cs_real_t  m[6],
 
 /*----------------------------------------------------------------------------*/
 /*!
+ * \brief Compute the product of a symmetric matrix of 3x3 real values by
+ * a vector of 3 real values and add it to the vector.
+ * NB: Symmetric matrix are stored as follows (s11, s22, s33, s12, s23, s13)
+ *
+ * \param[in]     m             matrix of 3x3 real values
+ * \param[in]     v             vector of 3 real values
+ * \param[out]    mv            vector of 3 real values
+ */
+/*----------------------------------------------------------------------------*/
+
+static inline void
+cs_math_sym_33_3_product_add(const cs_real_t  m[6],
+                             const cs_real_t  v[3],
+                             cs_real_t        mv[restrict 3])
+{
+  mv[0] += m[0] * v[0] + m[3] * v[1] + m[5] * v[2];
+  mv[1] += m[3] * v[0] + m[1] * v[1] + m[4] * v[2];
+  mv[2] += m[5] * v[0] + m[4] * v[1] + m[2] * v[2];
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Compute the product of a matrix of 6x6 real values by
+ * a vector of 6 real values.
+ *
+ * \param[in]     m             matrix of 6x6 real values
+ * \param[in]     v             vector of 6 real values
+ * \param[out]    mv            vector of 6 real values
+ */
+/*----------------------------------------------------------------------------*/
+
+static inline void
+cs_math_66_6_product(const cs_real_t  m[6][6],
+                     const cs_real_t  v[6],
+                     cs_real_t        mv[restrict 6])
+{
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 6; j++)
+      mv[i] = m[i][j] * v[j];
+  }
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Compute the product of a matrix of 6x6 real values by
+ * a vector of 6 real values and add it to the vector.
+ *
+ * \param[in]     m             matrix of 6x6 real values
+ * \param[in]     v             vector of 6 real values
+ * \param[out]    mv            vector of 6 real values
+ */
+/*----------------------------------------------------------------------------*/
+
+static inline void
+cs_math_66_6_product_add(const cs_real_t  m[6][6],
+                         const cs_real_t  v[6],
+                         cs_real_t        mv[restrict 6])
+{
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 6; j++)
+      mv[i] += m[i][j] * v[j];
+  }
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
  * \brief  Compute the determinant of a 3x3 matrix
  *
  * \param[in]  m    3x3 matrix

src/alge/cs_divergence.h
@@ -143,7 +143,7 @@ void CS_PROCF (projtv, PROJTV)
 );
 
 /*----------------------------------------------------------------------------
- * Wrapper to cs_tensor_flux
+ * Wrapper to cs_tensor_face_flux
  *----------------------------------------------------------------------------*/
 
 void CS_PROCF (divrij, DIVRIJ)
@@ -393,17 +393,15 @@ cs_ext_force_anisotropic_flux(const cs_mesh_t          *m,
 /*!
  * \brief Add \f$ \rho \tens{r} \vect{s}_\ij\f$ to a flux.
  *
- * This is done by computing the flux associated to each line of the tensor.
- *
  * \param[in]     m             pointer to mesh
  * \param[in]     fvq           pointer to finite volume quantities
- * \param[in]     f_id          field ids (or -1)
+ * \param[in]     f_id          field id (or -1)
  * \param[in]     itypfl        indicator (take rho into account or not)
  *                               - 1 compute \f$ \rho\vect{u}\cdot\vect{s} \f$
  *                               - 0 compute \f$ \vect{u}\cdot\vect{s} \f$
  * \param[in]     iflmb0        the mass flux is set to 0 on walls and
  *                               symmetries if = 1
- * \param[in]     init          the flux is initialized to 0 if > 0
+ * \param[in]     init          the mass flux is initialized to 0 if > 0
  * \param[in]     inc           indicator
  *                               - 0 solve an increment
  *                               - 1 otherwise
@@ -421,41 +419,39 @@ cs_ext_force_anisotropic_flux(const cs_mesh_t          *m,
  *                               reconstruction
  * \param[in]     climgu        clipping coefficient for the computation of
  *                               the gradient
- * \param[in]     rom           cell density
- * \param[in]     romb          density at boundary faces
- * \param[in]     symten        symmetric tensor variable
- * \param[in]     cofast        boundary condition array for the variable
+ * \param[in]     c_rho         cell density
+ * \param[in]     b_rho         density at boundary faces
+ * \param[in]     c_var         variable
+ * \param[in]     coefav        boundary condition array for the variable
  *                               (explicit part - symmetric tensor array)
- * \param[in]     cofbst        boundary condition array for the variable
+ * \param[in]     coefbv        boundary condition array for the variable
  *                               (implicit part - 6x6 symmetric tensor array)
- * \param[in,out] i_massflux    mass flux vector at interior faces
-                                 \f$ \dot{m}_\fij \f$
- * \param[in,out] b_massflux    mass flux vector at boundary faces
-                                 \f$ \dot{m}_\fib \f$
+ * \param[in,out] i_massflux    mass flux at interior faces \f$ \dot{m}_\fij \f$
+ * \param[in,out] b_massflux    mass flux at boundary faces \f$ \dot{m}_\fib \f$
  */
 /*----------------------------------------------------------------------------*/
 
 void
-cs_tensor_flux(const cs_mesh_t             *m,
-               cs_mesh_quantities_t        *fvq,
-               int                          f_id,
-               int                          itypfl,
-               int                          iflmb0,
-               int                          init,
-               int                          inc,
-               int                          imrgra,
-               int                          nswrgu,
-               int                          imligu,
-               int                          iwarnu,
-               double                       epsrgu,
-               double                       climgu,
-               const cs_real_t              rom[],
-               const cs_real_t              romb[],
-               const cs_real_6_t            tens[],
-               const cs_real_6_t            coefav[],
-               const cs_real_66_t           coefbv[],
-               cs_real_3_t        *restrict i_massflux,
-               cs_real_3_t        *restrict b_massflux);
+cs_tensor_face_flux(const cs_mesh_t          *m,
+                    cs_mesh_quantities_t     *fvq,
+                    int                       f_id,
+                    int                       itypfl,
+                    int                       iflmb0,
+                    int                       init,
+                    int                       inc,
+                    int                       imrgra,
+                    int                       nswrgu,
+                    int                       imligu,
+                    int                       iwarnu,
+                    double                    epsrgu,
+                    double                    climgu,
+                    const cs_real_t           c_rho[],
+                    const cs_real_t           b_rho[],
+                    const cs_real_6_t         c_var[],
+                    const cs_real_6_t         coefav[],
+                    const cs_real_66_t        coefbv[],
+                    cs_real_3_t     *restrict i_massflux,
+                    cs_real_3_t     *restrict b_massflux);
 
 /*----------------------------------------------------------------------------*/
 

src/base/clptrg.f90
@@ -1045,7 +1045,7 @@ do ifac = 1, nfabor
           coefaf_rij(isou, ifac)      = -hint * coefa_rij(isou, ifac)
           coefad_rij(isou, ifac)      = 0.d0
           do ii = 1, 6
-            coefb_rij(isou,ii, ifac)  = alpha(isou,ii)
+            coefb_rij(isou,ii, ifac)  = alpha(ii,isou)
             if (ii.eq.isou) then
               coefbf_rij(isou,ii, ifac) = hint &
                                         * (1.d0 - coefb_rij(isou,ii, ifac))

src/base/clptur.f90
@@ -1104,7 +1104,7 @@ do ifac = 1, nfabor
             coefaf_rij(isou, ifac)      = -hint * coefa_rij(isou, ifac)
             coefad_rij(isou, ifac)      = 0.d0
             do ii = 1, 6
-              coefb_rij(isou,ii, ifac)  = alpha(isou,ii)
+              coefb_rij(isou,ii, ifac)  = alpha(ii, isou)
               if (ii.eq.isou) then
                 coefbf_rij(isou,ii, ifac) = hint * (1.d0 - coefb_rij(isou,ii, ifac))
               else

src/base/clsyvt.f90
@@ -554,7 +554,7 @@ do ifac = 1, nfabor
           coefaf_rij(isou, ifac) = 0.d0
           coefad_rij(isou, ifac) = 0.d0
           do ii = 1, 6
-            coefb_rij(isou,ii, ifac)  = alpha(isou,ii)
+            coefb_rij(isou,ii, ifac)  = alpha(ii, isou)
             if (ii.eq.isou) then
               coefbf_rij(isou,ii, ifac) = hint * (1.d0 - coefb_rij(isou,ii, ifac))
             else

commit 57ee770fa8120298188d20ff96c8541573e733eb

Fix bug in 6x6 matrix vector product introduced in edd43985 (2017-08-07).
Fortunately this function is not used.

src/base/cs_math.h
@@ -1365,8 +1365,9 @@ cs_math_66_6_product(const cs_real_t       m[6][6],
                      cs_real_t  *restrict  mv)
 {
   for (int i = 0; i < 6; i++) {
+    mv[i] = 0.0;
     for (int j = 0; j < 6; j++)
-      mv[i] = m[i][j] * v[j];
+      mv[i] += m[i][j] * v[j];
   }
 }
 