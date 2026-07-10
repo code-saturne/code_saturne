code-saturne/code_saturne

commit 73e895c30260dd81ffef22165523a144bb933c69

Minor style/consistency improvements.

src/alge/cs_multigrid_smoother.cpp
@@ -1132,7 +1132,7 @@ _block_3_jacobi(cs_sles_it_t              *c,
     if (aux_vectors == nullptr || aux_size/sizeof(cs_real_t) < (wa_size * n_wa))
       CS_MALLOC(_aux_vectors, wa_size * n_wa, cs_real_t);
     else
-      _aux_vectors = (cs_real_t *)aux_vectors;
+      _aux_vectors = static_cast<cs_real_t *>(aux_vectors);
 
     rk  = _aux_vectors;
     vxx = _aux_vectors + wa_size;
@@ -1168,15 +1168,15 @@ _block_3_jacobi(cs_sles_it_t              *c,
 
   for (n_iter = iter_ini; n_iter < convergence->n_iterations_max; n_iter++) {
 
-    memcpy(rk, vx, n_rows * sizeof(cs_real_t));  /* rk <- vx */
-
     /* Compute vxx <- vx - (a-diag).rk */
 
-    cs_matrix_vector_multiply_partial(a, CS_MATRIX_SPMV_E, rk, vxx);
+    cs_matrix_vector_multiply_partial(a, CS_MATRIX_SPMV_E, vx, vxx);
 
     /* Compute vx <- diag^-1 . (vxx - rhs) */
 #   pragma omp parallel for if(n_blocks > CS_THR_MIN)
     for (cs_lnum_t ii = 0; ii < n_blocks; ii++) {
+      for (cs_lnum_t kk = 0; kk < 3; kk++)
+        rk[ii*3 + kk] = vx[ii*3 + kk];
       _mat_c_m_b_33(ad_inv + 9*ii,
                     vx + 3*ii,
                     vxx + 3*ii,
@@ -1250,7 +1250,7 @@ _block_jacobi(cs_sles_it_t              *c,
     if (aux_vectors == nullptr || aux_size/sizeof(cs_real_t) < (wa_size * n_wa))
       CS_MALLOC(_aux_vectors, wa_size * n_wa, cs_real_t);
     else
-      _aux_vectors = (cs_real_t *)aux_vectors;
+      _aux_vectors = static_cast<cs_real_t *>(aux_vectors);
 
     rk  = _aux_vectors;
     vxx = _aux_vectors + wa_size;
@@ -1267,15 +1267,18 @@ _block_jacobi(cs_sles_it_t              *c,
     /* Compute vx <- diag^-1 . (vxx - rhs) */
 #   pragma omp parallel for if(n_blocks > CS_THR_MIN)
     for (cs_lnum_t ii = 0; ii < n_blocks; ii++) {
+      assert(db_size <= DB_SIZE_MAX);
+      cs_real_t _vxx[DB_SIZE_MAX];
+
       for (cs_lnum_t jj = 0; jj < db_size; jj++) {
         rk[db_size*ii + jj] = 0;
-        vxx[db_size*ii + jj] = 0;
+        _vxx[jj] = 0;
       }
 
       _mat_c_m_b(ad_inv + db_size_2*ii,
                  db_size,
                  vx + db_size*ii,
-                 vxx + db_size*ii,
+                 _vxx,
                  rhs + db_size*ii);
     }
 
@@ -1287,14 +1290,14 @@ _block_jacobi(cs_sles_it_t              *c,
 
   for (n_iter = iter_ini; n_iter < convergence->n_iterations_max; n_iter++) {
 
-    memcpy(rk, vx, n_rows * sizeof(cs_real_t));  /* rk <- vx */
-
     /* Compute Vx <- Vx - (A-diag).Rk */
 
-    cs_matrix_vector_multiply_partial(a, CS_MATRIX_SPMV_E, rk, vxx);
+    cs_matrix_vector_multiply_partial(a, CS_MATRIX_SPMV_E, vx, vxx);
 
 #   pragma omp parallel for if(n_blocks > CS_THR_MIN)
     for (cs_lnum_t ii = 0; ii < n_blocks; ii++) {
+      for (cs_lnum_t kk = 0; kk < 3; kk++)
+        rk[ii*db_size + kk] = vx[ii*db_size + kk];
       _mat_c_m_b(ad_inv + db_size_2*ii,
                  db_size,
                  vx + db_size*ii,
@@ -1668,8 +1671,8 @@ _p_sym_gauss_seidel_msr(cs_sles_it_t              *c,
 
         cs_real_t vx0[DB_SIZE_MAX], _vx[DB_SIZE_MAX];
 
-        for (cs_lnum_t kk = 0; kk < diag_block_size; kk++)
-          vx0[kk] = rhs[ii*db_size + kk];
+        for (cs_lnum_t jj = 0; jj < diag_block_size; jj++)
+          vx0[jj] = rhs[ii*db_size + jj];
 
         for (cs_lnum_t jj = 0; jj < n_cols; jj++) {
           for (cs_lnum_t kk = 0; kk < diag_block_size; kk++)
@@ -1678,12 +1681,14 @@ _p_sym_gauss_seidel_msr(cs_sles_it_t              *c,
 
         for (cs_lnum_t jj = 0; jj < db_size; jj++) {
           _vx[jj] = 0;
-          for (cs_lnum_t kk = 0; kk < db_size; kk++)
+          for (cs_lnum_t kk = 0; kk < db_size; kk++) {
             _vx[jj] += _ad_inv[jj*db_size + kk] * vx0[kk];
+            assert(fabs(_ad_inv[jj*db_size + kk] * vx0[kk]) < 1e30);
+          }
         }
 
-        for (cs_lnum_t kk = 0; kk < diag_block_size; kk++)
-          vx[ii*db_size + kk] = _vx[kk];
+        for (cs_lnum_t jj = 0; jj < diag_block_size; jj++)
+          vx[ii*db_size + jj] = _vx[jj];
 
       }
 
@@ -1730,8 +1735,8 @@ _p_sym_gauss_seidel_msr(cs_sles_it_t              *c,
 
         cs_real_t vx0[DB_SIZE_MAX], _vx[DB_SIZE_MAX];
 
-        for (cs_lnum_t kk = 0; kk < db_size; kk++)
-          vx0[kk] = rhs[ii*db_size + kk];
+        for (cs_lnum_t jj = 0; jj < db_size; jj++)
+          vx0[jj] = rhs[ii*db_size + jj];
 
         for (cs_lnum_t jj = 0; jj < n_cols; jj++) {
           for (cs_lnum_t kk = 0; kk < db_size; kk++)
@@ -1740,12 +1745,16 @@ _p_sym_gauss_seidel_msr(cs_sles_it_t              *c,
 
         for (cs_lnum_t jj = 0; jj < db_size; jj++) {
           _vx[jj] = 0;
-          for (cs_lnum_t kk = 0; kk < db_size; kk++)
+          for (cs_lnum_t kk = 0; kk < db_size; kk++) {
             _vx[jj] += _ad_inv[jj*db_size + kk] * vx0[kk];
+            assert(fabs(_ad_inv[jj*db_size + kk] * vx0[kk]) < 1e30);
+          }
         }
 
-        for (cs_lnum_t kk = 0; kk < db_size; kk++)
-          vx[ii*db_size + kk] = _vx[kk];
+        for (cs_lnum_t jj = 0; jj < db_size; jj++) {
+          vx[ii*db_size + jj] = _vx[jj];
+          assert(fabs(vx[ii*db_size + jj]) < 1e30);
+        }
 
       }
 
@@ -1854,8 +1863,8 @@ _ts_f_gauss_seidel_msr(cs_sles_it_t                *c,
 
       cs_real_t vx0[DB_SIZE_MAX], _vx[DB_SIZE_MAX];
 
-      for (cs_lnum_t kk = 0; kk < db_size; kk++) {
-        vx0[kk] = rhs[ii*db_size + kk];
+      for (cs_lnum_t jj = 0; jj < db_size; jj++) {
+        vx0[jj] = rhs[ii*db_size + jj];
       }
 
       for (cs_lnum_t jj = 0; jj < n_cols; jj++) {
@@ -1870,8 +1879,8 @@ _ts_f_gauss_seidel_msr(cs_sles_it_t                *c,
           _vx[jj] += _ad_inv[jj*db_size + kk] * vx0[kk];
       }
 
-      for (cs_lnum_t kk = 0; kk < db_size; kk++) {
-        vx[ii*db_size + kk] = _vx[kk];
+      for (cs_lnum_t jj = 0; jj < db_size; jj++) {
+        vx[ii*db_size + jj] = _vx[jj];
       }
 
     }
@@ -1979,8 +1988,8 @@ _ts_b_gauss_seidel_msr(cs_sles_it_t              *c,
 
       cs_real_t vx0[DB_SIZE_MAX], _vx[DB_SIZE_MAX];
 
-      for (cs_lnum_t kk = 0; kk < db_size; kk++) {
-        vx0[kk] = rhs[ii*db_size + kk];
+      for (cs_lnum_t jj = 0; jj < db_size; jj++) {
+        vx0[jj] = rhs[ii*db_size + jj];
       }
 
       for (cs_lnum_t jj = n_cols-1; jj > -1; jj--) {
@@ -1995,8 +2004,8 @@ _ts_b_gauss_seidel_msr(cs_sles_it_t              *c,
           _vx[jj] += _ad_inv[jj*db_size + kk] * vx0[kk];
       }
 
-      for (cs_lnum_t kk = 0; kk < db_size; kk++) {
-        vx[ii*db_size + kk] = _vx[kk];
+      for (cs_lnum_t jj = 0; jj < db_size; jj++) {
+        vx[ii*db_size + jj] = _vx[jj];
       }
 
     }

src/alge/cs_sles_it.cpp
@@ -3843,8 +3843,8 @@ _p_sym_gauss_seidel_msr(cs_sles_it_t              *c,
 
         cs_real_t vx0[DB_SIZE_MAX], _vx[DB_SIZE_MAX];
 
-        for (cs_lnum_t kk = 0; kk < diag_block_size; kk++)
-          vx0[kk] = rhs[ii*db_size + kk];
+        for (cs_lnum_t jj = 0; jj < diag_block_size; jj++)
+          vx0[jj] = rhs[ii*db_size + jj];
 
         for (cs_lnum_t jj = 0; jj < n_cols; jj++) {
           for (cs_lnum_t kk = 0; kk < diag_block_size; kk++)
@@ -3857,8 +3857,8 @@ _p_sym_gauss_seidel_msr(cs_sles_it_t              *c,
             _vx[jj] += _ad_inv[jj*db_size + kk] * vx0[kk];
         }
 
-        for (cs_lnum_t kk = 0; kk < diag_block_size; kk++)
-          vx[ii*db_size + kk] = _vx[kk];
+        for (cs_lnum_t jj = 0; jj < diag_block_size; jj++)
+          vx[ii*db_size + jj] = _vx[jj];
 
       }
 
@@ -3911,9 +3911,9 @@ _p_sym_gauss_seidel_msr(cs_sles_it_t              *c,
 
         cs_real_t vx0[DB_SIZE_MAX], vxm1[DB_SIZE_MAX], _vx[DB_SIZE_MAX];
 
-        for (cs_lnum_t kk = 0; kk < db_size; kk++) {
-          vxm1[kk] = vx[ii*db_size + kk];
-          vx0[kk] = rhs[ii*db_size + kk];
+        for (cs_lnum_t jj = 0; jj < db_size; jj++) {
+          vxm1[jj] = vx[ii*db_size + jj];
+          vx0[jj] = rhs[ii*db_size + jj];
         }
 
         for (cs_lnum_t jj = 0; jj < n_cols; jj++) {
@@ -3935,6 +3935,7 @@ _p_sym_gauss_seidel_msr(cs_sles_it_t              *c,
                  * (_vx[kk] - vxm1[kk]);
           rr += (r*r);
           vx[ii*db_size + jj] = _vx[jj];
+          assert(fabs(vx[ii*db_size + jj]) < 1e30);
         }
         res2 += rr;
 

commit 2096e275f78959dba12cd069a24fc54f581677f9

[Alge] fix transparent bug introduced in 73e895c3026 (2025-07-07).
rk was computed with a dimension of 3 whereas it should be db_size.
By the way rk is not used here because the residual is not computed for
the smoother.

src/alge/cs_multigrid_smoother.cpp
@@ -515,7 +515,7 @@ _block_jacobi(cs_sles_it_t              *c,
               void                      *aux_vectors)
 {
   cs_real_t *_aux_vectors;
-  cs_real_t  *restrict rk, *restrict vxx;
+  cs_real_t *restrict vxx;
 
   unsigned n_iter = 0;
 
@@ -532,7 +532,7 @@ _block_jacobi(cs_sles_it_t              *c,
 
   {
     const cs_lnum_t n_cols = cs_matrix_get_n_columns(a) * diag_block_size;
-    const size_t n_wa = 2;
+    const size_t n_wa = 1;
     const size_t wa_size = CS_SIMD_SIZE(n_cols);
 
     if (aux_vectors == nullptr || aux_size/sizeof(cs_real_t) < (wa_size * n_wa))
@@ -540,8 +540,7 @@ _block_jacobi(cs_sles_it_t              *c,
     else
       _aux_vectors = static_cast<cs_real_t *>(aux_vectors);
 
-    rk  = _aux_vectors;
-    vxx = _aux_vectors + wa_size;
+    vxx = _aux_vectors;
   }
 
   int iter_ini = 0;
@@ -559,7 +558,6 @@ _block_jacobi(cs_sles_it_t              *c,
       cs_real_t _vxx[DB_SIZE_MAX];
 
       for (cs_lnum_t jj = 0; jj < db_size; jj++) {
-        rk[db_size*ii + jj] = 0;
         _vxx[jj] = 0;
       }
 
@@ -578,14 +576,13 @@ _block_jacobi(cs_sles_it_t              *c,
 
   for (n_iter = iter_ini; n_iter < convergence->n_iterations_max; n_iter++) {
 
-    /* Compute Vx <- Vx - (A-diag).Rk */
+    /* Compute vxx <- (A-diag).vx */
 
     cs_matrix_vector_multiply_partial(a, CS_MATRIX_SPMV_E, vx, vxx);
 
+    /* Compute Vx <- diag^-1 (RHS - Vxx) */
 #   pragma omp parallel for if(n_blocks > CS_THR_MIN)
     for (cs_lnum_t ii = 0; ii < n_blocks; ii++) {
-      for (cs_lnum_t kk = 0; kk < 3; kk++)
-        rk[ii*db_size + kk] = vx[ii*db_size + kk];
       _mat_c_m_b(ad_inv + db_size_2*ii,
                  db_size,
                  vx + db_size*ii,