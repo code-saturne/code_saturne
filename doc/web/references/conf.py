88dce55a02b0e1dd921831879ce552d10b39d8eb
Yvan Fournier <yvan.fournier@noreply.edf.fr>
2026-07-10 18:15:41 +0200

    Update references/citations.

--- doc/web/references/conf.py
+++ doc/web/references/conf.py
@@ -18,11 +18,11 @@
 # -- Project information -----------------------------------------------------
 
 project = 'code_saturne references'
-copyright = '2021, EDF S.A.'
+copyright = '2026, EDF S.A.'
 author = 'code_saturne dev. team'
 
 # The full version, including alpha/beta/rc tags
-release = '2021'
+release = '2026'
 
 
 # -- General configuration ---------------------------------------------------
@@ -48,12 +48,17 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'sphinxdoc'
+html_theme = 'haiku'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_sidebars = {
+   '**': ['globaltoc.html', 'sourcelink.html'],
+   'using/windows': ['windows-sidebar.html'],
+}
 
 # --bibliography --
 
