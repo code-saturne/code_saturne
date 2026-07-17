code-saturne/code_saturne

commit 328efe3169d0173db4d04fdec5dff00ca37078f6

Workaround AOCC compiler crash on some __builtin_FILE()/LINE() constructs.
This occurs at least with AOCC_5.0.0-Build#1377 2024_09_24 build with
Clang 17, though vanilla Clang 17 seems to do fine.

src/base/cs_array.h
@@ -1157,9 +1157,9 @@ public:
   operator new
   (
     std::size_t count,   /*!<[in] Size to allocate */
-#if (defined(__GNUC__) || defined(__clang__)) && \
-  __has_builtin(__builtin_LINE) && \
- __has_builtin(__builtin_FILE)
+#if (defined(__GNUC__) || defined(__clang__)) \
+    && !defined(CS_AOCC_BUILTIN_WORKAROUND) \
+    && __has_builtin(__builtin_LINE) && __has_builtin(__builtin_FILE)
     const char *file_name   = __builtin_FILE(), /*!<[in] Caller file (for log) */
     const int   line_number = __builtin_LINE()  /*!<[in] Caller line (for log) */
 #else

commit 76a73b278d15050a0c9cec24c1d25ee9aa81b3fc

Improve AOCC compiler crash workaround introduced in commit 328efe3169
  * Add a configure test to check for the compiler crash. If
    encountered, define a "HAVE_AOCC_BUILTIN_WORKAROUND" variable.
  * Test is done for all compilers, even if not AOCC, thus ensuring that
    a future such crash will be handled correctly

configure.ac
@@ -119,6 +119,66 @@ user_CS_LD=$CS_LD
 AC_PROG_CC
 AC_PROG_CXX
 
+# Check for AOCC workaround?
+AC_LANG_PUSH(C++)
+#AC_COMPILE_IFELSE([AC_LANG_PROGRAM(
+AC_COMPILE_IFELSE([AC_LANG_SOURCE(
+[[#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+namespace cs {
+template <class T>
+class array {
+
+public:
+
+void *
+operator new
+(
+  std::size_t count,
+  const char *file_name   = __builtin_FILE(),
+  const int   line_number = __builtin_LINE()
+)
+{
+  return ::operator new(count);
+}
+
+array() = default;
+~array() = default;
+
+private:
+  T* _data{nullptr};
+
+};
+}
+
+int
+main
+(
+ int argc,
+ char *argv[]
+)
+{
+  cs::array<int> *ptr = new cs::array<int>();
+
+  delete ptr;
+
+  return 0;
+}
+]]
+)],
+[ AC_MSG_RESULT([AOCC Workaround not needed])
+],
+[ AC_DEFINE([HAVE_AOCC_BUILTIN_WORKAROUND], 1, [AOCC Workaround needed])
+ cs_need_amdclang_workaround=yes
+])
+AC_SUBST(cs_need_amdclang_workaround)
+AC_SUBST(HAVE_AOCC_BUILTIN_WORKAROUND)
+AC_LANG_POP(C++)
+
+
 # For Fortran check if it is disabled first
 AC_ARG_ENABLE(fortran,
   [AS_HELP_STRING([--disable-fortran], [disable optional Fortran compilation])],

src/base/cs_array.h
@@ -1136,6 +1136,7 @@ public:
     std::size_t count,   /*!<[in] Size to allocate */
 #if (defined(__GNUC__) || defined(__clang__)) \
     && !defined(CS_AOCC_BUILTIN_WORKAROUND) \
+    && !defined(HAVE_AOCC_BUILTIN_WORKAROUND) \
     && __has_builtin(__builtin_LINE) && __has_builtin(__builtin_FILE)
     const char *file_name   = __builtin_FILE(), /*!<[in] Caller file (for log) */
     const int   line_number = __builtin_LINE()  /*!<[in] Caller line (for log) */