code-saturne/code_saturne

commit e513498ce8235c0fba7943a7168c4c17bb8eedd2

[Atmo] major commit in 1D radiative transfert.
Now the grid given is the one of levels (corresponding to 3D faces) and
slices are deduced (staguered grid).
A lot of changes in solar radiation as well as infrared radiation (fuse
slear sky and cloudy sky, make cloud detection more robust, recompute
CO2 absorption each time step, use AOD for IR directly...)

Done in collaboration with Luc MUSSON GENON (CEREA).

src/base/cs_measures_util.cpp
@@ -133,6 +133,8 @@ static int  _n_grids_max = 0;
 static cs_interpol_grid_t  *_grids = nullptr;
 static cs_map_name_to_id_t  *_grids_map = nullptr;
 
+static bool _warnings = true;
+
 /*============================================================================
  * Local Type Definitions
  *============================================================================*/
@@ -294,6 +296,8 @@ cs_interpol_field_on_grid(cs_interpol_grid_t *ig,
 
   const cs_real_3_t *g_coords = (cs_real_3_t *)(ig->coords);
 
+  bool continue_warnings = true;
+
   /* Loop over cells */
 # pragma omp parallel for
   for (cs_lnum_t g_id = 0; g_id < nb_points; g_id++) {
@@ -303,14 +307,8 @@ cs_interpol_field_on_grid(cs_interpol_grid_t *ig,
     /* get z_min and z_max of the level,
      * Note, first and last levels are truncated */
     cs_real_t z_min, z_max;
-    if (g_id == 0)
-      z_min = g_coords[g_id][2];
-    else
-      z_min = 0.5 * (g_coords[g_id-1][2] + g_coords[g_id][2]);
-    if (g_id == (nb_points-1))
-      z_max = g_coords[g_id][2];
-    else
-      z_max = 0.5 * (g_coords[g_id][2] + g_coords[g_id+1][2]);
+    z_min = g_coords[g_id][2];
+    z_max = g_coords[g_id+1][2];
 
     /* Loop over cells */
     for (cs_lnum_t c_id = 0; c_id < n_elts; c_id++) {
@@ -326,13 +324,13 @@ cs_interpol_field_on_grid(cs_interpol_grid_t *ig,
 
     if (total_vol > 0.)
       interpolated_values[g_id] /= total_vol;
-#if 0
-    else {
-      bft_printf("Warning: The grid level z[%d]=%f in [%f, %f] has no corresponding cells.\n",
+    else if (_warnings) {
+      continue_warnings = false;
+      bft_printf("Warning: The grid level zq[%d]=%f in [%f, %f] has no corresponding cells.\n",
           g_id, g_coords[g_id][2], z_min, z_max);
     }
-#endif
   }
+  _warnings = _warnings && continue_warnings;
 }
 
 /*----------------------------------------------------------------------------
@@ -496,16 +494,15 @@ cs_interpol_grid_init(cs_interpol_grid_t    *ig,
                       const cs_lnum_t        nb_points,
                       const cs_real_t       *coords)
 {
-  cs_lnum_t ii;
   CS_MALLOC(ig->cell_connect, nb_points, cs_lnum_t);
 #if defined(HAVE_MPI)
   if (cs_glob_n_ranks > 1)
     CS_MALLOC(ig->rank_connect, nb_points, int);
 #endif
-  CS_MALLOC(ig->coords, 3*nb_points, cs_real_t);
+  CS_MALLOC(ig->coords, 3*(nb_points+1), cs_real_t);
 
 #   pragma omp parallel for
-  for (ii = 0; ii < 3*nb_points; ii++) {
+  for (cs_lnum_t ii = 0; ii < 3*(nb_points+1); ii++) {
     ig->coords[ii] = coords[ii];
   }
 
@@ -641,7 +638,7 @@ cs_measures_set_map_values(cs_measures_set_t       *ms,
   if (nb_measures != ms->nb_measures) {
     CS_REALLOC(ms->measures, nb_measures*dim, cs_real_t);
     CS_REALLOC(ms->inf_radius, nb_measures*3, cs_real_t);
-    CS_REALLOC(ms->coords, nb_measures*3, cs_real_t);
+    CS_REALLOC(ms->coords, (nb_measures+1)*3, cs_real_t);
     CS_REALLOC(ms->is_cressman, nb_measures, int);
     CS_REALLOC(ms->is_interpol, nb_measures, int);
     ms->nb_measures = nb_measures;

src/atmo/atincl.f90
@@ -204,7 +204,7 @@ double precision, dimension(:,:), pointer :: xyvert
 
 !> vertical grid for 1D radiative scheme initialize in
 !>       cs_user_atmospheric_model.f90
-double precision, dimension(:), pointer :: zvert
+double precision, dimension(:), pointer  :: zray
 
 !> absorption for CO2 + 03
 double precision, dimension(:), pointer :: acinfe
@@ -254,7 +254,6 @@ double precision, dimension(:), pointer :: soil_density
 double precision, save :: tausup
 
 !> internal variable for 1D radiative model
-double precision, dimension(:), pointer  :: zray
 double precision, dimension(:,:), pointer  :: rayi, rayst
 
 !> Upward and downward radiative fluxes (infrared, solar) along each vertical
@@ -293,12 +292,21 @@ real(c_double), pointer, save :: aod_o3_tot
 !> adimensional :  aod_h2o_tot=0.10 other referenced values are  0.06, 0.08
 real(c_double), pointer, save :: aod_h2o_tot
 
+!> total aerosol optical depth in the IR domain for thermal radiation
+!> deduced from aeronet data
+double precision, save:: aod_ir = 0.1d0
+
+!> CO2 concentration in cm NTP with correction to the ratio of
+!> molar masses for Mco2 and Mair
+!> default is 350ppm
+double precision, save:: conco2 = 3.5d-2*44.d0/29.d0
+
 !> Asymmetry factor for O3 (non-dimensional)
 !> climatic value gaero_o3=0.66
 double precision, save:: gaero_o3 = 0.66d0
 !> Asymmetry factor for H2O (non-dimensional)
 !> climatic value gaero_h2o=0.64
-double precision, save:: gaero_h2o = 064d0
+double precision, save:: gaero_h2o = 0.64d0
 
 !> Single scattering albedo for O3 (non-dimensional)
 !> climatic value piaero_o3=0.84, other referenced values are 0.963
@@ -376,13 +384,13 @@ integer(c_int), pointer, save :: rad_atmo_model
          p_umet, p_vmet,                                                       &
          p_wmet  , p_tmmet, p_phmet, p_tpmet, p_ekmet, p_epmet,                &
          p_ttmet , p_rmet , p_qvmet, p_ncmet,                                  &
-         p_xyvert, p_zvert, p_acinfe,                                          &
+         p_xyvert, p_zray , p_acinfe,                                          &
          p_dacinfe, p_aco2, p_aco2s,                                           &
          p_daco2, p_daco2s,                                                    &
          p_acsup, p_acsups,                                                    &
          p_dacsup, p_dacsups,                                                  &
          p_tauzq, p_tauz, p_zq,                                                &
-         p_zray, p_rayi, p_rayst,                                              &
+         p_rayi, p_rayst,                                                      &
          p_iru, p_ird, p_solu, p_sold,                                         &
          p_soil_albedo,                                                        &
          p_soil_emissi,                                                        &
@@ -404,13 +412,13 @@ integer(c_int), pointer, save :: rad_atmo_model
       type(c_ptr), intent(out) :: p_umet, p_vmet, p_tmmet, p_wmet
       type(c_ptr), intent(out) :: p_phmet, p_tpmet, p_ekmet, p_epmet
       type(c_ptr), intent(out) :: p_ttmet, p_rmet, p_qvmet, p_ncmet
-      type(c_ptr), intent(out) :: p_xyvert, p_zvert, p_acinfe
+      type(c_ptr), intent(out) :: p_xyvert, p_zray , p_acinfe
       type(c_ptr), intent(out) :: p_dacinfe, p_aco2, p_aco2s
       type(c_ptr), intent(out) :: p_daco2, p_daco2s
       type(c_ptr), intent(out) :: p_acsup, p_acsups
       type(c_ptr), intent(out) :: p_dacsup, p_dacsups
       type(c_ptr), intent(out) :: p_tauzq, p_tauz, p_zq
-      type(c_ptr), intent(out) :: p_zray, p_rayi, p_rayst
+      type(c_ptr), intent(out) :: p_rayi, p_rayst
       type(c_ptr), intent(out) :: p_iru, p_ird, p_solu, p_sold
       type(c_ptr), intent(out) :: p_soil_albedo
       type(c_ptr), intent(out) :: p_soil_emissi
@@ -583,13 +591,13 @@ type(c_ptr) :: c_u_met, c_v_met, c_time_met
 type(c_ptr) :: c_w_met
 type(c_ptr) :: c_hyd_p_met, c_pot_t_met, c_ek_met, c_ep_met
 type(c_ptr) :: c_temp_met, c_rho_met, c_qw_met, c_ndrop_met
-type(c_ptr) :: c_xyvert, c_zvert, c_acinfe
+type(c_ptr) :: c_xyvert, c_zray , c_acinfe
 type(c_ptr) :: c_dacinfe, c_aco2, c_aco2s
 type(c_ptr) :: c_daco2, c_daco2s
 type(c_ptr) :: c_acsup, c_acsups
 type(c_ptr) :: c_dacsup, c_dacsups
 type(c_ptr) :: c_tauzq, c_tauz, c_zq
-type(c_ptr) :: c_zray, c_rayi, c_rayst
+type(c_ptr) :: c_rayi, c_rayst
 type(c_ptr) :: c_iru, c_ird, c_solu, c_sold
 type(c_ptr) :: c_soil_albedo
 type(c_ptr) :: c_soil_emissi
@@ -613,13 +621,13 @@ call cs_f_atmo_arrays_get_pointers(c_z_temp_met,                  &
                                    c_rho_met,                     &
                                    c_qw_met,                      &
                                    c_ndrop_met,                   &
-                                   c_xyvert, c_zvert, c_acinfe,   &
+                                   c_xyvert, c_zray , c_acinfe,   &
                                    c_dacinfe, c_aco2, c_aco2s,    &
                                    c_daco2, c_daco2s,             &
                                    c_acsup, c_acsups,             &
                                    c_dacsup, c_dacsups,           &
                                    c_tauzq, c_tauz, c_zq,         &
-                                   c_zray, c_rayi, c_rayst,       &
+                                   c_rayi, c_rayst,               &
                                    c_iru, c_ird, c_solu, c_sold,  &
                                    c_soil_albedo,                 &
                                    c_soil_emissi,                 &
@@ -648,7 +656,7 @@ call c_f_pointer(c_qw_met, qvmet, [dim_ntx_nt])
 call c_f_pointer(c_ndrop_met, ncmet, [dim_ntx_nt])
 
 call c_f_pointer(c_xyvert , xyvert , [dim_xyvert])
-call c_f_pointer(c_zvert  , zvert  , [kmx])
+call c_f_pointer(c_zray   , zray   , [kmx])
 call c_f_pointer(c_acinfe , acinfe , [kmx])
 call c_f_pointer(c_dacinfe, dacinfe, [kmx])
 call c_f_pointer(c_aco2   , aco2   , [dim_kmx2])
@@ -663,7 +671,6 @@ call c_f_pointer(c_tauzq  , tauzq  , [kmx+1])
 call c_f_pointer(c_tauz   , tauz   , [kmx+1])
 call c_f_pointer(c_zq     , zq     , [kmx+1])
 call c_f_pointer(c_rayi   , rayi   , [dim_kmx_nvert])
-call c_f_pointer(c_zray   , zray   , [kmx])
 call c_f_pointer(c_rayst  , rayst  , [dim_kmx_nvert])
 call c_f_pointer(c_iru    , iru    , [dim_kmx_nvert])
 call c_f_pointer(c_ird    , ird    , [dim_kmx_nvert])

src/atmo/atr1vf.f90
@@ -54,7 +54,7 @@ procedure() :: grimap, gripol, rayir, rayso, mesmap
 integer k, ii, jj
 integer k1
 integer ifac, isol
-integer ico2,imer1
+integer imer1
 integer ideb, icompt
 integer ktamp
 integer nfmodsol, nbrsol
@@ -157,7 +157,7 @@ if (mod(ntcabs,nfatr1).eq.0.or.ideb.eq.0) then
   call c_f_pointer(c_fnvert, fnvert, [dim_kmx_nvert])
   call c_f_pointer(c_aevert, aevert, [dim_kmx_nvert])
 
-  allocate(coords(3,kmx,nvert))
+  allocate(coords(3,kmx+1,nvert))
   allocate(cressm(kmx*nvert), interp(kmx*nvert))
   allocate(infrad(3*kmx*nvert))
 
@@ -167,15 +167,8 @@ if (mod(ntcabs,nfatr1).eq.0.or.ideb.eq.0) then
 
   if (idtvar.eq.0 .or. idtvar.eq.1) heuray = heuray + ttcabs/3600.d0
 
-  if (ntcabs.le.2.or.isuite.eq.1) then
-    ico2 = 1
-  else
-    ico2 = 0
-  endif
-
   ! --- Initialization:
   do k = 2, kvert
-    zray(k) = 0.d0
     preray(k) = 0.d0
     temray(k) = 0.d0
     qvray(k) = 0.d0
@@ -200,20 +193,31 @@ if (mod(ntcabs,nfatr1).eq.0.or.ideb.eq.0) then
   ! 2.  Computing long-wave and short-wave radiative fluxes
   !=============================================================================
 
+  ! Index of the bottom level
+  k1 = 1
+
   do ii = 1, nvert ! (ixj) index
 
     xvert = xyvert(ii,1)
     yvert = xyvert(ii,2)
 
-    do jj = 1, kmx ! k index
-      coords(1,jj,ii) = xvert
-      coords(2,jj,ii) = yvert
-      coords(3,jj,ii) = zvert(jj)
+    ! Addition of one level for solar radiation
+    ! TODO merge with 44km
+    zq(kmx+1) = 16000.d0
+
+    ! coords are levels (faces in 3D) whereas zray is slice (cells in 3D)
+    do k = 1, kmx+1
+      coords(1,k,ii) = xvert
+      coords(2,k,ii) = yvert
+      coords(3,k,ii) = zq(k)
+      if (k.le.kmx) then
+        zray(k) = 0.5d0 * (zq(k) + zq(k+1))
+      endif
     enddo
   enddo
 
   if (ntcabs.eq.1) then
-    call grimap(igrid, nvert*kmx, coords)
+    call grimap(igrid, nvert*kmx, coords) ! face grid
   endif
 
   ! Grid interpolation is refurbished to get a P0 interpolation
@@ -223,6 +227,8 @@ if (mod(ntcabs,nfatr1).eq.0.or.ideb.eq.0) then
   ! and then consider that a cell (or a face) belongs to a ijk if one node is
   ! in it (i.e. there is a non-zero intersection between the two).
   ! Then we can renormalise by the ratio "Volume(ijk)/SUM(cell_f_vol)"
+
+  ! Interpolation from 3D to 1D in the fluid domain
   call gripol(igrid, cpro_tempc, ttvert)
   call gripol(igrid, crom, romvert)
 
@@ -326,43 +332,35 @@ if (mod(ntcabs,nfatr1).eq.0.or.ideb.eq.0) then
     ! 2.1 Profiles used for the computation of the radiative fluxes
     !--------------------------------------------------------------
 
+    ! Loop over the variable defined until the top of the full domain
+    do k = 1, kmx
+
+      aeroso(k) = aevert(k, ii)
+      if (ippmod(iatmos).eq.2.and.moddis.eq.2) then
+        qlray(k)  = qlvert(k, ii)
+        ncray(k)  = ncvert(k, ii)
+        fneray(k) = fnvert(k, ii)
+      else
+        ! default values
+        ncray(k) = 0.d0
+        qlray(k) = 0.d0
+        fneray(k) = 0.d0
+      endif
+
+    enddo
+
     ! Soil variables
-    zray(1)   = zvert(1)
-    temray(1) = soil_ttsoil(ii)
-    qvray(1)  = soil_totwat(ii)
-    romray(1) = soil_density (ii)
-    preray(1) = soil_pressure(ii)
-    qlray(1)  = 0.d0
-    ncray(1)  = 0.d0
-    fneray(1) = 0.d0
-    aeroso(1) = aevert(1, ii)
+    !temray(1) = soil_ttsoil(ii)
+    !qvray(1)  = soil_totwat(ii)
+    !romray(1) = soil_density (ii)
+    !preray(1) = soil_pressure(ii)
 
-    ! Interpolation of temperature, humidity, density on the vertical
-    ! The ref pressure profile is the one computed from the meteo profile
-    if (ippmod(iatmos).eq.2.and.moddis.eq.2) then
-      qlray(1) = qlvert(1, ii)
-      ncray(1)  = ncvert(1, ii)
-      fneray(1) = fnvert(1, ii)
-    endif
-
-    do k = 2, kvert
-      zray(k) = zvert(k)
+    do k = 1, kvert
 
       temray(k) = ttvert(k + (ii-1)*kmx)
+
       qvray(k)  = qvvert(k, ii)
       romray(k) = romvert(k + (ii-1)*kmx)
-
-      ! default values
-      ncray(k) = 0.d0
-      qlray(k) = 0.d0
-      fneray(k) = 0.d0
-      aeroso(k) = aevert(k, ii)
-
-      if (ippmod(iatmos).eq.2.and.moddis.eq.2) then
-        ncray(k)  = ncvert(k, ii)
-        qlray(k)  = qlvert(k, ii)
-        fneray(k) = fnvert(k, ii)
-      endif
 
       if (imeteo.eq.0) then
         call atmstd(0.d0, &
@@ -383,13 +381,8 @@ if (mod(ntcabs,nfatr1).eq.0.or.ideb.eq.0) then
     enddo
 
     ! --- Filling the additional levels
+    !TODO do it before
     do k = kvert+1, kmx
-      zray(k) = zvert(k)
-      qlray(k) = 0.d0
-      qvray(k) = 0.d0
-      fneray(k) = 0.d0
-      ncray(k) = 0.d0
-      aeroso(k) = aevert(k, ii)
 
       ! Initialize with standard atmosphere
       ! above the domain
@@ -406,7 +399,7 @@ if (mod(ntcabs,nfatr1).eq.0.or.ideb.eq.0) then
 
     ktamp = 0
     if (imeteo.eq.1) then
-      ktamp = 6
+      ktamp = min(6, kvert)
       do k = kvert - ktamp+1, kmx
         call intprf(nbmaxt, nbmetm, ztmet, tmmet,                              &
                     ttmet, zray(k), ttcabs, temray(k))
@@ -431,7 +424,8 @@ if (mod(ntcabs,nfatr1).eq.0.or.ideb.eq.0) then
     ! --- Clipping the humidity
 
     do k = 1, kmx
-      qvray(k) = max(5.d-4,qvray(k))
+      qvray(k) = max(0.d0,qvray(k))
+      qlray(k) = max(0.d0,qlray(k))
     enddo
 
     ! --- Computing pressure and density according to temperature
@@ -448,15 +442,13 @@ if (mod(ntcabs,nfatr1).eq.0.or.ideb.eq.0) then
     ! 2.2 Computing the radiative fluxes for the vertical
     !-----------------------------------------------------
 
-    k1 = 1
-
     ! --- Long-wave: InfraRed
-    call rayir(ii, k1, kmx, ico2, emis,                           &
+    call rayir(ii, k1, kmx, emis,                                 &
                tauzq, tauz, tausup, zq,                           &
-               acinfe, dacinfe, aco2, daco2, aco2s, daco2s,       &
-               acsup, dacsup, acsups, dacsups,                    &
                zray, temray, qvray,                               &
                qlray, fneray, romray, preray, aeroso,             &
+               soil_ttsoil(ii), soil_totwat(ii),                  &
+               soil_density (ii), soil_pressure(ii),              &
                foir, rayi(:,ii), ncray)
 
     ! --- Short-wave: Sun
@@ -471,9 +463,9 @@ if (mod(ntcabs,nfatr1).eq.0.or.ideb.eq.0) then
   do ii = 1, kmx*nvert
     cressm(ii) = 1
     interp(ii) = 1
-    infrad(3*(ii-1) + 1) = 1.d0/8500.d0 ! horizontal(x) cressman radius
-    infrad(3*(ii-1) + 2) = 1.d0/8500.d0 ! horizontal(y) cressman radius
-    infrad(3*(ii-1) + 3) = 4.d0/1.d0    ! vertical(z) cressman radius
+    infrad(3*(ii-1) + 1) = 1.d0/8500.d0 ! horizontal(x) Cressman radius
+    infrad(3*(ii-1) + 2) = 1.d0/8500.d0 ! horizontal(y) Cressman radius
+    infrad(3*(ii-1) + 3) = 4.d0/1.d0    ! vertical(z) Cressman radius
   enddo
 
   ! Map Infra Red 1D (rayi) on the structure idrayi

src/atmo/cs_atmo.cpp
@@ -159,7 +159,6 @@ static cs_atmo_option_t  _atmo_option = {
   .rad_1d_tauzq = nullptr,
   .rad_1d_tauz = nullptr,
   .rad_1d_zq = nullptr,
-  .rad_1d_zray = nullptr,
   .rad_1d_ir_div = nullptr,
   .rad_1d_sol_div = nullptr,
   .rad_1d_iru = nullptr,
@@ -352,7 +351,7 @@ cs_f_atmo_arrays_get_pointers(cs_real_t **z_temp_met,
                               cs_real_t **qvmet,
                               cs_real_t **ncmet,
                               cs_real_t **xyvert,
-                              cs_real_t **zvert,
+                              cs_real_t **zray,
                               cs_real_t **acinfe,
                               cs_real_t **dacinfe,
                               cs_real_t **aco2,
@@ -366,7 +365,6 @@ cs_f_atmo_arrays_get_pointers(cs_real_t **z_temp_met,
                               cs_real_t **tauzq,
                               cs_real_t **tauz,
                               cs_real_t **zq,
-                              cs_real_t **zray,
                               cs_real_t **rayi,
                               cs_real_t **rayst,
                               cs_real_t **iru,
@@ -2056,7 +2054,7 @@ cs_f_atmo_arrays_get_pointers(cs_real_t **z_temp_met,
                               cs_real_t **qvmet,
                               cs_real_t **ncmet,
                               cs_real_t **xyvert,
-                              cs_real_t **zvert,
+                              cs_real_t **zray,
                               cs_real_t **acinfe,
                               cs_real_t **dacinfe,
                               cs_real_t **aco2,
@@ -2070,7 +2068,6 @@ cs_f_atmo_arrays_get_pointers(cs_real_t **z_temp_met,
                               cs_real_t **tauzq,
                               cs_real_t **tauz,
                               cs_real_t **zq,
-                              cs_real_t **zray,
                               cs_real_t **rayi,
                               cs_real_t **rayst,
                               cs_real_t **iru,
@@ -2172,8 +2169,6 @@ cs_f_atmo_arrays_get_pointers(cs_real_t **z_temp_met,
     CS_MALLOC(_atmo_option.rad_1d_tauz, n_level+1, cs_real_t);
   if (         _atmo_option.rad_1d_zq == nullptr)
     CS_MALLOC(_atmo_option.rad_1d_zq, n_level+1, cs_real_t);
-  if (         _atmo_option.rad_1d_zray == nullptr)
-    CS_MALLOC(_atmo_option.rad_1d_zray, n_level, cs_real_t);
   if (         _atmo_option.rad_1d_ir_div == nullptr)
     CS_MALLOC(_atmo_option.rad_1d_ir_div, n_level * n_vert, cs_real_t);
   if (         _atmo_option.rad_1d_sol_div == nullptr)
@@ -2244,7 +2239,7 @@ cs_f_atmo_arrays_get_pointers(cs_real_t **z_temp_met,
   }
 
   *xyvert = _atmo_option.rad_1d_xy;
-  *zvert  = _atmo_option.rad_1d_z;
+  *zray   = _atmo_option.rad_1d_z;
   *acinfe = _atmo_option.rad_1d_acinfe;
   *dacinfe = _atmo_option.rad_1d_dacinfe;
   *aco2   = _atmo_option.rad_1d_aco2  ;
@@ -2258,7 +2253,6 @@ cs_f_atmo_arrays_get_pointers(cs_real_t **z_temp_met,
   *tauzq  = _atmo_option.rad_1d_tauzq ;
   *tauz   = _atmo_option.rad_1d_tauz  ;
   *zq     = _atmo_option.rad_1d_zq    ;
-  *zray   = _atmo_option.rad_1d_zray  ;
   *rayi   = _atmo_option.rad_1d_ir_div  ;
   *rayst  = _atmo_option.rad_1d_sol_div ;
   *iru    = _atmo_option.rad_1d_iru   ;
@@ -5571,7 +5565,6 @@ cs_atmo_finalize(void)
   CS_FREE(_atmo_option.rad_1d_tauzq);
   CS_FREE(_atmo_option.rad_1d_tauz);
   CS_FREE(_atmo_option.rad_1d_zq);
-  CS_FREE(_atmo_option.rad_1d_zray);
   CS_FREE(_atmo_option.rad_1d_ir_div);
   CS_FREE(_atmo_option.rad_1d_sol_div);
   CS_FREE(_atmo_option.rad_1d_iru);
@@ -5602,6 +5595,7 @@ cs_atmo_finalize(void)
   CS_FREE(_atmo_option.soil_cat_w2);
   CS_FREE(_atmo_option.soil_cat_thermal_inertia);
   CS_FREE(_atmo_option.soil_cat_thermal_roughness);
+
 }
 
 /*----------------------------------------------------------------------------*/

src/atmo/cs_atmo.h
@@ -208,10 +208,9 @@ typedef struct {
   cs_real_t *rad_1d_tauzq;
   /*! internal variable for 1D radiative model */
   cs_real_t *rad_1d_tauz;
-  /*! internal variable for 1D radiative model */
+  /*! vertical grid for 1D radiative scheme
+   * (stagerred grid associated to faces) */
   cs_real_t *rad_1d_zq;
-  /*! internal variable for 1D radiative model */
-  cs_real_t *rad_1d_zray;
   /*! flux divergence of IR radiation */
   cs_real_t *rad_1d_ir_div;
   /*! flux divergence of solar radiation */

src/atmo/cs_atmo_solmoy.cpp
@@ -288,7 +288,7 @@ cs_solmoy(void)
                " ** Soil/atmosphere interface                 **\n"
                " ** Array of constants                        **\n"
                " ** ========================================= **\n");
-    bft_printf(" *            * minimum* mean*    maximum*\n");
+    bft_printf(" *            * minimum*    mean* maximum*\n");
     for (cs_lnum_t n = 0; n < 10; n++) {
       if (n == 4) {
         bft_printf(" *%-12s*%8.4f*%8.4f*%8.4f*\n",

src/atmo/rayigc.f90
@@ -2,7 +2,7 @@
 
 ! This file is part of code_saturne, a general-purpose CFD tool.
 !
-! Copyright (C) 1998-2024 EDF S.A.
+! Copyright (C) 1998-2025 EDF S.A.
 !
 ! This program is free software; you can redistribute it and/or modify it under
 ! the terms of the GNU General Public License as published by the Free Software
@@ -28,21 +28,19 @@
 !______________________________________________________________________________.
 !  mode           name          role
 !______________________________________________________________________________!
-!> \param[in]     zbas          ground level altitude
 !> \param[in]     zz            height above ground level
-!> \param[in]     pz            pressure normalized by ground level pressure
 !> \param[in]     zzp           intermediate altitude for ozone
-!> \param[in]     pzp           corresponding pressure for zzp level
 !> \param[out]    xa            CO2 + O3 absorption
 !> \param[out]    xda           differential absorption for CO2 + O3
+!> \param[in]     u             water vapor optical depth (zz, zzp)
 !> \param[in]     q             effective concentration for absorption by water
 !>                              vapor
-!> \param[in]     u             water vapor optical depth (zz, zzp)
-!> \param[in]     tco2          temperature for high level
+!> \param[in]     uco2          CO2 optical depth (zz, zzp)
+!> \param[in]     qco2          effective concentration for absorption by CO2
 !> \param[in]     ro            air density
 !_______________________________________________________________________________
 
-subroutine rayigc (zbas,zz,pz,zzp,pzp,xa,xda,q,u,tco2,ro)
+subroutine rayigc(zz, zzp, xa,xda,u,q,uco2,qco2,ro)
 
 !===============================================================================
 ! Module files
@@ -61,29 +59,23 @@ implicit none
 
 ! External variables
 
-double precision zbas
-double precision zz,pz,zzp,pzp,xa,xda,q,u,tco2,ro
+double precision zz,zzp,xa,xda,q,u, qco2,uco2,ro
 
 ! Local variables
 
 double precision x1,x2,x3,x4,x1c,x2c,x3c,x4c,y1,y2
-double precision tauv,dtauv,xx,exn,exnp1,conco2
-double precision uco2,duco2,ao,dao
 double precision uo3,duo3,xo1,xo2,xo3,xo4
 double precision yo1,yo2,ao3,dao3
-
-data x1,x2,x3,x4/1.33,-0.4572,0.26,0.286/
-data x1c,x2c,x3c,x4c/-0.00982,0.0676,0.421,0.01022/
-data y1,y2/0.0581,0.0546/
-data xo1,xo2,xo3,xo4/0.209,7.e-5,0.436,-0.00321/
-data yo1,yo2/0.0749,0.0212/
+double precision tauv,dtauv,duco2,ao,dao
+data x1,x2,x3,x4/1.33d0,-0.4572d0,0.26d0,0.286d0/
+data x1c,x2c,x3c,x4c/-0.00982d0,0.0676d0,0.421d0,0.01022d0/
+data y1,y2/0.0581d0,0.0546d0/
+data xo1,xo2,xo3,xo4/0.209d0,7.d-5,0.436d0,-0.00321d0/
+data yo1,yo2/0.0749d0,0.0212d0/
 
 !===============================================================================
 
-! Concentration of CO2
-conco2 = 3.5d-2
-
-! 1. Computation of th2o within the range 15mu of CO2
+! 1. Computation of th2o within the range 15mu of CO2 with u in kg/m2
 
 if(u.le.20.d0) then
   tauv = x1 + x2*(u + x4)**x3
@@ -93,17 +85,8 @@ else
   dtauv = -0.2754d0/log(10.d0)*ro*q/u
 endif
 
-! 2. Computation of the optical depth for CO2
-
-xx = 1.d0 - 0.0065d0*(zz - zbas)/288.15d0
-exn = 0.75d0
-exnp1 = exn+1.d0
-uco2 = -conco2*288.15d0/0.0065d0/(5.31d0*exnp1)*(pz**exnp1                &
-     - pzp**exnp1)*(tkelvi/tco2)**(exn/2.d0)
-
-if(uco2.lt.0.d0) uco2 = -uco2
-duco2 = conco2*pz**exnp1/xx
-duco2 = duco2*(tkelvi/tco2)**(exn/2.d0)
+! 2. Computation of the optical depth for CO2 in cm
+duco2=ro*qco2
 if(uco2.le.1.d0) then
   ao = x1c + x2c*(uco2 + x4c)**x3c
   dao = duco2*x2c*x3c*(uco2 + x4c)**(x3c - 1.d0)
@@ -112,13 +95,13 @@ else
   dao = y2/log(10.d0)*duco2/uco2
 endif
 
-! 3. Computation of the optical depth for O3
+! 3. Computation of the optical depth for O3 with uo3 in cm
 
 uo3 = abs(rayuoz(zz) - rayuoz(zzp))
 duo3 = raydoz(zz)
 if(uo3.le.0.01d0) then
   ao3 = xo1*(uo3 + xo2)**xo3 + xo4
-  dao3 = duo3*xo1*(uo3 + xo2)**(xo3 - 1.d0)
+  dao3 = duo3*xo1*xo3*(uo3 + xo2)**(xo3 - 1.d0)
 else
   ao3 = yo1 + yo2*log10(uo3)
   dao3 = yo2*duo3/log(10.d0)/uo3

commit cf8837f2a9539b3c3a789fcf7a664e1ade9fe2b0

[Atmo] 1D radiative transfer: fix bug introduced in e513498c (2025-10-21).

src/atmo/rayir.f90
@@ -149,7 +149,7 @@ allocate(qqc(kmx+1), qql(kmx+1))
 allocate(qqco2(kmx+1))
 allocate(qqqc(kmx), qqql(kmx))
 allocate(qqqco2(kmx))
-allocate(pspo(kmx), pspoqq(kmx), dt4(kmx), tqq(kmx))
+allocate(pspo(kmx), pspoqq(kmx+1), dt4(kmx), tqq(kmx+1))
 allocate(dz0(kmx))
 allocate(kliq(kmx+1))
 
@@ -194,6 +194,9 @@ do k = 1, kmx
 
   if (aeroso(k).gt.1.d-8) iaer = 1
 enddo
+
+pspoqq(kmx+1) = 0.d0
+tqq(kmx+1) = 0.d0
 
 qqv(kmx+1) = 0.d0
 qqc(kmx+1) = 0.d0
@@ -302,14 +305,21 @@ endif
 ! surface temperature (Kelvin)
 tqq(k1) = t_surf + tkelvi
 
+! Top interface temperature (isothermal above the domain)
+tqq(kmray+1) = temray(kmray) + tkelvi
+pspoqq(kmray+1) = pspo(kmray)
+
 do k = k1+1, kmray
-  alpha_k = ( zray(k) - zqq(k)) / (zray(k) - zray(k-1))
+  alpha_k = (zqq(k) - zray(k-1)) / (zray(k) - zray(k-1))
   tqq(k) = (alpha_k * temray(k)+(1.d0 - alpha_k)*temray(k-1)) + tkelvi
   pspoqq(k) = (alpha_k * pspo(k) + (1.d0 - alpha_k) * pspo(k-1))
 
-  ! dT^4/dz x dz = 4 T^3 dT/dz x dz
+  ! Calculate dt4 on the dual grid (intervals between layer centers)
   dt4(k) = 4.d0 * (tqq(k)**3) * (temray(k) - temray(k-1))
 enddo
+
+! Boundary dual cell: transition from soil surface to center of layer 1
+dt4(k1) = 4.d0 * (tqq(k1)**3) * (temray(k1) - t_surf)
 
 ! qqv(k+1) = int(0, zqq(k+1)) rov dz
 ! qqqv(k) = qqv(k) + dz/2(k) * rov(k)
@@ -351,8 +361,8 @@ t4zt = (temray(kmray) + tkelvi)**4
 t41 = (tqq(k1))**4
 
 ! For cloudy sky (but also valid for clear sky)
-fn = fnerir(k1+1)
-do k = k1+1, kmray
+fn = fnerir(k1)
+do k = k1, kmray
   ! cloud fraction estimation (we take the maximum)
   fn = max(fn,fnerir(k))
   if (fn.lt.1.d-3) then
@@ -416,7 +426,7 @@ do i = k1, kmray
   ! upward fluxes
   if (i.gt.k1) then
     fn = fnerir(i)
-    do k = k1+1, i
+    do k = k1, i
       ! cloud fraction estimation (we take the maximum)
       fn = max(fn,fnerir(k))
       qqqqv = qqqv(i)-qqv(k)
@@ -443,7 +453,7 @@ do i = k1, kmray
   ! flux reflected by the ground when emissivity is different from 1
   ! requires to compute the integral below (a3).
   a3 = 0.d0
-  do k = k1+1, kmray
+  do k = k1, kmray
     qqqqv = qqqv(i) + qqv(k)
     qqqqc = qqqc(i) + qqc(k)
     qqqqco2 = qqqco2(i) + qqco2(k)
@@ -489,7 +499,7 @@ do k = k1, kmray
   a1 = 0.d0
   dul = rol(k)
 
-  do kk = k1+1, k
+  do kk = k1, k
     qqqqv = qqqv(k) - qqv(kk)
     qqqqc = qqqc(k) - qqc(kk)
     qqqqco2 = qqqco2(k) - qqco2(kk)
@@ -547,7 +557,7 @@ do k = k1, kmray
   ! a3: contribution of the upward flux reflected at the ground
   ! Modification of Ponnulakshmi and al. (2009)
   a3 = 0.d0
-  do kk = k1+1, kmray
+  do kk = k1, kmray
     qqqqv = qqqv(k) + qqv(kk)
     qqqqc = qqqc(k) + qqc(kk)
     qqqqco2 = qqqco2(k) + qqco2(kk)
@@ -600,8 +610,8 @@ do k = k1, kmray
   ! Integration from
   call rayigc(zqq(k1), zqq(k),abcinfe,dabcinfe,qqqqv,qv0(k),qqqqco2,qco2(k),romray(k))
 
-  fni = fnerir(k1+1)
-  do ineb =2, k
+  fni = fnerir(k1)
+  do ineb = k1, k
     fni = max(fni,fnerir(ineb))
   enddo
 

src/base/cs_measures_util.cpp
@@ -674,11 +674,15 @@ cs_measures_set_map_values(cs_measures_set_t       *ms,
   {
     cs_lnum_t jj;
 #   pragma omp parallel for private(jj)
-    for (ii = 0; ii < nb_measures; ii++)
+    for (ii = 0; ii < nb_measures; ii++) {
       for (jj = 0; jj < 3; jj++) {
         ms->coords[ii*3 + jj] = measures_coords[ii*3 + jj];
-        ms->inf_radius[ii*3 +jj] = influence_radius[ii*3 + jj];
+        ms->inf_radius[ii*3 + jj] = influence_radius[ii*3 + jj];
       }
+    }
+    for (jj = 0; jj < 3; jj++) {
+      ms->coords[nb_measures*3 + jj] = measures_coords[nb_measures*3 + jj];
+    }
   }
 }
 