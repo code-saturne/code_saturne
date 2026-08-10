code-saturne/code_saturne

commit a5c16a3d77cc0bdc42d0f94e5995575a69aadc37

introduce separate model layer with XML-driven rules.
Extract hardcoded turbulence, thermal and time stepping rules from C++
and Python into centralized XML files following a Layers + Repository
architecture pattern. A single source of truth (XML) is shared between
the C++ solver and the Python GUI, ensuring consistency between layers.

data_model/TimeSteppingRules.xml
@@ -1,0 +1,230 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<ModuleRules version="1.0" module="TimeStepping">
+  
+
+  <Definitions>
+    
+    <TypeDef name="TimeScheme">
+      <Value>steady</Value>
+      <Value>first_order</Value>
+      <Value>second_order</Value>
+    </TypeDef>
+    
+    <TypeDef name="ExtrapolationMethod">
+      <Value>none</Value>          
+      <Value>linear</Value>         
+      <Value>second_order</Value>    
+    </TypeDef>
+    
+    <TypeDef name="SourceTermOrder">
+      <Value>explicit</Value>        
+      <Value>semi_implicit_1</Value> 
+      <Value>semi_implicit_2</Value> 
+    </TypeDef>
+    
+  </Definitions>
+
+
+  <ValidationRules>
+    
+    
+    <Rule Type="ThetaScheme" Property="viscosity">
+      <Extrapolation Method="none" Theta="0.0"/>
+      <Extrapolation Method="linear" Theta="0.5"/>
+      <Extrapolation Method="second_order" Theta="1.0"/>
+      <Description>Viscosity extrapolation scheme</Description>
+    </Rule>
+    
+    <Rule Type="ThetaScheme" Property="heat_capacity">
+      <Extrapolation Method="none" Theta="0.0"/>
+      <Extrapolation Method="linear" Theta="0.5"/>
+      <Extrapolation Method="second_order" Theta="1.0"/>
+      <Description>Heat capacity (Cp) extrapolation scheme</Description>
+    </Rule>
+    
+    <Rule Type="ThetaScheme" Property="navier_stokes_source">
+      <SourceTermOrder Order="explicit" Theta="0.0"/>
+      <SourceTermOrder Order="semi_implicit_1" Theta="0.5"/>
+      <SourceTermOrder Order="semi_implicit_2" Theta="1.0"/>
+      <Description>Navier-Stokes source terms</Description>
+    </Rule>
+    
+    <Rule Type="ThetaScheme" Property="turbulence_source">
+      <SourceTermOrder Order="explicit" Theta="0.0"/>
+      <SourceTermOrder Order="semi_implicit_1" Theta="0.5"/>
+      <SourceTermOrder Order="semi_implicit_2" Theta="1.0"/>
+      <Description>Turbulent source terms</Description>
+    </Rule>
+    
+    <Rule Type="ThetaScheme" Property="scalar_source">
+      <SourceTermOrder Order="explicit" Theta="0.0"/>
+      <SourceTermOrder Order="semi_implicit_1" Theta="0.5"/>
+      <SourceTermOrder Order="semi_implicit_2" Theta="1.0"/>
+      <Description>Scalar source terms</Description>
+    </Rule>
+    
+    <Rule Type="ThetaScheme" Property="scalar_diffusivity">
+      <Extrapolation Method="none" Theta="0.0"/>
+      <Extrapolation Method="linear" Theta="0.5"/>
+      <Extrapolation Method="second_order" Theta="1.0"/>
+      <Description>Scalar diffusivity extrapolation</Description>
+    </Rule>
+    
+    
+    <Rule Type="TimeStepLimits">
+      <Parameter Name="dt_ref" Type="double" Default="0.1">
+        <Range Min="0.0" Exclusive="true"/>
+      </Parameter>
+      <Parameter Name="dt_min" Type="double" Default="1e-5">
+        <Range Min="0.0"/>
+      </Parameter>
+      <Parameter Name="dt_max" Type="double" Default="1e10">
+        <Range Min="0.0" Exclusive="true"/>
+      </Parameter>
+    </Rule>
+    
+    
+    <Rule Type="AutomaticSetting" Setting="ThetaAutoInit">
+      <Property Name="viscosity" Key="iviext">
+        <Mapping ExtrapMethod="0" ThetaValue="0.0"/>
+        <Mapping ExtrapMethod="1" ThetaValue="0.5"/>
+        <Mapping ExtrapMethod="2" ThetaValue="1.0"/>
+      </Property>
+      
+      <Property Name="heat_capacity" Key="icpext">
+        <Mapping ExtrapMethod="0" ThetaValue="0.0"/>
+        <Mapping ExtrapMethod="1" ThetaValue="0.5"/>
+        <Mapping ExtrapMethod="2" ThetaValue="1.0"/>
+      </Property>
+    </Rule>
+    
+    <Rule Type="AutomaticSetting" Setting="ThetaSourceTerms">
+      <SourceTerm Name="navier_stokes" Key="isno2t">
+        <Mapping Order="0" ThetaValue="0.0"/>
+        <Mapping Order="1" ThetaValue="0.5"/>
+        <Mapping Order="2" ThetaValue="1.0"/>
+      </SourceTerm>
+      
+      <SourceTerm Name="turbulence" Key="isto2t">
+        <Mapping Order="0" ThetaValue="0.0"/>
+        <Mapping Order="1" ThetaValue="0.5"/>
+        <Mapping Order="2" ThetaValue="1.0"/>
+      </SourceTerm>
+      
+      <SourceTerm Name="scalar" Key="isso2t">
+        <Mapping Order="0" ThetaValue="0.0"/>
+        <Mapping Order="1" ThetaValue="0.5"/>
+        <Mapping Order="2" ThetaValue="1.0"/>
+      </SourceTerm>
+    </Rule>
+    
+  </ValidationRules>
+  
+
+  <Validations>
+    
+    <Constraint Target="dt_ref" Type="positive_strict">
+      <ErrorMessage>Reference time step must be strictly positive</ErrorMessage>
+    </Constraint>
+    
+    <Constraint Target="dt_min" Type="positive">
+      <ErrorMessage>Minimum time step must be positive or zero</ErrorMessage>
+    </Constraint>
+    
+    <Constraint Target="dt_max" Type="positive_strict">
+      <ErrorMessage>Maximum time step must be strictly positive</ErrorMessage>
+    </Constraint>
+    
+    <Constraint Target="dt_consistency" Type="comparison">
+      <Condition>dt_max > dt_min</Condition>
+      <ErrorMessage>Maximum time step must be greater than minimum time step</ErrorMessage>
+    </Constraint>
+    
+    <Constraint Target="theta" Type="range">
+      <MinValue>0.0</MinValue>
+      <MaxValue>1.0</MaxValue>
+      <ErrorMessage>Theta value must be between 0 and 1</ErrorMessage>
+    </Constraint>
+    
+    <Constraint Target="thetvi" Type="range">
+      <MinValue>0.0</MinValue>
+      <MaxValue>1.0</MaxValue>
+      <ErrorMessage>Viscosity theta must be between 0 and 1</ErrorMessage>
+    </Constraint>
+    
+    <Constraint Target="thetcp" Type="range">
+      <MinValue>0.0</MinValue>
+      <MaxValue>1.0</MaxValue>
+      <ErrorMessage>Heat capacity theta must be between 0 and 1</ErrorMessage>
+    </Constraint>
+    
+    <Constraint Target="cfl_max" Type="positive_strict">
+      <ErrorMessage>Maximum CFL number must be strictly positive</ErrorMessage>
+    </Constraint>
+    
+    <Constraint Target="courant_max" Type="positive_strict">
+      <ErrorMessage>Maximum Courant number must be strictly positive</ErrorMessage>
+    </Constraint>
+    
+    <Constraint Target="nt_max" Type="either_or">
+      <Condition>nt_max > 0</Condition>
+      <Condition>t_max > 0</Condition>
+      <ErrorMessage>Either maximum iterations or maximum time must be specified</ErrorMessage>
+    </Constraint>
+    
+  </Validations>
+
+  
+  <Defaults>
+    <Default Key="time_step_ref" Value="0.1"/>
+    <Default Key="time_step_min" Value="1e-5"/>
+    <Default Key="time_step_max" Value="1e10"/>
+    <Default Key="max_iterations" Value="10"/>
+    <Default Key="max_time" Value="-1.0"/>
+    <Default Key="cfl_max" Value="1.0"/>
+    <Default Key="courant_max" Value="1.0"/>
+    <Default Key="fourier_max" Value="10.0"/>
+    <Default Key="time_order" Value="1"/>
+    <Default Key="theta_default" Value="0.5"/>
+  </Defaults>
+  
+
+  
+  <Mappings>
+    
+    <EnumMapping Name="ExtrapolationMethod">
+      <Entry Value="none" Enum="0" Constant="CS_TIME_EXTRAP_NONE"/>
+      <Entry Value="linear" Enum="1" Constant="CS_TIME_EXTRAP_LINEAR"/>
+      <Entry Value="second_order" Enum="2" Constant="CS_TIME_EXTRAP_SECOND_ORDER"/>
+    </EnumMapping>
+    
+    <EnumMapping Name="SourceTermOrder">
+      <Entry Value="explicit" Enum="0" Constant="CS_SOURCE_TERM_EXPLICIT"/>
+      <Entry Value="semi_implicit_1" Enum="1" Constant="CS_SOURCE_TERM_SEMI_IMPLICIT_1"/>
+      <Entry Value="semi_implicit_2" Enum="2" Constant="CS_SOURCE_TERM_SEMI_IMPLICIT_2"/>
+    </EnumMapping>
+    
+  </Mappings>
+  
+
+  <GUIRules>
+    <ModuleGUI Name="TimeSteppingGUI">
+      <ModelParameters>
+        <Parameter Name="dt_ref" Type="double" Default="0.1">
+          <Description>Reference time step</Description>
+          <Range Min="0.0" Exclusive="true"/>
+        </Parameter>
+        <Parameter Name="dt_min" Type="double" Default="1e-5">
+          <Description>Minimum time step</Description>
+          <Range Min="0.0"/>
+        </Parameter>
+        <Parameter Name="dt_max" Type="double" Default="1e10">
+          <Description>Maximum time step</Description>
+          <Range Min="0.0" Exclusive="true"/>
+        </Parameter>
+      </ModelParameters>
+    </ModuleGUI>
+  </GUIRules>
+  
+</ModuleRules>

commit 36c144b382655531df39680edd618c1b25d85f59

Data model: fix theta scheme handling, broken by a5c16a3d (2026-07-30).
Theta scheme rules are missing from the XML model, so were not applied.
Actually, since switching from a keyword to the actual value only needs
to be handled in the code, not in the GUI, there is probably no need
for such XML rules. Simply specifying which values of the integer keyword
values are used should be enough.

Also, locally, perhaps the "time_extrapolated" and isto2t values
could be removed, as querying them is equivalent to querying the
underlying integer keyword and multiplying by 2. Keeping them is useful
only if we expect to add schemes with theta not in {0, 0.5, 1.0}.
Otherwise, using the integral keyword direclty would avoid requiring
specific tests such as here. The alternative would be to define
the theta value directly, instead of thev integral value.

data_model/TimeSteppingRules.xml
@@ -20,7 +20,6 @@
 
 <ModuleRules version="1.0" module="TimeStepping">
 
-
   <Definitions>
 
     <TypeDef name="TimeScheme">
@@ -43,9 +42,7 @@
 
   </Definitions>
 
-
   <ValidationRules>
-
 
     <Rule Type="ThetaScheme" Property="viscosity">
       <Extrapolation Method="none" Theta="0.0"/>
@@ -89,7 +86,6 @@
       <Description>Scalar diffusivity extrapolation</Description>
     </Rule>
 
-
     <Rule Type="TimeStepLimits">
       <Parameter Name="dt_ref" Type="double" Default="0.1">
         <Range Min="0.0" Exclusive="true"/>
@@ -101,7 +97,6 @@
         <Range Min="0.0" Exclusive="true"/>
       </Parameter>
     </Rule>
-
 
     <Rule Type="AutomaticSetting" Setting="ThetaAutoInit">
       <Property Name="viscosity" Key="iviext">

src/base/cs_parameters.cpp
@@ -715,6 +715,39 @@ _log_func_default_var_cal_opt(const void *t)
                 _("Relaxation of variables (1 for no relaxation)"));
 }
 
+/*----------------------------------------------------------------------------
+ * Determine theta scheme value from scheme parameter.
+ *
+ * \param[in]       k_name
+ * \param[in]       t_name
+ * \param[in]       k_id
+ * \param[in, out]  theta
+ */
+/*----------------------------------------------------------------------------*/
+
+static void
+_set_theta_scheme(const char  *k_name,
+                  const char  *t_name,
+                  int          k_id,
+                  double      &theta)
+{
+  if (std::abs(theta+999.) > cs_math_epzero) {
+    cs_parameters_error
+      (CS_ABORT_DELAYED,
+       _("in the data specification"),
+       _("%s = %d\n"
+         "%s will be initialized automatically.\n"
+         "Do not modify it.\n"),
+       k_name, k_id, t_name);
+  }
+  else if (k_id == 0)
+    theta = 0.;
+  else if (k_id == 1)
+    theta = 0.5;
+  else if (k_id == 2)
+    theta = 1.;
+}
+
 /*! (DOXYGEN_SHOULD_SKIP_THIS) \endcond */
 
 /*=============================================================================
@@ -1672,75 +1705,31 @@ cs_parameters_global_complete(void)
 
   /* Physical properties */
 
-  /* Model rules: theta values are initialized through TimeSteppingRules.xml */
-  cs_time_stepping_rules_manager *ts_rules = cs_get_time_stepping_rules_manager();
-
-  int iviext = f_mu->get_key_int(key_t_ext_id);
-  if (ts_rules->should_auto_init_theta("viscosity", time_scheme->thetvi)) {
-    time_scheme->thetvi = ts_rules->get_theta_for_extrapolation("viscosity", iviext);
-  }
-  else {
-    cs_parameters_error
-      (CS_ABORT_DELAYED,
-       _("in the data specification"),
-       _("iviext = %d\n"
-         "thetvi will be initialized automatically.\n"
-         "Do not modify it.\n"),
-       iviext);
-  }
+  _set_theta_scheme("molecular_viscosity, \"time_extrapolated\" keyword",
+                    "cs_glob_time_scheme->thetvi",
+                    f_mu->get_key_int(key_t_ext_id),
+                    time_scheme->thetvi);
 
   if (cs_glob_fluid_properties->icp >= 0) {
-
-    int icpext = f_cp->get_key_int(key_t_ext_id);
-    if (ts_rules->should_auto_init_theta("heat_capacity",
-                                         time_scheme->thetcp)) {
-      time_scheme->thetcp
-        = ts_rules->get_theta_for_extrapolation("heat_capacity", icpext);
-    }
-    else {
-      cs_parameters_error
-        (CS_ABORT_DELAYED,
-         _("in the data specification"),
-         _("icpext = %d\n"
-           "thetcp will be initialized automatically.\n"
-           "Do not modify it.\n"),
-         icpext);
-    }
+    _set_theta_scheme("specific_heat, \"time_extrapolated\" keyword",
+                      "cs_glob_time_scheme->thetcp",
+                      f_cp->get_key_int(key_t_ext_id),
+                      time_scheme->thetcp);
   }
 
   /* NS source terms */
-  int isno2t = cs_glob_time_scheme->isno2t;
-  if (ts_rules->should_auto_init_theta("navier_stokes_source",
-                                       time_scheme->thetsn)) {
-    time_scheme->thetsn
-      = ts_rules->get_theta_for_source_term("navier_stokes_source", isno2t);
-  }
-  else {
-    cs_parameters_error
-      (CS_ABORT_DELAYED,
-       _("in the data specification"),
-       _("isno2t = %d\n"
-         "thetsn will be initialized automatically.\n"
-         "Do not modify it.\n"),
-       isno2t);
-  }
+
+  _set_theta_scheme("cs_glob_time_scheme->isno2t" ,
+                    "cs_glob_time_scheme->thetsn",
+                    time_scheme->isno2t,
+                    time_scheme->thetsn);
 
   /* Turbulent variables source terms */
-  int isto2t = cs_glob_time_scheme->isto2t;
-  if (ts_rules->should_auto_init_theta("turbulence_source",
-      time_scheme->thetst)) {
-    time_scheme->thetst
-      = ts_rules->get_theta_for_source_term("turbulence_source", isto2t);
-  }
-  else {
-    cs_parameters_error
-      (CS_ABORT_DELAYED,
-       _("in the data specification"),
-       _("isto2t = %d\n"
-         "thetst will be initialized automatically.\n"
-         "Do not modify it.\n"),
-       isto2t);
-  }
+
+  _set_theta_scheme("cs_glob_time_scheme->isto2t" ,
+                    "cs_glob_time_scheme->thetst",
+                    time_scheme->isto2t,
+                    time_scheme->thetst);
 
   const int n_fields = cs_field_n_fields();
 
@@ -1774,36 +1763,31 @@ cs_parameters_global_complete(void)
         thetss = 0.;
         f->set_key_double(kthetss, thetss);
       }
+
       /* Scalar diffusivity */
       int f_id_d = f->get_key_int(kivisl);
+      int iviext = 0;
       if (f_id_d >= 0)
         iviext = cs_field_by_id(f_id_d)->get_key_int(key_t_ext_id);
-      else
-        iviext = 0;
 
       cs_real_t thetvs = f->get_key_double(kthetvs);
       if (fabs(thetvs+1.) > cs_math_epzero) {
         cs_parameters_error
           (CS_ABORT_DELAYED,
            _("in the data specification"),
-           _("Scalar (%s) iviext = %d\n"
-             "thetvs will be initialized automatically.\n"
+           _("Scalar (%s) \"diffusivity_id\" key value = %d\n"
+             "\"diffusivity_extrapolated\" value will be set automatically.\n"
              "Do not modify it.\n"),
            f->name, iviext);
       }
-      else if (iviext == 0) {
+      else if (iviext == 0)
         thetvs = 0.;
-        f->set_key_double(kthetvs, thetvs);
-      }
-      else if (iviext == 1) {
+      else if (iviext == 1)
         thetvs = 0.5;
-        f->set_key_double(kthetvs, thetvs);
-      }
-      else if (iviext == 2) {
+      else if (iviext == 2)
         thetvs = 1.;
-        f->set_key_double(kthetvs, thetvs);
-      }
 
+      f->set_key_double(kthetvs, thetvs);
     }
   }
 }

src/model/cs_time_stepping_rules_manager.cpp
@@ -158,7 +158,7 @@ cs_time_stepping_rules_manager::parse_theta_schemes_()
       cs_theta_config_t tc;
       tc.property_name = std::string(property);
 
-      // Parser les extrapolations
+      // Parse extrapolations
       cs_tree_node_t *extrap = cs_tree_node_get_child(config,
                                                       "Extrapolation");
       while (extrap != nullptr) {
@@ -179,7 +179,7 @@ cs_time_stepping_rules_manager::parse_theta_schemes_()
         extrap = cs_tree_node_get_next_of_name(extrap);
       }
 
-      // Parser les ordres de termes sources
+      // Parse source term orders
       cs_tree_node_t *source_order = cs_tree_node_get_child(config,
                                                             "SourceTermOrder");
       while (source_order != nullptr) {
@@ -377,7 +377,7 @@ cs_time_stepping_rules_manager::get_theta_for_extrapolation
     if (theta_it != it->second.extrap_method_to_theta.end())
       return theta_it->second;
   }
-  return -999.0;  // Valeur invalide
+  return -999.0;  // Invald value
 }
 
 double
@@ -393,7 +393,7 @@ cs_time_stepping_rules_manager::get_theta_for_source_term
     if (theta_it != it->second.source_order_to_theta.end())
       return theta_it->second;
   }
-  return -999.0;  // Valeur invalide
+  return -999.0;  // Invald value
 }
 
 cs_time_step_limits_t