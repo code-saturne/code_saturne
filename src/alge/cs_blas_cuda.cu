code-saturne/code_saturne

commit 8a984b49d883ad2001c74dfa24e56f6859b504f1

GPU: fix compilation and runtime error.

src/base/cs_cuda_reduce.h
@@ -69,7 +69,7 @@
 
 template <size_t blockSize, size_t stride, typename T>
 __device__ static void __forceinline__
-cs_cuda_reduce_warp_reduce_sum(volatile T  *stmp,
+cs_cuda_reduce_warp_reduce_sum(T           *stmp,
                                size_t       tid)
 {
   if (stride == 1) {
@@ -328,7 +328,7 @@ cs_cuda_reduce_sum_single_block(size_t   n,
 
 template <size_t blockSize, typename R, typename T>
 __device__ static void __forceinline__
-cs_cuda_reduce_warp_reduce(volatile T  *stmp,
+cs_cuda_reduce_warp_reduce(T           *stmp,
                            size_t       tid)
 {
   R reducer;

src/fvm/fvm_morton.cpp
@@ -850,11 +850,11 @@ _get_coord_extents(cs_lnum_t         n_coords,
   ctx.parallel_for_reduce
     (n_coords, rd, reducer,
      [=] CS_F_HOST_DEVICE (cs_lnum_t c_id, cs_double_n<dim*2> &res) {
-       for (cs_lnum_t j = 0; j < dim; j++) {
-         res.r[j]       = coords[c_id*dim + j];
-         res.r[j + dim] = coords[c_id*dim + j];
-       }
-     });
+    for (cs_lnum_t j = 0; j < dim; j++) {
+      res.r[j]       = coords[c_id*dim + j];
+      res.r[j + dim] = coords[c_id*dim + j];
+    }
+  });
 
   ctx.wait();
 
@@ -1616,4 +1616,3 @@ fvm_morton_dump(int                dim,
 }
 
 /*----------------------------------------------------------------------------*/
-

src/turb/cs_turbulence_rij.cpp
@@ -3082,7 +3082,7 @@ _solve_omega(int              phase_id,
     ctx.parallel_for(n_cells, [=] CS_F_HOST_DEVICE (cs_lnum_t c_id) {
       const cs_real_t tuexpw = c_st_prv[c_id];
       c_st_prv[c_id] = rhs[c_id];
-      rhs[c_id] = fimp[c_id]*cvara_omg[c_id] - cs_glob_time_scheme->thetst*tuexpw;
+      rhs[c_id] = fimp[c_id]*cvara_omg[c_id] - thets*tuexpw;
       fimp[c_id] = -eqp_loc.theta*fimp[c_id];
     });
   }

commit fcee643aabe3a20d43081a85adab32ab5da84e6e

CUDA: fix reduction bug introduced in 8a984b49 (2026-07-30).
Also refactor some duplicated code.

src/alge/cs_blas_cuda.cu
@@ -51,6 +51,7 @@
 #include "base/cs_mem.h"
 #include "base/cs_base_accel.h"
 #include "base/cs_base_cuda.h"
+#include "base/cs_cuda_reduce.h"
 #include "base/cs_dispatch.h"
 
 /*----------------------------------------------------------------------------
@@ -118,7 +119,7 @@ _asum_stage_1_of_2(cs_lnum_t    n,
 
   // Output: b_res for this block
 
-  cs_blas_cuda_block_reduce_sum<blockSize, 1>(stmp, tid, b_res);
+  cs_cuda_reduce_block_sum<blockSize, 1>(stmp, tid, b_res);
 }
 
 /*----------------------------------------------------------------------------*/
@@ -155,7 +156,7 @@ _dot_xy_stage_1_of_2(cs_lnum_t    n,
 
   // Output: b_res for this block
 
-  cs_blas_cuda_block_reduce_sum<blockSize, 1>(stmp, tid, b_res);
+  cs_cuda_reduce_block_sum<blockSize, 1>(stmp, tid, b_res);
 }
 
 /*----------------------------------------------------------------------------*/
@@ -191,7 +192,7 @@ _dot_xx_stage_1_of_2(cs_lnum_t    n,
 
   // Output: b_res for this block
 
-  cs_blas_cuda_block_reduce_sum<blockSize, 1>(stmp, tid, b_res);
+  cs_cuda_reduce_block_sum<blockSize, 1>(stmp, tid, b_res);
 }
 
 /*----------------------------------------------------------------------------
@@ -389,7 +390,7 @@ cs_blas_cuda_asum(cudaStream_t     stream,
 
   _asum_stage_1_of_2<block_size><<<grid_size, block_size, 0, stream>>>
     (n, x, _r_grid);
-  cs_blas_cuda_reduce_single_block<block_size, 1><<<1, block_size, 0, stream>>>
+  cs_cuda_reduce_sum_single_block<block_size, 1><<<1, block_size, 0, stream>>>
     (grid_size, _r_grid, _r_reduce);
 
   /* Need to synchronize stream in all cases so as to
@@ -429,7 +430,7 @@ cs_blas_cuda_dot(cudaStream_t     stream,
   else
     _dot_xx_stage_1_of_2<block_size><<<grid_size, block_size, 0, stream>>>
       (n, x, _r_grid);
-  cs_blas_cuda_reduce_single_block<block_size, 1><<<1, block_size, 0, stream>>>
+  cs_cuda_reduce_sum_single_block<block_size, 1><<<1, block_size, 0, stream>>>
     (grid_size, _r_grid, _r_reduce);
 
   /* Need to synchronize stream in all cases so as to

src/alge/cs_blas_cuda.h
@@ -49,273 +49,6 @@
  *============================================================================*/
 
 /*============================================================================
- * Templated function definitions
- *============================================================================*/
-
-#if defined(__CUDACC__)
-
-/*----------------------------------------------------------------------------*/
-/*!
- * \brief  Kernel for sum reduction within a warp (for warp size 32).
- *
- * \tparam  blockSize  size of CUDA block
- * \tparam  stride     vector stride
- * \tparam  T          data type
- *
- * \param[in, out]  stmp  shared value to reduce
- * \param[in, out]  tid   thread id
- */
-/*----------------------------------------------------------------------------*/
-
-template <size_t blockSize, size_t stride, typename T>
-__device__ static void __forceinline__
-cs_blas_cuda_warp_reduce_sum(volatile T  *stmp,
-                             size_t       tid)
-{
-  if (stride == 1) {
-
-    if (blockSize >= 64) stmp[tid] += stmp[tid + 32];
-    if (blockSize >= 32) stmp[tid] += stmp[tid + 16];
-    if (blockSize >= 16) stmp[tid] += stmp[tid +  8];
-    if (blockSize >=  8) stmp[tid] += stmp[tid +  4];
-    if (blockSize >=  4) stmp[tid] += stmp[tid +  2];
-    if (blockSize >=  2) stmp[tid] += stmp[tid +  1];
-
-  }
-  else {
-
-    if (blockSize >= 64) {
-      #pragma unroll
-      for (size_t i = 0; i < stride; i++)
-        stmp[tid*stride + i] += stmp[(tid + 32)*stride + i];
-    }
-    if (blockSize >= 32) {
-      #pragma unroll
-      for (size_t i = 0; i < stride; i++)
-        stmp[tid*stride + i] += stmp[(tid + 16)*stride + i];
-    }
-    if (blockSize >= 16) {
-      #pragma unroll
-      for (size_t i = 0; i < stride; i++)
-        stmp[tid*stride + i] += stmp[(tid + 8)*stride + i];
-    }
-    if (blockSize >= 8) {
-      #pragma unroll
-      for (size_t i = 0; i < stride; i++)
-        stmp[tid*stride + i] += stmp[(tid + 4)*stride + i];
-    }
-    if (blockSize >= 4) {
-      #pragma unroll
-      for (size_t i = 0; i < stride; i++)
-        stmp[tid*stride + i] += stmp[(tid + 2)*stride + i];
-    }
-    if (blockSize >= 2) {
-      #pragma unroll
-      for (size_t i = 0; i < stride; i++)
-        stmp[tid*stride + i] += stmp[(tid + 1)*stride + i];
-    }
-  }
-}
-
-/*----------------------------------------------------------------------------*/
-/*!
- * \brief Compute dot product x.y, summing result over all threads of a block.
- *
- * blockSize must be a power of 2.
- *
- * This kernel uses explicit loop unrolling, which seems to offer equal or
- * slightly better performance than the non-unrolled version on an Ampere
- * architecure with CUDA 11.
- *
- * \tparam  blockSize  size of CUDA block
- * \tparam  stride     vector stride
- * \tparam  T          data type
- *
- * \param[in, out]  stmp       shared value to reduce
- * \param[in, out]  tid        thread id
- * \param[out]      sum_block  contribution of this block to the sum
- */
-/*----------------------------------------------------------------------------*/
-
-template <size_t blockSize, size_t stride, typename T>
-__device__ static void __forceinline__
-cs_blas_cuda_block_reduce_sum(T       *stmp,
-                              size_t   tid,
-                              T       *sum_block)
-{
-  __syncthreads();
-
-  /* Loop explicitely unrolled */
-
-  if (stride == 1) { /* Scalar data */
-
-    if (blockSize >= 1024) {
-      if (tid < 512) {
-        stmp[tid] += stmp[tid + 512];
-      }
-      __syncthreads();
-    }
-    if (blockSize >= 512) {
-      if (tid < 256) {
-        stmp[tid] += stmp[tid + 256];
-      }
-      __syncthreads();
-    }
-    if (blockSize >= 256) {
-      if (tid < 128) {
-        stmp[tid] += stmp[tid + 128];
-      } __syncthreads();
-    }
-    if (blockSize >= 128) {
-      if (tid <  64) {
-        stmp[tid] += stmp[tid +  64];
-      } __syncthreads();
-    }
-
-    if (tid < 32) {
-      cs_blas_cuda_warp_reduce_sum<blockSize, stride>(stmp, tid);
-    }
-
-    // Output: b_res for this block
-
-    if (tid == 0) sum_block[blockIdx.x] = stmp[0];
-
-  }
-
-  else { /* Vector data */
-
-    if (blockSize >= 1024) {
-      if (tid < 512) {
-        #pragma unroll
-        for (size_t i = 0; i < stride; i++)
-          stmp[tid*stride + i] += stmp[(tid + 512)*stride + i];
-      }
-      __syncthreads();
-    }
-    if (blockSize >= 512) {
-      if (tid < 256) {
-        #pragma unroll
-        for (size_t i = 0; i < stride; i++)
-          stmp[tid*stride + i] += stmp[(tid + 256)*stride + i];
-      }
-      __syncthreads();
-    }
-    if (blockSize >= 256) {
-      if (tid < 128) {
-        #pragma unroll
-        for (size_t i = 0; i < stride; i++)
-          stmp[tid*stride + i] += stmp[(tid + 128)*stride + i];
-      } __syncthreads();
-    }
-    if (blockSize >= 128) {
-      if (tid <  64) {
-        #pragma unroll
-        for (size_t i = 0; i < stride; i++)
-          stmp[tid*stride + i] += stmp[(tid + 64)*stride + i];
-      } __syncthreads();
-    }
-
-    if (tid < 32)
-      cs_blas_cuda_warp_reduce_sum<blockSize, stride>(stmp, tid);
-
-    // Output: b_res for this block
-
-    if (tid == 0) {
-      #pragma unroll
-      for (size_t i = 0; i < stride; i++)
-        sum_block[(blockIdx.x)*stride + i] = stmp[i];
-    }
-
-  }
-}
-
-/*----------------------------------------------------------------------------*/
-/*!
- * \brief  Kernel for sum reduction of vector data within a block.
- *
- * \tparam  stride  number of vector components
- *
- * \param[in, out]  n        number of vector values to reduce
- * \param[in, out]  g_idata  input array (size n)
- * \param[in, out]  g_odata  onput array (size 1)
- */
-/*----------------------------------------------------------------------------*/
-
-template <size_t blockSize, size_t stride, typename T>
-__global__ static void
-cs_blas_cuda_reduce_single_block(size_t   n,
-                                 T       *g_idata,
-                                 T       *g_odata)
-{
-  __shared__ T sdata[blockSize * stride];
-
-  size_t tid = threadIdx.x;
-  T r_s[stride];
-
-  if (stride == 1) {
-
-    r_s[0] = 0;
-    sdata[tid] = 0.;
-
-    for (size_t i = threadIdx.x; i < n; i+= blockSize)
-      r_s[0] += g_idata[i];
-
-    sdata[tid] = r_s[0];
-    __syncthreads();
-
-    for (size_t j = blockSize/2; j > CS_CUDA_WARP_SIZE; j /= 2) {
-      if (tid < j) {
-        sdata[tid] += sdata[tid + j];
-      }
-      __syncthreads();
-    }
-
-    if (tid < 32) cs_blas_cuda_warp_reduce_sum<blockSize, stride>(sdata, tid);
-    if (tid == 0) *g_odata = sdata[0];
-
-  }
-  else {
-
-    #pragma unroll
-    for (size_t k = 0; k < stride; k++) {
-      r_s[k] = 0;
-      sdata[tid*stride + k] = 0.;
-    }
-
-    for (size_t i = threadIdx.x; i < n; i+= blockSize) {
-      #pragma unroll
-      for (size_t k = 0; k < stride; k++) {
-        r_s[k] += g_idata[i*stride + k];
-      }
-    }
-
-    #pragma unroll
-    for (size_t k = 0; k < stride; k++)
-      sdata[tid*stride + k] = r_s[k];
-    __syncthreads();
-
-    for (size_t j = blockSize/2; j > CS_CUDA_WARP_SIZE; j /= 2) {
-      if (tid < j) {
-        #pragma unroll
-        for (size_t k = 0; k < stride; k++)
-            sdata[tid*stride + k] += sdata[(tid + j)*stride + k];
-      }
-      __syncthreads();
-    }
-
-    if (tid < 32) cs_blas_cuda_warp_reduce_sum<blockSize, stride>(sdata, tid);
-    if (tid == 0) {
-      #pragma unroll
-      for (size_t k = 0; k < stride; k++)
-        g_odata[k] = sdata[k];
-    }
-
-  }
-}
-
-#endif /* defined(__CUDACC__) */
-
-/*============================================================================
  * Public function prototypes
  *============================================================================*/
 

src/alge/cs_sles_it_cuda.cu
@@ -181,7 +181,7 @@ _jacobi_compute_vx_ini0(cs_lnum_t                       n_rows,
  * Compute Vx <- Vx - (A-diag).Rk and residual for Jacobi solver.
  *
  * This call must be followed by
- * cs_blas_cuda_reduce_single_block<block_size><<<1, block_size, 0>>>
+ * cs_cuda_reduce_sum_single_block<block_size><<<1, block_size, 0>>>
  *  (grid_size, sum_block, _r_reduce);
  * Also, block_size must be a power of 2.
  *
@@ -219,7 +219,7 @@ _jacobi_compute_vx_and_residual(cs_lnum_t                       n_rows,
   else
     sdata[tid] = 0.0;
 
-  cs_blas_cuda_block_reduce_sum<block_size, 1>(sdata, tid, sum_block);
+  cs_cuda_reduce_block_sum<block_size, 1>(sdata, tid, sum_block);
 }
 
 /*----------------------------------------------------------------------------
@@ -227,7 +227,7 @@ _jacobi_compute_vx_and_residual(cs_lnum_t                       n_rows,
  * when the initial value of Vx is zero.
  *
  * This call must be followed by
- * cs_blas_cuda_reduce_single_block<block_size><<<1, block_size, 0>>>
+ * cs_cuda_reduce_sum_single_block<block_size><<<1, block_size, 0>>>
  *  (grid_size, sum_block, _r_reduce);
  * Also, block_size must be a power of 2.
  *
@@ -263,7 +263,7 @@ _jacobi_compute_vx_and_residual_ini0(cs_lnum_t                       n_rows,
   else
     sdata[tid] = 0.0;
 
-  cs_blas_cuda_block_reduce_sum<block_size, 1>(sdata, tid, sum_block);
+  cs_cuda_reduce_block_sum<block_size, 1>(sdata, tid, sum_block);
 }
 
 /*----------------------------------------------------------------------------
@@ -348,7 +348,7 @@ _block_3_jacobi_compute_vx0(cs_lnum_t                       n_rows,
  * Compute residual for Jacobi with 3x3 block-diagonal matrix.
  *
  * This call must be followed by
- * cs_blas_cuda_reduce_single_block<block_size><<<1, block_size, 0>>>
+ * cs_cuda_reduce_sum_single_block<block_size><<<1, block_size, 0>>>
  *  (grid_size, sum_block, _r_reduce);
  * Also, block_size must be a power of 2.
  *
@@ -392,7 +392,7 @@ _block_3_jacobi_compute_residual(cs_lnum_t                       n_rows,
     sdata[tid] += (r*r);
   }
 
-  cs_blas_cuda_block_reduce_sum<block_size, 1>(sdata, tid, sum_block);
+  cs_cuda_reduce_block_sum<block_size, 1>(sdata, tid, sum_block);
 }
 
 /*----------------------------------------------------------------------------
@@ -485,7 +485,7 @@ _block_jacobi_compute_vx0(cs_lnum_t                       n_rows,
  * nxn block-diagonal matrix.
  *
  * This call must be followed by
- * cs_blas_cuda_reduce_single_block<block_size><<<1, block_size, 0>>>
+ * cs_cuda_reduce_sum_single_block<block_size><<<1, block_size, 0>>>
  *  (grid_size, sum_block, _r_reduce);
  * Also, block_size must be a power of 2.
  *
@@ -534,7 +534,7 @@ _block_jacobi_compute_residual(cs_lnum_t                       n_rows,
     sdata[tid] += (r*r);
   }
 
-  cs_blas_cuda_block_reduce_sum<block_size, 1>(sdata, tid, sum_block);
+  cs_cuda_reduce_block_sum<block_size, 1>(sdata, tid, sum_block);
 }
 
 /*----------------------------------------------------------------------------
@@ -664,7 +664,7 @@ _fcg_update_0(cs_lnum_t                      n,
  * Compute y <- y - x and stage 1 of resulting y.y.
  *
  * This call must be followed by
- * cs_blas_cuda_reduce_single_block<block_size><<<1, block_size, 0>>>
+ * cs_cuda_reduce_sum_single_block<block_size><<<1, block_size, 0>>>
  *  (grid_size, sum_block, _r_reduce);
  * Also, block_size must be a power of 2.
  *
@@ -698,14 +698,14 @@ _ymx_dot_yy(cs_lnum_t                       n,
     ii += grid_size;
   }
 
-  cs_blas_cuda_block_reduce_sum<block_size, 1>(sdata, tid, sum_block);
+  cs_cuda_reduce_block_sum<block_size, 1>(sdata, tid, sum_block);
 }
 
 /*----------------------------------------------------------------------------
  * Set x to 0 and y to -r and compute stage 1 of resulting y.y.
  *
  * This call must be followed by
- * cs_blas_cuda_reduce_single_block<block_size><<<1, block_size, 0>>>
+ * cs_cuda_reduce_sum_single_block<block_size><<<1, block_size, 0>>>
  *  (grid_size, sum_block, _r_reduce);
  * Also, block_size must be a power 2.
  *
@@ -742,14 +742,14 @@ _x0_ymr_dot_yy(cs_lnum_t                       n,
     ii += grid_size;
   }
 
-  cs_blas_cuda_block_reduce_sum<block_size, 1>(sdata, tid, sum_block);
+  cs_cuda_reduce_block_sum<block_size, 1>(sdata, tid, sum_block);
 }
 
 /*----------------------------------------------------------------------------
  * Compute y <- -alpha.x + y and stage 1 of resulting y.y.
  *
  * This call must be followed by
- * cs_blas_cuda_reduce_single_block<block_size><<<1, block_size, 0>>>
+ * cs_cuda_reduce_sum_single_block<block_size><<<1, block_size, 0>>>
  *  (grid_size, sum_block, _r_reduce);
  *
  * parameters:
@@ -783,14 +783,14 @@ _smaxpy_dot_yy(cs_lnum_t                       n,
     ii += grid_size;
   }
 
-  cs_blas_cuda_block_reduce_sum<block_size, 1>(sdata, tid, sum_block);
+  cs_cuda_reduce_block_sum<block_size, 1>(sdata, tid, sum_block);
 }
 
 /*----------------------------------------------------------------------------
  * Compute y <- alpha.y and stage 1 of following x.y dot product.
  *
  * This call must be followed by
- * cs_blas_cuda_reduce_single_block<block_size><<<1, block_size, 0>>>
+ * cs_cuda_reduce_sum_single_block<block_size><<<1, block_size, 0>>>
  *  (grid_size, sum_block, _r_reduce);
  *
  * parameters:
@@ -825,7 +825,7 @@ _y_scale_dot_xy(cs_lnum_t                      n,
     ii += grid_size;
   }
 
-  cs_blas_cuda_block_reduce_sum<block_size, 1>(sdata, tid, sum_block);
+  cs_cuda_reduce_block_sum<block_size, 1>(sdata, tid, sum_block);
 }
 
 /*----------------------------------------------------------------------------
@@ -1204,7 +1204,7 @@ _dot_product_xx_stage_1_of_2(cs_lnum_t    n,
 
   // Output: b_res for this block
 
-  cs_blas_cuda_block_reduce_sum<blockSize, 1>(stmp, tid, b_res);
+  cs_cuda_reduce_block_sum<blockSize, 1>(stmp, tid, b_res);
 }
 
 /*----------------------------------------------------------------------------*/
@@ -1243,7 +1243,7 @@ _dot_product_xy_stage_1_of_2(cs_lnum_t    n,
 
   // Output: b_res for this block
 
-  cs_blas_cuda_block_reduce_sum<blockSize, 1>(stmp, tid, b_res);
+  cs_cuda_reduce_block_sum<blockSize, 1>(stmp, tid, b_res);
 }
 
 /*----------------------------------------------------------------------------*/
@@ -1284,7 +1284,7 @@ _dot_products_xx_xy_stage_1_of_2(cs_lnum_t    n,
 
   // Output: b_res for this block
 
-  cs_blas_cuda_block_reduce_sum<blockSize, 2>(stmp, tid, b_res);
+  cs_cuda_reduce_block_sum<blockSize, 2>(stmp, tid, b_res);
 }
 
 /*----------------------------------------------------------------------------*/
@@ -1327,7 +1327,7 @@ _dot_products_xy_yz_stage_1_of_2(cs_lnum_t    n,
 
   // Output: b_res for this block
 
-  cs_blas_cuda_block_reduce_sum<blockSize, 2>(stmp, tid, b_res);
+  cs_cuda_reduce_block_sum<blockSize, 2>(stmp, tid, b_res);
 }
 
 /*----------------------------------------------------------------------------*/
@@ -1372,7 +1372,7 @@ _dot_products_xx_xy_yz_stage_1_of_2(cs_lnum_t    n,
 
   // Output: b_res for this block
 
-  cs_blas_cuda_block_reduce_sum<blockSize, 3>(stmp, tid, b_res);
+  cs_cuda_reduce_block_sum<blockSize, 3>(stmp, tid, b_res);
 }
 
 /*----------------------------------------------------------------------------*/
@@ -1421,7 +1421,7 @@ _dot_products_xx_yy_xy_xz_yz_stage_1_of_2(cs_lnum_t    n,
 
   // Output: b_res for this block
 
-  cs_blas_cuda_block_reduce_sum<blockSize, 5>(stmp, tid, b_res);
+  cs_cuda_reduce_block_sum<blockSize, 5>(stmp, tid, b_res);
 }
 
 /*----------------------------------------------------------------------------*/
@@ -1469,7 +1469,7 @@ _dot_products_vr_vw_vq_rr_stage_1_of_2(cs_lnum_t    n,
 
   // Output: b_res for this block
 
-  cs_blas_cuda_block_reduce_sum<blockSize, 4>(stmp, tid, b_res);
+  cs_cuda_reduce_block_sum<blockSize, 4>(stmp, tid, b_res);
 }
 
 /*----------------------------------------------------------------------------
@@ -1520,7 +1520,7 @@ _dot_products_vr_vw_vq_rr(const cs_sles_it_t  *c,
     _dot_products_vr_vw_vq_rr_stage_1_of_2
       <block_size><<<grid_size, block_size, 0, stream>>>
       (n, v, w, q, r, sum_block);
-    cs_blas_cuda_reduce_single_block<block_size, 4><<<1, block_size, 0, stream>>>
+    cs_cuda_reduce_sum_single_block<block_size, 4><<<1, block_size, 0, stream>>>
       (grid_size, sum_block, s_d);
 
     _sync_reduction_sum(c, stream, 4, s_d, s_h);
@@ -1603,7 +1603,7 @@ cs_sles_it_dot_product
     _dot_product_xy_stage_1_of_2
       <block_size><<<grid_size, block_size, 0, stream>>>
       (n, x, y, sum_block);
-    cs_blas_cuda_reduce_single_block<block_size, 1><<<1, block_size, 0, stream>>>
+    cs_cuda_reduce_sum_single_block<block_size, 1><<<1, block_size, 0, stream>>>
       (grid_size, sum_block, s_d);
 
     _sync_reduction_sum(c, stream, 1, s_d, s_h);
@@ -1674,7 +1674,7 @@ cs_sles_it_dot_product_xx
     _dot_product_xx_stage_1_of_2
       <block_size><<<grid_size, block_size, 0, stream>>>
       (n, x, sum_block);
-    cs_blas_cuda_reduce_single_block<block_size, 1><<<1, block_size, 0, stream>>>
+    cs_cuda_reduce_sum_single_block<block_size, 1><<<1, block_size, 0, stream>>>
       (grid_size, sum_block, s_d);
 
     _sync_reduction_sum(c, stream, 1, s_d, s_h);
@@ -1748,7 +1748,7 @@ cs_sles_it_dot_products_xx_xy
     _dot_products_xx_xy_stage_1_of_2
       <block_size><<<grid_size, block_size, 0, stream>>>
       (n, x, y, sum_block);
-    cs_blas_cuda_reduce_single_block<block_size, 2><<<1, block_size, 0, stream>>>
+    cs_cuda_reduce_sum_single_block<block_size, 2><<<1, block_size, 0, stream>>>
       (grid_size, sum_block, s_d);
 
     _sync_reduction_sum(c, stream, 2, s_d, s_h);
@@ -1826,7 +1826,7 @@ cs_sles_it_dot_products_xy_yz
     _dot_products_xy_yz_stage_1_of_2
       <block_size><<<grid_size, block_size, 0, stream>>>
       (n, x, y, z, sum_block);
-    cs_blas_cuda_reduce_single_block<block_size, 2><<<1, block_size, 0, stream>>>
+    cs_cuda_reduce_sum_single_block<block_size, 2><<<1, block_size, 0, stream>>>
       (grid_size, sum_block, s_d);
 
     _sync_reduction_sum(c, stream, 2, s_d, s_h);
@@ -1906,7 +1906,7 @@ cs_sles_it_dot_products_xx_xy_yz
     _dot_products_xx_xy_yz_stage_1_of_2
       <block_size><<<grid_size, block_size, 0, stream>>>
       (n, x, y, z, sum_block);
-    cs_blas_cuda_reduce_single_block<block_size, 3><<<1, block_size, 0, stream>>>
+    cs_cuda_reduce_sum_single_block<block_size, 3><<<1, block_size, 0, stream>>>
       (grid_size, sum_block, s_d);
 
     _sync_reduction_sum(c, stream, 3, s_d, s_h);
@@ -1992,7 +1992,7 @@ cs_sles_it_dot_products_xx_yy_xy_xz_yz
     _dot_products_xx_yy_xy_xz_yz_stage_1_of_2
       <block_size><<<grid_size, block_size, 0, stream>>>
       (n, x, y, z, sum_block);
-    cs_blas_cuda_reduce_single_block<block_size, 5><<<1, block_size, 0, stream>>>
+    cs_cuda_reduce_sum_single_block<block_size, 5><<<1, block_size, 0, stream>>>
       (grid_size, sum_block, s_d);
 
     _sync_reduction_sum(c, stream, 5, s_d, s_h);
@@ -2157,7 +2157,7 @@ cs_sles_it_cuda_jacobi(cs_sles_it_t              *c,
       _jacobi_compute_vx_and_residual_ini0
         <blocksize><<<gridsize, blocksize, 0, stream>>>
           (n_rows, ad_inv, rhs, vx, rk, sum_block);
-      cs_blas_cuda_reduce_single_block<blocksize, 1><<<1, blocksize, 0, stream>>>
+      cs_cuda_reduce_sum_single_block<blocksize, 1><<<1, blocksize, 0, stream>>>
         (gridsize, sum_block, res_d);
       _sync_reduction_sum(c, stream, 1, res_d, res_h);
       residual = sqrt(*res_h); /* Actually, residual of previous iteration */
@@ -2195,7 +2195,7 @@ cs_sles_it_cuda_jacobi(cs_sles_it_t              *c,
     if (update_residual) {
       _jacobi_compute_vx_and_residual<blocksize><<<gridsize, blocksize, 0, stream>>>
         (n_rows, ad_inv, ad, rhs, vx, rk, sum_block);
-      cs_blas_cuda_reduce_single_block<blocksize, 1><<<1, blocksize, 0, stream>>>
+      cs_cuda_reduce_sum_single_block<blocksize, 1><<<1, blocksize, 0, stream>>>
         (gridsize, sum_block, res_d);
     }
     else {
@@ -2235,7 +2235,7 @@ cs_sles_it_cuda_jacobi(cs_sles_it_t              *c,
       if (update_residual) {
         _jacobi_compute_vx_and_residual<blocksize><<<gridsize, blocksize, 0, stream>>>
           (n_rows, ad_inv, ad, rhs, vx, rk, sum_block);
-        cs_blas_cuda_reduce_single_block<blocksize, 1><<<1, blocksize, 0, stream>>>
+        cs_cuda_reduce_sum_single_block<blocksize, 1><<<1, blocksize, 0, stream>>>
           (gridsize, sum_block, res_d);
       }
       else
@@ -2419,7 +2419,7 @@ cs_sles_it_cuda_block_jacobi(cs_sles_it_t              *c,
     {cs_real_t *v_t = vx_k; vx_k = rk; rk = v_t;}
 
     if (update_residual) {
-      cs_blas_cuda_reduce_single_block<blocksize, 1><<<1, blocksize, 0, stream>>>
+      cs_cuda_reduce_sum_single_block<blocksize, 1><<<1, blocksize, 0, stream>>>
         (gridsize, sum_block, res_d);
       _sync_reduction_sum(c, stream, 1, res_d, res_h);
       residual = sqrt(*res_h); /* Actually, residual of previous iteration */
@@ -2474,7 +2474,7 @@ cs_sles_it_cuda_block_jacobi(cs_sles_it_t              *c,
     }
 
     if (update_residual) {
-      cs_blas_cuda_reduce_single_block<blocksize, 1><<<1, blocksize, 0, stream>>>
+      cs_cuda_reduce_sum_single_block<blocksize, 1><<<1, blocksize, 0, stream>>>
         (gridsize, sum_block, res_d);
       _sync_reduction_sum(c, stream, 1, res_d, res_h);
       residual = sqrt(*res_h); /* Actually, residual of previous iteration */
@@ -2825,7 +2825,7 @@ cs_sles_it_cuda_gcr(cs_sles_it_t              *c,
   // CS_MALLOC_HD(gkj_inv, n_gkj, cs_real_t, CS_ALLOC_HOST_DEVICE_SHARED);
 
   const unsigned int blocksize = CS_BLOCKSIZE;
-  const unsigned int blocksize_rsb = 512; /* cs_blas_cuda_reduce_single_block */
+  const unsigned int blocksize_rsb = 512; /* cs_cuda_reduce_sum_single_block */
 
   cs_cuda_grid_size(n_rows, blocksize);
 

src/base/cs_cuda_reduce.h
@@ -69,8 +69,8 @@
 
 template <size_t blockSize, size_t stride, typename T>
 __device__ static void __forceinline__
-cs_cuda_reduce_warp_reduce_sum(T           *stmp,
-                               size_t       tid)
+cs_cuda_reduce_warp_sum(volatile T  *stmp,
+                        size_t       tid)
 {
   if (stride == 1) {
 
@@ -139,9 +139,9 @@ cs_cuda_reduce_warp_reduce_sum(T           *stmp,
 
 template <size_t blockSize, size_t stride, typename T>
 __device__ static void __forceinline__
-cs_cuda_reduce_block_reduce_sum(T       *stmp,
-                                size_t   tid,
-                                T       *sum_block)
+cs_cuda_reduce_block_sum(T       *stmp,
+                         size_t   tid,
+                         T       *sum_block)
 {
   __syncthreads();
 
@@ -173,7 +173,7 @@ cs_cuda_reduce_block_reduce_sum(T       *stmp,
     }
 
     if (tid < 32) {
-      cs_cuda_reduce_warp_reduce_sum<blockSize, stride>(stmp, tid);
+      cs_cuda_reduce_warp_sum<blockSize, stride>(stmp, tid);
     }
 
     // Output: b_res for this block
@@ -216,7 +216,7 @@ cs_cuda_reduce_block_reduce_sum(T       *stmp,
     }
 
     if (tid < 32)
-      cs_cuda_reduce_warp_reduce_sum<blockSize, stride>(stmp, tid);
+      cs_cuda_reduce_warp_sum<blockSize, stride>(stmp, tid);
 
     // Output: b_res for this block
 
@@ -270,7 +270,7 @@ cs_cuda_reduce_sum_single_block(size_t   n,
       __syncthreads();
     }
 
-    if (tid < 32) cs_cuda_reduce_warp_reduce_sum<blockSize, stride>(sdata, tid);
+    if (tid < 32) cs_cuda_reduce_warp_sum<blockSize, stride>(sdata, tid);
     if (tid == 0) *g_odata = sdata[0];
 
   }
@@ -303,7 +303,7 @@ cs_cuda_reduce_sum_single_block(size_t   n,
       __syncthreads();
     }
 
-    if (tid < 32) cs_cuda_reduce_warp_reduce_sum<blockSize, stride>(sdata, tid);
+    if (tid < 32) cs_cuda_reduce_warp_sum<blockSize, stride>(sdata, tid);
     if (tid == 0) {
       #pragma unroll
       for (size_t k = 0; k < stride; k++)

src/base/cs_dispatch.h
@@ -553,16 +553,16 @@ cs_cuda_kernel_parallel_for_reduce_sum(cs_lnum_t   n,
 
   switch (blockDim.x) {
   case 1024:
-    cs_cuda_reduce_block_reduce_sum<1024, 1>(stmp, tid, b_res);
+    cs_cuda_reduce_block_sum<1024, 1>(stmp, tid, b_res);
     break;
   case 512:
-    cs_cuda_reduce_block_reduce_sum<512, 1>(stmp, tid, b_res);
+    cs_cuda_reduce_block_sum<512, 1>(stmp, tid, b_res);
     break;
   case 256:
-    cs_cuda_reduce_block_reduce_sum<256, 1>(stmp, tid, b_res);
+    cs_cuda_reduce_block_sum<256, 1>(stmp, tid, b_res);
     break;
   case 128:
-    cs_cuda_reduce_block_reduce_sum<128, 1>(stmp, tid, b_res);
+    cs_cuda_reduce_block_sum<128, 1>(stmp, tid, b_res);
     break;
   default:
     assert(0);

tests/cs_gpu_cuda_test.cu
@@ -54,6 +54,7 @@ namespace cg = cooperative_groups;
 #include "base/cs_base_accel.h"
 #include "base/cs_base_cuda.h"
 #include "base/cs_cuda_contrib.h"
+#include "base/cs_cuda_reduce.h"
 
 #include "base/cs_system_info.h"
 
@@ -107,7 +108,7 @@ _reduce_single_block(size_t   n,
     __syncthreads();
   }
 
-  if (tid < 32) cs_blas_cuda_warp_reduce_sum<blockSize, 1>(sdata, tid);
+  if (tid < 32) cs_cuda_reduce_warp_sum<blockSize, 1>(sdata, tid);
   if (tid == 0) *g_odata = sdata[0];
 }
 
@@ -155,8 +156,9 @@ _dot_xy_stage_1_of_2(cs_lnum_t    n,
     __syncthreads();
   }
 
-  if (tid < 32)
-    cs_blas_cuda_warp_reduce_sum<blockSize, 1>(stmp, tid);
+  if (tid < 32) {
+    cs_cuda_reduce_warp_sum<blockSize, 1>(stmp, tid);
+  }
 
   // Output: b_res for this block
 